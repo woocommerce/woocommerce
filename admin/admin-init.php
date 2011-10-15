edb0ab6763ad54115124c5475acf3ef6571fb63c
Mike Jolley <mike.jolley@me.com>
2011-10-15 12:16:56 +0100

    css

--- admin/admin-init.php
+++ admin/admin-init.php
@@ -149,6 +149,8 @@ function woocommerce_admin_css() {
 		wp_enqueue_style( 'woocommerce_admin_styles', $woocommerce->plugin_url() . '/assets/css/admin.css' );
 		wp_enqueue_style( 'jquery-ui-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/smoothness/jquery-ui.css' );
 	endif;
+	
+	do_action('woocommerce_admin_css');
 }
 
 /**
