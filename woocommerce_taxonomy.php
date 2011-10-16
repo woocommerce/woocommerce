12f27f1307ae16075c17c8728c89745287d8761b
Mike Jolley <mike.jolley@me.com>
2011-10-16 12:34:15 +0100

    Fixed backend permalink

--- woocommerce_taxonomy.php
+++ woocommerce_taxonomy.php
@@ -300,9 +300,6 @@ function woocommerce_product_cat_filter_post_link( $permalink, $post, $leavename
     
     // Abort early if the placeholder rewrite tag isn't in the generated URL
     if ( false === strpos( $permalink, '%product_cat%' ) ) return $permalink;
-    
-    // Sample aborts
-    if ($sample) return $permalink;
 
     // Get the custom taxonomy terms in use by this post
     $terms = get_the_terms( $post->ID, 'product_cat' );
