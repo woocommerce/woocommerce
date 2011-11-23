73aad1810e1ebf69e9a08087a05efcd30a5d4571
Mike Jolley <mike.jolley@me.com>
2011-11-23 00:07:50 +0000

    Added strings file for localisation's. Closes #227.
    Re-scan to translate order status names.

---
+++ languages/strings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Strings
+ * 
+ * This file holds localisation strings that are scanned by localisation plugins. It is not included anywhere.
+ *
+ * @author 		WooThemes
+ * @category 	Languages
+ * @package 	WooCommerce
+ */
+
+// Order Status Terms
+__('pending', 'woothemes');
+__('failed', 'woothemes');
+__('on-hold', 'woothemes');
+__('processing', 'woothemes');
+__('completed', 'woothemes');
+__('refunded', 'woothemes');
+__('cancelled', 'woothemes');
