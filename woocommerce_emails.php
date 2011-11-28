6d1729e03eedb532750b45fc3b56e72da09f2897
Mike Jolley <mike.jolley@me.com>
2011-11-28 11:10:34 +0000

    Made stock notifications use email template

--- woocommerce_emails.php
+++ woocommerce_emails.php
@@ -250,23 +250,56 @@ function woocommerce_customer_note_notification( $id, $note ) {
  * Low stock notification email
  **/
 function woocommerce_low_stock_notification( $product ) {
+	global $email_heading;
+	
 	$_product = &new woocommerce_product($product);
+	
+	$email_heading = __('Product low in stock', 'woothemes');
+	
 	$subject = '[' . get_bloginfo('name') . '] ' . __('Product low in stock', 'woothemes');
-	$message = '#' . $_product->id .' '. $_product->get_title() . ' ('. $_product->sku.') ' . __('is low in stock.', 'woothemes');
-	$message = wordwrap( html_entity_decode( strip_tags( $message ) ), 70 );
-	wp_mail( get_option('woocommerce_stock_email_recipient'), $subject, $message );
-}
+	
+	// Buffer
+	ob_start();
 
+	do_action('woocommerce_email_header');
+	
+	echo wpautop(wptexturize( '#' . $_product->id .' '. $_product->get_title() . ' ('. $_product->sku.') ' . __('is low in stock.', 'woothemes') ));
+	
+	do_action('woocommerce_email_footer');
+	
+	// Get contents
+	$message = ob_get_clean();
+
+	// Send the mail
+	woocommerce_mail( get_option('woocommerce_stock_email_recipient'), $subject, $message );
+}
 
 /**
  * No stock notification email
  **/
 function woocommerce_no_stock_notification( $product ) {
+	global $email_heading;
+	
 	$_product = &new woocommerce_product($product);
+	
+	$email_heading = __('Product out of stock', 'woothemes');
+	
 	$subject = '[' . get_bloginfo('name') . '] ' . __('Product out of stock', 'woothemes');
-	$message = '#' . $_product->id .' '. $_product->get_title() . ' ('. $_product->sku.') ' . __('is out of stock.', 'woothemes');
-	$message = wordwrap( html_entity_decode( strip_tags( $message ) ), 70 );
-	wp_mail( get_option('woocommerce_stock_email_recipient'), $subject, $message );
+	
+	// Buffer
+	ob_start();
+
+	do_action('woocommerce_email_header');
+	
+	echo wpautop(wptexturize( '#' . $_product->id .' '. $_product->get_title() . ' ('. $_product->sku.') ' . __('is out of stock.', 'woothemes') ));
+	
+	do_action('woocommerce_email_footer');
+	
+	// Get contents
+	$message = ob_get_clean();
+
+	// Send the mail
+	woocommerce_mail( get_option('woocommerce_stock_email_recipient'), $subject, $message );
 }
 
 
@@ -274,11 +307,28 @@ function woocommerce_no_stock_notification( $product ) {
  * Backorder notification email
  **/
 function woocommerce_product_on_backorder_notification( $product, $amount ) {
+	global $email_heading;
+	
 	$_product = &new woocommerce_product($product);
+	
+	$email_heading = __('Product Backorder', 'woothemes');
+	
 	$subject = '[' . get_bloginfo('name') . '] ' . __('Product Backorder', 'woothemes');
-	$message = $amount . __(' units of #', 'woothemes') . $_product->id .' '. $_product->get_title() . ' ('. $_product->sku.') ' . __('have been backordered.', 'woothemes');
-	$message = wordwrap( html_entity_decode( strip_tags( $message ) ), 70 );
-	wp_mail( get_option('woocommerce_stock_email_recipient'), $subject, $message );
+
+	// Buffer
+	ob_start();
+
+	do_action('woocommerce_email_header');
+	
+	echo wpautop(wptexturize( $amount . __(' units of #', 'woothemes') . $_product->id .' '. $_product->get_title() . ' ('. $_product->sku.') ' . __('have been backordered.', 'woothemes') ));
+	
+	do_action('woocommerce_email_footer');
+	
+	// Get contents
+	$message = ob_get_clean();
+
+	// Send the mail
+	woocommerce_mail( get_option('woocommerce_stock_email_recipient'), $subject, $message );
 }
 
 /**
