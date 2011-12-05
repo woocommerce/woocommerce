123a887af11ace2b7d985ddf82ee02dd79b7a8a5
Mike Jolley <mike.jolley@me.com>
2011-12-05 17:18:07 +0000

    CZK currency Closes #331.

--- woocommerce.php
+++ woocommerce.php
@@ -438,8 +438,8 @@ function get_woocommerce_currency_symbol() {
 		case 'TRY' : $currency_symbol = 'TL'; break;
 		case 'NOK' : $currency_symbol = 'kr'; break;
 		case 'ZAR' : $currency_symbol = 'R'; break;
-		
-		case 'CZK' :
+		case 'CZK' : $currency_symbol = '&#75;&#269;'; break;
+
 		case 'DKK' :
 		case 'HUF' :
 		case 'ILS' :
