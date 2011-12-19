8788f8fb686318b1b31662724467e3c7e07ecb22
Mike Jolley <mike.jolley@me.com>
2011-12-19 10:52:54 +0000

    Products short code fix

--- assets/js/admin/editor_plugin.js
+++ assets/js/admin/editor_plugin.js
@@ -23,7 +23,7 @@
 								
 								a.addImmediate(b,"Product price/cart button", '[add_to_cart id="" sku=""]');
 								a.addImmediate(b,"Product by SKU/ID", '[product id="" sku=""]');
-								a.addImmediate(b,"Products by SKU/ID", '[product ids="" skus=""]');
+								a.addImmediate(b,"Products by SKU/ID", '[products ids="" skus=""]');
 								a.addImmediate(b,"Products by category slug", '[product_category category="" per_page="12" columns="4" orderby="date" order="desc"]');
 								
 								b.addSeparator();
