edb03258c61619a8d11132f3335a41ab21980b4c
Mike Jolley <mike.jolley@me.com>
2011-12-21 00:44:26 +0000

    Thumbnail total

--- templates/single-product/product-image.php
+++ templates/single-product/product-image.php
@@ -9,7 +9,7 @@ global $post, $woocommerce;
 
 	<?php if (has_post_thumbnail()) : $thumb_id = get_post_thumbnail_id(); $large_thumbnail_size = apply_filters('single_product_large_thumbnail_size', 'shop_single'); ?>
 
-		<a itemprop="image" href="<?php echo wp_get_attachment_url($thumb_id); ?>" class="zoom" rel="thumbnails"><?php echo get_the_post_thumbnail($post->ID, $large_thumbnail_size) ?></a>
+		<a itemprop="image" href="<?php echo wp_get_attachment_url($thumb_id); ?>" class="zoom" rel="thumbnails" title="<?php echo get_the_title( $thumb_id ); ?>"><?php echo get_the_post_thumbnail($post->ID, $large_thumbnail_size) ?></a>
 
 	<?php else : ?>
 	
