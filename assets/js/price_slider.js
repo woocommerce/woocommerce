1697bcd66cc4173795e3204f7e609e2e7cb4af42
Mike Jolley <mike.jolley@me.com>
2012-02-16 18:12:14 +0000

    slider triggers

--- assets/js/price_slider.js
+++ assets/js/price_slider.js
@@ -14,8 +14,7 @@ jQuery(document).ready(function($) {
 	if (woocommerce_price_slider_params.min_price) current_min_price = parseInt(woocommerce_price_slider_params.min_price);
 	if (woocommerce_price_slider_params.max_price) current_max_price = parseInt(woocommerce_price_slider_params.max_price);
 	
-	function update_price_span( min, max ) {
-	
+	$('body').bind('price_slider_create price_slider_slide', function( event, min, max ) {
 		if (woocommerce_price_slider_params.currency_pos == "left") {
 		
 			$( ".price_slider_amount span.from" ).html( woocommerce_price_slider_params.currency_symbol + min );
@@ -37,8 +36,7 @@ jQuery(document).ready(function($) {
 			$( ".price_slider_amount span.to" ).html( max + " " + woocommerce_price_slider_params.currency_symbol );
 			
 		}
-			
-	}
+	});
 	
 	$('.price_slider').slider({
 		range: true,
@@ -48,22 +46,23 @@ jQuery(document).ready(function($) {
 		values: [current_min_price,current_max_price],
 		create : function( event, ui ) {
 			
-			update_price_span(current_min_price, current_max_price);
-			
 			$( ".price_slider_amount #min_price" ).val(current_min_price);
 			$( ".price_slider_amount #max_price" ).val(current_max_price);
 			
-			$('body').trigger('price_slider_create');
+			$('body').trigger('price_slider_create', [current_min_price, current_max_price]);
 		},
 		slide: function( event, ui ) {
 			
-			update_price_span(ui.values[ 0 ], ui.values[ 1 ]);
-
 			$( "input#min_price" ).val(ui.values[ 0 ]);
 			$( "input#max_price" ).val(ui.values[ 1 ]);
 			
-			$('body').trigger('price_slider_slide');
-		}
+			$('body').trigger('price_slider_slide', [ui.values[ 0 ], ui.values[ 1 ]] );
+		},
+		change: function( event, ui ) {
+			
+			$('body').trigger('price_slider_change', [ui.values[ 0 ], ui.values[ 1 ]] );
+			
+		},
 	});
 	
 });
