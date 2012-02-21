00aec064c535d71c5a735971d3c97063c1e8933a
Mike Jolley <mike.jolley@me.com>
2012-02-21 13:40:05 +0000

    Hooks for add to cart text (single)

--- templates/single-product/add-to-cart/external.php
+++ templates/single-product/add-to-cart/external.php
@@ -12,6 +12,6 @@ if (!$product_url) return;
 
 <?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
-<p class="cart"><a href="<?php echo $product_url; ?>" rel="nofollow" class="button alt"><?php _e('Buy product', 'woocommerce'); ?></a></p>
+<p class="cart"><a href="<?php echo $product_url; ?>" rel="nofollow" class="button alt"><?php echo apply_filters('single_add_to_cart_text', __('Buy product', 'woocommerce'), $product->product_type); ?></a></p>
 
 <?php do_action('woocommerce_after_add_to_cart_button'); ?>
