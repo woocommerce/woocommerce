woocommerce/woocommerce

commit fdc122214e797d77cacc3a4e32a7fa7c6c0372ee

localisation to init

woocommerce.php
@@ -82,9 +82,6 @@ class Woocommerce {
 		// Define version constant
 		define( 'WOOCOMMERCE_VERSION', $this->version );
 		
-		// Set up localisation
-		$this->load_plugin_textdomain();
-		
 		// Include required files
 		$this->includes();
 		
@@ -143,16 +140,6 @@ class Woocommerce {
 			add_action( 'wp_footer', array(&$this, 'output_inline_js'), 25);
 		
 		endif;
-	}
-	
-	/**
-	 * Localisation
-	 **/
-	function load_plugin_textdomain() {
-		$variable_lang = (get_option('woocommerce_informal_localisation_type')=='yes') ? 'informal' : 'formal';
-		load_plugin_textdomain('woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages');
-		load_plugin_textdomain('woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/../../languages/woocommerce');
-		load_plugin_textdomain('woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' . $variable_lang );
 	}
 
 	/**
@@ -277,7 +264,10 @@ class Woocommerce {
 	 * Init WooCommerce when WordPress Initialises
 	 **/
 	function init() {
-	
+		
+		// Set up localisation
+		$this->load_plugin_textdomain();
+
 		// Register globals for WC environment
 		$this->register_globals();
 
@@ -294,6 +284,16 @@ class Woocommerce {
 		if (!is_admin()) $this->init_styles();
 
 		do_action( 'woocommerce_init' );
+	}
+	
+	/**
+	 * Localisation
+	 **/
+	function load_plugin_textdomain() {
+		$variable_lang = (get_option('woocommerce_informal_localisation_type')=='yes') ? 'informal' : 'formal';
+		load_plugin_textdomain('woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages');
+		load_plugin_textdomain('woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/../../languages/woocommerce');
+		load_plugin_textdomain('woocommerce', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' . $variable_lang );
 	}
 	
 	/**

commit 4d43dd700717d01ffa794d3be0241615fa78a402

Fixed localisation init
Bug introduced by fdc122214e797d77cacc3a4e32a7fa7c6c0372ee

woocommerce.php
@@ -81,7 +81,10 @@ class Woocommerce {
 		
 		// Define version constant
 		define( 'WOOCOMMERCE_VERSION', $this->version );
-		
+
+		// Set up localisation
+		$this->load_plugin_textdomain();
+
 		// Include required files
 		$this->includes();
 		
@@ -265,10 +268,6 @@ class Woocommerce {
 	 * Init WooCommerce when WordPress Initialises
 	 **/
 	function init() {
-		
-		// Set up localisation
-		$this->load_plugin_textdomain();
-
 		// Register globals for WC environment
 		$this->register_globals();
 