798ceb8c00d4001f119ccfefe5f9b8e8cbaeb205
Mike Jolley <mike.jolley@me.com>
2012-03-23 11:05:00 +0000

    current_page_parent for shop page when viewing a product

--- woocommerce-functions.php
+++ woocommerce-functions.php
@@ -54,32 +54,35 @@ function woocommerce_redirects() {
  **/
 function woocommerce_nav_menu_item_classes( $menu_items, $args ) {
 	
-	if (!is_woocommerce()) return $menu_items;
+	if ( ! is_woocommerce() ) return $menu_items;
 	
 	$shop_page 		= (int) woocommerce_get_page_id('shop');
 	$page_for_posts = (int) get_option( 'page_for_posts' );
 
-	foreach ( (array) $menu_items as $key => $menu_item ) :
+	foreach ( (array) $menu_items as $key => $menu_item ) {
 
 		$classes = (array) $menu_item->classes;
 
 		// Unset active class for blog page
-		if ( $page_for_posts == $menu_item->object_id ) :
+		if ( $page_for_posts == $menu_item->object_id ) {
 			$menu_items[$key]->current = false;
 			unset( $classes[ array_search('current_page_parent', $classes) ] );
 			unset( $classes[ array_search('current-menu-item', $classes) ] );
 
 		// Set active state if this is the shop page link
-		elseif ( is_shop() && $shop_page == $menu_item->object_id ) :
+		} elseif ( is_shop() && $shop_page == $menu_item->object_id ) {
 			$menu_items[$key]->current = true;
 			$classes[] = 'current-menu-item';
 			$classes[] = 'current_page_item';
 		
-		endif;
+		// Set parent state if this is a product page
+		} elseif ( is_singular( 'product' ) && $shop_page == $menu_item->object_id ) {
+			$classes[] = 'current_page_parent';
+		}
 
 		$menu_items[$key]->classes = array_unique( $classes );
 	
-	endforeach;
+	}
 
 	return $menu_items;
 }
