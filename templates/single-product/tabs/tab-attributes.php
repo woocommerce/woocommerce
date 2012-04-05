57570b62c1625ce607b457eb137012290a02bec1
Coen Jacobs <coenjacobs@gmail.com>
2012-04-05 10:46:39 +0200

    Last commit was a bit rude. Also hide panels containing the data. Closes #892

--- templates/single-product/tabs/tab-attributes.php
+++ templates/single-product/tabs/tab-attributes.php
@@ -1,10 +1,10 @@
 <?php global $product;
 
-if ( get_option('woocommerce_enable_dimension_product_attributes') == 'yes' ) {
-	if ( $product->has_attributes() || $product->has_dimensions() || $product->has_weight() ) {
-		?>
-		<li><a href="#tab-attributes"><?php _e('Additional Information', 'woocommerce'); ?></a></li>
-		<?php
-	}
+$show_attr = ( get_option( 'woocommerce_enable_dimension_product_attributes' ) == 'yes' ? true : false );
+
+if ( $product->has_attributes() || ( $show_attr && $product->has_dimensions() ) || ( $show_attr && $product->has_weight() ) ) {
+	?>
+	<li><a href="#tab-attributes"><?php _e('Additional Information', 'woocommerce'); ?></a></li>
+	<?php
 }
 ?>
