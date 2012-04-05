2fa6a9313dead3c15027f74fbdb4efdd2c107666
Coen Jacobs <coenjacobs@gmail.com>
2012-04-05 10:34:11 +0200

    Check if weight and dimension fields should be shown before checking values. Closes #892

--- templates/single-product/tabs/tab-attributes.php
+++ templates/single-product/tabs/tab-attributes.php
@@ -1,5 +1,10 @@
 <?php global $product;
 
-if ($product->has_attributes() || $product->has_dimensions() || $product->has_weight()) : ?>
-	<li><a href="#tab-attributes"><?php _e('Additional Information', 'woocommerce'); ?></a></li>
-<?php endif; ?>
+if ( get_option('woocommerce_enable_dimension_product_attributes') == 'yes' ) {
+	if ( $product->has_attributes() || $product->has_dimensions() || $product->has_weight() ) {
+		?>
+		<li><a href="#tab-attributes"><?php _e('Additional Information', 'woocommerce'); ?></a></li>
+		<?php
+	}
+}
+?>
