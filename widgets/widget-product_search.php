444a4c2fb183b0c4f84e7b12fea191f92c0b9fb1
Geert De Deckere <geertdd@gmail.com>
2012-04-16 07:00:36 +0200

    Escaped search form attributes

--- widgets/widget-product_search.php
+++ widgets/widget-product_search.php
@@ -46,8 +46,8 @@ class WooCommerce_Widget_Product_Search extends WP_Widget {
 		<form role="search" method="get" id="searchform" action="<?php echo esc_url( home_url() ); ?>">
 			<div>
 				<label class="screen-reader-text" for="s"><?php _e('Search for:', 'woocommerce'); ?></label>
-				<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="<?php _e('Search for products', 'woocommerce'); ?>" />
-				<input type="submit" id="searchsubmit" value="<?php _e('Search', 'woocommerce'); ?>" />
+				<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="<?php esc_attr_e('Search for products', 'woocommerce'); ?>" />
+				<input type="submit" id="searchsubmit" value="<?php esc_attr_e('Search', 'woocommerce'); ?>" />
 				<input type="hidden" name="post_type" value="product" />
 			</div>
 		</form>
