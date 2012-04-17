adc0d01611f4d7987ac35f511316ece8eed691f6
Brent Shepherd <thenbrent@gmail.com>
2012-04-18 08:44:33 +1000

    Replacing use of deprecated function get_current_theme() in versions of WP newer than 3.4 where the replacement wp_get_theme() exists.

--- woocommerce.php
+++ woocommerce.php
@@ -424,8 +424,9 @@ class Woocommerce {
 	 * Add body classes
 	 **/
 	function wp_head() {
-		$this->add_body_class('theme-' . strtolower( get_current_theme() ));
-	
+		$theme_name = ( function_exists( 'wp_get_theme' ) ) ? wp_get_theme() : get_current_theme();
+		$this->add_body_class( 'theme-' . strtolower( $theme_name ) );
+
 		if ( is_woocommerce() ) $this->add_body_class('woocommerce');
 		
 		if ( is_checkout() ) $this->add_body_class('woocommerce-checkout');
