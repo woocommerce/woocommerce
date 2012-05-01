c8ff0c22d53cb3436f34e1a6e6eeb63437019618
James Koster <james@jameskoster.co.uk>
2012-05-01 21:26:58 +0100

    added classes to product tabs

--- templates/single-product/tabs/tab-attributes.php
+++ templates/single-product/tabs/tab-attributes.php
@@ -4,7 +4,7 @@ $show_attr = ( get_option( 'woocommerce_enable_dimension_product_attributes' ) =
 
 if ( $product->has_attributes() || ( $show_attr && $product->has_dimensions() ) || ( $show_attr && $product->has_weight() ) ) {
 	?>
-	<li><a href="#tab-attributes"><?php _e('Additional Information', 'woocommerce'); ?></a></li>
+	<li class="attributes_tab"><a href="#tab-attributes"><?php _e('Additional Information', 'woocommerce'); ?></a></li>
 	<?php
 }
 ?>
--- templates/single-product/tabs/tab-description.php
+++ templates/single-product/tabs/tab-description.php
@@ -1,5 +1,5 @@
 <?php global $post;
 
 if ( $post->post_content ) : ?>
-	<li><a href="#tab-description"><?php _e('Description', 'woocommerce'); ?></a></li>
+	<li class="description_tab"><a href="#tab-description"><?php _e('Description', 'woocommerce'); ?></a></li>
 <?php endif; ?>
