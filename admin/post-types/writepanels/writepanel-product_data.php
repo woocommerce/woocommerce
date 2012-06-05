ed4067dee99200e18588c153472b2448b9c52ce5
Mike Jolley <mike.jolley@me.com>
2012-06-05 23:54:38 +0100

    trim sku

--- admin/post-types/writepanels/writepanel-product_data.php
+++ admin/post-types/writepanels/writepanel-product_data.php
@@ -568,8 +568,8 @@ function woocommerce_process_product_meta( $post_id, $post ) {
 		
 	// Unique SKU 
 	$sku = get_post_meta($post_id, '_sku', true);
-	$new_sku = esc_html(stripslashes( $_POST['_sku'] ));
-	if ($new_sku=='') :
+	$new_sku = esc_html( trim( stripslashes( $_POST['_sku'] ) ) );
+	if ( $new_sku == '' ) :
 		update_post_meta( $post_id, '_sku', '' );
 	elseif ($new_sku!==$sku) :
 		if ($new_sku && !empty($new_sku)) :
