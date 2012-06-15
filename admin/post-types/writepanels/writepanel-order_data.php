e689692cd9c3e470daf2dc9863b2a05d20e79732
Geert De Deckere <geertdd@gmail.com>
2012-06-15 12:34:33 +0200

    Fix whitespace

--- admin/post-types/writepanels/writepanel-order_data.php
+++ admin/post-types/writepanels/writepanel-order_data.php
@@ -85,7 +85,7 @@ function woocommerce_order_data_meta_box($post) {
 				<p class="form-field form-field-wide"><label for="excerpt"><?php _e('Customer Note:', 'woocommerce') ?></label>
 				<textarea rows="1" cols="40" name="excerpt" tabindex="6" id="excerpt" placeholder="<?php _e('Customer\'s notes about the order', 'woocommerce'); ?>"><?php echo $post->post_excerpt; ?></textarea></p>
 
-				<?php do_action('woocommerce_admin_order_data_after_order_details', $order->id); ?>
+				<?php do_action( 'woocommerce_admin_order_data_after_order_details', $order->id ); ?>
 
 			</div>
 			<div class="order_data_right">
@@ -168,7 +168,7 @@ function woocommerce_order_data_meta_box($post) {
 						
 						echo '</div>';
 
-						do_action('woocommerce_admin_order_data_after_billing_address', $order->id);
+						do_action( 'woocommerce_admin_order_data_after_billing_address', $order->id );
 					?>
 				</div>
 				<div class="order_data order_data_alt">
@@ -245,7 +245,7 @@ function woocommerce_order_data_meta_box($post) {
 						
 						echo '</div>';
 						
-						do_action('woocommerce_admin_order_data_after_shipping_address', $order->id);
+						do_action( 'woocommerce_admin_order_data_after_shipping_address', $order->id );
 					?>
 				</div>
 			</div>
