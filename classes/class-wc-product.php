2b542809997002c00eabe37ec8db2812cc18e636
matt <matt@thecoderepublic.com>
2012-06-29 11:08:50 -0400

    Modify WC_Product::get_image() for return
    Function currently only echos. Commit enables function to return product
    image. WCP+A replaces the product thumbnail with photo preview
    thumbnail. Original function showed two thumbnails directly next to each
    other.

--- classes/class-wc-product.php
+++ classes/class-wc-product.php
@@ -1089,16 +1089,22 @@ class WC_Product {
     /**
      * Gets the main product image
      */ 
-    function get_image( $size = 'shop_thumbnail' ) {
+    function get_image( $size = 'shop_thumbnail', $echo = true ) {
     	global $woocommerce;
     	
-    	if (has_post_thumbnail($this->id)) :
-			echo get_the_post_thumbnail($this->id, $size); 
+    	$output = '';
+		if (has_post_thumbnail($this->id)) :
+			$output = get_the_post_thumbnail($this->id, $size); 
 		elseif (($parent_id = wp_get_post_parent_id( $this->id )) && has_post_thumbnail($parent_id)) :
-			echo get_the_post_thumbnail($parent_id, $size); 
+			$output = get_the_post_thumbnail($parent_id, $size); 
 		else :
-			echo '<img src="'. woocommerce_placeholder_img_src() . '" alt="Placeholder" width="'.$woocommerce->get_image_size('shop_thumbnail_image_width').'" height="'.$woocommerce->get_image_size('shop_thumbnail_image_height').'" />'; 
+			$output = '<img src="'. woocommerce_placeholder_img_src() . '" alt="Placeholder" width="'.$woocommerce->get_image_size('shop_thumbnail_image_width').'" height="'.$woocommerce->get_image_size('shop_thumbnail_image_height').'" />'; 
 		endif;
+		
+		if ( $echo ) {
+			echo $output;
+		}
+		return $output;
     }
     
     /**
