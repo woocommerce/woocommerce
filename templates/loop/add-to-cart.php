174ccfadd2973c52e6c39c63ee68e94cbbdae93e
Manos Psychogyiopoulos <psyx@somewherewarm.net>
2012-07-06 15:21:20 +0300

    add_to_cart_url filter added in loop/add-to-cart.php
    Needed by Product Bundles to get rid of the last template file override.

--- templates/loop/add-to-cart.php
+++ templates/loop/add-to-cart.php
@@ -10,7 +10,7 @@ if( $product->get_price() === '' && $product->product_type != 'external' ) retur
 
 <?php if ( ! $product->is_in_stock() ) : ?>
 		
-	<a href="<?php echo get_permalink($product->id); ?>" class="button"><?php echo apply_filters('out_of_stock_add_to_cart_text', __('Read More', 'woocommerce')); ?></a>
+	<a href="<?php echo get_permalink( $product->id ); ?>" class="button"><?php echo apply_filters( 'out_of_stock_add_to_cart_text', __('Read More', 'woocommerce') ); ?></a>
 
 <?php else : ?>
 	
@@ -18,20 +18,20 @@ if( $product->get_price() === '' && $product->product_type != 'external' ) retur
 	
 		switch ( $product->product_type ) {
 			case "variable" :
-				$link 	= get_permalink($product->id);
-				$label 	= apply_filters('variable_add_to_cart_text', __('Select options', 'woocommerce'));
+				$link 	= get_permalink( $product->id );
+				$label 	= apply_filters( 'variable_add_to_cart_text', __('Select options', 'woocommerce') );
 			break;
 			case "grouped" :
 				$link 	= get_permalink($product->id);
-				$label 	= apply_filters('grouped_add_to_cart_text', __('View options', 'woocommerce'));
+				$label 	= apply_filters( 'grouped_add_to_cart_text', __('View options', 'woocommerce') );
 			break;
 			case "external" :
 				$link 	= get_permalink($product->id);
-				$label 	= apply_filters('external_add_to_cart_text', __('Read More', 'woocommerce'));
+				$label 	= apply_filters( 'external_add_to_cart_text', __('Read More', 'woocommerce') );
 			break;
 			default :
-				$link 	= esc_url( $product->add_to_cart_url() );
-				$label 	= apply_filters('add_to_cart_text', __('Add to cart', 'woocommerce'));
+				$link 	= apply_filters( 'add_to_cart_url', esc_url( $product->add_to_cart_url() ), $product );
+				$label 	= apply_filters( 'add_to_cart_text', __('Add to cart', 'woocommerce'), $product );
 			break;
 		}
 	
