52fc2c113264919557f23c019b31e2ef48a438d0
Max Rice <max@maxrice.com>
2012-07-28 23:11:29 -0400

    added add to cart google analytics event tracking
    two new filters: woocommerce_onclick_javascript - allows GA integration
    (and other plugins / themes) to hook into onClick attribute javascript
    for add to cart buttons / links.
    woocommerce_ga_event_tracking_parameters - allows change of category,
    action, or label parameters included in the _trackEvent js code within
    onClick.

--- templates/single-product/add-to-cart/external.php
+++ templates/single-product/add-to-cart/external.php
@@ -5,6 +5,6 @@
 ?>
 <?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
-<p class="cart"><a href="<?php echo $product_url; ?>" rel="nofollow" class="button alt"><?php echo apply_filters('single_add_to_cart_text', $button_text, 'external'); ?></a></p>
+<p class="cart"><a href="<?php echo $product_url; ?>" rel="nofollow" class="button alt" onClick="<?php echo apply_filters( 'woocommerce_onclick_javascript', '', 'add_to_cart', $product->id ); ?>"><?php echo apply_filters('single_add_to_cart_text', $button_text, 'external'); ?></a></p>
 
 <?php do_action('woocommerce_after_add_to_cart_button'); ?>
