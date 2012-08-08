c29b9c3a11958eee232590e2a13f6075d01ce8ca
Coen Jacobs <coenjacobs@gmail.com>
2012-08-08 11:05:58 +0200

    Added hook woocommerce_variation_options

--- admin/post-types/writepanels/writepanel-product-type-variable.php
+++ admin/post-types/writepanels/writepanel-product-type-variable.php
@@ -241,6 +241,8 @@ function variable_product_type_options() {
 										<label><input type="checkbox" class="checkbox variable_is_downloadable" name="variable_is_downloadable[<?php echo $loop; ?>]" <?php if (isset($variation_data['_downloadable'][0])) checked($variation_data['_downloadable'][0], 'yes'); ?> /> <?php _e('Downloadable', 'woocommerce'); ?> <a class="tips" data-tip="<?php _e('Enable this option if access is given to a downloadable file upon purchase of a product', 'woocommerce'); ?>" href="#">[?]</a></label>
 										
 										<label><input type="checkbox" class="checkbox variable_is_virtual" name="variable_is_virtual[<?php echo $loop; ?>]" <?php if (isset($variation_data['_virtual'][0])) checked($variation_data['_virtual'][0], 'yes'); ?> /> <?php _e('Virtual', 'woocommerce'); ?> <a class="tips" data-tip="<?php _e('Enable this option if a product is not shipped or there is no shipping cost', 'woocommerce'); ?>" href="#">[?]</a></label>
+
+										<?php do_action( 'woocommerce_variation_options', $loop, $variation_data ); ?>
 										
 									</td>
 								</tr>
