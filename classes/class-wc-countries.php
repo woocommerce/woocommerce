8c2cf2fd26207656766dbd4128932d29fffbb76c
Ramon van Belzen <github@ramoonus.nl>
2012-08-26 16:04:40 +0200

    States for Malaysia

--- classes/class-wc-countries.php
+++ classes/class-wc-countries.php
@@ -389,6 +389,23 @@ class WC_Countries {
 				'LD' => __('Lakshadeep', 'woocommerce'),				
 				'PY' => __('Pondicherry (Puducherry)', 'woocommerce')			
 			),
+			'MY' => array (
+				'KDH' => __('Kedah', 'woocommerce') ,    
+				'KTN' => __('Kelantan', 'woocommerce') ,    
+				'MLK' => __('Melaka', 'woocommerce') ,    
+				'NSN' => __('Negeri Sembilan', 'woocommerce') ,    
+				'PHG' => __('Pahang', 'woocommerce') ,    
+				'PRK' => __('Perak', 'woocommerce') ,    
+				'PLS' => __('Perlis', 'woocommerce') ,    
+				'PNG' => __('Pulau Pinang', 'woocommerce') ,    
+				'SBH' => __('Sabah', 'woocommerce') ,    
+				'SWK' => __('Sarawak', 'woocommerce') ,    
+				'SGR' => __('Selangor', 'woocommerce') ,    
+				'TRG' => __('Terengganu', 'woocommerce') ,    
+				'KUL' => __('W.P. Kuala Lumpur', 'woocommerce') ,    
+				'LBN' => __('W.P. Labuan', 'woocommerce') ,    
+				'PJY' => __('W.P. Putrajaya', 'woocommerce')
+			), 
 			'NL' => array(
 				'DR' => __('Drenthe', 'woocommerce') ,
 				'FL' => __('Flevoland', 'woocommerce') ,
