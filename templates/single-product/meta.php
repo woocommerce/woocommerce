2daa93912cbcf193c6318fd1d86c95a36015fe57
Mike Jolley <mike.jolley@me.com>
2012-08-29 14:08:19 +0100

    hooks for meta

--- templates/single-product/meta.php
+++ templates/single-product/meta.php
@@ -11,6 +11,8 @@ global $post, $product;
 ?>
 <div class="product_meta">
 
+	<?php do_action( 'woocommerce_product_meta_start' ); ?>
+
 	<?php if ( $product->is_type( array( 'simple', 'variable' ) ) && get_option('woocommerce_enable_sku') == 'yes' && $product->get_sku() ) : ?>
 		<span itemprop="productID" class="sku"><?php _e('SKU:', 'woocommerce'); ?> <?php echo $product->get_sku(); ?>.</span>
 	<?php endif; ?>
@@ -18,5 +20,7 @@ global $post, $product;
 	<?php echo $product->get_categories( ', ', ' <span class="posted_in">'.__('Category:', 'woocommerce').' ', '.</span>'); ?>
 
 	<?php echo $product->get_tags( ', ', ' <span class="tagged_as">'.__('Tags:', 'woocommerce').' ', '.</span>'); ?>
+	
+	<?php do_action( 'woocommerce_product_meta_end' ); ?>
 
 </div>
