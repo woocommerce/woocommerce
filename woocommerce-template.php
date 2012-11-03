4526a8af9d6bf1b6c77284c4370924dd5e5e520e
grappler <grapplerulrich@gmail.com>
2012-11-03 10:31:14 +0100

    Update woocommerce-template.php
    Improved i8n for the product search.

--- woocommerce-template.php
+++ woocommerce-template.php
@@ -1344,7 +1344,7 @@ if ( ! function_exists( 'get_product_search_form' ) ) {
 			<div>
 				<label class="screen-reader-text" for="s">' . __( 'Search for:', 'woocommerce' ) . '</label>
 				<input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="' . __( 'Search for products', 'woocommerce' ) . '" />
-				<input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search' ) .'" />
+				<input type="submit" id="searchsubmit" value="'. esc_attr__( 'Search', 'woocommerce' ) .'" />
 				<input type="hidden" name="post_type" value="product" />
 			</div>
 		</form>';
