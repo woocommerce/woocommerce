eeff8ce1cf9d7d429fc57c6ad54fe23583be64da
Leho Kraav <leho@kraav.com>
2012-11-24 21:36:16 +0200

    add missing standard classes for .shop_table th
    this probably needs further work to also add respective classes to td's

--- templates/checkout/form-pay.php
+++ templates/checkout/form-pay.php
@@ -16,9 +16,9 @@ global $woocommerce;
 	<table class="shop_table">
 		<thead>
 			<tr>
-				<th><?php _e( 'Product', 'woocommerce' ); ?></th>
-				<th><?php _e( 'Qty', 'woocommerce' ); ?></th>
-				<th><?php _e( 'Totals', 'woocommerce' ); ?></th>
+				<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
+				<th class="product-quantity"><?php _e( 'Qty', 'woocommerce' ); ?></th>
+				<th class="product-total"><?php _e( 'Totals', 'woocommerce' ); ?></th>
 			</tr>
 		</thead>
 		<tfoot>
