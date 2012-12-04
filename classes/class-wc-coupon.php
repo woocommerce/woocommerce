e5df3dce87a26811088d0fb115fefd9cb5b7c863
Mike Jolley <mike.jolley@me.com>
2012-12-04 20:24:37 +0000

    timestamp

--- classes/class-wc-coupon.php
+++ classes/class-wc-coupon.php
@@ -225,7 +225,7 @@ class WC_Coupon {
 
 			// Expired
 			if ( $this->expiry_date ) {
-				if ( strtotime( current_time( 'timestamp' ) ) > $this->expiry_date ) {
+				if ( current_time( 'timestamp' ) > $this->expiry_date ) {
 					$valid = false;
 					$error = __( 'This coupon has expired.', 'woocommerce' );
 				}
