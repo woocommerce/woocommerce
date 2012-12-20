c8852bcf408538ca043b04f0dba3c95e8123b0de
Mike Jolley <mike.jolley@me.com>
2012-12-20 01:13:06 +0000

    Revised, roduct gallery panel, images stored in meta = more flexibility.

--- templates/single-product/product-thumbnails.php
+++ templates/single-product/product-thumbnails.php
@@ -9,28 +9,20 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-global $post, $woocommerce;
+global $post, $product, $woocommerce;
 ?>
 <div class="thumbnails"><?php
-	$attachments = get_posts( array(
-		'post_type' 	=> 'attachment',
-		'numberposts' 	=> -1,
-		'post_status' 	=> null,
-		'post_parent' 	=> $post->ID,
-		'post__not_in'	=> array( get_post_thumbnail_id() ),
-		'post_mime_type'=> 'image',
-		'orderby'		=> 'menu_order',
-		'order'			=> 'ASC'
-	) );
-	if ($attachments) {
+
+	$attachment_ids = $product->get_images();
+
+	if ( $attachment_ids ) {
 
 		$loop = 0;
 		$columns = apply_filters( 'woocommerce_product_thumbnails_columns', 3 );
 
-		foreach ( $attachments as $key => $attachment ) {
+		foreach ( $attachment_ids as $id ) {
 
-			if ( get_post_meta( $attachment->ID, '_woocommerce_exclude_image', true ) == 1 )
-				continue;
+			$attachment = get_post( $id );
 
 			$classes = array( 'zoom' );
 
@@ -43,7 +35,6 @@ global $post, $woocommerce;
 			printf( '<a href="%s" title="%s" rel="thumbnails" class="%s">%s</a>', wp_get_attachment_url( $attachment->ID ), esc_attr( $attachment->post_title ), implode(' ', $classes), wp_get_attachment_image( $attachment->ID, apply_filters( 'single_product_small_thumbnail_size', 'shop_thumbnail' ) ) );
 
 			$loop++;
-
 		}
 
 	}
