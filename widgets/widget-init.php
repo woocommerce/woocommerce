269b038c2e7e64ff675387ab52f07558661f1984
Mike Jolley <mike.jolley@me.com>
2012-12-30 16:35:47 +0000

    Add indexed to tables. #1765. Closes #2080.

--- widgets/widget-init.php
+++ widgets/widget-init.php
@@ -19,39 +19,39 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * @return void
  */
 function woocommerce_register_widgets() {
-	register_widget('WooCommerce_Widget_Recent_Products');
-	register_widget('WooCommerce_Widget_Featured_Products');
-	register_widget('WooCommerce_Widget_Product_Categories');
-	register_widget('WooCommerce_Widget_Product_Tag_Cloud');
-	register_widget('WooCommerce_Widget_Cart');
-	register_widget('WooCommerce_Widget_Layered_Nav');
-	register_widget('WooCommerce_Widget_Layered_Nav_Filters');
-	register_widget('WooCommerce_Widget_Price_Filter');
-	register_widget('WooCommerce_Widget_Product_Search');
-	register_widget('WooCommerce_Widget_Top_Rated_Products');
-	register_widget('WooCommerce_Widget_Recent_Reviews');
-	register_widget('WooCommerce_Widget_Recently_Viewed');
-	register_widget('WooCommerce_Widget_Best_Sellers');
-	register_widget('WooCommerce_Widget_On_Sale');
-	register_widget('WooCommerce_Widget_Login');
-	register_widget('WooCommerce_Widget_Random_Products');
+	register_widget( 'WooCommerce_Widget_Recent_Products' );
+	register_widget( 'WooCommerce_Widget_Featured_Products' );
+	register_widget( 'WooCommerce_Widget_Product_Categories' );
+	register_widget( 'WooCommerce_Widget_Product_Tag_Cloud' );
+	register_widget( 'WooCommerce_Widget_Cart' );
+	register_widget( 'WooCommerce_Widget_Layered_Nav' );
+	register_widget( 'WooCommerce_Widget_Layered_Nav_Filters' );
+	register_widget( 'WooCommerce_Widget_Price_Filter' );
+	register_widget( 'WooCommerce_Widget_Product_Search' );
+	register_widget( 'WooCommerce_Widget_Top_Rated_Products' );
+	register_widget( 'WooCommerce_Widget_Recent_Reviews' );
+	register_widget( 'WooCommerce_Widget_Recently_Viewed' );
+	register_widget( 'WooCommerce_Widget_Best_Sellers' );
+	register_widget( 'WooCommerce_Widget_On_Sale' );
+	register_widget( 'WooCommerce_Widget_Login' );
+	register_widget( 'WooCommerce_Widget_Random_Products' );
 }
 
-add_action('widgets_init', 'woocommerce_register_widgets');
+add_action( 'widgets_init', 'woocommerce_register_widgets' );
 
-include_once('widget-cart.php');
-include_once('widget-featured_products.php');
-include_once('widget-layered_nav.php');
-include_once('widget-layered_nav_filters.php');
-include_once('widget-price_filter.php');
-include_once('widget-product_categories.php');
-include_once('widget-product_search.php');
-include_once('widget-product_tag_cloud.php');
-include_once('widget-recent_products.php');
-include_once('widget-top_rated_products.php');
-include_once('widget-recent_reviews.php');
-include_once('widget-recently_viewed.php');
-include_once('widget-best_sellers.php');
-include_once('widget-onsale.php');
-include_once('widget-login.php');
-include_once('widget-random_products.php');
+include_once( 'widget-cart.php' );
+include_once( 'widget-featured_products.php' );
+include_once( 'widget-layered_nav.php' );
+include_once( 'widget-layered_nav_filters.php' );
+include_once( 'widget-price_filter.php' );
+include_once( 'widget-product_categories.php' );
+include_once( 'widget-product_search.php' );
+include_once( 'widget-product_tag_cloud.php' );
+include_once( 'widget-recent_products.php' );
+include_once( 'widget-top_rated_products.php' );
+include_once( 'widget-recent_reviews.php' );
+include_once( 'widget-recently_viewed.php' );
+include_once( 'widget-best_sellers.php' );
+include_once( 'widget-onsale.php' );
+include_once( 'widget-login.php' );
+include_once( 'widget-random_products.php' );
