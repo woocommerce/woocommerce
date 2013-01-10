cf3369b7e683115e928c434f630ea502b0f12cc1
James Koster <james@jameskoster.co.uk>
2013-01-10 15:07:01 +0000

    Log in language

--- templates/checkout/form-login.php
+++ templates/checkout/form-login.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 if ( is_user_logged_in()  || ! $checkout->enable_signup ) return;
 
-$info_message = apply_filters( 'woocommerce_checkout_login_message', __( 'Already registered?', 'woocommerce' ) );
+$info_message = apply_filters( 'woocommerce_checkout_login_message', __( 'Returning customer?', 'woocommerce' ) );
 ?>
 
 <p class="woocommerce-info"><?php echo esc_html( $info_message ); ?> <a href="#" class="showlogin"><?php _e( 'Click here to login', 'woocommerce' ); ?></a></p>
