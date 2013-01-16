7eb82f1070d9a89b3a1cd2b7059acad1bd335a0d
Mike Jolley <mike.jolley@me.com>
2013-01-16 12:16:31 +0000

    Prompt for variation price since its required.

--- admin/post-types/writepanels/variation-admin-html.php
+++ admin/post-types/writepanels/variation-admin-html.php
@@ -63,11 +63,11 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 						<tr>
 							<td>
-								<label><?php _e( 'Price:', 'woocommerce' ); ?></label>
-								<input type="number" size="5" name="variable_regular_price[<?php echo $loop; ?>]" value="<?php if ( isset( $_regular_price ) ) echo esc_attr( $_regular_price ); ?>" step="any" min="0" />
+								<label><?php echo __( 'Regular Price:', 'woocommerce' ) . ' ('.get_woocommerce_currency_symbol().')'; ?></label>
+								<input type="number" size="5" name="variable_regular_price[<?php echo $loop; ?>]" value="<?php if ( isset( $_regular_price ) ) echo esc_attr( $_regular_price ); ?>" step="any" min="0" placeholder="<?php _e( 'Enter a price for this variation (required)', 'woocommerce' ); ?>" />
 							</td>
 							<td>
-								<label><?php _e( 'Sale Price:', 'woocommerce' ); ?> <a href="#" class="sale_schedule"><?php _e( 'Schedule', 'woocommerce' ); ?></a><a href="#" class="cancel_sale_schedule" style="display:none"><?php _e( 'Cancel schedule', 'woocommerce' ); ?></a></label>
+								<label><?php echo __( 'Sale Price:', 'woocommerce' ) . ' ('.get_woocommerce_currency_symbol().')'; ?> <a href="#" class="sale_schedule"><?php _e( 'Schedule', 'woocommerce' ); ?></a><a href="#" class="cancel_sale_schedule" style="display:none"><?php _e( 'Cancel schedule', 'woocommerce' ); ?></a></label>
 								<input type="number" size="5" name="variable_sale_price[<?php echo $loop; ?>]" value="<?php if ( isset( $_sale_price ) ) echo esc_attr( $_sale_price ); ?>" step="any" min="0" />
 							</td>
 						</tr>
