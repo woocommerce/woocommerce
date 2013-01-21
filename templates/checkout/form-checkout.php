20bfaea36caf1a746b4f525e0f0b29a875486ad3
Ken Bass <kbass@kenbass.com>
2013-01-21 10:12:15 -0500

    Issue 1950

--- templates/checkout/form-checkout.php
+++ templates/checkout/form-checkout.php
@@ -13,7 +13,7 @@ global $woocommerce;
 
 $woocommerce->show_messages();
 
-do_action( 'woocommerce_before_checkout_form' );
+do_action( 'woocommerce_before_checkout_form', $checkout );
 
 // If checkout registration is disabled and not logged in, the user cannot checkout
 if ( ! $checkout->enable_signup && ! $checkout->enable_guest_checkout && ! is_user_logged_in() ) {
@@ -56,4 +56,4 @@ $get_checkout_url = apply_filters( 'woocommerce_get_checkout_url', $woocommerce-
 
 </form>
 
-<?php do_action( 'woocommerce_after_checkout_form' ); ?>
+<?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
