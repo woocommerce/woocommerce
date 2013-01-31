e91751a4a2257a1ac40f61cf98d1c36497089ca4
Mike Jolley <mike.jolley@me.com>
2013-01-31 11:43:56 +0000

    Pedantic :)  Closes #2321.

--- woocommerce-hooks.php
+++ woocommerce-hooks.php
@@ -108,7 +108,7 @@ if ( ! is_admin() || defined('DOING_AJAX') ) {
 	 * After Single Products Summary Div
 	 *
 	 * @see woocommerce_output_product_data_tabs()
-	  * @see woocommerce_upsell_display()
+	 * @see woocommerce_upsell_display()
 	 * @see woocommerce_output_related_products()
 	 */
 	add_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
