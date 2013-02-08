aa5db92a882ac39aa5e9fa9d4871f78d9cc2038e
James Koster <james@jameskoster.co.uk>
2013-02-08 17:45:39 +0000

    filterable breadcrumb home url

--- templates/shop/breadcrumb.php
+++ templates/shop/breadcrumb.php
@@ -21,7 +21,7 @@ if ( ( ! is_home() && ! is_front_page() && ! ( is_post_type_archive() && get_opt
 	echo $wrap_before;
 
 	if ( ! empty( $home ) ) {
-		echo $before . '<a class="home" href="' . home_url() . '">' . $home . '</a>' . $after . $delimiter;
+		echo $before . '<a class="home" href="' . apply_filters( 'woocommerce_breadcrumb_home_url', home_url() ) . '">' . $home . '</a>' . $after . $delimiter;
 	}
 
 	if ( is_category() ) {
