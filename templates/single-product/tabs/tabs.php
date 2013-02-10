b1b1afbaeb1551c2f8b985eaa5ab36cb7520d933
Mike Jolley <mike.jolley@me.com>
2013-02-10 18:49:22 +0000

    Move tabs file

--- templates/single-product/tabs/tabs.php
+++ templates/single-product/tabs/tabs.php
@@ -12,7 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 /**
  * Filter tabs and allow third parties to add their own
  *
- * Each tab is an array containing title, callback and priority. See @woocommerce_default_product_tabs()
+ * Each tab is an array containing title, callback and priority.
+ * @see woocommerce_default_product_tabs()
  */
 $tabs = apply_filters( 'woocommerce_product_tabs', array() );
 
