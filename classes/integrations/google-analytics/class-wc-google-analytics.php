c8bb71c2c759aeef98b03f92a30d90887fba7b80
yitzchak <twburton@gmail.com>
2013-02-13 13:35:10 -0500

    Update classes/integrations/google-analytics/class-wc-google-analytics.php
    Added use of product SKU to "Add to cart" event.

--- classes/integrations/google-analytics/class-wc-google-analytics.php
+++ classes/integrations/google-analytics/class-wc-google-analytics.php
@@ -255,7 +255,7 @@ class WC_Google_Analytics extends WC_Integration {
 		// Add single quotes to allow jQuery to be substituted into _trackEvent parameters
 		$parameters['category'] = "'" . __( 'Products', 'woocommerce' ) . "'";
 		$parameters['action'] = "'" . __( 'Add to cart', 'woocommerce' ) . "'";
-		$parameters['label'] = "'#" . esc_js( $product->id ) . "'";
+		$parameters['label'] = "'" . ( $product->get_sku() ? __('SKU:', 'woocommerce') . ' ' . $product->get_sku() : "#" . $product->id ) . "'";
 
 		$this->event_tracking_code( $parameters, '.single_add_to_cart_button' );
 	}
