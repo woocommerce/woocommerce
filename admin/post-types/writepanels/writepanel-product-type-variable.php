6c384a0d3420b38220ec3ed476b17468dd67443b
Brent Shepherd <thenbrent@gmail.com>
2013-02-21 13:51:54 +1000

    Trigger change event when bulk editing variation

--- admin/post-types/writepanels/writepanel-product-type-variable.php
+++ admin/post-types/writepanels/writepanel-product-type-variable.php
@@ -431,7 +431,7 @@ function variable_product_type_options() {
 				var input_tag = jQuery('select#field_to_edit :selected').attr('rel') ? jQuery('select#field_to_edit :selected').attr('rel') : 'input';
 
 				var value = prompt("<?php _e( 'Enter a value', 'woocommerce' ); ?>");
-				jQuery(input_tag + '[name^="' + field_to_edit + '["]').val( value );
+				jQuery(input_tag + '[name^="' + field_to_edit + '["]').val( value ).change();
 				return false;
 
 			}
