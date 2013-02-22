c086d7236a92b122c8b7b33ca6e548fcdbe5bcf9
Mike Jolley <mike.jolley@me.com>
2013-02-22 08:55:48 +0000

    Initial display Closes #2505.

--- templates/checkout/form-coupon.php
+++ templates/checkout/form-coupon.php
@@ -19,7 +19,7 @@ $info_message = apply_filters('woocommerce_checkout_coupon_message', __( 'Have a
 
 <p class="woocommerce-info"><?php echo $info_message; ?> <a href="#" class="showcoupon"><?php _e( 'Click here to enter your code', 'woocommerce' ); ?></a></p>
 
-<form class="checkout_coupon" method="post">
+<form class="checkout_coupon" method="post" style="display:none">
 
 	<p class="form-row form-row-first">
 		<input type="text" name="coupon_code" class="input-text" placeholder="<?php _e( 'Coupon code', 'woocommerce' ); ?>" id="coupon_code" value="" />
