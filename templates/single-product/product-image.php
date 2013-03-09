983f455a6dd6bb29597c724528d8df2d28303948
Roy Ho <roy@splashingpixels.com>
2013-03-09 13:34:52 -0800

    Added filters on the product image HTML and product thumbnails for 3rd party customizations

--- templates/single-product/product-image.php
+++ templates/single-product/product-image.php
@@ -16,11 +16,14 @@ global $post, $woocommerce;
 
 	<?php if ( has_post_thumbnail() ) : ?>
 
-		<a class="woocommerce-main-image zoom" itemprop="image" href="<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>" rel="prettyPhoto[product-gallery]" title="<?php echo get_the_title( get_post_thumbnail_id() ); ?>"><?php echo get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ) ) ?></a>
+		<?php
+		echo apply_filters( 'woocommerce_product_image_with_link', '<a class="woocommerce-main-image zoom" itemprop="image" href="' . wp_get_attachment_url( get_post_thumbnail_id() ) . '" rel="prettyPhoto[product-gallery]" title="' . get_the_title( get_post_thumbnail_id() ) . '">' . get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ) ) . '</a>', $post->ID );
+		?>
 
 	<?php else : ?>
-
-		<img src="<?php echo woocommerce_placeholder_img_src(); ?>" alt="Placeholder" />
+		<?php
+		echo apply_filters( 'woocommerce_product_image_placeholder', '<img src="' . woocommerce_placeholder_img_src() . '" alt="Placeholder" />', $post->ID );
+		?>
 
 	<?php endif; ?>
 
