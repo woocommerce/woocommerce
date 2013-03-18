55b6a59e44bd5621769b5fd7c0316c24be81c548
Mike Jolley <mike.jolley@me.com>
2013-03-18 14:03:39 +0000

    Check variation exists. Closes #2700.

--- assets/js/frontend/add-to-cart-variation.js
+++ assets/js/frontend/add-to-cart-variation.js
@@ -237,33 +237,37 @@
 				        // Loop through variations
 				        for ( num in variations ) {
 
-				            var attributes = variations[ num ].attributes;
+				        	if ( typeof( variations[ num ] ) != "undefined" ) {
 
-				            for ( attr_name in attributes ) {
+					            var attributes = variations[ num ].attributes;
 
-				                var attr_val = attributes[ attr_name ];
+					            for ( attr_name in attributes ) {
 
-				                if ( attr_name == current_attr_name ) {
+					                var attr_val = attributes[ attr_name ];
 
-				                    if ( attr_val ) {
+					                if ( attr_name == current_attr_name ) {
 
-				                    	// Decode entities
-				                    	attr_val = $("<div/>").html( attr_val ).text();
+					                    if ( attr_val ) {
 
-				                    	// Add slashes
-				                    	attr_val = attr_val.replace(/'/g, "\\'");
-				                    	attr_val = attr_val.replace(/"/g, "\\\"");
+					                    	// Decode entities
+					                    	attr_val = $("<div/>").html( attr_val ).text();
 
-				                    	// Compare the meercat
-				                    	current_attr_select.find('option[value="' + attr_val + '"]').addClass('active');
+					                    	// Add slashes
+					                    	attr_val = attr_val.replace(/'/g, "\\'");
+					                    	attr_val = attr_val.replace(/"/g, "\\\"");
 
-				                    } else {
+					                    	// Compare the meercat
+					                    	current_attr_select.find('option[value="' + attr_val + '"]').addClass('active');
 
-				                    	current_attr_select.find('option:gt(0)').addClass('active');
+					                    } else {
 
-				                    }
+					                    	current_attr_select.find('option:gt(0)').addClass('active');
 
-				                }
+					                    }
+
+					                }
+
+					            }
 
 				            }
 
