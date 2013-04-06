eb6dfe9ad47c2089fac0a79dd52cb01c9da58865
Daniel Espinoza <daniel@growdevelopment.com>
2013-04-05 21:56:36 -0500

    Changes to template files that were causing validation errors.

--- templates/myaccount/my-orders.php
+++ templates/myaccount/my-orders.php
@@ -53,7 +53,7 @@ if ( $customer_orders ) : ?>
 						</a>
 					</td>
 					<td class="order-date">
-						<time title="<?php echo esc_attr( strtotime( $order->order_date ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></time>
+						<time datetime="<?php echo date('Y-m-d', strtotime( $order->order_date ) ); ?>" title="<?php echo esc_attr( strtotime( $order->order_date ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></time>
 					</td>
 					<td class="order-status" style="text-align:left; white-space:nowrap;">
 						<?php echo ucfirst( __( $status->name, 'woocommerce' ) ); ?>
