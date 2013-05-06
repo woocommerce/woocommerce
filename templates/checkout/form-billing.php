f03735d8f65357f9c17479adfe24071cf4c9921a
Coen Jacobs <coenjacobs@gmail.com>
2013-05-06 12:03:31 +0200

    Add 'woocommerce_create_account_default_checked' for #3098

--- templates/checkout/form-billing.php
+++ templates/checkout/form-billing.php
@@ -36,13 +36,15 @@ global $woocommerce;
 
 <?php if ( ! is_user_logged_in() && $checkout->enable_signup ) : ?>
 
-	<?php if ( $checkout->enable_guest_checkout ) : ?>
+	<?php if ( $checkout->enable_guest_checkout ) {
+		$checked_value = ( true === $checkout->get_value('createaccount') || ( true === apply_filters( 'woocommerce_create_account_default_checked', false ) ) );
+		?>
 
 		<p class="form-row form-row-wide">
-			<input class="input-checkbox" id="createaccount" <?php checked($checkout->get_value('createaccount'), true) ?> type="checkbox" name="createaccount" value="1" /> <label for="createaccount" class="checkbox"><?php _e( 'Create an account?', 'woocommerce' ); ?></label>
+			<input class="input-checkbox" id="createaccount" <?php checked( $checked_value, true) ?> type="checkbox" name="createaccount" value="1" /> <label for="createaccount" class="checkbox"><?php _e( 'Create an account?', 'woocommerce' ); ?></label>
 		</p>
 
-	<?php endif; ?>
+	<?php } ?>
 
 	<?php do_action( 'woocommerce_before_checkout_registration_form', $checkout ); ?>
 
