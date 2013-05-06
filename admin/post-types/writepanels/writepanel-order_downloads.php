32d36520188bdeede0ba9b58f434811342deec41
Coen Jacobs <coenjacobs@gmail.com>
2013-05-06 15:04:52 +0200

    esc_js for translatable string in the javascripts
    As on master branch:
    - https://github.com/woothemes/woocommerce/commit/ba923875a20b66292d4b5c2a4f492506cb4a4a99
    - https://github.com/woothemes/woocommerce/commit/3f1bca59ccf006480447d24cec7bbdb4cd7619a1

--- admin/post-types/writepanels/writepanel-order_downloads.php
+++ admin/post-types/writepanels/writepanel-order_downloads.php
@@ -122,7 +122,7 @@ function woocommerce_order_downloads_meta_box() {
 
 				} else {
 
-					alert('<?php _e( 'Could not grant access - the user may already have permission for this file or billing email is not set. Ensure the billing email is set, and the order has been saved.', 'woocommerce' ); ?>');
+					alert('<?php echo esc_js( __( 'Could not grant access - the user may already have permission for this file or billing email is not set. Ensure the billing email is set, and the order has been saved.', 'woocommerce' ) ); ?>');
 
 				}
 
@@ -145,7 +145,7 @@ function woocommerce_order_downloads_meta_box() {
 
 		jQuery('.order_download_permissions').on('click', 'button.revoke_access', function(e){
 			e.preventDefault();
-			var answer = confirm('<?php _e( 'Are you sure you want to revoke access to this download?', 'woocommerce' ); ?>');
+			var answer = confirm('<?php echo esc_js( __( 'Are you sure you want to revoke access to this download?', 'woocommerce' ) ); ?>');
 			if (answer){
 
 				var el = jQuery(this).parent().parent();
