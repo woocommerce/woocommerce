ad028e1832c658f3ae5de8e8be5327913ce1fc0f
Bernard Peh <bpeh@sitecritic.net>
2013-05-21 10:49:40 +1000

    wordpress adds a br if you echo text in a new line. better to put them all within the <a> tag

--- templates/myaccount/my-orders.php
+++ templates/myaccount/my-orders.php
@@ -48,9 +48,7 @@ if ( $customer_orders ) : ?>
 
 				?><tr class="order">
 					<td class="order-number">
-						<a href="<?php echo esc_url( add_query_arg('order', $order->id, get_permalink( woocommerce_get_page_id( 'view_order' ) ) ) ); ?>">
-							<?php echo $order->get_order_number(); ?>
-						</a>
+						<a href="<?php echo esc_url( add_query_arg('order', $order->id, get_permalink( woocommerce_get_page_id( 'view_order' ) ) ) ); ?>"><?php echo $order->get_order_number(); ?></a>
 					</td>
 					<td class="order-date">
 						<time datetime="<?php echo date('Y-m-d', strtotime( $order->order_date ) ); ?>" title="<?php echo esc_attr( strtotime( $order->order_date ) ); ?>"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $order->order_date ) ); ?></time>
