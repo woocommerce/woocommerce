c9f051dee250cb990c31702843b750ab2e7228b1
Mike Jolley <mike.jolley@me.com>
2013-06-28 10:10:13 +0100

    Cart empty classes. Closes #3357.

--- templates/cart/cart-empty.php
+++ templates/cart/cart-empty.php
@@ -11,8 +11,8 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?>
 
-<p><?php _e( 'Your cart is currently empty.', 'woocommerce' ) ?></p>
+<p class="cart-empty"><?php _e( 'Your cart is currently empty.', 'woocommerce' ) ?></p>
 
 <?php do_action('woocommerce_cart_is_empty'); ?>
 
-<p><a class="button" href="<?php echo get_permalink(woocommerce_get_page_id('shop')); ?>"><?php _e( '&larr; Return To Shop', 'woocommerce' ) ?></a></p>
+<p class="return-to-shop"><a class="button" href="<?php echo get_permalink(woocommerce_get_page_id('shop')); ?>"><?php _e( '&larr; Return To Shop', 'woocommerce' ) ?></a></p>
