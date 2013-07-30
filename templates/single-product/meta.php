15f98496f10a5328b1f7937ae3e226057d21d9ca
Mike Jolley <mike.jolley@me.com>
2013-07-30 10:43:55 +0100

    Ensure sku placeholder exists for variable products Closes #1734.

--- templates/single-product/meta.php
+++ templates/single-product/meta.php
@@ -15,19 +15,15 @@ global $post, $product;
 
 	<?php do_action( 'woocommerce_product_meta_start' ); ?>
 
-	<?php if ( $product->is_type( array( 'simple', 'variable' ) ) && get_option( 'woocommerce_enable_sku' ) == 'yes' && $product->get_sku() ) : ?>
-		<span itemprop="productID" class="sku_wrapper"><?php _e( 'SKU:', 'woocommerce' ); ?> <span class="sku"><?php echo $product->get_sku(); ?></span>.</span>
+	<?php if ( get_option( 'woocommerce_enable_sku' ) == 'yes' && ( $product->get_sku() || $product->is_type( 'variable' ) ) ) : ?>
+
+		<span itemprop="productID" class="sku_wrapper"><?php _e( 'SKU:', 'woocommerce' ); ?> <span class="sku"><?php echo $sku = $product->get_sku() ? $sku : __( 'n/a', 'woocommerce' ); ?></span>.</span>
+
 	<?php endif; ?>
 
-	<?php
-		$size = sizeof( get_the_terms( $post->ID, 'product_cat' ) );
-		echo $product->get_categories( ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', $size, 'woocommerce' ) . ' ', '.</span>' );
-	?>
+	<?php echo $product->get_categories( ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', sizeof( get_the_terms( $post->ID, 'product_cat' ) ), 'woocommerce' ) . ' ', '.</span>' ); ?>
 
-	<?php
-		$size = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
-		echo $product->get_tags( ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', $size, 'woocommerce' ) . ' ', '.</span>' );
-	?>
+	<?php echo $product->get_tags( ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', sizeof( get_the_terms( $post->ID, 'product_tag' ) ), 'woocommerce' ) . ' ', '.</span>' ); ?>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
 
