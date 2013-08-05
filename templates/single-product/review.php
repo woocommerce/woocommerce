719acf5104f3329d5bda7774f33b4b3863c7273d
James Koster <james@jameskoster.co.uk>
2013-08-05 13:54:27 +0100

    review gravatar size filterable & added alt text

--- templates/single-product/review.php
+++ templates/single-product/review.php
@@ -17,7 +17,7 @@ $rating = intval( get_comment_meta( $comment->comment_ID, 'rating', true ) );
 
 	<div id="comment-<?php comment_ID(); ?>" class="comment_container">
 
-		<?php echo get_avatar( $comment, '60' ); ?>
+		<?php echo get_avatar( $comment, apply_filters( 'woocommerce_review_gravatar_size', '60' ), '', get_comment_author() ); ?>
 
 		<div class="comment-text">
 
