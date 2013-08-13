7fda2bd832d9f85b142b5b789e695a4c55f79167
Mike Jolley <mike.jolley@me.com>
2013-08-13 12:38:26 +0100

    Search for US/UK on country field. Fixes #3626.

--- assets/js/frontend/chosen-frontend.js
+++ assets/js/frontend/chosen-frontend.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 
 	// Frontend Chosen selects
-	$("select.country_select, select.state_select").chosen();
+	$("select.country_select, select.state_select").chosen( { search_contains: true } );
 
 	$('body').bind('country_to_state_changed', function(){
 		$("select.state_select").chosen().trigger("chosen:updated");
