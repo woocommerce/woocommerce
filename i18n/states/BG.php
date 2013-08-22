c5f7d46109182fd3dec63e7972689d5d4de24ed8
Adrian Dimitrov <dimitrov.adrian@gmail.com>
2013-08-22 12:01:40 +0300

    Adding Bulgarian states.

---
+++ i18n/states/BG.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Bulgarian states
+ *
+ * @author 		WooThemes
+ * @category 	i18n
+ * @package 	WooCommerce/i18n
+ * @version     2.0.0
+ */
+global $states;
+
+$states['BG'] = array(
+	'Е' => __( 'Благоевград', 'woocommerce' ),
+	'А' => __( 'Бургас', 'woocommerce' ),
+	'В' => __( 'Варна', 'woocommerce' ),
+	'ВТ' => __( 'Велико Търново', 'woocommerce' ),
+	'ВН' => __( 'Видин', 'woocommerce' ),
+	'ВР' => __( 'Враца', 'woocommerce' ),
+	'ЕВ' => __( 'Габрово', 'woocommerce' ),
+	'ТХ' => __( 'Добрич', 'woocommerce' ),
+	'К' => __( 'Кърджали', 'woocommerce' ),
+	'КН' => __( 'Кюстендил', 'woocommerce' ),
+	'ОВ' => __( 'Ловеч', 'woocommerce' ),
+	'М' => __( 'Монтана', 'woocommerce' ),
+	'РА' => __( 'Пазарджик', 'woocommerce' ),
+	'РК' => __( 'Перник', 'woocommerce' ),
+	'ЕН' => __( 'Плевен', 'woocommerce' ),
+	'РВ' => __( 'Пловдив', 'woocommerce' ),
+	'PP' => __( 'Разград', 'woocommerce' ),
+	'Р' => __( 'Русе', 'woocommerce' ),
+	'СС' => __( 'Силистра', 'woocommerce' ),
+	'СН' => __( 'Сливен', 'woocommerce' ),
+	'СМ' => __( 'Смолян', 'woocommerce' ),
+	'СО' => __( 'София-област', 'woocommerce' ),
+	'С' => __( 'София-град', 'woocommerce' ),
+	'СТ' => __( 'Стара Загора', 'woocommerce' ),
+	'Т' => __( 'Търговище', 'woocommerce' ),
+	'Х' => __( 'Хасково', 'woocommerce' ),
+	'Н' => __( 'Шумен', 'woocommerce' ),
+	'У' => __( 'Ямбол', 'woocommerce' ),
+);
+
