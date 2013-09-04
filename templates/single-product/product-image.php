1597f28744eb0efb200d53b5231d92a560a7b581
Andrew Benbow <andrew@chromeorange.co.uk>
2013-09-04 17:55:37 +0100

    Update product-image.php
    set alt tag

--- templates/single-product/product-image.php
+++ templates/single-product/product-image.php
@@ -20,7 +20,8 @@ global $post, $woocommerce, $product;
 			$image_title 		= esc_attr( get_the_title( get_post_thumbnail_id() ) );
 			$image_link  		= wp_get_attachment_url( get_post_thumbnail_id() );
 			$image       		= get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
-				'title' => $image_title
+				'title' => $image_title,
+				'alt'	=> $image_title
 				) );
 			$attachment_count   = count( $product->get_gallery_attachment_ids() );
 
