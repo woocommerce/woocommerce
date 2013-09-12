7a3878846052ce4d6e1f5173269909128c24d700
crazyscience <brandon.patton@gmail.com>
2013-09-12 10:34:48 -0700

    Removed 'widget_text' from array of force_ssl filtered objects.
    Forcing SSL for all content in the text widget prevents adding links third party sites that do not support SSL. (ie. Partner sites). Furthermore, it's the assumed responsibility of the individual entering HTML into the text widget to ensure site security when referencing to outside sites.
    Conflicts:
    	includes/class-wc-https.php

--- woocommerce.php
+++ woocommerce.php
@@ -510,7 +510,7 @@ class Woocommerce {
 			add_action( 'wp_footer', array( $this, 'output_inline_js' ), 25 );
 
 			// HTTPS urls with SSL on
-			$filters = array( 'post_thumbnail_html', 'widget_text', 'wp_get_attachment_url', 'wp_get_attachment_image_attributes', 'wp_get_attachment_url', 'option_stylesheet_url', 'option_template_url', 'script_loader_src', 'style_loader_src', 'template_directory_uri', 'stylesheet_directory_uri', 'site_url' );
+			$filters = array( 'post_thumbnail_html', 'wp_get_attachment_url', 'wp_get_attachment_image_attributes', 'wp_get_attachment_url', 'option_stylesheet_url', 'option_template_url', 'script_loader_src', 'style_loader_src', 'template_directory_uri', 'stylesheet_directory_uri', 'site_url' );
 
 			foreach ( $filters as $filter )
 				add_filter( $filter, array( $this, 'force_ssl' ) );
