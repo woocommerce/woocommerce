0bebf9c436e193bff7891c1378dc677d28e31830
Mike Jolley <mike.jolley@me.com>
2013-09-17 11:59:06 +0100

    Version bump/readme

--- templates/single-product/add-to-cart/variable.php
+++ templates/single-product/add-to-cart/variable.php
@@ -4,7 +4,7 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.0.3
+ * @version     2.0.15
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
