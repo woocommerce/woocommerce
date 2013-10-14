d3c79a3097858fa8d2c63c73bb74c521e168654a
Mike Jolley <mike.jolley@me.com>
2013-10-14 14:53:15 +0100

    Fix notices

--- woocommerce-core-functions.php
+++ woocommerce-core-functions.php
@@ -1872,12 +1872,14 @@ function woocommerce_processing_order_count() {
 	if ( false === ( $order_count = get_transient( 'woocommerce_processing_order_count' ) ) ) {
 		$order_statuses = get_terms( 'shop_order_status' );
 	    $order_count = false;
-	    foreach ( $order_statuses as $status ) {
-	        if( $status->slug === 'processing' ) {
-	            $order_count += $status->count;
-	            break;
-	        }
-	    }
+	    if ( $order_statuses ) {
+		    foreach ( $order_statuses as $status ) {
+		        if( $status->slug === 'processing' ) {
+		            $order_count += $status->count;
+		            break;
+		        }
+		    }
+		}
 	    $order_count = apply_filters( 'woocommerce_admin_menu_count', intval( $order_count ) );
 		set_transient( 'woocommerce_processing_order_count', $order_count );
 	}
