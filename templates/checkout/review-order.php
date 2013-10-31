f6b317f199d799bce7d96a01c7cbd8706055f4e6
Mike Jolley <mike.jolley@me.com>
2013-10-31 12:01:11 +0000

    Labels need to be clickable

--- templates/checkout/review-order.php
+++ templates/checkout/review-order.php
@@ -131,8 +131,8 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 					foreach ( $available_gateways as $gateway ) {
 						?>
-						<li id="payment_method_<?php echo $gateway->id; ?>" >
-							<input type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> />
+						<li class="payment_method_<?php echo $gateway->id; ?>">
+							<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> />
 							<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?></label>
 							<?php
 								if ( $gateway->has_fields() || $gateway->get_description() ) :
