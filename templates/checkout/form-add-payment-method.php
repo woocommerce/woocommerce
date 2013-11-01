e80f71d73cd0022608cede6a886111cbe4056025
thenbrent <thenbrent@gmail.com>
2013-11-01 14:12:08 -0700

    Improve form-add-payment-method.php error & button
    Part of #3396

--- templates/checkout/form-add-payment-method.php
+++ templates/checkout/form-add-payment-method.php
@@ -37,7 +37,7 @@ global $woocommerce;
 					}
 				} else {
 
-					echo '<p>'.__( 'Sorry, it seems that there are no available payment methods for your location. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ).'</p>';
+					echo '<p>'.__( 'Sorry, it seems that there are no payment methods which support adding a new payment method. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ).'</p>';
 
 				}
 			?>
@@ -45,7 +45,7 @@ global $woocommerce;
 
 		<div class="form-row">
 			<?php wp_nonce_field( 'woocommerce-add-payment-method' ); ?>
-			<input type="submit" class="button alt" id="place_order" value="<?php _e( 'Pay for order', 'woocommerce' ); ?>" />
+			<input type="submit" class="button alt" id="place_order" value="<?php _e( 'Add Payment Method', 'woocommerce' ); ?>" />
 			<input type="hidden" name="woocommerce_add_payment_method" value="1" />
 		</div>
 
