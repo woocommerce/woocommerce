78832e7e7948bf04d4b5955bba3cf53b5bbe54f3
Mike Jolley <mike.jolley@me.com>
2013-11-08 15:56:38 +0000

    woocommerce_is_filtered filter Closes #4078

--- includes/wc-conditional-functions.php
+++ includes/wc-conditional-functions.php
@@ -185,7 +185,7 @@ if ( ! function_exists( 'is_filtered' ) ) {
 	function is_filtered() {
 		global $_chosen_attributes;
 
-		return ( sizeof( $_chosen_attributes ) > 0 || ( isset( $_GET['max_price'] ) && isset( $_GET['min_price'] ) ) ) ? true : false;
+		return apply_filters( 'woocommerce_is_filtered', ( sizeof( $_chosen_attributes ) > 0 || ( isset( $_GET['max_price'] ) && isset( $_GET['min_price'] ) ) ) );
 	}
 }
 
