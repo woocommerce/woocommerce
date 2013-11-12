6bcee46ffb3c16ef2a4a9a64464357df8d92493d
Mike Jolley <mike.jolley@me.com>
2013-11-12 12:01:05 +0000

    Convert float to string during update post meta #3862

---
+++ includes/class-wc-post-data.php
@@ -0,0 +1,53 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Post Data
+ *
+ * Standardises certain post data on save.
+ *
+ * @class 		WC_Post_Data
+ * @version		2.1.0
+ * @package		WooCommerce/Classes/Data
+ * @category	Class
+ * @author 		WooThemes
+ */
+class WC_Post_Data {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_filter( 'update_post_metadata', array( $this, 'update_post_metadata' ), 10, 5 );
+	}
+
+	/**
+	 * Ensure floats are correctly converted to strings based on PHP locale
+	 * 
+	 * @param  null $check
+	 * @param  int $object_id
+	 * @param  string $meta_key
+	 * @param  mixed $meta_value
+	 * @param  mixed $prev_value
+	 * @return null|bool
+	 */
+	public function update_post_metadata( $check, $object_id, $meta_key, $meta_value, $prev_value ) {
+		if ( ! empty( $meta_value ) && is_float( $meta_value ) && in_array( get_post_type( $object_id ), array( 'shop_order', 'shop_coupon', 'product', 'product_variation' ) ) ) {
+
+			// Convert float to string
+			$meta_value = wc_parse_float( $meta_value );
+
+			// Update meta value with new string
+			update_metadata( 'post', $object_id, $meta_key, $meta_value, $prev_value );
+
+			// Return
+			return true;
+		}
+
+		return $check;
+	}
+
+}
+
+new WC_Post_Data();
