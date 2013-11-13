9c839476137c596d7e32c498245dd2b5af1c3aba
Mike Jolley <mike.jolley@me.com>
2013-11-13 11:25:14 +0000

    Notice on no shop page

--- templates/shop/breadcrumb.php
+++ templates/shop/breadcrumb.php
@@ -17,7 +17,7 @@ $shop_page_id = woocommerce_get_page_id( 'shop' );
 $shop_page    = get_post( $shop_page_id );
 
 // If permalinks contain the shop page in the URI prepend the breadcrumb with shop
-if ( $shop_page_id && strstr( $permalinks['product_base'], '/' . $shop_page->post_name ) && get_option( 'page_on_front' ) !== $shop_page_id ) {
+if ( $shop_page_id && $shop_page && strstr( $permalinks['product_base'], '/' . $shop_page->post_name ) && get_option( 'page_on_front' ) !== $shop_page_id ) {
 	$prepend = $before . '<a href="' . get_permalink( $shop_page ) . '">' . $shop_page->post_title . '</a> ' . $after . $delimiter;
 }
 
