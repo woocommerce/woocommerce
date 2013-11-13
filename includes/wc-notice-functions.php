f3b1679ae54ee2abeaca3d90c271a11a445d9f93
thenbrent <thenbrent@gmail.com>
2013-11-12 16:23:28 -0800

    Introduce new wc_print_notices() function
    To replace wc_print_messages() function.
    
    Part of #4034

--- includes/wc-notice-functions.php
+++ includes/wc-notice-functions.php
@@ -55,23 +55,21 @@ function wc_clear_notices() {
 		WC()->session->set( $notice_type, null );
 	}
 }
+
 /**
  * Prints messages and errors which are stored in the session, then clears them.
  */
-function wc_print_messages() {
-	if ( wc_error_count() > 0  )
-		woocommerce_get_template( 'shop/errors.php', array(
-			'errors' => WC()->session->get( 'wc_errors', array() )
-		) );
+function wc_print_notices() {
 
+	foreach ( array( 'errors', 'messages', 'notices' ) as $notice_type ) {
+		if ( wc_notice_count( $notice_type ) > 0 ) {
+			woocommerce_get_template( "shop/{$notice_type}.php", array(
+				$notice_type => WC()->session->get( "wc_{$notice_type}", array() )
+			) );
+		}
+	}
 
-	if ( wc_message_count() > 0  )
-		woocommerce_get_template( 'shop/messages.php', array(
-			'messages' => WC()->session->get( 'wc_messages', array() )
-		) );
-
-	wc_clear_errors();
-	wc_clear_messages();
+	wc_clear_notices();
 }
-add_action( 'woocommerce_before_shop_loop', 'wc_print_messages', 10 );
-add_action( 'woocommerce_before_single_product', 'wc_print_messages', 10 );
+add_action( 'woocommerce_before_shop_loop', 'wc_print_notices', 10 );
+add_action( 'woocommerce_before_single_product', 'wc_print_notices', 10 );
