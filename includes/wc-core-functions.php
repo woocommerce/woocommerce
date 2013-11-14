479153918ec1b69d3f6b98155c41ba137370a624
Remi Corson <remicorson@gmail.com>
2013-11-14 11:44:58 +0100

    Fixed incorrect Icelandic currency code

--- includes/wc-core-functions.php
+++ includes/wc-core-functions.php
@@ -155,7 +155,7 @@ function get_woocommerce_currencies() {
 				'EUR' => __( 'Euros', 'woocommerce' ),
 				'HKD' => __( 'Hong Kong Dollar', 'woocommerce' ),
 				'HUF' => __( 'Hungarian Forint', 'woocommerce' ),
-				'IKS' => __( 'Icelandic krona', 'woocommerce' ),
+				'ISK' => __( 'Icelandic krona', 'woocommerce' ),
 				'IDR' => __( 'Indonesia Rupiah', 'woocommerce' ),
 				'INR' => __( 'Indian Rupee', 'woocommerce' ),
 				'ILS' => __( 'Israeli Shekel', 'woocommerce' ),
@@ -230,7 +230,7 @@ function get_woocommerce_currency_symbol( $currency = '' ) {
 		case 'HUF' : $currency_symbol = '&#70;&#116;'; break;
 		case 'IDR' : $currency_symbol = 'Rp'; break;
 		case 'INR' : $currency_symbol = 'Rs.'; break;
-		case 'IKS' : $currency_symbol = 'Kr.'; break;
+		case 'ISK' : $currency_symbol = 'Kr.'; break;
 		case 'ILS' : $currency_symbol = '&#8362;'; break;
 		case 'PHP' : $currency_symbol = '&#8369;'; break;
 		case 'PLN' : $currency_symbol = '&#122;&#322;'; break;
