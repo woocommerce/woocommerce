3d01a288ec4e1ecb531f6ffdb9e484a34098b19c
Mike Jolley <mike.jolley@me.com>
2013-11-18 13:00:56 +0000

    hook: 'data-rel', Closes #4115

--- templates/single-product/product-image.php
+++ templates/single-product/product-image.php
@@ -30,7 +30,7 @@ global $post, $woocommerce, $product;
 				$gallery = '';
 			}
 
-			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s"  rel="prettyPhoto' . $gallery . '">%s</a>', $image_link, $image_title, $image ), $post->ID );
+			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s" data-rel="prettyPhoto' . $gallery . '">%s</a>', $image_link, $image_title, $image ), $post->ID );
 
 		} else {
 
