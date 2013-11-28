903af259e06b1571d77d75d82464bea6b796a879
Gerhard <potgieterg@gmail.com>
2013-11-28 14:37:44 +0200

    refactor inline product images js

--- assets/js/admin/meta-boxes.js
+++ assets/js/admin/meta-boxes.js
@@ -1484,4 +1484,104 @@ jQuery( function($){
 		opacity: 0.65,
 	});
 
+	// Product gallery file uploads
+	var product_gallery_frame;
+	var $image_gallery_ids = $('#product_image_gallery');
+	var $product_images = $('#product_images_container ul.product_images');
+
+	jQuery('.add_product_images').on( 'click', 'a', function( event ) {
+		var $el = $(this);
+		var attachment_ids = $image_gallery_ids.val();
+
+		event.preventDefault();
+
+		// If the media frame already exists, reopen it.
+		if ( product_gallery_frame ) {
+			product_gallery_frame.open();
+			return;
+		}
+
+		// Create the media frame.
+		product_gallery_frame = wp.media.frames.downloadable_file = wp.media({
+			// Set the title of the modal.
+			title: $el.data('choose'),
+			button: {
+				text: $el.data('update'),
+			},
+			multiple: true
+		});
+
+		// When an image is selected, run a callback.
+		product_gallery_frame.on( 'select', function() {
+
+			var selection = product_gallery_frame.state().get('selection');
+
+			selection.map( function( attachment ) {
+
+				attachment = attachment.toJSON();
+
+				if ( attachment.id ) {
+					attachment_ids = attachment_ids ? attachment_ids + "," + attachment.id : attachment.id;
+
+					$product_images.append('\
+						<li class="image" data-attachment_id="' + attachment.id + '">\
+							<img src="' + attachment.url + '" />\
+							<ul class="actions">\
+								<li><a href="#" class="delete" title="' + $el.data('delete') + '">' + $el.data('text') + '</a></li>\
+							</ul>\
+						</li>');
+					}
+
+				});
+
+				$image_gallery_ids.val( attachment_ids );
+			});
+
+			// Finally, open the modal.
+			product_gallery_frame.open();
+		});
+
+		// Image ordering
+		$product_images.sortable({
+			items: 'li.image',
+			cursor: 'move',
+			scrollSensitivity:40,
+			forcePlaceholderSize: true,
+			forceHelperSize: false,
+			helper: 'clone',
+			opacity: 0.65,
+			placeholder: 'wc-metabox-sortable-placeholder',
+			start:function(event,ui){
+				ui.item.css('background-color','#f6f6f6');
+			},
+			stop:function(event,ui){
+				ui.item.removeAttr('style');
+			},
+			update: function(event, ui) {
+				var attachment_ids = '';
+
+				$('#product_images_container ul li.image').css('cursor','default').each(function() {
+					var attachment_id = jQuery(this).attr( 'data-attachment_id' );
+					attachment_ids = attachment_ids + attachment_id + ',';
+				});
+
+				$image_gallery_ids.val( attachment_ids );
+			}
+		});
+
+		// Remove images
+		$('#product_images_container').on( 'click', 'a.delete', function() {
+			$(this).closest('li.image').remove();
+
+			var attachment_ids = '';
+
+			$('#product_images_container ul li.image').css('cursor','default').each(function() {
+				var attachment_id = jQuery(this).attr( 'data-attachment_id' );
+				attachment_ids = attachment_ids + attachment_id + ',';
+			});
+
+			$image_gallery_ids.val( attachment_ids );
+
+			return false;
+		});
 });
