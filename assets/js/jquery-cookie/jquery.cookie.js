d196fe401bf652503e0c70105e42011e47ce2ae2
Ramon van Belzen <github@ramoonus.nl>
2013-12-05 19:55:37 +0100

    jQuery Cookie 1.4

--- assets/js/jquery-cookie/jquery.cookie.js
+++ assets/js/jquery-cookie/jquery.cookie.js
@@ -1,12 +1,12 @@
 /*!
- * jQuery Cookie Plugin v1.3.1
+ * jQuery Cookie Plugin v1.4.0
  * https://github.com/carhartl/jquery-cookie
  *
  * Copyright 2013 Klaus Hartl
  * Released under the MIT license
  */
 (function (factory) {
-	if (typeof define === 'function' && define.amd && define.amd.jQuery) {
+	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as anonymous module.
 		define(['jquery'], factory);
 	} else {
@@ -17,26 +17,47 @@
 
 	var pluses = /\+/g;
 
-	function raw(s) {
-		return s;
+	function encode(s) {
+		return config.raw ? s : encodeURIComponent(s);
 	}
 
-	function decoded(s) {
-		return decodeURIComponent(s.replace(pluses, ' '));
+	function decode(s) {
+		return config.raw ? s : decodeURIComponent(s);
 	}
 
-	function converted(s) {
+	function stringifyCookieValue(value) {
+		return encode(config.json ? JSON.stringify(value) : String(value));
+	}
+
+	function parseCookieValue(s) {
 		if (s.indexOf('"') === 0) {
-			// This is a quoted cookie as according to RFC2068, unescape
+			// This is a quoted cookie as according to RFC2068, unescape...
 			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
 		}
-		return config.json ? JSON.parse(s) : s;
+
+		try {
+			// Replace server-side written pluses with spaces.
+			// If we can't decode the cookie, ignore it, it's unusable.
+			s = decodeURIComponent(s.replace(pluses, ' '));
+		} catch(e) {
+			return;
+		}
+
+		try {
+			// If we can't parse the cookie, ignore it, it's unusable.
+			return config.json ? JSON.parse(s) : s;
+		} catch(e) {}
+	}
+
+	function read(s, converter) {
+		var value = config.raw ? s : parseCookieValue(s);
+		return $.isFunction(converter) ? converter(value) : value;
 	}
 
 	var config = $.cookie = function (key, value, options) {
 
-		// write
-		if (value !== undefined) {
+		// Write
+		if (value !== undefined && !$.isFunction(value)) {
 			options = $.extend({}, config.defaults, options);
 
 			if (typeof options.expires === 'number') {
@@ -44,10 +65,8 @@
 				t.setDate(t.getDate() + days);
 			}
 
-			value = config.json ? JSON.stringify(value) : String(value);
-
 			return (document.cookie = [
-				encodeURIComponent(key), '=', config.raw ? value : encodeURIComponent(value),
+				encode(key), '=', stringifyCookieValue(value),
 				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
 				options.path    ? '; path=' + options.path : '',
 				options.domain  ? '; domain=' + options.domain : '',
@@ -55,22 +74,29 @@
 			].join(''));
 		}
 
-		// read
-		var decode = config.raw ? raw : decoded;
-		var cookies = document.cookie.split('; ');
+		// Read
+
 		var result = key ? undefined : {};
+
+		// To prevent the for loop in the first place assign an empty array
+		// in case there are no cookies at all. Also prevents odd result when
+		// calling $.cookie().
+		var cookies = document.cookie ? document.cookie.split('; ') : [];
+
 		for (var i = 0, l = cookies.length; i < l; i++) {
 			var parts = cookies[i].split('=');
 			var name = decode(parts.shift());
-			var cookie = decode(parts.join('='));
+			var cookie = parts.join('=');
 
 			if (key && key === name) {
-				result = converted(cookie);
+				// If second argument (value) is a function it's a converter...
+				result = read(cookie, value);
 				break;
 			}
 
-			if (!key) {
-				result[name] = converted(cookie);
+			// Prevent storing a cookie that we couldn't decode.
+			if (!key && (cookie = read(cookie)) !== undefined) {
+				result[name] = cookie;
 			}
 		}
 
@@ -81,7 +107,8 @@
 
 	$.removeCookie = function (key, options) {
 		if ($.cookie(key) !== undefined) {
-			$.cookie(key, '', $.extend(options, { expires: -1 }));
+			// Must not alter options, thus extending a fresh object...
+			$.cookie(key, '', $.extend({}, options, { expires: -1 }));
 			return true;
 		}
 		return false;
