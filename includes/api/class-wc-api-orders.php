8b6145691c52e2505c9dec5ff9dd77fd4a33538d
Max Rice <max@maxrice.com>
2014-01-13 22:14:08 -0500

    API: Add item price to order line items response

--- includes/api/class-wc-api-orders.php
+++ includes/api/class-wc-api-orders.php
@@ -180,6 +180,7 @@ class WC_API_Orders extends WC_API_Resource {
 				'subtotal'   => wc_format_decimal( $order->get_line_subtotal( $item ), 2 ),
 				'total'      => wc_format_decimal( $order->get_line_total( $item ), 2 ),
 				'total_tax'  => wc_format_decimal( $order->get_line_tax( $item ), 2 ),
+				'price'      => wc_format_decimal( $order->get_item_total( $item ), 2 ),
 				'quantity'   => (int) $item['qty'],
 				'tax_class'  => ( ! empty( $item['tax_class'] ) ) ? $item['tax_class'] : null,
 				'name'       => $item['name'],
