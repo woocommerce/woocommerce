ac1cd5d23d38ce8a8f492a9b46e8075d9eedf3fd
Max Rice <max@maxrice.com>
2014-01-23 16:24:16 -0500

    Fix API keys permissions bug
    When API keys were generated for the first time, the permissions
    weren’t set, which required an additional save before they were set.
    This commit fixes this to set `read` permissions by default when
    initially generating API keys.
    
    Part of #4055

--- includes/admin/class-wc-admin-profile.php
+++ includes/admin/class-wc-admin-profile.php
@@ -268,7 +268,14 @@ class WC_Admin_Profile {
 				// permissions
 				if ( empty( $user->woocommerce_api_key_permissions ) ) {
 
-					$permissions = ( isset( $_POST['woocommerce_api_key_permissions'] ) && ! in_array( $_POST['woocommerce_api_key_permissions'], array( 'read', 'write', 'read_write' ) ) ) ? 'read' : $_POST['woocommerce_api_key_permissions'];
+					if ( isset( $_POST['woocommerce_api_key_permissions'] ) ) {
+
+						$permissions = ( in_array( $_POST['woocommerce_api_key_permissions'], array( 'read', 'write', 'read_write' ) ) ) ? $_POST['woocommerce_api_key_permissions'] : 'read';
+
+					} else {
+
+						$permissions = 'read';
+					}
 
 					update_user_meta( $user_id, 'woocommerce_api_key_permissions', $permissions );
 
