023fa75ca37738c7b788096f1264b2e6c6b67721
Coen Jacobs <coenjacobs@gmail.com>
2014-01-30 17:34:23 +0100

    LESS compiling files through Grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,25 @@
 module.exports = function(grunt){
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        // Compile specified less files
+        less: {
+            compile: {
+                options: {
+                    // These paths are searched for @imports
+                    paths: ["assets/css"]
+                },
+                files: {
+                    "assets/css/woocommerce.css": "assets/css/woocommerce.less"
+                }
+            }
+        },
     });
 
-    grunt.registerTask('default', []);
+    // Load NPM tasks to be used here
+    grunt.loadNpmTasks("grunt-contrib-less");
+
+    // Register tasks
+    grunt.registerTask( 'default', []);
+    grunt.registerTask( 'dev', ["less:compile"]);
 
 };
