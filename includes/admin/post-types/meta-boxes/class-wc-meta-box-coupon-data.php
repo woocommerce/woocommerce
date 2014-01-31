9222e6a854e541c9436591229f2415f7c11fa972
Mike Jolley <mike.jolley@me.com>
2014-01-31 14:48:19 +0000

    woocommerce_coupon_options  Hook breaks custom options - used in 2 places. May have been mistake

--- includes/admin/post-types/meta-boxes/class-wc-meta-box-coupon-data.php
+++ includes/admin/post-types/meta-boxes/class-wc-meta-box-coupon-data.php
@@ -275,7 +275,6 @@ class WC_Meta_Box_Coupon_Data {
 		update_post_meta( $post_id, 'minimum_amount', $minimum_amount );
 		update_post_meta( $post_id, 'customer_email', $customer_email );
 
-		/* Deprecated - same hook name as in the meta */ do_action( 'woocommerce_coupon_options' );
 		do_action( 'woocommerce_coupon_options_save' );
 	}
 }
