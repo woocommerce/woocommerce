a03d4ba1135765b5912d2d11fc3696ef741f1d9f
claudiosmweb <contato@claudiosmweb.com>
2014-02-02 17:20:52 -0200

    fixed wordpress coding standards

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
-module.exports = function(grunt){
+/* jshint node:true */
+module.exports = function( grunt ){
+	'use strict';
 
     grunt.initConfig({
         // Compile specified less files
@@ -6,16 +8,16 @@ module.exports = function(grunt){
             compile: {
                 options: {
                     // These paths are searched for @imports
-                    paths: ["assets/css"]
+                    paths: ['assets/css']
                 },
                 files: {
-                    "assets/css/woocommerce.css": "assets/css/woocommerce.less"
+                    'assets/css/woocommerce.css': 'assets/css/woocommerce.less'
                 }
             }
         },
 
         cssmin: {
-            "assets/css/woocommerce.css": ["assets/css/woocommerce.css"]
+            'assets/css/woocommerce.css': ['assets/css/woocommerce.css']
         },
 
         shell: {
@@ -24,19 +26,19 @@ module.exports = function(grunt){
                     'cd i18n/languages',
                     'for i in *.po; do msgfmt $i -o ${i%%.*}.mo; done',
                     'ls'
-                ].join('&&')
+                ].join( '&&' )
             }
         },
 
     });
 
     // Load NPM tasks to be used here
-    grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks("grunt-contrib-less");
-    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks( 'grunt-shell' );
+    grunt.loadNpmTasks( 'grunt-contrib-less' );
+    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 
     // Register tasks
     grunt.registerTask( 'default', []);
-    grunt.registerTask( 'dev', ["less:compile", "cssmin", "shell:generatemos" ] );
+    grunt.registerTask( 'dev', ['less:compile', 'cssmin', 'shell:generatemos' ] );
 
 };
