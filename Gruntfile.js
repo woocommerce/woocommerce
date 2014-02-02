2db6944e10ffbaf6b1427fd88b9a82d15499d546
claudiosmweb <contato@claudiosmweb.com>
2014-02-02 17:22:05 -0200

    fixed the indentation in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,43 +2,43 @@
 module.exports = function( grunt ){
 	'use strict';
 
-    grunt.initConfig({
-        // Compile specified less files
-        less: {
-            compile: {
-                options: {
-                    // These paths are searched for @imports
-                    paths: ['assets/css']
-                },
-                files: {
-                    'assets/css/woocommerce.css': 'assets/css/woocommerce.less'
-                }
-            }
-        },
+	grunt.initConfig({
+		// Compile specified less files
+		less: {
+			compile: {
+				options: {
+					// These paths are searched for @imports
+					paths: ['assets/css']
+				},
+				files: {
+					'assets/css/woocommerce.css': 'assets/css/woocommerce.less'
+				}
+			}
+		},
 
-        cssmin: {
-            'assets/css/woocommerce.css': ['assets/css/woocommerce.css']
-        },
+		cssmin: {
+			'assets/css/woocommerce.css': ['assets/css/woocommerce.css']
+		},
 
-        shell: {
-            generatemos: {
-                command: [
-                    'cd i18n/languages',
-                    'for i in *.po; do msgfmt $i -o ${i%%.*}.mo; done',
-                    'ls'
-                ].join( '&&' )
-            }
-        },
+		shell: {
+			generatemos: {
+				command: [
+					'cd i18n/languages',
+					'for i in *.po; do msgfmt $i -o ${i%%.*}.mo; done',
+					'ls'
+				].join( '&&' )
+			}
+		},
 
-    });
+	});
 
-    // Load NPM tasks to be used here
-    grunt.loadNpmTasks( 'grunt-shell' );
-    grunt.loadNpmTasks( 'grunt-contrib-less' );
-    grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	// Load NPM tasks to be used here
+	grunt.loadNpmTasks( 'grunt-shell' );
+	grunt.loadNpmTasks( 'grunt-contrib-less' );
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 
-    // Register tasks
-    grunt.registerTask( 'default', []);
-    grunt.registerTask( 'dev', ['less:compile', 'cssmin', 'shell:generatemos' ] );
+	// Register tasks
+	grunt.registerTask( 'default', []);
+	grunt.registerTask( 'dev', ['less:compile', 'cssmin', 'shell:generatemos' ] );
 
 };
