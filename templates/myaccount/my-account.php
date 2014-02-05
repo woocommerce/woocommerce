14e14787fc960ca2ff4f8fd12ed178438f671147
claudiosmweb <contato@claudiosmweb.com>
2014-02-05 00:55:27 -0200

    redirect user in logout to my account page, fixes #4633

--- templates/myaccount/my-account.php
+++ templates/myaccount/my-account.php
@@ -19,7 +19,7 @@ wc_print_notices(); ?>
 		__( 'Hello <strong>%s</strong> (not %s? <a href="%s">Sign out</a>).', 'woocommerce' ) . ' ',
 		$current_user->display_name,
 		$current_user->display_name,
-		wp_logout_url()
+		wp_logout_url( get_permalink( woocommerce_get_page_id( 'myaccount' ) ) )
 	);
 
 	printf( __( 'From your account dashboard you can view your recent orders, manage your shipping and billing addresses and <a href="%s">edit your password and account details</a>.', 'woocommerce' ),
