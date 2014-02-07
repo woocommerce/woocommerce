dc80dfdd3b4c3c10f53dbd75138192391953e8ef
Mike Jolley <mike.jolley@me.com>
2014-02-07 11:43:34 +0000

    Inline note for #4655

--- includes/wc-term-functions.php
+++ includes/wc-term-functions.php
@@ -13,7 +13,11 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 /**
- * Wrapper for wp_get_post_terms which supports ordering by parent
+ * Wrapper for wp_get_post_terms which supports ordering by parent.
+ *
+ * NOTE: At this point in time, ordering by menu_order for example isn't possible with this function. wp_get_post_terms has no
+ *   filters which we can utilise to modify it's query. https://core.trac.wordpress.org/ticket/19094
+ * 
  * @param  int $product_id
  * @param  string $taxonomy
  * @param  array  $args
