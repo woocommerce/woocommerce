21331cbe9cfa9da1c0e999a82e255fc03240d9a8
Mike Jolley <mike.jolley@me.com>
2014-02-07 13:09:45 +0000

    RC2

--- woocommerce.php
+++ woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: http://www.woothemes.com/woocommerce/
  * Description: An e-commerce toolkit that helps you sell anything. Beautifully.
- * Version: 2.1-RC1
+ * Version: 2.1-RC2
  * Author: WooThemes
  * Author URI: http://woothemes.com
  * Requires at least: 3.8
