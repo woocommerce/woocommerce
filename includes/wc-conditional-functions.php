18ca7c4d2fd76abb882371cc9f3bd0a0ca7ac95e
claudiosmweb <contato@claudiosmweb.com>
2014-02-07 15:57:17 -0200

    fixed the indentation in includes/wc-conditional-functions.php

--- includes/wc-conditional-functions.php
+++ includes/wc-conditional-functions.php
@@ -145,32 +145,32 @@ if ( ! function_exists( 'is_account_page' ) ) {
 
 if ( ! function_exists( 'is_order_received_page' ) ) {
 
-    /**
-    * is_order_received_page - Returns true when viewing the order received page.
-    *
-    * @access public
-    * @return bool
-    */
-    function is_order_received_page() {
-        global $wp;
+	/**
+	* is_order_received_page - Returns true when viewing the order received page.
+	*
+	* @access public
+	* @return bool
+	*/
+	function is_order_received_page() {
+		global $wp;
 
-        return ( is_page( wc_get_page_id( 'checkout' ) ) && isset( $wp->query_vars['order-received'] ) ) ? true : false;
-    }
+		return ( is_page( wc_get_page_id( 'checkout' ) ) && isset( $wp->query_vars['order-received'] ) ) ? true : false;
+	}
 }
 
 if ( ! function_exists( 'is_add_payment_method_page' ) ) {
 
-    /**
-    * is_add_payment_method_page - Returns true when viewing the add payment method page.
-    *
-    * @access public
-    * @return bool
-    */
-    function is_add_payment_method_page() {
-        global $wp;
+	/**
+	* is_add_payment_method_page - Returns true when viewing the add payment method page.
+	*
+	* @access public
+	* @return bool
+	*/
+	function is_add_payment_method_page() {
+		global $wp;
 
-        return ( is_page( wc_get_page_id( 'myaccount' ) ) && isset( $wp->query_vars['add-payment-method'] ) ) ? true : false;
-    }
+		return ( is_page( wc_get_page_id( 'myaccount' ) ) && isset( $wp->query_vars['add-payment-method'] ) ) ? true : false;
+	}
 }
 
 if ( ! function_exists( 'is_ajax' ) ) {
