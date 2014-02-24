d8ab9de620364c16501635d38d3a20cad579f817
Patrick Rauland <bftrick@gmail.com>
2014-02-24 16:42:40 -0600

    correcting cart template class names

--- templates/cart/cart-item-data.php
+++ templates/cart/cart-item-data.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 		foreach ( $item_data as $data ) :
 			$key = sanitize_text_field( $data['key'] );
 	?>
-		<dt class="variation-<?php echo $key; ?>"><?php echo wp_kses_post( $data['key'] ); ?>:</dt>
-		<dd class="variation-<?php echo $key; ?>"><?php echo wp_kses_post( wpautop( $data['value'] ) ); ?></dd>
+		<dt class="variation-<?php echo sanitize_html_class( $key ); ?>"><?php echo wp_kses_post( $data['key'] ); ?>:</dt>
+		<dd class="variation-<?php echo sanitize_html_class( $key ); ?>"><?php echo wp_kses_post( wpautop( $data['value'] ) ); ?></dd>
 	<?php endforeach; ?>
 </dl>
