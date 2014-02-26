510e46963a29aa533ad0c8017500989234a65d1b
tamarazuk <tamarazuk@gmail.com>
2014-02-26 15:50:58 -0500

    Add Canadian address format
    .. as per Canada Post's addressing guidelines: http://www.canadapost.ca/tools/pg/manual/PGaddress-e.asp#1413985

--- includes/class-wc-countries.php
+++ includes/class-wc-countries.php
@@ -604,6 +604,7 @@ class WC_Countries {
 				'AU' => "{name}\n{company}\n{address_1}\n{address_2}\n{city} {state} {postcode}\n{country}",
 				'AT' => $postcode_before_city,
 				'BE' => $postcode_before_city,
+				'CA' => "{company}\n{name}\n{address_1}\n{address_2}\n{city} {state} {postcode}\n{country}",
 				'CH' => $postcode_before_city,
 				'CN' => "{country} {postcode}\n{state}, {city}, {address_2}, {address_1}\n{company}\n{name}",
 				'CZ' => $postcode_before_city,
