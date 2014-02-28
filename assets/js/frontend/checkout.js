17cb8e017ad0a94befe31c63321f818fc55734ee
Marc Pfister <marc.pfister@gmail.com>
2014-02-28 08:18:41 -0700

    trim whitespace so parseHTML only contains HTML
    Conflicts:
    	assets/js/frontend/checkout.min.js

--- assets/js/frontend/checkout.js
+++ assets/js/frontend/checkout.js
@@ -72,7 +72,7 @@ jQuery(function($) {
 			data: 		data,
 			success: 	function( response ) {
 				if ( response ) {
-					var order_output = $($.parseHTML(response));
+					var order_output = $($.parseHTML(response.trim()));
 					$('#order_review').html(order_output.html());
 					$('body').trigger('updated_checkout');
 				}
