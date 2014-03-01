76021004c196337b32a451f68458831d7ad83b71
splashingpixels <roy@splashingpixels.com>
2014-03-01 12:10:25 -0800

    external product review link not opening on load fixes #5008

--- assets/js/frontend/single-product.js
+++ assets/js/frontend/single-product.js
@@ -1,74 +1,73 @@
-jQuery(function($) {
+jQuery( function( $ ) {
 
 	// wc_single_product_params is required to continue, ensure the object exists
-	if (typeof wc_single_product_params === "undefined")
+	if ( typeof wc_single_product_params === "undefined" )
 		return false;
 
 	// Tabs
-	$('.woocommerce-tabs .panel').hide();
+	$( '.woocommerce-tabs .panel' ).hide();
 
-	$('.woocommerce-tabs ul.tabs li a').click(function(){
+	$( '.woocommerce-tabs ul.tabs li a' ).click( function() {
 
-		var $tab = $(this);
-		var $tabs_wrapper = $tab.closest('.woocommerce-tabs');
+		var $tab = $( this ),
+			$tabs_wrapper = $tab.closest( '.woocommerce-tabs' );
 
-		$('ul.tabs li', $tabs_wrapper).removeClass('active');
-		$('div.panel', $tabs_wrapper).hide();
-		$('div' + $tab.attr('href'), $tabs_wrapper).show();
-		$tab.parent().addClass('active');
+		$( 'ul.tabs li', $tabs_wrapper ).removeClass( 'active' );
+		$( 'div.panel', $tabs_wrapper ).hide();
+		$( 'div' + $tab.attr( 'href' ), $tabs_wrapper).show();
+		$tab.parent().addClass( 'active' );
 
 		return false;
 	});
 
-	$('.woocommerce-tabs').each(function() {
-		var hash 	= window.location.hash;
-		var url		= window.location.href;
+	$( '.woocommerce-tabs' ).each( function() {
+		var hash	= window.location.hash,
+			url		= window.location.href,
+			tabs	= $( this );
 
-		if (hash.toLowerCase().indexOf("comment-") >= 0) {
-			$('ul.tabs li.reviews_tab a', $(this)).click();
-		} else {
-			$('ul.tabs li:first a', $(this)).click();
-		}
+		if ( hash.toLowerCase().indexOf( "comment-" ) >= 0 ) {
+			$('ul.tabs li.reviews_tab a', tabs ).click();
 
-		if ( url.indexOf("comment-page-") > 0 || url.indexOf("cpage=") > 0 ) {
+		} else if ( url.indexOf( "comment-page-" ) > 0 || url.indexOf( "cpage=" ) > 0 ) {
 			$( 'ul.tabs li.reviews_tab a', $( this ) ).click();
+
 		} else {
-			$( 'ul.tabs li:first a', $( this ) ).click();
+			$( 'ul.tabs li:first a', tabs ).click();
 		}
 	});
 
-	$('a.woocommerce-review-link').click(function() {
-		$('.reviews_tab a').click();
+	$( 'a.woocommerce-review-link' ).click( function() {
+		$( '.reviews_tab a' ).click();
 		return true;
 	});
 
 	// Star ratings for comments
-	$('#rating').hide().before('<p class="stars"><span><a class="star-1" href="#">1</a><a class="star-2" href="#">2</a><a class="star-3" href="#">3</a><a class="star-4" href="#">4</a><a class="star-5" href="#">5</a></span></p>');
+	$( '#rating' ).hide().before( '<p class="stars"><span><a class="star-1" href="#">1</a><a class="star-2" href="#">2</a><a class="star-3" href="#">3</a><a class="star-4" href="#">4</a><a class="star-5" href="#">5</a></span></p>' );
 
-	$('body')
-		.on( 'click', '#respond p.stars a', function(){
-			var $star   = $(this);
-			var $rating = $(this).closest('#respond').find('#rating');
+	$( 'body' )
+		.on( 'click', '#respond p.stars a', function() {
+			var $star   = $( this ),
+				$rating = $( this ).closest( '#respond' ).find( '#rating' );
 
 			$rating.val( $star.text() );
-			$star.siblings('a').removeClass('active');
-			$star.addClass('active');
+			$star.siblings( 'a' ).removeClass( 'active' );
+			$star.addClass( 'active' );
 
 			return false;
 		})
-		.on( 'click', '#respond #submit', function(){
-			var $rating = $(this).closest('#respond').find('#rating');
-			var rating  = $rating.val();
+		.on( 'click', '#respond #submit', function() {
+			var $rating = $( this ).closest( '#respond' ).find( '#rating' ),
+				rating  = $rating.val();
 
-			if ( $rating.size() > 0 && ! rating && wc_single_product_params.review_rating_required == 'yes' ) {
-				alert(wc_single_product_params.i18n_required_rating_text);
+			if ( $rating.size() > 0 && ! rating && wc_single_product_params.review_rating_required === 'yes' ) {
+				alert( wc_single_product_params.i18n_required_rating_text );
+
 				return false;
 			}
 		});
 
 	// prevent double form submission
-	$('form.cart').submit(function(){
-		$(this).find(':submit').attr( 'disabled','disabled' );
+	$( 'form.cart' ).submit( function() {
+		$( this ).find( ':submit' ).attr( 'disabled','disabled' );
 	});
-
 });
