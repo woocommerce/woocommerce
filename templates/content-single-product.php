3b85a5dcc5a675622a9aaec849758cb77299653d
Muhammad Haris <isharis@gmail.com>
2014-03-02 16:31:19 +0500

    Missing hook details for product thumbnails

--- templates/content-single-product.php
+++ templates/content-single-product.php
@@ -34,6 +34,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 		 *
 		 * @hooked woocommerce_show_product_sale_flash - 10
 		 * @hooked woocommerce_show_product_images - 20
+		 * @hooked woocommerce_show_product_thumbnails - 20
 		 */
 		do_action( 'woocommerce_before_single_product_summary' );
 	?>
