ba927d0bba7c428ef757366b3846f1b9890dbdad
Mike Jolley <mike.jolley@me.com>
2014-03-03 14:47:04 +0000

    Revert load order change #4986

--- woocommerce.php
+++ woocommerce.php
@@ -425,16 +425,16 @@ final class WooCommerce {
 	public function load_plugin_textdomain() {
 		$locale = apply_filters( 'plugin_locale', get_locale(), 'woocommerce' );
 
-		// Frontend Locale
-		if ( ! is_admin() || is_ajax() ) {
-			load_plugin_textdomain( 'woocommerce', false, plugin_basename( dirname( __FILE__ ) ) . "/i18n/languages" );
-			load_textdomain( 'woocommerce', WP_LANG_DIR . "/woocommerce/woocommerce-$locale.mo" );
-		}
-
 		// Admin Locale
 		if ( is_admin() ) {
 			load_textdomain( 'woocommerce', WP_LANG_DIR . "/woocommerce/woocommerce-admin-$locale.mo" );
 			load_textdomain( 'woocommerce', dirname( __FILE__ ) . "/i18n/languages/woocommerce-admin-$locale.mo" );
+		}
+		
+		// Frontend Locale
+		if ( ! is_admin() || is_ajax() ) {
+			load_plugin_textdomain( 'woocommerce', false, plugin_basename( dirname( __FILE__ ) ) . "/i18n/languages" );
+			load_textdomain( 'woocommerce', WP_LANG_DIR . "/woocommerce/woocommerce-$locale.mo" );
 		}
 	}
 
