ffd293bb53bd9807a7f427215de089111ba3d0aa
Andrew Benbow <andrew@chromeorange.co.uk>
2014-03-14 11:35:39 +0000

    Update add-to-cart.php
    Add new class to add to cart button if product is in stock

--- templates/loop/add-to-cart.php
+++ templates/loop/add-to-cart.php
@@ -12,11 +12,12 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 global $product;
 
 echo apply_filters( 'woocommerce_loop_add_to_cart_link',
-	sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" class="button %s product_type_%s">%s</a>',
+	sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" class="button%s%s product_type_%s">%s</a>',
 		esc_url( $product->add_to_cart_url() ),
 		esc_attr( $product->id ),
 		esc_attr( $product->get_sku() ),
-		$product->is_purchasable() ? 'add_to_cart_button' : '',
+		$product->is_purchasable() ? ' add_to_cart_button' : '',
+		$product->is_in_stock() ? ' add_to_cart_action' : '',
 		esc_attr( $product->product_type ),
 		esc_html( $product->add_to_cart_text() )
 	),
