abf318db8612f2531173bff42ac35ff629d8a8f0
splashingpixels <roy@splashingpixels.com>
2014-03-19 15:46:59 -0700

    frontend credit-card-form jshinted and coding standards

--- assets/js/frontend/credit-card-form.js
+++ assets/js/frontend/credit-card-form.js
@@ -1,12 +1,12 @@
-jQuery( function() {
-	jQuery('.wc-credit-card-form-card-number').payment('formatCardNumber');
-	jQuery('.wc-credit-card-form-card-expiry').payment('formatCardExpiry');
-	jQuery('.wc-credit-card-form-card-cvc').payment('formatCardCVC');
+jQuery( function( $ ) {
+	$( '.wc-credit-card-form-card-number' ).payment( 'formatCardNumber' );
+	$( '.wc-credit-card-form-card-expiry' ).payment( 'formatCardExpiry' );
+	$( '.wc-credit-card-form-card-cvc' ).payment( 'formatCardCVC' );
 
-	jQuery('body')
-		.on('updated_checkout', function() {
-			jQuery('.wc-credit-card-form-card-number').payment('formatCardNumber');
-			jQuery('.wc-credit-card-form-card-expiry').payment('formatCardExpiry');
-			jQuery('.wc-credit-card-form-card-cvc').payment('formatCardCVC');
+	$( 'body' )
+		.on( 'updated_checkout', function() {
+			$( '.wc-credit-card-form-card-number' ).payment( 'formatCardNumber' );
+			$( '.wc-credit-card-form-card-expiry' ).payment( 'formatCardExpiry' );
+			$( '.wc-credit-card-form-card-cvc' ).payment( 'formatCardCVC' );
 		});
 } );
