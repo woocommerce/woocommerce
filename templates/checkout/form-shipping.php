84899195c54498a8e0bfb0501bb105a3dcfafe52
Andrew Benbow <andrew@chromeorange.co.uk>
2014-03-28 14:41:57 +0000

    Update form-shipping.php

--- templates/checkout/form-shipping.php
+++ templates/checkout/form-shipping.php
@@ -10,7 +10,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 ?>
 <div class="woocommerce-shipping-fields">
-	<?php if ( WC()->cart->needs_shipping_address() == true ) : ?>
+	<?php if ( WC()->cart->needs_shipping_address() === true ) : ?>
 
 		<?php
 			if ( empty( $_POST ) ) {
