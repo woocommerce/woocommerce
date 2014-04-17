17abdb223a051c0464795ebe45d62bcff081393e
Remi Corson <remicorson@gmail.com>
2014-04-17 11:03:47 +0200

    fixed incorrect order received endpoit tooltip

--- includes/admin/settings/class-wc-settings-checkout.php
+++ includes/admin/settings/class-wc-settings-checkout.php
@@ -157,7 +157,7 @@ class WC_Settings_Payment_Gateways extends WC_Settings_Page {
 
 			array(
 				'title' => __( 'Order Received', 'woocommerce' ),
-				'desc' 		=> __( 'Endpoint for the Checkout &rarr; Pay page', 'woocommerce' ),
+				'desc' 		=> __( 'Endpoint for the Checkout &rarr; Order Received page', 'woocommerce' ),
 				'id' 		=> 'woocommerce_checkout_order_received_endpoint',
 				'type' 		=> 'text',
 				'default'	=> 'order-received',
