9feb1a8f0fb06ccae71dea93e6a7a9e9c9fe7e4f
Coen Jacobs <coenjacobs@gmail.com>
2014-05-21 11:26:20 +0200

    Prevent undefined notice for Layered Nav title

--- includes/widgets/class-wc-widget-layered-nav-filters.php
+++ includes/widgets/class-wc-widget-layered-nav-filters.php
@@ -50,7 +50,8 @@ class WC_Widget_Layered_Nav_Filters extends WC_Widget {
 
 		$current_term 	= is_tax() ? get_queried_object()->term_id : '';
 		$current_tax 	= is_tax() ? get_queried_object()->taxonomy : '';
-		$title          = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );
+		$title          = isset( $instance['title'] ) ? $instance['title'] : '';
+		$title          = apply_filters( 'widget_title', $title, $instance, $this->id_base );
 
 		// Price
 		$min_price = isset( $_GET['min_price'] ) ? esc_attr( $_GET['min_price'] ) : 0;
