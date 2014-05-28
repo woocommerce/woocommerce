54aec6ee284cce4d05a26ff6499fccae38340df2
Yakir Gottesman <yakirgot@users.noreply.github.com>
2014-05-28 20:57:03 +0300

    Update form-login.php

--- templates/myaccount/form-login.php
+++ templates/myaccount/form-login.php
@@ -92,11 +92,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<?php endif; ?>
 
 			<!-- Spam Trap -->
-			<?php // Avoids horizontal scroll on rtl
- 			$direction = 'left';
-			if ( is_rtl() ) { $direction = 'right'; }
- 			?>
-			<div style="<?php echo $direction; ?>: -999em; position: absolute;"><label for="trap"><?php _e( 'Anti-spam', 'woocommerce' ); ?></label><input type="text" name="email_2" id="trap" tabindex="-1" /></div>
+			<div style="<?php echo ( ( is_rtl() ) ? 'right' : 'left' ); ?>: -999em; position: absolute;"><label for="trap"><?php _e( 'Anti-spam', 'woocommerce' ); ?></label><input type="text" name="email_2" id="trap" tabindex="-1" /></div>
 
 			<?php do_action( 'woocommerce_register_form' ); ?>
 			<?php do_action( 'register_form' ); ?>
