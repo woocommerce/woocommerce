e6bfd393acea38014637aafc76a5a48aa5c6b1fb
mkayan-wakefly <mkayan-wakefly@users.noreply.github.com>
2014-06-03 15:51:09 -0400

    #5613 Typo in "don't ship to" error message
    Typo in "don't ship to" country message: Issue #5613
    
    Removing the word "to" from the error message indicating that a site doesn't ship to a certain country.
    
    The function, shipping_to_prefix(), will handle insert the word "to".
    Or, in the case of US, GB, and a few other countries, the words "to the".

--- includes/class-wc-checkout.php
+++ includes/class-wc-checkout.php
@@ -586,7 +586,7 @@ class WC_Checkout {
 
 		if ( WC()->cart->needs_shipping() ) {
 
-			if ( ! in_array( WC()->customer->get_shipping_country(), array_keys( WC()->countries->get_shipping_countries() ) ) )
+			if ( ! in_array( WC()->customer->get_shipping_country(), array_keys( WC()->countries->get_shipping_countries() ) ) ) 
 				wc_add_notice( sprintf( __( 'Unfortunately <strong>we do not ship %s</strong>. Please enter an alternative shipping address.', 'woocommerce' ), WC()->countries->shipping_to_prefix() . ' ' . WC()->customer->get_shipping_country() ), 'error' );
 
 			// Validate Shipping Methods
