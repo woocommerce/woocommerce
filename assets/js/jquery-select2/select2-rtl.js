437d79ed4ce09e85a34edf7a217f710c2044c8d0
splashingpixels <roy@splashingpixels.com>
2014-06-07 09:52:47 -0700

    added select2 rtl script

---
+++ assets/js/jquery-select2/select2-rtl.js
@@ -0,0 +1,6 @@
+jQuery( function( $ ) {
+
+	// Fix Chosen for RTL
+	$( '.select2_select, .select2_select_nostd, .product_attributes select.multiselect, select.country_select, select.state_select, select#dropdown_shop_coupon_type, select[name=m]').addClass( 'select2-rtl' );
+
+});
