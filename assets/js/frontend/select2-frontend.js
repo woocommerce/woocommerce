677161d9d4f58b73b5a0baf192f225ced99c08b1
splashingpixels <roy@splashingpixels.com>
2014-06-07 08:26:43 -0700

    added select2 to frontend checkout country and state select

--- assets/js/frontend/select2-frontend.js
+++ assets/js/frontend/select2-frontend.js
@@ -1,7 +1,7 @@
 jQuery( function( $ ) {
 	// run select2
 	function initSelect2() {
-		$( 'select.country_select, select.state_select' ).select2( 'destroy' ).select2({
+		$( 'select.country_select' ).select2( 'destroy' ).select2({
 			width: 'element',
 			dropdownAutoWidth: true
 		});
@@ -9,10 +9,19 @@ jQuery( function( $ ) {
 
 	// init select2
 	initSelect2();
+	
+	// run state/province select2
+	function initStateSelect2() {
+		$( 'select.state_select' ).select2( 'destroy' ).select2({
+			width: 'element',
+			dropdownAutoWidth: true,
+			placeholderOption: 'first'
+		});
+	}
 
 	// reinit select2 on country change
 	$( 'body' ).on( 'country_to_state_changed', function() {
-		initSelect2();
+		initStateSelect2();
 	});
 
 });
