3b922e8a7d457a68d3587146228bad7c07ca427b
Mike Jolley <mike.jolley@me.com>
2014-06-09 12:08:45 +0100

    Fix address format in plain text email
    Conflicts:
    
    	includes/admin/class-wc-admin-post-types.php
    	templates/emails/plain/email-addresses.php

--- templates/emails/plain/email-addresses.php
+++ templates/emails/plain/email-addresses.php
@@ -4,18 +4,17 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates/Emails/Plain
- * @version     2.0.0
+ * @version     2.1.11
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 echo "\n" . __( 'Billing address', 'woocommerce' ) . ":\n";
-echo $order->get_formatted_billing_address() . "\n\n";
+echo preg_replace( '#<br\s*/?>#i', ', ', $order->get_formatted_billing_address() ) . "\n\n";
 
-if ( get_option( 'woocommerce_ship_to_billing_address_only' ) == 'no' && ( $shipping = $order->get_formatted_shipping_address() ) ) :
+if ( get_option( 'woocommerce_ship_to_billing_address_only' ) == 'no' && ( $shipping = $order->get_formatted_shipping_address() ) ) {
 
 	echo __( 'Shipping address', 'woocommerce' ) . ":\n";
 
-	echo $shipping . "\n\n";
-
-endif;
+	echo preg_replace( '#<br\s*/?>#i', ', ', $shipping ) . "\n\n";
+}
