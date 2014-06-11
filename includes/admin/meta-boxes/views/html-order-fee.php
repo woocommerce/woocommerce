f1926834dabd7577cc83ebc21305af93099a1a59
claudiosmweb <contato@claudiosmweb.com>
2014-06-11 16:06:31 -0300

    fixed some coding standards in includes/admin/meta-boxes/views/html-order-fee.php

--- includes/admin/meta-boxes/views/html-order-fee.php
+++ includes/admin/meta-boxes/views/html-order-fee.php
@@ -1,25 +1,25 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
 ?>
-<tr class="fee <?php if ( ! empty( $class ) ) echo $class; ?>" data-order_item_id="<?php echo $item_id; ?>">
+<tr class="fee <?php echo ( ! empty( $class ) ) ? $class : ''; ?>" data-order_item_id="<?php echo $item_id; ?>">
 	<td class="check-column"><input type="checkbox" /></td>
 
 	<td class="thumb"></td>
 
 	<td class="name">
 		<div class="view">
-			<?php
-				echo ! empty( $item['name'] ) ? esc_html( $item['name'] ) : __( 'Fee', 'woocommerce' );
-			?>
+			<?php echo ! empty( $item['name'] ) ? esc_html( $item['name'] ) : __( 'Fee', 'woocommerce' ); ?>
 		</div>
 		<div class="edit" style="display:none">
-			<input type="text" placeholder="<?php _e( 'Fee Name', 'woocommerce' ); ?>" name="order_item_name[<?php echo absint( $item_id ); ?>]" value="<?php if ( isset( $item['name'] ) ) echo esc_attr( $item['name'] ); ?>" />
+			<input type="text" placeholder="<?php _e( 'Fee Name', 'woocommerce' ); ?>" name="order_item_name[<?php echo absint( $item_id ); ?>]" value="<?php echo ( isset( $item['name'] ) ) ? esc_attr( $item['name'] ) : ''; ?>" />
 			<input type="hidden" class="order_item_id" name="order_item_id[]" value="<?php echo esc_attr( $item_id ); ?>" />
 		</div>
 	</td>
 
-	<?php if ( get_option( 'woocommerce_calc_taxes' ) == 'yes' ) :
-		$tax_classes         = array_filter( array_map( 'trim', explode( "\n", get_option('woocommerce_tax_classes' ) ) ) );
+	<?php if ( 'yes' == get_option( 'woocommerce_calc_taxes' ) ) :
+		$tax_classes         = array_filter( array_map( 'trim', explode( "\n", get_option( 'woocommerce_tax_classes' ) ) ) );
 		$classes_options     = array();
 		$classes_options[''] = __( 'Standard', 'woocommerce' );
 		$tax_class           = isset( $item['tax_class'] ) ? sanitize_title( $item['tax_class'] ) : '';
@@ -57,21 +57,21 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 	<td class="line_cost" width="1%">
 		<div class="view">
-			<?php if ( isset( $item['line_total'] ) ) echo wc_price( wc_round_tax_total( $item['line_total'] ) ); ?>
+			<?php echo ( isset( $item['line_total'] ) ) ? wc_price( wc_round_tax_total( $item['line_total'] ) ) : ''; ?>
 		</div>
 		<div class="edit" style="display:none">
-			<label><?php _e( 'Total', 'woocommerce' ); ?>: <input type="text" name="line_total[<?php echo absint( $item_id ); ?>]" placeholder="<?php echo wc_format_localized_price( 0 ); ?>" value="<?php if ( isset( $item['line_total'] ) ) echo esc_attr( wc_format_localized_price( $item['line_total'] ) ); ?>" class="line_total wc_input_price" /></label>
+			<label><?php _e( 'Total', 'woocommerce' ); ?>: <input type="text" name="line_total[<?php echo absint( $item_id ); ?>]" placeholder="<?php echo wc_format_localized_price( 0 ); ?>" value="<?php echo ( isset( $item['line_total'] ) ) ? esc_attr( wc_format_localized_price( $item['line_total'] ) ) : ''; ?>" class="line_total wc_input_price" /></label>
 		</div>
 	</td>
 
-	<?php if ( get_option( 'woocommerce_calc_taxes' ) == 'yes' ) : ?>
+	<?php if ( 'yes' == get_option( 'woocommerce_calc_taxes' ) ) : ?>
 
 	<td class="line_tax" width="1%">
 		<div class="view">
-			<?php if ( isset( $item['line_tax'] ) ) echo wc_price( wc_round_tax_total( $item['line_tax'] ) ); ?>
+			<?php echo ( isset( $item['line_tax'] ) ) ? wc_price( wc_round_tax_total( $item['line_tax'] ) ) : ''; ?>
 		</div>
 		<div class="edit" style="display:none">
-			<input type="text" name="line_tax[<?php echo absint( $item_id ); ?>]" placeholder="<?php echo wc_format_localized_price( 0 ); ?>" value="<?php if ( isset( $item['line_tax'] ) ) echo esc_attr( wc_format_localized_price( $item['line_tax'] ) ); ?>" class="line_tax wc_input_price" />
+			<input type="text" name="line_tax[<?php echo absint( $item_id ); ?>]" placeholder="<?php echo wc_format_localized_price( 0 ); ?>" value="<?php echo ( isset( $item['line_tax'] ) ) ? esc_attr( wc_format_localized_price( $item['line_tax'] ) ) : ''; ?>" class="line_tax wc_input_price" />
 		</div>
 	</td>
 
