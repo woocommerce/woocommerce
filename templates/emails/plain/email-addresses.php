5186ec32664e23102bfb7fcb45e4faf3ee69e37a
claudiosmweb <contato@claudiosmweb.com>
2014-06-17 17:33:55 -0300

    changed the address format in plain text emails
    Conflicts:
    
    	templates/emails/plain/email-addresses.php

--- templates/emails/plain/email-addresses.php
+++ templates/emails/plain/email-addresses.php
@@ -10,11 +10,11 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 echo "\n" . __( 'Billing address', 'woocommerce' ) . ":\n";
-echo preg_replace( '#<br\s*/?>#i', ', ', $order->get_formatted_billing_address() ) . "\n\n";
+echo preg_replace( '#<br\s*/?>#i', "\n", $order->get_formatted_billing_address() ) . "\n\n";
 
 if ( get_option( 'woocommerce_ship_to_billing_address_only' ) == 'no' && ( $shipping = $order->get_formatted_shipping_address() ) ) {
 
 	echo __( 'Shipping address', 'woocommerce' ) . ":\n";
 
-	echo preg_replace( '#<br\s*/?>#i', ', ', $shipping ) . "\n\n";
+	echo preg_replace( '#<br\s*/?>#i', "\n", $shipping ) . "\n\n";
 }
