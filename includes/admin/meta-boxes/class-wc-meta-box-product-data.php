9790c6a0eb5f68c35886377ef194b60dadc18c85
darkrain <spiritkhl@gmail.com>
2014-06-30 21:41:42 +0700

    add filter to stock status options

--- includes/admin/meta-boxes/class-wc-meta-box-product-data.php
+++ includes/admin/meta-boxes/class-wc-meta-box-product-data.php
@@ -297,11 +297,22 @@ class WC_Meta_Box_Product_Data {
 
 				}
 
-				// Stock status
-				woocommerce_wp_select( array( 'id' => '_stock_status', 'label' => __( 'Stock status', 'woocommerce' ), 'options' => array(
+				$stock_status_options = apply_filters( 'woocommerce_stock_status_options', array(
 					'instock' => __( 'In stock', 'woocommerce' ),
 					'outofstock' => __( 'Out of stock', 'woocommerce' )
-				), 'desc_tip' => true, 'description' => __( 'Controls whether or not the product is listed as "in stock" or "out of stock" on the frontend.', 'woocommerce' ) ) );
+				));
+
+
+				// Stock status
+				woocommerce_wp_select( array( 
+					'id' => '_stock_status', 
+					'label' => __( 'Stock status', 'woocommerce' ), 
+					'options' => $stock_status_options,					
+					'desc_tip' => true, 
+					'description' => __( 'Controls whether or not the product is listed as "in stock" or "out of stock" on the frontend.', 'woocommerce' ) 
+
+				));
+
 
 				if (get_option('woocommerce_manage_stock')=='yes') {
 
