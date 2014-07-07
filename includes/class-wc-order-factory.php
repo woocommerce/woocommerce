f6b78be21c7ec97fcc24ae33d2a81641b8366be7
Mike Jolley <mike.jolley@me.com>
2014-07-07 11:44:27 +0100

    Prevent notices in factory

--- includes/class-wc-order-factory.php
+++ includes/class-wc-order-factory.php
@@ -30,27 +30,27 @@ class WC_Order_Factory {
 			$the_order = get_post( $the_order );
 		}
 
-		if ( ! $the_order )
+		if ( ! is_object( $the_order ) ) {
 			return false;
-
-		if ( is_object ( $the_order ) ) {
-			$order_id = absint( $the_order->ID );
-			$post_type  = $the_order->post_type;
 		}
 
-		if ( 'shop_order' == $post_type ) {
-			$classname = 'WC_Order';
+		$order_id  = absint( $the_order->ID );
+		$post_type = $the_order->post_type;
+
+		if ( 'shop_order' === $post_type ) {
+			$classname  = 'WC_Order';
 			$order_type = 'simple';
 		} else {
-			$classname = false;
+			$classname  = false;
 			$order_type = false;
 		}
 
 		// Filter classname so that the class can be overridden if extended.
 		$classname = apply_filters( 'woocommerce_order_class', $classname, $order_type, $post_type, $order_id );
 
-		if ( ! class_exists( $classname ) )
+		if ( ! class_exists( $classname ) ) {
 			$classname = 'WC_Order';
+		}
 
 		return new $classname( $the_order, $args );
 	}
