4de40e25cd6ed38b0edd17da6a2ab301d552fa43
claudiosmweb <contato@claudiosmweb.com>
2014-07-30 17:20:38 -0300

    generate pot files with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /* jshint node:true */
-module.exports = function( grunt ){
+module.exports = function( grunt ) {
 	'use strict';
 
 	grunt.initConfig({
@@ -102,18 +102,90 @@ module.exports = function( grunt ){
 			}
 		},
 
+		pot: {
+			options:{
+				encoding: 'UTF-8',
+				dest: 'i18n/languages/',
+				keywords: [
+					'__:1',
+					'_e:1',
+					'_x:1,2c',
+					'esc_html__:1',
+					'esc_html_e:1',
+					'esc_html_x:1,2c',
+					'esc_attr__:1',
+					'esc_attr_e:1',
+					'esc_attr_x:1,2c',
+					'_ex:1,2c',
+					'_n:1,2',
+					'_nx:1,2,4c',
+					'_n_noop:1,2',
+					'_nx_noop:1,2,3c'
+				],
+				msgid_bugs_address: 'https://github.com/woothemes/woocommerce/issues'
+			},
+			frontend: {
+				options: {
+					package_name: 'WooCommerce_Frontend',
+					text_domain: 'woocommerce'
+				},
+				src: [
+					'**/*.php', // Include all files
+					'!includes/admin/**', // Exclude includes/admin files
+					'!apigen/**', // Exclude apigen/
+					'!node_modules/**' // Exclude node_modules/
+				],
+				expand: true,
+			},
+			admin: {
+				options: {
+					package_name: 'WooCommerce_Admin',
+					text_domain: 'woocommerce-admin'
+				},
+				src: [
+					'includes/admin/**/*.php', // Include includes/admin files only
+					'!apigen/**', // Exclude apigen/
+					'!node_modules/**' // Exclude node_modules/
+				],
+				expand: true,
+			}
+		},
+
+		checktextdomain: {
+			options:{
+				text_domain: 'woocommerce',
+				correct_domain: false, //Will correct missing/variable domains
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				],
+			},
+			files: {
+				src:  [
+					'**/*.php', // Include all files
+					'!apigen/**', // Exclude apigen/
+					'!node_modules/**' // Exclude node_modules/
+				],
+				expand: true,
+			},
+		},
+
 		shell: {
 			options: {
 				stdout: true,
 				stderr: true
-			},
-			generatepot: {
-				command: [
-					'cd i18n/makepot/',
-					'sed -i "" "s/exit( \'Locked\' );/\\/\\/exit( \'Locked\' );/g" index.php',
-					'php index.php generate',
-					'sed -i "" "s/\\/\\/exit( \'Locked\' );/exit( \'Locked\' );/g" index.php',
-				].join( '&&' )
 			},
 			apigen: {
 				command: [
@@ -161,17 +233,14 @@ module.exports = function( grunt ){
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-pot' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
 
 	// Register tasks
 	grunt.registerTask( 'default', [
 		'less',
 		'cssmin',
 		'uglify'
-	]);
-
-	// Just an alias for pot file generation
-	grunt.registerTask( 'pot', [
-		'shell:generatepot'
 	]);
 
 	grunt.registerTask( 'docs', [
