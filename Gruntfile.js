73c46c11b13f73e446947ffc471c43cb43bd8a06
Shiva Poudel <shivapoudel@users.noreply.github.com>
2014-09-05 00:02:01 +0545

    Add support for PoEdit Headers

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,6 +124,7 @@ module.exports = function( grunt ) {
 				type: 'wp-plugin',
 				domainPath: 'i18n/languages',
 				potHeaders: {
+					poedit: true,
 					'report-msgid-bugs-to': 'https://github.com/woothemes/woocommerce/issues',
 					'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
 				}
