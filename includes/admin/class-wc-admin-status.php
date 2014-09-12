b47e649e6a78ff5f35761833dcb12e76bfdf6083
claudiosmweb <contato@claudiosmweb.com>
2014-09-12 19:32:21 -0300

    Improved the translation update messages
    When is multisite shows only the button for download directly
    If not is multisite will show the button for make the download with the
    WordPress update system and the button to download directly
    And added a new button to hide the update message
    
    @mikejolley

--- includes/admin/class-wc-admin-status.php
+++ includes/admin/class-wc-admin-status.php
@@ -132,6 +132,14 @@ class WC_Admin_Status {
 
 					echo '<div class="updated"><p>' . __( 'Tax rates successfully deleted', 'woocommerce' ) . '</p></div>';
 				break;
+				case 'hide_translation_upgrade' :
+					update_option( 'woocommerce_language_pack_version', array( WC_VERSION , get_locale() ) );
+					$notices = get_option( 'woocommerce_admin_notices', array() );
+					$notices = array_diff( $notices, array( 'translation_upgrade' ) );
+					update_option( 'woocommerce_admin_notices', $notices );
+
+					echo '<div class="updated"><p>' . __( 'Translation update message hidden successfully!', 'woocommerce' ) . '</p></div>';
+				break;
 				default :
 					$action = esc_attr( $_GET['action'] );
 					if ( isset( $tools[ $action ]['callback'] ) ) {
@@ -164,6 +172,9 @@ class WC_Admin_Status {
 					break;
 
 				default :
+					// Force WordPress find for new updates and hide the WooCommerce translation update
+					set_site_transient( 'update_plugins', null );
+
 					echo '<div class="updated"><p>' . __( 'Translations installed/updated successfully!', 'woocommerce' ) . '</p></div>';
 					break;
 			}
