284c5a43600d00511d0e32c3027067843c0af608
Shiva Poudel <shivapoudel@users.noreply.github.com>
2014-09-17 02:46:41 +0545

    Update ZA.php

--- i18n/states/ZA.php
+++ i18n/states/ZA.php
@@ -10,13 +10,13 @@
 global $states;
 
 $states['ZA'] = array(
-	'EC'  => __( 'Eastern Cape', 'woocommerce' ) ,
-	'FS'  => __( 'Free State', 'woocommerce' ) ,
-	'GP'  => __( 'Gauteng', 'woocommerce' ) ,
-	'KZN' => __( 'KwaZulu-Natal', 'woocommerce' ) ,
-	'LP'  => __( 'Limpopo', 'woocommerce' ) ,
-	'MP'  => __( 'Mpumalanga', 'woocommerce' ) ,
-	'NC'  => __( 'Northern Cape', 'woocommerce' ) ,
-	'NW'  => __( 'North West', 'woocommerce' ) ,
+	'EC'  => __( 'Eastern Cape', 'woocommerce' ),
+	'FS'  => __( 'Free State', 'woocommerce' ),
+	'GP'  => __( 'Gauteng', 'woocommerce' ),
+	'KZN' => __( 'KwaZulu-Natal', 'woocommerce' ),
+	'LP'  => __( 'Limpopo', 'woocommerce' ),
+	'MP'  => __( 'Mpumalanga', 'woocommerce' ),
+	'NC'  => __( 'Northern Cape', 'woocommerce' ),
+	'NW'  => __( 'North West', 'woocommerce' ),
 	'WC'  => __( 'Western Cape', 'woocommerce' )
 );
