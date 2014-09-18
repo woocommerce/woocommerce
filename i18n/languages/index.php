6bc79b0ead540e4066ec51d204c41d94266371a1
Shiva Poudel <shivapoudel@users.noreply.github.com>
2014-09-19 00:51:47 +0545

    Added translation file notice

---
+++ i18n/languages/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * WARNING! DO NOT PUT CUSTOM TRANSLATIONS HERE!
+ *
+ * WooCommerce updates will delete all custom translation files located here.
+ * You have been warned! :)
+ *
+ * Confidential information!
+ * WordPress Plugin Translations Supports: WP_LANG_DIR . "/plugins/{$textdomain}-{$locale}.mo";
+ */
