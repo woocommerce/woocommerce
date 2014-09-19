47651ed567e8288a4fd5423b635fc75710b28b94
Shiva Poudel <shivapoudel@users.noreply.github.com>
2014-09-19 19:25:28 +0545

    Let's capitalize for fun... :)

--- i18n/languages/index.php
+++ i18n/languages/index.php
@@ -5,7 +5,7 @@
  * WooCommerce updates will delete all custom translation files located here.
  * You have been warned! :)
  *
- * Confidential information!
+ * Confidential Information!
  * WooCommerce Plugin Translations Installed: WP_LANG_DIR . "/plugins/{$textdomain}-{$locale}.mo";
  * WooCommerce Plugin Translations Supported: WP_LANG_DIR . "/woocommerce/{$textdomain}-{$locale}.mo";
  * 
