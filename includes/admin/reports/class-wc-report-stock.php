a318f9dae22ed771be26b7816acbc2065b6aae54
shivapoudel <axisthemes@gmail.com>
2014-09-21 01:43:32 +0545

    Absolute path and Indentation fix for includes/admin/reports/*.php :)

--- includes/admin/reports/class-wc-report-stock.php
+++ includes/admin/reports/class-wc-report-stock.php
@@ -1,9 +1,12 @@
 <?php
-if ( ! defined( 'ABSPATH' ) )
-	exit; // Exit if accessed directly
 
-if ( ! class_exists( 'WP_List_Table' ) )
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( ! class_exists( 'WP_List_Table' ) ) {
 	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
 
 /**
  * WC_Report_Stock
