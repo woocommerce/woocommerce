e60135bbf31c62734ee82394b0f8b15e598b83a1
Mike Jolley <mike.jolley@me.com>
2014-10-09 16:43:21 +0100

    [2.2] Fix refund date.
    Fixes #6499

--- includes/admin/meta-boxes/views/html-order-refund.php
+++ includes/admin/meta-boxes/views/html-order-refund.php
@@ -13,8 +13,9 @@ $who_refunded = new WP_User( $refund->post->post_author );
 
 	<td class="name">
 		<?php
-			echo esc_attr__( 'Refund', 'woocommerce' ) . ' - ' . esc_attr( date_i18n( get_option( 'date_format' ) . ', ' . get_option( 'time_format' ), strtotime( $refund->post_date ) ) );
-			if ( $who_refunded->exists() ){
+			echo esc_attr__( 'Refund', 'woocommerce' ) . ' - ' . esc_attr( date_i18n( get_option( 'date_format' ) . ', ' . get_option( 'time_format' ), strtotime( $refund->post->post_date ) ) );
+
+			if ( $who_refunded->exists() ) {
 				echo ' ' . esc_attr_x( 'by', 'Ex: Refund - $date >by< $username', 'woocommerce' ) . ' ' . '<abbr class="refund_by" title="' . esc_attr__( 'ID: ', 'woocommerce' ) . absint( $who_refunded->ID ) . '">' . esc_attr( $who_refunded->display_name ) . '</abbr>' ;
 			}
 		?>
