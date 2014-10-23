7cc71dea44d378dcb9aeb2a0dced9df5597745ca
Mike Jolley <mike.jolley@me.com>
2014-10-23 11:58:39 +0100

    Allow API edit_product method to update post_name (slug)
    Closes #6601

--- includes/api/class-wc-api-products.php
+++ includes/api/class-wc-api-products.php
@@ -268,9 +268,14 @@ class WC_API_Products extends WC_API_Resource {
 
 		$data = apply_filters( 'woocommerce_api_edit_product_data', $data, $this );
 
-		// Product name.
+		// Product title.
 		if ( isset( $data['title'] ) ) {
 			wp_update_post( array( 'ID' => $id, 'post_title' => wc_clean( $data['title'] ) ) );
+		}
+
+		// Product name (slug).
+		if ( isset( $data['name'] ) ) {
+			wp_update_post( array( 'ID' => $id, 'post_name' => sanitize_title( $data['name'] ) ) );
 		}
 
 		// Product status.
