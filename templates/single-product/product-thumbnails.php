299858764bd13039b1b2d66e25441676672f05d5
James Koster <james@jameskoster.co.uk>
2014-11-17 15:41:21 +0000

    gallery column css. closes #6767

--- templates/single-product/product-thumbnails.php
+++ templates/single-product/product-thumbnails.php
@@ -4,7 +4,7 @@
  *
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     2.0.3
+ * @version     2.3.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -16,11 +16,10 @@ global $post, $product, $woocommerce;
 $attachment_ids = $product->get_gallery_attachment_ids();
 
 if ( $attachment_ids ) {
+	$loop 		= 0;
+	$columns 	= apply_filters( 'woocommerce_product_thumbnails_columns', 3 );
 	?>
-	<div class="thumbnails"><?php
-
-		$loop = 0;
-		$columns = apply_filters( 'woocommerce_product_thumbnails_columns', 3 );
+	<div class="thumbnails <?php echo 'columns-' . $columns; ?>"><?php
 
 		foreach ( $attachment_ids as $attachment_id ) {
 
