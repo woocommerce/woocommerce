551159510cd4247cc0d4440c6df6d10a80b8b012
Tamara Zuk <tamarazuk@gmail.com>
2014-11-29 23:59:58 -0500

    Autosuggest product tags on bulk edit
    This tiny change helps users quickly add tags to products using bulk
    edit.

--- assets/js/admin/quick-edit.js
+++ assets/js/admin/quick-edit.js
@@ -102,6 +102,10 @@ jQuery(function( $ ) {
 		$( 'input.text', '.inline-edit-row' ).val( '' );
 		$( '#woocommerce-fields select' ).prop( 'selectedIndex', 0 );
 		$( '#woocommerce-fields-bulk .inline-edit-group .change-input' ).hide();
+
+		// Autosuggest product tags on bulk edit
+		var tax = 'product_tag';
+		$( 'tr.inline-editor textarea[name="tax_input[' + tax + ']"]' ).suggest( ajaxurl + '?action=ajax-tag-search&tax=' + tax, { delay: 500, minchars: 2, multiple: true, multipleSep: inlineEditL10n.comma } );
 	});
 
 	$( '#wpbody' ).on( 'change', '#woocommerce-fields-bulk .inline-edit-group .change_to', function() {
