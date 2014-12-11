a34769d8943971a4ce55469b59ebec27f8bd8568
Mike Jolley <mike.jolley@me.com>
2014-12-11 11:05:56 +0000

    [2.2] Don't force tax_rate_id to an integer. Allow strings.
    Used in Taxamo extension.

--- includes/admin/meta-boxes/views/html-order-fee.php
+++ includes/admin/meta-boxes/views/html-order-fee.php
@@ -51,9 +51,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			foreach ( $order_taxes as $tax_item ) :
 				$tax_item_id       = $tax_item['rate_id'];
 				$tax_item_total    = isset( $tax_data['total'][ $tax_item_id ] ) ? $tax_data['total'][ $tax_item_id ] : '';
-
 				?>
-
 					<td class="line_tax" width="1%">
 						<div class="view">
 							<?php
@@ -65,10 +63,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 							?>
 						</div>
 						<div class="edit" style="display: none;">
-							<input type="text" name="line_tax[<?php echo absint( $item_id ); ?>][<?php echo absint( $tax_item_id ); ?>]" placeholder="<?php echo wc_format_localized_price( 0 ); ?>" value="<?php echo ( isset( $tax_item_total ) ) ? esc_attr( wc_format_localized_price( $tax_item_total ) ) : ''; ?>" class="line_tax wc_input_price" />
+							<input type="text" name="line_tax[<?php echo absint( $item_id ); ?>][<?php echo esc_attr( $tax_item_id ); ?>]" placeholder="<?php echo wc_format_localized_price( 0 ); ?>" value="<?php echo ( isset( $tax_item_total ) ) ? esc_attr( wc_format_localized_price( $tax_item_total ) ) : ''; ?>" class="line_tax wc_input_price" />
 						</div>
 						<div class="refund" style="display: none;">
-							<input type="text" name="refund_line_tax[<?php echo absint( $item_id ); ?>][<?php echo absint( $tax_item_id ); ?>]" placeholder="<?php echo wc_format_localized_price( 0 ); ?>" class="refund_line_tax wc_input_price" data-tax_id="<?php echo absint( $tax_item_id ); ?>" />
+							<input type="text" name="refund_line_tax[<?php echo absint( $item_id ); ?>][<?php echo esc_attr( $tax_item_id ); ?>]" placeholder="<?php echo wc_format_localized_price( 0 ); ?>" class="refund_line_tax wc_input_price" data-tax_id="<?php echo esc_attr( $tax_item_id ); ?>" />
 						</div>
 					</td>
 
