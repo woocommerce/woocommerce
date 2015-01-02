ea7ef0bea0c5935d1a2fccf9eb167c44c4082586
James Koster <james@jameskoster.co.uk>
2015-01-02 09:26:45 +0000

    email font family.

--- templates/emails/email-styles.php
+++ templates/emails/email-styles.php
@@ -40,7 +40,7 @@ $text_lighter_20 = wc_hex_lighter( $text, 20 );
     border-radius: 3px 3px 0 0 !important;
     color: <?php echo $base_text; ?>;
     border-bottom: 0;
-    font-family: "Helvetica Neue", sans-serif;
+    font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
     font-weight:bold;
     line-height:100%;
     vertical-align:middle;
@@ -65,7 +65,7 @@ $text_lighter_20 = wc_hex_lighter( $text, 20 );
 }
 #body_content_inner {
     color: <?php echo $text_lighter_20;?>;
-    font-family: "Helvetica Neue", sans-serif;
+    font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
     font-size:14px;
     line-height:150%;
     text-align: <?php echo is_rtl() ? 'right' : 'left'; ?>;
@@ -73,7 +73,7 @@ $text_lighter_20 = wc_hex_lighter( $text, 20 );
 h1 {
     color: <?php echo esc_attr( $base ); ?>;
     display: block;
-    font-family: "Helvetica Neue", sans-serif;
+    font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
     font-size: 30px;
     font-weight: 300;
     line-height: 150%;
@@ -86,7 +86,7 @@ h1 {
 h2 {
     color: <?php echo esc_attr( $base ); ?>;
     display: block;
-    font-family: "Helvetica Neue", sans-serif;
+    font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
     font-size: 18px;
     font-weight: bold;
     line-height: 130%;
@@ -96,7 +96,7 @@ h2 {
 h3 {
     color: <?php echo esc_attr( $base ); ?>;
     display: block;
-    font-family: "Helvetica Neue", sans-serif;
+    font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
     font-size: 16px;
     font-weight: bold;
     line-height: 130%;
