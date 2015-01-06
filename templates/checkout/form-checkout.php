8a2805413033e939030710173d2dac7d6d61481b
James Koster <james@jameskoster.co.uk>
2015-01-06 15:01:13 +0000

    order review hooks

--- templates/checkout/form-checkout.php
+++ templates/checkout/form-checkout.php
@@ -46,9 +46,13 @@ $get_checkout_url = apply_filters( 'woocommerce_get_checkout_url', WC()->cart->g
 
 	<?php endif; ?>
 
+	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+
 	<div id="order_review" class="woocommerce-checkout-review-order">
 		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
 	</div>
+
+	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
 
 </form>
 
