2d23b427208b3289696d76436feb3d6813fda54c
Sergey <grey.rsi@gmail.com>
2015-01-13 13:12:14 +0200

    add do_action in wc_price function

--- includes/wc-formatting-functions.php
+++ includes/wc-formatting-functions.php
@@ -314,6 +314,8 @@ function wc_price( $price, $args = array() ) {
 		'ex_tax_label' 	=> '0'
 	), $args ) );
 
+	do_action( 'before_wc_price', $price, $args );
+
 	$return          = '';
 	$num_decimals    = absint( get_option( 'woocommerce_price_num_decimals' ) );
 	$currency        = isset( $args['currency'] ) ? $args['currency'] : '';
