5803ef311d894db22e671d7ba3ee76e850cc7567
Teo Maragakis <teomaragakis@gmail.com>
2015-01-18 23:39:23 +0100

    Create GR.php
    Added greek regions

---
+++ i18n/states/GR.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Greek Regions
+ *
+ * @author      WooThemes
+ * @category    i18n
+ * @package     WooCommerce/i18n
+ * @version     2.0.0
+ */
+global $states;
+
+$states['GR'] = array(
+	'ATT' => __( 'Αττική', 'woocommerce' ),
+	'AMT' => __( 'Ανατολική Μακεδονία και Θράκη', 'woocommerce' ),
+	'CM' => __( 'Κεντρική Μακεδονία', 'woocommerce' ),
+	'WM'  => __( 'Δυτική Μακεδονία', 'woocommerce' ),
+	'EP' => __( 'Ήπειρος', 'woocommerce' ),
+	'THE'  => __( 'Θεσσαλία', 'woocommerce' ),
+	'ION' => __( 'Ιόνιοι Νήσοι', 'woocommerce' ),
+	'DE' => __( 'Δυτική Ελλάδα', 'woocommerce' ),
+	'SE'  => __( 'Στερεά Ελλάδα', 'woocommerce' ),
+	'PEL'  => __( 'Πελοπόννησος', 'woocommerce' ),
+	'NEG' => __( 'Βόρειο Αιγαίο', 'woocommerce' ),
+	'SEG' => __( 'Νότιο Αιγαίο', 'woocommerce' ),
+	'CRE'  => __( 'Κρήτη', 'woocommerce' )
+);
