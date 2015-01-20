2693e110c61eff24cde78ca1485706b53aee4a5b
Mike Jolley <mike.jolley@me.com>
2015-01-20 11:35:59 +0000

    Correct @var syntax.

--- includes/admin/meta-boxes/views/html-order-fee.php
+++ includes/admin/meta-boxes/views/html-order-fee.php
@@ -3,7 +3,7 @@
  * Shows an order item fee
  *
  * @var int $item_id The id of the item being displayed
- * @property $item The item being displayed
+ * @var object $item The item being displayed
  */
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
