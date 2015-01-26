4774646a0e62243b321e083077938743cfafe107
Claudio Sanches <contato@claudiosmweb.com>
2015-01-26 14:24:59 -0200

    [API] Removed cart_discount, we have now only total_discount

--- includes/api/class-wc-api-orders.php
+++ includes/api/class-wc-api-orders.php
@@ -160,7 +160,6 @@ class WC_API_Orders extends WC_API_Resource {
 			'cart_tax'                  => wc_format_decimal( $order->get_cart_tax(), 2 ),
 			'shipping_tax'              => wc_format_decimal( $order->get_shipping_tax(), 2 ),
 			'total_discount'            => wc_format_decimal( $order->get_total_discount(), 2 ),
-			'cart_discount'             => wc_format_decimal( $order->get_total_discount(), 2 ),
 			'shipping_methods'          => $order->get_shipping_method(),
 			'payment_details' => array(
 				'method_id'    => $order->payment_method,
