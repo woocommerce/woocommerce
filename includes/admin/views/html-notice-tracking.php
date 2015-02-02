28385b8868c6efae807d1859db3ae3090ae72250
Gerhard <potgieterg@gmail.com>
2015-02-02 14:40:52 +0200

    Update usage tracking notice link

--- includes/admin/views/html-notice-tracking.php
+++ includes/admin/views/html-notice-tracking.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <div id="message" class="updated woocommerce-message wc-connect">
-	<p><?php printf( __( 'Want to help make WooCommerce even more awesome? Allow WooThemes to collect non-sensitive diagnostic data and usage information, and get 20%% discount on your next WooThemes purchase. %sFind out more%s.', 'woocommerce' ), '<a href="http://docs.woothemes.com/document/woocommerce-usage-tracking/" target="_blank">', '</a>' ); ?></p>
+	<p><?php printf( __( 'Want to help make WooCommerce even more awesome? Allow WooThemes to collect non-sensitive diagnostic data and usage information, and get 20%% discount on your next WooThemes purchase. %sFind out more%s.', 'woocommerce' ), '<a href="http://www.woothemes.com/woocommerce/usage-tracking/" target="_blank">', '</a>' ); ?></p>
 	<p class="submit">
 		<a href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'wc_tracker_optin', 'true' ), 'wc_tracker_optin', 'wc_tracker_nonce' ) ); ?>" class="button-primary"><?php _e( 'Allow', 'woocommerce' ); ?></a>
 		<a class="skip button" href="<?php echo esc_url( add_query_arg( 'wc-hide-notice', 'tracking' ) ); ?>"><?php _e( 'No, do not bother me again', 'woocommerce' ); ?></a>
