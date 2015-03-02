560a6bec8741a3b4b6994dbdd36a4e476994a9d7
Mike Jolley <mike.jolley@me.com>
2015-03-02 10:34:20 +0000

    Correct review itemprop Closes #7587

--- templates/single-product/review.php
+++ templates/single-product/review.php
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 $rating = intval( get_comment_meta( $comment->comment_ID, 'rating', true ) );
 
 ?>
-<li itemprop="reviews" itemscope itemtype="http://schema.org/Review" <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
+<li itemprop="review" itemscope itemtype="http://schema.org/Review" <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
 
 	<div id="comment-<?php comment_ID(); ?>" class="comment_container">
 
