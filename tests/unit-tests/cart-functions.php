6ac167ac36ff303ac373f14a0939758fbee264a8
Nicola Mustone <mustone.nicola@gmail.com>
2015-03-02 17:06:03 +0100

    wc-cart-functions - unit tests begin

---
+++ tests/unit-tests/cart-functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Test WC cart functions
+ *
+ * @since 2.3.0
+ */
+class WC_Tests_Cart_Functions extends WC_Unit_Test_Case {
+
+	/**
+	 * Test wc_empty_cart()
+	 *
+	 * @since 2.3.0
+	 */
+	public function test_wc_empty_cart() {
+		// Create dummy product
+		$product = WC_Helper_Product::create_simple_product();
+
+		// Add the product to the cart
+		WC()->cart->add_to_cart( $product->id, 1 );
+
+		// Empty the cart
+		wc_empty_cart();
+
+		// Check if the cart is empty
+		$this->assertEquals( 0, WC()->cart->get_cart_contents_count() );
+	}
+}
