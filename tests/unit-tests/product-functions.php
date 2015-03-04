3615e75de1a76cfd7998892c23f9d1a87b213243
Nicola Mustone <mustone.nicola@gmail.com>
2015-03-04 09:33:05 +0100

    helper methods to get and delete a dummy product

--- tests/unit-tests/product-functions.php
+++ tests/unit-tests/product-functions.php
@@ -5,6 +5,33 @@
  * @since 2.3.0
  */
 class WC_Tests_Product_Functions extends WC_Unit_Test_Case {
+	/**
+	 * @var object
+	 * @access private
+	 */
+	private $_product = null;
+
+	/**
+	 * Helper method to get a product
+	 *
+	 * @since 2.3.0
+	 * @access private
+	 */
+	private function _get_product() {
+		$this->_product = WC_Helper_Product::create_simple_product();
+	}
+
+	/**
+	 * Helper method to delete a product
+	 *
+	 * @since 2.3.0
+	 * @access private
+	 */
+	private function _delete_product() {
+		// Delete the previously created product
+		WC_Helper_Product::delete_product( $this->_product->id );
+		$this->_product = null;
+	}
 
 	/**
 	 * Test wc_get_product()
@@ -12,10 +39,12 @@ class WC_Tests_Product_Functions extends WC_Unit_Test_Case {
 	 * @since 2.3.0
 	 */
 	public function test_wc_get_product() {
-		$product      = WC_Helper_Product::create_simple_product();
-		$product_copy = wc_get_product( $product->id );
+		$this->_get_product();
+		$product_copy = wc_get_product( $this->_product->id );
 
-		$this->assertEquals( $product->id, $product_copy->id );
+		$this->assertEquals( $this->_product->id, $product_copy->id );
+
+		$this->_delete_product();
 	}
 
 	/**
@@ -24,13 +53,13 @@ class WC_Tests_Product_Functions extends WC_Unit_Test_Case {
 	 * @since 2.3.0
 	 */
 	public function test_wc_update_product_stock() {
-		$product = WC_Helper_Product::create_simple_product();
+		$this->_get_product();
 
-		update_post_meta( $product->id, '_manage_stock', 'yes' );
+		update_post_meta( $this->_product->id, '_manage_stock', 'yes' );
 
-		wc_update_product_stock( $product->id, 5 );
-		$this->assertEquals( 5, $product->stock );
+		wc_update_product_stock( $this->_product->id, 5 );
+		$this->assertEquals( 5, $this->_product->stock );
 
-		update_post_meta( $product->id, '_manage_stock', 'no' );
+		$this->_delete_product();
 	}
 }
