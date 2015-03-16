d262c4453434a2f62c8af0d2e524f44c263afb67
Jack Gregory <jackjlgregory@gmail.com>
2015-03-16 13:26:22 +0000

    Include endpoints in body class filter

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -168,12 +168,6 @@ function wc_body_class( $classes ) {
 	if ( is_woocommerce() ) {
 		$classes[] = 'woocommerce';
 		$classes[] = 'woocommerce-page';
-		
-		foreach ( WC()->query->query_vars as $key => $value ) {
- 			if ( is_wc_endpoint_url( $key ) ) {
-				$classes[] = "woocommerce-{$key}";
-			}
-		}
 	}
 
 	elseif ( is_checkout() ) {
@@ -193,6 +187,12 @@ function wc_body_class( $classes ) {
 
 	if ( is_store_notice_showing() ) {
 		$classes[] = 'woocommerce-demo-store';
+	}
+	
+	foreach ( WC()->query->query_vars as $key => $value ) {
+		if ( is_wc_endpoint_url( $key ) ) {
+			$classes[] = "woocommerce-{$key}";
+		}
 	}
 
 	return array_unique( $classes );
