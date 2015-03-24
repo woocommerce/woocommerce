b35e7f7d4acaf5eb3b4d2f4ef5849d41cef3b766
Mike Jolley <mike.jolley@me.com>
2015-03-24 12:56:13 +0000

    Cast product IDs to int when adding to cart
    Closes #7245

--- includes/class-wc-cart.php
+++ includes/class-wc-cart.php
@@ -844,16 +844,19 @@ class WC_Cart {
 		/**
 		 * Add a product to the cart.
 		 *
-		 * @param string $product_id contains the id of the product to add to the cart
+		 * @param integer $product_id contains the id of the product to add to the cart
 		 * @param integer $quantity contains the quantity of the item to add
-		 * @param int $variation_id
+		 * @param integer $variation_id
 		 * @param array $variation attribute values
 		 * @param array $cart_item_data extra cart item data we want to pass into the item
 		 * @return string $cart_item_key
 		 */
-		public function add_to_cart( $product_id, $quantity = 1, $variation_id = '', $variation = array(), $cart_item_data = array() ) {
+		public function add_to_cart( $product_id = 0, $quantity = 1, $variation_id = 0, $variation = array(), $cart_item_data = array() ) {
 			// Wrap in try catch so plugins can throw an exception to prevent adding to cart
 			try {
+				$product_id   = absint( $product_id );
+				$variation_id = absint( $variation_id );
+
 				// Ensure we don't add a variation to the cart directly by variation ID
 				if ( 'product_variation' == get_post_type( $product_id ) ) {
 					$variation_id = $product_id;
