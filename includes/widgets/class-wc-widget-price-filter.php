580e89fb021659591965bb7cb17f0b7cbe174a2b
Gerhard <potgieterg@gmail.com>
2015-04-10 15:34:09 +0200

    Ensure $wp->request has a trailing slash to avoid pagination issues.

--- includes/widgets/class-wc-widget-price-filter.php
+++ includes/widgets/class-wc-widget-price-filter.php
@@ -158,7 +158,7 @@ class WC_Widget_Price_Filter extends WC_Widget {
 		if ( '' == get_option( 'permalink_structure' ) ) {
 			$form_action = remove_query_arg( array( 'page', 'paged' ), add_query_arg( $wp->query_string, '', home_url( $wp->request ) ) );
 		} else {
-			$form_action = preg_replace( '%\/page/[0-9]+%', '', home_url( $wp->request ) );
+			$form_action = preg_replace( '%\/page/[0-9]+%', '', home_url( trailingslashit( $wp->request ) ) );
 		}
 
 		echo '<form method="get" action="' . esc_url( $form_action ) . '">
