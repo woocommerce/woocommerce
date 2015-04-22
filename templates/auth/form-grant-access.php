0bb3f1066cac2461c21f92e22aeeaa091f6ca64d
Claudio Sanches <contato@claudiosmweb.com>
2015-04-22 17:48:18 -0300

    Show authentication data in templates/auth/form-grant-access.php

--- templates/auth/form-grant-access.php
+++ templates/auth/form-grant-access.php
@@ -17,19 +17,17 @@ $user = wp_get_current_user();
 
 <?php do_action( 'woocommerce_auth_page_header' ); ?>
 
-	<h1><?php echo esc_html( get_bloginfo( 'name' ) ); ?></h1>
+	<h1><?php printf( __( 'Grant "%s" permissions to "%s".', 'woocommerce' ), esc_html( $permission_type ), wc_clean( $app_name ) ); ?></h1>
 
 	<?php wc_print_notices(); ?>
 
-	<form method="post" class="grant-access">
-		<?php echo get_avatar( $user->ID, 70 ); ?>
+	<?php echo get_avatar( $user->ID, 70 ); ?>
 
-		<p><?php printf( __( 'Logged in as %s.', 'woocommerce' ), wc_clean( $user->display_name ) ); ?></p>
+	<p><?php printf( __( 'Logged in as %s.', 'woocommerce' ), wc_clean( $user->display_name ) ); ?></p>
 
-		<input type="hidden" name="permission_type" value="<?php echo absint( $permission_type ); ?>">
+	<a href="<?php echo esc_url( $granted_url ); ?>" class="button button-primary"><?php _e( 'Authorize', 'woocommerce' ); ?></a>
 
-		<p><?php printf( __( 'Not %s? %s', 'woocommerce' ), wc_clean( $user->display_name ), '<a href="' . esc_url( $logout_url ) . '">' . __( 'Logout', 'woocommerce' ) . '</a>' ); ?></p>
-	</form>
+	<p><?php printf( __( 'Not %s? %s', 'woocommerce' ), wc_clean( $user->display_name ), '<a href="' . esc_url( $logout_url ) . '">' . __( 'Logout', 'woocommerce' ) . '</a>' ); ?></p>
 
 	<a href="<?php echo esc_url( urldecode( $return_url ) ); ?>"><?php printf( __( 'Return To %s', 'woocommerce' ), wc_clean( $app_name ) ); ?></a>
 
