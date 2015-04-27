1b176c73e3951c5cf1c96ec110a124858aec2ca0
Claudio Sanches <contato@claudiosmweb.com>
2015-04-27 13:06:28 -0300

    Created wc_check_if_attribute_name_is_reserved() function

--- includes/wc-attribute-functions.php
+++ includes/wc-attribute-functions.php
@@ -115,3 +115,27 @@ function wc_get_attribute_types() {
 		'text'   => __( 'Text', 'woocommerce' )
 	) );
 }
+
+/**
+ * Check if attribute name is reserved
+ * http://codex.wordpress.org/Function_Reference/register_taxonomy#Reserved_Terms
+ *
+ * @since  2.4.0
+ * @param  string $attribute_name
+ * @return bool
+ */
+function wc_check_if_attribute_name_is_reserved( $attribute_name ) {
+	// Forbidden attribute names
+	$reserved_terms = array(
+		'attachment', 'attachment_id', 'author', 'author_name', 'calendar', 'cat', 'category', 'category__and',
+		'category__in', 'category__not_in', 'category_name', 'comments_per_page', 'comments_popup', 'cpage', 'day',
+		'debug', 'error', 'exact', 'feed', 'hour', 'link_category', 'm', 'minute', 'monthnum', 'more', 'name',
+		'nav_menu', 'nopaging', 'offset', 'order', 'orderby', 'p', 'page', 'page_id', 'paged', 'pagename', 'pb', 'perm',
+		'post', 'post__in', 'post__not_in', 'post_format', 'post_mime_type', 'post_status', 'post_tag', 'post_type',
+		'posts', 'posts_per_archive_page', 'posts_per_page', 'preview', 'robots', 's', 'search', 'second', 'sentence',
+		'showposts', 'static', 'subpost', 'subpost_id', 'tag', 'tag__and', 'tag__in', 'tag__not_in', 'tag_id',
+		'tag_slug__and', 'tag_slug__in', 'taxonomy', 'tb', 'term', 'type', 'w', 'withcomments', 'withoutcomments', 'year',
+	);
+
+	return in_array( $attribute_name, $reserved_terms );
+}
