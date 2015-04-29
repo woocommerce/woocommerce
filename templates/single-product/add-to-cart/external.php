7de4d6ad49660888c39cc2cf80d40a5b255e40be
Gabor Javorszky <gabor@javorszky.co.uk>
2015-04-29 02:25:16 +0100

    Adds escaping to url and title in link for grouped product page

--- templates/single-product/add-to-cart/external.php
+++ templates/single-product/add-to-cart/external.php
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 <p class="cart">
-	<a href="<?php echo esc_url( $product_url ); ?>" rel="nofollow" class="single_add_to_cart_button button alt"><?php echo $button_text; ?></a>
+	<a href="<?php echo esc_url( $product_url ); ?>" rel="nofollow" class="single_add_to_cart_button button alt"><?php echo esc_html( $button_text ); ?></a>
 </p>
 
 <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
--- templates/single-product/add-to-cart/variable.php
+++ templates/single-product/add-to-cart/variable.php
@@ -79,7 +79,7 @@ global $product, $post;
 
 			<div class="variations_button">
 				<?php woocommerce_quantity_input(); ?>
-				<button type="submit" class="single_add_to_cart_button button alt"><?php echo $product->single_add_to_cart_text(); ?></button>
+				<button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 			</div>
 
 			<input type="hidden" name="add-to-cart" value="<?php echo $product->id; ?>" />
