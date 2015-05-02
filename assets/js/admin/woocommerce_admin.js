2953c994400acc935a34ed9a1e557375918af2e9
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-05-02 22:13:42 +0545

    Fix JSHint error for field validation error tips

--- assets/js/admin/woocommerce_admin.js
+++ assets/js/admin/woocommerce_admin.js
@@ -10,69 +10,69 @@ jQuery( function ( $ ) {
 		.on( 'wc_add_error_tip', function( e, element, error_type ) {
 			var offset = element.position();
 
-			if ( element.parent().find('.wc_error_tip').size() == 0 ) {
+			if ( element.parent().find( '.wc_error_tip' ).size() === 0 ) {
 				element.after( '<div class="wc_error_tip ' + error_type + '">' + woocommerce_admin[error_type] + '</div>' );
-				element.parent().find('.wc_error_tip')
-					.css('left', offset.left + element.width() - ( element.width() / 2 ) - ( $('.wc_error_tip').width() / 2 ) )
-					.css('top', offset.top + element.height() )
-					.fadeIn('100');
+				element.parent().find( '.wc_error_tip' )
+					.css( 'left', offset.left + element.width() - ( element.width() / 2 ) - ( $( '.wc_error_tip' ).width() / 2 ) )
+					.css( 'top', offset.top + element.height() )
+					.fadeIn( '100' );
 			}
 		})
 		.on( 'wc_remove_error_tip', function( e, element, error_type ) {
-			element.parent().find('.wc_error_tip.' + error_type).remove();
+			element.parent().find( '.wc_error_tip.' + error_type ).remove();
 		})
 		.on( 'click', function() {
-			$('.wc_error_tip').fadeOut('100', function() { $(this).remove(); } );
+			$( '.wc_error_tip' ).fadeOut( '100', function() { $( this ).remove(); } );
 		})
 		.on( 'blur', '.wc_input_decimal[type=text], .wc_input_price[type=text], .wc_input_country_iso[type=text]', function() {
-			$('.wc_error_tip').fadeOut('100', function() { $(this).remove(); } );
+			$( '.wc_error_tip' ).fadeOut( '100', function() { $( this ).remove(); } );
 		})
 		.on( 'keyup change', '.wc_input_price[type=text]', function() {
-			var value    = $(this).val();
-			var regex    = new RegExp( "[^\-0-9\%\\" + woocommerce_admin.mon_decimal_point + "]+", "gi" );
+			var value    = $( this ).val();
+			var regex    = new RegExp( '[^\-0-9\%\\' + woocommerce_admin.mon_decimal_point + ']+', 'gi' );
 			var newvalue = value.replace( regex, '' );
 
 			if ( value !== newvalue ) {
-				$(this).val( newvalue );
-				$( document.body ).triggerHandler( 'wc_add_error_tip', [ $(this), 'i18n_mon_decimal_error' ] );
+				$( this ).val( newvalue );
+				$( document.body ).triggerHandler( 'wc_add_error_tip', [ $( this ), 'i18n_mon_decimal_error' ] );
 			} else {
-				$( document.body ).triggerHandler( 'wc_remove_error_tip', [ $(this), 'i18n_mon_decimal_error' ] );
+				$( document.body ).triggerHandler( 'wc_remove_error_tip', [ $( this ), 'i18n_mon_decimal_error' ] );
 			}
 		})
 		.on( 'keyup change', '.wc_input_decimal[type=text]', function() {
-			var value    = $(this).val();
-			var regex    = new RegExp( "[^\-0-9\%\\" + woocommerce_admin.decimal_point + "]+", "gi" );
+			var value    = $( this ).val();
+			var regex    = new RegExp( '[^\-0-9\%\\' + woocommerce_admin.decimal_point + ']+', 'gi' );
 			var newvalue = value.replace( regex, '' );
 
 			if ( value !== newvalue ) {
-				$(this).val( newvalue );
-				$( document.body ).triggerHandler( 'wc_add_error_tip', [ $(this), 'i18n_decimal_error' ] );
+				$( this ).val( newvalue );
+				$( document.body ).triggerHandler( 'wc_add_error_tip', [ $( this ), 'i18n_decimal_error' ] );
 			} else {
-				$( document.body ).triggerHandler( 'wc_remove_error_tip', [ $(this), 'i18n_decimal_error' ] );
+				$( document.body ).triggerHandler( 'wc_remove_error_tip', [ $( this ), 'i18n_decimal_error' ] );
 			}
 		})
 		.on( 'keyup change', '.wc_input_country_iso[type=text]', function() {
-			var value = $(this).val();
+			var value = $( this ).val();
 			var regex = new RegExp( '^([A-Z])?([A-Z])$' );
 
 			if ( ! regex.test( value ) ) {
-				$(this).val( '' );
-				$( document.body ).triggerHandler( 'wc_add_error_tip', [ $(this), 'i18n_country_iso_error' ] );
+				$( this ).val( '' );
+				$( document.body ).triggerHandler( 'wc_add_error_tip', [ $( this ), 'i18n_country_iso_error' ] );
 			} else {
-				$( document.body ).triggerHandler( 'wc_remove_error_tip', [ $(this), 'i18n_country_iso_error' ] );
+				$( document.body ).triggerHandler( 'wc_remove_error_tip', [ $( this ), 'i18n_country_iso_error' ] );
 			}
 		})
 		.on( 'keyup change', '#_sale_price.wc_input_price[type=text], .wc_input_price[name^=variable_sale_price]', function() {
-			var sale_price_field = $(this);
+			var sale_price_field = $( this ), regular_price_field;
 
-			if( sale_price_field.attr('name').indexOf('variable') !== -1 ) {
-				var regular_price_field = sale_price_field.parents('.variable_pricing').find('.wc_input_price[name^=variable_regular_price]');
+			if( sale_price_field.attr( 'name' ).indexOf( 'variable' ) !== -1 ) {
+				regular_price_field = sale_price_field.parents( '.variable_pricing' ).find( '.wc_input_price[name^=variable_regular_price]' );
 			} else {
-				var regular_price_field = $('#_regular_price');
+				regular_price_field = $( '#_regular_price' );
 			}
 
-			var sale_price    = parseFloat( accounting.unformat( sale_price_field.val(), woocommerce_admin.mon_decimal_point ) );
-			var regular_price = parseFloat( accounting.unformat( regular_price_field.val(), woocommerce_admin.mon_decimal_point ) );
+			var sale_price    = parseFloat( window.accounting.unformat( sale_price_field.val(), woocommerce_admin.mon_decimal_point ) );
+			var regular_price = parseFloat( window.accounting.unformat( regular_price_field.val(), woocommerce_admin.mon_decimal_point ) );
 
 			if ( sale_price >= regular_price ) {
 				$( document.body ).triggerHandler( 'wc_add_error_tip', [ $(this), 'i18_sale_less_than_regular_error' ] );
