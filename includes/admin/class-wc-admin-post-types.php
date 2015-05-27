169e72b8246b93dd3c3eebdd4da80e52ce0d0e4b
Claudio Sanches <contato@claudiosmweb.com>
2015-05-27 13:29:15 -0300

    Added extra arg to all woocommerce_order_item_name filters #8159

--- includes/admin/class-wc-admin-post-types.php
+++ includes/admin/class-wc-admin-post-types.php
@@ -597,9 +597,9 @@ class WC_Admin_Post_Types {
 							<td class="qty"><?php echo absint( $item['qty'] ); ?></td>
 							<td class="name">
 								<?php  if ( $product ) : ?>
-									<?php echo ( wc_product_sku_enabled() && $product->get_sku() ) ? $product->get_sku() . ' - ' : ''; ?><a href="<?php echo get_edit_post_link( $product->id ); ?>" title="<?php echo apply_filters( 'woocommerce_order_item_name', $item['name'], $item ); ?>"><?php echo apply_filters( 'woocommerce_order_item_name', $item['name'], $item ); ?></a>
+									<?php echo ( wc_product_sku_enabled() && $product->get_sku() ) ? $product->get_sku() . ' - ' : ''; ?><a href="<?php echo get_edit_post_link( $product->id ); ?>" title="<?php echo apply_filters( 'woocommerce_order_item_name', $item['name'], $item, false ); ?>"><?php echo apply_filters( 'woocommerce_order_item_name', $item['name'], $item, false ); ?></a>
 								<?php else : ?>
-									<?php echo apply_filters( 'woocommerce_order_item_name', $item['name'], $item ); ?>
+									<?php echo apply_filters( 'woocommerce_order_item_name', $item['name'], $item, false ); ?>
 								<?php endif; ?>
 								<?php if ( ! empty( $item_meta_html ) ) : ?>
 									<a class="tips" href="#" data-tip="<?php echo esc_attr( $item_meta_html ); ?>">[?]</a>
--- templates/emails/email-order-items.php
+++ templates/emails/email-order-items.php
@@ -26,7 +26,7 @@ foreach ( $items as $item_id => $item ) :
 				}
 
 				// Product name
-				echo apply_filters( 'woocommerce_order_item_name', $item['name'], $item );
+				echo apply_filters( 'woocommerce_order_item_name', $item['name'], $item, false );
 
 				// SKU
 				if ( $show_sku && is_object( $_product ) && $_product->get_sku() ) {
