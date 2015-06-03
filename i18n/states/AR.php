83cb4cfa06a42611d2c497dc69aec0f9e48a8e5f
Marcelo Pedra <marcelo@marcelopedra.com.ar>
2015-06-03 10:39:29 -0300

    Added Argentinian provinces

---
+++ i18n/states/AR.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Argentinian provinces
+ *
+ * @author      Marcelo Pedra
+ * @category    i18n
+ * @package     WooCommerce/i18n
+ * @version     2.0.0
+ */
+global $states;
+
+$states['AR'] = array(
+	'BA'	=> __( 'Buenos Aires', 'woocommerce' ),
+	'CT'	=> __( 'Catamarca', 'woocommerce' ),
+	'CABA'	=> __( 'Ciudad Autónoma de Buenos Aires', 'woocommerce' ),
+	'CC'	=> __( 'Chaco', 'woocommerce' ),
+	'CH'	=> __( 'Chubut', 'woocommerce' ),
+	'CBA'	=> __( 'Córdoba', 'woocommerce' ),
+	'CR'	=> __( 'Corrientes', 'woocommerce' ),
+	'ER'	=> __( 'Entre Ríos', 'woocommerce' ),
+	'FO'	=> __( 'Formosa', 'woocommerce' ),
+	'JY'	=> __( 'Jujuy', 'woocommerce' ),
+	'LP'	=> __( 'La Pampa', 'woocommerce' ),
+	'LR'	=> __( 'La Rioja', 'woocommerce' ),
+	'MZ'	=> __( 'Mendoza', 'woocommerce' ),
+	'MN'	=> __( 'Misiones', 'woocommerce' ),
+	'NQ'	=> __( 'Neuquén', 'woocommerce' ),
+	'RN'	=> __( 'Río Negro', 'woocommerce' ),
+	'SA'	=> __( 'Salta', 'woocommerce' ),
+	'SJ'	=> __( 'San Juan', 'woocommerce' ),
+	'SL'	=> __( 'San Luis', 'woocommerce' ),
+	'SC'	=> __( 'Santa Cruz', 'woocommerce' ),
+	'SF'	=> __( 'Santa Fe', 'woocommerce' ),
+	'SE'	=> __( 'Santiago del Estero', 'woocommerce' ),
+	'TF'	=> __( 'Tierra del Fuego, Antártida e Islas del Atlántico Sur', 'woocommerce' ),
+	'TM'	=> __( 'Tucumán', 'woocommerce' )
+);
