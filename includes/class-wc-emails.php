8b02b0214525a0c82812f91f2a56a077db17ff65
davidlenehan <lenny1@Lennys-MacBook-Air.local>
2015-06-17 15:35:54 +0100

    Add email notification for partial refunds
    fixes #8278
    
    Emails are now sent to the customer when a partial refund is issued. I
    referenced #7785 when doing this.
    
    When a full refund happens the email the customer receives is clear
    enough that they are getting a full refund. However I thought it would
    be useful to add a new line in these new partial refund emails that
    states exactly the value of the partial refund as it may not be that
    clear.

--- includes/class-wc-emails.php
+++ includes/class-wc-emails.php
@@ -70,6 +70,7 @@ class WC_Emails {
 			'woocommerce_order_status_on-hold_to_cancelled',
 			'woocommerce_order_status_completed',
 			'woocommerce_order_status_refunded',
+			'woocommerce_order_partially_refunded',
 			'woocommerce_new_customer_note',
 			'woocommerce_created_customer'
 		) );
@@ -120,15 +121,16 @@ class WC_Emails {
 		// Include email classes
 		include_once( 'emails/class-wc-email.php' );
 
-		$this->emails['WC_Email_New_Order']                 = include( 'emails/class-wc-email-new-order.php' );
-		$this->emails['WC_Email_Cancelled_Order']           = include( 'emails/class-wc-email-cancelled-order.php' );
-		$this->emails['WC_Email_Customer_Processing_Order'] = include( 'emails/class-wc-email-customer-processing-order.php' );
-		$this->emails['WC_Email_Customer_Completed_Order']  = include( 'emails/class-wc-email-customer-completed-order.php' );
-		$this->emails['WC_Email_Customer_Refunded_Order']   = include( 'emails/class-wc-email-customer-refunded-order.php' );
-		$this->emails['WC_Email_Customer_Invoice']          = include( 'emails/class-wc-email-customer-invoice.php' );
-		$this->emails['WC_Email_Customer_Note']             = include( 'emails/class-wc-email-customer-note.php' );
-		$this->emails['WC_Email_Customer_Reset_Password']   = include( 'emails/class-wc-email-customer-reset-password.php' );
-		$this->emails['WC_Email_Customer_New_Account']      = include( 'emails/class-wc-email-customer-new-account.php' );
+		$this->emails['WC_Email_New_Order']                 		= include( 'emails/class-wc-email-new-order.php' );
+		$this->emails['WC_Email_Cancelled_Order']           		= include( 'emails/class-wc-email-cancelled-order.php' );
+		$this->emails['WC_Email_Customer_Processing_Order'] 		= include( 'emails/class-wc-email-customer-processing-order.php' );
+		$this->emails['WC_Email_Customer_Completed_Order']  		= include( 'emails/class-wc-email-customer-completed-order.php' );
+		$this->emails['WC_Email_Customer_Refunded_Order']   		= include( 'emails/class-wc-email-customer-refunded-order.php' );
+		$this->emails['WC_Email_Customer_Partially_Refunded_Order'] = include( 'emails/class-wc-email-customer-partially-refunded-order.php' );
+		$this->emails['WC_Email_Customer_Invoice']          		= include( 'emails/class-wc-email-customer-invoice.php' );
+		$this->emails['WC_Email_Customer_Note']             		= include( 'emails/class-wc-email-customer-note.php' );
+		$this->emails['WC_Email_Customer_Reset_Password']   		= include( 'emails/class-wc-email-customer-reset-password.php' );
+		$this->emails['WC_Email_Customer_New_Account']      		= include( 'emails/class-wc-email-customer-new-account.php' );
 
 		$this->emails = apply_filters( 'woocommerce_email_classes', $this->emails );
 
---
+++ templates/emails/plain/customer-partially-refunded-order.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Customer partially refunded order email (plain text)
+ *
+ * @author   WooThemes
+ * @package  WooCommerce/Templates/Emails/Plain
+ * @version  2.4.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+echo "= " . $email_heading . " =\n\n";
+
+echo sprintf( __( "Hi there. Your order on %s has been partially refunded. Your order details are shown below for your reference:", 'woocommerce' ), get_option( 'blogname' ) ) . "\n\n";
+
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+
+do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text );
+
+echo strtoupper( sprintf( __( 'Order number: %s', 'woocommerce' ), $order->get_order_number() ) ) . "\n";
+echo date_i18n( __( 'jS F Y', 'woocommerce' ), strtotime( $order->order_date ) ) . "\n";
+
+do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text );
+
+echo "\n" . $order->email_order_items_table( true, false, true, '', '', true );
+
+echo "==========\n\n";
+
+if ( $refund->get_refund_amount() > 0 ) {
+	echo __( 'Amount Refunded', 'woocommerce' ) . "\t " . $refund->get_formatted_refund_amount() . "\n";
+}
+
+if ( $totals = $order->get_order_item_totals() ) {
+	foreach ( $totals as $total ) {
+		echo $total['label'] . "\t " . $total['value'] . "\n";
+	}
+}
+
+echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+
+do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text );
+
+do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text );
+
+echo "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
+
+echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
