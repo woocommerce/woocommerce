38dd251f96f705edcdb586a6a2e470f8952af5b8
Mike Jolley <mike.jolley@me.com>
2015-06-17 14:16:37 +0100

    geolocation

---
+++ assets/js/frontend/woocommerce.js
@@ -0,0 +1,15 @@
+jQuery( function( $ ) {
+	// Orderby
+	$( '.woocommerce-ordering' ).on( 'change', 'select.orderby', function() {
+		$( this ).closest( 'form' ).submit();
+	});
+
+	// Target quantity inputs on product pages
+	$( 'input.qty:not(.product-quantity input.qty)' ).each( function() {
+		var min = parseFloat( $( this ).attr( 'min' ) );
+
+		if ( min >= 0 && parseFloat( $( this ).val() ) < min ) {
+			$( this ).val( min );
+		}
+	});
+});
