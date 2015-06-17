ccc3387bbde666fbac28b59face06fd1f7100eed
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-06-17 20:03:45 +0545

    Wrap tabbed panels so we can trigger :P

--- assets/js/admin/meta-boxes.js
+++ assets/js/admin/meta-boxes.js
@@ -33,20 +33,21 @@ jQuery( function ( $ ) {
 		});
 	});
 
-	// Tabs
-	$( 'ul.wc-tabs' ).show();
-	$( 'ul.wc-tabs a' ).click( function() {
-		var panel_wrap = $( this ).closest( 'div.panel-wrap' );
-		$( 'ul.wc-tabs li', panel_wrap ).removeClass( 'active' );
-		$( this ).parent().addClass( 'active' );
-		$( 'div.panel', panel_wrap ).hide();
-		$( $( this ).attr( 'href' ) ).show();
-		return false;
-	});
-	$( 'div.panel-wrap' ).each( function() {
-		$( this ).find( 'div.panel:not(:first)' ).hide();
-		$( this ).find( 'ul.wc-tabs li' ).eq( 0 ).find( 'a' ).click();
-	});
+	// Tabbed Panels
+	$( document.body ).on( 'wc-init-tabbed-panels', function() {
+		$( 'ul.wc-tabs' ).show();
+		$( 'ul.wc-tabs a' ).click( function() {
+			var panel_wrap = $( this ).closest( 'div.panel-wrap' );
+			$( 'ul.wc-tabs li', panel_wrap ).removeClass( 'active' );
+			$( this ).parent().addClass( 'active' );
+			$( 'div.panel', panel_wrap ).hide();
+			$( $( this ).attr( 'href' ) ).show();
+			return false;
+		});
+		$( 'div.panel-wrap' ).each( function() {
+			$( this ).find( 'ul.wc-tabs li' ).eq( 0 ).find( 'a' ).click();
+		});
+	}).trigger( 'wc-init-tabbed-panels' );
 
 	// Date Picker
 	$( document.body ).on( 'wc-init-datepickers', function() {
