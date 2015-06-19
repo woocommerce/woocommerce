6fbf78554b1d2aecb9ebc302a715ecd4ca6812ca
aaronzdavis <aaronzdavis@gmail.com>
2015-06-19 13:52:01 -0700

    woocommerce_content should use the archive-product template

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -344,39 +344,9 @@ if ( ! function_exists( 'woocommerce_content' ) ) {
 
 			endwhile;
 
-		} else { ?>
+		} else {
 
-			<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-
-				<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
-
-			<?php endif; ?>
-
-			<?php do_action( 'woocommerce_archive_description' ); ?>
-
-			<?php if ( have_posts() ) : ?>
-
-				<?php do_action('woocommerce_before_shop_loop'); ?>
-
-				<?php woocommerce_product_loop_start(); ?>
-
-					<?php woocommerce_product_subcategories(); ?>
-
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php wc_get_template_part( 'content', 'product' ); ?>
-
-					<?php endwhile; // end of the loop. ?>
-
-				<?php woocommerce_product_loop_end(); ?>
-
-				<?php do_action('woocommerce_after_shop_loop'); ?>
-
-			<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-
-				<?php wc_get_template( 'loop/no-products-found.php' ); ?>
-
-			<?php endif;
+			wc_get_template_part( 'archive', 'product' );
 
 		}
 	}
