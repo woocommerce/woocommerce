ec2bd2f305dea4e1dfd5f5647d5999a69eccab1e
Mike Jolley <mike.jolley@me.com>
2015-07-07 11:49:12 +0100

    Fixed datepicker for variations

--- assets/js/admin/meta-boxes-product.js
+++ assets/js/admin/meta-boxes-product.js
@@ -228,23 +228,20 @@ jQuery( function( $ ){
 		}
 	}).change();
 
-
 	// DATE PICKER FIELDS
-	var dates = $( ".sale_price_dates_fields input" ).datepicker({
-		defaultDate: "",
-		dateFormat: "yy-mm-dd",
-		numberOfMonths: 1,
-		showButtonPanel: true,
-		onSelect: function( selectedDate ) {
-			var option = $(this).is('#_sale_price_dates_from, .sale_price_dates_from') ? "minDate" : "maxDate";
-
-			var instance = $( this ).data( "datepicker" ),
-				date = $.datepicker.parseDate(
-					instance.settings.dateFormat ||
-					$.datepicker._defaults.dateFormat,
-					selectedDate, instance.settings );
-			dates.not( this ).datepicker( "option", option, date );
-		}
+	$( '.sale_price_dates_fields' ).each( function() {
+		var dates = $( this ).find( 'input' ).datepicker({
+			defaultDate: '',
+			dateFormat: 'yy-mm-dd',
+			numberOfMonths: 1,
+			showButtonPanel: true,
+			onSelect: function( selectedDate ) {
+				var option   = $( this ).is( '#_sale_price_dates_from, .sale_price_dates_from' ) ? 'minDate' : 'maxDate';
+				var instance = $( this ).data( 'datepicker' );
+				var date     = $.datepicker.parseDate( instance.settings.dateFormat || $.datepicker._defaults.dateFormat, selectedDate, instance.settings );
+				dates.not( this ).datepicker( 'option', option, date );
+			}
+		});
 	});
 
 	// ATTRIBUTE TABLES
