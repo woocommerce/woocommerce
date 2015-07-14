6c936a162dcd35d93702624d73ccec36ecd89a3f
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-07-15 04:38:32 +0545

    Tweak coding standard for update notice script

--- includes/admin/views/html-notice-update.php
+++ includes/admin/views/html-notice-update.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<p class="submit"><a href="<?php echo esc_url( add_query_arg( 'do_update_woocommerce', 'true', admin_url( 'admin.php?page=wc-settings' ) ) ); ?>" class="wc-update-now button-primary"><?php _e( 'Run the updater', 'woocommerce' ); ?></a></p>
 </div>
 <script type="text/javascript">
-	jQuery('.wc-update-now').click('click', function(){
+	jQuery( '.wc-update-now' ).click( 'click', function() {
 		var answer = confirm( '<?php _e( 'It is strongly recommended that you backup your database before proceeding. Are you sure you wish to run the updater now?', 'woocommerce' ); ?>' );
 		return answer;
 	});
