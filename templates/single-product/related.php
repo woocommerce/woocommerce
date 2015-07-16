6fb8f0a47caa3f01b8294152a93f58257d362ca2
Chris Wilcoxson <chris@slushman.com>
2015-07-16 16:44:58 -0500

    Added woocommerce_related_products_heading filter to templates/single-product/related.php

--- templates/single-product/related.php
+++ templates/single-product/related.php
@@ -39,7 +39,7 @@ if ( $products->have_posts() ) : ?>
 
 	<div class="related products">
 
-		<h2><?php _e( 'Related Products', 'woocommerce' ); ?></h2>
+		<h2><?php echo apply_filters( 'woocommerce_related_products_heading', __( 'Related Products', 'woocommerce' ) ); ?></h2>
 
 		<?php woocommerce_product_loop_start(); ?>
 
