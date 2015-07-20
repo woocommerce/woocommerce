2fb5f5bf78f889b9dc2dc913ee27a8eef99ed7ab
Jodi Warren <jodi@jodiwarren.com>
2015-07-20 18:10:56 +0100

    Change product title to a hooked function. Fixes #8601

--- includes/wc-template-hooks.php
+++ includes/wc-template-hooks.php
@@ -88,6 +88,7 @@ add_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 )
  */
 add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
 
---
+++ templates/loop/title.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Single Product title
+ *
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 1.6.5
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+<h3><?php the_title(); ?></h3>
