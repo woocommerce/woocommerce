808c01cb902bc1158a17e09c899c4e9b30e66f57
Mike Jolley <mike.jolley@me.com>
2015-07-28 10:32:28 +0100

    Added woocommerce_single_variation hook for variation data and cart button to reduce code in template file and used hooks. @claudiosmweb
    Removed woocommerce_before_variations_button @Ninos. Use
    woocommerce_single_variation

--- includes/wc-template-hooks.php
+++ includes/wc-template-hooks.php
@@ -149,6 +149,8 @@ add_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 
 add_action( 'woocommerce_grouped_add_to_cart', 'woocommerce_grouped_add_to_cart', 30 );
 add_action( 'woocommerce_variable_add_to_cart', 'woocommerce_variable_add_to_cart', 30 );
 add_action( 'woocommerce_external_add_to_cart', 'woocommerce_external_add_to_cart', 30 );
+add_action( 'woocommerce_single_variation', 'woocommerce_single_variation', 10 );
+add_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 20 );
 
 /**
  * Pagination after shop loops
