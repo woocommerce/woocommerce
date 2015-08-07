7985aec2969e7d7fb471233a60ec6881f5fa0134
George Stephanis <george@stephanis.info>
2015-08-07 15:36:12 -0400

    Ernest the JS so none of it evaluates before dom ready.

--- assets/js/admin/settings-views-html-settings-tax.js
+++ assets/js/admin/settings-views-html-settings-tax.js
@@ -4,10 +4,11 @@
  */
 
 (function($, data, wp){
-	var rowTemplate = wp.template( 'wc-tax-table-row' ),
-		$tbody = $('#rates');
-
 	$(function() {
+
+		var rowTemplate = wp.template( 'wc-tax-table-row' ),
+			$tbody = $('#rates');
+
 		// Blank out the contents.
 		$tbody.innerHTML = '';
 
@@ -27,100 +28,100 @@
 			source: data.states,
 			minLength: 3
 		});
-	});
 
-	$('.wc_tax_rates .remove_tax_rates').click(function() {
-		if ( $tbody.find('tr.current').length > 0 ) {
-			var $current = $tbody.find('tr.current');
-			$current.find('input').val('');
-			$current.find('input.remove_tax_rate').val('1');
+		$('.wc_tax_rates .remove_tax_rates').click(function() {
+			if ( $tbody.find('tr.current').length > 0 ) {
+				var $current = $tbody.find('tr.current');
+				$current.find('input').val('');
+				$current.find('input.remove_tax_rate').val('1');
 
-			$current.each(function(){
-				if ( $(this).is('.new') ) {
-					$( this ).remove();
-				} else {
-					$( this ).hide();
-				}
-			});
-		} else {
-			window.alert( data.strings.no_rows_selected );
-		}
-		return false;
-	});
-
-	/**
-	 * Handle the exporting of tax rates.
-	 *
-	 * As an aside: Why is this being handled in Javascript instead of being built by php? -George
-	 */
-	$('.wc_tax_rates .export').click(function() {
-		var csv_data = 'data:application/csv;charset=utf-8,' + data.strings.csv_data_cols.join(',') + '\n';
-
-		$('#rates tr:visible').each(function() {
-			var row = '';
-			$(this).find('td:not(.sort) input').each(function() {
-				var val = '';
-
-				if ( $(this).is('.checkbox') ) {
-					if ( $(this).is(':checked') ) {
-						val = 1;
+				$current.each(function(){
+					if ( $(this).is('.new') ) {
+						$( this ).remove();
 					} else {
-						val = 0;
+						$( this ).hide();
 					}
-				} else {
-					val = $(this).val();
-					if ( ! val ) {
-						val = $( this ).attr( 'placeholder' );
+				});
+			} else {
+				window.alert( data.strings.no_rows_selected );
+			}
+			return false;
+		});
+
+		/**
+		 * Handle the exporting of tax rates.
+		 *
+		 * As an aside: Why is this being handled in Javascript instead of being built by php? -George
+		 */
+		$('.wc_tax_rates .export').click(function() {
+			var csv_data = 'data:application/csv;charset=utf-8,' + data.strings.csv_data_cols.join(',') + '\n';
+
+			$('#rates tr:visible').each(function() {
+				var row = '';
+				$(this).find('td:not(.sort) input').each(function() {
+					var val = '';
+
+					if ( $(this).is('.checkbox') ) {
+						if ( $(this).is(':checked') ) {
+							val = 1;
+						} else {
+							val = 0;
+						}
+					} else {
+						val = $(this).val();
+						if ( ! val ) {
+							val = $( this ).attr( 'placeholder' );
+						}
 					}
-				}
-				row = row + val + ',';
+					row = row + val + ',';
+				});
+				row = row + data.current_class;
+				//row.substring( 0, row.length - 1 );
+				csv_data = csv_data + row + '\n';
 			});
-			row = row + data.current_class;
-			//row.substring( 0, row.length - 1 );
-			csv_data = csv_data + row + '\n';
+
+			$(this).attr( 'href', encodeURI( csv_data ) );
+
+			return true;
 		});
 
-		$(this).attr( 'href', encodeURI( csv_data ) );
+		/**
+		 * Add a new blank row to the table for the user to fill out and save.
+		 */
+		$('.wc_tax_rates .insert').click(function() {
+			var size = $tbody.find('tr').length;
+			var code = wp.template( 'wc-tax-table-row' )( {
+				tax_rate_id       : 'new-' + size,
+				tax_rate_priority : 1,
+				tax_rate_shipping : 1,
+				newRow            : true
+			} );
 
-		return true;
-	});
+			if ( $tbody.find('tr.current').length > 0 ) {
+				$tbody.find('tr.current').after( code );
+			} else {
+				$tbody.append( code );
+			}
 
-	/**
-	 * Add a new blank row to the table for the user to fill out and save.
-	 */
-	$('.wc_tax_rates .insert').click(function() {
-		var size = $tbody.find('tr').length;
-		var code = wp.template( 'wc-tax-table-row' )( {
-			tax_rate_id       : 'new-' + size,
-			tax_rate_priority : 1,
-			tax_rate_shipping : 1,
-			newRow            : true
-		} );
+			$( 'td.country input' ).autocomplete({
+				source: data.countries,
+				minLength: 3
+			});
 
-		if ( $tbody.find('tr.current').length > 0 ) {
-			$tbody.find('tr.current').after( code );
-		} else {
-			$tbody.append( code );
-		}
+			$( 'td.state input' ).autocomplete({
+				source: data.states,
+				minLength: 3
+			});
 
-		$( 'td.country input' ).autocomplete({
-			source: data.countries,
-			minLength: 3
+			return false;
 		});
 
-		$( 'td.state input' ).autocomplete({
-			source: data.states,
-			minLength: 3
+		/**
+		 * Postcode and city don't have `name` values by default. They're only created if the contents changes, to save on database queries (I think)
+		 */
+		$('.wc_tax_rates td.postcode, .wc_tax_rates td.city').find('input').change(function() {
+			$(this).attr( 'name', $(this).attr( 'data-name' ) );
 		});
 
-		return false;
 	});
-
-	/**
-	 * Postcode and city don't have `name` values by default. They're only created if the contents changes, to save on database queries (I think)
-	 */
-	$('.wc_tax_rates td.postcode, .wc_tax_rates td.city').find('input').change(function() {
-		$(this).attr( 'name', $(this).attr( 'data-name' ) );
-	});
-
 })(jQuery, htmlSettingsTaxLocalizeScript, wp);
