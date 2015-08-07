877e390646e0b7023e2a5870d8c30a8f768187cc
Mike Jolley <mike.jolley@me.com>
2015-08-07 14:06:19 +0100

    Remove $type from prepare

--- includes/abstracts/abstract-wc-order.php
+++ includes/abstracts/abstract-wc-order.php
@@ -1126,15 +1126,9 @@ abstract class WC_Abstract_Order {
 			$type = array( $type );
 		}
 
-		$line_items = $wpdb->get_results( $wpdb->prepare( "
-			SELECT      order_item_id, order_item_name, order_item_type
-			FROM        {$wpdb->prefix}woocommerce_order_items
-			WHERE       order_id = %d
-			AND         order_item_type IN ( '" . implode( "','", array_map( 'esc_sql', $type ) ) . "' )
-			ORDER BY    order_item_id
-		", $this->id ) );
-
-		$items = array();
+		$get_items_sql  = $wpdb->prepare( "SELECT order_item_id, order_item_name, order_item_type FROM {$wpdb->prefix}woocommerce_order_items WHERE order_id = %d ", $this->id );
+		$get_items_sql .= "AND order_item_type IN ( '" . implode( "','", array_map( 'esc_sql', $type ) ) . "' ) ORDER BY order_item_id;";
+		$line_items     = $wpdb->get_results( $get_items_sql );
 
 		// Loop items
 		foreach ( $line_items as $item ) {
