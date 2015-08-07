4874408a984202dabcbcfadc52fa71f810c8bdfb
George Stephanis <george@stephanis.info>
2015-08-07 15:25:09 -0400

    Insert placeholder divs for search and pagination.

--- includes/admin/settings/views/html-settings-tax.php
+++ includes/admin/settings/views/html-settings-tax.php
@@ -67,6 +67,13 @@ wp_enqueue_script( 'wc-settings-tax' );
 
 <h3><?php printf( __( 'Tax Rates for the "%s" Class', 'woocommerce' ), $current_class ? esc_html( $current_class ) : __( 'Standard', 'woocommerce' ) ); ?></h3>
 <p><?php printf( __( 'Define tax rates for countries and states below. <a href="%s">See here</a> for available alpha-2 country codes.', 'woocommerce' ), 'http://en.wikipedia.org/wiki/ISO_3166-1#Current_codes' ); ?></p>
+
+<div class="wc-tax-rates-search" id="rates-search">
+	<input type="search" class="wc-tax-rates-search-field" placeholder="<?php esc_attr_e( 'Search…', 'woocommerce' ); ?>" />
+</div>
+
+<div class="wc-tax-rates-pagination" id="rates-pagination"></div>
+
 <table class="wc_tax_rates wc_input_table sortable widefat">
 	<thead>
 		<tr>
