8a416f105bb7a6a839e83c44e178760015703875
George Stephanis <george@stephanis.info>
2015-08-08 11:03:46 -0400

    Should be comparing to `'1'` not `1`.

--- includes/admin/settings/views/html-settings-tax.php
+++ includes/admin/settings/views/html-settings-tax.php
@@ -82,11 +82,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</td>
 
 		<td class="compound">
-			<input type="checkbox" class="checkbox" name="tax_rate_compound[{{ data.tax_rate_id }}]" <# if ( 1 === data.tax_rate_compound ) { #> checked="checked" <# } #> />
+			<input type="checkbox" class="checkbox" name="tax_rate_compound[{{ data.tax_rate_id }}]" <# if ( '1' === data.tax_rate_compound ) { #> checked="checked" <# } #> />
 		</td>
 
 		<td class="apply_to_shipping">
-			<input type="checkbox" class="checkbox" name="tax_rate_shipping[{{ data.tax_rate_id }}]" <# if ( 1 === data.tax_rate_shipping ) { #> checked="checked" <# } #> />
+			<input type="checkbox" class="checkbox" name="tax_rate_shipping[{{ data.tax_rate_id }}]" <# if ( '1' === data.tax_rate_shipping ) { #> checked="checked" <# } #> />
 		</td>
 	</tr>
 </script>
