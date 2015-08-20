cc122533398ddf61513ba27b705e67059143eec9
Claudio Sanches <contato@claudiosmweb.com>
2015-08-20 15:08:02 -0300

    Make travis use php lint in all files

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -3,6 +3,15 @@
 
 if [ $1 == 'before' ]; then
 
+	# Lint files
+	for file in $(find . -name "*.php" -and -not -path "./tmp/*" -and -not -path "./tests/*" -and -not -path "./apigen/*"); do
+		output=$( php -l $file )
+		if [[ $output == *"Errors parsing"* ]]; then
+			echo "Build stopped because of a syntax error: $output";
+			exit 1;
+		fi
+	done
+
 	# composer install fails in PHP 5.2
 	[ $TRAVIS_PHP_VERSION == '5.2' ] && exit;
 
