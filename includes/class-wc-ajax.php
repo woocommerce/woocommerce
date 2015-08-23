0cf74b5dba079662ca24c0473bb404ac2d2a8282
Mike Jolley <mike.jolley@me.com>
2015-08-23 21:17:03 +0100

    Set ajax endpoint headers like admin_ajax
    @claudiosmweb does this make sense to you?

--- includes/class-wc-ajax.php
+++ includes/class-wc-ajax.php
@@ -36,7 +36,7 @@ class WC_AJAX {
 	}
 
 	/**
-	 * Set AJAX defines.
+	 * Set WC AJAX constant and headers.
 	 */
 	public static function define_ajax() {
 		if ( ! empty( $_GET['wc-ajax'] ) ) {
@@ -50,6 +50,12 @@ class WC_AJAX {
 			if ( ! WP_DEBUG || ( WP_DEBUG && ! WP_DEBUG_DISPLAY ) ) {
 				@ini_set( 'display_errors', 0 );
 			}
+			// Send headers like admin-ajax.php
+			send_origin_headers();
+			@header( 'Content-Type: text/html; charset=' . get_option( 'blog_charset' ) );
+			@header( 'X-Robots-Tag: noindex' );
+			send_nosniff_header();
+			nocache_headers();
 		}
 	}
 
