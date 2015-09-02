9cee4fbb5dac4d3860804e25fc630c90b62325c7
Brandon Lavigne <brandon@caavadesign.com>
2015-09-02 13:15:10 -0700

    move content-product classes out of template and into post_class display filter

--- templates/content-product.php
+++ templates/content-product.php
@@ -11,38 +11,8 @@
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
-}
-
-global $product, $woocommerce_loop;
-
-// Store loop count we're currently on
-if ( empty( $woocommerce_loop['loop'] ) ) {
-	$woocommerce_loop['loop'] = 0;
-}
-
-// Store column count for displaying the grid
-if ( empty( $woocommerce_loop['columns'] ) ) {
-	$woocommerce_loop['columns'] = apply_filters( 'loop_shop_columns', 4 );
-}
-
-// Ensure visibility
-if ( ! $product || ! $product->is_visible() ) {
-	return;
-}
-
-// Increase loop count
-$woocommerce_loop['loop']++;
-
-// Extra post classes
-$classes = array();
-if ( 0 == ( $woocommerce_loop['loop'] - 1 ) % $woocommerce_loop['columns'] || 1 == $woocommerce_loop['columns'] ) {
-	$classes[] = 'first';
-}
-if ( 0 == $woocommerce_loop['loop'] % $woocommerce_loop['columns'] ) {
-	$classes[] = 'last';
-}
-?>
-<li <?php post_class( $classes ); ?>>
+} ?>
+<li <?php post_class(); ?>>
 
 	<?php do_action( 'woocommerce_before_shop_loop_item' ); ?>
 
