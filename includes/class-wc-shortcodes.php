cc3962cddb914ebaf74399f31f5738849726a9a4
Nicola Mustone <mustone.nicola@gmail.com>
2015-09-11 10:53:26 +0200

    wrong per_page attribute

--- includes/class-wc-shortcodes.php
+++ includes/class-wc-shortcodes.php
@@ -779,7 +779,7 @@ class WC_Shortcodes {
 	 */
 	public static function related_products( $atts ) {
 		$atts = shortcode_atts( array(
-			'posts_per_page' => '4',
+			'per_page' => '4',
 			'columns'        => '4',
 			'orderby'        => 'rand'
 		), $atts );
