82f8b61418293ad8d9759fcaa85ef7db2fe4edba
James Koster <james@jameskoster.co.uk>
2015-09-15 17:05:52 +0100

    make it clearer that variation drag/drop only applies to the backend. closes #9104

--- includes/admin/meta-boxes/views/html-variation-admin.php
+++ includes/admin/meta-boxes/views/html-variation-admin.php
@@ -16,7 +16,7 @@ extract( $variation_data );
 	<h3>
 		<a href="#" class="remove_variation delete" rel="<?php echo esc_attr( $variation_id ); ?>"><?php _e( 'Remove', 'woocommerce' ); ?></a>
 		<div class="handlediv" title="<?php esc_attr_e( 'Click to toggle', 'woocommerce' ); ?>"></div>
-		<div class="tips sort" data-tip="<?php esc_attr_e( 'Drag and drop, or click to set menu order manually', 'woocommerce' ); ?>"></div>
+		<div class="tips sort" data-tip="<?php esc_attr_e( 'Drag and drop, or click to set admin variation order', 'woocommerce' ); ?>"></div>
 		<strong>#<?php echo esc_html( $variation_id ); ?>: </strong>
 		<?php
 			foreach ( $parent_data['attributes'] as $attribute ) {
