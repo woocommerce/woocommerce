daf371052c37b7be8b8d8abe8aebfcebab93ccef
Claudio Sanches <contato@claudiosmweb.com>
2015-09-18 12:21:51 -0300

    Missing ) on order screen for customers select2

--- includes/admin/meta-boxes/class-wc-meta-box-order-data.php
+++ includes/admin/meta-boxes/class-wc-meta-box-order-data.php
@@ -222,7 +222,7 @@ class WC_Meta_Box_Order_Data {
 							if ( ! empty( $order->customer_user ) ) {
 								$user_id     = absint( $order->customer_user );
 								$user        = get_user_by( 'id', $user_id );
-								$user_string = esc_html( $user->display_name ) . ' (#' . absint( $user->ID ) . ' &ndash; ' . esc_html( $user->user_email );
+								$user_string = esc_html( $user->display_name ) . ' (#' . absint( $user->ID ) . ' &ndash; ' . esc_html( $user->user_email ) . ')';
 							}
 							?>
 							<input type="hidden" class="wc-customer-search" id="customer_user" name="customer_user" data-placeholder="<?php esc_attr_e( 'Guest', 'woocommerce' ); ?>" data-selected="<?php echo htmlspecialchars( $user_string ); ?>" value="<?php echo $user_id; ?>" data-allow_clear="true" />
