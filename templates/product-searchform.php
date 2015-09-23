0dfa047f93b1362443fa34e84ec1a2aac7f181a8
Claudio Sanches <contato@claudiosmweb.com>
2015-09-23 17:14:56 -0300

    Added template head and version for templates/product-searchform.php

--- templates/product-searchform.php
+++ templates/product-searchform.php
@@ -1,3 +1,21 @@
+<?php
+/**
+ * The template for displaying product search form.
+ *
+ * Override this template by copying it to yourtheme/woocommerce/product-searchform.php
+ *
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.5.0
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+
 <form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
 	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'woocommerce' ); ?></label>
 	<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'woocommerce' ); ?>" />
