96103724dc53e70c6861a61422f0ab8b89d184a2
Mike Jolley <mike.jolley@me.com>
2015-10-02 10:42:48 +0200

    Display 2 averages on report
    Closes #9144

--- includes/admin/reports/class-wc-report-sales-by-date.php
+++ includes/admin/reports/class-wc-report-sales-by-date.php
@@ -317,7 +317,8 @@ class WC_Report_Sales_By_Date extends WC_Admin_Report {
 		$this->report_data->net_sales   = wc_format_decimal( $this->report_data->total_sales - $this->report_data->total_shipping - $this->report_data->total_tax - $this->report_data->total_shipping_tax, 2 );
 
 		// Calculate average based on net
-		$this->report_data->average_sales = wc_format_decimal( $this->report_data->net_sales / ( $this->chart_interval + 1 ), 2 );
+		$this->report_data->average_sales       = wc_format_decimal( $this->report_data->net_sales / ( $this->chart_interval + 1 ), 2 );
+		$this->report_data->average_total_sales = wc_format_decimal( $this->report_data->total_sales / ( $this->chart_interval + 1 ), 2 );
 
 		// Total orders and discounts also includes those which have been refunded at some point
 		$this->report_data->total_orders          = absint( array_sum( wp_list_pluck( $this->report_data->order_counts, 'count' ) ) );
@@ -338,11 +339,13 @@ class WC_Report_Sales_By_Date extends WC_Admin_Report {
 
 		switch ( $this->chart_groupby ) {
 			case 'day' :
-				$average_sales_title = sprintf( __( '%s average daily sales', 'woocommerce' ), '<strong>' . wc_price( $data->average_sales ) . '</strong>' );
+				$average_total_sales_title = sprintf( __( '%s average gross daily sales', 'woocommerce' ), '<strong>' . wc_price( $data->average_total_sales ) . '</strong>' );
+				$average_sales_title       = sprintf( __( '%s average net daily sales', 'woocommerce' ), '<strong>' . wc_price( $data->average_sales ) . '</strong>' );
 			break;
 			case 'month' :
 			default :
-				$average_sales_title = sprintf( __( '%s average monthly sales', 'woocommerce' ), '<strong>' . wc_price( $data->average_sales ) . '</strong>' );
+				$average_total_sales_title = sprintf( __( '%s average gross monthly sales', 'woocommerce' ), '<strong>' . wc_price( $data->average_total_sales ) . '</strong>' );
+				$average_sales_title       = sprintf( __( '%s average net monthly sales', 'woocommerce' ), '<strong>' . wc_price( $data->average_sales ) . '</strong>' );
 			break;
 		}
 
@@ -350,21 +353,30 @@ class WC_Report_Sales_By_Date extends WC_Admin_Report {
 			'title'            => sprintf( __( '%s gross sales in this period', 'woocommerce' ), '<strong>' . wc_price( $data->total_sales ) . '</strong>' ),
 			'placeholder'      => __( 'This is the sum of the order totals after any refunds and including shipping and taxes.', 'woocommerce' ),
 			'color'            => $this->chart_colours['sales_amount'],
-			'highlight_series' => 5
-		);
-		$legend[] = array(
-			'title'            => sprintf( __( '%s net sales in this period', 'woocommerce' ), '<strong>' . wc_price( $data->net_sales ) . '</strong>' ),
-			'placeholder'      => __( 'This is the sum of the order totals after any refunds and excluding shipping and taxes.', 'woocommerce' ),
-			'color'            => $this->chart_colours['net_sales_amount'],
 			'highlight_series' => 6
 		);
-		if ( $data->average_sales > 0 ) {
+		if ( $data->average_total_sales > 0 ) {
 			$legend[] = array(
-				'title' => $average_sales_title,
+				'title' => $average_total_sales_title,
 				'color' => $this->chart_colours['average'],
 				'highlight_series' => 2
 			);
 		}
+
+		$legend[] = array(
+			'title'            => sprintf( __( '%s net sales in this period', 'woocommerce' ), '<strong>' . wc_price( $data->net_sales ) . '</strong>' ),
+			'placeholder'      => __( 'This is the sum of the order totals after any refunds and excluding shipping and taxes.', 'woocommerce' ),
+			'color'            => $this->chart_colours['net_sales_amount'],
+			'highlight_series' => 7
+		);
+		if ( $data->average_sales > 0 ) {
+			$legend[] = array(
+				'title' => $average_sales_title,
+				'color' => $this->chart_colours['net_average'],
+				'highlight_series' => 3
+			);
+		}
+
 		$legend[] = array(
 			'title' => sprintf( __( '%s orders placed', 'woocommerce' ), '<strong>' . $data->total_orders . '</strong>' ),
 			'color' => $this->chart_colours['order_count'],
@@ -379,17 +391,17 @@ class WC_Report_Sales_By_Date extends WC_Admin_Report {
 		$legend[] = array(
 			'title' => sprintf( _nx( '%s refunded %d order', '%s refunded %d orders', $this->report_data->total_refunded_orders, '%s = amount of the refunds, %d = number of refunded orders.', 'woocommerce' ), '<strong>' . wc_price( $data->total_refunds ) . '</strong>', $this->report_data->total_refunded_orders ) . ' (' . sprintf( _n( '%d item', '%d items', $this->report_data->refunded_order_items, 'woocommerce' ), $this->report_data->refunded_order_items ) . ')',
 			'color' => $this->chart_colours['refund_amount'],
-			'highlight_series' => 7
+			'highlight_series' => 8
 		);
 		$legend[] = array(
 			'title' => sprintf( __( '%s charged for shipping', 'woocommerce' ), '<strong>' . wc_price( $data->total_shipping ) . '</strong>' ),
 			'color' => $this->chart_colours['shipping_amount'],
-			'highlight_series' => 4
+			'highlight_series' => 5
 		);
 		$legend[] = array(
 			'title' => sprintf( __( '%s worth of coupons used', 'woocommerce' ), '<strong>' . wc_price( $data->total_coupons ) . '</strong>' ),
 			'color' => $this->chart_colours['coupon_amount'],
-			'highlight_series' => 3
+			'highlight_series' => 2
 		);
 
 		return $legend;
@@ -409,7 +421,8 @@ class WC_Report_Sales_By_Date extends WC_Admin_Report {
 		$this->chart_colours = array(
 			'sales_amount'     => '#b1d4ea',
 			'net_sales_amount' => '#3498db',
-			'average'          => '#95a5a6',
+			'average'          => '#b1d4ea',
+			'net_average'      => '#3498db',
 			'order_count'      => '#dbe1e3',
 			'item_count'       => '#ecf0f1',
 			'shipping_amount'  => '#5cc488',
@@ -525,10 +538,20 @@ class WC_Report_Sales_By_Date extends WC_Admin_Report {
 							hoverable: false
 						},
 						{
-							label: "<?php echo esc_js( __( 'Average sales amount', 'woocommerce' ) ) ?>",
-							data: [ [ <?php echo min( array_keys( $order_amounts ) ); ?>, <?php echo $this->report_data->average_sales; ?> ], [ <?php echo max( array_keys( $order_amounts ) ); ?>, <?php echo $this->report_data->average_sales; ?> ] ],
+							label: "<?php echo esc_js( __( 'Average gross sales amount', 'woocommerce' ) ) ?>",
+							data: [ [ <?php echo min( array_keys( $order_amounts ) ); ?>, <?php echo $this->report_data->average_total_sales; ?> ], [ <?php echo max( array_keys( $order_amounts ) ); ?>, <?php echo $this->report_data->average_total_sales; ?> ] ],
 							yaxis: 2,
 							color: '<?php echo $this->chart_colours['average']; ?>',
+							points: { show: false },
+							lines: { show: true, lineWidth: 2, fill: false },
+							shadowSize: 0,
+							hoverable: false
+						},
+						{
+							label: "<?php echo esc_js( __( 'Average net sales amount', 'woocommerce' ) ) ?>",
+							data: [ [ <?php echo min( array_keys( $order_amounts ) ); ?>, <?php echo $this->report_data->average_sales; ?> ], [ <?php echo max( array_keys( $order_amounts ) ); ?>, <?php echo $this->report_data->average_sales; ?> ] ],
+							yaxis: 2,
+							color: '<?php echo $this->chart_colours['net_average']; ?>',
 							points: { show: false },
 							lines: { show: true, lineWidth: 2, fill: false },
 							shadowSize: 0,
