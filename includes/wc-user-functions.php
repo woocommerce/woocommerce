1b370bb64a17b44a07db1bc98587776b55a5f8e2
Jeff Stieler <jeff.m.stieler@gmail.com>
2015-10-09 10:22:56 -0600

    Helper method to retrieve review verification status from comment meta, generating it on the fly for existing reviews.

--- includes/wc-user-functions.php
+++ includes/wc-user-functions.php
@@ -548,3 +548,19 @@ function wc_reset_order_customer_id_on_deleted_user( $user_id ) {
 }
 
 add_action( 'deleted_user', 'wc_reset_order_customer_id_on_deleted_user' );
+
+/**
+ * Get review verification status.
+ * @param  int $comment_id
+ * @return bool
+ */
+function wc_review_is_from_verified_owner( $comment_id ) {
+	$verified = get_comment_meta( $comment_id, 'verified', true );
+
+	// If no "verified" meta is present, generate it (if this is a product review).
+	if ( '' === $verified ) {
+		$verified = WC_Comments::add_comment_purchase_verification( $comment_id );
+	}
+
+	return (bool) $verified;
+}
