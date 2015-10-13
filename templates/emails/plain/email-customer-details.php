5f83acb7ff602e679592355ce513dc78b3661e0d
Lewis Cowles <lewiscowles@me.com>
2015-10-13 22:45:40 +0100

    Updated to reflect other WooCommerce files

--- templates/emails/plain/email-customer-details.php
+++ templates/emails/plain/email-customer-details.php
@@ -1,7 +1,23 @@
-<?php echo strtoupper( $heading ); ?> 
+<?php
+/**
+ * Admin new order email (plain text)
+ *
+ * @author		WooThemes
+ * @package 	WooCommerce/Templates/Emails/Plain
+ * @version 	2.3.13
+ */
 
-<?php foreach ( $fields as $field ) : ?>
-<?php if ( isset( $field['label'] ) && isset( $field['value'] ) && $field['value'] ) : ?>
-<?php echo $field['label']; ?>: <?php echo $field['value']; ?> 
-<?php endif; ?>
-<?php endforeach; ?>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+echo strtoupper( $heading ) . "\n\n";
+
+foreach ( $fields as $field ) {
+	
+	if ( isset( $field['label'] ) && isset( $field['value'] ) && $field['value'] ) {
+		
+		echo $field['label']. ' : ' . $field['value'] . "\n";
+		
+	}
+}
