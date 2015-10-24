298d47ba6db2da30014e6b1ca3b93a8adc7d1718
Travis <travis@travis-ci.org>
2015-10-24 16:19:42 +0530

    Fixes Variable Product Dropdown Issue in IE11 ref #9335

--- assets/js/frontend/add-to-cart-variation.js
+++ assets/js/frontend/add-to-cart-variation.js
@@ -148,12 +148,12 @@
 		} )
 
 		// Upon gaining focus
-		.on( 'focusin touchstart', '.variations select', function() {
+		/*.on( 'focusin touchstart', '.variations select', function() {
 			if ( ! $use_ajax ) {
 				$form.trigger( 'woocommerce_variation_select_focusin' );
 				$form.trigger( 'check_variations', [ $( this ).data( 'attribute_name' ) || $( this ).attr( 'name' ), true ] );
 			}
-		} )
+		} )*/
 
 		// Show single variation details (price, stock, image)
 		.on( 'found_variation', function( event, variation ) {
