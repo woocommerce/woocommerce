25783e3efe07d3f48bd92d106caa4ed6189932cd
nishitlangaliya <nishit.langaliya@multidots.in>
2015-10-26 19:50:09 +0530

    fixes: Dropdown Issue in IE11 ref #9335

--- assets/js/frontend/add-to-cart-variation.js
+++ assets/js/frontend/add-to-cart-variation.js
@@ -149,7 +149,7 @@
 
 		// Upon gaining focus
 		.on( 'focusin touchstart', '.variations select', function() {
-			if ( ! $use_ajax ) {
+			if ( $use_ajax ) {
 				$form.trigger( 'woocommerce_variation_select_focusin' );
 				$form.trigger( 'check_variations', [ $( this ).data( 'attribute_name' ) || $( this ).attr( 'name' ), true ] );
 			}
