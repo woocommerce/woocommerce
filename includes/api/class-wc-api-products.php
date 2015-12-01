b9b8a40b8a2ca49a7eaa281ad7b8daff7f74b93c
Claudio Sanches <contato@claudiosmweb.com>
2015-12-01 19:14:41 -0200

    [2.4] [API] Fixed backorders edit for variations
    closes #9618

--- includes/api/class-wc-api-products.php
+++ includes/api/class-wc-api-products.php
@@ -1342,17 +1342,17 @@ class WC_API_Products extends WC_API_Resource {
 			}
 
 			if ( 'yes' === $managing_stock ) {
+				$backorders = get_post_meta( $variation_id, '_backorders', true );
+
 				if ( isset( $variation['backorders'] ) ) {
 					if ( 'notify' == $variation['backorders'] ) {
 						$backorders = 'notify';
 					} else {
 						$backorders = ( true === $variation['backorders'] ) ? 'yes' : 'no';
 					}
-				} else {
-					$backorders = 'no';
 				}
 
-				update_post_meta( $variation_id, '_backorders', $backorders );
+				update_post_meta( $variation_id, '_backorders', '' === $backorders ? 'no' : $backorders );
 
 				if ( isset( $variation['stock_quantity'] ) ) {
 					wc_update_product_stock( $variation_id, wc_stock_amount( $variation['stock_quantity'] ) );
