88d7713a86f944220c12287a8c152401149fd947
Shiva Poudel <dev.shivapoudel@gmail.com>
2015-12-05 05:26:28 +0545

    Modified checkbox class value from widefat

--- includes/abstracts/abstract-wc-widget.php
+++ includes/abstracts/abstract-wc-widget.php
@@ -242,7 +242,7 @@ abstract class WC_Widget extends WP_Widget {
 				case 'checkbox' :
 					?>
 					<p>
-						<input class="widefat <?php echo esc_attr( $class ); ?>" id="<?php echo esc_attr( $this->get_field_id( $key ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( $key ) ); ?>" type="checkbox" value="1" <?php checked( $value, 1 ); ?> />
+						<input class="checkbox <?php echo esc_attr( $class ); ?>" id="<?php echo esc_attr( $this->get_field_id( $key ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( $key ) ); ?>" type="checkbox" value="1" <?php checked( $value, 1 ); ?> />
 						<label for="<?php echo $this->get_field_id( $key ); ?>"><?php echo $setting['label']; ?></label>
 					</p>
 					<?php
