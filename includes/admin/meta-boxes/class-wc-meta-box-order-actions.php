da2e0ac9500c1bf2f5e3f0e562dd33d5db21f7d3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-01-04 22:31:36 +0100

    Add missing phpdoc param tags to all WC_Meta_Box_* classes

--- includes/admin/meta-boxes/class-wc-meta-box-order-actions.php
+++ includes/admin/meta-boxes/class-wc-meta-box-order-actions.php
@@ -21,6 +21,8 @@ class WC_Meta_Box_Order_Actions {
 
 	/**
 	 * Output the metabox.
+	 *
+	 * @param WP_Post $post
 	 */
 	public static function output( $post ) {
 		global $theorder;
@@ -90,6 +92,9 @@ class WC_Meta_Box_Order_Actions {
 
 	/**
 	 * Save meta box data.
+	 *
+	 * @param int $post_id
+	 * @param WP_Post $post
 	 */
 	public static function save( $post_id, $post ) {
 
@@ -147,7 +152,7 @@ class WC_Meta_Box_Order_Actions {
 	/**
 	 * Set the correct message ID.
 	 *
-	 * @param $location
+	 * @param string $location
 	 *
 	 * @since  2.3.0
 	 *
