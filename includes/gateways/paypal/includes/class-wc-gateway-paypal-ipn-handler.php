918b84ee3a6072a6603ae8d21cb1117b24a3b6de
Mike Jolley <mike.jolley@me.com>
2016-01-06 12:38:22 +0000

    Strip tags from subject
    #9706

--- includes/gateways/paypal/includes/class-wc-gateway-paypal-ipn-handler.php
+++ includes/gateways/paypal/includes/class-wc-gateway-paypal-ipn-handler.php
@@ -301,6 +301,6 @@ class WC_Gateway_Paypal_IPN_Handler extends WC_Gateway_Paypal_Response {
 		$mailer             = WC()->mailer();
 		$message            = $mailer->wrap_message( $subject, $message );
 
-		$mailer->send( ! empty( $new_order_settings['recipient'] ) ? $new_order_settings['recipient'] : get_option( 'admin_email' ), $subject, $message );
+		$mailer->send( ! empty( $new_order_settings['recipient'] ) ? $new_order_settings['recipient'] : get_option( 'admin_email' ), strip_tags( $subject ), $message );
 	}
 }
