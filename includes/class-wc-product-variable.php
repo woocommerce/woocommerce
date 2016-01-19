13c8981a1393abcfe3897894c904385624d5dcb1
Elton Renda <elton.renda@gmail.com>
2016-01-19 16:24:24 +0200

    refactored check_stock_status filter

--- includes/class-wc-product-variable.php
+++ includes/class-wc-product-variable.php
@@ -67,10 +67,8 @@ class WC_Product_Variable extends WC_Product {
 		} elseif ( $this->backorders_allowed() || $this->get_stock_quantity() > get_option( 'woocommerce_notify_no_stock_amount' ) ) {
 			$set_child_stock_status = 'instock';
 		}
-		
-		$set_child_stock_status = apply_filters( 'woocommerce_product_check_stock_status', $set_child_stock_status, $this );
 
-		if ( $set_child_stock_status ) {
+		if ( $set_child_stock_status = apply_filters( 'woocommerce_product_check_stock_status', $set_child_stock_status, $this ) ) {
 			foreach ( $this->get_children() as $child_id ) {
 				if ( 'yes' !== get_post_meta( $child_id, '_manage_stock', true ) ) {
 					wc_update_product_stock_status( $child_id, $set_child_stock_status );
