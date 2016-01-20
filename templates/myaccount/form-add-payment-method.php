b5d7f1de1074585f657a7318b22b2b41ef8c47a3
Mike Jolley <mike.jolley@me.com>
2016-01-20 10:47:33 +0000

    [2.5] Only show payment method button when needed, and check for required fields
    Fixes #10112
    
    @thenbrent @claudiosmweb

--- templates/myaccount/form-add-payment-method.php
+++ templates/myaccount/form-add-payment-method.php
@@ -16,48 +16,42 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
 ?>
 <form id="add_payment_method" method="post">
 	<div id="payment">
-		<ul class="payment_methods methods">
-			<?php
-				if ( $available_gateways = WC()->payment_gateways->get_available_payment_gateways() ) {
-					// Chosen Method
-					if ( sizeof( $available_gateways ) )
-						current( $available_gateways )->set_current();
-
-					foreach ( $available_gateways as $gateway ) {
-						?>
-						<li class="payment_method_<?php echo $gateway->id; ?>">
-							<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> />
-							<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?></label>
-							<?php
-								if ( $gateway->has_fields() || $gateway->get_description() ) {
-									echo '<div class="payment_box payment_method_' . $gateway->id . '" style="display:none;">';
-									$gateway->payment_fields();
-									echo '</div>';
-								}
-							?>
-						</li>
-						<?php
-					}
-				} else {
-
-					echo '<p>' . __( 'Sorry, it seems that there are no payment methods which support adding a new payment method. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) . '</p>';
-
+		<?php if ( $available_gateways = WC()->payment_gateways->get_available_payment_gateways() ) : ?>
+			<ul class="payment_methods methods"><?php
+				// Chosen Method
+				if ( sizeof( $available_gateways ) ) {
+					current( $available_gateways )->set_current();
 				}
-			?>
-		</ul>
 
-		<div class="form-row">
-			<?php wp_nonce_field( 'woocommerce-add-payment-method' ); ?>
-			<input type="submit" class="button alt" id="place_order" value="<?php esc_attr_e( 'Add Payment Method', 'woocommerce' ); ?>" />
-			<input type="hidden" name="woocommerce_add_payment_method" value="1" />
-		</div>
-
+				foreach ( $available_gateways as $gateway ) {
+					?>
+					<li class="payment_method_<?php echo $gateway->id; ?>">
+						<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> />
+						<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?></label>
+						<?php
+							if ( $gateway->has_fields() || $gateway->get_description() ) {
+								echo '<div class="payment_box payment_method_' . $gateway->id . '" style="display:none;">';
+								$gateway->payment_fields();
+								echo '</div>';
+							}
+						?>
+					</li>
+					<?php
+				}
+			?></ul>
+			<div class="form-row">
+				<?php wp_nonce_field( 'woocommerce-add-payment-method' ); ?>
+				<input type="submit" class="button alt" id="place_order" value="<?php esc_attr_e( 'Add Payment Method', 'woocommerce' ); ?>" />
+				<input type="hidden" name="woocommerce_add_payment_method" value="1" />
+			</div>
+		<?php else : ?>
+			<p><?php _e( 'Sorry, it seems that there are no payment methods which support adding a new payment method. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ); ?></p>
+		<?php endif; ?>
 	</div>
-
 </form>
