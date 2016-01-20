65ac69cff741640a2c6326bdc4694925d16abec4
Mike Jolley <mike.jolley@me.com>
2016-01-20 16:58:55 +0000

    [2.5] Don't show downloads in admin email

--- includes/abstracts/abstract-wc-order.php
+++ includes/abstracts/abstract-wc-order.php
@@ -1948,10 +1948,11 @@ abstract class WC_Abstract_Order {
 		}
 
 		$defaults = array(
-			'show_sku'   => false,
-			'show_image' => false,
-			'image_size' => array( 32, 32 ),
-			'plain_text' => false
+			'show_sku'      => false,
+			'show_image'    => false,
+			'image_size'    => array( 32, 32 ),
+			'plain_text'    => false,
+			'sent_to_admin' => false
 		);
 
 		$args     = wp_parse_args( $args, $defaults );
@@ -1960,11 +1961,12 @@ abstract class WC_Abstract_Order {
 		wc_get_template( $template, array(
 			'order'               => $this,
 			'items'               => $this->get_items(),
-			'show_download_links' => $this->is_download_permitted(),
+			'show_download_links' => $this->is_download_permitted() && ! $args['sent_to_admin'],
 			'show_sku'            => $args['show_sku'],
 			'show_purchase_note'  => $this->is_paid(),
 			'show_image'          => $args['show_image'],
 			'image_size'          => $args['image_size'],
+			'sent_to_admin'       => $args['sent_to_admin']
 		) );
 
 		return apply_filters( 'woocommerce_email_order_items_table', ob_get_clean(), $this );
