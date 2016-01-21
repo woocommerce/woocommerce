2790307489e7a499bc498f86384ddd4ef873b8c1
Claudio Sanches <contato@claudiosmweb.com>
2016-01-21 19:33:49 -0200

    Changed allbuttons to select_buttons for multiselect #10079

--- includes/abstracts/abstract-wc-settings-api.php
+++ includes/abstracts/abstract-wc-settings-api.php
@@ -666,7 +666,7 @@ abstract class WC_Settings_API {
 			'desc_tip'          => false,
 			'description'       => '',
 			'custom_attributes' => array(),
-			'allbuttons'        => false,
+			'select_buttons'    => false,
 			'options'           => array()
 		);
 
@@ -689,7 +689,7 @@ abstract class WC_Settings_API {
 						<?php endforeach; ?>
 					</select>
 					<?php echo $this->get_description_html( $data ); ?>
-					<?php if ( $data['allbuttons'] ) : ?>
+					<?php if ( $data['select_buttons'] ) : ?>
 						<br/><a class="select_all button" href="#"><?php _e( 'Select all', 'woocommerce' ); ?></a> <a class="select_none button" href="#"><?php _e( 'Select none', 'woocommerce' ); ?></a>
 					<?php endif; ?>
 				</fieldset>
