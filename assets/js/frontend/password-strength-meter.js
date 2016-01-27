a2843c03c7fdded94bd2b48e0155d4857fdf0d27
Mike Jolley <mike.jolley@me.com>
2016-01-27 10:53:59 +0000

    [2.5] Added password hint text and error messages

--- assets/js/frontend/password-strength-meter.js
+++ assets/js/frontend/password-strength-meter.js
@@ -1,4 +1,4 @@
-/* global wp, pwsL10n */
+/* global wp, pwsL10n, wc_password_strength_meter_params */
 jQuery( function( $ ) {
 
 	/**
@@ -11,8 +11,8 @@ jQuery( function( $ ) {
 		 */
 		init: function() {
 			$( document.body )
-				.on( 'keyup', 'form.register #reg_password, form.checkout #account_password, form.edit-account #password_1, form.lost_reset_password #password_1', this.strengthMeter );
-
+				.on( 'keyup', 'form.register #reg_password, form.checkout #account_password, form.edit-account #password_1, form.lost_reset_password #password_1', this.strengthMeter )
+				.on( 'submit', 'form.register, form.edit-account, form.lost_reset_password', this.onSubmit );
 			$( 'form.checkout #createaccount' ).change();
 		},
 
@@ -29,11 +29,29 @@ jQuery( function( $ ) {
 
 			strength = wc_password_strength_meter.checkPasswordStrength( field );
 
+			// Add class to wrapper
+			if ( 3 === strength || 4 === strength ) {
+				wrapper.removeClass( 'has-weak-password' );
+			} else {
+				wrapper.addClass( 'has-weak-password' );
+			}
+
 			// Stop form if password is weak... But not in checkout form!
 			if ( 3 === strength || 4 === strength ) {
-				submit.removeAttr( 'disabled' );
+				submit.removeClass( 'disabled' );
 			} else if ( ! wrapper.hasClass( 'checkout' ) ) {
-				submit.attr( 'disabled', 'disabled' );
+				submit.addClass( 'disabled' );
+			}
+		},
+
+		onSubmit: function() {
+			$( '.woocommerce-password-error' ).remove();
+
+			if ( $( this ).is( '.has-weak-password' ) ) {
+				$( this ).prepend( '<div class="woocommerce-error woocommerce-password-error">' + wc_password_strength_meter_params.i18n_password_error + '</div>' );
+				return false;
+			} else {
+				return true;
 			}
 		},
 
@@ -61,15 +79,19 @@ jQuery( function( $ ) {
 		 * @return {Int}
 		 */
 		checkPasswordStrength: function( field ) {
-			var meter = $( '.woocommerce-password-strength' );
-			var strength = wp.passwordStrength.meter( field.val(), wp.passwordStrength.userInputBlacklist() );
+			var meter     = $( '.woocommerce-password-strength' );
+			var hint      = $( '.woocommerce-password-hint' );
+			var hint_html = '<small class="woocommerce-password-hint">' + wc_password_strength_meter_params.i18n_password_hint + '</small>';
+			var strength  = wp.passwordStrength.meter( field.val(), wp.passwordStrength.userInputBlacklist() );
 
-			// Reset classes
+			// Reset
 			meter.removeClass( 'short bad good strong' );
+			hint.remove();
 
 			switch ( strength ) {
 				case 2 :
 					meter.addClass( 'bad' ).html( pwsL10n.bad );
+					meter.after( hint_html );
 					break;
 				case 3 :
 					meter.addClass( 'good' ).html( pwsL10n.good );
@@ -80,9 +102,9 @@ jQuery( function( $ ) {
 				case 5 :
 					meter.addClass( 'short' ).html( pwsL10n.mismatch );
 					break;
-
 				default :
 					meter.addClass( 'short' ).html( pwsL10n['short'] );
+					meter.after( hint_html );
 			}
 
 			return strength;
