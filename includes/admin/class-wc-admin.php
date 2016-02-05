3cef28efe8c1d1cf1a0f587605abd56219acf365
Mike Jolley <mike.jolley@me.com>
2016-02-05 12:16:41 +0000

    [2.5] Error handling for screen ids
    Fixes #10280

--- includes/admin/class-wc-admin.php
+++ includes/admin/class-wc-admin.php
@@ -70,7 +70,9 @@ class WC_Admin {
 	 * Include admin files conditionally.
 	 */
 	public function conditional_includes() {
-		$screen = get_current_screen();
+		if ( ! $screen = get_current_screen() ) {
+			return;
+		}
 
 		switch ( $screen->id ) {
 			case 'dashboard' :
