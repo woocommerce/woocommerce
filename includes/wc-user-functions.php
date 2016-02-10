1b2195b8190cc67ea8987de3b79b97175f85d3cd
Justin Shreve <justin.shreve@gmail.com>
2016-02-10 03:56:13 -0800

    Track a users last updated time (profile/user attributes) and display it in the WC Customer API.

--- includes/wc-user-functions.php
+++ includes/wc-user-functions.php
@@ -581,3 +581,44 @@ function wc_disable_author_archives_for_customers() {
 }
 
 add_action( 'template_redirect', 'wc_disable_author_archives_for_customers' );
+
+/**
+ * Hooks into the `profile_update` hook to set the user last updated timestamp
+ *
+ * @since 2.6
+ * @param  int   $user_id    The user that was updated
+ * @param  array $old        The profile fields pre-change
+ */
+function wc_update_profile_last_update_time( $user_id, $old ) {
+	wc_set_user_last_update_time( $user_id );
+}
+
+add_action( 'profile_update', 'wc_update_profile_last_update_time', 10, 2 );
+
+/**
+ * Hooks into the update user meta function to set the user last updated timestamp
+ *
+ * @since  2.6
+ * @param  int    $meta_id     ID of the meta object that was changed
+ * @param  int    $user_id     The user that was updated
+ * @param  string $meta_key    Name of the meta key that was changed
+ * @param  string $_meta_value Value of the meta that was changed
+ */
+function wc_meta_update_last_update_time( $meta_id, $user_id, $meta_key, $_meta_value ) {
+	if ( 'last_update' === $meta_key ) {
+		return;
+	}
+	wc_set_user_last_update_time( $user_id );
+}
+
+add_action( "update_user_meta", 'wc_meta_update_last_update_time', 10, 4 );
+
+/**
+ * Sets a user's "last update" time to the current timestamp
+ *
+ * @since  2.6
+ * @param  int $user_id The user to set a timestamp for
+ */
+function wc_set_user_last_update_time( $user_id ) {
+	update_user_meta( $user_id, 'last_update', time() );
+}
