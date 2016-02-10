69a97d1af9c8b526803072f9a112f100c8169046
Diego Zanella <businessdad75@gmail.com>
2016-02-10 21:22:44 +0000

    Postponed call to woocommerce_shipping_methods filter
    * The filter should be called after adding all shipping methods, to provide a complete set of data to consumers that hook into it.

--- includes/class-wc-shipping.php
+++ includes/class-wc-shipping.php
@@ -102,11 +102,11 @@ class WC_Shipping {
 	 */
 	public function get_shipping_method_class_names() {
 		// Unique Method ID => Method Class name
-		$shipping_methods = apply_filters( 'woocommerce_shipping_methods', array(
+		$shipping_methods = array(
 			'flat_rate'     => 'WC_Shipping_Flat_Rate',
 			'free_shipping' => 'WC_Shipping_Free_Shipping',
 			'local_pickup'  => 'WC_Shipping_Local_Pickup',
-		) );
+		);
 
 		// For backwards compatibility with 2.5.x we load any ENABLED legacy shipping methods here
 		$maybe_load_legacy_methods = array( 'flat_rate', 'free_shipping', 'international_delivery', 'local_delivery', 'local_pickup' );
@@ -117,6 +117,8 @@ class WC_Shipping {
 				$shipping_methods[ 'legacy_' . $method ] = 'WC_Shipping_Legacy_' . $method;
 			}
 		}
+
+		$shipping_methods = apply_filters( 'woocommerce_shipping_methods', $shipping_methods );
 
 		return $shipping_methods;
 	}
