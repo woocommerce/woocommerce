860819d39f22c524a87e8cdced6bb478ca167a5c
Claudio Sanches <contato@claudiosmweb.com>
2016-03-02 20:22:29 -0300

    List and get product categories

--- includes/api/wc-rest-product-categories-controller.php
+++ includes/api/wc-rest-product-categories-controller.php
@@ -30,16 +30,127 @@ class WC_REST_Product_Categories_Controller extends WC_REST_Terms_Controller {
 	protected $rest_base = 'products/categories';
 
 	/**
-	 * Type of object.
+	 * Taxonomy.
 	 *
 	 * @var string
 	 */
-	protected $object = 'product_cat';
+	protected $taxonomy = 'product_cat';
 
 	/**
-	 * Register the routes for product categories.
+	 * Prepare a single product category output for response.
+	 *
+	 * @param obj $item Term object.
+	 * @param WP_REST_Request $request
+	 * @return WP_REST_Response $response
 	 */
-	public function register_routes() {
+	public function prepare_item_for_response( $item, $request ) {
+		// Get category display type.
+		$display_type = get_woocommerce_term_meta( $item->term_id, 'display_type' );
 
+		// Get category image.
+		$image = '';
+		if ( $image_id = get_woocommerce_term_meta( $item->term_id, 'thumbnail_id' ) ) {
+			$image = wp_get_attachment_url( $image_id );
+		}
+
+		$data = array(
+			'id'          => (int) $item->term_id,
+			'name'        => $item->name,
+			'slug'        => $item->slug,
+			'parent'      => (int) $item->parent,
+			'description' => $item->description,
+			'display'     => $display_type ? $display_type : 'default',
+			'image'       => $image ? esc_url( $image ) : '',
+			'count'       => (int) $item->count,
+		);
+
+		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data = $this->add_additional_fields_to_object( $data, $request );
+		$data = $this->filter_response_by_context( $data, $context );
+
+		$response = rest_ensure_response( $data );
+
+		$response->add_links( $this->prepare_links( $item ) );
+
+		/**
+		 * Filter a term item returned from the API.
+		 *
+		 * Allows modification of the term data right before it is returned.
+		 *
+		 * @param WP_REST_Response  $response  The response object.
+		 * @param object            $item      The original term object.
+		 * @param WP_REST_Request   $request   Request used to generate the response.
+		 */
+		return apply_filters( "woocommerce_rest_prepare_{$this->taxonomy}", $response, $item, $request );
+	}
+
+	/**
+	 * Get the Term's schema, conforming to JSON Schema.
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		$schema = array(
+			'$schema'              => 'http://json-schema.org/draft-04/schema#',
+			'title'                => $this->taxonomy,
+			'type'                 => 'object',
+			'properties'           => array(
+				'id' => array(
+					'description' => __( 'Unique identifier for the resource.', 'woocommerce' ),
+					'type'        => 'integer',
+					'context'     => array( 'view', 'edit' ),
+					'readonly'    => true,
+				),
+				'name' => array(
+					'description' => __( 'Category name.', 'woocommerce' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit' ),
+					'arg_options' => array(
+						'sanitize_callback' => 'sanitize_text_field',
+					),
+				),
+				'slug' => array(
+					'description' => __( 'An alphanumeric identifier for the resource unique to its type.', 'woocommerce' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit' ),
+					'arg_options' => array(
+						'sanitize_callback' => 'sanitize_title',
+					),
+				),
+				'parent' => array(
+					'description' => __( 'The id for the parent of the resource.', 'woocommerce' ),
+					'type'        => 'integer',
+					'context'     => array( 'view', 'edit' ),
+				),
+				'description' => array(
+					'description' => __( 'HTML description of the resource.', 'woocommerce' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit' ),
+					'arg_options' => array(
+						'sanitize_callback' => 'wp_filter_post_kses',
+					),
+				),
+				'display' => array(
+					'description' => __( 'Category archive display type.', 'woocommerce' ),
+					'type'        => 'string',
+					'enum'        => array( 'default', 'products', 'subcategories', 'both' ),
+					'context'     => array( 'view', 'edit' ),
+				),
+				'image' => array(
+					'description' => __( 'Image URL.', 'woocommerce' ),
+					'type'        => 'string',
+					'format'      => 'uri',
+					'context'     => array( 'view', 'edit' ),
+				),
+				'count' => array(
+					'description' => __( 'Number of published products for the resource.', 'woocommerce' ),
+					'type'        => 'integer',
+					'context'     => array( 'view', 'edit', 'woocommerce' ),
+					'readonly'    => true,
+				),
+			),
+		);
+
+		return $this->add_additional_fields_schema( $schema );
 	}
 }
