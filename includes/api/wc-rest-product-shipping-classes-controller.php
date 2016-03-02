cbdddd99ccbf2c9b4aafd85605bbbd0e2073e8ae
Claudio Sanches <contato@claudiosmweb.com>
2016-03-02 20:30:54 -0300

    List and get product shipping classes

--- includes/api/wc-rest-product-shipping-classes-controller.php
+++ includes/api/wc-rest-product-shipping-classes-controller.php
@@ -30,16 +30,104 @@ class WC_REST_Product_Shipping_Classes_Controller extends WC_REST_Terms_Controll
 	protected $rest_base = 'products/shipping_classes';
 
 	/**
-	 * Type of object.
+	 * Taxonomy.
 	 *
 	 * @var string
 	 */
-	protected $object = 'product_shipping_class';
+	protected $taxonomy = 'product_shipping_class';
 
 	/**
-	 * Register the routes for product shipping classes.
+	 * Prepare a single product shipping class output for response.
+	 *
+	 * @param obj $item Term object.
+	 * @param WP_REST_Request $request
+	 * @return WP_REST_Response $response
 	 */
-	public function register_routes() {
+	public function prepare_item_for_response( $item, $request ) {
+		$data = array(
+			'id'          => (int) $item->term_id,
+			'name'        => $item->name,
+			'slug'        => $item->slug,
+			'parent'      => (int) $item->parent,
+			'description' => $item->description,
+			'count'       => (int) $item->count,
+		);
 
+		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data    = $this->add_additional_fields_to_object( $data, $request );
+		$data    = $this->filter_response_by_context( $data, $context );
+
+		$response = rest_ensure_response( $data );
+
+		$response->add_links( $this->prepare_links( $item ) );
+
+		/**
+		 * Filter a term item returned from the API.
+		 *
+		 * Allows modification of the term data right before it is returned.
+		 *
+		 * @param WP_REST_Response  $response  The response object.
+		 * @param object            $item      The original term object.
+		 * @param WP_REST_Request   $request   Request used to generate the response.
+		 */
+		return apply_filters( "woocommerce_rest_prepare_{$this->taxonomy}", $response, $item, $request );
+	}
+
+	/**
+	 * Get the Term's schema, conforming to JSON Schema.
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		$schema = array(
+			'$schema'              => 'http://json-schema.org/draft-04/schema#',
+			'title'                => $this->taxonomy,
+			'type'                 => 'object',
+			'properties'           => array(
+				'id' => array(
+					'description' => __( 'Unique identifier for the resource.', 'woocommerce' ),
+					'type'        => 'integer',
+					'context'     => array( 'view', 'edit' ),
+					'readonly'    => true,
+				),
+				'name' => array(
+					'description' => __( 'Shipping class name.', 'woocommerce' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit' ),
+					'arg_options' => array(
+						'sanitize_callback' => 'sanitize_text_field',
+					),
+				),
+				'slug' => array(
+					'description' => __( 'An alphanumeric identifier for the resource unique to its type.', 'woocommerce' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit' ),
+					'arg_options' => array(
+						'sanitize_callback' => 'sanitize_title',
+					),
+				),
+				'parent' => array(
+					'description' => __( 'The id for the parent of the resource.', 'woocommerce' ),
+					'type'        => 'integer',
+					'context'     => array( 'view', 'edit' ),
+				),
+				'description' => array(
+					'description' => __( 'HTML description of the resource.', 'woocommerce' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit' ),
+					'arg_options' => array(
+						'sanitize_callback' => 'wp_filter_post_kses',
+					),
+				),
+				'count' => array(
+					'description' => __( 'Number of published products for the resource.', 'woocommerce' ),
+					'type'        => 'integer',
+					'context'     => array( 'view', 'edit', 'woocommerce' ),
+					'readonly'    => true,
+				),
+			),
+		);
+
+		return $this->add_additional_fields_schema( $schema );
 	}
 }
