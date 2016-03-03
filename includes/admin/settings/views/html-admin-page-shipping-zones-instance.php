1dbe6c21a0b616a9f5ee08e477df6bc9bb97ab87
Allen Snook <allen@allendav.com>
2016-03-03 09:44:09 -0800

    Add hide_save_button to shipping zones instance template (Like what is done for legacy shipping methods in the html admin settings view)

--- includes/admin/settings/views/html-admin-page-shipping-zones-instance.php
+++ includes/admin/settings/views/html-admin-page-shipping-zones-instance.php
@@ -6,5 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <h2><?php echo esc_html( $shipping_method->get_method_title() ); ?> <?php wc_back_link( __( 'Return to Shipping Methods', 'woocommerce' ), admin_url( 'admin.php?page=wc-settings&tab=shipping&zone_id=' . absint( $zone->get_zone_id() ) ) ); ?></h2>
 <?php $shipping_method->admin_options(); ?>
-<p class="submit"><input type="submit" class="button button-primary" name="save_method" value="<?php _e( 'Save changes', 'woocommerce' ); ?>" /></p>
+<?php if ( ! isset( $GLOBALS['hide_save_button'] ) ) : ?>
+	<p class="submit"><input type="submit" class="button button-primary" name="save_method" value="<?php _e( 'Save changes', 'woocommerce' ); ?>" /></p>
+<?php endif; ?>
 <?php wp_nonce_field( 'woocommerce_save_method', 'woocommerce_save_method_nonce' ); ?>
