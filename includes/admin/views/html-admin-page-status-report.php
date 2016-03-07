9eee0ff93f00720022bc75265d2fa87161366d07
Mike Jolley <mike.jolley@me.com>
2016-03-07 13:16:39 +0000

    Remove log dir from debug
    @mattyza

--- includes/admin/views/html-admin-page-status-report.php
+++ includes/admin/views/html-admin-page-status-report.php
@@ -45,7 +45,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<td class="help"><?php echo wc_help_tip( __( 'Several WooCommerce extensions can write logs which makes debugging problems easier. The directory must be writable for this to happen.', 'woocommerce' ) ); ?></td>
 			<td><?php
 				if ( @fopen( WC_LOG_DIR . 'test-log.log', 'a' ) ) {
-					echo '<mark class="yes">&#10004; <code>' . WC_LOG_DIR . '</code></mark> ';
+					echo '<mark class="yes">&#10004; <code class="private">' . WC_LOG_DIR . '</code></mark> ';
 				} else {
 					printf( '<mark class="error">&#10005; ' . __( 'To allow logging, make <code>%s</code> writable or define a custom <code>WC_LOG_DIR</code>.', 'woocommerce' ) . '</mark>', WC_LOG_DIR );
 				}
@@ -750,11 +750,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 					var the_name    = jQuery.trim( label ).replace( /(<([^>]+)>)/ig, '' ); // Remove HTML.
 					var image       = jQuery( this ).find( 'td:eq(2)' ).find( 'img' ); // Get WP 4.2 emojis.
 					var prefix      = ( undefined === image.attr( 'alt' ) ) ? '' : image.attr( 'alt' ) + ' '; // Remove WP 4.2 emojis.
-					var the_value   = jQuery.trim( prefix + jQuery( this ).find( 'td:eq(2)' ).text() );
+
+					// Find value
+					var $value_html   = jQuery( this ).find( 'td:eq(2)' );
+					$value_html.find( '.private' ).remove();
+
+					// Format value
+					var the_value   = jQuery.trim( prefix + $value_html.text() );
 					var value_array = the_value.split( ', ' );
 
 					if ( value_array.length > 1 ) {
-
 						// If value have a list of plugins ','.
 						// Split to add new line.
 						var temp_line ='';
