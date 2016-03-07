723b4109d2e6330dcf6437ff9403fb378fcc4763
Mike Jolley <mike.jolley@me.com>
2016-03-07 10:31:01 +0000

    Add notice on empty cart when checking out

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -28,6 +28,7 @@ function wc_template_redirect() {
 
 	// When on the checkout with an empty cart, redirect to cart page
 	elseif ( is_page( wc_get_page_id( 'checkout' ) ) && WC()->cart->is_empty() && empty( $wp->query_vars['order-pay'] ) && ! isset( $wp->query_vars['order-received'] ) ) {
+		wc_add_notice( __( 'Checkout is not available whilst your cart is empty.', 'woocommerce' ) );
 		wp_redirect( wc_get_page_permalink( 'cart' ) );
 		exit;
 	}
