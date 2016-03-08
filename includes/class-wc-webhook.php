b6376b86c501bb9bddbcbc47f60874cbfefca692
Claudio Sanches <contato@claudiosmweb.com>
2016-03-08 15:54:13 -0300

    Trigger webhook when user edit addresses on frontend, closes #10505

--- includes/class-wc-webhook.php
+++ includes/class-wc-webhook.php
@@ -553,6 +553,7 @@ class WC_Webhook {
 			'customer.updated' => array(
 				'profile_update',
 				'woocommerce_api_edit_customer',
+				'woocommerce_customer_save_address',
 			),
 			'customer.deleted' => array(
 				'delete_user',
