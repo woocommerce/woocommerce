2d2c64d2abdc59e783d755cb2e3b6cd2c823f588
Mike Jolley <mike.jolley@me.com>
2016-03-10 12:52:20 +0000

    Change heading and tab markup
    Closes #10515

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -9,9 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="wrap woocommerce">
 	<form method="<?php echo esc_attr( apply_filters( 'woocommerce_settings_form_method_tab_' . $current_tab, 'post' ) ); ?>" id="mainform" action="" enctype="multipart/form-data">
-		<div class="icon32 icon32-woocommerce-settings" id="icon-woocommerce"><br /></div>
-		<h1 class="screen-reader-text"><?php echo esc_html( $tabs[ $current_tab ] ); ?></h1>
-		<h2 class="nav-tab-wrapper woo-nav-tab-wrapper">
+		<nav class="nav-tab-wrapper woo-nav-tab-wrapper">
 			<?php
 				foreach ( $tabs as $name => $label ) {
 					echo '<a href="' . admin_url( 'admin.php?page=wc-settings&tab=' . $name ) . '" class="nav-tab ' . ( $current_tab == $name ? 'nav-tab-active' : '' ) . '">' . $label . '</a>';
@@ -19,7 +17,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 				do_action( 'woocommerce_settings_tabs' );
 			?>
-		</h2>
+		</nav>
+		<h1 class="screen-reader-text"><?php echo esc_html( $tabs[ $current_tab ] ); ?></h1>
 		<?php
 			do_action( 'woocommerce_sections_' . $current_tab );
 
