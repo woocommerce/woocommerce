e664f0f82b4d1eefbc5a005e5e2ea8884610cd46
Mike Jolley <mike.jolley@me.com>
2016-03-15 12:18:50 +0000

    Log PDT data correctly, including payer details
    Closes #10546

--- includes/gateways/paypal/includes/class-wc-gateway-paypal-pdt-handler.php
+++ includes/gateways/paypal/includes/class-wc-gateway-paypal-pdt-handler.php
@@ -30,7 +30,7 @@ class WC_Gateway_Paypal_PDT_Handler extends WC_Gateway_Paypal_Response {
 	/**
 	 * Validate a PDT transaction to ensure its authentic.
 	 * @param  string $transaction
-	 * @return bool
+	 * @return bool|array False or result array
 	 */
 	protected function validate_transaction( $transaction ) {
 		$pdt = array(
@@ -51,7 +51,16 @@ class WC_Gateway_Paypal_PDT_Handler extends WC_Gateway_Paypal_Response {
 			return false;
 		}
 
-		return true;
+		// Parse transaction result data
+		$transaction_result  = explode( "\n", $response['body'] );
+		$transaction_results = array();
+
+		foreach ( $transaction_result as $line ) {
+			 $line                                        = explode( "=", $line );
+			 $transaction_results[ wc_clean( urldecode( $line[0] ) ) ] = wc_clean( urldecode( isset( $line[1] ) ? $line[1] : '' ) );
+		}
+
+		return $transaction_results;
 	}
 
 	/**
@@ -71,16 +80,30 @@ class WC_Gateway_Paypal_PDT_Handler extends WC_Gateway_Paypal_Response {
 			return false;
 		}
 
-		if ( $this->validate_transaction( $transaction ) && 'completed' === $status ) {
+		$transaction_result = $this->validate_transaction( $transaction );
+
+		if ( $transaction_result && 'completed' === $status ) {
 			if ( $order->get_total() != $amount ) {
 				WC_Gateway_Paypal::log( 'Payment error: Amounts do not match (amt ' . $amount . ')' );
 				$this->payment_on_hold( $order, sprintf( __( 'Validation error: PayPal amounts do not match (amt %s).', 'woocommerce' ), $amount ) );
 			} else {
 				$this->payment_complete( $order, $transaction,  __( 'PDT payment completed', 'woocommerce' ) );
 
-				if ( ! empty( $_REQUEST['mc_fee'] ) ) {
-					// Log paypal transaction fee.
-					update_post_meta( $order->id, 'PayPal Transaction Fee', wc_clean( $_REQUEST['mc_fee'] ) );
+				// Log paypal transaction fee and other meta data.
+				if ( ! empty( $transaction_result['mc_fee'] ) ) {
+					update_post_meta( $order->id, 'PayPal Transaction Fee', $transaction_result['mc_fee'] );
+				}
+				if ( ! empty( $transaction_result['payer_email'] ) ) {
+					update_post_meta( $order->id, 'Payer PayPal address', $transaction_result['payer_email'] );
+				}
+				if ( ! empty( $transaction_result['first_name'] ) ) {
+					update_post_meta( $order->id, 'Payer first name', $transaction_result['first_name'] );
+				}
+				if ( ! empty( $transaction_result['last_name'] ) ) {
+					update_post_meta( $order->id, 'Payer last name', $transaction_result['last_name'] );
+				}
+				if ( ! empty( $transaction_result['payment_type'] ) ) {
+					update_post_meta( $order->id, 'Payment type', $transaction_result['payment_type'] );
 				}
 			}
 		}
