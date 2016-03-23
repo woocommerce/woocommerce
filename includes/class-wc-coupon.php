534cd2a38445afc577e0b129e6a5a638f37bc9ab
Mike Jolley <mike.jolley@me.com>
2016-03-23 11:27:36 +0000

    Use discount rounding mode when getting discounted price.
    #10573 @claudiosmweb

--- includes/class-wc-coupon.php
+++ includes/class-wc-coupon.php
@@ -746,7 +746,7 @@ class WC_Coupon {
 			}
 		}
 
-		$discount = round( $discount, WC_ROUNDING_PRECISION );
+		$discount = wc_cart_round_discount( $discount, wc_get_price_decimals() );
 
 		return apply_filters( 'woocommerce_coupon_get_discount_amount', $discount, $discounting_amount, $cart_item, $single, $this );
 	}
