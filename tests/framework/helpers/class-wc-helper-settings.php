fa5425109e612dbbf0f0e2c27dd3bc48756b4979
Justin Shreve <justin.shreve@gmail.com>
2016-03-23 12:36:59 -0700

    Refactor down to two concepts - settings groups and settings. Fix up tests, and start working on returning actual settings and their values.

--- tests/framework/helpers/class-wc-helper-settings.php
+++ tests/framework/helpers/class-wc-helper-settings.php
@@ -12,60 +12,74 @@ class WC_Helper_Settings {
 	 * @since 2.7.0
 	 */
 	public static function register() {
-		add_filter( 'woocommerce_settings_locations', array( 'WC_Helper_Settings', 'register_locations' ) );
-		add_filter( 'woocommerce_settings_groups_test', array( 'WC_Helper_Settings', 'register_test_groups' ) );
+		add_filter( 'woocommerce_settings_groups', array( 'WC_Helper_Settings', 'register_groups' ) );
+		add_filter( 'woocommerce_settings_test', array( 'WC_Helper_Settings', 'register_test_settings' ) );
 	}
 
 	/**
-	 * Registers some example locations, including invalid ones that should not show up in JSON responses.
+	 * Registers some example setting groups, including invalid ones that should not show up in JSON responses.
 	 * @since  2.7.0
-	 * @param  array $locations
+	 * @param  array $groups
 	 * @return array
 	 */
-	public static function register_locations( $locations ) {
-		$locations[] = array(
+	public static function register_groups( $groups ) {
+		$groups[] = array(
 			'id'          => 'test',
 			'type'        => 'page',
 			'bad'         => 'value',
 			'label'       => __( 'Test Extension', 'woocommerce' ),
 			'description' => __( 'My awesome test settings.', 'woocommerce' ),
 		);
-		$locations[] = array(
+		$groups[] = array(
 			'id'          => 'test-2',
 			'type'        => 'page',
 			'label'       => __( 'Test Extension', 'woocommerce' ),
 			'description' => '',
 		);
-		$locations[] = array(
+		$groups[] = array(
 			'id'    => 'coupon-data',
 			'type'  => 'metabox',
 			'label' => __( 'Coupon Data', 'woocommerce' ),
 		);
-		$locations[] = array(
-			'label' => __( 'Invalid', 'woocommerce' ),
-		);
-		$locations[] = array(
+		$groups[] = array(
 			'id' => 'invalid',
 		);
-		return $locations;
+		return $groups;
 	}
 
 	/**
-	 * Registers some example groups for the 'test' page.
+	 * Registers some example settings.
 	 * @since  2.7.0
-	 * @param  array $groups
+	 * @param  array $settings
 	 * @return array
 	 */
-	public static function register_test_groups( $groups ) {
-		$groups[] = array(
-			'id'    => 'general',
-			'label' => __( 'General', 'woocommerce' ),
+	public static function register_settings( $settings ) {
+		$settings[] = array(
+			'id' 	=> 'catalog_options',
+			'label' => __( 'Shop & Product Pages', 'woocommerce' ),
+			'type' 	=> 'title',
 		);
-		$groups[] = array(
-			'id'    => 'inventory',
-			'label' => __( 'Inventory', 'woocommerce' ),
+		$settings[] = array(
+			'id'          => 'woocommerce_shop_page_display',
+			'label'       => __( 'Shop Page Display', 'woocommerce' ),
+			'description' => __( 'This controls what is shown on the product archive.', 'woocommerce' ),
+			'default'     => '',
+			'type'        => 'select',
+			'options'     => array(
+				''              => __( 'Show products', 'woocommerce' ),
+				'subcategories' => __( 'Show categories &amp; subcategories', 'woocommerce' ),
+				'both'          => __( 'Show both', 'woocommerce' ),
+			),
 		);
-		return $groups;
+		$settings[] = array(
+			'id'            => 'woocommerce_enable_lightbox',
+			'label'         => __( 'Product Image Gallery', 'woocommerce' ),
+			'description'   => __( 'Enable Lightbox for product images', 'woocommerce' ),
+			'default'       => 'yes',
+			'tip'           => __( 'Product gallery images will open in a lightbox.', 'woocommerce' ),
+			'type'          => 'checkbox',
+		);
+		return $settings;
 	}
 
 }
