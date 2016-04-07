f5e9ea487597cf4cfd42061c338b78d9bcce124a
Qadriinfotech <shaikhaezaz80@gmail.com>
2016-04-07 13:06:06 +0530

    Update cart-empty.php
    Added filter to change "Return to Shop" button title on empty cart page.

--- templates/cart/cart-empty.php
+++ templates/cart/cart-empty.php
@@ -32,6 +32,6 @@ wc_print_notices();
 
 <p class="return-to-shop">
 	<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-		<?php _e( 'Return To Shop', 'woocommerce' ) ?>
+		<?php echo apply_filters('woocommerce_return_to_shop_title', sprintf( '%s', __( 'Return To Shop', 'woocommerce' ) )); ?>
 	</a>
 </p>
