157c50df0ad1400e7a2c9cdaef244d009b26c0d7
toddlahman <toddlahman@gmail.com>
2016-04-12 23:46:30 -0700

    return boolean log add and clear method result

--- includes/class-wc-logger.php
+++ includes/class-wc-logger.php
@@ -7,11 +7,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Allows log files to be written to for debugging purposes
  *
- * @class 		WC_Logger
- * @version		1.6.4
- * @package		WooCommerce/Classes
- * @category	Class
- * @author 		WooThemes
+ * @class          WC_Logger
+ * @version        1.6.4
+ * @package        WooCommerce/Classes
+ * @category       Class
+ * @author         WooThemes
  */
 class WC_Logger {
 
@@ -30,7 +30,6 @@ class WC_Logger {
 		$this->_handles = array();
 	}
 
-
 	/**
 	 * Destructor.
 	 */
@@ -40,12 +39,13 @@ class WC_Logger {
 		}
 	}
 
-
 	/**
 	 * Open log file for writing.
 	 *
 	 * @access private
+	 *
 	 * @param mixed $handle
+	 *
 	 * @return bool success
 	 */
 	private function open( $handle ) {
@@ -60,34 +60,44 @@ class WC_Logger {
 		return false;
 	}
 
-
 	/**
 	 * Add a log entry to chosen file.
 	 *
 	 * @param string $handle
 	 * @param string $message
+	 *
+	 * @return bool
 	 */
 	public function add( $handle, $message ) {
+		$result = false;
+
 		if ( $this->open( $handle ) && is_resource( $this->_handles[ $handle ] ) ) {
-			$time = date_i18n( 'm-d-Y @ H:i:s -' ); // Grab Time
-			@fwrite( $this->_handles[ $handle ], $time . " " . $message . "\n" );
+			$time   = date_i18n( 'm-d-Y @ H:i:s -' ); // Grab Time
+			$result = fwrite( $this->_handles[ $handle ], $time . " " . $message . "\n" );
 		}
 
 		do_action( 'woocommerce_log_add', $handle, $message );
-	}
 
+		return $result === false ? false : true;
+	}
 
 	/**
 	 * Clear entries from chosen file.
 	 *
 	 * @param mixed $handle
+	 *
+	 * @return bool
 	 */
 	public function clear( $handle ) {
+		$result = false;
+
 		if ( $this->open( $handle ) && is_resource( $this->_handles[ $handle ] ) ) {
-			@ftruncate( $this->_handles[ $handle ], 0 );
+			$result = ftruncate( $this->_handles[ $handle ], 0 );
 		}
 
 		do_action( 'woocommerce_log_clear', $handle );
+
+		return $result === false ? false : true;
 	}
 
 }
