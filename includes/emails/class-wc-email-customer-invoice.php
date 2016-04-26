81bc211c57d907f11de13a5c0a67a7396bae5992
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-04-26 18:26:17 +0200

    Add `public` keyword to public email methods

--- includes/emails/class-wc-email-customer-invoice.php
+++ includes/emails/class-wc-email-customer-invoice.php
@@ -36,7 +36,7 @@ class WC_Email_Customer_Invoice extends WC_Email {
 	/**
 	 * Constructor.
 	 */
-	function __construct() {
+	public function __construct() {
 
 		$this->id             = 'customer_invoice';
 		$this->title          = __( 'Customer invoice', 'woocommerce' );
@@ -65,7 +65,7 @@ class WC_Email_Customer_Invoice extends WC_Email {
 	 *
 	 * @param int|WC_Order $order
 	 */
-	function trigger( $order ) {
+	public function trigger( $order ) {
 
 		if ( ! is_object( $order ) ) {
 			$order = wc_get_order( absint( $order ) );
@@ -95,7 +95,7 @@ class WC_Email_Customer_Invoice extends WC_Email {
 	 * @access public
 	 * @return string
 	 */
-	function get_subject() {
+	public function get_subject() {
 		if ( $this->object->has_status( array( 'processing', 'completed' ) ) ) {
 			return apply_filters( 'woocommerce_email_subject_customer_invoice_paid', $this->format_string( $this->subject_paid ), $this->object );
 		} else {
@@ -109,7 +109,7 @@ class WC_Email_Customer_Invoice extends WC_Email {
 	 * @access public
 	 * @return string
 	 */
-	function get_heading() {
+	public function get_heading() {
 		if ( $this->object->has_status( array( 'completed', 'processing' ) ) ) {
 			return apply_filters( 'woocommerce_email_heading_customer_invoice_paid', $this->format_string( $this->heading_paid ), $this->object );
 		} else {
@@ -123,7 +123,7 @@ class WC_Email_Customer_Invoice extends WC_Email {
 	 * @access public
 	 * @return string
 	 */
-	function get_content_html() {
+	public function get_content_html() {
 		return wc_get_template_html( $this->template_html, array(
 			'order'         => $this->object,
 			'email_heading' => $this->get_heading(),
@@ -139,7 +139,7 @@ class WC_Email_Customer_Invoice extends WC_Email {
 	 * @access public
 	 * @return string
 	 */
-	function get_content_plain() {
+	public function get_content_plain() {
 		return wc_get_template_html( $this->template_plain, array(
 			'order'         => $this->object,
 			'email_heading' => $this->get_heading(),
@@ -152,7 +152,7 @@ class WC_Email_Customer_Invoice extends WC_Email {
 	/**
 	 * Initialise settings form fields.
 	 */
-	function init_form_fields() {
+	public function init_form_fields() {
 		$this->form_fields = array(
 			'subject' => array(
 				'title'         => __( 'Email Subject', 'woocommerce' ),
