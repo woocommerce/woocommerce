e5bbd9c09222cfc70003da8bdf52c9390ec7d3fd
Mike Jolley <mike.jolley@me.com>
2016-05-03 15:16:20 +0100

    Check wc_get_shipping_zone does not exist
    Closes #10843

--- includes/wc-core-functions.php
+++ includes/wc-core-functions.php
@@ -1158,16 +1158,18 @@ function woocommerce_register_shipping_method( $shipping_method ) {
 	WC()->shipping->register_shipping_method( $shipping_method );
 }
 
-/**
- * Get the shipping zone matching a given package from the cart.
- *
- * @since  2.6.0
- * @uses   WC_Shipping_Zones::get_zone_matching_package
- * @param  array $package
- * @return WC_Shipping_Zone
- */
-function wc_get_shipping_zone( $package ) {
-	return WC_Shipping_Zones::get_zone_matching_package( $package );
+if ( ! function_exists( 'wc_get_shipping_zone' ) ) {
+	/**
+	 * Get the shipping zone matching a given package from the cart.
+	 *
+	 * @since  2.6.0
+	 * @uses   WC_Shipping_Zones::get_zone_matching_package
+	 * @param  array $package
+	 * @return WC_Shipping_Zone
+	 */
+	function wc_get_shipping_zone( $package ) {
+		return WC_Shipping_Zones::get_zone_matching_package( $package );
+	}
 }
 
 /**
