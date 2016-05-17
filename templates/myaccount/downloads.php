17eae30bf939333f4d899dcc3e95d31b6a009c5d
Beka Rice <beka.rice@gmail.com>
2016-05-17 15:44:56 -0400

    [2.6] Add notice to My Account > Downloads..
    …when no downloads are present; consistent with “Orders” empty notice

--- templates/myaccount/downloads.php
+++ templates/myaccount/downloads.php
@@ -98,6 +98,13 @@ wc_print_notices(); ?>
 
 		<?php do_action( 'woocommerce_after_available_downloads' ); ?>
 
+	<?php else : ?>
+		<div class="woocommerce-Message woocommerce-Message--info woocommerce-info">
+			<a class="woocommerce-Button button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
+				<?php esc_html_e( 'Go Shop', 'woocommerce' ) ?>
+			</a>
+			<?php esc_html_e( 'No downloads available yet.', 'woocommerce' ); ?>
+		</div>
 	<?php endif; ?>
 
 	<?php do_action( 'woocommerce_after_account_downloads', $has_downloads ); ?>
