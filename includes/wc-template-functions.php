72a2c25536d144fd5d470baa18e45c60123eccf4
Caleb Burks <19caleb95@gmail.com>
2016-05-21 10:29:44 -0500

    Add single_product_archive_thumbnail_size filter
    Add in a filter to allow for custom image sizes w/o needing to override the whole function.

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -728,6 +728,7 @@ if ( ! function_exists( 'woocommerce_get_product_thumbnail' ) ) {
 	 */
 	function woocommerce_get_product_thumbnail( $size = 'shop_catalog', $deprecated1 = 0, $deprecated2 = 0 ) {
 		global $post;
+		$size = apply_filters( 'single_product_archive_thumbnail_size', 'shop_catalog' );
 
 		if ( has_post_thumbnail() ) {
 			$props = wc_get_product_attachment_props( get_post_thumbnail_id(), $post );
