980a726984ceecf973266fe9e47b2a22e49beeb0
Mike Jolley <mike.jolley@me.com>
2016-05-23 13:39:23 +0100

    Fix grouped add to cart notice
    Closes #10974

--- includes/wc-cart-functions.php
+++ includes/wc-cart-functions.php
@@ -73,7 +73,7 @@ function wc_add_to_cart_message( $products, $show_qty = false ) {
 		$show_qty = false;
 	}
 
-	if ( ! $show_qty ) {
+	if ( ! $show_qty && ! is_array( $products ) ) {
 		$products = array_fill_keys( array_values( $products ), 1 );
 	}
 
