18bfce07479f25f35e7cc6a6e21379015e4d8e8e
Philip Salzmann <philip.salzmann@red-six.net>
2016-05-23 13:32:16 +0200

    Add austrian postcode validation

--- includes/class-wc-validation.php
+++ includes/class-wc-validation.php
@@ -52,6 +52,9 @@ class WC_Validation {
 		}
 
 		switch ( $country ) {
+			case 'AT' :
+				$valid = (bool) preg_match( '/^([0-9]{4})$/', $postcode );
+				break;
 			case 'BR' :
 				$valid = (bool) preg_match( '/^([0-9]{5})([-])?([0-9]{3})$/', $postcode );
 				break;
