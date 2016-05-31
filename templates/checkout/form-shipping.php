da2337b05ba9c3b80e0ca8dd32ed80d9801dd107
Mike Jolley <mike.jolley@me.com>
2016-05-31 11:26:41 +0100

    POST is not needed here; form never updates
    Fixes #11006

--- templates/checkout/form-shipping.php
+++ templates/checkout/form-shipping.php
@@ -24,22 +24,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="woocommerce-shipping-fields">
 	<?php if ( true === WC()->cart->needs_shipping_address() ) : ?>
 
-		<?php
-			if ( empty( $_POST ) ) {
-
-				$ship_to_different_address = get_option( 'woocommerce_ship_to_destination' ) === 'shipping' ? 1 : 0;
-				$ship_to_different_address = apply_filters( 'woocommerce_ship_to_different_address_checked', $ship_to_different_address );
-
-			} else {
-
-				$ship_to_different_address = $checkout->get_value( 'ship_to_different_address' );
-
-			}
-		?>
-
 		<h3 id="ship-to-different-address">
 			<label for="ship-to-different-address-checkbox" class="checkbox"><?php _e( 'Ship to a different address?', 'woocommerce' ); ?></label>
-			<input id="ship-to-different-address-checkbox" class="input-checkbox" <?php checked( $ship_to_different_address, 1 ); ?> type="checkbox" name="ship_to_different_address" value="1" />
+			<input id="ship-to-different-address-checkbox" class="input-checkbox" <?php checked( apply_filters( 'woocommerce_ship_to_different_address_checked', 'shipping' === get_option( 'woocommerce_ship_to_destination' ) ? 1 : 0 ), 1 ); ?> type="checkbox" name="ship_to_different_address" value="1" />
 		</h3>
 
 		<div class="shipping_address">
