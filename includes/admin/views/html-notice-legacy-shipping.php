ebc913da38f6225eb0e2f248dc326e8392094a58
Mike Jolley <mike.jolley@me.com>
2016-06-03 12:22:05 +0100

    Notice tweaks

--- includes/admin/views/html-notice-legacy-shipping.php
+++ includes/admin/views/html-notice-legacy-shipping.php
@@ -10,8 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div id="message" class="updated woocommerce-message woocommerce-legacy-shipping-notice">
 	<a class="woocommerce-message-close notice-dismiss" href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'wc-hide-notice', 'legacy_shipping' ), 'woocommerce_hide_notices_nonce', '_wc_notice_nonce' ) ); ?>"><?php _e( 'Dismiss', 'woocommerce' ); ?></a>
 
-	<p class="main"><strong><?php _e( 'New feature: shipping zones!', 'woocommerce' ); ?></strong></p>
-	<p><?php _e( 'A shipping zone is a group of regions that can be assigned different shipping methods and rates.', 'woocommerce' ); ?></p>
+	<p class="main"><strong><?php _e( 'New:', 'woocommerce' ); ?> <?php _e( 'Shipping Zones', 'woocommerce' ); ?></strong> &#8211; <?php _e( 'a group of regions that can be assigned different shipping methods and rates.', 'woocommerce' ); ?></p>
 	<p><?php _e( 'Legacy shipping methods (Flat Rate, International Flat Rate, Local Pickup and Delivery, and Free Shipping) are deprecated but will continue to work as normal for now. <b><em>They will be removed in future versions of WooCommerce</em></b>. We recommend disabling these and setting up new rates within shipping zones as soon as possible.', 'woocommerce' ); ?></p>
 
 	<p class="submit">
