f47e9f4af5cdeffdfabed5cefe071dec0650b385
Mike Jolley <mike.jolley@me.com>
2016-06-09 12:20:57 +0100

    woocommerce_order_status_failed_to_on-hold_notification for on hold email

--- includes/emails/class-wc-email-customer-on-hold-order.php
+++ includes/emails/class-wc-email-customer-on-hold-order.php
@@ -34,6 +34,7 @@ class WC_Email_Customer_On_Hold_Order extends WC_Email {
 
 		// Triggers for this email
 		add_action( 'woocommerce_order_status_pending_to_on-hold_notification', array( $this, 'trigger' ) );
+		add_action( 'woocommerce_order_status_failed_to_on-hold_notification', array( $this, 'trigger' ) );
 
 		// Call parent constructor
 		parent::__construct();
