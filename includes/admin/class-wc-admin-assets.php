4913539977538324c8d9d60e843a46380f82d0e2
Kristina Plauche <kristinaplauche@Kristinas-MacBook-Pro.local>
2016-06-30 14:02:16 -0400

    11318 update
    Applied update suggested by Claudio

--- includes/admin/class-wc-admin-assets.php
+++ includes/admin/class-wc-admin-assets.php
@@ -36,7 +36,7 @@ class WC_Admin_Assets {
 
 		$screen         = get_current_screen();
 		$screen_id      = $screen ? $screen->id : '';
-		$jquery_version = isset( $wp_scripts->registered['jquery-ui-core']->ver ) ? $wp_scripts->registered['jquery-ui-core']->ver : '1.9.2';
+		$jquery_version = isset( $wp_scripts->registered['jquery-ui-core']->ver ) ? $wp_scripts->registered['jquery-ui-core']->ver : '1.11.4';
 
 		// Register admin styles
 		wp_register_style( 'woocommerce_admin_menu_styles', WC()->plugin_url() . '/assets/css/menu.css', array(), WC_VERSION );
