8d2357768302147a6f2418755543ba7aa030aeaa
Claudio Sanches <contato@claudiosmweb.com>
2016-07-04 17:37:16 -0300

    Fixed the load of the WC_Email_Customer_On_Hold_Order class, closes #11348

--- includes/emails/class-wc-email-customer-on-hold-order.php
+++ includes/emails/class-wc-email-customer-on-hold-order.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-if ( ! class_exists( 'WC_Email_Customer_Processing_Order' ) ) :
+if ( ! class_exists( 'WC_Email_Customer_On_Hold_Order' ) ) :
 
 /**
  * Customer On-hold Order Email.
