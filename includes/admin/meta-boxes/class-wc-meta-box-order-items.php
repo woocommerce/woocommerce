f8d5d8e1e6c9d15dfe8c1c3785da4125564abff4
mattyza <hello@matty.co.za>
2016-07-06 14:12:05 +0200

    Code consistency changes for includes/admin/meta-boxes/class-wc-meta-box-order-items.php

--- includes/admin/meta-boxes/class-wc-meta-box-order-items.php
+++ includes/admin/meta-boxes/class-wc-meta-box-order-items.php
@@ -44,10 +44,11 @@ class WC_Meta_Box_Order_Items {
 	/**
 	 * Save meta box data.
 	 *
-	 * @param int $post_id
+	 * @param int     $post_id
 	 * @param WP_Post $post
 	 */
 	public static function save( $post_id, $post ) {
 		wc_save_order_items( $post_id, $_POST );
 	}
+
 }
