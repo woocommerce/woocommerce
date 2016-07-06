bc4879ffd17de361d1d0b59caf530ba0a3f02d34
mattyza <hello@matty.co.za>
2016-07-06 14:21:18 +0200

    Code consistency changes for includes/widgets/class-wc-widget-rating-filter.php

--- includes/widgets/class-wc-widget-rating-filter.php
+++ includes/widgets/class-wc-widget-rating-filter.php
@@ -25,7 +25,7 @@ class WC_Widget_Rating_Filter extends WC_Widget {
 		$this->widget_id          = 'woocommerce_rating_filter';
 		$this->widget_name        = __( 'WooCommerce Average Rating Filter', 'woocommerce' );
 		$this->settings           = array(
-			'title'  => array(
+			'title' => array(
 				'type'  => 'text',
 				'std'   => __( 'Average Rating', 'woocommerce' ),
 				'label' => __( 'Title', 'woocommerce' )
@@ -41,10 +41,10 @@ class WC_Widget_Rating_Filter extends WC_Widget {
 	protected function get_page_base_url() {
 		if ( defined( 'SHOP_IS_ON_FRONT' ) ) {
 			$link = home_url();
-		} elseif ( is_post_type_archive( 'product' ) || is_page( wc_get_page_id('shop') ) ) {
+		} elseif ( is_post_type_archive( 'product' ) || is_page( wc_get_page_id( 'shop' ) ) ) {
 			$link = get_post_type_archive_link( 'product' );
 		} else {
-			$link = get_term_link( get_query_var('term'), get_query_var('taxonomy') );
+			$link = get_term_link( get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 		}
 
 		// Min/Max
@@ -174,8 +174,8 @@ class WC_Widget_Rating_Filter extends WC_Widget {
 
 			echo '<a href="' . esc_url( apply_filters( 'woocommerce_rating_filter_link', $link ) ) . '">';
 
-			echo '<span class="star-rating" title="' . esc_attr( sprintf( __( 'Rated %s and above', 'woocommerce' ), $rating ) ). '">
-					<span style="width:' . esc_attr( ( $rating / 5 ) * 100 ) . '%">' . sprintf( __( 'Rated %s and above', 'woocommerce'), $rating ) . '</span>
+			echo '<span class="star-rating" title="' . esc_attr( sprintf( __( 'Rated %s and above', 'woocommerce' ), $rating ) ) . '">
+					<span style="width:' . esc_attr( ( $rating / 5 ) * 100 ) . '%">' . sprintf( __( 'Rated %s and above', 'woocommerce' ), $rating ) . '</span>
 				</span> (' . esc_html( $count ) . ')';
 
 			echo '</a>';
@@ -193,4 +193,5 @@ class WC_Widget_Rating_Filter extends WC_Widget {
 			echo ob_get_clean();
 		}
 	}
+
 }
