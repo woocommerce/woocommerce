8d410d35055762b344ff4d5845223e20f1fd1f0e
mattyza <hello@matty.co.za>
2016-07-06 14:22:03 +0200

    Code consistency changes for templates/emails/customer-new-account.php

--- templates/emails/customer-new-account.php
+++ templates/emails/customer-new-account.php
@@ -24,14 +24,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<p><?php printf( __( "Thanks for creating an account on %s. Your username is <strong>%s</strong>", 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ) ); ?></p>
+<p><?php printf( __( 'Thanks for creating an account on %s. Your username is <strong>%s</strong>', 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ) ); ?></p>
 
 <?php if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated ) : ?>
 
-	<p><?php printf( __( "Your password has been automatically generated: <strong>%s</strong>", 'woocommerce' ), esc_html( $user_pass ) ); ?></p>
+	<p><?php printf( __( 'Your password has been automatically generated: <strong>%s</strong>', 'woocommerce' ), esc_html( $user_pass ) ); ?></p>
 
 <?php endif; ?>
 
 <p><?php printf( __( 'You can access your account area to view your orders and change your password here: %s.', 'woocommerce' ), wc_get_page_permalink( 'myaccount' ) ); ?></p>
 
-<?php do_action( 'woocommerce_email_footer', $email ); ?>
+<?php do_action( 'woocommerce_email_footer', $email );
