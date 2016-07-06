afe1e29c3d50b939fb2c7231cbd00f2b81d76ec3
mattyza <hello@matty.co.za>
2016-07-06 14:24:02 +0200

    Code consistency changes for templates/single-product/share.php

--- templates/single-product/share.php
+++ templates/single-product/share.php
@@ -24,4 +24,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<?php do_action( 'woocommerce_share' ); // Sharing plugins can hook into here ?>
+<?php do_action( 'woocommerce_share' ); // Sharing plugins can hook into here
