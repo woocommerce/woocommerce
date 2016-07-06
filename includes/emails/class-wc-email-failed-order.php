7b90d5ca34710f770920a0360f03c054313609cc
mattyza <hello@matty.co.za>
2016-07-06 14:18:32 +0200

    Code consistency changes for includes/emails/class-wc-email-failed-order.php

--- includes/emails/class-wc-email-failed-order.php
+++ includes/emails/class-wc-email-failed-order.php
@@ -23,13 +23,13 @@ class WC_Email_Failed_Order extends WC_Email {
 	 * Constructor
 	 */
 	public function __construct() {
-		$this->id               = 'failed_order';
-		$this->title            = __( 'Failed order', 'woocommerce' );
-		$this->description      = __( 'Failed order emails are sent to chosen recipient(s) when orders have been marked failed (if they were previously processing or on-hold).', 'woocommerce' );
-		$this->heading          = __( 'Failed order', 'woocommerce' );
-		$this->subject          = __( '[{site_title}] Failed order ({order_number})', 'woocommerce' );
-		$this->template_html    = 'emails/admin-failed-order.php';
-		$this->template_plain   = 'emails/plain/admin-failed-order.php';
+		$this->id             = 'failed_order';
+		$this->title          = __( 'Failed order', 'woocommerce' );
+		$this->description    = __( 'Failed order emails are sent to chosen recipient(s) when orders have been marked failed (if they were previously processing or on-hold).', 'woocommerce' );
+		$this->heading        = __( 'Failed order', 'woocommerce' );
+		$this->subject        = __( '[{site_title}] Failed order ({order_number})', 'woocommerce' );
+		$this->template_html  = 'emails/admin-failed-order.php';
+		$this->template_plain = 'emails/plain/admin-failed-order.php';
 
 		// Triggers for this email
 		add_action( 'woocommerce_order_status_pending_to_failed_notification', array( $this, 'trigger' ) );
@@ -49,7 +49,7 @@ class WC_Email_Failed_Order extends WC_Email {
 	 */
 	public function trigger( $order_id ) {
 		if ( $order_id ) {
-            $this->object                  = wc_get_order( $order_id );
+			$this->object                  = wc_get_order( $order_id );
 			$this->find['order-date']      = '{order_date}';
 			$this->find['order-number']    = '{order_number}';
 			$this->replace['order-date']   = date_i18n( wc_date_format(), strtotime( $this->object->order_date ) );
@@ -75,7 +75,7 @@ class WC_Email_Failed_Order extends WC_Email {
 			'email_heading' => $this->get_heading(),
 			'sent_to_admin' => true,
 			'plain_text'    => false,
-			'email'			=> $this
+			'email'         => $this
 		) );
 	}
 
@@ -90,7 +90,7 @@ class WC_Email_Failed_Order extends WC_Email {
 			'email_heading' => $this->get_heading(),
 			'sent_to_admin' => true,
 			'plain_text'    => true,
-			'email'			=> $this
+			'email'         => $this
 		) );
 	}
 
@@ -99,47 +99,48 @@ class WC_Email_Failed_Order extends WC_Email {
 	 */
 	public function init_form_fields() {
 		$this->form_fields = array(
-			'enabled' => array(
-				'title'         => __( 'Enable/Disable', 'woocommerce' ),
-				'type'          => 'checkbox',
-				'label'         => __( 'Enable this email notification', 'woocommerce' ),
-				'default'       => 'yes'
+			'enabled'    => array(
+				'title'   => __( 'Enable/Disable', 'woocommerce' ),
+				'type'    => 'checkbox',
+				'label'   => __( 'Enable this email notification', 'woocommerce' ),
+				'default' => 'yes'
 			),
-			'recipient' => array(
-				'title'         => __( 'Recipient(s)', 'woocommerce' ),
-				'type'          => 'text',
-				'description'   => sprintf( __( 'Enter recipients (comma separated) for this email. Defaults to <code>%s</code>.', 'woocommerce' ), esc_attr( get_option('admin_email') ) ),
-				'placeholder'   => '',
-				'default'       => '',
-				'desc_tip'      => true
+			'recipient'  => array(
+				'title'       => __( 'Recipient(s)', 'woocommerce' ),
+				'type'        => 'text',
+				'description' => sprintf( __( 'Enter recipients (comma separated) for this email. Defaults to <code>%s</code>.', 'woocommerce' ), esc_attr( get_option( 'admin_email' ) ) ),
+				'placeholder' => '',
+				'default'     => '',
+				'desc_tip'    => true
 			),
-			'subject' => array(
-				'title'         => __( 'Subject', 'woocommerce' ),
-				'type'          => 'text',
-				'description'   => sprintf( __( 'This controls the email subject line. Leave blank to use the default subject: <code>%s</code>.', 'woocommerce' ), $this->subject ),
-				'placeholder'   => '',
-				'default'       => '',
-				'desc_tip'      => true
+			'subject'    => array(
+				'title'       => __( 'Subject', 'woocommerce' ),
+				'type'        => 'text',
+				'description' => sprintf( __( 'This controls the email subject line. Leave blank to use the default subject: <code>%s</code>.', 'woocommerce' ), $this->subject ),
+				'placeholder' => '',
+				'default'     => '',
+				'desc_tip'    => true
 			),
-			'heading' => array(
-				'title'         => __( 'Email Heading', 'woocommerce' ),
-				'type'          => 'text',
-				'description'   => sprintf( __( 'This controls the main heading contained within the email notification. Leave blank to use the default heading: <code>%s</code>.', 'woocommerce' ), $this->heading ),
-				'placeholder'   => '',
-				'default'       => '',
-				'desc_tip'      => true
+			'heading'    => array(
+				'title'       => __( 'Email Heading', 'woocommerce' ),
+				'type'        => 'text',
+				'description' => sprintf( __( 'This controls the main heading contained within the email notification. Leave blank to use the default heading: <code>%s</code>.', 'woocommerce' ), $this->heading ),
+				'placeholder' => '',
+				'default'     => '',
+				'desc_tip'    => true
 			),
 			'email_type' => array(
-				'title'         => __( 'Email type', 'woocommerce' ),
-				'type'          => 'select',
-				'description'   => __( 'Choose which format of email to send.', 'woocommerce' ),
-				'default'       => 'html',
-				'class'         => 'email_type wc-enhanced-select',
-				'options'       => $this->get_email_type_options(),
-				'desc_tip'      => true
+				'title'       => __( 'Email type', 'woocommerce' ),
+				'type'        => 'select',
+				'description' => __( 'Choose which format of email to send.', 'woocommerce' ),
+				'default'     => 'html',
+				'class'       => 'email_type wc-enhanced-select',
+				'options'     => $this->get_email_type_options(),
+				'desc_tip'    => true
 			)
 		);
 	}
+
 }
 
 endif;
