4a6694a690e5b0d58c284c46874f853882f2abdf
mattyza <hello@matty.co.za>
2016-07-06 14:23:02 +0200

    Code consistency changes for templates/myaccount/downloads.php

--- templates/myaccount/downloads.php
+++ templates/myaccount/downloads.php
@@ -70,7 +70,7 @@ do_action( 'woocommerce_before_account_downloads', $has_downloads ); ?>
 						<?php elseif ( 'download-actions' === $column_id ) : ?>
 							<?php
 								$actions = array(
-									'download'  => array(
+									'download' => array(
 										'url'  => $download['download_url'],
 										'name' => __( 'Download', 'woocommerce' )
 									)
@@ -95,10 +95,10 @@ do_action( 'woocommerce_before_account_downloads', $has_downloads ); ?>
 <?php else : ?>
 	<div class="woocommerce-Message woocommerce-Message--info woocommerce-info">
 		<a class="woocommerce-Button button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-			<?php esc_html_e( 'Go Shop', 'woocommerce' ) ?>
+			<?php esc_html_e( 'Go Shop', 'woocommerce' ); ?>
 		</a>
 		<?php esc_html_e( 'No downloads available yet.', 'woocommerce' ); ?>
 	</div>
 <?php endif; ?>
 
-<?php do_action( 'woocommerce_after_account_downloads', $has_downloads ); ?>
+<?php do_action( 'woocommerce_after_account_downloads', $has_downloads );
