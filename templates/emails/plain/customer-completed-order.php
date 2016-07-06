092b6d6001e642c7dcb4630d68819c66c85fec88
mattyza <hello@matty.co.za>
2016-07-06 14:22:24 +0200

    Code consistency changes for templates/emails/plain/customer-completed-order.php

--- templates/emails/plain/customer-completed-order.php
+++ templates/emails/plain/customer-completed-order.php
@@ -20,9 +20,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-echo "= " . $email_heading . " =\n\n";
+echo '= ' . $email_heading . " =\n\n";
 
-echo sprintf( __( "Hi there. Your recent order on %s has been completed. Your order details are shown below for your reference:", 'woocommerce' ), get_option( 'blogname' ) ) . "\n\n";
+echo sprintf( __( 'Hi there. Your recent order on %s has been completed. Your order details are shown below for your reference:', 'woocommerce' ), get_option( 'blogname' ) ) . "\n\n";
 
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n";
 
