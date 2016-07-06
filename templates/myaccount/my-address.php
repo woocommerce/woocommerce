1eb6d1fe87707f75bdc0c7df084b45efde73d9a2
mattyza <hello@matty.co.za>
2016-07-06 14:23:13 +0200

    Code consistency changes for templates/myaccount/my-address.php

--- templates/myaccount/my-address.php
+++ templates/myaccount/my-address.php
@@ -24,7 +24,7 @@ $customer_id = get_current_user_id();
 
 if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) {
 	$get_addresses = apply_filters( 'woocommerce_my_account_get_addresses', array(
-		'billing' => __( 'Billing Address', 'woocommerce' ),
+		'billing'  => __( 'Billing Address', 'woocommerce' ),
 		'shipping' => __( 'Shipping Address', 'woocommerce' )
 	), $customer_id );
 } else {
@@ -53,27 +53,28 @@ $col    = 1;
 		<address>
 			<?php
 				$address = apply_filters( 'woocommerce_my_account_my_address_formatted_address', array(
-					'first_name'  => get_user_meta( $customer_id, $name . '_first_name', true ),
-					'last_name'   => get_user_meta( $customer_id, $name . '_last_name', true ),
-					'company'     => get_user_meta( $customer_id, $name . '_company', true ),
-					'address_1'   => get_user_meta( $customer_id, $name . '_address_1', true ),
-					'address_2'   => get_user_meta( $customer_id, $name . '_address_2', true ),
-					'city'        => get_user_meta( $customer_id, $name . '_city', true ),
-					'state'       => get_user_meta( $customer_id, $name . '_state', true ),
-					'postcode'    => get_user_meta( $customer_id, $name . '_postcode', true ),
-					'country'     => get_user_meta( $customer_id, $name . '_country', true )
+					'first_name' => get_user_meta( $customer_id, $name . '_first_name', true ),
+					'last_name'  => get_user_meta( $customer_id, $name . '_last_name', true ),
+					'company'    => get_user_meta( $customer_id, $name . '_company', true ),
+					'address_1'  => get_user_meta( $customer_id, $name . '_address_1', true ),
+					'address_2'  => get_user_meta( $customer_id, $name . '_address_2', true ),
+					'city'       => get_user_meta( $customer_id, $name . '_city', true ),
+					'state'      => get_user_meta( $customer_id, $name . '_state', true ),
+					'postcode'   => get_user_meta( $customer_id, $name . '_postcode', true ),
+					'country'    => get_user_meta( $customer_id, $name . '_country', true )
 				), $customer_id, $name );
 
 				$formatted_address = WC()->countries->get_formatted_address( $address );
 
-				if ( ! $formatted_address )
+				if ( ! $formatted_address ) {
 					_e( 'You have not set up this type of address yet.', 'woocommerce' );
-				else
+				} else {
 					echo $formatted_address;
+				}
 			?>
 		</address>
 	</div>
 
 <?php endforeach; ?>
 
-<?php if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) echo '</div>'; ?>
+<?php if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) echo '</div>';
