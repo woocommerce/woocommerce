d7958d10780ca24030cca0695c41ae848defca2e
mattyza <hello@matty.co.za>
2016-07-06 14:21:59 +0200

    Code consistency changes for templates/emails/admin-new-order.php

--- templates/emails/admin-new-order.php
+++ templates/emails/admin-new-order.php
@@ -16,38 +16,38 @@
  * @version 2.5.0
  */
 
- if ( ! defined( 'ABSPATH' ) ) {
- 	exit;
- }
+	if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+	}
 
- /**
-  * @hooked WC_Emails::email_header() Output the email header
-  */
- do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+	/**
+	 * @hooked WC_Emails::email_header() Output the email header
+	 */
+	do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
  <p><?php printf( __( 'You have received an order from %s. The order is as follows:', 'woocommerce' ), $order->get_formatted_billing_full_name() ); ?></p>
 
  <?php
 
- /**
-  * @hooked WC_Emails::order_details() Shows the order details table.
-  * @hooked WC_Emails::order_schema_markup() Adds Schema.org markup.
-  * @since 2.5.0
-  */
- do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
+	/**
+	 * @hooked WC_Emails::order_details() Shows the order details table.
+	 * @hooked WC_Emails::order_schema_markup() Adds Schema.org markup.
+	 * @since 2.5.0
+	 */
+	do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
 
- /**
-  * @hooked WC_Emails::order_meta() Shows order meta data.
-  */
- do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+	/**
+	 * @hooked WC_Emails::order_meta() Shows order meta data.
+	 */
+	do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
 
- /**
-  * @hooked WC_Emails::customer_details() Shows customer details
-  * @hooked WC_Emails::email_address() Shows email address
-  */
- do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
+	/**
+	 * @hooked WC_Emails::customer_details() Shows customer details
+	 * @hooked WC_Emails::email_address() Shows email address
+	 */
+	do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
 
- /**
-  * @hooked WC_Emails::email_footer() Output the email footer
-  */
- do_action( 'woocommerce_email_footer', $email );
+	/**
+	 * @hooked WC_Emails::email_footer() Output the email footer
+	 */
+	do_action( 'woocommerce_email_footer', $email );
