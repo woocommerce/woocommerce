f7a40d4c2b9ac36c2ed8f5a5fbd696969610055d
mattyza <hello@matty.co.za>
2016-07-06 14:22:56 +0200

    Code consistency changes for templates/loop/rating.php

--- templates/loop/rating.php
+++ templates/loop/rating.php
@@ -22,10 +22,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 
-if ( get_option( 'woocommerce_enable_review_rating' ) === 'no' )
+if ( get_option( 'woocommerce_enable_review_rating' ) === 'no' ) {
 	return;
+}
 ?>
 
 <?php if ( $rating_html = $product->get_rating_html() ) : ?>
 	<?php echo $rating_html; ?>
-<?php endif; ?>
+<?php endif;
