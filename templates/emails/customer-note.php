20ce4d743c71ba595e13c9c6412de31d551ef6f7
mattyza <hello@matty.co.za>
2016-07-06 14:22:05 +0200

    Code consistency changes for templates/emails/customer-note.php

--- templates/emails/customer-note.php
+++ templates/emails/customer-note.php
@@ -25,11 +25,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<p><?php _e( "Hello, a note has just been added to your order:", 'woocommerce' ); ?></p>
+<p><?php _e( 'Hello, a note has just been added to your order:', 'woocommerce' ); ?></p>
 
-<blockquote><?php echo wpautop( wptexturize( $customer_note ) ) ?></blockquote>
+<blockquote><?php echo wpautop( wptexturize( $customer_note ) ); ?></blockquote>
 
-<p><?php _e( "For your reference, your order details are shown below.", 'woocommerce' ); ?></p>
+<p><?php _e( 'For your reference, your order details are shown below.', 'woocommerce' ); ?></p>
 
 <?php
 
