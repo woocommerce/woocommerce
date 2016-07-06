55188002e220b5eff4a36ef71257b041a06747de
mattyza <hello@matty.co.za>
2016-07-06 14:20:22 +0200

    Code consistency changes for includes/walkers/class-product-cat-dropdown-walker.php

--- includes/walkers/class-product-cat-dropdown-walker.php
+++ includes/walkers/class-product-cat-dropdown-walker.php
@@ -41,16 +41,16 @@ class WC_Product_Cat_Dropdown_Walker extends Walker {
 	 * @see Walker::start_el()
 	 * @since 2.1.0
 	 *
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @param int $depth Depth of category in reference to parents.
+	 * @param string  $output            Passed by reference. Used to append additional content.
+	 * @param int     $depth             Depth of category in reference to parents.
 	 * @param integer $current_object_id
 	 */
 	public function start_el( &$output, $cat, $depth = 0, $args = array(), $current_object_id = 0 ) {
-
-		if ( ! empty( $args['hierarchical'] ) )
-			$pad = str_repeat('&nbsp;', $depth * 3);
-		else
+		if ( ! empty( $args['hierarchical'] ) ) {
+			$pad = str_repeat( '&nbsp;', $depth * 3 );
+		} else {
 			$pad = '';
+		}
 
 		$cat_name = apply_filters( 'list_product_cats', $cat->name, $cat );
 
@@ -58,15 +58,17 @@ class WC_Product_Cat_Dropdown_Walker extends Walker {
 
 		$output .= "\t<option class=\"level-$depth\" value=\"" . $value . "\"";
 
-		if ( $value == $args['selected'] || ( is_array( $args['selected'] ) && in_array( $value, $args['selected'] ) ) )
+		if ( $value == $args['selected'] || ( is_array( $args['selected'] ) && in_array( $value, $args['selected'] ) ) ) {
 			$output .= ' selected="selected"';
+		}
 
 		$output .= '>';
 
 		$output .= $pad . _x( $cat_name, 'product category name', 'woocommerce' );
 
-		if ( ! empty( $args['show_count'] ) )
+		if ( ! empty( $args['show_count'] ) ) {
 			$output .= '&nbsp;(' . $cat->count . ')';
+		}
 
 		$output .= "</option>\n";
 	}
@@ -83,13 +85,13 @@ class WC_Product_Cat_Dropdown_Walker extends Walker {
 	 *
 	 * @since 2.5.0
 	 *
-	 * @param object $element Data object
-	 * @param array $children_elements List of elements to continue traversing.
-	 * @param int $max_depth Max depth to traverse.
-	 * @param int $depth Depth of current element.
-	 * @param array $args
-	 * @param string $output Passed by reference. Used to append additional content.
-	 * @return null Null on failure with no changes to parameters.
+	 * @param  object $element           Data object
+	 * @param  array  $children_elements List of elements to continue traversing.
+	 * @param  int    $max_depth         Max depth to traverse.
+	 * @param  int    $depth             Depth of current element.
+	 * @param  array  $args
+	 * @param  string $output            Passed by reference. Used to append additional content.
+	 * @return null                      Null on failure with no changes to parameters.
 	 */
 	public function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
 		if ( ! $element || ( 0 === $element->count && ! empty( $args[0]['hide_empty'] ) ) ) {
@@ -97,6 +99,7 @@ class WC_Product_Cat_Dropdown_Walker extends Walker {
 		}
 		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
 	}
+
 }
 
 endif;
