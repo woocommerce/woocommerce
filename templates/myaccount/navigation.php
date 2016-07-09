a1cffe016b09254f3fcbc36df6ade7e8002eb894
Ninos Ego <me@ninosego.de>
2016-07-09 06:27:26 +0200

    Hardening: XSS

--- templates/myaccount/navigation.php
+++ templates/myaccount/navigation.php
@@ -26,7 +26,7 @@ do_action( 'woocommerce_before_account_navigation' );
 <nav class="woocommerce-MyAccount-navigation">
 	<ul>
 		<?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
-			<li class="<?php echo wc_get_account_menu_item_classes( $endpoint ); ?>">
+			<li class="<?php echo esc_attr( wc_get_account_menu_item_classes( $endpoint ) ); ?>">
 				<a href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>"><?php echo esc_html( $label ); ?></a>
 			</li>
 		<?php endforeach; ?>
