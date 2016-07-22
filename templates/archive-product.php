f0e069ed349a64e225c73adeb8052baeb7f8dd18
opportus <opportus@gmail.com>
2016-07-22 16:01:34 +0200

    archive product header - new class name convention - patch1

--- templates/archive-product.php
+++ templates/archive-product.php
@@ -32,7 +32,7 @@ get_header( 'shop' ); ?>
 		do_action( 'woocommerce_before_main_content' );
 	?>
 
-    <header class="woocommerce-products-header archive-product-header">
+    <header class="woocommerce-products-header">
 
 		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 
