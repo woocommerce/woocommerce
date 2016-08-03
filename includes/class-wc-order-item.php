a053d234c9c77158229f473ad03a7b484360bb7f
Mike Jolley <mike.jolley@me.com>
2016-08-03 12:52:51 +0100

    get_order() method

--- includes/class-wc-order-item.php
+++ includes/class-wc-order-item.php
@@ -29,6 +29,12 @@ class WC_Order_Item extends WC_Data implements ArrayAccess {
 	);
 
 	/**
+	 * May store an order to prevent retriving it multiple times.
+	 * @var object
+	 */
+	protected $_order;
+
+	/**
 	 * Stores meta in cache for future reads.
 	 * A group must be set to to enable caching.
 	 * @var string
@@ -88,6 +94,17 @@ class WC_Order_Item extends WC_Data implements ArrayAccess {
 	 */
 	public function get_qty() {
 		return 1;
+	}
+
+	/**
+	 * Get parent order object.
+	 * @return int
+	 */
+	public function get_order() {
+		if ( ! $this->_order ) {
+		 	$this->_order = wc_get_order( $this->get_order_id() );
+		}
+		return $this->_order;
 	}
 
 	/*
