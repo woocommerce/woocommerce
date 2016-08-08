7bdda1f7074c4b66bd80d32e6b5fdfabb89875de
Mike Jolley <mike.jolley@me.com>
2016-08-08 16:34:01 +0100

    more dates

--- includes/emails/class-wc-email-customer-on-hold-order.php
+++ includes/emails/class-wc-email-customer-on-hold-order.php
@@ -54,7 +54,7 @@ class WC_Email_Customer_On_Hold_Order extends WC_Email {
 			$this->find['order-date']      = '{order_date}';
 			$this->find['order-number']    = '{order_number}';
 
-			$this->replace['order-date']   = date_i18n( wc_date_format(), strtotime( $this->object->order_date ) );
+			$this->replace['order-date']   = date_i18n( wc_date_format(), $this->object->get_date_created() );
 			$this->replace['order-number'] = $this->object->get_order_number();
 		}
 
