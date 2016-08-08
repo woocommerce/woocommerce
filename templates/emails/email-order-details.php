8e5ddb4d815d7da62af7d0f0a835d1242e93b488
Mike Jolley <mike.jolley@me.com>
2016-08-08 16:35:03 +0100

    Fix wc_get_email_order_items

--- templates/emails/email-order-details.php
+++ templates/emails/email-order-details.php
@@ -37,7 +37,7 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
 		</tr>
 	</thead>
 	<tbody>
-		<?php echo wc_get_email_order_items( array(
+		<?php echo wc_get_email_order_items( $order, array(
 			'show_sku'      => $sent_to_admin,
 			'show_image'    => false,
 			'image_size'    => array( 32, 32 ),
