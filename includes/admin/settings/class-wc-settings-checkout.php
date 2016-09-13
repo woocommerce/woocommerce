e39a7fa602eb79e450e92a4a63a64b1284268bec
rspublishing <rynaldo.stoltz@automattic.com>
2016-09-13 13:50:52 +0200

    Link to SSL/HTTPS doc from checkout settings

--- includes/admin/settings/class-wc-settings-checkout.php
+++ includes/admin/settings/class-wc-settings-checkout.php
@@ -109,7 +109,7 @@ class WC_Settings_Payment_Gateways extends WC_Settings_Page {
 				'type'            => 'checkbox',
 				'checkboxgroup'   => '',
 				'show_if_checked' => 'option',
-				'desc_tip'        => __( 'Force SSL (HTTPS) on the checkout pages (an SSL Certificate is required).', 'woocommerce' ),
+				'desc_tip'        => sprintf( __( 'Force SSL (HTTPS) on the checkout pages (<a href="%s">an SSL Certificate is required</a>).', 'woocommerce' ), 'https://docs.woocommerce.com/document/ssl-and-https/#section-3' ),
 			),
 
 			'unforce_ssl_checkout' => array(
