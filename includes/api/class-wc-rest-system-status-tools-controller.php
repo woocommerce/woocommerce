180ac717a8b41d806feeb6df265cacfcdb5abbd6
rspublishing <rynaldo.stoltz@automattic.com>
2016-09-30 16:23:17 +0200

    Coding standards

--- includes/api/class-wc-rest-system-status-tools-controller.php
+++ includes/api/class-wc-rest-system-status-tools-controller.php
@@ -122,10 +122,10 @@ class WC_REST_System_Status_Tools_Controller extends WC_REST_Controller {
 				'desc'    => __( 'This tool will clear ALL expired transients from WordPress.', 'woocommerce' ),
 			),
 			'delete_orphaned_variations' => array(
-                'name'      => __( 'Orphaned Variations','woocommerce' ), 
-                'button'    => __( 'Delete orphaned variations','woocommerce' ), 
-                'desc'      => __( 'This tool will delete all variations which have no parent.', 'woocommerce' ), 
-            ), 
+                'name'      => __( 'Orphaned Variations', 'woocommerce' ),
+                'button'    => __( 'Delete orphaned variations', 'woocommerce' ),
+                'desc'      => __( 'This tool will delete all variations which have no parent.', 'woocommerce' ),
+            ),
 			'recount_terms' => array(
 				'name'    => __( 'Term counts', 'woocommerce' ),
 				'button'  => __( 'Recount terms', 'woocommerce' ),
