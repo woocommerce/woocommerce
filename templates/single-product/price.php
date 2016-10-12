a42d0218079e963801a42b4bde2849a3fd145c8b
Mike Jolley <mike.jolley@me.com>
2016-10-12 11:54:34 +0100

    We can still include empty meta here - change not needed

--- templates/single-product/price.php
+++ templates/single-product/price.php
@@ -27,10 +27,7 @@ global $product;
 
 	<p class="price"><?php echo $product->get_price_html(); ?></p>
 
-	<?php if ($product->get_display_price()): ?>
-		<meta itemprop="price" content="<?php echo esc_attr( $product->get_display_price() ); ?>" />
-		<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
-	<?php endif; ?>
+	<meta itemprop="price" content="<?php echo esc_attr( $product->get_display_price() ); ?>" />
+	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
 	<link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
-
 </div>
