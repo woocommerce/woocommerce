e9f4986d1bac70eab6ae64c8b4516d1caff75ebe
Rami Yushuvaev <r_a_m_i@hotmail.com>
2016-10-13 22:02:49 +0300

    Fix broken sprintf()

--- includes/wc-cart-functions.php
+++ includes/wc-cart-functions.php
@@ -244,7 +244,7 @@ function wc_cart_totals_coupon_label( $coupon, $echo = true ) {
 		$coupon = new WC_Coupon( $coupon );
 	}
 
-	$label = apply_filters( 'woocommerce_cart_totals_coupon_label', sprintf( esc_html( __( 'Coupon: %s', 'woocommerce' ), $coupon->get_code() ) ), $coupon );
+	$label = apply_filters( 'woocommerce_cart_totals_coupon_label', sprintf( esc_html__( 'Coupon: %s', 'woocommerce' ), $coupon->get_code() ), $coupon );
 
 	if ( $echo ) {
 		echo $label;
