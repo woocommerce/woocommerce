432d7365205782b90e615533e826c89685218ace
Justin Shreve <justin.shreve@gmail.com>
2016-10-17 13:22:38 -0700

    Clean up the abstract product class a bit, deprecate two functions we have renamed, make update & create work properly, and add some tests for it.

---
+++ tests/unit-tests/product/crud.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * CRUD Functions.
+ * @package WooCommerce\Tests\Product
+ * @since 2.7.0
+ */
+class WC_Tests_Product_CRUD extends WC_Unit_Test_Case {
+
+	/**
+	 * Test creating a new product.
+	 *
+	 * @since 2.7.0
+	 */
+	 function test_product_create() {
+		 $product = new WC_Product;
+		 $product->set_regular_price( 42 );
+		 $product->set_name( 'My Product' );
+		 $product->create();
+
+		 $read_product = new WC_Product( $product->get_id() );
+
+		 $this->assertEquals( '42', $read_product->get_regular_price() );
+		 $this->assertEquals( 'My Product', $read_product->get_name() );
+	 }
+
+	/**
+	 * Test reading a product.
+	 *
+	 * @since 2.7.0
+	 */
+	function test_product_read() {
+		$product = WC_Helper_Product::create_simple_product();
+		$product = new WC_Product( $product->get_id() );
+
+		$this->assertEquals( '10', $product->get_regular_price() );
+	}
+
+	/**
+	 * Test updating a product.
+	 *
+	 * @since 2.7.0
+	 */
+	function test_product_update() {
+		$product = WC_Helper_Product::create_simple_product();
+
+		$this->assertEquals( '10', $product->get_regular_price() );
+
+		$product->set_regular_price( 15 );
+		$product->save();
+
+		// Reread from database
+		$product = new WC_Product( $product->get_id() );
+
+		$this->assertEquals( '15', $product->get_regular_price() );
+	}
+
+	/**
+	 * Test deleting a product.
+	 *
+	 * @since 2.7.0
+	 */
+	function test_product_delete() {
+		$product = WC_Helper_Product::create_simple_product();
+		$product->delete();
+		$this->assertEquals( 0, $product->get_id() );
+	}
+
+	/**
+	 * Test product setters and getters
+	 * @todo needs tests for tags, categories, and attributes
+	 * @since 2.7.0
+	 */
+	 public function test_product_getters_and_setters() {
+		 $getters_and_setters = array(
+			 'name'               => 'Test',
+			 'slug'               => 'test',
+			 'status'             => 'publish',
+			 'catalog_visibility' => 'search',
+			 'featured'           => false,
+			 'description'        => 'Hello world',
+			 'short_description'  => 'hello',
+			 'sku'                => 'TEST SKU',
+			 'regular_price'      => 15.00,
+			 'sale_price'         => 10.00,
+			 'date_on_sale_from'  => '1475798400',
+			 'date_on_sale_to'    => '1477267200',
+			 'total_sales'        => 20,
+			 'tax_status'         => 'none',
+			 'tax_class'          => '',
+			 'manage_stock'       => true,
+			 'stock_quantity'     => 10,
+			 'stock_status'       => 'instock',
+			 'backorders'         => 'notify',
+			 'sold_individually'  => false,
+			 'weight'             => 100,
+			 'length'             => 10,
+			 'width'              => 10,
+			 'height'             => 10,
+			 'upsell_ids'         => array( 2, 3 ),
+			 'cross_sell_ids'     => array( 4, 5 ),
+			 'parent_id'          => 0,
+			 'reviews_allowed'    => true,
+			 'default_attributes' => array(),
+			 'purchase_note'      => 'A note',
+			 'menu_order'         => 2,
+		 );
+		 $product = new WC_Product;
+		  foreach ( $getters_and_setters as $function => $value ) {
+			 $product->{"set_{$function}"}( $value );
+		 }
+		 $product->create();
+		 $product = new WC_Product_Simple( $product->get_id() );
+		 foreach ( $getters_and_setters as $function => $value ) {
+			$this->assertEquals( $value, $product->{"get_{$function}"}(), $function );
+		}
+	 }
+}
