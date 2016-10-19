ec65084451e398da18d3ead84347d44b54593dec
Mike Jolley <mike.jolley@me.com>
2016-10-19 12:55:22 +0100

    [2.6] Send processing email on on-hold to processing transition.
    Closes #12117

--- includes/emails/class-wc-email-customer-processing-order.php
+++ includes/emails/class-wc-email-customer-processing-order.php
@@ -33,6 +33,7 @@ class WC_Email_Customer_Processing_Order extends WC_Email {
 		$this->template_plain   = 'emails/plain/customer-processing-order.php';
 
 		// Triggers for this email
+		add_action( 'woocommerce_order_status_on-hold_to_processing_notification', array( $this, 'trigger' ) );
 		add_action( 'woocommerce_order_status_pending_to_processing_notification', array( $this, 'trigger' ) );
 
 		// Call parent constructor
