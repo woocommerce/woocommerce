7a3f901b727d3784677e276aa9b629988e8bba55
Justin Shreve <justin.shreve@gmail.com>
2016-11-08 02:07:13 -0800

    Implement a Coupon Data Store using CPTs..

--- woocommerce.php
+++ woocommerce.php
@@ -285,8 +285,10 @@ final class WooCommerce {
 		include_once( WC_ABSPATH . 'includes/class-wc-https.php' ); // https Helper
 
 		include_once( WC_ABSPATH . 'includes/class-wc-data-store.php' ); // WC_Data_Store for CRUD
-		include_once( WC_ABSPATH . 'includes/data-stores/interface-wc-object-data-store.php' );
+		include_once( WC_ABSPATH . 'includes/data-stores/interfaces/interface-wc-object-data-store.php' );
+		include_once( WC_ABSPATH . 'includes/data-stores/interfaces/interface-wc-coupon-data-store.php' );
 		include_once( WC_ABSPATH . 'includes/data-stores/class-wc-data-store-cpt.php' );
+		include_once( WC_ABSPATH . 'includes/data-stores/class-wc-coupon-data-store-cpt.php' );
 
 		if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			include_once( WC_ABSPATH . 'includes/class-wc-cli.php' );
