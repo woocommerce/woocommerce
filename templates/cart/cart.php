2a851a7c8738caaf4feded24151d417b7faa50fa
Mike Jolley <mike.jolley@me.com>
2016-11-11 15:57:37 +0000

    Rebase fail

--- templates/cart/cart.php
+++ templates/cart/cart.php
@@ -27,7 +27,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 <form class="woocommerce-cart-form" action="<?php echo esc_url( wc_get_cart_url() ); ?>" method="post">
 	<?php do_action( 'woocommerce_before_cart_table' ); ?>
 
-	<table class="shop_table shop_table_responsive cart" cellspacing="0">
+	<table class="shop_table shop_table_responsive cart woocommerce-cart-form__contents" cellspacing="0">
 		<thead>
 			<tr>
 				<th class="product-remove">&nbsp;</th>
@@ -122,7 +122,8 @@ do_action( 'woocommerce_before_cart' ); ?>
 							?>
 						</td>
 					</tr>
-				<?php
+					<?php
+				}
 			}
 			?>
 
