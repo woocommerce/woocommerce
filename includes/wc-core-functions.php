9dc30a72032d7ad8cfa83ad86918e8197d86c07b
Jon Surrell <jon.surrell@gmail.com>
2016-11-13 20:42:45 +0100

    Remove 2nd filter parameter that would never be called

--- includes/wc-core-functions.php
+++ includes/wc-core-functions.php
@@ -1424,19 +1424,18 @@ function wc_get_logger() {
 }
 
 /**
- * Registers the included file log handler.
+ * Registers the included file log handler with threshold 'info'.
  *
  * @since 2.8
  * @param array $handlers
- * @param string $threshold Optional. Threshold for log handler. Defaults to 'info'.
  * @return array
  */
-function wc_register_file_log_handler( $handlers, $threshold = 'info' ) {
+function wc_register_file_log_handler( $handlers ) {
 	if ( ! class_exists( 'WC_Log_Handler_File' ) ) {
 		include_once( dirname( __FILE__ ) . '/log-handlers/class-wc-log-handler-file.php' );
 	}
 
-	array_push( $handlers, new WC_Log_Handler_File( array( 'threshold' => $threshold ) ) );
+	array_push( $handlers, new WC_Log_Handler_File( array( 'threshold' => 'info' ) ) );
 
 	return $handlers;
 }
