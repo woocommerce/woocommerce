ee047ac555e74ae757dd587d2b00947d47e0a0f9
Mike Jolley <mike.jolley@me.com>
2016-11-15 10:36:06 +0000

    args is not used any more - remove todo

--- includes/data-stores/class-wc-product-variable-data-store-cpt.php
+++ includes/data-stores/class-wc-product-variable-data-store-cpt.php
@@ -196,7 +196,7 @@ class WC_Product_Variable_Data_Store_CPT extends WC_Product_Data_Store_CPT imple
 				$sale_prices    = array();
 				$variation_ids  = $product->get_visible_children();
 				foreach ( $variation_ids as $variation_id ) {
-					if ( $variation = wc_get_product( $variation_id ) ) { // @todo loop from missing args? remember?
+					if ( $variation = wc_get_product( $variation_id ) ) {
 						$price         = apply_filters( 'woocommerce_variation_prices_price', $variation->get_price(), $variation, $product );
 						$regular_price = apply_filters( 'woocommerce_variation_prices_regular_price', $variation->get_regular_price(), $variation, $product );
 						$sale_price    = apply_filters( 'woocommerce_variation_prices_sale_price', $variation->get_sale_price(), $variation, $product );
