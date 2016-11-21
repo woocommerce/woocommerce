4de1f1c30317fd644e19278763f0f211e185f505
Mike Jolley <mike.jolley@me.com>
2016-11-21 12:14:57 +0000

    Fixed pagination when removing layered nav items via widget.
    Closes #12394

--- includes/widgets/class-wc-widget-layered-nav-filters.php
+++ includes/widgets/class-wc-widget-layered-nav-filters.php
@@ -35,10 +35,76 @@ class WC_Widget_Layered_Nav_Filters extends WC_Widget {
 	}
 
 	/**
+	 * Get current page URL for layered nav items.
+	 *
+	 * @return string
+	 */
+	protected function get_page_base_url() {
+		if ( defined( 'SHOP_IS_ON_FRONT' ) ) {
+			$link = home_url();
+		} elseif ( is_post_type_archive( 'product' ) || is_page( wc_get_page_id( 'shop' ) ) ) {
+			$link = get_post_type_archive_link( 'product' );
+		} elseif ( is_product_category() ) {
+			$link = get_term_link( get_query_var( 'product_cat' ), 'product_cat' );
+		} elseif ( is_product_tag() ) {
+			$link = get_term_link( get_query_var( 'product_tag' ), 'product_tag' );
+		} else {
+			$queried_object = get_queried_object();
+			$link = get_term_link( $queried_object->slug, $queried_object->taxonomy );
+		}
+
+		// Min/Max
+		if ( isset( $_GET['min_price'] ) ) {
+			$link = add_query_arg( 'min_price', wc_clean( $_GET['min_price'] ), $link );
+		}
+
+		if ( isset( $_GET['max_price'] ) ) {
+			$link = add_query_arg( 'max_price', wc_clean( $_GET['max_price'] ), $link );
+		}
+
+		// Orderby
+		if ( isset( $_GET['orderby'] ) ) {
+			$link = add_query_arg( 'orderby', wc_clean( $_GET['orderby'] ), $link );
+		}
+
+		/**
+		 * Search Arg.
+		 * To support quote characters, first they are decoded from &quot; entities, then URL encoded.
+		 */
+		if ( get_search_query() ) {
+			$link = add_query_arg( 's', rawurlencode( htmlspecialchars_decode( get_search_query() ) ), $link );
+		}
+
+		// Post Type Arg
+		if ( isset( $_GET['post_type'] ) ) {
+			$link = add_query_arg( 'post_type', wc_clean( $_GET['post_type'] ), $link );
+		}
+
+		// Min Rating Arg
+		if ( isset( $_GET['min_rating'] ) ) {
+			$link = add_query_arg( 'min_rating', wc_clean( $_GET['min_rating'] ), $link );
+		}
+
+		// All current filters
+		if ( $_chosen_attributes = WC_Query::get_layered_nav_chosen_attributes() ) {
+			foreach ( $_chosen_attributes as $name => $data ) {
+				$filter_name = sanitize_title( str_replace( 'pa_', '', $name ) );
+				if ( ! empty( $data['terms'] ) ) {
+					$link = add_query_arg( 'filter_' . $filter_name, implode( ',', $data['terms'] ), $link );
+				}
+				if ( 'or' == $data['query_type'] ) {
+					$link = add_query_arg( 'query_type_' . $filter_name, 'or', $link );
+				}
+			}
+		}
+
+		return $link;
+	}
+
+	/**
 	 * Output widget.
 	 *
 	 * @see WP_Widget
-	 *
 	 * @param array $args
 	 * @param array $instance
 	 */
@@ -48,9 +114,10 @@ class WC_Widget_Layered_Nav_Filters extends WC_Widget {
 		}
 
 		$_chosen_attributes = WC_Query::get_layered_nav_chosen_attributes();
-		$min_price          = isset( $_GET['min_price'] ) ? wc_clean( $_GET['min_price'] )   : 0;
-		$max_price          = isset( $_GET['max_price'] ) ? wc_clean( $_GET['max_price'] )   : 0;
+		$min_price          = isset( $_GET['min_price'] ) ? wc_clean( $_GET['min_price'] ) : 0;
+		$max_price          = isset( $_GET['max_price'] ) ? wc_clean( $_GET['max_price'] ) : 0;
 		$min_rating         = isset( $_GET['min_rating'] ) ? absint( $_GET['min_rating'] ) : 0;
+		$base_link          = $this->get_page_base_url();
 
 		if ( 0 < count( $_chosen_attributes ) || 0 < $min_price || 0 < $max_price || 0 < $min_rating ) {
 
@@ -71,7 +138,7 @@ class WC_Widget_Layered_Nav_Filters extends WC_Widget {
 						$current_filter = array_map( 'sanitize_title', $current_filter );
 						$new_filter      = array_diff( $current_filter, array( $term_slug ) );
 
-						$link = remove_query_arg( array( 'add-to-cart', $filter_name ) );
+						$link = remove_query_arg( array( 'add-to-cart', $filter_name ), $base_link );
 
 						if ( sizeof( $new_filter ) > 0 ) {
 							$link = add_query_arg( $filter_name, implode( ',', $new_filter ), $link );
@@ -83,17 +150,17 @@ class WC_Widget_Layered_Nav_Filters extends WC_Widget {
 			}
 
 			if ( $min_price ) {
-				$link = remove_query_arg( 'min_price' );
+				$link = remove_query_arg( 'min_price', $base_link );
 				echo '<li class="chosen"><a title="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" href="' . esc_url( $link ) . '">' . __( 'Min', 'woocommerce' ) . ' ' . wc_price( $min_price ) . '</a></li>';
 			}
 
 			if ( $max_price ) {
-				$link = remove_query_arg( 'max_price' );
+				$link = remove_query_arg( 'max_price', $base_link );
 				echo '<li class="chosen"><a title="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" href="' . esc_url( $link ) . '">' . __( 'Max', 'woocommerce' ) . ' ' . wc_price( $max_price ) . '</a></li>';
 			}
 
 			if ( $min_rating ) {
-				$link = remove_query_arg( 'min_rating' );
+				$link = remove_query_arg( 'min_rating', $base_link );
 				echo '<li class="chosen"><a title="' . esc_attr__( 'Remove filter', 'woocommerce' ) . '" href="' . esc_url( $link ) . '">' . sprintf( __( 'Rated %s and above', 'woocommerce' ), $min_rating ) . '</a></li>';
 			}
 
