f419ca8098c66e9207dd65ebd95b48fd475568de
Mike Jolley <mike.jolley@me.com>
2016-12-07 13:45:17 +0000

    Only return empty string if empty for weight and dimension functions
    Closes #12455

--- includes/abstracts/abstract-wc-product.php
+++ includes/abstracts/abstract-wc-product.php
@@ -1462,7 +1462,7 @@ class WC_Product {
 	 * @return string
 	 */
 	public function get_length() {
-		return apply_filters( 'woocommerce_product_length', $this->length ? $this->length : '', $this );
+		return apply_filters( 'woocommerce_product_length', '' === $this->length ? '' : wc_format_decimal( $this->length ), $this );
 	}
 
 	/**
@@ -1470,7 +1470,7 @@ class WC_Product {
 	 * @return string
 	 */
 	public function get_width() {
-		return apply_filters( 'woocommerce_product_width', $this->width ? $this->width : '', $this );
+		return apply_filters( 'woocommerce_product_width', '' === $this->width ? '' : wc_format_decimal( $this->width ), $this );
 	}
 
 	/**
@@ -1478,7 +1478,7 @@ class WC_Product {
 	 * @return string
 	 */
 	public function get_height() {
-		return apply_filters( 'woocommerce_product_height', $this->height ? $this->height : '', $this );
+		return apply_filters( 'woocommerce_product_height', '' === $this->height ? '' : wc_format_decimal( $this->height ), $this );
 	}
 
 	/**
@@ -1487,7 +1487,7 @@ class WC_Product {
 	 * @return string
 	 */
 	public function get_weight() {
-		return apply_filters( 'woocommerce_product_weight', apply_filters( 'woocommerce_product_get_weight', $this->weight ? $this->weight : '' ), $this );
+		return apply_filters( 'woocommerce_product_weight', apply_filters( 'woocommerce_product_get_weight', '' === $this->weight ? '' : wc_format_decimal( $this->weight ) ), $this );
 	}
 
 	/**
