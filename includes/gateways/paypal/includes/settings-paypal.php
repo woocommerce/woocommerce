fee3c47a89bdb906135186a06c1dd35574c331de
Jon Surrell <jon.surrell@gmail.com>
2016-12-19 22:20:56 +0100

    Update use of deprecated wc_get_log_file_path

--- includes/gateways/paypal/includes/settings-paypal.php
+++ includes/gateways/paypal/includes/settings-paypal.php
@@ -48,7 +48,7 @@ return array(
 		'type'        => 'checkbox',
 		'label'       => __( 'Enable logging', 'woocommerce' ),
 		'default'     => 'no',
-		'description' => sprintf( __( 'Log PayPal events, such as IPN requests, inside %s', 'woocommerce' ), '<code>' . wc_get_log_file_path( 'paypal' ) . '</code>' ),
+		'description' => sprintf( __( 'Log PayPal events, such as IPN requests, inside %s', 'woocommerce' ), '<code>' . WC_Log_Handler_File::get_log_file_path( 'paypal' ) . '</code>' ),
 	),
 	'advanced' => array(
 		'title'       => __( 'Advanced options', 'woocommerce' ),
