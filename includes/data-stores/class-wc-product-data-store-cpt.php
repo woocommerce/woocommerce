b749b654bcfc884dced7128759b6beff688f63c8
Mike Jolley <mike.jolley@me.com>
2017-01-11 11:48:31 +0000

    Fire action on type change

--- includes/data-stores/class-wc-product-data-store-cpt.php
+++ includes/data-stores/class-wc-product-data-store-cpt.php
@@ -586,8 +586,16 @@ class WC_Product_Data_Store_CPT extends WC_Data_Store_WP implements WC_Object_Da
 	 * @since 2.7.0
 	 */
 	protected function update_version_and_type( &$product ) {
-		wp_set_object_terms( $product->get_id(), $product->get_type(), 'product_type' );
+		$old_type = WC_Product_Factory::get_product_type( $product->get_id() );
+		$new_type = $product->get_type();
+
+		wp_set_object_terms( $product->get_id(), $new_type, 'product_type' );
 		update_post_meta( $product->get_id(), '_product_version', WC_VERSION );
+
+		// Action for the transition.
+		if ( $old_type !== $new_type ) {
+			do_action( 'woocommerce_product_type_changed', $product, $old_type, $new_type );
+		}
 	}
 
 	/**
