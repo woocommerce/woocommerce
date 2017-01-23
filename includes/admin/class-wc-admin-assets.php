41a937041dffdb89913b9f54aab593e2ed9a8b21
pierrebuet <pierrebuet@mac.com>
2017-01-23 22:04:52 +0100

    Updating the way select2 v4 loads translated strings
    Correcting Issue #12952.
    
    In select2 v4 the callback functions used to format the messages have
    changed their names and the structure of the options have changed,
    using now a „language“ object.
    
    I was not able to find a reference in the select2 v4 code how to format
    the number of matches back. Further when using v4 I don’t see any
    indication on the number of the number of returned matches, so I
    consider the parameter as obsolete.

--- includes/admin/class-wc-admin-assets.php
+++ includes/admin/class-wc-admin-assets.php
@@ -109,8 +109,6 @@ class WC_Admin_Assets {
 		wp_register_script( 'select2', WC()->plugin_url() . '/assets/js/select2/select2.full' . $suffix . '.js', array( 'jquery' ), '4.0.3' );
 		wp_register_script( 'wc-enhanced-select', WC()->plugin_url() . '/assets/js/admin/wc-enhanced-select' . $suffix . '.js', array( 'jquery', 'select2' ), WC_VERSION );
 		wp_localize_script( 'wc-enhanced-select', 'wc_enhanced_select_params', array(
-			'i18n_matches_1'            => _x( 'One result is available, press enter to select it.', 'enhanced select', 'woocommerce' ),
-			'i18n_matches_n'            => _x( '%qty% results are available, use up and down arrow keys to navigate.', 'enhanced select', 'woocommerce' ),
 			'i18n_no_matches'           => _x( 'No matches found', 'enhanced select', 'woocommerce' ),
 			'i18n_ajax_error'           => _x( 'Loading failed', 'enhanced select', 'woocommerce' ),
 			'i18n_input_too_short_1'    => _x( 'Please enter 1 or more characters', 'enhanced select', 'woocommerce' ),
