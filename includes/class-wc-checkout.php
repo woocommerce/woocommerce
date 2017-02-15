9b7395705abd8d57fbc254fb5e08ea1d87c9bec5
Mike Jolley <mike.jolley@me.com>
2017-02-15 17:07:03 +0000

    Fix checkout $order property

--- includes/class-wc-checkout.php
+++ includes/class-wc-checkout.php
@@ -921,18 +921,19 @@ class WC_Checkout {
 
 			if ( empty( $posted_data['woocommerce_checkout_update_totals'] ) && 0 === wc_notice_count( 'error' ) ) {
 				$this->process_customer( $posted_data );
-				$order = $this->create_order( $posted_data );
+				$order_id = $this->create_order( $posted_data );
+				$order    = wc_get_order( $order_id );
 
-				if ( is_wp_error( $order ) ) {
-					throw new Exception( $order->get_error_message() );
+				if ( is_wp_error( $order_id ) ) {
+					throw new Exception( $order_id->get_error_message() );
 				}
 
-				do_action( 'woocommerce_checkout_order_processed', $order->get_id(), $posted_data, $order );
+				do_action( 'woocommerce_checkout_order_processed', $order_id, $posted_data, $order );
 
 				if ( WC()->cart->needs_payment() ) {
-					$this->process_order_payment( $order, $posted_data['payment_method'] );
+					$this->process_order_payment( $order_id, $posted_data['payment_method'] );
 				} else {
-					$this->process_order_without_payment( $order );
+					$this->process_order_without_payment( $order_id );
 				}
 			}
 		} catch ( Exception $e ) {
