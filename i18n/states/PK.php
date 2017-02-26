51d2f80cb2068f9a6c72bd6024d871f24897e97f
Anwer Abdul Rehman <AnwerAR@users.noreply.github.com>
2017-02-26 23:50:51 +0500

    List of States in Pakistan
    Added PK.php in states for Pakistani states. all province codes are accoring to ISO 3166 Standard

---
+++ i18n/states/PK.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Pakistan's states
+ *
+ * @author      WooThemes
+ * @category    i18n
+ * @package     WooCommerce/i18n
+ * @version     2.0.0
+ */
+global $states;
+
+$states['PK'] = array(
+	'BA' => __( 'Balochistan', 'woocommerce' ),
+	'PB' => __( 'Punjab', 'woocommerce' ),
+	'SD' => __( 'Sindh', 'woocommerce' ),
+	'KP' => __( 'Khyber Pakhtunkhwa', 'woocommerce' ),
+	'GB' => __( 'Gilgit Baltistan', 'woocommerce' ),
+	'TA' => __( 'FATA', 'woocommerce' ),
+	'JK' => __( 'Azad Kashmir', 'woocommerce' ),
+	'IS' => __( 'Islamabad Capital Territory', 'woocommerce' ),
+);
