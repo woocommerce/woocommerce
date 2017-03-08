94eaa67bc9888cf74555b56e6ae2fe567d7e325b
Mike Jolley <mike.jolley@me.com>
2017-03-08 18:14:37 +0000

    to string method

--- includes/class-wc-datetime.php
+++ includes/class-wc-datetime.php
@@ -8,6 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * WC Wrapper for PHP DateTime.
  *
  * @class    WC_DateTime
+ * @since    2.7.0
  * @package  WooCommerce/Classes
  * @category Class
  * @author   WooThemes
@@ -15,7 +16,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 class WC_DateTime extends DateTime {
 
 	/**
+	 * Output an ISO 8601 date string in local timezone.
+	 *
+	 * @since  2.7.0
+	 * @return string
+	 */
+	public function __toString() {
+		return $this->format( DATE_ATOM );
+	}
+
+	/**
 	 * Missing in PHP 5.2.
+	 *
+	 * @since  2.7.0
 	 * @return int
 	 */
 	public function getTimestamp() {
@@ -24,6 +37,8 @@ class WC_DateTime extends DateTime {
 
 	/**
 	 * Get the timestamp with the WordPress timezone offset added or subtracted.
+	 *
+	 * @since  2.7.0
 	 * @return int
 	 */
 	public function getOffsetTimestamp() {
