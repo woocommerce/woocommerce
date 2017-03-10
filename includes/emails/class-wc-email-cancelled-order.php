05e6c506cebb00f01c671943c1d2fb897cefe31a
Mike Jolley <mike.jolley@me.com>
2017-03-10 16:29:15 +0000

    Email dates

--- includes/emails/class-wc-email-cancelled-order.php
+++ includes/emails/class-wc-email-cancelled-order.php
@@ -57,7 +57,7 @@ class WC_Email_Cancelled_Order extends WC_Email {
 			$this->object                  = $order;
 			$this->find['order-date']      = '{order_date}';
 			$this->find['order-number']    = '{order_number}';
-			$this->replace['order-date']   = date_i18n( wc_date_format(), $this->object->get_date_created() );
+			$this->replace['order-date']   = wc_format_datetime( $this->object->get_date_created() );
 			$this->replace['order-number'] = $this->object->get_order_number();
 		}
 
