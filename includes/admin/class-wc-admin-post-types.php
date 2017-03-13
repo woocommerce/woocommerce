9b7c08fe8faa253fb84aeafeb372890924d19cc4
Claudiu Lodromanean <claudiu@Claudius-MacBook-Pro.local>
2017-03-13 15:16:03 -0700

    Update post type page with new date

--- includes/admin/class-wc-admin-post-types.php
+++ includes/admin/class-wc-admin-post-types.php
@@ -505,7 +505,7 @@ class WC_Admin_Post_Types {
 				$expiry_date = $the_coupon->get_date_expires();
 
 				if ( $expiry_date ) {
-					echo esc_html( date_i18n( 'F j, Y', $expiry_date ) );
+					echo esc_html( date_i18n( 'F j, Y', $expiry_date->getTimestamp() ) );
 				} else {
 					echo '&ndash;';
 				}
