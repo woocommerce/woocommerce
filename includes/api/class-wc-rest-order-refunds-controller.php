7485b418d7cde43ecd98f253e435899071a40a62
Claudio Sanches <contato@claudiosanches.com>
2017-03-22 10:39:23 -0300

    Improve api_refund description

--- includes/api/class-wc-rest-order-refunds-controller.php
+++ includes/api/class-wc-rest-order-refunds-controller.php
@@ -525,7 +525,7 @@ class WC_REST_Order_Refunds_Controller extends WC_REST_Orders_Controller {
 					),
 				),
 				'api_refund' => array(
-					'description' => __( 'When true uses the payment gateway API to generate the refund.', 'woocommerce' ),
+					'description' => __( 'When true, the payment gateway API is used to generate the refund.', 'woocommerce' ),
 					'type'        => 'boolean',
 					'context'     => array( 'edit' ),
 					'default'     => true,
