54c353d67fc95cd59e0ddd7d652c6d9f87ae8817
Claudio Sanches <contato@claudiosanches.com>
2017-03-28 14:34:41 -0300

    [REST API] Fixed taxonomies query arguments.
    Fixed the follow error:
    
    PHP Warning: explode() expects parameter 2 to be string, array given in
    includes/api/v1/class-wc-rest-products-controller.php on line 156
    
    Error caused since terms are sanitized by wp_parse_id_list() that
    already creates an array.
    
    Closes #13790

--- includes/api/v1/class-wc-rest-products-controller.php
+++ includes/api/v1/class-wc-rest-products-controller.php
@@ -152,8 +152,6 @@ class WC_REST_Products_V1_Controller extends WC_REST_Posts_Controller {
 		// Set tax_query for each passed arg.
 		foreach ( $taxonomies as $taxonomy => $key ) {
 			if ( ! empty( $request[ $key ] ) ) {
-				$terms = explode( ',', $request[ $key ] );
-
 				$tax_query[] = array(
 					'taxonomy' => $taxonomy,
 					'field'    => 'term_id',
