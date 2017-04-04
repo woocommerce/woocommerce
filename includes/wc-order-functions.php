505813403e39387fdf4d6e6fc9c9a14c4bd6f9ae
Mike Jolley <mike.jolley@me.com>
2017-04-04 11:52:54 +0100

    @used-by WC_Order::set_status comment
    #13891

--- includes/wc-order-functions.php
+++ includes/wc-order-functions.php
@@ -104,6 +104,7 @@ function wc_get_order( $the_order = false ) {
  * Get all order statuses.
  *
  * @since 2.2
+ * @used-by WC_Order::set_status
  * @return array
  */
 function wc_get_order_statuses() {
