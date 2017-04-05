ab4c921bd1b2745250382da58b395dee5bb5f937
Mike Jolley <mike.jolley@me.com>
2017-04-05 16:36:28 +0100

    Wrap in try catch so WC_Data_Exceptions are caught on bad email.

--- includes/data-stores/class-wc-customer-data-store-session.php
+++ includes/data-stores/class-wc-customer-data-store-session.php
@@ -109,28 +109,30 @@ class WC_Customer_Data_Store_Session extends WC_Data_Store_WP implements WC_Cust
 	 * @param WC_Customer
 	 */
 	protected function set_defaults( &$customer ) {
-		$default = wc_get_customer_default_location();
+		try {
+			$default = wc_get_customer_default_location();
 
-		if ( ! $customer->get_billing_country() ) {
-			$customer->set_billing_country( $default['country'] );
-		}
+			if ( ! $customer->get_billing_country() ) {
+				$customer->set_billing_country( $default['country'] );
+			}
 
-		if ( ! $customer->get_shipping_country() ) {
-			$customer->set_shipping_country( $customer->get_billing_country() );
-		}
+			if ( ! $customer->get_shipping_country() ) {
+				$customer->set_shipping_country( $customer->get_billing_country() );
+			}
 
-		if ( ! $customer->get_billing_state() ) {
-			$customer->set_billing_state( $default['state'] );
-		}
+			if ( ! $customer->get_billing_state() ) {
+				$customer->set_billing_state( $default['state'] );
+			}
 
-		if ( ! $customer->get_shipping_state() ) {
-			$customer->set_shipping_state( $customer->get_billing_state() );
-		}
+			if ( ! $customer->get_shipping_state() ) {
+				$customer->set_shipping_state( $customer->get_billing_state() );
+			}
 
-		if ( ! $customer->get_billing_email() && is_user_logged_in() ) {
-			$current_user = wp_get_current_user();
-			$customer->set_billing_email( $current_user->user_email );
-		}
+			if ( ! $customer->get_billing_email() && is_user_logged_in() ) {
+				$current_user = wp_get_current_user();
+				$customer->set_billing_email( $current_user->user_email );
+			}
+		} catch ( WC_Data_Exception $e ) {}
 	}
 
 	/**
