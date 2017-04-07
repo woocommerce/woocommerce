3e0daa8215b10627d50a35d6533b1164a40fb991
refael iliaguyev <rellect@gmail.com>
2017-04-07 14:22:52 +0300

    Bump template versions for changes

--- templates/emails/email-addresses.php
+++ templates/emails/email-addresses.php
@@ -13,7 +13,7 @@
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates/Emails
- * @version     2.4.0
+ * @version     3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
