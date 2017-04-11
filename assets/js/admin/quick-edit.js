9a8d91432632adf20925b416248582c35023d407
Mike Jolley <mike.jolley@me.com>
2017-04-11 21:45:31 +0100

    Remove error causing legacy suggest code
    Fixes #14120

--- assets/js/admin/quick-edit.js
+++ assets/js/admin/quick-edit.js
@@ -104,10 +104,6 @@ jQuery(function( $ ) {
 		$( 'input.text', '.inline-edit-row' ).val( '' );
 		$( '#woocommerce-fields' ).find( 'select' ).prop( 'selectedIndex', 0 );
 		$( '#woocommerce-fields-bulk' ).find( '.inline-edit-group .change-input' ).hide();
-
-		// Autosuggest product tags on bulk edit
-		var tax = 'product_tag';
-		$( 'tr.inline-editor textarea[name="tax_input[' + tax + ']"]' ).suggest( ajaxurl + ( ajaxurl.indexOf( '?' ) > 0 ? '&' : '?' ) + 'action=ajax-tag-search&tax=' + tax, { delay: 500, minchars: 2, multiple: true, multipleSep: inlineEditL10n.comma } );
 	});
 
 	$( '#wpbody' ).on( 'change', '#woocommerce-fields-bulk .inline-edit-group .change_to', function() {
