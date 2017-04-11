98b21b6be7bb2e186bce6dbb6cc5c96547af0556
Mike Jolley <mike.jolley@me.com>
2017-04-11 12:08:04 +0100

    Fix caption on thumbnails as well as main image

--- templates/single-product/product-image.php
+++ templates/single-product/product-image.php
@@ -24,8 +24,7 @@ global $post, $product;
 $columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
 $post_thumbnail_id = get_post_thumbnail_id( $post->ID );
 $full_size_image   = wp_get_attachment_image_src( $post_thumbnail_id, 'full' );
-$thumbnail_post    = get_post( $post_thumbnail_id );
-$image_title       = $thumbnail_post->post_excerpt;
+$image_title       = get_post_field( 'post_excerpt', $post_thumbnail_id );
 $placeholder       = has_post_thumbnail() ? 'with-images' : 'without-images';
 $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_classes', array(
 	'woocommerce-product-gallery',
