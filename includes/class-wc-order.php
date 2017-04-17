99d1a724ae62f515b2c75beae7bcb2d86e668894
Matt Cohen <hello@matty.co.za>
2017-04-17 11:15:30 +0200

    Removes the first name, last name, and company from the shipping address map URL. Fixes #14398

--- includes/class-wc-order.php
+++ includes/class-wc-order.php
@@ -766,6 +766,11 @@ class WC_Order extends WC_Abstract_Order {
 	 */
 	public function get_shipping_address_map_url() {
 		$address = apply_filters( 'woocommerce_shipping_address_map_url_parts', $this->get_address( 'shipping' ), $this );
+		foreach ( array( 'first_name', 'last_name', 'company' ) as $key ) {
+			if ( isset( $address[$key] ) ) {
+				unset( $address[$key] );
+			}
+		}
 		return apply_filters( 'woocommerce_shipping_address_map_url', 'https://maps.google.com/maps?&q=' . urlencode( implode( ', ', $address ) ) . '&z=16', $this );
 	}
 
