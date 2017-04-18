412cebc813be12092e05e4c7dac625ebed9d8249
Roman Random <roman@romanrandom.com>
2017-04-18 14:31:05 +0200

    Added Bolivian states

---
+++ i18n/states/BO.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Bolivian states
+ *
+ * @author      Roman Random
+ * @category    i18n
+ * @package     WooCommerce/i18n
+ * @version     1.0.0
+ */
+global $states;
+
+$states['BO'] = array(
+	'B' => __( 'Chuquisaca', 'woocommerce' ),
+	'H' => __( 'Beni', 'woocommerce' ),
+	'C' => __( 'Cochabamba', 'woocommerce' ),
+	'L' => __( 'La Paz', 'woocommerce' ),
+	'O' => __( 'Oruro', 'woocommerce' ),
+	'N' => __( 'Pando', 'woocommerce' ),
+	'P' => __( 'Potosí', 'woocommerce' ),
+	'S' => __( 'Santa Cruz', 'woocommerce' ),
+	'T' => __( 'Tarija', 'woocommerce' )
+);
