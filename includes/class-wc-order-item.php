015b2fabea5b2ef169728b61155dff575e8250ac
Mike Jolley <mike.jolley@me.com>
2017-04-19 16:53:31 +0100

    Ensure order is not cached before download permissions are generated

--- includes/class-wc-order-item.php
+++ includes/class-wc-order-item.php
@@ -27,12 +27,6 @@ class WC_Order_Item extends WC_Data implements ArrayAccess {
 	);
 
 	/**
-	 * May store an order to prevent retriving it multiple times.
-	 * @var object
-	 */
-	protected $order;
-
-	/**
 	 * Stores meta in cache for future reads.
 	 * A group must be set to to enable caching.
 	 * @var string
@@ -122,10 +116,7 @@ class WC_Order_Item extends WC_Data implements ArrayAccess {
 	 * @return int
 	 */
 	public function get_order() {
-		if ( ! $this->order ) {
-		 	$this->order = wc_get_order( $this->get_order_id() );
-		}
-		return $this->order;
+		return wc_get_order( $this->get_order_id() );
 	}
 
 	/*
