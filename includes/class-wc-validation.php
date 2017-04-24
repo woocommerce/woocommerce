b49ecde82f950f08a9ba5ad53311d257152ac4aa
Mike Jolley <mike.jolley@me.com>
2017-04-24 15:24:37 +0100

    Allow forward slash in tel
    Closes #14646

--- includes/class-wc-validation.php
+++ includes/class-wc-validation.php
@@ -32,7 +32,7 @@ class WC_Validation {
 	 * @return  bool
 	 */
 	public static function is_phone( $phone ) {
-		if ( 0 < strlen( trim( preg_replace( '/[\s\#0-9_\-\+\(\)]/', '', $phone ) ) ) ) {
+		if ( 0 < strlen( trim( preg_replace( '/[\s\#0-9_\-\+\/\(\)]/', '', $phone ) ) ) ) {
 			return false;
 		}
 
