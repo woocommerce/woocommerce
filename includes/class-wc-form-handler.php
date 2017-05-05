05f3283c75e8babf155e8c2406cef2f61519a082
claudiulodro <claudiulodro@gmail.com>
2017-05-05 09:18:15 -0700

    Remove debug

--- includes/class-wc-form-handler.php
+++ includes/class-wc-form-handler.php
@@ -590,10 +590,8 @@ class WC_Form_Handler {
 				if ( taxonomy_is_product_attribute( $meta->key ) ) {
 					$term = get_term_by( 'slug', $meta->value, $meta->key );
 					$variations[ $meta->key ] = $term ? $term->name : $meta->value;
-					$test['tax'][] = $meta->key;
 				} elseif ( meta_is_product_attribute( $meta->key, $meta->value, $product_id ) ) {
 					$variations[ $meta->key ] = $meta->value;
-					$test['tax'][] = $meta->key;
 				}
 			}
 
