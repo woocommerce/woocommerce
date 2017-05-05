3dd5acfd472fe1d06600b443e827764b753297c8
Jason Judge <jason.judge@consil.co.uk>
2017-05-05 22:44:27 +0100

    Issue #14900 Tidy up code (long lines and commented old lines removed).

--- includes/wc-term-functions.php
+++ includes/wc-term-functions.php
@@ -584,11 +584,13 @@ function _wc_term_recount( $terms, $taxonomy, $callback = true, $terms_are_term_
 	}
 
 	if ( $exclude_term_ids ) {
-		$count_query_select .= " LEFT JOIN ( SELECT object_ID FROM {$wpdb->term_relationships} WHERE term_taxonomy_id IN (" . implode( ',', array_map( 'absint', $exclude_term_ids ) ) . " ) ) AS exclude_join ON exclude_join.object_ID = ID ";
+		$count_query_select .= " LEFT JOIN (
+				SELECT object_ID FROM {$wpdb->term_relationships}
+				WHERE term_taxonomy_id IN (" . implode( ',', array_map( 'absint', $exclude_term_ids ) ) . " )
+			) AS exclude_join
+			ON exclude_join.object_ID = ID ";
 
 		$count_query_where .= " AND exclude_join.object_ID IS NULL ";
-
-		//$count_query_where .= " AND ID NOT IN ( SELECT object_ID FROM {$wpdb->term_relationships} WHERE term_taxonomy_id IN (" . implode( ',', array_map( 'absint', $exclude_term_ids ) ) . " ) ) ";
 	}
 
 	// Pre-process term taxonomy ids.
@@ -635,9 +637,13 @@ function _wc_term_recount( $terms, $taxonomy, $callback = true, $terms_are_term_
 
 		// Generate term query
 		$join_index++;
-		$term_query_join = " INNER JOIN ( SELECT object_ID FROM {$wpdb->term_relationships} LEFT JOIN {$wpdb->term_taxonomy} AS tax USING( term_taxonomy_id ) WHERE term_id IN (" . implode( ',', array_map( 'absint', $terms_to_count ) ) . " ) ) AS include_join{$join_index} ON include_join{$join_index}.object_ID = ID ";
-
-		//$term_query = " AND ID IN ( SELECT object_ID FROM {$wpdb->term_relationships} LEFT JOIN {$wpdb->term_taxonomy} AS tax USING( term_taxonomy_id ) WHERE term_id IN (" . implode( ',', array_map( 'absint', $terms_to_count ) ) . " ) ) ";
+		$term_query_join = " INNER JOIN (
+				SELECT object_ID FROM {$wpdb->term_relationships}
+				LEFT JOIN {$wpdb->term_taxonomy} AS tax
+				USING( term_taxonomy_id )
+				WHERE term_id IN (" . implode( ',', array_map( 'absint', $terms_to_count ) ) . " )
+			) AS include_join{$join_index}
+			ON include_join{$join_index}.object_ID = ID ";
 
 		// Get the count
 		$count = $wpdb->get_var( $count_query_select . $term_query_join . $count_query_where );
