eee2ef82e8c2ec9c2894c9d9a8ccd9e5b23077b2
claudiulodro <claudiulodro@gmail.com>
2017-05-12 13:06:17 -0700

    Deprecate ->get_orders

--- includes/data-stores/class-wc-order-data-store-cpt.php
+++ includes/data-stores/class-wc-order-data-store-cpt.php
@@ -354,74 +354,14 @@ class WC_Order_Data_Store_CPT extends Abstract_WC_Order_Data_Store_CPT implement
 	/**
 	 * Get all orders matching the passed in args.
 	 *
+	 * @deprecated 3.1.0 - Use wc_get_orders instead.
 	 * @see    wc_get_orders()
 	 * @param  array $args
 	 * @return array of orders
 	 */
 	public function get_orders( $args = array() ) {
-		/**
-		 * Generate WP_Query args. This logic will change if orders are moved to
-		 * custom tables in the future.
-		 */
-		$wp_query_args = array(
-			'post_type'      => $args['type'] ? $args['type'] : 'shop_order',
-			'post_status'    => $args['status'],
-			'posts_per_page' => $args['limit'],
-			'meta_query'     => array(),
-			'fields'         => 'ids',
-			'orderby'        => $args['orderby'],
-			'order'          => $args['order'],
-		);
-
-		if ( ! is_null( $args['parent'] ) ) {
-			$wp_query_args['post_parent'] = absint( $args['parent'] );
-		}
-
-		if ( ! is_null( $args['offset'] ) ) {
-			$wp_query_args['offset'] = absint( $args['offset'] );
-		} else {
-			$wp_query_args['paged'] = absint( $args['page'] );
-		}
-
-		if ( isset( $args['customer'] ) && '' !== $args['customer'] ) {
-			$values = is_array( $args['customer'] ) ? $args['customer'] : array( $args['customer'] );
-			$wp_query_args['meta_query'][] = $this->get_orders_generate_customer_meta_query( $values );
-		}
-
-		if ( ! empty( $args['exclude'] ) ) {
-			$wp_query_args['post__not_in'] = array_map( 'absint', $args['exclude'] );
-		}
-
-		if ( ! $args['paginate'] ) {
-			$wp_query_args['no_found_rows'] = true;
-		}
-
-		if ( ! empty( $args['date_before'] ) ) {
-			$wp_query_args['date_query']['before'] = $args['date_before'];
-		}
-
-		if ( ! empty( $args['date_after'] ) ) {
-			$wp_query_args['date_query']['after'] = $args['date_after'];
-		}
-
-		// Get results.
-		$orders = new WP_Query( apply_filters( 'woocommerce_order_data_store_cpt_get_orders_query', $wp_query_args, $args, $this ) );
-
-		if ( 'objects' === $args['return'] ) {
-			$return = array_map( 'wc_get_order', $orders->posts );
-		} else {
-			$return = $orders->posts;
-		}
-
-		if ( $args['paginate'] ) {
-			return (object) array(
-				'orders'        => $return,
-				'total'         => $orders->found_posts,
-				'max_num_pages' => $orders->max_num_pages,
-			);
-		} else {
-			return $return;
-		}
+		wc_deprecated_function( 'WC_Order_Data_Store_CPT::get_orders', '3.1.0', 'Use wc_get_orders instead.' );
+		return wc_get_orders( $args );
 	}
 
 	/**
