73b7b2d91b7fa6f4cd9d4b4f84677a11f325bd3d
claudiulodro <claudiulodro@gmail.com>
2017-05-16 06:31:52 -0700

    Typo

--- includes/emails/class-wc-email.php
+++ includes/emails/class-wc-email.php
@@ -255,7 +255,7 @@ class WC_Email extends WC_Settings_API {
 	/**
 	 * Restore the locale to the default locale. Use after finished with setup_locale.
 	 */
-	public function restore_locale( $email ) {
+	public function restore_locale() {
 		if ( function_exists( 'restore_previous_locale' ) && $this->is_customer_email() ) {
 			restore_previous_locale();
 		}
