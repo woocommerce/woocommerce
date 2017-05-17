2ab0fdfe78b7987eb2b8ed0974ec80ae5b92c708
claudiulodro <claudiulodro@gmail.com>
2017-05-17 14:55:20 -0700

    Styling

--- includes/admin/importers/views/html-csv-import-progress.php
+++ includes/admin/importers/views/html-csv-import-progress.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<h1><?php esc_html_e( 'Importing', 'woocommerce' ); ?></h1>
 
 	<div class="woocommerce-importer-wrapper">
-		<form class="woocommerce-importer">
+		<form class="woocommerce-importer woocommerce-importer__importing">
 			<progress class="woocommerce-importer-progress" max="100" value="0"></progress>
 		</form>
 	</div>
