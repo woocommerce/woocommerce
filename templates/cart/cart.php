50dc533ebb2defb1d17f051172d8f5d32a865cbd
Mike Jolley <mike.jolley@me.com>
2017-05-30 11:03:51 +0100

    Template bump

--- templates/cart/cart.php
+++ templates/cart/cart.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.0.3
+ * @version 3.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
