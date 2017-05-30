3dd7cbf59ba87a64d1edca3f9edd9dee8c8694e7
Mike Jolley <mike.jolley@me.com>
2017-05-30 16:54:37 +0100

    Revert https://github.com/woocommerce/woocommerce/commit/d570a81241f49fb79007eba78bd0ad489d0911c1

--- includes/data-stores/class-wc-customer-data-store-session.php
+++ includes/data-stores/class-wc-customer-data-store-session.php
@@ -95,10 +95,7 @@ class WC_Customer_Data_Store_Session extends WC_Data_Store_WP implements WC_Cust
 					$session_key = str_replace( 'billing_', '', $session_key );
 				}
 				if ( ! empty( $data[ $session_key ] ) && is_callable( array( $customer, "set_{$function_key}" ) ) ) {
-					// Only set from session if data is already missing.
-					if ( ! $customer->{"get_{$function_key}"}() ) {
-						$customer->{"set_{$function_key}"}( $data[ $session_key ] );
-					}
+					$customer->{"set_{$function_key}"}( $data[ $session_key ] );
 				}
 			}
 		}
