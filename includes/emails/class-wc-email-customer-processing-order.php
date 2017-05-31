5e21bfe19a3eeeeaed317e1074c5d96a566bfe2c
Mike Jolley <mike.jolley@me.com>
2017-05-31 10:12:24 +0100

    Add failed to processing for the processing email
    Fixes 15365

--- includes/emails/class-wc-email-customer-processing-order.php
+++ includes/emails/class-wc-email-customer-processing-order.php
@@ -33,6 +33,7 @@ class WC_Email_Customer_Processing_Order extends WC_Email {
 		$this->template_plain   = 'emails/plain/customer-processing-order.php';
 
 		// Triggers for this email
+		add_action( 'woocommerce_order_status_failed_to_processing_notification', array( $this, 'trigger' ), 10, 2 );
 		add_action( 'woocommerce_order_status_on-hold_to_processing_notification', array( $this, 'trigger' ), 10, 2 );
 		add_action( 'woocommerce_order_status_pending_to_processing_notification', array( $this, 'trigger' ), 10, 2 );
 
