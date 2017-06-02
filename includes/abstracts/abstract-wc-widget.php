52f017c47dafd76ae02c437f9620213482147b37
Mike Jolley <mike.jolley@me.com>
2017-06-02 13:34:09 +0100

    Correctly append cache
    Fixes #15407

--- includes/abstracts/abstract-wc-widget.php
+++ includes/abstracts/abstract-wc-widget.php
@@ -69,7 +69,6 @@ abstract class WC_Widget extends WP_Widget {
 	 * @return bool true if the widget is cached otherwise false
 	 */
 	public function get_cached_widget( $args ) {
-
 		$cache = wp_cache_get( apply_filters( 'woocommerce_cached_widget_id', $this->widget_id ), 'widget' );
 
 		if ( ! is_array( $cache ) ) {
@@ -92,7 +91,15 @@ abstract class WC_Widget extends WP_Widget {
 	 * @return string the content that was cached
 	 */
 	public function cache_widget( $args, $content ) {
-		wp_cache_set( apply_filters( 'woocommerce_cached_widget_id', $this->widget_id ), array( $args['widget_id'] => $content ), 'widget' );
+		$cache = wp_cache_get( apply_filters( 'woocommerce_cached_widget_id', $this->widget_id ), 'widget' );
+
+		if ( ! is_array( $cache ) ) {
+			$cache = array();
+		}
+
+		$cache[ $args['widget_id'] ] = $content;
+
+		wp_cache_set( apply_filters( 'woocommerce_cached_widget_id', $this->widget_id ), $cache, 'widget' );
 
 		return $content;
 	}
