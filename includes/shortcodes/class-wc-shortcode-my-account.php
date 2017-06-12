8883ae5c7e2029709326572d4890a7f46701968a
Mike Jolley <mike.jolley@me.com>
2017-06-12 14:02:29 +0100

    Add a nonce and confirmation for logging out via the customer my account page

--- includes/shortcodes/class-wc-shortcode-my-account.php
+++ includes/shortcodes/class-wc-shortcode-my-account.php
@@ -51,9 +51,13 @@ class WC_Shortcode_My_Account {
 			} else {
 				wc_get_template( 'myaccount/form-login.php' );
 			}
-		 } else {
+		} else {
 			// Start output buffer since the html may need discarding for BW compatibility
 			ob_start();
+
+			if ( isset( $wp->query_vars['customer-logout'] ) ) {
+				wc_add_notice( sprintf( __( 'Are you sure you want to log out? <a href="%s">Confirm and log out</a>', 'woocommerce' ), wc_logout_url() ) );
+			}
 
 			// Collect notices before output
 			$notices = wc_get_notices();
