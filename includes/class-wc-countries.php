54a15b60a14d02cd141565d92515c8ff94651070
Mike Jolley <mike.jolley@me.com>
2017-06-16 11:45:53 +0100

    Allow state field to have the correct country set by passing it through
    Fixes #15632

--- includes/class-wc-countries.php
+++ includes/class-wc-countries.php
@@ -659,10 +659,10 @@ class WC_Countries {
 						'required' => false,
 						'hidden'   => true,
 					),
-                    'state' => array(
-                        'required' => false,
+					'state' => array(
+						'required' => false,
 					),
-                ),
+				),
 				'AF' => array(
 					'state' => array(
 						'required' => false,
@@ -1113,6 +1113,9 @@ class WC_Countries {
 		$address_fields = array();
 
 		foreach ( $fields as $key => $value ) {
+			if ( 'state' === $key ) {
+				$value['country_field'] = $type . 'country';
+			}
 			$address_fields[ $type . $key ] = $value;
 		}
 
