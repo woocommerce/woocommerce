a95dd091cd6ababf94988d54393b5eeb5d2f58b5
Gerhard Potgieter <potgieterg@gmail.com>
2017-06-21 16:45:41 +0200

    Adjust messaging and display information for collaborator accounts on woocommerce.com subscription page.

--- includes/admin/helper/views/html-main.php
+++ includes/admin/helper/views/html-main.php
@@ -58,6 +58,9 @@
 								} else {
 									_e( 'Subscription: Unlimited', 'woocommerce' );
 								}
+								if ( isset( $subscription['master_user_email'] ) ) {
+									printf( '</br>' . __( 'Shared by %s', 'woocommerce' ), $subscription['master_user_email'] );
+								}
 							?>
 							</span>
 						</div>
