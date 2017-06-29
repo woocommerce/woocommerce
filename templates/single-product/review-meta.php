b41b2ebb82bb8aab46dd4fc3826a2510014633cb
andrebalza <raylab@gmail.com>
2017-06-29 12:26:28 +0300

    Issue #15860 - Override instructions corrected

--- templates/single-product/review-meta.php
+++ templates/single-product/review-meta.php
@@ -2,7 +2,7 @@
 /**
  * The template to display the reviewers meta data (name, verified owner, review date)
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/content-product.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/review-meta.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
