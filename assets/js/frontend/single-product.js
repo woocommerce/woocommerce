bd2bd66334a9eaeac1f0eb6a0053d5e68f928cca
Mike Jolley <mike.jolley@me.com>
2017-06-30 15:58:55 +0100

    Force height for the gallery if the image is no loaded at time of init

--- assets/js/frontend/single-product.js
+++ assets/js/frontend/single-product.js
@@ -144,6 +144,26 @@ jQuery( function( $ ) {
 				$target.css( 'opacity', 1 );
 			}
 		} );
+
+		// Trigger resize after main image loads to ensure correct gallery size.
+		$( '.woocommerce-product-gallery__wrapper .woocommerce-product-gallery__image:eq(0) .wp-post-image' ).one( 'load', function() {
+			var $image = $( this );
+
+			if ( $image ) {
+				setTimeout(function(){
+					var setHeight = $image.closest( '.woocommerce-product-gallery__image' ).height();
+					var $viewport = $image.closest( '.flex-viewport' );
+
+					if ( setHeight && $viewport ) {
+						$viewport.height( setHeight );
+					}
+				}, 100 );
+			}
+		} ).each( function() {
+			if ( this.complete ) {
+				$( this ).load();
+			}
+		} );
 	};
 
 	/**
