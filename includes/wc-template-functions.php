47cd4702f9b8bf57340c34951a5f2dd93f3a2879
AudiLu <mrmu@mrmu.com.tw>
2017-07-03 17:37:42 +0800

    fixes woocommerce/woocommerce #15930 : corrects the argument of wc_get_orders.

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -2309,7 +2309,7 @@ if ( ! function_exists( 'woocommerce_account_orders' ) ) {
 	 */
 	function woocommerce_account_orders( $current_page ) {
 		$current_page    = empty( $current_page ) ? 1 : absint( $current_page );
-		$customer_orders = wc_get_orders( apply_filters( 'woocommerce_my_account_my_orders_query', array( 'customer' => get_current_user_id(), 'page' => $current_page, 'paginate' => true ) ) );
+		$customer_orders = wc_get_orders( apply_filters( 'woocommerce_my_account_my_orders_query', array( 'customer' => get_current_user_id(), 'paged' => $current_page, 'paginate' => true ) ) );
 
 		wc_get_template(
 			'myaccount/orders.php',
