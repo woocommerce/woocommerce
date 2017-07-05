40bb0df1e8e61626ffb408ae22fa5ce7704e6115
Marko <marko.idzan@gmail.com>
2017-07-05 13:48:54 +0200

    Croatian states
    Written with ISO 3166-2:HR entity codes
    Backup with original Croatina 3 letter States Shortnames is availiable
    at: https://github.com/markoidzan/woocommerce.hr-states

---
+++ i18n/states/HR.php
@@ -0,0 +1,39 @@
+<?php 
+/**
+ * Croatian States
+ *
+ * @author		markoidzan
+ * @category	i18n
+ * @package		WooCommerce/i18n
+ * @version 	1.0
+ */ 
+
+global $states;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$states['HR'] = array(
+	'HR07' => 'Bjelovarsko-bilogorska županija' ,
+	'HR12' => 'Brodsko-posavska županija' ,
+	'HR19' => 'Dubrovačko-neretvanska županija' ,
+	'HR18' => 'Istarska županija' ,
+	'HR04' => 'Karlovačka županija' ,
+	'HR06' => 'Koprivničko-križevačka županija' ,
+	'HR02' => 'Krapinsko-zagorska županija' ,
+	'HR09' => 'Ličko-senjska županija' ,
+	'HR20' => 'Međimurska županija' ,
+	'HR14' => 'Osiječko-branjska županija' ,
+	'HR11' => 'Požeško-slavonska županija' ,
+	'HR08' => 'Primorsko-goranska županija' ,
+	'HR03' => 'Sisačko-moslavačka županija' ,
+	'HR17' => 'Splitsko-dalmatinska županija' ,
+	'HR05' => 'Varaždinska županija' ,
+	'HR10' => 'Virovitičko-podravska županija' ,
+	'HR16' => 'Vukovarsko-srijemska županija' ,
+	'HR13' => 'Zadarska županija' ,
+	'HR01' => 'Zagrebačka županija' ,
+	'HR-15' => 'Šibensko-kninska županija' ,
+	'HR-21' => 'Grad Zagreb'
+);
