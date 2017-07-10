8fbe41edf496ba5b19df3c2526ec2d4694ea29e9
Marcio Zebedeu <marciozebedeu@gmail.com>
2017-07-10 21:30:53 +0100

    Added cantons of Angola

---
+++ i18n/states/AO.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Angola states
+ *
+ * @author      Marcio Zebedeu
+ * @category    i18n
+ * @package     WooCommerce/i18n
+ * @version     1.0.0
+ */
+global $states;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$states['AO'] = array(
+		'BG' => __( 'Bengo', 'woocommerce' ),
+	    'BL' => __( 'Benguela', 'woocommerce' ),
+	    'BI'  => __( 'Bié', 'woocommerce' ),
+	    'CA'  => __( 'Cabinda', 'woocommerce' ),
+	    'KU'  => __( 'Kuando Kubango', 'woocommerce' ),
+	    'CU'  => __( 'Cunene', 'woocommerce' ),
+	    'HU'  => __( 'Huambo', 'woocommerce' ),
+	    'HL'  => __( 'Huíla', 'woocommerce' ),
+	    'KN'  => __( 'Kwanza-Norte', 'woocommerce' ),
+	    'KS'  => __( 'Kwanza-Sul', 'woocommerce' ),
+	    'LU'  => __( 'Luanda', 'woocommerce' ),
+	    'LN'  => __( 'Lunda-Norte', 'woocommerce' ),
+	    'LS'  => __( 'Lunda-Sul', 'woocommerce' ),
+	    'ML'  => __( 'Malanje', 'woocommerce' ),
+	    'MO'  => __( 'Moxico', 'woocommerce' ),
+	    'NA'  => __( 'Namibe', 'woocommerce' ),
+	    'UI'  => __( 'Uíge', 'woocommerce' ),
+	    'ZA'  => __( 'Zaire', 'woocommerce' ),
+);
