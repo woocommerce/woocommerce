04a83b0faad79268b88ce0418a07bf158a90ea81
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-07-11 08:19:42 +0100

    Change the "Rest of the World" zone name to "Locations not covered by your other zones" everywhere

--- includes/data-stores/class-wc-shipping-zone-data-store.php
+++ includes/data-stores/class-wc-shipping-zone-data-store.php
@@ -64,7 +64,7 @@ class WC_Shipping_Zone_Data_Store extends WC_Data_Store_WP implements WC_Shippin
 		global $wpdb;
 		if ( 0 === $zone->get_id() || "0" === $zone->get_id() ) {
 			$this->read_zone_locations( $zone );
-			$zone->set_zone_name( __( 'Rest of the World', 'woocommerce' ) );
+			$zone->set_zone_name( __( 'Locations not covered by your other zones', 'woocommerce' ) );
 			$zone->read_meta_data();
 			$zone->set_object_read( true );
 			do_action( 'woocommerce_shipping_zone_loaded', $zone );
