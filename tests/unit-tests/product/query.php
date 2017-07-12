7be2c7dc55ff07849ed7078d8a8dfab6487e5beb
claudiulodro <claudiulodro@gmail.com>
2017-07-12 14:18:51 -0700

    More tests

--- tests/unit-tests/product/query.php
+++ tests/unit-tests/product/query.php
@@ -11,7 +11,7 @@ class WC_Tests_WC_Product_Query extends WC_Unit_Test_Case {
 	 *
 	 * @since 3.2
 	 */
-	public function test_order_query_new() {
+	public function test_product_query_new() {
 		$query = new WC_Product_Query();
 		$this->assertEquals( '', $query->get( 'weight' ) );
 		$this->assertEquals( array( 'product', 'product_variation' ), $query->get( 'type' ) );
@@ -22,7 +22,7 @@ class WC_Tests_WC_Product_Query extends WC_Unit_Test_Case {
 	 *
 	 * @since 3.2
 	 */
-	public function test_order_query_standard() {
+	public function test_product_query_standard() {
 		$product1 = new WC_Product_Simple();
 		$product1->set_sku( 'sku1' );
 		$product1->set_regular_price( '10.00' );
@@ -62,5 +62,87 @@ class WC_Tests_WC_Product_Query extends WC_Unit_Test_Case {
 		$query->set( 'limit', 1 );
 		$results = $query->get_products();
 		$this->assertEquals( 1, count( $results ) );
+	}
+
+
+	/**
+	 * Test querying by product date properties for dates stored in metadata.
+	 *
+	 * @since 3.2
+	 */
+	public function test_product_query_meta_date_queries() {
+		$now = current_time( 'mysql', true );
+		$now_stamp = strtotime( $now );
+		$now_date = date( 'Y-m-d', $now_stamp );
+		$past_stamp = $now_stamp - DAY_IN_SECONDS;
+		$past = date( 'Y-m-d', $past_stamp );
+		$future_stamp = $now_stamp + DAY_IN_SECONDS;
+		$future = date( 'Y-m-d', $future_stamp );
+
+		$product = new WC_Product_Simple();
+		$product->set_date_on_sale_from( $now_stamp );
+		$product->save();
+
+		// Check WC_DateTime support.
+		$query = new WC_Product_Query( array(
+			'date_on_sale_from' => $product->get_date_on_sale_from(),
+		) );
+		$products = $query->get_products();
+		$this->assertEquals( 1, count( $products ) );
+
+		// Check date support.
+		$query = new WC_Product_Query( array(
+			'date_on_sale_from' => $now_date,
+		) );
+		$this->assertEquals( 1, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', $past );
+		$this->assertEquals( 0, count( $query->get_products() ) );
+
+		// Check timestamp support.
+		$query = new WC_Product_Query( array(
+			'date_on_sale_from' => $product->get_date_on_sale_from()->getTimestamp(),
+		) );
+		$this->assertEquals( 1, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', $future_stamp );
+		$this->assertEquals( 0, count( $query->get_products() ) );
+
+		// Check comparison support.
+		$query = new WC_Product_Query( array(
+			'date_on_sale_from' => '>' . $past,
+		) );
+		$this->assertEquals( 1, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', '<' . $past );
+		$this->assertEquals( 0, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', '>=' . $now_date );
+		$this->assertEquals( 1, count( $query->get_products() ) );
+
+		// Check timestamp comparison support.
+		$query = new WC_Product_Query( array(
+			'date_on_sale_from' => '<' . $future_stamp,
+		) );
+		$this->assertEquals( 1, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', '<' . $past_stamp );
+		$this->assertEquals( 0, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', '>=' . $now_stamp );
+
+		// Check date range support.
+		$query = new WC_Product_Query( array(
+			'date_on_sale_from' => $past . '...' . $future,
+		) );
+		$this->assertEquals( 1, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', $now_date . '...' . $future );
+		$this->assertEquals( 1, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', $future . '...' . $now_date );
+		$this->assertEquals( 0, count( $query->get_products() ) );
+
+		// Check timestamp range support.
+		$query = new WC_Product_Query( array(
+			'date_on_sale_from' => $past_stamp . '...' . $future_stamp,
+		) );
+		$this->assertEquals( 1, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', $now_stamp . '...' . $future_stamp );
+		$this->assertEquals( 1, count( $query->get_products() ) );
+		$query->set( 'date_on_sale_from', $future_stamp . '...' . $now_stamp );
+		$this->assertEquals( 0, count( $query->get_products() ) );
 	}
 }
