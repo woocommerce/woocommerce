7c321a22691cf81c8e9be9d47b0bdea1541f9b88
Mike Jolley <mike.jolley@me.com>
2017-07-13 12:15:13 +0100

    Ajaxify widget remove from cart button
    Fixes #13171

--- templates/cart/mini-cart.php
+++ templates/cart/mini-cart.php
@@ -15,7 +15,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.1.0
+ * @version 3.2.0
  */
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -42,10 +42,11 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 					<li class="woocommerce-mini-cart-item <?php echo esc_attr( apply_filters( 'woocommerce_mini_cart_item_class', 'mini_cart_item', $cart_item, $cart_item_key ) ); ?>">
 						<?php
 						echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
-							'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+							'<a href="%s" class="remove remove_from_cart_button" aria-label="%s" data-product_id="%s" data-cart_item_key="%s" data-product_sku="%s">&times;</a>',
 							esc_url( WC()->cart->get_remove_url( $cart_item_key ) ),
 							__( 'Remove this item', 'woocommerce' ),
 							esc_attr( $product_id ),
+							esc_attr( $cart_item_key ),
 							esc_attr( $_product->get_sku() )
 						), $cart_item_key );
 						?>
