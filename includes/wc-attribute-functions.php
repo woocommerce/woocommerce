dd36ae87d3118ca93d58524f5e9ba906b342453a
Claudio Sanches <contato@claudiosanches.com>
2017-07-13 19:55:47 -0300

    Improved wc_create_attribute() inline doc

--- includes/wc-attribute-functions.php
+++ includes/wc-attribute-functions.php
@@ -382,17 +382,21 @@ function wc_get_attribute( $id ) {
 /**
  * Create attribute.
  *
- * Options:
- *
- * id           - Unique identifier, used to update an attribute.
- * name         - Attribute name.
- * slug         - Attribute alphanumeric identifier.
- * type         - Type of attribute, core options: 'select' and 'text', default to 'select'.
- * order_by     - Sort order, options: 'menu_order', 'name', 'name_num' and 'id'.
- * has_archives - Enable or disable attribute archives.
- *
  * @since  3.2.0
- * @param  array $args Attribute arguments.
+ * @param  array $args Attribute arguments {
+ *     Array of attribute parameters.
+ *
+ *     @type int    $id           Unique identifier, used to update an attribute.
+ *     @type string $name         Attribute name. Always required.
+ *     @type string $slug         Attribute alphanumeric identifier.
+ *     @type string $type         Type of attribute.
+ *                                Core by default accepts: 'select' and 'text'.
+ *                                Default to 'select'.
+ *     @type string $order_by     Sort order.
+ *                                Accepts: 'menu_order', 'name', 'name_num' and 'id'.
+ *                                Default to 'menu_order'.
+ *     @type bool   $has_archives Enable or disable attribute archives. False by default.
+ * }
  * @return int|WP_Error
  */
 function wc_create_attribute( $args ) {
