809ff9c31d589169b82efcc26abe9de8af1b1f54
Mike Jolley <mike.jolley@me.com>
2017-07-17 18:13:18 +0100

    correct escaping

--- includes/admin/views/html-notice-untested-extensions-inline.php
+++ includes/admin/views/html-notice-untested-extensions-inline.php
@@ -2,19 +2,14 @@
 /**
  * Admin View: Notice - Untested extensions.
  */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
 ?>
-
 <p class="wc_plugin_upgrade_notice extensions_warning <?php echo esc_attr( $upgrade_type ) ?>">
-	<strong>
-		<?php echo wc_clean( $message ) ?>
-	</strong><br />
+	<strong><?php echo esc_html( $message ) ?></strong><br />
 
 	<?php foreach ( $plugins as $plugin ): ?>
-		<span><?php echo wc_clean( $plugin['Name'] ); ?></span>
+		<span><?php echo esc_html( $plugin['Name'] ); ?></span>
 	<?php endforeach ?>
 </p>
