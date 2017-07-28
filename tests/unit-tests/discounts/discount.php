a721ab064e79cfdcdb03e245fb05ba82e53040c3
claudiulodro <claudiulodro@gmail.com>
2017-07-28 09:59:24 -0700

    Update WC_Discount tests

--- tests/unit-tests/discounts/discount.php
+++ tests/unit-tests/discounts/discount.php
@@ -9,24 +9,19 @@ class WC_Tests_Discount extends WC_Unit_Test_Case {
 	/**
 	 * Test get and set ID.
 	 */
-	public function test_get_set_id() {
-		$discount = new WC_Discount;
-		$discount->set_id( 'discount-5' );
-		$this->assertEquals( 'discount-5', $discount->get_id() );
-	}
-
-	/**
-	 * Test get and set ID.
-	 */
 	public function test_get_set_amount() {
 		$discount = new WC_Discount;
 		$discount->set_amount( '10' );
 		$this->assertEquals( '10', $discount->get_amount() );
-		$this->assertEquals( 'fixed_cart', $discount->get_discount_type() );
+	}
 
+	public function test_get_set_type() {
 		$discount = new WC_Discount;
-		$discount->set_amount( '10%' );
-		$this->assertEquals( '10', $discount->get_amount() );
+
+		$discount->set_discount_type( 'fixed' );
+		$this->assertEquals( 'fixed', $discount->get_discount_type() );
+
+		$discount->set_discount_type( 'percent' );
 		$this->assertEquals( 'percent', $discount->get_discount_type() );
 	}
 
@@ -37,19 +32,5 @@ class WC_Tests_Discount extends WC_Unit_Test_Case {
 		$discount = new WC_Discount;
 		$discount->set_discount_total( 1000 );
 		$this->assertEquals( 1000, $discount->get_discount_total() );
-	}
-
-	/**
-	 * Test get and set taxes.
-	 */
-	public function test_get_set_taxes() {
-
-	}
-
-	/**
-	 * Test calculate negative taxes.
-	 */
-	public function test_calculate_negative_taxes() {
-
 	}
 }
