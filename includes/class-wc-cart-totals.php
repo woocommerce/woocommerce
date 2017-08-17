9a9b8d720f9c646c611989bfd93b6e3c318b9aa6
claudiulodro <claudiulodro@gmail.com>
2017-08-17 13:18:58 -0700

    Add missing line_tax_data

--- includes/class-wc-cart-totals.php
+++ includes/class-wc-cart-totals.php
@@ -524,8 +524,9 @@ final class WC_Cart_Totals {
 				}
 			}
 
-			$this->cart->cart_contents[ $item_key ]['line_total'] = wc_remove_number_precision( $item->total );
-			$this->cart->cart_contents[ $item_key ]['line_tax']   = wc_remove_number_precision( $item->total_tax );
+			$this->cart->cart_contents[ $item_key ]['line_tax_data']['total'] = $item->taxes;
+			$this->cart->cart_contents[ $item_key ]['line_total']             = wc_remove_number_precision( $item->total );
+			$this->cart->cart_contents[ $item_key ]['line_tax']               = wc_remove_number_precision( $item->total_tax );
 		}
 
 		$this->set_total( 'items_total', array_sum( array_values( wp_list_pluck( $this->items, 'total' ) ) ) );
@@ -565,6 +566,7 @@ final class WC_Cart_Totals {
 				}
 			}
 
+			$this->cart->cart_contents[ $item_key ]['line_tax_data']     = array( 'subtotal' => $subtotal_taxes );
 			$this->cart->cart_contents[ $item_key ]['line_subtotal']     = wc_remove_number_precision( $item->subtotal );
 			$this->cart->cart_contents[ $item_key ]['line_subtotal_tax'] = wc_remove_number_precision( $item->subtotal_tax );
 		}
