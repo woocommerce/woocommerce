bc95b1c153ab1f70329a7d7d389080951ba28e04
Claudio Sanches <contato@claudiosanches.com>
2017-08-17 12:06:45 -0300

    Moved wc_is_webhook_valid_topic to wc-webhook-functions.php

--- includes/wc-webhook-functions.php
+++ includes/wc-webhook-functions.php
@@ -5,11 +5,43 @@
  * @author   WooThemes
  * @category Core
  * @package  WooCommerce/Functions
- * @version  2.3.0
+ * @version  3.2.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Check if the given topic is a valid webhook topic, a topic is valid if:
+ *
+ * + starts with `action.woocommerce_` or `action.wc_`.
+ * + it has a valid resource & event.
+ *
+ * @param  string $topic Webhook topic.
+ * @return bool
+ */
+function wc_is_webhook_valid_topic( $topic ) {
+
+	// Custom topics are prefixed with woocommerce_ or wc_ are valid.
+	if ( 0 === strpos( $topic, 'action.woocommerce_' ) || 0 === strpos( $topic, 'action.wc_' ) ) {
+		return true;
+	}
+
+	$data = explode( '.', $topic );
+
+	if ( ! isset( $data[0] ) || ! isset( $data[1] ) ) {
+		return false;
+	}
+
+	$valid_resources = apply_filters( 'woocommerce_valid_webhook_resources', array( 'coupon', 'customer', 'order', 'product' ) );
+	$valid_events    = apply_filters( 'woocommerce_valid_webhook_events', array( 'created', 'updated', 'deleted', 'restored' ) );
+
+	if ( in_array( $data[0], $valid_resources, true ) && in_array( $data[1], $valid_events, true ) ) {
+		return true;
+	}
+
+	return false;
 }
 
 /**
