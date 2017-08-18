8d8ffb6e9ecadc36204dd65e42c9ef3182326d1d
Claudio Sanches <contato@claudiosanches.com>
2017-08-18 13:41:51 -0300

    Included @since tag to wc_is_webhook_valid_topic()

--- includes/wc-webhook-functions.php
+++ includes/wc-webhook-functions.php
@@ -18,6 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * + starts with `action.woocommerce_` or `action.wc_`.
  * + it has a valid resource & event.
  *
+ * @since  2.2.0
  * @param  string $topic Webhook topic.
  * @return bool
  */
