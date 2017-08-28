42e6d4dffefe529d02015649c5d6fe1d214f9f6e
Matt Miller <matt@miller-media.com>
2017-08-28 13:25:20 -0700

    Removed extra tab character

--- includes/class-wc-install.php
+++ includes/class-wc-install.php
@@ -307,7 +307,7 @@ class WC_Install {
 		wp_clear_scheduled_hook( 'woocommerce_tracker_send_event' );
 
 		$ve = get_option( 'gmt_offset' ) > 0 ? '-' : '+';
-		
+
 		wp_schedule_event( strtotime( '00:00 tomorrow ' . $ve . absint( get_option( 'gmt_offset' ) ) . ' HOURS' ), 'daily', 'woocommerce_scheduled_sales' );
 
 		$held_duration = get_option( 'woocommerce_hold_stock_minutes', '60' );
