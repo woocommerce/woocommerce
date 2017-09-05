562912a27ad5c4abd9d12ae4ac5c54c05f279f0f
James Golovich <james@gnuinter.net>
2017-09-05 09:12:53 -0700

    Allow variable products to be added via simple HTTP Request
    Matches existing behavior of simple products that allow adding via a URL link.

--- includes/class-wc-form-handler.php
+++ includes/class-wc-form-handler.php
@@ -845,7 +845,7 @@ class WC_Form_Handler {
 		// If no variation ID is set, attempt to get a variation ID from posted attributes.
 		if ( empty( $variation_id ) ) {
 			$data_store   = WC_Data_Store::load( 'product' );
-			$variation_id = $data_store->find_matching_product_variation( $adding_to_cart, wp_unslash( $_POST ) );
+			$variation_id = $data_store->find_matching_product_variation( $adding_to_cart, wp_unslash( $_REQUEST ) );
 		}
 
 		// Validate the attributes.
