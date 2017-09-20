3d9902c4d59f4e904e3a6a0d961418a473a9b677
Mike Jolley <mike.jolley@me.com>
2017-09-20 15:47:30 +0100

    Page template should have higher priority.

--- includes/class-wc-template-loader.php
+++ includes/class-wc-template-loader.php
@@ -97,6 +97,10 @@ class WC_Template_Loader {
 		$search_files   = apply_filters( 'woocommerce_template_loader_files', array(), $default_file );
 		$search_files[] = 'woocommerce.php';
 
+		if ( is_page_template() ) {
+			$search_files[] = get_page_template_slug();
+		}
+
 		if ( is_product_taxonomy() ) {
 			$term   = get_queried_object();
 			$search_files[] = 'taxonomy-' . $term->taxonomy . '-' . $term->slug . '.php';
@@ -107,10 +111,6 @@ class WC_Template_Loader {
 
 		$search_files[] = $default_file;
 		$search_files[] = WC()->template_path() . $default_file;
-		
-		if ( is_page_template() ) {
-				$search_files[] = get_page_template_slug(); // Supporting Custom Post Type Template
-		}
 
 		return array_unique( $search_files );
 	}
