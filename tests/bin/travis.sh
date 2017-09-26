bf82c769e798a73ac11449e01942b1272e59db6e
Mike Jolley <mike.jolley@me.com>
2017-09-26 15:21:05 +0100

    scrutinizer

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -31,6 +31,9 @@ if [ $1 == 'after' ]; then
 
 	if [[ ${TRAVIS_PHP_VERSION} == ${PHP_LATEST_STABLE} ]]; then
 		bash <(curl -s https://codecov.io/bash)
+
+		wget https://scrutinizer-ci.com/ocular.phar
+		php ocular.phar code-coverage:upload --format=php-clover coverage.clover
 	fi
 
 fi
