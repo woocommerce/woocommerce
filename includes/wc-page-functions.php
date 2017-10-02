4071a7821fec4106ccab5ab5263893b42e6ff964
Mike Jolley <mike.jolley@me.com>
2017-10-02 11:41:44 +0100

    Check `$item->url` is set.
    Closes #17014

--- includes/wc-page-functions.php
+++ includes/wc-page-functions.php
@@ -111,7 +111,7 @@ function wc_get_endpoint_url( $endpoint, $value = '', $permalink = '' ) {
 /**
  * Hide menu items conditionally.
  *
- * @param array $items
+ * @param array $items Navigation items.
  * @return array
  */
 function wc_nav_menu_items( $items ) {
@@ -120,8 +120,12 @@ function wc_nav_menu_items( $items ) {
 
 		if ( ! empty( $customer_logout ) ) {
 			foreach ( $items as $key => $item ) {
-				$path = parse_url( $item->url, PHP_URL_PATH );
+				if ( empty( $item->url ) ) {
+					continue;
+				}
+				$path  = parse_url( $item->url, PHP_URL_PATH );
 				$query = parse_url( $item->url, PHP_URL_QUERY );
+
 				if ( strstr( $path, $customer_logout ) || strstr( $query, $customer_logout ) ) {
 					unset( $items[ $key ] );
 				}
