f57e2e51986acb9f8cda1736931feb54386e4d7b
lars <larskoester@LarsArbitskiste.fritz.box>
2017-10-04 10:56:56 +0200

    #16938 rename filter for clarity

--- includes/class-wc-cart-totals.php
+++ includes/class-wc-cart-totals.php
@@ -313,7 +313,7 @@ final class WC_Cart_Totals {
 				}
 			}
 
-			$fee->taxes = apply_filters( 'woocommerce_get_fees_from_cart_taxes', $fee->taxes, $fee, $this );
+			$fee->taxes = apply_filters( 'woocommerce_cart_totals_get_fees_from_cart_taxes', $fee->taxes, $fee, $this );
 
 			$fee->total_tax = array_sum( $fee->taxes );
 
