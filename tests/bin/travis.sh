cd554c1bdf99b81dc4737ac90a6ce2e48fe28fde
Claudio Sanches <contato@claudiosanches.com>
2017-10-05 13:09:39 -0300

    Restored tests/bin/phpunit.sh

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# usage: travis.sh before|during|after
+# usage: travis.sh before|after
 
 if [ $1 == 'before' ]; then
 
@@ -17,21 +17,12 @@ if [ $1 == 'before' ]; then
 
 fi
 
-if [ $1 == 'during' ]; then
-
-	if [[ ${TRAVIS_PHP_VERSION} == '7.1' ]]; then
-		phpunit -c phpunit.xml --coverage-clover=coverage.clover
-	else
-		phpunit -c phpunit.xml
-	fi
-
-fi
-
 if [ $1 == 'after' ]; then
 
 	if [[ ${TRAVIS_PHP_VERSION} == '7.1' ]]; then
 		bash <(curl -s https://codecov.io/bash)
 		wget https://scrutinizer-ci.com/ocular.phar
+		chmod +x ocular.phar
 		php ocular.phar code-coverage:upload --format=php-clover coverage.clover
 	fi
 
