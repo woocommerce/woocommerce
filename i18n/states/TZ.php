5c6770b35bdb4ca00e3e8d6a53e5fd005bb51ff3
Mohsin Sumar <mohsin@sumars.net>
2017-10-05 16:08:01 +0300

    States for Tanzania

---
+++ i18n/states/TZ.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * TZ states
+ *
+ * @author      Lev @ Extreme Web Technologies Ltd
+ * @category    i18n
+ * @package     WooCommerce/i18n
+ * @version     2.0.0
+ */
+global $states;
+
+$states['TZ'] = array(
+	'AR'  => __( 'Arusha', 'woocommerce' ),
+	'DSM'  => __( 'Dar es Salaam', 'woocommerce' ),
+	'DO'  => __( 'Dodoma', 'woocommerce' ),
+	'IR' => __( 'Iringa', 'woocommerce' ),
+	'KA'  => __( 'Kagera', 'woocommerce' ),
+	'KI'  => __( 'Kigoma', 'woocommerce' ),
+	'KIL'  => __( 'Kilimanjaro', 'woocommerce' ),
+	'LI'  => __( 'Lindi', 'woocommerce' ),
+	'MA'  => __( 'Manyara', 'woocommerce' ),
+	'MAR'  => __( 'Mara', 'woocommerce' ),
+	'MB'  => __( 'Mbeya', 'woocommerce' ),
+	'MOR'  => __( 'Morogoro', 'woocommerce' ),
+	'MT'  => __( 'Mtwara', 'woocommerce' ),
+	'MW'  => __( 'Mwanza', 'woocommerce' ),
+	'PI'  => __( 'Pemba Island', 'woocommerce' ),
+	'PW'  => __( 'Pwani', 'woocommerce' ),
+	'RU'  => __( 'Rukwa', 'woocommerce' ),
+	'RUV'  => __( 'Ruvuma', 'woocommerce' ),
+	'SHI'  => __( 'Shinyanga', 'woocommerce' ),
+	'SI'  => __( 'Singida', 'woocommerce' ),
+	'TAB'  => __( 'Tabora', 'woocommerce' ),
+	'IG'  => __( 'Igunga', 'woocommerce' ),
+	'TA'  => __( 'Tanga', 'woocommerce' ),
+	'ZAN'  => __( 'Zanzibar', 'woocommerce' ),
+);
