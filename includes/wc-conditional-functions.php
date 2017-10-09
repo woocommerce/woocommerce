2ff1ee145e5a3cce31e6c34190cc667056452edf
Gerhard Potgieter <potgieterg@gmail.com>
2017-10-09 12:39:47 +0200

    Also check the woocommerce_checkout conditional, ensure ajax conditional checks keeps working. Closes #17094

--- includes/wc-conditional-functions.php
+++ includes/wc-conditional-functions.php
@@ -97,7 +97,7 @@ if ( ! function_exists( 'is_checkout' ) ) {
 	 * @return bool
 	 */
 	function is_checkout() {
-		return is_page( wc_get_page_id( 'checkout' ) ) || wc_post_content_has_shortcode( 'woocommerce_checkout' ) || apply_filters( 'woocommerce_is_checkout', false );
+		return is_page( wc_get_page_id( 'checkout' ) ) || wc_post_content_has_shortcode( 'woocommerce_checkout' ) || apply_filters( 'woocommerce_is_checkout', false ) || defined( 'WOOCOMMERCE_CHECKOUT' );
 	}
 }
 
