b0b9eec9f1c6da78a89f625e122436e37590cccd
Valerie <valerie@automattic.com>
2017-10-14 00:19:45 +0200

    Wizard: Only save JP TOS opt-in after connection

--- includes/admin/class-wc-admin-setup-wizard.php
+++ includes/admin/class-wc-admin-setup-wizard.php
@@ -1392,6 +1392,9 @@ class WC_Admin_Setup_Wizard {
 			class_exists( 'Jetpack' ) &&
 			Jetpack::is_active()
 		) {
+			// Leave a note for WooCommerce Services that Jetpack has been opted into.
+			update_option( 'woocommerce_setup_jetpack_opted_in', true );
+
 			wp_redirect( esc_url_raw( remove_query_arg( 'from', $this->get_next_step_link() ) ) );
 			exit;
 		}
@@ -1544,9 +1547,6 @@ class WC_Admin_Setup_Wizard {
 	 */
 	public function wc_setup_activate_save() {
 		check_admin_referer( 'wc-setup' );
-
-		// Leave a note for WooCommerce Services that Jetpack has been opted into.
-		update_option( 'woocommerce_setup_jetpack_opted_in', true );
 
 		WC_Install::background_installer( 'jetpack', array(
 			'file'      => 'jetpack/jetpack.php',
