d1226f3ba523c94691a71aa62894183c4930579a
hamed <hamedmoodi2011@gmail.com>
2017-10-14 14:26:30 +0330

    Add actions for product download item
    Add actions for product download item to add extra attributes for download such as duration, format, size, Free.

--- includes/admin/meta-boxes/views/html-product-download.php
+++ includes/admin/meta-boxes/views/html-product-download.php
@@ -5,11 +5,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <tr>
 	<td class="sort"></td>
+	<?php do_action( 'wc_download_before_file_name', $file, $key );?>
 	<td class="file_name">
 		<input type="text" class="input_text" placeholder="<?php esc_attr_e( 'File name', 'woocommerce' ); ?>" name="_wc_file_names[]" value="<?php echo esc_attr( $file['name'] ); ?>" />
 		<input type="hidden" name="_wc_file_hashes[]" value="<?php echo esc_attr( $key ); ?>" />
 	</td>
+	<?php do_action( 'wc_download_before_file_url', $file, $key );?>
 	<td class="file_url"><input type="text" class="input_text" placeholder="<?php esc_attr_e( "http://", 'woocommerce' ); ?>" name="_wc_file_urls[]" value="<?php echo esc_attr( $file['file'] ); ?>" /></td>
+	<?php do_action( 'wc_download_before_upload_button', $file, $key );?>
 	<td class="file_url_choose" width="1%"><a href="#" class="button upload_file_button" data-choose="<?php esc_attr_e( 'Choose file', 'woocommerce' ); ?>" data-update="<?php esc_attr_e( 'Insert file URL', 'woocommerce' ); ?>"><?php echo str_replace( ' ', '&nbsp;', __( 'Choose file', 'woocommerce' ) ); ?></a></td>
+	<?php do_action( 'wc_download_before_delete_button', $file, $key );?>
 	<td width="1%"><a href="#" class="delete"><?php _e( 'Delete', 'woocommerce' ); ?></a></td>
 </tr>
