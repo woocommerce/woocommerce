adcf83441bc50ebdecc87e0ffd373e3276c6770b
Gareth McCumskey <gareth@mccumskey.com>
2017-10-16 09:16:43 +0200

    Re-adjusted all other style changes to better match back to project style requirements

--- templates/order/order-details-customer.php
+++ templates/order/order-details-customer.php
@@ -10,54 +10,54 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see    https://docs.woocommerce.com/document/template-structure/
+ * @see 	https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
  * @version 3.2.0
  */
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 ?>
 <section class="woocommerce-customer-details">
 
-    <?php if (!wc_ship_to_billing_address_only() && $order->needs_shipping_address()) : ?>
+	<?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() ) : ?>
 
-    <section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
+		<section class="woocommerce-columns woocommerce-columns--2 woocommerce-columns--addresses col2-set addresses">
 
-        <div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
+			<div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
 
     <?php endif; ?>
 
-            <h2 class="woocommerce-column__title"><?php _e('Billing address', 'woocommerce'); ?></h2>
+				<h2 class="woocommerce-column__title"><?php _e( 'Billing address', 'woocommerce' ); ?></h2>
 
-            <address>
-                <?php echo ($address = $order->get_formatted_billing_address()) ? $address : __('N/A', 'woocommerce'); ?>
-                <?php if ($order->get_billing_phone()) : ?>
-                    <p class="woocommerce-customer-details--phone"><?php echo esc_html($order->get_billing_phone()); ?></p>
-                <?php endif; ?>
-                <?php if ($order->get_billing_email()) : ?>
-                    <p class="woocommerce-customer-details--email"><?php echo esc_html($order->get_billing_email()); ?></p>
-                <?php endif; ?>
-            </address>
+				<address>
+					<?php echo ( $address = $order->get_formatted_billing_address() ) ? $address : __( 'N/A', 'woocommerce' ); ?>
+					<?php if ( $order->get_billing_phone() ) : ?>
+						<p class="woocommerce-customer-details--phone"><?php echo esc_html( $order->get_billing_phone() ); ?></p>
+					<?php endif; ?>
+					<?php if ( $order->get_billing_email() ) : ?>
+						<p class="woocommerce-customer-details--email"><?php echo esc_html( $order->get_billing_email() ); ?></p>
+					<?php endif; ?>
+				</address>
 
-            <?php if (!wc_ship_to_billing_address_only() && $order->needs_shipping_address()) : ?>
+				<?php if ( ! wc_ship_to_billing_address_only() && $order->needs_shipping_address() ) : ?>
 
-        </div><!-- /.col-1 -->
+			</div><!-- /.col-1 -->
 
-        <div class="woocommerce-column woocommerce-column--2 woocommerce-column--shipping-address col-2">
+			<div class="woocommerce-column woocommerce-column--2 woocommerce-column--shipping-address col-2">
 
-            <h2 class="woocommerce-column__title"><?php _e('Shipping address', 'woocommerce'); ?></h2>
+				<h2 class="woocommerce-column__title"><?php _e( 'Shipping address', 'woocommerce' ); ?></h2>
 
-            <address>
-                <?php echo ($address = $order->get_formatted_shipping_address()) ? $address : __('N/A', 'woocommerce'); ?>
-            </address>
+				<address>
+					<?php echo ( $address = $order->get_formatted_shipping_address() ) ? $address : __( 'N/A', 'woocommerce' ); ?>
+				</address>
 
-        </div><!-- /.col-2 -->
+			</div><!-- /.col-2 -->
 
-    </section><!-- /.col2-set -->
+		</section><!-- /.col2-set -->
 
-            <?php endif; ?>
+	            <?php endif; ?>
 
 </section>
