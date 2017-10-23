f846249dd3fefc2ad5b0ec91acbb76b509c9f14c
Mogen Cheng <mogen@Mogens-MacBook-Pro.local>
2017-10-22 22:00:04 -0400

    Simplified order tracking form

--- templates/order/form-tracking.php
+++ templates/order/form-tracking.php
@@ -26,13 +26,13 @@ global $post;
 
 <form action="<?php echo esc_url( get_permalink( $post->ID ) ); ?>" method="post" class="track_order">
 
-	<p><?php _e( 'To track your order please enter your Order ID in the box below and press the "Track" button. This was given to you on your receipt and in the confirmation email you should have received.', 'woocommerce' ); ?></p>
+	<p><?php _e( 'To track your order, please enter your Order ID below.', 'woocommerce' ); ?></p>
 
 	<p class="form-row form-row-first"><label for="orderid"><?php _e( 'Order ID', 'woocommerce' ); ?></label> <input class="input-text" type="text" name="orderid" id="orderid" value="<?php echo isset( $_REQUEST['orderid'] ) ? esc_attr( $_REQUEST['orderid'] ) : ''; ?>" placeholder="<?php esc_attr_e( 'Found in your order confirmation email.', 'woocommerce' ); ?>" /></p>
 	<p class="form-row form-row-last"><label for="order_email"><?php _e( 'Billing email', 'woocommerce' ); ?></label> <input class="input-text" type="text" name="order_email" id="order_email" value="<?php echo isset( $_REQUEST['order_email'] ) ? esc_attr( $_REQUEST['order_email'] ) : ''; ?>" placeholder="<?php esc_attr_e( 'Email you used during checkout.', 'woocommerce' ); ?>" /></p>
 	<div class="clear"></div>
 
-	<p class="form-row"><input type="submit" class="button" name="track" value="<?php esc_attr_e( 'Track', 'woocommerce' ); ?>" /></p>
+	<p class="form-row"><input type="submit" class="button" name="track" value="<?php esc_attr_e( 'Track Order', 'woocommerce' ); ?>" /></p>
 	<?php wp_nonce_field( 'woocommerce-order_tracking' ); ?>
 
 </form>
