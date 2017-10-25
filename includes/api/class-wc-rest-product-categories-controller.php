4b90c12db1815c57b62bf64194638a89944bf05a
Claudio Sanches <contato@claudiosanches.com>
2017-10-25 20:40:44 -0200

    Fixed coding standards.
    Still missing fix the file name, but I'll leave it for 3.3

--- includes/api/class-wc-rest-product-categories-controller.php
+++ includes/api/class-wc-rest-product-categories-controller.php
@@ -32,9 +32,9 @@ class WC_REST_Product_Categories_Controller extends WC_REST_Product_Categories_V
 	/**
 	 * Prepare a single product category output for response.
 	 *
-	 * @param WP_Term $item Term object.
-	 * @param WP_REST_Request $request
-	 * @return WP_REST_Response $response
+	 * @param WP_Term         $item    Term object.
+	 * @param WP_REST_Request $request Request instance.
+	 * @return WP_REST_Response
 	 */
 	public function prepare_item_for_response( $item, $request ) {
 		// Get category display type.
@@ -56,7 +56,8 @@ class WC_REST_Product_Categories_Controller extends WC_REST_Product_Categories_V
 		);
 
 		// Get category image.
-		if ( $image_id = get_woocommerce_term_meta( $item->term_id, 'thumbnail_id' ) ) {
+		$image_id = get_woocommerce_term_meta( $item->term_id, 'thumbnail_id' );
+		if ( $image_id ) {
 			$attachment = get_post( $image_id );
 
 			$data['image'] = array(
