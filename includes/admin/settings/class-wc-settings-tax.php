92dc994229b19959ad5b87eddcf6423223ea3d44
Mike Jolley <mike.jolley@me.com>
2017-10-25 12:51:19 +0100

    Prevent notice on settings page when accessing invalid tab
    Closes #17367

--- includes/admin/settings/class-wc-settings-tax.php
+++ includes/admin/settings/class-wc-settings-tax.php
@@ -26,7 +26,13 @@ class WC_Settings_Tax extends WC_Settings_Page {
 		$this->id = 'tax';
 		$this->label = __( 'Tax', 'woocommerce' );
 
-		parent::__construct();
+		add_filter( 'woocommerce_settings_tabs_array', array( $this, 'add_settings_page' ), 20 );
+
+		if ( wc_tax_enabled() ) {
+			add_action( 'woocommerce_sections_' . $this->id, array( $this, 'output_sections' ) );
+			add_action( 'woocommerce_settings_' . $this->id, array( $this, 'output' ) );
+			add_action( 'woocommerce_settings_save_' . $this->id, array( $this, 'save' ) );
+		}
 	}
 
 	/**
