f03b37b89005d2d09536f591a92fa210c84d0d28
claudiulodro <claudiulodro@gmail.com>
2017-10-27 13:49:16 -0700

    Remove ! empty() check that isn't needed and causes problems in PHP <=5.3

--- includes/wc-cart-functions.php
+++ includes/wc-cart-functions.php
@@ -306,7 +306,7 @@ function wc_cart_totals_order_total_html() {
 			foreach ( WC()->cart->get_tax_totals() as $code => $tax ) {
 				$tax_string_array[] = sprintf( '%s %s', $tax->formatted_amount, $tax->label );
 			}
-		} elseif ( ! empty( WC()->cart->get_tax_totals() ) ) {
+		} elseif ( WC()->cart->get_tax_totals() ) {
 			$tax_string_array[] = sprintf( '%s %s', wc_price( WC()->cart->get_taxes_total( true, true ) ), WC()->countries->tax_or_vat() );
 		}
 
