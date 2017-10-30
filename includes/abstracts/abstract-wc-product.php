203e915f9ae5d82e20ccef78b6cf534c68d6ac8d
Tim Howe <timbhowe@gmail.com>
2017-10-30 14:27:00 -0400

    Updated the `if empty` check to make it is compatible with older versions of php

--- includes/abstracts/abstract-wc-product.php
+++ includes/abstracts/abstract-wc-product.php
@@ -1420,8 +1420,9 @@ class WC_Product extends WC_Abstract_Legacy_Product {
 			$visible = false;
 		}
 
-		if ( !empty( $this->get_parent_id() ) ) {
-			if ( ( $parent_product = wc_get_product( $this->get_parent_id() ) ) && 'publish' !== $parent_product->get_status() ) {
+		$parent_id = $this->get_parent_id();
+		if ( ! empty( $parent_id ) ) {
+			if ( ( $parent_product = wc_get_product( $parent_id ) ) && 'publish' !== $parent_product->get_status() ) {
 				$visible = false;
 			}
 		}
