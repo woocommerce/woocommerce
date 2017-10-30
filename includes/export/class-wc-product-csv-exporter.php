c1d86b999413a7f6395ffe8a6e6407ca73e1e0c8
Mike Jolley <mike.jolley@me.com>
2017-10-30 12:15:32 +0000

    Split up skip logic

--- includes/export/class-wc-product-csv-exporter.php
+++ includes/export/class-wc-product-csv-exporter.php
@@ -525,8 +525,8 @@ class WC_Product_CSV_Exporter extends WC_CSV_Batch_Exporter {
 	 * Export meta data.
 	 *
 	 * @since 3.1.0
-	 * @param WC_Product $product
-	 * @param array $row
+	 * @param WC_Product $product Product being exported.
+	 * @param array      $row Row data.
 	 */
 	protected function prepare_meta_for_export( $product, &$row ) {
 		if ( $this->enable_meta_export ) {
@@ -537,17 +537,21 @@ class WC_Product_CSV_Exporter extends WC_CSV_Batch_Exporter {
 
 				$i = 1;
 				foreach ( $meta_data as $meta ) {
-					// Allow 3rd parties to process the meta, e.g. to transform non-scalar values to scalar, thus allowing
-					// them to be exported
-					$meta_value = apply_filters( 'woocommerce_product_export_meta_value', $meta->value, $meta, $product, $row );
-
-					if ( ! is_scalar( $meta_value ) || in_array( $meta->key, $meta_keys_to_skip ) ) {
+					if ( in_array( $meta->key, $meta_keys_to_skip, true ) ) {
 						continue;
 					}
+
+					// Allow 3rd parties to process the meta, e.g. to transform non-scalar values to scalar.
+					$meta_value = apply_filters( 'woocommerce_product_export_meta_value', $meta->value, $meta, $product, $row );
+
+					if ( ! is_scalar( $meta_value ) ) {
+						continue;
+					}
+
 					$column_key                        = 'meta:' . esc_attr( $meta->key );
 					$this->column_names[ $column_key ] = sprintf( __( 'Meta: %s', 'woocommerce' ), $meta->key );
 					$row[ $column_key ]                = $meta_value;
-					$i++;
+					$i ++;
 				}
 			}
 		}
