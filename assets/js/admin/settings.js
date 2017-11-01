c9f685f1e095509979d8d8641b1382bde4368539
Mike Jolley <mike.jolley@me.com>
2017-11-01 13:52:08 +0000

    Simple reset on validation error in iris picker

--- assets/js/admin/settings.js
+++ assets/js/admin/settings.js
@@ -48,6 +48,19 @@
 			event.stopPropagation();
 			$( '.iris-picker' ).hide();
 			$( this ).closest( 'td' ).find( '.iris-picker' ).show();
+			$( this ).data( 'original-value', $( this ).val() );
+		})
+
+		.on( 'change', function() {
+			if ( $( this ).is( '.iris-error' ) ) {
+				var original_value = $( this ).data( 'original-value' );
+
+				if ( original_value.match( /^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/ ) ) {
+					$( this ).val( $( this ).data( 'original-value' ) ).change();
+				} else {
+					$( this ).val( '' ).change();
+				}
+			}
 		});
 
 	$( 'body' ).on( 'click', function() {
