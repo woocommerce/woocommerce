23a4342b2aacf083844c64b204c906464a5ea5a2
Gerhard Potgieter <potgieterg@gmail.com>
2017-11-07 11:58:17 +0200

    Remove escaping around filter, breaks remove button

--- templates/cart/cart.php
+++ templates/cart/cart.php
@@ -53,13 +53,14 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 						<td class="product-remove">
 							<?php
-								echo esc_html( apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
+								// @codingStandardsIgnoreLine
+								echo apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
 									'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
 									esc_url( WC()->cart->get_remove_url( $cart_item_key ) ),
 									__( 'Remove this item', 'woocommerce' ),
 									esc_attr( $product_id ),
 									esc_attr( $_product->get_sku() )
-								), $cart_item_key ) );
+								), $cart_item_key );
 							?>
 						</td>
 
