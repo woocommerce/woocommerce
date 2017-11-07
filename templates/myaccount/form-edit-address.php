10f13c45fc96775c47a3af0591459d2ed0c7822e
Gerhard Potgieter <potgieterg@gmail.com>
2017-11-07 12:15:00 +0200

    input to button for submit in myaccount/form-edit-address.php template

--- templates/myaccount/form-edit-address.php
+++ templates/myaccount/form-edit-address.php
@@ -49,7 +49,7 @@ do_action( 'woocommerce_before_edit_account_address_form' ); ?>
 			<?php do_action( "woocommerce_after_edit_address_form_{$load_address}" ); ?>
 
 			<p>
-				<input type="submit" class="button" name="save_address" value="<?php esc_attr_e( 'Save address', 'woocommerce' ); ?>" />
+				<button type="submit" class="button" name="save_address" value="<?php esc_attr_e( 'Save address', 'woocommerce' ); ?>"><?php esc_html_e( 'Save address', 'woocommerce' ); ?></button>
 				<?php wp_nonce_field( 'woocommerce-edit_address' ); ?>
 				<input type="hidden" name="action" value="edit_address" />
 			</p>
