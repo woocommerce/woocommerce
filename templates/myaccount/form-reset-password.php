23de60b56cf20992fbc569690545ad16460c391b
Gerhard Potgieter <potgieterg@gmail.com>
2017-11-07 12:24:11 +0200

    input to button for submit in myaccount/form-reset-password.php template

--- templates/myaccount/form-reset-password.php
+++ templates/myaccount/form-reset-password.php
@@ -44,7 +44,7 @@ wc_print_notices(); ?>
 
 	<p class="woocommerce-form-row form-row">
 		<input type="hidden" name="wc_reset_password" value="true" />
-		<input type="submit" class="woocommerce-Button button" value="<?php esc_attr_e( 'Save', 'woocommerce' ); ?>" />
+		<button type="submit" class="woocommerce-Button button" value="<?php esc_attr_e( 'Save', 'woocommerce' ); ?>"><?php esc_html_e( 'Save', 'woocommerce' ); ?></button>
 	</p>
 
 	<?php wp_nonce_field( 'reset_password' ); ?>
