a6a63ad939d5701b509f3d5d61c343b713720a9d
Gerhard Potgieter <potgieterg@gmail.com>
2017-11-07 12:14:05 +0200

    PHPCS fixes for myaccount/form-edit-address.php template

--- templates/myaccount/form-edit-address.php
+++ templates/myaccount/form-edit-address.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.0.9
+ * @version 3.3.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -30,19 +30,19 @@ do_action( 'woocommerce_before_edit_account_address_form' ); ?>
 
 	<form method="post">
 
-		<h3><?php echo apply_filters( 'woocommerce_my_account_edit_address_title', $page_title, $load_address ); ?></h3>
+		<h3><?php echo apply_filters( 'woocommerce_my_account_edit_address_title', $page_title, $load_address ); ?></h3><?php // @codingStandardsIgnoreLine ?>
 
 		<div class="woocommerce-address-fields">
 			<?php do_action( "woocommerce_before_edit_address_form_{$load_address}" ); ?>
 
 			<div class="woocommerce-address-fields__field-wrapper">
 				<?php
-					foreach ( $address as $key => $field ) {
-						if ( isset( $field['country_field'], $address[ $field['country_field'] ] ) ) {
-							$field['country'] = wc_get_post_data_by_key( $field['country_field'], $address[ $field['country_field'] ]['value'] );
-						}
-						woocommerce_form_field( $key, $field, wc_get_post_data_by_key( $key, $field['value'] ) );
+				foreach ( $address as $key => $field ) {
+					if ( isset( $field['country_field'], $address[ $field['country_field'] ] ) ) {
+						$field['country'] = wc_get_post_data_by_key( $field['country_field'], $address[ $field['country_field'] ]['value'] );
 					}
+					woocommerce_form_field( $key, $field, wc_get_post_data_by_key( $key, $field['value'] ) );
+				}
 				?>
 			</div>
 
