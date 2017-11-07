86b20dd76b3fd098d167374f775e38d87fd47783
Gerhard Potgieter <potgieterg@gmail.com>
2017-11-07 12:09:12 +0200

    input to button for submit in myaccount/form-edit-account.php template

--- templates/myaccount/form-edit-account.php
+++ templates/myaccount/form-edit-account.php
@@ -63,7 +63,7 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
 
 	<p>
 		<?php wp_nonce_field( 'save_account_details' ); ?>
-		<input type="submit" class="woocommerce-Button button" name="save_account_details" value="<?php esc_attr_e( 'Save changes', 'woocommerce' ); ?>" />
+		<button type="submit" class="woocommerce-Button button" name="save_account_details" value="<?php esc_attr_e( 'Save changes', 'woocommerce' ); ?>"><?php esc_html_e( 'Save changes', 'woocommerce' ); ?></button>
 		<input type="hidden" name="action" value="save_account_details" />
 	</p>
 
