fb446e6bdfeb4b8cc09c57751b5b147b1c2d2bde
Mike Jolley <mike.jolley@me.com>
2017-11-07 20:23:10 +0000

    Add wp_unslash to meta key

--- includes/admin/wc-admin-functions.php
+++ includes/admin/wc-admin-functions.php
@@ -234,7 +234,8 @@ function wc_save_order_items( $order_id, $items ) {
 
 			if ( isset( $items['meta_key'][ $item_id ], $items['meta_value'][ $item_id ] ) ) {
 				foreach ( $items['meta_key'][ $item_id ] as $meta_id => $meta_key ) {
-					$meta_value = isset( $items['meta_value'][ $item_id ][ $meta_id ] ) ? wp_unslash( $items['meta_value'][ $item_id ][ $meta_id ] ) : '';
+					$meta_key   = wp_unslash( $meta_key );
+					$meta_value = isset( $items['meta_value'][ $item_id ][ $meta_id ] ) ? wp_unslash( $items['meta_value'][ $item_id ][ $meta_id ] ): '';
 
 					if ( '' === $meta_key && '' === $meta_value ) {
 						if ( ! strstr( $meta_id, 'new-' ) ) {
