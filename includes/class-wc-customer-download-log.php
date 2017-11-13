dab7dd9c4222401e97df8b9e624ca66f5aaf4115
Mike Jolley <mike.jolley@me.com>
2017-11-13 16:29:44 +0000

    PHPCS

--- includes/class-wc-customer-download-log.php
+++ includes/class-wc-customer-download-log.php
@@ -1,8 +1,4 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
 /**
  * Class for customer download logs.
  *
@@ -11,10 +7,19 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @package		WooCommerce/Classes
  * @author		Automattic
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * WC_Customer_Download_Log.
+ */
 class WC_Customer_Download_Log extends WC_Data {
 
 	/**
 	 * This is the name of this object type.
+	 *
 	 * @var string
 	 */
 	protected $object_type = 'customer_download_log';
@@ -34,9 +39,9 @@ class WC_Customer_Download_Log extends WC_Data {
 	/**
 	 * Constructor.
 	 *
-	 * @param int|object|array $download_log
+	 * @param int|object|array $download_log Download log ID.
 	 */
-	 public function __construct( $download_log = 0 ) {
+	public function __construct( $download_log = 0 ) {
 		parent::__construct( $download_log );
 
 		if ( is_numeric( $download_log ) && $download_log > 0 ) {
@@ -56,7 +61,7 @@ class WC_Customer_Download_Log extends WC_Data {
 		if ( $this->get_id() > 0 ) {
 			$this->data_store->read( $this );
 		}
- 	}
+	}
 
 	/*
 	|--------------------------------------------------------------------------
@@ -67,7 +72,7 @@ class WC_Customer_Download_Log extends WC_Data {
 	/**
 	 * Get timestamp.
 	 *
-	 * @param  string $context
+	 * @param  string $context Get context.
 	 * @return WC_DateTime|null Object if the date is set or null if there is no date.
 	 */
 	public function get_timestamp( $context = 'view' ) {
@@ -77,7 +82,7 @@ class WC_Customer_Download_Log extends WC_Data {
 	/**
 	 * Get permission id.
 	 *
-	 * @param  string $context
+	 * @param  string $context Get context.
 	 * @return integer
 	 */
 	public function get_permission_id( $context = 'view' ) {
@@ -87,7 +92,7 @@ class WC_Customer_Download_Log extends WC_Data {
 	/**
 	 * Get user id.
 	 *
-	 * @param  string $context
+	 * @param  string $context Get context.
 	 * @return integer
 	 */
 	public function get_user_id( $context = 'view' ) {
@@ -97,7 +102,7 @@ class WC_Customer_Download_Log extends WC_Data {
 	/**
 	 * Get user ip address.
 	 *
-	 * @param  string $context
+	 * @param  string $context Get context.
 	 * @return string
 	 */
 	public function get_user_ip_address( $context = 'view' ) {
@@ -122,7 +127,7 @@ class WC_Customer_Download_Log extends WC_Data {
 	/**
 	 * Set permission id.
 	 *
-	 * @param int $value
+	 * @param int $value Value to set.
 	 */
 	public function set_permission_id( $value ) {
 		$this->set_prop( 'permission_id', absint( $value ) );
@@ -131,7 +136,7 @@ class WC_Customer_Download_Log extends WC_Data {
 	/**
 	 * Set user id.
 	 *
-	 * @param int $value
+	 * @param int $value Value to set.
 	 */
 	public function set_user_id( $value ) {
 		$this->set_prop( 'user_id', absint( $value ) );
@@ -140,7 +145,7 @@ class WC_Customer_Download_Log extends WC_Data {
 	/**
 	 * Set user ip address.
 	 *
-	 * @param string $value
+	 * @param string $value Value to set.
 	 */
 	public function set_user_ip_address( $value ) {
 		$this->set_prop( 'user_ip_address', $value );
