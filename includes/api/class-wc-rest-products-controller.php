0066f06a8ff47ce4172bb81e0aa16de3e38ee3e9
Mike Jolley <mike.jolley@me.com>
2017-11-16 19:44:08 +0000

    Schema: Attributes are an array of strings

--- includes/api/class-wc-rest-products-controller.php
+++ includes/api/class-wc-rest-products-controller.php
@@ -1917,6 +1917,9 @@ class WC_REST_Products_Controller extends WC_REST_Legacy_Products_Controller {
 								'description' => __( 'List of available term names of the attribute.', 'woocommerce' ),
 								'type'        => 'array',
 								'context'     => array( 'view', 'edit' ),
+								'items'       => array(
+									'type' => 'string',
+								),
 							),
 						),
 					),
