d27410eaccf9927735442578b8964c57b67b26bd
Claudio Sanches <contato@claudiosanches.com>
2017-11-22 14:47:22 -0200

    Introduced new wc_deprecated_hook function as alias to _deprecated_hook

--- includes/abstracts/abstract-wc-deprecated-hooks.php
+++ includes/abstracts/abstract-wc-deprecated-hooks.php
@@ -81,7 +81,7 @@ abstract class WC_Deprecated_Hooks {
 	 * @param string $new_hook
 	 */
 	protected function display_notice( $old_hook, $new_hook ) {
-		wc_deprecated_function( sprintf( 'The "%s" hook uses out of date data structures and', esc_html( $old_hook ) ), WC_VERSION, esc_html( $new_hook ) );
+		wc_deprecated_hook( esc_html( $old_hook ), WC_VERSION, esc_html( $new_hook ) );
 	}
 
 	/**
