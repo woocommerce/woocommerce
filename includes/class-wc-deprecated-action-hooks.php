4b5f0c98d7f15628e3a972bb012fc079e441e9f8
Claudio Sanches <contato@claudiosanches.com>
2017-11-22 15:25:27 -0200

    Fixed coding standards

--- includes/class-wc-deprecated-action-hooks.php
+++ includes/class-wc-deprecated-action-hooks.php
@@ -1,12 +1,18 @@
 <?php
+/**
+ * Deprecated action hooks
+ *
+ * @package WooCommerce\Abstracts
+ * @since   3.0.0
+ * @version 3.3.0
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 /**
  * Handles deprecation notices and triggering of legacy action hooks.
- *
- * @since 3.0.0
  */
 class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 
@@ -16,7 +22,7 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 	 * @var array
 	 */
 	protected $deprecated_hooks = array(
-		'woocommerce_new_order_item'                        => array(
+		'woocommerce_new_order_item'        => array(
 			'woocommerce_order_add_shipping',
 			'woocommerce_order_add_coupon',
 			'woocommerce_order_add_tax',
@@ -25,16 +31,16 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 			'woocommerce_add_order_item_meta',
 			'woocommerce_add_order_fee_meta',
 		),
-		'woocommerce_update_order_item'                     => array(
+		'woocommerce_update_order_item'     => array(
 			'woocommerce_order_edit_product',
 			'woocommerce_order_update_coupon',
 			'woocommerce_order_update_shipping',
 			'woocommerce_order_update_fee',
 			'woocommerce_order_update_tax',
 		),
-		'woocommerce_new_payment_token'                     => 'woocommerce_payment_token_created',
-		'woocommerce_new_product_variation'                 => 'woocommerce_create_product_variation',
-		'woocommerce_order_details_after_order_table_items' => 'woocommerce_order_items_table'
+		'woocommerce_new_payment_token'     => 'woocommerce_payment_token_created',
+		'woocommerce_new_product_variation' => 'woocommerce_create_product_variation',
+		'woocommerce_order_details_after_order_table_items' => 'woocommerce_order_items_table',
 	);
 
 	/**
@@ -62,7 +68,8 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 
 	/**
 	 * Hook into the new hook so we can handle deprecated hooks once fired.
-	 * @param  string $hook_name
+	 *
+	 * @param string $hook_name Hook name.
 	 */
 	public function hook_in( $hook_name ) {
 		add_action( $hook_name, array( $this, 'maybe_handle_deprecated_hook' ), -1000, 8 );
@@ -71,10 +78,10 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 	/**
 	 * If the old hook is in-use, trigger it.
 	 *
-	 * @param string $new_hook
-	 * @param string $old_hook
-	 * @param array $new_callback_args
-	 * @param mixed $return_value
+	 * @param  string $new_hook          New hook name.
+	 * @param  string $old_hook          Old hook name.
+	 * @param  array  $new_callback_args New callback args.
+	 * @param  mixed  $return_value      Returned value.
 	 * @return mixed
 	 */
 	public function handle_deprecated_hook( $new_hook, $old_hook, $new_callback_args, $return_value ) {
@@ -88,14 +95,14 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 	/**
 	 * Fire off a legacy hook with it's args.
 	 *
-	 * @param  string $old_hook
-	 * @param  array $new_callback_args
+	 * @param  string $old_hook          Old hook name.
+	 * @param  array  $new_callback_args New callback args.
 	 * @return mixed
 	 */
 	protected function trigger_hook( $old_hook, $new_callback_args ) {
 		switch ( $old_hook ) {
-			case 'woocommerce_order_add_shipping' :
-			case 'woocommerce_order_add_fee' :
+			case 'woocommerce_order_add_shipping':
+			case 'woocommerce_order_add_fee':
 				$item_id  = $new_callback_args[0];
 				$item     = $new_callback_args[1];
 				$order_id = $new_callback_args[2];
@@ -103,7 +110,7 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 					do_action( $old_hook, $order_id, $item_id, $item );
 				}
 				break;
-			case 'woocommerce_order_add_coupon' :
+			case 'woocommerce_order_add_coupon':
 				$item_id  = $new_callback_args[0];
 				$item     = $new_callback_args[1];
 				$order_id = $new_callback_args[2];
@@ -111,7 +118,7 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 					do_action( $old_hook, $order_id, $item_id, $item->get_code(), $item->get_discount(), $item->get_discount_tax() );
 				}
 				break;
-			case 'woocommerce_order_add_tax' :
+			case 'woocommerce_order_add_tax':
 				$item_id  = $new_callback_args[0];
 				$item     = $new_callback_args[1];
 				$order_id = $new_callback_args[2];
@@ -119,7 +126,7 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 					do_action( $old_hook, $order_id, $item_id, $item->get_rate_id(), $item->get_tax_total(), $item->get_shipping_tax_total() );
 				}
 				break;
-			case 'woocommerce_add_shipping_order_item' :
+			case 'woocommerce_add_shipping_order_item':
 				$item_id  = $new_callback_args[0];
 				$item     = $new_callback_args[1];
 				$order_id = $new_callback_args[2];
@@ -127,7 +134,7 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 					do_action( $old_hook, $order_id, $item_id, $item->legacy_package_key );
 				}
 				break;
-			case 'woocommerce_add_order_item_meta' :
+			case 'woocommerce_add_order_item_meta':
 				$item_id  = $new_callback_args[0];
 				$item     = $new_callback_args[1];
 				$order_id = $new_callback_args[2];
@@ -135,7 +142,7 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 					do_action( $old_hook, $item_id, $item->legacy_values, $item->legacy_cart_item_key );
 				}
 				break;
-			case 'woocommerce_add_order_fee_meta' :
+			case 'woocommerce_add_order_fee_meta':
 				$item_id  = $new_callback_args[0];
 				$item     = $new_callback_args[1];
 				$order_id = $new_callback_args[2];
@@ -143,7 +150,7 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 					do_action( $old_hook, $order_id, $item_id, $item->legacy_fee, $item->legacy_fee_key );
 				}
 				break;
-			case 'woocommerce_order_edit_product' :
+			case 'woocommerce_order_edit_product':
 				$item_id  = $new_callback_args[0];
 				$item     = $new_callback_args[1];
 				$order_id = $new_callback_args[2];
@@ -151,15 +158,15 @@ class WC_Deprecated_Action_Hooks extends WC_Deprecated_Hooks {
 					do_action( $old_hook, $order_id, $item_id, $item, $item->get_product() );
 				}
 				break;
-			case 'woocommerce_order_update_coupon' :
-			case 'woocommerce_order_update_shipping' :
-			case 'woocommerce_order_update_fee' :
-			case 'woocommerce_order_update_tax' :
+			case 'woocommerce_order_update_coupon':
+			case 'woocommerce_order_update_shipping':
+			case 'woocommerce_order_update_fee':
+			case 'woocommerce_order_update_tax':
 				if ( ! is_a( $item, 'WC_Order_Item_Product' ) ) {
 					do_action( $old_hook, $order_id, $item_id, $item );
 				}
 				break;
-			default :
+			default:
 				do_action_ref_array( $old_hook, $new_callback_args );
 				break;
 		}
--- includes/class-wc-deprecated-filter-hooks.php
+++ includes/class-wc-deprecated-filter-hooks.php
@@ -1,12 +1,18 @@
 <?php
+/**
+ * Deprecated filter hooks
+ *
+ * @package WooCommerce\Abstracts
+ * @since   3.0.0
+ * @version 3.3.0
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 /**
- * Handles deprecation notices and triggering of legacy filter hooks.
- *
- * @since 3.0.0
+ * Handles deprecation notices and triggering of legacy filter hooks
  */
 class WC_Deprecated_Filter_Hooks extends WC_Deprecated_Hooks {
 
@@ -99,7 +105,8 @@ class WC_Deprecated_Filter_Hooks extends WC_Deprecated_Hooks {
 
 	/**
 	 * Hook into the new hook so we can handle deprecated hooks once fired.
-	 * @param  string $hook_name
+	 *
+	 * @param string $hook_name Hook name.
 	 */
 	public function hook_in( $hook_name ) {
 		add_filter( $hook_name, array( $this, 'maybe_handle_deprecated_hook' ), -1000, 8 );
@@ -108,10 +115,10 @@ class WC_Deprecated_Filter_Hooks extends WC_Deprecated_Hooks {
 	/**
 	 * If the old hook is in-use, trigger it.
 	 *
-	 * @param string $new_hook
-	 * @param string $old_hook
-	 * @param array $new_callback_args
-	 * @param mixed $return_value
+	 * @param  string $new_hook          New hook name.
+	 * @param  string $old_hook          Old hook name.
+	 * @param  array  $new_callback_args New callback args.
+	 * @param  mixed  $return_value      Returned value.
 	 * @return mixed
 	 */
 	public function handle_deprecated_hook( $new_hook, $old_hook, $new_callback_args, $return_value ) {
@@ -125,8 +132,8 @@ class WC_Deprecated_Filter_Hooks extends WC_Deprecated_Hooks {
 	/**
 	 * Fire off a legacy hook with it's args.
 	 *
-	 * @param  string $old_hook
-	 * @param  array $new_callback_args
+	 * @param  string $old_hook          Old hook name.
+	 * @param  array  $new_callback_args New callback args.
 	 * @return mixed
 	 */
 	protected function trigger_hook( $old_hook, $new_callback_args ) {
