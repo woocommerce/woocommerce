51867ac7c30abcde84d67fcbe2849c18cd195048
Rodrigo Primo <rodrigosprimo@gmail.com>
2017-11-23 09:05:44 -0200

    Remove Xdebug from PHP 5.3 Travis build job as well

--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -6,9 +6,6 @@ if [ $1 == 'before' ]; then
 	# Composer install fails in PHP 5.2
 	[[ ${TRAVIS_PHP_VERSION} == '5.2' ]] && exit;
 
-	# No Xdebug and therefore no coverage in PHP 5.3
-	[[ ${TRAVIS_PHP_VERSION} == '5.3' ]] && exit;
-
 	if [[ ${TRAVIS_PHP_VERSION:0:2} == "5." ]]; then
 		composer global require "phpunit/phpunit=4.8.*"
 	else
