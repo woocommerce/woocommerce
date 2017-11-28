8cc9212a43da047851115719612f7e9c05aee5ce
Grzegorz Rola <grola@seostudio.pl>
2017-11-28 20:42:47 +0100

    Atributes with 0 (zero) values cannot be passed to form field

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -1998,7 +1998,6 @@ if ( ! function_exists( 'woocommerce_form_field' ) ) {
 
 		// Custom attribute handling.
 		$custom_attributes         = array();
-		$args['custom_attributes'] = array_filter( (array) $args['custom_attributes'] );
 
 		if ( $args['maxlength'] ) {
 			$args['custom_attributes']['maxlength'] = absint( $args['maxlength'] );
