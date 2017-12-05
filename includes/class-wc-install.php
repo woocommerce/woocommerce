c39a181cb6fb3ae1a1a2d32a92b4ecc677ff3dd9
Jonathan Belcher <belcherj@gmail.com>
2017-12-05 12:23:29 -0500

    Clear addons transients on update to 3.3.0

--- includes/class-wc-install.php
+++ includes/class-wc-install.php
@@ -101,6 +101,7 @@ class WC_Install {
 			'wc_update_330_product_stock_status',
 			'wc_update_330_set_default_product_cat',
 			'wc_update_330_db_version',
+			'wc_update_330_clear_transients',
 		),
 	);
 
