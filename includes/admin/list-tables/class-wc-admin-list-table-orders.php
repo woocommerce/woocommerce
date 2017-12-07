5e51af9d2cf1bbead94443949f371caf1423bedf
Mike Jolley <mike.jolley@me.com>
2017-12-07 15:50:27 +0000

    Fix white space in order actions

--- includes/admin/list-tables/class-wc-admin-list-table-orders.php
+++ includes/admin/list-tables/class-wc-admin-list-table-orders.php
@@ -258,7 +258,7 @@ class WC_Admin_List_Table_Orders extends WC_Admin_List_Table {
 
 		if ( $this->object->has_status( array( 'pending', 'on-hold' ) ) ) {
 			$actions['processing'] = array(
-				'url'     => wp_nonce_url( admin_url( 'admin-ajax.php?action = woocommerce_mark_order_status&status = processing&order_id = ' . $this->object->get_id() ), 'woocommerce-mark-order-status' ),
+				'url'     => wp_nonce_url( admin_url( 'admin-ajax.php?action=woocommerce_mark_order_status&status=processing&order_id=' . $this->object->get_id() ), 'woocommerce-mark-order-status' ),
 				'name'    => __( 'Processing', 'woocommerce' ),
 				'action'  => 'processing',
 			);
@@ -266,7 +266,7 @@ class WC_Admin_List_Table_Orders extends WC_Admin_List_Table {
 
 		if ( $this->object->has_status( array( 'pending', 'on-hold', 'processing' ) ) ) {
 			$actions['complete'] = array(
-				'url'     => wp_nonce_url( admin_url( 'admin-ajax.php?action = woocommerce_mark_order_status&status = completed&order_id = ' . $this->object->get_id() ), 'woocommerce-mark-order-status' ),
+				'url'     => wp_nonce_url( admin_url( 'admin-ajax.php?action=woocommerce_mark_order_status&status=completed&order_id=' . $this->object->get_id() ), 'woocommerce-mark-order-status' ),
 				'name'    => __( 'Complete', 'woocommerce' ),
 				'action'  => 'complete',
 			);
