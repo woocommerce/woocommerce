b1edca8b90d1e5f383ff61bf8c650f087a7de622
Brent Sessions <brent.sessions@automattic.com>
2017-12-19 10:31:31 -0800

    Attempting to get tests to run in Travis CI

---
+++ tests/e2e-tests/scripts/git-woo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+export APP_NAME=$1
+if [ "$APP_NAME" == "" ]; then
+  echo "Please supply app directory name!"
+  exit 1
+elif [ ! -d apps/$APP_NAME/public/wp-content/plugins ]; then
+  echo "App directory apps/$APP_NAME/public/wp-content/plugins does not exist."
+  exit 1
+fi
+
+cd apps/$APP_NAME/public/
+
+prefix=`wp db prefix`
+sed -i 's/wp_commentmeta/'"$prefix"'commentmeta/g' ~/e2e-db.sql
+sed -i 's/wp_comments/'"$prefix"'comments/g' ~/e2e-db.sql
+sed -i 's/wp_links/'"$prefix"'links/g' ~/e2e-db.sql
+sed -i 's/wp_options/'"$prefix"'options/g' ~/e2e-db.sql
+sed -i 's/wp_postmeta/'"$prefix"'postmeta/g' ~/e2e-db.sql
+sed -i 's/wp_posts/'"$prefix"'posts/g' ~/e2e-db.sql
+sed -i 's/wp_term_relationships/'"$prefix"'term_relationships/g' ~/e2e-db.sql
+sed -i 's/wp_relationships/'"$prefix"'relationships/g' ~/e2e-db.sql
+sed -i 's/wp_term_taxonomy/'"$prefix"'term_taxonomy/g' ~/e2e-db.sql
+sed -i 's/wp_termmeta/'"$prefix"'termmeta/g' ~/e2e-db.sql
+sed -i 's/wp_terms/'"$prefix"'terms/g' ~/e2e-db.sql
+sed -i 's/wp_usermeta/'"$prefix"'usermeta/g' ~/e2e-db.sql
+sed -i 's/wp_users/'"$prefix"'users/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_api_keys/'"$prefix"'woocommerce_api_keys/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_attribute_taxonomies/'"$prefix"'woocommerce_attribute_taxonomies/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_downloadable_product_permissions/'"$prefix"'woocommerce_downloadable_product_permissions/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_log/'"$prefix"'woocommerce_log/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_order_itemmeta/'"$prefix"'woocommerce_order_itemmeta/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_order_items/'"$prefix"'woocommerce_order_items/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_payment_tokenmeta/'"$prefix"'woocommerce_payment_tokenmeta/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_payment_tokens/'"$prefix"'woocommerce_payment_tokens/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_sessions/'"$prefix"'woocommerce_sessions/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_shipping_zone_locations/'"$prefix"'woocommerce_shipping_zone_locations/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_shipping_zone_methods/'"$prefix"'woocommerce_shipping_zone_methods/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_shipping_zones/'"$prefix"'woocommerce_shipping_zones/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_tax_rate_locations/'"$prefix"'woocommerce_tax_rate_locations/g' ~/e2e-db.sql
+sed -i 's/wp_woocommerce_tax_rates/'"$prefix"'woocommerce_tax_rates/g' ~/e2e-db.sql
+
+wp theme install twentytwelve --activate
+wp plugin install https://github.com/woocommerce/woocommerce/archive/master.zip --activate
+wp db import ~/e2e-db.sql --debug
---
+++ tests/e2e-tests/scripts/wp-serverpilot-init.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+const config = require( 'config' );
+const ServerPilot = require( 'serverpilot' );
+
+const spConfig = config.get( 'spConfig' );
+
+const sp = new ServerPilot( {
+	clientId: spConfig.clientId,
+	apiKey: spConfig.apiKey
+} );
+
+const userConfig = config.get( 'testAccounts' );
+const username = userConfig.wooUserCI[0];
+const password = userConfig.wooUserCI[1];
+
+const serverPrefix = process.env.TRAVIS_PULL_REQUEST_SHA.substr( 0, 20 );
+const actionWaitTimeout = 2000;
+
+const serverOptions = {
+	name: `wordpress-${serverPrefix}`,
+	sysuserid: spConfig.sysuserid,
+	runtime: 'php7.2',
+	domains: [ `${serverPrefix}.wp-e2e-tests.pw` ],
+	wordpress: {
+		site_title: 'WooCommerce e2e Testing',
+		admin_user: username,
+		admin_password: password,
+		admin_email: 'example@example.com'
+	}
+};
+sp.createApp( serverOptions, function( err, data ) {
+	if ( err !== null ) {
+		console.log( err );
+		throw err;
+	}
+	waitForServerPilotAction( data.actionid, function( actionErr ) {
+		if ( err !== null ) {
+			console.log( actionErr );
+			throw err;
+		}
+		console.log( `Site created - http://${serverPrefix}.wp-e2e-tests.pw - ID ${data.data.id}` );
+	} );
+} );
+
+/**
+ * Waits for ServerPilot to finish an action
+ * @param  {String}   actionId The ServerPilot action Id as returned by the create app endpoint
+ * @param  {Function} cb       What to do after the action finishes or errors
+ */
+function waitForServerPilotAction( actionId, cb ) {
+	setTimeout( function() {
+		sp.getActionStatus( actionId, function( err, response ) {
+			if ( err ) {
+				return cb( err );
+			}
+			if ( response.data.status === 'error' ) {
+				return cb( new Error( 'ServerPilot app creation has completed but there were errors.' ) );
+			}
+			// If ServerPilot is still provisioning the app, recur
+			if ( response.data.status === 'open' ) {
+				return waitForServerPilotAction( actionId, cb );
+			}
+			cb();
+		} );
+	}, actionWaitTimeout );
+}
