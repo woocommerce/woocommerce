00df9ba14570fd5a9b7d7695cc3a4d4e78505669
Brent Sessions <brent.sessions@automattic.com>
2017-12-20 12:00:58 -0800

    Reorganizing

---
+++ tests/e2e-tests/scripts/run-e2e.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -x
+
+# Setup
+sh -e /etc/init.d/xvfb start
+sleep 3
+npm install
+export NODE_CONFIG_DIR="./tests/e2e-tests/config"
+
+# Delete existing site if it exists and then create new
+./tests/e2e-tests/scripts/wp-serverpilot-delete.js
+./tests/e2e-tests/scripts/wp-serverpilot-init.js
+
+# Import the encrypted SSH key
+openssl aes-256-cbc -K $encrypted_aa1eba18da39_key -iv $encrypted_aa1eba18da39_iv -in tests/e2e-tests/scripts/deploy-key.enc -out deploy-key -d
+chmod 600 deploy-key
+mv deploy-key ~/.ssh/id_rsa
+
+# Configure new server
+scp -o "StrictHostKeyChecking no" tests/e2e-tests/scripts/git-woo.sh serverpilot@wp-e2e-tests.pw:~serverpilot/git-woo.sh
+scp -o "StrictHostKeyChecking no" tests/e2e-tests/data/e2e-db.sql serverpilot@wp-e2e-tests.pw:~serverpilot/e2e-db.sql
+ssh -o "StrictHostKeyChecking no" serverpilot@wp-e2e-tests.pw ~serverpilot/git-woo.sh wordpress-${TRAVIS_JOB_ID:0:20}
+
+# Run the tests
+npm run test
+
+# Delete the site when complete
+./tests/e2e-tests/scripts/jetpack/wp-serverpilot-delete.js
+
+
--- tests/e2e-tests/scripts/ssh-configure.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-# Import the encrypted SSH key
-openssl aes-256-cbc -K $encrypted_aa1eba18da39_key -iv $encrypted_aa1eba18da39_iv -in tests/e2e-tests/scripts/deploy-key.enc -out deploy-key -d
-chmod 600 deploy-key
-mv deploy-key ~/.ssh/id_rsa
-
