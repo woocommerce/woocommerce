b3769387669bf6bd51453c5f47af3836641fe7f1
Brent Sessions <brent.sessions@automattic.com>
2017-12-20 10:49:58 -0800

    Fixing broken script

--- tests/e2e-tests/scripts/ssh-configure.sh
+++ tests/e2e-tests/scripts/ssh-configure.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 openssl aes-256-cbc -K $encrypted_aa1eba18da39_key -iv $encrypted_aa1eba18da39_iv -in tests/e2e-tests/scripts/deploy-key.enc -out deploy-key -d
-rm deploy-key.enc
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
