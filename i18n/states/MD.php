8bdc9a6a1de04ccbc949d15b85803d669352c99f
Claudio Sanches <contato@claudiosanches.com>
2017-12-20 15:49:50 -0200

    Fixed coding standards

--- i18n/states/MD.php
+++ i18n/states/MD.php
@@ -1,55 +1,56 @@
 <?php
 /**
- * Moldova
+ * Moldova states
  *
- * @author      Alexander Minza
- * @category    i18n
- * @package     WooCommerce/i18n
- * @version     3.1.0
+ * For more details check:
+ * https://ro.wikipedia.org/wiki/Organizarea_administrativ-teritorial%C4%83_a_Republicii_Moldova
+ * https://ro.wikipedia.org/wiki/Raioanele_Republicii_Moldova
+ *
+ * @author  Alexander Minza
+ * @package WooCommerce/i18n
+ * @version 3.3.0
  */
+
 global $states;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-//https://ro.wikipedia.org/wiki/Organizarea_administrativ-teritorial%C4%83_a_Republicii_Moldova
-//https://ro.wikipedia.org/wiki/Raioanele_Republicii_Moldova
-
 $states['MD'] = array(
-	'C'  => __( 'Chişinău' , 'woocommerce' ),
-	'BL' => __( 'Bălţi' , 'woocommerce' ),
-	'AN' => __( 'Anenii Noi' , 'woocommerce' ),
-	'BS' => __( 'Basarabeasca' , 'woocommerce' ),
-	'BR' => __( 'Briceni' , 'woocommerce' ),
-	'CH' => __( 'Cahul' , 'woocommerce' ),
-	'CT' => __( 'Cantemir' , 'woocommerce' ),
-	'CL' => __( 'Călăraşi' , 'woocommerce' ),
-	'CS' => __( 'Căuşeni' , 'woocommerce' ),
-	'CM' => __( 'Cimişlia' , 'woocommerce' ),
-	'CR' => __( 'Criuleni' , 'woocommerce' ),
-	'DN' => __( 'Donduşeni' , 'woocommerce' ),
-	'DR' => __( 'Drochia' , 'woocommerce' ),
-	'DB' => __( 'Dubăsari' , 'woocommerce' ),
-	'ED' => __( 'Edineţ' , 'woocommerce' ),
-	'FL' => __( 'Făleşti' , 'woocommerce' ),
-	'FR' => __( 'Floreşti' , 'woocommerce' ),
-	'GE' => __( 'Găgăuzia' , 'woocommerce' ),
-	'GL' => __( 'Glodeni' , 'woocommerce' ),
-	'HN' => __( 'Hînceşti' , 'woocommerce' ),
-	'IL' => __( 'Ialoveni' , 'woocommerce' ),
-	'LV' => __( 'Leova' , 'woocommerce' ),
-	'NS' => __( 'Nisporeni' , 'woocommerce' ),
-	'OC' => __( 'Ocniţa' , 'woocommerce' ),
-	'OR' => __( 'Orhei' , 'woocommerce' ),
-	'RZ' => __( 'Rezina' , 'woocommerce' ),
-	'RS' => __( 'Rîşcani' , 'woocommerce' ),
-	'SG' => __( 'Sîngerei' , 'woocommerce' ),
-	'SR' => __( 'Soroca' , 'woocommerce' ),
-	'ST' => __( 'Străşeni' , 'woocommerce' ),
-	'SD' => __( 'Şoldăneşti' , 'woocommerce' ),
-	'SV' => __( 'Ştefan Vodă' , 'woocommerce' ),
-	'TR' => __( 'Taraclia' , 'woocommerce' ),
-	'TL' => __( 'Teleneşti' , 'woocommerce' ),
-	'UN' => __( 'Ungheni' , 'woocommerce' )
+	'C'  => __( 'Chi&scedil;in&abreve;u', 'woocommerce' ),
+	'BL' => __( 'B&abreve;l&tcedil;i', 'woocommerce' ),
+	'AN' => __( 'Anenii Noi', 'woocommerce' ),
+	'BS' => __( 'Basarabeasca', 'woocommerce' ),
+	'BR' => __( 'Briceni', 'woocommerce' ),
+	'CH' => __( 'Cahul', 'woocommerce' ),
+	'CT' => __( 'Cantemir', 'woocommerce' ),
+	'CL' => __( 'C&abreve;l&abreve;ra&scedil;i', 'woocommerce' ),
+	'CS' => __( 'C&abreve;u&scedil;eni', 'woocommerce' ),
+	'CM' => __( 'Cimi&scedil;lia', 'woocommerce' ),
+	'CR' => __( 'Criuleni', 'woocommerce' ),
+	'DN' => __( 'Donduşeni', 'woocommerce' ),
+	'DR' => __( 'Drochia', 'woocommerce' ),
+	'DB' => __( 'Dub&abreve;sari', 'woocommerce' ),
+	'ED' => __( 'Edineţ', 'woocommerce' ),
+	'FL' => __( 'F&abreve;le&scedil;ti', 'woocommerce' ),
+	'FR' => __( 'Floreşti', 'woocommerce' ),
+	'GE' => __( 'G&abreve;g&abreve;uzia', 'woocommerce' ),
+	'GL' => __( 'Glodeni', 'woocommerce' ),
+	'HN' => __( 'H&icirc;nce&scedil;ti', 'woocommerce' ),
+	'IL' => __( 'Ialoveni', 'woocommerce' ),
+	'LV' => __( 'Leova', 'woocommerce' ),
+	'NS' => __( 'Nisporeni', 'woocommerce' ),
+	'OC' => __( 'Ocni&tcedil;a', 'woocommerce' ),
+	'OR' => __( 'Orhei', 'woocommerce' ),
+	'RZ' => __( 'Rezina', 'woocommerce' ),
+	'RS' => __( 'R&icirc;&scedil;cani', 'woocommerce' ),
+	'SG' => __( 'S&icirc;ngerei', 'woocommerce' ),
+	'SR' => __( 'Soroca', 'woocommerce' ),
+	'ST' => __( 'Str&abreve;&scedil;eni', 'woocommerce' ),
+	'SD' => __( '&Scedil;old&abreve;ne&scedil;ti', 'woocommerce' ),
+	'SV' => __( '&Scedil;tefan Vod&abreve;', 'woocommerce' ),
+	'TR' => __( 'Taraclia', 'woocommerce' ),
+	'TL' => __( 'Telene&scedil;ti', 'woocommerce' ),
+	'UN' => __( 'Ungheni', 'woocommerce' ),
 );
