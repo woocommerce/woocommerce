17af3c06508084aeebb96f69578f74a2b1bdf8d5
Brent Sessions <brent.sessions@automattic.com>
2017-12-20 11:33:37 -0800

    Trying to get bash output

--- tests/e2e-tests/scripts/ssh-configure.sh
+++ tests/e2e-tests/scripts/ssh-configure.sh
@@ -3,5 +3,10 @@ set -x
 
 # Import the encrypted SSH key
 openssl aes-256-cbc -K $encrypted_aa1eba18da39_key -iv $encrypted_aa1eba18da39_iv -in tests/e2e-tests/scripts/deploy-key.enc -out deploy-key -d
+echo decrypted
 chmod 600 deploy-key
+echo chmodded
+mkdir ~/.ssh
+echo directory created
 mv deploy-key ~/.ssh/id_rsa
+echo key moved
