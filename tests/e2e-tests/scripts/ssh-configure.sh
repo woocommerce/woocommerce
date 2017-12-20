12647508c873f69fd8a999be33885ca0c84d52a3
Brent Sessions <brent.sessions@automattic.com>
2017-12-20 11:13:26 -0800

    adjusting paths

--- tests/e2e-tests/scripts/ssh-configure.sh
+++ tests/e2e-tests/scripts/ssh-configure.sh
@@ -2,6 +2,6 @@
 set -x
 
 # Import the encrypted SSH key
-openssl aes-256-cbc -K $encrypted_aa1eba18da39_key -iv $encrypted_aa1eba18da39_iv -in tests/e2e-tests/scripts/deploy-key.enc -out tests/e2e-tests/scripts/deploy-key -d
-chmod 600 tests/e2e-tests/scripts/deploy-key
-mv tests/e2e-tests/scripts/deploy-key ~/.ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_aa1eba18da39_key -iv $encrypted_aa1eba18da39_iv -in tests/e2e-tests/scripts/deploy-key.enc -out deploy-key -d
+chmod 600 deploy-key
+mv deploy-key ~/.ssh/id_rsa
