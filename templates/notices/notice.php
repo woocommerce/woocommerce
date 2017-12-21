be895e0c84452f8a360c083f7fe4b5137d4a401e
Rami Yushuvaev <r_a_m_i@hotmail.com>
2017-12-21 06:23:15 +0200

    a11y: add `role=alert` to woocommerce info msgs
    See #18074 and #18096

--- templates/notices/notice.php
+++ templates/notices/notice.php
@@ -13,7 +13,7 @@
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     1.6.4
+ * @version     3.3.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -27,5 +27,5 @@ if ( ! $messages ) {
 ?>
 
 <?php foreach ( $messages as $message ) : ?>
-	<div class="woocommerce-info"><?php echo wp_kses_post( $message ); ?></div>
+	<div class="woocommerce-info" role="alert"><?php echo wp_kses_post( $message ); ?></div>
 <?php endforeach; ?>
