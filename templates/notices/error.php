533df4bd0a4d1f43039295f875a2663d6bf23de2
Ibrahim <ibrahim@transvelo.com>
2017-12-22 20:01:04 +0530

    error.php is different from notice and success
    The error notice is displayed in <ul> tags while success and notice messages are displayed in div. This will make sure all the notices follow the same structure

--- templates/notices/error.php
+++ templates/notices/error.php
@@ -25,8 +25,7 @@ if ( ! $messages ) {
 }
 
 ?>
-<ul class="woocommerce-error" role="alert">
-	<?php foreach ( $messages as $message ) : ?>
-		<li><?php echo wp_kses_post( $message ); ?></li>
-	<?php endforeach; ?>
-</ul>
+
+<?php foreach ( $messages as $message ) : ?>
+	<div class="woocommerce-error" role="alert"><?php echo wp_kses_post( $message ); ?></div>
+<?php endforeach; ?>
