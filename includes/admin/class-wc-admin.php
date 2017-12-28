581ed359d6c14492fc855a0e1560b3df98c50a92
Will Gorham <willgorham@gmail.com>
2017-12-28 00:19:50 -0500

    Coding standards

--- includes/admin/class-wc-admin.php
+++ includes/admin/class-wc-admin.php
@@ -3,14 +3,12 @@
  * WooCommerce Admin
  *
  * @class    WC_Admin
- * @author   WooThemes
- * @category Admin
  * @package  WooCommerce/Admin
  * @version  2.6.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
 /**
@@ -58,29 +56,29 @@ class WC_Admin {
 		include_once( dirname( __FILE__ ) . '/class-wc-admin-importers.php' );
 		include_once( dirname( __FILE__ ) . '/class-wc-admin-exporters.php' );
 
-		// Gridicons
+		// Gridicons.
 		include_once( WC_ABSPATH . '/includes/libraries/gridicons.php' );
 
-		// Help Tabs
+		// Help Tabs.
 		if ( apply_filters( 'woocommerce_enable_admin_help_tab', true ) ) {
 			include_once( dirname( __FILE__ ) . '/class-wc-admin-help.php' );
 		}
 
-		// Setup/welcome
+		// Setup/welcome.
 		if ( ! empty( $_GET['page'] ) ) {
 			switch ( $_GET['page'] ) {
-				case 'wc-setup' :
+				case 'wc-setup':
 					include_once( dirname( __FILE__ ) . '/class-wc-admin-setup-wizard.php' );
-				break;
+					break;
 			}
 		}
 
-		// Importers
+		// Importers.
 		if ( defined( 'WP_LOAD_IMPORTERS' ) ) {
 			include_once( dirname( __FILE__ ) . '/class-wc-admin-importers.php' );
 		}
 
-		// Helper
+		// Helper.
 		include_once( dirname( __FILE__ ) . '/helper/class-wc-helper-options.php' );
 		include_once( dirname( __FILE__ ) . '/helper/class-wc-helper-api.php' );
 		include_once( dirname( __FILE__ ) . '/helper/class-wc-helper-updater.php' );
@@ -93,29 +91,30 @@ class WC_Admin {
 	 * Include admin files conditionally.
 	 */
 	public function conditional_includes() {
-		if ( ! $screen = get_current_screen() ) {
+		$screen = get_current_screen();
+		if ( ! $screen ) {
 			return;
 		}
 
 		switch ( $screen->id ) {
-			case 'dashboard' :
+			case 'dashboard':
 				include( 'class-wc-admin-dashboard.php' );
-			break;
-			case 'options-permalink' :
+				break;
+			case 'options-permalink':
 				include( 'class-wc-admin-permalink-settings.php' );
-			break;
-			case 'plugins' :
-				include ( 'plugin-updates/class-wc-plugins-screen-updates.php' );
-			break;
-			case 'update-core' :
+				break;
+			case 'plugins':
+				include( 'plugin-updates/class-wc-plugins-screen-updates.php' );
+				break;
+			case 'update-core':
 				include( 'plugin-updates/class-wc-updates-screen-updates.php' );
-			break;
-			case 'users' :
-			case 'user' :
-			case 'profile' :
-			case 'user-edit' :
+				break;
+			case 'users':
+			case 'user':
+			case 'profile':
+			case 'user-edit':
 				include( 'class-wc-admin-profile.php' );
-			break;
+				break;
 		}
 	}
 
@@ -125,9 +124,9 @@ class WC_Admin {
 	 * For setup wizard, transient must be present, the user must have access rights, and we must ignore the network/bulk plugin updaters.
 	 */
 	public function admin_redirects() {
-		// Nonced plugin install redirects (whitelisted)
+		// Nonced plugin install redirects (whitelisted).
 		if ( ! empty( $_GET['wc-install-plugin-redirect'] ) ) {
-			$plugin_slug = wc_clean( $_GET['wc-install-plugin-redirect'] );
+			$plugin_slug = wc_clean( wp_unslash( $_GET['wc-install-plugin-redirect'] ) );
 
 			if ( current_user_can( 'install_plugins' ) && in_array( $plugin_slug, array( 'woocommerce-gateway-stripe' ) ) ) {
 				$nonce = wp_create_nonce( 'install-plugin_' . $plugin_slug );
@@ -140,15 +139,15 @@ class WC_Admin {
 			exit;
 		}
 
-		// Setup wizard redirect
+		// Setup wizard redirect.
 		if ( get_transient( '_wc_activation_redirect' ) ) {
 			delete_transient( '_wc_activation_redirect' );
 
-			if ( ( ! empty( $_GET['page'] ) && in_array( $_GET['page'], array( 'wc-setup' ) ) ) || is_network_admin() || isset( $_GET['activate-multi'] ) || ! current_user_can( 'manage_woocommerce' ) || apply_filters( 'woocommerce_prevent_automatic_wizard_redirect', false ) ) {
+			if ( ( ! empty( $_GET['page'] ) && in_array( $_GET['page'], array( 'wc-setup' ) ) ) || is_network_admin() || isset( $_GET['activate-multi'] ) || ! current_user_can( 'manage_woocommerce' ) || apply_filters( 'woocommerce_prevent_automatic_wizard_redirect', false ) ) { // WPCS: Sanitization ok.
 				return;
 			}
 
-			// If the user needs to install, send them to the setup wizard
+			// If the user needs to install, send them to the setup wizard.
 			if ( WC_Admin_Notices::has_notice( 'install' ) ) {
 				wp_safe_redirect( admin_url( 'index.php?page=wc-setup' ) );
 				exit;
@@ -162,7 +161,7 @@ class WC_Admin {
 	public function prevent_admin_access() {
 		$prevent_access = false;
 
-		if ( 'yes' === get_option( 'woocommerce_lock_down_admin', 'yes' ) && ! is_ajax() && basename( $_SERVER["SCRIPT_FILENAME"] ) !== 'admin-post.php' ) {
+		if ( 'yes' === get_option( 'woocommerce_lock_down_admin', 'yes' ) && ! is_ajax() && basename( $_SERVER['SCRIPT_FILENAME'] ) !== 'admin-post.php' ) { // @codingStandardsIgnoreLine.
 			$has_cap     = false;
 			$access_caps = array( 'edit_posts', 'manage_woocommerce', 'view_admin_dashboard' );
 
@@ -190,29 +189,29 @@ class WC_Admin {
 	public function preview_emails() {
 
 		if ( isset( $_GET['preview_woocommerce_mail'] ) ) {
-			if ( ! wp_verify_nonce( $_REQUEST['_wpnonce'], 'preview-mail' ) ) {
+			if ( ! wp_verify_nonce( $_REQUEST['_wpnonce'], 'preview-mail' ) ) { // @codingStandardsIgnoreLine.
 				die( 'Security check' );
 			}
 
-			// load the mailer class
+			// load the mailer class.
 			$mailer        = WC()->mailer();
 
-			// get the preview email subject
+			// Get the preview email subject.
 			$email_heading = __( 'HTML email template', 'woocommerce' );
 
-			// get the preview email content
+			// Get the preview email content.
 			ob_start();
 			include( 'views/html-email-template-preview.php' );
 			$message       = ob_get_clean();
 
-			// create a new email
+			// Create a new email.
 			$email         = new WC_Email();
 
-			// wrap the content with the email template and then add styles
+			// Wrap the content with the email template and then add styles.
 			$message       = apply_filters( 'woocommerce_mail_content', $email->style_inline( $mailer->wrap_message( $email_heading, $message ) ) );
 
-			// print the preview email
-			echo $message;
+			// Print the preview email.
+			echo $message; // WPCS: XSS ok.
 			exit;
 		}
 	}
@@ -221,7 +220,7 @@ class WC_Admin {
 	 * Change the admin footer text on WooCommerce admin pages.
 	 *
 	 * @since  2.3
-	 * @param  string $footer_text
+	 * @param  string $footer_text Footer text content.
 	 * @return string
 	 */
 	public function admin_footer_text( $footer_text ) {
@@ -236,7 +235,7 @@ class WC_Admin {
 
 		// Check to make sure we're on a WooCommerce admin page.
 		if ( isset( $current_screen->id ) && apply_filters( 'woocommerce_display_admin_footer_text', in_array( $current_screen->id, $wc_pages ) ) ) {
-			// Change the footer text
+			// Change the footer text.
 			if ( ! get_option( 'woocommerce_admin_footer_text_rated' ) ) {
 				$footer_text = sprintf(
 					/* translators: 1: WooCommerce 2:: five stars */
