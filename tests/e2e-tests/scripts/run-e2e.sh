ff75dc36cd8cfa58642fd26208bd42e9d95c1f0e
Brent Sessions <brent.sessions@automattic.com>
2018-01-03 13:18:13 -0800

    trying single test

--- tests/e2e-tests/scripts/run-e2e.sh
+++ tests/e2e-tests/scripts/run-e2e.sh
@@ -26,7 +26,7 @@ if [[ ${RUN_E2E} == 1 ]]; then
 	export BASE_URL="http://${TRAVIS_JOB_ID:0:20}.wp-e2e-tests.pw"
 
 	# Run the tests
-	npm run test
+	grunt e2e-test --file="cart-page.js"
 
 	# Delete the site when complete
 	./tests/e2e-tests/scripts/wp-serverpilot-delete.js
