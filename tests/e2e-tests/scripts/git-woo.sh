9a5bc5ec76f627fb9fc8e7b2df43354ba8c0bc3f
Brent Sessions <brent.sessions@automattic.com>
2018-01-04 09:56:35 -0800

    forcing

--- tests/e2e-tests/scripts/git-woo.sh
+++ tests/e2e-tests/scripts/git-woo.sh
@@ -14,7 +14,7 @@ dbname=`wp config get --constant=DB_NAME`
 dbuser=`wp config get --constant=DB_USER`
 dbpass=`wp config get --constant=DB_PASSWORD`
 
-wp db drop
+wp db drop --yes
 
 wp config create --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass --force --extra-php <<PHP
 define('SP_REQUEST_URL', ($_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST']);
