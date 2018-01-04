638c00827cf2fa437f0f396272571660f7c639a2
Brent Sessions <brent.sessions@automattic.com>
2018-01-04 09:05:39 -0800

    Attempting new DB approach

--- tests/e2e-tests/scripts/git-woo.sh
+++ tests/e2e-tests/scripts/git-woo.sh
@@ -10,34 +10,11 @@ fi
 
 cd apps/$APP_NAME/public/
 
-prefix=`wp db prefix`
-sed -i 's/wp_commentmeta/'"$prefix"'commentmeta/g' ~/e2e-db.sql
-sed -i 's/wp_comments/'"$prefix"'comments/g' ~/e2e-db.sql
-sed -i 's/wp_links/'"$prefix"'links/g' ~/e2e-db.sql
-sed -i 's/wp_options/'"$prefix"'options/g' ~/e2e-db.sql
-sed -i 's/wp_postmeta/'"$prefix"'postmeta/g' ~/e2e-db.sql
-sed -i 's/wp_posts/'"$prefix"'posts/g' ~/e2e-db.sql
-sed -i 's/wp_term_relationships/'"$prefix"'term_relationships/g' ~/e2e-db.sql
-sed -i 's/wp_relationships/'"$prefix"'relationships/g' ~/e2e-db.sql
-sed -i 's/wp_term_taxonomy/'"$prefix"'term_taxonomy/g' ~/e2e-db.sql
-sed -i 's/wp_termmeta/'"$prefix"'termmeta/g' ~/e2e-db.sql
-sed -i 's/wp_terms/'"$prefix"'terms/g' ~/e2e-db.sql
-sed -i 's/wp_usermeta/'"$prefix"'usermeta/g' ~/e2e-db.sql
-sed -i 's/wp_users/'"$prefix"'users/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_api_keys/'"$prefix"'woocommerce_api_keys/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_attribute_taxonomies/'"$prefix"'woocommerce_attribute_taxonomies/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_downloadable_product_permissions/'"$prefix"'woocommerce_downloadable_product_permissions/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_log/'"$prefix"'woocommerce_log/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_order_itemmeta/'"$prefix"'woocommerce_order_itemmeta/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_order_items/'"$prefix"'woocommerce_order_items/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_payment_tokenmeta/'"$prefix"'woocommerce_payment_tokenmeta/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_payment_tokens/'"$prefix"'woocommerce_payment_tokens/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_sessions/'"$prefix"'woocommerce_sessions/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_shipping_zone_locations/'"$prefix"'woocommerce_shipping_zone_locations/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_shipping_zone_methods/'"$prefix"'woocommerce_shipping_zone_methods/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_shipping_zones/'"$prefix"'woocommerce_shipping_zones/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_tax_rate_locations/'"$prefix"'woocommerce_tax_rate_locations/g' ~/e2e-db.sql
-sed -i 's/wp_woocommerce_tax_rates/'"$prefix"'woocommerce_tax_rates/g' ~/e2e-db.sql
+dbname=`wp config get --constant=DB_NAME`
+dbuser=`wp config get --constant=DB_USER`
+dbpass=`wp config get --constant=DB_PASSWORD`
+
+wp config create --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass --force
 
 wp theme install twentytwelve --activate
 wget https://github.com/woocommerce/woocommerce/archive/$1.zip -O woocommerce.zip
