de10379595fc259b7eb46173fcf036c472d02d6f
Brent Sessions <brent.sessions@automattic.com>
2018-01-09 15:14:42 -0800

    checking localhost

--- tests/e2e-tests/scripts/run-e2e.sh
+++ tests/e2e-tests/scripts/run-e2e.sh
@@ -24,14 +24,16 @@
 
 	#export BASE_URL="http://${TRAVIS_JOB_ID:0:20}.wp-e2e-tests.pw"
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-php wp-cli.phar core download
-php wp-cli.phar core install --url=localhost --title=Example --admin_user=admin --admin_password=password --admin_email=info@example.com
-php wp-cli.phar core install
-php wp-cli.phar db import ~/e2e-db.sql
+	curl -I http://localhost
 
-php wp-cli.phar theme install twentytwelve --activate
-php wp-cli.phar plugin install https://github.com/woocommerce/woocommerce/archive/$1.zip --activate
+#curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#php wp-cli.phar core download
+#php wp-cli.phar core install --url=localhost --title=Example --admin_user=admin --admin_password=password --admin_email=info@example.com
+#php wp-cli.phar core install
+#php wp-cli.phar db import ~/e2e-db.sql
+#
+#php wp-cli.phar theme install twentytwelve --activate
+#php wp-cli.phar plugin install https://github.com/woocommerce/woocommerce/archive/$1.zip --activate
 	export BASE_URL="http://localhost"
 
 	# Run the tests
