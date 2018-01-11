8eb11e7598782323925b4b277f09ecb0dff1010b
Brent Sessions <brent.sessions@automattic.com>
2018-01-10 16:17:42 -0800

    again

--- tests/e2e-tests/scripts/run-e2e.sh
+++ tests/e2e-tests/scripts/run-e2e.sh
@@ -40,8 +40,6 @@ php wp-cli.phar db import ~/build/woocommerce/woocommerce/tests/e2e-tests/data/e
 
 php wp-cli.phar theme install twentytwelve --activate
 php wp-cli.phar plugin install https://github.com/woocommerce/woocommerce/archive/$TRAVIS_BRANCH.zip --activate
-ls
-
 
 cd ~/build/woocommerce/woocommerce
 
