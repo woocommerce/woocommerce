656356530a41c259a188525837ba1658fbfabfa3
Rodrigo Primo <rodrigosprimo@gmail.com>
2018-01-22 14:43:28 -0200

    Use setUp() method in WC_Customer_Download() tests to remove code duplication

--- tests/unit-tests/customer/class-wc-tests-customer-download.php
+++ tests/unit-tests/customer/class-wc-tests-customer-download.php
@@ -14,30 +14,53 @@
 class WC_Tests_Customer_Download extends WC_Unit_Test_Case {
 
 	/**
+	 * Download object used for testing.
+	 *
+	 * @var WC_Customer_Download
+	 */
+	private $download;
+
+	/**
+	 * ID of the customer created for the tests.
+	 *
+	 * @var int
+	 */
+	private $customer_id;
+
+	/**
+	 * Tests set up.
+	 */
+	public function setUp() {
+		$customer = get_user_by( 'login', 'testuser' );
+
+		if ( $customer ) {
+			$this->customer_id = $customer->ID;
+		} else {
+			$this->customer_id = wc_create_new_customer( 'test@example.com', 'testuser', 'testpassword' );
+		}
+
+		$this->download = new WC_Customer_Download();
+		$this->download->set_user_id( $this->customer_id );
+		$this->download->set_user_email( 'test@example.com' );
+		$this->download->set_order_id( 1 );
+		$this->download->save();
+	}
+
+	/**
 	 * Test WC_Customer_Download_Data_Store::delete()
 	 */
 	public function test_delete() {
-		$customer_id = wc_create_new_customer( 'test@example.com', 'testuser', 'testpassword' );
-		$download    = new WC_Customer_Download();
-		$download->set_user_id( $customer_id );
-		$download->set_order_id( 1 );
-		$download->save();
 		$data_store = WC_Data_Store::load( 'customer-download' );
-		$data_store->delete( $download );
-		$this->assertEquals( 0, $download->get_id() );
+		$data_store->delete( $this->download );
+		$this->assertEquals( 0, $this->download->get_id() );
 	}
 
 	/**
 	 * Test WC_Customer_Download_Data_Store::delete_by_id()
 	 */
 	public function test_delete_by_id() {
-		$customer_id = wc_create_new_customer( 'test@example.com', 'testuser', 'testpassword' );
-		$download    = new WC_Customer_Download();
-		$download->set_user_id( $customer_id );
-		$download->set_order_id( 1 );
-		$download->save();
 		$data_store = WC_Data_Store::load( 'customer-download' );
-		$data_store->delete_by_id( $download->get_id() );
+		$data_store->delete_by_id( $this->download->get_id() );
 		$this->assertEquals( 0, $data_store->get_id() );
 	}
 
@@ -45,17 +68,12 @@ class WC_Tests_Customer_Download extends WC_Unit_Test_Case {
 	 * Test WC_Customer_Download_Data_Store::delete_by_download_id()
 	 */
 	public function test_delete_by_download_id() {
-		$customer_id = wc_create_new_customer( 'test@example.com', 'testuser', 'testpassword' );
-		$download    = new WC_Customer_Download();
-		$download->set_user_id( $customer_id );
-		$download->set_order_id( 1 );
-		$download->save();
-		$download_id = $download->get_download_id();
+		$download_id = $this->download->get_download_id();
 		$data_store  = WC_Data_Store::load( 'customer-download' );
-		$downloads   = $data_store->get_downloads_for_customer( $customer_id );
+		$downloads   = $data_store->get_downloads_for_customer( $this->customer_id );
 		$this->assertInstanceOf( 'StdClass', $downloads[0] );
 		$data_store->delete_by_download_id( $download_id );
-		$downloads = $data_store->get_downloads_for_customer( $customer_id );
+		$downloads = $data_store->get_downloads_for_customer( $this->customer_id );
 		$this->assertEquals( array(), $downloads );
 	}
 
@@ -63,15 +81,8 @@ class WC_Tests_Customer_Download extends WC_Unit_Test_Case {
 	 * Test WC_Customer_Download_Data_Store::get_downloads()
 	 */
 	public function test_get_downloads() {
-		$customer_id = wc_create_new_customer( 'test@example.com', 'testuser', 'testpassword' );
-		$download_1  = new WC_Customer_Download();
-		$download_1->set_user_id( $customer_id );
-		$download_1->set_user_email( 'test@example.com' );
-		$download_1->set_order_id( 1 );
-		$download_1->save();
-
 		$download_2 = new WC_Customer_Download();
-		$download_2->set_user_id( $customer_id );
+		$download_2->set_user_id( $this->customer_id );
 		$download_2->set_user_email( 'test@example.com' );
 		$download_2->set_order_id( 1 );
 		$download_2->save();
@@ -85,7 +96,7 @@ class WC_Tests_Customer_Download extends WC_Unit_Test_Case {
 		$downloads = $data_store->get_downloads( array( 'user_email' => 'test2@example.com' ) );
 		$this->assertEquals( array(), $downloads );
 
-		$expected_result = array( $download_1->get_id(), $download_2->get_id() );
+		$expected_result = array( $this->download->get_id(), $download_2->get_id() );
 		$downloads       = $data_store->get_downloads(
 			array(
 				'user_email' => 'test@example.com',
