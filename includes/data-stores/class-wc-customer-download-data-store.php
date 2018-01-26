woocommerce/woocommerce

commit a443419006542b1d04a2e80573d024b7a3474a93

remove extract and sanitize orderby against whitelist

includes/data-stores/class-wc-customer-download-data-store.php
@@ -214,38 +214,37 @@ class WC_Customer_Download_Data_Store implements WC_Customer_Download_Data_Store
 			'return'     => 'objects',
 		) );
 
-		extract( $args );
-
 		$query   = array();
 		$query[] = "SELECT * FROM {$wpdb->prefix}woocommerce_downloadable_product_permissions WHERE 1=1";
 
-		if ( $user_email ) {
-			$query[] = $wpdb->prepare( "AND user_email = %s", $user_email );
+		if ( $args['user_email'] ) {
+			$query[] = $wpdb->prepare( "AND user_email = %s", sanitize_email( $args['user_email'] ) );
 		}
 
-		if ( $order_id ) {
-			$query[] = $wpdb->prepare( "AND order_id = %d", $order_id );
+		if ( $args['order_id'] ) {
+			$query[] = $wpdb->prepare( "AND order_id = %d", $args['order_id'] );
 		}
 
-		if ( $order_key ) {
-			$query[] = $wpdb->prepare( "AND order_key = %s", $order_key );
+		if ( $args['order_key'] ) {
+			$query[] = $wpdb->prepare( "AND order_key = %s", $args['order_key'] );
 		}
 
-		if ( $product_id ) {
-			$query[] = $wpdb->prepare( "AND product_id = %d", $product_id );
+		if ( $args['product_id'] ) {
+			$query[] = $wpdb->prepare( "AND product_id = %d", $args['product_id'] );
 		}
 
-		$orderby = esc_sql( $orderby );
-		$order   = esc_sql( $order );
-		$query[] = "ORDER BY {$orderby} {$order}";
+		$order       = in_array( $args['order'], array( 'permission_id', 'download_id', 'product_id', 'order_id', 'order_key', 'user_email', 'user_id', 'downloads_remaining', 'access_granted', 'access_expires', 'download_count' ) ) ? $args['order'] : 'permission_id';
+		$orderby     = 'DESC' === strtoupper( $args['orderby'] ) ? 'DESC'                                                                                                                                                                                : 'ASC';
+		$orderby_sql = sanitize_sql_orderby( "{$orderby} {$order}" );
+		$query[]     = "ORDER BY {$orderby_sql}";
 
-		if ( 0 < $limit ) {
-			$query[] = $wpdb->prepare( "LIMIT %d", $limit );
+		if ( 0 < $args['limit'] ) {
+			$query[] = $wpdb->prepare( "LIMIT %d", $args['limit'] );
 		}
 
 		$raw_downloads = $wpdb->get_results( implode( ' ', $query ) );
 
-		switch ( $return ) {
+		switch ( $args['return'] ) {
 			case 'ids' :
 				return wp_list_pluck( $raw_downloads, 'permission_id' );
 			default :

commit 9c4c006baaf871099d7983303547ef9f0d55ccce

Fix: set orderby and order when calling WC_Customer_Download_Data_Store::get_downloads()
This commit fixes a bug in WC_Customer_Download_Data_Store::get_downloads() that made impossible to change the order in which the query returned the results. This method accepts the arguments `order_by` and `order` but it was ignoring them and always using the default values ('permission_id' and 'ASC' respectively).

One of the assertions of the WC_Customer_Download_Data_Store::get_downloads() test method was modified to make sure the code now works.

This bug was introduced by commit a443419.

includes/data-stores/class-wc-customer-download-data-store.php
@@ -223,7 +223,7 @@ class WC_Customer_Download_Data_Store implements WC_Customer_Download_Data_Store
 			'product_id'  => '',
 			'download_id' => '',
 			'orderby'     => 'permission_id',
-			'order'       => 'DESC',
+			'order'       => 'ASC',
 			'limit'       => -1,
 			'return'      => 'objects',
 		) );
@@ -252,9 +252,9 @@ class WC_Customer_Download_Data_Store implements WC_Customer_Download_Data_Store
 		}
 
 		$allowed_orders = array( 'permission_id', 'download_id', 'product_id', 'order_id', 'order_key', 'user_email', 'user_id', 'downloads_remaining', 'access_granted', 'access_expires', 'download_count' );
-		$order          = in_array( $args['order'], $allowed_orders ) ? $args['order'] : 'permission_id';
-		$orderby        = 'DESC' === strtoupper( $args['orderby'] ) ? 'DESC' : 'ASC';
-		$orderby_sql    = sanitize_sql_orderby( "{$order} {$orderby}" );
+		$orderby        = in_array( $args['orderby'], $allowed_orders, true ) ? $args['orderby'] : 'permission_id';
+		$order          = 'DESC' === strtoupper( $args['order'] ) ? 'DESC' : 'ASC';
+		$orderby_sql    = sanitize_sql_orderby( "{$orderby} {$order}" );
 		$query[]        = "ORDER BY {$orderby_sql}";
 
 		if ( 0 < $args['limit'] ) {

tests/unit-tests/customer/customer-download.php
@@ -50,17 +50,19 @@ class WC_Tests_Customer_Download extends WC_Unit_Test_Case {
 		$download_1->set_user_id( $customer_id );
 		$download_1->set_user_email( 'test@example.com' );
 		$download_1->set_order_id( 1 );
+		$download_1->set_access_granted( '2018-01-08' );
 		$download_1->save();
 
 		$download_2 = new WC_Customer_Download;
 		$download_2->set_user_id( $customer_id );
 		$download_2->set_user_email( 'test@example.com' );
-		$download_2->set_order_id( 1 );
+		$download_2->set_order_id( 2 );
+		$download_2->set_access_granted( '2018-01-08' );
 		$download_2->save();
 
 		$data_store = WC_Data_Store::load( 'customer-download' );
-		$downloads = $data_store->get_downloads( array( 'user_email' => 'test@example.com' ) );
-		$this->assertEquals( 2, count( $downloads ) );
+		$downloads = $data_store->get_downloads( array( 'user_email' => 'test@example.com', 'orderby' => 'order_id', 'order' => 'DESC' ) );
+		$this->assertEquals( array( $download_2, $download_1 ), $downloads );
 		$downloads = $data_store->get_downloads( array( 'user_email' => 'test2@example.com' ) );
 		$this->assertEquals( array(), $downloads );
 