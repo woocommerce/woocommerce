f6508856c9f6f0f616b0cf1e46be2da508768197
Rodrigo Primo <rodrigosprimo@gmail.com>
2018-02-06 11:17:57 -0200

    phpcs fixes

--- uninstall.php
+++ uninstall.php
@@ -4,8 +4,6 @@
  *
  * Uninstalling WooCommerce deletes user roles, pages, tables, and options.
  *
- * @author      WooCommerce
- * @category    Core
  * @package     WooCommerce/Uninstaller
  * @version     2.3.0
  */
@@ -29,7 +27,7 @@ wp_clear_scheduled_hook( 'woocommerce_tracker_send_event' );
  */
 if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
 	// Roles + caps.
-	include_once( dirname( __FILE__ ) . '/includes/class-wc-install.php' );
+	include_once dirname( __FILE__ ) . '/includes/class-wc-install.php';
 	WC_Install::remove_roles();
 
 	// Pages.
@@ -71,9 +69,9 @@ if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
 	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}woocommerce_order_items" );
 	$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}woocommerce_order_itemmeta" );
 
-	// Delete terms if > WP 4.2 (term splitting was added in 4.2)
+	// Delete terms if > WP 4.2 (term splitting was added in 4.2).
 	if ( version_compare( $wp_version, '4.2', '>=' ) ) {
-		// Delete term taxonomies
+		// Delete term taxonomies.
 		foreach ( array( 'product_cat', 'product_tag', 'product_shipping_class', 'product_type' ) as $taxonomy ) {
 			$wpdb->delete(
 				$wpdb->term_taxonomy,
@@ -83,7 +81,7 @@ if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
 			);
 		}
 
-		// Delete term attributes
+		// Delete term attributes.
 		foreach ( $wc_attributes as $taxonomy ) {
 			$wpdb->delete(
 				$wpdb->term_taxonomy,
@@ -93,18 +91,18 @@ if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
 			);
 		}
 
-		// Delete orphan relationships
+		// Delete orphan relationships.
 		$wpdb->query( "DELETE tr FROM {$wpdb->term_relationships} tr LEFT JOIN {$wpdb->posts} posts ON posts.ID = tr.object_id WHERE posts.ID IS NULL;" );
 
-		// Delete orphan terms
+		// Delete orphan terms.
 		$wpdb->query( "DELETE t FROM {$wpdb->terms} t LEFT JOIN {$wpdb->term_taxonomy} tt ON t.term_id = tt.term_id WHERE tt.term_id IS NULL;" );
 
-		// Delete orphan term meta
+		// Delete orphan term meta.
 		if ( ! empty( $wpdb->termmeta ) ) {
 			$wpdb->query( "DELETE tm FROM {$wpdb->termmeta} tm LEFT JOIN {$wpdb->term_taxonomy} tt ON tm.term_id = tt.term_id WHERE tt.term_id IS NULL;" );
 		}
 	}
 
-	// Clear any cached data that has been removed
+	// Clear any cached data that has been removed.
 	wp_cache_flush();
 }
