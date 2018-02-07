c21cc2e8fee5850eeb7cc7785aab71236a9891bd
Caleb Burks <19caleb95@gmail.com>
2018-02-07 17:01:12 -0500

    Replace “Cheatin’ uh?” with friendlier messages

--- includes/class-wc-shipping.php
+++ includes/class-wc-shipping.php
@@ -60,7 +60,7 @@ class WC_Shipping {
 	 * @since 2.1
 	 */
 	public function __clone() {
-		wc_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'woocommerce' ), '2.1' );
+		wc_doing_it_wrong( __FUNCTION__, __( 'Cloning is forbidden.', 'woocommerce' ), '2.1' );
 	}
 
 	/**
@@ -69,7 +69,7 @@ class WC_Shipping {
 	 * @since 2.1
 	 */
 	public function __wakeup() {
-		wc_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'woocommerce' ), '2.1' );
+		wc_doing_it_wrong( __FUNCTION__, __( 'Unserializing instances of this class is forbidden.', 'woocommerce' ), '2.1' );
 	}
 
 	/**
