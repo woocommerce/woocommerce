e3f92b9f7fb991bce069607373bc92bab37d7b39
Mike Jolley <mike.jolley@me.com>
2018-02-12 17:48:45 +0000

    wc_get_gallery_image_html helper function

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -1050,6 +1050,33 @@ if ( ! function_exists( 'woocommerce_show_product_thumbnails' ) ) {
 	}
 }
 
+/**
+ * Get HTML for a gallery image.
+ *
+ * @since 3.3.2
+ * @param int $attachment_id
+ * @return string
+ */
+function wc_get_gallery_image_html( $attachment_id ) {
+	$flexslider        = (bool) apply_filters( 'woocommerce_single_product_flexslider_enabled', get_theme_support( 'wc-product-gallery-slider' ) );
+	$gallery_thumbnail = wc_get_image_size( 'gallery_thumbnail' );
+	$thumbnail_size    = apply_filters( 'woocommerce_gallery_thumbnail_size', array( $gallery_thumbnail['width'], $gallery_thumbnail['height'] ) );
+	$image_size        = apply_filters( 'woocommerce_gallery_image_size', $flexslider ? 'woocommerce_single': $thumbnail_size );
+	$full_size         = apply_filters( 'woocommerce_gallery_full_size', apply_filters( 'woocommerce_product_thumbnails_large_size', 'full' ) );
+	$thumbnail_src     = wp_get_attachment_image_src( $attachment_id, $thumbnail_size );
+	$full_src          = wp_get_attachment_image_src( $attachment_id, $full_size );
+	$image             = wp_get_attachment_image( $attachment_id, $image_size, false, array(
+		'title'                   => get_post_field( 'post_title', $attachment_id ),
+		'data-caption'            => get_post_field( 'post_excerpt', $attachment_id ),
+		'data-src'                => $full_src[0],
+		'data-large_image'        => $full_src[0],
+		'data-large_image_width'  => $full_src[1],
+		'data-large_image_height' => $full_src[2],
+	) );
+
+	return '<div data-thumb="' . esc_url( $thumbnail_src[0] ) . '" class="woocommerce-product-gallery__image"><a href="' . esc_url( $full_src[0] ) . '">' . $image . '</a></div>';
+}
+
 if ( ! function_exists( 'woocommerce_output_product_data_tabs' ) ) {
 
 	/**
