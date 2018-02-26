e1beda3eb03616dbe2e08cde822386df1b5dd664
Mike Jolley <mike.jolley@me.com>
2018-02-26 18:51:46 +0000

    Use relative scheme for ajax endpoint

--- includes/class-wc-ajax.php
+++ includes/class-wc-ajax.php
@@ -32,7 +32,7 @@ class WC_AJAX {
 	 * @return string
 	 */
 	public static function get_endpoint( $request = '' ) {
-		return esc_url_raw( apply_filters( 'woocommerce_ajax_get_endpoint', add_query_arg( 'wc-ajax', $request, remove_query_arg( array( 'remove_item', 'add-to-cart', 'added-to-cart' ), home_url( '/' ) ) ), $request ) );
+		return esc_url_raw( apply_filters( 'woocommerce_ajax_get_endpoint', add_query_arg( 'wc-ajax', $request, remove_query_arg( array( 'remove_item', 'add-to-cart', 'added-to-cart' ), home_url( '/', 'relative' ) ) ), $request ) );
 	}
 
 	/**
