fb76e1dd7dc47bc1968395b9dbc502df04bf91cd
Gerhard Potgieter <potgieterg@gmail.com>
2018-03-07 12:53:27 +0200

    Revert tamplate version to 3.4.0

--- templates/single-product/add-to-cart/variable.php
+++ templates/single-product/add-to-cart/variable.php
@@ -12,7 +12,7 @@
  *
  * @see 	https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.3.4
+ * @version 3.4.0
  */
 
 defined( 'ABSPATH' ) || exit;
