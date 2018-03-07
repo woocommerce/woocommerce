765ed9ccf0989ee7f91c117df79933dd89610e7d
Mike Jolley <mike.jolley@me.com>
2018-03-07 13:57:33 +0000

    notes

--- includes/class-wc-privacy.php
+++ includes/class-wc-privacy.php
@@ -25,7 +25,27 @@ class WC_Privacy {
 	 * @param string $email Email address.
 	 */
 	public static function remove_personal_data( $email ) {
-		// Do something.
+		/**
+		 * Order Data:
+		 *
+		 * Transaction ID
+		 * Customer's IP Address and User Agent
+		 * Billing Address First Name, Last Name, Company, Address Line 1, Address Line 2, City, Postcode/ZIP, Country, State/County, Phone, Email Address
+		 * "Billing Fields" (_billing_address_index)
+		 * Same as above for shipping
+		 *
+		 * Customer Data (meta):
+		 *
+		 * Billing and shipping addresses
+		 *
+		 * Misc:
+		 *
+		 * Downloadable Product User Email
+		 * Download Log Entry User IP Address
+		 * Carts for user ID/Sessions
+		 * File based logs containing their email e.g. from webhooks
+		 * Payment tokens?
+		 */
 	}
 
 	/**
