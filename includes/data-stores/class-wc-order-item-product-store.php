0ebf90b50eb39a781931e2f75b353c9986e0e5aa
Rodrigo Primo <rodrigosprimo@gmail.com>
2018-03-08 16:29:11 -0300

    phpcs fixes in includes/data-stores/class-wc-order-item-product-store.php

--- includes/data-stores/class-wc-order-item-product-store.php
+++ includes/data-stores/class-wc-order-item-product-store.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Class WC_Order_Item_Product_Data_Store file.
+ *
+ * @package WooCommerce\DataStores
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -7,8 +13,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * WC Order Item Product Data Store
  *
  * @version  3.0.0
- * @category Class
- * @author   WooCommerce
  */
 class WC_Order_Item_Product_Data_Store extends Abstract_WC_Order_Item_Type_Data_Store implements WC_Object_Data_Store_Interface, WC_Order_Item_Type_Data_Store_Interface, WC_Order_Item_Product_Data_Store_Interface {
 
@@ -24,7 +28,7 @@ class WC_Order_Item_Product_Data_Store extends Abstract_WC_Order_Item_Type_Data_
 	 * Read/populate data properties specific to this order item.
 	 *
 	 * @since 3.0.0
-	 * @param WC_Order_Item_Product $item
+	 * @param WC_Order_Item_Product $item Product order item object.
 	 */
 	public function read( &$item ) {
 		parent::read( $item );
@@ -48,7 +52,7 @@ class WC_Order_Item_Product_Data_Store extends Abstract_WC_Order_Item_Type_Data_
 	 * Ran after both create and update, so $id will be set.
 	 *
 	 * @since 3.0.0
-	 * @param WC_Order_Item_Product $item
+	 * @param WC_Order_Item_Product $item Product order item object.
 	 */
 	public function save_item_data( &$item ) {
 		$id                = $item->get_id();
@@ -75,8 +79,8 @@ class WC_Order_Item_Product_Data_Store extends Abstract_WC_Order_Item_Type_Data_
 	 * Get a list of download IDs for a specific item from an order.
 	 *
 	 * @since 3.0.0
-	 * @param WC_Order_Item_Product $item
-	 * @param WC_Order              $order
+	 * @param WC_Order_Item_Product $item Product order item object.
+	 * @param WC_Order              $order Order object.
 	 * @return array
 	 */
 	public function get_download_ids( $item, $order ) {
