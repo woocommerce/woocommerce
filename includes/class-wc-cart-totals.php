7c3a64ba84b39564f136741094a70f33907fac54
claudiulodro <claudiulodro@gmail.com>
2018-03-14 08:35:41 -0700

    Round fractional cents when oob

--- includes/class-wc-cart-totals.php
+++ includes/class-wc-cart-totals.php
@@ -453,7 +453,7 @@ final class WC_Cart_Totals {
 				$new_taxes   = WC_Tax::calc_tax( $item->price - array_sum( $taxes ), $item->tax_rates, false );
 
 				// Now we have a new item price.
-				$item->price = $item->price - array_sum( $taxes ) + array_sum( $new_taxes );
+				$item->price = round( $item->price - array_sum( $taxes ) + array_sum( $new_taxes ) );
 			}
 		}
 		return $item;
