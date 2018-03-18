e0300b7405193e7a2d0fa55a5746b12ba06ec78d
Will Gorham <willgorham@gmail.com>
2018-03-18 17:23:15 -0400

    Replace icons: My account order details phone/mail

--- templates/order/order-details-customer.php
+++ templates/order/order-details-customer.php
@@ -36,11 +36,11 @@ $show_shipping = ! wc_ship_to_billing_address_only() && $order->needs_shipping_a
 		<?php echo wp_kses_post( $order->get_formatted_billing_address( __( 'N/A', 'woocommerce' ) ) ); ?>
 
 		<?php if ( $order->get_billing_phone() ) : ?>
-			<p class="woocommerce-customer-details--phone"><?php echo esc_html( $order->get_billing_phone() ); ?></p>
+			<p class="woocommerce-customer-details--phone"><?php echo get_gridicon( 'gridicons-phone' ); ?><?php echo esc_html( $order->get_billing_phone() ); ?></p>
 		<?php endif; ?>
 
 		<?php if ( $order->get_billing_email() ) : ?>
-			<p class="woocommerce-customer-details--email"><?php echo esc_html( $order->get_billing_email() ); ?></p>
+			<p class="woocommerce-customer-details--email"><?php echo get_gridicon( 'gridicons-mail' ); ?><?php echo esc_html( $order->get_billing_email() ); ?></p>
 		<?php endif; ?>
 	</address>
 
