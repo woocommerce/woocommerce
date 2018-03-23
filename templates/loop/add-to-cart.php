a940c0bfca30a194e747d8c4d32db8cb81795901
Peter Fabian <peter.fabian.github@gmail.com>
2018-03-23 18:13:50 +0100

    Apply strip_tags to attributes in loop template
    Fixes #19491

--- templates/loop/add-to-cart.php
+++ templates/loop/add-to-cart.php
@@ -27,7 +27,7 @@ echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 		esc_url( $product->add_to_cart_url() ),
 		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
 		esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
-		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+		isset( $args['attributes'] ) ? wc_implode_html_attributes( array_map( 'strip_tags', $args['attributes'] ) ) : '',
 		esc_html( $product->add_to_cart_text() )
 	),
 $product, $args );
