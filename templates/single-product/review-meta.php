97c591a5e5920a4e838849a0606102791df0131b
Jonathan Belcher <belcherj@gmail.com>
2018-03-26 13:44:16 -0400

    Add escaping to piblish date

--- templates/single-product/review-meta.php
+++ templates/single-product/review-meta.php
@@ -36,7 +36,10 @@ if ( '0' === $comment->comment_approved ) { ?>
 			echo '<em class="woocommerce-review__verified verified">(' . esc_attr__( 'verified owner', 'woocommerce' ) . ')</em> ';
 		}
 
-		?><span class="woocommerce-review__dash">&ndash;</span> <time class="woocommerce-review__published-date" datetime="<?php echo get_comment_date( 'c' ); ?>"><?php echo get_comment_date( wc_date_format() ); ?></time>
+		?><span class="woocommerce-review__dash">&ndash;</span>
+		<time class="woocommerce-review__published-date" datetime="<?php echo esc_attr( get_comment_date( 'c' ) ); ?>">
+			<?php echo esc_html( get_comment_date( wc_date_format() ) ); ?>
+		</time>
 	</p>
 
 <?php }
