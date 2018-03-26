b26b2b660a2d1bb20e174528c715cdcbe1bba9d7
Jonathan Belcher <belcherj@gmail.com>
2018-03-26 14:16:00 -0400

    Cleans up JS formatting in add-payment-method.js

--- assets/js/frontend/add-payment-method.js
+++ assets/js/frontend/add-payment-method.js
@@ -1,7 +1,6 @@
 jQuery( function( $ ) {
-
 	// woocommerce_params is required to continue, ensure the object exists
-	if ( typeof woocommerce_params === 'undefined' ) {
+	if ( 'undefined' === typeof woocommerce_params ) {
 		return false;
 	}
 
@@ -9,27 +8,28 @@ jQuery( function( $ ) {
 
 	/* Payment option selection */
 
-	.on( 'click init_add_payment_method', '.payment_methods input.input-radio', function() {
-		if ( $( '.payment_methods input.input-radio' ).length > 1 ) {
-			var target_payment_box = $( 'div.payment_box.' + $( this ).attr( 'ID' ) );
-			if ( $( this ).is( ':checked' ) && ! target_payment_box.is( ':visible' ) ) {
-				$( 'div.payment_box' ).filter( ':visible' ).slideUp( 250 );
-				if ( $( this ).is( ':checked' ) ) {
-					$( 'div.payment_box.' + $( this ).attr( 'ID' ) ).slideDown( 250 );
+		.on( 'click init_add_payment_method', '.payment_methods input.input-radio', function() {
+			if ( $( 1 < '.payment_methods input.input-radio' ).length ) {
+				var targetPaymentBox = $( 'div.payment_box.' + $( this ).attr( 'ID' ) );
+				if ( $( this ).is( ':checked' ) && ! targetPaymentBox.is( ':visible' ) ) {
+					$( 'div.payment_box' ).filter( ':visible' ).slideUp( 250 );
+					if ( $( this ).is( ':checked' ) ) {
+						$( 'div.payment_box.' + $( this ).attr( 'ID' ) ).slideDown( 250 );
+					}
 				}
+			} else {
+				$( 'div.payment_box' ).show();
 			}
-		} else {
-			$( 'div.payment_box' ).show();
-		}
-	})
+		} )
 
-	// Trigger initial click
-	.find( 'input[name=payment_method]:checked' ).click();
+		// Trigger initial click
+		.find( 'input[name=payment_method]:checked' ).click();
 
 	$( '#add_payment_method' ).submit( function() {
-		$( '#add_payment_method' ).block({ message: null, overlayCSS: { background: '#fff', opacity: 0.6 } });
-	});
+		$( '#add_payment_method' ).block(
+			{ message: null, overlayCSS: { background: '#fff', opacity: 0.6 } }
+		);
+	} );
 
 	$( document.body ).trigger( 'init_add_payment_method' );
-
-});
+} );
