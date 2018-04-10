0254177b0ca432005efdaddcc0050d40195f8908
Mike Jolley <mike.jolley@me.com>
2018-04-10 14:58:06 +0100

    Fix featured filter

--- includes/api/class-wc-rest-products-controller.php
+++ includes/api/class-wc-rest-products-controller.php
@@ -248,6 +248,7 @@ class WC_REST_Products_Controller extends WC_REST_Legacy_Products_Controller {
 				'taxonomy' => 'product_visibility',
 				'field'    => 'name',
 				'terms'    => 'featured',
+				'operator' => true === $request['featured'] ? 'IN' : 'NOT IN',
 			);
 		}
 
