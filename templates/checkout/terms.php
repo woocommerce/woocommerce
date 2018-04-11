2464a94920573bc79816cd470f5d7362fab4eb84
Mike Jolley <mike.jolley@me.com>
2018-04-11 12:57:07 +0100

    Indent

--- templates/checkout/terms.php
+++ templates/checkout/terms.php
@@ -18,19 +18,19 @@ if ( apply_filters( 'woocommerce_checkout_show_terms', true ) && function_exists
 		</div>
 
 		<?php if ( 0 < wc_get_page_id( 'terms' ) ) : ?>
-		<div class="woocommerce-terms-and-conditions" style="display: none; max-height: 200px; overflow: auto;">
-			<?php woocommerce_output_terms_and_conditions_page_content(); ?>
-		</div>
+			<div class="woocommerce-terms-and-conditions" style="display: none; max-height: 200px; overflow: auto;">
+				<?php woocommerce_output_terms_and_conditions_page_content(); ?>
+			</div>
 		<?php endif; ?>
 
 		<?php if ( woocommerce_terms_and_conditions_checkbox_enabled() ) : ?>
-		<p class="form-row validate-required">
-			<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-			<input type="checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" name="terms" <?php checked( apply_filters( 'woocommerce_terms_is_checked_default', isset( $_POST['terms'] ) ), true ); // WPCS: input var ok, csrf ok. ?> id="terms" />
-				<span class="woocommerce-terms-and-conditions-checkbox-text"><?php woocommerce_output_terms_and_conditions_checkbox_text(); ?></span>&nbsp;<span class="required">*</span>
-			</label>
-			<input type="hidden" name="terms-field" value="1" />
-		</p>
+			<p class="form-row validate-required">
+				<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
+				<input type="checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" name="terms" <?php checked( apply_filters( 'woocommerce_terms_is_checked_default', isset( $_POST['terms'] ) ), true ); // WPCS: input var ok, csrf ok. ?> id="terms" />
+					<span class="woocommerce-terms-and-conditions-checkbox-text"><?php woocommerce_output_terms_and_conditions_checkbox_text(); ?></span>&nbsp;<span class="required">*</span>
+				</label>
+				<input type="hidden" name="terms-field" value="1" />
+			</p>
 		<?php endif; ?>
 	</div>
 	<?php
