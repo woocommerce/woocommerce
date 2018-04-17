dbe06969fead29183ded440a150dd5dbe6f72efb
Kelly Dwan <ryelle@users.noreply.github.com>
2018-04-17 17:38:56 -0400

    Add build process

--- plugins/woocommerce-admin/Gruntfile.js
+++ plugins/woocommerce-admin/Gruntfile.js
@@ -47,7 +47,6 @@ module.exports = function( grunt ) {
 
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
-	grunt.registerTask( 'default', [ 'i18n','readme' ] );
 	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
 	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
 
---
+++ plugins/woocommerce-admin/webpack.config.js
@@ -0,0 +1,86 @@
+/* eslint-disable */
+const path = require( 'path' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+var NODE_ENV = process.env.NODE_ENV || 'development';
+
+/**
+ * Given a string, returns a new string with dash separators converedd to
+ * camel-case equivalent. This is not as aggressive as `_.camelCase` in
+ * converting to uppercase, where Lodash will convert letters following
+ * numbers.
+ *
+ * @param {string} string Input dash-delimited string.
+ *
+ * @return {string} Camel-cased string.
+ */
+function camelCaseDash( string ) {
+	return string.replace(
+		/-([a-z])/,
+		( match, letter ) => letter.toUpperCase()
+	);
+}
+
+const coreGlobals = [
+	'blocks',
+	'components',
+	'date',
+	'editor',
+	'element',
+	'utils',
+	'data',
+	'viewport',
+	'core-data',
+	'plugins',
+	'edit-post',
+	'hooks',
+	'i18n',
+	'api-request',
+];
+
+const externals = {
+	react: 'React',
+	'react-dom': 'ReactDOM',
+	tinymce: 'tinymce',
+	moment: 'moment',
+	jquery: 'jQuery',
+	lodash: 'lodash',
+};
+
+coreGlobals.forEach( ( name ) => {
+	externals[ `@wordpress/${ name }` ] = {
+		this: [ 'wp', camelCaseDash( name ) ],
+	};
+} );
+
+const webpackConfig = {
+	mode: NODE_ENV,
+	entry: './js/src/index.js',
+	output: {
+		path: path.resolve( 'js' ),
+		filename: 'index.js',
+		library: [ 'wp', '[name]' ],
+		libraryTarget: 'this',
+	},
+	externals,
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract( {
+					fallback: 'style-loader',
+					use: [ 'css-loader', 'sass-loader' ],
+				} ),
+			},
+		],
+	},
+	plugins: [
+		new ExtractTextPlugin( 'style.css' ),
+	],
+};
+
+module.exports = webpackConfig;
