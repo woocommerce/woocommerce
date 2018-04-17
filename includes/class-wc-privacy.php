6598f6dc61f900aecdad58824fe5145741b27cfd
Mike Jolley <mike.jolley@me.com>
2018-04-17 12:25:03 +0100

    Basic privacy class from #19330

---
+++ includes/class-wc-privacy.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Privacy/GDPR related functionality which ties into WordPress functionality.
+ *
+ * @since 3.4.0
+ * @package WooCommerce\Classes
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * WC_Privacy Class.
+ */
+class WC_Privacy {
+
+	/**
+	 * Init - hook into events.
+	 */
+	public static function init() {
+
+	}
+}
+
+WC_Privacy::init();
