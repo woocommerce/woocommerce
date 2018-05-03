9dbcf8570c632e2b866bf26297a72156f495e4b5
Kelly Dwan <ryelle@users.noreply.github.com>
2018-05-03 14:49:21 -0400

    Remove filters from base dashboard component

--- plugins/woocommerce-admin/js/src/dashboard/index.js
+++ plugins/woocommerce-admin/js/src/dashboard/index.js
@@ -2,30 +2,24 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { applyFilters } from '@wordpress/hooks';
-import { Component, compose } from '@wordpress/element';
+import { Component } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
-import useFilters from '../use-filters';
 import WidgetNumbers from './widgets/numbers';
+import ActivityList from './widgets/activity';
 
-class Dashboard extends Component {
+export default class extends Component {
 	render() {
 		return (
-			<div>
-				<h2>{ applyFilters( 'woodash.example2', __( 'Example Widget', 'woo-dash' ) ) }</h2>
-				<WidgetNumbers />
+			<div className="woo-dashboard">
+				<div className="woo-dash__primary">
+					<WidgetNumbers />
+				</div>
 
-				<h3>{ applyFilters( 'woodash.example', __( 'Example Text', 'woo-dash' ) ) }</h3>
 			</div>
 		);
 	}
 }
-
-export default compose( [
-	useFilters( [ 'woodash.example', 'woodash.example2' ] ),
-] )( Dashboard );
