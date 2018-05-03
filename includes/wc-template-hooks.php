3c905387dc2ce8c086a87bca2d74a925bf0d2a14
Mike Jolley <mike.jolley@me.com>
2018-05-03 10:06:47 +0100

    Actions should be filters

--- includes/wc-template-hooks.php
+++ includes/wc-template-hooks.php
@@ -18,8 +18,8 @@ add_filter( 'post_class', 'wc_product_post_class', 20, 3 );
  *
  * @see wc_generator_tag()
  */
-add_action( 'get_the_generator_html', 'wc_generator_tag', 10, 2 );
-add_action( 'get_the_generator_xhtml', 'wc_generator_tag', 10, 2 );
+add_filter( 'get_the_generator_html', 'wc_generator_tag', 10, 2 );
+add_filter( 'get_the_generator_xhtml', 'wc_generator_tag', 10, 2 );
 
 /**
  * Content Wrappers.
