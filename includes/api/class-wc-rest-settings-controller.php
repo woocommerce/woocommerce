2c476f60ceba4e578e6bfdf3f999cca8d04cc1ed
Claudio Sanches <contato@claudiosanches.com>
2018-05-17 16:14:38 +0100

    Included REST API feature plugin endpoints and changes

--- includes/api/class-wc-rest-settings-controller.php
+++ includes/api/class-wc-rest-settings-controller.php
@@ -24,4 +24,46 @@ class WC_REST_Settings_Controller extends WC_REST_Settings_V2_Controller {
 	 * @var string
 	 */
 	protected $namespace = 'wc/v3';
+
+	/**
+	 * Register routes.
+	 */
+	public function register_routes() {
+		parent::register_routes();
+		register_rest_route( $this->namespace, '/' . $this->rest_base . '/batch', array(
+			array(
+				'methods'             => WP_REST_Server::EDITABLE,
+				'callback'            => array( $this, 'batch_items' ),
+				'permission_callback' => array( $this, 'update_items_permissions_check' ),
+			),
+			'schema' => array( $this, 'get_public_batch_schema' ),
+		) );
+	}
+
+	/**
+	 * Makes sure the current user has access to WRITE the settings APIs.
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return WP_Error|bool
+	 */
+	public function update_items_permissions_check( $request ) {
+		if ( ! wc_rest_check_manager_permissions( 'settings', 'edit' ) ) {
+			return new WP_Error( 'woocommerce_rest_cannot_edit', __( 'Sorry, you cannot edit this resource.', 'woocommerce' ), array( 'status' => rest_authorization_required_code() ) );
+		}
+
+		return true;
+	}
+
+	/**
+	 * Update a setting.
+	 *
+	 * @param  WP_REST_Request $request Request data.
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public function update_item( $request ) {
+		$options_controller = new WC_REST_Dev_Setting_Options_Controller();
+		$response           = $options_controller->update_item( $request );
+
+		return $response;
+	}
 }
