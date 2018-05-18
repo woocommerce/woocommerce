6f352fb816dd640216cae8aa52aeea1d1ad08b36
Khan M Rashedun-Naby <naby88@gmail.com>
2018-05-18 15:43:22 +0600

    woocommerce_subcategory_thumbnail refactor

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -2334,32 +2334,44 @@ if ( ! function_exists( 'woocommerce_subcategory_thumbnail' ) ) {
 	 * @param mixed $category Category.
 	 */
 	function woocommerce_subcategory_thumbnail( $category ) {
-		$small_thumbnail_size = apply_filters( 'subcategory_archive_thumbnail_size', 'woocommerce_thumbnail' );
-		$dimensions           = wc_get_image_size( $small_thumbnail_size );
-		$thumbnail_id         = get_woocommerce_term_meta( $category->term_id, 'thumbnail_id', true );
+		$small_thumbnail_size   = apply_filters( 'subcategory_archive_thumbnail_size', 'woocommerce_thumbnail' );
+		$dimensions             = wc_get_image_size( $small_thumbnail_size );
+		$thumbnail_id           = get_woocommerce_term_meta( $category->term_id, 'thumbnail_id', true );
+
+		$image                  = wc_placeholder_img_src();
+		$image_srcset           = false;
+		$image_sizes            = false;
 
 		if ( $thumbnail_id ) {
 			$image        = wp_get_attachment_image_src( $thumbnail_id, $small_thumbnail_size );
 			$image        = $image[0];
-			$image_srcset = function_exists( 'wp_get_attachment_image_srcset' ) ? wp_get_attachment_image_srcset( $thumbnail_id, $small_thumbnail_size ) : false;
-			$image_sizes  = function_exists( 'wp_get_attachment_image_sizes' ) ? wp_get_attachment_image_sizes( $thumbnail_id, $small_thumbnail_size ) : false;
-		} else {
-			$image        = wc_placeholder_img_src();
-			$image_srcset = false;
-			$image_sizes  = false;
+			$image_srcset = function_exists( 'wp_get_attachment_image_srcset' )
+							? wp_get_attachment_image_srcset( $thumbnail_id, $small_thumbnail_size )
+							: false;
+			$image_sizes  = function_exists( 'wp_get_attachment_image_sizes' )
+							? wp_get_attachment_image_sizes( $thumbnail_id, $small_thumbnail_size )
+							: false;
 		}
 
 		if ( $image ) {
 			// Prevent esc_url from breaking spaces in urls for image embeds.
 			// Ref: https://core.trac.wordpress.org/ticket/23605.
 			$image = str_replace( ' ', '%20', $image );
+			$html  = '<img src="'
+						. esc_url( $image ) . '" alt="'
+						. esc_attr( $category->name ) . '" width="'
+						. esc_attr( $dimensions['width'] ) . '" height="';
 
 			// Add responsive image markup if available.
 			if ( $image_srcset && $image_sizes ) {
-				echo '<img src="' . esc_url( $image ) . '" alt="' . esc_attr( $category->name ) . '" width="' . esc_attr( $dimensions['width'] ) . '" height="' . esc_attr( $dimensions['height'] ) . '" srcset="' . esc_attr( $image_srcset ) . '" sizes="' . esc_attr( $image_sizes ) . '" />';
+				$html .= esc_attr( $dimensions['height'] ) . '" srcset="'
+							. esc_attr( $image_srcset ) . '" sizes="'
+							. esc_attr( $image_sizes ) . '" />';
 			} else {
-				echo '<img src="' . esc_url( $image ) . '" alt="' . esc_attr( $category->name ) . '" width="' . esc_attr( $dimensions['width'] ) . '" height="' . esc_attr( $dimensions['height'] ) . '" />';
+				$html .= esc_attr( $dimensions['height'] ) . '" />';
 			}
+
+			echo $html; // WPCS: XSS ok.
 		}
 	}
 }
