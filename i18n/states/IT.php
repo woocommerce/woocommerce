woocommerce/woocommerce

commit 7a453b602177f4212465c5ba03cb1fda816a90a9

Updated IT provinces
Removed some provinces that don't exist from February 2016 and added one that has been created at the same point.

i18n/states/IT.php
@@ -32,7 +32,6 @@ $states['IT'] = array(
 	'CA' => __( 'Cagliari', 'woocommerce' ),
 	'CL' => __( 'Caltanissetta', 'woocommerce' ),
 	'CB' => __( 'Campobasso', 'woocommerce' ),
-	'CI' => __( 'Carbonia-Iglesias', 'woocommerce' ),
 	'CE' => __( 'Caserta', 'woocommerce' ),
 	'CT' => __( 'Catania', 'woocommerce' ),
 	'CZ' => __( 'Catanzaro', 'woocommerce' ),
@@ -73,7 +72,6 @@ $states['IT'] = array(
 	'NA' => __( 'Napoli', 'woocommerce' ),
 	'NO' => __( 'Novara', 'woocommerce' ),
 	'NU' => __( 'Nuoro', 'woocommerce' ),
-	'OT' => __( 'Olbia-Tempio', 'woocommerce' ),
 	'OR' => __( 'Oristano', 'woocommerce' ),
 	'PD' => __( 'Padova', 'woocommerce' ),
 	'PA' => __( 'Palermo', 'woocommerce' ),
@@ -97,17 +95,16 @@ $states['IT'] = array(
 	'RM' => __( 'Roma', 'woocommerce' ),
 	'RO' => __( 'Rovigo', 'woocommerce' ),
 	'SA' => __( 'Salerno', 'woocommerce' ),
-	'VS' => __( 'Medio Campidano', 'woocommerce' ),
 	'SS' => __( 'Sassari', 'woocommerce' ),
 	'SV' => __( 'Savona', 'woocommerce' ),
 	'SI' => __( 'Siena', 'woocommerce' ),
 	'SR' => __( 'Siracusa', 'woocommerce' ),
 	'SO' => __( 'Sondrio', 'woocommerce' ),
+	'SU' -> __( 'Sud Sardegna', 'woocommerce' ),
 	'TA' => __( 'Taranto', 'woocommerce' ),
 	'TE' => __( 'Teramo', 'woocommerce' ),
 	'TR' => __( 'Terni', 'woocommerce' ),
 	'TO' => __( 'Torino', 'woocommerce' ),
-	'OG' => __( 'Ogliastra', 'woocommerce' ),
 	'TP' => __( 'Trapani', 'woocommerce' ),
 	'TN' => __( 'Trento', 'woocommerce' ),
 	'TV' => __( 'Treviso', 'woocommerce' ),

commit b66b101c9fb70b8734a09af65d7609e2f90ffafa

Fix syntax error in i18n/states/IT.php
Error introduced by commit 7a453b602177f4212465c5ba03cb1fda816a90a9 (PR #20286)

i18n/states/IT.php
@@ -100,7 +100,7 @@ $states['IT'] = array(
 	'SI' => __( 'Siena', 'woocommerce' ),
 	'SR' => __( 'Siracusa', 'woocommerce' ),
 	'SO' => __( 'Sondrio', 'woocommerce' ),
-	'SU' -> __( 'Sud Sardegna', 'woocommerce' ),
+	'SU' => __( 'Sud Sardegna', 'woocommerce' ),
 	'TA' => __( 'Taranto', 'woocommerce' ),
 	'TE' => __( 'Teramo', 'woocommerce' ),
 	'TR' => __( 'Terni', 'woocommerce' ),