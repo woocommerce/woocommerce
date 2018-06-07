5d270fd099a1fcdf106f775372efd74a256d6a75
Roshan Sameer <belong2roshan@gmail.com>
2018-06-07 14:25:04 +0545

    Simplify the function exits check

--- includes/wc-conditional-functions.php
+++ includes/wc-conditional-functions.php
@@ -262,11 +262,7 @@ if ( ! function_exists( 'is_ajax' ) ) {
 	 * @return bool
 	 */
 	function is_ajax() {
-		if ( function_exists( 'wp_doing_ajax' ) ) {
-			return wp_doing_ajax();
-		}
-
-		return defined( 'DOING_AJAX' );
+		return function_exists( 'wp_doing_ajax' ) ? wp_doing_ajax() : defined( 'DOING_AJAX' );
 	}
 }
 
