467bd73ea91595250265c2b834ebf8ce4cb074e9
Rodrigo Primo <rodrigosprimo@gmail.com>
2018-06-07 09:40:12 -0300

    phpcs fixes

--- includes/admin/views/html-notice-updating.php
+++ includes/admin/views/html-notice-updating.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Admin View: Notice - Updating
+ *
+ * @package WooCommerce\Admin
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -9,5 +11,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <div id="message" class="updated woocommerce-message wc-connect">
-	<p><strong><?php _e( 'WooCommerce data update', 'woocommerce' ); ?></strong> &#8211; <?php _e( 'Your database is being updated in the background.', 'woocommerce' ); ?> <a href="<?php echo esc_url( add_query_arg( 'force_update_woocommerce', 'true', admin_url( 'admin.php?page=wc-settings' ) ) ); ?>"><?php _e( 'Taking a while? Click here to run it now.', 'woocommerce' ); ?></a></p>
+	<p>
+		<strong><?php esc_html_e( 'WooCommerce data update', 'woocommerce' ); ?></strong> &#8211; <?php esc_html_e( 'Your database is being updated in the background.', 'woocommerce' ); ?>
+		<a href="<?php echo esc_url( add_query_arg( 'force_update_woocommerce', 'true', admin_url( 'admin.php?page=wc-settings' ) ) ); ?>">
+			<?php esc_html_e( 'Taking a while? Click here to run it now.', 'woocommerce' ); ?>
+		</a>
+	</p>
 </div>
