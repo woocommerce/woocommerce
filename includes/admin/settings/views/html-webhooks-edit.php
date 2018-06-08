38b1381fffdb24988386de3a42a4855af9be5120
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2018-06-09 00:07:20 +0900

    [#20404] Code style standards

--- includes/admin/settings/views/html-webhooks-edit.php
+++ includes/admin/settings/views/html-webhooks-edit.php
@@ -89,7 +89,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 							$selected = $topic_slug === $topic_data['topic'] || $topic_slug === $topic_data['resource'] . '.' . $topic_data['event'];
 
 							?>
-							<option value="<?php echo esc_attr( $topic_slug ); ?>" <?php selected( $selected, true, true ); ?>><?php echo esc_html( $topic_name ); ?></option>
+								<option value="<?php echo esc_attr( $topic_slug ); ?>" <?php selected( $selected, true, true ); ?>><?php echo esc_html( $topic_name ); ?></option>
 						<?php endforeach; ?>
 					</select>
 				</td>
