cfbf550783590fcaeac40c6b80e01616f6902279
Gerhard Potgieter <potgieterg@gmail.com>
2018-06-11 09:52:21 +0200

    Remove additional OR

--- includes/class-wc-install.php
+++ includes/class-wc-install.php
@@ -566,15 +566,18 @@ class WC_Install {
 		// Add constraint to download logs if the columns matches.
 		if ( ! empty( $download_permissions_column_type ) && ! empty( $download_log_column_type ) && $download_permissions_column_type === $download_log_column_type ) {
 			$wpdb->query( "
-				IF NOT EXISTS ( SELECT NULL FROM information_schema.TABLE_CONSTRAINTS WHERE
-					CONSTRAINT_SCHEMA = '{$wpdb->dbname}' AND
-					(
-						CONSTRAINT_NAME LIKE '%wc_download_log_ib%' OR
-						CONSTRAINT_NAME = 'fk_wc_download_log_permission_id' OR
+				IF NOT EXISTS (
+					SELECT NULL FROM information_schema.TABLE_CONSTRAINTS
+					WHERE
+						CONSTRAINT_SCHEMA = '{$wpdb->dbname}' AND
+						(
+							CONSTRAINT_NAME LIKE '%wc_download_log_ib%' OR
+							CONSTRAINT_NAME = 'fk_wc_download_log_permission_id' OR
+						)
+						AND CONSTRAINT_TYPE = 'FOREIGN KEY'
 					)
-					AND CONSTRAINT_TYPE = 'FOREIGN KEY')
 				THEN
-					ALTER TABLE {$wpdb->prefix}wc_download_log
+					ALTER TABLE `{$wpdb->prefix}wc_download_log`
 					ADD CONSTRAINT `fk_wc_download_log_permission_id`
 					FOREIGN KEY (permission_id)
 					REFERENCES {$wpdb->prefix}woocommerce_downloadable_product_permissions(permission_id) ON DELETE CASCADE
