007fc6e0caf8996b95cdf98d267a45617e7095d4
dezio1900 <info@dejankordic.com>
2018-06-11 20:27:32 +0200

    Allow items to be changed in wc_save_order_items function
    Allow other plugins to be able to change $item object, for example to adjust line subtotal/total before its saved. 
    
    My need for this change is because I have custom booking product item, and attached custom inputs for the item ( checkin / checkout dates etc ). So whenever those inputs are changed in order edit page and ajax save is triggered, I need to auto adjust booking item's line subtotal/total ( because of booking dates changed ) before item is saved.

--- includes/admin/wc-admin-functions.php
+++ includes/admin/wc-admin-functions.php
@@ -250,6 +250,9 @@ function wc_save_order_items( $order_id, $items ) {
 					}
 				}
 			}
+			
+			// Allow other plugins to change item object before it is saved.
+			do_action( 'woocommerce_before_save_order_item', $item, $item_data, $order_id );
 
 			$item->save();
 		}
