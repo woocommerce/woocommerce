2a87c2db70a510ad077e21130d8922473ebb560b
Mike Jolley <mike.jolley@me.com>
2018-06-22 13:43:14 +0100

    Correct version

--- templates/order/order-again.php
+++ templates/order/order-again.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 2.5.0
+ * @version 3.5.0
  */
 
 defined( 'ABSPATH' ) || exit;
