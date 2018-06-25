d92cefc88fc1356683c9050bca22af26e9a4f407
Hardip Parmar <hardip.parmar@multidots.com>
2018-06-25 13:05:01 +0530

    #20603 added enhancement Select2 in Product Categories widget.

--- includes/class-wc-frontend-scripts.php
+++ includes/class-wc-frontend-scripts.php
@@ -354,7 +354,7 @@ class WC_Frontend_Scripts {
 		if ( is_cart() ) {
 			self::enqueue_script( 'wc-cart' );
 		}
-		if ( is_cart() || is_checkout() || is_account_page() ) {
+		if ( is_cart() || is_checkout() || is_account_page() || is_product() ) {
 			self::enqueue_script( 'selectWoo' );
 			self::enqueue_style( 'select2' );
 
