0c01b6c3bbbe04e3b33110127d92bee0b5c26e58
Gerhard Potgieter <potgieterg@gmail.com>
2018-07-11 15:05:18 +0200

    Add wc_get_credit_card_type_label unit test

--- tests/unit-tests/util/class-wc-tests-core-functions.php
+++ tests/unit-tests/util/class-wc-tests-core-functions.php
@@ -780,4 +780,17 @@ class WC_Tests_Core_Functions extends WC_Unit_Test_Case {
 
 		$this->assertEquals( $expected_combinations, array_reverse( wc_array_cartesian( $variation_attributes ) ) );
 	}
+
+	/**
+	 * Test wc_get_credit_card_type_label.
+	 *
+	 * @return void
+	 */
+	public function test_wc_get_credit_card_type_label() {
+		$this->assertEquals( 'Visa', wc_get_credit_card_type_label( 'visa' ) );
+		$this->assertEquals( 'JCB', wc_get_credit_card_type_label( 'jCb' ) );
+		$this->assertEquals( 'MasterCard', wc_get_credit_card_type_label( 'Mastercard' ) );
+		$this->assertEquals( 'American Express', wc_get_credit_card_type_label( 'american_express' ) );
+		$this->assertEquals( 'American Express', wc_get_credit_card_type_label( 'american-express' ) );
+	}
 }
