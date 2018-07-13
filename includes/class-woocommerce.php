0c17fd49e4c57443f9d07f9a2c94ca573178c687
Brent Shepherd <thenbrent@gmail.com>
2018-07-13 14:21:03 +1000

    Expand error logging to E_ERROR like errors
    Including those triggered by code via trigger_error() and compile-time
    parse errors (including those generated by Zend Scripting Engine).

--- includes/class-woocommerce.php
+++ includes/class-woocommerce.php
@@ -183,7 +183,7 @@ final class WooCommerce {
 	 */
 	public function log_errors() {
 		$error = error_get_last();
-		if ( E_ERROR === $error['type'] ) {
+		if ( in_array( $error['type'], array( E_ERROR, E_PARSE, E_COMPILE_ERROR, E_USER_ERROR, E_RECOVERABLE_ERROR ) ) ) {
 			$logger = wc_get_logger();
 			$logger->critical(
 				$error['message'] . PHP_EOL,
