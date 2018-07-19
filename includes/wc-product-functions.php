95a99a93b7cde06dca12f5c228912b5c5bc30726
Gerhard Potgieter <potgieterg@gmail.com>
2018-07-19 11:53:28 +0200

    Just use src as there is no first index on src, it just returns the first char of the string which defeats the purpose of the check.

--- includes/wc-product-functions.php
+++ includes/wc-product-functions.php
@@ -455,8 +455,8 @@ add_action( 'woocommerce_scheduled_sales', 'wc_scheduled_sales' );
  * @return array
  */
 function wc_get_attachment_image_attributes( $attr ) {
-	if ( isset( $attr['src'][0] ) && strstr( $attr['src'][0], 'woocommerce_uploads/' ) ) {
-		$attr['src'][0] = wc_placeholder_img_src();
+	if ( isset( $attr['src'] ) && strstr( $attr['src'], 'woocommerce_uploads/' ) ) {
+		$attr['src'] = wc_placeholder_img_src();
 	}
 	return $attr;
 }
