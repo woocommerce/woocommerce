37ec78eb4249231d47ca4ceb1f2567a4dab2a2f3
Gerhard Potgieter <potgieterg@gmail.com>
2018-07-20 10:30:20 +0200

    Set no memory limit to avoid out of memory errors.

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ ${RUN_CODE_COVERAGE} == 1 ]]; then
-	phpdbg -qrr $HOME/.composer/vendor/bin/phpunit -c phpunit.xml --coverage-clover=coverage.clover
+	phpdbg -qrr -d memory_limit=-1 $HOME/.composer/vendor/bin/phpunit -c phpunit.xml --coverage-clover=coverage.clover
 else
 	phpunit -c phpunit.xml
 fi
