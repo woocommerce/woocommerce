8e4466c9c8e59b8227bdea5519fe866175c074e8
Brent Shepherd <brent@prospress.com>
2018-07-26 12:12:22 +1000

    Add WC_Queue_Interface
    To provide an agreed set of public methods that need to be defined
    for any class wanting to act as WooCommerce's queue handler.
    
    The require APIs support both delay jobs, by providing APIs to
    schedule an action/job/event for some time in the future, and a
    non-delayed job API, to add an event to the queue to run as soon
    as possible.

--- includes/class-woocommerce.php
+++ includes/class-woocommerce.php
@@ -276,6 +276,7 @@ final class WooCommerce {
 		include_once WC_ABSPATH . 'includes/interfaces/class-wc-logger-interface.php';
 		include_once WC_ABSPATH . 'includes/interfaces/class-wc-log-handler-interface.php';
 		include_once WC_ABSPATH . 'includes/interfaces/class-wc-webhooks-data-store-interface.php';
+		include_once WC_ABSPATH . 'includes/interfaces/class-wc-queue-interface.php';
 
 		/**
 		 * Abstract classes.
