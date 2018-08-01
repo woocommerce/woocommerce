83df113eac37b88c0ffe4e6b5a68c5c28ddc031c
claudiulodro <claudiulodro@gmail.com>
2018-08-01 10:45:44 -0700

    Set up product reports and schema

--- includes/api/class-wc-rest-reports-products-controller.php
+++ includes/api/class-wc-rest-reports-products-controller.php
@@ -31,4 +31,186 @@ class WC_REST_Reports_Products_Controller extends WC_REST_Reports_Controller {
 	 * @var string
 	 */
 	protected $rest_base = 'reports/products';
+
+	/**
+	 * Get items.
+	 *
+	 * @param WP_REST_Request $request Request data.
+	 *
+	 * @return array|WP_Error
+	 */
+	public function get_items( $request ) {
+		// @todo Apply reports interface.
+		$data    = array();
+		$reports = array();
+
+		foreach ( $reports as $report ) {
+			$item   = $this->prepare_item_for_response( (object) $report, $request );
+			$data[] = $this->prepare_response_for_collection( $item );
+		}
+
+		return rest_ensure_response( $data );
+	}
+
+	/**
+	 * Prepare a report object for serialization.
+	 *
+	 * @param stdClass        $report  Report data.
+	 * @param WP_REST_Request $request Request object.
+	 * @return WP_REST_Response
+	 */
+	public function prepare_item_for_response( $report, $request ) {
+		// @todo Apply reports interface.
+		$data = array();
+
+		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
+		$data    = $this->add_additional_fields_to_object( $data, $request );
+		$data    = $this->filter_response_by_context( $data, $context );
+
+		// Wrap the data in a response object.
+		$response = rest_ensure_response( $data );
+
+		/**
+		 * Filter a report returned from the API.
+		 *
+		 * Allows modification of the report data right before it is returned.
+		 *
+		 * @param WP_REST_Response $response The response object.
+		 * @param object           $report   The original report object.
+		 * @param WP_REST_Request  $request  Request used to generate the response.
+		 */
+		return apply_filters( 'woocommerce_rest_prepare_report_revenue_stats', $response, $report, $request );
+	}
+
+	/**
+	 * Get the Report's schema, conforming to JSON Schema.
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		$schema = array(
+			'$schema'    => 'http://json-schema.org/draft-04/schema#',
+			'title'      => 'report_products',
+			'type'       => 'object',
+			'properties' => array(
+				'product_id' => array(
+					'type'        => 'integer',
+					'readonly'    => true,
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Product ID.', 'woocommerce' ),
+				),
+				'items_sold' => array(
+					'type'        => 'integer',
+					'readonly'    => true,
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Number of items sold.', 'woocommerce' ),
+				),
+				'gross_revenue' => array(
+					'type'        => 'number',
+					'readonly'    => true,
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Total gross revenue of all items sold.', 'woocommerce' ),
+				),
+				'orders_count' => array(
+					'type'        => 'integer',
+					'readonly'    => true,
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Number of orders product appeared in.', 'woocommerce' ),
+				),
+				'_links' => array(
+					'type'        => 'object',
+					'context'     => array( 'view', 'edit' ),
+					'description' => __( 'Links to detailed product reports.', 'woocommerce' ),
+					'properties'  => array(
+						'product' => array(
+							'type'     => 'array',
+							'readonly' => true,
+							'context'  => array( 'view', 'edit' ),
+							'items' => array(
+								'type'       => 'object',
+								'readonly'   => true,
+								'context'    => array( 'view', 'edit' ),
+								'properties' => array(
+									'href' => array(
+										'type'     => 'string',
+										'readonly' => true,
+										'context'  => array( 'view', 'edit' ),
+									),
+								),
+							),
+						),
+					),
+				),
+			),
+		);
+
+		return $this->add_additional_fields_schema( $schema );
+	}
+
+	/**
+	 * Get the query params for collections.
+	 *
+	 * @return array
+	 */
+	public function get_collection_params() {
+		$params             = array();
+		$params['context']  = $this->get_context_param( array( 'default' => 'view' ) );
+		$params['page']     = array(
+			'description'       => __( 'Current page of the collection.', 'woocommerce' ),
+			'type'              => 'integer',
+			'default'           => 1,
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+			'minimum'           => 1,
+		);
+		$params['per_page'] = array(
+			'description'       => __( 'Maximum number of items to be returned in result set.', 'woocommerce' ),
+			'type'              => 'integer',
+			'default'           => 10,
+			'minimum'           => 1,
+			'maximum'           => 100,
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['after']    = array(
+			'description'       => __( 'Limit response to resources published after a given ISO8601 compliant date.', 'woocommerce' ),
+			'type'              => 'string',
+			'format'            => 'date-time',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['before']   = array(
+			'description'       => __( 'Limit response to resources published before a given ISO8601 compliant date.', 'woocommerce' ),
+			'type'              => 'string',
+			'format'            => 'date-time',
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['order']    = array(
+			'description'       => __( 'Order sort attribute ascending or descending.', 'woocommerce' ),
+			'type'              => 'string',
+			'default'           => 'desc',
+			'enum'              => array( 'asc', 'desc' ),
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['orderby']  = array(
+			'description'       => __( 'Sort collection by object attribute.', 'woocommerce' ),
+			'type'              => 'string',
+			'default'           => 'date',
+			'enum'              => array(
+				'date',
+				'gross_revenue',
+				'orders_count',
+				'items_sold',
+			),
+			'validate_callback' => 'rest_validate_request_arg',
+		);
+		$params['categories'] = array(
+			'description'       => __( 'Limit result to items from the specified categories.', 'woocommerce' ),
+			'type'              => 'integer',
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+			'minimum'           => 1,
+		);
+
+		return $params;
+	}
 }
