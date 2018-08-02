d013fe6926f6eac2e08c836ca23d7bc93e58365d
Timmy Crawford <timmydcrawford@gmail.com>
2018-08-02 16:10:55 -0700

    Dashboard: Add columns for blocks.

--- plugins/woocommerce-admin/client/dashboard/index.js
+++ plugins/woocommerce-admin/client/dashboard/index.js
@@ -8,9 +8,11 @@ import { Component, Fragment } from '@wordpress/element';
 /**
  * Internal dependencies
  */
+import './style.scss';
 import Header from 'layout/header';
 import StorePerformance from './store-performance';
 import Chart from 'components/chart';
+import Card from '../components/card';
 
 export default class Dashboard extends Component {
 	render() {
@@ -19,6 +21,14 @@ export default class Dashboard extends Component {
 				<Header sections={ [ __( 'Dashboard', 'wc-admin' ) ] } />
 				<StorePerformance />
 				<Chart />
+				<div className="woocommerce-dashboard__columns">
+					<div className="woocommerce-dashboard__column">
+						<Card title={ __( 'Top Selling Products', 'wc-admin' ) } />
+					</div>
+					<div className="woocommerce-dashboard__column">
+						<Card title={ __( 'Orders', 'wc-admin' ) } />
+					</div>
+				</div>
 			</Fragment>
 		);
 	}
