2bea2324948d2a60cef672bd8e21fd9bed5b2dbf
Kelly Dwan <ryelle@users.noreply.github.com>
2018-08-06 17:33:55 -0400

    Import compose from `wordpress/compose` (https://github.com/woocommerce/woocommerce-admin/pull/278)
    As of Gutenberg 3.3, compose is in its own package - element.compose is deprecated

--- plugins/woocommerce-admin/client/analytics/report/orders/index.js
+++ plugins/woocommerce-admin/client/analytics/report/orders/index.js
@@ -3,7 +3,8 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component, Fragment, compose } from '@wordpress/element';
+import { Component, Fragment } from '@wordpress/element';
+import { compose } from '@wordpress/compose';
 import { Button } from '@wordpress/components';
 import { withSelect, withDispatch } from '@wordpress/data';
 import moment from 'moment';
