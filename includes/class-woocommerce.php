1b9fe9156cdcf9343291dd3cf35d4faef6e3c808
claudiulodro <claudiulodro@gmail.com>
2018-08-08 10:48:27 -0700

    Re-add removed order stats query stuff

--- includes/class-woocommerce.php
+++ includes/class-woocommerce.php
@@ -333,6 +333,7 @@ final class WooCommerce {
 		include_once WC_ABSPATH . 'includes/class-wc-shortcodes.php';
 		include_once WC_ABSPATH . 'includes/class-wc-logger.php';
 		include_once WC_ABSPATH . 'includes/class-wc-reports-revenue-query.php';
+		include_once WC_ABSPATH . 'includes/class-wc-reports-orders-stats-query.php';
 		include_once WC_ABSPATH . 'includes/class-wc-reports-products-query.php';
 
 		/**
