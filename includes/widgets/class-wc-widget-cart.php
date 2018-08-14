826e94f5a67dba3a9c9bc74233b8bebad265d681
Khan M Rashedun-Naby <naby88@gmail.com>
2018-08-14 13:03:02 +0600

    Added @todo comment to PHPDoc

--- includes/widgets/class-wc-widget-cart.php
+++ includes/widgets/class-wc-widget-cart.php
@@ -77,6 +77,9 @@ class WC_Widget_Cart extends WC_Widget {
 	/**
 	 * This method provides the JS script which will execute on addition of a new widget.
 	 *
+	 * @todo 1. In this function there is a redundency of code, which needs to be fixed.
+	 *       2. Also sort out a better way to fix the raw JS code block. May be using `wc_enqueue_js()`.
+	 *
 	 * @return void
 	 */
 	private function enqueue_ajax_script() {
