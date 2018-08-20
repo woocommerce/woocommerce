ac550e9d37802bdc9f511a6da616cb541034a719
Peter Fabian <peter.fabian.github@gmail.com>
2018-08-20 18:38:53 +0200

    PHPCS.

--- templates/notices/notice.php
+++ templates/notices/notice.php
@@ -10,14 +10,14 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @author      WooThemes
+ * @package     WooCommerce/Templates
  * @version     1.6.4
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
 if ( ! $messages ) {
