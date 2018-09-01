03900a43cbdde493c07336fd83c27d0e2e24ff2b
Robert DeVore <deviodigital@gmail.com>
2018-09-01 11:24:04 -0400

    Add action hook to edit account form tag

--- templates/myaccount/form-edit-account.php
+++ templates/myaccount/form-edit-account.php
@@ -19,7 +19,7 @@ defined( 'ABSPATH' ) || exit;
 
 do_action( 'woocommerce_before_edit_account_form' ); ?>
 
-<form class="woocommerce-EditAccountForm edit-account" action="" method="post">
+<form class="woocommerce-EditAccountForm edit-account" action="" method="post"<?php do_action( 'woocommerce_edit_account_form_tag' ); ?>>
 
 	<?php do_action( 'woocommerce_edit_account_form_start' ); ?>
 
