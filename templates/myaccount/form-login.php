4a71ce465969d56c04299541e23cc67a67d2376f
Robert DeVore <deviodigital@gmail.com>
2018-09-01 11:24:33 -0400

    Add action hook to register form tag

--- templates/myaccount/form-login.php
+++ templates/myaccount/form-login.php
@@ -69,7 +69,7 @@ do_action( 'woocommerce_before_customer_login_form' ); ?>
 
 		<h2><?php esc_html_e( 'Register', 'woocommerce' ); ?></h2>
 
-		<form method="post" class="woocommerce-form woocommerce-form-register register">
+		<form method="post" class="woocommerce-form woocommerce-form-register register"<?php do_action( 'woocommerce_register_form_tag' ); ?>>
 
 			<?php do_action( 'woocommerce_register_form_start' ); ?>
 
