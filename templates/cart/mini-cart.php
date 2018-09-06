d4f6d173d57d4318d894b5e598c9411bf91bc36a
Manos Psychogyiopoulos <psyx@somewherewarm.net>
2018-09-06 21:05:31 +0300

    Bump template version

--- templates/cart/mini-cart.php
+++ templates/cart/mini-cart.php
@@ -15,7 +15,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.3.0
+ * @version 3.5.0
  */
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
