5eaa6da98b9f8d9aefd9748353cf08ea15421053
Gerhard Potgieter <potgieterg@gmail.com>
2018-09-06 13:00:21 +0200

    Update processing emails copy

--- includes/emails/class-wc-email-customer-processing-order.php
+++ includes/emails/class-wc-email-customer-processing-order.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WC_Email_Customer_Processing_Order', false ) ) :
 	 * An email sent to the customer when a new order is paid for.
 	 *
 	 * @class       WC_Email_Customer_Processing_Order
-	 * @version     2.0.0
+	 * @version     3.5.0
 	 * @package     WooCommerce/Classes/Emails
 	 * @extends     WC_Email
 	 */
@@ -56,7 +56,7 @@ if ( ! class_exists( 'WC_Email_Customer_Processing_Order', false ) ) :
 		 * @return string
 		 */
 		public function get_default_subject() {
-			return __( 'Your {site_title} order receipt from {order_date}', 'woocommerce' );
+			return __( 'Your {site_title} order is now complete', 'woocommerce' );
 		}
 
 		/**
@@ -66,7 +66,7 @@ if ( ! class_exists( 'WC_Email_Customer_Processing_Order', false ) ) :
 		 * @return string
 		 */
 		public function get_default_heading() {
-			return __( 'Thank you for your order', 'woocommerce' );
+			return __( 'Thanks for shopping with us', 'woocommerce' );
 		}
 
 		/**
@@ -99,7 +99,6 @@ if ( ! class_exists( 'WC_Email_Customer_Processing_Order', false ) ) :
 		/**
 		 * Get content html.
 		 *
-		 * @access public
 		 * @return string
 		 */
 		public function get_content_html() {
@@ -117,7 +116,6 @@ if ( ! class_exists( 'WC_Email_Customer_Processing_Order', false ) ) :
 		/**
 		 * Get content plain.
 		 *
-		 * @access public
 		 * @return string
 		 */
 		public function get_content_plain() {
