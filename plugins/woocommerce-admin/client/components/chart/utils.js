1ed72ede255b49be0010c0727461dafaf6d9332a
Robert Elliott <robert.elliott@automattic.com>
2018-09-07 14:45:29 +0200

    colour -> color

--- plugins/woocommerce-admin/client/components/chart/utils.js
+++ plugins/woocommerce-admin/client/components/chart/utils.js
@@ -306,7 +306,7 @@ const showTooltip = ( node, params, d ) => {
 		row => `
 				<li class="key-row">
 					<div class="key-container">
-						<span class="key-colour" style="background-color:${ getColor( row.key, params ) }"></span>
+						<span class="key-color" style="background-color:${ getColor( row.key, params ) }"></span>
 						<span class="key-key">${ row.key }:</span>
 					</div>
 					<span class="key-value">${ formatCurrency( d[ row.key ] ) }</span>
