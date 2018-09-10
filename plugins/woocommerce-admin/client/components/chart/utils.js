4242a64f30a7a2c7af70a6c1a1e5021f44690cbf
Robert Elliott <robert.elliott@automattic.com>
2018-09-10 15:59:14 +0200

    add smallcolorscale fractions for chromatic scaled colors

--- plugins/woocommerce-admin/client/components/chart/utils.js
+++ plugins/woocommerce-admin/client/components/chart/utils.js
@@ -99,10 +99,21 @@ export const getUniqueDates = ( lineData, parseDate ) => {
 };
 
 export const getColor = ( key, params ) => {
-	const keyValue =
-		params.orderedKeys.length > 1
-			? findIndex( params.orderedKeys, d => d.key === key ) / ( params.orderedKeys.length - 1 )
-			: 0;
+	const smallColorScales = [
+		[],
+		[ 0.5 ],
+		[ 0.333, 0.667 ],
+		[ 0.2, 0.5, 0.8 ],
+		[ 0.12, 0.375, 0.625, 0.88 ],
+	];
+	let keyValue = 0;
+	const len = params.orderedKeys.length;
+	const idx = findIndex( params.orderedKeys, d => d.key === key );
+	if ( len < 5 ) {
+		keyValue = smallColorScales[ len ][ idx ];
+	} else {
+		keyValue = idx / ( params.orderedKeys.length - 1 );
+	}
 	return params.colorScheme( keyValue );
 };
 
