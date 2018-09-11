2b7e5121f2742c59107cd2c45f332d37fee12376
Claudio Sanches <contato@claudiosanches.com>
2018-09-11 16:53:55 -0300

    [REST API] Allow apply and remove coupons in orders endpoint
    Closes #21119

--- includes/api/class-wc-rest-orders-controller.php
+++ includes/api/class-wc-rest-orders-controller.php
@@ -26,6 +26,100 @@ class WC_REST_Orders_Controller extends WC_REST_Orders_V2_Controller {
 	protected $namespace = 'wc/v3';
 
 	/**
+	 * Prepare a single order for create or update.
+	 *
+	 * @throws WC_REST_Exception When fails to set any item.
+	 * @param  WP_REST_Request $request Request object.
+	 * @param  bool            $creating If is creating a new object.
+	 * @return WP_Error|WC_Data
+	 */
+	protected function prepare_object_for_database( $request, $creating = false ) {
+		$id        = isset( $request['id'] ) ? absint( $request['id'] ) : 0;
+		$order     = new WC_Order( $id );
+		$schema    = $this->get_item_schema();
+		$data_keys = array_keys( array_filter( $schema['properties'], array( $this, 'filter_writable_props' ) ) );
+
+		// Handle all writable props.
+		foreach ( $data_keys as $key ) {
+			$value = $request[ $key ];
+
+			if ( ! is_null( $value ) ) {
+				switch ( $key ) {
+					case 'status':
+						// Status change should be done later so transitions have new data.
+						break;
+					case 'billing':
+					case 'shipping':
+						$this->update_address( $order, $value, $key );
+						break;
+					case 'line_items':
+					case 'shipping_lines':
+					case 'fee_lines':
+						if ( is_array( $value ) ) {
+							foreach ( $value as $item ) {
+								if ( is_array( $item ) ) {
+									if ( $this->item_is_null( $item ) || ( isset( $item['quantity'] ) && 0 === $item['quantity'] ) ) {
+										$order->remove_item( $item['id'] );
+									} else {
+										$this->set_item( $order, $key, $item );
+									}
+								}
+							}
+						}
+						break;
+					case 'coupon_lines':
+						if ( is_array( $value ) ) {
+							foreach ( $value as $item ) {
+								if ( is_array( $item ) ) {
+									if ( $this->item_is_null( $item ) || ( isset( $item['quantity'] ) && 0 === $item['quantity'] ) ) {
+										if ( ! empty( $item['id'] ) ) {
+											$order->remove_item( $item['id'] );
+										} elseif ( ! empty( $item['code'] ) ) {
+											$order->remove_coupon( wc_clean( $item['code'] ) );
+										}
+									} else {
+										if ( ! empty( $item['id'] ) ) {
+											if ( empty( $item['code'] ) ) {
+												throw new WC_REST_Exception( 'woocommerce_rest_invalid_coupon_coupon', __( 'Coupon code is required.', 'woocommerce' ), 400 );
+											}
+										}
+
+										$order->apply_coupon( wc_clean( $item['code'] ) );
+									}
+								}
+							}
+						}
+						break;
+					case 'meta_data':
+						if ( is_array( $value ) ) {
+							foreach ( $value as $meta ) {
+								$order->update_meta_data( $meta['key'], $meta['value'], isset( $meta['id'] ) ? $meta['id'] : '' );
+							}
+						}
+						break;
+					default:
+						if ( is_callable( array( $order, "set_{$key}" ) ) ) {
+							$order->{"set_{$key}"}( $value );
+						}
+						break;
+				}
+			}
+		}
+
+		/**
+		 * Filters an object before it is inserted via the REST API.
+		 *
+		 * The dynamic portion of the hook name, `$this->post_type`,
+		 * refers to the object type slug.
+		 *
+		 * @param WC_Data         $order    Object object.
+		 * @param WP_REST_Request $request  Request object.
+		 * @param bool            $creating If is creating a new object.
+		 */
+		return apply_filters( "woocommerce_rest_pre_insert_{$this->post_type}_object", $order, $request, $creating );
+	}
+
+	/**
 	 * Prepare objects query.
 	 *
 	 * @since  3.0.0
@@ -49,6 +143,19 @@ class WC_REST_Orders_Controller extends WC_REST_Orders_V2_Controller {
 		}
 
 		return $args;
+	}
+
+	/**
+	 * Get the Order's schema, conforming to JSON Schema.
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		$schema = parent::get_item_schema();
+
+		$schema['properties']['coupon_lines']['items']['properties']['discount']['readonly'] = true;
+
+		return $schema;
 	}
 
 	/**
