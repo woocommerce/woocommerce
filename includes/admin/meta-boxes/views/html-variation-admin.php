37da7b6680e14449688bd818c490de9c04524fed
Reuven Karasik <rubik@karasik.org>
2018-09-13 12:17:51 +0300

    Added action 'woocommerce_variation_header' action

--- includes/admin/meta-boxes/views/html-variation-admin.php
+++ includes/admin/meta-boxes/views/html-variation-admin.php
@@ -48,6 +48,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 		?>
 		<input type="hidden" name="variable_post_id[<?php echo esc_attr( $loop ); ?>]" value="<?php echo esc_attr( $variation_id ); ?>" />
 		<input type="hidden" class="variation_menu_order" name="variation_menu_order[<?php echo esc_attr( $loop ); ?>]" value="<?php echo esc_attr( $variation_object->get_menu_order( 'edit' ) ); ?>" />
+		
+		<?php
+		/**
+		 * woocommerce_variation_header action.
+		 *
+		 * @since 2.5.0
+		 *
+		 * @param array   $variation_data
+		 * @param WP_Post $variation
+		 */
+		do_action( 'woocommerce_variation_header', $variation_data, $variation ); ?>
 	</h3>
 	<div class="woocommerce_variable_attributes wc-metabox-content" style="display: none;">
 		<div class="data">
