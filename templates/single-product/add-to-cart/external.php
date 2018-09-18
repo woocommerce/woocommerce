dfab32b2580aa3ed27c86f793984807526d289dc
Mike Olthoff <mike@olthoff.ca>
2018-09-18 13:31:16 -0700

    Remove class="cart" from external product types
    Having this class is forcing the "add-to-cart" function, however, because this is an external link, this should not be called. This prevents the link to be called. Removing this class fixes the issue.

--- templates/single-product/add-to-cart/external.php
+++ templates/single-product/add-to-cart/external.php
@@ -19,7 +19,7 @@ defined( 'ABSPATH' ) || exit;
 
 do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-<form class="cart" action="<?php echo esc_url( $product_url ); ?>" method="get">
+<form action="<?php echo esc_url( $product_url ); ?>" method="get">
 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 	<button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $button_text ); ?></button>
