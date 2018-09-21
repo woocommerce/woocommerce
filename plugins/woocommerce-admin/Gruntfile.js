0136912a060d638678a9e1849357c0a2c896fd32
Claudio Sanches <contato@claudiosanches.com>
2018-09-21 14:13:28 -0300

    Added grunt-checktextdomain

--- plugins/woocommerce-admin/Gruntfile.js
+++ plugins/woocommerce-admin/Gruntfile.js
@@ -36,9 +36,42 @@ module.exports = function( grunt ) {
 				}
 			}
 		},
+
+		checktextdomain: {
+			options:{
+				text_domain: 'wc-admin',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php',               // Include all files/
+					'!node_modules/**',       // Exclude node_modules/
+					'!tests/**',              // Exclude tests/
+					'!vendor/**',             // Exclude vendor/
+					'!tmp/**'                 // Exclude tmp/
+				],
+				expand: true
+			}
+		}
 	} );
 
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
 	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
 
 	grunt.util.linefeed = '\n';
