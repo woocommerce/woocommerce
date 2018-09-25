e92256309e6fa5116efd2b4938b63043db92197c
Gerhard Potgieter <potgieterg@gmail.com>
2018-09-25 12:09:01 +0200

    Update template version number for template changes in https://github.com/woocommerce/woocommerce/commit/efe06cb14752bdfe2c18274783eac221cbeea5ff

--- templates/cart/cart.php
+++ templates/cart/cart.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.4.0
+ * @version 3.5.0
  */
 
 defined( 'ABSPATH' ) || exit;
