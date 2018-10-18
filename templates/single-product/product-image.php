a947b954f3207dc613b022c9f1691dac948edddb
Rodrigo Primo <rodrigosprimo@gmail.com>
2018-10-18 09:20:05 -0300

    Use $product->get_image_id() instead of has_post_thumbnail()
    This commit replaces all instances where has_post_thumbnail() was used with $product->get_image_id(). This is a continuation of 64b589f0325a729a1914942801f4b2cae929e013 and is needed to support the custom product tables plugin as has_post_thumbnail() relies on a post meta.

--- templates/single-product/product-image.php
+++ templates/single-product/product-image.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.5.0
+ * @version 3.5.1
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -28,7 +28,7 @@ $columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 
 $post_thumbnail_id = $product->get_image_id();
 $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_classes', array(
 	'woocommerce-product-gallery',
-	'woocommerce-product-gallery--' . ( has_post_thumbnail() ? 'with-images' : 'without-images' ),
+	'woocommerce-product-gallery--' . ( $product->get_image_id() ? 'with-images' : 'without-images' ),
 	'woocommerce-product-gallery--columns-' . absint( $columns ),
 	'images',
 ) );
@@ -36,7 +36,7 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
 <div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
 	<figure class="woocommerce-product-gallery__wrapper">
 		<?php
-		if ( has_post_thumbnail() ) {
+		if ( $product->get_image_id() ) {
 			$html = wc_get_gallery_image_html( $post_thumbnail_id, true );
 		} else {
 			$html  = '<div class="woocommerce-product-gallery__image--placeholder">';
