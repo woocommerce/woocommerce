8e46a78a2fa4bd8b071edafba0fad47b5db811fb
Jonathan Belcher <belcherj@gmail.com>
2018-10-23 09:57:45 -0400

    Fix linting errors in tests/test-sample.php

--- plugins/woocommerce-admin/tests/test-sample.php
+++ plugins/woocommerce-admin/tests/test-sample.php
@@ -13,7 +13,7 @@ class SampleTest extends WP_UnitTestCase {
 	/**
 	 * A single example test.
 	 */
-	function test_sample() {
+	public function test_sample() {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
 	}
