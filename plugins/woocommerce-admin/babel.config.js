d5c2787520e60f1f6bffc2399d157ea67dd8fb91
Kelly Dwan <ryelle@users.noreply.github.com>
2018-10-24 17:17:23 -0400

    Build: Update babel config (https://github.com/woocommerce/woocommerce-admin/pull/677)
    * Update babel config
    
    * Fix typo in babel config
    
    * Update babel config, fix missing transform

---
+++ plugins/woocommerce-admin/babel.config.js
@@ -0,0 +1,34 @@
+module.exports = function( api ) {
+	api.cache( true );
+
+	return {
+		presets: [ '@wordpress/babel-preset-default' ],
+		plugins: [
+			'@babel/plugin-transform-async-to-generator',
+			'transform-class-properties',
+			[ '@babel/transform-react-jsx', {
+				pragma: 'createElement',
+			} ],
+			[
+				'@wordpress/babel-plugin-import-jsx-pragma',
+				{
+					scopeVariable: 'createElement',
+					source: '@wordpress/element',
+					isDefault: false,
+				},
+			],
+		],
+		env: {
+			production: {
+				plugins: [
+					[
+						'@wordpress/babel-plugin-makepot',
+						{
+							output: 'languages/wc-admin.pot',
+						},
+					],
+				],
+			},
+		},
+	};
+};
