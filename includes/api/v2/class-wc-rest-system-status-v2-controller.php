acfeff66273178a156f39b5ddd8167c427249433
nishitlangaliya <n.langaliya@gmail.com>
2018-10-27 11:20:21 -0800

    fix: Various 'Table does not exist' in WooCommerce status

--- includes/api/v2/class-wc-rest-system-status-v2-controller.php
+++ includes/api/v2/class-wc-rest-system-status-v2-controller.php
@@ -721,10 +721,10 @@ class WC_REST_System_Status_V2_Controller extends WC_REST_Controller {
 			'index' => 0,
 		);
 
-		$site_tables = $wpdb->tables( 'all', true );
+		$site_tables_prefix = $wpdb->get_blog_prefix( get_current_blog_id() );
 		foreach ( $database_table_sizes as $table ) {
 			// Only include tables matching the prefix of the current site, this is to prevent displaying all tables on a MS install not relating to the current.
-			if ( is_multisite() && ! in_array( $table->name, $site_tables, true ) ) {
+			if ( is_multisite() && 0 !== strpos( $table->name, $site_tables_prefix ) ) {
 				continue;
 			}
 			$table_type = in_array( $table->name, $core_tables ) ? 'woocommerce' : 'other';
