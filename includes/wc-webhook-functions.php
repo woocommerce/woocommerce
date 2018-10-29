b0b3ddccb0cf6964e2ff1c70b85dbd3b9764fa85
claudiulodro <claudiulodro@gmail.com>
2018-10-29 10:32:14 -0700

    remove extra parens

--- includes/wc-webhook-functions.php
+++ includes/wc-webhook-functions.php
@@ -69,7 +69,7 @@ function wc_is_webhook_valid_topic( $topic ) {
 	}
 
 	// Custom topics are prefixed with woocommerce_ or wc_ are valid.
-	if ( ( 0 === strpos( $topic, 'action.woocommerce_' ) || 0 === strpos( $topic, 'action.wc_' ) ) ) {
+	if ( 0 === strpos( $topic, 'action.woocommerce_' ) || 0 === strpos( $topic, 'action.wc_' ) ) {
 		return true;
 	}
 
