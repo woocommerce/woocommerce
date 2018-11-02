da8b16888da60afd32f0053b0221a0e960a8e521
Peter Fabian <peter.fabian.github@gmail.com>
2018-11-02 13:17:38 +0100

    Made composer run in correct dir, now for real.

--- plugins/woocommerce-admin/bin/travis.sh
+++ plugins/woocommerce-admin/bin/travis.sh
@@ -3,11 +3,8 @@
 
 if [ $1 == 'before' ]; then
 
-	cd "$WP_CORE_DIR/wp-content/plugins/wc-admin/"
 	composer global require "phpunit/phpunit=6.*"
-
-	if [[ ${RUN_PHPCS} == 1 ]]; then
-		composer install
-	fi
+	cd "$WP_CORE_DIR/wp-content/plugins/wc-admin/"
+	composer install
 
 fi
