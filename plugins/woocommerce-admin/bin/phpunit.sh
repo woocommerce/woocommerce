3c490a5ce68e19b92eba4cd3ef6a3b3d060a27f3
Peter Fabian <peter.fabian.github@gmail.com>
2018-11-02 11:43:46 +0100

    Switch the directory from which the PHP Unit tests run on Travis.

--- plugins/woocommerce-admin/bin/phpunit.sh
+++ plugins/woocommerce-admin/bin/phpunit.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+WORKING_DIR="$PWD"
+cd "$WP_CORE_DIR/wp-content/plugins/wc-admin/"
+phpunit --version
 phpunit -c phpunit.xml.dist
+TEST_RESULT=$?
+cd "$WORKING_DIR"
+exit $TEST_RESULT
