e95cde4a3ea38acc78a83dcf889e97df713772f1
Peter Fabian <peter.fabian.github@gmail.com>
2018-11-02 15:06:29 +0100

    Updated Travis composer configuration.
    Composer can only run for PHP 7.1+, correct PHPUnit should already be specified in composer.json.
    The PATH export in travis.yml ensures that PHPUnit v6 is used for PHP v7.2.

--- plugins/woocommerce-admin/bin/phpunit.sh
+++ plugins/woocommerce-admin/bin/phpunit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 WORKING_DIR="$PWD"
 cd "$WP_CORE_DIR/wp-content/plugins/wc-admin/"
-./vendor/bin/phpunit --version
-./vendor/bin/phpunit -c phpunit.xml.dist
+phpunit --version
+phpunit -c phpunit.xml.dist
 TEST_RESULT=$?
 cd "$WORKING_DIR"
 exit $TEST_RESULT
--- plugins/woocommerce-admin/bin/travis.sh
+++ plugins/woocommerce-admin/bin/travis.sh
@@ -3,8 +3,10 @@
 
 if [ $1 == 'before' ]; then
 
-	composer global require "phpunit/phpunit=6.*"
-	cd "$WP_CORE_DIR/wp-content/plugins/wc-admin/"
-	composer install
+	if [[ ${RUN_PHPCS} == 1 ]]; then
+		cd "$WP_CORE_DIR/wp-content/plugins/wc-admin/"
+		# This can (currently) only run for PHP 7.1+
+		composer install
+	fi
 
 fi
