b482b3b3548ae715eb3453b86f84f076759b4897
Peter Fabian <peter.fabian.github@gmail.com>
2018-11-02 12:43:58 +0100

    Updated path to PHPUnit for Travis CI tests.

--- plugins/woocommerce-admin/bin/phpunit.sh
+++ plugins/woocommerce-admin/bin/phpunit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 WORKING_DIR="$PWD"
 cd "$WP_CORE_DIR/wp-content/plugins/wc-admin/"
-phpunit --version
-phpunit -c phpunit.xml.dist
+./vendor/bin/phpunit --version
+./vendor/bin/phpunit -c phpunit.xml.dist
 TEST_RESULT=$?
 cd "$WORKING_DIR"
 exit $TEST_RESULT
