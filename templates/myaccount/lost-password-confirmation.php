9c2a0715ae279d1ad7c452b6723806330645ea55
Ron Rennick <ron@ronandandrea.com>
2018-11-06 10:47:06 -0400

    update lost password filter name in password confirmation template

--- templates/myaccount/lost-password-confirmation.php
+++ templates/myaccount/lost-password-confirmation.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.5.0
+ * @version 3.5.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -22,4 +22,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 wc_print_notice( __( 'Password reset email has been sent.', 'woocommerce' ) );
 ?>
 
-<p><?php echo esc_html( apply_filters( 'woocommerce_lost_password_message', __( 'A password reset email has been sent to the email address on file for your account, but may take several minutes to show up in your inbox. Please wait at least 10 minutes before attempting another reset.', 'woocommerce' ) ) ); ?></p>
+<p><?php echo esc_html( apply_filters( 'woocommerce_lost_password_confirmation_message', __( 'A password reset email has been sent to the email address on file for your account, but may take several minutes to show up in your inbox. Please wait at least 10 minutes before attempting another reset.', 'woocommerce' ) ) ); ?></p>
