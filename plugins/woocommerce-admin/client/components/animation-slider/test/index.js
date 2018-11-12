ffaa80a95eb72ec5d5c377814e1fc76b5f0330d4
Jonathan Belcher <belcherj@gmail.com>
2018-11-12 16:49:40 -0500

    Add snapshotting to Animation Slider (https://github.com/woocommerce/woocommerce-admin/pull/819)

---
+++ plugins/woocommerce-admin/client/components/animation-slider/test/index.js
@@ -0,0 +1,23 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import renderer from 'react-test-renderer';
+
+/**
+ * Internal dependencies
+ */
+import AnimationSlider from '../';
+
+describe( 'AnimationSlider', () => {
+	test( 'it renders correctly', () => {
+		const tree = renderer
+			.create(
+				<AnimationSlider animationKey={ 0 } animate={ 'left' }>
+					{ () => <div /> }
+				</AnimationSlider>
+			)
+			.toJSON();
+		expect( tree ).toMatchSnapshot();
+	} );
+} );
