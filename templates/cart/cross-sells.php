3b8b18348e63633960e333bc6de5ef81ebef7ee3
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-11-21 21:03:21 +1000

    [ UPDATE ] wpcs warnings in doc comment and comment for abspath

--- templates/cart/cross-sells.php
+++ templates/cart/cross-sells.php
@@ -10,14 +10,14 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     3.0.0
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+	exit; // Exit if accessed directly.
 }
 
 if ( $cross_sells ) : ?>
