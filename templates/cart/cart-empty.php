816e03af48ff5509531e61f07abececc54075c5a
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-11-21 21:09:17 +1000

    [ UPDATE ] to doc comment with author tag

--- templates/cart/cart-empty.php
+++ templates/cart/cart-empty.php
@@ -11,6 +11,7 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
  * @package WooCommerce/Templates
  * @version 3.5.0
  */
