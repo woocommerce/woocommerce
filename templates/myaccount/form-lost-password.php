af8ca9bfe6e2b26c1f988850afd62881e1623725
Gerhard <potgieterg@gmail.com>
2018-11-21 10:32:28 +0200

    Add woocommerce_before_lost_password_form and woocommerce_after_lost_password_form actions and hook into woocommerce_output_all_notices

--- templates/myaccount/form-lost-password.php
+++ templates/myaccount/form-lost-password.php
@@ -12,11 +12,12 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.4.0
+ * @version 3.5.2
  */
 
 defined( 'ABSPATH' ) || exit;
 
+do_action( 'woocommerce_before_lost_password_form' );
 ?>
 
 <form method="post" class="woocommerce-ResetPassword lost_reset_password">
@@ -40,3 +41,5 @@ defined( 'ABSPATH' ) || exit;
 	<?php wp_nonce_field( 'lost_password', 'woocommerce-lost-password-nonce' ); ?>
 
 </form>
+<?php
+do_action( 'woocommerce_after_lost_password_form' );
