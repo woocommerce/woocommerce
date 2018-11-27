4e06574b4a0b03b002c7e7508cb26d1fbc9cfe3f
dws122 <david@bizbudding.com>
2018-11-27 01:14:34 -0500

    ensure cache_delete on coupon deletion
    regardless of whether coupon is trashed, or deleted permenantly, the object cache containing the coupon should be deleted.

--- includes/data-stores/class-wc-coupon-data-store-cpt.php
+++ includes/data-stores/class-wc-coupon-data-store-cpt.php
@@ -202,10 +202,10 @@ class WC_Coupon_Data_Store_CPT extends WC_Data_Store_WP implements WC_Coupon_Dat
 			return;
 		}
 
+		wp_cache_delete( WC_Cache_Helper::get_cache_prefix( 'coupons' ) . 'coupon_id_from_code_' . $coupon->get_code(), 'coupons' );
+
 		if ( $args['force_delete'] ) {
 			wp_delete_post( $id );
-
-			wp_cache_delete( WC_Cache_Helper::get_cache_prefix( 'coupons' ) . 'coupon_id_from_code_' . $coupon->get_code(), 'coupons' );
 
 			$coupon->set_id( 0 );
 			do_action( 'woocommerce_delete_coupon', $id );
