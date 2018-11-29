5dcd8a1d5fa7b7b41463f956d23e69490a20297c
claudiulodro <claudiulodro@gmail.com>
2018-11-29 08:46:52 -0800

    Bump versions and recompile

--- woocommerce.php
+++ woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
- * Version: 3.5.1
+ * Version: 3.5.2
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
