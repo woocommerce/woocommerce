b8bdc2bf276462bd78f4ddce7b30d94ae8426133
Kelly Dwan <ryelle@users.noreply.github.com>
2018-11-30 16:37:58 -0500

    Category Selection: Add hierarchy support for nested categories (https://github.com/woocommerce/woocommerce-blocks/pull/179)
    * Add buildTermsTree to handle hierarchical list input
    
    * Append items if their parent is not in the given list
    
    * Add `breadcrumbs` property to hierarchical list
    
    * Display the “breadcrumbs” parent categories when searching
    
    * Use display properties
    
    we’re providing alternative text for screen reader users, so we can just toggle display values for the prefix
    
    * Add all parent categories to name provided for screen readers
    
    * Display parent category when parent is a currently selected item
    
    * Add the depth class to items in defaultRenderItem
    
    * Add a test for a hierarchical item list
    
    * Clean up hierarchy docblock & commented-out code
    
    * Update proptype
    
    * Update depth CSS so we still indent very nested categories

--- plugins/woocommerce-blocks/assets/js/components/search-list-control/test/index.js
+++ plugins/woocommerce-blocks/assets/js/components/search-list-control/test/index.js
@@ -18,59 +18,115 @@ const list = [
 	{ id: 6, name: 'Mulberry' },
 ];
 
+const hierarchicalList = [
+	{ id: 1, name: 'Apricots', parent: 0 },
+	{ id: 2, name: 'Clementine', parent: 1 },
+	{ id: 3, name: 'Elderberry', parent: 1 },
+	{ id: 4, name: 'Guava', parent: 3 },
+	{ id: 5, name: 'Lychee', parent: 0 },
+	{ id: 6, name: 'Mulberry', parent: 0 },
+];
+
 describe( 'SearchListControl', () => {
 	test( 'should render a search box and list of options', () => {
 		const component = renderer.create(
-			<SearchListControl list={ list } selected={ [] } onChange={ noop } />
+			<SearchListControl
+				instanceId={ 1 }
+				list={ list }
+				selected={ [] }
+				onChange={ noop }
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
 
 	test( 'should render a search box and list of options with a custom className', () => {
 		const component = renderer.create(
-			<SearchListControl className="test-search" list={ list } selected={ [] } onChange={ noop } />
+			<SearchListControl
+				instanceId={ 1 }
+				className="test-search"
+				list={ list }
+				selected={ [] }
+				onChange={ noop }
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
 
 	test( 'should render a search box, a list of options, and 1 selected item', () => {
 		const component = renderer.create(
-			<SearchListControl list={ list } selected={ [ list[ 1 ] ] } onChange={ noop } />
+			<SearchListControl
+				instanceId={ 1 }
+				list={ list }
+				selected={ [ list[ 1 ] ] }
+				onChange={ noop }
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
 
 	test( 'should render a search box, a list of options, and 2 selected item', () => {
 		const component = renderer.create(
-			<SearchListControl list={ list } selected={ [ list[ 1 ], list[ 3 ] ] } onChange={ noop } />
+			<SearchListControl
+				instanceId={ 1 }
+				list={ list }
+				selected={ [ list[ 1 ], list[ 3 ] ] }
+				onChange={ noop }
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
 
 	test( 'should render a search box and no options', () => {
 		const component = renderer.create(
-			<SearchListControl list={ [] } selected={ [] } onChange={ noop } />
+			<SearchListControl
+				instanceId={ 1 }
+				list={ [] }
+				selected={ [] }
+				onChange={ noop }
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
 
 	test( 'should render a search box with a search term, and only matching options', () => {
 		const component = renderer.create(
-			<SearchListControl list={ list } search="berry" selected={ [] } onChange={ noop } debouncedSpeak={ noop } />
+			<SearchListControl
+				instanceId={ 1 }
+				list={ list }
+				search="berry"
+				selected={ [] }
+				onChange={ noop }
+				debouncedSpeak={ noop }
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
 
 	test( 'should render a search box with a search term, and only matching options, regardless of case sensitivity', () => {
 		const component = renderer.create(
-			<SearchListControl list={ list } search="bERry" selected={ [] } onChange={ noop } debouncedSpeak={ noop } />
+			<SearchListControl
+				instanceId={ 1 }
+				list={ list }
+				search="bERry"
+				selected={ [] }
+				onChange={ noop }
+				debouncedSpeak={ noop }
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
 
 	test( 'should render a search box with a search term, and no matching options', () => {
 		const component = renderer.create(
-			<SearchListControl list={ list } search="no matches" selected={ [] } onChange={ noop } debouncedSpeak={ noop } />
+			<SearchListControl
+				instanceId={ 1 }
+				list={ list }
+				search="no matches"
+				selected={ [] }
+				onChange={ noop }
+				debouncedSpeak={ noop }
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
@@ -78,15 +134,40 @@ describe( 'SearchListControl', () => {
 	test( 'should render a search box and list of options, with a custom search input message', () => {
 		const messages = { search: 'Testing search label' };
 		const component = renderer.create(
-			<SearchListControl list={ list } selected={ [] } onChange={ noop } messages={ messages } />
+			<SearchListControl
+				instanceId={ 1 }
+				list={ list }
+				selected={ [] }
+				onChange={ noop }
+				messages={ messages }
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
 
 	test( 'should render a search box and list of options, with a custom render callback for each item', () => {
-		const renderItem = ( { item } ) => <div key={ item.id }>{ item.name }!</div>; // eslint-disable-line
+		const renderItem = ({ item }) => <div key={item.id}>{item.name}!</div>; // eslint-disable-line
 		const component = renderer.create(
-			<SearchListControl list={ list } selected={ [] } onChange={ noop } renderItem={ renderItem } />
+			<SearchListControl
+				instanceId={ 1 }
+				list={ list }
+				selected={ [] }
+				onChange={ noop }
+				renderItem={ renderItem }
+			/>
+		);
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	test( 'should render a search box and list of hierarchical options', () => {
+		const component = renderer.create(
+			<SearchListControl
+				instanceId={ 1 }
+				list={ hierarchicalList }
+				selected={ [] }
+				onChange={ noop }
+				isHierarchical
+			/>
 		);
 		expect( component.toJSON() ).toMatchSnapshot();
 	} );
