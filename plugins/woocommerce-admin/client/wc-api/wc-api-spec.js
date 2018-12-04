0d6cd1a7768cfc27fbb4283a206f7d781fa61a94
Jeff Stieler <jeff.m.stieler@gmail.com>
2018-12-03 20:01:49 -0700

    Add report stats to the `wc-api` spec.

--- plugins/woocommerce-admin/client/wc-api/wc-api-spec.js
+++ plugins/woocommerce-admin/client/wc-api/wc-api-spec.js
@@ -5,18 +5,21 @@
  */
 import notes from './notes';
 import orders from './orders';
+import reportStats from './reports/stats';
 
 function createWcApiSpec() {
 	return {
 		selectors: {
 			...notes.selectors,
 			...orders.selectors,
+			...reportStats.selectors,
 		},
 		operations: {
 			read( resourceNames ) {
 				return [
 					...notes.operations.read( resourceNames ),
 					...orders.operations.read( resourceNames ),
+					...reportStats.operations.read( resourceNames ),
 				];
 			},
 		},
