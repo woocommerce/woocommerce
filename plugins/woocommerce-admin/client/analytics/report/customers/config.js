7f00329889912a19ffb416b9e5c7bcf2947fac48
Paul Sealock <psealock@gmail.com>
2018-12-11 11:18:45 +1300

    update comments

--- plugins/woocommerce-admin/client/analytics/report/customers/config.js
+++ plugins/woocommerce-admin/client/analytics/report/customers/config.js
@@ -33,19 +33,19 @@ export const advancedFilters = {
 				placeholder: __( 'Search customer name', 'wc-admin' ),
 				remove: __( 'Remove customer name filter', 'wc-admin' ),
 				rule: __( 'Select a customer name filter match', 'wc-admin' ),
-				/* translators: A sentence describing a Product filter. See screen shot for context: https://cloudup.com/cSsUY9VeCVJ */
+				/* translators: A sentence describing a Product filter. See screen shot for context: https://cloudup.com/cCsm3GeXJbE */
 				title: __( 'Name {{rule /}} {{filter /}}', 'wc-admin' ),
 				filter: __( 'Select customer name', 'wc-admin' ),
 			},
 			rules: [
 				{
 					value: 'includes',
-					/* translators: Sentence fragment, logical, "Includes" refers to orders including a given product(s). Screenshot for context: https://cloudup.com/cSsUY9VeCVJ */
+					/* translators: Sentence fragment, logical, "Includes" refers to customer names including a given name(s). Screenshot for context: https://cloudup.com/cCsm3GeXJbE */
 					label: _x( 'Includes', 'customer names', 'wc-admin' ),
 				},
 				{
 					value: 'excludes',
-					/* translators: Sentence fragment, logical, "Excludes" refers to orders excluding a given product(s). Screenshot for context: https://cloudup.com/cSsUY9VeCVJ */
+					/* translators: Sentence fragment, logical, "Excludes" refers to customer names excluding a given name(s). Screenshot for context: https://cloudup.com/cCsm3GeXJbE */
 					label: _x( 'Excludes', 'customer names', 'wc-admin' ),
 				},
 			],
