8a796138ef808077332df8cbf6a8e1f3dfef7ac8
Valerie <valerie@automattic.com>
2018-12-13 13:41:34 -0500

    OBW: Add Facebook to recommended step
    The end date for the Facebook recommendation
    is October 8, 2019.
    
    When the option is checked, this will install the extension
    'Facebook for WooCommerce' on the user's site.

--- includes/admin/class-wc-admin-setup-wizard.php
+++ includes/admin/class-wc-admin-setup-wizard.php
@@ -134,6 +134,21 @@ class WC_Admin_Setup_Wizard {
 	}
 
 	/**
+	 * Should we show the Facebook install option?
+	 * True only if the user can install plugins,
+	 * and up until the end date of the recommendation.
+	 *
+	 * @return boolean
+	 */
+	protected function should_show_facebook() {
+		$end_date_facebook_recommendation = new DateTime( '8 October 2019' );
+		$current_user_date = new DateTime( current_time( 'Y-m-d' ) );
+
+		return current_user_can( 'install_plugins' ) &&
+			$end_date_facebook_recommendation >= $current_user_date;
+	}
+
+	/**
 	 * Should we display the 'Recommended' step?
 	 * True if at least one of the recommendations will be displayed.
 	 *
@@ -142,7 +157,8 @@ class WC_Admin_Setup_Wizard {
 	protected function should_show_recommended_step() {
 		return $this->should_show_theme()
 			|| $this->should_show_automated_tax()
-			|| $this->should_show_mailchimp();
+			|| $this->should_show_mailchimp()
+			|| $this->should_show_facebook();
 	}
 
 	/**
@@ -1910,6 +1926,17 @@ class WC_Admin_Setup_Wizard {
 						'plugins'     => array( array( 'name' => __( 'Mailchimp for WooCommerce', 'woocommerce' ), 'slug' => 'mailchimp-for-woocommerce' ) ),
 					) );
 				endif;
+
+				if ( $this->should_show_facebook() ) :
+					$this->display_recommended_item( array(
+						'type'        => 'facebook',
+						'title'       => __( 'Facebook', 'woocommerce' ),
+						'description' => __( 'Enjoy all Facebook products combined in one extension: pixel tracking, catalog sync, messenger chat, shop functionality and Instagram shopping (coming soon)!', 'woocommerce' ),
+						'img_url'     => WC()->plugin_url() . '/assets/images/obw-facebook-icon.svg',
+						'img_alt'     => __( 'Facebook icon', 'woocommerce' ),
+						'plugins'     => array( array( 'name' => __( 'Facebook for WooCommerce', 'woocommerce' ), 'slug' => 'facebook-for-woocommerce' ) ),
+					) );
+				endif;
 			?>
 		</ul>
 			<p class="wc-setup-actions step">
@@ -1930,6 +1957,7 @@ class WC_Admin_Setup_Wizard {
 		$setup_storefront       = isset( $_POST['setup_storefront_theme'] ) && 'yes' === $_POST['setup_storefront_theme'];
 		$setup_automated_tax    = isset( $_POST['setup_automated_taxes'] ) && 'yes' === $_POST['setup_automated_taxes'];
 		$setup_mailchimp        = isset( $_POST['setup_mailchimp'] ) && 'yes' === $_POST['setup_mailchimp'];
+		$setup_facebook         = isset( $_POST['setup_facebook'] ) && 'yes' === $_POST['setup_facebook'];
 
 		update_option( 'woocommerce_calc_taxes', $setup_automated_tax ? 'yes' : 'no' );
 		update_option( 'woocommerce_setup_automated_taxes', $setup_automated_tax );
@@ -1952,6 +1980,16 @@ class WC_Admin_Setup_Wizard {
 					'name'      => __( 'MailChimp for WooCommerce', 'woocommerce' ),
 					'repo-slug' => 'mailchimp-for-woocommerce',
 					'file'      => 'mailchimp-woocommerce.php',
+				)
+			);
+		}
+
+		if ( $setup_facebook ) {
+			$this->install_plugin(
+				'facebook-for-woocommerce',
+				array(
+					'name'      => __( 'Facebook for WooCommerce', 'woocommerce' ),
+					'repo-slug' => 'facebook-for-woocommerce',
 				)
 			);
 		}
