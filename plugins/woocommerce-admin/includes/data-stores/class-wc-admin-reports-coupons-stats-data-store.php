85e6e8df8decceda9c19e1a4a4fb0ed40679a55e
Peter Fabian <peter.fabian.github@gmail.com>
2018-12-14 14:13:57 +0100

    Added correct cache handling.

--- plugins/woocommerce-admin/includes/data-stores/class-wc-admin-reports-coupons-stats-data-store.php
+++ plugins/woocommerce-admin/includes/data-stores/class-wc-admin-reports-coupons-stats-data-store.php
@@ -213,4 +213,13 @@ class WC_Admin_Reports_Coupons_Stats_Data_Store extends WC_Admin_Reports_Coupons
 		return $data;
 	}
 
+	/**
+	 * Returns string to be used as cache key for the data.
+	 *
+	 * @param array $params Query parameters.
+	 * @return string
+	 */
+	protected function get_cache_key( $params ) {
+		return 'woocommerce_' . self::TABLE_NAME . '_stats_' . md5( wp_json_encode( $params ) );
+	}
 }
