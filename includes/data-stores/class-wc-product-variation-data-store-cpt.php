1a02e9f64a45859df003ae8738abb477c82ccfb6
Ron Rennick <ron@ronandandrea.com>
2019-01-02 14:26:08 -0400

    update variation guid with the id after creation

--- includes/data-stores/class-wc-product-variation-data-store-cpt.php
+++ includes/data-stores/class-wc-product-variation-data-store-cpt.php
@@ -145,6 +145,7 @@ class WC_Product_Variation_Data_Store_CPT extends WC_Product_Data_Store_CPT impl
 			$product->apply_changes();
 
 			$this->update_version_and_type( $product );
+			$this->update_guid( $product );
 
 			$this->clear_caches( $product );
 
@@ -470,5 +471,27 @@ class WC_Product_Variation_Data_Store_CPT extends WC_Product_Data_Store_CPT impl
 		}
 
 		parent::update_post_meta( $product, $force );
+	}
+
+	/**
+	 * Update product variation guid.
+	 *
+	 * @param WC_Product_Variation $product Product variation object.
+	 *
+	 * @since 3.6.0
+	 */
+	protected function update_guid( $product ) {
+		global $wpdb;
+
+		$guid = home_url(
+			add_query_arg(
+				array(
+					'post_type' => 'product_variation',
+					'p'         => $product->get_id(),
+				),
+				''
+			)
+		);
+		$wpdb->update( $wpdb->posts, array( 'guid' => $guid ), array( 'ID' => $product->get_id() ) );
 	}
 }
