8ad3491b99499d7f2abfcfea949d2c98aab9b5ca
Mike Jolley <mike.jolley@me.com>
2019-01-07 11:22:29 +0000

    Add option to control if remove personal data bulk action is allowed

--- includes/admin/list-tables/class-wc-admin-list-table-orders.php
+++ includes/admin/list-tables/class-wc-admin-list-table-orders.php
@@ -139,7 +139,7 @@ class WC_Admin_List_Table_Orders extends WC_Admin_List_Table {
 		$actions['mark_on-hold']    = __( 'Change status to on-hold', 'woocommerce' );
 		$actions['mark_completed']  = __( 'Change status to completed', 'woocommerce' );
 
-		if ( wc_string_to_bool( get_option( 'woocommerce_erasure_request_removes_order_data', 'no' ) ) ) {
+		if ( wc_string_to_bool( get_option( 'woocommerce_allow_bulk_remove_personal_data', 'no' ) ) ) {
 			$actions['remove_personal_data'] = __( 'Remove personal data', 'woocommerce' );
 		}
 
