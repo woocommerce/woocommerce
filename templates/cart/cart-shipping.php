b4db23a4fc17954853ec1b4a2c4674758b393f77
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-01-09 17:39:02 -0200

    Bump template version after change in #22078

--- templates/cart/cart-shipping.php
+++ templates/cart/cart-shipping.php
@@ -14,7 +14,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.5.0
+ * @version 3.6.0
  */
 
 defined( 'ABSPATH' ) || exit;
