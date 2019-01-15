2fc9de0bb074483472ce03fe90f13d1bc85340c6
Peter Fabian <peter.fabian.github@gmail.com>
2019-01-15 11:36:56 +0100

    Preparation for merge of a moved file.

--- plugins/woocommerce-admin/includes/data-stores/class-wc-admin-reports-orders-stats-data-store.php
+++ plugins/woocommerce-admin/includes/data-stores/class-wc-admin-reports-orders-stats-data-store.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * WC_Admin_Reports_Orders_Data_Store class file.
+ * WC_Admin_Reports_Orders_Stats_Data_Store class file.
  *
  * @package WooCommerce Admin/Classes
  */
@@ -12,11 +12,11 @@ if ( ! class_exists( 'WC_Admin_Order_Stats_Background_Process', false ) ) {
 }
 
 /**
- * WC_Admin_Reports_Orders_Data_Store.
+ * WC_Admin_Reports_Orders_Stats_Data_Store.
  *
  * @version  3.5.0
  */
-class WC_Admin_Reports_Orders_Data_Store extends WC_Admin_Reports_Data_Store implements WC_Admin_Reports_Data_Store_Interface {
+class WC_Admin_Reports_Orders_Stats_Data_Store extends WC_Admin_Reports_Data_Store implements WC_Admin_Reports_Data_Store_Interface {
 
 	/**
 	 * Table used to get the data.
