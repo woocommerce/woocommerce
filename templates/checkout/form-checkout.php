8c568307223edd61009ff8282c26de76216c4cc3
Sandesh <sandesh2643@gmail.com>
2019-01-16 18:50:38 +0530

    Remove unnecessary action.

--- templates/checkout/form-checkout.php
+++ templates/checkout/form-checkout.php
@@ -53,9 +53,6 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 	
 	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
 	
-	<?php do_action( 'woocommerce_checkout_after_order_review_heading' ); ?>
-	
-	
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
 	<div id="order_review" class="woocommerce-checkout-review-order">
