78e1be139f1f7de230761451a9fb23eb12d12e97
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-01-16 17:32:12 -0700

    Refactor numberFormat() to use the store settings for precision, decimal separator, and thousand separator.

--- plugins/woocommerce-admin/client/lib/number/index.js
+++ plugins/woocommerce-admin/client/lib/number/index.js
@@ -1,20 +1,30 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+const number_format = require( 'locutus/php/strings/number_format' );
+
 /**
  * Formats a number using site's current locale
  *
- * @format
- * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NumberFormat
- * @param {Number|String}  number    number to format
- * @returns {?String}                  A formatted string.
+ * @see https://locutus.io/php/strings/number_format/
+ * @param {Number|String} number number to format
+ * @returns {?String} A formatted string.
  */
 
 export function numberFormat( number ) {
-	const locale = wcSettings.siteLocale || 'en-US'; // Default so we don't break.
-
 	if ( 'number' !== typeof number ) {
 		number = parseFloat( number );
 	}
+
 	if ( isNaN( number ) ) {
 		return '';
 	}
-	return new Intl.NumberFormat( locale ).format( number );
+
+	const precision = get( wcSettings, [ 'currency', 'precision' ], 2 );
+	const decimalSeparator = get( wcSettings, [ 'currency', 'decimal_separator' ], '.' );
+	const thousandSeparator = get( wcSettings, [ 'currency', 'thousand_separator' ], ',' );
+
+	return number_format( number, precision, decimalSeparator, thousandSeparator );
 }
--- plugins/woocommerce-admin/client/lib/number/test/index.js
+++ plugins/woocommerce-admin/client/lib/number/test/index.js
@@ -5,8 +5,8 @@
 import { numberFormat } from '../index';
 
 describe( 'numberFormat', () => {
-	it( 'should default to en-US formatting', () => {
-		expect( numberFormat( 1000 ) ).toBe( '1,000' );
+	it( 'should default to precision=2 decimal=. thousands=,', () => {
+		expect( numberFormat( 1000 ) ).toBe( '1,000.00' );
 	} );
 
 	it( 'should return an empty string if no argument is passed', () => {
@@ -14,6 +14,15 @@ describe( 'numberFormat', () => {
 	} );
 
 	it( 'should accept a string', () => {
-		expect( numberFormat( '10000' ) ).toBe( '10,000' );
+		expect( numberFormat( '10000' ) ).toBe( '10,000.00' );
+	} );
+
+	it( 'uses store currency settings, not locale', () => {
+		global.wcSettings.siteLocale = 'en-US';
+		global.wcSettings.currency.precision = 3;
+		global.wcSettings.currency.decimal_separator = ',';
+		global.wcSettings.currency.thousand_separator = '.';
+
+		expect( numberFormat( '12345.6789' ) ).toBe( '12.345,679' );
 	} );
 } );
