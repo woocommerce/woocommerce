987a24afdc68022496190367ff35b1b465915b18
Mike Jolley <mike.jolley@me.com>
2019-01-23 15:16:40 +0000

    Timeout param

--- assets/js/frontend/cart-fragments.js
+++ assets/js/frontend/cart-fragments.js
@@ -38,7 +38,7 @@ jQuery( function( $ ) {
 	var $fragment_refresh = {
 		url: wc_cart_fragments_params.wc_ajax_url.toString().replace( '%%endpoint%%', 'get_refreshed_fragments' ),
 		type: 'POST',
-		timeout: 5000,
+		timeout: wc_cart_fragments_params.request_timeout,
 		success: function( data ) {
 			if ( data && data.fragments ) {
 
