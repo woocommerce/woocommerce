0a73004f453d774c365ef700a7b4385e4b30bd9b
Gerhard <potgieterg@gmail.com>
2019-01-29 12:19:10 +0200

    Bump template version

--- templates/content-widget-product.php
+++ templates/content-widget-product.php
@@ -11,7 +11,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.5.2
+ * @version 3.5.5
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
