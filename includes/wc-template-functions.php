e3f2cad544ecc3c9043c09be5c51717c933c82e9
Mike Jolley <mike.jolley@me.com>
2019-02-01 16:10:14 +0000

    Remove escape

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -1066,7 +1066,7 @@ if ( ! function_exists( 'woocommerce_template_loop_product_title' ) ) {
 	 * Show the product title in the product loop. By default this is an H2.
 	 */
 	function woocommerce_template_loop_product_title() {
-		echo '<h2 class="woocommerce-loop-product__title">' . esc_html( get_the_title() ) . '</h2>';
+		echo '<h2 class="woocommerce-loop-product__title">' . get_the_title() . '</h2>'; // phpcs:ignore
 	}
 }
 if ( ! function_exists( 'woocommerce_template_loop_category_title' ) ) {
