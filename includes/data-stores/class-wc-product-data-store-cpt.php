03a60fddfcfc307da873b342625f95cf7086d0bd
Frédéric Demarle <frederic.demarle@yahoo.fr>
2019-02-06 10:24:41 +0100

    Ignore WordPress.DB.PreparedSQL.NotPrepared

--- includes/data-stores/class-wc-product-data-store-cpt.php
+++ includes/data-stores/class-wc-product-data-store-cpt.php
@@ -1259,7 +1259,7 @@ class WC_Product_Data_Store_CPT extends WC_Data_Store_WP implements WC_Object_Da
 
 		$sql = apply_filters( 'woocommerce_update_product_stock_query', $sql, $product_id_with_stock, $stock_quantity, $operation );
 
-		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.PreparedSQL.NotPrepared
 		$wpdb->query( $sql );
 
 		wp_cache_delete( $product_id_with_stock, 'post_meta' );
