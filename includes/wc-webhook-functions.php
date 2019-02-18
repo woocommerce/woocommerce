30db8a8d9cb61182d103f3f338cca056c6400c06
Mike Jolley <mike.jolley@me.com>
2019-02-18 13:07:57 +0000

    version comment

--- includes/wc-webhook-functions.php
+++ includes/wc-webhook-functions.php
@@ -130,7 +130,7 @@ function wc_get_webhook_statuses() {
  * @since  3.3.0
  * @throws Exception If webhook cannot be read/found and $data parameter of WC_Webhook class constructor is set.
  * @param  string   $status Optional - status to filter results by. Must be a key in return value of @see wc_get_webhook_statuses(). @since 3.5.0.
- * @param  null|int $limit Limit number of webhooks loaded. @since 3.5.0.
+ * @param  null|int $limit Limit number of webhooks loaded. @since 3.6.0.
  * @return bool
  */
 function wc_load_webhooks( $status = '', $limit = null ) {
