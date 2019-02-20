44604bcd02a263179e99d5c5d18bb4df9f6a2dd8
Joshua T Flowers <joshuatf@gmail.com>
2019-02-20 09:39:42 +0800

    Reduce net order total by refunded amount less tax and shipping refunds (https://github.com/woocommerce/woocommerce-admin/pull/1600)
    * Reduce net order total by refunded amount less tax and shipping refunds
    
    * Fix net_totals in tests with refunds

--- plugins/woocommerce-admin/includes/data-stores/class-wc-admin-reports-orders-stats-data-store.php
+++ plugins/woocommerce-admin/includes/data-stores/class-wc-admin-reports-orders-stats-data-store.php
@@ -401,7 +401,7 @@ class WC_Admin_Reports_Orders_Stats_Data_Store extends WC_Admin_Reports_Data_Sto
 			'refund_total'       => $order->get_total_refunded(),
 			'tax_total'          => $order->get_total_tax(),
 			'shipping_total'     => $order->get_shipping_total(),
-			'net_total'          => (float) $order->get_total() - (float) $order->get_total_tax() - (float) $order->get_shipping_total(),
+			'net_total'          => self::get_net_total( $order ),
 			'returning_customer' => self::is_returning_customer( $order ),
 			'status'             => self::normalize_order_status( $order->get_status() ),
 		);
@@ -505,6 +505,23 @@ class WC_Admin_Reports_Orders_Stats_Data_Store extends WC_Admin_Reports_Data_Sto
 		}
 
 		return $num_items;
+	}
+
+	/**
+	 * Get the net amount from an order without shipping, tax, or refunds.
+	 *
+	 * @param array $order WC_Order object.
+	 * @return float
+	 */
+	protected static function get_net_total( $order ) {
+		$net_total = $order->get_total() - $order->get_total_tax() - $order->get_shipping_total();
+
+		$refunds = $order->get_refunds();
+		foreach ( $refunds as $refund ) {
+			$net_total += $refund->get_total() - $refund->get_total_tax() - $refund->get_shipping_total();
+		}
+
+		return $net_total > 0 ? (float) $net_total : 0;
 	}
 
 	/**
