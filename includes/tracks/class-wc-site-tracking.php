ba63200e0d0b729b1e38c60b6fa5c582c0057166
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-02-20 12:41:06 -0700

    Add Tracks events for product import start and finish.

--- includes/tracks/class-wc-site-tracking.php
+++ includes/tracks/class-wc-site-tracking.php
@@ -31,6 +31,63 @@ class WC_Site_Tracking {
 	}
 
 	/**
+	 * Route product importer action to the right callback.
+	 *
+	 * @return void
+	 */
+	public static function tracks_product_importer() {
+		if ( ! isset( $_REQUEST['step'] ) ) {
+			return;
+		}
+
+		if ( 'import' === $_REQUEST['step'] ) {
+			return self::tracks_product_importer_start();
+		}
+
+		if ( 'done' === $_REQUEST['step'] ) {
+			return self::tracks_product_importer_complete();
+		}
+	}
+
+	/**
+	 * Send a Tracks event when the product importer is started.
+	 *
+	 * @return void
+	 */
+	public static function tracks_product_importer_start() {
+		if ( ! isset( $_REQUEST['file'] ) || ! isset( $_REQUEST['_wpnonce'] ) ) {
+			return;
+		}
+
+		$properties = array(
+			'update_existing' => isset( $_REQUEST['update_existing'] ) ? (bool) $_REQUEST['update_existing'] : false,
+			'delimiter'       => empty( $_REQUEST['delimiter'] ) ? ',' : wc_clean( wp_unslash( $_REQUEST['delimiter'] ) ),
+		);
+
+		WC_Tracks::record_event( 'product_import_start', $properties );
+	}
+
+	/**
+	 * Send a Tracks event when the product importer has finished.
+	 *
+	 * @return void
+	 */
+	public static function tracks_product_importer_complete() {
+		if ( ! isset( $_REQUEST['nonce'] ) ) {
+			return;
+		}
+
+		$properties = array(
+			'imported' => isset( $_GET['products-imported'] ) ? absint( $_GET['products-imported'] ) : 0,
+			'updated'  => isset( $_GET['products-updated'] ) ? absint( $_GET['products-updated'] ) : 0,
+			'failed'   => isset( $_GET['products-failed'] ) ? absint( $_GET['products-failed'] ) : 0,
+			'skipped'  => isset( $_GET['products-skipped'] ) ? absint( $_GET['products-skipped'] ) : 0,
+		);
+
+		WC_Tracks::record_event( 'product_import_complete', $properties );
+	}
+
+	/**
 	 * Check if tracking is enabled.
 	 *
 	 * @return bool
@@ -61,5 +118,6 @@ class WC_Site_Tracking {
 		}
 
 		add_action( 'edit_post', array( 'WC_Site_Tracking', 'tracks_product_updated' ), 10, 2 );
+		add_action( 'product_page_product_importer', array( __CLASS__, 'tracks_product_importer' ) );
 	}
 }
