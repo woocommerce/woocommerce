cec907dfdcf295b8bf52a7503fa7e27e30a90e99
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-02-20 14:43:33 -0700

    Track when product categories are created.

--- includes/tracks/events/class-wc-products-tracking.php
+++ includes/tracks/events/class-wc-products-tracking.php
@@ -17,6 +17,7 @@ class WC_Products_Tracking {
 	public static function init() {
 		add_action( 'edit_post', array( __CLASS__, 'track_product_updated' ), 10, 2 );
 		add_action( 'transition_post_status', array( __CLASS__, 'track_product_published' ), 10, 3 );
+		add_action( 'created_product_cat', array( __CLASS__, 'track_product_category_created' ) );
 	}
 
 	/**
@@ -58,5 +59,32 @@ class WC_Products_Tracking {
 		);
 
 		WC_Tracks::record_event( 'product_add_publish', $properties );
+	}
+
+	/**
+	 * Send a Tracks event when a product category is created.
+	 *
+	 * @param int $category_id Category ID.
+	 */
+	public static function track_product_category_created( $category_id ) {
+		// Only track category creation from the edit product screen or the
+		// category management screen (which both occur via AJAX).
+		if (
+			! defined( 'DOING_AJAX' ) ||
+			empty( $_POST['action'] ) ||
+			(
+				'add-tag' !== $_POST['action'] &&      // Product Categories screen.
+				'add-product_cat' !== $_POST['action'] // Edit Product screen.
+			)
+		) {
+			return;
+		}
+
+		$properties = array(
+			'category_id' => $category_id,
+			'source'      => ( 'add-tag' === $_POST['action'] ) ? 'categories' : 'product',
+		);
+
+		WC_Tracks::record_event( 'product_category_add', $properties );
 	}
 }
