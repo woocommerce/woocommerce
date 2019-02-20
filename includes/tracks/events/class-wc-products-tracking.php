8ea66a12050b5626371f8c5df8b52fe1bc1530f7
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-02-20 14:14:57 -0700

    Track when a product is published.

--- includes/tracks/events/class-wc-products-tracking.php
+++ includes/tracks/events/class-wc-products-tracking.php
@@ -16,13 +16,14 @@ class WC_Products_Tracking {
 	 */
 	public static function init() {
 		add_action( 'edit_post', array( __CLASS__, 'track_product_updated' ), 10, 2 );
+		add_action( 'transition_post_status', array( __CLASS__, 'track_product_published' ), 10, 3 );
 	}
 
 	/**
 	 * Send a Tracks event when a product is updated.
 	 *
 	 * @param int   $product_id Product id.
-	 * @param array $post WordPress post.
+	 * @param object $post WordPress post.
 	 */
 	public static function track_product_updated( $product_id, $post ) {
 		if ( 'product' !== $post->post_type ) {
@@ -34,5 +35,28 @@ class WC_Products_Tracking {
 		);
 
 		WC_Tracks::record_event( 'update_product', $properties );
+	}
+
+	/**
+	 * Send a Tracks event when a product is published.
+	 *
+	 * @param string $new_status New post_status.
+	 * @param string $old_status Previous post_status.
+	 * @param object $post WordPress post.
+	 */
+	public static function track_product_published( $new_status, $old_status, $post ) {
+		if (
+			'product' !== $post->post_type ||
+			'publish' !== $new_status ||
+			'publish' === $old_status
+		) {
+			return;
+		}
+
+		$properties = array(
+			'product_id' => $post->ID,
+		);
+
+		WC_Tracks::record_event( 'product_add_publish', $properties );
 	}
 }
