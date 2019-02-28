207a5ef4b3c02ce6aee788876ecfed3c1ee4eb6b
Gerhard <potgieterg@gmail.com>
2019-02-28 15:30:17 +0200

    Add user_order_remaining_expires index to woocommerce_downloadable_product_permissions create table statement.

--- includes/class-wc-install.php
+++ includes/class-wc-install.php
@@ -692,7 +692,8 @@ CREATE TABLE {$wpdb->prefix}woocommerce_downloadable_product_permissions (
   PRIMARY KEY  (permission_id),
   KEY download_order_key_product (product_id,order_id,order_key(16),download_id),
   KEY download_order_product (download_id,order_id,product_id),
-  KEY order_id (order_id)
+  KEY order_id (order_id),
+  KEY user_order_remaining_expires (user_id,order_id,downloads_remaining,access_expires)
 ) $collate;
 CREATE TABLE {$wpdb->prefix}woocommerce_order_items (
   order_item_id BIGINT UNSIGNED NOT NULL auto_increment,
