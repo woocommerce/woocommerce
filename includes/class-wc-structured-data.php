52533d9da6bc48b0b51796d1505f7816b9445b26
Mike Jolley <mike.jolley@me.com>
2019-03-04 12:47:31 +0000

    Add URL field

--- includes/class-wc-structured-data.php
+++ includes/class-wc-structured-data.php
@@ -200,6 +200,7 @@ class WC_Structured_Data {
 			'@type'       => 'Product',
 			'@id'         => $permalink . '#product', // Append '#product' to differentiate between this @id and the @id generated for the Breadcrumblist.
 			'name'        => $product->get_name(),
+			'url'         => $permalink,
 			'image'       => wp_get_attachment_url( $product->get_image_id() ),
 			'description' => wp_strip_all_tags( do_shortcode( $product->get_short_description() ? $product->get_short_description() : $product->get_description() ) ),
 		);
