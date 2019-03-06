3493481f01e92e4415eb7bb9e27a1ebaa36da3a7
Tiago Noronha <hello@tiagonoronha.com>
2019-03-06 21:36:54 +0000

    Add placeholder for StoreAlerts

---
+++ plugins/woocommerce-admin/client/layout/store-alerts/placeholder.js
@@ -0,0 +1,37 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { Component } from '@wordpress/element';
+
+class StoreAlertsPlaceholder extends Component {
+	render() {
+		const { hasMultipleAlerts } = this.props;
+
+		return (
+			<div className="woocommerce-card woocommerce-store-alerts is-loading" aria-hidden>
+				<div className="woocommerce-card__header">
+					<div className="woocommerce-card__title woocommerce-card__header-item">
+						<span className="is-placeholder" />
+					</div>
+					{ hasMultipleAlerts && (
+						<div className="woocommerce-card__action woocommerce-card__header-item">
+							<span className="is-placeholder" />
+						</div>
+					) }
+				</div>
+				<div className="woocommerce-card__body">
+					<div className="woocommerce-store-alerts__message">
+						<span className="is-placeholder" />
+						<span className="is-placeholder" />
+					</div>
+					<div className="woocommerce-store-alerts__actions">
+						<span className="is-placeholder" />
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default StoreAlertsPlaceholder;
