99455cfe341552c7dc8a82d6f113ebf29b0122f2
Mike Jolley <mike.jolley@me.com>
2019-03-06 12:00:54 +0000

    update text

--- includes/admin/views/html-notice-update.php
+++ includes/admin/views/html-notice-update.php
@@ -18,14 +18,14 @@ $update_url = wp_nonce_url(
 ?>
 <div id="message" class="updated woocommerce-message wc-connect">
 	<p>
-		<strong><?php esc_html_e( 'Database update required', 'woocommerce' ); ?></strong>
+		<strong><?php esc_html_e( 'WooCommerce database update required', 'woocommerce' ); ?></strong>
 	</p>
 	<p>
 		<?php
 			esc_html_e( 'WooCommerce has been updated! To keep things running smoothly, we have to update your database to the newest version.', 'woocommerce' );
 
 			/* translators: 1: Link to docs 2: Close link. */
-			printf( ' ' . esc_html__( 'The database update process may take a little while, so please be patient. Advanced users can alternatively update via %1$sWP CLI%2$s.', 'woocommerce' ), '<a href="https://github.com/woocommerce/woocommerce/wiki/Upgrading-the-database-using-WP-CLI">', '</a>' );
+			printf( ' ' . esc_html__( 'The database update process runs in the background and may take a little while, so please be patient. Advanced users can alternatively update via %1$sWP CLI%2$s.', 'woocommerce' ), '<a href="https://github.com/woocommerce/woocommerce/wiki/Upgrading-the-database-using-WP-CLI">', '</a>' );
 		?>
 	</p>
 	<p class="submit">
