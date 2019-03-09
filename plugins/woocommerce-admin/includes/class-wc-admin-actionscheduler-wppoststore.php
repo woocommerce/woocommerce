04db0e2b8d86a5677f663d5a03a392e005bff69e
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-03-08 18:41:59 -0700

    Add a custom “cancel all” method to our scheduled action data store.
    Performs direct database manipulation for performance (with large datasets).

--- plugins/woocommerce-admin/includes/class-wc-admin-actionscheduler-wppoststore.php
+++ plugins/woocommerce-admin/includes/class-wc-admin-actionscheduler-wppoststore.php
@@ -33,4 +33,35 @@ class WC_Admin_ActionScheduler_WPPostStore extends ActionScheduler_wpPostStore {
 
 		return $postdata;
 	}
+
+	/**
+	 * Forcefully delete all pending WC Admin scheduled actions.
+	 *
+	 * Directly deletes items from the database for performance.
+	 */
+	public function clear_pending_wcadmin_actions() {
+		global $wpdb;
+
+		// Remove all scheduled action posts and their metadata.
+		$delete_pending_sql =
+			"DELETE p.*, pm.* FROM {$wpdb->posts} p
+			JOIN {$wpdb->postmeta} pm ON p.ID = pm.post_id
+			WHERE post_type = 'scheduled-action'
+			AND post_status = 'pending'
+			AND post_title LIKE 'wc-admin_%'";
+
+		// phpcs:ignore WordPress.DB.PreparedSQL
+		$wpdb->query( $delete_pending_sql );
+
+		// Delete all taxonomy data related to the WC Admin scheduled action group.
+		$group_term = get_term_by( 'slug', WC_Admin_Reports_Sync::QUEUE_GROUP, parent::GROUP_TAXONOMY );
+
+		if ( $group_term ) {
+			$wpdb->delete( $wpdb->term_relationships, array( 'term_taxonomy_id' => $group_term->term_taxonomy_id ), array( '%d' ) );
+			$wpdb->delete( $wpdb->term_taxonomy, array( 'term_id' => $group_term->term_id ), array( '%d' ) );
+			$wpdb->delete( $wpdb->terms, array( 'term_id' => $group_term->term_id ), array( '%d' ) );
+
+			clean_taxonomy_cache( parent::GROUP_TAXONOMY );
+		}
+	}
 }
