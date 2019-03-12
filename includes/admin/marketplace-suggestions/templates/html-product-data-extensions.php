a751e6a054cf4e5c34afd9d9678aa9c1112c1936
haszari <rua.haszard@automattic.com>
2019-03-13 09:39:57 +1300

    content & icon for no-suggestions state in product edit metabox

--- includes/admin/marketplace-suggestions/templates/html-product-data-extensions.php
+++ includes/admin/marketplace-suggestions/templates/html-product-data-extensions.php
@@ -17,4 +17,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 		WC_Marketplace_Suggestions::render_suggestions_container( 'product-edit-meta-tab-body' );
 		WC_Marketplace_Suggestions::render_suggestions_container( 'product-edit-meta-tab-footer' );
 	?>
+	<div class="marketplace-suggestions-metabox-nosuggestions-placeholder hidden">
+		<img src="https://woocommerce.com/wp-content/plugins/wccom-plugins/marketplace-suggestions/icons/get_more_options.svg" class="marketplace-suggestion-icon">
+		<div class="marketplace-suggestion-placeholder-content">
+			<h4>Enhance your products</h4>
+			<p>Extensions can add new functionality to your product pages that make your store stand out</p>
+		</div>
+		<a href="https://woocommerce.com/product-category/woocommerce-extensions/?utm_source=editproduct&amp;utm_campaign=marketplacesuggestions&amp;utm_medium=product" target="blank" class="button">Browse the Marketplace</a>
+	</div>
 </div>
