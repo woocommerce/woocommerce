efc46344fc1552b9570d1ba48a7540f7200f3e90
Mike Jolley <mike.jolley@me.com>
2019-03-13 14:35:51 +0000

    Bump template version

--- templates/emails/admin-cancelled-order.php
+++ templates/emails/admin-cancelled-order.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates/Emails
- * @version 3.5.0
+ * @version 3.6.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
