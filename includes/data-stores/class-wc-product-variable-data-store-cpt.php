0002463c61cd837e76fb76f6e690948e19eee9fe
Mike Jolley <mike.jolley@me.com>
2019-03-13 13:48:38 +0000

    child_has_stock_status use lookup

--- includes/data-stores/class-wc-product-variable-data-store-cpt.php
+++ includes/data-stores/class-wc-product-variable-data-store-cpt.php
@@ -485,12 +485,19 @@ class WC_Product_Variable_Data_Store_CPT extends WC_Product_Data_Store_CPT imple
 			$format               = array_fill( 0, count( $children ), '%d' );
 			$query_in             = '(' . implode( ',', $format ) . ')';
 			$query_args           = array( 'stock_status' => $status ) + $children;
+			// phpcs:disable WordPress.DB.PreparedSQL.NotPrepared
 			$children_with_status = $wpdb->get_var(
-				$wpdb->prepare( // wpcs: PreparedSQLPlaceholders replacement count ok.
-					"SELECT COUNT( post_id ) FROM $wpdb->postmeta WHERE meta_key = '_stock_status' AND meta_value = %s AND post_id IN {$query_in}", // @codingStandardsIgnoreLine.
+				$wpdb->prepare(
+					"
+					SELECT COUNT( product_id )
+					FROM {$wpdb->wc_product_meta_lookup}
+					WHERE stock_status = %s
+					AND product_id IN {$query_in}
+					",
 					$query_args
 				)
 			);
+			// phpcs:enable WordPress.DB.PreparedSQL.NotPrepared
 		} else {
 			$children_with_status = 0;
 		}
