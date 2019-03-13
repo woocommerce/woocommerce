ab89222cfa9c2988a11133f8090cbc8f0752f121
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-03-13 09:10:53 -0300

    Fix typo
    Typo added in https://github.com/woocommerce/woocommerce/commit/be05eea5da5326661383b18905e6d6ce9ea86bbd#diff-133db5662c51f5686d87611121a05a3bR45

--- includes/class-wc-ajax.php
+++ includes/class-wc-ajax.php
@@ -42,7 +42,7 @@ class WC_AJAX {
 			wc_maybe_define_constant( 'DOING_AJAX', true );
 			wc_maybe_define_constant( 'WC_DOING_AJAX', true );
 			if ( ! WP_DEBUG || ( WP_DEBUG && ! WP_DEBUG_DISPLAY ) ) {
-				@ini_set( 'display_errors', 0 ); // Turn off display_errors during AJAX events to prevent malformed JSON. phpcs
+				@ini_set( 'display_errors', 0 ); // Turn off display_errors during AJAX events to prevent malformed JSON.
 			}
 			$GLOBALS['wpdb']->hide_errors();
 		}
