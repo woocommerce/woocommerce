b066922c406665bfba153bc2e23e020cd2062683
Kelly Dwan <ryelle@users.noreply.github.com>
2019-03-15 12:01:21 -0400

    Update the rating request text in the footer to be friendlier for screen reader users

--- includes/admin/class-wc-admin.php
+++ includes/admin/class-wc-admin.php
@@ -258,7 +258,7 @@ class WC_Admin {
 					/* translators: 1: WooCommerce 2:: five stars */
 					__( 'If you like %1$s please leave us a %2$s rating. A huge thanks in advance!', 'woocommerce' ),
 					sprintf( '<strong>%s</strong>', esc_html__( 'WooCommerce', 'woocommerce' ) ),
-					'<a href="https://wordpress.org/support/plugin/woocommerce/reviews?rate=5#new-post" target="_blank" class="wc-rating-link" data-rated="' . esc_attr__( 'Thanks :)', 'woocommerce' ) . '">&#9733;&#9733;&#9733;&#9733;&#9733;</a>'
+					'<a href="https://wordpress.org/support/plugin/woocommerce/reviews?rate=5#new-post" target="_blank" class="wc-rating-link" aria-label="' . esc_attr__( 'five star', 'woocommerce' ) . '" data-rated="' . esc_attr__( 'Thanks :)', 'woocommerce' ) . '">&#9733;&#9733;&#9733;&#9733;&#9733;</a>'
 				);
 				wc_enqueue_js(
 					"jQuery( 'a.wc-rating-link' ).click( function() {
