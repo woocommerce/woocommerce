0a1eb076ea8922947d835c7251e40dd047934ebb
Gerhard <potgieterg@gmail.com>
2019-03-19 15:23:36 +0200

    Add PHPUnit call fallback for 5.2 specifically

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -6,6 +6,8 @@ fi
 
 if [[ ${RUN_CODE_COVERAGE} == 1 ]]; then
 	phpdbg -qrr $HOME/.composer/vendor/phpunit/phpunit/phpunit -d memory_limit=-1 -c phpunit.xml --coverage-clover=coverage.clover --exclude-group=timeout
+else if [[ ${TRAVIS_PHP_VERSION:0:3} != "5.2" ]]; then
+	$HOME/.composer/vendor/phpunit/phpunit/phpunit -c phpunit.xml
 else
 	phpunit -c phpunit.xml
 fi
