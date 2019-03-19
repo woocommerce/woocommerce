8f9cd5c7e24386dd4776129c9ef4d24aa8347396
Gerhard <potgieterg@gmail.com>
2019-03-19 12:22:08 +0200

    Rework travis config for code coverage

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -6,10 +6,12 @@ fi
 
 if [[ ${RUN_CODE_COVERAGE} == 1 ]]; then
 	if [[ -n $COMPOSER_BIN_DIR && -x $COMPOSER_BIN_DIR/phpunit ]]; then
-    	phpdbg -qrr -d memory_limit=-1 $COMPOSER_BIN_DIR/phpunit -c phpunit.xml --coverage-clover=coverage.clover --exclude-group=timeout
-    else
+		$COMPOSER_BIN_DIR/phpunit --version
+		phpdbg -qrr -d memory_limit=-1 $COMPOSER_BIN_DIR/phpunit -c phpunit.xml --coverage-clover=coverage.clover --exclude-group=timeout
+	else
+		phpunit --version
 		phpdbg -qrr -d memory_limit=-1 phpunit -c phpunit.xml --coverage-clover=coverage.clover --exclude-group=timeout
-    fi
+	fi
 else
 	phpunit -c phpunit.xml
 fi
--- tests/bin/travis.sh
+++ tests/bin/travis.sh
@@ -5,7 +5,7 @@ if [ $1 == 'before' ]; then
 
 	composer global require "phpunit/phpunit=6.*"
 
-	if [[ ${RUN_PHPCS} == 1 ]]; then
+	if [[ ${RUN_PHPCS} == 1 ]] || [[ ${RUN_CODE_COVERAGE} == 1 ]]; then
 		composer install
 	fi
 
@@ -22,9 +22,9 @@ if [ $1 == 'after' ]; then
 
 	if [[ ${RUN_E2E} == 1 && $(ls -A $TRAVIS_BUILD_DIR/screenshots) ]]; then
 		if [[ -z "${ARTIFACTS_KEY}" ]]; then
-  			echo "Screenshots were not uploaded. Please run the e2e tests locally to see failures."
+			echo "Screenshots were not uploaded. Please run the e2e tests locally to see failures."
 		else
-  			curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
+			curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
 			artifacts upload
 		fi
 	fi
