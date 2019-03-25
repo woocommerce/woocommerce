0a44c124bed71197e4a2f54ab2fdb9541911b0ff
Mike Jolley <mike.jolley@me.com>
2019-03-25 10:53:57 +0000

    Change completed email wording to "We have finished processing your order"

--- templates/emails/customer-completed-order.php
+++ templates/emails/customer-completed-order.php
@@ -27,7 +27,7 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 <?php /* translators: %s: Customer first name */ ?>
 <p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $order->get_billing_first_name() ) ); ?></p>
 <?php /* translators: %s: Site title */ ?>
-<p><?php printf( esc_html__( 'Your %s order has been marked complete on our side.', 'woocommerce' ), esc_html( wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES ) ) ); ?></p>
+<p><?php esc_html_e( 'We have finished processing your order.', 'woocommerce' ); ?></p>
 <?php
 
 /*
