86fb87c7d169882dadc91e79cef2326f696e7f7c
Mike Jolley <mike.jolley@me.com>
2019-03-25 14:55:14 +0000

    Change coupon amount type based on coupon type

--- assets/js/admin/meta-boxes-coupon.js
+++ assets/js/admin/meta-boxes-coupon.js
@@ -23,8 +23,10 @@ jQuery(function( $ ) {
 
 			if ( select_val !== 'fixed_cart' ) {
 				$( '.limit_usage_to_x_items_field' ).show();
+				$( '#coupon_amount' ).removeClass( 'wc_input_price' ).addClass( 'wc_input_decimal' );
 			} else {
 				$( '.limit_usage_to_x_items_field' ).hide();
+				$( '#coupon_amount' ).removeClass( 'wc_input_decimal' ).addClass( 'wc_input_price' );
 			}
 		}
 	};
