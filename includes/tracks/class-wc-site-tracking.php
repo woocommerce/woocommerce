7af9d1813dde39ee3b814b378c353bb8aa231bb6
Paul Sealock <psealock@gmail.com>
2019-03-29 16:45:18 +1300

    Tracks: Add coupons view events

--- includes/tracks/class-wc-site-tracking.php
+++ includes/tracks/class-wc-site-tracking.php
@@ -112,6 +112,7 @@ class WC_Site_Tracking {
 		include_once WC_ABSPATH . 'includes/tracks/events/class-wc-orders-tracking.php';
 		include_once WC_ABSPATH . 'includes/tracks/events/class-wc-settings-tracking.php';
 		include_once WC_ABSPATH . 'includes/tracks/events/class-wc-status-tracking.php';
+		include_once WC_ABSPATH . 'includes/tracks/events/class-wc-coupons-tracking.php';
 
 		$tracking_classes = array(
 			'WC_Admin_Setup_Wizard_Tracking',
@@ -121,6 +122,7 @@ class WC_Site_Tracking {
 			'WC_Orders_Tracking',
 			'WC_Settings_Tracking',
 			'WC_Status_Tracking',
+			'WC_Coupons_Tracking',
 		);
 
 		foreach ( $tracking_classes as $tracking_class ) {
