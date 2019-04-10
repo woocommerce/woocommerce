1f016b5418a4fa89df33d326d557cb311021987e
Joshua Flowers <joshuatf@gmail.com>
2019-04-10 17:51:12 +0800

    Add days and skip options to regenerate reports function

--- plugins/woocommerce-admin/includes/class-wc-admin-reports-sync.php
+++ plugins/woocommerce-admin/includes/class-wc-admin-reports-sync.php
@@ -89,20 +89,24 @@ class WC_Admin_Reports_Sync {
 		add_action( self::QUEUE_BATCH_ACTION, array( __CLASS__, 'queue_batches' ), 10, 3 );
 		add_action( self::QUEUE_DEPEDENT_ACTION, array( __CLASS__, 'queue_dependent_action' ), 10, 3 );
 		add_action( self::CUSTOMERS_BATCH_ACTION, array( __CLASS__, 'customer_lookup_process_batch' ) );
-		add_action( self::ORDERS_BATCH_ACTION, array( __CLASS__, 'orders_lookup_process_batch' ) );
-		add_action( self::ORDERS_LOOKUP_BATCH_INIT, array( __CLASS__, 'orders_lookup_batch_init' ) );
+		add_action( self::ORDERS_BATCH_ACTION, array( __CLASS__, 'orders_lookup_process_batch' ), 10, 3 );
+		add_action( self::ORDERS_LOOKUP_BATCH_INIT, array( __CLASS__, 'orders_lookup_batch_init' ), 10, 2 );
 		add_action( self::SINGLE_ORDER_ACTION, array( __CLASS__, 'orders_lookup_process_order' ) );
 	}
 
 	/**
 	 * Regenerate data for reports.
+	 *
+	 * @param int|bool $days Number of days to process.
+	 * @param bool     $skip_existing Skip exisiting records.
+	 * @return string
 	 */
-	public static function regenerate_report_data() {
+	public static function regenerate_report_data( $days, $skip_existing ) {
 		// Add registered customers to the lookup table before updating order stats
 		// so that the orders can be associated with the `customer_id` column.
 		self::customer_lookup_batch_init();
 		// Queue orders lookup to occur after customers lookup generation is done.
-		self::queue_dependent_action( self::ORDERS_LOOKUP_BATCH_INIT, array(), self::CUSTOMERS_BATCH_ACTION );
+		self::queue_dependent_action( self::ORDERS_LOOKUP_BATCH_INIT, array( $days, $skip_existing ), self::CUSTOMERS_BATCH_ACTION );
 
 		return __( 'Report table data is being rebuilt.  Please allow some time for data to fully populate.', 'woocommerce-admin' );
 	}
@@ -211,47 +215,86 @@ class WC_Admin_Reports_Sync {
 
 	/**
 	 * Init order/product lookup tables update (in batches).
+	 *
+	 * @param integer|boolean $days Number of days to process.
+	 * @param boolean         $skip_existing Skip exisiting records.
 	 */
-	public static function orders_lookup_batch_init() {
-		$batch_size  = self::get_batch_size( self::ORDERS_BATCH_ACTION );
-		$order_query = new WC_Order_Query(
-			array(
-				'return'   => 'ids',
-				'limit'    => 1,
-				'paginate' => true,
-			)
-		);
-		$result      = $order_query->get_orders();
+	public static function orders_lookup_batch_init( $days, $skip_existing ) {
+		$batch_size = self::get_batch_size( self::ORDERS_BATCH_ACTION );
+		$orders     = self::get_orders( 1, 1, $days, $skip_existing );
 
-		if ( 0 === $result->total ) {
+		if ( 0 === $orders->total ) {
 			return;
 		}
 
-		$num_batches = ceil( $result->total / $batch_size );
+		$num_batches = ceil( $orders->total / $batch_size );
 
-		self::queue_batches( 1, $num_batches, self::ORDERS_BATCH_ACTION );
+		self::queue_batches( 1, $num_batches, self::ORDERS_BATCH_ACTION, array( $days, $skip_existing ) );
+	}
+
+	/**
+	 * Get the order IDs and total count that need to be synced.
+	 *
+	 * @param int      $limit Number of records to retrieve.
+	 * @param int      $page  Page number.
+	 * @param int|bool $days Number of days prior to current date to limit search results.
+	 * @param bool     $skip_existing Skip already imported orders.
+	 */
+	public static function get_orders( $limit = 10, $page = 1, $days = false, $skip_existing = false ) {
+		global $wpdb;
+		$where_clause = '';
+		$offset       = $page > 1 ? $page * $limit : 0;
+
+		if ( $days ) {
+			$days_ago      = date( 'Y-m-d H:i:s', time() - ( DAY_IN_SECONDS * $days ) );
+			$where_clause .= " AND post_date >= '{$days_ago}'";
+		}
+
+		if ( $skip_existing ) {
+			$where_clause .= " AND NOT EXISTS (
+				SELECT 1 FROM {$wpdb->prefix}wc_order_stats
+				WHERE {$wpdb->prefix}wc_order_stats.order_id = {$wpdb->posts}.ID
+			)";
+		}
+
+		$order_ids = $wpdb->get_col(
+			$wpdb->prepare(
+				"SELECT ID FROM {$wpdb->posts}
+				WHERE post_type = 'shop_order'
+				{$where_clause}
+				ORDER BY post_date ASC
+				LIMIT %d
+				OFFSET %d",
+				$limit,
+				$offset
+			)
+		); // WPCS: unprepared SQL ok.
+
+		$count = $wpdb->get_var(
+			"SELECT COUNT(*) FROM {$wpdb->posts}
+			WHERE post_type = 'shop_order'
+			{$where_clause}"
+		); // WPCS: unprepared SQL ok.
+
+		return (object) array(
+			'total'     => absint( $count ),
+			'order_ids' => $order_ids,
+		);
 	}
 
 	/**
 	 * Process a batch of orders to update (stats and products).
 	 *
-	 * @param int $batch_number Batch number to process (essentially a query page number).
+	 * @param int      $batch_number Batch number to process (essentially a query page number).
+	 * @param int|bool $days Number of days to process.
+	 * @param bool     $skip_existing Skip exisiting records.
 	 * @return void
 	 */
-	public static function orders_lookup_process_batch( $batch_number ) {
-		$batch_size  = self::get_batch_size( self::ORDERS_BATCH_ACTION );
-		$order_query = new WC_Order_Query(
-			array(
-				'return'  => 'ids',
-				'limit'   => $batch_size,
-				'page'    => $batch_number,
-				'orderby' => 'ID',
-				'order'   => 'ASC',
-			)
-		);
-		$order_ids   = $order_query->get_orders();
+	public static function orders_lookup_process_batch( $batch_number, $days, $skip_existing ) {
+		$batch_size = self::get_batch_size( self::ORDERS_BATCH_ACTION );
+		$orders     = self::get_orders( $batch_size, $batch_number, $days, $skip_existing );
 
-		foreach ( $order_ids as $order_id ) {
+		foreach ( $orders->order_ids as $order_id ) {
 			self::orders_lookup_process_order( $order_id );
 		}
 	}
@@ -314,9 +357,10 @@ class WC_Admin_Reports_Sync {
 	 * @param int    $range_start Starting batch number.
 	 * @param int    $range_end Ending batch number.
 	 * @param string $single_batch_action Action to schedule for a single batch.
+	 * @param array  $action_args Action arguments.
 	 * @return void
 	 */
-	public static function queue_batches( $range_start, $range_end, $single_batch_action ) {
+	public static function queue_batches( $range_start, $range_end, $single_batch_action, $action_args = array() ) {
 		$batch_size       = self::get_batch_size( self::QUEUE_BATCH_ACTION );
 		$range_size       = 1 + ( $range_end - $range_start );
 		$action_timestamp = time() + 5;
@@ -333,14 +377,15 @@ class WC_Admin_Reports_Sync {
 				self::queue()->schedule_single(
 					$action_timestamp,
 					self::QUEUE_BATCH_ACTION,
-					array( $batch_start, $batch_end, $single_batch_action ),
+					array( $batch_start, $batch_end, $single_batch_action, $action_args ),
 					self::QUEUE_GROUP
 				);
 			}
 		} else {
 			// Otherwise, queue the single batches.
 			for ( $i = $range_start; $i <= $range_end; $i++ ) {
-				self::queue()->schedule_single( $action_timestamp, $single_batch_action, array( $i ), self::QUEUE_GROUP );
+				array_unshift( $action_args, $i );
+				self::queue()->schedule_single( $action_timestamp, $single_batch_action, $action_args, self::QUEUE_GROUP );
 			}
 		}
 	}
