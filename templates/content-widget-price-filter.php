fb776534232a3cd2bc98abf7f80019c151f2078c
Luis <CasperBraske@users.noreply.github.com>
2019-04-19 00:19:43 -0300

    Updating with newest version of WC

--- templates/content-widget-price-filter.php
+++ templates/content-widget-price-filter.php
@@ -21,20 +21,19 @@ defined( 'ABSPATH' ) || exit;
 <?php do_action( 'woocommerce_widget_price_filter_item_start', $args ); ?>
 
 <form method="get" action="<?php echo esc_url( $form_action ); ?>">
-    <div class="price_slider_wrapper">
-        <div class="price_slider" style="display:none;"></div>
-        <div class="price_slider_amount">
-            <input type="text" id="min_price" name="min_price" value="<?php echo esc_attr( $min_price ); ?>" data-min="<?php echo esc_attr( apply_filters( 'woocommerce_price_filter_widget_min_amount', $min ) ); ?>" placeholder="<?php echo esc_attr__( 'Min price', 'woocommerce' ); ?>" />
-            <input type="text" id="max_price" name="max_price" value="<?php echo esc_attr( $max_price ); ?>" data-max="<?php echo esc_attr( apply_filters( 'woocommerce_price_filter_widget_max_amount', $max ) ); ?>" placeholder="<?php echo esc_attr__( 'Max price', 'woocommerce' ); ?>" />
-            <?php /* translators: verb  */ ?>
-            <button type="submit" class="button"><?php echo esc_html__( 'Filter', 'woocommerce' ); ?></button>
-            <div class="price_label" style="display:none;">
-                <?php echo esc_html__( 'Price:', 'woocommerce' ); ?> <span class="from"></span> &mdash; <span class="to"></span>
-            </div>
-            <?php wc_query_string_form_fields( null, array( 'min_price', 'max_price', 'paged' ), '', true ); ?>
-            <div class="clear"></div>
-        </div>
-    </div>
+	<div class="price_slider_wrapper">
+		<div class="price_slider" style="display:none;"></div>
+		<div class="price_slider_amount" data-step="<?php echo esc_attr( $step ); ?>">
+			<input type="text" id="min_price" name="min_price" value="<?php echo esc_attr( $current_min_price ); ?>" data-min="<?php echo esc_attr( $min_price ); ?>" placeholder="<?php echo esc_attr__( 'Min price', 'woocommerce' ); ?>" />
+			<input type="text" id="max_price" name="max_price" value="<?php echo esc_attr( $current_max_price ); ?>" data-max="<?php echo esc_attr( $max_price ); ?>" placeholder="<?php echo esc_attr__( 'Max price', 'woocommerce' ); ?>" />
+			<button type="submit" class="button"><?php echo esc_html__( 'Filter', 'woocommerce' ); ?></button>
+			<div class="price_label" style="display:none;">
+				<?php echo esc_html__( 'Price:', 'woocommerce' ); ?> <span class="from"></span> &mdash; <span class="to"></span>
+			</div>
+			<?php echo wc_query_string_form_fields( null, array( 'min_price', 'max_price', 'paged' ), '', true ); ?>
+			<div class="clear"></div>
+		</div>
+	</div>
 </form>
 
 <?php do_action( 'woocommerce_widget_price_filter_item_end', $args ); ?>
