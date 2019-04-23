8b4cebfc7cde7db91f96158ebc75ae3a12582c2d
Mike Jolley <mike.jolley@me.com>
2019-04-23 17:05:22 +0100

    Missing textdomain

--- templates/auth/header.php
+++ templates/auth/header.php
@@ -29,5 +29,5 @@ defined( 'ABSPATH' ) || exit;
 	<link rel="stylesheet" href="<?php echo esc_url( str_replace( array( 'http:', 'https:' ), '', WC()->plugin_url() ) . '/assets/css/auth.css' ); ?>" type="text/css" />
 </head>
 <body class="wc-auth wp-core-ui">
-	<h1 id="wc-logo"><img src="<?php echo esc_url( WC()->plugin_url() ); ?>/assets/images/woocommerce_logo.png" alt="WooCommerce" /></h1>
+	<h1 id="wc-logo"><img src="<?php echo esc_url( WC()->plugin_url() ); ?>/assets/images/woocommerce_logo.png" alt="<?php esc_attr_e( 'WooCommerce', 'woocommerce' ); ?>" /></h1>
 	<div class="wc-auth-content">
