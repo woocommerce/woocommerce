05a10f85002938eea3d025c701914b66412d9aae
Mike Jolley <mike.jolley@me.com>
2019-04-23 12:01:13 +0100

    Prevent tab causing extra white space

--- includes/admin/views/html-notice-updating.php
+++ includes/admin/views/html-notice-updating.php
@@ -11,6 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $pending_actions_url = admin_url( 'admin.php?page=wc-status&tab=action-scheduler&s=woocommerce_run_update&status=pending' );
 $cron_disabled       = defined( 'DISABLE_WP_CRON' ) && DISABLE_WP_CRON;
+$cron_cta            = $cron_disabled ? __( 'You can manually run queued updates here.', 'woocommerce' ) : __( 'View progress &rarr;', 'woocommerce' );
 ?>
 <div id="message" class="updated woocommerce-message wc-connect">
 	<p>
@@ -20,14 +21,6 @@ $cron_disabled       = defined( 'DISABLE_WP_CRON' ) && DISABLE_WP_CRON;
 			echo '<br><span style="color:red;">' . esc_html__( 'Warning: WP CRON has been disabled on your install which may prevent this update from completing.', 'woocommerce' ) . '</span>';
 		}
 		?>
-		&nbsp;<a href="<?php echo esc_url( $pending_actions_url ); ?>">
-			<?php
-			if ( $cron_disabled ) {
-				esc_html_e( 'You can manually run queued updates here.', 'woocommerce' );
-			} else {
-				esc_html_e( 'View progress &rarr;', 'woocommerce' );
-			}
-			?>
-		</a>
+		&nbsp;<a href="<?php echo esc_url( $pending_actions_url ); ?>"><?php echo esc_html( $cron_cta ); ?></a>
 	</p>
 </div>
