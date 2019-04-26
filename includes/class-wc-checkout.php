6b92769626b5ecad6f411dbe82fff7aebf94a327
parmarhardip <parmarhardip1995@gmail.com>
2019-04-26 17:21:40 +0530

    Fixes #23521 - Billing Email and Country now autofill for newly registered customers

--- includes/class-wc-checkout.php
+++ includes/class-wc-checkout.php
@@ -1176,7 +1176,7 @@ class WC_Checkout {
 		if ( is_user_logged_in() ) {
 			// Load customer object, but keep it cached to avoid reloading it multiple times.
 			if ( is_null( $this->logged_in_customer ) ) {
-				$this->logged_in_customer = new WC_Customer( get_current_user_id() );
+				$this->logged_in_customer = new WC_Customer( get_current_user_id(), true );
 			}
 			$customer_object = $this->logged_in_customer;
 		}
