7b2a33121301a8f3f84033148601821cee6759ff
parmarhardip <parmarhardip1995@gmail.com>
2019-05-09 17:42:08 +0530

    remove a phpcs ignore rule.

--- templates/single-product/add-to-cart/variable.php
+++ templates/single-product/add-to-cart/variable.php
@@ -29,7 +29,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 	<?php do_action( 'woocommerce_before_variations_form' ); ?>
 
 	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
-        <p class="stock out-of-stock"><?php echo apply_filters( 'woocommerce_out_of_stock_message', esc_html__( 'This product is currently out of stock and unavailable.', 'woocommerce' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
+        <p class="stock out-of-stock"><?php echo apply_filters( 'woocommerce_out_of_stock_message', esc_html__( 'This product is currently out of stock and unavailable.', 'woocommerce' ) ); ?></p>
 	<?php else : ?>
 		<table class="variations" cellspacing="0">
 			<tbody>
