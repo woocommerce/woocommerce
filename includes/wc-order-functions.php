31acfbcb4ae9004dffe40c55c38138be2c205abe
Gerhard <potgieterg@gmail.com>
2019-05-13 14:40:54 +0200

    Deprecate WC_Abstract_Order::get_used_coupons in favor of WC_Abstract_Order::get_used_coupon_codes, this reflects the actual data being returned better.

--- includes/wc-order-functions.php
+++ includes/wc-order-functions.php
@@ -854,8 +854,8 @@ function wc_update_coupon_usage_counts( $order_id ) {
 		return;
 	}
 
-	if ( count( $order->get_used_coupons() ) > 0 ) {
-		foreach ( $order->get_used_coupons() as $code ) {
+	if ( count( $order->get_used_coupon_codes() ) > 0 ) {
+		foreach ( $order->get_used_coupon_codes() as $code ) {
 			if ( ! $code ) {
 				continue;
 			}
--- tests/unit-tests/order/class-wc-tests-crud-orders.php
+++ tests/unit-tests/order/class-wc-tests-crud-orders.php
@@ -452,9 +452,9 @@ class WC_Tests_CRUD_Orders extends WC_Unit_Test_Case {
 	}
 
 	/**
-	 * Test: get_used_coupons
+	 * Test: get_used_coupon_codes
 	 */
-	public function test_get_used_coupons() {
+	public function test_get_used_coupon_codes() {
 		$object = new WC_Order();
 		$item   = new WC_Order_Item_Coupon();
 		$item->set_props(
@@ -466,7 +466,7 @@ class WC_Tests_CRUD_Orders extends WC_Unit_Test_Case {
 		);
 		$object->add_item( $item );
 		$object->save();
-		$this->assertCount( 1, $object->get_used_coupons() );
+		$this->assertCount( 1, $object->get_used_coupon_codes() );
 	}
 
 	/**
