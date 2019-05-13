4266cc9eaa0862bd1ff2f767187313cfa9349fdb
Tatu <17417945+rekkitatu@users.noreply.github.com>
2019-05-13 12:27:01 +0300

    Update class-wc-session-handler.php

--- includes/class-wc-session-handler.php
+++ includes/class-wc-session-handler.php
@@ -97,7 +97,7 @@ class WC_Session_Handler extends WC_Session {
 				$guest_session_id = $this->_customer_id;
 				$this->_customer_id = get_current_user_id();
 				$this->_dirty       = true;
-				$this->save_data($guest_session_id);
+				$this->save_data( $guest_session_id );
 				$this->set_customer_session_cookie( true );
 			}
 
@@ -254,7 +254,7 @@ class WC_Session_Handler extends WC_Session {
 
 			wp_cache_set( $this->get_cache_prefix() . $this->_customer_id, $this->_data, WC_SESSION_CACHE_GROUP, $this->_session_expiration - time() );
 			$this->_dirty = false;
-			if (get_current_user_id() !== $old_session_key) {
+			if ( get_current_user_id() !== $old_session_key ) {
 				$this->delete_session( $old_session_key );
 			}
 		}
