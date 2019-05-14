ab83c777adfd61ee5f06633387cc6e06b614d750
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-05-14 18:34:21 -0300

    Fix PHPCS errors

--- tests/unit-tests/setup/functions.php
+++ tests/unit-tests/setup/functions.php
@@ -1,8 +1,11 @@
 <?php
+/**
+ * Class WC_Tests_Setup_Functions.
+ * @package WooCommerce\Tests\Setup
+ */
 
 /**
- * Class Functions.
- * @package WooCommerce\Tests\Setup
+ * Class WC_Tests_Setup_Functions.
  */
 class WC_Tests_Setup_Functions extends WC_Unit_Test_Case {
 
@@ -15,21 +18,37 @@ class WC_Tests_Setup_Functions extends WC_Unit_Test_Case {
 	public function test_wizard_in_cart_payment_gateways() {
 		$setup_wizard = new WC_Admin_Setup_Wizard();
 
+		/**
+		 * Return wheter a given gateway is enable or not.
+		 *
+		 * @param array $gateway Gateway information.
+		 * @return bool
+		 */
 		function get_enabled( $gateway ) {
 			return isset( $gateway['enabled'] ) && $gateway['enabled'];
 		}
+
+		/**
+		 * Helper function to call the tested method and return a simplified version
+		 * of the returned values. It returns only if the gateways are enable or not
+		 * which is what we are currently checking in the tests.
+		 *
+		 * @param WC_Admin_Setup_Wizard $setup_wizard Setup wizard object.
+		 *
+		 * @return array
+		 */
 		function gateways( $setup_wizard ) {
 			return array_map( 'get_enabled', $setup_wizard->get_wizard_in_cart_payment_gateways() );
 		}
 
-		// non-admin user
+		// non-admin user.
 		$this->user_id = $this->factory->user->create( array( 'role' => 'shop_manager' ) );
 		wp_set_current_user( $this->user_id );
 		$this->assertEquals( gateways( $setup_wizard ), array(
 			'paypal' => false,
 		) );
 
-		// set admin user
+		// set admin user.
 		$this->user_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
 		wp_set_current_user( $this->user_id );
 
