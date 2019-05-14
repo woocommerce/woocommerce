c8b55ff62da4c145a56a87a7fa60e9de6def880e
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-05-14 18:38:53 -0300

    Improve WC_Admin_Setup_Wizard::get_wizard_in_cart_payment_gateways() tests
    This commit moves two functions declared inside a test method to their
    own protected methods inside the same test class to make it easier to
    read the tests and it fixes the order of the parameters for the calls to
    Assert::assertEquals(). The first parameter passed to this method should
    be the expected value and the second parameter should be the actual
    value and not the opposite.

--- tests/unit-tests/setup/functions.php
+++ tests/unit-tests/setup/functions.php
@@ -18,37 +18,14 @@ class WC_Tests_Setup_Functions extends WC_Unit_Test_Case {
 	public function test_wizard_in_cart_payment_gateways() {
 		$setup_wizard = new WC_Admin_Setup_Wizard();
 
-		/**
-		 * Return wheter a given gateway is enable or not.
-		 *
-		 * @param array $gateway Gateway information.
-		 * @return bool
-		 */
-		function get_enabled( $gateway ) {
-			return isset( $gateway['enabled'] ) && $gateway['enabled'];
-		}
-
-		/**
-		 * Helper function to call the tested method and return a simplified version
-		 * of the returned values. It returns only if the gateways are enable or not
-		 * which is what we are currently checking in the tests.
-		 *
-		 * @param WC_Admin_Setup_Wizard $setup_wizard Setup wizard object.
-		 *
-		 * @return array
-		 */
-		function gateways( $setup_wizard ) {
-			return array_map( 'get_enabled', $setup_wizard->get_wizard_in_cart_payment_gateways() );
-		}
-
 		// non-admin user.
 		$this->user_id = $this->factory->user->create( array( 'role' => 'shop_manager' ) );
 		wp_set_current_user( $this->user_id );
 		$this->assertEquals(
-			gateways( $setup_wizard ),
 			array(
 				'paypal' => false,
-			)
+			),
+			$this->get_gateways_statuses( $setup_wizard )
 		);
 
 		// set admin user.
@@ -57,50 +34,74 @@ class WC_Tests_Setup_Functions extends WC_Unit_Test_Case {
 
 		update_option( 'woocommerce_default_country', 'US' );
 		$this->assertEquals(
-			gateways( $setup_wizard ),
 			array(
 				'stripe'      => true,
 				'ppec_paypal' => true,
-			)
+			),
+			$this->get_gateways_statuses( $setup_wizard )
 		);
 
 		update_option( 'woocommerce_default_country', 'CN' );
 		$this->assertEquals(
-			gateways( $setup_wizard ),
 			array(
 				'ppec_paypal' => true,
-			)
+			),
+			$this->get_gateways_statuses( $setup_wizard )
 		);
 
 		update_option( 'woocommerce_default_country', 'SE' );
 		$this->assertEquals(
-			gateways( $setup_wizard ),
 			array(
 				'klarna_checkout' => true,
 				'ppec_paypal'     => true,
 				'stripe'          => false,
-			)
+			),
+			$this->get_gateways_statuses( $setup_wizard )
 		);
 
 		update_option( 'woocommerce_default_country', 'DE' );
 		$this->assertEquals(
-			gateways( $setup_wizard ),
 			array(
 				'klarna_payments' => true,
 				'ppec_paypal'     => true,
 				'stripe'          => false,
-			)
+			),
+			$this->get_gateways_statuses( $setup_wizard )
 		);
 
 		update_option( 'woocommerce_default_country', 'GB' );
 		update_option( 'woocommerce_sell_in_person', 'yes' );
 		$this->assertEquals(
-			gateways( $setup_wizard ),
 			array(
 				'square'      => true,
 				'ppec_paypal' => true,
 				'stripe'      => false,
-			)
+			),
+			$this->get_gateways_statuses( $setup_wizard )
 		);
 	}
+
+	/**
+	 * Helper method to call the tested method and return a simplified version
+	 * of the returned values. It returns only if the gateways are enable or not
+	 * which is what we are currently checking in the tests.
+	 *
+	 * @param WC_Admin_Setup_Wizard $setup_wizard Setup wizard object.
+	 *
+	 * @return array
+	 */
+	protected function get_gateways_statuses( $setup_wizard ) {
+		return array_map( array( $this, 'get_enabled' ), $setup_wizard->get_wizard_in_cart_payment_gateways() );
+	}
+
+	/**
+	 * Return wheter a given gateway is enable or not.
+	 *
+	 * @param array $gateway Gateway information.
+	 * @return bool
+	 */
+	protected function get_enabled( $gateway ) {
+		return isset( $gateway['enabled'] ) && $gateway['enabled'];
+	}
+
 }
