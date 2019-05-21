ca4e79bdd1d06a3a307f2012cdbe1aa8a31ba90a
Paul Sealock <psealock@gmail.com>
2019-05-21 12:20:25 +1200

    Tracks: Add check for OBW is-opting-in

--- includes/tracks/class-wc-tracks.php
+++ includes/tracks/class-wc-tracks.php
@@ -74,13 +74,9 @@ class WC_Tracks {
 	 */
 	public static function record_event( $event_name, $properties = array() ) {
 		/**
-		 * Don't track users who haven't opted-in to tracking or if a filter
-		 * has been applied to turn it off.
+		 * Don't track users who don't have tracking enabled.
 		 */
-		if (
-			'yes' !== get_option( 'woocommerce_allow_tracking' ) ||
-			! apply_filters( 'woocommerce_apply_tracking', true )
-		) {
+		if ( ! WC_Site_Tracking::is_tracking_enabled() ) {
 			return false;
 		}
 
