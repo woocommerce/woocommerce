464992a232ccb569bd8ce20b189f6486cce41d87
Claudio Sanches <contato@claudiosanches.com>
2019-05-22 21:07:46 -0300

    Fixed file version

--- i18n/states/UG.php
+++ i18n/states/UG.php
@@ -4,8 +4,8 @@
  *
  * Based on English names published at https://en.wikipedia.org/wiki/ISO_3166-2:UG
  *
- * @package WooCommerce/i18n
- * @version 2.0.0
+ * @package WooCommerce\i18n
+ * @version 3.7.0
  */
 
 global $states;
