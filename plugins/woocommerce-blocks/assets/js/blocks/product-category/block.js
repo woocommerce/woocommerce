40c551dba4e614fe74837a0cd195a9b9706a04a2
Kelly Dwan <ryelle@users.noreply.github.com>
2019-05-23 07:23:15 -0400

    Add dynamic rendering + SSR to Products By Category block (https://github.com/woocommerce/woocommerce-blocks/pull/596)

--- plugins/woocommerce-blocks/assets/js/blocks/product-category/block.js
+++ plugins/woocommerce-blocks/assets/js/blocks/product-category/block.js
@@ -1,91 +1,41 @@
 /**
  * External dependencies
  */
-import { __, _n } from '@wordpress/i18n';
-import { addQueryArgs } from '@wordpress/url';
-import apiFetch from '@wordpress/api-fetch';
-import { BlockControls, InspectorControls } from '@wordpress/editor';
+import { __ } from '@wordpress/i18n';
+import { BlockControls, InspectorControls, ServerSideRender } from '@wordpress/editor';
 import {
 	Button,
+	Disabled,
 	PanelBody,
 	Placeholder,
-	Spinner,
 	Toolbar,
 	withSpokenMessages,
 } from '@wordpress/components';
-import classnames from 'classnames';
 import { Component, Fragment } from '@wordpress/element';
-import { debounce } from 'lodash';
 import PropTypes from 'prop-types';
 
 /**
  * Internal dependencies
  */
-import getQuery from '../../utils/get-query';
 import GridContentControl from '../../components/grid-content-control';
 import GridLayoutControl from '../../components/grid-layout-control';
 import ProductCategoryControl from '../../components/product-category-control';
 import ProductOrderbyControl from '../../components/product-orderby-control';
-import ProductPreview from '../../components/product-preview';
 
 /**
  * Component to handle edit mode of "Products by Category".
  */
 class ProductByCategoryBlock extends Component {
-	constructor() {
-		super( ...arguments );
-		this.state = {
-			products: [],
-			loaded: false,
-		};
-
-		this.debouncedGetProducts = debounce( this.getProducts.bind( this ), 200 );
-	}
-
-	componentDidMount() {
-		if ( this.props.attributes.categories ) {
-			this.getProducts();
-		}
-	}
-
-	componentDidUpdate( prevProps ) {
-		const hasChange = [
-			'categories',
-			'catOperator',
-			'columns',
-			'orderby',
-			'rows',
-		].reduce( ( acc, key ) => {
-			return acc || prevProps.attributes[ key ] !== this.props.attributes[ key ];
-		}, false );
-		if ( hasChange ) {
-			this.debouncedGetProducts();
-		}
-	}
-
-	getProducts() {
-		if ( ! this.props.attributes.categories.length ) {
-			// We've removed all selected categories, or no categories have been selected yet.
-			this.setState( { products: [], loaded: true } );
-			return;
-		}
-		apiFetch( {
-			path: addQueryArgs(
-				'/wc-blocks/v1/products',
-				getQuery( this.props.attributes, this.props.name )
-			),
-		} )
-			.then( ( products ) => {
-				this.setState( { products, loaded: true } );
-			} )
-			.catch( () => {
-				this.setState( { products: [], loaded: true } );
-			} );
-	}
-
 	getInspectorControls() {
 		const { attributes, setAttributes } = this.props;
-		const { columns, catOperator, contentVisibility, editMode, orderby, rows } = attributes;
+		const {
+			columns,
+			catOperator,
+			contentVisibility,
+			editMode,
+			orderby,
+			rows,
+		} = attributes;
 
 		return (
 			<InspectorControls key="inspector">
@@ -180,37 +130,8 @@ class ProductByCategoryBlock extends Component {
 	}
 
 	render() {
-		const { setAttributes } = this.props;
-		const {
-			categories,
-			columns,
-			contentVisibility,
-			editMode,
-		} = this.props.attributes;
-		const { loaded, products = [] } = this.state;
-		const classes = classnames( {
-			'wc-block-products-grid': true,
-			'wc-block-products-category': true,
-			[ `cols-${ columns }` ]: columns,
-			'is-loading': ! loaded,
-			'is-not-found': loaded && ! products.length,
-			'is-hidden-title': ! contentVisibility.title,
-			'is-hidden-price': ! contentVisibility.price,
-			'is-hidden-rating': ! contentVisibility.rating,
-			'is-hidden-button': ! contentVisibility.button,
-		} );
-
-		const nothingFound = ! categories.length ?
-			__(
-				'Select at least one category to display its products.',
-				'woo-gutenberg-products-block'
-			) :
-			_n(
-				'No products in this category.',
-				'No products in these categories.',
-				categories.length,
-				'woo-gutenberg-products-block'
-			);
+		const { attributes, setAttributes } = this.props;
+		const { editMode } = attributes;
 
 		return (
 			<Fragment>
@@ -230,23 +151,12 @@ class ProductByCategoryBlock extends Component {
 				{ editMode ? (
 					this.renderEditMode()
 				) : (
-					<div className={ classes }>
-						{ products.length ? (
-							products.map( ( product ) => (
-								<ProductPreview product={ product } key={ product.id } />
-							) )
-						) : (
-							<Placeholder
-								icon="category"
-								label={ __(
-									'Products by Category',
-									'woo-gutenberg-products-block'
-								) }
-							>
-								{ ! loaded ? <Spinner /> : nothingFound }
-							</Placeholder>
-						) }
-					</div>
+					<Disabled>
+						<ServerSideRender
+							block="woocommerce/product-category"
+							attributes={ attributes }
+						/>
+					</Disabled>
 				) }
 			</Fragment>
 		);
