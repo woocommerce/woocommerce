e0ff109781570da66809f9cfc7a71dee8cee7305
Kelly Dwan <ryelle@users.noreply.github.com>
2019-05-28 08:06:12 -0400

    Add dynamic rendering + SSR to the Top Rated Products block (https://github.com/woocommerce/woocommerce-blocks/pull/600)

--- plugins/woocommerce-blocks/assets/js/blocks/product-top-rated/index.js
+++ plugins/woocommerce-blocks/assets/js/blocks/product-top-rated/index.js
@@ -2,20 +2,20 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import classnames from 'classnames';
 import { createBlock, registerBlockType } from '@wordpress/blocks';
-import { without } from 'lodash';
 import Gridicon from 'gridicons';
-import { RawHTML } from '@wordpress/element';
+import { without } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import Block from './block';
-import getShortcode from '../../utils/get-shortcode';
+import { deprecatedConvertToShortcode } from '../../utils/deprecations';
 import sharedAttributes, { sharedAttributeBlockTypes } from '../../utils/shared-attributes';
 
-registerBlockType( 'woocommerce/product-top-rated', {
+const blockTypeName = 'woocommerce/product-top-rated';
+
+registerBlockType( blockTypeName, {
 	title: __( 'Top Rated Products', 'woo-gutenberg-products-block' ),
 	icon: {
 		src: <Gridicon icon="trophy" />,
@@ -33,11 +33,12 @@ registerBlockType( 'woocommerce/product-top-rated', {
 	attributes: {
 		...sharedAttributes,
 	},
+
 	transforms: {
 		from: [
 			{
 				type: 'block',
-				blocks: without( sharedAttributeBlockTypes, 'woocommerce/product-top-rated' ),
+				blocks: without( sharedAttributeBlockTypes, blockTypeName ),
 				transform: ( attributes ) => createBlock(
 					'woocommerce/product-top-rated',
 					attributes
@@ -46,6 +47,14 @@ registerBlockType( 'woocommerce/product-top-rated', {
 		],
 	},
 
+	deprecated: [
+		{
+			// Deprecate shortcode save method in favor of dynamic rendering.
+			attributes: sharedAttributes,
+			save: deprecatedConvertToShortcode( blockTypeName ),
+		},
+	],
+
 	/**
 	 * Renders and manages the block.
 	 */
@@ -53,29 +62,7 @@ registerBlockType( 'woocommerce/product-top-rated', {
 		return <Block { ...props } />;
 	},
 
-	/**
-	 * Save the block content in the post content. Block content is saved as a products shortcode.
-	 *
-	 * @return string
-	 */
-	save( props ) {
-		const {
-			align,
-			contentVisibility,
-		} = props.attributes; /* eslint-disable-line react/prop-types */
-		const classes = classnames(
-			align ? `align${ align }` : '',
-			{
-				'is-hidden-title': ! contentVisibility.title,
-				'is-hidden-price': ! contentVisibility.price,
-				'is-hidden-rating': ! contentVisibility.rating,
-				'is-hidden-button': ! contentVisibility.button,
-			}
-		);
-		return (
-			<RawHTML className={ classes }>
-				{ getShortcode( props, 'woocommerce/product-top-rated' ) }
-			</RawHTML>
-		);
+	save() {
+		return null;
 	},
 } );
