fae9bc071be98d33320da319c7c4c34467a86259
Jonathan Moore <Jonathan.Moore@bcs.org>
2019-05-28 07:32:44 +0100

    reverse  to move to separate pull request
    get_review_count() / get_rating_count() issue on generate_product_data() to be submitted separately

--- includes/class-wc-structured-data.php
+++ includes/class-wc-structured-data.php
@@ -268,7 +268,7 @@ class WC_Structured_Data {
 			$markup['offers'] = array( apply_filters( 'woocommerce_structured_data_product_offer', $markup_offer, $product ) );
 		}
 
-		if ( $product->get_rating_count() && wc_review_ratings_enabled() ) {
+		if ( $product->get_review_count() && wc_review_ratings_enabled() ) {
 			$markup['aggregateRating'] = array(
 				'@type'       => 'AggregateRating',
 				'ratingValue' => $product->get_average_rating(),
