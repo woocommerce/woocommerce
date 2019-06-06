f9d064e778325df29fb02eeff3a16ac2a74fad3f
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-06-06 17:09:50 +0200

    Track changes to order creation date.

--- includes/tracks/events/class-wc-orders-tracking.php
+++ includes/tracks/events/class-wc-orders-tracking.php
@@ -17,6 +17,7 @@ class WC_Orders_Tracking {
 	public function init() {
 		add_action( 'woocommerce_order_status_changed', array( $this, 'track_order_status_change' ), 10, 3 );
 		add_action( 'load-edit.php', array( $this, 'track_orders_view' ), 10 );
+		add_action( 'pre_post_update', array( $this, 'track_created_date_change' ), 10 );
 	}
 
 	/**
@@ -54,5 +55,39 @@ class WC_Orders_Tracking {
 		);
 
 		WC_Tracks::record_event( 'orders_edit_status_change', $properties );
+	}
+
+	/**
+	 * Send a Tracks event when an order date is changed.
+	 *
+	 * @param int $id Order id.
+	 */
+	public function track_created_date_change( $id ) {
+		$post_type = get_post_type( $id );
+
+		if ( 'shop_order' !== $post_type ) {
+			return;
+		}
+
+		$order        = wc_get_order( $id );
+		$date_created = $order->get_date_created()->date( 'Y-m-d H:i:s' );
+		// phpcs:disable WordPress.Security.NonceVerification.NoNonceVerification
+		$new_date     = sprintf(
+			'%s %2d:%2d:%2d',
+			isset( $_POST['order_date'] ) ? wc_clean( wp_unslash( $_POST['order_date'] ) ) : '',
+			isset( $_POST['order_date_hour'] ) ? wc_clean( wp_unslash( $_POST['order_date_hour'] ) ) : '',
+			isset( $_POST['order_date_minute'] ) ? wc_clean( wp_unslash( $_POST['order_date_minute'] ) ) : '',
+			isset( $_POST['order_date_second'] ) ? wc_clean( wp_unslash( $_POST['order_date_second'] ) ) : ''
+		);
+		// phpcs:enable
+
+		if ( $new_date !== $date_created ) {
+			$properties = array(
+				'order_id'   => $id,
+				'status'     => $order->get_status(),
+			);
+
+			WC_Tracks::record_event( 'order_edit_date_created', $properties );
+		}
 	}
 }
