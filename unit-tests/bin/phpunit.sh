0753e3f6a8184dd3afb14aacaa967fb04c73d5f8
Mike Jolley <mike.jolley@me.com>
2019-06-11 10:50:13 +0100

    More config changes

--- unit-tests/bin/phpunit.sh
+++ unit-tests/bin/phpunit.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-
+WORKING_DIR="$PWD"
+cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-rest-api/"
 phpunit --version
-phpunit -c phpunit.xml $@
+phpunit -c phpunit.xml
+TEST_RESULT=$?
+cd "$WORKING_DIR"
+exit $TEST_RESULT
