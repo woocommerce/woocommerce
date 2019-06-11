52dc671ea81109e874cfead2884ac818a829ce5e
Mike Jolley <mike.jolley@me.com>
2019-06-11 11:24:52 +0100

    try travis build dir

--- unit-tests/bin/phpunit.sh
+++ unit-tests/bin/phpunit.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
-WORKING_DIR="$PWD"
-cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-rest-api"
-ls
-composer install
-./vendor/bin/phpunit --version
-./vendor/bin/phpunit -c phpunit.xml
-TEST_RESULT=$?
-cd "$WORKING_DIR"
-exit $TEST_RESULT
+"$TRAVIS_BUILD_DIR"/vendor/bin/phpunit --version
+"$TRAVIS_BUILD_DIR"/vendor/bin/phpunit -c phpunit.xml $@
