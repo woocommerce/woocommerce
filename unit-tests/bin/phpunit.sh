6dc3999ff9263d4d5bd85361a18bce29f4c0a0c8
Mike Jolley <mike.jolley@me.com>
2019-06-11 10:17:12 +0100

    phpunit.sh

--- unit-tests/bin/phpunit.sh
+++ unit-tests/bin/phpunit.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-
-$HOME/.composer/vendor/bin/phpunit -c phpunit.xml.dist $@
+WORKING_DIR="$PWD"
+cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-rest-api/"
+phpunit --version
+phpunit -c phpunit.xml.dist
+TEST_RESULT=$?
+cd "$WORKING_DIR"
+exit $TEST_RESULT
