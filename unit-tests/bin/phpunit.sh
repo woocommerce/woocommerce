6d211e17bfa92a6392d0b8d979afab08553aebc8
Mike Jolley <mike.jolley@me.com>
2019-06-11 11:18:59 +0100

    ls

--- unit-tests/bin/phpunit.sh
+++ unit-tests/bin/phpunit.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 WORKING_DIR="$PWD"
 cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-rest-api"
+ls
 composer install
 ./vendor/bin/phpunit --version
 ./vendor/bin/phpunit -c phpunit.xml
