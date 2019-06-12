4a860656c5911c650177edecfdcb136c0faf6538
Mike Jolley <mike.jolley@me.com>
2019-06-12 13:17:26 +0100

    Disable leaderboard tests if admin is not installed

--- unit-tests/Tests/Version4/Leaderboards.php
+++ unit-tests/Tests/Version4/Leaderboards.php
@@ -47,6 +47,10 @@ class Leaderboards extends WC_REST_Unit_Test_Case {
 	 * Setup our test server, endpoints, and user info.
 	 */
 	public function setUp() {
+		if ( ! class_exists( '\WC_Admin_Reports_Sync' ) ) {
+			$this->markTestSkipped( 'Skipping reports tests - WC_Admin_Reports_Sync class not found.' );
+			return;
+		}
 		parent::setUp();
 		wp_set_current_user( self::$user );
 	}
