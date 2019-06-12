3d89793c4295b592a0d980d090c54fb4013f7bdb
Mike Jolley <mike.jolley@me.com>
2019-06-12 18:47:35 +0100

    Force use of autoloader

--- src/Server.php
+++ src/Server.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || exit;
 if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
 	require __DIR__ . '/../vendor/autoload.php';
 } else {
-	return;
+	wp_die( 'WooCommerce Rest API build required' );
 }
 
 use WooCommerce\RestApi\Utilities\SingletonTrait;
