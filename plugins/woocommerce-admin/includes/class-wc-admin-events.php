2e0ef7f93c888ea8d3116e44e6efa5e15f96cc6c
Paul Sealock <psealock@gmail.com>
2019-06-12 14:37:45 +1200

    Notes: update sales record link

--- plugins/woocommerce-admin/includes/class-wc-admin-events.php
+++ plugins/woocommerce-admin/includes/class-wc-admin-events.php
@@ -50,7 +50,7 @@ class WC_Admin_Events {
 	 *
 	 * Note: WC_Admin_Notes_Order_Milestones::other_milestones is hooked to this as well.
 	 */
-	protected function do_wc_admin_daily() {
+	public function do_wc_admin_daily() {
 		WC_Admin_Notes_New_Sales_Record::possibly_add_sales_record_note();
 		WC_Admin_Notes_Giving_Feedback_Notes::add_notes_for_admin_giving_feedback();
 		WC_Admin_Notes_Mobile_App::possibly_add_mobile_app_note();
