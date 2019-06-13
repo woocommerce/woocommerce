f92730544305364054f885736545ea3099683b1d
Mike Jolley <mike.jolley@me.com>
2019-06-13 15:33:56 +0100

    set_backorders

--- src/Controllers/Version4/Products.php
+++ src/Controllers/Version4/Products.php
@@ -423,12 +423,12 @@ class Products extends AbstractObjectsController {
 
 			if ( $product->is_type( 'grouped' ) ) {
 				$product->set_manage_stock( false );
-				$product->set_backorders( false );
+				$product->set_backorders( 'no' );
 				$product->set_stock_quantity( '' );
 				$product->set_stock_status( $stock_status );
 			} elseif ( $product->is_type( 'external' ) ) {
 				$product->set_manage_stock( false );
-				$product->set_backorders( false );
+				$product->set_backorders( 'no' );
 				$product->set_stock_quantity( '' );
 				$product->set_stock_status( 'instock' );
 			} elseif ( $product->get_manage_stock() ) {
