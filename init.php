3be02138285c53aec1a83ffa2ae01a16935cf091
Mike Jolley <mike.jolley@me.com>
2019-06-20 13:47:04 +0100

    Revert "Test version without custom autoloader and allow core to generate this after install"
    This reverts commit 9283910e115b2df21f11b1d9d69d7601b03cae89.

--- init.php
+++ init.php
@@ -6,9 +6,12 @@
  */
 
 return function() {
-	if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-		return;
+	if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		require __DIR__ . '/vendor/autoload.php';
+	} else {
+		require __DIR__ . '/src/Autoloader.php';
+		$classmap = require 'classmap.php';
+		\WooCommerce\RestApi\Autoloader::register( $classmap );
 	}
-	require __DIR__ . '/vendor/autoload.php';
 	\WooCommerce\RestApi\Server::instance()->init();
 };
