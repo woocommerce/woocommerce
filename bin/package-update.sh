fcf06e97a328c671201a6bd51cabb14bf5fc8c6d
Mike Jolley <mike.jolley@me.com>
2019-06-24 15:29:26 +0100

    Tweak script to add POT generation on update

--- bin/package-update.sh
+++ bin/package-update.sh
@@ -15,15 +15,38 @@ output() {
   echo "$(tput setaf "$1")$2$(tput sgr0)"
 }
 
+updating=false
+
+# Script args.
+while [ ! $# -eq 0 ]
+do
+	case "$1" in
+		--updating | -u)
+			updating=true
+			;;
+	esac
+	shift
+done
+
+# Autoloader
+output 3 "Updating autoloader classmaps..."
+composer dump-autoload --no-dev
+output 2 "Done"
+
+# Convert textdomains
 output 3 "Updating package textdomains..."
 
-# Find woo-gutenberg-products-block textdomain and replace with woocommerce
+# Replace text domains within packages with woocommerce
 find ./packages/woocommerce-blocks -iname '*.php' -exec sed -i.bak -e "s/'woo-gutenberg-products-block'/'woocommerce'/g" {} \;
-
-# Find woocommerce-rest-api textdomain and replace with woocommerce
 find ./packages/woocommerce-rest-api -iname '*.php' -exec sed -i.bak -e "s/'woocommerce-rest-api'/'woocommerce'/g" {} \;
 
 # Cleanup backup files
 find ./packages -name "*.bak" -type f -delete
-
 output 2 "Done"
+
+if ( $updating ); then
+	# Update POT file
+	output 3 "Updating POT file..."
+	grunt makepot
+	output 2 "Done"
+fi
