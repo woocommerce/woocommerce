e46653f3a9f20b974d113578730cd12c58520471
Mike Jolley <mike.jolley@me.com>
2019-06-24 15:50:55 +0100

    More specific replacement

--- bin/package-update.sh
+++ bin/package-update.sh
@@ -37,8 +37,8 @@ output 2 "Done"
 output 3 "Updating package textdomains..."
 
 # Replace text domains within packages with woocommerce
-find ./packages/woocommerce-blocks -iname '*.php' -exec sed -i.bak -e "s/'woo-gutenberg-products-block'/'woocommerce'/g" {} \;
-find ./packages/woocommerce-rest-api -iname '*.php' -exec sed -i.bak -e "s/'woocommerce-rest-api'/'woocommerce'/g" {} \;
+find ./packages/woocommerce-blocks -iname '*.php' -exec sed -i.bak -e "s/, 'woo-gutenberg-products-block'/, 'woocommerce'/g" {} \;
+find ./packages/woocommerce-rest-api -iname '*.php' -exec sed -i.bak -e "s/, 'woocommerce-rest-api'/, 'woocommerce'/g" {} \;
 
 # Cleanup backup files
 find ./packages -name "*.bak" -type f -delete
