7e14630b3bf6e9ace289e08ccc96c79879d22243
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-06-24 10:42:08 -0600

    Set default params on each test request, and use an admin user.

--- plugins/woocommerce-admin/tests/api/report-controllers.php
+++ plugins/woocommerce-admin/tests/api/report-controllers.php
@@ -14,9 +14,28 @@
  */
 class WC_Tests_API_Report_Controllers extends WP_UnitTestCase {
 	/**
+	 * Setup test admin notes data. Called before every test.
+	 *
+	 * @since 3.5.0
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->user = $this->factory->user->create(
+			array(
+				'role' => 'administrator',
+			)
+		);
+
+		wp_set_current_user( $this->user );
+	}
+
+	/**
 	 * Ensure that every REST controller works with their defaults.
 	 */
 	public function test_default_params() {
+		// This list intentionall missing `WC_Admin_REST_Setting_Options_Controller`
+		// because it's just a version override.
 		$controller_classes = array(
 			'WC_Admin_REST_Admin_Notes_Controller',
 			'WC_Admin_REST_Admin_Note_Action_Controller',
@@ -33,7 +52,6 @@ class WC_Tests_API_Report_Controllers extends WP_UnitTestCase {
 			'WC_Admin_REST_Product_Reviews_Controller',
 			'WC_Admin_REST_Product_Variations_Controller',
 			'WC_Admin_REST_Reports_Controller',
-			'WC_Admin_REST_Setting_Options_Controller',
 			'WC_Admin_REST_Reports_Import_Controller',
 			'WC_Admin_REST_Reports_Products_Controller',
 			'WC_Admin_REST_Reports_Variations_Controller',
@@ -64,9 +82,25 @@ class WC_Tests_API_Report_Controllers extends WP_UnitTestCase {
 
 		foreach ( $controller_classes as $controller_class ) {
 			$controller = new $controller_class();
-			$response   = $controller->get_items( new WP_REST_Request() );
+			$params     = $controller->get_collection_params();
+			$defaults   = array();
 
-			$this->assertTrue( is_array( $response ) );
+			foreach ( $params as $arg => $options ) {
+				if ( isset( $options['default'] ) ) {
+					$defaults[ $arg ] = $options['default'];
+				}
+			}
+
+			$request = new WP_REST_Request();
+			$request->set_default_params( $defaults );
+			$response = $controller->get_items( $request );
+
+			// Surface any errors for easier debugging.
+			if ( is_wp_error( $response ) ) {
+				$this->fail( $response->get_error_message() );
+			}
+
+			$this->assertTrue( is_array( $response->get_data() ) );
 		}
 	}
 }
