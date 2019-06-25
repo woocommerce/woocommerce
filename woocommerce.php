f8d6b26e2edd39941a6a10869bdcc3c2d23e96fc
Mike Jolley <mike.jolley@me.com>
2019-06-25 13:13:04 +0100

    Add dedicated Packages loader and Autoloader to init functionality

--- woocommerce.php
+++ woocommerce.php
@@ -19,47 +19,26 @@ if ( ! defined( 'WC_PLUGIN_FILE' ) ) {
 }
 
 /**
- * Load all the packages.
+ * Load core packages and the autoloader.
  *
- * We want to fail gracefully if `composer install` has not been executed yet, so we are checking for the autoloader.
- * If the autoloader is not present, let's log the failure and display a nice admin notice.
+ * The new packages and autoloader require PHP 5.6+. If this dependency is not met, do not include them. Users will be warned
+ * that they are using an older version of PHP. WooCommerce will continue to load, but some functionality such as the REST API
+ * and Blocks will be missing.
+ *
+ * This requirement will be enforced in future versions of WooCommerce.
  */
-$autoloader = dirname( __FILE__ ) . '/vendor/autoload_packages.php';
-if ( is_readable( $autoloader ) ) {
-	require $autoloader;
-} else {
-	if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-		error_log(  // phpcs:ignore
-			esc_html__( 'Your installation of WooCommerce is incomplete. If you installed WooCommerce from GitHub, please refer to this document to set up your development environment: https://github.com/woocommerce/woocommerce/wiki/How-to-set-up-WooCommerce-development-environment', 'woocommerce' )
-		);
+if ( version_compare( PHP_VERSION, '5.6.0', '>=' ) ) {
+	require __DIR__ . '/src/Autoloader.php';
+	require __DIR__ . '/src/Packages.php';
+
+	if ( ! \Automattic\WooCommerce\Autoloader::init() ) {
+		return;
 	}
-	/**
-	 * Outputs an admin notice for folks running WooCommerce without having run composer install.
-	 */
-	add_action(
-		'admin_notices',
-		function() {
-			?>
-			<div class="notice notice-error">
-				<p>
-					<?php
-					printf(
-						/* translators: 1: is a link to a support document. 2: closing link */
-						esc_html__( 'Your installation of WooCommerce is incomplete. If you installed WooCommerce from GitHub, %1$splease refer to this document%2$s to set up your development environment.', 'woocommerce' ),
-						'<a href="' . esc_url( 'https://github.com/woocommerce/woocommerce/wiki/How-to-set-up-WooCommerce-development-environment' ) . '" target="_blank" rel="noopener noreferrer">',
-						'</a>'
-					);
-					?>
-				</p>
-			</div>
-			<?php
-		}
-	);
-	return;
+	\Automattic\WooCommerce\Packages::init();
 }
 
 // Include the main WooCommerce class.
-if ( ! class_exists( 'WooCommerce' ) ) {
+if ( ! class_exists( 'WooCommerce', false ) ) {
 	include_once dirname( __FILE__ ) . '/includes/class-woocommerce.php';
 }
 
