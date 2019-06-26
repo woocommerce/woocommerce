7dfc3fe240365be85fdc8db3262de7cfb0bccb09
Ron Rennick <ron@ronandandrea.com>
2019-06-26 17:16:54 -0300

    use current window for any dashboard URL

--- plugins/woocommerce-admin/client/header/activity-panel/panels/inbox.js
+++ plugins/woocommerce-admin/client/header/activity-panel/panels/inbox.js
@@ -37,7 +37,7 @@ class InboxPanel extends Component {
 		const { triggerNoteAction } = this.props;
 		const href = event.target.href || '';
 
-		if ( href.length && href.indexOf( 'wc-admin#/' ) <= 0 ) {
+		if ( ! href.startsWith( wcSettings.adminUrl ) ) {
 			event.preventDefault();
 			window.open( href, '_blank' );
 		}
