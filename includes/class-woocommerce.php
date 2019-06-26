698625ce1d08d31290c958357ad8caf683a2c0c2
Gerhard Potgieter <potgieterg@gmail.com>
2019-06-26 19:09:20 +0200

    WP & PHP Upgrade Notices (#23975)
    This commit introduces a banner that notifies and prompts users to upgrade their WordPress and/or PHP if they are outdated and not part of the minimum required versions going into WooCommerce 3.7.
    
    * Add backward compatible function wp_check_php_version for sites running WP version smaller than 5.1
    
    * Refactor some messaging, move widget to top of WC dashboard widgets, add buttons linking out to WP pages with instructions.
    
    * Add missing variables for adding nag class
    
    * Add styling to dashboard widget
    
    * Rework css to use exclamation instead
    
    * Clean up widget, make sure no widget is registered when dismissed. Add banner that links to widget and make it dismissable.
    
    * Update constant to reflect minimum requirements.
    
    * Don't show banner. Fix undefined variable.
    
    * Remove dashboard widget
    
    * Update banner link to docs site
    
    * Add link to docs page where instructions live for updating
    
    * Change button text
    
    * Add UTM params to docs link
    
    * Moved the msg logic from template to php file.
    
    * Bail out early when PHP and WP versions are ok.
    
    * Added missing translation functiog
    
    * Extracted notice function from reset_admin_notices.
    
    To follow suit with other similar functions in the file.
    
    * Synchronize fetching of WP version.
    
    * Removed unused functions.
    
    * Corrected indent.
    
    * Reduce number of variables and fixed coding standards
    
    * Updated wording of the notice.

--- includes/class-woocommerce.php
+++ includes/class-woocommerce.php
@@ -228,6 +228,8 @@ final class WooCommerce {
 		$this->define( 'WC_LOG_DIR', $upload_dir['basedir'] . '/wc-logs/' );
 		$this->define( 'WC_SESSION_CACHE_GROUP', 'wc_session_id' );
 		$this->define( 'WC_TEMPLATE_DEBUG_MODE', false );
+		$this->define( 'WC_MIN_PHP_VERSION', '5.6.20' );
+		$this->define( 'WC_MIN_WP_VERSION', '4.9' );
 	}
 
 	/**
