a99f88d51c796ee8eab5a360b0dcab2fb871e2f9
Vinicius <vkatatab@gmail.com>
2019-06-27 00:40:12 -0300

    payment

--- templates/checkout/payment.php
+++ templates/checkout/payment.php
@@ -48,12 +48,13 @@ if ( ! is_ajax() ) {
 
 		<?php do_action( 'woocommerce_review_order_before_submit' ); ?>
 
-		<?php echo apply_filters( 'woocommerce_order_button_html', '<button type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); // @codingStandardsIgnoreLine ?>
 
 		<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
 
 		<?php wp_nonce_field( 'woocommerce-process_checkout', 'woocommerce-process-checkout-nonce' ); ?>
 	</div>
+
+	<?php echo apply_filters( 'woocommerce_order_button_html', '<button type="submit" class="btn-block next-button button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); // @codingStandardsIgnoreLine ?>
 </div>
 <?php
 if ( ! is_ajax() ) {
