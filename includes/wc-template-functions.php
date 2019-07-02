692c22f9b57f001f6958cebbf08a53b4bee502ab
Natalya <Lady-Natalya@users.noreply.github.com>
2019-07-02 11:46:50 -0400

    Update wc-template-functions.php
    inputmode is not a valid attribute for the input element according to the current HTML standard
    
    templates/global/quantity-input.php already has type="number" on line 34 anyway

--- includes/wc-template-functions.php
+++ includes/wc-template-functions.php
@@ -1675,7 +1675,6 @@ if ( ! function_exists( 'woocommerce_quantity_input' ) ) {
 			'min_value'    => apply_filters( 'woocommerce_quantity_input_min', 0, $product ),
 			'step'         => apply_filters( 'woocommerce_quantity_input_step', 1, $product ),
 			'pattern'      => apply_filters( 'woocommerce_quantity_input_pattern', has_filter( 'woocommerce_stock_amount', 'intval' ) ? '[0-9]*' : '' ),
-			'inputmode'    => apply_filters( 'woocommerce_quantity_input_inputmode', has_filter( 'woocommerce_stock_amount', 'intval' ) ? 'numeric' : '' ),
 			'product_name' => $product ? $product->get_title() : '',
 		);
 
