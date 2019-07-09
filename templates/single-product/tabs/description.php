62a32e41d001a0c27f88786b5d83bd7ac0269dc4
Claudio Sanches <contato@claudiosanches.com>
2019-07-09 16:23:09 -0300

    Avoid extra escape

--- templates/single-product/tabs/description.php
+++ templates/single-product/tabs/description.php
@@ -19,7 +19,7 @@ defined( 'ABSPATH' ) || exit;
 
 global $post;
 
-$heading = apply_filters( 'woocommerce_product_description_heading', esc_html__( 'Description', 'woocommerce' ) );
+$heading = apply_filters( 'woocommerce_product_description_heading', __( 'Description', 'woocommerce' ) );
 
 ?>
 
