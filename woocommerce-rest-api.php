1b3ea5eef13afb15aef68dd5aff4b44052ff1fdf
Mike Jolley <mike.jolley@me.com>
2019-07-10 11:11:48 +0100

    Version

--- woocommerce-rest-api.php
+++ woocommerce-rest-api.php
@@ -5,7 +5,7 @@
  * Description: The WooCommerce core REST API, installed as a feature plugin for development and testing purposes. Requires WooCommerce 3.7+ and PHP 5.3+.
  * Author: Automattic
  * Author URI: https://woocommerce.com
- * Version: 1.0.0
+ * Version: 1.0.2
  * Requires PHP: 5.6
  * License: GPLv3
  *
