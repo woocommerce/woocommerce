cfc30283829f2ddf5ebc63382b461d826707dfc5
Mike Jolley <mike.jolley@me.com>
2019-07-10 11:38:03 +0100

    Rolling 2.3 release changelog (https://github.com/woocommerce/woocommerce-blocks/pull/672)
    * changelog
    
    * Updated changelog
    
    * featured category block readme
    
    * readme
    
    * version bump

--- plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
+++ plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
@@ -3,12 +3,12 @@
  * Plugin Name: WooCommerce Blocks
  * Plugin URI: https://github.com/woocommerce/woocommerce-gutenberg-products-block
  * Description: WooCommerce blocks for the Gutenberg editor.
- * Version: 2.3.0-dev
+ * Version: 2.3.0-beta
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain:  woo-gutenberg-products-block
  * WC requires at least: 3.6
- * WC tested up to: 3.6
+ * WC tested up to: 3.7
  *
  * @package WooCommerce\Blocks
  * @internal This file is only used when running the REST API as a feature plugin.
