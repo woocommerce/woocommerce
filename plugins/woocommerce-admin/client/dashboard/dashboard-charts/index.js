c820c92fe41b73e0733d0686083e5a47b282ca14
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-07-11 08:55:52 -0600

    Send a tracks event when the dashboard chart type is toggled.

--- plugins/woocommerce-admin/client/dashboard/dashboard-charts/index.js
+++ plugins/woocommerce-admin/client/dashboard/dashboard-charts/index.js
@@ -43,6 +43,7 @@ class DashboardCharts extends Component {
 				[ 'dashboard_chart_type' ]: chartType,
 			};
 			this.props.updateCurrentUserData( userDataFields );
+			recordEvent( 'dash_charts_type_toggle', { chart_type: chartType } );
 		};
 	}
 
