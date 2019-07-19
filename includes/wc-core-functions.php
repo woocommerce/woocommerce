fd42c439f91cc9714542843ba1bf5b5f150e4b85
Claudio Sanches <contato@claudiosanches.com>
2019-07-18 23:34:13 -0300

    Prevent run wc_load_cart() before woocommerce_loaded

--- includes/wc-core-functions.php
+++ includes/wc-core-functions.php
@@ -2263,6 +2263,12 @@ function wc_get_server_database_version() {
  * @return void
  */
 function wc_load_cart() {
+	if ( ! did_action( 'woocommerce_loaded' ) ) {
+		/* translators: 1: wc_get_product 2: woocommerce_init */
+		wc_doing_it_wrong( __FUNCTION__, sprintf( __( '%1$s should not be called before the %2$s action.', 'woocommerce' ), 'wc_load_cart', 'woocommerce_init' ), '3.7' );
+		return;
+	}
+
 	WC()->initialize_session();
 	WC()->initialize_cart();
 }
