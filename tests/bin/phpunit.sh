80f1e2fba261f5656afbd73cb7dc63ca58ca63cc
Claudio Sanches <contato@claudiosanches.com>
2019-07-18 23:01:14 -0300

    Remove all support for PHP 5.2 in Travis

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -6,8 +6,6 @@ fi
 
 if [[ ${RUN_CODE_COVERAGE} == 1 ]]; then
 	phpdbg -qrr $HOME/.composer/vendor/bin/phpunit -d memory_limit=-1 -c phpunit.xml --coverage-clover=coverage.clover --exclude-group=timeout $@
-elif [[ ${TRAVIS_PHP_VERSION:0:3} != "5.2" ]]; then
-	$HOME/.composer/vendor/bin/phpunit -c phpunit.xml $@
 else
-	phpunit -c phpunit.xml $@
+	$HOME/.composer/vendor/bin/phpunit -c phpunit.xml $@
 fi
