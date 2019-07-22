3a781d1af0a7887363831e233c77c2c2c4e31efc
Timmy Crawford <timmyc@users.noreply.github.com>
2019-07-22 14:15:41 -0700

    Settings: Fix notice shown after save. (https://github.com/woocommerce/woocommerce-admin/pull/2660)
    * Settings: Fix notice shown after save.
    
    * Updates per feedback.

--- plugins/woocommerce-admin/client/wc-api/settings/selectors.js
+++ plugins/woocommerce-admin/client/wc-api/settings/selectors.js
@@ -10,6 +10,7 @@ import { isNil } from 'lodash';
  */
 import { DEFAULT_REQUIREMENT } from '../constants';
 import { getResourceName } from '../utils';
+import { analyticsSettings } from 'analytics/settings/config';
 
 const getSettings = ( getResource, requireResource ) => (
 	group,
@@ -36,7 +37,26 @@ const isGetSettingsRequesting = getResource => group => {
 		return true;
 	}
 
-	return lastRequested > lastReceived;
+	// This selector is used by other "groups", so return early if not looking at wc_admin settings.
+	if ( 'wc_admin' !== group ) {
+		return lastRequested > lastReceived;
+	}
+
+	// Mutation operations for `wc_admin` settings update a different resource (batch endpoint) in fresh-data.
+	// As such we must use lastReceived stamp from that resource to properly compare lastRequested to lastReceived.
+	const settingName = analyticsSettings.length
+		? analyticsSettings[ 0 ].name
+		: 'woocommerce_actionable_order_statuses';
+	const { lastReceived: lastMutationReceived } = getResource(
+		getResourceName( 'settings/wc_admin', settingName )
+	);
+
+	// If we don't have a lastReceived on mutations, use the standard resource times.
+	if ( isNil( lastMutationReceived ) ) {
+		return lastRequested > lastReceived;
+	}
+
+	return lastRequested > lastMutationReceived;
 };
 
 export default {
