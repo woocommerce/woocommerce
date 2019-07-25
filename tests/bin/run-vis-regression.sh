af39fb7e5f6a475542ab4a604e8a00e2275d1861
Gerhard <potgieterg@gmail.com>
2019-07-25 13:12:24 +0200

    Hook up new run-vis-regression.sh script, do not run unit tests on vis regression tests

---
+++ tests/bin/run-vis-regression.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [[ ${RUN_E2E} == 1 ]]; then
+
+	WP_SITE_URL="http://localhost:8080"
+
+	# Setup enviromental variables
+	export BASE_URL="$WP_SITE_URL"
+
+	# Run the tests
+	npm test:vis-regression
+fi
