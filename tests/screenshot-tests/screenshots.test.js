e0371b70e8ea87b944d9a4682289ff7859848568
Gerhard <potgieterg@gmail.com>
2019-07-26 09:36:30 +0200

    Make config flexible to work on local and travis

--- tests/screenshot-tests/screenshots.test.js
+++ tests/screenshot-tests/screenshots.test.js
@@ -1,9 +1,11 @@
-const path = require('path');
+const path    = require('path');
+const config  = require('config');
+const baseUrl = config.get('url');
 
 describe('woocommerce screenshot tests', () => {
 
 	it('test my account page', async () => {
-		await page.goto('https://wc.local/my-account/');
+		await page.goto(baseUrl+'/my-account/');
 
 		const imagePath = path.resolve(__dirname, './master-screenshots', 'my-account-logged-out.png');
 		const image = await page.screenshot({path: imagePath});
