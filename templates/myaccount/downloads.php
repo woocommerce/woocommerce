1b4ac250a779b2131afadeccb347c21c0e27465b
Claudio Sanches <contato@claudiosanches.com>
2019-08-01 19:12:29 -0300

    Change wording to "Browse products"

--- templates/myaccount/downloads.php
+++ templates/myaccount/downloads.php
@@ -37,7 +37,7 @@ do_action( 'woocommerce_before_account_downloads', $has_downloads ); ?>
 <?php else : ?>
 	<div class="woocommerce-Message woocommerce-Message--info woocommerce-info">
 		<a class="woocommerce-Button button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-			<?php esc_html_e( 'Go to the shop', 'woocommerce' ); ?>
+			<?php esc_html_e( 'Browse products', 'woocommerce' ); ?>
 		</a>
 		<?php esc_html_e( 'No downloads available yet.', 'woocommerce' ); ?>
 	</div>
