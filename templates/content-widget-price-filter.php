802002a10aa4691c1f98b24b69c4311aa049dd4a
Luis <CasperBraske@users.noreply.github.com>
2019-08-02 11:31:11 -0300

    Adding the "translators" comment.

--- templates/content-widget-price-filter.php
+++ templates/content-widget-price-filter.php
@@ -26,6 +26,7 @@ defined( 'ABSPATH' ) || exit;
 		<div class="price_slider_amount" data-step="<?php echo esc_attr( $step ); ?>">
 			<input type="text" id="min_price" name="min_price" value="<?php echo esc_attr( $current_min_price ); ?>" data-min="<?php echo esc_attr( $min_price ); ?>" placeholder="<?php echo esc_attr__( 'Min price', 'woocommerce' ); ?>" />
 			<input type="text" id="max_price" name="max_price" value="<?php echo esc_attr( $current_max_price ); ?>" data-max="<?php echo esc_attr( $max_price ); ?>" placeholder="<?php echo esc_attr__( 'Max price', 'woocommerce' ); ?>" />
+			<?php /* translators: Filter: verb "to filter" */ ?>
 			<button type="submit" class="button"><?php echo esc_html__( 'Filter', 'woocommerce' ); ?></button>
 			<div class="price_label" style="display:none;">
 				<?php echo esc_html__( 'Price:', 'woocommerce' ); ?> <span class="from"></span> &mdash; <span class="to"></span>
