833cc7a1c273db638517f6c6704d783987e341f9
Ron Rennick <ron@ronandandrea.com>
2019-08-07 16:12:47 -0300

    convert zero last active date to null in customer query

--- plugins/woocommerce-admin/includes/data-stores/class-wc-admin-reports-customers-data-store.php
+++ plugins/woocommerce-admin/includes/data-stores/class-wc-admin-reports-customers-data-store.php
@@ -48,7 +48,7 @@ class WC_Admin_Reports_Customers_Data_Store extends WC_Admin_Reports_Data_Store 
 		'state'            => 'state',
 		'postcode'         => 'postcode',
 		'date_registered'  => 'date_registered',
-		'date_last_active' => 'date_last_active',
+		'date_last_active' => 'IF( date_last_active < 1, NULL, date_last_active ) AS date_last_active',
 		'orders_count'     => 'SUM( CASE WHEN parent_id = 0 THEN 1 ELSE 0 END ) as orders_count',
 		'total_spend'      => 'SUM( gross_total ) as total_spend',
 		'avg_order_value'  => '( SUM( gross_total ) / COUNT( order_id ) ) as avg_order_value',
@@ -391,7 +391,6 @@ class WC_Admin_Reports_Customers_Data_Store extends WC_Admin_Reports_Data_Store 
 			if ( $query_args['page'] < 1 || $query_args['page'] > $total_pages ) {
 				return $data;
 			}
-
 			$customer_data = $wpdb->get_results(
 				"SELECT
 						{$selections}
