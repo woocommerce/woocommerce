aae802f31993598103f81fb837bede17a1776a99
Akeda Bagus <akeda.bagus@automattic.com>
2019-08-17 00:36:28 +0700

    Add support to allow in-app purchase installation after connected.

--- includes/admin/class-wc-admin-addons.php
+++ includes/admin/class-wc-admin-addons.php
@@ -487,9 +487,10 @@ class WC_Admin_Addons {
 		// so WCCOM "back" link returns user to where they were.
 		$back_admin_path = add_query_arg( array() );
 		return array(
-			'wccom-site'        => site_url(),
-			'wccom-back'        => esc_url( $back_admin_path ),
-			'wccom-woo-version' => WC_VERSION,
+			'wccom-site'         => site_url(),
+			'wccom-back'         => esc_url( $back_admin_path ),
+			'wccom-woo-version'  => WC_VERSION,
+			'wccom-helper-nonce' => wp_create_nonce( 'connect' ),
 		);
 	}
 
