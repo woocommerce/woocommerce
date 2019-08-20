58a1e7b84cf5b63b90ec525902a6153116ffa775
Isaac Lubow <mail@isaaclubow.com>
2019-08-20 11:00:20 -0700

    Update cart.php
    Store `cart->get_cart()` in a variable instead of evaluating it on every iteration of the foreach.

--- templates/cart/cart.php
+++ templates/cart/cart.php
@@ -37,7 +37,8 @@ do_action( 'woocommerce_before_cart' ); ?>
 			<?php do_action( 'woocommerce_before_cart_contents' ); ?>
 
 			<?php
-			foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+			$cart_items = WC()->cart->get_cart();
+			foreach ( $cart_items as $cart_item_key => $cart_item ) {
 				$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 				$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 
