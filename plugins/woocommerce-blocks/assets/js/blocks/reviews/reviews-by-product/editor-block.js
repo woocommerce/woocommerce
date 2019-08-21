f5b1d022889b69e09f3127d74df5ebc79015a8dd
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-08-21 09:48:22 +0200

    Fix error appearing on Reviews by Product when there were no reviews (https://github.com/woocommerce/woocommerce-blocks/pull/884)
    * Fix error appearing on Reviews by Product when there were no reviews
    
    * Revert "Fix error appearing on Reviews by Product when there were no reviews"
    
    This reverts commit 73e95b39aa9a318888d08bb51faeff8bb51259d7.
    
    * Move withProduct() HOC to editor-block.js
    
    * Revert "Move withProduct() HOC to editor-block.js"
    
    This reverts commit ae9515792d4870cc5cf1d92a2d6acc91198b5f62.
    
    * Move renderNoReviews back to edit.js
    
    * Move no reviews placeholder to its own component
    
    * Remove usage of 'RawHTML'
    
    * Fix propTypes
    
    * Remove unnecessary escapeHTML

--- plugins/woocommerce-blocks/assets/js/blocks/reviews/reviews-by-product/editor-block.js
+++ plugins/woocommerce-blocks/assets/js/blocks/reviews/reviews-by-product/editor-block.js
@@ -1,12 +1,11 @@
 /**
  * External dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { debounce } from 'lodash';
-import { escapeHTML } from '@wordpress/escape-html';
-import { Disabled, Placeholder } from '@wordpress/components';
+import { Disabled } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -16,7 +15,7 @@ import LoadMoreButton from '../../../base/components/load-more-button';
 import ReviewList from '../../../base/components/review-list';
 import ReviewOrderSelect from '../../../base/components/review-order-select';
 import withComponentId from '../../../base/hocs/with-component-id';
-import { IconReviewsByProduct } from '../../../components/icons';
+import NoReviewsPlaceholder from './no-reviews-placeholder.js';
 import { ENABLE_REVIEW_RATING } from '../../../constants';
 /**
  * Block rendered in the editor.
@@ -69,34 +68,12 @@ class EditorBlock extends Component {
 		} );
 	}
 
-	renderNoReviews() {
-		const { attributes } = this.props;
-		const { product } = attributes;
-		return (
-			<Placeholder
-				className="wc-block-reviews-by-product"
-				icon={ <IconReviewsByProduct className="block-editor-block-icon" /> }
-				label={ __( 'Reviews by Product', 'woo-gutenberg-products-block' ) }
-			>
-				<div dangerouslySetInnerHTML={ {
-					__html: sprintf(
-						__(
-							"This block lists reviews for a selected product. %s doesn't have any reviews yet, but they will show up here when it does.",
-							'woo-gutenberg-products-block'
-						),
-						'<strong>' + escapeHTML( product.name ) + '</strong>'
-					),
-				} } />
-			</Placeholder>
-		);
-	}
-
 	render() {
 		const { attributes, componentId } = this.props;
 		const { reviews, totalReviews, isLoading } = this.state;
 
 		if ( 0 === reviews.length && ! isLoading ) {
-			return this.renderNoReviews();
+			return <NoReviewsPlaceholder attributes={ attributes } />;
 		}
 
 		return (
