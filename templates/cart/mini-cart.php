36bd34ec681999f54fc006bbce298a0f41c38c05
Yash Kapoor <52190071+yash-webkul@users.noreply.github.com>
2019-08-28 01:50:14 +0530

    Update mini-cart.php (#24465)
    * Update mini-cart.php
    
    Fixed spelling of woocommerce_widget_shopping_cart_total
    
    * Update mini-cart.php

--- templates/cart/mini-cart.php
+++ templates/cart/mini-cart.php
@@ -73,7 +73,7 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 	<p class="woocommerce-mini-cart__total total">
 		<?php
 		/**
-		 * Woocommerce_widget_shopping_cart_total hook.
+		 * Hook: woocommerce_widget_shopping_cart_total.
 		 *
 		 * @hooked woocommerce_widget_shopping_cart_subtotal - 10
 		 */
