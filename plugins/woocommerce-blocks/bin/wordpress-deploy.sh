d2e8db8378d20691e07a71d0d491f0c01521aa0a
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-08-27 10:56:59 +0200

    Exclude bundlesize config file from ZIP (https://github.com/woocommerce/woocommerce-blocks/pull/910)

--- plugins/woocommerce-blocks/bin/wordpress-deploy.sh
+++ plugins/woocommerce-blocks/bin/wordpress-deploy.sh
@@ -57,6 +57,7 @@ copy_dest_files() {
 	--exclude=renovate.json \
 	--exclude="*.config.js" \
 	--exclude="*-config.js" \
+	--exclude="*.config.json" \
 	--exclude=package.json \
 	--exclude=package-lock.json \
 	--exclude=none
