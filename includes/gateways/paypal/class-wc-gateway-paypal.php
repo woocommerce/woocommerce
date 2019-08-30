e7b8e3ed7be8134183a33a72fa57f03cf1447667
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-08-30 10:01:55 -0300

    Fix typo in order note
    PR #24501 fixed a typo in a order note string. This commit fix the same
    problem in another place where the same string is used.

--- includes/gateways/paypal/class-wc-gateway-paypal.php
+++ includes/gateways/paypal/class-wc-gateway-paypal.php
@@ -408,7 +408,7 @@ class WC_Gateway_Paypal extends WC_Payment_Gateway {
 			if ( is_wp_error( $result ) ) {
 				$this->log( 'Capture Failed: ' . $result->get_error_message(), 'error' );
 				/* translators: %s: Paypal gateway error message */
-				$order->add_order_note( sprintf( __( 'Payment could not captured: %s', 'woocommerce' ), $result->get_error_message() ) );
+				$order->add_order_note( sprintf( __( 'Payment could not be captured: %s', 'woocommerce' ), $result->get_error_message() ) );
 				return;
 			}
 
