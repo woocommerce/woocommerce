545f5a5f0a5dba41fc8908b3a72b572ba97dd094
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-09-04 15:18:07 +0200

    Add Product Search Icon (https://github.com/woocommerce/woocommerce-blocks/pull/944)

--- plugins/woocommerce-blocks/assets/js/components/icons/index.js
+++ plugins/woocommerce-blocks/assets/js/components/icons/index.js
@@ -4,6 +4,7 @@ export { default as IconCheckChecked } from './checkbox-checked';
 export { default as IconCheckUnchecked } from './checkbox-unchecked';
 export { default as IconFolder } from './folder';
 export { default as IconFolderStar } from './folder-star';
+export { default as IconProductSearch } from './product-search';
 export { default as IconNewReleases } from './new-releases';
 export { default as IconRadioSelected } from './radio-selected';
 export { default as IconRadioUnselected } from './radio-unselected';
