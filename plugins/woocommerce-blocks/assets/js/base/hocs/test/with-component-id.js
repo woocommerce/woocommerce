e4ff6eb606962e8dbe85c0d1e13b2c7fd4c7bf74
Darren Ethier <darren@roughsmootheng.in>
2019-09-07 08:04:34 -0400

    Fix increment bug in withComponentId (https://github.com/woocommerce/woocommerce-blocks/pull/951)
    * fix bug with component and add test coverage
    
    * code style spacing on doc block
    
    * fix prettier config

---
+++ plugins/woocommerce-blocks/assets/js/base/hocs/test/with-component-id.js
@@ -0,0 +1,33 @@
+/**
+ * External dependencies
+ */
+import TestRenderer from 'react-test-renderer';
+
+import withComponentId from '../with-component-id';
+
+const TestComponent = withComponentId( ( props ) => {
+	return <div componentId={ props.componentId } />;
+} );
+
+const render = () => {
+	return TestRenderer.create( <TestComponent /> );
+};
+
+describe( 'withComponentId Component', () => {
+	let renderer;
+	it( 'initializes with expected id on initial render', () => {
+		renderer = render();
+		const props = renderer.root.findByType( 'div' ).props;
+		expect( props.componentId ).toBe( 0 );
+	} );
+	it( 'does not increment on re-render', () => {
+		renderer.update( <TestComponent someValue={ 3 } /> );
+		const props = renderer.root.findByType( 'div' ).props;
+		expect( props.componentId ).toBe( 0 );
+	} );
+	it( 'increments on a new component instance', () => {
+		renderer.update( <TestComponent key={ 42 } /> );
+		const props = renderer.root.findByType( 'div' ).props;
+		expect( props.componentId ).toBe( 1 );
+	} );
+} );
--- plugins/woocommerce-blocks/assets/js/base/hocs/with-component-id.js
+++ plugins/woocommerce-blocks/assets/js/base/hocs/with-component-id.js
@@ -1,34 +1,28 @@
 import { Component } from 'react';
 
-const ids = [];
-
 /**
  * HOC that gives a component a unique ID.
  *
- * This is an alternative for withInstanceId from @wordpress/compose to avoid using that dependency on the frontend.
+ * This is an alternative for withInstanceId from @wordpress/compose to avoid
+ * using that dependency on the frontend.
  */
 const withComponentId = ( OriginalComponent ) => {
-	return class WrappedComponent extends Component {
-		generateUniqueID() {
-			const group = WrappedComponent.name;
+	let instances = 0;
 
-			if ( ! ids[ group ] ) {
-				ids[ group ] = 0;
-			}
-
-			ids[ group ]++;
-
-			return ids[ group ];
-		}
+	class WrappedComponent extends Component {
+		instanceId = instances++;
 
 		render() {
-			const componentId = this.generateUniqueID();
-
 			return (
-				<OriginalComponent { ...this.props } componentId={ componentId } />
+				<OriginalComponent
+					{ ...this.props }
+					componentId={ this.instanceId }
+				/>
 			);
 		}
-	};
+	}
+	WrappedComponent.displayName = 'withComponentId';
+	return WrappedComponent;
 };
 
 export default withComponentId;
