9d2b45b4716a5596227ea3fcefa6ff28fd3f1410
Tom Anderson <twocs@users.noreply.github.com>
2019-09-13 13:03:09 +0930

    use isset for data['payment_method']
    Some create_order calls in the backend, for example using WooCommerce Deposits, may not have a value for payment_method. For example, an order may not require payment. This triggers a PHP Notice. 
    
    I see that isset is used for other times that $data['payment_method'] is used, even in this file in the function get_posted_data() : 646.

--- includes/class-wc-checkout.php
+++ includes/class-wc-checkout.php
@@ -376,7 +376,11 @@ class WC_Checkout {
 			$order->set_customer_ip_address( WC_Geolocation::get_ip_address() );
 			$order->set_customer_user_agent( wc_get_user_agent() );
 			$order->set_customer_note( isset( $data['order_comments'] ) ? $data['order_comments'] : '' );
-			$order->set_payment_method( isset( $available_gateways[ $data['payment_method'] ] ) ? $available_gateways[ $data['payment_method'] ] : $data['payment_method'] );
+			if ( isset( $data['payment_method'] ) ) {
+				$order->set_payment_method( isset( $available_gateways[ $data['payment_method'] ] ) ? $available_gateways[ $data['payment_method'] ] : $data['payment_method'] );
+			} else {
+				$order->set_payment_method( '' );
+			}
 			$order->set_shipping_total( WC()->cart->get_shipping_total() );
 			$order->set_discount_total( WC()->cart->get_discount_total() );
 			$order->set_discount_tax( WC()->cart->get_discount_tax() );
