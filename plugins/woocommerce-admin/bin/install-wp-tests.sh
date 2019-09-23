307f3e12828c57058c96449ad5ccab0542ee71a4
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-09-23 15:20:19 -0700

    Fix pull request branch detection on CI (https://github.com/woocommerce/woocommerce-admin/pull/2942)
    * Only build pushes to master branch.
    
    * Fix branch detection in CI.

--- plugins/woocommerce-admin/bin/install-wp-tests.sh
+++ plugins/woocommerce-admin/bin/install-wp-tests.sh
@@ -163,7 +163,7 @@ install_deps() {
 	REPO=$TRAVIS_REPO_SLUG
 	WORKING_DIR="$PWD"
 
-	if [ "$TRAVIS_PULL_REQUEST_BRANCH" != "" ]; then
+	if [ "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
 		BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
 		REPO=$TRAVIS_PULL_REQUEST_SLUG
 	fi
@@ -188,7 +188,7 @@ install_deps() {
 	cd "$WP_CORE_DIR"
 	php wp-cli.phar plugin activate woocommerce
 
-	if [ "$TRAVIS_PULL_REQUEST_BRANCH" != "" ]; then
+	if [ "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
 		BRANCH="$(sed 's/#/%23/' <<<$BRANCH)"
 		# Install woocommerce-admin, the correct branch, if running from Travis CI.
 		php wp-cli.phar plugin install https://github.com/$REPO/archive/$BRANCH.zip --activate
