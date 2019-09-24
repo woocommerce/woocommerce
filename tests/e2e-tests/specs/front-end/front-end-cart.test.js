409775287e7ee8d53b637827758ea7065acd7e42
Julia Amosova <julia.amosova@gmail.com>
2019-09-24 12:56:02 +0100

    Add new Puppeteer front-end e2e test: cart page

---
+++ tests/e2e-tests/specs/front-end/front-end-cart.test.js
@@ -0,0 +1,86 @@
+/**
+ * @format
+ */
+
+/**
+ * External dependencies
+ */
+import { activatePlugin } from '@wordpress/e2e-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import { createSimpleProduct } from '../../utils/components';
+import { CustomerFlow, StoreOwnerFlow } from '../../utils/flows';
+import { uiUnblocked } from '../../utils';
+
+describe( 'Cart page', () => {
+	beforeAll( async () => {
+		await activatePlugin( 'woocommerce' );
+		await createSimpleProduct();
+		await StoreOwnerFlow.logout();
+	} );
+
+	it( 'should displays no item in the cart', async () => {
+		await CustomerFlow.goToCart();
+		await expect( page ).toMatchElement( '.cart-empty', { text: 'Your cart is currently empty.' } );
+	} );
+
+	it( 'should adds the product to the cart when "Add to cart" is clicked', async () => {
+		await CustomerFlow.goToShop();
+		await CustomerFlow.addToCartFromShopPage( 'Simple product' );
+
+		await CustomerFlow.goToCart();
+		await CustomerFlow.productIsInCart( 'Simple product' );
+	} );
+
+	it( 'should increases item qty when "Add to cart" of the same product is clicked', async () => {
+		await CustomerFlow.goToShop();
+		await CustomerFlow.addToCartFromShopPage( 'Simple product' );
+
+		await CustomerFlow.goToCart();
+		await CustomerFlow.productIsInCart( 'Simple product', 2 );
+	} );
+
+	it( 'should updates qty when updated via qty input', async () => {
+		await CustomerFlow.goToCart();
+		await CustomerFlow.setCartQuantity( 'Simple product', 4 );
+		await expect( page ).toClick( 'button', { text: 'Update cart' } );
+		await uiUnblocked();
+
+		await CustomerFlow.productIsInCart( 'Simple product', 4 );
+	} );
+
+	it( 'should remove the item from the cart when remove is clicked', async () => {
+		await CustomerFlow.goToCart();
+		await CustomerFlow.removeFromCart( 'Simple product' );
+		await uiUnblocked();
+
+		await expect( page ).toMatchElement( '.cart-empty', { text: 'Your cart is currently empty.' } );
+	} );
+
+	it( 'should update subtotal in cart totals when adding product to the cart', async () => {
+		await CustomerFlow.goToShop();
+		await CustomerFlow.addToCartFromShopPage( 'Simple product' );
+
+		await CustomerFlow.goToCart();
+		await CustomerFlow.productIsInCart( 'Simple product', 1 );
+		await expect( page ).toMatchElement( '.cart-subtotal .amount', { text: '$9.99' } );
+
+		await CustomerFlow.setCartQuantity( 'Simple product', 2 );
+		await expect( page ).toClick( 'button', { text: 'Update cart' } );
+		await uiUnblocked();
+
+		await expect( page ).toMatchElement( '.cart-subtotal .amount', { text: '$19.98' } );
+	} );
+
+	it( 'should go to the checkout page when "Proceed to Checkout" is clicked', async () => {
+		await CustomerFlow.goToCart();
+		await Promise.all( [
+			page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+			expect( page ).toClick( '.checkout-button', { text: 'Proceed to checkout' } ),
+		] );
+
+		await expect( page ).toMatchElement( '#order_review' );
+	} );
+} );
--- tests/e2e-tests/utils/flows.js
+++ tests/e2e-tests/utils/flows.js
@@ -2,11 +2,116 @@
  * @format
  */
 
+/**
+ * External dependencies
+ */
+import { pressKeyWithModifier } from '@wordpress/e2e-test-utils';
+
 const baseUrl = process.env.WP_BASE_URL;
 
 const WP_ADMIN_NEW_PRODUCT = baseUrl + '/wp-admin/post-new.php?post_type=product';
 
+const SHOP_PAGE = baseUrl + '/shop/';
+const SHOP_CART_PAGE = baseUrl + '/cart/';
+
+const getProductColumnExpression = ( productTitle ) => (
+	'td[@class="product-name" and ' +
+	`a[contains(text(), "${ productTitle }")]` +
+	']'
+);
+
+const getQtyColumnExpression = ( args ) => (
+	'td[@class="product-quantity" and ' +
+	'.//' + getQtyInputExpression( args ) +
+	']'
+);
+
+const getQtyInputExpression = ( args = {} ) => {
+	let qtyValue = '';
+
+	if ( args.checkQty ) {
+		qtyValue = ` and @value="${ args.qty }"`;
+	}
+
+	return 'input[contains(@class, "input-text")' + qtyValue + ']';
+};
+
+const getCartItemExpression = ( productTitle, args ) => (
+	'//tr[contains(@class, "cart_item") and ' +
+	getProductColumnExpression( productTitle ) +
+	' and ' +
+	getQtyColumnExpression( args ) +
+	']'
+);
+
+const getRemoveExpression = () => (
+	'td[@class="product-remove"]//a[@class="remove"]'
+);
+
+
+const CustomerFlow = {
+	addToCartFromShopPage: async ( productTitle ) => {
+		const addToCartXPath = `//li[contains(@class, "type-product") and a/h2[contains(text(), "${ productTitle }")]]` +
+			'//a[contains(@class, "add_to_cart_button") and contains(@class, "ajax_add_to_cart")';
+
+		const [ addToCartButton ] = await page.$x( addToCartXPath + ']' );
+		addToCartButton.click();
+
+		await page.waitFor( addToCartXPath + ' and contains(@class, "added")]' );
+	},
+
+	removeFromCart: async ( productTitle ) => {
+		const cartItemXPath = getCartItemExpression( productTitle );
+		const removeItemXPath = cartItemXPath + '//' + getRemoveExpression();
+
+		const [ removeButton ] = await page.$x( removeItemXPath );
+		await removeButton.click();
+	},
+
+	goToCart: async () => {
+		await page.goto( SHOP_CART_PAGE, {
+			waitUntil: 'networkidle0',
+		} );
+	},
+
+	goToShop: async () => {
+		await page.goto( SHOP_PAGE, {
+			waitUntil: 'networkidle0',
+		} );
+	},
+
+	productIsInCart: async ( productTitle, quantity = null ) => {
+		const cartItemArgs = quantity ? { qty: quantity } : {};
+		const cartItemXPath = getCartItemExpression( productTitle, cartItemArgs );
+
+		await expect( page.$x( cartItemXPath ) ).resolves.toHaveLength( 1 );
+	},
+
+	setCartQuantity: async ( productTitle, quantityValue ) => {
+		const cartItemXPath = getCartItemExpression( productTitle );
+		const quantityInputXPath = cartItemXPath + '//' + getQtyInputExpression();
+
+		const [ quantityInput ] = await page.$x( quantityInputXPath );
+		await quantityInput.focus();
+		await pressKeyWithModifier( 'primary', 'a' );
+		await quantityInput.type( quantityValue.toString() );
+	},
+};
+
 const StoreOwnerFlow = {
+	logout: async () => {
+		await page.goto( baseUrl + '/wp-login.php?action=logout', {
+			waitUntil: 'networkidle0',
+		} );
+
+		await expect( page ).toMatch( 'You are attempting to log out' );
+
+		await Promise.all( [
+			page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+			page.click( 'a' ),
+		] );
+	},
+
 	openNewProduct: async () => {
 		await page.goto( WP_ADMIN_NEW_PRODUCT, {
 			waitUntil: 'networkidle0',
@@ -14,4 +119,4 @@ const StoreOwnerFlow = {
 	},
 };
 
-export { StoreOwnerFlow };
+export { CustomerFlow, StoreOwnerFlow };
