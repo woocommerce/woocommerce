417a7c0259b0c0fad97085e0e8cbf0947a2b9f0f
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-09-26 14:26:09 -0300

    Bump template version

--- templates/single-product/tabs/tabs.php
+++ templates/single-product/tabs/tabs.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 2.4.0
+ * @version 3.8.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
