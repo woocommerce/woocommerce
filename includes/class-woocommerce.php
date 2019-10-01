414d31d1917e0396e2cbaf99082d380b7298fd36
Gerhard <potgieterg@gmail.com>
2019-10-01 12:21:51 +0200

    Bump PHP recommended version to 7.0 and WP recommended version to 5.0

--- includes/class-woocommerce.php
+++ includes/class-woocommerce.php
@@ -229,8 +229,8 @@ final class WooCommerce {
 		$this->define( 'WC_LOG_DIR', $upload_dir['basedir'] . '/wc-logs/' );
 		$this->define( 'WC_SESSION_CACHE_GROUP', 'wc_session_id' );
 		$this->define( 'WC_TEMPLATE_DEBUG_MODE', false );
-		$this->define( 'WC_NOTICE_MIN_PHP_VERSION', '5.6.20' );
-		$this->define( 'WC_NOTICE_MIN_WP_VERSION', '4.9' );
+		$this->define( 'WC_NOTICE_MIN_PHP_VERSION', '7.0' );
+		$this->define( 'WC_NOTICE_MIN_WP_VERSION', '5.0' );
 	}
 
 	/**
