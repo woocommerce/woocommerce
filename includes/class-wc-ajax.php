b9bb4a7c6a55c04e6c177799b652aee996d969f8
Timothy Jacobs <timothy@ironbounddesigns.com>
2019-10-02 15:01:09 -0400

    Use a separate first parameter for the WP_Error
    This matches the pattern used elsewhere in WooCommerce and allows access to the original `WC_Product` object if an error is to be returned.

--- includes/class-wc-ajax.php
+++ includes/class-wc-ajax.php
@@ -898,10 +898,11 @@ class WC_AJAX {
 				if ( ! $product ) {
 					throw new Exception( __( 'Invalid product ID', 'woocommerce' ) . ' ' . $product_id );
 				}
-				$check = apply_filters( 'woocommerce_ajax_add_order_item_validation', $product, $order, $qty );
+				$validation_error = new WP_Error();
+				$validation_error = apply_filters( 'woocommerce_ajax_add_order_item_validation', $validation_error, $product, $order, $qty );
 
-				if ( is_wp_error( $check ) ) {
-					throw new Exception( $check->get_error_message() );
+				if ( $validation_error->get_error_code() ) {
+					throw new Exception( '<strong>' . __( 'Error:', 'woocommerce' ) . '</strong> ' . $validation_error->get_error_message() );
 				}
 				$item_id                 = $order->add_product( $product, $qty );
 				$item                    = apply_filters( 'woocommerce_ajax_order_item', $order->get_item( $item_id ), $item_id, $order, $product );
