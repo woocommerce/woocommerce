83653650377342a3976936b1a807c7966edf5d08
Nico Mollet <nico.mollet@gmail.com>
2019-10-02 13:42:26 +0200

    Removed space at the end of the line

--- includes/admin/meta-boxes/class-wc-meta-box-coupon-data.php
+++ includes/admin/meta-boxes/class-wc-meta-box-coupon-data.php
@@ -116,7 +116,7 @@ class WC_Meta_Box_Coupon_Data {
 						'value'             => esc_attr( $expiry_date ),
 						'label'             => __( 'Coupon expiry date', 'woocommerce' ),
 						'placeholder'       => 'YYYY-MM-DD',
-						'description'       => __( 'The coupon will expire at 00:00:00 of this date.', 'woocommerce' ), 
+						'description'       => __( 'The coupon will expire at 00:00:00 of this date.', 'woocommerce' ),
 						'desc_tip'          => true,
 						'class'             => 'date-picker',
 						'custom_attributes' => array(
