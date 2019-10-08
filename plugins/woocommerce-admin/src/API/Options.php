37fbdaf5c0a4fa4cbdd6d9ba072e7961a88bf311
Joshua T Flowers <joshuatf@gmail.com>
2019-10-09 06:39:40 +0800

    Add options REST API permission checks (https://github.com/woocommerce/woocommerce-admin/pull/3004)

--- plugins/woocommerce-admin/src/API/Options.php
+++ plugins/woocommerce-admin/src/API/Options.php
@@ -43,7 +43,7 @@ class Options extends \WC_REST_Data_Controller {
 				array(
 					'methods'             => \WP_REST_Server::READABLE,
 					'callback'            => array( $this, 'get_options' ),
-					'permission_callback' => array( $this, 'update_item_permissions_check' ),
+					'permission_callback' => array( $this, 'get_item_permissions_check' ),
 				),
 				'schema' => array( $this, 'get_item_schema' ),
 			)
@@ -64,16 +64,84 @@ class Options extends \WC_REST_Data_Controller {
 	}
 
 	/**
-	 * Check if a given request has access to manage options.
+	 * Check if a given request has access to get options.
+	 *
+	 * @param  WP_REST_Request $request Full details about the request.
+	 * @return WP_Error|boolean
+	 */
+	public function get_item_permissions_check( $request ) {
+		$params = explode( ',', $request['options'] );
+
+		if ( ! isset( $request['options'] ) || ! is_array( $params ) ) {
+			return new \WP_Error( 'woocommerce_rest_cannot_view', __( 'You must supply an array of options.', 'woocommerce-admin' ), 500 );
+		}
+
+		foreach ( $params as $option ) {
+			if ( ! $this->user_has_permission( $option, $request ) ) {
+				return new \WP_Error( 'woocommerce_rest_cannot_view', __( 'Sorry, you cannot view these options.', 'woocommerce-admin' ), array( 'status' => rest_authorization_required_code() ) );
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Check if the user has permission given an option name.
+	 *
+	 * @param  string          $option Option name.
+	 * @param  WP_REST_Request $request Full details about the request.
+	 * @return boolean
+	 */
+	public function user_has_permission( $option, $request ) {
+		$permissions = $this->get_option_permissions( $request );
+
+		if ( isset( $permissions[ $option ] ) ) {
+			return $permissions[ $option ];
+		}
+
+		return current_user_can( 'manage_options' );
+	}
+
+	/**
+	 * Check if a given request has access to update options.
 	 *
 	 * @param  WP_REST_Request $request Full details about the request.
 	 * @return WP_Error|boolean
 	 */
 	public function update_item_permissions_check( $request ) {
-		if ( ! current_user_can( 'manage_options' ) ) {
-			return new \WP_Error( 'woocommerce_rest_cannot_update', __( 'Sorry, you cannot manage options.', 'woocommerce-admin' ), array( 'status' => rest_authorization_required_code() ) );
+		$params = $request->get_json_params();
+
+		if ( ! is_array( $params ) ) {
+			return new \WP_Error( 'woocommerce_rest_cannot_update', __( 'You must supply an array of options and values.', 'woocommerce-admin' ), 500 );
 		}
+
+		foreach ( $params as $option_name => $option_value ) {
+			if ( ! $this->user_has_permission( $option_name, $request ) ) {
+				return new \WP_Error( 'woocommerce_rest_cannot_update', __( 'Sorry, you cannot manage these options.', 'woocommerce-admin' ), array( 'status' => rest_authorization_required_code() ) );
+			}
+		}
+
 		return true;
+	}
+
+	/**
+	 * Get an array of options and respective permissions for the current user.
+	 *
+	 * @param  WP_REST_Request $request Full details about the request.
+	 * @return array
+	 */
+	public function get_option_permissions( $request ) {
+		$permissions = array(
+			'theme_mods_' . get_stylesheet()     => current_user_can( 'edit_theme_options' ),
+			'woocommerce_setup_jetpack_opted_in' => current_user_can( 'manage_woocommerce' ),
+			'woocommerce_stripe_settings'        => current_user_can( 'manage_woocommerce' ),
+			'woocommerce_ppec_paypal_settings'   => current_user_can( 'manage_woocommerce' ),
+			'woocommerce_onboarding_payments'    => current_user_can( 'manage_woocommerce' ),
+			'woocommerce_demo_store'             => current_user_can( 'manage_woocommerce' ),
+			'woocommerce_demo_store_notice'      => current_user_can( 'manage_woocommerce' ),
+		);
+
+		return apply_filters( 'woocommerce_rest_api_option_permissions', $permissions, $request );
 	}
 
 	/**
@@ -83,8 +151,8 @@ class Options extends \WC_REST_Data_Controller {
 	 * @return array Options object with option values.
 	 */
 	public function get_options( $request ) {
-		$params = explode( ',', $request[ 'options' ] );
-		$options  = array();
+		$params  = explode( ',', $request['options'] );
+		$options = array();
 
 		if ( ! is_array( $params ) ) {
 			return array();
@@ -104,7 +172,7 @@ class Options extends \WC_REST_Data_Controller {
 	 * @return array Options object with a boolean if the option was updated.
 	 */
 	public function update_options( $request ) {
-		$params = $request->get_json_params();
+		$params  = $request->get_json_params();
 		$updated = array();
 
 		if ( ! is_array( $params ) ) {
