e163271600393fb6c6d9037dfb029b44f7752cb5
Kai Neuwerth <kai.neuwerth@innogames.com>
2019-10-08 19:57:58 +0200

    Disable checking WordPress.Security.EscapeOutput.OutputNotEscaped on call of wc_make_phone_clickable

--- templates/emails/email-addresses.php
+++ templates/emails/email-addresses.php
@@ -31,7 +31,7 @@ $shipping   = $order->get_formatted_shipping_address();
 			<address class="address">
 				<?php echo wp_kses_post( $address ? $address : esc_html__( 'N/A', 'woocommerce' ) ); ?>
 				<?php if ( $order->get_billing_phone() ) : ?>
-					<br/><?php echo wc_make_phone_clickable( $order->get_billing_phone() ); ?>
+					<br/><?php echo wc_make_phone_clickable( $order->get_billing_phone() );  // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 				<?php endif; ?>
 				<?php if ( $order->get_billing_email() ) : ?>
 					<br/><?php echo esc_html( $order->get_billing_email() ); ?>
