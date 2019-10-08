8fc43ace8fadab1a6fb42869fcd2c6a6039c05a2
mindmantra <30974879+dhavalgshah@users.noreply.github.com>
2019-10-09 02:43:21 +0530

    Check if shipping calculator is enabled before showing related message in cart totals.
    Fixes #24674

--- templates/cart/cart-shipping.php
+++ templates/cart/cart-shipping.php
@@ -57,7 +57,7 @@ $calculator_text          = '';
 				</p>
 			<?php endif; ?>
 			<?php
-		elseif ( ! $has_calculated_shipping || ! $formatted_destination ) :
+		elseif ( ( ! $has_calculated_shipping || ! $formatted_destination ) && 'yes' === get_option( 'woocommerce_enable_shipping_calc' ) ) :
 			echo wp_kses_post( apply_filters( 'woocommerce_shipping_may_be_available_html', __( 'Enter your address to view shipping options.', 'woocommerce' ) ) );
 		elseif ( ! is_cart() ) :
 			echo wp_kses_post( apply_filters( 'woocommerce_no_shipping_available_html', __( 'There are no shipping options available. Please ensure that your address has been entered correctly, or contact us if you need any help.', 'woocommerce' ) ) );
