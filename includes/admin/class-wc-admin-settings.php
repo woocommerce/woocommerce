16dac4864b80a81714ed20320dd46647a5488a03
xristos3490 <xristos3490@gmail.com>
2019-10-15 12:36:14 +0300

    Add use of wp_unslash

--- includes/admin/class-wc-admin-settings.php
+++ includes/admin/class-wc-admin-settings.php
@@ -187,7 +187,8 @@ if ( ! class_exists( 'WC_Admin_Settings', false ) ) :
 			}
 
 			if ( is_array( $option_value ) ) {
-				$option_value = array_map( 'stripslashes_deep', $option_value );
+				$option_value = wp_unslash( $option_value );
+				// $option_value = array_map( 'stripslashes', $option_value );
 			} elseif ( ! is_null( $option_value ) ) {
 				$option_value = stripslashes( $option_value );
 			}
