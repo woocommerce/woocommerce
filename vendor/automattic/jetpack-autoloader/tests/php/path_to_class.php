1e0e552afd3c5871430bb39e74b9e2abf3f786c2
vedanshujain <vedanshu.jain.2012@gmail.com>
2019-10-16 13:23:43 +0530

    Adding /vendor directory to release

---
+++ vendor/automattic/jetpack-autoloader/tests/php/path_to_class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This is a very much a dummy class that is used for a test.
+ * To see if we are able to load it via the autoloader.
+ *
+ * @package jetpack
+ */
+
+namespace Jetpack\TestCase_ABC;
+
+/**
+ * Class className_ABC.
+ *
+ * @package Jetpack\TestCase_ABC
+ */
+class className_ABC {
+
+	/**
+	 * Returns True.
+	 *
+	 * @return bool
+	 */
+	function return_true() {
+		return true;
+	}
+}
