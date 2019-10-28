3a121539016b94bf9c82de2760b670845cdd26a4
Mike Jolley <mike.jolley@me.com>
2019-10-28 14:08:00 +0000

    BlocksWpQuery wrapper for WP_Query (https://github.com/woocommerce/woocommerce-blocks/pull/1086)
    * Introduce filter hook `woocommerce_blocks_product_grid_hash_args`
    
    This filter allows to extend the hash arguments depending on the context
    (e.g. a specific language with WPML).
    
    * BlocksWpQuery class to wrap WP Query
    
    * Use BlocksWpQuery
    
    * Use query_vars for hash and add inline docs

--- plugins/woocommerce-blocks/src/BlockTypes/AbstractProductGrid.php
+++ plugins/woocommerce-blocks/src/BlockTypes/AbstractProductGrid.php
@@ -9,6 +9,8 @@ namespace Automattic\WooCommerce\Blocks\BlockTypes;
 
 defined( 'ABSPATH' ) || exit;
 
+use Automattic\WooCommerce\Blocks\Utils\BlocksWpQuery;
+
 /**
  * AbstractProductGrid class.
  */
@@ -258,32 +260,14 @@ abstract class AbstractProductGrid extends AbstractDynamicBlock {
 	 * @return array List of product IDs
 	 */
 	protected function get_products() {
-		$is_cacheable = (bool) apply_filters( 'woocommerce_blocks_product_grid_is_cacheable', true, $this->query_args );
+		$is_cacheable      = (bool) apply_filters( 'woocommerce_blocks_product_grid_is_cacheable', true, $this->query_args );
+		$transient_version = \WC_Cache_Helper::get_transient_version( 'product_query' );
 
-		if ( $is_cacheable ) {
-			$query_hash        = md5( wp_json_encode( $this->query_args ) . __CLASS__ );
-			$transient_name    = 'wc_block_' . $query_hash;
-			$transient_value   = get_transient( $transient_name );
-			$transient_version = \WC_Cache_Helper::get_transient_version( 'product_query' );
-		}
+		$query   = new BlocksWpQuery( $this->query_args );
+		$results = wp_parse_id_list( $is_cacheable ? $query->get_cached_posts( $transient_version ) : $query->get_posts() );
 
-		if ( isset( $transient_value['value'], $transient_value['version'], $transient_version ) && $transient_value['version'] === $transient_version ) {
-			$results = $transient_value['value'];
-		} else {
-			$query   = new \WP_Query( $this->query_args );
-			$results = wp_parse_id_list( $query->posts );
-
-			if ( $is_cacheable ) {
-				$transient_value = array(
-					'version' => $transient_version,
-					'value'   => $results,
-				);
-				set_transient( $transient_name, $transient_value, DAY_IN_SECONDS * 30 );
-			}
-
-			// Remove ordering query arguments which may have been added by get_catalog_ordering_args.
-			WC()->query->remove_ordering_args();
-		}
+		// Remove ordering query arguments which may have been added by get_catalog_ordering_args.
+		WC()->query->remove_ordering_args();
 
 		// Prime caches to reduce future queries.
 		if ( is_callable( '_prime_post_caches' ) ) {
