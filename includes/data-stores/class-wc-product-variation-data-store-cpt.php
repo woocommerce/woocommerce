fefeff1aa2a68631fc8bfc9b791698e4de1e84c3
Claudio Sanches <contato@claudiosanches.com>
2019-11-13 14:14:27 -0300

    Make WC_Product_Variation_Data_Store_CPT::read check only for product_variation

--- includes/data-stores/class-wc-product-variation-data-store-cpt.php
+++ includes/data-stores/class-wc-product-variation-data-store-cpt.php
@@ -48,7 +48,7 @@ class WC_Product_Variation_Data_Store_CPT extends WC_Product_Data_Store_CPT impl
 
 		$post_object = get_post( $product->get_id() );
 
-		if ( ! $post_object || ! in_array( $post_object->post_type, array( 'product', 'product_variation' ), true ) ) {
+		if ( ! $post_object || 'product_variation' !== $post_object->post_type ) {
 			return;
 		}
 
