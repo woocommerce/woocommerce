c319df7f75ac6d0b48c0c5a134845863ac2339f6
Darren Ethier <darren@roughsmootheng.in>
2019-11-14 09:51:32 -0500

    Add context value validation for context apis that receive an object. (https://github.com/woocommerce/woocommerce-blocks/pull/1174)
    * add a context value validator and tests
    
    This handles validation incoming value prop on a context provider with an object.
    
    * implement context validator and fix related bugs
    
    * rename context to better match it’s purpose
    
    * correct spelling
    
    * Update assets/js/base/context/utils.js
    
    Co-Authored-By: Albert Juhé Lluveras <aljullu@gmail.com>
    
    * tighten up the iteration

--- plugins/woocommerce-blocks/assets/js/base/context/product-layout-context.js
+++ plugins/woocommerce-blocks/assets/js/base/context/product-layout-context.js
@@ -1,13 +1,36 @@
 /**
  * External dependencies
  */
-import { createContext, useContext } from '@wordpress/element';
+import { createContext, useContext, useEffect } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { assertValidContextValue } from './utils';
+
+const validationMap = {
+	layoutStyleClassPrefix: {
+		required: true,
+		type: 'string',
+	},
+};
 
 const ProductLayoutContext = createContext( {
 	layoutStyleClassPrefix: '',
 } );
 
-export const useProductLayoutContext = () => {
-	useContext( ProductLayoutContext );
+export const useProductLayoutContext = () => useContext( ProductLayoutContext );
+export const ProductLayoutContextProvider = ( { value, children } ) => {
+	useEffect( () => {
+		assertValidContextValue(
+			'ProductLayoutContextProvider',
+			validationMap,
+			value
+		);
+	}, [ value ] );
+	return (
+		<ProductLayoutContext.Provider value={ value }>
+			{ children }
+		</ProductLayoutContext.Provider>
+	);
 };
-export const ProductLayoutContextProvider = ProductLayoutContext.Provider;
---
+++ plugins/woocommerce-blocks/assets/js/base/context/test/utils.js
@@ -0,0 +1,42 @@
+/**
+ * Internal dependencies
+ */
+import { assertValidContextValue } from '../utils';
+
+describe( 'assertValidContextValue', () => {
+	const contextName = 'testContext';
+	const validationMap = {
+		cheeseburger: {
+			required: false,
+			type: 'string',
+		},
+		amountKetchup: {
+			required: true,
+			type: 'number',
+		},
+	};
+	it.each`
+		testValue                                       | expectedMessage   | expectError
+		${{}}                                           | ${'expected'}     | ${true}
+		${10}                                           | ${'expected'}     | ${true}
+		${{ amountKetchup: 20 }}                        | ${'not expected'} | ${false}
+		${{ amountKetchup: '10' }}                      | ${'expected'}     | ${true}
+		${{ cheeseburger: 'fries', amountKetchup: 20 }} | ${'not expected'} | ${false}
+	`(
+		'The value of $testValue is $expectedMessage to trigger an Error',
+		( { testValue, expectError } ) => {
+			const invokeTest = () => {
+				assertValidContextValue(
+					contextName,
+					validationMap,
+					testValue
+				);
+			};
+			if ( expectError ) {
+				expect( invokeTest ).toThrow();
+			} else {
+				expect( invokeTest ).not.toThrow();
+			}
+		}
+	);
+} );
