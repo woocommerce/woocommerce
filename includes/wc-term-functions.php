9bebc4e69edece845a5b6fb4f24ac64300918aad
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-11-21 12:43:01 -0300

    Simplify and modernize wc_walk_category_dropdown_tree()
    This commit replaces calls to call_user_func_array() and func_get_args() with the spread operation. Both to make more explicit that this is a variadic function and to do argument unpacking when calling the walker method.

--- includes/wc-term-functions.php
+++ includes/wc-term-functions.php
@@ -253,11 +253,10 @@ function wc_product_dropdown_categories( $args = array() ) {
  *
  * Previously used by wc_product_dropdown_categories, but wp_dropdown_categories has been fixed in core.
  *
+ * @param mixed ...$args Variable number of parameters to be passed to the walker.
  * @return mixed
  */
-function wc_walk_category_dropdown_tree() {
-	$args = func_get_args();
-
+function wc_walk_category_dropdown_tree( ...$args ) {
 	if ( ! class_exists( 'WC_Product_Cat_Dropdown_Walker', false ) ) {
 		include_once WC()->plugin_path() . '/includes/walkers/class-wc-product-cat-dropdown-walker.php';
 	}
@@ -269,7 +268,7 @@ function wc_walk_category_dropdown_tree() {
 		$walker = $args[2]['walker'];
 	}
 
-	return call_user_func_array( array( &$walker, 'walk' ), $args );
+	return $walker->walk( ...$args );
 }
 
 /**
