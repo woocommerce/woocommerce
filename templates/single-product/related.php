70cd18d047dcf2c94eb3f16b8cd7a9f8bd34d7a0
Andrej Mernik <R33D3M33R@users.noreply.github.com>
2019-11-21 11:12:33 +0100

    Globals check override
    Use the cross-sells.php globals check override

--- templates/single-product/related.php
+++ templates/single-product/related.php
@@ -38,7 +38,7 @@ if ( $related_products ) : ?>
 					<?php
 					$post_object = get_post( $related_product->get_id() );
 
-					setup_postdata( $GLOBALS['post'] =& $post_object );
+					setup_postdata( $GLOBALS['post'] =& $post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited, Squiz.PHP.DisallowMultipleAssignments.Found
 
 					wc_get_template_part( 'content', 'product' );
 					?>
@@ -48,8 +48,7 @@ if ( $related_products ) : ?>
 		<?php woocommerce_product_loop_end(); ?>
 
 	</section>
-
-<?php
+	<?php
 endif;
 
 wp_reset_postdata();
