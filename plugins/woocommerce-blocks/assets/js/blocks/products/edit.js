578ddbe94d4072568473b11a441aae94b712c2d8
Albert Juhé Lluveras <aljullu@gmail.com>
2019-12-05 17:03:31 +0100

    Minor code style fixes in All Products blocks (https://github.com/woocommerce/woocommerce-blocks/pull/1330)

--- plugins/woocommerce-blocks/assets/js/blocks/products/edit.js
+++ plugins/woocommerce-blocks/assets/js/blocks/products/edit.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Fragment } from '@wordpress/element';
 import { ToggleControl, SelectControl } from '@wordpress/components';
 
 /**
@@ -13,81 +12,71 @@ import './editor.scss';
 export const getSharedContentControls = ( attributes, setAttributes ) => {
 	const { contentVisibility } = attributes;
 	return (
-		<Fragment>
-			<ToggleControl
-				label={ __(
-					'Show Sorting Dropdown',
-					'woo-gutenberg-products-block'
-				) }
-				checked={ contentVisibility.orderBy }
-				onChange={ () =>
-					setAttributes( {
-						contentVisibility: {
-							...contentVisibility,
-							orderBy: ! contentVisibility.orderBy,
-						},
-					} )
-				}
-			/>
-		</Fragment>
+		<ToggleControl
+			label={ __(
+				'Show Sorting Dropdown',
+				'woo-gutenberg-products-block'
+			) }
+			checked={ contentVisibility.orderBy }
+			onChange={ () =>
+				setAttributes( {
+					contentVisibility: {
+						...contentVisibility,
+						orderBy: ! contentVisibility.orderBy,
+					},
+				} )
+			}
+		/>
 	);
 };
 
 export const getSharedListControls = ( attributes, setAttributes ) => {
 	return (
-		<Fragment>
-			<SelectControl
-				label={ __(
-					'Order Products By',
-					'woo-gutenberg-products-block'
-				) }
-				value={ attributes.orderby }
-				options={ [
-					{
-						label: __(
-							'Newness - newest first',
-							'woo-gutenberg-products-block'
-						),
-						value: 'date',
-					},
-					{
-						label: __(
-							'Price - low to high',
-							'woo-gutenberg-products-block'
-						),
-						value: 'price',
-					},
-					{
-						label: __(
-							'Price - high to low',
-							'woo-gutenberg-products-block'
-						),
-						value: 'price-desc',
-					},
-					{
-						label: __(
-							'Rating - highest first',
-							'woo-gutenberg-products-block'
-						),
-						value: 'rating',
-					},
-					{
-						label: __(
-							'Sales - most first',
-							'woo-gutenberg-products-block'
-						),
-						value: 'popularity',
-					},
-					{
-						label: __(
-							'Menu Order',
-							'woo-gutenberg-products-block'
-						),
-						value: 'menu_order',
-					},
-				] }
-				onChange={ ( orderby ) => setAttributes( { orderby } ) }
-			/>
-		</Fragment>
+		<SelectControl
+			label={ __( 'Order Products By', 'woo-gutenberg-products-block' ) }
+			value={ attributes.orderby }
+			options={ [
+				{
+					label: __(
+						'Newness - newest first',
+						'woo-gutenberg-products-block'
+					),
+					value: 'date',
+				},
+				{
+					label: __(
+						'Price - low to high',
+						'woo-gutenberg-products-block'
+					),
+					value: 'price',
+				},
+				{
+					label: __(
+						'Price - high to low',
+						'woo-gutenberg-products-block'
+					),
+					value: 'price-desc',
+				},
+				{
+					label: __(
+						'Rating - highest first',
+						'woo-gutenberg-products-block'
+					),
+					value: 'rating',
+				},
+				{
+					label: __(
+						'Sales - most first',
+						'woo-gutenberg-products-block'
+					),
+					value: 'popularity',
+				},
+				{
+					label: __( 'Menu Order', 'woo-gutenberg-products-block' ),
+					value: 'menu_order',
+				},
+			] }
+			onChange={ ( orderby ) => setAttributes( { orderby } ) }
+		/>
 	);
 };
