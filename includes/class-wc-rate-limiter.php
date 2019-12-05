4dfa68b62d965301d1e69ab780eb0941fe9754d6
Peter Fabian <peter.fabian.github@gmail.com>
2019-12-05 15:46:13 +0100

    More PHPCS fixes.

--- includes/class-wc-rate-limiter.php
+++ includes/class-wc-rate-limiter.php
@@ -34,7 +34,7 @@ class WC_Rate_Limiter {
 	/**
 	 * Constructs Option name from action identifier.
 	 *
-	 * @param string $action_id
+	 * @param string $action_id Identifier of the action.
 	 * @return string
 	 */
 	public static function storage_id( $action_id ) {
@@ -44,7 +44,7 @@ class WC_Rate_Limiter {
 	/**
 	 * Returns true if the action is not allowed to be run by the rate limiter yet, false otherwise.
 	 *
-	 * @param string $action_id Identifier for the action
+	 * @param string $action_id Identifier of the action.
 	 * @return bool
 	 */
 	public static function retried_too_soon( $action_id ) {
@@ -67,8 +67,8 @@ class WC_Rate_Limiter {
 	/**
 	 * Sets the rate limit delay in seconds for action with identifier $id.
 	 *
-	 * @param string $action_id Identifier for the action.
-	 * @param int $delay Delay in seconds.
+	 * @param string $action_id Identifier of the action.
+	 * @param int    $delay Delay in seconds.
 	 * @return bool True if the option setting was successful, false otherwise.
 	 */
 	public static function set_rate_limit( $action_id, $delay ) {
