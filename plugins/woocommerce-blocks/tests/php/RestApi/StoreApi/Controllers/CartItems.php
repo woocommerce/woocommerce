e5bc7428534ef610e47496bcaa5629cb49dd8a78
Rua Haszard <rua.haszard@automattic.com>
2019-12-12 23:31:25 +1300

    add pricing discount info to cart line items api (https://github.com/woocommerce/woocommerce-blocks/pull/1325)
    * add more pricing detail to cart line items api:
    - total line price, before any coupon/discount
    - saving, aka discount amount (if any)
    - boolean if saving > 0
    
    * add new cart price info properties to schema
    
    * tidy field naming – line subtotal=pre discounts, line total=discounted
    
    * remove saving / discount fields from API – could be calculated on client
    
    * return numbers (not string) for price fields
    
    * rename API field "price" to product price for clarity
    
    * fix schema type for total fields (now float)
    
    * Total descriptions in schema
    
    * Format all costs as strings to follow core rest api
    
    * Add totals to cart schema
    
    * Update tests
    
    * tax_lines

--- plugins/woocommerce-blocks/tests/php/RestApi/StoreApi/Controllers/CartItems.php
+++ plugins/woocommerce-blocks/tests/php/RestApi/StoreApi/Controllers/CartItems.php
@@ -79,8 +79,9 @@ class CartItems extends TestCase {
 		$this->assertEquals( $this->products[0]->get_sku(), $data['sku'] );
 		$this->assertEquals( $this->products[0]->get_permalink(), $data['permalink'] );
 		$this->assertEquals( 2, $data['quantity'] );
-		$this->assertEquals( '10.00', $data['price'] );
-		$this->assertEquals( '20.00', $data['line_price'] );
+		$this->assertEquals( '10.00', $data['product_price'] );
+		$this->assertEquals( '20.00', $data['line_subtotal'] );
+		$this->assertEquals( '20.00', $data['line_total'] );
 
 		$request  = new WP_REST_Request( 'DELETE', '/wc/store/cart/items/XXX815416f775098fe977004015c6193' );
 		$response = $this->server->dispatch( $request );
@@ -207,8 +208,9 @@ class CartItems extends TestCase {
 		$this->assertArrayHasKey( 'sku', $schema['properties'] );
 		$this->assertArrayHasKey( 'permalink', $schema['properties'] );
 		$this->assertArrayHasKey( 'images', $schema['properties'] );
-		$this->assertArrayHasKey( 'price', $schema['properties'] );
-		$this->assertArrayHasKey( 'line_price', $schema['properties'] );
+		$this->assertArrayHasKey( 'product_price', $schema['properties'] );
+		$this->assertArrayHasKey( 'line_subtotal', $schema['properties'] );
+		$this->assertArrayHasKey( 'line_total', $schema['properties'] );
 		$this->assertArrayHasKey( 'variation', $schema['properties'] );
 	}
 
@@ -227,8 +229,9 @@ class CartItems extends TestCase {
 		$this->assertArrayHasKey( 'sku', $response->get_data() );
 		$this->assertArrayHasKey( 'permalink', $response->get_data() );
 		$this->assertArrayHasKey( 'images', $response->get_data() );
-		$this->assertArrayHasKey( 'price', $response->get_data() );
-		$this->assertArrayHasKey( 'line_price', $response->get_data() );
+		$this->assertArrayHasKey( 'product_price', $response->get_data() );
+		$this->assertArrayHasKey( 'line_subtotal', $response->get_data() );
+		$this->assertArrayHasKey( 'line_total', $response->get_data() );
 		$this->assertArrayHasKey( 'variation', $response->get_data() );
 	}
 }
