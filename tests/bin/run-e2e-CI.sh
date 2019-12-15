14afe362044f90e97d3717073fde1daaafb0eaf5
Julia Amosova <julia.amosova@gmail.com>
2019-12-15 15:32:26 +0000

    Add check for Docker container to be built on Travis

--- tests/bin/run-e2e-CI.sh
+++ tests/bin/run-e2e-CI.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8084)" != "200" ]];
+do
+  echo "$(date) - Docker container is still being built";
+  sleep 1;
+done
+echo "$(date) - Docker container had been built successfully";
 npm run test:e2e;
