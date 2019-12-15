5645f5882a98d9cdffeb2aa79c09050758f74096
Julia Amosova <julia.amosova@gmail.com>
2019-12-15 16:04:06 +0000

    Add check for Docker container to be built on Travis when post is created

--- tests/bin/run-e2e-CI.sh
+++ tests/bin/run-e2e-CI.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8084)" != "200" ]];
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8084/?p=5)" != "200" ]];
 do
   echo "$(date) - Docker container is still being built";
   sleep 1;
