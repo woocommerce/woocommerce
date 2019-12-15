c1b358929b3e4a455458ebbcbefabc2395be744e
Julia Amosova <julia.amosova@gmail.com>
2019-12-15 16:44:40 +0000

    Add page creation & check for Docker container status to run e2e tests

--- tests/bin/run-e2e-CI.sh
+++ tests/bin/run-e2e-CI.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8084/?p=5)" != "200" ]];
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8084/?page_id=4)" != "200" ]];
 do
   echo "$(date) - Docker container is still being built";
-  sleep 1;
+  sleep 10;
 done
 echo "$(date) - Docker container had been built successfully";
 npm run test:e2e;
