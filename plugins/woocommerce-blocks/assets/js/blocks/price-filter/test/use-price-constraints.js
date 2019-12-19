cf495a775e8e081df4aee8236ad5302eb575e4a1
Seghir Nadir <nadir.seghir@gmail.com>
2019-12-19 13:52:49 +0100

    Fix Price Filter constraints by rounding up and down (https://github.com/woocommerce/woocommerce-blocks/pull/1419)
    * fix problem with price not rounding up right
    
    * add test

--- plugins/woocommerce-blocks/assets/js/blocks/price-filter/test/use-price-constraints.js
+++ plugins/woocommerce-blocks/assets/js/blocks/price-filter/test/use-price-constraints.js
@@ -7,24 +7,40 @@ import TestRenderer from 'react-test-renderer';
  * Internal dependencies
  */
 import { usePriceConstraint } from '../use-price-constraints';
+import { ROUND_UP, ROUND_DOWN } from '../constants';
 
 describe( 'usePriceConstraints', () => {
 	const TestComponent = ( { price } ) => {
-		const priceConstraint = usePriceConstraint( price, 2 );
-		return <div priceConstraint={ priceConstraint } />;
+		const maxPriceConstraint = usePriceConstraint( price, 2, ROUND_UP );
+		const minPriceConstraint = usePriceConstraint( price, 2, ROUND_DOWN );
+		return (
+			<div
+				minPriceConstraint={ minPriceConstraint }
+				maxPriceConstraint={ maxPriceConstraint }
+			/>
+		);
 	};
 
-	it( 'price constraint should be updated when new price is set', () => {
-		const renderer = TestRenderer.create(
-			<TestComponent price={ 1000 } />
-		);
+	it( 'max price constraint should be updated when new price is set', () => {
+		const renderer = TestRenderer.create( <TestComponent price={ 1000 } /> );
 		const container = renderer.root.findByType( 'div' );
 
-		expect( container.props.priceConstraint ).toBe( 1000 );
+		expect( container.props.maxPriceConstraint ).toBe( 1000 );
 
 		renderer.update( <TestComponent price={ 2000 } /> );
 
-		expect( container.props.priceConstraint ).toBe( 2000 );
+		expect( container.props.maxPriceConstraint ).toBe( 2000 );
+	} );
+
+	it( 'min price constraint should be updated when new price is set', () => {
+		const renderer = TestRenderer.create( <TestComponent price={ 1000 } /> );
+		const container = renderer.root.findByType( 'div' );
+
+		expect( container.props.minPriceConstraint ).toBe( 1000 );
+
+		renderer.update( <TestComponent price={ 2000 } /> );
+
+		expect( container.props.minPriceConstraint ).toBe( 2000 );
 	} );
 
 	it( 'previous price constraint should be preserved when new price is not a infinite number', () => {
@@ -33,10 +49,36 @@ describe( 'usePriceConstraints', () => {
 		);
 		const container = renderer.root.findByType( 'div' );
 
-		expect( container.props.priceConstraint ).toBe( 1000 );
+		expect( container.props.maxPriceConstraint ).toBe( 1000 );
 
 		renderer.update( <TestComponent price={ Infinity } /> );
 
-		expect( container.props.priceConstraint ).toBe( 1000 );
+		expect( container.props.maxPriceConstraint ).toBe( 1000 );
+	} );
+
+	it( 'max price constraint should be higher if the price is decimal', () => {
+		const renderer = TestRenderer.create(
+			<TestComponent price={ 1099 } />
+		);
+		const container = renderer.root.findByType( 'div' );
+
+		expect( container.props.maxPriceConstraint ).toBe( 2000 );
+
+		renderer.update( <TestComponent price={ 1999 } /> );
+
+		expect( container.props.maxPriceConstraint ).toBe( 2000 );
+	} );
+
+	it( 'min price constraint should be lower if the price is decimal', () => {
+		const renderer = TestRenderer.create(
+			<TestComponent price={ 999 } />
+		);
+		const container = renderer.root.findByType( 'div' );
+
+		expect( container.props.minPriceConstraint ).toBe( 0 );
+
+		renderer.update( <TestComponent price={ 1999 } /> );
+
+		expect( container.props.minPriceConstraint ).toBe( 1000 );
 	} );
 } );
