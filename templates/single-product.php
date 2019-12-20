90dbd8295f57b45cfba1df87a3c4ac11dbf85eaa
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-12-20 15:09:09 -0300

    Fix Squiz.ControlStructures.ControlSignature violations
    This commit fixes all violations of the Squiz.ControlStructures.ControlSignature sniff automatically using phpcbf.

--- templates/single-product.php
+++ templates/single-product.php
@@ -31,7 +31,8 @@ get_header( 'shop' ); ?>
 		do_action( 'woocommerce_before_main_content' );
 	?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : ?>
+			<?php the_post(); ?>
 
 			<?php wc_get_template_part( 'content', 'single-product' ); ?>
 
