b742c132da1e7ec62464c4f1e96366ca71c44fe8
Jeff Stieler <jeff.m.stieler@gmail.com>
2019-12-26 17:50:30 -0500

    Invalidate API caches when updating analytics settings. (https://github.com/woocommerce/woocommerce-admin/pull/3465)

--- plugins/woocommerce-admin/src/API/SettingOptions.php
+++ plugins/woocommerce-admin/src/API/SettingOptions.php
@@ -11,6 +11,8 @@ namespace Automattic\WooCommerce\Admin\API;
 
 defined( 'ABSPATH' ) || exit;
 
+use \Automattic\WooCommerce\Admin\API\Reports\Cache as ReportsCache;
+
 /**
  * Setting Options controller.
  *
@@ -25,4 +27,18 @@ class SettingOptions extends \WC_REST_Setting_Options_Controller {
 	 * @var string
 	 */
 	protected $namespace = 'wc-analytics';
+
+	/**
+	 * Invalidates API cache when updating settings options.
+	 *
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return array Of WP_Error or WP_REST_Response.
+	 */
+	public function batch_items( $request ) {
+		// Invalidate the API cache.
+		ReportsCache::invalidate();
+
+		// Process the request.
+		return parent::batch_items( $request );
+	}
 }
