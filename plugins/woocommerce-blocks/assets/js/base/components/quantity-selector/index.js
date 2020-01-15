6745aee61f5c389ea74049e5ee8bef2edd483e22
Mike Jolley <mike.jolley@me.com>
2020-01-15 11:32:54 +0000

    Add Quantity selector component for cart line items (https://github.com/woocommerce/woocommerce-blocks/pull/1558)
    * Implement quantity selector component
    
    * Improve default appearance
    
    * Remove underline
    
    * Add aria speak
    
    * Improved keycode handling
    
    * Add focus styles to buttons and inputs
    
    * Increase constrast

--- plugins/woocommerce-blocks/assets/js/base/components/quantity-selector/index.js
+++ plugins/woocommerce-blocks/assets/js/base/components/quantity-selector/index.js
@@ -1,8 +1,12 @@
 /**
  * External dependencies
  */
+import { __, sprintf } from '@wordpress/i18n';
+import { speak } from '@wordpress/a11y';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { useRef, useState, useEffect, useCallback } from '@wordpress/element';
+import { DOWN, UP } from '@wordpress/keycodes';
 
 /**
  * Internal dependencies
@@ -13,23 +17,122 @@ const QuantitySelector = ( {
 	className,
 	quantity = 1,
 	onChange = () => null,
+	itemName = '',
 } ) => {
-	const classes = classNames(
-		'wc-block-quantity-selector__input',
-		className
+	const classes = classNames( 'wc-block-quantity-selector', className );
+	const inputRef = useRef( null );
+	const [ currentValue, setCurrentValue ] = useState( quantity );
+
+	useEffect( () => {
+		if ( currentValue !== inputRef.current.value ) {
+			inputRef.current.value = currentValue;
+		}
+		onChange( currentValue );
+	}, [ currentValue, onChange ] );
+
+	/**
+	 * Handles keyboard up and down keys to change quantity value.
+	 *
+	 * @param {Object} event event data.
+	 */
+	const quantityInputOnKeyDown = useCallback(
+		( event ) => {
+			const isArrowDown =
+				typeof event.key !== undefined
+					? event.key === 'ArrowDown'
+					: event.keyCode === DOWN;
+			const isArrowUp =
+				typeof event.key !== undefined
+					? event.key === 'ArrowUp'
+					: event.keyCode === UP;
+
+			if ( isArrowDown && currentValue > 0 ) {
+				event.preventDefault();
+				setCurrentValue( currentValue - 1 );
+			}
+
+			if ( isArrowUp ) {
+				event.preventDefault();
+				setCurrentValue( currentValue + 1 );
+			}
+		},
+		[ currentValue, setCurrentValue ]
 	);
 
-	// For now just use a regular number edit. (temporary)
-	// @todo https://github.com/woocommerce/woocommerce-gutenberg-products-block/issues/1521
 	return (
-		<input
-			className={ classes }
-			type="number"
-			step="1"
-			min="0"
-			value={ quantity }
-			onChange={ ( event ) => onChange( event.target.value ) }
-		/>
+		<div className={ classes }>
+			<input
+				className="wc-block-quantity-selector__input"
+				type="number"
+				step="1"
+				min="0"
+				value={ quantity }
+				ref={ inputRef }
+				onKeyDown={ quantityInputOnKeyDown }
+				onChange={ ( event ) => {
+					const value =
+						isNaN( event.target.value ) || ! event.target.value
+							? 0
+							: parseInt( event.target.value, 10 );
+					setCurrentValue( value );
+				} }
+				aria-label={ sprintf(
+					__(
+						// translators: %s Item name.
+						'Quantity of %s in your cart.',
+						'woo-gutenberg-products-block'
+					),
+					itemName
+				) }
+			/>
+			<button
+				aria-label={ __(
+					'Reduce quantity',
+					'woo-gutenberg-products-block'
+				) }
+				className="wc-block-quantity-selector__button wc-block-quantity-selector__button--minus"
+				disabled={ currentValue <= 0 }
+				onClick={ () => {
+					const newQuantity = currentValue - 1;
+					setCurrentValue( newQuantity );
+					speak(
+						sprintf(
+							__(
+								// translators: %s Quantity amount.
+								'Quantity reduced to %s.',
+								'woo-gutenberg-products-block'
+							),
+							newQuantity
+						)
+					);
+				} }
+			>
+				&#65293;
+			</button>
+			<button
+				aria-label={ __(
+					'Increase quantity',
+					'woo-gutenberg-products-block'
+				) }
+				className="wc-block-quantity-selector__button wc-block-quantity-selector__button--plus"
+				onClick={ () => {
+					const newQuantity = currentValue + 1;
+					setCurrentValue( newQuantity );
+					speak(
+						sprintf(
+							__(
+								// translators: %s Quantity amount.
+								'Quantity increased to %s.',
+								'woo-gutenberg-products-block'
+							),
+							newQuantity
+						)
+					);
+				} }
+			>
+				&#65291;
+			</button>
+		</div>
 	);
 };
 
@@ -37,6 +140,7 @@ QuantitySelector.propTypes = {
 	className: PropTypes.string,
 	quantity: PropTypes.number,
 	onChange: PropTypes.func,
+	itemName: PropTypes.string,
 };
 
 export default QuantitySelector;
