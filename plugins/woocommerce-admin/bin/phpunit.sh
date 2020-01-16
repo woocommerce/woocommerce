92af5964fb19b7860794727f74beec42513464f9
Ron Rennick <ron@ronandandrea.com>
2020-01-16 10:03:44 -0400

    Update PHPUnit to version 7 (https://github.com/woocommerce/woocommerce-admin/pull/3571)
    * add php requirement to composer, update phpunit to 7.5.18
    
    * add random order test run (allowed to fail)

--- plugins/woocommerce-admin/bin/phpunit.sh
+++ plugins/woocommerce-admin/bin/phpunit.sh
@@ -2,7 +2,11 @@
 WORKING_DIR="$PWD"
 cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
 phpunit --version
-phpunit -c phpunit.xml.dist
+if [[ {$RUN_RANDOM} == 1 ]]; then
+	./vendor/bin/phpunit -c phpunit.xml.dist --order-by=random
+else
+	phpunit -c phpunit.xml.dist
+fi
 TEST_RESULT=$?
 cd "$WORKING_DIR"
 exit $TEST_RESULT
--- plugins/woocommerce-admin/bin/travis.sh
+++ plugins/woocommerce-admin/bin/travis.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # usage: travis.sh before|after
 
-if [ $1 == 'before' ]; then
+if [ "$1" == 'before' ]; then
 	cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
 	npm run build:feature-config
-	if [[ ${RUN_PHPCS} == 1 ]]; then
+	if [[ "$RUN_PHPCS" == "1" || "$RUN_RANDOM" == "1" ]]; then
 		composer install
 	else
 		composer install --no-dev
