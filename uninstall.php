ee561778966fd2c0ef26e345ebcfbb813bb990ed
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-01-17 10:32:17 -0700

    Fix unit tests.
    Only require autoloader in uninstall if WooCommerce Admin class hasn't been loaded.

--- uninstall.php
+++ uninstall.php
@@ -30,10 +30,12 @@ if ( defined( 'WC_REMOVE_ALL_DATA' ) && true === WC_REMOVE_ALL_DATA ) {
 	 * Load core packages autoloader.
 	 */
 	if ( version_compare( PHP_VERSION, '5.6.0', '>=' ) ) {
-		require __DIR__ . '/src/Autoloader.php';
+		if ( ! class_exists( 'Automattic\WooCommerce\Admin\Install' ) ) {
+			require __DIR__ . '/src/Autoloader.php';
 
-		if ( ! \Automattic\WooCommerce\Autoloader::init() ) {
-			return;
+			if ( ! \Automattic\WooCommerce\Autoloader::init() ) {
+				return;
+			}
 		}
 
 		// Hook in WooCommerce Admin installation code.
