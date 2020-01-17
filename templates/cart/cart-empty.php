e6c0c43457ba07da3a5d3cc802f139d73a3087a2
Aezaz Shaikh <shaikhaezaz80@gmail.com>
2020-01-17 12:38:51 +0530

    Update cart-empty.php
    Added filter to change "Return to Shop" button text

--- templates/cart/cart-empty.php
+++ templates/cart/cart-empty.php
@@ -25,7 +25,7 @@ do_action( 'woocommerce_cart_is_empty' );
 if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
 	<p class="return-to-shop">
 		<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-			<?php esc_html_e( 'Return to shop', 'woocommerce' ); ?>
+			<?php echo apply_filters('woocommerce_return_to_shop_title', sprintf( '%s', __('Return To Shop', 'woocommerce' ) )); ?>
 		</a>
 	</p>
 <?php endif; ?>
