2cb9bba89e180839b56a7ad2b94f075eae6f4c52
Kathy Daring <helgatheviking@gmail.com>
2020-01-20 10:28:11 -0700

    update template version to 3.10.0

--- templates/global/quantity-input.php
+++ templates/global/quantity-input.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.9.1
+ * @version 3.10.0
  */
 
 defined( 'ABSPATH' ) || exit;
