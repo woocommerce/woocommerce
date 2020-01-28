f488f46b37ffc6ca3984138b12a1583145bf47b2
TimBHowe <tim@hallme.com>
2020-01-28 11:01:13 -0500

    remove the pattern constraint on the flatrate input in the setup wiz

--- includes/admin/class-wc-admin-setup-wizard.php
+++ includes/admin/class-wc-admin-setup-wizard.php
@@ -937,7 +937,6 @@ class WC_Admin_Setup_Wizard {
 						'default_value' => __( 'Cost', 'woocommerce' ),
 						'description'   => __( 'What would you like to charge for flat rate shipping?', 'woocommerce' ),
 						'required'      => true,
-						'pattern'       => '^[+-]?[0-9]{1,3}(?:[0-9]*(?:[.,][0-9]{2})?|(?:,[0-9]{3})*(?:\.[0-9]{2})?|(?:\.[0-9]{3})*(?:,[0-9]{2})?)$',
 					),
 				),
 			),
@@ -1000,7 +999,6 @@ class WC_Admin_Setup_Wizard {
 					name="<?php echo esc_attr( $method_setting_id ); ?>"
 					class="<?php echo esc_attr( $setting['required'] ? 'shipping-method-required-field' : '' ); ?>"
 					<?php echo ( $method_id === $selected && $setting['required'] ) ? 'required' : ''; ?>
-					pattern=<?php echo esc_attr( $method_id === $selected && $setting['pattern'] ? $setting['pattern'] : '' ); ?>
 				/>
 				<p class="description">
 					<?php echo esc_html( $setting['description'] ); ?>
