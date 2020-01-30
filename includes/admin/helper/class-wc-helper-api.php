9e257064677f7331c51f1550dd8a970f32c3e58d
Denis Dvali <denis.dvali@automattic.com>
2020-01-30 15:17:50 +0300

    Add query params as fallback for passing authentication data to WC Helper API.

--- includes/admin/helper/class-wc-helper-api.php
+++ includes/admin/helper/class-wc-helper-api.php
@@ -56,7 +56,7 @@ class WC_Helper_API {
 	 * @param array  $args By-ref, the args that will be passed to wp_remote_request().
 	 * @return bool Were the headers added?
 	 */
-	private static function _authenticate( $url, &$args ) {
+	private static function _authenticate( &$url, &$args ) {
 		$auth = WC_Helper_Options::get( 'auth' );
 
 		if ( empty( $auth['access_token'] ) || empty( $auth['access_token_secret'] ) ) {
@@ -88,6 +88,14 @@ class WC_Helper_API {
 		$args['headers'] = array(
 			'Authorization'   => 'Bearer ' . $auth['access_token'],
 			'X-Woo-Signature' => $signature,
+		);
+
+		$url = add_query_arg(
+			array(
+				'token'     => $auth['access_token'],
+				'signature' => $signature,
+			),
+			$url
 		);
 
 		return true;
