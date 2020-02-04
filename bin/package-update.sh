a48b2b53c0372bf45fd433e12e90b943dad8df52
Peter Fabian <peter.fabian.github@gmail.com>
2020-02-04 15:45:40 +0100

    Handle PHP files with wp-textdomain, JS files with sed.

--- bin/package-update.sh
+++ bin/package-update.sh
@@ -26,8 +26,15 @@ composer dump-autoload
 output 2 "Done"
 
 # Convert textdomains
-output 3 "Updating package textdomains..."
+output 3 "Updating package PHP textdomains..."
 
 # Replace text domains within packages with woocommerce
 npm run packages:fix:textdomain
+output 2 "Done!"
+
+output 3 "Updating package JS textdomains..."
+find ./packages/woocommerce-blocks -iname '*.js' -exec sed -i.bak -e "s/'woo-gutenberg-products-block'/'woocommerce'/g" -e "s/\"woo-gutenberg-products-block\"/'woocommerce'/g" {} \;
+
+# Cleanup backup files
+find ./packages -name "*.bak" -type f -delete
 output 2 "Done!"
