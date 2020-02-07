e55461f6e430a35d580f91108f381c5f40c5e868
Peter Fabian <peter.fabian.github@gmail.com>
2020-02-07 15:28:06 +0100

    Change the setting part before the verification.

--- tests/e2e-tests/specs/wp-admin/wp-admin-settings-product.test.js
+++ tests/e2e-tests/specs/wp-admin/wp-admin-settings-product.test.js
@@ -21,7 +21,7 @@ describe( 'WooCommerce Products > Downloadable Products Settings', () => {
 		await expect( page ).toMatchElement( 'a.nav-tab-active', { text: 'Products' } );
 		await expect( page ).toMatchElement( 'ul.subsubsub > li > a.current', { text: 'Downloadable products' } );
 
-		await expect( page ).toSelect( '#woocommerce_file_download_method', 'Redirect only' );
+		await expect( page ).toSelect( '#woocommerce_file_download_method', 'Redirect only (Insecure)' );
 		await setCheckbox( '#woocommerce_downloads_require_login' );
 		await setCheckbox( '#woocommerce_downloads_grant_access_after_payment' );
 		await settingsPageSaveChanges();
