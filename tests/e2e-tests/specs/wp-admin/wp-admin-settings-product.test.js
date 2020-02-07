e3afbfdd8c6d96aab211ef3ce5dcd7575d92ae57
Julia Amosova <julia.amosova@gmail.com>
2020-02-07 14:09:56 +0000

    Update `Redirect only` file download method string

--- tests/e2e-tests/specs/wp-admin/wp-admin-settings-product.test.js
+++ tests/e2e-tests/specs/wp-admin/wp-admin-settings-product.test.js
@@ -29,7 +29,7 @@ describe( 'WooCommerce Products > Downloadable Products Settings', () => {
 		// Verify that settings have been saved
 		await Promise.all( [
 			expect( page ).toMatchElement( '#message', { text: 'Your settings have been saved.' } ),
-			expect( page ).toMatchElement( '#woocommerce_file_download_method', { text: 'Redirect only' } ),
+			expect( page ).toMatchElement( '#woocommerce_file_download_method', { text: 'Redirect only (Insecure)' } ),
 			verifyCheckboxIsSet( '#woocommerce_downloads_require_login' ),
 			verifyCheckboxIsSet( '#woocommerce_downloads_grant_access_after_payment' ),
 		] );
