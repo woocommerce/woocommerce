05cfc7eeceae2f6478f1b75fc97a32cfc929e4f1
Rodrigo Primo <rodrigosprimo@gmail.com>
2020-03-04 16:26:07 -0300

    Add @since tag to get_woocommerce_currency_symbols()
    See #25733

--- includes/wc-core-functions.php
+++ includes/wc-core-functions.php
@@ -510,6 +510,7 @@ function get_woocommerce_currencies() {
  *
  * Currency symbols and names should follow the Unicode CLDR recommendation (http://cldr.unicode.org/translation/currency-names)
  *
+ * @since 4.1.0
  * @return array
  */
 function get_woocommerce_currency_symbols() {
