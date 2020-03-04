63a25382dc09fb5d63e85743c4f8b339a13a6e02
vedanshujain <vedanshu.jain.2012@gmail.com>
2020-03-04 14:38:16 +0530

    Adding /vendor directory to release

---
+++ vendor/maxmind-db/reader/src/MaxMind/Db/Reader/InvalidDatabaseException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MaxMind\Db\Reader;
+
+use Exception;
+
+/**
+ * This class should be thrown when unexpected data is found in the database.
+ */
+class InvalidDatabaseException extends Exception
+{
+}
