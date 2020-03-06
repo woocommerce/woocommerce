427a229591c765de7f7be6ba45cbe079003a906e
Albert Juhé Lluveras <aljullu@gmail.com>
2020-03-06 13:27:54 +0100

    Split Full Cart sidebar totals into several components (https://github.com/woocommerce/woocommerce-blocks/pull/1885)

---
+++ plugins/woocommerce-blocks/assets/js/base/components/totals/totals-shipping-item/index.js
@@ -0,0 +1,66 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	DISPLAY_CART_PRICES_INCLUDING_TAX,
+	SHIPPING_ENABLED,
+} from '@woocommerce/block-settings';
+import ShippingCalculator from '@woocommerce/base-components/shipping-calculator';
+import ShippingLocation from '@woocommerce/base-components/shipping-location';
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import TotalsItem from '../totals-item';
+
+const TotalsShippingItem = ( {
+	currency,
+	shippingAddress,
+	updateShippingAddress,
+	values,
+} ) => {
+	if ( ! SHIPPING_ENABLED ) {
+		return null;
+	}
+	const {
+		total_shipping: totalShipping,
+		total_shipping_tax: totalShippingTax,
+	} = values;
+	const shippingValue = parseInt( totalShipping, 10 );
+	const shippingTaxValue = parseInt( totalShippingTax, 10 );
+
+	return (
+		<TotalsItem
+			currency={ currency }
+			description={
+				<>
+					<ShippingLocation address={ shippingAddress } />
+					<ShippingCalculator
+						address={ shippingAddress }
+						setAddress={ updateShippingAddress }
+					/>
+				</>
+			}
+			label={ __( 'Shipping', 'woo-gutenberg-products-block' ) }
+			value={
+				DISPLAY_CART_PRICES_INCLUDING_TAX
+					? shippingValue + shippingTaxValue
+					: shippingValue
+			}
+		/>
+	);
+};
+
+TotalsShippingItem.propTypes = {
+	currency: PropTypes.object.isRequired,
+	shippingAddress: PropTypes.object,
+	updateShippingAddress: PropTypes.func,
+	values: PropTypes.shape( {
+		total_shipping: PropTypes.string,
+		total_shipping_tax: PropTypes.string,
+	} ).isRequired,
+};
+
+export default TotalsShippingItem;
---
+++ plugins/woocommerce-blocks/assets/js/base/components/totals/totals-taxes-item/index.js
@@ -0,0 +1,31 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import TotalsItem from '../totals-item';
+
+const TotalsTaxesItem = ( { currency, values } ) => {
+	const { total_tax: totalTax } = values;
+
+	return (
+		<TotalsItem
+			currency={ currency }
+			label={ __( 'Taxes', 'woo-gutenberg-products-block' ) }
+			value={ parseInt( totalTax, 10 ) }
+		/>
+	);
+};
+
+TotalsTaxesItem.propTypes = {
+	currency: PropTypes.object.isRequired,
+	values: PropTypes.shape( {
+		total_tax: PropTypes.string,
+	} ).isRequired,
+};
+
+export default TotalsTaxesItem;
