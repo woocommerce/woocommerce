f55adc41cb9e094c8fe7bd9a51e44669e5ddc42f
Seghir Nadir <nadir.seghir@gmail.com>
2020-03-12 10:59:43 +0100

    Fix select rates when multiple packages exists (https://github.com/woocommerce/woocommerce-blocks/pull/1945)
    * Select rates when multiple packages exists
    
    * switch to using package_id
    
    * update docs
    
    * ponyfill fromEntries
    
    * revert useShippingRates fromEntries fix

--- plugins/woocommerce-blocks/assets/js/base/hooks/shipping/use-select-shipping-rate.js
+++ plugins/woocommerce-blocks/assets/js/base/hooks/shipping/use-select-shipping-rate.js
@@ -13,21 +13,35 @@ See also: https://github.com/woocommerce/woocommerce-gutenberg-products-block/tr
  * @return {Object} This hook will return an object with two properties:
  * - selectShippingRate    A function that immediately returns the selected
  * rate and dispatches an action generator.
- * - selectedShippingRates An array of selected shipping rates, maintained
+ * - selectedShippingRates An object of selected shipping rates and package id as key, maintained
  * locally by a state and updated optimistically.
  */
 export const useSelectShippingRate = ( shippingRates ) => {
-	const initiallySelectedRates = shippingRates.map(
-		// the API responds with those keys.
-		// eslint-disable-next-line camelcase
-		( p ) => p.shipping_rates.find( ( rate ) => rate.selected )?.rate_id
-	);
+	const initiallySelectedRates = shippingRates
+		.map(
+			// the API responds with those keys.
+			/* eslint-disable camelcase */
+			( p ) => [
+				p.package_id,
+				p.shipping_rates.find( ( rate ) => rate.selected )?.rate_id,
+			]
+			/* eslint-enable */
+		)
+		// A fromEntries ponyfill, creates an object from an array of arrays.
+		.reduce( ( obj, [ key, val ] ) => {
+			obj[ key ] = val;
+			return obj;
+		}, {} );
+
 	const [ selectedShippingRates, setSelectedShipping ] = useState(
 		initiallySelectedRates
 	);
 	const { selectShippingRate } = useDispatch( storeKey );
 	const setRate = ( newShippingRate, packageId ) => {
-		setSelectedShipping( [ newShippingRate ] );
+		setSelectedShipping( {
+			...selectedShippingRates,
+			[ packageId ]: newShippingRate,
+		} );
 		selectShippingRate( newShippingRate, packageId );
 	};
 	return {
