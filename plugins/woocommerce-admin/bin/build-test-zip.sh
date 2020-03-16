f8afb041874f75eeb8921cf08b20a3d25a209777
Timmy Crawford <timmyc@users.noreply.github.com>
2020-03-16 10:12:23 -0700

    Zip: include files instead of exclude (https://github.com/woocommerce/woocommerce-admin/pull/3898)
    Co-authored-by: Paul Sealock <psealock@gmail.com>

--- plugins/woocommerce-admin/bin/build-test-zip.sh
+++ plugins/woocommerce-admin/bin/build-test-zip.sh
@@ -3,6 +3,6 @@
 # Build a test zip from current branch for uploading to test site
 #
 npm run build
-composer install
+composer install --no-dev
 rm woocommerce-admin.zip
 ./bin/make-zip.sh woocommerce-admin.zip
--- plugins/woocommerce-admin/bin/make-zip.sh
+++ plugins/woocommerce-admin/bin/make-zip.sh
@@ -21,52 +21,15 @@ output 2 "Creating archive... 🎁"
 
 ZIP_FILE=$1
 
-# Folders listed first then individual files
-zip -r ${ZIP_FILE} . \
-	-x \
-	.git/\* \
-	tests/\* \
-	bin/\* \
-	config/\* \
-	node_modules/\* \
-	vendor/bin/\* \
-	vendor/dealerdirect/\* \
-	vendor/doctrine/\* \
-	vendor/phar-io/\* \
-	vendor/phpcompatibility/\* \
-	vendor/phpdocumentor/\* \
-	vendor/phpspec/\* \
-	vendor/phpunit/\* \
-	vendor/sebastian/\* \
-	vendor/squizlabs/\* \
-	vendor/theseer/\* \
-	vendor/webmozart/\* \
-	vendor/woocommerce/\* \
-	vendor/wp-coding-standards/\* \
-	.distignore \
-	.editorconfig \
-	.gitignore \
-	.gitlab-ci.yml \
-	.travis.yml \
-	.DS_Store \
-	.zipignore \
-	Thumbs.db \
-	behat.yml \
-	circle.yml \
-	composer.json \
-	composer.lock \
-	Gruntfile.js \
-	package.json \
-	package-lock.json \
-	phpunit.xml \
-	phpunit.xml.dist \
-	multisite.xml \
-	multisite.xml.dist \
-	phpcs.xml \
-	phpcs.xml.dist \
-	README.md \
-	wp-cli.local.yml \
-	yarn.lock \
-	*.sql \
-	*.tar.gz \
-	*.zip
+build_files=$(ls dist/*/*.{js,css})
+
+zip -r ${ZIP_FILE} \
+	woocommerce-admin.php \
+	uninstall.php \
+	includes/ \
+	images/ \
+	$build_files \
+	languages/woocommerce-admin.pot \
+	readme.txt \
+	src/ \
+	vendor/
