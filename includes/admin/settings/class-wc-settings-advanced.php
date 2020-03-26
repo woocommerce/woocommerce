93085e19b64f42eeccc7d4889d5fe9d9708ce10b
Nestor Soriano <konamiman@konamiman.com>
2020-03-26 14:21:44 +0100

    Change the "todo: remove in 4.0" comment in WC_Settings_Rest_API to "5.0"

--- includes/admin/settings/class-wc-settings-advanced.php
+++ includes/admin/settings/class-wc-settings-advanced.php
@@ -488,7 +488,7 @@ class WC_Settings_Advanced extends WC_Settings_Page {
  * WC_Settings_Rest_API class.
  *
  * @deprecated 3.4 in favour of WC_Settings_Advanced.
- * @todo remove in 4.0.
+ * @todo remove in 5.0.
  */
 class WC_Settings_Rest_API extends WC_Settings_Advanced {
 }
