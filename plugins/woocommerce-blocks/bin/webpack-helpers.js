6df879a922d54510890b9fc2b420872719bfb212
Albert Juhé Lluveras <aljullu@gmail.com>
2020-03-31 12:46:38 +0200

    Load product grid styles in WP <=5.2 (https://github.com/woocommerce/woocommerce-blocks/pull/2000)
    * Load product grid styles in WP <=5.2
    
    * Explicitly exclude 'product-list-style' from default builds

--- plugins/woocommerce-blocks/bin/webpack-helpers.js
+++ plugins/woocommerce-blocks/bin/webpack-helpers.js
@@ -115,6 +115,12 @@ const stableMainEntry = {
 	'snackbar-notice-style':
 		'./node_modules/wordpress-components/src/snackbar/style.scss',
 
+	// Styles for grid blocks. WP <=5.2 doesn't have the All Products block,
+	// so this file would not be included if not explicitly declared here.
+	// This file is excluded from the default build so CSS styles are included
+	// in the other the components are imported.
+	'product-list-style': './assets/js/base/components/product-list/style.scss',
+
 	// Blocks
 	'handpicked-products': './assets/js/blocks/handpicked-products/index.js',
 	'product-best-sellers': './assets/js/blocks/product-best-sellers/index.js',
