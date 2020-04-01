0fd403ea20f4807425e3def16af04629162d3d90
Jeff Stieler <jeff.m.stieler@gmail.com>
2020-03-31 18:57:40 -0600

    Fix aria-label attribute. (https://github.com/woocommerce/woocommerce-admin/pull/4034)

--- plugins/woocommerce-admin/client/dashboard/profile-wizard/steps/benefits/logo.js
+++ plugins/woocommerce-admin/client/dashboard/profile-wizard/steps/benefits/logo.js
@@ -13,7 +13,7 @@ class Logo extends Component {
 				viewBox="0 0 161 46"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
-				ariaLabel={ __( 'WooCommerce + Jetpack', 'woocommerce-admin' ) }
+				aria-label={ __( 'WooCommerce + Jetpack', 'woocommerce-admin' ) }
 				className="woocommerce-profile-wizard__benefits-logo"
 			>
 				<path
