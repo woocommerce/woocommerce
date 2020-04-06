999b9335805e8f431f27513846a0e130f3a9753c
Christopher Allford <christopher.allford@automattic.com>
2020-04-06 12:37:10 -0700

    Bumped the version for the admin-cancelled-order template, missed in #25316

--- templates/emails/admin-cancelled-order.php
+++ templates/emails/admin-cancelled-order.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates/Emails
- * @version 3.7.0
+ * @version 4.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
