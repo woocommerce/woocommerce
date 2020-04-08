9742085f0193ef3e611ef1bcf36c6e8e9db74798
Ron Rennick <ron@ronandandrea.com>
2020-04-08 10:05:28 -0300

    move before script to bash

---
+++ tests/e2e-env/bin/before-script-CI.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Setup for Travis CI
+
+if [ -f ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini ]; then
+  phpenv config-rm xdebug.ini
+else
+  echo "xdebug.ini does not exist"
+fi
+
+if [[ ! -z "$WP_VERSION" ]] ; then
+  composer install --no-dev
+  npm explore @woocommerce/e2e-env -- npm run install-wp-tests -- wc_e2e_tests root ' ' localhost $WP_VERSION
+  composer global require "phpunit/phpunit=4.8.*|5.7.*"
+fi
+
+if [[ "$WP_TRAVISCI" == "phpcs" ]] ; then
+  composer install
+fi
