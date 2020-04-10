39a4eed3f78a55a4fd152597526382336d26cc12
Marco Almeida <webdados@users.noreply.github.com>
2020-04-10 10:46:46 +0100

    Fix comment
    Fix comment for PR #26107

--- includes/class-wc-countries.php
+++ includes/class-wc-countries.php
@@ -359,7 +359,7 @@ class WC_Countries {
 		if ( 'eu_vat' === $type ) {
 			$countries[] = 'MC';
 			$countries[] = 'IM';
-			//The UK is still part of the EU VAT zone
+			// The UK is still part of the EU VAT zone.
 			$countries[] = 'GB';
 		}
 
