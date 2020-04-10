9a5b3b353d5eb3aa9e68349ba213454a578ad092
Nestor Soriano <nestor.soriano@automattic.com>
2020-04-10 15:36:54 +0200

    Add the @hack code annotation for tests.
    Now @hack class and method annotations can be used to register
    code hacks as an alternative to using before_ methods.
    The syntax is /* @hack HackClassName param1 param2 */
    where parameters will be passed to the class constructor.
    If the class name ends with "Hack", then that suffix can be
    omitted (e.g. "Foo" can be specified instead of "FooHack").

--- tests/includes/code-hacking/code-hacker-test-hook.php
+++ tests/includes/code-hacking/code-hacker-test-hook.php
@@ -25,6 +25,24 @@ use PHPUnit\Runner\BeforeTestHook;
  *
  *    You can also define a test-specific 'before_{$test_method_name}' hook.
  *    If both exist, first 'before_all' will be executed, then the test-specific one.
+ *
+ * 3. Additionally, you can register hacks via class/method annotations:
+ *
+ *    /**
+ *     * @hack HackClassName param1 param2
+ *     * /
+ *    class Some_Test
+ *    {
+ *       /**
+ *        * @hack HackClassName param1 param2
+ *        * /
+ *       public function test_something() {
+ *       }
+ *    }
+ *
+ *    If the class name ends with 'Hack' you can omit that suffix in the annotation (e.g. 'Foo' instead of 'FooHack').
+ *    Parameters specified after the class name will be passed to the class constructor.
+ *    Hacks defined as class annotations will be applied to all tests.
  */
 final class CodeHackerTestHook implements BeforeTestHook {
 
@@ -33,6 +51,66 @@ final class CodeHackerTestHook implements BeforeTestHook {
 		$class_name  = $parts[0];
 		$method_name = $parts[1];
 
+		// Make code hacker class and individual hack classes visible
+		include_once __DIR__ . '/code-hacker.php';
+		foreach ( glob( __DIR__ . '/hacks/*.php' ) as $hack_class_file ) {
+			include_once $hack_class_file;
+		}
+
+		$this->execute_before_methods( $class_name, $method_name );
+
+		$has_class_annotation_hacks = $this->add_hacks_from_annotations( new ReflectionClass( $class_name ) );
+		$has_method_annotaion_hacks = $this->add_hacks_from_annotations( new ReflectionMethod( $class_name, $method_name ) );
+		if ( $has_class_annotation_hacks || $has_method_annotaion_hacks ) {
+			CodeHacker::enable();
+		}
+	}
+
+	/**
+	 * Apply hacks defined in @hack annotations.
+	 *
+	 * @param object $reflection_object The class or method reflection object whose doc comment will be parsed.
+	 * @return bool True if at least one valid @hack annotation was found.
+	 * @throws Exception
+	 */
+	private function add_hacks_from_annotations( $reflection_object ) {
+		$annotations = \PHPUnit\Util\Test::parseAnnotations( $reflection_object->getDocComment() );
+		$hacks_added = false;
+
+		foreach ( $annotations as $id => $annotation_instances ) {
+			if ( $id !== 'hack' ) {
+				continue;
+			}
+
+			foreach ( $annotation_instances as $annotation ) {
+				preg_match_all( '/"(?:\\\\.|[^\\\\"])*"|\S+/', $annotation, $matches );
+				$params = $matches[0];
+
+				$hack_class = array_shift( $params );
+				if ( ! class_exists( $hack_class ) ) {
+					$original_hack_class = $hack_class;
+					$hack_class         .= 'Hack';
+					if ( ! class_exists( $hack_class ) ) {
+						throw new Exception( "Hack class '{$original_hack_class}' defined via annotation in {$class_name}::{$method_name} doesn't exist." );
+					}
+				}
+
+				CodeHacker::add_hack( new $hack_class( ...$params ) );
+				$hacks_added = true;
+			}
+		}
+
+		return $hacks_added;
+	}
+
+	/**
+	 * Run the 'before_all' and 'before_{test_method_name}' methods in a class.
+	 *
+	 * @param string $class_name Test class name.
+	 * @param string $method_name Test method name.
+	 * @throws ReflectionException
+	 */
+	private function execute_before_methods( $class_name, $method_name ) {
 		$methods = array( 'before_all', "before_{$method_name}" );
 		$methods = array_filter(
 			$methods,
@@ -40,16 +118,6 @@ final class CodeHackerTestHook implements BeforeTestHook {
 				return method_exists( $class_name, $item );
 			}
 		);
-
-		if ( empty( $methods ) ) {
-			return;
-		}
-
-		// Make code hacker class and individual hack classes available to tests
-		include_once __DIR__ . '/code-hacker.php';
-		foreach ( glob( __DIR__ . '/hacks/*.php' ) as $hack_class_file ) {
-			include_once $hack_class_file;
-		}
 
 		$rc = new ReflectionClass( $class_name );
 
