0a980376063a6dfb7a8ae97ef3c01a23da329530
Darren Ethier <darren@roughsmootheng.in>
2020-04-17 11:37:09 -0400

    Add documentation for checkout flow and payment method integrations (https://github.com/woocommerce/woocommerce-blocks/pull/2246)
    * add documentation for checkotu flow and payment method integrations
    
    * Update docs/block-client-apis/README.md
    
    Co-Authored-By: Mike Jolley <mike.jolley@me.com>
    
    * Update docs/block-client-apis/README.md
    
    Co-Authored-By: Mike Jolley <mike.jolley@me.com>
    
    * Update docs/block-client-apis/checkout/checkout-api.md
    
    Co-Authored-By: Mike Jolley <mike.jolley@me.com>
    
    * standardize around capitalized API
    
    * Remove extra dash.
    
    Co-Authored-By: Mike Jolley <mike.jolley@me.com>
    
    * Update docs/block-client-apis/checkout/checkout-api.md
    
    Co-Authored-By: Mike Jolley <mike.jolley@me.com>
    
    * Update docs/block-client-apis/checkout/checkout-api.md
    
    Co-Authored-By: Mike Jolley <mike.jolley@me.com>
    
    * Update docs/block-client-apis/checkout/checkout-api.md
    
    Co-Authored-By: Mike Jolley <mike.jolley@me.com>
    
    * remove 1st person narrative
    
    * various other grammar fixes
    
    * add table of contents to docs
    
    Co-authored-by: Mike Jolley <mike.jolley@me.com>

--- plugins/woocommerce-blocks/assets/js/blocks-registry/payment-methods/registry.js
+++ plugins/woocommerce-blocks/assets/js/blocks-registry/payment-methods/registry.js
@@ -5,11 +5,6 @@ import { assertValidPaymentMethodCreator } from './assertions';
 import { default as PaymentMethodConfig } from './payment-method-config';
 import { default as ExpressPaymentMethodConfig } from './express-payment-method-config';
 
-// currently much leeway is given to the payment method for the shape of their components. We should investigate payment methods
-// using a component creator that is fed a configuration object so that the built component for the payment method is tightly
-// controlled (fitting the ui/ux requirements of the checkout/cart).  Once we know the pattern most payment methods will follow
-// (i.e. fields, event callbacks, validation, etc) this is likely more feasible.
-
 const paymentMethods = {};
 const expressPaymentMethods = {};
 
---
+++ plugins/woocommerce-blocks/docs/extensibility/README.md
@@ -0,0 +1,10 @@
+# Extensibility in WooCommerce Blocks
+
+These documents are all dealing with extensibility in the various WooCommerce Blocks.
+
+## Checkout Block
+
+| Document | Description |
+| ---------- | ---------- |
+[Payment Method Integration](./payment-method-integration.md) | Information about implementing payment methods.
+[Checkout Flow and Events](./checkout-flow-and-events.md) | All about the checkout flow in the checkout block and the various emitted events that can be subscribed to.
