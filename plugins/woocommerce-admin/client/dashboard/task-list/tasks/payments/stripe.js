4fd6a171545900b52c7f67474b97f0c4791551da
Rasmy Nguyen <raz@automattic.com>
2020-04-24 09:28:29 +0900

    Remove auto connect functionality from Stripe payment task (https://github.com/woocommerce/woocommerce-admin/pull/4164)
    * Remove auto connect steps from Stripe payment task
    
    As of May 31 the API for stripe account auto creation will no longer be
    supported. As a result, the Stripe payment on-boarding step will no
    longer be able to auto create accounts for users.
    
    This removes all auto connect functionality from this step, and instead
    falls back to the oAuth setup flow.
    
    See:
    https://wcpay.wordpress.com/2020/02/06/stripe-ending-support-for-the-deferred-account-activation-api-on-may-31/#comment-2080
    
    * Fix logic to decide which connect method to render
    
    When the Stripe component renders for the first time, it
    always returns the manual connect method. This is because there is
    an additional call to requiresManualConfig() in getConnectStep which
    looks for values in activePlugins that won't be present on the initial
    component render.
    
    This removes this check and instead relies on the isPending state to
    help decide to render a connect method or wait for oAuth connectURL
    to finish fetching.
    
    * Remove redundant error modal render method
    
    This removes the error modal render method as it is also no longer
    needed with the removal of auto account creation.
    
    This also removes errorMessage and errorTitle state along with any
    references to these values.

--- plugins/woocommerce-admin/client/dashboard/task-list/tasks/payments/stripe.js
+++ plugins/woocommerce-admin/client/dashboard/task-list/tasks/payments/stripe.js
@@ -1,13 +1,13 @@
 /**
  * External dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import { Component, Fragment } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
 import apiFetch from '@wordpress/api-fetch';
 import { withDispatch } from '@wordpress/data';
 import interpolateComponents from 'interpolate-components';
-import { Button, Modal } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 import { get } from 'lodash';
 
 /**
@@ -20,24 +20,16 @@ import { WCS_NAMESPACE } from 'wc-api/constants';
 import withSelect from 'wc-api/with-select';
 import { PLUGINS_STORE_NAME } from '@woocommerce/data';
 
-/**
- * Internal dependencies
- */
-import { getCountryCode } from 'dashboard/utils';
-
 class Stripe extends Component {
 	constructor( props ) {
 		super( props );
 
 		this.state = {
-			autoConnectFailed: false,
+			oAuthConnectFailed: false,
 			connectURL: null,
-			errorTitle: null,
-			errorMessage: null,
-			isPending: true,
+			isPending: false,
 		};
 
-		this.autoCreateAccount = this.autoCreateAccount.bind( this );
 		this.updateSettings = this.updateSettings.bind( this );
 	}
 
@@ -54,12 +46,6 @@ class Stripe extends Component {
 				this.completeMethod();
 				return;
 			}
-
-			/* eslint-disable react/no-did-mount-set-state */
-			this.setState( {
-				autoConnectFailed: true,
-			} );
-			/* eslint-enable react/no-did-mount-set-state */
 		}
 
 		if ( ! this.requiresManualConfig() ) {
@@ -101,12 +87,12 @@ class Stripe extends Component {
 
 	requiresManualConfig() {
 		const { activePlugins, isJetpackConnected } = this.props;
-		const { autoConnectFailed } = this.state;
+		const { oAuthConnectFailed } = this.state;
 
 		return (
 			! isJetpackConnected ||
 			! activePlugins.includes( 'woocommerce-services' ) ||
-			autoConnectFailed
+			oAuthConnectFailed
 		);
 	}
 
@@ -142,7 +128,7 @@ class Stripe extends Component {
 			} );
 			if ( ! result || ! result.oauthUrl ) {
 				this.setState( {
-					autoConnectFailed: true,
+					oAuthConnectFailed: true,
 					isPending: false,
 				} );
 				return;
@@ -153,126 +139,18 @@ class Stripe extends Component {
 			} );
 		} catch ( error ) {
 			this.setState( {
-				autoConnectFailed: true,
+				oAuthConnectFailed: true,
 				isPending: false,
 			} );
 		}
 	}
 
-	async autoCreateAccount( values ) {
-		const { countryCode } = this.props;
+	renderConnectButton() {
 		const { connectURL } = this.state;
-		const { email } = values;
-
-		try {
-			this.setState( { isPending: true } );
-
-			const result = await apiFetch( {
-				path: WCS_NAMESPACE + '/connect/stripe/account',
-				method: 'POST',
-				data: {
-					email,
-					country: countryCode,
-				},
-			} );
-
-			if ( result ) {
-				this.completeMethod();
-				return;
-			}
-		} catch {
-			if ( ! connectURL ) {
-				const errorTitle = __( 'Stripe', 'woocommerce-admin' );
-				const errorMessage = interpolateComponents( {
-					mixedString: sprintf(
-						__(
-							'We tried to create a Stripe account automatically for {{strong}}%s{{/strong}}, but an error occured. Please try connecting manually to continue.',
-							'woocommerce-admin'
-						),
-						email
-					),
-					components: {
-						strong: <strong />,
-					},
-				} );
-
-				this.setState( {
-					autoConnectFailed: true,
-					errorTitle,
-					errorMessage,
-					isPending: false,
-				} );
-			} else {
-				// An account with that email may exist so send them to Stripe to connect via oAuth.
-				window.location = connectURL;
-			}
-		}
-	}
-
-	renderErrorModal() {
-		const { errorTitle, errorMessage } = this.state;
 		return (
-			<Modal
-				title={ errorTitle }
-				onRequestClose={ () =>
-					this.setState( { errorMessage: null, errorTitle: null } )
-				}
-				className="woocommerce-task-payments__stripe-error-modal"
-			>
-				<div className="woocommerce-task-payments__stripe-error-wrapper">
-					<div className="woocommerce-task-payments__stripe-error-message">
-						{ errorMessage }
-					</div>
-					<Button
-						isPrimary
-						isDefault
-						onClick={ () =>
-							this.setState( {
-								errorMessage: null,
-								errorTitle: null,
-							} )
-						}
-					>
-						{ __( 'OK', 'woocommerce-admin' ) }
-					</Button>
-				</div>
-			</Modal>
-		);
-	}
-
-	renderAutoConnect() {
-		const { isPending } = this.state;
-
-		return (
-			<Form
-				initialValues={ {
-					email: '',
-				} }
-				onSubmitCallback={ this.autoCreateAccount }
-				validate={ this.validateAutoConnect }
-			>
-				{ ( { getInputProps, handleSubmit } ) => {
-					return (
-						<div className="woocommerce-task-payments__woocommerce-services-options">
-							<TextControl
-								label={ __(
-									'Email address',
-									'woocommerce-admin'
-								) }
-								{ ...getInputProps( 'email' ) }
-							/>
-							<Button
-								isPrimary
-								isDefault
-								isBusy={ isPending }
-								onClick={ handleSubmit }
-							>
-								{ __( 'Connect', 'woocommerce-admin' ) }
-							</Button>
-						</div>
-					);
-				} }
-			</Form>
+			<Button isPrimary isDefault href={ connectURL }>
+				{ __( 'Connect', 'woocommerce-admin' ) }
+			</Button>
 		);
 	}
 
@@ -310,16 +188,6 @@ class Stripe extends Component {
 				'Please enter a valid secret key. Valid keys start with "sk_live" or "rk_live".',
 				'woocommerce-admin'
 			);
-		}
-
-		return errors;
-	}
-
-	validateAutoConnect( values ) {
-		const errors = {};
-
-		if ( ! values.email ) {
-			errors.email = __( 'Please enter your email', 'woocommerce-admin' );
 		}
 
 		return errors;
@@ -393,32 +261,25 @@ class Stripe extends Component {
 	}
 
 	getConnectStep() {
-		const { autoConnectFailed, connectURL, errorMessage } = this.state;
+		const { connectURL, isPending, oAuthConnectFailed } = this.state;
+
 		const connectStep = {
 			key: 'connect',
 			label: __( 'Connect your Stripe account', 'woocommerce-admin' ),
 		};
 
-		if ( errorMessage ) {
-			return {
-				...connectStep,
-				content: this.renderErrorModal(),
-			};
+		if ( isPending ) {
+			return connectStep;
 		}
 
-		if ( ! this.requiresManualConfig() ) {
-			// We may still be fetching the connect URL.
-			if ( ! autoConnectFailed && ! connectURL ) {
-				return connectStep;
-			}
-
+		if ( ! oAuthConnectFailed && connectURL ) {
 			return {
 				...connectStep,
 				description: __(
-					'A Stripe account is required to process payments. We’ll create an account for you if you don’t have one already.',
+					'A Stripe account is required to process payments.',
 					'woocommerce-admin'
 				),
-				content: this.renderAutoConnect(),
+				content: this.renderConnectButton(),
 			};
 		}
 
@@ -459,13 +320,7 @@ export default compose(
 		const { getActivePlugins, isJetpackConnected } = select(
 			PLUGINS_STORE_NAME
 		);
-		const options = getOptions( [
-			'woocommerce_stripe_settings',
-			'woocommerce_default_country',
-		] );
-		const countryCode = getCountryCode(
-			options.woocommerce_default_country
-		);
+		const options = getOptions( [ 'woocommerce_stripe_settings' ] );
 		const stripeSettings = get(
 			options,
 			[ 'woocommerce_stripe_settings' ],
@@ -480,7 +335,6 @@ export default compose(
 
 		return {
 			activePlugins: getActivePlugins(),
-			countryCode,
 			hasOptionsError,
 			isJetpackConnected: isJetpackConnected(),
 			isOptionsRequesting,
