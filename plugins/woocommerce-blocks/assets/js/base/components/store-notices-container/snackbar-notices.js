c2a26cb103fde33d4e53f31b82cd1c8ce1100da8
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-04-29 17:45:29 +0200

    Fix Cart block interaction in the editor (mobile) (https://github.com/woocommerce/woocommerce-blocks/pull/2354)
    * Prevent SnackbarList from loading in the editor
    
    * Fix snackbar styles leaking into the editor
    
    * Prevent 'Proceed to Checkout' button being fixed to the bottom in the editor

--- plugins/woocommerce-blocks/assets/js/base/components/store-notices-container/snackbar-notices.js
+++ plugins/woocommerce-blocks/assets/js/base/components/store-notices-container/snackbar-notices.js
@@ -3,12 +3,18 @@
  */
 import { SnackbarList } from 'wordpress-components';
 import { useStoreNotices } from '@woocommerce/base-hooks';
+import { useEditorContext } from '@woocommerce/base-context';
 
 const NoticesContainer = () => {
+	const { isEditor } = useEditorContext();
 	const { notices, removeNotice } = useStoreNotices();
 	const snackbarNotices = notices.filter(
 		( notice ) => notice.type === 'snackbar'
 	);
+
+	if ( isEditor ) {
+		return null;
+	}
 	return (
 		<SnackbarList
 			notices={ snackbarNotices }
