f4e169f21e2c4bee3c17c0160ac2b1b4cc21cff5
vedanshujain <vedanshu.jain.2012@gmail.com>
2020-05-02 00:27:17 +0530

    Moved test specific logic from bootstrap to test file

--- tests/php/includes/admin/helper/class-tests-wc-helper.php
+++ tests/php/includes/admin/helper/class-tests-wc-helper.php
@@ -20,6 +20,11 @@ class WC_Tests_WC_Helper extends WC_Unit_Test_Case {
 		wp_clean_plugins_cache( false );
 		get_plugins();
 
+		if ( file_exists( WP_PLUGIN_DIR . '/sample-woo-plugin.php' ) ) {
+			unlink( WP_PLUGIN_DIR . '/sample-woo-plugin.php' );
+		}
+		copy( WC_Unit_Tests_Bootstrap::instance()->tests_dir . '/data/sample-woo-plugin.php', WP_PLUGIN_DIR . '/sample-woo-plugin.php' );
+
 		add_filter( 'extra_plugin_headers', 'wc_enable_wc_plugin_headers' );
 
 		$woo_plugins = WC_Helper::get_local_woo_plugins();
