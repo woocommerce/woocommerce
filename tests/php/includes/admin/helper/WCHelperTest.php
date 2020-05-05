27ad4a552e343a176a857f2e1f078310fca78c15
vedanshujain <vedanshu.jain.2012@gmail.com>
2020-05-05 19:19:37 +0530

    Rename file and class to resemble PSR4 like convention for consistency.

--- tests/php/includes/admin/helper/WCHelperTest.php
+++ tests/php/includes/admin/helper/WCHelperTest.php
@@ -5,10 +5,12 @@
  * @package WooCommerce|Tests|WC_Helper.
  */
 
+namespace Automattic\WooCommerce;
+
 /**
  * Class WC_Tests_WC_Helper.
  */
-class WC_Tests_WC_Helper extends WC_Unit_Test_Case {
+class WCHelperTest extends \WC_Unit_Test_Case {
 
 	/**
 	 * Test that woo plugins are loaded correctly even if incorrect cache is intially set.
@@ -23,11 +25,11 @@ class WC_Tests_WC_Helper extends WC_Unit_Test_Case {
 		if ( file_exists( WP_PLUGIN_DIR . '/sample-woo-plugin.php' ) ) {
 			unlink( WP_PLUGIN_DIR . '/sample-woo-plugin.php' );
 		}
-		copy( WC_Unit_Tests_Bootstrap::instance()->tests_dir . '/data/sample-woo-plugin.php', WP_PLUGIN_DIR . '/sample-woo-plugin.php' );
+		copy( \WC_Unit_Tests_Bootstrap::instance()->tests_dir . '/data/sample-woo-plugin.php', WP_PLUGIN_DIR . '/sample-woo-plugin.php' );
 
 		add_filter( 'extra_plugin_headers', 'wc_enable_wc_plugin_headers' );
 
-		$woo_plugins = WC_Helper::get_local_woo_plugins();
+		$woo_plugins = \WC_Helper::get_local_woo_plugins();
 
 		// Restore previous state.
 		wp_clean_plugins_cache( false );
