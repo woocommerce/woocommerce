89f843b2b7a2d1202d851abd689a91a4b1cb6d2f
Rua Haszard <rua.haszard@automattic.com>
2020-05-15 11:35:17 +1200

    respect stripe saved cards store setting (https://github.com/woocommerce/woocommerce-blocks/pull/2455)
    * expose a allowSavedCards option to JS on stripe settings data
    
    * hide 'save my card' checkbox if merchant disables saved cards
    
    * use stripe allowSavedCards option to control "Save my card" checkbox
    
    * (linter) remove unnecessary useEffect dependency:
    
    - stripePromise const is defined outside component, so hook doesn't
    depend on it
    
    * clarify comment about saved cards and supports( tokenization )
    
    * correct comment about save card merchant option - doesn't affect paying with previously saved payment methods
    
    * use true boolean value for stripe allow saved cards setting:
    
    previously was using woo option `yes no` style, other JS settings all use true bool
    
    Co-authored-by: Darren Ethier <darren@roughsmootheng.in>
    
    * fix client side check now `allowSavedCards` is true boolean
    
    Co-authored-by: Darren Ethier <darren@roughsmootheng.in>

--- plugins/woocommerce-blocks/src/Payments/Integrations/Stripe.php
+++ plugins/woocommerce-blocks/src/Payments/Integrations/Stripe.php
@@ -104,7 +104,21 @@ final class Stripe extends AbstractPaymentMethodType {
 			],
 			'inline_cc_form'   => $this->get_inline_cc_form(),
 			'icons'            => $this->get_icons(),
+			'allowSavedCards'  => $this->get_allow_saved_cards(),
 		];
+	}
+
+	/**
+	 * Determine if store allows cards to be saved during checkout.
+	 *
+	 * @return bool True if merchant allows shopper to save card (payment method) during checkout).
+	 */
+	private function get_allow_saved_cards() {
+		// This assumes that Stripe supports `tokenization` - currently this is true, based on
+		// https://github.com/woocommerce/woocommerce-gateway-stripe/blob/master/includes/class-wc-gateway-stripe.php#L95 .
+		// See https://github.com/woocommerce/woocommerce-gateway-stripe/blob/ad19168b63df86176cbe35c3e95203a245687640/includes/class-wc-gateway-stripe.php#L271 and
+		// https://github.com/woocommerce/woocommerce/wiki/Payment-Token-API .
+		return apply_filters( 'wc_stripe_display_save_payment_method_checkbox', filter_var( $this->settings['saved_cards'], FILTER_VALIDATE_BOOLEAN ) );
 	}
 
 	/**
