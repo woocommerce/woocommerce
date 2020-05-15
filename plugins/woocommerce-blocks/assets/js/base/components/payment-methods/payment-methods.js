c48518de545eb415c9149ab36defae5ed4c53f71
Seghir Nadir <nadir.seghir@gmail.com>
2020-05-15 19:55:17 +0100

    Use Reakit in payment tabs (https://github.com/woocommerce/woocommerce-blocks/pull/2371)
    * WIP
    
    * convert to reakit
    
    * rebase and fix styling issues
    
    * add forget initialTabName
    
    * delete button and force manual select
    
    * fix git diff problem in package-lock
    
    * directly render tabs
    
    * fix regression
    
    * gaurd against unset values
    
    * update reakit and guard against empty tabs

--- plugins/woocommerce-blocks/assets/js/base/components/payment-methods/payment-methods.js
+++ plugins/woocommerce-blocks/assets/js/base/components/payment-methods/payment-methods.js
@@ -8,11 +8,11 @@ import {
 	useEmitResponse,
 } from '@woocommerce/base-hooks';
 import {
-	useCallback,
 	cloneElement,
 	useRef,
 	useEffect,
 	useState,
+	useCallback,
 } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import {
@@ -120,7 +120,6 @@ const PaymentMethods = () => {
 			customerId,
 		]
 	);
-
 	if (
 		isInitialized &&
 		Object.keys( currentPaymentMethods.current ).length === 0
@@ -147,6 +146,7 @@ const PaymentMethods = () => {
 											.components,
 							  } ),
 					ariaLabel,
+					content: getRenderedTab( name ),
 				};
 			} ) }
 			initialTabName={ activePaymentMethod }
@@ -155,9 +155,7 @@ const PaymentMethods = () => {
 				'woo-gutenberg-products-block'
 			) }
 			id="wc-block-payment-methods"
-		>
-			{ getRenderedTab }
-		</Tabs>
+		/>
 	);
 
 	const renderedSavedPaymentOptions = (
