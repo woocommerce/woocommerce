6d92e096d759c3b70ddc522d92bc96a5973a7c81
Nestor Soriano <nestor.soriano@automattic.com>
2020-05-19 17:12:50 +0200

    Abort db update and don't bump WC version if CREATE/ALTER TABLE fails during install
    If any of the CREATE/ALTER TABLE db statements that run as part of
    the "create tables" stage during a Woo install/update fails, skip the
    db update functions (since they might fail, given that the db
    doesn't have the expected structure) and don't bump the WooCommerce
    and db versions (since that would prevent the pending db updates
    from running at a later time).
    
    Code changes performed in WC_Install to achieve that:
    
    - Add a new `get_dbdelta_queries` method. This is identical(ish) to
      WP's `dbDelta`, except that instead of executing the queries
      it returns them.
    - Modify `create_tables` method so that it enqueues all the required
      manual ALTER TABLE statements, plus the result of get_dbdelta_queries,
      and then executes each of them in a loop. If one fails,
      it returns false immediately. If everything goes well, returns true.
    - Modify `install` method so that if `create_tables` returns false,
      the update functions are not executed and the Woo and db versions
      are not bumped.

--- includes/class-wc-install.php
+++ includes/class-wc-install.php
@@ -286,7 +286,7 @@ class WC_Install {
 
 		WC()->wpdb_table_fix();
 		self::remove_admin_notices();
-		self::create_tables();
+		$table_creation_succeeded = self::create_tables();
 		self::create_options();
 		self::create_roles();
 		self::setup_environment();
@@ -294,13 +294,18 @@ class WC_Install {
 		self::create_cron_jobs();
 		self::create_files();
 		self::maybe_enable_setup_wizard();
-		self::update_wc_version();
-		self::maybe_update_db_version();
+		if ( $table_creation_succeeded ) {
+			self::update_wc_version();
+			self::maybe_update_db_version();
+		}
 
 		delete_transient( 'wc_installing' );
 
 		do_action( 'woocommerce_flush_rewrite_rules' );
-		do_action( 'woocommerce_installed' );
+
+		if ( $table_creation_succeeded ) {
+			do_action( 'woocommerce_installed' );
+		}
 	}
 
 	/**
@@ -634,17 +639,23 @@ class WC_Install {
 	private static function create_tables() {
 		global $wpdb;
 
+		// phpcs:disable WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+
 		$wpdb->hide_errors();
 
 		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+		$schema_change_queries = array();
 
 		/**
 		 * Before updating with DBDELTA, remove any primary keys which could be
 		 * modified due to schema updates.
 		 */
-		if ( $wpdb->get_var( "SHOW TABLES LIKE '{$wpdb->prefix}woocommerce_downloadable_product_permissions';" ) ) {
-			if ( ! $wpdb->get_var( "SHOW COLUMNS FROM `{$wpdb->prefix}woocommerce_downloadable_product_permissions` LIKE 'permission_id';" ) ) {
-				$wpdb->query( "ALTER TABLE {$wpdb->prefix}woocommerce_downloadable_product_permissions DROP PRIMARY KEY, ADD `permission_id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT;" );
+		$product_permissions_table = "{$wpdb->prefix}woocommerce_downloadable_product_permissions";
+		if ( $wpdb->get_var( "SHOW TABLES LIKE '$product_permissions_table';" ) ) {
+			if ( ! $wpdb->get_var( "SHOW COLUMNS FROM `$product_permissions_table` LIKE 'permission_id';" ) ) {
+				$schema_change_queries[ $product_permissions_table ][] =
+					"ALTER TABLE $product_permissions_table DROP PRIMARY KEY, ADD `permission_id` BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT;";
 			}
 		}
 
@@ -656,22 +667,23 @@ class WC_Install {
 		 * This query needs to run before dbDelta() as this WP function is not able to handle primary key changes
 		 * (see https://github.com/woocommerce/woocommerce/issues/21534 and https://core.trac.wordpress.org/ticket/40357).
 		 */
-		if ( $wpdb->get_var( "SHOW TABLES LIKE '{$wpdb->prefix}woocommerce_sessions'" ) ) {
-			if ( ! $wpdb->get_var( "SHOW KEYS FROM {$wpdb->prefix}woocommerce_sessions WHERE Key_name = 'PRIMARY' AND Column_name = 'session_id'" ) ) {
-				$wpdb->query(
-					"ALTER TABLE `{$wpdb->prefix}woocommerce_sessions` DROP PRIMARY KEY, DROP KEY `session_id`, ADD PRIMARY KEY(`session_id`), ADD UNIQUE KEY(`session_key`)"
-				);
+		$sessions_table = "{$wpdb->prefix}woocommerce_sessions";
+		if ( $wpdb->get_var( "SHOW TABLES LIKE '$sessions_table'" ) ) {
+			if ( ! $wpdb->get_var( "SHOW KEYS FROM $sessions_table WHERE Key_name = 'PRIMARY' AND Column_name = 'session_id'" ) ) {
+				$schema_change_queries[ $sessions_table ][] =
+					"ALTER TABLE `$sessions_table` DROP PRIMARY KEY, DROP KEY `session_id`, ADD PRIMARY KEY(`session_id`), ADD UNIQUE KEY(`session_key`)";
 			}
 		}
 
-		dbDelta( self::get_schema() );
+		// Get the main schema creation/alteration queries.
+		$schema                = self::get_schema();
+		$schema_change_queries = array_merge_recursive( $schema_change_queries, self::get_dbdelta_queries( $schema ) );
 
+		// Add an index to the field comment_type to improve the response time of the query
+		// used by WC_Comments::wp_count_comments() to get the number of comments by type.
 		$index_exists = $wpdb->get_row( "SHOW INDEX FROM {$wpdb->comments} WHERE column_name = 'comment_type' and key_name = 'woo_idx_comment_type'" );
-
 		if ( is_null( $index_exists ) ) {
-			// Add an index to the field comment_type to improve the response time of the query
-			// used by WC_Comments::wp_count_comments() to get the number of comments by type.
-			$wpdb->query( "ALTER TABLE {$wpdb->comments} ADD INDEX woo_idx_comment_type (comment_type)" );
+			$schema_change_queries[ $wpdb->comments ][] = "ALTER TABLE {$wpdb->comments} ADD INDEX woo_idx_comment_type (comment_type)";
 		}
 
 		// Get tables data types and check it matches before adding constraint.
@@ -698,17 +710,366 @@ class WC_Install {
 				AND TABLE_NAME = '{$wpdb->prefix}wc_download_log'"
 			);
 			if ( 0 === (int) $fk_result->fk_count ) {
-				$wpdb->query(
-					"ALTER TABLE `{$wpdb->prefix}wc_download_log`
+				$download_log_table                             = "{$wpdb->prefix}wc_download_log";
+				$schema_change_queries[ $download_log_table ][] =
+					"ALTER TABLE `$download_log_table`
 					ADD CONSTRAINT `fk_{$wpdb->prefix}wc_download_log_permission_id`
 					FOREIGN KEY (`permission_id`)
-					REFERENCES `{$wpdb->prefix}woocommerce_downloadable_product_permissions` (`permission_id`) ON DELETE CASCADE;"
-				);
+					REFERENCES `{$wpdb->prefix}woocommerce_downloadable_product_permissions` (`permission_id`) ON DELETE CASCADE;";
+			}
+		}
+
+		// We have all the required schema change commands, now execute them.
+		foreach ( $schema_change_queries as $table_name => $queries ) {
+			foreach ( $queries as $query ) {
+				// phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
+				$success = $wpdb->query( $query );
+				if ( ! $success ) {
+					return false;
+				}
 			}
 		}
 
 		// Clear table caches.
 		delete_transient( 'wc_attribute_taxonomies' );
+
+		return true;
+
+		// phpcs:enable WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+	}
+
+	// phpcs:ignore Generic.Commenting.Todo.TaskFound
+	// TODO: Submit a patch for WordPress' dbDelta with so that it can return the queries instead of executing them, then get_dbdelta_queries method won't be necessary.
+
+	/**
+	 * This method is identical to WordPress' dbDelta function except that:
+	 *
+	 * - It doesn't execute any database schema modification query.
+	 * - It returns an array with the database schema modification queries generated,
+	 *   instead of information messages.
+	 * - It accepts only CREATE TABLE and ALTER TABLE statements as input, any other
+	 *   type of statement will throw an exception.
+	 * - It doesn't execute any filter.
+	 *
+	 * @param array|string $queries An array of queries, or a string with all the quieries concatenated with ';'.
+	 * @return array An associative array where the keys are table names, and values are CREATE/ALTER TABLE commands for that table.
+	 * @throws Exception A query that is neither CREATE TABLE nor ALTER TABLE has been supplied in $queries.
+	 */
+	private static function get_dbdelta_queries( $queries ) {
+		global $wpdb;
+
+		// phpcs:disable WordPress.DB.PreparedSQL.InterpolatedNotPrepared, WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+
+		// Separate individual queries into an array.
+		if ( ! is_array( $queries ) ) {
+			$queries = explode( ';', $queries );
+			$queries = array_filter( $queries );
+		}
+
+		$cqueries = array(); // CREATE TABLE queries.
+		$aqueries = array(); // ALTER TABLE queries.
+
+		// Create a tablename index for an array ($cqueries) of queries.
+		foreach ( $queries as $qry ) {
+			$qry = trim( $qry );
+			if ( preg_match( '|^CREATE TABLE ([^ ]*)|', $qry, $matches ) ) {
+				$cqueries[ trim( $matches[1], '`' ) ] = $qry;
+			} elseif ( preg_match( '|^ALTER TABLE ([^ ]*)|', $qry, $matches ) ) {
+				$aqueries[ trim( $matches[1], '`' ) ][] = $qry;
+			} elseif ( ! empty( $qry ) ) {
+				throw new Exception( 'WC_Install::get_dbdelta_queries: invalid query, only CREATE TABLE and ALTER TABLE are allowed. Query stats with ' . str_replace( "\n", ' ', ( substr( $qry, 30 ) ) ) );
+			}
+		}
+
+		$text_fields = array( 'tinytext', 'text', 'mediumtext', 'longtext' );
+		$blob_fields = array( 'tinyblob', 'blob', 'mediumblob', 'longblob' );
+
+		$global_tables = $wpdb->tables( 'global' );
+		foreach ( $cqueries as $table => $qry ) {
+			// Upgrade global tables only for the main site. Don't upgrade at all if conditions are not optimal.
+			if ( in_array( $table, $global_tables, true ) && ! wp_should_upgrade_global_tables() ) {
+				unset( $cqueries[ $table ] );
+				continue;
+			}
+
+			// Fetch the table column structure from the database.
+			$suppress    = $wpdb->suppress_errors();
+			$tablefields = $wpdb->get_results( "DESCRIBE {$table};" );
+			$wpdb->suppress_errors( $suppress );
+
+			if ( ! $tablefields ) {
+				unset( $cqueries[ $table ] );
+				$cqueries[ $table ][] = $qry;
+				continue;
+			}
+
+			// Clear the field and index arrays.
+			$cfields                  = array();
+			$indices                  = array();
+			$indices_without_subparts = array();
+
+			// Get all of the field names in the query from between the parentheses.
+			preg_match( '|\((.*)\)|ms', $qry, $match2 );
+			$qryline = trim( $match2[1] );
+
+			// Separate field lines into an array.
+			$flds = explode( "\n", $qryline );
+
+			// For every field line specified in the query.
+			foreach ( $flds as $fld ) {
+				$fld = trim( $fld, " \t\n\r\0\x0B," ); // Default trim characters, plus ','.
+
+				// Extract the field name.
+				preg_match( '|^([^ ]*)|', $fld, $fvals );
+				$fieldname            = trim( $fvals[1], '`' );
+				$fieldname_lowercased = strtolower( $fieldname );
+
+				// Verify the found field name.
+				$validfield = true;
+				switch ( $fieldname_lowercased ) {
+					case '':
+					case 'primary':
+					case 'index':
+					case 'fulltext':
+					case 'unique':
+					case 'key':
+					case 'spatial':
+						$validfield = false;
+
+						/*
+						 * Normalize the index definition.
+						 *
+						 * This is done so the definition can be compared against the result of a
+						 * `SHOW INDEX FROM $table_name` query which returns the current table
+						 * index information.
+						 */
+
+						// Extract type, name and columns from the definition.
+						// phpcs:disable Squiz.Strings.ConcatenationSpacing.PaddingFound -- don't remove regex indentation
+						preg_match(
+							'/^'
+							.   '(?P<index_type>'             // 1) Type of the index.
+							.       'PRIMARY\s+KEY|(?:UNIQUE|FULLTEXT|SPATIAL)\s+(?:KEY|INDEX)|KEY|INDEX'
+							.   ')'
+							.   '\s+'                         // Followed by at least one white space character.
+							.   '(?:'                         // Name of the index. Optional if type is PRIMARY KEY.
+							.       '`?'                      // Name can be escaped with a backtick.
+							.           '(?P<index_name>'     // 2) Name of the index.
+							.               '(?:[0-9a-zA-Z$_-]|[\xC2-\xDF][\x80-\xBF])+'
+							.           ')'
+							.       '`?'                      // Name can be escaped with a backtick.
+							.       '\s+'                     // Followed by at least one white space character.
+							.   ')*'
+							.   '\('                          // Opening bracket for the columns.
+							.       '(?P<index_columns>'
+							.           '.+?'                 // 3) Column names, index prefixes, and orders.
+							.       ')'
+							.   '\)'                          // Closing bracket for the columns.
+							. '$/im',
+							$fld,
+							$index_matches
+						);
+						// phpcs:enable Squiz.Strings.ConcatenationSpacing.PaddingFound
+
+						// Uppercase the index type and normalize space characters.
+						$index_type = strtoupper( preg_replace( '/\s+/', ' ', trim( $index_matches['index_type'] ) ) );
+
+						// 'INDEX' is a synonym for 'KEY', standardize on 'KEY'.
+						$index_type = str_replace( 'INDEX', 'KEY', $index_type );
+
+						// Escape the index name with backticks. An index for a primary key has no name.
+						$index_name = ( 'PRIMARY KEY' === $index_type ) ? '' : '`' . strtolower( $index_matches['index_name'] ) . '`';
+
+						// Parse the columns. Multiple columns are separated by a comma.
+						$index_columns                  = array_map( 'trim', explode( ',', $index_matches['index_columns'] ) );
+						$index_columns_without_subparts = $index_columns;
+
+						// Normalize columns.
+						foreach ( $index_columns as $id => &$index_column ) {
+							// Extract column name and number of indexed characters (sub_part).
+							preg_match(
+								'/'
+								. '`?'                      // Name can be escaped with a backtick.
+								. '(?P<column_name>'    // 1) Name of the column.
+								. '(?:[0-9a-zA-Z$_-]|[\xC2-\xDF][\x80-\xBF])+'
+								. ')'
+								. '`?'                      // Name can be escaped with a backtick.
+								. '(?:'                     // Optional sub part.
+								. '\s*'                 // Optional white space character between name and opening bracket.
+								. '\('                  // Opening bracket for the sub part.
+								. '\s*'             // Optional white space character after opening bracket.
+								. '(?P<sub_part>'
+								. '\d+'         // 2) Number of indexed characters.
+								. ')'
+								. '\s*'             // Optional white space character before closing bracket.
+								. '\)'                 // Closing bracket for the sub part.
+								. ')?'
+								. '/',
+								$index_column,
+								$index_column_matches
+							);
+
+							// Escape the column name with backticks.
+							$index_column = '`' . $index_column_matches['column_name'] . '`';
+
+							// We don't need to add the subpart to $index_columns_without_subparts.
+							$index_columns_without_subparts[ $id ] = $index_column;
+
+							// Append the optional sup part with the number of indexed characters.
+							if ( isset( $index_column_matches['sub_part'] ) ) {
+								$index_column .= '(' . $index_column_matches['sub_part'] . ')';
+							}
+						}
+
+						// Build the normalized index definition and add it to the list of indices.
+						$indices[]                  = "{$index_type} {$index_name} (" . implode( ',', $index_columns ) . ')';
+						$indices_without_subparts[] = "{$index_type} {$index_name} (" . implode( ',', $index_columns_without_subparts ) . ')';
+
+						// Destroy no longer needed variables.
+						unset( $index_column, $index_column_matches, $index_matches, $index_type, $index_name, $index_columns, $index_columns_without_subparts );
+
+						break;
+				}
+
+				// If it's a valid field, add it to the field array.
+				if ( $validfield ) {
+					$cfields[ $fieldname_lowercased ] = $fld;
+				}
+			}
+
+			// For every field in the table.
+			foreach ( $tablefields as $tablefield ) {
+				$tablefield_field_lowercased = strtolower( $tablefield->Field );
+				$tablefield_type_lowercased  = strtolower( $tablefield->Type );
+
+				// If the table field exists in the field array...
+				if ( array_key_exists( $tablefield_field_lowercased, $cfields ) ) {
+
+					// Get the field type from the query.
+					preg_match( '|`?' . $tablefield->Field . '`? ([^ ]*( unsigned)?)|i', $cfields[ $tablefield_field_lowercased ], $matches );
+					$fieldtype            = $matches[1];
+					$fieldtype_lowercased = strtolower( $fieldtype );
+
+					// Is actual field type different from the field type in query?
+					if ( $tablefield->Type !== $fieldtype ) {
+						$do_change = true;
+						if ( in_array( $fieldtype_lowercased, $text_fields, true ) && in_array( $tablefield_type_lowercased, $text_fields, true ) ) {
+							if ( array_search( $fieldtype_lowercased, $text_fields, true ) < array_search( $tablefield_type_lowercased, $text_fields, true ) ) {
+								$do_change = false;
+							}
+						}
+
+						if ( in_array( $fieldtype_lowercased, $blob_fields, true ) && in_array( $tablefield_type_lowercased, $blob_fields, true ) ) {
+							if ( array_search( $fieldtype_lowercased, $blob_fields, true ) < array_search( $tablefield_type_lowercased, $blob_fields, true ) ) {
+								$do_change = false;
+							}
+						}
+
+						if ( $do_change ) {
+							// Add a query to change the column type.
+							$aqueries[ $table ][] = "ALTER TABLE {$table} CHANGE COLUMN `{$tablefield->Field}` " . $cfields[ $tablefield_field_lowercased ];
+						}
+					}
+
+					// Get the default value from the array.
+					if ( preg_match( "| DEFAULT '(.*?)'|i", $cfields[ $tablefield_field_lowercased ], $matches ) ) {
+						$default_value = $matches[1];
+						if ( $tablefield->Default !== $default_value ) {
+							// Add a query to change the column's default value.
+							$aqueries[ $table ][] = "ALTER TABLE {$table} ALTER COLUMN `{$tablefield->Field}` SET DEFAULT '{$default_value}'";
+						}
+					}
+
+					// Remove the field from the array (so it's not added).
+					unset( $cfields[ $tablefield_field_lowercased ] );
+				}
+				// else - This field exists in the table, but not in the creation queries?
+			}
+
+			// For every remaining field specified for the table.
+			foreach ( $cfields as $fieldname => $fielddef ) {
+				// Push a query line into $cqueries that adds the field to that table.
+				$aqueries[ $table ][] = "ALTER TABLE {$table} ADD COLUMN $fielddef";
+			}
+
+			// Index stuff goes here. Fetch the table index structure from the database.
+			$tableindices = $wpdb->get_results( "SHOW INDEX FROM {$table};" );
+
+			if ( $tableindices ) {
+				// Clear the index array.
+				$index_ary = array();
+
+				// For every index in the table.
+				foreach ( $tableindices as $tableindex ) {
+					$keyname = strtolower( $tableindex->Key_name );
+
+					// Add the index to the index data array.
+					$index_ary[ $keyname ]['columns'][]  = array(
+						'fieldname' => $tableindex->Column_name,
+						'subpart'   => $tableindex->Sub_part,
+					);
+					$index_ary[ $keyname ]['unique']     = ( 0 === intval( $tableindex->Non_unique ) ) ? true : false;
+					$index_ary[ $keyname ]['index_type'] = $tableindex->Index_type;
+				}
+
+				// For each actual index in the index array.
+				foreach ( $index_ary as $index_name => $index_data ) {
+
+					// Build a create string to compare to the query.
+					$index_string = '';
+					if ( 'primary' === $index_name ) {
+						$index_string .= 'PRIMARY ';
+					} elseif ( $index_data['unique'] ) {
+						$index_string .= 'UNIQUE ';
+					}
+					if ( 'FULLTEXT' === strtoupper( $index_data['index_type'] ) ) {
+						$index_string .= 'FULLTEXT ';
+					}
+					if ( 'SPATIAL' === strtoupper( $index_data['index_type'] ) ) {
+						$index_string .= 'SPATIAL ';
+					}
+					$index_string .= 'KEY ';
+					if ( 'primary' !== $index_name ) {
+						$index_string .= '`' . $index_name . '`';
+					}
+					$index_columns = '';
+
+					// For each column in the index.
+					foreach ( $index_data['columns'] as $column_data ) {
+						if ( '' !== $index_columns ) {
+							$index_columns .= ',';
+						}
+
+						// Add the field to the column list string.
+						$index_columns .= '`' . $column_data['fieldname'] . '`';
+					}
+
+					// Add the column list to the index create string.
+					$index_string .= " ($index_columns)";
+
+					// Check if the index definition exists, ignoring subparts.
+					$aindex = array_search( $index_string, $indices_without_subparts, true );
+					if ( false !== $aindex ) {
+						// If the index already exists (even with different subparts), we don't need to create it.
+						unset( $indices_without_subparts[ $aindex ] );
+						unset( $indices[ $aindex ] );
+					}
+				}
+			}
+
+			// For every remaining index specified for the table.
+			foreach ( (array) $indices as $index ) {
+				// Push a query line into $cqueries that adds the index to that table.
+				$aqueries[ $table ][] = "ALTER TABLE {$table} ADD $index";
+			}
+
+			// Remove the original table creation query from processing.
+			unset( $cqueries[ $table ] );
+		}
+
+		return array_merge( $cqueries, $aqueries );
+
+		// phpcs:enable WordPress.DB.PreparedSQL.InterpolatedNotPrepared, WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 	}
 
 	/**
