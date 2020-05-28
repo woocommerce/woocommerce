cb7f540d1dc892683afd10bdbd985ec953c3eb94
Mike Jolley <mike.jolley@me.com>
2020-05-28 12:41:10 +0100

    Single Product Block Edit Mode (https://github.com/woocommerce/woocommerce-blocks/pull/2577)
    * Previews
    
    * Edit mode
    
    * Remove preview alt text
    
    * Remove unused styles

---
+++ plugins/woocommerce-blocks/assets/js/blocks/single-product/edit/layout-editor.js
@@ -0,0 +1,8 @@
+/**
+ * Component to handle edit mode of the "Single Product Block".
+ */
+const LayoutEditor = () => {
+	return null;
+};
+
+export default LayoutEditor;
--- plugins/woocommerce-blocks/assets/js/previews/checkout-block.js
+++ plugins/woocommerce-blocks/assets/js/previews/checkout-block.js
@@ -6,7 +6,7 @@ import { WC_BLOCKS_ASSET_URL } from '@woocommerce/block-settings';
 export const checkoutBlockPreview = (
 	<img
 		src={ WC_BLOCKS_ASSET_URL + 'img/checkout-preview.svg' }
-		alt="Checkout Preview"
+		alt=""
 		width="230"
 		height="250"
 		style={ {
