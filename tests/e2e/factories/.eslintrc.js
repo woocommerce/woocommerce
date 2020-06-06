1418b68910600968ae6ada9380d9a2df3177ef30
Christopher Allford <christopher.allford@automattic.com>
2020-06-06 16:44:44 -0700

    Initial configuration for e2e-factories package

---
+++ tests/e2e/factories/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	env: {
+		'jest/globals': true
+	},
+	ignorePatterns: [
+		'dist/',
+		'node_modules/'
+	],
+	rules: {
+		'no-unused-vars': 'off',
+	},
+	extends: [
+		'plugin:@wordpress/eslint-plugin/recommended',
+		'plugin:jest/recommended'
+	],
+}
