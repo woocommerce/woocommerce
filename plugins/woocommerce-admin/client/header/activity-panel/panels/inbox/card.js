7d42d389b88367581d4a9785411ab52a3eb78354
Fernando <ultimoround@gmail.com>
2020-06-10 10:32:55 -0300

    Inbox notification: fixed Dismiss dropdowns for IE 11 (https://github.com/woocommerce/woocommerce-admin/pull/4504)
    * Added refactor in "handleBlur" for IE compatibility
    
    This commit adds a refactor in the method "handleBlur" for IE compatibility
    
    * Modified styles for IE compatibility
    
    Co-authored-by: Fernando Marichal <contacto@fernandomarichal.com>

--- plugins/woocommerce-admin/client/header/activity-panel/panels/inbox/card.js
+++ plugins/woocommerce-admin/client/header/activity-panel/panels/inbox/card.js
@@ -131,9 +131,13 @@ class InboxNoteCard extends Component {
 			'woocommerce-admin-dismiss-notification',
 			'components-popover__content',
 		];
-		const isClickOutsideDropdown = event.relatedTarget
+		// This line is for IE compatibility.
+		const relatedTarget = event.relatedTarget
+			? event.relatedTarget
+			: document.activeElement;
+		const isClickOutsideDropdown = relatedTarget
 			? dropdownClasses.some( ( className ) =>
-					event.relatedTarget.className.includes( className )
+					relatedTarget.className.includes( className )
 			  )
 			: false;
 		if ( isClickOutsideDropdown ) {
@@ -146,6 +150,7 @@ class InboxNoteCard extends Component {
 	renderDismissButton() {
 		return (
 			<Dropdown
+				contentClassName="woocommerce-admin-dismiss-dropdown"
 				position="bottom right"
 				renderToggle={ ( { onClose, onToggle } ) => (
 					<Button
