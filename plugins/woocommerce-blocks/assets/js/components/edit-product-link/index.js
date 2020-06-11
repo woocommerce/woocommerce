74ca82dd3bc39b16b151c211da57a9d384fda20f
Mike Jolley <mike.jolley@me.com>
2020-06-11 10:02:00 +0100

    Stock Indicator Block (https://github.com/woocommerce/woocommerce-blocks/pull/2675)
    * Stock Indicator Block
    
    * Add to block map
    
    * Put behind flag
    
    * Move EditProductLink context
    
    * stock text props

--- plugins/woocommerce-blocks/assets/js/components/edit-product-link/index.js
+++ plugins/woocommerce-blocks/assets/js/components/edit-product-link/index.js
@@ -5,11 +5,22 @@ import { __ } from '@wordpress/i18n';
 import { Icon, external } from '@woocommerce/icons';
 import { ADMIN_URL } from '@woocommerce/settings';
 import { InspectorControls } from '@wordpress/block-editor';
+import { useProductDataContext } from '@woocommerce/shared-context';
 
 /**
  * Component to render an edit product link in the sidebar.
+ *
+ * @param {Object} props Component props.
  */
-const EditProductLink = ( { productId } ) => {
+const EditProductLink = ( props ) => {
+	const productDataContext = useProductDataContext();
+	const product = productDataContext.product || {};
+	const productId = product.id || props.productId || 0;
+
+	if ( ! productId ) {
+		return null;
+	}
+
 	return (
 		<InspectorControls>
 			<div className="wc-block-single-product__edit-card">
