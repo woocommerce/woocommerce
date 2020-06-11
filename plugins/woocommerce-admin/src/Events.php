8854a71d9c24a0e62cac8249b555300b2d95aacc
Bec Scott <bec@belfryimages.com.au>
2020-06-11 10:08:26 +1000

    Add First product admin note (https://github.com/woocommerce/woocommerce-admin/pull/4525)
    * Add First product admin note
    
    * Add "not setting up for client" rule
    
    Co-authored-by: Rebecca Scott <me@becdetat.com>

--- plugins/woocommerce-admin/src/Events.php
+++ plugins/woocommerce-admin/src/Events.php
@@ -26,6 +26,7 @@ use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_Migrate_From_Shopify;
 use \Automattic\WooCommerce\Admin\RemoteInboxNotifications\DataSourcePoller;
 use \Automattic\WooCommerce\Admin\RemoteInboxNotifications\RemoteInboxNotificationsEngine;
 use \Automattic\WooCommerce\Admin\Loader;
+use \Automattic\WooCommerce\Admin\Notes\WC_Admin_Notes_First_Product;
 
 /**
  * WC_Admin_Events Class.
@@ -83,6 +84,7 @@ class Events {
 		WC_Admin_Notes_Start_Dropshipping_Business::possibly_add_note();
 		WC_Admin_Notes_WooCommerce_Subscriptions::possibly_add_note();
 		WC_Admin_Notes_Migrate_From_Shopify::possibly_add_note();
+		WC_Admin_Notes_First_Product::possibly_add_note();
 
 		if ( Loader::is_feature_enabled( 'remote-inbox-notifications' ) ) {
 			DataSourcePoller::read_specs_from_data_sources();
