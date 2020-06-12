9bfa9599e661361f7593b119ac944d9d594ed777
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2020-06-12 15:29:35 +0200

    Fix WPCS sniff exclusion
    Checking `templates/checkout/form-pay.php` against the WordPress Coding Standards throws an error because the `WordPress.WP.GlobalVariablesOverride.OverrideProhibited` error code has been replaced by the `WordPress.WP.GlobalVariablesOverride.Prohibited` error code in WPCS v2.0.0.

--- templates/myaccount/orders.php
+++ templates/myaccount/orders.php
@@ -35,7 +35,7 @@ do_action( 'woocommerce_before_account_orders', $has_orders ); ?>
 		<tbody>
 			<?php
 			foreach ( $customer_orders->orders as $customer_order ) {
-				$order      = wc_get_order( $customer_order ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited
+				$order      = wc_get_order( $customer_order ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 				$item_count = $order->get_item_count() - $order->get_item_count_refunded();
 				?>
 				<tr class="woocommerce-orders-table__row woocommerce-orders-table__row--status-<?php echo esc_attr( $order->get_status() ); ?> order">
