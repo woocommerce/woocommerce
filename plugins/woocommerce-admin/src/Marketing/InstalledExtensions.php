2aa59bc8e68c9f8dc6ea0da48ec7a579119c5f13
Jason Conroy <jason@findingsimple.com>
2020-06-12 11:47:25 +0930

    Update hubspot-for-woocommerce extension info (https://github.com/woocommerce/woocommerce-admin/pull/4551)

--- plugins/woocommerce-admin/src/Marketing/InstalledExtensions.php
+++ plugins/woocommerce-admin/src/Marketing/InstalledExtensions.php
@@ -68,7 +68,7 @@ class InstalledExtensions {
 			'mailchimp-for-woocommerce',
 			'facebook-for-woocommerce',
 			'kliken-marketing-for-google',
-			'hubwoo-integration',
+			'hubspot-for-woocommerce',
 			'woocommerce-amazon-ebay-integration',
 		];
 	}
@@ -190,7 +190,7 @@ class InstalledExtensions {
 	 * @return array|bool
 	 */
 	protected static function get_hubspot_extension_data() {
-		$slug = 'hubwoo-integration';
+		$slug = 'hubspot-for-woocommerce';
 
 		if ( ! PluginsHelper::is_plugin_installed( $slug ) ) {
 			return false;
@@ -207,7 +207,7 @@ class InstalledExtensions {
 			}
 
 			$data['settingsUrl'] = admin_url( 'admin.php?page=hubwoo' );
-			$data['docsUrl']     = 'https://docs.makewebbetter.com/hubspot-woocommerce-integration/';
+			$data['docsUrl']     = 'https://docs.makewebbetter.com/hubspot-integration-for-woocommerce/';
 		}
 
 		return $data;
