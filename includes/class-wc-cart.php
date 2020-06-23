541d9ca483238e2922e3e7e7612e41beec3ffc23
vedanshujain <vedanshu.jain.2012@gmail.com>
2020-06-23 18:33:58 +0530

    Remove individual rounding because lines would have already rounded.
    When rounding at subtotal is enabled, line items are already rounded where they need to, so we only should do any further rounding when displaying values and no where else.
    
    This is part of our larger attempt to remove individual rounding as much as possible.

--- includes/class-wc-cart.php
+++ includes/class-wc-cart.php
@@ -1902,7 +1902,7 @@ class WC_Cart extends WC_Legacy_Cart {
 			if ( ! $compound && WC_Tax::is_compound( $key ) ) {
 				continue;
 			}
-			$total += wc_round_tax_total( $tax );
+			$total += $tax;
 		}
 		if ( $display ) {
 			$total = wc_format_decimal( $total, wc_get_price_decimals() );
