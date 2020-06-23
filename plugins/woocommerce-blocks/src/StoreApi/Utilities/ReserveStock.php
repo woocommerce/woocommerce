41c90a7bd7aa9097015be6a6caf7f44547a033aa
Mike Jolley <mike.jolley@me.com>
2020-06-23 11:35:10 +0100

    Update stock when a row already exists (https://github.com/woocommerce/woocommerce-blocks/pull/2747)

--- plugins/woocommerce-blocks/src/StoreApi/Utilities/ReserveStock.php
+++ plugins/woocommerce-blocks/src/StoreApi/Utilities/ReserveStock.php
@@ -165,7 +165,7 @@ final class ReserveStock {
 				INSERT INTO {$wpdb->wc_reserved_stock} ( `order_id`, `product_id`, `stock_quantity`, `timestamp`, `expires` )
 				SELECT %d, %d, %d, NOW(), ( NOW() + INTERVAL %d MINUTE ) FROM DUAL
 				WHERE ( $query_for_stock FOR UPDATE ) - ( $query_for_reserved_stock FOR UPDATE ) >= %d
-				ON DUPLICATE KEY UPDATE `expires` = VALUES( `expires` )
+				ON DUPLICATE KEY UPDATE `expires` = VALUES( `expires` ), `stock_quantity` = VALUES( `stock_quantity` )
 				",
 				$order->get_id(),
 				$product_id,
