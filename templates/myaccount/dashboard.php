c3afafee76620d84f11b8233dc622e8e03591f46
Claudio Sanches <contato@claudiosanches.com>
2020-06-24 11:53:39 -0300

    Update template version

--- templates/myaccount/dashboard.php
+++ templates/myaccount/dashboard.php
@@ -12,23 +12,24 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see         https://docs.woocommerce.com/document/template-structure/
- * @package     WooCommerce/Templates
- * @version     4.3.0
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates
+ * @version 4.4.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$allowed_html = array(
+	'a' => array(
+		'href' => array(),
+	),
+);
 ?>
 
 <p>
 	<?php
-	$allowed_html = array(
-		'a' => array(
-			'href' => array(),
-		),
-	);
 	printf(
 		/* translators: 1: user display name 2: logout url */
 		wp_kses( __( 'Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce' ), $allowed_html ),
