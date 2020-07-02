76ab0062191a827e845e552589771525aa283b1d
Darren Ethier <darren@roughsmootheng.in>
2020-07-02 09:51:05 -0400

    bump versions to 2.9.0-dev

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -101,7 +101,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '2.8.0';
+					$version = '2.9.0-dev';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ )
