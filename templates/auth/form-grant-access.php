1079b6ed761b0c2f4c8d46ca826e6edbf56ddc40
Peter Fabian <peter.fabian.github@gmail.com>
2020-07-04 21:57:39 +0200

    Update template versions for 4.3.

--- templates/auth/form-grant-access.php
+++ templates/auth/form-grant-access.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates/Auth
- * @version 2.4.0
+ * @version 4.3.0
  */
 
 defined( 'ABSPATH' ) || exit;
