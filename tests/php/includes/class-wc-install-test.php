13536bef5b0edd66d31d152171205935a6638ab9
Christopher Allford <christopher.allford@automattic.com>
2020-07-15 14:20:26 -0700

    Corrected the WCInstallTest filename
    It is in the `includes` test section and should have the same filename as the corresponding class.

--- tests/php/includes/class-wc-install-test.php
+++ tests/php/includes/class-wc-install-test.php
@@ -5,12 +5,10 @@
  * @package WooCommerce|Tests|WCInstallTest.
  */
 
-namespace Automattic\WooCommerce;
-
 /**
- * Class WC_Tests_WC_Helper.
+ * Class WC_Install_Test.
  */
-class WCInstallTest extends \WC_Unit_Test_Case {
+class WC_Install_Test extends \WC_Unit_Test_Case {
 
 	/**
 	 * Test if verify base table can detect missing table and adds/remove a notice.
