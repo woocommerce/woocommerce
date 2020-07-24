aca91f587dea544111b8102384506bdff0003ed4
Ron Rennick <ron@ronandandrea.com>
2020-07-24 11:39:30 -0300

    update version script to work in both desktop and Travis, add fallback versions

--- tests/e2e/env/bin/docker-compose.sh
+++ tests/e2e/env/bin/docker-compose.sh
@@ -4,24 +4,33 @@ if [[ $1 ]]; then
 #
 # Set environment variables for Docker
 #
+	if ! [[ $WP_VERSION =~ ^[0-9]+\.[0-9]+ ]]; then
+		WP_VERSION=$(git ls-remote --tags https://github.com/wordpress/wordpress.git | cut -f3 -d/ | grep -E '^\d+\.\d+(.\d+)*$' | tail -n 1)
+	fi
 	if [[ $WP_VERSION =~ ^[0-9]+\.[0-9]+ ]]; then
 		export WORDPRESS_VERSION=$WP_VERSION
 	else
-		export WORDPRESS_VERSION=`git ls-remote --tags https://github.com/wordpress/wordpress.git | cut -f3 -d/ | grep -E '^\d+\.\d+(.\d+)*$' | tail -n 1`
+		export WORDPRESS_VERSION="5.4.2"
 	fi
 
+	if ! [[ $TRAVIS_PHP_VERSION =~ ^[0-9]+\.[0-9]+ ]]; then
+		PVER=$(git ls-remote --tags https://github.com/php/php-src.git | cut -f3 -d/ | grep -E '^php-\d+\.\d+(.\d+)*$' | tail -n 1)
+		TRAVIS_PHP_VERSION=${PVER/php-/}
+	fi
 	if [[ $TRAVIS_PHP_VERSION =~ ^[0-9]+\.[0-9]+ ]]; then
 		export DC_PHP_VERSION=$TRAVIS_PHP_VERSION
 	else
-		PVER=`git ls-remote --tags https://github.com/php/php-src.git | cut -f3 -d/ | grep -E '^php-\d+\.\d+(.\d+)*$' | tail -n 1`
-		export DC_PHP_VERSION=${PVER/php-/}
+		export DC_PHP_VERSION="7.4.8"
 	fi
 
+	if ! [[ $TRAVIS_MARIADB_VERSION =~ ^[0-9]+\.[0-9]+ ]]; then
+		MVER=$(git ls-remote --tags https://github.com/mariadb/server.git | cut -f3 -d/ | grep -E '^mariadb-\d\d\.\d+(.\d+)*$' | tail -n 1)
+		TRAVIS_MARIADB_VERSION=${MVER/mariadb-/}
+	fi
 	if [[ $TRAVIS_MARIADB_VERSION =~ ^[0-9]+\.[0-9]+ ]]; then
 		export DC_MARIADB_VERSION=$TRAVIS_MARIADB_VERSION
 	else
-		MVER=`git ls-remote --tags https://github.com/mariadb/server.git | cut -f3 -d/ | grep -E '^mariadb-\d\d\.\d+(.\d+)*$' | tail -n 1`
-		export DC_MARIADB_VERSION=${MVER/mariadb-/}
+		export DC_MARIADB_VERSION="10.5.4"
 	fi
 
 	if [[ $1 == 'up' ]]; then
