de65c2ff8e588811876dcbfb83590760f984fd3a
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-07-29 18:20:17 +0200

    Update version numbers to 3.2.0-dev

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -101,7 +101,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '3.1.0';
+					$version = '3.2.0-dev';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ )
