a8275715e3e1614af0235a62d9c32b11f99588f4
Jason Conroy <jason@findingsimple.com>
2020-07-31 09:44:56 +0930

    Use clipRule and fillRule props (https://github.com/woocommerce/woocommerce-admin/pull/4889)

--- plugins/woocommerce-admin/client/marketing/components/product-icon/icons/library/facebook.js
+++ plugins/woocommerce-admin/client/marketing/components/product-icon/icons/library/facebook.js
@@ -12,8 +12,8 @@ const facebook = (
 		xmlns="http://www.w3.org/2000/svg"
 	>
 		<Path
-			fill-rule="evenodd"
-			clip-rule="evenodd"
+			fillRule="evenodd"
+			clipRule="evenodd"
 			d="M34 0H2C0.8 0 0 0.8 0 2V34C0 35 0.8 36 2 36H19.2V22H14.6V16.6H19.2V12.6C19.2 8 22 5.4 26.2 5.4C28.2 5.4 29.8 5.6 30.4 5.6V10.4H27.6C25.4 10.4 25 11.4 25 13V16.4H30.4L29.6 22H25V36H34C35 36 36 35.2 36 34V2C36 0.8 35.2 0 34 0Z"
 			fill="#3B5997"
 		/>
