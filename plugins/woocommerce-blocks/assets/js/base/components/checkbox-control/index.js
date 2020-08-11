fc9f6fe71a12bfe775871ef262430c0500358bf4
Seghir Nadir <nadir.seghir@gmail.com>
2020-08-11 09:59:59 +0100

    Use SVG for checkbox (https://github.com/woocommerce/woocommerce-blocks/pull/2320)
    * use svg for checkbox
    
    * update borders
    
    * switch to rems
    
    * define a rem function
    
    * fix ie11 issue
    
    * fix focus
    
    * add ie hack
    
    * fix spacing and switch to em
    
    * switch to rem

--- plugins/woocommerce-blocks/assets/js/base/components/checkbox-control/index.js
+++ plugins/woocommerce-blocks/assets/js/base/components/checkbox-control/index.js
@@ -38,6 +38,15 @@ const CheckboxControl = ( {
 				onChange={ ( event ) => onChange( event.target.checked ) }
 				{ ...rest }
 			/>
+			<svg
+				className="wc-block-components-checkbox__mark"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 20"
+			>
+				<path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+			</svg>
+
 			<span className="wc-block-components-checkbox__label">
 				{ label }
 			</span>
