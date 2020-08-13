38d358490b244deea83ca6c59a4c5dd382c5ac1f
Ron Rennick <ron@ronandandrea.com>
2020-08-13 10:32:19 -0300

    switch to site locale before translating refund reason

--- includes/wc-order-functions.php
+++ includes/wc-order-functions.php
@@ -778,6 +778,7 @@ function wc_order_fully_refunded( $order_id ) {
 	}
 
 	// Create the refund object.
+	wc_switch_to_site_locale();
 	wc_create_refund(
 		array(
 			'amount'     => $max_refund,
@@ -786,6 +787,7 @@ function wc_order_fully_refunded( $order_id ) {
 			'line_items' => array(),
 		)
 	);
+	wc_restore_locale();
 
 	$order->add_order_note( __( 'Order status set to refunded. To return funds to the customer you will need to issue a refund through your payment gateway.', 'woocommerce' ) );
 }
