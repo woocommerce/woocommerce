1701d606dbc82a3d25e38e1eeb3bf1afdc0800c1
Darren Ethier <darren@roughsmootheng.in>
2020-08-18 05:40:08 -0400

    Deprecate wc.wcSettings.setSetting function. (https://github.com/woocommerce/woocommerce-blocks/pull/3010)
    * Add deprecation notice for setSettings
    
    * add todo for followup issue to actually remove the function
    
    * update todo
    
    * fix tests
    
    * fix todo
    
    * Update assets/js/settings/shared/set-setting.js
    
    Co-authored-by: Seghir Nadir <nadir.seghir@gmail.com>

--- plugins/woocommerce-blocks/assets/js/settings/shared/test/set-setting.js
+++ plugins/woocommerce-blocks/assets/js/settings/shared/test/set-setting.js
@@ -7,14 +7,17 @@ import { getSetting } from '../get-setting';
 describe( 'setSetting', () => {
 	it( 'should add a new value to the settings state for value not present', () => {
 		setSetting( 'aSetting', 42 );
+		expect( console ).toHaveWarned();
 		expect( getSetting( 'aSetting' ) ).toBe( 42 );
 	} );
 	it( 'should replace existing value', () => {
 		setSetting( 'adminUrl', 'not original' );
+		expect( console ).toHaveWarned();
 		expect( getSetting( 'adminUrl' ) ).toBe( 'not original' );
 	} );
 	it( 'should save the value run through the provided filter', () => {
-		setSetting( 'aSetting', 'who', () => 42 );
+		setSetting( 'bSetting', 'who', () => 42 );
+		expect( console ).toHaveWarned();
 		expect( getSetting( 'aSetting' ) ).toBe( 42 );
 	} );
 } );
