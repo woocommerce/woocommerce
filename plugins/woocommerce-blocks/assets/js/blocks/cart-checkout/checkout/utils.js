569261a8d731e7af7308becb70c5f44d6aa0df3f
Seghir Nadir <nadir.seghir@gmail.com>
2020-09-01 10:56:11 +0100

    Use wp_login_url instead of hardcoding login path (https://github.com/woocommerce/woocommerce-blocks/pull/3090)
    * use wp_login_url instead of hardcoding login path
    
    * generating changeset for pull request
    
    * generating changeset for pull request
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/checkout/utils.js
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/checkout/utils.js
@@ -1,3 +1,8 @@
-export const LOGIN_TO_CHECKOUT_URL = `/wp-login.php?redirect_to=${ encodeURIComponent(
+/**
+ * External dependencies
+ */
+import { LOGIN_URL } from '@woocommerce/block-settings';
+
+export const LOGIN_TO_CHECKOUT_URL = `${ LOGIN_URL }?redirect_to=${ encodeURIComponent(
 	window.location.href
 ) }`;
