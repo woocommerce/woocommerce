1e75a866d84be2a6539ba49d08334bad42100708
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-09-07 17:43:05 +0200

    Split Checkout form component into smaller files (II) (https://github.com/woocommerce/woocommerce-blocks/pull/3106)
    * Fix wrong Form component name
    
    * Split CheckoutForm into smaller components for each step
    
    * Centralize call to useCheckoutAddress

---
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/checkout/form/contact-fields-step.js
@@ -0,0 +1,45 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { FormStep } from '@woocommerce/base-components/cart-checkout';
+import { ValidatedTextInput } from '@woocommerce/base-components/text-input';
+import { useCheckoutContext } from '@woocommerce/base-context';
+
+/**
+ * Internal dependencies
+ */
+import LoginPrompt from './login-prompt';
+
+const ContactFieldsStep = ( { emailValue, onChangeEmail } ) => {
+	const { isProcessing: checkoutIsProcessing } = useCheckoutContext();
+
+	return (
+		<FormStep
+			id="contact-fields"
+			disabled={ checkoutIsProcessing }
+			className="wc-block-checkout__contact-fields"
+			title={ __(
+				'Contact information',
+				'woo-gutenberg-products-block'
+			) }
+			description={ __(
+				"We'll use this email to send you details and updates about your order.",
+				'woo-gutenberg-products-block'
+			) }
+			stepHeadingContent={ () => <LoginPrompt /> }
+		>
+			<ValidatedTextInput
+				id="email"
+				type="email"
+				label={ __( 'Email address', 'woo-gutenberg-products-block' ) }
+				value={ emailValue }
+				autoComplete="email"
+				onChange={ onChangeEmail }
+				required={ true }
+			/>
+		</FormStep>
+	);
+};
+
+export default ContactFieldsStep;
