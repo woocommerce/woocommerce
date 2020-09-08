2002c74289fed3ca1ec854aed93e3712f52ae106
Rua Haszard <rua.haszard@automattic.com>
2020-09-09 07:54:42 +1200

    Update Storybook to latest (https://github.com/woocommerce/woocommerce-admin/pull/5105)
    * WIP - upgrade storybook via `npx sb upgrade`
    
    * fix broken storybook build - use custom MiniCssExtractPlugin package
    
    Co-authored-by: haszari <rua@automattic.com>

--- plugins/woocommerce-admin/storybook/webpack.config.js
+++ plugins/woocommerce-admin/storybook/webpack.config.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const MiniCssExtractPlugin = require( '@automattic/mini-css-extract-plugin-with-rtl' );
 const path = require( 'path' );
 
 /**
