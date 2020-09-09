5e78c1b8dfb442251789a7c7a6680368ede4d52b
Nestor Soriano <nestor.soriano@automattic.com>
2020-09-09 12:24:41 +0200

    Revert improved filtering for variations.
    This commit reverts the functionality introduced in PR #26260
    (later refined by #27175, #27190, #27508) in which filtering by
    attribute using the layered nav widget was improved to handle the
    cases of variations out of stock. The revert is a response to the
    numerous problems reported by users in Woo 4.4 and 4.5
    
    Not all the code has been reverted, only the code that resulted in
    visible functionality changes. Thus, the code that generates
    term relationships for variations is still in place to keep database
    consistency and to keep the reverting changes to the minimum needed.

--- includes/class-wc-query.php
+++ includes/class-wc-query.php
@@ -362,23 +362,10 @@ class WC_Query {
 		if ( 'product_query' !== $query->get( 'wc_query' ) ) {
 			return $posts;
 		}
-		$this->adjust_total_pages();
 		$this->remove_product_query_filters( $posts );
 		return $posts;
 	}
 
-	/**
-	 * The 'adjust_posts_count' method that handles the 'found_posts' filter indirectly initializes
-	 * the loop properties with a call to 'wc_setup_loop'. This includes setting 'total_pages' to
-	 * '$GLOBALS['wp_query']->max_num_pages', which at that point has a value of zero.
-	 * Thus we need to set the real value from the 'the_posts' filter, where $GLOBALS['wp_query']->max_num_pages'
-	 * will aready have been initialized.
-	 */
-	private function adjust_total_pages() {
-		if ( 0 === wc_get_loop_prop( 'total_pages' ) ) {
-			wc_set_loop_prop( 'total_pages', $GLOBALS['wp_query']->max_num_pages );
-		}
-	}
 
 	/**
 	 * Pre_get_posts above may adjust the main query to add WooCommerce logic. When this query is done, we need to ensure
@@ -395,52 +382,6 @@ class WC_Query {
 		return $posts;
 	}
 
-	/**
-	 * When we are listing products and the request is filtering by attributes via layered nav plugin
-	 * we need to adjust the total posts count to account for variable products having stock
-	 * in some variations but not in others.
-	 * We do that by just checking if each product is visible.
-	 *
-	 * We also cache the post visibility so that it isn't checked again when displaying the posts list.
-	 *
-	 * @since 4.4.0
-	 * @param int      $count Original posts count, as supplied by the found_posts filter.
-	 * @param WP_Query $query The current WP_Query object.
-	 *
-	 * @return int Adjusted posts count.
-	 */
-	public function adjust_posts_count( $count, $query ) {
-		if ( ! $query->get( 'wc_query' ) ) {
-			return $count;
-		}
-
-		$posts = $this->get_current_posts();
-		if ( is_null( $posts ) ) {
-			return $count;
-		}
-
-		foreach ( $posts as $post ) {
-			if ( is_object( $post ) && 'product' !== $post->post_type ) {
-				continue;
-			}
-
-			$product_id = is_object( $post ) ? $post->ID : $post;
-			$product    = wc_get_product( $product_id );
-			if ( ! is_object( $product ) ) {
-				continue;
-			}
-
-			if ( $product->is_visible() ) {
-				wc_set_loop_product_visibility( $product_id, true );
-			} else {
-				wc_set_loop_product_visibility( $product_id, false );
-				$count--;
-			}
-		}
-
-		wc_set_loop_prop( 'total', $count );
-		return $count;
-	}
 
 	/**
 	 * Instance version of get_layered_nav_chosen_attributes, needed for unit tests.
@@ -514,7 +455,7 @@ class WC_Query {
 		// Additonal hooks to change WP Query.
 		add_filter( 'posts_clauses', array( $this, 'price_filter_post_clauses' ), 10, 2 );
 		add_filter( 'the_posts', array( $this, 'handle_get_posts' ), 10, 2 );
-		add_filter( 'found_posts', array( $this, 'adjust_posts_count' ), 10, 2 );
+
 		do_action( 'woocommerce_product_query', $q, $this );
 	}
 
