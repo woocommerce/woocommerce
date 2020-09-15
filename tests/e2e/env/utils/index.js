7c509d8ddb16bfe75a3475a321fbff20dda451ef
Ron Rennick <ron@ronandandrea.com>
2020-09-15 10:10:59 -0300

    use tabs for indentation

--- tests/e2e/env/utils/index.js
+++ tests/e2e/env/utils/index.js
@@ -2,7 +2,7 @@ const { getAppRoot, getAppName } = require( './app-root' );
 const getTestConfig = require( './test-config' );
 
 module.exports = {
-    getAppRoot,
+	getAppRoot,
 	getAppName,
-    getTestConfig,
+	getTestConfig,
 };
