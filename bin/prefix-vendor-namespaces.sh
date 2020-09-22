fcd9b115caabd00f0035f12fb447ac9c1192d2a9
Nestor Soriano <nestor.soriano@automattic.com>
2020-09-22 09:19:12 +0200

    Adjust the namespace prefixing script to macOS/Linux
    Both OSes have a slightly different syntax for "sed -i" and therefore
    two versions of the command are needed.

--- bin/prefix-vendor-namespaces.sh
+++ bin/prefix-vendor-namespaces.sh
@@ -4,5 +4,12 @@
 
 output 6 "Prefixing the appropriate vendor namespaces with Automattic\WooCommerce\Vendor"
 
-find vendor/league/container -type f -name "*.php" -print0 | \
-xargs -0 sed -i '' -E -e 's/^[[:space:]]*(use|namespace)[[:space:]]*(League\\Container)/\1 Automattic\\WooCommerce\\Vendor\\\2/g'
+# Replace "League\Container" in "use" and "namespace" with "Automattic\WooCommerce\Vendor\League\Container".
+REGEX='s/^[[:space:]]*(use|namespace)[[:space:]]*(League\\Container)/\1 Automattic\\WooCommerce\\Vendor\\\2/g'
+
+# macOS and Linux have a slightly different syntax for 'sed -i' so we need two versions.
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find ./vendor/league/container -type f -name '*.php' -print0 | xargs -0 sed -i '' -E -e "$REGEX"
+else
+  find ./vendor/league/container -type f -name '*.php' -print0 | xargs -0 sed -i -E -e "$REGEX"
+fi
