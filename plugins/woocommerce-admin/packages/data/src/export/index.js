20959e76b4337194d156f9ad7268d83f6ec7c5cf
Sam Seay <samueljseay@gmail.com>
2020-09-24 09:49:11 +1200

    Remove store registration checks that are no longer needed. (https://github.com/woocommerce/woocommerce-admin/pull/5170)
    Fixes woocommerce/woocommerce-admin#4443
    
    Some investigation found that woocommerce/woocommerce-admin#4443 is no longer a reproducible issue. Why this issue no longer exists is not clear, but it makes sense now to disable the store registration checks as they are only called once. This PR just removes the checks but does not change any other behaviour.

--- plugins/woocommerce-admin/packages/data/src/export/index.js
+++ plugins/woocommerce-admin/packages/data/src/export/index.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 
-import { select, registerStore } from '@wordpress/data';
+import { registerStore } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -13,17 +13,11 @@ import * as actions from './actions';
 import controls from '../controls';
 import reducer from './reducer';
 
-const storeSelectors = select( STORE_NAME );
-
-// @todo This is used to prevent double registration of the store due to webpack chunks.
-// The `storeSelectors` condition can be removed once this is fixed.
-if ( ! storeSelectors ) {
-	registerStore( STORE_NAME, {
-		reducer,
-		actions,
-		controls,
-		selectors,
-	} );
-}
+registerStore( STORE_NAME, {
+	reducer,
+	actions,
+	controls,
+	selectors,
+} );
 
 export const EXPORT_STORE_NAME = STORE_NAME;
--- plugins/woocommerce-admin/packages/data/src/reports/index.js
+++ plugins/woocommerce-admin/packages/data/src/reports/index.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 
-import { select, registerStore } from '@wordpress/data';
+import { registerStore } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -14,19 +14,12 @@ import * as resolvers from './resolvers';
 import controls from '../controls';
 import reducer from './reducer';
 
-const storeSelectors = select( STORE_NAME );
-
-// @todo This is used to prevent double registration of the store due to webpack chunks.
-// The `storeSelectors` condition can be removed once this is fixed.
-// See https://github.com/woocommerce/woocommerce-admin/issues/4443.
-if ( ! storeSelectors ) {
-	registerStore( STORE_NAME, {
-		reducer,
-		actions,
-		controls,
-		selectors,
-		resolvers,
-	} );
-}
+registerStore( STORE_NAME, {
+	reducer,
+	actions,
+	controls,
+	selectors,
+	resolvers,
+} );
 
 export const REPORTS_STORE_NAME = STORE_NAME;
