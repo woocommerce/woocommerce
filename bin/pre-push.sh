woocommerce/woocommerce

commit 8c25557bf58be31d2136611bf9916a1d3e9f13f7

Fixed the test to actually catch master.

bin/pre-push.sh
@@ -4,7 +4,7 @@ PROTECTED_BRANCH="master"
 REMOTE_REF=$(echo "$HUSKY_GIT_STDIN" | cut -d " " -f 3)
 
 if [ -n "$REMOTE_REF" ]; then
-	if [ -z "${REMOTE_REF##$PROTECTED_BRANCH}" ]; then
+	if [ "refs/heads/${PROTECTED_BRANCH}" == "$REMOTE_REF" ]; then
 		printf "%sYou're about to push to master, is that what you intended? [y/N]: %s" "$(tput setaf 3)" "$(tput sgr0)"
 		read -r PROCEED < /dev/tty
 		echo

commit 6e61398c839413630e874077996a5996bd489bfa

Fix pre-push hook used by husky
The pre-push hook uses sh but contained syntax that is only supported by
bash. `=` in sh should be used instead of `==` to check for string
equality. Before this change, we were getting the following error when
pushing to any branch:

```
git push
husky > pre-push (node v10.22.1)
./bin/pre-push.sh: 7: [: refs/heads/master: unexpected operator
```

This error didn't block the push, but meant that the message asking
developers to confirm if they wanted to push to master was never
displayed. This problem was introduced in commit
8c25557bf58be31d2136611bf9916a1d3e9f13f7 added in PR #27028.

bin/pre-push.sh
@@ -4,8 +4,8 @@ PROTECTED_BRANCH="master"
 REMOTE_REF=$(echo "$HUSKY_GIT_STDIN" | cut -d " " -f 3)
 
 if [ -n "$REMOTE_REF" ]; then
-	if [ "refs/heads/${PROTECTED_BRANCH}" == "$REMOTE_REF" ]; then
-		if [ "$TERM" == "dumb" ]; then
+	if [ "refs/heads/${PROTECTED_BRANCH}" = "$REMOTE_REF" ]; then
+		if [ "$TERM" = "dumb" ]; then
 			>&2 echo "Sorry, you are unable to push to master using a GUI client! Please use git CLI."
 			exit 1
 		fi