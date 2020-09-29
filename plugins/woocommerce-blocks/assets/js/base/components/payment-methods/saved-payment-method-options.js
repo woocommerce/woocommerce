3174dda84d755d9e848851642f909bdebed7b07a
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-09-29 10:15:45 +0200

    Remove selectedToken state from PaymentMethods (https://github.com/woocommerce/woocommerce-blocks/pull/3135)

--- plugins/woocommerce-blocks/assets/js/base/components/payment-methods/saved-payment-method-options.js
+++ plugins/woocommerce-blocks/assets/js/base/components/payment-methods/saved-payment-method-options.js
@@ -87,7 +87,7 @@ const getDefaultPaymentMethodOptions = (
 	};
 };
 
-const SavedPaymentMethodOptions = ( { onSelect } ) => {
+const SavedPaymentMethodOptions = () => {
 	const { isEditor } = useEditorContext();
 	const {
 		setPaymentStatus,
@@ -148,9 +148,8 @@ const SavedPaymentMethodOptions = ( { onSelect } ) => {
 				setPaymentStatus().started();
 			}
 			setSelectedToken( token );
-			onSelect( token );
 		},
-		[ setSelectedToken, setPaymentStatus, onSelect ]
+		[ setSelectedToken, setPaymentStatus ]
 	);
 	useEffect( () => {
 		if ( selectedToken && currentOptions.current.length > 0 ) {
