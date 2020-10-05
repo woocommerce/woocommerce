b1139f30839dd2cbfd6d20936a74215a446979e1
Christopher Allford <christopher.allford@automattic.com>
2020-10-05 15:17:45 -0700

    Fixed an OBW E2E tests

--- tests/e2e/utils/src/components.js
+++ tests/e2e/utils/src/components.js
@@ -172,8 +172,8 @@ const completeOnboardingWizard = async () => {
 	// Benefits section
 
 	// Wait for Benefits section to appear
-	await page.waitForSelector( '.woocommerce-profile-wizard__step-header' );
-
+	await page.waitForSelector( '.woocommerce-profile-wizard__benefits' );
+	
 	// Wait for "No thanks" button to become active
 	await page.waitForSelector( 'button.is-secondary:not(:disabled)' );
 	// Click on "No thanks" button to move to the next step
