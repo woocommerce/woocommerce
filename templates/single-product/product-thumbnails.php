4a8898dfbdabdfd8cca8f03f64ee86e14ec9473c
Ron Rennick <ron@ronandandrea.com>
2020-10-08 15:08:03 -0300

    restore $attachment_ids for product thumbnails template

--- templates/single-product/product-thumbnails.php
+++ templates/single-product/product-thumbnails.php
@@ -12,7 +12,7 @@
  *
  * @see         https://docs.woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     4.6.0
+ * @version     4.7.0
  */
 
 defined( 'ABSPATH' ) || exit;
