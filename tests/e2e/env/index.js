9d47a1c07230f414db5ee6299bee14950601251f
Ron Rennick <ron@ronandandrea.com>
2020-10-21 16:17:10 -0300

    simplify e2e-environment exports

--- tests/e2e/env/index.js
+++ tests/e2e/env/index.js
@@ -3,50 +3,17 @@
   */
 const babelConfig = require( './babel.config' );
 const esLintConfig = require( './.eslintrc.js' );
-const {
-	jestConfig,
-	jestPuppeteerConfig,
-	useE2EBabelConfig,
-	useE2EEsLintConfig,
-	useE2EJestConfig,
-	useE2EJestPuppeteerConfig,
-} = require( './config' );
-const { getAppRoot, getTestConfig } = require( './utils' );
-
-const {
-	clickAndWaitForNewPage,
-	getAccountCredentials,
-	isEventuallyPresent,
-	isEventuallyVisible,
-	logDebugLog,
-	logHTML,
-	waitAndClick,
-	waitAndType,
-	waitForSelector,
-	scrollIntoView,
-	Page
-} = require( '@automattic/puppeteer-utils' );
+const allE2EConfig = require( './config' );
+const allE2EUtils = require( './utils' );
+/**
+ * External dependencies
+ */
+const allPuppeteerUtils = require( '@automattic/puppeteer-utils' );
 
 module.exports = {
 	babelConfig,
 	esLintConfig,
-	jestConfig,
-	jestPuppeteerConfig,
-	useE2EBabelConfig,
-	useE2EEsLintConfig,
-	useE2EJestConfig,
-	useE2EJestPuppeteerConfig,
-	getAppRoot,
-	getTestConfig,
-	clickAndWaitForNewPage,
-	getAccountCredentials,
-	isEventuallyPresent,
-	isEventuallyVisible,
-	logDebugLog,
-	logHTML,
-	waitAndClick,
-	waitAndType,
-	waitForSelector,
-	scrollIntoView,
-	Page,
+	...allE2EConfig,
+	...allE2EUtils,
+	...allPuppeteerUtils,
 };
