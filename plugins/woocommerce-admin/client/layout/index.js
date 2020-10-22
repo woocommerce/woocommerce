3c62e171dab63704bea2604b46396544dd5d813a
Joel Thiessen <444632+joelclimbsthings@users.noreply.github.com>
2020-10-22 15:13:57 -0700

    Navigation: Adding Tracks by tying into wc-admin page view (https://github.com/woocommerce/woocommerce-admin/pull/5466)
    * Adding 'has_navigation' flag to tracks pageView events to indicate if new navigation is enabled (https://github.com/woocommerce/woocommerce-admin/pull/5332)
    
    * Testing if navigation is active using window.wcNavigation instead of window.wcAdminFeatures to handle the edge case of a single page opting out

--- plugins/woocommerce-admin/client/layout/index.js
+++ plugins/woocommerce-admin/client/layout/index.js
@@ -78,9 +78,16 @@ class _Layout extends Component {
 			isJetpackConnected,
 		} = this.props;
 
+		const navigationFlag = {
+			has_navigation: !! window.wcNavigation,
+		};
+
 		if ( isEmbedded ) {
 			const path = document.location.pathname + document.location.search;
-			recordPageView( path, { is_embedded: true } );
+			recordPageView( path, {
+				is_embedded: true,
+				...navigationFlag,
+			} );
 			return;
 		}
 
@@ -101,6 +108,7 @@ class _Layout extends Component {
 			jetpack_installed: installedPlugins.includes( 'jetpack' ),
 			jetpack_active: activePlugins.includes( 'jetpack' ),
 			jetpack_connected: isJetpackConnected,
+			...navigationFlag,
 		} );
 	}
 
