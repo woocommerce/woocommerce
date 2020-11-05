8bb0f196e622c78e1fec55b22ceb2d903859bf60
Nestor Soriano <konamiman@konamiman.com>
2020-11-05 17:19:14 +0100

    Change the strategy for moving packages
    Since the Mozart package requires PHP 7.2, and we need to support 7.0
    and 7.1, the moving step has been removed from the composer install
    stage. Instead, now it has to be triggered manually via
    `composer run move-vendor-namespaces`, and the moved package must be
    added to source control. See the updated src/Internal/Vendor/REAMDE.md
    for details.

--- bin/move-vendor-packages.sh
+++ bin/move-vendor-packages.sh
@@ -15,6 +15,11 @@ output() {
 	echo "$(tput setaf "$1")$2$(tput sgr0)"
 }
 
+if [ -z "$(php -r "echo version_compare(PHP_VERSION,'7.2','>=');")" ]; then
+	output 1 "PHP 7.2 or newer is required to run Mozart, the current PHP version is $(php -r 'echo PHP_VERSION;')"
+	exit 1
+fi
+
 output 6 "Moving the appropriate vendor packages to Automattic\WooCommerce\Internal\Vendor"
 
 # Delete the entire contents of src/Internal/Vendor, except README.md
@@ -22,4 +27,8 @@ find src/Internal/Vendor/* ! -name README.md -prune -exec rm -rf {} +
 
 # Move the appropriate vendor packages to src/Internal/Vendor
 # (see extra/mozart for configuration)
-./vendor/bin/mozart compose
+./vendor/bin/mozart compose || exit "$?"
+
+output 6 "Updating autoload files"
+
+composer dump-autoload
