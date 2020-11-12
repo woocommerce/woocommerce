30d2e278b93a54dbbcfd32086f3bd3f7dfe78771
Christopher Allford <christopher.allford@automattic.com>
2020-11-12 13:26:30 -0800

    Replaced the deprecated `WC_Admin_Note` class

--- tests/legacy/unit-tests/rest-api/Helpers/AdminNotesHelper.php
+++ tests/legacy/unit-tests/rest-api/Helpers/AdminNotesHelper.php
@@ -1,4 +1,4 @@
-<?php
+<?php // phpcs:ignore WordPress.Files.FileName
 /**
  * Admin notes helper for wc-admin unit tests.
  *
@@ -9,8 +9,8 @@ namespace Automattic\WooCommerce\RestApi\UnitTests\Helpers;
 
 defined( 'ABSPATH' ) || exit;
 
+use Automattic\WooCommerce\Admin\Notes\Note;
 use \WC_Data_Store;
-use \WC_Admin_Note;
 
 /**
  * Class AdminNotesHelper.
@@ -34,11 +34,11 @@ class AdminNotesHelper {
 	public static function add_notes_for_tests() {
 		$data_store = WC_Data_Store::load( 'admin-note' );
 
-		$note_1 = new WC_Admin_Note();
+		$note_1 = new Note();
 		$note_1->set_title( 'PHPUNIT_TEST_NOTE_1_TITLE' );
 		$note_1->set_content( 'PHPUNIT_TEST_NOTE_1_CONTENT' );
 		$note_1->set_content_data( (object) array( 'amount' => 1.23 ) );
-		$note_1->set_type( WC_Admin_Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
+		$note_1->set_type( Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
 		$note_1->set_icon( 'info' );
 		$note_1->set_name( 'PHPUNIT_TEST_NOTE_NAME' );
 		$note_1->set_source( 'PHPUNIT_TEST' );
@@ -54,27 +54,27 @@ class AdminNotesHelper {
 		);
 		$note_1->save();
 
-		$note_2 = new WC_Admin_Note();
+		$note_2 = new Note();
 		$note_2->set_title( 'PHPUNIT_TEST_NOTE_2_TITLE' );
 		$note_2->set_content( 'PHPUNIT_TEST_NOTE_2_CONTENT' );
 		$note_2->set_content_data( (object) array( 'amount' => 4.56 ) );
-		$note_2->set_type( WC_Admin_Note::E_WC_ADMIN_NOTE_WARNING );
+		$note_2->set_type( Note::E_WC_ADMIN_NOTE_WARNING );
 		$note_2->set_icon( 'info' );
 		$note_2->set_name( 'PHPUNIT_TEST_NOTE_NAME' );
 		$note_2->set_source( 'PHPUNIT_TEST' );
-		$note_2->set_status( WC_Admin_Note::E_WC_ADMIN_NOTE_ACTIONED );
+		$note_2->set_status( Note::E_WC_ADMIN_NOTE_ACTIONED );
 		// This note has no actions.
 		$note_2->save();
 
-		$note_3 = new WC_Admin_Note();
+		$note_3 = new Note();
 		$note_3->set_title( 'PHPUNIT_TEST_NOTE_3_TITLE' );
 		$note_3->set_content( 'PHPUNIT_TEST_NOTE_3_CONTENT' );
 		$note_3->set_content_data( (object) array( 'amount' => 7.89 ) );
-		$note_3->set_type( WC_Admin_Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
+		$note_3->set_type( Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
 		$note_3->set_icon( 'info' );
 		$note_3->set_name( 'PHPUNIT_TEST_NOTE_NAME' );
 		$note_3->set_source( 'PHPUNIT_TEST' );
-		$note_3->set_status( WC_Admin_Note::E_WC_ADMIN_NOTE_SNOOZED );
+		$note_3->set_status( Note::E_WC_ADMIN_NOTE_SNOOZED );
 		$note_3->set_date_reminder( time() - HOUR_IN_SECONDS );
 		// This note has no actions.
 		$note_3->save();
