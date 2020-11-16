364b764eb50a5c0f23078eaaba1d06962d14be70
Sam Seay <samueljseay@gmail.com>
2020-11-16 17:33:06 +1300

    Fix the travis build and move the e2e tests to GH actions (https://github.com/woocommerce/woocommerce-admin/pull/5635)

--- plugins/woocommerce-admin/bin/travis.sh
+++ plugins/woocommerce-admin/bin/travis.sh
@@ -4,6 +4,7 @@
 if [ "$1" == 'before' ]; then
 	cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-admin/"
 	npm run build:feature-config
+	composer self-update 2.0.6
 	if [[ "$COMPOSER_DEV" == "1" ]]; then
 		composer install
 	else
