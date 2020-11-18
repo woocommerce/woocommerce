a5a086015fcead476f012608615ed3bfca6b5c11
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-11-18 10:11:37 +0100

    Exclude checkout-draft orders from WC Admin reports and My Account > Orders (https://github.com/woocommerce/woocommerce-blocks/pull/3379)
    * Exclude checkout-draft orders from WC Admin reports
    
    * Hook into order query args
    
    * Add comments before applying filters
    
    * Revert "Add comments before applying filters"
    
    This reverts commit 83b695665c05a3a9e7669e8777080c6ff10d8421.
    
    * Revert "Hook into order query args"
    
    This reverts commit 94cec6c83cf5b5ce05cd106daf217c0b08f13a03.
    
    * Hide draft orders from My Account > Orders
    
    * Move filter to its own file
    
    * Use custom plugin namespace for filter

--- plugins/woocommerce-blocks/assets/js/settings/shared/index.js
+++ plugins/woocommerce-blocks/assets/js/settings/shared/index.js
@@ -10,6 +10,7 @@ import { getSetting } from './get-setting';
 
 export * from './default-constants';
 export { setSetting } from './set-setting';
+import './exclude-draft-status-from-analytics';
 
 /**
  * Note: this attempts to coerce the wpVersion to a semver for comparison
