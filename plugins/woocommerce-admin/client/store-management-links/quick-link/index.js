e5cf64cb14bdfa80742eef88305a6fceae2d0f19
Fernando <ultimoround@gmail.com>
2020-11-18 12:31:21 -0300

    Fixed Link component prop (https://github.com/woocommerce/woocommerce-admin/pull/5653)
    * Fixed Link component prop
    
    This commit fixes a Link component prop name
    
    * Fixed Link prop
    
    Co-authored-by: Fernando Marichal <contacto@fernandomarichal.com>

--- plugins/woocommerce-admin/client/store-management-links/quick-link/index.js
+++ plugins/woocommerce-admin/client/store-management-links/quick-link/index.js
@@ -19,7 +19,7 @@ export const QuickLink = ( { icon, title, href, linkType, onClick } ) => {
 			<Link
 				onClick={ onClick }
 				href={ href }
-				linkType={ linkType }
+				type={ linkType }
 				className="woocommerce-quick-links__item-link"
 			>
 				<Icon
