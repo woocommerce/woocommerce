126496be6c0d2edf57afe489d700b2563603cafd
Kevin Amorim <kevinamorim4@gmail.com>
2020-11-20 18:36:27 +0000

    Fixed for 'update' action.

--- includes/rest-api/Controllers/Version2/class-wc-rest-orders-v2-controller.php
+++ includes/rest-api/Controllers/Version2/class-wc-rest-orders-v2-controller.php
@@ -646,7 +646,7 @@ class WC_REST_Orders_V2_Controller extends WC_REST_CRUD_Controller {
 
 		$has_filter_applied = has_filter( 'wc_product_has_unique_sku', '__return_false' );
 
-		if ( $has_filter_applied > 0 ) {
+		if ( ! empty( $has_filter_applied ) && $has_filter_applied > 0 ) {
 			// If a filter is applied we cannot check for SKU uniqueness. Use ID first
 			if ( ! empty ( $posted['variation_id'] ) ) {
 				$product_id = (int) $posted['variation_id'];
@@ -654,6 +654,8 @@ class WC_REST_Orders_V2_Controller extends WC_REST_CRUD_Controller {
 				$product_id = (int) $posted['product_id'];
 			} elseif ( ! empty( $posted['sku'] ) ) {
 				$product_id = (int) wc_get_product_id_by_sku( $posted['sku'] );
+			}  elseif ( 'update' === $action ) {
+				$product_id = 0;
 			} else {
 				throw new WC_REST_Exception( 'woocommerce_rest_required_product_reference', __( 'Product ID or SKU is required.', 'woocommerce' ), 400 );
 			}
@@ -680,6 +682,8 @@ class WC_REST_Orders_V2_Controller extends WC_REST_CRUD_Controller {
 				$product_id = (int) $posted['variation_id'];
 			} elseif ( ! empty( $posted['product_id'] ) ) {
 				$product_id = (int) $posted['product_id'];
+			}  elseif ( 'update' === $action ) {
+				$product_id = 0;
 			} else {
 				throw new WC_REST_Exception( 'woocommerce_rest_required_product_reference', __( 'Product ID or SKU is required.', 'woocommerce' ), 400 );
 			}
