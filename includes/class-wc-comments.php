b35e43f3b3998728c5ff072511dab8a8ed131de7
Frolov <phrolov.ivan@gmail.com>
2020-11-22 12:38:41 +0200

    moved code to correct position

--- includes/class-wc-comments.php
+++ includes/class-wc-comments.php
@@ -22,7 +22,6 @@ class WC_Comments {
 		// Rating posts.
 		add_filter( 'comments_open', array( __CLASS__, 'comments_open' ), 10, 2 );
 		add_filter( 'preprocess_comment', array( __CLASS__, 'check_comment_rating' ), 0 );
-		add_filter( 'preprocess_comment', array( __CLASS__, 'check_comment_verified_owner_only' ), 0 );
 		add_action( 'comment_post', array( __CLASS__, 'add_comment_rating' ), 1 );
 		add_action( 'comment_moderation_recipients', array( __CLASS__, 'comment_moderation_recipients' ), 10, 2 );
 
@@ -48,6 +47,7 @@ class WC_Comments {
 		add_filter( 'get_avatar_comment_types', array( __CLASS__, 'add_avatar_for_review_comment_type' ) );
 
 		// Review of verified purchase.
+		add_filter( 'preprocess_comment', array( __CLASS__, 'check_comment_verified_owner_only' ), 0 );
 		add_action( 'comment_post', array( __CLASS__, 'add_comment_purchase_verification' ) );
 
 		// Set comment type.
@@ -149,21 +149,6 @@ class WC_Comments {
 		// If posting a comment (not trackback etc) and not logged in.
 		if ( ! is_admin() && isset( $_POST['comment_post_ID'], $_POST['rating'], $comment_data['comment_type'] ) && 'product' === get_post_type( absint( $_POST['comment_post_ID'] ) ) && empty( $_POST['rating'] ) && self::is_default_comment_type( $comment_data['comment_type'] ) && wc_review_ratings_enabled() && wc_review_ratings_required() ) { // WPCS: input var ok, CSRF ok.
 			wp_die( esc_html__( 'Please rate the product.', 'woocommerce' ) );
-			exit;
-		}
-		return $comment_data;
-	}
-
-	/**
-	 * Validate the 'Reviews can only be left by "verified owners"'.
-	 *
-	 * @param  array $comment_data Comment data.
-	 * @return array
-	 */
-	public static function check_comment_verified_owner_only( $comment_data ) {
-		// If posting a comment (not trackback etc) and not logged in.
-		if ( ! is_admin() && isset( $_POST['comment_post_ID'] ) && 'product' === get_post_type( absint( $_POST['comment_post_ID'] ) ) && 'yes' === get_option( 'woocommerce_review_rating_verification_required', 'no' ) && ! wc_customer_bought_product( '', get_current_user_id(), absint( $_POST['comment_post_ID'] ) ) ) { // WPCS: input var ok, CSRF ok.
-			wp_die( esc_html__( 'Only logged in customers who have purchased this product may leave a review.', 'woocommerce' ) );
 			exit;
 		}
 		return $comment_data;
@@ -304,6 +289,21 @@ class WC_Comments {
 	 */
 	public static function add_avatar_for_review_comment_type( $comment_types ) {
 		return array_merge( $comment_types, array( 'review' ) );
+	}
+
+	/**
+	 * Validate the 'Reviews can only be left by "verified owners"'.
+	 *
+	 * @param  array $comment_data Comment data.
+	 * @return array
+	 */
+	public static function check_comment_verified_owner_only( $comment_data ) {
+		// If posting a comment (not trackback etc) and not logged in.
+		if ( ! is_admin() && isset( $_POST['comment_post_ID'] ) && 'product' === get_post_type( absint( $_POST['comment_post_ID'] ) ) && 'yes' === get_option( 'woocommerce_review_rating_verification_required', 'no' ) && ! wc_customer_bought_product( '', get_current_user_id(), absint( $_POST['comment_post_ID'] ) ) ) { // WPCS: input var ok, CSRF ok.
+			wp_die( esc_html__( 'Only logged in customers who have purchased this product may leave a review.', 'woocommerce' ) );
+			exit;
+		}
+		return $comment_data;
 	}
 
 	/**
