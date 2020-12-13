29e130a2e930cc1947c7e65cd3714d96c6390834
szyminson <szyminson@hotmail.com>
2020-12-13 13:12:49 +0100

    Add WC_Query chosen attributes filter Add a possibility to filter chosen attributes so filtering products by custom taxonomies will be easier to accomplish.

--- includes/class-wc-query.php
+++ includes/class-wc-query.php
@@ -872,6 +872,7 @@ class WC_Query {
 					}
 				}
 			}
+			self::$chosen_attributes = apply_filters( 'woocommerce_layered_nav_chosen_attributes', self::$chosen_attributes );
 		}
 		return self::$chosen_attributes;
 		// phpcs:disable WordPress.Security.NonceVerification.Recommended
