9dc87f629a48a6785c7c225613eda529a0c9b45e
mfs-mindsize <mark.simchock@mindsize.com>
2020-12-17 11:41:50 -0500

    added filter: woocommerce_cancel_unpaid_orders_interval_minutes (default 5 mins) that separates interval for checking cancelsfrom option: woocommerce_hold_stock_minutes

--- includes/wc-order-functions.php
+++ includes/wc-order-functions.php
@@ -924,7 +924,8 @@ function wc_cancel_unpaid_orders() {
 		}
 	}
 	wp_clear_scheduled_hook( 'woocommerce_cancel_unpaid_orders' );
-	wp_schedule_single_event( time() + ( absint( $held_duration ) * 60 ), 'woocommerce_cancel_unpaid_orders' );
+	$cancel_unpaid_interval = apply_filters( 'woocommerce_cancel_unpaid_orders_interval_minutes', 5 );
+	wp_schedule_single_event( time() + ( absint( $cancel_unpaid_interval ) * 60 ), 'woocommerce_cancel_unpaid_orders' );
 }
 add_action( 'woocommerce_cancel_unpaid_orders', 'wc_cancel_unpaid_orders' );
 
