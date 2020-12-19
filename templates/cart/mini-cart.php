814618ea7a8871f54f230f2be529e47b69041c36
Michael Silber <michael@cfxdesign.com>
2020-12-18 22:47:47 -0500

    Add mini cart item filter woocommerce_mini_cart_items

--- templates/cart/mini-cart.php
+++ templates/cart/mini-cart.php
@@ -27,7 +27,9 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 		<?php
 		do_action( 'woocommerce_before_mini_cart_contents' );
 
-		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+		$cart_items = apply_filters( 'woocommerce_mini_cart_items', WC()->cart->get_cart() );
+
+		foreach ( $cart_items as $cart_item_key => $cart_item ) {
 			$_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 			$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 
