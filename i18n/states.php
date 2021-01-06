b7f863dc02178f7219b88206b047248a18787658
Soren Singh Dary <67230851+sorensd@users.noreply.github.com>
2021-01-07 01:02:31 +0530

    Updated the state name of 'OR' from India
    Updated the state name of Odisha (previously Orissa) 'IN' array. Cite: https://en.wikipedia.org/wiki/Odisha#Etymology

--- i18n/states.php
+++ i18n/states.php
@@ -590,7 +590,7 @@ return array(
 		'ML' => __( 'Meghalaya', 'woocommerce' ),
 		'MZ' => __( 'Mizoram', 'woocommerce' ),
 		'NL' => __( 'Nagaland', 'woocommerce' ),
-		'OR' => __( 'Orissa', 'woocommerce' ),
+		'OD' => __( 'Odisha', 'woocommerce' ),
 		'PB' => __( 'Punjab', 'woocommerce' ),
 		'RJ' => __( 'Rajasthan', 'woocommerce' ),
 		'SK' => __( 'Sikkim', 'woocommerce' ),
