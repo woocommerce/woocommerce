8660f2211754e45b4a83cb9eb38b34ac2317d100
vedanshujain <vedanshu.jain.2012@gmail.com>
2021-01-06 15:12:55 +0530

    Add tests for checking all fields are returned even requested individually.

--- tests/php/includes/rest-api/Controllers/Version3/class-wc-rest-orders-controller-tests.php
+++ tests/php/includes/rest-api/Controllers/Version3/class-wc-rest-orders-controller-tests.php
@@ -20,12 +20,10 @@ class WC_REST_Orders_Controller_Tests extends WC_REST_Unit_Test_Case {
 	}
 
 	/**
-	 * Test that all expected response fields are present.
-	 * Note: This has fields hardcoded intentionally instead of fetching from schema to test for any bugs in schema result. Add new fields manually when added to schema.
+	 * Get all expected fields.
 	 */
-	public function test_orders_api_get_all_fields() {
-		wp_set_current_user( $this->user );
-		$expected_response_fields = array(
+	public function get_expected_response_fields() {
+		return array(
 			'id',
 			'parent_id',
 			'number',
@@ -69,6 +67,15 @@ class WC_REST_Orders_Controller_Tests extends WC_REST_Unit_Test_Case {
 			'currency_symbol',
 			'refunds',
 		);
+	}
+
+	/**
+	 * Test that all expected response fields are present.
+	 * Note: This has fields hardcoded intentionally instead of fetching from schema to test for any bugs in schema result. Add new fields manually when added to schema.
+	 */
+	public function test_orders_api_get_all_fields() {
+		wp_set_current_user( $this->user );
+		$expected_response_fields = $this->get_expected_response_fields();
 
 		$order = \Automattic\WooCommerce\RestApi\UnitTests\Helpers\OrderHelper::create_order( $this->user );
 		$response = $this->server->dispatch( new WP_REST_Request( 'GET', '/wc/v3/orders/' . $order->get_id() ) );
@@ -80,5 +87,24 @@ class WC_REST_Orders_Controller_Tests extends WC_REST_Unit_Test_Case {
 		$this->assertEmpty( array_diff( $expected_response_fields, $response_fields ), 'These fields were expected but not present in API response: ' . print_r( array_diff( $expected_response_fields, $response_fields ), true ) );
 
 		$this->assertEmpty( array_diff( $response_fields, $expected_response_fields ), 'These fields were not expected in the API response: ' . print_r( array_diff( $response_fields, $expected_response_fields ), true ) );
+	}
+
+	/**
+	 * Test that all fields are returned when requested one by one.
+	 */
+	public function test_orders_get_each_field_one_by_one() {
+		wp_set_current_user( $this->user );
+		$expected_response_fields = $this->get_expected_response_fields();
+		$order = \Automattic\WooCommerce\RestApi\UnitTests\Helpers\OrderHelper::create_order( $this->user );
+
+		foreach ( $expected_response_fields as $field ) {
+			$request = new WP_REST_Request( 'GET', '/wc/v3/orders/' . $order->get_id() );
+			$request->set_param( '_fields', $field );
+			$response = $this->server->dispatch( $request );
+			$this->assertEquals( 200, $response->get_status() );
+			$response_fields = array_keys( $response->get_data() );
+
+			$this->assertContains( $field, $response_fields, "Field $field was expected but not present in order API response." );
+		}
 	}
 }
