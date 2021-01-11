760a879cb9aa4def74b574ac4de598cdf38e6277
Veljko V <veljano@yahoo.com>
2021-01-11 16:45:31 +0100

    Fix white spaces issue

--- tests/e2e/core-tests/specs/shopper/front-end-cart-coupons.test.js
+++ tests/e2e/core-tests/specs/shopper/front-end-cart-coupons.test.js
@@ -5,7 +5,7 @@
 const {
 	shopper,
 	merchant,
-    createCoupon,
+	createCoupon,
 	createSimpleProduct,
 	uiUnblocked
 } = require( '@woocommerce/e2e-utils' );
@@ -26,8 +26,8 @@ const runCartApplyCouponsTest = () => {
 			let couponFixedProduct;
 		beforeAll(async () => {
 			await merchant.login();
-            await createSimpleProduct();
-            couponFixedCart = await createCoupon();
+			await createSimpleProduct();
+			couponFixedCart = await createCoupon();
 			couponPercentage = await createCoupon('50', 'Percentage discount');
 			couponFixedProduct = await createCoupon('5', 'Fixed product discount');
 			await merchant.logout();
@@ -44,19 +44,19 @@ const runCartApplyCouponsTest = () => {
 			await expect(page).toClick('button', {text: 'Apply coupon'});
 			await uiUnblocked();
 			await page.waitForSelector('.woocommerce-message', {text: 'Coupon code applied successfully.'});
-			
+
 			// Wait for page to expand total calculations to avoid flakyness
 			await page.waitForSelector('.order-total');
 
 			// Verify discount applied and order total
 			await page.waitForSelector('.cart-discount .amount', {text: '$5.00'});
 			await page.waitForSelector('.order-total .amount', {text: '$4.99'});
-			
+
 			// Remove coupon
 			await expect(page).toClick('.woocommerce-remove-coupon', {text: '[Remove]'});
 			await uiUnblocked();
 			await page.waitForSelector('.woocommerce-message', {text: 'Coupon has been removed.'});
-			
+
 			// Apply Percentage discount coupon
 			await expect(page).toFill('#coupon_code', couponPercentage);
 			await expect(page).toClick('button', {text: 'Apply coupon'});
@@ -64,7 +64,7 @@ const runCartApplyCouponsTest = () => {
 			await page.waitForSelector('.woocommerce-message', {text: 'Coupon code applied successfully.'});
 			await page.waitForSelector('.cart-discount .amount', {text: '$4.99'});
 			await page.waitForSelector('.order-total .amount', {text: '$5.00'});
-			
+
 			// Remove coupon
 			await expect(page).toClick('.woocommerce-remove-coupon', {text: '[Remove]'});
 			await uiUnblocked();
@@ -77,7 +77,7 @@ const runCartApplyCouponsTest = () => {
 			await page.waitForSelector('.woocommerce-message', {text: 'Coupon code applied successfully.'});
 			await page.waitForSelector('.cart-discount .amount', {text: '$5.00'});
 			await page.waitForSelector('.order-total .amount', {text: '$4.99'});
-			
+
 			// Try to apply the same coupon
 			await expect(page).toFill('#coupon_code', couponFixedProduct);
 			await expect(page).toClick('button', {text: 'Apply coupon'});
@@ -94,13 +94,13 @@ const runCartApplyCouponsTest = () => {
 			// Remove coupon
 			await expect(page).toClick('.woocommerce-remove-coupon', {text: '[Remove]'});
 			await uiUnblocked();
-            await page.waitForSelector('.woocommerce-message', {text: 'Coupon has been removed.'});
+			await page.waitForSelector('.woocommerce-message', {text: 'Coupon has been removed.'});
 			await expect(page).toClick('.woocommerce-remove-coupon', {text: '[Remove]'});
 			await uiUnblocked();
-            await page.waitForSelector('.woocommerce-message', {text: 'Coupon has been removed.'});
-            
-            // Verify the total amount after all coupons removal
-            await page.waitForSelector('.order-total .amount', {text: '$9.99'});
+			await page.waitForSelector('.woocommerce-message', {text: 'Coupon has been removed.'});
+
+			// Verify the total amount after all coupons removal
+			await page.waitForSelector('.order-total .amount', {text: '$9.99'});
 		});
 	});
 };
