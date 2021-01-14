aae807b1b4ef6d37f14ea2f5856fb90da3def9a1
Ron Rennick <ron@ronandandrea.com>
2021-01-14 14:09:12 -0400

    share product object, update test descriptions

--- tests/e2e/specs/rest-api/create-simple-product.test.js
+++ tests/e2e/specs/rest-api/create-simple-product.test.js
@@ -2,7 +2,7 @@ import { getTestConfig } from '@woocommerce/e2e-environment';
 import { HTTPClientFactory } from "@woocommerce/api";
 import { SimpleProduct } from '@woocommerce/api';
 
-describe('test product endpoint', () => {
+describe('REST API > Product Endpoint', () => {
 	const productProperties = {
 		name: 'Test Product',
 		regular_price: '99.95',
@@ -11,6 +11,7 @@ describe('test product endpoint', () => {
 		stock_quantity: 1000,
 	};
 	let client;
+	let product;
 
 	beforeAll( () => {
 		const testConfig = getTestConfig();
@@ -22,7 +23,7 @@ describe('test product endpoint', () => {
 			.create();
 	} );
 
-	it('creates simple product', async () => {
+	it('can create a simple product', async () => {
 		const repository = SimpleProduct.restRepository( client );
 		const transformedProperties = {
 			name: productProperties.name,
@@ -33,11 +34,11 @@ describe('test product endpoint', () => {
 		};
 
 		// Check properties of product in the create product response.
-		const product = await repository.create( productProperties );
+		product = await repository.create( productProperties );
 		expect( product ).toEqual( expect.objectContaining( transformedProperties ) );
 	});
 
-	it('retrieves simple product properties', async () => {
+	it('can retrieve a simple product', async () => {
 		const combinedProperties = {
 			...productProperties,
 			id: product.id,
