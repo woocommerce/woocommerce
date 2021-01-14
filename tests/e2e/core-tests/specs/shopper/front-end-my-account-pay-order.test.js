c41fd3b4acbe62a1723482fac186bed4e1e65e4e
Veljko V <veljano@yahoo.com>
2021-01-14 17:19:06 +0100

    Update scenario of shopper paying his order

--- tests/e2e/core-tests/specs/shopper/front-end-my-account-pay-order.test.js
+++ tests/e2e/core-tests/specs/shopper/front-end-my-account-pay-order.test.js
@@ -10,6 +10,7 @@ const {
 } = require( '@woocommerce/e2e-utils' );
 
 let simplePostIdValue;
+let orderNum;
 const config = require( 'config' );
 const simpleProductName = config.get( 'products.simple.name' );
 
@@ -21,26 +22,29 @@ const runMyAccountPayOrderTest = () => {
 			await merchant.logout();
 			await shopper.login();
 			await shopper.goToProduct(simplePostIdValue);
-			await shopper.addToCartFromShopPage(simpleProductName);
+			await shopper.addToCart(simpleProductName);
 			await shopper.goToCheckout();
 			await shopper.fillBillingDetails(config.get('addresses.customer.billing'));
 			await uiUnblocked();
 			await shopper.placeOrder();
-			const orderElement = await page.$(".order");
-			const orderId = await page.evaluate(orderElement => orderElement.textContent, orderElement);
-			await shopper.logout();
+
+			// Get order ID from the order received html element on the page
+			orderNum = await page.$$eval(".woocommerce-order-overview__order strong",
+				elements => elements.map(item => item.textContent));
+
 			await merchant.login();
-			await merchant.updateOrderStatus(orderId, 'Pending payment');
+			await merchant.updateOrderStatus(orderNum, 'Pending payment');
 			await merchant.logout();
 		})
 
 		it('allows customer to pay for his order in my account', async () => {
 			await shopper.login();
 			await shopper.goToOrders();
-			await expect(page).toClick('.wc_payment_method woocommerce-button button pay', {text: 'Pay'});
-            await expect(page).toMatchElement('.entry-title', {text: 'Pay for order'});
-            await shopper.placeOrder();
-            await expect(page).toMatch('Order received');
+			await expect(page).toClick('a.woocommerce-button.button.pay');
+			await page.waitForNavigation({ waitUntil: 'networkidle0' })
+			await expect(page).toMatchElement('.entry-title', { text: 'Pay for order' });
+			await shopper.placeOrder();
+			await expect(page).toMatch('Order received');
 		});
 	});
 }
