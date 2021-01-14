ea55bc26eb92d121254174a05bcf91c4afd0c1ea
Ron Rennick <ron@ronandandrea.com>
2021-01-14 13:10:13 -0400

    add first simple product api test

---
+++ tests/e2e/specs/rest-api/create-simple-product.test.js
@@ -0,0 +1,55 @@
+import { getTestConfig } from '@woocommerce/e2e-environment';
+import { HTTPClientFactory } from "@woocommerce/api";
+import { SimpleProduct } from '@woocommerce/api';
+
+describe('test product endpoint', () => {
+	const productProperties = {
+		name: 'Test Product',
+		regular_price: '99.95',
+		manage_stock: true,
+		type: 'simple',
+		stock_quantity: 1000,
+	};
+	let client;
+
+	beforeAll( () => {
+		const testConfig = getTestConfig();
+		const admin = testConfig.users.admin;
+
+		client = HTTPClientFactory.build(testConfig.url)
+			.withBasicAuth(admin.username, admin.password)
+			.withIndexPermalinks()
+			.create();
+	} );
+
+	it('creates simple product', async () => {
+		const repository = SimpleProduct.restRepository( client );
+		const transformedProperties = {
+			name: productProperties.name,
+			regularPrice: productProperties.regular_price,
+			trackInventory: productProperties.manage_stock,
+			type: productProperties.type,
+			remainingStock: productProperties.stock_quantity,
+		};
+
+		// Check properties of product in the create product response.
+		const product = await repository.create( productProperties );
+		expect( product ).toEqual( expect.objectContaining( transformedProperties ) );
+	});
+
+	it('retrieves simple product properties', async () => {
+		const combinedProperties = {
+			...productProperties,
+			id: product.id,
+			stock_status: 'instock',
+			catalog_visibility: 'visible',
+			price: productProperties.regular_price,
+			sale_price: '',
+		};
+
+		// Read product directly from api.
+		const response = await client.get( `/wc/v3/products/${product.id}` );
+		expect( response.statusCode ).toBe( 200 );
+		expect( response.data ).toEqual( expect.objectContaining( combinedProperties ) );
+	});
+});
