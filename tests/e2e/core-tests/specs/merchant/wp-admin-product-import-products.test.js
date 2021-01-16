962e5bdb92c6457f8296561f5d8ae989ac3e4f62
Milos Petrovic <>
2021-01-16 17:32:51 +0100

    adding test for updating existing products

--- tests/e2e/core-tests/specs/merchant/wp-admin-product-import-products.test.js
+++ tests/e2e/core-tests/specs/merchant/wp-admin-product-import-products.test.js
@@ -3,7 +3,8 @@
  * Internal dependencies
  */
 const {
-	moveAllItemsToTrash
+	moveAllItemsToTrash,
+	setCheckbox
 } = require( '@woocommerce/e2e-utils' );
 
 /**
@@ -52,6 +53,51 @@ const runImportProductsTest = () => {
 			await page.waitForSelector('a.row-title');
 			let linkTexts = await page.$$eval('a.row-title', elements => elements.map(item => item.innerHTML));
 
+			// Compare imported product names
+			await expect(product_names).toEqual(linkTexts.sort());
+		});
+
+
+		it('Import example file and update existing products', async () => {
+
+			let filePath = path.resolve('../../../sample-data/sample_products.csv');
+
+			const product_names = ["V-Neck T-Shirt", "Hoodie", "Hoodie with Logo", "T-Shirt", "Beanie",
+				"Belt", "Cap", "Sunglasses", "Hoodie with Pocket", "Hoodie with Zipper", "Long Sleeve Tee",
+				"Polo", "Album", "Single", "T-Shirt with Logo", "Beanie with Logo", "Logo Collection", "WordPress Pennant"].sort();
+
+			const product_prices = []
+
+			// Click import
+			await expect(page).toClick('a[href*="product_importer"]');
+
+			// Upload file
+			await page.waitForSelector('#upload');
+			const input = await page.$('input[type=file]');
+			await input.uploadFile(filePath);
+
+			// Check 'Update existing products' checkbox
+			setCheckbox('#woocommerce-importer-update-existing')
+
+			// Click on Continue
+			await expect(page).toClick('button[value="Continue"]');
+
+			// Click on Run the importer
+			await page.waitForSelector('button[value="Run the importer"]');
+			await expect(page).toClick('button[value="Run the importer"]');
+
+			// Waiting for importer to finish
+			await page.waitForSelector('section.woocommerce-importer-done', {visible:true, timeout: 60000});
+
+			// Click on view products
+			await page.waitForSelector('div.wc-actions a.button.button-primary');
+			await expect(page).toClick('div.wc-actions a.button.button-primary');
+
+			// Getting product names
+			await page.waitForSelector('td.price.column-price');
+			let prices = await page.$$eval('td.price.column-price', elements => elements.map(item => item.text));
+
+			debugger;
 			// Compare imported product names
 			await expect(product_names).toEqual(linkTexts.sort());
 		});
