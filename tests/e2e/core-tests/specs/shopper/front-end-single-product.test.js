521f5d9610adc1fef422d78d1e46911d77d8db06
Veljko <veljano@yahoo.com>
2021-01-19 18:20:04 +0100

    Add new e2e test shopper grouped product

--- tests/e2e/core-tests/specs/shopper/front-end-single-product.test.js
+++ tests/e2e/core-tests/specs/shopper/front-end-single-product.test.js
@@ -7,11 +7,13 @@ const {
 	merchant,
 	createSimpleProduct,
 	createVariableProduct,
+	createGroupedProduct,
 	uiUnblocked
 } = require( '@woocommerce/e2e-utils' );
 
 let simplePostIdValue;
 let variablePostIdValue;
+let groupedPostIdValue;
 const config = require( 'config' );
 const simpleProductName = config.get( 'products.simple.name' );
 
@@ -67,6 +69,50 @@ const runSingleProductPageTest = () => {
 			await expect(page).toMatchElement('.cart-empty', {text: 'Your cart is currently empty.'});
 		});
 	});
+
+	describe('Grouped Product Page', () => {
+		beforeAll(async () => {
+			await merchant.login();
+			groupedPostIdValue = await createGroupedProduct();
+			await merchant.logout();
+		})
+
+		it('should be able to add grouped products to the cart', async () => {
+			// Add a grouped product to cart
+			await shopper.goToProduct(groupedPostIdValue);
+			await page.waitForSelector('form.grouped_form');
+			await shopper.addToCart();
+			await expect(page).toMatchElement('.woocommerce-error',
+			 {text: 'Please choose the quantity of items you wish to add to your cart…'});
+			const quantityFields = await page.$$('div.quantity input.qty');
+			await quantityFields[0].click({clickCount: 3});
+			await quantityFields[0].type('5');
+			await quantityFields[1].click({clickCount: 3});
+			await quantityFields[1].type('5');
+			await shopper.addToCart();
+			await expect(page).toMatchElement('.woocommerce-message',
+			 {text: '“'+simpleProductName+' 1” and “'+simpleProductName+' 2” have been added to your cart.'});
+
+			// Verify cart contents
+			await shopper.goToCart();
+			await shopper.productIsInCart(simpleProductName+' 1');
+			await shopper.productIsInCart(simpleProductName+' 2');
+
+			// Remove items from cart
+			await shopper.removeFromCart(simpleProductName+' 1');
+			await uiUnblocked();
+			await expect(page).toMatchElement('.woocommerce-message', {text: '“'+simpleProductName+' 1” removed.'});
+			await Promise.all( [
+				// Reload page and perform item removal, since removeFromCart won't remove it when placed in a row
+				page.reload(),
+				page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+			] );
+			await shopper.removeFromCart(simpleProductName+' 2');
+			await uiUnblocked();
+			await expect(page).toMatchElement('.woocommerce-message', {text: '“'+simpleProductName+' 2” removed.'});
+			await expect(page).toMatchElement('.cart-empty', {text: 'Your cart is currently empty.'});
+		})
+	})
 };
 
 module.exports = runSingleProductPageTest;
--- tests/e2e/utils/src/components.js
+++ tests/e2e/utils/src/components.js
@@ -6,7 +6,7 @@
  * Internal dependencies
  */
 import { merchant } from './flows';
-import { clickTab, uiUnblocked, verifyCheckboxIsUnset } from './page-utils';
+import { clickTab, uiUnblocked, verifyCheckboxIsUnset, selectOptionInSelect2 } from './page-utils';
 import factories from './factories';
 
 const config = require( 'config' );
@@ -346,6 +346,40 @@ const createVariableProduct = async () => {
 };
 
 /**
+ * Create grouped product.
+ */
+const createGroupedProduct = async () => {
+	// Create two products to be linked in a grouped product after
+	await factories.products.simple.create( {
+		name: simpleProductName + ' 1',
+		regularPrice: simpleProductPrice
+	} );
+	await factories.products.simple.create( {
+		name: simpleProductName + ' 2',
+		regularPrice: simpleProductPrice
+	} );
+
+	// Go to "add product" page
+	await merchant.openNewProduct();
+
+	// Make sure we're on the add product page
+	await expect( page.title() ).resolves.toMatch( 'Add new product' );
+
+	// Set product data and save the product
+	await expect( page ).toFill( '#title', 'Grouped Product' );
+	await expect( page ).toSelect( '#product-type', 'Grouped product' );
+	await clickTab('Linked Products');
+	await selectOptionInSelect2(simpleProductName + ' 1');
+	await selectOptionInSelect2(simpleProductName + ' 2');
+	await verifyAndPublish();
+
+	// Get product ID
+	const groupedPostId = await page.$( '#post_ID' );
+	let groupedPostIdValue = ( await ( await groupedPostId.getProperty( 'value' ) ).jsonValue() );
+	return groupedPostIdValue;
+}
+
+/**
  * Create a basic order with the provided order status.
  *
  * @param orderStatus Status of the new order. Defaults to `Pending payment`.
@@ -431,6 +465,7 @@ export {
 	completeOnboardingWizard,
 	createSimpleProduct,
 	createVariableProduct,
+	createGroupedProduct,
 	createSimpleOrder,
 	verifyAndPublish,
 	addProductToOrder,
