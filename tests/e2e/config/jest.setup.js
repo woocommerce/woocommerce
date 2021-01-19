10b639510b8edac53005069fa5a621edbcb25fe2
Ron Rennick <ron@ronandandrea.com>
2021-01-19 09:49:18 -0400

    use api to delete products before e2e tests

--- tests/e2e/config/jest.setup.js
+++ tests/e2e/config/jest.setup.js
@@ -1,10 +1,12 @@
-const {
+import { SimpleProduct } from '@woocommerce/api';
+import {
 	switchUserToAdmin,
 	visitAdminPage,
 	switchUserToTest,
 	clearLocalStorage,
-	setBrowserViewport
-} = require( "@wordpress/e2e-test-utils" );
+	setBrowserViewport,
+	factories,
+} from '@woocommerce/e2e-utils';
 
 /**
  * Navigates to the post listing screen and bulk-trashes any posts which exist.
@@ -36,32 +38,21 @@ async function trashExistingPosts() {
 }
 
 /**
- * Navigates to the product listing screen and bulk-trashes any product which exist.
+ * Use api package to delete products.
  *
  * @return {Promise} Promise resolving once products have been trashed.
  */
-async function trashExistingProducts() {
-	await switchUserToAdmin();
-	// Visit `/wp-admin/edit.php?post_type=product` so we can see a list of products and delete them.
-	await visitAdminPage( 'edit.php', 'post_type=product' );
+async function deleteAllProducts() {
+	const repository = SimpleProduct.restRepository( factories.api.withDefaultPermalinks );
+	let products;
 
-	// If this selector doesn't exist there are no products for us to delete.
-	const bulkSelector = await page.$( '#bulk-action-selector-top' );
-	if ( ! bulkSelector ) {
-		return;
+	products = await repository.list();
+	while ( products.length > 0 ) {
+		for( let p = 0; p < products.length; p++ ) {
+			await repository.delete( products[ p ].id );
+		}
+		products = await repository.list();
 	}
-
-	// Select all products.
-	await page.waitForSelector( '#cb-select-all-1' );
-	await page.click( '#cb-select-all-1' );
-	// Select the "bulk actions" > "trash" option.
-	await page.select( '#bulk-action-selector-top', 'trash' );
-	// Submit the form to send all draft/scheduled/published posts to the trash.
-	await page.click( '#doaction' );
-	await page.waitForXPath(
-		'//*[contains(@class, "updated notice")]/p[contains(text(), "moved to the Trash.")]'
-	);
-	await switchUserToTest();
 }
 
 // Before every test suite run, delete all content created by the test. This ensures
@@ -69,8 +60,7 @@ async function trashExistingProducts() {
 // each other's side-effects.
 beforeAll( async () => {
 	await trashExistingPosts();
-	await trashExistingProducts();
-	await switchUserToTest();
+	await deleteAllProducts();
 	await clearLocalStorage();
 	await setBrowserViewport( 'large' );
 } );
