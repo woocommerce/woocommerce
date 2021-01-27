ef20b35826663bc950b7aee43d9c670b81ed24a5
Moon <moon.kyong@automattic.com>
2021-01-27 15:58:06 -0800

    Normalize installed WC Admin version when comparing. (https://github.com/woocommerce/woocommerce-admin/pull/6205)

--- plugins/woocommerce-admin/src/Notes/ManageStoreActivityFromHomeScreen.php
+++ plugins/woocommerce-admin/src/Notes/ManageStoreActivityFromHomeScreen.php
@@ -38,6 +38,10 @@ class ManageStoreActivityFromHomeScreen {
 	 */
 	public static function get_note() {
 		$installed_version = get_option( 'woocommerce_admin_version' );
+		// the value can be in 1.9.0-rc.3 format for RC or BETA releases
+		// get the version without -rc.3.
+		$version_segments  = explode( '-', $installed_version );
+		$installed_version = $version_segments[0];
 		if ( ! version_compare( $installed_version, '1.9.0', '=' ) ) {
 			return;
 		}
