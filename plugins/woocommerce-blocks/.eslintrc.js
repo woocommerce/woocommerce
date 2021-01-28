bfaae9e5a1d1b222f69586238cfe8affa35b2d1c
Darren Ethier <darren@roughsmootheng.in>
2021-01-28 08:53:09 -0500

    Enable defaults for new import rules and configure (https://github.com/woocommerce/woocommerce-blocks/pull/3755)
    * enable rules and add exceptions
    
    The `import/no-unresolved` and `import/no-extraneous-dependencies` rules defaults are enabled and configuration for adding exceptions for both webpack config resolution and externals or indirect dependencies are added.
    
    * woops forgot to commit eslint-import-plugin-webpack resolver dependency
    
    * I guess node resolver needs installed too?
    
    * remove default resolve (doesn’t seem to help with the GH action errors)
    
    * change config to lowercase for node

--- plugins/woocommerce-blocks/.eslintrc.js
+++ plugins/woocommerce-blocks/.eslintrc.js
@@ -19,17 +19,44 @@ module.exports = {
 	},
 	settings: {
 		jsdoc: { mode: 'typescript' },
+		// List of modules that are externals in our webpack config.
+		// This helps the `import/no-extraneous-dependencies` and
+		//`import/no-unresolved` rules account for them.
+		'import/core-modules': [
+			'@woocommerce/block-data',
+			'@woocommerce/blocks-checkout',
+			'@woocommerce/settings',
+			'@woocommerce/shared-context',
+			'@woocommerce/shared-hocs',
+			'@woocommerce/knobs',
+			'@wordpress/a11y',
+			'@wordpress/api-fetch',
+			'@wordpress/block-editor',
+			'@wordpress/compose',
+			'@wordpress/data',
+			'@wordpress/escape-html',
+			'@wordpress/hooks',
+			'@wordpress/keycodes',
+			'@wordpress/url',
+			'babel-jest',
+			'dotenv',
+			'jest-environment-puppeteer',
+			'lodash/kebabCase',
+			'lodash',
+			'prop-types',
+			'react',
+			'requireindex',
+		],
+		'import/resolver': {
+			node: {},
+			webpack: {},
+		},
 	},
 	rules: {
 		'woocommerce/feature-flag': 'off',
 		'react-hooks/exhaustive-deps': 'error',
 		'react/jsx-fragments': [ 'error', 'syntax' ],
 		'@wordpress/no-global-active-element': 'warn',
-		// @todo Remove temporary conversion of eslint rules to warn instead of error.
-		//       These are new rules surfacing as a result of a eslint plugin update and the intent
-		//       is to update them in their own individual prs to make for easier review.
-		'import/no-extraneous-dependencies': 'warn',
-		'import/no-unresolved': 'warn',
 	},
 	overrides: [
 		{
