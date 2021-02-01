5be2c80b891e2e6c11fda41ae84a3fbc8a1a0733
mfs-mindsize <mark.simchock@mindsize.com>
2021-02-01 14:16:30 -0500

    adjusted new filter's default to option value + added absint

--- includes/wc-order-functions.php
+++ includes/wc-order-functions.php
@@ -924,7 +924,7 @@ function wc_cancel_unpaid_orders() {
 		}
 	}
 	wp_clear_scheduled_hook( 'woocommerce_cancel_unpaid_orders' );
-	$cancel_unpaid_interval = apply_filters( 'woocommerce_cancel_unpaid_orders_interval_minutes', 5 );
+	$cancel_unpaid_interval = apply_filters( 'woocommerce_cancel_unpaid_orders_interval_minutes', absint( $held_duration ) );
 	wp_schedule_single_event( time() + ( absint( $cancel_unpaid_interval ) * 60 ), 'woocommerce_cancel_unpaid_orders' );
 }
 add_action( 'woocommerce_cancel_unpaid_orders', 'wc_cancel_unpaid_orders' );
