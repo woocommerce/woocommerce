be2f682230b2cf30fb1cdd86c330ce3a7f0714d8
roykho <roykho77@gmail.com>
2021-02-03 09:24:02 -0800

    Update deprecated jQuery functions for jquery.flot

--- assets/js/jquery-flot/jquery.flot.pie.js
+++ assets/js/jquery-flot/jquery.flot.pie.js
@@ -120,10 +120,10 @@ More detail and specific examples can be found in the included HTML file.
 			var options = plot.getOptions();
 			if (options.series.pie.show) {
 				if (options.grid.hoverable) {
-					eventHolder.unbind("mousemove").mousemove(onMouseMove);
+					eventHolder.off("mousemove").on( 'mousemove', onMouseMove );
 				}
 				if (options.grid.clickable) {
-					eventHolder.unbind("click").click(onClick);
+					eventHolder.off("click").on( 'click', onClick );
 				}
 			}
 		});
@@ -180,11 +180,11 @@ More detail and specific examples can be found in the included HTML file.
 				// new one; this is more efficient and preserves any extra data
 				// that the user may have stored in higher indexes.
 
-				if ($.isArray(value) && value.length == 1) {
+				if (Array.isArray(value) && value.length == 1) {
     				value = value[0];
 				}
 
-				if ($.isArray(value)) {
+				if (Array.isArray(value)) {
 					// Equivalent to $.isNumeric() but compatible with jQuery < 1.7
 					if (!isNaN(parseFloat(value[1])) && isFinite(value[1])) {
 						value[1] = +value[1];
