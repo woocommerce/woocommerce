d559ff40b3727bc57120562807dd088e733c5d03
roykho <roykho77@gmail.com>
2021-02-04 10:57:55 -0800

    Revert load function

--- assets/js/admin/meta-boxes-product.js
+++ assets/js/admin/meta-boxes-product.js
@@ -495,7 +495,7 @@ jQuery( function( $ ) {
 				var this_page = window.location.toString();
 				this_page = this_page.replace( 'post-new.php?', 'post.php?post=' + woocommerce_admin_meta_boxes.post_id + '&action=edit&' );
 
-				$( '#variable_product_options' ).on( 'load', this_page + ' #variable_product_options_inner', function() {
+				$( '#variable_product_options' ).load( this_page + ' #variable_product_options_inner', function() {
 					$( '#variable_product_options' ).trigger( 'reload' );
 				} );
 			}
