16575092c45d894a0b0e73eb837e0047ac0a209a
roykho <roykho77@gmail.com>
2021-02-04 08:10:23 -0800

    Update deprecated jQuery functions for backbone-modal.js

--- assets/js/admin/backbone-modal.js
+++ assets/js/admin/backbone-modal.js
@@ -72,7 +72,7 @@
 			_.bindAll( this, 'render' );
 			this.render();
 
-			$( window ).resize(function() {
+			$( window ).on( 'resize', function() {
 				view.resizeContent();
 			});
 		},
@@ -88,7 +88,7 @@
 			}).append( this.$el );
 
 			this.resizeContent();
-			this.$( '.wc-backbone-modal-content' ).attr( 'tabindex' , '0' ).focus();
+			this.$( '.wc-backbone-modal-content' ).attr( 'tabindex' , '0' ).trigger( 'focus' );
 
 			$( document.body ).trigger( 'init_tooltips' );
 
