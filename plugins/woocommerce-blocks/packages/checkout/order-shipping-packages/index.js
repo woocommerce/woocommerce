3f8962a2a6a1aed90f006f8ba49291e552f27759
Seghir Nadir <nadir.seghir@gmail.com>
2021-02-08 12:37:55 +0100

    Add guards to useSlot and move shared code to a new file. (https://github.com/woocommerce/woocommerce-blocks/pull/3772)
    * guard against __experimentalUseSlot
    
    * move slot to its own file
    
    * remove todo
    
    * add docblocks
    
    * add docs to errorBoundary
    
    * add docs

--- plugins/woocommerce-blocks/packages/checkout/order-shipping-packages/index.js
+++ plugins/woocommerce-blocks/packages/checkout/order-shipping-packages/index.js
@@ -1,47 +1,21 @@
 /**
- * @todo Create guards against __experimentalUseSlot use.
- */
-/**
  * External dependencies
  */
 import classnames from 'classnames';
-import {
-	createSlotFill,
-	__experimentalUseSlot as useSlot,
-} from 'wordpress-components';
-import { CURRENT_USER_IS_ADMIN } from '@woocommerce/block-settings';
-import { Children, cloneElement } from '@wordpress/element';
 import { useStoreCart } from '@woocommerce/base-hooks';
 
 /**
  * Internal dependencies
  */
-import BlockErrorBoundary from '../error-boundary';
+import { createSlotFill, useSlot } from '../slot';
 
 const slotName = '__experimentalOrderShippingPackages';
-const { Fill, Slot: OrderShippingPackagesSlot } = createSlotFill( slotName );
+const {
+	Fill: ExperimentalOrderShippingPackages,
+	Slot: OrderShippingPackagesSlot,
+} = createSlotFill( slotName );
 
-function ExperimentalOrderShippingPackages( { children } ) {
-	return (
-		<Fill>
-			{ ( fillProps ) => {
-				return Children.map( children, ( fill ) => {
-					return (
-						<BlockErrorBoundary
-							renderError={
-								CURRENT_USER_IS_ADMIN ? null : () => null
-							}
-						>
-							{ cloneElement( fill, fillProps ) }
-						</BlockErrorBoundary>
-					);
-				} );
-			} }
-		</Fill>
-	);
-}
-
-function Slot( { className, collapsible, noResultsMessage, renderOption } ) {
+const Slot = ( { className, collapsible, noResultsMessage, renderOption } ) => {
 	// We need to pluck out receiveCart.
 	// eslint-disable-next-line no-unused-vars
 	const { extensions, receiveCart, ...cart } = useStoreCart();
@@ -49,7 +23,6 @@ function Slot( { className, collapsible, noResultsMessage, renderOption } ) {
 	const hasMultiplePackages = fills.length > 1;
 	return (
 		<OrderShippingPackagesSlot
-			bubblesVirtually
 			className={ classnames(
 				'wc-block-components-shipping-rates-control',
 				className
@@ -65,7 +38,7 @@ function Slot( { className, collapsible, noResultsMessage, renderOption } ) {
 			} }
 		/>
 	);
-}
+};
 
 ExperimentalOrderShippingPackages.Slot = Slot;
 
