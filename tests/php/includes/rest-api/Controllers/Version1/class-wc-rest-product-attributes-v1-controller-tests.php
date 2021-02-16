436a3db6e4b66403c5dbdf0af8a2d73f7e6d8031
Nestor Soriano <konamiman@konamiman.com>
2021-02-16 09:23:50 +0100

    Fix code standards errors

--- tests/php/includes/rest-api/Controllers/Version1/class-wc-rest-product-attributes-v1-controller-tests.php
+++ tests/php/includes/rest-api/Controllers/Version1/class-wc-rest-product-attributes-v1-controller-tests.php
@@ -6,14 +6,22 @@
  */
 class WC_REST_Product_Attributes_V1_Controller_Tests extends WC_Unit_Test_Case {
 
+	/**
+	 * Runs before any test.
+	 */
 	public function setUp() {
+		// phpcs:disable Generic.CodeAnalysis, Squiz.Commenting
 		$this->sut = new class() extends WC_REST_Product_Attributes_V1_Controller {
 			public function get_taxonomy( $request ) {
-				  return parent::get_taxonomy( $request );
+				return parent::get_taxonomy( $request );
 			}
 		};
+		// phpcs:enable Generic.CodeAnalysis, Squiz.Commenting
 	}
 
+	/**
+	 * testdox 'get_taxonomy' returns the proper values when called for different requests.
+	 */
 	public function test_get_taxonomy_returns_the_proper_values_for_different_requests() {
 		$this->register_legacy_proxy_function_mocks(
 			array(
