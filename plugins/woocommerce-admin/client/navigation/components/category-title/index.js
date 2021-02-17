bf3ed5ad87caa313109341ada505c2bd0fb942b4
Joshua T Flowers <joshuatf@gmail.com>
2021-02-17 09:29:28 -0500

    Add the favorites tooltip to the navigation (https://github.com/woocommerce/woocommerce-admin/pull/6312)
    * Add tooltip to plugin category titles
    
    * Add useAnchor
    
    * Hide tooltips if previously shown
    
    * Refactor tooltip and favorite button to separate components
    
    * Check if favorites resolved before showing button
    
    * Update favorites tooltip option name
    
    * Handle rebase changes
    
    * Don't show tooltip when nav is folded

--- plugins/woocommerce-admin/client/navigation/components/category-title/index.js
+++ plugins/woocommerce-admin/client/navigation/components/category-title/index.js
@@ -1,68 +1,21 @@
 /**
- * External dependencies
- */
-import { __ } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
-import { NAVIGATION_STORE_NAME } from '@woocommerce/data';
-import { recordEvent } from '@woocommerce/tracks';
-import { useDispatch, useSelect } from '@wordpress/data';
-
-/**
  * Internal dependencies
  */
 import './style.scss';
+import { FavoriteButton } from '../favorite-button';
+import { FavoritesTooltip } from '../favorites-tooltip';
 
 export const CategoryTitle = ( { category } ) => {
 	const { id, title } = category;
 
-	const { favorites, isResolving } = useSelect( ( select ) => {
-		return {
-			favorites: select( NAVIGATION_STORE_NAME ).getFavorites(),
-			isResolving: select( NAVIGATION_STORE_NAME ).isResolving(
-				'getFavorites'
-			),
-		};
-	} );
-
-	const { addFavorite, removeFavorite } = useDispatch(
-		NAVIGATION_STORE_NAME
-	);
-
-	const isFavorited = ( favorites || [] ).includes( id );
 	const className = 'woocommerce-navigation-category-title';
-
-	const toggleFavorite = () => {
-		const toggle = isFavorited ? removeFavorite : addFavorite;
-		toggle( id );
-		recordEvent( 'navigation_favorite', {
-			id,
-			action: isFavorited ? 'unfavorite' : 'favorite',
-		} );
-	};
 
 	if ( [ 'plugins', 'favorites' ].includes( category.menuId ) ) {
 		return (
 			<span className={ className }>
 				<span className={ `${ className }__text` }>{ title }</span>
-				{ ! isResolving && (
-					<Button
-						className={ `${ className }__favorite-button` }
-						isTertiary
-						onClick={ toggleFavorite }
-						icon={ isFavorited ? 'star-filled' : 'star-empty' }
-						aria-label={
-							isFavorited
-								? __(
-										'Add this item to your favorites.',
-										'woocommerce-admin'
-								  )
-								: __(
-										'Remove this item from your favorites.',
-										'woocommerce-admin'
-								  )
-						}
-					/>
-				) }
+				<FavoriteButton id={ id } />
+				<FavoritesTooltip />
 			</span>
 		);
 	}
