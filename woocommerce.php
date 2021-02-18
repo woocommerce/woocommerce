b103c1e357afa95b961e42cfef8b417d58c34581
Claudio Sanches <contato@claudiosanches.com>
2021-02-18 17:32:50 -0300

    Prepare default branch for 5.2-dev

--- woocommerce.php
+++ woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
- * Version: 5.1.0-dev
+ * Version: 5.2.0-dev
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
