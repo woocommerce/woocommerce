2930057847e2a568442ee8a9cb29b6e2d72b2f6a
vedanshujain <vedanshu.jain.2012@gmail.com>
2021-02-23 16:07:12 +0530

    Add tests to make sure discounts are rounded properly in cart.

---
+++ tests/php/includes/class-wc-cart-totals-test.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Class WC_Cart_Totals_Tests. Tests for WC_Cart_Total class.
+ */
+class WC_Cart_Totals_Tests extends WC_Unit_Test_Case {
+
+	/**
+	 * Tests whether discount tax is rounded properly in cart.
+	 *
+	 * @link https://github.com/woocommerce/woocommerce/issues/23916.
+	 */
+	public function test_discount_tax_rounding() {
+		update_option( 'woocommerce_prices_include_tax', 'yes' );
+		update_option( 'woocommerce_calc_taxes', 'yes' );
+
+		WC()->cart->empty_cart();
+
+		$tax_rate = array(
+			'tax_rate_country'  => '',
+			'tax_rate_state'    => '',
+			'tax_rate'          => '27.0000',
+			'tax_rate_name'     => 'TAX27',
+			'tax_rate_priority' => '1',
+			'tax_rate_compound' => '0',
+			'tax_rate_shipping' => '0',
+			'tax_rate_order'    => '1',
+		);
+
+		WC_Tax::_insert_tax_rate( $tax_rate );
+		$product_240  = WC_Helper_Product::create_simple_product( true, array( 'regular_price' => 240 ) );
+		$product_1250 = WC_Helper_Product::create_simple_product( true, array( 'regular_price' => 1250 ) );
+		$product_1990 = WC_Helper_Product::create_simple_product( true, array( 'regular_price' => 1990 ) );
+		$product_3390 = WC_Helper_Product::create_simple_product( true, array( 'regular_price' => 3390 ) );
+		$product_6200 = WC_Helper_Product::create_simple_product( true, array( 'regular_price' => 6200 ) );
+		$coupon = WC_Helper_Coupon::create_coupon( 'flat2000', array( 'coupon_amount' => 2000 ) );
+
+		WC()->cart->add_to_cart( $product_240->get_id(), 1 );
+		WC()->cart->add_to_cart( $product_1250->get_id(), 1 );
+		WC()->cart->add_to_cart( $product_1990->get_id(), 1 );
+		WC()->cart->add_to_cart( $product_3390->get_id(), 1 );
+		WC()->cart->add_to_cart( $product_6200->get_id(), 1 );
+		WC()->cart->apply_coupon( $coupon->get_code() );
+
+		$this->assert_discount_tax_rounding_when_rounding_at_subtotal();
+		$this->assert_discount_tax_rounding_when_rounding_at_line();
+	}
+
+	/**
+	 * Helper method for assertions when prices are rounded at line.
+	 */
+	private function assert_discount_tax_rounding_when_rounding_at_line() {
+		update_option( 'woocommerce_tax_round_at_subtotal', 'no' );
+		$decimal_precision = wc_get_price_decimals();
+		update_option( 'woocommerce_price_num_decimals', 0 );
+
+		WC()->cart->calculate_totals();
+		update_option( 'woocommerce_price_num_decimals', $decimal_precision );
+
+		$this->assertEquals( '1575', wc_format_decimal( WC()->cart->get_discount_total(), 0 ) );
+		$this->assertEquals( '425', wc_format_decimal( WC()->cart->get_discount_tax(), 0 ) );
+		$this->assertEquals( '11070', wc_format_decimal( WC()->cart->get_total(), 0 ) );
+	}
+
+	/**
+	 * Helper method for assertions when prices are rounded at line.
+	 */
+	private function assert_discount_tax_rounding_when_rounding_at_subtotal() {
+		update_option( 'woocommerce_tax_round_at_subtotal', 'yes' );
+		$decimal_precision = wc_get_price_decimals();
+		update_option( 'woocommerce_price_num_decimals', 0 );
+
+		WC()->cart->calculate_totals();
+
+		$this->assertEquals( '1575', wc_format_decimal( WC()->cart->get_discount_total(), 0 ) );
+		$this->assertEquals( '425', wc_format_decimal( WC()->cart->get_discount_tax(), 0 ) );
+		$this->assertEquals( '11070', wc_format_decimal( WC()->cart->get_total(), 0 ) );
+	}
+
+}
