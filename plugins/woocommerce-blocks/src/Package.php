5ce59edd057150ec9e92e9506f530846c5bdfc99
Thomas Roberts <thomas.roberts@automattic.com>
2021-02-23 11:19:09 +0000

    Bumping version strings to new version.

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -106,7 +106,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '4.5.1';
+					$version = '4.5.2';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
