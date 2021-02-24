5fecdd90f61b85da0061dabe700a042384363837
Ron Rennick <ron@ronandandrea.com>
2021-02-24 10:29:02 -0400

    add support for grouped product to api package

--- tests/e2e/api/src/repositories/rest/products/index.ts
+++ tests/e2e/api/src/repositories/rest/products/index.ts
@@ -1,9 +1,11 @@
 import { createProductTransformer } from './shared';
+import { groupedProductRESTRepository } from './grouped-product';
 import { simpleProductRESTRepository } from './simple-product';
 import { variableProductRESTRepository } from './variable-product';
 
 export {
 	createProductTransformer,
+	groupedProductRESTRepository,
 	simpleProductRESTRepository,
 	variableProductRESTRepository,
 };
