6c5d4670dbc121b5c3b878f3d1b902754b9646a4
Timmy Crawford <timmy.crawford@a8c.com>
2021-02-25 12:48:56 -0800

    Fix tests.

--- tests/php/includes/class-wc-tracker-test.php
+++ tests/php/includes/class-wc-tracker-test.php
@@ -9,46 +9,69 @@
  * Class WC_Tracker_Test
  */
 class WC_Tracker_Test extends \WC_Unit_Test_Case {
-
-    /**
-     * Utility method to add filter on woocommerce_admin_disabled
-     */
-    public function disable_woocommerce_admin() {
-        return true;
-    }
-
 	/**
 	 * Test the tracking of wc_admin being disabled via filter.
 	 */
 	public function test_wc_admin_disabled_get_tracking_data() {
 		$posted_data = null;
+
+		// Test the case for woocommerce_admin_disabled filter returning true.
+		add_filter(
+			'woocommerce_admin_disabled',
+			function( $default ) {
+				return true;
+			}
+		);
+
 		add_filter(
 			'pre_http_request',
 			function( $pre, $args, $url ) use ( &$posted_data ) {
+				if ( $posted_data ) {
+					$posted_data = null;
+				}
 				$posted_data = $args;
 				return true;
-			}, 3, 10
+			},
+			3,
+			10
 		);
 		WC_Tracker::send_tracking_data( true );
 		$tracking_data = json_decode( $posted_data['body'], true );
 
-        // Test the default case of no filter for set for woocommerce_admin_disabled.
-        $this->assertArrayHasKey( 'wc_admin_disabled', $tracking_data );
-		$this->assertEquals( 'no', $tracking_data['wc_admin_disabled'] );
-
-		// Test the case for woocommerce_admin_disabled filter returning true.
-        add_filter( 'wc_admin_disabled', $this->disable_woocommerce_admin() );
-        
-        // Bypass the 1h cooldown period so we can invoke send_tracking_data again.
-		add_filter(
-			'woocommerce_tracker_last_send_time',
-			function( $time ) { return $time - 10000; }
-		);
-		WC_Tracker::send_tracking_data( true );
-		$tracking_data_disabled_wc_admin = json_decode( $posted_data['body'], true );
-        $this->assertArrayHasKey( 'wc_admin_disabled', $tracking_data_disabled_wc_admin );
+		// Test the default case of no filter for set for woocommerce_admin_disabled.
+		$this->assertArrayHasKey( 'wc_admin_disabled', $tracking_data );
 		$this->assertEquals( 'yes', $tracking_data['wc_admin_disabled'] );
-        remove_filter( 'wc_admin_disabled', $this->disable_woocommerce_admin() );
+		$posted_data = null;
+
+		// Bypass time delay so we can invoke send_tracking_data again.
+		update_option( 'woocommerce_tracker_last_send', strtotime( '-2 weeks' ) );
 	}
 
+	/**
+	 * Test the tracking of wc_admin being not disabled via filter.
+	 */
+	public function test_wc_admin_not_disabled_get_tracking_data() {
+		$posted_data = null;
+		// Bypass time delay so we can invoke send_tracking_data again.
+		update_option( 'woocommerce_tracker_last_send', strtotime( '-2 weeks' ) );
+
+		add_filter(
+			'pre_http_request',
+			function( $pre, $args, $url ) use ( &$posted_data ) {
+				if ( $posted_data ) {
+					$posted_data = null;
+				}
+				$posted_data = $args;
+				return true;
+			},
+			3,
+			10
+		);
+		WC_Tracker::send_tracking_data( true );
+		$tracking_data = json_decode( $posted_data['body'], true );
+
+		// Test the default case of no filter for set for woocommerce_admin_disabled.
+		$this->assertArrayHasKey( 'wc_admin_disabled', $tracking_data );
+		$this->assertEquals( 'no', $tracking_data['wc_admin_disabled'] );
+	}
 }
