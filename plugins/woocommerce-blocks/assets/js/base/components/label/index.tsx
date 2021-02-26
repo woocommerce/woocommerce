e49a7eaffd0a736f447a9ec96d7d5ddedd5f6a14
Thomas Roberts <5656702+opr@users.noreply.github.com>
2021-02-26 11:57:49 +0000

    Update Label component to TSX (https://github.com/woocommerce/woocommerce-blocks/pull/3889)
    * Add tsconfig to components directory and include it as project
    
    * Add @types/classnames package
    
    * Migrate Label component to TSX
    
    * Remove unused imports from Label component
    
    * fix package-lock.json
    
    It was generated with npm 7 and we don’t support that yet (and that update should be done in a separate pull)
    
    * Add interface for LabelProps and implement.
    
    Also:
    - import `Fragment` from @wordpress/element.
    - import `HTMLAttributes` explicitly as a type from react (@types/wordpress__element doesn’t export this interface).
    
    * fix jest configuration
    
    Co-authored-by: Darren Ethier <darren@roughsmootheng.in>

--- plugins/woocommerce-blocks/assets/js/base/components/label/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/label/index.tsx
@@ -1,27 +1,29 @@
 /**
  * External dependencies
  */
-import PropTypes from 'prop-types';
-import { Fragment } from 'react';
+import { Fragment } from '@wordpress/element';
 import classNames from 'classnames';
+import type { HTMLAttributes } from 'react';
+
+interface LabelProps {
+	label?: string;
+	screenReaderLabel?: string;
+	wrapperElement?: string;
+	wrapperProps?: HTMLAttributes< HTMLElement >;
+}
 
 /**
  * Component used to render an accessible text given a label and/or a
  * screenReaderLabel. The wrapper element and wrapper props can also be
  * specified via props.
  *
- * @param {Object} props Incoming props for the component.
- * @param {string} [props.label] Label content.
- * @param {string} [props.screenReaderLabel] Content for screen readers.
- * @param {string} [props.wrapperElement] What element is used to wrap the label.
- * @param {Object} [props.wrapperProps] Props passed into wrapper element.
  */
 const Label = ( {
 	label,
 	screenReaderLabel,
 	wrapperElement,
 	wrapperProps = {},
-} ) => {
+}: LabelProps ): JSX.Element => {
 	let Wrapper;
 
 	const hasLabel = typeof label !== 'undefined' && label !== null;
@@ -55,13 +57,6 @@ const Label = ( {
 	}
 
 	return <Wrapper { ...wrapperProps }>{ label }</Wrapper>;
-};
-
-Label.propTypes = {
-	label: PropTypes.node,
-	screenReaderLabel: PropTypes.node,
-	wrapperElement: PropTypes.elementType,
-	wrapperProps: PropTypes.object,
 };
 
 export default Label;
