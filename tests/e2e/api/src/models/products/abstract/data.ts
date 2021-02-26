6cf413c378f53314a9112773d4b29099c0574ccf
Ron Rennick <ron@ronandandrea.com>
2021-02-26 17:19:54 -0400

    remove price from base product data

--- tests/e2e/api/src/models/products/abstract/data.ts
+++ tests/e2e/api/src/models/products/abstract/data.ts
@@ -93,56 +93,6 @@ export abstract class AbstractProductData extends Model {
 	 */
 	public readonly images: readonly ProductImage[] = [];
 
-	// @todo: remove price properties once https://github.com/woocommerce/woocommerce/issues/28885 is merged.
-	/**
-	 * The current price of the product.
-	 *
-	 * @type {string}
-	 */
-	public readonly price: string = '';
-
-	/**
-	 * The rendered HTML for the current price of the product.
-	 *
-	 * @type {string}
-	 */
-	public readonly priceHtml: string = '';
-
-	/**
-	 * The regular price of the product when not discounted.
-	 *
-	 * @type {string}
-	 */
-	public readonly regularPrice: string = '';
-
-	/**
-	 * Indicates whether or not the product is currently on sale.
-	 *
-	 * @type {boolean}
-	 */
-	public readonly onSale: boolean = false;
-
-	/**
-	 * The price of the product when on sale.
-	 *
-	 * @type {string}
-	 */
-	public readonly salePrice: string = '';
-
-	/**
-	 * The GMT datetime when the product should start to be on sale.
-	 *
-	 * @type {Date|null}
-	 */
-	public readonly saleStart: Date | null = null;
-
-	/**
-	 * The GMT datetime when the product should no longer be on sale.
-	 *
-	 * @type {Date|null}
-	 */
-	public readonly saleEnd: Date | null = null;
-
 	/**
 	 * The extra metadata for the product.
 	 *
