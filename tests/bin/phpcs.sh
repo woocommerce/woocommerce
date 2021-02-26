781eefc2c9e1883c28a8a4282b1808c0388234d7
Peter Fabian <peter.fabian.github@gmail.com>
2021-02-26 13:48:38 +0100

    debug2

--- tests/bin/phpcs.sh
+++ tests/bin/phpcs.sh
@@ -2,14 +2,11 @@
 
 echo '1'
 
-if [[ ${RUN_PHPCS} == 1 ]]; then
-	echo "2"
-	echo "git diff --name-only --diff-filter=ACMR ${COMMIT_RANGE}"
-	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR ${COMMIT_RANGE} | grep \\\\.php | awk '{print}' ORS=' '`
-	IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
-	echo "changed files: $CHANGED_FILES"
-	if [ "$CHANGED_FILES" != "" ]; then
-		echo "Running Code Sniffer."
-		vendor/bin/phpcs --ignore=$IGNORE --encoding=utf-8 -s -n -p $CHANGED_FILES
-	fi
+echo "git diff --name-only --diff-filter=ACMR ${COMMIT_RANGE}"
+CHANGED_FILES=`git diff --name-only --diff-filter=ACMR ${COMMIT_RANGE} | grep \\\\.php | awk '{print}' ORS=' '`
+IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
+echo "changed files: $CHANGED_FILES"
+if [ "$CHANGED_FILES" != "" ]; then
+	echo "Running Code Sniffer."
+	./vendor/bin/phpcs --ignore=$IGNORE --encoding=utf-8 -s -n -p $CHANGED_FILES
 fi
