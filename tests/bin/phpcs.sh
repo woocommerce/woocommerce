1b90130d52c244c3f24dbf3c867c9e0de35d92ff
Peter Fabian <peter.fabian.github@gmail.com>
2021-03-01 17:26:07 +0100

    Make PHPCS work, plus add PHPCS comments to code review.

--- tests/bin/phpcs.sh
+++ tests/bin/phpcs.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ ${RUN_PHPCS} == 1 ]]; then
-	echo "git diff --name-only --diff-filter=ACMR ${COMMIT_RANGE}"
-	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR ${COMMIT_RANGE} | grep \\\\.php | awk '{print}' ORS=' '`
-	IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
-
-	if [ "$CHANGED_FILES" != "" ]; then
-		echo "Running Code Sniffer."
-		vendor/bin/phpcs --ignore=$IGNORE --encoding=utf-8 -s -n -p $CHANGED_FILES
-	fi
+CHANGED_FILES=`git diff --name-only --diff-filter=ACMR "${1}..${2}" | grep \\\\.php | awk '{print}' ORS=' '`
+IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
+echo "Changed files: $CHANGED_FILES"
+if [ "$CHANGED_FILES" != "" ]; then
+	echo "Running Code Sniffer."
+	./vendor/bin/phpcs --ignore=$IGNORE --encoding=utf-8 -s -n -p --report-full --report-checkstyle=./phpcs-report.xml ${CHANGED_FILES}
 fi
