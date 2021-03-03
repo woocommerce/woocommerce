cfdd8fb0c03a52d85352d8ad1c6a5f32b113d066
Ron Rennick <ron@ronandandrea.com>
2021-03-03 19:52:29 -0400

    update shared objects among models
    - add optional defaultKey, defaultValue to meta data
    - move REST _links object to model shared
    - add _links collection to coupons

--- tests/e2e/api/src/models/shared-types.ts
+++ tests/e2e/api/src/models/shared-types.ts
@@ -36,12 +36,82 @@ export class MetaData extends Model {
 	public readonly value: any = '';
 
 	/**
+	 * The key of the metadata.
+	 *
+	 * @type {string}
+	 */
+	public readonly displayKey?: string = '';
+
+	/**
+	 * The value of the metadata.
+	 *
+	 * @type {*}
+	 */
+	public readonly displayValue?: string = '';
+
+	/**
 	 * Creates a new metadata.
 	 *
 	 * @param {Partial.<MetaData>} properties The properties to set.
 	 */
 	public constructor( properties?: Partial< MetaData > ) {
 		super();
+		Object.assign( this, properties );
+	}
+}
+
+/**
+ * An object link item.
+ */
+class LinkItem {
+	/**
+	 * The href of the link.
+	 *
+	 * @type {ReadonlyArray.<string>}
+	 */
+	public readonly href: string = '';
+
+	/**
+	 * Creates a new product link item.
+	 *
+	 * @param {Partial.<LinkItem>} properties The properties to set.
+	 */
+	public constructor( properties?: Partial< LinkItem > ) {
+		Object.assign( this, properties );
+	}
+}
+
+/**
+ * An object's links.
+ */
+export class ObjectLinks {
+	/**
+	 * The collection containing the object.
+	 *
+	 * @type {ReadonlyArray.<LinkItem>}
+	 */
+	public readonly collection: readonly LinkItem[] = [];
+
+	/**
+	 * Self referential link to the object.
+	 *
+	 * @type {ReadonlyArray.<LinkItem>}
+	 */
+	public readonly self: readonly LinkItem[] = [];
+
+	/**
+	 * The link to the parent object.
+	 *
+	 * @type {ReadonlyArray.<LinkItem>}
+	 */
+	public readonly up?: readonly LinkItem[] = [];
+
+	/**
+	 * Creates a new product link list.
+	 *
+	 * @param {Partial.<ObjectLinks>} properties The properties to set.
+	 */
+	public constructor( properties?: Partial< ObjectLinks > ) {
 		Object.assign( this, properties );
 	}
 }
