5bf3e8ed9868762cd84c6cd990352dc1666bbc6c
zhongruige <zhongruige@gmail.com>
2021-03-04 14:17:43 -0700

    Code review feedback

--- tests/e2e/core-tests/specs/index.js
+++ tests/e2e/core-tests/specs/index.js
@@ -73,7 +73,6 @@ const runMerchantTests = () => {
 	runProductEditDetailsTest();
 	runProductSearchTest();
 	runMerchantOrdersCustomerPaymentPage();
-	runMerchantOrderEmailsTest();
 }
 
 const runApiTests = () => {
