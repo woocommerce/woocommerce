7d8e5430ec1f372384f52ca59702e60f9007f458
Veljko <veljano@yahoo.com>
2021-03-05 12:05:07 +0100

    Fix select element

--- tests/e2e/core-tests/specs/merchant/wp-admin-order-searching.test.js
+++ tests/e2e/core-tests/specs/merchant/wp-admin-order-searching.test.js
@@ -39,7 +39,7 @@ const runOrderSearchingTest = () => {
 			await clearAndFillInput('#_shipping_city', 'Buffalo');
 			await clearAndFillInput('#_shipping_postcode', '14201');
 			await page.waitFor(1000); // to avoid flakiness
-			await page.select('#_shipping_state', 'NY');
+			await page.select('select[name="_shipping_state"]', 'NY');
 
 			// Save new order
 			await page.click('button.save_order');
