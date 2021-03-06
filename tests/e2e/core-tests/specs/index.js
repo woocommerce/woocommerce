94f83cb85942215ff13875ecfc73880eee940f41
Veljko <veljano@yahoo.com>
2021-03-06 12:09:12 +0100

    Revert changes and add waits

--- tests/e2e/core-tests/specs/index.js
+++ tests/e2e/core-tests/specs/index.js
@@ -19,6 +19,7 @@ const runSingleProductPageTest = require( './shopper/front-end-single-product.te
 const runVariableProductUpdateTest = require( './shopper/front-end-variable-product-updates.test' );
 
 // Merchant tests
+const runOrderSearchingTest = require( './merchant/wp-admin-order-searching.test' );
 const runCreateCouponTest = require( './merchant/wp-admin-coupon-new.test' );
 const runCreateOrderTest = require( './merchant/wp-admin-order-new.test' );
 const runEditOrderTest = require( './merchant/wp-admin-order-edit.test' );
@@ -32,7 +33,6 @@ const runOrderApplyCouponTest = require( './merchant/wp-admin-order-apply-coupon
 const runProductEditDetailsTest = require( './merchant/wp-admin-product-edit-details.test' );
 const runProductSearchTest = require( './merchant/wp-admin-product-search.test' );
 const runMerchantOrdersCustomerPaymentPage = require( './merchant/wp-admin-order-customer-payment-page.test' );
-const runOrderSearchingTest = require( './merchant/wp-admin-order-searching.test' );
 
 // REST API tests
 const runExternalProductAPITest = require( './api/external-product.test' );
