1aeb103feb0d74e07c37d41bcf1d45e918761e82
Claudio Sanches <contato@claudiosanches.com>
2021-03-09 16:57:08 -0300

    Return 0 if order isn't available

--- includes/abstracts/abstract-wc-payment-gateway.php
+++ includes/abstracts/abstract-wc-payment-gateway.php
@@ -262,7 +262,9 @@ abstract class WC_Payment_Gateway extends WC_Settings_API {
 		// Gets order total from "pay for order" page.
 		if ( 0 < $order_id ) {
 			$order = wc_get_order( $order_id );
-			$total = (float) $order->get_total();
+			if ( $order ) {
+				$total = (float) $order->get_total();
+			}
 
 			// Gets order total from cart/checkout.
 		} elseif ( 0 < WC()->cart->total ) {
