871af30517a4d6ab1af31a241eccc39f1f8a3243
Peter Fabian <peter.fabian.github@gmail.com>
2021-03-11 16:58:51 +0100

    Added comment documenting the history of the function.

--- includes/wc-stock-functions.php
+++ includes/wc-stock-functions.php
@@ -390,6 +390,10 @@ add_action( 'woocommerce_order_status_on-hold', 'wc_release_stock_for_order', 11
 /**
  * Return low stock amount to determine if notification needs to be sent
  *
+ * Since 5.3.0, this function no longer redirects from variation to its parent product.
+ * Low stock amount can now be attached to the variation itself and if it isn't,
+ * the default from store-wide setting gets used.
+ *
  * @param  WC_Product $product Product to get data from.
  * @since  3.5.0
  * @return int
