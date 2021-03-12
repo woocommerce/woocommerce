e2e914ed396621970e01c68ce9fe15d1d70dda8c
Claudio Sanches <contato@claudiosanches.com>
2021-03-12 14:30:07 -0300

    Remove todo

--- includes/admin/settings/class-wc-settings-advanced.php
+++ includes/admin/settings/class-wc-settings-advanced.php
@@ -487,7 +487,6 @@ class WC_Settings_Advanced extends WC_Settings_Page {
  * WC_Settings_Rest_API class.
  *
  * @deprecated 3.4 in favour of WC_Settings_Advanced.
- * @todo remove in 5.0.
  */
 class WC_Settings_Rest_API extends WC_Settings_Advanced {
 }
