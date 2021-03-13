63698cec3381ee99d4e9e15bcb964cf181271aa2
Veljko <veljano@yahoo.com>
2021-03-13 13:49:06 +0100

    Fix reordering tests

--- tests/e2e/core-tests/specs/index.js
+++ tests/e2e/core-tests/specs/index.js
@@ -63,13 +63,13 @@ const runShopperTests = () => {
 };
 
 const runMerchantTests = () => {
+	runAddNewShippingZoneTest();
 	runCreateCouponTest();
 	runCreateOrderTest();
 	runEditOrderTest();
 	runAddSimpleProductTest();
 	runAddVariableProductTest();
 	runUpdateGeneralSettingsTest();
-	runAddNewShippingZoneTest();
 	runProductSettingsTest();
 	runTaxSettingsTest();
 	runOrderStatusFiltersTest();
