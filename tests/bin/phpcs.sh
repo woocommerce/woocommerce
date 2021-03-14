75ed94bd38c665f6cb5162ceec42479b16966fb3
Claudio Sanches <contato@claudiosanches.com>
2021-03-13 21:01:21 -0300

    Fixed PHPCS run and added cs2pr support

--- tests/bin/phpcs.sh
+++ tests/bin/phpcs.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ ${RUN_PHPCS} == 1 ]]; then
-	CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
-	IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
+COMMIT_RANGE="${1}...${2}"
+CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
+IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
 
-	if [ "$CHANGED_FILES" != "" ]; then
-		echo "Running Code Sniffer."
-		vendor/bin/phpcs --ignore=$IGNORE --encoding=utf-8 -s -n -p $CHANGED_FILES
-	fi
+echo "Changed files: $CHANGED_FILES"
+
+if [ "$CHANGED_FILES" != "" ]; then
+	echo "Running Code Sniffer."
+	./vendor/bin/phpcs --ignore=$IGNORE --encoding=utf-8 -s -n -p --report-full --report-checkstyle=./phpcs-report.xml ${CHANGED_FILES}
 fi
