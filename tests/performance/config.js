8d1deab9937491820ee36149823be66050c2cb98
Tam Mullen <tammullen@outlook.com>
2021-03-19 16:40:04 +0000

    Proof of concept k6 test for checkout with config.

---
+++ tests/performance/config.js
@@ -0,0 +1,20 @@
+export const base_url = __ENV.URL || 'http://localhost:8084';
+export const base_host = __ENV.HOST || 'localhost:8084';
+
+export const addresses_customer_billing_first_name = 'John';
+export const addresses_customer_billing_last_name = 'Doe';
+export const addresses_customer_billing_company = 'Automattic';
+export const addresses_customer_billing_country = 'US';
+export const addresses_customer_billing_address_1 = 'addr 1';
+export const addresses_customer_billing_address_2 = 'addr 2';
+export const addresses_customer_billing_city = 'San Francisco';
+export const addresses_customer_billing_state = 'CA';
+export const addresses_customer_billing_postcode = '94107';
+export const addresses_customer_billing_phone = '123456789';
+export const addresses_customer_billing_email = 'john.doe@example.com';
+
+export const payment_method = 'cod';
+
+export const product_sku = 'woo-beanie';
+export const product_url = 'beanie';
+export const product_id = '13';
