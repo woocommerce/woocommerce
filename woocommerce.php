a1a1438c0e8c65feaf199118eb48fec142ea79aa
Nestor Soriano <konamiman@konamiman.com>
2021-03-19 10:50:55 +0100

    Update minimum required version of WordPress to 5.5

--- woocommerce.php
+++ woocommerce.php
@@ -8,7 +8,7 @@
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
  * Domain Path: /i18n/languages/
- * Requires at least: 5.4
+ * Requires at least: 5.5
  * Requires PHP: 7.0
  *
  * @package WooCommerce
