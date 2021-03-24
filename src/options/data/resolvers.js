f0098cdbcb35c45c52a965ff5faeb21a6a25c9eb
Moon <moon.kyong@automattic.com>
2021-03-23 22:54:19 -0700

    Options improvement
    * Added loading state
    
    * Fixed bug where it does not search when the same keyword is used twice

--- src/options/data/resolvers.js
+++ src/options/data/resolvers.js
@@ -7,13 +7,15 @@ import { apiFetch } from '@wordpress/data-controls';
  * Internal dependencies
  */
 import { API_NAMESPACE } from './constants';
-import { setOptions } from './actions';
+import { setLoadingState, setOptions } from './actions';
 
 export function* getOptions( search ) {
 	let path = `${ API_NAMESPACE }/options?`;
 	if ( search ) {
 		path += `search=${ search }`;
 	}
+
+	yield setLoadingState( true );
 
 	try {
 		const response = yield apiFetch( {
