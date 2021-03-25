5b3bc1665dfb3e672f969af3a4028786a22e16c3
Veljko <veljano@yahoo.com>
2021-03-25 18:12:54 +0100

    Updated core-tests-root file

--- tests/e2e/core-tests/core-tests-root.js
+++ tests/e2e/core-tests/core-tests-root.js
@@ -6,17 +6,21 @@
  const getCoreTestsRoot = () => {
      // Figure out where we're installed.
      // Typically will be in node_modules/, but WooCommerce
-     // uses a local file path (tests/e2e/env).
+     // uses a local file path (tests/e2e/core-tests).
      let coreTestsPath = false;
-     const moduleDir = path.dirname( require.resolve( '@woocommerce/e2e-environment' ) );
+     const moduleDir = path.dirname( require.resolve( '@woocommerce/e2e-core-tests' ) );
  
      if ( -1 < moduleDir.indexOf( 'node_modules' ) ) {
         coreTestsPath = moduleDir.split( 'node_modules' )[ 0 ];
-     } else if ( -1 < moduleDir.indexOf( 'tests/e2e/env' ) ) {
-        coreTestsPath = moduleDir.split( 'tests/e2e/env' )[ 0 ];
+     } else if ( -1 < moduleDir.indexOf( 'tests/e2e/core-tests' ) ) {
+        coreTestsPath = moduleDir.split( 'tests/e2e/core-tests' )[ 0 ];
      }
  
-     return coreTestsPath;
+     return {
+      appRoot: coreTestsPath,
+      packageRoot: moduleDir,
+      };
  };
  
  module.exports = getCoreTestsRoot;
+ 
