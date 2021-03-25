a320be734ee5bcea3d36d3be41c7be28bb91ddd4
Joel Thiessen <dygerati@gmail.com>
2021-03-25 13:11:36 -0700

    Fix typo with import from app instead of App

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { render } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-import { App } from './app';
+import { App } from './App';
 import './index.scss';
 
-const appRoot = document.getElementById( 'woocommerce-admin-test-helper-app-root' );
+const appRoot = document.getElementById(
+	'woocommerce-admin-test-helper-app-root'
+);
 
-if ( appRoot ) {
-    render( <App/>, appRoot );
+if (appRoot) {
+	render(<App />, appRoot);
 }
