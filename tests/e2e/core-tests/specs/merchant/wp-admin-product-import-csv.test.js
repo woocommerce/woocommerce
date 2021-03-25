c7077524e4da472362dd283f48a400290eadce28
Veljko <veljano@yahoo.com>
2021-03-25 18:12:54 +0100

    Updated core-tests-root file

--- tests/e2e/core-tests/specs/merchant/wp-admin-product-import-csv.test.js
+++ tests/e2e/core-tests/specs/merchant/wp-admin-product-import-csv.test.js
@@ -19,8 +19,8 @@ const {
 
 const path = require( 'path' );
 const coreTestsPath = getCoreTestsRoot();
-const filePath = path.resolve( coreTestsPath, 'sample-data/sample_products.csv' );
-const filePathOverride = path.resolve( coreTestsPath, 'tests/e2e/core-tests/test-data/sample_products_override.csv' );
+const filePath = path.resolve( coreTestsPath.appRoot, 'sample-data/sample_products.csv' );
+const filePathOverride = path.resolve( coreTestsPath.packageRoot, 'test-data/sample_products_override.csv' );
 const productNames = ["V-Neck T-Shirt", "Hoodie", "Hoodie with Logo", "T-Shirt", "Beanie",
 	"Belt", "Cap", "Sunglasses", "Hoodie with Pocket", "Hoodie with Zipper", "Long Sleeve Tee", "Polo", "Album",
 	"Single", "T-Shirt with Logo", "Beanie with Logo", "Logo Collection", "WordPress Pennant"];
