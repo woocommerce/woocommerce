01e6de385dd0b90ccb62c08942062fe4676270e0
rodel <od.rodel.calasagsag@gmail.com>
2021-03-28 12:31:20 +0800

    WIP: re-construct products

--- tests/e2e/core-tests/specs/merchant/wp-admin-order-new.test.js
+++ tests/e2e/core-tests/specs/merchant/wp-admin-order-new.test.js
@@ -8,7 +8,8 @@ const {
 	uiUnblocked
 } = require('@woocommerce/e2e-utils');
 const config = require('config');
-const { HTTPClientFactory,
+const {
+	HTTPClientFactory,
 	VariableProduct,
 	GroupedProduct,
 	SimpleProduct,
@@ -18,21 +19,35 @@ const { HTTPClientFactory,
 
 let variations;
 let products;
+const taxClasses = [
+	{
+		name: 'Tax Class Simple',
+		slug: 'tax-class-simple'
+	},
+	{
+		name: 'Tax Class Variable',
+		slug: 'tax-class-variable'
+	},
+	{
+		name: 'Tax Class External',
+		slug: 'tax-class-external '
+	}
+];
 const taxRates = [
 	{
-		name: 'My Standard Tax 1',
+		name: 'Tax Rate Simple',
 		rate: '10',
-		class: 'standard'
+		class: 'tax-class-simple'
 	},
 	{
-		name: 'My Reduced Rate Tax 2',
+		name: 'Tax Rate Variable',
 		rate: '20',
-		class: 'reduced-rate'
+		class: 'tax-class-variable'
 	},
 	{
-		name: 'My Zero Rate Tax 3',
+		name: 'Tax Rate External',
 		rate: '30',
-		class: 'zero-rate'
+		class: 'tax-class-external'
 	}
 ];
 
@@ -47,21 +62,132 @@ const runCreateOrderTest = () => {
 				.withBasicAuth(adminUsername, adminPassword)
 				.create();
 
+			// Initialize tax classes and rates
+			const initTaxClassesAndRates = async () => {
+				// Enable taxes in settings
+				const enableTaxes = async () => {
+					const path =
+						'/wc/v3/settings/general/woocommerce_calc_taxes';
+					const data = {
+						value: 'yes'
+					};
+					await httpClient.put(path, data);
+				};
+				await enableTaxes();
+
+				// Initialize tax classes
+				const initTaxClasses = async () => {
+					const path = '/wc/v3/taxes/classes';
+					for (const txClass of taxClasses) {
+						await httpClient.post(path, txClass);
+					}
+				};
+				await initTaxClasses();
+
+				// Set up tax rates
+				const initTaxRates = async () => {
+					const path = '/wc/v3/taxes';
+					for (const rate of taxRates) {
+						await httpClient.post(path, rate);
+					}
+				};
+				await initTaxRates();
+			};
+			await initTaxClassesAndRates();
+
 			// Initialize products for each product type
 			const initProducts = async () => {
 				// Initialization functions per product type
 				const initSimpleProduct = async () => {
 					const repo = SimpleProduct.restRepository(httpClient);
-					const props = config.get('products.simple');
-					return await repo.create(props);
+					const simpleProduct = {
+						name: 'Simple Product',
+						regularPrice: '100',
+						tax_class: 'Tax Class Simple'
+					};
+					return await repo.create(simpleProduct);
 				};
 				const initVariableProduct = async () => {
-					const prodProps = config.get('products.variable');
-					variations = config.get('products.variations');;
-					const productRepo = VariableProduct.restRepository(httpClient);
-					const variationRepo = ProductVariation.restRepository(httpClient);
-					const variableProduct = await productRepo.create(prodProps);
-					for (const v of variations) {
+					// todo remove these old code
+					// const prodProps = config.get('products.variable');
+					// variations = config.get('products.variations');
+
+					// Set the default attributes
+					// todo see if this is still necessary. remove if not
+					const defaultAttributes = [
+						{
+							name: 'Default Size',
+							option: 'Medium'
+						},
+						{
+							name: 'Default Colour',
+							option: 'White'
+						}
+					];
+
+					// Set attributes
+					// todo see if this is still necessary. remove if not
+					const attributes = [
+						{
+							name: 'Size',
+							variation: true,
+							options: ['Small', 'Medium', 'Large']
+						},
+						{
+							name: 'Colour',
+							otions: ['Red', 'Green', 'Blue'],
+							variation: true
+						}
+					];
+
+					// todo create variations (this is where we set tax classes for variable products)
+					const variationData = [
+						{
+							regularPrice: '200',
+							attributes: [
+								{
+									name: 'Size',
+									option: 'Small'
+								},
+								{
+									name: 'Colour',
+									option: 'Cyan'
+								}
+							],
+							tax_class: 'Tax Class Variable'
+						},
+						{
+							regularPrice: '300',
+							attributes: [
+								{
+									name: 'Size',
+									option: 'Medium'
+								},
+								{
+									name: 'Colour',
+									option: 'Magenta'
+								}
+							],
+							tax_class: 'Tax Class Variable'
+						}
+					];
+
+					// todo construct variable product object
+					const variableProductData = {
+						name: 'Variable Product',
+						type: 'variable'
+					};
+
+					const variationRepo = ProductVariation.restRepository(
+						httpClient
+					);
+					const productRepo = VariableProduct.restRepository(
+						httpClient
+					);
+					const variableProduct = await productRepo.create(
+						variableProductData
+					);
+					for (const v of variationData) {
 						await variationRepo.create(variableProduct.id, v);
 					}
 
@@ -86,50 +212,18 @@ const runCreateOrderTest = () => {
 
 				return [
 					simpleProduct,
-					variableProduct,
-					groupedProduct,
-					externalProduct
+					variableProduct
+					// groupedProduct,
+					// externalProduct
 				];
 			};
 			products = await initProducts();
 
-			// Initialize tax rates & classes
-			const initTaxes = async () => {
-				// Enable taxes in settings
-				const enableTaxes = async () => {
-					const path = '/wc/v3/settings/general/woocommerce_calc_taxes';
-					const data = {
-						value: 'yes'
-					}
-					await httpClient.put(path, data);
-				}
-				await enableTaxes();
-
-				// Delete existing tax rates
-				const deleteTaxRates = async () => {
-					const path = '/wc/v3/taxes';
-					await httpClient.get(path).then(async (response) => {
-						for (const { id } of response.data) {
-							await httpClient.delete(`${path}/${id}?force=true`);
-						}
-					});
-				};
-				await deleteTaxRates();
-
-				// Set up tax rates
-				const createTaxRates = async () => {
-					const path = '/wc/v3/taxes';
-					for (const t of taxRates) {
-						await httpClient.post(path, t);
-					}
-				};
-				await createTaxRates();
-			};
-			await initTaxes();
-
 			// Login
 			await merchant.login();
 		});
+
+		// todo afterAll cleanup
 
 		it('can create new order', async () => {
 			// Go to "add order" page
@@ -153,6 +247,7 @@ const runCreateOrderTest = () => {
 			);
 		});
 
+		// todo remove .only
 		it('can create new complex order with multiple product types & tax classes', async () => {
 			// Go to "add order" page
 			await merchant.openNewOrder();
@@ -164,11 +259,16 @@ const runCreateOrderTest = () => {
 
 			// Search for each product to add, then verify that they are saved
 			for (const { name } of products) {
-				await expect(page).toClick('.wc-backbone-modal-content tr:last-child .select2-selection__arrow');
-				await expect(page).toFill('#wc-backbone-modal-dialog + .select2-container .select2-search__field',
+				await expect(page).toClick(
+					'.wc-backbone-modal-content tr:last-child .select2-selection__arrow'
+				);
+				await expect(page).toFill(
+					'#wc-backbone-modal-dialog + .select2-container .select2-search__field',
 					name
 				);
-				const firstResult = await page.waitForSelector('li[data-selected]');
+				const firstResult = await page.waitForSelector(
+					'li[data-selected]'
+				);
 				await firstResult.click();
 				await expect(page).toMatchElement(
 					'.wc-backbone-modal-content tr:nth-last-child(2) .wc-product-search option',
@@ -190,14 +290,18 @@ const runCreateOrderTest = () => {
 			await expect(page).toClick('button.save_order');
 			await page.waitForNavigation();
 			for (const { name } of products) {
-				await expect(page).toMatchElement('.wc-order-item-name', { text: name });
+				await expect(page).toMatchElement('.wc-order-item-name', {
+					text: name
+				});
 			}
 
 			// Verify variation details
 			const firstVariation = variations[0];
 			const attributes = firstVariation.attributes;
 			const actualAttributes = await page.$('.display_meta');
-			await expect(page).toMatchElement('.wc-order-item-variation', { text: firstVariation.id });
+			await expect(page).toMatchElement('.wc-order-item-variation', {
+				text: firstVariation.id
+			});
 			for (const { name, option } of attributes) {
 				await expect(actualAttributes).toMatch(name);
 				await expect(actualAttributes).toMatch(option);
@@ -205,18 +309,26 @@ const runCreateOrderTest = () => {
 
 			// Verify that the names of each tax class were shown
 			for (const { name } of taxRates) {
-				await expect(page).toMatchElement('th.line_tax', { text: name });
-				await expect(page).toMatchElement('.wc-order-totals td.label', { text: name });
+				await expect(page).toMatchElement('th.line_tax', {
+					text: name
+				});
+				await expect(page).toMatchElement('.wc-order-totals td.label', {
+					text: name
+				});
 			}
 
 			// Verify tax amounts
 			const taxTotals = ['£10.00', '£4.00', '£7.50'];
 			for (const amount of taxTotals) {
-				await expect(page).toMatchElement('td.line_tax', { text: amount });
-				await expect(page).toMatchElement('.wc-order-totals td.total', { text: amount });
+				await expect(page).toMatchElement('td.line_tax', {
+					text: amount
+				});
+				await expect(page).toMatchElement('.wc-order-totals td.total', {
+					text: amount
+				});
 			}
-		})
+		});
 	});
-}
+};
 
 module.exports = runCreateOrderTest;
