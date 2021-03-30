eb1bf9ccbde8b44d91323999f2d9ae401a420c31
Nestor Soriano <konamiman@konamiman.com>
2021-03-30 11:04:44 +0200

    Make product name escaping consistent in the front-end

--- templates/content-widget-reviews.php
+++ templates/content-widget-reviews.php
@@ -21,14 +21,27 @@ defined( 'ABSPATH' ) || exit;
 <li>
 	<?php do_action( 'woocommerce_widget_product_review_item_start', $args ); ?>
 
+	<?php
+	// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+	?>
+
 	<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
 		<?php echo $product->get_image(); ?>
-		<span class="product-title"><?php echo $product->get_name(); ?></span>
+		<span class="product-title"><?php echo wp_kses_post( $product->get_name() ); ?></span>
 	</a>
 
 	<?php echo wc_get_rating_html( intval( get_comment_meta( $comment->comment_ID, 'rating', true ) ) ); ?>
 
-	<span class="reviewer"><?php echo sprintf( esc_html__( 'by %s', 'woocommerce' ), get_comment_author( $comment->comment_ID ) ); ?></span>
+	<span class="reviewer">
+	<?php
+	/* translators: %s: Comment author. */
+	echo sprintf( esc_html__( 'by %s', 'woocommerce' ), get_comment_author( $comment->comment_ID ) );
+	?>
+	</span>
+
+	<?php
+	// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
+	?>
 
 	<?php do_action( 'woocommerce_widget_product_review_item_end', $args ); ?>
 </li>
--- templates/order/order-details-item.php
+++ templates/order/order-details-item.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.7.0
+ * @version 5.2.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -30,7 +30,7 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 		$is_visible        = $product && $product->is_visible();
 		$product_permalink = apply_filters( 'woocommerce_order_item_permalink', $is_visible ? $product->get_permalink( $item ) : '', $item, $order );
 
-		echo apply_filters( 'woocommerce_order_item_name', $product_permalink ? sprintf( '<a href="%s">%s</a>', $product_permalink, $item->get_name() ) : $item->get_name(), $item, $is_visible ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo wp_kses_post( apply_filters( 'woocommerce_order_item_name', $product_permalink ? sprintf( '<a href="%s">%s</a>', $product_permalink, $item->get_name() ) : $item->get_name(), $item, $is_visible ) );
 
 		$qty          = $item->get_quantity();
 		$refunded_qty = $order->get_qty_refunded_for_item( $item_id );
