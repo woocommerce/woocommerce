8d68dceacb359d359e1148ed83a0f5f76debcfdd
Raluca Stan <ralucastn@gmail.com>
2021-04-01 12:13:18 +0100

    Update to 4.9.0-dev version

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -106,7 +106,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '4.8.0';
+					$version = '4.9.0-dev';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
