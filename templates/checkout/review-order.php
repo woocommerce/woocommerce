05bbc740bcb6385522b82c8d5146466b09f5d9c3
Brian <brian@brianhaas.li>
2021-04-04 01:04:57 +0200

    quick version fix

--- templates/checkout/review-order.php
+++ templates/checkout/review-order.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.8.0
+ * @version 5.2.0
  */
 
 defined( 'ABSPATH' ) || exit;
