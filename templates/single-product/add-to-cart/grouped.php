02868d224c08d5fbc98755526a8d1b771dbad45e
roykho <roykho77@gmail.com>
2021-04-06 11:37:52 -0700

    Bump version for template closes #29595

--- templates/single-product/add-to-cart/grouped.php
+++ templates/single-product/add-to-cart/grouped.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 4.0.0
+ * @version 4.8.0
  */
 
 defined( 'ABSPATH' ) || exit;
