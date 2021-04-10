12ea9273f113f7b079b1956629964914856408b7
Michael Silber <michael@cfxdesign.com>
2021-04-10 14:46:24 -0400

    Add woocommerce_after_variations_table action hook after variations table

--- plugins/woocommerce/templates/single-product/add-to-cart/variable.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/variable.php
@@ -52,6 +52,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 				<?php endforeach; ?>
 			</tbody>
 		</table>
+		<?php do_action( 'woocommerce_after_variations_table' ); ?>
 
 		<div class="single_variation_wrap">
 			<?php
