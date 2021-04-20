21aee612868c1b9f4a98c3d1cdf852476e14c7dc
Nestor Soriano <konamiman@konamiman.com>
2021-04-20 09:42:07 +0200

    Fix code sniffer violations in stock functions and its tests

--- includes/wc-stock-functions.php
+++ includes/wc-stock-functions.php
@@ -403,7 +403,7 @@ function wc_get_low_stock_amount( WC_Product $product ) {
 	$low_stock_amount = $product->get_low_stock_amount();
 
 	if ( '' === $low_stock_amount && $product->is_type( 'variation' ) ) {
-		$product = wc_get_product( $product->get_parent_id() );
+		$product          = wc_get_product( $product->get_parent_id() );
 		$low_stock_amount = $product->get_low_stock_amount();
 	}
 
