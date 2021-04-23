14e14d3719d99cc6e0af90ba5b2da684fab675f0
Fernando Marichal <contacto@fernandomarichal.com>
2021-04-23 16:37:34 -0300

    Added note types

--- api/admin-notes/add-note.php
+++ api/admin-notes/add-note.php
@@ -6,44 +6,61 @@ register_woocommerce_admin_test_helper_rest_route(
 	'admin_notes_add_note'
 );
 
-register_woocommerce_admin_test_helper_rest_route(
-	'/admin-notes/add-email-note/v1',
-	'admin_notes_add_email_note'
-);
-
 
 function admin_notes_add_note( $request ) {
 	$note = new Note();
+	$default_note_data = get_default_note_data();
+	$type = $request->get_param( 'type' );
+	$layout = $request->get_param( 'layout' );
 
 	$note->set_name( $request->get_param( 'name' ) );
 	$note->set_title( $request->get_param( 'title' ) );
+	$note->set_content( $default_note_data[ 'content' ] );
+	$note->set_image( $default_note_data[ $type ][ $layout ] );
+	$note->set_layout( $layout );
+	$note->set_type( $type );
+	possibly_add_action( $note );
 	
+	if ( 'email' === $type ) {
+		add_email_note_params( $note );
+	}
+
 	$note->save();
 
 	return true;
 }
 
-function admin_notes_add_email_note( $request ) {
-	$note = new Note();
-
+function add_email_note_params( $note ) {
 	$additional_data = array(
 		'role' => 'administrator',
 	);
+	$note->set_content_data( (object) $additional_data );
+}
+
+function possibly_add_action( $note ) {
+	if ( $note->get_type() === 'info' ) {
+		return;
+	}
 	$action_name = sprintf(
 		'test-action-%s',
-		$request->get_param( 'name' )
+		$note->get_name()
 	);
-
-	$content = $request->get_param( 'content' ) ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.';
-	
-	$note->set_name( $request->get_param( 'name' ) );
-	$note->set_title( $request->get_param( 'title' ) );
-	$note->set_type( 'email' );
-	$note->set_content( $content );
-	$note->set_content_data( (object) $additional_data );
 	$note->add_action( $action_name, 'Test action', wc_admin_url() );
-	
-	$note->save();
+}
 
-	return true;
+function get_default_note_data() {
+	return array(
+		'content'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+		'info'      => array(
+			'banner'    => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSAK9Xg4L6FGmDAW5UVtVEv1IXKtGV3-rxYLfAzOBF-fMUdmyWz&usqp=CAU',
+			'thumbnail' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlm2mX1h6GfMnT9sDD9GsJB7of3MZw3UDzCAQM9EBepiJzyxXH&usqp=CAU',
+			'plain'     => ''
+		),
+		'email'     => array(
+			'plain' => 'https://www.logosvgpng.com/wp-content/uploads/2018/10/woocommerce-logo-vector.png'
+		),
+		'update'    => array(
+			'plain' => ''
+		)
+	);
 }
--- src/admin-notes/admin-notes.js
+++ src/admin-notes/admin-notes.js
@@ -3,7 +3,6 @@
  */
 import { DeleteAllNotes } from './delete-all-notes';
 import { AddNote } from './add-note';
-import { AddEmailNote } from './add-email-note';
 
 export const AdminNotes = () => {
 	return (
@@ -11,7 +10,6 @@ export const AdminNotes = () => {
 			<h2>Admin notes</h2>
 			<p>This section contains tools for managing admin notes.</p>
 			<AddNote />
-			<AddEmailNote />
 			<DeleteAllNotes />
 		</>
 	);
