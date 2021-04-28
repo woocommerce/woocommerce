0ac8db592cd7da97d8c269db814dc7b19db9097e
Nestor Soriano <konamiman@konamiman.com>
2021-04-28 09:47:31 +0200

    Improvements in the engine behind the debug tools page.
    Two new optional keys have been added to the tool definition array:
    
    - 'disabled': when true the tool button will appear disabled.
    - 'needs_refresh': when running a tool, by default the tool definitions
      are retrieved first, then the selected tool is executed,
      then the definitions previously retrieved are rendered.
      When this key is true the tool definitions are retrieved again
      after execution, useful for cases where the tool description
      or button enable/disable state changes after the tool execution.
    
    Also now if a tool execution throws an exception a notice will be
    shown with the execption message, previously the exception
    was unhandled.

--- tests/php/src/Utilities/ArrayUtilTest.php
+++ tests/php/src/Utilities/ArrayUtilTest.php
@@ -8,6 +8,7 @@ use Automattic\WooCommerce\Utilities\ArrayUtil;
  * A collection of tests for the array utility class.
  */
 class ArrayUtilTest extends \WC_Unit_Test_Case {
+
 	/**
 	 * @testdox `get_nested_value` should return null if the requested key doesn't exist and no default value is supplied.
 	 *
@@ -62,5 +63,75 @@ class ArrayUtilTest extends \WC_Unit_Test_Case {
 		$actual = ArrayUtil::get_nested_value( $array, 'foo::bar::fizz' );
 
 		$this->assertEquals( 'buzz', $actual );
+	}
+
+	/**
+	 * @testdox `is_truthy` returns false when the key does not exist in the array.
+	 */
+	public function test_is_truthy_returns_false_if_key_does_not_exist() {
+		$array = array( 'foo' => 'bar' );
+
+		$this->assertFalse( ArrayUtil::is_truthy( $array, 'fizz' ) );
+	}
+
+	/**
+	 * @testdox `is_truthy` returns false for values that evaluate to false.
+	 *
+	 * @testWith [0]
+	 *           ["0"]
+	 *           [false]
+	 *           [""]
+	 *           [null]
+	 *           [[]]
+	 *
+	 * @param mixed $value Value to test.
+	 */
+	public function test_is_truthy_returns_false_if_value_evaluates_to_false( $value ) {
+		$array = array( 'foo' => $value );
+
+		$this->assertFalse( ArrayUtil::is_truthy( $array, 'foo' ) );
+	}
+
+	/**
+	 * @testdox `is_truthy` returns true for values that evaluate to true.
+	 *
+	 * @testWith [1]
+	 *           ["foo"]
+	 *           [true]
+	 *           [[1]]
+	 *
+	 * @param mixed $value Value to test.
+	 */
+	public function test_is_truthy_returns_false_if_value_evaluates_to_true( $value ) {
+		$array = array( 'foo' => $value );
+
+		$this->assertTrue( ArrayUtil::is_truthy( $array, 'foo' ) );
+	}
+
+	/**
+	 * @testdox `get_value_or_default` returns the correct value for an existing key.
+	 */
+	public function test_get_value_or_default_returns_value_if_key_exists() {
+		$array = array( 'foo' => 'bar' );
+
+		$this->assertEquals( 'bar', ArrayUtil::get_value_or_default( $array, 'foo' ) );
+	}
+
+	/**
+	 * @testdox `get_value_or_default` returns null if the key does not exist and no default value is supplied.
+	 */
+	public function test_get_value_or_default_returns_null_if_key_not_exists_and_no_default_supplied() {
+		$array = array( 'foo' => 'bar' );
+
+		$this->assertNull( ArrayUtil::get_value_or_default( $array, 'fizz' ) );
+	}
+
+	/**
+	 * @testdox `get_value_or_default` returns the supplied default value if the key does not exist.
+	 */
+	public function test_get_value_or_default_returns_supplied_default_value_if_key_not_exists() {
+		$array = array( 'foo' => 'bar' );
+
+		$this->assertEquals( 'buzz', ArrayUtil::get_value_or_default( $array, 'fizz', 'buzz' ) );
 	}
 }
