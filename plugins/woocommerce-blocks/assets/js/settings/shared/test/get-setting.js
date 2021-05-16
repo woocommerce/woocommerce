b14b557c91771ef39d281c4dbff6c9b226d58bb5
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-05-16 20:00:06 +0200

    Don't default to fallback in getSetting if value is falsy (https://github.com/woocommerce/woocommerce-blocks/pull/4202)

--- plugins/woocommerce-blocks/assets/js/settings/shared/test/get-setting.js
+++ plugins/woocommerce-blocks/assets/js/settings/shared/test/get-setting.js
@@ -11,6 +11,9 @@ describe( 'getSetting', () => {
 	it( 'returns expected value for existing setting', () => {
 		expect( getSetting( 'adminUrl', 'not this' ) ).toEqual( ADMIN_URL );
 	} );
+	it( "returns expected value for existing setting even if it's a falsy value", () => {
+		expect( getSetting( 'currentUserIsAdmin', true ) ).toBe( false );
+	} );
 	it( 'filters value via provided filter callback', () => {
 		expect( getSetting( 'some value', 'default', () => 42 ) ).toBe( 42 );
 	} );
