7f43bad21d1019489ece0720474031b8439b8f05
roykho <roykho77@gmail.com>
2021-05-20 13:16:16 -0700

    Clean up code and update version

--- src/Internal/RestockRefundedItemsAdjuster.php
+++ src/Internal/RestockRefundedItemsAdjuster.php
@@ -31,7 +31,7 @@ class RestockRefundedItemsAdjuster {
 	}
 
 	/**
-	 * Initializes the restock refunded items meta for order version less than 5.4.
+	 * Initializes the restock refunded items meta for order version less than 5.5.
 	 *
 	 * @see https://github.com/woocommerce/woocommerce/issues/29502
 	 *
@@ -42,13 +42,15 @@ class RestockRefundedItemsAdjuster {
 		$order         = wc_get_order( $order_id );
 		$order_version = $order->get_version();
 
-		if ( version_compare( $order_version, '5.4', '>=' ) ) {
+		if ( version_compare( $order_version, '5.5', '>=' ) ) {
 			return;
 		}
-       // If there are no refund lines, then this migration isn't necessary because restock related meta's wouldn't be set.
+
+		// If there are no refund lines, then this migration isn't necessary because restock related meta's wouldn't be set.
 		if ( 0 === count( $order->get_refunds() ) ) {
 			return;
 		}
+
 		if ( isset( $items['order_item_id'] ) ) {
 			foreach ( $items['order_item_id'] as $item_id ) {
 				$item = $this->order_factory::get_order_item( absint( $item_id ) );
@@ -60,10 +62,12 @@ class RestockRefundedItemsAdjuster {
 				if ( 'line_item' !== $item->get_type() ) {
 					continue;
 				}
-    // there could be code paths in custom code which don't update version number but still update the items.
-	if ( '' !== $item->get_meta( '_restock_refunded_items', true ) ) {
+
+				// There could be code paths in custom code which don't update version number but still update the items.
+				if ( '' !== $item->get_meta( '_restock_refunded_items', true ) ) {
 					continue;
-	}
+				}
+
 				$refunded_item_quantity = abs( $order->get_qty_refunded_for_item( $item->get_id() ) );
 				$item->add_meta_data( '_restock_refunded_items', $refunded_item_quantity, false );
 				$item->save();
