072af834b9e1480e94368a9e2870f7333e3b0312
Rodel Calasagsag <rodel.calasagsag@automattic.com>
2021-05-21 19:27:50 +0800

    Clear browser cache and cookies after each test. Add merchant logins to relevant onboarding & setup tests

--- tests/e2e/config/jest.setup.js
+++ tests/e2e/config/jest.setup.js
@@ -25,7 +25,7 @@ async function trashExistingPosts() {
 	const posts = response.data;
 
 	// Delete each post
-	for(const post of posts){
+	for (const post of posts) {
 		await client.delete(`${wpPostsEndpoint}/${post.id}`);
 	}
 
@@ -34,11 +34,19 @@ async function trashExistingPosts() {
 // Before every test suite run, delete all content created by the test. This ensures
 // other posts/comments/etc. aren't dirtying tests and tests don't depend on
 // each other's side-effects.
-beforeAll( async () => {
+beforeAll(async () => {
 	await trashExistingPosts();
 	await withRestApi.deleteAllProducts();
 	await withRestApi.deleteAllCoupons();
 	await page.goto(WP_ADMIN_LOGIN);
 	await clearLocalStorage();
-	await setBrowserViewport( 'large' );
-} );
+	await setBrowserViewport('large');
+});
+
+// Clear browser cookies and cache using DevTools.
+// This is to ensure that each test ends with no user logged in.
+afterAll(async () => {
+	const client = await page.target().createCDPSession();
+	await client.send('Network.clearBrowserCookies');
+	await client.send('Network.clearBrowserCache');
+});
