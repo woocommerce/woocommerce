woocommerce/woocommerce

commit ef572483d8c71732018d11d00a03b103c9167fa1

Update deprecated jQuery functions for jquery.payment.js closes #28284

assets/js/jquery-payment/jquery.payment.js
@@ -554,8 +554,8 @@ jQuery( function( $ ) {
     if (!(month && year)) {
       return false;
     }
-    month = $.trim(month);
-    year = $.trim(year);
+    month = 'string' === typeof month ? month.trim() : '';
+    year = 'string' === typeof year ? year.trim() : '';
     if (!/^\d+$/.test(month)) {
       return false;
     }
@@ -584,7 +584,7 @@ jQuery( function( $ ) {
 
   $.payment.validateCardCVC = function(cvc, type) {
     var card, _ref;
-    cvc = $.trim(cvc);
+    cvc = 'string' === typeof cvc ? cvc.trim() : '';
     if (!/^\d+$/.test(cvc)) {
       return false;
     }

commit 7cc501d9299dadd03d9e80f8634e67a929349947

Fix bug introduced in ef57248
This fixes a bug introduced in ef57248 wherein Numbers are no longer supported by the `$.payment.validateCardExpiry()` and `$.payment.validateCardCVC()` functions. This is important because `$.payment.cardExpiryVal()` returns Numbers. If you then pass the results of that function into the validation function, it will fail incorrectly.

This commit uses the technique used by the last version of the `$.trim()` function. This will fix all backward-compatibility breaks.

See: https://github.com/jquery/jquery/blob/3.6.0/src/deprecated.js#L82-L86

assets/js/jquery-payment/jquery.payment.js
@@ -2,7 +2,7 @@ jQuery( function( $ ) {
 
 // Generated by CoffeeScript 1.7.1
 (function() {
-  var cardFromNumber, cardFromType, cards, defaultFormat, formatBackCardNumber, formatBackExpiry, formatCardNumber, formatExpiry, formatForwardExpiry, formatForwardSlashAndSpace, hasTextSelected, luhnCheck, reFormatCVC, reFormatCardNumber, reFormatExpiry, reFormatNumeric, replaceFullWidthChars, restrictCVC, restrictCardNumber, restrictExpiry, restrictNumeric, safeVal, setCardType,
+  var cardFromNumber, cardFromType, cards, defaultFormat, formatBackCardNumber, formatBackExpiry, formatCardNumber, formatExpiry, formatForwardExpiry, formatForwardSlashAndSpace, hasTextSelected, luhnCheck, reFormatCVC, reFormatCardNumber, reFormatExpiry, reFormatNumeric, replaceFullWidthChars, trim, restrictCVC, restrictCardNumber, restrictExpiry, restrictNumeric, safeVal, setCardType,
     __slice = [].slice,
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
@@ -197,6 +197,10 @@ jQuery( function( $ ) {
       value += chr;
     }
     return value;
+  };
+
+  trim = function(str) {
+    return str == null ? '' : (str + '').replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
   };
 
   reFormatNumeric = function(e) {
@@ -554,8 +558,8 @@ jQuery( function( $ ) {
     if (!(month && year)) {
       return false;
     }
-    month = 'string' === typeof month ? month.trim() : '';
-    year = 'string' === typeof year ? year.trim() : '';
+    month = trim(month);
+    year = trim(year);
     if (!/^\d+$/.test(month)) {
       return false;
     }
@@ -584,7 +588,7 @@ jQuery( function( $ ) {
 
   $.payment.validateCardCVC = function(cvc, type) {
     var card, _ref;
-    cvc = 'string' === typeof cvc ? cvc.trim() : '';
+    cvc = trim(cvc);
     if (!/^\d+$/.test(cvc)) {
       return false;
     }