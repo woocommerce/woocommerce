6f27ec40d6a4b751555b383bdaa621c4d300ed14
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-06-15 11:47:11 +0200

    Add 5.3.1 testing instructions (https://github.com/woocommerce/woocommerce-blocks/pull/4351)
    * Update changelog and compatibility version for 5.3.1
    
    * Add testing instructions for WC Blocks 5.3.1
    
    * Update ZIP file for WC Blocks 5.3.1 testing

--- plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
+++ plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
@@ -9,8 +9,8 @@
  * Text Domain:  woo-gutenberg-products-block
  * Requires at least: 5.5
  * Requires PHP: 7.0
- * WC requires at least: 4.9
- * WC tested up to: 5.3
+ * WC requires at least: 5.2
+ * WC tested up to: 5.5
  *
  * @package WooCommerce\Blocks
  * @internal This file is only used when running as a feature plugin.
