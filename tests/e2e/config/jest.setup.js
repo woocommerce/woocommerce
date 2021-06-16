89d166e8b4c54c54bc0b39b95af47570a18068a6
Greg <greg.bell@automattic.com>
2021-06-16 15:28:04 -0600

    Added logic to draft and publish the ready page on each run

--- tests/e2e/config/jest.setup.js
+++ tests/e2e/config/jest.setup.js
@@ -32,13 +32,43 @@ async function trashExistingPosts() {
 	for (const post of posts) {
 		await client.delete(`${wpPostsEndpoint}/${post.id}`);
 	}
+}
 
+/**
+ * Uses the WordPress API to update the Ready page's status.
+ *
+ * @param {string} status | Status to update the page to. One of: publish, future, draft, pending, private
+ */
+async function updateReadyPageStatus( status ) {
+	const apiUrl = config.get('url');
+	const wpPagesEndpoint = '/wp/v2/pages';
+	const adminUsername = config.get('users.admin.username');
+	const adminPassword = config.get('users.admin.password');
+	const client = HTTPClientFactory.build(apiUrl)
+		.withBasicAuth(adminUsername, adminPassword)
+		.create();
+
+	// As the default status filter in the API is `publish`, we need to
+	// filter based on the supplied status otherwise no results are returned.
+	let statusFilter = 'publish';
+	if ( 'publish' === status ) {
+		// The page will be in a draft state, so we need to filter on that status
+		statusFilter = 'draft';
+	}
+	const getPostsResponse = await client.get(`${wpPagesEndpoint}?search=ready&status=${statusFilter}`);
+	const pageId = getPostsResponse.data[0].id;
+
+	// Update the page to the new status
+	await client.post(`${wpPagesEndpoint}/${pageId}`, { 'status': status });
 }
 
 // Before every test suite run, delete all content created by the test. This ensures
 // other posts/comments/etc. aren't dirtying tests and tests don't depend on
 // each other's side-effects.
 beforeAll(async () => {
+	// Update the ready page to prevent concurrent test runs
+	await updateReadyPageStatus('draft');
+
 	await trashExistingPosts();
 	await withRestApi.deleteAllProducts();
 	await withRestApi.deleteAllCoupons();
@@ -50,6 +80,9 @@ beforeAll(async () => {
 // Clear browser cookies and cache using DevTools.
 // This is to ensure that each test ends with no user logged in.
 afterAll(async () => {
+	// Reset the ready page to published to allow future test runs
+	await updateReadyPageStatus('publish');
+
 	const client = await page.target().createCDPSession();
 	await client.send('Network.clearBrowserCookies');
 	await client.send('Network.clearBrowserCache');
