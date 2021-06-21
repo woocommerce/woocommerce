87499992f023398b1b646b7c7ada84c4537fb776
OGINO Mizuho <webmaster@contempo.jp>
2021-06-21 23:31:33 +0900

    Update class-wc-admin-post-types.php

--- includes/admin/class-wc-admin-post-types.php
+++ includes/admin/class-wc-admin-post-types.php
@@ -383,7 +383,7 @@ class WC_Admin_Post_Types {
 				$product->set_shipping_class_id( $shipping_class_id );
 			}
 		}
-		
+
 		if ( ! empty( $request_data['_tax_class'] ) ) {
 			$tax_class = sanitize_title( wp_unslash( $request_data['_tax_class'] ) );
 			if ( 'standard' === $tax_class ) {
