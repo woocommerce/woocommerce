5ba5722a49d729b05f959e6341e5ab8a0089f2fd
Mike Jolley <mike.jolley@me.com>
2021-06-23 11:51:32 +0100

    Allow HTML in the All Products Block Product Titles (https://github.com/woocommerce/woocommerce-blocks/pull/4363)
    * Render titles as HTML in ProductName component and use Typescript
    
    * Update snaps

---
+++ plugins/woocommerce-blocks/assets/js/base/components/product-name/index.tsx
@@ -0,0 +1,48 @@
+/**
+ * External dependencies
+ */
+import { decodeEntities } from '@wordpress/html-entities';
+import classnames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+/**
+ * Render the Product name.
+ *
+ * The store API runs titles through `wp_kses_post()` which removes dangerous HTML tags, so using it inside `dangerouslySetInnerHTML` is considered safe.
+ */
+export default ( {
+	className = '',
+	disabled = false,
+	name,
+	permalink = '',
+	...props
+}: {
+	className: string;
+	disabled: boolean;
+	name: string;
+	permalink: string;
+} ): JSX.Element => {
+	const classes = classnames( 'wc-block-components-product-name', className );
+	return disabled ? (
+		<span
+			className={ classes }
+			{ ...props }
+			dangerouslySetInnerHTML={ {
+				__html: decodeEntities( name ),
+			} }
+		/>
+	) : (
+		<a
+			className={ classes }
+			href={ permalink }
+			{ ...props }
+			dangerouslySetInnerHTML={ {
+				__html: decodeEntities( name ),
+			} }
+		/>
+	);
+};
