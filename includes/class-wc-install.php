8d9dc9e6213c4c272ee54b0ed21195017263102e
roykho <roykho77@gmail.com>
2021-06-29 09:25:59 -0700

    Add refund and return page for creation

--- includes/class-wc-install.php
+++ includes/class-wc-install.php
@@ -559,25 +559,30 @@ class WC_Install {
 		$pages = apply_filters(
 			'woocommerce_create_pages',
 			array(
-				'shop'      => array(
+				'shop'          => array(
 					'name'    => _x( 'shop', 'Page slug', 'woocommerce' ),
 					'title'   => _x( 'Shop', 'Page title', 'woocommerce' ),
 					'content' => '',
 				),
-				'cart'      => array(
+				'cart'          => array(
 					'name'    => _x( 'cart', 'Page slug', 'woocommerce' ),
 					'title'   => _x( 'Cart', 'Page title', 'woocommerce' ),
 					'content' => '<!-- wp:shortcode -->[' . apply_filters( 'woocommerce_cart_shortcode_tag', 'woocommerce_cart' ) . ']<!-- /wp:shortcode -->',
 				),
-				'checkout'  => array(
+				'checkout'      => array(
 					'name'    => _x( 'checkout', 'Page slug', 'woocommerce' ),
 					'title'   => _x( 'Checkout', 'Page title', 'woocommerce' ),
 					'content' => '<!-- wp:shortcode -->[' . apply_filters( 'woocommerce_checkout_shortcode_tag', 'woocommerce_checkout' ) . ']<!-- /wp:shortcode -->',
 				),
-				'myaccount' => array(
+				'myaccount'     => array(
 					'name'    => _x( 'my-account', 'Page slug', 'woocommerce' ),
 					'title'   => _x( 'My account', 'Page title', 'woocommerce' ),
 					'content' => '<!-- wp:shortcode -->[' . apply_filters( 'woocommerce_my_account_shortcode_tag', 'woocommerce_my_account' ) . ']<!-- /wp:shortcode -->',
+				),
+				'refund_return' => array(
+					'name'    => _x( 'refund_return', 'Page slug', 'woocommerce' ),
+					'title'   => _x( 'Refund and Returns Policy', 'Page title', 'woocommerce' ),
+					'content' => self::get_refunds_return_policy_page_content(),
 				),
 			)
 		);
@@ -1647,6 +1652,157 @@ CREATE TABLE {$wpdb->prefix}wc_reserved_stock (
 		if ( class_exists( 'WC_Gateway_Paypal' ) ) {
 			( new WC_Gateway_Paypal() )->should_load();
 		}
+	}
+
+	/**
+	 * Gets the content of the sample refunds and return policy page.
+	 *
+	 * @since 5.6.0
+	 * @return HTML The content for the page
+	 */
+	private static function get_refunds_return_policy_page_content() {
+		return <<<EOT
+<!-- wp:paragraph -->
+<p><b>This is a sample page.</b></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<h3>Overview</h3>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Our refund and returns policy lasts 30 days. If 30 days have passed since your purchase, we can’t offer you a full refund or exchange.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>To be eligible for a return, your item must be unused and in the same condition that you received it. It must also be in the original packaging.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Several types of goods are exempt from being returned. Perishable goods such as food, flowers, newspapers or magazines cannot be returned. We also do not accept products that are intimate or sanitary goods, hazardous materials, or flammable liquids or gases.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Additional non-returnable items:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<li>Gift cards</li>
+<li>Downloadable software products</li>
+<li>Some health and personal care items</li>
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>To complete your return, we require a receipt or proof of purchase.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Please do not send your purchase back to the manufacturer.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>There are certain situations where only partial refunds are granted:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul>
+<li>Book with obvious signs of use</li>
+<li>CD, DVD, VHS tape, software, video game, cassette tape, or vinyl record that has been opened.</li>
+<li>Any item not in its original condition, is damaged or missing parts for reasons not due to our error.</li>
+<li>Any item that is returned more than 30 days after delivery</li>
+</ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<h2>Refunds</h2>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Once your return is received and inspected, we will send you an email to notify you that we have received your returned item. We will also notify you of the approval or rejection of your refund.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>If you are approved, then your refund will be processed, and a credit will automatically be applied to your credit card or original method of payment, within a certain amount of days.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<b>Late or missing refunds</b>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>If you haven’t received a refund yet, first check your bank account again.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Then contact your credit card company, it may take some time before your refund is officially posted.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Next contact your bank. There is often some processing time before a refund is posted.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>If you’ve done all of this and you still have not received your refund yet, please contact us at {email address}.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<b>Sale items</b>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Only regular priced items may be refunded. Sale items cannot be refunded.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<h2>Exchanges</h2>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>We only replace items if they are defective or damaged. If you need to exchange it for the same item, send us an email at {email address} and send your item to: {physical address}.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<h2>Gifts</h2>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>If the item was marked as a gift when purchased and shipped directly to you, you’ll receive a gift credit for the value of your return. Once the returned item is received, a gift certificate will be mailed to you.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>If the item wasn’t marked as a gift when purchased, or the gift giver had the order shipped to themselves to give to you later, we will send a refund to the gift giver and they will find out about your return.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<h2>Shipping returns</h2>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>To return your product, you should mail your product to: {physical address}.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>You will be responsible for paying for your own shipping costs for returning your item. Shipping costs are non-refundable. If you receive a refund, the cost of return shipping will be deducted from your refund.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Depending on where you live, the time it may take for your exchanged product to reach you may vary.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>If you are returning more expensive items, you may consider using a trackable shipping service or purchasing shipping insurance. We don’t guarantee that we will receive your returned item.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<h2>Need help?</h2>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Contact us at {email} for questions related to refunds and returns.</p>
+<!-- /wp:paragraph -->
+EOT;
 	}
 }
 
