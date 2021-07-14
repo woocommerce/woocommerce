aa939f6081d8513701e5156047b1d3f5ec1ce4df
roykho <roykho77@gmail.com>
2021-07-14 08:45:01 -0700

    Use single quotes to delineate mysql values instead of double quotes closes #29969

--- includes/data-stores/class-wc-product-data-store-cpt.php
+++ includes/data-stores/class-wc-product-data-store-cpt.php
@@ -1118,7 +1118,7 @@ class WC_Product_Data_Store_CPT extends WC_Data_Store_WP implements WC_Object_Da
 			$product->get_id()
 		);
 
-		$query .= ' AND postmeta.meta_key IN ( "' . implode( '","', array_map( 'esc_sql', $meta_attribute_names ) ) . '" )';
+		$query .= " AND postmeta.meta_key IN ( '" . implode( "','", array_map( 'esc_sql', $meta_attribute_names ) ) . "' )";
 
 		$query .= ' ORDER BY posts.menu_order ASC, postmeta.post_id ASC;';
 
