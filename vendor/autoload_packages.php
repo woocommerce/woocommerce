29b9a82af0e50da8f7cc9a318e99b25402c3962b
Jonathan Sadowski <sadowski@automattic.com>
2021-07-16 13:03:59 -0500

    Add the package files for Blocks 2.7.4

--- vendor/autoload_packages.php
+++ vendor/autoload_packages.php
@@ -179,7 +179,7 @@ if ( ! function_exists( __NAMESPACE__ . '\autoloader' ) ) {
 /**
  * Prepare all the classes for autoloading.
  */
-function enqueue_packages_59b2271d25a961ec5100b6f811b89e89() {
+function enqueue_packages_4160dd3a69cfd1276fbc0bf8fb83ac38() {
 	$class_map = require_once dirname( __FILE__ ) . '/composer/autoload_classmap_package.php';
 	foreach ( $class_map as $class_name => $class_info ) {
 		enqueue_package_class( $class_name, $class_info['version'], $class_info['path'] );
@@ -203,4 +203,4 @@ function enqueue_packages_59b2271d25a961ec5100b6f811b89e89() {
 		file_loader(); // Either WordPress is not loaded or plugin is doing it wrong. Either way we'll load the files so nothing breaks.
 	}
 }
-enqueue_packages_59b2271d25a961ec5100b6f811b89e89();
+enqueue_packages_4160dd3a69cfd1276fbc0bf8fb83ac38();
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit59af27a7bf321fa3c5eec815bc33891c
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit59af27a7bf321fa3c5eec815bc33891c::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire59af27a7bf321fa3c5eec815bc33891c($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire59af27a7bf321fa3c5eec815bc33891c($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
