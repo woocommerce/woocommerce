87a96dd473e176b617a895818a1af9a5d086d335
barryhughes <3594411+barryhughes@users.noreply.github.com>
2021-07-19 15:43:31 -0700

    Revise "Redirect only" deprecation message.

--- includes/admin/views/html-notice-redirect-only-download.php
+++ includes/admin/views/html-notice-redirect-only-download.php
@@ -15,7 +15,7 @@ defined( 'ABSPATH' ) || exit;
 		echo wp_kses_post(
 			sprintf(
 				/* translators: %s: Link to settings page. */
-				__( 'Your store is configured to serve digital products using "Redirect only" method. This method is deprecated, <a href="%s">please switch to  a different method instead.</a><br><em>If you use a remote server for downloadable files (such as Google Drive, Dropbox, Amazon S3), the right method will automatically be used, so select any of the other options to make this notice go away.</em>', 'woocommerce' ),
+				__( 'Your store is configured to serve digital products using "Redirect only" method. This method is deprecated, <a href="%s">please switch to a different method instead.</a><br><em>If you use a remote server for downloadable files (such as Google Drive, Dropbox, Amazon S3), you may optionally wish to "allow using redirects as a last resort". Enabling that and/or selecting any of the other options will make this notice go away.</em>', 'woocommerce' ),
 				add_query_arg(
 					array(
 						'page'    => 'wc-settings',
