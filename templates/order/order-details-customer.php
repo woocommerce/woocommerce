442f357c291c9f53785e491f4c10ae624c72f20c
Claudio Sanches <contato@claudiosanches.com>
2021-07-20 20:58:19 -0300

    Bump templates for 5.6 release

--- templates/order/order-details-customer.php
+++ templates/order/order-details-customer.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.4.4
+ * @version 5.6.0
  */
 
 defined( 'ABSPATH' ) || exit;
