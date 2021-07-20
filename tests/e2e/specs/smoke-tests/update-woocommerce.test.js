f31693aaec5a1c1e54e9eae4f580a44d622fb5c0
Greg <greg.bell@automattic.com>
2021-07-20 15:26:51 -0600

    Add in env flag

--- tests/e2e/specs/smoke-tests/update-woocommerce.test.js
+++ tests/e2e/specs/smoke-tests/update-woocommerce.test.js
@@ -1,37 +1,37 @@
 /**
  * Internal dependencies
  */
- const { merchant } = require( '@woocommerce/e2e-utils' );
+const { merchant, utils } = require( '@woocommerce/e2e-utils' );
 
- const { getRemotePluginZip } = require( '@woocommerce/e2e-environment' );
+const { getRemotePluginZip } = require( '@woocommerce/e2e-environment' );
 
- /**
-  * External dependencies
-  */
- const {
-	  it,
-	  describe,
-	  beforeAll,
-  } = require( '@jest/globals' );
+/**
+ * External dependencies
+ */
+const {
+	it,
+	beforeAll,
+} = require( '@jest/globals' );
 
+const { UPDATE_WC } = process.env;
 
- const nightlyZip = 'https://github.com/woocommerce/woocommerce/releases/download/nightly/woocommerce-trunk-nightly.zip';
- const pluginName = 'WooCommerce';
+const nightlyZip = 'https://github.com/woocommerce/woocommerce/releases/download/nightly/woocommerce-trunk-nightly.zip';
+const pluginName = 'WooCommerce';
 
- let pluginPath;
+let pluginPath;
 
- describe( 'WooCommerce plugin can be uploaded and activated', () => {
-	 beforeAll( async () => {
-		 pluginPath = await getRemotePluginZip( nightlyZip );
-		 await merchant.login();
-	 });
+utils.describeIf( UPDATE_WC )( 'WooCommerce plugin can be uploaded and activated', () => {
+	beforeAll( async () => {
+		pluginPath = await getRemotePluginZip( nightlyZip );
+		await merchant.login();
+	});
 
-	 afterAll( async () => {
-		 await merchant.logout();
-	 });
+	afterAll( async () => {
+		await merchant.logout();
+	});
 
-	 it( 'can upload and activate the WooCommerce plugin', async () => {
-		 await merchant.uploadAndActivatePlugin( pluginPath, pluginName );
-	 });
+	it( 'can upload and activate the WooCommerce plugin', async () => {
+		await merchant.uploadAndActivatePlugin( pluginPath, pluginName );
+	});
 
- });
+});
