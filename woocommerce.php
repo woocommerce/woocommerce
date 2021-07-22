0d5ce9b793270224553755bf7c5f07d35881a803
vedanshujain <vedanshu.jain.2012@gmail.com>
2021-07-22 16:18:13 +0530

    Version bump and changelog updates to 5.5.2

--- woocommerce.php
+++ woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
- * Version: 5.5.1
+ * Version: 5.5.2
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
