2c32b5756fa67a578299fbb36baecb1d1afb4be1
Thomas Roberts <5656702+opr@users.noreply.github.com>
2021-07-26 09:13:54 +0100

    Update `snackbarNotices` filter to `snackbarNoticeVisibility` (https://github.com/woocommerce/woocommerce-blocks/pull/4417)
    * Change snackbarNotices filter to snackbarNoticeVisibility
    
    * Add deprecation notice to snackbarNotices
    
    * Rename noticesToDisplay to visibleNotices
    
    * Update link in deprecated message

--- plugins/woocommerce-blocks/packages/checkout/registry/index.ts
+++ plugins/woocommerce-blocks/packages/checkout/registry/index.ts
@@ -36,6 +36,20 @@ export const __experimentalRegisterCheckoutFilters = (
 	filters: Record< string, CheckoutFilterFunction >
 ): void => {
 	/**
+	 * Let developers know snackbarNotices is no longer available as a filter.
+	 *
+	 * See: https://github.com/woocommerce/woocommerce-gutenberg-products-block/pull/4417
+	 */
+	if ( Object.keys( filters ).includes( 'couponName' ) ) {
+		deprecated( 'snackbarNotices', {
+			alternative: 'snackbarNoticeVisibility',
+			plugin: 'WooCommerce Blocks',
+			link:
+				'https://github.com/woocommerce/woocommerce-gutenberg-products-block/pull/4417',
+		} );
+	}
+
+	/**
 	 * Let the user know couponName is no longer available as a filter.
 	 *
 	 * See https://github.com/woocommerce/woocommerce-gutenberg-products-block/pull/4312
