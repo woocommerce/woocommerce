03a6d890e537e484764a7b2e554dee8c2ed2317f
Nestor Soriano <konamiman@konamiman.com>
2021-08-02 09:33:18 +0200

    Fix: 'woocommerce_email_settings' filter being triggered twice.

--- includes/admin/settings/class-wc-settings-emails.php
+++ includes/admin/settings/class-wc-settings-emails.php
@@ -51,8 +51,7 @@ class WC_Settings_Emails extends WC_Settings_Page {
 			'https://wordpress.org/plugins/wp-mail-logging/',
 			'https://docs.woocommerce.com/document/email-faq'
 		);
-		$settings = apply_filters(
-			'woocommerce_email_settings',
+		$settings =
 			array(
 				array(
 					'title' => __( 'Email notifications', 'woocommerce' ),
@@ -217,8 +216,7 @@ class WC_Settings_Emails extends WC_Settings_Page {
 					'type' => 'sectionend',
 					'id'   => 'email_merchant_notes',
 				),
-			)
-		);
+			);
 
 		return apply_filters( 'woocommerce_email_settings', $settings );
 	}
