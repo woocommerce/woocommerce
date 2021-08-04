d7c2ab5193a0140742ab2ae51ba03b53e63148f4
Tam Mullen <tammullen@outlook.com>
2021-08-04 10:56:09 +0100

    Updated k6 Merchant smoke test scenario

--- tests/performance/tests/simple-all-merchant-requests.js
+++ tests/performance/tests/simple-all-merchant-requests.js
@@ -3,6 +3,7 @@ import { products } from '../requests/merchant/products.js';
 import { addProduct } from '../requests/merchant/add-product.js';
 import { orders } from '../requests/merchant/orders.js';
 import { homeWCAdmin } from '../requests/merchant/home-wc-admin.js';
+import { myAccountMerchantLogin } from '../requests/merchant/my-account-merchant.js';
 
 export let options = {
     scenarios: {
@@ -16,6 +17,7 @@ export let options = {
     },
 };
 
+// Use myAccountMerchantLogin() instead of wpLogin() if having issues with login.
 export function allMerchantFlow() {
     wpLogin();
     homeWCAdmin();
