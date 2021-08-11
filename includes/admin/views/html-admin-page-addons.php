af7c5a4e1d4f37cd531a1deaf87aa1809d0c375a
And Finally <andfinally@users.noreply.github.com>
2021-08-11 17:42:11 +0100

    Simplifying styling of notices on Marketplace and My Subscriptions pages.
    - Added `.wp-header-end` div on Marketplace page, so WordPress will insert `admin_notices` there.
    - Added top margin on notice on My Subscriptions page to keep it clear of Help tab.

--- includes/admin/views/html-admin-page-addons.php
+++ includes/admin/views/html-admin-page-addons.php
@@ -34,6 +34,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</form>
 	</div>
 
+	<div class="wp-header-end"></div>
+
 	<div class="marketplace-content-wrapper">
 		<div class="marketplace-content">
 			<?php if ( isset( $_GET['search'] ) ) : // phpcs:ignore WordPress.Security.NonceVerification.Recommended ?>
