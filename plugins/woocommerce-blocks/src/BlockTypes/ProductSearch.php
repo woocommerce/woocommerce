a7d7ea32bb400edc467543751c1f6921e1e6688a
Mike Jolley <mike.jolley@me.com>
2021-08-12 17:39:38 +0100

    Fix unfiltered html usage of blocks (https://github.com/woocommerce/woocommerce-blocks/pull/4551)
    * SSR search
    
    * deprecation handling for search
    
    * Update snaps
    
    * Fix duplicate class and align class property

--- plugins/woocommerce-blocks/src/BlockTypes/ProductSearch.php
+++ plugins/woocommerce-blocks/src/BlockTypes/ProductSearch.php
@@ -31,6 +31,19 @@ class ProductSearch extends AbstractBlock {
 	 * @return string Rendered block type output.
 	 */
 	protected function render( $attributes, $content ) {
+		static $instance_id = 0;
+
+		$attributes = wp_parse_args(
+			$attributes,
+			array(
+				'hasLabel'    => true,
+				'align'       => '',
+				'className'   => '',
+				'label'       => __( 'Search', 'woo-gutenberg-products-block' ),
+				'placeholder' => __( 'Search products…', 'woo-gutenberg-products-block' ),
+			)
+		);
+
 		/**
 		 * Product Search event.
 		 *
@@ -57,6 +70,58 @@ class ProductSearch extends AbstractBlock {
 			",
 			'after'
 		);
-		return $content;
+
+		$input_id           = 'wp-block-search__input-' . ( ++$instance_id );
+		$wrapper_attributes = get_block_wrapper_attributes(
+			array(
+				'class' => implode(
+					' ',
+					array_filter(
+						[
+							'wc-block-product-search',
+							$attributes['align'] ? 'align' . $attributes['align'] : '',
+						]
+					)
+				),
+			)
+		);
+
+		$label_markup = $attributes['hasLabel'] ? sprintf(
+			'<label for="%s" class="wc-block-product-search__label">%s</label>',
+			esc_attr( $input_id ),
+			esc_html( $attributes['label'] )
+		) : sprintf(
+			'<label for="%s" class="wc-block-product-search__label screen-reader-text">%s</label>',
+			esc_attr( $input_id ),
+			esc_html( $attributes['label'] )
+		);
+
+		$input_markup  = sprintf(
+			'<input type="search" id="%s" className="wc-block-product-search__field" placeholder="%s" name="s" />',
+			esc_attr( $input_id ),
+			esc_attr( $attributes['placeholder'] )
+		);
+		$button_markup = sprintf(
+			'<button type="submit" className="wc-block-product-search__button" aria-label="%s">
+				<svg aria-hidden="true" role="img" focusable="false" className="dashicon dashicons-arrow-right-alt2" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+					<path d="M6 15l5-5-5-5 1-2 7 7-7 7z" />
+				</svg>
+			</button>',
+			esc_attr__( 'Search', 'woo-gutenberg-products-block' )
+		);
+
+		$field_markup = '
+			<div className="wc-block-product-search__fields">
+				' . $input_markup . $button_markup . '
+				<input type="hidden" name="post_type" value="product" />
+			</div>
+		';
+
+		return sprintf(
+			'<div %s><form role="search" method="get" action="%s">%s</form></div>',
+			$wrapper_attributes,
+			esc_url( home_url( '/' ) ),
+			$label_markup . $field_markup
+		);
 	}
 }
