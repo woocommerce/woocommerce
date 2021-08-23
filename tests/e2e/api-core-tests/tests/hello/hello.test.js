f5434bec8f6a9dba0e8d4fdb15c11065558f1344
Greg <greg.bell@automattic.com>
2021-08-23 14:51:33 -0600

    Minor spacing adjustments, updated readme, added comments

--- tests/e2e/api-core-tests/tests/hello/hello.test.js
+++ tests/e2e/api-core-tests/tests/hello/hello.test.js
@@ -10,12 +10,12 @@ describe('Test API connectivity', () => {
 
 	it('can access a non-authenticated endpoint', async () => {
 		const result = await getRequest( '' );
-		expect(result.statusCode).toEqual(200);
+		expect( result.statusCode ).toEqual( 200 );
 	});
 
 	it('can access an authenticated endpoint', async () => {
 		const result = await getRequest( 'system_status' );
-		expect(result.statusCode).toEqual(200);
+		expect( result.statusCode ).toEqual( 200 );
 	});
 
 });
