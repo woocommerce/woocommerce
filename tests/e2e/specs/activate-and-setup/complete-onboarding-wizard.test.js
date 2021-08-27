d129a980d922af2b47c4217b4168b5138ade277b
Ron Rennick <ron@ronandandrea.com>
2021-08-27 11:12:13 -0300

    reset onboarding on retest

--- tests/e2e/specs/activate-and-setup/complete-onboarding-wizard.test.js
+++ tests/e2e/specs/activate-and-setup/complete-onboarding-wizard.test.js
@@ -2,6 +2,15 @@ const {
 	testAdminOnboardingWizard,
 	testSelectiveBundleWCPay,
 } = require( '@woocommerce/admin-e2e-tests' );
+const {
+	withRestApi,
+	IS_RETEST_MODE,
+} = require( '@woocommerce/e2e-utils' );
+
+// Reset onboarding profile when re-running tests on a site
+if ( IS_RETEST_MODE ) {
+	withRestApi.resetOnboarding();
+}
 
 testAdminOnboardingWizard();
 testSelectiveBundleWCPay();
