95e45ff781b6c1656abaa356d6aeace0b171b5c0
Greg <greg.bell@automattic.com>
2021-08-30 12:00:31 -0600

    Added updateSettingOption and updatePaymentGateway to withRestAPI flows

--- tests/e2e/core-tests/specs/shopper/front-end-checkout.test.js
+++ tests/e2e/core-tests/specs/shopper/front-end-checkout.test.js
@@ -6,11 +6,7 @@ const {
 	merchant,
 	withRestApi,
 	createSimpleProduct,
-	setCheckbox,
-	settingsPageSaveChanges,
 	uiUnblocked,
-	verifyCheckboxIsSet,
-	addShippingZoneAndMethod,
 } = require( '@woocommerce/e2e-utils' );
 
 const config = require( 'config' );
@@ -33,46 +29,23 @@ const runCheckoutPageTest = () => {
 			await withRestApi.resetSettingsGroupToDefault('tax');
 
 			// Set free shipping within California
-			await merchant.login();
-			await addShippingZoneAndMethod('Free Shipping CA', 'state:US:CA', ' ', 'free_shipping');
-			// Go to general settings page
-			await merchant.openSettings('general');
+			await withRestApi.addShippingZoneAndMethod('Free Shipping CA', 'state:US:CA', ' ', 'free_shipping');
 
 			// Set base location with state CA.
-			await expect(page).toSelect('select[name="woocommerce_default_country"]', 'United States (US) — California');
-			// Sell to all countries
-			await expect(page).toSelect('#woocommerce_allowed_countries', 'Sell to all countries');
-			// Set currency to USD
-			await expect(page).toSelect('#woocommerce_currency', 'United States (US) dollar ($)');
-			// Tax calculation should have been enabled by another test - no-op
-			// Save
-			await settingsPageSaveChanges();
+			await withRestApi.updateSettingOption( 'general', 'woocommerce_default_country', { value: 'US:CA' } );
 
-			// Verify that settings have been saved
-			await Promise.all([
-				expect(page).toMatchElement('#message', {text: 'Your settings have been saved.'}),
-				expect(page).toMatchElement('select[name="woocommerce_default_country"]', {text: 'United States (US) — California'}),
-				expect(page).toMatchElement('#woocommerce_allowed_countries', {text: 'Sell to all countries'}),
-				expect(page).toMatchElement('#woocommerce_currency', {text: 'United States (US) dollar ($)'}),
-			]);
+			// Sell to all countries
+			await withRestApi.updateSettingOption( 'general', 'woocommerce_allowed_countries', { value: 'all' } );
+
+			// Set currency to USD
+			await withRestApi.updateSettingOption( 'general', 'woocommerce_currency', { value: 'USD' } );
+			// Tax calculation should have been enabled by another test - no-op
 
 			// Enable BACS payment method
-			await merchant.openSettings('checkout', 'bacs');
-			await setCheckbox('#woocommerce_bacs_enabled');
-			await settingsPageSaveChanges();
-
-			// Verify that settings have been saved
-			await verifyCheckboxIsSet('#woocommerce_bacs_enabled');
+			await withRestApi.updatePaymentGateway( 'bacs', { enabled: true } );
 
 			// Enable COD payment method
-			await merchant.openSettings('checkout', 'cod');
-			await setCheckbox('#woocommerce_cod_enabled');
-			await settingsPageSaveChanges();
-
-			// Verify that settings have been saved
-			await verifyCheckboxIsSet('#woocommerce_cod_enabled');
-
-			await merchant.logout();
+			await withRestApi.updatePaymentGateway( 'cod', { enabled: true } );
 		});
 
 		it('should display cart items in order review', async () => {
@@ -160,7 +133,7 @@ const runCheckoutPageTest = () => {
 			customerOrderId = orderReceivedText.split(/(\s+)/)[6].toString();
 		});
 
-		it('store owner can confirm the order was received', async () => {
+		it('merchant can confirm the order was received', async () => {
 			await merchant.login();
 			await merchant.verifyOrder(guestOrderId, simpleProductName, singleProductPrice, 5, fiveProductPrice);
 			await merchant.verifyOrder(customerOrderId, simpleProductName, singleProductPrice, 1, singleProductPrice, true);
