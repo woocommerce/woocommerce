fe8801e3561234e67c50a5c0854248bff632120b
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-09-02 11:44:25 +0200

    Add Drawer component (https://github.com/woocommerce/woocommerce-blocks/pull/4608)
    * Add Drawer component
    
    * Use element dataset instead of a window global
    
    * Rename miniCartOverlay to miniCartDrawerPlaceholder
    
    * Refactor styles so screen overlay covers the entire screen
    
    * Use rem instead of em for the drawer title size to keep it consistent between renders
    
    * Keep focus when replacing the mini cart button
    
    * Match overlay colors from designs
    
    * Use is-loading class like several other blocks
    
    * Add more doc comments
    
    * Don't listen to clicks on Mini Cart button when it's open
    
    * Update Drawer component styles

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/mini-cart/component-frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/mini-cart/component-frontend.tsx
@@ -1,9 +1,11 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, _n, sprintf } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
 import { renderFrontend } from '@woocommerce/base-utils';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
+import Drawer from '@woocommerce/base-components/drawer';
 import {
 	withStoreCartApiHydration,
 	withRestApiHydration,
@@ -13,25 +15,116 @@ import {
  * Internal dependencies
  */
 import CartLineItemsTable from '../cart/full-cart/cart-line-items-table';
+import './style.scss';
 
-const MiniCartContents = () => {
-	const { cartItems, cartIsLoading } = useStoreCart();
+interface MiniCartBlock {
+	isPlaceholderOpen?: boolean;
+}
 
-	if ( cartItems.length === 0 ) {
-		return <>{ __( 'Cart is empty', 'woo-gutenberg-products-block' ) }</>;
-	}
+const MiniCartBlock = ( { isPlaceholderOpen = false } ): JSX.Element => {
+	const { cartItems, cartItemsCount, cartIsLoading } = useStoreCart();
+	const [ isOpen, setIsOpen ] = useState< boolean >( isPlaceholderOpen );
+	// We already rendered the HTML drawer placeholder, so we want to skip the
+	// slide in animation.
+	const [ skipSlideIn, setSkipSlideIn ] = useState< boolean >(
+		isPlaceholderOpen
+	);
+
+	const contents =
+		cartItems.length === 0 ? (
+			<>{ __( 'Cart is empty', 'woo-gutenberg-products-block' ) }</>
+		) : (
+			<div className="is-mobile">
+				<CartLineItemsTable
+					lineItems={ cartItems }
+					isLoading={ cartIsLoading }
+				/>
+			</div>
+		);
 
 	return (
-		<CartLineItemsTable
-			lineItems={ cartItems }
-			isLoading={ cartIsLoading }
-		/>
+		<>
+			<button
+				className="wc-block-mini-cart__button"
+				onClick={ () => {
+					if ( ! isOpen ) {
+						setIsOpen( true );
+						setSkipSlideIn( false );
+					}
+				} }
+			>
+				{ sprintf(
+					/* translators: %d is the count of items in the cart. */
+					_n(
+						'%d item',
+						'%d items',
+						cartItemsCount,
+						'woo-gutenberg-products-block'
+					),
+					cartItemsCount
+				) }
+			</button>
+			<Drawer
+				title={ sprintf(
+					/* translators: %d is the count of items in the cart. */
+					_n(
+						'Your cart (%d item)',
+						'Your cart (%d items)',
+						cartItemsCount,
+						'woo-gutenberg-products-block'
+					),
+					cartItemsCount
+				) }
+				isOpen={ isOpen }
+				onClose={ () => {
+					setIsOpen( false );
+				} }
+				slideIn={ ! skipSlideIn }
+			>
+				{ contents }
+			</Drawer>
+		</>
 	);
 };
 
-renderFrontend( {
-	selector: '.wc-block-mini-cart__contents',
-	Block: withStoreCartApiHydration(
-		withRestApiHydration( MiniCartContents )
-	),
-} );
+const renderMiniCartFrontend = () => {
+	// Check if button is focused. In that case, we want to refocus it after we
+	// replace it with the React equivalent.
+	let focusedMiniCartBlock: HTMLElement | null = null;
+	/* eslint-disable @wordpress/no-global-active-element */
+	if (
+		document.activeElement &&
+		document.activeElement.classList.contains(
+			'wc-block-mini-cart__button'
+		) &&
+		document.activeElement.parentNode instanceof HTMLElement
+	) {
+		focusedMiniCartBlock = document.activeElement.parentNode;
+	}
+	/* eslint-enable @wordpress/no-global-active-element */
+
+	renderFrontend( {
+		selector: '.wc-block-mini-cart',
+		Block: withStoreCartApiHydration(
+			withRestApiHydration( MiniCartBlock )
+		),
+		getProps: ( el: HTMLElement ) => ( {
+			isPlaceholderOpen: el.dataset.isPlaceholderOpen,
+		} ),
+	} );
+
+	// Refocus previously focused button if drawer is not open.
+	if (
+		focusedMiniCartBlock instanceof HTMLElement &&
+		! focusedMiniCartBlock.dataset.isPlaceholderOpen
+	) {
+		const innerButton = focusedMiniCartBlock.querySelector(
+			'.wc-block-mini-cart__button'
+		);
+		if ( innerButton instanceof HTMLElement ) {
+			innerButton.focus();
+		}
+	}
+};
+
+renderMiniCartFrontend();
