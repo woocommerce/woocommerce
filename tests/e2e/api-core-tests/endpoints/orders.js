04fd90c63f5a454001cdb89b88aa2c809ee3c1e4
Greg <greg.bell@automattic.com>
2021-09-07 16:20:44 -0600

    Code review feedback

--- tests/e2e/api-core-tests/endpoints/orders.js
+++ tests/e2e/api-core-tests/endpoints/orders.js
@@ -17,29 +17,29 @@ const ordersApi = {
 		path: 'orders',
 		responseCode: 201,
 		payload: order,
-		order: async ( orderDetails ) => postRequest( 'orders', orderDetails )
+		order: async ( orderDetails ) => postRequest( 'orders', orderDetails ),
 	},
 	retrieve: {
 		name: 'Retrieve an order',
 		method: 'GET',
 		path: 'orders/<id>',
 		responseCode: 200,
-		order: async ( orderId ) => getRequest( `orders/${orderId}` )
+		order: async ( orderId ) => getRequest( `orders/${orderId}` ),
 	},
 	listAll: {
 		name: 'List all orders',
 		method: 'GET',
 		path: 'orders',
 		responseCode: 200,
-		orders: async () => { getRequest( 'orders' ) }
+		orders: async () => { getRequest( 'orders' ) },
 	},
 	update: {
 		name: 'Update an order',
 		method: 'PUT',
 		path: 'orders/<id>',
 		responseCode: 200,
-		payload: order,
-		order: async ( orderId, orderDetails ) => putRequest( `orders/${orderId}`, orderDetails )
+		payload: order.order,
+		order: async ( orderId, orderDetails ) => putRequest( `orders/${orderId}`, orderDetails ),
 	},
 	delete: {
 		name: 'Delete an order',
@@ -49,18 +49,20 @@ const ordersApi = {
 		payload: {
 			force: false
 		},
-		order: async ( orderId, deletePermanently ) => deleteRequest( `orders/${orderId}`, deletePermanently )
+		order: async ( orderId, deletePermanently ) => deleteRequest( `orders/${orderId}`, deletePermanently ),
 	},
 	batch: {
 		name: 'Batch update orders',
 		method: 'POST',
 		path: 'orders/batch',
 		responseCode: 200,
-		payload: shared.getBatchPayloadExample( order ),
-		orders: async ( batchUpdatePayload ) => postRequest( `orders/batch`, batchUpdatePayload )
+		payload: shared.getBatchPayloadExample( order.order ),
+		orders: async ( batchUpdatePayload ) => postRequest( `orders/batch`, batchUpdatePayload ),
 	},
-}
+};
+
+// TODO -- build example payload here? Remove ID etc from the example and just add here?
 
 module.exports = {
 	ordersApi,
-}
+};
