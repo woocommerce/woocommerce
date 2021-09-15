61f31df06b435f994be52ac29109fa4350977555
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-09-15 11:58:00 +0200

    Fix calculation of number of reviews in Reviews by Category block (https://github.com/woocommerce/woocommerce-blocks/pull/4729)
    * Remove unused code
    
    * Fix incorrect number of reviews in Reviews by Category block
    
    * Add error handling around get_term_children()

--- plugins/woocommerce-blocks/assets/js/blocks/reviews/reviews-by-category/edit.js
+++ plugins/woocommerce-blocks/assets/js/blocks/reviews/reviews-by-category/edit.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { __, _n, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/block-editor';
 import {
 	Button,
@@ -10,11 +10,9 @@ import {
 	ToggleControl,
 	withSpokenMessages,
 } from '@wordpress/components';
-import { SearchListItem } from '@woocommerce/components';
 import PropTypes from 'prop-types';
 import ProductCategoryControl from '@woocommerce/editor-components/product-category-control';
 import { Icon, review } from '@woocommerce/icons';
-import classNames from 'classnames';
 
 /**
  * Internal dependencies
@@ -42,39 +40,6 @@ const ReviewsByCategoryEditor = ( {
 } ) => {
 	const { editMode, categoryIds } = attributes;
 
-	const renderCategoryControlItem = ( args ) => {
-		const { item, search, depth = 0 } = args;
-
-		const accessibleName = ! item.breadcrumbs.length
-			? item.name
-			: `${ item.breadcrumbs.join( ', ' ) }, ${ item.name }`;
-
-		return (
-			<SearchListItem
-				className={ classNames(
-					'woocommerce-product-categories__item',
-					'has-count',
-					{
-						'is-searching': search.length > 0,
-						'is-skip-level': depth === 0 && item.parent !== 0,
-					}
-				) }
-				{ ...args }
-				aria-label={ sprintf(
-					/* translators: %1$s is the search term name, %2$d is the number of products returned for search query. */
-					_n(
-						'%1$s, has %2$d product',
-						'%1$s, has %2$d products',
-						item.count,
-						'woo-gutenberg-products-block'
-					),
-					accessibleName,
-					item.count
-				) }
-			/>
-		);
-	};
-
 	const getInspectorControls = () => {
 		return (
 			<InspectorControls key="inspector">
@@ -88,7 +53,6 @@ const ReviewsByCategoryEditor = ( {
 							const ids = value.map( ( { id } ) => id );
 							setAttributes( { categoryIds: ids } );
 						} }
-						renderItem={ renderCategoryControlItem }
 						isCompact={ true }
 						showReviewCount={ true }
 					/>
