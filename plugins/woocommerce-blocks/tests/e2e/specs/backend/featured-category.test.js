9689aa7a3909ab43285666a347f7bab6c22154fb
Niels Lange <info@nielslange.de>
2021-09-20 13:34:06 +0200

    Add e2e tests for Featured Category (https://github.com/woocommerce/woocommerce-blocks/pull/4773)

---
+++ plugins/woocommerce-blocks/tests/e2e/specs/backend/featured-category.test.js
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import { getAllBlocks, switchUserToAdmin } from '@wordpress/e2e-test-utils';
+
+import { visitBlockPage } from '@woocommerce/blocks-test-utils';
+
+import { insertBlockDontWaitForInsertClose } from '../../utils.js';
+
+const block = {
+	name: 'Featured Category',
+	slug: 'woocommerce/featured-category',
+	class: '.wc-block-featured-category',
+};
+
+describe( `${ block.name } Block`, () => {
+	beforeAll( async () => {
+		await switchUserToAdmin();
+		await visitBlockPage( `${ block.name } Block` );
+	} );
+
+	it( 'can be inserted more than once', async () => {
+		await insertBlockDontWaitForInsertClose( block.name );
+		expect( await getAllBlocks() ).toHaveLength( 2 );
+	} );
+
+	it( 'renders without crashing', async () => {
+		await expect( page ).toRenderBlock( block );
+	} );
+} );
