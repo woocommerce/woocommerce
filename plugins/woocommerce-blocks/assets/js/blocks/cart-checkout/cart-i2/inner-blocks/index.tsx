a459360dc19d2831a616cc4c856bd37f665d7a6c
Seghir Nadir <nadir.seghir@gmail.com>
2021-09-21 13:38:56 +0100

    Add Express Checkout block for Cart i2 (https://github.com/woocommerce/woocommerce-blocks/pull/4791)
    * setup basic column blocks
    
    * fix classnames
    
    * move hacks back
    
    * dubplciate columns
    
    * add express payment block
    
    * adjust styles
    
    * fix locking after rebase

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/cart-i2/inner-blocks/index.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/cart-i2/inner-blocks/index.tsx
@@ -5,4 +5,5 @@ import './cart-items-block';
 import './cart-totals-block';
 import './cart-line-items-block';
 import './cart-order-summary-block';
+import './cart-express-payment-block';
 import './proceed-to-checkout-block';
