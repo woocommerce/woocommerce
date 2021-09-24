b7fef9cbbfee3ab72388d768db1ee315d9bea587
Claudio Sanches <contato@claudiosanches.com>
2021-09-23 21:20:37 -0300

    Tag 5.1.2

--- includes/class-wc-install.php
+++ includes/class-wc-install.php
@@ -161,6 +161,11 @@ class WC_Install {
 			'wc_update_500_fix_product_review_count',
 			'wc_update_500_db_version',
 		),
+		'5.1.2' => array(
+			'Automattic\\WooCommerce\\Admin\\ReportCSVExporter::maybe_create_directory',
+			'wc_admin_update_204_delete_report_downloads',
+			'wc_update_512_db_version',
+		),
 	);
 
 	/**
--- includes/class-woocommerce.php
+++ includes/class-woocommerce.php
@@ -23,7 +23,7 @@ final class WooCommerce {
 	 *
 	 * @var string
 	 */
-	public $version = '5.1.1';
+	public $version = '5.1.2';
 
 	/**
 	 * WooCommerce Schema version.
