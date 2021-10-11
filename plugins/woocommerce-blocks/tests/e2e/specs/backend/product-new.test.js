6286f7fbd646e3b6eb2f881b49e2df4ecce21ac5
Niels Lange <info@nielslange.de>
2021-10-11 18:55:38 +0700

    Add e2e tests for Newest Products (https://github.com/woocommerce/woocommerce-blocks/pull/4789)

---
+++ plugins/woocommerce-blocks/tests/e2e/specs/backend/product-new.test.js
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import { getAllBlocks, switchUserToAdmin } from '@wordpress/e2e-test-utils';
+
+import { visitBlockPage } from '@woocommerce/blocks-test-utils';
+
+import { insertBlockDontWaitForInsertClose } from '../../utils.js';
+
+const block = {
+	name: 'Newest Products',
+	slug: 'woocommerce/product-new',
+	class: '.wc-block-product-new',
+};
+
+describe( `${ block.name } Block`, () => {
+	beforeAll( async () => {
+		await switchUserToAdmin();
+		await visitBlockPage( `${ block.name } Block` );
+	} );
+
+	it( 'renders without crashing', async () => {
+		await expect( page ).toRenderBlock( block );
+	} );
+
+	it( 'can be inserted more than once', async () => {
+		await insertBlockDontWaitForInsertClose( block.name );
+		expect( await getAllBlocks() ).toHaveLength( 2 );
+	} );
+} );
