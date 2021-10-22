2fe5531b5762eaac9807d6bc6b108b55e2f1d4b0
roykho <roykho77@gmail.com>
2021-10-22 07:09:26 -0700

    Set correct paths for core tests

--- packages/js/e2e-core-tests/core-tests-root.js
+++ packages/js/e2e-core-tests/core-tests-root.js
@@ -9,18 +9,17 @@
      // uses a local file path (tests/e2e/core-tests).
      let coreTestsPath = false;
      const moduleDir = path.dirname( require.resolve( '@woocommerce/e2e-core-tests' ) );
- 
+
      if ( -1 < moduleDir.indexOf( 'node_modules' ) ) {
         coreTestsPath = moduleDir.split( 'node_modules' )[ 0 ];
-     } else if ( -1 < moduleDir.indexOf( 'tests/e2e/core-tests' ) ) {
-        coreTestsPath = moduleDir.split( 'tests/e2e/core-tests' )[ 0 ];
+     } else if ( -1 < moduleDir.indexOf( 'packages/js/e2e-core-tests' ) ) {
+        coreTestsPath = moduleDir.split( 'packages/js/e2e-core-tests' )[ 0 ];
      }
- 
+
      return {
       appRoot: coreTestsPath,
       packageRoot: moduleDir,
       };
  };
- 
+
  module.exports = getCoreTestsRoot;
- 
