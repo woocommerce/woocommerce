fc9e104055083eca222d6afb73ccc1599b865a55
Paul Sealock <psealock@gmail.com>
2021-10-26 13:44:28 +1300

    reset jest path

--- plugins/woocommerce/tests/e2e/config/jest.config.js
+++ plugins/woocommerce/tests/e2e/config/jest.config.js
@@ -2,9 +2,7 @@ const path = require( 'path' );
 const { useE2EJestConfig, getAppRoot } = require( '@woocommerce/e2e-environment' );
 
 const jestConfig = useE2EJestConfig( {
-	// roots: [ path.resolve( __dirname, '../specs' ) ],
-	// roots: [ path.resolve( getAppRoot(), 'plugins/woocommerce/tests/e2e/specs' ) ],
-	roots: [ '/home/runner/work/woocommerce/woocommerce/package/woocommerce/tests/e2e-tests/specs' ],
+	roots: [ path.resolve( __dirname, '../specs' ) ],
 } );
 
 module.exports = jestConfig;
