82b96262e1cfb99982f78edde4993a0ff7238984
Paul Sealock <psealock@gmail.com>
2021-10-27 12:01:21 +1300

    better logging

--- plugins/woocommerce/tests/e2e/config/jest.config.js
+++ plugins/woocommerce/tests/e2e/config/jest.config.js
@@ -1,6 +1,12 @@
 const path = require( 'path' );
 const { useE2EJestConfig, getAppRoot } = require( '@woocommerce/e2e-environment' );
 
+console.log('<<< Logging >>>>\n');
+
+console.log( "process.cwd():\t " + process.cwd()  );
+
+console.log("__dirname:\t " + __dirname + '\n');
+
 const jestConfig = useE2EJestConfig( {
 	roots: [ path.resolve( __dirname, '../specs' ) ],
 } );
