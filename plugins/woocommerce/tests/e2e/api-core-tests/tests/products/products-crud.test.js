ec6f839ee299b7002b615d72d1f9282086f541ac
Rodel <rodel.calasagsag@a8c.com>
2021-10-26 18:14:57 +0800

    Added tests for batch update products

--- plugins/woocommerce/tests/e2e/api-core-tests/tests/products/products-crud.test.js
+++ plugins/woocommerce/tests/e2e/api-core-tests/tests/products/products-crud.test.js
@@ -6,6 +6,7 @@ const {
 	virtualProduct,
 	variableProduct,
 } = require( '../../data/products-crud' );
+const { batch } = require( '../../data/shared/batch-update' );
 const { productsApi } = require( '../../endpoints/products' );
 
 /**
@@ -104,5 +105,91 @@ describe( 'Products API tests: CRUD', () => {
 			status: retrieveDeletedProductStatus,
 		} = await productsApi.retrieve.product( productId );
 		expect( retrieveDeletedProductStatus ).toEqual( 404 );
+	} );
+
+	describe( 'Batch update products', () => {
+		const product1 = { ...simpleProduct, name: 'Batch Created Product 1' };
+		const product2 = { ...simpleProduct, name: 'Batch Created Product 2' };
+		const expectedProducts = [ product1, product2 ];
+
+		it( 'can batch create products', async () => {
+			// Send request to batch create products
+			const batchCreatePayload = batch( 'create', expectedProducts );
+			const { status, body } = await productsApi.batch.products(
+				batchCreatePayload
+			);
+			const actualBatchCreatedProducts = body.create;
+
+			expect( status ).toEqual( productsApi.batch.responseCode );
+			expect( actualBatchCreatedProducts ).toHaveLength(
+				expectedProducts.length
+			);
+
+			// Verify id and name of the batch created products
+			for ( let i = 0; i < actualBatchCreatedProducts.length; i++ ) {
+				const { id, name } = actualBatchCreatedProducts[ i ];
+
+				expect( typeof id ).toEqual( 'number' );
+				expect( name ).toEqual( expectedProducts[ i ].name );
+
+				// Save the product ID for use later in the batch update and delete tests
+				expectedProducts[ i ].id = id;
+			}
+		} );
+
+		it( 'can batch update products', async () => {
+			// Send request to batch update the regular price
+			const newRegularPrice = '12.34';
+			for ( let i = 0; i < expectedProducts.length; i++ ) {
+				expectedProducts[ i ].regular_price = newRegularPrice;
+			}
+			const batchUpdatePayload = batch( 'update', expectedProducts );
+			const { status, body } = await productsApi.batch.products(
+				batchUpdatePayload
+			);
+			const actualUpdatedProducts = body.update;
+
+			expect( status ).toEqual( productsApi.batch.responseCode );
+			expect( actualUpdatedProducts ).toHaveLength(
+				expectedProducts.length
+			);
+
+			// Verify that the regular price of each product was updated
+			for ( let i = 0; i < actualUpdatedProducts.length; i++ ) {
+				const { id, regular_price } = actualUpdatedProducts[ i ];
+
+				expect( id ).toEqual( expectedProducts[ i ].id );
+				expect( regular_price ).toEqual( newRegularPrice );
+			}
+		} );
+
+		it( 'can batch delete products', async () => {
+			// Send request to batch delete the products created earlier
+			const idsToDelete = expectedProducts.map( ( { id } ) => id );
+			const batchDeletePayload = batch( 'delete', idsToDelete );
+			const { status, body } = await productsApi.batch.products(
+				batchDeletePayload
+			);
+			const actualBatchDeletedProducts = body.delete;
+
+			expect( status ).toEqual( productsApi.batch.responseCode );
+			expect( actualBatchDeletedProducts ).toHaveLength(
+				expectedProducts.length
+			);
+
+			// Verify that the correct products were deleted
+			for ( let i = 0; i < actualBatchDeletedProducts.length; i++ ) {
+				const deletedProduct = actualBatchDeletedProducts[ i ];
+
+				expect( deletedProduct ).toMatchObject( expectedProducts[ i ] );
+			}
+
+			// Verify that the deleted product ID's can no longer be retrieved
+			for ( const id of idsToDelete ) {
+				const { status } = await productsApi.retrieve.product( id );
+
+				expect( status ).toEqual( 404 );
+			}
+		} );
 	} );
 } );
