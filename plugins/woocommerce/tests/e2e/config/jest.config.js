558e76cffcb3faafad4ae90ac1931a3c878c9c71
roykho <roykho77@gmail.com>
2021-10-26 09:59:22 -0700

    Console log jestConfig

--- plugins/woocommerce/tests/e2e/config/jest.config.js
+++ plugins/woocommerce/tests/e2e/config/jest.config.js
@@ -4,5 +4,5 @@ const { useE2EJestConfig, getAppRoot } = require( '@woocommerce/e2e-environment'
 const jestConfig = useE2EJestConfig( {
 	roots: [ path.resolve( __dirname, '../specs' ) ],
 } );
-
+console.log( jestConfig );
 module.exports = jestConfig;
