3047603e2bbd5907a5651e776809f01327c369b5
Tom Cafferkey <tjcafferkey@gmail.com>
2021-10-29 13:02:23 +0100

    Updated has_block_template to have type casting, added a block comment to the filter and updated filter prefix to use woocommerce_

--- plugins/woocommerce/includes/class-wc-template-loader.php
+++ plugins/woocommerce/includes/class-wc-template-loader.php
@@ -107,7 +107,17 @@ class WC_Template_Loader {
 			return false;
 		}
 
-		return apply_filters( 'wc_has_block_template', is_readable( get_stylesheet_directory() . '/block-templates/' . $template_name . '.html' ) );
+		$has_template = (bool) is_readable( get_stylesheet_directory() . '/block-templates/' . $template_name . '.html' );
+
+		/**
+		 * Filters the value of the result of the block template check.
+		 *
+		 * @since x.x.x
+		 *
+		 * @param boolean $has_template value to be filtered.
+		 * @param string $template_name The name of the template.
+		 */
+		return apply_filters( 'woocommerce_has_block_template', $has_template, $template_name );
 	}
 
 	/**
