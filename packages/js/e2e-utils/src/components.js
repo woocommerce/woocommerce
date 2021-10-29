5ba261950c62756ad64e9f4817bde6e381b9009d
Greg <greg.bell@automattic.com>
2021-10-29 15:19:14 -0600

    Use category ID instead of category name

--- packages/js/e2e-utils/src/components.js
+++ packages/js/e2e-utils/src/components.js
@@ -208,12 +208,15 @@ const createSimpleProduct = async ( productTitle = simpleProductName, productPri
  * @param categoryName Product's category which can be changed when writing a test
  */
 const createSimpleProductWithCategory = async ( productName, productPrice, categoryName ) => {
+	// Get the category ID so we can add it to the product below
+	const categoryId = await withRestApi.createProductCategory( categoryName );
+
 	const product = await factories.products.simple.create( {
 		name: productName,
 		regularPrice: productPrice,
 		categories: [
 			{
-				name: categoryName,
+				id: categoryId,
 			}
 		],
 		isVirtual: true,
