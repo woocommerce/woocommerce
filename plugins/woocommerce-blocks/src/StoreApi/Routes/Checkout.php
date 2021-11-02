12045c731a3ebb32b3466094c49eb3442b228853
Thomas Roberts <5656702+opr@users.noreply.github.com>
2021-11-02 08:46:54 +0000

    Graduate `__experimental_woocommerce_blocks_checkout_order_processed` action to stable (https://github.com/woocommerce/woocommerce-blocks/pull/5014)
    * Deprecate __experimental_woocommerce_blocks_checkout_order_processed
    
    * Update docs to reflect new hook name
    
    I also mentioned this hook is deprecated in the experimental interfaces doc.
    
    * Update deprecated tag in docblock
    
    * Fix typo in deprecated docblock
    
    * Send args to do_action_deprecated in an array
    
    * Amend deprecated tag and add version numbers

--- plugins/woocommerce-blocks/src/StoreApi/Routes/Checkout.php
+++ plugins/woocommerce-blocks/src/StoreApi/Routes/Checkout.php
@@ -246,8 +246,33 @@ class Checkout extends AbstractCartRoute {
 		 * @internal This Hook is experimental and may change or be removed.
 
 		 * @param \WC_Order $order Order object.
+		 * @deprecated 6.3.0 Use woocommerce_blocks_checkout_order_processed instead.
 		 */
-		do_action( '__experimental_woocommerce_blocks_checkout_order_processed', $this->order );
+		do_action_deprecated(
+			'__experimental_woocommerce_blocks_checkout_order_processed',
+			array(
+				$this->order,
+			),
+			'6.3.0',
+			'woocommerce_blocks_checkout_order_processed',
+			'This action was deprecated in WooCommerce Blocks version 6.3.0. Please use woocommerce_blocks_checkout_order_processed instead.'
+		);
+
+		/**
+		 * Fires before an order is processed by the Checkout Block/Store API.
+		 *
+		 * This hook informs extensions that $order has completed processing and is ready for payment.
+		 *
+		 * This is similar to existing core hook woocommerce_checkout_order_processed. We're using a new action:
+		 * - To keep the interface focused (only pass $order, not passing request data).
+		 * - This also explicitly indicates these orders are from checkout block/StoreAPI.
+		 *
+		 * @see https://github.com/woocommerce/woocommerce-gutenberg-products-block/pull/3238
+		 * @example See docs/examples/checkout-order-processed.md
+
+		 * @param \WC_Order $order Order object.
+		 */
+		do_action( 'woocommerce_blocks_checkout_order_processed', $this->order );
 
 		/**
 		 * Process the payment and return the results.
