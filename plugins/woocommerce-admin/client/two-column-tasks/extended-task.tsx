5391b9e06619d670b3e98a477ac186d8b09d8467
Moon <moon.kyong@automattic.com>
2021-11-01 19:33:10 -0700

    Remove store setup from the things to do list (https://github.com/woocommerce/woocommerce-admin/pull/7876)

--- plugins/woocommerce-admin/client/two-column-tasks/extended-task.tsx
+++ plugins/woocommerce-admin/client/two-column-tasks/extended-task.tsx
@@ -109,7 +109,10 @@ const ExtendedTask: React.FC< TasksProps > = ( { query } ) => {
 		...new Set(
 			extendedTaskList.tasks.concat(
 				setupTaskList?.tasks.filter( ( unallowedTask ) => {
-					return ! allowedTasks.includes( unallowedTask.id );
+					return (
+						! allowedTasks.includes( unallowedTask.id ) &&
+						unallowedTask.id !== 'store_details'
+					);
 				} ) || []
 			)
 		),
