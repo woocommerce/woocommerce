cc287ed93ba53ffe09031fa6f242981a0c9a6990
Paul Sealock <psealock@gmail.com>
2021-11-03 14:12:39 +1300

    extra logging

--- .github/workflows/scripts/mirrors.sh
+++ .github/workflows/scripts/mirrors.sh
@@ -8,11 +8,15 @@ else
 	exit 1
 fi
 
-printf "\n\n\e[7m Mirror: %s \e[0m\n" "woocommerce"
-	CLONE_DIR="${BUILD_BASE}/${GIT_SLUG}"
-	cd "${CLONE_DIR}"
+printf "\n\n\e[7m Mirror: %s \e[0m\n" "${GIT_SLUG}"
 
-	# Initialize the directory as a git repo, and set the remote
-	git init -b "$BRANCH" .
-	git remote add origin "https://github.com/${GIT_SLUG}"
-	git config --local http.https://github.com/.extraheader "AUTHORIZATION: basic $(printf "x-access-token:%s" "$API_TOKEN_GITHUB" | base64)"
+CLONE_DIR="${BUILD_BASE}/${GIT_SLUG}"
+echo "navigation to ${CLONE_DIR}"
+cd "${CLONE_DIR}"
+
+# Initialize the directory as a git repo, and set the remote
+echo "Initializing with new branch: $BRANCH"
+git init -b "$BRANCH" .
+echo "Adding origin: https://github.com/${GIT_SLUG}"
+git remote add origin "https://github.com/${GIT_SLUG}"
+git config --local http.https://github.com/.extraheader "AUTHORIZATION: basic $(printf "x-access-token:%s" "$TOKEN" | base64)"
