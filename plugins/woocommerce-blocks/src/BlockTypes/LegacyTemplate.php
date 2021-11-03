927af6ea261701ac799683bcdde4b83f1b54493e
Tom Cafferkey <tjcafferkey@gmail.com>
2021-11-03 09:31:47 +0000

    Add Single Product Block Template (https://github.com/woocommerce/woocommerce-blocks/pull/5054)
    * Add single-product.html template to render core PHP template
    
    * Update placeholder SVG

--- plugins/woocommerce-blocks/src/BlockTypes/LegacyTemplate.php
+++ plugins/woocommerce-blocks/src/BlockTypes/LegacyTemplate.php
@@ -56,9 +56,37 @@ class LegacyTemplate extends AbstractDynamicBlock {
 	protected function render_single_product() {
 		ob_start();
 
-		echo 'This method will render the single product template';
+		/**
+		 * Woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+
+		while ( have_posts() ) :
+
+			the_post();
+			wc_get_template_part( 'content', 'single-product' );
+
+		endwhile;
+
+		/**
+		 * Woocommerce_after_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action( 'woocommerce_after_main_content' );
+
+		/**
+		 * Woocommerce_sidebar hook.
+		 *
+		 * @hooked woocommerce_get_sidebar - 10
+		 */
+		do_action( 'woocommerce_sidebar' );
 
 		wp_reset_postdata();
+
 		return ob_get_clean();
 	}
 
