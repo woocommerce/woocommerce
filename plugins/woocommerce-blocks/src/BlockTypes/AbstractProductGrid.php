62e2a2b61e9e7df4fc0f569979c091087b67a50d
Mike Jolley <mike.jolley@me.com>
2021-11-04 11:39:16 +0000

    Fix Duplicate Queries in Product Grids woocommerce/woocommerce-blocks#4695 (https://github.com/woocommerce/woocommerce-blocks/pull/5002)
    * Cache variation_meta_data to prevent duplicate queries with multiple grids
    
    * Prime the cache
    
    * Improve existing cache detection
    
    * Expand comment

--- plugins/woocommerce-blocks/src/BlockTypes/AbstractProductGrid.php
+++ plugins/woocommerce-blocks/src/BlockTypes/AbstractProductGrid.php
@@ -306,12 +306,90 @@ abstract class AbstractProductGrid extends AbstractDynamicBlock {
 		// Remove ordering query arguments which may have been added by get_catalog_ordering_args.
 		WC()->query->remove_ordering_args();
 
-		// Prime caches to reduce future queries.
+		// Prime caches to reduce future queries. Note _prime_post_caches is private--we could replace this with our own
+		// query if it becomes unavailable.
 		if ( is_callable( '_prime_post_caches' ) ) {
 			_prime_post_caches( $results );
 		}
 
+		$this->prime_product_variations( $results );
+
 		return $results;
+	}
+
+	/**
+	 * Retrieve IDs that are not already present in the cache.
+	 *
+	 * Based on WordPress function: _get_non_cached_ids
+	 *
+	 * @param int[]  $product_ids Array of IDs.
+	 * @param string $cache_key  The cache bucket to check against.
+	 * @return int[] Array of IDs not present in the cache.
+	 */
+	protected function get_non_cached_ids( $product_ids, $cache_key ) {
+		$non_cached_ids = array();
+		$cache_values   = wp_cache_get_multiple( $product_ids, $cache_key );
+
+		foreach ( $cache_values as $id => $value ) {
+			if ( ! $value ) {
+				$non_cached_ids[] = (int) $id;
+			}
+		}
+
+		return $non_cached_ids;
+	}
+
+	/**
+	 * Prime query cache of product variation meta data.
+	 *
+	 * Prepares values in the product_ID_variation_meta_data cache for later use in the ProductSchema::get_variations()
+	 * method. Doing so here reduces the total number of queries needed.
+	 *
+	 * @param int[] $product_ids Product ids to prime variation cache for.
+	 */
+	protected function prime_product_variations( $product_ids ) {
+		$cache_group       = 'product_variation_meta_data';
+		$prime_product_ids = $this->get_non_cached_ids( wp_parse_id_list( $product_ids ), $cache_group );
+
+		if ( ! $prime_product_ids ) {
+			return;
+		}
+
+		global $wpdb;
+
+		// phpcs:disable WordPress.DB.PreparedSQL.NotPrepared
+		$product_variations      = $wpdb->get_results( "SELECT ID as variation_id, post_parent as product_id from {$wpdb->posts} WHERE post_parent IN ( " . implode( ',', $prime_product_ids ) . ' )', ARRAY_A );
+		$prime_variation_ids     = array_column( $product_variations, 'variation_id' );
+		$variation_ids_by_parent = array_column( $product_variations, 'product_id', 'variation_id' );
+		$all_variation_meta_data = $wpdb->get_results(
+			$wpdb->prepare(
+				"SELECT post_id as variation_id, meta_key as attribute_key, meta_value as attribute_value FROM {$wpdb->postmeta} WHERE post_id IN (" . implode( ',', array_map( 'esc_sql', $prime_variation_ids ) ) . ') AND meta_key LIKE %s',
+				$wpdb->esc_like( 'attribute_' ) . '%'
+			)
+		);
+		// phpcs:enable
+
+		// Prepare the data to cache by indexing by the parent product.
+		$primed_data = array_reduce(
+			$all_variation_meta_data,
+			function( $values, $data ) use ( $variation_ids_by_parent ) {
+				$values[ $variation_ids_by_parent[ $data->variation_id ] ?? 0 ][] = $data;
+				return $values;
+			},
+			array_fill_keys( $prime_product_ids, [] )
+		);
+
+		// Cache everything.
+		foreach ( $primed_data as $product_id => $variation_meta_data ) {
+			wp_cache_set(
+				$product_id,
+				[
+					'last_modified' => get_the_modified_date( 'U', $product_id ),
+					'data'          => $variation_meta_data,
+				],
+				$cache_group
+			);
+		}
 	}
 
 	/**
