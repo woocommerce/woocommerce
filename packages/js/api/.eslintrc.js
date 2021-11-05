608d42711d39e5057328bb1f07b4f44c495dd7d5
roykho <roykho77@gmail.com>
2021-11-05 05:43:46 -0700

    Implement Nx lint for API package

--- packages/js/api/.eslintrc.js
+++ packages/js/api/.eslintrc.js
@@ -1,12 +1,9 @@
 module.exports = {
 	parser: '@typescript-eslint/parser',
 	env: {
-		'jest/globals': true
+		'jest/globals': true,
 	},
-	ignorePatterns: [
-		'dist/',
-		'node_modules/'
-	],
+	ignorePatterns: [ 'dist/', 'node_modules/' ],
 	rules: {
 		'no-unused-vars': 'off',
 		'no-dupe-class-members': 'off',
@@ -14,18 +11,11 @@ module.exports = {
 		'no-useless-constructor': 'off',
 		'@typescript-eslint/no-useless-constructor': 2,
 	},
-	plugins: [
-		'@typescript-eslint'
-	],
-	extends: [
-		'plugin:@wordpress/eslint-plugin/recommended-with-formatting'
-	],
+	plugins: [ '@typescript-eslint/eslint-plugin' ],
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended-with-formatting' ],
 	overrides: [
 		{
-			files: [
-				'**/*.js',
-				'**/*.ts'
-			],
+			files: [ '**/*.js', '**/*.ts' ],
 			settings: {
 				jsdoc: {
 					mode: 'typescript',
@@ -33,13 +23,10 @@ module.exports = {
 			},
 		},
 		{
-			files: [
-				'**/*.spec.ts',
-				'**/*.test.ts'
-			],
+			files: [ '**/*.spec.ts', '**/*.test.ts' ],
 			rules: {
 				'no-console': 'off',
-			}
-		}
-	]
-}
+			},
+		},
+	],
+};
