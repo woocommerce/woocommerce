c8401841bd337864b4db83ec0fb660107eb483a9
Cullen Whitmore <cwhitmore@matchboxdesigngroup.com>
2021-11-08 11:30:18 -0600

    Change attribute label column from `td` to `th`

--- plugins/woocommerce/templates/single-product/add-to-cart/variable.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/variable.php
@@ -35,7 +35,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 			<tbody>
 				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
 					<tr>
-						<td class="label"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok. ?></label></td>
+						<th class="label"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok. ?></label></th>
 						<td class="value">
 							<?php
 								wc_dropdown_variation_attribute_options(
