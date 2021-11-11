a903885430252cbc625f43a66aeaad3359a4cb71
Rodel <rodel.calasagsag@automattic.com>
2021-11-11 19:39:40 +0800

    Added shipping zone tests

--- packages/js/api-core-tests/endpoints/index.js
+++ packages/js/api-core-tests/endpoints/index.js
@@ -1,11 +1,13 @@
-const { ordersApi } = require('./orders');
-const { couponsApi } = require('./coupons');
-const { productsApi } = require('./products');
-const { refundsApi } = require('./refunds');
+const { ordersApi } = require( './orders' );
+const { couponsApi } = require( './coupons' );
+const { productsApi } = require( './products' );
+const { refundsApi } = require( './refunds' );
+const { shippingZonesApi } = require( './shipping-zones' );
 
 module.exports = {
 	ordersApi,
 	couponsApi,
 	productsApi,
 	refundsApi,
+	shippingZonesApi,
 };
