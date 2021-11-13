9d9af843d6165b512b197cfb7ef4a44c81471984
Brian <brian@brianhaas.li>
2021-11-13 18:56:41 +0100

    add label for ecuador

--- plugins/woocommerce/includes/class-wc-countries.php
+++ plugins/woocommerce/includes/class-wc-countries.php
@@ -975,6 +975,11 @@ class WC_Countries {
 							'hidden'   => true,
 						),
 					),
+					'EC' => array(
+						'state'    => array(
+							'label' => __( 'Provincia', 'woocommerce' ),
+						),
+					),
 					'EE' => array(
 						'postcode' => array(
 							'priority' => 65,
