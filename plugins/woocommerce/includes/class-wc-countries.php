c21a78bc919112c9e5af9bf273a123a1d3111d7c
Brian <brian@brianhaas.li>
2021-11-13 13:10:23 +0100

    add label for nicaragua state

--- plugins/woocommerce/includes/class-wc-countries.php
+++ plugins/woocommerce/includes/class-wc-countries.php
@@ -1214,6 +1214,11 @@ class WC_Countries {
 							'label' => __( 'Province', 'woocommerce' ),
 						),
 					),
+					'NI' => array(
+						'state'    => array(
+							'label' => __( 'Departamento', 'woocommerce' ),
+						),
+					),
 					'NL' => array(
 						'postcode' => array(
 							'priority' => 65,
