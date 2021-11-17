ebcd4d9682623e9bc378616e548988280ee6fd58
hellwig-data443 <80766926+hellwig-data443@users.noreply.github.com>
2021-11-17 11:36:25 -0500

    Update PageController.php (https://github.com/woocommerce/woocommerce-admin/pull/7855)
    * Update PageController.php
    
    * Create fix-7758
    
    * Minor fixes

--- plugins/woocommerce-admin/src/PageController.php
+++ plugins/woocommerce-admin/src/PageController.php
@@ -356,7 +356,7 @@ class PageController {
 
 		// Disable embed on the block editor.
 		$current_screen = did_action( 'current_screen' ) ? get_current_screen() : false;
-		if ( method_exists( $current_screen, 'is_block_editor' ) && $current_screen->is_block_editor() ) {
+		if ( ! empty( $current_screen ) && method_exists( $current_screen, 'is_block_editor' ) && $current_screen->is_block_editor() ) {
 			$is_connected_page = false;
 		}
 
