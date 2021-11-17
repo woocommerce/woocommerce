4fa088af47c0c1a565316dafd7b3f1884d503388
Tung Du <dinhtungdu@gmail.com>
2021-11-17 22:39:07 +0700

    Add global style support to Mini Cart (button) (https://github.com/woocommerce/woocommerce-blocks/pull/5100)

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/mini-cart/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/mini-cart/edit.tsx
@@ -1,19 +1,65 @@
 /**
  * External dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	InspectorControls,
+	useBlockProps,
+	getColorClassName,
+} from '@wordpress/block-editor';
 import type { ReactElement } from 'react';
 import { formatPrice } from '@woocommerce/price-format';
 import { CartCheckoutCompatibilityNotice } from '@woocommerce/editor-components/compatibility-notices';
+import { PanelBody, ToggleControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import classnames from 'classnames';
 
 /**
  * Internal dependencies
  */
 import QuantityBadge from './quantity-badge';
 
-const MiniCartBlock = (): ReactElement => {
+interface Attributes {
+	isInitiallyOpen?: boolean;
+	transparentButton: boolean;
+	backgroundColor?: string;
+	textColor?: string;
+	style?: Record< string, Record< string, string > >;
+}
+
+interface Props {
+	attributes: Attributes;
+	setAttributes: ( attributes: Record< string, unknown > ) => void;
+}
+
+const MiniCartBlock = ( {
+	attributes,
+	setAttributes,
+}: Props ): ReactElement => {
+	const { transparentButton, backgroundColor, textColor, style } = attributes;
 	const blockProps = useBlockProps( {
-		className: 'wc-block-mini-cart',
+		className: classnames( 'wc-block-mini-cart', {
+			'is-transparent': transparentButton,
+		} ),
+	} );
+
+	/**
+	 * @todo Replace `getColorClassName` and manual style manipulation with
+	 * `useColorProps` once the hook is no longer experimental.
+	 */
+	const backgroundClass = getColorClassName(
+		'background-color',
+		backgroundColor
+	);
+	const textColorClass = getColorClassName( 'color', textColor );
+
+	const colorStyle = {
+		backgroundColor: style?.color?.background,
+		color: style?.color?.text,
+	};
+
+	const colorClassNames = classnames( backgroundClass, textColorClass, {
+		'has-background': backgroundClass || style?.color?.background,
+		'has-text-color': textColorClass || style?.color?.text,
 	} );
 
 	const productCount = 0;
@@ -21,11 +67,42 @@ const MiniCartBlock = (): ReactElement => {
 
 	return (
 		<div { ...blockProps }>
-			<button className="wc-block-mini-cart__button">
+			<InspectorControls>
+				<PanelBody
+					title={ __(
+						'Button style',
+						'woo-gutenberg-products-block'
+					) }
+				>
+					<ToggleControl
+						label={ __(
+							'Use transparent button',
+							'woo-gutenberg-products-block'
+						) }
+						checked={ transparentButton }
+						onChange={ () =>
+							setAttributes( {
+								transparentButton: ! transparentButton,
+							} )
+						}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<button
+				className={ classnames(
+					'wc-block-mini-cart__button',
+					colorClassNames
+				) }
+				style={ colorStyle }
+			>
 				<span className="wc-block-mini-cart__amount">
 					{ formatPrice( productTotal ) }
 				</span>
-				<QuantityBadge count={ productCount } />
+				<QuantityBadge
+					count={ productCount }
+					colorClassNames={ colorClassNames }
+					style={ colorStyle }
+				/>
 			</button>
 			<CartCheckoutCompatibilityNotice blockName="mini-cart" />
 		</div>
