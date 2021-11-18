82f4120208aa243b66377c1b003b0156881eafa7
Ilyas Foo <foo.ilyas@gmail.com>
2021-11-18 12:52:43 +0800

    Fix race condition in data package's options module (https://github.com/woocommerce/woocommerce-admin/pull/7947)
    * Fix race condition on data package's options module.
    
    * Update changelog

--- plugins/woocommerce-admin/packages/data/CHANGELOG.md
+++ plugins/woocommerce-admin/packages/data/CHANGELOG.md
@@ -3,6 +3,7 @@
 -   Fix the batch fetch logic for the options data store. #7587
 -   Add backwards compability for old function format. #7688
 -   Add console warning for inbox note contents exceeding 320 characters and add dompurify dependency. #7869
+-   Fix race condition in data package's options module. #7947
 
 # 1.4.0
 
--- plugins/woocommerce-admin/packages/data/src/options/controls.js
+++ plugins/woocommerce-admin/packages/data/src/options/controls.js
@@ -26,23 +26,28 @@ export const controls = {
 
 		return new Promise( ( resolve ) => {
 			setTimeout( function () {
-				const names = optionNames.join( ',' );
-				if ( fetches[ names ] ) {
-					return fetches[ names ].then( ( result ) => {
+				if ( fetches[ optionName ] ) {
+					return fetches[ optionName ].then( ( result ) => {
 						resolve( result );
 					} );
 				}
 
+				// Get unique option names.
+				const names = [ ...new Set( optionNames ) ].join( ',' );
+				// Send request for a group of options.
 				const url = WC_ADMIN_NAMESPACE + '/options?options=' + names;
-				fetches[ names ] = apiFetch( { path: url } );
-				fetches[ names ].then( ( result ) => resolve( result ) );
+				const fetch = apiFetch( { path: url } );
+				fetch.then( ( result ) => resolve( result ) );
+				optionNames.forEach( ( option ) => {
+					fetches[ option ] = fetch;
+					fetches[ option ].then( () => {
+						// Delete the fetch after to allow wp data to handle cache invalidation.
+						delete fetches[ option ];
+					} );
+				} );
 
-				// Clear option names after all resolved;
-				setTimeout( () => {
-					optionNames = [];
-					// Delete the fetch after to allow wp data to handle cache invalidation.
-					delete fetches[ names ];
-				}, 1 );
+				// Clear option names after we've sent the request for a group of options.
+				optionNames = [];
 			}, 1 );
 		} );
 	},
