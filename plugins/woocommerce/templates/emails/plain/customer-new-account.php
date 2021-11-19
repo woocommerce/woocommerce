c916d80df27be6d108064fb4b45f01fb0af17e2b
Peter Fabian <peter.fabian.github@gmail.com>
2021-11-19 19:34:46 +0100

    Bump template version.

--- plugins/woocommerce/templates/emails/plain/customer-new-account.php
+++ plugins/woocommerce/templates/emails/plain/customer-new-account.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails\Plain
- * @version 3.7.0
+ * @version 6.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
