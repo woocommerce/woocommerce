3b1bedd15f86d6175068f51b54c50845d200338e
Mike Jolley <mike.jolley@me.com>
2021-11-22 08:52:43 +0000

    Checkbox Component Styling Fix (https://github.com/woocommerce/woocommerce-blocks/pull/5164)
    * Wrap label in div to prevent clickable area growing too large
    
    * Prevent pointer events on svg
    
    * Double gap in editor

--- plugins/woocommerce-blocks/packages/checkout/components/checkbox-control/index.tsx
+++ plugins/woocommerce-blocks/packages/checkout/components/checkbox-control/index.tsx
@@ -35,7 +35,7 @@ const CheckboxControl = ( {
 	const checkboxId = id || `checkbox-control-${ instanceId }`;
 
 	return (
-		<label
+		<div
 			className={ classNames(
 				'wc-block-components-checkbox',
 				{
@@ -43,31 +43,32 @@ const CheckboxControl = ( {
 				},
 				className
 			) }
-			htmlFor={ checkboxId }
 		>
-			<input
-				id={ checkboxId }
-				className="wc-block-components-checkbox__input"
-				type="checkbox"
-				onChange={ ( event ) => onChange( event.target.checked ) }
-				aria-invalid={ hasError === true }
-				{ ...rest }
-			/>
-			<svg
-				className="wc-block-components-checkbox__mark"
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 20"
-			>
-				<path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
-			</svg>
-			{ label && (
-				<span className="wc-block-components-checkbox__label">
-					{ label }
-				</span>
-			) }
-			{ children }
-		</label>
+			<label htmlFor={ checkboxId }>
+				<input
+					id={ checkboxId }
+					className="wc-block-components-checkbox__input"
+					type="checkbox"
+					onChange={ ( event ) => onChange( event.target.checked ) }
+					aria-invalid={ hasError === true }
+					{ ...rest }
+				/>
+				<svg
+					className="wc-block-components-checkbox__mark"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 20"
+				>
+					<path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+				</svg>
+				{ label && (
+					<span className="wc-block-components-checkbox__label">
+						{ label }
+					</span>
+				) }
+				{ children }
+			</label>
+		</div>
 	);
 };
 
