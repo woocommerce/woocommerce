8fdbc77bf8892596f7cd03e2c54c4159b21520b4
jamelreid <jnoelreid@gmail.com>
2021-11-24 12:15:30 -0500

    Added WooCommerce menu page load test script

---
+++ packages/js/e2e-core-tests/specs/merchant/wp-admin-woocommerce-page-loads.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable jest/no-export, jest/no-disabled-tests */
+/**
+ * Internal dependencies
+ */
+const {
+	merchant,
+	waitForSelectorWithoutThrow,
+} = require( '@woocommerce/e2e-utils' );
+
+/**
+ * External dependencies
+ */
+const {
+	it,
+	describe,
+	beforeAll,
+} = require( '@jest/globals' );
+
+const runWooCommercePageLoadTest = () => {
+	describe( ' WooCommerce > Opening Top Level Pages', () => {
+
+	});
+};
+
+module.exports = runWooCommercePageLoadTest;
