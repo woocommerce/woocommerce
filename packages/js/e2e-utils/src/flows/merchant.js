e5a12e533489a1ed9c3f11c97984cc5b48d189d3
jamelreid <jnoelreid@gmail.com>
2021-11-24 19:15:34 -0500

    Added function to open WC Admin Page

--- packages/js/e2e-utils/src/flows/merchant.js
+++ packages/js/e2e-utils/src/flows/merchant.js
@@ -215,6 +215,15 @@ const merchant = {
 		} );
 	},
 
+	/**
+	 * Opens the WordPress WooCommerce Page
+	 */
+	openAdminWcPage: async () => {
+		await page.goto( WP_ADMIN_WC_HOME, {
+			waitUntil: 'networkidle0',
+		} );
+	},
+
 	openAllUsersView: async () => {
 		await page.goto( WP_ADMIN_ALL_USERS_VIEW, {
 			waitUntil: 'networkidle0',
