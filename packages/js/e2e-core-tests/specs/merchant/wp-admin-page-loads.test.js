0f495cdbaee18bb1319a71e212356c19b221c8b0
jamelreid <jnoelreid@gmail.com>
2021-11-25 13:20:36 -0500

    Moved setViewport and logged out after each test
    Changes:
    - Moved setViewport to the beforeAll block
    - Added a afterAll block to log out after each set of tests
    - References the MENUS array from specs/data/elements.js

--- packages/js/e2e-core-tests/specs/merchant/wp-admin-page-loads.test.js
+++ packages/js/e2e-core-tests/specs/merchant/wp-admin-page-loads.test.js
@@ -2,101 +2,23 @@
  * Internal dependencies
  */
 const { merchant } = require( '@woocommerce/e2e-utils' );
+const { MENUS } = require( '../data/elements' );
 
 /**
  * External dependencies
  */
 const { it, describe, beforeAll } = require( '@jest/globals' );
 
-// Top-level menu items and their associated sub-menus
-const menus = [
-	[
-		'WooCommerce',
-		'#adminmenu > li:nth-child(8) > a',
-		[
-			[
-				'Home',
-				'#toplevel_page_woocommerce > ul > li:nth-child(2)',
-				'Home',
-			],
-			[
-				'Orders',
-				'#toplevel_page_woocommerce > ul > li:nth-child(3)',
-				'Orders',
-			],
-			[
-				'Reports',
-				'#toplevel_page_woocommerce > ul > li:nth-child(6)',
-				'Orders',
-			],
-			[
-				'Settings',
-				'#toplevel_page_woocommerce > ul > li:nth-child(7)',
-				'General',
-			],
-			[
-				'Status',
-				'#toplevel_page_woocommerce > ul > li:nth-child(8)',
-				'System status',
-			],
-			// [ 'Extensions', '#toplevel_page_woocommerce > ul > li:nth-child(9)', 'Extensions' ],
-		],
-	],
-	[
-		'Products',
-		'#adminmenu > li:nth-child(9) > a',
-		[
-			[
-				'All Products',
-				'#menu-posts-product > ul > li:nth-child(2)',
-				'Products',
-			],
-			[
-				'Add New',
-				'#menu-posts-product > ul > li:nth-child(3)',
-				'Add New',
-			],
-			[
-				'Categories',
-				'#menu-posts-product > ul > li:nth-child(4)',
-				'Product categories',
-			],
-			[
-				'Product tags',
-				'#menu-posts-product > ul > li:nth-child(5)',
-				'Product tags',
-			],
-			[
-				'Attributes',
-				'#menu-posts-product > ul > li:nth-child(6)',
-				'Attributes',
-			],
-		],
-	],
-	[
-		'Marketing',
-		'#adminmenu > li:nth-child(11) > a',
-		[
-			[
-				'Overview',
-				'#toplevel_page_woocommerce-marketing > ul > li:nth-child(2)',
-				'Overview',
-			],
-			[
-				'Coupons',
-				'#toplevel_page_woocommerce-marketing > ul > li:nth-child(3)',
-				'Coupons',
-			],
-		],
-	],
-];
-
 const runPageLoadTest = () => {
-	describe.each( menus )(
+	describe.each( MENUS )(
 		' %s > Opening Top Level Pages',
 		( menuTitle, menuElement, subMenus ) => {
 			beforeAll( async () => {
 				await merchant.login();
+				await page.setViewport( {
+					width: 1280,
+					height: 800,
+				} );
 			} );
 
 			it.each( subMenus )(
@@ -106,10 +28,6 @@ const runPageLoadTest = () => {
 					await Promise.all( [
 						page.click( menuElement ),
 						page.waitForNavigation( { waitUntil: 'networkidle0' } ),
-						page.setViewport( {
-							width: 1280,
-							height: 800,
-						} ),
 					] );
 
 					// Click sub-menu item and wait for the page to finish loading
@@ -123,6 +41,10 @@ const runPageLoadTest = () => {
 					} );
 				}
 			);
+
+			afterAll( async () => {
+				await merchant.logout();
+			} );
 		}
 	);
 };
