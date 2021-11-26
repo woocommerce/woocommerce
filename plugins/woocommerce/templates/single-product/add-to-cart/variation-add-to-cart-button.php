a79069ae5d5cb1a7dca3056993ed556dad0486d2
Marc Guay <marc.guay@gmail.com>
2021-11-26 09:30:55 -0500

    Make button class filterable

--- plugins/woocommerce/templates/single-product/add-to-cart/variation-add-to-cart-button.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -28,7 +28,7 @@ global $product;
 	do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
 
-	<button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+	<button type="submit" class="<?php echo esc_attr( apply_filters( 'woocommerce_single_add_to_cart_button_class', 'single_add_to_cart_button button alt' ) ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
 	<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 
