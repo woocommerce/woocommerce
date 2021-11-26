0e77c70c96f9d912140a0508248d23803245bed2
Marc Guay <marc.guay@gmail.com>
2021-11-26 09:30:25 -0500

    Make button class filterable

--- plugins/woocommerce/templates/single-product/add-to-cart/simple.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/simple.php
@@ -46,7 +46,7 @@ if ( $product->is_in_stock() ) : ?>
 		do_action( 'woocommerce_after_add_to_cart_quantity' );
 		?>
 
-		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="<?php echo esc_attr( apply_filters( 'woocommerce_single_add_to_cart_button_class', 'single_add_to_cart_button button alt' ) ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
