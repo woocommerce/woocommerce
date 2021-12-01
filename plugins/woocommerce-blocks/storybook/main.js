7e9f8c822da50b928644b23c89d394da67e3d15e
M. L. Giannotta <lucio.giannotta@a8c.com>
2021-12-01 12:28:27 +0100

    Migrate Storybook to 6.4.0 and enable auto-detection of controls (https://github.com/woocommerce/woocommerce-blocks/pull/5249) (https://github.com/woocommerce/woocommerce-blocks/pull/5264)
    * Migrate Storybook to 6.4.0 and enable auto-detection of controls (https://github.com/woocommerce/woocommerce-blocks/pull/5249)
    
    Previously our Babel configuration conflicted with the one provided by
    default by Storybook, making us unable to leverage the automatic
    detection of properties from both JS and TS components to create
    controls (formerly known as knobs).
    
    Upgrading to 6.4.0 fixes a bug that makes it easier to work with
    Babel configurations. Also done here:
    
    * Removed a few deprecated addons in favor of the replacement ones
    bundled within Storybook Essentials.
    * Add auto-detection of certain control types depending on the property names
    * Add support for TS stories
    
    More context: https://github.com/storybookjs/storybook/issues/12292
    
    * Use `react-docgen` only in development environments
    
    This should reduce production bundle size dramatically. The only
    problem here is that Storybook runs its `build` command with the
    `NODE_ENV` set to `production`. For this reason, we change the
    script to provide `BABEL_ENV` set to `development` to still make sure
    the correct configuration is loaded, while trying to reduce any
    side-effects on changing the whole `NODE_ENV`.

--- plugins/woocommerce-blocks/storybook/main.js
+++ plugins/woocommerce-blocks/storybook/main.js
@@ -1,18 +1,16 @@
 module.exports = {
 	stories: [
 		// WooCommerce Blocks stuff (anywhere in repo!)
-		'../assets/js/**/stories/*.js',
+		'../assets/js/**/stories/*.@(js|jsx|ts|tsx)',
 	],
 	addons: [
-		{
-			name: '@storybook/addon-docs',
-			options: { configureJSX: true },
-		},
-		'@storybook/addon-knobs',
-		'@storybook/addon-storysource',
-		'@storybook/addon-viewport',
+		'@storybook/addon-essentials',
 		'@storybook/addon-a11y',
-		'@storybook/addon-actions',
+		'@storybook/addon-knobs',
 		'@storybook/addon-links',
+		'storybook-addon-react-docgen',
 	],
+	features: {
+		babelModeV7: true,
+	},
 };
