d2b1e86213ea2bbc0cad5cea1a65efd9064ce03a
Seghir Nadir <nadir.seghir@gmail.com>
2021-12-08 14:41:31 +0100

    Wait until carts load before calling `canMakePayment` (https://github.com/woocommerce/woocommerce-blocks/pull/5337)
    * wait till cart loads before calling canMakePayment
    
    * fix tests

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/payment-methods/test/payment-methods.js
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/payment-methods/test/payment-methods.js
@@ -2,6 +2,10 @@
  * External dependencies
  */
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { previewCart } from '@woocommerce/resource-previews';
+import { dispatch } from '@wordpress/data';
+import { CART_STORE_KEY as storeKey } from '@woocommerce/block-data';
+import { default as fetchMock } from 'jest-fetch-mock';
 import {
 	registerPaymentMethod,
 	__experimentalDeRegisterPaymentMethod,
@@ -14,17 +18,8 @@ import {
 /**
  * Internal dependencies
  */
-import * as useStoreCartHook from '../../../../base/context/hooks/cart/use-store-cart';
-
-// Somewhere in your test case or test suite
-useStoreCartHook.useStoreCart = jest
-	.fn()
-	.mockReturnValue( useStoreCartHook.defaultCartData );
-
-/**
- * Internal dependencies
- */
 import PaymentMethods from '../payment-methods';
+import { defaultCartState } from '../../../../data/default-states';
 
 jest.mock( '../saved-payment-method-options', () => ( { onChange } ) => {
 	return (
@@ -73,6 +68,22 @@ const resetMockPaymentMethods = () => {
 };
 
 describe( 'PaymentMethods', () => {
+	beforeEach( () => {
+		fetchMock.mockResponse( ( req ) => {
+			if ( req.url.match( /wc\/store\/cart/ ) ) {
+				return Promise.resolve( JSON.stringify( previewCart ) );
+			}
+			return Promise.resolve( '' );
+		} );
+		// need to clear the store resolution state between tests.
+		dispatch( storeKey ).invalidateResolutionForStore();
+		dispatch( storeKey ).receiveCart( defaultCartState.cartData );
+	} );
+
+	afterEach( () => {
+		fetchMock.resetMocks();
+	} );
+
 	test( 'should show no payment methods component when there are no payment methods', async () => {
 		render(
 			<PaymentMethodDataProvider>
@@ -88,6 +99,8 @@ describe( 'PaymentMethods', () => {
 			// creates an extra `div` with the notice contents used for a11y.
 			expect( noPaymentMethods.length ).toBeGreaterThanOrEqual( 1 );
 		} );
+		// ["`select` control in `@wordpress/data-controls` is deprecated. Please use built-in `resolveSelect` control in `@wordpress/data` instead."]
+		expect( console ).toHaveWarned();
 	} );
 
 	test( 'selecting new payment method', async () => {
