59f1f3a6b518ece5a3babc97c0b25d7e918d4833
Ron Rennick <ron@ronandandrea.com>
2021-12-08 13:47:30 -0400

    Give installFiles its own index

--- packages/js/e2e-core-tests/index.js
+++ packages/js/e2e-core-tests/index.js
@@ -4,16 +4,4 @@
 const allSpecs = require( './specs' );
 const fs = require( 'fs' );
 
-/**
- * Test set up configuration for the test scaffolding tool.
- */
-const packageInstallFiles = {
-	testSpecs: 'data/install-specs.json',
-	defaultJson: 'data/default-test-config.json',
-	initializeSh: 'data/initialize.sh.default',
-};
-
-module.exports = {
-	allSpecs,
-	packageInstallFiles,
-};
+module.exports = allSpecs;
---
+++ packages/js/e2e-core-tests/installFiles/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+	testSpecs: 'data/scaffold-tests.json',
+	defaultJson: 'data/default-test-config.json',
+	initializeSh: 'data/initialize.sh.default',
+};
