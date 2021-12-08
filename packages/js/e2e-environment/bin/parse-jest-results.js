83cdbfc001c9cf0dd6041e2ea0f4fc9de1414d74
jamelreid <jnoelreid@gmail.com>
2021-12-08 11:30:46 -0500

    Updated script to post directly to github

--- packages/js/e2e-environment/bin/parse-jest-results.js
+++ packages/js/e2e-environment/bin/parse-jest-results.js
@@ -1,45 +1,44 @@
-const fs = require('fs');
+const fs = require( 'fs' );
 const path = require( 'path' );
-const program = require( 'commander' );
-
-program
-	.usage( '<file ...> [options]' )
-	.option( '--e2e', 'Parse E2E Test Results' )
-	.option( '--smoke', 'Parse Smoke Test Results' )
-	.parse( process.argv );
 
 const resultsFile = path.resolve( __dirname, '../test-results.json' );
 
-if ( fs.existsSync( resultsFile ) ) {
-	const data = fs.readFileSync(resultsFile);
-	const results = JSON.parse( data );
-	let title = 'Test Results';
+module.exports = async ( { github, context } ) => {
+	let output = '';
 
-	title = program.e2e ? 'E2E Test Results' : title;
-	title = program.smoke ? 'Smoke Test Results' : title;
+	if ( fs.existsSync( resultsFile ) ) {
+		const data = fs.readFileSync( resultsFile );
+		const results = JSON.parse( data );
 
-	const totalTests = results.numTotalTests;
-	const totalTestSuites = results.numTotalTestSuites;
-	const totalPassedTestSuites = results.numPassedTestSuites
-	const totalFailedTestSuites = results.numFailedTestSuites
-	const totalFailedTests = results.numFailedTests;
-	const totalPassedTests = results.numPassedTests;
+		const { TITLE } = process.env;
 
-	// TODO: create funciton that makes markdown based on a object
-	/**
-	 * e.g {}
-	 */
-	console.log(`
-		## ${title}:
-		**Total Number of Tests:** ${totalTests}
-		**Total Number of Passed Tests:** ${totalPassedTests}
-		**Total Number of Failed Tests:** ${totalFailedTests}
-		**Total Number of Test Suites:** ${totalTestSuites}
-		**Total Number of Passed Test Suites:** ${totalPassedTestSuites}
-		**Total Number of Failed Test Suites:** ${totalFailedTestSuites}`
-	);
-} else {
-	console.log(`
-		## Test Results Not Found!
-	`);
-}
+		const totalTests = results.numTotalTests;
+		const totalTestSuites = results.numTotalTestSuites;
+		const totalPassedTestSuites = results.numPassedTestSuites;
+		const totalFailedTestSuites = results.numFailedTestSuites;
+		const totalFailedTests = results.numFailedTests;
+		const totalPassedTests = results.numPassedTests;
+
+		// TODO: create funciton that makes markdown based on a object
+		/**
+		 * e.g {}
+		 */
+		output = `
+			## ${ TITLE }:
+			**Total Number of Tests:** ${ totalTests }
+			**Total Number of Passed Tests:** ${ totalPassedTests }
+			**Total Number of Failed Tests:** ${ totalFailedTests }
+			**Total Number of Test Suites:** ${ totalTestSuites }
+			**Total Number of Passed Test Suites:** ${ totalPassedTestSuites }
+			**Total Number of Failed Test Suites:** ${ totalFailedTestSuites }`;
+	} else {
+		output = `## Test Results Not Found!`;
+	}
+
+	await github.rest.issues.createComment( {
+		issue_number: context.issue.number,
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		body: output,
+	} );
+};
