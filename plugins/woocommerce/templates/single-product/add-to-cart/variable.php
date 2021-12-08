70a635dc885de0c23b8f9c8de6c11f0d6770dafc
Cullen Whitmore <cwhitmore@matchboxdesigngroup.com>
2021-12-08 10:13:11 -0600

    Bump the version string in the template header to 6.1.0

--- plugins/woocommerce/templates/single-product/add-to-cart/variable.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/variable.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.5.5
+ * @version 6.1.0
  */
 
 defined( 'ABSPATH' ) || exit;
