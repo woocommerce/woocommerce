ac83762aca80c1f150f6e5189def5655378d0a08
jamelreid <jnoelreid@gmail.com>
2021-12-10 11:02:21 -0500

    Disabled jest retries by default

--- packages/js/e2e-environment/config/jest-object.config.js
+++ packages/js/e2e-environment/config/jest-object.config.js
@@ -3,7 +3,7 @@
  */
 const { E2E_RETRY_TIMES } = process.env;
 
-const setupJestRetries = ( retries = 2 ) => {
+const setupJestRetries = ( retries = 0 ) => {
 	const retryTimes = E2E_RETRY_TIMES ? E2E_RETRY_TIMES : retries;
 
 	jest.retryTimes( retryTimes );
