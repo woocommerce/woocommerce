1c2e3e1f51cfa6495869091abed8c847a6b497db
jamelreid <jnoelreid@gmail.com>
2021-12-13 18:33:17 -0500

    Added config to e2e-environment exports

--- packages/js/e2e-environment/config/index.js
+++ packages/js/e2e-environment/config/index.js
@@ -1,6 +1,7 @@
 /**
  * Internal dependencies
  */
+const config = require('./config');
 const jestConfig = require( './jest.config' );
 const jestPuppeteerConfig = require( './jest-puppeteer.config' );
 const jestobjectConfig = require('./jest-object.config');
@@ -12,6 +13,7 @@ const {
 } = require( './use-config' );
 
 module.exports = {
+	config,
 	jestConfig,
 	...jestobjectConfig,
 	jestPuppeteerConfig,
