5b1b807bdd7d3f8b391fcc51a5884139d44991e5
jamelreid <jnoelreid@gmail.com>
2021-12-14 13:14:41 -0500

    Updated jest.retryTimes() to be a conditional
    This way it doesn't matter if the call is made locally or from the package.

--- packages/js/e2e-environment/config/jest-object.config.js
+++ packages/js/e2e-environment/config/jest-object.config.js
@@ -6,7 +6,9 @@ const { E2E_RETRY_TIMES } = process.env;
 const setupJestRetries = ( retries = 0 ) => {
 	const retryTimes = E2E_RETRY_TIMES ? E2E_RETRY_TIMES : retries;
 
-	jest.retryTimes( retryTimes );
+	if ( retryTimes > 0 ) {
+		jest.retryTimes( retryTimes );
+	}
 };
 
 // If more methods are added to setupJestObject, it should be include in the readme
