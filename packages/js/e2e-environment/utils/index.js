fdee881ab86d419b2acce3c60c48d0fda85a4eba
Greg <greg.bell@automattic.com>
2021-12-15 13:59:17 -0700

    Prettier fixes

--- packages/js/e2e-environment/utils/index.js
+++ packages/js/e2e-environment/utils/index.js
@@ -1,11 +1,13 @@
 const getAppRoot = require( './app-root' );
 const { getAppName, getAppBase } = require( './app-name' );
-const { getTestConfig, getAdminConfig, resolveLocalE2ePath } = require( './test-config' );
-const { getRemotePluginZip, getLatestReleaseZipUrl, deleteDownloadedPluginFiles } = require('./get-plugin-zip');
 const testConfig = require( './test-config' );
-const { getRemotePluginZip, getLatestReleaseZipUrl } = require('./get-plugin-zip');
+const {
+	getRemotePluginZip,
+	getLatestReleaseZipUrl,
+	deleteDownloadedPluginFiles,
+} = require( './get-plugin-zip' );
 const takeScreenshotFor = require( './take-screenshot' );
-const updateReadyPageStatus = require('./update-ready-page');
+const updateReadyPageStatus = require( './update-ready-page' );
 const consoleUtils = require( './filter-console' );
 
 module.exports = {
