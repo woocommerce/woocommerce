91c2ca2143e96e173a0d27ec5ac496815cf78aae
M. L. Giannotta <lucio.giannotta@a8c.com>
2021-12-15 17:03:02 +0100

    Migrate storybook entries of QuantitySelector component (https://github.com/woocommerce/woocommerce-blocks/pull/5370)
    * Add experimental Emotion11 compatibility
    
    Revert the changes introduced in woocommerce/woocommerce-blocks#5354. The team over at Storybook
    has recently implemented a workaround to enable Emotion11 compatibility
    in 6.x releases.
    
    See: https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#emotion11-quasi-compatibility
    
    * Add `QuantitySelector` property docs and migrate stories
    
    *Note:* Because of a Storybook bug, the number input on the control panel of the
    story, is not going to get updated by the component.
    
    See: https://github.com/storybookjs/storybook/issues/15924

--- plugins/woocommerce-blocks/assets/js/base/components/quantity-selector/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/quantity-selector/index.tsx
@@ -12,13 +12,38 @@ import { DOWN, UP } from '@wordpress/keycodes';
  */
 import './style.scss';
 
-interface QuantitySelectorProps {
+export interface QuantitySelectorProps {
+	/**
+	 * Component wrapper classname
+	 *
+	 * @default 'wc-block-components-quantity-selector'
+	 */
 	className?: string;
+	/**
+	 * Current quantity
+	 */
 	quantity?: number;
+	/**
+	 * Minimum quantity
+	 */
 	minimum?: number;
+	/**
+	 * Maximum quantity
+	 */
 	maximum: number;
+	/**
+	 * Event handler triggered when the quantity is changed
+	 */
 	onChange: ( newQuantity: number ) => void;
+	/**
+	 * Name of the item the quantity selector refers to
+	 *
+	 * Used for a11y purposes
+	 */
 	itemName?: string;
+	/**
+	 * Whether the component should be interactable or not
+	 */
 	disabled: boolean;
 }
 
@@ -27,9 +52,7 @@ const QuantitySelector = ( {
 	quantity = 1,
 	minimum = 1,
 	maximum,
-	onChange = () => {
-		/* Do nothing. */
-	},
+	onChange = () => void 0,
 	itemName = '',
 	disabled,
 }: QuantitySelectorProps ): JSX.Element => {
