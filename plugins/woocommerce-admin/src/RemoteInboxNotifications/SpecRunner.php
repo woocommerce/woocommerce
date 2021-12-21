0ef8959a22a6cced0d085f0e086d906cb51a0f85
Joshua T Flowers <joshuatf@gmail.com>
2021-12-21 10:52:15 -0500

    Allow content data note props to be passed from remote sources (https://github.com/woocommerce/woocommerce-admin/pull/8047)
    * Allow content data note props to be passed from remote sources
    
    * Add changelog entry

--- plugins/woocommerce-admin/src/RemoteInboxNotifications/SpecRunner.php
+++ plugins/woocommerce-admin/src/RemoteInboxNotifications/SpecRunner.php
@@ -59,7 +59,7 @@ class SpecRunner {
 		// Set up the note.
 		$note->set_title( $locale->title );
 		$note->set_content( $locale->content );
-		$note->set_content_data( (object) array() );
+		$note->set_content_data( isset( $spec->content_data ) ? $spec->content_data : (object) array() );
 		$note->set_status( $status );
 		$note->set_type( $spec->type );
 		$note->set_name( $spec->slug );
