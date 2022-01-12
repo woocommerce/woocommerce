a5f5c8de77889420c5d3f046db1faddacca0c934
Luigi Teschio <gigitux@gmail.com>
2022-01-12 16:31:25 +0100

    Summary Product block: Add support for global style (https://github.com/woocommerce/woocommerce-blocks/pull/5524)
    * Product title: add support global style woocommerce/woocommerce-blocks#4965
    
    * add specific type
    
    * add custom save function
    
    * Summary Product block: add support for global style woocommerce/woocommerce-blocks#4965
    
    Summary Product block: add support for global style
    
    * add color global style under feature flag
    
    * fix import after merge
    
    * fix typo

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/index.js
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/index.js
@@ -9,18 +9,23 @@ import { registerBlockType } from '@wordpress/blocks';
 import sharedConfig from '../shared/config';
 import attributes from './attributes';
 import edit from './edit';
+import { supports } from './supports';
 import {
 	BLOCK_TITLE as title,
 	BLOCK_ICON as icon,
 	BLOCK_DESCRIPTION as description,
 } from './constants';
+import { Save } from './save';
 
 const blockConfig = {
+	apiVersion: 2,
 	title,
 	description,
 	icon: { src: icon },
 	attributes,
+	supports,
 	edit,
+	save: Save,
 };
 
 registerBlockType( 'woocommerce/product-summary', {
