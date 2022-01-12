d441fc8161243ce836ace1a47c3994839dabd651
Alex Florisca <alex.florisca@automattic.com>
2022-01-12 17:50:48 +0000

    Default all product images to "cropped" and actual image width (https://github.com/woocommerce/woocommerce-blocks/pull/5551)
    * Default imageSizing for the all products block to 'cropped'
    
    * set max-width:100%, width: auto on product grid images

--- plugins/woocommerce-blocks/assets/js/blocks/products/base-utils.js
+++ plugins/woocommerce-blocks/assets/js/blocks/products/base-utils.js
@@ -2,7 +2,7 @@
  * The default layout built from the default template.
  */
 export const DEFAULT_PRODUCT_LIST_LAYOUT = [
-	[ 'woocommerce/product-image' ],
+	[ 'woocommerce/product-image', { imageSizing: 'cropped' } ],
 	[ 'woocommerce/product-title' ],
 	[ 'woocommerce/product-price' ],
 	[ 'woocommerce/product-rating' ],
