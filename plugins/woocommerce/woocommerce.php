c9a0686e81a650478e4fa7a3d1d50fd9abc6a6eb
Claudio Sanches <contato@claudiosanches.com>
2022-01-13 20:28:58 -0300

    Prepares trunk to 6.3.0-dev and update changelog

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
- * Version: 6.1.0-dev
+ * Version: 6.3.0-dev
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
