17905f29e6b409dca1087fdb01448e470bf184cd
jonathansadowski <jonathansadowski@users.noreply.github.com>
2022-01-18 13:15:32 -0600

    Add `@see` tag to hint at usage of `wp_doing_ajax`

--- plugins/woocommerce/includes/wc-conditional-functions.php
+++ plugins/woocommerce/includes/wc-conditional-functions.php
@@ -261,6 +261,7 @@ if ( ! function_exists( 'is_ajax' ) ) {
 	/**
 	 * Is_ajax - Returns true when the page is loaded via ajax.
 	 *
+	 * @see wp_doing_ajax() for an equivalent function provided by WordPress since 4.7.0
 	 * @return bool
 	 */
 	function is_ajax() {
