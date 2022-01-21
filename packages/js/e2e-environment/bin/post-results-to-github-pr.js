3749ada6df4b15de149af27ae22cbc96e40081aa
Jon Lane <jon.lane@automattic.com>
2022-01-21 15:47:32 -0800

    Results path

--- packages/js/e2e-environment/bin/post-results-to-github-pr.js
+++ packages/js/e2e-environment/bin/post-results-to-github-pr.js
@@ -1,10 +1,8 @@
-#!/usr/bin/env node
-
 const fs = require('fs');
 const path = require( 'path' );
 const { resolveLocalE2ePath } = require( '../utils' );
 
-const resultsFile = resolveLocalE2ePath( 'test-results.json' );
+const resultsFile = resolveLocalE2ePath( '/test-results.json' );
 
 const buildOutput = ( results ) => {
 	const { TITLE, SMOKE_TEST_URL } = process.env;
