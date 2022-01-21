2565df378e8a9a8102c3419f1485bb9419d1a662
Nestor Soriano <konamiman@konamiman.com>
2022-01-21 15:50:02 +0100

    Add scafolding for the custom orders table feature.
    - Add the src/Internal/DataStores/Orders, with the appropriate
      class files.
    
    - Add an entry in the tools page to initiate the (re)generation
      of the table data (does nothing for now).
    
    - Add a new data store class (empty for now).

--- plugins/woocommerce/src/Container.php
+++ plugins/woocommerce/src/Container.php
@@ -8,6 +8,7 @@ namespace Automattic\WooCommerce;
 use Automattic\WooCommerce\Internal\DependencyManagement\ExtendedContainer;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\DownloadPermissionsAdjusterServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\AssignDefaultCategoryServiceProvider;
+use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\OrdersDataStoreServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\ProductAttributesLookupServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\ProxiesServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\RestockRefundedItemsAdjusterServiceProvider;
@@ -38,6 +39,7 @@ final class Container implements \Psr\Container\ContainerInterface {
 	private $service_providers = array(
 		AssignDefaultCategoryServiceProvider::class,
 		DownloadPermissionsAdjusterServiceProvider::class,
+		OrdersDataStoreServiceProvider::class,
 		ProductAttributesLookupServiceProvider::class,
 		ProxiesServiceProvider::class,
 		RestockRefundedItemsAdjusterServiceProvider::class,
