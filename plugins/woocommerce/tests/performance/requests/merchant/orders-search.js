d0f70804db840cd0aaf1b07e575223ce09be99ad
Veljko V <veljano@yahoo.com>
2022-01-24 15:33:52 +0100

    Add missing product_search_term in import
    Added a missing product_search_term in the imports

--- plugins/woocommerce/tests/performance/requests/merchant/orders-search.js
+++ plugins/woocommerce/tests/performance/requests/merchant/orders-search.js
@@ -2,7 +2,12 @@ import { sleep, check, group } from "k6";
 import http from "k6/http";
 import { Trend } from "k6/metrics";
 import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.1.0/index.js";
-import { base_url, think_time_min, think_time_max } from "../../config.js";
+import {
+	base_url,
+	think_time_min,
+	think_time_max,
+	product_search_term,
+} from "../../config.js";
 import {
 	htmlRequestHeader,
 	commonRequestHeaders,
