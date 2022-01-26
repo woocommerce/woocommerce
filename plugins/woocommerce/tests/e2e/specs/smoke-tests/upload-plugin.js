a631454b91606eff34ab80fd762e13993bec3ae8
jamelreid <jnoelreid@gmail.com>
2022-01-26 11:53:43 -0500

    Updated describe block for better logging

--- plugins/woocommerce/tests/e2e/specs/smoke-tests/upload-plugin.js
+++ plugins/woocommerce/tests/e2e/specs/smoke-tests/upload-plugin.js
@@ -18,25 +18,30 @@ const { GITHUB_REPOSITORY, PLUGIN_NAME, GITHUB_TOKEN } = process.env;
 let zipUrl;
 let pluginPath;
 
-utils.describeIf( GITHUB_REPOSITORY )( 'Upload and activate plugin', () => {
-	beforeAll( async () => {
-		zipUrl = await getLatestReleaseZipUrl( GITHUB_REPOSITORY, GITHUB_TOKEN );
+utils.describeIf( GITHUB_REPOSITORY )(
+	`Upload and activate ${ PLUGIN_NAME } from ${ GITHUB_REPOSITORY }`,
+	() => {
+		beforeAll( async () => {
+			zipUrl = await getLatestReleaseZipUrl(
+				GITHUB_REPOSITORY,
+				GITHUB_TOKEN
+			);
 
-		pluginPath = await getRemotePluginZip( zipUrl, GITHUB_TOKEN );
+			pluginPath = await getRemotePluginZip( zipUrl, GITHUB_TOKEN );
 
-		await merchant.login();
-	});
+			await merchant.login();
+		} );
 
-	afterAll( async () => {
-		await merchant.logout();
-	});
+		afterAll( async () => {
+			await merchant.logout();
+		} );
 
-	it( 'can upload and activate the provided plugin', async () => {
-		await merchant.uploadAndActivatePlugin( pluginPath, PLUGIN_NAME );
-	});
+		it( 'can upload and activate the provided plugin', async () => {
+			await merchant.uploadAndActivatePlugin( pluginPath, PLUGIN_NAME );
+		} );
 
-	it( 'can remove downloaded plugin zip', async () => {
-		await deleteDownloadedPluginFiles();
-	} );
-
-});
+		it( 'can remove downloaded plugin zip', async () => {
+			await deleteDownloadedPluginFiles();
+		} );
+	}
+);
