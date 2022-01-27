745c28e9b2d5d07fdabbf3e2edeb8cd6586f6571
Barry Hughes <3594411+barryhughes@users.noreply.github.com>
2022-01-27 15:33:08 -0800

    Add docblock for revised filter.

--- plugins/woocommerce/includes/admin/importers/class-wc-product-csv-importer-controller.php
+++ plugins/woocommerce/includes/admin/importers/class-wc-product-csv-importer-controller.php
@@ -92,6 +92,12 @@ class WC_Product_CSV_Importer_Controller {
 	 * @return bool
 	 */
 	public static function is_file_valid_csv( $file, $check_path = true ) {
+		/**
+		 * Can be used to override the decision to check the import file path.
+		 *
+		 * @param bool   $check_import_file_path If the import file path should be checked.
+		 * @param string $file                   Path of the file to be checked.
+		 */
 		if ( $check_path && apply_filters( 'woocommerce_product_csv_importer_check_import_file_path', true, $file ) && false !== stripos( $file, '://' ) ) {
 			return false;
 		}
