b55d370779b6531524958e5961b91fd956a176b7
Mike Jolley <mike.jolley@me.com>
2022-01-28 14:34:36 +0000

    Fix devtool config broken in woocommerce/woocommerce-blocks#5589

--- plugins/woocommerce-blocks/webpack.config.js
+++ plugins/woocommerce-blocks/webpack.config.js
@@ -30,7 +30,7 @@ const sharedConfig = {
 	watchOptions: {
 		ignored: /node_modules/,
 	},
-	devtool: NODE_ENV === 'development',
+	devtool: NODE_ENV === 'development' ? 'source-map' : false,
 };
 
 // Core config for shared libraries.
