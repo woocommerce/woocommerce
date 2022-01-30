75bc438973e506c2cb5b18f7c7dede2a303a7e3e
Joshua T Flowers <joshuatf@gmail.com>
2022-01-30 10:28:40 -0500

    Update store setup widget to use task list API (#31755)
    * Hide store setup widget if user cannot manage woocommerce
    
    * Use task list API to retrieve tasks
    
    * Fix up URL and URL fragments from tasks
    
    * Remove URL assertion on external API response
    
    * Simplify task count and completed count test
    
    * Update tests around widget visibility
    
    * Update step number to be completed count +1

--- plugins/woocommerce/includes/admin/class-wc-admin-dashboard-setup.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-dashboard-setup.php
@@ -7,6 +7,7 @@
  */
 
 use Automattic\Jetpack\Constants;
+use Automattic\WooCommerce\Admin\Features\OnboardingTasks\TaskLists;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -20,40 +21,14 @@ if ( ! class_exists( 'WC_Admin_Dashboard_Setup', false ) ) :
 	class WC_Admin_Dashboard_Setup {
 
 		/**
-		 * List of tasks.
-		 *
-		 * @var array
+		 * The task list.
 		 */
-		private $tasks = array(
-			'store_details'        => array(
-				'completed'   => false,
-				'button_link' => 'admin.php?page=wc-admin&path=%2Fsetup-wizard',
-			),
-			'products'             => array(
-				'completed'   => false,
-				'button_link' => 'admin.php?page=wc-admin&task=products',
-			),
-			'woocommerce-payments' => array(
-				'completed'   => false,
-				'button_link' => 'admin.php?page=wc-admin&path=%2Fpayments%2Fconnect',
-			),
-			'payments'             => array(
-				'completed'   => false,
-				'button_link' => 'admin.php?page=wc-admin&task=payments',
-			),
-			'tax'                  => array(
-				'completed'   => false,
-				'button_link' => 'admin.php?page=wc-admin&task=tax',
-			),
-			'shipping'             => array(
-				'completed'   => false,
-				'button_link' => 'admin.php?page=wc-admin&task=shipping',
-			),
-			'appearance'           => array(
-				'completed'   => false,
-				'button_link' => 'admin.php?page=wc-admin&task=appearance',
-			),
-		);
+		private $task_list = null;
+
+		/**
+		 * The tasks.
+		 */
+		private $tasks = null;
 
 		/**
 		 * # of completed tasks.
@@ -67,9 +42,6 @@ if ( ! class_exists( 'WC_Admin_Dashboard_Setup', false ) ) :
 		 */
 		public function __construct() {
 			if ( $this->should_display_widget() ) {
-				$this->populate_general_tasks();
-				$this->populate_payment_tasks();
-				$this->completed_tasks_count = $this->get_completed_tasks_count();
 				add_meta_box(
 					'wc_admin_dashboard_setup',
 					__( 'WooCommerce Setup', 'woocommerce' ),
@@ -93,9 +65,10 @@ if ( ! class_exists( 'WC_Admin_Dashboard_Setup', false ) ) :
 				return;
 			}
 
-			$button_link           = $task['button_link'];
-			$completed_tasks_count = $this->completed_tasks_count;
-			$tasks_count           = count( $this->tasks );
+			$button_link           = $this->get_button_link( $task );
+			$completed_tasks_count = $this->get_completed_tasks_count();
+			$step_number           = $this->get_completed_tasks_count() + 1;
+			$tasks_count           = count( $this->get_tasks() );
 
 			// Given 'r' (circle element's r attr), dashoffset = ((100-$desired_percentage)/100) * PI * (r*2).
 			$progress_percentage = ( $completed_tasks_count / $tasks_count ) * 100;
@@ -106,35 +79,68 @@ if ( ! class_exists( 'WC_Admin_Dashboard_Setup', false ) ) :
 		}
 
 		/**
-		 * Populate tasks from the database.
+		 * Get the button link for a given task.
+		 *
+		 * @param Task $task Task.
+		 * @return string
 		 */
-		private function populate_general_tasks() {
-			$tasks = get_option( 'woocommerce_task_list_tracked_completed_tasks', array() );
-			foreach ( $tasks as $task ) {
-				if ( isset( $this->tasks[ $task ] ) ) {
-					$this->tasks[ $task ]['completed']   = true;
-					$this->tasks[ $task ]['button_link'] = wc_admin_url( $this->tasks[ $task ]['button_link'] );
-				}
+		public function get_button_link( $task ) {
+			$url = $task->get_json()['actionUrl'];
+
+			if ( substr( $url, 0, 4 ) === 'http' ) {
+				return $url;
+			} elseif ( $url ) {
+				return wc_admin_url( '&path=' . $url );
 			}
+
+			return admin_url( 'admin.php?page=wc-admin&task=' . $task->get_id() );
 		}
 
 		/**
-		 * Getter for $tasks
+		 * Get the task list.
+		 *
+		 * @return array
+		 */
+		public function get_task_list() {
+			if ( $this->task_list ) {
+				return $this->task_list;
+			}
+
+			$this->set_task_list( TaskLists::get_list( 'setup' ) );
+			return $this->task_list;
+		}
+
+		/**
+		 * Set the task list.
+		 */
+		public function set_task_list( $task_list ) {
+			return $this->task_list = $task_list;
+		}
+
+		/**
+		 * Get the tasks.
 		 *
 		 * @return array
 		 */
 		public function get_tasks() {
+			if ( $this->tasks ) {
+				return $this->tasks;
+			}
+
+			$this->tasks = $this->get_task_list()->get_viewable_tasks();
 			return $this->tasks;
 		}
 
 		/**
 		 * Return # of completed tasks
+		 *
+		 * @return integer
 		 */
 		public function get_completed_tasks_count() {
 			$completed_tasks = array_filter(
-				$this->tasks,
+				$this->get_tasks(),
 				function( $task ) {
-					return $task['completed'];
+					return $task->is_complete();
 				}
 			);
 
@@ -148,7 +154,7 @@ if ( ! class_exists( 'WC_Admin_Dashboard_Setup', false ) ) :
 		 */
 		private function get_next_task() {
 			foreach ( $this->get_tasks() as $task ) {
-				if ( false === $task['completed'] ) {
+				if ( false === $task->is_complete() ) {
 					return $task;
 				}
 			}
@@ -161,51 +167,13 @@ if ( ! class_exists( 'WC_Admin_Dashboard_Setup', false ) ) :
 		 *
 		 * @return bool
 		 */
-		private function should_display_widget() {
-			return WC()->is_wc_admin_active() &&
-				'yes' !== get_option( 'woocommerce_task_list_complete' ) &&
-				'yes' !== get_option( 'woocommerce_task_list_hidden' );
+		public function should_display_widget() {
+			return current_user_can( 'manage_woocommerce' ) &&
+				WC()->is_wc_admin_active() &&
+				! $this->get_task_list()->is_complete() &&
+				! $this->get_task_list()->is_hidden();
 		}
 
-		/**
-		 * Populate payment tasks's visibility and completion
-		 */
-		private function populate_payment_tasks() {
-			$is_woo_payment_installed = is_plugin_active( 'woocommerce-payments/woocommerce-payments.php' );
-			$country                  = explode( ':', get_option( 'woocommerce_default_country', 'US:CA' ) )[0];
-
-			// woocommerce-payments requires its plugin activated and country must be US.
-			if ( ! $is_woo_payment_installed || 'US' !== $country ) {
-				unset( $this->tasks['woocommerce-payments'] );
-			}
-
-			// payments can't be used when woocommerce-payments exists and country is US.
-			if ( $is_woo_payment_installed && 'US' === $country ) {
-				unset( $this->tasks['payments'] );
-			}
-
-			if ( isset( $this->tasks['payments'] ) ) {
-				$gateways                             = WC()->payment_gateways->get_available_payment_gateways();
-				$enabled_gateways                     = array_filter(
-					$gateways,
-					function ( $gateway ) {
-						return 'yes' === $gateway->enabled;
-					}
-				);
-				$this->tasks['payments']['completed'] = ! empty( $enabled_gateways );
-			}
-
-			if ( isset( $this->tasks['woocommerce-payments'] ) ) {
-				$wc_pay_is_connected = false;
-				if ( class_exists( '\WC_Payments' ) ) {
-					$wc_payments_gateway = \WC_Payments::get_gateway();
-					$wc_pay_is_connected = method_exists( $wc_payments_gateway, 'is_connected' )
-						? $wc_payments_gateway->is_connected()
-						: false;
-				}
-				$this->tasks['woocommerce-payments']['completed'] = $wc_pay_is_connected;
-			}
-		}
 	}
 
 endif;
