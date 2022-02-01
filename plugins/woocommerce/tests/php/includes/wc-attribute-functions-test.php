0fc4a30232f8945cb668b0825c8ee11dff7db7d7
Saggre <sakri.koskimies@hotmail.com>
2022-02-01 16:21:10 +0200

    Add wc_create_attribute tests

--- plugins/woocommerce/tests/php/includes/wc-attribute-functions-test.php
+++ plugins/woocommerce/tests/php/includes/wc-attribute-functions-test.php
@@ -114,6 +114,61 @@ class WC_Attribute_Functions_Test extends \WC_Unit_Test_Case {
 		);
 	}
 
+	/**
+	 * Test wc_create_attribute() function.
+	 */
+	public function test_wc_create_attribute() {
+		$ids = array();
+
+		$ids[] = wc_create_attribute( array( 'name' => 'Brand' ) );
+		$this->assertInternalType(
+			'int',
+			end( $ids ),
+			'wc_create_attribute should return a numeric id on success.'
+		);
+
+		$ids[] = wc_create_attribute( array( 'name' => str_repeat( 'n', 28 ) ) );
+		$this->assertInternalType(
+			'int',
+			end( $ids ),
+			'Attribute creation should succeed when its slug is 28 characters long.'
+		);
+
+		$err = wc_create_attribute( array() );
+		$this->assertEquals(
+			'missing_attribute_name',
+			$err->get_error_code(),
+			'Attributes should not be allowed to be created without specifying a name.'
+		);
+
+		$err = wc_create_attribute( array( 'name' => str_repeat( 'n', 29 ) ) );
+		$this->assertEquals(
+			'invalid_product_attribute_slug_too_long',
+			$err->get_error_code(),
+			'Attribute slugs should not be allowed to be over 28 characters long.'
+		);
+
+		$err = wc_create_attribute( array( 'name' => 'Cat' ) );
+		$this->assertEquals(
+			'invalid_product_attribute_slug_reserved_name',
+			$err->get_error_code(),
+			'Attributes should not be allowed to be created with reserved names.'
+		);
+
+		register_taxonomy( 'pa_brand', array( 'product' ), array( 'labels' => array( 'name' => 'Brand' ) ) );
+		$err = wc_create_attribute( array( 'name' => 'Brand' ) );
+		$this->assertEquals(
+			'invalid_product_attribute_slug_already_exists',
+			$err->get_error_code(),
+			'Duplicate attribute slugs should not be allowed to exist.'
+		);
+		unregister_taxonomy( 'pa_brand' );
+
+		foreach ( $ids as $id ) {
+			wc_delete_attribute( $id );
+		}
+	}
+
 	public function get_attribute_names_and_slugs() {
 		return [
 			[ 'Dash Me', 'dash-me' ],
