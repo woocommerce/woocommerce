608287db42f22bb741cbca61a14ce00bf7efa02d
Luigi Teschio <gigitux@gmail.com>
2022-02-01 15:01:31 +0100

    Featured Product: Enable global style (https://github.com/woocommerce/woocommerce-blocks/pull/5555)
    * Product title: add support global style woocommerce/woocommerce-blocks#4965
    
    * add specific type
    
    * add custom save function
    
    * move hooks in a specific folder
    
    * fix crash on WP 5.8
    
    * Featured Category block: Add support for global style woocommerce/woocommerce-blocks#4965
    
    Featured Category block: Add support for global style
    
    * fix border color
    
    * Featured Product block: enable global style woocommerce/woocommerce-blocks#4965
    
    Featured Product block: enable global style
    
    * fix border color
    
    * fix eslint error
    
    * fix opacity
    
    * fix border radius
    
    * fix opacity
    
    * disable font-size support
    
    * restore height for featured product and featured category

--- plugins/woocommerce-blocks/assets/js/blocks/featured-product/index.js
+++ plugins/woocommerce-blocks/assets/js/blocks/featured-product/index.js
@@ -6,19 +6,20 @@ import { InnerBlocks } from '@wordpress/block-editor';
 import { registerBlockType } from '@wordpress/blocks';
 import { getSetting } from '@woocommerce/settings';
 import { Icon, star } from '@woocommerce/icons';
+import { isFeaturePluginBuild } from '@woocommerce/block-settings';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
-import './editor.scss';
 import { example } from './example';
-import Block from './block';
+import { Edit } from './edit';
 
 /**
  * Register and run the "Featured Product" block.
  */
 registerBlockType( 'woocommerce/featured-product', {
+	apiVersion: 2,
 	title: __( 'Featured Product', 'woo-gutenberg-products-block' ),
 	icon: {
 		src: (
@@ -37,6 +38,15 @@ registerBlockType( 'woocommerce/featured-product', {
 	supports: {
 		align: [ 'wide', 'full' ],
 		html: false,
+		color: true,
+		...( isFeaturePluginBuild() && {
+			__experimentalBorder: {
+				color: true,
+				radius: true,
+				width: true,
+				__experimentalSkipSerialization: false,
+			},
+		} ),
 	},
 	example,
 	attributes: {
@@ -154,9 +164,7 @@ registerBlockType( 'woocommerce/featured-product', {
 	 *
 	 * @param {Object} props Props to pass to block.
 	 */
-	edit( props ) {
-		return <Block { ...props } />;
-	},
+	edit: Edit,
 
 	/**
 	 * Block content is rendered in PHP, not via save function.
