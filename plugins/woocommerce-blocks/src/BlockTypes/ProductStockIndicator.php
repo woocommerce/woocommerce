f00fdb702a140a423d2edb7b5577db28b001a794
Luigi Teschio <gigitux@gmail.com>
2022-02-02 10:20:17 +0100

    Fix 404 HTTP error for Product Title, Product Summary, and Product Stock indicator blocks woocommerce/woocommerce-blocks#5468 (https://github.com/woocommerce/woocommerce-blocks/pull/5649)
    Fix 404 HTTP error for Product Title, Product Summary, and Product Stock indicator blocks

--- plugins/woocommerce-blocks/src/BlockTypes/ProductStockIndicator.php
+++ plugins/woocommerce-blocks/src/BlockTypes/ProductStockIndicator.php
@@ -21,6 +21,21 @@ class ProductStockIndicator extends AbstractBlock {
 	protected $api_version = '2';
 
 	/**
+	 * Get the editor script handle for this block type.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 * @return array|string;
+	 */
+	protected function get_block_type_editor_script( $key = null ) {
+		$script = [
+			'handle'       => 'wc-' . $this->block_name . '-block',
+			'path'         => $this->asset_api->get_block_asset_build_path( 'atomic-block-components/stock-indicator' ),
+			'dependencies' => [ 'wc-blocks' ],
+		];
+		return $key ? $script[ $key ] : $script;
+	}
+
+	/**
 	 * Get block supports. Shared with the frontend.
 	 * IMPORTANT: If you change anything here, make sure to update the JS file too.
 	 *
