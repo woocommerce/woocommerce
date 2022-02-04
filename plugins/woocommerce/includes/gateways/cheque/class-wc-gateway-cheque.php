a3cbbfe1319a6375aa1161072c9f6eee8d91e098
Fernando Marichal <contacto@fernandomarichal.com>
2022-02-04 10:22:52 -0300

    Add `woocommerce_update_options_payment_gateways_` action

--- plugins/woocommerce/includes/gateways/cheque/class-wc-gateway-cheque.php
+++ plugins/woocommerce/includes/gateways/cheque/class-wc-gateway-cheque.php
@@ -41,6 +41,7 @@ class WC_Gateway_Cheque extends WC_Payment_Gateway {
 		$this->instructions = $this->get_option( 'instructions' );
 
 		// Actions.
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'add_admin_options_tracking' ) );
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
 		add_action( 'woocommerce_thankyou_cheque', array( $this, 'thankyou_page' ) );
 
--- plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
+++ plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
@@ -70,6 +70,8 @@ class WC_Gateway_Paypal extends WC_Payment_Gateway {
 			$this->description  = trim( $this->description );
 		}
 
+		// Actions.
+		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'add_admin_options_tracking' ) );
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
 		add_action( 'woocommerce_order_status_processing', array( $this, 'capture_payment' ) );
 		add_action( 'woocommerce_order_status_completed', array( $this, 'capture_payment' ) );
