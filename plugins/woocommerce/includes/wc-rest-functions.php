8e28ee15d5720f08a7d2b08ebad182f0fa094e64
barryhughes <3594411+barryhughes@users.noreply.github.com>
2022-02-08 14:48:04 -0800

    Further adjust permissions for reviews (REST API).

--- plugins/woocommerce/includes/wc-rest-functions.php
+++ plugins/woocommerce/includes/wc-rest-functions.php
@@ -342,14 +342,22 @@ function wc_rest_check_product_reviews_permissions( $context = 'read', $object_i
 	$permission = false;
 	$contexts   = array(
 		'read'   => 'moderate_comments',
-		'create' => 'moderate_comments',
-		'edit'   => 'moderate_comments',
-		'delete' => 'moderate_comments',
-		'batch'  => 'moderate_comments',
+		'create' => 'edit_products',
+		'edit'   => 'edit_comment',
+		'delete' => 'edit_comment',
+		'batch'  => 'edit_products',
 	);
 
+	if ( $object_id > 0 ) {
+		$object = get_comment( $object_id );
+
+		if ( ! is_a( $object, 'WP_Comment' ) || $object->comment_type !== 'review' ) {
+			return false;
+		}
+	}
+
 	if ( isset( $contexts[ $context ] ) ) {
-		$permission = current_user_can( $contexts[ $context ] );
+		$permission = current_user_can( $contexts[ $context ], $object_id );
 	}
 
 	return apply_filters( 'woocommerce_rest_check_permissions', $permission, $context, $object_id, 'product_review' );
