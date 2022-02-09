37eb783774e5f7799e3ac3e93522fc502eb267f4
Luigi Teschio <gigitux@gmail.com>
2022-02-09 10:51:30 +0100

    Set focus on Empty Mini Cart Contents Block (https://github.com/woocommerce/woocommerce-blocks/pull/5744)
    * Set focus on Empty Mini Cart Contents Block woocommerce/woocommerce-blocks#4618
    
    Set focus on Empty Mini Cart Contents Block
    
    * fix useEffect logic

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/mini-cart-contents/inner-blocks/empty-mini-cart-contents-block/frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/mini-cart-contents/inner-blocks/empty-mini-cart-contents-block/frontend.tsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { useStoreCart } from '@woocommerce/base-context/hooks';
+import { useEffect, useRef } from 'react';
 
 /**
  * Internal dependencies
@@ -16,12 +17,24 @@ const EmptyMiniCartContentsBlock = ( {
 }: EmptyMiniCartContentsBlockProps ): JSX.Element | null => {
 	const { cartItems, cartIsLoading } = useStoreCart();
 
+	const elementRef = useRef< HTMLDivElement >( null );
+
+	useEffect( () => {
+		if ( cartItems.length === 0 && ! cartIsLoading ) {
+			elementRef.current?.focus();
+		}
+	}, [ cartItems, cartIsLoading ] );
+
 	if ( cartIsLoading || cartItems.length > 0 ) {
 		return null;
 	}
 
 	return (
-		<div className="wp-block-woocommerce-empty-mini-cart-contents-block">
+		<div
+			tabIndex={ -1 }
+			ref={ elementRef }
+			className="wp-block-woocommerce-empty-mini-cart-contents-block"
+		>
 			{ children }
 		</div>
 	);
