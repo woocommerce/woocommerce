18a5d3a751f5ca9aff524a8a177c66501c19f004
Jonathan Sadowski <sadowski@automattic.com>
2022-02-10 08:51:26 -0600

    Remove confusing trailing dots near version numbers; use PHP_EOL; add newline to EOF

--- .github/workflows/scripts/release-code-freeze.php
+++ .github/workflows/scripts/release-code-freeze.php
@@ -19,14 +19,14 @@ $release_day_of_month = (int) date( 'j', $release_time );
 
 // If 26 days from now isn't the second Tuesday, then it's not code freeze day.
 if ( 'Tuesday' !== $release_day_of_week || $release_day_of_month < 8 || $release_day_of_month > 14 ) {
-	echo "Info: Today is not the Thursday of the code freeze.\n";
+	echo 'Info: Today is not the Thursday of the code freeze.' . PHP_EOL;
 	return;
 }
 
 $latest_milestone = get_latest_milestone_from_api();
 
 if ( is_null( $latest_milestone ) ) {
-	echo "*** Error: Unable to get latest milestone\n";
+	echo '*** Error: Unable to get latest milestone' . PHP_EOL;
 	return;
 }
 
@@ -42,20 +42,20 @@ $release_branch_to_create = "release/{$latest_major_minor}";
 $milestone_to_create      = "{$next_major_minor}.0";
 
 if ( getenv( 'DRY_RUN' ) ) {
-	echo "DRY RUN: Skipping actual creation of release branch and milestone...\n";
-	echo "Release Branch: {$release_branch_to_create}\n";
-	echo "Milestone: {$milestone_to_create}\n";
+	echo 'DRY RUN: Skipping actual creation of release branch and milestone...' . PHP_EOL;
+	echo "Release Branch: {$release_branch_to_create}" . PHP_EOL;
+	echo "Milestone: {$milestone_to_create}" . PHP_EOL;
 	return;
 }
 
 if ( create_github_milestone( $milestone_to_create ) ) {
-	echo "Created milestone {$milestone_to_create}.\n";
+	echo "Created milestone {$milestone_to_create}" . PHP_EOL;
 } else {
-	echo "*** Error: Unable to create {$milestone_to_create} milestone.\n";
+	echo "*** Error: Unable to create {$milestone_to_create} milestone" . PHP_EOL;
 }
 
 if ( create_github_branch_from_branch( 'trunk', $release_branch_to_create ) ) {
-	echo "Created branch {$release_branch_to_create}.\n";
+	echo "Created branch {$release_branch_to_create}" . PHP_EOL;
 } else {
-	echo "*** Error: Unable to create {$release_branch_to_create}.\n";
+	echo "*** Error: Unable to create {$release_branch_to_create}" . PHP_EOL;
 }
