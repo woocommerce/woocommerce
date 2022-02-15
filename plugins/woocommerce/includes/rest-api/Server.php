ceb8206b283e9e4b1ad82796e9300e54c0a7c531
Josh Betz <j@joshbetz.com>
2022-02-15 09:14:49 -0600

    lint
    Older versions of PHP don't support arrow functions

--- plugins/woocommerce/includes/rest-api/Server.php
+++ plugins/woocommerce/includes/rest-api/Server.php
@@ -31,7 +31,10 @@ class Server {
 		add_action( 'rest_api_init', array( $this, 'register_rest_routes' ), 10 );
 
 		// Set up a cart with an in-memory store (instead of storing in a cookie).
-		add_filter( 'woocommerce_session_handler', fn() => 'WC_Session' );
+		add_filter( 'woocommerce_session_handler', function() {
+			return 'WC_Session';
+		});
+
 		wc_load_cart();
 	}
 
