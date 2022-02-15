d77634c372afef7dfbcd94f9d8fe26ab7e94ad4f
Luigi Teschio <gigitux@gmail.com>
2022-02-15 15:48:45 +0100

    Rating Product block: Add support for global style (https://github.com/woocommerce/woocommerce-blocks/pull/5521)
    * Product title: add support global style woocommerce/woocommerce-blocks#4965
    
    * add specific type
    
    * add custom save function
    
    * Rating Product block: add support for global style woocommerce/woocommerce-blocks#4965
    
    Rating Product block: add support for global style
    
    * add feature flag
    
    * fix import after merge
    
    * fix global style css generation
    
    * restore css for classic product rating html element
    
    * don't apply the global style to PHP blocks
    
    * remove registration

---
+++ plugins/woocommerce-blocks/src/BlockTypes/ProductRating.php
@@ -0,0 +1,56 @@
+<?php
+namespace Automattic\WooCommerce\Blocks\BlockTypes;
+
+/**
+ * ProductRating class.
+ */
+class ProductRating extends AbstractBlock {
+
+	/**
+	 * Block name.
+	 *
+	 * @var string
+	 */
+	protected $block_name = 'product-rating';
+
+	/**
+	 * API version name.
+	 *
+	 * @var string
+	 */
+	protected $api_version = '2';
+
+	/**
+	 * Get the editor script handle for this block type.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 * @return array|string;
+	 */
+	protected function get_block_type_editor_script( $key = null ) {
+		$script = [
+			'handle'       => 'wc-' . $this->block_name . '-block',
+			'path'         => $this->asset_api->get_block_asset_build_path( 'atomic-block-components/rating' ),
+			'dependencies' => [ 'wc-blocks' ],
+		];
+		return $key ? $script[ $key ] : $script;
+	}
+
+	/**
+	 * Get block supports. Shared with the frontend.
+	 * IMPORTANT: If you change anything here, make sure to update the JS file too.
+	 *
+	 * @return array
+	 */
+	protected function get_block_type_supports() {
+		return array(
+			'color'                  =>
+			array(
+				'text'       => true,
+				'background' => false,
+				'link'       => false,
+			),
+			'__experimentalSelector' => '.wc-block-components-product-rating',
+		);
+	}
+
+}
