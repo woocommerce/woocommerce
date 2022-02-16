c17d7a4d195b7de99fe2e909cce054e806db4671
barryhughes <3594411+barryhughes@users.noreply.github.com>
2022-02-16 15:02:43 -0800

    Tweak docblock for `woocommerce_admin_order_should_render_refunds` filter hook.

--- plugins/woocommerce/includes/admin/meta-boxes/views/html-order-items.php
+++ plugins/woocommerce/includes/admin/meta-boxes/views/html-order-items.php
@@ -10,11 +10,11 @@ defined( 'ABSPATH' ) || exit;
 /**
  * Allow plugins to determine whether refunds UI should be rendered in the template.
  *
- * @since 6.3.0
+ * @since 6.4.0
  *
- * @param $order_id The Order ID.
- * @param $order The Order object.
- * @return bool
+ * @param bool     $render_refunds If the refunds UI should be rendered.
+ * @param int      $order_id       The Order ID.
+ * @param WC_Order $order          The Order object.
  */
 $render_refunds = (bool) apply_filters( 'woocommerce_admin_order_should_render_refunds', 0 < $order->get_total() - $order->get_total_refunded() || 0 < absint( $order->get_item_count() - $order->get_item_count_refunded() ), $order->get_id(), $order );
 
