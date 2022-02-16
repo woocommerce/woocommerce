823fc5e13be5bbdcdaad20e2e36c70655b62459a
xristos3490 <MY_NAME@example.com>
2022-02-16 21:56:24 +0200

    Move filter at the top of the template

--- plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-order-items.php
+++ plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-order-items.php
@@ -38,17 +38,6 @@ class WC_Meta_Box_Order_Items {
 		$order = $theorder;
 		$data  = get_post_meta( $post->ID );
 
-		/**
-		 * Allow plugins to determine whether refunds UI should be rendered in the template.
-		 *
-		 * @since 6.3.0
-		 *
-		 * @param $order_id The Order ID.
-		 * @param $order The Order object.
-		 * @return bool
-		 */
-		$should_render_refunds = (bool) apply_filters( 'woocommerce_admin_order_should_render_refunds', 0 < $order->get_total() - $order->get_total_refunded() || 0 < absint( $order->get_item_count() - $order->get_item_count_refunded() ), $order->get_id(), $order );
-
 		include __DIR__ . '/views/html-order-items.php';
 	}
 
