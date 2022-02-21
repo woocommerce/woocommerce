88a7e6665643d4941dbb0cd9589991f5744b6440
Lucio Giannotta <lucio.giannotta@a8c.com>
2022-02-21 11:01:42 +0100

    E2E tests for Single Product Template (https://github.com/woocommerce/woocommerce-blocks/pull/5722)
    * Add empty block theme to mock E2E tests
    * Install empty theme in the test WP instance

--- plugins/woocommerce-blocks/tests/e2e/utils.js
+++ plugins/woocommerce-blocks/tests/e2e/utils.js
@@ -2,13 +2,51 @@
  * External dependencies
  */
 import {
+	disableSiteEditorWelcomeGuide,
 	openGlobalBlockInserter,
 	pressKeyWithModifier,
+	visitAdminPage,
+	visitSiteEditor,
 } from '@wordpress/e2e-test-utils';
+import { getQueryArgs } from '@wordpress/url';
 import { WP_ADMIN_DASHBOARD } from '@woocommerce/e2e-utils';
 
-const INSERTER_SEARCH_SELECTOR =
-	'.components-search-control__input,.block-editor-inserter__search input,.block-editor-inserter__search-input,input.block-editor-inserter__search';
+/**
+ * Internal dependencies
+ */
+import { elementExists, getTextContent } from './page-utils';
+
+/**
+ * @typedef {import('@types/puppeteer').ElementHandle} ElementHandle
+ * @typedef {import('@wordpress/blocks').Block} WPBlock
+ */
+
+/**
+ * @typedef {{ addedBy: string, hasActions: boolean, templateTitle: string }} TemplateTableItem
+ */
+
+export const DEFAULT_TIMEOUT = 30000;
+
+const SELECTORS = {
+	canvas: 'iframe[name="editor-canvas"]',
+	inserter: {
+		search:
+			'.components-search-control__input,.block-editor-inserter__search input,.block-editor-inserter__search-input,input.block-editor-inserter__search',
+	},
+	templatesListTable: {
+		actionsContainer: '.edit-site-list-table__actions',
+		cells: '.edit-site-list-table-column',
+		headings: 'thead th.edit-site-list-table-column',
+		root: '.edit-site-list-table',
+		rows: '.edit-site-list-table-row',
+		templateTitle: '[data-wp-component="Heading"]',
+	},
+	toolbar: {
+		confirmSave: '.editor-entities-saved-states__save-button',
+		saveButton: '.edit-site-save-button__button',
+		savePrompt: '.entities-saved-states__text-prompt',
+	},
+};
 
 /**
  * Search for block in the global inserter.
@@ -18,8 +56,8 @@ const INSERTER_SEARCH_SELECTOR =
  * @param {string} searchTerm The text to search the inserter for.
  */
 export async function searchForBlock( searchTerm ) {
-	await page.waitForSelector( INSERTER_SEARCH_SELECTOR );
-	await page.focus( INSERTER_SEARCH_SELECTOR );
+	await page.waitForSelector( SELECTORS.inserter.search );
+	await page.focus( SELECTORS.inserter.search );
 	await pressKeyWithModifier( 'primary', 'a' );
 	await page.keyboard.type( searchTerm );
 }
@@ -92,3 +130,113 @@ export const isBlockInsertedInWidgetsArea = async ( blockName ) => {
 				.length ) > 0
 	);
 };
+/**
+ * Visits the Site Editor main page in Core WordPress
+ *
+ * There are two different possible site editor pages:
+ *
+ * 1. `themes.php?page=gutenberg-edit-site` is the one used and available if the Gutenberg plugin is enabled.
+ * 2. `site-editor.php` is the one available in WP Core.
+ *
+ * @param {string} query String to be serialized as query portion of URL.
+ * @param {'core' | 'gutenberg'} [editorContext='core'] Whether to go to the Gutenberg URL or the Core one.
+ */
+export async function goToSiteEditor( query, editorContext = 'core' ) {
+	if ( editorContext === 'gutenberg' ) {
+		const queryArgs = getQueryArgs( query );
+
+		await visitSiteEditor( queryArgs );
+	} else {
+		await visitAdminPage( 'site-editor.php', query );
+		await disableSiteEditorWelcomeGuide();
+	}
+}
+
+/**
+ * Waits for the Gutenberg canvas to be available
+ *
+ * @param {number} [timeout=DEFAULT_TIMEOUT] The amount of ms to wait for the element
+ *
+ * @return {Promise<?ElementHandle>} The canvas element handle
+ */
+export function waitForCanvas( timeout = DEFAULT_TIMEOUT ) {
+	return page.waitForSelector( SELECTORS.canvas, { timeout } );
+}
+
+/**
+ * Saves a template
+ */
+export async function saveTemplate() {
+	const { confirmSave, saveButton, savePrompt } = SELECTORS.toolbar;
+
+	await page.click( saveButton );
+	await page.waitForSelector( savePrompt );
+	await page.click( confirmSave );
+	await page.waitForSelector( `${ saveButton }[aria-disabled="true"]` );
+}
+
+/**
+ * Gets all available templates from the template list table UI
+ *
+ * @return {Promise<TemplateTableItem[]>} A promise of an array of informations about the templates extracted from the UI
+ */
+export async function getAllTemplates() {
+	const { templatesListTable } = SELECTORS;
+
+	await page.waitForSelector( templatesListTable.root );
+	const table = await page.$( templatesListTable.root );
+
+	if ( ! table ) throw new Error( 'Templates table not found' );
+
+	const rows = await table.$$( templatesListTable.rows );
+
+	return Promise.all(
+		rows.map( async ( row ) => ( {
+			addedBy: (
+				await getTextContent( templatesListTable.cells, row )
+			 )[ 1 ],
+			hasActions: await elementExists(
+				templatesListTable.actionsContainer,
+				row
+			),
+			templateTitle: (
+				await getTextContent( templatesListTable.templateTitle, row )
+			 )[ 0 ],
+		} ) )
+	);
+}
+
+/**
+ * Gets all the blocks that fulfill a given predicate
+ *
+ * @param {( block: WPBlock ) => boolean} predicate The function invoked per iteration
+ * @return {Promise< Partial< WPBlock >[] >} The blocks which have been found
+ */
+export async function filterCurrentBlocks( predicate ) {
+	/**
+	 * @type {WPBlock[]}
+	 */
+	const blocks = await page.evaluate( () => {
+		/**
+		 * Gets all serializeable data from a block
+		 *
+		 * @param {WPBlock} block A Gutenberg Block
+		 * @return {Partial<WPBlock>} A block with unserializeable values turned to `null`
+		 */
+		function getSerializeableBlockData( block ) {
+			return JSON.parse( JSON.stringify( block ) );
+		}
+
+		const blockEditorStore = window.wp.data.select( 'core/block-editor' );
+		/**
+		 * @type {string[]}
+		 */
+		const allClientIds = blockEditorStore.getClientIdsWithDescendants();
+
+		return allClientIds.map( ( id ) =>
+			getSerializeableBlockData( blockEditorStore.getBlock( id ) )
+		);
+	} );
+
+	return blocks.filter( predicate );
+}
