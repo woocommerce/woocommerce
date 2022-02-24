ed9a9fca55ad660ed94a33dfc1860e048ed15cad
Moon <moon.kyong@automattic.com>
2022-02-24 12:22:01 -0800

    Add constants for the backend

--- src/experiments/data/constants.js
+++ src/experiments/data/constants.js
@@ -1,2 +1,4 @@
 export const STORE_KEY = 'wc-admin-helper/experiments';
 export const EXPERIMENT_NAME_PREFIX = 'explat-experiment--';
+export const TRANSIENT_NAME_PREFIX = '_transient_abtest_variation_';
+export const API_NAMESPACE = '/wc-admin-test-helper';
