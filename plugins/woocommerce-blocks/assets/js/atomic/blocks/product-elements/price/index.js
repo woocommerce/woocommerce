475b50573dccff72c423541b038a5f8096f5aebc
Tomasz Tunik <tomasztunik@gmail.com>
2022-02-28 12:36:55 +0100

    Global Styles support for Product Price block (https://github.com/woocommerce/woocommerce-blocks/pull/5950)
    * Add Global Styles support to product price block
    
    - This comes sat a cost of losing control over regular vs sale price
    - This includes refactor of attribute from align to textAlign
       which is a reserved supports feature attribute and textAlign is
       used across the Gutenberg project
    
    * Add experimentalSelector and register as PHP block
    
    This is required to make the GlobalStyles work inside of
    the InnerContent block. Ie. All Products block.
    
    * move sharedConfig supports outside of isFeaturePluginBuild check
    
    * restore regularPriceClassName
    
    * remove douplicated block registration on php side

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/price/index.js
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/price/index.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { isFeaturePluginBuild } from '@woocommerce/block-settings';
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
@@ -8,6 +9,7 @@ import { registerBlockType } from '@wordpress/blocks';
  */
 import sharedConfig from '../shared/config';
 import edit from './edit';
+import { Save } from './save';
 import attributes from './attributes';
 import {
 	BLOCK_TITLE as title,
@@ -16,14 +18,32 @@ import {
 } from './constants';
 
 const blockConfig = {
+	...sharedConfig,
+	apiVersion: 2,
 	title,
 	description,
 	icon: { src: icon },
 	attributes,
 	edit,
+	save: Save,
+	supports: {
+		...sharedConfig.supports,
+		...( isFeaturePluginBuild() && {
+			color: {
+				text: true,
+				background: false,
+				link: false,
+				__experimentalSkipSerialization: true,
+			},
+			typography: {
+				fontSize: true,
+				__experimentalFontWeight: true,
+				__experimentalFontStyle: true,
+				__experimentalSkipSerialization: true,
+			},
+			__experimentalSelector: '.wc-block-components-product-price',
+		} ),
+	},
 };
 
-registerBlockType( 'woocommerce/product-price', {
-	...sharedConfig,
-	...blockConfig,
-} );
+registerBlockType( 'woocommerce/product-price', blockConfig );
