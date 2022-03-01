22eccc8614a86acea584ced4f99cb5e35f649910
Tung Du <dinhtungdu@gmail.com>
2022-03-01 18:25:13 +0700

    Shopper → Mini Cart → Can view filled mini cart contents block (https://github.com/woocommerce/woocommerce-blocks/pull/5923)

--- plugins/woocommerce-blocks/tests/utils/shopper.js
+++ plugins/woocommerce-blocks/tests/utils/shopper.js
@@ -1,7 +1,11 @@
 /**
  * External dependencies
  */
-import { shopper as wcShopper } from '@woocommerce/e2e-utils';
+import {
+	shopper as wcShopper,
+	uiUnblocked,
+	SHOP_CART_PAGE,
+} from '@woocommerce/e2e-utils';
 
 /**
  * Internal dependencies
@@ -55,5 +59,39 @@ export const shopper = {
 		} );
 
 		await expect( page ).toMatchElement( 'h1', { text: title } );
+	},
+
+	/**
+	 * Override the @woocommerce/e2e-utils `emptyCart` method to fix the
+	 * ReferenceError issue and remove the cart items.
+	 *
+	 * @todo Remove shopper.emptyCart overload once the upstream  is fixed
+	 */
+	emptyCart: async () => {
+		await page.goto( SHOP_CART_PAGE, {
+			waitUntil: 'networkidle0',
+		} );
+
+		// Remove products if they exist
+		if ( ( await page.$( '.remove' ) ) !== null ) {
+			let products = await page.$$( '.remove' );
+			while ( products && products.length > 0 ) {
+				await page.click( '.remove' );
+				await uiUnblocked();
+				products = await page.$$( '.remove' );
+			}
+		}
+
+		// Remove coupons if they exist
+		if ( ( await page.$( '.woocommerce-remove-coupon' ) ) !== null ) {
+			await page.click( '.woocommerce-remove-coupon' );
+			await uiUnblocked();
+		}
+
+		await page.waitForSelector( '.woocommerce-info' );
+		// eslint-disable-next-line jest/no-standalone-expect
+		await expect( page ).toMatchElement( '.woocommerce-info', {
+			text: 'Your cart is currently empty.',
+		} );
 	},
 };
