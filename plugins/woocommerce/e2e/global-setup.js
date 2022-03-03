4eee8d21b0cf511eb3cb065f1a8490a74c8fc351
Jon Lane <jon.lane@automattic.com>
2022-03-03 13:02:13 -0800

    Install Playwright and initial config

---
+++ plugins/woocommerce/e2e/global-setup.js
@@ -0,0 +1,21 @@
+const { chromium } = require('@playwright/test');
+
+module.exports = async config => {
+  const { baseURL } = config.projects[0].use;
+  // Sign in as admin user and save state
+  const browser = await chromium.launch();
+  const adminPage = await browser.newPage();
+  await adminPage.goto(`${baseURL}/wp-admin`);
+  await adminPage.fill('input[name="log"]', 'admin');
+  await adminPage.fill('input[name="pwd"]', 'password');
+  await adminPage.click('text=Log In');
+  await adminPage.context().storageState({ path: 'e2e/storage/adminState.json' });
+  // Sign in as customer user and save state
+  const customerPage = await browser.newPage();
+  await customerPage.goto(`${baseURL}/wp-admin`);
+  await customerPage.fill('input[name="log"]', 'customer');
+  await customerPage.fill('input[name="pwd"]', 'password');
+  await customerPage.click('text=Log In');
+  await customerPage.context().storageState({ path: 'e2e/storage/customerState.json' });
+  await browser.close();
+};
---
+++ plugins/woocommerce/e2e/playwright.config.js
@@ -0,0 +1,36 @@
+// playwright.config.js
+// @ts-check
+const { devices } = require('@playwright/test');
+
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+	timeout: 60000,
+	globalSetup: require.resolve('./global-setup'),
+	testDir: 'tests',
+	reporter: [['list'], ['html', { outputFolder: 'e2e/output' }]],
+	use: {
+	  workers: 3,
+	  retries: 2,
+	  screenshot: 'only-on-failure',
+	  video: 'retain-on-failure',
+	  trace: 'retain-on-failure',
+	  viewport: { width: 1280, height: 720 },
+	  baseURL: 'http://localhost:8084'
+  },
+  projects: [
+    {
+      name: 'Chrome',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'Webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+};
+
+module.exports = config;
