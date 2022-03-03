e5ac9e4ae6be42da226f4a1e33283064acd0d90c
Tom Cafferkey <tjcafferkey@gmail.com>
2022-03-03 14:42:32 +0000

    Track initial sites theme

--- plugins/woocommerce/includes/tracks/events/class-wc-theme-tracking.php
+++ plugins/woocommerce/includes/tracks/events/class-wc-theme-tracking.php
@@ -16,22 +16,40 @@ class WC_Theme_Tracking {
 	 * Init tracking.
 	 */
 	public function init() {
-		add_action( 'switch_theme', array( $this, 'track_theme_activated' ) );
+		$this->track_initial_theme();
+		add_action( 'switch_theme', array( $this, 'track_active_theme' ) );
+	}
+
+	/**
+	 * Tracking the initial theme being used for the first time.
+	 */
+	public function track_initial_theme() {
+		$has_been_initially_tracked = get_option( 'has_tracked_default_theme' );
+
+		if ( $has_been_initially_tracked ) {
+			return;
+		}
+
+		$this->track_active_theme();
+		add_option( 'has_tracked_default_theme', 'true' );
 	}
 
 	/**
 	 * Send a Tracks event when a theme is activated so that we can track active block themes.
 	 */
-	public function track_theme_activated() {
-		if ( ! function_exists( 'wc_current_theme_is_fse_theme' ) ) {
-			return;
+	public function track_active_theme() {
+		$is_block_theme = false;
+		$theme_object = wp_get_theme();
+
+		if ( function_exists( 'wc_current_theme_is_fse_theme' ) ) {
+			$is_block_theme = wc_current_theme_is_fse_theme();
 		}
 
 		$properties = array(
-			'block_theme' => wc_current_theme_is_fse_theme(),
+			'block_theme' => $is_block_theme,
+			'theme_domain' => $theme_object->get( 'TextDomain' ),
 		);
 
 		WC_Tracks::record_event( 'theme_activated', $properties );
 	}
 }
-
