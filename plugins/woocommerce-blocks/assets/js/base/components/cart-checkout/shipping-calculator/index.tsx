6b8ef2773ae669aaf79f7dc39a57d858f612f01a
Thomas Roberts <5656702+opr@users.noreply.github.com>
2022-03-04 17:43:45 +0000

    Try move shipping related dat to a `@wordpress/data` store (https://github.com/woocommerce/woocommerce-blocks/pull/5896)
    * Add address-related items to wc/store/cart data store
    
    * Add useUpdateCustomerData hook
    
    This allows us to have a single hook responsible for updating the customer information on the server.
    
    * Add useUpdateCustomerData hook in Checkout block
    
    * Remove shippingAsBilling from previousCustomerData ref type
    
    * Add useShippingAsBillingCheckbox hook
    
    * Remove checkbox handling from useCheckoutAddress
    
    * Merge with woocommerce/woocommerce-blocks#5810 changes
    
    * Move shipping as billing to checkout state context provider
    
    * Subscribe to changes
    
    * Cache customerDataToUpdate
    
    * Combine customerDataType and customerDataContextType
    
    * Fix notice context
    
    * Clean up inline docs for push changes
    
    * Add useShippingData hook
    
    * Add shipping related selectors to cart store
    
    * Update useShippingDataContext to useCustomerData hook
    
    * Update uses of useShippingDataContext to get data from hook instead
    
    * Remove rogue linebreak
    
    * Re-add linebreak
    
    * Re-add linebreak, remove shippingAsBilling
    
    * Re-add linebreak
    
    * Use useShippingData and useCustomerData instead of context
    
    * Fix fromEntriesPolyfill to use number or undefined as an index option
    
    * Convert derive-selected-shipping-rates to TS
    
    * Add SelectShippingRateType
    
    * Get needsShipping from new hook and not context
    
    * Get address data from useCustomerData instead of useShippingDataContext
    
    * Move selectedRates, selectShippingRate and isSelectingRate
    
    * Remove items from ShippingDatacontext that are available in data stores
    
    * Get shipping data from stores, not context in payment method interface
    
    * Consider shipping rates to be loading if customer data is updating
    
    * Get rates from useShippingData hook instead of context
    
    * Fix incorrect TypeScript types and incorrectly named destructure
    
    * Move useShippingData into shipping folder
    
    * Update tests to mock useShippingData instead of context
    
    * Remove empty string fallback from shipping phone
    
    * Get types from Cart declaration instead of Picking them
    
    Co-authored-by: Mike Jolley <mike.jolley@me.com>

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/shipping-calculator/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/shipping-calculator/index.tsx
@@ -1,8 +1,8 @@
 /**
  * External dependencies
  */
-import { useShippingDataContext } from '@woocommerce/base-context';
 import type { EnteredAddress } from '@woocommerce/settings';
+import { useCustomerData } from '@woocommerce/base-context/hooks';
 
 /**
  * Internal dependencies
@@ -21,7 +21,7 @@ const ShippingCalculator = ( {
 	},
 	addressFields = [ 'country', 'state', 'city', 'postcode' ],
 }: ShippingCalculatorProps ): JSX.Element => {
-	const { shippingAddress, setShippingAddress } = useShippingDataContext();
+	const { shippingAddress, setShippingAddress } = useCustomerData();
 	return (
 		<div className="wc-block-components-shipping-calculator">
 			<ShippingCalculatorAddress
