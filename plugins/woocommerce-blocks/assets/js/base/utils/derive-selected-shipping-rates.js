db1989aefb5c4747de0dabbade2a9a4d29959f88
Thomas Roberts <5656702+opr@users.noreply.github.com>
2022-03-04 14:42:16 +0000

    Remove `fromEntriesPolyfill` (https://github.com/woocommerce/woocommerce-blocks/pull/5981)
    * Remove fromEntriesPolyfill
    
    * Remove further instances of fromEntriesPolyfill

--- plugins/woocommerce-blocks/assets/js/base/utils/derive-selected-shipping-rates.js
+++ plugins/woocommerce-blocks/assets/js/base/utils/derive-selected-shipping-rates.js
@@ -1,16 +1,11 @@
 /**
- * Internal dependencies
- */
-import { fromEntriesPolyfill } from './from-entries-polyfill';
-
-/**
  * Get an array of selected shipping rates keyed by Package ID.
  *
  * @param {Array} shippingRates Array of shipping rates.
  * @return {Object} Object containing the package IDs and selected rates in the format: { [packageId:string]: rateId:string }
  */
 export const deriveSelectedShippingRates = ( shippingRates ) =>
-	fromEntriesPolyfill(
+	Object.fromEntries(
 		shippingRates.map(
 			( { package_id: packageId, shipping_rates: packageRates } ) => [
 				packageId,
