163717e3f1ee8c2b676c714c92a430760d0fa846
vedanshujain <vedanshu.jain.2012@gmail.com>
2022-03-04 12:50:30 +0530

    Add error logger class for custom table migration.

---
+++ plugins/woocommerce/src/Database/Migrations/MigrationErrorLogger.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Error logger for custom table migrations.
+ */
+
+namespace Automattic\WooCommerce\DataBase\Migrations;
+
+/**
+ * Class MigrationErrorLogger.
+ *
+ * Error logging for custom table migrations.
+ *
+ * @package Automattic\WooCommerce\DataBase\Migrations
+ */
+class MigrationErrorLogger extends \WC_Logger {
+
+}
