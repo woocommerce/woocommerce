6df359d7160f486140dcfce8b57c7b03412f5834
Luigi Teschio <gigitux@gmail.com>
2022-03-07 18:39:11 +0100

    Add feature gate for enabling the global styles for Product Sale Badge block only for the feature plugin (https://github.com/woocommerce/woocommerce-blocks/pull/6008)
    * Add feature gate for the global styles for Product Sale Badge block woocommerce/woocommerce-blocks#6007
    
    Add feature gate for the global styles for Product Sale Badge block
    
    * add a check for the spacing support

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/image/supports.ts
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/image/supports.ts
@@ -3,6 +3,11 @@
  */
 import { isFeaturePluginBuild } from '@woocommerce/block-settings';
 
+/**
+ * Internal dependencies
+ */
+import { hasSpacingStyleSupport } from '../../../../utils/global-style';
+
 export const supports = {
 	...( isFeaturePluginBuild() && {
 		__experimentalBorder: {
@@ -13,10 +18,12 @@ export const supports = {
 			fontSize: true,
 			__experimentalSkipSerialization: true,
 		},
-		spacing: {
-			margin: true,
-			__experimentalSkipSerialization: true,
-		},
+		...( hasSpacingStyleSupport() && {
+			spacing: {
+				margin: true,
+				__experimentalSkipSerialization: true,
+			},
+		} ),
 		__experimentalSelector: '.wc-block-components-product-image',
 	} ),
 };
