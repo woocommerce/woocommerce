7a1bbd48dddb9267ddb21eb4a2119d95f9e12ef8
Saad Tarhi <saad.trh@gmail.com>
2022-03-07 16:23:31 +0100

    Shopper → Cart → Can proceed to checkout (https://github.com/woocommerce/woocommerce-blocks/pull/5901)
    * Shopper → Cart → Can proceed to checkout
    
    * Refactor the code for E2E testing
    
    Introduce some discussed solution to structure and optimize the code
    
    * Resolve some conflicts with the trunk branch
    
    * Remove method overriding and refactor code
    
    We are trying to avoid overriding exiting E2E Woo's utility functions
    
    Instead we can use our own "shopper.block"
    
    * Use a consistent constant naming convention
    
    Co-authored-by: Saad Tarhi <saad.tarhi@automattic.com>

--- plugins/woocommerce-blocks/tests/utils/constants.js
+++ plugins/woocommerce-blocks/tests/utils/constants.js
@@ -11,3 +11,4 @@ const baseUrl = config.get( 'url' );
  */
 export const SHOP_CART_BLOCK_PAGE = baseUrl + 'cart-block';
 export const SHOP_CHECKOUT_BLOCK_PAGE = baseUrl + 'checkout-block';
+export const SIMPLE_PRODUCT_NAME = 'Woo Single #1';
