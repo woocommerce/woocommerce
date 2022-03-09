c36ce57ba6db3e9ff662fabfb52ff0c7e32312c4
Niels Lange <info@nielslange.de>
2022-03-09 13:23:52 +0100

    Shopper can choose shipping option (https://github.com/woocommerce/woocommerce-blocks/pull/5844)

--- plugins/woocommerce-blocks/tests/e2e/fixtures/fixture-data.js
+++ plugins/woocommerce-blocks/tests/e2e/fixtures/fixture-data.js
@@ -344,6 +344,30 @@ const Shipping = () => [
 			},
 		],
 	},
+	{
+		name: 'US',
+		locations: [
+			{
+				code: 'US',
+			},
+		],
+		methods: [
+			{
+				method_id: 'flat_rate',
+				settings: {
+					title: 'Normal Shipping',
+					cost: '20.00',
+				},
+			},
+			{
+				method_id: 'free_shipping',
+				settings: {
+					title: 'Free Shipping',
+					cost: '00.00',
+				},
+			},
+		],
+	},
 ];
 
 /**
