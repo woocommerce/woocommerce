22cdefd3da4af9faac15f9cdc5c395b6a70e40f7
Tung Du <dinhtungdu@gmail.com>
2022-03-09 07:06:32 +0700

    Cart: Accepted Payment Methods block: Use utility instead of the local function (https://github.com/woocommerce/woocommerce-blocks/pull/6001)

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/cart/inner-blocks/cart-accepted-payment-methods-block/block.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/cart/inner-blocks/cart-accepted-payment-methods-block/block.tsx
@@ -3,21 +3,7 @@
  */
 import { PaymentMethodIcons } from '@woocommerce/base-components/cart-checkout';
 import { usePaymentMethods } from '@woocommerce/base-context/hooks';
-import type {
-	PaymentMethods,
-	PaymentMethodIcons as PaymentMethodIconsType,
-} from '@woocommerce/type-defs/payments';
-
-const getIconsFromPaymentMethods = (
-	paymentMethods: PaymentMethods
-): PaymentMethodIconsType => {
-	return Object.values( paymentMethods ).reduce( ( acc, paymentMethod ) => {
-		if ( paymentMethod.icons !== null ) {
-			acc = acc.concat( paymentMethod.icons );
-		}
-		return acc;
-	}, [] as PaymentMethodIconsType );
-};
+import { getIconsFromPaymentMethods } from '@woocommerce/base-utils';
 
 const Block = ( { className }: { className: string } ): JSX.Element => {
 	const { paymentMethods } = usePaymentMethods();
