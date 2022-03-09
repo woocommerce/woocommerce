9804d3974cde91a8ab506aa5f3a7085b462eef70
Jon Lane <jon.lane@automattic.com>
2022-03-08 16:32:53 -0800

    Changed timeout from 60 to 30 seconds

--- plugins/woocommerce/e2e/playwright.config.js
+++ plugins/woocommerce/e2e/playwright.config.js
@@ -4,7 +4,7 @@ const { devices } = require('@playwright/test');
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-	timeout: 60000,
+	timeout: 30000,
 	globalSetup: require.resolve('./global-setup'),
 	testDir: 'tests',
 	reporter: [['list'], ['html', { outputFolder: 'e2e/output' }]],
@@ -24,10 +24,6 @@ const config = {
     {
       name: 'Firefox',
       use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'Webkit',
-      use: { ...devices['Desktop Safari'] },
     },
   ],
 };
