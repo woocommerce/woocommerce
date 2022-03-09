98df58ff8a81b71323d16cfcdda294ca604a9bfe
Jon Lane <jon.lane@automattic.com>
2022-03-08 16:33:12 -0800

    Finished basic setup tests

--- plugins/woocommerce/e2e/tests/activate-and-setup/basic-setup.spec.js
+++ plugins/woocommerce/e2e/tests/activate-and-setup/basic-setup.spec.js
@@ -4,9 +4,28 @@ test.describe('Store owner can finish initial store setup', () => {
 	test.use({ storageState: 'e2e/storage/adminState.json' });
 	test('can enable tax rates and calculations', async ({ page }) => {
 		await page.goto('/wp-admin/admin.php?page=wc-settings');
-		const enableTaxes = page.locator('#woocommerce_calc_taxes');
-		await enableTaxes.click();
+		// Check the enable taxes checkbox
+		await page.check('#woocommerce_calc_taxes');
 		await page.locator('text=Save changes').click();
-		await expect(enableTaxes).toBeChecked();
+		// Verify changes have been saved
+		const checked = await page.isChecked('#woocommerce_calc_taxes');
+		expect(checked).toBeTruthy();
+	});
+	test('can configure permalink settings', async ({ page }) => {
+		await page.goto('/wp-admin/options-permalink.php');
+		// Select "Post name" option in common settings section
+		await page.check('label >> text=Post name');
+		// Select "Custom base" in product permalinks section
+		await page.check('label >> text=Custom base');
+		// Fill custom base slug to use
+		await page.fill('#woocommerce_permalink_structure', '/product/');
+		await page.click('#submit');
+		// Verify that settings have been saved
+		const notice = await page.textContent('#setting-error-settings_updated');
+		expect(notice).toContain('Permalink structure updated.');
+		const postSlug = await page.getAttribute('#permalink_structure', 'value');
+		expect(postSlug).toBe('/%postname%/');
+		const wcSlug = await page.getAttribute('#woocommerce_permalink_structure', 'value');
+		expect(wcSlug).toBe('/product/');
 	});
 });
