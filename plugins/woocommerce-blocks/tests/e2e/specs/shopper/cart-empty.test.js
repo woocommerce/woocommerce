bc309179c657cb6517928ce614d4588cb5791325
Tarun Vijwani <tarun.vijwani@automattic.com>
2022-03-10 15:50:07 +0400

    Critical flows: Shopper → Cart → Can view empty cart message (https://github.com/woocommerce/woocommerce-blocks/pull/6024)
    * Added test case for Critical flows: Shopper → Cart → Can view empty cart message

---
+++ plugins/woocommerce-blocks/tests/e2e/specs/shopper/cart-empty.test.js
@@ -0,0 +1,23 @@
+/**
+ * Internal dependencies
+ */
+import { shopper } from '../../../utils';
+
+if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 2 )
+	// eslint-disable-next-line jest/no-focused-tests
+	test.only( `skipping ${ block.name } tests`, () => {} );
+
+describe( 'Shopper → Cart → Can view empty cart message', () => {
+	beforeAll( async () => {
+		await shopper.block.emptyCart();
+	} );
+
+	it( 'Shopper Can view empty cart message', async () => {
+		await shopper.block.goToCart();
+
+		// Verify cart is empty'
+		await expect( page ).toMatchElement( 'h2', {
+			text: 'Your cart is currently empty!',
+		} );
+	} );
+} );
