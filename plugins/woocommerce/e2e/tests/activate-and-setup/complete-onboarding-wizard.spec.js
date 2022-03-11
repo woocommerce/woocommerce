755a725a16215fe3002d9c10b3f4f21ca234741a
Jon Lane <jon.lane@automattic.com>
2022-03-11 15:28:23 -0800

    Refactor onboarding tests

--- plugins/woocommerce/e2e/tests/activate-and-setup/complete-onboarding-wizard.spec.js
+++ plugins/woocommerce/e2e/tests/activate-and-setup/complete-onboarding-wizard.spec.js
@@ -2,13 +2,12 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('Store owner can complete onboarding wizard', () => {
 	test.use({ storageState: 'e2e/storage/adminState.json' });
-	test('can start the profile wizard', async ({ page }) => {
+
+	test.beforeEach(async ({ page }) => {
+		// These tests all take place with a US store
 		await page.goto('wp-admin/admin.php?page=wc-admin&path=/setup-wizard');
 		const pageHeading = await page.textContent('div.woocommerce-profile-wizard__step-header > h2');
 		expect(pageHeading).toContain('Welcome to WooCommerce');
-	});
-	test('can complete the store details section', async ({ page }) => {
-		await page.goto('wp-admin/admin.php?page=wc-admin&path=/setup-wizard');
 		// Fill store's address - first line
 		await page.fill('#inspector-text-control-0', 'addr 1');
 		// Fill store's address - second line
@@ -32,8 +31,9 @@ test.describe('Store owner can complete onboarding wizard', () => {
 		expect(dialogHeading).toContain('Build a better WooCommerce');
 		await page.click('button >> text=No thanks');
 	});
+
 	test('can complete the industry section', async ({ page }) => {
-		// There are 8 checkboxes on the page, adjust this constant if we change that
+		// There are 8 checkboxes on the page (in the US), adjust this constant if we change that
 		const expectedIndustries = 8;
 		await page.goto('wp-admin/admin.php?page=wc-admin&path=%2Fsetup-wizard&step=industry');
 		const pageHeading = await page.textContent('div.woocommerce-profile-wizard__step-header > h2');
@@ -51,6 +51,7 @@ test.describe('Store owner can complete onboarding wizard', () => {
 		await page.check('label >> text=Health and beauty');
 		await page.click('button >> text=Continue');
 	});
+
 	test('can complete the product types section', async ({ page }) => {
 		// There are 7 checkboxes on the page, adjust this constant if we change that
 		const expectedProductTypes = 7;
@@ -70,6 +71,7 @@ test.describe('Store owner can complete onboarding wizard', () => {
 		await page.check('label >> text=Downloads');
 		await page.click('button >> text=Continue');
 	});
+
 	test('can complete the business section', async ({ page }) => {
 		await page.goto('wp-admin/admin.php?page=wc-admin&path=%2Fsetup-wizard&step=business-details');
 		const pageHeading = await page.textContent('div.woocommerce-profile-wizard__step-header > h2');
@@ -82,6 +84,7 @@ test.describe('Store owner can complete onboarding wizard', () => {
 		await page.click('#woocommerce-select-control__option-1-no');
 		await page.click('button >> text=Continue');
 	});
+
 	test('can unselect all business features and continue', async ({ page }) => {
 		await page.goto('wp-admin/admin.php?page=wc-admin&path=%2Fsetup-wizard&step=business-details');
 		// Click the Free features tab
@@ -99,6 +102,7 @@ test.describe('Store owner can complete onboarding wizard', () => {
 		};
 		await page.click('button >> text=Continue');
 	});
+
 	test('can complete the theme selection section', async ({ page }) => {
 		await page.goto('wp-admin/admin.php?page=wc-admin&path=%2Fsetup-wizard&step=theme');
 		const pageHeading = await page.textContent('div.woocommerce-profile-wizard__step-header > h2');
@@ -110,33 +114,25 @@ test.describe('Store owner can complete onboarding wizard', () => {
 
 test.describe('A japanese store can complete the selective bundle install but does not include WCPay.', () => {
 	test.use({ storageState: 'e2e/storage/adminState.json' });
-	test('can complete the store details section', async ({ page }) => {
+
+	test.beforeEach(async ({ page }) => {
+		// These tests all take place with a store based in Japan
 		await page.goto('wp-admin/admin.php?page=wc-admin&path=/setup-wizard');
-		// Fill store's address - first line
 		await page.fill('#inspector-text-control-0', 'addr 1');
-		// Fill store's address - second line
 		await page.fill('#inspector-text-control-1', 'addr 2');
-		// Type the requested country/region
 		await page.click('#woocommerce-select-control-0__control-input');
 		await page.fill('#woocommerce-select-control-0__control-input', 'Japan — Hokkaido');
 		await page.click('button >> text=Japan — Hokkaido');
-		// Fill the city where the store is located
-		await page.fill('#inspector-text-control-2', 'San Francisco');
-		// Fill postcode of the store
-		await page.fill('#inspector-text-control-3', '94107');
-		// Fill store's email address
+		await page.fill('#inspector-text-control-2', 'Sapporo');
+		await page.fill('#inspector-text-control-3', '007-0852');
 		await page.fill('#inspector-text-control-4', 'admin@woocommercecoree2etestsuite.com');
-		// Verify that checkbox next to "Get tips, product updates and inspiration straight to your mailbox" is selected
 		await page.check('#inspector-checkbox-control-0');
-		// Click continue button
 		await page.click('button >> text=Continue');
-		// Usage tracking dialog
-		const dialogHeading = await page.textContent('.components-modal__header-heading')
-		expect(dialogHeading).toContain('Build a better WooCommerce');
 		await page.click('button >> text=No thanks');
 	});
+
 	test('can choose the "Other" industry', async ({ page }) => {
-		const expectedIndustries = 8;
+		const expectedIndustries = 7;
 		await page.goto('wp-admin/admin.php?page=wc-admin&path=%2Fsetup-wizard&step=industry');
 		const pageHeading = await page.textContent('div.woocommerce-profile-wizard__step-header > h2');
 		expect(pageHeading).toContain('In which industry does the store operate?');
@@ -149,23 +145,8 @@ test.describe('A japanese store can complete the selective bundle install but do
 		await page.check('label >> text="Other"');
 		await page.click('button >> text=Continue');
 	});
+
 	test('can choose not to install any extensions', async ({ page }) => {
-		// Setup
-		await page.goto('wp-admin/admin.php?page=wc-admin&path=/setup-wizard');
-		await page.fill('#inspector-text-control-0', 'addr 1');
-		await page.fill('#inspector-text-control-1', 'addr 2');
-		await page.click('#woocommerce-select-control-0__control-input');
-		await page.fill('#woocommerce-select-control-0__control-input', 'Japan — Hokkaido');
-		await page.click('button >> text=Japan — Hokkaido');
-		await page.fill('#inspector-text-control-2', 'San Francisco');
-		await page.fill('#inspector-text-control-3', '94107');
-		await page.fill('#inspector-text-control-4', 'admin@woocommercecoree2etestsuite.com');
-		await page.check('#inspector-checkbox-control-0');
-		await page.click('button >> text=Continue');
-		const dialogHeading = await page.textContent('.components-modal__header-heading')
-		expect(dialogHeading).toContain('Build a better WooCommerce');
-		await page.click('button >> text=No thanks');
-		// Start actual test
 		await page.goto('wp-admin/admin.php?page=wc-admin&path=%2Fsetup-wizard&step=business-details');
 		// Click the Free features tab
 		await page.click('#tab-panel-0-business-features');
@@ -181,27 +162,13 @@ test.describe('A japanese store can complete the selective bundle install but do
 		};
 		await page.click('button >> text=Continue');
 	});
+
 	test('should display the choose payments task, and not the WC Pay task', async ({ page }) => {
 		// Setup
-		await page.goto('wp-admin/admin.php?page=wc-admin&path=/setup-wizard');
-		await page.fill('#inspector-text-control-0', 'addr 1');
-		await page.fill('#inspector-text-control-1', 'addr 2');
-		await page.click('#woocommerce-select-control-0__control-input');
-		await page.fill('#woocommerce-select-control-0__control-input', 'Japan — Hokkaido');
-		await page.click('button >> text=Japan — Hokkaido');
-		await page.fill('#inspector-text-control-2', 'San Francisco');
-		await page.fill('#inspector-text-control-3', '94107');
-		await page.fill('#inspector-text-control-4', 'admin@woocommercecoree2etestsuite.com');
-		await page.check('#inspector-checkbox-control-0');
-		await page.click('button >> text=Continue');
-		const dialogHeading = await page.textContent('.components-modal__header-heading')
-		expect(dialogHeading).toContain('Build a better WooCommerce');
-		await page.click('button >> text=No thanks');
 		await page.goto('wp-admin/admin.php?page=wc-admin&path=%2Fsetup-wizard&step=theme');
 		await page.click('button >> text=Continue with my active theme');
 		// Start test
 		// If the test is being retried, the modal may have already been dismissed
-		await page.goto('wp-admin/admin.php?page=wc-admin');
 		await page.locator('#adminmenumain');
 		const modalHeading = await page.$('h2.woocommerce__welcome-modal__page-content__header');
 		if (modalHeading) {
