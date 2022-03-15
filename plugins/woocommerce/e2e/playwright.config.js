c45ab133d1d2c65df556b01976da2c92c7219e6d
Jon Lane <jon.lane@automattic.com>
2022-03-15 10:40:00 -0700

    Only run on Chrome, update report output

--- plugins/woocommerce/e2e/playwright.config.js
+++ plugins/woocommerce/e2e/playwright.config.js
@@ -5,6 +5,7 @@ const { devices } = require('@playwright/test');
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
 	timeout: 30000,
+	outputDir: './output',
 	globalSetup: require.resolve('./global-setup'),
 	testDir: 'tests',
 	reporter: [['list'], ['html', { outputFolder: 'e2e/output' }]],
@@ -21,10 +22,14 @@ const config = {
       name: 'Chrome',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'Firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //  name: 'Firefox',
+    //  use: { ...devices['Desktop Firefox'] },
+    // },
+	// {
+    //  name: 'Webkit',
+    //  use: { ...devices['Desktop Webkit'] },
+    // },
   ],
 };
 
