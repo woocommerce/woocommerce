839b6ffbc6b9ca859d5555d0ae68e5d10bdffaaf
Christopher Allford <christopher.allford@automattic.com>
2022-03-18 15:44:40 -0700

    Moved WooCommerce Admin Images
    This commit moves the images from `plugins/woocommerce-admin/images`
    to their new home in `plugins/woocommerce/assets/images`

--- plugins/woocommerce/src/Internal/Admin/FeaturePlugin.php
+++ plugins/woocommerce/src/Internal/Admin/FeaturePlugin.php
@@ -153,7 +153,7 @@ class FeaturePlugin {
 		$this->define( 'WC_ADMIN_DIST_JS_FOLDER', 'assets/client/admin/' );
 		$this->define( 'WC_ADMIN_DIST_CSS_FOLDER', 'assets/client/admin/' );
 		$this->define( 'WC_ADMIN_PLUGIN_FILE', WC_PLUGIN_FILE );
-		$this->define( 'WC_ADMIN_IMAGES_FOLDER_URL', plugins_url( 'images', WC_PLUGIN_FILE ) );
+		$this->define( 'WC_ADMIN_IMAGES_FOLDER_URL', plugins_url( 'assets/images', WC_PLUGIN_FILE ) );
 
 		// WARNING: Do not directly edit this version number constant.
 		// It is updated as part of the prebuild process from the package.json value.
