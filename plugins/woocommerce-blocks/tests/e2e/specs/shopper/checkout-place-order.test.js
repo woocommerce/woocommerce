729340380f0fd342a48e4b908d93bdb3bf22bd2a
Niels Lange <info@nielslange.de>
2022-03-23 12:23:22 +0000

    Shopper can place an order as logged in user (https://github.com/woocommerce/woocommerce-blocks/pull/6037)
    * Shopper can place an order as logged in user
    
    * Fill billing address for logged in user
    
    Co-authored-by: Saad Tarhi <saad.trh@gmail.com>

---
+++ plugins/woocommerce-blocks/tests/e2e/specs/shopper/checkout-place-order.test.js
@@ -0,0 +1,32 @@
+/**
+ * Internal dependencies
+ */
+import { shopper } from '../../../utils';
+import { SIMPLE_PRODUCT_NAME, BILLING_DETAILS } from '../../../utils/constants';
+
+if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 2 )
+	// eslint-disable-next-line jest/no-focused-tests
+	test.only( `Skipping checkout tests`, () => {} );
+
+describe( 'Shopper → Checkout → Can place an order', () => {
+	it( 'allows customer to place an order as a guest', async () => {
+		await shopper.logout();
+		await shopper.goToShop();
+		await shopper.addToCartFromShopPage( SIMPLE_PRODUCT_NAME );
+		await shopper.block.goToCheckout();
+		await shopper.block.fillBillingDetails( BILLING_DETAILS );
+		await shopper.block.placeOrder();
+		await expect( page ).toMatch( 'Your order has been received.' );
+	} );
+
+	it( 'allows customer to place an order as a logged in user', async () => {
+		await shopper.login();
+		await shopper.goToShop();
+		await shopper.addToCartFromShopPage( SIMPLE_PRODUCT_NAME );
+		await shopper.block.goToCheckout();
+		await shopper.block.fillBillingDetails( BILLING_DETAILS );
+		await shopper.block.placeOrder();
+		await expect( page ).toMatch( 'Your order has been received.' );
+		await shopper.logout();
+	} );
+} );
