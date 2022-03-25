7bfb8c1f97c79fd3b7f4ffb2a56ec79710113919
vedanshujain <vedanshu.jain.2012@gmail.com>
2022-03-25 10:19:35 +0530

    Add default woocommerce file for quality of life.

---
+++ woocommerce.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: WooCommerce
+ * Plugin URI: https://woocommerce.com/
+ * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
+ * Version: development
+ * Author: Automattic
+ * Author URI: https://woocommerce.com
+ * Text Domain: woocommerce
+ * Domain Path: /i18n/languages/
+ * Requires at least: 5.7
+ * Requires PHP: 7.0
+ *
+ * @package WooCommerce
+ */
+
+require_once __DIR__ . '/plugins/woocommerce/woocommerce.php';
