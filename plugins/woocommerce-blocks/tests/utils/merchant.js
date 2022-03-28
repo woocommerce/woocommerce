ed3fa508076058c951f196fd5e2c9a7db5458076
Alex Florisca <alex.florisca@automattic.com>
2022-03-28 16:20:59 +0100

    E2E tests for translated Cart & Checkout blocks (https://github.com/woocommerce/woocommerce-blocks/pull/6101)
    * Translation tests for Cart & Checkout
    
    * Remove comments

---
+++ plugins/woocommerce-blocks/tests/utils/merchant.js
@@ -0,0 +1,14 @@
+/**
+ * External dependencies
+ */
+import { merchant as wcMerchant } from '@woocommerce/e2e-utils';
+import { visitAdminPage } from '@wordpress/e2e-test-utils';
+
+export const merchant = {
+	...wcMerchant,
+	changeLanguage: async ( language ) => {
+		await visitAdminPage( 'options-general.php' );
+		await page.select( 'select#WPLANG', language );
+		await page.click( 'input[type="submit"]' );
+	},
+};
