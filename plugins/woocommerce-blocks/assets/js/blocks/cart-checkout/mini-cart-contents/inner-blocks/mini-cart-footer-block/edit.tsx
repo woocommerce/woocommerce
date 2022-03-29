f4d4bed2c18b6f81b69f7ceb04487b7c6d7c01c7
Tung Du <dinhtungdu@gmail.com>
2022-03-29 20:48:59 +0700

    Mini Cart Contents: Refactor inner blocks styling using `currentColor` (https://github.com/woocommerce/woocommerce-blocks/pull/6130)

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/mini-cart-contents/inner-blocks/mini-cart-footer-block/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout/mini-cart-contents/inner-blocks/mini-cart-footer-block/edit.tsx
@@ -3,40 +3,19 @@
  */
 import { useBlockProps } from '@wordpress/block-editor';
 import Noninteractive from '@woocommerce/base-components/noninteractive';
-import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import Block from './block';
-import { blockName as miniCartContentsBlockName } from '../../attributes';
-import { useColorProps } from '../../../../../hooks/style-attributes';
 
-export const Edit = ( { clientId }: { clientId: string } ): JSX.Element => {
+export const Edit = (): JSX.Element => {
 	const blockProps = useBlockProps();
-
-	const parentAttributes = useSelect( ( select ) => {
-		const { getBlockAttributes, getBlockParentsByBlockName } = select(
-			'core/block-editor'
-		);
-		const parentBlockIds = getBlockParentsByBlockName(
-			clientId,
-			miniCartContentsBlockName
-		);
-
-		if ( parentBlockIds.length !== 1 ) {
-			return {};
-		}
-
-		return getBlockAttributes( parentBlockIds[ 0 ] );
-	} );
-
-	const parentColorProps = useColorProps( parentAttributes );
 
 	return (
 		<div { ...blockProps }>
 			<Noninteractive>
-				<Block { ...parentColorProps.style } />
+				<Block />
 			</Noninteractive>
 		</div>
 	);
