e78e0a1676c69708a7275d940c21519954f3c0d9
Jon Lane <jon.lane@automattic.com>
2022-04-01 13:59:19 -0700

    Update coupons test for wc-admin

--- plugins/woocommerce/e2e/tests/admin-marketing/coupons.spec.js
+++ plugins/woocommerce/e2e/tests/admin-marketing/coupons.spec.js
@@ -6,6 +6,6 @@ test.describe('Coupons page', () => {
 	test('A user can view the coupons overview without it crashing', async ({ page }) => {
 		await page.goto('wp-admin/edit.php?post_type=shop_coupon&legacy_coupon_menu=1');
 		await expect(page.locator('h1.wp-heading-inline')).toHaveText('Coupons');
-		await expect(page.locator('text=Create your first coupon')).toBeVisible();
+		await expect(page.locator('a.page-title-action')).toHaveText('Add coupon');
 	});
 });
