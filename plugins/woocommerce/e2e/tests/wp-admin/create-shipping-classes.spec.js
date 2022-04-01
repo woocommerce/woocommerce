32629beb53f5976226e33d65b786730f2d6b3bd0
Jon Lane <jon.lane@automattic.com>
2022-04-01 14:00:07 -0700

    Shipping classes test

---
+++ plugins/woocommerce/e2e/tests/wp-admin/create-shipping-classes.spec.js
@@ -0,0 +1,59 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Merchant can add shipping classes', () => {
+	test.use({ storageState: 'e2e/storage/adminState.json' });
+
+	test.only('can add shipping classes', async ({ page }) => {
+		page.goto(
+			'wp-admin/admin.php?page=wc-settings&tab=shipping&section=classes'
+		);
+
+		const shippingClassSlug = {
+			name: 'Small Items',
+			slug: 'small-items',
+			description: "Small items that don't cost much to ship.",
+		};
+		const shippingClassNoSlug = {
+			name: 'Poster Pack',
+			slug: '',
+			description: '',
+		};
+		const shippingClasses = [shippingClassSlug, shippingClassNoSlug];
+
+		// Add shipping classes
+		for (const { name, slug, description } of shippingClasses) {
+			await page.locator('text=Add shipping class').click();
+			await page.fill(
+				'.editing:last-child [data-attribute="name"]',
+				name
+			);
+			await page.fill(
+				'.editing:last-child [data-attribute="slug"]',
+				slug
+			);
+			await page.fill(
+				'.editing:last-child [data-attribute="description"]',
+				description
+			);
+		}
+		await page.locator('text=Save shipping classes').click();
+
+		// Set the expected auto-generated slug
+		shippingClassNoSlug.slug = 'poster-pack';
+
+		// Verify that the specified shipping classes were saved
+		for (const { name, slug, description } of shippingClasses) {
+			await expect(page.locator(`text=${name} Edit | Remove`)).toBeVisible();
+			await expect(page.locator(`text=${slug}`)).toBeVisible();
+			// account for blank description
+			if (description != "") {
+				await expect(page.locator(`text=${description}`)).toBeVisible();
+			}
+		}
+
+		// clean up
+		await page.dispatchEvent('.wc-shipping-class-delete >> nth=0', 'click');
+		await page.dispatchEvent('.wc-shipping-class-delete >> nth=0', 'click');
+		await page.dispatchEvent('text=Save shipping classes', 'click');
+	});
+});
