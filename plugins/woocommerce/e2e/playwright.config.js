aef790b38e3002c86dee90fc6ee65e3112078614
Jon Lane <jon.lane@automattic.com>
2022-04-01 13:59:03 -0700

    Bump test timeout to 15 seconds

--- plugins/woocommerce/e2e/playwright.config.js
+++ plugins/woocommerce/e2e/playwright.config.js
@@ -4,33 +4,33 @@ const { devices } = require('@playwright/test');
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-	timeout: 10000,
+	timeout: 15000,
 	outputDir: './report',
 	globalSetup: require.resolve('./global-setup'),
 	testDir: 'tests',
 	retries: 1,
 	reporter: [['list'], ['html', { outputFolder: 'e2e/output' }]],
 	use: {
-	  screenshot: 'only-on-failure',
-	  video: 'retain-on-failure',
-	  trace: 'retain-on-failure',
-	  viewport: { width: 1280, height: 720 },
-	  baseURL: 'http://localhost:8084'
-  },
-  projects: [
-    {
-      name: 'Chrome',
-      use: { ...devices['Desktop Chrome'] },
-    },
-    // {
-    //  name: 'Firefox',
-    //  use: { ...devices['Desktop Firefox'] },
-    // },
-	// {
-    //  name: 'Webkit',
-    //  use: { ...devices['Desktop Webkit'] },
-    // },
-  ],
+		screenshot: 'only-on-failure',
+		video: 'retain-on-failure',
+		trace: 'retain-on-failure',
+		viewport: { width: 1280, height: 720 },
+		baseURL: 'http://localhost:8084'
+	},
+	projects: [
+		{
+			name: 'Chrome',
+			use: { ...devices['Desktop Chrome'] },
+		},
+		// {
+		//  name: 'Firefox',
+		//  use: { ...devices['Desktop Firefox'] },
+		// },
+		// {
+		//  name: 'Webkit',
+		//  use: { ...devices['Desktop Webkit'] },
+		// },
+	],
 };
 
 module.exports = config;
