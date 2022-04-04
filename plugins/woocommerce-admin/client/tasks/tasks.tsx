e4803ef40fb1e9afec02a91e3b0b267d8c4472f1
Lourens Schep <lourensschep@gmail.com>
2022-04-04 16:43:15 -0300

    Fix tests

--- plugins/woocommerce-admin/client/tasks/tasks.tsx
+++ plugins/woocommerce-admin/client/tasks/tasks.tsx
@@ -4,7 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { MenuGroup, MenuItem } from '@wordpress/components';
 import { check } from '@wordpress/icons';
-import { Fragment, useEffect, Suspense } from '@wordpress/element';
+import { Fragment, useEffect } from '@wordpress/element';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { ONBOARDING_STORE_NAME, OPTIONS_STORE_NAME } from '@woocommerce/data';
 import { useExperiment } from '@woocommerce/explat';
@@ -147,17 +147,14 @@ export const Tasks: React.FC< TasksProps > = ( { query } ) => {
 
 			return (
 				<Fragment key={ id }>
-					<Suspense fallback={ null }>
-						<TaskListComponent
-							isExpandable={
-								experimentAssignment?.variationName ===
-								'treatment'
-							}
-							query={ query }
-							twoColumns={ false }
-							{ ...taskList }
-						/>
-					</Suspense>
+					<TaskListComponent
+						isExpandable={
+							experimentAssignment?.variationName === 'treatment'
+						}
+						query={ query }
+						twoColumns={ false }
+						{ ...taskList }
+					/>
 					{ isToggleable && (
 						<DisplayOption>
 							<MenuGroup
