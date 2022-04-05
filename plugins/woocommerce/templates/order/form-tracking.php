e72bc47535b91cb07127c455e9d84dfbcc1bc302
Jorge A. Torres <jorge.torres@automattic.com>
2022-04-05 14:02:40 -0300

    Bump ‘form-tracking’ template version

--- plugins/woocommerce/templates/order/form-tracking.php
+++ plugins/woocommerce/templates/order/form-tracking.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.6.0
+ * @version 6.5.0
  */
 
 defined( 'ABSPATH' ) || exit;
