70ff5ad3ae6b2f8a9915cd300883c6cc7611720a
moon <moon.kyong@automattic.com>
2022-04-06 18:51:31 -0700

    Move delete_obsolete_notes to the core

--- plugins/woocommerce/includes/class-wc-install.php
+++ plugins/woocommerce/includes/class-wc-install.php
@@ -7,6 +7,7 @@
  */
 
 use Automattic\Jetpack\Constants;
+use Automattic\WooCommerce\Admin\Notes\Notes;
 use Automattic\WooCommerce\Internal\ProductAttributesLookup\DataRegenerator;
 use Automattic\WooCommerce\Internal\ProductDownloads\ApprovedDirectories\Register as Download_Directories;
 use Automattic\WooCommerce\Internal\ProductDownloads\ApprovedDirectories\Synchronize as Download_Directories_Sync;
@@ -387,7 +388,7 @@ class WC_Install {
 		self::setup_environment();
 		self::create_terms();
 		self::create_cron_jobs();
-		WCA_Install::delete_obsolete_notes();
+		self::delete_obsolete_notes();
 		self::create_files();
 		self::maybe_create_pages();
 		self::maybe_set_activation_transients();
@@ -753,6 +754,45 @@ class WC_Install {
 			// For new installs, setup and enable Approved Product Download Directories.
 			wc_get_container()->get( Download_Directories_Sync::class )->init_feature( false, true );
 		}
+	}
+
+	public static function delete_obsolete_notes() {
+		$obsolete_notes_names = array(
+			'wc-admin-welcome-note',
+			'wc-admin-store-notice-setting-moved',
+			'wc-admin-store-notice-giving-feedback',
+			'wc-admin-learn-more-about-product-settings',
+			'wc-admin-onboarding-profiler-reminder',
+			'wc-admin-historical-data',
+			'wc-admin-review-shipping-settings',
+			'wc-admin-home-screen-feedback',
+			'wc-admin-effortless-payments-by-mollie',
+			'wc-admin-google-ads-and-marketing',
+			'wc-admin-marketing-intro',
+			'wc-admin-draw-attention',
+			'wc-admin-need-some-inspiration',
+			'wc-admin-choose-niche',
+			'wc-admin-start-dropshipping-business',
+			'wc-admin-filter-by-product-variations-in-reports',
+			'wc-admin-learn-more-about-variable-products',
+			'wc-admin-getting-started-ecommerce-webinar',
+			'wc-admin-navigation-feedback',
+			'wc-admin-navigation-feedback-follow-up',
+		);
+
+		$additional_obsolete_notes_names = apply_filters(
+			'woocommerce_admin_obsolete_notes_names',
+			array()
+		);
+
+		if ( is_array( $additional_obsolete_notes_names ) ) {
+			$obsolete_notes_names = array_merge(
+				$obsolete_notes_names,
+				$additional_obsolete_notes_names
+			);
+		}
+
+		Notes::delete_notes_with_name( $obsolete_notes_names );
 	}
 
 	/**
--- plugins/woocommerce/src/Internal/Admin/Install.php
+++ plugins/woocommerce/src/Internal/Admin/Install.php
@@ -21,42 +21,5 @@ class Install {
 	/**
 	 * Delete obsolete notes.
 	 */
-	public static function delete_obsolete_notes() {
-		$obsolete_notes_names = array(
-			'wc-admin-welcome-note',
-			'wc-admin-store-notice-setting-moved',
-			'wc-admin-store-notice-giving-feedback',
-			'wc-admin-learn-more-about-product-settings',
-			'wc-admin-onboarding-profiler-reminder',
-			'wc-admin-historical-data',
-			'wc-admin-review-shipping-settings',
-			'wc-admin-home-screen-feedback',
-			'wc-admin-effortless-payments-by-mollie',
-			'wc-admin-google-ads-and-marketing',
-			'wc-admin-marketing-intro',
-			'wc-admin-draw-attention',
-			'wc-admin-need-some-inspiration',
-			'wc-admin-choose-niche',
-			'wc-admin-start-dropshipping-business',
-			'wc-admin-filter-by-product-variations-in-reports',
-			'wc-admin-learn-more-about-variable-products',
-			'wc-admin-getting-started-ecommerce-webinar',
-			'wc-admin-navigation-feedback',
-			'wc-admin-navigation-feedback-follow-up',
-		);
 
-		$additional_obsolete_notes_names = apply_filters(
-			'woocommerce_admin_obsolete_notes_names',
-			array()
-		);
-
-		if ( is_array( $additional_obsolete_notes_names ) ) {
-			$obsolete_notes_names = array_merge(
-				$obsolete_notes_names,
-				$additional_obsolete_notes_names
-			);
-		}
-
-		Notes::delete_notes_with_name( $obsolete_notes_names );
-	}
 }
