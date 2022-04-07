855dc716e97afb23d28b4f11039d716aa6284b6b
Jon Lane <jon.lane@automattic.com>
2022-04-07 15:30:06 -0700

    Refactor payment tests

--- plugins/woocommerce/e2e/tests/admin-tasks/payment.spec.js
+++ plugins/woocommerce/e2e/tests/admin-tasks/payment.spec.js
@@ -7,15 +7,15 @@ test.describe('Payment setup task', () => {
 		await page.goto(
 			'wp-admin/admin.php?page=wc-admin&path=/setup-wizard'
 		);
-		await page.locator('text=Skip setup store details').click();
-		await page.locator('text=No thanks').click();
+		await page.click('text=Skip setup store details');
+		await page.locator('text=No thanks');
 	});
 
 	test('Can visit the payment setup task from the homescreen if the setup wizard has been skipped', async ({
 		page,
 	}) => {
 		await page.goto('wp-admin/admin.php?page=wc-admin');
-		await page.locator('text=Set up payments').click();
+		await page.click('text=Set up payments');
 		await expect(page.locator('h1')).toHaveText('Set up payments');
 	});
 
@@ -31,7 +31,7 @@ test.describe('Payment setup task', () => {
 		await page.fill('//input[@placeholder="Sort code"]', '12');
 		await page.fill('//input[@placeholder="IBAN"]', '12 3456 7890');
 		await page.fill('//input[@placeholder="BIC / Swift"]', 'ABBA');
-		await page.locator('text=Save').click();
+		await page.click('text=Save');
 		await expect(
 			page.locator(
 				'div.components-snackbar__content'
@@ -50,15 +50,15 @@ test.describe('Payment setup task', () => {
 		await page.goto(
 			'wp-admin/admin.php?page=wc-settings&tab=checkout&section=bacs'
 		);
-		await page.locator('text="Enable bank transfer"').click();
-		await page.locator('text="Save changes"').click();
+		await page.dispatchEvent('text="Enable bank transfer"', 'click');
+		await page.dispatchEvent('text="Save changes', 'click');
 	});
 
 	test('Enabling cash on delivery enables the payment method', async ({
 		page,
 	}) => {
 		await page.goto('wp-admin/admin.php?page=wc-admin&task=payments');
-		await page.locator('text=Enable').click(); // enable COD payment option
+		await page.click('text=Enable'); // enable COD payment option
 		await page.goto('wp-admin/admin.php?page=wc-admin&task=payments');
 		await expect(page.locator('h1')).toHaveText('Set up payments');
 		await expect(
@@ -71,7 +71,7 @@ test.describe('Payment setup task', () => {
 		await page.goto(
 			'wp-admin/admin.php?page=wc-settings&tab=checkout&section=cod'
 		);
-		await page.locator('text="Enable cash on delivery"').click();
-		await page.locator('text="Save changes"').click();
+		await page.dispatchEvent('text="Enable cash on delivery"', 'click');
+		await page.dispatchEvent('text="Save changes', 'click');
 	});
 });
