ed2fee259be2477ea20bff01ff8e94f396597877
Jon Lane <jon.lane@automattic.com>
2022-04-07 15:36:43 -0700

    Refactor onboarding tests

--- plugins/woocommerce/e2e/tests/activate-and-setup/setup-onboarding.spec.js
+++ plugins/woocommerce/e2e/tests/activate-and-setup/setup-onboarding.spec.js
@@ -1,13 +1,19 @@
 const { test, expect } = require('@playwright/test');
 
-test.describe('Store owner can login and make sure WooCommerce is activated', () => {
-	test.use({ storageState: 'e2e/storage/adminState.json' });
-	test('can make sure WooCommerce is activated.', async ({ page }) => {
-		await page.goto('/wp-admin/plugins.php');
-		const wooPlugin = await page.locator(`//tr[@data-slug='woocommerce']`);
-		// Expect the woo plugin to be displayed
-		expect(wooPlugin).toBeVisible;
-		// Expect it to have an active class
-		expect(wooPlugin).toHaveClass('active is-uninstallable');
-	});
-});
+test.describe(
+	'Store owner can login and make sure WooCommerce is activated',
+	() => {
+		test.use({ storageState: 'e2e/storage/adminState.json' });
+
+		test('can make sure WooCommerce is activated.', async ({ page }) => {
+			await page.goto('/wp-admin/plugins.php');
+			// Expect the woo plugin to be displayed
+			expect(page.locator(`//tr[@data-slug='woocommerce']`))
+				.toBeVisible;
+			// Expect it to have an active class
+			expect(
+				page.locator(`//tr[@data-slug='woocommerce']`)
+			).toHaveClass('active is-uninstallable');
+		});
+	}
+);
