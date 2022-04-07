3c1ce9c6139186a1f175d81609dd7583a113f447
moon <moon.kyong@automattic.com>
2022-04-06 18:57:41 -0700

    Make create_tables public -- this is needed from WCA classes

--- plugins/woocommerce/includes/class-wc-install.php
+++ plugins/woocommerce/includes/class-wc-install.php
@@ -909,7 +909,7 @@ class WC_Install {
 	 *      woocommerce_tax_rates - Tax Rates are stored inside 2 tables making tax queries simple and efficient.
 	 *      woocommerce_tax_rate_locations - Each rate can be applied to more than one postcode/city hence the second table.
 	 */
-	private static function create_tables() {
+	public static function create_tables() {
 		global $wpdb;
 
 		$wpdb->hide_errors();
