263bd5776ed66e5cb888f529583f2142f40b04a3
barryhughes <3594411+barryhughes@users.noreply.github.com>
2022-04-11 06:16:52 -0700

    Correct text-domain.

--- plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-orders-v2-controller.php
+++ plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-orders-v2-controller.php
@@ -1852,7 +1852,7 @@ class WC_REST_Orders_V2_Controller extends WC_REST_CRUD_Controller {
 		);
 		$params['order_item_display_meta'] = array(
 			'default' => false,
-			'description' => __( 'Only show meta which is meant to be displayed for an order.', 'woocomerce' ),
+			'description' => __( 'Only show meta which is meant to be displayed for an order.', 'woocommerce' ),
 			'type' => 'boolean',
 			'sanitize_callback' => 'rest_sanitize_boolean',
 			'validate_callback' => 'rest_validate_request_arg',
