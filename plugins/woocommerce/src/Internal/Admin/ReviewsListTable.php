d49b713166e19d0d138a5ea192539b2e29c1d1e3
Ashley Gibson <agibson@godaddy.com>
2022-04-12 17:06:57 +0100

    Add the list of supported bulk actions.

--- plugins/woocommerce/src/Internal/Admin/ReviewsListTable.php
+++ plugins/woocommerce/src/Internal/Admin/ReviewsListTable.php
@@ -28,4 +28,43 @@ class ReviewsListTable extends WP_List_Table {
 		$this->items = $comments;
 	}
 
+	/**
+	 * Returns a list of available bulk actions.
+	 *
+	 * @global string $comment_status
+	 *
+	 * @return array
+	 */
+	protected function get_bulk_actions() {
+		global $comment_status;
+
+		$actions = [];
+
+		if ( in_array( $comment_status, [ 'all', 'approved' ], true ) ) {
+			$actions['unapprove'] = __( 'Unapprove', 'woocommerce' );
+		}
+
+		if ( in_array( $comment_status, [ 'all', 'moderated' ], true ) ) {
+			$actions['approve'] = __( 'Approve', 'woocommerce' );
+		}
+
+		if ( in_array( $comment_status, [ 'all', 'moderated', 'approved', 'trash' ], true ) ) {
+			$actions['spam'] = _x( 'Mark as spam', 'review', 'woocommerce' );
+		}
+
+		if ( 'trash' === $comment_status ) {
+			$actions['untrash'] = __( 'Restore', 'woocommerce' );
+		} elseif ( 'spam' === $comment_status ) {
+			$actions['unspam'] = _x( 'Not spam', 'review', 'woocommerce' );
+		}
+
+		if ( in_array( $comment_status, [ 'trash', 'spam' ], true ) || ! EMPTY_TRASH_DAYS ) {
+			$actions['delete'] = __( 'Delete permanently', 'woocommerce' );
+		} else {
+			$actions['trash'] = __( 'Move to Trash', 'woocommerce' );
+		}
+
+		return $actions;
+	}
+
 }
--- plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsListTableTest.php
+++ plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsListTableTest.php
@@ -2,6 +2,9 @@
 
 namespace Automattic\WooCommerce\Tests\Internal\Admin;
 
+use Automattic\WooCommerce\Internal\Admin\ReviewsListTable;
+use Generator;
+use ReflectionClass;
 use WC_Unit_Test_Case;
 
 /**
@@ -10,5 +13,40 @@ use WC_Unit_Test_Case;
  * @covers \Automattic\WooCommerce\Internal\Admin\ReviewsListTable
  */
 class ReviewsListTableTest extends WC_Unit_Test_Case {
+
+	/**
+	 * @dataProvider provider_get_bulk_actions
+	 *
+	 * @param string $current_comment_status Currently set status.
+	 * @param array  $expected_actions       Keys of the expected actions.
+	 * @return void
+	 */
+	public function test_get_bulk_actions( string $current_comment_status, array $expected_actions ) {
+		$list_table = new ReviewsListTable( [ 'screen' => 'product_page_product-reviews' ] );
+		$method = ( new ReflectionClass( $list_table ) )->getMethod( 'get_bulk_actions' );
+		$method->setAccessible( true );
+
+		// @TODO PHPCS doesn't like this {agibson 2022-04-12}
+		// global $comment_status;
+		// $comment_status = $current_comment_status;
+
+		$this->assertEqualsCanonicalizing(
+			$expected_actions,
+			array_keys( $method->invoke( $list_table ) )
+		);
+	}
+
+	/** @see test_get_bulk_actions */
+	public function provider_get_bulk_actions() : Generator {
+		yield 'all statuses' => [
+			'current_comment_status' => 'all',
+			'expected_actions' => [
+				'unapprove',
+				'approve',
+				'spam',
+				'trash',
+			],
+		];
+	}
 
 }
