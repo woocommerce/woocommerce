c5691060f3d980b0d4a6a74210a5a517080988ac
Fulvio Notarstefano <unfulvio@godaddy.com>
2022-04-12 11:17:02 +0800

    Adds the ReviewsListTableTest file

---
+++ plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsListTableTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Automattic\WooCommerce\Tests\Internal\Admin;
+
+use WC_Unit_Test_Case;
+
+/**
+ * Tests that product reviews page handler.
+ *
+ * @covers \Automattic\WooCommerce\Internal\Admin\ReviewsListTable
+ */
+class ReviewsListTableTest extends WC_Unit_Test_Case {
+
+}
