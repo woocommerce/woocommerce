f7ece5221f9212294e6335a55efa3c800a8652ea
Ashley Gibson <agibson@godaddy.com>
2022-04-12 13:21:15 +0100

    Register submenu & render callback.

--- plugins/woocommerce/src/Internal/Admin/Reviews.php
+++ plugins/woocommerce/src/Internal/Admin/Reviews.php
@@ -10,4 +10,97 @@ namespace Automattic\WooCommerce\Internal\Admin;
  */
 class Reviews {
 
+	/**
+	 * Class instance.
+	 *
+	 * @var Reviews|null instance
+	 */
+	protected static $instance;
+
+	/**
+	 * Reviews page hook name.
+	 *
+	 * @var string
+	 */
+	protected $reviews_page_hook;
+
+	/**
+	 * Reviews list table instance.
+	 *
+	 * @var ReviewsListTable
+	 */
+	protected $reviews_list_table;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_action( 'admin_menu', [ $this, 'add_reviews_page' ] );
+	}
+
+	/**
+	 * Gets the class instance.
+	 *
+	 * @return Reviews instance
+	 */
+	public static function get_instance(): ?Reviews {
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Registers the Reviews submenu page.
+	 *
+	 * @return void
+	 */
+	public function add_reviews_page() {
+		$this->reviews_page_hook = add_submenu_page(
+			'edit.php?post_type=product',
+			__( 'Reviews', 'woocommerce' ),
+			__( 'Reviews', 'woocommerce' ),
+			'moderate_comments',
+			'product-reviews',
+			[ $this, 'render_reviews_list_table' ]
+		);
+
+		add_action( "load-{$this->reviews_page_hook}", [ $this, 'load_reviews_screen' ] );
+	}
+
+	/**
+	 * Initializes the list table.
+	 *
+	 * @return void
+	 */
+	public function load_reviews_screen() {
+		$this->reviews_list_table = new ReviewsListTable( [ 'screen' => $this->reviews_page_hook ] );
+	}
+
+	/**
+	 * Renders the Reviews page.
+	 *
+	 * @return void
+	 */
+	public function render_reviews_list_table() {
+		$this->reviews_list_table->prepare_items();
+		?>
+		<div class="wrap">
+			<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+			<?php $this->reviews_list_table->views(); ?>
+
+			<form id="reviews-filter" method="get">
+
+				<input type="hidden" name="page" value="product-reviews" />
+
+				<?php $this->reviews_list_table->search_box( __( 'Search reviews', 'woocommerce' ), 'reviews' ); ?>
+
+				<?php $this->reviews_list_table->display(); ?>
+			</form>
+		</div>
+		<?php
+	}
+
 }
