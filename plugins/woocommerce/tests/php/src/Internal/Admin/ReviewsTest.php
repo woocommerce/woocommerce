1348d0f8751accd4c1973433aa8ce6fea43e3041
Fulvio Notarstefano <unfulvio@godaddy.com>
2022-04-12 09:48:57 +0800

    Add the admin Reviews handler tests file

---
+++ plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Automattic\WooCommerce\Tests\Internal\Admin;
+
+use WC_Unit_Test_Case;
+
+/**
+ * Tests for the admin reviews handler.
+ *
+ * @covers \Automattic\WooCommerce\Internal\Admin\Reviews
+ */
+class ReviewsTest extends WC_Unit_Test_Case
+{
+
+}
