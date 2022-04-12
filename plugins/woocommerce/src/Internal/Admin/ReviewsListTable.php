7055ba893f3edbaf58dd7366a89003c90e5a3762
Ashley Gibson <agibson@godaddy.com>
2022-04-12 12:49:13 +0100

    Set the primary column.

--- plugins/woocommerce/src/Internal/Admin/ReviewsListTable.php
+++ plugins/woocommerce/src/Internal/Admin/ReviewsListTable.php
@@ -13,6 +13,15 @@ use WP_List_Table;
 class ReviewsListTable extends WP_List_Table {
 
 	/**
+	 * Gets the name of the default primary column.
+	 *
+	 * @return string Name of the primary colum.
+	 */
+	protected function get_primary_column_name() {
+		return 'comment';
+	}
+
+	/**
 	 * Prepare reviews for display
 	 */
 	public function prepare_items() {
--- plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsListTableTest.php
+++ plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsListTableTest.php
@@ -2,6 +2,8 @@
 
 namespace Automattic\WooCommerce\Tests\Internal\Admin;
 
+use Automattic\WooCommerce\Internal\Admin\ReviewsListTable;
+use ReflectionClass;
 use WC_Unit_Test_Case;
 
 /**
@@ -10,5 +12,26 @@ use WC_Unit_Test_Case;
  * @covers \Automattic\WooCommerce\Internal\Admin\ReviewsListTable
  */
 class ReviewsListTableTest extends WC_Unit_Test_Case {
+
+	/**
+	 * Returns a new instance of the ReviewsListTable class.
+	 * @TODO Change the `screen` to be our page when it gets created in MWC-5330 {agibson 2022-04-12}
+	 *
+	 * @return ReviewsListTable
+	 */
+	protected function get_reviews_list_table() : ReviewsListTable {
+		return new ReviewsListTable( [ 'screen' => 'comments' ] );
+	}
+
+	/**
+	 * @covers \Automattic\WooCommerce\Internal\Admin\ReviewsListTable::get_primary_column_name()
+	 */
+	public function test_get_primary_column_name() {
+		$list_table = $this->get_reviews_list_table();
+		$method = ( new ReflectionClass( $list_table ) )->getMethod( 'get_primary_column_name' );
+		$method->setAccessible( true );
+
+		$this->assertSame( 'comment', $method->invoke( $list_table ) );
+	}
 
 }
