a3be982fc273d578acf3272bad298982ef52d1d9
Ashley Gibson <agibson@godaddy.com>
2022-04-12 16:50:17 +0100

    Add test.

--- plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsCommentsOverridesTest.php
+++ plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsCommentsOverridesTest.php
@@ -2,6 +2,7 @@
 
 namespace Automattic\WooCommerce\Tests\Internal\Admin;
 
+use Automattic\WooCommerce\Internal\Admin\ReviewsCommentsOverrides;
 use WC_Unit_Test_Case;
 
 /**
@@ -10,5 +11,22 @@ use WC_Unit_Test_Case;
  * @covers \Automattic\WooCommerce\Internal\Admin\ReviewsCommentsOverrides
  */
 class ReviewsCommentsOverridesTest extends WC_Unit_Test_Case {
+
+	/**
+	 * @covers \Automattic\WooCommerce\Internal\Admin\ReviewsCommentsOverrides::exclude_reviews_from_comments()
+	 */
+	public function test_exclude_reviews_from_comments() {
+		$overrides = new ReviewsCommentsOverrides();
+
+		$original_args = [
+			'post_type' => [ 'product' ],
+		];
+
+		$this->assertTrue( in_array( 'product', $original_args['post_type'] ) );
+
+		$new_args = $overrides->exclude_reviews_from_comments( $original_args );
+
+		$this->assertFalse( in_array( 'product', $new_args['post_type'] ) );
+	}
 
 }
