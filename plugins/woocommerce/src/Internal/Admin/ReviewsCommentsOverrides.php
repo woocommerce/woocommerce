17077a9daac7d86f2bc420075bdeae85ca07a5fe
Fulvio Notarstefano <unfulvio@godaddy.com>
2022-04-12 17:45:47 +0800

    Exclude product reviews from showing on the comments page

--- plugins/woocommerce/src/Internal/Admin/ReviewsCommentsOverrides.php
+++ plugins/woocommerce/src/Internal/Admin/ReviewsCommentsOverrides.php
@@ -2,9 +2,69 @@
 
 namespace Automattic\WooCommerce\Internal\Admin;
 
+use WP_Comment_Query;
+
 /**
  * Tweaks the WordPress comments page to exclude reviews.
  */
 class ReviewsCommentsOverrides {
+
+	/**
+	 * Class instance.
+	 *
+	 * @var ReviewsCommentsOverrides|null instance
+	 */
+	protected static $instance;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+
+		add_filter( 'comments_list_table_query_args', [ $this, 'exclude_reviews_from_comments' ] );
+	}
+
+	/**
+	 * Gets the class instance.
+	 *
+	 * @return object instance
+	 */
+	public static function get_instance() {
+
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Excludes product reviews from showing in the comments page.
+	 *
+	 * @internal
+	 *
+	 * @param array $args {@see WP_Comment_Query} query args.
+	 * @return array
+	 */
+	public function exclude_reviews_from_comments( $args ) {
+
+		$post_types = get_post_types();
+
+		if ( ! empty( $args['post_type'] ) ) {
+			if ( 'any' !== $args['post_type'] ) {
+				$post_types = (array) $args['post_type'];
+			}
+		}
+
+		$index = array_search( 'product', $post_types );
+
+		if ( false !== $index ) {
+			unset( $post_types[ $index ] );
+		}
+
+		$args['post_type'] = $post_types;
+
+		return $args;
+	}
 
 }
