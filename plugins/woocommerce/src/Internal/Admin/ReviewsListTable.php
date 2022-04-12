4709a06c5461358896015c8d2468a94fd22dcdc9
Fulvio Notarstefano <unfulvio@godaddy.com>
2022-04-12 11:02:51 +0800

    Add the ReviewsListTable class

---
+++ plugins/woocommerce/src/Internal/Admin/ReviewsListTable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Product > Reviews
+ */
+
+namespace Automattic\WooCommerce\Internal\Admin;
+
+use WP_List_Table;
+
+/**
+ * Handles the Product Reviews page.
+ */
+class ReviewsListTable extends WP_List_Table {
+
+}
