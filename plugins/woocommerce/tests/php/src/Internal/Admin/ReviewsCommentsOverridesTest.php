f092d0df29a0761590b86bbf15da2438419cfa1e
Fulvio Notarstefano <unfulvio@godaddy.com>
2022-04-12 11:55:22 +0800

    Add the ReviewsCommentsOverrides tests file

---
+++ plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsCommentsOverridesTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Automattic\WooCommerce\Tests\Internal\Admin;
+
+use WC_Unit_Test_Case;
+
+/**
+ * Tests the product reviews overrides for the comments page.
+ *
+ * @covers \Automattic\WooCommerce\Internal\Admin\ReviewsCommentsOverrides
+ */
+class ReviewsCommentsOverridesTest extends WC_Unit_Test_Case {
+
+}
