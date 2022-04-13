a4eef85238f189d7209ca3375c0bc4dc6670814b
Ashley Gibson <agibson@godaddy.com>
2022-04-13 11:37:14 +0100

    Revert "Escape late."
    This reverts commit faa2a5994bf9a05548f0f43f96cfb8ea541890aa.

--- plugins/woocommerce/src/Internal/Admin/ReviewsListTable.php
+++ plugins/woocommerce/src/Internal/Admin/ReviewsListTable.php
@@ -228,12 +228,12 @@ class ReviewsListTable extends WP_List_Table {
 
 			if ( current_user_can( 'edit_product', $product_post->ID ) ) :
 				$post_link  = "<a href='" . esc_url( get_edit_post_link( $product_post->ID ) ) . "' class='comments-edit-item-link'>";
-				$post_link .= get_the_title( $product_post->ID ) . '</a>';
+				$post_link .= esc_html( get_the_title( $product_post->ID ) ) . '</a>';
 			else :
-				$post_link = get_the_title( $product_post->ID );
+				$post_link = esc_html( get_the_title( $product_post->ID ) );
 			endif;
 
-			echo esc_html( $post_link );
+			echo $post_link; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 			$post_type_object = get_post_type_object( $product_post->post_type );
 
