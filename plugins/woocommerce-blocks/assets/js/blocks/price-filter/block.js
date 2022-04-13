230bc2f9f0ba18c8e568c40945285b421d556df9
Tung Du <dinhtungdu@gmail.com>
2022-04-13 11:31:55 +0700

    Fix: Shop page: Filter Products by Price block only works with Number of decimals set to 2 (https://github.com/woocommerce/woocommerce-blocks/pull/6229)

--- plugins/woocommerce-blocks/assets/js/blocks/price-filter/block.js
+++ plugins/woocommerce-blocks/assets/js/blocks/price-filter/block.js
@@ -46,7 +46,7 @@ function formatParams( url, params ) {
 
 	for ( const [ key, value ] of Object.entries( params ) ) {
 		if ( value ) {
-			paramObject[ key ] = ( value / 100 ).toString();
+			paramObject[ key ] = value.toString();
 		} else {
 			delete paramObject[ key ];
 		}
@@ -71,31 +71,32 @@ const PriceFilterBlock = ( { attributes, isEditor = false } ) => {
 		''
 	);
 
-	const minPriceParam = findGetParameter( 'min_price' );
-	const maxPriceParam = findGetParameter( 'max_price' );
-
-	const [ minPriceQuery, setMinPriceQuery ] = useQueryStateByKey(
-		'min_price',
-		Number( minPriceParam ) * 100 || null
-	);
-	const [ maxPriceQuery, setMaxPriceQuery ] = useQueryStateByKey(
-		'max_price',
-		Number( maxPriceParam ) * 100 || null
-	);
 	const [ queryState ] = useQueryStateByContext();
 	const { results, isLoading } = useCollectionData( {
 		queryPrices: true,
 		queryState,
 	} );
 
-	const [ minPrice, setMinPrice ] = useState(
-		Number( minPriceParam ) * 100 || null
+	const currency = getCurrencyFromPriceResponse( results.price_range );
+
+	const minPriceParam = findGetParameter( 'min_price' );
+	const maxPriceParam = findGetParameter( 'max_price' );
+
+	const [ minPriceQuery, setMinPriceQuery ] = useQueryStateByKey(
+		'min_price',
+		Number( minPriceParam ) * 10 ** currency.minorUnit || null
 	);
-	const [ maxPrice, setMaxPrice ] = useState(
-		Number( maxPriceParam ) * 100 || null
+	const [ maxPriceQuery, setMaxPriceQuery ] = useQueryStateByKey(
+		'max_price',
+		Number( maxPriceParam ) * 10 ** currency.minorUnit || null
 	);
 
-	const currency = getCurrencyFromPriceResponse( results.price_range );
+	const [ minPrice, setMinPrice ] = useState(
+		Number( minPriceParam ) * 10 ** currency.minorUnit || null
+	);
+	const [ maxPrice, setMaxPrice ] = useState(
+		Number( maxPriceParam ) * 10 ** currency.minorUnit || null
+	);
 
 	const { minConstraint, maxConstraint } = usePriceConstraints( {
 		minPrice: results.price_range
@@ -122,8 +123,8 @@ const PriceFilterBlock = ( { attributes, isEditor = false } ) => {
 			// For block templates that render the PHP Classic Template block we need to add the filters as params and reload the page.
 			if ( filteringForPhpTemplate && window ) {
 				const newUrl = formatParams( window.location.href, {
-					min_price: finalMinPrice,
-					max_price: finalMaxPrice,
+					min_price: finalMinPrice / 10 ** currency.minorUnit,
+					max_price: finalMaxPrice / 10 ** currency.minorUnit,
 				} );
 
 				// If the params have changed, lets reload the page.
@@ -141,6 +142,7 @@ const PriceFilterBlock = ( { attributes, isEditor = false } ) => {
 			setMinPriceQuery,
 			setMaxPriceQuery,
 			filteringForPhpTemplate,
+			currency,
 		]
 	);
 
