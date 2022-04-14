bac2c59daa0d4e9260bc4a6517dada2ed0e03f16
Luigi Teschio <gigitux@gmail.com>
2022-04-14 11:45:00 +0200

    Add Product Search Results Template (https://github.com/woocommerce/woocommerce-blocks/pull/6219)
    * Add Search Result Product Template woocommerce/woocommerce-blocks#6218
    
    Add Search Result Product Template
    
    * update title
    
    * add E2E test for Product Search Results template
    
    * renaming class
    
    * update E2E tests

--- plugins/woocommerce-blocks/tests/e2e/specs/backend/site-editing-templates.test.js
+++ plugins/woocommerce-blocks/tests/e2e/specs/backend/site-editing-templates.test.js
@@ -474,4 +474,95 @@ describe( 'Store Editing Templates', () => {
 			} );
 		} );
 	} );
+
+	describe( 'Product Search Results block template', () => {
+		it( 'default template from WooCommerce Blocks is available on an FSE theme', async () => {
+			const EXPECTED_TEMPLATE = defaultTemplateProps(
+				'Product Search Results'
+			);
+
+			await goToTemplatesList();
+
+			const templates = await getAllTemplates();
+
+			try {
+				expect( templates ).toContainEqual( EXPECTED_TEMPLATE );
+			} catch ( ok ) {
+				// Depending on the speed of the execution and whether Chrome is headless or not
+				// the id might be parsed or not
+
+				expect( templates ).toContainEqual( {
+					...EXPECTED_TEMPLATE,
+					addedBy: WOOCOMMERCE_PARSED_ID,
+				} );
+			}
+		} );
+
+		it( 'should contain the "WooCommerce Product Grid Block" classic template', async () => {
+			await goToTemplateEditor( {
+				postId: 'woocommerce/woocommerce//product-search-results',
+			} );
+
+			const [ classicBlock ] = await filterCurrentBlocks(
+				( block ) => block.name === BLOCK_DATA[ 'archive-product' ].name
+			);
+
+			expect( classicBlock.attributes.template ).toBe(
+				BLOCK_DATA[ 'archive-product' ].attributes.template
+			);
+			expect( await getCurrentSiteEditorContent() ).toMatchSnapshot();
+		} );
+
+		it( 'should show the action menu if the template has been customized by the user', async () => {
+			const EXPECTED_TEMPLATE = {
+				...defaultTemplateProps( 'Product Search Results' ),
+				hasActions: true,
+			};
+
+			await visitTemplateAndAddCustomParagraph(
+				'product-search-results'
+			);
+
+			await goToTemplatesList( { waitFor: 'actions' } );
+
+			const templates = await getAllTemplates();
+
+			try {
+				expect( templates ).toContainEqual( EXPECTED_TEMPLATE );
+			} catch ( ok ) {
+				// Depending on the speed of the execution and whether Chrome is headless or not
+				// the id might be parsed or not
+
+				expect( templates ).toContainEqual( {
+					...EXPECTED_TEMPLATE,
+					addedBy: WOOCOMMERCE_PARSED_ID,
+				} );
+			}
+		} );
+
+		it( 'should preserve and correctly show the user customization on the back-end', async () => {
+			await goToTemplateEditor( {
+				postId: 'woocommerce/woocommerce//product-search-results',
+			} );
+
+			await expect( canvas() ).toMatchElement(
+				SELECTORS.blocks.paragraph,
+				{
+					text: CUSTOMIZED_STRING,
+					timeout: DEFAULT_TIMEOUT,
+				}
+			);
+		} );
+
+		it( 'should show the user customization on the front-end', async () => {
+			await page.goto(
+				new URL( '?s=shirt&post_type=product', BASE_URL )
+			);
+
+			await expect( page ).toMatchElement( 'p', {
+				text: CUSTOMIZED_STRING,
+				timeout: DEFAULT_TIMEOUT,
+			} );
+		} );
+	} );
 } );
