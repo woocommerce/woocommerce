128f91dddb7ac58b3d1f14efb715f790f0c886b2
Jon Lane <jon.lane@automattic.com>
2022-04-14 12:19:38 -0700

    Add in API framework for setup and teardown

--- plugins/woocommerce/e2e/global-setup.js
+++ plugins/woocommerce/e2e/global-setup.js
@@ -31,6 +31,13 @@ module.exports = async (config) => {
 	await adminPage
 		.context()
 		.storageState({ path: 'e2e/storage/adminState.json' });
+	// While we're here, let's add a consumer token for API access
+	await adminPage.goto(`${baseURL}/wp-admin/admin.php?page=wc-settings&tab=advanced&section=keys&create-key=1`);
+	await adminPage.fill('#key_description', 'Key for API access');
+	await adminPage.selectOption('#key_permissions', 'read_write');
+	await adminPage.click('text=Generate API key');
+	process.env.CONSUMER_KEY = await adminPage.inputValue('#key_consumer_key');
+	process.env.CONSUMER_SECRET = await adminPage.inputValue('#key_consumer_secret');
 
 	// Sign in as customer user and save state
 	const customerPage = await browser.newPage();
