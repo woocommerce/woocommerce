981407da7912753352d766a4aa23b70eec5fba50
Fulvio Notarstefano <unfulvio@godaddy.com>
2022-04-14 11:28:24 +0800

    Helper method can be private

--- plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsListTableTest.php
+++ plugins/woocommerce/tests/php/src/Internal/Admin/ReviewsListTableTest.php
@@ -21,7 +21,7 @@ class ReviewsListTableTest extends WC_Unit_Test_Case {
 	 *
 	 * @return ReviewsListTable
 	 */
-	protected function get_reviews_list_table() : ReviewsListTable {
+	private function get_reviews_list_table() : ReviewsListTable {
 		return new ReviewsListTable( [ 'screen' => 'product_page_product-reviews' ] );
 	}
 
