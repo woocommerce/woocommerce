fb420020d3143c3724759aef687f01145730fd04
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-04-14 11:08:07 +0800

    Declare TS module for non-ts imports

--- plugins/woocommerce-admin/client/typings/index.d.ts
+++ plugins/woocommerce-admin/client/typings/index.d.ts
@@ -1,8 +1,10 @@
 declare module '@woocommerce/e2e-utils';
 declare module '@woocommerce/e2e-environment';
 declare module '@woocommerce/settings';
+declare module '@woocommerce/tracks';
 declare module '@wordpress/data';
 declare module '@wordpress/compose';
+declare module '@wordpress/plugins';
 declare module 'gridicons/dist/*' {
 	const value: React.ElementType< {
 		size?: 12 | 18 | 24 | 36 | 48 | 54 | 72;
@@ -10,3 +12,5 @@ declare module 'gridicons/dist/*' {
 	} >;
 	export default value;
 }
+declare module '*.png';
+declare module '*.jpg';
