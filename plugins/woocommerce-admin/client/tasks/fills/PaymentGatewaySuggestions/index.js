89b7415297565cca18a81cecc04c0a101c130f2b
Ilyas Foo <foo.ilyas@gmail.com>
2022-04-15 18:38:11 +0800

    Refactor and add unit tests

--- plugins/woocommerce-admin/client/tasks/fills/PaymentGatewaySuggestions/index.js
+++ plugins/woocommerce-admin/client/tasks/fills/PaymentGatewaySuggestions/index.js
@@ -25,25 +25,16 @@ import { List, Placeholder as ListPlaceholder } from './components/List';
 import { Setup, Placeholder as SetupPlaceholder } from './components/Setup';
 import { Toggle } from './components/Toggle/Toggle';
 import { WCPaySuggestion } from './components/WCPay';
-import { getPluginSlug } from '~/utils';
 import { getCountryCode } from '~/dashboard/utils';
+import {
+	getEnrichedPaymentGateways,
+	getSplitGateways,
+	getIsWCPayOrOtherCategoryDoneSetup,
+	getIsGatewayWCPay,
+	comparePaymentGatewaysByPriority,
+} from './utils';
 import './plugins/Bacs';
 import './payment-gateway-suggestions.scss';
-
-const comparePaymentGatewaysByPriority = ( a, b ) =>
-	a.recommendation_priority - b.recommendation_priority;
-
-const isGatewayWCPay = ( gateway ) =>
-	gateway.plugins?.length === 1 &&
-	gateway.plugins[ 0 ] === 'woocommerce-payments';
-
-const isGatewayOtherCategory = ( gateway, countryCode ) =>
-	gateway.category_other &&
-	gateway.category_other.indexOf( countryCode ) !== -1;
-
-const isGatewayAdditionalCategory = ( gateway, countryCode ) =>
-	gateway.category_additional &&
-	gateway.category_additional.indexOf( countryCode ) !== -1;
 
 export const PaymentGatewaySuggestions = ( { onComplete, query } ) => {
 	const { updatePaymentGateway } = useDispatch( PAYMENT_GATEWAYS_STORE_NAME );
@@ -73,54 +64,14 @@ export const PaymentGatewaySuggestions = ( { onComplete, query } ) => {
 		};
 	}, [] );
 
-	const getEnrichedPaymentGateways = () => {
-		const mappedPaymentGateways = installedPaymentGateways.reduce(
-			( map, gateway ) => {
-				map[ gateway.id ] = gateway;
-				return map;
-			},
-			{}
-		);
-
-		return paymentGatewaySuggestions.reduce( ( map, suggestion ) => {
-			// A colon ':' is used sometimes to have multiple configs for the same gateway ex: woocommerce_payments:us.
-			const id = getPluginSlug( suggestion.id );
-			const installedGateway = mappedPaymentGateways[ id ]
-				? mappedPaymentGateways[ id ]
-				: {};
-
-			const enrichedSuggestion = {
-				installed: !! mappedPaymentGateways[ id ],
-				postInstallScripts: installedGateway.post_install_scripts,
-				hasPlugins: !! (
-					suggestion.plugins && suggestion.plugins.length
-				),
-				enabled: installedGateway.enabled || false,
-				needsSetup: installedGateway.needs_setup,
-				settingsUrl: installedGateway.settings_url,
-				connectionUrl: installedGateway.connection_url,
-				setupHelpText: installedGateway.setup_help_text,
-				title: installedGateway.title,
-				requiredSettings: installedGateway.required_settings_keys
-					? installedGateway.required_settings_keys
-							.map(
-								( settingKey ) =>
-									installedGateway.settings[ settingKey ]
-							)
-							.filter( Boolean )
-					: [],
-				...suggestion,
-			};
-
-			map.set( suggestion.id, enrichedSuggestion );
-			return map;
-		}, new Map() );
-	};
-
-	const paymentGateways = useMemo( getEnrichedPaymentGateways, [
-		installedPaymentGateways,
-		paymentGatewaySuggestions,
-	] );
+	const paymentGateways = useMemo(
+		() =>
+			getEnrichedPaymentGateways(
+				installedPaymentGateways,
+				paymentGatewaySuggestions
+			),
+		[ installedPaymentGateways, paymentGatewaySuggestions ]
+	);
 
 	useEffect( () => {
 		if ( paymentGateways.size ) {
@@ -201,86 +152,30 @@ export const PaymentGatewaySuggestions = ( { onComplete, query } ) => {
 		return gateway;
 	}, [ isResolving, query, paymentGateways ] );
 
-	const isWCPayOrOtherCategoryDoneSetup = useMemo( () => {
-		for ( const [ , gateway ] of paymentGateways.entries() ) {
-			if ( ! gateway.installed || gateway.needsSetup ) {
-				continue;
-			}
-
-			if ( isGatewayWCPay( gateway ) ) {
-				return true;
-			}
-
-			if ( isGatewayOtherCategory( gateway, countryCode ) ) {
-				return true;
-			}
-		}
-		return false;
-	}, [ countryCode, paymentGateways ] );
+	const isWCPayOrOtherCategoryDoneSetup = useMemo(
+		() =>
+			getIsWCPayOrOtherCategoryDoneSetup( paymentGateways, countryCode ),
+		[ countryCode, paymentGateways ]
+	);
 
 	const isWCPaySupported =
-		Array.from( paymentGateways.values() ).findIndex( isGatewayWCPay ) !==
-		-1;
+		Array.from( paymentGateways.values() ).findIndex(
+			getIsGatewayWCPay
+		) !== -1;
 
 	const [ wcPayGateway, offlineGateways, additionalGateways ] = useMemo(
 		() =>
-			Array.from( paymentGateways.values() )
-				.sort( ( a, b ) => {
-					if ( a.hasPlugins === b.hasPlugins ) {
-						return comparePaymentGatewaysByPriority( a, b );
-					}
-
-					// hasPlugins payment first
-					if ( a.hasPlugins ) {
-						return -1;
-					}
-
-					return 1;
-				} )
-				.reduce(
-					( all, gateway ) => {
-						const [ wcPay, offline, additional ] = all;
-
-						// WCPay is handled separately when not installed and configured
-						if (
-							isGatewayWCPay( gateway ) &&
-							! ( gateway.installed && ! gateway.needsSetup )
-						) {
-							wcPay.push( gateway );
-						} else if ( gateway.is_offline ) {
-							offline.push( gateway );
-						} else if ( gateway.enabled ) {
-							// Enabled gateways should be ignored.
-						} else if ( isWCPayOrOtherCategoryDoneSetup ) {
-							// If WCPay or "other" gateway is enabled in an WCPay-eligible country, only
-							// allow to list "additional" gateways.
-							if (
-								isGatewayAdditionalCategory(
-									gateway,
-									countryCode
-								)
-							) {
-								additional.push( gateway );
-							}
-						} else if ( ! isWCPaySupported ) {
-							// When WCPay-ineligible, just show all gateways.
-							additional.push( gateway );
-						} else if (
-							isGatewayOtherCategory( gateway, countryCode )
-						) {
-							// When nothing is set up and eligible for WCPay, only show "other" gateways.
-							additional.push( gateway );
-						}
-
-						return all;
-					},
-					[ [], [], [] ]
-				),
+			getSplitGateways(
+				paymentGateways,
+				countryCode,
+				isWCPaySupported,
+				isWCPayOrOtherCategoryDoneSetup
+			),
 		[
+			paymentGateways,
 			countryCode,
 			isWCPaySupported,
 			isWCPayOrOtherCategoryDoneSetup,
-			paymentGateways,
 		]
 	);
 
