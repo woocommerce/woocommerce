27da9ef790f3ddc0127bb81f5adb8e9a6a16b25d
Tam Mullen <tammullen@outlook.com>
2022-04-15 11:24:04 +0100

    Removing debugging output

--- packages/js/e2e-utils/src/flows/merchant.js
+++ packages/js/e2e-utils/src/flows/merchant.js
@@ -497,9 +497,6 @@ const merchant = {
 
 		const uploader = await page.$( 'input[type=file]' );
 
-		console.log( 'pluginFilePath in uploadAndActivatePlugin before upload is: ' + pluginFilePath );
-
-
 		await uploader.uploadFile( pluginFilePath );
 
 		// Manually update the button to `enabled` so we can submit the file
@@ -534,7 +531,7 @@ const merchant = {
 	/**
 	 * Deactivate a plugin by the plugin's name with the option to delete the plugin as well.
 	 *
-	 * @param {string} pluginName The name of the plugin to deactivate. For example, `WooCommerce`.
+	 * @param {string}  pluginName   The name of the plugin to deactivate. For example, `WooCommerce`.
 	 * @param {boolean} deletePlugin Pass in `true` to delete the plugin. Defaults to `false`.
 	 */
 	deactivatePlugin: async ( pluginName, deletePlugin = false ) => {
