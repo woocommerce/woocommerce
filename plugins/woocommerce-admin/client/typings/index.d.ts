0c554fddadfbc56b6a4479e16b66e03e0c34cbaa
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-04-15 17:00:10 +0800

    Add @wp/plugins, data, compose types dev deps to woo admin
    Add their type definitions and remove them from index.d.ts declare module

--- plugins/woocommerce-admin/client/typings/index.d.ts
+++ plugins/woocommerce-admin/client/typings/index.d.ts
@@ -2,9 +2,6 @@ declare module '@woocommerce/e2e-utils';
 declare module '@woocommerce/e2e-environment';
 declare module '@woocommerce/settings';
 declare module '@woocommerce/tracks';
-declare module '@wordpress/data';
-declare module '@wordpress/compose';
-declare module '@wordpress/plugins';
 declare module 'gridicons/dist/*' {
 	const value: React.ElementType< {
 		size?: 12 | 18 | 24 | 36 | 48 | 54 | 72;
