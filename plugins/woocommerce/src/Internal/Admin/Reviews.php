26e0ab9cb072b61030ba8be168f3a17f1fab065e
Fulvio Notarstefano <unfulvio@godaddy.com>
2022-04-21 18:06:55 +0800

    Handle review replies when altering menu highlight or translation

--- plugins/woocommerce/src/Internal/Admin/Reviews.php
+++ plugins/woocommerce/src/Internal/Admin/Reviews.php
@@ -234,6 +234,10 @@ class Reviews {
 			$comment_id = absint( $_GET['c'] );
 			$comment = get_comment( $comment_id );
 
+			if ( $comment->comment_parent > 0 ) {
+				$comment = get_comment( $comment->comment_parent );
+			}
+
 			if ( $comment && 'product' === get_post_type( $comment->comment_post_ID ) ) {
 				$parent_file  = 'edit.php?post_type=product';
 				$submenu_file = 'product-reviews'; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
@@ -264,14 +268,21 @@ class Reviews {
 			$comment    = get_comment( $comment_id ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 		}
 
+		$is_reply = false;
+
+		if ( $comment && $comment->comment_parent > 0 ) {
+			$is_reply = true;
+			$comment = get_comment( $comment->comment_parent ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		}
+
 		// Only replace the translated text if we are editing a comment left on a product (ie. a review).
 		if ( $comment && 'product' === get_post_type( $comment->comment_post_ID ) ) {
 			if ( 'Edit Comment' === $text ) {
-				$translation = $comment->comment_parent > 0
+				$translation = $is_reply
 					? __( 'Edit Review Reply', 'woocommerce' )
 					: __( 'Edit Review', 'woocommerce' );
 			} elseif ( 'Moderate Comment' === $text ) {
-				$translation = $comment->comment_parent > 0
+				$translation = $is_reply
 					? __( 'Moderate Review Reply', 'woocommerce' )
 					: __( 'Moderate Review', 'woocommerce' );
 			}
