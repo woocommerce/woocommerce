f10bb2da82b5a198af0c1351803ea01dc2687368
Niels Lange <info@nielslange.de>
2022-04-25 10:48:54 +0200

    Critical flows: Merchant → Checkout → Can adjust T&S and Privacy Policy options (https://github.com/woocommerce/woocommerce-blocks/pull/6211)
    * Add test to check if T&C and Privacy links are visible
    
    * Fix filling wrong type of form error
    
    With a virtual product we shouldn't need to fill the shipping details
    We need to fill the billing instead
    
    * Fix undefined 'termsCheckbox' bug
    
    * Fix terms checkbox name
    
    * Activate test case
    
    * Replace static URL with dynamic one
    
    * Convert external plugin into mock
    
    * Fix JS linting issue
    
    Co-authored-by: Saad Tarhi <saad.trh@gmail.com>

---
+++ plugins/woocommerce-blocks/tests/e2e/specs/merchant/checkout-terms.test.js
@@ -0,0 +1,106 @@
+/**
+ * External dependencies
+ */
+import {
+	merchant,
+	openDocumentSettingsSidebar,
+	setCheckbox,
+	unsetCheckbox,
+} from '@woocommerce/e2e-utils';
+import {
+	visitBlockPage,
+	selectBlockByName,
+	saveOrPublish,
+} from '@woocommerce/blocks-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import {
+	shopper,
+	preventCompatibilityNotice,
+	reactivateCompatibilityNotice,
+} from '../../../utils';
+import {
+	BASE_URL,
+	BILLING_DETAILS,
+	SIMPLE_VIRTUAL_PRODUCT_NAME,
+} from '../../../utils/constants';
+
+if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 2 ) {
+	// eslint-disable-next-line jest/no-focused-tests
+	test.only( `Skipping checkout tests`, () => {} );
+}
+
+describe( 'Merchant → Checkout → Can adjust T&S and Privacy Policy options', () => {
+	beforeAll( async () => {
+		await shopper.goToShop();
+		await page.goto( `${ BASE_URL }/?setup_terms_and_privacy` );
+		await expect( page ).toMatch( 'Terms & Privacy pages set up.' );
+		await shopper.block.emptyCart();
+	} );
+
+	afterAll( async () => {
+		await shopper.block.emptyCart();
+		await page.goto( `${ BASE_URL }/?teardown_terms_and_privacy` );
+		await expect( page ).toMatch( 'Terms & Privacy pages teared down.' );
+	} );
+
+	it( 'Merchant can see T&S and Privacy Policy links without checkbox', async () => {
+		await shopper.goToShop();
+		await shopper.addToCartFromShopPage( SIMPLE_VIRTUAL_PRODUCT_NAME );
+		await shopper.block.goToCheckout();
+		await expect( page ).toMatch(
+			'By proceeding with your purchase you agree to our Terms and Conditions and Privacy Policy'
+		);
+		await shopper.block.placeOrder();
+		await expect( page ).toMatch( 'Order received' );
+	} );
+
+	it( 'Merchant can see T&S and Privacy Policy links with checkbox', async () => {
+		// Activate checkboxes for T&S and Privacy Policy links.
+		await preventCompatibilityNotice();
+		await merchant.login();
+		await visitBlockPage( 'Checkout Block' );
+		await openDocumentSettingsSidebar();
+		await selectBlockByName( 'woocommerce/checkout-terms-block' );
+		const [ termsCheckboxLabel ] = await page.$x(
+			`//label[contains(text(), "Require checkbox") and contains(@class, "components-toggle-control__label")]`
+		);
+		const termsCheckboxId = await page.evaluate(
+			( label ) => `#${ label.getAttribute( 'for' ) }`,
+			termsCheckboxLabel
+		);
+		await setCheckbox( termsCheckboxId );
+		await saveOrPublish();
+		await shopper.goToShop();
+		await shopper.addToCartFromShopPage( SIMPLE_VIRTUAL_PRODUCT_NAME );
+		await shopper.block.goToCheckout();
+		await shopper.block.fillBillingDetails( BILLING_DETAILS );
+
+		// Placing an order now, must lead to an error.
+		await page.click( '.wc-block-components-checkout-place-order-button' );
+
+		const termsCheckbox = await page.$(
+			'.wp-block-woocommerce-checkout-terms-block div'
+		);
+		const termsCheckboxClassList = (
+			await ( await termsCheckbox.getProperty( 'className' ) ).jsonValue()
+		 ).split( ' ' );
+		expect( termsCheckboxClassList ).toContain( 'has-error' );
+		await setCheckbox( '#terms-and-conditions' );
+
+		// Placing an order now, must succeed.
+		await shopper.block.placeOrder();
+		await expect( page ).toMatch( 'Order received' );
+
+		// Deactivate checkboxes for T&S and Privacy Policy links.
+		await visitBlockPage( 'Checkout Block' );
+		await openDocumentSettingsSidebar();
+		await selectBlockByName( 'woocommerce/checkout-terms-block' );
+		await unsetCheckbox( termsCheckboxId );
+		await saveOrPublish();
+		await merchant.logout();
+		await reactivateCompatibilityNotice();
+	} );
+} );
