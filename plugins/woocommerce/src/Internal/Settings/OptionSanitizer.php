d94b7af9dc15d10a74492b344c0fa7e6d115e848
Jorge A. Torres <jorge.torres@automattic.com>
2022-04-26 14:01:23 -0700

    Add OptionSanitizer class for sanitization of settings

---
+++ plugins/woocommerce/src/Internal/Settings/OptionSanitizer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * FormatValidator class.
+ */
+
+namespace Automattic\WooCommerce\Internal\Settings;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * This class handles sanitization of core options that need to conform to certain format.
+ *
+ * @since 6.6.0
+ */
+class OptionSanitizer {
+
+	/**
+	 * OptionSanitizer constructor.
+	 */
+	public function __construct() {
+	}
+
+}
