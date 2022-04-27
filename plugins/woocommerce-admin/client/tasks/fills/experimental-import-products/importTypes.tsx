de836d1f796c7ab477918c3056c4ea0c68ee4220
moon <moon.kyong@automattic.com>
2022-04-27 13:58:55 -0700

    Add Learn More link

--- plugins/woocommerce-admin/client/tasks/fills/experimental-import-products/importTypes.tsx
+++ plugins/woocommerce-admin/client/tasks/fills/experimental-import-products/importTypes.tsx
@@ -4,6 +4,9 @@
 import { __ } from '@wordpress/i18n';
 import PageIcon from 'gridicons/dist/pages';
 import ReblogIcon from 'gridicons/dist/reblog';
+import { getAdminLink } from '@woocommerce/settings';
+import interpolateComponents from '@automattic/interpolate-components';
+import { ExternalLink } from '@wordpress/components';
 
 export const importTypes = [
 	{
@@ -14,14 +17,24 @@ export const importTypes = [
 			'woocommerce'
 		),
 		before: <PageIcon />,
+		href: getAdminLink(
+			'edit.php?post_type=product&page=product_importer&wc_onboarding_active_task=products'
+		),
 	},
 	{
 		key: 'from-cart2cart' as const,
 		title: __( 'FROM CART2CART', 'woocommerce' ),
-		content: __(
-			'Migrate all store data like products, customers, orders and much more in no time and in a fully automated way',
-			'woocommerce'
-		),
+		content: interpolateComponents( {
+			mixedString: __(
+				'Migrate all store data like products, customers, and orders in no time with this 3rd party plugin. {{link}}Learn more{{/link}}',
+				'woocommerce'
+			),
+			components: {
+				link: (
+					<ExternalLink href="https://woocommerce.com/products/cart2cart/?utm_medium=product"></ExternalLink>
+				),
+			},
+		} ),
 		before: <ReblogIcon />,
 	},
 ];
