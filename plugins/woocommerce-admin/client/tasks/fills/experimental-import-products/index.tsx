b109eba8c7a54232fdcf6171a97b3fec41755274
moon <moon.kyong@automattic.com>
2022-04-27 14:02:01 -0700

    Adjust styles and add initial stacks functionality

--- plugins/woocommerce-admin/client/tasks/fills/experimental-import-products/index.tsx
+++ plugins/woocommerce-admin/client/tasks/fills/experimental-import-products/index.tsx
@@ -3,18 +3,35 @@
  */
 import { WooOnboardingTask } from '@woocommerce/onboarding';
 import { registerPlugin } from '@wordpress/plugins';
+import { __ } from '@wordpress/i18n';
+import { Icon, chevronUp, chevronDown } from '@wordpress/icons';
+import { Button } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import CardList from './CardList';
 import { importTypes } from './importTypes';
+import './style.scss';
+
+const Stacks = () => {
+	return <div>Hello</div>;
+};
 
 const Products = () => {
+	const [ showStacks, setStackVisibility ] = useState< boolean >( false );
 	return (
-		<div>
-			<h1>Import products</h1>
+		<div className="woocommerce-task-import-products">
+			<h1>{ __( 'Import your products', 'woocommerce' ) }</h1>
 			<CardList items={ importTypes } />
+			<div className="woocommerce-task-import-products-stacks">
+				<Button onClick={ () => setStackVisibility( ! showStacks ) }>
+					{ __( 'Or add your products from scratch', 'woocommerce' ) }
+					<Icon icon={ showStacks ? chevronUp : chevronDown } />
+				</Button>
+				{ showStacks && <Stacks /> }
+			</div>
 		</div>
 	);
 };
