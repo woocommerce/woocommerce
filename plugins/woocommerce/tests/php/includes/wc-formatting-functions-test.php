19d230af6336bb8582fd123d2ced7250c92c4ebd
vedanshujain <vedanshu.jain.2012@gmail.com>
2022-04-27 11:40:51 +0530

    Merge latvian formatting test with other tests for DRY.

--- plugins/woocommerce/tests/php/includes/wc-formatting-functions-test.php
+++ plugins/woocommerce/tests/php/includes/wc-formatting-functions-test.php
@@ -17,31 +17,4 @@ class WC_Formatting_Functions_Test extends \WC_Unit_Test_Case {
 		$this->assertEquals( 'DUMMYCOUPON', wc_sanitize_coupon_code( 'DUMMYCOUPON' ) );
 		$this->assertEquals( 'a&amp;a', wc_sanitize_coupon_code( 'a&a' ) );
 	}
-
-	/**
-	 * Test wc_format_postcode() function.
-	 */
-	public function test_wc_format_postcode() {
-		// IE postcode.
-		$this->assertEquals( 'D02 AF30', wc_format_postcode( 'D02AF30', 'IE' ) );
-
-		// PT postcode.
-		$this->assertEquals( '1000-205', wc_format_postcode( '1000205', 'PT' ) );
-
-		// BR/PL postcode.
-		$this->assertEquals( '99999-999', wc_format_postcode( '99999999', 'BR' ) );
-
-		// JP postcode.
-		$this->assertEquals( '999-9999', wc_format_postcode( '9999999', 'JP' ) );
-
-		// Test LV postcode without mandatory country code.
-		$this->assertEquals( 'LV-1337', wc_format_postcode( '1337', 'LV' ) );
-
-		// Test LV postcode with incorrect format (no dash).
-		$this->assertEquals( 'LV-1337', wc_format_postcode( 'lv1337', 'LV' ) );
-
-		// Test empty NL postcode.
-		$this->assertEquals( '', wc_format_postcode( '', 'NL' ) );
-
-	}
 }
