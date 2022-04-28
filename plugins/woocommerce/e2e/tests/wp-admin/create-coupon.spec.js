2933f107e6a1d23f0fc8e79f8c2e88da5c132f2c
Jon Lane <jon.lane@automattic.com>
2022-04-28 09:44:59 -0700

    Fix spacing in file

--- plugins/woocommerce/e2e/tests/wp-admin/create-coupon.spec.js
+++ plugins/woocommerce/e2e/tests/wp-admin/create-coupon.spec.js
@@ -1,20 +1,25 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require( '@playwright/test' );
 
-test.describe('Add New Coupon Page', () => {
-	test.use({ storageState: 'e2e/storage/adminState.json' });
+test.describe( 'Add New Coupon Page', () => {
+	test.use( { storageState: 'e2e/storage/adminState.json' } );
 
-	test('can create new coupon', async ({ page }) => {
-		await page.goto('wp-admin/post-new.php?post_type=shop_coupon');
-		await page.fill('#title', `code-${new Date().getTime().toString()}`);
-		await page.fill('#woocommerce-coupon-description', 'test coupon');
+	test( 'can create new coupon', async ( { page } ) => {
+		await page.goto( 'wp-admin/post-new.php?post_type=shop_coupon' );
+		await page.fill(
+			'#title',
+			`code-${ new Date().getTime().toString() }`
+		);
+		await page.fill( '#woocommerce-coupon-description', 'test coupon' );
 
-		await page.fill('#coupon_amount', '100');
+		await page.fill( '#coupon_amount', '100' );
 
-		await page.click('#publish');
+		await page.click( '#publish' );
 
-		await expect(page.locator('div.notice.notice-success')).toHaveText('Coupon updated.Dismiss this notice.');
+		await expect( page.locator( 'div.notice.notice-success' ) ).toHaveText(
+			'Coupon updated.Dismiss this notice.'
+		);
 
 		// delete the coupon
-		await page.dispatchEvent('a.submitdelete', 'click');
-	});
-});
+		await page.dispatchEvent( 'a.submitdelete', 'click' );
+	} );
+} );
