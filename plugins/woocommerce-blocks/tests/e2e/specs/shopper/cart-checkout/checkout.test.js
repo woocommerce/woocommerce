8cfe66d82bbe46c500bc4d42c8089d88b2ad7c5b
Tarun Vijwani <tarun.vijwani@automattic.com>
2022-05-05 12:19:38 +0400

    Meaningful comment for skipping the e2e tests (https://github.com/woocommerce/woocommerce-blocks/pull/6353)
    * Added meaningful comment for skipping the tests if it's a WooCommerce Core process environment.
    
    * Fixed the flag number for MiniCart

--- plugins/woocommerce-blocks/tests/e2e/specs/shopper/cart-checkout/checkout.test.js
+++ plugins/woocommerce-blocks/tests/e2e/specs/shopper/cart-checkout/checkout.test.js
@@ -31,6 +31,7 @@ import {
 import { createCoupon } from '../../../utils';
 
 if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 2 ) {
+	// Skips all the tests if it's a WooCommerce Core process environment.
 	// eslint-disable-next-line jest/no-focused-tests
 	test.only( 'Skipping Cart & Checkout tests', () => {} );
 }
