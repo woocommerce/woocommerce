aa1a6189481a757217ca9f6d35b6974df36a30f0
Fulvio Notarstefano <unfulvio@godaddy.com>
2022-05-12 11:47:52 +0800

    Update Product Reviews page hooks since tags

--- plugins/woocommerce/includes/admin/class-wc-admin-notices.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-notices.php
@@ -174,6 +174,8 @@ class WC_Admin_Notices {
 			/**
 			 * Filter the capability required to dismiss a given notice.
 			 *
+			 * @since 6.6.0
+			 *
 			 * @param string $default_capability The default required capability.
 			 * @param string $notice_name The notice name.
 			 */
