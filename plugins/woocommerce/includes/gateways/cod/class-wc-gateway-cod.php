c0125046f265c9ca8db268983133b2495ceb5788
barryhughes <3594411+barryhughes@users.noreply.github.com>
2022-05-13 21:24:40 +0000

    Introduce a 'safe_text' field that allows a reduced subset of HTML tags.

--- plugins/woocommerce/includes/gateways/cod/class-wc-gateway-cod.php
+++ plugins/woocommerce/includes/gateways/cod/class-wc-gateway-cod.php
@@ -75,7 +75,7 @@ class WC_Gateway_COD extends WC_Payment_Gateway {
 			),
 			'title'              => array(
 				'title'       => __( 'Title', 'woocommerce' ),
-				'type'        => 'text',
+				'type'        => 'safe_text',
 				'description' => __( 'Payment method description that the customer will see on your checkout.', 'woocommerce' ),
 				'default'     => __( 'Cash on delivery', 'woocommerce' ),
 				'desc_tip'    => true,
