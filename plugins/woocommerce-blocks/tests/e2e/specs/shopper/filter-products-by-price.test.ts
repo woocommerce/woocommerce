bf9f04dd4651dabe8f12d5b27b93378b7eed76ab
Luigi Teschio <gigitux@gmail.com>
2022-05-13 14:15:33 +0200

    Add E2E tests for the Filter Products By Price Block (https://github.com/woocommerce/woocommerce-blocks/pull/6373)
    * Add E2E tests for the Filter Products By Price Block woocommerce/woocommerce-blocks#6363
    
    add E2E tests for the Filter Products By Price Block
    
    * Add E2E tests for the Filter Products By Attribute Block woocommerce/woocommerce-blocks#6322
    
    add E2E tests for the Filter Products By Attribute Block
    
    * Add E2E tests for the Filter Products By Stock Block woocommerce/woocommerce-blocks#6323
    
    add E2E tests for the Filter Products By Stock Block
    
    * improve checks
    
    * improve checks
    
    * remove globals.d.ts file
    
    * replace waitForTimeout

--- plugins/woocommerce-blocks/tests/e2e/specs/shopper/filter-products-by-price.test.ts
+++ plugins/woocommerce-blocks/tests/e2e/specs/shopper/filter-products-by-price.test.ts
@@ -1,12 +1,10 @@
 import {
-	canvas,
 	createNewPost,
 	deleteAllTemplates,
 	insertBlock,
 	switchUserToAdmin,
 	publishPost,
 } from '@wordpress/e2e-test-utils';
-import { Frame } from 'puppeteer';
 import { selectBlockByName } from '@woocommerce/blocks-test-utils';
 import {
 	BASE_URL,
@@ -17,29 +15,25 @@ import {
 } from '../../utils';
 
 const block = {
-	name: 'Filter Products by Attribute',
-	slug: 'woocommerce/attribute-filter',
-	class: '.wc-block-attribute-filter',
+	name: 'Filter Products by Price',
+	slug: 'woocommerce/price-filter',
+	class: '.wc-block-price-filter',
 	selectors: {
 		editor: {
-			firstAttributeInTheList:
-				'.woocommerce-search-list__list > li > label > input.woocommerce-search-list__item-input',
 			filterButtonToggle: "//label[text()='Filter button']",
-			doneButton: '.wc-block-attribute-filter__selection > button',
 		},
 		frontend: {
-			firstAttributeInTheList:
-				'.wc-block-attribute-filter-list > li:not([class^="is-loading"])',
+			priceMaxAmount: '.wc-block-price-filter__amount--max',
 			productsList: '.wc-block-grid__products > li',
 			classicProductsList: '.products.columns-3 > li',
-			filter: "input[id='128gb']",
 			submitButton: '.wc-block-components-filter-submit-button',
 		},
 	},
-	urlSearchParamWhenFilterIsApplied:
-		'?filter_capacity=128gb&query_type_capacity=or',
-	foundProduct: '128GB USB Stick',
+	urlSearchParamWhenFilterIsApplied: '?max_price=1.99',
+	foundProduct: '32GB USB Stick',
 };
+
+const { selectors } = block;
 
 const waitForAllProductsBlockLoaded = () =>
 	page.waitForSelector( selectors.frontend.productsList + '.is-loading', {
@@ -51,7 +45,17 @@ const goToShopPage = () =>
 		waitUntil: 'networkidle0',
 	} );
 
-const { selectors } = block;
+const setMaxPrice = async () => {
+	await page.waitForSelector( selectors.frontend.priceMaxAmount );
+	await page.focus( selectors.frontend.priceMaxAmount );
+	await page.click( selectors.frontend.priceMaxAmount, {
+		clickCount: 3,
+	} );
+	await page.keyboard.type( '1.99' );
+	await page.$eval( selectors.frontend.priceMaxAmount, ( el ) =>
+		( el as HTMLElement ).blur()
+	);
+};
 
 describe( `${ block.name } Block`, () => {
 	describe( 'with All Products Block', () => {
@@ -63,15 +67,12 @@ describe( `${ block.name } Block`, () => {
 			} );
 
 			await insertBlock( block.name );
-			await page.click( selectors.editor.firstAttributeInTheList );
-			await page.click( selectors.editor.doneButton );
 			await insertBlock( 'All Products' );
 			await publishPost();
 
 			const link = await page.evaluate( () =>
 				wp.data.select( 'core/editor' ).getPermalink()
 			);
-
 			await page.goto( link );
 		} );
 
@@ -85,7 +86,8 @@ describe( `${ block.name } Block`, () => {
 		it( 'should show only products that match the filter', async () => {
 			const isRefreshed = jest.fn( () => void 0 );
 			page.on( 'load', isRefreshed );
-			await page.click( selectors.frontend.filter );
+			await setMaxPrice();
+			await page.waitForNetworkIdle();
 			await waitForAllProductsBlockLoaded();
 			const products = await page.$$( selectors.frontend.productsList );
 
@@ -108,14 +110,6 @@ describe( `${ block.name } Block`, () => {
 				postId: productCatalogTemplateId,
 			} );
 			await insertBlock( block.name );
-			const canvasEl: Frame = canvas();
-
-			// It seems that .click doesn't work well with radio input element.
-			await canvasEl.$eval(
-				block.selectors.editor.firstAttributeInTheList,
-				( el ) => ( el as HTMLInputElement ).click()
-			);
-			await canvasEl.click( selectors.editor.doneButton );
 			await saveTemplate();
 			await goToShopPage();
 		} );
@@ -141,13 +135,14 @@ describe( `${ block.name } Block`, () => {
 				hidden: true,
 			} );
 
+			await expect( page ).toMatch( block.foundProduct );
 			expect( isRefreshed ).not.toBeCalled();
 
 			await Promise.all( [
+				setMaxPrice(),
 				page.waitForNavigation( {
 					waitUntil: 'networkidle0',
 				} ),
-				page.click( selectors.frontend.filter ),
 			] );
 
 			const products = await page.$$(
@@ -159,10 +154,10 @@ describe( `${ block.name } Block`, () => {
 
 			expect( isRefreshed ).toBeCalledTimes( 1 );
 			expect( products ).toHaveLength( 1 );
-			await expect( page ).toMatch( block.foundProduct );
 			expect( parsedURL.search ).toEqual(
 				block.urlSearchParamWhenFilterIsApplied
 			);
+			await expect( page ).toMatch( block.foundProduct );
 		} );
 
 		it( 'should refresh the page only if the user click on button', async () => {
@@ -172,6 +167,9 @@ describe( `${ block.name } Block`, () => {
 
 			await selectBlockByName( block.slug );
 			await openBlockEditorSettings();
+			await page.waitForXPath(
+				block.selectors.editor.filterButtonToggle
+			);
 			const [ filterButtonToggle ] = await page.$x(
 				block.selectors.editor.filterButtonToggle
 			);
@@ -184,16 +182,15 @@ describe( `${ block.name } Block`, () => {
 			await page.waitForSelector( block.class + '.is-loading', {
 				hidden: true,
 			} );
-			await page.waitForSelector( selectors.frontend.filter );
-			await page.click( selectors.frontend.filter );
-
 			expect( isRefreshed ).not.toBeCalled();
 
+			await setMaxPrice();
+
 			await Promise.all( [
+				page.click( selectors.frontend.submitButton ),
 				page.waitForNavigation( {
 					waitUntil: 'networkidle0',
 				} ),
-				page.click( selectors.frontend.submitButton ),
 			] );
 
 			const products = await page.$$(
