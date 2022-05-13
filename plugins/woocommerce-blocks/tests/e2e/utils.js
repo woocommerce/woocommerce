783533b7e1f9424ebf414dc0c636aab9dd150174
Luigi Teschio <gigitux@gmail.com>
2022-05-13 15:18:51 +0200

    Add E2E tests for the Active Filters Block (https://github.com/woocommerce/woocommerce-blocks/pull/6393)
    * wip
    
    * set editor using env var instead
    
    * fix attribute missing issue when watching the test
    
    * wip php templates test
    
    * Add E2E tests for the Filter Products By Price Block woocommerce/woocommerce-blocks#6363
    
    add E2E tests for the Filter Products By Price Block
    
    * Add E2E tests for the Filter Products By Price Block woocommerce/woocommerce-blocks#6363
    
    add E2E tests for the Filter Products By Price Block
    
    * create clickLink util
    
    * Add E2E tests for the Active Filters Block woocommerce/woocommerce-blocks#6377
    
    add E2E tests for the Active Filters Block
    
    * Add E2E tests for the Filter Products By Attribute Block woocommerce/woocommerce-blocks#6322
    
    add E2E tests for the Filter Products By Attribute Block
    
    * Add E2E tests for the Filter Products By Stock Block woocommerce/woocommerce-blocks#6323
    
    add E2E tests for the Filter Products By Stock Block
    
    * improve checks
    
    * improve checks
    
    * improve checks
    
    * remove globals.d.ts file
    
    * replace waitForTimeout
    
    * create clickLint utils
    
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>

--- plugins/woocommerce-blocks/tests/e2e/utils.js
+++ plugins/woocommerce-blocks/tests/e2e/utils.js
@@ -447,3 +447,15 @@ export const openBlockEditorSettings = async () => {
 		await page.click( buttonSelector );
 	}
 };
+
+/**
+ * Click a link and wait for the page to load.
+ *
+ * @param {string} selector The CSS selector of the link to click.
+ */
+export const clickLink = async ( selector ) => {
+	await Promise.all( [
+		page.click( selector ),
+		page.waitForNavigation( { waitUntil: 'networkidle0' } ),
+	] );
+};
