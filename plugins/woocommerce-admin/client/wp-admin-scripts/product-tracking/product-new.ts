030a7ede6e4f5954b9b4b835c311cc5cdc42259f
Joshua Flowers <joshuatf@gmail.com>
2022-05-19 12:22:32 -0400

    Add product screen tracks

--- plugins/woocommerce-admin/client/wp-admin-scripts/product-tracking/product-new.ts
+++ plugins/woocommerce-admin/client/wp-admin-scripts/product-tracking/product-new.ts
@@ -3,10 +3,16 @@
  */
 import { recordEvent } from '@woocommerce/tracks';
 
+/**
+ * Internal dependencies
+ */
+import { initProductScreenTracks } from './shared';
+
 const initTracks = () => {
 	recordEvent( 'product_add_view' );
 };
 
 if ( window.location.pathname.split( '/' ).pop() === 'post-new.php' ) {
 	initTracks();
+	initProductScreenTracks();
 }
