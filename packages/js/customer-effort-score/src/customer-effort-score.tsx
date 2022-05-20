e16aae9a82f3c25bec7a66db350f5753c1d77937
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-05-20 14:35:48 +0800

    Update icon type

--- packages/js/customer-effort-score/src/customer-effort-score.tsx
+++ packages/js/customer-effort-score/src/customer-effort-score.tsx
@@ -19,7 +19,7 @@ type CustomerEffortScoreProps = {
 	onNoticeShownCallback?: () => void;
 	onNoticeDismissedCallback?: () => void;
 	onModalShownCallback?: () => void;
-	icon?: React.ReactNode;
+	icon?: React.ReactElement | null;
 };
 
 /**
