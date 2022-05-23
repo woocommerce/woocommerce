eec68ed3711974bd69a3593e5db671e957005838
Alba Rincón <albarin@users.noreply.github.com>
2022-05-23 10:14:51 +0200

    Convert `Handpicked Products` block to TS (https://github.com/woocommerce/woocommerce-blocks/pull/6417)
    * Convert `Handpicked Products` block to TS
    
    * Extract InspectorControls and EditMode into components
    
    * Refactor edit and remove the `editMode` attribute
    
    * Change `isEditing` default depending on products selected or not
    
    * Improve interfaces/props after review
    
    * Move withSpokenMessages to wrap the whole Edit block

--- plugins/woocommerce-blocks/assets/js/blocks/handpicked-products/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/handpicked-products/edit.tsx
@@ -1,20 +1,63 @@
 /**
  * External dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
-
+import { BlockControls, useBlockProps } from '@wordpress/block-editor';
+import {
+	ToolbarGroup,
+	Disabled,
+	withSpokenMessages,
+} from '@wordpress/components';
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import Block from './block';
 import './editor.scss';
+import { Props } from './types';
+import { HandpickedProductsInspectorControls } from './inspector-controls';
+import { HandpickedProductsEditMode } from './edit-mode';
+import { HandpickedProductsBlock } from './block';
 
-export const Edit = ( props: unknown ): JSX.Element => {
+export const EditBlock = ( props: Props ): JSX.Element => {
 	const blockProps = useBlockProps();
+
+	const {
+		attributes: { products },
+	} = props;
+
+	const [ isEditing, setIsEditing ] = useState( ! products.length );
 
 	return (
 		<div { ...blockProps }>
-			<Block { ...props } />
+			<BlockControls>
+				<ToolbarGroup
+					controls={ [
+						{
+							icon: 'edit',
+							title: __(
+								'Edit selected products',
+								'woo-gutenberg-products-block'
+							),
+							onClick: () => setIsEditing( ! isEditing ),
+							isActive: isEditing,
+						},
+					] }
+				/>
+			</BlockControls>
+			<HandpickedProductsInspectorControls { ...props } />
+			{ isEditing ? (
+				<HandpickedProductsEditMode
+					isEditing={ isEditing }
+					setIsEditing={ setIsEditing }
+					{ ...props }
+				/>
+			) : (
+				<Disabled>
+					<HandpickedProductsBlock { ...props } />
+				</Disabled>
+			) }
 		</div>
 	);
 };
+
+export const Edit = withSpokenMessages( EditBlock );
