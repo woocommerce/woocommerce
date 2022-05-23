0faf047b09ad84d6fe8dd2e200b06e80a6679c28
Alba Rincón <albarin@users.noreply.github.com>
2022-05-23 10:45:54 +0200

    Convert `Products by Attribute` to TS (https://github.com/woocommerce/woocommerce-blocks/pull/6418)
    * Convert `Products by Attribute` block to TS
    
    * Small tweaks after review
    
    * Extract InspectorControls and EditMode into components
    
    * Refactor: move the edit related code to the edit.jsx file
    
    * Remove the `editMode` attribute and control the edit mode inside the edit component
    
    * Remove edit mode
    
    * Change `isEditing` depending on attributes are selected or not
    
    * Improved interfaces/props after review
    
    * Add enum allowed values to `attrOperator` and `orderby` on the `block.json`
    
    * Move `withSpokenMessages` to wrap the whole Edit block

--- plugins/woocommerce-blocks/assets/js/blocks/products-by-attribute/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/products-by-attribute/edit.tsx
@@ -1,20 +1,64 @@
 /**
  * External dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { BlockControls, useBlockProps } from '@wordpress/block-editor';
+import {
+	Disabled,
+	ToolbarGroup,
+	withSpokenMessages,
+} from '@wordpress/components';
+import { useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import Block from './block';
 import './editor.scss';
+import { Props } from './types';
+import { ProductsByAttributeInspectorControls } from './inspector-controls';
+import { ProductsByAttributeEditMode } from './edit-mode';
+import { ProductsByAttributeBlock } from './block';
 
-export const Edit = ( props: unknown ): JSX.Element => {
+export const EditBlock = ( props: Props ): JSX.Element => {
 	const blockProps = useBlockProps();
+
+	const {
+		attributes: { attributes },
+	} = props;
+
+	const [ isEditing, setIsEditing ] = useState( ! attributes.length );
 
 	return (
 		<div { ...blockProps }>
-			<Block { ...props } />
+			<BlockControls>
+				<ToolbarGroup
+					controls={ [
+						{
+							icon: 'edit',
+							title: __(
+								'Edit selected attribute',
+								'woo-gutenberg-products-block'
+							),
+							onClick: () => setIsEditing( ! isEditing ),
+							isActive: isEditing,
+						},
+					] }
+				/>
+			</BlockControls>
+			<ProductsByAttributeInspectorControls { ...props } />
+			{ isEditing ? (
+				<ProductsByAttributeEditMode
+					isEditing={ isEditing }
+					setIsEditing={ setIsEditing }
+					{ ...props }
+				/>
+			) : (
+				<Disabled>
+					<ProductsByAttributeBlock { ...props } />
+				</Disabled>
+			) }
 		</div>
 	);
 };
+
+export const Edit = withSpokenMessages( EditBlock );
