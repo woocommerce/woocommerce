fdde40bd62fb4e1d962e050231fe36e0b81e4565
Tung Du <dinhtungdu@gmail.com>
2022-05-31 07:42:28 +0700

    Show a warning to notify the user if the WC Blocks version is behind the one bundled in WC Core (https://github.com/woocommerce/woocommerce-blocks/pull/6459)
    Co-authored-by: Albert Juhé Lluveras <contact@albertjuhe.com>

--- plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
+++ plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
@@ -238,3 +238,40 @@ function woocommerce_blocks_get_php_translation_from_core( $translation, $text, 
 }
 
 add_filter( 'gettext', 'woocommerce_blocks_get_php_translation_from_core', 10, 3 );
+
+/**
+ * Add notice to the admin dashboard if the plugin is outdated.
+ *
+ * @see https://github.com/woocommerce/woocommerce-blocks/issues/5587
+ */
+function woocommerce_blocks_plugin_outdated_notice() {
+	$is_active =
+		is_plugin_active( 'woo-gutenberg-products-block/woocommerce-gutenberg-products-block.php' ) ||
+		is_plugin_active( 'woocommerce-gutenberg-products-block/woocommerce-gutenberg-products-block.php' ) ||
+		is_plugin_active( 'woocommerce-blocks/woocommerce-gutenberg-products-block.php' );
+
+	if ( ! $is_active ) {
+		return;
+	}
+
+	$woocommerce_blocks_path = \Automattic\WooCommerce\Blocks\Package::get_path();
+
+	/**
+	 * Check the current WC Blocks path. If the WC Blocks plugin is active but
+	 * the current path is from the WC Core, we can consider the plugin is
+	 * outdated because Jetpack Autoloader always loads the newer package.
+	 */
+	if ( ! strpos( $woocommerce_blocks_path, 'packages/woocommerce-blocks' ) ) {
+		return;
+	}
+
+	if ( should_display_compatibility_notices() ) {
+		?>
+		<div class="notice notice-warning">
+			<p><?php esc_html_e( 'You have WooCommerce Blocks installed, but the WooCommerce bundled version is running because it is more up-to-date. This may cause unexpected compatibility issues. Please update the WooCommerce Blocks plugin.', 'woo-gutenberg-products-block' ); ?></p>
+		</div>
+		<?php
+	}
+}
+
+add_action( 'admin_notices', 'woocommerce_blocks_plugin_outdated_notice' );
