a5d17636cf588776cc66dddc9931aa4a95b7a682
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-06-02 14:25:00 +0800

    Export TourKitTypes

--- packages/js/components/src/index.ts
+++ packages/js/components/src/index.ts
@@ -59,3 +59,4 @@ export { default as WebPreview } from './web-preview';
 export { Badge } from './badge';
 export { DynamicForm } from './dynamic-form';
 export { default as TourKit } from './tour-kit';
+export * as TourKitTypes from './tour-kit/types';
