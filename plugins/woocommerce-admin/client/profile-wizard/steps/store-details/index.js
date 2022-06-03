62f25a8e6161cd371d0bd7382276b5cc4c2c2252
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2022-06-03 15:58:40 +0800

    Add inline documentation

--- plugins/woocommerce-admin/client/profile-wizard/steps/store-details/index.js
+++ plugins/woocommerce-admin/client/profile-wizard/steps/store-details/index.js
@@ -523,6 +523,8 @@ export default compose(
 			city: settings.woocommerce_store_city || '',
 			countryState,
 			postCode: settings.woocommerce_store_postcode || '',
+
+			// By default, the marketing checkbox should be unticked by default to comply with WordPress.org plugin review guidelines.
 			isAgreeMarketing:
 				typeof profileItems.is_agree_marketing === 'boolean'
 					? profileItems.is_agree_marketing
