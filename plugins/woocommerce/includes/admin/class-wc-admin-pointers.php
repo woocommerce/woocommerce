138f3c5eead4be7046e73fcafccae173ffab61db
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-06-08 13:19:00 +0800

    Use spotlight query param to toggle new product tour feature

--- plugins/woocommerce/includes/admin/class-wc-admin-pointers.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-pointers.php
@@ -46,39 +46,30 @@ class WC_Admin_Pointers {
 	 * Pointers for creating a product.
 	 */
 	public function create_product_tutorial() {
-		if ( ! current_user_can( 'manage_options' ) ) {
+		if ( ! isset( $_GET['tutorial'] ) || ! current_user_can( 'manage_options' ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 			return;
 		}
 
 		global $wp_post_types;
-		global $post;
 
 		if (
 			Features::is_enabled( 'experimental-product-tour' ) &&
-			isset( $post ) &&
+			isset( $_GET['spotlight'] ) && // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 			isset( $wp_post_types )
 		) {
-			$product_type = WC_Product_Factory::get_product_type( $post->id );
+			$labels          = $wp_post_types['product']->labels;
+			$labels->add_new = __( 'Enable guided mode', 'woocommerce' );
 
-			if ( 'simple' === $product_type ) {
-				$labels          = $wp_post_types['product']->labels;
-				$labels->add_new = __( 'Enable guided mode', 'woocommerce' );
+			$script_assets_filename = WCAdminAssets::get_script_asset_filename( 'wp-admin-scripts', 'onboarding-homepage-notice' );
+			$script_assets          = require WC_ADMIN_ABSPATH . WC_ADMIN_DIST_JS_FOLDER . 'wp-admin-scripts/' . $script_assets_filename;
 
-				$script_assets_filename = WCAdminAssets::get_script_asset_filename( 'wp-admin-scripts', 'onboarding-homepage-notice' );
-				$script_assets          = require WC_ADMIN_ABSPATH . WC_ADMIN_DIST_JS_FOLDER . 'wp-admin-scripts/' . $script_assets_filename;
-
-				wp_enqueue_script(
-					'product-tutorial',
-					WCAdminAssets::get_url( 'wp-admin-scripts/product-tour', 'js' ),
-					array_merge( array( WC_ADMIN_APP ), $script_assets ['dependencies'] ),
-					WC_VERSION,
-					true
-				);
-				return;
-			}
-		}
-
-		if ( ! isset( $_GET['tutorial'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			wp_enqueue_script(
+				'product-tutorial',
+				WCAdminAssets::get_url( 'wp-admin-scripts/product-tour', 'js' ),
+				array_merge( array( WC_ADMIN_APP ), $script_assets ['dependencies'] ),
+				WC_VERSION,
+				true
+			);
 			return;
 		}
 
