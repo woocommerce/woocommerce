e822b97619e88ad3241bd9b25ba918ab875957a4
Néstor Soriano <konamiman@konamiman.com>
2022-06-09 08:39:56 +0200

    [COT] Add database transactions support for data synchronization (#33255)
    * Add optional usage of database transactions for orders sync.
    
    Includes a new setting in Advanced - Custom data stores to enable
    or disable transactions usage and to select the transaction
    isolation level.
    
    * Improve the selection of data to be updated when syncing orders.
    
    The SQL query that chooses records to be migrated to core tables
    now explicitly excludes unchanged records based on the column mappings.
    
    Also when syncing meta tables with single meta values, records for
    which the value hasn't changed are now excluded from the update.
    
    * Fix MetaToCustomTableMigrator to work with order addresses table.
    
    * Add unit tests for db transactions on PostsToOrdersMigrationController
    
    Also add the DynamicDecorator::call_original_method method

--- plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
@@ -29,6 +29,18 @@ class CustomOrdersTableController {
 	private const AUTO_FLIP_AUTHORITATIVE_TABLE_ROLES_OPTION = 'woocommerce_auto_flip_authoritative_table_roles';
 
 	/**
+	 * The name of the option that tells whether database transactions are to be used or not for data synchronization.
+	 */
+	public const USE_DB_TRANSACTIONS_OPTION = 'woocommerce_use_db_transactions_for_custom_orders_table_data_sync';
+
+	/**
+	 * The name of the option to store the transaction isolation level to use when database transactions are enabled.
+	 */
+	public const DB_TRANSACTIONS_ISOLATION_LEVEL_OPTION = 'woocommerce_db_transactions_isolation_level_for_custom_orders_table_data_sync';
+
+	public const DEFAULT_DB_TRANSACTIONS_ISOLATION_LEVEL = 'REPEATABLE READ';
+
+	/**
 	 * The data store object to use.
 	 *
 	 * @var OrdersTableDataStore
@@ -381,6 +393,21 @@ class CustomOrdersTableController {
 					);
 				}
 			}
+
+			$settings[] = array(
+				'desc' => __( 'Use database transactions for the orders data synchronization', 'woocommerce' ),
+				'id'   => self::USE_DB_TRANSACTIONS_OPTION,
+				'type' => 'checkbox',
+			);
+
+			$isolation_level_names = self::get_valid_transaction_isolation_levels();
+			$settings[]            = array(
+				'desc'    => __( 'Database transaction isolation level to use', 'woocommerce' ),
+				'id'      => self::DB_TRANSACTIONS_ISOLATION_LEVEL_OPTION,
+				'type'    => 'select',
+				'options' => array_combine( $isolation_level_names, $isolation_level_names ),
+				'default' => self::DEFAULT_DB_TRANSACTIONS_ISOLATION_LEVEL,
+			);
 		} else {
 			$settings[] = array(
 				'title' => __( 'Custom orders tables', 'woocommerce' ),
@@ -397,6 +424,20 @@ class CustomOrdersTableController {
 		$settings[] = array( 'type' => 'sectionend' );
 
 		return $settings;
+	}
+
+	/**
+	 * Get the valid database transaction isolation level names.
+	 *
+	 * @return string[]
+	 */
+	public static function get_valid_transaction_isolation_levels() {
+		return array(
+			'REPEATABLE READ',
+			'READ COMMITTED',
+			'READ UNCOMMITTED',
+			'SERIALIZABLE',
+		);
 	}
 
 	/**
