666a5a375502ee3d82bbc967a74f905f850a016d
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-06-10 15:54:20 +0100

    Add Tree Select Control Component

---
+++ packages/js/components/src/tree-select-control/constants.js
@@ -0,0 +1,8 @@
+export const ROOT_VALUE = '__WC_TREE_SELECT_COMPONENT_ROOT__';
+export const BACKSPACE = 'Backspace';
+export const ESCAPE = 'Escape';
+export const ENTER = 'Enter';
+export const ARROW_UP = 'ArrowUp';
+export const ARROW_DOWN = 'ArrowDown';
+export const ARROW_LEFT = 'ArrowLeft';
+export const ARROW_RIGHT = 'ArrowRight';
