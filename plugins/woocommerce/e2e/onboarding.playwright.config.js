94f424055c97cb73f35d0c7ff89d64a36c6f69de
Rodel <rodel.calasagsag@automattic.com>
2022-06-14 10:22:03 +0800

    Remove unnecessary onboarding-related config and test list

--- plugins/woocommerce/e2e/onboarding.playwright.config.js
+++
@@ -1,9 +0,0 @@
-const defaultConfig = require( './ci.playwright.config' );
-
-const config = {
-	...defaultConfig,
-	workers: 1,
-	testMatch: 'onboarding.test.list.js',
-};
-
-module.exports = config;
--- plugins/woocommerce/e2e/tests/onboarding.test.list.js
+++
@@ -1,4 +0,0 @@
-require( './basic.spec' );
-require( './activate-and-setup/setup-onboarding.spec' );
-require( './activate-and-setup/basic-setup.spec' );
-require( './activate-and-setup/complete-onboarding-wizard.spec' );
