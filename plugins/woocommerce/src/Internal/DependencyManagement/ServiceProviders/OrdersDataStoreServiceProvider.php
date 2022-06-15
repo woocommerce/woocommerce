4b63f348415b856566d8f56763c1874dcc8cff96
Jorge A. Torres <jorge.torres@automattic.com>
2022-06-15 05:39:41 -0300

    [COT] Implement metadata CRUD and `update()` method in COT datastore (#33026)
    * Add `OrdersTableDataStoreMeta` to handle metadata for orders
    
    * Add `OrdersTableDataStoreHelper` with various helper functions used in the COT datastore
    
    * Pass some helper classes as args to the COT datastore
    
    * Use `OrdersTableDataStoreMeta` for meta in COT datastore
    
    * Minor fixes to columns definition in COT datastore
    
    * First pass at update() in the COT datastore
    
    * PHPCS fixes
    
    * Remove duplicate `read_meta` calls.
    
    * Register `OrdersTableDataStore` earlier to make container happy
    
    * Do not hardcode table metadata in `OrdersTableDataStoreMeta`
    
    * Correctly format decimals for storing in the db
    
    * read() shouldn’t success on non-existing orders
    
    * Rework persisting to db in OrdersTableDataStore
    
    * Correctly handle some props in OrdersTableDataStore
    
    * Add changelog
    
    * Add missing TODOs
    
    * Remove unused variables
    
    * No need to query db before deleting meta in `OrdersTableDataStoreMeta`
    
    * Simplify OrdersTableDataStoreMeta::update_meta()
    
    * Explicitly enumerate columns in OrdersTableDataStoreMeta::get_metadata_by_id()
    
    * Make COT metadata implementation more generic
    
    * Do not use property_exists() to determine existence of meta value
    
    * Move some methods over to DatabaseUtil and get rid of COT datastore helper
    
    * Rename `CustomDataStoreMeta` to `CustomMetaDataStore`
    
    * Make PHPCS happy
    
    * Add unit test.
    
    * Correct arg passed to persist_order_to_db()
    
    * Remove comment
    
    * Split conditional on multiple lines
    
    Co-authored-by: vedanshujain <vedanshu.jain.2012@gmail.com>

--- plugins/woocommerce/src/Internal/DependencyManagement/ServiceProviders/OrdersDataStoreServiceProvider.php
+++ plugins/woocommerce/src/Internal/DependencyManagement/ServiceProviders/OrdersDataStoreServiceProvider.php
@@ -12,6 +12,7 @@ use Automattic\WooCommerce\Internal\DependencyManagement\AbstractServiceProvider
 use Automattic\WooCommerce\Internal\DataStores\Orders\DataSynchronizer;
 use Automattic\WooCommerce\Internal\DataStores\Orders\CustomOrdersTableController;
 use Automattic\WooCommerce\Internal\DataStores\Orders\OrdersTableDataStore;
+use Automattic\WooCommerce\Internal\DataStores\Orders\OrdersTableDataStoreMeta;
 use Automattic\WooCommerce\Internal\Utilities\DatabaseUtil;
 
 /**
@@ -29,12 +30,17 @@ class OrdersDataStoreServiceProvider extends AbstractServiceProvider {
 		CustomOrdersTableController::class,
 		OrdersTableDataStore::class,
 		CLIRunner::class,
+		OrdersTableDataStoreMeta::class,
 	);
 
 	/**
 	 * Register the classes.
 	 */
 	public function register() {
+		$this->share( OrdersTableDataStoreMeta::class );
+		$this->share( OrdersTableDataStoreHelper::class );
+
+		$this->share( OrdersTableDataStore::class )->addArguments( array( OrdersTableDataStoreMeta::class, DatabaseUtil::class ) );
 		$this->share( DataSynchronizer::class )->addArguments( array( OrdersTableDataStore::class, DatabaseUtil::class, PostsToOrdersMigrationController::class ) );
 		$this->share( CustomOrdersTableController::class )->addArguments( array( OrdersTableDataStore::class, DataSynchronizer::class ) );
 		$this->share( OrdersTableDataStore::class );
