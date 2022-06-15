55eebebeba70de5fd2b75113459efe858768aa55
Fernando <fernando.marichal@automattic.com>
2022-06-15 09:25:10 -0300

    Add product deletion via datastore API (#33285)
    * Add delete products via data store api
    
    # Conflicts:
    #	packages/js/data/src/products/action-types.ts
    #	packages/js/data/src/products/actions.ts
    #	packages/js/data/src/products/reducer.ts
    #	packages/js/data/src/products/selectors.ts
    
    * Add tests
    
    # Conflicts:
    #	packages/js/data/src/products/test/reducer.ts
    
    * Set `force = false` by default
    
    * Add changelog
    
    * Fix thrown error
    
    * Remove error
    
    * Remove `DeleteProductType`
    
    # Conflicts:
    #	packages/js/data/src/products/actions.ts
    
    * Remove conditional retrun
    
    Co-authored-by: Fernando Marichal <contacto@fernandomarichal.com>

--- packages/js/data/src/products/reducer.ts
+++ packages/js/data/src/products/reducer.ts
@@ -108,6 +108,27 @@ const reducer: Reducer< ProductState, Actions > = (
 						[ `update/${ payload.id }` ]: payload.error,
 					},
 				};
+			case TYPES.DELETE_PRODUCT_ERROR:
+				return {
+					...state,
+					errors: {
+						...state.errors,
+						[ `delete/${ payload.id }` ]: payload.error,
+					},
+				};
+			case TYPES.DELETE_PRODUCT_SUCCESS:
+				const prData = state.data || {};
+				return {
+					...state,
+					data: {
+						...prData,
+						[ payload.id ]: {
+							...( prData[ payload.id ] || {} ),
+							...payload.product,
+							status: payload.force ? 'deleted' : 'trash',
+						},
+					},
+				};
 			default:
 				return state;
 		}
