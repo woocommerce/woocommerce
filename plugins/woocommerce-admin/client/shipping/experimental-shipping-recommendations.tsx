f81b8a050a2dde9d00622d47fb8424beed570e20
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-06-21 12:35:21 +0800

    Update shipping recommendations display logic

---
+++ plugins/woocommerce-admin/client/shipping/experimental-shipping-recommendations.tsx
@@ -0,0 +1,64 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
+
+import { PLUGINS_STORE_NAME, SETTINGS_STORE_NAME } from '@woocommerce/data';
+
+/**
+ * Internal dependencies
+ */
+import { getCountryCode } from '~/dashboard/utils';
+import WooCommerceServicesItem from './experimental-woocommerce-services-item';
+import { ShippingRecommendationsList } from './shipping-recommendations';
+import './shipping-recommendations.scss';
+
+const ShippingRecommendations: React.FC = () => {
+	const { activePlugins, installedPlugins, countryCode, isJetpackConnected } =
+		useSelect( ( select ) => {
+			const settings = select( SETTINGS_STORE_NAME ).getSettings< {
+				general?: {
+					woocommerce_default_country: string;
+				};
+			} >( 'general' );
+
+			const {
+				getActivePlugins,
+				getInstalledPlugins,
+				isJetpackConnected: _isJetpackConnected,
+			} = select( PLUGINS_STORE_NAME );
+
+			return {
+				activePlugins: getActivePlugins(),
+				installedPlugins: getInstalledPlugins(),
+				countryCode: getCountryCode(
+					settings.general?.woocommerce_default_country
+				),
+				isJetpackConnected: _isJetpackConnected(),
+			};
+		} );
+
+	if (
+		activePlugins.includes( 'woocommerce-services' ) &&
+		isJetpackConnected
+	) {
+		return null;
+	}
+
+	if ( countryCode !== 'US' ) {
+		return null;
+	}
+
+	return (
+		<ShippingRecommendationsList>
+			<WooCommerceServicesItem
+				isWCSInstalled={ installedPlugins.includes(
+					'woocommerce-services'
+				) }
+			/>
+		</ShippingRecommendationsList>
+	);
+};
+
+export default ShippingRecommendations;
