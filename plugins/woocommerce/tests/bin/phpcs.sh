67a73573688188c50bca0796a4e2064ad8b5209d
roykho <roykho77@gmail.com>
2022-06-22 14:38:57 -0700

    Update flags

--- plugins/woocommerce/tests/bin/phpcs.sh
+++ plugins/woocommerce/tests/bin/phpcs.sh
@@ -8,7 +8,7 @@ if [ "$CHANGED_FILES" != "" ]; then
 	echo "Changed files: $CHANGED_FILES"
 	echo "Running Code Sniffer."
 
-	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --ignore=$IGNORE --encoding=utf-8 -s -n -p --report-full --report-checkstyle=./phpcs-report.xml ${CHANGED_FILES}
+	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --ignore=$IGNORE -s ${CHANGED_FILES}
 else
 	echo "No changes found. Skipping PHPCS run."
 fi
