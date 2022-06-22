0a39c5c46048f2252fa88c869fdc3220d3dea2e4
Joshua Flowers <joshuatf@gmail.com>
2022-06-22 13:55:35 -0400

    Update data store tests

--- packages/js/data/src/crud/test/reducer.ts
+++ packages/js/data/src/crud/test/reducer.ts
@@ -126,6 +126,7 @@ describe( 'crud reducer', () => {
 			type: TYPES.GET_ITEMS_ERROR,
 			query,
 			error,
+			errorType: CRUD_ACTIONS.GET_ITEMS,
 		} );
 
 		expect( state.errors[ resourceName ] ).toBe( error );
@@ -139,8 +140,105 @@ describe( 'crud reducer', () => {
 			type: TYPES.GET_ITEM_ERROR,
 			id,
 			error,
+			errorType: CRUD_ACTIONS.GET_ITEM,
 		} );
 
 		expect( state.errors[ resourceName ] ).toBe( error );
+	} );
+
+	it( 'should handle GET_ITEM_ERROR', () => {
+		const id = 3;
+		const resourceName = getResourceName( CRUD_ACTIONS.GET_ITEM, { id } );
+		const error = 'Baaam!';
+		const state = reducer( defaultState, {
+			type: TYPES.GET_ITEM_ERROR,
+			id,
+			error,
+			errorType: CRUD_ACTIONS.GET_ITEM,
+		} );
+
+		expect( state.errors[ resourceName ] ).toBe( error );
+	} );
+
+	it( 'should handle CREATE_ITEM_ERROR', () => {
+		const query = { name: 'Invalid product' };
+		const resourceName = getResourceName( CRUD_ACTIONS.CREATE_ITEM, query );
+		const error = 'Baaam!';
+		const state = reducer( defaultState, {
+			type: TYPES.CREATE_ITEM_ERROR,
+			query,
+			error,
+			errorType: CRUD_ACTIONS.CREATE_ITEM,
+		} );
+
+		expect( state.errors[ resourceName ] ).toBe( error );
+	} );
+
+	it( 'should handle UPDATE_ITEM_ERROR', () => {
+		const id = 2;
+		const resourceName = getResourceName( CRUD_ACTIONS.UPDATE_ITEM, {
+			id,
+		} );
+		const error = 'Baaam!';
+		const state = reducer( defaultState, {
+			type: TYPES.UPDATE_ITEM_ERROR,
+			id,
+			error,
+			errorType: CRUD_ACTIONS.UPDATE_ITEM,
+		} );
+
+		expect( state.errors[ resourceName ] ).toBe( error );
+	} );
+
+	it( 'should handle UPDATE_ITEM_SUCCESS', () => {
+		const itemType = 'guyisms';
+		const initialState: ResourceState = {
+			items: {
+				[ itemType ]: {
+					data: [ 1, 2 ],
+				},
+			},
+			errors: {},
+			data: {
+				1: { id: 1, name: 'Donkey', status: 'draft' },
+				2: { id: 2, name: 'Sauce', status: 'publish' },
+			},
+		};
+		const item: Item = {
+			id: 2,
+			name: 'Holy smokes!',
+			status: 'draft',
+		};
+
+		const state = reducer( initialState, {
+			type: TYPES.UPDATE_ITEM_SUCCESS,
+			id: item.id,
+			item,
+		} );
+
+		expect( state.items ).toEqual( initialState.items );
+		expect( state.errors ).toEqual( initialState.errors );
+
+		expect( state.data[ 1 ] ).toEqual( initialState.data[ 1 ] );
+		expect( state.data[ 2 ].id ).toEqual( initialState.data[ 2 ].id );
+		expect( state.data[ 2 ].title ).toEqual( initialState.data[ 2 ].title );
+		expect( state.data[ 2 ].name ).toEqual( item.name );
+	} );
+
+	it( 'should handle CREATE_ITEM_SUCCESS', () => {
+		const item: Item = {
+			id: 2,
+			name: 'Off the hook!',
+			status: 'draft',
+		};
+
+		const state = reducer( defaultState, {
+			type: TYPES.CREATE_ITEM_SUCCESS,
+			id: item.id,
+			item,
+		} );
+
+		expect( state.data[ 2 ].name ).toEqual( item.name );
+		expect( state.data[ 2 ].status ).toEqual( item.status );
 	} );
 } );
