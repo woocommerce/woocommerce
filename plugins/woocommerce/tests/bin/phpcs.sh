406089205d47b643fe497f4dd729d874cb554a5a
roykho <roykho77@gmail.com>
2022-06-22 12:10:22 -0700

    Remove flags for testing

--- plugins/woocommerce/tests/bin/phpcs.sh
+++ plugins/woocommerce/tests/bin/phpcs.sh
@@ -7,8 +7,8 @@ IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
 if [ "$CHANGED_FILES" != "" ]; then
 	echo "Changed files: $CHANGED_FILES"
 	echo "Running Code Sniffer."
-echo ${CHANGED_FILES}
-	./vendor/bin/phpcs-changed --git --ignore=$IGNORE --encoding=utf-8 -s -n -p --report-full --report-checkstyle=./phpcs-report.xml ${CHANGED_FILES}
+
+	./vendor/bin/phpcs-changed --git --ignore=$IGNORE ${CHANGED_FILES}
 else
 	echo "No changes found. Skipping PHPCS run."
 fi
