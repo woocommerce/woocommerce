82abea91f94b84fef02ba265576b7e346bf7d1e4
moon <moon.kyong@automattic.com>
2022-06-22 21:39:23 -0700

    Run the changes only if shipping-smart-defaults is enabled

--- plugins/woocommerce/src/Internal/Admin/Homescreen.php
+++ plugins/woocommerce/src/Internal/Admin/Homescreen.php
@@ -6,8 +6,7 @@
 
 namespace Automattic\WooCommerce\Internal\Admin;
 
-use Automattic\WooCommerce\Internal\Admin\Settings;
-use WC_Shipping_Flat_Rate;
+use Automattic\WooCommerce\Admin\Features\Features;
 
 /**
  * Contains backend logic for the homescreen feature.
@@ -50,8 +49,16 @@ class Homescreen {
 			// priority is 20 to run after https://github.com/woocommerce/woocommerce/blob/a55ae325306fc2179149ba9b97e66f32f84fdd9c/includes/admin/class-wc-admin-menus.php#L165.
 			add_action( 'admin_head', array( $this, 'update_link_structure' ), 20 );
 		}
+
 		add_filter( 'woocommerce_admin_preload_options', array( $this, 'preload_options' ) );
-		add_filter( 'woocommerce_admin_shared_settings', array( $this, 'maybe_set_default_shipping_options_on_home' ), 9999 );
+
+		if ( Features::is_enabled( 'shipping-smart-defaults' ) ) {
+			add_filter(
+				'woocommerce_admin_shared_settings',
+				array( $this, 'maybe_set_default_shipping_options_on_home' ),
+				9999
+			);
+		}
 	}
 
 	/**
