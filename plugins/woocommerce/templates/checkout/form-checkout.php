c5b53c8488e96175aa87dd790028891e003bc426
Ahmet Nebioğlu <info@ahmetnebioglu.com.tr>
2022-06-23 13:07:24 +0300

    checkout page ui problem
    this improvement is done in accordance with Bootstrap

--- plugins/woocommerce/templates/checkout/form-checkout.php
+++ plugins/woocommerce/templates/checkout/form-checkout.php
@@ -30,37 +30,24 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 ?>
 
 <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
-
 	<?php if ( $checkout->get_checkout_fields() ) : ?>
-
-		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
-
-		<div class="col2-set" id="customer_details">
-			<div class="col-1">
+		<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+		<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+		<div class="row">
+			<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+			<div class="col-12 col-md-6" id="customer_details">
 				<?php do_action( 'woocommerce_checkout_billing' ); ?>
-			</div>
-
-			<div class="col-2">
 				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 			</div>
+			<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+			<div class="col-12 col-md-6">
+				<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+				<div id="order_review" class="woocommerce-checkout-review-order float-none w-100">
+					<?php do_action( 'woocommerce_checkout_order_review' ); ?>
+				</div>
+				<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
+			</div>
 		</div>
-
-		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
 	<?php endif; ?>
-	
-	<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-	
-	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
-	
-	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
-
-	<div id="order_review" class="woocommerce-checkout-review-order">
-		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
-	</div>
-
-	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
-
 </form>
-
 <?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
