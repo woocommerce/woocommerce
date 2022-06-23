ca8ff2f66b1c671d897205b69c5225fbc4bd1ee4
roykho <roykho77@gmail.com>
2022-06-23 07:14:49 -0700

    Temp remove ignore

--- plugins/woocommerce/tests/bin/phpcs.sh
+++ plugins/woocommerce/tests/bin/phpcs.sh
@@ -8,7 +8,7 @@ if [ "$CHANGED_FILES" != "" ]; then
 	echo "Changed files: $CHANGED_FILES"
 	echo "Running Code Sniffer."
 
-	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --git-base --ignore=$IGNORE -s trunk ${CHANGED_FILES}
+	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --git-base -s trunk ${CHANGED_FILES}
 else
 	echo "No changes found. Skipping PHPCS run."
 fi
