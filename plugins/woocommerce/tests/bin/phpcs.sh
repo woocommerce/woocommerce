9fbb9148b029fd1d92c727d59159e69f09436851
roykho <roykho77@gmail.com>
2022-06-23 12:51:13 -0700

    use base sha

--- plugins/woocommerce/tests/bin/phpcs.sh
+++ plugins/woocommerce/tests/bin/phpcs.sh
@@ -3,14 +3,13 @@
 COMMIT_RANGE="${1} ${2}"
 CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $COMMIT_RANGE | grep '\.php' | grep 'plugins/woocommerce/' | sed -e 's/^plugins\/woocommerce\///' | awk '{print}' ORS=' '`
 IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
-DEFAULT_BRANCH=`git remote show origin | awk '/HEAD branch/ {print $NF}'`
 
 if [ "$CHANGED_FILES" != "" ]; then
 	echo "Changed files: $CHANGED_FILES"
 	echo "Running Code Sniffer."
 	echo "Default branch: $DEFAULT_BRANCH"
 
-	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --report=full -s --ignore=${IGNORE} --git-base ${DEFAULT_BRANCH} ${CHANGED_FILES}
+	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --report=full -s --ignore=${IGNORE} --git-base ${1} ${CHANGED_FILES}
 else
 	echo "No changes found. Skipping PHPCS run."
 fi
