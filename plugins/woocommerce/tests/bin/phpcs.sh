99c00d0ecbca996c8dfc29c04df1a7715b3447bd
roykho <roykho77@gmail.com>
2022-06-23 11:22:36 -0700

    Try default branch

--- plugins/woocommerce/tests/bin/phpcs.sh
+++ plugins/woocommerce/tests/bin/phpcs.sh
@@ -3,12 +3,14 @@
 COMMIT_RANGE="${1} ${2}"
 CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $COMMIT_RANGE | grep '\.php' | grep 'plugins/woocommerce/' | sed -e 's/^plugins\/woocommerce\///' | awk '{print}' ORS=' '`
 IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
+DEFAULT_BRANCH=$(git remote show origin | awk '/HEAD branch/ {print $NF}')
+DEFAULT_BRANCH_REF="refs/heads/${DEFAULT_BRANCH}"
 
 if [ "$CHANGED_FILES" != "" ]; then
 	echo "Changed files: $CHANGED_FILES"
 	echo "Running Code Sniffer."
 
-	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --report=full -s --git-base HEAD~1 ${CHANGED_FILES}
+	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --report=full -s --git-base ${DEFAULT_BRANCH} ${CHANGED_FILES}
 else
 	echo "No changes found. Skipping PHPCS run."
 fi
