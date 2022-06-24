050856063d13ca44f8cdc14a36c82bf85ac34482
roykho <roykho77@gmail.com>
2022-06-23 18:00:05 -0700

    Checkout repo

--- plugins/woocommerce/tests/bin/phpcs.sh
+++ plugins/woocommerce/tests/bin/phpcs.sh
@@ -7,9 +7,8 @@ IGNORE="tests/cli/,includes/libraries/,includes/api/legacy/"
 if [ "$CHANGED_FILES" != "" ]; then
 	echo "Changed files: $CHANGED_FILES"
 	echo "Running Code Sniffer."
-	echo "Default branch: $DEFAULT_BRANCH"
 
-	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --report=full -s --ignore=${IGNORE} --git-base trunk ${CHANGED_FILES}
+	PHPCS="./vendor/bin/phpcs" ./vendor/bin/phpcs-changed --git --report=full -s --ignore=${IGNORE} --git-base ${1} ${CHANGED_FILES}
 else
 	echo "No changes found. Skipping PHPCS run."
 fi
