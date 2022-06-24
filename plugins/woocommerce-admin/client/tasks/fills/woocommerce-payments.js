d2967e0ee80c4973644ad985feee6b223727642b
RJ <27843274+rjchow@users.noreply.github.com>
2022-06-24 17:06:43 +0800

    Fixed missing wcpay setup task (#33590)
    - task fill page was missing for the woocommerce-payments task

--- plugins/woocommerce-admin/client/tasks/fills/woocommerce-payments.js
+++ plugins/woocommerce-admin/client/tasks/fills/woocommerce-payments.js
@@ -1,11 +1,15 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { registerPlugin } from '@wordpress/plugins';
-import { WooOnboardingTaskListItem } from '@woocommerce/onboarding';
+import {
+	WooOnboardingTaskListItem,
+	WooOnboardingTask,
+} from '@woocommerce/onboarding';
 import { PLUGINS_STORE_NAME } from '@woocommerce/data';
 import { useDispatch } from '@wordpress/data';
+import { Spinner } from '@woocommerce/components';
 
 /**
  * Internal dependencies
@@ -20,6 +24,7 @@ const WoocommercePaymentsTaskItem = () => {
 		<WooOnboardingTaskListItem id="woocommerce-payments">
 			{ ( { defaultTaskItem: DefaultTaskItem } ) => (
 				<DefaultTaskItem
+					// intercept the click on the task list item so that we don't have to see a intermediate page before installing woocommerce payments
 					onClick={ () => {
 						return new Promise( ( resolve, reject ) => {
 							return installActivateAndConnectWcpay(
@@ -38,4 +43,48 @@ const WoocommercePaymentsTaskItem = () => {
 registerPlugin( 'woocommerce-admin-task-wcpay', {
 	scope: 'woocommerce-tasks',
 	render: WoocommercePaymentsTaskItem,
+} );
+
+const ReadyWcPay = () => {
+	const { installAndActivatePlugins } = useDispatch( PLUGINS_STORE_NAME );
+	const { createNotice } = useDispatch( 'core/notices' );
+
+	useEffect( () => {
+		new Promise( ( resolve, reject ) => {
+			return installActivateAndConnectWcpay(
+				reject,
+				createNotice,
+				installAndActivatePlugins
+			);
+		} );
+	}, [ createNotice, installAndActivatePlugins ] );
+
+	return (
+		<div
+			style={ {
+				height: '70vh',
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+			} }
+		>
+			<Spinner />
+			<div style={ { marginTop: '1rem' } }>
+				Preparing payment settings...
+			</div>
+		</div>
+	);
+};
+
+// shows up at http://host/wp-admin/admin.php?page=wc-admin&task=woocommerce-payments which is the default url for woocommerce-payments task
+const WoocommercePaymentsTaskPage = () => (
+	<WooOnboardingTask id="woocommerce-payments">
+		<ReadyWcPay />
+	</WooOnboardingTask>
+);
+
+registerPlugin( 'woocommerce-admin-task-wcpay-page', {
+	scope: 'woocommerce-tasks',
+	render: WoocommercePaymentsTaskPage,
 } );
