6cb6fd02df1fc466f69939976a0abdf682cc6fe5
Matthew Hodge <mhodge.webdev@gmail.com>
2022-06-30 19:34:02 +0200

    Added a filter to set fopen mode for streams that dont support a+ (#33652)
    Added a filter to set fopen mode for streams that dont support a+
    
    Co-authored-by: barryhughes <3594411+barryhughes@users.noreply.github.com>

--- plugins/woocommerce/includes/export/abstract-wc-csv-batch-exporter.php
+++ plugins/woocommerce/includes/export/abstract-wc-csv-batch-exporter.php
@@ -130,7 +130,18 @@ abstract class WC_CSV_Batch_Exporter extends WC_CSV_Exporter {
 			return false;
 		}
 
-		$fp = fopen( $this->get_file_path(), 'a+' );
+		/**
+		 * Filters the mode parameter which specifies the type of access you require to the stream (used during file
+		 * writing for CSV exports). Defaults to 'a+' (which supports both reading and writing, and places the file
+		 * pointer at the end of the file).
+		 *
+		 * @see   https://www.php.net/manual/en/function.fopen.php
+		 * @since 6.8.0
+		 *
+		 * @param string $fopen_mode, either (r, r+, w, w+, a, a+, x, x+, c, c+, e)
+		 */
+		$fopen_mode = apply_filters( 'woocommerce_csv_exporter_fopen_mode', 'a+' );
+		$fp         = fopen( $this->get_file_path(), $fopen_mode );
 
 		if ( $fp ) {
 			fwrite( $fp, $data );
