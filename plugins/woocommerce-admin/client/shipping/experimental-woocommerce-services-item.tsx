8d281241a9388e8c7dbfc1fa0da0d9d1443c72bf
Ilyas Foo <foo.ilyas@gmail.com>
2022-07-06 20:00:08 +0800

    Add tracks for shipping smart defaults (#33748)
    * Change review shipping task id to 'review-shipping'
    
    * Add click track for shipping-recommendation task from the settings page
    
    * Changelog
    
    * Add test
    
    * Add more specific test param

--- plugins/woocommerce-admin/client/shipping/experimental-woocommerce-services-item.tsx
+++ plugins/woocommerce-admin/client/shipping/experimental-woocommerce-services-item.tsx
@@ -5,17 +5,29 @@ import { __ } from '@wordpress/i18n';
 import { Button, ExternalLink } from '@wordpress/components';
 import { Pill } from '@woocommerce/components';
 import { getNewPath, navigateTo } from '@woocommerce/navigation';
+import { recordEvent } from '@woocommerce/tracks';
+import { useContext, useMemo } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import './woocommerce-services-item.scss';
 import WooIcon from './woo-icon.svg';
+import { LayoutContext } from '~/layout';
 
 const WooCommerceServicesItem: React.FC< {
 	isWCSInstalled: boolean | undefined;
 } > = ( { isWCSInstalled } ) => {
+	const layoutContext = useContext( LayoutContext );
+	const updatedLayoutContext = useMemo(
+		() => layoutContext.getExtendedContext( 'wc-settings' ),
+		[ layoutContext ]
+	);
 	const handleSetupClick = () => {
+		recordEvent( 'tasklist_click', {
+			task_name: 'shipping-recommendation',
+			context: updatedLayoutContext.toString(),
+		} );
 		navigateTo( {
 			url: getNewPath( { task: 'shipping-recommendation' }, '/', {} ),
 		} );
--- plugins/woocommerce-admin/client/shipping/test/experimental-woocommerce-services-item.tsx
+++ plugins/woocommerce-admin/client/shipping/test/experimental-woocommerce-services-item.tsx
@@ -2,11 +2,16 @@
  * External dependencies
  */
 import { render, screen } from '@testing-library/react';
+import { recordEvent } from '@woocommerce/tracks';
 
 /**
  * Internal dependencies
  */
 import WooCommerceServicesItem from '../experimental-woocommerce-services-item';
+jest.mock( '@woocommerce/tracks', () => ( {
+	...jest.requireActual( '@woocommerce/tracks' ),
+	recordEvent: jest.fn(),
+} ) );
 
 describe( 'WooCommerceServicesItem', () => {
 	it( 'should render WCS item with CTA = "Get started" when WCS is not installed', () => {
@@ -31,5 +36,15 @@ describe( 'WooCommerceServicesItem', () => {
 		expect(
 			screen.queryByRole( 'button', { name: 'Activate' } )
 		).toBeInTheDocument();
+	} );
+
+	it( 'should record track when clicking setup button', () => {
+		render( <WooCommerceServicesItem isWCSInstalled={ false } /> );
+
+		screen.queryByRole( 'button', { name: 'Get started' } )?.click();
+		expect( recordEvent ).toHaveBeenCalledWith( 'tasklist_click', {
+			context: 'root/wc-settings',
+			task_name: 'shipping-recommendation',
+		} );
 	} );
 } );
