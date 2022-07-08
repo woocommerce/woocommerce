f3b6dfb28698dd196770de53466e605f068dfc2f
Thilina Hasantha <thilina.hasantha@gmail.com>
2022-07-08 14:16:34 +0200

    Update php docs and change registered command names for wc com.

--- plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
+++ plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
@@ -17,29 +17,29 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class WC_CLI_COM_Command {
 	/**
-	 * Registers a command for showing WooCommerce Tracker snapshot data.
+	 * Registers a commands for managing WooCommerce.com extensions.
 	 */
 	public static function register_commands() {
-		WP_CLI::add_command( 'wc com install', array( 'WC_CLI_COM_Command', 'install_extension' ) );
-		WP_CLI::add_command( 'wc com list', array( 'WC_CLI_COM_Command', 'list_extensions' ) );
+		WP_CLI::add_command( 'wc com extension install', array( 'WC_CLI_COM_Command', 'install_extension' ) );
+		WP_CLI::add_command( 'wc com extension list', array( 'WC_CLI_COM_Command', 'list_extensions' ) );
 	}
 
 	/**
-	 *
+	 * List extensions available for the connected site.
 	 *
 	 * ## EXAMPLES
 	 *
 	 * wp wc com extension list
 	 *
-	 * @param  array  $args  WP-CLI positional arguments.
-	 * @param  array  $assoc_args  WP-CLI associative arguments.
+	 * @param  array $args WP-CLI positional arguments.
+	 * @param  array $assoc_args WP-CLI associative arguments.
 	 *
 	 */
 	public static function list_extensions( array $args, array $assoc_args ) {
 	}
 
 	/**
-	 * Dump tracker snapshot data to screen.
+	 * Download and install an extension.
 	 *
 	 * ## OPTIONS
 	 *
@@ -54,8 +54,8 @@ class WC_CLI_COM_Command {
 	 * wp wc com extension install automatewoo
 	 * wp wc com extension install woocommerce-subscriptions --activate
 	 *
-	 * @param  array  $args  WP-CLI positional arguments.
-	 * @param  array  $assoc_args  WP-CLI associative arguments.
+	 * @param  array $args WP-CLI positional arguments.
+	 * @param  array $assoc_args WP-CLI associative arguments.
 	 *
 	 */
 	public static function install_extension( array $args, array $assoc_args ) {
