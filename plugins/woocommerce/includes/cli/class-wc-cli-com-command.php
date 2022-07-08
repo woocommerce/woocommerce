7c566f328f5049d930eb07ca42c95eed5104d89c
Thilina Hasantha <thilina.hasantha@gmail.com>
2022-07-08 15:16:43 +0200

    Retrieve and display extension list and for the connected site.

--- plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
+++ plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
@@ -35,6 +35,46 @@ class WC_CLI_COM_Command {
 	 * @param  array $assoc_args WP-CLI associative arguments.
 	 */
 	public static function list_extensions( array $args, array $assoc_args ) {
+		$data = WC_Helper::get_subscriptions();
+
+		$data = array_values( $data );
+
+		$formatter = new \WP_CLI\Formatter(
+			$assoc_args,
+			array(
+				'product_slug',
+				'product_name',
+				'auto_renew',
+				'expires_on',
+				'expired',
+				'sites_max',
+				'sites_active',
+				'maxed',
+			)
+		);
+
+		$data = array_map(
+			function( $item ) {
+				$product_slug      = '';
+				$product_url_parts = explode( '/', $item['product_url'] );
+				if ( count( $product_url_parts ) > 2 ) {
+					$product_slug = $product_url_parts[ count( $product_url_parts ) - 2 ];
+				}
+				return array(
+					'product_slug' => $product_slug,
+					'product_name' => htmlspecialchars_decode( $item['product_name'] ),
+					'auto_renew'   => $item['autorenew'] ? 'On' : 'Off',
+					'expires_on'   => gmdate( 'Y-m-d', $item['expires'] ),
+					'expired'      => $item['expired'] ? 'Yes' : 'No',
+					'sites_max'    => $item['sites_max'],
+					'sites_active' => $item['sites_active'],
+					'maxed'        => $item['maxed'] ? 'Yes' : 'No',
+				);
+			},
+			$data
+		);
+
+		$formatter->display_items( $data );
 	}
 
 	/**
