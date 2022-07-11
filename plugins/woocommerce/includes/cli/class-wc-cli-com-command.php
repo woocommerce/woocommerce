5e08b20e678311210ab63763853b18cc85fa49ed
Thilina Hasantha <thilina.hasantha@gmail.com>
2022-07-11 13:46:37 +0200

    Remove the install command which not implemented in current branch.

--- plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
+++ plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
@@ -20,7 +20,6 @@ class WC_CLI_COM_Command {
 	 * Registers a commands for managing WooCommerce.com extensions.
 	 */
 	public static function register_commands() {
-		WP_CLI::add_command( 'wc com extension install', array( 'WC_CLI_COM_Command', 'install_extension' ) );
 		WP_CLI::add_command( 'wc com extension list', array( 'WC_CLI_COM_Command', 'list_extensions' ) );
 	}
 
@@ -75,27 +74,5 @@ class WC_CLI_COM_Command {
 		);
 
 		$formatter->display_items( $data );
-	}
-
-	/**
-	 * Download and install an extension.
-	 *
-	 * ## OPTIONS
-	 *
-	 * <extension>
-	 * : One or more plugins to install. Accepts a plugin slug
-	 *
-	 * [--activate]
-	 * : If set, the plugin will be activated immediately after install.
-	 *
-	 * ## EXAMPLES
-	 *
-	 * wp wc com extension install automatewoo
-	 * wp wc com extension install woocommerce-subscriptions --activate
-	 *
-	 * @param  array $args WP-CLI positional arguments.
-	 * @param  array $assoc_args WP-CLI associative arguments.
-	 */
-	public static function install_extension( array $args, array $assoc_args ) {
 	}
 }
