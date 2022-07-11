2f942310070304c179905f90c371950138c01062
Thilina Hasantha <thilina.hasantha@gmail.com>
2022-07-11 18:29:10 +0200

    Remove code related to extension list command.

--- plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
+++ plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
@@ -21,7 +21,6 @@ class WC_CLI_COM_Command {
 	 */
 	public static function register_commands() {
 		WP_CLI::add_command( 'wc com connect', array( 'WC_CLI_COM_Command', 'connect_site' ) );
-		WP_CLI::add_command( 'wc com extension list', array( 'WC_CLI_COM_Command', 'list_extensions' ) );
 	}
 
 	/**
@@ -54,58 +53,5 @@ class WC_CLI_COM_Command {
 				)
 			)
 		);
-	}
-
-	/**
-	 * List extensions available for the connected site.
-	 *
-	 * ## EXAMPLES
-	 *
-	 * wp wc com extension list
-	 *
-	 * @param  array $args WP-CLI positional arguments.
-	 * @param  array $assoc_args WP-CLI associative arguments.
-	 */
-	public static function list_extensions( array $args, array $assoc_args ) {
-		$data = WC_Helper::get_subscriptions();
-
-		$data = array_values( $data );
-
-		$formatter = new \WP_CLI\Formatter(
-			$assoc_args,
-			array(
-				'product_slug',
-				'product_name',
-				'auto_renew',
-				'expires_on',
-				'expired',
-				'sites_max',
-				'sites_active',
-				'maxed',
-			)
-		);
-
-		$data = array_map(
-			function( $item ) {
-				$product_slug      = '';
-				$product_url_parts = explode( '/', $item['product_url'] );
-				if ( count( $product_url_parts ) > 2 ) {
-					$product_slug = $product_url_parts[ count( $product_url_parts ) - 2 ];
-				}
-				return array(
-					'product_slug' => $product_slug,
-					'product_name' => htmlspecialchars_decode( $item['product_name'] ),
-					'auto_renew'   => $item['autorenew'] ? 'On' : 'Off',
-					'expires_on'   => gmdate( 'Y-m-d', $item['expires'] ),
-					'expired'      => $item['expired'] ? 'Yes' : 'No',
-					'sites_max'    => $item['sites_max'],
-					'sites_active' => $item['sites_active'],
-					'maxed'        => $item['maxed'] ? 'Yes' : 'No',
-				);
-			},
-			$data
-		);
-
-		$formatter->display_items( $data );
 	}
 }
