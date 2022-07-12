eb365c3d8ca231bdef20824f0dbe545dad7be664
Thilina Hasantha <thilina.hasantha@gmail.com>
2022-07-12 12:33:24 +0200

    Update example usages for wp wc com extension list command.

--- plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
+++ plugins/woocommerce/includes/cli/class-wc-cli-com-command.php
@@ -24,14 +24,24 @@ class WC_CLI_COM_Command {
 	}
 
 	/**
-	 * List extensions available for the connected site.
+	 * List extensions owned by the connected site
+	 *
+	 * [--format]
+	 * : If set, the command will use the specified format. Possible values are table, json, csv and yaml. By default the table format will be used.
+	 *
+	 * [--fields]
+	 * : If set, the command will show only the specified fields instead of showing all the fields in the output.
 	 *
 	 * ## EXAMPLES
 	 *
-	 * wp wc com extension list
+	 *     # List extensions owned by the connected site in table format with all the fields
+	 *     $ wp wc com extension list
 	 *
-	 * @param  array $args WP-CLI positional arguments.
-	 * @param  array $assoc_args WP-CLI associative arguments.
+	 *     # List the product slug of the extension owned by the connected site in csv format
+	 *     $ wp wc com extension list --format=csv --fields=product_slug
+	 *
+	 * @param  array $args  WP-CLI positional arguments.
+	 * @param  array $assoc_args  WP-CLI associative arguments.
 	 */
 	public static function list_extensions( array $args, array $assoc_args ) {
 		$data = WC_Helper::get_subscriptions();
