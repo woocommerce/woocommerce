9c8a497d18088f685dabc51b83673c98080530d0
jamelreid <jnoelreid@gmail.com>
2022-07-14 13:43:46 +0200

    Updated condition

--- plugins/woocommerce/e2e/tests/activate-and-setup/complete-onboarding-wizard.spec.js
+++ plugins/woocommerce/e2e/tests/activate-and-setup/complete-onboarding-wizard.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require( '@playwright/test' );
 
 const adminEmail =
-	process.env.USE_WP_ENV !== '1'
+	process.env.USE_WP_ENV !== 1
 		? 'wordpress@example.com'
 		: 'admin@woocommercecoree2etestsuite.com';
 
