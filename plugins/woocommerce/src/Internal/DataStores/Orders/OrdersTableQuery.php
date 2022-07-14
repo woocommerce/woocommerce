f3e4d8783abc592212cc35ea0c8b648c5b230f9f
Jorge A. Torres <jorge.torres@automattic.com>
2022-07-14 16:38:00 -0300

    Add docblocks & phpcs fixes

--- plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableQuery.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableQuery.php
@@ -1,35 +1,119 @@
 <?php
+// phpcs:disable Generic.Commenting.Todo.TaskFound
+/**
+ * OrdersTableQuery class file.
+ */
+
 namespace Automattic\WooCommerce\Internal\DataStores\Orders;
 
 use Automattic\WooCommerce\Internal\Utilities\DatabaseUtil;
 
-/** @package Automattic\WooCommerce\Internal\DataStores\Orders */
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * This class provides a `WP_Query`-like interface to custom order tables.
+ */
 class OrdersTableQuery {
 
+	/**
+	 * Values to ignore when parsing query arguments.
+	 */
 	private const SKIPPED_VALUES = array( '', array(), null );
 
-	public $sql;
-	public $max_num_pages = 0;
-	public $orders = array();
-	public $orders_count = 0;
-	public $found_orders = 0;
-
+	/**
+	 * Query vars set by the user.
+	 *
+	 * @var array
+	 */
 	private $original_args = array();
+
+	/**
+	 * Query vars after processing and sanitization.
+	 *
+	 * @var array
+	 */
 	private $args = array();
 
+	/**
+	 * Columns to be selected in the SELECT clause.
+	 *
+	 * @var array
+	 */
 	private $fields = array();
+
+	/**
+	 * Array of table aliases and conditions used to compute the JOIN clause of the query.
+	 *
+	 * @var array
+	 */
 	private $join = array();
+
+	/**
+	 * Array of fields and conditions used to compute the WHERE clause of the query.
+	 *
+	 * @var array
+	 */
 	private $where = array();
+
+	/**
+	 * Field to be used in the GROUP BY clause of the query.
+	 *
+	 * @var array
+	 */
 	private $groupby = array();
+
+	/**
+	 * Array of fields used to compute the ORDER BY clause of the query.
+	 *
+	 * @var array
+	 */
 	private $orderby = array();
+
+	/**
+	 * Limits used to compute the LIMIT clause of the query.
+	 *
+	 * @var array
+	 */
 	private $limits = array();
 
+	/**
+	 * Results (order IDs) for the current query.
+	 *
+	 * @var array
+	 */
+	private $results = array();
 
+	/**
+	 * Final SQL query to run after processing of args.
+	 *
+	 * @var string
+	 */
+	private $sql = '';
+
+	/**
+	 * The number of pages (when pagination is enabled).
+	 *
+	 * @var int
+	 */
+	private $max_num_pages = 0;
+
+	/**
+	 * The number of orders found.
+	 *
+	 * @var int
+	 */
+	private $found_orders = 0;
+
+	/**
+	 * Sets up and runs the query after processing arguments.
+	 *
+	 * @param array $args Array of query vars.
+	 */
 	public function __construct( $args = array() ) {
 		$datastore = wc_get_container()->get( OrdersTableDataStore::class );
 
 		// TODO: maybe OrdersTableDataStore::get_all_table_names() could return these keys/indices instead.
-		$this->tables = array(
+		$this->tables   = array(
 			'orders'           => $datastore::get_orders_table_name(),
 			'addresses'        => $datastore::get_addresses_table_name(),
 			'operational_data' => $datastore::get_operational_data_table_name(),
@@ -37,87 +121,21 @@ class OrdersTableQuery {
 		);
 		$this->mappings = $datastore->get_all_order_column_mappings();
 
-
 		$this->original_args = $args;
 		$this->args          = $args;
 
+		$this->remap_args();
 		$this->validate_args();
 		$this->build_query();
 		$this->run_query();
 	}
 
-	private function validate_args() {
-		$this->remap_args();
-
-		// status:
-		$valid_statuses = array_keys( wc_get_order_statuses() );
-
-		if ( empty( $this->args['status'] ) || 'any' === $this->args['status'] ) {
-			$this->args['status'] = $valid_statuses;
-		} else if ( 'all' === $this->args['status'] ) {
-			$this->args['status'] = array();
-		} else {
-			$this->args['status'] = is_array( $this->args['status'] ) ? $this->args['status'] : array( $this->args['status'] );
-
-			foreach ( $this->args['status'] as &$status ) {
-				$status = in_array( 'wc-' . $status, $valid_statuses, true ) ? 'wc-' . $status : $status;
-			}
-
-			$this->args['status'] = array_unique( array_filter( $this->args['status'] ) );
-		}
-
-		// order and orderby:
-		$this->args['order'] = $this->sanitize_order( $this->args['order'] ?? '' );
-		$this->sanitize_orderby( $this->args['orderby'] ?? 'date' );
-
-		// TODO: 'type', 'customer'.
-		// TODO: Keys from $this->internal_meta_keys (i.e. _key)
-		// TODO: meta_query
-		// TODO: customer_note, name.
-		unset( $this->args['type'], $this->args['customer'], $this->args['meta_query'], $this->args['customer_note'], $this->args['name'] );
-	}
-
-	private function sanitize_order( $order ) {
-		$order = strtoupper( is_string( $order ) ? $order : '' );
-
-		return in_array( $order, array( 'ASC', 'DESC' ), true ) ? $order : 'DESC';
-	}
-
-	private function sanitize_orderby() {
-		// Allowed keys.
-		// TODO: rand, metakeys, etc.
-		$allowed_keys = array( 'ID'  , 'id'  , 'type', 'date', 'modified', 'parent' );
-
-		// Translate $orderby to a valid field.
-		$mapping = array(
-			'ID'   => 'orders.id',
-			'id'   => 'orders.id',
-			'type' => 'orders.type',
-			'date' => 'orders.date_created_gmt',
-			'modified' => 'orders.date_updated_gmt',
-			'parent' => 'orders.parent_order_id',
-		);
-
-		$order   = $this->args['order'];
-		$orderby = $this->args['orderby'];
-
-		if ( 'none' === $orderby ) {
-			return;
-		}
-
-		if ( is_string( $orderby ) ) {
-			$orderby = array( $orderby => $order );
-		}
-
-		$this->args['orderby'] = array();
-		foreach ( $orderby as $order_key => $order ) {
-			if ( isset( $mapping[ $order_key ] ) ) {
-				$this->args['orderby'][ $mapping[ $order_key ] ] = $this->sanitize_order( $order );
-			}
-		}
-	}
-
-	private function remap_args() {
+	/**
+	 * Remaps some legacy and `WP_Query` specific query vars to vars available in the customer order table scheme.
+	 *
+	 * @return void
+	 */
+	private function remap_args(): void {
 		$mapping = array(
 			// WP_Query legacy.
 			'post_date'           => 'date_created_gmt',
@@ -170,18 +188,104 @@ class OrdersTableQuery {
 		}
 	}
 
-	private function build_query() {
-		$this->build_query_fields();
-		$this->build_query_WHERE();
-		$this->build_query_ORDERBY();
-		$this->build_query_LIMIT();
+	/**
+	 * Validates query vars.
+	 *
+	 * @return void
+	 */
+	private function validate_args(): void {
+		// Order statuses.
+		$valid_statuses = array_keys( wc_get_order_statuses() );
 
-		$distinct = '';
-		$groupby = '';
-		$orderby = 'ORDER BY ' . $this->orderby;
+		if ( empty( $this->args['status'] ) || 'any' === $this->args['status'] ) {
+			$this->args['status'] = $valid_statuses;
+		} elseif ( 'all' === $this->args['status'] ) {
+			$this->args['status'] = array();
+		} else {
+			$this->args['status'] = is_array( $this->args['status'] ) ? $this->args['status'] : array( $this->args['status'] );
+
+			foreach ( $this->args['status'] as &$status ) {
+				$status = in_array( 'wc-' . $status, $valid_statuses, true ) ? 'wc-' . $status : $status;
+			}
+
+			$this->args['status'] = array_unique( array_filter( $this->args['status'] ) );
+		}
+
+		// 'order' and 'orderby' vars.
+		$this->args['order'] = $this->sanitize_order( $this->args['order'] ?? '' );
+		$this->sanitize_orderby( $this->args['orderby'] ?? 'date' );
+
+		// TODO: 'type', 'customer'.
+		// TODO: Keys from $this->internal_meta_keys (i.e. _key)
+		// TODO: meta_query
+		// TODO: customer_note, name.
+		unset( $this->args['type'], $this->args['customer'], $this->args['meta_query'], $this->args['customer_note'], $this->args['name'] );
+	}
+
+	/**
+	 * Parses and sanitizes the 'orderby' query var.
+	 *
+	 * @return void
+	 */
+	private function sanitize_orderby(): void {
+		// Allowed keys.
+		// TODO: rand, metakeys, etc.
+		$allowed_keys = array( 'ID', 'id', 'type', 'date', 'modified', 'parent' );
+
+		// Translate $orderby to a valid field.
+		$mapping = array(
+			'ID'       => 'orders.id',
+			'id'       => 'orders.id',
+			'type'     => 'orders.type',
+			'date'     => 'orders.date_created_gmt',
+			'modified' => 'orders.date_updated_gmt',
+			'parent'   => 'orders.parent_order_id',
+		);
+
+		$order   = $this->args['order'];
+		$orderby = $this->args['orderby'];
+
+		if ( 'none' === $orderby ) {
+			return;
+		}
+
+		if ( is_string( $orderby ) ) {
+			$orderby = array( $orderby => $order );
+		}
+
+		$this->args['orderby'] = array();
+		foreach ( $orderby as $order_key => $order ) {
+			if ( isset( $mapping[ $order_key ] ) ) {
+				$this->args['orderby'][ $mapping[ $order_key ] ] = $this->sanitize_order( $order );
+			}
+		}
+	}
+
+	/**
+	 * Makes sure the order in an ORDER BY statement is either 'ASC' o 'DESC'.
+	 *
+	 * @param string $order The unsanitized order.
+	 * @return string The sanitized order.
+	 */
+	private function sanitize_order( string $order ): string {
+		$order = strtoupper( is_string( $order ) ? $order : '' );
+
+		return in_array( $order, array( 'ASC', 'DESC' ), true ) ? $order : 'DESC';
+	}
+
+	/**
+	 * Builds the final SQL query to be run.
+	 *
+	 * @return void
+	 */
+	private function build_query(): void {
+		$this->parse_core_fields();
+		$this->parse_orderby();
+		$this->parse_limit();
 
 		// SELECT [fields].
-		$fields = $this->fields;
+		$this->fields = 'orders.id';
+		$fields       = $this->fields;
 
 		// SQL_CALC_FOUND_ROWS.
 		if ( ( ! $this->arg_isset( 'no_found_rows' ) || ! $this->args['no_found_rows'] ) && $this->limits ) {
@@ -208,22 +312,31 @@ class OrdersTableQuery {
 			$where .= " AND ({$condition})";
 		}
 
+		// ORDER BY.
+		$orderby = 'ORDER BY ' . $this->orderby;
+
 		// LIMITS.
 		$limits = $this->limits ? 'LIMIT ' . implode( ',', $this->limits ) : '';
 
-	// 	$found_rows = '';
-	// 	$distinct = '';
-	// 	$fields = "orders.*";
-	// 	$join = '';
-	// 	$where = '';
-	// 	$groupby = '';
-	// 	$orderby = '';
-	// 	$limits = 'LIMIT 20';
+		$distinct = '';
+		$groupby  = '';
 
-		$this->sql = "SELECT $found_rows $distinct $fields FROM {$this->tables['orders']} orders $join WHERE $where $groupby $orderby $limits";
+		$orders_table = $this->tables['orders'];
+
+		$this->sql = "SELECT $found_rows $distinct $fields FROM $orders_table orders $join WHERE $where $groupby $orderby $limits";
 	}
 
-	private function get_where_condition( $table, $field, $value, $type, $operator = false ) {
+	/**
+	 * Generates a properly escaped and sanitized WHERE condition for a given field.
+	 *
+	 * @param string $table    The table the field belongs to.
+	 * @param string $field    The field or column name.
+	 * @param mixed  $value    The value.
+	 * @param string $type     The column type as specified in {@see OrdersTableDataStore} column mappings.
+	 * @param string $operator The operator to use in the condition. Defaults to '=' or 'IN' depending on $value.
+	 * @return string The resulting WHERE condition.
+	 */
+	private function get_where_condition( string $table, string $field, $value, string $type, string $operator = '' ): string {
 		global $wpdb;
 
 		$db_util  = wc_get_container()->get( DatabaseUtil::class );
@@ -238,7 +351,7 @@ class OrdersTableQuery {
 		// = and != can be shorthands for IN and NOT in for array values.
 		if ( is_array( $value ) && '=' === $operator ) {
 			$operator = 'IN';
-		} else if ( is_array( $value ) && '!=' === $operator ) {
+		} elseif ( is_array( $value ) && '!=' === $operator ) {
 			$operator = 'NOT IN';
 		}
 
@@ -253,49 +366,75 @@ class OrdersTableQuery {
 			$placeholder = $format;
 		}
 
-		$sql = $wpdb->prepare( "{$table}.{$field} {$operator} {$placeholder}", $value );
+		$sql = $wpdb->prepare( "{$table}.{$field} {$operator} {$placeholder}", $value ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared,WordPress.DB.PreparedSQLPlaceholders.UnfinishedPrepare
 
 		return $sql;
 	}
 
-	private function build_query_fields() {
-		$this->fields = 'orders.id';
-	}
-
-	private function build_query_WHERE() {
+	/**
+	 * Processes query vars for all supported core fields from custom order tables.
+	 *
+	 * @return void
+	 */
+	private function parse_core_fields(): void {
 		global $wpdb;
 
-		// orders:
+		// Orders.
 		foreach ( array( 'id', 'status', 'type', 'currency', 'tax_amount', 'customer_id', 'billing_email', 'total_amount', 'parent_order_id', 'payment_method', 'payment_method_title', 'transaction_id', 'ip_address', 'user_agent' ) as $arg_key ) {
 			if ( ! $this->arg_isset( $arg_key ) ) {
 				continue;
 			}
 
-			$this->where[] = array( 'table'  => 'orders', 'field' => $arg_key, 'value' => $this->args[ $arg_key ], 'type' => $this->mappings['orders'][ $arg_key ]['type'] );
+			$this->where[] = array(
+				'table' => 'orders',
+				'field' => $arg_key,
+				'value' => $this->args[ $arg_key ],
+				'type'  => $this->mappings['orders'][ $arg_key ]['type'],
+			);
 		}
 
 		if ( $this->arg_isset( 'parent_exclude' ) ) {
-			$this->where[] = array( 'table'  => 'orders', 'field' => 'id', 'operator' => '!=', 'value' => $this->args['parent_exclude'], 'type' => 'int' );
+			$this->where[] = array(
+				'table'    => 'orders',
+				'field'    => 'id',
+				'operator' => '!=',
+				'value'    => $this->args['parent_exclude'],
+				'type'     => 'int',
+			);
 		}
 
 		if ( $this->arg_isset( 'exclude' ) ) {
-			$this->where[] = array( 'table'  => 'orders', 'field' => 'id', 'operator' => '!=', 'value' => $this->args['exclude'], 'type' => 'int' );
+			$this->where[] = array(
+				'table'    => 'orders',
+				'field'    => 'id',
+				'operator' => '!=',
+				'value'    => $this->args['exclude'],
+				'type'     => 'int',
+			);
 		}
 
-		// operational_data:
-		foreach( array( 'created_via', 'woocommerce_version', 'prices_include_tax', 'order_key', 'discount_total_amount', 'discount_tax_amount', 'shipping_total_amount', 'shipping_tax_amount' ) as $arg_key ) {
+		// Operational data.
+		foreach ( array( 'created_via', 'woocommerce_version', 'prices_include_tax', 'order_key', 'discount_total_amount', 'discount_tax_amount', 'shipping_total_amount', 'shipping_tax_amount' ) as $arg_key ) {
 			if ( ! $this->arg_isset( $arg_key ) ) {
 				continue;
 			}
 
 			if ( ! isset( $this->join['operational_data'] ) ) {
-				$this->join['operational_data'] = array( 'table' => $this->tables['operational_data'], 'on' => 'orders.id = operational_data.order_id' );
+				$this->join['operational_data'] = array(
+					'table' => $this->tables['operational_data'],
+					'on'    => 'orders.id = operational_data.order_id',
+				);
 			}
 
-			$this->where[] = array( 'table'  => 'operational_data', 'field' => $arg_key, 'value' => $this->args[ $arg_key ], 'type' => $this->mappings['orders'][ $arg_key ]['type'] );
+			$this->where[] = array(
+				'table' => 'operational_data',
+				'field' => $arg_key,
+				'value' => $this->args[ $arg_key ],
+				'type'  => $this->mappings['operational_data'][ $arg_key ]['type'],
+			);
 		}
 
-		// billing & shipping address fields (except email):
+		// Process billing & shipping address fields.
 		foreach ( array( 'billing', 'shipping' ) as $address_type ) {
 			foreach ( array( 'first_name', 'last_name', 'company', 'address_1', 'address_2', 'city', 'state', 'postcode', 'country', 'phone' ) as $field_name ) {
 				if ( ! $this->arg_isset( "{$address_type}_{$field_name}" ) ) {
@@ -303,15 +442,28 @@ class OrdersTableQuery {
 				}
 
 				if ( ! isset( $this->join[ "{$address_type}_address" ] ) ) {
-					$this->join[ "{$address_type}_address" ] = array( 'table' => $this->tables['addresses'], 'on' => $wpdb->prepare( "orders.id = {$address_type}_address.order_id AND {$address_type}_address.address_type = %s", $address_type ) );
+					$this->join[ "{$address_type}_address" ] = array(
+						'table' => $this->tables['addresses'],
+						'on'    => $wpdb->prepare( "orders.id = {$address_type}_address.order_id AND {$address_type}_address.address_type = %s", $address_type ), // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+					);
 				}
 
-				$this->where[] = array( 'table'  => "{$address_type}_address", 'field' => $field_name, 'value' => $this->args[ "{$address_type}_{$field_name}" ], 'type' => $this->mappings[ "{$address_type}_address" ][ $field_name ]['type'] );
+				$this->where[] = array(
+					'table' => "{$address_type}_address",
+					'field' => $field_name,
+					'value' => $this->args[ "{$address_type}_{$field_name}" ],
+					'type'  => $this->mappings[ "{$address_type}_address" ][ $field_name ]['type'],
+				);
 			}
 		}
 	}
 
-	private function build_query_ORDERBY() {
+	/**
+	 * Generates the ORDER BY clause.
+	 *
+	 * @return void
+	 */
+	private function parse_orderby(): void {
 		$orderby = $this->args['orderby'];
 
 		if ( 'none' === $orderby ) {
@@ -327,7 +479,12 @@ class OrdersTableQuery {
 		$this->orderby = implode( ', ', $orderby_array );
 	}
 
-	private function build_query_LIMIT() {
+	/**
+	 * Generates the limits to be used in the LIMIT clause.
+	 *
+	 * @return void
+	 */
+	private function parse_limit(): void {
 		$paginate = ( $this->arg_isset( 'paginate' ) ? (bool) $this->args['paginate'] : false );
 		$limit    = ( $this->arg_isset( 'limit' ) ? absint( $this->args['limit'] ) : false );
 		$page     = ( $this->arg_isset( 'page' ) ? absint( $this->args['page'] ) : 1 );
@@ -340,15 +497,26 @@ class OrdersTableQuery {
 		$this->limits = array( $offset ? $offset : absint( ( $page - 1 ) * $limit ), $limit );
 	}
 
-	private function arg_isset( $arg_key ) {
+	/**
+	 * Checks if a query var is set (i.e. not one of the "skipped values").
+	 *
+	 * @param string $arg_key Query var.
+	 * @return bool TRUE if query var is set.
+	 */
+	private function arg_isset( string $arg_key ): bool {
 		return ( isset( $this->args[ $arg_key ] ) && ! in_array( $this->args[ $arg_key ], self::SKIPPED_VALUES, true ) );
 	}
 
+	/**
+	 * Runs the SQL query.
+	 *
+	 * @return void
+	 */
 	private function run_query() {
 		global $wpdb;
 
 		// Run query.
-		$this->orders = array_map( 'absint', $wpdb->get_col( $this->sql ) );
+		$this->orders = array_map( 'absint', $wpdb->get_col( $this->sql ) ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
 
 		// Set max_num_pages and found_orders if necessary.
 		if ( ( $this->arg_isset( 'no_found_rows' ) && ! $this->args['no_found_rows'] ) || empty( $this->orders ) ) {
@@ -360,6 +528,27 @@ class OrdersTableQuery {
 			$this->max_num_pages = (int) ceil( $this->found_orders / $this->args['limit'] );
 		} else {
 			$this->found_orders = count( $this->orders );
+		}
+	}
+
+	/**
+	 * Make some private available for backwards compatibility.
+	 *
+	 * @param string $name Property to get.
+	 * @return mixed
+	 */
+	public function __get( string $name ) {
+		switch ( $name ) {
+			case 'found_orders':
+			case 'found_posts':
+				return $this->found_orders;
+			case 'max_num_pages':
+				return $this->max_num_pages;
+			case 'posts':
+			case 'orders':
+				return $this->results;
+			default:
+				break;
 		}
 	}
 
