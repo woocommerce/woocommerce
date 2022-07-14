39470e9f5c024a40fc7c9cdd2228663916f288b8
louwie17 <lourensschep@gmail.com>
2022-07-14 08:30:03 -0300

    Fix API triggered tracks not firing (#33872)
    * Revert back track property changes to WC_tracks class
    
    * Add changelog

--- plugins/woocommerce/includes/tracks/class-wc-tracks.php
+++ plugins/woocommerce/includes/tracks/class-wc-tracks.php
@@ -16,6 +16,57 @@ class WC_Tracks {
 	const PREFIX = 'wcadmin_';
 
 	/**
+	 * Get total product counts.
+	 *
+	 * @return int Number of products.
+	 */
+	public static function get_products_count() {
+		$product_counts = WC_Tracker::get_product_counts();
+		return $product_counts['total'];
+	}
+
+	/**
+	 * Gather blog related properties.
+	 *
+	 * @param int $user_id User id.
+	 * @return array Blog details.
+	 */
+	public static function get_blog_details( $user_id ) {
+		$blog_details = get_transient( 'wc_tracks_blog_details' );
+		if ( false === $blog_details ) {
+			$blog_details = array(
+				'url'            => home_url(),
+				'blog_lang'      => get_user_locale( $user_id ),
+				'blog_id'        => class_exists( 'Jetpack_Options' ) ? Jetpack_Options::get_option( 'id' ) : null,
+				'products_count' => self::get_products_count(),
+				'wc_version'     => WC()->version,
+			);
+			set_transient( 'wc_tracks_blog_details', $blog_details, DAY_IN_SECONDS );
+		}
+		return $blog_details;
+	}
+
+	/**
+	 * Gather details from the request to the server.
+	 *
+	 * @return array Server details.
+	 */
+	public static function get_server_details() {
+		$data = array();
+
+		$data['_via_ua'] = isset( $_SERVER['HTTP_USER_AGENT'] ) ? wc_clean( wp_unslash( $_SERVER['HTTP_USER_AGENT'] ) ) : '';
+		$data['_via_ip'] = isset( $_SERVER['REMOTE_ADDR'] ) ? wc_clean( wp_unslash( $_SERVER['REMOTE_ADDR'] ) ) : '';
+		$data['_lg']     = isset( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) ? wc_clean( wp_unslash( $_SERVER['HTTP_ACCEPT_LANGUAGE'] ) ) : '';
+		$data['_dr']     = isset( $_SERVER['HTTP_REFERER'] ) ? wc_clean( wp_unslash( $_SERVER['HTTP_REFERER'] ) ) : '';
+
+		$uri         = isset( $_SERVER['REQUEST_URI'] ) ? wc_clean( wp_unslash( $_SERVER['REQUEST_URI'] ) ) : '';
+		$host        = isset( $_SERVER['HTTP_HOST'] ) ? wc_clean( wp_unslash( $_SERVER['HTTP_HOST'] ) ) : '';
+		$data['_dl'] = isset( $_SERVER['REQUEST_SCHEME'] ) ? wc_clean( wp_unslash( $_SERVER['REQUEST_SCHEME'] ) ) . '://' . $host . $uri : '';
+
+		return $data;
+	}
+
+	/**
 	 * Record an event in Tracks - this is the preferred way to record events from PHP.
 	 * Note: the event request won't be made if $properties has a member called `error`.
 	 *
@@ -69,6 +120,16 @@ class WC_Tracks {
 		unset( $properties['_ui'] );
 		unset( $properties['_ut'] );
 
-		return array_merge( $properties, $identity );
+		$data = $event_name
+			? array(
+				'_en' => $event_name,
+				'_ts' => WC_Tracks_Client::build_timestamp(),
+			)
+			: array();
+
+		$server_details = self::get_server_details();
+		$blog_details   = self::get_blog_details( $user->ID );
+
+		return array_merge( $properties, $data, $server_details, $identity, $blog_details );
 	}
 }
