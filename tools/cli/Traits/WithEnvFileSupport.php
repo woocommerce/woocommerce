14889db36a7c124208f8aea0020fc5b70c451fa4
Lucas Bustamante <lucasfbustamante@gmail.com>
2022-07-15 13:05:58 -0300

    PHPCS

--- tools/cli/Traits/WithEnvFileSupport.php
+++ tools/cli/Traits/WithEnvFileSupport.php
@@ -1,12 +1,13 @@
 <?php
+
+namespace WooCommerce\Dev\CLI\Traits;
+
 /**
  * Implements methods to provide environment file (.env) support
  * to a command.
  *
  * @package WooCommerce\Dev\CLI\Traits
  */
-
-namespace WooCommerce\Dev\CLI\Traits;
 
 use RuntimeException;
 
@@ -15,46 +16,43 @@ use RuntimeException;
  *
  * @package WooCommerce\Dev\CLI\Traits
  */
-trait WithEnvFileSupport
-{
-
+trait WithEnvFileSupport {
 	/**
 	 * Reads and exports, by means of calls to the `setenv` function, the contents
 	 * of a list of files in the .env format (KEY=VALUE\n).
-	 *
-	 * @since TBD
 	 *
 	 * @param array<string> $envFiles A list of .env format files to read. Non-existing files will
 	 *                             be ignored.
 	 *
 	 * @throws RuntimeException If one of the environment files cannot be read or there's an issue with its handling.
+	 * @since TBD
+	 *
 	 */
-	protected function exportEnvFiles(array $envFiles): void
-	{
-		foreach ($envFiles as $envFile) {
-			if ( ! (is_file($envFile) && is_readable($envFile))) {
+	protected function exportEnvFiles( array $envFiles ): void {
+		foreach ( $envFiles as $envFile ) {
+			if ( ! ( is_file( $envFile ) && is_readable( $envFile ) ) ) {
 				continue;
 			}
 
-			if ( ! $envFileHandle = fopen($envFile, 'rb')) {
-				throw new RuntimeException("Cannot read file {$envFile}.");
+			if ( ! $envFileHandle = fopen( $envFile, 'rb' ) ) {
+				throw new RuntimeException( "Cannot read file {$envFile}." );
 			}
 
-			while ($line = fgets($envFileHandle)) {
-				if ( ! preg_match('/[\w_-]/', $line) || strpos($line, '=') === false) {
+			while ( $line = fgets( $envFileHandle ) ) {
+				if ( ! preg_match( '/[\w_-]/', $line ) || strpos( $line, '=' ) === false ) {
 					// Comment or blob or not a correct line.
 					continue;
 				}
-				$setting = trim($line);
-				[$key, $value] = explode('=', $setting, 2);
-				putenv($setting);
-				if (isset($_ENV)) {
-					$_ENV[trim($key)] = trim($value);
+				$setting = trim( $line );
+				[ $key, $value ] = explode( '=', $setting, 2 );
+				putenv( $setting );
+				if ( isset( $_ENV ) ) {
+					$_ENV[ trim( $key ) ] = trim( $value );
 				}
 			}
 
-			if ( ! fclose($envFileHandle)) {
-				throw new RuntimeException("Cannot close {$envFile} file handle.");
+			if ( ! fclose( $envFileHandle ) ) {
+				throw new RuntimeException( "Cannot close {$envFile} file handle." );
 			}
 		}
 	}
