fab865b937ebbf2469c1ba01e6c9367972eeaa21
Sam Seay <samueljseay@gmail.com>
2022-07-15 15:08:42 +1200

    Migrate the analyzer's DB scan to the tmp repo clone approach

--- tools/code-analyzer/src/commands/analyzer/index.ts
+++ tools/code-analyzer/src/commands/analyzer/index.ts
@@ -3,7 +3,7 @@
  */
 import { CliUx, Command, Flags } from '@oclif/core';
 import { join } from 'path';
-import { readFileSync } from 'fs';
+import { readFileSync, rmSync } from 'fs';
 
 /**
  * Internal dependencies
@@ -24,7 +24,8 @@ import {
 	getHookDescription,
 	getHookChangeType,
 } from '../../utils';
-import { generateDiff, generatePatch, generateSchemaDiff } from '../../git';
+import { cloneRepo, generateDiff, generateSchemaDiff } from '../../git';
+import { execSync } from 'child_process';
 
 /**
  * Analyzer class
@@ -87,29 +88,61 @@ export default class Analyzer extends Command {
 	async run(): Promise< void > {
 		const { args, flags } = await this.parse( Analyzer );
 
+		CliUx.ux.action.start(
+			`Making a temporary clone of '${ flags.source }'`
+		);
+		const tmpRepoPath = await cloneRepo( flags.source );
+		CliUx.ux.action.stop();
+
+		CliUx.ux.action.start(
+			`Comparing '${ flags.base }' with '${ args.compare }'`
+		);
 		const diff = await generateDiff(
-			flags.source,
+			tmpRepoPath,
 			flags.base,
 			args.compare,
 			this.error
 		);
+		CliUx.ux.action.stop();
 
 		const pluginData = this.getPluginData( flags.plugin );
 		this.log( `${ pluginData[ 1 ] } Version: ${ pluginData[ 0 ] }` );
 
 		// Run schema diffs only in the monorepo.
 		if ( flags[ 'is-woocommerce' ] ) {
+			CliUx.ux.action.start( 'Building WooCommerce' );
+
+			const pluginPath = join( tmpRepoPath, 'plugins/woocommerce' );
+
+			// Note doing the minimal work to get a DB scan to work, avoiding full build for speed.
+			execSync( 'composer install', { cwd: pluginPath, stdio: [] } );
+			execSync( 'pnpm run build:feature-config', {
+				cwd: pluginPath,
+			} );
+
+			CliUx.ux.action.stop();
+			CliUx.ux.action.start(
+				`Comparing WooCommerce DB schemas of '${ flags.base }' and '${ args.compare }'`
+			);
+
 			const schemaDiff = await generateSchemaDiff(
-				'woocommerce/woocommerce',
+				tmpRepoPath,
 				args.compare,
 				flags.base,
 				( e: string ): void => this.error( e )
 			);
 
+			CliUx.ux.action.stop();
+
 			this.scanChanges( diff, pluginData[ 0 ], flags.output, schemaDiff );
 		} else {
 			this.scanChanges( diff, pluginData[ 0 ], flags.output );
 		}
+
+		// Clean up the temporary repo.
+		CliUx.ux.action.start( 'Cleaning up temporary files' );
+		rmSync( tmpRepoPath, { force: true, recursive: true } );
+		CliUx.ux.action.stop();
 	}
 
 	/**
@@ -191,6 +224,7 @@ export default class Analyzer extends Command {
 			};
 		} | void
 	): void {
+		CliUx.ux.action.start( 'Generating changes' );
 		const templates = this.scanTemplates( content, version );
 		const hooks = this.scanHooks( content, version, output );
 		const databaseUpdates = this.scanDatabases( content );
@@ -234,6 +268,8 @@ export default class Analyzer extends Command {
 		} else {
 			this.log( 'No database updates found' );
 		}
+
+		CliUx.ux.action.stop();
 	}
 	/**
 	 * Scan patches for changes in the database
--- tools/code-analyzer/src/git.ts
+++ tools/code-analyzer/src/git.ts
@@ -5,14 +5,14 @@ import { CliUx } from '@oclif/core';
 import { execSync } from 'child_process';
 import { join } from 'path';
 import { tmpdir } from 'os';
-import { mkdirSync, readFileSync, rmSync } from 'fs';
+import { mkdirSync } from 'fs';
 import { simpleGit } from 'simple-git';
 import { v4 } from 'uuid';
 
 /**
  * Internal dependencies
  */
-import { startWPEnv, stopWPEnv, isValidCommitHash } from './utils';
+import { startWPEnv, stopWPEnv } from './utils';
 
 /**
  * Check if a string is a valid url.
@@ -46,6 +46,9 @@ export const cloneRepo = async ( repoPath: string ) => {
 	if ( ! isUrl( repoPath ) ) {
 		execSync( `cp ${ repoPath }/.git/config ${ folderPath }/.git/config` );
 	}
+
+	// Update the repo.
+	await git.fetch();
 
 	return folderPath;
 };
@@ -105,21 +108,19 @@ export const getCommitHash = async ( baseDir: string, ref: string ) => {
 /**
  * generateDiff generates a diff for a given repo and 2 hashes or branch names.
  *
- * @param {string}   repoPath - the url or filepath of the repo to clone.
- * @param {string}   hashA    - commit hash or branch name.
- * @param {string}   hashB    - commit hash or branch name.
- * @param {Function} onError  - the handler to call when an error occurs.
+ * @param {string}   tmpRepoPath - filepath to the repo to generate a diff from.
+ * @param {string}   hashA       - commit hash or branch name.
+ * @param {string}   hashB       - commit hash or branch name.
+ * @param {Function} onError     - the handler to call when an error occurs.
  */
 export const generateDiff = async (
-	repoPath: string,
+	tmpRepoPath: string,
 	hashA: string,
 	hashB: string,
 	onError: ( error: string ) => void
 ) => {
 	try {
-		const tmpRepoPath = await cloneRepo( repoPath );
 		const git = simpleGit( { baseDir: tmpRepoPath } );
-		await git.fetch();
 
 		const validBranches = [ hashA, hashB ].filter(
 			( hash ) => ! refIsHash( hash )
@@ -145,9 +146,6 @@ export const generateDiff = async (
 
 		const diff = await diffHashes( tmpRepoPath, commitHashA, commitHashB );
 
-		// time to clean up
-		rmSync( tmpRepoPath, { force: true, recursive: true } );
-
 		return diff;
 	} catch ( e ) {
 		if ( e instanceof Error ) {
@@ -163,163 +161,65 @@ export const generateDiff = async (
 		return '';
 	}
 };
-
-/**
- * Fetch branches from origin.
- *
- * @param {string}   branch branch/commit hash.
- * @param {Function} error  error print method.
- * @return {boolean} Promise.
- */
-export const fetchBranch = (
-	branch: string,
-	error: ( s: string ) => void
-): boolean => {
-	CliUx.ux.action.start( `Fetching ${ branch }` );
-	const branches = execSync( 'git branch', {
-		encoding: 'utf-8',
-	} );
-
-	const branchExistsLocally = branches.includes( branch );
-
-	if ( branchExistsLocally ) {
-		CliUx.ux.action.stop();
-		return true;
-	}
-
-	if ( isValidCommitHash( branch ) ) {
-		// The hash is valid and available in history. No need to fetch anything.
-		return true;
-	}
-
-	try {
-		// Fetch branch.
-		execSync( `git fetch origin ${ branch }` );
-		// Create branch.
-		execSync( `git branch ${ branch } origin/${ branch }` );
-	} catch ( e ) {
-		error(
-			`Unable to fetch ${ branch }. Supply a valid branch name or commit hash.`
-		);
-		return false;
-	}
-
-	CliUx.ux.action.stop();
-	return true;
-};
-
-/**
- * Generate a patch file into the temp directory and return its contents
- *
- * @param {string}   source  The GitHub repository.
- * @param {string}   compare Branch/commit hash to compare against the base.
- * @param {string}   base    Base branch/commit hash.
- * @param {Function} error   error print method.
- * @return {string} patch string.
- */
-export const generatePatch = (
-	source: string,
-	compare: string,
-	base: string,
-	error: ( s: string ) => void
-): string => {
-	const filename = `${ source }-${ base }-${ compare }.patch`.replace(
-		/\//g,
-		'-'
-	);
-	const filepath = join( tmpdir(), filename );
-
-	fetchBranch( base, error );
-	fetchBranch( compare, error );
-
-	CliUx.ux.action.start( 'Generating patch for ' + compare );
-
-	try {
-		const diffCommand = `git diff ${ base }...${ compare } > ${ filepath }`;
-		execSync( diffCommand );
-	} catch ( e ) {
-		error(
-			'Unable to create diff. Check that git origin, base branch, and compare branch all exist.'
-		);
-	}
-
-	const content = readFileSync( filepath ).toString();
-
-	CliUx.ux.action.stop();
-	return content;
-};
-
 /**
  * Get all schema strings found in WooCommerce.
  *
- * @param {string}   branch branch being compared.
- * @param {Function} error  Error logging function.
+ * @param {string}   tmpRepoPath - filepath to the repo to generate a schema from.
+ * @param {Function} error       - Error logging function.
  * @return {Object}	Object of schema strings.
  */
-export const getSchema = (
-	branch: string,
+export const getSchema = async (
+	tmpRepoPath: string,
 	error: ( s: string ) => void
-): {
+): Promise< {
 	schema: string;
 	OrdersTableDataStore: string;
-} | void => {
-	// Save the current branch for later.
-	const currentBranch = execSync( 'git rev-parse --abbrev-ref HEAD' );
-
+} | void > => {
 	try {
-		// Make sure the branch is available.
-		fetchBranch( branch, error );
-		// Start spinner.
-		CliUx.ux.action.start( `Gathering schema from ${ branch }` );
-
-		// Checkout branch to compare
-		execSync( `git checkout ${ branch }` );
-
+		const pluginPath = join( tmpRepoPath, 'plugins/woocommerce' );
 		const getSchemaPath =
 			'wp-content/plugins/woocommerce/bin/wc-get-schema.php';
+
 		// Get the WooCommerce schema from wp cli
 		const schema = execSync(
 			`wp-env run cli "wp eval-file '${ getSchemaPath }'"`,
 			{
-				cwd: 'plugins/woocommerce',
+				cwd: pluginPath,
 				encoding: 'utf-8',
 			}
 		);
+
 		// Get the OrdersTableDataStore schema.
 		const OrdersTableDataStore = execSync(
 			'wp-env run cli "wp eval \'echo (new Automattic\\WooCommerce\\Internal\\DataStores\\Orders\\OrdersTableDataStore)->get_database_schema();\'"',
 			{
-				cwd: 'plugins/woocommerce',
+				cwd: pluginPath,
 				encoding: 'utf-8',
 			}
 		);
-		// Return to the current branch.
-		execSync( `git checkout ${ currentBranch }` );
 
-		CliUx.ux.action.stop();
 		return {
 			schema,
 			OrdersTableDataStore,
 		};
 	} catch ( e ) {
-		// Return to the current branch.
-		execSync( `git checkout ${ currentBranch }` );
-
-		error( `Unable to get schema for branch ${ branch }. \n${ e }` );
+		if ( e instanceof Error ) {
+			error( e.message );
+		}
 	}
 };
 
 /**
  * Generate a schema for each branch being compared.
  *
- * @param {string}   source  The GitHub repository.
- * @param {string}   compare Branch/commit hash to compare against the base.
- * @param {string}   base    Base branch/commit hash.
- * @param {Function} error   error print method.
+ * @param {string}   tmpRepoPath Path to repository used to generate schema diff.
+ * @param {string}   compare     Branch/commit hash to compare against the base.
+ * @param {string}   base        Base branch/commit hash.
+ * @param {Function} error       error print method.
  * @return {Object|void}     diff object.
  */
 export const generateSchemaDiff = async (
-	source: string,
+	tmpRepoPath: string,
 	compare: string,
 	base: string,
 	error: ( s: string ) => void
@@ -332,13 +232,40 @@ export const generateSchemaDiff = async (
 		areEqual: boolean;
 	};
 } | void > => {
+	const git = simpleGit( { baseDir: tmpRepoPath } );
+
 	// Be sure the wp-env engine is started.
-	await startWPEnv( error );
+	await startWPEnv( tmpRepoPath, error );
 
-	const baseSchema = getSchema( base, error );
-	const compareSchema = getSchema( compare, error );
+	CliUx.ux.action.start( `Gathering schema from ${ base }` );
 
-	stopWPEnv( error );
+	// Force checkout because sometimes a build will generate a lockfile change.
+	await git.checkout( base, [ '--force' ] );
+	const baseSchema = await getSchema(
+		tmpRepoPath,
+		( errorMessage: string ) => {
+			error(
+				`Unable to get schema for branch ${ base }. \n${ errorMessage }`
+			);
+		}
+	);
+	CliUx.ux.action.stop();
+
+	CliUx.ux.action.start( `Gathering schema from ${ compare }` );
+
+	// Force checkout because sometimes a build will generate a lockfile change.
+	await git.checkout( compare, [ '--force' ] );
+	const compareSchema = await getSchema(
+		tmpRepoPath,
+		( errorMessage: string ) => {
+			error(
+				`Unable to get schema for branch ${ compare }. \n${ errorMessage }`
+			);
+		}
+	);
+	CliUx.ux.action.stop();
+
+	stopWPEnv( tmpRepoPath, error );
 
 	if ( ! baseSchema || ! compareSchema ) {
 		return;
