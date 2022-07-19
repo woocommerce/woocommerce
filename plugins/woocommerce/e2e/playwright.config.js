b88862b41b3c59a44777758940f73ac556b9265e
Lucas Bustamante <lucasfbustamante@gmail.com>
2022-07-19 11:48:30 -0300

    Increase expect timeout to 10s

--- plugins/woocommerce/e2e/playwright.config.js
+++ plugins/woocommerce/e2e/playwright.config.js
@@ -2,6 +2,7 @@ const { devices } = require( '@playwright/test' );
 
 const config = {
 	timeout: 60 * 1000,
+	expect: { timeout: 10000 },
 	outputDir: './report',
 	globalSetup: require.resolve( './global-setup' ),
 	globalTeardown: require.resolve( './global-teardown' ),
