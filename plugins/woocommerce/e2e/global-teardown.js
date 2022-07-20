399796aa2dad8761411712c01a2c8afcd7d6b40e
Lucas Bustamante <lucasfbustamante@gmail.com>
2022-07-20 14:46:29 +0100

    Retry login on e2e (#33997)
    * Retry login on e2e
    
    * Add changelog

--- plugins/woocommerce/e2e/global-teardown.js
+++ plugins/woocommerce/e2e/global-teardown.js
@@ -2,15 +2,22 @@ const { chromium } = require( '@playwright/test' );
 
 module.exports = async ( config ) => {
 	const { baseURL } = config.projects[ 0 ].use;
-	// Clean up the consumer keys
+
 	const browser = await chromium.launch();
 	const adminPage = await browser.newPage();
-	await adminPage.goto( `${ baseURL }/wp-admin` );
-	await adminPage.fill( 'input[name="log"]', 'admin' );
-	await adminPage.fill( 'input[name="pwd"]', 'password' );
-	await adminPage.click( 'text=Log In' );
-	await adminPage.goto(
-		`${ baseURL }/wp-admin/admin.php?page=wc-settings&tab=advanced&section=keys`
-	);
-	await adminPage.dispatchEvent( 'a.submitdelete', 'click' );
+
+	// Clean up the consumer keys
+	const keysRetries = 5;
+	for ( let i = 0; i < keysRetries; i++ ) {
+		try {
+			await adminPage.goto( `${ baseURL }/wp-admin` );
+			await adminPage.fill( 'input[name="log"]', 'admin' );
+			await adminPage.fill( 'input[name="pwd"]', 'password' );
+			await adminPage.click( 'text=Log In' );
+			await adminPage.goto(
+				`${ baseURL }/wp-admin/admin.php?page=wc-settings&tab=advanced&section=keys`
+			);
+			await adminPage.dispatchEvent( 'a.submitdelete', 'click' );
+		} catch ( e ) {}
+	}
 };
