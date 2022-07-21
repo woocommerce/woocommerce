6bdeb0202d5c3bd2b4933d0ae766c679ff8e96b9
Matt Sherman <matt@jam123.com>
2022-07-21 12:37:57 -0400

    Fix phpcs errors

--- plugins/woocommerce-beta-tester/api/api.php
+++ plugins/woocommerce-beta-tester/api/api.php
@@ -1,42 +1,58 @@
 <?php
+/**
+ * REST API routes for the WooCommerce Admin Test Helper.
+ *
+ * @package WC_Beta_Tester
+ */
+
+/**
+ * Register a REST route handler.
+ *
+ *  @param string   $route The base URL for route you are adding.
+ *  @param function $callback The handler for the route.
+ *  @param array    $additional_options An array of options for the endpoint.
+ */
 function register_woocommerce_admin_test_helper_rest_route( $route, $callback, $additional_options = array() ) {
-    add_action( 'rest_api_init', function() use ( $route, $callback, $additional_options ) {
+	add_action(
+		'rest_api_init',
+		function() use ( $route, $callback, $additional_options ) {
 
-    	$default_options = array(
-		    'methods'  => 'POST',
-		    'callback' => $callback,
-		    'permission_callback' => function( $request ) {
-			    if ( ! wc_rest_check_manager_permissions( 'settings', 'edit' ) ) {
-				    return new \WP_Error(
-					    'woocommerce_rest_cannot_edit',
-					    __( 'Sorry, you cannot perform this action', 'woocommerce-admin-test-helper' )
-				    );
-			    }
-			    return true;
-		    },
-	    );
+			$default_options = array(
+				'methods'             => 'POST',
+				'callback'            => $callback,
+				'permission_callback' => function( $request ) {
+					if ( ! wc_rest_check_manager_permissions( 'settings', 'edit' ) ) {
+						return new \WP_Error(
+							'woocommerce_rest_cannot_edit',
+							__( 'Sorry, you cannot perform this action', 'woocommerce-admin-test-helper' )
+						);
+					}
+					return true;
+				},
+			);
 
-    	$default_options = array_merge( $default_options, $additional_options );
+			$default_options = array_merge( $default_options, $additional_options );
 
-        register_rest_route(
-            'wc-admin-test-helper',
-            $route,
-            $default_options
-        );
-    } );
+			register_rest_route(
+				'wc-admin-test-helper',
+				$route,
+				$default_options
+			);
+		}
+	);
 }
 
-require( 'admin-notes/delete-all-notes.php' );
-require( 'admin-notes/add-note.php' );
-require( 'tools/trigger-wca-install.php' );
-require( 'tools/trigger-cron-job.php' );
-require( 'tools/run-wc-admin-daily.php' );
-require( 'options/rest-api.php' );
-require( 'tools/delete-all-products.php');
-require( 'tools/disable-wc-email.php' );
-require( 'tools/trigger-update-callbacks.php' );
-require( 'tracks/recent-tracks-events.php' );
-require( 'tracks/tracks-debug-log.php' );
-require( 'features/features.php' );
-require( 'rest-api-filters/rest-api-filters.php' );
-require( 'rest-api-filters/hook.php' );
+require 'admin-notes/delete-all-notes.php';
+require 'admin-notes/add-note.php';
+require 'tools/trigger-wca-install.php';
+require 'tools/trigger-cron-job.php';
+require 'tools/run-wc-admin-daily.php';
+require 'options/rest-api.php';
+require 'tools/delete-all-products.php';
+require 'tools/disable-wc-email.php';
+require 'tools/trigger-update-callbacks.php';
+require 'tracks/recent-tracks-events.php';
+require 'tracks/tracks-debug-log.php';
+require 'features/features.php';
+require 'rest-api-filters/rest-api-filters.php';
+require 'rest-api-filters/hook.php';
--- plugins/woocommerce-beta-tester/api/tracks/recent-tracks-events.php
+++ plugins/woocommerce-beta-tester/api/tracks/recent-tracks-events.php
@@ -1,29 +1,44 @@
 <?php
+/**
+ * Recent Tracks events.
+ *
+ * @package WC_Beta_Tester
+ */
 
 register_woocommerce_admin_test_helper_rest_route(
 	'/recent-tracks-events',
 	'wca_test_helper_get_recent_tracks_events',
-	[
-		'methods' => 'GET'
-	]
+	array(
+		'methods' => 'GET',
+	)
 );
 
 register_woocommerce_admin_test_helper_rest_route(
 	'/recent-tracks-events',
 	'wca_test_helper_add_recent_tracks_event',
-	[
-		'methods' => 'PUT'
-	]
+	array(
+		'methods' => 'PUT',
+	)
 );
 
 add_filter( 'woocommerce_tracks_event_properties', 'wca_test_helper_tracks_event_properties_filter', 10, 2 );
 
+/**
+ * Handle request to get recent Tracks events.
+ *
+ * @param WP_REST_Request $request Request instance.
+ */
 function wca_test_helper_get_recent_tracks_events( $request ) {
 	$events = get_recent_tracks_events();
 
 	return new WP_REST_Response( $events, 200 );
 }
 
+/**
+ * Handle request to add a recent Tracks event.
+ *
+ * @param WP_REST_Request $request Request instance.
+ */
 function wca_test_helper_add_recent_tracks_event( $request ) {
 	$event = json_decode( $request->get_body() );
 
@@ -32,8 +47,14 @@ function wca_test_helper_add_recent_tracks_event( $request ) {
 	return new WP_REST_Response( $event, 200 );
 }
 
+/**
+ * Hook that logs the Tracks event.
+ *
+ * @param array  $properties Event properties.
+ * @param string $event_name Event name.
+ */
 function wca_test_helper_tracks_event_properties_filter( $properties, $event_name ) {
-	if ( $event_name === false ) {
+	if ( false === $event_name ) {
 		return $properties;
 	}
 
@@ -44,29 +65,43 @@ function wca_test_helper_tracks_event_properties_filter( $properties, $event_nam
 	return $properties;
 }
 
+/**
+ * Log Tracks event.
+ *
+ * @param object $event Tracks event.
+ */
 function log_recent_tracks_event( $event ) {
-	$event_as_json = json_encode( $event );
+	$event_as_json = wp_json_encode( $event );
 
 	$log_file_name = get_recent_tracks_events_log_file_name();
 	file_put_contents( $log_file_name, $event_as_json . "\n", FILE_APPEND );
 }
 
+/**
+ * Build a Tracks event from the supplied properties and event name.
+ *
+ * @param array  $properties Event properties.
+ * @param string $event_name Event name.
+ */
 function build_tracks_event( $properties, $event_name ) {
-	$event = [
-		'eventname' => $event_name,
-		'eventprops' => $properties
-	];
+	$event = array(
+		'eventname'  => $event_name,
+		'eventprops' => $properties,
+	);
 	return $event;
 }
 
+/**
+ * Get recent Tracks events.
+ */
 function get_recent_tracks_events() {
-	// TODO: this implementation feels very inefficient; it would be nice to be able to just send the string back
-	// and set content type to JSON
+	// TODO: this implementation feels very inefficient;
+	// it would be nice to be able to just send the string back and set content type to JSON.
 	$log_file_name = get_recent_tracks_events_log_file_name();
 
 	$log_lines = file( $log_file_name, FILE_IGNORE_NEW_LINES );
 
-	$events = [];
+	$events = array();
 	foreach ( $log_lines as $line ) {
 		$events[] = json_decode( $line );
 	}
@@ -74,6 +109,9 @@ function get_recent_tracks_events() {
 	return $events;
 }
 
+/**
+ * Get the file name for the recent Tracks events log.
+ */
 function get_recent_tracks_events_log_file_name() {
 	return trailingslashit( WC_LOG_DIR ) . 'wca_test_helper_recent_tracks_events.log';
 }
