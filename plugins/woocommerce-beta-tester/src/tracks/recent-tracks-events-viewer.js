d6d4fda572ef872744d8573af6574e3ec7f5c9fb
Matt Sherman <matt@jam123.com>
2022-07-22 15:06:05 -0400

    Show Tracks event properties in viewer.

--- plugins/woocommerce-beta-tester/src/tracks/recent-tracks-events-viewer.js
+++ plugins/woocommerce-beta-tester/src/tracks/recent-tracks-events-viewer.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { useEffect, useState } from "@wordpress/element";
+import { useEffect, useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 
 const RecentTracksEventsViewer = () => {
@@ -20,15 +20,62 @@ const RecentTracksEventsViewer = () => {
 		getRecentTracksEvents();
 	}, [] );
 
+	const renderTracksEventProperty = ( tracksEventProperty ) => {
+		let propertyValue = tracksEventProperty[ 1 ];
+
+		if ( propertyValue === true ) {
+			propertyValue = 'true';
+		} else if ( propertyValue === false ) {
+			propertyValue = 'false';
+		}
+
+		return (
+			<li style={ { color: 'gray' } }>
+				{ tracksEventProperty[ 0 ] }: { propertyValue }
+			</li>
+		);
+	};
+
+	const renderTracksEventProperties = ( tracksEventProperties ) => {
+		return (
+			<ul>
+				{ Object.entries( tracksEventProperties ).map(
+					renderTracksEventProperty
+				) }
+			</ul>
+		);
+	};
+
+	const renderTracksEvent = ( tracksEvent ) => {
+		return (
+			<li>
+				<div>{ tracksEvent.eventname }</div>
+				{ tracksEvent.eventprops &&
+					renderTracksEventProperties( tracksEvent.eventprops ) }
+			</li>
+		);
+	};
+
 	return (
-		<div style={ {position: 'absolute', bottom: 0, height: '100px', overflow: 'scroll', zIndex: 9999, backgroundColor: 'red', color: 'white'} }>
-			{ recentTracksEvents && recentTracksEvents.map( ( tracksEvent ) => (
-				<div>
-					{tracksEvent.eventname}
-				</div>
-			) ) }
+		<div
+			style={ {
+				position: 'fixed',
+				bottom: 0,
+				height: '150px',
+				width: '100%',
+				overflow: 'scroll',
+				zIndex: 9999,
+				backgroundColor: 'black',
+				color: 'white',
+			} }
+		>
+			<div>Recent Tracks Events</div>
+			<ul>
+				{ recentTracksEvents &&
+					recentTracksEvents.map( renderTracksEvent ) }
+			</ul>
 		</div>
 	);
-}
+};
 
 export default RecentTracksEventsViewer;
