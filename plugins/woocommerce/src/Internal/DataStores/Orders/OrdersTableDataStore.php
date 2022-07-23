54a2b65711719f2efa07dcf491660659d3f8f048
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2022-07-23 11:09:40 +0530

    Add suppport for types and refund in migrations.

--- plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableDataStore.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableDataStore.php
@@ -1374,6 +1374,7 @@ CREATE TABLE $orders_table_name (
 	id bigint(20) unsigned auto_increment,
 	status varchar(20) null,
 	currency varchar(10) null,
+	type varchar(20) null,
 	tax_amount decimal(26,8) null,
 	total_amount decimal(26,8) null,
 	customer_id bigint(20) unsigned null,
