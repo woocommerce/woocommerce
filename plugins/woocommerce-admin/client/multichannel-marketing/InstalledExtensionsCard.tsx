e1f36608f7b593960a55bc61381f09fc8b2968d9
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-07-27 02:57:34 +0800

    Display extensions in InstalledExtensionsCard.

--- plugins/woocommerce-admin/client/multichannel-marketing/InstalledExtensionsCard.tsx
+++ plugins/woocommerce-admin/client/multichannel-marketing/InstalledExtensionsCard.tsx
@@ -1,17 +1,64 @@
 /**
  * External dependencies
  */
+import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
-import { CollapsibleCard, CardBody } from './components/CollapsibleCard';
+import {
+	CollapsibleCard,
+	CardBody,
+	CardDivider,
+} from './components/CollapsibleCard';
+import { STORE_KEY } from '../marketing/data/constants';
+
+type Plugin = {
+	slug: string;
+	icon: string;
+	name: string;
+	description: string;
+	status: string;
+	docsUrl?: string;
+	supportUrl?: string;
+	settingsUrl?: string;
+	dashboardUrl?: string;
+};
+
+type UsePluginsType = {
+	installed: Plugin[];
+	activating: Plugin[];
+};
+
+const usePlugins = (): UsePluginsType => {
+	return useSelect( ( select ) => {
+		const { getInstalledPlugins, getActivatingPlugins } =
+			select( STORE_KEY );
+
+		return {
+			installed: getInstalledPlugins(),
+			activating: getActivatingPlugins(),
+		};
+	}, [] );
+};
 
 const InstalledExtensionsCard = () => {
+	const { installed } = usePlugins();
+
 	return (
 		<CollapsibleCard header={ __( 'Installed extensions', 'woocommerce' ) }>
-			<CardBody>Placeholder</CardBody>
+			{ installed.map( ( el, idx ) => {
+				return (
+					<Fragment key={ el.slug }>
+						<CardBody>{ el.name }</CardBody>
+						{ idx !== installed.length - 1 && (
+							<CardDivider></CardDivider>
+						) }
+					</Fragment>
+				);
+			} ) }
 		</CollapsibleCard>
 	);
 };
