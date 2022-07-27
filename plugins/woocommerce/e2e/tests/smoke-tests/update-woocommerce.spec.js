d6a58d66cd9af854d5b6bf1526502ae0a38ef1a4
Rodel <rodel.calasagsag@automattic.com>
2022-07-28 01:54:14 +0800

    Stabilize tests

--- plugins/woocommerce/e2e/tests/smoke-tests/update-woocommerce.spec.js
+++ plugins/woocommerce/e2e/tests/smoke-tests/update-woocommerce.spec.js
@@ -2,6 +2,7 @@ const { UPDATE_WC, ADMINSTATE, PLUGIN_PATH } = process.env;
 const { test, expect } = require( '@playwright/test' );
 
 test.describe( 'WooCommerce plugin can be uploaded and activated', () => {
+	// Skip this test group when the UPDATE_WC environment variable wasn't set, for example in PR smoke tests.
 	test.skip(
 		() => ! Boolean( UPDATE_WC ),
 		'Skipping the WooCommerce plugin upload test.'
@@ -9,15 +10,11 @@ test.describe( 'WooCommerce plugin can be uploaded and activated', () => {
 
 	test.use( { storageState: ADMINSTATE } );
 
-	test.beforeAll( async () => {
-		// mytodo delete current WooCommerce plugin
-	} );
+	test.beforeAll( async ( { browser } ) => {
+		// Create a browser context
+		const context = await browser.newContext();
+		const page = await context.newPage();
 
-	test( 'can upload and activate the WooCommerce plugin', async ( {
-		page,
-	} ) => {
-		// mytodo move this later to beforeAll hook
-		// Delete current WooCommerce plugin
 		// Navigate to 'Installed Plugins' page
 		await page.goto( 'wp-admin/plugins.php', {
 			waitUntil: 'networkidle',
@@ -27,15 +24,30 @@ test.describe( 'WooCommerce plugin can be uploaded and activated', () => {
 		await page
 			.locator( '[data-slug="woocommerce"] #deactivate-woocommerce' )
 			.click();
-		await page.waitForLoadState( 'networkidle' );
+		await expect(
+			page.locator( '#message', { hasText: 'Plugin deactivated.' } )
+		).toBeVisible();
 
 		// Delete WooCommerce
 		page.on( 'dialog', ( dialog ) => dialog.accept() );
 		await page
 			.locator( '[data-slug="woocommerce"] #delete-woocommerce' )
 			.click();
-		await page.waitForLoadState( 'networkidle' );
 
+		// Assert that a confirmation message was displayed.
+		await expect(
+			page.locator( '#woocommerce-deleted', {
+				hasText: 'WooCommerce was successfully deleted.',
+			} )
+		).toBeVisible();
+
+		// Close the created context as a best practice. See https://playwright.dev/docs/api/class-browser#browser-new-context.
+		await context.close();
+	} );
+
+	test( 'can upload and activate the WooCommerce plugin', async ( {
+		page,
+	} ) => {
 		// Open the plugin install page
 		await page.goto( 'wp-admin/plugin-install.php', {
 			waitUntil: 'networkidle',
@@ -65,9 +77,53 @@ test.describe( 'WooCommerce plugin can be uploaded and activated', () => {
 		} );
 
 		// Assert that 'WooCommerce' is listed and active
-		expect( page.locator( '.plugin-title strong' ) ).toHaveText(
-			/^WooCommerce$/
+		await expect(
+			page.locator( '.plugin-title strong', { hasText: /^WooCommerce$/ } )
+		).toBeVisible();
+		await expect( page.locator( '#deactivate-woocommerce' ) ).toBeVisible();
+	} );
+
+	test( 'can run the database update', async ( { page } ) => {
+		const updateButton = page.locator( 'text=Update WooCommerce Database' );
+		const updateCompleteMessage = page.locator(
+			'text=WooCommerce database update complete.'
 		);
-		expect( page.locator( '#deactivate-woocommerce' ) ).toBeVisible();
+
+		// Navigate to 'Installed Plugins' page
+		await page.goto( 'wp-admin/plugins.php', {
+			waitUntil: 'networkidle',
+		} );
+
+		// Skip this test if the "Update WooCommerce Database" button didn't appear.
+		test.skip(
+			! ( await updateButton.isVisible() ),
+			'The "Update WooCommerce Database" button did not appear after updating WooCommerce. Verify with the team if the WooCommerce version being tested does not really trigger a database update.'
+		);
+
+		// If the notice appears, start DB update
+		await updateButton.click();
+		await page.waitForLoadState( 'networkidle' );
+
+		// For 1.5 minutes, repeatedly reload the page every 5 seconds until the "WooCommerce database update complete." message appears.
+		for (
+			let reloads = 0;
+			reloads < 18 && ! ( await updateCompleteMessage.isVisible() );
+			reloads++
+		) {
+			await page.reload( { waitUntil: 'networkidle' } );
+			await page.waitForTimeout( 5000 );
+		}
+
+		// Fail the test if the message didn't appear.
+		test.fail(
+			! ( await updateCompleteMessage.isVisible() ),
+			'The "WooCommerce database update complete." was not visible after 1 minute.'
+		);
+
+		// If the message appeared, click on the "Thanks!" button.
+		await page.locator( 'a', { hasText: 'Thanks!' } ).click();
+
+		// Verify that the "WooCommerce database update complete" message is gone.
+		await expect( updateCompleteMessage ).not.toBeVisible();
 	} );
 } );
