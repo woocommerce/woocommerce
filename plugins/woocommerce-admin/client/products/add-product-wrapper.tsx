7be26eabed6816007de5ec559b865111fc2d6bd2
Fernando Marichal <contacto@fernandomarichal.com>
2022-07-28 18:29:02 -0300

    Add Tracks event

--- plugins/woocommerce-admin/client/products/add-product-wrapper.tsx
+++ plugins/woocommerce-admin/client/products/add-product-wrapper.tsx
@@ -1,4 +1,13 @@
+/**
+ * External dependencies
+ */
+import { recordEvent } from '@woocommerce/tracks';
+import { useEffect } from '@wordpress/element';
+
 export const AddProduct: React.FC = () => {
+	useEffect( () => {
+		recordEvent( 'view_add_product_new_management_experience' );
+	}, [] );
 	return (
 		<div className="woocommerce-add-product">
 			<h1>Add Product</h1>
