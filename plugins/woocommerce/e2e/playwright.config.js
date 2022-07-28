ba5c733c6bcd5b56047e421f5337b9a352c73d8e
Rodel <rodel.calasagsag@automattic.com>
2022-07-28 15:39:32 +0800

    Parse default timeout to a Number

--- plugins/woocommerce/e2e/playwright.config.js
+++ plugins/woocommerce/e2e/playwright.config.js
@@ -2,7 +2,9 @@ const { devices } = require( '@playwright/test' );
 const { CI, BASE_URL, DEFAULT_TIMEOUT_OVERRIDE } = process.env;
 
 const config = {
-	timeout: DEFAULT_TIMEOUT_OVERRIDE ?? 90 * 1000,
+	timeout: DEFAULT_TIMEOUT_OVERRIDE
+		? Number( DEFAULT_TIMEOUT_OVERRIDE )
+		: 90 * 1000,
 	expect: { timeout: 20 * 1000 },
 	outputDir: './report',
 	globalSetup: require.resolve( './global-setup' ),
