6316cb380e314e67fac18d1044f13a0dc8816923
Rodel <rodel.calasagsag@automattic.com>
2022-07-28 20:54:26 +0800

    Lower timeout further

--- plugins/woocommerce/e2e/global-setup.js
+++ plugins/woocommerce/e2e/global-setup.js
@@ -55,7 +55,7 @@ module.exports = async ( config ) => {
 	const customerPage = await browser.newPage();
 
 	// Sign in as admin user and save state
-	const adminRetries = 3; // mytodo switch back to 5
+	const adminRetries = 2; // mytodo switch back to 5
 	for ( let i = 0; i < adminRetries; i++ ) {
 		try {
 			console.log( 'Trying to log-in as admin...' );
