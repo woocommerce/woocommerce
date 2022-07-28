2c8a68315bd950019769938fc842b134e565254e
Néstor Soriano <konamiman@konamiman.com>
2022-07-28 13:20:09 +0200

    Introduce the object cache infrastructure (#33634)
    * Introduce the ObjectCache class.
    
    It's an abstract class intended to be used to implement caching for
    any kind of object (arrays or PHP objects) using the underlying
    WordPress caching or any alternative custom cache engine.
    
    * Add changelog file
    
    * Change the order of parameters in ObjectCache::get
    
    Also change the max expiration value from one day to one month

--- plugins/woocommerce/src/Container.php
+++ plugins/woocommerce/src/Container.php
@@ -9,6 +9,7 @@ use Automattic\WooCommerce\Internal\DependencyManagement\ExtendedContainer;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\COTMigrationServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\DownloadPermissionsAdjusterServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\AssignDefaultCategoryServiceProvider;
+use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\ObjectCacheServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\OrdersDataStoreServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\OptionSanitizerServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\ProductAttributesLookupServiceProvider;
@@ -54,6 +55,7 @@ final class Container implements \Psr\Container\ContainerInterface {
 		RestockRefundedItemsAdjusterServiceProvider::class,
 		UtilsClassesServiceProvider::class,
 		COTMigrationServiceProvider::class,
+		ObjectCacheServiceProvider::class,
 		BatchProcessingServiceProvider::class,
 	);
 
