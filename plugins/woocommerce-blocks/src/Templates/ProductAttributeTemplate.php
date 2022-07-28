aa9ba3823b6848c5204e8c93f4d360b0ca2e3b69
Alba Rincón <albarin@users.noreply.github.com>
2022-07-28 12:49:13 +0200

    Use the archive-product template to render product attributes pages (https://github.com/woocommerce/woocommerce-blocks/pull/6776)

---
+++ plugins/woocommerce-blocks/src/Templates/ProductAttributeTemplate.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Automattic\WooCommerce\Blocks\Templates;
+
+/**
+ * ProductAttributeTemplate class.
+ *
+ * @internal
+ */
+class ProductAttributeTemplate {
+	const SLUG = 'archive-product';
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->init();
+	}
+
+	/**
+	 * Initialization method.
+	 */
+	protected function init() {
+		add_filter( 'taxonomy_template_hierarchy', array( $this, 'update_taxonomy_template_hierarchy' ), 10, 3 );
+	}
+
+	/**
+	 * Render the Archive Product Template for product attributes.
+	 *
+	 * @param array $templates Templates that match the product attributes taxonomy.
+	 */
+	public function update_taxonomy_template_hierarchy( $templates ) {
+		if ( taxonomy_is_product_attribute( get_query_var( 'taxonomy' ) ) && wc_current_theme_is_fse_theme() ) {
+			array_unshift( $templates, self::SLUG );
+		}
+
+		return $templates;
+	}
+}
