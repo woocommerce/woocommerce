bab500771c49aeb6dfc8abb137bd92a9ee4b7162
Fernando Marichal <contacto@fernandomarichal.com>
2022-07-29 12:05:05 -0300

    Renamed tracks event name

--- plugins/woocommerce-admin/client/products/add-product-wrapper.tsx
+++ plugins/woocommerce-admin/client/products/add-product-wrapper.tsx
@@ -6,7 +6,7 @@ import { useEffect } from '@wordpress/element';
 
 export const AddProduct: React.FC = () => {
 	useEffect( () => {
-		recordEvent( 'view_add_product_new_management_experience' );
+		recordEvent( 'view_new_product_management_experience' );
 	}, [] );
 	return (
 		<div className="woocommerce-add-product">
