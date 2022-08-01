e728512bb8965875c09edd4631e3eebdf475b461
Seghir Nadir <nadir.seghir@gmail.com>
2022-08-01 16:29:28 +0100

    fix proceed to checkout button (https://github.com/woocommerce/woocommerce-blocks/pull/6804)

--- plugins/woocommerce-blocks/src/BlockTypes/ProceedToCheckoutBlock.php
+++ plugins/woocommerce-blocks/src/BlockTypes/ProceedToCheckoutBlock.php
@@ -11,4 +11,16 @@ class ProceedToCheckoutBlock extends AbstractInnerBlock {
 	 * @var string
 	 */
 	protected $block_name = 'proceed-to-checkout-block';
+
+
+	/**
+	 * Extra data passed through from server to client for block.
+	 *
+	 * @param array $attributes  Any attributes that currently are available from the block.
+	 *                           Note, this will be empty in the editor context when the block is
+	 *                           not in the post content on editor load.
+	 */
+	protected function enqueue_data( array $attributes = [] ) {
+		$this->asset_data_registry->register_page_id( isset( $attributes['checkoutPageId'] ) ? $attributes['checkoutPageId'] : 0 );
+	}
 }
