b4efd8ad1fd46bd072098c768e99a04e6ff5ae7d
Rodel <rodel.calasagsag@automattic.com>
2022-08-01 17:49:37 +0800

    Add temporary config file for testing

---
+++ plugins/woocommerce/e2e/daily.playwright.config.js
@@ -0,0 +1,43 @@
+const { devices } = require( '@playwright/test' );
+const { CI, BASE_URL, DEFAULT_TIMEOUT_OVERRIDE } = process.env;
+
+const config = {
+	timeout: DEFAULT_TIMEOUT_OVERRIDE
+		? Number( DEFAULT_TIMEOUT_OVERRIDE )
+		: 90 * 1000,
+	expect: { timeout: 20 * 1000 },
+	outputDir: './report',
+	globalSetup: require.resolve( './global-setup' ),
+	globalTeardown: require.resolve( './global-teardown' ),
+	testDir: 'tests',
+	retries: CI ? 4 : 2,
+	workers: 4,
+	reporter: [
+		[ 'list' ],
+		[
+			'html',
+			{
+				outputFolder: 'output',
+				open: CI ? 'never' : 'always',
+			},
+		],
+		[ 'allure-playwright', { outputFolder: 'e2e/allure-results' } ],
+		[ 'json', { outputFile: 'e2e/test-results.json' } ],
+	],
+	use: {
+		screenshot: 'only-on-failure',
+		video: 'on-first-retry',
+		trace: 'retain-on-failure',
+		viewport: { width: 1280, height: 720 },
+		baseURL: BASE_URL ?? 'http://localhost:8086',
+		stateDir: 'e2e/storage/',
+	},
+	projects: [
+		{
+			name: 'Chrome',
+			use: { ...devices[ 'Desktop Chrome' ] },
+		},
+	],
+};
+
+module.exports = config;
