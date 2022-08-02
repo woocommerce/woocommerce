c2d93d5a566d57cb6019e021ded3a0d73289230e
Daniel W. Robert <danielwrobert@users.noreply.github.com>
2022-08-02 10:28:52 -0400

    Add option to hide/show price in Mini Cart block  (https://github.com/woocommerce/woocommerce-blocks/pull/6796)
    * Add toggle control to sidebar in editor.
    
    Adds a visibility toggle in the Mini Cart block's sidebar editor
    settings for the block price.
    
    * Update name of hasVisiblePrice attrubite.
    
    Adjusts the name of the hasVisiblePrice attribute to hasHiddenPrice.
    This seems a bit easier to reason about, where we can set the default
    state of the visibility toggle to 'off'.
    
    * Add conditional price rendering to server side.
    
    Since this is a dynamic block, need to add the condidional attribute
    state to the server-side render callback.
    
    * Add price visibility setting to block frontend.
    
    We need to prevent pricec from rendering when hidden setting is active
    after the JS kicks in on the frontend (i.e., when the Mini Cart is
    interacted with by the user).
    
    * Fix logic error in frontend rendering conditional.
    
    * Add unit tests for both show/hide price states.

--- plugins/woocommerce-blocks/assets/js/blocks/mini-cart/component-frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/mini-cart/component-frontend.tsx
@@ -42,6 +42,7 @@ const renderMiniCartFrontend = () => {
 				colorClassNames,
 				style: el.dataset.style ? JSON.parse( el.dataset.style ) : {},
 				addToCartBehaviour: el.dataset.addToCartBehaviour || 'none',
+				hasHiddenPrice: el.dataset.hasHiddenPrice,
 				contents:
 					el.querySelector( '.wc-block-mini-cart__template-part' )
 						?.innerHTML ?? '',
