38625ee3cdf7e3b118d7020ea32585812b42def7
Sam Seay <samueljseay@gmail.com>
2022-08-02 15:10:35 +1200

    Remove run-turbo

--- tools/run-turbo/bin/run-turbo-pkg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-node ./node_modules/run-turbo/bin/process-args.js "${@:1}" "--turbo-filter=$npm_package_name"
--- tools/run-turbo/bin/run-turbo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-node ./node_modules/run-turbo/bin/process-args.js "${@:1}"
