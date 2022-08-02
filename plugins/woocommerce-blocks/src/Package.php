c5577dbc24b7ab8ab75ac5b189a53b0db48525e2
Albert Juhé Lluveras <contact@albertjuhe.com>
2022-08-02 17:24:34 +0200

    Update version to 8.3.0-dev

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -106,7 +106,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '8.2.0';
+					$version = '8.3.0-dev';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
