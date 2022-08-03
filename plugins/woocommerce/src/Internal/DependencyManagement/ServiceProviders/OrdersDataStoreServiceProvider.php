ae58bff28be6b0c1c843b813fe39ba160104e7a6
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2022-08-03 16:46:34 +0530

    Share concrete instance of cot controller to share feature value.

--- plugins/woocommerce/src/Internal/DependencyManagement/ServiceProviders/OrdersDataStoreServiceProvider.php
+++ plugins/woocommerce/src/Internal/DependencyManagement/ServiceProviders/OrdersDataStoreServiceProvider.php
@@ -6,6 +6,7 @@
 namespace Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders;
 
 use Automattic\Jetpack\Constants;
+use Automattic\WooCommerce\Admin\Overrides\Order;
 use Automattic\WooCommerce\DataBase\Migrations\CustomOrderTable\CLIRunner;
 use Automattic\WooCommerce\Database\Migrations\CustomOrderTable\PostsToOrdersMigrationController;
 use Automattic\WooCommerce\Internal\BatchProcessing\BatchProcessingController;
@@ -42,8 +43,16 @@ class OrdersDataStoreServiceProvider extends AbstractServiceProvider {
 
 		$this->share( OrdersTableDataStore::class )->addArguments( array( OrdersTableDataStoreMeta::class, DatabaseUtil::class ) );
 		$this->share( DataSynchronizer::class )->addArguments( array( OrdersTableDataStore::class, DatabaseUtil::class, PostsToOrdersMigrationController::class ) );
-		$this->share( CustomOrdersTableController::class )->addArguments( array( OrdersTableDataStore::class, DataSynchronizer::class, BatchProcessingController::class ) );
 		$this->share( OrdersTableDataStore::class );
+
+		$custom_orders_table_controller = new CustomOrdersTableController();
+		$custom_orders_table_controller->init(
+			wc_get_container()->get( OrdersTableDataStore::class ),
+			wc_get_container()->get( DataSynchronizer::class ),
+			wc_get_container()->get( BatchProcessingController::class )
+		);
+		$this->share( CustomOrdersTableController::class, $custom_orders_table_controller );
+
 		if ( Constants::is_defined( 'WP_CLI' ) && WP_CLI ) {
 			$this->share( CLIRunner::class )->addArguments( array( CustomOrdersTableController::class, DataSynchronizer::class, PostsToOrdersMigrationController::class ) );
 		}
