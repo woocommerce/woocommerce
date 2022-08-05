53b89d0ef196a3341aae2b4a694f47d27413d472
nigeljamesstevenson <nigeljamesstevenson@gmail.com>
2022-08-05 09:40:50 +0100

    group shipping zone tests to avoid concurrency issues

--- plugins/woocommerce/e2e/tests/merchant/create-shipping-zones.spec.js
+++ plugins/woocommerce/e2e/tests/merchant/create-shipping-zones.spec.js
@@ -2,6 +2,7 @@ const { test, expect } = require( '@playwright/test' );
 const wcApi = require( '@woocommerce/woocommerce-rest-api' ).default;
 
 const maynePostal = 'V0N 2J0';
+const shippingZoneRegion = 'United States Zone';
 const shippingZoneNameFlatRate = 'Canada with Flat rate';
 const shippingZoneNameFreeShip = 'BC with Free shipping';
 const shippingZoneNameLocalPickup = 'Mayne Island with Local pickup';
@@ -20,6 +21,7 @@ test.describe( 'WooCommerce Shipping Settings - Add new shipping zone', () => {
 			for ( let i = 0; i < response.data.length; i++ ) {
 				if (
 					[
+						shippingZoneRegion,
 						shippingZoneNameFlatRate,
 						shippingZoneNameFreeShip,
 						shippingZoneNameLocalPickup,
@@ -31,6 +33,64 @@ test.describe( 'WooCommerce Shipping Settings - Add new shipping zone', () => {
 				}
 			}
 		} );
+	} );
+
+	test( 'add shipping zone with region and then delete the region', async ( {
+		page,
+	} ) => {
+		await page.goto( 'wp-admin/admin.php?page=wc-settings&tab=shipping' );
+		if ( await page.isVisible( `text=${ shippingZoneRegion }` ) ) {
+			// this shipping zone already exists, don't create it
+		} else {
+			await page.goto(
+				'wp-admin/admin.php?page=wc-settings&tab=shipping&zone_id=new'
+			);
+			await page.fill( '#zone_name', shippingZoneRegion );
+
+			await page.click( '.select2-search__field' );
+			await page.type( '.select2-search__field', 'United States' );
+			await page.click(
+				'.select2-results__option.select2-results__option--highlighted'
+			);
+
+			await page.click( '#submit' );
+
+			await page.goto(
+				'wp-admin/admin.php?page=wc-settings&tab=shipping'
+			);
+			await page.reload(); // Playwright runs so fast, the location shows up as "Everywhere" at first
+		}
+		await expect( page.locator( '.wc-shipping-zones' ) ).toHaveText(
+			/United States Zone.*/
+		);
+
+		//delete created shipping zone region after confirmation it exists
+		await page.goto( 'wp-admin/admin.php?page=wc-settings&tab=shipping' );
+
+		await page.locator( 'a:has-text("United States") >> nth=0' ).click();
+
+		//delete
+		await page.locator( 'text=×' ).click();
+		//save changes
+		await page.click( '#submit' );
+
+		await page.goto( 'wp-admin/admin.php?page=wc-settings&tab=shipping' );
+
+		//prove that the Region has been removed (Everywhere will display)
+		await expect( page.locator( '.wc-shipping-zones' ) ).toHaveText(
+			/Everywhere.*/
+		);
+
+		//hover over option to reveal Delete
+		await page.locator( 'a:has-text("United States") >> nth=0' ).hover();
+
+		//set up dialog handler
+		page.on( 'dialog', ( dialog ) => dialog.accept() );
+		await page.locator( 'a:has-text("Delete") >> nth=0' ).click();
+
+		await expect( page.locator( '.wc-shipping-zones' ) ).not.toHaveText(
+			/United States.*/
+		);
 	} );
 
 	test( 'add shipping zone for Mayne Island with free Local pickup', async ( {
