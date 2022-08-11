ee70fd7136deb007cef47c60a470a1cd0775285b
Jacob Sewell <jacob.sewell@automattic.com>
2022-08-11 13:58:11 -0500

    Improve SplitDropdown test: remove snapshot, add toggle click.

--- packages/js/components/src/split-dropdown/test/index.tsx
+++ packages/js/components/src/split-dropdown/test/index.tsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { render } from '@testing-library/react';
+import { createEvent, fireEvent, render } from '@testing-library/react';
 import { createElement } from '@wordpress/element';
 import { Button } from '@wordpress/components';
 
@@ -12,7 +12,7 @@ import { SplitDropdown } from '..';
 
 describe( 'SplitDropdown', () => {
 	test( 'it renders correctly collapsed', () => {
-		const { container, getByText, queryByText } = render(
+		const { getByText, queryByText, queryAllByRole } = render(
 			<SplitDropdown>
 				<Button>Default Action</Button>
 				<Button className="test-collapsed-button">
@@ -23,26 +23,27 @@ describe( 'SplitDropdown', () => {
 				</Button>
 			</SplitDropdown>
 		);
-		expect( container ).toMatchSnapshot();
-
-		// should have correct content
+		// should have a default action button
 		expect( getByText( 'Default Action' ) ).toBeInTheDocument();
-		expect( queryByText( 'Secondary Action' ) ).not.toBeInTheDocument();
 
-		// should have correct classes
-		const presentClassNames = [
-			'woocommerce-split-dropdown__container',
-			'woocommerce-split-dropdown__main-button',
-			'woocommerce-split-dropdown__toggle',
-		];
-		presentClassNames.map( ( className ) =>
-			expect(
-				container.getElementsByClassName( className )
-			).toHaveLength( 1 )
-		);
+		// should not yet have any of the dropdown actions
+		expect( queryByText( 'Secondary Action' ) ).not.toBeInTheDocument();
+		expect( queryByText( 'Tertiary Action' ) ).not.toBeInTheDocument();
+
+		// should have a toggle button
+		const toggleButton = queryAllByRole( 'button' ).pop();
+		expect( toggleButton ).toBeInTheDocument();
+
+		// should display other actions after toggle is clicked.
+		if ( toggleButton !== undefined ) {
+			const toggleClick = createEvent.click( toggleButton, {} );
+			fireEvent( toggleButton, toggleClick );
+		}
+		expect( getByText( 'Secondary Action' ) ).toBeInTheDocument();
+		expect( getByText( 'Tertiary Action' ) ).toBeInTheDocument();
 	} );
 	test( 'it does not render the toggle where there are no items for the dropdown menu', () => {
-		const { container, getByText, queryByText } = render(
+		const { container } = render(
 			<SplitDropdown>
 				<Button>Only Action</Button>
 			</SplitDropdown>
