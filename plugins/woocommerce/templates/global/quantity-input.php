02272cb4f053aa09f2e82fa6ac2c642950eb27d2
Kirtan Gajjar <kirtangajjar95@gmail.com>
2022-08-11 16:19:11 +0530

    Fix qty input not visible

--- plugins/woocommerce/templates/global/quantity-input.php
+++ plugins/woocommerce/templates/global/quantity-input.php
@@ -23,29 +23,43 @@ if ( $max_value && $min_value === $max_value ) {
 		<input type="hidden" id="<?php echo esc_attr( $input_id ); ?>" class="qty" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $min_value ); ?>" />
 	</div>
 	<?php
-} else {
-	/* translators: %s: Quantity. */
-	$label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 'woocommerce' ), wp_strip_all_tags( $args['product_name'] ) ) : esc_html__( 'Quantity', 'woocommerce' );
-	?>
-	<div class="quantity">
-		<?php do_action( 'woocommerce_before_quantity_input_field' ); ?>
-		<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>
-		<input
-			type="number"
-			id="<?php echo esc_attr( $input_id ); ?>"
-			class="<?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
-			step="<?php echo esc_attr( $step ); ?>"
-			min="<?php echo esc_attr( $min_value ); ?>"
-			max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
-			name="<?php echo esc_attr( $input_name ); ?>"
-			value="<?php echo esc_attr( $input_value ); ?>"
-			title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ); ?>"
-			size="4"
-			placeholder="<?php echo esc_attr( $placeholder ); ?>"
-			inputmode="<?php echo esc_attr( $inputmode ); ?>"
-			autocomplete="<?php echo esc_attr( isset( $autocomplete ) ? $autocomplete : 'on' ); ?>"
-		/>
-		<?php do_action( 'woocommerce_after_quantity_input_field' ); ?>
-	</div>
-	<?php
 }
+/* translators: %s: Quantity. */
+$label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 'woocommerce' ), wp_strip_all_tags( $args['product_name'] ) ) : esc_html__( 'Quantity', 'woocommerce' );
+?>
+<div class="quantity">
+	<?php
+	/**
+	 * Hook to output something before quantity input field
+	 *
+	 * @since 3.6.0
+	 */
+	do_action( 'woocommerce_before_quantity_input_field' );
+	?>
+	<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>
+	<input
+		type="number"
+		<?php disabled( $max_value && $min_value === $max_value ); ?>
+		id="<?php echo esc_attr( $input_id ); ?>"
+		class="<?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
+		step="<?php echo esc_attr( $step ); ?>"
+		min="<?php echo esc_attr( $min_value ); ?>"
+		max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
+		name="<?php echo esc_attr( $input_name ); ?>"
+		value="<?php echo esc_attr( $input_value ); ?>"
+		title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ); ?>"
+		size="4"
+		placeholder="<?php echo esc_attr( $placeholder ); ?>"
+		inputmode="<?php echo esc_attr( $inputmode ); ?>"
+		autocomplete="<?php echo esc_attr( isset( $autocomplete ) ? $autocomplete : 'on' ); ?>"
+	/>
+	<?php
+	/**
+	 * Hook to output something after quantity input field
+	 *
+	 * @since 3.6.0
+	 */
+	do_action( 'woocommerce_after_quantity_input_field' );
+	?>
+</div>
+<?php
