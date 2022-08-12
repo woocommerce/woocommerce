4693e251f76e02cee3b602a837f13794635a41e3
Fernando <fernando.marichal@automattic.com>
2022-08-12 10:47:54 -0300

    Add product name and checkboxes for Product details (#34214)
    * Add product details
    
    # Conflicts:
    #	plugins/woocommerce-admin/client/products/sections/product-details-section.tsx
    
    # Conflicts:
    #	plugins/woocommerce-admin/client/products/sections/product-details-section.tsx
    
    * Rename folder
    
    * Fix help icon
    
    * Fix form
    
    * Removed useless `useState`
    
    * Add `getCheckboxProps`
    
    * Add params to getCheckboxProps
    
    * Add recordEvent
    
    * Rename event
    
    * Add changelog
    
    * Fix texts
    
    * Remove Fragment
    
    * Fix useState initial state
    
    * Convert EnrichedLabel to TS
    
    * Fix unknown value prop
    
    * Rename event
    
    * Move EnrichedLabel
    
    * Fix js warning
    
    * Add storybook
    
    * Add readme
    
    * Add components changelog
    
    Co-authored-by: Fernando Marichal <contacto@fernandomarichal.com>

--- plugins/woocommerce-admin/client/products/sections/product-details-section.tsx
+++ plugins/woocommerce-admin/client/products/sections/product-details-section.tsx
@@ -1,18 +1,58 @@
 /**
  * External dependencies
  */
-import { TextControl } from '@wordpress/components';
+import { CheckboxControl, TextControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { useFormContext } from '@woocommerce/components';
+import { EnrichedLabel, useFormContext } from '@woocommerce/components';
 import { Product } from '@woocommerce/data';
+import classnames from 'classnames';
+import { recordEvent } from '@woocommerce/tracks';
 
 /**
  * Internal dependencies
  */
 import { ProductSectionLayout } from '../layout/product-section-layout';
 
+const PRODUCT_DETAILS_SLUG = 'product-details';
+
 export const ProductDetailsSection: React.FC = () => {
-	const formContext = useFormContext< Product >();
+	const { getInputProps } = useFormContext< Product >();
+	const getCheckboxProps = ( item: string ) => {
+		const { checked, className, onChange, onBlur } =
+			getInputProps< boolean >( item );
+		return {
+			checked,
+			className: classnames(
+				'woocommerce-add-product__checkbox',
+				className
+			),
+			onChange: ( isChecked: boolean ) => {
+				recordEvent( `add_product_checkbox_${ item }`, {
+					checked: isChecked,
+				} );
+				return onChange( isChecked );
+			},
+			onBlur,
+		};
+	};
+	const getTextControlProps = ( item: string ) => {
+		const {
+			className,
+			onBlur,
+			onChange,
+			value = '',
+		} = getInputProps< string >( item );
+		return {
+			value,
+			className: classnames(
+				'woocommerce-add-product__checkbox',
+				className
+			),
+			onChange,
+			onBlur,
+		};
+	};
+
 	return (
 		<ProductSectionLayout
 			title={ __( 'Product details', 'woocommerce' ) }
@@ -23,8 +63,27 @@ export const ProductDetailsSection: React.FC = () => {
 		>
 			<TextControl
 				label={ __( 'Name', 'woocommerce' ) }
-				name="name"
-				{ ...formContext.getInputProps< string >( 'name' ) }
+				name={ `${ PRODUCT_DETAILS_SLUG }-name` }
+				placeholder={ __( 'e.g. 12 oz Coffee Mug', 'woocommerce' ) }
+				{ ...getTextControlProps( 'name' ) }
+			/>
+			<CheckboxControl
+				label={
+					<EnrichedLabel
+						label={ __( 'Feature this product', 'woocommerce' ) }
+						helpDescription={ __(
+							'Include this product in a featured section on your website with a widget or shortcode.',
+							'woocommerce'
+						) }
+						moreUrl="https://woocommerce.com/document/woocommerce-shortcodes/#products"
+						tooltipLinkCallback={ () =>
+							recordEvent( 'add_product_learn_more', {
+								category: PRODUCT_DETAILS_SLUG,
+							} )
+						}
+					/>
+				}
+				{ ...getCheckboxProps( 'featured' ) }
 			/>
 		</ProductSectionLayout>
 	);
