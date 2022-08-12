30a713273661b44e0f99732cd6e5bdca956efcbd
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-08-13 02:45:55 +0800

    Display plugins in DiscoverTools component.

--- plugins/woocommerce-admin/client/marketing/overview-multichannel/DiscoverTools.tsx
+++ plugins/woocommerce-admin/client/marketing/overview-multichannel/DiscoverTools.tsx
@@ -1,20 +1,135 @@
 /**
  * External dependencies
  */
+import { Fragment } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
+import { TabPanel, Button } from '@wordpress/components';
+import { recordEvent } from '@woocommerce/tracks';
+
 /**
  * Internal dependencies
  */
-import { CollapsibleCard } from '../components';
+import {
+	CollapsibleCard,
+	CardDivider,
+	ProductIcon,
+	PluginCardBody,
+} from '~/marketing/components';
+import { STORE_KEY } from '~/marketing/data/constants';
+import { getInAppPurchaseUrl } from '~/lib/in-app-purchase';
+
+export type Plugin = {
+	title: string;
+	description: string;
+	url: string;
+	icon: string;
+	product: string;
+	plugin: string;
+	categories: Array< string >;
+	subcategories: Array< string >;
+	tags: Array< string >;
+};
+
+const category = 'marketing';
+const tabs = Object.freeze( [
+	{
+		name: 'email',
+		title: __( 'Email', 'woocommerce' ),
+	},
+	{
+		name: 'automations',
+		title: __( 'Automations', 'woocommerce' ),
+	},
+	{
+		name: 'sales-channels',
+		title: __( 'Sales channels', 'woocommerce' ),
+	},
+	{
+		name: 'crm',
+		title: __( 'CRM', 'woocommerce' ),
+	},
+] );
+
+type SelectResult = {
+	isLoading: boolean;
+	plugins: Array< Plugin >;
+};
 
 export const DiscoverTools = () => {
+	const { isLoading, plugins } = useSelect< SelectResult >(
+		( select ) => {
+			const { getRecommendedPlugins, isResolving } = select( STORE_KEY );
+
+			return {
+				isLoading: isResolving( 'getRecommendedPlugins', [ category ] ),
+				plugins: getRecommendedPlugins( category ),
+			};
+		},
+		[ category ]
+	);
+
+	// TODO: handle loading state.
+
 	return (
 		<CollapsibleCard
 			initialCollapsed
 			className="woocommerce-marketing-discover-tools-card"
 			header={ __( 'Discover more marketing tools', 'woocommerce' ) }
 		>
-			<div>TODO:</div>
+			<TabPanel tabs={ tabs }>
+				{ ( tab ) => {
+					const filteredPlugins = plugins.filter( ( el: Plugin ) =>
+						el.subcategories?.includes( tab.name )
+					);
+
+					// TODO: handle filteredPlugins.length === 0.
+
+					return (
+						<>
+							<CardDivider />
+							{ filteredPlugins.map( ( el, idx ) => {
+								return (
+									<Fragment key={ el.product }>
+										<PluginCardBody
+											icon={
+												<ProductIcon
+													product={ el.product }
+												/>
+											}
+											name={ el.title }
+											description={ el.description }
+											button={
+												<Button
+													variant="secondary"
+													href={ getInAppPurchaseUrl(
+														el.url
+													) }
+													onClick={ () => {
+														recordEvent(
+															'marketing_recommended_extension',
+															{ name: el.title }
+														);
+													} }
+												>
+													{ __(
+														'Get started',
+														'woocommerce'
+													) }
+												</Button>
+											}
+										/>
+										{ idx !==
+											filteredPlugins.length - 1 && (
+											<CardDivider />
+										) }
+									</Fragment>
+								);
+							} ) }
+						</>
+					);
+				} }
+			</TabPanel>
 		</CollapsibleCard>
 	);
 };
