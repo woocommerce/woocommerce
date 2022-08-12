f20498d5ca90007473ffb45e426299301d7a5caf
Joshua Flowers <joshuatf@gmail.com>
2022-08-12 10:49:42 -0700

    Rebase and use variant instead of isSecondary for Button

--- packages/js/components/src/media-uploader/media-uploader.tsx
+++ packages/js/components/src/media-uploader/media-uploader.tsx
@@ -12,7 +12,7 @@ import {
 	UploadMediaErrorCode,
 } from '@wordpress/media-utils';
 
-const DEFAULT_ALLOWED_MEDIA_TYPES = [ 'image' ];
+const DEFAULT_ALLOWED_MEDIA_TYPES = ['image'];
 
 type MediaUploaderProps = {
 	allowedMediaTypes?: string[];
@@ -21,58 +21,58 @@ type MediaUploaderProps = {
 	icon?: JSX.Element;
 	label?: string;
 	maxUploadFileSize?: number;
-	MediaUploadComponent?: < T extends boolean = false >(
-		props: MediaUpload.Props< T >
+	MediaUploadComponent?: <T extends boolean = false>(
+		props: MediaUpload.Props<T>
 	) => JSX.Element;
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	onSelect?: ( value: { id: number } & { [ k: string ]: any } ) => void;
-	onError?: ( error: {
+	onSelect?: (value: { id: number } & { [k: string]: any }) => void;
+	onError?: (error: {
 		code: UploadMediaErrorCode;
 		message: string;
 		file: File;
-	} ) => void;
-	onUpload?: ( files: MediaItem[] ) => void;
-	uploadMedia?: ( options: UploadMediaOptions ) => Promise< void >;
+	}) => void;
+	onUpload?: (files: MediaItem[]) => void;
+	uploadMedia?: (options: UploadMediaOptions) => Promise<void>;
 };
 
-export const MediaUploader = ( {
+export const MediaUploader = ({
 	allowedMediaTypes = DEFAULT_ALLOWED_MEDIA_TYPES,
-	buttonText = __( 'Choose images', 'woocommerce' ),
+	buttonText = __('Choose images', 'woocommerce'),
 	hasDropZone = true,
-	label = __( 'Drag images here or click to upload', 'woocommerce' ),
+	label = __('Drag images here or click to upload', 'woocommerce'),
 	maxUploadFileSize = 10000000,
 	MediaUploadComponent = MediaUpload,
 	onError = () => null,
 	onUpload = () => null,
 	onSelect = () => null,
 	uploadMedia = wpUploadMedia,
-}: MediaUploaderProps ) => {
+}: MediaUploaderProps) => {
 	return (
 		<div className="woocommerce-media-uploader">
-			<div className="woocommerce-media-uploader__label">{ label }</div>
+			<div className="woocommerce-media-uploader__label">{label}</div>
 
 			<MediaUploadComponent
-				onSelect={ onSelect }
-				allowedTypes={ allowedMediaTypes }
-				render={ ( { open } ) => (
-					<Button isSecondary onClick={ open }>
-						{ buttonText }
+				onSelect={onSelect}
+				allowedTypes={allowedMediaTypes}
+				render={({ open }) => (
+					<Button variant="secondary" onClick={open}>
+						{buttonText}
 					</Button>
-				) }
+				)}
 			/>
 
-			{ hasDropZone && (
+			{hasDropZone && (
 				<DropZone
-					onFilesDrop={ ( files ) =>
-						uploadMedia( {
+					onFilesDrop={(files) =>
+						uploadMedia({
 							filesList: files,
 							onError,
 							onFileChange: onUpload,
 							maxUploadFileSize,
-						} )
+						})
 					}
 				/>
-			) }
+			)}
 		</div>
 	);
 };
