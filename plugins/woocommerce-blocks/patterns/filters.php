84d0344b9efffbd344ccdfb6909c635279037c34
Alba Rincón <albarin@users.noreply.github.com>
2022-08-12 13:09:37 +0200

    Register patterns under `patterns` folder and add filters pattern (https://github.com/woocommerce/woocommerce-blocks/pull/6861)
    * POC about registering patterns
    
    * POC load patterns from `/patterns` folder
    
    * Change class name and remove unnecessary comment
    
    * Add all four blocks to the filters pattern
    
    * Add all 4 blocks to the block type to allow for transformations
    
    * Add internal
    
    * Improve filters name
    
    * Extract regex to constants
    
    * Add class description
    
    * Rename and move class
    
    * Fix wrong imports
    
    * Address feedback
    
    * Early return from condition
    
    * Fix wrong block type

---
+++ plugins/woocommerce-blocks/patterns/filters.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: WooCommerce Product Filters
+ * Slug: woocommerce-blocks/product-filters
+ * Categories: WooCommerce
+ * Block Types: woocommerce/active-filters, woocommerce/price-filter, woocommerce/attribute-filter, woocommerce/stock-filter
+ */
+?>
+
+<!-- wp:woocommerce/active-filters -->
+<div class="wp-block-woocommerce-active-filters is-loading" data-display-style="list" data-heading="Active filters" data-heading-level="3"><span aria-hidden="true" class="wc-block-active-product-filters__placeholder"></span></div>
+<!-- /wp:woocommerce/active-filters -->
+
+<!-- wp:woocommerce/price-filter -->
+<div class="wp-block-woocommerce-price-filter is-loading" data-showinputfields="true" data-showfilterbutton="false" data-heading="Filter by price" data-heading-level="3"><span aria-hidden="true" class="wc-block-product-categories__placeholder"></span></div>
+<!-- /wp:woocommerce/price-filter -->
+
+<!-- wp:woocommerce/attribute-filter -->
+<div class="wp-block-woocommerce-attribute-filter is-loading" data-attribute-id="0" data-show-counts="true" data-query-type="or" data-heading="Filter by attribute" data-heading-level="3"><span aria-hidden="true" class="wc-block-product-attribute-filter__placeholder"></span></div>
+<!-- /wp:woocommerce/attribute-filter -->
+
+<!-- wp:woocommerce/stock-filter -->
+<div class="wp-block-woocommerce-stock-filter is-loading" data-show-counts="true" data-heading="Filter by stock status" data-heading-level="3"><span aria-hidden="true" class="wc-block-product-stock-filter__placeholder"></span></div>
+<!-- /wp:woocommerce/stock-filter -->
