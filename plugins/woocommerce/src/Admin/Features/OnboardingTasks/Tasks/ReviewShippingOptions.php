76a7d7797ab5f7ff8d93848cb1bf11589374412c
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-08-12 08:15:08 +0800

    Fix Review Shipping Options Task title

--- plugins/woocommerce/src/Admin/Features/OnboardingTasks/Tasks/ReviewShippingOptions.php
+++ plugins/woocommerce/src/Admin/Features/OnboardingTasks/Tasks/ReviewShippingOptions.php
@@ -23,7 +23,7 @@ class ReviewShippingOptions extends Task {
 	 * @return string
 	 */
 	public function get_title() {
-		return __( 'Review Shipping Options', 'woocommerce' );
+		return __( 'Review shipping options', 'woocommerce' );
 	}
 
 	/**
