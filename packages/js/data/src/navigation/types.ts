02bdfa111570b980f3f40b2427278e1aa1982dfe
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2022-08-16 19:14:12 +0800

    Add a doc comment to MenuItem type

--- packages/js/data/src/navigation/types.ts
+++ packages/js/data/src/navigation/types.ts
@@ -3,6 +3,7 @@
  */
 import { getPersistedQuery } from '@woocommerce/navigation';
 
+// https://github.com/woocommerce/woocommerce/blob/ecec9eaa76cb7b2e36f79175837b71f4f64996b1/plugins/woocommerce/src/Admin/Features/Navigation/Menu.php
 export type MenuItem = {
 	id: string;
 	title: string;
