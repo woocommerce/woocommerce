0b7a195b660cea33890be2671c7724d3137011f8
Joshua Flowers <joshuatf@gmail.com>
2022-08-17 16:56:21 -0700

    Add placeholders for dropping items

--- packages/js/components/src/image-gallery/image-gallery.tsx
+++ packages/js/components/src/image-gallery/image-gallery.tsx
@@ -2,30 +2,16 @@
  * External dependencies
  */
 import { Children, createElement, Fragment } from '@wordpress/element';
-import { DragEventHandler } from 'react';
 
 /**
  * Internal dependencies
  */
+import { Handle } from '../list-item';
 import { Sortable } from '../sortable';
 
 export type ImageGalleryProps = {
 	children: JSX.Element | JSX.Element[];
 } & React.HTMLAttributes< HTMLDivElement >;
-
-type CustomListItemProps = {
-	children: React.ReactNode;
-	onDraggableEnd?: DragEventHandler< Element >;
-	onDraggableStart?: DragEventHandler< Element >;
-};
-
-const CustomListItem = ( {
-	children,
-	onDraggableStart,
-	onDraggableEnd,
-}: CustomListItemProps ) => {
-	return <>{ children }</>;
-};
 
 export const ImageGallery: React.FC< ImageGalleryProps > = ( {
 	children,
@@ -34,7 +20,7 @@ export const ImageGallery: React.FC< ImageGalleryProps > = ( {
 		<div className="woocommerce-image-gallery">
 			<Sortable>
 				{ Children.map( children, ( child ) => (
-					<CustomListItem>{ child }</CustomListItem>
+					<Handle>{ child }</Handle>
 				) ) }
 			</Sortable>
 		</div>
--- packages/js/components/src/sortable/sortable-item.tsx
+++ packages/js/components/src/sortable/sortable-item.tsx
@@ -3,35 +3,48 @@
  */
 import { DragEvent, DragEventHandler } from 'react';
 import classnames from 'classnames';
-import { cloneElement, createElement, Fragment } from '@wordpress/element';
+import {
+	cloneElement,
+	createElement,
+	Fragment,
+	useCallback,
+	useState,
+} from '@wordpress/element';
 import { Draggable } from '@wordpress/components';
+import { throttle } from 'lodash';
 
 /**
  * Internal dependencies
  */
+import { isUpperHalf } from './utils';
 import { SortableChild } from './types';
 
 export type SortableItemProps = {
 	id: string | number;
+	index: number;
 	children: SortableChild;
 	isDragging?: boolean;
-	isDraggingOver?: boolean;
 	onDragStart?: DragEventHandler< HTMLDivElement >;
 	onDragEnd?: DragEventHandler< HTMLDivElement >;
 	onDragOver?: DragEventHandler< HTMLLIElement >;
-	style?: React.CSSProperties;
+	setDropIndex: ( index: number | null ) => void;
 };
+
+const THROTTLE_TIME = 30;
 
 export const SortableItem = ( {
 	id,
+	index,
 	children,
 	isDragging = false,
-	isDraggingOver = false,
 	onDragStart = () => null,
 	onDragEnd = () => null,
 	onDragOver = () => null,
-	style,
+	setDropIndex,
 }: SortableItemProps ) => {
+	const [ isDraggingOver, setIsDraggingOver ] = useState( false );
+	const [ isBefore, setIsBefore ] = useState( true );
+
 	const handleDragStart = ( event: DragEvent< HTMLDivElement > ) => {
 		onDragStart( event );
 	};
@@ -40,15 +53,43 @@ export const SortableItem = ( {
 		onDragEnd( event );
 	};
 
+	const handleDragOver = ( event: DragEvent< HTMLLIElement > ) => {
+		setIsDraggingOver( true );
+		onDragOver( event );
+
+		if ( isUpperHalf( event ) ) {
+			setDropIndex( index );
+			setIsBefore( true );
+			return;
+		}
+
+		setDropIndex( index );
+		setIsBefore( false );
+	};
+
+	const throttledHandleDragOver = useCallback(
+		throttle( handleDragOver, THROTTLE_TIME ),
+		[ index ]
+	);
+
+	const handleDragLeave = () => {
+		setTimeout( () => {
+			setDropIndex( null );
+			setIsDraggingOver( false );
+		}, THROTTLE_TIME );
+	};
+
 	return (
 		<li
 			className={ classnames( 'woocommerce-sortable__item', {
 				'is-dragging': isDragging,
 				'is-dragging-over': isDraggingOver,
+				'is-dragging-over-before': isDraggingOver && isBefore,
+				'is-dragging-over-after': isDraggingOver && ! isBefore,
 			} ) }
 			id={ `woocommerce-sortable__item-${ id }` }
-			onDragOver={ onDragOver }
-			style={ style }
+			onDragOver={ throttledHandleDragOver }
+			onDragLeave={ handleDragLeave }
 		>
 			<Draggable
 				elementId={ `woocommerce-sortable__item-${ id }` }
