043cfe5e3ba5d86f33852b6c9eff23233cd83ee0
Joshua Flowers <joshuatf@gmail.com>
2022-08-17 13:48:18 -0700

    Remove onboarding e2e changes

--- plugins/woocommerce/tests/e2e-pw/tests/activate-and-setup/complete-onboarding-wizard.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/activate-and-setup/complete-onboarding-wizard.spec.js
@@ -275,8 +275,6 @@ test.describe( 'Store owner can go through setup Task List', () => {
 		await page.goto(
 			'wp-admin/admin.php?page=wc-admin&path=/setup-wizard'
 		);
-		await page.fill( '#inspector-text-control-0', 'addr 1' );
-		await page.fill( '#inspector-text-control-1', 'addr 2' );
 		await page.click( '#woocommerce-select-control-0__control-input' );
 		await page.fill(
 			'#woocommerce-select-control-0__control-input',
