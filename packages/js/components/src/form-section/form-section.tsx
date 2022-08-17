887e349ec268c906281fe14cd8a2f15c9e6122ee
Joshua Flowers <joshuatf@gmail.com>
2022-08-17 14:19:13 -0700

    Allow JSX elements for title and description

--- packages/js/components/src/form-section/form-section.tsx
+++ packages/js/components/src/form-section/form-section.tsx
@@ -4,26 +4,24 @@
 import { createElement } from '@wordpress/element';
 
 type FormSectionProps = {
-	title: string;
-	description: string;
+	title: JSX.Element | string;
+	description: JSX.Element | string;
 };
 
-export const FormSection: React.FC< FormSectionProps > = ( {
+export const FormSection: React.FC<FormSectionProps> = ({
 	title,
 	description,
 	children,
-} ) => {
+}) => {
 	return (
 		<div className="woocommerce-form-section">
 			<div className="woocommerce-form-section__header">
-				<h3 className="woocommerce-form-section__title">{ title }</h3>
-				<p className="woocommerce-form-section__description">
-					{ description }
-				</p>
+				<h3 className="woocommerce-form-section__title">{title}</h3>
+				<div className="woocommerce-form-section__description">
+					{description}
+				</div>
 			</div>
-			<div className="woocommerce-form-section__content">
-				{ children }
-			</div>
+			<div className="woocommerce-form-section__content">{children}</div>
 		</div>
 	);
 };
--- packages/js/components/src/form-section/stories/index.tsx
+++ packages/js/components/src/form-section/stories/index.tsx
@@ -2,13 +2,14 @@
  * External dependencies
  */
 import React from 'react';
+import { Button, Card, CardBody, TextControl } from '@wordpress/components';
 import { createElement } from '@wordpress/element';
-import { Card, CardBody, TextControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import { FormSection } from '../';
+import Pill from '../../pill';
 
 export const Basic: React.FC = () => {
 	return (
@@ -20,12 +21,12 @@ export const Basic: React.FC = () => {
 				<CardBody>
 					<TextControl
 						label="My first field"
-						onChange={ () => {} }
+						onChange={() => {}}
 						value=""
 					/>
 					<TextControl
 						label="My second field"
-						onChange={ () => {} }
+						onChange={() => {}}
 						value=""
 					/>
 				</CardBody>
@@ -35,7 +36,40 @@ export const Basic: React.FC = () => {
 				<CardBody>
 					<TextControl
 						label="My third field"
-						onChange={ () => {} }
+						onChange={() => {}}
+						value=""
+					/>
+				</CardBody>
+			</Card>
+		</FormSection>
+	);
+};
+
+export const CustomElements: React.FC = () => {
+	return (
+		<FormSection
+			title={
+				<>
+					Custom elements <Pill>Cool</Pill>
+				</>
+			}
+			description={
+				<>
+					<p>Some text to describe what this section covers</p>
+					<Button variant="link">Read more</Button>
+				</>
+			}
+		>
+			<Card>
+				<CardBody>
+					<TextControl
+						label="My first field"
+						onChange={() => {}}
+						value=""
+					/>
+					<TextControl
+						label="My second field"
+						onChange={() => {}}
 						value=""
 					/>
 				</CardBody>
