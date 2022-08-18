32de8bee6f53bbd5e0d2d628bb3cd41b4321fc6c
Joshua T Flowers <joshuatf@gmail.com>
2022-08-18 10:36:20 -0700

    Add SearchControl component (#34159)
    * Add initial SearchControl component
    
    * Add async example
    
    * Reorganize getItemPropsType type
    
    * Create separate MenuItem component
    
    * Update items to use value/label pairs
    
    * Add fuzzy matching example
    
    * Use MenuItem component in example
    
    * Add callback method example and onSelect prop
    
    * Add custom render example
    
    * Add styling
    
    * Simplify Menu component
    
    * Add changelog entry
    
    * Update SelectControl to DeprecatedSelectControl
    
    * Rename SearchControl to SelectControl
    
    * Add readme
    
    * Add placeholder prop
    
    * Add icon to combox box
    
    * Rename deprecated SelectControl classes
    
    * Add changelog entries
    
    * Pass menu props to menu to fix ref issues
    
    * Update lock file
    
    * Rebase and update lock file
    
    * Fix up IDs in e2e tests
    
    * Make list structure more semantic
    
    * Fix update conflict with pnpm-lock
    
    * Move new SelectControl to experimental
    
    * Change experimental class name to avoid style conflicts
    
    * Fix up latest lock file from rebase
    
    * Remove onboarding e2e changes
    
    * Update changelogs
    
    * Update lock file again
    
    * Update pnpm-lock and fix lint error
    
    Co-authored-by: Lourens Schep <lourensschep@gmail.com>

--- packages/js/components/src/index.ts
+++ packages/js/components/src/index.ts
@@ -37,6 +37,8 @@ export { default as SearchListItem } from './search-list-control/item';
 export { default as SectionHeader } from './section-header';
 export { default as SegmentedSelection } from './segmented-selection';
 export { default as SelectControl } from './select-control';
+export { SelectControl as __experimentalSelectControl } from './experimental-select-control';
+export { MenuItem as __experimentalSelectControlMenuItem } from './experimental-select-control/menu-item';
 export { default as ScrollTo } from './scroll-to';
 export { SortableList } from './sortable-list';
 export { default as Spinner } from './spinner';
