4e339c399cbbcd98d1a7e5cd76ca7a37baa51a24
Jonathan Sadowski <sadowski@automattic.com>
2022-08-19 11:06:25 -0500

    Don't execute suggested command to fix mismatches automatically.

--- bin/pre-push.sh
+++ bin/pre-push.sh
@@ -29,6 +29,6 @@ pnpm exec syncpack -- list-mismatches
 
 if [ $? -ne 0 ]; then
 	echo "You must sync the dependencies listed above before you can push this branch."
-	echo "This can usually be accomplished automatically by running `pnpm exec syncpack -- fix-mismatches`."
+	echo "This can usually be accomplished automatically by running \`pnpm exec syncpack -- fix-mismatches\`."
 	exit 1
 fi
