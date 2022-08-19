9758a1d4d901ffbb518e19c3e0fce40c7f98492a
Jonathan Sadowski <sadowski@automattic.com>
2022-08-19 11:02:54 -0500

    Fix typo in pre-push script

--- bin/pre-push.sh
+++ bin/pre-push.sh
@@ -27,7 +27,7 @@ php tools/monorepo/check-changelogger-use.php $PROTECTED_BRANCH $CURRENT_BRANCH
 
 pnpm exec syncpack -- list-mismatches
 
-if [ $? ne 0 ]; then
+if [ $? -ne 0 ]; then
 	echo "You must sync the dependencies listed above before you can push this branch."
 	echo "This can usually be accomplished automatically by running `pnpm exec syncpack -- fix-mismatches`."
 	exit 1
