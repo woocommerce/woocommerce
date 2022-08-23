d6b5d6d3095fdf43c8004a4dd376bdfe310e19ba
Joshua Flowers <joshuatf@gmail.com>
2022-08-23 11:50:49 -0700

    Add image gallery styles

--- packages/js/components/src/image-gallery/stories/index.tsx
+++ packages/js/components/src/image-gallery/stories/index.tsx
@@ -21,12 +21,75 @@ export const Basic: React.FC = () => {
 				src="https://picsum.photos/id/208/200/200"
 			/>
 			<ImageGalleryItem
-				alt="Random image 2"
+				alt="Random image 3"
 				src="https://picsum.photos/id/24/200/200"
 			/>
 			<ImageGalleryItem
-				alt="Random image 2"
+				alt="Random image 4"
 				src="https://picsum.photos/id/58/200/200"
+			/>
+			<ImageGalleryItem
+				alt="Random image 5"
+				src="https://picsum.photos/id/309/200/200"
+			/>
+			<ImageGalleryItem
+				alt="Random image 6"
+				src="https://picsum.photos/id/46/200/200"
+			/>
+			<ImageGalleryItem
+				alt="Random image 7"
+				src="https://picsum.photos/id/8/200/200"
+			/>
+			<ImageGalleryItem
+				alt="Random image 8"
+				src="https://picsum.photos/id/101/200/200"
+			/>
+		</ImageGallery>
+	);
+};
+
+export const Cover: React.FC = () => {
+	return (
+		<ImageGallery>
+			<ImageGalleryItem
+				alt="Random image 1"
+				src="https://picsum.photos/id/137/200/200"
+				isCover
+			/>
+			<ImageGalleryItem
+				alt="Random image 2"
+				src="https://picsum.photos/id/208/200/200"
+			/>
+		</ImageGallery>
+	);
+};
+
+export const Columns: React.FC = () => {
+	return (
+		<ImageGallery columns={ 3 }>
+			<ImageGalleryItem
+				alt="Random image 1"
+				src="https://picsum.photos/id/137/200/200"
+			/>
+			<ImageGalleryItem
+				alt="Random image 2"
+				src="https://picsum.photos/id/208/200/200"
+			/>
+			<ImageGalleryItem
+				alt="Random image 3"
+				src="https://picsum.photos/id/24/200/200"
+			/>
+			<ImageGalleryItem
+				alt="Random image 4"
+				src="https://picsum.photos/id/58/200/200"
+			/>
+			<ImageGalleryItem
+				alt="Random image 5"
+				src="https://picsum.photos/id/309/200/200"
+			/>
+			<ImageGalleryItem
+				alt="Random image 6"
+				src="https://picsum.photos/id/46/200/200"
 			/>
 		</ImageGallery>
 	);
