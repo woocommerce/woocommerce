901164d60ffde5bcd3b2a1a1e53c73bd0c14ede4
Tomasz Tunik <tomasztunik@gmail.com>
2022-08-23 18:06:17 +0200

    Product search block as core/search variation (https://github.com/woocommerce/woocommerce-blocks/pull/6191)
    Co-authored-by: Albert Juhé Lluveras <contact@albertjuhe.com>
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>

--- plugins/woocommerce-blocks/tests/e2e/specs/backend/product-search.test.js
+++ plugins/woocommerce-blocks/tests/e2e/specs/backend/product-search.test.js
@@ -12,52 +12,60 @@ import {
 	visitBlockPage,
 } from '@woocommerce/blocks-test-utils';
 
+/**
+ * Internal dependencies
+ */
+import { GUTENBERG_EDITOR_CONTEXT, describeOrSkip } from '../../utils';
+
 const block = {
 	name: 'Product Search',
 	slug: 'woocommerce/product-search',
 	class: '.wc-block-product-search',
 };
 
-describe( `${ block.name } Block`, () => {
-	beforeAll( async () => {
-		await switchUserToAdmin();
-		await visitBlockPage( `${ block.name } Block` );
-	} );
+describeOrSkip( GUTENBERG_EDITOR_CONTEXT !== 'gutenberg' )(
+	`${ block.name } Block`,
+	() => {
+		beforeAll( async () => {
+			await switchUserToAdmin();
+			await visitBlockPage( `${ block.name } Block` );
+		} );
 
-	it( 'renders without crashing', async () => {
-		await expect( page ).toRenderBlock( block );
-	} );
+		it( 'renders without crashing', async () => {
+			await expect( page ).toRenderBlock( block );
+		} );
 
-	it( 'can toggle field label', async () => {
-		await openDocumentSettingsSidebar();
-		await page.click( block.class );
-		const selector = `${ block.class } .wc-block-product-search__label`;
-		const toggleLabel = await findLabelWithText(
-			'Show search field label'
-		);
-		await expect( toggleLabel ).toToggleElement( selector );
-	} );
+		it( 'can toggle field label', async () => {
+			await openDocumentSettingsSidebar();
+			await page.click( block.class );
+			const selector = `${ block.class } .wc-block-product-search__label`;
+			const toggleLabel = await findLabelWithText(
+				'Show search field label'
+			);
+			await expect( toggleLabel ).toToggleElement( selector );
+		} );
 
-	it( 'can change field labels in editor', async () => {
-		await expect( page ).toFill(
-			'textarea.wc-block-product-search__label',
-			'I am a new label'
-		);
+		it( 'can change field labels in editor', async () => {
+			await expect( page ).toFill(
+				'textarea.wc-block-product-search__label',
+				'I am a new label'
+			);
 
-		await expect( page ).toFill(
-			'.wc-block-product-search__field input',
-			'I am a new placeholder'
-		);
+			await expect( page ).toFill(
+				'.wc-block-product-search__field input',
+				'I am a new placeholder'
+			);
 
-		await clearAndFillInput(
-			'textarea.wc-block-product-search__label',
-			'The Label'
-		);
-		await clearAndFillInput(
-			'.wc-block-product-search__field input',
-			'The Placeholder'
-		);
+			await clearAndFillInput(
+				'textarea.wc-block-product-search__label',
+				'The Label'
+			);
+			await clearAndFillInput(
+				'.wc-block-product-search__field input',
+				'The Placeholder'
+			);
 
-		expect( await getEditedPostContent() ).toMatchSnapshot();
-	} );
-} );
+			expect( await getEditedPostContent() ).toMatchSnapshot();
+		} );
+	}
+);
