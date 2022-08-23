684bc538f83dc6bab36f5f0379d687717d445a3c
Alba Rincón <albarin@users.noreply.github.com>
2022-08-23 12:10:46 +0200

    Make the inlineInput default to false to avoid changing existing block behaviour (https://github.com/woocommerce/woocommerce-blocks/pull/6957)

--- plugins/woocommerce-blocks/assets/js/blocks/price-filter/frontend.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/price-filter/frontend.ts
@@ -14,7 +14,7 @@ const getProps = ( el: HTMLElement ) => {
 	return {
 		attributes: {
 			showInputFields: el.dataset.showinputfields === 'true',
-			inlineInput: el.dataset.inlineInput !== 'false',
+			inlineInput: el.dataset.inlineInput === 'true',
 			showFilterButton: el.dataset.showfilterbutton === 'true',
 			heading: el.dataset.heading || blockAttributes.heading.default,
 			headingLevel: el.dataset.headingLevel
