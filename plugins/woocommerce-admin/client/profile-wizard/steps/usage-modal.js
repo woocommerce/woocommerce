f993baaaa38110358fb9845daecddb254c5b009c
moon <moon.kyong@automattic.com>
2022-08-24 12:47:53 -0700

    Revert skip button changes

--- plugins/woocommerce-admin/client/profile-wizard/steps/usage-modal.js
+++ plugins/woocommerce-admin/client/profile-wizard/steps/usage-modal.js
@@ -99,7 +99,8 @@ class UsageModal extends Component {
 
 		// Bail if site has already opted in to tracking
 		if ( allowTracking ) {
-			return null;
+			onClose();
+			onContinue();
 		}
 
 		const {
@@ -158,8 +159,6 @@ class UsageModal extends Component {
 							onClick={ () => {
 								this.setState( { selectedAction: 'accept' } );
 								this.updateTracking( { allowTracking: true } );
-								onClose();
-								onContinue();
 							} }
 						>
 							{ acceptActionText }
