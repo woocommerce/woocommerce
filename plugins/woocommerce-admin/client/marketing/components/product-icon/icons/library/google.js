5d1df3a9211adb0bc076c6a08ab9c72d513f490b
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-08-26 18:21:50 +0800

    Set style for Google logo icon.
    We set white background to override the gray backgroun in ProductIcon.
    
    Padding is used to make it look visually similar size with other icons.

--- plugins/woocommerce-admin/client/marketing/components/product-icon/icons/library/google.js
+++ plugins/woocommerce-admin/client/marketing/components/product-icon/icons/library/google.js
@@ -4,7 +4,11 @@
 import { WC_ASSET_URL } from '~/utils/admin-settings';
 
 const google = (
-	<img src={ `${ WC_ASSET_URL }images/marketing/google.svg` } alt="Google" />
+	<img
+		src={ `${ WC_ASSET_URL }images/marketing/google.svg` }
+		alt="Google"
+		style={ { backgroundColor: 'white', padding: '2px' } }
+	/>
 );
 
 export default google;
