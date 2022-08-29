42f3ffefc94d551a621f0a0dff12729feae3d501
Seghir Nadir <nadir.seghir@gmail.com>
2022-08-29 09:35:05 +0100

    Turn current page into the default Cart/Checkout page (https://github.com/woocommerce/woocommerce-blocks/pull/6867)
    * Add default page notice
    
    * show notice all inner blocks
    
    * support flow when page isnt saved
    
    * switch from where we get the current post id
    
    * update lock
    
    * fix types
    
    * update logic to support cart as well
    
    * fix package.json
    
    * update design and move away from wc.data
    
    * restore notice
    
    * handle older versions of WooCommerce
    
    * fix package lock
    
    * fix typo

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout-shared/index.js
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout-shared/index.js
@@ -2,6 +2,5 @@ export * from './hacks';
 export * from './use-forced-layout';
 export * from './editor-utils';
 export * from './use-view-switcher';
-export * from './default-notice';
 export * from './sidebar-notices';
 export * from './block-settings';
