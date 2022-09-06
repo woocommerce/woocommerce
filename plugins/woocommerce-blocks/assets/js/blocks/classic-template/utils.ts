cf9325fc07551a5bfd9e3cff293a1fb2b5ca27ea
Tom Cafferkey <tjcafferkey@gmail.com>
2022-09-06 10:52:33 +0100

    Fix corrupt Classic Template placeholders for specific products. (https://github.com/woocommerce/woocommerce-blocks/pull/7033)
    * If user has created a template for a specific product, find the closest matching template details
    
    * Move Classic Template functions into Utils file and refactor function for specific template names
    
    * Classic Template utils refactor and unit tests

---
+++ plugins/woocommerce-blocks/assets/js/blocks/classic-template/utils.ts
@@ -0,0 +1,49 @@
+/**
+ * External dependencies
+ */
+import { Block } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies
+ */
+import { TEMPLATES } from './constants';
+import { TemplateDetails } from './types';
+
+// Finds the most appropriate template details object for specific template keys such as single-product-hoodie.
+export function getTemplateDetailsBySlug(
+	parsedTemplate: string,
+	templates: TemplateDetails
+) {
+	const templateKeys = Object.keys( templates );
+	let templateDetails = null;
+
+	for ( let i = 0; templateKeys.length > i; i++ ) {
+		const keyToMatch = parsedTemplate.substr( 0, templateKeys[ i ].length );
+		const maybeTemplate = templates[ keyToMatch ];
+		if ( maybeTemplate ) {
+			templateDetails = maybeTemplate;
+			break;
+		}
+	}
+
+	return templateDetails;
+}
+
+export function isClassicTemplateBlockRegisteredWithAnotherTitle(
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	block: Block< any > | undefined,
+	parsedTemplate: string
+) {
+	const templateDetails = getTemplateDetailsBySlug(
+		parsedTemplate,
+		TEMPLATES
+	);
+	return block?.title !== templateDetails?.title;
+}
+
+export function hasTemplateSupportForClassicTemplateBlock(
+	parsedTemplate: string,
+	templates: TemplateDetails
+): boolean {
+	return getTemplateDetailsBySlug( parsedTemplate, templates ) ? true : false;
+}
