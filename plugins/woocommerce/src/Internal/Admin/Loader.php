07af460bf672075c74d92693b0a8c10aa2597184
Fernando Marichal <contacto@fernandomarichal.com>
2022-09-09 16:48:43 -0300

    Add JITMs

--- plugins/woocommerce/src/Internal/Admin/Loader.php
+++ plugins/woocommerce/src/Internal/Admin/Loader.php
@@ -230,9 +230,13 @@ class Loader {
 			return;
 		}
 
+		// The JITMs won't be shown in the Onboarding Wizard.
+		$is_onboarding   = isset( $_GET['path'] ) && '/setup-wizard' === wc_clean( wp_unslash( $_GET['path'] ) ); // phpcs:ignore WordPress.Security.NonceVerification
+		$maybe_hide_jitm = $is_onboarding ? 'woocommerce-layout__notice-list-hide' : '';
+
 		// Wrap the notices in a hidden div to prevent flickering before
 		// they are moved elsewhere in the page by WordPress Core.
-		echo '<div class="woocommerce-layout__notice-list-hide" id="wp__notice-list">';
+		echo '<div class="woocommerce-layout__jitm ' . sanitize_html_class( $maybe_hide_jitm ) . '" id="jp-admin-notices">';
 
 		if ( PageController::is_admin_page() ) {
 			// Capture all notices and hide them. WordPress Core looks for
