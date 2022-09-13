de433503c804b37b724c03ed0c949f7ff2863259
Luigi Teschio <gigitux@gmail.com>
2022-09-13 17:16:59 +0200

    Fix e2e test - Filter products by stock block (https://github.com/woocommerce/woocommerce-blocks/pull/7137)

--- plugins/woocommerce-blocks/tests/e2e/specs/backend/filter-products-by-stock.test.js
+++ plugins/woocommerce-blocks/tests/e2e/specs/backend/filter-products-by-stock.test.js
@@ -43,7 +43,9 @@ describe( `${ block.name } Block`, () => {
 		} );
 
 		it( 'product count can be toggled', async () => {
-			const toggleLabel = await findLabelWithText( 'Product count' );
+			const toggleLabel = await findLabelWithText(
+				'Display product count'
+			);
 			await expect( toggleLabel ).toToggleElement(
 				`${ block.class } .wc-filter-element-label-list-count`
 			);
