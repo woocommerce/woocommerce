81d2b4971609bd34bde26dbd2d049257dd418e58
Lourens Schep <lourensschep@gmail.com>
2022-09-21 13:56:41 -0300

    Add initial attribute list with dragging and removing functionality

--- plugins/woocommerce-admin/client/products/fields/attribute-field/attribute-field.tsx
+++ plugins/woocommerce-admin/client/products/fields/attribute-field/attribute-field.tsx
@@ -5,6 +5,8 @@ import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 import { ProductAttribute } from '@woocommerce/data';
 import { Text } from '@woocommerce/experimental';
+import { Sortable, ListItem } from '@woocommerce/components';
+import { trash } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -21,6 +23,13 @@ export const AttributeField: React.FC< AttributeFieldProps > = ( {
 	value,
 	onChange,
 } ) => {
+	const onRemove = ( attribute: ProductAttribute ) => {
+		// eslint-disable-next-line no-alert
+		if ( window.confirm( __( 'Remove this attribute', 'woocommerce' ) ) ) {
+			onChange( value.filter( ( attr ) => attr.id !== attribute.id ) );
+		}
+	};
+
 	if ( ! value || value.length === 0 ) {
 		return (
 			<div className="woocommerce-attribute-field">
@@ -50,5 +59,88 @@ export const AttributeField: React.FC< AttributeFieldProps > = ( {
 			</div>
 		);
 	}
-	return <div className="woocommerce-attribute-field"></div>;
+
+	const sortedAttributes = value.sort( ( a, b ) => a.position - b.position );
+	const attributeKeyValues = value.reduce(
+		(
+			keyValue: Record< number, ProductAttribute >,
+			attribute: ProductAttribute
+		) => {
+			keyValue[ attribute.id ] = attribute;
+			return keyValue;
+		},
+		{} as Record< number, ProductAttribute >
+	);
+	return (
+		<div className="woocommerce-attribute-field">
+			<Sortable
+				onOrderChange={ ( items ) => {
+					const newAttributes: ProductAttribute[] = items
+						.map( ( item, index ): ProductAttribute | undefined => {
+							const key = item.key
+								? parseInt( item.key as string, 10 )
+								: NaN;
+							if ( key !== NaN ) {
+								return {
+									...attributeKeyValues[ key ],
+									position: index,
+								};
+							}
+							return undefined;
+						} )
+						.filter(
+							( attr ): attr is ProductAttribute =>
+								attr !== undefined
+						);
+					onChange( newAttributes );
+				} }
+			>
+				{ sortedAttributes.map( ( attribute ) => (
+					<ListItem key={ attribute.id }>
+						<div>{ attribute.name }</div>
+						<div className="woocommerce-attribute-field__attribute-options">
+							{ attribute.options
+								.slice( 0, 2 )
+								.map( ( option, index ) => (
+									<div
+										className="woocommerce-attribute-field__attribute-option-chip"
+										key={ index }
+									>
+										{ option }
+									</div>
+								) ) }
+							{ attribute.options.length > 2 && (
+								<div className="woocommerce-attribute-field__attribute-option-chip">
+									+ { attribute.options.length - 2 }&nbsp;
+									{ __( 'more', 'woocommerce' ) }
+								</div>
+							) }
+						</div>
+						<div className="woocommerce-attribute-field__attribute-actions">
+							<Button variant="tertiary" disabled>
+								{ __( 'edit', 'woocommerce' ) }
+							</Button>
+							<Button
+								icon={ trash }
+								label={ __(
+									'Remove attribute',
+									'woocommerce'
+								) }
+								onClick={ () => onRemove( attribute ) }
+							></Button>
+						</div>
+					</ListItem>
+				) ) }
+			</Sortable>
+			<ListItem>
+				<Button
+					variant="secondary"
+					className="woocommerce-attribute-field__add"
+					disabled={ true }
+				>
+					{ __( 'Add attribute', 'woocommerce' ) }
+				</Button>
+			</ListItem>
+		</div>
+	);
 };
