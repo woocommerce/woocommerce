3173487aa165ff50a2adbf371fa763f8af717a37
Alex Florisca <alex.florisca@automattic.com>
2022-09-21 15:20:00 +0100

    Revert "Add static class name for product-details (https://github.com/woocommerce/woocommerce-blocks/pull/6914)" (https://github.com/woocommerce/woocommerce-blocks/pull/7191)
    This reverts commit d8b4d55b6e21c557887c34c6601fd11f5bb92508.

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/product-details/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/product-details/index.tsx
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { kebabCase } from 'lodash';
 import { decodeEntities } from '@wordpress/html-entities';
 import type { ProductResponseItemData } from '@woocommerce/type-defs/product-response';
 
@@ -31,10 +32,15 @@ const ProductDetails = ( {
 			{ details.map( ( detail ) => {
 				// Support both `key` and `name` props
 				const name = detail?.key || detail.name || '';
+				const className = name
+					? `wc-block-components-product-details__${ kebabCase(
+							name
+					  ) }`
+					: '';
 				return (
 					<li
 						key={ name + ( detail.display || detail.value ) }
-						className="wc-block-components-product-details__item"
+						className={ className }
 					>
 						{ name && (
 							<>
