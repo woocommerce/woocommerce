ad8ae54c62f63c3a6255786dd308700bb07632da
Niels Lange <info@nielslange.de>
2022-09-22 12:28:55 +0700

    Remove experimental flag from Cross-Sells block (https://github.com/woocommerce/woocommerce-blocks/pull/7188)
    * Remove experimental flag from Cross-Sells block
    
    * Remove experimental flag from Cross-Sells block
    
    * Remove experimental flag from woocommerce/cart-cross-sells-products-block

--- plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-cross-sells-block/index.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-cross-sells-block/index.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { Icon, column } from '@wordpress/icons';
-import { registerExperimentalBlockType } from '@woocommerce/block-settings';
+import { registerBlockType } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
@@ -10,7 +10,7 @@ import { registerExperimentalBlockType } from '@woocommerce/block-settings';
 import { Edit, Save } from './edit';
 import metadata from './block.json';
 
-registerExperimentalBlockType( metadata, {
+registerBlockType( metadata, {
 	icon: {
 		src: (
 			<Icon
--- plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-items-block/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-items-block/edit.tsx
@@ -4,7 +4,6 @@
 import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 import { Main } from '@woocommerce/base-components/sidebar-layout';
 import { innerBlockAreas } from '@woocommerce/blocks-checkout';
-import { isExperimentalBuild } from '@woocommerce/block-settings';
 import type { TemplateArray } from '@wordpress/blocks';
 /**
  * Internal dependencies
@@ -23,9 +22,7 @@ export const Edit = ( { clientId }: Props ): JSX.Element => {
 	const allowedBlocks = getAllowedBlocks( innerBlockAreas.CART_ITEMS );
 	const defaultTemplate = [
 		[ 'woocommerce/cart-line-items-block', {}, [] ],
-		...( isExperimentalBuild()
-			? [ [ 'woocommerce/cart-cross-sells-block', {}, [] ] ]
-			: [] ),
+		[ 'woocommerce/cart-cross-sells-block', {}, [] ],
 	] as unknown as TemplateArray;
 
 	useForcedLayout( {
