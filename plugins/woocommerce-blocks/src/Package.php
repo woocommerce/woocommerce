9109fd6088f5c8bac763c5abd3b52ce4af524009
Seghir Nadir <nadir.seghir@gmail.com>
2022-09-26 15:51:32 +0100

    Local pickup rates are always returned regardless of the shipping zone. (https://github.com/woocommerce/woocommerce-blocks/pull/7177)
    * local pickup is always returned no matter the zone
    
    * wrap filters in feature flags
    
    * switch to experimental

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -85,16 +85,10 @@ class Package {
 	 *
 	 * @return boolean
 	 */
-	// This function will be kept around but commented out in case we add feature-plugin-specific code in the future.
-	// phpcs:disable Squiz.PHP.CommentedOutCode
-	// phpcs:disable Squiz.Commenting.InlineComment.InvalidEndChar
-	// phpcs:disable Squiz.Commenting.InlineComment.SpacingBefore
-	//	public static function is_feature_plugin_build() {
-	//		return self::get_package()->is_feature_plugin_build();
-	//	}
-	// phpcs:enable Squiz.PHP.CommentedOutCode
-	// phpcs:enable Squiz.Commenting.InlineComment.InvalidEndChar
-	// phpcs:enable Squiz.Commenting.InlineComment.SpacingBefore
+	public static function is_feature_plugin_build() {
+		return self::get_package()->is_feature_plugin_build();
+	}
+
 
 	/**
 	 * Loads the dependency injection container for woocommerce blocks.
