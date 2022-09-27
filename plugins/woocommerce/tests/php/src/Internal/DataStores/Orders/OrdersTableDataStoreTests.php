59ed950bb4dd1776fb9312e9d8a276dc82a0c572
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2022-09-27 16:34:08 +0530

    Fix unit test to extpect yes.

--- plugins/woocommerce/tests/php/src/Internal/DataStores/Orders/OrdersTableDataStoreTests.php
+++ plugins/woocommerce/tests/php/src/Internal/DataStores/Orders/OrdersTableDataStoreTests.php
@@ -178,7 +178,7 @@ class OrdersTableDataStoreTests extends WC_Unit_Test_Case {
 			'cart_hash'          => 'YET-ANOTHER-CART-HASH',
 		);
 		static $datastore_updates = array(
-			'email_sent' => true,
+			'email_sent' => 'yes',
 		);
 		static $meta_to_update    = array(
 			'my_meta_key' => array( 'my', 'custom', 'meta' ),
