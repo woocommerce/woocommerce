3732363ba6ebf040030ffae409f81ad10250fd26
Luigi Teschio <gigitux@gmail.com>
2022-09-28 10:54:38 +0200

    Add to the Product Button block the support for the Product Query block (https://github.com/woocommerce/woocommerce-blocks/pull/6948)
    * Adds to the Product Image Block the support for the Product Query Block
    
    Adds to the Product Image Block the support for the Product Query Block woocommerce/woocommerce-blocks#6911
    
    * use shared config
    
    * use shared config
    
    * use shared config
    
    * Add to the Product Button Block the support for the Product Query Block
    
    * Add to the Product Image Block the support for the Product Query Block
    
    * fix lint errors
    
    * address feedback
    
    * set grid view and font-size L as default

--- plugins/woocommerce-blocks/src/BlockTypes/ProductButton.php
+++ plugins/woocommerce-blocks/src/BlockTypes/ProductButton.php
@@ -1,6 +1,8 @@
 <?php
 namespace Automattic\WooCommerce\Blocks\BlockTypes;
 
+use Automattic\WooCommerce\Blocks\Utils\StyleAttributesUtils;
+
 /**
  * ProductButton class.
  */
@@ -51,12 +53,61 @@ class ProductButton extends AbstractBlock {
 	}
 
 	/**
-	 * Register script and style assets for the block type before it is registered.
-	 *
-	 * This registers the scripts; it does not enqueue them.
+	 * It is necessary to register and enqueues assets during the render phase because we want to load assets only if the block has the content.
 	 */
 	protected function register_block_type_assets() {
-		parent::register_block_type_assets();
-		$this->register_chunk_translations( [ $this->block_name ] );
+		return null;
+	}
+
+	/**
+	 * Register the context.
+	 */
+	protected function get_block_type_uses_context() {
+		return [ 'query', 'queryId', 'postId' ];
+	}
+
+	/**
+	 * Include and render the block
+	 *
+	 * @param array    $attributes Block attributes. Default empty array.
+	 * @param string   $content    Block content. Default empty string.
+	 * @param WP_Block $block      Block instance.
+	 * @return string Rendered block type output.
+	 */
+	protected function render( $attributes, $content, $block ) {
+		if ( ! empty( $content ) ) {
+			parent::register_block_type_assets();
+			$this->register_chunk_translations( [ $this->block_name ] );
+			return $content;
+		}
+
+		$post_id = $block->context['postId'];
+		$product = wc_get_product( $post_id );
+
+		if ( $product ) {
+			$cart_redirect_after_add = get_option( 'woocommerce_cart_redirect_after_add' ) === 'yes';
+			$html_element            = ( ! $product->has_options() && $product->is_purchasable() && $product->is_in_stock() && ! $cart_redirect_after_add ) ? 'button' : 'a';
+			$styles_and_classes      = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes, array( 'border_radius', 'font_size', 'text_color' ) );
+
+			return apply_filters(
+				'woocommerce_loop_add_to_cart_link',
+				sprintf(
+					'<div class="wp-block-button wc-block-components-product-button wc-block-grid__product-add-to-cart">
+					<%s href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" class="wp-block-button__link %s wc-block-components-product-button__button product_type_%s %s" style="%s">%s</%s>
+				</div>',
+					$html_element,
+					esc_url( $product->add_to_cart_url() ),
+					esc_attr( $product->get_id() ),
+					esc_attr( $product->get_sku() ),
+					$product->is_purchasable() ? 'ajax_add_to_cart add_to_cart_button' : '',
+					esc_attr( $product->get_type() ),
+					$styles_and_classes['classes'],
+					$styles_and_classes['styles'],
+					esc_html( $product->add_to_cart_text() ),
+					$html_element
+				),
+				$product
+			);
+		}
 	}
 }
