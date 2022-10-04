c19228da957751e0d80628b9b75844332798dd85
RJ <27843274+rjchow@users.noreply.github.com>
2022-10-04 19:42:40 +0800

    Dev: migrate @woocommerce/data reviews to Typescript (#34844)

--- packages/js/data/src/reviews/test/reducer.ts
+++ packages/js/data/src/reviews/test/reducer.ts
@@ -7,6 +7,8 @@
  */
 import reducer from '../reducer';
 import TYPES from '../action-types';
+import { ReviewObject } from '../types';
+import { Action } from '../actions';
 
 const defaultState = {
 	reviews: {},
@@ -16,7 +18,7 @@ const defaultState = {
 
 describe( 'reviews reducer', () => {
 	it( 'should return a default state', () => {
-		const state = reducer( undefined, {} );
+		const state = reducer( undefined, {} as Action );
 		expect( state ).toEqual( defaultState );
 		expect( state ).not.toBe( defaultState );
 	} );
@@ -27,7 +29,7 @@ describe( 'reviews reducer', () => {
 			{ id: 2, review: 'Dynamite!' },
 		];
 		const totalCount = 45;
-		const query = { status: 'flavortown' };
+		const query = { status: 'hold' as const };
 		const state = reducer( defaultState, {
 			type: TYPES.UPDATE_REVIEWS,
 			reviews,
@@ -46,14 +48,14 @@ describe( 'reviews reducer', () => {
 		).toBeTruthy();
 
 		expect( state.reviews[ stringifiedQuery ].totalCount ).toBe( 45 );
-		expect( state.data[ '1' ] ).toEqual( reviews[ 0 ] );
-		expect( state.data[ '2' ] ).toEqual( reviews[ 1 ] );
+		expect( state.data[ 1 ] ).toEqual( reviews[ 0 ] );
+		expect( state.data[ 2 ] ).toEqual( reviews[ 1 ] );
 	} );
 
 	it( 'should handle UPDATE_REVIEWS with _fields, only update updated fields', () => {
 		const reviews = [ { id: 1 }, { id: 2 } ];
 		const totalCount = 45;
-		const query = { status: 'flavortown', _fields: [ 'id' ] };
+		const query = { status: 'approved' as const, _fields: [ 'id' ] };
 		const state = reducer(
 			{
 				...defaultState,
@@ -81,12 +83,12 @@ describe( 'reviews reducer', () => {
 		).toBeTruthy();
 
 		expect( state.reviews[ stringifiedQuery ].totalCount ).toBe( 45 );
-		expect( state.data[ '1' ].review ).toEqual( 'Yum!' );
-		expect( state.data[ '2' ].review ).toEqual( 'Dynamite!' );
+		expect( state.data[ 1 ].review ).toEqual( 'Yum!' );
+		expect( state.data[ 2 ].review ).toEqual( 'Dynamite!' );
 	} );
 
 	it( 'should handle SET_ERROR', () => {
-		const query = { status: 'flavortown' };
+		const query = { status: 'spam' as const };
 		const error = 'Baaam!';
 		const state = reducer( defaultState, {
 			type: TYPES.SET_ERROR,
@@ -103,19 +105,19 @@ describe( 'reviews reducer', () => {
 			{
 				...defaultState,
 				data: {
-					4: { title: 'test' },
+					4: { review: 'test' },
 				},
 			},
 			{
 				type: TYPES.SET_REVIEW,
 				reviewId: 4,
 				reviewData: {
-					title: 'test updated',
-				},
+					review: 'test updated',
+				} as ReviewObject, // type coercion so that we don't need to populate a whole object
 			}
 		);
 
-		expect( state.data[ 4 ].title ).toEqual( 'test updated' );
+		expect( state.data[ 4 ].review ).toEqual( 'test updated' );
 	} );
 
 	it( 'should handle SET_REVIEW_IS_UPDATING', () => {
@@ -123,7 +125,7 @@ describe( 'reviews reducer', () => {
 			{
 				...defaultState,
 				data: {
-					4: { title: 'test' },
+					4: { review: 'test' },
 				},
 			},
 			{
