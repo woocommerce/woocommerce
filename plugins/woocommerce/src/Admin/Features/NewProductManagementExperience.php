0f636c0d15ea7f4e40891a7ec7ac5bbcb5cecf0f
Joshua Flowers <joshuatf@gmail.com>
2022-10-04 16:30:30 -0700

    Create feature class and enqueue styles

---
+++ plugins/woocommerce/src/Admin/Features/NewProductManagementExperience.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * WooCommerce New Product Management Experience
+ */
+
+namespace Automattic\WooCommerce\Admin\Features;
+
+use \Automattic\WooCommerce\Internal\Admin\Loader;
+
+/**
+ * Loads assets related to the new product management experience page.
+ */
+class NewProductManagementExperience {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_action( 'admin_init', array( $this, 'enqueue_styles' ) );
+	}
+
+    /**
+     * Enqueue styles needed for the rich text editor.
+     */
+    public function enqueue_styles() {
+		wp_enqueue_style( 'wp-edit-post' );
+		wp_enqueue_style( 'wp-format-library' );
+		wp_enqueue_editor();
+		do_action( 'enqueue_block_editor_assets' );
+    }
+
+}
