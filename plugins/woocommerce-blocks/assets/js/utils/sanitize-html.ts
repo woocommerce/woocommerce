5cc0dc76040f2acb486e7bb2acf8fea3ca2236cb
Hsing-yu Flowers <hsingyuc.7@gmail.com>
2022-10-05 02:59:38 -0700

    Fix use of sanitizeHTML (https://github.com/woocommerce/woocommerce-blocks/pull/7231)
    * Remove object from sanitizeHTML return value
    
    * Import sanitizeHTML from utils
    
    * Fix dangerously set inner HTML format
    
    * Update package-lock
    
    * Update package-lock
    
    * Update package-lock
    
    * Update @types/dompurify version
    
    Co-authored-by: Thomas Roberts <5656702+opr@users.noreply.github.com>

--- plugins/woocommerce-blocks/assets/js/utils/sanitize-html.ts
+++ plugins/woocommerce-blocks/assets/js/utils/sanitize-html.ts
@@ -1,12 +1,7 @@
 /**
  * External dependencies
  */
-import DOMPurify from 'dompurify';
-
-type sanitizedHTMLObject = {
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	__html: string;
-};
+import { sanitize } from 'dompurify';
 
 const ALLOWED_TAGS = [ 'a', 'b', 'em', 'i', 'strong', 'p', 'br' ];
 const ALLOWED_ATTR = [ 'target', 'href', 'rel', 'name', 'download' ];
@@ -14,14 +9,12 @@ const ALLOWED_ATTR = [ 'target', 'href', 'rel', 'name', 'download' ];
 export const sanitizeHTML = (
 	html: string,
 	config?: { tags?: typeof ALLOWED_TAGS; attr?: typeof ALLOWED_ATTR }
-): sanitizedHTMLObject => {
+) => {
 	const tagsValue = config?.tags || ALLOWED_TAGS;
 	const attrValue = config?.attr || ALLOWED_ATTR;
 
-	return {
-		__html: DOMPurify.sanitize( html, {
-			ALLOWED_TAGS: tagsValue,
-			ALLOWED_ATTR: attrValue,
-		} ),
-	};
+	return sanitize( html, {
+		ALLOWED_TAGS: tagsValue,
+		ALLOWED_ATTR: attrValue,
+	} );
 };
