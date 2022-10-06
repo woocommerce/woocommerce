c8137bffcf5f3a6607971015361a6cd8527e73e3
Manish Menaria <the.manish.menaria@gmail.com>
2022-10-06 17:57:09 +0530

    Register new footer patterns (https://github.com/woocommerce/woocommerce-blocks/pull/7306)
    * Register new footer patterns
    
    * Capitalize the footer word
    
    * Fix large footer pattern content cut-off issue
    
    For more info:
    https://github.com/woocommerce/woocommerce-blocks/pull/7306#issuecomment-1268491571
    https://github.com/woocommerce/woocommerce-blocks/pull/7306#issuecomment-1269717251
    
    Co-authored-by: Alba Rincón <alba.rincon@automattic.com>

---
+++ plugins/woocommerce-blocks/patterns/footer-large-light.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Title: WooCommerce Large Footer | Light
+ * Slug: woocommerce-blocks/footer-large-light
+ * Categories: WooCommerce
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"32px","right":"4%","bottom":"32px","left":"4%"},"blockGap":"40px"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"backgroundColor":"background","textColor":"black"} -->
+<div class="wp-block-group alignfull has-black-color has-background-background-color has-text-color has-background has-link-color" style="padding-top:32px;padding-right:4%;padding-bottom:32px;padding-left:4%"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"16px"}}} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"45%","style":{"spacing":{"padding":{"right":"50px"}}}} -->
+<div class="wp-block-column" style="padding-right:50px;flex-basis:45%"><!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":5} -->
+<h5>Join the community</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Learn about new products and discounts!</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:social-links {"iconColor":"black","iconColorValue":"#000000","size":"has-small-icon-size","className":"is-style-logos-only"} -->
+<ul class="wp-block-social-links has-small-icon-size has-icon-color is-style-logos-only"><!-- wp:social-link {"service":"facebook"} /-->
+
+<!-- wp:social-link {"service":"twitter"} /-->
+
+<!-- wp:social-link {"service":"instagram"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"20%","style":{"spacing":{"padding":{"top":"0px"}}}} -->
+<div class="wp-block-column" style="padding-top:0px;flex-basis:20%"><!-- wp:navigation {"layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"20%","style":{"spacing":{"blockGap":"16px"}}} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:20%"><!-- wp:navigation {"layout":{"type":"flex","orientation":"vertical"}} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px"><!-- wp:group {"style":{"spacing":{"blockGap":"8px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"12px"}}} -->
+<p style="font-size:12px">@ 2022</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"400","fontSize":"12px"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"14px"}}} -->
+<p style="font-size:14px"><em>Built with <a href="https://woocommerce.com/">WooCommerce</a> </em></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
