f6a536b00758d25b7a453b36219b360d7478c939
Alba Rincón <albarin@users.noreply.github.com>
2022-10-06 14:16:32 +0100

    Register new header patterns (https://github.com/woocommerce/woocommerce-blocks/pull/7292)
    * Register new header patterns
    
    * Fix name
    
    * Capitalize the header word
    
    * Remove ref from the navigation menu

---
+++ plugins/woocommerce-blocks/patterns/header-minimal-dark.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: WooCommerce Minimal Header | Dark
+ * Slug: woocommerce-blocks/minimal-header-dark
+ * Categories: WooCommerce
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"2%","bottom":"16px","left":"2%","top":"16px"},"margin":{"top":"0px","bottom":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"black","textColor":"background","className":"sticky-header","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull sticky-header has-background-color has-black-background-color has-text-color has-background has-link-color" style="margin-top:0px;margin-bottom:0px;padding-top:16px;padding-right:2%;padding-bottom:16px;padding-left:2%"><!-- wp:group {"style":{"spacing":{"blockGap":"40px"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"shouldSyncIcon":false} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"8px"},"typography":{"fontSize":"13px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group" style="font-size:13px"><!-- wp:group {"style":{"spacing":{"blockGap":"16px"}},"layout":{"type":"flex","flexWrap":"nowrap"},"fontSize":"small"} -->
+<div class="wp-block-group has-small-font-size"><!-- wp:navigation {"textColor":"background","overlayMenu":"always","layout":{"type":"flex","justifyContent":"center"}} /-->
+
+<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search store","width":220,"widthUnit":"px","buttonText":"Search","buttonPosition":"no-button"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:woocommerce/mini-cart {"textColor":"background","style":{"typography":{"fontSize":"13px"}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
