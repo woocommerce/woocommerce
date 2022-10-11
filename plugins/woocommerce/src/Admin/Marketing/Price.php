a38359463da22545e8f6136d2ecb091f9e9bcae9
Nima <nima.karimi@automattic.com>
2022-10-11 18:19:57 +0100

    Add Price class to represent a price with currency

---
+++ plugins/woocommerce/src/Admin/Marketing/Price.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Represents a price with a currency.
+ */
+
+namespace Automattic\WooCommerce\Admin\Marketing;
+
+use JsonSerializable;
+
+/**
+ * Price class
+ *
+ * @since x.x.x
+ */
+class Price implements JsonSerializable {
+	/**
+	 * The price.
+	 *
+	 * @var string
+	 */
+	protected $value;
+
+	/**
+	 * The currency of the price.
+	 *
+	 * @var string
+	 */
+	protected $currency;
+
+	/**
+	 * Price constructor.
+	 *
+	 * @param string $value    The value of the price.
+	 * @param string $currency The currency of the price.
+	 */
+	public function __construct( string $value, string $currency ) {
+		$this->value    = $value;
+		$this->currency = $currency;
+	}
+
+	/**
+	 * Get value of the price.
+	 *
+	 * @return string
+	 */
+	public function get_value(): string {
+		return $this->value;
+	}
+
+	/**
+	 * Get the currency of the price.
+	 *
+	 * @return string
+	 */
+	public function get_currency(): string {
+		return $this->currency;
+	}
+
+	/**
+	 * Serialize the price data.
+	 *
+	 * @return array
+	 */
+	public function jsonSerialize() {
+		return [
+			'value'    => $this->get_value(),
+			'currency' => $this->get_currency(),
+		];
+	}
+}
