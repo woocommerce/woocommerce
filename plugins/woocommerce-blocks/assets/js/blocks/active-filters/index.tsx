ab9fd291af35d0c2921e899957241c4b5180a8b8
Tung Du <dinhtungdu@gmail.com>
2022-10-11 15:39:34 +0700

    Correct block name and description (https://github.com/woocommerce/woocommerce-blocks/pull/7354)
    * Correct block name and description
    
    * remove duplicated description for controls blocks
    
    * remove duplicated title and desc
    
    * Revert "remove duplicated description for controls blocks"
    
    This reverts commit ed5417eb0e5191ed2f1def2a04c9e724fe70856a.

--- plugins/woocommerce-blocks/assets/js/blocks/active-filters/index.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/active-filters/index.tsx
@@ -17,14 +17,6 @@ import { blockAttributes } from './attributes';
 import { Attributes } from './types';
 
 registerBlockType( metadata, {
-	title: __(
-		'Active Product Filter Controls',
-		'woo-gutenberg-products-block'
-	),
-	description: __(
-		'Display the currently active product filters.',
-		'woo-gutenberg-products-block'
-	),
 	icon: {
 		src: (
 			<Icon
