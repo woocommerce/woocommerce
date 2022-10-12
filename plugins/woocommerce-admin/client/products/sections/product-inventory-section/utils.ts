58911d271cab1619259822b2b2fb307b7f058ca1
Joshua Flowers <joshuatf@gmail.com>
2022-10-12 13:53:23 -0700

    Remove sku related validation

--- plugins/woocommerce-admin/client/products/sections/product-inventory-section/utils.ts
+++ plugins/woocommerce-admin/client/products/sections/product-inventory-section/utils.ts
@@ -5,36 +5,11 @@ import { __ } from '@wordpress/i18n';
 import { ProductStatus, ProductType, Product } from '@woocommerce/data';
 import type { FormErrors } from '@woocommerce/components';
 
-/**
- * Internal dependencies
- */
-import { isValidSlug } from '../utils';
-
 export const validate = (
 	values: Partial< Product< ProductStatus, ProductType > >,
 	errors: FormErrors< typeof values >
 ) => {
 	const nextErrors = { ...errors };
-	if ( values.sku && values.sku.length > 0 && ! isValidSlug( values.sku ) ) {
-		nextErrors.sku = __(
-			'!!SKU can only contain alphanumeric characters, hyphens, and underscores.',
-			'woocommerce'
-		);
-	}
-
-	if ( values.sku && values.sku.length > 160 ) {
-		nextErrors.sku = __(
-			'SKU must be a maximum of 160 characters.',
-			'woocommerce'
-		);
-	}
-
-	if ( values.sku && values.sku.length > 160 ) {
-		nextErrors.sku = __(
-			'SKU must be a maximum of 160 characters.',
-			'woocommerce'
-		);
-	}
 
 	if ( values.stock_quantity && values.stock_quantity < 0 ) {
 		nextErrors.stock_quantity = __(
