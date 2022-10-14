aa8622d9851a4d0a5183541a50ccbdf736b796d6
Tung Du <dinhtungdu@gmail.com>
2022-10-14 10:25:02 +0700

    E2E tests for Product Query Block (https://github.com/woocommerce/woocommerce-blocks/pull/7386)

--- plugins/woocommerce-blocks/assets/js/blocks/product-query/inspector-controls.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-query/inspector-controls.tsx
@@ -37,17 +37,19 @@ export const withProductQueryControls =
 	< T extends EditorBlock< T > >( BlockEdit: ElementType ) =>
 	( props: ProductQueryBlock ) => {
 		const allowedControls = useAllowedControls( props.attributes );
-		return isWooQueryBlockVariation( props ) ? (
+
+		const availableControls = Object.entries( INSPECTOR_CONTROLS ).filter(
+			( [ key ] ) => allowedControls?.includes( key )
+		);
+		return isWooQueryBlockVariation( props ) &&
+			availableControls.length > 0 ? (
 			<>
 				<BlockEdit { ...props } />
 				<InspectorControls>
 					<PanelBody>
-						{ Object.entries( INSPECTOR_CONTROLS ).map(
-							( [ key, Control ] ) =>
-								allowedControls?.includes( key ) ? (
-									<Control { ...props } />
-								) : null
-						) }
+						{ availableControls.map( ( [ key, Control ] ) => (
+							<Control key={ key } { ...props } />
+						) ) }
 					</PanelBody>
 				</InspectorControls>
 			</>
