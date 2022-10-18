1f7deb139fdecc6f3b521712d5d7baebadac20a6
louwie17 <lourensschep@gmail.com>
2022-10-18 16:55:37 -0300

    Add popover support to select control dropdown (#34967)
    * Add dropdownPlacement option to SelectControl
    
    * Make use of parent element of menu for width
    
    * Add changelog
    
    * Add useEffect for boundingRect so it gets updated accordingly
    
    * Fix styling for popover dropdown
    
    * Move popup markup down to render return
    
    * Move getMenuProps up
    
    * Move getMenuProps down to list again and always render the popover menu
    
    * Add MenuSlot that adds popover slot to mody with aria-live=off so it is skipped by Modal
    
    * Add comment in relation to the eslint-disable comment
    
    * Fix flashing of popover and addressed some minor PR suggestions

--- packages/js/components/src/experimental-select-control/menu.tsx
+++ packages/js/components/src/experimental-select-control/menu.tsx
@@ -1,8 +1,15 @@
 /**
  * External dependencies
  */
+import { Popover } from '@wordpress/components';
 import classnames from 'classnames';
-import { createElement } from '@wordpress/element';
+import {
+	createElement,
+	useEffect,
+	useRef,
+	useState,
+	createPortal,
+} from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -22,21 +29,63 @@ export const Menu = ( {
 	isOpen,
 	className,
 }: MenuProps ) => {
+	const [ boundingRect, setBoundingRect ] = useState< DOMRect >();
+	const selectControlMenuRef = useRef< HTMLDivElement >( null );
+
+	useEffect( () => {
+		if ( selectControlMenuRef.current?.parentElement ) {
+			setBoundingRect(
+				selectControlMenuRef.current.parentElement.getBoundingClientRect()
+			);
+		}
+	}, [ selectControlMenuRef.current ] );
+
+	/* eslint-disable jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/click-events-have-key-events */
+	/* Disabled because of the onmouseup on the ul element below. */
 	return (
-		<ul
-			{ ...getMenuProps() }
+		<div
+			ref={ selectControlMenuRef }
 			className={ classnames(
 				'woocommerce-experimental-select-control__menu',
-				className,
-				{
-					'is-open': isOpen,
-					'has-results': Array.isArray( children )
-						? children.length
-						: Boolean( children ),
-				}
+				className
 			) }
 		>
-			{ isOpen && children }
-		</ul>
+			<Popover
+				focusOnMount={ false }
+				className={ classnames(
+					'woocommerce-experimental-select-control__popover-menu',
+					{
+						'is-open': isOpen,
+						'has-results':
+							Array.isArray( children ) && children.length > 0,
+					}
+				) }
+				position="bottom center"
+				animate={ false }
+			>
+				<ul
+					{ ...getMenuProps() }
+					className="woocommerce-experimental-select-control__popover-menu-container"
+					style={ {
+						width: boundingRect?.width,
+					} }
+					onMouseUp={ ( e ) =>
+						// Fix to prevent select control dropdown from closing when selecting within the Popover.
+						e.stopPropagation()
+					}
+				>
+					{ isOpen && children }
+				</ul>
+			</Popover>
+		</div>
 	);
+	/* eslint-enable jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/click-events-have-key-events */
 };
+
+export const MenuSlot: React.FC = () =>
+	createPortal(
+		<div aria-live="off">
+			<Popover.Slot />
+		</div>,
+		document.body
+	);
