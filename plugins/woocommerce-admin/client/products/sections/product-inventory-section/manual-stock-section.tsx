672dc9620f070b7945a773351023fe387b02c3ef
Joshua Flowers <joshuatf@gmail.com>
2022-10-18 09:45:27 -0700

    Fix up lint issues

--- plugins/woocommerce-admin/client/products/sections/product-inventory-section/manual-stock-section.tsx
+++ plugins/woocommerce-admin/client/products/sections/product-inventory-section/manual-stock-section.tsx
@@ -8,24 +8,34 @@ import { Product } from '@woocommerce/data';
 
 export const ManualStockSection: React.FC = () => {
 	const { getInputProps, values } = useFormContext< Product >();
-    const inputProps = getInputProps( 'stock_status' );
-    // These properties cause issues with the RadioControl component.
-    // A fix to form upstream would help if we can identify what type of input is used.
-    // @ts-ignore
-    delete inputProps.checked;
-    delete inputProps.value;
+	const inputProps = getInputProps( 'stock_status' );
+	// These properties cause issues with the RadioControl component.
+	// A fix to form upstream would help if we can identify what type of input is used.
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	delete inputProps.checked;
+	delete inputProps.value;
 
 	return (
 		<>
-            <RadioControl
-                label={ __( 'Stock status', 'woocommerce' ) }
-                options={ [
-                    { label: __( 'In stock', 'woocommerce' ), value: 'instock' },
-                    { label: __( 'Out of stock', 'woocommerce' ), value: 'outofstock' },
-                    { label: __( 'On backorder', 'woocommerce' ), value: 'onbackorder' },
-                ] }
-                { ...inputProps }
-            />
+			<RadioControl
+				label={ __( 'Stock status', 'woocommerce' ) }
+				options={ [
+					{
+						label: __( 'In stock', 'woocommerce' ),
+						value: 'instock',
+					},
+					{
+						label: __( 'Out of stock', 'woocommerce' ),
+						value: 'outofstock',
+					},
+					{
+						label: __( 'On backorder', 'woocommerce' ),
+						value: 'onbackorder',
+					},
+				] }
+				{ ...inputProps }
+			/>
 		</>
 	);
 };
