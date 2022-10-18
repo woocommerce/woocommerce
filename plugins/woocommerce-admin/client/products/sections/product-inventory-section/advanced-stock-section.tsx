b98f7d61349dd025cf4d769c9b52ed99058b88b7
Joshua Flowers <joshuatf@gmail.com>
2022-10-18 10:39:46 -0700

    Add backorder purchase options

--- plugins/woocommerce-admin/client/products/sections/product-inventory-section/advanced-stock-section.tsx
+++ plugins/woocommerce-admin/client/products/sections/product-inventory-section/advanced-stock-section.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { CheckboxControl } from '@wordpress/components';
+import { CheckboxControl, RadioControl } from '@wordpress/components';
 import { Product } from '@woocommerce/data';
 import { useFormContext } from '@woocommerce/components';
 
@@ -12,10 +12,41 @@ import { useFormContext } from '@woocommerce/components';
 import { getCheckboxProps } from '../utils';
 
 export const AdvancedStockSection: React.FC = () => {
-	const { getInputProps } = useFormContext< Product >();
+	const { getInputProps, values } = useFormContext< Product >();
+
+	const backordersProp = getInputProps( 'backorders' );
+	// These properties cause issues with the RadioControl component.
+	// A fix to form upstream would help if we can identify what type of input is used.
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	delete backordersProp.checked;
+	delete backordersProp.value;
 
 	return (
 		<>
+			{ values.manage_stock && (
+				<RadioControl
+					label={ __( 'When out of stock', 'woocommerce' ) }
+					options={ [
+						{
+							label: __( 'Allow purchases', 'woocommerce' ),
+							value: 'yes',
+						},
+						{
+							label: __(
+								'Allow purchases, but notify customers',
+								'woocommerce'
+							),
+							value: 'notify',
+						},
+						{
+							label: __( "Don't allow purchases", 'woocommerce' ),
+							value: 'no',
+						},
+					] }
+					{ ...backordersProp }
+				/>
+			) }
 			<h4>{ __( 'Restrictions', 'woocommerce' ) }</h4>
 			<CheckboxControl
 				label={ __(
