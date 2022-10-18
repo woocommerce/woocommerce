c72e00d85c1eb78ec50693f4e460d66eafca3f8a
Joshua T Flowers <joshuatf@gmail.com>
2022-10-18 16:08:49 -0700

    Add Tooltip component and remove EnrichedLabel (#35024)
    * Add experimental tooltip component
    
    * Add tooltip stories
    
    * Update EnrichedLabel component
    
    * Remove EnrichedLabel
    
    * Add changelog entries
    
    * Fix up linting issues
    
    * Handle PR feedback

--- plugins/woocommerce-admin/client/products/sections/product-details-section.tsx
+++ plugins/woocommerce-admin/client/products/sections/product-details-section.tsx
@@ -13,10 +13,12 @@ import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
 import { cleanForSlug } from '@wordpress/url';
 import {
-	EnrichedLabel,
+	Link,
 	useFormContext,
 	__experimentalRichTextEditor as RichTextEditor,
+	__experimentalTooltip as Tooltip,
 } from '@woocommerce/components';
+import interpolateComponents from '@automattic/interpolate-components';
 import {
 	Product,
 	ProductCategory,
@@ -30,10 +32,10 @@ import { BlockInstance, serialize, parse } from '@wordpress/blocks';
  * Internal dependencies
  */
 import './product-details-section.scss';
+import { CategoryField } from '../fields/category-field';
+import { EditProductLinkModal } from '../shared/edit-product-link-modal';
 import { getCheckboxProps, getTextControlProps } from './utils';
 import { ProductSectionLayout } from '../layout/product-section-layout';
-import { EditProductLinkModal } from '../shared/edit-product-link-modal';
-import { CategoryField } from '../fields/category-field';
 
 const PRODUCT_DETAILS_SLUG = 'product-details';
 
@@ -133,22 +135,40 @@ export const ProductDetailsSection: React.FC = () => {
 					/>
 					<CheckboxControl
 						label={
-							<EnrichedLabel
-								label={ __(
-									'Feature this product',
-									'woocommerce'
-								) }
-								helpDescription={ __(
-									'Include this product in a featured section on your website with a widget or shortcode.',
-									'woocommerce'
-								) }
-								moreUrl="https://woocommerce.com/document/woocommerce-shortcodes/#products"
-								tooltipLinkCallback={ () =>
-									recordEvent( 'add_product_learn_more', {
-										category: PRODUCT_DETAILS_SLUG,
-									} )
-								}
-							/>
+							<>
+								{ __( 'Feature this product', 'woocommerce' ) }
+								<Tooltip
+									text={ interpolateComponents( {
+										mixedString: __(
+											'Include this product in a featured section on your website with a widget or shortcode. {{moreLink/}}',
+											'woocommerce'
+										),
+										components: {
+											moreLink: (
+												<Link
+													href="https://woocommerce.com/document/woocommerce-shortcodes/#products"
+													target="_blank"
+													type="external"
+													onClick={ () =>
+														recordEvent(
+															'add_product_learn_more',
+															{
+																category:
+																	PRODUCT_DETAILS_SLUG,
+															}
+														)
+													}
+												>
+													{ __(
+														'Learn more',
+														'woocommerce'
+													) }
+												</Link>
+											),
+										},
+									} ) }
+								/>
+							</>
 						}
 						{ ...getCheckboxProps( {
 							...getInputProps( 'featured' ),
