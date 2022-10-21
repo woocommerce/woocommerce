43fab856b9c869a3c8765d00287c683c85b32017
rodel.calasagsag@automattic.com <rodel.calasagsag@automattic.com>
2022-10-22 00:34:27 +0800

    Add util functions for shipping

---
+++ plugins/woocommerce/tests/e2e-pw/utils/shipping.js
@@ -0,0 +1,29 @@
+const { Page } = require( '@playwright/test' );
+
+/**
+ * Delete shipping classes through the UI since we don't have API endpoints for shipping classes yet.
+ *
+ * @param { Page } adminPage
+ */
+const deleteAllShippingClasses = async ( adminPage ) => {
+	await adminPage.goto(
+		'wp-admin/admin.php?page=wc-settings&tab=shipping&section=classes'
+	);
+
+	let { length: count } = await adminPage.$$( '.wc-shipping-class-delete' );
+
+	if ( count === 0 ) {
+		return;
+	}
+
+	while ( count-- ) {
+		await adminPage.locator( '.wc-shipping-class-delete' ).first().click();
+	}
+
+	await adminPage.click( '.wc-shipping-class-save' );
+	await adminPage.waitForLoadState( 'networkidle' );
+};
+
+module.exports = {
+	deleteAllShippingClasses,
+};
