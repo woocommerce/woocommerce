1da7f5609ce613d735237216b55f62aa40a87f15
rodel.calasagsag@automattic.com <rodel.calasagsag@automattic.com>
2022-10-22 00:44:10 +0800

    Minor documentation

--- plugins/woocommerce/tests/e2e-pw/utils/shipping.js
+++ plugins/woocommerce/tests/e2e-pw/utils/shipping.js
@@ -6,16 +6,19 @@ const { Page } = require( '@playwright/test' );
  * @param { Page } adminPage
  */
 const deleteAllShippingClasses = async ( adminPage ) => {
+	// Navigate to Shipping classes page.
 	await adminPage.goto(
 		'wp-admin/admin.php?page=wc-settings&tab=shipping&section=classes'
 	);
 
+	// Count shipping classes.
 	let { length: count } = await adminPage.$$( '.wc-shipping-class-delete' );
 
 	if ( count === 0 ) {
 		return;
 	}
 
+	// Delete shipping classes.
 	while ( count-- ) {
 		await adminPage.dispatchEvent(
 			'.wc-shipping-class-delete >> nth=0',
