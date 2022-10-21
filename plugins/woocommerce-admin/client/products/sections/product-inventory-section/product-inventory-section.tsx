15e32b6de90aa436f7fc3f9cdd2449211f612a0f
Joshua Flowers <joshuatf@gmail.com>
2022-10-21 11:35:24 -0700

    Add wrapper around field to maintain block item formatting

--- plugins/woocommerce-admin/client/products/sections/product-inventory-section/product-inventory-section.tsx
+++ plugins/woocommerce-admin/client/products/sections/product-inventory-section/product-inventory-section.tsx
@@ -76,36 +76,38 @@ export const ProductInventorySection: React.FC = () => {
 						) }
 						{ ...getInputProps( 'sku' ) }
 					/>
-					<ConditionalWrapper
-						condition={ ! canManageStock }
-						wrapper={ ( children: JSX.Element ) => (
-							<Tooltip
-								text={ __(
-									'Quantity tracking is disabled for all products. Go to global store settings to change it.',
+					<div className="woocommerce-product-form__field">
+						<ConditionalWrapper
+							condition={ ! canManageStock }
+							wrapper={ ( children: JSX.Element ) => (
+								<Tooltip
+									text={ __(
+										'Quantity tracking is disabled for all products. Go to global store settings to change it.',
+										'woocommerce'
+									) }
+									position="top center"
+								>
+									<div className="woocommerce-product-form__tooltip-disabled-overlay">
+										{ children }
+									</div>
+								</Tooltip>
+							) }
+						>
+							<ToggleControl
+								label={ __(
+									'Track quantity for this product',
 									'woocommerce'
 								) }
-								position="top center"
-							>
-								<div className="woocommerce-product-form__tooltip-disabled-overlay">
-									{ children }
-								</div>
-							</Tooltip>
-						) }
-					>
-						<ToggleControl
-							label={ __(
-								'Track quantity for this product',
-								'woocommerce'
-							) }
-							{ ...getCheckboxControlProps(
-								'manage_stock',
-								getCheckboxTracks( 'manage_stock' )
-							) }
-							// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-							// @ts-ignore This prop does exist, but is not typed in @wordpress/components.
-							disabled={ ! canManageStock }
-						/>
-					</ConditionalWrapper>
+								{ ...getCheckboxControlProps(
+									'manage_stock',
+									getCheckboxTracks( 'manage_stock' )
+								) }
+								// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+								// @ts-ignore This prop does exist, but is not typed in @wordpress/components.
+								disabled={ ! canManageStock }
+							/>
+						</ConditionalWrapper>
+					</div>
 					{ values.manage_stock ? (
 						<ManageStockSection />
 					) : (
