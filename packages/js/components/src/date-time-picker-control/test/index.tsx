b0fd6d8e5623d1842da27bf810c84c51767765ee
Matt Sherman <matt@jam123.com>
2022-10-24 15:56:39 -0400

    Add tests for forceTimeTo

--- packages/js/components/src/date-time-picker-control/test/index.tsx
+++ packages/js/components/src/date-time-picker-control/test/index.tsx
@@ -343,6 +343,92 @@ describe( 'DateTimePickerControl', () => {
 	// tearing down the component while test microtasks are still being executed
 	// (see https://github.com/facebook/jest/issues/12670)
 	//       TypeError: Cannot read properties of null (reading 'createEvent')
+	it( 'should force time to the start of the day', async () => {
+		const originalDateTime = moment( '2022-09-15 02:30:40' );
+		const dateTimeFormat = 'HH:mm, MM-DD-YYYY';
+		const newDateTimeInputString = '02:04, 06-08-2010';
+		const newDateTime = moment(
+			newDateTimeInputString,
+			dateTimeFormat
+		).startOf( 'day' );
+		const onChangeHandler = jest.fn();
+
+		const { container } = render(
+			<DateTimePickerControl
+				dateTimeFormat={ dateTimeFormat }
+				forceTimeTo={ 'start-of-day' }
+				currentDate={ originalDateTime.toISOString() }
+				onChange={ onChangeHandler }
+				onChangeDebounceWait={ 10 }
+			/>
+		);
+
+		const input = container.querySelector( 'input' );
+		userEvent.type(
+			input!,
+			'{selectall}{backspace}' + newDateTimeInputString
+		);
+
+		await waitFor(
+			() =>
+				expect( onChangeHandler ).toHaveBeenLastCalledWith(
+					newDateTime.toISOString(),
+					true
+				),
+			{ timeout: 100 }
+		);
+	}, 10000 );
+
+	// We need to bump up the timeout for this test because:
+	//     1. userEvent.type() is slow (see https://github.com/testing-library/user-event/issues/577)
+	//     2. moment.js is slow
+	// Otherwise, the following error can occur on slow machines (such as our CI), because Jest times out and starts
+	// tearing down the component while test microtasks are still being executed
+	// (see https://github.com/facebook/jest/issues/12670)
+	//       TypeError: Cannot read properties of null (reading 'createEvent')
+	it( 'should force time to the end of the day', async () => {
+		const originalDateTime = moment( '2022-09-15 02:30:40' );
+		const dateTimeFormat = 'HH:mm, MM-DD-YYYY';
+		const newDateTimeInputString = '02:04, 06-08-2010';
+		const newDateTime = moment(
+			newDateTimeInputString,
+			dateTimeFormat
+		).endOf( 'day' );
+		const onChangeHandler = jest.fn();
+
+		const { container } = render(
+			<DateTimePickerControl
+				dateTimeFormat={ dateTimeFormat }
+				forceTimeTo={ 'end-of-day' }
+				currentDate={ originalDateTime.toISOString() }
+				onChange={ onChangeHandler }
+				onChangeDebounceWait={ 10 }
+			/>
+		);
+
+		const input = container.querySelector( 'input' );
+		userEvent.type(
+			input!,
+			'{selectall}{backspace}' + newDateTimeInputString
+		);
+
+		await waitFor(
+			() =>
+				expect( onChangeHandler ).toHaveBeenLastCalledWith(
+					newDateTime.toISOString(),
+					true
+				),
+			{ timeout: 100 }
+		);
+	}, 10000 );
+
+	// We need to bump up the timeout for this test because:
+	//     1. userEvent.type() is slow (see https://github.com/testing-library/user-event/issues/577)
+	//     2. moment.js is slow
+	// Otherwise, the following error can occur on slow machines (such as our CI), because Jest times out and starts
+	// tearing down the component while test microtasks are still being executed
+	// (see https://github.com/facebook/jest/issues/12670)
+	//       TypeError: Cannot read properties of null (reading 'createEvent')
 	it( 'should call onChange with isValid false when the input is invalid', async () => {
 		const originalDateTime = moment( '2022-09-15 02:30:40' );
 		const onChangeHandler = jest.fn();
