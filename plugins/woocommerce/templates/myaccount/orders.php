9fb53d09a2456fa71c88ae7430ca528a658c5a35
Peter Fabian <peter.fabian.github@gmail.com>
2022-10-25 10:26:23 +0200

    Buttons buttons my buttons!

--- plugins/woocommerce/templates/myaccount/orders.php
+++ plugins/woocommerce/templates/myaccount/orders.php
@@ -67,7 +67,7 @@ do_action( 'woocommerce_before_account_orders', $has_orders ); ?>
 
 								if ( ! empty( $actions ) ) {
 									foreach ( $actions as $key => $action ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-										echo '<a href="' . esc_url( $action['url'] ) . '" class="woocommerce-button button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
+										echo '<a href="' . esc_url( $action['url'] ) . '" class="woocommerce-button wp-element-button button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
 									}
 								}
 								?>
