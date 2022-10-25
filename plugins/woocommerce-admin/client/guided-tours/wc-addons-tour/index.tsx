8c2f73db986a877a73d41282dfc2c555739c18bf
timur987 <115007291+timur987@users.noreply.github.com>
2022-10-25 15:45:35 +0300

    Add base for the In-App Marketplace Tour (#35268)

---
+++ plugins/woocommerce-admin/client/guided-tours/wc-addons-tour/index.tsx
@@ -0,0 +1,76 @@
+/**
+ * External dependencies
+ */
+import { useEffect, useState } from '@wordpress/element';
+import { TourKit, TourKitTypes } from '@woocommerce/components';
+import qs from 'qs';
+
+/**
+ * Internal dependencies
+ */
+import { waitUntilElementTopNotChange } from '../utils';
+import { getTourConfig } from './get-config';
+
+const WCAddonsTour = () => {
+	const [ showTour, setShowTour ] = useState( false );
+	const [ tourConfig, setTourConfig ] = useState< TourKitTypes.WooConfig >();
+
+	const closeHandler: TourKitTypes.CloseHandler = (
+		steps,
+		currentStepIndex
+	) => {
+		setShowTour( false );
+		if ( steps.length - 1 === currentStepIndex ) {
+			// TODO: Track Tour as completed
+		} else {
+			// TODO: Track Tour as dismissed
+		}
+	};
+
+	const onNextStepHandler = ( stepIndex: number ) => {
+		// const stepName = tourConfig?.steps[ stepIndex ]?.meta?.name || '';
+		// TODO: Maybe scroll to a proper element
+		// TODO: Track tour's step as completed
+	};
+
+	const onPreviousStepHandler = ( stepIndex: number ) => {
+		// TODO: Maybe scroll to a proper element
+	};
+
+	const defaultAutoScrollBlock: ScrollLogicalPosition = 'center';
+
+	useEffect( () => {
+		const theConfig = getTourConfig( {
+			closeHandler,
+			onNextStepHandler,
+			onPreviousStepHandler,
+			autoScrollBlock: defaultAutoScrollBlock,
+		} );
+
+		setTourConfig( theConfig );
+
+		const query = qs.parse( window.location.search.slice( 1 ) );
+		if ( query?.tutorial === 'true' ) {
+			const intervalId = waitUntilElementTopNotChange(
+				theConfig.steps[ 0 ].referenceElements?.desktop || '',
+				() => {
+					setShowTour( true );
+
+					// TODO: track the tour is started
+				},
+				500
+			);
+			return () => clearInterval( intervalId );
+		}
+		// only run once
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [] );
+
+	if ( ! showTour || ! tourConfig ) {
+		return null;
+	}
+
+	return <TourKit config={ tourConfig } />;
+};
+
+export default WCAddonsTour;
