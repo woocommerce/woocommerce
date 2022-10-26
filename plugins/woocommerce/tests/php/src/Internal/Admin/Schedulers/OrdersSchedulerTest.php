9268b01246b5e84593fd8da0445f248ffe39a599
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2022-10-26 18:09:12 +0530

    test commit for ci.

--- plugins/woocommerce/tests/php/src/Internal/Admin/Schedulers/OrdersSchedulerTest.php
+++ plugins/woocommerce/tests/php/src/Internal/Admin/Schedulers/OrdersSchedulerTest.php
@@ -3,31 +3,21 @@
 /**
  * Test that orders are synced to stats table.
  */
-class OrdersSchedulerTest extends \WC_Unit_Test_Case {
+class OrdersSchedulerTest extends \WC_Admin_Tests_Reports_Orders_Stats {
 	use \Automattic\WooCommerce\RestApi\UnitTests\HPOSToggleTrait;
-
-	/**
-	 * @var WC_Admin_Tests_Reports_Orders_Stats Legacy order scheduler tests.
-	 */
-	private $legacy_order_scheduler_test;
-
-	/**
-	 * @var WC_Admin_Tests_API_Reports_Import Legace report importer test.
-	 */
-	private $legacy_report_importer_test;
 
 	/**
 	 * Setup HPOS.
 	 */
 	public function setUp(): void {
 		parent::setUp();
-		$this->legacy_order_scheduler_test = new WC_Admin_Tests_Reports_Orders_Stats();
-		$this->legacy_report_importer_test = new WC_Admin_Tests_API_Reports_Import();
+//		$this->legacy_order_scheduler_test = new WC_Admin_Tests_Reports_Orders_Stats();
+//		$this->legacy_report_importer_test = new WC_Admin_Tests_API_Reports_Import();
 		add_filter( 'woocommerce_analytics_report_should_use_cache', '__return_false' );
 		$this->setup_cot();
 		$this->disable_cot_sync();
 		$this->toggle_cot( true );
-		$this->legacy_report_importer_test->setUp();
+//		$this->legacy_report_importer_test->setUp();
 	}
 
 	/**
@@ -37,22 +27,15 @@ class OrdersSchedulerTest extends \WC_Unit_Test_Case {
 		$this->clean_up_cot_setup();
 		$this->toggle_cot( false );
 		remove_filter( 'woocommerce_analytics_report_should_use_cache', '__return_false' );
-		$this->legacy_report_importer_test->tearDown();
+//		$this->legacy_report_importer_test->tearDown();
 		parent::tearDown();
 	}
 
-	/**
-	 * Test that orders are synced to stats table with HPOS enabled.
-	 */
-	public function test_populate_and_query() {
-		$this->legacy_order_scheduler_test->test_populate_and_query();
-	}
-
-	/**
-	 * @testDox Test that orders are imported to stats table with HPOS enabled.
-	 */
-	public function test_import_params() {
-		$this->legacy_report_importer_test->test_import_params();
-	}
+//	/**
+//	 * @testDox Test that orders are imported to stats table with HPOS enabled.
+//	 */
+//	public function test_import_params() {
+//		$this->legacy_report_importer_test->test_import_params();
+//	}
 
 }
