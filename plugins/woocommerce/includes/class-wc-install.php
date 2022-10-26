46ccaa43182e5867cd6099c8963e040aa4721a96
Joshua Flowers <joshuatf@gmail.com>
2022-10-26 15:13:09 -0700

    Remove obsolete note

--- plugins/woocommerce/includes/class-wc-install.php
+++ plugins/woocommerce/includes/class-wc-install.php
@@ -828,6 +828,7 @@ class WC_Install {
 			'wc-admin-learn-more-about-product-settings',
 			'wc-admin-onboarding-profiler-reminder',
 			'wc-admin-historical-data',
+			'wc-admin-manage-store-activity-from-home-screen',
 			'wc-admin-review-shipping-settings',
 			'wc-admin-home-screen-feedback',
 			'wc-admin-effortless-payments-by-mollie',
