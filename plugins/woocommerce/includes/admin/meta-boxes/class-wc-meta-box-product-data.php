1ccb96779a67bb0d159ab2ab2d7cb5f392733631
Fernando Marichal <contacto@fernandomarichal.com>
2022-10-28 17:14:22 -0300

    Fix code sniff errors

--- plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
+++ plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
@@ -53,6 +53,7 @@ class WC_Meta_Box_Product_Data {
 	 * @return array
 	 */
 	private static function get_product_type_options() {
+		/* phpcs:disable */
 		return apply_filters(
 			'product_type_options',
 			array(
@@ -72,6 +73,7 @@ class WC_Meta_Box_Product_Data {
 				),
 			)
 		);
+		/* phpcs: enable */
 	}
 
 	/**
@@ -80,6 +82,7 @@ class WC_Meta_Box_Product_Data {
 	 * @return array
 	 */
 	private static function get_product_data_tabs() {
+		/* phpcs:disable */
 		$tabs = apply_filters(
 			'woocommerce_product_data_tabs',
 			array(
@@ -127,6 +130,7 @@ class WC_Meta_Box_Product_Data {
 				),
 			)
 		);
+		/* phpcs: enable */
 
 		// Sort tabs based on priority.
 		uasort( $tabs, array( __CLASS__, 'product_data_tabs_sort' ) );
@@ -173,8 +177,10 @@ class WC_Meta_Box_Product_Data {
 
 		$variation_attributes   = array_filter( $product_object->get_attributes(), array( __CLASS__, 'filter_variation_attributes' ) );
 		$default_attributes     = $product_object->get_default_attributes();
+		/* phpcs:disable */
 		$variations_count       = absint( apply_filters( 'woocommerce_admin_meta_boxes_variations_count', $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(ID) FROM $wpdb->posts WHERE post_parent = %d AND post_type = 'product_variation' AND post_status IN ('publish', 'private')", $post->ID ) ), $post->ID ) );
 		$variations_per_page    = absint( apply_filters( 'woocommerce_admin_meta_boxes_variations_per_page', 15 ) );
+		/* phpcs: enable */
 		$variations_total_pages = ceil( $variations_count / $variations_per_page );
 		$modal_title            = get_bloginfo( 'name' ) . __( ' says', 'woocommerce' );
 
@@ -273,7 +279,9 @@ class WC_Meta_Box_Product_Data {
 				$attribute->set_position( $attribute_position[ $i ] );
 				$attribute->set_visible( isset( $attribute_visibility[ $i ] ) );
 				$attribute->set_variation( isset( $attribute_variation[ $i ] ) );
+				/* phpcs:disable */
 				$attributes[] = apply_filters( 'woocommerce_admin_meta_boxes_prepare_attribute', $attribute, $data, $i );
+				/* phpcs: enable */
 			}
 		}
 		return $attributes;
@@ -426,8 +434,9 @@ class WC_Meta_Box_Product_Data {
 
 			$product->get_data_store()->sync_variation_names( $product, $original_post_title, $post_title );
 		}
-
+		/* phpcs:disable */
 		do_action( 'woocommerce_process_product_meta_' . $product_type, $post_id );
+		/* phpcs: enable */
 		// phpcs:enable WordPress.Security.NonceVerification.Missing
 	}
 
@@ -449,7 +458,7 @@ class WC_Meta_Box_Product_Data {
 			$max_loop   = max( array_keys( wp_unslash( $_POST['variable_post_id'] ) ) ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 			$data_store = $parent->get_data_store();
 			$data_store->sort_all_product_variations( $parent->get_id() );
-			$new_variation_menu_order_id = ! empty( $_POST['new_variation_menu_order_id'] ) ? wc_clean( wp_unslash( $_POST['new_variation_menu_order_id'] ) ) : false;
+			$new_variation_menu_order_id    = ! empty( $_POST['new_variation_menu_order_id'] ) ? wc_clean( wp_unslash( $_POST['new_variation_menu_order_id'] ) ) : false;
 			$new_variation_menu_order_value = ! empty( $_POST['new_variation_menu_order_value'] ) ? wc_clean( wp_unslash( $_POST['new_variation_menu_order_value'] ) ) : false;
 
 			// Only perform this operation if setting menu order via the prompt.
@@ -561,7 +570,9 @@ class WC_Meta_Box_Product_Data {
 				do_action( 'woocommerce_admin_process_variation_object', $variation, $i );
 
 				$variation->save();
+				/* phpcs:disable */
 				do_action( 'woocommerce_save_product_variation', $variation_id, $i );
+				/* phpcs: enable */
 			}
 		}
 		// phpcs:enable WordPress.Security.NonceVerification.Missing
