9d28ab23b11489d0433fe471c61ab5cde3c750e6
Peter Fabian <peter.fabian.github@gmail.com>
2022-10-28 18:01:40 +0200

    TT3 compatibility (#35306)

--- plugins/woocommerce/includes/wc-core-functions.php
+++ plugins/woocommerce/includes/wc-core-functions.php
@@ -2397,6 +2397,7 @@ function wc_is_active_theme( $theme ) {
 function wc_is_wp_default_theme_active() {
 	return wc_is_active_theme(
 		array(
+			'twentytwentythree',
 			'twentytwentytwo',
 			'twentytwentyone',
 			'twentytwenty',
