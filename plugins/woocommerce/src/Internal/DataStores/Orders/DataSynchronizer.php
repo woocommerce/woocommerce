199a38607822e4d8fba0b5aa2d61f282bfe015ed
barryhughes <3594411+barryhughes@users.noreply.github.com>
2022-10-31 11:52:17 -0700

    Woops! Restore use of `$wpdb->prepare()`, add phpcs:ignore rule.

--- plugins/woocommerce/src/Internal/DataStores/Orders/DataSynchronizer.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/DataSynchronizer.php
@@ -256,13 +256,17 @@ SELECT(
 		// phpcs:disable WordPress.DB.PreparedSQL.InterpolatedNotPrepared
 		switch ( $type ) {
 			case self::ID_TYPE_MISSING_IN_ORDERS_TABLE:
-				$sql = "
+				// phpcs:disable WordPress.DB.PreparedSQL.InterpolatedNotPrepared, WordPress.DB.PreparedSQLPlaceholders.UnfinishedPrepare -- $order_post_type_placeholders is prepared.
+				$sql = $wpdb->prepare(
+					"
 SELECT posts.ID FROM $wpdb->posts posts
 LEFT JOIN $orders_table orders ON posts.ID = orders.id
 WHERE
   posts.post_type IN ($order_post_type_placeholders)
   AND posts.post_status != 'auto-draft'
-  AND orders.id IS NULL";
+  AND orders.id IS NULL",
+					$order_post_types
+				);
 				break;
 			case self::ID_TYPE_MISSING_IN_POSTS_TABLE:
 				$sql = "
