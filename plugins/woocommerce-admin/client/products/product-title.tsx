ff73d057889229170fc7dfa039ef5c98acdc6f41
Joshua Flowers <joshuatf@gmail.com>
2022-10-31 14:17:31 -0700

    Create method to get product title

--- plugins/woocommerce-admin/client/products/product-title.tsx
+++ plugins/woocommerce-admin/client/products/product-title.tsx
@@ -2,19 +2,41 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import {
+	Product,
+	PRODUCTS_STORE_NAME,
+	WCDataSelector,
+} from '@woocommerce/data';
 import { registerPlugin } from '@wordpress/plugins';
 import { useFormContext } from '@woocommerce/components';
-import { Product } from '@woocommerce/data';
+import { useParams } from 'react-router-dom';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
+import { getProductTitle } from './utils/get-product-title';
 import { WooHeaderPageTitle } from '~/header/utils';
 
 export const ProductTitle: React.FC = () => {
 	const { values } = useFormContext< Product >();
+	const { productId } = useParams();
+	const { persistedName } = useSelect( ( select: WCDataSelector ) => {
+		const product = productId
+			? select( PRODUCTS_STORE_NAME ).getProduct(
+					parseInt( productId, 10 ),
+					undefined
+			  )
+			: null;
 
-	return <WooHeaderPageTitle>{ values.name }</WooHeaderPageTitle>;
+		return {
+			persistedName: product?.name,
+		};
+	} );
+
+	const title = getProductTitle( values.name, values.type, persistedName );
+
+	return <WooHeaderPageTitle>{ title }</WooHeaderPageTitle>;
 };
 
 registerPlugin( 'woocommerce-product-title', {
