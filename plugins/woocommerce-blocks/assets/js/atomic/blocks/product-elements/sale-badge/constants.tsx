2cf2eec76b6e0bc4e2ea1bc0ea27e979db390122
Niels Lange <info@nielslange.de>
2022-11-02 13:30:58 +0700

    Convert product-elements/sale-badge to TypeScript (https://github.com/woocommerce/woocommerce-blocks/pull/7543)

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/sale-badge/constants.tsx
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/sale-badge/constants.tsx
@@ -4,14 +4,14 @@
 import { __ } from '@wordpress/i18n';
 import { percent, Icon } from '@wordpress/icons';
 
-export const BLOCK_TITLE = __(
+export const BLOCK_TITLE: string = __(
 	'On-Sale Badge',
 	'woo-gutenberg-products-block'
 );
-export const BLOCK_ICON = (
+export const BLOCK_ICON: JSX.Element = (
 	<Icon icon={ percent } className="wc-block-editor-components-block-icon" />
 );
-export const BLOCK_DESCRIPTION = __(
+export const BLOCK_DESCRIPTION: string = __(
 	'Displays an on-sale badge if the product is on-sale.',
 	'woo-gutenberg-products-block'
 );
