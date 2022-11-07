12ae36f1871f0005f60325639c9d6cf9a459b753
Alba Rincón <alba.rincon@automattic.com>
2022-11-07 15:19:32 +0100

    Bump versions

--- plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
+++ plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
@@ -7,9 +7,9 @@
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain:  woo-gutenberg-products-block
- * Requires at least: 6.0
+ * Requires at least: 6.1
  * Requires PHP: 7.0
- * WC requires at least: 6.8
+ * WC requires at least: 6.9
  * WC tested up to: 6.9
  *
  * @package WooCommerce\Blocks
@@ -18,7 +18,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$minimum_wp_version = '6.0';
+$minimum_wp_version = '6.1';
 
 if ( ! defined( 'WC_BLOCKS_IS_FEATURE_PLUGIN' ) ) {
 	define( 'WC_BLOCKS_IS_FEATURE_PLUGIN', true );
