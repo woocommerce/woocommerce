3e6aaf47b38d31fc43501b6bb67a6487bf0e8b50
Thomas Roberts <5656702+opr@users.noreply.github.com>
2022-11-07 10:48:27 +0000

    Fix E2E errors with debounced `updatePaymentMethods` function (https://github.com/woocommerce/woocommerce-blocks/pull/7507)
    * Add non-debounced versions of updatePaymentMethods
    
    * Call undebounced function until store is ready then call debounced
    
    * Reset debounce timer to 1 second
    
    * Add better comments
    
    * Try setting the trail setting of the debounce function
    
    * Revert "Try setting the trail setting of the debounce function"
    
    This reverts commit 040fb63cb612c19ac396396d1949b6429f402616.
    
    Co-authored-by: Alex Florisca <alex.florisca@automattic.com>

--- plugins/woocommerce-blocks/assets/js/data/cart/update-payment-methods.ts
+++ plugins/woocommerce-blocks/assets/js/data/cart/update-payment-methods.ts
@@ -10,15 +10,26 @@ import { debounce } from 'lodash';
 import { STORE_KEY as PAYMENT_STORE_KEY } from '../payment/constants';
 import { STORE_KEY } from './constants';
 
-export const updatePaymentMethods = debounce( async () => {
+/**
+ * This function is used to update payment methods when the cart changes, or on first load.
+ *
+ * @return {boolean} True if the __internalUpdateAvailablePaymentMethods action was dispatched, false if not.
+ */
+export const updatePaymentMethods = async () => {
 	const isInitialized =
 		select( STORE_KEY ).hasFinishedResolution( 'getCartData' );
-
 	if ( ! isInitialized ) {
-		return;
+		return false;
 	}
-
 	await dispatch(
 		PAYMENT_STORE_KEY
 	).__internalUpdateAvailablePaymentMethods();
-}, 1000 );
+	return true;
+};
+
+// We debounce this because it's possible for multiple cart updates to happen in quick succession, we don't want to run
+// each payment method's canMakePayment function on every single change.
+export const debouncedUpdatePaymentMethods = debounce(
+	updatePaymentMethods,
+	1000
+);
