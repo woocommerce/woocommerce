76388bdca01ee4cf66fda5d64f2cb19b551ed670
Thomas Roberts <5656702+opr@users.noreply.github.com>
2022-11-07 10:48:27 +0000

    Fix E2E errors with debounced `updatePaymentMethods` function (https://github.com/woocommerce/woocommerce-blocks/pull/7507)
    * Add non-debounced versions of updatePaymentMethods
    
    * Call undebounced function until store is ready then call debounced
    
    * Reset debounce timer to 1 second
    
    * Add better comments
    
    * Try setting the trail setting of the debounce function
    
    * Revert "Try setting the trail setting of the debounce function"
    
    This reverts commit 040fb63cb612c19ac396396d1949b6429f402616.
    
    Co-authored-by: Alex Florisca <alex.florisca@automattic.com>

--- plugins/woocommerce-blocks/assets/js/data/cart/index.ts
+++ plugins/woocommerce-blocks/assets/js/data/cart/index.ts
@@ -16,7 +16,10 @@ import { controls as sharedControls } from '../shared-controls';
 import { controls } from './controls';
 import type { SelectFromMap, DispatchFromMap } from '../mapped-types';
 import { pushChanges } from './push-changes';
-import { updatePaymentMethods } from './update-payment-methods';
+import {
+	updatePaymentMethods,
+	debouncedUpdatePaymentMethods,
+} from './update-payment-methods';
 
 const registeredStore = registerStore< State >( STORE_KEY, {
 	reducer,
@@ -28,7 +31,20 @@ const registeredStore = registerStore< State >( STORE_KEY, {
 } );
 
 registeredStore.subscribe( pushChanges );
-registeredStore.subscribe( updatePaymentMethods );
+
+// First we will run the updatePaymentMethods function without any debounce to ensure payment methods are ready as soon
+// as the cart is loaded. After that, we will unsubscribe this function and instead run the
+// debouncedUpdatePaymentMethods function on subsequent cart updates.
+const unsubscribeUpdatePaymentMethods = registeredStore.subscribe( async () => {
+	const didActionDispatch = await updatePaymentMethods();
+	if ( didActionDispatch ) {
+		// The function we're currently in will unsubscribe itself. When we reach this line, this will be the last time
+		// this function is called.
+		unsubscribeUpdatePaymentMethods();
+		// Resubscribe, but with the debounced version of updatePaymentMethods.
+		registeredStore.subscribe( debouncedUpdatePaymentMethods );
+	}
+} );
 
 export const CART_STORE_KEY = STORE_KEY;
 
