fc4ce6dac6ff2dc1060ce626dea6fb6b969396da
Tung Du <dinhtungdu@gmail.com>
2022-11-09 08:53:26 +0700

    E2E: Product Query: Testing atomic blocks (https://github.com/woocommerce/woocommerce-blocks/pull/7482)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ plugins/woocommerce-blocks/tests/utils/selectors.ts
@@ -0,0 +1,7 @@
+export default {
+	quickInserter: {
+		button: '.block-editor-inserter button',
+		searchInput:
+			'.block-editor-inserter__quick-inserter.has-search.has-expand .components-search-control__input',
+	},
+};
