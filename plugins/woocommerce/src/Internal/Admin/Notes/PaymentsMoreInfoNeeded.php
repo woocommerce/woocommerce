7192a66abad3ea525dcefcafea806d68d8593d1d
Daniel Mallory <daniel.mallory@automattic.com>
2022-11-14 18:02:29 +0000

    Add changelog entry, update to logic when dispalying note

--- plugins/woocommerce/src/Internal/Admin/Notes/PaymentsMoreInfoNeeded.php
+++ plugins/woocommerce/src/Internal/Admin/Notes/PaymentsMoreInfoNeeded.php
@@ -12,7 +12,7 @@ use Automattic\WooCommerce\Admin\PluginsHelper;
 defined( 'ABSPATH' ) || exit;
 
 /**
- * PaymentsRemindMeLater
+ * PaymentsMoreInfoNeeded
  */
 class PaymentsMoreInfoNeeded {
 	/**
@@ -38,13 +38,11 @@ class PaymentsMoreInfoNeeded {
 	 * @return bool
 	 */
 	public static function should_display_note() {
-		return true;
-
 		// If user has installed WCPay, don't show this note.
 		$installed_plugins = PluginsHelper::get_installed_plugin_slugs();
-		// if ( in_array( 'woocommerce-payments', $installed_plugins, true ) ) {
-		// 	return false;
-		// }
+		 if ( in_array( 'woocommerce-payments', $installed_plugins, true ) ) {
+		 	return false;
+		 }
 
 		// User has dismissed the WCPay Welcome Page.
 		if ( 'yes' !== get_option( 'wc_calypso_bridge_payments_dismissed', 'no' ) ) {
@@ -54,7 +52,6 @@ class PaymentsMoreInfoNeeded {
 		// More than 30 days since viewing the welcome page.
 		$exit_survey_timestamp = get_option( 'wc_pay_exit_survey_more_info_needed_timestamp', false );
 		if ( ! $exit_survey_timestamp ||
-			// TODO: FLIP THE IF STATEMENT
 			( time() - $exit_survey_timestamp < 30 * DAY_IN_SECONDS )
 		) {
 			return false;
