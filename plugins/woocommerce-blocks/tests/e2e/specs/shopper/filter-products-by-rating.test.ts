f94fee9b7db1f1b47791796d302f070a123e6e8d
Luigi Teschio <gigitux@gmail.com>
2022-11-18 16:44:39 +0100

    Product Query - Add e2e tests for the filter by price block (https://github.com/woocommerce/woocommerce-blocks/pull/7351)
    * Product Query - Add e2e tests for the Filter By Price block
    
    * fix comment
    
    * remove not used import
    
    * update description
    
    * update functions after merge
    
    * improve check

--- plugins/woocommerce-blocks/tests/e2e/specs/shopper/filter-products-by-rating.test.ts
+++ plugins/woocommerce-blocks/tests/e2e/specs/shopper/filter-products-by-rating.test.ts
@@ -157,7 +157,7 @@ describe( `${ block.name } Block`, () => {
 
 			await waitForCanvas();
 			await selectBlockByName( block.slug );
-			await openBlockEditorSettings( { isFSEEditor: true } );
+			await openBlockEditorSettings();
 			await page.waitForXPath(
 				block.selectors.editor.filterButtonToggle
 			);
