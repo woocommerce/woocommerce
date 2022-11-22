227a34aa40c7c0045245586ddad8044677cb542d
Sam Seay <samueljseay@gmail.com>
2022-11-22 17:09:27 +1300

    Fix some lint issues

--- plugins/woocommerce-beta-tester/api/live-branches/manifest.php
+++ plugins/woocommerce-beta-tester/api/live-branches/manifest.php
@@ -8,6 +8,9 @@ register_woocommerce_admin_test_helper_rest_route(
 	)
 );
 
+/**
+ * API endpoint to fetch the manifest of live branches.
+ */
 function fetch_live_branches_manifest() {
 	$response = wp_remote_get( 'https://betadownload.jetpack.me/woocommerce-branches.json' );
 	$body     = wp_remote_retrieve_body( $response );
