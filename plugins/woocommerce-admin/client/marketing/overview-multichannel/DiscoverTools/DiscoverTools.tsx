768817d1b316695c5d82065ec6d6439238f5df7e
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-11-23 00:00:39 +0800

    Code refactor with PluginsTabPanel.

--- plugins/woocommerce-admin/client/marketing/overview-multichannel/DiscoverTools/DiscoverTools.tsx
+++ plugins/woocommerce-admin/client/marketing/overview-multichannel/DiscoverTools/DiscoverTools.tsx
@@ -1,101 +1,30 @@
 /**
  * External dependencies
  */
-import { Fragment, useState } from '@wordpress/element';
-import { useDispatch } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import { TabPanel, Button } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 import { Icon, trendingUp } from '@wordpress/icons';
 import { recordEvent } from '@woocommerce/tracks';
-import { Pill, Spinner } from '@woocommerce/components';
-import { PLUGINS_STORE_NAME } from '@woocommerce/data';
-import { flatMapDeep, uniqBy } from 'lodash';
+import { Spinner } from '@woocommerce/components';
 
 /**
  * Internal dependencies
  */
-import {
-	CollapsibleCard,
-	CardDivider,
-	CardBody,
-	PluginCardBody,
-} from '~/marketing/components';
-import { useInstalledPlugins } from '~/marketing/hooks';
-import { getInAppPurchaseUrl } from '~/lib/in-app-purchase';
-import { createNoticesFromResponse } from '~/lib/notices';
-import { Plugin } from './types';
+import { CollapsibleCard, CardBody } from '~/marketing/components';
 import { useRecommendedPlugins } from './useRecommendedPlugins';
+import { PluginsTabPanel } from './PluginsTabPanel';
 import './DiscoverTools.scss';
-
-/**
- * Return tabs (`{ name, title }`) for the TabPanel.
- *
- * Subcategories that have no plugins
- * will not be displayed as a tab in the UI.
- * This is done by doing the following:
- *
- * 1. Get an array of unique subcategories from the list of plugins.
- * 2. Map the subcategories schema into tabs schema.
- */
-const getTabs = ( plugins: Plugin[] ) => {
-	const pluginSubcategories = uniqBy(
-		flatMapDeep( plugins, ( p ) => p.subcategories ),
-		( subcategory ) => subcategory.slug
-	);
-
-	return pluginSubcategories.map( ( subcategory ) => ( {
-		name: subcategory.slug,
-		title: subcategory.name,
-	} ) );
-};
 
 export const DiscoverTools = () => {
 	const { isInitializing, isLoading, plugins, refetch } =
 		useRecommendedPlugins();
-	const [ currentPlugin, setCurrentPlugin ] = useState< string | null >(
-		null
-	);
-	const { installAndActivatePlugins } = useDispatch( PLUGINS_STORE_NAME );
-	const { loadInstalledPluginsAfterActivation } = useInstalledPlugins();
-
-	/**
-	 * Install and activate a plugin.
-	 *
-	 * When the process is successful, the plugin will disappear in the recommended list,
-	 * and appear in the installed extension list. A success notice will be displayed.
-	 *
-	 * When the process is not successful, an error notice will be displayed.
-	 *
-	 * @param  plugin Plugin to be installed and activated.
-	 */
-	const installAndActivate = async ( plugin: Plugin ) => {
-		setCurrentPlugin( plugin.product );
-
-		try {
-			recordEvent( 'marketing_recommended_extension', {
-				name: plugin.title,
-			} );
-
-			const response = await installAndActivatePlugins( [
-				plugin.product,
-			] );
-
-			refetch();
-			loadInstalledPluginsAfterActivation( plugin.product );
-			createNoticesFromResponse( response );
-		} catch ( error ) {
-			createNoticesFromResponse( error );
-		}
-
-		setCurrentPlugin( null );
-	};
 
 	/**
 	 * Renders card body.
 	 *
 	 * - If loading is in progress, it renders a loading indicator.
 	 * - If there are zero plugins, it renders an empty content.
-	 * - Otherwise, it renders a TabPanel. Each tab is a subcategory displaying the plugins.
+	 * - Otherwise, it renders PluginsTabPanel.
 	 */
 	const renderCardContent = () => {
 		if ( isInitializing ) {
@@ -133,92 +62,11 @@ export const DiscoverTools = () => {
 		}
 
 		return (
-			<TabPanel tabs={ getTabs( plugins ) }>
-				{ ( tab ) => {
-					const subcategoryPlugins = plugins.filter( ( plugin ) =>
-						plugin.subcategories.some(
-							( subcategory ) => subcategory.slug === tab.name
-						)
-					);
-					const buttonDisabled = !! currentPlugin || isLoading;
-
-					return (
-						<>
-							{ subcategoryPlugins.map( ( plugin ) => {
-								return (
-									<Fragment key={ plugin.product }>
-										<CardDivider />
-										<PluginCardBody
-											icon={
-												<img
-													src={ plugin.icon }
-													alt={ plugin.title }
-												/>
-											}
-											name={ plugin.title }
-											pills={ plugin.tags.map(
-												( tag ) => (
-													<Pill key={ tag.slug }>
-														{ tag.name }
-													</Pill>
-												)
-											) }
-											description={ plugin.description }
-											button={
-												plugin.direct_install ? (
-													<Button
-														variant="secondary"
-														isBusy={
-															currentPlugin ===
-															plugin.product
-														}
-														disabled={
-															buttonDisabled
-														}
-														onClick={ () => {
-															installAndActivate(
-																plugin
-															);
-														} }
-													>
-														{ __(
-															'Install plugin',
-															'woocommerce'
-														) }
-													</Button>
-												) : (
-													<Button
-														variant="secondary"
-														href={ getInAppPurchaseUrl(
-															plugin.url
-														) }
-														disabled={
-															buttonDisabled
-														}
-														onClick={ () => {
-															recordEvent(
-																'marketing_recommended_extension',
-																{
-																	name: plugin.title,
-																}
-															);
-														} }
-													>
-														{ __(
-															'View details',
-															'woocommerce'
-														) }
-													</Button>
-												)
-											}
-										/>
-									</Fragment>
-								);
-							} ) }
-						</>
-					);
-				} }
-			</TabPanel>
+			<PluginsTabPanel
+				plugins={ plugins }
+				isLoading={ isLoading }
+				onRefetch={ refetch }
+			/>
 		);
 	};
 
