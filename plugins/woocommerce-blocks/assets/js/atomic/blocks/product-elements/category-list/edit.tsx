de5c426e8abfa6e8053dce8777837bbbe1445c32
Manish Menaria <the.manish.menaria@gmail.com>
2022-11-28 11:47:27 +0530

    Adds product query support for Category list block (https://github.com/woocommerce/woocommerce-blocks/pull/7675)
    * Adds product query support for Category list block
    
    On the client side, when the Category list block is used within the
    product query bloc, the markup will be rendered on the server side -
    No javascript related to Category list block will be rendered.
    
    * bot: update checkstyle.xml
    
    * Fix extra space after the category link
    
    I used the solution from following link: https://css-tricks.com/fighting-the-space-between-inline-block-elements/#aa-remove-the-spaces
    
    Here is the content from the above link:
    
    The reason you get the spaces is because, well, you have spaces between the elements (a line break and a few tabs counts as a space, just to be clear). Minimized HTML will solve this problem, or one of these tricks:
    
    ```CSS
    <ul>
      <li>
       one</li><li>
       two</li><li>
       three</li>
    </ul>
    ```
    
    or
    
    ```CSS
    <ul>
      <li>one</li
      ><li>two</li
      ><li>three</li>
    </ul>
    ```
    
    or with comments…
    
    ```CSS
    <ul>
      <li>one</li><!--
      --><li>two</li><!--
      --><li>three</li>
    </ul>
    ```
    
    They’re all pretty funky, but it does the trick.
    
    * Fix custom style doesn't work on the frontend side
    
    For more info, check this comment on the PR:
    https://github.com/woocommerce/woocommerce-blocks/pull/7675#pullrequestreview-1179267957
    
    In summary, user can set the custom styles like text color, link color, font size, font weight, line height using the editor sidebar.
    These styles weren't showing on the frontend side.
    
    * Add support for additional CSS class(es)
    
    * Fix "custom style -> link color" not working
    
    To understand the issue in more details please check following comment:
    https://github.com/woocommerce/woocommerce-blocks/pull/7675#issuecomment-1319822535
    
    * Fix classname undefined issue
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/category-list/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/category-list/edit.tsx
@@ -2,9 +2,12 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import type { BlockEditProps } from '@wordpress/blocks';
+import { ProductQueryContext as Context } from '@woocommerce/blocks/product-query/types';
 import { Disabled } from '@wordpress/components';
 import EditProductLink from '@woocommerce/editor-components/edit-product-link';
 import { useBlockProps } from '@wordpress/block-editor';
+import { useEffect } from 'react';
 
 /**
  * Internal dependencies
@@ -14,18 +17,29 @@ import withProductSelector from '../shared/with-product-selector';
 import { BLOCK_TITLE, BLOCK_ICON } from './constants';
 import { Attributes } from './types';
 
-interface Props {
-	attributes: Attributes;
-}
-
-const Edit = ( { attributes }: Props ): JSX.Element => {
+const Edit = ( {
+	attributes,
+	setAttributes,
+	context,
+}: BlockEditProps< Attributes > & { context: Context } ): JSX.Element => {
 	const blockProps = useBlockProps();
+
+	const blockAttrs = {
+		...attributes,
+		...context,
+	};
+	const isDescendentOfQueryLoop = Number.isFinite( context.queryId );
+
+	useEffect(
+		() => setAttributes( { isDescendentOfQueryLoop } ),
+		[ setAttributes, isDescendentOfQueryLoop ]
+	);
 
 	return (
 		<div { ...blockProps }>
 			<EditProductLink />
 			<Disabled>
-				<Block { ...attributes } />
+				<Block { ...blockAttrs } />
 			</Disabled>
 		</div>
 	);
