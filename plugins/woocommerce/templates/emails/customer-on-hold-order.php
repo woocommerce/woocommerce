3d46510f76d130a68bd97dc262e9e3a718e75b3d
Barry Hughes <3594411+barryhughes@users.noreply.github.com>
2022-11-30 12:34:11 -0800

    Update version tag to 7.3.0.

--- plugins/woocommerce/templates/emails/customer-on-hold-order.php
+++ plugins/woocommerce/templates/emails/customer-on-hold-order.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
- * @version 7.1.0
+ * @version 7.3.0
  */
 
 defined( 'ABSPATH' ) || exit;
