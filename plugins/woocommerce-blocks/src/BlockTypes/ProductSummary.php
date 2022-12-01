d48c7ba0ba9cb2b09cf2754eed20a06669b25030
Manish Menaria <the.manish.menaria@gmail.com>
2022-12-01 17:37:41 +0530

    Add product query support for Product Summary block (https://github.com/woocommerce/woocommerce-blocks/pull/7774)
    * Add product query support for Product Summary block
    
    On the client side, when the Product Summary block is used within the product query block, the markup will be rendered on the server side - No javascript related to Product Summary block will be rendered.
    
    * Update variable names for clarity & readability
    
    * Escape all values in output string
    
    * Fix custom style not working
    
    More info: https://github.com/woocommerce/woocommerce-blocks/pull/7774#discussion_r1035909243

--- plugins/woocommerce-blocks/src/BlockTypes/ProductSummary.php
+++ plugins/woocommerce-blocks/src/BlockTypes/ProductSummary.php
@@ -1,6 +1,8 @@
 <?php
 namespace Automattic\WooCommerce\Blocks\BlockTypes;
 
+use Automattic\WooCommerce\Blocks\Utils\StyleAttributesUtils;
+
 /**
  * ProductSummary class.
  */
@@ -51,5 +53,71 @@ class ProductSummary extends AbstractBlock {
 	protected function register_block_type_assets() {
 		parent::register_block_type_assets();
 		$this->register_chunk_translations( [ $this->block_name ] );
+		return null;
+	}
+
+	/**
+	 * Register the context.
+	 */
+	protected function get_block_type_uses_context() {
+		return [ 'query', 'queryId', 'postId' ];
+	}
+
+	/**
+	 * Include and render the block.
+	 *
+	 * @param array    $attributes Block attributes. Default empty array.
+	 * @param string   $content    Block content. Default empty string.
+	 * @param WP_Block $block      Block instance.
+	 * @return string Rendered block type output.
+	 */
+	protected function render( $attributes, $content, $block ) {
+		if ( ! empty( $content ) ) {
+			parent::register_block_type_assets();
+			$this->register_chunk_translations( [ $this->block_name ] );
+			return $content;
+		}
+
+		$post_id         = $block->context['postId'];
+		$product         = wc_get_product( $post_id );
+		$summary_content = self::get_summary_content( $product );
+
+		if ( ! $summary_content ) {
+			return '';
+		}
+
+		$classes_and_styles = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes );
+		$classname          = isset( $attributes['className'] ) ? $attributes['className'] : '';
+
+		$output = '<div class="wc-block-components-product-summary '
+			. esc_attr( $classes_and_styles['classes'] ) . ' ' . esc_attr( $classname ) . '"';
+
+		if ( isset( $classes_and_styles['styles'] ) ) {
+			$output .= ' style="' . esc_attr( $classes_and_styles['styles'] ) . '"';
+		}
+
+		$output .= '>';
+		$output .= '<p>' . wp_kses_post( $summary_content ) . '</p>';
+		$output .= '</div>';
+
+		return $output;
+	}
+
+	/**
+	 * Returns summary text for a product.
+	 *
+	 * @param [WC_Product] $product Product to get summary text for.
+	 * @return (string|null) Summary text for the product.
+	 */
+	protected function get_summary_content( $product ) {
+		$short_description = $product->get_short_description();
+		$summary_content   = $short_description ? $short_description : $product->get_description();
+
+		if ( ! $summary_content ) {
+			return null;
+		}
+
+		$summary = wc_trim_string( $summary_content, 150 );
+		return $summary;
 	}
 }
