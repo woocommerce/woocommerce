6f87dd8d4414a14e6eca669ab5f3b307b9860d13
Maikel David Pérez Gómez <mdperez86@gmail.com>
2022-12-02 19:51:27 -0300

    Add async filter hook to attribute input field

--- plugins/woocommerce-admin/client/products/fields/attribute-input-field/attribute-input-field.tsx
+++ plugins/woocommerce-admin/client/products/fields/attribute-input-field/attribute-input-field.tsx
@@ -2,17 +2,19 @@
  * External dependencies
  */
 import { sprintf, __ } from '@wordpress/i18n';
-import { useSelect } from '@wordpress/data';
+import { resolveSelect } from '@wordpress/data';
+import { useCallback, useEffect, useState } from '@wordpress/element';
 import { Spinner, Icon } from '@wordpress/components';
 import { plus } from '@wordpress/icons';
 import {
 	EXPERIMENTAL_PRODUCT_ATTRIBUTES_STORE_NAME,
 	QueryProductAttribute,
 	ProductAttribute,
-	WCDataSelector,
 } from '@woocommerce/data';
 import { recordEvent } from '@woocommerce/tracks';
 import {
+	SelectControlProps,
+	useAsyncFilter,
 	__experimentalSelectControl as SelectControl,
 	__experimentalSelectControlMenu as Menu,
 	__experimentalSelectControlMenuItem as MenuItem,
@@ -43,6 +45,52 @@ function isNewAttributeListItem( attribute: NarrowedQueryAttribute ): boolean {
 	return attribute.id === -99;
 }
 
+/**
+ * Add a new item at the end of the list if the
+ * current set of items do not match exactly the
+ * input's value
+ *
+ * @param allItems The item list
+ * @param inputValue The input's value
+ * @param ignoredAttributeIds The item ids to ignore
+ * @return The `allItems` + 1 new item if condition is met
+ */
+function addCreateNewAttributeItem(
+	allItems: NarrowedQueryAttribute[] = [],
+	inputValue: string,
+	ignoredAttributeIds: number[]
+) {
+	const ignoreIdsFilter = ( item: NarrowedQueryAttribute ) =>
+		ignoredAttributeIds.length
+			? ! ignoredAttributeIds.includes( item.id )
+			: true;
+
+	const filteredItems = allItems.filter(
+		( item ) =>
+			ignoreIdsFilter( item ) &&
+			( item.name || '' )
+				.toLowerCase()
+				.startsWith( inputValue.toLowerCase() )
+	);
+
+	if (
+		inputValue.length > 0 &&
+		! allItems.find(
+			( item ) => item.name.toLowerCase() === inputValue.toLowerCase()
+		)
+	) {
+		return [
+			...filteredItems,
+			{
+				id: -99,
+				name: inputValue,
+			},
+		];
+	}
+
+	return filteredItems;
+}
+
 export const AttributeInputField: React.FC< AttributeInputFieldProps > = ( {
 	value = null,
 	onChange,
@@ -51,80 +99,81 @@ export const AttributeInputField: React.FC< AttributeInputFieldProps > = ( {
 	disabled,
 	ignoredAttributeIds = [],
 } ) => {
-	const { attributes, isLoading } = useSelect( ( select: WCDataSelector ) => {
-		const { getProductAttributes, hasFinishedResolution } = select(
-			EXPERIMENTAL_PRODUCT_ATTRIBUTES_STORE_NAME
-		);
-		return {
-			isLoading: ! hasFinishedResolution( 'getProductAttributes' ),
-			attributes: getProductAttributes(),
-		};
-	} );
+	const [ items, setItems ] = useState< NarrowedQueryAttribute[] >( [] );
+	const [ isLoading, setIsLoading ] = useState( false );
 
-	const getFilteredItems = (
-		allItems: NarrowedQueryAttribute[],
-		inputValue: string
-	) => {
-		const ignoreIdsFilter = ( item: NarrowedQueryAttribute ) =>
-			ignoredAttributeIds.length
-				? ! ignoredAttributeIds.includes( item.id )
-				: true;
+	const filter = useCallback(
+		async ( searchValue = '' ) => {
+			return resolveSelect( EXPERIMENTAL_PRODUCT_ATTRIBUTES_STORE_NAME )
+				.getProductAttributes< NarrowedQueryAttribute[] >()
+				.then( ( attributes ) => {
+					return addCreateNewAttributeItem(
+						attributes,
+						searchValue,
+						ignoredAttributeIds
+					);
+				} );
+		},
+		[ ignoredAttributeIds ]
+	);
 
-		const filteredItems = allItems.filter(
-			( item ) =>
-				ignoreIdsFilter( item ) &&
-				( item.name || '' )
-					.toLowerCase()
-					.startsWith( inputValue.toLowerCase() )
-		);
+	useEffect( () => {
+		setIsLoading( true );
+		filter()
+			.then( setItems )
+			.finally( () => {
+				setIsLoading( false );
+			} );
+	}, [] );
 
-		if (
-			inputValue.length > 0 &&
-			! allItems.find(
-				( item ) => item.name.toLowerCase() === inputValue.toLowerCase()
-			)
-		) {
-			return [
-				...filteredItems,
-				{
-					id: -99,
-					name: inputValue,
-				},
-			];
-		}
-
-		return filteredItems;
+	const selectProps: SelectControlProps< NarrowedQueryAttribute > = {
+		className: 'woocommerce-attribute-input-field',
+		items,
+		label: label || '',
+		disabled,
+		placeholder,
+		getItemLabel: ( item: NarrowedQueryAttribute | null ) =>
+			item?.name || '',
+		getItemValue: ( item: NarrowedQueryAttribute | null ) => item?.id || '',
+		selected: value,
+		onSelect: ( attribute: NarrowedQueryAttribute ) => {
+			if ( isNewAttributeListItem( attribute ) ) {
+				recordEvent( 'product_attribute_add_custom_attribute', {
+					new_product_page: true,
+				} );
+			}
+			onChange(
+				isNewAttributeListItem( attribute )
+					? attribute.name
+					: {
+							id: attribute.id,
+							name: attribute.name,
+							options: [],
+					  }
+			);
+		},
+		onRemove: () => onChange(),
+		__experimentalOpenMenuOnFocus: true,
 	};
+
+	const selectPropsWithSyncFilter = useAsyncFilter< NarrowedQueryAttribute >(
+		{
+			...selectProps,
+			filter,
+			onFilterStart() {
+				setIsLoading( true );
+				setItems( [] );
+			},
+			onFilterEnd( filteredItems: NarrowedQueryAttribute[] ) {
+				setItems( filteredItems );
+				setIsLoading( false );
+			},
+		}
+	);
 
 	return (
 		<SelectControl< NarrowedQueryAttribute >
-			className="woocommerce-attribute-input-field"
-			items={ attributes || [] }
-			label={ label || '' }
-			disabled={ disabled }
-			getFilteredItems={ getFilteredItems }
-			placeholder={ placeholder }
-			getItemLabel={ ( item ) => item?.name || '' }
-			getItemValue={ ( item ) => item?.id || '' }
-			selected={ value }
-			onSelect={ ( attribute ) => {
-				if ( isNewAttributeListItem( attribute ) ) {
-					recordEvent( 'product_attribute_add_custom_attribute', {
-						new_product_page: true,
-					} );
-				}
-				onChange(
-					isNewAttributeListItem( attribute )
-						? attribute.name
-						: {
-								id: attribute.id,
-								name: attribute.name,
-								options: [],
-						  }
-				);
-			} }
-			onRemove={ () => onChange() }
-			__experimentalOpenMenuOnFocus
+			{ ...selectPropsWithSyncFilter }
 		>
 			{ ( {
 				items: renderItems,
--- plugins/woocommerce-admin/client/products/fields/attribute-input-field/test/attribute-input-field.spec.tsx
+++ plugins/woocommerce-admin/client/products/fields/attribute-input-field/test/attribute-input-field.spec.tsx
@@ -1,9 +1,9 @@
 /**
  * External dependencies
  */
-import { render } from '@testing-library/react';
-import { useSelect } from '@wordpress/data';
-import { useState } from '@wordpress/element';
+import { act, render, screen } from '@testing-library/react';
+import { resolveSelect } from '@wordpress/data';
+import { useEffect, useState } from '@wordpress/element';
 import { ProductAttribute, QueryProductAttribute } from '@woocommerce/data';
 
 /**
@@ -13,7 +13,7 @@ import { AttributeInputField } from '../attribute-input-field';
 
 jest.mock( '@wordpress/data', () => ( {
 	...jest.requireActual( '@wordpress/data' ),
-	useSelect: jest.fn(),
+	resolveSelect: jest.fn(),
 } ) );
 
 jest.mock( '@wordpress/components', () => ( {
@@ -48,9 +48,9 @@ jest.mock( '@woocommerce/components', () => {
 		__experimentalSelectControl: ( {
 			children,
 			items,
-			getFilteredItems,
 			onSelect,
 			onRemove,
+			onInputChange,
 		}: {
 			children: ( options: {
 				isOpen: boolean;
@@ -63,13 +63,14 @@ jest.mock( '@woocommerce/components', () => {
 			items: QueryProductAttribute[];
 			onSelect: ( item: QueryProductAttribute ) => void;
 			onRemove: ( item: QueryProductAttribute ) => void;
-			getFilteredItems: (
-				allItems: QueryProductAttribute[],
-				inputValue: string,
-				selectedItems: QueryProductAttribute[]
-			) => QueryProductAttribute[];
+			onInputChange?: ( inputValue?: string ) => void;
 		} ) => {
 			const [ input, setInput ] = useState( '' );
+
+			useEffect( () => {
+				if ( onInputChange ) onInputChange( input );
+			}, [ input ] );
+
 			return (
 				<div>
 					attribute_input_field
@@ -82,7 +83,7 @@ jest.mock( '@woocommerce/components', () => {
 					<div>
 						{ children( {
 							isOpen: true,
-							items: getFilteredItems( items, input, [] ),
+							items,
 							getMenuProps: () => ( {} ),
 							getItemProps: ( {
 								item,
@@ -96,6 +97,23 @@ jest.mock( '@woocommerce/components', () => {
 				</div>
 			);
 		},
+		useAsyncFilter: jest.fn(
+			( { filter, onFilterStart, onFilterEnd, ...props } ) => {
+				const onInputChange = ( value = '' ) => {
+					onFilterStart( value );
+					filter( value )
+						.then( ( items: [] ) => {
+							onFilterEnd( items, value );
+						} )
+						.catch( () => {
+							onFilterEnd( [], value );
+						} );
+					if ( props.onInputChange ) props.onInputChange( value );
+				};
+				return { ...props, onInputChange };
+			}
+		),
+		useSyncFilter: jest.fn(),
 	};
 } );
 
@@ -132,94 +150,102 @@ const attributeList: ProductAttribute[] = [
 ];
 
 describe( 'AttributeInputField', () => {
+	const getProductAttributes = jest.fn();
+
 	beforeEach( () => {
+		( resolveSelect as jest.Mock ).mockReturnValue( {
+			getProductAttributes,
+		} );
+	} );
+
+	afterEach( () => {
 		jest.clearAllMocks();
 	} );
 
-	it( 'should show spinner while attributes are loading', () => {
-		( useSelect as jest.Mock ).mockReturnValue( {
-			isLoading: true,
-			attributes: undefined,
-		} );
-		const { queryByText } = render(
-			<AttributeInputField onChange={ jest.fn() } />
+	it( 'should show spinner while attributes are loading', async () => {
+		getProductAttributes.mockImplementation(
+			() => new Promise( ( resolve ) => setTimeout( resolve, 500 ) )
 		);
-		expect( queryByText( 'spinner' ) ).toBeInTheDocument();
+		await act( async () => {
+			render( <AttributeInputField onChange={ jest.fn() } /> );
+		} );
+		expect( screen.queryByText( 'spinner' ) ).toBeInTheDocument();
 	} );
 
-	it( 'should render attributes when finished loading', () => {
-		( useSelect as jest.Mock ).mockReturnValue( {
-			isLoading: false,
-			attributes: attributeList,
-		} );
-		const { queryByText } = render(
-			<AttributeInputField onChange={ jest.fn() } />
+	it( 'should render attributes when finished loading', async () => {
+		getProductAttributes.mockResolvedValue( attributeList );
+		await act( async () =>
+			render( <AttributeInputField onChange={ jest.fn() } /> )
 		);
-		expect( queryByText( 'spinner' ) ).not.toBeInTheDocument();
-		expect( queryByText( attributeList[ 0 ].name ) ).toBeInTheDocument();
-		expect( queryByText( attributeList[ 1 ].name ) ).toBeInTheDocument();
-	} );
-
-	it( 'should filter out attribute ids passed into ignoredAttributeIds', () => {
-		( useSelect as jest.Mock ).mockReturnValue( {
-			isLoading: false,
-			attributes: attributeList,
-		} );
-		const { queryByText } = render(
-			<AttributeInputField
-				onChange={ jest.fn() }
-				ignoredAttributeIds={ [ attributeList[ 0 ].id ] }
-			/>
-		);
-		expect( queryByText( 'spinner' ) ).not.toBeInTheDocument();
+		expect( screen.queryByText( 'spinner' ) ).not.toBeInTheDocument();
 		expect(
-			queryByText( attributeList[ 0 ].name )
-		).not.toBeInTheDocument();
-		expect( queryByText( attributeList[ 1 ].name ) ).toBeInTheDocument();
-	} );
-
-	it( 'should filter attributes by name case insensitive', () => {
-		( useSelect as jest.Mock ).mockReturnValue( {
-			isLoading: false,
-			attributes: attributeList,
-		} );
-		const { queryByText } = render(
-			<AttributeInputField onChange={ jest.fn() } />
-		);
-		queryByText( 'Update Input' )?.click();
+			screen.queryByText( attributeList[ 0 ].name )
+		).toBeInTheDocument();
 		expect(
-			queryByText( attributeList[ 0 ].name )
-		).not.toBeInTheDocument();
-		expect( queryByText( attributeList[ 1 ].name ) ).toBeInTheDocument();
+			screen.queryByText( attributeList[ 1 ].name )
+		).toBeInTheDocument();
 	} );
 
-	it( 'should filter out attributes ids from ignoredAttributeIds', () => {
-		( useSelect as jest.Mock ).mockReturnValue( {
-			isLoading: false,
-			attributes: attributeList,
+	it( 'should filter out attribute ids passed into ignoredAttributeIds', async () => {
+		getProductAttributes.mockResolvedValue( attributeList );
+		await act( async () => {
+			render(
+				<AttributeInputField
+					onChange={ jest.fn() }
+					ignoredAttributeIds={ [ attributeList[ 0 ].id ] }
+				/>
+			);
 		} );
-		const { queryByText } = render(
-			<AttributeInputField
-				onChange={ jest.fn() }
-				ignoredAttributeIds={ [ attributeList[ 1 ].id ] }
-			/>
-		);
-		expect( queryByText( attributeList[ 0 ].name ) ).toBeInTheDocument();
+		expect( screen.queryByText( 'spinner' ) ).not.toBeInTheDocument();
 		expect(
-			queryByText( attributeList[ 1 ].name )
+			screen.queryByText( attributeList[ 0 ].name )
+		).not.toBeInTheDocument();
+		expect(
+			screen.queryByText( attributeList[ 1 ].name )
+		).toBeInTheDocument();
+	} );
+
+	it( 'should filter attributes by name case insensitive', async () => {
+		getProductAttributes.mockResolvedValue( attributeList );
+		await act( async () =>
+			render( <AttributeInputField onChange={ jest.fn() } /> )
+		);
+		await act( async () => {
+			screen.getByText( 'Update Input' ).click();
+		} );
+		expect(
+			screen.queryByText( attributeList[ 0 ].name )
+		).not.toBeInTheDocument();
+		expect(
+			screen.queryByText( attributeList[ 1 ].name )
+		).toBeInTheDocument();
+	} );
+
+	it( 'should filter out attributes ids from ignoredAttributeIds', async () => {
+		getProductAttributes.mockResolvedValue( attributeList );
+		await act( async () => {
+			render(
+				<AttributeInputField
+					onChange={ jest.fn() }
+					ignoredAttributeIds={ [ attributeList[ 1 ].id ] }
+				/>
+			);
+		} );
+		expect(
+			screen.queryByText( attributeList[ 0 ].name )
+		).toBeInTheDocument();
+		expect(
+			screen.queryByText( attributeList[ 1 ].name )
 		).not.toBeInTheDocument();
 	} );
 
-	it( 'should trigger onChange when onSelect is triggered with attribute value', () => {
+	it( 'should trigger onChange when onSelect is triggered with attribute value', async () => {
 		const onChangeMock = jest.fn();
-		( useSelect as jest.Mock ).mockReturnValue( {
-			isLoading: false,
-			attributes: attributeList,
-		} );
-		const { queryByText } = render(
-			<AttributeInputField onChange={ onChangeMock } />
+		getProductAttributes.mockResolvedValue( attributeList );
+		await act( async () =>
+			render( <AttributeInputField onChange={ onChangeMock } /> )
 		);
-		queryByText( attributeList[ 0 ].name )?.click();
+		screen.queryByText( attributeList[ 0 ].name )?.click();
 		expect( onChangeMock ).toHaveBeenCalledWith( {
 			id: attributeList[ 0 ].id,
 			name: attributeList[ 0 ].name,
@@ -227,42 +253,39 @@ describe( 'AttributeInputField', () => {
 		} );
 	} );
 
-	it( 'should trigger onChange when onRemove is triggered with undefined', () => {
+	it( 'should trigger onChange when onRemove is triggered with undefined', async () => {
 		const onChangeMock = jest.fn();
-		( useSelect as jest.Mock ).mockReturnValue( {
-			isLoading: false,
-			attributes: attributeList,
-		} );
-		const { queryByText } = render(
-			<AttributeInputField onChange={ onChangeMock } />
+		getProductAttributes.mockResolvedValue( attributeList );
+		await act( async () =>
+			render( <AttributeInputField onChange={ onChangeMock } /> )
 		);
-		queryByText( 'remove attribute' )?.click();
+		screen.queryByText( 'remove attribute' )?.click();
 		expect( onChangeMock ).toHaveBeenCalledWith();
 	} );
 
-	it( 'should show the create option when the search value does not match any attributes', () => {
-		( useSelect as jest.Mock ).mockReturnValue( {
-			isLoading: false,
-			attributes: [ attributeList[ 0 ] ],
-		} );
-		const { queryByText } = render(
-			<AttributeInputField onChange={ jest.fn() } />
+	it( 'should show the create option when the search value does not match any attributes', async () => {
+		getProductAttributes.mockResolvedValue( [ attributeList[ 0 ] ] );
+		await act( async () =>
+			render( <AttributeInputField onChange={ jest.fn() } /> )
 		);
-		queryByText( 'Update Input' )?.click();
-		expect( queryByText( 'Create "Co"' ) ).toBeInTheDocument();
+		await act( async () => {
+			screen.getByText( 'Update Input' ).click();
+		} );
+		expect( screen.queryByText( 'Create "Co"' ) ).toBeInTheDocument();
 	} );
 
 	it( 'trigger the onChange callback when the create new value is clicked with only a string', async () => {
 		const onChangeMock = jest.fn();
-		( useSelect as jest.Mock ).mockReturnValue( {
-			isLoading: false,
-			attributes: [ attributeList[ 0 ] ],
-		} );
-		const { queryByText } = render(
-			<AttributeInputField onChange={ onChangeMock } />
+		getProductAttributes.mockResolvedValue( [ attributeList[ 0 ] ] );
+		await act( async () =>
+			render( <AttributeInputField onChange={ onChangeMock } /> )
 		);
-		queryByText( 'Update Input' )?.click();
-		queryByText( 'Create "Co"' )?.click();
+		await act( async () => {
+			screen.getByText( 'Update Input' ).click();
+		} );
+		await act( async () => {
+			screen.getByText( 'Create "Co"' ).click();
+		} );
 		expect( onChangeMock ).toHaveBeenCalledWith( 'Co' );
 	} );
 } );
