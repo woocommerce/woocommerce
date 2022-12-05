7d5021e6a709ff64132af8796120268dbadcf746
Joshua Flowers <joshuatf@gmail.com>
2022-12-05 11:16:49 -0800

    Add product variations data store

---
+++ packages/js/data/src/product-variations/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Internal dependencies
+ */
+import { STORE_NAME, WC_PRODUCT_VARIATIONS_NAMESPACE } from './constants';
+import { createCrudDataStore } from '../crud';
+
+console.log('creating');
+
+createCrudDataStore( {
+	storeName: STORE_NAME,
+	resourceName: 'ProductVariation',
+	pluralResourceName: 'ProductVariations',
+	namespace: WC_PRODUCT_VARIATIONS_NAMESPACE,
+} );
+
+export const EXPERIMENTAL_PRODUCT_VARIATIONS_STORE_NAME = STORE_NAME;
