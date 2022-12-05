9f495fb937926e5a793ed58172ace7aa0168db4b
Darren Ethier <darren@roughsmootheng.in>
2022-12-05 08:24:13 -0500

    add i18n to pattern strings (https://github.com/woocommerce/woocommerce-blocks/pull/7841)

--- plugins/woocommerce-blocks/patterns/store-info-alt-image-and-text.php
+++ plugins/woocommerce-blocks/patterns/store-info-alt-image-and-text.php
@@ -15,32 +15,32 @@
 
 <!-- wp:column {"width":"50%","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
 <div class="wp-block-column" style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;flex-basis:50%"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"small"} -->
-<p class="has-small-font-size">THE GOODS</p>
+<p class="has-small-font-size"><?php esc_html_e( 'THE GOODS', 'woo-gutenberg-products-block' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"style":{"typography":{"lineHeight":"1.3"},"spacing":{"margin":{"top":"var:preset|spacing|20","right":"0","bottom":"0","left":"0"}}},"fontSize":"x-large"} -->
-<h2 class="has-x-large-font-size" id="all-items-are-100-hand-made-using-the-potter-s-wheel-or-traditional-techniques-created-with-love-and-care-in-australia" style="margin-top:var(--wp--preset--spacing--20);margin-right:0;margin-bottom:0;margin-left:0;line-height:1.3">Created with love and care in Australia.</h2>
+<h2 class="has-x-large-font-size" id="all-items-are-100-hand-made-using-the-potter-s-wheel-or-traditional-techniques-created-with-love-and-care-in-australia" style="margin-top:var(--wp--preset--spacing--20);margin-right:0;margin-bottom:0;margin-left:0;line-height:1.3"><?php esc_html_e( 'Created with love and care in Australia.', 'woo-gutenberg-products-block' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.65"}}} -->
-<p style="line-height:1.65">All items are 100% hand-made, using the potter’s wheel or traditional techniques.</p>
+<p style="line-height:1.65"><?php esc_html_e( 'All items are 100% hand-made, using the potter’s wheel or traditional techniques.', 'woo-gutenberg-products-block' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:list {"style":{"typography":{"lineHeight":"2"}}} -->
 <ul style="line-height:2"><!-- wp:list-item -->
-<li>Timeless style.</li>
+<li><?php esc_html_e( 'Timeless style.', 'woo-gutenberg-products-block' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>Earthy, organic feel.</li>
+<li><?php esc_html_e( 'Earthy, organic feel.', 'woo-gutenberg-products-block' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>Enduring quality.</li>
+<li><?php esc_html_e( 'Enduring quality.', 'woo-gutenberg-products-block' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>Unique, one-of-a-kind pieces.</li>
+<li><?php esc_html_e( 'Unique, one-of-a-kind pieces.', 'woo-gutenberg-products-block' ); ?></li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list --></div>
 <!-- /wp:column --></div>
@@ -49,20 +49,20 @@
 <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"40px"}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"50%"} -->
 <div class="wp-block-column" style="flex-basis:50%"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"small"} -->
-<p class="has-small-font-size">ABOUT US</p>
+<p class="has-small-font-size"><?php esc_html_e( 'ABOUT US', 'woo-gutenberg-products-block' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"style":{"typography":{"lineHeight":"1.3"},"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"fontSize":"x-large"} -->
-<h2 class="has-x-large-font-size" id="all-items-are-100-hand-made-using-the-potter-s-wheel-or-traditional-techniques-created-with-love-and-care-in-australia" style="margin-top:var(--wp--preset--spacing--20);line-height:1.3">Marl is an independent studio and artisanal gallery.</h2>
+<h2 class="has-x-large-font-size" id="all-items-are-100-hand-made-using-the-potter-s-wheel-or-traditional-techniques-created-with-love-and-care-in-australia" style="margin-top:var(--wp--preset--spacing--20);line-height:1.3"><?php esc_html_e( 'Marl is an independent studio and artisanal gallery.', 'woo-gutenberg-products-block' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
-<p style="font-size:18px">We specialize in limited collections of handmade tableware. We collaborate with restaurants and cafes to create unique items that complement the menu perfectly. Please get in touch if you want to know more about our process and pricing.</p>
+<p style="font-size:18px"><?php esc_html_e( 'We specialize in limited collections of handmade tableware. We collaborate with restaurants and cafes to create unique items that complement the menu perfectly. Please get in touch if you want to know more about our process and pricing.', 'woo-gutenberg-products-block' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"style":{"spacing":{"margin":{"top":"20px","bottom":"20px"}}}} -->
 <div class="wp-block-buttons" style="margin-top:20px;margin-bottom:20px"><!-- wp:button {"style":{"border":{"radius":"100px"},"spacing":{"padding":{"top":"6px","bottom":"6px","left":"36px","right":"36px"}}},"className":"is-style-outline","fontSize":"small"} -->
-<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size"><a class="wp-block-button__link wp-element-button" style="border-radius:100px;padding-top:6px;padding-right:36px;padding-bottom:6px;padding-left:36px">LEARN MORE</a></div>
+<div class="wp-block-button has-custom-font-size is-style-outline has-small-font-size"><a class="wp-block-button__link wp-element-button" style="border-radius:100px;padding-top:6px;padding-right:36px;padding-bottom:6px;padding-left:36px"><?php esc_html_e( 'LEARN MORE', 'woo-gutenberg-products-block' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:column -->
