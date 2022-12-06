6edd4b8fa77cd622cf82b3b34f4a072b578de19d
Joshua Flowers <joshuatf@gmail.com>
2022-12-06 08:23:49 -0800

    Add product form tabs to layout

--- plugins/woocommerce-admin/client/products/layout/product-form-layout.tsx
+++ plugins/woocommerce-admin/client/products/layout/product-form-layout.tsx
@@ -1,8 +1,47 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Children } from '@wordpress/element';
+import { TabPanel } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import './product-form-layout.scss';
 
-export const ProductFormLayout: React.FC = ( { children } ) => {
-	return <div className="product-form-layout">{ children }</div>;
+export const ProductFormLayout: React.FC< {
+	children: JSX.Element | JSX.Element[];
+} > = ( { children } ) => {
+	const tabs = Children.map( children, ( child: JSX.Element ) => {
+		if ( child.type.name !== 'ProductFormTab' ) {
+			return null;
+		}
+		return {
+			name: child.props.name,
+			title: child.props.title,
+		};
+	} );
+
+	return (
+		<TabPanel
+			className="product-form-layout"
+			activeClass="is-active"
+			tabs={ tabs }
+		>
+			{ ( tab ) => (
+				<>
+					{ Children.map( children, ( child: JSX.Element ) => {
+						if (
+							child.type.name !== 'ProductFormTab' ||
+							child.props.name !== tab.name
+						) {
+							return null;
+						}
+						return child;
+					} ) }
+				</>
+			) }
+		</TabPanel>
+	);
 };
