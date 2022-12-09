be621df8da1cf327c88c17b80c1065b5abe2f5b8
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-12-10 02:28:13 +0800

    Add more data for testing.

--- plugins/woocommerce-admin/client/marketing/overview-multichannel/Channels/useRecommendedChannels.ts
+++ plugins/woocommerce-admin/client/marketing/overview-multichannel/Channels/useRecommendedChannels.ts
@@ -25,7 +25,10 @@ export const useRecommendedChannels = (): UseRecommendedChannels => {
 				plugin: 'facebook-for-woocommerce/facebook-for-woocommerce.php',
 				categories: [ 'marketing' ],
 				subcategories: [
-					{ slug: 'sales-channels', name: 'Sales channels' },
+					{
+						slug: 'sales-channels',
+						name: 'Sales channels',
+					},
 				],
 				tags: [
 					{
@@ -33,6 +36,70 @@ export const useRecommendedChannels = (): UseRecommendedChannels => {
 						name: 'Built by WooCommerce',
 					},
 				],
+			},
+			{
+				title: 'Google Listings and Ads',
+				description:
+					'Get in front of shoppers and drive traffic so you can grow your business with Smart Shopping Campaigns and free listings.',
+				url: 'https://woocommerce.com/products/google-listings-and-ads/?utm_source=marketingtab&utm_medium=product&utm_campaign=wcaddons',
+				direct_install: true,
+				icon: 'https://woocommerce.com/wp-content/plugins/wccom-plugins/marketing-tab-rest-api/icons/google.svg',
+				product: 'google-listings-and-ads',
+				plugin: 'google-listings-and-ads/google-listings-and-ads.php',
+				categories: [ 'marketing' ],
+				subcategories: [
+					{
+						slug: 'sales-channels',
+						name: 'Sales channels',
+					},
+				],
+				tags: [
+					{
+						slug: 'built-by-woocommerce',
+						name: 'Built by WooCommerce',
+					},
+				],
+			},
+			{
+				title: 'Pinterest for WooCommerce',
+				description:
+					'Grow your business on Pinterest! Use this official plugin to allow shoppers to Pin products while browsing your store, track conversions, and advertise on Pinterest.',
+				url: 'https://woocommerce.com/products/pinterest-for-woocommerce/?utm_source=marketingtab&utm_medium=product&utm_campaign=wcaddons',
+				direct_install: true,
+				icon: 'https://woocommerce.com/wp-content/plugins/wccom-plugins/marketing-tab-rest-api/icons/pinterest.svg',
+				product: 'pinterest-for-woocommerce',
+				plugin: 'pinterest-for-woocommerce/pinterest-for-woocommerce.php',
+				categories: [ 'marketing' ],
+				subcategories: [
+					{
+						slug: 'sales-channels',
+						name: 'Sales channels',
+					},
+				],
+				tags: [
+					{
+						slug: 'built-by-woocommerce',
+						name: 'Built by WooCommerce',
+					},
+				],
+			},
+			{
+				title: 'TikTok for WooCommerce',
+				description:
+					'Create advertising campaigns and reach one billion global users with TikTok for WooCommerce.',
+				url: 'https://woocommerce.com/products/tiktok-for-woocommerce/?utm_source=marketingtab&utm_medium=product&utm_campaign=wcaddons',
+				direct_install: true,
+				icon: 'https://woocommerce.com/wp-content/plugins/wccom-plugins/marketing-tab-rest-api/icons/tiktok.jpg',
+				product: 'tiktok-for-business',
+				plugin: 'tiktok-for-business/tiktok-for-woocommerce.php',
+				categories: [ 'marketing' ],
+				subcategories: [
+					{
+						slug: 'sales-channels',
+						name: 'Sales channels',
+					},
+				],
+				tags: [],
 			},
 			{
 				title: 'Amazon, eBay & Walmart Integration for WooCommerce',
@@ -45,7 +112,10 @@ export const useRecommendedChannels = (): UseRecommendedChannels => {
 				plugin: 'woocommerce-amazon-ebay-integration/woocommerce-amazon-ebay-integration.php',
 				categories: [ 'marketing' ],
 				subcategories: [
-					{ slug: 'sales-channels', name: 'Sales channels' },
+					{
+						slug: 'sales-channels',
+						name: 'Sales channels',
+					},
 				],
 				tags: [],
 			},
--- plugins/woocommerce-admin/client/marketing/overview-multichannel/Channels/useRegisteredChannels.ts
+++ plugins/woocommerce-admin/client/marketing/overview-multichannel/Channels/useRegisteredChannels.ts
@@ -5,13 +5,18 @@ export const useRegisteredChannels = () => {
 		loading: false,
 		data: [
 			{
+				slug: 'google-listings-and-ads',
 				name: 'Google Listings and Ads',
+				title: 'Google Listings and Ads',
 				description:
 					'Get in front of shoppers and drive traffic so you can grow your business with Smart Shopping Campaigns and free listings.',
 				icon: 'https://woocommerce.com/wp-content/plugins/wccom-plugins/marketing-tab-rest-api/icons/google.svg',
 				isSetupCompleted: true,
 				setupUrl: 'www.google.com/setup',
 				manageUrl: 'www.google.com/manage',
+				syncStatus: 'synced' as const,
+				issueType: 'none' as const,
+				issueText: '',
 			},
 		],
 	};
