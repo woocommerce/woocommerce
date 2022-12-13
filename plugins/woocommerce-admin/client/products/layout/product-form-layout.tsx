4eacc67501c04b18c7e93eeb5263bb1672a075a3
Joshua T Flowers <joshuatf@gmail.com>
2022-12-13 08:13:12 -0800

    Add product tabs to product layout (#35862)
    * Add product form tabs to layout
    
    * Move product sections to respective tabs
    
    * Add tab styling
    
    * Add changelog entry
    
    * Scroll to top on tab change
    
    * Update font weight on active or inactive tabs
    
    * Add blank EOL

--- plugins/woocommerce-admin/client/products/layout/product-form-layout.tsx
+++ plugins/woocommerce-admin/client/products/layout/product-form-layout.tsx
@@ -1,8 +1,60 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Children, useEffect } from '@wordpress/element';
+import { TabPanel } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import './product-form-layout.scss';
 
-export const ProductFormLayout: React.FC = ( { children } ) => {
-	return <div className="product-form-layout">{ children }</div>;
+export const ProductFormLayout: React.FC< {
+	children: JSX.Element | JSX.Element[];
+} > = ( { children } ) => {
+	useEffect( () => {
+		window.document.body.classList.add(
+			'woocommerce-admin-product-layout'
+		);
+
+		return () => {
+			window.document.body.classList.remove(
+				'woocommerce-admin-product-layout'
+			);
+		};
+	}, [] );
+
+	const tabs = Children.map( children, ( child: JSX.Element ) => {
+		if ( child.type.name !== 'ProductFormTab' ) {
+			return null;
+		}
+		return {
+			name: child.props.name,
+			title: child.props.title,
+		};
+	} );
+
+	return (
+		<TabPanel
+			className="product-form-layout"
+			activeClass="is-active"
+			tabs={ tabs }
+			onSelect={ () => ( window.document.documentElement.scrollTop = 0 ) }
+		>
+			{ ( tab ) => (
+				<>
+					{ Children.map( children, ( child: JSX.Element ) => {
+						if (
+							child.type.name !== 'ProductFormTab' ||
+							child.props.name !== tab.name
+						) {
+							return null;
+						}
+						return child;
+					} ) }
+				</>
+			) }
+		</TabPanel>
+	);
 };
