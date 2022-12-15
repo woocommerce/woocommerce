b0bef26fea984cd4c73caf5e3fe98e36fb199c4b
Mike Jolley <mike.jolley@me.com>
2022-12-15 15:47:20 +0000

    Local Pickup: filter out local pickup rates from regular shipping.

--- plugins/woocommerce-blocks/assets/js/blocks/checkout/inner-blocks/checkout-shipping-methods-block/block.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/checkout/inner-blocks/checkout-shipping-methods-block/block.tsx
@@ -120,7 +120,7 @@ const Block = (): JSX.Element | null => {
 					}
 					renderOption={ renderShippingRatesControlOption }
 					collapsible={ false }
-					shippingRates={ shippingRates }
+					shippingRates={ filteredShippingRates }
 					isLoadingRates={ isLoadingRates }
 					context="woocommerce/checkout"
 				/>
