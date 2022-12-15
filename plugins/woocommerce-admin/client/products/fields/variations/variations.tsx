393cc629d9df626a1022c4579a5a3f775d4eaf9a
Maikel David Pérez Gómez <mdperez86@gmail.com>
2022-12-15 10:13:29 -0300

    Add pagination to variations list (#35979)
    * Add changelog
    
    * Add pagination to variations list
    
    * Apply styles for pagination paddings and to keep it at the bottom of the section card
    
    * Returning back per page option from 2 to 25
    
    * Fix pagination label text from uppercase to normal and font styles
    
    * Update variation footer gap
    
    Co-authored-by: Joshua T Flowers <joshuatf@gmail.com>

--- plugins/woocommerce-admin/client/products/fields/variations/variations.tsx
+++ plugins/woocommerce-admin/client/products/fields/variations/variations.tsx
@@ -7,8 +7,8 @@ import {
 	EXPERIMENTAL_PRODUCT_VARIATIONS_STORE_NAME,
 	ProductVariation,
 } from '@woocommerce/data';
-import { ListItem, Sortable, Tag } from '@woocommerce/components';
-import { useContext } from '@wordpress/element';
+import { ListItem, Pagination, Sortable, Tag } from '@woocommerce/components';
+import { useContext, useState } from '@wordpress/element';
 import { useParams } from 'react-router-dom';
 import { useSelect } from '@wordpress/data';
 
@@ -19,25 +19,46 @@ import { CurrencyContext } from '../../../lib/currency-context';
 import { getProductStockStatus } from '../../utils/get-product-stock-status';
 import './variations.scss';
 
+/**
+ * Since the pagination component does not exposes the way of
+ * changing the per page options which are [25, 50, 75, 100]
+ * the default per page option will be the min in the list to
+ * keep compatibility.
+ *
+ * @see https://github.com/woocommerce/woocommerce/blob/trunk/packages/js/components/src/pagination/index.js#L12
+ */
+const DEFAULT_PER_PAGE_OPTION = 25;
+
 export const Variations: React.FC = () => {
+	const [ currentPage, setCurrentPage ] = useState( 1 );
+	const [ perPage, setPerPage ] = useState( DEFAULT_PER_PAGE_OPTION );
 	const { productId } = useParams();
 	const context = useContext( CurrencyContext );
 	const { formatAmount, getCurrencyConfig } = context;
-	const { isLoading, variations } = useSelect( ( select ) => {
-		const { getProductVariations, hasFinishedResolution } = select(
-			EXPERIMENTAL_PRODUCT_VARIATIONS_STORE_NAME
-		);
-		return {
-			isLoading: ! hasFinishedResolution( 'getProductVariations', [
-				{
-					product_id: productId,
-				},
-			] ),
-			variations: getProductVariations< ProductVariation[] >( {
+	const { isLoading, variations, totalCount } = useSelect(
+		( select ) => {
+			const {
+				getProductVariations,
+				hasFinishedResolution,
+				getProductVariationsTotalCount,
+			} = select( EXPERIMENTAL_PRODUCT_VARIATIONS_STORE_NAME );
+			const requestParams = {
 				product_id: productId,
-			} ),
-		};
-	} );
+				page: currentPage,
+				per_page: perPage,
+			};
+			return {
+				isLoading: ! hasFinishedResolution( 'getProductVariations', [
+					requestParams,
+				] ),
+				variations:
+					getProductVariations< ProductVariation[] >( requestParams ),
+				totalCount:
+					getProductVariationsTotalCount< number >( requestParams ),
+			};
+		},
+		[ currentPage, perPage ]
+	);
 
 	if ( ! variations || isLoading ) {
 		return (
@@ -86,6 +107,16 @@ export const Variations: React.FC = () => {
 					</ListItem>
 				) ) }
 			</Sortable>
+
+			<Pagination
+				className="woocommerce-product-variations__footer"
+				page={ currentPage }
+				perPage={ perPage }
+				total={ totalCount }
+				showPagePicker={ false }
+				onPageChange={ setCurrentPage }
+				onPerPageChange={ setPerPage }
+			/>
 		</Card>
 	);
 };
