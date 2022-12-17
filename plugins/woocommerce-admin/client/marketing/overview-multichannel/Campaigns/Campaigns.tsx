af4f79ab28da6f4cd04191d2a3ca8639ab6b8d5d
Gan Eng Chin <ecgan@users.noreply.github.com>
2022-12-17 21:08:14 +0800

    Add loading state for Campaigns card.

--- plugins/woocommerce-admin/client/marketing/overview-multichannel/Campaigns/Campaigns.tsx
+++ plugins/woocommerce-admin/client/marketing/overview-multichannel/Campaigns/Campaigns.tsx
@@ -8,35 +8,60 @@ import { Icon, megaphone } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
-import { CardHeaderTitle } from '~/marketing/components';
+import { CardHeaderTitle, CenteredSpinner } from '~/marketing/components';
+import { useCampaigns } from './useCampaigns';
 import './Campaigns.scss';
 
 export const Campaigns = () => {
-	return (
-		<Card className="woocommerce-marketing-campaigns-card">
-			<CardHeader>
-				<CardHeaderTitle>
-					{ __( 'Campaigns', 'woocommerce' ) }
-				</CardHeaderTitle>
-				<Button variant="secondary">
-					{ __( 'Create new campaign', 'woocommerce' ) }
-				</Button>
-			</CardHeader>
-			<CardBody className="woocommerce-marketing-campaigns-card-body-empty-content">
-				<Icon icon={ megaphone } size={ 32 } />
-				<div className="woocommerce-marketing-campaigns-card-body-empty-content-title">
-					{ __(
-						'Advertise with marketing campaigns',
-						'woocommerce'
-					) }
-				</div>
-				<div>
-					{ __(
-						'Easily create and manage marketing campaigns without leaving WooCommerce.',
-						'woocommerce'
-					) }
-				</div>
-			</CardBody>
-		</Card>
-	);
+	const { loading, data } = useCampaigns();
+
+	if ( loading ) {
+		return (
+			<Card>
+				<CardHeader>
+					<CardHeaderTitle>
+						{ __( 'Campaigns', 'woocommerce' ) }
+					</CardHeaderTitle>
+					<Button variant="secondary">
+						{ __( 'Create new campaign', 'woocommerce' ) }
+					</Button>
+				</CardHeader>
+				<CardBody>
+					<CenteredSpinner />
+				</CardBody>
+			</Card>
+		);
+	}
+
+	if ( data.length === 0 ) {
+		return (
+			<Card className="woocommerce-marketing-campaigns-card">
+				<CardHeader>
+					<CardHeaderTitle>
+						{ __( 'Campaigns', 'woocommerce' ) }
+					</CardHeaderTitle>
+					<Button variant="secondary">
+						{ __( 'Create new campaign', 'woocommerce' ) }
+					</Button>
+				</CardHeader>
+				<CardBody className="woocommerce-marketing-campaigns-card-body-empty-content">
+					<Icon icon={ megaphone } size={ 32 } />
+					<div className="woocommerce-marketing-campaigns-card-body-empty-content-title">
+						{ __(
+							'Advertise with marketing campaigns',
+							'woocommerce'
+						) }
+					</div>
+					<div>
+						{ __(
+							'Easily create and manage marketing campaigns without leaving WooCommerce.',
+							'woocommerce'
+						) }
+					</div>
+				</CardBody>
+			</Card>
+		);
+	}
+
+	return <div>TODO: campaigns here.</div>;
 };
---
+++ plugins/woocommerce-admin/client/marketing/overview-multichannel/Campaigns/useCampaigns.ts
@@ -0,0 +1,15 @@
+// TODO: testing for loading state.
+export const useCampaigns = () => {
+	return {
+		loading: true,
+		data: [],
+	};
+};
+
+// TODO: testing for empty data.
+// export const useCampaigns = () => {
+// 	return {
+// 		loading: false,
+// 		data: [],
+// 	};
+// };
