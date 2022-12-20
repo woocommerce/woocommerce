60e3adabd9b8a418fdfe31d988a4236c2eaaf57d
Maikel David Pérez Gómez <mdperez86@gmail.com>
2022-12-20 18:02:25 -0300

    Add product variation navigation component (#36076)
    * Add simple navigation component
    
    * Add useProductVariationNavigation hook to manage navigation logic
    
    * Integrate navigation component and hook in product variation form
    
    * Add changelog file
    
    * Add comment feedbacks

--- plugins/woocommerce-admin/client/products/product-variation-form.tsx
+++ plugins/woocommerce-admin/client/products/product-variation-form.tsx
@@ -1,12 +1,14 @@
 /**
  * External dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { Form } from '@woocommerce/components';
 import { PartialProduct, ProductVariation } from '@woocommerce/data';
 
 /**
  * Internal dependencies
  */
+import PostsNavigation from './shared/posts-navigation';
 import { ProductFormHeader } from './layout/product-form-header';
 import { ProductFormLayout } from './layout/product-form-layout';
 import { ProductFormFooter } from './layout/product-form-footer';
@@ -15,11 +17,18 @@ import { PricingSection } from './sections/pricing-section';
 import { ProductInventorySection } from './sections/product-inventory-section';
 import { ProductShippingSection } from './sections/product-shipping-section';
 import { ProductVariationDetailsSection } from './sections/product-variation-details-section';
+import useProductVariationNavigation from './hooks/use-product-variation-navigation';
+import './product-variation-form.scss';
 
 export const ProductVariationForm: React.FC< {
 	product: PartialProduct;
 	productVariation: Partial< ProductVariation >;
-} > = ( { productVariation } ) => {
+} > = ( { product, productVariation } ) => {
+	const navigationProps = useProductVariationNavigation( {
+		product,
+		productVariation,
+	} );
+
 	return (
 		<Form< Partial< ProductVariation > >
 			initialValues={ productVariation }
@@ -43,6 +52,21 @@ export const ProductVariationForm: React.FC< {
 				</ProductFormTab>
 			</ProductFormLayout>
 			<ProductFormFooter />
+
+			<div className="product-variation-form__navigation">
+				<PostsNavigation
+					{ ...navigationProps }
+					actionLabel={ __(
+						'Return to main product',
+						'woocommerce'
+					) }
+					prevLabel={ __(
+						'Previous product variation',
+						'woocommerce'
+					) }
+					nextLabel={ __( 'Next product variation', 'woocommerce' ) }
+				/>
+			</div>
 		</Form>
 	);
 };
