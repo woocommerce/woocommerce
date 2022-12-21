efdf49affe2780a2f840d9994e357af258c53221
Albert Juhé Lluveras <contact@albertjuhe.com>
2022-12-21 12:16:48 +0100

    Remove feature flag from Products block (https://github.com/woocommerce/woocommerce-blocks/pull/8001)
    * Remove feature flag from Products block
    
    * Remove Products block from feature flags documentation

--- plugins/woocommerce-blocks/assets/js/blocks/product-query/variations/product-query.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-query/variations/product-query.tsx
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-import { isFeaturePluginBuild } from '@woocommerce/block-settings';
 import { registerBlockVariation } from '@wordpress/blocks';
 import { Icon } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
@@ -19,33 +18,31 @@ import {
 
 const VARIATION_NAME = 'woocommerce/product-query';
 
-if ( isFeaturePluginBuild() ) {
-	registerBlockVariation( QUERY_LOOP_ID, {
-		description: __(
-			'A block that displays a selection of products in your store.',
-			'woo-gutenberg-products-block'
-		),
-		name: VARIATION_NAME,
-		/* translators: “Products“ is the name of the block. */
-		title: __( 'Products (Beta)', 'woo-gutenberg-products-block' ),
-		isActive: ( blockAttributes ) =>
-			blockAttributes.namespace === VARIATION_NAME,
-		icon: (
-			<Icon
-				icon={ stacks }
-				className="wc-block-editor-components-block-icon wc-block-editor-components-block-icon--stacks"
-			/>
-		),
-		attributes: {
-			...QUERY_DEFAULT_ATTRIBUTES,
-			namespace: VARIATION_NAME,
-		},
-		// Gutenberg doesn't support this type yet, discussion here:
-		// https://github.com/WordPress/gutenberg/pull/43632
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
-		allowedControls: DEFAULT_ALLOWED_CONTROLS,
-		innerBlocks: INNER_BLOCKS_TEMPLATE,
-		scope: [ 'inserter' ],
-	} );
-}
+registerBlockVariation( QUERY_LOOP_ID, {
+	description: __(
+		'A block that displays a selection of products in your store.',
+		'woo-gutenberg-products-block'
+	),
+	name: VARIATION_NAME,
+	/* translators: “Products“ is the name of the block. */
+	title: __( 'Products (Beta)', 'woo-gutenberg-products-block' ),
+	isActive: ( blockAttributes ) =>
+		blockAttributes.namespace === VARIATION_NAME,
+	icon: (
+		<Icon
+			icon={ stacks }
+			className="wc-block-editor-components-block-icon wc-block-editor-components-block-icon--stacks"
+		/>
+	),
+	attributes: {
+		...QUERY_DEFAULT_ATTRIBUTES,
+		namespace: VARIATION_NAME,
+	},
+	// Gutenberg doesn't support this type yet, discussion here:
+	// https://github.com/WordPress/gutenberg/pull/43632
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	allowedControls: DEFAULT_ALLOWED_CONTROLS,
+	innerBlocks: INNER_BLOCKS_TEMPLATE,
+	scope: [ 'inserter' ],
+} );
