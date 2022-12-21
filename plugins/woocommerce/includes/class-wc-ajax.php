309ed6303f5a4d377b0a2cd8e47c8757b261b82e
louwie17 <lourensschep@gmail.com>
2022-12-21 04:11:12 -0400

    Make sure attribute term dropdown adheres to sort order setting (#36047)
    * Make sure attribute term dropdown adheres to sort order setting of attribute
    
    * Fix spacing
    
    * Fix lint errors

--- plugins/woocommerce/includes/class-wc-ajax.php
+++ plugins/woocommerce/includes/class-wc-ajax.php
@@ -1745,10 +1745,11 @@ class WC_AJAX {
 		$search_text = isset( $_GET['term'] ) ? wc_clean( wp_unslash( $_GET['term'] ) ) : '';
 		$limit       = isset( $_GET['limit'] ) ? absint( wp_unslash( $_GET['limit'] ) ) : null;
 		$taxonomy    = isset( $_GET['taxonomy'] ) ? wc_clean( wp_unslash( $_GET['taxonomy'] ) ) : '';
+		$orderby     = isset( $_GET['orderby'] ) ? wc_clean( wp_unslash( $_GET['orderby'] ) ) : 'name';
 
 		$args = array(
 			'taxonomy'        => $taxonomy,
-			'orderby'         => 'id',
+			'orderby'         => $orderby,
 			'order'           => 'ASC',
 			'hide_empty'      => false,
 			'fields'          => 'all',
