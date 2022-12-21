4877e4b36e029b8084a6ee467b3d848b4bd29f40
Sam Seay <samueljseay@gmail.com>
2022-12-21 13:58:10 +1300

    Support installing live branches from the manifest (#36072)

--- plugins/woocommerce-beta-tester/src/live-branches/hooks/live-branches.tsx
+++ plugins/woocommerce-beta-tester/src/live-branches/hooks/live-branches.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 // @ts-ignore
 import { API_NAMESPACE } from '../../features/data/constants';
 
+type PluginStatus = 'not-installed' | 'installed' | 'active';
+
 export type Branch = {
 	branch: string;
 	commit: string;
@@ -11,6 +13,7 @@ export type Branch = {
 	update_date: string;
 	version: string;
 	pr: number;
+	install_status: PluginStatus;
 };
 
 export const useLiveBranchesData = () => {
@@ -38,4 +41,102 @@ export const useLiveBranchesData = () => {
 	}, [] );
 
 	return { branches, isLoading: loading };
+};
+
+export const useLiveBranchInstall = (
+	downloadUrl: string,
+	prName: string,
+	version: string,
+	status: PluginStatus
+) => {
+	const [ isInProgress, setIsInProgress ] = useState( false );
+	const [ isError, setIsError ] = useState( false );
+	const [ pluginStatus, setPluginStatus ] = useState( status );
+
+	const activate = async () => {
+		setIsInProgress( true );
+
+		try {
+			const deactivateResult = await apiFetch< Response >( {
+				path: `${ API_NAMESPACE }/live-branches/deactivate/v1`,
+			} );
+
+			if ( deactivateResult.status >= 400 ) {
+				throw new Error( 'Could not deactivate' );
+			}
+
+			const activateResult = await apiFetch< Response >( {
+				path: `${ API_NAMESPACE }/live-branches/activate/v1`,
+				method: 'POST',
+				body: JSON.stringify( {
+					version,
+				} ),
+			} );
+
+			if ( activateResult.status >= 400 ) {
+				throw new Error( 'Could not activate' );
+			}
+		} catch ( e ) {
+			setIsError( true );
+		}
+
+		setPluginStatus( 'active' );
+		setIsInProgress( false );
+	};
+
+	const installAndActivate = async () => {
+		setIsInProgress( true );
+
+		try {
+			const installResult = await apiFetch< Response >( {
+				path: `${ API_NAMESPACE }/live-branches/install/v1`,
+				method: 'POST',
+				body: JSON.stringify( {
+					pr_name: prName,
+					download_url: downloadUrl,
+					version,
+				} ),
+			} );
+
+			if ( installResult.status >= 400 ) {
+				throw new Error( 'Could not install' );
+			}
+
+			setPluginStatus( 'installed' );
+
+			const deactivateResult = await apiFetch< Response >( {
+				path: `${ API_NAMESPACE }/live-branches/deactivate/v1`,
+			} );
+
+			if ( deactivateResult.status >= 400 ) {
+				throw new Error( 'Could not deactivate' );
+			}
+
+			const activateResult = await apiFetch< Response >( {
+				path: `${ API_NAMESPACE }/live-branches/activate/v1`,
+				method: 'POST',
+				body: JSON.stringify( {
+					version,
+				} ),
+			} );
+
+			if ( activateResult.status >= 400 ) {
+				throw new Error( 'Could not activate' );
+			}
+
+			setPluginStatus( 'active' );
+		} catch ( e ) {
+			setIsError( true );
+		}
+
+		setIsInProgress( false );
+	};
+
+	return {
+		installAndActivate,
+		activate,
+		isError,
+		isInProgress,
+		status: pluginStatus,
+	};
 };
