d6b3b26c01686548544bdc95e400d2f7bf13e235
Luigi Teschio <gigitux@gmail.com>
2022-12-22 15:48:24 +0100

    Mini Cart block - added notice support  (https://github.com/woocommerce/woocommerce-blocks/pull/7234)
    * Mini Cart block - added notice support woocommerce/woocommerce-blocks#6941
    
    Mini Cart block - added notice support
    
    * set isDismissible to true
    
    * address feedback
    
    * improve layout
    
    * fix style and update close button label
    
    * fix UI when the admin bar is visible
    
    * Split out dismissible notices
    
    * fix css
    
    Co-authored-by: Mike Jolley <mike.jolley@me.com>

--- plugins/woocommerce-blocks/assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/filled-mini-cart-contents-block/frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/filled-mini-cart-contents-block/frontend.tsx
@@ -1,7 +1,12 @@
 /**
  * External dependencies
  */
+import { StoreNoticesContainer } from '@woocommerce/blocks-checkout';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
+
+import { useDispatch } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
+import { decodeEntities } from '@wordpress/html-entities';
 
 type FilledMiniCartContentsBlockProps = {
 	children: JSX.Element;
@@ -12,13 +17,31 @@ const FilledMiniCartContentsBlock = ( {
 	children,
 	className,
 }: FilledMiniCartContentsBlockProps ): JSX.Element | null => {
-	const { cartItems } = useStoreCart();
+	const { cartItems, cartItemErrors } = useStoreCart();
+
+	const { createErrorNotice } = useDispatch( 'core/notices' );
+
+	// Ensures any cart errors listed in the API response get shown.
+	useEffect( () => {
+		cartItemErrors.forEach( ( error ) => {
+			createErrorNotice( decodeEntities( error.message ), {
+				isDismissible: false,
+				id: error.code,
+				context: 'wc/cart',
+			} );
+		} );
+	}, [ createErrorNotice, cartItemErrors ] );
 
 	if ( cartItems.length === 0 ) {
 		return null;
 	}
 
-	return <div className={ className }>{ children }</div>;
+	return (
+		<div className={ className }>
+			<StoreNoticesContainer context="wc/cart" />
+			{ children }
+		</div>
+	);
 };
 
 export default FilledMiniCartContentsBlock;
