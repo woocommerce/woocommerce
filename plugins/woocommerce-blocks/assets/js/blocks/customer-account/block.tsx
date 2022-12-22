384ce12e046f0f131060ca8ee3ce4fdd2c3a24dd
Alba Rincón <albarin@users.noreply.github.com>
2022-12-22 16:01:01 +0100

    Add 'Customer Account' block (https://github.com/woocommerce/woocommerce-blocks/pull/7876)
    * Add empty 'Customer Account' block
    
    * Add edit to render the block on the editor
    
    * Add Customer Account icon to @woocommerce/icons library
    
    This icon is used in the Customer Account block.
    
    * Use customerAccount icon in the Customer Account block
    
    * Add Block Settings to Customer Account block
    
    Add Typography and Color settings to the Customer Account block that can be used during Edit mode.
    
    * Add Customer Account style icons
    
    This adds the customer account style icons to be used by the customer account block.
    
    * Render the block in the frontend
    
    * Add Display setting to the Customer Account block
    
    This setting allow users to choose if they want to include an icon with the customer account link.
    
    * Add icon style selector to Display settings
    
    This adds an icon style selector in which users can choose the customer account link icon.
    
    * Display on the frontend depending on the icon style
    
    * Rename attributes
    
    * Refactor block rendering
    
    * Improve styling
    
    * Make the account link dynamic
    
    * Add alignment menu
    
    * Get dashboardUrl with getSettings and add fallback
    
    * Add link to account settings
    
    * Add tests
    
    * Fix test
    
    * Change display title
    
    * Split css file
    
    * Switch to SSR
    
    * Add styles to the front end
    
    * Address latest feedback
    
    * Remove experimental flag
    
    Co-authored-by: Alexandre Lara <allexandrelara@gmail.com>

---
+++ plugins/woocommerce-blocks/assets/js/blocks/customer-account/block.tsx
@@ -0,0 +1,73 @@
+/**
+ * External dependencies
+ */
+import { Icon } from '@wordpress/icons';
+import {
+	customerAccountStyle,
+	customerAccountStyleAlt,
+} from '@woocommerce/icons';
+import { getSetting } from '@woocommerce/settings';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { Attributes, DisplayStyle, IconStyle } from './types';
+
+const AccountIcon = ( {
+	iconStyle,
+	displayStyle,
+}: {
+	iconStyle: IconStyle;
+	displayStyle: DisplayStyle;
+} ) => {
+	const icon =
+		iconStyle === IconStyle.ALT
+			? customerAccountStyleAlt
+			: customerAccountStyle;
+
+	return displayStyle === DisplayStyle.TEXT_ONLY ? null : (
+		<Icon className="icon" icon={ icon } size={ 16 } />
+	);
+};
+
+const Label = ( { displayStyle }: { displayStyle: DisplayStyle } ) => {
+	if ( displayStyle === DisplayStyle.ICON_ONLY ) {
+		return null;
+	}
+
+	const currentUserId = getSetting( 'currentUserId', null );
+
+	return (
+		<span className="label">
+			{ currentUserId
+				? __( 'My Account', 'woo-gutenberg-products-block' )
+				: __( 'Log in', 'woo-gutenberg-products-block' ) }
+		</span>
+	);
+};
+
+export const CustomerAccountBlock = ( {
+	attributes,
+}: {
+	attributes: Attributes;
+} ): JSX.Element => {
+	const { displayStyle, iconStyle } = attributes;
+
+	return (
+		<a
+			href={ getSetting(
+				'dashboardUrl',
+				getSetting( 'wpLoginUrl', '/wp-login.php' )
+			) }
+		>
+			<AccountIcon
+				iconStyle={ iconStyle }
+				displayStyle={ displayStyle }
+			/>
+			<Label displayStyle={ displayStyle } />
+		</a>
+	);
+};
+
+export default CustomerAccountBlock;
