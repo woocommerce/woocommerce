f9dd42a36a50d5aa5da2f7391ddfdb327f15d879
Alba Rincón <albarin@users.noreply.github.com>
2022-12-23 10:08:44 +0100

    Add `Customer account` to patterns (https://github.com/woocommerce/woocommerce-blocks/pull/7944)
    * Render the block in the frontend
    
    * Add icon style selector to Display settings
    
    This adds an icon style selector in which users can choose the customer account link icon.
    
    * Display on the frontend depending on the icon style
    
    * Rename attributes
    
    * Refactor block rendering
    
    * Improve styling
    
    * Add link to account settings
    
    * Split css file
    
    * Switch to SSR
    
    * Add customer account block to header patterns
    
    * Add customer account block to footer patterns
    
    * Adapt minimal header to designs
    
    * Adapt large header to designs
    
    * Adapt header patterns to new designs and format code
    
    * Adapt footer patterns to the designs
    
    * Change display style for the essential header
    
    * Increase alt icon size
    
    * Make the icon resize with the label
    
    * Adjust icon size on headers
    
    * Add padding and move second row to the right
    
    * Remove unused enum
    
    Co-authored-by: Alexandre Lara <allexandrelara@gmail.com>

--- plugins/woocommerce-blocks/assets/js/blocks/customer-account/sidebar-settings.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/customer-account/sidebar-settings.tsx
@@ -142,7 +142,7 @@ export const BlockSettings = ( {
 							label={
 								<Icon
 									icon={ customerAccountStyleAlt }
-									size={ 18 }
+									size={ 20 }
 									className={ classNames(
 										'wc-block-customer-account__icon-option',
 										{
