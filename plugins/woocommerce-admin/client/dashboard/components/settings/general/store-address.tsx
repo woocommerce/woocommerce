fef9b746b3e8f9359817c5c90b17ba69fd37b6c0
Moon <moon.kyong@automattic.com>
2022-12-22 21:30:50 -0800

    Update country select control regex (#36159)
    * Update regex to match country name or " - region "
    
    * Add changelog
    
    * Fix style

--- plugins/woocommerce-admin/client/dashboard/components/settings/general/store-address.tsx
+++ plugins/woocommerce-admin/client/dashboard/components/settings/general/store-address.tsx
@@ -358,7 +358,10 @@ export function StoreAddress( {
 				required
 				autoComplete="new-password" // disable autocomplete and autofill
 				getSearchExpression={ ( query: string ) => {
-					return new RegExp( '^' + query, 'i' );
+					return new RegExp(
+						'(^' + query + '| — (' + query + '))',
+						'i'
+					);
 				} }
 				options={ countryStateOptions }
 				excludeSelectedOptions={ false }
