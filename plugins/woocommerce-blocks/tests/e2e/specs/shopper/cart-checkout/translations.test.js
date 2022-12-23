46f0330a0ab8d211bb268a752a3bc4a9a2c94f06
Niels Lange <info@nielslange.de>
2022-12-23 19:54:04 +0700

    Use Woo Blocks instead Woo core translation within the Checkout order Summary block (https://github.com/woocommerce/woocommerce-blocks/pull/7995)
    * Use Woo Blocks instead Woo core translation
    
    * Activale previously skipped e2e tests
    
    * Adjust loading order of $shared_chunks, $vendor_chunks and $chunks
    
    * Revert chunk order of MiniCart.php

--- plugins/woocommerce-blocks/tests/e2e/specs/shopper/cart-checkout/translations.test.js
+++ plugins/woocommerce-blocks/tests/e2e/specs/shopper/cart-checkout/translations.test.js
@@ -53,7 +53,7 @@ describe( 'Shopper → Cart & Checkout → Translations', () => {
 	} );
 
 	// The translation of WooCommerce Core is taking over translations of WC Blocks. We have to fix this issue https://github.com/woocommerce/woocommerce-blocks/issues/7775 before we can enable this test.
-	it.skip( 'User can view translated Checkout block', async () => {
+	it( 'User can view translated Checkout block', async () => {
 		await shopper.block.goToCheckout();
 
 		const contactHeading = await page.$(
