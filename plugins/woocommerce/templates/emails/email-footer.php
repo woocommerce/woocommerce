c75f1b4070aa0f66d966340496c30f7e39770230
barryhughes <3594411+barryhughes@users.noreply.github.com>
2023-01-05 07:54:57 -0800

    Update existing code to match current coding standards.

--- plugins/woocommerce/templates/emails/email-footer.php
+++ plugins/woocommerce/templates/emails/email-footer.php
@@ -30,7 +30,7 @@ defined( 'ABSPATH' ) || exit;
 										</tr>
 									</table>
 								</td>
-							</tr	>
+							</tr>
 							<tr>
 								<td align="center" valign="top">
 									<!-- Footer -->
@@ -40,7 +40,21 @@ defined( 'ABSPATH' ) || exit;
 												<table border="0" cellpadding="10" cellspacing="0" width="100%">
 													<tr>
 														<td colspan="2" valign="middle" id="credit">
-															<?php echo wp_kses_post( wpautop( wptexturize( apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) ) ) ) ); ?>
+															<?php
+															echo wp_kses_post(
+																wpautop(
+																	wptexturize(
+																		/**
+																		 * Provides control over the email footer text used for most order emails.
+																		 *
+																		 * @since 4.0.0
+																		 *
+																		 * @param string $email_footer_text
+																		 */
+																		apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) )
+																	)
+																)
+															); ?>
 														</td>
 													</tr>
 												</table>
--- plugins/woocommerce/templates/emails/email-header.php
+++ plugins/woocommerce/templates/emails/email-header.php
@@ -38,8 +38,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 								<td align="center" valign="top">
 									<div id="template_header_image">
 										<?php
-										if ( $img = get_option( 'woocommerce_email_header_image' ) ) {
-											echo '<p style="margin-top:0;"><img src="' . esc_url( $img ) . '" alt="' . get_bloginfo( 'name', 'display' ) . '" /></p>';
+										$img = get_option( 'woocommerce_email_header_image' );
+
+										if ( $img ) {
+											echo '<p style="margin-top:0;"><img src="' . esc_url( $img ) . '" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" /></p>';
 										}
 										?>
 									</div>
@@ -50,7 +52,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 												<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header">
 													<tr>
 														<td id="header_wrapper">
-															<h1><?php echo $email_heading; ?></h1>
+															<h1><?php echo esc_html( $email_heading ); ?></h1>
 														</td>
 													</tr>
 												</table>
