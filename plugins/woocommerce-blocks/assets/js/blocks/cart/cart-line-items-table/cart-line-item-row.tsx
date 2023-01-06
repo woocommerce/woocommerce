8aa435b2eb07b1abf99414fb3806527a93d1fa5e
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2023-01-06 15:21:56 +0000

    Context for aria-label on cart quantity controls (https://github.com/woocommerce/woocommerce-blocks/pull/8099)
    * Aria label context for "Remove item" action on cart
    
    * Aria label context for quantity controls on cart

--- plugins/woocommerce-blocks/assets/js/blocks/cart/cart-line-items-table/cart-line-item-row.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart/cart-line-items-table/cart-line-item-row.tsx
@@ -311,6 +311,14 @@ const CartLineItemRow: React.ForwardRefExoticComponent<
 							{ showRemoveItemLink && (
 								<button
 									className="wc-block-cart-item__remove-link"
+									aria-label={ sprintf(
+										/* translators: %s refers to the item's name in the cart. */
+										__(
+											'Remove %s from cart',
+											'woo-gutenberg-products-block'
+										),
+										name
+									) }
 									onClick={ () => {
 										onRemove();
 										removeItem();
