28249ef7ee7e172e2f517e3d1b3b4eea11ddf9e0
Moon <moon.kyong@automattic.com>
2023-01-05 19:57:13 -0800

    Update packages/js/components/src/section/section.tsx
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>

--- packages/js/components/src/section/section.tsx
+++ packages/js/components/src/section/section.tsx
@@ -11,7 +11,7 @@ import { Level } from './context';
 type SectionProps = {
 	/** The wrapper component for this section. Optional, defaults to `div`. If passed false, no wrapper is used. Additional props passed to Section are passed on to the component. */
 	component?: React.ComponentType | string | false;
-	// Optional classname
+	/** Optional classname */
 	className?: string;
 	// The children inside this section, rendered in the `component`. This increases the context level for the next heading used.
 	children: React.ReactNode;
