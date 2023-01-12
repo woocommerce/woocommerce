b55c8940e3d815bb3ef1b4536f4808b8de626f46
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-01-12 12:39:01 +0100

    Product Elements [Variations]: Change block icon colors to match the core blocks. Fixes woocommerce/woocommerce-blocks#8084 (https://github.com/woocommerce/woocommerce-blocks/pull/8155)

--- plugins/woocommerce-blocks/assets/js/blocks/product-query/variations/elements/product-summary.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-query/variations/elements/product-summary.tsx
@@ -2,11 +2,12 @@
  * External dependencies
  */
 import { isFeaturePluginBuild } from '@woocommerce/block-settings';
+import { Icon } from '@wordpress/components';
 import {
 	BLOCK_DESCRIPTION,
-	BLOCK_ICON,
 	BLOCK_TITLE,
 } from '@woocommerce/atomic-blocks/product-elements/summary/constants';
+import { page } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -19,7 +20,7 @@ export const VARIATION_NAME = 'woocommerce/product-query/product-summary';
 if ( isFeaturePluginBuild() ) {
 	registerElementVariation( CORE_NAME, {
 		blockDescription: BLOCK_DESCRIPTION,
-		blockIcon: BLOCK_ICON,
+		blockIcon: <Icon icon={ page } />,
 		blockTitle: BLOCK_TITLE,
 		variationName: VARIATION_NAME,
 	} );
