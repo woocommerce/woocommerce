0064935234c728a390f46ae8207f4ad9f36c7acb
Thomas Roberts <5656702+opr@users.noreply.github.com>
2023-01-12 12:44:26 +0000

    Remove old cart notices before showing new ones (https://github.com/woocommerce/woocommerce-blocks/pull/7363)
    * Remove cart notices before showing new ones
    
    * bot: update checkstyle.xml
    
    * Update deps for useEffect
    
    * Remove outdated notices from mini cart
    
    * Add comment to note that this functionality is also present in mini cart
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- plugins/woocommerce-blocks/assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/filled-mini-cart-contents-block/frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/filled-mini-cart-contents-block/frontend.tsx
@@ -4,7 +4,7 @@
 import { StoreNoticesContainer } from '@woocommerce/blocks-checkout';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
 
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { useEffect } from '@wordpress/element';
 import { decodeEntities } from '@wordpress/html-entities';
 
@@ -19,10 +19,29 @@ const FilledMiniCartContentsBlock = ( {
 }: FilledMiniCartContentsBlockProps ): JSX.Element | null => {
 	const { cartItems, cartItemErrors } = useStoreCart();
 
-	const { createErrorNotice } = useDispatch( 'core/notices' );
+	const { createErrorNotice, removeNotice } = useDispatch( 'core/notices' );
+
+	/*
+	 * The code for removing old notices is also present in the filled-cart-block/frontend.tsx file and will take care
+	 * of removing outdated errors in the Cart block.
+	 */
+	const currentlyDisplayedErrorNoticeCodes = useSelect( ( select ) => {
+		return select( 'core/notices' )
+			.getNotices( 'wc/cart' )
+			.filter(
+				( notice ) =>
+					notice.status === 'error' && notice.type === 'default'
+			)
+			.map( ( notice ) => notice.id );
+	} );
 
 	// Ensures any cart errors listed in the API response get shown.
 	useEffect( () => {
+		// Clear errors out of the store before adding the new ones from the response.
+		currentlyDisplayedErrorNoticeCodes.forEach( ( id ) => {
+			removeNotice( id, 'wc/cart' );
+		} );
+
 		cartItemErrors.forEach( ( error ) => {
 			createErrorNotice( decodeEntities( error.message ), {
 				isDismissible: false,
@@ -30,7 +49,12 @@ const FilledMiniCartContentsBlock = ( {
 				context: 'wc/cart',
 			} );
 		} );
-	}, [ createErrorNotice, cartItemErrors ] );
+	}, [
+		createErrorNotice,
+		cartItemErrors,
+		currentlyDisplayedErrorNoticeCodes,
+		removeNotice,
+	] );
 
 	if ( cartItems.length === 0 ) {
 		return null;
