c41eccc58ef65a00def17f6adaed946dc657425b
barryhughes <3594411+barryhughes@users.noreply.github.com>
2023-01-13 14:38:56 -0800

    Check a valid product object was obtained before calling its methods.

--- plugins/woocommerce/src/Internal/DownloadPermissionsAdjuster.php
+++ plugins/woocommerce/src/Internal/DownloadPermissionsAdjuster.php
@@ -99,7 +99,21 @@ class DownloadPermissionsAdjuster {
 
 		$children_with_downloads = array();
 		foreach ( $children_ids as $child_id ) {
-			$child                                = wc_get_product( $child_id );
+			$child = wc_get_product( $child_id );
+
+			// Ensure we have a valid child product.
+			if ( ! $child ) {
+				wc_get_logger()->warning(
+					sprintf(
+						/* translators: 1: child product ID 2: parent product ID. */
+						__( 'Unable to load child product %1$d while adjusting download permissions for product %2$d.', 'woocommerce' ),
+						$child_id,
+						$product_id
+					)
+				);
+				continue;
+			}
+
 			$children_with_downloads[ $child_id ] = $this->get_download_files_and_permissions( $child );
 		}
 
