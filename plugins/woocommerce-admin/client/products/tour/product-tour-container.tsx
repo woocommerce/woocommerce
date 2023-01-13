29d20e1e74c977a2d6347a19d0c28281fbd27e1d
Joshua Flowers <joshuatf@gmail.com>
2023-01-13 10:19:50 -0800

    Add product tour

--- plugins/woocommerce-admin/client/products/tour/product-tour-container.tsx
+++ plugins/woocommerce-admin/client/products/tour/product-tour-container.tsx
@@ -4,22 +4,25 @@
 import { __ } from '@wordpress/i18n';
 import { OPTIONS_STORE_NAME } from '@woocommerce/data';
 import { useSelect, useDispatch } from '@wordpress/data';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
+import { ProductTour } from './product-tour';
 import { ProductTourModal } from './product-tour-modal';
 
 const PRODUCT_TOUR_MODAL_HIDDEN = 'woocommerce_product_tour_modal_hidden';
 
 export const ProductTourContainer: React.FC = () => {
+	const [ isTouring, setIsTouring ] = useState( false );
 	const { updateOptions } = useDispatch( OPTIONS_STORE_NAME );
-	const { isHidden } = useSelect( ( select ) => {
+	const { isModalHidden } = useSelect( ( select ) => {
 		const { getOption, hasFinishedResolution } =
 			select( OPTIONS_STORE_NAME );
 
 		return {
-			isHidden:
+			isModalHidden:
 				getOption( PRODUCT_TOUR_MODAL_HIDDEN ) === 'yes' ||
 				! hasFinishedResolution( 'getOption', [
 					PRODUCT_TOUR_MODAL_HIDDEN,
@@ -33,9 +36,18 @@ export const ProductTourContainer: React.FC = () => {
 		} );
 	};
 
-	const onStart = () => {};
+	const onStart = () => {
+		updateOptions( {
+			[ PRODUCT_TOUR_MODAL_HIDDEN ]: 'yes',
+		} );
+		setIsTouring( true );
+	};
 
-	if ( isHidden ) {
+	if ( isTouring ) {
+		return <ProductTour onClose={ () => setIsTouring( false ) } />;
+	}
+
+	if ( isModalHidden ) {
 		return null;
 	}
 
