bde100f74e4bee8f0af07eb57fc7a2d2128e662d
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-01-14 02:20:00 -0300

    Add the type definitions

--- packages/js/components/src/experimental-tree-control/types.ts
+++ packages/js/components/src/experimental-tree-control/types.ts
@@ -10,52 +10,91 @@ export interface LinkedTree {
 	children: LinkedTree[];
 }
 
-export type TreeProps = React.DetailedHTMLProps<
-	React.OlHTMLAttributes< HTMLOListElement >,
-	HTMLOListElement
-> & {
-	level?: number;
-	items: LinkedTree[];
+export type CheckedStatus = 'checked' | 'unchecked' | 'indeterminate';
+
+type BaseTreeProps = {
 	/**
-	 * It gives a way to render a different Element as the
-	 * tree item label.
-	 *
-	 * @example
-	 * <Tree
-	 * 	getItemLabel={ ( item ) => <span>${ item.data.label }</span> }
-	 * />
-	 *
-	 * @param  item The current rendering tree item
-	 *
-	 * @see {@link LinkedTree}
+	 * It contians one item if `multiple` value is false or
+	 * a list of items if it is true.
 	 */
-	getItemLabel?( item: LinkedTree ): JSX.Element;
+	selected?: Item | Item[];
 	/**
-	 * Return if the tree item passed in should be expanded.
-	 *
-	 * @example
-	 * <Tree
-	 * 	shouldItemBeExpanded={
-	 * 		( item ) => checkExpanded( item, filter )
-	 * 	}
-	 * />
-	 *
-	 * @param  item The tree item to determine if should be expanded.
-	 *
-	 * @see {@link LinkedTree}
+	 * Whether the tree items are single or multiple selected.
 	 */
-	shouldItemBeExpanded?( item: LinkedTree ): boolean;
+	multiple?: boolean;
+	/**
+	 * When `multipe` is true and a child item is selected, all its
+	 * ancestors and its descendants are also selected. If it's false
+	 * only the clicked item is selected.
+	 *
+	 * @param value The selection
+	 */
+	onSelect?( value: Item | Item[] ): void;
+	/**
+	 * When `multipe` is true and a child item is unselected, all its
+	 * ancestors (if no sibblings are selected) and its descendants
+	 * are also unselected. If it's false only the clicked item is
+	 * unselected.
+	 *
+	 * @param value The unselection
+	 */
+	onRemove?( value: Item | Item[] ): void;
 };
 
-export type TreeItemProps = React.DetailedHTMLProps<
-	React.LiHTMLAttributes< HTMLLIElement >,
-	HTMLLIElement
-> & {
-	level: number;
-	item: LinkedTree;
-	getLabel?( item: LinkedTree ): JSX.Element;
-	shouldItemBeExpanded?( item: LinkedTree ): boolean;
-};
+export type TreeProps = BaseTreeProps &
+	Omit<
+		React.DetailedHTMLProps<
+			React.OlHTMLAttributes< HTMLOListElement >,
+			HTMLOListElement
+		>,
+		'onSelect'
+	> & {
+		level?: number;
+		items: LinkedTree[];
+		/** It gives a way to render a different Element as the
+		 * tree item label.
+		 *
+		 * @example
+		 * <Tree
+		 * 	getItemLabel={ ( item ) => <span>${ item.data.label }</span> }
+		 * />
+		 *
+		 * @param item The current rendering tree item
+		 *
+		 * @see {@link LinkedTree}
+		 */
+		getItemLabel?( item: LinkedTree ): JSX.Element;
+		/**
+		 * Return if the tree item passed in should be expanded.
+		 *
+		 * @example
+		 * <Tree
+		 * 	shouldItemBeExpanded={
+		 * 		( item ) => checkExpanded( item, filter )
+		 * 	}
+		 * />
+		 *
+		 * @param  item The tree item to determine if should be expanded.
+		 *
+		 * @see {@link LinkedTree}
+		 */
+		shouldItemBeExpanded?( item: LinkedTree ): boolean;
+	};
+
+export type TreeItemProps = BaseTreeProps &
+	Omit<
+		React.DetailedHTMLProps<
+			React.LiHTMLAttributes< HTMLLIElement >,
+			HTMLLIElement
+		>,
+		'onSelect'
+	> & {
+		level: number;
+		item: LinkedTree;
+		index: number;
+		getLabel?( item: LinkedTree ): JSX.Element;
+		shouldItemBeExpanded?( item: LinkedTree ): boolean;
+	};
 
 export type TreeControlProps = Omit< TreeProps, 'items' | 'level' > & {
 	items: Item[];
