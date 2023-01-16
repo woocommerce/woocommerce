5db88b8cc39dc215378b42d70fb2a7d059cf9a80
AnnaMag <anna.mag.kedzierska@gmail.com>
2023-01-16 19:25:42 +0100

    Disable the new navigation.

--- plugins/woocommerce/src/Internal/Features/FeaturesController.php
+++ plugins/woocommerce/src/Internal/Features/FeaturesController.php
@@ -607,18 +607,12 @@ class FeaturesController {
 			$disabled = true;
 			$desc_tip = __( 'WooCommerce Admin has been disabled', 'woocommerce' );
 		} elseif ( 'new_navigation' === $feature_id ) {
-			$needs_update = version_compare( get_bloginfo( 'version' ), '5.6', '<' );
-			if ( $needs_update && current_user_can( 'update_core' ) && current_user_can( 'update_php' ) ) {
-				$update_text = sprintf(
-				// translators: 1: line break tag, 2: open link to WordPress update link, 3: close link tag.
-					__( '%1$s %2$sUpdate WordPress to enable the new navigation%3$s', 'woocommerce' ),
-					'<br/>',
-					'<a href="' . self_admin_url( 'update-core.php' ) . '" target="_blank">',
-					'</a>'
+			$update_text = sprintf(
+					__( '%1$s The development of this feature is currently on hold.', 'woocommerce' ),
+				'<br/>',
 				);
-				$description .= $update_text;
-				$disabled     = true;
-			}
+			$description .= $update_text;
+			$disabled     = true;
 		}
 
 		if ( ! $this->is_legacy_feature( $feature_id ) && ! $disabled && $this->verify_did_woocommerce_init() ) {
