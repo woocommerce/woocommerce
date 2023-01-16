a8a446ea0288afd69de0900e148a80603633c784
Lourens Schep <lourensschep@gmail.com>
2023-01-16 12:07:42 +0100

    Update story to more accurately reflect use case and add tooltip example

--- packages/js/components/src/product-fields/stories/index.tsx
+++ packages/js/components/src/product-fields/stories/index.tsx
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import { useState, createElement } from '@wordpress/element';
-import { createRegistry, RegistryProvider, select } from '@wordpress/data';
+import { createRegistry, RegistryProvider } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -19,36 +19,75 @@ registry.register( store );
 
 registerCoreProductFields();
 
+const fieldConfigs = [
+	{
+		name: 'text-field',
+		type: 'text',
+		label: 'Text field',
+	},
+	{
+		name: 'number-field',
+		type: 'number',
+		label: 'Number field',
+	},
+	{
+		name: 'toggle-field',
+		type: 'toggle',
+		label: 'Toggle field',
+	},
+	{
+		name: 'checkbox-field',
+		type: 'checkbox',
+		label: 'Checkbox field',
+	},
+	{
+		name: 'radio-field',
+		type: 'radio',
+		label: 'Radio field',
+		options: [
+			{ label: 'Option', value: 'option' },
+			{ label: 'Option 2', value: 'option2' },
+			{ label: 'Option 3', value: 'option3' },
+		],
+	},
+	{
+		name: 'basic-select-control-field',
+		type: 'basic-select-control',
+		label: 'Basic select control field',
+		options: [
+			{ label: 'Option', value: 'option' },
+			{ label: 'Option 2', value: 'option2' },
+			{ label: 'Option 3', value: 'option3' },
+		],
+	},
+];
+
 const RenderField = () => {
-	const fields: string[] = select( store ).getRegisteredProductFields();
 	const [ selectedField, setSelectedField ] = useState(
-		fields ? fields[ 0 ] : undefined
+		fieldConfigs[ 0 ].name || undefined
 	);
 	const [ value, setValue ] = useState();
 
 	const handleChange = ( event ) => {
 		setSelectedField( event.target.value );
 	};
+	const selectedFieldConfig = fieldConfigs.find(
+		( f ) => f.name === selectedField
+	);
 	return (
 		<div>
 			<select value={ selectedField } onChange={ handleChange }>
-				{ fields.map( ( field ) => (
-					<option key={ field } value={ field }>
-						{ field }
+				{ fieldConfigs.map( ( field ) => (
+					<option key={ field.name } value={ field.name }>
+						{ field.label }
 					</option>
 				) ) }
 			</select>
-			{ selectedField &&
-				renderField( selectedField, {
+			{ selectedFieldConfig &&
+				renderField( selectedFieldConfig.type, {
 					value,
-					name: 'test',
-					label: 'Test field',
 					onChange: setValue,
-					options: [
-						{ label: 'Option', value: 'option' },
-						{ label: 'Option 2', value: 'option2' },
-						{ label: 'Option 3', value: 'option3' },
-					],
+					...selectedFieldConfig,
 				} ) }
 		</div>
 	);
@@ -58,6 +97,21 @@ export const Basic: React.FC = () => {
 	return (
 		<RegistryProvider value={ registry }>
 			<RenderField />
+		</RegistryProvider>
+	);
+};
+
+export const ToggleWithTooltip: React.FC = () => {
+	const [ value, setValue ] = useState();
+	return (
+		<RegistryProvider value={ registry }>
+			{ renderField( 'toggle', {
+				value,
+				onChange: setValue,
+				name: 'toggle',
+				label: 'Toggle with Tooltip',
+				tooltip: 'This is a sample tooltip',
+			} ) }
 		</RegistryProvider>
 	);
 };
