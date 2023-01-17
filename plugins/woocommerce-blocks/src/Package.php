f1e79d6ca851f27b7c792200a5d9d8920ffa0f77
Luigi <gigitux@gmail.com>
2023-01-17 15:10:39 +0100

    bump version to 9.5.0-dev

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -109,7 +109,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '9.4.0';
+					$version = '9.5.0-dev';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
