1763376e3dbea0bd80d8b5e78f01418ec96b24d2
moon <moon.kyong@automattic.com>
2023-01-17 13:08:58 -0800

    Remove unnecessary empty space and convert comment stlye

--- packages/js/components/src/table/placeholder.tsx
+++ packages/js/components/src/table/placeholder.tsx
@@ -11,16 +11,18 @@ import Table from './table';
 import { QueryProps, TableHeader } from './types';
 
 type TablePlaceholderProps = {
-	// An object of the query parameters passed to the page
+	/**  An object of the query parameters passed to the page */
 	query?: QueryProps;
-	// A label for the content in this table.
+	/**  A label for the content in this table. */
 	caption: string;
-	// An integer with the number of rows to display.
+	/**  An integer with the number of rows to display. */
 	numberOfRows?: number;
-
+	/**
+	 * Which column should be the row header, defaults to the first item (`0`) (but could be set to `1`, if the first col
+	 * is checkboxes, for example). Set to false to disable row headers.
+	 */
 	rowHeader?: number | false;
-
-	// An array of column headers (see `Table` props).
+	/** An array of column headers (see `Table` props). */
 	headers: Array< TableHeader >;
 };
 
