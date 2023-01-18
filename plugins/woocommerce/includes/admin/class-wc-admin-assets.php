c71573d7f9330873005b1ac0d78f2492e2cdcf5a
Joshua T Flowers <joshuatf@gmail.com>
2023-01-18 15:44:24 -0800

    Update product links when new product management experience is enabled (#36382)
    * Replace add new product link with new experience
    
    * Replace add new button on Products page
    
    * Update all edit product links to point to new experience
    
    * Update default add_product value to null
    
    * Add changelog entry
    
    * Fix additional space around equals sign

--- plugins/woocommerce/includes/admin/class-wc-admin-assets.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-assets.php
@@ -7,6 +7,7 @@
  */
 
 use Automattic\Jetpack\Constants;
+use Automattic\WooCommerce\Admin\Features\Features;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -214,6 +215,7 @@ if ( ! class_exists( 'WC_Admin_Assets', false ) ) :
 						'gateway_toggle' => wp_create_nonce( 'woocommerce-toggle-payment-gateway-enabled' ),
 					),
 					'urls'                              => array(
+						'add_product'     => Features::is_enabled( 'new-product-management-experience' ) ? esc_url_raw( admin_url( 'admin.php?page=wc-admin&path=/add-product' ) ) : null,
 						'import_products' => current_user_can( 'import' ) ? esc_url_raw( admin_url( 'edit.php?post_type=product&page=product_importer' ) ) : null,
 						'export_products' => current_user_can( 'export' ) ? esc_url_raw( admin_url( 'edit.php?post_type=product&page=product_exporter' ) ) : null,
 					),
