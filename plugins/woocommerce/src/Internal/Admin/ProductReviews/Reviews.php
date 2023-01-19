c78bb8341dc86d9edf31a3cadbdc828929b913df
Mert Şakar <mertsakar@hotmail.com>
2023-01-19 17:56:11 +0300

    Fix issue #34344 - Cannot edit replies to product reviews (#35888)
    * Fix issue #34344 - Cannot edit replies to product reviews
    
    * Simplify logic in product review edit screen.
    
    * Add changelog
    
    Co-authored-by: Jorge A. Torres <jorge.torres@automattic.com>

--- plugins/woocommerce/src/Internal/Admin/ProductReviews/Reviews.php
+++ plugins/woocommerce/src/Internal/Admin/ProductReviews/Reviews.php
@@ -533,12 +533,7 @@ class Reviews {
 			$comment    = get_comment( $comment_id ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 		}
 
-		$is_reply = false;
-
-		if ( isset( $comment->comment_parent ) && $comment->comment_parent > 0 ) {
-			$is_reply = true;
-			$comment  = get_comment( $comment->comment_parent ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-		}
+		$is_reply = isset( $comment->comment_parent ) && $comment->comment_parent > 0;
 
 		// Only replace the translated text if we are editing a comment left on a product (ie. a review).
 		if ( isset( $comment->comment_post_ID ) && get_post_type( $comment->comment_post_ID ) === 'product' ) {
