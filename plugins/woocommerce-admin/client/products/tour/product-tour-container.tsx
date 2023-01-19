9e16aa05bdf44ae6f990217e3d04d94ca18962aa
Joshua Flowers <joshuatf@gmail.com>
2023-01-19 14:50:25 -0800

    Show product CES footer on product tour close

--- plugins/woocommerce-admin/client/products/tour/product-tour-container.tsx
+++ plugins/woocommerce-admin/client/products/tour/product-tour-container.tsx
@@ -1,21 +1,50 @@
 /**
+ * External dependencies
+ */
+import { useDispatch } from '@wordpress/data';
+import { OPTIONS_STORE_NAME } from '@woocommerce/data';
+
+/**
  * Internal dependencies
  */
 import { ProductTour } from './product-tour';
 import { ProductTourModal } from './product-tour-modal';
+import { useProductMVPCESFooter } from '~/customer-effort-score-tracks/use-product-mvp-ces-footer';
 import { useProductTour } from './use-product-tour';
 
 export const ProductTourContainer: React.FC = () => {
 	const { dismissModal, endTour, isModalHidden, isTouring, startTour } =
 		useProductTour();
+	// const { updateOptions } = useDispatch( OPTIONS_STORE_NAME );
+	const { showCesFooter } = useProductMVPCESFooter();
+	// const showCesFooter = () => {
+	// 	updateOptions( {
+	// 		[ PRODUCT_MVP_CES_ACTION_OPTION_NAME ]: 'new_product',
+	// 	} );
+	// }
 
 	if ( isTouring ) {
-		return <ProductTour onClose={ endTour } />;
+		return (
+			<ProductTour
+				onClose={ () => {
+					endTour();
+					showCesFooter();
+				} }
+			/>
+		);
 	}
 
 	if ( isModalHidden ) {
 		return null;
 	}
 
-	return <ProductTourModal onClose={ dismissModal } onStart={ startTour } />;
+	return (
+		<ProductTourModal
+			onClose={ () => {
+				dismissModal();
+				showCesFooter();
+			} }
+			onStart={ startTour }
+		/>
+	);
 };
