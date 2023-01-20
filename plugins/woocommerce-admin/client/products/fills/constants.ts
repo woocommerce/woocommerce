67d811a67d121f999e24300ad44f7e46bae258be
Joel Thiessen <444632+joelclimbsthings@users.noreply.github.com>
2023-01-20 08:56:56 -0800

    Migrating attributes section in product editor to slot fills (#36483)

--- plugins/woocommerce-admin/client/products/fills/constants.ts
+++ plugins/woocommerce-admin/client/products/fills/constants.ts
@@ -2,6 +2,7 @@ export const PRODUCT_DETAILS_SLUG = 'product-details';
 
 export const DETAILS_SECTION_ID = 'general/details';
 export const IMAGES_SECTION_ID = 'general/images';
+export const ATTRIBUTES_SECTION_ID = 'general/attributes';
 
 export const TAB_GENERAL_ID = 'tab/general';
 export const PLUGIN_ID = 'woocommerce';
--- plugins/woocommerce-admin/client/products/fills/index.ts
+++ plugins/woocommerce-admin/client/products/fills/index.ts
@@ -5,3 +5,4 @@ import './product-form-fills';
 
 export * from './details-section/details-section-fills';
 export * from './images-section/images-section-fills';
+export * from './attributes-section/attributes-section-fills';
