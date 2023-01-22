291880e435e1e023a787050d0225bfb75d9fb985
Willington Vega <wvega@wvega.com>
2023-01-22 15:59:46 -0500

    Fix space usage

--- plugins/woocommerce/tests/php/includes/class-woocommerce-test.php
+++ plugins/woocommerce/tests/php/includes/class-woocommerce-test.php
@@ -9,9 +9,9 @@ class WooCommerce_Test extends \WC_Unit_Test_Case {
 	 * Test that the $api property is defined, public and initialized correctly.
 	 */
 	public function test_api_property() {
-		$property = new ReflectionProperty(WooCommerce::class, 'api');
+		$property = new ReflectionProperty( WooCommerce::class, 'api' );
 
-		$this->assertTrue($property->isPublic());
-		$this->assertInstanceOf(WC_API::class, $property->getValue(WC()));
+		$this->assertTrue( $property->isPublic() );
+		$this->assertInstanceOf( WC_API::class, $property->getValue( WC() ) );
 	}
 }
