4ef40df35c98895b44a2f797198349206b5122c8
Thomas Roberts <5656702+opr@users.noreply.github.com>
2023-01-23 09:56:14 +0000

    Stop notice removal when current notices changes (https://github.com/woocommerce/woocommerce-blocks/pull/8253)
    * Stop notice removal when current notices changes
    
    * Move select inside useEffect

--- plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/filled-cart-block/frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/filled-cart-block/frontend.tsx
@@ -4,6 +4,9 @@
 import classnames from 'classnames';
 import { SidebarLayout } from '@woocommerce/base-components/sidebar-layout';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
+import { useEffect } from '@wordpress/element';
+import { decodeEntities } from '@wordpress/html-entities';
+import { select, useDispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -19,6 +22,35 @@ const FrontendBlock = ( {
 } ): JSX.Element | null => {
 	const { cartItems, cartIsLoading } = useStoreCart();
 	const { hasDarkControls } = useCartBlockContext();
+	const { createErrorNotice, removeNotice } = useDispatch( 'core/notices' );
+
+	useEffect( () => {
+		/*
+		 * The code for removing old notices is also present in the filled-mini-cart-contents-block/frontend.tsx file and
+		 * will take care of removing outdated errors in the Mini Cart block.
+		 */
+		const currentlyDisplayedErrorNoticeCodes = select( 'core/notices' )
+			.getNotices( 'wc/cart' )
+			.filter(
+				( notice ) =>
+					notice.status === 'error' && notice.type === 'default'
+			)
+			.map( ( notice ) => notice.id );
+
+		// Clear errors out of the store before adding the new ones from the response.
+		currentlyDisplayedErrorNoticeCodes.forEach( ( id ) => {
+			removeNotice( id, 'wc/cart' );
+		} );
+
+		// Ensures any cart errors listed in the API response get shown.
+		cartItemErrors.forEach( ( error ) => {
+			createErrorNotice( decodeEntities( error.message ), {
+				isDismissible: true,
+				id: error.code,
+				context: 'wc/cart',
+			} );
+		} );
+	}, [ createErrorNotice, cartItemErrors, removeNotice ] );
 
 	if ( cartIsLoading || cartItems.length >= 1 ) {
 		return (
--- plugins/woocommerce-blocks/assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/filled-mini-cart-contents-block/frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/filled-mini-cart-contents-block/frontend.tsx
@@ -4,6 +4,10 @@
 import { StoreNoticesContainer } from '@woocommerce/blocks-checkout';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
 
+import { select, useDispatch } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
+import { decodeEntities } from '@wordpress/html-entities';
+
 type FilledMiniCartContentsBlockProps = {
 	children: JSX.Element;
 	className: string;
@@ -13,7 +17,42 @@ const FilledMiniCartContentsBlock = ( {
 	children,
 	className,
 }: FilledMiniCartContentsBlockProps ): JSX.Element | null => {
-	const { cartItems } = useStoreCart();
+	const { cartItems, cartItemErrors } = useStoreCart();
+
+	const { createErrorNotice, removeNotice } = useDispatch( 'core/notices' );
+
+	// Ensures any cart errors listed in the API response get shown.
+	useEffect( () => {
+		/*
+		 * The code for removing old notices is also present in the filled-mini-cart-contents-block/frontend.tsx file and
+		 * will take care of removing outdated errors in the Mini Cart block.
+		 */
+		const currentlyDisplayedErrorNoticeCodes = select( 'core/notices' )
+			.getNotices( 'wc/cart' )
+			.filter(
+				( notice ) =>
+					notice.status === 'error' && notice.type === 'default'
+			)
+			.map( ( notice ) => notice.id );
+
+		// Clear errors out of the store before adding the new ones from the response.
+		currentlyDisplayedErrorNoticeCodes.forEach( ( id ) => {
+			removeNotice( id, 'wc/cart' );
+		} );
+
+		// Clear errors out of the store before adding the new ones from the response.
+		currentlyDisplayedErrorNoticeCodes.forEach( ( id ) => {
+			removeNotice( id, 'wc/cart' );
+		} );
+
+		cartItemErrors.forEach( ( error ) => {
+			createErrorNotice( decodeEntities( error.message ), {
+				isDismissible: false,
+				id: error.code,
+				context: 'wc/cart',
+			} );
+		} );
+	}, [ createErrorNotice, cartItemErrors, removeNotice ] );
 
 	if ( cartItems.length === 0 ) {
 		return null;
