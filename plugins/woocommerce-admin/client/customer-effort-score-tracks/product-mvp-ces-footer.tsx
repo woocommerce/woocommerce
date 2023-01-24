75fdea3cd4904a041ad4ced5e4772adc0a077721
Fernando Marichal <fernando.marichal@automattic.com>
2023-01-23 22:43:17 -0300

    Add survey when disabling new experience (#36544)
    * Add customer-score-tracks data
    
    * Add callback after disabling new exp
    
    * Add TransientNotice after filling out the survey
    
    # Conflicts:
    #	plugins/woocommerce/src/Admin/Features/NewProductManagementExperience.php
    
    * Remove comments
    
    * Remove NEW_PRODUCT_MANAGEMENT_FEEDBACK
    
    * Add changelog
    
    * Rename const
    
    * Remove queryParam after showing notice
    
    * Fix lint
    
    * Fix lint 2.0
    
    * Remove empty line
    
    * Refactor `maybe_show_disabled_notice`
    
    * Fix lint 3.0
    
    Co-authored-by: Fernando Marichal <contacto@fernandomarichal.com>

--- plugins/woocommerce-admin/client/customer-effort-score-tracks/product-mvp-ces-footer.tsx
+++ plugins/woocommerce-admin/client/customer-effort-score-tracks/product-mvp-ces-footer.tsx
@@ -21,9 +21,12 @@ import { STORE_KEY } from './data/constants';
 
 export const PRODUCT_MVP_CES_ACTION_OPTION_NAME =
 	'woocommerce_ces_product_mvp_ces_action';
+export const NEW_PRODUCT_MANAGEMENT =
+	'woocommerce_new_product_management_enabled';
 
 export const ProductMVPCESFooter: React.FC = () => {
-	const { showCesModal } = useDispatch( STORE_KEY );
+	const { showCesModal, showProductMVPFeedbackModal } =
+		useDispatch( STORE_KEY );
 	const { updateOptions } = useDispatch( OPTIONS_STORE_NAME );
 	const {
 		cesAction,
@@ -98,6 +101,16 @@ export const ProductMVPCESFooter: React.FC = () => {
 		} );
 	};
 
+	const onDisablingNewProductExperience = () => {
+		updateOptions( {
+			[ PRODUCT_MVP_CES_ACTION_OPTION_NAME ]: 'hide',
+		} );
+		updateOptions( {
+			[ NEW_PRODUCT_MANAGEMENT ]: 'no',
+		} );
+		showProductMVPFeedbackModal();
+	};
+
 	const onDisablingCES = () => {
 		updateOptions( {
 			[ PRODUCT_MVP_CES_ACTION_OPTION_NAME ]: 'hide',
@@ -125,7 +138,7 @@ export const ProductMVPCESFooter: React.FC = () => {
 								{ __( 'Share feedback', 'woocommerce' ) }
 							</Button>
 							<Button
-								onClick={ onDisablingCES }
+								onClick={ onDisablingNewProductExperience }
 								variant="tertiary"
 							>
 								{ __( 'Turn it off', 'woocommerce' ) }
