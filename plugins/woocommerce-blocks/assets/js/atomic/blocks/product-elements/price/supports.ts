1e84ec4c99eadf3b869ba7f8b578e0eaeb6052bc
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-01-24 12:52:49 +0100

    Product Price: allow margin styles in WC core (https://github.com/woocommerce/woocommerce-blocks/pull/8269)

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/price/supports.ts
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/price/supports.ts
@@ -24,12 +24,11 @@ export const supports = {
 			__experimentalFontStyle: true,
 			__experimentalSkipSerialization: true,
 		},
-		...( typeof __experimentalGetSpacingClassesAndStyles === 'function' && {
-			spacing: {
-				margin: true,
-				__experimentalSkipSerialization: true,
-			},
-		} ),
 		__experimentalSelector: '.wc-block-components-product-price',
+	} ),
+	...( typeof __experimentalGetSpacingClassesAndStyles === 'function' && {
+		spacing: {
+			margin: true,
+		},
 	} ),
 };
