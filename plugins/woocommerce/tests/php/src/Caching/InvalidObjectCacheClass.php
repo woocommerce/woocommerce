7619c335ffbd31418010cc5380781a05591c3ba4
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2023-01-25 12:29:55 +0530

    Fix unit tests for object cache to accomodate recent changes.

--- plugins/woocommerce/tests/php/src/Caching/InvalidObjectCacheClass.php
+++ plugins/woocommerce/tests/php/src/Caching/InvalidObjectCacheClass.php
@@ -17,4 +17,25 @@ class InvalidObjectCacheClass extends ObjectCache {
 	}
 
 	// phpcs:enable Squiz.Commenting
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function get_object_id( $object ) {
+		// TODO: Implement get_object_id() method.
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function validate( $object ): ?array {
+		// TODO: Implement validate() method.
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function get_from_datastore( $id ) {
+		// TODO: Implement get_from_datastore() method.
+	}
 }
