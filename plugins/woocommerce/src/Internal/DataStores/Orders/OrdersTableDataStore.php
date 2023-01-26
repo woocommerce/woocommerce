5bfaecf981e594784d5337ccad6f7f5c9ce2351a
Nestor Soriano <konamiman@konamiman.com>
2023-01-26 11:29:21 +0100

    Fix inappropriate deletion of order from posts with sync off
    When the orders table is authoritative and sync is off, deleting
    and order should not delete the order record from the posts table,
    this commit fixes that.
    
    Additionally, OrdersTableDataStore::delete will now trigger the
    woocommerce_delete_order action in all cases, even when the
    method is called while the posts table is authoritative.

--- plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableDataStore.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableDataStore.php
@@ -1766,14 +1766,14 @@ FROM $order_meta_table
 
 			$order->set_id( 0 );
 
-			// If this datastore method is called while the posts table is authoritative, refrain from deleting post data.
-			if ( $order->get_data_store()->get_current_class_name() !== self::class ) {
-				return;
+			// If this datastore method is called while the posts table is authoritative, or if data synchronization is disabled,
+			// refrain from deleting post data.
+			$data_synchronizer = wc_get_container()->get( DataSynchronizer::class );
+			if ( $data_synchronizer->data_sync_is_enabled() && $order->get_data_store()->get_current_class_name() === self::class ) {
+				// Delete the associated post, which in turn deletes order items, etc. through {@see WC_Post_Data}.
+				// Once we stop creating posts for orders, we should do the cleanup here instead.
+				wp_delete_post( $order_id );
 			}
-
-			// Delete the associated post, which in turn deletes order items, etc. through {@see WC_Post_Data}.
-			// Once we stop creating posts for orders, we should do the cleanup here instead.
-			wp_delete_post( $order_id );
 
 			do_action( 'woocommerce_delete_order', $order_id ); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
 		} else {
