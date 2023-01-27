ed1bae37a980b657dd961d92ff4483212492aecb
Joel <dygerati@gmail.com>
2023-01-26 17:07:59 -0800

    Simplifying IDs used for slots

--- plugins/woocommerce-admin/client/products/fills/constants.ts
+++ plugins/woocommerce-admin/client/products/fills/constants.ts
@@ -1,14 +1,24 @@
-export const DETAILS_SECTION_ID = 'section/general/details';
-export const IMAGES_SECTION_ID = 'section/general/images';
-export const ATTRIBUTES_SECTION_ID = 'section/general/attributes';
-export const PRICING_SECTION_BASIC_ID = 'section/pricing/basic';
-export const PRICING_SECTION_TAXES_ID = 'section/pricing/taxes';
-export const PRICING_SECTION_TAXES_ADVANCED_ID =
-	'section/pricing/taxes/advanced';
-export const INVENTORY_SECTION_ID = 'section/inventory/basic';
-export const INVENTORY_SECTION_ADVANCED_ID = 'section/inventory/advanced';
-export const SHIPPING_SECTION_BASIC_ID = 'section/shipping/basic';
-export const SHIPPING_SECTION_DIMENSIONS_ID = 'section/shipping/dimensions';
+export const TAB_GENERAL_ID = 'tab/general';
+export const TAB_PRICING_ID = 'tab/pricing';
+export const TAB_INVENTORY_ID = 'tab/inventory';
+export const TAB_SHIPPING_ID = 'tab/shipping';
+export const TAB_OPTIONS_ID = 'tab/options';
+
+export const VARIANT_TAB_GENERAL_ID = `variant/${ TAB_GENERAL_ID }`;
+export const VARIANT_TAB_PRICING_ID = `variant/${ TAB_PRICING_ID }`;
+export const VARIANT_TAB_INVENTORY_ID = `variant/${ TAB_INVENTORY_ID }`;
+export const VARIANT_TAB_SHIPPING_ID = `variant/${ TAB_SHIPPING_ID }`;
+
+export const DETAILS_SECTION_ID = `${ TAB_GENERAL_ID }/details`;
+export const IMAGES_SECTION_ID = `${ TAB_GENERAL_ID }/images`;
+export const ATTRIBUTES_SECTION_ID = `${ TAB_GENERAL_ID }/attributes`;
+export const PRICING_SECTION_BASIC_ID = `${ TAB_PRICING_ID }/basic`;
+export const PRICING_SECTION_TAXES_ID = `${ TAB_PRICING_ID }/taxes`;
+export const PRICING_SECTION_TAXES_ADVANCED_ID = `${ TAB_PRICING_ID }/taxes/advanced`;
+export const INVENTORY_SECTION_ID = `${ TAB_INVENTORY_ID }/basic`;
+export const INVENTORY_SECTION_ADVANCED_ID = `${ TAB_INVENTORY_ID }/advanced`;
+export const SHIPPING_SECTION_BASIC_ID = `${ TAB_SHIPPING_ID }/basic`;
+export const SHIPPING_SECTION_DIMENSIONS_ID = `${ TAB_SHIPPING_ID }/dimensions`;
 
 export const VARIANT_PRICING_SECTION_BASIC_ID = `variant/${ PRICING_SECTION_BASIC_ID }`;
 export const VARIANT_PRICING_SECTION_TAXES_ID = `variant/${ PRICING_SECTION_TAXES_ID }`;
@@ -17,17 +27,6 @@ export const VARIANT_INVENTORY_SECTION_ID = `variant/${ INVENTORY_SECTION_ID }`;
 export const VARIANT_INVENTORY_SECTION_ADVANCED_ID = `variant/${ INVENTORY_SECTION_ADVANCED_ID }`;
 export const VARIANT_SHIPPING_SECTION_BASIC_ID = `variant/${ SHIPPING_SECTION_BASIC_ID }`;
 export const VARIANT_SHIPPING_SECTION_DIMENSIONS_ID = `variant/${ SHIPPING_SECTION_DIMENSIONS_ID }`;
-
-export const TAB_INVENTORY_ID = 'tab/inventory';
-export const TAB_GENERAL_ID = 'tab/general';
-export const TAB_SHIPPING_ID = 'tab/shipping';
-export const TAB_PRICING_ID = 'tab/pricing';
-export const TAB_OPTIONS_ID = 'tab/options';
-
-export const VARIANT_TAB_INVENTORY_ID = `variant/${ TAB_INVENTORY_ID }`;
-export const VARIANT_TAB_GENERAL_ID = `variant/${ TAB_GENERAL_ID }`;
-export const VARIANT_TAB_SHIPPING_ID = `variant/${ TAB_SHIPPING_ID }`;
-export const VARIANT_TAB_PRICING_ID = `variant/${ TAB_PRICING_ID }`;
 
 export const PLUGIN_ID = 'woocommerce';
 export const PRODUCT_DETAILS_SLUG = 'product-details';
