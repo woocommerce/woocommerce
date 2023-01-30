2c04c47fd88c2b923733eb81e569068b673d3e41
Nathan Schneider <nsschneider1@gmail.com>
2023-01-30 15:14:23 -0300

    Migrate feature field

--- plugins/woocommerce-admin/client/products/fills/details-section/details-field-feature.tsx
+++ plugins/woocommerce-admin/client/products/fills/details-section/details-field-feature.tsx
@@ -11,6 +11,8 @@ import {
 import interpolateComponents from '@automattic/interpolate-components';
 import { Product } from '@woocommerce/data';
 import { recordEvent } from '@woocommerce/tracks';
+import { Controller } from 'react-hook-form';
+import _omit from 'lodash/omit';
 
 /**
  * Internal dependencies
@@ -19,47 +21,59 @@ import { getCheckboxTracks } from '../../sections/utils';
 import { PRODUCT_DETAILS_SLUG } from '../constants';
 
 export const DetailsFeatureField = () => {
-	const { getCheckboxControlProps } = useFormContext< Product >();
+	const { control } = useFormContext< Product >();
+
+	const checkboxTracksFn = getCheckboxTracks( 'featured' );
 
 	return (
-		<CheckboxControl
-			label={
-				<>
-					{ __( 'Feature this product', 'woocommerce' ) }
-					<Tooltip
-						text={ interpolateComponents( {
-							mixedString: __(
-								'Include this product in a featured section on your website with a widget or shortcode. {{moreLink/}}',
-								'woocommerce'
-							),
-							components: {
-								moreLink: (
-									<Link
-										href="https://woocommerce.com/document/woocommerce-shortcodes/#products"
-										target="_blank"
-										type="external"
-										onClick={ () =>
-											recordEvent(
-												'add_product_learn_more',
-												{
-													category:
-														PRODUCT_DETAILS_SLUG,
+		<Controller
+			name="featured"
+			control={ control }
+			render={ ( { field } ) => (
+				<CheckboxControl
+					{ ..._omit( field, [ 'value' ] ) }
+					onChange={ ( value ) => {
+						field.onChange( value );
+						checkboxTracksFn.onChange( value );
+					} }
+					label={
+						<>
+							{ __( 'Feature this product', 'woocommerce' ) }
+							<Tooltip
+								text={ interpolateComponents( {
+									mixedString: __(
+										'Include this product in a featured section on your website with a widget or shortcode. {{moreLink/}}',
+										'woocommerce'
+									),
+									components: {
+										moreLink: (
+											<Link
+												href="https://woocommerce.com/document/woocommerce-shortcodes/#products"
+												target="_blank"
+												type="external"
+												onClick={ () =>
+													recordEvent(
+														'add_product_learn_more',
+														{
+															category:
+																PRODUCT_DETAILS_SLUG,
+														}
+													)
 												}
-											)
-										}
-									>
-										{ __( 'Learn more', 'woocommerce' ) }
-									</Link>
-								),
-							},
-						} ) }
-					/>
-				</>
-			}
-			{ ...getCheckboxControlProps(
-				'featured',
-				getCheckboxTracks( 'featured' )
+											>
+												{ __(
+													'Learn more',
+													'woocommerce'
+												) }
+											</Link>
+										),
+									},
+								} ) }
+							/>
+						</>
+					}
+				/>
 			) }
-		/>
+		></Controller>
 	);
 };
