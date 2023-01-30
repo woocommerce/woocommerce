3c4d25b82df8e2318d5efaa3d9fdfdc912a0b2f6
Nathan Schneider <nsschneider1@gmail.com>
2023-01-30 15:07:40 -0300

    Migrate categories field

--- plugins/woocommerce-admin/client/products/fills/details-section/details-field-categories.tsx
+++ plugins/woocommerce-admin/client/products/fills/details-section/details-field-categories.tsx
@@ -4,6 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { useFormContext } from '@woocommerce/components';
 import { Product, ProductCategory } from '@woocommerce/data';
+import { Controller } from 'react-hook-form';
 
 /**
  * Internal dependencies
@@ -11,15 +12,22 @@ import { Product, ProductCategory } from '@woocommerce/data';
 import { CategoryField } from '../../fields/category-field';
 
 export const DetailsCategoriesField = () => {
-	const { getInputProps } = useFormContext< Product >();
+	const { control } = useFormContext< Product >();
 
 	return (
-		<CategoryField
-			label={ __( 'Categories', 'woocommerce' ) }
-			placeholder={ __( 'Search or create category…', 'woocommerce' ) }
-			{ ...getInputProps< Pick< ProductCategory, 'id' | 'name' >[] >(
-				'categories'
+		<Controller
+			name="categories"
+			control={ control }
+			render={ ( { field } ) => (
+				<CategoryField
+					{ ...field }
+					label={ __( 'Categories', 'woocommerce' ) }
+					placeholder={ __(
+						'Search or create category…',
+						'woocommerce'
+					) }
+				/>
 			) }
-		/>
+		></Controller>
 	);
 };
