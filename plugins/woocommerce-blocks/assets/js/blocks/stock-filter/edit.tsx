3059287d84df2dfe49f700c51e67af9aca67f8a3
Roy Ho <roykho77@gmail.com>
2023-01-31 05:58:00 -0800

    Update apply button description to be more clear for filter blocks (https://github.com/woocommerce/woocommerce-blocks/pull/8339)

--- plugins/woocommerce-blocks/assets/js/blocks/stock-filter/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/stock-filter/edit.tsx
@@ -126,17 +126,10 @@ const Edit = ( {
 							"Show 'Apply filters' button",
 							'woo-gutenberg-products-block'
 						) }
-						help={
-							showFilterButton
-								? __(
-										'Products will only update when the button is clicked.',
-										'woo-gutenberg-products-block'
-								  )
-								: __(
-										'Products will update as soon as attributes are selected.',
-										'woo-gutenberg-products-block'
-								  )
-						}
+						help={ __(
+							'Products will update when the button is clicked.',
+							'woo-gutenberg-products-block'
+						) }
 						checked={ showFilterButton }
 						onChange={ ( value ) =>
 							setAttributes( {
