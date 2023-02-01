b09a8826880e542fe39744697b69727c0b0bfd78
Nathan Schneider <nsschneider1@gmail.com>
2023-02-01 14:18:45 -0300

    Remove unused code

--- packages/js/components/src/form/nu-form.tsx
+++ packages/js/components/src/form/nu-form.tsx
@@ -1,30 +1,8 @@
 /**
  * External dependencies
  */
-import {
-	useForm,
-	UseFormReturn,
-	FieldErrors,
-	FieldValues,
-	UseFormReset,
-	FormProvider,
-	useFormContext,
-} from 'react-hook-form';
-import { createContext, createElement, useContext } from 'react';
-
-const FormContext2 = createContext(
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	{} as FormCompatibilityLayer< any >
-);
-
-interface FormCompatibilityLayer< Values extends FieldValues >
-	extends UseFormReturn {
-	errors: FieldErrors< Values >;
-	isDirty: boolean;
-	isValidForm: boolean;
-	touched?: Record< keyof Values, boolean >;
-	resetForm: UseFormReset< Values >;
-}
+import { useForm, FormProvider, useFormContext } from 'react-hook-form';
+import { createElement } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function NuForm( props: any ) {
