81650d3ae1ef4010091a46030f21e02c4a51ce2e
Sam Seay <samueljseay@gmail.com>
2023-02-02 16:53:15 +1300

    Log between checkouts

--- bin/pre-push.sh
+++ bin/pre-push.sh
@@ -32,7 +32,9 @@ if [ $? -ne 0 ]; then
 fi
 
 # Ensure both branches are tracked or check-changelogger-use will fail. Skip husky hooks re-running
+echo "checkout 1"
 HUSKY=0 git checkout $PROTECTED_BRANCH --quiet
+echo "checkout 2"
 HUSKY=0 git checkout $CURRENT_BRANCH --quiet
 
 php tools/monorepo/check-changelogger-use.php $PROTECTED_BRANCH $CURRENT_BRANCH
