a544f01c671c7ab35e8c8f1ce8218b503f2ea2ae
Sam Seay <samueljseay@gmail.com>
2023-02-02 17:01:28 +1300

    Revert changes to OG script

--- bin/pre-push.sh
+++ bin/pre-push.sh
@@ -31,10 +31,8 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-# Skip running husky hooks as a result of running this script.
-export HUSKY=0 
 # Ensure both branches are tracked or check-changelogger-use will fail.
 git checkout $PROTECTED_BRANCH --quiet
 git checkout $CURRENT_BRANCH --quiet
 
-# php tools/monorepo/check-changelogger-use.php $PROTECTED_BRANCH $CURRENT_BRANCH
+php tools/monorepo/check-changelogger-use.php $PROTECTED_BRANCH $CURRENT_BRANCH
