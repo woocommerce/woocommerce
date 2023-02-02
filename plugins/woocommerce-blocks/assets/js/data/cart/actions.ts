882b642e16927f9223a3c37db6a4f362de379c01
Seghir Nadir <nadir.seghir@gmail.com>
2023-02-02 10:12:52 +0100

    Switch to select instead of resolveSelector (https://github.com/woocommerce/woocommerce-blocks/pull/8356)

--- plugins/woocommerce-blocks/assets/js/data/cart/actions.ts
+++ plugins/woocommerce-blocks/assets/js/data/cart/actions.ts
@@ -22,7 +22,7 @@ import {
 import { ACTION_TYPES as types } from './action-types';
 import { apiFetchWithHeaders } from '../shared-controls';
 import { ReturnOrGeneratorYieldUnion } from '../mapped-types';
-import { CartDispatchFromMap, CartResolveSelectFromMap } from './index';
+import { CartDispatchFromMap, CartSelectFromMap } from './index';
 import type { Thunks } from './thunks';
 
 // Thunks are functions that can be dispatched, similar to actions creators
@@ -188,7 +188,7 @@ export const shippingRatesBeingSelected = ( isResolving: boolean ) =>
  */
 export const applyExtensionCartUpdate =
 	( args: ExtensionCartUpdateArgs ) =>
-	async ( { dispatch } ) => {
+	async ( { dispatch }: { dispatch: CartDispatchFromMap } ) => {
 		try {
 			const { response } = await apiFetchWithHeaders( {
 				path: '/wc/store/v1/cart/extensions',
@@ -213,7 +213,7 @@ export const applyExtensionCartUpdate =
  */
 export const applyCoupon =
 	( couponCode: string ) =>
-	async ( { dispatch } ) => {
+	async ( { dispatch }: { dispatch: CartDispatchFromMap } ) => {
 		try {
 			dispatch.receiveApplyingCoupon( couponCode );
 			const { response } = await apiFetchWithHeaders( {
@@ -243,7 +243,7 @@ export const applyCoupon =
  */
 export const removeCoupon =
 	( couponCode: string ) =>
-	async ( { dispatch } ) => {
+	async ( { dispatch }: { dispatch: CartDispatchFromMap } ) => {
 		try {
 			dispatch.receiveRemovingCoupon( couponCode );
 			const { response } = await apiFetchWithHeaders( {
@@ -276,7 +276,7 @@ export const removeCoupon =
  */
 export const addItemToCart =
 	( productId: number, quantity = 1 ) =>
-	async ( { dispatch } ) => {
+	async ( { dispatch }: { dispatch: CartDispatchFromMap } ) => {
 		try {
 			triggerAddingToCartEvent();
 			const { response } = await apiFetchWithHeaders( {
@@ -346,12 +346,12 @@ export const changeCartItemQuantity =
 	) =>
 	async ( {
 		dispatch,
-		resolveSelect,
+		select,
 	}: {
 		dispatch: CartDispatchFromMap;
-		resolveSelect: CartResolveSelectFromMap;
+		select: CartSelectFromMap;
 	} ) => {
-		const cartItem = await resolveSelect.getCartItem( cartItemKey );
+		const cartItem = select.getCartItem( cartItemKey );
 		if ( cartItem?.quantity === quantity ) {
 			return;
 		}
