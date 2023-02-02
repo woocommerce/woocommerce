2891da68c55b428e58ef7a6bdc2f1be7aa074f68
Sam Seay <samueljseay@gmail.com>
2023-02-02 16:48:42 +1300

    apparently its now HUSKY=0

--- bin/pre-push.sh
+++ bin/pre-push.sh
@@ -32,7 +32,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Ensure both branches are tracked or check-changelogger-use will fail.
-HUSKY_SKIP_HOOKS=1 git checkout $PROTECTED_BRANCH --quiet
-HUSKY_SKIP_HOOKS=1 git checkout $CURRENT_BRANCH --quiet
+HUSKY=0 git checkout $PROTECTED_BRANCH --quiet
+HUSKY=0 git checkout $CURRENT_BRANCH --quiet
 
 php tools/monorepo/check-changelogger-use.php $PROTECTED_BRANCH $CURRENT_BRANCH
