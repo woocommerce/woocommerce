bead51c8d28db0f9f39da5d116de380a83510306
Paul Sealock <psealock@gmail.com>
2023-02-13 12:06:32 +0100

    install packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
 	root: true,
 	extends: [ 'plugin:@woocommerce/eslint-plugin/recommended' ],
+	plugins: [ 'prettier' ],
+	rules: {
+		'prettier/prettier': 'error',
+	},
 };
--- plugins/woocommerce-admin/.eslintrc.js
+++ plugins/woocommerce-admin/.eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
 	extends: [ 'plugin:@woocommerce/eslint-plugin/recommended' ],
 	root: true,
+	plugins: [ 'prettier' ],
 	overrides: [
 		{
 			files: [ 'client/**/*.js', 'client/**/*.jsx', 'client/**/*.tsx' ],
 			rules: {
 				'react/react-in-jsx-scope': 'off',
+				'prettier/prettier': 'error',
 			},
 		},
 	],
