17628141b48c06072fb62e8926bd9218bfd2c90e
moon <moon.kyong@automattic.com>
2023-02-13 18:00:29 -0800

    Add reusuable plugin banner component

---
+++ plugins/woocommerce-admin/client/tasks/fills/experimental-shipping-recommendation/components/plugin-banner.tsx
@@ -0,0 +1,53 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import './wcs-banner.scss';
+
+type Feature = {
+	icon: string;
+	title: string;
+	description: string;
+};
+
+type PluginBannerProps = {
+	logo: {
+		image: string;
+		alt?: string;
+	};
+	features: Array< Feature >;
+};
+
+export const PluginBanner = ( { logo, features }: PluginBannerProps ) => {
+	return (
+		<div className="woocommerce-task-shipping-recommendation__plugins-install">
+			<div className="plugins-install__plugin-banner-image">
+				<img src={ logo.image } alt={ logo?.alt } />
+			</div>
+			<div className="plugins-install__list">
+				{ features.map( ( feature: Feature, index ) => {
+					return (
+						<div
+							className="plugins-install__list-item"
+							key={ index }
+						>
+							<div className="plugins-install__list-icon">
+								<img src={ feature.icon } alt="" />
+							</div>
+							<div>
+								<div>
+									<strong>{ feature.title }</strong>
+								</div>
+								<div>{ feature.description }</div>
+							</div>
+						</div>
+					);
+				} ) }
+			</div>
+		</div>
+	);
+};
