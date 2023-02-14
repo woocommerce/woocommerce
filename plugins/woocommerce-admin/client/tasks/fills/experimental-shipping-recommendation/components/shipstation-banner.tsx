bd21616946ad890dacf8e5f23ba5b93df318e560
moon <moon.kyong@automattic.com>
2023-02-13 18:16:13 -0800

    Convert ShipStationBanner to use the PluginBanner comp

--- plugins/woocommerce-admin/client/tasks/fills/experimental-shipping-recommendation/components/shipstation-banner.tsx
+++ plugins/woocommerce-admin/client/tasks/fills/experimental-shipping-recommendation/components/shipstation-banner.tsx
@@ -10,70 +10,39 @@ import ShipStationImage from '../images/shipstation.svg';
 import TimerImage from '../images/timer.svg';
 import StarImage from '../images/star.svg';
 import DiscountImage from '../images/discount.svg';
-import './plugin-banner.scss';
+import { PluginBanner } from './plugin-banner';
 
+const features = [
+	{
+		icon: TimerImage,
+		title: __( 'Save your time', 'woocommerce' ),
+		description: __(
+			'Import your orders automatically, no matter where you sell.',
+			'woocommerce'
+		),
+	},
+	{
+		icon: DiscountImage,
+		title: __( 'Save your money', 'woocommerce' ),
+		description: __(
+			'Live shipping rates amongst all the carrier choices.',
+			'woocommerce'
+		),
+	},
+	{
+		icon: StarImage,
+		title: __( 'Wow your shoppers', 'woocommerce' ),
+		description: __(
+			'Customize notification emails, packing slips, shipping labels.',
+			'woocommerce'
+		),
+	},
+];
 export const ShipStationBanner = () => {
 	return (
-		<div className="woocommerce-task-shipping-recommendation__plugins-install">
-			<div className="plugins-install__shipstation-image">
-				<img src={ ShipStationImage } alt="" />
-			</div>
-			<div className="plugins-install__list">
-				<div className="plugins-install__list-item">
-					<div className="plugins-install__list-icon">
-						<img src={ TimerImage } alt="" />
-					</div>
-					<div>
-						<div>
-							<strong>
-								{ __( 'Save your time', 'woocommerce' ) }
-							</strong>
-						</div>
-						<div>
-							{ __(
-								'Import your orders automatically, no matter where you sell.',
-								'woocommerce'
-							) }
-						</div>
-					</div>
-				</div>
-				<div className="plugins-install__list-item">
-					<div className="plugins-install__list-icon">
-						<img src={ DiscountImage } alt="" />
-					</div>
-					<div>
-						<div>
-							<strong>
-								{ __( 'Save your money', 'woocommerce' ) }
-							</strong>
-						</div>
-						<div>
-							{ __(
-								'Live shipping rates amongst all the carrier choices.',
-								'woocommerce'
-							) }
-						</div>
-					</div>
-				</div>
-				<div className="plugins-install__list-item">
-					<div className="plugins-install__list-icon">
-						<img src={ StarImage } alt="" />
-					</div>
-					<div>
-						<div>
-							<strong>
-								{ __( 'Wow your shoppers', 'woocommerce' ) }
-							</strong>
-						</div>
-						<div>
-							{ __(
-								'Customize notification emails, packing slips, shipping labels.',
-								'woocommerce'
-							) }
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<PluginBanner
+			logo={ { image: ShipStationImage } }
+			features={ features }
+		/>
 	);
 };
