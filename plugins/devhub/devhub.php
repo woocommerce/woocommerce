20a4ded4c24a27267f80b12f58ede9762dbed77d
Sam Seay <samueljseay@gmail.com>
2023-02-16 21:09:30 +1300

    A few adjustments to load JS assets and to render Gatsby content without head.

--- plugins/devhub/devhub.php
+++ plugins/devhub/devhub.php
@@ -10,9 +10,9 @@
  * Tested up to: 6.0
  * WC requires at least: 6.7
  * WC tested up to: 7.0
- * Text Domain: woocommerce-beta-tester
+ * Text Domain: woocommerce-dev-hub
  *
- * @package WC_Beta_Tester
+ * @package WC_DevHub
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -30,12 +30,36 @@ function create_gatsby_page() {
       'post_type' => 'page'
     );
 
-    $page_id = wp_insert_post($page);
-
-    if (! is_wp_error( $page_id ) ) {
-      update_post_meta($page_id, '_wp_page_template', 'gatsby.php');
-    }
+    wp_insert_post($page);
   }
 }
 
 register_activation_hook(__FILE__, 'create_gatsby_page');
+
+
+function gatsby_template( $original_template ) {
+	global $post;
+
+  if ($post->post_title == 'WooCommerce Developer Docs') {
+		return dirname(__FILE__) . '/gatsby.php';    
+  } 
+  
+	return $original_template;
+}
+
+add_filter( 'template_include', 'gatsby_template' );
+
+function enqueue_gatsby_assets() {
+  $gatsby_assets_dir = dirname(__FILE__) . '/site/public'; // Replace with actual path to Gatsby public directory
+  $gatsby_js_files = glob( $gatsby_assets_dir . '/*.js' );
+  
+  foreach ( $gatsby_js_files as $js_file ) {
+    $js_file_url = str_replace( $gatsby_assets_dir, get_site_url(), $js_file );    
+    $js_file_handle = 'gatsby-' . basename( $js_file, '.js' );
+    wp_register_script( $js_file_handle, $js_file_url, array(), null, true );
+    wp_enqueue_script( basename( $js_file ), $js_file_url, array(), null, true );
+  }
+}
+
+add_action( 'wp_enqueue_scripts', 'enqueue_gatsby_assets' );
+
--- plugins/devhub/site/gatsby-config.ts
+++ plugins/devhub/site/gatsby-config.ts
@@ -1,27 +1,28 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Dev Hub`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+	siteMetadata: {
+		title: `Dev Hub`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+
+	pathPrefix: '/wp-content/plugins/devhub/site/public',
+	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+	// If you use VSCode you can also use the GraphQL plugin
+	// Learn more at: https://gatsby.dev/graphql-typegen
+	graphqlTypegen: true,
+	plugins: [
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-mdx',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: './src/pages/',
+			},
+			__key: 'pages',
+		},
+	],
 };
 
 export default config;
