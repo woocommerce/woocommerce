964176acf2e650518336d8ff67c1f1d9e6b6a49e
Saad Tarhi <saad.trh@gmail.com>
2023-02-17 15:57:16 +0100

    Bumping version strings to new version.

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -109,7 +109,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '9.6.0';
+					$version = '9.6.1';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
