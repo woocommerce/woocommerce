e65458053adfc308ccec0f8e7d06bac0a477ed67
kmanijak <karol.manijak@automattic.com>
2023-02-17 15:09:26 +0100

    In editor use the components from @wordpress/components instead of wordpress-components (https://github.com/woocommerce/woocommerce-blocks/pull/8432)

--- plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-express-payment-block/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-express-payment-block/edit.tsx
@@ -3,7 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
-import { Placeholder, Button } from 'wordpress-components';
+import { Placeholder, Button } from '@wordpress/components';
 import { useExpressPaymentMethods } from '@woocommerce/base-context/hooks';
 import { Icon, payment } from '@wordpress/icons';
 import { ADMIN_URL } from '@woocommerce/settings';
