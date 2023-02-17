54151becc5b7162bb4a02e90db9d5afbac07be8e
Michael Pretty <prettyboymp@users.noreply.github.com>
2023-02-17 10:33:36 -0500

    Avoid preparing the table name for truncate query

--- plugins/woocommerce/src/Internal/ProductAttributesLookup/DataRegenerator.php
+++ plugins/woocommerce/src/Internal/ProductAttributesLookup/DataRegenerator.php
@@ -104,7 +104,7 @@ class DataRegenerator {
 		$this->data_store->unset_regeneration_in_progress_flag();
 
 		if ( $this->data_store->check_lookup_table_exists() ) {
-			$wpdb->query( $wpdb->prepare( 'TRUNCATE TABLE %s', $this->lookup_table_name ) );
+			$wpdb->query( "TRUNCATE TABLE {$this->lookup_table_name}" );
 		}
 	}
 
