52d65609bfc2d9ac70ce7ae32593117df1e80a31
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-02-21 14:04:35 +0100

    Fix e2e tests of the Product Details block (https://github.com/woocommerce/woocommerce-blocks/pull/8470)
    * Fix e2e tests which use the insertBlock function
    
    * Move more instances of insertBlock to insertWCBlock
    
    * Ensure block inserter is open before searching for a block
    
    * Get rid of openWidgetsEditorBlockInserter
    
    * Rename insertWCBlocks to insertBlockDontWaitForInsertClose
    
    * Fix e2e tests of the Product Details block
    
    * Migrate to insertBlockDontWaitForInsertClose instead of insertBlock
    
    * Go to single product template
    
    * Fix Product Details classname and slug in tests
    
    * Replace usage of 'foo' variable name
    
    * Fix typo in 'can not be inserted in a post' test

--- plugins/woocommerce-blocks/tests/e2e/specs/backend/single-product-details.test.js
+++ plugins/woocommerce-blocks/tests/e2e/specs/backend/single-product-details.test.js
@@ -4,7 +4,6 @@
 import {
 	canvas,
 	createNewPost,
-	insertBlock,
 	switchUserToAdmin,
 } from '@wordpress/e2e-test-utils';
 import { searchForBlock } from '@wordpress/e2e-test-utils/build/inserter';
@@ -14,15 +13,15 @@ import { searchForBlock } from '@wordpress/e2e-test-utils/build/inserter';
  */
 import {
 	filterCurrentBlocks,
-	goToSiteEditor,
+	goToTemplateEditor,
+	insertBlockDontWaitForInsertClose,
 	useTheme,
-	waitForCanvas,
 } from '../../utils.js';
 
 const block = {
-	name: 'Product Results Count',
-	slug: 'woocommerce/product-results-count',
-	class: '.wc-block-product-results-count',
+	name: 'Product Details',
+	slug: 'woocommerce/product-details',
+	class: '.wp-block-woocommerce-product-details',
 };
 
 describe( `${ block.name } Block`, () => {
@@ -40,18 +39,19 @@ describe( `${ block.name } Block`, () => {
 		useTheme( 'emptytheme' );
 
 		beforeEach( async () => {
-			await goToSiteEditor();
-			await waitForCanvas();
+			await goToTemplateEditor( {
+				postId: 'woocommerce/woocommerce//single-product',
+			} );
 		} );
 
 		it( 'can be inserted in FSE area', async () => {
-			await insertBlock( block.name );
+			await insertBlockDontWaitForInsertClose( block.name );
 			await expect( canvas() ).toMatchElement( block.class );
 		} );
 
 		it( 'can be inserted more than once', async () => {
-			await insertBlock( block.name );
-			await insertBlock( block.name );
+			await insertBlockDontWaitForInsertClose( block.name );
+			await insertBlockDontWaitForInsertClose( block.name );
 			const filteredBlocks = await filterCurrentBlocks(
 				( b ) => b.name === block.slug
 			);
