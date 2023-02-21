4b8e69fefe2a0a3f6155adab3c3cdddf0cb88d9d
Sam Seay <samueljseay@gmail.com>
2023-02-21 16:33:26 +1300

    Don't need a custom page anymore.

--- plugins/devhub/devhub.php
+++ plugins/devhub/devhub.php
@@ -17,37 +17,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-function create_static_site_page() {
-  $page_title = 'WooCommerce Developer Docs!';
-  $page_content = '';
-  $page_exists = get_page_by_title($page_title);
-
-  if (!$page_exists) {
-    $page = array(
-      'post_title' => $page_title,
-      'post_content' => $page_content,
-      'post_status' => 'publish',
-      'post_type' => 'page',
-    );
-
-    wp_insert_post($page);
-  }
-}
-
-register_activation_hook(__FILE__, 'create_static_site_page');
-
-// function gatsby_template( $original_template ) {
-// 	global $post;
-
-//   if ($post->post_title == 'WooCommerce Developer Docs') {
-// 		return dirname(__FILE__) . '/gatsby.php';    
-//   } 
-  
-// 	return $original_template;
-// }
-
-// add_filter( 'template_include', 'gatsby_template' );
-
+// Load static site template for web requests going to "/docs" or "/docs/<..>/..."
 function load_static_site_template( $original_template ) {
     $url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     $url_parts = parse_url($url);
@@ -58,5 +28,4 @@ function load_static_site_template( $original_template ) {
     return $template;
 }
 
-// Load custom template for web requests going to "/docs" or "/docs/<..>/..."
 add_filter( 'template_include', 'load_static_site_template' );
