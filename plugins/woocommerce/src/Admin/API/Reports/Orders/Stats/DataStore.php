d2a763d10bfc6daa1ad01cd43bd37c338eb6610d
Nathan Schneider <nsschneider1@gmail.com>
2023-02-22 09:59:48 -0300

    Assign date_column_name before calling parent constructor

--- plugins/woocommerce/src/Admin/API/Reports/Orders/Stats/DataStore.php
+++ plugins/woocommerce/src/Admin/API/Reports/Orders/Stats/DataStore.php
@@ -73,8 +73,8 @@ class DataStore extends ReportsDataStore implements DataStoreInterface {
 	 * Dynamically sets the date column name based on configuration
 	 */
 	public function __construct() {
-		parent::__construct();
 		$this->date_column_name = get_option( 'woocommerce_date_type', 'date_paid' );
+		parent::__construct();
 	}
 
 	/**
