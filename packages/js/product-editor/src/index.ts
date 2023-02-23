a8e801f1297af27b26602a41b84a89af5f59e95b
Joshua Flowers <joshuatf@gmail.com>
2023-02-22 17:48:58 -0800

    Move util import/exports to separate index file

--- packages/js/product-editor/src/index.ts
+++ packages/js/product-editor/src/index.ts
@@ -6,21 +6,4 @@ export {
 /**
  * Utils
  */
-export { formatCurrencyDisplayValue } from './utils/format-currency-display-value';
-export { getCheckboxTracks } from './utils/get-checkbox-tracks';
-export { getCurrencySymbolProps } from './utils/get-currency-symbol-props';
-export { getDerivedProductType } from './utils/get-derived-product-type';
-export {
-	getProductStatus,
-	PRODUCT_STATUS_LABELS,
-} from './utils/get-product-status';
-export {
-	getProductStockStatus,
-	getProductStockStatusClass,
-} from './utils/get-product-stock-status';
-export { getProductTitle, AUTO_DRAFT_NAME } from './utils/get-product-title';
-export {
-	getProductVariationTitle,
-	getTruncatedProductVariationTitle,
-} from './utils/get-product-variation-title';
-export { preventLeavingProductForm } from './utils/prevent-leaving-product-form';
+export * from './utils';
