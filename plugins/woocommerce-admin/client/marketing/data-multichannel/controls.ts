20ae504be09ae1ba7ddd08c47d72d224b4adfdd4
Gan Eng Chin <ecgan@users.noreply.github.com>
2023-02-24 02:08:59 +0800

    Add controls with fetchWithHeaders.

---
+++ plugins/woocommerce-admin/client/marketing/data-multichannel/controls.ts
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import { controls as dataControls } from '@wordpress/data-controls';
+import apiFetch, { APIFetchOptions } from '@wordpress/api-fetch';
+import { AnyAction } from 'redux';
+
+export const fetchWithHeaders = (
+	options: APIFetchOptions
+): AnyAction & { options: APIFetchOptions } => {
+	return {
+		type: 'FETCH_WITH_HEADERS',
+		options,
+	};
+};
+
+const controls = {
+	...dataControls,
+	FETCH_WITH_HEADERS( action: AnyAction ) {
+		return apiFetch< Response >( { ...action.options, parse: false } )
+			.then( ( response ) => {
+				return Promise.all( [
+					response.headers,
+					response.status,
+					response.json(),
+				] );
+			} )
+			.then( ( [ headers, status, data ] ) => ( {
+				headers,
+				status,
+				data,
+			} ) )
+			.catch( ( response ) => {
+				return response.json().then( ( data: unknown ) => {
+					throw data;
+				} );
+			} );
+	},
+};
+
+export default controls;
