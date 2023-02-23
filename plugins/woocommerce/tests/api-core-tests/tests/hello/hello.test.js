131be68fd556cc5162eeed2f545b3b335a7ad936
rodelgc <rodel.calasagsag@automattic.com>
2023-02-24 00:06:51 +0800

    Enable all api tests

--- plugins/woocommerce/tests/api-core-tests/tests/hello/hello.test.js
+++ plugins/woocommerce/tests/api-core-tests/tests/hello/hello.test.js
@@ -16,8 +16,4 @@ test.describe( 'Test API connectivity', () => {
 		const result = await request.get( '/wp-json/wc/v3/system_status' );
 		expect( result.status() ).toEqual( 200 );
 	} );
-
-	test( 'failing api test', async ( { page } ) => {
-		test.fail();
-	} );
 } );
