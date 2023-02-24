467bb5ba5ad2539bca6fcb7290053c9027094422
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-02-24 23:12:53 +0100

    Fine-tune the Add to cart form block (https://github.com/woocommerce/woocommerce-blocks/pull/8482)
    * Restrict usage to the single product template.
    
    * Update class prefix from wc to wp
    
    * Move the add-to-cart-form block to atomic blocks so it is aligned with the updated structure adopted for single product related blocks.
    
    * Address CR
    
    * ditch woocommerce class.
    
    * remove attributes key
    
    * Add new styles to account for the removal of the .summary and .entry-summary selectors
    
    * Narrow down the styles to the add-to-cart-form class.

--- plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
+++ plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
@@ -48,7 +48,7 @@ class AddToCartForm extends AbstractBlock {
 		$classes_and_styles = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes );
 
 		return sprintf(
-			'<div class="woocommerce wc-block-add-to-cart-form %1$s %2$s" style="%3$s"><div class="product type-product"><div class="summary entry-summary">%4$s</div></div></div>',
+			'<div class="wp-block-add-to-cart-form %1$s %2$s" style="%3$s">%4$s</div>',
 			esc_attr( $classes_and_styles['classes'] ),
 			esc_attr( $classname ),
 			esc_attr( $classes_and_styles['styles'] ),
@@ -62,6 +62,13 @@ class AddToCartForm extends AbstractBlock {
 	 * @param string $key Data to get, or default to everything.
 	 */
 	protected function get_block_type_script( $key = null ) {
+		return null;
+	}
+
+	/**
+	 * It isn't necessary register block assets because it is a server side block.
+	 */
+	protected function register_block_type_assets() {
 		return null;
 	}
 }
