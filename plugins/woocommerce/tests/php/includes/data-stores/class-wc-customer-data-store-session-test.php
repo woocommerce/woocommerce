5c6392dc7ca0893c15a3cf79462b7b08e94b59f8
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2023-02-24 13:04:01 +0530

    Use closure instead of returning DB object to prevent interference in other tests.
    PHPUnit will evaluate all data providers before starting anyh test (to count the number of tests to be executed). Which means that any data provider inserting data into DB is going to interfere with other tests. Using closure instead helps PHP in counting the data, yet not having to actual DB calls.

--- plugins/woocommerce/tests/php/includes/data-stores/class-wc-customer-data-store-session-test.php
+++ plugins/woocommerce/tests/php/includes/data-stores/class-wc-customer-data-store-session-test.php
@@ -11,11 +11,12 @@ class WC_Customer_Data_Store_Session_Test extends WC_Unit_Test_Case {
 	 * @see https://github.com/woocommerce/woocommerce/issues/28759
 	 * @dataProvider provide_customers_with_different_addresses
 	 *
-	 * @param WC_Customer $customer               The customer object being tested.
+	 * @param Closure     $customer_closure        The customer object being tested.
 	 * @param bool        $states_should_match    If the billing and shipping states should match.
 	 * @param bool        $countries_should_match If the billing and shipping countries should match.
 	 */
-	public function test_setting_default_address_fields( WC_Customer $customer, bool $states_should_match, bool $countries_should_match ) {
+	public function test_setting_default_address_fields( Closure $customer_closure, bool $states_should_match, bool $countries_should_match ) {
+		$customer = $customer_closure();
 		$session_data = new WC_Customer_Data_Store_Session();
 		$session_data->read( $customer );
 
@@ -46,66 +47,78 @@ class WC_Customer_Data_Store_Session_Test extends WC_Unit_Test_Case {
 	 * @return array[]
 	 */
 	public function provide_customers_with_different_addresses() {
-		$has_billing_address_only = new WC_Customer();
-		$has_billing_address_only->set_email( 'wc-customer-test-01@test.user' );
-		$has_billing_address_only->set_billing_address( '1234 Quality Lane' );
-		$has_billing_address_only->set_billing_city( 'Testville' );
-		$has_billing_address_only->set_billing_country( 'US' );
-		$has_billing_address_only->set_billing_state( 'CA' );
-		$has_billing_address_only->set_billing_postcode( '90123' );
-		$has_billing_address_only->save();
+		$cust1_closure = function () {
+			$has_billing_address_only = new WC_Customer();
+			$has_billing_address_only->set_email( 'wc-customer-test-01@test.user' );
+			$has_billing_address_only->set_billing_address( '1234 Quality Lane' );
+			$has_billing_address_only->set_billing_city( 'Testville' );
+			$has_billing_address_only->set_billing_country( 'US' );
+			$has_billing_address_only->set_billing_state( 'CA' );
+			$has_billing_address_only->set_billing_postcode( '90123' );
+			$has_billing_address_only->save();
+			return $has_billing_address_only;
+		};
 
-		$separate_billing_and_shipping_state_and_country = new WC_Customer();
-		$separate_billing_and_shipping_state_and_country->set_email( 'wc-customer-test-02@test.user' );
-		$separate_billing_and_shipping_state_and_country->set_billing_address( '4567 Scenario Street' );
-		$separate_billing_and_shipping_state_and_country->set_billing_city( 'Unitly' );
-		$separate_billing_and_shipping_state_and_country->set_billing_country( 'UK' );
-		$separate_billing_and_shipping_state_and_country->set_billing_state( 'Computershire' );
-		$separate_billing_and_shipping_state_and_country->set_billing_postcode( 'ZX1 2PQ' );
-		$separate_billing_and_shipping_state_and_country->set_shipping_address( '8901 Situation Court' );
-		$separate_billing_and_shipping_state_and_country->set_shipping_city( 'Endtoendly' );
-		$separate_billing_and_shipping_state_and_country->set_shipping_country( 'CA' );
-		$separate_billing_and_shipping_state_and_country->set_shipping_state( 'BC' );
-		$separate_billing_and_shipping_state_and_country->set_shipping_postcode( 'A1B 2C3' );
-		$separate_billing_and_shipping_state_and_country->save();
+		$cust2_closure = function () {
+			$separate_billing_and_shipping_state_and_country = new WC_Customer();
+			$separate_billing_and_shipping_state_and_country->set_email( 'wc-customer-test-02@test.user' );
+			$separate_billing_and_shipping_state_and_country->set_billing_address( '4567 Scenario Street' );
+			$separate_billing_and_shipping_state_and_country->set_billing_city( 'Unitly' );
+			$separate_billing_and_shipping_state_and_country->set_billing_country( 'UK' );
+			$separate_billing_and_shipping_state_and_country->set_billing_state( 'Computershire' );
+			$separate_billing_and_shipping_state_and_country->set_billing_postcode( 'ZX1 2PQ' );
+			$separate_billing_and_shipping_state_and_country->set_shipping_address( '8901 Situation Court' );
+			$separate_billing_and_shipping_state_and_country->set_shipping_city( 'Endtoendly' );
+			$separate_billing_and_shipping_state_and_country->set_shipping_country( 'CA' );
+			$separate_billing_and_shipping_state_and_country->set_shipping_state( 'BC' );
+			$separate_billing_and_shipping_state_and_country->set_shipping_postcode( 'A1B 2C3' );
+			$separate_billing_and_shipping_state_and_country->save();
+			return $separate_billing_and_shipping_state_and_country;
+		};
 
-		$separate_billing_state_same_country = new WC_Customer();
-		$separate_billing_state_same_country->set_email( 'wc-customer-test-03@test.user' );
-		$separate_billing_state_same_country->set_billing_address( '4567 Scenario Street' );
-		$separate_billing_state_same_country->set_billing_city( 'Unitly' );
-		$separate_billing_state_same_country->set_billing_country( 'UK' );
-		$separate_billing_state_same_country->set_billing_state( 'Computershire' );
-		$separate_billing_state_same_country->set_billing_postcode( 'ZX1 2PQ' );
-		$separate_billing_state_same_country->set_shipping_address( '8901 Situation Court' );
-		$separate_billing_state_same_country->set_shipping_city( 'Endtoendly' );
-		$separate_billing_state_same_country->set_shipping_country( 'UK' );
-		$separate_billing_state_same_country->set_shipping_state( 'Byteshire' );
-		$separate_billing_state_same_country->set_shipping_postcode( 'RS1 2TU' );
-		$separate_billing_state_same_country->save();
+		$cust3_closure = function () {
+			$separate_billing_state_same_country = new WC_Customer();
+			$separate_billing_state_same_country->set_email( 'wc-customer-test-03@test.user' );
+			$separate_billing_state_same_country->set_billing_address( '4567 Scenario Street' );
+			$separate_billing_state_same_country->set_billing_city( 'Unitly' );
+			$separate_billing_state_same_country->set_billing_country( 'UK' );
+			$separate_billing_state_same_country->set_billing_state( 'Computershire' );
+			$separate_billing_state_same_country->set_billing_postcode( 'ZX1 2PQ' );
+			$separate_billing_state_same_country->set_shipping_address( '8901 Situation Court' );
+			$separate_billing_state_same_country->set_shipping_city( 'Endtoendly' );
+			$separate_billing_state_same_country->set_shipping_country( 'UK' );
+			$separate_billing_state_same_country->set_shipping_state( 'Byteshire' );
+			$separate_billing_state_same_country->set_shipping_postcode( 'RS1 2TU' );
+			$separate_billing_state_same_country->save();
+			return $separate_billing_state_same_country;
+		};
 
-		$shipping_address_is_effectively_empty = new WC_Customer();
-		$shipping_address_is_effectively_empty->set_email( 'wc-customer-test-04@test.user' );
-		$shipping_address_is_effectively_empty->set_shipping_address( ' ' );
-		$shipping_address_is_effectively_empty->save();
+		$cust4_closure = function () {
+			$shipping_address_is_effectively_empty = new WC_Customer();
+			$shipping_address_is_effectively_empty->set_email( 'wc-customer-test-04@test.user' );
+			$shipping_address_is_effectively_empty->set_shipping_address( ' ' );
+			$shipping_address_is_effectively_empty->save();
+			return $shipping_address_is_effectively_empty;
+		};
 
 		return array(
 			'has_billing_address_only' => array(
-				$has_billing_address_only,
+				$cust1_closure,
 				true,
 				true,
 			),
 			'separate_billing_and_shipping_state_and_country' => array(
-				$separate_billing_and_shipping_state_and_country,
+				$cust2_closure,
 				false,
 				false,
 			),
 			'separate_billing_state_same_country' => array(
-				$separate_billing_state_same_country,
+				$cust3_closure,
 				false,
 				true,
 			),
 			'shipping_address_is_effectively_empty' => array(
-				$shipping_address_is_effectively_empty,
+				$cust4_closure,
 				true,
 				true,
 			),
