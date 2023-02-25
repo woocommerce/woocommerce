648f739bb6dbf7458dae9e6731d0a2911a64f053
rodelgc <rodel.calasagsag@automattic.com>
2023-02-25 17:15:09 +0800

    E2e/pr merge reports (#36969)
    * Add dummy failing API test
    
    * Add option to exclude allure results
    
    * REmove surrounding quote

--- plugins/woocommerce/tests/api-core-tests/tests/hello/hello.test.js
+++ plugins/woocommerce/tests/api-core-tests/tests/hello/hello.test.js
@@ -16,4 +16,8 @@ test.describe( 'Test API connectivity', () => {
 		const result = await request.get( '/wp-json/wc/v3/system_status' );
 		expect( result.status() ).toEqual( 200 );
 	} );
+
+	test( '(dummy) failing test', async () => {
+		test.fail();
+	} );
 } );
