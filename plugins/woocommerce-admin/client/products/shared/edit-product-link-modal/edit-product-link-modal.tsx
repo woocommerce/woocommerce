bf55a0ed88b5aa315319b0684093d048f29b566d
Joel <dygerati@gmail.com>
2023-02-28 13:02:41 -0800

    Removing old useProductHelper hook and references

--- plugins/woocommerce-admin/client/products/shared/edit-product-link-modal/edit-product-link-modal.tsx
+++ plugins/woocommerce-admin/client/products/shared/edit-product-link-modal/edit-product-link-modal.tsx
@@ -11,12 +11,12 @@ import { Product } from '@woocommerce/data';
 import { Text } from '@woocommerce/experimental';
 import { useFormContext } from '@woocommerce/components';
 import { recordEvent } from '@woocommerce/tracks';
+import { __experimentalUseProductHelper as useProductHelper } from '@woocommerce/product-editor';
 
 /**
  * Internal dependencies
  */
 import './edit-product-link-modal.scss';
-import { useProductHelper } from '../../use-product-helper';
 
 type EditProductLinkModalProps = {
 	product: Product;
