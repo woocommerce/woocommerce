103637c26df1068e284aae0ed26e32de793730df
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-03-01 17:17:01 -0300

    Create the editor component

---
+++ packages/js/product-editor/src/components/editor/editor.tsx
@@ -0,0 +1,53 @@
+/**
+ * External dependencies
+ */
+import { createElement, Fragment, StrictMode } from '@wordpress/element';
+import {
+	EditorSettings,
+	EditorBlockListSettings,
+} from '@wordpress/block-editor';
+import { Popover, SlotFillProvider } from '@wordpress/components';
+import { Product } from '@woocommerce/data';
+// @ts-ignore
+import { ShortcutProvider } from '@wordpress/keyboard-shortcuts';
+// @ts-ignore
+import { FullscreenMode, InterfaceSkeleton } from '@wordpress/interface';
+
+/**
+ * Internal dependencies
+ */
+import { Header } from '../header';
+import { Sidebar } from '../sidebar';
+import { BlockEditor } from '../block-editor';
+import { initBlocks } from './init-blocks';
+
+initBlocks();
+
+type EditorProps = {
+	product: Product;
+	settings: Partial< EditorSettings & EditorBlockListSettings > | undefined;
+};
+
+export function Editor( { product, settings }: EditorProps ) {
+	return (
+		<StrictMode>
+			<ShortcutProvider>
+				<FullscreenMode isActive={ false } />
+				{ /* @ts-ignore */ }
+				<SlotFillProvider>
+					<InterfaceSkeleton
+						header={ <Header title={ product.name } /> }
+						sidebar={ <Sidebar /> }
+						content={
+							<BlockEditor
+								settings={ settings }
+								product={ product }
+							/>
+						}
+					/>
+					<Popover.Slot />
+				</SlotFillProvider>
+			</ShortcutProvider>
+		</StrictMode>
+	);
+}
