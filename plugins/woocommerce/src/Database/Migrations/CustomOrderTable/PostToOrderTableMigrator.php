3d1965b065ee9104173fd8d45995d7652d821ea4
Peter Fabian <peter.fabian.github@gmail.com>
2023-03-01 11:23:39 +0100

    Fix dynamic property declaration.

--- plugins/woocommerce/src/Database/Migrations/CustomOrderTable/PostToOrderTableMigrator.php
+++ plugins/woocommerce/src/Database/Migrations/CustomOrderTable/PostToOrderTableMigrator.php
@@ -22,7 +22,7 @@ class PostToOrderTableMigrator extends MetaToCustomTableMigrator {
 	protected function get_schema_config(): array {
 		global $wpdb;
 
-		$this->table_names = array(
+		$table_names = array(
 			'orders'    => $wpdb->prefix . 'wc_orders',
 			'addresses' => $wpdb->prefix . 'wc_order_addresses',
 			'op_data'   => $wpdb->prefix . 'wc_order_operational_data',
@@ -45,7 +45,7 @@ class PostToOrderTableMigrator extends MetaToCustomTableMigrator {
 				),
 			),
 			'destination' => array(
-				'table_name'        => $this->table_names['orders'],
+				'table_name'        => $table_names['orders'],
 				'source_rel_column' => 'id',
 				'primary_key'       => 'id',
 				'primary_key_type'  => 'int',
