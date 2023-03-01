57d8a280ebf55a2ed8ea23698c5abb89b8da8fc2
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-03-01 17:20:21 -0300

    Create the new block mode product page

---
+++ plugins/woocommerce-admin/client/products/product-page.tsx
@@ -0,0 +1,20 @@
+/**
+ * External dependencies
+ */
+import { Editor } from '@woocommerce/product-editor';
+import { Product } from '@woocommerce/data';
+
+/**
+ * Internal dependencies
+ */
+import './product-page.scss';
+import './fills';
+
+const dummyProduct = {
+	name: 'Example product',
+	short_description: 'Short product description content',
+} as Product;
+
+export default function ProductPage() {
+	return <Editor product={ dummyProduct } settings={ {} } />;
+}
