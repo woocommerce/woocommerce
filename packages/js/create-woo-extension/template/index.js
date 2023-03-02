0c379922392f3189ef9b32c7725ed09c21baa4c3
Leif Singer <git@singer.sh>
2023-03-02 23:28:35 +0100

    wip: add dynamic config and Woo version number
    props @psealock #35922

--- packages/js/create-woo-extension/template/index.js
+++ packages/js/create-woo-extension/template/index.js
@@ -1,21 +1,43 @@
-module.exports = {
-	templatesPath: __dirname,
-	defaultValues: {
-		npmDependencies: [
-			'@wordpress/hooks',
-			'@wordpress/i18n',
-			'@woocommerce/components',
-		],
-		npmDevDependencies: [
-			'@woocommerce/dependency-extraction-webpack-plugin',
-			'@woocommerce/eslint-plugin',
-			'@wordpress/prettier-config',
-			'@wordpress/scripts@24.6.0',
-		],
-		namespace: 'extension',
-		license: 'GPL-3.0+',
+/**
+ * External dependencies
+ */
+const fetch = require( 'node-fetch' );
+
+async function getLatestWooVersion() {
+	const api =
+		'https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&request[slug]=woocommerce';
+
+	const response = await fetch( api );
+	const json = await response.json();
+
+	return json.version;
+}
+
+module.exports = ( async function () {
+	const latestWooVersion = await getLatestWooVersion();
+
+	return {
+		templatesPath: __dirname,
+		defaultValues: {
+			npmDependencies: [
+                '@wordpress/hooks',
+                '@wordpress/i18n',
+                '@woocommerce/components',
+            ],
+			npmDevDependencies: [
+				'@woocommerce/dependency-extraction-webpack-plugin',
+				'@woocommerce/eslint-plugin',
+				'@wordpress/prettier-config',
+				'@wordpress/scripts@24.6.0',
+			],
+			namespace: 'extension',
+			license: 'GPL-3.0+',
+			attributes: {
+				latestWooVersion,
+			},
+		},
 		customScripts: {
 			postinstall: 'composer install',
 		},
-	},
-};
+	};
+} )();
