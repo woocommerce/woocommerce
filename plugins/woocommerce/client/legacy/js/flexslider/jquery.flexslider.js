87c40e7715a78f6c9eb27581a7e8fe9838731b77
Atanas Penchev <apenchev@users.noreply.github.com>
2023-03-05 20:16:25 +0200

    Correct 'jswing' easing

--- plugins/woocommerce/client/legacy/js/flexslider/jquery.flexslider.js
+++ plugins/woocommerce/client/legacy/js/flexslider/jquery.flexslider.js
@@ -48,8 +48,8 @@
     easeInOutBack:  'cubic-bezier(0.68, -0.6, 0.32, 1.6)' // https://easings.net/#easeInOutBack
   };
 
-  easings['jswing'] = easings['swing'];       // https://github.com/gdsmith/jquery.easing/blob/v1.4.0/jquery.easing.js#L22
-  easings['def']    = easings['easeOutQuad']; // https://github.com/gdsmith/jquery.easing/blob/v1.4.0/jquery.easing.js#L52
+  // Preserve the original jQuery "swing" easing as "jswing"
+  easings['jswing'] = easings['swing'];
 
   //FlexSlider: Object Instance
   $.flexslider = function(el, options) {
