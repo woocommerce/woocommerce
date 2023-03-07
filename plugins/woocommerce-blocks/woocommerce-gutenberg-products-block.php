dea7d3dac78f5240b3f6f9f26bd16521103a67f5
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-03-07 09:49:26 +0100

    Remove patch version from Requires at least (https://github.com/woocommerce/woocommerce-blocks/pull/8649)

--- plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
+++ plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
@@ -7,7 +7,7 @@
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain:  woo-gutenberg-products-block
- * Requires at least: 6.1.1
+ * Requires at least: 6.1
  * Requires PHP: 7.2
  * WC requires at least: 7.3
  * WC tested up to: 7.4
@@ -18,7 +18,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$minimum_wp_version = '6.1.1';
+$minimum_wp_version = '6.1';
 
 if ( ! defined( 'WC_BLOCKS_IS_FEATURE_PLUGIN' ) ) {
 	define( 'WC_BLOCKS_IS_FEATURE_PLUGIN', true );
