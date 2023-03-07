4e581a48259ef32f1fec38a07da9393d16075a1e
Mike Jolley <mike.jolley@me.com>
2023-03-07 11:21:13 +0000

    Allow view switcher to toggle view when using Gutenberg list view (https://github.com/woocommerce/woocommerce-blocks/pull/8429)
    * Remove old view switcher component (not used)
    
    * Switch in list view
    
    * useSelect
    
    * Optional selection of parent block on changing view
    
    ---------
    
    Co-authored-by: Seghir Nadir <nadir.seghir@gmail.com>

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout-shared/view-switcher/switcher.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout-shared/view-switcher/switcher.tsx
@@ -2,7 +2,8 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useDispatch, select as dataSelect } from '@wordpress/data';
+import { useLayoutEffect } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
 import { ToolbarGroup, ToolbarDropdownMenu } from '@wordpress/components';
 import { BlockControls } from '@wordpress/block-editor';
 import { Icon } from '@wordpress/icons';
@@ -12,7 +13,7 @@ import { eye } from '@woocommerce/icons';
  * Internal dependencies
  */
 import type { View } from './types';
-import { getView } from './utils';
+import { getView, selectView } from './utils';
 
 export const Switcher = ( {
 	currentView,
@@ -23,10 +24,64 @@ export const Switcher = ( {
 	views: View[];
 	clientId: string;
 } ): JSX.Element | null => {
+	const {
+		getBlockName,
+		getSelectedBlockClientId,
+		getBlockParentsByBlockName,
+	} = useSelect( ( select ) => {
+		const blockEditor = select( 'core/block-editor' );
+		return {
+			getBlockName: blockEditor.getBlockName,
+			getSelectedBlockClientId: blockEditor.getSelectedBlockClientId,
+			getBlockParentsByBlockName: blockEditor.getBlockParentsByBlockName,
+		};
+	}, [] );
+	const selectedBlockClientId = getSelectedBlockClientId();
 	const currentViewObject = getView( currentView, views ) || views[ 0 ];
 	const currentViewLabel = currentViewObject.label;
-	const { updateBlockAttributes, selectBlock } =
-		useDispatch( 'core/block-editor' );
+
+	useLayoutEffect( () => {
+		const selectedBlock = selectedBlockClientId
+			? getBlockName( selectedBlockClientId )
+			: null;
+
+		// If there is no selected block, or the selected block is the current view, do nothing.
+		if ( ! selectedBlock || currentView === selectedBlock ) {
+			return;
+		}
+
+		const viewNames = views.map( ( view ) => view.view );
+
+		if ( viewNames.includes( selectedBlock ) ) {
+			selectView( clientId, selectedBlock );
+			return;
+		}
+
+		// Look at the parent blocks to see if any of them are a view we can select.
+		const parentBlockClientIds = getBlockParentsByBlockName(
+			selectedBlockClientId,
+			viewNames
+		);
+
+		const parentBlock =
+			parentBlockClientIds.length === 1
+				? getBlockName( parentBlockClientIds[ 0 ] )
+				: null;
+
+		// If there is no parent block, or the parent block is the current view, do nothing.
+		if ( ! parentBlock || currentView === parentBlock ) {
+			return;
+		}
+
+		selectView( clientId, parentBlock, false );
+	}, [
+		clientId,
+		currentView,
+		getBlockName,
+		getBlockParentsByBlockName,
+		selectedBlockClientId,
+		views,
+	] );
 
 	return (
 		<BlockControls>
@@ -49,17 +104,7 @@ export const Switcher = ( {
 						),
 						isActive: view.view === currentView,
 						onClick: () => {
-							updateBlockAttributes( clientId, {
-								currentView: view.view,
-							} );
-							selectBlock(
-								dataSelect( 'core/block-editor' )
-									.getBlock( clientId )
-									?.innerBlocks.find(
-										( block: { name: string } ) =>
-											block.name === view.view
-									)?.clientId || clientId
-							);
+							selectView( clientId, view.view );
 						},
 					} ) ) }
 				/>
