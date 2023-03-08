3ded2b46e976e5c11393a6cf70943b4f6ef14cb3
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-03-08 06:51:59 -0300

    Create editor skeleton on add/edit product pages (#37023)
    * Add required dependencies
    
    * Create the block editor component
    
    * Create the header component
    
    * Create the sidebar component
    
    * Create the editor component
    
    * Remove comments
    
    * Export the block editor from its package
    
    * Enqueue wp-edit-site
    
    * Create the new block mode product page
    
    * Register the new ProductPage component in /add-product route
    
    * Add temporal layout styles
    
    * Fix rebasing conflicts
    
    * Fix linter errors
    
    * Redirect to the new product-editor page when edit
    
    * Add changelog files
    
    * Fixing dependencies conflicts and ignoring some non running test
    
    * Add comment suggestions
    
    * Fix conflicts after rebasing
    
    * Add comment suggestions

---
+++ plugins/woocommerce-admin/client/products/product-page.tsx
@@ -0,0 +1,19 @@
+/**
+ * External dependencies
+ */
+import { __experimentalEditor as Editor } from '@woocommerce/product-editor';
+import { Product } from '@woocommerce/data';
+
+/**
+ * Internal dependencies
+ */
+import './product-page.scss';
+
+const dummyProduct = {
+	name: 'Example product',
+	short_description: 'Short product description content',
+} as Product;
+
+export default function ProductPage() {
+	return <Editor product={ dummyProduct } settings={ {} } />;
+}
