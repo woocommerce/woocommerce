5b09d5fe915ad8baf2d372c3a639ead82c4629f7
Joshua Flowers <joshuatf@gmail.com>
2023-03-09 11:33:23 -0800

    Init all blocks in product editor

--- packages/js/product-editor/src/components/editor/init-blocks.ts
+++ packages/js/product-editor/src/components/editor/init-blocks.ts
@@ -2,7 +2,9 @@
  * Internal dependencies
  */
 import { init as initName } from '../details-name-block';
+import { init as initSection } from '../section';
 
 export const initBlocks = () => {
 	initName();
+	initSection();
 };
