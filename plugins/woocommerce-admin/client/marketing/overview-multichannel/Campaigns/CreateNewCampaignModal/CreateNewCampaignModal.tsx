9f0326b398c6197d1397c1c2b24df7322ac40a7d
Gan Eng Chin <ecgan@users.noreply.github.com>
2023-03-09 22:26:06 +0800

    Remove unneeded return in CreateNewCampaignModal.
    This helps us save indentation and make the code few lines shorter.

--- plugins/woocommerce-admin/client/marketing/overview-multichannel/Campaigns/CreateNewCampaignModal/CreateNewCampaignModal.tsx
+++ plugins/woocommerce-admin/client/marketing/overview-multichannel/Campaigns/CreateNewCampaignModal/CreateNewCampaignModal.tsx
@@ -60,62 +60,57 @@ export const CreateNewCampaignModal = ( props: CreateCampaignModalProps ) => {
 					) }
 				</div>
 				<div>
-					{ campaignTypes?.map( ( el ) => {
-						return (
-							<Flex
-								key={ el.id }
-								className="woocommerce-marketing-new-campaign-type"
-								gap={ 4 }
-							>
-								<FlexItem>
-									<img
-										src={ el.icon }
-										alt={ el.name }
-										width="32"
-										height="32"
-									/>
-								</FlexItem>
-								<FlexBlock>
-									<Flex direction="column" gap={ 1 }>
-										<FlexItem className="woocommerce-marketing-new-campaign-type__name">
-											{ el.name }
+					{ campaignTypes?.map( ( el ) => (
+						<Flex
+							key={ el.id }
+							className="woocommerce-marketing-new-campaign-type"
+							gap={ 4 }
+						>
+							<FlexItem>
+								<img
+									src={ el.icon }
+									alt={ el.name }
+									width="32"
+									height="32"
+								/>
+							</FlexItem>
+							<FlexBlock>
+								<Flex direction="column" gap={ 1 }>
+									<FlexItem className="woocommerce-marketing-new-campaign-type__name">
+										{ el.name }
+									</FlexItem>
+									<FlexItem className="woocommerce-marketing-new-campaign-type__description">
+										{ el.description }
+									</FlexItem>
+								</Flex>
+							</FlexBlock>
+							<FlexItem>
+								<Button
+									variant="secondary"
+									href={ el.createUrl }
+									target={
+										isExternalURL( el.createUrl )
+											? '_blank'
+											: '_self'
+									}
+								>
+									<Flex gap={ 1 }>
+										<FlexItem>
+											{ __( 'Create', 'woocommerce' ) }
 										</FlexItem>
-										<FlexItem className="woocommerce-marketing-new-campaign-type__description">
-											{ el.description }
-										</FlexItem>
-									</Flex>
-								</FlexBlock>
-								<FlexItem>
-									<Button
-										variant="secondary"
-										href={ el.createUrl }
-										target={
-											isExternalURL( el.createUrl )
-												? '_blank'
-												: '_self'
-										}
-									>
-										<Flex gap={ 1 }>
+										{ isExternalURL( el.createUrl ) && (
 											<FlexItem>
-												{ __(
-													'Create',
-													'woocommerce'
-												) }
+												<Icon
+													icon={ external }
+													size={ 16 }
+												/>
 											</FlexItem>
-											{ isExternalURL( el.createUrl ) && (
-												<FlexItem>
-													<Icon
-														icon={ external }
-														size={ 16 }
-													/>
-												</FlexItem>
-											) }
-										</Flex>
-									</Button>
-								</FlexItem>
-							</Flex>
-						);
-					} ) }
+										) }
+									</Flex>
+								</Button>
+							</FlexItem>
+						</Flex>
+					) ) }
 				</div>
 			</div>
 			{ !! recommendedChannels?.length && (
