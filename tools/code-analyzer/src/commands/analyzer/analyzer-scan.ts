6ba88f60397ced7fdbcdd6fe0d6f96207a6c342e
Sam Seay <samueljseay@gmail.com>
2023-03-10 14:46:46 +1300

    Add granular command for scanning individual change types.

---
+++ tools/code-analyzer/src/commands/analyzer/analyzer-scan.ts
@@ -0,0 +1,138 @@
+/**
+ * External dependencies
+ */
+import { Command } from '@commander-js/extra-typings';
+import { Logger } from 'cli-core/src/logger';
+import { join } from 'path';
+
+/**
+ * Internal dependencies
+ */
+import {
+	scanChangesForDB,
+	scanChangesForHooks,
+	scanChangesForSchema,
+	scanChangesForTemplates,
+	ScanType,
+} from '../../lib/scan-changes';
+import {
+	printDatabaseUpdates,
+	printHookResults,
+	printSchemaChange,
+	printTemplateResults,
+} from '../../print';
+
+const printEmptyNotice = ( scanType: ScanType ) => {
+	Logger.notice( `\n\n##  ${ scanType.toUpperCase() } CHANGES` );
+	Logger.notice( '---------------------------------------------------' );
+	Logger.notice( `No ${ scanType } changes found.` );
+	Logger.notice( '---------------------------------------------------' );
+};
+
+const program = new Command()
+	.command( 'scan' )
+	.argument(
+		'<type>',
+		'Type of change to scan for. Options: templates, hooks, database, schema.'
+	)
+	.argument(
+		'<compare>',
+		'GitHub branch/tag/commit hash to compare against the base branch/tag/commit hash.'
+	)
+	.argument(
+		'<sinceVersion>',
+		'Specify the version used to determine which changes are included (version listed in @since code doc).'
+	)
+	.option(
+		'-b, --base <base>',
+		'GitHub base branch/tag/commit hash.',
+		'trunk'
+	)
+	.option(
+		'-s, --source <source>',
+		'Git repo url or local path to a git repo.',
+		join( process.cwd(), '../../' )
+	)
+	.option(
+		'-o, --outputStyle <outputStyle>',
+		'Output style for the results. Options: github, cli. Github output will set the results as an output variable for Github actions.',
+		'cli'
+	)
+	.action( async ( scanType, compare, sinceVersion, options ) => {
+		const { source, base, outputStyle } = options;
+
+		switch ( scanType ) {
+			case 'hooks':
+				const hookChanges = await scanChangesForHooks(
+					compare,
+					sinceVersion,
+					base,
+					source
+				);
+
+				if ( hookChanges.length ) {
+					printHookResults(
+						hookChanges,
+						outputStyle,
+						'HOOKS',
+						Logger.notice
+					);
+				} else {
+					printEmptyNotice( 'hooks' );
+				}
+				break;
+			case 'templates':
+				const templateChanges = await scanChangesForTemplates(
+					compare,
+					sinceVersion,
+					base,
+					source
+				);
+				if ( templateChanges && templateChanges.length ) {
+					printTemplateResults(
+						templateChanges,
+						outputStyle,
+						'TEMPLATES',
+						Logger.notice
+					);
+				} else {
+					printEmptyNotice( 'templates' );
+				}
+				break;
+			case 'database':
+				const dbChanges = await scanChangesForDB(
+					compare,
+					base,
+					source
+				);
+				if ( dbChanges ) {
+					printDatabaseUpdates( dbChanges, 'cli', Logger.notice );
+				} else {
+					printEmptyNotice( 'database' );
+				}
+				break;
+			case 'schema':
+				const schemaChanges = await scanChangesForSchema(
+					compare,
+					base,
+					source
+				);
+				if ( schemaChanges && schemaChanges.length ) {
+					printSchemaChange(
+						schemaChanges,
+						sinceVersion,
+						'cli',
+						Logger.notice
+					);
+				} else {
+					printEmptyNotice( 'schema' );
+				}
+				break;
+			default:
+				throw new Error(
+					'Invalid scan type. Options: templates, hooks, database, schema.'
+				);
+		}
+	} );
+
+program.parse( process.argv );
