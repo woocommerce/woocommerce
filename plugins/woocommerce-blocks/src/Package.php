f3f942ae9a0c5edb350cbea90f5ca9553ca724cb
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-03-14 11:56:23 +0100

    Update dev version to 9.9.0-dev

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -109,7 +109,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '9.8.0-dev';
+					$version = '9.9.0-dev';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
