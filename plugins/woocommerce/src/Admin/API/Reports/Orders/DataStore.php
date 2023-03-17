b12d1ba31f4954278b870fa767e951c63e58a1c8
Matt Sherman <matt@jam123.com>
2023-03-16 21:24:09 -0400

    Remove ability to pass table into get_attribute_subqueries since it should always be the same table we join on

--- plugins/woocommerce/src/Admin/API/Reports/Orders/DataStore.php
+++ plugins/woocommerce/src/Admin/API/Reports/Orders/DataStore.php
@@ -191,7 +191,7 @@ class DataStore extends ReportsDataStore implements DataStoreInterface {
 			$where_subquery[] = "{$order_tax_lookup_table}.tax_rate_id NOT IN ({$excluded_tax_rates}) OR {$order_tax_lookup_table}.tax_rate_id IS NULL";
 		}
 
-		$attribute_subqueries = $this->get_attribute_subqueries( $query_args, $order_product_lookup_table );
+		$attribute_subqueries = $this->get_attribute_subqueries( $query_args );
 		if ( $attribute_subqueries['join'] && $attribute_subqueries['where'] ) {
 			$this->subquery->add_sql_clause( 'join', "JOIN {$order_product_lookup_table} ON {$order_stats_lookup_table}.order_id = {$order_product_lookup_table}.order_id" );
 
