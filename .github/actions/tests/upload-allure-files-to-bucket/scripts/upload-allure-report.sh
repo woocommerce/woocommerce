76dca5548b6aeeb2dd2e706a3b99759ab891918f
rodelgc <rodel.calasagsag@automattic.com>
2023-03-17 15:41:06 +0800

    Use separate files

---
+++ .github/actions/tests/upload-allure-files-to-bucket/scripts/upload-allure-report.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Uploading allure-report folder..."
+aws s3 cp $ALLURE_REPORT_DIR \
+    $S3_BUCKET/artifacts/$GITHUB_RUN_ID/$DESTINATION_DIR/allure-report \
+    --recursive \
+    --only-show-errors
+echo "Done"
---
+++ .github/actions/tests/upload-allure-files-to-bucket/scripts/upload-allure-results.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Uploading allure-results folder..."
+aws s3 cp $ALLURE_RESULTS_DIR \
+    $S3_BUCKET/artifacts/$GITHUB_RUN_ID/$DESTINATION_DIR/allure-results \
+    --recursive \
+    --only-show-errors
+echo "Done"
