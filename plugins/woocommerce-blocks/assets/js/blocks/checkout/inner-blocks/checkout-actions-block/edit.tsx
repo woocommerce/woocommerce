ec19838355528dd4b7187139b7d64b558808c460
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-03-17 13:26:34 +0100

    Fix issue that prevented spaces being added to Mini Cart, Cart and Checkout buttons (https://github.com/woocommerce/woocommerce-blocks/pull/8777)
    * Fix issue that prevented spaces being added to Mini Cart, Cart and Checkout buttons
    
    * Fix a couple of comments

--- plugins/woocommerce-blocks/assets/js/blocks/checkout/inner-blocks/checkout-actions-block/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/checkout/inner-blocks/checkout-actions-block/edit.tsx
@@ -4,17 +4,13 @@
 import { useRef } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
-import {
-	InspectorControls,
-	useBlockProps,
-	RichText,
-} from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import PageSelector from '@woocommerce/editor-components/page-selector';
 import { PanelBody, ToggleControl } from '@wordpress/components';
 import { CHECKOUT_PAGE_ID } from '@woocommerce/block-settings';
 import { getSetting } from '@woocommerce/settings';
 import { ReturnToCartButton } from '@woocommerce/base-components/cart-checkout';
-import Button from '@woocommerce/base-components/button';
+import EditableButton from '@woocommerce/editor-components/editable-button';
 import Noninteractive from '@woocommerce/base-components/noninteractive';
 
 /**
@@ -104,19 +100,16 @@ export const Edit = ( {
 						/>
 					) }
 				</Noninteractive>
-				<Button className="wc-block-cart__submit-button wc-block-components-checkout-place-order-button">
-					<RichText
-						multiline={ false }
-						allowedFormats={ [] }
-						value={ placeOrderButtonLabel }
-						placeholder={ defaultPlaceOrderButtonLabel }
-						onChange={ ( content ) => {
-							setAttributes( {
-								placeOrderButtonLabel: content,
-							} );
-						} }
-					/>
-				</Button>
+				<EditableButton
+					className="wc-block-cart__submit-button wc-block-components-checkout-place-order-button"
+					value={ placeOrderButtonLabel }
+					placeholder={ defaultPlaceOrderButtonLabel }
+					onChange={ ( content ) => {
+						setAttributes( {
+							placeOrderButtonLabel: content,
+						} );
+					} }
+				/>
 			</div>
 		</div>
 	);
