4cef10001926fd2b0be23869584bba92e53bcf7c
Fernando Marichal <contacto@fernandomarichal.com>
2023-03-17 18:41:58 -0300

    Set default quantity value

--- plugins/woocommerce/includes/admin/meta-boxes/views/html-product-data-inventory.php
+++ plugins/woocommerce/includes/admin/meta-boxes/views/html-product-data-inventory.php
@@ -50,7 +50,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			woocommerce_wp_text_input(
 				array(
 					'id'                => '_stock',
-					'value'             => wc_stock_amount( $product_object->get_stock_quantity( 'edit' ) ),
+					'value'             => wc_stock_amount( $product_object->get_stock_quantity( 'edit' ) ?? 1 ),
 					'label'             => __( 'Stock quantity', 'woocommerce' ),
 					'desc_tip'          => true,
 					'description'       => __( 'Stock quantity. If this is a variable product this value will be used to control stock for all variations, unless you define stock at variation level.', 'woocommerce' ),
@@ -62,7 +62,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 				)
 			);
 
-			echo '<input type="hidden" name="_original_stock" value="' . esc_attr( wc_stock_amount( $product_object->get_stock_quantity( 'edit' ) ) ) . '" />';
+			echo '<input type="hidden" name="_original_stock" value="' . esc_attr( wc_stock_amount( $product_object->get_stock_quantity( 'edit' ) ?? 1 ) ) . '" />';
 
 			woocommerce_wp_select(
 				array(
