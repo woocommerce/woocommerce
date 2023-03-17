96b27622bb7f57800fd760513591d84c50aa26b8
rodelgc <rodel.calasagsag@automattic.com>
2023-03-18 07:27:48 +0800

    Enclose in double quotes

--- .github/actions/tests/upload-allure-files-to-bucket/scripts/upload-allure-report.sh
+++ .github/actions/tests/upload-allure-files-to-bucket/scripts/upload-allure-report.sh
@@ -2,7 +2,7 @@
 
 echo "Uploading allure-report folder..."
 aws s3 cp $ALLURE_REPORT_DIR \
-    $S3_BUCKET/artifacts/$GITHUB_RUN_ID/$DESTINATION_DIR/allure-report \
+    "$S3_BUCKET/artifacts/$GITHUB_RUN_ID/$DESTINATION_DIR/allure-report" \
     --recursive \
     --only-show-errors
 echo "Done"
--- .github/actions/tests/upload-allure-files-to-bucket/scripts/upload-allure-results.sh
+++ .github/actions/tests/upload-allure-files-to-bucket/scripts/upload-allure-results.sh
@@ -2,7 +2,7 @@
 
 echo "Uploading allure-results folder..."
 aws s3 cp $ALLURE_RESULTS_DIR \
-    $S3_BUCKET/artifacts/$GITHUB_RUN_ID/$DESTINATION_DIR/allure-results \
+    "$S3_BUCKET/artifacts/$GITHUB_RUN_ID/$DESTINATION_DIR/allure-results" \
     --recursive \
     --only-show-errors
 echo "Done"
