3370c9a88133e905fc507de01d1bd587b57a5d99
Lourens Schep <lourensschep@gmail.com>
2023-03-20 12:44:07 +0100

    Add label to font family for block editor and fix template

--- plugins/woocommerce/includes/class-wc-post-types.php
+++ plugins/woocommerce/includes/class-wc-post-types.php
@@ -402,8 +402,25 @@ class WC_Post_Types {
 											),
 										),
 										array(
-											'woocommerce/product-summary',
+								'id'    => 'pricing',
+								'title' => __( 'Pricing', 'woocommerce' ),
+							),
+							array(
+								array(
+									'woocommerce/product-section',
+									array(
+										'title'       => __( 'Pricing', 'woocommerce' ),
+										'description' => sprintf(
+											/* translators: %1$s: Images guide link opening tag. %2$s: Images guide link closing tag.*/
+											__( 'Set a competitive price, put the product on sale, and manage tax calculations. %1$sHow to price your product?%2$s', 'woocommerce' ),
+											'<a href="https://woocommerce.com/posts/how-to-price-products-strategies-expert-tips/" target="_blank" rel="noreferrer">',
+											'</a>'
 										),
+										'icon'        => array(
+											'src' => '<svg xmlns="http://www.w3.org/2000/svg" view-box="0 0 24 24"><path fill-rule="evenodd" d="M16.83 6.342l.602.3.625-.25.443-.176v12.569l-.443-.178-.625-.25-.603.301-1.444.723-2.41-.804-.475-.158-.474.158-2.41.803-1.445-.722-.603-.3-.625.25-.443.177V6.215l.443.178.625.25.603-.301 1.444-.722 2.41.803.475.158.474-.158 2.41-.803 1.445.722zM20 4l-1.5.6-1 .4-2-1-3 1-3-1-2 1-1-.4L5 4v17l1.5-.6 1-.4 2 1 3-1 3 1 2-1 1 .4 1.5.6V4zm-3.5 6.25v-1.5h-8v1.5h8zm0 3v-1.5h-8v1.5h8zm-8 3v-1.5h8v1.5h-8z" clip-rule="evenodd" /></svg>',
+										),
+									),
+									array(
 										array(
 											'core/columns',
 											array(),
@@ -463,33 +480,15 @@ class WC_Post_Types {
 										),
 									),
 								),
-							),
-						),
-						array(
-							'woocommerce/product-tab',
-							array(
-								'id'    => 'pricing',
-								'title' => __( 'Pricing', 'woocommerce' ),
-							),
-							array(
 								array(
 									'woocommerce/product-section',
 									array(
-										'title'       => __( 'Pricing', 'woocommerce' ),
-										'description' => sprintf(
-											/* translators: %1$s: Images guide link opening tag. %2$s: Images guide link closing tag.*/
-											__( 'Set a competitive price, put the product on sale, and manage tax calculations. %1$sHow to price your product?%2$s', 'woocommerce' ),
-											'<a href="https://woocommerce.com/posts/how-to-price-products-strategies-expert-tips/" target="_blank" rel="noreferrer">',
-											'</a>'
-										),
-										'icon'        => array(
-											'src' => '<svg xmlns="http://www.w3.org/2000/svg" view-box="0 0 24 24"><path fill-rule="evenodd" d="M16.83 6.342l.602.3.625-.25.443-.176v12.569l-.443-.178-.625-.25-.603.301-1.444.723-2.41-.804-.475-.158-.474.158-2.41.803-1.445-.722-.603-.3-.625.25-.443.177V6.215l.443.178.625.25.603-.301 1.444-.722 2.41.803.475.158.474-.158 2.41-.803 1.445.722zM20 4l-1.5.6-1 .4-2-1-3 1-3-1-2 1-1-.4L5 4v17l1.5-.6 1-.4 2 1 3-1 3 1 2-1 1 .4 1.5.6V4zm-3.5 6.25v-1.5h-8v1.5h8zm0 3v-1.5h-8v1.5h8zm-8 3v-1.5h8v1.5h-8z" clip-rule="evenodd" /></svg>',
-										),
+										'title'       => __( 'Organization & visibility', 'woocommerce' ),
+										'description' => __( 'Help customers find this product by assigning it to categories or featuring it across your sales channels.', 'woocommerce' ),
 									),
 									array(
 										array(
-											'core/columns',
-											array(),
+											'woocommerce/product-category',
 											array(
 												array(
 													'core/column',
@@ -588,6 +587,9 @@ class WC_Post_Types {
 									),
 								),
 							),
+						),
+=======
+>>>>>>> 09bfcca82f (Add label to font family for block editor and fix template)
 						),
 						array(
 							'woocommerce/product-tab',
