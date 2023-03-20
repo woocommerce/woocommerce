849b395eaa93c8fbc8becac426f3ac55638ca3b3
Joshua Flowers <joshuatf@gmail.com>
2023-03-20 11:16:14 -0700

    Fix up RTL style builds for block assets

--- packages/js/product-editor/webpack.config.js
+++ packages/js/product-editor/webpack.config.js
@@ -43,13 +43,13 @@ module.exports = {
 			chunkFilename: 'chunks/[id].style.css',
 		} ),
 		new WebpackRTLPlugin( {
-			test: /^((?!editor).)*scss$/,
+			test: /style\.css$/,
 			filename: '[name]/style-rtl.css',
 			minify: NODE_ENV === 'development' ? false : { safe: true },
 		} ),
 		new WebpackRTLPlugin( {
-			test: /^editor\.scss$/,
-			filename: 'blocks/[name]/editor-rtl.css',
+			test: /(?<!style)\.css$/,
+			filename: '[name]-rtl.css',
 			minify: NODE_ENV === 'development' ? false : { safe: true },
 		} ),
 		new CopyWebpackPlugin( {
