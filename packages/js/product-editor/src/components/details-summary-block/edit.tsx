24c19586fe485bea74bfb0ed68247b22100598d5
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-03-20 15:33:32 -0300

    Add text alignment

--- packages/js/product-editor/src/components/details-summary-block/edit.tsx
+++ packages/js/product-editor/src/components/details-summary-block/edit.tsx
@@ -3,14 +3,28 @@
  */
 import { __ } from '@wordpress/i18n';
 import { createElement } from '@wordpress/element';
-import { RichText, useBlockProps } from '@wordpress/block-editor';
+import {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore No types for this exist yet.
+	AlignmentControl,
+	BlockControls,
+	RichText,
+	useBlockProps,
+} from '@wordpress/block-editor';
 import { useEntityProp } from '@wordpress/core-data';
 import { BaseControl } from '@wordpress/components';
 import uniqueId from 'lodash/uniqueId';
 import { BlockAttributes } from '@wordpress/blocks';
+import classNames from 'classnames';
 
-export function Edit( { attributes }: { attributes: BlockAttributes } ) {
-	const { label } = attributes;
+export function Edit( {
+	attributes,
+	setAttributes,
+}: {
+	attributes: BlockAttributes;
+	setAttributes( attributes: BlockAttributes ): void;
+} ) {
+	const { align, label } = attributes;
 	const blockProps = useBlockProps();
 	const id = uniqueId();
 	const [ summary, setSummary ] = useEntityProp< string >(
@@ -19,8 +33,23 @@ export function Edit( { attributes }: { attributes: BlockAttributes } ) {
 		'short_description'
 	);
 
+	function handleAlignmentChange( value: string ) {
+		setAttributes( {
+			align: value,
+		} );
+	}
+
 	return (
 		<div { ...blockProps }>
+			{ /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */ }
+			{ /* @ts-ignore No types for this exist yet. */ }
+			<BlockControls group="block">
+				<AlignmentControl
+					value={ align }
+					onChange={ handleAlignmentChange }
+				/>
+			</BlockControls>
+
 			<BaseControl
 				id={ id }
 				label={ label || __( 'Summary', 'woocommerce' ) }
@@ -36,7 +65,9 @@ export function Edit( { attributes }: { attributes: BlockAttributes } ) {
 						'woocommerce'
 					) }
 					data-empty={ Boolean( summary ) }
-					className="components-summary-control"
+					className={ classNames( 'components-summary-control', {
+						[ `has-text-align-${ align }` ]: align,
+					} ) }
 				/>
 			</BaseControl>
 		</div>
