f9cdca762559679dc95581cd596ac6a8a84a4ad9
Luigi Teschio <gigitux@gmail.com>
2023-03-20 14:27:23 +0100

    Remove Single Product Block (https://github.com/woocommerce/woocommerce-blocks/pull/8763)
    * Remove Single Product Block
    
    * remove more files that belong the Single Product block
    
    * fix php linter error
    
    * remove Product Tag List and Product Category List blocks

--- plugins/woocommerce-blocks/assets/js/previews/index.js
+++ plugins/woocommerce-blocks/assets/js/previews/index.js
@@ -6,4 +6,3 @@ export { previewShippingRates } from './shipping-rates';
 export { previewSavedPaymentMethods } from './saved-payment-methods';
 
 export { gridBlockPreview } from './grid-block';
-export { singleProductBlockPreview } from './single-product-block';
