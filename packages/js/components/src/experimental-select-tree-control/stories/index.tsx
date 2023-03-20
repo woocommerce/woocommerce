2ef7b25a695e6fe33cfacc7e7ce9512d7f170109
Nathan Schneider <nsschneider1@gmail.com>
2023-03-20 16:40:25 -0300

    Add storybook example for SelectTreeControl

---
+++ packages/js/components/src/experimental-select-tree-control/stories/index.tsx
@@ -0,0 +1,93 @@
+/**
+ * External dependencies
+ */
+
+import React, { createElement } from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { SelectTree } from '../../experimental-tree-control/select-tree';
+import { Item } from '../../experimental-tree-control/types';
+import '../../experimental-tree-control/select-tree.scss';
+import '../../experimental-tree-control/tree.scss';
+const listItems: Item[] = [
+	{ id: '1', name: 'Technology' },
+	{ id: '1.1', name: 'Notebooks', parent: '1' },
+	{ id: '1.2', name: 'Phones', parent: '1' },
+	{ id: '1.2.1', name: 'iPhone', parent: '1.2' },
+	{ id: '1.2.1.1', name: 'iPhone 14 Pro', parent: '1.2.1' },
+	{ id: '1.2.1.2', name: 'iPhone 14 Pro Max', parent: '1.2.1' },
+	{ id: '1.2.2', name: 'Samsung', parent: '1.2' },
+	{ id: '1.2.2.1', name: 'Samsung Galaxy 22 Plus', parent: '1.2.2' },
+	{ id: '1.2.2.2', name: 'Samsung Galaxy 22 Ultra', parent: '1.2.2' },
+	{ id: '1.3', name: 'Wearables', parent: '1' },
+	{ id: '2', name: 'Hardware' },
+	{ id: '2.1', name: 'CPU', parent: '2' },
+	{ id: '2.2', name: 'GPU', parent: '2' },
+	{ id: '2.3', name: 'Memory RAM', parent: '2' },
+	{ id: '3', name: 'Other' },
+];
+
+export const MultipleSelectTree: React.FC = () => {
+	const [ value, setValue ] = React.useState( '' );
+	const [ selected, setSelected ] = React.useState< Item[] >( [] );
+	return (
+		<SelectTree
+			label="Multiple Select Tree"
+			multiple
+			items={ listItems }
+			selected={ selected }
+			getFilteredItems={ ( items, searchValue ) => {
+				const filteredItems = items.filter( ( e ) =>
+					e.name.includes( searchValue )
+				);
+				const itemsToIterate = [ ...filteredItems ];
+				while ( itemsToIterate.length > 0 ) {
+					// The filter should include the parents of the filtered items
+					const element = itemsToIterate.pop();
+					if ( element ) {
+						const parent = listItems.find(
+							( x ) => x.id === element.parent
+						);
+						if ( parent && ! filteredItems.includes( parent ) ) {
+							filteredItems.push( parent );
+							itemsToIterate.push( parent );
+						}
+					}
+				}
+				return filteredItems;
+			} }
+			shouldNotRecursivelySelect
+			allowCreate
+			createValue={ value }
+			// eslint-disable-next-line no-alert
+			onCreateNew={ () => alert( 'create new called' ) }
+			onInputChange={ ( a ) => setValue( a || '' ) }
+			onSelect={ ( selectedItems ) => {
+				if ( Array.isArray( selectedItems ) ) {
+					setSelected( [ ...selected, ...selectedItems ] );
+				}
+			} }
+			onRemove={ ( removedItems ) => {
+				const newValues = Array.isArray( removedItems )
+					? selected.filter(
+							( item ) =>
+								! removedItems.some(
+									( { id: removedValue } ) =>
+										item.id === removedValue
+								)
+					  )
+					: selected.filter(
+							( item ) => item.id !== removedItems.id
+					  );
+				setSelected( newValues );
+			} }
+		/>
+	);
+};
+
+export default {
+	title: 'WooCommerce Admin/experimental/SelectTreeControl',
+	component: SelectTree,
+};
