b9df0e09f01a55effc48d23aa79d8ae33396cc10
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-03-20 23:12:09 -0300

    Create a helper hook for field validation (#37196)
    * Add custom hook to validate product fields within blocks
    
    * Add changelog file
    
    * Add unit tests
    
    * Fix non working tests
    
    * Fix linter errors
    
    * Add changelog file
    
    * Fix async validation rejection
    
    * Solve conflicts after rebasing
    
    * Revert false negative lint errors
    
    * Move the hook to its own folder and add the readme.md file

---
+++ packages/js/product-editor/src/hooks/use-validation/test/use-validation.test.ts
@@ -0,0 +1,95 @@
+/**
+ * External dependencies
+ */
+import { renderHook } from '@testing-library/react-hooks';
+import { useDispatch } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { useValidation } from '../use-validation';
+
+jest.mock( '@wordpress/data', () => ( {
+	useDispatch: jest.fn(),
+} ) );
+
+describe( 'useValidation', () => {
+	const useDispatchMock = useDispatch as jest.Mock;
+	const lockPostSaving = jest.fn();
+	const unlockPostSaving = jest.fn();
+
+	beforeEach( () => {
+		useDispatchMock.mockReturnValue( {
+			lockPostSaving,
+			unlockPostSaving,
+		} );
+	} );
+
+	afterEach( () => {
+		jest.clearAllMocks();
+	} );
+
+	describe( 'sync', () => {
+		it( 'should lock the editor if validate returns false', async () => {
+			const { result, waitForNextUpdate } = renderHook( () =>
+				useValidation( 'product/name', () => false )
+			);
+
+			await waitForNextUpdate();
+
+			expect( result.current ).toBeFalsy();
+			expect( lockPostSaving ).toHaveBeenCalled();
+			expect( unlockPostSaving ).not.toHaveBeenCalled();
+		} );
+
+		it( 'should unlock the editor if validate returns true', async () => {
+			const { result, waitForNextUpdate } = renderHook( () =>
+				useValidation( 'product/name', () => true )
+			);
+
+			await waitForNextUpdate();
+
+			expect( result.current ).toBeTruthy();
+			expect( lockPostSaving ).not.toHaveBeenCalled();
+			expect( unlockPostSaving ).toHaveBeenCalled();
+		} );
+	} );
+
+	describe( 'async', () => {
+		it( 'should lock the editor if validate resolves false', async () => {
+			const { result, waitForNextUpdate } = renderHook( () =>
+				useValidation( 'product/name', () => Promise.resolve( false ) )
+			);
+
+			await waitForNextUpdate();
+
+			expect( result.current ).toBeFalsy();
+			expect( lockPostSaving ).toHaveBeenCalled();
+			expect( unlockPostSaving ).not.toHaveBeenCalled();
+		} );
+
+		it( 'should lock the editor if validate rejects', async () => {
+			const { result, waitForNextUpdate } = renderHook( () =>
+				useValidation( 'product/name', () => Promise.reject() )
+			);
+
+			await waitForNextUpdate();
+
+			expect( result.current ).toBeFalsy();
+			expect( lockPostSaving ).toHaveBeenCalled();
+			expect( unlockPostSaving ).not.toHaveBeenCalled();
+		} );
+
+		it( 'should unlock the editor if validate resolves true', async () => {
+			const { result, waitForNextUpdate } = renderHook( () =>
+				useValidation( 'product/name', () => Promise.resolve( true ) )
+			);
+
+			await waitForNextUpdate();
+
+			expect( result.current ).toBeTruthy();
+			expect( lockPostSaving ).not.toHaveBeenCalled();
+			expect( unlockPostSaving ).toHaveBeenCalled();
+		} );
+	} );
+} );
