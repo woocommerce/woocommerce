1fd59edd02b0c0c79b404529adf3fb653947c35f
Lourens Schep <lourensschep@gmail.com>
2023-03-21 11:52:48 +0100

    Removed the conditional hook logic, because of React rule - Don’t call Hooks inside loops, conditions, or nested functions.

--- plugins/woocommerce-admin/client/products/hooks/use-product-entity-record.ts
+++ plugins/woocommerce-admin/client/products/hooks/use-product-entity-record.ts
@@ -5,48 +5,46 @@ import { AUTO_DRAFT_NAME } from '@woocommerce/product-editor';
 import { Product } from '@woocommerce/data';
 import { useDispatch, useSelect, select as WPSelect } from '@wordpress/data';
 
-import { useEffect, useState, useMemo } from '@wordpress/element';
-
-const useAutoDraft = () => {
-	const { saveEntityRecord } = useDispatch( 'core' );
-	const [ product, setProduct ] = useState< Product | undefined >(
-		undefined
-	);
-
-	useEffect( () => {
-		saveEntityRecord( 'postType', 'product', {
-			title: AUTO_DRAFT_NAME,
-			status: 'auto-draft',
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore Incorrect types.
-		} ).then( ( autoDraftProduct: Product ) => {
-			setProduct( autoDraftProduct );
-		} );
-	}, [] );
-
-	return product;
-};
-
-const useProductEntity = ( productId: number ) => {
-	const product = useSelect( ( select: typeof WPSelect ) => {
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore Missing types.
-		const { getEntityRecord } = select( 'core' );
-
-		return getEntityRecord( 'postType', 'product', productId ) as Product;
-	} );
-
-	return product;
-};
+import { useEffect, useState } from '@wordpress/element';
 
 export function useProductEntityRecord(
 	productId: string | undefined
 ): Product | undefined {
-	const selectedHook = useMemo( () => {
-		return productId
-			? useProductEntity.bind( {}, Number.parseInt( productId, 10 ) )
-			: useAutoDraft;
+	const { saveEntityRecord } = useDispatch( 'core' );
+	const [ newProduct, setNewProduct ] = useState< Product | undefined >(
+		undefined
+	);
+
+	useEffect( () => {
+		if ( ! productId ) {
+			saveEntityRecord( 'postType', 'product', {
+				title: AUTO_DRAFT_NAME,
+				status: 'auto-draft',
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore Incorrect types.
+			} ).then( ( autoDraftProduct: Product ) => {
+				setNewProduct( autoDraftProduct );
+			} );
+		}
 	}, [ productId ] );
 
-	return selectedHook();
+	const product = useSelect(
+		( select: typeof WPSelect ) => {
+			if ( ! productId ) {
+				return undefined;
+			}
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore Missing types.
+			const { getEntityRecord } = select( 'core' );
+
+			return getEntityRecord(
+				'postType',
+				'product',
+				Number.parseInt( productId, 10 )
+			) as Product;
+		},
+		[ productId ]
+	);
+
+	return productId !== undefined ? product : newProduct;
 }
