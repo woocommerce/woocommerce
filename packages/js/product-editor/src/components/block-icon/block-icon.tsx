4558b59fc775165bfb7eb1ad6db861d306fee67c
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-03-21 16:05:59 -0300

    Add block icon component

---
+++ packages/js/product-editor/src/components/block-icon/block-icon.tsx
@@ -0,0 +1,28 @@
+/**
+ * External dependencies
+ */
+import { Block } from '@wordpress/blocks';
+import { useSelect } from '@wordpress/data';
+import { createElement, Fragment } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { BlockIconProps } from './types';
+
+export function BlockIcon( { clientId }: BlockIconProps ) {
+	const icon = useSelect(
+		( select ) => {
+			const { getBlockName } = select( 'core/block-editor' );
+			const { getBlockType } = select( 'core/blocks' );
+
+			const blockName = getBlockName( clientId );
+			const block = getBlockType< Block >( blockName );
+
+			return block?.icon;
+		},
+		[ clientId ]
+	);
+
+	return icon?.src as JSX.Element;
+}
