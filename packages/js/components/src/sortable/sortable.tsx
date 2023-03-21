a3924d0e333cee768447794192ed1e2e7a3b8aec
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-03-21 13:24:44 -0700

    Fixed Miscellaneous TypeScript Errors
    After localizing the `"typeRoots"` there were some problems introduced.
    This commit addresses those problems so that the build compiles.

--- packages/js/components/src/sortable/sortable.tsx
+++ packages/js/components/src/sortable/sortable.tsx
@@ -43,7 +43,7 @@ export type SortableProps = {
 
 const THROTTLE_TIME = 16;
 
-export const SortableContext = createContext( {} );
+export const SortableContext: React.Context< {} > = createContext( {} );
 
 export const Sortable = ( {
 	children,
