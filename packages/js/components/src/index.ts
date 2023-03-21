edbe36d7ad16348c8227f2d14feb316fb0d2f6b3
Joel Thiessen <444632+joelclimbsthings@users.noreply.github.com>
2023-03-20 18:18:44 -0700

    Adding collapsible content block with flexible rendering (#37305)
    * Adding collapsible content block with flexible rendering
    
    * Adding changelogs
    
    * Move content inside display state to avoid backwards compat issues
    
    ---------
    
    Co-authored-by: Lourens Schep <lourensschep@gmail.com>

--- packages/js/components/src/index.ts
+++ packages/js/components/src/index.ts
@@ -103,3 +103,4 @@ export {
 	ProductSectionLayout as __experimentalProductSectionLayout,
 	ProductFieldSection as __experimentalProductFieldSection,
 } from './product-section-layout';
+export { DisplayState } from './display-state';
