aef6106713f7564e1d2fc9d07dc2b2160d42abea
Lourens Schep <lourensschep@gmail.com>
2023-03-21 16:27:34 +0100

    Use promise for receiving entity, and reload template on product change

--- plugins/woocommerce-admin/client/products/hooks/use-product-entity-record.ts
+++ plugins/woocommerce-admin/client/products/hooks/use-product-entity-record.ts
@@ -3,7 +3,7 @@
  */
 import { AUTO_DRAFT_NAME } from '@woocommerce/product-editor';
 import { Product } from '@woocommerce/data';
-import { useDispatch, useSelect, select as WPSelect } from '@wordpress/data';
+import { useDispatch, resolveSelect } from '@wordpress/data';
 
 import { useEffect, useState } from '@wordpress/element';
 
@@ -11,40 +11,32 @@ export function useProductEntityRecord(
 	productId: string | undefined
 ): Product | undefined {
 	const { saveEntityRecord } = useDispatch( 'core' );
-	const [ newProduct, setNewProduct ] = useState< Product | undefined >(
+	const [ product, setProduct ] = useState< Product | undefined >(
 		undefined
 	);
 
 	useEffect( () => {
-		if ( ! productId ) {
-			saveEntityRecord( 'postType', 'product', {
-				title: AUTO_DRAFT_NAME,
-				status: 'auto-draft',
-				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-				// @ts-ignore Incorrect types.
-			} ).then( ( autoDraftProduct: Product ) => {
-				setNewProduct( autoDraftProduct );
+		const getRecordPromise: Promise< Product > = productId
+			? resolveSelect( 'core' ).getEntityRecord< Product >(
+					'postType',
+					'product',
+					Number.parseInt( productId, 10 )
+			  )
+			: // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			  // @ts-ignore Incorrect types.
+			  ( saveEntityRecord( 'postType', 'product', {
+					title: AUTO_DRAFT_NAME,
+					status: 'auto-draft',
+			  } ) as Promise< Product > );
+		getRecordPromise
+			.then( ( autoDraftProduct: Product ) => {
+				setProduct( autoDraftProduct );
+			} )
+			.catch( ( e ) => {
+				setProduct( undefined );
+				throw e;
 			} );
-		}
 	}, [ productId ] );
 
-	const product = useSelect(
-		( select: typeof WPSelect ) => {
-			if ( ! productId ) {
-				return undefined;
-			}
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore Missing types.
-			const { getEntityRecord } = select( 'core' );
-
-			return getEntityRecord(
-				'postType',
-				'product',
-				Number.parseInt( productId, 10 )
-			) as Product;
-		},
-		[ productId ]
-	);
-
-	return productId !== undefined ? product : newProduct;
+	return product;
 }
