82c59ffb2992217f15d2bc6470350d5bc47a4b2c
Tarun Vijwani <tarun.vijwani@automattic.com>
2023-03-22 11:15:13 +0400

    Display the link to add the shipping address when shipping address is not available (https://github.com/woocommerce/woocommerce-blocks/pull/8141)

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/shipping-location/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/shipping-location/index.tsx
@@ -2,56 +2,15 @@
  * External dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
-import { ShippingAddress, getSetting } from '@woocommerce/settings';
-import { decodeEntities } from '@wordpress/html-entities';
 
 interface ShippingLocationProps {
-	address: ShippingAddress;
+	formattedLocation: string | null;
 }
 
-/**
- * Shows a formatted shipping location.
- *
- * @param {Object} props         Incoming props for the component.
- * @param {Object} props.address Incoming address information.
- */
+// Shows a formatted shipping location.
 const ShippingLocation = ( {
-	address,
+	formattedLocation,
 }: ShippingLocationProps ): JSX.Element | null => {
-	// we bail early if we don't have an address.
-	if ( Object.values( address ).length === 0 ) {
-		return null;
-	}
-	const shippingCountries = getSetting( 'shippingCountries', {} ) as Record<
-		string,
-		string
-	>;
-	const shippingStates = getSetting( 'shippingStates', {} ) as Record<
-		string,
-		Record< string, string >
-	>;
-	const formattedCountry =
-		typeof shippingCountries[ address.country ] === 'string'
-			? decodeEntities( shippingCountries[ address.country ] )
-			: '';
-
-	const formattedState =
-		typeof shippingStates[ address.country ] === 'object' &&
-		typeof shippingStates[ address.country ][ address.state ] === 'string'
-			? decodeEntities(
-					shippingStates[ address.country ][ address.state ]
-			  )
-			: address.state;
-
-	const addressParts = [];
-
-	addressParts.push( address.postcode.toUpperCase() );
-	addressParts.push( address.city );
-	addressParts.push( formattedState );
-	addressParts.push( formattedCountry );
-
-	const formattedLocation = addressParts.filter( Boolean ).join( ', ' );
-
 	if ( ! formattedLocation ) {
 		return null;
 	}
