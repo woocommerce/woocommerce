d23f55daaf6a72dd968d6674ceba3a42eb2b4107
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-03-24 10:12:37 -0300

    Change the BlockIcon component to get the icon from the attributes first and then from the metadata

--- packages/js/product-editor/src/components/block-icon/block-icon.tsx
+++ packages/js/product-editor/src/components/block-icon/block-icon.tsx
@@ -1,9 +1,10 @@
 /**
  * External dependencies
  */
+import { BlockIcon as BaseBlockIcon } from '@wordpress/block-editor';
 import { Block } from '@wordpress/blocks';
 import { useSelect } from '@wordpress/data';
-import { createElement } from '@wordpress/element';
+import { createElement, Fragment, RawHTML } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -13,16 +14,54 @@ import { BlockIconProps } from './types';
 export function BlockIcon( { clientId }: BlockIconProps ) {
 	const icon = useSelect(
 		( select ) => {
-			const { getBlockName } = select( 'core/block-editor' );
-			const { getBlockType } = select( 'core/blocks' );
+			// Try to get the icon from the block's attributes
+			const { getBlockAttributes, getBlockName } =
+				select( 'core/block-editor' );
+			const attributes = getBlockAttributes( clientId );
+			if ( attributes?.icon ) {
+				return attributes.icon;
+			}
 
+			// If there is no icon defined in attributes
+			// Then try to get icon from block's metadata
+			const { getBlockType } = select( 'core/blocks' );
 			const blockName = getBlockName( clientId );
 			const block = getBlockType< Block >( blockName );
-
 			return block?.icon;
 		},
 		[ clientId ]
 	);
 
-	return icon?.src as JSX.Element;
+	if ( ! icon ) {
+		return <Fragment />;
+	}
+
+	if ( typeof icon === 'object' ) {
+		const { src, ...iconProps } = icon;
+
+		if ( /^<(.)+>$/.test( src ) ) {
+			const iconComponent = (
+				<RawHTML aria-hidden="true" { ...iconProps }>
+					{ src }
+				</RawHTML>
+			);
+			return <BaseBlockIcon icon={ iconComponent } showColors />;
+		}
+
+		if ( /^https?:\/\/(.)+/.test( src ) ) {
+			const iconImage = (
+				<img
+					src={ src }
+					alt=""
+					aria-hidden="true"
+					{ ...iconProps }
+					height={ 24 }
+					width={ 24 }
+				/>
+			);
+			return <BaseBlockIcon icon={ iconImage } showColors />;
+		}
+	}
+
+	return <BaseBlockIcon icon={ icon } showColors />;
 }
