49892808928d6fec91c5b89184f26ca76f9e472f
Peter Fabian <peter.fabian.github@gmail.com>
2023-03-28 20:53:59 +0200

    Now actually working.

--- plugins/woocommerce/src/Internal/Utilities/LockUtil.php
+++ plugins/woocommerce/src/Internal/Utilities/LockUtil.php
@@ -16,29 +16,37 @@ class LockUtil {
 	 * Test and set a lock.
 	 *
 	 * @param string $lock_name The name of the lock.
-	 * @param int    $expiration The expiration time in minutes.
+	 * @param int    $expiration The expiration time in seconds.
 	 *
 	 * @return bool True if the lock was set, false otherwise.
 	 */
-	public static function test_and_set_expiring( $lock_name, $expiration = 10 ) {
+	public static function test_and_set_expiring( $lock_name, $expiration = 10 * MINUTE_IN_SECONDS ) {
 		global $wpdb;
 
 		$expiration = (int) $expiration;
 
 		$result = $wpdb->query(
 			$wpdb->prepare(
-				"UPDATE {$wpdb->options}
-					SET option_value = IF(NOW() > CAST(option_value AS DATETIME), DATE_ADD(NOW(), INTERVAL %d MINUTE), option_value)
-					WHERE option_name = %s;",
+				"INSERT INTO {$wpdb->options} (option_name, option_value, autoload) VALUES (%s, DATE_ADD(NOW(), INTERVAL %d SECOND), 'no')
+					ON DUPLICATE KEY UPDATE
+						option_value = IF(NOW() > CAST(option_value AS DATETIME), DATE_ADD(NOW(), INTERVAL %d SECOND), option_value);",
+				$lock_name,
 				$expiration,
-				$lock_name
+				$expiration
 			)
 		);
-		return 1 === $result;
+		/**  With ON DUPLICATE KEY UPDATE, the affected-rows value per row is 
+		 * 1 if the row is inserted as a new row,
+		 * 2 if an existing row is updated, and 
+		 * 0 if an existing row is set to its current values.
+		 */
+		return $result > 0;
 	}
 
 	/**
 	 * Unlock a lock.
+	 * 
+	 * Sets the expiration time to 1 second ago.
 	 *
 	 * @param string $lock_name The name of the lock.
 	 */
@@ -48,7 +56,7 @@ class LockUtil {
 		$wpdb->query(
 			$wpdb->prepare(
 				"UPDATE {$wpdb->options}
-					SET option_value = 0
+					SET option_value = DATE_SUB(NOW(), INTERVAL 1 SECOND)
 					WHERE option_name = %s;",
 				$lock_name
 			)
