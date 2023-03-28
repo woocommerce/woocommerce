e1cccc5b8eddc8f5b2bf8de4dbd8c251fa793868
Lucio Giannotta <lucio.giannotta@a8c.com>
2023-03-28 19:14:13 +0200

    Add Product Reviews block (https://github.com/woocommerce/woocommerce-blocks/pull/8857)
    ---------
    
    Co-authored-by: Patricia Hillebrandt <patriciahillebrandt@gmail.com>

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/index.js
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/index.js
@@ -14,5 +14,6 @@ import './product-elements/add-to-cart';
 import './product-elements/add-to-cart-form';
 import './product-elements/product-image-gallery';
 import './product-elements/product-details';
+import './product-elements/product-reviews';
 import './product-elements/related-products';
 import './product-elements/product-meta';
