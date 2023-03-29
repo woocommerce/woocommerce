03057fb36011ee07dc0ad95e0a372a0b2cedd2a8
Joshua Flowers <joshuatf@gmail.com>
2023-03-29 11:52:13 -0700

    Fix up php lint errors

--- plugins/woocommerce/includes/class-wc-post-types.php
+++ plugins/woocommerce/includes/class-wc-post-types.php
@@ -437,7 +437,8 @@ class WC_Post_Types {
 									array(
 										'title'       => __( 'Images', 'woocommerce' ),
 										'description' => sprintf(
-											__( 'Drag images, upload new ones or select files from your library. For best results, use JPEG files that are 1000 by 1000 pixels or larger. %sHow to prepare images?%s.', 'woocommerce' ),
+											/* translators: %1$s: Images guide link opening tag. %2$s: Images guide link closing tag.*/
+											__( 'Drag images, upload new ones or select files from your library. For best results, use JPEG files that are 1000 by 1000 pixels or larger. %1$sHow to prepare images?%2$s.', 'woocommerce' ),
 											'<a href="http://woocommerce.com/#" target="_blank" rel="noreferrer">',
 											'</a>'
 										),
@@ -447,7 +448,7 @@ class WC_Post_Types {
 											'woocommerce/product-images',
 											array(
 												'images' => array(),
-											)
+											),
 										),
 									),
 								),
