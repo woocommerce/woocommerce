4bc3e4080ac533226139378753e5d10f59423c10
Thomas Roberts <thomas.roberts@automattic.com>
2023-04-03 09:56:44 +0100

    Bump version numbers following release

--- plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
+++ plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Blocks
  * Plugin URI: https://github.com/woocommerce/woocommerce-gutenberg-products-block
  * Description: WooCommerce blocks for the Gutenberg editor.
- * Version: 9.9.0
+ * Version: 10.0.0-dev
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain:  woo-gutenberg-products-block
