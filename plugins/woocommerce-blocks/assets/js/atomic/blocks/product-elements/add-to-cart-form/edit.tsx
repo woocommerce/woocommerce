883d254ecf6917b81bec07d3b00fb5e3c501e9cd
Alexandre Lara <allexandrelara@gmail.com>
2023-04-05 09:38:58 -0300

    Add to Cart Form: Replace Notice with Skeleton component (https://github.com/woocommerce/woocommerce-blocks/pull/8842)
    * Add Skeleton component
    
    * Replace notice with skeleton on Add to Cart Form block
    
    * Fix css lint error
    
    * add tooltip
    
    * fix css linter
    
    * add css for change cursor
    
    ---------
    
    Co-authored-by: Luigi <gigitux@gmail.com>

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/add-to-cart-form/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/add-to-cart-form/edit.tsx
@@ -3,7 +3,9 @@
  */
 import { useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
-import { Button, Disabled, Notice } from '@wordpress/components';
+import { Button, Disabled, Tooltip } from '@wordpress/components';
+import { Skeleton } from '@woocommerce/base-components/skeleton';
+
 /**
  * Internal dependencies
  */
@@ -19,31 +21,34 @@ const Edit = () => {
 
 	return (
 		<div { ...blockProps }>
-			<Disabled>
-				<Notice
-					className={ 'wc-block-add-to-cart-form__notice' }
-					status={ 'warning' }
-					isDismissible={ false }
-				>
-					<p>
-						{ __(
-							'Customers will see product add-to-cart options displayed here, dependent on the product type.',
-							'woo-gutenberg-products-block'
-						) }
-					</p>
-				</Notice>
-				<input
-					type={ 'number' }
-					value={ '1' }
-					className={ 'wc-block-add-to-cart-form__quantity' }
-				/>
-				<Button
-					variant={ 'primary' }
-					className={ 'wc-block-add-to-cart-form__button' }
-				>
-					{ __( 'Add to cart', 'woo-gutenberg-products-block' ) }
-				</Button>
-			</Disabled>
+			<Tooltip
+				text="Customer will see product add-to-cart options in this space, dependend on the product type. "
+				position="bottom right"
+			>
+				<div className="wc-block-editor-container">
+					<Skeleton numberOfLines={ 3 } />
+					<Disabled>
+						<input
+							type={ 'number' }
+							value={ '1' }
+							className={
+								'wc-block-editor-add-to-cart-form__quantity'
+							}
+						/>
+						<Button
+							variant={ 'primary' }
+							className={
+								'wc-block-editor-add-to-cart-form__button'
+							}
+						>
+							{ __(
+								'Add to cart',
+								'woo-gutenberg-products-block'
+							) }
+						</Button>
+					</Disabled>
+				</div>
+			</Tooltip>
 		</div>
 	);
 };
