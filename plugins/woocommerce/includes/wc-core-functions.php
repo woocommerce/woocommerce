fadcc48e92238b6447110793e6ba11186c634c76
John Greys <j.grzes@greys.co>
2023-04-05 01:27:48 +0000

    Pass sanitized/unsanitized tip to wc_help_tip filter

--- plugins/woocommerce/includes/wc-core-functions.php
+++ plugins/woocommerce/includes/wc-core-functions.php
@@ -1593,20 +1593,24 @@ function wc_back_link( $label, $url ) {
  */
 function wc_help_tip( $tip, $allow_html = false ) {
 	if ( $allow_html ) {
-		$tip = wc_sanitize_tooltip( $tip );
+		$sanitized_tip = wc_sanitize_tooltip( $tip );
 	} else {
-		$tip = esc_attr( $tip );
+		$sanitized_tip = esc_attr( $tip );
 	}
 
 	/**
 	 * Filter the help tip.
 	 *
-	 * @param string $tip_html   Help tip HTML.
-	 * @param string $tip        Help tip text.
-	 * @param bool   $allow_html Allow sanitized HTML if true or escape.
+	 * @since 7.7.0
+	 *
+	 * @param string $tip_html       Help tip HTML.
+	 * @param string $sanitized_tip  Sanitized help tip text.
+	 * @param string $tip            Original help tip text.
+	 * @param bool   $allow_html     Allow sanitized HTML if true or escape.
+	 * 
 	 * @return string
 	 */
-	return apply_filters( 'wc_help_tip', '<span class="woocommerce-help-tip" data-tip="' . $tip . '"></span>', $tip, $allow_html );
+	return apply_filters( 'wc_help_tip', '<span class="woocommerce-help-tip" data-tip="' . $sanitized_tip . '"></span>', $sanitized_tip, $tip, $allow_html );
 }
 
 /**
