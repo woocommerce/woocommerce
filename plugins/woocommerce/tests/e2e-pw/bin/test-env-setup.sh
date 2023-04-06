a8c1386aafd55b7c0d42a298dec8d6c24741a6bc
jamelreid <jnoelreid@gmail.com>
2023-04-05 20:07:41 -0500

    Run docker compose directly and fix permission
    Running docker-compose directly circumvents the permission issues that arise occur when we try to specify a WP version for wp-env.
    
    This commit also normalizes the permission for wp-config.php file which allows wp-cli to properly update the url structure of the site

--- plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
+++ plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
@@ -4,39 +4,35 @@ ENABLE_HPOS="${ENABLE_HPOS:-0}"
 ENABLE_NEW_PRODUCT_EDITOR="${ENABLE_NEW_PRODUCT_EDITOR:-0}"
 ENABLE_TRACKING="${ENABLE_TRACKING:-0}"
 
-wp-env run tests-cli "wp theme install twentynineteen --activate"
+docker-compose -f $(wp-env install-path)/docker-compose.yml run --rm -u www-data -e HOME=/tmp tests-wordpress sh -c "chmod -c ugo+w /var/www/html/wp-config.php"
 
-wp-env run tests-cli "wp plugin install https://github.com/WP-API/Basic-Auth/archive/master.zip --activate"
-
-wp-env run tests-cli "wp plugin install wp-mail-logging --activate"
-
-wp-env run tests-cli "wp plugin install https://github.com/woocommerce/woocommerce-reset/archive/refs/heads/trunk.zip --activate"
-
-wp-env run tests-cli "wp rewrite structure /%postname%/"
-
-wp-env run tests-cli "wp user create customer customer@woocommercecoree2etestsuite.com \
+docker-compose -f $(wp-env install-path)/docker-compose.yml run --rm -u $(id -u) -e HOME=/tmp tests-cli sh -c "ls \
+&& wp theme install twentynineteen --activate \
+&& wp plugin install https://github.com/WP-API/Basic-Auth/archive/master.zip --activate \
+&& wp plugin install wp-mail-logging --activate \
+&& wp plugin install https://github.com/woocommerce/woocommerce-reset/archive/refs/heads/trunk.zip --activate \
+&& wp rewrite structure '/%postname%/' --hard \
+&& wp user create customer customer@woocommercecoree2etestsuite.com \
 	--user_pass=password \
 	--role=subscriber \
 	--first_name='Jane' \
 	--last_name='Smith' \
-	--path=/var/www/html \
-	--user_registered='2022-01-01 12:23:45'
-"
+	--user_registered='2022-01-01 12:23:45'"
 
 echo -e 'Update Blog Name \n'
-wp-env run tests-cli 'wp option update blogname "WooCommerce Core E2E Test Suite"'
+docker-compose -f $(wp-env install-path)/docker-compose.yml run --rm -u $(id -u) -e HOME=/tmp tests-cli sh -c 'wp option update blogname "WooCommerce Core E2E Test Suite"'
 
 if [ $ENABLE_HPOS == 1 ]; then
 	echo 'Enable the COT feature'
-	wp-env run tests-cli "wp plugin install https://gist.github.com/vedanshujain/564afec8f5e9235a1257994ed39b1449/archive/b031465052fc3e04b17624acbeeb2569ef4d5301.zip --activate"
+	docker-compose -f $(wp-env install-path)/docker-compose.yml run --rm -u $(id -u) -e HOME=/tmp tests-cli sh -c "wp plugin install https://gist.github.com/vedanshujain/564afec8f5e9235a1257994ed39b1449/archive/b031465052fc3e04b17624acbeeb2569ef4d5301.zip --activate"
 fi
 
 if [ $ENABLE_NEW_PRODUCT_EDITOR == 1 ]; then
 	echo 'Enable the new product editor feature'
-	wp-env run tests-cli "wp plugin install https://github.com/woocommerce/woocommerce-experimental-enable-new-product-editor/releases/download/0.1.0/woocommerce-experimental-enable-new-product-editor.zip --activate"
+	docker-compose -f $(wp-env install-path)/docker-compose.yml run --rm -u $(id -u) -e HOME=/tmp tests-cli sh -c "wp plugin install https://github.com/woocommerce/woocommerce-experimental-enable-new-product-editor/releases/download/0.1.0/woocommerce-experimental-enable-new-product-editor.zip --activate"
 fi
 
 if [ $ENABLE_TRACKING == 1 ]; then
 	echo 'Enable tracking'
-	wp-env run tests-cli "wp option update woocommerce_allow_tracking 'yes'"
+	docker-compose -f $(wp-env install-path)/docker-compose.yml run --rm -u $(id -u) -e HOME=/tmp tests-cli sh -c "wp option update woocommerce_allow_tracking 'yes'"
 fi
