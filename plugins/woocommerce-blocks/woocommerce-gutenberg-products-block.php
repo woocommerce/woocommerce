f5e0ddf34566ee9ab2eb8834d1a662bd61fc7e64
Thomas Roberts <thomas.roberts@automattic.com>
2023-04-11 15:33:30 +0100

    Port changes from release branch to trunk

--- plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
+++ plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
@@ -3,12 +3,12 @@
  * Plugin Name: WooCommerce Blocks
  * Plugin URI: https://github.com/woocommerce/woocommerce-gutenberg-products-block
  * Description: WooCommerce blocks for the Gutenberg editor.
- * Version: 10.0.0-dev
+ * Version: 10.0.0
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain:  woo-gutenberg-products-block
  * Requires at least: 6.1
- * Requires PHP: 7.2
+ * Requires PHP: 7.3
  * WC requires at least: 7.4
  * WC tested up to: 7.5
  *
