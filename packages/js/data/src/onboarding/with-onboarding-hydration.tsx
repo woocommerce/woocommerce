a0f40d1324ca0342bba43a74907794892098d480
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-04-11 12:52:30 +0800

    Add back additional checks

--- packages/js/data/src/onboarding/with-onboarding-hydration.tsx
+++ packages/js/data/src/onboarding/with-onboarding-hydration.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { createHigherOrderComponent } from '@wordpress/compose';
-import { useDispatch } from '@wordpress/data';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { createElement, useEffect, useRef } from '@wordpress/element';
 
 /**
@@ -19,8 +19,24 @@ export const withOnboardingHydration = ( data: {
 	return createHigherOrderComponent< Record< string, unknown > >(
 		( OriginalComponent ) => ( props ) => {
 			const onboardingRef = useRef( data );
+
+			const { isResolvingGroup, hasFinishedResolutionGroup } = useSelect(
+				( select ) => {
+					const { isResolving, hasFinishedResolution } =
+						select( STORE_NAME );
+					return {
+						isResolvingGroup: isResolving( 'getProfileItems', [] ),
+						hasFinishedResolutionGroup: hasFinishedResolution(
+							'getProfileItems',
+							[]
+						),
+					};
+				}
+			);
+
 			const { startResolution, finishResolution, setProfileItems } =
 				useDispatch( STORE_NAME );
+
 			useEffect( () => {
 				if ( ! onboardingRef.current ) {
 					return;
@@ -31,14 +47,25 @@ export const withOnboardingHydration = ( data: {
 					return;
 				}
 
-				if ( profileItems && ! hydratedProfileItems ) {
+				if (
+					profileItems &&
+					! hydratedProfileItems &&
+					! isResolvingGroup &&
+					! hasFinishedResolutionGroup
+				) {
 					startResolution( 'getProfileItems', [] );
 					setProfileItems( profileItems, true );
 					finishResolution( 'getProfileItems', [] );
 
 					hydratedProfileItems = true;
 				}
-			}, [ finishResolution, setProfileItems, startResolution ] );
+			}, [
+				finishResolution,
+				setProfileItems,
+				startResolution,
+				isResolvingGroup,
+				hasFinishedResolutionGroup,
+			] );
 
 			return <OriginalComponent { ...props } />;
 		},
