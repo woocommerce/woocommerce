24ffcb4b3abed9e2c6c1fc9304cc1024a834c09f
Mindaugas Budreika <mindaugas.bu@gmail.com>
2023-04-12 22:01:47 +0400

    Spacing fixes

--- plugins/woocommerce/includes/class-wc-rest-authentication.php
+++ plugins/woocommerce/includes/class-wc-rest-authentication.php
@@ -590,9 +590,9 @@ class WC_REST_Authentication {
 		/**
 		 * This filter enables the exclusion of the most recent access time from being logged for REST API calls.
 		 *
-		 * @param bool $result Default value.
-		 * @param int $key_id  Key ID associated with REST API request.
-		 * @param int $user_id User ID associated with REST API request.
+		 * @param bool $result  Default value.
+		 * @param int  $key_id  Key ID associated with REST API request.
+		 * @param int  $user_id User ID associated with REST API request.
 		 *
 		 * @since 7.7.0
 		 */
