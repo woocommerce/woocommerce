e8e7b436250f9745f62240d72ad0cdf25ecc55c9
Thomas Roberts <thomas.roberts@automattic.com>
2023-04-12 10:25:25 +0100

    Bump version numbers post-release

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -109,7 +109,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '10.0.0';
+					$version = '10.1.0-dev';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
