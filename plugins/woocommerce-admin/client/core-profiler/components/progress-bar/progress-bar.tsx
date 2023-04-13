3693965ac14a6b2b5342d1a21419aaebab605676
moon <moon.kyong@automattic.com>
2023-04-13 16:24:11 -0700

    Add navigation and progress-bar components

--- plugins/woocommerce-admin/client/core-profiler/components/progress-bar/progress-bar.tsx
+++ plugins/woocommerce-admin/client/core-profiler/components/progress-bar/progress-bar.tsx
@@ -1,9 +1,12 @@
 /**
+<<<<<<< HEAD
  * External dependencies
  */
 
 import { HTMLAttributes } from 'react';
 /**
+=======
+>>>>>>> 86c4dd7f82 (Add navigation and progress-bar components)
  * Internal dependencies
  */
 
@@ -16,16 +19,28 @@ type ProgressBarProps = {
 	bgcolor?: string;
 };
 
+<<<<<<< HEAD
 const ProgressBar = ( {
 	className = '',
 	percent = 0,
 	color = '#674399',
 	bgcolor = 'var(--wp-admin-theme-color)',
 }: ProgressBarProps ) => {
+=======
+function ProgressBar( props: ProgressBarProps ): JSX.Element {
+	const {
+		className = '',
+		percent = 0,
+		color = '#674399',
+		bgcolor = 'var(--wp-admin-theme-color)',
+	} = props;
+
+>>>>>>> 86c4dd7f82 (Add navigation and progress-bar components)
 	const containerStyles = {
 		backgroundColor: bgcolor,
 	};
 
+<<<<<<< HEAD
 	const fillerStyles: HTMLAttributes< HTMLDivElement >[ 'style' ] = {
 		backgroundColor: color,
 		width: `${ percent }%`,
@@ -40,11 +55,36 @@ const ProgressBar = ( {
 			>
 				<div
 					className="woocommerce-profiler-progress-bar__filler"
+=======
+	let fillerStyles: Record< string, string > = {
+		backgroundColor: color,
+		width: `${ percent }%`,
+	};
+
+	if ( percent === 0 ) {
+		fillerStyles = {
+			display: 'none',
+		};
+	}
+
+	return (
+		<div className={ `wc-progress-bar ${ className }` }>
+			<div
+				className="wc-progress-bar__container"
+				style={ containerStyles }
+			>
+				<div
+					className="wc-progress-bar__filler"
+>>>>>>> 86c4dd7f82 (Add navigation and progress-bar components)
 					style={ fillerStyles }
 				/>
 			</div>
 		</div>
 	);
+<<<<<<< HEAD
 };
+=======
+}
+>>>>>>> 86c4dd7f82 (Add navigation and progress-bar components)
 
 export default ProgressBar;
