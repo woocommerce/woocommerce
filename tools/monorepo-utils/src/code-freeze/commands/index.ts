4401d7fb702ab83d278aeaac0d218ae7ee26e58e
Paul Sealock <psealock@gmail.com>
2023-04-13 17:01:13 +1200

    Add milestone computation cli

--- tools/monorepo-utils/src/code-freeze/commands/index.ts
+++ tools/monorepo-utils/src/code-freeze/commands/index.ts
@@ -7,9 +7,11 @@ import { Command } from '@commander-js/extra-typings';
  * Internal dependencies
  */
 import { verifyDayCommand } from './verify-day';
+import { milesStoneCommand } from './milestone';
 
 const program = new Command( 'code-freeze' )
 	.description( 'Code freeze utilities' )
-	.addCommand( verifyDayCommand );
+	.addCommand( verifyDayCommand )
+	.addCommand( milesStoneCommand );
 
 export default program;
