b6039d1d7466002afdaa9e94d86c9de9af7632a8
Alba Rincón <albarin@users.noreply.github.com>
2023-04-14 09:48:55 +0200

    Mini cart: allow changing the drawer width (https://github.com/woocommerce/woocommerce-blocks/pull/8930)
    * WIP
    
    * Add width control to the settings sidebar
    
    * Fix linting errors
    
    * Remove comments
    
    * Inline function
    
    * Fix issue with smaller viewports
    
    * Remove unnecessary code

--- plugins/woocommerce-blocks/assets/js/blocks/mini-cart/mini-cart-contents/block.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/mini-cart/mini-cart-contents/block.tsx
@@ -8,11 +8,19 @@
 import './inner-blocks/register-components';
 
 type MiniCartContentsBlockProps = {
+	attributes: Record< string, unknown >;
 	children: JSX.Element | JSX.Element[];
 };
 
-export const MiniCartContentsBlock = ( {
-	children,
-}: MiniCartContentsBlockProps ): JSX.Element => {
+export const MiniCartContentsBlock = (
+	props: MiniCartContentsBlockProps
+): JSX.Element => {
+	const {
+		children,
+		attributes: { width },
+	} = props;
+
+	document.documentElement.style.setProperty( '--drawer-width', width );
+
 	return <>{ children }</>;
 };
