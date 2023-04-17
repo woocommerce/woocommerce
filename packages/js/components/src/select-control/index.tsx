810a62923f79ac9a37ed76a2ea7a38c2a7ae04f4
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-04-17 16:44:01 +0800

    Migrate select control to TS

--- packages/js/components/src/select-control/index.tsx
+++ packages/js/components/src/select-control/index.tsx
@@ -4,26 +4,205 @@
 import { __, _n, sprintf } from '@wordpress/i18n';
 import classnames from 'classnames';
 import { Component, createElement } from '@wordpress/element';
-import { debounce, escapeRegExp, identity, noop } from 'lodash';
-import PropTypes from 'prop-types';
+import {
+	debounce,
+	escapeRegExp,
+	identity,
+	isArray,
+	isNumber,
+	noop,
+} from 'lodash';
 import { withFocusOutside, withSpokenMessages } from '@wordpress/components';
 import { withInstanceId, compose } from '@wordpress/compose';
+import { ChangeEvent, InputHTMLAttributes } from 'react';
 
 /**
  * Internal dependencies
  */
+import { Option, Selected } from './types';
 import List from './list';
 import Tags from './tags';
 import Control from './control';
 
-const initialState = { isExpanded: false, isFocused: false, query: '' };
+type Props = {
+	/**
+	 * Name to use for the autofill field, not used if no string is passed.
+	 */
+	autofill?: string;
+	/**
+	 * A renderable component (or string) which will be displayed before the `Control` of this component.
+	 */
+	children?: React.ReactNode;
+	/**
+	 * Class name applied to parent div.
+	 */
+	className?: string;
+	/**
+	 * Class name applied to control wrapper.
+	 */
+	controlClassName?: string;
+	/**
+	 * Allow the select options to be disabled.
+	 */
+	disabled?: boolean;
+	/**
+	 * Exclude already selected options from the options list.
+	 */
+	excludeSelectedOptions?: boolean;
+	/**
+	 * Add or remove items to the list of options after filtering,
+	 * passed the array of filtered options and should return an array of options.
+	 */
+	onFilter?: (
+		options: Array< Option >,
+		query: string | null
+	) => Array< Option >;
+	/**
+	 * Function to add regex expression to the filter the results, passed the search query.
+	 */
+	getSearchExpression?: ( query: string ) => RegExp;
+	/**
+	 * Help text to be appended beneath the input.
+	 */
+	help?: string | JSX.Element;
+	/**
+	 * Render tags inside input, otherwise render below input.
+	 */
+	inlineTags?: boolean;
+	/**
+	 * Allow the select options to be filtered by search input.
+	 */
+	isSearchable?: boolean;
+	/**
+	 * A label to use for the main input.
+	 */
+	label?: string;
+	/**
+	 * Function called when selected results change, passed result list.
+	 */
+	onChange?: ( selected: string | Option[], query?: string | null ) => void;
+	/**
+	 * Function run after search query is updated, passed previousOptions and query,
+	 * should return a promise with an array of updated options.
+	 */
+	onSearch?: (
+		previousOptions: Array< Option >,
+		query: string | null
+	) => Promise< Array< Option > >;
+	/**
+	 * An array of objects for the options list.  The option along with its key, label and
+	 * value will be returned in the onChange event.
+	 */
+	options: Option[];
+	/**
+	 * A placeholder for the search input.
+	 */
+	placeholder?: string;
+	/**
+	 * Time in milliseconds to debounce the search function after typing.
+	 */
+	searchDebounceTime?: number;
+	/**
+	 * An array of objects describing selected values or optionally a string for a single value.
+	 * If the label of the selected value is omitted, the Tag of that value will not
+	 * be rendered inside the search box.
+	 */
+	selected?: Selected;
+	/**
+	 * A limit for the number of results shown in the options menu.  Set to 0 for no limit.
+	 */
+	maxResults?: number;
+	/**
+	 * Allow multiple option selections.
+	 */
+	multiple?: boolean;
+	/**
+	 * Render a 'Clear' button next to the input box to remove its contents.
+	 */
+	showClearButton?: boolean;
+	/**
+	 * The input type for the search box control.
+	 */
+	searchInputType?: InputHTMLAttributes< HTMLInputElement >[ 'type' ];
+	/**
+	 * Only show list options after typing a search query.
+	 */
+	hideBeforeSearch?: boolean;
+	/**
+	 * Show all options on focusing, even if a query exists.
+	 */
+	showAllOnFocus?: boolean;
+	/**
+	 * Render results list positioned statically instead of absolutely.
+	 */
+	staticList?: boolean;
+	/**
+	 * autocomplete prop for the Control input field.
+	 */
+	autoComplete?: string;
+	/**
+	 * Instance ID for the component.
+	 */
+	instanceId?: number;
+	/**
+	 * From withSpokenMessages
+	 */
+	debouncedSpeak?: ( message: string, assertive?: string ) => void;
+	/**
+	 * aria-label for the search input.
+	 */
+	ariaLabel?: string;
+	/**
+	 * On Blur callback.
+	 */
+	onBlur?: () => void;
+};
+
+type State = {
+	isExpanded: boolean;
+	isFocused: boolean;
+	query: string | null;
+	searchOptions: Option[];
+	selectedIndex?: number | null;
+};
+
+const initialState: State = {
+	isExpanded: false,
+	isFocused: false,
+	query: '',
+	searchOptions: [],
+};
 
 /**
  * A search box which filters options while typing,
  * allowing a user to select from an option from a filtered list.
  */
-export class SelectControl extends Component {
-	constructor( props ) {
+export class SelectControl extends Component< Props, State > {
+	static defaultProps: Partial< Props > = {
+		excludeSelectedOptions: true,
+		getSearchExpression: identity,
+		inlineTags: false,
+		isSearchable: false,
+		onChange: noop,
+		onFilter: identity,
+		onSearch: ( options: Option[] ) => Promise.resolve( options ),
+		maxResults: 0,
+		multiple: false,
+		searchDebounceTime: 0,
+		searchInputType: 'search',
+		selected: [],
+		showAllOnFocus: false,
+		showClearButton: false,
+		hideBeforeSearch: false,
+		staticList: false,
+		autoComplete: 'off',
+	};
+
+	node: HTMLDivElement | null = null;
+	activePromise: Promise< void | Option[] > | null = null;
+	cacheSearchOptions: Option[] = [];
+
+	constructor( props: Props ) {
 		super( props );
 
 		const { selected, options, excludeSelectedOptions } = props;
@@ -50,7 +229,7 @@ export class SelectControl extends Component {
 		this.setNewValue = this.setNewValue.bind( this );
 	}
 
-	bindNode( node ) {
+	bindNode( node: HTMLDivElement ) {
 		this.node = node;
 	}
 
@@ -58,7 +237,12 @@ export class SelectControl extends Component {
 		const { multiple, excludeSelectedOptions } = this.props;
 		const newState = { ...initialState };
 		// Reset selectedIndex if single selection.
-		if ( ! multiple && selected.length && selected[ 0 ].key ) {
+		if (
+			! multiple &&
+			isArray( selected ) &&
+			selected.length &&
+			selected[ 0 ].key
+		) {
 			newState.selectedIndex = ! excludeSelectedOptions
 				? this.props.options.findIndex(
 						( i ) => i.key === selected[ 0 ].key
@@ -101,9 +285,12 @@ export class SelectControl extends Component {
 		return selectedOption ? [ selectedOption ] : [];
 	}
 
-	selectOption( option ) {
+	selectOption( option: Option ) {
 		const { multiple, selected } = this.props;
-		const newSelected = multiple ? [ ...selected, option ] : [ option ];
+		const newSelected =
+			multiple && isArray( selected )
+				? [ ...selected, option ]
+				: [ option ];
 
 		this.reset( newSelected );
 
@@ -129,25 +316,24 @@ export class SelectControl extends Component {
 		} );
 	}
 
-	setNewValue( newValue ) {
+	setNewValue( newValue: Option[] ) {
 		const { onChange, selected, multiple } = this.props;
 		const { query } = this.state;
 		// Trigger a change if the selected value is different and pass back
 		// an array or string depending on the original value.
 		if ( multiple || Array.isArray( selected ) ) {
-			onChange( newValue, query );
+			onChange!( newValue, query );
 		} else {
-			onChange( newValue.length > 0 ? newValue[ 0 ].key : '', query );
+			onChange!( newValue.length > 0 ? newValue[ 0 ].key : '', query );
 		}
 	}
 
 	decrementSelectedIndex() {
 		const { selectedIndex } = this.state;
 		const options = this.getOptions();
-		const nextSelectedIndex =
-			selectedIndex !== null
-				? ( selectedIndex === 0 ? options.length : selectedIndex ) - 1
-				: options.length - 1;
+		const nextSelectedIndex = isNumber( selectedIndex )
+			? ( selectedIndex === 0 ? options.length : selectedIndex ) - 1
+			: options.length - 1;
 
 		this.setState( { selectedIndex: nextSelectedIndex } );
 	}
@@ -155,13 +341,14 @@ export class SelectControl extends Component {
 	incrementSelectedIndex() {
 		const { selectedIndex } = this.state;
 		const options = this.getOptions();
-		const nextSelectedIndex =
-			selectedIndex !== null ? ( selectedIndex + 1 ) % options.length : 0;
+		const nextSelectedIndex = isNumber( selectedIndex )
+			? ( selectedIndex + 1 ) % options.length
+			: 0;
 
 		this.setState( { selectedIndex: nextSelectedIndex } );
 	}
 
-	announce( searchOptions ) {
+	announce( searchOptions: Option[] ) {
 		const { debouncedSpeak } = this.props;
 		if ( ! debouncedSpeak ) {
 			return;
@@ -169,6 +356,7 @@ export class SelectControl extends Component {
 		if ( !! searchOptions.length ) {
 			debouncedSpeak(
 				sprintf(
+					// translators: %d: number of results.
 					_n(
 						'%d result found, use up and down arrow keys to navigate.',
 						'%d results found, use up and down arrow keys to navigate.',
@@ -187,23 +375,26 @@ export class SelectControl extends Component {
 	getOptions() {
 		const { isSearchable, options, excludeSelectedOptions } = this.props;
 		const { searchOptions } = this.state;
-		const selectedKeys = this.getSelected().map( ( option ) => option.key );
+		const selected = this.getSelected();
+		const selectedKeys = isArray( selected )
+			? selected.map( ( option ) => option.key )
+			: [];
 		const shownOptions = isSearchable ? searchOptions : options;
 
 		if ( excludeSelectedOptions ) {
-			return shownOptions.filter(
+			return shownOptions?.filter(
 				( option ) => ! selectedKeys.includes( option.key )
 			);
 		}
 		return shownOptions;
 	}
 
-	getOptionsByQuery( options, query ) {
+	getOptionsByQuery( options: Option[], query: string | null ) {
 		const { getSearchExpression, maxResults, onFilter } = this.props;
 		const filtered = [];
 
 		// Create a regular expression to filter the options.
-		const expression = getSearchExpression(
+		const expression = getSearchExpression!(
 			escapeRegExp( query ? query.trim() : '' )
 		);
 		const search = expression ? new RegExp( expression, 'i' ) : /^$/;
@@ -232,14 +423,14 @@ export class SelectControl extends Component {
 			}
 		}
 
-		return onFilter( filtered, query );
+		return onFilter!( filtered, query );
 	}
 
-	setExpanded( value ) {
+	setExpanded( value: boolean ) {
 		this.setState( { isExpanded: value } );
 	}
 
-	search( query ) {
+	search( query: string | null ) {
 		const cacheSearchOptions = this.cacheSearchOptions || [];
 		const searchOptions =
 			query !== null && ! query.length && ! this.props.hideBeforeSearch
@@ -252,11 +443,13 @@ export class SelectControl extends Component {
 				isFocused: true,
 				searchOptions,
 				selectedIndex:
-					query?.length > 0 ? null : this.state.selectedIndex, // Only reset selectedIndex if we're actually searching.
+					query && query?.length > 0
+						? null
+						: this.state.selectedIndex, // Only reset selectedIndex if we're actually searching.
 			},
 			() => {
 				this.setState( {
-					isExpanded: Boolean( this.getOptions().length ),
+					isExpanded: Boolean( this.getOptions()?.length ),
 				} );
 			}
 		);
@@ -264,11 +457,11 @@ export class SelectControl extends Component {
 		this.updateSearchOptions( query );
 	}
 
-	updateSearchOptions( query ) {
+	updateSearchOptions( query: string | null ) {
 		const { hideBeforeSearch, options, onSearch } = this.props;
 
 		const promise = ( this.activePromise = Promise.resolve(
-			onSearch( options, query )
+			onSearch!( options, query )
 		).then( ( promiseOptions ) => {
 			if ( promise !== this.activePromise ) {
 				// Another promise has become active since this one was asked to resolve, so do nothing,
@@ -288,7 +481,9 @@ export class SelectControl extends Component {
 				{
 					searchOptions,
 					selectedIndex:
-						query?.length > 0 ? null : this.state.selectedIndex, // Only reset selectedIndex if we're actually searching.
+						query && query?.length > 0
+							? null
+							: this.state.selectedIndex, // Only reset selectedIndex if we're actually searching.
 				},
 				() => {
 					this.setState( {
@@ -300,7 +495,7 @@ export class SelectControl extends Component {
 		} ) );
 	}
 
-	onAutofillChange( event ) {
+	onAutofillChange( event: ChangeEvent< HTMLInputElement > ) {
 		const { options } = this.props;
 		const searchOptions = this.getOptionsByQuery(
 			options,
@@ -327,13 +522,14 @@ export class SelectControl extends Component {
 		const { isExpanded, isFocused, selectedIndex } = this.state;
 
 		const hasMultiple = this.hasMultiple();
-		const { key: selectedKey = '' } = options[ selectedIndex ] || {};
+		const { key: selectedKey = '' } =
+			( isNumber( selectedIndex ) && options[ selectedIndex ] ) || {};
 		const listboxId = isExpanded
 			? `woocommerce-select-control__listbox-${ instanceId }`
-			: null;
+			: undefined;
 		const activeId = isExpanded
 			? `woocommerce-select-control__option-${ instanceId }-${ selectedKey }`
-			: null;
+			: undefined;
 
 		return (
 			<div
@@ -354,13 +550,24 @@ export class SelectControl extends Component {
 						name={ autofill }
 						type="text"
 						className="woocommerce-select-control__autofill-input"
-						tabIndex="-1"
+						tabIndex={ -1 }
 					/>
 				) }
 				{ children }
 				<Control
-					{ ...this.props }
-					{ ...this.state }
+					help={ this.props.help }
+					label={ this.props.label }
+					inlineTags={ inlineTags }
+					isSearchable={ isSearchable }
+					isFocused={ isFocused }
+					instanceId={ instanceId }
+					query={ this.state.query }
+					placeholder={ this.props.placeholder }
+					autoComplete={ this.props.autoComplete }
+					multiple={ this.props.multiple }
+					ariaLabel={ this.props.ariaLabel }
+					onBlur={ this.props.onBlur }
+					showAllOnFocus={ this.props.showAllOnFocus }
 					activeId={ activeId }
 					className={ controlClassName }
 					disabled={ disabled }
@@ -374,15 +581,20 @@ export class SelectControl extends Component {
 					updateSearchOptions={ this.updateSearchOptions }
 					decrementSelectedIndex={ this.decrementSelectedIndex }
 					incrementSelectedIndex={ this.incrementSelectedIndex }
+					showClearButton={ this.props.showClearButton }
 				/>
 				{ ! inlineTags && hasMultiple && (
-					<Tags { ...this.props } selected={ this.getSelected() } />
+					<Tags
+						onChange={ this.props.onChange! }
+						showClearButton={ this.props.showClearButton }
+						selected={ this.getSelected() }
+					/>
 				) }
 				{ isExpanded && (
 					<List
-						{ ...this.props }
-						{ ...this.state }
-						activeId={ activeId }
+						instanceId={ instanceId! }
+						selectedIndex={ selectedIndex }
+						staticList={ this.props.staticList! }
 						listboxId={ listboxId }
 						node={ this.node }
 						onSelect={ this.selectOption }
@@ -397,171 +609,6 @@ export class SelectControl extends Component {
 		);
 	}
 }
-
-SelectControl.propTypes = {
-	/**
-	 * Name to use for the autofill field, not used if no string is passed.
-	 */
-	autofill: PropTypes.string,
-	/**
-	 * A renderable component (or string) which will be displayed before the `Control` of this component.
-	 */
-	children: PropTypes.node,
-	/**
-	 * Class name applied to parent div.
-	 */
-	className: PropTypes.string,
-	/**
-	 * Class name applied to control wrapper.
-	 */
-	controlClassName: PropTypes.string,
-	/**
-	 * Allow the select options to be disabled.
-	 */
-	disabled: PropTypes.bool,
-	/**
-	 * Exclude already selected options from the options list.
-	 */
-	excludeSelectedOptions: PropTypes.bool,
-	/**
-	 * Add or remove items to the list of options after filtering,
-	 * passed the array of filtered options and should return an array of options.
-	 */
-	onFilter: PropTypes.func,
-	/**
-	 * Function to add regex expression to the filter the results, passed the search query.
-	 */
-	getSearchExpression: PropTypes.func,
-	/**
-	 * Help text to be appended beneath the input.
-	 */
-	help: PropTypes.oneOfType( [ PropTypes.string, PropTypes.node ] ),
-	/**
-	 * Render tags inside input, otherwise render below input.
-	 */
-	inlineTags: PropTypes.bool,
-	/**
-	 * Allow the select options to be filtered by search input.
-	 */
-	isSearchable: PropTypes.bool,
-	/**
-	 * A label to use for the main input.
-	 */
-	label: PropTypes.string,
-	/**
-	 * Function called when selected results change, passed result list.
-	 */
-	onChange: PropTypes.func,
-	/**
-	 * Function run after search query is updated, passed previousOptions and query,
-	 * should return a promise with an array of updated options.
-	 */
-	onSearch: PropTypes.func,
-	/**
-	 * An array of objects for the options list.  The option along with its key, label and
-	 * value will be returned in the onChange event.
-	 */
-	options: PropTypes.arrayOf(
-		PropTypes.shape( {
-			isDisabled: PropTypes.bool,
-			key: PropTypes.oneOfType( [ PropTypes.number, PropTypes.string ] )
-				.isRequired,
-			keywords: PropTypes.arrayOf(
-				PropTypes.oneOfType( [ PropTypes.string, PropTypes.number ] )
-			),
-			label: PropTypes.oneOfType( [
-				PropTypes.string,
-				PropTypes.object,
-			] ),
-			value: PropTypes.any,
-		} )
-	).isRequired,
-	/**
-	 * A placeholder for the search input.
-	 */
-	placeholder: PropTypes.string,
-	/**
-	 * Time in milliseconds to debounce the search function after typing.
-	 */
-	searchDebounceTime: PropTypes.number,
-	/**
-	 * An array of objects describing selected values or optionally a string for a single value.
-	 * If the label of the selected value is omitted, the Tag of that value will not
-	 * be rendered inside the search box.
-	 */
-	selected: PropTypes.oneOfType( [
-		PropTypes.string,
-		PropTypes.arrayOf(
-			PropTypes.shape( {
-				key: PropTypes.oneOfType( [
-					PropTypes.number,
-					PropTypes.string,
-				] ).isRequired,
-				label: PropTypes.string,
-			} )
-		),
-	] ),
-	/**
-	 * A limit for the number of results shown in the options menu.  Set to 0 for no limit.
-	 */
-	maxResults: PropTypes.number,
-	/**
-	 * Allow multiple option selections.
-	 */
-	multiple: PropTypes.bool,
-	/**
-	 * Render a 'Clear' button next to the input box to remove its contents.
-	 */
-	showClearButton: PropTypes.bool,
-	/**
-	 * The input type for the search box control.
-	 */
-	searchInputType: PropTypes.oneOf( [
-		'text',
-		'search',
-		'number',
-		'email',
-		'tel',
-		'url',
-	] ),
-	/**
-	 * Only show list options after typing a search query.
-	 */
-	hideBeforeSearch: PropTypes.bool,
-	/**
-	 * Show all options on focusing, even if a query exists.
-	 */
-	showAllOnFocus: PropTypes.bool,
-	/**
-	 * Render results list positioned statically instead of absolutely.
-	 */
-	staticList: PropTypes.bool,
-	/**
-	 * autocomplete prop for the Control input field.
-	 */
-	autoComplete: PropTypes.string,
-};
-
-SelectControl.defaultProps = {
-	autofill: null,
-	excludeSelectedOptions: true,
-	getSearchExpression: identity,
-	inlineTags: false,
-	isSearchable: false,
-	onChange: noop,
-	onFilter: identity,
-	onSearch: ( options ) => Promise.resolve( options ),
-	maxResults: 0,
-	multiple: false,
-	searchDebounceTime: 0,
-	searchInputType: 'search',
-	selected: [],
-	showAllOnFocus: false,
-	showClearButton: false,
-	hideBeforeSearch: false,
-	staticList: false,
-	autoComplete: 'off',
-};
 
 export default compose(
 	withSpokenMessages,
