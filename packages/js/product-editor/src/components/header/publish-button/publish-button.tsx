cbe65ef92d6c2bdfb1ca0acaa8bbd13b5c9c1e57
Joshua Flowers <joshuatf@gmail.com>
2023-04-17 16:59:58 -0700

    Refactor to use shared function for recording product events

--- packages/js/product-editor/src/components/header/publish-button/publish-button.tsx
+++ packages/js/product-editor/src/components/header/publish-button/publish-button.tsx
@@ -6,31 +6,25 @@ import { Button } from '@wordpress/components';
 import { createElement } from '@wordpress/element';
 import { getNewPath, navigateTo } from '@woocommerce/navigation';
 import { MouseEvent } from 'react';
-import { Product } from '@woocommerce/data';
-import { recordEvent } from '@woocommerce/tracks';
-import { useDispatch, useSelect } from '@wordpress/data';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore No types for this exist yet.
-// eslint-disable-next-line @woocommerce/dependency-group
-import { useEntityId } from '@wordpress/core-data';
+import { Product, ProductStatus } from '@woocommerce/data';
+import { useDispatch } from '@wordpress/data';
+import { useEntityProp } from '@wordpress/core-data';
 
 /**
  * Internal dependencies
  */
+import { recordProductEvent } from '../../../utils/record-product-event';
 import { usePublish } from '../hooks/use-publish';
 
 export function PublishButton(
 	props: Omit< Button.ButtonProps, 'aria-disabled' | 'variant' | 'children' >
 ) {
-	const productId = useEntityId( 'postType', 'product' );
-	const product: Product = useSelect( ( select ) =>
-		select( 'core' ).getEditedEntityRecord(
-			'postType',
-			'product',
-			productId
-		)
+	const [ productStatus ] = useEntityProp< ProductStatus | 'auto-draft' >(
+		'postType',
+		'product',
+		'status'
 	);
-	const { downloadable, id, manage_stock, status, type, virtual } = product;
+
 
 	const isCreating = status === 'auto-draft';
 
@@ -40,14 +34,8 @@ export function PublishButton(
 	const publishButtonProps = usePublish( {
 		...props,
 		onPublishSuccess( savedProduct: Product ) {
-			recordEvent( 'product_update', {
-				new_product_page: true,
-				product_id: id,
-				product_type: type,
-				is_downloadable: downloadable,
-				is_virtual: virtual,
-				manage_stock,
-			} );
+			recordProductEvent( 'product_update', savedProduct );
+
 			const noticeContent = isCreating
 				? __( 'Product successfully created.', 'woocommerce' )
 				: __( 'Product published.', 'woocommerce' );
@@ -70,7 +58,7 @@ export function PublishButton(
 
 			createSuccessNotice( noticeContent, noticeOptions );
 
-			if ( status === 'auto-draft' ) {
+			if ( productStatus === 'auto-draft' ) {
 				const url = getNewPath( {}, `/product/${ savedProduct.id }` );
 				navigateTo( { url } );
 			}
--- packages/js/product-editor/src/components/header/save-draft-button/save-draft-button.tsx
+++ packages/js/product-editor/src/components/header/save-draft-button/save-draft-button.tsx
@@ -5,31 +5,24 @@ import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 import { createElement } from '@wordpress/element';
 import { getNewPath, navigateTo } from '@woocommerce/navigation';
-import { Product } from '@woocommerce/data';
-import { recordEvent } from '@woocommerce/tracks';
-import { useDispatch, useSelect } from '@wordpress/data';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore No types for this exist yet.
-// eslint-disable-next-line @woocommerce/dependency-group
-import { useEntityId } from '@wordpress/core-data';
+import { Product, ProductStatus } from '@woocommerce/data';
+import { useDispatch } from '@wordpress/data';
+import { useEntityProp } from '@wordpress/core-data';
 
 /**
  * Internal dependencies
  */
+import { recordProductEvent } from '../../../utils/record-product-event';
 import { useSaveDraft } from '../hooks/use-save-draft';
 
 export function SaveDraftButton(
 	props: Omit< Button.ButtonProps, 'aria-disabled' | 'variant' | 'children' >
 ) {
-	const productId = useEntityId( 'postType', 'product' );
-	const product: Product = useSelect( ( select ) =>
-		select( 'core' ).getEditedEntityRecord(
-			'postType',
-			'product',
-			productId
-		)
+	const [ productStatus ] = useEntityProp< ProductStatus | 'auto-draft' >(
+		'postType',
+		'product',
+		'status'
 	);
-	const { downloadable, id, manage_stock, status, type, virtual } = product;
 
 	const { createSuccessNotice, createErrorNotice } =
 		useDispatch( 'core/notices' );
@@ -37,20 +30,13 @@ export function SaveDraftButton(
 	const saveDraftButtonProps = useSaveDraft( {
 		...props,
 		onSaveSuccess( savedProduct: Product ) {
-			recordEvent( 'product_edit', {
-				new_product_page: true,
-				product_id: id,
-				product_type: type,
-				is_downloadable: downloadable,
-				is_virtual: virtual,
-				manage_stock,
-			} );
+			recordProductEvent( 'product_edit', savedProduct );
 
 			createSuccessNotice(
 				__( 'Product saved as draft.', 'woocommerce' )
 			);
 
-			if ( status === 'auto-draft' ) {
+			if ( productStatus === 'auto-draft' ) {
 				const url = getNewPath( {}, `/product/${ savedProduct.id }` );
 				navigateTo( { url } );
 			}
