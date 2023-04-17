acd2bcd9bd9e9d3238eb4368ed9228e6f5fb4219
Joshua Flowers <joshuatf@gmail.com>
2023-04-17 16:29:58 -0700

    Record event on draft save

--- packages/js/product-editor/src/components/header/save-draft-button/save-draft-button.tsx
+++ packages/js/product-editor/src/components/header/save-draft-button/save-draft-button.tsx
@@ -1,13 +1,17 @@
 /**
  * External dependencies
  */
-import { Product, ProductStatus } from '@woocommerce/data';
-import { getNewPath, navigateTo } from '@woocommerce/navigation';
-import { Button } from '@wordpress/components';
-import { useEntityProp } from '@wordpress/core-data';
-import { useDispatch } from '@wordpress/data';
-import { createElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+import { createElement } from '@wordpress/element';
+import { getNewPath, navigateTo } from '@woocommerce/navigation';
+import { Product } from '@woocommerce/data';
+import { recordEvent } from '@woocommerce/tracks';
+import { useDispatch, useSelect } from '@wordpress/data';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore No types for this exist yet.
+// eslint-disable-next-line @woocommerce/dependency-group
+import { useEntityId } from '@wordpress/core-data';
 
 /**
  * Internal dependencies
@@ -17,11 +21,15 @@ import { useSaveDraft } from '../hooks/use-save-draft';
 export function SaveDraftButton(
 	props: Omit< Button.ButtonProps, 'aria-disabled' | 'variant' | 'children' >
 ) {
-	const [ productStatus ] = useEntityProp< ProductStatus | 'auto-draft' >(
-		'postType',
-		'product',
-		'status'
+	const productId = useEntityId( 'postType', 'product' );
+	const product: Product = useSelect( ( select ) =>
+		select( 'core' ).getEditedEntityRecord(
+			'postType',
+			'product',
+			productId
+		)
 	);
+	const { downloadable, id, manage_stock, status, type, virtual } = product;
 
 	const { createSuccessNotice, createErrorNotice } =
 		useDispatch( 'core/notices' );
@@ -29,11 +37,20 @@ export function SaveDraftButton(
 	const saveDraftButtonProps = useSaveDraft( {
 		...props,
 		onSaveSuccess( savedProduct: Product ) {
+			recordEvent( 'product_edit', {
+				new_product_page: true,
+				product_id: id,
+				product_type: type,
+				is_downloadable: downloadable,
+				is_virtual: virtual,
+				manage_stock,
+			} );
+
 			createSuccessNotice(
 				__( 'Product saved as draft.', 'woocommerce' )
 			);
 
-			if ( productStatus === 'auto-draft' ) {
+			if ( status === 'auto-draft' ) {
 				const url = getNewPath( {}, `/product/${ savedProduct.id }` );
 				navigateTo( { url } );
 			}
