1109f7147a16177c7a2820dbd6a2e5f9801b0add
Abdalsalaam Halawa <abdalsalaam@outlook.com>
2023-04-20 00:55:56 +0200

    Add comment to the translatable string

--- plugins/woocommerce/src/Checkout/Helpers/ReserveStock.php
+++ plugins/woocommerce/src/Checkout/Helpers/ReserveStock.php
@@ -130,6 +130,7 @@ final class ReserveStock {
 
 		// Add order note after successfully holding the stock.
 		if ( ! empty( $held_stock_notes ) ) {
+			// translators: %s is a time in minutes
 			$order->add_order_note( sprintf( __( 'Stock is held for %s minutes for payment:', 'woocommerce' ), $minutes ) . ' ' . implode( ', ', $order_notes ) );
 		}
 	}
