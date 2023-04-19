96ec158789d40a3a37a731305de7d81ea9786e3a
James Collins <james@om4.com.au>
2023-04-19 15:26:49 +0800

    Add test

--- plugins/woocommerce/tests/php/includes/class-wc-webhook-tests.php
+++ plugins/woocommerce/tests/php/includes/class-wc-webhook-tests.php
@@ -34,4 +34,49 @@ class WC_Webhook_Test extends WC_Unit_Test_Case {
 		$this->assertFalse( $call_is_valid_function->call( $webhook, $product->get_id() ) );
 	}
 
+	/**
+	 * @testDox Check that a deleted administrator user (with content re-assigned to another user)
+	 * does not cause webhook payloads to fail.
+	 */
+	public function test_payload_for_deleted_user_id() {
+		$admin_user_id_1 = wp_insert_user(
+			array(
+				'user_login' => 'test_admin',
+				'user_pass'  => 'password',
+				'role'       => 'administrator',
+			)
+		);
+
+		$webhook = new WC_Webhook();
+		$webhook->set_topic( 'order.created' );
+		$webhook->set_user_id( $admin_user_id_1 );
+		$webhook->save();
+
+		$order = \Automattic\WooCommerce\RestApi\UnitTests\Helpers\OrderHelper::create_order();
+
+		$payload = $webhook->build_payload( $order->get_id() );
+		$this->assertArrayNotHasKey( 'code', $payload );
+		$this->assertArrayHasKey( 'id', $payload );
+		$this->assertSame( $order->get_id(), $payload['id'] );
+
+		// Create a second admin user and delete the first one, reassigning existing content to the second user.
+		$admin_user_id_2 = wp_insert_user(
+			array(
+				'user_login' => 'test_admin2',
+				'user_pass'  => 'password',
+				'role'       => 'administrator',
+			)
+		);
+		wp_delete_user( $admin_user_id_1, $admin_user_id_2 );
+
+		// Re-load the webhook from the database.
+		$webhook = new WC_Webhook( $webhook->get_id() );
+		// Confirm user_id has been updated to the second admin user.
+		$this->assertSame( $admin_user_id_2, $webhook->get_user_id() );
+
+		$this->assertArrayNotHasKey( 'code', $payload );
+		$this->assertArrayHasKey( 'id', $payload );
+		$this->assertSame( $order->get_id(), $payload['id'] );
+	}
+
 }
