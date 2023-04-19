fdb14f639c2562b3a86b179f8046daa24538ba50
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-04-19 09:53:34 -0700

    Added Logging

--- plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
+++ plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
@@ -21,7 +21,12 @@ wp-env run tests-cli "user create customer customer@woocommercecoree2etestsuite.
 echo -e 'Update Blog Name \n'
 wp-env run tests-cli 'wp option update blogname "WooCommerce Core E2E Test Suite"'
 
-wp-env run tests-cli "wp plugin install https://gist.github.com/vedanshujain/564afec8f5e9235a1257994ed39b1449/archive/b031465052fc3e04b17624acbeeb2569ef4d5301.zip --activate"
+echo -e 'Root Permissions \n';
+wp-env run tests-cli "ls -la /var/www/html"
+echo -e 'Content Permissions \n';
+wp-env run tests-cli "ls -la /var/www/html/wp-content"
+echo -e 'Plugin Permissions \n';
+wp-env run tests-cli "ls -la /var/www/html/wp-content/plugins"
 
 if [ $ENABLE_HPOS == 1 ]; then
 	echo 'Enable the COT feature'
