1b13c81ff72fa16c164c299574f90bba4b41e6ad
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-04-19 12:31:26 -0700

    E2E Debugging

--- plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
+++ plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
@@ -8,7 +8,7 @@ echo -e 'Activate twentynineteen theme \n'
 wp-env run tests-cli "wp theme activate twentynineteen"
 
 echo -e 'Update URL structure \n'
-wp-env run tests-cli "wp rewrite structure '/%postname%/' --hard"
+wp-env run tests-cli "wp rewrite structure '/%postname%/' --hard --debug"
 
 echo -e 'Add Customer user \n'
 wp-env run tests-cli "user create customer customer@woocommercecoree2etestsuite.com \
