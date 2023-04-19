924c87173a5a70940f89f0512c474cfc662a81db
Darin Kotter <darin.kotter@gmail.com>
2023-04-19 16:21:39 -0600

    Bump template version number. Remove a few lines of CSS that aren't needed anymore

--- plugins/woocommerce/templates/single-product/product-image.php
+++ plugins/woocommerce/templates/single-product/product-image.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.5.1
+ * @version 7.8.0
  */
 
 defined( 'ABSPATH' ) || exit;
