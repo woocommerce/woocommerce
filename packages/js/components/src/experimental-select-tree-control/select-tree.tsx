2ba90b2fe61a805f442a341cc573674209d548fb
Nathan Schneider <nsschneider1@gmail.com>
2023-04-20 11:08:12 -0300

    Update SelectTree to support single scenario

--- packages/js/components/src/experimental-select-tree-control/select-tree.tsx
+++ packages/js/components/src/experimental-select-tree-control/select-tree.tsx
@@ -6,6 +6,7 @@ import { createElement, useState } from '@wordpress/element';
 import classNames from 'classnames';
 import { search } from '@wordpress/icons';
 import { useInstanceId } from '@wordpress/compose';
+import { TextControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -19,7 +20,7 @@ import { SelectTreeMenu } from './select-tree-menu';
 
 interface SelectTreeProps extends TreeControlProps {
 	id: string;
-	selected?: Item[];
+	selected?: Item | Item[];
 	getSelectedItemProps?: any;
 	treeRef?: React.ForwardedRef< HTMLOListElement >;
 	suffix?: JSX.Element | null;
@@ -48,6 +49,57 @@ export const SelectTree = function SelectTree( {
 	const [ isFocused, setIsFocused ] = useState( false );
 	const [ isOpen, setIsOpen ] = useState( false );
 
+	const inputProps: any = {
+		className: 'woocommerce-experimental-select-control__input',
+		id: `${ props.id }-input`,
+		'aria-autocomplete': 'list',
+		'aria-controls': `${ props.id }-menu`,
+		autoComplete: 'off',
+		onFocus: () => {
+			if ( ! isOpen ) {
+				setIsOpen( true );
+			}
+			setIsFocused( true );
+		},
+		onBlur: ( event: any ) => {
+			// if blurring to an element inside the dropdown, don't close it
+			if (
+				isOpen &&
+				! document
+					.querySelector( '.' + menuInstanceId )
+					?.contains( event.relatedTarget )
+			) {
+				setIsOpen( false );
+				if ( ! props.multiple ) {
+					if ( props.selected && onInputChange ) {
+						onInputChange( ( props.selected as Item ).label );
+					} else if ( onInputChange ) {
+						onInputChange( '' );
+					}
+				}
+			}
+			setIsFocused( false );
+		},
+		onKeyDown: ( event: any ) => {
+			setIsOpen( true );
+			if ( event.key === 'ArrowDown' ) {
+				event.preventDefault();
+				// focus on the first element from the Popover
+				(
+					document.querySelector(
+						`.${ menuInstanceId } input, .${ menuInstanceId } button`
+					) as HTMLInputElement | HTMLButtonElement
+				 )?.focus();
+			}
+			if ( event.key === 'Tab' ) {
+				setIsOpen( false );
+			}
+		},
+		onChange: ( event: any ) =>
+			onInputChange && onInputChange( event.target.value ),
+		placeholder,
+	};
+
 	return (
 		<div
 			className="woocommerce-experimental-select-tree-control__dropdown"
@@ -68,79 +120,69 @@ export const SelectTree = function SelectTree( {
 				>
 					{ props.label }
 				</label>
-				<ComboBox
-					comboBoxProps={ {
-						className:
-							'woocommerce-experimental-select-control__combo-box-wrapper',
-						role: 'combobox',
-						'aria-expanded': isOpen,
-						'aria-haspopup': 'tree',
-						'aria-labelledby': `${ props.id }-label`,
-						'aria-owns': `${ props.id }-menu`,
-					} }
-					inputProps={ {
-						className:
-							'woocommerce-experimental-select-control__input',
-						id: `${ props.id }-input`,
-						'aria-autocomplete': 'list',
-						'aria-controls': `${ props.id }-menu`,
-						autoComplete: 'off',
-						onFocus: () => {
-							if ( ! isOpen ) {
-								setIsOpen( true );
-							}
-							setIsFocused( true );
-						},
-						onBlur: ( event ) => {
-							// if blurring to an element inside the dropdown, don't close it
-							if (
-								isOpen &&
-								! document
-									.querySelector( '.' + menuInstanceId )
-									?.contains( event.relatedTarget )
-							) {
-								setIsOpen( false );
-							}
-							setIsFocused( false );
-						},
-						onKeyDown: ( event ) => {
-							setIsOpen( true );
-							if ( event.key === 'ArrowDown' ) {
-								event.preventDefault();
-								// focus on the first element from the Popover
-								(
-									document.querySelector(
-										`.${ menuInstanceId } input, .${ menuInstanceId } button`
-									) as HTMLInputElement | HTMLButtonElement
-								 )?.focus();
-							}
-							if ( event.key === 'Tab' ) {
-								setIsOpen( false );
-							}
-						},
-						onChange: ( event ) =>
-							onInputChange &&
-							onInputChange( event.target.value ),
-						placeholder,
-					} }
-					suffix={ suffix }
-				>
-					<SelectedItems
-						items={ ( props.selected as Item[] ) || [] }
-						getItemLabel={ ( item ) => item?.label || '' }
-						getItemValue={ ( item ) => item?.value || '' }
-						onRemove={ ( item ) => {
-							if ( ! Array.isArray( item ) && props.onRemove ) {
-								props.onRemove( item );
-								setIsOpen( false );
+				{ props.multiple ? (
+					<ComboBox
+						comboBoxProps={ {
+							className:
+								'woocommerce-experimental-select-control__combo-box-wrapper',
+							role: 'combobox',
+							'aria-expanded': isOpen,
+							'aria-haspopup': 'tree',
+							'aria-labelledby': `${ props.id }-label`,
+							'aria-owns': `${ props.id }-menu`,
+						} }
+						inputProps={ {
+							...inputProps,
+							onChange: ( event: any ) =>
+								onInputChange &&
+								onInputChange( event.target.value ),
+						} }
+						suffix={ suffix }
+					>
+						<SelectedItems
+							items={ ( props.selected as Item[] ) || [] }
+							getItemLabel={ ( item ) => item?.label || '' }
+							getItemValue={ ( item ) => item?.value || '' }
+							onRemove={ ( item ) => {
+								if (
+									! Array.isArray( item ) &&
+									props.onRemove
+								) {
+									props.onRemove( item );
+									setIsOpen( false );
+								}
+							} }
+							getSelectedItemProps={ () => ( {} ) }
+						/>
+					</ComboBox>
+				) : (
+					<TextControl
+						{ ...inputProps }
+						value={ props.createValue }
+						onChange={ ( value ) => {
+							if ( onInputChange ) onInputChange( value );
+							const item = items.find(
+								( i ) => i.label === value
+							);
+							if ( props.onSelect && item ) {
+								props.onSelect( item );
 							}
 						} }
-						getSelectedItemProps={ () => ( {} ) }
 					/>
-				</ComboBox>
+				) }
 			</div>
 			<SelectTreeMenu
 				{ ...props }
+				onSelect={ ( item ) => {
+					if ( ! props.multiple && onInputChange ) {
+						onInputChange( ( item as Item ).label );
+						setIsOpen( false );
+						setIsFocused( false );
+					}
+					if ( props.onSelect ) {
+						props.onSelect( item );
+					}
+				} }
 				id={ `${ props.id }-menu` }
 				className={ menuInstanceId.toString() }
 				ref={ ref }
