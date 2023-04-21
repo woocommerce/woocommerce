a4c88e90b16716cf6f1c3e69bdbeb3887c2101b9
faisal-alvi <faisalalvi33@gmail.com>
2023-04-21 14:05:15 +0530

    templates version bump to 7.8.0

--- plugins/woocommerce/templates/cart/mini-cart.php
+++ plugins/woocommerce/templates/cart/mini-cart.php
@@ -14,7 +14,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 5.2.0
+ * @version 7.8.0
  */
 
 defined( 'ABSPATH' ) || exit;
