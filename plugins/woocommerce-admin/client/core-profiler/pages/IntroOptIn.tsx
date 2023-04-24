e8fe8eb84dac33758d3e547fdb3531da02d21ffa
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-04-24 21:08:37 +0800

    Add core profiler - welcome to woo page

--- plugins/woocommerce-admin/client/core-profiler/pages/IntroOptIn.tsx
+++ plugins/woocommerce-admin/client/core-profiler/pages/IntroOptIn.tsx
@@ -1,36 +1,73 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Button, CheckboxControl } from '@wordpress/components';
+import { useState } from '@wordpress/element';
+
+/**
  * Internal dependencies
  */
 import { IntroOptInEvent } from '../index';
+import { Heading } from '../components/heading/heading';
+import Navigation from '../components/navigation/navigation';
+import { WelcomeImage } from '../assets/images/welcome-img';
 
 export const IntroOptIn = ( {
 	sendEvent,
+	navigationProgress,
 }: {
 	sendEvent: ( event: IntroOptInEvent ) => void;
+	navigationProgress: number;
 } ) => {
+	const [ iOptInDataSharing, setIsOptInDataSharing ] =
+		useState< boolean >( true );
+
 	return (
-		<>
-			<div>Intro Opt In</div>
-			<button
-				onClick={ () =>
-					sendEvent( {
-						type: 'INTRO_COMPLETED',
-						payload: { optInDataSharing: true },
-					} )
-				}
-			>
-				Next
-			</button>
-			<button
-				onClick={ () =>
+		<div className="woocommerce-profiler-intro-opt-in">
+			<Navigation
+				percentage={ navigationProgress }
+				onSkip={ () =>
 					sendEvent( {
 						type: 'INTRO_SKIPPED',
 						payload: { optInDataSharing: false },
 					} )
 				}
-			>
-				Skip
-			</button>
-		</>
+			/>
+			<div className="woocommerce-profiler-page__content woocommerce-profiler-intro-opt-in__content">
+				<WelcomeImage />
+				<Heading
+					title={ __( 'Welcome to Woo!', 'woocommerce' ) }
+					subTitle={ __(
+						'We’ll ask you a few questions to tailor your experience with Woo and help you set up your online store in a breeze.',
+						'woocommerce'
+					) }
+				/>
+				<Button
+					className="woocommerce-profiler-setup-store__button"
+					variant="primary"
+					onClick={ () =>
+						sendEvent( {
+							type: 'INTRO_COMPLETED',
+							payload: { optInDataSharing: iOptInDataSharing },
+						} )
+					}
+				>
+					{ __( 'Set up my store', 'woocommerce' ) }
+				</Button>
+
+				<div className="woocommerce-profiler-intro-opt-in__footer">
+					<CheckboxControl
+						className="woocommerce-profiler-intro-opt-in__checkbox"
+						label={ __(
+							'I agree to share my data to tailor my store setup experience and get more relevant content. WooCommerce never rent or sell your data and you can opt out at any time in WooCommerce settings. Learn more about usage tracking.',
+							'woocommerce'
+						) }
+						checked={ iOptInDataSharing }
+						onChange={ setIsOptInDataSharing }
+					/>
+				</div>
+			</div>
+		</div>
 	);
 };
