34bd5e1bf008598f791a7f788ff82560aac9297a
Paul Sealock <psealock@gmail.com>
2023-04-26 10:53:19 +1200

    Code Freeze CLI: Add branch command (#37914)

--- tools/monorepo-utils/src/code-freeze/commands/index.ts
+++ tools/monorepo-utils/src/code-freeze/commands/index.ts
@@ -7,11 +7,13 @@ import { Command } from '@commander-js/extra-typings';
  * Internal dependencies
  */
 import { verifyDayCommand } from './verify-day';
-import { milesStoneCommand } from './milestone';
+import { milestoneCommand } from './milestone';
+import { branchCommand } from './branch';
 
 const program = new Command( 'code-freeze' )
 	.description( 'Code freeze utilities' )
 	.addCommand( verifyDayCommand )
-	.addCommand( milesStoneCommand );
+	.addCommand( milestoneCommand )
+	.addCommand( branchCommand );
 
 export default program;
