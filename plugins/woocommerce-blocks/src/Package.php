8b769a812a362d3331e2c59775859049e85609b3
Alexandre Lara <allexandrelara@gmail.com>
2023-04-26 14:25:17 -0300

    Update plugin version

--- plugins/woocommerce-blocks/src/Package.php
+++ plugins/woocommerce-blocks/src/Package.php
@@ -109,7 +109,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '10.1.0';
+					$version = '10.2.0-dev';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
