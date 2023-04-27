6c54d482657df584faa1f4bdb0bf2a6f2bd4cc4e
moon <moon.kyong@automattic.com>
2023-04-27 13:18:27 -0700

    Fix js lint error

--- plugins/woocommerce-admin/client/core-profiler/index.tsx
+++ plugins/woocommerce-admin/client/core-profiler/index.tsx
@@ -76,7 +76,7 @@ export type CoreProfilerStateMachineContext = {
 	geolocatedLocation: {
 		location: string;
 	};
-	extensionsAvailable: ExtensionList[ 'plugins' ] | [  ];
+	extensionsAvailable: ExtensionList[ 'plugins' ] | [];
 	extensionsSelected: string[]; // extension slugs
 	businessInfo: { foo?: { bar: 'qux' }; location: string };
 	countries: { [ key: string ]: string };
