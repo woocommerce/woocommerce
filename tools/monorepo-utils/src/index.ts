6b158648e386646deba516937ea4b188fafa4443
Paul Sealock <psealock@gmail.com>
2023-04-28 08:40:44 +1200

    WooCommerce Utilities CLI: Add banner (#37910)
    * Add figlet
    
    * lockfile update

--- tools/monorepo-utils/src/index.ts
+++ tools/monorepo-utils/src/index.ts
@@ -2,11 +2,17 @@
  * External dependencies
  */
 import { Command } from '@commander-js/extra-typings';
+import figlet from 'figlet';
+import chalk from 'chalk';
 
 /**
  * Internal dependencies
  */
 import CodeFreeze from './code-freeze/commands';
+
+console.log(
+	chalk.rgb( 150, 88, 138 ).bold( figlet.textSync( 'WooCommerce Utilities' ) )
+);
 
 export const program = new Command()
 	.name( 'utils' )
