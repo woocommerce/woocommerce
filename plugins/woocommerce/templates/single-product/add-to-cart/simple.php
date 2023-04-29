080f88c5452bbec0bdb4ee160773fc751f8e22af
Sagehen Studio <caroline@sagehen.studio>
2023-04-29 07:40:54 -0700

    Add form id attribute
    Add form id attribute to all single product add-to-cart templates, for plugins/add-ons which might add <inputs> outside the <form>. Inputs can then use the form attribute to be included in the form submission.

--- plugins/woocommerce/templates/single-product/add-to-cart/simple.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/simple.php
@@ -29,7 +29,7 @@ if ( $product->is_in_stock() ) : ?>
 
 	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
+	<form class="cart" id="wc-add-to-cart-<?php esc_attr_e( $product->get_id() ); ?>" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
 		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
 		<?php
