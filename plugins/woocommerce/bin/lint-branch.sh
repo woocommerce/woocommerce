aa72f74e7a31e9be86c519d7ff8e673c6b435ee2
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2023-05-02 15:20:17 -0700

    Add doc block and make var name more explicit

--- plugins/woocommerce/bin/lint-branch.sh
+++ plugins/woocommerce/bin/lint-branch.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+# Lint branch
+#
+# Runs phpcs-changed, comparing the current branch to its "base" or "parent" branch.
+# The base branch defaults to trunk, but another branch name can be specified as an
+# optional positional argument.
+#
+# Example:
+# ./lint-branch.sh base-branch
+
 baseBranch=${1:-"trunk"}
 
-chg=$(git diff $(git merge-base HEAD $baseBranch) --relative --name-only -- '*.php')
+changedFiles=$(git diff $(git merge-base HEAD $baseBranch) --relative --name-only -- '*.php')
 
-[ -z $chg ] || composer exec phpcs-changed -- -s --git --git-base $baseBranch $chg
+# Only complete this if changed files are detected.
+[ -z $changedFiles ] || composer exec phpcs-changed -- -s --git --git-base $baseBranch $changedFiles
