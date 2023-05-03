eca933adda95609b9cf45ada3ab373ac62173563
Daniel W. Robert <danielwrobert@users.noreply.github.com>
2023-05-03 12:11:24 -0400

    Implement new `Hero Product – Split` pattern (https://github.com/woocommerce/woocommerce-blocks/pull/9191)
    * Add pattern for split hero product banner.
    
    This adds a new pattern for a hero product banner with a 50/50 split,
    heading and CTA on left and product image on right.
    
    Uses the Media + Text block.
    
    * Update image to use Unsplash placeholder.
    
    * Remove WooCommerce prefix in pattern title.
    
    * Remove unneeded wrapper group block.
    
    * Serve placeholder image from plugin directory.

---
+++ plugins/woocommerce-blocks/patterns/hero-product-split.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Hero Product - Split
+ * Slug: woocommerce-blocks/hero-product-split
+ * Categories: WooCommerce
+ */
+?>
+
+<!-- wp:media-text {"align":"full","mediaPosition":"right","mediaType":"image","mediaSizeSlug":"full","imageFill":false,"backgroundColor":"contrast","textColor":"base"} -->
+<div class="wp-block-media-text alignfull has-media-on-the-right is-stacked-on-mobile has-base-color has-contrast-background-color has-text-color has-background">
+	<div class="wp-block-media-text__content"><!-- wp:heading -->
+		<h2 class="wp-block-heading">Get cozy this fall with<br>flannel shirts</h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:buttons {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
+		<div class="wp-block-buttons" style="margin-bottom:var(--wp--preset--spacing--40)">
+			<!-- wp:button -->
+			<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Shop now</a></div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+
+	<figure class="wp-block-media-text__media">
+		<img src="<?php echo esc_url( plugins_url() ); ?>/woocommerce-blocks/images/pattern-placeholders/hero-product-split.webp" alt="Woman in red, black, and white plaid hoodie." />
+	</figure>
+</div>
+<!-- /wp:media-text -->
