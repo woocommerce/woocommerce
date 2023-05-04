e52e7e55569d7cfc256bbdb95a38e0f209d34bab
Nathan Schneider <nsschneider1@gmail.com>
2023-05-04 15:33:01 -0300

    Add tracks tests to activity_panel_stock_update_stock

--- plugins/woocommerce-admin/client/homescreen/activity-panel/stock/test/index.js
+++ plugins/woocommerce-admin/client/homescreen/activity-panel/stock/test/index.js
@@ -4,11 +4,23 @@
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { createElement } from '@wordpress/element';
+import { recordEvent } from '@woocommerce/tracks';
 
 /**
  * Internal dependencies
  */
 import { StockPanel } from '../';
+
+jest.mock( '@woocommerce/tracks', () => {
+	// Require the original module to not be mocked...
+	const originalModule = jest.requireActual( '@woocommerce/tracks' );
+
+	return {
+		__esModule: true, // Use it when dealing with esModules
+		...originalModule,
+		recordEvent: jest.fn(),
+	};
+} );
 
 describe( 'StockPanel', () => {
 	it( 'should the correct number of placeholders', () => {
@@ -61,5 +73,35 @@ describe( 'StockPanel', () => {
 		await waitFor( () => {
 			expect( invalidateResolution ).toHaveBeenCalled();
 		} );
+	} );
+	it( 'should fire tracks event activity_panel_stock_update_stock when Update stock is clicked', async () => {
+		const createNotice = jest.fn();
+		const invalidateResolution = jest.fn();
+		const updateProductStock = jest.fn().mockResolvedValue( true );
+
+		const { getByRole } = render(
+			<StockPanel
+				lowStockProductsCount={ 1 }
+				isError={ false }
+				isRequesting={ false }
+				products={ [
+					{
+						id: 1,
+						name: 'Test Product',
+						low_stock_amount: 5,
+						stock_quantity: 1,
+						type: 'simple',
+					},
+				] }
+				invalidateResolution={ invalidateResolution }
+				updateProductStock={ updateProductStock }
+				createNotice={ createNotice }
+			/>
+		);
+		userEvent.click( getByRole( 'button', { name: 'Update stock' } ) );
+		expect( recordEvent ).toHaveBeenLastCalledWith(
+			'activity_panel_stock_update_stock',
+			{}
+		);
 	} );
 } );
