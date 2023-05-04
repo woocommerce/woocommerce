ba44be015476b2851d9504026eec7eed2c495ae5
Matt Sherman <matt@jam123.com>
2023-05-04 15:36:09 -0400

    Remove unnecessary add-attribute-container div

--- plugins/woocommerce/includes/admin/meta-boxes/views/html-product-data-attributes.php
+++ plugins/woocommerce/includes/admin/meta-boxes/views/html-product-data-attributes.php
@@ -17,25 +17,23 @@ $product_attributes = $product_object->get_attributes( 'edit' );
 ?>
 <div id="product_attributes" class="panel wc-metaboxes-wrapper hidden">
 	<div class="toolbar toolbar-top">
-		<div class="add-attribute-container">
-			<div id="message" class="inline notice woocommerce-message">
-				<p>
-					<?php
-					esc_html_e(
-						'Add descriptive pieces of information that customers can use to search for this product on your store, such as “Material” or “Brand”.',
-						'woocommerce'
-					);
-					?>
-				</p>
-			</div>
-			<span class="expand-close">
-				<a href="#" class="expand_all"><?php esc_html_e( 'Expand', 'woocommerce' ); ?></a> / <a href="#" class="close_all"><?php esc_html_e( 'Close', 'woocommerce' ); ?></a>
-			</span>
-			<div class="actions">
-				<button type="button" class="button add_custom_attribute"><?php esc_html_e( 'Add new', 'woocommerce' ); ?></button>
-				<select class="wc-attribute-search" data-placeholder="<?php esc_attr_e( 'Add existing', 'woocommerce' ); ?>" data-minimum-input-length="0">
-				</select>
-			</div>
+		<div id="message" class="inline notice woocommerce-message">
+			<p>
+				<?php
+				esc_html_e(
+					'Add descriptive pieces of information that customers can use to search for this product on your store, such as “Material” or “Brand”.',
+					'woocommerce'
+				);
+				?>
+			</p>
+		</div>
+		<span class="expand-close">
+			<a href="#" class="expand_all"><?php esc_html_e( 'Expand', 'woocommerce' ); ?></a> / <a href="#" class="close_all"><?php esc_html_e( 'Close', 'woocommerce' ); ?></a>
+		</span>
+		<div class="actions">
+			<button type="button" class="button add_custom_attribute"><?php esc_html_e( 'Add new', 'woocommerce' ); ?></button>
+			<select class="wc-attribute-search" data-placeholder="<?php esc_attr_e( 'Add existing', 'woocommerce' ); ?>" data-minimum-input-length="0">
+			</select>
 		</div>
 	</div>
 	<div class="product_attributes wc-metaboxes">
