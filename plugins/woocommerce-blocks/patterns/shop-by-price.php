77307df7f67bdc60b159dfbd10012a8684704c7b
Roy Ho <roykho77@gmail.com>
2023-05-05 06:33:20 -0700

    Add shop by price pattern (https://github.com/woocommerce/woocommerce-blocks/pull/9373)

---
+++ plugins/woocommerce-blocks/patterns/shop-by-price.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Title: Shop by Price
+ * Slug: woocommerce-blocks/shop-by-price
+ * Categories: WooCommerce
+ */
+?>
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"25%","layout":{"type":"constrained"}} -->
+	<div class="wp-block-column" style="flex-basis:25%">
+		<!-- wp:cover {"customOverlayColor":"#e09300","minHeight":130,"minHeightUnit":"px","contentPosition":"top left","isDark":false,"style":{"spacing":{"blockGap":"0.8em","padding":{"top":"1em","right":"1em","bottom":"1em","left":"1em"}}},"textColor":"background","layout":{"type":"default"}} -->
+		<div class="wp-block-cover is-light has-custom-content-position is-position-top-left has-background-color has-text-color" style="padding-top:1em;padding-right:1em;padding-bottom:1em;padding-left:1em;min-height:130px">
+			<span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#e09300"></span>
+
+			<div class="wp-block-cover__inner-container">
+				<!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"typography":{"fontSize":"0.7em","lineHeight":"0"}},"textColor":"background"} -->
+				<p class="has-text-align-left has-background-color has-text-color" style="font-size:0.7em;line-height:0">Up to</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"style":{"typography":{"lineHeight":"0"}},"textColor":"background","fontSize":"large"} -->
+				<p class="has-background-color has-text-color has-large-font-size" style="line-height:0"><strong>30% Off</strong></p>
+				<!-- /wp:paragraph -->
+			</div>
+		</div>
+		<!-- /wp:cover -->
+
+		<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"10px","right":"10px","bottom":"10px","left":"10px"}}},"fontSize":"small"} -->
+		<p class="has-text-align-center has-small-font-size" style="margin-top:10px;margin-right:10px;margin-bottom:10px;margin-left:10px">
+			<strong>Outdoor Furniture &amp; Accessories</strong>
+		</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"width":"25%"} -->
+	<div class="wp-block-column" style="flex-basis:25%">
+		<!-- wp:cover {"customOverlayColor":"#6f64f1","minHeight":130,"minHeightUnit":"px","contentPosition":"top left","style":{"spacing":{"blockGap":"0.8em"}},"textColor":"background","layout":{"type":"default"}} -->
+		<div class="wp-block-cover has-custom-content-position is-position-top-left has-background-color has-text-color" style="min-height:130px">
+			<span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#6f64f1"></span>
+			<div class="wp-block-cover__inner-container">
+				<!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"typography":{"fontSize":"0.7em","lineHeight":"0"}},"textColor":"background"} -->
+				<p class="has-text-align-left has-background-color has-text-color" style="font-size:0.7em;line-height:0">Under</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"style":{"typography":{"lineHeight":"0"}},"textColor":"background","fontSize":"large"} -->
+				<p class="has-background-color has-text-color has-large-font-size" style="line-height:0">
+					<strong>$15</strong>
+				</p>
+				<!-- /wp:paragraph -->
+			</div>
+		</div>
+		<!-- /wp:cover -->
+
+		<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"10px","right":"10px","bottom":"10px","left":"10px"}}},"fontSize":"small"} -->
+		<p class="has-text-align-center has-small-font-size" style="margin-top:10px;margin-right:10px;margin-bottom:10px;margin-left:10px">
+			<strong>Summer Dinning</strong>
+		</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"width":"25%"} -->
+	<div class="wp-block-column" style="flex-basis:25%">
+		<!-- wp:cover {"customOverlayColor":"#c1d21c","minHeight":130,"minHeightUnit":"px","contentPosition":"top left","isDark":false,"style":{"spacing":{"blockGap":"0.8em"}},"textColor":"background","layout":{"type":"default"}} -->
+		<div class="wp-block-cover is-light has-custom-content-position is-position-top-left has-background-color has-text-color" style="min-height:130px">
+			<span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#c1d21c"></span>
+			<div class="wp-block-cover__inner-container">
+				<!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"typography":{"fontSize":"0.7em","lineHeight":"0"}},"textColor":"background"} -->
+				<p class="has-text-align-left has-background-color has-text-color" style="font-size:0.7em;line-height:0">Under</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:paragraph {"style":{"typography":{"lineHeight":"0"}},"textColor":"background","fontSize":"large"} -->
+				<p class="has-background-color has-text-color has-large-font-size" style="line-height:0">
+					<strong>$25</strong>
+				</p>
+				<!-- /wp:paragraph -->
+			</div>
+		</div>
+		<!-- /wp:cover -->
+
+		<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"10px","right":"10px","bottom":"10px","left":"10px"}}},"fontSize":"small"} -->
+		<p class="has-text-align-center has-small-font-size" style="margin-top:10px;margin-right:10px;margin-bottom:10px;margin-left:10px">
+			<strong>Women's Styles</strong>
+		</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"width":"25%"} -->
+	<div class="wp-block-column" style="flex-basis:25%">
+		<!-- wp:cover {"customOverlayColor":"#10ca99","minHeight":130,"minHeightUnit":"px","contentPosition":"top left","isDark":false,"style":{"spacing":{"blockGap":"0.8em"}},"textColor":"background","layout":{"type":"default"}} -->
+		<div class="wp-block-cover is-light has-custom-content-position is-position-top-left has-background-color has-text-color" style="min-height:130px">
+			<span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#10ca99"></span><div class="wp-block-cover__inner-container">
+			<!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"typography":{"fontSize":"0.7em","lineHeight":"0"}},"textColor":"background"} -->
+			<p class="has-text-align-left has-background-color has-text-color" style="font-size:0.7em;line-height:0">Under</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"style":{"typography":{"lineHeight":"0"}},"textColor":"background","fontSize":"large"} -->
+			<p class="has-background-color has-text-color has-large-font-size" style="line-height:0">
+				<strong>$20</strong>
+			</p>
+			<!-- /wp:paragraph -->
+			</div>
+		</div>
+		<!-- /wp:cover -->
+
+		<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"10px","right":"10px","bottom":"10px","left":"10px"}}},"fontSize":"small"} -->
+		<p class="has-text-align-center has-small-font-size" style="margin-top:10px;margin-right:10px;margin-bottom:10px;margin-left:10px">
+			<strong>Kids' Styles</strong>
+		</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
