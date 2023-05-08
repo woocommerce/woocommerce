5cc1dc74a168d619ed2bb99253147b14e069741c
rodelgc <rodel.calasagsag@automattic.com>
2023-05-08 13:33:01 +0800

    Provide env slug and release version as inputs

--- .github/actions/tests/slack-summary-on-release/slack-blocks/scripts/create-result-block/index.js
+++ .github/actions/tests/slack-summary-on-release/slack-blocks/scripts/create-result-block/index.js
@@ -1,13 +1,22 @@
 module.exports = () => {
-	const { API_RESULT, E2E_RESULT, TEST_NAME } = process.env;
+	const { API_RESULT, E2E_RESULT, ENV_SLUG, TEST_NAME, RELEASE_VERSION } =
+		process.env;
 	const { initContextBlock } = require( './utils/init-context-block' );
-	const {
-		createElementBlock,
-	} = require( './utils/create-element-block' );
+	const { createElementBlock } = require( './utils/create-element-block' );
 
 	const contextBlock = initContextBlock( TEST_NAME );
-	const apiElementBlock = createElementBlock( 'API', API_RESULT );
-	const e2eElementBlock = createElementBlock( 'E2E', E2E_RESULT );
+	const apiElementBlock = createElementBlock( {
+		testType: 'API',
+		result: API_RESULT,
+		envSlug: ENV_SLUG,
+		releaseVersion: RELEASE_VERSION,
+	} );
+	const e2eElementBlock = createElementBlock( {
+		testType: 'E2E',
+		result: E2E_RESULT,
+		envSlug: ENV_SLUG,
+		releaseVersion: RELEASE_VERSION,
+	} );
 
 	contextBlock.elements.push( apiElementBlock );
 	contextBlock.elements.push( e2eElementBlock );
--- .github/actions/tests/slack-summary-on-release/slack-blocks/scripts/create-result-block/utils/create-element-block.js
+++ .github/actions/tests/slack-summary-on-release/slack-blocks/scripts/create-result-block/utils/create-element-block.js
@@ -1,11 +1,17 @@
-const createElementBlock = ( testType, result ) => {
+const createElementBlock = ( {
+	testType,
+	result,
+	envSlug,
+	releaseVersion,
+} ) => {
 	const { selectEmoji } = require( './select-emoji' );
-
+	const allureReportURL = `https://woocommerce.github.io/woocommerce-test-reports/release/${ releaseVersion }/${ envSlug }/${ testType.toLowerCase() }`;
 	const emoji = selectEmoji( result );
+	const textValue = `<${ allureReportURL }|${ testType.toUpperCase() } ${ emoji }>`;
 
 	return {
 		type: 'mrkdwn',
-		text: `${ testType.toUpperCase() } ${ emoji }`,
+		text: textValue,
 	};
 };
 
