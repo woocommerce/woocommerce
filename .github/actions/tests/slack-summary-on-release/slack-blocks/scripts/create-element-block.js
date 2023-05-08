3a340cc3eaffa85d86774826a75b305c252fac69
rodelgc <rodel.calasagsag@automattic.com>
2023-05-08 12:33:54 +0800

    Further modularize

--- .github/actions/tests/slack-summary-on-release/slack-blocks/scripts/create-element-block.js
+++ .github/actions/tests/slack-summary-on-release/slack-blocks/scripts/create-element-block.js
@@ -1,26 +1,5 @@
 const createElementBlock = ( testType, result ) => {
-	const selectEmoji = ( result ) => {
-		const emojis = {
-			PASSED: ':workflow-passed:',
-			FAILED: ':workflow-failed:',
-			SKIPPED: ':workflow-skipped:',
-			CANCELLED: ':workflow-cancelled:',
-			UNKNOWN: ':grey_question:',
-		};
-
-		switch ( result ) {
-			case 'success':
-				return emojis.PASSED;
-			case 'failure':
-				return emojis.FAILED;
-			case 'skipped':
-				return emojis.SKIPPED;
-			case 'cancelled':
-				return emojis.CANCELLED;
-			default:
-				return emojis.UNKNOWN;
-		}
-	};
+	const { selectEmoji } = require( './select-emoji' );
 
 	const emoji = selectEmoji( result );
 
