069a1203ef4c1b7c9aade03ee955be7fed95c51f
Paul Sealock <psealock@gmail.com>
2023-05-10 15:39:06 +1200

    use try/catch instead of .catch()

--- tools/monorepo-utils/src/code-freeze/commands/changelog/lib/index.ts
+++ tools/monorepo-utils/src/code-freeze/commands/changelog/lib/index.ts
@@ -44,71 +44,45 @@ export const updateReleaseBranchChangelogs = async (
 
 	const branch = `update/${ version }-changelog`;
 
-	await git
-		.checkout( {
+	try {
+		await git.checkout( {
 			'-b': null,
 			[ branch ]: null,
-		} )
-		.catch( ( e ) => {
-			Logger.error( e );
 		} );
 
-	Logger.notice( `Running the changelog script in ${ tmpRepoPath }` );
-	execSync(
-		`pnpm --filter=woocommerce run changelog write --add-pr-num -n -vvv --use-version ${ version }`,
-		{
+		Logger.notice( `Running the changelog script in ${ tmpRepoPath }` );
+		execSync(
+			`pnpm --filter=woocommerce run changelog write --add-pr-num -n -vvv --use-version ${ version }`,
+			{
+				cwd: tmpRepoPath,
+				stdio: 'inherit',
+			}
+		);
+		Logger.notice( `Committing deleted files in ${ tmpRepoPath }` );
+		//Checkout pnpm-lock.yaml to prevent issues in case of an out of date lockfile.
+		await git.checkout( 'pnpm-lock.yaml' );
+		await git.add( 'plugins/woocommerce/changelog/' );
+		await git.commit( `Delete changelog files from ${ version } release` );
+		const deletionCommitHash = await git.raw( [ 'rev-parse', 'HEAD' ] );
+		Logger.notice( `git deletion hash: ${ deletionCommitHash }` );
+
+		Logger.notice( `Updating readme.txt in ${ tmpRepoPath }` );
+		execSync( 'php .github/workflows/scripts/release-changelog.php', {
 			cwd: tmpRepoPath,
 			stdio: 'inherit',
-		}
-	);
-
-	//Checkout pnpm-lock.yaml to prevent issues in case of an out of date lockfile.
-	await git.checkout( 'pnpm-lock.yaml' ).catch( ( e ) => {
-		Logger.error( e );
-	} );
-
-	Logger.notice( `Committing deleted files in ${ tmpRepoPath }` );
-
-	await git.add( 'plugins/woocommerce/changelog/' ).catch( ( e ) => {
-		Logger.error( e );
-	} );
-	await git
-		.commit( `Delete changelog files from ${ version } release` )
-		.catch( ( e ) => {
-			Logger.error( e );
 		} );
 
-	// Can use git.revparse() here I think.
-	const deletionCommitHash = await git.raw( [ 'rev-parse', 'HEAD' ] );
-	Logger.notice( `git deletion hash: ${ deletionCommitHash }` );
+		Logger.notice(
+			`Committing readme.txt changes in ${ branch } on ${ tmpRepoPath }`
+		);
+		await git.add( 'plugins/woocommerce/readme.txt' );
+		await git.commit(
+			`Update the readme files for the ${ version } release`
+		);
+		await git.push( 'origin', branch );
+		await git.checkout( '.' );
 
-	// Do readme.txt changes
-	Logger.notice( `Updating readme.txt in ${ tmpRepoPath }` );
-	execSync( 'php .github/workflows/scripts/release-changelog.php', {
-		cwd: tmpRepoPath,
-		stdio: 'inherit',
-	} );
-
-	Logger.notice(
-		`Committing readme.txt changes in ${ branch } on ${ tmpRepoPath }`
-	);
-	await git.add( 'plugins/woocommerce/readme.txt' ).catch( ( e ) => {
-		Logger.error( e );
-	} );
-	await git
-		.commit( `Update the readme files for the ${ version } release` )
-		.catch( ( e ) => {
-			Logger.error( e );
-		} );
-	await git.push( 'origin', branch ).catch( ( e ) => {
-		Logger.error( e );
-	} );
-	await git.checkout( '.' ).catch( ( e ) => {
-		Logger.error( e );
-	} );
-
-	Logger.notice( `Creating PR for ${ branch }` );
-	try {
+		Logger.notice( `Creating PR for ${ branch }` );
 		const pullRequest = await createPullRequest( {
 			owner,
 			name,
@@ -150,28 +124,20 @@ export const updateTrunkChangelog = async (
 		baseDir: tmpRepoPath,
 		config: [ 'core.hooksPath=/dev/null' ],
 	} );
-	await git.checkout( 'trunk' ).catch( ( e ) => {
-		Logger.error( e );
-	} );
-	const branch = `delete/${ version }-changelog`;
-	Logger.notice( `Committing deletions in ${ branch } on ${ tmpRepoPath }` );
-	await git
-		.checkout( {
+
+	try {
+		await git.checkout( 'trunk' );
+		const branch = `delete/${ version }-changelog`;
+		Logger.notice(
+			`Committing deletions in ${ branch } on ${ tmpRepoPath }`
+		);
+		await git.checkout( {
 			'-b': null,
 			[ branch ]: null,
-		} )
-		.catch( ( e ) => {
-			Logger.error( e );
 		} );
-	await git.raw( [ 'cherry-pick', deletionCommitHash ] ).catch( ( e ) => {
-		Logger.error( e );
-	} );
-	await git.push( 'origin', branch ).catch( ( e ) => {
-		Logger.error( e );
-	} );
-
-	Logger.notice( `Creating PR for ${ branch }` );
-	try {
+		await git.raw( [ 'cherry-pick', deletionCommitHash ] );
+		await git.push( 'origin', branch );
+		Logger.notice( `Creating PR for ${ branch }` );
 		const pullRequest = await createPullRequest( {
 			owner,
 			name,
