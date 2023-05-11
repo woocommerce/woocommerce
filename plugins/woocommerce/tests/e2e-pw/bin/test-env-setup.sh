52f8cd0c15ac09ce0df23aeaede4493ec3e87c04
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-05-11 10:27:07 -0700

    Reverted Quote Removal
    There is an upstream bug relating to quotes that needs to be resolved!

--- plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
+++ plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
@@ -5,33 +5,33 @@ ENABLE_NEW_PRODUCT_EDITOR="${ENABLE_NEW_PRODUCT_EDITOR:-0}"
 ENABLE_TRACKING="${ENABLE_TRACKING:-0}"
 
 echo -e 'Activate twentynineteen theme \n'
-wp-env run tests-cli wp theme activate twentynineteen
+wp-env run tests-cli "wp theme activate twentynineteen"
 
 echo -e 'Update URL structure \n'
-wp-env run tests-cli wp rewrite structure \'/%postname%/\' --hard
+wp-env run tests-cli "wp rewrite structure '/%postname%/' --hard"
 
 echo -e 'Add Customer user \n'
-wp-env run tests-cli wp user create customer customer@woocommercecoree2etestsuite.com \
+wp-env run tests-cli "wp user create customer customer@woocommercecoree2etestsuite.com \
 	--user_pass=password \
 	--role=subscriber \
-	--first_name=\'Jane\' \
-	--last_name=\'Smith\' \
-	--user_registered=\'2022-01-01 12:23:45\'
+	--first_name='Jane' \
+	--last_name='Smith' \
+	--user_registered='2022-01-01 12:23:45'"
 
 echo -e 'Update Blog Name \n'
-wp-env run tests-cli wp option update blogname \'WooCommerce Core E2E Test Suite\'
+wp-env run tests-cli "wp option update blogname 'WooCommerce Core E2E Test Suite'"
 
 if [ $ENABLE_HPOS == 1 ]; then
 	echo 'Enable the COT feature'
-	wp-env run tests-cli wp plugin install https://gist.github.com/vedanshujain/564afec8f5e9235a1257994ed39b1449/archive/b031465052fc3e04b17624acbeeb2569ef4d5301.zip --activate
+	wp-env run tests-cli "wp plugin install https://gist.github.com/vedanshujain/564afec8f5e9235a1257994ed39b1449/archive/b031465052fc3e04b17624acbeeb2569ef4d5301.zip --activate"
 fi
 
 if [ $ENABLE_NEW_PRODUCT_EDITOR == 1 ]; then
 	echo 'Enable the new product editor feature'
-	wp-env run tests-cli wp plugin install https://github.com/woocommerce/woocommerce-experimental-enable-new-product-editor/releases/download/0.1.0/woocommerce-experimental-enable-new-product-editor.zip --activate
+	wp-env run tests-cli "wp plugin install https://github.com/woocommerce/woocommerce-experimental-enable-new-product-editor/releases/download/0.1.0/woocommerce-experimental-enable-new-product-editor.zip --activate"
 fi
 
 if [ $ENABLE_TRACKING == 1 ]; then
 	echo 'Enable tracking'
-	wp-env run tests-cli wp option update woocommerce_allow_tracking \'yes\'
+	wp-env run tests-cli "wp option update woocommerce_allow_tracking 'yes'"
 fi
