72be8c3b1bdfe7227d6ded129623b3705e76fe69
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-05-12 11:17:45 +0200

    Add Product Details: product listing pattern (https://github.com/woocommerce/woocommerce-blocks/pull/9383)
    * Add the skeleton for the Product Details: product listing pattern
    
    * Update the wp-block-woocommerce-single-product class.
    
    * Update the product details pattern for usage with core blocks.
    
    * Update margins for the product reviews.
    
    * Update the styles and spacing for ratings.
    
    * Additional changes to the editor preview for product ratings.
    
    * update spacing between ratings and text.
    
    * Update content padding.
    
    * Adjust the pricing
    
    * Make adjustments to the button
    
    * Update styles.
    
    * Update styles for the star ratings.
    
    * Extra fine-tunning for reviews and font size
    
    * Add the product details image.

---
+++ plugins/woocommerce-blocks/patterns/product-details-listing.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Title: Product Details: product listing
+ * Slug: woocommerce-blocks/product-details-listing
+ * Categories: WooCommerce
+ */
+?>
+<!-- wp:columns {"style":{"spacing":{"padding":"0px","blockGap":{"top":"0","left":"0"}}}} -->
+<div class="wp-block-columns" style="padding:0">
+	<!-- wp:column {"style":{"spacing":{"blockGap":"0","padding":"0px"}}} -->
+	<div class="wp-block-column" style="padding:0">
+		<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+		<figure class="wp-block-image size-full">
+			<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/product-details-product-listing.jpg', dirname( __FILE__ ) ) ); ?>" alt="" />
+		</figure>
+		<!-- /wp:image -->
+		<!-- wp:group {"style":{"spacing":{"padding":"0px","blockGap":"0px","margin":{"top":"30px","bottom":"20px","left":"31%"}}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center"}} -->
+		<div class="wp-block-group is-content-justification-center" style="margin-top:30px;margin-bottom:20px;margin-left:31%;padding:0">
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"24px","textColor":"#fda700"}} -->
+			<p class="has-text-color" style="color:#fda700;font-size:24px">★★★★</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"24px"},"textColor":"#ffe8a4","spacing":{"margin":{"right":"8px"}}} -->
+			<p class="has-text-color" style="color:#ffe8a4;margin-right:8px;font-size:24px">★</p>
+			<!-- /wp:paragraph -->
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"13px"},"textColor":"foreground"} -->
+			<p class="has-foreground-color has-text-color" style="font-size:13px"><strong>4.2 </strong>(1,079 reviews)</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"20px"}},"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"700","lineHeight":"120%"}},"textColor":"black","fontSize":"x-large"} -->
+		<h2 class="wp-block-heading has-black-color has-text-color has-text-align-center has-x-large-font-size" style="font-size:48px;font-style:normal;font-weight:700;line-height:120%;margin-bottom:20px;">Bella Pro Series - 1.6-qt. Deep Fryer - Stainless Steel</h2>
+		<!-- /wp:heading -->
+		<!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"40px"}},"padding":"0px","blockGap":"0px","margin":{"bottom":"20px"}},"typography":{"fontSize":"28px","fontWeight":"700"}},"textColor":"foreground"} -->
+		<p class="has-foreground-color has-text-color has-text-align-center" style="font-size:28px;margin-bottom:40px;">
+			<strong><sup><sub>$</sub></sup>37.49 </strong><s style="color:lightgrey;font-weight:400;">$47.49</s>
+		</p>
+		<!-- /wp:paragraph -->
+		<!-- wp:buttons {"style":{"spacing":{"blockGap":"0px"}}} -->
+		<div class="wp-block-buttons is-content-justification-center">
+			<!-- wp:button {"backgroundColor":"black","textColor":"white","className":"is-style-fill","fontSize":"medium","style":{"spacing":{"padding":{"left":"94px","right":"94px","top":"20px","bottom":"20px"}}}} -->
+			<div class="wp-block-button has-custom-font-size is-style-fill has-medium-font-size"><a class="wp-block-button__link has-white-color has-black-background-color has-text-color has-background wp-element-button" style="padding-right:94px;padding-left:94px;padding-top:20px;padding-bottom:20px;"><strong>Add to cart</strong></a></div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+	<!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
