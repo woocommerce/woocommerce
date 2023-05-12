522fe5d93d58637f2a0597cb17cec647b92909fd
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-05-12 09:54:26 -0700

    Fixed WP Version Parsing

--- .github/workflows/scripts/parse-wp-version.js
+++ .github/workflows/scripts/parse-wp-version.js
@@ -1,4 +1,6 @@
-module.exports = function parseWPVersion( wpVersion ) {
+const https = require( 'http' );
+
+module.exports = async function parseWPVersion( wpVersion ) {
     	// Start with versions we can infer immediately.
 	switch ( wpVersion ) {
         case 'master':
@@ -15,22 +17,63 @@ module.exports = function parseWPVersion( wpVersion ) {
 		}
 	}
 
-	// Set the correct branch based on the version request being made.
-	const matches = wpVersion.match( /([0-9]+)\.([0-9]+)(?:\.([0-9]+))?/ );
-	if ( ! matches ) {
-		throw new Error( `Invalid 'wp-version': ${ wpVersion } must be 'trunk', 'nightly', 'latest', 'X.X', or 'X.X.X'.` );
-	}
-
-	// They've requested a very specific version and we should respect that.
-	if ( matches[ 3 ] !== undefined ) {
-		// .0 versions are tagged without the patch.
-		if ( matches[ 3 ] === '0' ) {
-			return `WordPress/WordPress#tags/${ matches[ 1 ] }.${ matches[ 2 ] }.${ matches[ 3 ] }`;
+	return new Promise( ( resolve, reject ) => {
+		// We're going to download the correct zip archive based on the version they're requesting.
+		const parsedVersion = wpVersion.match( /([0-9]+)\.([0-9]+)(?:\.([0-9]+))?/ );
+		if ( ! parsedVersion ) {
+			throw new Error( `Invalid 'wp-version': ${ wpVersion } must be 'trunk', 'nightly', 'latest', 'X.X', or 'X.X.X'.` );
 		}
 
-		return `WordPress/WordPress#tags/${ matches[ 1 ] }.${ matches[ 2 ] }.${ matches[ 3 ] }`;
-	}
+		// When they've provided a specific version we can just provide that.
+		if ( parsedVersion[ 3 ] !== undefined ) {
+			let zipVersion = `${ parsedVersion[ 1 ] }.${ parsedVersion[ 2 ] }`;
+			// .0 versions do not have a patch.
+			if ( parsedVersion[ 3 ] !== '0' ) {
+				zipVersion += `.${ parsedVersion[ 3 ] }`;
+			}
 
-	// We can give them the batest release from a given development branch.
-	return `WordPress/WordPress#${ matches[ 1 ] }.${ matches[ 2 ] }-branch`;
+			resolve( `https://wordpress.org/wordpress-${ zipVersion }.zip` );
+		}
+
+		const request = https.get(
+			'http://api.wordpress.org/core/stable-check/1.0/',
+			( response ) => {
+				// Listen for the response data.
+				let data = '';
+				response.on('data', (chunk) => {
+					data += chunk;
+				});
+
+				// Once we have the entire response we can process it.
+				response.on('end', () => {
+					// Parse the response and find the latest version of every minor release.
+					const latestVersions = {};
+					const rawVersions = JSON.parse( data );
+					for ( const v in rawVersions ) {
+						// Parse the version so we can find the latest.
+						const matches = v.match( /([0-9]+)\.([0-9]+)(?:\.([0-9]+))?/ );
+						const minor = `${ matches[1] }.${ matches[2] }`;
+						const patch = matches[ 3 ] === undefined ? 0 : parseInt( matches[ 3 ] );
+
+						// We will only be keeping the latest release of each minor.
+						if ( latestVersions[ minor ] === undefined || patch > latestVersions[ minor ] ) {
+							latestVersions[ minor ] = patch;
+						}
+					}
+
+					let zipVersion = `${ parsedVersion[ 1 ] }.${ parsedVersion[ 2 ] }`;
+					// .0 versions do not have a patch.
+					if ( latestVersions[ zipVersion ] !== 0 ) {
+						zipVersion += `.${ latestVersions[ zipVersion ]}`;
+					}
+
+					resolve( `https://wordpress.org/wordpress-${ zipVersion }.zip` );
+				});
+			},
+		);
+
+		request.on( 'error', ( error ) => {
+			reject( error );
+		} );
+	} );
 }
