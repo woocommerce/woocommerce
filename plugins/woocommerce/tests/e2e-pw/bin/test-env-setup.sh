18744c327ecf1d17f9a0924dfe0939f543e98e38
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-05-12 08:43:40 -0700

    Added Test File Copying
    One of the unit tests requires a file outside of the web root, so,
    we need to move it in the setup script using `sudo` privileges.

--- plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
+++ plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
@@ -21,17 +21,21 @@ wp-env run tests-cli "wp user create customer customer@woocommercecoree2etestsui
 echo -e 'Update Blog Name \n'
 wp-env run tests-cli "wp option update blogname 'WooCommerce Core E2E Test Suite'"
 
+echo -e 'Preparing Test Files \n'
+wp-env run tests-cli "sudo cp /var/www/html/wp-content/plugins/woocommerce/tests/legacy/unit-tests/importer/sample.csv \
+	/var/www/sample.csv"
+
 if [ $ENABLE_HPOS == 1 ]; then
-	echo 'Enable the COT feature'
+	echo -e 'Enable the COT feature\n'
 	wp-env run tests-cli "wp plugin install https://gist.github.com/vedanshujain/564afec8f5e9235a1257994ed39b1449/archive/b031465052fc3e04b17624acbeeb2569ef4d5301.zip --activate"
 fi
 
 if [ $ENABLE_NEW_PRODUCT_EDITOR == 1 ]; then
-	echo 'Enable the new product editor feature'
+	echo -e 'Enable the new product editor feature\n'
 	wp-env run tests-cli "wp plugin install https://github.com/woocommerce/woocommerce-experimental-enable-new-product-editor/releases/download/0.1.0/woocommerce-experimental-enable-new-product-editor.zip --activate"
 fi
 
 if [ $ENABLE_TRACKING == 1 ]; then
-	echo 'Enable tracking'
+	echo -e 'Enable tracking\n'
 	wp-env run tests-cli "wp option update woocommerce_allow_tracking 'yes'"
 fi
