f1b1df93d1bcb969008efb537e7a5efcb3fbecd4
“Chris <xristos3490@gmail.com>
2023-05-15 14:25:18 +0300

    Bump order-again template

--- plugins/woocommerce/templates/order/order-again.php
+++ plugins/woocommerce/templates/order/order-again.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version x.x.x
+ * @version 7.8.0
  */
 
 defined( 'ABSPATH' ) || exit;
