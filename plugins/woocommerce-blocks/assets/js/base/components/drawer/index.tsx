4c497e44d32fd7cab91f837ada1bc02f8c1d84d9
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-05-16 10:41:26 +0200

    Mini Cart: stop using Modal component (https://github.com/woocommerce/woocommerce-blocks/pull/9345)
    * Remove unused styles
    
    * Replace usage of Modal component with custom Drawer
    
    * Update MiniCart.php class structure
    
    * Update tests
    
    * Prevent focus styles to appear unnecessarily when opening the Mini Cart drawer
    
    * Work-around issue with disabled buttons causing lose of focus inside the Mini-Cart drawer
    
    * Revert "Work-around issue with disabled buttons causing lose of focus inside the Mini-Cart drawer"
    
    This reverts commit 4360f77384ad1f1d90a3ba8a0385ad79da2449f2.
    
    * QuantitySelector: focus text input field after decrease or increase button become disabled
    
    * Move focus to the input field also when the body has focus, that fixes the issue in Chrome
    
    * Add explanatory comment

--- plugins/woocommerce-blocks/assets/js/base/components/drawer/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/drawer/index.tsx
@@ -1,14 +1,35 @@
 /**
+ * Some code of the Drawer component is based on the Modal component from Gutenberg:
+ * https://github.com/WordPress/gutenberg/blob/trunk/packages/components/src/modal/index.tsx
+ */
+/**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { Modal } from 'wordpress-components';
-import { useDebounce } from 'use-debounce';
 import classNames from 'classnames';
+import { useDebounce } from 'use-debounce';
+import type { ForwardedRef, KeyboardEvent } from 'react';
+import { __ } from '@wordpress/i18n';
+import {
+	createPortal,
+	useEffect,
+	useRef,
+	forwardRef,
+} from '@wordpress/element';
+import { close } from '@wordpress/icons';
+import {
+	useFocusReturn,
+	useFocusOnMount,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalUseFocusOutside as useFocusOutside,
+	useConstrainedTabbing,
+	useMergeRefs,
+} from '@wordpress/compose';
 
 /**
  * Internal dependencies
  */
+import Button from '../button';
+import * as ariaHelper from './utils/aria-helper';
 import './style.scss';
 
 interface DrawerProps {
@@ -18,32 +39,77 @@ interface DrawerProps {
 	onClose: () => void;
 	slideIn?: boolean;
 	slideOut?: boolean;
-	title: string;
 }
 
-const Drawer = ( {
-	children,
-	className,
-	isOpen,
-	onClose,
-	slideIn = true,
-	slideOut = true,
-	title,
-}: DrawerProps ): JSX.Element | null => {
+const UnforwardedDrawer = (
+	{
+		children,
+		className,
+		isOpen,
+		onClose,
+		slideIn = true,
+		slideOut = true,
+	}: DrawerProps,
+	forwardedRef: ForwardedRef< HTMLDivElement >
+): JSX.Element | null => {
 	const [ debouncedIsOpen ] = useDebounce< boolean >( isOpen, 300 );
 	const isClosing = ! isOpen && debouncedIsOpen;
+	const bodyOpenClassName = 'drawer-open';
+
+	const onRequestClose = () => {
+		document.body.classList.remove( bodyOpenClassName );
+		ariaHelper.showApp();
+		onClose();
+	};
+
+	const ref = useRef< HTMLDivElement >();
+	const focusOnMountRef = useFocusOnMount();
+	const constrainedTabbingRef = useConstrainedTabbing();
+	const focusReturnRef = useFocusReturn();
+	const focusOutsideProps = useFocusOutside( onRequestClose );
+	const contentRef = useRef< HTMLDivElement >( null );
+
+	useEffect( () => {
+		if ( isOpen ) {
+			ariaHelper.hideApp( ref.current );
+			document.body.classList.add( bodyOpenClassName );
+		}
+	}, [ isOpen, bodyOpenClassName ] );
+
+	const overlayRef = useMergeRefs( [ ref, forwardedRef ] );
+	const drawerRef = useMergeRefs( [
+		constrainedTabbingRef,
+		focusReturnRef,
+		focusOnMountRef,
+	] );
 
 	if ( ! isOpen && ! isClosing ) {
 		return null;
 	}
 
-	return (
-		<Modal
-			title={ title }
-			focusOnMount={ true }
-			onRequestClose={ onClose }
-			className={ classNames( className, 'wc-block-components-drawer' ) }
-			overlayClassName={ classNames(
+	function handleEscapeKeyDown( event: KeyboardEvent< HTMLDivElement > ) {
+		if (
+			// Ignore keydowns from IMEs
+			event.nativeEvent.isComposing ||
+			// Workaround for Mac Safari where the final Enter/Backspace of an IME composition
+			// is `isComposing=false`, even though it's technically still part of the composition.
+			// These can only be detected by keyCode.
+			event.keyCode === 229
+		) {
+			return;
+		}
+
+		if ( event.code === 'Escape' && ! event.defaultPrevented ) {
+			event.preventDefault();
+			onRequestClose();
+		}
+	}
+
+	return createPortal(
+		// eslint-disable-next-line jsx-a11y/no-static-element-interactions
+		<div
+			ref={ overlayRef }
+			className={ classNames(
 				'wc-block-components-drawer__screen-overlay',
 				{
 					'wc-block-components-drawer__screen-overlay--is-hidden':
@@ -54,11 +120,38 @@ const Drawer = ( {
 						slideOut,
 				}
 			) }
-			closeButtonLabel={ __( 'Close', 'woo-gutenberg-products-block' ) }
+			onKeyDown={ handleEscapeKeyDown }
 		>
-			{ children }
-		</Modal>
+			<div
+				className={ classNames(
+					className,
+					'wc-block-components-drawer'
+				) }
+				ref={ drawerRef }
+				role="dialog"
+				tabIndex={ -1 }
+				{ ...focusOutsideProps }
+			>
+				<div
+					className="wc-block-components-drawer__content"
+					role="document"
+					ref={ contentRef }
+				>
+					<Button
+						className="wc-block-components-drawer__close"
+						onClick={ onRequestClose }
+						icon={ close }
+						label={ __( 'Close', 'woo-gutenberg-products-block' ) }
+						showTooltip={ false }
+					/>
+					{ children }
+				</div>
+			</div>
+		</div>,
+		document.body
 	);
 };
+
+export const Drawer = forwardRef( UnforwardedDrawer );
 
 export default Drawer;
