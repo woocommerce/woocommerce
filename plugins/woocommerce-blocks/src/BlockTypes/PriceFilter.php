84d9c9497a55816b45a69e4eef7e09228faafb36
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-05-16 12:05:24 +0200

    Fix Filter by Price block making a 404 request in the frontend (https://github.com/woocommerce/woocommerce-blocks/pull/9481)

--- plugins/woocommerce-blocks/src/BlockTypes/PriceFilter.php
+++ plugins/woocommerce-blocks/src/BlockTypes/PriceFilter.php
@@ -14,4 +14,13 @@ class PriceFilter extends AbstractBlock {
 	protected $block_name     = 'price-filter';
 	const MIN_PRICE_QUERY_VAR = 'min_price';
 	const MAX_PRICE_QUERY_VAR = 'max_price';
+
+	/**
+	 * Get the frontend script handle for this block type.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 */
+	protected function get_block_type_script( $key = null ) {
+		return null;
+	}
 }
