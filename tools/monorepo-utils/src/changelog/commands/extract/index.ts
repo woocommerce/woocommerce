6cf769fe39df03da1eac32a8107925cd2fb842c6
paul sealock <psealock@gmail.com>
2023-05-17 13:01:47 +1200

    regexes

--- tools/monorepo-utils/src/changelog/commands/extract/index.ts
+++ tools/monorepo-utils/src/changelog/commands/extract/index.ts
@@ -7,7 +7,14 @@ import { Command } from '@commander-js/extra-typings';
  * Internal dependencies
  */
 import { Logger } from '../../../core/logger';
-import { getPullRequestData } from './lib';
+import {
+	getPullRequestData,
+	getChangelogSignificance,
+	getShouldAutomateChangelog,
+	getChangelogType,
+	getChangelogMessage,
+	getChangelogComment,
+} from './lib';
 
 export const extractCommand = new Command( 'extract' )
 	.option(
@@ -30,17 +37,35 @@ export const extractCommand = new Command( 'extract' )
 			}
 		) => {
 			const { owner, name } = options;
-			// const project = 'woocommerce';
-			// const message = `Add test changelog for PR #${ prNumber }. This is generated using an action`;
-			// const significance = 'patch';
-			// const type = 'fix';
 			const { prData, isCommunityPR, prOwner, branch, fileName } =
 				await getPullRequestData( { owner, name }, prNumber );
+
+			const shouldAutomateChangelog = getShouldAutomateChangelog(
+				prData.body
+			);
+
+			if ( ! shouldAutomateChangelog ) {
+				Logger.notice(
+					`PR #${ prNumber } does not have the "Automatically create a changelog entry from the details" checkbox checked. No changelog will be created.`
+				);
+				process.exit( 0 );
+			}
+
 			Logger.notice(
 				`PR #${ prNumber } ${
 					isCommunityPR ? 'is' : 'is not'
 				} a community PR. Making a clone of ${ prOwner }/${ name } and adding a changelog to branch ${ branch } in a file called ${ fileName }`
 			);
+
+			const significance = getChangelogSignificance( prData.body );
+			const type = getChangelogType( prData.body );
+			const message = getChangelogMessage( prData.body );
+			const comment = getChangelogComment( prData.body );
+
+			Logger.notice( significance );
+			Logger.notice( type );
+			Logger.notice( message );
+			Logger.notice( comment );
 			// const { body } = prData;
 			// Logger.notice( JSON.stringify( body, null, 2 ) );
 		}
