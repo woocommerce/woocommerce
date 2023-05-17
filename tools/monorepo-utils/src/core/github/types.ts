85d9755bc8195749609ab8a46096c21a0c600d7d
paul sealock <psealock@gmail.com>
2023-05-17 13:44:01 +1200

    TS repo functions

--- tools/monorepo-utils/src/core/github/types.ts
+++ tools/monorepo-utils/src/core/github/types.ts
@@ -3,5 +3,8 @@
  */
 import { Endpoints } from '@octokit/types';
 
-export type PullRequestEndpointResponse =
+export type CreatePullRequestEndpointResponse =
 	Endpoints[ 'POST /repos/{owner}/{repo}/pulls' ][ 'response' ];
+
+export type PullRequestEndpointResponse =
+	Endpoints[ 'GET /repos/{owner}/{repo}/pulls/{pull_number}' ][ 'response' ];
