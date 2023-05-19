1c327995ec932cdf692896398e30c407f97dfbab
rodelgc <rodel.calasagsag@automattic.com>
2023-05-19 17:03:04 +0800

    Add documentation

--- .github/actions/tests/reports/publish-report-pr/scripts/set-upload-path.js
+++ .github/actions/tests/reports/publish-report-pr/scripts/set-upload-path.js
@@ -1,3 +1,12 @@
+/**
+ * Set upload path based on feature flag, test type.
+ *
+ * @param {*} contexts GitHub contexts passed by caller action
+ * @param {string} reportsHome Bucket URL
+ * @param {string} featSlug Slug to separate reports by feature flag
+ * @param {string} testType e2e, api, or k6
+ * @returns {string} Full upload path
+ */
 module.exports = ( { github }, reportsHome, featSlug, testType ) => {
 	const prNumber = github.event.pull_request.number;
 	const prPath = `${ reportsHome }/public/pr/${ prNumber }`;
