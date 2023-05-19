ebf0b929603ccfb47387574e742d82e7c08f283a
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-05-19 12:53:55 -0400

    Use feature the new feature flags engine to guard the access to the new product blocks experience (#37122)
    Unregister the product-block-editor feature flag from WC Admin Test Helper, and enable product block editor feature.

--- plugins/woocommerce/src/Internal/Admin/Settings.php
+++ plugins/woocommerce/src/Internal/Admin/Settings.php
@@ -9,6 +9,7 @@ use Automattic\WooCommerce\Admin\API\Plugins;
 use Automattic\WooCommerce\Admin\PageController;
 use Automattic\WooCommerce\Admin\API\Reports\Orders\DataStore as OrdersDataStore;
 use Automattic\WooCommerce\Admin\PluginsHelper;
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
 use WC_Marketplace_Suggestions;
 
 /**
@@ -233,7 +234,28 @@ class Settings {
 		$settings['connectNonce']                     = wp_create_nonce( 'connect' );
 		$settings['wcpay_welcome_page_connect_nonce'] = wp_create_nonce( 'wcpay-connect' );
 
+		$settings['features'] = $this->get_features();
+
 		return $settings;
+	}
+
+	/**
+	 * Removes non necesary feature properties for the client side.
+	 *
+	 * @return array
+	 */
+	public function get_features() {
+		$features     = FeaturesUtil::get_features( true, true );
+		$new_features = array();
+
+		foreach ( array_keys( $features ) as $feature_id ) {
+			$new_features[ $feature_id ] = array(
+				'is_enabled'      => $features[ $feature_id ]['is_enabled'],
+				'is_experimental' => $features[ $feature_id ]['is_experimental'],
+			);
+		}
+
+		return $new_features;
 	}
 
 	/**
