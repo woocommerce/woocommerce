82d9106db9090db5f8eff477083aaa6773891444
rodelgc <rodel.calasagsag@automattic.com>
2023-05-19 17:36:55 +0800

    Directly pass PR number

--- .github/actions/tests/reports/publish-report-pr/scripts/set-upload-path.js
+++ .github/actions/tests/reports/publish-report-pr/scripts/set-upload-path.js
@@ -1,18 +1,14 @@
 /**
  * Set upload path based on feature flag, test type.
  *
- * @param {*} contexts GitHub contexts passed by caller action
- * @param {string} reportsHome Bucket URL
- * @param {string} featSlug Slug to separate reports by feature flag
- * @param {string} testType e2e, api, or k6
  * @returns {string} Full upload path
  */
-module.exports = ( { github }, reportsHome, featSlug, testType ) => {
-	const prNumber = github.event.pull_request.number;
-	const prPath = `${ reportsHome }/public/pr/${ prNumber }`;
-	const testTypePath = featSlug
-		? `${ featSlug }/${ testType }`
-		: `${ testType }`;
+module.exports = () => {
+	const { PR_NUMBER, REPORTS_HOME, FEAT_SLUG, TEST_TYPE } = process.env;
+	const prPath = `${ REPORTS_HOME }/public/pr/${ PR_NUMBER }`;
+	const testTypePath = FEAT_SLUG
+		? `${ FEAT_SLUG }/${ TEST_TYPE }`
+		: `${ TEST_TYPE }`;
 	const uploadPath = `${ prPath }/${ testTypePath }`;
 
 	return uploadPath;
