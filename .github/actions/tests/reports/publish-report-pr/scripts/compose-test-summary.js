f422a8a9e3c5be4e356ab32bab29f4576d690863
Rodel Calasagsag <rodel.calasagsag@automattic.com>
2023-05-22 05:10:27 +0800

    Use markdown template

--- .github/actions/tests/reports/publish-report-pr/scripts/compose-test-summary.js
+++ .github/actions/tests/reports/publish-report-pr/scripts/compose-test-summary.js
@@ -23,41 +23,61 @@ const formatDuration = ( duration ) => {
 	return `${ durationMinutes }m ${ durationSeconds }s`;
 };
 
-const parseRow = async ( { github, content, stats, rowTitle, reportUrl } ) => {
-	const duration = formatDuration( stats.duration );
-
-	return content.replace( '$API_TITLE', rowTitle ).replace( '' );
-};
-
-const getCommitMessage = async () => {
+const insertCommitDetails = async ( github, content ) => {
 	const requestURL = `https://api.github.com/repos/woocommerce/woocommerce/commits/${ SHA }`;
-
 	const response = await github.request( requestURL );
 	const message = response.data.commit.message;
+
+	return content
+		.replace( '$COMMIT_SHA', SHA )
+		.replace( '$COMMIT_MESSAGE', message );
+};
+
+const insertRow = ( content, rowTitle, reportURL, stats ) => {
+	const placeHolder = '\n<!-- EOT -->';
+	const { passed, failed, broken, skipped, unknown, total, duration } = stats;
+	const fmtDuration = formatDuration( duration );
+	const testLink = `[${ rowTitle }](${ reportURL })`;
+	const cells = [
+		testLink,
+		passed.toString(),
+		failed.toString(),
+		broken.toString(),
+		skipped.toString(),
+		unknown.toString(),
+		total.toString(),
+		fmtDuration,
+	];
+	const row = `| ${ cells.join( ' | ' ) } |`;
+
+	return content.replace( placeHolder, row + placeHolder );
 };
 
 module.exports = async ( { github, core } ) => {
 	const fs = require( 'fs' );
-
 	const apiStats = JSON.parse( API_STATS );
 	const e2eStats = JSON.parse( E2E_STATS );
 	const apiHposStats = JSON.parse( API_HPOS_STATS );
 	const e2eHposStats = JSON.parse( E2E_HPOS_STATS );
-
-	const commitMessage = getCommitMessage();
-
 	const content = fs.readFileSync(
 		'./.github/actions/tests/reports/publish-report-pr/scripts/test-summary-template.md'
 	);
 
-	// Parse API row
-	content = await parseRow( {
-		github,
+	content = await insertCommitDetails( github, content );
+	content = insertRow( content, 'API Tests', API_REPORT_URL, apiStats );
+	content = insertRow( content, 'E2E Tests', E2E_REPORT_URL, e2eStats );
+	content = insertRow(
 		content,
-		stats: apiStats,
-		rowTitle: 'API Tests',
-		reportUrl: API_REPORT_URL,
-	} );
+		'API Tests (HPOS enabled)',
+		API_HPOS_REPORT_URL,
+		apiHposStats
+	);
+	content = insertRow(
+		content,
+		'E2E Tests (HPOS enabled)',
+		E2E_HPOS_REPORT_URL,
+		e2eHposStats
+	);
 
-	core.setOutput( 'summary', summary );
+	core.setOutput( 'summary', content );
 };
--- .github/actions/tests/reports/publish-report-pr/scripts/test-summary-template.md
+++ .github/actions/tests/reports/publish-report-pr/scripts/test-summary-template.md
@@ -4,11 +4,9 @@ _Commit:_
 
 > $COMMIT_SHA $COMMIT_MESSAGE
 
-| Test :test_tube:                        | Passed :white_check_mark: | Failed :rotating_light: | Broken :construction: | Skipped :next_track_button: | Unknown :grey_question: | Total :bar_chart: | Duration :stopwatch: |
-| --------------------------------------- | ------------------------- | ----------------------- | --------------------- | --------------------------- | ----------------------- | ----------------- | -------------------- |
-| [$API_TITLE]($API_REPORT_URL)           | $API_PASSED               | $API_FAILED             | $API_BROKEN           | $API_SKIPPED                | $API_UNKNOWN            | $API_TOTAL        | $API_DURATION        |
-| [$E2E_TITLE]($E2E_REPORT_URL)           | $E2E_PASSED               | $E2E_FAILED             | $E2E_BROKEN           | $E2E_SKIPPED                | $E2E_UNKNOWN            | $E2E_TOTAL        | $E2E_DURATION        |
-| [$API_HPOS_TITLE]($API_HPOS_REPORT_URL) | $API_HPOS_PASSED          | $API_HPOS_FAILED        | $API_HPOS_BROKEN      | $API_HPOS_SKIPPED           | $API_HPOS_UNKNOWN       | $API_HPOS_TOTAL   | $API_HPOS_DURATION   |
-| [$E2E_HPOS_TITLE]($E2E_HPOS_REPORT_URL) | $E2E_HPOS_PASSED          | $E2E_HPOS_FAILED        | $E2E_HPOS_BROKEN      | $E2E_HPOS_SKIPPED           | $E2E_HPOS_UNKNOWN       | $E2E_HPOS_TOTAL   | $E2E_HPOS_DURATION   |
+| Test :test_tube: | Passed :white_check_mark: | Failed :rotating_light: | Broken :construction: | Skipped :next_track_button: | Unknown :grey_question: | Total :bar_chart: | Duration :stopwatch: |
+| ---------------- | ------------------------- | ----------------------- | --------------------- | --------------------------- | ----------------------- | ----------------- | -------------------- |
+
+<!-- EOT -->
 
 Visit the [WooCommerce Test Reports Dashboard](https://woocommerce.github.io/woocommerce-test-reports) to view all reports.
