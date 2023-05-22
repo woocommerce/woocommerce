7abb843e934c724f1e0f392aeff68eaa4e3dc8b3
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-05-22 21:41:36 +0200

    Fix > Product details listing pattern - Ensure the editor preview is properly rendered after page refresh (https://github.com/woocommerce/woocommerce-blocks/pull/9563)
    * Fix editor render for a few paragraphs within the Product Listing Pattern.
    
    * Update spacing and fontWeight.

--- plugins/woocommerce-blocks/patterns/product-details-listing.php
+++ plugins/woocommerce-blocks/patterns/product-details-listing.php
@@ -14,27 +14,38 @@
 			<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/product-details-product-listing.jpg', dirname( __FILE__ ) ) ); ?>" alt="" />
 		</figure>
 		<!-- /wp:image -->
-		<!-- wp:group {"style":{"spacing":{"padding":"0px","blockGap":"0px","margin":{"top":"30px","bottom":"20px","left":"31%"}}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center"}} -->
-		<div class="wp-block-group is-content-justification-center" style="margin-top:30px;margin-bottom:20px;margin-left:31%;padding:0">
-			<!-- wp:paragraph {"style":{"typography":{"fontSize":"24px","textColor":"#fda700"}} -->
-			<p class="has-text-color" style="color:#fda700;font-size:24px">★★★★</p>
+		<!-- wp:group {"align":"full","style":{"spacing":{"padding":"0px","blockGap":"0px","margin":{"top":"30px","bottom":"20px","left":"0px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","verticalAlignment":"center"}} -->
+		<div class="wp-block-group alignfull" style="margin-top:30px;margin-bottom:20px;margin-left:0px;padding:0">
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"24px"},"color":{"text":"#fda700"}}} -->
+			<p class="has-text-color" style="color:#fda700;font-size:24px;">★★★★</p>
 			<!-- /wp:paragraph -->
-			<!-- wp:paragraph {"style":{"typography":{"fontSize":"24px"},"textColor":"#ffe8a4","spacing":{"margin":{"right":"8px"}}} -->
-			<p class="has-text-color" style="color:#ffe8a4;margin-right:8px;font-size:24px">★</p>
+
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"24px"},"color":{"text":"#ffe8a4"},"spacing":{"margin":{"right":"8px"}}}} -->
+			<p class="has-text-color" style="color:#ffe8a4;margin-right:8px;font-size:24px;">★</p>
 			<!-- /wp:paragraph -->
-			<!-- wp:paragraph {"style":{"typography":{"fontSize":"13px"},"textColor":"foreground"} -->
-			<p class="has-foreground-color has-text-color" style="font-size:13px"><strong>4.2 </strong>(1,079 reviews)</p>
+
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"13px"},"color":{"text":"#000000"}}} -->
+			<p class="has-text-color" style="color:#000000;font-size:13px;"><strong>4.2 </strong>(1,079 reviews)</p>
 			<!-- /wp:paragraph -->
 		</div>
 		<!-- /wp:group -->
+
 		<!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"20px"}},"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"700","lineHeight":"120%"}},"textColor":"black","fontSize":"x-large"} -->
 		<h2 class="wp-block-heading has-black-color has-text-color has-text-align-center has-x-large-font-size" style="font-size:48px;font-style:normal;font-weight:700;line-height:120%;margin-bottom:20px;">Bella Pro Series - 1.6-qt. Deep Fryer - Stainless Steel</h2>
 		<!-- /wp:heading -->
-		<!-- wp:paragraph {"style":{"spacing":{"margin":{"bottom":"40px"}},"padding":"0px","blockGap":"0px","margin":{"bottom":"20px"}},"typography":{"fontSize":"28px","fontWeight":"700"}},"textColor":"foreground"} -->
-		<p class="has-foreground-color has-text-color has-text-align-center" style="font-size:28px;margin-bottom:40px;">
-			<strong><sup><sub>$</sub></sup>37.49 </strong><s style="color:lightgrey;font-weight:400;">$47.49</s>
-		</p>
-		<!-- /wp:paragraph -->
+
+		<!-- wp:group {"align":"full","style":{"spacing":{"padding":"0px","blockGap":"0px","margin":{"top":"0px","bottom":"40px","left":"0px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center","verticalAlignment":"center"}} -->
+		<div class="wp-block-group alignfull" style="margin-top:0px;margin-bottom:40px;margin-left:0px;padding:0">
+			<!-- wp:paragraph {"style":{"spacing":{"padding":{"right":"5px"}},"typography":{"fontSize":"28px"},"color":{"text":"#000000"}}} -->
+			<p class="has-text-color" style="color:#000000;font-size:28px;padding-right:5px"><strong><sup><sub>$</sub></sup>37.49</strong></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"28px","fontWeight":"400"},"layout":{"selfStretch":"fit","flexSize":null},"color":{"text":"#d3d3d3ff"}}} -->
+			<p class="has-text-color" style="color:#d3d3d3ff;font-size:28px;font-weight:400;"><s>$47.49</s></p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+
 		<!-- wp:buttons {"style":{"spacing":{"blockGap":"0px"}}} -->
 		<div class="wp-block-buttons is-content-justification-center">
 			<!-- wp:button {"backgroundColor":"black","textColor":"white","className":"is-style-fill","fontSize":"medium","style":{"spacing":{"padding":{"left":"94px","right":"94px","top":"20px","bottom":"20px"}}}} -->
