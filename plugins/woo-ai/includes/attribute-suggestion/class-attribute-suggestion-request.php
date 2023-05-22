f704392459e65d6b975bee4de10c8496d91cfb3c
Nima <73110514+nima-karimi@users.noreply.github.com>
2023-05-22 10:21:13 +0100

    Add attribute suggestion service

---
+++ plugins/woo-ai/includes/attribute-suggestion/class-attribute-suggestion-request.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Woo AI Attribute Suggestion Request Class
+ *
+ * @package Woo_AI
+ */
+
+namespace Automattic\WooCommerce\AI\AttributeSuggestion;
+
+use Exception;
+
+defined( 'ABSPATH' ) || exit;
+
+class Attribute_Suggestion_Request {
+	const REQUESTED_ATTRIBUTE_NAME        = 'name';
+	const REQUESTED_ATTRIBUTE_DESCRIPTION = 'description';
+	const REQUESTED_ATTRIBUTE_TAGS        = 'tags';
+	const REQUESTED_ATTRIBUTE_CATEGORIES  = 'categories';
+
+	/**
+	 * Name of the attribute that is being requested (e.g. title, description, tags, etc.).
+	 *
+	 * @var string
+	 */
+	public $requested_attribute;
+
+	/**
+	 * The name of the product.
+	 *
+	 * @var string
+	 */
+	public $name;
+
+	/**
+	 * The description of the product.
+	 *
+	 * @var string
+	 */
+	public $description;
+
+	/**
+	 * The product tags.
+	 *
+	 * @var string[]
+	 */
+	public $tags;
+
+	/**
+	 * Categories of the product as an associative array.
+	 *
+	 * @var string[]
+	 */
+	public $categories;
+
+	/**
+	 * Other attributes of the product as an associative array.
+	 *
+	 * @var array[] Associative array of attributes. Each attribute is an associative array with the following keys:
+	 *              - name: The name of the attribute.
+	 *              - value: The value of the attribute.
+	 */
+	public $attributes;
+
+	/**
+	 * Constructor
+	 *
+	 * @param string   $requested_attribute The attribute that suggestions are being requested for.
+	 * @param string   $name                The name of the product.
+	 * @param string   $description         The description of the product.
+	 * @param string[] $tags                The product tags.
+	 * @param string[] $categories          Categories of the product as an associative array.
+	 * @param array[]  $attributes          Other attributes of the product as an associative array.
+	 */
+	public function __construct( string $requested_attribute, string $name, string $description, array $tags = array(), array $categories = array(), array $attributes = array() ) {
+		$this->validate_requested_attribute( $requested_attribute );
+
+		$this->requested_attribute = $requested_attribute;
+		$this->name                = $name;
+		$this->description         = $description;
+		$this->tags                = $tags;
+		$this->categories          = $categories;
+		$this->attributes          = $attributes;
+	}
+
+	/**
+	 * Validates the requested attribute.
+	 *
+	 * @param string $requested_attribute The attribute that suggestions are being requested for.
+	 *
+	 * @return void
+	 *
+	 * @throws Exception If the requested attribute is invalid.
+	 */
+	protected function validate_requested_attribute( string $requested_attribute ): void {
+		if ( ! in_array(
+			$requested_attribute,
+			array(
+				self::REQUESTED_ATTRIBUTE_NAME,
+				self::REQUESTED_ATTRIBUTE_DESCRIPTION,
+				self::REQUESTED_ATTRIBUTE_TAGS,
+				self::REQUESTED_ATTRIBUTE_CATEGORIES,
+			),
+			true
+		) ) {
+			throw new Exception( 'Invalid requested attribute.' );
+		}
+	}
+}
---
+++ plugins/woo-ai/includes/attribute-suggestion/class-attribute-suggestion-service.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Woo AI Attribute Suggestion Service Class
+ *
+ * @package Woo_AI
+ */
+
+namespace Automattic\WooCommerce\AI\AttributeSuggestion;
+
+use Automattic\WooCommerce\AI\Completion\Completion_Service_Interface;
+use Exception;
+use JsonException;
+
+defined( 'ABSPATH' ) || exit;
+
+class Attribute_Suggestion_Service {
+	/**
+	 * The prompt generator.
+	 *
+	 * @var Attribute_Suggestion_Prompt_Generator
+	 */
+	protected $prompt_generator;
+
+
+	/**
+	 * The completion service.
+	 *
+	 * @var Completion_Service_Interface
+	 */
+	protected $completion_service;
+
+	/**
+	 * Constructor
+	 *
+	 * @param Attribute_Suggestion_Prompt_Generator $prompt_generator   The prompt generator.
+	 * @param Completion_Service_Interface          $completion_service The completion service.
+	 */
+	public function __construct( Attribute_Suggestion_Prompt_Generator $prompt_generator, Completion_Service_Interface $completion_service ) {
+		$this->prompt_generator   = $prompt_generator;
+		$this->completion_service = $completion_service;
+	}
+
+	/**
+	 * Get suggestions for the given request.
+	 *
+	 * @param Attribute_Suggestion_Request $request The request.
+	 *
+	 * @return array An array of suggestions. Each suggestion is an associative array with the following keys:
+	 *               - content: The suggested content.
+	 *               - reason: The reason for the suggestion.
+	 *
+	 * @throws Exception If the getting the suggestions fails.
+	 */
+	public function get_suggestions( Attribute_Suggestion_Request $request ): array {
+		$messages = array(
+			array(
+				'role'    => 'system',
+				'content' => $this->prompt_generator->get_system_prompt(),
+			),
+			array(
+				'role'    => 'user',
+				'content' => $this->prompt_generator->get_user_prompt( $request ),
+			),
+		);
+
+		$completion  = $this->completion_service->get_completion( $messages, array( 'temperature' => 0.9 ) );
+		$suggestions = $this->validate_and_decode_json( $completion );
+		if ( null === $suggestions ) {
+			$messages[] = array(
+				'role'    => 'assistant',
+				'content' => $completion,
+			);
+			// Add another message and explicitly ask for a JSON response.
+			$messages[] = array(
+				'role'    => 'user',
+				'content' => 'You respond only in JSON. Do not speak normal text.',
+			);
+			$completion = $this->completion_service->get_completion( $messages );
+		}
+
+		$suggestions = $this->validate_and_decode_json( $completion );
+		if ( null === $suggestions ) {
+			// If we still don't have a valid JSON response, throw an exception.
+			throw new Exception( 'Invalid response from the API. Please try again.' );
+		}
+
+		return $suggestions;
+	}
+
+	/**
+	 * Validate and decode the JSON data.
+	 *
+	 * @param string $data The response to validate.
+	 *
+	 * @return array|null Decodes and returns the data as an associative array if it is valid JSON, returns null otherwise.
+	 */
+	protected function validate_and_decode_json( string $data ): ?array {
+		try {
+			$data_array = json_decode( $data, true, 512, JSON_THROW_ON_ERROR );
+		} catch ( JsonException $e ) {
+			return null;
+		}
+
+		return $data_array;
+	}
+}
