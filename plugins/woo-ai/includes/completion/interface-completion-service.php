2c6047717cf26329af9e1599e37047aa0fa09cd8
Nima <73110514+nima-karimi@users.noreply.github.com>
2023-05-22 10:18:13 +0100

    Add completion service

---
+++ plugins/woo-ai/includes/completion/interface-completion-service.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Completion Service Interface
+ *
+ * @package Woo_AI
+ */
+
+namespace Automattic\WooCommerce\AI\Completion;
+
+use Exception;
+
+defined( 'ABSPATH' ) || exit;
+
+interface Completion_Service_Interface {
+	/**
+	 * Gets the completion from the OpenAI API.
+	 *
+	 * @param array $messages An array of messages to send to the API.
+	 * @param array $options An array of options to send to the API.
+	 *
+	 * @return string
+	 *
+	 * @throws Exception If the request fails.
+	 */
+	public function get_completion( array $messages, array $options = array() ): string;
+}
