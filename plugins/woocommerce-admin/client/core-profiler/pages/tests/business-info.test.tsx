5e82a5759f35b748925cc4f39d7c2327b61aaa71
rjchow <me@rjchow.com>
2023-05-23 23:07:47 +1000

    add: core profiler business info page

---
+++ plugins/woocommerce-admin/client/core-profiler/pages/tests/business-info.test.tsx
@@ -0,0 +1,308 @@
+/**
+ * External dependencies
+ */
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+/**
+ * Internal dependencies
+ */
+import { BusinessInfo } from '../BusinessInfo';
+import { CoreProfilerStateMachineContext } from '../..';
+
+describe( 'BusinessInfo', () => {
+	let props: {
+		sendEvent: jest.Mock;
+		navigationProgress: number;
+		context: CoreProfilerStateMachineContext;
+	};
+
+	beforeEach( () => {
+		props = {
+			sendEvent: jest.fn(),
+			navigationProgress: 0,
+			context: {
+				geolocatedLocation: undefined,
+				userProfile: {
+					businessChoice: 'im_just_starting_my_business',
+				},
+				businessInfo: {
+					storeName: '',
+					location: '',
+				},
+				countries: [
+					{
+						key: 'AU:VIC',
+						label: 'Australia — Victoria',
+					},
+					{
+						key: 'AW',
+						label: 'Aruba',
+					},
+				],
+				// @ts-expect-error -- we don't need the other props in the tests
+				onboardingProfile: {
+					is_store_country_set: false,
+					industry: [ 'other' ],
+				},
+			},
+		};
+	} );
+
+	it( 'should render business info page', () => {
+		render( <BusinessInfo { ...props } /> );
+		expect(
+			screen.getByText( /Tell us a bit about your store/i, {
+				selector: 'h1',
+			} )
+		).toBeInTheDocument();
+
+		expect(
+			screen.getByText( /Give your store a name/i )
+		).toBeInTheDocument();
+
+		expect(
+			screen.getByText( /Where is your store located?/i )
+		).toBeInTheDocument();
+
+		expect(
+			screen.getByRole( 'button', {
+				name: /Continue/i,
+			} )
+		).toBeInTheDocument();
+	} );
+
+	it( 'should show the correct label for each businessChoice', () => {
+		props.context.userProfile.businessChoice =
+			'im_just_starting_my_business';
+		render( <BusinessInfo { ...props } /> );
+		expect(
+			screen.getByText(
+				/What type of products or services do you plan to sell?/i
+			)
+		).toBeInTheDocument();
+
+		props.context.userProfile.businessChoice = 'im_already_selling';
+		render( <BusinessInfo { ...props } /> );
+		expect(
+			screen.getByText( /Which industry is your business in?/i )
+		).toBeInTheDocument();
+
+		props.context.userProfile.businessChoice =
+			'im_setting_up_a_store_for_a_client';
+		render( <BusinessInfo { ...props } /> );
+		expect(
+			screen.getByText( /Which industry is your client's business in?/i )
+		).toBeInTheDocument();
+	} );
+
+	it( 'should prepopulate the store country selector if geolocation is available', () => {
+		props.context.geolocatedLocation = {
+			latitude: '-37.83961',
+			longitude: '144.94228',
+			country_short: 'AU',
+			country_long: 'Australia',
+			region: 'Victoria',
+			city: 'Port Melbourne',
+		};
+		render( <BusinessInfo { ...props } /> );
+		expect(
+			screen.getByRole( 'combobox', {
+				name: '', // unfortunately the SelectControl loses its name when its populated and there's no other unique identifier
+			} )
+		).toHaveValue( 'Australia — Victoria' );
+	} );
+
+	it( 'should not prepopulate the store country selector if geolocation is not available', () => {
+		render( <BusinessInfo { ...props } /> );
+		expect(
+			screen.getByRole( 'combobox', {
+				name: /Select country\/region/i,
+			} )
+		).toHaveValue( '' );
+	} );
+
+	it( 'should correctly prepopulate the store name if it is passed in', () => {
+		props.context.businessInfo.storeName = 'Test Store Name';
+		render( <BusinessInfo { ...props } /> );
+		expect(
+			screen.getByRole( 'textbox', {
+				name: /Give your store a name/i,
+			} )
+		).toHaveValue( 'Test Store Name' );
+	} );
+
+	it( 'should correctly prepopulate the industry if it is passed in', () => {
+		props.context.onboardingProfile.industry = [ 'food_and_drink' ];
+		render( <BusinessInfo { ...props } /> );
+		expect(
+			screen.getByRole( 'combobox', {
+				name: /Select an industry/i,
+			} )
+		).toHaveValue( 'Food and drink' );
+	} );
+
+	it( 'should correctly prepopulate the store location if it is passed in', () => {
+		props.context.businessInfo.location = 'AW';
+		props.context.onboardingProfile.is_store_country_set = true;
+		render( <BusinessInfo { ...props } /> );
+		expect(
+			screen.getByRole( 'combobox', {
+				name: '',
+			} )
+		).toHaveValue( 'Aruba' );
+	} );
+
+	it( 'should correctly send event with empty form inputs when continue is clicked', () => {
+		props.context.geolocatedLocation = {
+			latitude: '-37.83961',
+			longitude: '144.94228',
+			country_short: 'AU',
+			country_long: 'Australia',
+			region: 'Victoria',
+			city: 'Port Melbourne',
+		};
+		render( <BusinessInfo { ...props } /> );
+		const continueButton = screen.getByRole( 'button', {
+			name: /Continue/i,
+		} );
+		userEvent.click( continueButton );
+		expect( props.sendEvent ).toHaveBeenCalledWith( {
+			payload: {
+				geolocationOverruled: false,
+				industry: 'other',
+				storeLocation: 'AU:VIC',
+				storeName: '',
+			},
+			type: 'BUSINESS_INFO_COMPLETED',
+		} );
+	} );
+
+	it( 'should show the warning tooltip if geolocation is available and the user has manually changed the country, and send the event with geolocationOverruled', async () => {
+		props.context.geolocatedLocation = {
+			latitude: '-37.83961',
+			longitude: '144.94228',
+			country_short: 'AU',
+			country_long: 'Australia',
+			region: 'Victoria',
+			city: 'Port Melbourne',
+		};
+		render( <BusinessInfo { ...props } /> );
+		const countrySelector = screen.getByRole( 'combobox', {
+			name: '',
+		} );
+
+		countrySelector.focus();
+
+		await waitFor( () => {
+			expect(
+				screen.getByRole( 'option', {
+					name: /Aruba/i,
+				} )
+			).toBeInTheDocument();
+		} );
+
+		await userEvent.click(
+			screen.getByRole( 'option', {
+				name: /Aruba/i,
+			} )
+		);
+
+		expect(
+			screen.getByText(
+				/Setting up your store in the wrong country may lead to the following issues: /i
+			)
+		).toBeInTheDocument();
+
+		const continueButton = screen.getByRole( 'button', {
+			name: /Continue/i,
+		} );
+		userEvent.click( continueButton );
+		expect( props.sendEvent ).toHaveBeenCalledWith( {
+			payload: {
+				geolocationOverruled: true,
+				industry: 'other',
+				storeLocation: 'AW',
+				storeName: '',
+			},
+			type: 'BUSINESS_INFO_COMPLETED',
+		} );
+	} );
+
+	it( 'should correctly send event with inputs filled in a fresh form when continue is clicked', async () => {
+		props.context.geolocatedLocation = {
+			latitude: '-37.83961',
+			longitude: '144.94228',
+			country_short: 'AU',
+			country_long: 'Australia',
+			region: 'Victoria',
+			city: 'Port Melbourne',
+		};
+		render( <BusinessInfo { ...props } /> );
+		const storeNameInput = screen.getByRole( 'textbox', {
+			name: /Give your store a name/i,
+		} );
+		userEvent.type( storeNameInput, 'Test Store Name' );
+		const industrySelector = screen.getByRole( 'combobox', {
+			name: /Select an industry/i,
+		} );
+
+		industrySelector.focus();
+
+		await waitFor( () => {
+			expect(
+				screen.getByRole( 'option', {
+					name: /Food and drink/i,
+				} )
+			).toBeInTheDocument();
+		} );
+
+		await userEvent.click(
+			screen.getByRole( 'option', {
+				name: /Food and drink/i,
+			} )
+		);
+
+		const continueButton = screen.getByRole( 'button', {
+			name: /Continue/i,
+		} );
+		userEvent.click( continueButton );
+		expect( props.sendEvent ).toHaveBeenCalledWith( {
+			payload: {
+				geolocationOverruled: false,
+				industry: 'food_and_drink',
+				storeLocation: 'AU:VIC',
+				storeName: 'Test Store Name',
+			},
+			type: 'BUSINESS_INFO_COMPLETED',
+		} );
+	} );
+
+	it( 'should send the event with the correct values if the form has been pre-filled from context', () => {
+		props.context.geolocatedLocation = {
+			latitude: '-37.83961',
+			longitude: '144.94228',
+			country_short: 'AU',
+			country_long: 'Australia',
+			region: 'Victoria',
+			city: 'Port Melbourne',
+		};
+		props.context.onboardingProfile.industry = [ 'food_and_drink' ];
+		props.context.businessInfo.storeName = 'Test Store Name';
+		props.context.businessInfo.location = 'AU:VIC';
+		render( <BusinessInfo { ...props } /> );
+		const continueButton = screen.getByRole( 'button', {
+			name: /Continue/i,
+		} );
+		userEvent.click( continueButton );
+		expect( props.sendEvent ).toHaveBeenCalledWith( {
+			payload: {
+				geolocationOverruled: false,
+				industry: 'food_and_drink',
+				storeLocation: 'AU:VIC',
+				storeName: 'Test Store Name',
+			},
+			type: 'BUSINESS_INFO_COMPLETED',
+		} );
+	} );
+} );
