7f689a8f86e3db361892d571fa2fcb1bcbb2a58b
Nima <73110514+nima-karimi@users.noreply.github.com>
2023-05-24 14:16:30 +0100

    Skip the uncategorized category

--- plugins/woo-ai/includes/prompt-formatter/class-product-category-formatter.php
+++ plugins/woo-ai/includes/prompt-formatter/class-product-category-formatter.php
@@ -9,6 +9,7 @@ namespace Automattic\WooCommerce\AI\PromptFormatter;
 
 use Exception;
 use InvalidArgumentException;
+use WP_Term;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -16,6 +17,9 @@ defined( 'ABSPATH' ) || exit;
  * Product Category Formatter class.
  */
 class Product_Category_Formatter implements Prompt_Formatter_Interface {
+	private const CATEGORY_TAXONOMY         = 'product_cat';
+	private const PARENT_CATEGORY_SEPARATOR = ' > ';
+	private const UNCATEGORIZED_SLUG        = 'uncategorized';
 
 	/**
 	 * Get the category names from the category ids from WooCommerce and recursively get all the parent categories and prepend them to the category names separated by >.
@@ -33,64 +37,17 @@ class Product_Category_Formatter implements Prompt_Formatter_Interface {
 
 		$categories = array();
 		foreach ( $data as $category_id ) {
-			try {
-				$categories[] = $this->format_category_name( $category_id );
-			} catch ( Exception $e ) {
-				// If the category is not found, skip it.
+			$category = get_term( $category_id, self::CATEGORY_TAXONOMY );
+
+			// If the category is not found, or it is the uncategorized category, skip it.
+			if ( ! $category instanceof WP_Term || self::UNCATEGORIZED_SLUG === $category->slug ) {
 				continue;
 			}
+
+			$categories[] = $this->format_category_name( $category );
 		}
 
 		return implode( ', ', $categories );
-	}
-
-	/**
-	 * Get formatted category name with parent categories prepended separated by >.
-	 *
-	 * @param int $category_id The category id.
-	 *
-	 * @return string The formatted category name.
-	 *
-	 * @throws Exception If the category is not found.
-	 */
-	private function format_category_name( int $category_id ): string {
-		$category = get_term( $category_id, 'product_cat' );
-
-		// If the category is not found, throw an exception.
-		if ( ( ! $category || is_wp_error( $category ) ) ) {
-			throw new Exception( 'Category not found.' );
-		}
-
-		$parent_categories   = $this->get_parent_categories( $category_id );
-		$parent_categories[] = $category->name;
-
-		return implode( ' > ', $parent_categories );
-	}
-
-	/**
-	 * Get parent categories for the given category id.
-	 *
-	 * @param int $category_id The category id.
-	 *
-	 * @return array An array of names of the parent categories in the order of the hierarchy.
-	 */
-	private function get_parent_categories( int $category_id ): array {
-		$parent_category_ids = get_ancestors( $category_id, 'product_cat' );
-
-		if ( empty( $parent_category_ids ) ) {
-			return array();
-		}
-
-		$parent_category_ids = array_reverse( $parent_category_ids );
-
-		return array_map(
-			function ( $parent_category_id ) {
-				$parent_category = get_term( $parent_category_id, 'product_cat' );
-
-				return $parent_category->name;
-			},
-			$parent_category_ids
-		);
 	}
 
 	/**
@@ -102,6 +59,46 @@ class Product_Category_Formatter implements Prompt_Formatter_Interface {
 	 */
 	public function validate_data( $data ): bool {
 		return ! empty( $data ) && is_array( $data );
+	}
+
+	/**
+	 * Get formatted category name with parent categories prepended separated by >.
+	 *
+	 * @param WP_Term $category The category as a WP_Term object.
+	 *
+	 * @return string The formatted category name.
+	 */
+	private function format_category_name( WP_Term $category ): string {
+		$parent_categories   = $this->get_parent_categories( $category->term_id );
+		$parent_categories[] = $category->name;
+
+		return implode( self::PARENT_CATEGORY_SEPARATOR, $parent_categories );
+	}
+
+	/**
+	 * Get parent categories for the given category id.
+	 *
+	 * @param int $category_id The category id.
+	 *
+	 * @return array An array of names of the parent categories in the order of the hierarchy.
+	 */
+	private function get_parent_categories( int $category_id ): array {
+		$parent_category_ids = get_ancestors( $category_id, self::CATEGORY_TAXONOMY );
+
+		if ( empty( $parent_category_ids ) ) {
+			return array();
+		}
+
+		$parent_category_ids = array_reverse( $parent_category_ids );
+
+		return array_map(
+			function ( $parent_category_id ) {
+				$parent_category = get_term( $parent_category_id, self::CATEGORY_TAXONOMY );
+
+				return $parent_category->name;
+			},
+			$parent_category_ids
+		);
 	}
 
 }
