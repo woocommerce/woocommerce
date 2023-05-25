27a264e1bcdea9bd7ba909fda6993858aac11caa
Nima <73110514+nima-karimi@users.noreply.github.com>
2023-05-25 18:42:52 +0100

    Toggle AI popup when title in focus

--- plugins/woo-ai/src/product-name/product-name-suggestions.tsx
+++ plugins/woo-ai/src/product-name/product-name-suggestions.tsx
@@ -4,6 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { useEffect, useRef, useState } from '@wordpress/element';
 import React from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 /**
  * Internal dependencies
@@ -31,37 +32,52 @@ enum SuggestionsState {
 export function ProductNameSuggestions() {
 	const [ suggestionsState, setSuggestionsState ] =
 		useState< SuggestionsState >( SuggestionsState.None );
-	const [ error, setError ] = useState( '' );
+	const [ error, setError ] = useState< string >( '' );
+	const [ visible, setVisible ] = useState< boolean >( false );
 	const [ suggestions, setSuggestions ] = useState< ProductDataSuggestion[] >(
 		[]
 	);
 	const { fetchSuggestions } = useProductDataSuggestions();
+	const nameInputRef = useRef< HTMLInputElement >(
+		document.querySelector( '#title' )
+	);
 
-	const clearError = () => {
+	const resetError = () => {
 		setError( '' );
 		setSuggestionsState( SuggestionsState.None );
 	};
 
-	const nameEl = useRef< HTMLInputElement >(
-		document.querySelector( '#title' )
-	);
+	const onOutsideClick = ( e: MouseEvent ) => {
+		if ( e.target instanceof Node && e.target !== nameInputRef.current ) {
+			setVisible( false );
+		}
+	};
 
 	useEffect( () => {
-		const name = nameEl.current;
+		const nameInput = nameInputRef.current;
 
-		name?.addEventListener( 'keyup', clearError );
-		name?.addEventListener( 'change', clearError );
+		const onFocus = () => {
+			setVisible( true );
+		};
+
+		if ( nameInput ) {
+			nameInput.addEventListener( 'focus', onFocus );
+			// Initially hide the container unless the input is already in focus
+			const inputOwnerDocument = nameInput.ownerDocument;
+			setVisible( inputOwnerDocument?.activeElement === nameInput );
+		}
 
 		return () => {
-			name?.removeEventListener( 'keyup', clearError );
-			name?.removeEventListener( 'change', clearError );
+			if ( nameInput ) {
+				nameInput.removeEventListener( 'focus', onFocus );
+			}
 		};
-	}, [ nameEl ] );
+	}, [ nameInputRef ] );
 
 	const updateProductName = ( newName: string ) => {
-		if ( ! nameEl.current || ! newName.length ) return;
-		nameEl.current.value = newName;
-		nameEl.current.setAttribute( 'value', newName );
+		if ( ! nameInputRef.current || ! newName.length ) return;
+		nameInputRef.current.value = newName;
+		nameInputRef.current.setAttribute( 'value', newName );
 	};
 
 	const handleSuggestionClick = ( suggestion: ProductDataSuggestion ) => {
@@ -69,8 +85,8 @@ export function ProductNameSuggestions() {
 		setSuggestions( [] );
 	};
 
-	const getSuggestions = async () => {
-		clearError();
+	const fetchProductSuggestions = async () => {
+		resetError();
 		setSuggestions( [] );
 		setSuggestionsState( SuggestionsState.Fetching );
 		try {
@@ -91,60 +107,67 @@ export function ProductNameSuggestions() {
 	};
 
 	return (
-		<div className="wc-product-name-suggestions-container">
-			{ suggestions.length > 0 &&
-				suggestionsState !== SuggestionsState.Fetching && (
-					<ul className="wc-product-name-suggestions__suggested-names">
-						{ suggestions.map( ( suggestion, index ) => (
-							<SuggestionItem
-								key={ index }
-								suggestion={ suggestion }
-								onSuggestionClick={ handleSuggestionClick }
-							/>
-						) ) }
-					</ul>
-				) }
-			{ suggestionsState !== SuggestionsState.Fetching && (
-				<button
-					className="button woo-ai-get-suggestions-btn"
-					type="button"
-					onClick={ getSuggestions }
-				>
-					<img src={ MagicIcon } alt="magic button icon" />
-					{ suggestions.length > 0 &&
-						__( 'Suggest Alternatives', 'woocommerce' ) }
-					{ ! suggestions.length &&
-						__( 'Get Suggestions (beta)', 'woocommerce' ) }
-				</button>
-			) }
-			{ suggestionsState === SuggestionsState.Fetching && (
-				<p className="wc-product-name-suggestions__loading-message">
-					<RandomLoadingMessage
-						isLoading={
-							suggestionsState === SuggestionsState.Fetching
-						}
-					/>
-				</p>
-			) }
-			{ suggestionsState === SuggestionsState.Fetching && (
-				<p className="wc-product-name-suggestions__tip-message">
-					<RandomTipMessage />
-				</p>
-			) }
-			{ suggestionsState === SuggestionsState.Failed && (
-				<p className="wc-product-name-suggestions__error-message">
-					<ErrorMessage error={ error } />
+		<OutsideClickHandler
+			onOutsideClick={ onOutsideClick }
+			display="contents"
+		>
+			<div
+				className="wc-product-name-suggestions-container"
+				hidden={ ! visible }
+			>
+				{ suggestions.length > 0 &&
+					suggestionsState !== SuggestionsState.Fetching && (
+						<ul className="wc-product-name-suggestions__suggested-names">
+							{ suggestions.map( ( suggestion, index ) => (
+								<SuggestionItem
+									key={ index }
+									suggestion={ suggestion }
+									onSuggestionClick={ handleSuggestionClick }
+								/>
+							) ) }
+						</ul>
+					) }
+				{ suggestionsState !== SuggestionsState.Fetching && (
 					<button
-						className="notice-dismiss"
+						className="button woo-ai-get-suggestions-btn"
 						type="button"
-						onClick={ clearError }
+						onClick={ fetchProductSuggestions }
 					>
-						<span className="screen-reader-text">
-							{ __( 'Dismiss this notice.', 'woocommerce' ) }
-						</span>
+						<img src={ MagicIcon } alt="magic button icon" />
+						{ suggestions.length > 0
+							? __( 'Suggest Alternatives', 'woocommerce' )
+							: __( 'Get Suggestions (beta)', 'woocommerce' ) }
 					</button>
-				</p>
-			) }
-		</div>
+				) }
+				{ suggestionsState === SuggestionsState.Fetching && (
+					<p className="wc-product-name-suggestions__loading-message">
+						<RandomLoadingMessage
+							isLoading={
+								suggestionsState === SuggestionsState.Fetching
+							}
+						/>
+					</p>
+				) }
+				{ suggestionsState === SuggestionsState.Fetching && (
+					<p className="wc-product-name-suggestions__tip-message">
+						<RandomTipMessage />
+					</p>
+				) }
+				{ suggestionsState === SuggestionsState.Failed && (
+					<p className="wc-product-name-suggestions__error-message">
+						<ErrorMessage error={ error } />
+						<button
+							className="notice-dismiss"
+							type="button"
+							onClick={ resetError }
+						>
+							<span className="screen-reader-text">
+								{ __( 'Dismiss this notice.', 'woocommerce' ) }
+							</span>
+						</button>
+					</p>
+				) }
+			</div>
+		</OutsideClickHandler>
 	);
 }
