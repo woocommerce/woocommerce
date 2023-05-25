7614bc340ea3a46538489b8119a7d57201d6cb85
Lourens Schep <lourensschep@gmail.com>
2023-05-25 09:49:30 +0200

    Fix merge conflicts

--- plugins/woocommerce/src/Admin/Features/ProductBlockEditor/Init.php
+++ plugins/woocommerce/src/Admin/Features/ProductBlockEditor/Init.php
@@ -116,107 +116,6 @@ class Init {
 		return $link;
 	}
 
-<<<<<<< HEAD
-=======
-	/**
-	 * Get the resolved assets needed for the iframe editor.
-	 *
-	 * @return array Styles and scripts.
-	 */
-	private function get_resolved_assets() {
-		if ( function_exists( 'gutenberg_resolve_assets_override' ) ) {
-			return gutenberg_resolve_assets_override();
-		}
-
-		global $pagenow;
-
-		$script_handles = array(
-			'wp-polyfill',
-		);
-		// Note for core merge: only 'wp-edit-blocks' should be in this array.
-		$style_handles = array(
-			'wp-edit-blocks',
-		);
-
-		if ( current_theme_supports( 'wp-block-styles' ) ) {
-			$style_handles[] = 'wp-block-library-theme';
-		}
-
-		if ( 'widgets.php' === $pagenow || 'customize.php' === $pagenow ) {
-			$style_handles[] = 'wp-widgets';
-			$style_handles[] = 'wp-edit-widgets';
-		}
-
-		$block_registry = \WP_Block_Type_Registry::get_instance();
-
-		foreach ( $block_registry->get_all_registered() as $block_type ) {
-			// In older WordPress versions, like 6.0, these properties are not defined.
-			if ( isset( $block_type->style_handles ) && is_array( $block_type->style_handles ) ) {
-				$style_handles = array_merge( $style_handles, $block_type->style_handles );
-			}
-
-			if ( isset( $block_type->editor_style_handles ) && is_array( $block_type->editor_style_handles ) ) {
-				$style_handles = array_merge( $style_handles, $block_type->editor_style_handles );
-			}
-
-			if ( isset( $block_type->script_handles ) && is_array( $block_type->script_handles ) ) {
-				$script_handles = array_merge( $script_handles, $block_type->script_handles );
-			}
-		}
-
-		$style_handles = array_unique( $style_handles );
-		$done          = wp_styles()->done;
-
-		ob_start();
-
-		// We do not need reset styles for the iframed editor.
-		wp_styles()->done = array( 'wp-reset-editor-styles' );
-		wp_styles()->do_items( $style_handles );
-		wp_styles()->done = $done;
-
-		$styles = ob_get_clean();
-
-		$script_handles = array_unique( $script_handles );
-		$done           = wp_scripts()->done;
-
-		ob_start();
-
-		wp_scripts()->done = array();
-		wp_scripts()->do_items( $script_handles );
-		wp_scripts()->done = $done;
-
-		$scripts = ob_get_clean();
-
-		/*
-		 * Generate font @font-face styles for the site editor iframe.
-		 * Use the registered font families for printing.
-		 */
-		if ( class_exists( '\WP_Fonts' ) ) {
-			$wp_fonts   = wp_fonts();
-			$registered = $wp_fonts->get_registered_font_families();
-			if ( ! empty( $registered ) ) {
-				$queue = $wp_fonts->queue;
-				$done  = $wp_fonts->done;
-
-				$wp_fonts->done  = array();
-				$wp_fonts->queue = $registered;
-
-				ob_start();
-				$wp_fonts->do_items();
-				$styles .= ob_get_clean();
-
-				// Reset the Web Fonts API.
-				$wp_fonts->done  = $done;
-				$wp_fonts->queue = $queue;
-			}
-		}
-
-		return array(
-			'styles'  => $styles,
-			'scripts' => $scripts,
-		);
-	}
-
 	/**
 	 * Enqueue styles needed for the rich text editor.
 	 *
@@ -232,7 +131,6 @@ class Init {
 					array(
 						'id'    => 'general',
 						'title' => __( 'General', 'woocommerce' ),
-						'order' => 10,
 					),
 					array(
 						array(
@@ -241,18 +139,18 @@ class Init {
 								'title'       => __( 'Basic details', 'woocommerce' ),
 								'description' => __( 'This info will be displayed on the product page, category pages, social media, and search results.', 'woocommerce' ),
 								'icon'        => array(
-									'src' => plugins_url( '/assets/client/admin/product-editor/icons/section_basic.svg', WC_PLUGIN_FILE ),
+									'src' => '<svg xmlns="http://www.w3.org/2000/svg" view-box="0 0 24 24"><path fill-rule="evenodd" d="M5 5.5h14a.5.5 0 01.5.5v1.5a.5.5 0 01-.5.5H5a.5.5 0 01-.5-.5V6a.5.5 0 01.5-.5zM4 9.232A2 2 0 013 7.5V6a2 2 0 012-2h14a2 2 0 012 2v1.5a2 2 0 01-1 1.732V18a2 2 0 01-2 2H6a2 2 0 01-2-2V9.232zm1.5.268V18a.5.5 0 00.5.5h12a.5.5 0 00.5-.5V9.5h-13z" clip-rule="evenodd" /></svg>',
 								),
 							),
 							array(
 								array(
-									'woocommerce/product-name-field',
+									'woocommerce/product-name',
 									array(
 										'name' => 'Product name',
 									),
 								),
 								array(
-									'woocommerce/product-summary-field',
+									'woocommerce/product-summary',
 								),
 								array(
 									'core/columns',
@@ -265,11 +163,11 @@ class Init {
 											),
 											array(
 												array(
-													'woocommerce/product-regular-price-field',
+													'woocommerce/product-pricing',
 													array(
-														'name'  => 'regular_price',
+														'name' => 'regular_price',
 														'label' => __( 'List price', 'woocommerce' ),
-														'help'  => __( 'Manage more settings in <PricingTab>Pricing.</PricingTab>', 'woocommerce' ),
+														'showPricingSection' => true,
 													),
 												),
 											),
@@ -281,8 +179,9 @@ class Init {
 											),
 											array(
 												array(
-													'woocommerce/product-sale-price-field',
+													'woocommerce/product-pricing',
 													array(
+														'name' => 'sale_price',
 														'label' => __( 'Sale price', 'woocommerce' ),
 													),
 												),
@@ -295,73 +194,20 @@ class Init {
 						array(
 							'woocommerce/product-section',
 							array(
-								'title'       => __( 'Description', 'woocommerce' ),
-								'description' => __( 'What makes this product unique? What are its most important features? Enrich the product page by adding rich content using blocks.', 'woocommerce' ),
-								'icon'        => array(
-									'src' => '<svg width="14" height="17" viewBox="0 0 14 17" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="9.91663" y1="16.5" x2="9.91663" y2="1.38889" stroke="#1E1E1E" stroke-width="1.5"/><line x1="5.47217" y1="16.5" x2="5.47217" y2="1.38889" stroke="#1E1E1E" stroke-width="1.5"/><line x1="13.3334" y1="1.25" x2="4.44448" y2="1.25" stroke="#1E1E1E" stroke-width="1.5"/><path d="M4.13889 5.38889V9.46C2.21109 9.10713 0.75 7.41864 0.75 5.38889C0.75 3.35914 2.21109 1.67065 4.13889 1.31778V5.38889Z" fill="#1E1E1E" stroke="#1E1E1E" stroke-width="1.5"/></svg>',
-								),
-							),
-							array(
-								array(
-									'woocommerce/product-description-field',
-								),
-							),
-						),
-						array(
-							'woocommerce/product-section',
-							array(
 								'title'       => __( 'Images', 'woocommerce' ),
 								'description' => sprintf(
 									/* translators: %1$s: Images guide link opening tag. %2$s: Images guide link closing tag.*/
-									__( 'Drag images, upload new ones or select files from your library. For best results, use JPEG files that are 1000 by 1000 pixels or larger. %1$sHow to prepare images?%2$s', 'woocommerce' ),
+									__( 'Drag images, upload new ones or select files from your library. For best results, use JPEG files that are 1000 by 1000 pixels or larger. %1$sHow to prepare images?%2$s.', 'woocommerce' ),
 									'<a href="http://woocommerce.com/#" target="_blank" rel="noreferrer">',
 									'</a>'
 								),
-								'icon'        => array(
-									'src' => plugins_url( '/assets/client/admin/product-editor/icons/section_images.svg', WC_PLUGIN_FILE ),
-								),
 							),
 							array(
 								array(
-									'woocommerce/product-images-field',
+									'woocommerce/product-images',
 									array(
 										'images' => array(),
 									),
-								),
-							),
-						),
-						array(
-							'woocommerce/product-section',
-							array(
-								'title'       => __( 'Organization & visibility', 'woocommerce' ),
-								'description' => __( 'Help customers find this product by assigning it to categories or featuring it across your sales channels.', 'woocommerce' ),
-							),
-							array(
-								array(
-									'woocommerce/product-category-field',
-									array(
-										'name' => 'categories',
-									),
-								),
-							),
-						),
-						array(
-							'woocommerce/product-section',
-							array(
-								'title'       => __( 'Attributes', 'woocommerce' ),
-								'description' => sprintf(
-									/* translators: %1$s: Attributes guide link opening tag. %2$s: Attributes guide link closing tag.*/
-									__( 'Add descriptive pieces of information that customers can use to filter and search for this product. %1$sLearn more%2$s', 'woocommerce' ),
-									'<a href="https://woocommerce.com/document/managing-product-taxonomies/#product-attributes" target="_blank" rel="noreferrer">',
-									'</a>'
-								),
-								'icon'        => array(
-									'src' => plugins_url( '/assets/client/admin/product-editor/icons/section_attributes.svg', WC_PLUGIN_FILE ),
-								),
-							),
-							array(
-								array(
-									'woocommerce/product-attributes-field',
 								),
 							),
 						),
@@ -372,7 +218,6 @@ class Init {
 					array(
 						'id'    => 'pricing',
 						'title' => __( 'Pricing', 'woocommerce' ),
-						'order' => 20,
 					),
 					array(
 						array(
@@ -386,7 +231,7 @@ class Init {
 									'</a>'
 								),
 								'icon'        => array(
-									'src' => plugins_url( '/assets/client/admin/product-editor/icons/section_pricing.svg', WC_PLUGIN_FILE ),
+									'src' => '<svg xmlns="http://www.w3.org/2000/svg" view-box="0 0 24 24"><path fill-rule="evenodd" d="M16.83 6.342l.602.3.625-.25.443-.176v12.569l-.443-.178-.625-.25-.603.301-1.444.723-2.41-.804-.475-.158-.474.158-2.41.803-1.445-.722-.603-.3-.625.25-.443.177V6.215l.443.178.625.25.603-.301 1.444-.722 2.41.803.475.158.474-.158 2.41-.803 1.445.722zM20 4l-1.5.6-1 .4-2-1-3 1-3-1-2 1-1-.4L5 4v17l1.5-.6 1-.4 2 1 3-1 3 1 2-1 1 .4 1.5.6V4zm-3.5 6.25v-1.5h-8v1.5h8zm0 3v-1.5h-8v1.5h8zm-8 3v-1.5h8v1.5h-8z" clip-rule="evenodd" /></svg>',
 								),
 							),
 							array(
@@ -401,10 +246,11 @@ class Init {
 											),
 											array(
 												array(
-													'woocommerce/product-regular-price-field',
+													'woocommerce/product-pricing',
 													array(
-														'name'  => 'regular_price',
+														'name' => 'regular_price',
 														'label' => __( 'List price', 'woocommerce' ),
+														'showPricingSection' => true,
 													),
 												),
 											),
@@ -416,8 +262,9 @@ class Init {
 											),
 											array(
 												array(
-													'woocommerce/product-sale-price-field',
+													'woocommerce/product-pricing',
 													array(
+														'name' => 'sale_price',
 														'label' => __( 'Sale price', 'woocommerce' ),
 													),
 												),
@@ -429,7 +276,7 @@ class Init {
 									'woocommerce/product-schedule-sale-fields',
 								),
 								array(
-									'woocommerce/product-radio-field',
+									'woocommerce/product-radio',
 									array(
 										'title'    => __( 'Charge sales tax on', 'woocommerce' ),
 										'property' => 'tax_status',
@@ -450,7 +297,7 @@ class Init {
 									),
 								),
 								array(
-									'woocommerce/product-collapsible',
+									'woocommerce/collapsible',
 									array(
 										'toggleText'       => __( 'Advanced', 'woocommerce' ),
 										'initialCollapsed' => true,
@@ -458,7 +305,7 @@ class Init {
 									),
 									array(
 										array(
-											'woocommerce/product-radio-field',
+											'woocommerce/product-radio',
 											array(
 												'title'    => __( 'Tax class', 'woocommerce' ),
 												'description' => sprintf(
@@ -495,7 +342,6 @@ class Init {
 					array(
 						'id'    => 'inventory',
 						'title' => __( 'Inventory', 'woocommerce' ),
-						'order' => 30,
 					),
 					array(
 						array(
@@ -509,67 +355,18 @@ class Init {
 									'</a>'
 								),
 								'icon'        => array(
-									'src' => plugins_url( '/assets/client/admin/product-editor/icons/section_inventory.svg', WC_PLUGIN_FILE ),
+									'src' => '<svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 2H14C14.2761 2 14.5 2.22386 14.5 2.5V9.5H11.5H10C10 10.6046 9.10457 11.5 8 11.5C6.89543 11.5 6 10.6046 6 9.5H4.5H1.5V2.5C1.5 2.22386 1.72386 2 2 2ZM1.5 11V14.5C1.5 14.7761 1.72386 15 2 15H14C14.2761 15 14.5 14.7761 14.5 14.5V11H11.1632C10.6015 12.1825 9.3962 13 8 13C6.6038 13 5.39855 12.1825 4.83682 11H1.5ZM0 9.5V2.5C0 1.39543 0.895431 0.5 2 0.5H14C15.1046 0.5 16 1.39543 16 2.5V9.5V11V14.5C16 15.6046 15.1046 16.5 14 16.5H2C0.895431 16.5 0 15.6046 0 14.5V11V9.5Z" fill="#1E1E1E"/></svg>',
 								),
 							),
 							array(
 								array(
-									'woocommerce/product-sku-field',
+									'woocommerce/product-sku',
 								),
 								array(
-									'woocommerce/product-toggle-field',
-									array(
-										'label'    => __( 'Track stock quantity for this product', 'woocommerce' ),
-										'property' => 'manage_stock',
-										'disabled' => 'yes' !== get_option( 'woocommerce_manage_stock' ),
-									),
+									'woocommerce/product-track-inventory-fields',
 								),
 								array(
-									'woocommerce/conditional',
-									array(
-										'mustMatch' => array(
-											'manage_stock' => array( true ),
-										),
-									),
-									array(
-										array(
-											'woocommerce/product-inventory-quantity-field',
-										),
-									),
-								),
-								array(
-									'woocommerce/conditional',
-									array(
-										'mustMatch' => array(
-											'manage_stock' => array( false ),
-										),
-									),
-									array(
-										array(
-											'woocommerce/product-radio-field',
-											array(
-												'title'    => __( 'Stock status', 'woocommerce' ),
-												'property' => 'stock_status',
-												'options'  => array(
-													array(
-														'label' => __( 'In stock', 'woocommerce' ),
-														'value' => 'instock',
-													),
-													array(
-														'label' => __( 'Out of stock', 'woocommerce' ),
-														'value' => 'outofstock',
-													),
-													array(
-														'label' => __( 'On backorder', 'woocommerce' ),
-														'value' => 'onbackorder',
-													),
-												),
-											),
-										),
-									),
-								),
-								array(
-									'woocommerce/product-collapsible',
+									'woocommerce/collapsible',
 									array(
 										'toggleText'       => __( 'Advanced', 'woocommerce' ),
 										'initialCollapsed' => true,
@@ -585,7 +382,7 @@ class Init {
 											),
 											array(
 												array(
-													'woocommerce/product-radio-field',
+													'woocommerce/product-radio',
 													array(
 														'title'    => __( 'When out of stock', 'woocommerce' ),
 														'property' => 'backorders',
@@ -609,12 +406,12 @@ class Init {
 													),
 												),
 												array(
-													'woocommerce/product-inventory-email-field',
+													'woocommerce/product-inventory-email',
 												),
 											),
 										),
 										array(
-											'woocommerce/product-checkbox-field',
+											'woocommerce/product-checkbox',
 											array(
 												'title'    => __(
 													'Restrictions',
@@ -625,7 +422,7 @@ class Init {
 													'woocommerce'
 												),
 												'property' => 'sold_individually',
-												'tooltip'  => __(
+												'tooltip' => __(
 													'When checked, customers will be able to purchase only 1 item in a single order. This is particularly useful for items that have limited quantity, like art or handmade goods.',
 													'woocommerce'
 												),
@@ -645,7 +442,6 @@ class Init {
 					array(
 						'id'    => 'shipping',
 						'title' => __( 'Shipping', 'woocommerce' ),
-						'order' => 40,
 					),
 					array(
 						array(
@@ -659,7 +455,7 @@ class Init {
 									'</a>'
 								),
 								'icon'        => array(
-									'src' => plugins_url( '/assets/client/admin/product-editor/icons/section_shipping.svg', WC_PLUGIN_FILE ),
+									'src' => '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 7.75C3.5 6.7835 4.2835 6 5.25 6H14.75H15.5V6.75V9H17.25H17.5607L17.7803 9.21967L20.7803 12.2197L21 12.4393V12.75V14.75C21 15.7165 20.2165 16.5 19.25 16.5H19.2377C19.2458 16.5822 19.25 16.6656 19.25 16.75C19.25 18.1307 18.1307 19.25 16.75 19.25C15.3693 19.25 14.25 18.1307 14.25 16.75C14.25 16.6656 14.2542 16.5822 14.2623 16.5H14H10.2377C10.2458 16.5822 10.25 16.6656 10.25 16.75C10.25 18.1307 9.13071 19.25 7.75 19.25C6.36929 19.25 5.25 18.1307 5.25 16.75C5.25 16.6656 5.25418 16.5822 5.26234 16.5H4.25H3.5V15.75V7.75ZM14 15V9.75V9V7.5H5.25C5.11193 7.5 5 7.61193 5 7.75V15H5.96464C6.41837 14.5372 7.05065 14.25 7.75 14.25C8.44935 14.25 9.08163 14.5372 9.53536 15H14ZM18.5354 15H19.25C19.3881 15 19.5 14.8881 19.5 14.75V13.0607L16.9393 10.5H15.5V14.5845C15.8677 14.3717 16.2946 14.25 16.75 14.25C17.4493 14.25 18.0816 14.5372 18.5354 15ZM6.7815 16.5C6.76094 16.5799 6.75 16.6637 6.75 16.75C6.75 17.3023 7.19772 17.75 7.75 17.75C8.30228 17.75 8.75 17.3023 8.75 16.75C8.75 16.6637 8.73906 16.5799 8.7185 16.5C8.60749 16.0687 8.21596 15.75 7.75 15.75C7.28404 15.75 6.89251 16.0687 6.7815 16.5ZM15.7815 16.5C15.7609 16.5799 15.75 16.6637 15.75 16.75C15.75 17.3023 16.1977 17.75 16.75 17.75C17.3023 17.75 17.75 17.3023 17.75 16.75C17.75 16.6637 17.7391 16.5799 17.7185 16.5C17.7144 16.4841 17.7099 16.4683 17.705 16.4526C17.5784 16.0456 17.1987 15.75 16.75 15.75C16.284 15.75 15.8925 16.0687 15.7815 16.5Z" fill="#1E1E1E"/></svg>',
 								),
 							),
 							array(
@@ -680,5 +476,4 @@ class Init {
 		}
 		return $args;
 	}
->>>>>>> 0ea5205672 (Move template addition to feature flag (#38276))
 }
