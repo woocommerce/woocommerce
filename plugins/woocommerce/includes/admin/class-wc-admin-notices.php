27e2f7b3341c72fe68b993d98561c0e44d305e7f
Rua Haszard <rua@automattic.com>
2023-05-25 16:01:44 +1200

    prototype contextual notice component – php func, template, and css

--- plugins/woocommerce/includes/admin/class-wc-admin-notices.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-notices.php
@@ -253,6 +253,24 @@ class WC_Admin_Notices {
 	}
 
 	/**
+	 * Render a custom contextual notice.
+	 *
+	 * This is a wrapper for a contextual notice template.
+	 * Allows Woo or extensions to render a relevant notice in
+	 * a specific section of the Woo UI.
+	 * For example, rendering a payment dispute notice in order
+	 * edit screen, using `woocommerce_order_details_after_payment_info` hook.
+	 *
+	 * @param string $notice_html Notice message. Can be rich html (e.g. bold, italics, links).
+	 * @param string $severity    Severity of the notice. Can be 'error', 'warning', 'success', 'info'.
+	 * @param string $button_text Button text (plain text).
+	 * @param string $button_url  URL to link to.
+	 */
+	public static function render_contextual_notice( $notice_html, $button_text, $severity, $button_url ) {
+		include dirname( __FILE__ ) . '/views/html-notice-contextual.php';
+	}
+
+	/**
 	 * Output any stored custom notices.
 	 */
 	public static function output_custom_notices() {
---
+++ plugins/woocommerce/includes/admin/views/html-notice-contextual.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Admin View: Custom Notices
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+<div class="woocommerce-contextual-notice woocommerce-contextual-notice-<?php echo esc_attr( $severity ); ?>">
+  <?php echo wp_kses_post( $notice_html ); ?>
+  <div class="woocommerce-contextual-notice-cta" >
+  	<a class="components-button is-secondary"
+      href="<?php echo esc_url( $button_url ); ?>">
+      <?php echo esc_html( $button_text ) ?>
+    </a>
+  </div>
+</div>
