5c140769ffdf9355131a2461beeb7f03ed630783
Matt Sherman <matt@jam123.com>
2023-05-26 16:29:35 -0400

    Mark the current screen as the block editor if wc-admin

--- plugins/woocommerce/src/Admin/Features/ProductBlockEditor/Init.php
+++ plugins/woocommerce/src/Admin/Features/ProductBlockEditor/Init.php
@@ -31,6 +31,9 @@ class Init {
 			}
 			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 			add_filter( 'woocommerce_register_post_type_product', array( $this, 'add_product_template' ) );
+
+			add_action( 'current_screen', array( $this, 'set_current_screen_to_block_editor_if_wc_admin' ) );
+
 			$block_registry = new BlockRegistry();
 			$block_registry->init();
 		}
@@ -647,5 +650,19 @@ class Init {
 			);
 		}
 		return $args;
+	}
+
+	/**
+	 * Sets the current screen to the block editor if a wc-admin page.
+	 */
+	public function set_current_screen_to_block_editor_if_wc_admin() {
+		$screen = get_current_screen();
+
+		// we can't check the $_GET['path'] because client-side routing is used within wc-admin,
+		// so this action handler is only called on the initial page load from the server, which might
+		// not be the product edit page (it mostly likely isn't).
+		if ( isset( $_GET['page'] ) && 'wc-admin' === $_GET['page'] ) {
+			$screen->is_block_editor( true );
+		}
 	}
 }
