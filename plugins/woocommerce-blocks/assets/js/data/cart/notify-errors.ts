44a8aa60b39aa4dee4d6da9ae10e97328ff29090
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-05-26 19:39:33 +0200

    Mini-Cart: Make error notifications non-dismissable. Closes woocommerce/woocommerce-blocks#8965. (https://github.com/woocommerce/woocommerce-blocks/pull/9578)

--- plugins/woocommerce-blocks/assets/js/data/cart/notify-errors.ts
+++ plugins/woocommerce-blocks/assets/js/data/cart/notify-errors.ts
@@ -25,7 +25,7 @@ export const notifyCartErrors = (
 				createNotice( 'error', decodeEntities( error.message ), {
 					id: error.code,
 					context: 'wc/cart',
-					isDismissible: true,
+					isDismissible: false,
 				} );
 			}
 		} );
