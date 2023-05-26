5bd706edd93a9d2ca3e277c3cbbb355711b9f454
Hritik Chaudhary <33057454+hritikchaudhary@users.noreply.github.com>
2023-05-26 12:16:13 +0530

    Remove propTypes definitions from Top Rated Products block (https://github.com/woocommerce/woocommerce-blocks/pull/9595)
    * replace propTypes and converted to tsx -  product-top-rated/block.js
    
    * replace propTypes and converted to tsx -  product-top-rated/block.js
    
    * converted product-top-rated to typescript
    
    * added JSX.Element
    
    ---------
    
    Co-authored-by: Niels Lange <info@nielslange.de>

--- plugins/woocommerce-blocks/assets/js/blocks/product-top-rated/block.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-top-rated/block.tsx
@@ -2,11 +2,9 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
 import { Disabled, PanelBody } from '@wordpress/components';
 import { InspectorControls } from '@wordpress/block-editor';
 import ServerSideRender from '@wordpress/server-side-render';
-import PropTypes from 'prop-types';
 import GridContentControl from '@woocommerce/editor-components/grid-content-control';
 import GridLayoutControl from '@woocommerce/editor-components/grid-layout-control';
 import ProductCategoryControl from '@woocommerce/editor-components/product-category-control';
@@ -15,21 +13,29 @@ import { gridBlockPreview } from '@woocommerce/resource-previews';
 import { getSetting } from '@woocommerce/settings';
 
 /**
+ * Internal dependencies
+ */
+import { Props } from './types';
+/**
  * Component to handle edit mode of "Top Rated Products".
  */
-class ProductTopRatedBlock extends Component {
-	getInspectorControls() {
-		const { attributes, setAttributes } = this.props;
-		const {
-			categories,
-			catOperator,
-			columns,
-			contentVisibility,
-			rows,
-			alignButtons,
-			stockStatus,
-		} = attributes;
+export const ProductTopRatedBlock = ( {
+	attributes,
+	name,
+	setAttributes,
+}: Props ): JSX.Element => {
+	const {
+		categories,
+		catOperator,
+		columns,
+		contentVisibility,
+		rows,
+		alignButtons,
+		stockStatus,
+		isPreview,
+	} = attributes;
 
+	const getInspectorControls = () => {
 		return (
 			<InspectorControls key="inspector">
 				<PanelBody
@@ -91,42 +97,20 @@ class ProductTopRatedBlock extends Component {
 				</PanelBody>
 			</InspectorControls>
 		);
+	};
+
+	if ( isPreview ) {
+		return gridBlockPreview;
 	}
 
-	render() {
-		const { name, attributes } = this.props;
-
-		if ( attributes.isPreview ) {
-			return gridBlockPreview;
-		}
-
-		return (
-			<>
-				{ this.getInspectorControls() }
-				<Disabled>
-					<ServerSideRender
-						block={ name }
-						attributes={ attributes }
-					/>
-				</Disabled>
-			</>
-		);
-	}
-}
-
-ProductTopRatedBlock.propTypes = {
-	/**
-	 * The attributes for this block
-	 */
-	attributes: PropTypes.object.isRequired,
-	/**
-	 * The register block name.
-	 */
-	name: PropTypes.string.isRequired,
-	/**
-	 * A callback to update attributes
-	 */
-	setAttributes: PropTypes.func.isRequired,
+	return (
+		<>
+			{ getInspectorControls() }
+			<Disabled>
+				<ServerSideRender block={ name } attributes={ attributes } />
+			</Disabled>
+		</>
+	);
 };
 
 export default ProductTopRatedBlock;
