0cfbf0a653b97b241b9943124dd02cb8ec902ba4
Paul Sealock <psealock@gmail.com>
2023-05-26 12:02:20 +1200

    Changelog CLI: Add command to make entry from PR description (#38357)

---
+++ tools/monorepo-utils/src/changefile/lib/__tests__/github.ts
@@ -0,0 +1,401 @@
+/**
+ * External dependencies
+ */
+import path from 'path';
+
+/**
+ * Internal dependencies
+ */
+import {
+	getChangelogSignificance,
+	getChangelogType,
+	getChangelogDetails,
+} from '../github';
+import { Logger } from '../../../core/logger';
+
+jest.mock( '../../../core/logger', () => {
+	return {
+		Logger: {
+			error: jest.fn(),
+		},
+	};
+} );
+
+describe( 'getChangelogSignificance', () => {
+	it( 'should return the selected significance', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Patch\r\n' +
+			'- [ ] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [ ] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message\r\n' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment\r\n' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'\r\n' +
+			'</details>';
+
+		const significance = getChangelogSignificance( body );
+		expect( significance ).toBe( 'patch' );
+	} );
+
+	it( 'should error when no significance selected', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [ ] Patch\r\n' +
+			'- [ ] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [ ] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message\r\n' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment\r\n' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'\r\n' +
+			'</details>';
+
+		const significance = getChangelogSignificance( body );
+		expect( significance ).toBeUndefined();
+		expect( Logger.error ).toHaveBeenCalledWith(
+			'No changelog significance found'
+		);
+	} );
+
+	it( 'should error when more than one significance selected', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Patch\r\n' +
+			'- [x] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [ ] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message\r\n' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment\r\n' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'\r\n' +
+			'</details>';
+
+		const significance = getChangelogSignificance( body );
+		expect( significance ).toBeUndefined();
+		expect( Logger.error ).toHaveBeenCalledWith(
+			'Multiple changelog significances found. Only one can be entered'
+		);
+	} );
+} );
+
+describe( 'getChangelogType', () => {
+	it( 'should return the selected changelog type', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Patch\r\n' +
+			'- [ ] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [ ] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message\r\n' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment\r\n' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'\r\n' +
+			'</details>';
+
+		const type = getChangelogType( body );
+		expect( type ).toBe( 'fix' );
+	} );
+
+	it( 'should error when no type selected', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Patch\r\n' +
+			'- [ ] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [ ] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [ ] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message\r\n' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment\r\n' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'\r\n' +
+			'</details>';
+
+		const type = getChangelogType( body );
+		expect( type ).toBeUndefined();
+		expect( Logger.error ).toHaveBeenCalledWith(
+			'No changelog type found'
+		);
+	} );
+
+	it( 'should error more than one type selected', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Patch\r\n' +
+			'- [ ] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [ ] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [x] Update - Update existing functionality\r\n' +
+			'- [x] Dev - Development related task\r\n' +
+			'- [x] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message\r\n' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment\r\n' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'\r\n' +
+			'</details>';
+
+		const type = getChangelogType( body );
+		expect( type ).toBeUndefined();
+		expect( Logger.error ).toHaveBeenCalledWith(
+			'Multiple changelog types found. Only one can be entered'
+		);
+	} );
+} );
+
+describe( 'getChangelogDetails', () => {
+	it( 'should return the changelog details', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Patch\r\n' +
+			'- [ ] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [ ] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message\r\n' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment\r\n' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'\r\n' +
+			'</details>';
+
+		const details = getChangelogDetails( body );
+		expect( details.significance ).toEqual( 'patch' );
+		expect( details.type ).toEqual( 'fix' );
+		expect( details.message ).toEqual( 'This is a very useful fix.' );
+		expect( details.comment ).toEqual( '' );
+	} );
+
+	it( 'should error if a comment and message are added', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Patch\r\n' +
+			'- [ ] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [ ] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message\r\n' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment\r\n' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'This is a very useful comment.\r\n' +
+			'\r\n' +
+			'</details>';
+
+		const details = getChangelogDetails( body );
+		expect( details ).toBeUndefined();
+		expect( Logger.error ).toHaveBeenCalledWith(
+			'Both a message and comment were found. Only one can be entered'
+		);
+	} );
+
+	it( 'should error if a comment is entered with a significance other than patch', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [ ] Patch\r\n' +
+			'- [x] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [ ] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message\r\n' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'\r\n' +
+			'#### Comment\r\n' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'This is a very useful comment.\r\n' +
+			'\r\n' +
+			'</details>';
+
+		const details = getChangelogDetails( body );
+		expect( details ).toBeUndefined();
+		expect( Logger.error ).toHaveBeenCalledWith(
+			'Only patch changes can have a comment. Please change the significance to patch or remove the comment'
+		);
+	} );
+} );
---
+++ tools/monorepo-utils/src/changefile/lib/github.ts
@@ -0,0 +1,167 @@
+/**
+ * Internal dependencies
+ */
+import { getPullRequest, isCommunityPullRequest } from '../../core/github/repo';
+import { Logger } from '../../core/logger';
+
+/**
+ * Get relevant data from a pull request.
+ *
+ * @param {Object} options
+ * @param {string} options.owner repository owner.
+ * @param {string} options.name  repository name.
+ * @param {string} prNumber      pull request number.
+ * @return {Promise<object>}     pull request data.
+ */
+export const getPullRequestData = async (
+	options: { owner: string; name: string },
+	prNumber: string
+) => {
+	const { owner, name } = options;
+	const prData = await getPullRequest( { owner, name, prNumber } );
+	const isCommunityPR = isCommunityPullRequest( prData, owner, name );
+	const headOwner = isCommunityPR ? prData.head.repo.owner.login : owner;
+	const branch = prData.head.ref;
+	const fileName = branch.replace( /\//g, '-' );
+	const prBody = prData.body;
+	const head = prData.head.sha;
+	const base = prData.base.sha;
+	return {
+		prBody,
+		isCommunityPR,
+		headOwner,
+		branch,
+		fileName,
+		head,
+		base,
+	};
+};
+
+/**
+ * Determine if a pull request description activates the changelog automation.
+ *
+ * @param {string} body pull request description.
+ * @return {boolean} if the pull request description activates the changelog automation.
+ */
+export const shouldAutomateChangelog = ( body: string ) => {
+	const regex =
+		/\[x\] Automatically create a changelog entry from the details/gm;
+	return regex.test( body );
+};
+
+/**
+ * Get the changelog significance from a pull request description.
+ *
+ * @param {string} body pull request description.
+ * @return {void|string} changelog significance.
+ */
+export const getChangelogSignificance = ( body: string ) => {
+	const regex = /\[x\] (Patch|Minor|Major)\r\n/gm;
+	const matches = body.match( regex );
+
+	if ( matches === null ) {
+		Logger.error( 'No changelog significance found' );
+		// Logger.error has a process.exit( 1 ) call, this return is purely for testing purposes.
+		return;
+	}
+
+	if ( matches.length > 1 ) {
+		Logger.error(
+			'Multiple changelog significances found. Only one can be entered'
+		);
+		// Logger.error has a process.exit( 1 ) call, this return is purely for testing purposes.
+		return;
+	}
+	const significance = regex.exec( body );
+
+	return significance[ 1 ].toLowerCase();
+};
+
+/**
+ * Get the changelog type from a pull request description.
+ *
+ * @param {string} body pull request description.
+ * @return {void|string} changelog type.
+ */
+export const getChangelogType = ( body: string ) => {
+	const regex =
+		/\[x\] (Fix|Add|Update|Dev|Tweak|Performance|Enhancement) -/gm;
+	const matches = body.match( regex );
+
+	if ( matches === null ) {
+		Logger.error( 'No changelog type found' );
+		// Logger.error has a process.exit( 1 ) call, this return is purely for testing purposes.
+		return;
+	}
+
+	if ( matches.length > 1 ) {
+		Logger.error(
+			'Multiple changelog types found. Only one can be entered'
+		);
+		// Logger.error has a process.exit( 1 ) call, this return is purely for testing purposes.
+		return;
+	}
+
+	const type = regex.exec( body );
+	return type[ 1 ].toLowerCase();
+};
+
+/**
+ * Get the changelog message from a pull request description.
+ *
+ * @param {string} body pull request description.
+ * @return {void|string} changelog message.
+ */
+export const getChangelogMessage = ( body: string ) => {
+	const messageRegex = /#### Message\r\n(<!--(.*)-->)?(.*)#### Comment/gms;
+	const match = messageRegex.exec( body );
+
+	if ( ! match ) {
+		Logger.error( 'No changelog message found' );
+	}
+
+	return match[ 3 ].trim();
+};
+
+/**
+ * Get the changelog comment from a pull request description.
+ *
+ * @param {string} body pull request description.
+ * @return {void|string} changelog comment.
+ */
+export const getChangelogComment = ( body: string ) => {
+	const commentRegex = /#### Comment\r\n(<!--(.*)-->)?(.*)<\/details>/gms;
+	const match = commentRegex.exec( body );
+
+	return match ? match[ 3 ].trim() : '';
+};
+
+export const getChangelogDetails = ( body: string ) => {
+	const message = getChangelogMessage( body );
+	const comment = getChangelogComment( body );
+
+	if ( comment && message ) {
+		Logger.error(
+			'Both a message and comment were found. Only one can be entered'
+		);
+		// Logger.error has a process.exit( 1 ) call, this return is purely for testing purposes.
+		return;
+	}
+
+	const significance = getChangelogSignificance( body );
+
+	if ( comment && significance !== 'patch' ) {
+		Logger.error(
+			'Only patch changes can have a comment. Please change the significance to patch or remove the comment'
+		);
+		// Logger.error has a process.exit( 1 ) call, this return is purely for testing purposes.
+		return;
+	}
+
+	return {
+		significance,
+		type: getChangelogType( body ),
+		message,
+		comment,
+	};
+};
