2904b28a91bf9428625870c5a61b9536ca12b0cd
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2023-05-31 00:07:35 +0100

    Improve the prompt of product title suggestions (#38499)
    Co-authored-by: Joel <dygerati@gmail.com>

--- plugins/woo-ai/includes/product-data-suggestion/class-product-data-suggestion-prompt-generator.php
+++ plugins/woo-ai/includes/product-data-suggestion/class-product-data-suggestion-prompt-generator.php
@@ -19,33 +19,14 @@ defined( 'ABSPATH' ) || exit;
 class Product_Data_Suggestion_Prompt_Generator {
 	private const PROMPT_TEMPLATE = <<<PROMPT_TEMPLATE
 You are a SEO and marketing expert specializing in e-commerce stores built using WooCommerce.
-
 You are given the product's name, description, tags, categories, and other attributes.
 Your task is to provide three optimized alternatives to the product's %s to enhance the online store's SEO performance and sales.
-You will provide the best possible option for the product's %s based on the product properties provided.
-
-Return only the optimized alternative value for product's %s in the "content" part of your JSON response.
-Return a short and concise reason for each suggestion in seven words in the "reason" part of your JSON response.
+You provide the best possible option for the product's %s based on the product properties.
+Return only the optimized alternative value for product's %s in the "content" part of your response.
+Return a short and concise reason for each suggestion in seven words in the "reason" part of your response.
 The product's properties are:
-
 %s
 PROMPT_TEMPLATE;
-
-	const EXAMPLE_JSON_RESPONSE_TEMPLATE = <<<EXAMPLE_JSON_RESPONSE_TEMPLATE
-{
-    "suggestions": [
-        {
-            "content": "An improved alternative to the product's %s",
-            "reason": "First concise reason why this %s helps the SEO and sales of the product."
-        },
-        {
-            "content": "Another improved alternative to the product's %s",
-            "reason": "Second concise reason this %s helps the SEO and sales of the product."
-        }
-    ]
-}
-EXAMPLE_JSON_RESPONSE_TEMPLATE;
-
 
 	/**
 	 * The product category formatter.
@@ -71,9 +52,9 @@ EXAMPLE_JSON_RESPONSE_TEMPLATE;
 	/**
 	 * Product_Data_Suggestion_Prompt_Generator constructor.
 	 *
-	 * @param Product_Category_Formatter  $product_category_formatter The product category formatter.
+	 * @param Product_Category_Formatter  $product_category_formatter  The product category formatter.
 	 * @param Product_Attribute_Formatter $product_attribute_formatter The product attribute formatter.
-	 * @param Json_Request_Formatter      $json_request_formatter     The JSON request formatter.
+	 * @param Json_Request_Formatter      $json_request_formatter      The JSON request formatter.
 	 */
 	public function __construct( Product_Category_Formatter $product_category_formatter, Product_Attribute_Formatter $product_attribute_formatter, Json_Request_Formatter $json_request_formatter ) {
 		$this->product_category_formatter  = $product_category_formatter;
@@ -99,16 +80,8 @@ EXAMPLE_JSON_RESPONSE_TEMPLATE;
 			$request_prompt
 		);
 
-		$json_example = sprintf(
-			self::EXAMPLE_JSON_RESPONSE_TEMPLATE,
-			$request->requested_data,
-			$request->requested_data,
-			$request->requested_data,
-			$request->requested_data,
-		);
-
 		// Append the JSON request prompt.
-		$prompt .= "\n" . $this->json_request_formatter->format( $json_example );
+		$prompt .= "\n" . $this->get_example_response( $request );
 
 		return $prompt;
 	}
@@ -160,4 +133,29 @@ EXAMPLE_JSON_RESPONSE_TEMPLATE;
 
 		return $request_prompt;
 	}
+
+	/**
+	 * Get an example response for the request.
+	 *
+	 * @param Product_Data_Suggestion_Request $request The request to build the example response for.
+	 *
+	 * @return string
+	 */
+	private function get_example_response( Product_Data_Suggestion_Request $request ): string {
+		$response_array = array(
+			'suggestions' => array(
+				array(
+					'content' => sprintf( 'An improved alternative to the product\'s %s', $request->requested_data ),
+					'reason'  => sprintf( 'First concise reason why this %s helps the SEO and sales of the product.', $request->requested_data ),
+				),
+				array(
+					'content' => sprintf( 'Another improved alternative to the product\'s %s', $request->requested_data ),
+					'reason'  => sprintf( 'Second concise reason this %s helps the SEO and sales of the product.', $request->requested_data ),
+				),
+			),
+		);
+
+		return $this->json_request_formatter->format( $response_array );
+	}
+
 }
