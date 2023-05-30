b076a7b521499e5676b21b4bff944f05856a4f3f
Sam Seay <samueljseay@gmail.com>
2023-05-31 11:45:10 +1200

    Fix linter issues and update Syncpack (#38523)

--- packages/js/eslint-plugin/configs/recommended.js
+++ packages/js/eslint-plugin/configs/recommended.js
@@ -16,6 +16,8 @@ module.exports = {
 		radix: 'error',
 		yoda: [ 'error', 'never' ],
 		// temporary conversion to warnings until the below are all handled.
+		'jsdoc/check-line-alignment': 'warn',
+		'jsdoc/require-returns-check': 'warn',
 		'@wordpress/i18n-translator-comments': 'warn',
 		'@wordpress/i18n-text-domain': [
 			'error',
