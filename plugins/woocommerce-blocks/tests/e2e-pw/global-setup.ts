acb5ab4bdb86bcfb52342aa2717464e03a4d8960
Roy Ho <roykho77@gmail.com>
2023-06-01 07:14:50 -0700

    Add e2e tests for attributes count (https://github.com/woocommerce/woocommerce-blocks/pull/9500)
    * Add e2e tests for attributes count
    
    * Add test with pricing filter and turn on debug to prevent cache
    
    * Prevent tests from passing if test page is not loaded
    
    * Use WP wrapper to call WC CLI
    
    * Refactor to use more of PW methods
    
    * Use existing active filters block post for testing
    
    * Move prepareAttributes function to global setup

--- plugins/woocommerce-blocks/tests/e2e-pw/global-setup.ts
+++ plugins/woocommerce-blocks/tests/e2e-pw/global-setup.ts
@@ -5,17 +5,17 @@
 import { FullConfig, chromium, request } from '@playwright/test';
 import { RequestUtils } from '@wordpress/e2e-test-utils-playwright';
 import fs from 'fs';
+import { cli } from '@woocommerce/e2e-utils';
 
 /**
  * Internal dependencies
  */
-import { customer } from './test-data/data/data';
+import { customer, admin } from './test-data/data/data';
 
 const loginAsCustomer = async ( config: FullConfig ) => {
 	const { stateDir, baseURL, userAgent } = config.projects[ 0 ].use;
 
 	// used throughout tests for authentication
-	process.env.ADMINSTATE = `${ stateDir }adminState.json`;
 	process.env.CUSTOMERSTATE = `${ stateDir }customerState.json`;
 
 	try {
@@ -75,6 +75,52 @@ const loginAsCustomer = async ( config: FullConfig ) => {
 	await browser.close();
 };
 
+const prepareAttributes = async ( config: FullConfig ) => {
+	const { baseURL, userAgent } = config.projects[ 0 ].use;
+
+	// Specify user agent when running against an external test site to avoid getting HTTP 406 NOT ACCEPTABLE errors.
+	const contextOptions = { baseURL, userAgent };
+
+	// Create browser, browserContext, and page for customer and admin users
+	const browser = await chromium.launch();
+	const context = await browser.newContext( contextOptions );
+	const page = await context.newPage();
+
+	await page.goto( `/wp-admin` );
+	await page.fill( 'input[name="log"]', admin.username );
+	await page.fill( 'input[name="pwd"]', admin.password );
+	await page.click( 'text=Log In' );
+
+	/*
+	 * Intercept the dialog event.
+	 * This is needed because when the regenerate
+	 * button is clicked, a dialog is shown.
+	 */
+	page.on( 'dialog', async ( dialog ) => {
+		await dialog.accept();
+	} );
+
+	await page.goto( '/wp-admin/admin.php?page=wc-status&tab=tools' );
+
+	await page.click( '.regenerate_product_attributes_lookup_table input' );
+
+	await context.close();
+	await browser.close();
+
+	/*
+	 * Note that the two commands below are intentionally
+	 * duplicated as we need to run the cron task twice as
+	 * we need to process more than 1 batch of items.
+	 */
+	await cli(
+		`npm run wp-env run tests-cli wp action-scheduler run --hooks="woocommerce_run_product_attribute_lookup_regeneration_callback"`
+	);
+
+	await cli(
+		`npm run wp-env run tests-cli wp action-scheduler run --hooks="woocommerce_run_product_attribute_lookup_regeneration_callback"`
+	);
+};
+
 async function globalSetup( config: FullConfig ) {
 	const { storageState, baseURL } = config.projects[ 0 ].use;
 	const storageStatePath =
@@ -91,6 +137,7 @@ async function globalSetup( config: FullConfig ) {
 	await requestUtils.setupRest();
 	await requestContext.dispose();
 
+	await prepareAttributes( config );
 	await loginAsCustomer( config );
 }
 
