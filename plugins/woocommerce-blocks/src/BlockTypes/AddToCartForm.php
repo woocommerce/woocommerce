8bf316b572c0ba227389a6cef28a2306ff5c0d21
Alexandre Lara <allexandrelara@gmail.com>
2023-06-02 11:29:22 -0300

    Add to Cart Form block: Prevent notice from appearing after clicking on Add to Cart button inside Single Product block (https://github.com/woocommerce/woocommerce-blocks/pull/9685)
    * Add isDescendentOfSingleProductBlock attribute to Add to Cart Form block
    
    * Prevent notice from appearing when Add to Cart button is clicked
    
    When the Add to Cart Form is added inside the Single Product Block, we
    have to prevent the notice from appearing when the Add to Cart button is
    clicked.
    
    * Fix PHP CS errors
    
    * Fix PHP CS errors
    
    * Fix PHP CS errors
    
    * Fix PHP CS errors
    
    * Fix PHP CS errors
    
    * Fix PHP CS errors
    
    * Add phpcs:ignore WordPress.Security.NonceVerification.Recommended
    
    * Fix Processing form data without nonce verification.
    
    * PHP Ignore
    
    * Improve doc comments
    
    * Improve name for add_to_cart_message_html_filter hook
    
    * Replace equal operator with identity operator in the conditional statement

--- plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
+++ plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
@@ -16,6 +16,33 @@ class AddToCartForm extends AbstractBlock {
 	protected $block_name = 'add-to-cart-form';
 
 	/**
+	 * Initializes the AddToCartForm block and hooks into the `wc_add_to_cart_message_html` filter
+	 * to prevent displaying the Cart Notice when the block is inside the Single Product block
+	 * and the Add to Cart button is clicked.
+	 *
+	 * @return void
+	 */
+	protected function initialize() {
+		parent::initialize();
+		add_filter( 'wc_add_to_cart_message_html', array( $this, 'add_to_cart_message_html_filter' ), 10, 2 );
+	}
+
+	/**
+	 * Get the block's attributes.
+	 *
+	 * @param array $attributes Block attributes. Default empty array.
+	 * @return array  Block attributes merged with defaults.
+	 */
+	private function parse_attributes( $attributes ) {
+		// These should match what's set in JS `registerBlockType`.
+		$defaults = array(
+			'isDescendentOfSingleProductBlock' => false,
+		);
+
+		return wp_parse_args( $attributes, $defaults );
+	}
+
+	/**
 	 * Render the block.
 	 *
 	 * @param array    $attributes Block attributes.
@@ -42,6 +69,7 @@ class AddToCartForm extends AbstractBlock {
 		}
 
 		ob_start();
+
 		/**
 		 * Trigger the single product add to cart action for each product type.
 		*
@@ -57,6 +85,10 @@ class AddToCartForm extends AbstractBlock {
 			return '';
 		}
 
+		$parsed_attributes = $this->parse_attributes( $attributes );
+		$is_descendent_of_single_product_block = $parsed_attributes['isDescendentOfSingleProductBlock'];
+		$product = $this->add_is_descendent_of_single_product_block_hidden_input_to_product_form( $product, $is_descendent_of_single_product_block );
+
 		$classname          = $attributes['className'] ?? '';
 		$classes_and_styles = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes );
 
@@ -71,6 +103,45 @@ class AddToCartForm extends AbstractBlock {
 		$product = $previous_product;
 
 		return $form;
+	}
+
+	/**
+	 * Add a hidden input to the Add to Cart form to indicate that it is a descendent of a Single Product block.
+	 *
+	 * @param string $product The Add to Cart Form HTML.
+	 * @param string $is_descendent_of_single_product_block Indicates if block is descendent of Single Product block.
+	 *
+	 * @return string The Add to Cart Form HTML with the hidden input.
+	 */
+	protected function add_is_descendent_of_single_product_block_hidden_input_to_product_form( $product, $is_descendent_of_single_product_block ) {
+
+		$hidden_is_descendent_of_single_product_block_input = sprintf(
+			'<input type="hidden" name="is-descendent-of-single-product-block" value="%1$s">',
+			$is_descendent_of_single_product_block ? 'true' : 'false'
+		);
+		$regex_pattern = '/<button\s+type="submit"[^>]*>.*?<\/button>/i';
+
+		preg_match( $regex_pattern, $product, $input_matches );
+
+		if ( ! empty( $input_matches ) ) {
+			$product = preg_replace( $regex_pattern, $hidden_is_descendent_of_single_product_block_input . $input_matches[0], $product );
+		}
+
+		return $product;
+	}
+
+	/**
+	 * Filter the add to cart message to prevent the Notice from being displayed when the Add to Cart form is a descendent of a Single Product block
+	 * and the Add to Cart button is clicked.
+	 *
+	 * @param string $message Message to be displayed when product is added to the cart.
+	 */
+	public function add_to_cart_message_html_filter( $message ) {
+		// phpcs:ignore
+		if ( isset( $_POST['is-descendent-of-single-product-block'] ) && 'true' === $_POST['is-descendent-of-single-product-block'] ) {
+			return false;
+		}
+		return $message;
 	}
 
 	/**
