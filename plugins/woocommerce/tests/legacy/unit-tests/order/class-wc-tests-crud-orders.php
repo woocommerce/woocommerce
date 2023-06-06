baddaffa497db70de7e928d5a0c9cd785cac68f2
Dimitrios Pantazis <dpanta94@gmail.com>
2023-06-06 12:44:46 +0300

    Added Unit Tests

--- plugins/woocommerce/tests/legacy/unit-tests/order/class-wc-tests-crud-orders.php
+++ plugins/woocommerce/tests/legacy/unit-tests/order/class-wc-tests-crud-orders.php
@@ -611,6 +611,158 @@ class WC_Tests_CRUD_Orders extends WC_Unit_Test_Case {
 		$this->assertEquals( 20, $object->get_shipping_total() );
 	}
 
+	public function test_calculate_shipping_with_shipping_classes() {
+		// Create shipping classes.
+		$shipping_class_1 = wp_insert_term( 'New York', 'product_shipping_class' );
+		$shipping_class_2 = wp_insert_term( 'Florida', 'product_shipping_class' );
+		$shipping_class_3 = wp_insert_term( 'California', 'product_shipping_class' );
+		$shipping_class_4 = wp_insert_term( 'Illinois', 'product_shipping_class' );
+
+		// Create products and assign to each one of them a different shipping class.
+		$product_1 = WC_Helper_Product::create_simple_product( false, array( 'name' => 'Test Shipping Product 1', 'sku' => 'TSSKU1' ) );
+		$product_1->set_shipping_class_id( $shipping_class_1['term_id'] );
+		$product_1->save();
+
+		$product_2 = WC_Helper_Product::create_simple_product( false, array( 'name' => 'Test Shipping Product 2', 'sku' => 'TSSKU2' ) );
+		$product_2->set_shipping_class_id( $shipping_class_2['term_id'] );
+		$product_2->save();
+
+		$product_3 = WC_Helper_Product::create_simple_product( false, array( 'name' => 'Test Shipping Product 3', 'sku' => 'TSSKU3' ) );
+		$product_3->set_shipping_class_id( $shipping_class_3['term_id'] );
+		$product_3->save();
+
+		$product_4 = WC_Helper_Product::create_simple_product( false, array( 'name' => 'Test Shipping Product 4', 'sku' => 'TSSKU4' ) );
+		$product_4->set_shipping_class_id( $shipping_class_4['term_id'] );
+		$product_4->save();
+
+		// Create 3 shipping zones. 2 will have one flat rate each while the last will have 2 flat rates.
+		$zone_1 = new WC_Shipping_Zone();
+		$zone_1->set_zone_name( 'New York' );
+		$zone_1->set_zone_order( 1 );
+		$zone_1->add_location( 'US:NY', 'state' );
+		$instance_id_1 = $zone_1->add_shipping_method( 'flat_rate' );
+		$zone_1->save();
+
+		$zone_2 = new WC_Shipping_Zone();
+		$zone_2->set_zone_name( 'Florida' );
+		$zone_2->set_zone_order( 2 );
+		$zone_2->add_location( 'US:FL', 'state' );
+		$instance_id_2 = $zone_2->add_shipping_method( 'flat_rate' );
+		$zone_2->save();
+
+		$zone_3 = new WC_Shipping_Zone();
+		$zone_3->set_zone_name( 'California and Illinois' );
+		$zone_3->set_zone_order( 3 );
+		$zone_3->add_location( 'US:CA', 'state' );
+		$zone_3->add_location( 'US:IL', 'state' );
+		$instance_id_3 = $zone_3->add_shipping_method( 'flat_rate' );
+		$instance_id_4 = $zone_3->add_shipping_method( 'flat_rate' );
+		$zone_3->save();
+
+		$map = array(
+			$shipping_class_1['term_id'] => $instance_id_1, // NY
+			$shipping_class_2['term_id'] => $instance_id_2, // FL
+			$shipping_class_3['term_id'] => $instance_id_3, // CA
+			$shipping_class_4['term_id'] => $instance_id_4, // IL
+		);
+
+		// 2 every flat rate add no cost by default and configure the cost for only one of the shipping classes. Different to each flat rate.
+		foreach ( $map as $shipping_class_id => $instance_id ) {
+			$data = array(
+				'woocommerce_flat_rate_title'                                      => 'Flat rate',
+				'woocommerce_flat_rate_tax_status'                                 => 'taxable',
+				'woocommerce_flat_rate_cost'                                       => '',
+				'woocommerce_flat_rate_class_cost_' . $shipping_class_1['term_id'] => '',
+				'woocommerce_flat_rate_class_cost_' . $shipping_class_2['term_id'] => '',
+				'woocommerce_flat_rate_class_cost_' . $shipping_class_3['term_id'] => '',
+				'woocommerce_flat_rate_class_cost_' . $shipping_class_4['term_id'] => '',
+				'woocommerce_flat_rate_type'                                       => 'class',
+				'instance_id'                                                      => $instance_id,
+			);
+
+			$data['woocommerce_flat_rate_class_cost_' . $shipping_class_id ] = $shipping_class_id * 10;
+
+			$shipping_method = WC_Shipping_Zones::get_shipping_method( $instance_id );
+			$shipping_method->set_post_data( wp_unslash( $data ) ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+			$_REQUEST['instance_id'] = $instance_id;
+			$shipping_method->process_admin_options();
+		}
+
+		WC_Cache_Helper::get_transient_version( 'shipping', true );
+		// Set up has finished. Now we need to imitate the different scenarios.
+
+		// Set customer location to US. NY by default.
+		WC_Helper_Shipping::force_customer_us_address();
+
+		WC()->cart->empty_cart();
+
+		WC()->cart->add_to_cart( $product_1->get_id(), 1 );
+
+		WC()->cart->calculate_shipping();
+
+		// Customers address in in NY and product_1 is shippable to NY.
+		$this->assertEquals( (string) ( 10 * $shipping_class_1['term_id'] ), WC()->cart->get_shipping_total() );
+
+		WC()->cart->add_to_cart( $product_2->get_id(), 1 );
+
+		WC()->cart->calculate_shipping();
+
+		// product_2 is not shippable to NY.
+		$this->assertEquals( '0', WC()->cart->get_shipping_total() );
+
+		// Set customer location to FL.
+		add_filter( 'woocommerce_customer_get_shipping_state', function () {
+			return 'FL';
+		}, 11 );
+
+		WC()->cart->empty_cart();
+
+		WC()->cart->add_to_cart( $product_2->get_id(), 1 );
+
+		WC()->cart->calculate_shipping();
+
+		$this->assertEquals( (string) ( 10 * $shipping_class_2['term_id'] ), WC()->cart->get_shipping_total() );
+
+		WC()->cart->add_to_cart( $product_1->get_id(), 1 );
+
+		WC()->cart->calculate_shipping();
+
+		// product_1 is not shippable to FL.
+		$this->assertEquals( '0', WC()->cart->get_shipping_total() );
+
+		// Set customer location to CA.
+		add_filter( 'woocommerce_customer_get_shipping_state', function () {
+			return 'CA';
+		}, 12 );
+
+		WC()->cart->empty_cart();
+
+		WC()->cart->add_to_cart( $product_3->get_id(), 1 );
+
+		WC()->cart->calculate_shipping();
+
+		$this->assertEquals( (string) ( 10 * $shipping_class_3['term_id'] ), WC()->cart->get_shipping_total() );
+
+		// Set customer location to IL.
+		add_filter( 'woocommerce_customer_get_shipping_state', function () {
+			return 'IL';
+		}, 13 );
+
+		WC()->cart->calculate_shipping();
+
+		// product 3 is shippable to illinois and california with a different rate.
+		$this->assertEquals( (string) ( 10 * $shipping_class_4['term_id'] ), WC()->cart->get_shipping_total() );
+
+		WC()->cart->add_to_cart( $product_4->get_id(), 1 );
+
+		WC()->cart->calculate_shipping();
+
+		// product_4 cant be combined with product_3. Each flat rate has cost only for one of those classes.
+		$this->assertEquals( '0', WC()->cart->get_shipping_total() );
+
+		WC_Helper_Shipping_Zones::remove_mock_zones();
+	}
+
 	/**
 	 * Test: calculate_taxes
 	 */
