fabeed032dc6087dc8b35492723a9976c8162dca
Joel <dygerati@gmail.com>
2023-06-07 14:10:31 -0700

    Fixing linting error

--- plugins/woo-ai/includes/product-data-suggestion/class-product-data-suggestion-service.php
+++ plugins/woo-ai/includes/product-data-suggestion/class-product-data-suggestion-service.php
@@ -58,7 +58,7 @@ class Product_Data_Suggestion_Service {
 		$arguments = array(
 			'content'    => $this->prompt_generator->get_user_prompt( $request ),
 			'skip_cache' => true,
-			'feature'    => 'woo-ai-plugin'
+			'feature'    => 'woo-ai-plugin',
 		);
 
 		try {
