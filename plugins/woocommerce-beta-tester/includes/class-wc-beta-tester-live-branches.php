2ef73bbd4d3fb7ac8ba3d0156db085c3ea482d1d
Sam Seay <samueljseay@gmail.com>
2023-06-12 11:53:11 +0800

    Go back to admin_init so that our JS runs before hooks are run.

--- plugins/woocommerce-beta-tester/includes/class-wc-beta-tester-live-branches.php
+++ plugins/woocommerce-beta-tester/includes/class-wc-beta-tester-live-branches.php
@@ -16,7 +16,7 @@ class WC_Beta_Tester_Live_Branches {
 	 */
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'register_page' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'register_scripts' ) );
+		add_action( 'admin_init', array( $this, 'register_scripts' ) );
 	}
 
 	/**
