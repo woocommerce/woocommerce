e1b4f5899af6db5e4c1e9144b6143b92542f305d
Fernando Marichal <ultimoround@gmail.com>
2023-06-11 21:12:13 -0300

    Add styles and js

--- packages/js/product-editor/src/components/feedback-bar/feedback-bar.tsx
+++ packages/js/product-editor/src/components/feedback-bar/feedback-bar.tsx
@@ -2,7 +2,12 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
+import {
+	BaseControl,
+	Button,
+	TextControl,
+	TextareaControl,
+} from '@wordpress/components';
 import {
 	createElement,
 	createInterpolateElement,
@@ -38,6 +43,12 @@ export function FeedbackBar( { product }: FeedbackBarProps ) {
 		return tracksProps;
 	};
 
+	const isValidEmail = ( email: string ) => {
+		const re =
+			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test( String( email ).toLowerCase() );
+	};
+
 	const onShareFeedbackClick = () => {
 		recordEvent( 'product_editor_feedback_bar_share_feedback_click', {
 			...getProductTracksProps(),
@@ -46,8 +57,9 @@ export function FeedbackBar( { product }: FeedbackBarProps ) {
 		showCesModal(
 			{
 				action: PRODUCT_EDITOR_FEEDBACK_CES_ACTION,
+				showDescription: false,
 				title: __(
-					"How's your experience with the product editor?",
+					"How's your experience with the new product form?",
 					'woocommerce'
 				),
 				firstQuestion: __(
@@ -55,14 +67,116 @@ export function FeedbackBar( { product }: FeedbackBarProps ) {
 					'woocommerce'
 				),
 				secondQuestion: __(
-					"The product editing screen's functionality meets my needs",
+					'Product form is easy to use',
 					'woocommerce'
 				),
 				onsubmitLabel: __(
 					"Thanks for the feedback. We'll put it to good use!",
 					'woocommerce'
 				),
-				shouldShowComments: () => true,
+				shouldShowComments: () => false,
+				extraFields: (
+					values: {
+						email?: string;
+						additionalThoughts?: string;
+						errors?: { [ key: string ]: string };
+					},
+					setValues: ( value: {
+						email?: string;
+						additionalThoughts?: string;
+						errors?: { [ key: string ]: string };
+					} ) => void
+				) => (
+					<Fragment>
+						<BaseControl
+							id={ 'feedback_additional_thoughts' }
+							className="woocommerce-product-feedback__additional-thoughts"
+							label={ createInterpolateElement(
+								__(
+									'ADDITIONAL THOUGHTS <optional />',
+									'woocommerce'
+								),
+								{
+									optional: (
+										<span className="woocommerce-product-feedback__optional-input">
+											{ __(
+												'(OPTIONAL)',
+												'woocommerce'
+											) }
+										</span>
+									),
+								}
+							) }
+						>
+							<TextareaControl
+								value={ values.additionalThoughts || '' }
+								onChange={ ( value: string ) =>
+									setValues( {
+										...values,
+										additionalThoughts: value,
+									} )
+								}
+								help={ values.errors?.additionalThoughts || '' }
+							/>
+						</BaseControl>
+						<BaseControl
+							id={ 'feedback_email' }
+							className="woocommerce-product-feedback__email"
+							label={ createInterpolateElement(
+								__(
+									'YOUR EMAIL ADDRESS <optional />',
+									'woocommerce'
+								),
+								{
+									optional: (
+										<span className="woocommerce-product-feedback__optional-input">
+											{ __(
+												'(OPTIONAL)',
+												'woocommerce'
+											) }
+										</span>
+									),
+								}
+							) }
+						>
+							<TextControl
+								value={ values.email || '' }
+								onChange={ ( value: string ) =>
+									setValues( { ...values, email: value } )
+								}
+								help={ values.errors?.email || '' }
+							/>
+							<span>
+								{ __(
+									'In case you want to participate in further discussion and future user research.',
+									'woocommerce'
+								) }
+							</span>
+						</BaseControl>
+					</Fragment>
+				),
+				validateExtraFields: ( {
+					email = '',
+					additionalThoughts = '',
+				}: {
+					email?: string;
+					additionalThoughts?: string;
+				} ) => {
+					const errors: { [ key: string ]: string } = {};
+					if ( email.length > 0 && ! isValidEmail( email ) ) {
+						errors.email = __(
+							'Please enter a valid email address.',
+							'woocommerce'
+						);
+					}
+					if ( additionalThoughts?.length > 500 ) {
+						errors.additionalThoughts = __(
+							'Please enter no more than 500 characters.',
+							'woocommerce'
+						);
+					}
+					return errors;
+				},
 			},
 			{},
 			{
@@ -92,44 +206,48 @@ export function FeedbackBar( { product }: FeedbackBarProps ) {
 
 	return (
 		<>
-			{ shouldShowFeedbackBar && (
-				<WooFooterItem>
-					<div className="woocommerce-product-mvp-ces-footer">
-						<Pill>Beta</Pill>
-						<div className="woocommerce-product-mvp-ces-footer__message">
-							{ createInterpolateElement(
-								__(
-									'How is your experience with the new product form? <span><shareButton>Share feedback</shareButton> or <turnOffButton>turn it off</turnOffButton></span>',
+			{ shouldShowFeedbackBar ||
+				( true && (
+					<WooFooterItem>
+						<div className="woocommerce-product-mvp-ces-footer">
+							<Pill>Beta</Pill>
+							<div className="woocommerce-product-mvp-ces-footer__message">
+								{ createInterpolateElement(
+									__(
+										'How is your experience with the new product form? <span><shareButton>Share feedback</shareButton> or <turnOffButton>turn it off</turnOffButton></span>',
+										'woocommerce'
+									),
+									{
+										span: (
+											<span className="woocommerce-product-mvp-ces-footer__message-buttons" />
+										),
+										shareButton: (
+											<Button
+												variant="link"
+												onClick={ onShareFeedbackClick }
+											/>
+										),
+										turnOffButton: (
+											<Button
+												onClick={ onTurnOffEditorClick }
+												variant="link"
+											/>
+										),
+									}
+								) }
+							</div>
+							<Button
+								className="woocommerce-product-mvp-ces-footer__close-button"
+								icon={ closeSmall }
+								label={ __(
+									'Hide this message',
 									'woocommerce'
-								),
-								{
-									span: (
-										<span className="woocommerce-product-mvp-ces-footer__message-buttons" />
-									),
-									shareButton: (
-										<Button
-											variant="link"
-											onClick={ onShareFeedbackClick }
-										/>
-									),
-									turnOffButton: (
-										<Button
-											onClick={ onTurnOffEditorClick }
-											variant="link"
-										/>
-									),
-								}
-							) }
+								) }
+								onClick={ onHideFeedbackBarClick }
+							></Button>
 						</div>
-						<Button
-							className="woocommerce-product-mvp-ces-footer__close-button"
-							icon={ closeSmall }
-							label={ __( 'Hide this message', 'woocommerce' ) }
-							onClick={ onHideFeedbackBarClick }
-						></Button>
-					</div>
-				</WooFooterItem>
-			) }
+					</WooFooterItem>
+				) ) }
 		</>
 	);
 }
