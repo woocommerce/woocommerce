63fa78e5e6bd316b3b94bd3bd2a76b057c15bb95
moon <moon.kyong@automattic.com>
2023-06-12 20:37:34 -0700

    Add installed_ext_success=1

--- plugins/woocommerce-admin/client/core-profiler/index.tsx
+++ plugins/woocommerce-admin/client/core-profiler/index.tsx
@@ -1206,7 +1206,8 @@ export const coreProfilerStateMachineDefinition = createMachine( {
 						onDone: {
 							actions: [
 								( _context, event ) => {
-									// window.location.href = event.data;
+									window.location.href =
+										event.data + '&installed_ext_success=1';
 								},
 							],
 						},
