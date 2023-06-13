e024c3f0835e2fdce25e6d98c7ecb74f07116ca6
Nathan Schneider <nsschneider1@gmail.com>
2023-06-13 09:55:01 -0300

    Show the guide when clicking the menu item

--- plugins/woocommerce-admin/client/products/fills/more-menu-items/about-the-editor-menu-item.tsx
+++ plugins/woocommerce-admin/client/products/fills/more-menu-items/about-the-editor-menu-item.tsx
@@ -4,15 +4,38 @@
 import { __ } from '@wordpress/i18n';
 import { MenuItem } from '@wordpress/components';
 import { info, Icon } from '@wordpress/icons';
+import { useState } from '@wordpress/element';
 
-export const AboutTheEditorMenuItem = () => {
+/**
+ * Internal dependencies
+ */
+import BlockEditorGuide from '~/products/tour/block-editor/block-editor-guide';
+
+export const AboutTheEditorMenuItem = ( {
+	onClose,
+}: {
+	onClose: () => void;
+} ) => {
+	const [ isGuideOpen, setIsGuideOpen ] = useState( false );
 	return (
-		<MenuItem
-			onClick={ () => {} }
-			icon={ <Icon icon={ info } /> }
-			iconPosition="right"
-		>
-			{ __( 'About the editor…', 'woocommerce' ) }
-		</MenuItem>
+		<>
+			<MenuItem
+				onClick={ () => {
+					setIsGuideOpen( true );
+				} }
+				icon={ <Icon icon={ info } /> }
+				iconPosition="right"
+			>
+				{ __( 'About the editor…', 'woocommerce' ) }
+			</MenuItem>
+			{ isGuideOpen && (
+				<BlockEditorGuide
+					onCloseGuide={ () => {
+						setIsGuideOpen( false );
+						onClose();
+					} }
+				/>
+			) }
+		</>
 	);
 };
