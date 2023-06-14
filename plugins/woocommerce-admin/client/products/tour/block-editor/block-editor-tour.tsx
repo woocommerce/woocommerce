2f2b4bc883c633afeced43f643df883ab673cb8b
Matt Sherman <matt@jam123.com>
2023-06-14 14:14:59 -0400

    Implement new user check

--- plugins/woocommerce-admin/client/products/tour/block-editor/block-editor-tour.tsx
+++ plugins/woocommerce-admin/client/products/tour/block-editor/block-editor-tour.tsx
@@ -1,11 +1,13 @@
 /**
  * External dependencies
  */
-import { Pill, TourKit } from '@woocommerce/components';
-import { __ } from '@wordpress/i18n';
-import { recordEvent } from '@woocommerce/tracks';
+import { useSelect } from '@wordpress/data';
 import { useEffect, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { Pill, TourKit } from '@woocommerce/components';
+import { PRODUCTS_STORE_NAME } from '@woocommerce/data';
 import { __experimentalUseFeedbackBar as useFeedbackBar } from '@woocommerce/product-editor';
+import { recordEvent } from '@woocommerce/tracks';
 
 /**
  * Internal dependencies
@@ -19,7 +21,30 @@ interface Props {
 	dismissModal: () => void;
 }
 
+const PUBLISHED_PRODUCTS_QUERY_PARAMS = {
+	status: 'publish',
+	_fields: [ 'id' ],
+};
+
 const BlockEditorTour = ( { shouldTourBeShown, dismissModal }: Props ) => {
+	const { publishedProductsCount, loadingPublishedProductsCount } = useSelect(
+		( select ) => {
+			const { getProductsTotalCount, hasFinishedResolution } =
+				select( PRODUCTS_STORE_NAME );
+
+			return {
+				publishedProductsCount: getProductsTotalCount(
+					PUBLISHED_PRODUCTS_QUERY_PARAMS,
+					0
+				),
+				loadingPublishedProductsCount: ! hasFinishedResolution(
+					'getProductsTotalCount',
+					[ PUBLISHED_PRODUCTS_QUERY_PARAMS, 0 ]
+				),
+			};
+		}
+	);
+
 	useEffect( () => {
 		if ( shouldTourBeShown ) {
 			recordEvent( 'block_product_editor_spotlight_view' );
@@ -30,7 +55,8 @@ const BlockEditorTour = ( { shouldTourBeShown, dismissModal }: Props ) => {
 
 	const { maybeShowFeedbackBar } = useFeedbackBar();
 
-	const isNewUser = true;
+	// we consider a user new if they have no published products
+	const isNewUser = publishedProductsCount < 1;
 
 	const openGuide = () => {
 		setIsGuideOpen( true );
@@ -52,6 +78,10 @@ const BlockEditorTour = ( { shouldTourBeShown, dismissModal }: Props ) => {
 				  ),
 		};
 	};
+
+	if ( loadingPublishedProductsCount ) {
+		return null;
+	}
 
 	if ( isGuideOpen ) {
 		return (
