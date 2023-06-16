95fe7e10be0c71d961ce0434c41578665c8eebc2
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-06-16 16:44:49 +0800

    Add canvas spinner

--- plugins/woocommerce-admin/client/store-customize/editor.jsx
+++ plugins/woocommerce-admin/client/store-customize/editor.jsx
@@ -8,43 +8,47 @@ import { EntityProvider } from '@wordpress/core-data';
 import { GlobalStylesRenderer } from '@wordpress/edit-site/build-module/components/global-styles-renderer';
 import { InterfaceSkeleton } from '@wordpress/interface';
 import useEditedEntityRecord from '@wordpress/edit-site/build-module/components/use-edited-entity-record';
+import CanvasSpinner from '@wordpress/edit-site/build-module/components/canvas-spinner';
 
 /**
  * Internal dependencies
  */
 import BlockEditor from './block-editor';
 
-export const Editor = ( { blocks } ) => {
+export const Editor = ( { blocks, isLoading } ) => {
 	const { record: template } = useEditedEntityRecord();
 	const { id: templateId, type: templateType } = template;
 
 	return (
-		<EntityProvider kind="root" type="site">
-			<EntityProvider
-				kind="postType"
-				type={ templateType }
-				id={ templateId }
-			>
-				<InterfaceSkeleton
-					enableRegionNavigation={ false }
-					className={ classnames(
-						'edit-site-editor__interface-skeleton',
-						{
-							'show-icon-labels': false,
-							'is-loading': false,
+		<>
+			{ isLoading ? <CanvasSpinner /> : null }
+			<EntityProvider kind="root" type="site">
+				<EntityProvider
+					kind="postType"
+					type={ templateType }
+					id={ templateId }
+				>
+					<InterfaceSkeleton
+						enableRegionNavigation={ false }
+						className={ classnames(
+							'edit-site-editor__interface-skeleton',
+							{
+								'show-icon-labels': false,
+								'is-loading': isLoading,
+							}
+						) }
+						content={
+							<>
+								<GlobalStylesRenderer />
+								<BlockEditor
+									blocks={ blocks }
+									template={ template }
+								/>
+							</>
 						}
-					) }
-					content={
-						<>
-							<GlobalStylesRenderer />
-							<BlockEditor
-								blocks={ blocks }
-								template={ template }
-							/>
-						</>
-					}
-				/>
+					/>
+				</EntityProvider>
 			</EntityProvider>
-		</EntityProvider>
+		</>
 	);
 };
