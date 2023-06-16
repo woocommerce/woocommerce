13c52b0da64a71d7b0af0c9badb2766135204a5a
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-06-16 16:31:37 +0800

    Add store customize poc demo

--- plugins/woocommerce-admin/webpack.config.js
+++ plugins/woocommerce-admin/webpack.config.js
@@ -5,8 +5,8 @@ const { get } = require( 'lodash' );
 const path = require( 'path' );
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const CustomTemplatedPathPlugin = require( '@wordpress/custom-templated-path-webpack-plugin' );
-const BundleAnalyzerPlugin = require( 'webpack-bundle-analyzer' )
-	.BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+	require( 'webpack-bundle-analyzer' ).BundleAnalyzerPlugin;
 const MomentTimezoneDataPlugin = require( 'moment-timezone-data-webpack-plugin' );
 const ForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' );
 const ReactRefreshWebpackPlugin = require( '@pmmmwh/react-refresh-webpack-plugin' );
@@ -214,6 +214,11 @@ const webpackConfig = {
 				requestToExternal( request ) {
 					if ( request === '@wordpress/components/build/ui' ) {
 						// The external wp.components does not include ui components, so we need to skip requesting to external here.
+						return null;
+					}
+
+					if ( request.startsWith( '@wordpress/edit-site' ) ) {
+						// The external wp.editSite does not include edit-site components, so we need to skip requesting to external here.
 						return null;
 					}
 				},
