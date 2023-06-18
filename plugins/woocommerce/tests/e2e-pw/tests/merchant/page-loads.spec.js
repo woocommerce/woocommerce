35d3584e110070745c2a722044acaf461167b914
moon <moon.kyong@automattic.com>
2023-06-18 15:01:29 -0700

    Remove test code

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/page-loads.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/page-loads.spec.js
@@ -8,32 +8,32 @@ const wcPages = [
 		name: 'WooCommerce',
 		subpages: [
 			{ name: 'Home', heading: 'Home' },
-			// { name: 'Orders', heading: 'Orders' },
-			// { name: 'Customers', heading: 'Customers' },
-			// { name: 'Coupons', heading: 'Coupons' },
-			// { name: 'Reports', heading: 'Orders' },
-			// { name: 'Settings', heading: 'General' },
-			// { name: 'Status', heading: 'System status' },
+			{ name: 'Orders', heading: 'Orders' },
+			{ name: 'Customers', heading: 'Customers' },
+			{ name: 'Coupons', heading: 'Coupons' },
+			{ name: 'Reports', heading: 'Orders' },
+			{ name: 'Settings', heading: 'General' },
+			{ name: 'Status', heading: 'System status' },
 		],
 	},
-	// {
-	// 	name: 'Products',
-	// 	subpages: [
-	// 		{ name: 'All Products', heading: 'Products' },
-	// 		{ name: 'Add New', heading: 'Add New' },
-	// 		{ name: 'Categories', heading: 'Product categories' },
-	// 		{ name: 'Tags', heading: 'Product tags' },
-	// 		{ name: 'Attributes', heading: 'Attributes' },
-	// 	],
-	// },
-	// // analytics is handled through a separate test
-	// {
-	// 	name: 'Marketing',
-	// 	subpages: [
-	// 		{ name: 'Overview', heading: 'Overview' },
-	// 		{ name: 'Coupons', heading: 'Coupons' },
-	// 	],
-	// },
+	{
+		name: 'Products',
+		subpages: [
+			{ name: 'All Products', heading: 'Products' },
+			{ name: 'Add New', heading: 'Add New' },
+			{ name: 'Categories', heading: 'Product categories' },
+			{ name: 'Tags', heading: 'Product tags' },
+			{ name: 'Attributes', heading: 'Attributes' },
+		],
+	},
+	// analytics is handled through a separate test
+	{
+		name: 'Marketing',
+		subpages: [
+			{ name: 'Overview', heading: 'Overview' },
+			{ name: 'Coupons', heading: 'Coupons' },
+		],
+	},
 ];
 
 for ( const currentPage of wcPages ) {
