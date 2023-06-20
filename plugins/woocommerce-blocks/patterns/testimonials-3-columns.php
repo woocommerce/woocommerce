82a8f6006dcaf870163358b38f7c9f9c54e83854
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-06-20 17:44:02 +0200

    Remove unnecessary white space from Testimonials 3 Columns pattern (https://github.com/woocommerce/woocommerce-blocks/pull/9786)

--- plugins/woocommerce-blocks/patterns/testimonials-3-columns.php
+++ plugins/woocommerce-blocks/patterns/testimonials-3-columns.php
@@ -15,11 +15,7 @@
 		<!-- /wp:heading -->
 
 		<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
-		<p style="font-size:18px">
-			In the end the couch wasn’t exactly what I was looking for but my experience with the
-			Burrow team was excellent. First in providing a discount when the couch was delayed, then timely feedback
-			and updates as the…
-		</p>
+		<p style="font-size:18px">In the end the couch wasn’t exactly what I was looking for but my experience with the Burrow team was excellent. First in providing a discount when the couch was delayed, then timely feedback and updates as the…</p>
 		<!-- /wp:paragraph -->
 
 		<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"},"color":{"text":"#646970"}}} -->
@@ -34,10 +30,7 @@
 		<!-- /wp:heading -->
 
 		<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
-		<p style="font-size:18px">
-			Great couch. color as advertise. seat is nice and firm. Easy to put together.
-			Versatile. Bought one for my mother in law as well. And she loves hers!
-		</p>
+		<p style="font-size:18px">Great couch. color as advertise. seat is nice and firm. Easy to put together. Versatile. Bought one for my mother in law as well. And she loves hers!</p>
 		<!-- /wp:paragraph -->
 
 		<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"},"color":{"text":"#646970"}}} -->
@@ -52,11 +45,7 @@
 		<!-- /wp:heading -->
 
 		<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"}}} -->
-		<p style="font-size:18px">
-			I got the kind sofa. The look and feel is high quality, and I enjoy that it is a
-			medium level of firmness. Assembly took a little longer than I expected, and it came in 4 boxes. I am
-			excited about the time / st…
-		</p>
+		<p style="font-size:18px">I got the kind sofa. The look and feel is high quality, and I enjoy that it is a medium level of firmness. Assembly took a little longer than I expected, and it came in 4 boxes. I am excited about the time / st…</p>
 		<!-- /wp:paragraph -->
 
 		<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"},"color":{"text":"#646970"}}} -->
