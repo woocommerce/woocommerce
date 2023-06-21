071d913b0ff041233087675f7a2062af0dc74233
kidinov <andrey.kidinov@a8c.com>
2023-06-21 15:15:27 +0200

    Added tests

--- plugins/woocommerce/src/Admin/API/Reports/Customers/DataStore.php
+++ plugins/woocommerce/src/Admin/API/Reports/Customers/DataStore.php
@@ -321,11 +321,11 @@ class DataStore extends ReportsDataStore implements DataStoreInterface {
 		);
 
 		if ( ! empty( $query_args['filter_empty'] ) ) {
-			$fields_to_filter_by       = array_intersect( $query_args['filter_empty'], $filter_empty_params );
-            if ( in_array( 'name', $fields_to_filter_by, true ) ) {
-                $fields_to_filter_by   = array_diff( $fields_to_filter_by, array( 'name' ) );
-                $fields_to_filter_by[] = "CONCAT_WS( ' ', first_name, last_name )";
-            }
+			$fields_to_filter_by = array_intersect( $query_args['filter_empty'], $filter_empty_params );
+			if ( in_array( 'name', $fields_to_filter_by, true ) ) {
+				$fields_to_filter_by   = array_diff( $fields_to_filter_by, array( 'name' ) );
+				$fields_to_filter_by[] = "CONCAT_WS( ' ', first_name, last_name )";
+			}
 			$fields_with_not_condition = array_map(
 				function ( $field ) {
 					return $field . ' <> \'\'';
