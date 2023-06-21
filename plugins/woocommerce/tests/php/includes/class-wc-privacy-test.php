9445cdb6930547bbf7fe0c7de5e3f0597e984606
barryhughes <3594411+barryhughes@users.noreply.github.com>
2023-06-20 20:41:10 -0700

    Fix test for customer data clean-up.

--- plugins/woocommerce/tests/php/includes/class-wc-privacy-test.php
+++ plugins/woocommerce/tests/php/includes/class-wc-privacy-test.php
@@ -11,8 +11,8 @@ class WC_Privacy_Test extends WC_Unit_Test_Case {
 	 */
 	public function test_delete_inactive_accounts(): void {
 		$customer = WC_Helper_Customer::create_customer();
+		$post_id  = $this->factory->post->create( array( 'post_author' => $customer->get_id() ) );
 		update_user_meta( $customer->get_id(), 'wc_last_active', time() - YEAR_IN_SECONDS );
-		$post_id = wp_insert_post( array() );
 
 		update_option(
 			'woocommerce_delete_inactive_accounts',
@@ -22,8 +22,9 @@ class WC_Privacy_Test extends WC_Unit_Test_Case {
 			)
 		);
 
+		$this->assertEquals( $customer->get_id(), get_post_field( 'post_author', $post_id ), 'The test post was successfully assigned to the customer.' );
 		$this->assertEquals( 1, WC_Privacy::delete_inactive_accounts(), 'Dormant customer accounts are removed in accordance with configured privacy settings.' );
 		$this->assertFalse( get_user_by( 'id', $customer->get_id() ), 'The deleted user account no longer exists.' );
-		$this->assertEquals( 0, get_post_field( 'post_author', $post_id ), 'Posts belonging to the removed user still exist, but do not have an author.' );
+		$this->assertEquals( '0', get_post_field( 'post_author', $post_id ), 'Posts belonging to the removed user still exist, but do not have an author.' );
 	}
 }
