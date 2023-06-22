41690b4995d5d2483d5f41c3dd63a22c870b4f13
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-06-22 16:09:37 +0200

    Add back filter blocks frontend scripts (https://github.com/woocommerce/woocommerce-blocks/pull/9954)

--- plugins/woocommerce-blocks/src/BlockTypes/PriceFilter.php
+++ plugins/woocommerce-blocks/src/BlockTypes/PriceFilter.php
@@ -14,13 +14,4 @@ class PriceFilter extends AbstractBlock {
 	protected $block_name     = 'price-filter';
 	const MIN_PRICE_QUERY_VAR = 'min_price';
 	const MAX_PRICE_QUERY_VAR = 'max_price';
-
-	/**
-	 * Get the frontend script handle for this block type.
-	 *
-	 * @param string $key Data to get, or default to everything.
-	 */
-	protected function get_block_type_script( $key = null ) {
-		return null;
-	}
 }
