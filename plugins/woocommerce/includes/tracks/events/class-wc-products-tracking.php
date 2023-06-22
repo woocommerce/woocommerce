26dd2d4f5c4fbfbd55c8cba8dbf5e86620a0a3db
Matt Sherman <matt.sherman@automattic.com>
2023-06-22 08:16:34 -0400

    Always pass defaults to product_type_options filter (#38854)
    * Add wc_get_default_product_type_options function
    
    * Use wc_get_default_product_type_options() as initial input for product_type_options filter
    
    * Changelog

--- plugins/woocommerce/includes/tracks/events/class-wc-products-tracking.php
+++ plugins/woocommerce/includes/tracks/events/class-wc-products-tracking.php
@@ -230,18 +230,20 @@ class WC_Products_Tracking {
 	 * @return array
 	 */
 	private static function get_possible_product_type_options_ids() {
-		$product_type_options_ids = array_merge(
+		$product_type_options_ids =
 			array_values(
 				array_map(
 					function ( $product_type_option ) {
 						return $product_type_option['id'];
 					},
 					/* phpcs:disable WooCommerce.Commenting.CommentHooks.MissingHookComment */
-					apply_filters( 'product_type_options', array() )
+					apply_filters(
+						'product_type_options',
+						wc_get_default_product_type_options(),
+					)
+					/* phpcs: enable */
 				)
-			),
-			array( '_downloadable', '_virtual' )
-		);
+			);
 
 		return $product_type_options_ids;
 	}
