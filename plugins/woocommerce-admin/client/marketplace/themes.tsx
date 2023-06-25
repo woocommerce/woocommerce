ffa1783aa8e1f33b4791007a27b7821452601773
And Finally <andfinally@users.noreply.github.com>
2023-06-25 19:09:11 +0100

    Added tsx and scss files for WC Admin-powered Marketplace.

---
+++ plugins/woocommerce-admin/client/marketplace/themes.tsx
@@ -0,0 +1,20 @@
+/**
+ * External dependencies
+ */
+
+/**
+ * Internal dependencies
+ */
+import './marketplace.scss';
+
+export default function Themes() {
+	return (
+		<>
+			<nav className="woocommerce-marketplace__nav">
+				<a href="/wp-admin/admin.php?page=wc-admin&path=%2Fmarketplace">Discover</a>
+				<a href="/wp-admin/admin.php?page=wc-admin&path=%2Fmarketplace%2Fextensions">Extensions</a>
+				<span>Themes</span>
+			</nav>
+		</>
+	);
+}
