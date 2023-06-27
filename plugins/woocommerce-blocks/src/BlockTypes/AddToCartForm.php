1355c6dfa72ba965bb869ee34f2e9034fcb44377
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-06-27 14:20:56 +0200

    Single Product: Fix incorrect add to cart form markup (https://github.com/woocommerce/woocommerce-blocks/pull/9986)

--- plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
+++ plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
@@ -98,7 +98,7 @@ class AddToCartForm extends AbstractBlock {
 		$classes_and_styles = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes );
 
 		$form = sprintf(
-			'<div class="wp-block-add-to-cart-form product %1$s %2$s" style="%3$s">%4$s</div>',
+			'<div class="wp-block-add-to-cart-form %1$s %2$s" style="%3$s">%4$s</div>',
 			esc_attr( $classes_and_styles['classes'] ),
 			esc_attr( $classname ),
 			esc_attr( $classes_and_styles['styles'] ),
