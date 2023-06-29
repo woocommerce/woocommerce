0b265b07d39b56823e70533e74744656c927ea48
paul sealock <psealock@gmail.com>
2023-06-30 10:11:48 +1200

    save

--- tools/monorepo-utils/src/changelogger.ts
+++ tools/monorepo-utils/src/changelogger.ts
@@ -10,8 +10,18 @@ const MONOREPO_UTILS_ROOT = join( MONOREPO_ROOT, 'tools', 'monorepo-utils' );
 // const cwd = process.cwd();
 const args = process.argv.slice( 2 );
 
-execSync( 'composer exec -- changelogger ' + args.join( ' ' ), {
-	cwd: MONOREPO_UTILS_ROOT,
-	encoding: 'utf-8',
-	stdio: 'inherit',
-} );
+// execSync( 'composer exec -- changelogger ' + args.join( ' ' ), {
+// 	cwd: MONOREPO_UTILS_ROOT,
+// 	encoding: 'utf-8',
+// 	stdio: 'inherit',
+// } );
+
+const addedFiles = execSync( 'git ls-files -o  --exclude-standard', {
+	cwd: MONOREPO_ROOT,
+} ).toString();
+
+const changeFile = addedFiles
+	.split( '\n' )
+	.find( ( file ) => file.startsWith( 'tools/monorepo-utils/changelog/' ) );
+
+console.log( changeFile );
