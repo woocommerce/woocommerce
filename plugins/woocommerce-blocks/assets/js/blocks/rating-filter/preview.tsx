dc73f1e737dcfbbe656bd4d431e7a08d7c173489
Roy Ho <roykho77@gmail.com>
2023-06-29 06:15:52 -0700

    Make filter by rating preview consistent with default settings (https://github.com/woocommerce/woocommerce-blocks/pull/10007)
    * Make filter by rating preview consistent with default settings
    
    * Remove unneeded classnames

--- plugins/woocommerce-blocks/assets/js/blocks/rating-filter/preview.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/rating-filter/preview.tsx
@@ -6,58 +6,23 @@ import Rating from '@woocommerce/base-components/product-rating';
 
 export const previewOptions = [
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 5 }
-				rating={ 5 }
-				ratedProductsCount={ 5 }
-			/>
-		),
+		label: <Rating key={ 5 } rating={ 5 } ratedProductsCount={ null } />,
 		value: '5',
 	},
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 4 }
-				rating={ 4 }
-				ratedProductsCount={ 4 }
-			/>
-		),
+		label: <Rating key={ 4 } rating={ 4 } ratedProductsCount={ null } />,
 		value: '4',
 	},
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 3 }
-				rating={ 3 }
-				ratedProductsCount={ 3 }
-			/>
-		),
+		label: <Rating key={ 3 } rating={ 3 } ratedProductsCount={ null } />,
 		value: '3',
 	},
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 2 }
-				rating={ 2 }
-				ratedProductsCount={ 2 }
-			/>
-		),
+		label: <Rating key={ 2 } rating={ 2 } ratedProductsCount={ null } />,
 		value: '2',
 	},
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 1 }
-				rating={ 1 }
-				ratedProductsCount={ 1 }
-			/>
-		),
+		label: <Rating key={ 1 } rating={ 1 } ratedProductsCount={ null } />,
 		value: '1',
 	},
 ];
