175abc9d7562b33ce114ef3ad4edec770823e20d
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-06-29 15:20:44 -0400

    Hide or show the empty state or inner blocks if the product has attributes

--- packages/js/product-editor/src/blocks/variations/edit.tsx
+++ packages/js/product-editor/src/blocks/variations/edit.tsx
@@ -1,8 +1,11 @@
 /**
  * External dependencies
  */
+import classNames from 'classnames';
 import type { BlockEditProps } from '@wordpress/blocks';
 import { Button } from '@wordpress/components';
+import { useEntityProp } from '@wordpress/core-data';
+import { Product } from '@woocommerce/data';
 import { createElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import {
@@ -18,12 +21,38 @@ import { sanitizeHTML } from '../../utils/sanitize-html';
 import { VariationsBlockAttributes } from './types';
 import { EmptyVariationsImage } from './empty-variations-image';
 
+function hasAttributesUsedForVariations(
+	productAttributes: Product[ 'attributes' ]
+) {
+	return productAttributes.some( ( { variation } ) => variation );
+}
+
 export function Edit( {
 	attributes,
 }: BlockEditProps< VariationsBlockAttributes > ) {
 	const { description } = attributes;
-	const blockProps = useBlockProps();
-	const innerBlockProps = useInnerBlocksProps( {}, { templateLock: 'all' } );
+
+	const [ productAttributes ] = useEntityProp< Product[ 'attributes' ] >(
+		'postType',
+		'product',
+		'attributes'
+	);
+
+	const hasAttributes = hasAttributesUsedForVariations( productAttributes );
+
+	const blockProps = useBlockProps( {
+		className: classNames( {
+			'wp-block-woocommerce-product-variations-fields--has-attributes':
+				hasAttributes,
+		} ),
+	} );
+	const innerBlockProps = useInnerBlocksProps(
+		{
+			className:
+				'wp-block-woocommerce-product-variations-fields__content',
+		},
+		{ templateLock: 'all' }
+	);
 
 	return (
 		<div { ...blockProps }>
