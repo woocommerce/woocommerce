d5b8f41f45adaf1a5d89386c84a4a22489823b23
Lourens Schep <lourensschep@gmail.com>
2023-06-29 15:02:38 -0300

    Require wc-admin-functions into products tracking class

--- plugins/woocommerce/includes/tracks/events/class-wc-products-tracking.php
+++ plugins/woocommerce/includes/tracks/events/class-wc-products-tracking.php
@@ -10,6 +10,8 @@ use Automattic\WooCommerce\Internal\Admin\WCAdminAssets;
 
 defined( 'ABSPATH' ) || exit;
 
+require_once WC_ABSPATH . 'includes/admin/wc-admin-functions.php';
+
 /**
  * This class adds actions to track usage of WooCommerce Products.
  */
@@ -230,6 +232,7 @@ class WC_Products_Tracking {
 	 * @return array
 	 */
 	private static function get_possible_product_type_options_ids() {
+		$default_product_type_options = wc_get_default_product_type_options();
 		$product_type_options_ids =
 			array_values(
 				array_map(
@@ -239,7 +242,7 @@ class WC_Products_Tracking {
 					/* phpcs:disable WooCommerce.Commenting.CommentHooks.MissingHookComment */
 					apply_filters(
 						'product_type_options',
-						wc_get_default_product_type_options(),
+						$default_product_type_options,
 					)
 					/* phpcs: enable */
 				)
