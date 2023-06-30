woocommerce/woocommerce

commit bff51c0e74407df2aa18f467453b0299e92df52a

Removing registration of unneeded pages from `Marketplace` class.

plugins/woocommerce/src/Internal/Admin/Marketplace.php
@@ -23,44 +23,20 @@ class Marketplace {
 	 * Registers report pages.
 	 */
 	public function register_pages() {
-		$marketplace_pages = self::get_marketplace_pages();
-		foreach ( $marketplace_pages as $marketplace_page ) {
-			if ( ! is_null( $marketplace_page ) ) {
-				wc_admin_register_page( $marketplace_page );
-			}
-		}
+		$marketplace_page = self::get_marketplace_page();
+		wc_admin_register_page( $marketplace_page );
 	}
 
 	/**
 	 * Get report pages.
 	 */
-	public static function get_marketplace_pages() {
-		$marketplace_pages = array(
+	public static function get_marketplace_page() {
+		$marketplace_page = array(
 			array(
 				'id'     => 'woocommerce-marketplace',
 				'parent' => 'woocommerce',
 				'title'  => __( 'Marketplace', 'woocommerce' ),
 				'path'   => '/marketplace',
-			),
-			array(
-				'id'       => 'woocommerce-marketplace-extensions',
-				'title'    => __( 'Extensions', 'woocommerce' ),
-				'parent'   => 'woocommerce-marketplace',
-				'path'     => '/marketplace/extensions',
-				'nav_args' => array(
-					'order'  => 10,
-					'parent' => 'woocommerce-marketplace',
-				),
-			),
-			array(
-				'id'       => 'woocommerce-marketplace-themes',
-				'title'    => __( 'Themes', 'woocommerce' ),
-				'parent'   => 'woocommerce-marketplace',
-				'path'     => '/marketplace/themes',
-				'nav_args' => array(
-					'order'  => 20,
-					'parent' => 'woocommerce-marketplace',
-				),
 			),
 		);
 
@@ -69,6 +45,6 @@ class Marketplace {
 		 *
 		 * @since 8.0
 		 */
-		return apply_filters( 'woocommerce_marketplace_menu_items', $marketplace_pages );
+		return apply_filters( 'woocommerce_marketplace_menu_items', $marketplace_page );
 	}
 }

commit 1996c3e8abc33f89f40870ff82e06f0086fabcfc

Correcting error in registration of pages introduced in bff51c0e74407df2aa18f467453b0299e92df52a.

plugins/woocommerce/src/Internal/Admin/Marketplace.php
@@ -23,15 +23,19 @@ class Marketplace {
 	 * Registers report pages.
 	 */
 	public function register_pages() {
-		$marketplace_page = self::get_marketplace_page();
-		wc_admin_register_page( $marketplace_page );
+		$marketplace_pages = self::get_marketplace_pages();
+		foreach ( $marketplace_pages as $marketplace_page ) {
+			if ( ! is_null( $marketplace_page ) ) {
+				wc_admin_register_page( $marketplace_page );
+			}
+		}
 	}
 
 	/**
 	 * Get report pages.
 	 */
-	public static function get_marketplace_page() {
-		$marketplace_page = array(
+	public static function get_marketplace_pages() {
+		$marketplace_pages = array(
 			array(
 				'id'     => 'woocommerce-marketplace',
 				'parent' => 'woocommerce',
@@ -45,6 +49,6 @@ class Marketplace {
 		 *
 		 * @since 8.0
 		 */
-		return apply_filters( 'woocommerce_marketplace_menu_items', $marketplace_page );
+		return apply_filters( 'woocommerce_marketplace_menu_items', $marketplace_pages );
 	}
 }