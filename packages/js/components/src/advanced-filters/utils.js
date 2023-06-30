7f06b0fc11f7bd1f6734b2afacd8dacc9a8c38e7
Fernando Marichal <ultimoround@gmail.com>
2023-06-30 14:12:30 -0300

    Create function getInterpolatedString

--- packages/js/components/src/advanced-filters/utils.js
+++ packages/js/components/src/advanced-filters/utils.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { isArray, isNumber, isString } from 'lodash';
+import deprecated from '@wordpress/deprecated';
 
 /**
  * DOM Node.textContent for React components
@@ -33,4 +34,47 @@ export function textContent( components ) {
 	toText( components );
 
 	return text;
+}
+
+/**
+ * This function processes an input string, checks for deprecated interpolation formatting, and
+ * modifies it to conform to the new standard.
+ * The deprecated interpolation formatting is `{{element}}...{{/element}}`, and the new standard
+ * formatting is `<element>...</element>`.
+ *
+ * @param {string} interpolatedString The interpolation string to be parsed.
+ *
+ * @return {string}  Fixed interpolation string.
+ */
+export function getInterpolatedString( interpolatedString ) {
+	const regex = /(\{\{)(\/?\s*\w+\s*\/?)(\}\})/g;
+
+	if ( regex.exec( interpolatedString ) !== null ) {
+		deprecated(
+			'The interpolation string format `{{element}}...{{/element}}` or `{{element/}}` is deprecated.',
+			{
+				version: '7.8.0',
+				hint: 'Please update your code to use the new format: `<element>...</element>` or `<element/>`.',
+			}
+		);
+
+		return interpolatedString.replaceAll( regex, ( match, p1, p2 ) => {
+			const inner = p2.trim();
+			let replacement;
+			if ( inner.startsWith( '/' ) ) {
+				// Closing tag
+				replacement = `</${ inner.slice( 1 ) }>`;
+			} else if ( inner.endsWith( '/' ) ) {
+				// Self-closing tag
+				replacement = `<${ inner.slice( 0, -1 ) }/>`;
+			} else {
+				// Opening tag
+				replacement = `<${ inner }>`;
+			}
+
+			return replacement;
+		} );
+	}
+
+	return interpolatedString;
 }
