e5e7766b90d72ae7eaada8ff4fc654ad83ead9b8
And Finally <andfinally@users.noreply.github.com>
2023-06-30 12:27:31 +0100

    Changed routing, so we have one page, `path=/marketplace`, and tabs for Discover and Extensions.

---
+++ plugins/woocommerce-admin/client/marketplace/components/tabs/tabs.tsx
@@ -0,0 +1,103 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useEffect } from '@wordpress/element';
+import { Button } from '@wordpress/components';
+import classNames from 'classnames';
+import { getQuery, getNewPath, navigateTo } from '@woocommerce/navigation';
+
+/**
+ * Internal dependencies
+ */
+import './tabs.scss';
+import { MARKETPLACE_PATH } from '../constants';
+
+export interface TabsProps {
+	selectedTab?: string | undefined;
+	setSelectedTab: ( value: string ) => void;
+}
+
+interface Tab {
+	name: string,
+	title: string,
+}
+
+interface Tabs {
+	[ key: string ]: Tab;
+}
+
+const tabs: Tabs = {
+	discover: {
+		name: 'discover',
+		title: __( 'Discover', 'woocommerce' ),
+	},
+	extensions: {
+		name: 'extensions',
+		title: __( 'Extensions', 'woocommerce' ),
+	},
+};
+
+const setUrlTabParam = ( tabKey: string ) => {
+	if ( 'discover' === tabKey ) {
+		navigateTo( {
+			url: getNewPath( {}, MARKETPLACE_PATH, {} ),
+		} );
+		return;
+	}
+	navigateTo( {
+		url: getNewPath( { tab: tabKey } ),
+	} );
+};
+
+const renderTabs = ( props: TabsProps ) => {
+	const { selectedTab, setSelectedTab } = props;
+	let tabContent = [];
+	for ( const tabKey in tabs ) {
+		tabContent.push(
+			<Button
+				className={ classNames(
+					'woocommerce-marketplace__tab-button',
+					{
+						'is-active': tabKey === selectedTab
+					}
+				) }
+				onClick={ () => {
+					setSelectedTab( tabKey );
+					setUrlTabParam( tabKey );
+				} }
+				key={ tabKey }
+			>
+				{ tabs[ tabKey ]?.title }
+			</Button>
+		);
+	}
+	return tabContent;
+};
+
+const Tabs = ( props: TabsProps ): JSX.Element => {
+	const { setSelectedTab } = props;
+
+	interface Query {
+		path?: string,
+		tab?: string,
+	}
+
+	useEffect( () => {
+		const query: Query = getQuery();
+		if (
+			query?.path === MARKETPLACE_PATH &&
+			query?.tab
+		) {
+			setSelectedTab( query.tab );
+		}
+	}, [] );
+
+	return (
+		<nav className="woocommerce-marketplace__tabs">
+			{ renderTabs( props ) }
+		</nav>
+	);
+};
+
+export default Tabs;
