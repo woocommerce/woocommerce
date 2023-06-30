4af802a170dc77ffc78b378aca0ecba128872246
paul sealock <psealock@gmail.com>
2023-06-30 17:46:53 +1200

    cleanup

--- tools/monorepo-utils/src/changelogger.ts
+++ tools/monorepo-utils/src/changelogger.ts
@@ -5,8 +5,6 @@ import { execSync } from 'child_process';
 import { dirname, join } from 'path';
 
 const MONOREPO_ROOT = dirname( dirname( dirname( __dirname ) ) );
-const MONOREPO_UTILS_ROOT = join( MONOREPO_ROOT, 'tools', 'monorepo-utils' );
-
 const CHANGELOGGER_PATH = join(
 	MONOREPO_ROOT,
 	'tools',
@@ -21,8 +19,6 @@ const CHANGELOGGER_PATH = join(
 const cwd = process.cwd();
 const args = process.argv.slice( 2 );
 const argString = args.map( ( value ) => `"${ value }"` ).join( ' ' );
-// const args =
-// 	'"add" "-f" "uuuuupdate-add_gitignore_to_package" "-s" "patch" "-t" "tweak" "-e" "changelog message" "-n"';
 
 execSync( CHANGELOGGER_PATH + ' ' + argString, {
 	cwd,
