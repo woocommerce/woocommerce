0550921d2d05733e75a5dfcc562f70687bb22af6
Luigi Teschio <gigitux@gmail.com>
2023-06-30 11:31:14 +0200

    Product Rating: append #reviews on the Single Product Block (https://github.com/woocommerce/woocommerce-blocks/pull/10040)
    * Product Rating: append #reviews on the Single Product Block
    
    * refactor after feedback

--- plugins/woocommerce-blocks/src/BlockTypes/ProductRating.php
+++ plugins/woocommerce-blocks/src/BlockTypes/ProductRating.php
@@ -151,10 +151,9 @@ class ProductRating extends AbstractBlock {
 					);
 
 					if ( $is_descendent_of_single_product_block ) {
-						$customer_reviews_count = '<a href="' . esc_url( $product_permalink ) . '">' . $customer_reviews_count . '</a>';
-					} elseif ( $is_descendent_of_single_product_template ) {
-						$product_permalink      = untrailingslashit( $product_permalink );
 						$customer_reviews_count = '<a href="' . esc_url( $product_permalink ) . '#reviews">' . $customer_reviews_count . '</a>';
+					} elseif ( $is_descendent_of_single_product_template ) {
+						$customer_reviews_count = '<a class="woocommerce-review-link" rel="nofollow" href="#reviews">' . $customer_reviews_count . '</a>';
 					}
 
 					$reviews_count_html = sprintf( '<span class="wc-block-components-product-rating__reviews_count">%1$s</span>', $customer_reviews_count );
