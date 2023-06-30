b103a136f658565d2b69b51cdcc89b1c00474b63
paul sealock <psealock@gmail.com>
2023-06-30 17:59:38 +1200

    save

--- tools/monorepo-utils/src/changelogger.ts
+++ tools/monorepo-utils/src/changelogger.ts
@@ -5,22 +5,24 @@ import { execSync } from 'child_process';
 import { dirname, join } from 'path';
 
 const MONOREPO_ROOT = dirname( dirname( dirname( __dirname ) ) );
+const MONOREPO_UTILS_ROOT = join( MONOREPO_ROOT, 'tools', 'monorepo-utils' );
 const CHANGELOGGER_PATH = join(
-	MONOREPO_ROOT,
-	'tools',
-	'monorepo-utils',
+	MONOREPO_UTILS_ROOT,
 	'vendor',
 	'automattic',
 	'jetpack-changelogger',
 	'bin',
 	'changelogger'
 );
+const COMPOSER_PATH = join( MONOREPO_UTILS_ROOT, 'composer.json' );
 
 const cwd = process.cwd();
 const args = process.argv.slice( 2 );
 const argString = args.map( ( value ) => `"${ value }"` ).join( ' ' );
 
-execSync( CHANGELOGGER_PATH + ' ' + argString, {
+const command = `COMPOSER=${ COMPOSER_PATH } ${ CHANGELOGGER_PATH } ${ argString }`;
+
+execSync( command, {
 	cwd,
 	encoding: 'utf-8',
 	stdio: 'inherit',
