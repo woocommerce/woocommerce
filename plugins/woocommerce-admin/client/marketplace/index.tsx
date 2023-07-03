06fbef9e7e93dd71fd5a0a23d0000582806f83c7
And Finally <andfinally@users.noreply.github.com>
2023-07-03 13:00:52 +0100

    Added `discover` key for default tab to constants. Added `setSelectedTab` to dependencies for `useEffect` function that sets the selected tab based on the URL. Simplified the logic in this function – we can rely on the `path` always being `MARKETPLACE_PATH`. If tab in URL is not a recognised tab, set the default tab as selected.

--- plugins/woocommerce-admin/client/marketplace/index.tsx
+++ plugins/woocommerce-admin/client/marketplace/index.tsx
@@ -7,11 +7,12 @@ import { useState } from '@wordpress/element';
  * Internal dependencies
  */
 import './marketplace.scss';
+import { DEFAULT_TAB_KEY } from './components/constants';
 import Tabs from './components/tabs/tabs';
 import Content from './components/content/content';
 
 export default function Marketplace() {
-	const [ selectedTab, setSelectedTab ] = useState( 'discover' );
+	const [ selectedTab, setSelectedTab ] = useState( DEFAULT_TAB_KEY );
 
 	return (
 		<>
