edc2ed9d5b9f01484566d153e9db0cc80329f695
Roy Ho <roykho77@gmail.com>
2023-07-03 05:39:45 -0700

    Remove attributes data from save function for filter by price (https://github.com/woocommerce/woocommerce-blocks/pull/10039)

--- plugins/woocommerce-blocks/assets/js/blocks/price-filter/deprecated.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/price-filter/deprecated.tsx
@@ -1,40 +1,41 @@
 /**
  * External dependencies
  */
-import { registerBlockType } from '@wordpress/blocks';
 import classNames from 'classnames';
-import { Icon, currencyDollar } from '@wordpress/icons';
 import { useBlockProps } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
-import edit from './edit';
 import metadata from './block.json';
 import { blockAttributes } from './attributes';
-import deprecated from './deprecated';
+import type { Attributes } from './types';
 
-registerBlockType( metadata, {
-	icon: {
-		src: (
-			<Icon
-				icon={ currencyDollar }
-				className="wc-block-editor-components-block-icon"
-			/>
-		),
-	},
+const v1 = {
 	attributes: {
 		...metadata.attributes,
 		...blockAttributes,
 	},
-	edit,
-	save( { attributes } ) {
-		const { className } = attributes;
+	save: ( { attributes }: { attributes: Attributes } ) => {
+		const {
+			className,
+			showInputFields,
+			showFilterButton,
+			heading,
+			headingLevel,
+		} = attributes;
+		const data = {
+			'data-showinputfields': showInputFields,
+			'data-showfilterbutton': showFilterButton,
+			'data-heading': heading,
+			'data-heading-level': headingLevel,
+		};
 		return (
 			<div
 				{ ...useBlockProps.save( {
 					className: classNames( 'is-loading', className ),
 				} ) }
+				{ ...data }
 			>
 				<span
 					aria-hidden
@@ -43,5 +44,8 @@ registerBlockType( metadata, {
 			</div>
 		);
 	},
-	deprecated,
-} );
+};
+
+const deprecated = [ v1 ];
+
+export default deprecated;
