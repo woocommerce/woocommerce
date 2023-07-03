b1bc51e3f022b65db72283676239b3b49a05dfa5
Alba Rincón <albarin@users.noreply.github.com>
2023-07-03 11:48:50 +0200

    Add new `Product Rating Stars` block (https://github.com/woocommerce/woocommerce-blocks/pull/10005)
    * Add new `Product Rating Stars` block
    
    * Make block experimental
    
    * Fix dep

--- plugins/woocommerce-blocks/bin/webpack-entries.js
+++ plugins/woocommerce-blocks/bin/webpack-entries.js
@@ -58,6 +58,9 @@ const blocks = {
 	'product-top-rated': {},
 	'products-by-attribute': {},
 	'rating-filter': {},
+	'product-rating-stars': {
+		isExperimental: true,
+	},
 	'reviews-by-category': {
 		customDir: 'reviews/reviews-by-category',
 	},
