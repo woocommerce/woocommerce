9e10e5c605e02efc3aa6a452a3f41fd829575fbe
Nestor Soriano <konamiman@konamiman.com>
2023-07-04 12:45:16 +0900

    Fix linting errors in StringUtilTest

--- plugins/woocommerce/tests/php/src/Utilities/StringUtilTest.php
+++ plugins/woocommerce/tests/php/src/Utilities/StringUtilTest.php
@@ -115,21 +115,21 @@ class StringUtilTest extends \WC_Unit_Test_Case {
 	 * @testDox 'to_sql_list' generates a parenthesized and comma-separated list of the passed values.
 	 */
 	public function test_to_sql_list() {
-		$result = StringUtil::to_sql_list([34]);
-		$this->assertEquals('(34)', $result);
+		$result = StringUtil::to_sql_list( array( 34 ) );
+		$this->assertEquals( '(34)', $result );
 
-		$result = StringUtil::to_sql_list([34, 'foo', true]);
-		$this->assertEquals('(34,foo,1)', $result);
+		$result = StringUtil::to_sql_list( array( 34, 'foo', true ) );
+		$this->assertEquals( '(34,foo,1)', $result );
 	}
 
 	/**
 	 * @testDox 'to_sql_list' throws an exception if an empty array is passed.
 	 */
 	public function test_to_sql_list_with_empty_input() {
-		$this->expectException(\InvalidArgumentException::class);
-		$this->expectExceptionMessage('StringUtil::to_sql_list: the values array is empty');
+		$this->expectException( \InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'StringUtil::to_sql_list: the values array is empty' );
 
-		StringUtil::to_sql_list([]);
+		StringUtil::to_sql_list( array() );
 	}
 
 	/**
@@ -139,12 +139,11 @@ class StringUtilTest extends \WC_Unit_Test_Case {
 	 *           ["foo", "foo"]
 	 *           ["", ""]
 	 *
-	 * @param string $input
-	 * @param $expected_output
-	 * @return void
+	 * @param string $input The string to test.
+	 * @param string $expected_output The expected output.
 	 */
-	public function test_class_name_without_namespace(string $input, $expected_output) {
-		$actual_output = StringUtil::class_name_without_namespace($input);
-		$this->assertEquals($expected_output, $actual_output);
+	public function test_class_name_without_namespace( string $input, string $expected_output ) {
+		$actual_output = StringUtil::class_name_without_namespace( $input );
+		$this->assertEquals( $expected_output, $actual_output );
 	}
 }
