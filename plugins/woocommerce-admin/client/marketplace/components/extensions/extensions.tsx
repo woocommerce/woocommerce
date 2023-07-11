2107543c85e2fb238cdc286655b135011152286d
Kyle Nel <22053773+kdevnel@users.noreply.github.com>
2023-07-11 15:16:54 +0200

    Add temporary placeholder components
    All components here are temporary and can be removed/replaced when their relevant PR is ready.

--- plugins/woocommerce-admin/client/marketplace/components/extensions/extensions.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/extensions/extensions.tsx
@@ -5,11 +5,13 @@
 /**
  * Internal dependencies
  */
+import './extensions.scss';
 
 export default function Extensions() {
 	return (
 		<div className="woocommerce-marketplace__content">
 			<h1>Extensions</h1>
+			<div className="woocommerce-marketplace__content__placeholder"></div>
 		</div>
 	);
 }
