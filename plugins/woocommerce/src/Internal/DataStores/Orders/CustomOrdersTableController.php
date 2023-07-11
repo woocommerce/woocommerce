ffb20eb7ec56942c085d791ca13475e6fa370a88
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2023-07-11 18:43:33 +0530

    Use proper pluralization of pending sync message.
    Co-authored-by: Barry Hughes <3594411+barryhughes@users.noreply.github.com>

--- plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
@@ -511,7 +511,12 @@ class CustomOrdersTableController {
 		} elseif ( $sync_status['current_pending_count'] > 0 ) {
 			$sync_message = sprintf(
 				// translators: %d: number of pending orders.
-				__( 'Sync %d pending orders. You can switch data storage for orders only when posts and orders table are in sync.', 'woocommerce' ),
+				_n( 
+					'Sync %d pending order. You can switch data storage for orders only when posts and orders table are in sync.', 'woocommerce' ),
+					'Sync %d pending orders. You can switch data storage for orders only when posts and orders table are in sync.', 'woocommerce' ),
+					$sync_status['current_pending_count'],
+					'woocommerce'
+				),
 				$sync_status['current_pending_count']
 			);
 		}
