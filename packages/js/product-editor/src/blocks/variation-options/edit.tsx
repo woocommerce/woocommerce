b1a29ea940c6f01455f0686b83c1280a04658e19
Nathan Schneider <nsschneider1@gmail.com>
2023-07-13 17:26:14 -0300

    Add AttributeControl component to screen

--- packages/js/product-editor/src/blocks/variation-options/edit.tsx
+++ packages/js/product-editor/src/blocks/variation-options/edit.tsx
@@ -4,13 +4,35 @@
 
 import { useBlockProps } from '@wordpress/block-editor';
 import { createElement } from '@wordpress/element';
+import { ProductAttribute } from '@woocommerce/data';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore No types for this exist yet.
+// eslint-disable-next-line @woocommerce/dependency-group
+import { useEntityProp, useEntityId } from '@wordpress/core-data';
+
+/**
+ * Internal dependencies
+ */
+import { AttributeControl } from '../../components/attribute-control';
+import { useProductAttributes } from '../../hooks/use-product-attributes';
 
 export function Edit() {
 	const blockProps = useBlockProps();
 
+	const [ entityAttributes, setEntityAttributes ] = useEntityProp<
+		ProductAttribute[]
+	>( 'postType', 'product', 'attributes' );
+
+	const { attributes, handleChange } = useProductAttributes( {
+		allAttributes: entityAttributes,
+		onChange: setEntityAttributes,
+		isVariationAttributes: true,
+		productId: useEntityId( 'postType', 'product' ),
+	} );
+
 	return (
 		<div { ...blockProps }>
-			<p>hello world</p>
+			<AttributeControl value={ attributes } onChange={ handleChange } />
 		</div>
 	);
 }
