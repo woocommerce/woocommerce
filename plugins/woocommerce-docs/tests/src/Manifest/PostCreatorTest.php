37f6f1621c4858d5a11cb27ffcc576cb79db7f8f
Sam Seay <samueljseay@gmail.com>
2023-07-18 09:30:28 +0800

    Add tests for PostArgs and PostCreator

---
+++ plugins/woocommerce-docs/tests/src/Manifest/PostCreatorTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace WooCommerceDocs\Tests\Manifest;
+
+use WP_UnitTestCase;
+use WooCommerceDocs\Manifest\PostCreator;
+
+
+/**
+ * Class ManifestProcessorTest
+ *
+ * @package WooCommerceDocs\Tests\Manifest
+ */
+class PostCreatorTest extends WP_UnitTestCase {
+
+	/**
+	 * Test a post is created from a manifest entry.
+	 */
+	public function test_create_post() {
+		$manifest_post = array(
+			'post_title'     => 'Test Post Title',
+			'post_status'    => 'draft',
+			'post_date'      => '2019-01-01 00:00:00',
+			'comment_status' => 'closed',
+		);
+
+		$actual_content = 'Hello World';
+
+		$post_id = PostCreator::create_post_from_manifest_entry( $manifest_post, $actual_content, 123 );
+
+		$post = get_post( $post_id );
+
+		$block_content = '<!-- wp:paragraph -->
+<p>Hello World</p>
+<!-- /wp:paragraph -->
+';
+
+		$this->assertEquals( 'Test Post Title', $post->post_title );
+		$this->assertEquals( $block_content, $post->post_content );
+		$this->assertEquals( 'draft', $post->post_status );
+	}
+
+	/**
+	 * Test a  post is updated from a manifest entry if it exists.
+	 */
+	public function test_update_post() {
+		$manifest_post = array(
+			'post_title'     => 'Test Post Title',
+			'post_status'    => 'draft',
+			'post_date'      => '2019-01-01 00:00:00',
+			'comment_status' => 'closed',
+		);
+
+		$actual_content = 'Hello World';
+		$post_id        = PostCreator::create_post_from_manifest_entry( $manifest_post, $actual_content, 123 );
+		$post           = get_post( $post_id );
+
+		$manifest_post = array(
+			'post_title'     => 'Test Post Title 2',
+			'post_status'    => 'publish',
+			'post_date'      => '2019-01-01 00:00:00',
+			'comment_status' => 'closed',
+		);
+
+		$actual_content = 'Hello World 2';
+
+		$post_id = PostCreator::create_post_from_manifest_entry( $manifest_post, $actual_content, 123 );
+
+		$post = get_post( $post_id );
+
+		$block_content = '<!-- wp:paragraph -->
+<p>Hello World 2</p>
+<!-- /wp:paragraph -->
+';
+
+		$this->assertEquals( 'Test Post Title 2', $post->post_title );
+		$this->assertEquals( $block_content, $post->post_content );
+		$this->assertEquals( 'publish', $post->post_status );
+	}
+}
