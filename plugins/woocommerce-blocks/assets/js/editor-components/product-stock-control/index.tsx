eea2b75f8a0e1683920a2f10e700d588ece98e14
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-07-18 08:39:09 +0200

    Newest Products: fix Stock Status filter (https://github.com/woocommerce/woocommerce-blocks/pull/10201)
    * Newest Products: fix Stock Status filter
    
    * Change the default of Product Stock Control to ALL available stock statuses

--- plugins/woocommerce-blocks/assets/js/editor-components/product-stock-control/index.tsx
+++ plugins/woocommerce-blocks/assets/js/editor-components/product-stock-control/index.tsx
@@ -38,9 +38,14 @@ const ProductStockControl = ( {
 	const displayOptions = Object.entries( stockStatusOptions )
 		.map( ( [ slug, name ] ) => ( { value: slug, label: name } ) )
 		.filter( ( status ) => !! status.label );
+	const defaultCheckedOptions = Object.keys( stockStatusOptions ).filter(
+		( key: string ) => !! key
+	);
 
 	// Set the initial state to the default or saved value.
-	const [ checkedOptions, setChecked ] = useState( value );
+	const [ checkedOptions, setChecked ] = useState(
+		value || defaultCheckedOptions
+	);
 
 	/**
 	 * Set attributes when checked items change.
