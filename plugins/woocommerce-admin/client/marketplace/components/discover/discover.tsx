113b794754c89f6927171bf4a02898c14ec94700
And Finally <andfinally@users.noreply.github.com>
2023-07-19 16:07:23 +0100

    First commit. This contains the changes from `update/react-marketplace`, reviewed in https://github.com/woocommerce/woocommerce/pull/38885.

---
+++ plugins/woocommerce-admin/client/marketplace/components/discover/discover.tsx
@@ -0,0 +1,15 @@
+/**
+ * External dependencies
+ */
+
+/**
+ * Internal dependencies
+ */
+
+export default function Discover() {
+	return (
+		<div className="woocommerce-marketplace__content">
+			<h1>Discover</h1>
+		</div>
+	);
+}
