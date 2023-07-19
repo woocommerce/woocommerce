2ca0a113dac3b9efa17a83cd95588ece6984957b
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-07-19 14:21:41 +0200

    Store Customization > Hero Product Split pattern - Update the pattern to become wireframed (https://github.com/woocommerce/woocommerce-blocks/pull/10270)
    * Remove opinionated styles from the Hero Product Split pattern.
    
    * Remove additional has-text-color and has-background

--- plugins/woocommerce-blocks/patterns/hero-product-split.php
+++ plugins/woocommerce-blocks/patterns/hero-product-split.php
@@ -6,18 +6,18 @@
  */
 ?>
 
-<!-- wp:media-text {"align":"full","mediaPosition":"right","mediaType":"image","mediaSizeSlug":"full","imageFill":false,"style":{"color":{"background":"#000000","text":"#ffffff"}}} -->
-<div class="wp-block-media-text alignfull has-media-on-the-right is-stacked-on-mobile has-text-color has-background" style="color:#ffffff;background-color:#000000">
+<!-- wp:media-text {"align":"full","mediaPosition":"right","mediaType":"image","mediaSizeSlug":"full","imageFill":false} -->
+<div class="wp-block-media-text alignfull has-media-on-the-right is-stacked-on-mobile">
 	<div class="wp-block-media-text__content">
-		<!-- wp:heading {"style":{"color":{"text":"#ffffff"}}} -->
-		<h2 class="wp-block-heading has-text-color" style="color:#ffffff;"><?php esc_html_e( 'Get cozy this fall with knit sweaters', 'woo-gutenberg-products-block' ); ?></h2>
+		<!-- wp:heading -->
+		<h2 class="wp-block-heading"><?php esc_html_e( 'Get cozy this fall with knit sweaters', 'woo-gutenberg-products-block' ); ?></h2>
 		<!-- /wp:heading -->
 
 		<!-- wp:buttons {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
 		<div class="wp-block-buttons" style="margin-bottom:var(--wp--preset--spacing--40)">
-		<!-- wp:button {"style":{"color":{"text":"#000000","background":"#ffffff"}}} -->
+		<!-- wp:button -->
 		<div class="wp-block-button">
-			<a href="<?php echo esc_url( get_permalink( wc_get_page_id( 'shop' ) ) ); ?>" class="wp-block-button__link has-text-color has-background wp-element-button" style="color:#000000;background-color:#ffffff;"><?php esc_html_e( 'Shop now', 'woo-gutenberg-products-block' ); ?></a>
+			<a href="<?php echo esc_url( get_permalink( wc_get_page_id( 'shop' ) ) ); ?>" class="wp-block-button__link wp-element-button"><?php esc_html_e( 'Shop now', 'woo-gutenberg-products-block' ); ?></a>
 		</div>
 		<!-- /wp:button -->
 		</div>
