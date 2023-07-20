c3e76523ecbb58a56d61f547cf951c4ccc90d5ad
Jon Lane <jon.lane@automattic.com>
2023-07-20 14:28:40 -0700

    Update config to use API URL

--- plugins/woocommerce/tests/api-core-tests/global-setup.js
+++ plugins/woocommerce/tests/api-core-tests/global-setup.js
@@ -2,7 +2,7 @@ const { site } = require( '../e2e-pw/utils' );
 
 module.exports = async ( config ) => {
 	// If BASE_URL is configured, we can assume we're on CI
-	if ( process.env.BASE_URL ) {
+	if ( process.env.API_BASE_URL ) {
 		await site.reset( process.env.USER_KEY, process.env.USER_SECRET );
 	}
 };
