dcad71f51ed34ab0a9ad369eabf74569ccd16952
Joel <dygerati@gmail.com>
2023-07-20 15:04:06 -0700

    Declaring wordpress data package

--- plugins/woo-ai/src/product-description/product-description-button-container.tsx
+++ plugins/woo-ai/src/product-description/product-description-button-container.tsx
@@ -7,10 +7,6 @@ import {
 	__experimentalUseCompletion as useCompletion,
 	UseCompletionError,
 } from '@woocommerce/ai';
-// TODO: Re-add "@types/wordpress__data" package to resolve this, causing other issues until pnpm 8.6.0 is usable
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-// eslint-disable-next-line @woocommerce/dependency-group
 import { useDispatch } from '@wordpress/data';
 
 /**
