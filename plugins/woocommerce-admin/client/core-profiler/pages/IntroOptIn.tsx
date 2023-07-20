af6f3acd95f5ef311463a4d5561c57ae8f7cea7c
moon <moon.kyong@automattic.com>
2023-07-20 06:25:29 -0700

    Put ,

--- plugins/woocommerce-admin/client/core-profiler/pages/IntroOptIn.tsx
+++ plugins/woocommerce-admin/client/core-profiler/pages/IntroOptIn.tsx
@@ -72,7 +72,7 @@ export const IntroOptIn = ( {
 						className="core-profiler__checkbox"
 						label={ interpolateComponents( {
 							mixedString: __(
-								'I agree to share my data to tailor my store setup experience and get more relevant content. WooCommerce will never rent or sell your data and you can opt out at any time in WooCommerce settings. {{link}}Learn more about usage tracking{{/link}}.',
+								'I agree to share my data to tailor my store setup experience and get more relevant content. WooCommerce will never rent or sell your data, and you can opt out at any time in WooCommerce settings. {{link}}Learn more about usage tracking{{/link}}.',
 								'woocommerce'
 							),
 							components: {
