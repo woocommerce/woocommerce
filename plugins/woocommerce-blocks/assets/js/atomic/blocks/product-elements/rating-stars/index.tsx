b8f798b01743519ed04d6852d7cb314da9807dfe
Alba Rincón <albarin@users.noreply.github.com>
2023-07-24 11:02:24 +0200

    Use the `Single Product` block for the `Product Details Product Listing` pattern (https://github.com/woocommerce/woocommerce-blocks/pull/9978)
    * Show alignment toolbar also when the block is in the single product block
    
    * Start using `Single Product` for the `Product Details Product Listing` pattern
    
    * Use counter and stars blocks in the pattern
    
    * Undo button change
    
    * Make the new rating blocks not experimental
    
    * Add Beta to the average rating block
    
    * Add blocks to block types controller

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/rating-stars/index.tsx
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/rating-stars/index.tsx
@@ -3,7 +3,6 @@
  */
 import { registerBlockType } from '@wordpress/blocks';
 import { Icon, starFilled } from '@wordpress/icons';
-import { isExperimentalBuild } from '@woocommerce/block-settings';
 
 /**
  * Internal dependencies
@@ -12,17 +11,15 @@ import metadata from './block.json';
 import edit from './edit';
 import { supports } from './support';
 
-if ( isExperimentalBuild() ) {
-	registerBlockType( metadata, {
-		icon: {
-			src: (
-				<Icon
-					icon={ starFilled }
-					className="wc-block-editor-components-block-icon"
-				/>
-			),
-		},
-		supports,
-		edit,
-	} );
-}
+registerBlockType( metadata, {
+	icon: {
+		src: (
+			<Icon
+				icon={ starFilled }
+				className="wc-block-editor-components-block-icon"
+			/>
+		),
+	},
+	supports,
+	edit,
+} );
