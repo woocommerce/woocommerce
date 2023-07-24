f7d8f8742724b530fa2b2139ad59c4eb62666fbe
Alexandre Lara <allexandrelara@gmail.com>
2023-07-24 11:45:40 -0300

    Add to cart form: Fix fatal error when missing `product` param in add_to_cart_redirect_filter (https://github.com/woocommerce/woocommerce-blocks/pull/10316)
    * Transform product param optional in add_to_cart_redirect_filter method
    
    * Remove unnecessary argument from add_to_cart_redirect_filter
    
    * Remove mocked filter call
    
    * Remove unnecessary argument from function docs

--- plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
+++ plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
@@ -29,7 +29,7 @@ class AddToCartForm extends AbstractBlock {
 	protected function initialize() {
 		parent::initialize();
 		add_filter( 'wc_add_to_cart_message_html', array( $this, 'add_to_cart_message_html_filter' ), 10, 2 );
-		add_filter( 'woocommerce_add_to_cart_redirect', array( $this, 'add_to_cart_redirect_filter' ), 10, 2 );
+		add_filter( 'woocommerce_add_to_cart_redirect', array( $this, 'add_to_cart_redirect_filter' ), 10, 1 );
 	}
 
 	/**
@@ -157,10 +157,9 @@ class AddToCartForm extends AbstractBlock {
 	 * is clicked.
 	 *
 	 * @param string $url The URL to redirect to after the product is added to the cart.
-	 * @param object $product The product being added to the cart.
 	 * @return string The filtered redirect URL.
 	 */
-	public function add_to_cart_redirect_filter( $url, $product ) {
+	public function add_to_cart_redirect_filter( $url ) {
 		// phpcs:ignore
 		if ( isset( $_POST['is-descendent-of-single-product-block'] ) && 'true' == $_POST['is-descendent-of-single-product-block'] ) {
 			return wp_validate_redirect( wp_get_referer(), $url );
