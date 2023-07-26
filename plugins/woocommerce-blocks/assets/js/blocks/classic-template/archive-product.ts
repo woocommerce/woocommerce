0312fc128c671caee438467378293eb1fb6ee37c
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-07-26 11:08:01 +0200

    Optimise imports of Product Query variation (https://github.com/woocommerce/woocommerce-blocks/pull/10340)
    * Optimise imports of Product Query variation
    
    * Lint: add whitespace

--- plugins/woocommerce-blocks/assets/js/blocks/classic-template/archive-product.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/classic-template/archive-product.ts
@@ -8,15 +8,15 @@ import {
 } from '@wordpress/blocks';
 import { isWpVersion } from '@woocommerce/settings';
 import { __, sprintf } from '@wordpress/i18n';
+import {
+	INNER_BLOCKS_TEMPLATE as productsInnerBlocksTemplate,
+	QUERY_DEFAULT_ATTRIBUTES as productsQueryDefaultAttributes,
+	PRODUCT_QUERY_VARIATION_NAME as productsVariationName,
+} from '@woocommerce/blocks/product-query/constants';
 
 /**
  * Internal dependencies
  */
-import {
-	INNER_BLOCKS_TEMPLATE as productsInnerBlocksTemplate,
-	QUERY_DEFAULT_ATTRIBUTES as productsQueryDefaultAttributes,
-} from '../product-query/constants';
-import { VARIATION_NAME as productsVariationName } from '../product-query/variations/product-query';
 import { createArchiveTitleBlock, createRowBlock } from './utils';
 import { OnClickCallbackParameter, type InheritedAttributes } from './types';
 
