1cd9f2a727fafafc16b53e4c787abc7d5ac9a293
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-07-27 08:53:24 +0200

    Add tests in Playwright for Mini-Cart drawer closing (https://github.com/woocommerce/woocommerce-blocks/pull/10305)
    * Add tests in Playwright for Mini-Cart drawer closing
    
    * Make use of waitForSelector instead of waitForTimeout
    
    * Make use of waitForSelector instead of waitForTimeout (II)

--- plugins/woocommerce-blocks/tests/e2e-pw/types/block-data.ts
+++ plugins/woocommerce-blocks/tests/e2e-pw/types/block-data.ts
@@ -1,5 +1,5 @@
 export type BlockData< T = unknown > = {
 	name: string;
 	mainClass: string;
-	selectors: Record< 'editor' | 'frontend', Record< string, unknown > >;
+	selectors: Record< 'editor' | 'frontend', Record< string, string > >;
 } & ( T extends undefined ? Record< string, never > : T );
