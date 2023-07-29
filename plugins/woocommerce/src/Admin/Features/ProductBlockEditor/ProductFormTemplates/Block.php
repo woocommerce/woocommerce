33756dd1b82f2984390d7bb8ee49cf3661de9f39
Matt Sherman <matt@jam123.com>
2023-07-29 08:11:58 -0400

    Add doc comments to Block

--- plugins/woocommerce/src/Admin/Features/ProductBlockEditor/ProductFormTemplates/Block.php
+++ plugins/woocommerce/src/Admin/Features/ProductBlockEditor/ProductFormTemplates/Block.php
@@ -1,10 +1,15 @@
 <?php
 /**
- * Block configuration.
+ * Block configuration used to specify blocks in BlockTemplate.
  */
 
 namespace Automattic\WooCommerce\Admin\Features\ProductBlockEditor\ProductFormTemplates;
 
+/**
+ * Class Block
+ *
+ * @package Automattic\WooCommerce\Admin\Features\ProductBlockEditor\ProductFormTemplates
+ */
 class Block implements BlockContainerInterface {
 	use BlockContainerTrait;
 
@@ -29,36 +34,57 @@ class Block implements BlockContainerInterface {
 	const ATTRIBUTES_KEY = 'attributes';
 
 	/**
+	 * The block name.
+	 *
 	 * @var string
 	 */
 	private $name;
 
 	/**
+	 * The block ID.
+	 *
 	 * @var string
 	 */
 	private $id;
 
 	/**
+	 * The block order.
+	 *
 	 * @var int
 	 */
 	private $order = 10;
 
 	/**
+	 * The block attributes.
+	 *
 	 * @var array
 	 */
 	private $attributes = [];
 
 	/**
+	 * The block template that this block belongs to.
+	 *
 	 * @var BlockTemplate
 	 */
 	private $root_template;
 
 	/**
+	 * The parent block container.
+	 *
 	 * @var BlockContainerInterface
 	 */
 	private $parent;
 
-	public function __construct( array $data, BlockTemplate &$root_template, BlockContainerInterface &$parent = null ) {
+	/**
+	 * Block constructor.
+	 *
+	 * @param array                        $config The block configuration.
+	 * @param BlockTemplate                $root_template The block template that this block belongs to.
+	 * @param BlockContainerInterface|null $parent The parent block container.
+	 * @throws \ValueError If the block configuration is invalid.
+	 * @throws \ValueError If the parent block container does not belong to the same template as the block.
+	 */
+	public function __construct( array $config, BlockTemplate &$root_template, BlockContainerInterface &$parent = null ) {
 		$this->root_template = $root_template;
 		$this->parent        = is_null( $parent ) ? $root_template : $parent;
 
@@ -66,65 +92,96 @@ class Block implements BlockContainerInterface {
 			throw new \ValueError( 'The parent block must belong to the same template as the block.' );
 		}
 
-		if ( ! isset( $data[ self::NAME_KEY ] ) || ! is_string( $data[ self::NAME_KEY ] ) ) {
+		if ( ! isset( $config[ self::NAME_KEY ] ) || ! is_string( $config[ self::NAME_KEY ] ) ) {
 			throw new \ValueError( 'The block name must be specified.' );
 		} else {
-			$this->name = $data[ self::NAME_KEY ];
+			$this->name = $config[ self::NAME_KEY ];
 		}
 
-		if ( ! isset( $data[ self::ID_KEY ] ) ) {
+		if ( ! isset( $config[ self::ID_KEY ] ) ) {
 			$this->id = $this->root_template->generate_block_id( $this->get_name() );
 		} else {
-			$this->id = $data[ self::ID_KEY ];
+			$this->id = $config[ self::ID_KEY ];
 		}
 
-		if ( isset( $data[ self::ORDER_KEY ] ) && ! is_int( $data[ self::ORDER_KEY ] ) ) {
+		if ( isset( $config[ self::ORDER_KEY ] ) && ! is_int( $config[ self::ORDER_KEY ] ) ) {
 			throw new \ValueError( 'The block order must be an integer.' );
-		} elseif ( isset( $data[ self::ORDER_KEY ] ) ) {
-			$this->order = $data[ self::ORDER_KEY ];
+		} elseif ( isset( $config[ self::ORDER_KEY ] ) ) {
+			$this->order = $config[ self::ORDER_KEY ];
 		}
 
-		if ( isset( $data[ self::ATTRIBUTES_KEY ] ) && ! is_array( $data[ self::ATTRIBUTES_KEY ] ) ) {
+		if ( isset( $config[ self::ATTRIBUTES_KEY ] ) && ! is_array( $config[ self::ATTRIBUTES_KEY ] ) ) {
 			throw new \ValueError( 'The block attributes must be an array.' );
-		} elseif ( isset( $data[ self::ATTRIBUTES_KEY ] ) ) {
-			$this->attributes = $data[ self::ATTRIBUTES_KEY ];
+		} elseif ( isset( $config[ self::ATTRIBUTES_KEY ] ) ) {
+			$this->attributes = $config[ self::ATTRIBUTES_KEY ];
 		}
-
-
 	}
 
+	/**
+	 * Get the block name.
+	 */
 	public function get_name(): string {
 		return $this->name;
 	}
 
+	/**
+	 * Get the block ID.
+	 */
 	public function get_id(): string {
 		return $this->id;
 	}
 
+	/**
+	 * Get the block order.
+	 */
 	public function get_order(): int {
 		return $this->order;
 	}
 
+	/**
+	 * Set the block order.
+	 *
+	 * @param int $order The block order.
+	 */
 	public function set_order( int $order ) {
 		$this->order = $order;
 	}
 
+	/**
+	 * Get the block attributes.
+	 */
 	public function get_attributes(): array {
 		return $this->attributes;
 	}
 
+	/**
+	 * Set the block attributes.
+	 *
+	 * @param array $attributes The block attributes.
+	 */
 	public function set_attributes( array $attributes ) {
 		$this->attributes = $attributes;
 	}
 
+	/**
+	 * Get the block template that this block belongs to.
+	 */
 	public function &get_root_template(): BlockTemplate {
 		return $this->root_template;
 	}
 
+	/**
+	 * Get the parent block container.
+	 */
 	public function &get_parent(): BlockContainerInterface {
 		return $this->parent;
 	}
 
+	/**
+	 * Get the block configuration as a simple array.
+	 *
+	 * @return array The block configuration as a simple array.
+	 */
 	public function get_as_simple_array(): array {
 		$arr = [
 			$this->get_name(),
