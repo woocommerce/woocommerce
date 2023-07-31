e0296c15fddba6bf2345e14928f77f2f43264ced
Luigi Teschio <gigitux@gmail.com>
2023-07-31 14:26:20 +0200

    fix unit test (https://github.com/woocommerce/woocommerce-blocks/pull/10409)

--- plugins/woocommerce-blocks/assets/js/blocks/classic-template/test/utils.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/classic-template/test/utils.ts
@@ -8,16 +8,10 @@ describe( 'getTemplateDetailsBySlug', function () {
 	it( 'should return single-product object when given an exact match', () => {
 		expect(
 			getTemplateDetailsBySlug( 'single-product', TEMPLATES )
-		).toBeTruthy();
-		expect(
-			getTemplateDetailsBySlug( 'single-product', TEMPLATES )
 		).toStrictEqual( TEMPLATES[ 'single-product' ] );
 	} );
 
 	it( 'should return single-product object when given a partial match', () => {
-		expect(
-			getTemplateDetailsBySlug( 'single-product-hoodie', TEMPLATES )
-		).toBeTruthy();
 		expect(
 			getTemplateDetailsBySlug( 'single-product-hoodie', TEMPLATES )
 		).toStrictEqual( TEMPLATES[ 'single-product' ] );
@@ -26,16 +20,10 @@ describe( 'getTemplateDetailsBySlug', function () {
 	it( 'should return taxonomy-product object when given a partial match', () => {
 		expect(
 			getTemplateDetailsBySlug( 'taxonomy-product_tag', TEMPLATES )
-		).toBeTruthy();
-		expect(
-			getTemplateDetailsBySlug( 'taxonomy-product_tag', TEMPLATES )
 		).toStrictEqual( TEMPLATES[ 'taxonomy-product_tag' ] );
 	} );
 
 	it( 'should return taxonomy-product object when given an exact match', () => {
-		expect(
-			getTemplateDetailsBySlug( 'taxonomy-product_brands', TEMPLATES )
-		).toBeTruthy();
 		expect(
 			getTemplateDetailsBySlug( 'taxonomy-product_brands', TEMPLATES )
 		).toStrictEqual( TEMPLATES[ 'taxonomy-product' ] );
