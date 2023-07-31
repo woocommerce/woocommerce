a94575f662f69d3f67cd051d0490be03a0af0aa8
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-07-31 08:48:41 +0200

    Store Customization > Featured Products 5-Item Grid pattern - Update the pattern to become wireframed (https://github.com/woocommerce/woocommerce-blocks/pull/10401)
    * Featured Products 5-Item Grid pattern: Ensure a placeholder image is displayed when the product doesn't have any
    
    * Ensure the wp-block-group is aligned wide.
    
    * Update text styles
    
    * Make texts translatable.
    
    * Update the Featured Products 5-Item Grid pattern

--- plugins/woocommerce-blocks/patterns/featured-products-5-cols.php
+++ plugins/woocommerce-blocks/patterns/featured-products-5-cols.php
@@ -6,38 +6,27 @@
  * Block Types: core/query/woocommerce/product-query
  */
 ?>
-<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"800"},"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}},"fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size" style="margin-bottom:var(--wp--preset--spacing--40);font-style:normal;font-weight:800">
-	Shop new arrivals
-</h2>
+<!-- wp:heading {"textAlign":"center","level":3} -->
+<h3 class="wp-block-heading has-text-align-center">
+	<?php esc_html_e( 'Shop new arrivals', 'woo-gutenberg-products-block' ); ?>
+</h3>
 <!-- /wp:heading -->
 
-<!-- wp:query {"queryId":2,"query":{"perPage":"5","pages":0,"offset":0,"postType":"product","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"__woocommerceAttributes":[],"__woocommerceStockStatus":["instock","onbackorder"]},"displayLayout":{"type":"flex","columns":5},"namespace":"woocommerce/product-query","align":"wide","layout":{"type":"default"}} -->
+<!-- wp:query {"queryId":3,"query":{"perPage":"5","pages":0,"offset":"5","postType":"product","order":"asc","orderBy":"title","author":"","search":"","exclude":[],"sticky":"","inherit":false,"__woocommerceAttributes":[],"__woocommerceStockStatus":["instock","outofstock","onbackorder"]},"namespace":"woocommerce/product-query","align":"wide","layout":{"type":"constrained"}} -->
 <div class="wp-block-query alignwide">
-	<!-- wp:post-template {"__woocommerceNamespace":"woocommerce/product-query/product-template"} -->
+	<!-- wp:post-template {"align":"wide","className":"products-block-post-template","layout":{"type":"grid","columnCount":5},"__woocommerceNamespace":"woocommerce/product-query/product-template"} -->
+	<!-- wp:woocommerce/product-image {"imageSizing":"thumbnail","isDescendentOfQueryLoop":true} /-->
 
-		<!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"minHeight":190,"minHeightUnit":"px","contentPosition":"top right","isDark":false,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
-		<div class="wp-block-cover is-light has-custom-content-position is-position-top-right" style="margin-bottom:var(--wp--preset--spacing--40);min-height:190px">
-			<span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
-			<div class="wp-block-cover__inner-container">
-				<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-				<div class="wp-block-group">
-					<!-- wp:woocommerce/product-sale-badge {"isDescendentOfQueryLoop":true,"style":{"border":{"width":"0px","style":"none","radius":"0px"},"typography":{"textTransform":"capitalize","fontSize":"12px","fontStyle":"normal","fontWeight":"400"}}} /-->
-				</div>
-				<!-- /wp:group -->
-			</div>
-		</div>
-		<!-- /wp:cover -->
+	<!-- wp:post-title {"textAlign":"left","level":3,"isLink":true,"style":{"spacing":{"margin":{"bottom":"0.75rem","top":"0"}}},"fontSize":"medium","__woocommerceNamespace":"woocommerce/product-query/product-title"} /-->
 
-		<!-- wp:post-title {"level":3,"isLink":true,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0rem"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small","__woocommerceNamespace":"woocommerce/product-query/product-title"} /-->
-
-		<!-- wp:woocommerce/product-price {"isDescendentOfQueryLoop":true,"textAlign":"left","fontSize":"small","style":{"spacing":{"margin":{"bottom":"0","top":"var:preset|spacing|20"}}}} /-->
-
+	<!-- wp:woocommerce/product-price {"isDescendentOfQueryLoop":true,"textAlign":"left","fontSize":"small"} /-->
 	<!-- /wp:post-template -->
 
+	<!-- wp:query-no-results -->
 	<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
 	<p></p>
 	<!-- /wp:paragraph -->
+	<!-- /wp:query-no-results -->
 </div>
 <!-- /wp:query -->
 
@@ -46,7 +35,7 @@
 	<!-- wp:button {"textAlign":"center"} -->
 	<div class="wp-block-button">
 		<a class="wp-block-button__link has-text-align-center wp-element-button" href="<?php echo esc_url( get_permalink( wc_get_page_id( 'shop' ) ) ); ?>">
-			Shop All
+			<?php esc_html_e( 'Shop All', 'woo-gutenberg-products-block' ); ?>
 		</a>
 	</div>
 	<!-- /wp:button -->
