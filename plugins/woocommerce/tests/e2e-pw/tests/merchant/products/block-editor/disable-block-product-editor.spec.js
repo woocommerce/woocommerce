b9c217b8ef778758d11c9ee1bc60f7e9f9596f97
Fernando Marichal <ultimoround@gmail.com>
2023-07-31 13:54:52 -0300

    Add test disable new product editor

---
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/products/block-editor/disable-block-product-editor.spec.js
@@ -0,0 +1,91 @@
+const { test, expect } = require( '@playwright/test' );
+const {
+	clickAddNewMenuItem,
+	isBlockProductEditorEnabled,
+	toggleBlockProductEditor,
+} = require( '../../../../utils/simple-products' );
+
+const ALL_PRODUCTS_URL = 'wp-admin/edit.php?post_type=product';
+const NEW_EDITOR_ADD_PRODUCT_URL =
+	'wp-admin//admin.php?page=wc-admin&path=%2Fadd-product';
+const SETTINGS_URL =
+	'wp-admin/admin.php?page=wc-settings&tab=advanced&section=features';
+
+let isNewProductEditorEnabled = false;
+
+const isTrackingSupposedToBeEnabled = !! process.env.ENABLE_TRACKING;
+
+async function dismissFeedbackModalIfShown( page ) {
+	if ( ! isTrackingSupposedToBeEnabled ) {
+		// no modal should be shown, so don't even look for button
+		return;
+	}
+
+	try {
+		await page
+			.locator( '.woocommerce-product-mvp-feedback-modal' )
+			.getByRole( 'button', { name: 'Skip' } )
+			.click( { timeout: 5000 } );
+	} catch ( error ) {}
+}
+
+async function expectOldProductEditor( page ) {
+	await expect(
+		page.locator( '#woocommerce-product-data h2' )
+	).toContainText( 'Product data' );
+}
+
+async function expectBlockProductEditor( page ) {
+	await expect(
+		page.locator( '.woocommerce-product-header__inner h1' )
+	).toContainText( 'Add new product' );
+}
+
+test.describe( 'Disable block product editor', () => {
+	test.use( { storageState: process.env.ADMINSTATE } );
+
+	test.beforeEach( async ( { page } ) => {
+		isNewProductEditorEnabled = await isBlockProductEditorEnabled( page );
+		if ( ! isNewProductEditorEnabled ) {
+			await toggleBlockProductEditor( 'enable', page );
+		}
+	} );
+
+	test.afterAll( async ( { browser } ) => {
+		const context = await browser.newContext();
+		const page = await context.newPage();
+		isNewProductEditorEnabled = await isBlockProductEditorEnabled( page );
+		if ( isNewProductEditorEnabled ) {
+			await toggleBlockProductEditor( 'disable', page );
+		}
+	} );
+
+	test.skip(
+		isNewProductEditorEnabled && isTrackingSupposedToBeEnabled,
+		'The block product editor is not being tested'
+	);
+
+	test( 'can be disabled from the header', async ( { page } ) => {
+		await page.goto( NEW_EDITOR_ADD_PRODUCT_URL );
+
+		// turn off block product editor from the header
+		await page
+			.locator( '.components-dropdown-menu' )
+			.getByRole( 'button', { name: 'Options' } )
+			.click();
+		await page
+			.getByRole( 'menuitem', {
+				name: 'Turn off the new product form',
+			} )
+			.click();
+		await dismissFeedbackModalIfShown( page );
+		await expectOldProductEditor( page );
+	} );
+	test( 'can be disabled from settings', async ( { page } ) => {
+		await toggleBlockProductEditor( 'disable', page );
+		await page.goto( ALL_PRODUCTS_URL );
+		await clickAddNewMenuItem( page );
+		await expectOldProductEditor( page );
+	} );
+	// } );
+} );
