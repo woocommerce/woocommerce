eefbf652c6ddc704d9c4b72127447cd04fe4b809
Mike Jolley <mike.jolley@me.com>
2023-08-01 12:13:18 +0100

    Refactor Store API hydration logic and prevent fatal errors from session class usage (https://github.com/woocommerce/woocommerce-blocks/pull/10373)
    * Refactor hydration logic
    
    * spacing

--- plugins/woocommerce-blocks/src/BlockTypes/AllProducts.php
+++ plugins/woocommerce-blocks/src/BlockTypes/AllProducts.php
@@ -31,18 +31,10 @@ class AllProducts extends AbstractBlock {
 		$this->asset_data_registry->add( 'max_rows', wc_get_theme_support( 'product_blocks::max_rows', 6 ), true );
 		$this->asset_data_registry->add( 'default_rows', wc_get_theme_support( 'product_blocks::default_rows', 3 ), true );
 
-		// Hydrate the following data depending on admin or frontend context.
+		// Hydrate the All Product block with data from the API. This is for the add to cart buttons which show current quantity in cart, and events.
 		if ( ! is_admin() && ! WC()->is_rest_api_request() ) {
-			$this->hydrate_from_api();
+			$this->asset_data_registry->hydrate_api_request( '/wc/store/v1/cart' );
 		}
-	}
-
-	/**
-	 * Hydrate the All Product block with data from the API. This is for the add to cart buttons which show current
-	 * quantity in cart, and events.
-	 */
-	protected function hydrate_from_api() {
-		$this->asset_data_registry->hydrate_api_request( '/wc/store/v1/cart' );
 	}
 
 	/**
