aaac65ff68af5bb310b00d1b457381f2049df964
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-08-01 19:03:12 +0200

    Revert "Add Product Gallery Thumbnails block (https://github.com/woocommerce/woocommerce-blocks/pull/10247)" (https://github.com/woocommerce/woocommerce-blocks/pull/10441)
    This reverts commit 9e4a4d715a42ff898e31a1f54bc2191bd67659e9.

--- plugins/woocommerce-blocks/assets/js/blocks/product-gallery/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-gallery/edit.tsx
@@ -1,94 +1,25 @@
 /**
  * External dependencies
  */
-import {
-	InnerBlocks,
-	InspectorControls,
-	useBlockProps,
-} from '@wordpress/block-editor';
-import { BlockEditProps, InnerBlockTemplate } from '@wordpress/blocks';
-import { useEffect } from '@wordpress/element';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
-/**
- * Internal dependencies
- */
-import {
-	moveInnerBlocksToPosition,
-	updateGroupBlockType,
-	getInnerBlocksLockAttributes,
-} from './utils';
-import { ProductGalleryThumbnailsBlockSettings } from './inner-blocks/product-gallery-thumbnails/block-settings';
-import { ProductGalleryBlockSettings } from './block-settings/index';
-import type {
-	ProductGalleryThumbnailsBlockAttributes,
-	ProductGalleryBlockAttributes,
-} from './types';
-
-const TEMPLATE: InnerBlockTemplate[] = [
-	[
-		'core/group',
-		{ layout: { type: 'flex' } },
-		[
-			[
-				'woocommerce/product-gallery-thumbnails',
-				getInnerBlocksLockAttributes( 'lock' ),
-			],
-			[
-				'woocommerce/product-gallery-large-image',
-				getInnerBlocksLockAttributes( 'lock' ),
-			],
-		],
-	],
-];
-
-export const Edit = ( {
-	clientId,
-	attributes,
-	setAttributes,
-}: BlockEditProps<
-	ProductGalleryThumbnailsBlockAttributes & ProductGalleryBlockAttributes
-> ) => {
+export const Edit = (): JSX.Element => {
 	const blockProps = useBlockProps();
-
-	// Update the Group block type when the thumbnailsPosition attribute changes.
-	updateGroupBlockType( attributes, clientId );
-
-	useEffect( () => {
-		setAttributes( {
-			...attributes,
-			productGalleryClientId: clientId,
-		} );
-		// Move the Thumbnails block to the correct above or below the Large Image based on the thumbnailsPosition attribute.
-		moveInnerBlocksToPosition( attributes, clientId );
-	}, [ setAttributes, attributes, clientId ] );
 
 	return (
 		<div { ...blockProps }>
-			<InspectorControls>
-				<ProductGalleryThumbnailsBlockSettings
-					attributes={ attributes }
-					setAttributes={ setAttributes }
-					context={ {
-						productGalleryClientId: clientId,
-						thumbnailsPosition: attributes.thumbnailsPosition,
-						thumbnailsNumberOfThumbnails:
-							attributes.thumbnailsNumberOfThumbnails,
-					} }
-				/>
-			</InspectorControls>
-			<InspectorControls>
-				<ProductGalleryBlockSettings
-					attributes={ attributes }
-					setAttributes={ setAttributes }
-				/>
-			</InspectorControls>
 			<InnerBlocks
-				allowedBlocks={ [
-					'woocommerce/product-gallery-large-image',
-					'woocommerce/product-gallery-thumbnails',
-				] }
-				template={ TEMPLATE }
+				allowedBlocks={ [ 'woocommerce/product-gallery-large-image' ] }
+				templateLock={ false }
 			/>
+		</div>
+	);
+};
+
+export const Save = (): JSX.Element => {
+	return (
+		<div { ...useBlockProps.save() }>
+			<InnerBlocks.Content />
 		</div>
 	);
 };
