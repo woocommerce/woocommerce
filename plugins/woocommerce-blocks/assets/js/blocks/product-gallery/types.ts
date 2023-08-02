d3cae8dbb417c7ef7dbb83de749329d1ec1695ff
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-08-02 12:43:08 +0200

    Product Gallery: Add Crop, Zoom and Full-screen settings (https://github.com/woocommerce/woocommerce-blocks/pull/10445)
    * WIP Product Gallery: Add the Thumbnails block
    
    * Product Gallery Thumbnails: Add block settings
    
    * Add template for the Product Gallery block
    
    * Add template for the Product Gallery block. Add the rest of the files.
    
    * Product Gallery Thumbnails: Add context settings sharing between the Product Gallery and Thumbnails block.
    
    * Product Gallery Thumbnails: Add UI functionality and frontend functionality. Add settings for the Thumbnails in both places - Product Gallery and the Thumbnails block.
    
    * Product Gallery Thumbnails: Move the static template ouside of the component
    
    * Make sure the context is set before accesing the array values
    
    * Product Gallery Thumbnails: Move the setGroupAttributes() function outside of the component
    
    * Product Gallery Thumbnails: Fix TS errors
    
    * Product Gallery Thumbnails: Update the Features Flags and Experimental Interfaces doc
    
    * Product Gallery Thumbnails: Fix TS error
    
    * Product Gallery Thumbnails: Remove unused stylesheet
    
    * Product Gallery Thumbnails: Fix TS errors
    
    * Product Gallery Thumbnails: Remove unused context and fix the thumbnails bottom position styling on the frontend.
    
    * Product Gallery Thumbnails: Allow the user to move the horizontal thumbnails above the large image and don't overwrite that automatically
    
    * Product Gallery Thumbnails: Add code comments and remove the incorrect conditional check when moving thumbnails up and down
    
    * Product Gallery Thumbnails: Fix the eslint dependency error
    
    * Product Gallery Thumbnails: Refactor Product Gallery edit code and move the logic to a utils file
    
    * Product Gallery Thumbnails: Update the utils file
    
    * Product Gallery Thumbnails: Update the utils file. Fix comment indentation
    
    * Product Gallery Thumbnails: Fix undefined variable html when only 1 product image is set
    
    * Product Gallery: Rename clientId to productGalleryClientId
    
    * Product Gallery Thumbnails: Combine the useEffect code having the same dependencies
    
    * Product Gallery Thumbnails: Combine all useEffect code together
    
    * Product Gallery Thumbnails: Add a ThumbnailsPosition enum
    
    * Product Gallery Thumbnails: Update the thumbnailsPosition to an enum
    
    * Product Gallery Thumbnails: Fix TS errors
    
    * Product Gallery Thumbnails: Fix TS errors
    
    * Product Gallery Thumbnails: Add missing dependency
    
    * Product Gallery Thumbnails: Uppercase the enum and fix the thumbnails position bug when initially adding the Product Gallery block
    
    * Product Gallery: Add crop, zoom and full-screen settings
    
    * Product Gallery Thumbnails: Replace ts-ignore with ts-expect-error
    
    * Product Gallery Thumbnails: Replace ts-ignore with ts-expect-error
    
    * Product Gallery Thumbnails: Revert back to ts-ignore
    
    * Revert "Product Gallery: Add crop, zoom and full-screen settings"
    
    This reverts commit f138ee0e947ccd44cadd04265f7b6de3f3c00195.
    
    * Product Gallery: Add crop, zoom and full-screen settings
    
    * Product Gallery: Remove the redundant React Fragment

--- plugins/woocommerce-blocks/assets/js/blocks/product-gallery/types.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-gallery/types.ts
@@ -3,10 +3,37 @@
  */
 import { ThumbnailsPosition } from './inner-blocks/product-gallery-thumbnails/constants';
 
-export interface BlockAttributes {
+export interface ProductGalleryBlockAttributes {
+	cropImages?: boolean;
+	hoverZoom?: boolean;
+	fullScreenOnClick?: boolean;
+}
+
+export interface ProductGalleryThumbnailsBlockAttributes {
 	thumbnailsPosition: ThumbnailsPosition;
 	thumbnailsNumberOfThumbnails: number;
 	productGalleryClientId: string;
+}
+
+export interface ProductGalleryBlockEditProps {
+	clientId: string;
+	attributes: ProductGalleryThumbnailsBlockAttributes;
+	setAttributes: (
+		newAttributes: ProductGalleryThumbnailsBlockAttributes
+	) => void;
+}
+
+export interface ProductGallerySettingsProps {
+	attributes: ProductGalleryBlockAttributes;
+	setAttributes: ( attributes: ProductGalleryBlockAttributes ) => void;
+}
+
+export interface ProductGalleryThumbnailsSettingsProps {
+	attributes: ProductGalleryThumbnailsBlockAttributes;
+	setAttributes: (
+		attributes: ProductGalleryThumbnailsBlockAttributes
+	) => void;
+	context: ProductGalleryThumbnailsBlockAttributes;
 }
 
 export interface Context {
@@ -15,16 +42,4 @@ export interface Context {
 		thumbnailsNumberOfThumbnails: number;
 		productGalleryClientId: string;
 	};
-}
-
-export interface ProductGalleryBlockEditProps {
-	clientId: string;
-	attributes: BlockAttributes;
-	setAttributes: ( newAttributes: BlockAttributes ) => void;
-}
-
-export interface ThumbnailsSettingProps {
-	attributes: BlockAttributes;
-	context: BlockAttributes;
-	setAttributes: ( attributes: BlockAttributes ) => void;
 }
