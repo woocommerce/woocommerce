c4a31a6cf89a49371151e2e2d98b101dacf91dbc
Sam Seay <samueljseay@gmail.com>
2023-08-02 14:42:20 +0800

    Install jest, add tests folder.

---
+++ tools/code-analyzer/src/lib/__tests__/template-changes-test.ts
@@ -0,0 +1,7 @@
+describe( 'scanForTemplateChanges', () => {
+	it( 'should return an empty Map if the content does not contain any template changes', async () => {} );
+
+	it( 'should return a Map with a single entry if the content contains a single template change', async () => {} );
+
+	it( 'should return a Map with multiple entries if the content contains multiple template changes', async () => {} );
+} );
