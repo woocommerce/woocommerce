bb3e404facef76f1f25d6f5e893b82836e29a513
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2023-08-02 23:08:25 +0530

    Add sync and usage flags to legacy features. (#39537)
    * Add sync and usage flags to legacy features.
    
    * Use transient instead of constant.
    
    * Add unit test and changelog.
    
    * Add more robustness to unit test by adding more setting values.
    
    * PHPCS fixes
    
    ---------
    
    Co-authored-by: Jorge Torres <jorge.torres@automattic.com>

--- plugins/woocommerce/tests/php/src/Internal/DataStores/Orders/OrdersTableDataStoreTests.php
+++ plugins/woocommerce/tests/php/src/Internal/DataStores/Orders/OrdersTableDataStoreTests.php
@@ -106,6 +106,7 @@ class OrdersTableDataStoreTests extends HposTestCase {
 	 */
 	public function test_backfill_post_record() {
 		$post_order_id = OrderHelper::create_complex_wp_post_order();
+		$this->disable_cot_sync();
 		$this->migrator->migrate_orders( array( $post_order_id ) );
 
 		$post_data             = get_post( $post_order_id, ARRAY_A );
@@ -1872,6 +1873,7 @@ class OrdersTableDataStoreTests extends HposTestCase {
 	 */
 	public function test_legacy_getters_setters() {
 		$this->toggle_cot_feature_and_usage( true );
+		$this->disable_cot_sync();
 		$order_id = OrderHelper::create_complex_data_store_order( $this->sut );
 		$order    = wc_get_order( $order_id );
 		$this->switch_data_store( $order, $this->sut );
@@ -2001,6 +2003,7 @@ class OrdersTableDataStoreTests extends HposTestCase {
 	 */
 	public function test_read_multiple_dont_sync_again_for_same_order() {
 		$this->toggle_cot_feature_and_usage( true );
+		$this->disable_cot_sync();
 		$order = $this->create_complex_cot_order();
 		$this->sut->backfill_post_record( $order );
 		$this->enable_cot_sync();
