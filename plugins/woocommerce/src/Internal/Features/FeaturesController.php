e247c29b99cc4d8618dcd9b1d0110a046b0b1d5e
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2023-08-02 17:43:05 +0530

    Add sync and usage flags to legacy features.

--- plugins/woocommerce/src/Internal/Features/FeaturesController.php
+++ plugins/woocommerce/src/Internal/Features/FeaturesController.php
@@ -132,7 +132,14 @@ class FeaturesController {
 			),
 		);
 
-		$this->legacy_feature_ids = array( 'analytics', 'new_navigation', 'product_block_editor' );
+		$this->legacy_feature_ids = array(
+			'analytics',
+			'new_navigation',
+			'product_block_editor',
+			// Compatibility for COT is determined by `custom_order_tables'.
+			CustomOrdersTableController::CUSTOM_ORDERS_TABLE_USAGE_ENABLED_OPTION,
+			DataSynchronizer::ORDERS_DATA_SYNC_ENABLED_OPTION,
+		);
 
 		$this->init_features( $features );
 
