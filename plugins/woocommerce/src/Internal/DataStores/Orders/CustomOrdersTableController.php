5e05b7a99ed8ca0682bbde250cd2b76e4d7819e1
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2023-08-02 13:05:23 +0530

    Connect switching on the option with queuing sync.

--- plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
@@ -273,8 +273,11 @@ class CustomOrdersTableController {
 	 * @param mixed  $value New value of the setting.
 	 */
 	private function process_updated_option( $option, $old_value, $value ) {
-		if ( DataSynchronizer::ORDERS_DATA_SYNC_ENABLED_OPTION === $option && 'no' === $value ) {
-			$this->data_synchronizer->cleanup_synchronization_state();
+		if ( DataSynchronizer::ORDERS_DATA_SYNC_ENABLED_OPTION === $option ) {
+			$this->handle_data_sync_option_changed();
+			if ( 'no' === $value ) {
+				$this->data_synchronizer->cleanup_synchronization_state();
+			}
 		}
 	}
 
