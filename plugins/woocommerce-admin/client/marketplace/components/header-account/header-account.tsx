a8070402085b69d734843622eb8f02035341b901
Kyle Nel <22053773+kdevnel@users.noreply.github.com>
2023-08-03 10:30:26 +0200

    Add/user menu component (#39383)
    User Menu: Create component and add to header
    
    - Create initial component design
    - We may need to return to implement the helper connection URLs once those are in place.
    - Some PHPCS errors still exist - this is to be expected for now.
    
    # Conflicts:
    #	plugins/woocommerce-admin/client/marketplace/stylesheets/_variables.scss
    
    Co-authored-by: And Finally <andfinally@users.noreply.github.com>
    Co-authored-by: raicem <unalancem@gmail.com>

--- plugins/woocommerce-admin/client/marketplace/components/header-account/header-account.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/header-account/header-account.tsx
@@ -1,32 +1,143 @@
 /**
  * External dependencies
  */
+import { useState } from '@wordpress/element';
+import { DropdownMenu, MenuGroup, MenuItem } from '@wordpress/components';
+import { Icon, commentAuthorAvatar, external, linkOff } from '@wordpress/icons';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
+import './header-account.scss';
+import { getAdminSetting } from '../../../utils/admin-settings';
+import HeaderAccountModal from './header-account-modal';
 
-export default function HeaderAccount() {
-	return (
-		<a
-			className="woocommerce-marketplace__header-account"
-			href="https://woocommerce.com/my-account/"
-		>
-			<svg
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					id="Union"
-					fillRule="evenodd"
-					clipRule="evenodd"
-					d="M7.25 16.4371C6.16445 15.2755 5.5 13.7153 5.5 12C5.5 8.41015 8.41015 5.5 12 5.5C15.5899 5.5 18.5 8.41015 18.5 12C18.5 13.7153 17.8356 15.2755 16.75 16.4371V16C16.75 14.4812 15.5188 13.25 14 13.25L10 13.25C8.48122 13.25 7.25 14.4812 7.25 16V16.4371ZM8.75 17.6304C9.70606 18.1835 10.8161 18.5 12 18.5C13.1839 18.5 14.2939 18.1835 15.25 17.6304V16C15.25 15.3096 14.6904 14.75 14 14.75L10 14.75C9.30964 14.75 8.75 15.3096 8.75 16V17.6304ZM4 12C4 7.58172 7.58172 4 12 4C16.4183 4 20 7.58172 20 12C20 16.4183 16.4183 20 12 20C7.58172 20 4 16.4183 4 12ZM14 10C14 11.1046 13.1046 12 12 12C10.8954 12 10 11.1046 10 10C10 8.89543 10.8954 8 12 8C13.1046 8 14 8.89543 14 10Z"
-					fill="black"
+export default function HeaderAccount(): JSX.Element {
+	const [ isModalOpen, setIsModalOpen ] = useState( false );
+	const openModal = () => setIsModalOpen( true );
+
+	const wccomSettings = getAdminSetting( 'wccomHelper', {} );
+	const isConnected = wccomSettings?.isConnected ?? false;
+	const connectionURL = wccomSettings?.connectURL ?? '';
+	const userEmail = wccomSettings?.userEmail;
+	const avatarURL = wccomSettings?.userAvatar ?? commentAuthorAvatar;
+
+	// This is a hack to prevent TypeScript errors. The MenuItem component passes these as an href prop to the underlying button
+	// component. That component is either an anchor with href if provided or a button that won't accept an href if no href is provided.
+	// Due to early erroring of TypeScript, it only takes the button version into account which doesn't accept href.
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const accountURL: any = 'https://woocommerce.com/my-dashboard/';
+	const accountOrConnect = isConnected ? accountURL : connectionURL;
+
+	const avatar = () => {
+		if ( ! isConnected ) {
+			return commentAuthorAvatar;
+		}
+
+		return (
+			<img
+				src={ avatarURL }
+				alt=""
+				className="woocommerce-marketplace__menu-avatar-image"
+			/>
+		);
+	};
+
+	const connectionStatusText = isConnected
+		? __( 'Connected', 'woocommerce' )
+		: __( 'Not Connected', 'woocommerce' );
+
+	const connectionDetails = () => {
+		if ( isConnected ) {
+			return (
+				<>
+					<Icon
+						icon={ commentAuthorAvatar }
+						size={ 24 }
+						className="woocommerce-marketplace__menu-icon"
+					/>
+					<span className="woocommerce-marketplace__main-text">
+						{ userEmail }
+					</span>
+				</>
+			);
+		}
+		return (
+			<>
+				<Icon
+					icon={ commentAuthorAvatar }
+					size={ 24 }
+					className="woocommerce-marketplace__menu-icon"
 				/>
-			</svg>
-		</a>
+				<div className="woocommerce-marketplace__menu-text">
+					{ __( 'Connect account', 'woocommerce' ) }
+					<span className="woocommerce-marketplace__sub-text">
+						{ __(
+							'Manage your subscriptions, get updates and support for your extensions and themes.',
+							'woocommerce'
+						) }
+					</span>
+				</div>
+			</>
+		);
+	};
+
+	return (
+		<>
+			<DropdownMenu
+				className="woocommerce-marketplace__user-menu"
+				icon={ avatar() }
+				label={ __( 'User options', 'woocommerce' ) }
+			>
+				{ () => (
+					<>
+						<MenuGroup
+							className="woocommerce-layout__homescreen-display-options"
+							label={ connectionStatusText }
+						>
+							<MenuItem
+								className="woocommerce-marketplace__menu-item"
+								href={ accountOrConnect }
+							>
+								{ connectionDetails() }
+							</MenuItem>
+							<MenuItem href={ accountURL }>
+								<Icon
+									icon={ external }
+									size={ 24 }
+									className="woocommerce-marketplace__menu-icon"
+								/>
+								{ __(
+									'WooCommerce.com account',
+									'woocommerce'
+								) }
+							</MenuItem>
+						</MenuGroup>
+						{ isConnected && (
+							<MenuGroup className="woocommerce-layout__homescreen-display-options">
+								<MenuItem onClick={ openModal }>
+									<Icon
+										icon={ linkOff }
+										size={ 24 }
+										className="woocommerce-marketplace__menu-icon"
+									/>
+									{ __(
+										'Disconnect account',
+										'woocommerce'
+									) }
+								</MenuItem>
+							</MenuGroup>
+						) }
+					</>
+				) }
+			</DropdownMenu>
+			{ isModalOpen && (
+				<HeaderAccountModal
+					setIsModalOpen={ setIsModalOpen }
+					disconnectURL={ connectionURL }
+				/>
+			) }
+		</>
 	);
 }
