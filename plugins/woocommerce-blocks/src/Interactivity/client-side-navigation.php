77ae41e19cc42045f6b63761c068936ba91e466d
David Arenas <david.arenas@automattic.com>
2023-08-03 10:55:13 +0200

    Interactivity API: Update interactive regions during client-side navigation (https://github.com/woocommerce/woocommerce-blocks/pull/10200)
    * Update router to hydrate only interactive regions
    
    * Rename link directive to navigation-link
    
    * Add navigation directives to Query and Pagination blocks
    
    * Enable the Interactivity API by default
    
    * Remove client-side navigation meta tag
    
    * Cache initial regions
    
    * Move data-wc-interactive from query to query-pagination
    
    * Add woo prefix to navigation id
    
    * Add keys and move wc-interactive back to the query block
    
    * Reuse root fragments for each interactive region
    
    * Fix navigation-id retrieval

--- plugins/woocommerce-blocks/src/Interactivity/client-side-navigation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Print client-side navigation meta tag (hard-coded for now).
- */
-function woocommerce_interactivity_add_client_side_navigation_meta_tag() {
-	echo '<meta itemprop="wc-client-side-navigation" content="active">';
-}
-add_action( 'wp_head', 'woocommerce_interactivity_add_client_side_navigation_meta_tag' );
