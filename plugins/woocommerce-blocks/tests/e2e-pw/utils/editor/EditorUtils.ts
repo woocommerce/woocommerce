c411137ba45c9c84c1fd1822f8547cc40e8d1005
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-08-04 13:07:31 +0200

    E2E: Product Gallery Thumbnails block tests (https://github.com/woocommerce/woocommerce-blocks/pull/10370)
    * WIP Product Gallery: Add the Thumbnails block
    
    * Product Gallery Thumbnails: Add block settings
    
    * Add template for the Product Gallery block
    
    * Add template for the Product Gallery block. Add the rest of the files.
    
    * Product Gallery Thumbnails: Add context settings sharing between the Product Gallery and Thumbnails block.
    
    * Product Gallery Thumbnails: Add UI functionality and frontend functionality. Add settings for the Thumbnails in both places - Product Gallery and the Thumbnails block.
    
    * Product Gallery Thumbnails: Move the static template ouside of the component
    
    * Make sure the context is set before accesing the array values
    
    * Product Gallery Thumbnails: Move the setGroupAttributes() function outside of the component
    
    * Product Gallery Thumbnails: Fix TS errors
    
    * Product Gallery Thumbnails: Update the Features Flags and Experimental Interfaces doc
    
    * Product Gallery Thumbnails: Fix TS error
    
    * Product Gallery Thumbnails: Remove unused stylesheet
    
    * Product Gallery Thumbnails: Fix TS errors
    
    * Product Gallery Thumbnails: Remove unused context and fix the thumbnails bottom position styling on the frontend.
    
    * Product Gallery Thumbnails: Allow the user to move the horizontal thumbnails above the large image and don't overwrite that automatically
    
    * E2E: Add tests for the Product Gallery Thumbnails block
    
    * Product Gallery Thumbnails: Add code comments and remove the incorrect conditional check when moving thumbnails up and down
    
    * Product Gallery Thumbnails: Add failure handling
    
    * Product Gallery Thumbnails: Fix the eslint dependency error
    
    * Product Gallery Thumbnails: Add inner blocks to the sideEffects array
    
    * Product Gallery Thumbnails: Refactor Product Gallery edit code and move the logic to a utils file
    
    * Product Gallery Thumbnails: Update the utils file
    
    * Product Gallery Thumbnails: Update the utils file. Fix comment indentation
    
    * Product Gallery Thumbnails: Fix failing tests
    
    * Revert unrelated package.json changes
    
    * Product Gallery Thumbnails: Further package.json reverts
    
    * Product Gallery Thumbnails: Rename the test screenshots
    
    * Product Gallery Thumbnails: Fix undefined variable html when only 1 product image is set
    
    * Product Gallery: Rename clientId to productGalleryClientId
    
    * Product Gallery Thumbnails: Combine the useEffect code having the same dependencies
    
    * Product Gallery Thumbnails: Combine all useEffect code together
    
    * Product Gallery Thumbnails: Add a ThumbnailsPosition enum
    
    * Product Gallery Thumbnails: Update the thumbnailsPosition to an enum
    
    * Product Gallery Thumbnails: Fix TS errors
    
    * Product Gallery Thumbnails: Fix TS errors
    
    * Product Gallery Thumbnails: Add missing dependency
    
    * Product Gallery Thumbnails: Uppercase the enum and fix the thumbnails position bug when initially adding the Product Gallery block
    
    * Product Gallery Thumbnails: Fix E2E tests
    
    * Product Gallery Thumbnails: Remove unused function from frontend utils
    
    * Product Gallery Thumbnails: Remove unused screenshots and config amendment
    
    * Product Gallery Thumbnails: Add check for the order of block on the frontend
    
    * Product Gallery: Add crop, zoom and full-screen settings
    
    * Product Gallery Thumbnails: Replace ts-ignore with ts-expect-error
    
    * Product Gallery Thumbnails: Replace ts-ignore with ts-expect-error
    
    * Product Gallery Thumbnails: Revert back to ts-ignore
    
    * Revert "Product Gallery: Add crop, zoom and full-screen settings"
    
    This reverts commit 840654197619e2611029b81990493387ae0b543d.
    
    * Product Gallery: Add crop, zoom and full-screen settings
    
    * Product Gallery: Remove the redundant React Fragment
    
    * Product Gallery E2E: Simplify and combine the tests

--- plugins/woocommerce-blocks/tests/e2e-pw/utils/editor/EditorUtils.ts
+++ plugins/woocommerce-blocks/tests/e2e-pw/utils/editor/EditorUtils.ts
@@ -17,6 +17,15 @@ export class EditorUtils {
 		return this.editor.canvas.locator( `[data-type="${ name }"]` );
 	}
 
+	async getBlockByTypeWithParent( name: string, parentName: string ) {
+		const parentBlock = await this.getBlockByName( parentName );
+		if ( ! parentBlock ) {
+			throw new Error( `Parent block "${ parentName }" not found.` );
+		}
+		const block = await parentBlock.locator( `[data-type="${ name }"]` );
+		return block;
+	}
+
 	// todo: Make a PR to @wordpress/e2e-test-utils-playwright to add this method.
 	/**
 	 * Inserts a block after a given client ID.
@@ -70,5 +79,49 @@ export class EditorUtils {
 			{ timeout: 3000 }
 		);
 		await this.editor.canvas.click( 'body' );
+	}
+
+	async isBlockEarlierThan< T >(
+		containerBlock: T,
+		firstBlock: string,
+		secondBlock: string
+	) {
+		const container =
+			containerBlock instanceof Function
+				? await containerBlock()
+				: containerBlock;
+
+		if ( ! container ) {
+			throw new Error( 'Container block not found.' );
+		}
+
+		const childBlocks = container.locator( ':scope > .wp-block' );
+
+		let firstBlockIndex = -1;
+		let secondBlockIndex = -1;
+
+		for ( let i = 0; i < ( await childBlocks.count() ); i++ ) {
+			const blockName = await childBlocks
+				.nth( i )
+				.getAttribute( 'data-type' );
+
+			if ( blockName === firstBlock ) {
+				firstBlockIndex = i;
+			}
+
+			if ( blockName === secondBlock ) {
+				secondBlockIndex = i;
+			}
+
+			if ( firstBlockIndex !== -1 && secondBlockIndex !== -1 ) {
+				break;
+			}
+		}
+
+		if ( firstBlockIndex === -1 || secondBlockIndex === -1 ) {
+			throw new Error( 'Both blocks must exist within the editor' );
+		}
+
+		return firstBlockIndex < secondBlockIndex;
 	}
 }
