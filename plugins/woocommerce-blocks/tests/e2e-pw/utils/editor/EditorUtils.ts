cba5d59faf4e85b4399f0b2c60fee3b58105605d
Thomas Roberts <5656702+opr@users.noreply.github.com>
2023-08-07 16:59:06 +0100

    Preliminary work to add Playwright E2E tests for the Cart block & merchant cart tests (https://github.com/woocommerce/woocommerce-blocks/pull/9739)
    * Add cartCheckout project
    
    * Add navigation utils
    
    * Add global block inserter utils to EditorUtils class
    
    * Add initial Cart block tests
    
    * Add slug to block data type
    
    * Get block editor selector when navigating in editor
    
    * add test for inner block filtering
    
    * Update navigation util to use block name instead of title
    
    * Add view switcher test
    
    * Update block name/title
    
    * Run only cart/checkout tests in cartCheckout project
    
    * Remove old comment
    
    * Update selector to use locator instead
    
    * Revert selector type change
    
    * Fix TS issues
    
    * Remove unnecessary admin state
    
    * Update tests to use editorUtils and fix TS issue with selector
    
    * Replace string concatenation with template
    
    * Rename cart test to contain block_theme suffix
    
    * Change page to editor.page and use canvas where necessary
    
    * Rename directory to cart-checkout
    
    * Wait for editor response before continuing
    
    * Remove cartcheckout playwright project
    
    * Remove typecast and switch to .toNotBeHidden
    
    * Remove double comment
    
    * Force click the empty cart button
    
    * Wait for empty cart button for longer
    
    * Isolate test to run in CI
    
    * Dispatch click event instead of simulating click
    
    * Remove timeouts
    
    * Revert "Isolate test to run in CI"
    
    This reverts commit 28af5a50700476639d0a4f12fca9800a4a5aed91.

--- plugins/woocommerce-blocks/tests/e2e-pw/utils/editor/EditorUtils.ts
+++ plugins/woocommerce-blocks/tests/e2e-pw/utils/editor/EditorUtils.ts
@@ -73,6 +73,55 @@ export class EditorUtils {
 		}, clientId );
 	}
 
+	/**
+	 * Toggles the global inserter.
+	 */
+	async toggleGlobalBlockInserter() {
+		// "Add block" selector is required to make sure performance comparison
+		// doesn't fail on older branches where we still had "Add block" as label.
+		await this.page.click(
+			'.edit-post-header [aria-label="Add block"],' +
+				'.edit-site-header [aria-label="Add block"],' +
+				'.edit-post-header [aria-label="Toggle block inserter"],' +
+				'.edit-site-header [aria-label="Toggle block inserter"],' +
+				'.edit-widgets-header [aria-label="Add block"],' +
+				'.edit-widgets-header [aria-label="Toggle block inserter"],' +
+				'.edit-site-header-edit-mode__inserter-toggle'
+		);
+	}
+
+	/**
+	 * Checks if the global inserter is open.
+	 *
+	 * @return {Promise<boolean>} Whether the inserter is open or not.
+	 */
+	async isGlobalInserterOpen() {
+		return await this.page.evaluate( () => {
+			// "Add block" selector is required to make sure performance comparison
+			// doesn't fail on older branches where we still had "Add block" as
+			// label.
+			return !! document.querySelector(
+				'.edit-post-header [aria-label="Add block"].is-pressed,' +
+					'.edit-site-header-edit-mode [aria-label="Add block"].is-pressed,' +
+					'.edit-post-header [aria-label="Toggle block inserter"].is-pressed,' +
+					'.edit-site-header [aria-label="Toggle block inserter"].is-pressed,' +
+					'.edit-widgets-header [aria-label="Toggle block inserter"].is-pressed,' +
+					'.edit-widgets-header [aria-label="Add block"].is-pressed,' +
+					'.edit-site-header-edit-mode__inserter-toggle.is-pressed'
+			);
+		} );
+	}
+
+	/**
+	 * Opens the global inserter.
+	 */
+	async openGlobalBlockInserter() {
+		if ( ! ( await this.isGlobalInserterOpen() ) ) {
+			await this.toggleGlobalBlockInserter();
+			await this.page.waitForSelector( '.block-editor-inserter__menu' );
+		}
+	}
+
 	async enterEditMode() {
 		await this.editor.page.waitForSelector(
 			'.edit-site-visual-editor__editor-canvas[role="button"]',
