b56654867afc565c54f1d6e6e412228c0116e5c7
And Finally <andfinally@users.noreply.github.com>
2023-08-08 15:29:08 +0100

    Changes from `add/marketplace-product-card-component` after rebasing on feature branch.

--- plugins/woocommerce-admin/client/marketplace/components/discover/discover.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/discover/discover.tsx
@@ -1,16 +1,41 @@
 /**
  * External dependencies
  */
+import { useEffect, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
+import ProductList from '../product-list/product-list';
+import { fetchDiscoverPageData, ProductGroup } from '../../utils/functions';
 import './discover.scss';
 
-export default function Discover(): JSX.Element {
+export default function Discover(): JSX.Element | null {
+	const [ productGroups, setProductGroups ] = useState<
+		Array< ProductGroup >
+	>( [] );
+
+	useEffect( () => {
+		fetchDiscoverPageData().then( ( products: Array< ProductGroup > ) => {
+			setProductGroups( products );
+		} );
+	}, [] );
+
+	if ( ! productGroups.length ) {
+		return null;
+	}
+
+	const groupsList = productGroups.flatMap( ( group ) => group );
 	return (
 		<div className="woocommerce-marketplace__discover">
-			<h1>Discover Our Favorites</h1>
+			{ groupsList.map( ( groups ) => (
+				<ProductList
+					key={ groups.id }
+					title={ groups.title }
+					products={ groups.items }
+					groupURL={ groups.url }
+				/>
+			) ) }
 		</div>
 	);
 }
--- plugins/woocommerce-admin/client/marketplace/components/product-list/product-list.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/product-list/product-list.tsx
@@ -5,22 +5,24 @@
 /**
  * Internal dependencies
  */
-import CategorySelector from '../category-selector/category-selector';
-import ProductListContent from '../product-list-content/product-list-content';
+import ProductListContent, {
+	Product,
+} from '../product-list-content/product-list-content';
 import ProductListHeader from '../product-list-header/product-list-header';
 
 interface ProductListProps {
 	title: string;
+	products: Product[];
+	groupURL: string;
 }
 
 export default function ProductList( props: ProductListProps ): JSX.Element {
-	const { title } = props;
+	const { title, products, groupURL } = props;
 
 	return (
 		<div className="woocommerce-marketplace__product-list">
-			<ProductListHeader title={ title } />
-			<CategorySelector />
-			<ProductListContent />
+			<ProductListHeader title={ title } groupURL={ groupURL } />
+			<ProductListContent products={ products } />
 		</div>
 	);
 }
