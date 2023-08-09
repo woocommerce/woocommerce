5bac7bc09d61d7c032765446a1df686192cf1cf7
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-08-09 11:18:34 -0400

    Use variations-table component in variation-items block

--- packages/js/product-editor/src/blocks/variation-items/edit.tsx
+++ packages/js/product-editor/src/blocks/variation-items/edit.tsx
@@ -7,9 +7,14 @@ import { createElement } from '@wordpress/element';
 /**
  * Internal dependencies
  */
+import { VariationsTable } from '../../components/variations-table';
 
 export function Edit() {
 	const blockProps = useBlockProps();
 
-	return <div { ...blockProps }></div>;
+	return (
+		<div { ...blockProps }>
+			<VariationsTable />
+		</div>
+	);
 }
