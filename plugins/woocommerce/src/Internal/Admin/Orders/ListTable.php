722bbd8147f5a3914db04b08af0baafaf00dfce8
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2023-08-09 15:01:02 +0530

    Add filter to allow preventing deletion.

--- plugins/woocommerce/src/Internal/Admin/Orders/ListTable.php
+++ plugins/woocommerce/src/Internal/Admin/Orders/ListTable.php
@@ -1340,13 +1340,11 @@ class ListTable extends WP_List_Table {
 	 * @return int Number of orders that were trashed.
 	 */
 	private function do_delete( array $ids, bool $force_delete = false ): int {
-		$orders_store = wc_get_container()->get( OrdersTableDataStore::class );
-		$delete_args  = $force_delete ? array( 'force_delete' => true ) : array();
 		$changed      = 0;
 
 		foreach ( $ids as $id ) {
 			$order = wc_get_order( $id );
-			$orders_store->delete( $order, $delete_args );
+			$order->delete( $force_delete );
 			$updated_order = wc_get_order( $id );
 
 			if ( ( $force_delete && false === $updated_order ) || ( ! $force_delete && $updated_order->get_status() === 'trash' ) ) {
