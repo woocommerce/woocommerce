b0f92797ceca433f71dae7462fca483055bc77e4
Fernando Marichal <ultimoround@gmail.com>
2023-08-10 16:45:42 -0300

    Remove warning

--- packages/js/product-editor/src/blocks/index.ts
+++ packages/js/product-editor/src/blocks/index.ts
@@ -25,4 +25,3 @@ export { init as initVariations } from './variations';
 export { init as initRequirePassword } from './password';
 export { init as initVariationItems } from './variation-items';
 export { init as initVariationOptions } from './variation-options';
-export { init as initWarning } from './warning';
