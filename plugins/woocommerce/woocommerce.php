17088ae6f75836b8586ebf1d3bcb4e186dba05b2
jonathansadowski <jonathansadowski@users.noreply.github.com>
2023-08-11 11:44:02 -0500

    Prepare woocommerce.php for 8.0.2 release

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
- * Version: 8.0.1
+ * Version: 8.0.2
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
