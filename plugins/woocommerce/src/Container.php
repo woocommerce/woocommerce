aef67e43072cad451e5ba67a0babbf7b062cb98b
Jeremy Pry <jeremy.pry@gmail.com>
2023-08-15 18:25:26 -0400

    Create separate service provider for Order Attribution

--- plugins/woocommerce/src/Container.php
+++ plugins/woocommerce/src/Container.php
@@ -19,6 +19,7 @@ use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\OrderM
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\ObjectCacheServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\OrdersDataStoreServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\OptionSanitizerServiceProvider;
+use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\OrderSourceAttributionServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\ProductAttributesLookupServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\ProductDownloadsServiceProvider;
 use Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders\ProductReviewsServiceProvider;
@@ -64,6 +65,7 @@ final class Container {
 		UtilsClassesServiceProvider::class,
 		COTMigrationServiceProvider::class,
 		OrdersControllersServiceProvider::class,
+		OrderSourceAttributionServiceProvider::class,
 		ObjectCacheServiceProvider::class,
 		BatchProcessingServiceProvider::class,
 		OrderMetaBoxServiceProvider::class,
