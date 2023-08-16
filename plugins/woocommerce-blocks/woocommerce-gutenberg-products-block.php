8e9652d3187e3095dded82b081ae3f885f0ee1f5
Niels Lange <info@nielslange.de>
2023-08-16 15:05:23 +0200

    Release: 10.9.0

--- plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
+++ plugins/woocommerce-blocks/woocommerce-gutenberg-products-block.php
@@ -3,14 +3,14 @@
  * Plugin Name: WooCommerce Blocks
  * Plugin URI: https://github.com/woocommerce/woocommerce-gutenberg-products-block
  * Description: WooCommerce blocks for the Gutenberg editor.
- * Version: 10.9.0-dev
+ * Version: 10.9.0
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain:  woo-gutenberg-products-block
- * Requires at least: 6.2
+ * Requires at least: 6.3
  * Requires PHP: 7.3
- * WC requires at least: 7.8
- * WC tested up to: 7.9
+ * WC requires at least: 7.9
+ * WC tested up to: 8.0
  *
  * @package WooCommerce\Blocks
  * @internal This file is only used when running as a feature plugin.
@@ -18,7 +18,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$minimum_wp_version = '6.2';
+$minimum_wp_version = '6.3';
 
 if ( ! defined( 'WC_BLOCKS_IS_FEATURE_PLUGIN' ) ) {
 	define( 'WC_BLOCKS_IS_FEATURE_PLUGIN', true );
