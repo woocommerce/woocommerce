2dc9c9bc61d45e2815178c070e14b57ac92746eb
And Finally <andfinally@users.noreply.github.com>
2023-08-16 08:49:01 +0100

    More concise use of selectors, so we only do one lookup
    Co-authored-by: Dan Q <dan@danq.me>

--- plugins/woocommerce/client/legacy/js/admin/wc-admin-menu.js
+++ plugins/woocommerce/client/legacy/js/admin/wc-admin-menu.js
@@ -1,12 +1,8 @@
 document.addEventListener( 'DOMContentLoaded', () => {
 
-	const adminMenu = document.getElementById( 'adminmenu' );
-	if ( ! adminMenu ) {
-		return;
-	}
-
-	const addonsSubMenuLinks = adminMenu.querySelectorAll(
-		'#toplevel_page_woocommerce' +
+	const addonsSubMenuLinks = document.querySelectorAll(
+		'#adminmenu' +
+		' #toplevel_page_woocommerce' +
 		' .wp-submenu' +
 		' li a[href="admin.php?page=wc-addons"]'
 	);
