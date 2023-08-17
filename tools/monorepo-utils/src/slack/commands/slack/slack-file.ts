be427a59f2244f762ecfce7267f77965133accc9
Sam Seay <samueljseay@gmail.com>
2023-08-17 12:20:14 +0800

    Clean up, add file command to program.

--- tools/monorepo-utils/src/slack/commands/slack/slack-file.ts
+++ tools/monorepo-utils/src/slack/commands/slack/slack-file.ts
@@ -3,6 +3,7 @@
  */
 import { Command } from '@commander-js/extra-typings';
 import { WebClient } from '@slack/web-api';
+import { basename } from 'path';
 
 /**
  * External dependencies
@@ -14,14 +15,14 @@ import { existsSync } from 'fs';
  */
 import { Logger } from '../../../core/logger';
 
-export const slackMessageCommand = new Command( 'message' )
+export const slackFileCommand = new Command( 'message' )
 	.description( 'Send a file upload message to a slack channel' )
 	.argument(
 		'<token>',
 		'Slack authentication token bearing required scopes.'
 	)
 	.argument( '<text>', 'Text based message to send to the slack channel.' )
-	.argument( '<file>', 'File path to upload to the slack channel.' )
+	.argument( '<filePath>', 'File path to upload to the slack channel.' )
 	.argument(
 		'<channels...>',
 		'Slack channels to send the message to. Pass as many as you like.'
@@ -30,7 +31,7 @@ export const slackMessageCommand = new Command( 'message' )
 		'--dont-fail',
 		'Do not fail the command if a message fails to send to any channel.'
 	)
-	.action( async ( token, text, file, channels, { dontFail } ) => {
+	.action( async ( token, text, filePath, channels, { dontFail } ) => {
 		Logger.startTask(
 			`Attempting to send message to Slack for channels: ${ channels.join(
 				','
@@ -39,9 +40,9 @@ export const slackMessageCommand = new Command( 'message' )
 
 		const shouldFail = ! dontFail;
 
-		if ( file && ! existsSync( file ) ) {
+		if ( filePath && ! existsSync( filePath ) ) {
 			Logger.error(
-				`Unable to open file with path: ${ file }`,
+				`Unable to open file with path: ${ filePath }`,
 				shouldFail
 			);
 		}
@@ -51,14 +52,14 @@ export const slackMessageCommand = new Command( 'message' )
 			try {
 				// @ts-expect-error - types are not up to date.
 				await client.files.uploadV2( {
-					file,
-					filename: basename( file ),
+					file: filePath,
+					filename: basename( filePath ),
 					channels: channel,
 					initial_comment: text.replace( /\\n/g, '\n' ),
 				} );
 
 				Logger.notice(
-					`Successfully uploaded ${ file } to channel: ${ channel }`
+					`Successfully uploaded ${ filePath } to channel: ${ channel }`
 				);
 			} catch ( e ) {
 				Logger.error( e, shouldFail );
