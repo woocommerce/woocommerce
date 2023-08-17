9a82d767a7694159d2821f45f7782d5e75d92ef6
Jonathan Sadowski <sadowski@automattic.com>
2023-08-16 20:02:31 -0500

    Attempt to add replies and upload to slack command

--- tools/monorepo-utils/src/slack/commands/slack/slack-message.ts
+++ tools/monorepo-utils/src/slack/commands/slack/slack-message.ts
@@ -2,16 +2,23 @@
  * External dependencies
  */
 import { Command } from '@commander-js/extra-typings';
+import { setOutput } from '@actions/core';
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
+import { basename } from 'path';
+import FormData from 'form-data';
 
 /**
  * Internal dependencies
  */
 import { Logger } from '../../../core/logger';
 import { requestAsync } from '../../../core/util';
+import { isGithubCI } from '../../../core/environment';
 
 type SlackResponse = {
 	ok: boolean;
 	error?: string;
+	ts: string;
 };
 
 export const slackMessageCommand = new Command( 'message' )
@@ -29,7 +36,20 @@ export const slackMessageCommand = new Command( 'message' )
 		'--dont-fail',
 		'Do not fail the command if a message fails to send to any channel.'
 	)
-	.action( async ( token, text, channels, { dontFail } ) => {
+	.option(
+		'--reply-ts <ts>',
+		'Message will be in reply to message specified by ts value.'
+	)
+	.option(
+		'--attach-file <file>',
+		'The specified file will be added as an attachment.'
+	)
+	.action( async ( token, text, channels, { dontFail, replyTs, attachFile } ) => {
+		if ( attachFile && ! existsSync( attachFile ) ) {
+			Logger.error( `Unable to open file ${ attachFile }` );
+			return;
+		}
+
 		Logger.startTask(
 			`Attempting to send message to Slack for channels: ${ channels.join(
 				','
@@ -39,13 +59,28 @@ export const slackMessageCommand = new Command( 'message' )
 		const shouldFail = ! dontFail;
 
 		for ( const channel of channels ) {
+			const form = new FormData();
+			form.append( 'channel', channel );
+
+			if ( attachFile ) {
+				const file = await readFile( attachFile );
+				form.append( 'initial_comment', text.replace( /\\n/g, '\n' ) );
+				form.append( 'file', file, basename( attachFile ) );
+			} else {
+				form.append( 'text', text.replace( /\\n/g, '\n' ) );
+			}
+
+			if ( replyTs ) {
+				form.append( 'thread_ts', replyTs );
+			}
+
 			// Define the request options
 			const options = {
 				hostname: 'slack.com',
-				path: '/api/chat.postMessage',
+				path: attachFile ? '/api/files.upload' : '/api/chat.postMessage',
 				method: 'POST',
 				headers: {
-					'Content-Type': 'application/json',
+					...form.getHeaders(),
 					Authorization: `Bearer ${ token }`,
 				},
 			};
@@ -53,15 +88,14 @@ export const slackMessageCommand = new Command( 'message' )
 			try {
 				const { statusCode, body } = await requestAsync(
 					options,
-					JSON.stringify( {
-						channel,
-						text: text.replace( /\\n/g, '\n' ),
-					} )
+					form.getBuffer()
 				);
 
 				Logger.endTask();
 
 				const response = JSON.parse( body ) as SlackResponse;
+
+				Logger.notice( body );
 
 				if ( ! response.ok || statusCode !== 200 ) {
 					Logger.error(
@@ -70,8 +104,11 @@ export const slackMessageCommand = new Command( 'message' )
 					);
 				} else {
 					Logger.notice(
-						`Slack message sent successfully to channel: ${ channel }`
+						`Slack message sent successfully to channel: ${ channel }. Message has ts: ${ response.ts }`
 					);
+					if ( isGithubCI() ) {
+						setOutput( `ts_${ channel }`, response.ts );
+					}
 				}
 			} catch ( e: unknown ) {
 				Logger.error( e, shouldFail );
