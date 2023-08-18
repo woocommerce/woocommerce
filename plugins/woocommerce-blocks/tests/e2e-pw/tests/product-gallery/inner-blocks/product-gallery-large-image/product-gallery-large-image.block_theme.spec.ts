601adc6e1f54351c224ed35a5f00fd14996a79fb
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-08-18 14:02:06 +0200

    Product Gallery Large Image: Add E2E test (https://github.com/woocommerce/woocommerce-blocks/pull/10482)

---
+++ plugins/woocommerce-blocks/tests/e2e-pw/tests/product-gallery/inner-blocks/product-gallery-large-image/product-gallery-large-image.block_theme.spec.ts
@@ -0,0 +1,61 @@
+/**
+ * External dependencies
+ */
+import { test, expect } from '@woocommerce/e2e-playwright-utils';
+
+/**
+ * Internal dependencies
+ */
+const blockData = {
+	name: 'woocommerce/product-gallery-large-image',
+	selectors: {
+		frontend: {},
+		editor: {},
+	},
+	slug: 'single-product',
+	productPage: '/product/v-neck-t-shirt/',
+};
+
+test.describe( `${ blockData.name }`, () => {
+	test.beforeEach( async ( { requestUtils, admin, editorUtils } ) => {
+		await requestUtils.deleteAllTemplates( 'wp_template' );
+		await requestUtils.deleteAllTemplates( 'wp_template_part' );
+		await admin.visitSiteEditor( {
+			postId: `woocommerce/woocommerce//${ blockData.slug }`,
+			postType: 'wp_template',
+		} );
+		await editorUtils.enterEditMode();
+	} );
+
+	test( 'Renders Product Gallery Large Image block on the editor and frontend side', async ( {
+		page,
+		editor,
+		editorUtils,
+		frontendUtils,
+	} ) => {
+		await editor.insertBlock( {
+			name: 'woocommerce/product-gallery',
+		} );
+
+		const block = await editorUtils.getBlockByName( blockData.name );
+
+		await expect( block ).toBeVisible();
+
+		await Promise.all( [
+			editor.saveSiteEditorEntities(),
+			page.waitForResponse( ( response ) =>
+				response.url().includes( 'wp-json/wp/v2/templates/' )
+			),
+		] );
+
+		await page.goto( blockData.productPage, {
+			waitUntil: 'networkidle',
+		} );
+
+		const blockFrontend = await frontendUtils.getBlockByName(
+			blockData.name
+		);
+
+		await expect( blockFrontend ).toBeVisible();
+	} );
+} );
