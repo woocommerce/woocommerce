7b6178e06345838d4cecf1be7ddc531e68aae37c
Thomas Roberts <5656702+opr@users.noreply.github.com>
2023-08-24 09:37:14 +0100

    Dispatch the `wc-blocks_render_blocks_frontend` event when rendering the empty cart block (https://github.com/woocommerce/woocommerce-blocks/pull/10619)

--- plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/empty-cart-block/frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/empty-cart-block/frontend.tsx
@@ -19,12 +19,15 @@ const FrontendBlock = ( {
 } ): JSX.Element | null => {
 	const { cartItems, cartIsLoading } = useStoreCart();
 	useEffect( () => {
+		if ( cartItems.length !== 0 || cartIsLoading ) {
+			return;
+		}
 		dispatchEvent( 'wc-blocks_render_blocks_frontend', {
 			element: document.body.querySelector(
 				'.wp-block-woocommerce-cart'
 			),
 		} );
-	}, [] );
+	}, [ cartIsLoading, cartItems ] );
 	if ( ! cartIsLoading && cartItems.length === 0 ) {
 		return <div className={ className }>{ children }</div>;
 	}
