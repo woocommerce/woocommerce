c718a056b8487f5d2117e56666fe8a86a1da4917
Alexandre Lara <allexandrelara@gmail.com>
2023-08-25 15:42:31 -0300

    Product Gallery block: Add support to Interactivity API (https://github.com/woocommerce/woocommerce-blocks/pull/10716)
    * Add support to Interactivity API
    
    * Fix php cs errors
    
    * Rename numberOfThumbnailImages to numberOfThumbnails
    
    * Use frontend.tsx with InteractivityAPI
    
    * Replace viewScript in block.json

--- plugins/woocommerce-blocks/src/BlockTypes/ProductGallery.php
+++ plugins/woocommerce-blocks/src/BlockTypes/ProductGallery.php
@@ -22,11 +22,58 @@ class ProductGallery extends AbstractBlock {
 	}
 
 	/**
+	 * Include and render the block.
+	 *
+	 * @param array    $attributes Block attributes. Default empty array.
+	 * @param string   $content    Block content. Default empty string.
+	 * @param WP_Block $block      Block instance.
+	 * @return string Rendered block type output.
+	 */
+	protected function render( $attributes, $content, $block ) {
+		$classname          = $attributes['className'] ?? '';
+		$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => trim( sprintf( 'woocommerce %1$s', $classname ) ) ) );
+		$html               = sprintf(
+			'<div data-wc-interactive %1$s>
+				%2$s
+			</div>',
+			$wrapper_attributes,
+			$content
+		);
+		$p                  = new \WP_HTML_Tag_Processor( $content );
+
+		if ( $p->next_tag() ) {
+			$p->set_attribute( 'data-wc-interactive', true );
+			$p->set_attribute(
+				'data-wc-context',
+				wp_json_encode( array( 'productGallery' => array( 'numberOfThumbnails' => 0 ) ) )
+			);
+			$html = $p->get_updated_html();
+		}
+
+		return $html;
+	}
+
+	/**
 	 * It isn't necessary register block assets because it is a server side block.
 	 */
 	protected function register_block_type_assets() {
 		parent::register_block_type_assets();
 
 		return null;
+	}
+
+	/**
+	 * Get the Interactivity API's view script handle for this block type.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 */
+	protected function get_block_type_script( $key = null ) {
+		$script = [
+			'handle'       => 'wc-' . $this->block_name . '-interactivity-frontend',
+			'path'         => $this->asset_api->get_block_asset_build_path( $this->block_name . '-interactivity-frontend' ),
+			'dependencies' => [ 'wc-interactivity' ],
+		];
+
+		return $key ? $script[ $key ] : $script;
 	}
 }
