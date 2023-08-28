31e9854316a1177e9510062e396bf5d97f2a920b
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-08-28 12:28:17 +0200

    Add Product Collection Full Grid pattern (https://github.com/woocommerce/woocommerce-blocks/pull/10665)
    * Add initial look of Full Grid Product Collection pattern
    
    * Refactor textAlign property of Product Button so it uses flex rather than text-align
    
    * Update Product Button text align after the fix
    
    * Remove debug log (https://github.com/woocommerce/woocommerce-blocks/pull/10719)
    
    * Add Product Collection Grid pattern (https://github.com/woocommerce/woocommerce-blocks/pull/10660)
    
    * Dispatch the `wc-blocks_render_blocks_frontend` event when rendering the empty cart block (https://github.com/woocommerce/woocommerce-blocks/pull/10619)
    
    * E2E: Refactor Mini Cart to be ready for fully parallel (https://github.com/woocommerce/woocommerce-blocks/pull/10704)
    
    * Revert dequeue add-to-cart-variation script which is needed to properly handle variable products in single product page (https://github.com/woocommerce/woocommerce-blocks/pull/10723)
    
    * Update alignment options to new setting
    
    ---------
    
    Co-authored-by: Thomas Roberts <5656702+opr@users.noreply.github.com>
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>

---
+++ plugins/woocommerce-blocks/patterns/product-collection-full-grid.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Product Collection Full Grid
+ * Slug: woocommerce-blocks/product-collection-full-grid
+ * Categories: WooCommerce
+ * Inserter: false
+ */
+?>
+
+<!-- wp:woocommerce/product-collection {"queryId":0,"query":{"perPage":3,"pages":0,"offset":0,"postType":"product","order":"asc","orderBy":"title","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{},"parents":[],"isProductCollectionBlock":true,"woocommerceOnSale":false,"woocommerceStockStatus":["instock","outofstock","onbackorder"],"woocommerceAttributes":[],"woocommerceHandPickedProducts":[]},"tagName":"div","displayLayout":{"type":"flex","columns":3}} -->
+<div class="wp-block-woocommerce-product-collection"><!-- wp:woocommerce/product-template -->
+<!-- wp:woocommerce/product-image {"imageSizing":"thumbnail","isDescendentOfQueryLoop":true} /-->
+
+<!-- wp:woocommerce/product-price {"isDescendentOfQueryLoop":true,"textAlign":"left","fontSize":"medium","style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1"}}} /-->
+
+<!-- wp:post-title {"textAlign":"left","level":3,"isLink":true,"style":{"spacing":{"margin":{"bottom":"0.75rem","top":"0"}},"typography":{"lineHeight":"1"}},"fontSize":"small","__woocommerceNamespace":"woocommerce/product-collection/product-title"} /-->
+
+<!-- wp:woocommerce/product-button {"textAlign":"left","width":50,"isDescendentOfQueryLoop":true,"style":{"typography":{"fontSize":"0.8rem"}}} /-->
+<!-- /wp:woocommerce/product-template -->
+
+<!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"center"}} -->
+<!-- wp:query-pagination-previous /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
+<p></p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:woocommerce/product-collection -->
