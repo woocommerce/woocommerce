b8720ef42e3b3d7ddc24289c0fab22e8ffaed6b7
barryhughes <3594411+barryhughes@users.noreply.github.com>
2023-08-28 14:33:08 -0700

    Increase the number of cases controlled by the `woocommerce_order_email_verification_required` filter hook.

--- plugins/woocommerce/includes/shortcodes/class-wc-shortcode-checkout.php
+++ plugins/woocommerce/includes/shortcodes/class-wc-shortcode-checkout.php
@@ -387,12 +387,6 @@ class WC_Shortcode_Checkout {
 			return false;
 		}
 
-		$email = filter_input( INPUT_POST, 'email' );
-		$nonce = filter_input( INPUT_POST, 'check_submission' );
-		if ( $email && ! wp_verify_nonce( $nonce, 'wc_verify_email' ) ) {
-			return true;
-		}
-
 		/**
 		 * Controls the grace period within which we do not require any sort of email verification step before rendering
 		 * the 'order received' or 'order pay' pages.
@@ -426,11 +420,12 @@ class WC_Shortcode_Checkout {
 			$session_email = is_array( $customer ) && isset( $customer['email'] ) ? $customer['email'] : '';
 		}
 
-		$session_email_match  = $session_email === $order->get_billing_email();
-		$supplied_email_match = isset( $_POST['email'] ) && sanitize_email( wp_unslash( $_POST['email'] ) ?? '' ) === $order->get_billing_email();
+		// Email verification is required if the user cannot be identified, or if they supplied an email address but the nonce check failed.
 		$can_view_orders      = current_user_can( 'read_private_shop_orders' );
+		$session_email_match  = $session_email === $order->get_billing_email();
+		$supplied_email_match = sanitize_email( wp_unslash( filter_input( INPUT_POST, 'email' ) ) ) === $order->get_billing_email()
+			&& wp_verify_nonce( filter_input( INPUT_POST, 'check_submission' ), 'wc_verify_email' );
 
-		// If we cannot match the order with the current user, the user should verify their email address.
 		$email_verification_required = ! $session_email_match && ! $supplied_email_match && ! $can_view_orders;
 
 		/**
