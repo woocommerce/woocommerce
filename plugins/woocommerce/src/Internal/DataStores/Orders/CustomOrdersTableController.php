5fe1bef495c33371560b366d560bafdbb4afdb5a
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2023-08-28 17:24:21 -0700

    Fix regression: HPOS not experimental now

--- plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/CustomOrdersTableController.php
@@ -391,7 +391,6 @@ class CustomOrdersTableController {
 		$feature_definitions['custom_order_tables'] = array(
 			'name'                => __( 'High-Performance order storage', 'woocommerce' ),
 			'option_key'          => self::CUSTOM_ORDERS_TABLE_USAGE_ENABLED_OPTION,
-			'is_experimental'     => true,
 			'enabled_by_default'  => false,
 			'setting'             => $this->get_hpos_setting_for_feature(),
 			'additional_settings' => array(
