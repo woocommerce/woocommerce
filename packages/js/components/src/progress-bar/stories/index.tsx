b3336e99d1f637941c5333b9921f20548f77635c
moon <moon.kyong@automattic.com>
2023-08-28 17:15:46 -0700

    Add ProgressBar component to @woocommerce/components

---
+++ packages/js/components/src/progress-bar/stories/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * External dependencies
+ */
+import { ProgressBar } from '@woocommerce/components';
+
+export const Basic = () => (
+	<div style={ { background: '#fff', height: '200px', padding: '20px' } }>
+		<ProgressBar percent={ 20 } bgcolor={ '#eeeeee' } color={ '#007cba' } />
+	</div>
+);
+
+export default {
+	title: 'WooCommerce Admin/components/ProgressBar',
+	component: ProgressBar,
+};
