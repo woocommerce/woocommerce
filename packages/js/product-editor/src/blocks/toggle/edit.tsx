48f532345e3cef81af3fed77e8b2f6bf5882da18
Fernando Marichal <ultimoround@gmail.com>
2023-09-01 17:51:56 -0300

    Add notice to "track inventory" toggle

--- packages/js/product-editor/src/blocks/toggle/edit.tsx
+++ packages/js/product-editor/src/blocks/toggle/edit.tsx
@@ -11,17 +11,22 @@ import { ToggleControl } from '@wordpress/components';
  * Internal dependencies
  */
 import { ToggleBlockAttributes } from './types';
+import { sanitizeHTML } from '../../utils/sanitize-html';
 
 export function Edit( {
 	attributes,
 }: BlockEditProps< ToggleBlockAttributes > ) {
 	const blockProps = useBlockProps();
-	const { label, property, disabled } = attributes;
+	const { label, property, disabled, disabledCopy } = attributes;
 	const [ value, setValue ] = useEntityProp< boolean >(
 		'postType',
 		'product',
 		property
 	);
+
+	console.log( 'attributes', attributes );
+	console.log( 'blockProps', blockProps );
+	console.log( 'disabledCopy', disabledCopy );
 
 	return (
 		<div { ...blockProps }>
@@ -31,6 +36,12 @@ export function Edit( {
 				disabled={ disabled }
 				onChange={ setValue }
 			/>
+			{ disabled && (
+				<p
+					className="wp-block-woocommerce-product-toggle__disable-copy"
+					dangerouslySetInnerHTML={ sanitizeHTML( disabledCopy ) }
+				/>
+			) }
 		</div>
 	);
 }
