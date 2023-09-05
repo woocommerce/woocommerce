8dba4d9af180e9eeda8def222db0674fa62fe835
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-09-05 12:20:57 +0200

    Product Hero Pattern > Adjust the content width (https://github.com/woocommerce/woocommerce-blocks/pull/10832)
    * Minor width adjustment for the Product Hero pattern.
    
    * close div on a new line

--- plugins/woocommerce-blocks/patterns/product-hero.php
+++ plugins/woocommerce-blocks/patterns/product-hero.php
@@ -21,14 +21,14 @@ $product_id = $products ? $products[0] : null;
 <div class="wp-block-woocommerce-single-product alignwide">
 	<!-- wp:columns -->
 	<div class="wp-block-columns">
-		<!-- wp:column {"width":"40%","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"verticalAlignment":"center"} -->
+		<!-- wp:column {"verticalAlignment":"center","width":"40%","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
 		<div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:40%">
 			<!-- wp:woocommerce/product-image {"showSaleBadge":false,"isDescendentOfSingleProductBlock":true,"height":"300px"} /-->
 		</div>
 		<!-- /wp:column -->
 
-		<!-- wp:column {"verticalAlignment":"center"} -->
-		<div class="wp-block-column is-vertically-aligned-center">
+		<!-- wp:column {"verticalAlignment":"center","width":"60%","layout":{"type":"constrained","justifyContent":"left","contentSize":"650px"}} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%">
 			<!-- wp:post-title {"textAlign":"","isLink":true,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"__woocommerceNamespace":"woocommerce/product-query/product-title"} /-->
 
 			<!-- wp:woocommerce/product-price {"isDescendentOfSingleProductBlock":true} /-->
