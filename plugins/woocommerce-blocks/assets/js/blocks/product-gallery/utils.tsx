791ac48b03f232abe2e5b2b45c70febec289d496
Alexandre Lara <allexandrelara@gmail.com>
2023-09-05 09:52:19 -0300

    Product Gallery > Pager block: Fix block alignment (https://github.com/woocommerce/woocommerce-blocks/pull/10782)
    * Fix block alignment with Large Image block
    
    * Prevent error when layout is not present in the attributes object
    
    * Add Large Image and Pager to its own group
    
    * Fix issue when moving around the thumbnails block
    
    * Fix e2e tests for Product Gallery block
    
    * Change ancestor block for the Pager block

--- plugins/woocommerce-blocks/assets/js/blocks/product-gallery/utils.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-gallery/utils.tsx
@@ -1,8 +1,7 @@
 /**
  * External dependencies
  */
-import { store as blockEditorStore } from '@wordpress/block-editor';
-import { BlockAttributes } from '@wordpress/blocks';
+import { BlockAttributes, BlockInstance } from '@wordpress/blocks';
 import { select, dispatch } from '@wordpress/data';
 
 /**
@@ -69,6 +68,59 @@ export const updateBlockAttributes = (
 	}
 };
 
+const controlBlocksLockAttribute = ( {
+	blocks,
+	lockBlocks,
+}: {
+	blocks: BlockInstance[];
+	lockBlocks: boolean;
+} ) => {
+	for ( const block of blocks ) {
+		if ( lockBlocks ) {
+			updateBlockAttributes(
+				getInnerBlocksLockAttributes( 'lock' ),
+				block
+			);
+		} else {
+			updateBlockAttributes(
+				getInnerBlocksLockAttributes( 'unlock' ),
+				block
+			);
+		}
+	}
+};
+
+/**
+ * Recursively searches through an array of `BlockInstance` objects and their nested `innerBlocks` arrays to find a block that matches a given condition.
+ *
+ * @param { { blocks: BlockInstance[], findCondition: Function } } parameters Parameters containing an array of `BlockInstance` objects to search through and a function that takes a `BlockInstance` object as its argument and returns a boolean indicating whether the block matches the desired condition.
+ * @return If a matching block is found, the function returns the `BlockInstance` object. If no matching block is found, the function returns `undefined`.
+ */
+const findBlock = ( {
+	blocks,
+	findCondition,
+}: {
+	blocks: BlockInstance[];
+	findCondition: ( block: BlockInstance ) => boolean;
+} ): BlockInstance | undefined => {
+	for ( const block of blocks ) {
+		if ( findCondition( block ) ) {
+			return block;
+		}
+		if ( block.innerBlocks ) {
+			const largeImageParentBlock = findBlock( {
+				blocks: block.innerBlocks,
+				findCondition,
+			} );
+			if ( largeImageParentBlock ) {
+				return largeImageParentBlock;
+			}
+		}
+	}
+
+	return undefined;
+};
+
 /**
  * Moves inner blocks to a position based on provided attributes.
  *
@@ -79,76 +131,71 @@ export const moveInnerBlocksToPosition = (
 	attributes: BlockAttributes,
 	clientId: string
 ): void => {
-	const parentBlock = select( 'core/block-editor' ).getBlock( clientId );
+	const { getBlock, getBlockRootClientId, getBlockIndex } =
+		select( 'core/block-editor' );
+	const { moveBlockToPosition } = dispatch( 'core/block-editor' );
+	const productGalleryBlock = getBlock( clientId );
 
-	if ( parentBlock?.name === 'woocommerce/product-gallery' ) {
-		const groupBlock = parentBlock.innerBlocks.find(
-			( innerBlock ) => innerBlock.name === 'core/group'
+	if ( productGalleryBlock ) {
+		const thumbnailsBlock = findBlock( {
+			blocks: [ productGalleryBlock ],
+			findCondition( block ) {
+				return block.name === 'woocommerce/product-gallery-thumbnails';
+			},
+		} );
+		const largeImageParentBlock = findBlock( {
+			blocks: [ productGalleryBlock ],
+			findCondition( block ) {
+				return Boolean(
+					block.innerBlocks?.find(
+						( innerBlock ) =>
+							innerBlock.name ===
+							'woocommerce/product-gallery-large-image'
+					)
+				);
+			},
+		} );
+		const largeImageParentBlockIndex = getBlockIndex(
+			largeImageParentBlock?.clientId || ''
+		);
+		const thumbnailsBlockIndex = getBlockIndex(
+			thumbnailsBlock?.clientId || ''
 		);
 
-		if ( groupBlock ) {
-			const largeImageBlock = groupBlock.innerBlocks.find(
-				( innerBlock ) =>
-					innerBlock.name ===
-					'woocommerce/product-gallery-large-image'
-			);
+		if (
+			largeImageParentBlock &&
+			thumbnailsBlock &&
+			largeImageParentBlockIndex !== -1 &&
+			thumbnailsBlockIndex !== -1
+		) {
+			controlBlocksLockAttribute( {
+				blocks: [ thumbnailsBlock, largeImageParentBlock ],
+				lockBlocks: false,
+			} );
 
-			const thumbnailsBlock = groupBlock.innerBlocks.find(
-				( innerBlock ) =>
-					innerBlock.name === 'woocommerce/product-gallery-thumbnails'
-			);
+			const { thumbnailsPosition } = attributes;
 
-			const thumbnailsIndex = groupBlock.innerBlocks.findIndex(
-				( innerBlock ) =>
-					innerBlock.name === 'woocommerce/product-gallery-thumbnails'
-			);
-
-			const largeImageIndex = groupBlock.innerBlocks.findIndex(
-				( innerBlock ) =>
-					innerBlock.name ===
-					'woocommerce/product-gallery-large-image'
-			);
-
-			if ( thumbnailsIndex !== -1 && largeImageIndex !== -1 ) {
-				updateBlockAttributes(
-					getInnerBlocksLockAttributes( 'unlock' ),
-					thumbnailsBlock
-				);
-				updateBlockAttributes(
-					getInnerBlocksLockAttributes( 'unlock' ),
-					largeImageBlock
-				);
-
-				const { thumbnailsPosition } = attributes;
-				const clientIdToMove =
-					groupBlock.innerBlocks[ thumbnailsIndex ].clientId;
-
-				if (
-					thumbnailsPosition === 'bottom' ||
-					thumbnailsPosition === 'right'
-				) {
-					// @ts-expect-error - Ignoring because `moveBlocksDown` is not yet in the type definitions.
-					dispatch( blockEditorStore ).moveBlocksDown(
-						[ clientIdToMove ],
-						groupBlock.clientId
-					);
-				} else {
-					// @ts-expect-error - Ignoring because `moveBlocksUp` is not yet in the type definitions.
-					dispatch( blockEditorStore ).moveBlocksUp(
-						[ clientIdToMove ],
-						groupBlock.clientId
-					);
-				}
-
-				updateBlockAttributes(
-					getInnerBlocksLockAttributes( 'lock' ),
-					thumbnailsBlock
-				);
-				updateBlockAttributes(
-					getInnerBlocksLockAttributes( 'lock' ),
-					largeImageBlock
+			if (
+				( ( thumbnailsPosition === 'bottom' ||
+					thumbnailsPosition === 'right' ) &&
+					thumbnailsBlockIndex < largeImageParentBlockIndex ) ||
+				( thumbnailsPosition === 'left' &&
+					thumbnailsBlockIndex > largeImageParentBlockIndex )
+			) {
+				moveBlockToPosition(
+					thumbnailsBlock.clientId,
+					getBlockRootClientId( thumbnailsBlock.clientId ) ||
+						undefined,
+					getBlockRootClientId( largeImageParentBlock.clientId ) ||
+						undefined,
+					largeImageParentBlockIndex
 				);
 			}
+
+			controlBlocksLockAttribute( {
+				blocks: [ thumbnailsBlock, largeImageParentBlock ],
+				lockBlocks: true,
+			} );
 		}
 	}
 };
