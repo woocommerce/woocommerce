74eb47c22b4d0fc217dec989b59d3c3918636386
Fernando Marichal <ultimoround@gmail.com>
2023-09-06 12:45:50 -0300

    Refactor use-tag-search

--- packages/js/product-editor/src/components/tags-field/use-tag-search.ts
+++ packages/js/product-editor/src/components/tags-field/use-tag-search.ts
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { useCallback, useEffect, useRef, useState } from '@wordpress/element';
+import { useEffect, useState } from '@wordpress/element';
 import { resolveSelect } from '@wordpress/data';
 import {
 	EXPERIMENTAL_PRODUCT_TAGS_STORE_NAME,
@@ -11,88 +11,29 @@ import {
 export type ProductTagNode = Pick< ProductTag, 'id' | 'name' >;
 
 /**
- * Filters the provided list of tags based on a given search term by checking
- * if the term is included in the tag's slug.
- */
-function getFilteredTags( items: ProductTag[] = [], search: string ) {
-	return items.filter( ( tag ) => tag.slug?.includes( search ) );
-}
-
-async function fetchProductTags( search?: string ): Promise< ProductTag[] > {
-	const query = search !== undefined ? { search } : '';
-	return resolveSelect( EXPERIMENTAL_PRODUCT_TAGS_STORE_NAME ).getProductTags(
-		query
-	);
-}
-
-/**
  * A hook used to handle all the search logic for the tag search component.
  */
 export const useTagSearch = () => {
-	const lastSearchValue = useRef( '' );
 	const [ fetchedTags, setFetchedTags ] = useState< ProductTag[] >( [] );
 	const [ isSearching, setIsSearching ] = useState( true );
-	const [ tagsAndNewItem, setTagsAndNewItem ] = useState< ProductTag[] >(
-		[]
-	);
 
-	useEffect( () => {
-		const fetchAndSetTags = async () => {
-			try {
-				const tags = await fetchProductTags();
-				setFetchedTags( tags );
-				setIsSearching( false );
-			} catch ( error ) {
-				setIsSearching( false );
-			}
-		};
+	const fetchProductTags = ( search?: string ) => {
+		setIsSearching( true );
+		const query = search !== undefined ? { search } : '';
+		resolveSelect( EXPERIMENTAL_PRODUCT_TAGS_STORE_NAME ).getProductTags(
+			query
+		).then( ( tags ) => {
+		   setFetchedTags( tags as ProductTag[] );
+		}).finally( () => {
+		   setIsSearching( false );
+		});
+	}
 
-		fetchAndSetTags();
-	}, [ tagsAndNewItem ] );
-
-	useEffect( () => {
-		if (
-			fetchedTags &&
-			fetchedTags.length > 0 &&
-			( tagsAndNewItem.length === 0 ||
-				lastSearchValue.current.length === 0 )
-		) {
-			setTagsAndNewItem( fetchedTags );
-			setIsSearching( false );
-		}
-	}, [ fetchedTags ] );
-
-	const searchTags = useCallback(
-		async ( search?: string ): Promise< ProductTag[] > => {
-			lastSearchValue.current = search || '';
-			if ( fetchedTags.length > 0 ) {
-				const tags = getFilteredTags(
-					// initialTags,
-					fetchedTags,
-					lastSearchValue.current.toLowerCase()
-				);
-				setTagsAndNewItem( tags );
-				setIsSearching( false );
-				return tags;
-			}
-			setIsSearching( true );
-			try {
-				const newTags = await fetchProductTags( search );
-
-				setIsSearching( false );
-				setTagsAndNewItem( newTags );
-				return newTags;
-			} catch ( e ) {
-				setIsSearching( false );
-				return [];
-			}
-		},
-		[ fetchedTags ]
-	);
+	useEffect( fetchProductTags, [] );
 
 	return {
-		searchTags,
-		tagsSelectList: tagsAndNewItem,
+		searchTags: fetchProductTags,
+		tagsSelectList: fetchedTags,
 		isSearching,
 	};
 };
