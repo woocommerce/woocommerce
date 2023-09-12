8b92dbd6cd5f8085ca97c747180865e82228a315
Veljko <veljano@yahoo.com>
2023-09-12 16:57:47 +0200

    Add new E2E test to cover Shopper Product page

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/single-product.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/single-product.spec.js
@@ -44,9 +44,23 @@ const variations = [
 ];
 const groupedProductName = 'Grouped single product';
 
-let simpleProductId, simpleProduct2Id, variableProductId, groupedProductId;
+let simpleProductId,
+	simpleProduct1Id,
+	simpleProduct2Id,
+	variableProductId,
+	groupedProductId,
+	productCategory1Id,
+	productCategory2Id;
 
 test.describe( 'Single Product Page', () => {
+	const simpleProduct1 = simpleProductName + ' Related';
+	const simpleProduct2 = simpleProductName + ' Upsell';
+	const productCategoryName1 = 'Hoodies';
+	const productCategoryName2 = 'Jumpers';
+	const productDescription = 'Lorem ipsum dolor sit amet.';
+	const reviewerEmailAddress = 'john.doe.test123@example.com';
+	const reviewerFullName = 'John Doe';
+
 	test.beforeAll( async ( { baseURL } ) => {
 		const api = new wcApi( {
 			url: baseURL,
@@ -54,12 +68,68 @@ test.describe( 'Single Product Page', () => {
 			consumerSecret: process.env.CONSUMER_SECRET,
 			version: 'wc/v3',
 		} );
-		// add product
+
+		// add product categories
+		await api
+			.post( 'products/categories', {
+				name: productCategoryName1,
+			} )
+			.then( ( response ) => {
+				productCategory1Id = response.data.id;
+			} );
+		await api
+			.post( 'products/categories', {
+				name: productCategoryName2,
+			} )
+			.then( ( response ) => {
+				productCategory2Id = response.data.id;
+			} );
+
+		// add products
+		await api
+			.post( 'products', {
+				name: simpleProduct1,
+				type: 'simple',
+				categories: [
+					{
+						id: productCategory1Id,
+						name: productCategoryName1,
+					},
+				],
+				regular_price: productPrice,
+			} )
+			.then( ( response ) => {
+				simpleProduct1Id = response.data.id;
+			} );
+		await api
+			.post( 'products', {
+				name: simpleProduct2,
+				type: 'simple',
+				categories: [
+					{
+						id: productCategory2Id,
+						name: productCategoryName2,
+					},
+				],
+				regular_price: productPrice,
+			} )
+			.then( ( response ) => {
+				simpleProduct2Id = response.data.id;
+			} );
 		await api
 			.post( 'products', {
 				name: simpleProductName,
+				description: productDescription,
 				type: 'simple',
+				categories: [
+					{
+						id: productCategory1Id,
+						name: productCategoryName1,
+					},
+				],
 				regular_price: productPrice,
+				related_ids: simpleProduct1Id,
+				upsell_ids: simpleProduct2Id,
 			} )
 			.then( ( response ) => {
 				simpleProductId = response.data.id;
@@ -81,6 +151,81 @@ test.describe( 'Single Product Page', () => {
 		await api.delete( `products/${ simpleProductId }`, {
 			force: true,
 		} );
+		await api.delete( `products/${ simpleProduct1Id }`, {
+			force: true,
+		} );
+		await api.delete( `products/${ simpleProduct2Id }`, {
+			force: true,
+		} );
+		await api.post( 'products/categories/batch', {
+			delete: [ productCategory1Id, productCategory2Id ],
+		} );
+	} );
+
+	test( 'should be able to see upsell and related products', async ( {
+		page,
+	} ) => {
+		const slug = simpleProductName.replace( / /gi, '-' ).toLowerCase();
+		await page.goto( `product/${ slug }` );
+
+		await expect( page.locator( '.upsells > h2' ) ).toContainText(
+			'You may also like…'
+		);
+		await expect( page.locator( '.related > h2' ) ).toContainText(
+			'Related products'
+		);
+		await expect(
+			page.locator(
+				'.upsells > .products > li > a > .woocommerce-loop-product__title'
+			)
+		).toContainText( simpleProduct2 );
+		await expect(
+			page.locator(
+				'.related > .products > li > a > .woocommerce-loop-product__title'
+			)
+		).toContainText( simpleProduct1 );
+	} );
+
+	test( 'should be able to post a review and see it after', async ( {
+		page,
+	} ) => {
+		await page.goto( 'my-account' );
+		await page.locator( '#username' ).fill( 'admin' );
+		await page.locator( '#password' ).fill( 'password' );
+		await page.locator( 'text=Log in' ).click();
+
+		const slug = simpleProductName.replace( / /gi, '-' ).toLowerCase();
+		await page.goto( `product/${ slug }` );
+
+		await expect( page.locator( '.reviews_tab' ) ).toContainText(
+			'Reviews (0)'
+		);
+		await page.locator( '.reviews_tab' ).click();
+		await page.locator( '.star-4' ).click();
+		await page.locator( '#comment' ).fill( 'This product is great!' );
+		await page.locator( 'text=Submit' ).click();
+		await expect(
+			page.locator( '.woocommerce-Reviews-title' )
+		).toContainText( `1 review for ${ simpleProductName }` );
+		await expect( page.locator( '.reviews_tab' ) ).toContainText(
+			'Reviews (1)'
+		);
+	} );
+
+	test( 'should be able to see product description and image', async ( {
+		page,
+	} ) => {
+		const slug = simpleProductName.replace( / /gi, '-' ).toLowerCase();
+		await page.goto( `product/${ slug }` );
+
+		await expect( page.locator( '#tab-description > p' ) ).toContainText(
+			productDescription
+		);
+		await expect(
+			page.locator(
+				'.woocommerce-product-gallery__image--placeholder > .wp-post-image'
+			)
+		).toBeVisible();
 	} );
 
 	test( 'should be able to add simple products to the cart', async ( {
