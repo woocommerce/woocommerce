01dd039b21f3e83b76396d631431392629cbab87
Nima Karimi <73110514+nima-karimi@users.noreply.github.com>
2023-09-12 17:59:26 +0100

    Suggest product categories with AI (#39437)
    * Accept arguments for the TinyContent getContent method.
    
    Used to fetch the plain text version of the description.
    
    * Add a class to the loading message content
    
    * Set the max description length as global constant
    
    * Create a MagicButton component and use it
    
    * Get plain text description instead of HTML
    
    * Return full category hierarchy (Parent > Child)
    
    * Add method to return all available categories on the product edit page
    
    * Helper function to generate product data instructions for the prompt
    
    * Helper function to select category checkboxes on DOM
    
    * Create component to display a list of suggestion items as pills
    
    * Add product category suggestions to product edit page
    
    * Use the AI package to get text completion
    
    * Add tracks
    
    * Add changelog
    
    * Fix merge conflict
    
    * Remove NoMatch state for category suggestions
    
    * Get available categories using WC REST API
    
    * Suggest new categories
    
    * Run separate prompts for existing and new category generation
    
    * Fix overflow in suggestion pills
    
    * Don't include existing selected categories in prompt
    
    * Add util to encode html entities
    
    * Exclude "Uncategorized" category from product data
    
    * Allow excluding properties from the product data instructions
    
    * Create category from suggestion if it doesn't exist
    
    * Show suggestions as links instead of pills
    
    * Ask for feedback after suggestion selected
    
    * Decode html entities in available categories results
    
    * Don't encode html entities when comparing available categories
    
    * Change feedback box style
    
    * Suggest only one category
    
    * Remove log
    
    * Show feedback box after generating suggestions
    
    Instead of showing it after a suggestion is selected
    
    * Fix typo

--- plugins/woo-ai/src/components/index.ts
+++ plugins/woo-ai/src/components/index.ts
@@ -1,3 +1,5 @@
 export * from './random-loading-message';
 export * from './description-completion-buttons';
+export * from './magic-button';
+export * from './suggestion-pills';
 export * from './info-modal';
