47a553b25e46a73d168eb985c6c15db406ba529e
moon <moon.kyong@automattic.com>
2023-09-12 12:41:12 -0700

    Move EntityProvider to access blocks from the sidebars

--- plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/sidebar-navigation-screen-footer.tsx
+++ plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/sidebar-navigation-screen-footer.tsx
@@ -2,15 +2,49 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { createInterpolateElement } from '@wordpress/element';
+import { createInterpolateElement, useCallback } from '@wordpress/element';
 import { Link } from '@woocommerce/components';
+import { Spinner } from '@wordpress/components';
+// @ts-expect-error Missing type in core-data.
+import { __experimentalBlockPatternsList as BlockPatternList } from '@wordpress/block-editor';
+
 /**
  * Internal dependencies
  */
 import { SidebarNavigationScreen } from './sidebar-navigation-screen';
 import { ADMIN_URL } from '~/utils/admin-settings';
+import { useEditorBlocks } from '../hooks/use-editor-blocks';
+import { usePatternsByCategory } from '../hooks/use-pattern';
 
 export const SidebarNavigationScreenFooter = () => {
+	const { isLoading, patterns } = usePatternsByCategory( 'footer' );
+	const [ blocks, onChange ] = useEditorBlocks();
+
+	const onClickHeaderPattern = useCallback(
+		( _pattern, selectedBlocks ) => {
+			const newHeaderBlock = {
+				...selectedBlocks[ 0 ],
+				attributes: {
+					...selectedBlocks[ 0 ].attributes,
+					slug: 'footer',
+				},
+			};
+
+			onChange(
+				[
+					...blocks.map( ( block ) => {
+						if ( block.attributes?.slug === 'footer' ) {
+							return newHeaderBlock;
+						}
+						return block;
+					} ),
+				],
+				{ selection: {} }
+			);
+		},
+		[ blocks, onChange ]
+	);
+
 	return (
 		<SidebarNavigationScreen
 			title={ __( 'Change your footer', 'woocommerce' ) }
@@ -30,7 +64,26 @@ export const SidebarNavigationScreenFooter = () => {
 			) }
 			content={
 				<>
-					<div className="edit-site-sidebar-navigation-screen-patterns__group-footer"></div>
+					<div className="edit-site-sidebar-navigation-screen-patterns__group-footer">
+						{ isLoading && (
+							<span className="components-placeholder__preview">
+								<Spinner />
+							</span>
+						) }
+
+						{ ! isLoading && (
+							<BlockPatternList
+								shownPatterns={ patterns }
+								blockPatterns={ patterns }
+								onClickPattern={ onClickHeaderPattern }
+								label={ 'Footers' }
+								orientation="vertical"
+								category={ 'footers' }
+								isDraggable={ false }
+								showTitlesAsTooltip={ true }
+							/>
+						) }
+					</div>
 				</>
 			}
 		/>
