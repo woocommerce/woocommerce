68ab140c6fdaa2848cbcee4d0f5c7f51bb9a359c
Leif Singer <git@singer.sh>
2023-09-13 12:07:21 +0200

    grammar nitpick

--- plugins/woocommerce/includes/wc-account-functions.php
+++ plugins/woocommerce/includes/wc-account-functions.php
@@ -311,7 +311,7 @@ function wc_get_account_orders_actions( $order ) {
  * @since  3.2.0
  * @param  string $address_type Type of address; 'billing' or 'shipping'.
  * @param  int    $customer_id  Customer ID.
- *                              Default to 0.
+ *                              Defaults to 0.
  * @return string
  */
 function wc_get_account_formatted_address( $address_type = 'billing', $customer_id = 0 ) {
