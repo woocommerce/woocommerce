225cd4875ab700869ee7d9048795abaa9a8ec2c3
Fernando Marichal <ultimoround@gmail.com>
2023-09-14 14:24:17 -0300

    Check with Explat experiment toggling

--- plugins/woocommerce-admin/client/task-lists/fills/products/use-create-product-by-type.ts
+++ plugins/woocommerce-admin/client/task-lists/fills/products/use-create-product-by-type.ts
@@ -15,11 +15,28 @@ import { ProductTypeKey } from './constants';
 import { createNoticesFromResponse } from '../../../lib/notices';
 import { getAdminSetting } from '~/utils/admin-settings';
 
+const PHYSICAL = 'physical';
+const VARIABLE = 'variable';
+
 export const useCreateProductByType = () => {
 	const { createProductFromTemplate } = useDispatch( ITEMS_STORE_NAME );
 	const [ isRequesting, setIsRequesting ] = useState< boolean >( false );
+
 	const isNewExperienceEnabled =
 		window.wcAdminFeatures[ 'new-product-management-experience' ];
+
+	const getExperimentName = ( type: string ) => {
+		const baseName = 'woocommerce_product_creation_experience';
+		const version =
+			type === PHYSICAL ? '202308_v3' : 'add_variations_202310_v1';
+		return `${ baseName }_${ version }`;
+	};
+
+	const redirectToProductBlockEditor = () => {
+		const _feature_nonce = getAdminSetting( '_feature_nonce' );
+		const url = `post-new.php?post_type=product&product_block_editor=1&_feature_nonce=${ _feature_nonce }`;
+		window.location.href = getAdminLink( url );
+	};
 
 	const createProductByType = async ( type: ProductTypeKey ) => {
 		if ( type === 'subscription' ) {
@@ -31,21 +48,15 @@ export const useCreateProductByType = () => {
 
 		setIsRequesting( true );
 
-		if ( type === 'physical' ) {
+		if ( [ PHYSICAL, VARIABLE ].includes( type ) ) {
 			if ( isNewExperienceEnabled ) {
 				navigateTo( { url: getNewPath( {}, '/add-product', {} ) } );
 				return;
 			}
-
-			const assignment = await loadExperimentAssignment(
-				'woocommerce_product_creation_experience_202308_v3'
-			);
-
+			const experimentName = getExperimentName( type );
+			const assignment = await loadExperimentAssignment( experimentName );
 			if ( assignment.variationName === 'treatment' ) {
-				const _feature_nonce = getAdminSetting( '_feature_nonce' );
-				window.location.href = getAdminLink(
-					`post-new.php?post_type=product&product_block_editor=1&_feature_nonce=${ _feature_nonce }`
-				);
+				redirectToProductBlockEditor();
 				return;
 			}
 		}
