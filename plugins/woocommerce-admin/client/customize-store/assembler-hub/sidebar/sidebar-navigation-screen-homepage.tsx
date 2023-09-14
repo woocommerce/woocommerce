70d8c62634379697599fb5bc63c2077bb9872883
moon <moon.kyong@automattic.com>
2023-09-13 17:03:58 -0700

    Load Hero, Featured products, About your store, Testimonials, and Social media patterns

--- plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/sidebar-navigation-screen-homepage.tsx
+++ plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/sidebar-navigation-screen-homepage.tsx
@@ -3,38 +3,88 @@
  */
 import { __ } from '@wordpress/i18n';
 import { createInterpolateElement, useCallback } from '@wordpress/element';
-import { Link } from '@woocommerce/components';
+import { Link, CollapsibleContent } from '@woocommerce/components';
 import { Spinner } from '@wordpress/components';
+
 // @ts-expect-error Missing type in core-data.
 import { __experimentalBlockPatternsList as BlockPatternList } from '@wordpress/block-editor';
-
 /**
  * Internal dependencies
  */
 import { SidebarNavigationScreen } from './sidebar-navigation-screen';
 import { ADMIN_URL } from '~/utils/admin-settings';
 import { useEditorBlocks } from '../hooks/use-editor-blocks';
-import { usePatternsByCategory } from '../hooks/use-pattern';
+import { usePatternsByCategory, Pattern } from '../hooks/use-pattern';
+
+function filterPatternsByNames( patterns: Pattern[], namesToFilter: string[] ) {
+	return patterns.filter( ( pattern: Pattern ) =>
+		namesToFilter.includes( pattern.name )
+	);
+}
 
 export const SidebarNavigationScreenHomepage = () => {
-	const { isLoading, patterns } = usePatternsByCategory( 'wireframe' );
-	const [ blocks, onChange ] = useEditorBlocks();
+	const { isLoading, patterns } = usePatternsByCategory( 'woo-commerce' );
+	const lists = [
+		{
+			label: __( 'Hero', 'woocommerce' ),
+			patterns: filterPatternsByNames( patterns, [
+				'woocommerce-blocks/hero-product-3-split',
+				'woocommerce-blocks/hero-product-chessboard',
+				'woocommerce-blocks/hero-product-split',
+				'woocommerce-blocks/just-arrived-full-hero',
+				'woocommerce-blocks/product-hero',
+				'woocommerce-blocks/product-hero-2-col-2-row',
+			] ),
+		},
+		{
+			label: __( 'Featured products', 'woocommerce' ),
+			patterns: filterPatternsByNames( patterns, [
+				'woocommerce-blocks/featured-category-cover-image',
+				'woocommerce-blocks/featured-category-focus',
+				'woocommerce-blocks/featured-category-triple',
+				'woocommerce-blocks/featured-products-5-item-grid',
+				'woocommerce-blocks/product-collections-featured-collection',
+				'woocommerce-blocks/product-collections-featured-collections',
+				'woocommerce-blocks/product-collections-newest-arrivals',
+				'woocommerce-blocks/featured-products-2-cols',
+			] ),
+		},
+		{
+			label: __( 'About your store', 'woocommerce' ),
+			patterns: filterPatternsByNames( patterns, [
+				'woocommerce-blocks/alt-image-and-text',
+			] ),
+		},
+		{
+			label: __( 'Testimonials', 'woocommerce' ),
+			patterns: filterPatternsByNames( patterns, [
+				'woocommerce-blocks/testimonials-3-columns',
+				'woocommerce-blocks/testimonials-single',
+			] ),
+		},
+		{
+			label: __( 'Social media', 'woocommerce' ),
+			patterns: filterPatternsByNames( patterns, [
+				'woocommerce-blocks/social-follow-us-in-social-media',
+			] ),
+		},
+	];
 
-	console.log( 'blocks', blocks );
-	const onClickHeaderPattern = useCallback(
+	const [ blocks, onChange ] = useEditorBlocks();
+	const onClickPattern = useCallback(
 		( _pattern, selectedBlocks ) => {
 			const newHeaderBlock = {
 				...selectedBlocks[ 0 ],
 				attributes: {
 					...selectedBlocks[ 0 ].attributes,
-					slug: 'footer',
+					tagName: 'main',
 				},
 			};
 
 			onChange(
 				[
 					...blocks.map( ( block ) => {
-						if ( block.attributes?.slug === 'homepage' ) {
+						if ( block.attributes?.tagName === 'main' ) {
 							return newHeaderBlock;
 						}
 						return block;
@@ -45,6 +95,7 @@ export const SidebarNavigationScreenHomepage = () => {
 		},
 		[ blocks, onChange ]
 	);
+
 	return (
 		<SidebarNavigationScreen
 			title={ __( 'Change your homepage', 'woocommerce' ) }
@@ -71,18 +122,30 @@ export const SidebarNavigationScreenHomepage = () => {
 							</span>
 						) }
 
-						{ ! isLoading && (
-							<BlockPatternList
-								shownPatterns={ patterns }
-								blockPatterns={ patterns }
-								onClickPattern={ onClickHeaderPattern }
-								label={ 'Hompeage' }
-								orientation="vertical"
-								category={ 'homepage' }
-								isDraggable={ false }
-								showTitlesAsTooltip={ true }
-							/>
-						) }
+						{ ! isLoading &&
+							lists.map( ( list ) => {
+								if ( list.patterns.length === 0 ) {
+									return null;
+								}
+
+								return (
+									<CollapsibleContent
+										toggleText={ list.label }
+										key={ list.label }
+									>
+										<BlockPatternList
+											shownPatterns={ list.patterns }
+											blockPatterns={ list.patterns }
+											onClickPattern={ onClickPattern }
+											label={ 'Hompeage' }
+											orientation="vertical"
+											category={ 'homepage' }
+											isDraggable={ false }
+											showTitlesAsTooltip={ true }
+										/>
+									</CollapsibleContent>
+								);
+							} ) }
 					</div>
 				</>
 			}
