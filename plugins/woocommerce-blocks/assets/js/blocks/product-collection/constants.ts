d80c5101b3c51a027a1d8da826af98ec782a3eae
Manish Menaria <the.manish.menaria@gmail.com>
2023-09-15 15:38:48 +0530

    Transfer layout options from Toolbar to Inspector controls (https://github.com/woocommerce/woocommerce-blocks/pull/10922)
    In this update, the layout options for the Product Collection block are transferred from the Toolbar to the Inspector controls. Below is the breakdown of the changes:
    
    1. **Constants Update**
        - `LayoutOptions` enumeration has been imported into `constants.ts`, facilitating a more structured approach to managing layout types (grid and stack).
        - The default display layout type has been updated from 'flex' to reference `LayoutOptions.GRID`.
        - The `getDefaultQuery` function now uses the `getDefaultValueOfInheritQueryFromTemplate` utility to set the default `inherit` value. (This is mainly done to fix a bug)
    
    2. **Display Layout Control Removal**
        - The `display-layout-control.tsx` file has been removed, discontinuing the previous method of layout management.
    
    3. **New Layout Options Control**
        - A new component `LayoutOptionsControl` has been introduced in the `layout-options-control.tsx` file, utilizing the experimental `ToggleGroupControl` and `ToggleGroupControlOption` components from the WordPress package to provide a more intuitive layout selection experience.
        - The `types.ts` file has been updated to define the `LayoutOptions` enum, effectively mapping 'flex' to 'GRID' and 'list' to 'STACK'.
    
    4. **Inspector Controls Update**
        - In `inspector-controls/index.tsx`, the obsolete `DisplayLayoutControl` has been replaced with the new `LayoutOptionsControl`, integrating it into the `ProductCollectionInspectorControls` component.
        - The `BlockControls` wrapper has been removed, and layout options have been relocated to the Inspector controls, presented as a toggle group within the ToolsPanel.
    
    5. **Inherit Query Control Modification**
        - The `inherit-query-control.tsx` file sees a change in the reset value for the `inherit` query attribute to employ a default value which fix one bug.
    
    These changes aim to streamline the user experience by relocating the layout options from the Toolbar to the Inspector controls, offering a centralized location for block settings. Leveraging an enum for layout options fosters code readability and maintainability.
    
    Do note that the update uses experimental components, hence it would be prudent to keep an eye on potential alterations or deprecations in upcoming WordPress releases.

--- plugins/woocommerce-blocks/assets/js/blocks/product-collection/constants.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-collection/constants.ts
@@ -13,7 +13,9 @@ import {
 	TProductCollectionOrderBy,
 	ProductCollectionQuery,
 	ProductCollectionDisplayLayout,
+	LayoutOptions,
 } from './types';
+import { getDefaultValueOfInheritQueryFromTemplate } from './utils';
 
 export const STOCK_STATUS_OPTIONS = getSetting< Record< string, string > >(
 	'stockStatusOptions',
@@ -56,7 +58,7 @@ export const DEFAULT_ATTRIBUTES: Partial< ProductCollectionAttributes > = {
 	query: DEFAULT_QUERY,
 	tagName: 'div',
 	displayLayout: {
-		type: 'flex',
+		type: LayoutOptions.GRID,
 		columns: 3,
 	},
 };
@@ -67,7 +69,7 @@ export const getDefaultQuery = (
 	...currentQuery,
 	orderBy: DEFAULT_QUERY.orderBy as TProductCollectionOrderBy,
 	order: DEFAULT_QUERY.order as TProductCollectionOrder,
-	inherit: DEFAULT_QUERY.inherit,
+	inherit: getDefaultValueOfInheritQueryFromTemplate(),
 } );
 
 export const getDefaultDisplayLayout = () =>
