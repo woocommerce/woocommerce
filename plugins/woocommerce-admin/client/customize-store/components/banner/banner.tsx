1bf1f5b7aa2e4832bdb4db7ea1c4ea0122bdb9ad
Chris Runnells <chris.runnells@automattic.com>
2023-09-15 15:34:11 -1000

    Added banner component and assets

---
+++ plugins/woocommerce-admin/client/customize-store/components/banner/banner.tsx
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+
+import './banner.scss';
+
+export const Banner = () => {
+	return (
+		<div className={ `woocommerce-customize-store-banner-content` }>
+			<h1>{ __( 'Use the power of AI to design your store', 'woocommerce' ) }</h1>
+			<p>{ __( 'Design the look of your store, create pages, and generate copy using our built-in AI tools.', 'woocommerce' ) }</p>
+			<div><a className="button" href="#">{ __('Design with AI') }</a></div>
+		</div>
+	);
+};
