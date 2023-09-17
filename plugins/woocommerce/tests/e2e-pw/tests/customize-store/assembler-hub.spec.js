772d60686d5b41d07227d6c12378d2f6d0ad82f4
Sam Seay <samueljseay@gmail.com>
2023-09-17 13:24:30 +0800

    Add a test to check the block patterns are populated in the header customizer.

--- plugins/woocommerce/tests/e2e-pw/tests/customize-store/assembler-hub.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/customize-store/assembler-hub.spec.js
@@ -2,6 +2,9 @@ const { test, expect, request } = require( '@playwright/test' );
 const { features } = require( '../../utils' );
 const { activateTheme } = require( '../../utils/themes' );
 
+const ASSEMBLER_HUB_URL =
+	'/wp-admin/admin.php?page=wc-admin&path=%2Fcustomize-store%2Fassembler-hub';
+
 test.describe( 'Store owner can view Assembler Hub for store customization', () => {
 	test.use( { storageState: process.env.ADMINSTATE } );
 
@@ -30,11 +33,32 @@ test.describe( 'Store owner can view Assembler Hub for store customization', () 
 	} );
 
 	test( 'Can view the Assembler Hub page', async ( { page } ) => {
-		await page.goto(
-			'wp-admin/admin.php?page=wc-admin&path=%2Fcustomize-store%2Fassembler-hub'
-		);
+		await page.goto( ASSEMBLER_HUB_URL );
 		await page.waitForSelector( `h1:text("Let's get creative")`, {
 			timeout: 5000,
 		} );
+	} );
+
+	test( 'Visiting change header should show a list of block patterns to choose from', async ( {
+		page,
+	} ) => {
+		await page.goto( ASSEMBLER_HUB_URL );
+		await page.waitForSelector( `h1:text("Let's get creative")`, {
+			timeout: 5000,
+		} );
+
+		await page.click( 'text=Change your header' );
+
+		// Wait for at least one element to appear
+		await page.waitForSelector(
+			'.block-editor-block-patterns-list__list-item',
+			{ timeout: 5000 }
+		);
+
+		// Now query all elements and check their count
+		const elements = await page.$$(
+			'.block-editor-block-patterns-list__list-item'
+		);
+		expect( elements.length ).toBe( 4 );
 	} );
 } );
