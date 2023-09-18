f213b400fa38e34a116788fc88c741d55816a9c2
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2023-09-18 13:33:41 +0800

    Update appearance task to choose a WP theme

--- plugins/woocommerce/src/Admin/Features/OnboardingTasks/Tasks/Appearance.php
+++ plugins/woocommerce/src/Admin/Features/OnboardingTasks/Tasks/Appearance.php
@@ -14,17 +14,6 @@ use Automattic\WooCommerce\Internal\Admin\WCAdminAssets;
 class Appearance extends Task {
 
 	/**
-	 * Constructor
-	 *
-	 * @param TaskList $task_list Parent task list.
-	 */
-	public function __construct( $task_list ) {
-		parent::__construct( $task_list );
-		add_action( 'admin_enqueue_scripts', array( $this, 'add_media_scripts' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'possibly_add_return_notice_script' ) );
-	}
-
-	/**
 	 * ID.
 	 *
 	 * @return string
@@ -39,13 +28,7 @@ class Appearance extends Task {
 	 * @return string
 	 */
 	public function get_title() {
-		if ( $this->get_parent_option( 'use_completed_title' ) === true ) {
-			if ( $this->is_complete() ) {
-				return __( 'You personalized your store', 'woocommerce' );
-			}
-			return __( 'Personalize your store', 'woocommerce' );
-		}
-		return __( 'Personalize my store', 'woocommerce' );
+		return __( 'Choose your theme', 'woocommerce' );
 	}
 
 	/**
@@ -55,7 +38,7 @@ class Appearance extends Task {
 	 */
 	public function get_content() {
 		return __(
-			'Add your logo, create a homepage, and start designing your store.',
+			"Choose a theme that best fits your brand's look and feel, then make it your own. Change the colors, add your logo, and create pages.",
 			'woocommerce'
 		);
 	}
@@ -70,68 +53,20 @@ class Appearance extends Task {
 	}
 
 	/**
-	 * Addtional data.
+	 * Action label.
 	 *
-	 * @return array
+	 * @return string
 	 */
-	public function get_additional_data() {
-		return array(
-			'has_homepage'        => self::has_homepage(),
-			'has_products'        => Products::has_products(),
-			'stylesheet'          => get_option( 'stylesheet' ),
-			'theme_mods'          => get_theme_mods(),
-			'support_custom_logo' => false !== get_theme_support( 'custom-logo' ),
-		);
+	public function get_action_label() {
+		return __( 'Choose theme', 'woocommerce' );
 	}
 
 	/**
-	 * Add media scripts for image uploader.
-	 */
-	public function add_media_scripts() {
-		if ( ! PageController::is_admin_page() || ! $this->can_view() ) {
-			return;
-		}
-
-		wp_enqueue_media();
-	}
-
-
-	/**
-	 * Adds a return to task list notice when completing the task.
+	 * Task completion.
 	 *
-	 * @param string $hook Page hook.
+	 * @return bool
 	 */
-	public function possibly_add_return_notice_script( $hook ) {
-		global $post;
-
-		if ( $hook !== 'post.php' || $post->post_type !== 'page' ) {
-			return;
-		}
-
-		if ( $this->is_complete() || ! $this->is_active() ) {
-			return;
-		}
-
-		WCAdminAssets::register_script( 'wp-admin-scripts', 'onboarding-homepage-notice', true );
-	}
-
-	/**
-	 * Check if the site has a homepage set up.
-	 */
-	public static function has_homepage() {
-		if ( get_option( 'classic-editor-replace' ) === 'classic' ) {
-			return true;
-		}
-
-		$homepage_id = get_option( 'woocommerce_onboarding_homepage_post_id', false );
-
-		if ( ! $homepage_id ) {
-			return false;
-		}
-
-		$post      = get_post( $homepage_id );
-		$completed = $post && $post->post_status === 'publish';
-
-		return $completed;
+	public function is_complete() {
+		return 'twentytwentythree' !== get_stylesheet() ;
 	}
 }
