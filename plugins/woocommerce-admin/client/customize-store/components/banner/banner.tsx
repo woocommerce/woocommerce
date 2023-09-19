be6475b215bf31e7d98be8ee7a1f3da356d22a01
Chris Runnells <chris.runnells@automattic.com>
2023-09-18 15:45:08 -1000

    Update markup and styling

--- plugins/woocommerce-admin/client/customize-store/components/banner/banner.tsx
+++ plugins/woocommerce-admin/client/customize-store/components/banner/banner.tsx
@@ -12,10 +12,14 @@ import './banner.scss';
 
 export const Banner = () => {
 	return (
-		<div className={ `woocommerce-customize-store-banner-content` }>
-			<h1>{ __( 'Use the power of AI to design your store', 'woocommerce' ) }</h1>
-			<p>{ __( 'Design the look of your store, create pages, and generate copy using our built-in AI tools.', 'woocommerce' ) }</p>
-			<div><a className="button" href="#">{ __('Design with AI') }</a></div>
+		<div className="woocommerce-customize-store-banner">
+			<div className={ `woocommerce-customize-store-banner-content` }>
+				<h1>{ __( 'Use the power of AI to design your store', 'woocommerce' ) }</h1>
+				<p>{ __( 'Design the look of your store, create pages, and generate copy using our built-in AI tools.', 'woocommerce' ) }</p>
+				<div>
+					<button>{ __( 'Design with AI', 'woocommerce' ) }</button>
+				</div>
+			</div>
 		</div>
 	);
 };
