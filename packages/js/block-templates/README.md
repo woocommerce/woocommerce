2ffcdd7aceb09e6cd3e4d893092af783fc25c339
Matt Sherman <matt@jam123.com>
2023-09-19 20:30:44 -0400

    Initial @woocommerce/block-templates package

---
+++ packages/js/block-templates/README.md
@@ -0,0 +1,3 @@
+# @woocommerce/block-templates
+
+A collection of utility functions for use with WooCommerce admin block templates.
---
+++ packages/js/block-templates/src/hooks/use-woo-block-props/use-woo-block-props.ts
@@ -0,0 +1,20 @@
+/**
+ * External dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+interface TemplateBlockAttributes {
+	_templateBlockId?: string;
+	_templateBlockOrder?: number;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	[ key: string ]: any;
+}
+
+export const useWooBlockProps = ( attributes: TemplateBlockAttributes ) => {
+	const additionalProps = {
+		'data-template-block-id': attributes._templateBlockId,
+		'data-template-block-order': attributes._templateBlockOrder,
+	};
+
+	return useBlockProps( additionalProps );
+};
