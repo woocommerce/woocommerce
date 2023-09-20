bf3c21daa0c87c4511ed1452d04eb588f954f36b
Matt Sherman <matt@jam123.com>
2023-09-20 15:15:32 -0400

    Update collapsible block

--- packages/js/product-editor/src/blocks/collapsible/edit.tsx
+++ packages/js/product-editor/src/blocks/collapsible/edit.tsx
@@ -1,13 +1,14 @@
 /**
  * External dependencies
  */
+import { useWooBlockProps } from '@woocommerce/block-templates';
 import { CollapsibleContent } from '@woocommerce/components';
 import type { BlockAttributes } from '@wordpress/blocks';
 import { createElement } from '@wordpress/element';
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks } from '@wordpress/block-editor';
 
 export function Edit( { attributes }: { attributes: BlockAttributes } ) {
-	const blockProps = useBlockProps();
+	const blockProps = useWooBlockProps( attributes );
 	const { toggleText, initialCollapsed, persistRender = true } = attributes;
 
 	return (
