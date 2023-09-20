effe8d940c1ba2307237c5ab75cef3dbe7fdf7b5
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2023-09-20 12:48:16 +0800

    Fix lint issues

--- plugins/woocommerce-admin/client/customize-store/intro/index.tsx
+++ plugins/woocommerce-admin/client/customize-store/intro/index.tsx
@@ -34,7 +34,6 @@ export const Intro: CustomizeStoreComponent = ( { sendEvent, context } ) => {
 			</div>
 
 			<div className="woocommerce-customize-store-container">
-
 				<div className="woocommerce-customize-store-sidebar">
 					<div className="woocommerce-customize-store-sidebar__title">
 						<button
@@ -46,30 +45,52 @@ export const Intro: CustomizeStoreComponent = ( { sendEvent, context } ) => {
 						</button>
 						{ __( 'Customize your store', 'woocommerce' ) }
 					</div>
-					<p>{ __( 'Create a store that reflects your brand and business. Select one of our professionally designed themes to customize, or create your own using AI.', 'woocommerce' ) }</p>
+					<p>
+						{ __(
+							'Create a store that reflects your brand and business. Select one of our professionally designed themes to customize, or create your own using AI.',
+							'woocommerce'
+						) }
+					</p>
 				</div>
 
 				<div className="woocommerce-customize-store-main">
-
 					<div className="woocommerce-customize-store-banner">
-						<div className={ `woocommerce-customize-store-banner-content` }>
-							<h1>{ __( 'Use the power of AI to design your store', 'woocommerce' ) }</h1>
-							<p>{ __( 'Design the look of your store, create pages, and generate copy using our built-in AI tools.', 'woocommerce' ) }</p>
-							<button onClick={ () => sendEvent( { type: 'DESIGN_WITH_AI' } ) }>
+						<div
+							className={ `woocommerce-customize-store-banner-content` }
+						>
+							<h1>
+								{ __(
+									'Use the power of AI to design your store',
+									'woocommerce'
+								) }
+							</h1>
+							<p>
+								{ __(
+									'Design the look of your store, create pages, and generate copy using our built-in AI tools.',
+									'woocommerce'
+								) }
+							</p>
+							<button
+								onClick={ () =>
+									sendEvent( { type: 'DESIGN_WITH_AI' } )
+								}
+							>
 								{ __( 'Design with AI', 'woocommerce' ) }
 							</button>
 						</div>
 					</div>
 
-					<p>{ __( 'Or select a professionally designed theme to customize and make your own.', 'woocommerce' ) }</p>
+					<p>
+						{ __(
+							'Or select a professionally designed theme to customize and make your own.',
+							'woocommerce'
+						) }
+					</p>
 
 					<div className="woocommerce-customize-store-theme-cards">
 						{ themeCards?.map( ( themeCard ) => (
-							<div 
-								className="theme-card"
-								key={ themeCard.slug }
-							>
-								<div 
+							<div className="theme-card" key={ themeCard.slug }>
+								<div
 									onClick={ () =>
 										sendEvent( {
 											type: 'SELECTED_NEW_THEME',
@@ -77,25 +98,34 @@ export const Intro: CustomizeStoreComponent = ( { sendEvent, context } ) => {
 										} )
 									}
 								>
-									<img 
+									<img
 										src={ themeCard.image }
 										alt={ themeCard.description }
 									/>
 								</div>
-								<h2 className="theme-card__title">{ themeCard.name }</h2>
+								<h2 className="theme-card__title">
+									{ themeCard.name }
+								</h2>
 							</div>
 						) ) }
 					</div>
 
 					<div className="woocommerce-customize-store-browse-themes">
-						<button onClick={ () => sendEvent( { type: 'SELECTED_BROWSE_ALL_THEMES' } ) } >
-								{ __( 'Browse all themes', 'woocommerce' ) }
+						<button
+							onClick={ () =>
+								sendEvent( {
+									type: 'SELECTED_BROWSE_ALL_THEMES',
+								} )
+							}
+						>
+							{ __( 'Browse all themes', 'woocommerce' ) }
 						</button>
 					</div>
 
-
 					<button
-						onClick={ () => sendEvent( { type: 'SELECTED_ACTIVE_THEME' } ) }
+						onClick={ () =>
+							sendEvent( { type: 'SELECTED_ACTIVE_THEME' } )
+						}
 					>
 						Assembler Hub
 					</button>
