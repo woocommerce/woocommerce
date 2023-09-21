c3a72f5fa98acfc0c16869805f10363142abc9c3
Lourens Schep <lourensschep@gmail.com>
2023-09-21 10:49:03 -0300

    Add support for default_values when generating product variations

--- packages/js/product-editor/src/hooks/use-product-variations-helper.ts
+++ packages/js/product-editor/src/hooks/use-product-variations-helper.ts
@@ -9,12 +9,49 @@ import {
 	EXPERIMENTAL_PRODUCT_VARIATIONS_STORE_NAME,
 	Product,
 	ProductDefaultAttribute,
+	ProductVariation,
 } from '@woocommerce/data';
 
 /**
  * Internal dependencies
  */
 import { EnhancedProductAttribute } from './use-product-attributes';
+
+async function getDefaultVariationValues(
+	productId: number
+): Promise< Partial< Omit< ProductVariation, 'id' > > > {
+	const attributes = (
+		( await resolveSelect( 'core' ).getEntityRecord(
+			'postType',
+			'product',
+			productId
+		) ) as Product
+	 ).attributes;
+	const alreadyHasVariableAttribute = attributes.some(
+		( attr ) => attr.variation
+	);
+	if ( ! alreadyHasVariableAttribute ) {
+		return {};
+	}
+	try {
+		const productsWithPrice = await resolveSelect(
+			EXPERIMENTAL_PRODUCT_VARIATIONS_STORE_NAME
+		).getProductVariations< Pick< ProductVariation, 'regular_price' >[] >( {
+			product_id: productId,
+			per_page: 1,
+			has_price: true,
+			_fields: [ 'regular_price' ],
+		} );
+		if ( productsWithPrice && productsWithPrice.length > 0 ) {
+			return {
+				regular_price: productsWithPrice[ 0 ].regular_price,
+			};
+		}
+	} catch {
+		return {};
+	}
+	return {};
+}
 
 export function useProductVariationsHelper() {
 	const [ productId ] = useEntityProp< number >(
@@ -47,6 +84,10 @@ export function useProductVariationsHelper() {
 				( attr ) => attr.variation
 			);
 
+			const defaultVariationValues = await getDefaultVariationValues(
+				productId
+			);
+
 			return _generateProductVariations< {
 				count: number;
 				deleted_count: number;
@@ -61,6 +102,7 @@ export function useProductVariationsHelper() {
 				},
 				{
 					delete: true,
+					default_values: defaultVariationValues,
 				}
 			)
 				.then( () => {
