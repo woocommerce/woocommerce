da9bd6189cb77a793d01f8ccc44096a86090e9ca
Bero <berislav.grgicak@gmail.com>
2023-09-21 14:05:07 +0200

    Update search section display conditions

--- plugins/woocommerce-admin/client/marketplace/components/search-results/search-results.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/search-results/search-results.tsx
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { useContext } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -10,12 +11,17 @@ import { Product, ProductType } from '../product-list/types';
 import Extensions from '../extensions/extensions';
 import Themes from '../themes/themes';
 import { MARKETPLACE_SEARCH_RESULTS_PER_PAGE } from '../constants';
+import NoResults from '../product-list-content/no-results';
+import { MarketplaceContext } from '../../contexts/marketplace-context';
+import ProductLoader from '../product-loader/product-loader';
 
 export interface SearchResultProps {
 	products?: Product[];
 }
 
 export default function SearchResults( props: SearchResultProps ): JSX.Element {
+	const marketplaceContextValue = useContext( MarketplaceContext );
+	const { isLoading } = marketplaceContextValue;
 	const extensions = props.products?.filter(
 		( product ) => product.type === ProductType.extension
 	);
@@ -23,16 +29,36 @@ export default function SearchResults( props: SearchResultProps ): JSX.Element {
 		( product ) => product.type === ProductType.theme
 	);
 
+	function content() {
+		if ( isLoading ) {
+			return <ProductLoader />;
+		}
+
+		if ( props.products?.length === 0 ) {
+			return <NoResults />;
+		}
+
+		return (
+			<>
+				{ !! extensions?.length && (
+					<Extensions
+						products={ extensions }
+						perPage={ MARKETPLACE_SEARCH_RESULTS_PER_PAGE }
+					/>
+				) }
+				{ !! themes?.length && (
+					<Themes
+						products={ themes }
+						perPage={ MARKETPLACE_SEARCH_RESULTS_PER_PAGE }
+					/>
+				) }
+			</>
+		);
+	}
+
 	return (
 		<div className="woocommerce-marketplace__search-results">
-			<Extensions
-				products={ extensions }
-				perPage={ MARKETPLACE_SEARCH_RESULTS_PER_PAGE }
-			/>
-			<Themes
-				products={ themes }
-				perPage={ MARKETPLACE_SEARCH_RESULTS_PER_PAGE }
-			/>
+			{ content() }
 		</div>
 	);
 }
