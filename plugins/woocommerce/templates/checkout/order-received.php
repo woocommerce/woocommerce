89bd514cba2cbfcc6fb9b0de0c00d754cc26f06d
Yordan Soares <contacto@yordansoar.es>
2023-09-22 16:46:30 +0200

    Bump template version to 8.3.0

--- plugins/woocommerce/templates/checkout/order-received.php
+++ plugins/woocommerce/templates/checkout/order-received.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 8.1.0
+ * @version 8.3.0
  *
  * @var WC_Order|false $order
  */
