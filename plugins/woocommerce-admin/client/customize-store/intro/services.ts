9bc108f4be26c8e0cb8e6fc3cce92005a7afc6a5
moon <moon.kyong@automattic.com>
2023-09-25 14:50:53 -0700

    Use correct APIs to check  template, pages, and styles modifications

--- plugins/woocommerce-admin/client/customize-store/intro/services.ts
+++ plugins/woocommerce-admin/client/customize-store/intro/services.ts
@@ -3,6 +3,8 @@
  */
 import { resolveSelect } from '@wordpress/data';
 import { ONBOARDING_STORE_NAME } from '@woocommerce/data';
+// @ts-ignore No types for this exist yet.
+import { store as coreStore } from '@wordpress/core-data';
 
 // placeholder xstate async service that returns a set of theme cards
 export const fetchThemeCards = async () => {
@@ -63,11 +65,34 @@ export const fetchThemeCards = async () => {
 };
 
 export const fetchIntroData = async () => {
-	const { getActiveThemeModsCount, getTask } = resolveSelect(
-		ONBOARDING_STORE_NAME
-	);
+	const currentTemplate = await resolveSelect(
+		coreStore
+		// @ts-expect-error No types for this exist yet.
+	).__experimentalGetTemplateForLink( '/' );
 
-	const activeThemeHasMods = ( await getActiveThemeModsCount() ) > 0;
+	const styleRevs = await resolveSelect(
+		coreStore
+		// @ts-expect-error No types for this exist yet.
+	).getCurrentThemeGlobalStylesRevisions();
+
+	const hasModifiedPages = await resolveSelect( coreStore )
+		// @ts-expect-error No types for this exist yet.
+		.getEntityRecords( 'postType', 'page', {
+			per_page: 100,
+			_fields: [ 'id', '_links.version-history' ],
+			orderby: 'menu_order',
+			order: 'asc',
+		} )
+		?.some( ( page: { _links: { [ key: string ]: string[] } } ) => {
+			return page._links?.[ 'version-history' ]?.length > 1;
+		} );
+
+	const { getTask } = resolveSelect( ONBOARDING_STORE_NAME );
+
+	const activeThemeHasMods =
+		currentTemplate?.modified !== null ||
+		styleRevs?.length > 0 ||
+		hasModifiedPages.length > 0;
 	const customizeStoreTaskCompleted = ( await getTask( 'customize-store' ) )
 		?.isComplete;
 	const themeCards = await fetchThemeCards();
