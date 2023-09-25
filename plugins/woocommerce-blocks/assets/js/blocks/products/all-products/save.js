358b76a3dba5bd2cd027f37d566c4e06c89cc87d
Hritik Chaudhary <hritikshiwach7@gmail.com>
2023-09-25 20:27:59 +0530

    Remove data attributes from being saved in html of All products block (https://github.com/woocommerce/woocommerce-blocks/pull/10032)
    * Remove data attributes from being saved in html of All products block
    
    * added old save in deprecated
    
    * Addressed review comments
    
    ---------
    
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>
    Co-authored-by: Roy Ho <roykho77@gmail.com>

--- plugins/woocommerce-blocks/assets/js/blocks/products/all-products/save.js
+++ plugins/woocommerce-blocks/assets/js/blocks/products/all-products/save.js
@@ -9,22 +9,12 @@ import { InnerBlocks } from '@wordpress/block-editor';
 import { getBlockClassName } from '../utils.js';
 
 export default function save( { attributes } ) {
-	const dataAttributes = {};
-	Object.keys( attributes )
-		.sort()
-		.forEach( ( key ) => {
-			dataAttributes[ key ] = attributes[ key ];
-		} );
-	const data = {
-		'data-attributes': JSON.stringify( dataAttributes ),
-	};
 	return (
 		<div
 			className={ getBlockClassName(
 				'wc-block-all-products',
 				attributes
 			) }
-			{ ...data }
 		>
 			<InnerBlocks.Content />
 		</div>
