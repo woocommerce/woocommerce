0ee05e00ead24a2d83c59ddf17a8d0c495755c15
Ilyas Foo <foo.ilyas@gmail.com>
2023-09-25 11:38:01 +0800

    Lint

--- plugins/woocommerce-admin/client/customize-store/assembler-hub/hooks/test/use-home-templates.js
+++ plugins/woocommerce-admin/client/customize-store/assembler-hub/hooks/test/use-home-templates.js
@@ -12,39 +12,45 @@ import { usePatterns } from '../use-patterns';
 // Mocking the dependent hooks and data
 jest.mock( '../use-patterns' );
 jest.mock( '~/customize-store/data/homepageTemplates', () => ( {
-  HOMEPAGE_TEMPLATES: {
-    'template1': { blocks: [ 'header', 'content1', 'content2', 'footer' ] },
-    'template2': { blocks: [ 'header', 'content3', 'footer' ] }
-  },
+	HOMEPAGE_TEMPLATES: {
+		template1: { blocks: [ 'header', 'content1', 'content2', 'footer' ] },
+		template2: { blocks: [ 'header', 'content3', 'footer' ] },
+	},
 } ) );
 
 const mockUsePatterns = usePatterns;
 
 const mockPatternsByName = {
-  'header': { name: 'header', content: '<div>Header</div>' },
-  'content1': { name: 'content1', content: '<div>Content1</div>' },
-  'content2': { name: 'content2', content: '<div>Content2</div>' },
-  'content3': { name: 'content3', content: '<div>Content3</div>' },
-  'footer': { name: 'footer', content: '<div>Footer</div>' }
+	header: { name: 'header', content: '<div>Header</div>' },
+	content1: { name: 'content1', content: '<div>Content1</div>' },
+	content2: { name: 'content2', content: '<div>Content2</div>' },
+	content3: { name: 'content3', content: '<div>Content3</div>' },
+	footer: { name: 'footer', content: '<div>Footer</div>' },
 };
 
 describe( 'useHomeTemplates', () => {
-  beforeEach( () => {
-    mockUsePatterns.mockReturnValue( {
-      blockPatterns: Object.values( mockPatternsByName ),
-      isLoading: false
-    } );
-  } );
+	beforeEach( () => {
+		mockUsePatterns.mockReturnValue( {
+			blockPatterns: Object.values( mockPatternsByName ),
+			isLoading: false,
+		} );
+	} );
 
-  it( 'should return home templates without first and last items', () => {
-    const { result } = renderHook( () => useHomeTemplates() );
+	it( 'should return home templates without first and last items', () => {
+		const { result } = renderHook( () => useHomeTemplates() );
 
-    // The expected result based on the HOMEPAGE_TEMPLATES and mock patterns
-    const expectedResult = {
-      'template1': getTemplatePatterns( [ 'content1', 'content2' ], mockPatternsByName ),
-      'template2': getTemplatePatterns( [ 'content3' ], mockPatternsByName )
-    };
+		// The expected result based on the HOMEPAGE_TEMPLATES and mock patterns
+		const expectedResult = {
+			template1: getTemplatePatterns(
+				[ 'content1', 'content2' ],
+				mockPatternsByName
+			),
+			template2: getTemplatePatterns(
+				[ 'content3' ],
+				mockPatternsByName
+			),
+		};
 
-    expect( result.current.homeTemplates ).toEqual( expectedResult );
-  } );
+		expect( result.current.homeTemplates ).toEqual( expectedResult );
+	} );
 } );
