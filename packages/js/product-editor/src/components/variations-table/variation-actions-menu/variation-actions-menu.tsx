2723566ecb6b6a74749a98dce706b930cdad34ff
Fernando Marichal <ultimoround@gmail.com>
2023-09-26 10:10:54 -0300

    Add noreferrer

--- packages/js/product-editor/src/components/variations-table/variation-actions-menu/variation-actions-menu.tsx
+++ packages/js/product-editor/src/components/variations-table/variation-actions-menu/variation-actions-menu.tsx
@@ -46,6 +46,7 @@ export function VariationActionsMenu( {
 						<MenuItem
 							href={ selection.permalink }
 							target="_blank"
+							rel="noreferrer"
 							onClick={ () => {
 								recordEvent( 'product_variations_preview', {
 									source: TRACKS_SOURCE,
