ec0921c58885a59305fe3eec854b2d73c52456b4
Joel <dygerati@gmail.com>
2023-09-27 12:19:05 -0700

    Adding basic link with styles

--- plugins/woo-ai/src/image-background-removal/background-removal-link.tsx
+++ plugins/woo-ai/src/image-background-removal/background-removal-link.tsx
@@ -1,3 +1,13 @@
+/**
+ * Internal dependencies
+ */
+import MagicIcon from '../../assets/images/icons/magic.svg';
+
 export const BackgroundRemovalLink = () => {
-	return <span>Test</span>;
+	return (
+		<>
+			<button>Remove background</button>
+			<img src={ MagicIcon } alt="" />
+		</>
+	);
 };
