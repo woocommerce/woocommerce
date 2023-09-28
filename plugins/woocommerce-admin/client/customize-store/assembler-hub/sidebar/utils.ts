010a4aace6f24c839afe7f422a863c7f8bc27bb1
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-09-28 23:33:24 +0800

    Fix findPatternByBlock

--- plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/utils.ts
+++ plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/utils.ts
@@ -13,14 +13,17 @@ export const findPatternByBlock = (
 	patterns: PatternWithBlocks[],
 	block: BlockInstance
 ) => {
-	const blockAttributes = {
-		...block.attributes,
+	const blockAttributes = block.attributes;
+	if (
+		blockAttributes.className &&
+		blockAttributes.className.includes( 'preview-opacity' )
+	) {
 		// Remove the preview opacity class from the footer block because it's added by the editor, not the pattern.
-		className: block.attributes.className.replaceAll(
+		blockAttributes.className = blockAttributes.className.replaceAll(
 			' preview-opacity',
 			''
-		),
-	};
+		);
+	}
 
 	return patterns.find( ( pattern ) =>
 		isEqual( pattern.blocks[ 0 ].attributes, blockAttributes )
