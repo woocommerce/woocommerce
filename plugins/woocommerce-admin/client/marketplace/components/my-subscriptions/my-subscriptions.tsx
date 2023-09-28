3de2a2bf3daacc8e8743b216fb9ac08fdf8e927a
Bero <berislav.grgicak@gmail.com>
2023-09-28 20:49:22 +0200

    Fix linter error

--- plugins/woocommerce-admin/client/marketplace/components/my-subscriptions/my-subscriptions.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/my-subscriptions/my-subscriptions.tsx
@@ -143,20 +143,16 @@ export default function MySubscriptions(): JSX.Element {
 	const getVersion = ( subscription: Subscription ): string => {
 		if ( subscription.local.version === subscription.version ) {
 			return subscription.local.version;
-		} 
-		
+		}
 		if ( subscription.local.version && subscription.version ) {
 			return subscription.local.version + ' > ' + subscription.version;
 		}
-		
 		if ( subscription.version ) {
 			return subscription.version;
 		}
-		
-		  if ( subscription.local.version ) {
+		if ( subscription.local.version ) {
 			return subscription.local.version;
 		}
-		
 		return '';
 	};
 
