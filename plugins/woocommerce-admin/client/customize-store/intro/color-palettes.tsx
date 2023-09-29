b2fac99d33e81c4f095dc3649b3f01076b7e66f4
moon <moon.kyong@automattic.com>
2023-09-29 13:02:32 -0700

    Remove use of color palette border color

--- plugins/woocommerce-admin/client/customize-store/intro/color-palettes.tsx
+++ plugins/woocommerce-admin/client/customize-store/intro/color-palettes.tsx
@@ -11,26 +11,22 @@ export const ColorPalettes = ( {
 	return (
 		<ul className="theme-card__color-palettes">
 			{ colorPalettes.map( ( colorPalette ) => (
-				<li key={ colorPalette.title }>
-					<div
-						className="theme-card__color-palette-left"
-						style={ {
-							border: colorPalette?.primary_border
-								? '1px solid ' + colorPalette.primary_border
-								: 'none',
-							backgroundColor: colorPalette.primary,
-						} }
-					></div>
-					<div
-						className="theme-card__color-palette-right"
-						style={ {
-							border: colorPalette?.secondary_border
-								? '1px solid ' + colorPalette.secondary_border
-								: 'none',
-							backgroundColor: colorPalette.secondary,
-						} }
-					></div>
-				</li>
+				<li
+					key={ colorPalette.title }
+					style={ {
+						background:
+							'linear-gradient(to right, ' +
+							colorPalette.primary +
+							' 0px, ' +
+							colorPalette.primary +
+							' 50%, ' +
+							colorPalette.secondary +
+							' 50%, ' +
+							colorPalette.secondary +
+							' 100%' +
+							')',
+					} }
+				></li>
 			) ) }
 		</ul>
 	);
