6f82692e6cd0d995540445d62ed2a60ae4bd087a
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-10-02 16:28:15 -0300

    Add Toggle shipping item to the variations quick actions menu

--- packages/js/product-editor/src/components/variations-table/shipping-menu-item/shipping-menu-item.tsx
+++ packages/js/product-editor/src/components/variations-table/shipping-menu-item/shipping-menu-item.tsx
@@ -74,6 +74,41 @@ export function ShippingMenuItem( {
 								'product_variations_menu_shipping_select',
 								{
 									source: TRACKS_SOURCE,
+									action: 'toggle_shipping',
+									variation_id: ids,
+								}
+							);
+							if ( Array.isArray( selection ) ) {
+								onChange(
+									selection.map( ( { id, virtual } ) => ( {
+										id,
+										virtual: ! virtual,
+									} ) )
+								);
+							} else {
+								onChange( {
+									virtual: ! selection.virtual,
+								} );
+							}
+							recordEvent(
+								'product_variations_menu_shipping_update',
+								{
+									source: TRACKS_SOURCE,
+									action: 'toggle_shipping',
+									variation_id: ids,
+								}
+							);
+							onClose();
+						} }
+					>
+						{ __( 'Toggle shipping', 'woocommerce' ) }
+					</MenuItem>
+					<MenuItem
+						onClick={ () => {
+							recordEvent(
+								'product_variations_menu_shipping_select',
+								{
+									source: TRACKS_SOURCE,
 									action: 'dimensions_length_set',
 									variation_id: ids,
 								}
