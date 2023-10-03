367d6ef31a1c12396caa338c860b887cf9908eeb
Jaclyn Chen <watertranquil@gmail.com>
2023-10-03 16:08:26 +0800

    Extract mobile installation view to a component, and show it in the installation page and Jetpack installed page with style tweaks.

---
+++ plugins/woocommerce-admin/client/homescreen/mobile-app-modal/components/InstallMobileAppInfo.tsx
@@ -0,0 +1,27 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import MobileQRCode from '../illustrations/woo-mobile-download-qrcode.svg';
+
+export const InstallMobileAppInfo = () => {
+	return (
+		<div>
+			<img
+				src={ MobileQRCode }
+				alt={ __( 'Download WooCommerce mobile app', 'woocommerce' ) }
+				width="100"
+			/>
+			<p>
+				{ __(
+					'Scan the code above to download the WooCommerce mobile app, or visit woocommerce.com/mobile from your mobile device.',
+					'woocommerce'
+				) }
+			</p>
+		</div>
+	);
+};
--- plugins/woocommerce-admin/client/homescreen/mobile-app-modal/pages/InstallMobileAppPage.tsx
+++ plugins/woocommerce-admin/client/homescreen/mobile-app-modal/pages/InstallMobileAppPage.tsx
@@ -1,33 +1,13 @@
 /**
- * External dependencies
- */
-import { __ } from '@wordpress/i18n';
-
-/**
  * Internal dependencies
  */
-import MobileQRCode from '../illustrations/woo-mobile-download-qrcode.svg';
 import { ModalContentLayoutWithTitle } from '../layouts/ModalContentLayoutWithTitle';
+import { InstallMobileAppInfo } from '../components/InstallMobileAppInfo';
 
 export const InstallMobileAppPage = () => {
 	return (
 		<ModalContentLayoutWithTitle>
-			<div>
-				<img
-					src={ MobileQRCode }
-					alt={ __(
-						'Download WooCommerce mobile app',
-						'woocommerce'
-					) }
-					width="100"
-				/>
-				<p>
-					{ __(
-						'Scan the code above to download the WooCommerce mobile app, or visit woocommerce.com/mobile from your mobile device.',
-						'woocommerce'
-					) }
-				</p>
-			</div>
+			<InstallMobileAppInfo />
 		</ModalContentLayoutWithTitle>
 	);
 };
