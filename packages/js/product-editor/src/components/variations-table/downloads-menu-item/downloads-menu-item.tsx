7f6b06b23e94aa82bc0758c7e2b583851add1bc4
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-10-03 10:50:09 -0300

    Add Downloads -> Upload files item to the variations quick actions menu

--- packages/js/product-editor/src/components/variations-table/downloads-menu-item/downloads-menu-item.tsx
+++ packages/js/product-editor/src/components/variations-table/downloads-menu-item/downloads-menu-item.tsx
@@ -5,6 +5,7 @@ import { Dropdown, MenuItem } from '@wordpress/components';
 import { createElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { chevronRight } from '@wordpress/icons';
+import { MediaUpload } from '@wordpress/media-utils';
 import { recordEvent } from '@woocommerce/tracks';
 
 /**
@@ -14,6 +15,10 @@ import { TRACKS_SOURCE } from '../../../constants';
 import { VariationActionsMenuItemProps } from '../types';
 import { handlePrompt } from '../../../utils/handle-prompt';
 
+function convertMediaFileToDownloadFile( value: Record< string, any > ) {
+	return { id: `${ value.id }`, name: value.name, file: value.url };
+}
+
 export function DownloadsMenuItem( {
 	selection,
 	onChange,
@@ -22,6 +27,50 @@ export function DownloadsMenuItem( {
 	const ids = Array.isArray( selection )
 		? selection.map( ( { id } ) => id )
 		: selection.id;
+
+	function handleMediaUploadSelect(
+		value: Record< string, any > | Record< string, any >[]
+	) {
+		const downloads = Array.isArray( value )
+			? value.map( convertMediaFileToDownloadFile )
+			: convertMediaFileToDownloadFile( value );
+
+		const partialVariation = {
+			downloadable: true,
+			downloads,
+		};
+
+		if ( Array.isArray( selection ) ) {
+			onChange(
+				selection.map( ( { id } ) => ( {
+					...partialVariation,
+					id,
+				} ) )
+			);
+		} else {
+			onChange( partialVariation );
+		}
+
+		recordEvent( 'product_variations_menu_downloads_update', {
+			source: TRACKS_SOURCE,
+			action: 'downloads_set',
+			variation_id: ids,
+		} );
+
+		onClose();
+	}
+
+	function uploadFilesClickHandler( openMediaUploadModal: () => void ) {
+		return function handleUploadFilesClick() {
+			recordEvent( 'product_variations_menu_downloads_select', {
+				source: TRACKS_SOURCE,
+				action: 'downloads_set',
+				variation_id: ids,
+			} );
+
+			openMediaUploadModal();
+		};
+	}
 
 	function menuItemClickHandler( name: string, message: string ) {
 		return function handleMenuItemClick() {
@@ -79,6 +128,19 @@ export function DownloadsMenuItem( {
 			) }
 			renderContent={ () => (
 				<div className="components-dropdown-menu__menu">
+					<MediaUpload
+						// @ts-expect-error multiple also accepts string.
+						multiple={ 'add' }
+						onSelect={ handleMediaUploadSelect }
+						render={ ( { open } ) => (
+							<MenuItem
+								onClick={ uploadFilesClickHandler( open ) }
+							>
+								{ __( 'Upload files', 'woocommerce' ) }
+							</MenuItem>
+						) }
+					/>
+
 					<MenuItem
 						onClick={ menuItemClickHandler(
 							'download_limit',
