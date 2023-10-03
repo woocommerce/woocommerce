dfd6e565017f5f6e477159a20e128e90bf9525ce
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-10-03 12:55:09 +0800

    Remove wp-editor dependency if we're not on a customize store page

--- plugins/woocommerce/src/Internal/Admin/WCAdminAssets.php
+++ plugins/woocommerce/src/Internal/Admin/WCAdminAssets.php
@@ -298,11 +298,22 @@ class WCAdminAssets {
 				$script_assets_filename = self::get_script_asset_filename( $script_path_name, 'index' );
 				$script_assets          = require WC_ADMIN_ABSPATH . WC_ADMIN_DIST_JS_FOLDER . $script_path_name . '/' . $script_assets_filename;
 
-				global $wp_version;
+				global
+				$wp_version;
 				if ( 'app' === $script_path_name && version_compare( $wp_version, '6.3', '<' ) ) {
 					// Remove wp-router dependency for WordPress versions < 6.3 because wp-router is not included in those versions. We only use wp-router in customize store pages and the feature is only available in WordPress 6.3+.
 					// We can remove this once our minimum support is WP 6.3.
 					$script_assets['dependencies'] = array_diff( $script_assets['dependencies'], array( 'wp-router' ) );
+				}
+
+				// Remove wp-editor dependency if we're not on a customize store page since we don't use wp-editor in other pages.
+				$is_customize_store_page = (
+					PageController::is_admin_page() &&
+					isset( $_GET['path'] ) &&
+					str_starts_with( wc_clean( wp_unslash( $_GET['path'] ) ), '/customize-store' )
+				);
+				if ( ! $is_customize_store_page ) {
+					$script_assets['dependencies'] = array_diff( $script_assets['dependencies'], array( 'wp-editor' ) );
 				}
 
 				wp_register_script(
