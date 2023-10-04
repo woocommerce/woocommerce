28d3da940361901b77d5d1f6a6e142240d81c969
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-10-04 14:47:19 +0200

    Store Customization > Wireframe and adjust width for the Just Arrived Full Hero pattern (https://github.com/woocommerce/woocommerce-blocks/pull/11115)
    * Update the width; Add the button text to the patterns dictionary; adjust the width to wide.
    
    * Remove unnecessary space before the title.

--- plugins/woocommerce-blocks/patterns/just-arrived-full-hero.php
+++ plugins/woocommerce-blocks/patterns/just-arrived-full-hero.php
@@ -8,20 +8,30 @@
 use Automattic\WooCommerce\Blocks\Patterns\PatternsHelper;
 $content = PatternsHelper::get_pattern_content( 'woocommerce-blocks/just-arrived-full-hero' );
 ?>
-<!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"contentPosition":"center right","isDark":false,"align":"full","style":{"spacing":{"padding":{"right":"4em"}}}} -->
-<div class="wp-block-cover alignfull is-light has-custom-content-position is-position-center-right" style="padding-right:4em"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"x-large","anchor":"just-arrived"} -->
-<h2 class="wp-block-heading has-x-large-font-size" id="just-arrived" style="font-style:normal;font-weight:700"><?php echo esc_html( $content['titles'][0]['default'] ); ?></h2>
-<!-- /wp:heading -->
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"contentPosition":"center right","isDark":false,"align":"wide","style":{"spacing":{"padding":{"right":"4em"}}}} -->
+<div class="wp-block-cover alignwide is-light has-custom-content-position is-position-center-right" style="padding-right:4em">
+	<span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+	<div class="wp-block-cover__inner-container">
+		<!-- wp:group {"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:heading {"anchor":"just-arrived"} -->
+			<h2 class="wp-block-heading" id="just-arrived"><?php echo esc_html( $content['titles'][0]['default'] ); ?></h2>
+			<!-- /wp:heading -->
 
-<!-- wp:paragraph {"fontSize":"medium"} -->
-<p class="has-medium-font-size"><?php echo esc_html( $content['descriptions'][0]['default'] ); ?></p>
-<!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+			<p><?php echo esc_html( $content['descriptions'][0]['default'] ); ?></p>
+			<!-- /wp:paragraph -->
 
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"black","textColor":"white","className":"is-style-fill","fontSize":"small"} -->
-<div class="wp-block-button has-custom-font-size is-style-fill has-small-font-size"><a class="wp-block-button__link has-white-color has-black-background-color has-text-color has-background wp-element-button">Shop now</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:group --></div></div>
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons">
+				<!-- wp:button -->
+				<div class="wp-block-button">
+					<a class="wp-block-button__link wp-element-button"><?php echo esc_html( $content['buttons'][0]['default'] ); ?></a>
+				</div>
+				<!-- /wp:button -->
+			</div>
+			<!-- /wp:buttons -->
+		</div>
+		<!-- /wp:group -->
+	</div></div>
 <!-- /wp:cover -->
