9805a665abaa5d9207b570b1510e514bc2057f1f
Tom Cafferkey <tjcafferkey@gmail.com>
2023-10-04 13:03:52 +0200

    Remove opinionated styles from the Hero Product 3 Split pattern (https://github.com/woocommerce/woocommerce-blocks/pull/11110)

--- plugins/woocommerce-blocks/patterns/hero-product-3-split.php
+++ plugins/woocommerce-blocks/patterns/hero-product-3-split.php
@@ -14,8 +14,8 @@ $images  = PatternsHelper::get_pattern_images( 'woocommerce-blocks/hero-product-
 <div class="wp-block-columns alignwide" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
 	<!-- wp:column {"width":"66.66%"} -->
 	<div class="wp-block-column" style="flex-basis:66.66%">
-		<!-- wp:media-text {"mediaPosition":"right","mediaId":1,"mediaLink":"<?php echo esc_url( plugins_url( 'images/pattern-placeholders/hand-guitar-finger-tshirt-clothing-rack.png', dirname( __FILE__ ) ) ); ?>","mediaType":"image","style":{"color":{"background":"#000000","text":"#ffffff"}}} -->
-		<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile has-text-color has-background" style="color:#ffffff;background-color:#000000">
+		<!-- wp:media-text {"mediaPosition":"right","mediaId":1,"mediaLink":"<?php echo esc_url( plugins_url( 'images/pattern-placeholders/hand-guitar-finger-tshirt-clothing-rack.png', dirname( __FILE__ ) ) ); ?>","mediaType":"image"} -->
+		<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile">
 			<div class="wp-block-media-text__content">
 				<!-- wp:group {"style":{"spacing":{"margin":{"top":"20px","bottom":"20px"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
 				<div class="wp-block-group" style="margin-top:20px;margin-bottom:20px;">
@@ -29,9 +29,9 @@ $images  = PatternsHelper::get_pattern_images( 'woocommerce-blocks/hero-product-
 
 					<!-- wp:buttons -->
 					<div class="wp-block-buttons">
-						<!-- wp:button {"textAlign":"left","style":{"color":{"background":"#ffffff","text":"#000000"}}} -->
+						<!-- wp:button {"textAlign":"left"} -->
 						<div class="wp-block-button has-custom-font-size">
-							<a class="wp-block-button__link has-text-color has-background has-text-align-left wp-element-button" href="<?php echo esc_url( get_permalink( wc_get_page_id( 'shop' ) ) ); ?>" style="color:#000000;background-color:#ffffff"><?php esc_attr_e( 'Shop now', 'woo-gutenberg-products-block' ); ?></a>
+							<a class="wp-block-button__link has-text-align-left wp-element-button" href="<?php echo esc_url( get_permalink( wc_get_page_id( 'shop' ) ) ); ?>"><?php esc_attr_e( 'Shop now', 'woo-gutenberg-products-block' ); ?></a>
 						</div>
 						<!-- /wp:button -->
 					</div>
