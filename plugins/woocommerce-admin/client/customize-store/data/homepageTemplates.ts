5da7f24eea7fd9a0b5dfdaa8ac54dae33c453ad8
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-10-04 19:49:45 +0200

    Update the Homepage Templates list.

--- plugins/woocommerce-admin/client/customize-store/data/homepageTemplates.ts
+++ plugins/woocommerce-admin/client/customize-store/data/homepageTemplates.ts
@@ -1,4 +1,3 @@
-// TODO: It might be better to create an API endpoint to get the templates.
 export const HOMEPAGE_TEMPLATES = {
 	template1: {
 		blocks: [
@@ -6,12 +5,12 @@ export const HOMEPAGE_TEMPLATES = {
 			'woocommerce-blocks/header-centered-menu-with-search',
 
 			// Body
-			'a8c/cover-image-with-left-aligned-call-to-action',
-			'woocommerce-blocks/featured-products-5-item-grid',
-			'woocommerce-blocks/featured-products-fresh-and-tasty',
+			'woocommerce-blocks/just-arrived-full-hero',
+			'woocommerce-blocks/product-collection-5-columns',
+			'woocommerce-blocks/hero-product-3-split',
+			'woocommerce-blocks/product-collection-3-columns',
+			'woocommerce-blocks/testimonials-3-columns',
 			'woocommerce-blocks/featured-category-triple',
-			'a8c/3-column-testimonials',
-			'a8c/quotes-2',
 			'woocommerce-blocks/social-follow-us-in-social-media',
 
 			// Footer
@@ -37,13 +36,11 @@ export const HOMEPAGE_TEMPLATES = {
 			'woocommerce-blocks/header-essential',
 
 			// Body
-			'woocommerce-blocks/hero-product-split',
-			'woocommerce-blocks/featured-products-fresh-and-tasty',
-			'woocommerce-blocks/featured-category-triple',
-			'woocommerce-blocks/featured-products-fresh-and-tasty',
-			'a8c/three-columns-with-images-and-text',
+			'woocommerce-blocks/featured-category-cover-image',
+			'woocommerce-blocks/product-collection-4-columns',
+			'woocommerce-blocks/hero-product-chessboard',
+			'woocommerce-blocks/product-collection-5-columns',
 			'woocommerce-blocks/testimonials-3-columns',
-			'a8c/subscription',
 
 			// Footer
 			'woocommerce-blocks/footer-large',
@@ -63,13 +60,10 @@ export const HOMEPAGE_TEMPLATES = {
 			'woocommerce-blocks/header-centered-menu-with-search',
 
 			// Body
-			'a8c/call-to-action-7',
-			'a8c/3-column-testimonials',
-			'woocommerce-blocks/featured-products-fresh-and-tasty',
 			'woocommerce-blocks/featured-category-cover-image',
-			'woocommerce-blocks/featured-products-5-item-grid',
-			'woocommerce-blocks/featured-products-5-item-grid',
-			'woocommerce-blocks/social-follow-us-in-social-media',
+			'woocommerce-blocks/product-collection-featured-products-5-columns',
+			'woocommerce-blocks/featured-category-triple',
+			'woocommerce-blocks/product-query-product-gallery',
 
 			// Footer
 			'woocommerce-blocks/footer-with-3-menus',
@@ -80,80 +74,6 @@ export const HOMEPAGE_TEMPLATES = {
 			audience: [ 'general' ],
 			design: [ 'contemporary' ],
 			features: [ 'small-banner', 'social-media' ],
-			complexity: 'high',
-		},
-	},
-	template4: {
-		blocks: [
-			// Header
-			'woocommerce-blocks/header-essential',
-
-			// Body
-			'woocommerce-blocks/featured-products-fresh-and-tasty',
-			'woocommerce-blocks/testimonials-single',
-			'woocommerce-blocks/hero-product-3-split',
-			'a8c/contact-8',
-
-			// Footer
-			'woocommerce-blocks/footer-simple-menu-and-cart', // This is supposed to be the "Footer with Newsletter Subscription Form"
-		],
-		metadata: {
-			businessType: [ 'e-commerce', 'small-medium-business' ],
-			contentFocus: [ 'products' ],
-			audience: [ 'focused' ],
-			design: [ 'sleek' ],
-			features: [ 'single-testimonial', 'contact', 'call-to-action' ],
-			complexity: 'medium',
-		},
-	},
-	template5: {
-		blocks: [
-			// Header
-			'woocommerce-blocks/header-essential',
-
-			// Body
-			'a8c/about-me-4',
-			'a8c/product-feature-with-buy-button',
-			'woocommerce-blocks/featured-products-fresh-and-tasty',
-			'a8c/subscription',
-			'woocommerce-blocks/testimonials-3-columns',
-			'a8c/contact-with-map-on-the-left',
-
-			// Footer
-			'woocommerce-blocks/footer-simple-menu-and-cart',
-		],
-		metadata: {
-			businessType: [ 'e-commerce', 'small-medium-business' ],
-			contentFocus: [ 'products', 'featured-product' ],
-			audience: [ 'focused' ],
-			design: [ 'sleek' ],
-			features: [ 'testimonial' ],
-			complexity: 'medium',
-		},
-	},
-	template6: {
-		blocks: [
-			// Header
-			'woocommerce-blocks/header-minimal',
-
-			// Body
-			'a8c/heading-and-video',
-			'a8c/3-column-testimonials',
-			'woocommerce-blocks/product-hero',
-			'a8c/quotes-2',
-			'a8c/product-feature-with-buy-button',
-			'a8c/simple-two-column-layout',
-			'woocommerce-blocks/social-follow-us-in-social-media',
-
-			// Footer
-			'woocommerce-blocks/footer-simple-menu-and-cart',
-		],
-		metadata: {
-			businessType: [ 'e-commerce', 'creative', 'small-medium-business' ],
-			contentFocus: [ 'services', 'storytelling', 'video' ],
-			audience: [ 'focused' ],
-			design: [ 'modern' ],
-			features: [ 'social-media', 'video' ],
 			complexity: 'high',
 		},
 	},
