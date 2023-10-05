5bd94f7b77d6ceb9841455e8863175b366a9fff8
Alba Rincón <albarin@users.noreply.github.com>
2023-10-05 17:44:14 +0200

    Store Customization > Homepage Template 1: Review layout and spacing in between patterns (https://github.com/woocommerce/woocommerce-blocks/pull/11144)
    * Add titles to patterns and set the aligment to Wide
    
    * Replace product query patterns with product collection ones
    
    * Add group and spacing to the 3-cols and social patterns
    
    * Fix alignment issue in the testimonals 3 in the editor
    
    * Add padding to the featured catergory triple pattern

--- plugins/woocommerce-blocks/patterns/testimonials-3-columns.php
+++ plugins/woocommerce-blocks/patterns/testimonials-3-columns.php
@@ -8,15 +8,14 @@
 use Automattic\WooCommerce\Blocks\Patterns\PatternsHelper;
 $content = PatternsHelper::get_pattern_content( 'woocommerce-blocks/testimonials-3-columns' );
 ?>
-
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
-	<!-- wp:heading {"level":3,"align":"wide"} -->
-	<h3 class="wp-block-heading alignwide"><?php echo esc_html( $content['titles'][3]['default'] ); ?></h3>
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)">
+	<!-- wp:heading {"level":3,"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+	<h3 class="wp-block-heading" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><?php echo esc_html( $content['titles'][3]['default'] ); ?></h3>
 	<!-- /wp:heading -->
 
-	<!-- wp:columns {"align":"full"} -->
-	<div class="wp-block-columns alignfull">
+	<!-- wp:columns {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+	<div class="wp-block-columns alignfull" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
 		<!-- wp:column -->
 		<div class="wp-block-column">
 			<!-- wp:paragraph -->
