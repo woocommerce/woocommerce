06ea7ae24aea86986891d9e805425fa43bae25d4
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-10-05 20:36:08 +0800

    [Customize your store] Fix site preview in transitional screen (#40588)
    * Replace mshot image with preview editor frame in transitional screen
    
    * Add changelog
    
    * Fix test
    
    * Use current xstate to check if it is transitional page

--- plugins/woocommerce-admin/client/customize-store/index.tsx
+++ plugins/woocommerce-admin/client/customize-store/index.tsx
@@ -25,11 +25,7 @@ import {
 } from './intro';
 import { DesignWithAi, events as designWithAiEvents } from './design-with-ai';
 import { AssemblerHub, events as assemblerHubEvents } from './assembler-hub';
-import {
-	Transitional,
-	events as transitionalEvents,
-	services as transitionalServices,
-} from './transitional';
+import { events as transitionalEvents } from './transitional';
 import { findComponentMeta } from '~/utils/xstate/find-component';
 import {
 	CustomizeStoreComponentMeta,
@@ -102,7 +98,6 @@ export const customizeStoreStateMachineActions = {
 
 export const customizeStoreStateMachineServices = {
 	...introServices,
-	...transitionalServices,
 	browserPopstateHandler,
 	markTaskComplete,
 };
@@ -264,14 +259,6 @@ export const customizeStoreStateMachineDefinition = createMachine( {
 					invoke: {
 						src: 'markTaskComplete',
 						onDone: {
-							target: 'waitForSitePreview',
-						},
-					},
-				},
-				waitForSitePreview: {
-					after: {
-						// Wait for 5 seconds before redirecting to the transitional page. This is to ensure that the site preview image is refreshed.
-						5000: {
 							target: '#customizeStore.transitionalScreen',
 						},
 					},
@@ -279,8 +266,6 @@ export const customizeStoreStateMachineDefinition = createMachine( {
 			},
 			on: {
 				FINISH_CUSTOMIZATION: {
-					// Pre-fetch the site preview image for the site for transitional page.
-					actions: [ 'prefetchSitePreview' ],
 					target: '.postAssemblerHub',
 				},
 				GO_BACK_TO_DESIGN_WITH_AI: {
@@ -291,7 +276,7 @@ export const customizeStoreStateMachineDefinition = createMachine( {
 		transitionalScreen: {
 			entry: [ { type: 'updateQueryStep', step: 'transitional' } ],
 			meta: {
-				component: Transitional,
+				component: AssemblerHub,
 			},
 			on: {
 				GO_BACK_TO_HOME: {
@@ -368,6 +353,7 @@ export const CustomizeStoreController = ( {
 						parentMachine={ service }
 						sendEvent={ send }
 						context={ state.context }
+						currentState={ state.value }
 					/>
 				) : (
 					<div />
