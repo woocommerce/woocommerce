da72d62d70cc2c907f93bd70e95b9a8d60d4ec5d
Tom Cafferkey <tjcafferkey@gmail.com>
2023-10-05 09:05:59 +0100

    Store Customization > Add default image to Just Arrived pattern (https://github.com/woocommerce/woocommerce-blocks/pull/11130)

--- plugins/woocommerce-blocks/patterns/just-arrived-full-hero.php
+++ plugins/woocommerce-blocks/patterns/just-arrived-full-hero.php
@@ -7,10 +7,15 @@
 
 use Automattic\WooCommerce\Blocks\Patterns\PatternsHelper;
 $content = PatternsHelper::get_pattern_content( 'woocommerce-blocks/just-arrived-full-hero' );
+$images  = PatternsHelper::get_pattern_images( 'woocommerce-blocks/just-arrived-full-hero' );
+
+$image1 = PatternsHelper::get_image_url( $images, 0, 'images/pattern-placeholders/plant-in-vase.jpg' );
 ?>
-<!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"contentPosition":"center right","isDark":false,"align":"wide","style":{"spacing":{"padding":{"right":"4em"}}}} -->
-<div class="wp-block-cover alignwide is-light has-custom-content-position is-position-center-right" style="padding-right:4em">
+
+<!-- wp:cover {"url":"<?php echo esc_url( $image1 ); ?>","dimRatio":0,"minHeight":739,"contentPosition":"center right","isDark":false,"align":"wide","style":{"spacing":{"padding":{"right":"4em"}}}} -->
+<div class="wp-block-cover alignwide is-light has-custom-content-position is-position-center-right" style="padding-right:4em;min-height:739px">
 	<span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+	<img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( $image1 ); ?>" data-object-fit="cover"/>
 	<div class="wp-block-cover__inner-container">
 		<!-- wp:group {"layout":{"type":"constrained"}} -->
 		<div class="wp-block-group">
@@ -26,7 +31,7 @@ $content = PatternsHelper::get_pattern_content( 'woocommerce-blocks/just-arrived
 			<div class="wp-block-buttons">
 				<!-- wp:button -->
 				<div class="wp-block-button">
-					<a class="wp-block-button__link wp-element-button"><?php echo esc_html( $content['buttons'][0]['default'] ); ?></a>
+					<a class="wp-block-button__link wp-element-button" href="<?php echo esc_url( get_permalink( wc_get_page_id( 'shop' ) ) ); ?>"><?php echo esc_html( $content['buttons'][0]['default'] ); ?></a>
 				</div>
 				<!-- /wp:button -->
 			</div>
