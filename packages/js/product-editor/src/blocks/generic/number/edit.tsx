129f11df9e37f04c330423ed6611be9e95f3e2bf
Nathan Silveira <nsschneider1@gmail.com>
2023-10-06 13:21:33 -0300

    Create woocommerce/product-number-field block (#40619)
    * Add initial structure for number block
    
    * Add more attributes and remove currenty formatting
    
    * Add changelog
    
    * Refactor
    
    * Add readme
    
    * Remove unused import
    
    * Add changelog
    
    * Use 'formatNumber' and 'parseNumber' functions from useProductHelper hook
    
    * Use 36px default size
    
    * Fix README

---
+++ packages/js/product-editor/src/blocks/generic/number/edit.tsx
@@ -0,0 +1,42 @@
+/**
+ * External dependencies
+ */
+import { createElement } from '@wordpress/element';
+import { useWooBlockProps } from '@woocommerce/block-templates';
+import {
+	// @ts-expect-error `__experimentalInputControl` does exist.
+	__experimentalInputControl as InputControl,
+} from '@wordpress/components';
+/**
+ * Internal dependencies
+ */
+import { ProductEditorBlockEditProps } from '../../../types';
+import useProductEntityProp from '../../../hooks/use-product-entity-prop';
+import { useNumberInputProps } from '../../../hooks/use-number-input-props';
+import { NumberBlockAttributes } from './types';
+
+export function Edit( {
+	attributes,
+}: ProductEditorBlockEditProps< NumberBlockAttributes > ) {
+	const blockProps = useWooBlockProps( attributes );
+	const { label, property, suffix, placeholder, help } = attributes;
+	const [ value, setValue ] = useProductEntityProp( property );
+
+	const inputProps = useNumberInputProps( {
+		value,
+		onChange: setValue,
+	} );
+
+	return (
+		<div { ...blockProps }>
+			<InputControl
+				{ ...inputProps }
+				__next36pxDefaultSize
+				label={ label }
+				suffix={ suffix }
+				help={ help }
+				placeholder={ placeholder }
+			/>
+		</div>
+	);
+}
