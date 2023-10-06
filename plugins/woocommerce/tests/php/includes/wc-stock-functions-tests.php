eaad1dc510bca90f384c5fca79287c8a9af8ef6c
Sagar Tamang <mi5t4n@gmail.com>
2023-10-06 19:34:43 +0545

    fix - phpcs errors

--- plugins/woocommerce/tests/php/includes/wc-stock-functions-tests.php
+++ plugins/woocommerce/tests/php/includes/wc-stock-functions-tests.php
@@ -363,9 +363,12 @@ class WC_Stock_Functions_Tests extends \WC_Unit_Test_Case {
 	public function test_wc_get_default_stock_amount() {
 		$this->assertIsIntAndEquals( 1, wc_get_default_stock_amount() );
 
-		add_filter( 'woocommerce_default_stock_amount', function( $amount ) {
-			return '200abc';
-		});
+		add_filter(
+			'woocommerce_default_stock_amount',
+			function( $amount ) {
+				return '200abc';
+			}
+		);
 
 		$this->assertIsIntAndEquals( 200, wc_get_default_stock_amount() );
 	}
