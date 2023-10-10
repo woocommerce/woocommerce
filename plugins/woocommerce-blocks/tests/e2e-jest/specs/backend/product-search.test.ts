27ae9acafc1cfd78e47bba1c200a82f2d6b1334b
Mike Jolley <mike.jolley@me.com>
2023-10-10 11:57:26 +0100

    Remove all instances of GUTENBERG_EDITOR_CONTEXT (https://github.com/woocommerce/woocommerce-blocks/pull/11126)

--- plugins/woocommerce-blocks/tests/e2e-jest/specs/backend/product-search.test.ts
+++ plugins/woocommerce-blocks/tests/e2e-jest/specs/backend/product-search.test.ts
@@ -7,40 +7,33 @@ import {
 	insertBlock,
 } from '@wordpress/e2e-test-utils';
 
-/**
- * Internal dependencies
- */
-import { GUTENBERG_EDITOR_CONTEXT, describeOrSkip } from '../../utils';
-
 const block = {
 	name: 'Product Search',
 	slug: 'core/search',
 	class: '.wp-block-search',
 };
 
-describeOrSkip( GUTENBERG_EDITOR_CONTEXT === 'gutenberg' )(
-	`${ block.name } Block`,
-	() => {
-		it( 'inserting Product Search block renders the core/search variation', async () => {
-			await switchUserToAdmin();
+// These tests are skipped and previously relied on GUTENBERG_EDITOR_CONTEXT.
+describe.skip( `${ block.name } Block`, () => {
+	it( 'inserting Product Search block renders the core/search variation', async () => {
+		await switchUserToAdmin();
 
-			await createNewPost( {
-				postType: 'page',
-			} );
-
-			await insertBlock( block.name );
-
-			await page.waitForSelector( block.class );
-
-			await expect( page ).toRenderBlock( block );
-
-			await expect( page ).toMatchElement( '.wp-block-search__label', {
-				text: 'Search',
-			} );
-
-			await expect( page ).toMatchElement(
-				'.wp-block-search__input[value="Search products…"]'
-			);
+		await createNewPost( {
+			postType: 'page',
 		} );
-	}
-);
+
+		await insertBlock( block.name );
+
+		await page.waitForSelector( block.class );
+
+		await expect( page ).toRenderBlock( block );
+
+		await expect( page ).toMatchElement( '.wp-block-search__label', {
+			text: 'Search',
+		} );
+
+		await expect( page ).toMatchElement(
+			'.wp-block-search__input[value="Search products…"]'
+		);
+	} );
+} );
