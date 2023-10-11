e52e48a7b5623520e1c9aef627709942cdc20bfb
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-10-11 14:51:34 -0300

    Create downloads menu component

---
+++ packages/js/product-editor/src/blocks/product-fields/downloads/downloads-menu/downloads-menu.tsx
@@ -0,0 +1,79 @@
+/**
+ * External dependencies
+ */
+import { Button, Dropdown, MenuGroup, MenuItem } from '@wordpress/components';
+import { createElement } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import {
+	chevronDown,
+	chevronUp,
+	customLink,
+	media,
+	upload,
+} from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import { DownloadsMenuProps } from './types';
+
+export function DownloadsMenu( {}: DownloadsMenuProps ) {
+	return (
+		<Dropdown
+			position="bottom left"
+			contentClassName="woocommerce-downloads-menu__menu-content"
+			renderToggle={ ( { isOpen, onToggle } ) => (
+				<Button
+					aria-expanded={ isOpen }
+					icon={ isOpen ? chevronUp : chevronDown }
+					variant="secondary"
+					onClick={ onToggle }
+					className="woocommerce-downloads-menu__toogle"
+				>
+					<span>{ __( 'Add new', 'woocommerce' ) }</span>
+				</Button>
+			) }
+			renderContent={ ( { onClose } ) => (
+				<div className="components-dropdown-menu__menu">
+					<MenuGroup>
+						<MenuItem
+							icon={ upload }
+							iconPosition="left"
+							onClick={ onClose }
+							info={ __(
+								'Select files from your device',
+								'woocommerce'
+							) }
+						>
+							{ __( 'Upload', 'woocommerce' ) }
+						</MenuItem>
+
+						<MenuItem
+							icon={ media }
+							iconPosition="left"
+							onClick={ onClose }
+							info={ __(
+								'Choose from uploaded media',
+								'woocommerce'
+							) }
+						>
+							{ __( 'Media Library', 'woocommerce' ) }
+						</MenuItem>
+
+						<MenuItem
+							icon={ customLink }
+							iconPosition="left"
+							onClick={ onClose }
+							info={ __(
+								'Import a file hosted elsewhere',
+								'woocommerce'
+							) }
+						>
+							{ __( 'Insert from URL', 'woocommerce' ) }
+						</MenuItem>
+					</MenuGroup>
+				</div>
+			) }
+		/>
+	);
+}
