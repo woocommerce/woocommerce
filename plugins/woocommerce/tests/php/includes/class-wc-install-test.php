62b29f0a3f0eb89ee28e7a7f681377c60c77de52
abelsonlive <brian@abelson.live>
2023-10-11 14:49:49 -0400

    linting fixes

--- plugins/woocommerce/tests/php/includes/class-wc-install-test.php
+++ plugins/woocommerce/tests/php/includes/class-wc-install-test.php
@@ -169,17 +169,17 @@ class WC_Install_Test extends \WC_Unit_Test_Case {
 	 */
 	public function test_maybe_set_store_id() {
 
-		// simulate a store ID not being set
+		// simulate a store ID not being set.
 		delete_option( \WC_Install::STORE_ID_OPTION );
 		$store_id = \WC_Install::maybe_set_store_id();
 		$this->assertSame( $store_id, get_option( \WC_Install::STORE_ID_OPTION ) );
-		// uuid4 is 36 characters long
+		// uuid4 is 36 characters long.
 		$this->assertSame( 36, strlen( $store_id ) );
 
 		// simulate a store ID already being set.
 		$existing_store_id = \WC_Install::maybe_set_store_id();
 		$this->assertSame( $store_id, $existing_store_id );
-		// cleanup
+		// cleanup.
 		delete_option( \WC_Install::STORE_ID_OPTION );
 	}
 }
