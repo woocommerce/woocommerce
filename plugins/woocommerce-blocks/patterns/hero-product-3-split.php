2416f2b665f0c957fdac7c4f03bd6633d706d01a
Alba Rincón <albarin@users.noreply.github.com>
2023-10-12 09:06:33 +0200

    Make patterns full width and fix margin (https://github.com/woocommerce/woocommerce-blocks/pull/11206)
    * Make patterns full width and fix margin
    
    * Fix horizontal spacing

--- plugins/woocommerce-blocks/patterns/hero-product-3-split.php
+++ plugins/woocommerce-blocks/patterns/hero-product-3-split.php
@@ -10,8 +10,8 @@ $content = PatternsHelper::get_pattern_content( 'woocommerce-blocks/hero-product
 $images  = PatternsHelper::get_pattern_images( 'woocommerce-blocks/hero-product-3-split' );
 ?>
 
-<!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-<div class="wp-block-columns alignwide" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+<!-- wp:columns {"align":"full","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-columns alignfull" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
 	<!-- wp:column {"width":"66.66%"} -->
 	<div class="wp-block-column" style="flex-basis:66.66%">
 		<!-- wp:media-text {"mediaPosition":"right","mediaId":1,"mediaLink":"<?php echo esc_url( plugins_url( 'images/pattern-placeholders/hand-guitar-finger-tshirt-clothing-rack.png', dirname( __FILE__ ) ) ); ?>","mediaType":"image"} -->
@@ -47,8 +47,8 @@ $images  = PatternsHelper::get_pattern_images( 'woocommerce-blocks/hero-product-
 	</div>
 	<!-- /wp:column -->
 
-	<!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
-	<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%">
+	<!-- wp:column {"verticalAlignment":"center","width":"30%","style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+	<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:30%">
 		<!-- wp:paragraph -->
 		<p><strong><?php echo esc_html( $content['titles'][1]['default'] ); ?></strong></p>
 		<!-- /wp:paragraph -->
