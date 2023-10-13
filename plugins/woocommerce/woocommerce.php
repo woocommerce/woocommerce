4c463011633156e37b276069027f49917508ad65
Jonathan Sadowski <sadowski@automattic.com>
2023-10-13 17:34:50 -0500

    Yep  again

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -49,13 +49,13 @@ function WC() { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName.Fu
 
 /**
  * Returns the WooCommerce object container.
- * Code in the `includes` directory should use the container to get instances of classes in the `src` directory.
+ * Code in the `includes` directories should use the container to get instances of classes in the `src` directory.
  *
  * @since  4.4.0
  * @return \Automattic\WooCommerce\Container The WooCommerce object container.
  */
 function wc_get_container() {
-	return $GLOBALS['wc_container'] || false;
+	return $GLOBALS['wc_container'];
 }
 
 // Global for backwards compatibility.
