88a3a7a098a5d7615217c356f7d6723048e376e7
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-10-13 12:54:48 -0300

    Keep drop zone on to of the table

--- packages/js/product-editor/src/blocks/product-fields/downloads/edit.tsx
+++ packages/js/product-editor/src/blocks/product-fields/downloads/edit.tsx
@@ -123,79 +123,43 @@ export function Edit( {
 				/>
 			</div>
 
-			{ Boolean( downloads.length ) ? (
-				<Sortable className="wp-block-woocommerce-product-downloads-field__table">
-					{ downloads.map( ( download ) => {
-						const nameFromUrl = getFileName( download.file );
-						const isUploading = download.file.startsWith( 'blob' );
-
-						return (
-							<ListItem key={ download.file }>
-								<div className="wp-block-woocommerce-product-downloads-field__table-filename">
-									<span>{ download.name }</span>
-									{ download.name !== nameFromUrl && (
-										<span className="wp-block-woocommerce-product-downloads-field__table-filename-description">
-											{ nameFromUrl }
-										</span>
-									) }
-								</div>
-
-								<div className="wp-block-woocommerce-product-downloads-field__table-actions">
-									{ isUploading && (
-										<Spinner
-											aria-label={ __(
-												'Uploading file',
-												'woocommerce'
-											) }
-										/>
-									) }
-									<Button
-										icon={ closeSmall }
-										label={ __(
-											'Remove file',
-											'woocommerce'
-										) }
-										disabled={ isUploading }
-										onClick={ removeHandler( download ) }
-									/>
-								</div>
-							</ListItem>
-						);
-					} ) }
-				</Sortable>
-			) : (
+			<div className="wp-block-woocommerce-product-downloads-field__body">
 				<MediaUploader
 					label={
-						<>
-							<UploadImage />
-							<p className="woocommerce-product-form__supported-files-drop-zone-label">
-								{ createInterpolateElement(
-									__(
-										'Supported file types: <Types /> and more. <link>View all</link>',
-										'woocommerce'
-									),
-									{
-										Types: (
-											<Fragment>
-												PNG, JPG, PDF, PPT, DOC, MP3,
-												MP4
-											</Fragment>
+						! Boolean( downloads.length ) ? (
+							<div className="wp-block-woocommerce-product-downloads-field__drop-zone-content">
+								<UploadImage />
+								<p className="wp-block-woocommerce-product-downloads-field__drop-zone-label">
+									{ createInterpolateElement(
+										__(
+											'Supported file types: <Types /> and more. <link>View all</link>',
+											'woocommerce'
 										),
-										link: (
-											// eslint-disable-next-line jsx-a11y/anchor-has-content
-											<a
-												href="https://codex.wordpress.org/Uploading_Files"
-												target="_blank"
-												rel="noreferrer"
-												onClick={ ( event ) =>
-													event.stopPropagation()
-												}
-											/>
-										),
-									}
-								) }
-							</p>
-						</>
+										{
+											Types: (
+												<Fragment>
+													PNG, JPG, PDF, PPT, DOC,
+													MP3, MP4
+												</Fragment>
+											),
+											link: (
+												// eslint-disable-next-line jsx-a11y/anchor-has-content
+												<a
+													href="https://codex.wordpress.org/Uploading_Files"
+													target="_blank"
+													rel="noreferrer"
+													onClick={ ( event ) =>
+														event.stopPropagation()
+													}
+												/>
+											),
+										}
+									) }
+								</p>
+							</div>
+						) : (
+							''
+						)
 					}
 					buttonText=""
 					allowedMediaTypes={ allowedTypes }
@@ -204,7 +168,52 @@ export function Edit( {
 					onFileUploadChange={ handleFileUpload }
 					onError={ handleUploadError }
 				/>
-			) }
+
+				{ Boolean( downloads.length ) && (
+					<Sortable className="wp-block-woocommerce-product-downloads-field__table">
+						{ downloads.map( ( download ) => {
+							const nameFromUrl = getFileName( download.file );
+							const isUploading =
+								download.file.startsWith( 'blob' );
+
+							return (
+								<ListItem key={ download.file }>
+									<div className="wp-block-woocommerce-product-downloads-field__table-filename">
+										<span>{ download.name }</span>
+										{ download.name !== nameFromUrl && (
+											<span className="wp-block-woocommerce-product-downloads-field__table-filename-description">
+												{ nameFromUrl }
+											</span>
+										) }
+									</div>
+
+									<div className="wp-block-woocommerce-product-downloads-field__table-actions">
+										{ isUploading && (
+											<Spinner
+												aria-label={ __(
+													'Uploading file',
+													'woocommerce'
+												) }
+											/>
+										) }
+										<Button
+											icon={ closeSmall }
+											label={ __(
+												'Remove file',
+												'woocommerce'
+											) }
+											disabled={ isUploading }
+											onClick={ removeHandler(
+												download
+											) }
+										/>
+									</div>
+								</ListItem>
+							);
+						} ) }
+					</Sortable>
+				) }
+			</div>
 		</div>
 	);
 }
