a4002105a4dfe30e93e316f6a9561f57c4ff4435
Matt Sherman <matt@jam123.com>
2023-10-17 10:18:56 -0400

    Refactor client-side attribute augmentation

--- packages/js/block-templates/src/utils/register-woo-block-type.ts
+++ packages/js/block-templates/src/utils/register-woo-block-type.ts
@@ -63,6 +63,31 @@ function getEdit<
 	};
 }
 
+function augmentAttributes<
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	T extends Record< string, any > = Record< string, any >
+>( attributes: T ) {
+	// Note: If you modify this function, also update the server-side
+	// Automattic\WooCommerce\Admin\Features\ProductBlockEditor\BlockRegistry::augment_attributes() function.
+	return {
+		...attributes,
+		...{
+			_templateBlockId: {
+				type: 'string',
+				__experimentalRole: 'content',
+			},
+			_templateBlockOrder: {
+				type: 'integer',
+				__experimentalRole: 'content',
+			},
+			_templateBlockHideConditions: {
+				type: 'array',
+				__experimentalRole: 'content',
+			},
+		},
+	};
+}
+
 /**
  * Function to register an individual block.
  *
@@ -86,27 +111,9 @@ export function registerWooBlockType<
 		return;
 	}
 
-	const templateBlockAttributes = {
-		_templateBlockId: {
-			type: 'string',
-			__experimentalRole: 'content',
-		},
-		_templateBlockOrder: {
-			type: 'integer',
-			__experimentalRole: 'content',
-		},
-		_templateBlockHideConditions: {
-			type: 'array',
-			__experimentalRole: 'content',
-		},
-	};
-
 	const augmentedMetadata = {
 		...metadata,
-		attributes: {
-			...metadata.attributes,
-			...templateBlockAttributes,
-		},
+		attributes: augmentAttributes( metadata.attributes ),
 	};
 
 	return registerBlockType< T >(
