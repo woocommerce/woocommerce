4d4d7c0cd091af8da302ab8873bef091e68779a3
Alba Rincón <albarin@users.noreply.github.com>
2023-10-17 14:50:19 +0200

    [Store Customization] Fix the "Testimonials 3 Columns" title alignment (https://github.com/woocommerce/woocommerce-blocks/pull/11281)

--- plugins/woocommerce-blocks/patterns/testimonials-3-columns.php
+++ plugins/woocommerce-blocks/patterns/testimonials-3-columns.php
@@ -20,8 +20,8 @@ $third_description  = $content['descriptions'][2]['default'] ?? '';
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"0px","bottom":"80px"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
 <div class="wp-block-group alignwide" style="margin-top:0px;margin-bottom:80px">
-	<!-- wp:heading {"level":3,"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
-	<h3 class="wp-block-heading" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><?php echo esc_html( $main_header ); ?></h3>
+	<!-- wp:heading {"level":3,"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+	<h3 class="wp-block-heading alignfull" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><?php echo esc_html( $main_header ); ?></h3>
 	<!-- /wp:heading -->
 
 	<!-- wp:columns {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
