6b6ffd184e93b687a4062e0a67100bf3a4039f13
Matt Sherman <matt@jam123.com>
2023-10-18 18:44:32 -0400

    Remove unused imports

--- packages/js/product-editor/src/components/block-editor/block-editor.tsx
+++ packages/js/product-editor/src/components/block-editor/block-editor.tsx
@@ -3,7 +3,6 @@
  */
 import { synchronizeBlocksWithTemplate, Template } from '@wordpress/blocks';
 import { createElement, useMemo, useLayoutEffect } from '@wordpress/element';
-import { Product } from '@woocommerce/data';
 import { useDispatch, useSelect, select as WPSelect } from '@wordpress/data';
 import { uploadMedia } from '@wordpress/media-utils';
 import {
