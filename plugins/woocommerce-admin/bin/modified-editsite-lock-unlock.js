dd3e373f90cf052ba9de19adcae4176907b3a6d6
moon <moon.kyong@automattic.com>
2023-10-19 17:14:40 -0700

    Update version compare logic to include higher versions

--- plugins/woocommerce-admin/bin/modified-editsite-lock-unlock.js
+++ plugins/woocommerce-admin/bin/modified-editsite-lock-unlock.js
@@ -2,9 +2,14 @@
  * WordPress dependencies
  */
 var consentString = 'I know using unstable features means my plugin or theme will inevitably break on the next WordPress release.';
-if (window.wcSettings.wpVersion.indexOf('6.4') === 0) {
+
+if (window.wcSettings && window.wcSettings.wpVersion) {
+  // Parse the version string as a floating-point number
+  const wpVersion = parseFloat(window.wcSettings.wpVersion);
+  if ( ! isNaN( wpVersion ) && wpVersion >= 6.4 ) {
 	consentString = 'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.';
-} 
+  } 
+}
 
 import { __dangerousOptInToUnstableAPIsOnlyForCoreModules } from '@wordpress/private-apis';
 export const {
