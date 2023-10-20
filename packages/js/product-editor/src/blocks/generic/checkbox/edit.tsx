3d182f342f7bde441ca3c4df2bb4edb6d1d23d19
Nathan Silveira <nsschneider1@gmail.com>
2023-10-20 10:52:56 -0300

    Create React component wrappers for the checkbox and number blocks (#40828)
    * Add @woocommerce/number to @woocommerce/components
    
    * Create a component for checkbox and use it on the block
    
    * Create a component for number and use it on the block
    
    * Add changelogs
    
    * Remove scss import
    
    * Rename Numbercontrol prop
    
    * Expose useProductEntityProp
    
    * Move components to product-editor package and fix UI bug created in checkbox component
    
    * Revert unnecessary changes in components and update changelogs
    
    * Revert changes in use-number-input-props
    
    * Fix issue in checkbox
    
    * Revert css change

--- packages/js/product-editor/src/blocks/generic/checkbox/edit.tsx
+++ packages/js/product-editor/src/blocks/generic/checkbox/edit.tsx
@@ -2,8 +2,6 @@
  * External dependencies
  */
 import { createElement } from '@wordpress/element';
-import { CheckboxControl, Tooltip } from '@wordpress/components';
-import { Icon, help } from '@wordpress/icons';
 import { useWooBlockProps } from '@woocommerce/block-templates';
 
 /**
@@ -12,6 +10,7 @@ import { useWooBlockProps } from '@woocommerce/block-templates';
 import { ProductEditorBlockEditProps } from '../../../types';
 import useProductEntityProp from '../../../hooks/use-product-entity-prop';
 import { CheckboxBlockAttributes } from './types';
+import { Checkbox } from '../../../components/checkbox-control';
 
 export function Edit( {
 	attributes,
@@ -20,10 +19,7 @@ export function Edit( {
 	const { property, title, label, tooltip, checkedValue, uncheckedValue } =
 		attributes;
 
-	const blockProps = useWooBlockProps( {
-		className: 'woocommerce-product-form__checkbox',
-		...attributes,
-	} );
+	const blockProps = useWooBlockProps( attributes );
 
 	const [ value, setValue ] = useProductEntityProp< boolean | string | null >(
 		property,
@@ -33,47 +29,17 @@ export function Edit( {
 		}
 	);
 
-	function isChecked() {
-		if ( checkedValue !== undefined ) {
-			return checkedValue === value;
-		}
-		return value as boolean;
-	}
-
-	function handleChange( checked: boolean ) {
-		if ( checked ) {
-			setValue( checkedValue !== undefined ? checkedValue : checked );
-		} else {
-			setValue( uncheckedValue !== undefined ? uncheckedValue : checked );
-		}
-	}
-
 	return (
 		<div { ...blockProps }>
-			{ title && <h4>{ title }</h4> }
-			<div className="woocommerce-product-form__checkbox-wrapper">
-				<CheckboxControl
-					label={ label }
-					checked={ isChecked() }
-					onChange={ handleChange }
-				/>
-				{ tooltip && (
-					<Tooltip
-						text={ <span>{ tooltip }</span> }
-						position="top center"
-						// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-						// @ts-ignore Incorrect types.
-						className={
-							'woocommerce-product-form__checkbox-tooltip'
-						}
-						delay={ 0 }
-					>
-						<span className="woocommerce-product-form__checkbox-tooltip-icon">
-							<Icon icon={ help } size={ 21.94 } fill="#949494" />
-						</span>
-					</Tooltip>
-				) }
-			</div>
+			<Checkbox
+				value={ Boolean( value ) }
+				onChange={ setValue }
+				label={ label || '' }
+				title={ title }
+				tooltip={ tooltip }
+				checkedValue={ checkedValue }
+				uncheckedValue={ uncheckedValue }
+			/>
 		</div>
 	);
 }
