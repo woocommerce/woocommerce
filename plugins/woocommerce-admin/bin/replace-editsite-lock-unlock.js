45c70f317a6e2bb396566e4be8efb8536ce6c2a9
moon <moon.kyong@automattic.com>
2023-10-23 10:54:41 -0700

    Use webpack to replace modified version lock lock-unlock

--- plugins/woocommerce-admin/bin/replace-editsite-lock-unlock.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require("child_process");
-
-// Find edit-site module directory
-const EDIT_SITE_DIR = execSync("find ../../node_modules/.pnpm -type d -name '@wordpress+edit-site@5.15.0*' -print -quit").toString().trim();
-
-// Replace it with modified version
-execSync(`cp -rf "./bin/modified-editsite-lock-unlock.js" ${EDIT_SITE_DIR}/node_modules/@wordpress/edit-site/build-module/lock-unlock.js`);
