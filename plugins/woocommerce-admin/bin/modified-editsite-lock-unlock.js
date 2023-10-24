2d3756a82ccffbde6a71644947124c86bc075a47
moon <moon.kyong@automattic.com>
2023-10-23 17:18:37 -0700

    Lint fixes

--- plugins/woocommerce-admin/bin/modified-editsite-lock-unlock.js
+++ plugins/woocommerce-admin/bin/modified-editsite-lock-unlock.js
@@ -1,19 +1,23 @@
 /**
  * WordPress dependencies
  */
-var consentString = 'I know using unstable features means my plugin or theme will inevitably break on the next WordPress release.';
+import { __dangerousOptInToUnstableAPIsOnlyForCoreModules } from '@wordpress/private-apis';
 
-if (window.wcSettings && window.wcSettings.wpVersion) {
-  // Parse the version string as a floating-point number
-  const wpVersion = parseFloat(window.wcSettings.wpVersion);
-  if ( ! isNaN( wpVersion ) && wpVersion >= 6.4 ) {
-	consentString = 'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.';
-  } 
+let consentString =
+	'I know using unstable features means my plugin or theme will inevitably break on the next WordPress release.';
+
+if ( window.wcSettings && window.wcSettings.wpVersion ) {
+	// Parse the version string as a floating-point number
+	const wpVersion = parseFloat( window.wcSettings.wpVersion );
+	if ( ! isNaN( wpVersion ) && wpVersion >= 6.4 ) {
+		consentString =
+			'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.';
+	}
 }
 
-import { __dangerousOptInToUnstableAPIsOnlyForCoreModules } from '@wordpress/private-apis';
-export const {
-  lock,
-  unlock
-} = __dangerousOptInToUnstableAPIsOnlyForCoreModules(consentString, '@wordpress/edit-site');
+export const { lock, unlock } =
+	__dangerousOptInToUnstableAPIsOnlyForCoreModules(
+		consentString,
+		'@wordpress/edit-site'
+	);
 //# sourceMappingURL=lock-unlock.js.map
