b0ce2b57237b9f9dd611edfc2913d720a3418d23
Matt Sherman <matt@jam123.com>
2023-10-26 14:16:53 -0400

    Hook up menu

--- plugins/woocommerce-beta-tester/src/product-editor-dev-tools/product-editor-dev-tools-menu.tsx
+++ plugins/woocommerce-beta-tester/src/product-editor-dev-tools/product-editor-dev-tools-menu.tsx
@@ -2,20 +2,33 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { MenuGroup } from '@wordpress/components';
+import { MenuGroup, MenuItem } from '@wordpress/components';
+import { cog, Icon } from '@wordpress/icons';
 import { __experimentalWooProductMoreMenuItem as WooProductMoreMenuItem } from '@woocommerce/product-editor';
 
-/**
- * Internal dependencies
- */
-import { BlockInspectorMenuItem } from './block-inspector-menu-item';
-
-export function ProductEditorDevToolsMenu() {
+export function ProductEditorDevToolsMenu( {
+	shouldShowDevTools,
+	onToggleShowDevTools,
+}: {
+	shouldShowDevTools: boolean;
+	onToggleShowDevTools: () => void;
+} ) {
 	return (
 		<WooProductMoreMenuItem>
 			{ ( { onClose }: { onClose: () => void } ) => (
-				<MenuGroup label={ __( 'Development tools', 'woocommerce' ) }>
-					<BlockInspectorMenuItem onClick={ onClose } />
+				<MenuGroup label={ __( 'Developer tools', 'woocommerce' ) }>
+					<MenuItem
+						icon={ <Icon icon={ cog } /> }
+						iconPosition="right"
+						onClick={ () => {
+							onToggleShowDevTools();
+							onClose();
+						} }
+					>
+						{ shouldShowDevTools
+							? __( 'Hide developer tools', 'woocommerce' )
+							: __( 'Show developer tools', 'woocommerce' ) }
+					</MenuItem>
 				</MenuGroup>
 			) }
 		</WooProductMoreMenuItem>
--- plugins/woocommerce-beta-tester/src/product-editor-dev-tools/product-editor-dev-tools.tsx
+++ plugins/woocommerce-beta-tester/src/product-editor-dev-tools/product-editor-dev-tools.tsx
@@ -1,15 +1,28 @@
 /**
+ * External dependencies
+ */
+import { useState } from 'react';
+
+/**
  * Internal dependencies
  */
 import { BlockInspector } from './block-inspector';
 import { ProductEditorDevToolsMenu } from './product-editor-dev-tools-menu';
 
 export function ProductEditorDevTools() {
+	const [ shouldShowDevTools, setShouldShowDevTools ] =
+		useState< boolean >( false );
+
 	return (
 		<>
-			<ProductEditorDevToolsMenu />
+			<ProductEditorDevToolsMenu
+				shouldShowDevTools={ shouldShowDevTools }
+				onToggleShowDevTools={ () => {
+					setShouldShowDevTools( ! shouldShowDevTools );
+				} }
+			/>
 
-			<BlockInspector />
+			{ shouldShowDevTools && <BlockInspector /> }
 		</>
 	);
 }
