2975c04269906dac039a0c3e645d11b5daac91cc
Matt Sherman <matt@jam123.com>
2023-10-27 11:53:24 -0400

    Tab buttons styling

--- plugins/woocommerce-beta-tester/src/product-editor-dev-tools/product-editor-dev-tools-bar.tsx
+++ plugins/woocommerce-beta-tester/src/product-editor-dev-tools/product-editor-dev-tools-bar.tsx
@@ -5,13 +5,35 @@ import { useState } from 'react';
 import { WooFooterItem } from '@woocommerce/admin-layout';
 import { Button, NavigableMenu } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { close } from '@wordpress/icons';
+import { closeSmall } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import { BlockInspector } from './block-inspector';
 import { useFocusedBlock } from './hooks/use-focused-block';
+
+function TabButton( {
+	name,
+	selectedTab,
+	onClick,
+	children,
+}: {
+	name: string;
+	selectedTab: string;
+	onClick: ( name: string ) => void;
+	children: React.ReactNode;
+} ) {
+	return (
+		<Button
+			onClick={ () => onClick( name ) }
+			aria-selected={ name === selectedTab }
+			className="woocommerce-product-editor-dev-tools-bar__tab-button"
+		>
+			{ children }
+		</Button>
+	);
+}
 
 export function ProductEditorDevToolsBar( {
 	onClose,
@@ -40,19 +62,25 @@ export function ProductEditorDevToolsBar( {
 							orientation="horizontal"
 							onNavigate={ handleNavigate }
 						>
-							<Button
-								onClick={ () => handleTabClick( 'inspector' ) }
+							<TabButton
+								name="inspector"
+								selectedTab={ selectedTab }
+								onClick={ handleTabClick }
 							>
-								Block Inspector
-							</Button>
-							<Button onClick={ () => handleTabClick( 'about' ) }>
-								About
-							</Button>
+								{ __( 'Block Inspector', 'woocommerce' ) }
+							</TabButton>
+							<TabButton
+								name="about"
+								selectedTab={ selectedTab }
+								onClick={ handleTabClick }
+							>
+								{ __( 'About', 'woocommerce' ) }
+							</TabButton>
 						</NavigableMenu>
 					</div>
 					<div className="woocommerce-product-editor-dev-tools-bar__actions">
 						<Button
-							icon={ close }
+							icon={ closeSmall }
 							label={ __( 'Close', 'woocommerce' ) }
 							onClick={ onClose }
 						/>
