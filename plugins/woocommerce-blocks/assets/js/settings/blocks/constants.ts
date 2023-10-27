1a4968fa6609c82528fddcc308aa835262eb3eaf
Thomas Roberts <5656702+opr@users.noreply.github.com>
2023-10-27 10:05:43 +0100

    Fix for storybook to ensure all components render (https://github.com/woocommerce/woocommerce-blocks/pull/11465)

--- plugins/woocommerce-blocks/assets/js/settings/blocks/constants.ts
+++ plugins/woocommerce-blocks/assets/js/settings/blocks/constants.ts
@@ -34,15 +34,15 @@ export const WC_BLOCKS_IMAGE_URL = blocksConfig.pluginUrl + 'images/';
 export const WC_BLOCKS_BUILD_URL = blocksConfig.pluginUrl + 'build/';
 export const WC_BLOCKS_PHASE = blocksConfig.buildPhase;
 export const SHOP_URL = STORE_PAGES.shop?.permalink;
-export const CHECKOUT_PAGE_ID = STORE_PAGES.checkout.id;
-export const CHECKOUT_URL = STORE_PAGES.checkout.permalink;
-export const PRIVACY_URL = STORE_PAGES.privacy.permalink;
-export const PRIVACY_PAGE_NAME = STORE_PAGES.privacy.title;
-export const TERMS_URL = STORE_PAGES.terms.permalink;
-export const TERMS_PAGE_NAME = STORE_PAGES.terms.title;
-export const CART_PAGE_ID = STORE_PAGES.cart.id;
-export const CART_URL = STORE_PAGES.cart.permalink;
-export const LOGIN_URL = STORE_PAGES.myaccount.permalink
+export const CHECKOUT_PAGE_ID = STORE_PAGES.checkout?.id;
+export const CHECKOUT_URL = STORE_PAGES.checkout?.permalink;
+export const PRIVACY_URL = STORE_PAGES.privacy?.permalink;
+export const PRIVACY_PAGE_NAME = STORE_PAGES.privacy?.title;
+export const TERMS_URL = STORE_PAGES.terms?.permalink;
+export const TERMS_PAGE_NAME = STORE_PAGES.terms?.title;
+export const CART_PAGE_ID = STORE_PAGES.cart?.id;
+export const CART_URL = STORE_PAGES.cart?.permalink;
+export const LOGIN_URL = STORE_PAGES.myaccount?.permalink
 	? STORE_PAGES.myaccount.permalink
 	: getSetting( 'wpLoginUrl', '/wp-login.php' );
 export const LOCAL_PICKUP_ENABLED = getSetting< boolean >(
