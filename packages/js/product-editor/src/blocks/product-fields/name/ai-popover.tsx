f776203601333fe79f5269a16009ac9985cb75ce
Nathan Schneider <nsschneider1@gmail.com>
2023-10-27 10:17:54 -0300

    Add more buttons

--- packages/js/product-editor/src/blocks/product-fields/name/ai-popover.tsx
+++ packages/js/product-editor/src/blocks/product-fields/name/ai-popover.tsx
@@ -1,11 +1,20 @@
 /**
  * External dependencies
  */
-import { createElement, useState, useLayoutEffect } from '@wordpress/element';
+import {
+	createElement,
+	Fragment,
+	useState,
+	useLayoutEffect,
+} from '@wordpress/element';
 
 import classNames from 'classnames';
 import { Button, Popover } from '@wordpress/components';
 import { UseComboboxPropGetters } from 'downshift';
+
+/**
+ * Internal dependencies
+ */
 import { ThumbsUpSVG } from './thumbs-up';
 
 type Suggestions = string;
@@ -32,33 +41,46 @@ export function AIPopover( {
 		}
 	}, [ widthRef.current, widthRef.current?.clientWidth ] );
 	return (
-		<Popover>
-			<ul style={ { width: boundingRect?.width } } { ...getMenuProps() }>
-				{ items.map( ( item, index ) => (
-					<li
-						key={ item }
-						className={ classNames( {
-							'woocommerce-product-form-name-ai-suggestions__highlighted':
-								highlightedIndex === index,
-						} ) }
-						{ ...getItemProps( { item, index } ) }
-					>
-						<div className="woocommerce-product-form-name-ai-suggestions__item">
-							<div>
-								<p className="woocommerce-product-form-name-ai-suggestions__item-name">
-									{ item }
-								</p>
-								<p className="woocommerce-product-form-name-ai-suggestions__item-description">
-									{ item }
-								</p>
+		<Popover focusOnMount={ false }>
+			<>
+				<ul
+					style={ { width: boundingRect?.width, marginBottom: '0' } }
+					{ ...getMenuProps() }
+				>
+					{ items.map( ( item, index ) => (
+						<li
+							key={ item }
+							className={ classNames( {
+								'woocommerce-product-form-name-ai-suggestions__highlighted':
+									highlightedIndex === index,
+							} ) }
+							{ ...getItemProps( { item, index } ) }
+						>
+							<div className="woocommerce-product-form-name-ai-suggestions__item">
+								<div>
+									<p className="woocommerce-product-form-name-ai-suggestions__item-name">
+										{ item }
+									</p>
+									<p className="woocommerce-product-form-name-ai-suggestions__item-description">
+										{ item }
+									</p>
+								</div>
+								<div>
+									<ThumbsUpSVG />
+								</div>
 							</div>
-							<div>
-								<ThumbsUpSVG />
-							</div>
-						</div>
-					</li>
-				) ) }
-			</ul>
+						</li>
+					) ) }
+				</ul>
+				<Button className="woocommerce-product-form-name-ai-suggestions__button">
+					<div className="woocommerce-product-form-name-ai-suggestions__flex">
+						<p>Get more suggestions</p>
+						<p className="woocommerce-product-form-name-ai-suggestions__item-description">
+							Powered by experimental AI
+						</p>
+					</div>
+				</Button>
+			</>
 		</Popover>
 	);
 }
