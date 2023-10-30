e82089bcd0cae6e0b5ffc1f951e84bade1d5e7ce
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-10-30 18:59:29 +0100

    Essential Header Dark pattern: Fix color contrast issues in Twenty Twenty-Four (https://github.com/woocommerce/woocommerce-blocks/pull/11480)

--- plugins/woocommerce-blocks/patterns/header-essential-dark.php
+++ plugins/woocommerce-blocks/patterns/header-essential-dark.php
@@ -11,8 +11,8 @@
 	<!-- wp:group {"style":{"spacing":{"blockGap":"40px"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
 	<div class="wp-block-group">
 		<!-- wp:site-logo {"shouldSyncIcon":false} /-->
-		<!-- wp:search {"label":"<?php esc_html_e( 'Search', 'woo-gutenberg-products-block' ); ?>","showLabel":false,"placeholder":"<?php esc_html_e( 'Search', 'woo-gutenberg-products-block' ); ?>","buttonText":"<?php esc_html_e( 'Search', 'woo-gutenberg-products-block' ); ?>","buttonUseIcon":true,"query":{"post_type":"product"},"width":100,"widthUnit":"%"} /-->
-		<!-- wp:navigation {"textColor":"background","layout":{"type":"flex","justifyContent":"center"}} /-->
+		<!-- wp:search {"label":"<?php esc_html_e( 'Search', 'woo-gutenberg-products-block' ); ?>","showLabel":false,"placeholder":"<?php esc_html_e( 'Search', 'woo-gutenberg-products-block' ); ?>","buttonText":"<?php esc_html_e( 'Search', 'woo-gutenberg-products-block' ); ?>","buttonUseIcon":true,"query":{"post_type":"product"},"width":100,"widthUnit":"%","backgroundColor":"contrast-2"} /-->
+		<!-- wp:navigation {"textColor":"background","overlayTextColor":"contrast","layout":{"type":"flex","justifyContent":"center"}} /-->
 	</div>
 	<!-- /wp:group -->
 
