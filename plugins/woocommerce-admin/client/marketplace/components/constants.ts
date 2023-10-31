c003a257eb4a4e6c670502ae4114cbb5e303f209
Kyle Nel <22053773+kdevnel@users.noreply.github.com>
2023-10-31 10:59:41 +0200

    Change woocommerce.com to woo.com

--- plugins/woocommerce-admin/client/marketplace/components/constants.ts
+++ plugins/woocommerce-admin/client/marketplace/components/constants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_TAB_KEY = 'discover';
-export const MARKETPLACE_HOST = 'https://woocommerce.com';
+export const MARKETPLACE_HOST = 'https://woo.com';
 export const MARKETPLACE_PATH = '/extensions';
 export const MARKETPLACE_SEARCH_API_PATH =
 	'/wp-json/wccom-extensions/1.0/search';
