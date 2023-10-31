221b79be3e6e25e0d5011a7da9ef89ced1d0e38a
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-10-31 21:09:21 +0800

    Update CYS AI loading screen (#41123)
    * Update the CYS Design with AI loading iframes
    
    * Add changelog

--- plugins/woocommerce-admin/client/customize-store/design-with-ai/state-machine.tsx
+++ plugins/woocommerce-admin/client/customize-store/design-with-ai/state-machine.tsx
@@ -19,6 +19,7 @@ import {
 	LookAndFeel,
 	ToneOfVoice,
 	ApiCallLoader,
+	AssembleHubLoader,
 } from './pages';
 import { actions } from './actions';
 import { services } from './services';
@@ -423,13 +424,17 @@ export const designWithAiStateMachineDefinition = createMachine(
 							},
 						},
 						onDone: {
-							actions: [
-								// Full redirect to the Assembler Hub to ensure the user see the new generated content.
-								'redirectToAssemblerHub',
-							],
+							target: '#designWithAi.showAssembleHub',
 						},
 					},
 				},
+			},
+			showAssembleHub: {
+				meta: {
+					component: AssembleHubLoader,
+				},
+				entry: [ 'redirectToAssemblerHub' ],
+				type: 'final',
 			},
 		},
 	},
