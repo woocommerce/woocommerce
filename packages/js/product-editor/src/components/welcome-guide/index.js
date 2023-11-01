fb4314c10d49371650d120437a832a644fa18ef1
Nathan Schneider <nsschneider1@gmail.com>
2023-11-01 15:21:38 -0300

    Add comment

--- packages/js/product-editor/src/components/welcome-guide/index.js
+++ packages/js/product-editor/src/components/welcome-guide/index.js
@@ -9,6 +9,12 @@ import { createElement } from '@wordpress/element';
  */
 import WelcomeGuideDefault from './default';
 
+/**
+ * The components from this directory are copied from gutenberg.
+ * They are used to show the welcome guide if the user opens the block editor for the first time from the product editor.
+ * Ideally it should be exposed from gutenberg and we should use it from there.
+ */
+
 export default function WelcomeGuide() {
 	const { isActive } = useSelect( ( select ) => {
 		const { get } = select( 'core/preferences' );
