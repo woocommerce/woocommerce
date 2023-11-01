532b3babf792e7a084f27d8a32717f0818ddb35e
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-11-01 15:28:53 +0800

    Add cys transitiona page secondary button slotfill

---
+++ plugins/woocommerce-admin/client/customize-store/transitional/secondary-button-slot/index.tsx
@@ -0,0 +1,26 @@
+/**
+ * External dependencies
+ */
+import { useSlot } from '@woocommerce/experimental';
+export * from './example-fill'; // just so that the example script is run in the package
+
+/**
+ * Internal dependencies
+ */
+import {
+	EXPERIMENTAL_WC_CYS_TRANSITIONAL_PAGE_SECONDARY_BUTTON_SLOT_NAME,
+	WooCYSSecondaryButton,
+} from './utils';
+
+export const WooCYSSecondaryButtonSlot = () => {
+	const slot = useSlot(
+		EXPERIMENTAL_WC_CYS_TRANSITIONAL_PAGE_SECONDARY_BUTTON_SLOT_NAME
+	);
+
+	const hasFills = Boolean( slot?.fills?.length );
+	if ( ! hasFills ) {
+		return null;
+	}
+
+	return <WooCYSSecondaryButton.Slot />;
+};
