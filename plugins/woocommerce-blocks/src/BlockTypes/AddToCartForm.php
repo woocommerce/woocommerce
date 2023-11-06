bf145b282a802be1d8a77795d5431074951fad4f
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-11-06 16:30:25 +0100

    Add to Cart with Options block: fix inconsistency between editor and frontend (https://github.com/woocommerce/woocommerce-blocks/pull/11614)
    * Add to Cart with Options block: fix inconsistency between editor and frontend
    
    * Typo
    
    * Use same margin values as block theme stylesheet

--- plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
+++ plugins/woocommerce-blocks/src/BlockTypes/AddToCartForm.php
@@ -99,7 +99,7 @@ class AddToCartForm extends AbstractBlock {
 		$product_classname  = $is_descendent_of_single_product_block ? 'product' : '';
 
 		$form = sprintf(
-			'<div class="wp-block-add-to-cart-form %1$s %2$s %3$s" style="%4$s">%5$s</div>',
+			'<div class="wp-block-add-to-cart-form wc-block-add-to-cart-form %1$s %2$s %3$s" style="%4$s">%5$s</div>',
 			esc_attr( $classes_and_styles['classes'] ),
 			esc_attr( $classname ),
 			esc_attr( $product_classname ),
