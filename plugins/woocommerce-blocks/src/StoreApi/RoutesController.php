82e91b1c6bf31e5d9cad00638653679db35f17e1
Luigi Teschio <gigitux@gmail.com>
2023-11-07 14:50:19 +0100

    Fix pattern route performance (https://github.com/woocommerce/woocommerce-blocks/pull/11535)
    * fix pattern route performance
    
    * update namespace
    
    * improve middleware
    
    * improve ProductSchema
    
    * improve error handling
    
    * update identifier
    
    * fix middleware
    
    * update description
    
    * use schema to return the response
    
    * Break down the generate_content method and create the new fetch_dummy_products_to_update method for handling the fetch of dummy products to be updated.
    
    * Ensure the Product endpoint relies on the fetch_dummy_products_to_update method for fetching dummy products to avoid code repetition and add safety checks and handle errors in case certain properties are not available.
    
    * Add error handling for the Products endpoint.
    
    * Remove memory limit increase and update docblocks.
    
    * re-add set_time_limit
    
    ---------
    
    Co-authored-by: Patricia Hillebrandt <patriciahillebrandt@gmail.com>

--- plugins/woocommerce-blocks/src/StoreApi/RoutesController.php
+++ plugins/woocommerce-blocks/src/StoreApi/RoutesController.php
@@ -1,7 +1,6 @@
 <?php
 namespace Automattic\WooCommerce\StoreApi;
 
-use Automattic\WooCommerce\Blocks\Package;
 use Automattic\WooCommerce\StoreApi\Routes\V1\AbstractRoute;
 
 /**
@@ -30,7 +29,7 @@ class RoutesController {
 	public function __construct( SchemaController $schema_controller ) {
 		$this->schema_controller = $schema_controller;
 		$this->routes            = [
-			'v1' => [
+			'v1'      => [
 				Routes\V1\Batch::IDENTIFIER              => Routes\V1\Batch::class,
 				Routes\V1\Cart::IDENTIFIER               => Routes\V1\Cart::class,
 				Routes\V1\CartAddItem::IDENTIFIER        => Routes\V1\CartAddItem::class,
@@ -48,7 +47,6 @@ class RoutesController {
 				Routes\V1\Checkout::IDENTIFIER           => Routes\V1\Checkout::class,
 				Routes\V1\CheckoutOrder::IDENTIFIER      => Routes\V1\CheckoutOrder::class,
 				Routes\V1\Order::IDENTIFIER              => Routes\V1\Order::class,
-				Routes\V1\Patterns::IDENTIFIER           => Routes\V1\Patterns::class,
 				Routes\V1\ProductAttributes::IDENTIFIER  => Routes\V1\ProductAttributes::class,
 				Routes\V1\ProductAttributesById::IDENTIFIER => Routes\V1\ProductAttributesById::class,
 				Routes\V1\ProductAttributeTerms::IDENTIFIER => Routes\V1\ProductAttributeTerms::class,
@@ -61,6 +59,14 @@ class RoutesController {
 				Routes\V1\ProductsById::IDENTIFIER       => Routes\V1\ProductsById::class,
 				Routes\V1\ProductsBySlug::IDENTIFIER     => Routes\V1\ProductsBySlug::class,
 			],
+			// @todo Migrate internal AI routes to WooCommerce Core codebase.
+			'private' => [
+				Routes\V1\AI\Images::IDENTIFIER   => Routes\V1\AI\Images::class,
+				Routes\V1\AI\Patterns::IDENTIFIER => Routes\V1\AI\Patterns::class,
+				Routes\V1\AI\Product::IDENTIFIER  => Routes\V1\AI\Product::class,
+				Routes\V1\AI\Products::IDENTIFIER => Routes\V1\AI\Products::class,
+				Routes\V1\AI\BusinessDescription::IDENTIFIER => Routes\V1\AI\BusinessDescription::class,
+			],
 		];
 	}
 
@@ -70,6 +76,7 @@ class RoutesController {
 	public function register_all_routes() {
 		$this->register_routes( 'v1', 'wc/store' );
 		$this->register_routes( 'v1', 'wc/store/v1' );
+		$this->register_routes( 'private', 'wc/private' );
 	}
 
 	/**
