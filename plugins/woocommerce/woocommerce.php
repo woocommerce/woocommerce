8ba127f53ec991dc01a2b2c0b449a45a56a492c3
Alex López <alex.lopez@automattic.com>
2023-11-08 14:05:35 +0100

    Prep release/8.3.0.40 for 8.3.0.41 cycle with version bump to 8.3.0.41

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
- * Version: 8.3.0.40
+ * Version: 8.3.0.41
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
