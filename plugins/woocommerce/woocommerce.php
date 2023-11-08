8e55663ff7878f28f77f5ca36b9f05944f1cf6ac
Jonathan Sadowski <sadowski@automattic.com>
2023-11-08 10:28:48 -0600

    Prep release/8.2 for 8.2 cycle with version bump to 8.2.2

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
- * Version: 8.2.1
+ * Version: 8.2.2
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
