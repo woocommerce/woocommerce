a64e397cd782348b3c5e7c89950052346c6bb221
Matt Sherman <matt@jam123.com>
2023-11-09 10:47:38 -0500

    Add code docs

--- plugins/woocommerce/src/Internal/Admin/BlockTemplates/BlockTemplateLogger.php
+++ plugins/woocommerce/src/Internal/Admin/BlockTemplates/BlockTemplateLogger.php
@@ -5,40 +5,61 @@ namespace Automattic\WooCommerce\Internal\Admin\BlockTemplates;
 use Automattic\WooCommerce\Admin\BlockTemplates\BlockContainerInterface;
 use Automattic\WooCommerce\Admin\BlockTemplates\BlockInterface;
 use Automattic\WooCommerce\Admin\BlockTemplates\BlockTemplateInterface;
+use Automattic\WooCommerce\Admin\BlockTemplates\ContainerInterface;
 
 /**
  * Logger for block template modifications.
  */
 class BlockTemplateLogger {
-	const BLOCK_ADDED                       = 'block_added';
-	const BLOCK_REMOVED                     = 'block_removed';
-	const BLOCK_MODIFIED                    = 'block_modified';
-	const BLOCK_ADDED_TO_DETACHED_CONTAINER = 'block_added_to_detached_container';
-	const ERROR_AFTER_BLOCK_ADDED           = 'error_after_block_added';
-	const ERROR_AFTER_BLOCK_REMOVED         = 'error_after_block_removed';
+	const BLOCK_ADDED                            = 'block_added';
+	const BLOCK_REMOVED                          = 'block_removed';
+	const BLOCK_MODIFIED                         = 'block_modified';
+	const BLOCK_ADDED_TO_DETACHED_CONTAINER      = 'block_added_to_detached_container';
+	const HIDE_CONDITION_ADDED                   = 'hide_condition_added';
+	const HIDE_CONDITION_REMOVED                 = 'hide_condition_removed';
+	const HIDE_CONDITION_ADDED_TO_DETACHED_BLOCK = 'hide_condition_added_to_detached_block';
+	const ERROR_AFTER_BLOCK_ADDED                = 'error_after_block_added';
+	const ERROR_AFTER_BLOCK_REMOVED              = 'error_after_block_removed';
 
+	/**
+	 * Event types.
+	 *
+	 * @var array
+	 */
 	public static $event_types = array(
-		self::BLOCK_ADDED                       => array(
+		self::BLOCK_ADDED                            => array(
 			'level'   => \WC_Log_Levels::DEBUG,
 			'message' => 'Block added to template.',
 		),
-		self::BLOCK_REMOVED                     => array(
+		self::BLOCK_REMOVED                          => array(
 			'level'   => \WC_Log_Levels::NOTICE,
 			'message' => 'Block removed from template.',
 		),
-		self::BLOCK_MODIFIED                    => array(
+		self::BLOCK_MODIFIED                         => array(
 			'level'   => \WC_Log_Levels::NOTICE,
 			'message' => 'Block modified in template.',
 		),
-		self::BLOCK_ADDED_TO_DETACHED_CONTAINER => array(
+		self::BLOCK_ADDED_TO_DETACHED_CONTAINER      => array(
 			'level'   => \WC_Log_Levels::WARNING,
 			'message' => 'Block added to detached container. Block will not be included in the template, since the container will not be included in the template.',
 		),
-		self::ERROR_AFTER_BLOCK_ADDED           => array(
+		self::HIDE_CONDITION_ADDED                   => array(
+			'level'   => \WC_Log_Levels::NOTICE,
+			'message' => 'Hide condition added to block.',
+		),
+		self::HIDE_CONDITION_REMOVED                 => array(
+			'level'   => \WC_Log_Levels::NOTICE,
+			'message' => 'Hide condition removed from block.',
+		),
+		self::HIDE_CONDITION_ADDED_TO_DETACHED_BLOCK => array(
+			'level'   => \WC_Log_Levels::WARNING,
+			'message' => 'Hide condition added to block.',
+		),
+		self::ERROR_AFTER_BLOCK_ADDED                => array(
 			'level'   => \WC_Log_Levels::WARNING,
 			'message' => 'Error after block added to template.',
 		),
-		self::ERROR_AFTER_BLOCK_REMOVED         => array(
+		self::ERROR_AFTER_BLOCK_REMOVED              => array(
 			'level'   => \WC_Log_Levels::WARNING,
 			'message' => 'Error after block removed from template.',
 		),
@@ -58,8 +79,18 @@ class BlockTemplateLogger {
 	 */
 	protected $logger = null;
 
+	/**
+	 * All template events.
+	 *
+	 * @var array
+	 */
 	private $all_template_events = array();
 
+	/**
+	 * Threshold severity.
+	 *
+	 * @var int
+	 */
 	private $threshold_severity = null;
 
 	/**
@@ -87,13 +118,27 @@ class BlockTemplateLogger {
 		$this->threshold_severity = \WC_Log_Levels::get_level_severity( $threshold );
 	}
 
+	/**
+	 * Get all template events for a given template.
+	 *
+	 * @param string $template_id Template ID.
+	 */
 	public function get_template_events( string $template_id ): array {
 		return isset( $this->all_template_events[ $template_id ] )
 			? $this->all_template_events[ $template_id ]
 			: array();
 	}
 
-	public function log( string $event_type, $template, $container, $block, $additional_info = array() ) {
+	/**
+	 * Log an event.
+	 *
+	 * @param string                 $event_type      Event type.
+	 * @param BlockTemplateInterface $template        Template.
+	 * @param ContainerInterface     $container       Container.
+	 * @param BlockInterface         $block           Block.
+	 * @param array                  $additional_info Additional info.
+	 */
+	public function log( string $event_type, BlockTemplateInterface $template, ContainerInterface $container, $block, $additional_info = array() ) {
 		if ( ! isset( self::$event_types[ $event_type ] ) ) {
 			/* translators: 1: WC_Logger::log 2: level */
 			wc_doing_it_wrong( __METHOD__, sprintf( __( '%1$s was called with an invalid event type "%2$s".', 'woocommerce' ), '<code>BlockTemplateLogger::log</code>', $event_type ), '8.4' );
@@ -120,11 +165,25 @@ class BlockTemplateLogger {
 		$this->add_template_event( $event_type_info, $template, $container, $block, $additional_info );
 	}
 
+	/**
+	 * Should the logger handle a given level?
+	 *
+	 * @param int $level Level to check.
+	 */
 	private function should_handle( $level ) {
 		return $this->threshold_severity <= \WC_Log_Levels::get_level_severity( $level );
 	}
 
-	private function log_to_logger( $event_type_info, $template, $container, $block, $additional_info = array() ) {
+	/**
+	 * Log to the logger.
+	 *
+	 * @param array                  $event_type_info Event type info.
+	 * @param BlockTemplateInterface $template        Template.
+	 * @param ContainerInterface     $container       Container.
+	 * @param BlockInterface         $block           Block.
+	 * @param array                  $additional_info Additional info.
+	 */
+	private function log_to_logger( array $event_type_info, BlockTemplateInterface $template, ContainerInterface $container, BlockInterface $block, array $additional_info = array() ) {
 		$info = array_merge(
 			array(
 				'template'  => $template,
@@ -143,7 +202,16 @@ class BlockTemplateLogger {
 		);
 	}
 
-	private function add_template_event( $event_type_info, $template, $container, $block, $additional_info = array() ) {
+	/**
+	 * Add a template event.
+	 *
+	 * @param array                  $event_type_info Event type info.
+	 * @param BlockTemplateInterface $template        Template.
+	 * @param ContainerInterface     $container       Container.
+	 * @param BlockInterface         $block           Block.
+	 * @param array                  $additional_info Additional info.
+	 */
+	private function add_template_event( array $event_type_info, BlockTemplateInterface $template, ContainerInterface $container, BlockInterface $block, array $additional_info = array() ) {
 		$template_id = $template->get_id();
 
 		if ( ! isset( $this->all_template_events[ $template_id ] ) ) {
@@ -176,7 +244,7 @@ class BlockTemplateLogger {
 	 * @param string $message Message to log.
 	 * @param array  $info    Additional info to log.
 	 */
-	private function format_message( string $message, array $info = [] ): string {
+	private function format_message( string $message, array $info = array() ): string {
 		$formatted_message = sprintf(
 			"%s\n%s",
 			$message,
@@ -226,12 +294,12 @@ class BlockTemplateLogger {
 	 * @param \Exception $exception Exception to format.
 	 */
 	private function format_exception( \Exception $exception ): array {
-		return [
+		return array(
 			'message' => $exception->getMessage(),
 			'source'  => "{$exception->getFile()}: {$exception->getLine()}",
 			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r
 			'trace'   => print_r( $this->format_exception_trace( $exception->getTrace() ), true ),
-		];
+		);
 	}
 
 	/**
@@ -240,7 +308,7 @@ class BlockTemplateLogger {
 	 * @param array $trace Exception trace to format.
 	 */
 	private function format_exception_trace( array $trace ): array {
-		$formatted_trace = [];
+		$formatted_trace = array();
 
 		foreach ( $trace as $source ) {
 			$formatted_trace[] = "{$source['file']}: {$source['line']}";
