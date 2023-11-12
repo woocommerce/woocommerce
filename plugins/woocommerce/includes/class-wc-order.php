e691048eaf7889c476daceecd54a3f5d170c500f
Sagar Tamang <mi5t4n@gmail.com>
2023-11-12 11:26:29 +0545

    fix - phpcs errors

--- plugins/woocommerce/includes/class-wc-order.php
+++ plugins/woocommerce/includes/class-wc-order.php
@@ -400,12 +400,12 @@ class WC_Order extends WC_Abstract_Order {
 				 * @param int Order ID.
 				 * @param WC_Order $order Order object.
 				 * @param array $status_transition {
-				 * 		Status transition data.
+				 *      Status transition data.
 				 *
-				 * 		@type string $from Order status from.
-				 *		@type string $to Order status to
-				 *		@type string $note Order note.
-				 *		@type boolean $manual True if the order is manually changed.
+				 *      @type string $from Order status from.
+				 *      @type string $to Order status to
+				 *      @type string $note Order note.
+				 *      @type boolean $manual True if the order is manually changed.
 				 * }
 				 */
 				do_action( 'woocommerce_order_status_' . $status_transition['to'], $this->get_id(), $this, $status_transition );
