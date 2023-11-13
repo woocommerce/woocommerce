027905e7067b348d9f577a1298e64e03b6da698e
Bero <berislav.grgicak@gmail.com>
2023-11-13 12:40:07 +0100

    Refresh notices

--- plugins/woocommerce-admin/client/marketplace/components/my-subscriptions/table/actions/refresh-button.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/my-subscriptions/table/actions/refresh-button.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { Button } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { useContext, useState } from '@wordpress/element';
 
 /**
@@ -10,25 +10,62 @@ import { useContext, useState } from '@wordpress/element';
  */
 import RefreshIcon from '../../../../assets/images/refresh.svg';
 import { SubscriptionsContext } from '../../../../contexts/subscriptions-context';
+import { addNotice, removeNotice } from '../../../../utils/functions';
+import { NoticeStatus } from '../../../../contexts/types';
+
+const NOTICE_ID = 'woocommerce-marketplace-refresh-subscriptions';
 
 export function RefreshButton() {
 	const { refreshSubscriptions } = useContext( SubscriptionsContext );
 	const [ isLoading, setIsLoading ] = useState( false );
 
-	const onClick = () => {
+	const refresh = () => {
 		if ( isLoading ) {
 			return;
 		}
+
+		removeNotice( NOTICE_ID );
 		setIsLoading( true );
-		refreshSubscriptions().finally( () => {
-			setIsLoading( false );
-		} );
+
+		refreshSubscriptions()
+			.then( () => {
+				addNotice(
+					NOTICE_ID,
+					__( 'Subscriptions refreshed.', 'woocommerce' ),
+					NoticeStatus.Success
+				);
+			} )
+			.catch( ( error ) => {
+				addNotice(
+					NOTICE_ID,
+					sprintf(
+						// translators: %s is the error message.
+						__(
+							'Error refreshing subscriptions: %s',
+							'woocommerce'
+						),
+						error.message
+					),
+					NoticeStatus.Error,
+					{
+						actions: [
+							{
+								label: __( 'Try again', 'woocommerce' ),
+								onClick: refresh,
+							},
+						],
+					}
+				);
+			} )
+			.finally( () => {
+				setIsLoading( false );
+			} );
 	};
 
 	return (
 		<Button
 			className="woocommerce-marketplace__refresh-subscriptions"
-			onClick={ onClick }
+			onClick={ refresh }
 			isBusy={ isLoading }
 		>
 			<img
