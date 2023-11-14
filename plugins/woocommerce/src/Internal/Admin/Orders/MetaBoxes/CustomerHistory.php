1a2215fc6d117686aa11fc6515438bd39783e480
Justin Palmer <228780+layoutd@users.noreply.github.com>
2023-11-14 18:21:31 +0100

    Updated customer history logic (billing email + better totals) - Uses billing_email if no customer_id is provided - Accounts for refunds and multiple types of order status

--- plugins/woocommerce/src/Internal/Admin/Orders/MetaBoxes/CustomerHistory.php
+++ plugins/woocommerce/src/Internal/Admin/Orders/MetaBoxes/CustomerHistory.php
@@ -14,36 +14,86 @@ class CustomerHistory {
 	/**
 	 * Output the customer history template for the order.
 	 *
-	 * @since x.x.x
-	 *
 	 * @param WC_Order $order The order object.
 	 *
 	 * @return void
 	 */
-	public function output( WC_Order $order ) {
-		$this->display_customer_history( $order->get_customer_id() );
+	public function output( WC_Order $order ): void {
+		$this->display_customer_history( $order->get_customer_id(), $order->get_billing_email() );
 	}
 
 	/**
 	 * Display the customer history template for the customer.
 	 *
-	 * @since x.x.x
-	 *
-	 * @param int $customer_id The customer ID.
+	 * @param int    $customer_id The customer ID.
+	 * @param string $billing_email The customer billing email.
 	 *
 	 * @return void
 	 */
-	private function display_customer_history( int $customer_id ) {
-		// Calculate the data needed for the template.
-		$order_count = wc_get_customer_order_count( $customer_id );
-		$total_spent = wc_get_customer_total_spent( $customer_id );
+	private function display_customer_history( int $customer_id, string $billing_email ): void {
+		$has_customer_id = false;
+		if ( $customer_id ) {
+			$has_customer_id = true;
+			$args            = $this->get_customer_history( $customer_id );
+		} elseif ( $billing_email ) {
+			$args = $this->get_customer_history( $billing_email );
+		} else {
+			$args = array(
+				'order_count'   => 0,
+				'total_spent'   => 0,
+				'average_spent' => 0,
+			);
+		}
 
+		$args['has_customer_id'] = $has_customer_id;
+		wc_get_template( 'order/customer-history.php', $args );
+	}
+
+	/**
+	 * Get the order history for the customer (matches Customers report).
+	 *
+	 * @param mixed $customer_identifier The customer ID or billing email.
+	 *
+	 * @return array Order count, total spend, and average spend per order.
+	 */
+	private function get_customer_history( $customer_identifier ): array {
+
+		// Get the valid customer orders.
 		$args = array(
+			'limit'  => - 1,
+			'return' => 'objects',
+			// Don't count cancelled or failed orders.
+			'status' => array( 'pending', 'processing', 'on-hold', 'completed', 'refunded' ),
+			'type'   => 'shop_order',
+		);
+
+		// If the customer_identifier is a valid ID, use it. Otherwise, use the billing email.
+		if ( is_numeric( $customer_identifier ) && $customer_identifier > 0 ) {
+			$args['customer_id'] = $customer_identifier;
+		} else {
+			$args['billing_email'] = $customer_identifier;
+		}
+
+		$orders = wc_get_orders( $args );
+
+		// Populate the order_count and total_spent variables with the valid orders.
+		$order_count = 0;
+		$total_spent = 0;
+		foreach ( $orders as $order ) {
+			$order_count ++;
+			$total_spent += $order->get_total();
+
+			// Remove any refunded amount from the total_spent.
+			foreach ( $order->get_refunds() as $refund ) {
+				$total_spent += $refund->get_total();
+			}
+		}
+
+		return array(
 			'order_count'   => $order_count,
 			'total_spent'   => $total_spent,
 			'average_spent' => $order_count ? $total_spent / $order_count : 0,
 		);
-
-		wc_get_template( 'order/customer-history.php', $args );
 	}
+
 }
