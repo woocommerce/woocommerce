6b7c8dbea1be7e39d8ef4822c3542c84fcc160be
moon <moon.kyong@automattic.com>
2023-11-15 13:45:31 -0800

    Lint fix

--- plugins/woocommerce/tests/php/src/Admin/API/PaymentGatewaySuggestionsTest.php
+++ plugins/woocommerce/tests/php/src/Admin/API/PaymentGatewaySuggestionsTest.php
@@ -39,50 +39,55 @@ class PaymentGatewaySuggestionsTest extends WC_REST_Unit_Test_Case {
 
 	}
 
-	public function test_it_clears_cache_when_the_base_country_gets_updated()
-	{
-		// Clear any exisiting cache first
+	/**
+	 * Test it clears cache when the base country gets updated.
+	 *
+	 * @return void
+	 */
+	public function test_it_clears_cache_when_the_base_country_gets_updated() {
+		// Clear any exisiting cache first.
 		Init::delete_specs_transient();
 
 		$current_base_country = wc_get_base_location()['country'];
-		$response_mock_ref = function( $preempt, $parsed_args, $url ) use ( $current_base_country ) {
+		$response_mock_ref    = function( $preempt, $parsed_args, $url ) use ( $current_base_country ) {
 			if ( str_contains( $url, 'https://woocommerce.com/wp-json/wccom/payment-gateway-suggestions/1.0/suggestions.json' ) ) {
 				return array(
 					'success' => true,
-					'body' => json_encode(array(
+					'body'    => wp_json_encode(
 						array(
-							'id' => wc_get_base_location()['country']
+							array(
+								'id' => wc_get_base_location()['country'],
+							),
 						)
-					))
+					),
 				);
 			}
 
 			return $preempt;
 		};
-		// Make a new request -- this should populate the cache with the base country
-		add_filter( 'pre_http_request', $response_mock_ref , 10, 3 );
-		$request = new WP_REST_Request( 'GET', self::ENDPOINT );
+		// Make a new request -- this should populate the cache with the base country.
+		add_filter( 'pre_http_request', $response_mock_ref, 10, 3 );
+		$request  = new WP_REST_Request( 'GET', self::ENDPOINT );
 		$response = rest_get_server()->dispatch( $request )->get_data();
 
-		// Confirm the current data returns id = US
-		$this->assertEquals( $current_base_country , $response[0]->id);
+		// Confirm the current data returns id = US.
+		$this->assertEquals( $current_base_country, $response[0]->id );
 
-		// Remove filter just in case and a new request still returns the cached data
+		// Remove filter just in case and a new request still returns the cached data.
 		remove_filter( 'pre_http_request', $response_mock_ref );
 		$response = rest_get_server()->dispatch( $request )->get_data();
-		$this->assertEquals( $current_base_country , $response[0]->id);
+		$this->assertEquals( $current_base_country, $response[0]->id );
 
-		add_filter( 'pre_http_request', $response_mock_ref , 10, 3 );
+		add_filter( 'pre_http_request', $response_mock_ref, 10, 3 );
 
-		// Update the base country to CA
+		// Update the base country to CA.
 		update_option( 'woocommerce_default_country', 'CA:ON' );
 
-		// Make a new request -- this should populate the cache with the updated country
+		// Make a new request -- this should populate the cache with the updated country.
 		$response = rest_get_server()->dispatch( $request )->get_data();
-		$this->assertEquals('CA', $response[0]->id);
+		$this->assertEquals( 'CA', $response[0]->id );
 
-		// Clean up
+		// Clean up.
 		remove_filter( 'pre_http_request', $response_mock_ref );
-
 	}
 }
