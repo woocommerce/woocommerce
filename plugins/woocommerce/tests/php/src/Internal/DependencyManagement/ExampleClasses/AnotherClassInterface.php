b22519d7de0f989f30fc29d5a0293502a67c7bb3
Dima <9010963+message-dimke@users.noreply.github.com>
2023-11-17 00:41:36 +0200

    Adding AbstractInterfaceServiceProvider unit test and test supplies.

---
+++ plugins/woocommerce/tests/php/src/Internal/DependencyManagement/ExampleClasses/AnotherClassInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * AnotherClassInterface interface file.
+ */
+
+namespace Automattic\WooCommerce\Tests\Internal\DependencyManagement\ExampleClasses;
+
+/**
+ * An example of an interface.
+ */
+interface AnotherClassInterface {
+}
