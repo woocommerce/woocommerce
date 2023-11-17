78b1bf7996b1f03d5cfb79680f34a0d876f83556
Thomas Shellberg <6723003+tommyshellberg@users.noreply.github.com>
2023-11-17 14:38:24 +0100

    Make requests to Woo Wizard WPCOCM endpoint.

--- plugins/woo-ai/src/woo-wizard/chat-modal.tsx
+++ plugins/woo-ai/src/woo-wizard/chat-modal.tsx
@@ -2,7 +2,9 @@
  * External dependencies
  */
 import React, { useState } from 'react';
-import { Modal, Button, TextControl } from '@wordpress/components';
+import { Modal, Button, Spinner, TextControl } from '@wordpress/components';
+import apiFetch from '@wordpress/api-fetch';
+import { __experimentalRequestJetpackToken as requestJetpackToken } from '@woocommerce/ai';
 
 /**
  * Internal dependencies
@@ -21,14 +23,52 @@ interface ChatModalProps {
 const ChatModal: React.FC< ChatModalProps > = ( { onClose } ) => {
 	const [ input, setInput ] = useState( '' );
 	const [ messages, setMessages ] = useState< Message[] >( [] );
+	const [ isLoading, setLoading ] = useState( false );
 
-	const handleSubmit = ( event: React.FormEvent ) => {
+	const handleSubmit = async ( event: React.FormEvent ) => {
 		event.preventDefault();
 		if ( ! input.trim() ) return;
-		const newMessage: Message = { sender: 'user', text: input };
-		setMessages( [ ...messages, newMessage ] );
+		setLoading( true );
+
+		const userMessage: Message = { sender: 'user', text: input };
+		setMessages( [ ...messages, userMessage ] );
 		setInput( '' );
-		// Add logic here to send the message to the assistant and receive a reply
+
+		try {
+			const { token } = await requestJetpackToken();
+			const url = new URL(
+				'https://public-api.wordpress.com/wpcom/v2/woo-wizard'
+			);
+			url.searchParams.append( 'message', input );
+			url.searchParams.append( 'token', token );
+			const response = ( await apiFetch( {
+				url: url.toString(),
+				method: 'GET',
+			} ) ) as any;
+
+			// Assuming the response contains the assistant's message
+			const messages = response.messages;
+			if ( ! messages || ! messages.length ) {
+				throw new Error( 'No messages returned from assistant' );
+			}
+			// create a new assistantMessage array
+			const assistantMessages: Message[] = [];
+			for ( const message of messages ) {
+				const assistantMessage: Message = {
+					sender: 'assistant',
+					text: message.content,
+				};
+				assistantMessages.push( assistantMessage );
+			}
+			setMessages( ( messages ) => [
+				...messages,
+				...assistantMessages,
+			] );
+		} catch ( error ) {
+			console.error( 'Error fetching response:', error );
+			// Handle the error appropriately
+		}
+		setLoading( false );
 	};
 
 	return (
@@ -53,7 +93,12 @@ const ChatModal: React.FC< ChatModalProps > = ( { onClose } ) => {
 					onChange={ setInput }
 					placeholder="Type your message..."
 				/>
-				<Button isPrimary type="submit">
+				<Button
+					disabled={ isLoading }
+					isBusy={ isLoading }
+					type="submit"
+					variant="primary"
+				>
 					Send
 				</Button>
 			</form>
