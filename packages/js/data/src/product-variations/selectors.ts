e340a7cfc1c2429c315ad4fbf5b91fac47f34877
Fernando Marichal <ultimoround@gmail.com>
2023-11-20 16:48:10 -0300

    Add generateProductVariationsError selector

--- packages/js/data/src/product-variations/selectors.ts
+++ packages/js/data/src/product-variations/selectors.ts
@@ -22,3 +22,18 @@ export const isGeneratingVariations = (
 	);
 	return state.requesting[ itemQuery ];
 };
+export const generateProductVariationsError = (
+	state: ResourceState,
+	idQuery: IdQuery
+) => {
+	const urlParameters = getUrlParameters(
+		WC_PRODUCT_VARIATIONS_NAMESPACE,
+		idQuery
+	);
+	const { key } = parseId( idQuery, urlParameters );
+	const itemQuery = getRequestIdentifier(
+		CRUD_ACTIONS.GENERATE_VARIATIONS,
+		key
+	);
+	return state.errors[ itemQuery ];
+};
