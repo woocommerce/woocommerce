7d736cc03ba37229b34c344491d473b208b0be04
Tung Du <dinhtungdu@gmail.com>
2023-11-21 16:25:43 +0700

    add: default template for Collection Filters block (https://github.com/woocommerce/woocommerce-blocks/pull/11805)

--- plugins/woocommerce-blocks/assets/js/blocks/collection-filters/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/collection-filters/edit.tsx
@@ -5,6 +5,9 @@ import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 import { useEffect } from '@wordpress/element';
 import { useCollection } from '@woocommerce/base-context/hooks';
+import { sprintf, __ } from '@wordpress/i18n';
+import { getSetting } from '@woocommerce/settings';
+import type { AttributeSetting } from '@woocommerce/types';
 
 /**
  * Internal dependencies
@@ -12,9 +15,59 @@ import { useCollection } from '@woocommerce/base-context/hooks';
 import { formatQuery, getQueryParams } from './utils';
 import type { EditProps } from './type';
 
+const ATTRIBUTES = getSetting< AttributeSetting[] >( 'attributes', [] );
+
+const template = [
+	[
+		'core/heading',
+		{
+			content: __( 'Filter by Price', 'woo-gutenberg-products-block' ),
+			level: 3,
+		},
+	],
+	[ 'woocommerce/collection-price-filter', {} ],
+	[
+		'core/heading',
+		{
+			content: __(
+				'Filter by Stock status',
+				'woo-gutenberg-products-block'
+			),
+			level: 3,
+		},
+	],
+	[ 'woocommerce/collection-stock-filter', {} ],
+];
+
+const firstAttribute = ATTRIBUTES.find( Boolean );
+
+if ( firstAttribute ) {
+	template.push(
+		[
+			'core/heading',
+			{
+				content: sprintf(
+					// translators: %s is the attribute label.
+					__( 'Filter by %s', 'woo-gutenberg-products-block' ),
+					firstAttribute.attribute_label
+				),
+				level: 3,
+			},
+		],
+		[
+			'woocommerce/collection-attribute-filter',
+			{
+				attributeId: parseInt( firstAttribute?.attribute_id, 10 ),
+			},
+		]
+	);
+}
+
 const Edit = ( { clientId, setAttributes, context }: EditProps ) => {
 	const blockProps = useBlockProps();
-	const innerBlockProps = useInnerBlocksProps( blockProps );
+	const innerBlockProps = useInnerBlocksProps( blockProps, {
+		template,
+	} );
 
 	// Get inner blocks by clientId
 	const currentBlock = useSelect( ( select ) => {
