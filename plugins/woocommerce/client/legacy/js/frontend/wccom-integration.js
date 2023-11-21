7a01b3beaf051f7f796106f000b3652ac81be63d
Justin Palmer <228780+layoutd@users.noreply.github.com>
2023-11-21 07:17:05 +0100

    Remove WCCOM attribution source integration

--- plugins/woocommerce/client/legacy/js/frontend/wccom-integration.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener( 'load' , function ( e ) {
-	if ( window.wccom && window.wccom.canTrackUser('analytics') ) {
-		window.wc_order_source_attribution.setAllowTrackingConsent( true );
-	}
-
-} );
