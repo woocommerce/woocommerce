87f4dad8428447e6ca288c24c2950072a30f319c
berislav grgičak <berislav.grgicak@gmail.com>
2023-11-22 13:38:47 +0100

    Update plugins/woocommerce/src/Internal/Admin/Marketplace.php
    Co-authored-by: And Finally <andfinally@users.noreply.github.com>

--- plugins/woocommerce/src/Internal/Admin/Marketplace.php
+++ plugins/woocommerce/src/Internal/Admin/Marketplace.php
@@ -120,7 +120,7 @@ class Marketplace {
 			.plugin-install-woo > a::after {
 				content: "";
 				display: inline-block;
-				background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M8.33321 3H12.9999V7.66667H11.9999V4.70711L8.02009 8.68689L7.31299 7.97978L11.2928 4H8.33321V3Z' fill='%23646970'/%3E%3Cpath d='M6.33333 4.1665H4.33333C3.8731 4.1665 3.5 4.5396 3.5 4.99984V11.6665C3.5 12.1267 3.8731 12.4998 4.33333 12.4998H11C11.4602 12.4998 11.8333 12.1267 11.8333 11.6665V9.6665' stroke='%23646970'/%3E%3C/svg%3E%0A");				width: 24px;
+				background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M8.33321 3H12.9999V7.66667H11.9999V4.70711L8.02009 8.68689L7.31299 7.97978L11.2928 4H8.33321V3Z' fill='%23646970'/%3E%3Cpath d='M6.33333 4.1665H4.33333C3.8731 4.1665 3.5 4.5396 3.5 4.99984V11.6665C3.5 12.1267 3.8731 12.4998 4.33333 12.4998H11C11.4602 12.4998 11.8333 12.1267 11.8333 11.6665V9.6665' stroke='%23646970'/%3E%3C/svg%3E%0A");
 				width: 16px;
 				height: 16px;
 				background-repeat: no-repeat;
