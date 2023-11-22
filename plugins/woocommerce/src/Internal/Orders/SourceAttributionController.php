497bc3047dd00eb391327eafd299caec3071ffb0
Justin Palmer <228780+layoutd@users.noreply.github.com>
2023-11-22 15:54:26 +0100

    Fix array assignment spacing

--- plugins/woocommerce/src/Internal/Orders/SourceAttributionController.php
+++ plugins/woocommerce/src/Internal/Orders/SourceAttributionController.php
@@ -371,14 +371,14 @@ class SourceAttributionController implements RegisterHooksInterface {
 	 */
 	private function send_order_tracks( array $source_data, WC_Order $order ) {
 		$tracks_data = array(
-			'order_id'              => $order->get_id(),
-			'type'                  => $source_data['type'] ?? '',
-			'medium'                => $source_data['utm_medium'] ?? '',
-			'source'                => $source_data['utm_source'] ?? '',
-			'device_type'           => strtolower( $source_data['device_type'] ?? '(unknown)' ),
-			'session_pages'         => $source_data['session_pages'] ?? 0,
-			'session_count'         => $source_data['session_count'] ?? 0,
-			'order_total'           => $order->get_total(),
+			'order_id'             => $order->get_id(),
+			'type'                 => $source_data['type'] ?? '',
+			'medium'               => $source_data['utm_medium'] ?? '',
+			'source'               => $source_data['utm_source'] ?? '',
+			'device_type'          => strtolower( $source_data['device_type'] ?? '(unknown)' ),
+			'session_pages'        => $source_data['session_pages'] ?? 0,
+			'session_count'        => $source_data['session_count'] ?? 0,
+			'order_total'          => $order->get_total(),
 			'customer_order_count' => wc_get_customer_order_count( $order->get_customer_id() ),
 		);
 
