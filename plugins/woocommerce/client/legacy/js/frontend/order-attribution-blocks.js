cb4dd6e59221ec40b8eb7b9182431610e43c1bc8
Tomek Wytrębowicz <tomalecpub@gmail.com>
2023-11-23 16:50:38 +0100

    Blanket Order Source Attribution → Order Attribution for blocks
    Propagate chenges from https://github.com/woocommerce/woocommerce/pull/39701/commits/c9c2a05ca5cab352777108332ecc871b3f87fcfb to the Block-related classes

---
+++ plugins/woocommerce/client/legacy/js/frontend/order-attribution-blocks.js
@@ -0,0 +1,4 @@
+window.wp.data.dispatch( window.wc.wcBlocksData.CHECKOUT_STORE_KEY ).__internalSetExtensionData(
+	'woocommerce/order-attribution',
+	window.wc_order_attribution.sbjsDataToSchema( window.sbjs.get )
+);
--- plugins/woocommerce/client/legacy/js/frontend/order-source-attribution-blocks.js
+++
@@ -1,4 +0,0 @@
-window.wp.data.dispatch( window.wc.wcBlocksData.CHECKOUT_STORE_KEY ).__internalSetExtensionData(
-	'woocommerce/order-source-attribution',
-	window.wc_order_source_attribution.sbjsDataToSchema( window.sbjs.get )
-);
