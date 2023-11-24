430e38692b6791a84c5072576f777b8da9da42c1
Fernando Marichal <ultimoround@gmail.com>
2023-11-24 14:17:55 -0300

    Add empty state for attributes

--- packages/js/product-editor/src/components/attribute-input-field/attribute-input-field.tsx
+++ packages/js/product-editor/src/components/attribute-input-field/attribute-input-field.tsx
@@ -7,6 +7,11 @@ import { Spinner, Icon } from '@wordpress/components';
 import { plus } from '@wordpress/icons';
 import { createElement, useMemo } from '@wordpress/element';
 import {
+	UseComboboxGetMenuPropsOptions,
+	GetPropsCommonOptions,
+	UseComboboxGetItemPropsOptions,
+} from 'downshift';
+import {
 	EXPERIMENTAL_PRODUCT_ATTRIBUTES_STORE_NAME,
 	QueryProductAttribute,
 	ProductAttribute,
@@ -47,6 +52,19 @@ type AttributeInputFieldProps = {
 	ignoredAttributeIds?: number[];
 	createNewAttributesAsGlobal?: boolean;
 };
+
+export type getMenuPropsType = (
+	options?: UseComboboxGetMenuPropsOptions,
+	otherOptions?: GetPropsCommonOptions
+	// These are the types provided by Downshift.
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+) => any;
+
+// type getItemPropsType< T > = MenuItemProps< T >[ 'getItemProps' ];
+export type getItemPropsType< ItemType > = (
+	options: UseComboboxGetItemPropsOptions< ItemType >
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+) => any;
 
 function isNewAttributeListItem( attribute: NarrowedQueryAttribute ): boolean {
 	return attribute.id === -99;
@@ -164,6 +182,60 @@ export const AttributeInputField: React.FC< AttributeInputFieldProps > = ( {
 		}
 	};
 
+	const renderMenuItems = (
+		renderItems: NarrowedQueryAttribute[],
+		highlightedIndex: number,
+		getItemProps: (
+			options: UseComboboxGetMenuPropsOptions
+		) => getItemPropsType< NarrowedQueryAttribute >
+	) => {
+		if ( renderItems.length > 0 ) {
+			return renderItems.map( ( item, index: number ) => (
+				<MenuItem
+					key={ item.id }
+					index={ index }
+					isActive={ highlightedIndex === index }
+					item={ item }
+					getItemProps={ (
+						options: UseComboboxGetMenuPropsOptions
+					) => ( {
+						...getItemProps( options ),
+						disabled: item.isDisabled || undefined,
+					} ) }
+					tooltipText={
+						item.isDisabled
+							? disabledAttributeMessage
+							: sanitizeSlugName( item.slug )
+					}
+				>
+					{ isNewAttributeListItem( item ) ? (
+						<div className="woocommerce-attribute-input-field__add-new">
+							<Icon
+								icon={ plus }
+								size={ 20 }
+								className="woocommerce-attribute-input-field__add-new-icon"
+							/>
+							<span>
+								{ sprintf(
+									/* translators: The name of the new attribute term to be created */
+									__( 'Create "%s"', 'woocommerce' ),
+									item.name
+								) }
+							</span>
+						</div>
+					) : (
+						item.name
+					) }
+				</MenuItem>
+			) );
+		}
+		return (
+			<div className="woocommerce-attribute-input-field__no-results">
+				{ __( 'Nothing yet. Type to create.', 'woocommerce' ) }
+			</div>
+		);
+	};
+
 	return (
 		<SelectControl< NarrowedQueryAttribute >
 			className="woocommerce-attribute-input-field"
@@ -175,7 +247,7 @@ export const AttributeInputField: React.FC< AttributeInputFieldProps > = ( {
 			getItemLabel={ ( item ) => item?.name || '' }
 			getItemValue={ ( item ) => item?.id || '' }
 			selected={ value }
-			onSelect={ ( attribute ) => {
+			onSelect={ ( attribute: NarrowedQueryAttribute ) => {
 				if ( isNewAttributeListItem( attribute ) ) {
 					addNewAttribute( attribute );
 				} else {
@@ -196,51 +268,27 @@ export const AttributeInputField: React.FC< AttributeInputFieldProps > = ( {
 				getItemProps,
 				getMenuProps,
 				isOpen,
+			}: {
+				items: NarrowedQueryAttribute[];
+				highlightedIndex: number;
+				getItemProps: (
+					options: UseComboboxGetMenuPropsOptions
+				) => getItemPropsType< NarrowedQueryAttribute >;
+				getMenuProps: getMenuPropsType;
+				isOpen: boolean;
 			} ) => {
 				return (
 					<Menu getMenuProps={ getMenuProps } isOpen={ isOpen }>
 						{ isLoading ? (
 							<Spinner />
 						) : (
-							renderItems.map( ( item, index: number ) => (
-								<MenuItem
-									key={ item.id }
-									index={ index }
-									isActive={ highlightedIndex === index }
-									item={ item }
-									getItemProps={ ( options ) => ( {
-										...getItemProps( options ),
-										disabled: item.isDisabled || undefined,
-									} ) }
-									tooltipText={
-										item.isDisabled
-											? disabledAttributeMessage
-											: sanitizeSlugName( item.slug )
-									}
-								>
-									{ isNewAttributeListItem( item ) ? (
-										<div className="woocommerce-attribute-input-field__add-new">
-											<Icon
-												icon={ plus }
-												size={ 20 }
-												className="woocommerce-attribute-input-field__add-new-icon"
-											/>
-											<span>
-												{ sprintf(
-													/* translators: The name of the new attribute term to be created */
-													__(
-														'Create "%s"',
-														'woocommerce'
-													),
-													item.name
-												) }
-											</span>
-										</div>
-									) : (
-										item.name
-									) }
-								</MenuItem>
-							) )
+							renderMenuItems(
+								renderItems,
+								highlightedIndex,
+								getItemProps as (
+									options: UseComboboxGetMenuPropsOptions
+								) => getItemPropsType< NarrowedQueryAttribute >
+							)
 						) }
 					</Menu>
 				);
