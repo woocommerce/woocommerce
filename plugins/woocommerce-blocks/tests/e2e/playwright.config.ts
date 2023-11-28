fbb00dfd6b74c6ff0afa65abe93b577428e00749
Thomas Roberts <5656702+opr@users.noreply.github.com>
2023-11-28 19:16:25 +0000

    Move the Checkout -> Account tests from puppeteer to Playwright (https://github.com/woocommerce/woocommerce-blocks/pull/11961)

--- plugins/woocommerce-blocks/tests/e2e/playwright.config.ts
+++ plugins/woocommerce-blocks/tests/e2e/playwright.config.ts
@@ -7,6 +7,8 @@ import path from 'path';
 
 import { fileURLToPath } from 'url';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require( 'dotenv' ).config();
 interface ExtendedPlaywrightTestConfig extends PlaywrightTestConfig {
 	use: {
 		stateDir?: string;
