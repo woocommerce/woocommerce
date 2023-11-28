6af4cfef8249ef0dd67aee7b0aac988fc797815c
Alfredo Sumaran <a.sumaran@gmail.com>
2023-11-28 17:52:07 -0500

    Move count call outside the loop to satisfy the linter

--- plugins/woocommerce/includes/admin/meta-boxes/views/html-order-refund.php
+++ plugins/woocommerce/includes/admin/meta-boxes/views/html-order-refund.php
@@ -64,8 +64,9 @@ $who_refunded = new WP_User( $refund->get_refunded_by() );
 
 	<?php
 	if ( wc_tax_enabled() && is_countable( $order_taxes ) ) :
+		$total_taxes_count = count( $total_taxes );
 		?>
-		<?php for ( $i = 0;  $i < count( $total_taxes ); $i++ ) : ?>
+		<?php for ( $i = 0;  $i < $total_taxes_count; $i++ ) : ?>
 			<td class="line_tax" width="1%"></td>
 		<?php endfor; ?>
 	<?php endif; ?>
