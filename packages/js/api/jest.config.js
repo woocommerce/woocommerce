4b0b022a1876141cb8bc7ef4900ae589e0805427
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-11-28 15:48:29 -0800

    Fixed API Test Config
    Since axios moved to ESM we need to make
    sure to transpile the source we're using
    directly.

--- packages/js/api/jest.config.js
+++ packages/js/api/jest.config.js
@@ -3,5 +3,15 @@ module.exports = {
 	verbose: true,
 	rootDir: 'src',
 	testEnvironment: 'node',
-	testPathIgnorePatterns: [ '/node_modules/', '/dist/' ],
+	testPathIgnorePatterns: [ 
+		'/node_modules/',
+		'/dist/'
+	],
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+		'node_modules/axios/lib/.+\\.js$': 'babel-jest',
+	},
+	transformIgnorePatterns: [
+		'node_modules/(?!(?:.pnpm/)?axios)',
+	],
 };
