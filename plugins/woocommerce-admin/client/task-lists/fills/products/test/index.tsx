b1ac7300c710ee493ea173c7140661157a28bf90
Jason Kytros <jason.kytros@automattic.com>
2023-11-29 17:45:14 +0200

    Remove unused )

--- plugins/woocommerce-admin/client/task-lists/fills/products/test/index.tsx
+++ plugins/woocommerce-admin/client/task-lists/fills/products/test/index.tsx
@@ -43,7 +43,6 @@ const confirmModalText =
 describe( 'Products', () => {
 	beforeEach( () => {
 		jest.clearAllMocks();
-		);
 	} );
 
 	it( 'should render default products types when onboardingData.profile.productType is null', () => {
