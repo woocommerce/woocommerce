aba7d75d09e39534e15a72208fa5fc7d22e7db2e
Damián Suárez <rdsuarez@gmail.com>
2023-12-01 13:19:27 -0300

    fix TS issues

--- packages/js/product-editor/src/blocks/product-fields/description/components/withFullEditorToolbarButton.tsx
+++ packages/js/product-editor/src/blocks/product-fields/description/components/withFullEditorToolbarButton.tsx
@@ -13,59 +13,64 @@ import { useDispatch } from '@wordpress/data';
  * Internal dependencies
  */
 import { store } from '../../../../store/product-editor-ai';
+import type {
+	DescriptionBlockEditComponent,
+	DescriptionBlockEditProps,
+} from '../types';
 
-/**
- * Internal dependencies
- */
-// import type { ProductEditorBlockEditProps } from '../../../types';
+const wooBlockwithFullEditorToolbarButton =
+	createHigherOrderComponent< DescriptionBlockEditComponent >(
+		( BlockEdit: DescriptionBlockEditComponent ) => {
+			return ( props: DescriptionBlockEditProps ) => {
+				const { openModalEditor } = useDispatch( store );
 
-const wooBlockwithFullEditorToolbarButton = createHigherOrderComponent<
-	Record< string, unknown >
->( ( BlockEdit ) => {
-	return ( props ) => {
-		const { openModalEditor } = useDispatch( store );
+				// Only extend summary field block instances
+				if ( props?.name !== 'woocommerce/product-summary-field' ) {
+					return <BlockEdit { ...props } />;
+				}
 
-		// Only extend summary field block instances
-		if ( props?.name !== 'woocommerce/product-summary-field' ) {
-			return <BlockEdit { ...props } />;
-		}
+				// Only add the `Full editor` button when the block is selected
+				if ( ! props?.isSelected ) {
+					return <BlockEdit { ...props } />;
+				}
 
-		// Only add the `Full editor` button when the block is selected
-		if ( ! props?.isSelected ) {
-			return <BlockEdit { ...props } />;
-		}
+				/*
+				 * Extend the toolbar only to the sumary field block instance
+				 * that has the `woocommerce/product-description-field__content` template block ID.
+				 */
+				if (
+					props?.attributes?._templateBlockId !==
+					'product-description__content'
+				) {
+					return <BlockEdit { ...props } />;
+				}
 
-		/*
-		 * Extend the toolbar only to the sumary field block instance
-		 * that has the `woocommerce/product-description-field__content` template block ID.
-		 */
-		if (
-			props?.attributes?._templateBlockId !==
-			'product-description__content'
-		) {
-			return <BlockEdit { ...props } />;
-		}
+				const blockControlProps = { group: 'other' };
 
-		return (
-			<>
-				<BlockControls group="other">
-					<ToolbarButton
-						label={ __(
-							'Edit Product description',
-							'woocommerce'
-						) }
-						onClick={ () => {
-							recordEvent( 'product_add_description_click' );
-							openModalEditor();
-						} }
-					>
-						{ __( 'Full editor', 'woocommerce' ) }
-					</ToolbarButton>
-				</BlockControls>
-				<BlockEdit { ...props } />
-			</>
-		);
-	};
-}, 'wooBlockwithFullEditorToolbarButton' );
+				return (
+					<>
+						<BlockControls { ...blockControlProps }>
+							<ToolbarButton
+								label={ __(
+									'Edit Product description',
+									'woocommerce'
+								) }
+								onClick={ () => {
+									recordEvent(
+										'product_add_description_click'
+									);
+									openModalEditor();
+								} }
+							>
+								{ __( 'Full editor', 'woocommerce' ) }
+							</ToolbarButton>
+						</BlockControls>
+						<BlockEdit { ...props } />
+					</>
+				);
+			};
+		},
+		'wooBlockwithFullEditorToolbarButton'
+	);
 
 export default wooBlockwithFullEditorToolbarButton;
