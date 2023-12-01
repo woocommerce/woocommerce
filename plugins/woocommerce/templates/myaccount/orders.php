2a88d54dbc6f8de00ca9310921e8d3907b38df82
Chris Lilitsas <1105590+xristos3490@users.noreply.github.com>
2023-12-01 13:48:47 +0200

    Template bump

--- plugins/woocommerce/templates/myaccount/orders.php
+++ plugins/woocommerce/templates/myaccount/orders.php
@@ -14,7 +14,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 8.3.1
+ * @version 8.4.0
  */
 
 defined( 'ABSPATH' ) || exit;
