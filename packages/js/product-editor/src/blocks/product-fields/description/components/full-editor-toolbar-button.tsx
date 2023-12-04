84b2aa6f8a7b8514f274f9643af07a21f9859d10
Damián Suárez <rdsuarez@gmail.com>
2023-12-04 15:35:53 -0300

    store is not ready to use

--- packages/js/product-editor/src/blocks/product-fields/description/components/full-editor-toolbar-button.tsx
+++ packages/js/product-editor/src/blocks/product-fields/description/components/full-editor-toolbar-button.tsx
@@ -5,25 +5,21 @@ import { createElement } from '@wordpress/element';
 import { ToolbarButton } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { recordEvent } from '@woocommerce/tracks';
-import { dispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
-import { store } from '../../../../store/product-editor-ai';
-
 export default function FullEditorToolbarButton( {
 	label = __( 'Edit Product description', 'woocommerce' ),
 	text = __( 'Full editor', 'woocommerce' ),
 } ) {
-	const { openModalEditor } = dispatch( store );
-
 	return (
 		<ToolbarButton
 			label={ label }
 			onClick={ () => {
 				recordEvent( 'product_add_description_click' );
-				openModalEditor();
+				// @todo: dispatch acion once store is ready to use
+				// @see https://github.com/woocommerce/woocommerce/pull/41859
 			} }
 		>
 			{ text }
