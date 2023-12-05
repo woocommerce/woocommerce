6138845203dc2fd5f4fc853e2931bcf0a3ff7331
paul sealock <psealock@gmail.com>
2023-12-05 14:21:13 +1300

    Add currency filter

--- plugins/woocommerce-admin/client/wp-admin-scripts/shipping-settings-region-picker/currency-context.js
+++ plugins/woocommerce-admin/client/wp-admin-scripts/shipping-settings-region-picker/currency-context.js
@@ -4,13 +4,14 @@
 import { useContext, useEffect } from '@wordpress/element';
 import { CurrencyContext } from '@woocommerce/currency';
 import { numberFormat } from '@woocommerce/number';
+import { applyFilters } from '@wordpress/hooks';
 
 export const ShippingCurrencyContext = () => {
 	const context = useContext( CurrencyContext );
 
 	useEffect( () => {
 		window.wc.ShippingCurrencyContext =
-			window.wc.ShippingCurrencyContext || context;
+			window.wc.ShippingCurrencyContext || applyFilters( 'woocommerce_shipping_zone_method_currency_context', context );
 		window.wc.ShippingCurrencyNumberFormat =
 			window.wc.ShippingCurrencyNumberFormat || numberFormat;
 	}, [ context ] );
