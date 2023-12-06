cfe1226a28dfaeaf25b5129c0afca3976c5923f0
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2023-12-06 17:14:16 +0800

    Fix lint

--- plugins/woocommerce-admin/client/customize-store/intro/services.ts
+++ plugins/woocommerce-admin/client/customize-store/intro/services.ts
@@ -7,7 +7,6 @@ import { store as coreStore } from '@wordpress/core-data';
 import { resolveSelect } from '@wordpress/data';
 import { ONBOARDING_STORE_NAME, OPTIONS_STORE_NAME } from '@woocommerce/data';
 import apiFetch from '@wordpress/api-fetch';
-import { apiFetch as wpApiFetch } from '@wordpress/data-controls';
 
 /**
  * Internal dependencies
