7fd99375e6683881363e383f8ecbf53a3da98457
Damián Suárez <rdsuarez@gmail.com>
2023-12-07 12:21:44 -0300

    parse and set iframe blocks when clicking on toolbar button

--- packages/js/product-editor/src/blocks/product-fields/description/components/full-editor-toolbar-button.tsx
+++ packages/js/product-editor/src/blocks/product-fields/description/components/full-editor-toolbar-button.tsx
@@ -6,6 +6,8 @@ import { ToolbarButton } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { recordEvent } from '@woocommerce/tracks';
 import { dispatch } from '@wordpress/data';
+import { useEntityProp } from '@wordpress/core-data';
+import { parse } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
@@ -16,12 +18,18 @@ export default function FullEditorToolbarButton( {
 	label = __( 'Edit Product description', 'woocommerce' ),
 	text = __( 'Full editor', 'woocommerce' ),
 } ) {
-	const { openModalEditor } = dispatch( store );
+	const { openModalEditor, setModalEditorBlocks } = dispatch( store );
+	const [ description ] = useEntityProp< string >(
+		'postType',
+		'product',
+		'description'
+	);
 
 	return (
 		<ToolbarButton
 			label={ label }
 			onClick={ () => {
+				setModalEditorBlocks( parse( description ) );
 				recordEvent( 'product_add_description_click' );
 				openModalEditor();
 			} }
