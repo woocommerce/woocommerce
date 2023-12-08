dea220b5d965d78cc63ccdd212e6ff406faea6cd
paul sealock <psealock@gmail.com>
2023-12-08 15:33:54 +1300

    Add a back button to method configuration screen

--- plugins/woocommerce/includes/admin/settings/views/html-admin-page-shipping-zone-methods.php
+++ plugins/woocommerce/includes/admin/settings/views/html-admin-page-shipping-zone-methods.php
@@ -149,10 +149,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 				</article>
 				<footer>
 					<div class="inner">
-						<button id="btn-ok" data-status='{{ data.status }}' class="button button-primary button-large">
-							<div class="wc-backbone-modal-action-{{ data.status === 'new' ? 'active' : 'inactive' }}"><?php esc_html_e( 'Create', 'woocommerce' ); ?></div>
-							<div class="wc-backbone-modal-action-{{ data.status === 'existing' ? 'active' : 'inactive' }}"><?php esc_html_e( 'Save', 'woocommerce' ); ?></div>
-						</button>
+						<div>
+							<button id="btn-back" class="button button-large wc-backbone-modal-back-{{ data.status === 'new' ? 'active' : 'inactive' }}"><?php esc_html_e( 'Back', 'woocommerce' ); ?></button>
+							<button id="btn-ok" data-status='{{ data.status }}' class="button button-primary button-large">
+								<div class="wc-backbone-modal-action-{{ data.status === 'new' ? 'active' : 'inactive' }}"><?php esc_html_e( 'Create', 'woocommerce' ); ?></div>
+								<div class="wc-backbone-modal-action-{{ data.status === 'existing' ? 'active' : 'inactive' }}"><?php esc_html_e( 'Save', 'woocommerce' ); ?></div>
+							</button>
+						</div>
 						<div class="wc-shipping-zone-method-modal-info wc-shipping-zone-method-modal-info-{{ data.status === 'existing' ? 'inactive' : 'active' }}"><?php esc_html_e( 'STEP 2 OF 2', 'woocommerce' ); ?></div>
 					</div>
 				</footer>
