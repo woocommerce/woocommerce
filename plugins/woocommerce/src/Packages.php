b8966ef4fb40d9a9a1e3f4994f736e39c60b1065
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-12-07 16:44:23 -0800

    Automatically Deactivate Merged Packages
    Once a feature plugin has been merged it is no longer necessary for it
    to be activated. In some cases, having it activated can actually
    lead to problems. This will automatically disable any packages
    that we have marked as "merged".

--- plugins/woocommerce/src/Packages.php
+++ plugins/woocommerce/src/Packages.php
@@ -5,6 +5,8 @@
 
 namespace Automattic\WooCommerce;
 
+use Automattic\Jetpack\Constants;
+
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -25,7 +27,29 @@ class Packages {
 	 * @var array Key is the package name/directory, value is the main package class which handles init.
 	 */
 	protected static $packages = array(
-		'woocommerce-blocks'   => '\\Automattic\\WooCommerce\\Blocks\\Package'
+		'woocommerce-admin' => '\\Automattic\\WooCommerce\\Admin\\Composer\\Package',
+		'woocommerce-blocks' => '\\Automattic\\WooCommerce\\Blocks\\Package',
+	);
+
+	/**
+	 * Array of package names and their main package classes.
+	 * 
+	 * One a package has been merged into WooCommerce Core it should be moved fron the package list and placed in
+	 * this list. This will ensure that the feature plugin is disabled as well as provide the class to handle
+	 * initialization for the now-merged feature plugin.
+	 * 
+	 * Once a package has been merged into WooCommerce Core it should have its slug added here. This will ensure
+	 * that we deactivate the feature plugin automaticatlly to prevent any problems caused by conflicts between
+	 * the two versions caused by them both being active.
+	 *
+	 * @var array Key is the package name/directory, value is the main package class which handles init.
+	 */
+	protected static $merged_packages = array(
+		'woocommerce-admin',
+		// Accommodate all of the different names WooCommerce Blocks may be referred to using.
+		'woo-gutenberg-products-block',
+		'woocommerce-gutenberg-products-block',
+		'woocommerce-blocks',
 	);
 
 	/**
@@ -41,9 +65,10 @@ class Packages {
 	 * Callback for WordPress init hook.
 	 */
 	public static function on_init() {
-		self::load_packages();
+		self::deactivate_merged_packages();
+		self::initialize_packages();
 	}
-
+	
 	/**
 	 * Checks a package exists by looking for it's directory.
 	 *
@@ -51,7 +76,84 @@ class Packages {
 	 * @return boolean
 	 */
 	public static function package_exists( $package ) {
+		// Once a package has been merged it always exists.
+		if ( in_array( $package, self::$merged_packages, true ) ) {
+			return true;
+		}
+
 		return file_exists( dirname( __DIR__ ) . '/packages/' . $package );
+	}
+
+	/**
+	 * Deactivates merged feature plugins.
+	 *
+	 * Once a feature plugin is merged into WooCommerce Core it should be deactivated. This method will
+	 * ensure that a plugin gets deactivated. Note that for the first request it will still be active,
+	 * and as such, there may be some odd behavior. This is unlikely to cause any issues though
+	 * because it will be deactivated on the request that updates or activates WooCommerce.
+	 */
+	protected static function deactivate_merged_packages() {
+		// Developers may need to be able to run merged feature plugins alongside merged packages for testing purposes.
+		if ( Constants::is_true( 'WC_ALLOW_MERGED_FEATURE_PLUGINS' ) ) {
+			return;
+		}
+
+		// Since we are relying on the Jetpack Autloader to resolve class versioning differences we can use it to
+		// detect whether or not an mu-plugin is registered in the autoloader. This is powerful because it means
+		// we don't have to do expensive mu-plugin checks on every request.
+		global $jetpack_autoloader_cached_plugin_paths;
+		$active_plugins = get_option( 'active_plugins', array() );
+		foreach ( $jetpack_autoloader_cached_plugin_paths as $plugin_dir_path ) {
+			$plugin_dir = basename( $plugin_dir_path );
+
+			if ( ! in_array( $plugin_dir, self::$merged_packages, true ) ) {
+				continue;
+			}
+
+			// Deactivate the plugin if possible so that there are no conflicts.
+			foreach ( $active_plugins as $active_plugin_path ) {
+				$active_plugin_dir = dirname( plugin_basename( $active_plugin_path ) );
+				if ( $active_plugin_dir !== $plugin_dir ) {
+					continue;
+				}
+
+				require_once ABSPATH . 'wp-admin/includes/plugin.php';
+
+				// Make sure to display a message informing the user that the plugin has been deactivated.
+				$plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . $active_plugin_path );
+				deactivate_plugins( $active_plugin_path );
+				add_action(
+					'admin_notices',
+					function() use ( $plugin_data ) {
+						echo '<div class="error"><p>';
+						printf(
+							/* translators: %s: is referring to the plugin's name. */
+							esc_html__( 'The %1$s plugin has been deactivated as the latest improvements are now included with the %2$s plugin.', 'woocommerce' ),
+							'<code>' . $plugin_data[ 'Name' ] . '</code>',
+							'<code>WooCommerce</code>'
+						);
+						echo '</p></div>';
+					}
+				);
+
+				continue 2;
+			}
+
+			// Since no active plugin was found this is most likely an mu-plugin. We can't deactivate this ourselves so we need
+			// to display a message informing the site administrator that the plugin needs to be disabled.
+			add_action(
+				'admin_notices',
+				function() use ( $plugin_dir ) {
+					echo '<div class="error"><p>';
+					printf(
+						/* translators: %s: is referring to the plugin's name. */
+						esc_html__( 'The %1$s Must-Use plugin has been merged into WooCommerce and should be removed from your site.', 'woocommerce' ),
+						'<code>' . $plugin_dir . '</code>'
+					);
+					echo '</p></div>';
+				}
+			);
+		}
 	}
 
 	/**
@@ -59,10 +161,7 @@ class Packages {
 	 *
 	 * Each package should include an init file which loads the package so it can be used by core.
 	 */
-	protected static function load_packages() {
-		// Initialize WooCommerce Admin.
-		\Automattic\WooCommerce\Admin\Composer\Package::init();
-
+	protected static function initialize_packages() {
 		foreach ( self::$packages as $package_name => $package_class ) {
 			if ( ! self::package_exists( $package_name ) ) {
 				self::missing_package( $package_name );
