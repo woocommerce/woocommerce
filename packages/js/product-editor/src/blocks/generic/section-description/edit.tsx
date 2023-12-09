b26488c8bb0e083c63df7d3f01e75d9008f0a7d2
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2023-12-09 08:15:27 -0800

    Builds Now Functional

--- packages/js/product-editor/src/blocks/generic/section-description/edit.tsx
+++ packages/js/product-editor/src/blocks/generic/section-description/edit.tsx
@@ -19,7 +19,7 @@ export function SectionDescriptionBlockEdit( {
 	const { content } = attributes;
 	const blockProps = useWooBlockProps( attributes );
 
-	const rootClientId: string = useSelect(
+	const rootClientId = useSelect(
 		( select ) => {
 			const { getBlockRootClientId } = select( 'core/block-editor' );
 			return getBlockRootClientId( clientId );
