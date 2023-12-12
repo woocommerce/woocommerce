226d25e176aca3c2b279837c42e4e342ba960b56
roykho <roykho77@gmail.com>
2023-12-12 07:15:26 -0800

    Product Image Gallery: Add transform to Product Gallery block

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/product-image-gallery/index.ts
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/product-image-gallery/index.ts
@@ -3,6 +3,7 @@
  */
 import { gallery as icon } from '@wordpress/icons';
 import { registerBlockSingleProductTemplate } from '@woocommerce/atomic-utils';
+import { createBlock } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
@@ -19,6 +20,17 @@ registerBlockSingleProductTemplate( {
 		icon,
 		// @ts-expect-error `edit` can be extended to include other attributes
 		edit,
+		transforms: {
+			to: [
+				{
+					type: 'block',
+					blocks: [ 'woocommerce/product-gallery' ],
+					transform: () => {
+						return createBlock( 'woocommerce/product-gallery' );
+					},
+				},
+			],
+		},
 	},
 	isAvailableOnPostEditor: false,
 } );
