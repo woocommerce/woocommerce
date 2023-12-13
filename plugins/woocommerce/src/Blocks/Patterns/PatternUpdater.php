9267cb5c68c757e1ce4d04b64deae5f0f2a4c606
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-12-13 10:53:13 -0300

    Update the prompt for AI content generation in patterns.

--- plugins/woocommerce/src/Blocks/Patterns/PatternUpdater.php
+++ plugins/woocommerce/src/Blocks/Patterns/PatternUpdater.php
@@ -218,7 +218,7 @@ class PatternUpdater {
 		$formatted_prompts = [];
 		foreach ( $prompts as $prompt ) {
 			$formatted_prompts[] = sprintf(
-				"Given the following description '%s' generate long texts for the sections using the following prompts for each one of them: `'%s'`. Ensure each entry is unique and does not repeat the given examples. The response should be an array of data in JSON format. Each element should be an object with the pattern name as the key, and the generated content as values. Do not include backticks or the word json in the response. Here's an example format: `'%s'`",
+				"You are an experienced writer. Given a business described as: '%s', generate content for the sections using the following prompts for each one of them: `'%s'`, always making sure that the expected number of characters is respected. The response should be an array of data in JSON format. Each element should be an object with the pattern name as the key, and the generated content as values. Here's an example format: `'%s'`",
 				$business_description,
 				wp_json_encode( $prompt ),
 				wp_json_encode( $expected_results_format[ $i ] )
