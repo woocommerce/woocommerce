bf95a08ad5de518b7b6fbc894de29937d3cd9c25
Thomas Roberts <thomas.roberts@automattic.com>
2023-12-15 19:35:51 +0000

    Register Additional information block frontend files

---
+++ plugins/woocommerce-blocks/assets/js/blocks/checkout/inner-blocks/checkout-additional-information-block/block.tsx
@@ -0,0 +1,5 @@
+const Block = ( { className }: { className?: string } ): React.ReactElement => {
+	return <div className={ className }>Additional information block!</div>;
+};
+
+export default Block;
