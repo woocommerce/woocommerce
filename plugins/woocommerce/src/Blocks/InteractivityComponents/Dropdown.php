8589c3fc2edfecf0120e31bf399697a7a05cede3
Sam Seay <samueljseay@gmail.com>
2023-12-15 17:56:03 +0800

    Fix styling issues with the multi select mode.

--- plugins/woocommerce/src/Blocks/InteractivityComponents/Dropdown.php
+++ plugins/woocommerce/src/Blocks/InteractivityComponents/Dropdown.php
@@ -25,10 +25,6 @@ class Dropdown {
 
 		$dropdown_context = array(
 			'selectedItems' => $selected_items,
-			'hoveredItem'   => array(
-				'label' => null,
-				'value' => null,
-			),
 			'isOpen'        => false,
 			'selectType'    => $select_type,
 		);
@@ -40,11 +36,13 @@ class Dropdown {
 
 		$namespace = wp_json_encode( array( 'namespace' => 'woocommerce/interactivity-dropdown' ) );
 
+		$wrapper_class = 'multiple' === $select_type ? '' : 'single-selection';
+
 		ob_start();
 		?>
 		<div data-wc-interactive='<?php echo esc_attr( $namespace ); ?>'>
 			<div class="wc-interactivity-dropdown" data-wc-context='<?php echo esc_attr( wp_json_encode( $dropdown_context ) ); ?>' >
-				<div class="wc-blocks-components-form-token-field-wrapper single-selection" >
+				<div class="wc-blocks-components-form-token-field-wrapper <?php echo esc_attr( $wrapper_class ); ?>" >
 					<div class="components-form-token-field" tabindex="-1">
 						<div class="components-form-token-field__input-container" 
 							data-wc-class--is-active="context.isOpen" 
@@ -57,21 +55,21 @@ class Dropdown {
 											<span class="components-form-token-field__token-text">
 												<?php echo esc_html( $selected['label'] ); ?>
 											</span>
+											<button 
+												type="button" 
+												data-wc-context="<?php echo esc_attr( wp_json_encode( array( 'currentItem' => $selected ) ) ); ?>"
+												data-wc-on--click="actions.unselectDropdownItem" 
+												data-wc-on--click--parent-action="<?php echo esc_attr( $action ); ?>" 
+												class="components-button components-form-token-field__remove-token has-icon"
+											>
+												<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+													<path d="M12 13.06l3.712 3.713 1.061-1.06L13.061 12l3.712-3.712-1.06-1.06L12 10.938 8.288 7.227l-1.061 1.06L10.939 12l-3.712 3.712 1.06 1.061L12 13.061z"></path>
+												</svg>
+											</button>
 										</span>
-										<button 
-											type="button" 
-											data-wc-context="<?php echo esc_attr( wp_json_encode( array( 'currentItem' => $selected ) ) ); ?>"
-											data-wc-on--click="actions.unselectDropdownItem" 
-											data-wc-on--click--parent-action="<?php echo esc_attr( $action ); ?>" 
-											class="components-button components-form-token-field__remove-token has-icon"
-										>
-											<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-												<path d="M12 13.06l3.712 3.713 1.061-1.06L13.061 12l3.712-3.712-1.06-1.06L12 10.938 8.288 7.227l-1.061 1.06L10.939 12l-3.712 3.712 1.06 1.061L12 13.061z"></path>
-											</svg>
-										</button>
 									<?php } ?>
 								<?php } ?>
-							<input id="components-form-token-input-1" type="text" autocomplete="off" data-wc-bind--placeholder="state.placeholderText" class="components-form-token-field__input" role="combobox" aria-expanded="false" aria-autocomplete="list" aria-describedby="components-form-token-suggestions-howto-1" value="">
+								<input id="components-form-token-input-1" type="text" autocomplete="off" data-wc-bind--placeholder="state.placeholderText" class="components-form-token-field__input" role="combobox" aria-expanded="false" aria-autocomplete="list" aria-describedby="components-form-token-suggestions-howto-1" value="">
 							<ul hidden data-wc-bind--hidden="!context.isOpen" class="components-form-token-field__suggestions-list" id="components-form-token-suggestions-1" role="listbox">
 								<?php
 								foreach ( $items as $item ) :
