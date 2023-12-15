64f137207b8056055b0a29a81650a10c86fe3197
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-12-15 09:07:19 +0100

    Bring back Inherit query from template

--- plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-controls/index.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-controls/index.tsx
@@ -5,7 +5,7 @@ import type { BlockEditProps } from '@wordpress/blocks';
 import { InspectorControls } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 import { type ElementType, useMemo } from '@wordpress/element';
-import { EditorBlock } from '@woocommerce/types';
+import { EditorBlock, isEmpty } from '@woocommerce/types';
 import { addFilter } from '@wordpress/hooks';
 import { ProductCollectionFeedbackPrompt } from '@woocommerce/editor-components/feedback-prompt';
 import {
@@ -35,6 +35,7 @@ import { DEFAULT_FILTERS, getDefaultSettings } from '../../constants';
 import { getUnchangeableFilters } from '../../collections';
 import UpgradeNotice from './upgrade-notice';
 import ColumnsControl from './columns-control';
+import InheritQueryControl from './inherit-query-control';
 import OrderByControl from './order-by-control';
 import OnSaleControl from './on-sale-control';
 import StockStatusControl from './stock-status-control';
@@ -60,6 +61,8 @@ const ProductCollectionInspectorControls = (
 	const shouldShowFilter = prepareShouldShowFilter( unchangeableFilters );
 
 	const showQueryControls = inherit === false;
+	const showInheritQueryControls =
+		isEmpty( collection ) || shouldShowFilter( CoreFilterNames.INHERIT );
 	const showOrderControl =
 		showQueryControls && shouldShowFilter( CoreFilterNames.ORDER );
 	const showFeaturedControl = shouldShowFilter( CoreFilterNames.FEATURED );
@@ -93,9 +96,12 @@ const ProductCollectionInspectorControls = (
 			>
 				<LayoutOptionsControl { ...displayControlProps } />
 				<ColumnsControl { ...displayControlProps } />
-				{ showOrderControl ? (
+				{ showInheritQueryControls && (
+					<InheritQueryControl { ...queryControlProps } />
+				) }
+				{ showOrderControl && (
 					<OrderByControl { ...queryControlProps } />
-				) : null }
+				) }
 			</ToolsPanel>
 
 			{ showQueryControls ? (
@@ -109,17 +115,17 @@ const ProductCollectionInspectorControls = (
 					} }
 					className="wc-block-editor-product-collection-inspector-toolspanel__filters"
 				>
-					{ showOnSaleControl ? (
+					{ showOnSaleControl && (
 						<OnSaleControl { ...queryControlProps } />
-					) : null }
+					) }
 					<StockStatusControl { ...queryControlProps } />
 					<HandPickedProductsControl { ...queryControlProps } />
 					<KeywordControl { ...queryControlProps } />
 					<AttributesControl { ...queryControlProps } />
 					<TaxonomyControls { ...queryControlProps } />
-					{ showFeaturedControl ? (
+					{ showFeaturedControl && (
 						<FeaturedProductsControl { ...queryControlProps } />
-					) : null }
+					) }
 					<CreatedControl { ...queryControlProps } />
 				</ToolsPanel>
 			) : null }
