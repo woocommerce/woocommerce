22f44fe5d05919066921bb540b1b31174525b8ec
Ilyas Foo <foo.ilyas@gmail.com>
2023-12-18 23:00:35 +0800

    Fix shipping cost formatting to respect formula

--- plugins/woocommerce-admin/client/wp-admin-scripts/shipping-settings-region-picker/currency-context.js
+++ plugins/woocommerce-admin/client/wp-admin-scripts/shipping-settings-region-picker/currency-context.js
@@ -5,6 +5,30 @@ import { useContext, useEffect } from '@wordpress/element';
 import { CurrencyContext } from '@woocommerce/currency';
 import { numberFormat } from '@woocommerce/number';
 
+/**
+ * Format all numbers with respect to shipping formula.
+ */
+export const numberFormatWithShippingFormula = ( config, number ) => {
+	if ( typeof number === 'number' ) {
+		return numberFormat( config, number );
+	}
+
+	if ( typeof number === 'string' ) {
+		/**
+		 * \b: Ensures that we're capturing whole numbers (word boundaries).
+		 * \d+: Matches one or more digits.
+		 * (\.\d+)?: Optionally matches a decimal point followed by one or more digits.
+		 * (?![^[]*\]): A negative lookahead to avoid numbers inside square brackets.
+		 * g: Global flag to match all instances in the string.
+		 */
+		return number.replace( /(\b\d+(\.\d+)?\b)(?![^[]*\])/g, ( n ) =>
+			numberFormat( config, n )
+		);
+	}
+
+	return number;
+};
+
 export const ShippingCurrencyContext = () => {
 	const context = useContext( CurrencyContext );
 
@@ -12,7 +36,8 @@ export const ShippingCurrencyContext = () => {
 		window.wc.ShippingCurrencyContext =
 			window.wc.ShippingCurrencyContext || context;
 		window.wc.ShippingCurrencyNumberFormat =
-			window.wc.ShippingCurrencyNumberFormat || numberFormat;
+			window.wc.ShippingCurrencyNumberFormat ||
+			numberFormatWithShippingFormula;
 	}, [ context ] );
 
 	return null;
---
+++ plugins/woocommerce-admin/client/wp-admin-scripts/shipping-settings-region-picker/test/currency-context.js
@@ -0,0 +1,70 @@
+/**
+ * External dependencies
+ */
+import { numberFormat } from '@woocommerce/number';
+
+/**
+ * Internal dependencies
+ */
+import { numberFormatWithShippingFormula } from '../currency-context';
+
+const config = {
+	code: 'USD',
+	symbol: '$',
+	symbolPosition: 'left',
+	decimalSeparator: '.',
+	priceFormat: '%1$s%2$s',
+	thousandSeparator: ',',
+	precision: 2,
+};
+
+describe( 'CurrencyContext', () => {
+	it( 'should format a number input correctly', () => {
+		expect( numberFormatWithShippingFormula( config, 1234 ) ).toBe(
+			numberFormat( config, 1234 )
+		);
+	} );
+
+	it( 'should format numbers within a string correctly', () => {
+		expect(
+			numberFormatWithShippingFormula( config, 'Value is 123456789' )
+		).toBe( `Value is 123,456,789.00` );
+	} );
+
+	it( 'should not format numbers within square brackets', () => {
+		expect(
+			numberFormatWithShippingFormula( config, 'Value [1234] is inside' )
+		).toBe( 'Value [1234] is inside' );
+	} );
+
+	it( 'should format all numbers inside parenthesis', () => {
+		expect(
+			numberFormatWithShippingFormula(
+				config,
+				'50 + (([qty]*2+1)(5*10))(1)'
+			)
+		).toBe( '50.00 + (([qty]*2.00+1.00)(5.00*10.00))(1.00)' );
+	} );
+
+	it( 'should format numbers and ignore shortcodes', () => {
+		expect(
+			numberFormatWithShippingFormula(
+				config,
+				`0.5 * [qty] + [fee percent="10" max_fee='8.33']`
+			)
+		).toBe( `0.50 * [qty] + [fee percent="10" max_fee='8.33']` );
+	} );
+
+	it( 'should return the original string for non-numeric inputs', () => {
+		expect( numberFormatWithShippingFormula( config, 'Hello World' ) ).toBe(
+			'Hello World'
+		);
+	} );
+
+	it( 'should return the original input for non-string, non-number inputs', () => {
+		const input = { some: 'object' };
+		expect( numberFormatWithShippingFormula( config, input ) ).toBe(
+			input
+		);
+	} );
+} );
