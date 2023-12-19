e3900631a786bc3f96b89c076899a9dab5155759
Nathan Schneider <nsschneider1@gmail.com>
2023-12-19 13:49:14 -0300

    Update type to string

--- packages/js/product-editor/src/hooks/use-product-metadata.ts
+++ packages/js/product-editor/src/hooks/use-product-metadata.ts
@@ -8,14 +8,14 @@ import { useEntityProp } from '@wordpress/core-data';
 import { Metadata } from '../types';
 
 function useProductMetadata( postType?: string ) {
-	const [ metadata, setMetadata ] = useEntityProp< Metadata< never >[] >(
+	const [ metadata, setMetadata ] = useEntityProp< Metadata< string >[] >(
 		'postType',
 		postType || 'product',
 		'meta_data'
 	);
 
 	return {
-		updateMetadata: ( entries: Metadata< never >[] ) => {
+		updateMetadata: ( entries: Metadata< string >[] ) => {
 			setMetadata( [
 				...metadata.filter(
 					( item ) =>
