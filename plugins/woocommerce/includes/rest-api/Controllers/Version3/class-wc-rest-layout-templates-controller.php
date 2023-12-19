24f88f380f1f3a411fcb0879d95c926e5f66ebaa
Matt Sherman <matt@jam123.com>
2023-12-19 11:10:34 -0500

    Single layout template route

--- plugins/woocommerce/includes/rest-api/Controllers/Version3/class-wc-rest-layout-templates-controller.php
+++ plugins/woocommerce/includes/rest-api/Controllers/Version3/class-wc-rest-layout-templates-controller.php
@@ -42,17 +42,39 @@ class WC_REST_Layout_Templates_Controller extends WC_REST_Controller {
 				'args'                => array(),
 			)
 		);
+
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/(?P<id>\w[\w\s\-]*)',
+			array(
+				'args' => array(
+					'id' => array(
+						'description' => __( 'Unique identifier for the resource.', 'woocommerce' ),
+						'type'        => 'string',
+					),
+				),
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => array( $this, 'get_item' ),
+					'permission_callback' => array( $this, 'get_item_permissions_check' ),
+					'args'                => array(),
+				),
+			)
+		);
 	}
 
 	public function get_items_permissions_check( $request ) {
 		return true;
 	}
 
+	public function get_item_permissions_check( $request ) {
+		return true;
+	}
+
 	public function get_items( $request ) {
 		$layout_templates = array();
 
-		$template_registry = wc_get_container()->get( BlockTemplateRegistry::class );
-
+		$template_registry           = wc_get_container()->get( BlockTemplateRegistry::class );
 		$registered_layout_templates = $template_registry->get_all_registered();
 
 		foreach ( $registered_layout_templates as $layout_template ) {
@@ -60,6 +82,21 @@ class WC_REST_Layout_Templates_Controller extends WC_REST_Controller {
 		}
 
 		$response = rest_ensure_response( $layout_templates );
+
+		return $response;
+	}
+
+	public function get_item( $request ) {
+		$id = $request['id'];
+
+		$template_registry = wc_get_container()->get( BlockTemplateRegistry::class );
+		$layout_template   = $template_registry->get_registered( $id );
+
+		if ( ! $layout_template ) {
+			return new WP_Error( 'woocommerce_rest_layout_template_invalid_id', __( 'Invalid layout template ID.', 'woocommerce' ), array( 'status' => 404 ) );
+		}
+
+		$response = rest_ensure_response( $layout_template->get_formatted_template() );
 
 		return $response;
 	}
