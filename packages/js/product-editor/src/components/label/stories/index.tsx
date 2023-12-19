65663ec3f87ceac37cf7a3a984da831b59eb3e17
Damián Suárez <rdsuarez@gmail.com>
2023-12-19 12:03:41 -0300

    add simple Label story

--- packages/js/product-editor/src/components/label/stories/index.tsx
+++ packages/js/product-editor/src/components/label/stories/index.tsx
@@ -1,19 +1,22 @@
 /**
  * External dependencies
  */
-import { createElement } from '@wordpress/element';
-import React from 'react';
+import { __ } from '@wordpress/i18n';
+import React, { createElement } from 'react';
 
 /**
  * Internal dependencies
  */
-// import { Label } from '../label';
-
-function Label( { children = null } ) {
-	return <div>Label: { children }</div>;
-}
+import { Label, LabelProps } from '../label';
 
 export default {
 	title: 'Product Editor/components/Label',
 	component: Label,
+	args: {
+		label: 'Woo',
+		required: false,
+		tooltip: __( 'Visit woo.com', 'woocommerce' ),
+	},
 };
+
+export const Default = ( args: LabelProps ) => <Label { ...args } />;
