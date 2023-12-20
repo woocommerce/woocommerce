0c3adb8f586733a1e40de9bcecfdbb90f413b081
Gan Eng Chin <ecgan@users.noreply.github.com>
2023-12-21 01:34:55 +0800

    Add spec_key for MarketingRecommendationsDataSourcePoller.
    If we don't specify it, the default is `id`, which is not used in our recommendation data, and this will cause things not to work as expected.

--- plugins/woocommerce/src/Admin/Features/MarketingRecommendations/MarketingRecommendationsDataSourcePoller.php
+++ plugins/woocommerce/src/Admin/Features/MarketingRecommendations/MarketingRecommendationsDataSourcePoller.php
@@ -33,7 +33,9 @@ class MarketingRecommendationsDataSourcePoller extends DataSourcePoller {
 	 */
 	public static function get_instance() {
 		if ( ! self::$instance ) {
-			self::$instance = new self( self::ID, self::DATA_SOURCES );
+			self::$instance = new self( self::ID, self::DATA_SOURCES, array(
+				'spec_key' => 'product',
+			) );
 		}
 		return self::$instance;
 	}
