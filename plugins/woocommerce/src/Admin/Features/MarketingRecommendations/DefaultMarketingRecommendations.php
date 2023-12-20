106685d667544456e578e062b3e8edbf29d88d16
Gan Eng Chin <ecgan@users.noreply.github.com>
2023-12-21 00:39:48 +0800

    Add MarketingRecommendations feature.
    The code here is based on other feature with default fallbacks and with data source poller, e.g. see payment gateway suggestions.

---
+++ plugins/woocommerce/src/Admin/Features/MarketingRecommendations/DefaultMarketingRecommendations.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Gets a list of fallback methods if remote fetching is disabled.
+ */
+
+namespace Automattic\WooCommerce\Admin\Features\MarketingRecommendations;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Default Marketing Recommendations
+ */
+class DefaultMarketingRecommendations {
+	/**
+	 * Get default specs.
+	 *
+	 * @return array Default specs.
+	 */
+	public static function get_all() {
+		// Read the JSON file
+		$json = file_get_contents( __DIR__ . '/recommendations.json', true);
+
+		// Decode the JSON file
+		$json_data = json_decode($json,true);
+
+		return $json_data;
+	}
+}
---
+++ plugins/woocommerce/src/Admin/Features/MarketingRecommendations/Init.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Automattic\WooCommerce\Admin\Features\MarketingRecommendations;
+
+defined( 'ABSPATH' ) || exit;
+
+class Init {
+	/**
+	 * Slug of the category specifying marketing extensions on the Woo.com store.
+	 *
+	 * @var string
+	 */
+	const MARKETING_EXTENSION_CATEGORY_SLUG = 'marketing';
+
+	/**
+	 * Slug of the subcategory specifying marketing channels on the Woo.com store.
+	 *
+	 * @var string
+	 */
+	const MARKETING_CHANNEL_SUBCATEGORY_SLUG = 'sales-channels';
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		add_action( 'woocommerce_updated', array( __CLASS__, 'delete_specs_transient' ) );
+	}
+
+	/**
+	 * Delete the specs transient.
+	 */
+	public static function delete_specs_transient() {
+		MarketingRecommendationsDataSourcePoller::get_instance()->delete_specs_transient();
+	}
+
+	/**
+	 * Get specs or fetch remotely if they don't exist.
+	 */
+	public static function get_specs() {
+		if ( 'no' === get_option( 'woocommerce_show_marketplace_suggestions', 'yes' ) ) {
+			return DefaultMarketingRecommendations::get_all();
+		}
+		$specs = MarketingRecommendationsDataSourcePoller::get_instance()->get_specs_from_data_sources();
+
+		// Fetch specs if they don't yet exist.
+		if ( false === $specs || ! is_array( $specs ) || 0 === count( $specs ) ) {
+			return DefaultMarketingRecommendations::get_all();
+		}
+
+		return $specs;
+	}
+
+
+	/**
+	 * Load recommended plugins from Woo.com
+	 *
+	 * @return array
+	 */
+	public static function get_recommended_plugins(): array {
+		$specs   = self::get_specs();
+		return $specs;
+	}
+
+	/**
+	 * Return only the recommended marketing channels from Woo.com.
+	 *
+	 * @return array
+	 */
+	public static function get_recommended_marketing_channels(): array {
+		return array_filter(
+			self::get_recommended_plugins(),
+			function ( array $plugin_data ) {
+				return self::is_marketing_channel_plugin( $plugin_data );
+			}
+		);
+	}
+
+	/**
+	 * Return all recommended marketing extensions EXCEPT the marketing channels from Woo.com.
+	 *
+	 * @return array
+	 */
+	public static function get_recommended_marketing_extensions_excluding_channels(): array {
+		return array_filter(
+			self::get_recommended_plugins(),
+			function ( array $plugin_data ) {
+				return self::is_marketing_plugin( $plugin_data ) && ! self::is_marketing_channel_plugin( $plugin_data );
+			}
+		);
+	}
+
+	/**
+	 * Returns whether a plugin is a marketing extension.
+	 *
+	 * @param array $plugin_data The plugin properties returned by the API.
+	 *
+	 * @return bool
+	 */
+	protected static function is_marketing_plugin( array $plugin_data ): bool {
+		$categories = $plugin_data['categories'] ?? [];
+
+		return in_array( self::MARKETING_EXTENSION_CATEGORY_SLUG, $categories, true );
+	}
+
+	/**
+	 * Returns whether a plugin is a marketing channel.
+	 *
+	 * @param array $plugin_data The plugin properties returned by the API.
+	 *
+	 * @return bool
+	 */
+	protected static function is_marketing_channel_plugin( array $plugin_data ): bool {
+		if ( ! self::is_marketing_plugin( $plugin_data ) ) {
+			return false;
+		}
+
+		$subcategories = $plugin_data['subcategories'] ?? [];
+		foreach ( $subcategories as $subcategory ) {
+			if ( isset( $subcategory['slug'] ) && self::MARKETING_CHANNEL_SUBCATEGORY_SLUG === $subcategory['slug'] ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
