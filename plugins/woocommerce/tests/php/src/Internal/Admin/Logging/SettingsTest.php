9353db8a767a7df8e99760a2480951fd0ba21680
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2023-12-20 10:43:32 -0800

    Scaffold the unit tests

---
+++ plugins/woocommerce/tests/php/src/Internal/Admin/Logging/SettingsTest.php
@@ -0,0 +1,22 @@
+<?php
+declare( strict_types = 1 );
+
+namespace Automattic\WooCommerce\Tests\Internal\Admin\Logging;
+
+use WC_Unit_Test_Case;
+
+/**
+ * SettingsTest class.
+ */
+class SettingsTest extends WC_Unit_Test_Case {
+
+	public function test_logs_not_created_while_logging_disabled() {}
+
+	public function test_default_handler_setting() {}
+
+	public function test_retention_period_setting() {}
+
+	public function test_level_threshold_setting() {}
+
+	public function test_render_form() {}
+}
