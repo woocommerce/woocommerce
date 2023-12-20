4ba50f070f8e7633bf6b6204a91147bbe4f998a7
Tomek Wytrębowicz <tomalecpub@gmail.com>
2023-12-20 20:18:17 +0100

    Update the function name in `maybe_set_admin_source`

--- plugins/woocommerce/src/Internal/Orders/OrderAttributionController.php
+++ plugins/woocommerce/src/Internal/Orders/OrderAttributionController.php
@@ -175,7 +175,7 @@ class OrderAttributionController implements RegisterHooksInterface {
 	 */
 	private function maybe_set_admin_source( WC_Order $order ) {
 		if ( function_exists( 'is_admin' ) && is_admin() ) {
-			$order->add_meta_data( $this->get_meta_prefixed_field( 'source_type' ), 'admin' );
+			$order->add_meta_data( $this->get_meta_prefixed_field_name( 'source_type' ), 'admin' );
 			$order->save();
 		}
 	}
