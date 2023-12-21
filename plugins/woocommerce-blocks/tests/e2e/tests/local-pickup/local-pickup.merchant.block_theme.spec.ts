6a9ca92732c02ffdc2faa64df5fddbf6bace9e90
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2023-12-21 19:27:16 +0000

    Playwright migrated test file bootstrap and first test for local pickup

---
+++ plugins/woocommerce-blocks/tests/e2e/tests/local-pickup/local-pickup.merchant.block_theme.spec.ts
@@ -0,0 +1,26 @@
+/**
+ * External dependencies
+ */
+import { test, expect } from '@woocommerce/e2e-playwright-utils';
+
+test.describe( 'Merchant → Local Pickup Settings', () => {
+	test.beforeEach( async ( { admin, page } ) => {
+		await admin.visitAdminPage(
+			'admin.php',
+			'page=wc-settings&tab=shipping&section=pickup_location'
+		);
+		await page.getByLabel( 'Enable local pickup' ).check();
+		await page.locator( 'button:text("Save changes")' ).click();
+		await page.waitForSelector(
+			'.components-snackbar__content:has-text("Local Pickup settings have been saved.")'
+		);
+	} );
+
+	test( 'renders without crashing', async ( { page } ) => {
+		await page.waitForSelector( '#local-pickup-settings' );
+		expect( page.locator( '#local-pickup-settings' ) ).not.toBeNull();
+		expect(
+			page.getByLabel( 'Enable local pickup' ).isChecked()
+		).toBeTruthy();
+	} );
+} );
