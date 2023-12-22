f7c9b2beb76e99ad9a5e31cdeac2636621bc3bf6
Matt Sherman <matt@jam123.com>
2023-12-22 16:38:43 -0500

    Code docs for LayoutTemplateRegistry

--- plugins/woocommerce/src/LayoutTemplates/LayoutTemplateRegistry.php
+++ plugins/woocommerce/src/LayoutTemplates/LayoutTemplateRegistry.php
@@ -4,6 +4,9 @@ namespace Automattic\WooCommerce\LayoutTemplates;
 
 use Automattic\WooCommerce\Admin\BlockTemplates\BlockTemplateInterface;
 
+/**
+ * Layout template registry.
+ */
 final class LayoutTemplateRegistry {
 
 	/**
@@ -31,6 +34,11 @@ final class LayoutTemplateRegistry {
 		return self::$instance;
 	}
 
+	/**
+	 * Check if a layout template is registered.
+	 *
+	 * @param string $layout_template_id Layout template ID.
+	 */
 	public function is_registered( $layout_template_id ): bool {
 		return isset( $this->layout_templates_info[ $layout_template_id ] );
 	}
@@ -38,11 +46,14 @@ final class LayoutTemplateRegistry {
 	/**
 	 * Register a single layout template.
 	 *
-	 * @param string                  $layout_template_id    Layout template ID.
-	 * @param string                  $layout_template_area  Layout template area.
-	 * @param LayoutTemplateInterface $template Layout template to register.
+	 * @param string $layout_template_id         Layout template ID.
+	 * @param string $layout_template_area       Layout template area.
+	 * @param string $layout_template_class_name Layout template class to register.
 	 *
 	 * @throws \ValueError If a layout template with the same ID already exists.
+	 * @throws \ValueError If the specified layout template area is empty.
+	 * @throws \ValueError If the specified layout template class does not exist.
+	 * @throws \ValueError If the specified layout template class does not implement the BlockTemplateInterface.
 	 */
 	public function register( $layout_template_id, $layout_template_area, $layout_template_class_name ) {
 		if ( $this->is_registered( $layout_template_id ) ) {
@@ -67,6 +78,11 @@ final class LayoutTemplateRegistry {
 		);
 	}
 
+	/**
+	 * Instantiate the matching layout templates and return them.
+	 *
+	 * @param array $query_params Query params.
+	 */
 	public function instantiate_layout_templates( array $query_params = array() ): array {
 		$layout_templates = array();
 
@@ -82,7 +98,7 @@ final class LayoutTemplateRegistry {
 	/**
 	 * Get matching layout template class names.
 	 *
-	 * @return string[]
+	 * @param array $query_params Query params.
 	 */
 	private function get_class_names( array $query_params = array() ): array {
 		$area_to_match = isset( $query_params['area'] ) ? $query_params['area'] : null;
