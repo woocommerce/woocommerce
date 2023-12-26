25dd6418526a1424b1ad974c5233a547bc6e9749
Damián Suárez <rdsuarez@gmail.com>
2023-12-26 18:51:25 -0300

    introduce CashRegister image component

---
+++ packages/js/product-editor/src/images/cash-register/stories/index.tsx
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import { CashRegister } from '../index';
+
+export default {
+	title: 'Product Editor/images/CashRegister',
+	component: CashRegister,
+	args: {
+		size: '88',
+		colorOne: '#E0E0E0',
+		colorTwo: '#F0F0F0',
+	},
+};
+
+export const Default = CashRegister;
