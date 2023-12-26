95597093d253740c44542018db694277335509a5
Damián Suárez <rdsuarez@gmail.com>
2023-12-26 10:38:09 -0300

    sort exporting blocks

--- packages/js/product-editor/src/blocks/index.ts
+++ packages/js/product-editor/src/blocks/index.ts
@@ -1,36 +1,43 @@
-export { init as initCatalogVisibility } from './product-fields/catalog-visibility';
+/**
+ * Generic blocks
+ */
 export { init as initCheckbox } from './generic/checkbox';
 export { init as initCollapsible } from './generic/collapsible';
 export { init as initConditional } from './generic/conditional';
+export { init as initNumber } from './generic/number';
+export { init as initPricing } from './generic/pricing';
+export { init as initRadio } from './generic/radio';
+export { init as initSection } from './generic/section';
+export { init as initSectionDescription } from './generic/section-description';
+export { init as initTab } from './generic/tab';
+export { init as initTaxonomy } from './generic/taxonomy';
+export { init as initText } from './generic/text';
+export { init as initToggle } from './generic/toggle';
+
+/*
+ * Product Field blocks
+ */
+export { init as attributesInit } from './product-fields/attributes';
+export { init as initCatalogVisibility } from './product-fields/catalog-visibility';
 export { init as initDescription } from './product-fields/description';
 export { init as initDownloads } from './product-fields/downloads';
 export { init as initImages } from './product-fields/images';
-export { init as initLowStockQty } from './product-fields/inventory-email';
-export { init as initSku } from './product-fields/inventory-sku';
-export { init as initName } from './product-fields/name';
-export { init as initPricing } from './generic/pricing';
-export { init as initRadio } from './generic/radio';
-export { init as initRegularPrice } from './product-fields/regular-price';
-export { init as initSalePrice } from './product-fields/sale-price';
-export { init as initScheduleSale } from './product-fields/schedule-sale';
-export { init as initSection } from './generic/section';
-export { init as initSectionDescription } from './generic/section-description';
-export { init as initShippingClass } from './product-fields/shipping-class';
-export { init as initShippingDimensions } from './product-fields/shipping-dimensions';
-export { init as initSummary } from './product-fields/summary';
-export { init as initTab } from './generic/tab';
-export { init as initTag } from './product-fields/tag';
 export { init as initInventoryQuantity } from './product-fields/inventory-quantity';
-export { init as initToggle } from './generic/toggle';
-export { init as attributesInit } from './product-fields/attributes';
-export { init as initVariations } from './product-fields/variations';
-export { init as initRequirePassword } from './product-fields/password';
-export { init as initProductDetailsSectionDescription } from './product-fields/product-details-section-description';
-export { init as initProductList } from './product-fields/product-list';
-export { init as initVariationItems } from './product-fields/variation-items';
-export { init as initVariationOptions } from './product-fields/variation-options';
+export { init as initLowStockQty } from './product-fields/inventory-email';
+export { init as initName } from './product-fields/name';
 export { init as initNotice } from './product-fields/notice-edit-single-variation';
 export { init as initNoticeHasVariations } from './product-fields/notice-has-variations';
-export { init as initTaxonomy } from './generic/taxonomy';
-export { init as initText } from './generic/text';
-export { init as initNumber } from './generic/number';
+export { init as initProductDetailsSectionDescription } from './product-fields/product-details-section-description';
+export { init as initProductList } from './product-fields/product-list';
+export { init as initRegularPrice } from './product-fields/regular-price';
+export { init as initRequirePassword } from './product-fields/password';
+export { init as initSalePrice } from './product-fields/sale-price';
+export { init as initScheduleSale } from './product-fields/schedule-sale';
+export { init as initShippingClass } from './product-fields/shipping-class';
+export { init as initShippingDimensions } from './product-fields/shipping-dimensions';
+export { init as initSku } from './product-fields/inventory-sku';
+export { init as initSummary } from './product-fields/summary';
+export { init as initTag } from './product-fields/tag';
+export { init as initVariationItems } from './product-fields/variation-items';
+export { init as initVariationOptions } from './product-fields/variation-options';
+export { init as initVariations } from './product-fields/variations';
