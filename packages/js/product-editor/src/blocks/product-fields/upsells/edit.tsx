609d8a63e1d956843f77637abe6e58e20f63d8f6
Damián Suárez <rdsuarez@gmail.com>
2023-12-26 11:27:09 -0300

    render empty state for the Upsells section

--- packages/js/product-editor/src/blocks/product-fields/upsells/edit.tsx
+++ packages/js/product-editor/src/blocks/product-fields/upsells/edit.tsx
@@ -3,14 +3,28 @@
  */
 import { createElement } from '@wordpress/element';
 import { useWooBlockProps } from '@woocommerce/block-templates';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
+import { AdviceCard } from '../../../components/advice-card';
+import { ShoppingBags } from '../../../images/shopping-bugs';
 import type { UpsellsBlockEditComponent } from './types';
 
 export function UpsellsBlockEdit( { attributes }: UpsellsBlockEditComponent ) {
 	const blockProps = useWooBlockProps( attributes );
 
-	return <div { ...blockProps }></div>;
+	return (
+		<div { ...blockProps }>
+			<AdviceCard
+				tip={ __(
+					'Tip: Upsells are typically products that are extra profitable or better quality or more expensive. Experiment with combinations to boost sales.',
+					'woocommerce'
+				) }
+				image={ <ShoppingBags /> }
+				isDismissible={ true }
+			/>
+		</div>
+	);
 }
