068f263ff7835f599d43f492cc36e413b051ac22
Damián Suárez <rdsuarez@gmail.com>
2023-12-26 15:41:24 -0300

    introduce UserPreference panel

---
+++ plugins/woocommerce-beta-tester/src/product-editor-dev-tools/user-preferences-panel.tsx
@@ -0,0 +1,66 @@
+/**
+ * External dependencies
+ */
+import { useUserPreferences, UserPreferences } from '@woocommerce/data';
+import { useCallback } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { TabPanel } from './tab-panel';
+
+declare global {
+	interface Window {
+		__wca: object;
+	}
+}
+
+type UserPreferenceProp = keyof UserPreferences;
+
+export function UserPreferencesTabPanel( {
+	isSelected,
+}: {
+	isSelected: boolean;
+} ) {
+	const { updateUserPreferences, ...userPreferences } = useUserPreferences();
+
+	const update = useCallback(
+		( preferences: UserPreferences | UserPreferenceProp, value? ) => {
+			const dataToUpdate =
+				typeof preferences === 'string'
+					? { [ preferences ]: value }
+					: preferences;
+
+			/*
+			 * Only update the preferences already defined.
+			 * @todo: consider to implement straight in the data layer.
+			 */
+			Object.keys( dataToUpdate ).forEach( ( key: string ) => {
+				const userPreferenceKey = key as UserPreferenceProp;
+				if ( ! userPreferences.hasOwnProperty( userPreferenceKey ) ) {
+					delete dataToUpdate[ userPreferenceKey ];
+				}
+			} );
+
+			updateUserPreferences( dataToUpdate );
+		},
+		[ updateUserPreferences, userPreferences ]
+	);
+
+	// Expose updateUserPreferences globaly for debugging purposes.
+	window.__wca = {
+		...window.__wca,
+		updateUserPreferences: update,
+		userPreferences,
+	};
+
+	return (
+		<TabPanel isSelected={ isSelected }>
+			<div className="woocommerce-product-editor-dev-tools-user-preferences">
+				<div className="woocommerce-product-editor-dev-tools-user-preferences-entity">
+					{ JSON.stringify( userPreferences, null, 4 ) }
+				</div>
+			</div>
+		</TabPanel>
+	);
+}
