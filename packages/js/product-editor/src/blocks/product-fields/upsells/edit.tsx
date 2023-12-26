e83bc04d9864a1c063bc1d67417929033800b9d2
Damián Suárez <rdsuarez@gmail.com>
2023-12-26 11:41:41 -0300

    iadd hardcoded isEmpty flag

--- packages/js/product-editor/src/blocks/product-fields/upsells/edit.tsx
+++ packages/js/product-editor/src/blocks/product-fields/upsells/edit.tsx
@@ -15,16 +15,22 @@ import type { UpsellsBlockEditComponent } from './types';
 export function UpsellsBlockEdit( { attributes }: UpsellsBlockEditComponent ) {
 	const blockProps = useWooBlockProps( attributes );
 
-	return (
-		<div { ...blockProps }>
-			<AdviceCard
-				tip={ __(
-					'Tip: Upsells are typically products that are extra profitable or better quality or more expensive. Experiment with combinations to boost sales.',
-					'woocommerce'
-				) }
-				image={ <ShoppingBags /> }
-				isDismissible={ true }
-			/>
-		</div>
-	);
+	const isEmpty = true; // @todo: implement.
+
+	if ( isEmpty ) {
+		return (
+			<div { ...blockProps }>
+				<AdviceCard
+					tip={ __(
+						'Upsells are products that are extra profitable or better quality or more expensive. Experiment with combinations to boost sales.',
+						'woocommerce'
+					) }
+					image={ <ShoppingBags /> }
+					isDismissible={ true }
+				/>
+			</div>
+		);
+	}
+
+	return <div { ...blockProps } />;
 }
