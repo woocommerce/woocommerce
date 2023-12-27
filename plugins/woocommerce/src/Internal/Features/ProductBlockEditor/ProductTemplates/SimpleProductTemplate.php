4ed01dedda4fd489e7ab3e2258ee232dad3bd951
Damián Suárez <rdsuarez@gmail.com>
2023-12-27 15:36:03 -0300

    add Cross-sells block instance into the Cross-sell section

--- plugins/woocommerce/src/Internal/Features/ProductBlockEditor/ProductTemplates/SimpleProductTemplate.php
+++ plugins/woocommerce/src/Internal/Features/ProductBlockEditor/ProductTemplates/SimpleProductTemplate.php
@@ -1141,7 +1141,7 @@ class SimpleProductTemplate extends AbstractProductFormTemplate implements Produ
 			)
 		);
 
-		$linked_products_group->add_section(
+		$linked_product_cross_sells_section = $linked_products_group->add_section(
 			array(
 				'id'             => 'product-linked-cross-sells-section',
 				'order'          => 20,
@@ -1158,6 +1158,17 @@ class SimpleProductTemplate extends AbstractProductFormTemplate implements Produ
 					array(
 						'expression' => 'editedProduct.type === "external" || editedProduct.type === "grouped"',
 					),
+				),
+			)
+		);
+
+		$linked_product_cross_sells_section->add_block(
+			array(
+				'id'         => 'product-linked-cross-sells',
+				'blockName'  => 'woocommerce/product-cross-sells',
+				'order'      => 10,
+				'attributes' => array(
+					'property' => 'upsell_ids',
 				),
 			)
 		);
