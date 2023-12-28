1f4cd241293b191b3b8d77bf312efe4048535bc7
Niels Lange <info@nielslange.de>
2023-12-28 12:07:34 +0700

    Show rating stars for the cross-sells products

--- plugins/woocommerce-blocks/assets/js/blocks/cart/cart-cross-sells-product-list/cart-cross-sells-product.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart/cart-cross-sells-product-list/cart-cross-sells-product.tsx
@@ -12,7 +12,7 @@ import { ProductResponseItem } from '@woocommerce/types';
  */
 import { Block as ProductImage } from '../../../atomic/blocks/product-elements/image/block';
 import { Block as ProductName } from '../../../atomic/blocks/product-elements/title/block';
-import { Block as ProductRating } from '../../../atomic/blocks/product-elements/rating/block';
+import { Block as ProductRating } from '../../../atomic/blocks/product-elements/rating-stars/block';
 import { Block as ProductPrice } from '../../../atomic/blocks/product-elements/price/block';
 import { Block as ProductButton } from '../../../atomic/blocks/product-elements/button/block';
 import { ImageSizing } from '../../../atomic/blocks/product-elements/image/types';
@@ -32,7 +32,6 @@ const CartCrossSellsProduct = ( {
 				parentClassName={ 'wp-block-cart-cross-sells-product' }
 			>
 				<ProductDataContextProvider
-					// Setting isLoading to false, given this parameter is required.
 					isLoading={ false }
 					product={ product }
 				>
@@ -45,13 +44,23 @@ const CartCrossSellsProduct = ( {
 							saleBadgeAlign={ 'left' }
 							imageSizing={ ImageSizing.SINGLE }
 							isDescendentOfQueryLoop={ false }
+							scale={ 'cover' }
+							aspectRatio={ '1:1' }
 						/>
 						<ProductName
 							align={ '' }
 							headingLevel={ 3 }
 							showProductLink={ true }
 						/>
-						<ProductRating />
+						<ProductRating
+							isDescendentOfQueryLoop={ false }
+							isDescendentOfSingleProductBlock={ false }
+							productId={ product.id }
+							postId={ 0 }
+							shouldDisplayMockedReviewsWhenProductHasNoReviews={
+								false
+							}
+						/>
 						<ProductPrice />
 					</div>
 					<ProductButton />
