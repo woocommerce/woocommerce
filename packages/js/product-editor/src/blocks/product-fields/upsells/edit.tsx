woocommerce/woocommerce

commit 35a4f077cf9145f2abdb19d5fb522859b35a4867

[Product Block Editor]: render empty state for the Upsells section (#43116)
* Upsells block: scafolding

* export Upsells block

* add Upsells block into the Upsells section

* render empty state for the Upsells section

* iadd hardcoded isEmpty flag

* changelog

packages/js/product-editor/src/blocks/product-fields/upsells/edit.tsx
@@ -1,0 +1,36 @@
+/**
+ * External dependencies
+ */
+import { createElement } from '@wordpress/element';
+import { useWooBlockProps } from '@woocommerce/block-templates';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { AdviceCard } from '../../../components/advice-card';
+import { ShoppingBags } from '../../../images/shopping-bugs';
+import type { UpsellsBlockEditComponent } from './types';
+
+export function UpsellsBlockEdit( { attributes }: UpsellsBlockEditComponent ) {
+	const blockProps = useWooBlockProps( attributes );
+
+	const isEmpty = true; // @todo: implement.
+
+	if ( isEmpty ) {
+		return (
+			<div { ...blockProps }>
+				<AdviceCard
+					tip={ __(
+						'Upsells are products that are extra profitable or better quality or more expensive. Experiment with combinations to boost sales.',
+						'woocommerce'
+					) }
+					image={ <ShoppingBags /> }
+					isDismissible={ true }
+				/>
+			</div>
+		);
+	}
+
+	return <div { ...blockProps } />;
+}

packages/js/product-editor/src/blocks/index.ts
@@ -34,3 +34,4 @@ export { init as initNoticeHasVariations } from './product-fields/notice-has-var
 export { init as initTaxonomy } from './generic/taxonomy';
 export { init as initText } from './generic/text';
 export { init as initNumber } from './generic/number';
+export { init as initUpsells } from './product-fields/upsells';

packages/js/product-editor/src/blocks/product-fields/upsells/index.ts
@@ -1,0 +1,22 @@
+/**
+ * Internal dependencies
+ */
+import blockConfiguration from './block.json';
+import { UpsellsBlockEdit } from './edit';
+import { registerProductEditorBlockType } from '../../../utils';
+
+const { name, ...metadata } = blockConfiguration;
+
+export { metadata, name };
+
+export const settings = {
+	example: {},
+	edit: UpsellsBlockEdit,
+};
+
+export const init = () =>
+	registerProductEditorBlockType( {
+		name,
+		metadata: metadata as never,
+		settings: settings as never,
+	} );

packages/js/product-editor/src/blocks/product-fields/upsells/types.ts
@@ -1,0 +1,19 @@
+/**
+ * External dependencies
+ */
+import {
+	// @ts-expect-error no exported member.
+	ComponentType,
+} from '@wordpress/element';
+/**
+ * Internal dependencies
+ */
+import {
+	ProductEditorBlockAttributes,
+	ProductEditorBlockEditProps,
+} from '../../../types';
+
+export type UpsellsBlockEditProps =
+	ProductEditorBlockEditProps< ProductEditorBlockAttributes >;
+
+export type UpsellsBlockEditComponent = ComponentType< UpsellsBlockEditProps >;

plugins/woocommerce/src/Internal/Features/ProductBlockEditor/ProductTemplates/SimpleProductTemplate.php
@@ -1114,7 +1114,7 @@ class SimpleProductTemplate extends AbstractProductFormTemplate implements Produ
 			return;
 		}
 
-		$linked_products_group->add_section(
+		$linked_product_upsells_section = $linked_products_group->add_section(
 			array(
 				'id'         => 'product-linked-upsells-section',
 				'order'      => 10,
@@ -1126,6 +1126,17 @@ class SimpleProductTemplate extends AbstractProductFormTemplate implements Produ
 						'<br /><a href="https://woo.com/document/related-products-up-sells-and-cross-sells/" target="_blank" rel="noreferrer">',
 						'</a>'
 					),
+				),
+			)
+		);
+
+		$linked_product_upsells_section->add_block(
+			array(
+				'id'         => 'product-linked-upsells',
+				'blockName'  => 'woocommerce/product-upsells',
+				'order'      => 10,
+				'attributes' => array(
+					'property' => 'upsell_ids',
 				),
 			)
 		);

commit ffda83746546ad1875c8f864f5cdacaeb620eb50

Fix typo introduced in #43116

packages/js/product-editor/src/blocks/product-fields/upsells/edit.tsx
@@ -9,7 +9,7 @@ import { __ } from '@wordpress/i18n';
  * Internal dependencies
  */
 import { AdviceCard } from '../../../components/advice-card';
-import { ShoppingBags } from '../../../images/shopping-bugs';
+import { ShoppingBags } from '../../../images/shopping-bags';
 import type { UpsellsBlockEditComponent } from './types';
 
 export function UpsellsBlockEdit( { attributes }: UpsellsBlockEditComponent ) {