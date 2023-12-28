dac47426848071a7703f707f6fbf0b14a38605d6
Matt Sherman <matt@jam123.com>
2023-12-28 15:54:10 -0500

    Unit tests for invalid params when registering layout template

--- plugins/woocommerce/tests/php/src/LayoutTemplates/LayoutTemplateRegistryTest.php
+++ plugins/woocommerce/tests/php/src/LayoutTemplates/LayoutTemplateRegistryTest.php
@@ -34,4 +34,37 @@ class LayoutTemplateRegistryTest extends WC_Unit_Test_Case {
 
 		$this->assertTrue( $this->layout_template_registry->is_registered( 'test-layout-template' ) );
 	}
+
+	/**
+	 * Test registering a layout template with an existing ID.
+	 */
+	public function test_register_duplicate_id() {
+		$this->expectException( \ValueError::class );
+		$this->layout_template_registry->register( 'test-layout-template', 'test', TestLayoutTemplate::class );
+		$this->layout_template_registry->register( 'test-layout-template', 'test', TestLayoutTemplate::class );
+	}
+
+	/**
+	 * Test registering a layout template with an empty area.
+	 */
+	public function test_register_empty_area() {
+		$this->expectException( \ValueError::class );
+		$this->layout_template_registry->register( 'test-layout-template', '', TestLayoutTemplate::class );
+	}
+
+	/**
+	 * Test registering a layout template with a non-existing class.
+	 */
+	public function test_register_non_existing_class() {
+		$this->expectException( \ValueError::class );
+		$this->layout_template_registry->register( 'test-layout-template', 'test', 'NonExistingClass' );
+	}
+
+	/**
+	 * Test registering a layout template with a class that does not implement the BlockTemplateInterface.
+	 */
+	public function test_register_non_block_template_class() {
+		$this->expectException( \ValueError::class );
+		$this->layout_template_registry->register( 'test-layout-template', 'test', \stdClass::class );
+	}
 }
