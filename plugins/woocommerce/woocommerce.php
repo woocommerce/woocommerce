5ab7ebfc02d0e301921fb08e951fdbdc4aff5d40
Alex López <alex.lopez@automattic.com>
2024-01-02 13:15:46 +0100

    Prep release/8.5 for 8.5 cycle with version bump to 8.5.0-rc.1

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woo.com/
  * Description: An eCommerce toolkit that helps you sell anything. Beautifully.
- * Version: 8.5.0-beta.1
+ * Version: 8.5.0-rc.1
  * Author: Automattic
  * Author URI: https://woo.com
  * Text Domain: woocommerce
