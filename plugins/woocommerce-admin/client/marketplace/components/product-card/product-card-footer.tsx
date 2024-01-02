9115856a105fd9740267bf9187e5ca9a6a43b5ff
Kyle Nel <22053773+kdevnel@users.noreply.github.com>
2024-01-02 16:10:50 +0200

    Add rating and change product card layout

---
+++ plugins/woocommerce-admin/client/marketplace/components/product-card/product-card-footer.tsx
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+import { Icon } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+export interface ProductCardFooterProps {
+	price: number | null | undefined;
+	currencySymbol: string;
+	averageRating: number | null | undefined;
+	reviewsCount: number | null | undefined;
+}
+
+function ProductCardFooter( props: ProductCardFooterProps ) {
+	const { price, currencySymbol, averageRating, reviewsCount } = props;
+	return (
+		<footer className="woocommerce-marketplace__product-card-footer">
+			<div className="woocommerce-marketplace__product-card__price">
+				<span className="woocommerce-marketplace__product-card__price-label">
+					{
+						// '0' is a free product
+						price === 0
+							? __( 'Free download', 'woocommerce' )
+							: currencySymbol + price
+					}
+				</span>
+				<span className="woocommerce-marketplace__product-card__price-billing">
+					{ props.price === 0
+						? ''
+						: __( ' annually', 'woocommerce' ) }
+				</span>
+			</div>
+			<div className="woocommerce-marketplace__product-card__rating">
+				{ averageRating !== null && (
+					<>
+						<span className="woocommerce-marketplace__product-card__rating-icon">
+							<Icon icon={ 'star-filled' } size={ 16 } />
+						</span>
+						<span className="woocommerce-marketplace__product-card__rating-average">
+							{ averageRating }
+						</span>
+						<span className="woocommerce-marketplace__product-card__rating-count">
+							({ reviewsCount })
+						</span>
+					</>
+				) }
+			</div>
+		</footer>
+	);
+}
+
+export default ProductCardFooter;
