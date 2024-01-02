26b837d1bd978e37e87b9239e7d07ec09ac4f08b
Sam Seay <samueljseay@gmail.com>
2024-01-02 13:02:33 +0800

    Register rating filter wrapper block.

--- plugins/woocommerce-blocks/assets/js/blocks/collection-filters/inner-blocks/rating-filter/index.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/collection-filters/inner-blocks/rating-filter/index.tsx
@@ -4,25 +4,43 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { Icon, starEmpty } from '@wordpress/icons';
 import { InnerBlocks } from '@wordpress/block-editor';
+import { isExperimentalBuild } from '@woocommerce/block-settings';
 
 /**
  * Internal dependencies
  */
 import edit from './edit';
 import metadata from './block.json';
+import wrapperMetadata from './wrapper/block.json';
+import EditWrapper from './wrapper/edit';
 
-registerBlockType( metadata, {
-	icon: {
-		src: (
-			<Icon
-				icon={ starEmpty }
-				className="wc-block-editor-components-block-icon"
-			/>
-		),
-	},
-	attributes: {
-		...metadata.attributes,
-	},
-	edit,
-	save: InnerBlocks.Content,
-} );
+if ( isExperimentalBuild() ) {
+	registerBlockType( metadata, {
+		icon: {
+			src: (
+				<Icon
+					icon={ starEmpty }
+					className="wc-block-editor-components-block-icon"
+				/>
+			),
+		},
+		attributes: {
+			...metadata.attributes,
+		},
+		edit,
+		save: InnerBlocks.Content,
+	} );
+
+	registerBlockType( wrapperMetadata, {
+		icon: {
+			src: (
+				<Icon
+					icon={ starEmpty }
+					className="wc-block-editor-components-block-icon"
+				/>
+			),
+		},
+		edit: EditWrapper,
+		save: InnerBlocks.Content,
+	} );
+}
