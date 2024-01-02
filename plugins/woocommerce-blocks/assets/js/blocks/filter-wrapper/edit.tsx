0a55f669d3907b4d121b8f52b1ed05adf141c087
Tung Du <dinhtungdu@gmail.com>
2024-01-02 11:33:12 +0700

    add: upgrade panel

--- plugins/woocommerce-blocks/assets/js/blocks/filter-wrapper/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/filter-wrapper/edit.tsx
@@ -1,19 +1,81 @@
 /**
  * External dependencies
  */
-import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
-import type { BlockEditProps } from '@wordpress/blocks';
+import {
+	useBlockProps,
+	InnerBlocks,
+	InspectorControls,
+} from '@wordpress/block-editor';
+import { createBlock, type BlockEditProps } from '@wordpress/blocks';
+import { PanelBody, Button } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { useCallback } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { Attributes } from './types';
 
-const Edit = ( { attributes }: BlockEditProps< Attributes > ) => {
+const Edit = ( { attributes, clientId }: BlockEditProps< Attributes > ) => {
+	const block = useSelect( ( select ) => {
+		return select( 'core/block-editor' ).getBlock( clientId );
+	} );
+	const { replaceBlock } = useDispatch( 'core/block-editor' );
+
+	const upgradeBlock = useCallback( () => {
+		if ( ! block || ! block.innerBlocks ) return;
+		const filterType = block.attributes.filterType;
+		const filterBlock = block.innerBlocks.find(
+			( item ) => item.name === `woocommerce/${ filterType }`
+		);
+		if ( ! filterBlock ) return;
+
+		const { lock, ...filterBlockAttributes } = filterBlock.attributes;
+		const headingBlock = block.innerBlocks.find(
+			( item ) => item.name === 'core/heading'
+		);
+
+		replaceBlock(
+			clientId,
+			createBlock( `woocommerce/collection-filters`, {}, [
+				createBlock(
+					`woocommerce/collection-${ filterType }`,
+					filterBlockAttributes,
+					headingBlock
+						? [
+								createBlock(
+									'core/heading',
+									headingBlock.attributes
+								),
+						  ]
+						: []
+				),
+			] )
+		);
+	}, [ block, clientId, replaceBlock ] );
+
 	const blockProps = useBlockProps();
 
 	return (
 		<div { ...blockProps }>
+			<InspectorControls key="inspector">
+				<PanelBody title={ __( 'Block Upgrade', 'woocommerce' ) }>
+					<p>
+						{ __(
+							'We have created new filter blocks with better performance and flexibility.',
+							'woocommerce'
+						) }
+					</p>
+					<Button
+						variant="primary"
+						size="small"
+						onClick={ upgradeBlock }
+					>
+						{ __( 'Upgrade', 'woocommerce' ) }
+					</Button>
+				</PanelBody>
+			</InspectorControls>
 			<InnerBlocks
 				allowedBlocks={ [ 'core/heading' ] }
 				template={ [
