b763261947c44f3013539c5299fbfb2adb338629
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-01-02 18:47:54 +0000

    WIP: figuring out flaky state of local pickup enabled

--- plugins/woocommerce-blocks/tests/e2e/tests/local-pickup/local-pickup.merchant.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/local-pickup/local-pickup.merchant.block_theme.spec.ts
@@ -15,48 +15,58 @@ test.describe( 'Merchant → Local Pickup Settings', () => {
 	} );
 
 	test( 'Renders without crashing, and settings persist', async ( {
+		admin,
 		page,
 	} ) => {
-		await page.reload( { waitUntil: 'load' } );
+		await admin.visitAdminPage(
+			'admin.php',
+			'page=wc-settings&tab=shipping&section=pickup_location'
+		);
 		await page.waitForSelector( '#local-pickup-settings' );
 		expect( page.locator( '#local-pickup-settings' ) ).not.toBeNull();
-		const checkboxSelector = '#inspector-checkbox-control-0';
-		// eslint-disable-next-line playwright/no-eval
-		const isChecked = await page.$eval(checkboxSelector, checkbox => checkbox.checked);
-		expect( isChecked ).toBe( true );
-		// expect(
-		// 	page.locator( '#inspector-checkbox-control-0' ).isChecked()
-		// ).toBeTruthy();
+		expect(
+			await page.locator( '#inspector-checkbox-control-0' ).isChecked()
+		).toBeTruthy();
 	} );
 
 	test.describe( 'Core Settings', () => {
+		test.beforeEach( async ( { admin, page } ) => {
+			await utils.clearLocations( admin, page );
+			await utils.enableLocalPickup( admin, page );
+		} );
+
 		test( 'Shows the correct shipping options depending on whether Local Pickup is enabled', async ( {
 			admin,
 			page,
 		} ) => {
 			await utils.disableLocalPickup( admin, page );
-			console.log( await page.locator( '#inspector-checkbox-control-0' ).isChecked());
+
+			await admin.visitAdminPage(
+				'admin.php',
+				'page=wc-settings&tab=shipping&section=pickup_location'
+			);
+
 			expect(
-				page.locator( '#inspector-checkbox-control-0' ).isChecked()
+				await page.locator( '#inspector-checkbox-control-0' ).isChecked()
 			).toBeFalsy();
-			// await visitAdminPage(
-			// 	'admin.php',
-			// 	'page=wc-settings&tab=shipping&section=options'
-			// );
-			// const hideShippingLabel = await page.$x(
-			// 	'//label[contains(., "Hide shipping costs until an address is entered")]'
-			// );
-			// await expect( hideShippingLabel ).toHaveLength( 1 );
-			//
-			// await merchant.enableLocalPickup();
-			// await visitAdminPage(
-			// 	'admin.php',
-			// 	'page=wc-settings&tab=shipping&section=options'
-			// );
-			// const modifiedHideShippingLabel = await page.$x(
-			// 	'//label[contains(., "Hide shipping costs until an address is entered (Not available when using WooCommerce Blocks Local Pickup)")]'
-			// );
-			// await expect( modifiedHideShippingLabel ).toHaveLength( 1 );
+
+			await admin.visitAdminPage(
+				'admin.php',
+				'page=wc-settings&tab=shipping&section=options'
+			);
+			expect(
+				await page.locator( '#woocommerce_shipping_cost_requires_address' )
+			).not.toBeNull();
+
+			await utils.enableLocalPickup( admin, page );
+
+			await admin.visitAdminPage(
+				'admin.php',
+				'page=wc-settings&tab=shipping&section=options'
+			);
+			expect(
+				await page.locator( '#woocommerce_shipping_cost_requires_address' )
+			).not.toBeNull();
 		} );
 	} );
 } );
--- plugins/woocommerce-blocks/tests/e2e/tests/local-pickup/utils.local-pickup.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/local-pickup/utils.local-pickup.ts
@@ -1,10 +1,15 @@
 import { expect } from '@woocommerce/e2e-playwright-utils';
 
 export const utilsLocalPickup = {
-	savelocalPickupSettings: async ( page ) => {
+	savelocalPickupSettings: async ( admin, page ) => {
 		await page.locator( 'button:text("Save changes")' ).click();
 		await page.waitForSelector(
 			'.components-snackbar__content:has-text("Local Pickup settings have been saved.")'
+		);
+		// Refresh page.
+		await admin.visitAdminPage(
+			'admin.php',
+			'page=wc-settings&tab=shipping&section=pickup_location'
 		);
 	},
 	enableLocalPickup: async ( admin, page ) => {
@@ -12,11 +17,11 @@ export const utilsLocalPickup = {
 			'admin.php',
 			'page=wc-settings&tab=shipping&section=pickup_location'
 		);
-		const checkbox = page.locator( '#inspector-checkbox-control-0' );
+		const checkbox = await page.locator( '#inspector-checkbox-control-0' );
 
 		if ( ! checkbox.isChecked() ) {
 			await checkbox.click();
-			await utilsLocalPickup.savelocalPickupSettings( page );
+			await utilsLocalPickup.savelocalPickupSettings( admin, page );
 		}
 	},
 	disableLocalPickup: async ( admin, page ) => {
@@ -25,11 +30,11 @@ export const utilsLocalPickup = {
 			'page=wc-settings&tab=shipping&section=pickup_location'
 		);
 
-		const checkbox = page.locator( '#inspector-checkbox-control-0' );
+		const checkbox = await page.locator( '#inspector-checkbox-control-0' );
 
 		if ( checkbox.isChecked() ) {
 			await checkbox.click();
-			await utilsLocalPickup.savelocalPickupSettings( page );
+			await utilsLocalPickup.savelocalPickupSettings( admin, page );
 		}
 	},
 	clearLocations: async ( admin, page ) => {
@@ -55,6 +60,6 @@ export const utilsLocalPickup = {
 		await page.waitForSelector(
 			'.pickup-locations tbody tr td:has-text("When you add a pickup location, it will appear here.")'
 		);
-		await utilsLocalPickup.savelocalPickupSettings( page );
+		await utilsLocalPickup.savelocalPickupSettings( admin, page );
 	},
 };
