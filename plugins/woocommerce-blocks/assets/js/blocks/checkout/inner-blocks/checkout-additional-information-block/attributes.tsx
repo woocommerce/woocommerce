54bfcb96b3d261f6c8401f533cf52f6d5e29c2f9
Thomas Roberts <thomas.roberts@automattic.com>
2024-01-03 15:26:43 +0000

    Add attributes for form step and add them to block registration

---
+++ plugins/woocommerce-blocks/assets/js/blocks/checkout/inner-blocks/checkout-additional-information-block/attributes.tsx
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import formStepAttributes from '../../form-step/attributes';
+
+export default {
+	...formStepAttributes( {
+		defaultTitle: __( 'Additional order information', 'woocommerce' ),
+		defaultDescription: __(
+			'Enter any additional information about your order.',
+			'woocommerce'
+		),
+	} ),
+	className: {
+		type: 'string',
+		default: '',
+	},
+	lock: {
+		type: 'object',
+		default: {
+			move: true,
+			remove: true,
+		},
+	},
+};
