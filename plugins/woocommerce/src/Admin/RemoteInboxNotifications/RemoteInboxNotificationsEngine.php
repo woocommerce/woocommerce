2e3d808dc48c4b81c50e13cb428097befaa04643
Alfredo Sumaran <a.sumaran@gmail.com>
2024-01-02 20:41:10 -0500

    PHP8 fixes for src/Admin

--- plugins/woocommerce/src/Admin/RemoteInboxNotifications/RemoteInboxNotificationsEngine.php
+++ plugins/woocommerce/src/Admin/RemoteInboxNotifications/RemoteInboxNotificationsEngine.php
@@ -113,7 +113,7 @@ class RemoteInboxNotificationsEngine extends RemoteSpecsEngine {
 	public static function run() {
 		$specs = DataSourcePoller::get_instance()->get_specs_from_data_sources();
 
-		if ( $specs === false || count( $specs ) === 0 ) {
+		if ( $specs === false || ! is_countable( $specs ) || count( $specs ) === 0 ) {
 			return;
 		}
 
