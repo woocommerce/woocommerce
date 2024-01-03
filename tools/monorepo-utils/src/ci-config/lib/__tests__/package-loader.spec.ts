aab54de59077329c0eb872a15fcb8400526d8731
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2024-01-03 09:57:38 -0800

    Added Package Loader
    This is a convenience method for loading the package JSON file with support for caching the
    files that have already been imported.

---
+++ tools/monorepo-utils/src/ci-config/lib/__tests__/package-loader.spec.ts
@@ -0,0 +1,76 @@
+/**
+ * External dependencies
+ */
+import fs from 'fs';
+
+/**
+ * Internal dependencies
+ */
+import { loadPackage } from '../package-loader';
+
+jest.mock( 'fs' );
+
+describe( 'Package Loader', () => {
+	describe( 'loadPackage', () => {
+		it( "should throw for file that doesn't exist", () => {
+			jest.mocked( fs.readFileSync ).mockImplementation( ( path ) => {
+				if ( path === 'foo' ) {
+					throw new Error( 'ENOENT' );
+				}
+
+				return '';
+			} );
+
+			expect( () => loadPackage( 'foo' ) ).toThrow( 'ENOENT' );
+		} );
+
+		it( 'should load package.json', () => {
+			jest.mocked( fs.readFileSync ).mockImplementationOnce( ( path ) => {
+				if ( path === __dirname + '/test-package.json' ) {
+					return JSON.stringify( {
+						name: 'foo',
+						version: '1.0.0',
+					} );
+				}
+
+				throw new Error( 'ENOENT' );
+			} );
+
+			const loadedFile = loadPackage( __dirname + '/test-package.json' );
+
+			expect( loadedFile ).toMatchObject( {
+				name: 'foo',
+				version: '1.0.0',
+			} );
+		} );
+
+		it( 'should cache using normalized paths', () => {
+			jest.mocked( fs.readFileSync ).mockImplementationOnce( ( path ) => {
+				if ( path === __dirname + '/test-package.json' ) {
+					return JSON.stringify( {
+						name: 'foo',
+						version: '1.0.0',
+					} );
+				}
+
+				throw new Error( 'ENOENT' );
+			} );
+			loadPackage( __dirname + '/test-package.json' );
+
+			// Just throw if it's called again so that we can make sure we're using the cache.
+			jest.mocked( fs.readFileSync ).mockImplementationOnce( () => {
+				throw new Error( 'ENOENT' );
+			} );
+
+			const cachedFile = loadPackage(
+				// Use a token that needs to be normalized to match the cached path.
+				__dirname + '/./test-package.json'
+			);
+
+			expect( cachedFile ).toMatchObject( {
+				name: 'foo',
+				version: '1.0.0',
+			} );
+		} );
+	} );
+} );
