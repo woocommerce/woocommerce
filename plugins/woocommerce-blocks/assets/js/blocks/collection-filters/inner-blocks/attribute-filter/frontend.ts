83a32fee41b952dfeecffe4c3ce5c3ea17359aeb
Tung Du <dinhtungdu@gmail.com>
2024-01-03 09:31:09 +0700

    add: multiple select support to attribute dropdown

--- plugins/woocommerce-blocks/assets/js/blocks/collection-filters/inner-blocks/attribute-filter/frontend.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/collection-filters/inner-blocks/attribute-filter/frontend.ts
@@ -15,6 +15,10 @@ interface ActiveAttributeFilterContext extends AttributeFilterContext {
 	value: string;
 }
 
+function nonNullable< T >( value: T ): value is NonNullable< T > {
+	return value !== null && value !== undefined;
+}
+
 function getUrl(
 	selectedTerms: string[],
 	slug: string,
@@ -47,18 +51,14 @@ store( 'woocommerce/collection-attribute-filter', {
 			const dropdownContext = getContext< DropdownContext >(
 				'woocommerce/interactivity-dropdown'
 			);
-
 			const context = getContext< AttributeFilterContext >();
+			const filters = dropdownContext.selectedItems
+				.map( ( item ) => item.value )
+				.filter( nonNullable );
 
-			if ( dropdownContext.selectedItems[ 0 ].value ) {
-				navigate(
-					getUrl(
-						[ dropdownContext.selectedItems[ 0 ].value ],
-						context.attributeSlug,
-						context.queryType
-					)
-				);
-			}
+			navigate(
+				getUrl( filters, context.attributeSlug, context.queryType )
+			);
 		},
 		updateProducts: ( event: HTMLElementEvent< HTMLInputElement > ) => {
 			if ( ! event.target.value ) return;
--- plugins/woocommerce/src/Blocks/BlockTypes/CollectionAttributeFilter.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/CollectionAttributeFilter.php
@@ -211,8 +211,8 @@ final class CollectionAttributeFilter extends AbstractBlock {
 			return '';
 		}
 
-		$list_items    = array();
-		$selected_item = array();
+		$list_items     = array();
+		$selected_items = array();
 
 		$product_attribute = wc_get_attribute( $attributes['attributeId'] );
 
@@ -225,7 +225,7 @@ final class CollectionAttributeFilter extends AbstractBlock {
 			$list_items[] = $item;
 
 			if ( $option['selected'] ) {
-				$selected_item = $item;
+				$selected_items[] = $item;
 			}
 		}
 
@@ -233,7 +233,8 @@ final class CollectionAttributeFilter extends AbstractBlock {
 			array(
 				'items'          => $list_items,
 				'action'         => 'woocommerce/collection-attribute-filter::actions.navigate',
-				'selected_items' => empty( $selected_item ) ? array() : array( $selected_item ),
+				'selected_items' => $selected_items,
+				'select_type'    => $attributes['selectType'] ?? 'multiple',
 				// translators: %s is a product attribute name.
 				'placeholder'    => sprintf( __( 'Select %s', 'woocommerce' ), $product_attribute->name ),
 			)
