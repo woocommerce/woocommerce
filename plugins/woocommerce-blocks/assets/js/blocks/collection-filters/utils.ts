faf1c6f09e9390e1bb24d17b9a6cb564f9ed841b
Sam Seay <samueljseay@gmail.com>
2024-01-04 13:33:59 +0800

    [Experimental] Use variations to provide all the product filters wrapped by CollectionFilters block (#43216)
    ---------
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>

--- plugins/woocommerce-blocks/assets/js/blocks/collection-filters/utils.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/collection-filters/utils.ts
@@ -1,15 +1,18 @@
 /**
  * External dependencies
  */
-import { navigate as navigateFn } from '@woocommerce/interactivity';
-import { getSetting } from '@woocommerce/settings';
+import { getBlockTypes } from '@wordpress/blocks';
 
-const isBlockTheme = getSetting< boolean >( 'isBlockTheme' );
-const isProductArchive = getSetting< boolean >( 'isProductArchive' );
+/**
+ * Returns an array of allowed block names excluding the disallowedBlocks array.
+ *
+ * @param disallowedBlocks Array of block names to disallow.
+ * @return Array of allowed block names.
+ */
+export const getAllowedBlocks = ( disallowedBlocks: string[] ) => {
+	const allBlocks = getBlockTypes();
 
-export function navigate( href: string, options = {} ) {
-	if ( ! isBlockTheme && isProductArchive ) {
-		return ( window.location.href = href );
-	}
-	return navigateFn( href, options );
-}
+	return allBlocks
+		.map( ( block ) => block.name )
+		.filter( ( name ) => ! disallowedBlocks.includes( name ) );
+};
