8f1a112202147ce96148b12905a9073fefc87fd3
Thomas Roberts <thomas.roberts@automattic.com>
2024-01-05 13:57:18 +0000

    Show notice about 0 registered fields in editor

--- plugins/woocommerce-blocks/assets/js/blocks/checkout/inner-blocks/checkout-additional-information-block/block.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/checkout/inner-blocks/checkout-additional-information-block/block.tsx
@@ -2,18 +2,22 @@
  * External dependencies
  */
 import { noticeContexts } from '@woocommerce/base-context';
+import { __ } from '@wordpress/i18n';
 import { StoreNoticesContainer } from '@woocommerce/blocks-components';
 import { useDispatch, useSelect } from '@wordpress/data';
 import { CHECKOUT_STORE_KEY } from '@woocommerce/block-data';
 import { ADDITIONAL_FORM_KEYS } from '@woocommerce/block-settings';
 import { Form } from '@woocommerce/base-components/cart-checkout';
 import type { FunctionComponent } from 'react';
+import NoticeBanner from '@woocommerce/base-components/notice-banner';
 
 const Block: FunctionComponent = () => {
-	const { additionalFields } = useSelect( ( select ) => {
+	const { additionalFields, isEditor } = useSelect( ( select ) => {
 		const store = select( CHECKOUT_STORE_KEY );
+		const editorStore = select( 'core/editor' );
 		return {
 			additionalFields: store.getAdditionalFields(),
+			isEditor: !! editorStore,
 		};
 	} );
 
@@ -27,8 +31,19 @@ const Block: FunctionComponent = () => {
 		...additionalFields,
 	};
 
-	if ( ADDITIONAL_FORM_KEYS.length === 0 ) {
+	if ( ADDITIONAL_FORM_KEYS.length === 0 && ! isEditor ) {
 		return null;
+	}
+
+	if ( ADDITIONAL_FORM_KEYS.length === 0 && isEditor ) {
+		return (
+			<NoticeBanner status="warning" isDismissible={ false }>
+				{ __(
+					'There are no custom fields registered. This block will not be visible on the front-end.',
+					'woocommerce'
+				) }
+			</NoticeBanner>
+		);
 	}
 
 	return (
