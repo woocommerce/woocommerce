891a3d1a4e3e249e33af71a05dca5ccc472cecdd
Tommy Ferry <tommy@wholegraindigital.com>
2024-01-05 09:54:37 +0000

    bump template versions correctly

--- plugins/woocommerce/templates/loop/header.php
+++ plugins/woocommerce/templates/loop/header.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version     1.0.0
+ * @version 8.6.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
