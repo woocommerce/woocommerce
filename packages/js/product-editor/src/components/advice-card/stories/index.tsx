69aec436fcd1b318c3d6574b1f1682d2d2af6216
Damián Suárez <rdsuarez@gmail.com>
2024-01-08 16:58:40 -0300

    rename image prop with children

--- packages/js/product-editor/src/components/advice-card/stories/index.tsx
+++ packages/js/product-editor/src/components/advice-card/stories/index.tsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Flex } from '@wordpress/components';
 import React from 'react';
 
 /**
@@ -9,23 +10,46 @@ import React from 'react';
  */
 import { AdviceCard, AdviceCardProps } from '..';
 import { ShoppingBags } from '../../../images/shopping-bags';
+import { Shirt } from '../../../images/shirt';
+import { Pants } from '../../../images/pants';
+import { Glasses } from '../../../images/glasses';
 
 export default {
 	title: 'Product Editor/components/AdviceCard',
 	component: AdviceCard,
-	args: {
-		tip: __(
-			'Tip: Upsells are typically products that are extra profitable or better quality or more expensive. Experiment with combinations to boost sales.',
-			'woocommerce'
-		),
-		image: <ShoppingBags />,
-		isDismissible: true,
-	},
 };
 
 export const Default = ( args: AdviceCardProps ) => <AdviceCard { ...args } />;
 
 Default.args = {
 	// @todo: use an addon
+	onDismiss: console.log, // eslint-disable-line no-console
+
+	tip: __(
+		'Tip: Upsells are typically products that are extra profitable or better quality or more expensive. Experiment with combinations to boost sales.',
+		'woocommerce'
+	),
+	children: <ShoppingBags />,
+	isDismissible: true,
+};
+
+export const MultipleChildren = ( args: AdviceCardProps ) => (
+	<AdviceCard { ...args } />
+);
+
+MultipleChildren.args = {
+	tip: __(
+		'Tip: Group together items that have a clear relationship or compliment each other well, e.g., garment bundles, camera kits, or skincare product sets.',
+		'woocommerce'
+	),
+
+	children: (
+		<Flex justify="center">
+			<Shirt />
+			<Pants />
+			<Glasses />
+		</Flex>
+	),
+	isDismissible: false,
 	onDismiss: console.log, // eslint-disable-line no-console
 };
