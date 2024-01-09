e35be86f601fea2053af7368fedced2057496134
Nathan Schneider <nsschneider1@gmail.com>
2024-01-09 14:18:18 -0300

    Change to onKeyDown event

--- packages/js/product-editor/src/hooks/use-number-input-props.ts
+++ packages/js/product-editor/src/hooks/use-number-input-props.ts
@@ -8,7 +8,6 @@ export type NumberInputProps = {
 	value: string;
 	onChange: ( value: string ) => void;
 	onFocus: ( event: React.FocusEvent< HTMLInputElement > ) => void;
-	onKeyUp: ( event: React.KeyboardEvent< HTMLInputElement > ) => void;
 	onBlur: ( event: React.FocusEvent ) => void;
 	onKeyDown: ( event: React.KeyboardEvent< HTMLInputElement > ) => void;
 };
@@ -18,6 +17,7 @@ type Props = {
 	onChange: ( value: string ) => void;
 	onFocus?: ( event: React.FocusEvent< HTMLInputElement > ) => void;
 	onKeyUp?: ( event: React.KeyboardEvent< HTMLInputElement > ) => void;
+	onKeyDown?: ( event: React.KeyboardEvent< HTMLInputElement > ) => void;
 	onBlur?: ( event: React.FocusEvent ) => void;
 };
 
@@ -25,7 +25,7 @@ export const useNumberInputProps = ( {
 	value,
 	onChange,
 	onFocus,
-	onKeyUp,
+	onKeyDown,
 	onBlur,
 }: Props ) => {
 	const { formatNumber, parseNumber } = useProductHelper();
@@ -43,22 +43,19 @@ export const useNumberInputProps = ( {
 				onBlur( event );
 			}
 		},
-		onKeyUp( event: React.KeyboardEvent< HTMLInputElement > ) {
+		onKeyDown( event: React.KeyboardEvent< HTMLInputElement > ) {
 			const amount = Number.parseFloat( value || '0' );
 			const step = Number( event.currentTarget.step || '1' );
 			if ( event.code === 'ArrowUp' ) {
+				event.preventDefault();
 				onChange( String( amount + step ) );
 			}
 			if ( event.code === 'ArrowDown' ) {
+				event.preventDefault();
 				onChange( String( amount - step ) );
 			}
-			if ( onKeyUp ) {
-				onKeyUp( event );
-			}
-		},
-		onKeyDown( event: React.KeyboardEvent< HTMLInputElement > ) {
-			if ( event.code === 'ArrowUp' || event.code === 'ArrowDown' ) {
-				event.preventDefault();
+			if ( onKeyDown ) {
+				onKeyDown( event );
 			}
 		},
 		onChange( newValue: string ) {
