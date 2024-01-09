a0ee10da3289755f39c4a2db4a28faf7227fde93
Matt Sherman <matt@jam123.com>
2024-01-09 11:44:02 -0500

    Only enable useEntityRecord when layoutTemplateId is set

--- packages/js/block-templates/src/hooks/use-layout-template/use-layout-template.ts
+++ packages/js/block-templates/src/hooks/use-layout-template/use-layout-template.ts
@@ -39,7 +39,12 @@ export const useLayoutTemplate = ( layoutTemplateId: string | undefined ) => {
 	const { record: layoutTemplate, isResolving } = useEntityRecord(
 		'root',
 		'wcLayoutTemplate',
-		layoutTemplateId
+		layoutTemplateId,
+		// Only perform the query if we have a layout template ID; otherwise, just return null.
+		// Note: Until we are using Gutenberg 17.2, the REST API will still be triggered even
+		// when the query is disabled due to a regression. See:
+		// https://github.com/WordPress/gutenberg/pull/56108
+		{ enabled: !! layoutTemplateId }
 	);
 
 	return { layoutTemplate, isResolving };
