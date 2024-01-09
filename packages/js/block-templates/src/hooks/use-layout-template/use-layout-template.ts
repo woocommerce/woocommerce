6a4a8aab9c03d9ff91ad9a2ddc8e7153ae516290
Matt Sherman <matt@jam123.com>
2024-01-09 11:58:28 -0500

    Workaround for query made when layoutTemplateID is not set

--- packages/js/block-templates/src/hooks/use-layout-template/use-layout-template.ts
+++ packages/js/block-templates/src/hooks/use-layout-template/use-layout-template.ts
@@ -39,9 +39,14 @@ export const useLayoutTemplate = ( layoutTemplateId: string | undefined ) => {
 	const { record: layoutTemplate, isResolving } = useEntityRecord(
 		'root',
 		'wcLayoutTemplate',
-		layoutTemplateId,
+		// Because of the regression mentioned below, REST API requests will still be triggered
+		// even when the query is disabled. This means that if we pass `undefined`/`null` as the ID,
+		// the query will be triggered with no ID, which will return all layout templates.
+		// To prevent this, we pass `__invalid-template-id` as the ID when there is no layout template ID.
+		// A request will still be triggered, but it will return no results.
+		layoutTemplateId || '__invalid-template-id',
 		// Only perform the query if we have a layout template ID; otherwise, just return null.
-		// Note: Until we are using Gutenberg 17.2, the REST API will still be triggered even
+		// Note: Until we are using Gutenberg 17.2, the REST API requests will still be triggered even
 		// when the query is disabled due to a regression. See:
 		// https://github.com/WordPress/gutenberg/pull/56108
 		{ enabled: !! layoutTemplateId }
