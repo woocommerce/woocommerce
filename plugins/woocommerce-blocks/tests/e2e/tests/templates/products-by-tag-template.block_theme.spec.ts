e69173a06e43b5a9697b235e37dd9a8d9c06005f
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-01-10 17:00:04 +0100

    Add e2e tests to verify Products by Category/Tag/Attribute templates default to Product Catalog template

--- plugins/woocommerce-blocks/tests/e2e/tests/templates/products-by-tag-template.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/templates/products-by-tag-template.block_theme.spec.ts
@@ -42,4 +42,42 @@ test.describe( 'Products by Tag template', async () => {
 			page.getByText( 'Hello World in the template' )
 		).toHaveCount( 0 );
 	} );
+
+	test( 'defaults to the Product Catalog template', async ( {
+		admin,
+		editorUtils,
+		page,
+	} ) => {
+		// Edit Product Catalog template and verify changes are visible.
+		await admin.visitSiteEditor( {
+			postId: 'woocommerce/woocommerce//archive-product',
+			postType: 'wp_template',
+		} );
+		await editorUtils.enterEditMode();
+		await editorUtils.closeWelcomeGuideModal();
+		await editorUtils.editor.insertBlock( {
+			name: 'core/paragraph',
+			attributes: {
+				content: 'Hello World in the Product Catalog template',
+			},
+		} );
+		await editorUtils.saveTemplate();
+		await page.goto( permalink );
+		await expect(
+			page
+				.getByText( 'Hello World in the Product Catalog template' )
+				.first()
+		).toBeVisible();
+
+		// Verify the edition can be reverted.
+		await admin.visitAdminPage(
+			'site-editor.php',
+			`path=/wp_template/all`
+		);
+		await editorUtils.revertTemplateCustomizations( 'Product Catalog' );
+		await page.goto( permalink );
+		await expect(
+			page.getByText( 'Hello World in the Product Catalog template' )
+		).toHaveCount( 0 );
+	} );
 } );
