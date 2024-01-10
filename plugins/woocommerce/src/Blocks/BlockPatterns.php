a905c0df492f6d79f80e8c6ede056d7fa3d40bbe
Alba Rincón <albarin@users.noreply.github.com>
2024-01-10 09:03:56 +0100

    CYS: fix PHP warnings and pattern button (#43354)
    * Remove custom styles from button
    
    * Avoid php warning about images and slug not defined
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Fix linting errors
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/src/Blocks/BlockPatterns.php
+++ plugins/woocommerce/src/Blocks/BlockPatterns.php
@@ -243,6 +243,9 @@ class BlockPatterns {
 				for each page load. This way we only do it once on registration.
 				For more context: https://github.com/woocommerce/woocommerce-blocks/pull/11733
 			*/
+
+			$content = array();
+			$images  = array();
 			if ( ! is_null( $pattern_data_from_dictionary ) ) {
 				$content = $pattern_data_from_dictionary['content'];
 				$images  = $pattern_data_from_dictionary['images'] ?? array();
@@ -391,7 +394,7 @@ class BlockPatterns {
 	 */
 	private function get_pattern_from_dictionary( $dictionary, $slug ) {
 		foreach ( $dictionary as $pattern_dictionary ) {
-			if ( $pattern_dictionary['slug'] === $slug ) {
+			if ( isset( $pattern_dictionary['slug'] ) && $pattern_dictionary['slug'] === $slug ) {
 				return $pattern_dictionary;
 			}
 		}
