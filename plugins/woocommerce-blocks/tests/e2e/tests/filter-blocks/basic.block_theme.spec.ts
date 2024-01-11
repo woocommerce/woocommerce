5fdbe63530fd0c145a8d15985b40ed6cbe12abfa
Sam Seay <samueljseay@gmail.com>
2024-01-11 19:27:33 +0800

    [Experimental] Rename Collection Filters to Product Filters (#43363)
    ---------
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>

--- plugins/woocommerce-blocks/tests/e2e/tests/filter-blocks/basic.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/filter-blocks/basic.block_theme.spec.ts
@@ -4,38 +4,38 @@
 import { test, expect } from '@woocommerce/e2e-playwright-utils';
 
 const wrapperBlock = {
-	name: 'woocommerce/collection-filters',
-	title: 'Collection Filters',
+	name: 'woocommerce/product-filters',
+	title: 'Product Filters',
 };
 const filterBlocks = [
 	{
-		name: 'woocommerce/collection-price-filter',
-		title: 'Collection Price Filter',
-		variation: 'Filter Products by Price',
+		name: 'woocommerce/product-filters-price',
+		title: 'Product Filters: Price',
+		variation: 'Product Filters: Price',
 		heading: 'Filter by Price',
 	},
 	{
-		name: 'woocommerce/collection-stock-filter',
-		title: 'Collection Stock Filter',
-		variation: 'Filter Products by Stock',
+		name: 'woocommerce/product-filters-stock-status',
+		title: 'Product Filters: Stock Status',
+		variation: 'Product Filters: Stock Status',
 		heading: 'Filter by Stock Status',
 	},
 	{
-		name: 'woocommerce/collection-rating-filter',
-		title: 'Collection Rating Filter',
-		variation: 'Filter Products by Rating',
+		name: 'woocommerce/product-filters-rating',
+		title: 'Product Filters: Rating',
+		variation: 'Product Filters: Rating',
 		heading: 'Filter by Rating',
 	},
 	{
-		name: 'woocommerce/collection-attribute-filter',
-		title: 'Collection Attribute Filter',
-		variation: 'Filter Products by Attribute',
-		heading: 'Filter Products by Attribute',
+		name: 'woocommerce/product-filters-attribute',
+		title: 'Product Filters: Attribute',
+		variation: 'Product Filters: Attribute',
+		heading: 'Filter by ', // The attribute filter comes with a dynamic title
 	},
 	{
-		name: 'woocommerce/collection-active-filters',
-		title: 'Collection Active Filters',
-		variation: 'Active Product Filters',
+		name: 'woocommerce/product-filters-active',
+		title: 'Product Filters: Active Filters',
+		variation: 'Product Filters: Active Filters',
 		heading: 'Active Filters',
 	},
 ];
@@ -52,7 +52,9 @@ test.describe( 'Filter blocks registration', async () => {
 		await editorUtils.insertBlockUsingGlobalInserter( wrapperBlock.title );
 
 		await expect(
-			editor.canvas.getByLabel( `Block: ${ wrapperBlock.title }` )
+			editor.canvas.getByLabel( `Block: ${ wrapperBlock.title }`, {
+				exact: true,
+			} )
 		).toBeVisible();
 	} );
 
@@ -75,7 +77,10 @@ test.describe( 'Filter blocks registration', async () => {
 		await editorUtils.insertBlockUsingGlobalInserter( wrapperBlock.title );
 		for ( const block of filterBlocks ) {
 			await expect(
-				editor.canvas.getByText( block.heading )
+				editor.canvas
+					.getByLabel( `Block: ${ block.title }` )
+					.getByLabel( 'Block: Heading' )
+					.and( editor.canvas.getByText( block.heading ) )
 			).toBeVisible();
 		}
 	} );
