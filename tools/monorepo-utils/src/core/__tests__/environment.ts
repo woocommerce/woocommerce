5b93dc44ee84c87d373867bc924d21d5e7632c98
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2024-01-11 15:22:25 -0800

    Fixed CI Test Breakage

--- tools/monorepo-utils/src/core/__tests__/environment.ts
+++ tools/monorepo-utils/src/core/__tests__/environment.ts
@@ -4,13 +4,20 @@
 import { isGithubCI } from '../environment';
 
 describe( 'isGithubCI', () => {
+	// Store the original environment variables so we can restore them.
+	const originalEnv = process.env;
+
+	beforeEach( () => {
+		delete process.env.CI;
+		delete process.env.GITHUB_ACTIONS;
+	} );
+
 	it( 'should return true if CI is set', () => {
 		process.env.CI = 'true';
 		expect( isGithubCI() ).toBe( true );
 	} );
 
 	it( 'should return false if CI is not set', () => {
-		delete process.env.CI;
 		expect( isGithubCI() ).toBe( false );
 	} );
 
@@ -20,12 +27,13 @@ describe( 'isGithubCI', () => {
 	} );
 
 	it( 'should return false if GITHUB_ACTIONS is not set', () => {
-		delete process.env.GITHUB_ACTIONS;
 		expect( isGithubCI() ).toBe( false );
 	} );
 
+	// Once we've completed all of the tests we need to set the environment
+	// back to the way it was before we started. This makes sure we don't
+	// leak anything to any other tests.
 	afterAll( () => {
-		delete process.env.CI;
-		delete process.env.GITHUB_ACTIONS;
+		process.env = originalEnv;
 	} );
 } );
