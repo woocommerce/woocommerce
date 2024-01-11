771ee7a2c97b438ea322a0df59e222bc15f4b727
Niels Lange <info@nielslange.de>
2024-01-11 21:17:11 +0700

    Changing template version numbers to 8.5.0 as they haven’t been touched

--- plugins/woocommerce/templates/notices/success.php
+++ plugins/woocommerce/templates/notices/success.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 8.5.1
+ * @version 8.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
