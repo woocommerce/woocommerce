c6a2497b4711d08ca9e4d909556ee89b3e63aff9
Alfredo Sumaran <a.sumaran@gmail.com>
2024-01-12 16:27:22 -0500

    Remove unnecessary default assignment

--- plugins/woocommerce/includes/class-wc-geolocation.php
+++ plugins/woocommerce/includes/class-wc-geolocation.php
@@ -98,7 +98,6 @@ class WC_Geolocation {
 	 * @return string
 	 */
 	public static function get_external_ip_address() {
-		$transient_name      = null;
 		$external_ip_address = '0.0.0.0';
 
 		if ( '' !== self::get_ip_address() ) {
