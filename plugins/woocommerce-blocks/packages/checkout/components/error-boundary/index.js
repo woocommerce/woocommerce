60a937ba72911df4a5f92ace635b2408d126b3b2
jonathansadowski <jonathansadowski@users.noreply.github.com>
2024-01-15 12:05:41 -0600

    Test PR

--- plugins/woocommerce-blocks/packages/checkout/components/error-boundary/index.js
+++ plugins/woocommerce-blocks/packages/checkout/components/error-boundary/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { Component } from '@wordpress/element';
+import { Component } from '@wordpress/element'
 
 class CheckoutSlotErrorBoundary extends Component {
 	state = { errorMessage: '', hasError: false };
