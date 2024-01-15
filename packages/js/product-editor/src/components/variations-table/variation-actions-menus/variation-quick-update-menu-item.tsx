4f18d70cd85673410b4cff46bd412ab7fef2618b
Fernando Marichal <ultimoround@gmail.com>
2024-01-15 10:04:35 +0700

    Show multipleSelection items in both components

--- packages/js/product-editor/src/components/variations-table/variation-actions-menus/variation-quick-update-menu-item.tsx
+++ packages/js/product-editor/src/components/variations-table/variation-actions-menus/variation-quick-update-menu-item.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { Slot, Fill } from '@wordpress/components';
-import { createElement } from '@wordpress/element';
+import { createElement, Fragment } from '@wordpress/element';
 import {
 	createOrderedChildren,
 	sortFillsByOrder,
@@ -14,12 +14,9 @@ import {
 import { MenuItemProps } from './types';
 import { QUICK_UPDATE, SINGLE_UPDATE } from './constants';
 
-const getGroupName = (
-	group?: string,
-	supportsMultipleSelection?: boolean
-) => {
+const getGroupName = ( group?: string, isMultipleSelection?: boolean ) => {
 	const name = 'woocommerce-actions-menu-slot';
-	const nameSuffix = supportsMultipleSelection
+	const nameSuffix = isMultipleSelection
 		? `_${ QUICK_UPDATE }`
 		: `_${ SINGLE_UPDATE }`;
 	return group ? `${ name }_${ group }${ nameSuffix }` : name;
@@ -35,6 +32,25 @@ export const VariationQuickUpdateMenuItem: React.FC< MenuItemProps > & {
 	group = 'top-level',
 	supportsMultipleSelection,
 } ) => {
+	if ( supportsMultipleSelection ) {
+		return (
+			<>
+				{ [ QUICK_UPDATE, SINGLE_UPDATE ].map( ( updateType ) => (
+					<Fill
+						key={ updateType }
+						name={ getGroupName(
+							group,
+							updateType === QUICK_UPDATE
+						) }
+					>
+						{ ( fillProps: Fill.Props ) =>
+							createOrderedChildren( children, order, fillProps )
+						}
+					</Fill>
+				) ) }
+			</>
+		);
+	}
 	return (
 		<Fill name={ getGroupName( group, supportsMultipleSelection ) }>
 			{ ( fillProps: Fill.Props ) =>
