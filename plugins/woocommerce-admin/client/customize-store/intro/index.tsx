b62b791dc55cdda552a30b7fa20eb56cd74cd8db
Alba Rincón <alba.rincon@automattic.com>
2024-01-15 11:13:42 +0100

    Avoid using variables in __()

--- plugins/woocommerce-admin/client/customize-store/intro/index.tsx
+++ plugins/woocommerce-admin/client/customize-store/intro/index.tsx
@@ -125,9 +125,16 @@ export const Intro: CustomizeStoreComponent = ( { sendEvent, context } ) => {
 
 	const BannerComponent = BANNER_COMPONENTS[ bannerStatus ];
 
-	const sidebarMessage = `Create a store that reflects your brand and business. Select one of our professionally designed themes to customize, or create your own using ${
-		context.flowType === FlowType.AIOnline ? 'AI' : 'our store designer'
-	}.`;
+	const sidebarMessage =
+		context.flowType === FlowType.AIOnline
+			? __(
+					'Create a store that reflects your brand and business. Select one of our professionally designed themes to customize, or create your own using AI',
+					'woocommerce'
+			  )
+			: __(
+					'Create a store that reflects your brand and business. Select one of our professionally designed themes to customize, or create your own using our store designer',
+					'woocommerce'
+			  );
 
 	return (
 		<>
@@ -162,7 +169,7 @@ export const Intro: CustomizeStoreComponent = ( { sendEvent, context } ) => {
 						</button>
 						{ __( 'Customize your store', 'woocommerce' ) }
 					</div>
-					<p>{ __( sidebarMessage, 'woocommerce' ) }</p>
+					<p>{ sidebarMessage }</p>
 				</div>
 
 				<div className="woocommerce-customize-store-main">
