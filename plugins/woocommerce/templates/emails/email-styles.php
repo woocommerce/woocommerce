df00f10fc6b22a57741060bdaff8cb57ae3d8e3c
Mike Jolley <mike.jolley@me.com>
2024-01-16 14:49:35 +0000

    Version bump for styles

--- plugins/woocommerce/templates/emails/email-styles.php
+++ plugins/woocommerce/templates/emails/email-styles.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woo.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
- * @version 7.4.0
+ * @version 8.6.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
