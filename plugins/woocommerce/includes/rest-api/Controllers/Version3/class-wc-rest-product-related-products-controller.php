75a8319494e6a0a6f91bbd8ab2099f4275970d68
Damián Suárez <rdsuarez@gmail.com>
2024-01-16 18:03:04 -0300

    filter categoris from endpoint params

--- plugins/woocommerce/includes/rest-api/Controllers/Version3/class-wc-rest-product-related-products-controller.php
+++ plugins/woocommerce/includes/rest-api/Controllers/Version3/class-wc-rest-product-related-products-controller.php
@@ -36,6 +36,13 @@ class WC_REST_Product_Related_Products_Controller extends WC_REST_Products_V2_Co
 	protected $rest_base = 'products/(?P<product_id>[\d]+)/related-products';
 
 	/**
+	 * Related product categories.
+	 *
+	 * @var array
+	 */
+	protected $categories = array();
+
+	/**
 	 * Register the routes for products.
 	 */
 	public function register_routes() {
@@ -49,6 +56,15 @@ class WC_REST_Product_Related_Products_Controller extends WC_REST_Products_V2_Co
 						'description' => __( 'Unique identifier for the variable product.', 'woocommerce' ),
 						'type'        => 'integer',
 					),
+					'categories'     => array(
+						'description'       => __( 'Limit result set to specific product categorie ids.', 'woocommerce' ),
+						'type'              => 'array',
+						'items'             => array(
+							'type' => 'integer',
+						),
+						'default'           => array(),
+						'sanitize_callback' => 'wp_parse_id_list',
+					),
 				),
 				array(
 					'methods'             => WP_REST_Server::READABLE,
@@ -61,13 +77,36 @@ class WC_REST_Product_Related_Products_Controller extends WC_REST_Products_V2_Co
 		);
 	}
 
+	/**
+	 * Get the related product categories.
+	 */
+	public function get_related_product_cat_terms( $terms, $product_id ) {
+		return $this->categories;
+	}
+
 	public function get_items( $request ) {
 		$product_id  = $request->get_param( 'product_id' );
 		$object      = wc_get_product( $product_id );
-		$ids         = wc_get_related_products( $product_id );
+
+		$categories = $request->get_param( 'categories' );
+
+		// Filter the related product categories.
+		if ( ! empty( $categories ) ) {
+			$this->categories = $categories;
+			add_filter(
+				'woocommerce_get_related_product_cat_terms',
+				array( $this, 'get_related_product_cat_terms' ),
+				100, 2
+			);
+		}
+
+		$ids = wc_get_related_products( $product_id );
+
+		// Remove the product categories filter.
+		remove_filter( 'woocommerce_get_related_product_cat_terms', array( $this, 'get_related_product_cat_terms' ), 100, 2 );
 
 		return (object) array(
-			'ids' => $ids,
+			'ids'        => $ids,
 		);
 	}
 }
