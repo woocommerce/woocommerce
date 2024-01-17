736a9651063e67df65305e44ea563a4cb8ade049
Damián Suárez <rdsuarez@gmail.com>
2024-01-17 18:23:04 -0300

    add script to compile product editor extension

---
+++ plugins/woo-ai/src-product-editor-extension/index.ts
@@ -0,0 +1,13 @@
+/**
+ * External dependencies
+ */
+import debugFactory from 'debug';
+
+/**
+ * Internal dependencies
+ */
+import './styles.scss';
+
+const debug = debugFactory( 'woo-ai:product-editor' );
+
+debug( 'Product Editor extension loaded' );
