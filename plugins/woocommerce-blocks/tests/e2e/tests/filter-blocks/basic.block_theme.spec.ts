b573f74d1f2c019ba0aa5da048413ba9acd3d04e
Sam Seay <samueljseay@gmail.com>
2024-01-17 14:44:12 +0800

    Adjust the tests again, the title is not nested anymore.

--- plugins/woocommerce-blocks/tests/e2e/tests/filter-blocks/basic.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/filter-blocks/basic.block_theme.spec.ts
@@ -7,31 +7,26 @@ const filterBlocks = [
 	{
 		name: 'woocommerce/product-filters-price',
 		title: 'Product Filters: Price',
-		variation: 'Product Filters: Price',
 		heading: 'Filter by Price',
 	},
 	{
 		name: 'woocommerce/product-filters-stock-status',
 		title: 'Product Filters: Stock Status',
-		variation: 'Product Filters: Stock Status',
 		heading: 'Filter by Stock Status',
 	},
 	{
 		name: 'woocommerce/product-filters-rating',
 		title: 'Product Filters: Rating',
-		variation: 'Product Filters: Rating',
 		heading: 'Filter by Rating',
 	},
 	{
 		name: 'woocommerce/product-filters-attribute',
 		title: 'Product Filters: Attribute',
-		variation: 'Product Filters: Attribute',
 		heading: 'Filter by ', // The attribute filter comes with a dynamic title
 	},
 	{
 		name: 'woocommerce/product-filters-active',
 		title: 'Product Filters: Active Filters',
-		variation: 'Product Filters: Active Filters',
 		heading: 'Active Filters',
 	},
 ];
@@ -46,7 +41,7 @@ test.describe( 'Filter blocks registration', async () => {
 		editorUtils,
 	} ) => {
 		for ( const block of filterBlocks ) {
-			await editorUtils.insertBlockUsingGlobalInserter( block.variation );
+			await editorUtils.insertBlockUsingGlobalInserter( block.title );
 
 			await expect(
 				editor.canvas.getByLabel( `Block: ${ block.title }` )
@@ -59,11 +54,10 @@ test.describe( 'Filter blocks registration', async () => {
 		editorUtils,
 	} ) => {
 		for ( const block of filterBlocks ) {
-			await editorUtils.insertBlockUsingGlobalInserter( block.variation );
+			await editorUtils.insertBlockUsingGlobalInserter( block.title );
 
 			await expect(
 				editor.canvas
-					.getByLabel( `Block: ${ block.title }` )
 					.getByLabel( 'Block: Heading' )
 					.and( editor.canvas.getByText( block.heading ) )
 			).toBeVisible();
