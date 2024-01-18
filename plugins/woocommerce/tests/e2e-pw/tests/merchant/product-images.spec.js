03a11a4432a4c1865c92adf452316bc226ab8727
Adrian Moldovan <adim.moldovan@gmail.com>
2024-01-18 13:59:10 +0200

    Implement can update the product image

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/product-images.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/product-images.spec.js
@@ -10,13 +10,7 @@ baseTest.describe('Products > Product Images', () => {
 				url: baseURL,
 				consumerKey: process.env.CONSUMER_KEY,
 				consumerSecret: process.env.CONSUMER_SECRET,
-				version: 'wc/v3',
-				axiosConfig: {
-					// allow 404s, so we can check if the product was deleted without try/catch
-					validateStatus: function (status) {
-						return status >= 200 && status < 300 || status === 404;
-					},
-				},
+				version: 'wc/v3'
 			});
 
 			await use(api);
@@ -27,10 +21,15 @@ baseTest.describe('Products > Product Images', () => {
 		product: async ({page, api}, use) => {
 			let product = {
 				id: 0,
-				name: `Product with images ${Date.now()}`,
+				name: `Product ${Date.now()}`,
 				type: 'simple',
 				regular_price: '12.99',
-				sale_price: '11.59'
+				sale_price: '11.59',
+				images: [
+					{
+						src: "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg"
+					}
+				]
 			}
 
 			await api
@@ -50,7 +49,19 @@ baseTest.describe('Products > Product Images', () => {
 		},
 	});
 
-	test('can set product image', async ({page, product}) => {
+	test('can set product image', async ({page, product, api}) => {
+		// Remove the product image first and reload the page
+		await api
+			.put(`products/${product.id}`,
+				{
+					images: []
+				}
+			)
+			.then((response) => {
+				product = response.data;
+			});
+		await page.reload();
+
 		await test.step('Set product image', async () => {
 			await page.getByRole('link', {name: 'Set product image'}).click();
 			await page.getByRole('tab', {name: 'Media Library'}).click();
@@ -59,7 +70,9 @@ baseTest.describe('Products > Product Images', () => {
 			await expect(imageLocator).toBeChecked()
 			await page.getByRole('button', {name: 'Set product image'}).click();
 
-			await expect(page.locator('#set-post-thumbnail > img')).toBeVisible();
+			// Wait for the product image thumbnail to be updated.
+			// Clicking the "Update" button before this happens will not update the image.
+			await expect(page.locator('#set-post-thumbnail img[src$="image-01.jpeg"]')).toBeVisible();
 
 			await page.getByRole('button', {name: 'Update'}).click();
 		});
@@ -67,9 +80,6 @@ baseTest.describe('Products > Product Images', () => {
 		await test.step('Verify product image was set', async () => {
 			// Verify product was updated
 			await expect(page.getByText('Product updated.')).toBeVisible()
-
-			// Verify image in admin area
-			await expect(page.locator('#set-post-thumbnail > img')).toBeVisible();
 
 			// Verify image in store frontend
 			await page.goto(product.permalink);
@@ -79,16 +89,31 @@ baseTest.describe('Products > Product Images', () => {
 
 	test('can update the product image', async ({page, product}) => {
 		await test.step('Update product image', async () => {
+			await page.locator('#set-post-thumbnail').click();
+
+			const imageLocator = page.getByLabel('image-02').nth(0)
+			await imageLocator.click();
+			await expect(imageLocator).toBeChecked()
+			await page.getByRole('button', {name: 'Set product image'}).click();
+
+			// Wait for the product image thumbnail to be updated.
+			// Clicking the "Update" button before this happens will not update the image.
+			await expect(page.locator('#set-post-thumbnail img[src$="image-02.png"]')).toBeVisible();
+
+			await page.getByRole('button', {name: 'Update'}).click();
 		});
 
 		await test.step('Verify product image was set', async () => {
-			// Verify image in admin area
+			// Verify product was updated
+			await expect(page.getByText('Product updated.')).toBeVisible()
 
 			// Verify image in store frontend
+			await page.goto(product.permalink);
+			await expect(page.getByTitle(`image-02`)).toBeVisible();
 		});
 	});
 
-	test('can delete the product image', async ({page, product}) => {
+	test.skip('can delete the product image', async ({page, product}) => {
 		await test.step('Remove product image', async () => {
 		});
 
@@ -99,7 +124,7 @@ baseTest.describe('Products > Product Images', () => {
 		});
 	});
 
-	test('can create a product gallery', async ({page, product}) => {
+	test.skip('can create a product gallery', async ({page, product}) => {
 		await test.step('Add product gallery images', async () => {
 		});
 
