d3254eb4b3b53705c4b1fe9649778bc2a4bfdeaa
Tarun Vijwani <tarun.vijwani@automattic.com>
2024-01-19 01:05:26 +0400

    Hide the shipping rates in editor to match front-end
    - If there are no shipping rates, hide the shipping rates in editor to match front-end.

--- plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-order-summary-shipping/block.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-order-summary-shipping/block.tsx
@@ -3,14 +3,26 @@
  */
 import { TotalsShipping } from '@woocommerce/base-components/cart-checkout';
 import { getCurrencyFromPriceResponse } from '@woocommerce/price-format';
-import { useStoreCart } from '@woocommerce/base-context/hooks';
+import { useStoreCart, useEditorContext } from '@woocommerce/base-context/';
 import { TotalsWrapper } from '@woocommerce/blocks-components';
 import { getSetting } from '@woocommerce/settings';
+import { getShippingRatesPackageCount } from '@woocommerce/base-utils';
+import { select } from '@wordpress/data';
 
 const Block = ( { className }: { className: string } ): JSX.Element | null => {
 	const { cartTotals, cartNeedsShipping } = useStoreCart();
+	const { isEditor } = useEditorContext();
 
 	if ( ! cartNeedsShipping ) {
+		return null;
+	}
+
+	const shippingRates = select( 'wc/store/cart' ).getShippingRates();
+	const shippingRatesPackageCount =
+		getShippingRatesPackageCount( shippingRates );
+
+	// If there are no shipping rates, and we're in the editor, hide the shipping rates to match front-end.
+	if ( ! shippingRatesPackageCount && isEditor ) {
 		return null;
 	}
 
