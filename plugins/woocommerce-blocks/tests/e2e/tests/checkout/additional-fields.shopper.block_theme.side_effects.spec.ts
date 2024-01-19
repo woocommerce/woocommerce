e3e1e6739e8c76ff2a04519ba703c0ecceb12668
Thomas Roberts <thomas.roberts@automattic.com>
2024-01-19 19:11:43 +0000

    Add test for filling additional checkout fields

---
+++ plugins/woocommerce-blocks/tests/e2e/tests/checkout/additional-fields.shopper.block_theme.side_effects.spec.ts
@@ -0,0 +1,72 @@
+/**
+ * External dependencies
+ */
+import { expect, test as base } from '@woocommerce/e2e-playwright-utils';
+import { BlockData } from '@woocommerce/e2e-types';
+import { customerFile, guestFile } from '@woocommerce/e2e-utils';
+
+/**
+ * Internal dependencies
+ */
+import { REGULAR_PRICED_PRODUCT_NAME } from './constants';
+import { CheckoutPage } from './checkout.page';
+
+const test = base.extend< { checkoutPageObject: CheckoutPage } >( {
+	checkoutPageObject: async ( { page }, use ) => {
+		const pageObject = new CheckoutPage( {
+			page,
+		} );
+		await use( pageObject );
+	},
+} );
+
+test.describe( 'Shopper → Additional Checkout Fields', () => {
+	test.use( { storageState: customerFile } );
+	test.beforeAll( async ( { browser } ) => {
+		const context = await browser.newContext();
+		const page = await context.newPage();
+		await page.goto( '/?enable_custom_checkout_fields=true' );
+		await expect(
+			page.getByText( 'Enabled custom checkout fields' )
+		).toBeVisible();
+	} );
+	test.afterAll( async ( { browser } ) => {
+		const context = await browser.newContext();
+		const page = await context.newPage();
+		await page.goto( '/?disable_custom_checkout_fields=true' );
+		await expect(
+			page.getByText( 'Disabled custom checkout fields' )
+		).toBeVisible();
+	} );
+
+	test.beforeEach( async ( { frontendUtils } ) => {
+		await frontendUtils.emptyCart();
+		await frontendUtils.goToShop();
+		await frontendUtils.addToCart( REGULAR_PRICED_PRODUCT_NAME );
+		await frontendUtils.goToCheckout();
+	} );
+
+	test( 'Shopper can fill in the checkout form with additional fields', async ( {
+		checkoutPageObject,
+	} ) => {
+		await checkoutPageObject.fillInCheckoutWithTestData(
+			{},
+			{
+				address: [ { label: 'Government ID', value: '12345' } ],
+				additional: [
+					{ label: 'How did you hear about us?', value: 'other' },
+				],
+			}
+		);
+		await checkoutPageObject.placeOrder();
+
+		const shippingGovernmentIdText = checkoutPageObject.page
+			.getByText( 'Government ID' )
+			.first();
+		const governmentId = await shippingGovernmentIdText.evaluate(
+			( node ) => node.nextSibling?.textContent
+		);
+		await expect( shippingGovernmentIdText ).toBeVisible();
+		expect( governmentId ).toEqual( '12345' );
+	} );
+} );
