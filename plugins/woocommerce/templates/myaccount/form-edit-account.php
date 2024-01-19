ce38ab6bf2f093f353c699eae64b1fc1b61be319
Mike Jolley <mike.jolley@me.com>
2024-01-19 11:40:58 +0000

    Move woocommerce_edit_account_form above password section, which should be last.

--- plugins/woocommerce/templates/myaccount/form-edit-account.php
+++ plugins/woocommerce/templates/myaccount/form-edit-account.php
@@ -12,7 +12,7 @@
  *
  * @see https://woo.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 7.0.1
+ * @version 8.6.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -44,6 +44,8 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
 		<input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" autocomplete="email" value="<?php echo esc_attr( $user->user_email ); ?>" />
 	</p>
 
+	<?php do_action( 'woocommerce_edit_account_form' ); ?>
+
 	<fieldset>
 		<legend><?php esc_html_e( 'Password change', 'woocommerce' ); ?></legend>
 
@@ -61,8 +63,6 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
 		</p>
 	</fieldset>
 	<div class="clear"></div>
-
-	<?php do_action( 'woocommerce_edit_account_form' ); ?>
 
 	<p>
 		<?php wp_nonce_field( 'save_account_details', 'save-account-details-nonce' ); ?>
