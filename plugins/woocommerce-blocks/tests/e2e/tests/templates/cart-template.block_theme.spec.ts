986f89a938daa858d5a94f8f9996a4f7cf601cb7
Niels Lange <info@nielslange.de>
2024-01-19 15:36:24 +0700

    Unskip template-related e2e tests

--- plugins/woocommerce-blocks/tests/e2e/tests/templates/cart-template.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/templates/cart-template.block_theme.spec.ts
@@ -27,28 +27,27 @@ test.describe( 'Test the cart template', async () => {
 		).toBeVisible();
 	} );
 
-	// Remove the skip once this ticket is resolved: https://github.com/woocommerce/woocommerce-blocks/issues/11671
-	// eslint-disable-next-line playwright/no-skipped-test
-	test.skip( 'Template can be accessed from the page editor', async ( {
+	test( 'Template can be accessed from the page editor', async ( {
 		admin,
 		editor,
 		page,
 		editorUtils,
 	} ) => {
-		await admin.visitSiteEditor( {
-			postId: templatePath,
-			postType: templateType,
-		} );
-		await editor.page.getByRole( 'button', { name: /Pages/i } ).click();
-		await editor.page.getByRole( 'button', { name: /Cart/i } ).click();
+		await admin.visitAdminPage( `/site-editor.php?path=%2Fpage` );
+		await editor.page
+			.getByRole( 'button', { name: 'Cart', exact: true } )
+			.click();
 		await editorUtils.enterEditMode();
 		await editorUtils.closeWelcomeGuideModal();
+
 		await expect(
 			editor.canvas.locator( 'h1:has-text("Cart")' ).first()
 		).toBeVisible();
+
 		await editor.openDocumentSettingsSidebar();
 		await page.getByLabel( 'Template options' ).click();
-		await page.getByRole( 'button', { name: 'Edit template' } ).click();
+		await page.getByRole( 'menuitem', { name: 'Edit template' } ).click();
+
 		await expect(
 			editor.canvas.locator( 'h1:has-text("Cart")' ).first()
 		).toBeVisible();
