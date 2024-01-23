b4ac1d25a5205630c1dea0050a285fa29b3e8198
Ilyas Foo <foo.ilyas@gmail.com>
2024-01-24 01:35:07 +0800

    Add breaks

--- plugins/woocommerce/src/Admin/RemoteInboxNotifications/ComparisonOperation.php
+++ plugins/woocommerce/src/Admin/RemoteInboxNotifications/ComparisonOperation.php
@@ -39,6 +39,7 @@ class ComparisonOperation {
 				if ( is_string( $right_operand ) && is_string( $left_operand ) ) {
 					return strpos( $right_operand, $left_operand ) !== false;
 				}
+				break;
 			case '!contains':
 				if ( is_array( $left_operand ) && is_string( $right_operand ) ) {
 					return ! in_array( $right_operand, $left_operand, true );
@@ -46,6 +47,7 @@ class ComparisonOperation {
 				if ( is_string( $right_operand ) && is_string( $left_operand ) ) {
 					return strpos( $right_operand, $left_operand ) === false;
 				}
+				break;
 			case 'in':
 				if ( is_array( $right_operand ) && is_string( $left_operand ) ) {
 					return in_array( $left_operand, $right_operand, true );
@@ -53,6 +55,7 @@ class ComparisonOperation {
 				if ( is_string( $left_operand ) && is_string( $right_operand ) ) {
 					return strpos( $left_operand, $right_operand ) !== false;
 				}
+				break;
 			case '!in':
 				if ( is_array( $right_operand ) && is_string( $left_operand ) ) {
 					return ! in_array( $left_operand, $right_operand, true );
@@ -60,6 +63,7 @@ class ComparisonOperation {
 				if ( is_string( $left_operand ) && is_string( $right_operand ) ) {
 					return strpos( $left_operand, $right_operand ) === false;
 				}
+				break;
 		}
 
 		return false;
