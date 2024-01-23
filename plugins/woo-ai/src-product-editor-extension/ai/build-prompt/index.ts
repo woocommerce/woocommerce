9153208984fa028ff8463b28b3a26a884949fbab
Damián Suárez <rdsuarez@gmail.com>
2024-01-23 16:40:49 -0300

    polish prompt

--- plugins/woo-ai/src-product-editor-extension/ai/build-prompt/index.ts
+++ plugins/woo-ai/src-product-editor-extension/ai/build-prompt/index.ts
@@ -19,7 +19,7 @@ export async function buildProductTitleSuggestionsPromp( productId: number ) {
 
 	const {
 		name,
-		tags,
+		tags: tagsObject,
 		categories: categoriesObject,
 		attributes,
 		type: product_type,
@@ -33,41 +33,49 @@ export async function buildProductTitleSuggestionsPromp( productId: number ) {
 		}
 	);
 
-	const validProductData = Object.entries( {
-		name,
-		tags,
-		categories,
-		attributes,
-		product_type,
-		is_downloadable,
-		is_virtual,
-	} ).reduce( ( acc, [ key, value ] ) => {
-		if (
-			typeof value === 'boolean' ||
-			( value instanceof Array
-				? Boolean( value.length )
-				: Boolean( value ) )
-		) {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			acc[ key ] = value;
-		}
-		return acc;
-	}, {} );
+	const tags = tagsObject.map( ( { name: tagName }: { name: string } ) => {
+		return tagName;
+	} );
 
-	const instructions = [
-		'You are a WooCommerce SEO and marketing expert.',
-		"Using the product's name, description, tags, categories, and other attributes, provide three optimized alternatives to the product's title to enhance the store's SEO performance and sales.",
-		"Provide the best option for the product's title based on the product properties.",
-		'Identify the language used in the given title and use the same language in your response.',
-		'Product titles should contain at least 20 characters.',
-		"The product's properties are:",
-		`${ JSON.stringify( validProductData ) }`,
-		``,
-		'Important!: Respect the format of the response. The response should be an array of strings: [ "first-title", "second-title", "third-title" ]',
-	];
+	const productPros = [];
+	if ( name ) {
+		productPros.push( `- Current Product Name: "${ name }"` );
+	}
+	if ( categories.length ) {
+		productPros.push( `- Categories: "${ categories.join( '", "' ) }"` );
+	}
+	if ( tags.length ) {
+		productPros.push( `- Tags: "${ tags.join( '", "' ) }"` );
+	}
+	if ( attributes.length ) {
+		productPros.push( `- Attributes: "${ attributes.join( '", "' ) }"` );
+	}
+	if ( product_type ) {
+		productPros.push( `- Product Type: "${ product_type }"` );
+	}
+	if ( is_downloadable ) {
+		productPros.push(
+			`- Downloadable: ${ is_downloadable ? 'YES' : 'NO' }`
+		);
+	}
+	if ( is_virtual ) {
+		productPros.push( `- Virtual: ${ is_virtual ? 'YES' : 'NO' }` );
+	}
 
+	const productProsString = productPros.join( '\n' );
+
+	const instructions = `
+You are a WooCommerce SEO and marketing expert.
+Using the product's name, description, tags, categories, and other attributes, provide three optimized alternatives to the product's title to enhance the store's SEO performance and sales.
+Provide the best option for the product's title based on the product properties.
+Identify the language used in the given title and use the same language in your response.
+Product titles should contain at least 20 characters.
+The product's properties are:
+${ productProsString }
+
+Important!: Respect the format of the response. The response should be an array of strings: [ "first-title", "second-title", "third-title" ]'
+`;
 	debug( 'instructions', instructions );
 
-	return instructions.join( '\n' );
+	return instructions;
 }
