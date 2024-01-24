3eafde5f1dcabe601e2a5c1333d164d94adae38e
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-01-24 09:29:24 +0100

    Clean up order-confirmation.block_theme.spec.ts file

--- plugins/woocommerce-blocks/tests/e2e/tests/templates/order-confirmation.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/templates/order-confirmation.block_theme.spec.ts
@@ -1,30 +1,7 @@
 /**
  * External dependencies
  */
-import { test as base, expect } from '@woocommerce/e2e-playwright-utils';
-
-/**
- * Internal dependencies
- */
-import { CheckoutPage } from '../checkout/checkout.page';
-import {
-	FREE_SHIPPING_NAME,
-	FREE_SHIPPING_PRICE,
-	SIMPLE_PHYSICAL_PRODUCT_NAME,
-} from '../checkout/constants';
-
-const test = base.extend< { pageObject: CheckoutPage } >( {
-	pageObject: async ( { page }, use ) => {
-		const pageObject = new CheckoutPage( {
-			page,
-		} );
-		await use( pageObject );
-	},
-} );
-
-const templatePath = 'woocommerce/woocommerce//order-confirmation';
-const templateType = 'wp_template';
-const userText = 'Hello World in the template';
+import { test, expect } from '@woocommerce/e2e-playwright-utils';
 
 test.describe( 'Test the order confirmation template', async () => {
 	test( 'Template can be opened in the site editor', async ( {
