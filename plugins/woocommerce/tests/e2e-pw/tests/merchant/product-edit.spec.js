8e6d4568ef23bfdba5c8dc315e8baa6fc149daee
Adrian Moldovan <adim.moldovan@gmail.com>
2024-01-24 17:20:32 +0200

    Update existing edit product test

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/product-edit.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/product-edit.spec.js
@@ -1,64 +1,96 @@
-const { test, expect } = require( '@playwright/test' );
+const { test: baseTest, expect } = require( '@playwright/test' );
 const wcApi = require( '@woocommerce/woocommerce-rest-api' ).default;
 
-test.describe( 'Products > Edit Product', () => {
-	test.use( { storageState: process.env.ADMINSTATE } );
+baseTest.describe( 'Products > Edit Product', () => {
+	baseTest.use( { storageState: process.env.ADMINSTATE } );
 
-	let productId;
+	const test = baseTest.extend( {
+		api: async ( { baseURL }, use ) => {
+			const api = new wcApi( {
+				url: baseURL,
+				consumerKey: process.env.CONSUMER_KEY,
+				consumerSecret: process.env.CONSUMER_SECRET,
+				version: 'wc/v3',
+			} );
 
-	test.beforeAll( async ( { baseURL } ) => {
-		const api = new wcApi( {
-			url: baseURL,
-			consumerKey: process.env.CONSUMER_KEY,
-			consumerSecret: process.env.CONSUMER_SECRET,
-			version: 'wc/v3',
-		} );
-		await api
-			.post( 'products', {
-				name: 'Product to edit',
+			await use( api );
+		},
+
+		product: async ( { api }, use ) => {
+			let product = {
+				id: 0,
+				name: `Product ${ Date.now() }`,
 				type: 'simple',
 				regular_price: '12.99',
-			} )
-			.then( ( response ) => {
-				productId = response.data.id;
+			};
+
+			await api.post( 'products', product ).then( ( response ) => {
+				product = response.data;
 			} );
+
+			await use( product );
+
+			// permanently delete the product if it still exists
+			const r = await api.get( `products/${ product.id }` );
+			if ( r.status !== 404 ) {
+				await api.delete( `products/${ product.id }`, {
+					force: true,
+				} );
+			}
+		},
 	} );
 
-	test.afterAll( async ( { baseURL } ) => {
-		const api = new wcApi( {
-			url: baseURL,
-			consumerKey: process.env.CONSUMER_KEY,
-			consumerSecret: process.env.CONSUMER_SECRET,
-			version: 'wc/v3',
+	test( 'can edit a product and save the changes', async ( {
+		page,
+		product,
+	} ) => {
+		await page.goto( `wp-admin/post.php?post=${ product.id }&action=edit` );
+
+		const newProduct = {
+			name: `Product ${ Date.now() }`,
+			description: `This product is pretty awesome ${ Date.now() }`,
+			regularPrice: '100.05',
+			salePrice: '99.05',
+		};
+
+		await test.step( 'edit the product name', async () => {
+			await page.getByLabel( 'Product name' ).fill( newProduct.name );
 		} );
-		await api.delete( `products/${ productId }`, {
-			force: true,
+
+		await test.step( 'edit the product description', async () => {
+			await page.locator( '#content-html' ).click(); // text mode to work around iframe
+			await page
+				.locator( '.wp-editor-area' )
+				.first()
+				.fill( newProduct.description );
 		} );
-	} );
 
-	test( 'can edit a product and save the changes', async ( { page } ) => {
-		await page.goto( `wp-admin/post.php?post=${ productId }&action=edit` );
+		await test.step( 'edit the product price', async () => {
+			await page
+				.getByLabel( 'Regular price ($)' )
+				.fill( newProduct.regularPrice );
+			await page
+				.getByLabel( 'Sale price ($)' )
+				.fill( newProduct.salePrice );
+		} );
 
-		// make some edits
-		await page.locator( '#title' ).fill( 'Awesome product' );
-		await page.locator( '#content-html' ).click(); // text mode to work around iframe
-		await page
-			.locator( '.wp-editor-area >> nth=0' )
-			.fill( 'This product is pretty awesome' );
-		await page.locator( '#_regular_price' ).fill( '100.05' );
+		await test.step( 'publish the updated product', async () => {
+			await page.locator( '#publish' ).click();
+		} );
 
-		// publish the edits
-		await page.locator( '#publish' ).click();
-
-		// verify the changes saved
-		await expect( page.locator( '#title' ) ).toHaveValue(
-			'Awesome product'
-		);
-		await expect(
-			page.locator( '.wp-editor-area >> nth=0' )
-		).toContainText( 'This product is pretty awesome' );
-		await expect( page.locator( '#_regular_price' ) ).toHaveValue(
-			'100.05'
-		);
+		await test.step( 'verify the changes', async () => {
+			await expect( page.getByLabel( 'Product name' ) ).toHaveValue(
+				newProduct.name
+			);
+			await expect(
+				page.locator( '.wp-editor-area' ).first()
+			).toContainText( newProduct.description );
+			await expect( page.getByLabel( 'Regular price ($)' ) ).toHaveValue(
+				newProduct.regularPrice
+			);
+			await expect( page.getByLabel( 'Sale price ($)' ) ).toHaveValue(
+				newProduct.salePrice
+			);
+		} );
 	} );
 } );
