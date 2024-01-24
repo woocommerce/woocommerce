081f9d303b1a83d5cebcda8457f9ff4eded9ef9b
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-01-24 15:26:07 +0100

    Move Cart, Checkout and Order Confirmation template customization tests to the parameterized tests file (#43992)
    * Move Cart, Checkout and Order Confirmation template customization tests to the parameterized tests file
    
    * Add changefile(s) from automation for the following project(s): woocommerce-blocks
    
    * Clean up order-confirmation.block_theme.spec.ts file
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce-blocks/tests/e2e/tests/templates/order-confirmation.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/templates/order-confirmation.block_theme.spec.ts
@@ -1,30 +1,7 @@
 /**
  * External dependencies
  */
-import { test as base, expect } from '@woocommerce/e2e-playwright-utils';
-
-/**
- * Internal dependencies
- */
-import { CheckoutPage } from '../checkout/checkout.page';
-import {
-	FREE_SHIPPING_NAME,
-	FREE_SHIPPING_PRICE,
-	SIMPLE_PHYSICAL_PRODUCT_NAME,
-} from '../checkout/constants';
-
-const test = base.extend< { pageObject: CheckoutPage } >( {
-	pageObject: async ( { page }, use ) => {
-		const pageObject = new CheckoutPage( {
-			page,
-		} );
-		await use( pageObject );
-	},
-} );
-
-const templatePath = 'woocommerce/woocommerce//order-confirmation';
-const templateType = 'wp_template';
-const userText = 'Hello World in the template';
+import { test, expect } from '@woocommerce/e2e-playwright-utils';
 
 test.describe( 'Test the order confirmation template', async () => {
 	test( 'Template can be opened in the site editor', async ( {
@@ -84,38 +61,5 @@ test.describe( 'Test the order confirmation template', async () => {
 					exact: true,
 				} )
 		).toBeVisible();
-	} );
-
-	test( 'Template can be modified', async ( {
-		page,
-		admin,
-		editorUtils,
-		frontendUtils,
-		pageObject,
-	} ) => {
-		await admin.visitSiteEditor( {
-			postId: templatePath,
-			postType: templateType,
-		} );
-		await editorUtils.enterEditMode();
-		await editorUtils.editor.insertBlock( {
-			name: 'core/paragraph',
-			attributes: { content: userText },
-		} );
-		await editorUtils.saveTemplate();
-		await frontendUtils.emptyCart();
-		await frontendUtils.goToShop();
-		await frontendUtils.addToCart( SIMPLE_PHYSICAL_PRODUCT_NAME );
-		await frontendUtils.goToCheckout();
-		await expect(
-			await pageObject.selectAndVerifyShippingOption(
-				FREE_SHIPPING_NAME,
-				FREE_SHIPPING_PRICE
-			)
-		).toBe( true );
-		await pageObject.fillInCheckoutWithTestData();
-		await pageObject.placeOrder();
-
-		await expect( page.getByText( userText ).first() ).toBeVisible();
 	} );
 } );
