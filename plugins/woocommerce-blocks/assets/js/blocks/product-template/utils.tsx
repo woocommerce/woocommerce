3c3f0bb6ba2701e67e494fdfb12439e7ab9c6d52
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-01-24 11:23:48 +0100

    Import core data store instead of hardcoding store name

--- plugins/woocommerce-blocks/assets/js/blocks/product-template/utils.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-template/utils.tsx
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import { store as coreStore, resolveSelect } from '@wordpress/data';
+import { resolveSelect } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 import { useState, useEffect } from '@wordpress/element';
 // import { store as editorStore } from '@wordpress/editor';
 import { isNumber } from '@woocommerce/types';
@@ -26,20 +27,18 @@ export const useGetLocation = ( context ) => {
 	// 	return select( editorStore ).getCurrentPostId();
 	// } );
 
-	// Case 1.2: Product context, specific ID
-	//           - Single Product Block
+	// Case 1.2: Product context, specific ID - Single Product Block
 	if ( isNumber( postId ) ) {
 		return createLocationObject( 'product', { productId: postId } );
 	}
 
-	// Case 1.2: Product context, specific ID
-	//           - Specific Single Product Template
+	// Case 1.2: Product context, specific ID - Specific Single Product Template
 	if (
 		templateSlug.includes( 'single-product' ) &&
 		templateSlug !== 'single-product'
 	) {
-		const [ , slug ] = templateSlug.split( 'single-product' );
 		useEffect( () => {
+			const [ , slug ] = templateSlug.split( 'single-product-' );
 			const getProductId = async () => {
 				const response = ( await resolveSelect(
 					coreStore
@@ -51,20 +50,20 @@ export const useGetLocation = ( context ) => {
 				setProductId( productId );
 			};
 
-			getProductId();
+			if ( slug ) {
+				getProductId();
+			}
 		}, [ templateSlug ] );
 
 		return createLocationObject( 'product', { productId } );
 	}
 
-	// Case 1.1: Product context.
-	//           - Single Product Template
+	// Case 1.1: Product context - Single Product Template
 	if ( templateSlug === 'single-product' ) {
 		return createLocationObject( 'product', { postId: null } );
 	}
 
-	// Case 2.1: Taxonomy context.
-	// 			 - Specific Taxonomy Template
+	// Case 2.1: Taxonomy context - Specific Taxonomy Template
 	if (
 		templateSlug.includes( 'taxonomy-product_cat' ) &&
 		templateSlug !== 'taxonomy-product_cat'
@@ -75,8 +74,7 @@ export const useGetLocation = ( context ) => {
 			attr: 'TODO',
 		} );
 	}
-	// Case 2.2: Taxonomy context, specific ID
-	//           - Products by * Templates
+	// Case 2.2: Taxonomy context, specific ID - Products by * Templates
 	if ( templateSlug === 'taxonomy-product_cat' ) {
 		return createLocationObject( 'archive', {
 			tag: null,
@@ -84,7 +82,7 @@ export const useGetLocation = ( context ) => {
 			attr: null,
 		} );
 	}
-	// Case 3: Cart context
+	// Case 3: Cart context:
 	//           - Cart template
 	//           - Checkout template
 	//           - Mini Cart template part
@@ -94,14 +92,12 @@ export const useGetLocation = ( context ) => {
 		} );
 	}
 
-	// Case 4: Order context
-	//           - Order confirmation template
+	// Case 4: Order context - Order confirmation template
 	if ( templateSlug === 'order-confirmation' ) {
 		return createLocationObject( 'order', {
 			orderId: null,
 		} );
 	}
-	// Case 5: Generic context
-	//           - Others
+	// Case 5: Generic context - Others
 	return createLocationObject( 'generic', {} );
 };
