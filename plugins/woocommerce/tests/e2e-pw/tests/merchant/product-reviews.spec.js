0fc6bfbd54e9caddb8be32896bb3791011ddf421
Ivan Stojadinov <ivan.stojadinov@automattic.com>
2024-01-25 11:51:54 +0100

    Add "can view product review" test

---
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/product-reviews.spec.js
@@ -0,0 +1,55 @@
+const { test, expect } = require( '@playwright/test' );
+const wcApi = require( '@woocommerce/woocommerce-rest-api' ).default;
+
+const timestamp = Date.now().toString();
+const productName = `Review me ${ timestamp }`;
+const productReview = `Nice one, Playwright! ${ timestamp }`;
+
+test.describe( 'Products > Reviews', () => {
+	test.use( { storageState: process.env.ADMINSTATE } );
+
+	let productId, api;
+
+	test.beforeAll( async ( { baseURL } ) => {
+		api = new wcApi( {
+			url: baseURL,
+			consumerKey: process.env.CONSUMER_KEY,
+			consumerSecret: process.env.CONSUMER_SECRET,
+			version: 'wc/v3',
+		} );
+		await api
+			.post( 'products', {
+				name: productName,
+				type: 'simple',
+				regular_price: '9.99',
+			} )
+			.then( ( response ) => {
+				productId = response.data.id;
+			} );
+		await api.post( 'products/reviews', {
+			product_id: productId,
+			review: productReview,
+			reviewer: 'John Doe',
+			reviewer_email: 'john.doe@example.com',
+			rating: 5,
+		} );
+	} );
+
+	test.afterAll( async ( {} ) => {
+		await api.delete( `products/${ productId }`, {
+			force: true,
+		} );
+	} );
+
+	test( 'can view product review', async ( { page } ) => {
+		await page.goto(
+			`wp-admin/edit.php?post_type=product&page=product-reviews`
+		);
+		await expect(
+			page.getByRole( 'cell', { name: productReview } )
+		).toBeVisible();
+		await expect(
+			page.getByRole( 'link', { name: productName } )
+		).toBeVisible();
+	} );
+} );
