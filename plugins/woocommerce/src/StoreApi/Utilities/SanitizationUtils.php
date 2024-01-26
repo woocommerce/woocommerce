c3a5cac5f734a2c7271494b5146de5222a33f3dd
Thomas Roberts <thomas.roberts@automattic.com>
2024-01-26 16:18:06 +0000

    Handle passing non-array types

--- plugins/woocommerce/src/StoreApi/Utilities/SanitizationUtils.php
+++ plugins/woocommerce/src/StoreApi/Utilities/SanitizationUtils.php
@@ -10,13 +10,18 @@ class SanitizationUtils {
 	/**
 	 * Runs wp_kses on an array. This function runs wp_kses on strings in the array and recurses into arrays.
 	 *
-	 * @param array $array The array to run wp_kses on.
-	 * @return array       The array, all string keys will have been run through wp_kses.
+	 * @param  array $array The array to run wp_kses on.
+	 * @return mixed       The array, all string keys will have been run through wp_kses.
 	 */
-	public function kses_array( $array ) {
-		foreach( $array as $key => $value ) {
+	public function wp_kses_array( $array ) {
+		if ( ! is_countable( $array ) ) {
+			_doing_it_wrong( __METHOD__, 'This function expects an array.', '8.7.0' );
+			return $array;
+		}
+
+		foreach ( $array as $key => $value ) {
 			if ( is_array( $value ) ) {
-				$array[ $key ] = $this->kses_array( $value );
+				$array[ $key ] = $this->wp_kses_array( $value );
 			}
 			if ( is_string( $value ) ) {
 				$array[ $key ] = wp_kses( $value, [] );
