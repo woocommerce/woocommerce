d63adc3eba93b49af8922117cac5388fa5f2915c
Sam Seay <samueljseay@gmail.com>
2024-01-30 23:22:10 +0800

    Add e2e tests for the active filters

---
+++ plugins/woocommerce-blocks/tests/e2e/tests/filter-blocks/active-filters.block_theme.spec.ts
@@ -0,0 +1,114 @@
+/**
+ * External dependencies
+ */
+import { test, expect } from '@woocommerce/e2e-playwright-utils';
+
+test.describe( 'Product Filter: Active Filters Block', async () => {
+	describe( 'frontend', () => {
+		test( 'Without any filters selected, only a wrapper block is rendered', async ( {
+			page,
+		} ) => {
+			await page.goto( '/product-filters-active-block/' );
+
+			const locator = page.locator(
+				'.wp-block-woocommerce-product-filter'
+			);
+
+			const count = await locator.count();
+			expect( count ).toBe( 1 );
+
+			const html = await locator.innerHTML();
+			expect( html.trim() ).toBe( '' );
+		} );
+
+		test( 'With rating filters applied it shows the correct active filters', async ( {
+			page,
+		} ) => {
+			await page.goto(
+				'/product-filters-active-block/?rating_filter=1,2,5'
+			);
+
+			const listLocator = page.locator( '.filter-list' );
+			const hasTitle =
+				( await listLocator.locator( 'text=Rating:' ).count() ) === 1;
+
+			expect( hasTitle ).toBe( true );
+
+			for ( const text of [
+				'Rated 1 out of 5',
+				'Rated 2 out of 5',
+				'Rated 5 out of 5',
+			] ) {
+				const hasFilter =
+					( await listLocator
+						.locator( `text=${ text }` )
+						.count() ) === 1;
+				expect( hasFilter ).toBe( true );
+			}
+		} );
+
+		test( 'With stock filters applied it shows the correct active filters', async ( {
+			page,
+		} ) => {
+			await page.goto(
+				'/product-filters-active-block/?filter_stock_status=instock,outofstock'
+			);
+
+			const listLocator = page.locator( '.filter-list' );
+			const hasTitle =
+				( await listLocator.locator( 'text=Rating:' ).count() ) === 1;
+
+			expect( hasTitle ).toBe( true );
+
+			for ( const text of [ 'In stock', 'Out of stock' ] ) {
+				const hasFilter =
+					( await listLocator
+						.locator( `text=${ text }` )
+						.count() ) === 1;
+				expect( hasFilter ).toBe( true );
+			}
+		} );
+
+		test( 'With attribute filters applied it shows the correct active filters', async ( {
+			page,
+		} ) => {
+			await page.goto(
+				'/product-filters-active-block/?filter_color=blue,gray&query_type_color=or'
+			);
+
+			const listLocator = page.locator( '.filter-list' );
+			const hasTitle =
+				( await listLocator.locator( 'text=Color:' ).count() ) === 1;
+
+			expect( hasTitle ).toBe( true );
+
+			for ( const text of [ 'Blue', 'Gray' ] ) {
+				const hasFilter =
+					( await listLocator
+						.locator( `text=${ text }` )
+						.count() ) === 1;
+				expect( hasFilter ).toBe( true );
+			}
+		} );
+
+		test( 'With price filters applied it shows the correct active filters', async ( {
+			page,
+		} ) => {
+			await page.goto(
+				'/product-filters-active-block/?min_price=17&max_price=71'
+			);
+
+			const listLocator = page.locator( '.filter-list' );
+			const hasTitle =
+				( await listLocator.locator( 'text=Price:' ).count() ) === 1;
+
+			expect( hasTitle ).toBe( true );
+
+			const hasFilter =
+				( await listLocator
+					.locator( `text=Between $17 and $71` )
+					.count() ) === 1;
+			expect( hasFilter ).toBe( true );
+		} );
+	} );
+} );
