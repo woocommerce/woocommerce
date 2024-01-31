976ba01dcbebcfa4b07b2d9932b93a77163b4391
Tung Du <dinhtungdu@gmail.com>
2024-01-31 14:40:24 +0700

    fix: and query and variable products
    given three attribute terms: red, blue, green
    - product alpha has red and blue variations
    - product beta has blue and green variations
    
    A filter query for blue and red with query type set to 'and' will return both products, while only alpha should be return.
    
    This commit fix that issue.

--- plugins/woocommerce/src/Internal/ProductQueryFilters/FilterClausesGenerator.php
+++ plugins/woocommerce/src/Internal/ProductQueryFilters/FilterClausesGenerator.php
@@ -187,6 +187,8 @@ class FilterClausesGenerator implements ClausesGeneratorInterface {
 				WHERE is_variation_attribute=1
 				{$in_stock_clause}
 				AND term_id in {$term_ids_to_filter_by_list}
+				GROUP BY product_or_parent_id
+				HAVING COUNT(product_or_parent_id)>={$count}
 			)";
 		}
 
