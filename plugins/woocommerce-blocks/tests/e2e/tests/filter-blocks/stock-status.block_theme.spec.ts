ca0f0b2c89d0b86701e1d412092f5b6ec877255b
Sam Seay <samueljseay@gmail.com>
2024-01-31 13:16:05 +0800

    Add some basic working e2e tests of stock status filtering.

--- plugins/woocommerce-blocks/tests/e2e/tests/filter-blocks/stock-status.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/filter-blocks/stock-status.block_theme.spec.ts
@@ -9,29 +9,38 @@ test.describe( 'Product Filter: Stock Status Block', async () => {
 			page,
 		} ) => {
 			await page.goto( '/product-filters-stock-status-block/' );
-			await page.evaluate( () =>
-				window.scrollTo( 0, document.body.scrollHeight * 0.35 )
+
+			const stockStatuses = page.locator(
+				'.wc-block-components-checkbox__label'
 			);
 
-			const ratingStars = page.locator(
-				'.wc-block-components-product-rating__stars'
-			);
-			const count = await ratingStars.count();
-			expect( count ).toBe( 2 );
+			await expect( stockStatuses ).toHaveCount( 2 );
 
-			//  See bin/scripts/parallel/reviews.sh for reviews data.
-			await expect( ratingStars.nth( 0 ) ).toHaveAttribute(
-				'aria-label',
-				'Rated 1 out of 5'
-			);
-			await expect( ratingStars.nth( 1 ) ).toHaveAttribute(
-				'aria-label',
-				'Rated 5 out of 5'
-			);
+			await expect( stockStatuses.nth( 0 ) ).toHaveText( 'In stock' );
+			await expect( stockStatuses.nth( 1 ) ).toHaveText( 'Out of stock' );
 		} );
 
 		test( 'Selecting a stock status filters the list of products', async ( {
 			page,
-		} ) => {} );
+		} ) => {
+			await page.goto( '/product-filters-stock-status-block/' );
+
+			const stockStatusCheckboxes = page.locator(
+				'.wc-block-components-checkbox__input'
+			);
+
+			// Out of stock
+			stockStatusCheckboxes.nth( 1 ).check();
+
+			// wait for navigation
+			await page.waitForURL(
+				'/product-filters-stock-status-block/?filter_stock_status=outofstock'
+			);
+
+			const products = page.locator( '.wc-block-product' );
+			const productCount = await products.count();
+
+			expect( productCount ).toBe( 1 );
+		} );
 	} );
 } );
