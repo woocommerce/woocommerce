7c8eb3b96ac6955b720e25a42a12e2a2883717e8
Sam Seay <samueljseay@gmail.com>
2024-01-31 12:43:57 +0800

    Add additional reviews to a separate product to show more review options in the e2e test.

--- plugins/woocommerce-blocks/tests/e2e/tests/filter-blocks/rating-filter.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/filter-blocks/rating-filter.block_theme.spec.ts
@@ -17,17 +17,17 @@ test.describe( 'Product Filter: Rating Filter Block', async () => {
 				'.wc-block-components-product-rating__stars'
 			);
 			const count = await ratingStars.count();
-			expect( count ).toBe( 1 );
+			expect( count ).toBe( 2 );
 
 			//  See bin/scripts/parallel/reviews.sh for reviews data.
 			await expect( ratingStars.nth( 0 ) ).toHaveAttribute(
 				'aria-label',
-				'Rated 4 out of 5'
+				'Rated 1 out of 5'
 			);
-			// await expect( ratingStars.nth( 1 ) ).toHaveAttribute(
-			// 	'aria-label',
-			// 	'Rated 5 out of 5'
-			// );
+			await expect( ratingStars.nth( 1 ) ).toHaveAttribute(
+				'aria-label',
+				'Rated 5 out of 5'
+			);
 		} );
 	} );
 } );
