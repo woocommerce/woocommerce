f7b0d00f20ec648e95265de8c8fb84f4fe3c3ba8
Thomas Roberts <thomas.roberts@automattic.com>
2024-02-01 14:53:37 +0000

    Update comments in bash script

--- plugins/woocommerce-blocks/tests/e2e/bin/scripts/parallel/themes.sh
+++ plugins/woocommerce-blocks/tests/e2e/bin/scripts/parallel/themes.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# Get the directory of the current script
+# Get the directory of the current script.
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Navigate two levels up and then to the themes directory
+# The theme dir is two levels up from where this script is running.
 themes_dir="$script_dir/../../themes"
 
-# Delete existing themes if they exist
+# Delete the child themes if they already exist.
 wp theme delete storefront-child
 wp theme delete twentytwentyfour-child
 
-# Install and activate the themes
+# Install the child themes.
 wp theme install "$themes_dir/storefront-child.zip"
 wp theme install "$themes_dir/twentytwentyfour-child.zip"
