e916acb5064c5851606fff96063da11de583e1aa
Maikel Perez <maikel.perez@automattic.com>
2024-02-01 11:18:23 -0300

    Add common options to the variations options empty state (#44001)
    * Always show Variation options and Variations sections within the Variations tab
    
    * Remove woocommerce/product-variations-fields block since it's not needed anymore
    
    * Create ProductTShirt image for variation options empty state
    
    * Add renderCustomEmptyState to the attribute control component to be able to set a not default empty state
    
    * Render a custom empty state for variation options
    
    * Adds defaultSearch prop to NewAttributeModal so it can be used to start searching right after the modal is shown
    
    * Let the empty state adds an attribute that matches a given text
    
    * Add changelog files
    
    * Fix linter errors

--- packages/js/product-editor/src/components/attribute-control/attribute-control.tsx
+++ packages/js/product-editor/src/components/attribute-control/attribute-control.tsx
@@ -35,43 +35,7 @@ import { RemoveConfirmationModal } from '../remove-confirmation-modal';
 import { TRACKS_SOURCE } from '../../constants';
 import { AttributeEmptyStateSkeleton } from './attribute-empty-state-skeleton';
 import { SectionActions } from '../block-slot-fill';
-
-type AttributeControlProps = {
-	value: EnhancedProductAttribute[];
-	onAdd?: ( attribute: EnhancedProductAttribute[] ) => void;
-	onAddAnother?: () => void;
-	onRemoveItem?: () => void;
-	onChange: ( value: ProductAttribute[] ) => void;
-	onEdit?: ( attribute: ProductAttribute ) => void;
-	onRemove?: ( attribute: ProductAttribute ) => void;
-	onRemoveCancel?: ( attribute: ProductAttribute ) => void;
-	onNewModalCancel?: () => void;
-	onNewModalClose?: () => void;
-	onNewModalOpen?: () => void;
-	onEditModalCancel?: ( attribute?: ProductAttribute ) => void;
-	onEditModalClose?: ( attribute?: ProductAttribute ) => void;
-	onEditModalOpen?: ( attribute?: ProductAttribute ) => void;
-	onNoticeDismiss?: () => void;
-	createNewAttributesAsGlobal?: boolean;
-	useRemoveConfirmationModal?: boolean;
-	disabledAttributeIds?: number[];
-	termsAutoSelection?: 'first' | 'all';
-	defaultVisibility?: boolean;
-	uiStrings?: {
-		notice?: string | React.ReactElement;
-		emptyStateSubtitle?: string;
-		newAttributeListItemLabel?: string;
-		newAttributeModalTitle?: string;
-		newAttributeModalDescription?: string | React.ReactElement;
-		newAttributeModalNotice?: string;
-		customAttributeHelperMessage?: string;
-		attributeRemoveLabel?: string;
-		attributeRemoveConfirmationMessage?: string;
-		attributeRemoveConfirmationModalMessage?: string;
-		globalAttributeHelperMessage?: string;
-		disabledAttributeMessage?: string;
-	};
-};
+import { AttributeControlProps } from './types';
 
 export const AttributeControl: React.FC< AttributeControlProps > = ( {
 	value,
@@ -89,6 +53,7 @@ export const AttributeControl: React.FC< AttributeControlProps > = ( {
 	onRemove = () => {},
 	onRemoveCancel = () => {},
 	onNoticeDismiss = () => {},
+	renderCustomEmptyState = () => <AttributeEmptyStateSkeleton />,
 	uiStrings,
 	createNewAttributesAsGlobal = false,
 	useRemoveConfirmationModal = false,
@@ -109,6 +74,8 @@ export const AttributeControl: React.FC< AttributeControlProps > = ( {
 		...uiStrings,
 	};
 	const [ isNewModalVisible, setIsNewModalVisible ] = useState( false );
+	const [ defaultAttributeSearch, setDefaultAttributeSearch ] =
+		useState< string >();
 	const [ removingAttribute, setRemovingAttribute ] =
 		useState< null | ProductAttribute >();
 	const [ currentAttributeId, setCurrentAttributeId ] = useState<
@@ -161,6 +128,7 @@ export const AttributeControl: React.FC< AttributeControlProps > = ( {
 
 	const closeNewModal = () => {
 		setIsNewModalVisible( false );
+		setDefaultAttributeSearch( undefined );
 		onNewModalClose();
 	};
 
@@ -311,6 +279,7 @@ export const AttributeControl: React.FC< AttributeControlProps > = ( {
 					}
 					termsAutoSelection={ termsAutoSelection }
 					defaultVisibility={ defaultVisibility }
+					defaultSearch={ defaultAttributeSearch }
 				/>
 			) }
 			<SelectControlMenuSlot />
@@ -376,9 +345,14 @@ export const AttributeControl: React.FC< AttributeControlProps > = ( {
 					} }
 				/>
 			) }
-			{ ! isMobileViewport && value.length === 0 && (
-				<AttributeEmptyStateSkeleton />
-			) }
+			{ ! isMobileViewport &&
+				value.length === 0 &&
+				renderCustomEmptyState( {
+					addAttribute( search ) {
+						setDefaultAttributeSearch( search );
+						openNewModal();
+					},
+				} ) }
 		</div>
 	);
 };
