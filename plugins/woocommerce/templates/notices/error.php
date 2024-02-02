e6d33a00b9c48bbfb09cdd9152490e9048bdcb8c
Thomas Roberts <thomas.roberts@automattic.com>
2024-02-02 10:25:15 +0000

    Prevent phpcs warning for not escaped output
    It is escaped in the wc_get_notice_data_attr function

--- plugins/woocommerce/templates/notices/error.php
+++ plugins/woocommerce/templates/notices/error.php
@@ -26,7 +26,7 @@ if ( ! $notices ) {
 ?>
 <ul class="woocommerce-error" role="alert">
 	<?php foreach ( $notices as $notice ) : ?>
-		<li<?php echo wc_get_notice_data_attr( $notice ); ?>>
+		<li<?php echo wc_get_notice_data_attr( $notice ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
 			<?php echo wc_kses_notice( $notice['notice'] ); ?>
 		</li>
 	<?php endforeach; ?>
--- plugins/woocommerce/templates/notices/success.php
+++ plugins/woocommerce/templates/notices/success.php
@@ -24,7 +24,7 @@ if ( ! $notices ) {
 ?>
 
 <?php foreach ( $notices as $notice ) : ?>
-	<div class="woocommerce-message"<?php echo wc_get_notice_data_attr( $notice ); ?> role="alert">
+	<div class="woocommerce-message"<?php echo wc_get_notice_data_attr( $notice ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?> role="alert">
 		<?php echo wc_kses_notice( $notice['notice'] ); ?>
 	</div>
 <?php endforeach; ?>
