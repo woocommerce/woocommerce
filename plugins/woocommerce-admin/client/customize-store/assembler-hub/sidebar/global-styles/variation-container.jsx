f3a232d52aded50acda0c6aa0f0fbb2a90700033
Luigi Teschio <gigitux@gmail.com>
2024-02-02 16:44:34 +0100

    add comment

--- plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/global-styles/variation-container.jsx
+++ plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/global-styles/variation-container.jsx
@@ -75,7 +75,8 @@ export const VariationContainer = ( { variation, children } ) => {
 		if ( variation.settings.color ) {
 			return isEqual( variation.settings.color, user.settings.color );
 		}
-
+		// With the Font Library, the fontFamilies object contains an array of font families installed with the Font Library under the key 'custom'.
+		// We need to compare only the active theme font families, so we compare the theme font families with the current variation.
 		const { theme } = user.settings.typography.fontFamilies;
 
 		return isEqual( variation.settings.typography, {
