27a375b6b381f00cf63f7726b4752f638fbe02c3
Sam Seay <samueljseay@gmail.com>
2024-02-02 13:49:06 +0800

    Remove assertions that are just testing WP, we don't need them in project setup files.

--- plugins/woocommerce-blocks/tests/e2e/block-theme.setup.ts
+++ plugins/woocommerce-blocks/tests/e2e/block-theme.setup.ts
@@ -1,21 +1,13 @@
 /**
  * External dependencies
  */
-import {
-	BLOCK_THEME_NAME,
-	BLOCK_THEME_SLUG,
-	cli,
-} from '@woocommerce/e2e-utils';
-import { test as setup, expect } from '@woocommerce/e2e-playwright-utils';
+import { BLOCK_THEME_SLUG, cli } from '@woocommerce/e2e-utils';
+import { test as setup } from '@woocommerce/e2e-playwright-utils';
 
-setup( 'Sets up the block theme', async ( { admin } ) => {
+setup( 'Sets up the block theme', async () => {
 	await cli(
 		`npm run wp-env run tests-cli -- wp theme install ${ BLOCK_THEME_SLUG } --activate`
 	);
-	await admin.page.goto( '/wp-admin/themes.php' );
-	await expect(
-		admin.page.getByText( `Active: ${ BLOCK_THEME_NAME }` )
-	).toBeVisible();
 	// Enable permalinks.
 	await cli(
 		`npm run wp-env run tests-cli -- wp rewrite structure /%postname%/ --hard`
--- plugins/woocommerce-blocks/tests/e2e/classic-theme.setup.ts
+++ plugins/woocommerce-blocks/tests/e2e/classic-theme.setup.ts
@@ -1,19 +1,11 @@
 /**
  * External dependencies
  */
-import {
-	CLASSIC_THEME_NAME,
-	CLASSIC_THEME_SLUG,
-	cli,
-} from '@woocommerce/e2e-utils';
-import { test as setup, expect } from '@woocommerce/e2e-playwright-utils';
+import { CLASSIC_THEME_SLUG, cli } from '@woocommerce/e2e-utils';
+import { test as setup } from '@woocommerce/e2e-playwright-utils';
 
-setup( 'Sets up the classic theme', async ( { admin } ) => {
+setup( 'Sets up the classic theme', async () => {
 	await cli(
 		`npm run wp-env run tests-cli -- wp theme install ${ CLASSIC_THEME_SLUG } --activate`
 	);
-	await admin.page.goto( '/wp-admin/themes.php' );
-	await expect(
-		admin.page.getByText( `Active: ${ CLASSIC_THEME_NAME }` )
-	).toBeVisible();
 } );
