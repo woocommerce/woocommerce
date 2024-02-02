fced9de08250b45889b104916e7f65a203c7ac1d
Matt Sherman <matt@jam123.com>
2024-02-01 21:22:21 -0500

    Initial useProductFormTemplate (does not use post type)

---
+++ packages/js/product-editor/src/hooks/use-product-form-template/use-product-form-template.ts
@@ -0,0 +1,46 @@
+/**
+ * External dependencies
+ */
+import { useMemo } from '@wordpress/element';
+import {
+	useLayoutTemplate,
+	useLayoutTemplates,
+} from '@woocommerce/block-templates';
+import { ProductType } from '@woocommerce/data';
+
+export const useProductFormTemplate = (
+	productFormTemplateId: string | undefined,
+	productType: ProductType | undefined,
+	postType: string | undefined
+) => {
+	const { layoutTemplates, isResolving: isLayoutTemplatesResolving } =
+		useLayoutTemplates();
+
+	const bestMatchingLayoutTemplateId = useMemo( () => {
+		if ( ! layoutTemplates ) {
+			return null;
+		}
+
+		let matchingLayoutTemplate = layoutTemplates.find(
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			( layoutTemplate: any ) =>
+				layoutTemplate.id === productFormTemplateId &&
+				layoutTemplate.productData?.type === productType
+		);
+
+		if ( ! matchingLayoutTemplate ) {
+			matchingLayoutTemplate = layoutTemplates.find(
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				( layoutTemplate: any ) =>
+					layoutTemplate.productData?.type === productType
+			);
+		}
+
+		return matchingLayoutTemplate?.id;
+	}, [ layoutTemplates, productFormTemplateId, productType, postType ] );
+
+	const { layoutTemplate, isResolving: isLayoutTemplateResolving } =
+		useLayoutTemplate( bestMatchingLayoutTemplateId );
+
+	return [ layoutTemplate, isLayoutTemplateResolving ];
+};
