f75d615bfdad3dbae19c21d40ad1dbcb6601c5db
Damián Suárez <rdsuarez@gmail.com>
2024-02-05 16:19:14 -0300

    extend core/paragraph

---
+++ packages/js/product-editor/src/blocks/generic/text-area/extend/core-paragraph/index.ts
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import type { BlockConfiguration } from '@wordpress/blocks';
+/**
+ * Internal dependencies
+ */
+import coreParagraphBlockEditChildTextArea from './components/core-paragraph-with-text-area-role';
+
+/**
+ * Extend core/paragraph block with attributes
+ * to
+ *
+ * @param {BlockConfiguration} settings - The block settings.
+ * @param {string}             name     - The block name.
+ * @return {BlockConfiguration} The updated block settings.
+ */
+export default function coreParagraphChildOfTextAreaField(
+	settings: BlockConfiguration,
+	name: string
+) {
+	if ( name !== 'core/paragraph' ) {
+		return settings;
+	}
+
+	// Use `product-editor/entity-prop` context.
+	const usesContext = [
+		...( settings.usesContext, [] ),
+		'product-editor/entity-prop',
+	];
+
+	return {
+		...settings,
+		usesContext,
+		edit: settings.edit
+			? coreParagraphBlockEditChildTextArea( settings.edit )
+			: null,
+	};
+}
