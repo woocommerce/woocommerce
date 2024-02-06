ace859a8842033a25624bcc67cf570274bb8544e
Thomas Roberts <thomas.roberts@automattic.com>
2024-02-06 17:26:15 +0000

    Add test to ensure fields are saved across orders

--- plugins/woocommerce-blocks/tests/e2e/tests/checkout/additional-fields.shopper.block_theme.side_effects.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/checkout/additional-fields.shopper.block_theme.side_effects.spec.ts
@@ -46,6 +46,7 @@ test.describe( 'Shopper → Additional Checkout Fields', () => {
 
 		test( 'Shopper can fill in the checkout form with additional fields and can have different value for same field in shipping and billing address', async ( {
 			checkoutPageObject,
+			frontendUtils,
 		} ) => {
 			await checkoutPageObject.editShippingDetails();
 			await checkoutPageObject.unsyncBillingWithShipping();
@@ -169,6 +170,97 @@ test.describe( 'Shopper → Additional Checkout Fields', () => {
 					'Is this a personal purchase or a business purchase?business'
 				)
 			).toBeVisible();
+
+			await frontendUtils.emptyCart();
+			await frontendUtils.goToShop();
+			await frontendUtils.addToCart( REGULAR_PRICED_PRODUCT_NAME );
+			await frontendUtils.goToCheckout();
+
+			await checkoutPageObject.editShippingDetails();
+			await checkoutPageObject.editBillingDetails();
+
+			// Now check all the fields previously filled are still filled on a fresh checkout.
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Contact information',
+					} )
+					.getByLabel(
+						'Enter a gift message to include in the package'
+					)
+			).toHaveValue( 'This is for you!' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Contact information',
+					} )
+					.getByLabel(
+						'Is this a personal purchase or a business purchase?'
+					)
+			).toHaveValue( 'Business' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Contact information',
+					} )
+					.getByLabel( 'Do you want to subscribe to our newsletter?' )
+			).toBeChecked();
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Shipping address',
+					} )
+					.getByLabel( 'Government ID', { exact: true } )
+			).toHaveValue( '12345' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Shipping address',
+					} )
+					.getByLabel( 'Confirm Government ID' )
+			).toHaveValue( '12345' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Shipping address',
+					} )
+					.getByLabel( 'Can a truck fit down your road?' )
+			).toBeChecked();
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Shipping address',
+					} )
+					.getByLabel( 'How wide is your road?' )
+			).toHaveValue( 'Wide' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Billing address',
+					} )
+					.getByLabel( 'Government ID', { exact: true } )
+			).toHaveValue( '54321' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Billing address',
+					} )
+					.getByLabel( 'Confirm Government ID' )
+			).toHaveValue( '54321' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Billing address',
+					} )
+					.getByLabel( 'Can a truck fit down your road?' )
+			).not.toBeChecked();
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Billing address',
+					} )
+					.getByLabel( 'How wide is your road?' )
+			).toHaveValue( 'Narrow' );
 		} );
 
 		test( 'Shopper can see an error message when a required field is not filled in the checkout form', async ( {
