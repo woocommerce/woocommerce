7edbf8f36e94fffd0e64884f1a71eca6de91b763
Thilina Hasantha <thilina.hasantha@gmail.com>
2024-02-07 12:34:31 +0100

    Utility class for Woo Marketplace plugin.

---
+++ plugins/woocommerce/includes/admin/helper/class-wc-helper-plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Woo Marketplace plugin manager.
+ *
+ * @class WC_Helper_Updater
+ * @package WooCommerce\Admin\Helper
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * WC_Helper_Plugin Class
+ *
+ * Contains the logic to manage the Woo Marketplace plugin.
+ */
+class WC_Helper_Plugin {
+	const WOO_MARKETPLACE_PLUGIN_MAIN_FILE = 'woo-marketplace/woo-marketplace.php';
+
+	/**
+	 * Check if the marketplace plugin is active.
+	 *
+	 * @return bool
+	 */
+	public function is_plugin_active(): bool {
+		return is_plugin_active_for_network( self::WOO_MARKETPLACE_PLUGIN_MAIN_FILE ) || is_plugin_active( self::WOO_MARKETPLACE_PLUGIN_MAIN_FILE );
+	}
+}
