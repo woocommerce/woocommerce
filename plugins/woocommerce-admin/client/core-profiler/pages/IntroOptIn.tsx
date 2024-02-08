e31372df10323a296cb56bbebe4961c8961f4ea3
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2024-02-08 18:51:17 -0300

    Remove the closing round brackets

--- plugins/woocommerce-admin/client/core-profiler/pages/IntroOptIn.tsx
+++ plugins/woocommerce-admin/client/core-profiler/pages/IntroOptIn.tsx
@@ -71,7 +71,7 @@ export const IntroOptIn = ( {
 						className="core-profiler__checkbox"
 						label={ interpolateComponents( {
 							mixedString: __(
-								'I agree to share my data to tailor my store setup experience, get more relevant content, and help make WooCommerce better for everyone. You can opt out at any time in WooCommerce settings. {{link}}Learn more about usage tracking.{{/link}})',
+								'I agree to share my data to tailor my store setup experience, get more relevant content, and help make WooCommerce better for everyone. You can opt out at any time in WooCommerce settings. {{link}}Learn more about usage tracking.{{/link}}',
 								'woocommerce'
 							),
 							components: {
