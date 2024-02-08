286c760cc9a860a8c6457a20f6a6b2c7fe1ac675
Álvaro Thomas <alvaro.thomas@automattic.com>
2024-02-08 13:50:44 +0100

    Added test data in fixture and changelog extra line
    Change test data use from constants at the top to test data created in a fixture for each test. Also, added extra line to the changelog file.

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/product-reviews.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/product-reviews.spec.js
@@ -1,121 +1,136 @@
-const { test, expect } = require( '@playwright/test' );
+const { test: baseTest, expect } = require( '@playwright/test' );
 const wcApi = require( '@woocommerce/woocommerce-rest-api' ).default;
 
-const timestamp = Date.now().toString();
-const productName = `Review me ${ timestamp }`;
-const productReview = `Nice one, Playwright! ${ timestamp }`;
-const randomRating = ( Math.random() * ( 5 - 1 ) + 1 ).toFixed( 0 );
-const reviewerName = 'John Doe';
-const reviewerEmail = `john.doe.${ timestamp }@example.com`;
-const updatedReview = `(edited ${ timestamp })`;
+baseTest.describe( 'Product Reviews > Edit Product Review', () => {
+	baseTest.use( { storageState: process.env.ADMINSTATE } );
 
-test.describe( 'Products > Reviews', () => {
-	test.use( { storageState: process.env.ADMINSTATE } );
-
-	let productId, api;
-
-	test.beforeAll( async ( { baseURL } ) => {
-		api = new wcApi( {
-			url: baseURL,
-			consumerKey: process.env.CONSUMER_KEY,
-			consumerSecret: process.env.CONSUMER_SECRET,
-			version: 'wc/v3',
-		} );
-		await api
-			.post( 'products', {
-				name: productName,
-				type: 'simple',
-				regular_price: '9.99',
-			} )
-			.then( ( response ) => {
-				productId = response.data.id;
+	const test = baseTest.extend( {
+		api: async ( { baseURL }, use ) => {
+			const api = new wcApi( {
+				url: baseURL,
+				consumerKey: process.env.CONSUMER_KEY,
+				consumerSecret: process.env.CONSUMER_SECRET,
+				version: 'wc/v3',
 			} );
-		await api.post( 'products/reviews', {
-			product_id: productId,
-			review: productReview,
-			reviewer: reviewerName,
-			reviewer_email: reviewerEmail,
-			rating: randomRating,
-		} );
+
+			await use( api );
+		},
+		testData: async ( { api }, use ) => {
+			const timestamp = Date.now().toString();
+			let product = {};
+			let review = {};
+
+			// Create the product
+			await api
+				.post( 'products', {
+					name: `Review me ${ timestamp }`,
+					type: 'simple',
+					regular_price: '9.99',
+				} )
+				.then( ( response ) => {
+					product = response.data;
+				} );
+
+			// Create the product review
+			await api
+				.post( 'products/reviews', {
+					product_id: product.id,
+					review: `Nice one, Playwright! ${ timestamp }`,
+					reviewer: 'John Doe',
+					reviewer_email: `john.doe.${ timestamp }@example.com`,
+					rating: ( Math.random() * ( 5 - 1 ) + 1 ).toFixed( 0 ),
+				} )
+				.then( ( response ) => {
+					review = response.data;
+				} );
+
+			await use( { product, review } );
+
+			// Cleanup
+			await api.delete( `products/reviews/${ review.id }`, {
+				force: true,
+			} );
+			await api.delete( `products/${ product.id }`, {
+				force: true,
+			} );
+		},
 	} );
 
-	test.afterAll( async ( {} ) => {
-		await api.delete( `products/${ productId }`, {
-			force: true,
-		} );
-	} );
-
-	test( 'can view product review', async ( { page } ) => {
+	test( 'can view product review', async ( { page, testData } ) => {
 		await page.goto(
 			`wp-admin/edit.php?post_type=product&page=product-reviews`
 		);
 		await expect(
-			page.getByRole( 'cell', { name: productReview } )
+			page.getByRole( 'cell', { name: testData.review.review } )
 		).toBeVisible();
 		await expect(
-			page.getByRole( 'link', { name: productName } )
+			page.getByRole( 'link', { name: testData.product.name } )
 		).toBeVisible();
 		await expect(
-			page.getByLabel( `${ randomRating } out of 5` )
+			page.getByLabel( `${ testData.review.rating } out of 5` )
 		).toBeVisible();
 		await expect(
-			page.getByRole( 'link', { name: reviewerEmail } )
+			page.getByRole( 'link', { name: testData.review.reviewer_email } )
 		).toBeVisible();
 	} );
 
-	test( 'can edit product review', async ( { page } ) => {
+	test( 'can edit product review', async ( { page, testData } ) => {
 		await page.goto(
 			`wp-admin/edit.php?post_type=product&page=product-reviews`
 		);
 		await expect(
-			page.getByRole( 'cell', { name: productReview } )
+			page.getByRole( 'cell', { name: testData.review.review } )
 		).toBeVisible();
 
 		//Hover over the product review to have 'Edit' displayed
 		await page.hover( '.comment-text' );
 
-		// Select Quick Edit, edit the review and save
+		// Create new review, Quick Edit it and save
+		const EditedtimeStamp = Date.now().toString();
+		const updatedReview = `(edited ${ EditedtimeStamp })`;
 		await page.getByRole( 'button', { name: 'Quick Edit' } ).click();
-		await page.locator( '.wp-editor-area' ).first().fill( updatedReview ); //updatedReview
+		await page.locator( '.wp-editor-area' ).first().fill( updatedReview );
 		await page.getByRole( 'button', { name: 'Update Comment' } ).click();
-		await page.waitForTimeout( 2000 );
 
 		// Verify that the edited comment is there
-		const commentTextElement = page.locator( '.comment-text' );
-		await commentTextElement.waitFor( { state: 'visible' } );
-		const updatedComment = await commentTextElement.innerText(); //updatedComment
-		await expect( updatedComment ).toBe( updatedReview ); //updatedComment updatedReview
+		const commentText = page.locator( '.comment-text' );
+		await commentText.waitFor( { state: 'visible' } );
+		const updatedComment = await commentText.innerText();
+		await expect( updatedComment ).toBe( updatedReview );
 	} );
 
-	test( 'can delete product review', async ( { page } ) => {
+	test( 'can delete product review', async ( { page, testData } ) => {
 		await page.goto(
 			`wp-admin/edit.php?post_type=product&page=product-reviews`
 		);
 		await expect(
-			page.getByRole( 'cell', { name: updatedReview } )
+			page.getByRole( 'cell', { name: testData.review.review } )
 		).toBeVisible();
+		const reviewerName = testData.review.reviewer;
 
 		//Hover over the product review to have 'Edit' displayed
 		await page.hover( '.comment-text' );
 
 		// Select Trash action, check confirmation prompt and undo
 		await page.getByRole( 'button', { name: 'Trash' } ).click();
-		await page.waitForTimeout( 2000 );
 		await expect(
-			page.getByText( `Comment by ${ reviewerName } moved to the Trash` )
+			page.getByText(
+				`Comment by ${ reviewerName } moved to the Trash`
+			)
 		).toBeVisible();
 		await page.getByRole( 'button', { name: 'Undo' } ).click();
 
 		// Verify that the review has been restored
 		await expect(
-			page.getByRole( 'cell', { name: updatedReview } )
+			page.getByRole( 'cell', { name: testData.review.review } )
 		).toBeVisible();
 
 		// Select Trash action and delete it permanently
 		await page.getByRole( 'button', { name: 'Trash' } ).click();
-		await page.waitForTimeout( 2000 );
 		await expect(
-			page.getByText( `Comment by ${ reviewerName } moved to the Trash` )
+			page.getByText(
+				`Comment by ${ reviewerName } moved to the Trash`
+			)
 		).toBeVisible();
 		await page.reload();
 		await page.waitForSelector( 'tr.no-items' );
