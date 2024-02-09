b5f59d7ced55c0b9eba800d424f0c66cd26c5b8e
Tarun Vijwani <tarun.vijwani@automattic.com>
2024-02-09 19:40:27 +0400

    Revert "Update cart actions to use new checkout filter registry"
    This reverts commit 4f41a6b5bac0d3ae5cef92038a3a2931f8f5de93.

--- plugins/woocommerce-blocks/assets/js/data/cart/actions.ts
+++ plugins/woocommerce-blocks/assets/js/data/cart/actions.ts
@@ -12,6 +12,7 @@ import type {
 	CartShippingRate,
 } from '@woocommerce/types';
 import { BillingAddress, ShippingAddress } from '@woocommerce/settings';
+import { applyCheckoutFilter } from '@woocommerce/blocks-checkout';
 import {
 	triggerAddedToCartEvent,
 	triggerAddingToCartEvent,
@@ -26,7 +27,6 @@ import { apiFetchWithHeaders } from '../shared-controls';
 import { ReturnOrGeneratorYieldUnion } from '../mapped-types';
 import { CartDispatchFromMap, CartSelectFromMap } from './index';
 import type { Thunks } from './thunks';
-import { applyCheckoutFilter } from '../../../../packages/checkout/filter-registry';
 
 // Thunks are functions that can be dispatched, similar to actions creators
 // @todo Many of the functions that return promises in this file need to be moved to thunks.ts.
@@ -38,24 +38,9 @@ export * from './thunks';
  * @param cart the parsed cart object. (Parsed into camelCase).
  */
 export const setCartData = ( cart: Cart ): { type: string; response: Cart } => {
-	const shippingAddress = applyCheckoutFilter( {
-		filterName: 'setFixedShippingAddress',
-		defaultValue: cart.shippingAddress,
-	} );
-	console.log( 'setCartData', shippingAddress );
-	const billingAddress = applyCheckoutFilter( {
-		filterName: 'setFixedBillingAddress',
-		defaultValue: cart.billingAddress,
-	} );
-	console.log( 'setCartData', billingAddress );
-	const cartWithUpdatedAddresses = {
-		...cart,
-		shippingAddress,
-		billingAddress,
-	};
 	return {
 		type: types.SET_CART_DATA,
-		response: cartWithUpdatedAddresses,
+		response: cart,
 	};
 };
 
@@ -458,7 +443,6 @@ export const setBillingAddress = (
 		filterName: 'setFixedBillingAddress',
 		defaultValue: billingAddress,
 	} );
-	console.log( 'setBillingAddress', address );
 	return { type: types.SET_BILLING_ADDRESS, address };
 };
 
@@ -472,7 +456,6 @@ export const setShippingAddress = (
 		filterName: 'setFixedShippingAddress',
 		defaultValue: shippingAddress,
 	} );
-	console.log( 'setShippingAddress', address );
 	return { type: types.SET_SHIPPING_ADDRESS, address };
 };
 
