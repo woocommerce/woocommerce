ae9800ba3ff9cca4b14b93c3e9a495bc163133b8
barryhughes <3594411+barryhughes@users.noreply.github.com>
2024-02-09 09:09:35 -0800

    Restore use of _table property, for consistency with other methods.

--- plugins/woocommerce/includes/class-wc-session-handler.php
+++ plugins/woocommerce/includes/class-wc-session-handler.php
@@ -358,7 +358,7 @@ class WC_Session_Handler extends WC_Session {
 
 			$wpdb->query(
 				$wpdb->prepare(
-					"INSERT INTO {$wpdb->prefix}woocommerce_sessions (`session_key`, `session_value`, `session_expiry`) VALUES (%s, %s, %d)
+					"INSERT INTO $this->_table (`session_key`, `session_value`, `session_expiry`) VALUES (%s, %s, %d)
  					ON DUPLICATE KEY UPDATE `session_value` = VALUES(`session_value`), `session_expiry` = VALUES(`session_expiry`)",
 					$this->_customer_id,
 					maybe_serialize( $this->_data ),
