03c00c7938cccff0044141ff268477caa25c7cc1
Adrian Moldovan <adim.moldovan@gmail.com>
2024-02-09 19:17:56 +0200

    Add tests `can replace an image`

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/products/block-editor/product-images-block-editor.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/products/block-editor/product-images-block-editor.spec.js
@@ -3,11 +3,24 @@ const {
 	toggleBlockProductEditor,
 } = require( '../../../../utils/simple-products' );
 
-async function addImagesFromLibrary( page, imagesNames ) {
+async function selectImagesInLibrary( page, imagesNames ) {
 	const dataIds = [];
 
 	await page.getByRole( 'tab', { name: 'Media Library' } ).click();
 
+	// Deselect all images
+	// await page.waitForTimeout( 2000 );
+	// for ( const checkbox of await page
+	// 	.getByRole( 'checkbox', { checked: true } )
+	// 	.all() ) {
+	// 	await checkbox.getByRole( 'button' ).click();
+	//
+	// 	expect(
+	// 		await page.getByRole( 'checkbox', { checked: true } ).count()
+	// 	).toBe( 0 );
+	// }
+
+	// Select the given images
 	for ( const imageName of imagesNames ) {
 		await page
 			.getByRole( 'searchbox', { name: 'Search' } )
@@ -52,10 +65,33 @@ baseTest.describe( 'Products > Edit Product', () => {
 			// Cleanup
 			await api.delete( `products/${ product.id }`, { force: true } );
 		},
+		productWithGallery: async ( { api, product }, use ) => {
+			let productWithGallery;
+			await api
+				.put( `products/${ product.id }`, {
+					images: [
+						{
+							src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg',
+						},
+						{
+							src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg',
+						},
+						{
+							src: 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_3_front.jpg',
+						},
+					],
+				} )
+				.then( ( response ) => {
+					productWithGallery = response.data;
+				} );
+
+			await use( productWithGallery );
+		},
 	} );
 
 	test( 'can add images', async ( { page, product } ) => {
 		const images = [ 'image-01', 'image-02' ];
+
 		await test.step( 'navigate to product edit page', async () => {
 			await page.goto(
 				`wp-admin/post.php?post=${ product.id }&action=edit`
@@ -64,7 +100,11 @@ baseTest.describe( 'Products > Edit Product', () => {
 
 		await test.step( 'add images', async () => {
 			await page.getByText( 'Choose an image' ).click();
-			const dataIds = await addImagesFromLibrary( page, images );
+			const dataIds = await selectImagesInLibrary( page, images );
+
+			await expect(
+				page.getByLabel( 'Block: Product images' ).locator( 'img' )
+			).toHaveCount( images.length );
 
 			for ( const dataId of dataIds ) {
 				await expect(
@@ -73,19 +113,107 @@ baseTest.describe( 'Products > Edit Product', () => {
 						.locator( `img[id="${ dataId }"]` )
 				).toBeVisible();
 			}
-
-			await page.getByRole( 'button', { name: 'Update' } ).click();
 		} );
 
-		await test.step( 'Verify product image was set', async () => {
+		await test.step( 'update the product', async () => {
+			await page.getByRole( 'button', { name: 'Update' } ).click();
 			// Verify product was updated
 			await expect(
 				page.getByLabel( 'Dismiss this notice' )
 			).toContainText( 'Product updated' );
+		} );
 
+		await test.step( 'verify product image was set', async () => {
 			// Verify image in store frontend
 			await page.goto( product.permalink );
-			await expect( page.getByTitle( `image-01` ) ).toBeVisible();
+
+			for ( const image of images ) {
+				await expect( page.getByTitle( image ) ).toBeVisible();
+			}
 		} );
 	} );
+
+	test( 'can replace an image', async ( { page, productWithGallery } ) => {
+		const newImageName = 'image-01';
+		const replacedImgLocator = page
+			.getByLabel( 'Block: Product images' )
+			.locator( 'img' )
+			.nth( 1 );
+		let dataIds = [];
+
+		await test.step( 'navigate to product edit page', async () => {
+			await page.goto(
+				`wp-admin/post.php?post=${ productWithGallery.id }&action=edit`
+			);
+		} );
+
+		await test.step( 'replace an image', async () => {
+			await replacedImgLocator.click();
+			await page
+				.getByRole( 'toolbar', { name: 'Options' } )
+				.getByLabel( 'Options' )
+				.click();
+			await page.getByRole( 'menuitem', { name: 'Replace' } ).click();
+			dataIds = await selectImagesInLibrary( page, [ newImageName ] );
+
+			expect( await replacedImgLocator.getAttribute( 'src' ) ).toContain(
+				newImageName
+			);
+		} );
+
+		await test.step( 'update the product', async () => {
+			await page.getByRole( 'button', { name: 'Update' } ).click();
+			// Verify product was updated
+			await expect(
+				page.getByLabel( 'Dismiss this notice' )
+			).toContainText( 'Product updated' );
+		} );
+
+		await test.step( 'verify product image was set', async () => {
+			await expect( replacedImgLocator ).toHaveId( dataIds[ 0 ] );
+
+			// Verify image in store frontend
+			await page.goto( productWithGallery.permalink );
+			await expect( page.getByTitle( newImageName ) ).toBeVisible();
+		} );
+	} );
+
+	// test( 'can update the gallery', async ( {
+	// 	page,
+	// 	productWithGallery,
+	// } ) => {
+	// 	const images = [ 'image-01', 'image-02' ];
+	//
+	// 	await test.step( 'navigate to product edit page', async () => {
+	// 		await page.goto(
+	// 			`wp-admin/post.php?post=${ productWithGallery.id }&action=edit`
+	// 		);
+	// 	} );
+	//
+	// 	await test.step( 'update the images', async () => {
+	// 		await page.getByText( 'Choose an image' ).click();
+	// 		const dataIds = await selectImagesInLibrary( page, images );
+	//
+	// 		for ( const dataId of dataIds ) {
+	// 			await expect(
+	// 				page
+	// 					.getByLabel( 'Block: Product images' )
+	// 					.locator( `img[id="${ dataId }"]` )
+	// 			).toBeVisible();
+	// 		}
+	//
+	// 		await page.getByRole( 'button', { name: 'Update' } ).click();
+	// 	} );
+	//
+	// 	await test.step( 'Verify product image was set', async () => {
+	// 		// Verify product was updated
+	// 		await expect(
+	// 			page.getByLabel( 'Dismiss this notice' )
+	// 		).toContainText( 'Product updated' );
+	//
+	// 		// Verify image in store frontend
+	// 		await page.goto( productWithGallery.permalink );
+	// 		await expect( page.getByTitle( `image-01` ) ).toBeVisible();
+	// 	} );
+	// } );
 } );
