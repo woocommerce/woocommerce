40bb3590fc5906cd88df926cfdd2172249ba50a3
Sam Seay <samueljseay@gmail.com>
2024-02-09 14:26:13 +0800

    Remove experimental feature flag from new front-end filter blocks in prep for a beta release.

--- plugins/woocommerce-blocks/assets/js/blocks/product-filter/inner-blocks/active-filters/index.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filter/inner-blocks/active-filters/index.tsx
@@ -4,7 +4,6 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { Icon } from '@wordpress/icons';
 import { toggle } from '@woocommerce/icons';
-import { isExperimentalBuild } from '@woocommerce/block-settings';
 
 /**
  * Internal dependencies
@@ -13,16 +12,14 @@ import metadata from './block.json';
 import Edit from './edit';
 import './style.scss';
 
-if ( isExperimentalBuild() ) {
-	registerBlockType( metadata, {
-		icon: {
-			src: (
-				<Icon
-					icon={ toggle }
-					className="wc-block-editor-components-block-icon"
-				/>
-			),
-		},
-		edit: Edit,
-	} );
-}
+registerBlockType( metadata, {
+	icon: {
+		src: (
+			<Icon
+				icon={ toggle }
+				className="wc-block-editor-components-block-icon"
+			/>
+		),
+	},
+	edit: Edit,
+} );
