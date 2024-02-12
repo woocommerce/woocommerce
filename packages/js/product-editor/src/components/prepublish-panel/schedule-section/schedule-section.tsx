8d14f9b3d0622c4ed75985f568b7a8441cf7ca8b
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-02-12 12:29:19 -0300

    Add publish date time picket to the schedule section

--- packages/js/product-editor/src/components/prepublish-panel/schedule-section/schedule-section.tsx
+++ packages/js/product-editor/src/components/prepublish-panel/schedule-section/schedule-section.tsx
@@ -1,19 +1,48 @@
 /**
  * External dependencies
  */
+import {
+	// @ts-expect-error no exported member
+	__experimentalPublishDateTimePicker as PublishDateTimePicker,
+} from '@wordpress/block-editor';
 import { PanelBody } from '@wordpress/components';
-import { createElement, Fragment } from '@wordpress/element';
+import { __experimentalGetSettings as getSettings } from '@wordpress/date';
+import { createElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
+import useProductEntityProp from '../../../hooks/use-product-entity-prop';
 import { ScheduleSectionProps } from './types';
 
+function is12HourTime() {
+	const settings = getSettings() as { formats: { time: string } };
+
+	return /a(?!\\)/i.test(
+		settings.formats.time
+			.toLowerCase()
+			.replace( /\\\\/g, '' )
+			.split( '' )
+			.reverse()
+			.join( '' )
+	);
+}
+
 export function ScheduleSection( {}: ScheduleSectionProps ) {
+	const [ date, setDate ] = useProductEntityProp( 'date_created' );
+
+	function handlePublishDateTimePickerChange( value: string ) {
+		setDate( value );
+	}
+
 	return (
 		<PanelBody initialOpen={ false } title={ __( 'Add', 'woocommerce' ) }>
-			<></>
+			<PublishDateTimePicker
+				currentDate={ date }
+				onChange={ handlePublishDateTimePickerChange }
+				is12Hour={ is12HourTime() }
+			/>
 		</PanelBody>
 	);
 }
