127290cd9938225a659571b208615d2b339342de
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-02-13 14:10:51 +0100

    Fix lint problems

--- plugins/woocommerce-blocks/tests/e2e/tests/product-collection/product-collection.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/product-collection/product-collection.block_theme.spec.ts
@@ -327,128 +327,111 @@ test.describe( 'Product Collection', () => {
 			await expect( pageObject.products ).toHaveCount( 4 );
 		} );
 
-		test.describe(
-			'Sync with current template aka Inherit query from template',
-			() => {
-				test( 'should not be visible on posts', async ( {
-					pageObject,
-				} ) => {
-					await pageObject.createNewPostAndInsertBlock();
+		test.describe( 'Sync with current template aka Inherit query from template', () => {
+			test( 'should not be visible on posts', async ( {
+				pageObject,
+			} ) => {
+				await pageObject.createNewPostAndInsertBlock();
 
-					const sidebarSettings =
-						await pageObject.locateSidebarSettings();
-					await expect(
-						sidebarSettings.locator(
-							SELECTORS.inheritQueryFromTemplateControl
-						)
-					).toBeHidden();
+				const sidebarSettings =
+					await pageObject.locateSidebarSettings();
+				await expect(
+					sidebarSettings.locator(
+						SELECTORS.inheritQueryFromTemplateControl
+					)
+				).toBeHidden();
+			} );
+
+			test( 'should work as expected in Product Catalog template', async ( {
+				pageObject,
+			} ) => {
+				await pageObject.goToProductCatalogAndInsertCollection();
+
+				const sidebarSettings =
+					await pageObject.locateSidebarSettings();
+
+				// Inherit query from template should be visible & enabled by default
+				await expect(
+					sidebarSettings.locator(
+						SELECTORS.inheritQueryFromTemplateControl
+					)
+				).toBeVisible();
+				await expect(
+					sidebarSettings.locator(
+						`${ SELECTORS.inheritQueryFromTemplateControl } input`
+					)
+				).toBeChecked();
+
+				// "On sale control" should be hidden when inherit query from template is enabled
+				await expect(
+					sidebarSettings.getByLabel( SELECTORS.onSaleControlLabel )
+				).toBeHidden();
+
+				// "On sale control" should be visible when inherit query from template is disabled
+				await pageObject.setInheritQueryFromTemplate( false );
+				await expect(
+					sidebarSettings.getByLabel( SELECTORS.onSaleControlLabel )
+				).toBeVisible();
+
+				// "On sale control" should retain its state when inherit query from template is enabled again
+				pageObject.setShowOnlyProductsOnSale( {
+					onSale: true,
+					isLocatorsRefreshNeeded: false,
 				} );
+				await expect(
+					sidebarSettings.getByLabel( SELECTORS.onSaleControlLabel )
+				).toBeChecked();
+				await pageObject.setInheritQueryFromTemplate( true );
+				await expect(
+					sidebarSettings.getByLabel( SELECTORS.onSaleControlLabel )
+				).toBeHidden();
+				await pageObject.setInheritQueryFromTemplate( false );
+				await expect(
+					sidebarSettings.getByLabel( SELECTORS.onSaleControlLabel )
+				).toBeVisible();
+				await expect(
+					sidebarSettings.getByLabel( SELECTORS.onSaleControlLabel )
+				).toBeChecked();
+			} );
 
-				test( 'should work as expected in Product Catalog template', async ( {
-					pageObject,
-				} ) => {
-					await pageObject.goToProductCatalogAndInsertCollection();
+			test( 'is enlabled by default in 1st Product Collection and disabled in 2nd+', async ( {
+				pageObject,
+			} ) => {
+				// First Product Catalog
+				// Option should be visible & ENABLED by default
+				await pageObject.goToProductCatalogAndInsertCollection();
 
-					const sidebarSettings =
-						await pageObject.locateSidebarSettings();
+				const sidebarSettings =
+					await pageObject.locateSidebarSettings();
 
-					// Inherit query from template should be visible & enabled by default
-					await expect(
-						sidebarSettings.locator(
-							SELECTORS.inheritQueryFromTemplateControl
-						)
-					).toBeVisible();
-					await expect(
-						sidebarSettings.locator(
-							`${ SELECTORS.inheritQueryFromTemplateControl } input`
-						)
-					).toBeChecked();
+				await expect(
+					sidebarSettings.locator(
+						SELECTORS.inheritQueryFromTemplateControl
+					)
+				).toBeVisible();
+				await expect(
+					sidebarSettings.locator(
+						`${ SELECTORS.inheritQueryFromTemplateControl } input`
+					)
+				).toBeChecked();
 
-					// "On sale control" should be hidden when inherit query from template is enabled
-					await expect(
-						sidebarSettings.getByLabel(
-							SELECTORS.onSaleControlLabel
-						)
-					).toBeHidden();
+				// Second Product Catalog
+				// Option should be visible & DISABLED by default
+				await pageObject.insertProductCollection();
+				await pageObject.chooseCollectionInTemplate( 'productCatalog' );
 
-					// "On sale control" should be visible when inherit query from template is disabled
-					await pageObject.setInheritQueryFromTemplate( false );
-					await expect(
-						sidebarSettings.getByLabel(
-							SELECTORS.onSaleControlLabel
-						)
-					).toBeVisible();
-
-					// "On sale control" should retain its state when inherit query from template is enabled again
-					pageObject.setShowOnlyProductsOnSale( {
-						onSale: true,
-						isLocatorsRefreshNeeded: false,
-					} );
-					await expect(
-						sidebarSettings.getByLabel(
-							SELECTORS.onSaleControlLabel
-						)
-					).toBeChecked();
-					await pageObject.setInheritQueryFromTemplate( true );
-					await expect(
-						sidebarSettings.getByLabel(
-							SELECTORS.onSaleControlLabel
-						)
-					).toBeHidden();
-					await pageObject.setInheritQueryFromTemplate( false );
-					await expect(
-						sidebarSettings.getByLabel(
-							SELECTORS.onSaleControlLabel
-						)
-					).toBeVisible();
-					await expect(
-						sidebarSettings.getByLabel(
-							SELECTORS.onSaleControlLabel
-						)
-					).toBeChecked();
-				} );
-
-				test( 'is enlabled by default in 1st Product Collection and disabled in 2nd+', async ( {
-					pageObject,
-				} ) => {
-					// First Product Catalog
-					// Option should be visible & ENABLED by default
-					await pageObject.goToProductCatalogAndInsertCollection();
-
-					const sidebarSettings =
-						await pageObject.locateSidebarSettings();
-
-					await expect(
-						sidebarSettings.locator(
-							SELECTORS.inheritQueryFromTemplateControl
-						)
-					).toBeVisible();
-					await expect(
-						sidebarSettings.locator(
-							`${ SELECTORS.inheritQueryFromTemplateControl } input`
-						)
-					).toBeChecked();
-
-					// Second Product Catalog
-					// Option should be visible & DISABLED by default
-					await pageObject.insertProductCollection();
-					await pageObject.chooseCollectionInTemplate(
-						'productCatalog'
-					);
-
-					await expect(
-						sidebarSettings.locator(
-							SELECTORS.inheritQueryFromTemplateControl
-						)
-					).toBeVisible();
-					await expect(
-						sidebarSettings.locator(
-							`${ SELECTORS.inheritQueryFromTemplateControl } input`
-						)
-					).not.toBeChecked();
-				} );
-			}
-		);
+				await expect(
+					sidebarSettings.locator(
+						SELECTORS.inheritQueryFromTemplateControl
+					)
+				).toBeVisible();
+				await expect(
+					sidebarSettings.locator(
+						`${ SELECTORS.inheritQueryFromTemplateControl } input`
+					)
+				).not.toBeChecked();
+			} );
+		} );
 	} );
 
 	test.describe( 'Toolbar settings', () => {
