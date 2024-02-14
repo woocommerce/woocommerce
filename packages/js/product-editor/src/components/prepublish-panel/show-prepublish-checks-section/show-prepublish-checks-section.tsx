fffb1bd63fc2098cb843ca2a1dd7161bcf55116e
Fernando Marichal <fernando.marichal@automattic.com>
2024-02-14 16:04:46 -0300

    Refactor prepublish panel button visiblity
    # Conflicts:
    #	packages/js/product-editor/src/components/header/header.tsx
    
    # Conflicts:
    #	packages/js/product-editor/src/components/header/header.tsx

--- packages/js/product-editor/src/components/prepublish-panel/show-prepublish-checks-section/show-prepublish-checks-section.tsx
+++ packages/js/product-editor/src/components/prepublish-panel/show-prepublish-checks-section/show-prepublish-checks-section.tsx
@@ -5,35 +5,28 @@ import { createElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { recordEvent } from '@woocommerce/tracks';
 import { CheckboxControl } from '@wordpress/components';
-import { useDispatch, useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
 import { TRACKS_SOURCE } from '../../../constants';
+import { useShowPrepublishChecks } from '../../../hooks/use-show-prepublish-checks';
 
 export function ShowPrepublishChecksSection() {
-	const { disablePublishSidebar, enablePublishSidebar } =
-		useDispatch( 'core/editor' );
+	const { isResolving, showPrepublishChecks, togglePrepublishChecks } =
+		useShowPrepublishChecks();
 
-	const { isPrepublishSidebarEnabled } = useSelect( ( select ) => {
-		const { isPublishSidebarEnabled } = select( 'core/editor' );
-		return {
-			isPrepublishSidebarEnabled: isPublishSidebarEnabled() ?? true,
-		};
-	}, [] );
+	if ( isResolving ) {
+		return null;
+	}
 
 	return (
 		<div className="woocommerce-publish-panel-show-checks">
 			<CheckboxControl
 				label={ __( 'Always show pre-publish checks.', 'woocommerce' ) }
-				checked={ isPrepublishSidebarEnabled }
+				checked={ showPrepublishChecks }
 				onChange={ ( selected: boolean ) => {
-					if ( selected ) {
-						enablePublishSidebar();
-					} else {
-						disablePublishSidebar();
-					}
+					togglePrepublishChecks();
 					recordEvent( 'product_prepublish_panel', {
 						source: TRACKS_SOURCE,
 						action: 'enable_prepublish_checks',
