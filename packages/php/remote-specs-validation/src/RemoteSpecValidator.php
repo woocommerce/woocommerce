247d070617e30c60ebb02b8c0dd5c1a2a923aa95
moon <moon.kyong@automattic.com>
2024-02-13 18:34:44 -0800

    Add RemoteSpecValidator

---
+++ packages/php/remote-specs-validation/src/RemoteSpecValidator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Automattic\WooCommerce\Tests\RemoteSpecsValidation;
+
+use Opis\JsonSchema\Validator;
+
+/***
+ * A thin wrapper of Opis JSON Schema Validator.
+ *
+ * Validates a remote spec against a JSON schema.
+ */
+class RemoteSpecValidator {
+
+	/**
+	 * Decoded JSON schema.
+	 * @var string
+	 */
+	private $schema;
+
+	public function __construct( $json_schema_path ) {
+		$this->schema = json_decode( file_get_contents( $json_schema_path ) );
+	}
+
+	/**
+	 * Create a RemoteSpecValidator from a bundle file.
+	 *
+	 * @param string $bundle The name of the bundle.
+	 * @return RemoteSpecValidator
+	 * @throws \InvalidArgumentException If the bundle is not supported.
+	 */
+	public static function create_from_bundle( $bundle ) {
+		$supported_bundles = [
+			'remote-inbox-notification' => 'remote-inbox-notification.json',
+			'wc-pay-promotions'   => 'wc-pay-promotions.json',
+			'shipping-partner-suggestions' => 'shipping-partner-suggestions.json',
+			'payment-gateway-suggestions' => 'payment-gateway-suggestions.json',
+			'obw-free-extensions' => 'obw-free-extensions.json',
+		];
+
+		if ( ! array_key_exists( $bundle, $supported_bundles ) ) {
+			throw new \InvalidArgumentException( "Unsupported bundle: $bundle. ".
+				"Supported bundles are: " . implode( ', ', array_keys( $supported_bundles ) ) );
+		}
+
+		return new self( __DIR__ . "/../bundles/{$supported_bundles[$bundle]}" );
+	}
+
+	/**
+	 * Validate a remote spec against the schema.
+	 *
+	 * @param string $spec The remote spec to validate.
+	 * @return RemoteSpecValidationResult The validation result.
+	 */
+	public function validate( $spec ) {
+		$validator = new Validator();
+		return new RemoteSpecValidationResult( $validator->validate( $spec, $this->schema ) );
+	}
+}
