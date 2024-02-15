fbbf5274bc976da2e28a35fe98d978a5a3b54394
Jon Lane <jon.lane@automattic.com>
2024-02-15 11:11:32 -0800

    Fix linting issues (code formatting)

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/account-email-receiving.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/account-email-receiving.spec.js
@@ -10,7 +10,9 @@ test.describe( 'Shopper Account Email Receiving', () => {
 
 	test.beforeEach( async ( { page } ) => {
 		await page.goto(
-			`wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent( email ) }`
+			`wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent(
+				email
+			) }`
 		);
 		// clear out the email logs before each test
 		while (
@@ -36,14 +38,16 @@ test.describe( 'Shopper Account Email Receiving', () => {
 		await page.getByRole( 'button', { name: 'Confirm Deletion' } ).click();
 	} );
 
-	test( 'should receive an email when creating an account', async ( { page } ) => {
+	test( 'should receive an email when creating an account', async ( {
+		page,
+	} ) => {
 		// create a new customer
 		await page.goto( 'wp-admin/user-new.php' );
 
-		await page.getByLabel(' Username (required) ').fill( username );
-		await page.getByLabel(' Email (required) ').fill( email );
-		await page.getByLabel(' First Name ').fill( 'New' );
-		await page.getByLabel(' Last Name ').fill( 'Customer' );
+		await page.getByLabel( ' Username (required) ' ).fill( username );
+		await page.getByLabel( ' Email (required) ' ).fill( email );
+		await page.getByLabel( ' First Name ' ).fill( 'New' );
+		await page.getByLabel( ' Last Name ' ).fill( 'Customer' );
 
 		await page.getByLabel( 'Send the new user an email' ).check();
 
@@ -54,24 +58,38 @@ test.describe( 'Shopper Account Email Receiving', () => {
 		await page.waitForSelector( '.notice' );
 
 		// verify that the email was sent
-		await page.goto( `wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent( email ) }` );
+		await page.goto(
+			`wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent(
+				email
+			) }`
+		);
 
-		await expect( page.locator( 'td.column-receiver' ).first() ).toHaveText( email );
-		await expect( page.getByRole( 'cell', { name: '[WooCommerce Core E2E Test Suite] Login Details' } ) ).toBeVisible();
+		await expect( page.locator( 'td.column-receiver' ).first() ).toHaveText(
+			email
+		);
+		await expect(
+			page.getByRole( 'cell', {
+				name: '[WooCommerce Core E2E Test Suite] Login Details',
+			} )
+		).toBeVisible();
 
 		await page.getByRole( 'button', { name: 'View log' } ).first().click();
 		await page.locator( emailContentJson ).click();
-		await expect( page.locator( emailContent) ).toContainText( `Username: ${ username }` );
+		await expect( page.locator( emailContent ) ).toContainText(
+			`Username: ${ username }`
+		);
 	} );
 
-	test( 'should receive an email when password reset initiated from admin', async ( { page } ) => {
+	test( 'should receive an email when password reset initiated from admin', async ( {
+		page,
+	} ) => {
 		// create a new customer
 		await page.goto( 'wp-admin/user-new.php' );
 
-		await page.getByLabel(' Username (required) ').fill( username );
-		await page.getByLabel(' Email (required) ').fill( email );
-		await page.getByLabel(' First Name ').fill( 'New' );
-		await page.getByLabel(' Last Name ').fill( 'Customer' );
+		await page.getByLabel( ' Username (required) ' ).fill( username );
+		await page.getByLabel( ' Email (required) ' ).fill( email );
+		await page.getByLabel( ' First Name ' ).fill( 'New' );
+		await page.getByLabel( ' Last Name ' ).fill( 'Customer' );
 
 		await page.getByLabel( 'Send the new user an email' ).uncheck();
 
@@ -82,34 +100,56 @@ test.describe( 'Shopper Account Email Receiving', () => {
 		await page.waitForSelector( '.notice' );
 
 		// verify that no email was sent on account creation
-		await page.goto( `wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent( email ) }` );
-		await expect( page.locator( 'td.column-receiver' ).first() ).not.toHaveText( email );
+		await page.goto(
+			`wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent(
+				email
+			) }`
+		);
+		await expect(
+			page.locator( 'td.column-receiver' ).first()
+		).not.toHaveText( email );
 
 		// initiate password reset from admin
 		await page.goto( 'wp-admin/users.php' );
 
 		await page.getByText( username ).last().hover();
-		await page.getByRole( 'link', { name: 'Send password reset' } ).last().click();
+		await page
+			.getByRole( 'link', { name: 'Send password reset' } )
+			.last()
+			.click();
 
 		// verify that the email was sent
-		await page.goto( `wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent( email ) }` );
+		await page.goto(
+			`wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent(
+				email
+			) }`
+		);
 
-		await expect( page.locator( 'td.column-receiver' ).first() ).toHaveText( email );
-		await expect( page.getByRole( 'cell', { name: '[WooCommerce Core E2E Test Suite] Password Reset' } ) ).toBeVisible();
+		await expect( page.locator( 'td.column-receiver' ).first() ).toHaveText(
+			email
+		);
+		await expect(
+			page.getByRole( 'cell', {
+				name: '[WooCommerce Core E2E Test Suite] Password Reset',
+			} )
+		).toBeVisible();
 
 		await page.getByRole( 'button', { name: 'View log' } ).first().click();
 		await page.locator( emailContentJson ).click();
-		await expect( page.locator( emailContent) ).toContainText( `Username: ${ username }` );
+		await expect( page.locator( emailContent ) ).toContainText(
+			`Username: ${ username }`
+		);
 	} );
 } );
 
 test.describe( 'Shopper Password Reset Email Receiving', () => {
-
 	test.use( { storageState: process.env.ADMINSTATE } );
 
 	test.beforeEach( async ( { page } ) => {
 		await page.goto(
-			`wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent( 'customer@woocommercecoree2etestsuite.com' ) }`
+			`wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent(
+				'customer@woocommercecoree2etestsuite.com'
+			) }`
 		);
 		// clear out the email logs before each test
 		while (
@@ -129,10 +169,14 @@ test.describe( 'Shopper Password Reset Email Receiving', () => {
 
 	test.use( { cookies: [], origins: [] } );
 
-	test ( 'should receive an email when initiating a password reset', async ( { page } ) => {
+	test( 'should receive an email when initiating a password reset', async ( {
+		page,
+	} ) => {
 		await page.goto( 'my-account/lost-password/' );
 
-		await page.getByLabel( 'Username or email' ).fill( 'customer@woocommercecoree2etestsuite.com' );
+		await page
+			.getByLabel( 'Username or email' )
+			.fill( 'customer@woocommercecoree2etestsuite.com' );
 		await page.getByRole( 'button', { name: 'Reset password' } ).click();
 
 		await page.waitForSelector( '.woocommerce-message' );
@@ -142,13 +186,25 @@ test.describe( 'Shopper Password Reset Email Receiving', () => {
 		await page.getByLabel( 'Username or Email Address' ).fill( 'admin' );
 		await page.getByLabel( 'Password', { exact: true } ).fill( 'password' );
 		await page.getByRole( 'button', { name: 'Log In' } ).click();
-		await page.goto( `wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent( 'customer@woocommercecoree2etestsuite.com' ) }` );
+		await page.goto(
+			`wp-admin/tools.php?page=wpml_plugin_log&s=${ encodeURIComponent(
+				'customer@woocommercecoree2etestsuite.com'
+			) }`
+		);
 
-		await expect( page.locator( 'td.column-receiver' ).first() ).toHaveText( 'customer@woocommercecoree2etestsuite.com' );
-		await expect( page.getByRole( 'cell', { name: 'Password Reset Request for' } ).first() ).toBeVisible();
+		await expect( page.locator( 'td.column-receiver' ).first() ).toHaveText(
+			'customer@woocommercecoree2etestsuite.com'
+		);
+		await expect(
+			page
+				.getByRole( 'cell', { name: 'Password Reset Request for' } )
+				.first()
+		).toBeVisible();
 
 		await page.getByRole( 'button', { name: 'View log' } ).first().click();
 		await page.locator( emailContentJson ).click();
-		await expect( page.locator( emailContent) ).toContainText( 'Username: customer' );
+		await expect( page.locator( emailContent ) ).toContainText(
+			'Username: customer'
+		);
 	} );
 } );
