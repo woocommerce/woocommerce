49f5865158f143313b166fb0e90248b866b5feba
nigeljamesstevenson <nigeljamesstevenson@gmail.com>
2024-02-16 18:10:38 +0000

    Prep release/8.6 for 8.6 cycle with version bump to 8.6.1

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woo.com/
  * Description: An ecommerce toolkit that helps you sell anything. Beautifully.
- * Version: 8.6.0
+ * Version: 8.6.1
  * Author: Automattic
  * Author URI: https://woo.com
  * Text Domain: woocommerce
