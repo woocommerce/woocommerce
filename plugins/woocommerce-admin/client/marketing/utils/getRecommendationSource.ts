870378a377ccacd9bab4a1432d2d66c63a79cb1a
Gan Eng Chin <ecgan@users.noreply.github.com>
2024-02-17 00:23:33 +0800

    Add JSDoc for getRecommendationSource.

--- plugins/woocommerce-admin/client/marketing/utils/getRecommendationSource.ts
+++ plugins/woocommerce-admin/client/marketing/utils/getRecommendationSource.ts
@@ -3,6 +3,11 @@
  */
 import { getAdminSetting } from '~/utils/admin-settings';
 
+/**
+ * Get the source of the marketing recommendations.
+ *
+ * When the marketplace suggestions feature is turned on, the source is 'woocommerce.com'. Otherwise, it is 'plugin-woocommerce'.
+ */
 export const getRecommendationSource = () => {
 	if ( getAdminSetting( 'allowMarketplaceSuggestions', false ) ) {
 		return 'woocommerce.com';
