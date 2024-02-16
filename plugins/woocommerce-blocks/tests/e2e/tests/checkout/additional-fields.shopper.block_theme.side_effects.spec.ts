65e6e47ee3d5c695a02a44a5753f175e69256242
Thomas Roberts <thomas.roberts@automattic.com>
2024-02-16 17:20:00 +0000

    Make check for guest shopper same as logged in

--- plugins/woocommerce-blocks/tests/e2e/tests/checkout/additional-fields.shopper.block_theme.side_effects.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/checkout/additional-fields.shopper.block_theme.side_effects.spec.ts
@@ -1070,10 +1070,9 @@ test.describe( 'Shopper → Additional Checkout Fields', () => {
 
 		test( 'Shopper can fill in the checkout form with additional fields and can have different value for same field in shipping and billing address', async ( {
 			checkoutPageObject,
+			frontendUtils,
 		} ) => {
-			await checkoutPageObject.editShippingDetails();
 			await checkoutPageObject.unsyncBillingWithShipping();
-			await checkoutPageObject.editBillingDetails();
 			await checkoutPageObject.fillInCheckoutWithTestData(
 				{},
 				{
@@ -1147,8 +1146,12 @@ test.describe( 'Shopper → Additional Checkout Fields', () => {
 					[ 'Government ID', '12345' ],
 					[ 'Government ID', '54321' ],
 					[ 'What is your favourite colour?', 'Blue' ],
-					[ 'Would you like a free gift with your order?', 'Yes' ],
+					[
+						'Enter a gift message to include in the package',
+						'This is for you!',
+					],
 					[ 'Do you want to subscribe to our newsletter?', 'Yes' ],
+					[ 'Would you like a free gift with your order?', 'Yes' ],
 					[ 'Can a truck fit down your road?', 'Yes' ],
 					[ 'Can a truck fit down your road?', 'No' ],
 					[ 'How wide is your road?', 'Wide' ],
@@ -1157,12 +1160,99 @@ test.describe( 'Shopper → Additional Checkout Fields', () => {
 						'Is this a personal purchase or a business purchase?',
 						'business',
 					],
-					[
-						'Enter a gift message to include in the package',
-						'This is for you!',
-					],
 				] )
 			).toBe( true );
+
+			await frontendUtils.emptyCart();
+			await frontendUtils.goToShop();
+			await frontendUtils.addToCart( REGULAR_PRICED_PRODUCT_NAME );
+			await frontendUtils.goToCheckout();
+
+			await checkoutPageObject.editShippingDetails();
+			await checkoutPageObject.editBillingDetails();
+
+			// Now check all the fields previously filled are still filled on a fresh checkout.
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Contact information',
+					} )
+					.getByLabel(
+						'Enter a gift message to include in the package'
+					)
+			).toHaveValue( 'This is for you!' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Contact information',
+					} )
+					.getByLabel(
+						'Is this a personal purchase or a business purchase?'
+					)
+			).toHaveValue( 'Business' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Contact information',
+					} )
+					.getByLabel( 'Do you want to subscribe to our newsletter?' )
+			).toBeChecked();
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Shipping address',
+					} )
+					.getByLabel( 'Government ID', { exact: true } )
+			).toHaveValue( '12345' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Shipping address',
+					} )
+					.getByLabel( 'Confirm Government ID' )
+			).toHaveValue( '12345' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Shipping address',
+					} )
+					.getByLabel( 'Can a truck fit down your road?' )
+			).toBeChecked();
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Shipping address',
+					} )
+					.getByLabel( 'How wide is your road?' )
+			).toHaveValue( 'Wide' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Billing address',
+					} )
+					.getByLabel( 'Government ID', { exact: true } )
+			).toHaveValue( '54321' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Billing address',
+					} )
+					.getByLabel( 'Confirm Government ID' )
+			).toHaveValue( '54321' );
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Billing address',
+					} )
+					.getByLabel( 'Can a truck fit down your road?' )
+			).not.toBeChecked();
+			await expect(
+				checkoutPageObject.page
+					.getByRole( 'group', {
+						name: 'Billing address',
+					} )
+					.getByLabel( 'How wide is your road?' )
+			).toHaveValue( 'Narrow' );
 		} );
 	} );
 } );
