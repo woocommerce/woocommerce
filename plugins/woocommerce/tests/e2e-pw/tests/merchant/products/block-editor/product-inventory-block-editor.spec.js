267aa1f3f6a61c713eae45cdb7e4ddd7b3b21be1
Adrian Moldovan <adim.moldovan@gmail.com>
2024-02-16 15:40:05 +0200

    Finish test 'can update sku'

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/products/block-editor/product-inventory-block-editor.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/products/block-editor/product-inventory-block-editor.spec.js
@@ -25,7 +25,8 @@ test( 'can update sku', async ( { page, product } ) => {
 	await page.goto( `wp-admin/post.php?post=${ product.id }&action=edit` );
 	await page.getByRole( 'button', { name: 'Inventory' } ).click();
 
-	await page.locator( '[name="woocommerce-product-sku"]' ).fill( 'new-sku' );
+	const sku = `SKU_${ Date.now() }`;
+	await page.locator( '[name="woocommerce-product-sku"]' ).fill( sku );
 
 	await test.step( 'update the product', async () => {
 		await page.getByRole( 'button', { name: 'Update' } ).click();
@@ -33,5 +34,18 @@ test( 'can update sku', async ( { page, product } ) => {
 		await expect( page.getByLabel( 'Dismiss this notice' ) ).toContainText(
 			'Product updated'
 		);
+	} );
+
+	await test.step( 'verify the change', async () => {
+		await expect(
+			page.locator( '[name="woocommerce-product-sku"]' )
+		).toHaveValue( sku );
+	} );
+
+	await test.step( 'verify the changes in the store frontend', async () => {
+		// Verify image in store frontend
+		await page.goto( product.permalink );
+
+		await expect( page.getByText( `SKU: ${ sku }` ) ).toBeVisible();
 	} );
 } );
