492dcc3b0f088a3ed5d38b77f3419042d45c17e6
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-02-19 10:19:26 +0100

    Active theme doesn't mean permalinks are enabled

--- plugins/woocommerce-blocks/tests/e2e/block-theme.setup.ts
+++ plugins/woocommerce-blocks/tests/e2e/block-theme.setup.ts
@@ -5,24 +5,18 @@ import { BLOCK_THEME_SLUG, cli } from '@woocommerce/e2e-utils';
 import { test as setup, expect } from '@woocommerce/e2e-playwright-utils';
 
 setup( 'Sets up the block theme', async () => {
-	const isActiveOutput = await cli(
-		`npm run wp-env run tests-cli -- wp theme is-active ${ BLOCK_THEME_SLUG }`
+	const { stdout } = await cli(
+		`npm run wp-env run tests-cli -- wp theme install ${ BLOCK_THEME_SLUG } --activate`
 	);
 
-	if ( isActiveOutput.stderr.includes( 'Command failed' ) ) {
-		const installOutput = await cli(
-			`npm run wp-env run tests-cli -- wp theme install ${ BLOCK_THEME_SLUG } --activate`
-		);
+	expect(
+		stdout,
+		`Could not install and/or activate ${ BLOCK_THEME_SLUG }`
+	).toContain( 'Success' );
 
-		expect(
-			installOutput.stdout,
-			`Could not install and/or activate ${ BLOCK_THEME_SLUG }`
-		).toContain( 'Success' );
-
-		// Enable permalinks.
-		await cli(
-			`npm run wp-env run tests-cli -- wp rewrite structure /%postname%/ --hard`
-		);
-		await cli( `npm run wp-env run tests-cli -- wp rewrite flush --hard` );
-	}
+	// Enable permalinks.
+	await cli(
+		`npm run wp-env run tests-cli -- wp rewrite structure /%postname%/ --hard`
+	);
+	await cli( `npm run wp-env run tests-cli -- wp rewrite flush --hard` );
 } );
