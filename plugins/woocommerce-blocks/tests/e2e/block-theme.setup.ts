180b71d796bea9c467a31e75eed4a79486278ccb
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-02-19 10:12:30 +0100

    Drop REST and take different route with CLI

--- plugins/woocommerce-blocks/tests/e2e/block-theme.setup.ts
+++ plugins/woocommerce-blocks/tests/e2e/block-theme.setup.ts
@@ -2,21 +2,23 @@
  * External dependencies
  */
 import { BLOCK_THEME_SLUG, cli } from '@woocommerce/e2e-utils';
-import { test as setup } from '@woocommerce/e2e-playwright-utils';
+import { test as setup, expect } from '@woocommerce/e2e-playwright-utils';
 
-type ThemeItem = {
-	status: string;
-	stylesheet: string;
-};
+setup( 'Sets up the block theme', async () => {
+	const isActiveOutput = await cli(
+		`npm run wp-env run tests-cli -- wp theme is-active ${ BLOCK_THEME_SLUG }`
+	);
 
-setup( 'Sets up the block theme', async ( { requestUtils } ) => {
-	const themes = await requestUtils.rest< ThemeItem[] >( {
-		path: '/wp/v2/themes',
-	} );
-	const currentTheme = themes.find( ( { status } ) => status === 'active' );
+	if ( isActiveOutput.stderr.includes( 'Command failed' ) ) {
+		const installOutput = await cli(
+			`npm run wp-env run tests-cli -- wp theme install ${ BLOCK_THEME_SLUG } --activate`
+		);
 
-	if ( ! currentTheme || currentTheme.stylesheet !== BLOCK_THEME_SLUG ) {
-		await requestUtils.activateTheme( BLOCK_THEME_SLUG );
+		expect(
+			installOutput.stdout,
+			`Could not install and/or activate ${ BLOCK_THEME_SLUG }`
+		).toContain( 'Success' );
+
 		await cli(
 			`npm run wp-env run tests-cli -- wp rewrite structure /%postname%/ --hard`
 		);
--- plugins/woocommerce-blocks/tests/e2e/utils/cli.ts
+++ plugins/woocommerce-blocks/tests/e2e/utils/cli.ts
@@ -1,10 +1,17 @@
 /**
  * External dependencies
  */
-import { exec } from 'child_process';
+import { ExecException, exec } from 'child_process';
 
-export function cli( cmd, args = [] ) {
-	return new Promise( ( resolve ) => {
+type CliOutput = {
+	code: number;
+	error: ExecException | null;
+	stdout: string;
+	stderr: string;
+};
+
+export function cli( cmd: string, args = [] ) {
+	return new Promise< CliOutput >( ( resolve ) => {
 		exec( `${ cmd } ${ args.join( ' ' ) }`, ( error, stdout, stderr ) => {
 			resolve( {
 				code: error && error.code ? error.code : 0,
