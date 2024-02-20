904cdb29d506e9bbe30458ff1295cf047a41cd2f
Thilina Hasantha <thilina.hasantha@gmail.com>
2024-02-20 16:47:42 +0100

    Function to identify if the market-place plugin is installed.

--- plugins/woocommerce/includes/admin/helper/class-wc-helper-plugin.php
+++ plugins/woocommerce/includes/admin/helper/class-wc-helper-plugin.php
@@ -26,4 +26,13 @@ class WC_Helper_Plugin {
 	public static function is_plugin_active(): bool {
 		return is_plugin_active_for_network( self::WOO_MARKETPLACE_PLUGIN_MAIN_FILE ) || is_plugin_active( self::WOO_MARKETPLACE_PLUGIN_MAIN_FILE );
 	}
+
+	/**
+	 * Check if the marketplace plugin is installed.
+	 *
+	 * @return bool
+	 */
+	public static function is_plugin_installed() {
+		return file_exists( WP_PLUGIN_DIR . '/' . self::WOO_MARKETPLACE_PLUGIN_MAIN_FILE );
+	}
 }
