7064a0d4391fc21a317219a3e921c3d2b0d0f72c
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-02-20 12:08:04 -0300

    Fix date time formating

--- packages/js/product-editor/src/hooks/use-product-scheduled/use-product-scheduled.ts
+++ packages/js/product-editor/src/hooks/use-product-scheduled/use-product-scheduled.ts
@@ -3,7 +3,7 @@
  */
 import { useEntityProp } from '@wordpress/core-data';
 import { useDispatch } from '@wordpress/data';
-import { isInTheFuture } from '@wordpress/date';
+import { isInTheFuture, date as formatDate } from '@wordpress/date';
 import { Product, ProductStatus } from '@woocommerce/data';
 
 /**
@@ -23,16 +23,18 @@ export function useProductScheduled( postType: string ) {
 	const [ date ] = useEntityProp< string >(
 		'postType',
 		postType,
-		'date_created'
+		'date_created_gmt'
 	);
+
+	const gmtDate = `${ date }+00:00`;
 
 	// @ts-expect-error There are no types for this.
 	const { editEntityRecord } = useDispatch( 'core' );
 
 	return {
-		isScheduled: editedStatus === 'future' || isInTheFuture( date ),
-		date,
-		formattedDate: formatDatetime( date ),
+		isScheduled: editedStatus === 'future' || isInTheFuture( gmtDate ),
+		date: gmtDate,
+		formattedDate: formatDatetime( gmtDate ),
 		async editDate( value: string ) {
 			let status = prevStatus;
 			if ( isInTheFuture( value ) ) {
@@ -43,7 +45,7 @@ export function useProductScheduled( postType: string ) {
 
 			await editEntityRecord( 'postType', postType, productId, {
 				status,
-				date_created: value,
+				date_created_gmt: formatDate( 'c', value, 'GMT' ),
 			} satisfies Partial< Product > );
 		},
 	};
--- packages/js/product-editor/src/utils/date/format-datetime.ts
+++ packages/js/product-editor/src/utils/date/format-datetime.ts
@@ -10,8 +10,7 @@ import {
 export function formatDatetime( date: string ) {
 	const {
 		formats: { datetime },
-		timezone,
 	} = getSettings() as DateSettings;
 
-	return dateI18n( datetime, date, timezone.offset );
+	return dateI18n( datetime, date, undefined );
 }
