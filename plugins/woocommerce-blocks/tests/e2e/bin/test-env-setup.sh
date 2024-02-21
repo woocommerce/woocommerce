385fd45a4ea77992f93c8f1524d89c3569d5ab33
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-02-21 20:33:45 +0100

    Revert "Remove storage states when restarting env"
    This reverts commit 94e4507724bfd5e465d7ea9304a978c8c3e84d05.

--- plugins/woocommerce-blocks/tests/e2e/bin/test-env-setup.sh
+++ plugins/woocommerce-blocks/tests/e2e/bin/test-env-setup.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-
 # Extract the relative path from the plugin root to this script directory.
 # By doing so, we can run this script from anywhere.
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 head_dir=$(cd "$(dirname "$script_dir")" && cd ../../.. && pwd)
 relative_path=${script_dir#$head_dir/}
-storage_states_dir="$head_dir/woocommerce-blocks/tests/e2e/artifacts/storage-states"
-
-# Remove the storage states to ensure we re-authenticate.
-rm -rf "$storage_states_dir"
 
 # Run the main script in the container for better performance.
 wp-env run tests-cli -- bash wp-content/plugins/woocommerce/blocks-bin/playwright/scripts/index.sh
