bc2bb3c731548a4fd769e0687e4bea01709428c4
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-02-21 12:31:39 +0100

    Keep storage states in global artifacts folder ...so that they're uploaded along with failed tests artifacts. It also aligns with the upstream file structure.

--- plugins/woocommerce-blocks/tests/e2e/utils/constants.ts
+++ plugins/woocommerce-blocks/tests/e2e/utils/constants.ts
@@ -10,12 +10,18 @@ export const BLOCK_THEME_NAME = 'Twenty Twenty-Four';
 export const CLASSIC_THEME_SLUG = 'storefront';
 export const CLASSIC_THEME_NAME = 'Storefront';
 export const BASE_URL = 'http://localhost:8889';
-export const STORAGE_STATE_PATH = path.join(
-	process.cwd(),
-	'artifacts/storage-states/admin.json'
-);
+
+export const WP_ARTIFACTS_PATH =
+	process.env.WP_ARTIFACTS_PATH || path.join( process.cwd(), 'artifacts' );
+
+export const STORAGE_STATE_PATH =
+	process.env.STORAGE_STATE_PATH ||
+	path.join( WP_ARTIFACTS_PATH, 'storage-states/admin.json' );
 
 // User roles file paths
 export const adminFile = STORAGE_STATE_PATH;
-export const customerFile = '.auth/customer.json';
+export const customerFile = path.join(
+	path.dirname( STORAGE_STATE_PATH ),
+	'customer.json'
+);
 export const guestFile = '.auth/guest.json';
