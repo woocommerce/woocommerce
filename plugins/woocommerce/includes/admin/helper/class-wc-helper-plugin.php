5da46d747be3baa842c4b1e256f4a63b7f94a025
Thilina Hasantha <thilina.hasantha@gmail.com>
2024-02-26 19:31:59 +0100

    Show admin notice to install Woo Update Manager on plugins.php.

--- plugins/woocommerce/includes/admin/helper/class-wc-helper-plugin.php
+++ plugins/woocommerce/includes/admin/helper/class-wc-helper-plugin.php
@@ -16,7 +16,17 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Contains the logic to manage the Woo Marketplace plugin.
  */
 class WC_Helper_Plugin {
-	const WOO_MARKETPLACE_PLUGIN_MAIN_FILE = 'woo-marketplace/woo-marketplace.php';
+	const WOO_UPDATE_MANAGER_PLUGIN_MAIN_FILE = 'woo-marketplace/woo-marketplace.php';
+	const WOO_UPDATE_MANAGER_DOWNLOAD_URL     = 'https://woo.com/woo-update-manager/download/';
+
+	/**
+	 * Loads the class, runs on init.
+	 *
+	 * @return void
+	 */
+	public static function load(): void {
+		add_action( 'admin_init', array( __CLASS__, 'configure_admin_notices' ) );
+	}
 
 	/**
 	 * Check if the marketplace plugin is active.
@@ -24,7 +34,7 @@ class WC_Helper_Plugin {
 	 * @return bool
 	 */
 	public static function is_plugin_active(): bool {
-		return is_plugin_active_for_network( self::WOO_MARKETPLACE_PLUGIN_MAIN_FILE ) || is_plugin_active( self::WOO_MARKETPLACE_PLUGIN_MAIN_FILE );
+		return is_plugin_active_for_network( self::WOO_UPDATE_MANAGER_PLUGIN_MAIN_FILE ) || is_plugin_active( self::WOO_UPDATE_MANAGER_PLUGIN_MAIN_FILE );
 	}
 
 	/**
@@ -33,7 +43,7 @@ class WC_Helper_Plugin {
 	 * @return bool
 	 */
 	public static function is_plugin_installed(): bool {
-		return file_exists( WP_PLUGIN_DIR . '/' . self::WOO_MARKETPLACE_PLUGIN_MAIN_FILE );
+		return file_exists( WP_PLUGIN_DIR . '/' . self::WOO_UPDATE_MANAGER_PLUGIN_MAIN_FILE );
 	}
 
 	/**
@@ -89,4 +99,59 @@ class WC_Helper_Plugin {
 			$url
 		);
 	}
+
+	/**
+	 * Configure admin notices.
+	 *
+	 * @return void
+	 */
+	public static function configure_admin_notices(): void {
+		if ( self::is_plugin_installed() ) {
+			return;
+		}
+
+		if ( ! self::is_loading_plugin_management_page() ) {
+			return;
+		}
+
+		add_action( 'admin_notices', array( __CLASS__, 'show_install_notice_on_plugin_management_page' ) );
+	}
+
+	/**
+	 * Show a notice on the plugins management page to install the Woo Update Manager plugin.
+	 *
+	 * @return void
+	 */
+	public static function show_install_notice_on_plugin_management_page(): void {
+		printf(
+			'<div class="notice notice-error is-dismissible"><p>%s</p></div>',
+			sprintf(
+				wp_kses(
+				/* translators: 1: Woo Update Manager plugin install URL 2: Woo Update Manager plugin download URL */
+					__(
+						'Please <a href="%1$s">Install the Woo Update Manager</a> plugin to keep getting updates and streamlined support for your Woo.com subscriptions. You can also <a href="%2$s">download</a> and install it manually.',
+						'woocommerce'
+					),
+					array(
+						'a' => array(
+							'href' => array(),
+						),
+					)
+				),
+				esc_url( self::generate_install_url() ),
+				esc_url( self::WOO_UPDATE_MANAGER_DOWNLOAD_URL )
+			)
+		);
+	}
+
+	/**
+	 * Check if the current page is the plugin management page.
+	 *
+	 * @return bool
+	 */
+	protected static function is_loading_plugin_management_page(): bool {
+		return 'plugins.php' === wp_parse_url( basename( get_self_link() ), PHP_URL_PATH );
+	}
 }
+
+WC_Helper_Plugin::load();
