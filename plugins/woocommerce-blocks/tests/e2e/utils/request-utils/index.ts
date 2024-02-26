4d6a816b9b7518186073ff67e8e2cdc171c1da97
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-02-26 15:06:14 +0100

    Fix setup haxx

--- plugins/woocommerce-blocks/tests/e2e/utils/request-utils/index.ts
+++ plugins/woocommerce-blocks/tests/e2e/utils/request-utils/index.ts
@@ -13,10 +13,28 @@ export class RequestUtils extends BaseRequestUtils {
 	// The `setup` override is necessary only until
 	// https://github.com/WordPress/gutenberg/pull/59362 is merged.
 	static async setup( ...args: Parameters< typeof BaseRequestUtils.setup > ) {
-		const baseRequestUtils = await BaseRequestUtils.setup( ...args );
-		baseRequestUtils.constructor = this;
+		const { request, user, storageState, storageStatePath, baseURL } =
+			await BaseRequestUtils.setup( ...args );
 
-		return baseRequestUtils;
+		// We need those checks to satisfy TypeScript.
+		if ( ! storageState ) {
+			throw new Error( 'Storage state is required' );
+		}
+
+		if ( ! storageStatePath ) {
+			throw new Error( 'Storage state path is required' );
+		}
+
+		if ( ! baseURL ) {
+			throw new Error( 'Base URL is required' );
+		}
+
+		return new this( request, {
+			user,
+			storageState,
+			storageStatePath,
+			baseURL,
+		} );
 	}
 
 	/** @borrows createPostFromTemplate as this.createPostFromTemplate */
