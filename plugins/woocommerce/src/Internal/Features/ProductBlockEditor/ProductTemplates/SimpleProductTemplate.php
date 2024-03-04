058fdcd972f450d055c486c9f311c71b5d905846
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-03-04 14:51:31 -0300

    Register woocommerce/product-custom-fields-toggle-field block

--- plugins/woocommerce/src/Internal/Features/ProductBlockEditor/ProductTemplates/SimpleProductTemplate.php
+++ plugins/woocommerce/src/Internal/Features/ProductBlockEditor/ProductTemplates/SimpleProductTemplate.php
@@ -568,6 +568,17 @@ class SimpleProductTemplate extends AbstractProductFormTemplate implements Produ
 				'order'     => 10,
 			)
 		);
+
+		$product_catalog_section->add_block(
+			array(
+				'id'         => 'product-custom-fields-toggle',
+				'blockName'  => 'woocommerce/product-custom-fields-toggle-field',
+				'order'      => 20,
+				'attributes' => array(
+					'label' => __( 'Show custom fields', 'woocommerce' ),
+				),
+			)
+		);
 	}
 
 	/**
