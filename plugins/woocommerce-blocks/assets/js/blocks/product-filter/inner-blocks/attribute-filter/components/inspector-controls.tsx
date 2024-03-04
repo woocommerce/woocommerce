woocommerce/woocommerce

commit ccb1fb3d7674d9ca9ba3932cece391773a84b15b

[Experimental] Fix a bug in new attribute filter where we didn't set the attribute from content panel (#44757)

plugins/woocommerce-blocks/assets/js/blocks/product-filter/inner-blocks/attribute-filter/components/inspector-controls.tsx
@@ -3,7 +3,6 @@
  */
 import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/block-editor';
-import { BlockEditProps } from '@wordpress/blocks';
 import {
 	PanelBody,
 	ToggleControl,
@@ -23,8 +22,11 @@ import { BlockAttributes } from '../types';
 
 export const Inspector = ( {
 	attributes,
-	setAttributes,
-}: BlockEditProps< BlockAttributes > ) => {
+	setAttributeId,
+}: {
+	attributes: BlockAttributes;
+	setAttributeId: ( id: unknown ) => void;
+} ) => {
 	const { attributeId, showCounts, queryType, displayStyle, selectType } =
 		attributes;
 	return (
@@ -120,7 +122,7 @@ export const Inspector = ( {
 				<AttributeSelectControls
 					isCompact={ true }
 					attributeId={ attributeId }
-					setAttributes={ setAttributes }
+					setAttributeId={ setAttributeId }
 				/>
 			</PanelBody>
 		</InspectorControls>

plugins/woocommerce-blocks/assets/js/blocks/product-filter/inner-blocks/attribute-filter/edit.tsx
@@ -115,6 +115,7 @@ const Edit = ( props: EditProps ) => {
 	const [ isEditing, setIsEditing ] = useState(
 		! attributeId && ! isPreview
 	);
+
 	const [ attributeOptions, setAttributeOptions ] = useState<
 		AttributeTerm[]
 	>( [] );
@@ -240,13 +241,18 @@ const Edit = ( props: EditProps ) => {
 			</Wrapper>
 		);
 
+	const inspectorProps = {
+		...props,
+		setAttributeId,
+	};
+
 	return (
 		<Wrapper
 			onClickToolbarEdit={ toggleEditing }
 			isEditing={ isEditing }
 			blockProps={ blockProps }
 		>
-			<Inspector { ...props } />
+			<Inspector { ...inspectorProps } />
 			<Disabled>
 				{ displayStyle === 'dropdown' ? (
 					<AttributeDropdown

plugins/woocommerce-blocks/assets/js/editor-components/search-list-control/style.scss
@@ -117,10 +117,14 @@
 		padding: $gap-small 0;
 		text-align: center;
 		border: none;
+		display: flex;
+		justify-content: center;
 
 		.woocommerce-search-list__not-found-icon,
 		.woocommerce-search-list__not-found-text {
-			display: inline-block;
+			display: flex;
+			align-content: center;
+			flex-wrap: wrap;
 		}
 
 		.woocommerce-search-list__not-found-icon {
@@ -236,7 +240,7 @@
 
 		@for $i from 1 to 5 {
 			&.depth-#{$i} {
-				padding-left: $gap * ( $i + 1 );
+				padding-left: $gap * ($i + 1);
 			}
 
 			&.depth-#{$i} .woocommerce-search-list__item-label::before {

commit eb9dc630c694e609875208b78e0d38246b612387

Fix: regression introduced in #44757 that breaks the inspector settings of the new attribute filter block

plugins/woocommerce-blocks/assets/js/blocks/product-filter/inner-blocks/attribute-filter/components/inspector-controls.tsx
@@ -18,17 +18,12 @@ import {
  * Internal dependencies
  */
 import { AttributeSelectControls } from './attribute-select-controls';
-import { BlockAttributes } from '../types';
+import { EditProps } from '../types';
 
-export const Inspector = ( {
-	attributes,
-	setAttributeId,
-}: {
-	attributes: BlockAttributes;
-	setAttributeId: ( id: unknown ) => void;
-} ) => {
+export const Inspector = ( { attributes, setAttributes }: EditProps ) => {
 	const { attributeId, showCounts, queryType, displayStyle, selectType } =
 		attributes;
+
 	return (
 		<InspectorControls key="inspector">
 			<PanelBody title={ __( 'Display Settings', 'woocommerce' ) }>
@@ -122,7 +117,11 @@ export const Inspector = ( {
 				<AttributeSelectControls
 					isCompact={ true }
 					attributeId={ attributeId }
-					setAttributeId={ setAttributeId }
+					setAttributeId={ ( id: number ) => {
+						setAttributes( {
+							attributeId: id,
+						} );
+					} }
 				/>
 			</PanelBody>
 		</InspectorControls>

plugins/woocommerce-blocks/assets/js/blocks/product-filter/inner-blocks/attribute-filter/edit.tsx
@@ -241,18 +241,13 @@ const Edit = ( props: EditProps ) => {
 			</Wrapper>
 		);
 
-	const inspectorProps = {
-		...props,
-		setAttributeId,
-	};
-
 	return (
 		<Wrapper
 			onClickToolbarEdit={ toggleEditing }
 			isEditing={ isEditing }
 			blockProps={ blockProps }
 		>
-			<Inspector { ...inspectorProps } />
+			<Inspector { ...props } />
 			<Disabled>
 				{ displayStyle === 'dropdown' ? (
 					<AttributeDropdown