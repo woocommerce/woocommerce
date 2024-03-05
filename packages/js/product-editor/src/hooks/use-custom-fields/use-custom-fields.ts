f93be6677a031728d5c786f01049f25880a3cb62
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-03-05 15:02:22 -0300

    Create useCustomFields hook

---
+++ packages/js/product-editor/src/hooks/use-custom-fields/use-custom-fields.ts
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import type { Dispatch, SetStateAction } from 'react';
+import { useEntityProp } from '@wordpress/core-data';
+import { useMemo } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import type { Metadata } from '../../types';
+import { disjoinMetas, isCustomField } from './utils';
+
+export function useCustomFields<
+	T extends Metadata< string > = Metadata< string >
+>(): [ T[], Dispatch< T[] > ] {
+	const [ metas, setMetas ] = useEntityProp< T[] >(
+		'postType',
+		'product',
+		'meta_data'
+	);
+
+	const { customFields, internalMetas } = useMemo(
+		function extractCustomFieldsFromMetas() {
+			return metas.reduce( disjoinMetas< T >, {
+				customFields: [],
+				internalMetas: [],
+			} );
+		},
+		[ metas ]
+	);
+
+	function setCustomFields( value: SetStateAction< T[] > ) {
+		const newValue =
+			typeof value === 'function' ? value( customFields ) : value;
+
+		const newValueWithoutPrefix = newValue.filter( isCustomField );
+
+		setMetas( [ ...internalMetas, ...newValueWithoutPrefix ] );
+	}
+
+	return [ customFields, setCustomFields ];
+}
