57115efe6d92fec226311445ccca572ed1b1bb6c
Nathan Schneider <nsschneider1@gmail.com>
2024-03-06 10:02:06 -0300

    Allow using woocommerce icons for product details section description and add gift icon

---
+++ packages/js/product-editor/src/icons/gift.tsx
@@ -0,0 +1,38 @@
+/**
+ * External dependencies
+ */
+import { createElement } from '@wordpress/element';
+
+const gift = (
+	<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<rect
+			x="-0.75"
+			y="-0.75"
+			fill="none"
+			width="9.5"
+			height="14.5"
+			transform="matrix(3.97376e-08 -1 -1 -4.80825e-08 18.5 18.5)"
+			stroke="#1E1E1E"
+			strokeWidth="1.5"
+		/>
+		<path
+			fillRule="evenodd"
+			clipRule="evenodd"
+			d="M13 19L13 9L11.5 9L11.5 19L13 19Z"
+		/>
+		<path
+			d="M16.5 6.5C16.5 7.4665 15.7165 8.25 14.75 8.25H13V6.5C13 5.5335 13.7835 4.75 14.75 4.75C15.7165 4.75 16.5 5.5335 16.5 6.5Z"
+			stroke="#1E1E1E"
+			fill="none"
+			strokeWidth="1.5"
+		/>
+		<path
+			d="M8 6.5C8 7.4665 8.7835 8.25 9.75 8.25H11.5V6.5C11.5 5.5335 10.7165 4.75 9.75 4.75C8.7835 4.75 8 5.5335 8 6.5Z"
+			stroke="#1E1E1E"
+			fill="none"
+			strokeWidth="1.5"
+		/>
+	</svg>
+);
+
+export default gift;
