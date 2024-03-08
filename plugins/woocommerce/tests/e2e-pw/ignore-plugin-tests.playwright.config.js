0ade64b552881371b291524e3186a3c75f8514d4
Jon Lane <jon.lane@automattic.com>
2024-03-07 16:16:35 -0800

    Hide slow tests

--- plugins/woocommerce/tests/e2e-pw/ignore-plugin-tests.playwright.config.js
+++ plugins/woocommerce/tests/e2e-pw/ignore-plugin-tests.playwright.config.js
@@ -3,6 +3,7 @@ const defaultUse = defaultConfig.use;
 
 const config = {
 	...defaultConfig,
+	reportSlowTests: { max: 0, threshold: 30 * 1000 }, // 30 seconds threshold
 	testIgnore: '**/smoke-tests/**',
 	use: {
 		...defaultUse,
