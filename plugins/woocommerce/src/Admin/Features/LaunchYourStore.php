0558ed6e3acafebd4dd950e0d6f7725b4b9753d8
moon <moon.kyong@automattic.com>
2024-03-07 18:29:02 -0800

    Lint fixes

--- plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
+++ plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
@@ -19,6 +19,11 @@ class LaunchYourStore {
 	 * @return void
 	 */
 	public function save_site_visibility_options() {
+		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		if ( empty( $_REQUEST['_wpnonce'] ) || ! wp_verify_nonce( wp_unslash( $_REQUEST['_wpnonce'] ), 'woocommerce-settings' ) ) {
+			return;
+		}
+
 		$options = array(
 			'woocommerce_coming_soon'      => array( 'yes', 'no' ),
 			'woocommerce_store_pages_only' => array( 'yes', 'no' ),
@@ -26,8 +31,10 @@ class LaunchYourStore {
 		);
 
 		foreach ( $options as $name => $option ) {
+			// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 			if ( isset( $_POST[ $name ] ) && in_array( $_POST[ $name ], $option, true ) ) {
-				update_option( $name, $_POST[ $name ] );
+				// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+				update_option( $name, wp_unslash( $_POST[ $name ] ) );
 			}
 		}
 	}
