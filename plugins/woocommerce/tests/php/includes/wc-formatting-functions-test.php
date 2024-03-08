c324b7a511abbb8a63d3ae5d487baa4db3d8a436
Sakri Koskimies <sakri.koskimies@hotmail.com>
2024-03-08 22:46:00 +0200

    Add data providers for WC_Formatting_Functions_Test tests

--- plugins/woocommerce/tests/php/includes/wc-formatting-functions-test.php
+++ plugins/woocommerce/tests/php/includes/wc-formatting-functions-test.php
@@ -11,19 +11,52 @@
 class WC_Formatting_Functions_Test extends \WC_Unit_Test_Case {
 
 	/**
-	 * Test wc_sanitize_coupon_code() function.
+	 * Data provider for test_wc_sanitize_coupon_code.
+	 *
+	 * @return array[]
 	 */
-	public function test_wc_sanitize_coupon_code() {
-		$this->assertEquals( 'DUMMYCOUPON', wc_sanitize_coupon_code( 'DUMMYCOUPON' ) );
-		$this->assertEquals( 'a&amp;a', wc_sanitize_coupon_code( 'a&a' ) );
-		$this->assertEquals( "test's", wc_sanitize_coupon_code( "test's" ) );
+	public function data_provider_test_wc_sanitize_coupon_code(): array {
+		return array(
+			array( 'DUMMYCOUPON', 'DUMMYCOUPON' ),
+			array( 'a&amp;a', 'a&a' ),
+			array( "test's", "test's" ),
+		);
+	}
+
+	/**
+	 * Test wc_sanitize_coupon_code() function.
+	 *
+	 * @dataProvider data_provider_test_wc_sanitize_coupon_code
+	 *
+	 * @param string $assert Expected result.
+	 * @param string $input Input for wc_sanitize_coupon_code().
+	 */
+	public function test_wc_sanitize_coupon_code( string $assert, string $input ) {
+		$this->assertSame( $assert, wc_sanitize_coupon_code( $input ) );
+	}
+
+	/**
+	 * Data provider for test_wc_format_postcode.
+	 *
+	 * @return array[]
+	 */
+	public function data_provider_test_wc_format_postcode(): array {
+		return array(
+			array( 'D02 AF30', 'D02AF30', 'IE' ),
+			array( '1000-205', '1000205', 'PT' ),
+		);
 	}
 
 	/**
 	 * Test wc_format_postcode() function.
+	 *
+	 * @dataProvider data_provider_test_wc_format_postcode
+	 *
+	 * @param string $assert Expected result.
+	 * @param string $postcode Postcode input for wc_format_postcode().
+	 * @param string $country Country input for wc_format_postcode().
 	 */
-	public function test_wc_format_postcode() {
-		$this->assertEquals( 'D02 AF30', wc_format_postcode( 'D02AF30', 'IE' ), 'Test formatting of IE postcodes.' );
-		$this->assertEquals( '1000-205', wc_format_postcode( '1000205', 'PT' ), 'Test formatting of PT postcodes.' );
+	public function test_wc_format_postcode( string $assert, string $postcode, string $country ) {
+		$this->assertSame( $assert, wc_format_postcode( $postcode, $country ) );
 	}
 }
