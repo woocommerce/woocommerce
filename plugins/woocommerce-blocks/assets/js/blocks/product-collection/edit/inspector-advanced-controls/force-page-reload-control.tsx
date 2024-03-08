b394fb94a7f7cdb42f70564c7ab92c4e85f7a55e
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-03-08 12:33:00 +0100

    Add enhancedPagination attribute that decides if client side navigation is enabled

--- plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-advanced-controls/force-page-reload-control.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-advanced-controls/force-page-reload-control.tsx
@@ -4,21 +4,39 @@
 import { __ } from '@wordpress/i18n';
 import { ToggleControl } from '@wordpress/components';
 
-/**
- * Internal dependencies
- */
-// import { QueryControlProps } from '../../types';
+import type { ProductCollectionSetAttributes } from '../../types';
 
-const ForcePageReloadControl = ( props ) => {
-	console.log( props );
+type ForcePageReloadControlProps = {
+	enhancedPagination: boolean;
+	setAttributes: ProductCollectionSetAttributes;
+};
+
+const helpTextIfEnabled = __(
+	'Enforce full page reload on certain interactions, like using paginations controls.',
+	'woocommerce'
+);
+const helpTextIfDisabled = __(
+	"Force page reload can't be disabled because there are non-compatible blocks inside the Product Collection block.",
+	'woocommerce'
+);
+
+const ForcePageReloadControl = ( props: ForcePageReloadControlProps ) => {
+	const { enhancedPagination, setAttributes } = props;
+	const hasUnsupportedBlocks = false; // TODO: Detect unsupported blocks
+
+	const helpText = hasUnsupportedBlocks
+		? helpTextIfDisabled
+		: helpTextIfEnabled;
+
 	return (
 		<ToggleControl
 			label={ __( 'Force Page Reload', 'woocommerce' ) }
-			help={ __(
-				'Enforce full page reload on certain interactions, like using paginations controls.'
-			) }
-			checked={ false }
-			onChange={ () => true }
+			help={ helpText }
+			checked={ enhancedPagination }
+			onChange={ () =>
+				setAttributes( { enhancedPagination: ! enhancedPagination } )
+			}
+			disabled={ hasUnsupportedBlocks }
 		/>
 	);
 };
--- plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-advanced-controls/index.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-advanced-controls/index.tsx
@@ -8,25 +8,20 @@ import { InspectorAdvancedControls } from '@wordpress/block-editor';
  * Internal dependencies
  */
 import ForcePageReloadControl from './force-page-reload-control';
-import { setQueryAttribute } from '../../utils';
 import type { ProductCollectionEditComponentProps } from '../../types';
 
 export default function ToolbarControls(
 	props: ProductCollectionEditComponentProps
 ) {
-	const { attributes, openCollectionSelectionModal, setAttributes } = props;
-	const { query, displayLayout } = attributes;
-
-	const setQueryAttributeBind = useMemo(
-		() => setQueryAttribute.bind( null, props ),
-		[ props ]
-	);
-
-	console.log( 'it goes', InspectorAdvancedControls );
+	const { attributes, setAttributes } = props;
+	const { enhancedPagination } = attributes;
 
 	return (
 		<InspectorAdvancedControls>
-			<ForcePageReloadControl { ...props } />
+			<ForcePageReloadControl
+				enhancedPagination={ enhancedPagination }
+				setAttributes={ setAttributes }
+			/>
 		</InspectorAdvancedControls>
 	);
 }
