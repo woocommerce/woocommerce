bc20d5681ad7f4b082dd2be09b7e197d380bf98f
moon <moon.kyong@automattic.com>
2024-03-07 18:20:01 -0800

    Lint fixes

--- plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
+++ plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
@@ -6,6 +6,9 @@ namespace Automattic\WooCommerce\Admin\Features;
  * Takes care of Launch Your Store related actions.
  */
 class LaunchYourStore {
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 		add_action( 'woocommerce_update_options_general', array( $this, 'save_site_visibility_options' ) );
 	}
@@ -17,13 +20,13 @@ class LaunchYourStore {
 	 */
 	public function save_site_visibility_options() {
 		$options = array(
-			'woocommerce_coming_soon' => ['yes', 'no'],
-			'woocommerce_store_pages_only' => ['yes', 'no'],
-			'woocommerce_private_link' => ['yes', 'no']
+			'woocommerce_coming_soon'      => array( 'yes', 'no' ),
+			'woocommerce_store_pages_only' => array( 'yes', 'no' ),
+			'woocommerce_private_link'     => array( 'yes', 'no' ),
 		);
 
 		foreach ( $options as $name => $option ) {
-			if ( isset( $_POST[ $name ] ) && in_array( $_POST[ $name ], $option ) ) {
+			if ( isset( $_POST[ $name ] ) && in_array( $_POST[ $name ], $option, true ) ) {
 				update_option( $name, $_POST[ $name ] );
 			}
 		}
