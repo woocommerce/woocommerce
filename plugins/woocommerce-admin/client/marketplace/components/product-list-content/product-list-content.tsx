17049b2aae1feb5f13e600fd206bc7558a3531fb
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2024-03-11 21:29:54 +0100

    Add the 'Browse the WordPress.org theme directory to discover more' link.

--- plugins/woocommerce-admin/client/marketplace/components/product-list-content/product-list-content.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/product-list-content/product-list-content.tsx
@@ -1,8 +1,14 @@
 /**
  * External dependencies
  */
-import { Fragment, useEffect, useState } from '@wordpress/element';
+import {
+	createInterpolateElement,
+	Fragment,
+	useEffect,
+	useState,
+} from '@wordpress/element';
 import classnames from 'classnames';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
@@ -13,7 +19,7 @@ import '~/customize-store/style.scss';
 import ProductCard from '../product-card/product-card';
 import { Product, ProductType } from '../product-list/types';
 import { appendURLParams } from '../../utils/functions';
-import { getAdminSetting } from '~/utils/admin-settings';
+import { ADMIN_URL, getAdminSetting } from '~/utils/admin-settings';
 import { NoAIBanner } from '~/customize-store/intro/intro-banners';
 
 export default function ProductListContent( props: {
@@ -58,58 +64,89 @@ export default function ProductListContent( props: {
 	const bannerPosition = columns * 2 - 1;
 
 	return (
-		<div className={ classes }>
-			{ props.products.map( ( product, index ) => (
-				<Fragment key={ product.id }>
-					<ProductCard
-						key={ product.id }
-						type={ props.type }
-						product={ {
-							id: product.id,
-							slug: product.slug,
-							title: product.title,
-							image: product.image,
-							type: product.type,
-							icon: product.icon,
-							vendorName: product.vendorName,
-							vendorUrl: product.vendorUrl
-								? appendURLParams( product.vendorUrl, [
-										[ 'utm_source', 'extensionsscreen' ],
-										[ 'utm_medium', 'product' ],
-										[ 'utm_campaign', 'wcaddons' ],
-										[ 'utm_content', 'devpartner' ],
-								  ] )
-								: '',
-							price: product.price,
-							url: appendURLParams(
-								product.url,
-								Object.entries( {
-									...wccomHelperSettings.inAppPurchaseURLParams,
-									...( props.productGroup !== undefined
-										? { utm_group: props.productGroup }
-										: {} ),
-								} )
-							),
-							averageRating: product.averageRating,
-							reviewsCount: product.reviewsCount,
-							description: product.description,
-							isInstallable: product.isInstallable,
-						} }
-						tracksData={ {
-							position: index + 1,
-							...( product.label && { label: product.label } ),
-							...( props.group && { group: props.group } ),
-							...( props.searchTerm && {
-								searchTerm: props.searchTerm,
-							} ),
-							...( props.category && {
-								category: props.category,
-							} ),
-						} }
-					/>
-					{ index === bannerPosition && <NoAIBanner /> }
-				</Fragment>
-			) ) }
-		</div>
+		<>
+			<div className={ classes }>
+				{ props.products.map( ( product, index ) => (
+					<Fragment key={ product.id }>
+						<ProductCard
+							key={ product.id }
+							type={ props.type }
+							product={ {
+								id: product.id,
+								slug: product.slug,
+								title: product.title,
+								image: product.image,
+								type: product.type,
+								icon: product.icon,
+								vendorName: product.vendorName,
+								vendorUrl: product.vendorUrl
+									? appendURLParams( product.vendorUrl, [
+											[
+												'utm_source',
+												'extensionsscreen',
+											],
+											[ 'utm_medium', 'product' ],
+											[ 'utm_campaign', 'wcaddons' ],
+											[ 'utm_content', 'devpartner' ],
+									  ] )
+									: '',
+								price: product.price,
+								url: appendURLParams(
+									product.url,
+									Object.entries( {
+										...wccomHelperSettings.inAppPurchaseURLParams,
+										...( props.productGroup !== undefined
+											? { utm_group: props.productGroup }
+											: {} ),
+									} )
+								),
+								averageRating: product.averageRating,
+								reviewsCount: product.reviewsCount,
+								description: product.description,
+								isInstallable: product.isInstallable,
+							} }
+							tracksData={ {
+								position: index + 1,
+								...( product.label && {
+									label: product.label,
+								} ),
+								...( props.group && { group: props.group } ),
+								...( props.searchTerm && {
+									searchTerm: props.searchTerm,
+								} ),
+								...( props.category && {
+									category: props.category,
+								} ),
+							} }
+						/>
+						{ index === bannerPosition && <NoAIBanner /> }
+					</Fragment>
+				) ) }
+			</div>
+			<div
+				className={
+					'woocommerce-marketplace__browse-wp-theme-directory'
+				}
+			>
+				<b>{ __( 'Didn’t find a theme you like?', 'woocommerce' ) }</b>
+				{ createInterpolateElement(
+					__(
+						' Browse the <a>WordPress.org theme directory</a> to discover more.',
+						'woocommerce'
+					),
+					{
+						a: (
+							// eslint-disable-next-line jsx-a11y/anchor-has-content
+							<a
+								href={
+									ADMIN_URL +
+									'theme-install.php?browse=block-themes'
+								}
+							/>
+						),
+					}
+				) }
+			</div>
+		</>
 	);
 }
