cb81cb94b79bb38d68065184ce02c9b2120345b1
Jorge Torres <jorge.torres@automattic.com>
2024-03-11 13:53:07 -0500

    Augment update_order_from_object() with args parameter

--- plugins/woocommerce/includes/data-stores/abstract-wc-order-data-store-cpt.php
+++ plugins/woocommerce/includes/data-stores/abstract-wc-order-data-store-cpt.php
@@ -654,10 +654,16 @@ abstract class Abstract_WC_Order_Data_Store_CPT extends WC_Data_Store_WP impleme
 	 * Given an initialized order object, update the post/postmeta records.
 	 *
 	 * @param WC_Abstract_Order $order Order object.
+	 * @param array             $args  Arguments to control the update.
 	 *
 	 * @return bool Whether the order was updated.
 	 */
-	public function update_order_from_object( $order ) {
+	public function update_order_from_object( $order, $args = array() ) {
+		// Args are not supported by default.
+		if ( ! empty( $args ) ) {
+			return false;
+		}
+
 		if ( ! $order->get_id() ) {
 			return false;
 		}
