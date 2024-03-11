666e51e020ce71b101834e004ba0fe7f1dd75b65
Nathan Schneider <nsschneider1@gmail.com>
2024-03-11 15:17:26 -0300

    Use core components for SVG

--- packages/js/product-editor/src/icons/gift.tsx
+++ packages/js/product-editor/src/icons/gift.tsx
@@ -2,10 +2,11 @@
  * External dependencies
  */
 import { createElement } from '@wordpress/element';
+import { SVG, Path, Rect } from '@wordpress/components';
 
 const gift = (
-	<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<rect
+	<SVG viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<Rect
 			x="-0.75"
 			y="-0.75"
 			fill="none"
@@ -15,24 +16,24 @@ const gift = (
 			stroke="#1E1E1E"
 			strokeWidth="1.5"
 		/>
-		<path
+		<Path
 			fillRule="evenodd"
 			clipRule="evenodd"
 			d="M13 19L13 9L11.5 9L11.5 19L13 19Z"
 		/>
-		<path
+		<Path
 			d="M16.5 6.5C16.5 7.4665 15.7165 8.25 14.75 8.25H13V6.5C13 5.5335 13.7835 4.75 14.75 4.75C15.7165 4.75 16.5 5.5335 16.5 6.5Z"
 			stroke="#1E1E1E"
 			fill="none"
 			strokeWidth="1.5"
 		/>
-		<path
+		<Path
 			d="M8 6.5C8 7.4665 8.7835 8.25 9.75 8.25H11.5V6.5C11.5 5.5335 10.7165 4.75 9.75 4.75C8.7835 4.75 8 5.5335 8 6.5Z"
 			stroke="#1E1E1E"
 			fill="none"
 			strokeWidth="1.5"
 		/>
-	</svg>
+	</SVG>
 );
 
 export default gift;
