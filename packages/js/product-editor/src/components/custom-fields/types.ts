88f1187928a74e49ec12c71d4ea3fee879249d4e
Maikel Perez <maikel.perez@automattic.com>
2024-03-12 10:02:24 -0300

    Add Custom Fields for New Product Editor (#45484)
    * Create the Add new button
    
    * Integrate the CreateModal with the CustomFields component to add fields
    
    * Add validation utils
    
    * Fix validation errors in the edit modal component
    
    * Fix linter error
    
    * Add changelog file

--- packages/js/product-editor/src/components/custom-fields/types.ts
+++ packages/js/product-editor/src/components/custom-fields/types.ts
@@ -1,4 +1,6 @@
 export type CustomFieldsProps = React.DetailedHTMLProps<
 	React.TableHTMLAttributes< HTMLTableElement >,
 	HTMLTableElement
->;
+> & {
+	renderActionButtonsWrapper?( buttons: React.ReactNode ): React.ReactNode;
+};
