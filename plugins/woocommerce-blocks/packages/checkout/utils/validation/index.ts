6b07ca44f5c0ac0cf31a082cd390f40e3e43dcec
Niels Lange <info@nielslange.de>
2024-03-13 00:42:24 +0700

    Add totalValue filter (#45170)
    * Add totalValue filter
    
    * Add changefile(s) from automation for the following project(s): woocommerce-blocks
    
    * Remove nowrap to prevent text overflow
    
    * Start docs for ‘totalValue’ filter
    
    * Fix *.md lint issues
    
    * Ignore .md lint errors for now
    
    * Update code example and add screenshots
    
    * Fix failing JS unit tests
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce-blocks/packages/checkout/utils/validation/index.ts
+++ plugins/woocommerce-blocks/packages/checkout/utils/validation/index.ts
@@ -1,3 +1,4 @@
 export { default as mustContain } from './must-contain';
 export { default as getValidityMessageForInput } from './get-validity-message-for-input';
 export { default as isPostcode } from './is-postcode';
+export { productPriceValidation } from './validate-filter';
