11510e9fdbee5449dfeac807db310a21e17f8f9f
Alba Rincón <alba.rincon@automattic.com>
2024-03-13 09:26:36 +0100

    Show the loader while activating the theme

--- plugins/woocommerce-admin/client/customize-store/design-without-ai/state-machine.tsx
+++ plugins/woocommerce-admin/client/customize-store/design-without-ai/state-machine.tsx
@@ -81,6 +81,9 @@ export const designWithNoAiStateMachineDefinition = createMachine(
 						actions: 'redirectToIntroWithError',
 					},
 				},
+				meta: {
+					component: ApiCallLoader,
+				},
 			},
 			preAssembleSite: {
 				initial: 'preApiCallLoader',
