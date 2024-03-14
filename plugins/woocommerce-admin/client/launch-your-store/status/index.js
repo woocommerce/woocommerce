c672e3d88631ffaaf5fc22b7fc3b0e9693c47d1c
paul sealock <psealock@gmail.com>
2024-03-15 11:24:44 +1300

    Add styling based on state

--- plugins/woocommerce-admin/client/launch-your-store/status/index.js
+++ plugins/woocommerce-admin/client/launch-your-store/status/index.js
@@ -1,16 +1,22 @@
 /**
  * External dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { Icon, moreVertical } from '@wordpress/icons';
 import { Dropdown, Button, MenuGroup, MenuItem } from '@wordpress/components';
 import { getAdminLink } from '@woocommerce/settings';
+import classnames from 'classnames';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 
-export const LaunchYourStoreStatus = () => {
+export const LaunchYourStoreStatus = ( { comingSoon, storePagesOnly } ) => {
+	const isComingSoon = comingSoon && comingSoon === 'yes';
+	const dropdownText = isComingSoon
+		? __( 'Coming soon', 'woocommerce' )
+		: __( 'Live', 'woocommerce' );
 	return (
 		<div className="woocommerce-lys-status">
 			<div className="woocommerce-lys-status-pill-wrapper">
@@ -20,8 +26,13 @@ export const LaunchYourStoreStatus = () => {
 					popoverProps={ { placement: 'bottom-start' } }
 					renderToggle={ ( { isOpen, onToggle } ) => (
 						<Button onClick={ onToggle } aria-expanded={ isOpen }>
-							<div className="woocommerce-lys-status-pill">
-								<span>Coming soon</span>
+							<div
+								className={ classnames(
+									'woocommerce-lys-status-pill',
+									{ 'is-live': ! isComingSoon }
+								) }
+							>
+								<span>{ dropdownText }</span>
 								<Icon icon={ moreVertical } size={ 18 } />
 							</div>
 						</Button>
@@ -36,13 +47,15 @@ export const LaunchYourStoreStatus = () => {
 								>
 									Manage site visibility
 								</MenuItem>
-								<MenuItem
-									href={ getAdminLink(
-										'admin.php?page=wc-settings' // For now, waiting on the actual link
-									) }
-								>
-									Customize "Coming soon" page
-								</MenuItem>
+								{ isComingSoon && (
+									<MenuItem
+										href={ getAdminLink(
+											'admin.php?page=wc-settings' // For now, waiting on the actual link
+										) }
+									>
+										Customize "Coming soon" page
+									</MenuItem>
+								) }
 							</MenuGroup>
 						</>
 					) }
