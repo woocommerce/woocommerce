75c8a3ad6e2c77797af4d8f4d1e2917b90b444b8
Ilyas Foo <foo.ilyas@gmail.com>
2024-03-14 21:51:52 +0800

    Ugh ignore faulty lint rule

--- plugins/woocommerce/src/Admin/RemoteSpecs/RuleProcessors/Transformers/TransformerService.php
+++ plugins/woocommerce/src/Admin/RemoteSpecs/RuleProcessors/Transformers/TransformerService.php
@@ -54,7 +54,7 @@ class TransformerService {
 
 			$transformer = self::create_transformer( $transformer_config->use );
 			if ( null === $transformer ) {
-				throw new InvalidArgumentException( "Unable to find a transformer by name: {$transformer_config->use}" ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				throw new InvalidArgumentException( "Unable to find a transformer by name: {$transformer_config->use}" ); // phpcs:ignore WordPress.Security.EscapeOutput.ExceptionNotEscaped
 			}
 
 			$target_value = $transformer->transform( $target_value, $transformer_config->arguments, $is_default_set ? $default : null );
