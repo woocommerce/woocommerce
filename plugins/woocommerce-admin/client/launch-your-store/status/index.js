89808c1717659ae9dae84c58f3213f33894e8b11
paul sealock <psealock@gmail.com>
2024-03-15 10:23:57 +1300

    Add basic wp dropdown component

--- plugins/woocommerce-admin/client/launch-your-store/status/index.js
+++ plugins/woocommerce-admin/client/launch-your-store/status/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { Icon, moreVertical } from '@wordpress/icons';
+import { Dropdown, Button, MenuGroup, MenuItem } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -12,10 +13,31 @@ export const LaunchYourStoreStatus = () => {
 	return (
 		<div className="woocommerce-lys-status">
 			<div className="woocommerce-lys-status-pill-wrapper">
-				<div className="woocommerce-lys-status-pill">
-					<span>Coming soon</span>
-					<Icon icon={ moreVertical } size={ 18 } />
-				</div>
+				<Dropdown
+					className="woocommerce-lys-status-dropdown"
+					focusOnMount={ true }
+					popoverProps={ { placement: 'bottom-start' } }
+					renderToggle={ ( { isOpen, onToggle } ) => (
+						<Button onClick={ onToggle } aria-expanded={ isOpen }>
+							<div className="woocommerce-lys-status-pill">
+								<span>Coming soon</span>
+								<Icon icon={ moreVertical } size={ 18 } />
+							</div>
+						</Button>
+					) }
+					renderContent={ () => (
+						<>
+							<MenuGroup>
+								<MenuItem href="https://developer.wordpress.org/block-editor/reference-guides/components/button/">
+									Manage site visibility
+								</MenuItem>
+								<MenuItem href="https://developer.wordpress.org/block-editor/reference-guides/components/button/">
+									Customize "Coming soon" page
+								</MenuItem>
+							</MenuGroup>
+						</>
+					) }
+				/>
 			</div>
 		</div>
 	);
