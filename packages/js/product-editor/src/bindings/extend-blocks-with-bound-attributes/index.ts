d3e847a11b8c95c58e4e7ee24d34b0385593f09a
Damián Suárez <rdsuarez@gmail.com>
2024-03-18 06:50:01 +0000

    initial implementation of custom binding handler

---
+++ packages/js/product-editor/src/bindings/extend-blocks-with-bound-attributes/index.ts
@@ -0,0 +1,54 @@
+/**
+ * External dependencies
+ */
+import type { BlockConfiguration } from '@wordpress/blocks';
+/**
+ * Internal dependencies
+ */
+import { isItPossibleToBindBlock } from '..';
+import withBlockBindingSupport from './core-block-binding-support';
+
+/**
+ * Extend the block settings with the bound attributes.
+ *
+ * @param {BlockConfiguration} settings - The block settings.
+ * @param {string}             name     - The block name.
+ * @return {BlockConfiguration} The extended block settings.
+ */
+export default function extendBlockWithBoundAttributes(
+	settings: BlockConfiguration,
+	name: string
+): BlockConfiguration {
+	if ( ! isItPossibleToBindBlock( name ) ) {
+		return settings;
+	}
+
+	/*
+	 * Bail early if the block
+	 * doesn't have an edit function.
+	 */
+	if ( ! settings.edit ) {
+		return settings;
+	}
+
+	return {
+		...settings,
+		/*
+		 * Expose relevant data through
+		 * the block context.
+		 */
+		usesContext: [
+			...new Set( [
+				...( settings.usesContext || [] ),
+				'postId',
+				'postType',
+				'queryId',
+			] ),
+		],
+
+		/*
+		 * Add the block binding support.
+		 */
+		edit: withBlockBindingSupport( settings.edit ),
+	};
+}
