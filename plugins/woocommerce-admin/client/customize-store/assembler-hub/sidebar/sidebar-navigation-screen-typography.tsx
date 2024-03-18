cdb7799d75b0b17d4a4a782a4438067fd7f6fdff
Luigi Teschio <gigitux@gmail.com>
2024-03-18 11:20:28 +0100

    remove not necessary async

--- plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/sidebar-navigation-screen-typography.tsx
+++ plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/sidebar-navigation-screen-typography.tsx
@@ -201,7 +201,7 @@ export const SidebarNavigationScreenTypography = () => {
 											{ __( 'Cancel', 'woocommerce' ) }
 										</Button>
 										<Button
-											onClick={ async () => {
+											onClick={ () => {
 												optIn();
 												if ( isIframe( window ) ) {
 													sendMessageToParent( {
