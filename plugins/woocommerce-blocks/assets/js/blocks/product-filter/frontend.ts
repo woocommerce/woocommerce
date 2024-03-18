a2eb59ecd991abc4a6c11b48c5580cd8e883b2ba
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2024-03-18 15:18:15 -0700

    Reset Product Collection Pagination When Filtering
    Since we can't be sure that a page exists once the filters have been
    changed we should reset the page when they do.

--- plugins/woocommerce-blocks/assets/js/blocks/product-filter/frontend.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filter/frontend.ts
@@ -12,6 +12,14 @@ const needsRefresh = getSetting< boolean >(
 );
 
 export function navigate( href: string, options = {} ) {
+	// Remove the page number if we're changing the filters to prevent
+	// a 404 if the page does not exist with a given set of filters.
+	if ( href.includes( 'paged' ) ) {
+		const url = new URL( href );
+		url.searchParams.delete( 'paged' );
+		href = url.href;
+	}
+
 	if ( needsRefresh || ( ! isBlockTheme && isProductArchive ) ) {
 		return ( window.location.href = href );
 	}
