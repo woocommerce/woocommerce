1d97d3cecaaf3f658bae7be150994b4149b54f79
Sam Seay <samueljseay@gmail.com>
2024-03-19 14:01:48 +0800

    Create vendor bundle for shared deps between blocks-checkout and blocks-components.

--- plugins/woocommerce-blocks/bin/webpack-configs.js
+++ plugins/woocommerce-blocks/bin/webpack-configs.js
@@ -135,6 +135,12 @@ woocommerce_blocks_env = ${ NODE_ENV }
 				automaticNameDelimiter: '--',
 				cacheGroups: {
 					...getCacheGroups(),
+					commons: {
+						// Just vendor the largest UI dependencies shared across blocks-components and blocks-checkout.
+						test: /[\\/]node_modules[\\/](?:@wordpress[\\/](?:components|icons)|@ariakit[\\/](?:react|react-core|core)|dompurify|stylis|@floating-ui|@emotion|react-number-format|react-transition-group|postcode-validator)[\\/]/,
+						name: 'wc-blocks-vendors-core',
+						chunks: 'all',
+					},
 				},
 			},
 			minimizer: [
