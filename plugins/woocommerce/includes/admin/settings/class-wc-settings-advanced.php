1583ff0632dc41c6fe5f50c6be100efe949ddba4
moon <moon.kyong@automattic.com>
2024-03-19 14:05:38 -0700

    Add 'coming-soon' page
    * Add coming soon page when user hits the core profiler page.
    
    * Added coming soon page selector under Page setup

--- plugins/woocommerce/includes/admin/settings/class-wc-settings-advanced.php
+++ plugins/woocommerce/includes/admin/settings/class-wc-settings-advanced.php
@@ -134,6 +134,26 @@ class WC_Settings_Advanced extends WC_Settings_Page {
 				),
 
 				array(
+					'title'    => __( 'Coming soon', 'woocommerce' ),
+					'desc'     => __( 'TBD', 'woocommerce' ),
+					'id'       => 'woocommerce_coming_soon_page_id',
+					'type'     => 'single_select_page_with_search',
+					'default'  => '',
+					'class'    => 'wc-page-search',
+					'css'      => 'min-width:300px;',
+					'args'     => array(
+						'exclude' =>
+							array(
+								wc_get_page_id( 'checkout' ),
+								wc_get_page_id( 'myaccount' ),
+							),
+					),
+					'desc_tip' => true,
+					'autoload' => false,
+				),
+
+
+				array(
 					'type' => 'sectionend',
 					'id'   => 'advanced_page_options',
 				),
