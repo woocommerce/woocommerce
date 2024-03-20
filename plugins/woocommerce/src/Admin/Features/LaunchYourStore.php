ccd1549274659b5db2932b2550928531f710d95c
moon <moon.kyong@automattic.com>
2024-03-20 13:05:45 -0700

    Move coming soon page logic to launch your store feature class

--- plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
+++ plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
@@ -11,6 +11,7 @@ class LaunchYourStore {
 	 */
 	public function __construct() {
 		add_action( 'woocommerce_update_options_general', array( $this, 'save_site_visibility_options' ) );
+		add_action( 'current_screen', array( $this, 'maybe_create_coming_soon_page' ) );
 	}
 
 	/**
@@ -36,6 +37,27 @@ class LaunchYourStore {
 				// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 				update_option( $name, wp_unslash( $_POST[ $name ] ) );
 			}
+		}
+	}
+
+	/**
+	 * Add `coming soon` page when it hasn't been created yet.
+	 *
+	 * @param WP_Screen $current_screen Current screen object.
+	 *
+	 * @return void
+	 */
+	public function maybe_create_coming_soon_page( $current_screen ) {
+		// phpcs:ignore
+		$is_welcome_page = isset( $_GET['path'] ) && '/setup-wizard' === $_GET['path'];
+		$page_id_option  = get_option( 'woocommerce_coming_soon_page_id', false );
+		if ( $current_screen && 'woocommerce_page_wc-admin' === $current_screen->id && $is_welcome_page && ! $page_id_option ) {
+			wc_create_page(
+				esc_sql( _x( 'Coming Soon', 'Page slug', 'woocommerce' ) ),
+				'woocommerce_coming_soon_page_id',
+				_x( 'Coming Soon', 'Page title', 'woocommerce' ),
+				'tbd',
+			);
 		}
 	}
 }
