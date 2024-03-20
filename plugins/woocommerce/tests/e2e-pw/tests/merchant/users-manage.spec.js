b6d7c68f18bf3065b6f02129b7a890cfeeab6410
Adrian Moldovan <adim.moldovan@gmail.com>
2024-03-20 17:49:33 +0200

    Update user edit tests

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/users-manage.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/users-manage.spec.js
@@ -7,6 +7,9 @@ const test = baseTest.extend( {
 		let user = {
 			username: `customer.${ now }`,
 			email: `customer.${ now }@example.com`,
+			billing: {
+				country: 'GB',
+			},
 		};
 		await api.post( 'customers', user ).then( ( response ) => {
 			user = response.data;
@@ -68,11 +71,14 @@ async function userDeletionTest( page, username ) {
 		await expect( page.locator( '#the-list tr' ) ).toHaveCount( 1 );
 		await expect( page.getByText( 'No users found' ) ).toBeVisible();
 	} );
+
+	return true;
 }
 
-test.only( `can edit a customer`, async ( { page, customer } ) => {
+test( `can update customer data`, async ( { page, customer } ) => {
 	await page.goto( `wp-admin/user-edit.php?user_id=${ customer.id }` );
 	await expect( page ).toHaveTitle( /Edit User/ );
+	await expect( page.locator( '#role' ) ).toHaveValue( 'customer' );
 
 	const now = Date.now();
 	const updatedCustomer = {
@@ -133,11 +139,33 @@ test.only( `can edit a customer`, async ( { page, customer } ) => {
 			.locator( '#billing_postcode' )
 			.fill( updatedCustomer.billing.postcode );
 		await page
-			.locator( '#billing_country' )
-			.selectText( updatedCustomer.billing.country );
+			.getByRole( 'combobox' )
+			.filter( {
+				has: page.locator( '#select2-billing_country-container' ),
+			} )
+			.locator( '.select2-selection__arrow' )
+			.click();
 		await page
-			.locator( '#billing_state' )
-			.selectText( updatedCustomer.billing.state );
+			.getByRole( 'option', {
+				name: updatedCustomer.billing.country,
+				exact: true,
+			} )
+			.click();
+
+		await page
+			.getByRole( 'combobox' )
+			.filter( {
+				has: page.locator( '#select2-billing_state-container' ),
+			} )
+			.locator( '.select2-selection__arrow' )
+			.click();
+		await page
+			.getByRole( 'option', {
+				name: updatedCustomer.billing.state,
+				exact: true,
+			} )
+			.click();
+
 		await page
 			.locator( '#billing_phone' )
 			.fill( updatedCustomer.billing.phone );
@@ -155,22 +183,140 @@ test.only( `can edit a customer`, async ( { page, customer } ) => {
 	} );
 
 	await test.step( 'verify the updates', async () => {
-		await page.getByText( 'User updated' ).isVisible();
+		await expect( page.getByText( 'User updated' ) ).toBeVisible();
+
+		// Check the user data
+		await expect
+			.soft( page.getByLabel( 'First Name', { exact: true } ) )
+			.toHaveValue( updatedCustomer.first_name );
+		await expect
+			.soft( page.getByLabel( 'Last Name', { exact: true } ) )
+			.toHaveValue( updatedCustomer.last_name );
+		await expect
+			.soft( page.getByLabel( 'Nickname (required)', { exact: true } ) )
+			.toHaveValue( updatedCustomer.nick_name );
+		await expect
+			.soft( page.getByLabel( 'Email (required)', { exact: true } ) )
+			.toHaveValue( updatedCustomer.email );
+
+		// Check the billing address
+		await expect
+			.soft( page.locator( '#billing_first_name' ) )
+			.toHaveValue( updatedCustomer.billing.first_name );
+		await expect
+			.soft( page.locator( '#billing_last_name' ) )
+			.toHaveValue( updatedCustomer.billing.last_name );
+		await expect
+			.soft( page.locator( '#billing_company' ) )
+			.toHaveValue( updatedCustomer.billing.company );
+		await expect
+			.soft( page.locator( '#billing_address_1' ) )
+			.toHaveValue( updatedCustomer.billing.address_1 );
+		await expect
+			.soft( page.locator( '#billing_address_2' ) )
+			.toHaveValue( updatedCustomer.billing.address_2 );
+		await expect
+			.soft( page.locator( '#billing_city' ) )
+			.toHaveValue( updatedCustomer.billing.city );
+		await expect
+			.soft( page.locator( '#billing_postcode' ) )
+			.toHaveValue( updatedCustomer.billing.postcode );
+		await expect
+			.soft(
+				page.locator( '#select2-billing_country-container', {
+					exact: true,
+				} )
+			)
+			.toHaveText( updatedCustomer.billing.country );
+		await expect
+			.soft(
+				page.locator( '#select2-billing_state-container', {
+					exact: true,
+				} )
+			)
+			.toHaveText( updatedCustomer.billing.state );
+		await expect
+			.soft( page.locator( '#billing_phone' ) )
+			.toHaveValue( updatedCustomer.billing.phone );
+		await expect
+			.soft( page.locator( '#billing_email' ) )
+			.toHaveValue( updatedCustomer.billing.email );
+
+		// Check the shipping address
+		await expect
+			.soft( page.locator( '#shipping_first_name' ) )
+			.toHaveValue( updatedCustomer.billing.first_name );
+		await expect
+			.soft( page.locator( '#shipping_last_name' ) )
+			.toHaveValue( updatedCustomer.billing.last_name );
+		await expect
+			.soft( page.locator( '#shipping_company' ) )
+			.toHaveValue( updatedCustomer.billing.company );
+		await expect
+			.soft( page.locator( '#shipping_address_1' ) )
+			.toHaveValue( updatedCustomer.billing.address_1 );
+		await expect
+			.soft( page.locator( '#shipping_address_2' ) )
+			.toHaveValue( updatedCustomer.billing.address_2 );
+		await expect
+			.soft( page.locator( '#shipping_city' ) )
+			.toHaveValue( updatedCustomer.billing.city );
+		await expect
+			.soft( page.locator( '#shipping_postcode' ) )
+			.toHaveValue( updatedCustomer.billing.postcode );
+		await expect
+			.soft(
+				page.locator( '#select2-shipping_country-container', {
+					exact: true,
+				} )
+			)
+			.toHaveText( updatedCustomer.billing.country );
+		await expect
+			.soft(
+				page.locator( '#select2-shipping_state-container', {
+					exact: true,
+				} )
+			)
+			.toHaveText( updatedCustomer.billing.state );
+		await expect
+			.soft( page.locator( '#shipping_phone' ) )
+			.toHaveValue( updatedCustomer.billing.phone );
 	} );
 } );
 
-test( `can edit a shop manager`, async ( { page, manager } ) => {
+test( `can update shop manager data`, async ( { page, manager } ) => {
 	await page.goto( `wp-admin/user-edit.php?user_id=${ manager.id }` );
+	await expect( page ).toHaveTitle( /Edit User/ );
+	await expect( page.locator( '#role' ) ).toHaveValue( 'shop_manager' );
+
+	const now = Date.now();
+	const updatedUser = {
+		first_name: `First ${ now }`,
+		last_name: `Last ${ now }`,
+		nick_name: `nickname${ now }`,
+		email: `customer.${ now }@example.com`,
+	};
 
 	await test.step( 'update user data', async () => {
-		await expect( page ).toHaveTitle( /Edit User/ );
+		await page
+			.getByLabel( 'First Name', { exact: true } )
+			.fill( updatedUser.first_name );
+		await page
+			.getByLabel( 'Last Name', { exact: true } )
+			.fill( updatedUser.last_name );
+		await page
+			.getByLabel( 'Nickname (required)', { exact: true } )
+			.fill( updatedUser.nick_name );
+		await page
+			.getByLabel( 'Email (required)', { exact: true } )
+			.fill( updatedUser.email );
 	} );
 } );
 
 test( `can delete a customer`, async ( { page, customer } ) => {
-	await userDeletionTest( page, customer.username );
+	expect( await userDeletionTest( page, customer.username ) ).toBeTruthy();
 } );
 
 test( `can delete a shop manager`, async ( { page, manager } ) => {
-	await userDeletionTest( page, manager.username );
+	expect( await userDeletionTest( page, manager.username ) ).toBeTruthy();
 } );
