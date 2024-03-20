e0fc4fd8fe010258e2656d298546262e414b17a0
Alex Florisca <alex.florisca@automattic.com>
2024-03-20 16:30:10 +0000

    Tidy up design for desktop and add separator back in for blocks on checkout order summary

--- plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-order-summary-block/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart/inner-blocks/cart-order-summary-block/edit.tsx
@@ -26,20 +26,12 @@ export const Edit = ( { clientId }: { clientId: string } ): JSX.Element => {
 		innerBlockAreas.CART_ORDER_SUMMARY
 	);
 	const defaultTemplate = [
-		[
-			'woocommerce/cart-order-summary-heading-block',
-			{
-				content: __( 'Cart totals', 'woocommerce' ),
-			},
-			[],
-		],
+		[ 'woocommerce/cart-order-summary-coupon-form-block', {}, [] ],
 		[ 'woocommerce/cart-order-summary-subtotal-block', {}, [] ],
 		[ 'woocommerce/cart-order-summary-fee-block', {}, [] ],
 		[ 'woocommerce/cart-order-summary-discount-block', {}, [] ],
 		[ 'woocommerce/cart-order-summary-shipping-block', {}, [] ],
-		[ 'woocommerce/cart-order-summary-coupon-form-block', {}, [] ],
 		[ 'woocommerce/cart-order-summary-taxes-block', {}, [] ],
-		[ 'woocommerce/cart-order-summary-coupon-form-block', {}, [] ],
 	] as TemplateArray;
 
 	useForcedLayout( {
