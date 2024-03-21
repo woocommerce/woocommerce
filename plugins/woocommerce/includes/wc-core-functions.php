7f5ac962198693e8a85087356758b833bb38df4b
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2024-03-21 12:29:42 +0530

    Add prefix + correct version.

--- plugins/woocommerce/includes/wc-core-functions.php
+++ plugins/woocommerce/includes/wc-core-functions.php
@@ -1899,11 +1899,11 @@ function wc_get_rounding_precision() {
 	 * Filter the rounding precision for internal WC calculations. This is different from the number of decimals used for display.
 	 * Generally, this filter can be used to decrease the precision, but if you choose to decrease, there maybe side effects such as off by one rounding errors for certain tax rate combinations.
 	 *
-	 * @since 8.9.0
+	 * @since 8.8.0
 	 *
 	 * @param int $precision The number of decimals to round to.
 	 */
-	return apply_filters( 'wc_get_rounding_precision', $precision );
+	return apply_filters( 'woocommerce_wc_get_rounding_precision', $precision );
 }
 
 /**
