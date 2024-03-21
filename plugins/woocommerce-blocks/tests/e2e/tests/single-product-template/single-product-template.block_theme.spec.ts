5aac82b0fe79398e272ef8a2ab771adf992e1d96
Roy Ho <roykho77@gmail.com>
2024-03-21 07:44:30 -0700

    Consolidate and move password protected product test (#45752)
    * Consolidate and move password protected product test
    
    * Add changefile(s) from automation for the following project(s): woocommerce-blocks
    
    * Move test to non side effect and remove remaining empty test file
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce-blocks/tests/e2e/tests/single-product-template/single-product-template.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/single-product-template/single-product-template.block_theme.spec.ts
@@ -54,3 +54,20 @@ for ( const { classes, product, frontendPage } of products ) {
 			} );
 		} ) );
 }
+
+test( 'shows password form in products protected with password', async ( {
+	page,
+} ) => {
+	// Sunglasses are defined as requiring password in /bin/scripts/products.sh.
+	await page.goto( '/product/sunglasses/' );
+	await expect(
+		page.getByText( 'This content is password protected.' ).first()
+	).toBeVisible();
+
+	// Verify after introducing the password, the page is visible.
+	await page.getByLabel( 'Password:' ).fill( 'password' );
+	await page.getByRole( 'button', { name: 'Enter' } ).click();
+	await expect(
+		page.getByRole( 'link', { name: 'Description' } )
+	).toBeVisible();
+} );
