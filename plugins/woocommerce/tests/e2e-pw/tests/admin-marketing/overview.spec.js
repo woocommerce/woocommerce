b25833bca9c349c3c733d272399a969fd76bdcdd
Jon Lane <jon.lane@automattic.com>
2024-03-21 14:21:57 -0700

    Improve tests for the marketing overview page

--- plugins/woocommerce/tests/e2e-pw/tests/admin-marketing/overview.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/admin-marketing/overview.spec.js
@@ -11,7 +11,89 @@ test.describe( 'Marketing page', () => {
 
 		// Users should see the "Learn about marketing a store" card.
 		await expect(
-			page.locator( '"Learn about marketing a store"' )
+			page.getByText( 'Learn about marketing a store' )
 		).toBeVisible();
+	} );
+
+	test( 'Marketing Overview page have relevant content', async ( {
+		page,
+	} ) => {
+		// Go to the Marketing page.
+		await page.goto( 'wp-admin/admin.php?page=wc-admin&path=%2Fmarketing' );
+
+		// Heading should be overview
+		await expect(
+			page.getByRole( 'heading', { name: 'Overview' } )
+		).toBeVisible();
+
+		// Call to action is visible
+		await expect(
+			page.getByText( 'Reach new customers and' )
+		).toBeVisible();
+
+		// Sections present
+		await expect(
+			page.getByText( 'ChannelsStart by adding a' )
+		).toBeVisible();
+		await expect(
+			page.getByText( 'Discover more marketing tools' )
+		).toBeVisible();
+		await expect(
+			page.getByText( 'Learn about marketing a store' )
+		).toBeVisible();
+	} );
+
+	test( 'Introduction can be dismissed', async ( { page } ) => {
+		// Go to the Marketing page.
+		await page.goto( 'wp-admin/admin.php?page=wc-admin&path=%2Fmarketing' );
+
+		// Dismiss the introduction (if it's visible)
+		try {
+			await page
+				.locator(
+					'.woocommerce-marketing-introduction-banner-illustration > .components-button'
+				)
+				.click( { timeout: 2000 } );
+		} catch ( e ) {
+			console.log( 'Info: introduction already hidden' );
+		}
+
+		// The introduction should be hidden.
+		await expect(
+			page.getByText(
+				'Reach new customers and increase sales without leaving WooCommerce'
+			)
+		).not.toBeVisible();
+
+		// Refresh the page to make sure the state is saved.
+		await page.reload();
+
+		// The introduction should still be hidden.
+		await expect(
+			page.getByText(
+				'Reach new customers and increase sales without leaving WooCommerce'
+			)
+		).not.toBeVisible();
+	} );
+
+	test( 'Learning section can be expanded', async ( { page } ) => {
+		// Go to the Marketing page.
+		await page.goto( 'wp-admin/admin.php?page=wc-admin&path=%2Fmarketing' );
+
+		// Expand the learning section
+		await page.getByLabel( 'Expand' ).click( { timeout: 2000 } );
+
+		// The learning section should be expanded.
+		await expect( page.getByText( 'Page 1 of 4' ) ).toBeVisible();
+
+		// Can navigate to next page
+		await page.getByLabel( 'Next page' ).click();
+		await expect( page.getByText( 'Page 2 of 4' ) ).toBeVisible();
+
+		// Collapse the learning section
+		await page.getByLabel( 'Collapse' ).nth( 2 ).click( { timeout: 2000 } );
+
+		// The learning section should be collapsed.
+		await expect( page.getByText( 'Page 1 of 4' ) ).not.toBeVisible();
 	} );
 } );
