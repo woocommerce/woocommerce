7ece8345640fe88dafa7ee95b22d2b6f3f7e3d55
Thomas Roberts <thomas.roberts@automattic.com>
2024-03-22 17:45:16 +0000

    Add getFormattedState util

--- plugins/woocommerce-blocks/assets/js/blocks/checkout/utils.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/checkout/utils.ts
@@ -1,8 +1,13 @@
 /**
  * External dependencies
  */
-import { LOGIN_URL } from '@woocommerce/block-settings';
+import {
+	ALLOWED_STATES,
+	LOGIN_URL,
+} from '@woocommerce/block-settings';
 import { getSetting } from '@woocommerce/settings';
+import { isObject, isString } from '@woocommerce/types';
+import { decodeEntities } from '@wordpress/html-entities';
 
 export const LOGIN_TO_CHECKOUT_URL = `${ LOGIN_URL }?redirect_to=${ encodeURIComponent(
 	window.location.href
@@ -11,5 +16,15 @@ export const LOGIN_TO_CHECKOUT_URL = `${ LOGIN_URL }?redirect_to=${ encodeURICom
 export const isLoginRequired = ( customerId: number ): boolean => {
 	return ! customerId && ! getSetting( 'checkoutAllowsGuest', false );
 };
+
+export const getFormattedState = (
+	address: CartBillingAddress | CartShippingAddress
+): string => {
+	return isObject( ALLOWED_STATES[ address.country ] ) &&
+		isString( ALLOWED_STATES[ address.country ][ address.state ] )
+		? decodeEntities( ALLOWED_STATES[ address.country ][ address.state ] )
+		: address.state;
+};
+
 
 export const reloadPage = (): void => void window.location.reload( true );
