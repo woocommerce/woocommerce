bc4b29f13bac7950e58ab94c96f5956ecaa6f776
Wesley Rosa <wesley.rosa@automattic.com>
2024-03-26 15:06:48 -0300

    PHPCS fixes

--- plugins/woocommerce/templates/myaccount/payment-methods.php
+++ plugins/woocommerce/templates/myaccount/payment-methods.php
@@ -57,13 +57,13 @@ do_action( 'woocommerce_before_account_payment_methods', $has_methods ); ?>
 									/* translators: %s: a credit card brand. */
 									$brands_label .= ' (' . sprintf( esc_html__( '%s preferred', 'woocommerce' ), esc_html( wc_get_credit_card_type_label( $method['method']['preferred_network'] ) ) ) . ')';
 								} else {
-									$brands_label = esc_html( wc_get_credit_card_type_label( $method['method']['brand'] ) );
+									$brands_label = wc_get_credit_card_type_label( $method['method']['brand'] );
 								}
 								if ( ! empty( $method['method']['last4'] ) ) {
 									/* translators: 1: credit card type 2: last 4 digits */
 									printf( esc_html__( '%1$s ending in %2$s', 'woocommerce' ), $brands_label, esc_html( $method['method']['last4'] ) );
 								} else {
-									echo $brands_label;
+									echo esc_html( $brands_label );
 								}
 							} elseif ( 'expires' === $column_id ) {
 								echo esc_html( $method['expires'] );
