0019db41450547de93a525193d4bd89b29b7e86d
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-03-27 15:14:34 +0800

    Add lys hub site preview

--- plugins/woocommerce-admin/client/launch-your-store/hub/main-content/pages/site-preview.tsx
+++ plugins/woocommerce-admin/client/launch-your-store/hub/main-content/pages/site-preview.tsx
@@ -2,20 +2,39 @@
  * External dependencies
  */
 import classnames from 'classnames';
+import { useState, useRef, useEffect } from '@wordpress/element';
+import { Spinner } from '@woocommerce/components';
+
 /**
  * Internal dependencies
  */
+import { getAdminSetting } from '~/utils/admin-settings';
 import type { MainContentComponentProps } from '../xstate';
+import './site-preview.scss';
 
 export const SitePreviewPage = ( props: MainContentComponentProps ) => {
+	const siteUrl = getAdminSetting( 'siteUrl' ) + '?site-preview=1';
+	const [ isLoading, setIsLoading ] = useState( true );
+
 	return (
 		<div
 			className={ classnames(
 				'launch-store-site-preview-page__container',
+				{ 'is-loading': isLoading },
 				props.className
 			) }
 		>
-			<p>Main Content - Site Preview</p>
+			{ isLoading && (
+				<div className="launch-store-site-preview-site_spinner">
+					<Spinner />
+				</div>
+			) }
+			<iframe
+				className="launch-store-site__preview-site-iframe"
+				src={ siteUrl }
+				title="Preview"
+				onLoad={ () => setIsLoading( false ) }
+			/>
 		</div>
 	);
 };
