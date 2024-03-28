0eb41fd50c0ec356806c62879298f7e8f373c544
M. L. Giannotta <sunyatasattva@gmail.com>
2024-03-28 19:41:47 +0800

    Re-export APIs from `@wordpress/interactivity`

--- plugins/woocommerce-blocks/assets/js/interactivity/index.js
+++ plugins/woocommerce-blocks/assets/js/interactivity/index.js
@@ -1,19 +1,32 @@
 // @ts-nocheck
 
 /**
+ * External dependencies
+ */
+
+import { privateApis } from '@wordpress/interactivity/build';
+
+/**
  * Internal dependencies
  */
-import registerDirectives from './directives';
 import { init } from './router';
 
-export { store } from './store';
-export { directive, getContext, getElement } from './hooks';
+const consent =
+	'I acknowledge that using private APIs means my theme or plugin will inevitably break in the next version of WordPress.';
+
+const { directive, h: createElement, deepSignal } = privateApis( consent );
+
+export {
+	getContext,
+	getElement,
+	store,
+	useEffect,
+	useMemo,
+} from '@wordpress/interactivity';
+export { createElement, deepSignal, directive };
 export { navigate, prefetch } from './router';
-export { h as createElement } from 'preact';
-export { useEffect, useContext, useMemo } from 'preact/hooks';
-export { deepSignal } from 'deepsignal';
+export { useContext } from 'preact/hooks';
 
 document.addEventListener( 'DOMContentLoaded', async () => {
-	registerDirectives();
 	await init();
 } );
