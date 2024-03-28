c1422892b9534ce57db4c8f79fba447f68837938
Thomas Roberts <thomas.roberts@automattic.com>
2024-03-28 21:09:36 +0000

    Add shipping method before testing validate_selected_shipping_methods

--- plugins/woocommerce/tests/php/src/Blocks/StoreApi/Utilities/OrderController.php
+++ plugins/woocommerce/tests/php/src/Blocks/StoreApi/Utilities/OrderController.php
@@ -28,12 +28,20 @@ class OrderControllerTests extends TestCase {
 	 * test_validate_selected_shipping_methods.
 	 */
 	public function test_validate_selected_shipping_methods() {
+		// Add a flat rate to the default zone.
+		$flat_rate    = WC()->shipping()->get_shipping_methods()['flat_rate'];
+		$default_zone = \WC_Shipping_Zones::get_zone( 0 );
+		$default_zone->add_shipping_method( $flat_rate->id );
+		$default_zone->save();
+
 		$class = new OrderController();
 
+		$registered_methods = \WC_Shipping_Zones::get_zone( 0 )->get_shipping_methods();
+		$valid_method       = array_shift( $registered_methods );
+
 		// By running this method we assert that it doesn't error because if it does this test will fail.
-		$class->validate_selected_shipping_methods( true, array( 'free-shipping' ) );
+		$class->validate_selected_shipping_methods( true, array( $valid_method->id . ':' . $valid_method->instance_id ) );
 		$class->validate_selected_shipping_methods( false, array( 'free-shipping' ) );
-		$class->validate_selected_shipping_methods( true, null );
 		// The above methods throw Exception on error, but this is classed as a risky test because there are no
 		// assertions. Assert true to work around this warning.
 		$this->assertTrue( true );
