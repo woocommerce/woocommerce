8ff4b0d83f13ecfd05041ec8feeaa3971fd860dd
Thomas Roberts <thomas.roberts@automattic.com>
2024-03-29 15:33:56 +0000

    Update MockSessionHandler to handle caching

--- plugins/woocommerce/tests/php/src/Blocks/StoreApi/MockSessionHandler.php
+++ plugins/woocommerce/tests/php/src/Blocks/StoreApi/MockSessionHandler.php
@@ -3,29 +3,137 @@
 namespace Automattic\WooCommerce\Tests\Blocks\StoreApi;
 
 use WC_Session_Handler;
+use Automattic\Jetpack\Constants;
 
 defined( 'ABSPATH' ) || exit;
 
 /**
- * MockSessionHandler class for PHPUnit tests.
+ * Mock Session handler class. Required because PHPUnit doesn't allow cookies to be set in the response as it runs tests
+ * in a single context.
  */
-final class MockSessionHandler extends WC_Session_Handler {
+class MockSessionHandler extends WC_Session_Handler {
 
 	/**
-	 * Setup cookie and customer ID.
+	 * @var array A mock cache replacement for wp_cache.
+	 */
+	private $mock_cache;
+
+	/**
+	 * Sets the session cookie on-demand - see parent method. Instead of using wc_setcookie, we set the cookie directly
+	 * due to PHPUnit running tests in a single context, cookies cannot be sent in a response.
 	 *
 	 * @param bool $set Should the session cookie be set.
 	 */
-	public function init_session_cookie( $set = true ) {
-		$to_hash           = $this->_customer_id . '|' . $this->_session_expiration;
-		$cookie_hash       = hash_hmac( 'md5', $to_hash, wp_hash( $to_hash ) );
-		$cookie_value      = $this->_customer_id . '||' . $this->_session_expiration . '||' . $this->_session_expiring . '||' . $cookie_hash;
-		$this->_has_cookie = true;
+	public function set_customer_session_cookie( $set ) {
+		if ( $set ) {
+			$to_hash           = $this->_customer_id . '|' . $this->_session_expiration;
+			$cookie_hash       = hash_hmac( 'md5', $to_hash, wp_hash( $to_hash ) );
+			$cookie_value      = $this->_customer_id . '||' . $this->_session_expiration . '||' . $this->_session_expiring . '||' . $cookie_hash;
+			$this->_has_cookie = true;
 
-		if ( ! isset( $_COOKIE[ $this->_cookie ] ) || $_COOKIE[ $this->_cookie ] !== $cookie_value ) {
-
-			// Manually set the cookie here because the response is already sent, so wc_setcookie() (as in the parent class) won't work.
-			$_COOKIE[ $this->_cookie ] = $cookie_value;
+			if ( ! isset( $_COOKIE[ $this->_cookie ] ) || $_COOKIE[ $this->_cookie ] !== $cookie_value ) {
+				$_COOKIE[ $this->_cookie ] = $cookie_value;
+			}
 		}
+	}
+
+	/**
+	 * Save data and delete guest session.
+	 *
+	 * @param int $old_session_key session ID before user logs in.
+	 */
+	public function save_data( $old_session_key = 0 ) {
+		// Dirty if something changed - prevents saving nothing new.
+		if ( $this->_dirty && $this->has_session() ) {
+			global $wpdb;
+
+			$wpdb->query(
+				$wpdb->prepare(
+				// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+					"INSERT INTO $this->_table (`session_key`, `session_value`, `session_expiry`) VALUES (%s, %s, %d)
+ 					ON DUPLICATE KEY UPDATE `session_value` = VALUES(`session_value`), `session_expiry` = VALUES(`session_expiry`)",
+					$this->_customer_id,
+					maybe_serialize( $this->_data ),
+					$this->_session_expiration
+				)
+			);
+
+			$this->mock_cache[ $this->_customer_id ] = $this->_data;
+			$this->_dirty = false;
+			//  Ignoring the below because this is how it is in the parent class.
+			// phpcs:ignore Universal.Operators.StrictComparisons.LooseNotEqual
+			if ( get_current_user_id() != $old_session_key && ! is_object( get_user_by( 'id', $old_session_key ) ) ) {
+				$this->delete_session( $old_session_key );
+			}
+		}
+	}
+
+	/**
+	 * Forget all session data without destroying it.
+	 * Overriden in this mock because the parent method sets a cookie using wc_setcookie which doesn't work with PHPUnit.
+	 */
+	public function forget_session() {
+		unset( $_COOKIE[ $this->_cookie ] );
+
+		if ( ! is_admin() ) {
+			include_once WC_ABSPATH . 'includes/wc-cart-functions.php';
+
+			wc_empty_cart();
+		}
+
+		$this->_data        = array();
+		$this->_dirty       = false;
+		$this->_customer_id = $this->generate_customer_id();
+	}
+
+	/**
+	 * Returns the session.
+	 *
+	 * @param string $customer_id Customer ID.
+	 * @param mixed  $default Default session value.
+	 * @return string|array
+	 */
+	public function get_session( $customer_id, $default = false ) { // phpcs:ignore Universal.NamingConventions.NoReservedKeywordParameterNames.defaultFound
+		global $wpdb;
+
+		if ( Constants::is_defined( 'WP_SETUP_CONFIG' ) ) {
+			return false;
+		}
+
+		// Try to get it from the cache, it will return false if not present or if object cache not in use.
+		$value = $this->mock_cache[ $customer_id ];
+
+		if ( false === $value ) {
+			$value = $wpdb->get_var( $wpdb->prepare( "SELECT session_value FROM $this->_table WHERE session_key = %s", $customer_id ) ); // @codingStandardsIgnoreLine.
+
+			if ( is_null( $value ) ) {
+				$value = $default;
+			}
+
+			$cache_duration = $this->_session_expiration - time();
+			if ( 0 < $cache_duration ) {
+				$this->mock_cache[ $customer_id ] = $value;
+			}
+		}
+
+		return maybe_unserialize( $value );
+	}
+
+	/**
+	 * Delete the session from the cache and database.
+	 *
+	 * @param int $customer_id Customer ID.
+	 */
+	public function delete_session( $customer_id ) {
+		global $wpdb;
+
+		unset( $this->mock_cache[ $customer_id ] );
+
+		$wpdb->delete(
+			$this->_table,
+			array(
+				'session_key' => $customer_id,
+			)
+		);
 	}
 }
