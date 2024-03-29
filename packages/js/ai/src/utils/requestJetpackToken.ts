511c76319ca11cea56cbaa8bf93d079ca970c625
Thomas Shellberg <6723003+tommyshellberg@users.noreply.github.com>
2024-03-29 13:18:16 +0100

    Convert custom Jetpack JWT util to use RequestJWT from jetpack-ai-client library.

--- packages/js/ai/src/utils/requestJetpackToken.ts
+++ packages/js/ai/src/utils/requestJetpackToken.ts
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import debugFactory from 'debug';
-import apiFetch from '@wordpress/api-fetch';
+import { requestJwt } from '@automattic/jetpack-ai-client';
 
 /**
  * Internal dependencies
@@ -31,57 +31,12 @@ declare global {
  */
 
 export async function requestJetpackToken() {
-	const token = localStorage.getItem( JWT_TOKEN_ID );
-	let tokenData;
-
-	if ( token ) {
-		try {
-			tokenData = JSON.parse( token );
-		} catch ( err ) {
-			debugToken( 'Error parsing token', err );
-			throw createExtendedError(
-				'Error parsing cached token',
-				'token_parse_error'
-			);
-		}
-	}
-
-	if ( tokenData && tokenData?.expire > Date.now() ) {
-		debugToken( 'Using cached token' );
-		return tokenData;
-	}
-
-	const apiNonce = window.JP_CONNECTION_INITIAL_STATE?.apiNonce;
-	const siteSuffix = window.JP_CONNECTION_INITIAL_STATE?.siteSuffix;
-
-	try {
-		const data: { token: string; blog_id: string } = await apiFetch( {
-			path: '/jetpack/v4/jetpack-ai-jwt?_cacheBuster=' + Date.now(),
-			credentials: 'same-origin',
-			headers: {
-				'X-WP-Nonce': apiNonce,
-			},
-			method: 'POST',
-		} );
-
-		const newTokenData = {
-			token: data.token,
-			blogId: siteSuffix,
-
-			/**
-			 * Let's expire the token in 2 minutes
-			 */
-			expire: Date.now() + JWT_TOKEN_EXPIRATION_TIME,
-		};
-
-		debugToken( 'Storing new token' );
-		localStorage.setItem( JWT_TOKEN_ID, JSON.stringify( newTokenData ) );
-
-		return newTokenData;
-	} catch ( e ) {
+	const { token, blogId } = await requestJwt();
+	if ( ! token ) {
 		throw createExtendedError(
 			'Error fetching new token',
 			'token_fetch_error'
 		);
 	}
+	return { token, blogId };
 }
