03d1dfb0355c18617f11a2f534e4598211b4f1fb
Thomas Roberts <thomas.roberts@automattic.com>
2024-04-01 15:12:31 +0100

    Add spacing

--- plugins/woocommerce/src/Blocks/BlockTypes/Checkout.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/Checkout.php
@@ -278,6 +278,7 @@ class Checkout extends AbstractBlock {
 
 		$post_blocks = parse_blocks( $post->post_content );
 		$title       = $this->find_local_pickup_text_in_checkout_block( $post_blocks );
+
 		if ( $title ) {
 			$pickup_location_settings['title'] = $title;
 			update_option( 'woocommerce_pickup_location_settings', $pickup_location_settings );
