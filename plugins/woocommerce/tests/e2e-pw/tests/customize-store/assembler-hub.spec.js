c0d0596574e131fea94bc3f3be039b4d0d62774d
Luigi Teschio <gigitux@gmail.com>
2024-04-02 10:35:15 +0200

    CYS - E2E tests: fix flaky assembler-hub test (#46030)
    * CYS - E2E tests: fix flaky assembler-hub test
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * fix flakiness
    
    * add waitUntil
    
    * improve logic
    
    * fix build error
    
    * try now
    
    * fix unit test
    
    * fix flakiness
    
    * fix logic
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/tests/e2e-pw/tests/customize-store/assembler-hub.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/customize-store/assembler-hub.spec.js
@@ -1,5 +1,5 @@
-const { test, expect, request } = require( '@playwright/test' );
-const { BASE_URL } = process.env;
+const { test: base, expect, request } = require( '@playwright/test' );
+const { AssemblerPage } = require( './assembler/assembler.page' );
 const { activateTheme } = require( '../../utils/themes' );
 const { setOption } = require( '../../utils/options' );
 
@@ -8,21 +8,12 @@ const ASSEMBLER_HUB_URL =
 const CUSTOMIZE_STORE_URL =
 	'/wp-admin/admin.php?page=wc-admin&path=%2Fcustomize-store';
 
-const skipTestIfUndefined = () => {
-	const skipMessage = `Skipping this test on daily run. Environment not compatible.`;
-
-	test.skip( () => {
-		const shouldSkip = BASE_URL !== undefined;
-
-		if ( shouldSkip ) {
-			console.log( skipMessage );
-		}
-
-		return shouldSkip;
-	}, skipMessage );
-};
-
-skipTestIfUndefined();
+const test = base.extend( {
+	assemblerPageObject: async ( { page }, use ) => {
+		const pageObject = new AssemblerPage( { page } );
+		await use( pageObject );
+	},
+} );
 
 test.describe( 'Store owner can view Assembler Hub for store customization', () => {
 	test.use( { storageState: process.env.ADMINSTATE } );
@@ -79,17 +70,18 @@ test.describe( 'Store owner can view Assembler Hub for store customization', () 
 
 	test( 'Can view the Assembler Hub page when the theme is already customized', async ( {
 		page,
+		assemblerPageObject,
 	} ) => {
 		await page.goto( CUSTOMIZE_STORE_URL );
 		await page.click( 'text=Start designing' );
 		await page
 			.getByRole( 'button', { name: 'Design a new theme' } )
 			.click();
-
-		await page.waitForURL( ASSEMBLER_HUB_URL );
-
-		await page.goto( ASSEMBLER_HUB_URL );
-		await expect( page.locator( "text=Let's get creative" ) ).toBeVisible();
+		await assemblerPageObject.waitForLoadingScreenFinish();
+		const assembler = await assemblerPageObject.getAssembler();
+		await expect(
+			assembler.locator( "text=Let's get creative" )
+		).toBeVisible();
 	} );
 
 	test( 'Visiting change header should show a list of block patterns to choose from', async ( {
