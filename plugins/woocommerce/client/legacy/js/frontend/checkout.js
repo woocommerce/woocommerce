77586f537604280e9c47ead78df53b5898cb60d2
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-04-03 18:27:53 +0100

    Disambiguated error log

--- plugins/woocommerce/client/legacy/js/frontend/checkout.js
+++ plugins/woocommerce/client/legacy/js/frontend/checkout.js
@@ -501,13 +501,13 @@ jQuery( function( $ ) {
 							var maybe_valid_json = raw_response.match( /{"result.*}/ );
 
 							if ( null === maybe_valid_json ) {
-								console.log( 'Unable to fix malformed JSON' );
+								console.log( 'Unable to fix malformed JSON #1' );
 							} else if ( wc_checkout_form.is_valid_json( maybe_valid_json[0] ) ) {
 								console.log( 'Fixed malformed JSON. Original:' );
 								console.log( raw_response );
 								raw_response = maybe_valid_json[0];
 							} else {
-								console.log( 'Unable to fix malformed JSON' );
+								console.log( 'Unable to fix malformed JSON #2' );
 							}
 						}
 
