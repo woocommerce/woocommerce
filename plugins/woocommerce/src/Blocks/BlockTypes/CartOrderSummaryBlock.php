27f0e48e84c138a9735a4ee7027c02474868fa98
Alex Florisca <alex.florisca@automattic.com>
2024-04-03 14:38:15 +0100

    Migration to handle older order summary without totals block

--- plugins/woocommerce/src/Blocks/BlockTypes/CartOrderSummaryBlock.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/CartOrderSummaryBlock.php
@@ -14,42 +14,44 @@ class CartOrderSummaryBlock extends AbstractInnerBlock {
 	protected $block_name = 'cart-order-summary-block';
 
 	private function get_inner_block_content($block_name, $content) {
-		$inner_block_regex = '/<!-- wp:woocommerce\/' . $block_name . '(.+?)-->(.+?)<!-- \/wp:woocommerce\/' . $block_name . ' -->/si';
-		if (preg_match($inner_block_regex, $content, $matches)) {
+		if ( preg_match( $this->inner_block_regex( $block_name ), $content, $matches) ) {
 			return $matches[0];
 		}
 		return false;
 	}
 
+	private function inner_block_regex($block_name) {
+		return '/<div data-block-name="woocommerce\/cart-order-summary-' . $block_name . '-block"(.+?)>(.*?)<\/div>/si';
+	}
+
 	protected function render( $attributes, $content, $block ) {
 		// The order-summary-totals block was introduced as a new parent block for the totals
 		// (subtotal, discount, fees, shipping and taxes) blocks.
-		$regex_for_cart_order_summary_totals = '/<!-- wp:woocommerce\/cart-order-summary-totals-block(.+?)-->/';
-		$order_summary_totals_content = '<!-- wp:woocommerce/cart-order-summary-totals-block -->';
+		$regex_for_cart_order_summary_totals = '/<div data-block-name="woocommerce\/cart-order-summary-totals-block"(.+?)>/';
+		$order_summary_totals_content = '<div data-block-name="woocommerce/cart-order-summary-totals-block" class="wp-block-woocommerce-cart-order-summary-totals-block">';
 
 		// We want to move these blocks inside a parent 'totals' block
-		$totals_inner_blocks = array('subtotal', 'discount', 'fees', 'shipping', 'taxes');
+		$totals_inner_blocks = array('subtotal', 'discount', 'fee', 'shipping', 'taxes');
 
-		if ( ! preg_match( $regex_for_cart_order_summary_totals, $content ) ) {		
+		if ( ! preg_match( $regex_for_cart_order_summary_totals, $content ) ) {
+			foreach ($totals_inner_blocks as $key => $block_name) {
+				$inner_block_content = $this->get_inner_block_content( $block_name, $content );
 
-			foreach ($totals_inner_blocks as $key => $inner_block_name) {
-				$inner_block_content = $this->get_inner_block_content($inner_block_name, $content);
-				if ( !$inner_block_content ) {
-					$order_summary_totals_content .= $inner_block_content;
+				if ( $inner_block_content ) {
+					$order_summary_totals_content .= "\n" . $inner_block_content;
 					
 					// The last block is replaced with the totals block.
-					if( $key === count($totals_inner_blocks) - 1 ) {
-						$order_summary_totals_content .= '<!-- /wp:woocommerce/cart-order-summary-totals-block -->';
-						$content = preg_replace('/<!-- wp:woocommerce\/cart-order-summary-' . $inner_block_name . '-block(.+?)-->(.+?)<!-- \/wp:woocommerce\/cart-order-summary-' . $inner_block_name . '-block -->/si', $order_summary_totals_content, $content);
+					if( $key === count( $totals_inner_blocks ) - 1 ) {
+						$order_summary_totals_content .= '</div>';
+						$content = preg_replace( $this->inner_block_regex( $block_name ), $order_summary_totals_content, $content);
 					}
 					// Otherwise, remove the block
 					else {
-						$content = preg_replace('/<!-- wp:woocommerce\/cart-order-summary-' . $inner_block_name . '-block(.+?)-->(.+?)<!-- \/wp:woocommerce\/cart-order-summary-' . $inner_block_name . '-block -->/si', '', $content);
+						$content = preg_replace( $this->inner_block_regex( $block_name ), '', $content );
 					}
 				}
 			}
 		}
-		
 		return $content;
 	}
 }
