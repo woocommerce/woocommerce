081ecc8686fe42e2690941108c6280cccc919a57
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-04-03 14:31:01 +0300

    Update wordpress-post.spec.js

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/wordpress-post.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/wordpress-post.spec.js
@@ -1,17 +1,20 @@
 const { test, expect } = require( '@playwright/test' );
 
-test.describe( 'WordPress', async () => {
-	test.use( { storageState: process.env.CUSTOMERSTATE } );
+test.use( { storageState: process.env.CUSTOMERSTATE } );
 
-	test( 'logged-in customer can comment on a post', async ( { page } ) => {
-		await page.goto( 'hello-world/' );
-		await expect(
-			page.getByRole( 'heading', { name: 'Hello world!', exact: true } )
-		).toBeVisible();
+test( 'logged-in customer can comment on a post', async ( { page } ) => {
+	await page.goto( 'hello-world/' );
+	await expect(
+		page.getByRole( 'heading', { name: 'Hello world!', exact: true } )
+	).toBeVisible();
 
-		const comment = `This is a test comment ${ Date.now() }`;
-		await page.getByRole( 'textbox', { name: 'comment' } ).fill( comment );
-		await page.getByRole( 'button', { name: 'Post Comment' } ).click();
-		await expect( page.getByText( comment ) ).toBeVisible();
-	} );
+	const comment = `This is a test comment ${ Date.now() }`;
+	await page.getByRole( 'textbox', { name: 'comment' } ).fill( comment );
+
+	await expect(
+		page.getByRole( 'textbox', { name: 'comment' } )
+	).toHaveValue( comment );
+
+	await page.getByRole( 'button', { name: 'Post Comment' } ).click();
+	await expect( page.getByText( comment ) ).toBeVisible();
 } );
