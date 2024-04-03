5ca72d195f9aa2484b03e9c7dbff6ce81e4d8f68
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-04-03 16:10:16 +0300

    Update wordpress-post.spec.js

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/wordpress-post.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/wordpress-post.spec.js
@@ -1,12 +1,16 @@
-const { test, expect } = require( '@playwright/test' );
+const { test: baseTest, expect } = require( '../../fixtures/fixtures' );
 
-test.use( { storageState: process.env.CUSTOMERSTATE } );
+const test = baseTest.extend( {
+	storageState: process.env.CUSTOMERSTATE,
+} );
 
 test( 'logged-in customer can comment on a post', async ( { page } ) => {
 	await page.goto( 'hello-world/' );
 	await expect(
 		page.getByRole( 'heading', { name: 'Hello world!', exact: true } )
 	).toBeVisible();
+
+	await expect( page.getByText( `Logged in as` ) ).toBeVisible();
 
 	const comment = `This is a test comment ${ Date.now() }`;
 	await page.getByRole( 'textbox', { name: 'comment' } ).fill( comment );
