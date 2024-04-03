62f6da98c9dff42b704dcbd97afef94854ad7561
Ilyas Foo <foo.ilyas@gmail.com>
2024-04-03 18:13:24 +0800

    Temporarily disable deprecation logging and use old DataSourcePoller class

--- plugins/woocommerce/src/Admin/DataSourcePoller.php
+++ plugins/woocommerce/src/Admin/DataSourcePoller.php
@@ -16,6 +16,8 @@ abstract class DataSourcePoller extends RemoteSpecsDataSourcePoller {
 	 * Log a deprecation to the error log.
 	 */
 	private static function log_deprecation() {
+		// Temporarily disable deprecation message in logs since due to upgrade issues https://github.com/woocommerce/woocommerce/pull/45892.
+		return;
 		error_log( // phpcs:ignore
 			sprintf(
 				'%1$s is deprecated since version %2$s! Use %3$s instead.',
