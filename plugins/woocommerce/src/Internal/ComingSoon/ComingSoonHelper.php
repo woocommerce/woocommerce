7c8ae78f8e6beb1b2c8c5b655a3423f57b15f4f8
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2024-04-04 20:10:28 +0800

    Add coming soon helpers

---
+++ plugins/woocommerce/src/Internal/ComingSoon/ComingSoonHelper.php
@@ -0,0 +1,55 @@
+<?php
+namespace Automattic\WooCommerce\Internal\ComingSoon;
+
+use Automattic\WooCommerce\Admin\WCAdminHelper;
+
+/**
+ * Provides helper methods for coming soon functionality.
+ */
+class ComingSoonHelper {
+
+	/**
+	 * Returns true when the entire site is live.
+	 */
+	public function is_site_live(): bool {
+		return 'yes' !== get_option( 'woocommerce_coming_soon' );
+	}
+
+	/**
+	 * Returns true when the entire site is coming soon mode.
+	 */
+	public function is_site_coming_soon(): bool {
+		return 'yes' === get_option( 'woocommerce_coming_soon' ) && 'no' === get_option( 'woocommerce_store_pages_only' );
+	}
+
+	/**
+	 * Returns true when only the store pages are in coming soon mode.
+	 */
+	public function is_store_coming_soon(): bool {
+		return 'yes' === get_option( 'woocommerce_coming_soon' ) && 'yes' === get_option( 'woocommerce_store_pages_only' );
+	}
+
+	/**
+	 * Returns true when the provided URL is behind a coming soon screen.
+	 *
+	 * @param string $url The URL to check.
+	 */
+	public function is_url_coming_soon( string $url ): bool {
+		// Early exit if coming soon mode not active.
+		if ( $this->is_site_live() ) {
+			return false;
+		}
+
+		if ( $this->is_site_coming_soon() ) {
+			return true;
+		}
+
+		// Check the URL is a store page when in "store coming soon" mode.
+		if ( $this->is_store_coming_soon() && WCAdminHelper::is_store_page( $url ) ) {
+			return true;
+		}
+
+		// Default to false.
+		return false;
+	}
+}
