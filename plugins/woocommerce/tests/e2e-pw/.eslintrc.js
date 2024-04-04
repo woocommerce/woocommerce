76e45d9f047d68913e3b733ef1c9c21303908c5b
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-04-04 13:37:43 +0300

    Add a warning for test

--- plugins/woocommerce/tests/e2e-pw/.eslintrc.js
+++ plugins/woocommerce/tests/e2e-pw/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	extends: [ 'plugin:playwright/recommended' ],
 	rules: {
 		'playwright/no-skipped-test': 'off',
-		'no-console': 'off',
+		'no-console': 'warn',
 		'jest/no-test-callback': 'off',
 		'jest/no-disabled-tests': 'off',
 		'jest/valid-expect': 'off',
