4b4498f1bdd6821f0fb9340fd73baf869980b7fa
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-04-04 10:23:57 +0300

    Use disableWelcomeModal

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/checkout-block.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/checkout-block.spec.js
@@ -8,6 +8,7 @@ const {
 	fillBillingCheckoutBlocks,
 } = require( '../../utils/checkout' );
 const { getOrderIdFromUrl } = require( '../../utils/order' );
+const { disableWelcomeModal } = require( '../../utils/editor' );
 
 const guestEmail = 'checkout-guest@example.com';
 const newAccountEmail = 'marge-test-account@example.com';
@@ -212,19 +213,11 @@ test.describe( 'Checkout Block page', () => {
 	test( 'can see empty checkout block page', async ( { page } ) => {
 		// create a new page with checkout block
 		await page.goto( 'wp-admin/post-new.php?post_type=page' );
-		await page.waitForLoadState( 'networkidle' );
 		await page.locator( 'input[name="log"]' ).fill( admin.username );
 		await page.locator( 'input[name="pwd"]' ).fill( admin.password );
 		await page.locator( 'text=Log In' ).click();
 
-		// Close welcome popup if prompted
-		try {
-			await page
-				.getByLabel( 'Close', { exact: true } )
-				.click( { timeout: 5000 } );
-		} catch ( error ) {
-			console.log( "Welcome modal wasn't present, skipping action." );
-		}
+		await disableWelcomeModal( { page } );
 
 		await page
 			.getByRole( 'textbox', { name: 'Add title' } )
