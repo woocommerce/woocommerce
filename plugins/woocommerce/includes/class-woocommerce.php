779b5c4a9a11d3e9460233b93ebf5ace9653a7b3
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2024-04-05 20:37:41 +0800

    Add cache invalidator

--- plugins/woocommerce/includes/class-woocommerce.php
+++ plugins/woocommerce/includes/class-woocommerce.php
@@ -10,6 +10,7 @@ defined( 'ABSPATH' ) || exit;
 
 use Automattic\WooCommerce\Internal\AssignDefaultCategory;
 use Automattic\WooCommerce\Internal\BatchProcessing\BatchProcessingController;
+use Automattic\WooCommerce\Internal\ComingSoon\ComingSoonCacheInvalidator;
 use Automattic\WooCommerce\Internal\ComingSoon\ComingSoonRequestHandler;
 use Automattic\WooCommerce\Internal\DataStores\Orders\CustomOrdersTableController;
 use Automattic\WooCommerce\Internal\DownloadPermissionsAdjuster;
@@ -277,6 +278,7 @@ final class WooCommerce {
 		$container->get( WebhookUtil::class );
 		$container->get( Marketplace::class );
 		$container->get( TimeUtil::class );
+		$container->get( ComingSoonCacheInvalidator::class );
 		$container->get( ComingSoonRequestHandler::class );
 
 		/**
--- plugins/woocommerce/src/Internal/DependencyManagement/ServiceProviders/ComingSoonServiceProvider.php
+++ plugins/woocommerce/src/Internal/DependencyManagement/ServiceProviders/ComingSoonServiceProvider.php
@@ -3,17 +3,20 @@
 namespace Automattic\WooCommerce\Internal\DependencyManagement\ServiceProviders;
 
 use Automattic\WooCommerce\Internal\DependencyManagement\AbstractServiceProvider;
+use Automattic\WooCommerce\Internal\ComingSoon\ComingSoonCacheInvalidator;
 use Automattic\WooCommerce\Internal\ComingSoon\ComingSoonRequestHandler;
 use Automattic\WooCommerce\Internal\ComingSoon\ComingSoonHelper;
 
 class ComingSoonServiceProvider extends AbstractServiceProvider {
 
-    protected $provides = array(
-        ComingSoonRequestHandler::class,
+	protected $provides = array(
+		ComingSoonCacheInvalidator::class,
 		ComingSoonHelper::class,
-    );
+		ComingSoonRequestHandler::class,
+	);
 
 	public function register() {
+		$this->add( ComingSoonCacheInvalidator::class );
 		$this->add( ComingSoonHelper::class );
 		$this->add( ComingSoonRequestHandler::class )->addArgument( ComingSoonHelper::class );
 	}
