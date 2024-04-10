ddf27e313c4caed177d0646f2d984fe23d5e1fbc
paul sealock <psealock@gmail.com>
2024-04-10 12:51:51 +1200

    Align wide

--- plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
+++ plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
@@ -153,8 +153,8 @@ class LaunchYourStore {
 			<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
 			<!-- /wp:spacer -->
 
-			<!-- wp:heading {"textAlign":"center","level":1} -->
-			<h1 class="wp-block-heading has-text-align-center">%s</h1>
+			<!-- wp:heading {"textAlign":"center","level":1,"align":"wide"} -->
+			<h1 class="wp-block-heading alignwide has-text-align-center">%s</h1>
 			<!-- /wp:heading -->
 
 			<!-- wp:spacer -->
