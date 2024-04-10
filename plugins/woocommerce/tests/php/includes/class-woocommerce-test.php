b900469af53ccbf9c796cd19a21191be3ecff1cd
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-04-10 15:58:12 +0800

    Fix tests

--- plugins/woocommerce/tests/php/includes/class-woocommerce-test.php
+++ plugins/woocommerce/tests/php/includes/class-woocommerce-test.php
@@ -88,7 +88,7 @@ class WooCommerce_Test extends \WC_Unit_Test_Case {
 
 		$this->assertEquals( 'yes', get_option( 'woocommerce_coming_soon' ) );
 		$this->assertEquals( 'no', get_option( 'woocommerce_store_pages_only' ) );
-		$this->assertEquals( 'no', get_option( 'woocommerce_private_link' ) );
+		$this->assertEquals( 'yes', get_option( 'woocommerce_private_link' ) );
 		$this->assertEquals( 'test', get_option( 'woocommerce_share_key' ) );
 	}
 
