c6cc23088dc7b1b850074d0116b37255611d9c44
Lourens Schep <lourensschep@gmail.com>
2024-04-11 16:43:02 +0200

    Fix some tests

--- plugins/woocommerce/tests/legacy/unit-tests/woocommerce-admin/api/reports-taxes.php
+++ plugins/woocommerce/tests/legacy/unit-tests/woocommerce-admin/api/reports-taxes.php
@@ -26,6 +26,7 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 	public function setUp(): void {
 		parent::setUp();
 
+		update_option( 'woocommerce_calc_taxes', 'yes' );
 		$this->user = $this->factory->user->create(
 			array(
 				'role' => 'administrator',
@@ -66,7 +67,7 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 				'tax_rate_id'       => 1,
 				'tax_rate'          => '7',
 				'tax_rate_country'  => 'US',
-				'tax_rate_state'    => 'GA',
+				'tax_rate_state'    => 'NY',
 				'tax_rate_name'     => 'TestTax',
 				'tax_rate_priority' => 1,
 				'tax_rate_order'    => 1,
@@ -75,13 +76,14 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 
 		$tax_item = new WC_Order_Item_Tax();
 		$tax_item->set_rate( $tax_rate_id );
+		$tax_item->set_tax_total( 5 );
+		$tax_item->set_shipping_tax_total( 2 );
 
 		$order = WC_Helper_Order::create_order( 1, $product );
 		$order->add_item( $tax_item );
 		$order->set_status( 'completed' );
 		$order->set_total( 100 ); // $25 x 4.
 		$order->save();
-		$order->calculate_totals( true );
 
 		// @todo Remove this once order data is synced to wc_order_tax_lookup
 		$wpdb->insert(
@@ -102,9 +104,19 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 		$reports  = $response->get_data();
 
 		$this->assertEquals( 200, $response->get_status() );
-		$this->assertEquals( 0, count( $reports ) );
+		$this->assertEquals( 1, count( $reports ) );
 
 		$tax_report = reset( $reports );
+
+		$this->assertEquals( 1, $tax_report['tax_rate_id'] );
+		// $this->assertEquals( 'TestTax', $tax_report['name'] );
+		$this->assertEquals( 7, $tax_report['tax_rate'] );
+		$this->assertEquals( 'US', $tax_report['country'] );
+		$this->assertEquals( 'NY', $tax_report['state'] );
+		$this->assertEquals( 7, $tax_report['total_tax'] );
+		$this->assertEquals( 5, $tax_report['order_tax'] );
+		$this->assertEquals( 2, $tax_report['shipping_tax'] );
+		$this->assertEquals( 1, $tax_report['orders_count'] );
 	}
 
 	/**
@@ -123,13 +135,13 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 		$product->set_regular_price( 25 );
 		$product->save();
 
-		$tax_rate_id = $wpdb->insert(
+		$wpdb->insert(
 			$wpdb->prefix . 'woocommerce_tax_rates',
 			array(
 				'tax_rate_id'       => 1,
 				'tax_rate'          => '7',
 				'tax_rate_country'  => 'US',
-				'tax_rate_state'    => 'GA',
+				'tax_rate_state'    => 'NY',
 				'tax_rate_name'     => 'TestTax',
 				'tax_rate_priority' => 1,
 				'tax_rate_order'    => 1,
@@ -149,10 +161,30 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 			)
 		);
 
+		$tax_item = new WC_Order_Item_Tax();
+		$tax_item->set_rate( 1 );
+		$tax_item->set_tax_total( 5 );
+		$tax_item->set_shipping_tax_total( 2 );
+
 		$order = WC_Helper_Order::create_order( 1, $product );
+		$order->add_item( $tax_item );
 		$order->set_status( 'completed' );
 		$order->set_total( 100 ); // $25 x 4.
 		$order->save();
+
+		$tax_item_ca = new WC_Order_Item_Tax();
+		$tax_item_ca->set_rate( 2 );
+		$tax_item_ca->set_tax_total( 15 );
+		$tax_item_ca->set_shipping_tax_total( 0 );
+
+		$order_ca = WC_Helper_Order::create_order( 1, $product );
+		$order_ca->set_shipping_state( 'ON' );
+		$order_ca->set_shipping_country( 'CA' );
+		$order_ca->add_item( $tax_item_ca );
+		$order_ca->set_status( 'completed' );
+		$order_ca->set_total( 100 ); // $25 x 4.
+		$order_ca->save();
+		$order_ca->calculate_totals( true );
 
 		// @todo Remove this once order data is synced to wc_order_tax_lookup
 		$wpdb->insert(
@@ -166,31 +198,15 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 				'total_tax'    => 7,
 			)
 		);
-
-		$order_item_id = $wpdb->insert(
-			$wpdb->prefix . 'woocommerce_order_items',
-			array(
-				'order_item_name' => 'US-GA-TestTax-1',
-				'order_item_type' => 'tax',
-				'order_id'        => $order->get_id(),
-			)
-		);
-
 		$wpdb->insert(
-			$wpdb->prefix . 'woocommerce_order_itemmeta',
+			$wpdb->prefix . 'wc_order_tax_lookup',
 			array(
-				'order_item_id' => $order_item_id,
-				'meta_key'      => 'rate_id',
-				'meta_value'    => "$tax_rate_id",
-			)
-		);
-
-		$wpdb->insert(
-			$wpdb->prefix . 'woocommerce_order_itemmeta',
-			array(
-				'order_item_id' => $order_item_id,
-				'meta_key'      => 'rate_percent',
-				'meta_value'    => '7',
+				'order_id'     => $order_ca->get_id(),
+				'tax_rate_id'  => 2,
+				'date_created' => gmdate( 'Y-m-d H:i:s' ),
+				'shipping_tax' => 2,
+				'order_tax'    => 5,
+				'total_tax'    => 7,
 			)
 		);
 
@@ -207,9 +223,31 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 		$reports  = $response->get_data();
 
 		$this->assertEquals( 200, $response->get_status() );
-		$this->assertEquals( 0, count( $reports ) );
+		$this->assertEquals( 2, count( $reports ) );
 
 		$tax_report = reset( $reports );
+
+		$this->assertEquals( 2, $tax_report['tax_rate_id'] );
+		// $this->assertEquals( 'TestTax 2', $tax_report['name'] );
+		$this->assertEquals( 8.0, $tax_report['tax_rate'] );
+		$this->assertEquals( 'CA', $tax_report['country'] );
+		$this->assertEquals( 'ON', $tax_report['state'] );
+		$this->assertEquals( 8.8, $tax_report['total_tax'] );
+		$this->assertEquals( 8, $tax_report['order_tax'] );
+		$this->assertEquals( 0.8, $tax_report['shipping_tax'] );
+		$this->assertEquals( 1, $tax_report['orders_count'] );
+
+		$tax_report = next( $reports );
+
+		$this->assertEquals( 1, $tax_report['tax_rate_id'] );
+		// $this->assertEquals( 'TestTax', $tax_report['name'] );
+		$this->assertEquals( 7, $tax_report['tax_rate'] );
+		$this->assertEquals( 'US', $tax_report['country'] );
+		$this->assertEquals( 'NY', $tax_report['state'] );
+		$this->assertEquals( 7, $tax_report['total_tax'] );
+		$this->assertEquals( 5, $tax_report['order_tax'] );
+		$this->assertEquals( 2, $tax_report['shipping_tax'] );
+		$this->assertEquals( 1, $tax_report['orders_count'] );
 	}
 
 	/**
@@ -222,12 +260,7 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 		wp_set_current_user( $this->user );
 		WC_Helper_Reports::reset_stats_dbs();
 
-		$product = new WC_Product_Simple();
-		$product->set_name( 'Test Product' );
-		$product->set_regular_price( 25 );
-		$product->save();
-
-		$tax_rate_id = $wpdb->insert(
+		$wpdb->insert(
 			$wpdb->prefix . 'woocommerce_tax_rates',
 			array(
 				'tax_rate_id'       => 1,
@@ -244,57 +277,12 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 			$wpdb->prefix . 'woocommerce_tax_rates',
 			array(
 				'tax_rate_id'       => 2,
-				'tax_rate'          => '8',
+				'tax_rate'          => '10',
 				'tax_rate_country'  => 'CA',
 				'tax_rate_state'    => 'ON',
 				'tax_rate_name'     => 'TestTax 2',
 				'tax_rate_priority' => 1,
 				'tax_rate_order'    => 1,
-			)
-		);
-
-		$order = WC_Helper_Order::create_order( 1, $product );
-		$order->set_status( 'completed' );
-		$order->set_total( 100 ); // $25 x 4.
-		$order->save();
-
-		// @todo Remove this once order data is synced to wc_order_tax_lookup
-		$wpdb->insert(
-			$wpdb->prefix . 'wc_order_tax_lookup',
-			array(
-				'order_id'     => $order->get_id(),
-				'tax_rate_id'  => 1,
-				'date_created' => gmdate( 'Y-m-d H:i:s' ),
-				'shipping_tax' => 2,
-				'order_tax'    => 5,
-				'total_tax'    => 7,
-			)
-		);
-
-		$order_item_id = $wpdb->insert(
-			$wpdb->prefix . 'woocommerce_order_items',
-			array(
-				'order_item_name' => 'US-GA-TestTax-1',
-				'order_item_type' => 'tax',
-				'order_id'        => $order->get_id(),
-			)
-		);
-
-		$wpdb->insert(
-			$wpdb->prefix . 'woocommerce_order_itemmeta',
-			array(
-				'order_item_id' => $order_item_id,
-				'meta_key'      => 'rate_id',
-				'meta_value'    => $tax_rate_id,
-			)
-		);
-
-		$wpdb->insert(
-			$wpdb->prefix . 'woocommerce_order_itemmeta',
-			array(
-				'order_item_id' => $order_item_id,
-				'meta_key'      => 'rate_percent',
-				'meta_value'    => '7',
 			)
 		);
 
@@ -310,7 +298,13 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 		$reports  = $response->get_data();
 
 		$this->assertEquals( 200, $response->get_status() );
-		$this->assertEquals( 0, count( $reports ) );
+		$this->assertEquals( 2, count( $reports ) );
+
+		$this->assertEquals( 1, $reports[0]['tax_rate_id'] );
+		$this->assertEquals( 7, $reports[0]['tax_rate'] );
+
+		$this->assertEquals( 2, $reports[1]['tax_rate_id'] );
+		$this->assertEquals( 10, $reports[1]['tax_rate'] );
 	}
 
 	/**
@@ -323,12 +317,7 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 		wp_set_current_user( $this->user );
 		WC_Helper_Reports::reset_stats_dbs();
 
-		$product = new WC_Product_Simple();
-		$product->set_name( 'Test Product' );
-		$product->set_regular_price( 25 );
-		$product->save();
-
-		$tax_rate_id = $wpdb->insert(
+		$wpdb->insert(
 			$wpdb->prefix . 'woocommerce_tax_rates',
 			array(
 				'tax_rate_id'       => 1,
@@ -345,57 +334,12 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 			$wpdb->prefix . 'woocommerce_tax_rates',
 			array(
 				'tax_rate_id'       => 2,
-				'tax_rate'          => '8',
+				'tax_rate'          => '10',
 				'tax_rate_country'  => 'CA',
 				'tax_rate_state'    => 'ON',
 				'tax_rate_name'     => 'TestTax 2',
 				'tax_rate_priority' => 1,
 				'tax_rate_order'    => 1,
-			)
-		);
-
-		$order = WC_Helper_Order::create_order( 1, $product );
-		$order->set_status( 'completed' );
-		$order->set_total( 100 ); // $25 x 4.
-		$order->save();
-
-		// @todo Remove this once order data is synced to wc_order_tax_lookup
-		$wpdb->insert(
-			$wpdb->prefix . 'wc_order_tax_lookup',
-			array(
-				'order_id'     => $order->get_id(),
-				'tax_rate_id'  => 1,
-				'date_created' => gmdate( 'Y-m-d H:i:s' ),
-				'shipping_tax' => 2,
-				'order_tax'    => 5,
-				'total_tax'    => 7,
-			)
-		);
-
-		$order_item_id = $wpdb->insert(
-			$wpdb->prefix . 'woocommerce_order_items',
-			array(
-				'order_item_name' => 'US-GA-TestTax-1',
-				'order_item_type' => 'tax',
-				'order_id'        => $order->get_id(),
-			)
-		);
-
-		$wpdb->insert(
-			$wpdb->prefix . 'woocommerce_order_itemmeta',
-			array(
-				'order_item_id' => $order_item_id,
-				'meta_key'      => 'rate_id',
-				'meta_value'    => $tax_rate_id,
-			)
-		);
-
-		$wpdb->insert(
-			$wpdb->prefix . 'woocommerce_order_itemmeta',
-			array(
-				'order_item_id' => $order_item_id,
-				'meta_key'      => 'rate_percent',
-				'meta_value'    => '7',
 			)
 		);
 
@@ -411,6 +355,11 @@ class WC_Admin_Tests_API_Reports_Taxes extends WC_REST_Unit_Test_Case {
 		$reports  = $response->get_data();
 
 		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( 2, count( $reports ) );
+
+		$this->assertEquals( 2, $reports[0]['tax_rate_id'] );
+
+		$this->assertEquals( 1, $reports[1]['tax_rate_id'] );
 	}
 
 	/**
