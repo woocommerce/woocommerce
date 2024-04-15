df2fc0fd2cf3043723f1b5080c9bf6e2daf5900e
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-04-15 15:09:13 +1200

    Update conditional inclusion of header and footer templates in ComingSoonRequestHandler.php

--- plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
+++ plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
@@ -70,7 +70,16 @@ class ComingSoonRequestHandler {
 		add_theme_support( 'block-template-parts' );
 
 		$template = get_query_template( 'coming-soon' );
+
+		if ( ! wc_current_theme_is_fse_theme() && $this->coming_soon_helper->is_store_coming_soon() ) {
+			get_header();
+		}
+
 		include $template;
+
+		if ( ! wc_current_theme_is_fse_theme() && $this->coming_soon_helper->is_store_coming_soon() ) {
+			get_footer();
+		}
 
 		die();
 	}
