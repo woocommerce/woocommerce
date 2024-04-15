b52d2ad41fe7834817ec18e0dfc17d6f777eb919
Luigi Teschio <gigitux@gmail.com>
2024-04-15 18:12:59 +0200

    improve E2E tests

--- plugins/woocommerce-blocks/tests/e2e/tests/stock-filter/stock-filter.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/stock-filter/stock-filter.block_theme.spec.ts
@@ -1,17 +1,49 @@
 /**
  * External dependencies
  */
-import { test, expect } from '@woocommerce/e2e-playwright-utils';
+import { test as base, expect } from '@woocommerce/e2e-playwright-utils';
+import { cli } from '@woocommerce/e2e-utils';
 
-const blockData = {
+/**
+ * Internal dependencies
+ */
+import ProductCollectionPage from '../product-collection/product-collection.page';
+
+export const blockData = {
 	name: 'Filter by Stock',
 	slug: 'woocommerce/stock-filter',
+	urlSearchParamWhenFilterIsApplied: '?filter_stock_status=outofstock',
 };
 
+const test = base.extend< {
+	productCollectionPageObject: ProductCollectionPage;
+} >( {
+	productCollectionPageObject: async (
+		{ page, admin, editor, templateApiUtils, editorUtils },
+		use
+	) => {
+		const pageObject = new ProductCollectionPage( {
+			page,
+			admin,
+			editor,
+			templateApiUtils,
+			editorUtils,
+		} );
+		await use( pageObject );
+	},
+} );
+
 test.describe( `${ blockData.name } Block`, () => {
-	test.beforeEach( async ( { admin, editor, editorUtils } ) => {
+	test.beforeEach( async ( { admin, editor } ) => {
 		await admin.createNewPost();
-		await editorUtils.insertBlockUsingGlobalInserter( 'Filter by Stock' );
+		await editor.insertBlock( {
+			name: 'woocommerce/filter-wrapper',
+			attributes: {
+				filterType: 'stock-filter',
+				heading: 'Filter By Price',
+			},
+		} );
+
 		await editor.openDocumentSettingsSidebar();
 	} );
 
@@ -24,8 +56,6 @@ test.describe( `${ blockData.name } Block`, () => {
 		await page.fill( textSelector, title );
 
 		await expect( page.locator( textSelector ) ).toHaveText( title );
-
-		expect( true ).toBe( true );
 	} );
 
 	test( 'should allow changing the display style', async ( {
@@ -88,5 +118,171 @@ test.describe( `${ blockData.name } Block`, () => {
 				name: 'Apply',
 			} )
 		).toBeVisible();
+	} );
+} );
+
+test.describe( `${ blockData.name } Block - with PHP classic template`, () => {
+	test.beforeEach( async ( { admin, page, editor } ) => {
+		await cli(
+			'npm run wp-env run tests-cli -- wp option update wc_blocks_use_blockified_product_grid_block_as_template false'
+		);
+
+		await admin.visitSiteEditor( {
+			postId: 'woocommerce/woocommerce//archive-product',
+			postType: 'wp_template',
+		} );
+
+		await editor.canvas.locator( 'body' ).click();
+
+		await editor.insertBlock( {
+			name: 'woocommerce/filter-wrapper',
+			attributes: {
+				filterType: 'stock-filter',
+				heading: 'Filter By Price',
+			},
+		} );
+		await editor.saveSiteEditorEntities();
+		await page.goto( `/shop` );
+	} );
+
+	test( 'should show all products', async ( { frontendUtils } ) => {
+		const legacyTemplate = await frontendUtils.getBlockByName(
+			'woocommerce/legacy-template'
+		);
+
+		const stockFilter = await frontendUtils.getBlockByName(
+			'woocommerce/filter-wrapper'
+		);
+
+		const products = legacyTemplate
+			.getByRole( 'list' )
+			.locator( '.product' );
+
+		await expect( products ).toHaveCount( 16 );
+
+		await expect( stockFilter.getByText( 'In Stock' ) ).toBeVisible();
+		await expect( stockFilter.getByText( 'Out of Stock' ) ).toBeVisible();
+	} );
+
+	test( 'should show only products that match the filter', async ( {
+		frontendUtils,
+	} ) => {
+		const stockFilter = await frontendUtils.getBlockByName(
+			blockData.slug
+		);
+
+		await stockFilter.getByText( 'Out of Stock' ).click();
+
+		const legacyTemplate = await frontendUtils.getBlockByName(
+			'woocommerce/legacy-template'
+		);
+
+		const products = legacyTemplate
+			.getByRole( 'list' )
+			.locator( '.product' );
+
+		await expect( products ).toHaveCount( 1 );
+	} );
+} );
+
+test.describe( `${ blockData.name } Block - with Product Collection`, () => {
+	test.beforeEach(
+		async ( {
+			admin,
+			editorUtils,
+			productCollectionPageObject,
+			editor,
+		} ) => {
+			await admin.createNewPost();
+			await productCollectionPageObject.insertProductCollection();
+			await productCollectionPageObject.chooseCollectionInPost(
+				'productCatalog'
+			);
+			await editor.insertBlock( {
+				name: 'woocommerce/filter-wrapper',
+				attributes: {
+					filterType: 'stock-filter',
+					heading: 'Filter By Stock',
+				},
+			} );
+			await editorUtils.publishAndVisitPost();
+		}
+	);
+
+	test( 'should show all products', async ( { page } ) => {
+		const products = page
+			.locator( '.wp-block-woocommerce-product-template' )
+			.getByRole( 'listitem' );
+
+		await expect( products ).toHaveCount( 9 );
+	} );
+
+	test( 'should show only products that match the filter', async ( {
+		page,
+	} ) => {
+		await page.getByText( 'Out of Stock' ).click();
+
+		await page.waitForURL( ( url ) =>
+			url
+				.toString()
+				.includes( blockData.urlSearchParamWhenFilterIsApplied )
+		);
+
+		const products = page
+			.locator( '.wp-block-woocommerce-product-template' )
+			.getByRole( 'listitem' );
+
+		await expect( products ).toHaveCount( 1 );
+	} );
+
+	test( 'should refresh the page only if the user click on button', async ( {
+		page,
+		admin,
+		editor,
+		editorUtils,
+		productCollectionPageObject,
+	} ) => {
+		await admin.createNewPost();
+		await productCollectionPageObject.insertProductCollection();
+		await productCollectionPageObject.chooseCollectionInPost(
+			'productCatalog'
+		);
+		await editor.insertBlock( {
+			name: 'woocommerce/filter-wrapper',
+			attributes: {
+				filterType: 'stock-filter',
+				heading: 'Filter By Price',
+			},
+		} );
+		const stockFilterControls = await editorUtils.getBlockByName(
+			blockData.slug
+		);
+		await editor.selectBlocks( stockFilterControls );
+		await editor.openDocumentSettingsSidebar();
+		await page.getByText( "Show 'Apply filters' button" ).click();
+		await editorUtils.publishAndVisitPost();
+
+		await page.addInitScript( () => {
+			document.addEventListener( 'DOMContentLoaded', () => {
+				// eslint-disable-next-line dot-notation
+				window[ '__DOMContentLoaded__' ] = true;
+			} );
+		} );
+
+		await page.getByText( 'Out of Stock' ).click();
+		await page.getByRole( 'button', { name: 'Apply' } ).click();
+
+		await page.waitForEvent( 'domcontentloaded' );
+
+		const domContentLoaded = await page.evaluate(
+			// eslint-disable-next-line dot-notation
+			() => window[ '__DOMContentLoaded__' ] === true
+		);
+
+		await expect( page.url() ).toContain(
+			blockData.urlSearchParamWhenFilterIsApplied
+		);
+
+		expect( domContentLoaded ).toBe( true );
 	} );
 } );
