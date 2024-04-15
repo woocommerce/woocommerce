2988a1c3c69607039ee2ba4756fa9d16b1855126
Ilyas Foo <foo.ilyas@gmail.com>
2024-04-15 15:34:33 +1200

    Change coming soon template to use template_include instead

--- plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
+++ plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
@@ -25,41 +25,46 @@ class ComingSoonRequestHandler {
 	 */
 	final public function init( ComingSoonHelper $coming_soon_helper ) {
 		$this->coming_soon_helper = $coming_soon_helper;
-		add_action( 'parse_request', array( $this, 'handle_parse_request' ) );
+		add_filter( 'template_include', array( $this, 'handle_template_include' ) );
 	}
 
 	/**
-	 * Parses the current request and sets the page ID to the coming soon page if it
-	 * needs to be shown in place of the normal page.
+	 * Replaces the template with a coming soon screen if needed.
 	 *
 	 * @internal
 	 *
-	 * @param \WP $wp Current WordPress environment instance (passed by reference).
+	 * @param string $template Current template.
 	 */
-	public function handle_parse_request( \WP &$wp ) {
+	public function handle_template_include( $template ) {
 		// Early exit if LYS feature is disabled.
 		if ( ! Features::is_enabled( 'launch-your-store' ) ) {
-			return $wp;
+			return $template;
+		}
+
+		if ( is_404() ) {
+			return $template;
 		}
 
 		// Early exit if the user is logged in as administrator / shop manager.
 		if ( current_user_can( 'manage_woocommerce' ) ) {
-			return $wp;
+			return $template;
 		}
 
 		// Early exit if the URL doesn't need a coming soon screen.
+		global $wp;
 		$url = $this->coming_soon_helper->get_url_from_wp( $wp );
 
 		if ( ! $this->coming_soon_helper->is_url_coming_soon( $url ) ) {
-			return $wp;
+			return $template;
 		}
 
-		// A coming soon page needs to be displayed. Don't cache this response.
-		nocache_headers();
+		$coming_soon_template = get_query_template( 'coming-soon' );
+		if ( $coming_soon_template ) {
+			// A coming soon page needs to be displayed. Don't cache this response.
+			nocache_headers();
+			return $coming_soon_template;
+		};
 
-		$template = get_query_template( 'coming-soon' );
-		include $template;
-
-		die();
+		return $template;
 	}
 }
