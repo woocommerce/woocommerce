eeb5f4e30348bcd6b468b9e9e7370a1325ca85b7
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-04-16 13:10:56 +1200

    Hide woo breadcrumb

--- plugins/woocommerce/patterns/coming-soon-store-only.php
+++ plugins/woocommerce/patterns/coming-soon-store-only.php
@@ -31,7 +31,6 @@ if ( wc_current_theme_is_fse_theme() ) {
 <!-- wp:paragraph {"align":"center"} -->
 <p class="has-text-align-center"><?php echo esc_html__( 'Something big is brewing! Our store is in the works - Launching shortly!', 'woocommerce' ); ?></p>
 <!-- /wp:paragraph -->
-
 <!-- wp:spacer -->
 <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer --></div>
