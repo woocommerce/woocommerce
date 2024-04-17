1ca48d0b5e5b39015b07621068245656c7b656fe
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-04-17 14:54:59 +0100

    Simplify commands

--- tools/compare-perf/config.js
+++ tools/compare-perf/config.js
@@ -1,11 +1,11 @@
 const config = {
 	gitRepositoryURL: 'https://github.com/woocommerce/woocommerce.git',
 	setupTestRunner:
-		'pnpm install --filter="@woocommerce/plugin-woocommerce" &> /dev/null && cd plugins/woocommerce && pnpm exec playwright install chromium',
+		'pnpm --filter="@woocommerce/plugin-woocommerce" exec playwright install chromium',
 	setupCommand: 'pnpm install &> /dev/null && pnpm build &> /dev/null',
 	pluginPath: '/plugins/woocommerce',
 	testsPath: '/plugins/woocommerce/tests/metrics/specs',
-	testCommand: 'cd plugins/woocommerce && pnpm test:metrics',
+	testCommand: 'pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics',
 };
 
 module.exports = config;
