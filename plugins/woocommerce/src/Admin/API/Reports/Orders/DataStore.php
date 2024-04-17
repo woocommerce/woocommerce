f3099a70fe6bee7a230b418807ca6444e130e867
Gan Eng Chin <ecgan@users.noreply.github.com>
2024-04-18 01:29:38 +0800

    Change `origin` string to `attribution` array in Orders DataStore.
    Co-authored-by: Kader Ibrahim S <kader.ibrahim.s@a8c.com>

--- plugins/woocommerce/src/Admin/API/Reports/Orders/DataStore.php
+++ plugins/woocommerce/src/Admin/API/Reports/Orders/DataStore.php
@@ -400,10 +400,10 @@ class DataStore extends ReportsDataStore implements DataStoreInterface {
 
 		foreach ( $orders_data as $key => $order_data ) {
 			$defaults                             = array(
-				'products' => array(),
-				'coupons'  => array(),
-				'customer' => array(),
-				'origin'   => '',
+				'products'    => array(),
+				'coupons'     => array(),
+				'customer'    => array(),
+				'attribution' => array(),
 			);
 			$orders_data[ $key ]['extended_info'] = isset( $mapped_data[ $order_data['order_id'] ] ) ? array_merge( $defaults, $mapped_data[ $order_data['order_id'] ] ) : $defaults;
 			if ( $order_data['customer_id'] && isset( $mapped_customers[ $order_data['customer_id'] ] ) ) {
