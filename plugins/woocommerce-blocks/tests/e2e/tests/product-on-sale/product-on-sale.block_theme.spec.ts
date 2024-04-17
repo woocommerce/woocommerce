55353a27a671456fe0bc179abe1a4b624834f586
Luigi Teschio <gigitux@gmail.com>
2024-04-17 10:55:50 +0200

    migrate jest-puppetter E2E tests to Playwright

---
+++ plugins/woocommerce-blocks/tests/e2e/tests/product-on-sale/product-on-sale.block_theme.spec.ts
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import { expect, test } from '@woocommerce/e2e-playwright-utils';
+
+const blockData = {
+	name: 'On Sale Products',
+	slug: 'woocommerce/product-on-sale',
+};
+
+test.describe( `${ blockData.slug } Block`, () => {
+	test( 'can be inserted in Post Editor and it is visible on the frontend', async ( {
+		editorUtils,
+		editor,
+		admin,
+		frontendUtils,
+	} ) => {
+		await admin.createNewPost();
+		await editor.insertBlock( { name: blockData.slug } );
+		const blockLocator = await editorUtils.getBlockByName( blockData.slug );
+		await expect( blockLocator.getByRole( 'listitem' ) ).toHaveCount( 9 );
+		await editorUtils.publishAndVisitPost();
+		const blockLocatorFrontend = await frontendUtils.getBlockByName(
+			blockData.slug
+		);
+		await expect(
+			blockLocatorFrontend.getByRole( 'listitem' )
+		).toHaveCount( 9 );
+	} );
+} );
---
+++ plugins/woocommerce-blocks/tests/e2e/tests/product-results-count/product-results-count.block_theme.spec.ts
@@ -0,0 +1,36 @@
+/**
+ * External dependencies
+ */
+import { expect, test } from '@woocommerce/e2e-playwright-utils';
+
+const blockData = {
+	name: 'Product Results Count',
+	slug: 'woocommerce/product-results-count',
+	class: '.wc-block-product-results-count',
+};
+
+test.describe( `${ blockData.slug } Block`, () => {
+	test( "block can't be inserted in Post Editor", async ( {
+		editorUtils,
+		admin,
+	} ) => {
+		await admin.createNewPost();
+		await expect(
+			editorUtils.insertBlockUsingGlobalInserter( blockData.name )
+		).rejects.toThrow();
+	} );
+
+	test( 'block can be inserted in Site Editor', async ( {
+		editorUtils,
+		admin,
+	} ) => {
+		await admin.visitSiteEditor( {
+			postId: 'woocommerce/woocommerce//archive-product',
+			postType: 'wp_template',
+		} );
+		await editorUtils.enterEditMode();
+		await expect(
+			editorUtils.insertBlockUsingGlobalInserter( blockData.name )
+		).resolves.not.toThrow();
+	} );
+} );
