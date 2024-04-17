87868097d2454c021f31f3ae636b11dc5577f198
Moon <moon.kyong@automattic.com>
2024-04-17 12:26:42 +1200

    Add dismiss button to coming soon banner (#46149)
    * Add coming_soon page as a store page
    
    * Implement coming soon banner dismiss
    
    * Added a new REST for Launch Your Store with a dismiss endpoint
    
    * Updated coming soon banner logic to check for woocommerce_coming_soon_banner_dismissed
    
    * Lint fixes
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Update plugins/woocommerce/src/Admin/API/LaunchYourStore.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Update plugins/woocommerce/src/Admin/API/LaunchYourStore.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Update plugins/woocommerce/src/Admin/API/LaunchYourStore.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Use user meta to save woocommerce_coming_soon_banner_dismissed
    
    * Lint fixes
    
    * Use the built-in rest api to manage woocommerce_coming_soon_banner_dismissed user meta
    
    * Reset woocommerce_coming_soon_banner_dismissed on login
    
    * Fix lint issues and add back wp_login hook to reset banner dismiss user meta
    
    * Use constant for user meta field
    
    * Update woocommerce_coming_soon_banner_dismissed user meta when X button is clicked
    
    * Restrict user meta to shop manager or admin
    
    * Fix lint issue
    
    * Fix lint issue
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>

--- plugins/woocommerce/client/legacy/js/frontend/woocommerce.js
+++ plugins/woocommerce/client/legacy/js/frontend/woocommerce.js
@@ -99,4 +99,24 @@ jQuery( function( $ ) {
 			}
 		}
 	);
+
+
+	$( 'a.coming-soon-footer-banner-dismiss' ).on( 'click', function( e ) {
+		var target = $( e.target );
+		$.ajax( {
+			type: 'post',
+			url: target.data( 'rest-url' ),
+			data: {
+				meta: {
+					'woocommerce_coming_soon_banner_dismissed': 'yes'
+				}
+			},
+			beforeSend: function ( xhr ) {
+				xhr.setRequestHeader( 'X-WP-Nonce',  target.data( 'rest-nonce' ) );
+			},
+			complete: function () {
+				$('#coming-soon-footer-banner').hide();
+			}
+		} );
+	} );
 });
