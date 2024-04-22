3c2bd95e6df4a79fcd91dff421776ac760b12bbf
Nadir Seghir <nadir.seghir@gmail.com>
2024-04-22 17:28:54 +0200

    expand test to also account for frontend

--- plugins/woocommerce-blocks/tests/e2e/tests/local-pickup/local-pickup.merchant.block_theme.side_effects.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/local-pickup/local-pickup.merchant.block_theme.side_effects.spec.ts
@@ -64,6 +64,13 @@ test.describe( 'Merchant → Local Pickup Settings', () => {
 		await fakeInput.pressSequentially( 'This is a test' );
 		await editor.canvas.getByText( 'This is a test' ).isVisible();
 		await editor.saveSiteEditorEntities();
+
+		// Make sure the result is visible in frontend
+		await frontendUtils.goToCheckout();
+
+		await expect( page.getByText( 'This is a test' ) ).toBeVisible();
+
+		// Now check if it's visible in the local pickup settings.
 		await localPickupUtils.openLocalPickupSettings();
 		await expect( page.getByLabel( 'Title' ) ).toHaveValue(
 			'This is a test'
