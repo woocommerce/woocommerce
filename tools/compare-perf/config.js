c35681377545d555ef72601ad45d401eea63f647
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-04-22 15:47:13 +0300

    Read and use engines.pnpm in config file

--- tools/compare-perf/config.js
+++ tools/compare-perf/config.js
@@ -1,13 +1,17 @@
 const packageJson = require( '../../package.json' );
-const pnpm_version = `@${ packageJson.engines.pnpm }` || '';
+let pnpm_package = 'pnpm';
+
+if ( packageJson.engines.pnpm ) {
+	pnpm_package = `pnpm@${ packageJson.engines.pnpm }`;
+}
 
 const config = {
 	gitRepositoryURL: 'https://github.com/woocommerce/woocommerce.git',
-	setupTestRunner: `npm install -g pnpm${ pnpm_version } && pnpm install --filter="@woocommerce/plugin-woocommerce" &> /dev/null && cd plugins/woocommerce && pnpm exec playwright install chromium`,
-	setupCommand: `npm install -g pnpm${ pnpm_version } && pnpm install &> /dev/null && pnpm build &> /dev/null`,
+	setupTestRunner: `npm install -g ${ pnpm_package } && pnpm install --filter="@woocommerce/plugin-woocommerce" &> /dev/null && cd plugins/woocommerce && pnpm exec playwright install chromium`,
+	setupCommand: `npm install -g ${ pnpm_package } && pnpm install &> /dev/null && pnpm build &> /dev/null`,
 	pluginPath: '/plugins/woocommerce',
 	testsPath: '/plugins/woocommerce/tests/metrics/specs',
-	testCommand: `npm install -g pnpm${ pnpm_version } && cd plugins/woocommerce && pnpm test:metrics`,
+	testCommand: `npm install -g ${ pnpm_package } && cd plugins/woocommerce && pnpm test:metrics`,
 };
 
 module.exports = config;
