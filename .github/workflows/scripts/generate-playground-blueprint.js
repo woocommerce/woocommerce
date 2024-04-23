be6b8f3ca01d6e5e04904b34f3d45f17e448fa9b
Sam Seay <samueljseay@gmail.com>
2024-04-23 15:45:56 +0800

    Clean up unused code in script, fix artifact upload path

--- .github/workflows/scripts/generate-playground-blueprint.js
+++ .github/workflows/scripts/generate-playground-blueprint.js
@@ -49,93 +49,42 @@ const generateWordpressPlaygroundBlueprint = ( runId, prNumber ) => {
 };
 
 async function run( { github, context, core } ) {
-	// Retrieve the PR branch name from the GitHub context
-	// const prBranchName = context.payload.pull_request.head.ref;
-	// const branchesUrl =
-	// 	'https://betadownload.jetpack.me/woocommerce-branches.json';
+	const commentInfo = {
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+		issue_number: context.issue.number,
+	};
 
-	// Function to fetch JSON data using https
-	// async function fetchJson( url ) {
-	// 	return new Promise( ( resolve, reject ) => {
-	// 		https.get( url, ( res ) => {
-	// 			let data = '';
+	const comments = ( await github.rest.issues.listComments( commentInfo ) )
+		.data;
 
-	// 			res.on( 'data', ( chunk ) => {
-	// 				data += chunk;
-	// 			} );
-
-	// 			res.on( 'end', () => {
-	// 				resolve( JSON.parse( data ) );
-	// 			} );
-
-	// 			res.on( 'error', ( err ) => {
-	// 				reject( err );
-	// 			} );
-	// 		} );
-	// 	} );
-	// }
-
-	// Fetch the branches data and extract the download URL
-	try {
-		// const payload = await fetchJson( branchesUrl );
-		// const branches = Object.values( payload.pr );
-
-		// const branch = branches.find( ( branch ) => {
-		// 	return branch.branch === prBranchName;
-		// } );
-
-		// const artifactUrl = branch?.download_url;
-
-		// if ( ! artifactUrl ) {
-		// 	console.error(
-		// 		'Download URL not found for the branch:',
-		// 		prBranchName
-		// 	);
-		// 	return;
-		// }
-
-		const commentInfo = {
-			owner: context.repo.owner,
-			repo: context.repo.repo,
-			issue_number: context.issue.number,
-		};
-
-		const comments = (
-			await github.rest.issues.listComments( commentInfo )
-		 ).data;
-
-		for ( const currentComment of comments ) {
-			if (
-				currentComment.user.type === 'Bot' &&
-				currentComment.body.includes(
-					'Test using WordPress Playground'
-				)
-			) {
-				return;
-			}
+	for ( const currentComment of comments ) {
+		if (
+			currentComment.user.type === 'Bot' &&
+			currentComment.body.includes( 'Test using WordPress Playground' )
+		) {
+			return;
 		}
+	}
 
-		const defaultSchema = generateWordpressPlaygroundBlueprint(
-			context.runId,
-			context.issue.number
-		);
+	const defaultSchema = generateWordpressPlaygroundBlueprint(
+		context.runId,
+		context.issue.number
+	);
 
-		const url = `https://playground.wordpress.net/#${ Buffer.from(
-			JSON.stringify( defaultSchema )
-		).toString( 'base64' ) }`;
+	const url = `https://playground.wordpress.net/#${ Buffer.from(
+		JSON.stringify( defaultSchema )
+	).toString( 'base64' ) }`;
 
-		const proxyUrl = ( commentInfo.body = `
+	commentInfo.body = `
 ## Test using WordPress Playground
 The changes in this pull request can be previewed and tested using a [WordPress Playground](https://developer.wordpress.org/playground/) instance.
 [WordPress Playground](https://developer.wordpress.org/playground/) is an experimental project that creates a full WordPress instance entirely within the browser.
 
 [Test this pull request with WordPress Playground](${ url }).
-` );
+`;
 
-		await github.rest.issues.createComment( commentInfo );
-	} catch ( error ) {
-		console.error( 'Error fetching branches data:', error );
-	}
+	await github.rest.issues.createComment( commentInfo );
 }
 
 module.exports = { run };
