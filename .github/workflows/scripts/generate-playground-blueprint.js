3971b65182d31e29ec5a47cf3f50dcd5afa95484
Sam Seay <samueljseay@gmail.com>
2024-04-23 16:14:29 +0800

    smooth generator install breaks for some reason,

--- .github/workflows/scripts/generate-playground-blueprint.js
+++ .github/workflows/scripts/generate-playground-blueprint.js
@@ -23,16 +23,6 @@ const generateWordpressPlaygroundBlueprint = ( runId, prNumber ) => {
 				},
 			},
 			{
-				step: 'installPlugin',
-				pluginZipFile: {
-					resource: 'url',
-					url: 'https://github.com/woocommerce/wc-smooth-generator/releases/download/1.1.0/wc-smooth-generator.zip',
-				},
-				options: {
-					activate: true,
-				},
-			},
-			{
 				step: 'login',
 				username: 'admin',
 				password: 'password',
