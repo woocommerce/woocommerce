ad4d1167012f6b452b357f49ca819c28ec27c78a
Jonathan Sadowski <git@jonathan.st>
2024-04-25 14:14:16 -0500

    Update case insensitivity to every field

--- tools/monorepo-utils/src/changefile/lib/__tests__/github.ts
+++ tools/monorepo-utils/src/changefile/lib/__tests__/github.ts
@@ -102,6 +102,45 @@ describe( 'getChangelogSignificance', () => {
 		expect( significance ).toBe( 'patch' );
 	} );
 
+	it( 'should return the selected significance when upper-case', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [ ] Patch\r\n' +
+			'- [X] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [ ] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message ' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment ' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'\r\n' +
+			'</details>';
+
+		const significance = getChangelogSignificance( body );
+		expect( significance ).toBe( 'minor' );
+	} );
+
 	it( 'should error when no significance selected', () => {
 		const body =
 			'### Changelog entry\r\n' +
@@ -225,6 +264,45 @@ describe( 'getChangelogType', () => {
 
 		const type = getChangelogType( body );
 		expect( type ).toBe( 'fix' );
+	} );
+
+	it( 'should return the selected changelog type when upper-case', () => {
+		const body =
+			'### Changelog entry\r\n' +
+			'\r\n' +
+			'<!-- You can optionally choose to enter a changelog entry by checking the box and supplying data. -->\r\n' +
+			'\r\n' +
+			'- [x] Automatically create a changelog entry from the details below.\r\n' +
+			'\r\n' +
+			'<details>\r\n' +
+			'\r\n' +
+			'#### Significance\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [x] Patch\r\n' +
+			'- [ ] Minor\r\n' +
+			'- [ ] Major\r\n' +
+			'\r\n' +
+			'#### Type\r\n' +
+			'<!-- Choose only one -->\r\n' +
+			'- [ ] Fix - Fixes an existing bug\r\n' +
+			'- [ ] Add - Adds functionality\r\n' +
+			'- [ ] Update - Update existing functionality\r\n' +
+			'- [X] Dev - Development related task\r\n' +
+			'- [ ] Tweak - A minor adjustment to the codebase\r\n' +
+			'- [ ] Performance - Address performance issues\r\n' +
+			'- [ ] Enhancement\r\n' +
+			'\r\n' +
+			'#### Message ' +
+			'<!-- Add a changelog message here -->\r\n' +
+			'This is a very useful fix.\r\n' +
+			'\r\n' +
+			'#### Comment ' +
+			`<!-- If the changes in this pull request don't warrant a changelog entry, you can alternatively supply a comment here. Note that comments are only accepted with a significance of "Patch" -->\r\n` +
+			'\r\n' +
+			'</details>';
+
+		const type = getChangelogType( body );
+		expect( type ).toBe( 'dev' );
 	} );
 
 	it( 'should error when no type selected', () => {
