8a0ed2dd0bdca36ff4f214a4fd7e9ddcabdf12f2
Vlad Olaru <vlad@thinkwritecode.com>
2024-04-25 16:24:05 +0300

    Revert "Upgrade the WooPayments surfaces to the new designs" (#46915)
    Revert "Upgrade the WooPayments surfaces to the new designs (#46873)"
    
    This reverts commit 24808a0a8bd36d74952ee51b2333b22127514ba7.

--- plugins/woocommerce-admin/client/task-lists/fills/PaymentGatewaySuggestions/components/WCPay/Suggestion.js
+++ plugins/woocommerce-admin/client/task-lists/fills/PaymentGatewaySuggestions/components/WCPay/Suggestion.js
@@ -1,7 +1,14 @@
 /**
  * External dependencies
  */
-import { WCPayConnectCard } from '@woocommerce/onboarding';
+import { __ } from '@wordpress/i18n';
+import {
+	WCPayBanner,
+	WCPayBannerFooter,
+	WCPayBannerBody,
+	WCPayBenefits,
+	WCPayBannerImageCut,
+} from '@woocommerce/onboarding';
 import { useDispatch } from '@wordpress/data';
 
 /**
@@ -17,47 +24,51 @@ export const Suggestion = ( { paymentGateway, onSetupCallback = null } ) => {
 	const {
 		id,
 		needsSetup,
+		installed,
 		enabled: isEnabled,
 		installed: isInstalled,
 	} = paymentGateway;
 	const isWooPayEligible = getAdminSetting( 'isWooPayEligible' );
 
 	const { createNotice } = useDispatch( 'core/notices' );
-	// When WCPay is installed and onSetupCallback is null
+	// When the WC Pay is installed and onSetupCallback is null
 	// Overwrite onSetupCallback to redirect to the setup page
 	// when the user clicks on the "Finish setup" button.
-	// WCPay doesn't need to be configured in WCA.
+	// WC Pay doesn't need to be configured in WCA.
 	// It should be configured in its onboarding flow.
-	if ( isInstalled && onSetupCallback === null ) {
+	if ( installed && onSetupCallback === null ) {
 		onSetupCallback = () => {
 			connectWcpay( createNotice );
 		};
 	}
 
-	const wccomSettings = getAdminSetting( 'wccomHelper', false );
-
-	const firstName = getAdminSetting( 'currentUserData' )?.first_name;
-
 	return (
-		<div className="woocommerce-wcpay-suggestion woocommerce-task-payment-wcpay">
-			<WCPayConnectCard
-				actionButton={
-					<Action
-						id={ id }
-						hasSetup={ true }
-						needsSetup={ needsSetup }
-						isEnabled={ isEnabled }
-						isRecommended={ true }
-						isInstalled={ isInstalled }
-						hasPlugins={ true }
-						onSetupCallback={ onSetupCallback }
-					/>
-				}
-				firstName={ firstName }
-				businessCountry={ wccomSettings?.storeCountry ?? '' }
-				isWooPayEligible={ isWooPayEligible }
-				showNotice={ true }
-			/>
+		<div className="woocommerce-wcpay-suggestion">
+			<WCPayBanner>
+				<WCPayBannerBody
+					textPosition="left"
+					actionButton={
+						<Action
+							id={ id }
+							hasSetup={ true }
+							needsSetup={ needsSetup }
+							isEnabled={ isEnabled }
+							isRecommended={ true }
+							isInstalled={ isInstalled }
+							hasPlugins={ true }
+							setupButtonText={ __(
+								'Get started',
+								'woocommerce'
+							) }
+							onSetupCallback={ onSetupCallback }
+						/>
+					}
+					bannerImage={ <WCPayBannerImageCut /> }
+					isWooPayEligible={ isWooPayEligible }
+				/>
+				<WCPayBenefits isWooPayEligible={ isWooPayEligible } />
+				<WCPayBannerFooter isWooPayEligible={ isWooPayEligible } />
+			</WCPayBanner>
 		</div>
 	);
 };
