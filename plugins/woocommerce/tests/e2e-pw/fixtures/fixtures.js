b0c22c8d758e17d490001c6fcbc1ea7012527b02
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-04-26 11:43:58 +0300

    Added testPage fixture and testPgeTitle fixture option

--- plugins/woocommerce/tests/e2e-pw/fixtures/fixtures.js
+++ plugins/woocommerce/tests/e2e-pw/fixtures/fixtures.js
@@ -1,6 +1,7 @@
 const base = require( '@playwright/test' );
 const wcApi = require( '@woocommerce/woocommerce-rest-api' ).default;
 const { admin } = require( '../test-data/data' );
+const uuid = require( 'uuid' );
 
 exports.test = base.test.extend( {
 	api: async ( { baseURL }, use ) => {
@@ -42,6 +43,34 @@ exports.test = base.test.extend( {
 		} );
 
 		await use( wpApi );
+	},
+
+	testPageTitle: [ `Test page ${ uuid.v1() }`, { option: true } ],
+
+	testPage: async ( { wpApi, testPageTitle }, use ) => {
+		const pageSlug = testPageTitle.replace( / /gi, '-' ).toLowerCase();
+
+		await use( { title: testPageTitle, slug: pageSlug } );
+
+		// Cleanup
+		const pages = await wpApi.get(
+			`/wp-json/wp/v2/pages?slug=${ pageSlug }`,
+			{
+				data: {
+					_fields: [ 'id' ],
+				},
+				failOnStatusCode: false,
+			}
+		);
+
+		for ( const page of await pages.json() ) {
+			console.log( `Deleting page ${ page.id }` );
+			await wpApi.delete( `/wp-json/wp/v2/pages/${ page.id }`, {
+				data: {
+					force: true,
+				},
+			} );
+		}
 	},
 } );
 
