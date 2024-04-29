18ad5036fee1a68f6ac07ab0401bc4acd4649a39
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-04-29 16:59:28 +0300

    Disable woocommerce_coming_soon in test env setup

--- plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
+++ plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
@@ -38,5 +38,7 @@ if [ $ENABLE_TRACKING == 1 ]; then
 	wp-env run tests-cli wp option update woocommerce_allow_tracking 'yes'
 fi
 
+wp-env run tests-cli wp option update woocommerce_coming_soon 'no'
+
 echo -e 'Upload test images \n'
 wp-env run tests-cli wp media import './test-data/images/image-01.png' './test-data/images/image-02.png' './test-data/images/image-03.png'
