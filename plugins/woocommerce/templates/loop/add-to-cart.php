7f8cc254aef232202875dd83480ba52a36f8b86e
Sam Seay <samueljseay@gmail.com>
2024-04-29 09:59:50 +0800

    Apparently it should be 9.0.0

--- plugins/woocommerce/templates/loop/add-to-cart.php
+++ plugins/woocommerce/templates/loop/add-to-cart.php
@@ -12,7 +12,7 @@
  *
  * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     8.9.0
+ * @version     9.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
