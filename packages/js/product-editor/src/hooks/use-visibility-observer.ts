fa054e8aef05186951bb2fddc9210cab52e185f2
Fernando Marichal <fernando.marichal@automattic.com>
2024-04-30 11:37:29 -0300

    Hook accepts elements or selectors

--- packages/js/product-editor/src/hooks/use-visibility-observer.ts
+++ packages/js/product-editor/src/hooks/use-visibility-observer.ts
@@ -5,21 +5,27 @@ import { RefObject } from 'react';
 import { useEffect, useState, useRef } from '@wordpress/element';
 
 export function useVisibilityObserver(
-	elementRef: RefObject< HTMLInputElement >,
-	options = { threshold: 0.1 }
+	elementOrSelector: RefObject< HTMLInputElement > | string,
+	options = { threshold: 0.1 } // 10% of the element is visible.
 ) {
 	const [ isVisible, setIsVisible ] = useState( false );
 	const observer = useRef< IntersectionObserver | null >( null );
 
-	const handleObserver = ( entries: IntersectionObserverEntry[] ) => {
-		entries.forEach( ( entry ) => {
-			setIsVisible( entry.isIntersecting );
-		} );
-	};
-
 	useEffect( () => {
-		const currentElement = elementRef.current;
+		const currentElement =
+			typeof elementOrSelector === 'string'
+				? document.querySelector< HTMLInputElement >(
+						elementOrSelector
+				  )
+				: elementOrSelector.current;
+
 		if ( ! currentElement || observer.current ) return;
+
+		const handleObserver = ( entries: IntersectionObserverEntry[] ) => {
+			entries.forEach( ( entry ) => {
+				setIsVisible( entry.isIntersecting );
+			} );
+		};
 
 		const obs = new IntersectionObserver( handleObserver, options );
 		obs.observe( currentElement );
