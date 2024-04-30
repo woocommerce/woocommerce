c7be21c86751c451484a9e8ae8cc52d026be5462
Alba Rincón <alba.rincon@automattic.com>
2024-04-30 14:01:07 +0200

    Add tests for the customize button

--- plugins/woocommerce/tests/e2e-pw/tests/customize-store/intro.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/customize-store/intro.spec.js
@@ -1,9 +1,17 @@
-const { test, expect, request } = require( '@playwright/test' );
+const { test: base, expect, request } = require( '@playwright/test' );
 const { activateTheme, DEFAULT_THEME } = require( '../../utils/themes' );
 const { setOption } = require( '../../utils/options' );
+const { AssemblerPage } = require( './assembler/assembler.page' );
 
 const CUSTOMIZE_STORE_URL =
 	'/wp-admin/admin.php?page=wc-admin&path=%2Fcustomize-store';
+
+const test = base.extend( {
+	assemblerPageObject: async ( { page }, use ) => {
+		const pageObject = new AssemblerPage( { page } );
+		await use( pageObject );
+	},
+} );
 
 test.describe( 'Store owner can view the Intro page', () => {
 	test.use( { storageState: process.env.ADMINSTATE } );
@@ -98,5 +106,64 @@ test.describe( 'Store owner can view the Intro page', () => {
 		await expect(
 			page.getByRole( 'button', { name: 'Customize your theme' } )
 		).toBeVisible();
+	} );
+
+	test( 'Clicking on "Customize your theme" with a block theme should go to the assembler', async ( {
+		page,
+		assemblerPageObject,
+		baseURL,
+	} ) => {
+		await activateTheme( DEFAULT_THEME );
+
+		try {
+			await setOption(
+				request,
+				baseURL,
+				'woocommerce_admin_customize_store_completed',
+				'yes'
+			);
+		} catch ( error ) {
+			console.log( 'Store completed option not updated' );
+		}
+		await page.goto( CUSTOMIZE_STORE_URL );
+
+		await page
+			.getByRole( 'button', { name: 'Customize your theme' } )
+			.click();
+
+		await assemblerPageObject.waitForLoadingScreenFinish();
+		const assembler = await assemblerPageObject.getAssembler();
+		await expect(
+			assembler.locator( "text=Let's get creative" )
+		).toBeVisible();
+	} );
+
+	test( 'clicking on "Customize your theme" with a classic theme should go to the customizer', async ( {
+		page,
+		assemblerPageObject,
+		baseURL,
+	} ) => {
+		await activateTheme( 'twentytwenty' );
+
+		try {
+			await setOption(
+				request,
+				baseURL,
+				'woocommerce_admin_customize_store_completed',
+				'yes'
+			);
+		} catch ( error ) {
+			console.log( 'Store completed option not updated' );
+		}
+		await page.goto( CUSTOMIZE_STORE_URL );
+
+		await page
+			.getByRole( 'button', { name: 'Customize your theme' } )
+			.click();
+
+		//wait for loading state
+		await page.waitForNavigation();
+		//check url
+		await expect( page.url() ).toContain( 'customize.php' );
 	} );
 } );
