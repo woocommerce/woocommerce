065c1a89725b9d815c1cb3e2233e1083acde738c
Fernando Marichal <fernando.marichal@automattic.com>
2024-04-30 14:57:37 -0300

    Create useConditionalExecution hook

---
+++ packages/js/product-editor/src/hooks/use-conditional-execution.ts
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+import { RefObject } from 'react';
+import { useEffect, useRef } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { useVisibilityObserver } from './use-visibility-observer';
+
+type useConditionalExecutionProps = {
+	elementOrSelector?: RefObject< HTMLInputElement > | string;
+	onVisible?: () => void;
+	onHidden?: () => void;
+	isMemorized?: boolean;
+};
+
+export function useConditionalExecution( {
+	elementOrSelector,
+	onVisible,
+	onHidden,
+	isMemorized = false,
+}: useConditionalExecutionProps ) {
+	const elementRef = useRef( null );
+	const isVisible = useVisibilityObserver( elementOrSelector ?? elementRef );
+
+	function getDependencies() {
+		const dependenciesList = [];
+		if ( isMemorized ) {
+			if ( onVisible !== undefined ) {
+				dependenciesList.push( onVisible );
+			}
+			if ( onHidden !== undefined ) {
+				dependenciesList.push( onHidden );
+			}
+		}
+		return dependenciesList;
+	}
+
+	const dependencies = getDependencies();
+
+	useEffect( () => {
+		if ( isVisible && onVisible !== undefined ) {
+			onVisible();
+		} else if ( onHidden !== undefined ) {
+			onHidden();
+		}
+	}, [ isVisible, ...dependencies ] );
+
+	return elementRef;
+}
