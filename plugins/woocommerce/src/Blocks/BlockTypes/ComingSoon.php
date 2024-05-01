0b40e16af46432c0fe9f27d04dcb40b646afd978
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-05-01 09:20:13 +0800

    Fix failed to load coming soon css/js

--- plugins/woocommerce/src/Blocks/BlockTypes/ComingSoon.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ComingSoon.php
@@ -5,18 +5,39 @@ namespace Automattic\WooCommerce\Blocks\BlockTypes;
  * ComingSoon class.
  */
 class ComingSoon extends AbstractBlock {
-		/**
-		 * Block name.
-		 *
-		 * @var string
-		 */
-		protected $block_name = 'coming-soon';
+	/**
+	 * Block name.
+	 *
+	 * @var string
+	 */
+	protected $block_name = 'coming-soon';
 
-		/**
-		 * It is necessary to register and enqueue assets during the render phase because we want to load assets only if the block has the content.
-		 */
+	/**
+	 * It is necessary to register and enqueue assets during the render phase because we want to load assets only if the block has the content.
+	 */
 	protected function register_block_type_assets() {
 			parent::register_block_type_assets();
 			$this->register_chunk_translations( [ $this->block_name ] );
+	}
+
+
+	/**
+	 * Get the frontend style handle for this block type.
+	 *
+	 * @return null
+	 */
+	protected function get_block_type_style() {
+		return null;
+	}
+
+	/**
+	 * Get the frontend script handle for this block type.
+	 *
+	 * @see $this->register_block_type()
+	 * @param string $key Data to get, or default to everything.
+	 * @return array|string|null
+	 */
+	protected function get_block_type_script( $key = null ) {
+		return null;
 	}
 }
