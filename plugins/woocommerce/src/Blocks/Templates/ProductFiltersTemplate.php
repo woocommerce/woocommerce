81377a5e2b5381f80ce2199639e4f6bb9b011149
Alexandre Lara <allexandrelara@gmail.com>
2024-05-01 17:36:49 -0300

    Move Product Filters Template part to Products area

--- plugins/woocommerce/src/Blocks/Templates/ProductFiltersTemplate.php
+++ plugins/woocommerce/src/Blocks/Templates/ProductFiltersTemplate.php
@@ -20,12 +20,14 @@ class ProductFiltersTemplate extends AbstractTemplatePart {
 	 *
 	 * @var string
 	 */
-	public $template_area = 'header';
+	public $template_area = 'products';
 
 	/**
 	 * Initialization method.
 	 */
-	public function init() {}
+	public function init() {
+		add_filter( 'default_wp_template_part_areas', array( $this, 'register_products_template_part_area' ), 10, 1 );
+	}
 
 	/**
 	 * Returns the title of the template.
@@ -43,5 +45,22 @@ class ProductFiltersTemplate extends AbstractTemplatePart {
 	 */
 	public function get_template_description() {
 		return __( 'Template used to display the simplified Product Filters.', 'woocommerce' );
+	}
+
+	/**
+	 * Add Product Filters to the default template part areas.
+	 *
+	 * @param array $default_area_definitions An array of supported area objects.
+	 * @return array The supported template part areas including the Product Filters one.
+	 */
+	public function register_products_template_part_area( $default_area_definitions ) {
+		$products_template_part_area = array(
+			'area'        => 'products',
+			'label'       => __( 'Products', 'woocommerce' ),
+			'description' => __( 'This is the area for template parts used to consistently display various product information, such as images and prices, across all of your store pages.', 'woocommerce' ),
+			'icon'        => 'products',
+			'area_tag'    => 'products',
+		);
+		return array_merge( $default_area_definitions, array( $products_template_part_area ) );
 	}
 }
