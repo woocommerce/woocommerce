b0b8d4b9497528167affba7d917a07df1c563a89
Matt Sherman <matt@jam123.com>
2024-05-02 16:28:21 -0400

    Provide comments to clarify sidebar props settings

--- packages/js/product-editor/src/components/iframe-editor/sidebar/settings-sidebar/settings-sidebar.tsx
+++ packages/js/product-editor/src/components/iframe-editor/sidebar/settings-sidebar/settings-sidebar.tsx
@@ -19,10 +19,14 @@ const SettingsHeader = () => {
 export const SettingsSidebar = () => {
 	return (
 		<PluginSidebar
+			// By not providing a name, the sidebar will not be listed in
+			// the more menu's Plugins menu group.
 			identifier="woocommerce-product-editor-settings-sidebar"
 			title={ __( 'Settings', 'woocommerce' ) }
 			icon={ isRTL() ? drawerRight : drawerLeft }
 			isActiveByDefault={ true }
+			// We need to pass a custom header to the sidebar to prevent
+			// the pin button in the default header from being displayed.
 			header={ <SettingsHeader /> }
 			closeLabel={ __( 'Close settings', 'woocommerce' ) }
 		>
