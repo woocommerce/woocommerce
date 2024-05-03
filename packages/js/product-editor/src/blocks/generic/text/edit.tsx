10767de53e0b29d14e22e418c68b0827b0fea002
Joshua Flowers <joshuatf@gmail.com>
2024-05-03 11:41:05 -0400

    Add validation to generic text field block

--- packages/js/product-editor/src/blocks/generic/text/edit.tsx
+++ packages/js/product-editor/src/blocks/generic/text/edit.tsx
@@ -1,27 +1,25 @@
 /**
  * External dependencies
  */
+import classNames from 'classnames';
 import { useWooBlockProps } from '@woocommerce/block-templates';
 import { Link } from '@woocommerce/components';
-import { Product } from '@woocommerce/data';
-import { createElement, useRef } from '@wordpress/element';
-import { __, sprintf } from '@wordpress/i18n';
+import { createElement, useRef, useState } from '@wordpress/element';
 import { Icon, external } from '@wordpress/icons';
 
 /**
  * Internal dependencies
  */
 import { TextControl } from '../../../components/text-control';
-import { useValidation } from '../../../contexts/validation-context';
-import { useProductEdits } from '../../../hooks/use-product-edits';
 import useProductEntityProp from '../../../hooks/use-product-entity-prop';
 import { ProductEditorBlockEditProps } from '../../../types';
 import { TextBlockAttributes } from './types';
 
 export function Edit( {
 	attributes,
-	context: { postType },
+	context: { postType, validationErrors },
 }: ProductEditorBlockEditProps< TextBlockAttributes > ) {
+	const [ isTouched, setIsTouched ] = useState< boolean >( false );
 	const blockProps = useWooBlockProps( attributes );
 
 	const {
@@ -46,92 +44,9 @@ export function Edit( {
 		fallbackValue: '',
 	} );
 
-	const { hasEdit } = useProductEdits();
-
 	const inputRef = useRef< HTMLInputElement >( null );
 
-	const { error, validate } = useValidation< Product >(
-		property,
-		async function validator() {
-			if ( ! inputRef.current ) return;
-
-			const input = inputRef.current;
-
-			let customErrorMessage = '';
-
-			if ( input.validity.typeMismatch ) {
-				customErrorMessage =
-					type?.message ??
-					__( 'Invalid value for the field.', 'woocommerce' );
-			}
-			if ( input.validity.valueMissing ) {
-				customErrorMessage =
-					typeof required === 'string'
-						? required
-						: __( 'This field is required.', 'woocommerce' );
-			}
-			if ( input.validity.patternMismatch ) {
-				customErrorMessage =
-					pattern?.message ??
-					__( 'Invalid value for the field.', 'woocommerce' );
-			}
-			if ( input.validity.tooShort ) {
-				// eslint-disable-next-line @wordpress/valid-sprintf
-				customErrorMessage = sprintf(
-					minLength?.message ??
-						/* translators: %d: minimum length */
-						__(
-							'The minimum length of the field is %d',
-							'woocommerce'
-						),
-					minLength?.value
-				);
-			}
-			if ( input.validity.tooLong ) {
-				// eslint-disable-next-line @wordpress/valid-sprintf
-				customErrorMessage = sprintf(
-					maxLength?.message ??
-						/* translators: %d: maximum length */
-						__(
-							'The maximum length of the field is %d',
-							'woocommerce'
-						),
-					maxLength?.value
-				);
-			}
-			if ( input.validity.rangeUnderflow ) {
-				// eslint-disable-next-line @wordpress/valid-sprintf
-				customErrorMessage = sprintf(
-					min?.message ??
-						/* translators: %d: minimum length */
-						__(
-							'The minimum value of the field is %d',
-							'woocommerce'
-						),
-					min?.value
-				);
-			}
-			if ( input.validity.rangeOverflow ) {
-				// eslint-disable-next-line @wordpress/valid-sprintf
-				customErrorMessage = sprintf(
-					max?.message ??
-						/* translators: %d: maximum length */
-						__(
-							'The maximum value of the field is %d',
-							'woocommerce'
-						),
-					max?.value
-				);
-			}
-
-			input.setCustomValidity( customErrorMessage );
-
-			if ( ! input.validity.valid ) {
-				return input.validationMessage;
-			}
-		},
-		[ type, required, pattern, minLength, maxLength, min, max ]
-	);
+	const error = validationErrors[ property ];
 
 	function getSuffix() {
 		if ( ! suffix || ! value || ! inputRef.current ) return;
@@ -166,12 +81,7 @@ export function Edit( {
 				disabled={ disabled }
 				label={ label }
 				onChange={ setValue }
-				onBlur={ () => {
-					if ( hasEdit( property ) ) {
-						validate();
-					}
-				} }
-				error={ error }
+				error={ !! isTouched && !! error ? error.message : undefined }
 				help={ help }
 				placeholder={ placeholder }
 				tooltip={ tooltip }
@@ -182,6 +92,7 @@ export function Edit( {
 				maxLength={ maxLength?.value }
 				min={ min?.value }
 				max={ max?.value }
+				onBlur={ () => setIsTouched( true ) }
 			/>
 		</div>
 	);
