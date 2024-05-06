66660f2594c864848a74b3f8fb037f97c75b016c
Akeda Bagus <akeda.bagus@automattic.com>
2024-05-06 14:32:33 +0700

    Add Tracks event when connect-notice in plugin is shown and clicked

--- plugins/woocommerce/src/Admin/PluginsHelper.php
+++ plugins/woocommerce/src/Admin/PluginsHelper.php
@@ -40,6 +40,7 @@ class PluginsHelper {
 		add_action( 'woocommerce_plugins_activate_callback', array( __CLASS__, 'activate_plugins' ), 10, 2 );
 		add_action( 'admin_notices', array( __CLASS__, 'maybe_show_connect_notice_in_plugin_list' ) );
 		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'maybe_enqueue_scripts_for_connect_notice' ) );
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'maybe_enqueue_scripts_for_connect_notice_in_plugins' ) );
 	}
 
 	/**
@@ -600,4 +601,12 @@ class PluginsHelper {
 		wp_enqueue_script( 'woo-connect-notice' );
 	}
 
+	public static function maybe_enqueue_scripts_for_connect_notice_in_plugins() {
+		if ( 'plugins' !== get_current_screen()->id ) {
+			return;
+		}
+
+		WCAdminAssets::register_script( 'wp-admin-scripts', 'woo-plugin-update-connect-notice' );
+		wp_enqueue_script( 'woo-plugin-update-connect-notice' );
+	}
 }
