6af2355c4a54cc8ed2f230ed67f571fa3b6ab6bc
Naman Malhotra <naman03malhotra@gmail.com>
2024-05-06 15:30:51 +0530

    Updated templae version

--- plugins/woocommerce/templates/block-notices/error.php
+++ plugins/woocommerce/templates/block-notices/error.php
@@ -12,7 +12,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 8.6.0
+ * @version 8.7.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
