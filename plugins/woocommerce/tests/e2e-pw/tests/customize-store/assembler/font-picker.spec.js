62bba2ad6526257d2f382e39ea429dd2a0ff4de8
Luigi Teschio <gigitux@gmail.com>
2024-05-06 14:36:25 +0200

    CYS: not run font picker tests on WordPress 6.4

--- plugins/woocommerce/tests/e2e-pw/tests/customize-store/assembler/font-picker.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/customize-store/assembler/font-picker.spec.js
@@ -40,7 +40,22 @@ const slugFontMap = {
 		'System Sans-serif',
 };
 
+const skipTestIfWPIs64 = () => {
+	const skipMessage = `Skipping this test because on WordPress 6.4, because the font picker is not available`;
+
+	test.skip( () => {
+		const shouldSkip = process.env.WP_ENV_CORE.includes( '6.4' );
+
+		if ( shouldSkip ) {
+			console.log( skipMessage );
+		}
+
+		return shouldSkip;
+	}, skipMessage );
+};
+
 test.describe( 'Assembler -> Font Picker', () => {
+	skipTestIfWPIs64();
 	test.use( { storageState: process.env.ADMINSTATE } );
 
 	test.beforeAll( async ( { baseURL } ) => {
