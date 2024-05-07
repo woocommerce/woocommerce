bd5cdc8fe2df381fc85415623df06e2dd77a50cd
Sam Seay <samueljseay@gmail.com>
2024-05-07 21:35:14 +0800

    Remove redundant newline.

--- .github/workflows/scripts/generate-playground-blueprint.js
+++ .github/workflows/scripts/generate-playground-blueprint.js
@@ -73,8 +73,7 @@ The changes in this pull request can be previewed and tested using a [WordPress 
 
 [Test this pull request with WordPress Playground](${ url }).
 
-Note that this URL is valid for 30 days from when this comment was last updated. You can update it by closing/reopening the PR
-or pushing a new commit.
+Note that this URL is valid for 30 days from when this comment was last updated. You can update it by closing/reopening the PR or pushing a new commit.
 `;
 
 	if ( existingCommentId ) {
