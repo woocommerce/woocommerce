30e17a6c78e12371ebb1cbb99e32bf4a01d9a94c
Brian <brian@brianhaas.li>
2024-05-07 07:49:52 +0200

    Update plugins/woocommerce/templates/myaccount/navigation.php
    Co-authored-by: Niels Lange <niels.lange@a8c.com>

--- plugins/woocommerce/templates/myaccount/navigation.php
+++ plugins/woocommerce/templates/myaccount/navigation.php
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 do_action( 'woocommerce_before_account_navigation' );
 ?>
 
-<nav class="woocommerce-MyAccount-navigation" aria-label="Account Pages">
+<nav class="woocommerce-MyAccount-navigation" aria-label="Account pages">
 	<ul>
 		<?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
 			<li class="<?php echo wc_get_account_menu_item_classes( $endpoint ); ?>">
