814f47c2aa42603b06fa9f08327093e099f6136c
Sam Seay <samueljseay@gmail.com>
2024-05-07 21:05:19 +0800

    Extend retention to 30 days, inform users in the generated comment.

--- .github/workflows/scripts/generate-playground-blueprint.js
+++ .github/workflows/scripts/generate-playground-blueprint.js
@@ -72,6 +72,9 @@ The changes in this pull request can be previewed and tested using a [WordPress 
 [WordPress Playground](https://developer.wordpress.org/playground/) is an experimental project that creates a full WordPress instance entirely within the browser.
 
 [Test this pull request with WordPress Playground](${ url }).
+
+Note that this URL is valid for 30 days from when this comment was last updated. You can update it by closing/reopening the PR
+or pushing a new commit.
 `;
 
 	if ( existingCommentId ) {
