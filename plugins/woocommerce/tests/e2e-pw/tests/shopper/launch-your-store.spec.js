d976776581d183fcfe8fa20f6d19c803bcc9f858
paul sealock <psealock@gmail.com>
2024-05-08 14:17:08 +1200

    Store only coming soon mode - logged out

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/launch-your-store.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/launch-your-store.spec.js
@@ -1,7 +1,7 @@
 const { test, expect, request } = require( '@playwright/test' );
 const { setOption } = require( '../../utils/options' );
 
-test.describe( 'LYS', () => {
+test.describe( 'Launch Your Store front end', () => {
 	test.afterAll( async ( { baseURL } ) => {
 		try {
 			await setOption(
@@ -15,7 +15,10 @@ test.describe( 'LYS', () => {
 		}
 	} );
 
-	test( 'Options set correctly', async ( { page, baseURL } ) => {
+	test( 'Entire site coming soon mode - logged out', async ( {
+		page,
+		baseURL,
+	} ) => {
 		try {
 			await setOption(
 				request,
@@ -39,6 +42,41 @@ test.describe( 'LYS', () => {
 		await expect(
 			page.getByText(
 				'Pardon our dust! We’re working on something amazing — check back soon!'
+			)
+		).toBeVisible();
+	} );
+
+	test( 'Store only coming soon mode - logged out', async ( {
+		page,
+		baseURL,
+	} ) => {
+		try {
+			await setOption(
+				request,
+				baseURL,
+				'woocommerce_coming_soon',
+				'yes'
+			);
+
+			await setOption(
+				request,
+				baseURL,
+				'woocommerce_store_pages_only',
+				'yes'
+			);
+		} catch ( error ) {
+			console.log( error );
+		}
+
+		await page.goto( baseURL + '/shop/' );
+
+		await expect(
+			page.getByText( 'Great things are on the horizon' )
+		).toBeVisible();
+
+		await expect(
+			page.getByText(
+				'Something big is brewing! Our store is in the works and will be launching soon!'
 			)
 		).toBeVisible();
 	} );
