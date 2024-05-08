1659acf4fb0eccfd88ea67abe1aa69c997c5d087
Naman Malhotra <naman03malhotra@gmail.com>
2024-05-08 19:11:17 +0530

    Address feedback

--- plugins/woocommerce/templates/myaccount/form-lost-password.php
+++ plugins/woocommerce/templates/myaccount/form-lost-password.php
@@ -25,8 +25,8 @@ do_action( 'woocommerce_before_lost_password_form' );
 	<p><?php echo apply_filters( 'woocommerce_lost_password_message', esc_html__( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p><?php // @codingStandardsIgnoreLine ?>
 
 	<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
-		<label for="user_login"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?>&nbsp;<span class="required" aria-label="required">*</span></label>
-		<input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" autocomplete="username" />
+		<label for="user_login"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+		<input class="woocommerce-Input woocommerce-Input--text input-text" type="text" name="user_login" id="user_login" autocomplete="username" aria-required="true" />
 	</p>
 
 	<div class="clear"></div>
