49da9384c80a74edfff5e240ebb87de327b76658
Alexandre Lara <allexandrelara@gmail.com>
2024-05-09 17:09:35 -0300

    Add the blocks template for the Product Filters block

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/edit.tsx
@@ -1,18 +1,66 @@
 /**
  * External dependencies
  */
-import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
-import { BlockEditProps } from '@wordpress/blocks';
+import {
+	InnerBlocks,
+	useBlockProps,
+	useInnerBlocksProps,
+} from '@wordpress/block-editor';
+import { BlockEditProps, InnerBlockTemplate } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import type { ProductFiltersBlockAttributes } from './types';
 
+const TEMPLATE: InnerBlockTemplate[] = [
+	[
+		'core/heading',
+		{
+			level: 3,
+			style: { typography: { fontSize: '24px' } },
+			content: __( 'Filters', 'woocommerce' ),
+		},
+	],
+	[
+		'woocommerce/product-filter',
+		{
+			filterType: 'active-filters',
+			heading: __( 'Active', 'woocommerce' ),
+		},
+	],
+	[
+		'woocommerce/product-filter',
+		{
+			filterType: 'price-filter',
+			heading: __( 'Price', 'woocommerce' ),
+		},
+	],
+	[
+		'woocommerce/product-filter',
+		{
+			filterType: 'stock-filter',
+			heading: __( 'Status', 'woocommerce' ),
+		},
+	],
+	[
+		'woocommerce/product-filter',
+		{
+			filterType: 'rating-filter',
+			heading: __( 'Rating', 'woocommerce' ),
+		},
+	],
+];
+
 export const Edit = ( {}: BlockEditProps< ProductFiltersBlockAttributes > ) => {
 	const blockProps = useBlockProps();
 
-	return <div { ...blockProps }>Product Filters</div>;
+	return (
+		<div { ...blockProps }>
+			<InnerBlocks templateLock={ false } template={ TEMPLATE } />
+		</div>
+	);
 };
 
 export const Save = () => {
