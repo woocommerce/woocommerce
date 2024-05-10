ad5b0e98c79ee081d652c3e05f1b915f88a7ee5a
moon <moon.kyong@automattic.com>
2024-05-10 05:07:37 -0700

    Use flex layout

--- plugins/woocommerce/patterns/coming-soon-store-only.php
+++ plugins/woocommerce/patterns/coming-soon-store-only.php
@@ -19,7 +19,7 @@ if ( wc_current_theme_is_fse_theme() ) {
 }
 ?>
 
-<!-- wp:group {"layout":{"type":"flow"}} -->
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","orientation":"vertical"}} -->
 <div class="wp-block-group"><!-- wp:spacer -->
 <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
