989d97943c6b29b3fb5fc23fc47dbbcf0bc25898
Sam Seay <samueljseay@gmail.com>
2024-05-12 13:45:31 +0800

    More act wrapping, fix exception reporting in test.

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/coupon/test/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/coupon/test/index.tsx
@@ -11,13 +11,16 @@ import { VALIDATION_STORE_KEY } from '@woocommerce/block-data';
  */
 import { TotalsCoupon } from '..';
 
-describe( 'TotalsCoupon', async () => {
+describe( 'TotalsCoupon', () => {
 	it( "Shows a validation error when one is in the wc/store/validation data store and doesn't show one when there isn't", async () => {
+		const user = userEvent.setup();
 		const { rerender } = render( <TotalsCoupon instanceId={ 'coupon' } /> );
 
 		const openCouponFormButton = screen.getByText( 'Add a coupon' );
 		expect( openCouponFormButton ).toBeInTheDocument();
-		await userEvent.click( openCouponFormButton );
+		await act( async () => {
+			await user.click( openCouponFormButton );
+		} );
 		expect(
 			screen.queryByText( 'Invalid coupon code' )
 		).not.toBeInTheDocument();
--- plugins/woocommerce-blocks/packages/checkout/filter-registry/test/admin.js
+++ plugins/woocommerce-blocks/packages/checkout/filter-registry/test/admin.js
@@ -19,6 +19,7 @@ jest.mock( '@woocommerce/settings', () => {
 
 describe( 'Checkout registry (as admin user)', () => {
 	test( 'should throw if the filter throws and user is an admin', () => {
+		expect.assertions( 1 );
 		const filterName = 'ErrorTestFilter';
 		const value = 'Hello World';
 		registerCheckoutFilters( filterName, {
@@ -27,30 +28,35 @@ describe( 'Checkout registry (as admin user)', () => {
 			},
 		} );
 
-		const { result } = renderHook( () =>
+		try {
 			applyCheckoutFilter( {
 				filterName,
 				defaultValue: value,
-			} )
-		);
-		expect( result.error ).toEqual( Error( 'test error' ) );
+			} );
+		} catch ( e ) {
+			// eslint-disable-next-line  -- The toThrow helper does not stop wordpress/jest-console from erroring.
+			expect( e.message ).toBe( 'test error' );
+		}
 	} );
 
 	test( 'should throw if validation throws and user is an admin', () => {
+		expect.assertions( 1 );
 		const filterName = 'ValidationTestFilter';
 		const value = 'Hello World';
 		registerCheckoutFilters( filterName, {
 			[ filterName ]: ( val ) => val,
 		} );
-		const { result } = renderHook( () =>
+		try {
 			applyCheckoutFilter( {
 				filterName,
 				defaultValue: value,
 				validation: () => {
 					throw Error( 'validation error' );
 				},
-			} )
-		);
-		expect( result.error ).toEqual( Error( 'validation error' ) );
+			} );
+		} catch ( e ) {
+			// eslint-disable-next-line  -- The toThrow helper does not stop wordpress/jest-console from erroring.
+			expect( e.message ).toBe( 'validation error' );
+		}
 	} );
 } );
