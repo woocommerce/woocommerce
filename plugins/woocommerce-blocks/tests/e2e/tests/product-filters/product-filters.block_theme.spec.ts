ebe76931c309e19db3d4ec2354dc6852ed6f64e5
Alexandre Lara <allexandrelara@gmail.com>
2024-05-13 18:47:55 -0300

    Add E2E tests for the Product Filters block

--- plugins/woocommerce-blocks/tests/e2e/tests/product-filters/product-filters.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/product-filters/product-filters.block_theme.spec.ts
@@ -43,18 +43,78 @@ test.describe( `${ blockData.name }`, () => {
 		await editorUtils.enterEditMode();
 	} );
 
-	test( 'should contain correct inner blocks', async ( {
+	test( 'should be visible and contain correct inner blocks', async ( {
 		page,
-		editor,
 		pageObject,
 	} ) => {
 		await pageObject.addProductFiltersBlock( { cleanContent: true } );
 
-		const loadingIndicator = page.locator( '.components-spinner' );
-		await expect( loadingIndicator ).toBeHidden();
+		const block = page
+			.frameLocator( 'iframe[name="editor-canvas"]' )
+			.getByLabel( 'Block: Product Filters (Beta)' );
+		await expect( block ).toBeVisible();
 
-		const block = page.getByText( 'Active' );
-		console.log( await block.allInnerTexts() );
-		await expect( block ).toBeVisible( { timeout: 15000 } );
+		const filtersbBlockHeading = block.getByRole( 'document', {
+			name: 'Filters',
+		} );
+		await expect( filtersbBlockHeading ).toBeVisible();
+
+		const activeHeading = block.getByRole( 'document', {
+			name: 'Active',
+		} );
+		const activeFilterBlock = block.getByLabel(
+			'Block: Product Filter: Active'
+		);
+		await expect( activeHeading ).toBeVisible();
+		await expect( activeFilterBlock ).toBeVisible();
+
+		const priceHeading = block.getByRole( 'document', {
+			name: 'Price',
+		} );
+		const priceFilterBlock = block.getByLabel(
+			'Block: Product Filter: Price'
+		);
+		await expect( priceHeading ).toBeVisible();
+		await expect( priceFilterBlock ).toBeVisible();
+
+		const statusHeading = block.getByRole( 'document', {
+			name: 'Status',
+		} );
+		const statusFilterBlock = block.getByLabel(
+			'Block: Product Filter: Stock'
+		);
+		await expect( statusHeading ).toBeVisible();
+		await expect( statusFilterBlock ).toBeVisible();
+
+		const colorHeading = block.getByText( 'Color', {
+			exact: true,
+		} );
+		const colorFilterBlock = block.getByLabel(
+			'Block: Product Filter: Attribute (Beta)'
+		);
+		const expectedColorFilterOptions = [
+			'Blue',
+			'Green',
+			'Gray',
+			'Red',
+			'Yellow',
+		];
+		const colorFilterOptions = (
+			await colorFilterBlock.allInnerTexts()
+		 )[ 0 ].split( '\n' );
+		await expect( colorHeading ).toBeVisible();
+		await expect( colorFilterBlock ).toBeVisible();
+		await expect( colorFilterOptions ).toEqual(
+			expect.arrayContaining( expectedColorFilterOptions )
+		);
+
+		const ratingHeading = block.getByRole( 'document', {
+			name: 'Rating',
+		} );
+		const ratingFilterBlock = block.getByLabel(
+			'Block: Product Filter: Rating (Beta)'
+		);
+		await expect( ratingHeading ).toBeVisible();
+		await expect( ratingFilterBlock ).toBeVisible();
 	} );
 } );
