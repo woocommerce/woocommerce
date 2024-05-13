d5e4c7eeb2ecd3364d7c7f12b2e728313d5c8065
Dan Q <danq@automattic.com>
2024-05-13 09:41:32 +0100

    Switch wordWrap for overflowWrap
    Modern browesers interpret the former as an alias of the latter, including allowing the use of 'anywhere'. But the current version of https://www.npmjs.com/package/csstype only recognises 'anywhere' as valid for overflowWrap. Switching it prevents compilation errors, still exhibits the correct behavior.

--- plugins/woocommerce-admin/client/marketplace/components/my-subscriptions/table/rows/functions.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/my-subscriptions/table/rows/functions.tsx
@@ -226,7 +226,7 @@ export function nameAndStatus( subscription: Subscription ): TableRow {
 								subscription.owner_email
 							),
 							{
-								email: <strong style={ { wordWrap: 'anywhere' } }>email</strong>,
+								email: <strong style={ { overflowWrap: 'anywhere' } }>email</strong>,
 								link: (
 									<a
 										href={ MARKETPLACE_SHARING_PATH }
