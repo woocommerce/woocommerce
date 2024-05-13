08b2c2058dc092a0876c695ab4967fc684b1287a
Fernando Marichal <fernando.marichal@automattic.com>
2024-05-13 13:04:27 -0300

    Add AttributeControl to Attributes component

--- packages/js/product-editor/src/blocks/product-fields/attributes/edit.tsx
+++ packages/js/product-editor/src/blocks/product-fields/attributes/edit.tsx
@@ -5,6 +5,8 @@ import { BlockAttributes } from '@wordpress/blocks';
 import { createElement, useEffect } from '@wordpress/element';
 import { useWooBlockProps } from '@woocommerce/block-templates';
 import { ProductProductAttribute } from '@woocommerce/data';
+import { __ } from '@wordpress/i18n';
+import { recordEvent } from '@woocommerce/tracks';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore No types for this exist yet.
 // eslint-disable-next-line @woocommerce/dependency-group
@@ -13,7 +15,7 @@ import { useEntityProp, useEntityId } from '@wordpress/core-data';
 /**
  * Internal dependencies
  */
-import { Attributes as AttributesContainer } from '../../../components/attributes/attributes';
+import { AttributeControl } from '../../../components/attribute-control';
 import { ProductEditorBlockEditProps } from '../../../types';
 import { useProductAttributes } from '../../../hooks/use-product-attributes';
 
@@ -47,10 +49,59 @@ export function AttributesBlockEdit( {
 
 	return (
 		<div { ...blockProps }>
-			<AttributesContainer
-				attributeList={ attributeList }
+			<AttributeControl
+				value={ attributeList }
+				disabledAttributeIds={ entityAttributes
+					.filter( ( attr ) => !! attr.variation )
+					.map( ( attr ) => attr.id ) }
+				uiStrings={ {
+					disabledAttributeMessage: __(
+						'Already used in Variations',
+						'woocommerce'
+					),
+				} }
+				onAdd={ () => {
+					recordEvent(
+						'product_add_attributes_modal_add_button_click'
+					);
+				} }
 				onChange={ handleChange }
-				value={ entityAttributes }
+				onNewModalCancel={ () => {
+					recordEvent(
+						'product_add_attributes_modal_cancel_button_click'
+					);
+				} }
+				onNewModalOpen={ () => {
+					if ( ! attributeList.length ) {
+						recordEvent(
+							'product_add_first_attribute_button_click'
+						);
+						return;
+					}
+					recordEvent( 'product_add_attribute_button' );
+				} }
+				onAddAnother={ () => {
+					recordEvent(
+						'product_add_attributes_modal_add_another_attribute_button_click'
+					);
+				} }
+				onRemoveItem={ () => {
+					recordEvent(
+						'product_add_attributes_modal_remove_attribute_button_click'
+					);
+				} }
+				onRemove={ () =>
+					recordEvent(
+						'product_remove_attribute_confirmation_confirm_click'
+					)
+				}
+				onRemoveCancel={ () =>
+					recordEvent(
+						'product_remove_attribute_confirmation_cancel_click'
+					)
+				}
+				termsAutoSelection="first"
+				defaultVisibility={ true }
 			/>
 		</div>
 	);
