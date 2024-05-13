ac8034e65a039532e66fd19c963e4104c2c48e8f
Paul Sealock <psealock@gmail.com>
2024-05-14 11:41:35 +1200

    [Launch Your Store] Add e2e tests (#47239)
    * LYS spec
    
    * get homescreen check working
    
    * move over to shopper
    
    * basic test
    
    * get first test working
    
    * Store only coming soon mode - logged out
    
    * add logged in ttests
    
    * linter
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Remove skip from tests
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/launch-your-store.spec.js
@@ -0,0 +1,77 @@
+const { test, expect, request } = require( '@playwright/test' );
+const { setOption } = require( '../../utils/options' );
+
+test.describe( 'Launch Your Store front end - logged out', () => {
+	test.afterAll( async ( { baseURL } ) => {
+		try {
+			await setOption(
+				request,
+				baseURL,
+				'woocommerce_coming_soon',
+				'no'
+			);
+		} catch ( error ) {
+			console.log( error );
+		}
+	} );
+
+	test( 'Entire site coming soon mode', async ( { page, baseURL } ) => {
+		try {
+			await setOption(
+				request,
+				baseURL,
+				'woocommerce_coming_soon',
+				'yes'
+			);
+
+			await setOption(
+				request,
+				baseURL,
+				'woocommerce_store_pages_only',
+				'no'
+			);
+		} catch ( error ) {
+			console.log( error );
+		}
+
+		await page.goto( baseURL );
+
+		await expect(
+			page.getByText(
+				'Pardon our dust! We’re working on something amazing — check back soon!'
+			)
+		).toBeVisible();
+	} );
+
+	test( 'Store only coming soon mode', async ( { page, baseURL } ) => {
+		try {
+			await setOption(
+				request,
+				baseURL,
+				'woocommerce_coming_soon',
+				'yes'
+			);
+
+			await setOption(
+				request,
+				baseURL,
+				'woocommerce_store_pages_only',
+				'yes'
+			);
+		} catch ( error ) {
+			console.log( error );
+		}
+
+		await page.goto( baseURL + '/shop/' );
+
+		await expect(
+			page.getByText( 'Great things are on the horizon' )
+		).toBeVisible();
+
+		await expect(
+			page.getByText(
+				'Something big is brewing! Our store is in the works and will be launching soon!'
+			)
+		).toBeVisible();
+	} );
+} );
