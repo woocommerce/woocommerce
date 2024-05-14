d114f2d1184681cd716a9811a245ed7e4668af4b
Matt Sherman <matt.sherman@automattic.com>
2024-05-14 14:25:22 -0400

    Produc Editor API: Modal block editor more menu extensibility (description field) (#47436)
    * Const for more menu action item slot name
    
    * PluginMoreMenuItem implementation
    
    * Changelog
    
    * Use WP core MenuItem instead of WooCommerce MenuItem

--- packages/js/product-editor/src/components/iframe-editor/constants.ts
+++ packages/js/product-editor/src/components/iframe-editor/constants.ts
@@ -1,2 +1,4 @@
 export const SIDEBAR_COMPLEMENTARY_AREA_SCOPE =
 	'woocommerce/product-editor/modal-block-editor/sidebar';
+
+export const MORE_MENU_ACTION_ITEM_SLOT_NAME = `${ SIDEBAR_COMPLEMENTARY_AREA_SCOPE }/plugin-more-menu`;
