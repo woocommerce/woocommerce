72d71a0b6ae405b68cabc297523d3a209dc716f0
Fernando Marichal <fernando.marichal@automattic.com>
2024-05-14 17:27:03 -0300

    Create EmptyState component

---
+++ packages/js/product-editor/src/components/empty-state/empty-state.tsx
@@ -0,0 +1,36 @@
+/**
+ * External dependencies
+ */
+import { createElement } from '@wordpress/element';
+
+type EmptyStateProps = React.DetailedHTMLProps<
+	React.HTMLAttributes< HTMLDivElement >,
+	HTMLDivElement
+> & {
+	names?: string[];
+};
+
+export function EmptyState( { names = [], ...props }: EmptyStateProps ) {
+	return (
+		<div
+			{ ...props }
+			role="none"
+			className="woocommerce-product-empty-state"
+		>
+			{ names.map( ( name ) => (
+				<div
+					key={ name }
+					className="woocommerce-product-empty-state__row"
+				>
+					<div>{ name }</div>
+					<div>
+						<div className="woocommerce-product-empty-state__value" />
+					</div>
+					<div>
+						<div className="woocommerce-product-empty-state__actions" />
+					</div>
+				</div>
+			) ) }
+		</div>
+	);
+}
