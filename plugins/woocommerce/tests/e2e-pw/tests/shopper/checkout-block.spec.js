0e362bea4538656e4c60ab52a0b052742135dbeb
Niels Lange <info@nielslange.de>
2024-05-15 16:26:29 +0700

    Fix core e2e tests

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/checkout-block.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/checkout-block.spec.js
@@ -307,7 +307,7 @@ test.describe( 'Checkout Block page', () => {
 			page.getByLabel( 'Address', { exact: true } )
 		).toBeEditable();
 		await expect(
-			page.getByLabel( 'Apartment, suite, etc. (optional)' )
+			page.getByText( 'Add apartment, suite, etc.' )
 		).toBeEnabled();
 		await expect(
 			page.getByLabel( 'United States (US), Country/Region' )
