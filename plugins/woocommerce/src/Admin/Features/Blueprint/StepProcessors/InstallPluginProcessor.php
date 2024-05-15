ef906d1c532092a38b4227970a9f1d5bb72049e2
moon <moon.kyong@automattic.com>
2024-05-15 15:27:52 -0700

    Refactor -- process should accept schema, not the constructor

--- plugins/woocommerce/src/Admin/Features/Blueprint/StepProcessors/InstallPluginProcessor.php
+++ plugins/woocommerce/src/Admin/Features/Blueprint/StepProcessors/InstallPluginProcessor.php
@@ -6,14 +6,9 @@ use Automattic\WooCommerce\Admin\Features\Blueprint\StepProcessor;
 use Automattic\WooCommerce\Admin\PluginsHelper;
 
 class InstallPluginProcessor implements StepProcessor {
-	private $schema;
-	public function __construct( $schema ) {
-		$this->schema = $schema;
-	}
-
-	public function process() {
-		PluginsHelper::install_plugins( array( $this->schema->pluginZipFile->slug ) );
-		PluginsHelper::activate_plugins( array( $this->schema->pluginZipFile->slug ) );
+	public function process($schema) {
+		PluginsHelper::install_plugins( array( $schema->pluginZipFile->slug ) );
+		PluginsHelper::activate_plugins( array( $schema->pluginZipFile->slug ) );
 
 		return true;
 	}
