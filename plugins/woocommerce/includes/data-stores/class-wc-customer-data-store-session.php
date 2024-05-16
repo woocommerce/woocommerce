f568902da83320e81a484b51229fb7fa23375c4f
Nadir Seghir <nadir.seghir@gmail.com>
2024-05-16 13:21:47 +0200

    reduce code needed

--- plugins/woocommerce/includes/data-stores/class-wc-customer-data-store-session.php
+++ plugins/woocommerce/includes/data-stores/class-wc-customer-data-store-session.php
@@ -94,22 +94,17 @@ class WC_Customer_Data_Store_Session extends WC_Data_Store_WP implements WC_Cust
 				 * @param array $allowed_keys The allowed meta data keys.
 				 * @param WC_Customer $customer The customer object.
 				 */
-				$allowed_keys      = apply_filters( 'woocommerce_customer_allowed_session_meta_keys', array(), $customer );
-				$session_value     = array_map(
-					function ( $meta_data ) {
-						// Data comes to us a WC_Meta_Data, we cast it to an array to ensure it is serializable when doing WC()->session->set.
-							return array(
-								'key'   => $meta_data->key,
-								'value' => $meta_data->value,
-							);
-					},
-					array_filter(
-						$customer->get_meta_data(),
-						function ( $meta_data ) use ( $allowed_keys ) {
-								return in_array( $meta_data->key, $allowed_keys, true );
-						}
-					)
-				);
+				$allowed_keys = apply_filters( 'woocommerce_customer_allowed_session_meta_keys', array(), $customer );
+
+				$session_value = array();
+				foreach ( $customer->get_meta_data() as $meta_data ) {
+					if ( in_array( $meta_data->key, $allowed_keys, true ) ) {
+						$session_value[] = array(
+							'key'   => $meta_data->key,
+							'value' => $meta_data->value,
+						);
+					}
+				}
 				$data['meta_data'] = $session_value;
 			} else {
 				$session_value        = $customer->{"get_$function_key"}( 'edit' );
@@ -145,9 +140,8 @@ class WC_Customer_Data_Store_Session extends WC_Data_Store_WP implements WC_Cust
 				}
 				if ( ! empty( $data[ $session_key ] ) && is_callable( array( $customer, "set_{$function_key}" ) ) ) {
 					if ( 'meta_data' === $session_key ) {
-						$meta_data_values = is_serialized( $data[ $session_key ], true ) ? maybe_unserialize( $data[ $session_key ] ) : $data[ $session_key ];
-						if ( is_array( $meta_data_values ) ) {
-							foreach ( $meta_data_values as $meta_data_value ) {
+						if ( is_array( $data[ $session_key ] ) ) {
+							foreach ( $data[ $session_key ] as $meta_data_value ) {
 								if ( ! isset( $meta_data_value['key'], $meta_data_value['value'] ) ) {
 									continue;
 								}
