5f081bc00032dce11e94c51b17b421a25e877bf9
Nestor Soriano <konamiman@konamiman.com>
2024-05-16 15:44:54 +0200

    Mark get_woocommerce_api_url function as deprecated

--- plugins/woocommerce/includes/wc-core-functions.php
+++ plugins/woocommerce/includes/wc-core-functions.php
@@ -1109,6 +1109,8 @@ function wc_setcookie( $name, $value, $expire = 0, $secure = false, $httponly = 
  * See the setup_legacy_api_stub method in includes/class-woocommerce.php and:
  * https://developer.woocommerce.com/2023/10/03/the-legacy-rest-api-will-move-to-a-dedicated-extension-in-woocommerce-9-0/
  *
+ * @deprecated 9.0.0 The Legacy REST API has been removed from WooCommerce core.
+ *
  * @since 2.1
  * @param string $path an endpoint to include in the URL.
  * @return string the URL.
