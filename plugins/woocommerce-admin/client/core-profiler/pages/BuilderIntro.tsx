99a1e37a9f8cd869ce6cb6b1d603891e42b37bd5
paul sealock <psealock@gmail.com>
2024-05-16 20:28:52 +1200

    get elements on page

--- plugins/woocommerce-admin/client/core-profiler/pages/BuilderIntro.tsx
+++ plugins/woocommerce-admin/client/core-profiler/pages/BuilderIntro.tsx
@@ -1,1 +1,17 @@
-export const BuilderIntro = () => <div>hello Paul</div>;
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+
+export const BuilderIntro = () => (
+	<div className="woocommerce-profiler-builder-intro">
+		<h1>
+			{ __(
+				'Upload your Blueprint to provision your site',
+				'woocommerce'
+			) }{ ' ' }
+		</h1>
+		<Button variant="primary">{ __( 'Upload Blueprint' ) }</Button>
+	</div>
+);
