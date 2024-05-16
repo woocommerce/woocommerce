d305bcb096234d70c9404f37632fe907e79d5e5d
Nathan Schneider <nsschneider1@gmail.com>
2024-05-16 15:44:16 -0300

    Update tests

--- packages/js/product-editor/src/hooks/use-product-template/test/use-product-template.test.ts
+++ packages/js/product-editor/src/hooks/use-product-template/test/use-product-template.test.ts
@@ -26,6 +26,7 @@ describe( 'useProductTemplate', () => {
 					productData: {
 						type: 'simple',
 					},
+					priority: 10,
 				},
 				{
 					id: 'template-2',
@@ -38,6 +39,7 @@ describe( 'useProductTemplate', () => {
 					productData: {
 						type: 'grouped',
 					},
+					priority: 10,
 				},
 				{
 					id: 'template-3',
@@ -50,6 +52,7 @@ describe( 'useProductTemplate', () => {
 					productData: {
 						type: 'simple',
 					},
+					priority: 10,
 				},
 				{
 					id: 'standard-product-template',
@@ -62,6 +65,7 @@ describe( 'useProductTemplate', () => {
 					productData: {
 						type: 'simple',
 					},
+					priority: 10,
 				},
 				{
 					id: 'gift-card-product-template',
@@ -75,6 +79,7 @@ describe( 'useProductTemplate', () => {
 						type: 'simple',
 						meta_data: [ { key: '_gift_card', value: 'yes' } ],
 					},
+					priority: 10,
 				},
 			],
 		};
@@ -87,7 +92,7 @@ describe( 'useProductTemplate', () => {
 
 	it( 'should return the product template if it exists', () => {
 		const { result } = renderHook( () =>
-			useProductTemplate( 'template-3', { type: 'simple' } )
+			useProductTemplate( 'template-3', { type: 'simple' }, {} )
 		);
 
 		expect( result.current.productTemplate?.id ).toEqual( 'template-3' );
@@ -95,7 +100,7 @@ describe( 'useProductTemplate', () => {
 
 	it( 'should return the first product template with a matching type in the productData if no matching product template by id', () => {
 		const { result } = renderHook( () =>
-			useProductTemplate( 'invalid-template-id', { type: 'grouped' } )
+			useProductTemplate( 'invalid-template-id', { type: 'grouped' }, {} )
 		);
 
 		expect( result.current.productTemplate?.id ).toEqual( 'template-2' );
@@ -103,7 +108,7 @@ describe( 'useProductTemplate', () => {
 
 	it( 'should return the first product template with a matching type in the productData if no product template id is set', () => {
 		const { result } = renderHook( () =>
-			useProductTemplate( undefined, { type: 'grouped' } )
+			useProductTemplate( undefined, { type: 'grouped' }, {} )
 		);
 
 		expect( result.current.productTemplate?.id ).toEqual( 'template-2' );
@@ -111,25 +116,19 @@ describe( 'useProductTemplate', () => {
 
 	it( 'should return undefined if no matching product template by id or type', () => {
 		const { result } = renderHook( () =>
-			useProductTemplate( 'invalid-template-id', { type: 'external' } )
+			useProductTemplate(
+				'invalid-template-id',
+				{ type: 'external' },
+				{}
+			)
 		);
 
 		expect( result.current.productTemplate ).toBeUndefined();
 	} );
 
-	it( 'should use the standard product template if no templateId is provided', () => {
-		const { result } = renderHook( () =>
-			useProductTemplate( undefined, { type: 'simple' } )
-		);
-
-		expect( result.current.productTemplate?.id ).toEqual(
-			'standard-product-template'
-		);
-	} );
-
 	it( 'should use the product type to match if the product template id matches a template with a different product type', () => {
 		const { result } = renderHook( () =>
-			useProductTemplate( 'template-2', { type: 'simple' } )
+			useProductTemplate( 'template-2', { type: 'simple' }, {} )
 		);
 
 		expect( result.current.productTemplate?.id ).toEqual( 'template-1' );
@@ -137,10 +136,14 @@ describe( 'useProductTemplate', () => {
 
 	it( 'should select the product template with the most matching fields if there are multiple matching templates', () => {
 		const { result } = renderHook( () =>
-			useProductTemplate( 'invalid-template-id', {
-				type: 'simple',
-				meta_data: [ { key: '_gift_card', value: 'yes' } ],
-			} )
+			useProductTemplate(
+				'invalid-template-id',
+				{
+					type: 'simple',
+					meta_data: [ { key: '_gift_card', value: 'yes' } ],
+				},
+				{}
+			)
 		);
 
 		expect( result.current.productTemplate?.id ).toEqual(
