cebf8ffbbba615f382c084b970dca1cafc0146c2
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-05-17 18:38:48 +0100

    Bumped template version

--- plugins/woocommerce/templates/order/order-details.php
+++ plugins/woocommerce/templates/order/order-details.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 8.5.0
+ * @version 9.0.0
  *
  * @var bool $show_downloads Controls whether the downloads table should be rendered.
  */
