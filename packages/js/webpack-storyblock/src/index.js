340a75293ca6ea07c6760d4e4b745456a7fec65f
Sam Seay <samueljseay@gmail.com>
2024-05-18 16:23:38 +0800

    Embed the storyblock files within the project when building

--- packages/js/webpack-storyblock/src/index.js
+++ packages/js/webpack-storyblock/src/index.js
@@ -3,44 +3,129 @@ const path = require( 'path' );
 
 class WebpackStoryblockPlugin {
 	constructor( options ) {
-		this.options = { exclude: [ 'node_modules' ], ...options };
+		this.buildDir = path.resolve( __dirname, '../build' );
+
+		// TODO - user should be able to provide their own entry point.
+		const blockPath = path.resolve( this.buildDir, 'block/index.js' );
+
+		this.options = {
+			exclude: [ 'node_modules' ],
+			entryName: 'woocommerce-storyblock',
+			blockEntry: blockPath,
+			...options,
+		};
+
+		// TODO - validate options.
 		this.exclude = this.options.exclude;
+		this.entryName = this.options.entryName;
+		this.blockEntry = this.options.blockEntry;
+		this.storyIndexPath = path.resolve(
+			__dirname,
+			'../build/block/story-index.js'
+		);
+	}
+
+	makeBuildDirectories() {
+		const blockDir = path.resolve( this.buildDir, 'block' );
+
+		if ( ! fs.existsSync( blockDir ) ) {
+			fs.mkdirSync( blockDir, {
+				recursive: true,
+			} );
+		}
+
+		if ( ! fs.existsSync( path.join( blockDir, 'stories' ) ) ) {
+			fs.mkdirSync( path.join( blockDir, 'stories' ), {
+				recursive: true,
+			} );
+		}
+
+		// clear the directories
+		fs.rmdirSync( blockDir, { recursive: true } );
+		fs.mkdirSync( path.join( blockDir, 'stories' ), {
+			recursive: true,
+		} );
+
+		const blockAssets = fs.readdirSync(
+			path.resolve( __dirname, 'block' )
+		);
+
+		blockAssets.forEach( ( asset ) => {
+			const assetPath = path.resolve( __dirname, 'block', asset );
+			const assetContent = fs.readFileSync( assetPath );
+
+			fs.writeFileSync(
+				path.resolve( __dirname, '../build/block', asset ),
+				assetContent
+			);
+		} );
+
+		// make the file
+		fs.writeFileSync( this.storyIndexPath, '' );
 	}
 
 	apply( compiler ) {
-		const blockPath = path.resolve( __dirname, 'block/index.js' );
+		const logger = compiler.getInfrastructureLogger(
+			'WebpackStoryblockPlugin'
+		);
+		const context = compiler.options.context || process.cwd();
+		const stories = this.findStories( context );
 
-		// Dynamically add entry point
-		compiler.options.entry[ 'woocommerce-storyblock' ] = {
-			import: [ blockPath ],
+		this.makeBuildDirectories();
+
+		const outputPath = compiler.options.output.path;
+		const blockDir = path.resolve( outputPath, 'storybook-blocks/block' );
+
+		const indexContent = stories
+			.map( ( story ) => {
+				return `import '${ path
+					.relative( blockDir, story )
+					.replace( /\\/g, '/' ) }';`;
+			} )
+			.join( '\n' );
+
+		// now write the index file.
+		fs.writeFileSync( this.storyIndexPath, indexContent );
+
+		// make the build dir in the output path
+		fs.mkdirSync( path.resolve( outputPath, 'storybook-blocks' ), {
+			recursive: true,
+		} );
+
+		// clear the directory
+		fs.rmdirSync( path.resolve( outputPath, 'storybook-blocks' ), {
+			recursive: true,
+		} );
+
+		// copy the build dir
+		this.copyFiles(
+			this.buildDir,
+			path.join( outputPath, 'storybook-blocks' )
+		);
+
+		// Dynamically add entry points - we dynamically import the stories,
+		// so we force webpack to build them here with an index as an entry point.
+		compiler.options.entry[ `${ this.entryName }-story-index` ] = {
+			import: [
+				path.join(
+					outputPath,
+					'storybook-blocks',
+					'block',
+					'story-index.js'
+				),
+			],
 		};
+
+		compiler.options.entry[ this.entryName ] = {
+			import: [ this.blockEntry ],
+		};
+
+		// All this needs to be done before compilation, because we want the user's webpack config
+		// to compile these files as if they were part of the project.
 
 		compiler.hooks.emit.tapAsync(
 			'WebpackStoryblockPlugin',
 			( compilation, callback ) => {
-				const context = compiler.options.context || process.cwd();
-				const stories = this.findStories( context );
-
-				console.log( 'ACTIVATION' );
-				console.log( stories );
-
-				// const storiesContent = stories.map( ( story ) => {
-				// 	const storyContent = fs.readFileSync( story, 'utf-8' );
-				// 	return JSON.stringify( storyContent );
-				// } );
-
-				// const indexPath = path.resolve( __dirname, 'src/index.js' );
-				// const indexContent = `
-				//         import React from 'react';
-				//         import ReactDOM from 'react-dom';
-				//         import App from './App';
-
-				//         const stories = [${ storiesContent.join( ',' ) }];
-
-				//         ReactDOM.render(<App stories={stories} />, document.getElementById('root'));
-				//     `;
-
-				// fs.writeFileSync( indexPath, indexContent );
 				callback();
 			}
 		);
@@ -67,6 +152,27 @@ class WebpackStoryblockPlugin {
 		} );
 
 		return fileList;
+	}
+
+	copyFiles( srcDir, destDir ) {
+		const entries = fs.readdirSync( srcDir, { withFileTypes: true } );
+
+		entries.forEach( ( entry ) => {
+			const srcPath = path.join( srcDir, entry.name );
+			const destPath = path.join( destDir, entry.name );
+
+			if ( entry.isDirectory() ) {
+				// Create directory if it doesn't exist
+				if ( ! fs.existsSync( destPath ) ) {
+					fs.mkdirSync( destPath, { recursive: true } );
+				}
+				// Recurse into the directory
+				this.copyFiles( srcPath, destPath );
+			} else {
+				// Copy file
+				fs.copyFileSync( srcPath, destPath );
+			}
+		} );
 	}
 }
 
