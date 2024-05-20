c424e10e45c7095558db9cf2b23f402eb899e6f7
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-05-20 20:38:53 +0100

    Remove check for logged in user on the order received page for displaying billing/shipping addresses (#47477)
    * Remove check for logged in user on the order received page, enabling guest users with a valid link to visualize the order shipping/billing addresses.
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Update comment
    
    * Bumped template version
    
    * fix linting
    
    * ignore hook for the whole file
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Nadir Seghir <nadir.seghir@gmail.com>

--- plugins/woocommerce/templates/order/order-details.php
+++ plugins/woocommerce/templates/order/order-details.php
@@ -12,10 +12,12 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 8.5.0
+ * @version 9.0.0
  *
  * @var bool $show_downloads Controls whether the downloads table should be rendered.
  */
+
+ // phpcs:disable WooCommerce.Commenting.CommentHooks.MissingHookComment
 
 defined( 'ABSPATH' ) || exit;
 
@@ -25,10 +27,12 @@ if ( ! $order ) {
 	return;
 }
 
-$order_items           = $order->get_items( apply_filters( 'woocommerce_purchase_order_item_types', 'line_item' ) );
-$show_purchase_note    = $order->has_status( apply_filters( 'woocommerce_purchase_note_order_statuses', array( 'completed', 'processing' ) ) );
-$show_customer_details = is_user_logged_in() && $order->get_user_id() === get_current_user_id();
-$downloads             = $order->get_downloadable_items();
+$order_items        = $order->get_items( apply_filters( 'woocommerce_purchase_order_item_types', 'line_item' ) );
+$show_purchase_note = $order->has_status( apply_filters( 'woocommerce_purchase_note_order_statuses', array( 'completed', 'processing' ) ) );
+$downloads          = $order->get_downloadable_items();
+
+// We make sure the order belongs to the user. This will also be true if the user is a guest, and the order belongs to a guest (userID === 0).
+$show_customer_details = $order->get_user_id() === get_current_user_id();
 
 if ( $show_downloads ) {
 	wc_get_template(
