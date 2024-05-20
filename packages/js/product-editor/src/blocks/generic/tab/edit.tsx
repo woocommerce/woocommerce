dd85da95fed252f230d8e2b91ed6cd6016d8e509
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-05-20 13:46:07 -0400

    Defer the tab content render 500ms to reduce the total blocking time

--- packages/js/product-editor/src/blocks/generic/tab/edit.tsx
+++ packages/js/product-editor/src/blocks/generic/tab/edit.tsx
@@ -3,7 +3,7 @@
  */
 import { InnerBlocks } from '@wordpress/block-editor';
 import classnames from 'classnames';
-import { createElement } from '@wordpress/element';
+import { createElement, useEffect, useState } from '@wordpress/element';
 import type { BlockAttributes } from '@wordpress/blocks';
 import { useWooBlockProps } from '@woocommerce/block-templates';
 
@@ -25,20 +25,29 @@ export function TabBlockEdit( {
 	context,
 }: ProductEditorBlockEditProps< TabBlockAttributes > ) {
 	const blockProps = useWooBlockProps( attributes );
-	const {
-		id,
-		title,
-		_templateBlockOrder: order,
-		isSelected: contextIsSelected,
-	} = attributes;
-	const isSelected = context.selectedTab === id;
-	if ( isSelected !== contextIsSelected ) {
-		setAttributes( { isSelected } );
-	}
+	const { id, title, _templateBlockOrder: order, isSelected } = attributes;
 
 	const classes = classnames( 'wp-block-woocommerce-product-tab__content', {
 		'is-selected': isSelected,
 	} );
+
+	const [ canRenderChildren, setCanRenderChildren ] = useState( false );
+
+	useEffect( () => {
+		if ( ! context.selectedTab ) return;
+
+		const isSelectedInContext = context.selectedTab === id;
+
+		setAttributes( { isSelected: isSelectedInContext } );
+
+		if ( isSelectedInContext ) {
+			setCanRenderChildren( true );
+			return;
+		}
+
+		const timeoutId = setTimeout( setCanRenderChildren, 500, true );
+		return () => clearTimeout( timeoutId );
+	}, [ context.selectedTab, id ] );
 
 	return (
 		<div { ...blockProps }>
@@ -51,9 +60,11 @@ export function TabBlockEdit( {
 				role="tabpanel"
 				className={ classes }
 			>
-				{ /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */ }
-				{ /* @ts-ignore Content only template locking does exist for this property. */ }
-				<InnerBlocks templateLock="contentOnly" />
+				{ canRenderChildren && (
+					/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+					/* @ts-ignore Content only template locking does exist for this property. */
+					<InnerBlocks templateLock="contentOnly" />
+				) }
 			</div>
 		</div>
 	);
