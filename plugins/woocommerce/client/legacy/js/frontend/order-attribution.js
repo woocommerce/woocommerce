9f628747e643885319af23a9eb0b657231c759a9
Justin Palmer <228780+layoutd@users.noreply.github.com>
2024-05-21 01:56:14 +0200

    Add filter to apply Base64 encoding to order attribution cookies (#47597)
    * Add filter to set Sourcebuster JS base64 configuration value
    
    * Use Sourcebuster v1.2.0
    
    * Changelog
    
    * Reduce line length for linter

--- plugins/woocommerce/client/legacy/js/frontend/order-attribution.js
+++ plugins/woocommerce/client/legacy/js/frontend/order-attribution.js
@@ -82,6 +82,7 @@
 			sbjs.init( {
 				lifetime: Number( params.lifetime ),
 				session_length: Number( params.session ),
+				base64: Boolean( params.base64 ),
 				timezone_offset: '0', // utc
 			} );
 		}
@@ -184,7 +185,10 @@
 					if( input ) {
 						input.value = stringifyFalsyInputValue( this.values[ fieldName ] );
 					} else {
-						console.warn( `Field "${fieldName}" not found. Most likely, the '<wc-order-attribution-inputs>' element was manipulated.`);
+						console.warn(
+							`Field "${fieldName}" not found. ` +
+							`Most likely, the '<wc-order-attribution-inputs>' element was manipulated.`
+						);
 					}
 				}
 			}
