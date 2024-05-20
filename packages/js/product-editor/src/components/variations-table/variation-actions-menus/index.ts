e85fe25cfdb7087539831a36fb78eb19fc254f41
Nathan Schneider <nsschneider1@gmail.com>
2024-05-20 14:45:23 -0300

    Remove outdated export and export menugroup

--- packages/js/product-editor/src/components/variations-table/variation-actions-menus/index.ts
+++ packages/js/product-editor/src/components/variations-table/variation-actions-menus/index.ts
@@ -1,5 +1,5 @@
 export * from './multiple-update-menu';
 export * from './single-update-menu';
 export * from './variation-quick-update-menu-item';
-export * from './variation-quick-update-fill';
+export * from './variation-quick-update-menu-group';
 export * from './types';
