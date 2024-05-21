242b6d4d5367ceef8f77912ee6561650fbe1e8bc
Fernando Marichal <fernando.marichal@automattic.com>
2024-05-21 17:24:40 -0300

    Add Playwright performance tests to Product editor (#47590)
    * Add product editor tests
    
    * Fix post editor lint
    
    * Add changelog
    
    * Fix lint error
    
    * Fix Typing test
    
    * Add log to resultsFiles
    
    * Fix comment
    
    * Remove PerfUtils

--- plugins/woocommerce/tests/metrics/specs/product-editor.spec.js
+++ plugins/woocommerce/tests/metrics/specs/product-editor.spec.js
@@ -8,51 +8,55 @@ import { test, Metrics } from '@wordpress/e2e-test-utils-playwright';
 /**
  * Internal dependencies
  */
-import { PerfUtils } from '../fixtures';
 import { median } from '../utils';
+import { toggleBlockProductEditor } from '../../e2e-pw/utils/simple-products';
 
 // See https://github.com/WordPress/gutenberg/issues/51383#issuecomment-1613460429
 const BROWSER_IDLE_WAIT = 1000;
+const NEW_EDITOR_ADD_PRODUCT_URL =
+	'wp-admin/admin.php?page=wc-admin&path=%2Fadd-product';
 
-const results = {};
+const results = {
+	totalBlockingTime: [],
+	cumulativeLayoutShift: [],
+	largestContentfulPaint: [],
+	type: [],
+};
 
-async function setPreferences( page, context, preferences ) {
-	await page.waitForFunction( () => window?.wp?.data );
+async function getTotalBlockingTime( page, idleWait ) {
+	const totalBlockingTime = await page.evaluate( async ( waitTime ) => {
+		return new Promise( ( resolve ) => {
+			const longTaskEntries = [];
+			// Create a performance observer to observe long task entries
+			new PerformanceObserver( ( list ) => {
+				const entries = list.getEntries();
+				// Store each long task entry in the longTaskEntries array
+				entries.forEach( ( entry ) => longTaskEntries.push( entry ) );
+			} ).observe( { type: 'longtask', buffered: true } );
 
-	await page.evaluate(
-		async ( props ) => {
-			for ( const [ key, value ] of Object.entries(
-				props.preferences
-			) ) {
-				await window.wp.data
-					.dispatch( 'core/preferences' )
-					.set( props.context, key, value );
-			}
-		},
-		{ context, preferences }
-	);
+			// Give some time to collect entries
+			setTimeout( () => {
+				// Calculate the total blocking time by summing the durations of all long tasks
+				const tbt = longTaskEntries.reduce(
+					( acc, entry ) => acc + entry.duration,
+					0
+				);
+				resolve( tbt );
+			}, waitTime );
+		} );
+	}, idleWait );
+	return totalBlockingTime;
 }
 
-async function editPost( admin, page, postId ) {
-	const query = new URLSearchParams();
-	query.set( 'post', String( postId ) );
-	query.set( 'action', 'edit' );
-
-	await admin.visitAdminPage( 'post.php', query.toString() );
-	await setPreferences( page, 'core/edit-post', {
-		welcomeGuide: false,
-		fullscreenMode: false,
-	} );
-}
-
-test.describe( 'Editor Performance', () => {
+test.describe( 'Product editor performance', () => {
 	test.use( {
-		perfUtils: async ( { page }, use ) => {
-			await use( new PerfUtils( { page } ) );
-		},
 		metrics: async ( { page }, use ) => {
 			await use( new Metrics( { page } ) );
 		},
+	} );
+
+	test.beforeEach( async ( { page } ) => {
+		await toggleBlockProductEditor( 'enable', page );
 	} );
 
 	test.afterAll( async ( {}, testInfo ) => {
@@ -67,33 +71,38 @@ test.describe( 'Editor Performance', () => {
 	} );
 
 	test.describe( 'Loading', () => {
-		let draftId = null;
-
-		test( 'Setup the test post', async ( { admin, perfUtils } ) => {
-			await admin.createNewPost();
-			await perfUtils.loadBlocksForLargePost();
-			draftId = await perfUtils.saveDraft();
-		} );
-
 		const samples = 2;
 		const throwaway = 1;
 		const iterations = samples + throwaway;
 		for ( let i = 1; i <= iterations; i++ ) {
 			test( `Run the test (${ i } of ${ iterations })`, async ( {
-				admin,
 				page,
-				perfUtils,
 				metrics,
 			} ) => {
-				// Open the test draft.
-				await editPost( admin, page, draftId );
-				const canvas = await perfUtils.getCanvas();
+				await page.goto( NEW_EDITOR_ADD_PRODUCT_URL );
 
-				// Wait for the first block.
-				await canvas.locator( '.wp-block' ).first().waitFor();
+				// Wait for the Preview button.
+				await page
+					.locator( '[aria-label="Preview in new tab"]' )
+					.first()
+					.waitFor();
 
 				// Get the durations.
 				const loadingDurations = await metrics.getLoadingDurations();
+
+				// Measure CLS
+				const cumulativeLayoutShift =
+					await metrics.getCumulativeLayoutShift();
+
+				// Measure LCP
+				const largestContentfulPaint =
+					await metrics.getLargestContentfulPaint();
+
+				// Measure TBT
+				const totalBlockingTime = await getTotalBlockingTime(
+					page,
+					BROWSER_IDLE_WAIT
+				);
 
 				// Save the results.
 				if ( i > throwaway ) {
@@ -109,30 +118,32 @@ test.describe( 'Editor Performance', () => {
 							results[ metricKey ].push( duration );
 						}
 					);
+					results.totalBlockingTime = results.tbt || [];
+					results.totalBlockingTime.push( totalBlockingTime );
+					results.cumulativeLayoutShift =
+						results.cumulativeLayoutShift || [];
+					results.cumulativeLayoutShift.push( cumulativeLayoutShift );
+					results.largestContentfulPaint =
+						results.largestContentfulPaint || [];
+					results.largestContentfulPaint.push(
+						largestContentfulPaint
+					);
 				}
 			} );
 		}
 	} );
 
 	test.describe( 'Typing', () => {
-		let draftId = null;
+		test( 'Run the test', async ( { page, metrics } ) => {
+			await page.goto( NEW_EDITOR_ADD_PRODUCT_URL );
 
-		test( 'Setup the test post', async ( { admin, perfUtils, editor } ) => {
-			await admin.createNewPost();
-			await perfUtils.loadBlocksForLargePost();
-			await editor.insertBlock( { name: 'core/paragraph' } );
-			draftId = await perfUtils.saveDraft();
-			console.log( draftId );
-		} );
+			// Wait for the Preview button.
+			await page
+				.locator( '[aria-label="Preview in new tab"]' )
+				.first()
+				.waitFor();
 
-		test( 'Run the test', async ( { admin, page, perfUtils, metrics } ) => {
-			await editPost( admin, page, draftId );
-			await perfUtils.disableAutosave();
-			const canvas = await perfUtils.getCanvas();
-
-			const paragraph = canvas.getByRole( 'document', {
-				name: /Empty block/i,
-			} );
+			const input = page.getByPlaceholder( 'e.g. 12 oz Coffee Mug' );
 
 			// The first character typed triggers a longer time (isTyping change).
 			// It can impact the stability of the metric, so we exclude it. It
@@ -144,8 +155,8 @@ test.describe( 'Editor Performance', () => {
 			// Start tracing.
 			await metrics.startTracing();
 
-			// Type the testing sequence into the empty paragraph.
-			await paragraph.type( 'x'.repeat( iterations ), {
+			// Type the testing sequence into the empty input.
+			await input.type( 'x'.repeat( iterations ), {
 				delay: BROWSER_IDLE_WAIT,
 				// The extended timeout is needed because the typing is very slow
 				// and the `delay` value itself does not extend it.
