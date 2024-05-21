8fdbc58d02c2717cdbd83f7805847f1f8971ae44
Moon <moon.kyong@automattic.com>
2024-05-22 07:15:33 +1200

    Run wc_update_900_add_launch_your_store_tour_option on WC 9.0 update (#47634)
    * Run wc_update_900_add_launch_your_store_tour_option on WC 9.0 update
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Use add_option
    
    * Lint fixes
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/includes/wc-update-functions.php
+++ plugins/woocommerce/includes/wc-update-functions.php
@@ -2724,3 +2724,10 @@ function wc_update_891_create_plugin_autoinstall_history_option() {
 		}
 	}
 }
+
+/**
+ * Add woocommerce_show_lys_tour.
+ */
+function wc_update_900_add_launch_your_store_tour_option() {
+	add_option( 'woocommerce_show_lys_tour', 'yes' );
+}
