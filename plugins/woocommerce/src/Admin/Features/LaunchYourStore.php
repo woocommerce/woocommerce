b7c26dc62c98fd41e7bfe53e77e479475ae19e6f
paul sealock <psealock@gmail.com>
2024-05-22 14:19:55 +1200

    lint

--- plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
+++ plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
@@ -78,12 +78,12 @@ class LaunchYourStore {
 			return $settings;
 		}
 
-		$current_screen  = get_current_screen();
-		$is_setting_page = $current_screen && 'woocommerce_page_wc-settings' === $current_screen->id;
+		$current_screen         = get_current_screen();
+		$is_setting_page        = $current_screen && 'woocommerce_page_wc-settings' === $current_screen->id;
 		$is_woopayments_connect = isset( $_GET['path'] ) &&
-								  isset( $_GET['page'] ) &&
-								  ( '/payments/connect' === $_GET['path'] || '/payments/onboarding' === $_GET['path'] ) &&
-								  'wc-admin' === $_GET['page'];
+								isset( $_GET['page'] ) &&
+								( '/payments/connect' === sanitize_text_field( $_GET['path'] ) || '/payments/onboarding' === sanitize_text_field( $_GET['path'] ) ) &&
+								'wc-admin' === $_GET['page'];
 
 		if ( $is_setting_page || $is_woopayments_connect ) {
 			// Regnerate the share key if it's not set.
