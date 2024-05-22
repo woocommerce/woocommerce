531924e6633233d29fd3b9d6141cc90763ea4979
Akeda Bagus <akeda.bagus@automattic.com>
2024-05-22 14:26:30 +0700

    Rename dir for the modal script

--- plugins/woocommerce-admin/webpack.config.js
+++ plugins/woocommerce-admin/webpack.config.js
@@ -78,6 +78,7 @@ const wpAdminScripts = [
 	'woo-enable-autorenew',
 	'woo-renew-subscription',
 	'woo-subscriptions-notice',
+	'woo-check-subscription',
 ];
 const getEntryPoints = () => {
 	const entryPoints = {
