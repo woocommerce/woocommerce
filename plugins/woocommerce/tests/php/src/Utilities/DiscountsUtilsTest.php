f2ee9ee58103854ff0941f8ee24d2a5f81a075ba
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-05-22 17:13:45 +0100

    Linting.

--- plugins/woocommerce/tests/php/src/Utilities/DiscountsUtilsTest.php
+++ plugins/woocommerce/tests/php/src/Utilities/DiscountsUtilsTest.php
@@ -12,23 +12,23 @@ class DiscountsUtilTest extends \WC_Unit_Test_Case {
 	 * @testdox `is_coupon_emails_allowed` should return true/false based on direct match, wildcard match, case sensitivity, empty allowed list, and special characters.
 	 */
 	public function test_is_coupon_emails_allowed() {
-		// Direct match
+		// Direct match.
 		$this->assertEquals( true, DiscountsUtil::is_coupon_emails_allowed( array( 'customer@wc.local' ), array( 'customer@wc.local' ) ) );
 		$this->assertEquals( false, DiscountsUtil::is_coupon_emails_allowed( array( 'customer@wc.local' ), array( 'customer2@wc.local' ) ) );
 
-		// Wildcard match
+		// Wildcard match.
 		$this->assertEquals( true, DiscountsUtil::is_coupon_emails_allowed( array( 'customer@wc.local' ), array( '*.local' ) ) );
 		$this->assertEquals( false, DiscountsUtil::is_coupon_emails_allowed( array( 'customer@wc.local' ), array( '*.test' ) ) );
 
-		// Case sensitivity
+		// Case sensitivity.
 		$this->assertEquals( false, DiscountsUtil::is_coupon_emails_allowed( array( 'Customer@WC.local' ), array( 'customer@wc.local' ) ) );
 		$this->assertEquals( false, DiscountsUtil::is_coupon_emails_allowed( array( 'customer@wc.local' ), array( 'CUSTOMER@WC.LOCAL' ) ) );
 
-		// Empty allowed list
+		// Empty allowed list.
 		$this->assertEquals( false, DiscountsUtil::is_coupon_emails_allowed( array( 'customer@wc.local' ), array() ) );
 		$this->assertEquals( false, DiscountsUtil::is_coupon_emails_allowed( array(), array( 'customer@wc.local' ) ) );
 
-		// Special characters in email
+		// Special characters in email.
 		$this->assertEquals( false, DiscountsUtil::is_coupon_emails_allowed( array( 'special@example.com' ), array( 'special+char@example.com' ) ) );
 	}
 }
