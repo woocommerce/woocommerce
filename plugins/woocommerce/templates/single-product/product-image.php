0e69ccaf012aea8e31016884dcf8df6eecd60202
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-05-22 15:57:09 +0200

    Still add placeholder class for extra backwards compatibility

--- plugins/woocommerce/templates/single-product/product-image.php
+++ plugins/woocommerce/templates/single-product/product-image.php
@@ -43,7 +43,7 @@ $wrapper_classes   = apply_filters(
 		if ( $post_thumbnail_id ) {
 			$html = wc_get_gallery_image_html( $post_thumbnail_id, true );
 		} else {
-			$wrapper_classname = $has_images ? 'woocommerce-product-gallery__image' : 'woocommerce-product-gallery__image--placeholder';
+			$wrapper_classname = $has_images ? 'woocommerce-product-gallery__image woocommerce-product-gallery__image--placeholder' : 'woocommerce-product-gallery__image--placeholder';
 			$html              = sprintf( '<div class="%s">', esc_attr( $wrapper_classname ) );
 			$html             .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
 			$html             .= '</div>';
