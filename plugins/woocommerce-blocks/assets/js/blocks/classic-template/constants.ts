493bb3c184183bacae4a48aa8c54a479dd4b287e
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-05-22 09:21:51 +0200

    Update classic templates descriptions

--- plugins/woocommerce-blocks/assets/js/blocks/classic-template/constants.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/classic-template/constants.ts
@@ -28,37 +28,59 @@ export const TEMPLATES: TemplateDetails = {
 	'single-product': {
 		type: TYPES.singleProduct,
 		title: __( 'Product (Classic)', 'woocommerce' ),
+		description: __( 'Displays the PHP product page.', 'woocommerce' ),
 		placeholder: PLACEHOLDERS.singleProduct,
 	},
 	'archive-product': {
 		type: TYPES.productCatalog,
 		title: __( 'Product Grid (Classic)', 'woocommerce' ),
+		description: __(
+			'Displays the PHP product grid page. ',
+			'woocommerce'
+		),
 		placeholder: PLACEHOLDERS.archiveProduct,
 	},
 	'taxonomy-product_cat': {
 		type: TYPES.productTaxonomy,
-		title: __( 'Product Taxonomy (Classic)', 'woocommerce' ),
+		title: __( 'Product Category (Classic)', 'woocommerce' ),
+		description: __(
+			'Displays the PHP product category page.',
+			'woocommerce'
+		),
 		placeholder: PLACEHOLDERS.archiveProduct,
 	},
 	'taxonomy-product_tag': {
 		type: TYPES.productTaxonomy,
 		title: __( 'Product Tag (Classic)', 'woocommerce' ),
+		description: __( 'Displays the PHP product tag page.', 'woocommerce' ),
 		placeholder: PLACEHOLDERS.archiveProduct,
 	},
 	'taxonomy-product_attribute': {
 		type: TYPES.productTaxonomy,
 		title: __( 'Product Attribute (Classic)', 'woocommerce' ),
+		description: __(
+			'Displays the PHP product attribute page.',
+			'woocommerce'
+		),
 		placeholder: PLACEHOLDERS.archiveProduct,
 	},
 	// Since that it is a fallback value, it has to be the last one.
 	'taxonomy-product': {
 		type: TYPES.productTaxonomy,
 		title: __( "Product's Custom Taxonomy (Classic)", 'woocommerce' ),
+		description: __(
+			"Displays the PHP product's custom taxonomy page.",
+			'woocommerce'
+		),
 		placeholder: PLACEHOLDERS.archiveProduct,
 	},
 	'product-search-results': {
 		type: TYPES.productSearchResults,
 		title: __( 'Product Search Results (Classic)', 'woocommerce' ),
+		description: __(
+			'Displays the PHP product search results.',
+			'woocommerce'
+		),
 		placeholder: PLACEHOLDERS.archiveProduct,
 	},
 	'checkout-header': {
