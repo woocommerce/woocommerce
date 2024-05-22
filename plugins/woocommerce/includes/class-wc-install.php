10ee3a58d7031b4b8ffeccab0cc12b3945fdd7be
Ilyas Foo <foo.ilyas@gmail.com>
2024-05-22 17:39:32 +0800

    Update plugins/woocommerce/includes/class-wc-install.php
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>

--- plugins/woocommerce/includes/class-wc-install.php
+++ plugins/woocommerce/includes/class-wc-install.php
@@ -425,7 +425,8 @@ class WC_Install {
 			WC_Admin_Notices::add_notice( 'update', true );
 		}
 		if ( ! empty( $_GET['return_url'] ) ) { // WPCS: input var ok.
-			wp_safe_redirect( $_GET['return_url'] ); // WPCS: input var ok.
+			$return_url = esc_url_raw( wp_unslash( $_GET['return_url'] ) );
+			wp_safe_redirect( $return_url ); // WPCS: input var ok.
 		}
 	}
 
