6b2119d25df1461cc805da2c0ee9f236b5d866b7
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-05-23 14:35:31 +0200

    Add e2e test

--- plugins/woocommerce/tests/e2e-pw/tests/shopper/cart.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/shopper/cart.spec.js
@@ -1,5 +1,6 @@
 const { test, expect } = require( '@playwright/test' );
 const wcApi = require( '@woocommerce/woocommerce-rest-api' ).default;
+const { addAProductToCart } = require( '../../utils/cart' );
 
 const productName = `Cart product test ${ Date.now() }`;
 const productPrice = '13.99';
@@ -96,6 +97,54 @@ test.describe( 'Cart page', () => {
 		await page.goto( '/cart/' );
 		await expect( page.locator( 'td.product-name' ) ).toContainText(
 			productName
+		);
+	} );
+
+	test( 'should add only one product to the cart with AJAX add to cart buttons disabled and "Geolocate (with page caching support)" as the default customer location', async ( {
+		page,
+		baseURL,
+	} ) => {
+		// Set settings combination that allowed reproducing the bug.
+		// @see https://github.com/woocommerce/woocommerce/issues/33077
+		const api = new wcApi( {
+			url: baseURL,
+			consumerKey: process.env.CONSUMER_KEY,
+			consumerSecret: process.env.CONSUMER_SECRET,
+			version: 'wc/v3',
+		} );
+		await api.put(
+			'settings/general/woocommerce_default_customer_address',
+			{
+				value: 'geolocation_ajax',
+			}
+		);
+		await api.put(
+			'settings/products/woocommerce_enable_ajax_add_to_cart',
+			{
+				value: 'no',
+			}
+		);
+		await addAProductToCart( page, productId );
+		await page.goto( '/cart/' );
+		await expect( page.locator( 'td.product-name' ) ).toContainText(
+			productName
+		);
+		await expect( page.getByLabel( 'Product quantity' ) ).toHaveValue(
+			'1'
+		);
+
+		// Reset settings.
+		await api.put(
+			'settings/general/woocommerce_default_customer_address',
+			{
+				value: '',
+			}
+		);
+		await api.put(
+			'settings/products/woocommerce_enable_ajax_add_to_cart',
+			{
+				value: 'yes',
+			}
 		);
 	} );
 
