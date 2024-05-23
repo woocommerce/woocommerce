528aa30444ec76fd7f30c62c368a071688a790e9
Nathan Schneider <nsschneider1@gmail.com>
2024-05-23 10:13:03 -0300

    Remove outdated tests

--- packages/js/product-editor/src/hooks/use-product-template/test/use-product-template.test.ts
+++ packages/js/product-editor/src/hooks/use-product-template/test/use-product-template.test.ts
@@ -93,51 +93,11 @@ describe( 'useProductTemplate', () => {
 		expect( result.current.productTemplate?.id ).toEqual( 'template-3' );
 	} );
 
-	it( 'should return the first product template with a matching type in the productData if no matching product template by id', () => {
-		const { result } = renderHook( () =>
-			useProductTemplate( 'invalid-template-id' )
-		);
-
-		expect( result.current.productTemplate?.id ).toEqual( 'template-2' );
-	} );
-
-	it( 'should return the first product template with a matching type in the productData if no product template id is set', () => {
-		const { result } = renderHook( () => useProductTemplate( undefined ) );
-
-		expect( result.current.productTemplate?.id ).toEqual( 'template-2' );
-	} );
-
 	it( 'should return undefined if no matching product template by id or type', () => {
 		const { result } = renderHook( () =>
 			useProductTemplate( 'invalid-template-id' )
 		);
 
 		expect( result.current.productTemplate ).toBeUndefined();
-	} );
-
-	it( 'should use the standard product template if no templateId is provided', () => {
-		const { result } = renderHook( () => useProductTemplate( undefined ) );
-
-		expect( result.current.productTemplate?.id ).toEqual(
-			'standard-product-template'
-		);
-	} );
-
-	it( 'should use the product type to match if the product template id matches a template with a different product type', () => {
-		const { result } = renderHook( () =>
-			useProductTemplate( 'template-2' )
-		);
-
-		expect( result.current.productTemplate?.id ).toEqual( 'template-1' );
-	} );
-
-	it( 'should select the product template with the most matching fields if there are multiple matching templates', () => {
-		const { result } = renderHook( () =>
-			useProductTemplate( 'invalid-template-id' )
-		);
-
-		expect( result.current.productTemplate?.id ).toEqual(
-			'gift-card-product-template'
-		);
 	} );
 } );
