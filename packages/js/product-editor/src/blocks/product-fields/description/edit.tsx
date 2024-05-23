35eb99c55d436b7e489ed73582173fa2a856bfb3
Matt Sherman <matt@jam123.com>
2024-05-23 08:25:51 -0400

    Explicitly check whether the only block is a paragraph

--- packages/js/product-editor/src/blocks/product-fields/description/edit.tsx
+++ packages/js/product-editor/src/blocks/product-fields/description/edit.tsx
@@ -64,9 +64,27 @@ export function getContentFromFreeform(
  */
 function clearDescriptionIfEmpty( blocks: BlockInstance[] ) {
 	if ( blocks.length === 1 ) {
-		const { content } = blocks[ 0 ].attributes;
-		if ( ! content || ! content.trim() ) {
-			return [];
+		const block = blocks[ 0 ];
+		const isParagraph = block.name === 'core/paragraph';
+
+		if ( isParagraph ) {
+			// dropCap is set to false by default; we don't care what it is set to
+			// when determining if a paragraph block is empty
+			const { content, dropCap, backgroundColor, ...attributes } =
+				block.attributes;
+			const isContentEmpty = ! content || ! content.trim();
+			// When the background is cleared, the backgroundColor is set to undefined,
+			// so we need to check if it is actually set to a value
+			const isBackgroundColorSet = !! backgroundColor;
+			const isAttributesSet = Object.keys( attributes ).length > 0;
+
+			if (
+				isContentEmpty &&
+				! isBackgroundColorSet &&
+				! isAttributesSet
+			) {
+				return [];
+			}
 		}
 	}
 	return blocks;
