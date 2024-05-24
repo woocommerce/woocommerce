8ab74e34235ba6153d5dc41a1f4f11fd647c549e
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-05-24 09:08:57 -0400

    Remove ProductSettings component

--- plugins/woocommerce-admin/client/products/layout/product-form-header.tsx
+++ plugins/woocommerce-admin/client/products/layout/product-form-header.tsx
@@ -2,12 +2,10 @@
  * Internal dependencies
  */
 import { ProductMoreMenu } from '../product-more-menu';
-import { ProductSettings } from '../product-settings';
 
 export const ProductFormHeader: React.FC = () => {
 	return (
 		<>
-			<ProductSettings />
 			<ProductMoreMenu />
 		</>
 	);
