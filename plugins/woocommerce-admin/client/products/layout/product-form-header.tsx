2fc8402c9420b34f901e84173a17c2a84beb946d
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-05-24 09:13:11 -0400

    Remove ProductMoreMenu component

--- plugins/woocommerce-admin/client/products/layout/product-form-header.tsx
+++ plugins/woocommerce-admin/client/products/layout/product-form-header.tsx
@@ -1,12 +1,7 @@
 /**
  * Internal dependencies
  */
-import { ProductMoreMenu } from '../product-more-menu';
 
 export const ProductFormHeader: React.FC = () => {
-	return (
-		<>
-			<ProductMoreMenu />
-		</>
-	);
+	return <></>;
 };
