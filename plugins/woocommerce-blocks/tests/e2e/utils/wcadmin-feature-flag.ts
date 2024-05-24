7f2b2f27b772af0d05033801849f4fed49fb7c31
Sam Seay <samueljseay@gmail.com>
2024-05-24 13:58:34 +0800

    improve logic for enableFeatureFlag

--- plugins/woocommerce-blocks/tests/e2e/utils/wcadmin-feature-flag.ts
+++ plugins/woocommerce-blocks/tests/e2e/utils/wcadmin-feature-flag.ts
@@ -4,13 +4,18 @@
 import { cli } from './cli';
 
 export const enableFeatureFlag = async ( featureFlag: string ) => {
-	const { stdout } = await cli(
-		'npm run wp-env run tests-cli -- wp option get wc_admin_helper_feature_values'
-	);
+	let featureFlagValues = {};
 
 	try {
-		const featureFlagValues = JSON.parse( stdout );
+		const { stdout } = await cli(
+			'npm run wp-env run tests-cli -- wp option get wc_admin_helper_feature_values --json'
+		);
 
+		featureFlagValues = JSON.parse( stdout );
+		// wp option get will fail if the option does not exist, so we catch the error and ignore it.
+	} catch ( error ) {}
+
+	try {
 		await cli(
 			`npm run wp-env run tests-cli -- wp option update wc_admin_helper_feature_values '${ JSON.stringify(
 				{ ...featureFlagValues, [ featureFlag ]: 1 }
