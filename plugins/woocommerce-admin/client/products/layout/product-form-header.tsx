de5eec7178113f99fe77b45a392632b2f1138de0
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-05-24 09:07:45 -0400

    Remove ProductTitle component

--- plugins/woocommerce-admin/client/products/layout/product-form-header.tsx
+++ plugins/woocommerce-admin/client/products/layout/product-form-header.tsx
@@ -3,12 +3,10 @@
  */
 import { ProductMoreMenu } from '../product-more-menu';
 import { ProductSettings } from '../product-settings';
-import { ProductTitle } from '../product-title';
 
 export const ProductFormHeader: React.FC = () => {
 	return (
 		<>
-			<ProductTitle />
 			<ProductSettings />
 			<ProductMoreMenu />
 		</>
--- plugins/woocommerce-admin/client/products/layout/product-variation-form-header.tsx
+++ plugins/woocommerce-admin/client/products/layout/product-variation-form-header.tsx
@@ -1,12 +1,7 @@
 /**
  * Internal dependencies
  */
-import { ProductTitle } from '../product-title';
 
 export const ProductVariationFormHeader: React.FC = () => {
-	return (
-		<>
-			<ProductTitle />
-		</>
-	);
+	return <></>;
 };
