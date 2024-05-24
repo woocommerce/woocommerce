4248b6ce1ecb6d0c8bfad04d560373ffe90c0527
Naman Malhotra <naman03malhotra@gmail.com>
2024-05-24 20:02:27 +0530

    lint fixes

--- plugins/woocommerce/includes/data-stores/class-wc-product-data-store-cpt.php
+++ plugins/woocommerce/includes/data-stores/class-wc-product-data-store-cpt.php
@@ -184,7 +184,7 @@ class WC_Product_Data_Store_CPT extends WC_Data_Store_WP implements WC_Object_Da
 			if ( ! empty( $sku ) && ! $this->obtain_lock_on_sku_for_concurrent_requests( $product ) ) {
 				$product->delete();
 				// translators: 1: SKU, 2: Product Id.
-				throw new Exception( sprintf( __( 'The SKU (%1$s) you are trying to insert with Product Id (%2$s) is already under processing', 'woocommerce' ), esc_html( $sku ), esc_html( $id ) ) );
+				throw new Exception( esc_html( sprintf( __( 'The SKU (%1$s) you are trying to insert with Product Id (%2$s) is already under processing', 'woocommerce' ), $sku, $id ) ) );
 			}
 
 			$this->update_post_meta( $product, true );
