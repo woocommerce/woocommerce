63b5af2549e217e9bd6b018e331c61513882f985
Fernando Marichal <fernando.marichal@automattic.com>
2024-05-24 16:51:54 -0300

    Add metrics to editor tests

--- plugins/woocommerce/tests/metrics/specs/product-editor.spec.js
+++ plugins/woocommerce/tests/metrics/specs/product-editor.spec.js
@@ -8,7 +8,7 @@ import { test, Metrics } from '@wordpress/e2e-test-utils-playwright';
 /**
  * Internal dependencies
  */
-import { median } from '../utils';
+import { getTotalBlockingTime, median } from '../utils';
 import { toggleBlockProductEditor } from '../../e2e-pw/utils/simple-products';
 
 // See https://github.com/WordPress/gutenberg/issues/51383#issuecomment-1613460429
@@ -22,31 +22,6 @@ const results = {
 	largestContentfulPaint: [],
 	type: [],
 };
-
-async function getTotalBlockingTime( page, idleWait ) {
-	const totalBlockingTime = await page.evaluate( async ( waitTime ) => {
-		return new Promise( ( resolve ) => {
-			const longTaskEntries = [];
-			// Create a performance observer to observe long task entries
-			new PerformanceObserver( ( list ) => {
-				const entries = list.getEntries();
-				// Store each long task entry in the longTaskEntries array
-				entries.forEach( ( entry ) => longTaskEntries.push( entry ) );
-			} ).observe( { type: 'longtask', buffered: true } );
-
-			// Give some time to collect entries
-			setTimeout( () => {
-				// Calculate the total blocking time by summing the durations of all long tasks
-				const tbt = longTaskEntries.reduce(
-					( acc, entry ) => acc + entry.duration,
-					0
-				);
-				resolve( tbt );
-			}, waitTime );
-		} );
-	}, idleWait );
-	return totalBlockingTime;
-}
 
 test.describe( 'Product editor performance', () => {
 	test.use( {
