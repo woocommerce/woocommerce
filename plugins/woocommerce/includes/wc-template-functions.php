05f38795dab612c3d1375ec47fbac2f0d26c4cc5
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-05-24 15:03:55 +0200

    Fix wrong type

--- plugins/woocommerce/includes/wc-template-functions.php
+++ plugins/woocommerce/includes/wc-template-functions.php
@@ -1597,8 +1597,8 @@ if ( ! function_exists( 'woocommerce_show_product_thumbnails' ) ) {
  * Hooks: woocommerce_gallery_thumbnail_size, woocommerce_gallery_image_size and woocommerce_gallery_full_size accept name based image sizes, or an array of width/height values.
  *
  * @since 3.3.2
- * @param string $attachment_id Attachment ID.
- * @param bool   $main_image Is this the main image or a thumbnail?.
+ * @param int  $attachment_id Attachment ID.
+ * @param bool $main_image Is this the main image or a thumbnail?.
  * @return string
  */
 function wc_get_gallery_image_html( $attachment_id, $main_image = false ) {
