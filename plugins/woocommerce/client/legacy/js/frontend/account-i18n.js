068a94418f832b146d83bb5087ce85553664f8f8
Naman Malhotra <naman03malhotra@gmail.com>
2024-05-24 17:58:39 +0530

    Added comment for more context about setTimeout

--- plugins/woocommerce/client/legacy/js/frontend/account-i18n.js
+++ plugins/woocommerce/client/legacy/js/frontend/account-i18n.js
@@ -6,6 +6,13 @@ jQuery( function( $ ) {
     });
 
     if (notices.length > 0) {
+        /**
+         * Queuing the focus event at last of the event queue
+         * to override any other focus events in case of critical error.
+         * For example, "Skip to content" was being focused just after
+         * the error, resulting in the voiceover breaking the message
+         * in between.
+         */
         setTimeout(function() {
             $(notices[0]).attr('tabindex', '-1').focus();
         });
