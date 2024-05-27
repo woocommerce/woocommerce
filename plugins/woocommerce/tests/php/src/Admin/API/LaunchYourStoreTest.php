63f28bccb786b0aef5d1d244aa61851c78d050e8
moon <moon.kyong@automattic.com>
2024-05-27 14:26:47 -0700

    Add tests for test order count and delete endpoints

---
+++ plugins/woocommerce/tests/php/src/Admin/API/LaunchYourStoreTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Automattic\WooCommerce\Tests\Admin\API;
+
+use Automattic\WooCommerce\Admin\Features\PaymentGatewaySuggestions\Init;
+use Automattic\WooCommerce\Admin\Marketing\MarketingCampaign;
+use Automattic\WooCommerce\Admin\Marketing\MarketingCampaignType;
+use Automattic\WooCommerce\Admin\Marketing\MarketingChannelInterface;
+use Automattic\WooCommerce\Admin\Marketing\MarketingChannels as MarketingChannelsService;
+use Automattic\WooCommerce\RestApi\UnitTests\Helpers\OrderHelper;
+use WC_REST_Unit_Test_Case;
+use WP_REST_Request;
+
+/**
+ * LaunchYourStoreTest API controller test.
+ *
+ * @class LaunchYourStoreTest.
+ */
+class LaunchYourStoreTest extends WC_REST_Unit_Test_Case {
+	/**
+	 * Endpoint.
+	 *
+	 * @var string
+	 */
+	const ENDPOINT = '/wc-admin/launch-your-store';
+
+	/**
+	 * Set up.
+	 */
+	public function setUp(): void {
+		parent::setUp();
+
+		// Register an administrator user and log in.
+		$this->user = $this->factory->user->create(
+			array(
+				'role' => 'administrator',
+			)
+		);
+		wp_set_current_user( $this->user );
+	}
+
+	protected function get_order_count() {
+		$request = new WP_REST_Request( 'GET', self::ENDPOINT . '/woopayments/test-orders/count' );
+		return $this->server->dispatch( $request )->get_data()['count'];
+	}
+
+	protected function add_test_order() {
+		$order = OrderHelper::create_order( $this->user );
+		$order->update_meta_data( '_wcpay_mode', 'test' );
+		$order->save();
+	}
+
+	public function test_it_returns_test_order_count() {
+		$count = $this->get_order_count();
+		$this->assertEquals( 0, $count );
+
+		$this->add_test_order();
+
+		$count = $this->get_order_count();
+		$this->assertEquals( 1, $count );
+	}
+
+	public function test_delete_endpoint_returns_204() {
+		$request = new WP_REST_Request( 'DELETE', self::ENDPOINT . '/woopayments/test-orders' );
+		$status  = $this->server->dispatch( $request )->get_status();
+		$this->assertEquals( 204, $status );
+	}
+
+	public function test_delete_endpoint_deletes_test_order() {
+		$this->add_test_order();
+		$request = new WP_REST_Request( 'DELETE', self::ENDPOINT . '/woopayments/test-orders' );
+		$this->server->dispatch( $request );
+		$this->assertEquals( 0, $this->get_order_count() );
+	}
+}
