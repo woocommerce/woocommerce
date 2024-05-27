1849511ca3e2bd9980df77311f0aeffed73d7ec8
Tarun Vijwani <tarun.vijwani@automattic.com>
2024-05-28 01:47:58 +0400

    Remove unused import in shipping-rate-selector.tsx

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/shipping/shipping-rate-selector.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/shipping/shipping-rate-selector.tsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import type {
 	CartResponseShippingRate,
 	CartResponseShippingAddress,
