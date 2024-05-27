dcde3af2824e0940e3c3963b1f33143f498b9094
moon <moon.kyong@automattic.com>
2024-05-27 14:28:20 -0700

    Lint fixes

--- plugins/woocommerce/tests/php/src/Admin/API/LaunchYourStoreTest.php
+++ plugins/woocommerce/tests/php/src/Admin/API/LaunchYourStoreTest.php
@@ -39,17 +39,32 @@ class LaunchYourStoreTest extends WC_REST_Unit_Test_Case {
 		wp_set_current_user( $this->user );
 	}
 
+	/**
+	 * Get test order count.
+	 *
+	 * @return mixed
+	 */
 	protected function get_order_count() {
 		$request = new WP_REST_Request( 'GET', self::ENDPOINT . '/woopayments/test-orders/count' );
 		return $this->server->dispatch( $request )->get_data()['count'];
 	}
 
+	/**
+	 * Add a new test order
+	 *
+	 * @return void
+	 */
 	protected function add_test_order() {
 		$order = OrderHelper::create_order( $this->user );
 		$order->update_meta_data( '_wcpay_mode', 'test' );
 		$order->save();
 	}
 
+	/**
+	 * Test order count.
+	 *
+	 * @return void
+	 */
 	public function test_it_returns_test_order_count() {
 		$count = $this->get_order_count();
 		$this->assertEquals( 0, $count );
@@ -60,12 +75,22 @@ class LaunchYourStoreTest extends WC_REST_Unit_Test_Case {
 		$this->assertEquals( 1, $count );
 	}
 
+	/**
+	 * Test delete endpoint returns 204.
+	 *
+	 * @return void
+	 */
 	public function test_delete_endpoint_returns_204() {
 		$request = new WP_REST_Request( 'DELETE', self::ENDPOINT . '/woopayments/test-orders' );
 		$status  = $this->server->dispatch( $request )->get_status();
 		$this->assertEquals( 204, $status );
 	}
 
+	/**
+	 * Test delete order endpoint.
+	 *
+	 * @return void
+	 */
 	public function test_delete_endpoint_deletes_test_order() {
 		$this->add_test_order();
 		$request = new WP_REST_Request( 'DELETE', self::ENDPOINT . '/woopayments/test-orders' );
