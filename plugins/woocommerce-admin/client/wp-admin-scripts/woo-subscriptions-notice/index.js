8862c9096f6f0df6ff3d3a7ce17f1bc57d292771
prahesa.setia <prahesa.kusuma.setia@automattic.com>
2024-05-28 14:39:56 +0700

    fix notice so that it only trigger dismiss API on notices close

--- plugins/woocommerce-admin/client/wp-admin-scripts/woo-subscriptions-notice/index.js
+++ plugins/woocommerce-admin/client/wp-admin-scripts/woo-subscriptions-notice/index.js
@@ -1,18 +1,34 @@
-( function ( $ ) {
-	$( document ).on( 'click', '.woo-subscription-notices', function () {
-		const notice_id = this.id;
-		if ( ! notice_id ) {
-			return;
-		}
+window.jQuery( document ).ready( function () {
+	const expiredNoticeSelector = '#woo-subscription-expired-notice';
+	const expiringNoticeSelector = '#woo-subscription-expiring-notice';
 
-		const data = {
-			notice_id,
-		};
+	window
+		.jQuery( expiredNoticeSelector )
+		.on( 'click', 'button.notice-dismiss', function () {
+			const notice_id = 'woo-subscription-expired-notice';
+			const data = {
+				notice_id,
+			};
 
-		window.wp.apiFetch( {
-			path: `/wc-admin/notice/dismiss`,
-			method: 'POST',
-			data,
+			window.wp.apiFetch( {
+				path: `/wc-admin/notice/dismiss`,
+				method: 'POST',
+				data,
+			} );
 		} );
-	} );
-} )( window.jQuery );
+
+	window
+		.jQuery( expiringNoticeSelector )
+		.on( 'click', 'button.notice-dismiss', function () {
+			const notice_id = 'woo-subscription-expiring-notice';
+			const data = {
+				notice_id,
+			};
+
+			window.wp.apiFetch( {
+				path: `/wc-admin/notice/dismiss`,
+				method: 'POST',
+				data,
+			} );
+		} );
+} );
