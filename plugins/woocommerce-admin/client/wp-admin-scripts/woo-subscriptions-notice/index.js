442a5afc43eded00ed5ce30df938b127462a8c1c
prahesa.setia <prahesa.kusuma.setia@automattic.com>
2024-05-28 13:38:06 +0700

    fix lint

--- plugins/woocommerce-admin/client/wp-admin-scripts/woo-subscriptions-notice/index.js
+++ plugins/woocommerce-admin/client/wp-admin-scripts/woo-subscriptions-notice/index.js
@@ -1,22 +1,18 @@
 ( function ( $ ) {
-	$( document ).on(
-		'click',
-		'.woo-subscription-notices',
-		function () {
-			const notice_id = this.id;
-			if ( ! notice_id ) {
-				return;
-			}
-
-			const data = {
-				notice_id,
-			};
-
-			window.wp.apiFetch( {
-				path: `/wc-admin/notice/dismiss`,
-				method: 'POST',
-				data,
-			} );
+	$( document ).on( 'click', '.woo-subscription-notices', function () {
+		const notice_id = this.id;
+		if ( ! notice_id ) {
+			return;
 		}
-	);
+
+		const data = {
+			notice_id,
+		};
+
+		window.wp.apiFetch( {
+			path: `/wc-admin/notice/dismiss`,
+			method: 'POST',
+			data,
+		} );
+	} );
 } )( window.jQuery );
