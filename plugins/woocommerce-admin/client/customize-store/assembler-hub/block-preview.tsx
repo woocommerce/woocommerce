f12d6e4726668db86d465af9a0b12c9681a6c0b7
Luigi Teschio <gigitux@gmail.com>
2024-05-28 08:55:14 +0200

    CYS: allow to the user to move the pattern (#47322)
    * CYS: allow to the user to move the pattern
    
    * add feature flag
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * use WooCommerce feature flag approach
    
    * hide scrollbar on the body
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce-admin/client/customize-store/assembler-hub/block-preview.tsx
+++ plugins/woocommerce-admin/client/customize-store/assembler-hub/block-preview.tsx
@@ -9,6 +9,7 @@
 import { BlockEditorProvider } from '@wordpress/block-editor';
 import { memo } from '@wordpress/element';
 import { BlockInstance } from '@wordpress/blocks';
+
 /**
  * Internal dependencies
  */
@@ -17,6 +18,7 @@ import {
 	ScaledBlockPreviewProps,
 } from './auto-block-preview';
 import { ChangeHandler } from './hooks/use-editor-blocks';
+import { Toolbar } from './toolbar/toolbar';
 
 export const BlockPreview = ( {
 	blocks,
@@ -33,15 +35,20 @@ export const BlockPreview = ( {
 	const renderedBlocks = Array.isArray( blocks ) ? blocks : [ blocks ];
 
 	return (
-		<BlockEditorProvider
-			value={ renderedBlocks }
-			settings={ settings }
-			// We need to set onChange for logo to work, but we don't want to trigger the onChange callback when highlighting blocks in the preview. It would persist the highlighted block and cause the opacity to be applied to block permanently.
-			onChange={ onChange }
-			useSubRegistry={ useSubRegistry }
-		>
-			<AutoHeightBlockPreview settings={ settings } { ...props } />
-		</BlockEditorProvider>
+		<>
+			<BlockEditorProvider
+				value={ renderedBlocks }
+				settings={ settings }
+				// We need to set onChange for logo to work, but we don't want to trigger the onChange callback when highlighting blocks in the preview. It would persist the highlighted block and cause the opacity to be applied to block permanently.
+				onChange={ onChange }
+				useSubRegistry={ useSubRegistry }
+			>
+				{ window.wcAdminFeatures[
+					'pattern-toolkit-full-composability'
+				] && <Toolbar /> }
+				<AutoHeightBlockPreview settings={ settings } { ...props } />
+			</BlockEditorProvider>
+		</>
 	);
 };
 
