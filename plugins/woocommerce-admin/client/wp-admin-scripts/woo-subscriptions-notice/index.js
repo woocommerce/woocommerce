fc86054b433176dbea2343704b2558a286e86c22
prahesa.setia <prahesa.kusuma.setia@automattic.com>
2024-05-28 15:26:39 +0700

    track events in the wc settings page

--- plugins/woocommerce-admin/client/wp-admin-scripts/woo-subscriptions-notice/index.js
+++ plugins/woocommerce-admin/client/wp-admin-scripts/woo-subscriptions-notice/index.js
@@ -1,10 +1,27 @@
+/**
+ * External dependencies
+ */
+import { recordEvent } from '@woocommerce/tracks';
+
 window.jQuery( document ).ready( function () {
 	const expiredNoticeSelector = '#woo-subscription-expired-notice';
 	const expiringNoticeSelector = '#woo-subscription-expiring-notice';
 
+	if ( window.jQuery( expiredNoticeSelector ).length ) {
+		recordEvent( 'woo_subscription_expired_notice_in_settings_shown' );
+	}
+
+	if ( window.jQuery( expiringNoticeSelector ).length ) {
+		recordEvent( 'woo_subscription_expiring_notice_in_settings_shown' );
+	}
+
 	window
 		.jQuery( expiredNoticeSelector )
 		.on( 'click', 'button.notice-dismiss', function () {
+			recordEvent(
+				'woo_subscription_expired_notice_in_settings_dismissed'
+			);
+
 			const notice_id = 'woo-subscription-expired-notice';
 			const data = {
 				notice_id,
@@ -17,9 +34,18 @@ window.jQuery( document ).ready( function () {
 			} );
 		} );
 
+	window.jQuery( expiredNoticeSelector ).on( 'click', 'a', function () {
+		recordEvent( 'woo_subscription_expired_notice_in_settings_clicked' );
+		return true;
+	} );
+
 	window
 		.jQuery( expiringNoticeSelector )
 		.on( 'click', 'button.notice-dismiss', function () {
+			recordEvent(
+				'woo_subscription_expiring_notice_in_settings_dismissed'
+			);
+
 			const notice_id = 'woo-subscription-expiring-notice';
 			const data = {
 				notice_id,
@@ -31,4 +57,9 @@ window.jQuery( document ).ready( function () {
 				data,
 			} );
 		} );
+
+	window.jQuery( expiringNoticeSelector ).on( 'click', 'a', function () {
+		recordEvent( 'woo_subscription_expiring_notice_in_settings_clicked' );
+		return true;
+	} );
 } );
