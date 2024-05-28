581f0b8d8c36460773722b6e8fcdcd69c7baa601
paul sealock <psealock@gmail.com>
2024-05-28 17:45:32 +1200

    feature flag

---
+++ plugins/woocommerce/src/Admin/Features/SettingsRefresh.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Automattic\WooCommerce\Admin\Features;
+
+/**
+ * New Settings refresh.
+ */
+class SettingsRefresh {
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		// print_r( 'This is working' );
+	}
+}
