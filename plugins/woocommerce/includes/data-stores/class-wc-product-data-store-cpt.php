ef2c747e86fddc7d3476209de7cab3157317c840
Naman Malhotra <naman03malhotra@gmail.com>
2024-05-29 15:06:39 +0530

    self review - selecting values from DUAL

--- plugins/woocommerce/includes/data-stores/class-wc-product-data-store-cpt.php
+++ plugins/woocommerce/includes/data-stores/class-wc-product-data-store-cpt.php
@@ -118,11 +118,10 @@ class WC_Product_Data_Store_CPT extends WC_Data_Store_WP implements WC_Object_Da
 
 		$query = $wpdb->prepare(
 			"INSERT INTO $wpdb->wc_product_meta_lookup (product_id, sku)
-			SELECT %d, %s FROM $wpdb->wc_product_meta_lookup
+			SELECT %d, %s FROM DUAL
 			WHERE NOT EXISTS (
 				SELECT * FROM $wpdb->wc_product_meta_lookup WHERE sku = %s LIMIT 1
-			) LIMIT 1 FOR UPDATE
-			ON DUPLICATE KEY UPDATE sku = VALUES(sku);",
+			) LIMIT 1 FOR UPDATE;",
 			$product_id,
 			$sku,
 			$sku
