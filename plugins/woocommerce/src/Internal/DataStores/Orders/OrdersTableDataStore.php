4125132067b0bc83897139bbf8b2003d23b9af69
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-05-29 17:03:55 +0100

    Added "draft" status on Order creation in HPOS to prevent triggering the new order hook

--- plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableDataStore.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableDataStore.php
@@ -2524,7 +2524,7 @@ FROM $order_meta_table
 		$this->persist_save( $order );
 
 		// Do not fire 'woocommerce_new_order' for draft statuses for backwards compatibility.
-		if ( in_array( $order->get_status( 'edit' ), array( 'auto-draft', 'checkout-draft' ), true ) ) {
+		if ( in_array( $order->get_status( 'edit' ), array( 'auto-draft', 'draft', 'checkout-draft' ), true ) ) {
 			return;
 		}
 
