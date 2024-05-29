b63e7e713371020d9a69e7a78a2d5339ad6f4dcd
moon <moon.kyong@automattic.com>
2024-05-28 19:02:50 -0700

    Cache /wc-admin/launch-your-store/survey-completed call

--- plugins/woocommerce-admin/client/launch-your-store/hub/main-content/pages/launch-store-success/services.tsx
+++ plugins/woocommerce-admin/client/launch-your-store/hub/main-content/pages/launch-store-success/services.tsx
@@ -6,12 +6,23 @@ import { resolveSelect } from '@wordpress/data';
 import { fromPromise } from 'xstate5';
 import apiFetch from '@wordpress/api-fetch';
 
+type SurveyCompletedResponse = string | null;
+let cachedSurveyCompleted: SurveyCompletedResponse = null;
+const fetchSurveyCompletedOption =
+	async (): Promise< SurveyCompletedResponse > => {
+		if ( cachedSurveyCompleted !== null ) {
+			return cachedSurveyCompleted;
+		}
+		const response = await apiFetch( {
+			path: `/wc-admin/launch-your-store/survey-completed`,
+		} );
+		cachedSurveyCompleted = response as SurveyCompletedResponse;
+		return cachedSurveyCompleted;
+	};
+
 export const fetchCongratsData = fromPromise( async () => {
 	const [ surveyCompleted, tasklists, activePlugins ] = await Promise.all( [
-		await apiFetch( {
-			path: `/wc-admin/launch-your-store/survey-completed`,
-		} ),
-
+		fetchSurveyCompletedOption(),
 		resolveSelect( ONBOARDING_STORE_NAME ).getTaskListsByIds( [
 			'setup',
 			'extended',
