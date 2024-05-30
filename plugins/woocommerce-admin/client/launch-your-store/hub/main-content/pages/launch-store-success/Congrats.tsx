c81762cd0ecc8da52d4537cde5bb706cc8d28dcc
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-05-30 21:36:36 +0800

    Fix survey score

--- plugins/woocommerce-admin/client/launch-your-store/hub/main-content/pages/launch-store-success/Congrats.tsx
+++ plugins/woocommerce-admin/client/launch-your-store/hub/main-content/pages/launch-store-success/Congrats.tsx
@@ -49,12 +49,12 @@ export const Congrats = ( {
 	const [ isShowSurvey, setIsShowSurvey ] = useState< boolean >(
 		! hasCompleteSurvey
 	);
-	const [ emojiValue, setEmojiValue ] = useState< number | null >( null );
+	const [ score, setScore ] = useState< number | null >( null );
 	const [ feedbackText, setFeedbackText ] = useState< string >( '' );
 	const [ isShowThanks, setIsShowThanks ] = useState< boolean >( false );
 	const [ copyLinkText, setCopyLinkText ] = useState( copyLink );
 
-	const shouldShowComment = isInteger( emojiValue );
+	const shouldShowComment = isInteger( score );
 
 	const copyClipboardRef = useCopyToClipboard< HTMLAnchorElement >(
 		homeUrl,
@@ -71,19 +71,9 @@ export const Congrats = ( {
 	);
 
 	const sendData = () => {
-		const emojis = {
-			1: 'very_difficult',
-			2: 'difficult',
-			3: 'neutral',
-			4: 'good',
-			5: 'very_good',
-		} as const;
-		const emoji_value = emojiValue
-			? emojis[ emojiValue as keyof typeof emojis ]
-			: 'none';
 		recordEvent( 'launch_your_store_congrats_survey_complete', {
 			action: 'lys_experience',
-			score: emoji_value,
+			score,
 			comments: feedbackText,
 		} );
 
@@ -206,10 +196,8 @@ export const Congrats = ( {
 												'How was the experience of launching your store?',
 												'woocommerce'
 											) }
-											onSelect={ ( score ) =>
-												setEmojiValue( score )
-											}
-											selectedValue={ emojiValue }
+											onSelect={ setScore }
+											selectedValue={ score }
 										/>
 									</div>
 									{ shouldShowComment && (
@@ -267,7 +255,7 @@ export const Congrats = ( {
 											className=""
 											variant="tertiary"
 											onClick={ () => {
-												setEmojiValue( null );
+												setScore( null );
 											} }
 										>
 											{ __( 'Cancel', 'woocommerce' ) }
