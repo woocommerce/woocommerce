7f6dcecdc76c8919bab9c9997b554d0323f16834
Manish Menaria <the.manish.menaria@gmail.com>
2024-05-30 14:24:21 +0530

    Remove Demo file

--- plugins/woocommerce-blocks/tests/e2e/tests/demo.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/demo.spec.ts
@@ -8,7 +8,16 @@ test( 'Passing test', async ( { page } ) => {
 	expect( true ).toBeTruthy();
 } );
 
-test( 'Flaky test', async ( { page }, testInfo ) => {
+test( 'Flaky test 1010', async ( { page }, testInfo ) => {
+	await page.goto( 'https://example.com' );
+	// Introduce flakiness
+	if ( testInfo.retry < 2 ) {
+		throw new Error( 'Flaky failure' );
+	}
+	expect( true ).toBeTruthy();
+} );
+
+test( 'Flaky test 1011', async ( { page }, testInfo ) => {
 	await page.goto( 'https://example.com' );
 	// Introduce flakiness
 	if ( testInfo.retry < 2 ) {
