60934d55882b6d56b6d7a1fcfc234628f8b7201a
paul sealock <psealock@gmail.com>
2024-05-31 16:32:22 +1200

    gather form data

--- plugins/woocommerce-admin/client/settings/components/checkbox.js
+++ plugins/woocommerce-admin/client/settings/components/checkbox.js
@@ -2,15 +2,22 @@
  * External dependencies
  */
 import { CheckboxControl } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 
-export const SettingsCheckbox = ( { setting } ) => {
+export const SettingsCheckbox = ( { setting, handleFormChange } ) => {
+	const [ checked, setChecked ] = useState( 'yes' === setting.value );
+	const onChange = ( value ) => {
+		setChecked( value );
+		handleFormChange( { id: setting.id, value: value ? 'yes' : 'no' } );
+	};
 	return (
 		<div className="woocommerce-settings-element">
 			<h4>{ setting.title }</h4>
 			<CheckboxControl
+				id={ setting.id }
 				label={ setting.desc }
-				onChange={ () => console.log( 'change' ) }
-				checked={ 'yes' === setting.value }
+				onChange={ onChange }
+				checked={ checked }
 			/>
 		</div>
 	);
