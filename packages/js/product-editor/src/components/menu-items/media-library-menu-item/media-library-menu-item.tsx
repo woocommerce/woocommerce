b885f8e2d5c6b73d7d94721fbb894fc316a77eb7
Maikel David Pérez Gómez <mdperez86@gmail.com>
2024-05-31 15:33:01 -0400

    Create MediaLibraryMenuItem component

---
+++ packages/js/product-editor/src/components/menu-items/media-library-menu-item/media-library-menu-item.tsx
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import type { ChangeEvent } from 'react';
+import { FormFileUpload, MenuItem } from '@wordpress/components';
+import { createElement } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { media } from '@wordpress/icons';
+import { MediaUpload, uploadMedia } from '@wordpress/media-utils';
+
+/**
+ * Internal dependencies
+ */
+import type { MediaLibraryMenuItemProps } from './types';
+
+export function MediaLibraryMenuItem( {
+	// MenuItem.Props
+	icon,
+	iconPosition,
+	text,
+	info,
+	// MediaUpload.Props
+	...props
+}: MediaLibraryMenuItemProps ) {
+	return (
+		<MediaUpload
+			{ ...props }
+			render={ ( { open } ) => (
+				<MenuItem
+					icon={ icon ?? media }
+					iconPosition={ iconPosition ?? 'left' }
+					onClick={ open }
+					info={
+						info ??
+						__( 'Choose from uploaded media', 'woocommerce' )
+					}
+				>
+					{ text ?? __( 'Media Library', 'woocommerce' ) }
+				</MenuItem>
+			) }
+		/>
+	);
+}
