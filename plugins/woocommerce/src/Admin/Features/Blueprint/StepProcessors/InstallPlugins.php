1f4b32279dddb1ac57d25f33bd0d523a757dec4e
moon <moon.kyong@automattic.com>
2024-06-03 13:05:54 -0700

    Change InstallPlugin to InstallPlugins

--- plugins/woocommerce/src/Admin/Features/Blueprint/StepProcessors/InstallPlugins.php
+++ plugins/woocommerce/src/Admin/Features/Blueprint/StepProcessors/InstallPlugins.php
@@ -5,11 +5,12 @@ namespace Automattic\WooCommerce\Admin\Features\Blueprint\StepProcessors;
 use Automattic\WooCommerce\Admin\Features\Blueprint\StepProcessor;
 use Automattic\WooCommerce\Admin\PluginsHelper;
 
-class InstallPlugin implements StepProcessor {
+class InstallPlugins implements StepProcessor {
 	public function process($schema) {
-		PluginsHelper::install_plugins( array( $schema->pluginZipFile->slug ) );
-		PluginsHelper::activate_plugins( array( $schema->pluginZipFile->slug ) );
-
+		foreach ($schema->plugins as $plugin) {
+			PluginsHelper::install_plugins( array( $plugin->slug ) );
+			PluginsHelper::activate_plugins( array( $plugin->slug ) );
+		}
 		return true;
 	}
 }
