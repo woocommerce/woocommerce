d8c1d5e4d6901fd21853f30d57f6114745d87444
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-06-04 00:40:53 +0300

    Validate RELEASE_TAG and ARTIFACT_NAME

--- plugins/woocommerce/tests/e2e-pw/bin/override-wp-env-plugins.js
+++ plugins/woocommerce/tests/e2e-pw/bin/override-wp-env-plugins.js
@@ -1,6 +1,17 @@
 const fs = require( 'fs' );
 
 const { RELEASE_TAG, ARTIFACT_NAME } = process.env;
+
+if ( ! RELEASE_TAG ) {
+	console.error( 'Please set the RELEASE_TAG environment variable!' );
+	process.exit( 1 );
+}
+
+if ( ! ARTIFACT_NAME ) {
+	console.error( 'Please set the ARTIFACT_NAME environment variable!' );
+	process.exit( 1 );
+}
+
 const artifactUrl = `https://github.com/woocommerce/woocommerce/releases/download/${ RELEASE_TAG }/${ ARTIFACT_NAME }`;
 // https://github.com/woocommerce/woocommerce/releases/download/nightly/woocommerce-trunk-nightly.zip
 // https://github.com/woocommerce/woocommerce/releases/download/9.0.0-beta.2/woocommerce.zip
