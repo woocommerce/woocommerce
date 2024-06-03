c7460c6209384ea2f72d96d98eda5a28846608ad
paul sealock <psealock@gmail.com>
2024-06-03 23:53:03 +1200

    handleFormINput on submit

--- plugins/woocommerce-admin/client/settings/components/checkbox.js
+++ plugins/woocommerce-admin/client/settings/components/checkbox.js
@@ -4,12 +4,11 @@
 import { CheckboxControl } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 
-export const SettingsCheckbox = ( { setting, handleFormChange } ) => {
+export const SettingsCheckbox = ( { setting } ) => {
 	const { id, desc } = setting;
 	const [ checked, setChecked ] = useState( 'yes' === setting.value );
 	const onChange = ( value ) => {
 		setChecked( value );
-		handleFormChange( { id, value: value ? 'yes' : 'no' } );
 	};
 	return (
 		<div className="woocommerce-settings-element">
