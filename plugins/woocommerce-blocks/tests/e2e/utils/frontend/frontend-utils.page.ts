04b7da947d142617328cdc03af900c9a0ad9400a
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-06-03 10:26:27 +0200

    Blocks E2E: Use layout-based selectors in Thumbnails block tests (#47904)
    * Use layout-based selectors in Thumbnails block tests
    
    * improve test readability
    
    * add changelog entry

--- plugins/woocommerce-blocks/tests/e2e/utils/frontend/frontend-utils.page.ts
+++ plugins/woocommerce-blocks/tests/e2e/utils/frontend/frontend-utils.page.ts
@@ -21,15 +21,6 @@ export class FrontendUtils {
 		return this.page.locator( selector );
 	}
 
-	async getBlockByClassWithParent( blockClass: string, parentName: string ) {
-		const parentBlock = await this.getBlockByName( parentName );
-		if ( ! parentBlock ) {
-			throw new Error( `Parent block "${ parentName }" not found.` );
-		}
-		const block = parentBlock.locator( `.${ blockClass }` );
-		return block;
-	}
-
 	async addToCart( itemName = '' ) {
 		await this.page.waitForLoadState( 'domcontentloaded' );
 		if ( itemName !== '' ) {
@@ -97,93 +88,6 @@ export class FrontendUtils {
 				`Got an error response when trying to empty cart. Status code: ${ res.status() }`
 			);
 		}
-	}
-
-	async isBlockEarlierThan< T >(
-		containerBlock: T,
-		firstBlock: string,
-		secondBlock: string
-	) {
-		const container =
-			containerBlock instanceof Function
-				? await containerBlock()
-				: containerBlock;
-
-		if ( ! container ) {
-			throw new Error( 'Container block not found.' );
-		}
-
-		const childBlocks = container.locator( '[data-block-name]' );
-
-		let firstBlockIndex = -1;
-		let secondBlockIndex = -1;
-
-		for ( let i = 0; i < ( await childBlocks.count() ); i++ ) {
-			const blockName = await childBlocks
-				.nth( i )
-				.getAttribute( 'data-block-name' );
-
-			if ( blockName === firstBlock ) {
-				firstBlockIndex = i;
-			}
-
-			if ( blockName === secondBlock ) {
-				secondBlockIndex = i;
-			}
-
-			if ( firstBlockIndex !== -1 && secondBlockIndex !== -1 ) {
-				break;
-			}
-		}
-
-		if ( firstBlockIndex === -1 || secondBlockIndex === -1 ) {
-			throw new Error( 'Both blocks must exist within the editor' );
-		}
-
-		return firstBlockIndex < secondBlockIndex;
-	}
-
-	async isBlockEarlierThanGroupBlock(
-		containerBlock: Locator,
-		firstBlock: string
-	) {
-		if ( ! containerBlock ) {
-			throw new Error( 'Container block not found.' );
-		}
-
-		const childBlocks: Locator = containerBlock.locator( '> div' );
-
-		let firstBlockIndex = -1;
-		let secondBlockIndex = -1;
-
-		for ( let i = 0; i < ( await childBlocks.count() ); i++ ) {
-			const blockName = await childBlocks
-				.nth( i )
-				.getAttribute( 'data-block-name' );
-			const isGroupBlock = await childBlocks
-				.nth( i )
-				.evaluate( ( node ) =>
-					node.classList.contains( 'wp-block-group' )
-				);
-
-			if ( blockName === firstBlock ) {
-				firstBlockIndex = i;
-			}
-
-			if ( isGroupBlock ) {
-				secondBlockIndex = i;
-			}
-
-			if ( firstBlockIndex !== -1 && secondBlockIndex !== -1 ) {
-				break;
-			}
-		}
-
-		if ( firstBlockIndex === -1 || secondBlockIndex === -1 ) {
-			throw new Error( 'Both blocks must exist within the editor' );
-		}
-
-		return firstBlockIndex < secondBlockIndex;
 	}
 
 	/**
