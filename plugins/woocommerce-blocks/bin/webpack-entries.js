e32459bdfba78dc13a97e611d3a80762fd4d5214
Sam Seay <samueljseay@gmail.com>
2024-06-03 18:51:51 +0800

    Use correct file ext

--- plugins/woocommerce-blocks/bin/webpack-entries.js
+++ plugins/woocommerce-blocks/bin/webpack-entries.js
@@ -226,7 +226,7 @@ const entries = {
 		wcBlocksSharedHocs: './assets/js/shared/hocs/index.js',
 		priceFormat: './packages/prices/index.js',
 		wcTypes: './assets/js/types/index.ts',
-		wcBaseUtils: './assets/js/base-utils/index.ts',
+		wcBaseUtils: './assets/js/base-utils/index.js',
 
 		// interactivity components, exported as separate entries for now
 		'wc-interactivity-dropdown':
