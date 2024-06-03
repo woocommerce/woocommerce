de7e223a0f42093727630ac3e6dd026a5b757d38
Luigi Teschio <gigitux@gmail.com>
2024-06-03 15:52:36 +0200

    remove dispatch

--- plugins/woocommerce-admin/client/customize-store/assembler-hub/index.tsx
+++ plugins/woocommerce-admin/client/customize-store/assembler-hub/index.tsx
@@ -127,11 +127,7 @@ const initializeAssembleHub = () => {
 		welcomeGuideTemplate: false,
 		showListViewByDefault: false,
 		showBlockBreadcrumbs: true,
-		isDistractionFree: true,
 	} );
-
-	// @ts-expect-error No types for this exist yet.
-	dispatch( preferencesStore ).set( 'core', 'distractionFree', true );
 
 	// @ts-ignore No types for this exist yet.
 	dispatch( editSiteStore ).updateSettings( settings );
