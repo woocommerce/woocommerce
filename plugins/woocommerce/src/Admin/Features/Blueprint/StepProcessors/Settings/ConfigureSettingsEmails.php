7fd2d0ddaa52b291033d4960059f2adcb7d9b83e
moon <moon.kyong@automattic.com>
2024-06-05 16:05:12 -0700

    save

---
+++ plugins/woocommerce/src/Admin/Features/Blueprint/StepProcessors/Settings/ConfigureSettingsEmails.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Automattic\WooCommerce\Admin\Features\Blueprint\StepProcessors\Settings;
+
+use Automattic\WooCommerce\Admin\Features\Blueprint\StepProcessor;
+
+class ConfigureSettingsEmails extends MapFieldsToOptions implements StepProcessor {
+
+}
