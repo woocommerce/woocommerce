e1728d77a0270f6e4f14f90f3094104dbe09896d
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-06-06 08:36:55 +0200

    Add Search button label in product search form pattern

--- plugins/woocommerce/patterns/product-search-form.php
+++ plugins/woocommerce/patterns/product-search-form.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Product Search
  * Slug: woocommerce/product-search-form
@@ -6,4 +7,4 @@
  * Categories: WooCommerce
  */
 ?>
-<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_attr_x( 'Search products…', 'placeholder for search field', 'woocommerce' ); ?>","query":{"post_type":"product"}} /-->
+<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_attr_x('Search products…', 'placeholder for search field', 'woocommerce'); ?>","buttonText":"<?php echo esc_attr_x('Search', 'button label of product search block', 'woocommerce'); ?>","query":{"post_type":"product"}} /-->
