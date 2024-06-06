44c26363aec49fb65b96437bc07d52d682271792
Damián Suárez <rdsuarez@gmail.com>
2024-06-06 09:18:07 +0100

    rollback TS global changes

--- packages/js/product-editor/typings/global.d.ts
+++ packages/js/product-editor/typings/global.d.ts
@@ -1,42 +1,6 @@
 declare global {
 	interface Window {
-		wcAdminFeatures: {
-			'activity-panels': boolean;
-			analytics: boolean;
-			'product-block-editor': boolean;
-			'experimental-blocks': boolean;
-			coupons: boolean;
-			'core-profiler': boolean;
-			'customize-store': boolean;
-			'import-products-task': boolean;
-			'experimental-fashion-sample-products': boolean;
-			'shipping-smart-defaults': boolean;
-			'shipping-setting-tour': boolean;
-			homescreen: boolean;
-			marketing: boolean;
-			'minified-js': boolean;
-			'mobile-app-banner': boolean;
-			navigation: boolean;
-			onboarding: boolean;
-			'onboarding-tasks': boolean;
-			'pattern-toolkit-full-composability': boolean;
-			'product-pre-publish-modal': boolean;
-			'product-custom-fields': boolean;
-			'remote-inbox-notifications': boolean;
-			'remote-free-extensions': boolean;
-			'payment-gateway-suggestions': boolean;
-			settings: boolean;
-			'shipping-label-banner': boolean;
-			subscriptions: boolean;
-			'store-alerts': boolean;
-			'transient-notices': boolean;
-			'woo-mobile-welcome': boolean;
-			'wc-pay-promotion': boolean;
-			'wc-pay-welcome-page': boolean;
-			'async-product-editor-category-field': boolean;
-			'launch-your-store': boolean;
-			'product-editor-template-system': boolean;
-		};
+		wcAdminFeatures: Record< string, boolean >;
 		wcTracks: {
 			isEnabled: boolean;
 			validateEvent: (
