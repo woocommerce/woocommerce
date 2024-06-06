097abc2ff0fd2e27db742ecc5ae648f129dd52ad
Manish Menaria <the.manish.menaria@gmail.com>
2024-06-06 15:15:57 +0530

    Add plugin to test __experimentalRegisterProductCollection

---
+++ plugins/woocommerce-blocks/tests/e2e/plugins/register-product-collection-tester/collections.js
@@ -0,0 +1,60 @@
+const { __experimentalRegisterProductCollection } = wc.wcBlocksRegistry;
+
+// Example 1: Register a new collection.
+__experimentalRegisterProductCollection( {
+	name: 'woocommerce/product-collection/my-custom-collection',
+	title: 'My Custom Collection',
+	icon: 'games',
+	description: 'This is a custom collection.',
+	keywords: [ 'custom collection', 'product collection' ],
+} );
+
+// Example 2: Register a new collection with a preview.
+__experimentalRegisterProductCollection( {
+	name: 'woocommerce/product-collection/my-custom-collection-with-preview',
+	title: 'My Custom Collection with Preview',
+	icon: 'games',
+	description: 'This is a custom collection with preview.',
+	keywords: [ 'custom collection', 'product collection' ],
+	preview: {
+		initialPreviewState: {
+			isPreview: true,
+			previewMessage:
+				'This is a preview message for my custom collection with preview.',
+		},
+	},
+} );
+
+// Example 3: Advanced usage of preview.
+__experimentalRegisterProductCollection( {
+	name: 'woocommerce/product-collection/my-custom-collection-with-advanced-preview',
+	title: 'My Custom Collection with Advanced Preview',
+	icon: 'games',
+	description: 'This is a custom collection with advanced preview.',
+	keywords: [ 'custom collection', 'product collection' ],
+	preview: {
+		setPreviewState: ( {
+			setState,
+			attributes: currentAttributes,
+			location,
+		} ) => {
+			// You can access the current attributes and location.
+			// console.log( 'Current attributes:', currentAttributes );
+			// console.log( 'Location:', location );
+
+			const timeoutID = setTimeout( () => {
+				setState( {
+					isPreview: false,
+					previewMessage: '',
+				} );
+			}, 5000 );
+
+			return () => clearTimeout( timeoutID );
+		},
+		initialPreviewState: {
+			isPreview: true,
+			previewMessage:
+				'This is a preview message for my custom collection with advanced preview.',
+		},
+	},
+} );
