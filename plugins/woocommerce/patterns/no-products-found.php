898c73934cd3b472d456d5e2dde05576fb4c34a9
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-06-06 10:22:26 +0200

    Exclude woocommerce/patterns from phpcs rules that dont apply there

--- plugins/woocommerce/patterns/no-products-found.php
+++ plugins/woocommerce/patterns/no-products-found.php
@@ -6,7 +6,6 @@
  * Categories: WooCommerce
  */
 ?>
-
 <!-- wp:paragraph -->
 <p>
 	<?php echo esc_html_x( 'No products were found matching your selection.', 'Message explaining that there are no products returned from a search', 'woocommerce' ); ?>
