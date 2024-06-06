55e3ad4cfee5bbc1b15db2ab6c285028fcf3d653
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-06-06 08:59:35 +0200

    Fix lint

--- plugins/woocommerce/patterns/product-search-form.php
+++ plugins/woocommerce/patterns/product-search-form.php
@@ -7,4 +7,4 @@
  * Categories: WooCommerce
  */
 ?>
-<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_attr_x('Search products…', 'placeholder for search field', 'woocommerce'); ?>","buttonText":"<?php echo esc_attr_x('Search', 'button label of product search block', 'woocommerce'); ?>","query":{"post_type":"product"}} /-->
+<!-- wp:search {"showLabel":false,"placeholder":"<?php echo esc_attr_x( 'Search products…', 'placeholder for search field', 'woocommerce' ); ?>","buttonText":"<?php echo esc_attr_x( 'Search', 'button label of product search block', 'woocommerce' ); ?>","query":{"post_type":"product"}} /-->
