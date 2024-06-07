85a07dfb7137d90abd8f71cc12ecd0d57c3a2d27
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-06-07 19:11:17 +0300

    Test with more flaky tests

--- plugins/woocommerce-blocks/tests/e2e/tests/flaky-test-2.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/flaky-test-2.spec.ts
@@ -3,9 +3,9 @@
  */
 import { test, expect } from '@woocommerce/e2e-utils';
 
-test.describe( 'Flaky test describe block', () => {
-	test.describe( 'Flaky test inner describe block', () => {
-		test( 'I should pass on retry', async ( {}, testInfo ) => {
+test.describe( 'Flaky test describe block 2', () => {
+	test.describe( 'Flaky test inner describe block 2', () => {
+		test( 'I should pass on retry 2', async ( {}, testInfo ) => {
 			expect( testInfo.retry ).toBeTruthy();
 		} );
 	} );
