d18d4737faadb976dcbb82e7180c75a44c3c3eec
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2024-06-07 12:14:46 -0700

    Ensure attribute slugs are properly handled in the Products REST API controller (#48198)
    * Ensure attribute slugs are properly sanitized when setting
    
    * Update approach to avoid breaking functionality in WP Admin UI
    
    * Add changelog file
    
    * Add unit test

--- plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-products-v2-controller.php
+++ plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-products-v2-controller.php
@@ -462,8 +462,12 @@ class WC_REST_Products_V2_Controller extends WC_REST_CRUD_Controller {
 	protected function get_attribute_taxonomy_name( $slug, $product ) {
 		// Format slug so it matches attributes of the product.
 		$slug       = wc_attribute_taxonomy_slug( $slug );
-		$attributes = $product->get_attributes();
-		$attribute  = false;
+		$attributes = array_combine(
+			array_map( 'wc_sanitize_taxonomy_name', array_keys( $product->get_attributes() ) ),
+			array_values( $product->get_attributes() )
+		);
+
+		$attribute = false;
 
 		// pa_ attributes.
 		if ( isset( $attributes[ wc_attribute_taxonomy_name( $slug ) ] ) ) {
