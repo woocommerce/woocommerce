3e2f6705cb0e9c5e2ceb3402fd4c3addc9b7fe8d
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-06-07 16:50:56 +0800

    Add tests for error-boundary

---
+++ plugins/woocommerce-admin/client/error-boundary/test/index.test.tsx
@@ -0,0 +1,108 @@
+/**
+ * External dependencies
+ */
+import { render, screen, fireEvent } from '@testing-library/react';
+/**
+ * Internal dependencies
+ */
+import { ErrorBoundary } from '../';
+
+const ThrowError = () => {
+	throw new Error( 'Test error' );
+	return null;
+};
+
+describe( 'ErrorBoundary', () => {
+	function onError( event: Event ) {
+		// Note: this will swallow reports about unhandled errors!
+		// Use with extreme caution.
+		event.preventDefault();
+	}
+
+	// Mock window.location.reload by using a global variable
+	const originalLocation = window.location;
+
+	beforeAll( () => {
+		// Opt Out of the jsdom error messages
+		window.addEventListener( 'error', onError );
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore - Ignore TS error for deleting window.location
+		delete window.location;
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore - Ignore TS error for assigning window.location
+		window.location = { reload: jest.fn() };
+	} );
+
+	afterAll( () => {
+		window.location = originalLocation;
+		window.removeEventListener( 'error', onError );
+	} );
+
+	it( 'catches errors and displays an error message', () => {
+		render(
+			<ErrorBoundary>
+				<ThrowError />
+			</ErrorBoundary>
+		);
+
+		expect(
+			screen.getByText( 'Oops, Something went wrong' )
+		).toBeInTheDocument();
+	} );
+
+	it( 'shows refresh and report issue buttons', () => {
+		render(
+			<ErrorBoundary>
+				<ThrowError />
+			</ErrorBoundary>
+		);
+
+		expect(
+			screen.getByText( 'Refresh Page and Try Again' )
+		).toBeInTheDocument();
+		expect(
+			screen.getByText( 'Report Issue on Github' )
+		).toBeInTheDocument();
+	} );
+
+	it( 'refreshes the page when Refresh Page button is clicked', () => {
+		const reloadMock = jest.fn();
+		Object.defineProperty( window.location, 'reload', {
+			configurable: true,
+			value: reloadMock,
+		} );
+
+		render(
+			<ErrorBoundary>
+				<ThrowError />
+			</ErrorBoundary>
+		);
+
+		fireEvent.click( screen.getByText( 'Refresh Page and Try Again' ) );
+
+		expect( reloadMock ).toHaveBeenCalled();
+	} );
+
+	it( 'opens a new issue when Report Issue button is clicked', () => {
+		const openSpy = jest
+			.spyOn( window, 'open' )
+			.mockImplementation( () => null );
+
+		render(
+			<ErrorBoundary>
+				<ThrowError />
+			</ErrorBoundary>
+		);
+
+		fireEvent.click( screen.getByText( 'Report Issue on Github' ) );
+
+		expect( openSpy ).toHaveBeenCalledWith(
+			expect.stringContaining(
+				'https://github.com/woocommerce/woocommerce/issues/new?body='
+			),
+			'_blank'
+		);
+
+		openSpy.mockRestore();
+	} );
+} );
