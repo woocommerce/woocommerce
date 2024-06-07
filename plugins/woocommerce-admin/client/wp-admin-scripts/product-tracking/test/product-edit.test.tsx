46c96a4106cbab2c7e74ea1ff5b8c42e2a5a331d
Ilyas Foo <foo.ilyas@gmail.com>
2024-06-07 17:51:11 +0800

    Add product_edit_view test and falsey test

--- plugins/woocommerce-admin/client/wp-admin-scripts/product-tracking/test/product-edit.test.tsx
+++ plugins/woocommerce-admin/client/wp-admin-scripts/product-tracking/test/product-edit.test.tsx
@@ -13,21 +13,24 @@ jest.mock( '@woocommerce/tracks', () => ( {
 jest.mock( '../shared', () => ( {
 	addExitPageListener: jest.fn().mockImplementation( () => {} ),
 	initProductScreenTracks: jest.fn().mockImplementation( () => {} ),
+	getProductData: jest.fn().mockImplementation( () => ( { product_id: 1 } ) ),
 } ) );
 
 describe( 'Product Screen Tracking', () => {
 	beforeEach( () => {
 		jest.clearAllMocks();
 	} );
-	it( 'should trigger product_add_view event when productScreen.name is "new"', () => {
-		global.productScreen = { name: 'new' };
-		require( '../product-new' );
-		expect( recordEvent ).toHaveBeenCalledWith( 'product_add_view' );
+	it( 'should trigger product_edit_view event when productScreen.name is "edit"', () => {
+		global.productScreen = { name: 'edit' };
+		require( '../product-edit' );
+		expect( recordEvent ).toHaveBeenCalledWith( 'product_edit_view', {
+			product_id: 1,
+		} );
 	} );
 
-	it( 'should not trigger product_add_view event when productScreen.name is not "new"', () => {
+	it( 'should not trigger product_edit_view event when productScreen.name is not "edit"', () => {
 		global.productScreen = { name: '' };
-		require( '../product-new' );
+		require( '../product-edit' );
 		expect( recordEvent ).not.toHaveBeenCalled();
 	} );
 } );
