9e984896a3a690ddaa5e109f9bc359655173dfe0
Tarun Vijwani <tarun.vijwani@automattic.com>
2024-06-11 02:19:00 +0400

    Use useEffect to update the state correctly for the shipping address

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/shipping/shipping-address.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/shipping/shipping-address.tsx
@@ -8,6 +8,7 @@ import { ShippingAddress as ShippingAddressType } from '@woocommerce/settings';
 import PickupLocation from '@woocommerce/base-components/cart-checkout/pickup-location';
 import { CHECKOUT_STORE_KEY } from '@woocommerce/block-data';
 import { useSelect } from '@wordpress/data';
+import { useEffect, useState } from '@wordpress/element';
 
 export interface ShippingAddressProps {
 	setShippingCalculatorLabel: React.Dispatch<
@@ -31,38 +32,59 @@ export const ShippingAddress = ( {
 		select( CHECKOUT_STORE_KEY ).prefersCollection()
 	);
 
-	const hasFormattedAddress = !! formatShippingAddress( shippingAddress );
+	const [ showPickupLocation, setShowPickupLocation ] = useState( false );
 
-	// If it's the editor and store has no formatted address,
-	// then the shipping calculator labels will not be modified.
-	// Shipping calculator will display the default label.
-	if ( isEditor && ! hasFormattedAddress ) {
-		return null;
-	}
+	useEffect( () => {
+		const hasFormattedAddress = !! formatShippingAddress( shippingAddress );
 
-	if ( prefersCollection )
+		// If it's the editor and store has no formatted address,
+		// then the shipping calculator labels will not be modified.
+		// Shipping calculator will display the default label.
+		if ( isEditor && ! hasFormattedAddress ) {
+			return;
+		}
+
+		if ( prefersCollection ) {
+			setShowPickupLocation( true );
+			return;
+		}
+
+		setShowPickupLocation( false );
+
+		const formattedAddress = formatShippingAddress( shippingAddress );
+		let deliveryLabel;
+		if ( hasRates ) {
+			deliveryLabel = __( 'Delivers to', 'woocommerce' );
+		} else {
+			deliveryLabel = __(
+				'No delivery options available for',
+				'woocommerce'
+			);
+		}
+
+		if ( formattedAddress ) {
+			setShippingCalculatorLabel( deliveryLabel );
+			setShippingCalculatorAddress( formattedAddress );
+		}
+	}, [
+		shippingAddress,
+		hasRates,
+		prefersCollection,
+		isEditor,
+		setShippingCalculatorLabel,
+		setShippingCalculatorAddress,
+	] );
+
+	if ( showPickupLocation ) {
 		return (
 			<PickupLocation
 				setShippingCalculatorLabel={ setShippingCalculatorLabel }
 				setShippingCalculatorAddress={ setShippingCalculatorAddress }
 			/>
 		);
-	const formattedAddress = formatShippingAddress( shippingAddress );
-	let deliveryLabel;
-	if ( hasRates ) {
-		deliveryLabel = __( 'Delivers to', 'woocommerce' );
-	} else {
-		deliveryLabel = __(
-			'No delivery options available for',
-			'woocommerce'
-		);
 	}
 
-	setShippingCalculatorLabel( deliveryLabel );
-	if ( formattedAddress ) {
-		setShippingCalculatorAddress( formattedAddress );
-	}
-	return <></>;
+	return null;
 };
 
 export default ShippingAddress;
