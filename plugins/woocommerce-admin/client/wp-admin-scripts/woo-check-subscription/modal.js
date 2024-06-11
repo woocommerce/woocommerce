0400957c9dcb15372557c1623e19f7479a40fcbe
Akeda Bagus <akeda.bagus@automattic.com>
2024-06-11 09:41:52 +0700

    Update modal design

--- plugins/woocommerce-admin/client/wp-admin-scripts/woo-check-subscription/modal.js
+++ plugins/woocommerce-admin/client/wp-admin-scripts/woo-check-subscription/modal.js
@@ -3,10 +3,30 @@
  */
 import { __, sprintf } from '@wordpress/i18n';
 import { Component } from '@wordpress/element';
-import { Button, Modal } from '@wordpress/components';
+import {
+	Button,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	CardMedia,
+	ExternalLink,
+	Flex,
+	FlexItem,
+	Icon,
+	Modal,
+	ResponsiveWrapper,
+} from '@wordpress/components';
+import { commentContent, reusableBlock } from '@wordpress/icons';
 import { addQueryArgs } from '@wordpress/url';
+import { Text } from '@woocommerce/experimental';
 import { getSetting } from '@woocommerce/settings';
 import apiFetch from '@wordpress/api-fetch';
+
+/**
+ * Internal dependencies
+ */
+import extensionsSvg from './extensions.svg';
 
 export class CheckSubscriptionModal extends Component {
 	constructor( props ) {
@@ -38,6 +58,73 @@ export class CheckSubscriptionModal extends Component {
 		this.setState( { isModalOpen: false } )
 	}
 
+	renderBenefits() {
+		return (
+			<div className="woocommerce-subscription-benefits">
+				<div className="woocommerce-subscription-benefits__item">
+					<div className="woocommerce-subscription-benefits__icon">
+						<Icon icon={ reusableBlock } />
+					</div>
+					<div className="woocommerce-subscription-benefits__content">
+						<Text
+							as="h3"
+							lineHeight={ "20px" }
+							size={ "14px" }
+							weight={ 600 }
+						>
+							{ __( 'New features and improvements', 'woocommerce' ) }
+						</Text>
+						<Text as="p">
+							{ __( 'Continue receiving the latest product updates and ongoing improvements.', 'woocommerce' )
+							}
+						</Text>
+					</div>
+				</div>
+
+				<div className="woocommerce-subscription-benefits__item">
+					<div className="woocommerce-subscription-benefits__icon">
+						<Icon icon={ commentContent } />
+					</div>
+					<div className="woocommerce-subscription-benefits__content">
+						<Text
+							as="h3"
+							lineHeight={ "20px" }
+							size={ "14px" }
+							weight={ 600 }
+						>
+							{ __( 'Get help when you need it', 'woocommerce' ) }
+						</Text>
+						<Text as="p">
+							{ __( 'Continue receiving streamlined support from our global support team.', 'woocommerce' )
+							}
+						</Text>
+					</div>
+				</div>
+
+				<div className="woocommerce-subscription-benefits__item">
+					<div className="woocommerce-subscription-benefits__icon">
+						<Icon icon={ commentContent } />
+					</div>
+					<div className="woocommerce-subscription-benefits__content">
+						<Text
+							as="h3"
+							lineHeight={ "20px" }
+							size={ "14px" }
+							weight={ 600 }
+						>
+							{ __( 'Support the ecosystem', 'woocommerce' ) }
+						</Text>
+						<Text as="p">
+							{ __( 'This helps support the Woo ecosystem to continuously improve extensions and themes.', 'woocommerce' )
+							}
+							<ExternalLink href="#">{ __( 'Read more', 'woocommerce' ) }</ExternalLink>
+						</Text>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
 	render() {
 		const { isModalOpen } = this.state;
 		if ( ! isModalOpen ) {
@@ -46,39 +133,71 @@ export class CheckSubscriptionModal extends Component {
 
 		return (
 			<Modal
-				title={ __( 'Missing subscription', 'woocommerce' ) }
+				style={ { borderRadius: '2px' } }
 				onRequestClose={ () => this.dismiss() }
-				className="woocommerce-navigation-opt-out-modal"
+				className="woocommerce-check-subscription-modal"
 			>
-				<p>
-					{
-						sprintf(
-							__(
-								"We couldn't find active subscription for %s on your store.",
-								'woocommerce'
-							),
-							this.props.productName
-						)
-					}
-				</p>
+				<Flex
+					gap={ 0 }
+					align={ 'stretch' }
+				>
+					<FlexItem>
+						<Card className="primary">
+							<CardHeader>
+								<div>
+									<Text
+										className="subscription-status subscription-status__expired"
+									>
+										{ 'Expired' }
+									</Text>
+								</div>
+								<h2>
+									{
+										sprintf(
+											__(
+												'Renew %s',
+												'woocommerce'
+											),
+											this.props.productName
+										)
+									}
+								</h2>
+							</CardHeader>
+							<CardBody>
+								{ this.renderBenefits() }
+							</CardBody>
+							<CardFooter>
+								<Button
+									isDefault
+									onClick={ () => this.dismiss() }
+								>
+									{ __( 'Maybe later', 'woocommerce' ) }
+								</Button>
 
-				<div className="woocommerce-navigation-opt-out-modal__actions">
-					<Button
-						isDefault
-						onClick={ () => this.dismiss() }
-					>
-						{ __( 'Remind me later', 'woocommerce' ) }
-					</Button>
-
-					<Button
-						isPrimary
-						target="_blank"
-						href={ this.props.manageSubscriptionsUrl }
-						onClick={ () => this.dismiss() }
-					>
-						{ __( 'Manage subscriptions', 'woocommerce' ) }
-					</Button>
-				</div>
+								<Button
+									isPrimary
+									target="_blank"
+									href={ this.props.manageSubscriptionsUrl }
+									onClick={ () => this.dismiss() }
+								>
+									{ __( 'Manage subscriptions', 'woocommerce' ) }
+								</Button>
+							</CardFooter>
+						</Card>
+					</FlexItem>
+					<FlexItem>
+						<Card className="secondary">
+							<CardMedia>
+								<ResponsiveWrapper naturalWidth={ 240 } naturalHeight={ 240 }>
+									<img
+										src={ extensionsSvg }
+										alt=""
+									/>
+								</ResponsiveWrapper>
+							</CardMedia>
+						</Card>
+					</FlexItem>
+				</Flex>
 			</Modal>
 		);
 	}
