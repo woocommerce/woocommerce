2b11ab04420e71cfa4e006939dc86dc7513e7076
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-06-11 12:03:48 +0800

    Remove onboarding welcome modals

--- plugins/woocommerce-admin/client/navigation/components/intro-modal/index.js
+++ plugins/woocommerce-admin/client/navigation/components/intro-modal/index.js
@@ -16,7 +16,6 @@ import './style.scss';
 import NavInto1 from './images/nav-intro-1.png';
 import NavInto2 from './images/nav-intro-2.png';
 import NavInto3 from './images/nav-intro-3.png';
-import { WELCOME_MODAL_DISMISSED_OPTION_NAME } from '../../../homescreen/constants';
 
 export const INTRO_MODAL_DISMISSED_OPTION_NAME =
 	'woocommerce_navigation_intro_modal_dismissed';
@@ -26,29 +25,20 @@ export const IntroModal = () => {
 
 	const { updateOptions } = useDispatch( OPTIONS_STORE_NAME );
 
-	const { isDismissed, isResolving, isWelcomeModalShown } = useSelect(
-		( select ) => {
-			const { getOption, isResolving: isOptionResolving } =
-				select( OPTIONS_STORE_NAME );
-			const dismissedOption = getOption(
-				INTRO_MODAL_DISMISSED_OPTION_NAME
-			);
+	const { isDismissed, isResolving } = useSelect( ( select ) => {
+		const { getOption, isResolving: isOptionResolving } =
+			select( OPTIONS_STORE_NAME );
+		const dismissedOption = getOption( INTRO_MODAL_DISMISSED_OPTION_NAME );
 
-			return {
-				isDismissed: dismissedOption === 'yes',
-				isWelcomeModalShown:
-					getOption( WELCOME_MODAL_DISMISSED_OPTION_NAME ) !== 'yes',
-				isResolving:
-					typeof dismissedOption === 'undefined' ||
-					isOptionResolving( 'getOption', [
-						INTRO_MODAL_DISMISSED_OPTION_NAME,
-					] ) ||
-					isOptionResolving( 'getOption', [
-						WELCOME_MODAL_DISMISSED_OPTION_NAME,
-					] ),
-			};
-		}
-	);
+		return {
+			isDismissed: dismissedOption === 'yes',
+			isResolving:
+				typeof dismissedOption === 'undefined' ||
+				isOptionResolving( 'getOption', [
+					INTRO_MODAL_DISMISSED_OPTION_NAME,
+				] ),
+		};
+	} );
 
 	const dismissModal = () => {
 		updateOptions( {
@@ -61,12 +51,12 @@ export const IntroModal = () => {
 	// Dismiss the modal when the welcome modal is shown.
 	// It is likely in this case that the navigation is on by default.
 	useEffect( () => {
-		if ( ! isResolving && isWelcomeModalShown && ! isDismissed ) {
+		if ( ! isResolving && ! isDismissed ) {
 			dismissModal();
 		}
-	}, [ isDismissed, isResolving, isWelcomeModalShown ] );
+	}, [ isDismissed, isResolving ] );
 
-	if ( ! isOpen || isDismissed || isResolving || isWelcomeModalShown ) {
+	if ( ! isOpen || isDismissed || isResolving ) {
 		return null;
 	}
 
