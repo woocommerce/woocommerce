96db2d178ebc851593a0a17aa8cc49e7dfba8ef0
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-06-12 16:35:33 -0300

    Fix "Flext" typo

--- plugins/woocommerce-admin/client/profile-wizard/steps/store-details/index.js
+++ plugins/woocommerce-admin/client/profile-wizard/steps/store-details/index.js
@@ -42,14 +42,14 @@ import './style.scss';
 
 // FlexItem is not available until WP version 5.5. This code is safe to remove
 // once the minimum WP supported version becomes 5.5.
-const FlextItemSubstitute = ( { children, align } ) => {
+const FlexItemSubstitute = ( { children, align } ) => {
 	const style = {
 		display: 'flex',
 		'justify-content': align ? 'center' : 'flex-start',
 	};
 	return <div style={ style }>{ children }</div>;
 };
-const FlexItem = MaybeFlexItem || FlextItemSubstitute;
+const FlexItem = MaybeFlexItem || FlexItemSubstitute;
 
 const LoadingPlaceholder = () => (
 	<div className="woocommerce-admin__store-details__spinner">
