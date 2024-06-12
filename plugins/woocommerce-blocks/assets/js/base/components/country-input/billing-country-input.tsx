f2f1f7720506794e4169027ab1b2ba1829d437c8
Sam Seay <samueljseay@gmail.com>
2024-06-12 11:28:32 +0800

    Remove redundant JSX wrapping

--- plugins/woocommerce-blocks/assets/js/base/components/country-input/billing-country-input.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/country-input/billing-country-input.tsx
@@ -13,11 +13,7 @@ const BillingCountryInput = ( props: CountryInputProps ): JSX.Element => {
 	//  TODO - are errorMessage and errorId still relevant when select always has a value?
 	const { errorMessage: _, errorId: __, ...restOfProps } = props;
 
-	return (
-		<>
-			<CountryInput countries={ ALLOWED_COUNTRIES } { ...restOfProps } />
-		</>
-	);
+	return <CountryInput countries={ ALLOWED_COUNTRIES } { ...restOfProps } />;
 };
 
 export default BillingCountryInput;
