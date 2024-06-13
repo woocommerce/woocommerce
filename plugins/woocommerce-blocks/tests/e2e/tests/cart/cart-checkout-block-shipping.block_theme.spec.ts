bbe14abfb27215469fd2bdf8f77403e3b760f1cd
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-06-13 14:06:40 -0300

    Fix "addres" typo

--- plugins/woocommerce-blocks/tests/e2e/tests/cart/cart-checkout-block-shipping.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/cart/cart-checkout-block-shipping.block_theme.spec.ts
@@ -78,7 +78,7 @@ test.describe( 'Shopper → Shipping', () => {
 
 		await expect(
 			userPage.getByText(
-				'Shipping options will be displayed here after entering your full shipping addres'
+				'Shipping options will be displayed here after entering your full shipping address'
 			)
 		).toBeVisible();
 
@@ -86,7 +86,7 @@ test.describe( 'Shopper → Shipping', () => {
 
 		await expect(
 			userPage.getByText(
-				'Shipping options will be displayed here after entering your full shipping addres'
+				'Shipping options will be displayed here after entering your full shipping address'
 			)
 		).toBeHidden();
 	} );
