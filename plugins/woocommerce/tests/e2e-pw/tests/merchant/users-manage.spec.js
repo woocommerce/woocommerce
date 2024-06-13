687f7a8588f7caa29a627e9df874da96def5743a
Lucas Bustamante <lucasfbustamante@gmail.com>
2024-06-13 12:33:08 -0300

    Tweak

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/users-manage.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/users-manage.spec.js
@@ -62,7 +62,7 @@ async function userDeletionTest( page, username ) {
 
 	await test.step( 'confirm deletion', async () => {
 		await expect(
-			page.getByRole( 'link', { name: `${ username }`, exact: true } )
+			page.getByRole( 'link', { name: username, exact: true } )
 		).toBeVisible();
 		await page.getByRole( 'button', { name: 'Confirm Deletion' } ).click();
 	} );
