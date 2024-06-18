1b9ebf24af1356b3eac268f484db55a0c8948324
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-06-18 11:29:49 +0200

    Remove closing the welcome popup conditional

--- plugins/woocommerce-blocks/tests/e2e/tests/templates/checkout-template.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/templates/checkout-template.block_theme.spec.ts
@@ -63,13 +63,7 @@ test.describe( 'Test the checkout template', () => {
 		await admin.page.locator( '#wp-admin-bar-site-editor a' ).click();
 
 		// Close welcome popup.
-		const getStartedButton = admin.page.getByRole( 'button', {
-			name: 'Get started',
-		} );
-		const isGetStartedButtonVisible = await getStartedButton.isVisible();
-		if ( isGetStartedButtonVisible ) {
-			await getStartedButton.click();
-		}
+		await admin.page.getByRole( 'button', { name: 'Get started' } ).click();
 
 		await expect(
 			editor.canvas.getByRole( 'button', {
