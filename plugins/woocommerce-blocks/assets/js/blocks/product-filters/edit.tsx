d0a6389f4f2cba294fb38646cb2f97235149d50b
roykho <roykho77@gmail.com>
2024-06-19 07:11:35 -0700

    Add overlay settings

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/edit.tsx
@@ -5,17 +5,30 @@ import {
 	InnerBlocks,
 	useBlockProps,
 	useInnerBlocksProps,
+	InspectorControls,
 } from '@wordpress/block-editor';
 import { BlockEditProps, InnerBlockTemplate } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 import { useCollection } from '@woocommerce/base-context/hooks';
 import { AttributeTerm } from '@woocommerce/types';
-import { Spinner } from '@wordpress/components';
+import {
+	PanelBody,
+	RadioControl,
+	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
+	__experimentalToggleGroupControl as ToggleGroupControl,
+	Spinner,
+	ExternalLink,
+	RangeControl,
+} from '@wordpress/components';
+import { Icon, settings, menu } from '@wordpress/icons';
+import { getSetting } from '@woocommerce/settings';
+import { WC_BLOCKS_IMAGE_URL } from '@woocommerce/block-settings';
 
 /**
  * Internal dependencies
  */
 import type { ProductFiltersBlockAttributes } from './types';
+import './editor.scss';
 
 const TEMPLATE: InnerBlockTemplate[] = [
 	[
@@ -93,15 +106,19 @@ const addHighestProductCountAttributeToTemplate = (
 	} );
 };
 
-export const Edit = ( {}: BlockEditProps< ProductFiltersBlockAttributes > ) => {
+export const Edit = ( {
+	setAttributes,
+	attributes,
+}: BlockEditProps< ProductFiltersBlockAttributes > ) => {
 	const blockProps = useBlockProps();
-	const { results: attributes, isLoading } = useCollection< AttributeTerm >( {
-		namespace: '/wc/store/v1',
-		resourceName: 'products/attributes',
-	} );
+	const { results: attributeTerms, isLoading } =
+		useCollection< AttributeTerm >( {
+			namespace: '/wc/store/v1',
+			resourceName: 'products/attributes',
+		} );
 
 	const highestProductCountAttribute =
-		attributes.reduce< AttributeTerm | null >(
+		attributeTerms.reduce< AttributeTerm | null >(
 			( attributeWithMostProducts, attribute ) => {
 				if ( attributeWithMostProducts === null ) {
 					return attribute;
@@ -121,8 +138,185 @@ export const Edit = ( {}: BlockEditProps< ProductFiltersBlockAttributes > ) => {
 		return <Spinner />;
 	}
 
+	const templatePartEditUri = getSetting(
+		'productFiltersTemplatePartEditUri',
+		''
+	) as string;
+
 	return (
 		<div { ...blockProps }>
+			<InspectorControls>
+				<PanelBody title={ __( 'Overlay', 'woocommerce' ) }>
+					<ToggleGroupControl
+						className="wc-block-editor-product-filters__overlay-toggle"
+						isBlock={ true }
+						value={ attributes.overlay }
+						onChange={ ( value: 'never' | 'mobile' | 'always' ) => {
+							setAttributes( { overlay: value } );
+						} }
+					>
+						<ToggleGroupControlOption
+							value={ 'never' }
+							label={ __( 'Never', 'woocommerce' ) }
+						/>
+						<ToggleGroupControlOption
+							value={ 'mobile' }
+							label={ __( 'Mobile', 'woocommerce' ) }
+						/>
+						<ToggleGroupControlOption
+							value={ 'always' }
+							label={ __( 'Always', 'woocommerce' ) }
+						/>
+					</ToggleGroupControl>
+					{ ( attributes.overlay === 'always' ||
+						attributes.overlay === 'mobile' ) && (
+						<>
+							<img
+								className="wc-block-editor-product-filters__overlay-image"
+								src={ `${ WC_BLOCKS_IMAGE_URL }blocks/product-filters/overlay-preview.svg` }
+								alt={ __( 'Overlay preview', 'woocommerce' ) }
+							/>
+							<p className="wc-block-editor-product-filters__overlay-link">
+								<ExternalLink href={ templatePartEditUri }>
+									{ __( 'Edit overlay', 'woocommerce' ) }
+								</ExternalLink>
+							</p>
+						</>
+					) }
+					{ ( attributes.overlay === 'mobile' ||
+						attributes.overlay === 'always' ) && (
+						<>
+							<RadioControl
+								className="wc-block-editor-product-filters__overlay-button-style-toggle"
+								label={ __( 'BUTTON', 'woocommerce' ) }
+								selected={ attributes.overlayButtonStyle }
+								onChange={ (
+									value: 'label-icon' | 'label' | 'icon'
+								) => {
+									setAttributes( {
+										overlayButtonStyle: value,
+									} );
+								} }
+								options={ [
+									{
+										value: 'label-icon',
+										label: __(
+											'Label and icon',
+											'woocommerce'
+										),
+									},
+									{
+										value: 'label',
+										label: __(
+											'Label only',
+											'woocommerce'
+										),
+									},
+									{
+										value: 'icon',
+										label: __( 'Icon only', 'woocommerce' ),
+									},
+								] }
+							/>
+							{ attributes.overlayButtonStyle !== 'label' && (
+								<>
+									<ToggleGroupControl
+										className="wc-block-editor-product-filters__overlay-button-toggle"
+										isBlock={ true }
+										value={ attributes.overlayButton }
+										onChange={ (
+											value:
+												| 'filter-icon-1'
+												| 'filter-icon-2'
+												| 'filter-icon-3'
+												| 'filter-icon-4'
+										) => {
+											setAttributes( {
+												overlayButton: value,
+											} );
+										} }
+									>
+										<ToggleGroupControlOption
+											value={ 'filter-icon-1' }
+											label={
+												<Icon
+													size={ 32 }
+													icon={
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 24 24"
+															width="32"
+															height="32"
+															aria-hidden="true"
+														>
+															<path d="M10.541 4.20007H5.20245C4.27908 4.20007 3.84904 5.34461 4.54394 5.95265L10.541 11.2001V16.2001L10.541 17.9428C10.541 18.1042 10.619 18.2558 10.7504 18.3496L13.2504 20.1353C13.5813 20.3717 14.041 20.1352 14.041 19.7285V11.2001L19.3339 5.90718C19.9639 5.27722 19.5177 4.20007 18.6268 4.20007H13.041H10.541Z" />
+														</svg>
+													}
+												/>
+											}
+										/>
+										<ToggleGroupControlOption
+											value={ 'filter-icon-2' }
+											label={
+												<Icon
+													size={ 32 }
+													icon={
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 24 24"
+															width="32"
+															height="32"
+															aria-hidden="true"
+														>
+															<path
+																d="M4 11.5H8V10H4V11.5ZM0 0V1.5H12V0H0ZM2 6.5H10V5H2V6.5Z"
+																transform="translate(6,6)"
+															/>
+														</svg>
+													}
+												/>
+											}
+										/>
+										<ToggleGroupControlOption
+											value={ 'filter-icon-3' }
+											label={
+												<Icon
+													size={ 32 }
+													icon={ menu }
+												/>
+											}
+										/>
+										<ToggleGroupControlOption
+											value={ 'filter-icon-4' }
+											label={
+												<Icon
+													size={ 32 }
+													icon={ settings }
+												/>
+											}
+										/>
+									</ToggleGroupControl>
+									<RangeControl
+										label={ __(
+											'ICON SIZE',
+											'woocommerce'
+										) }
+										value={ attributes.overlayButtonSize }
+										onChange={ ( value ) =>
+											setAttributes( {
+												overlayButtonSize: value,
+											} )
+										}
+										min={ 20 }
+										max={ 80 }
+										afterIcon={ 'px' }
+									/>
+								</>
+							) }
+						</>
+					) }
+				</PanelBody>
+			</InspectorControls>
 			<InnerBlocks templateLock={ false } template={ updatedTemplate } />
 		</div>
 	);
--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/types.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/types.ts
@@ -1,7 +1,8 @@
-/**
- * Internal dependencies
- */
-
 export interface ProductFiltersBlockAttributes {
 	productId?: string;
+	setAttributes: ( attributes: ProductFiltersBlockAttributes ) => void;
+	overlay: string;
+	overlayButton: string;
+	overlayButtonStyle: string;
+	overlayButtonSize: number | undefined;
 }
