235ff7fcf2c77f0b1d2d521d80870dd0e9912da2
Luigi Teschio <gigitux@gmail.com>
2024-06-20 09:41:49 +0200

    CYS: fix crash of CYS on WordPress 6.6

--- plugins/woocommerce-admin/bin/modified-editsite-lock-unlock.js
+++ plugins/woocommerce-admin/bin/modified-editsite-lock-unlock.js
@@ -1,14 +1,20 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
 import { __dangerousOptInToUnstableAPIsOnlyForCoreModules } from '@wordpress/private-apis';
 
-const oldConsentString =
-	'I know using unstable features means my plugin or theme will inevitably break on the next WordPress release.';
-const newConsentString =
-	'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.';
+const wordPressConsentString = {
+	6.4: 'I know using unstable features means my plugin or theme will inevitably break on the next WordPress release.',
+	6.5: 'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.',
+	6.6: 'I acknowledge private features are not for use in themes or plugins and doing so will break in the next version of WordPress.',
+};
 
-let consentString = oldConsentString;
+const gutenbergConsentString = {
+	16.9: 'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress',
+	18.7: 'I acknowledge private features are not for use in themes or plugins and doing so will break in the next version of WordPress.',
+};
+
+let consentString;
 const wcSettings = window.wcSettings;
 const admin = wcSettings?.admin;
 
@@ -17,13 +23,26 @@ const gutenbergVersion = parseFloat( admin?.gutenberg_version );
 
 // Use the new consent string if the WP version is 6.4 and above
 if ( ! isNaN( wpVersion ) && wpVersion >= 6.4 ) {
-	consentString = newConsentString;
+	consentString = wordPressConsentString[ wpVersion ];
 }
+
+// Use the new consent string if the Gutenberg version is 6.4 and abo
 
 // Users can install the latest Gutenberg plugin manually
 // Use the new consent string for Gutenberg 16.9 and above
-if ( ! isNaN( gutenbergVersion ) && gutenbergVersion >= 16.9 ) {
-	consentString = newConsentString;
+if ( ! isNaN( gutenbergVersion ) ) {
+	const gutenbergConsentStringValues = Object.keys(
+		gutenbergConsentString
+	).map( parseFloat );
+
+	const gutenbergVersionKeys = gutenbergConsentStringValues.filter(
+		( value ) => gutenbergVersion >= value
+	);
+
+	if ( gutenbergVersionKeys.length ) {
+		consentString =
+			gutenbergConsentString[ Math.max( ...gutenbergVersionKeys ) ];
+	}
 }
 
 export const { lock, unlock } =
