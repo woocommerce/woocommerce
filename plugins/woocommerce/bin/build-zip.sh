5b4c63506bdbcc5600182e04f579d4f0b3f6a554
Vladimir Reznichenko <kalessil@gmail.com>
2024-06-26 12:09:31 +0200

    Tooling: pnpm related tweaks

--- plugins/woocommerce/bin/build-zip.sh
+++ plugins/woocommerce/bin/build-zip.sh
@@ -16,7 +16,7 @@ echo "Cleaning up assets..."
 find "$PROJECT_PATH/assets/css/." ! -name '.gitkeep' -type f -exec rm -f {} + && find "$PROJECT_PATH/assets/client/." ! -name '.gitkeep' -type f -exec rm -f {} + && find "$PROJECT_PATH/assets/js/." ! -name '.gitkeep' -type f -exec rm -f {} +
 
 echo "Installing PHP and JS dependencies..."
-pnpm install --no-optional
+pnpm install
 
 echo "Running JS Build..."
 if [ -z "${NODE_ENV}" ]; then
