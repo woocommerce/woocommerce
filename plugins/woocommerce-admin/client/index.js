a4a3722dceeb441214ceb9d7df0e4dcb44c6047b
rjchow <me@rjchow.com>
2024-06-27 20:04:18 +0800

    fix: fixed react18 React.render deprecation notice for WCAdmin
    removed intermediate variables

--- plugins/woocommerce-admin/client/index.js
+++ plugins/woocommerce-admin/client/index.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import '@wordpress/notices';
-import { render } from '@wordpress/element';
+import { createRoot } from '@wordpress/element';
 import { CustomerEffortScoreTracksContainer } from '@woocommerce/customer-effort-score';
 import {
 	withCurrentUserHydration,
@@ -50,11 +50,11 @@ if ( appRoot ) {
 		HydratedPageLayout =
 			withCurrentUserHydration( hydrateUser )( HydratedPageLayout );
 	}
-	render(
+
+	createRoot( appRoot ).render(
 		<ErrorBoundary>
 			<HydratedPageLayout />
-		</ErrorBoundary>,
-		appRoot
+		</ErrorBoundary>
 	);
 } else if ( embeddedRoot ) {
 	let HydratedEmbedLayout = withSettingsHydration(
@@ -66,7 +66,7 @@ if ( appRoot ) {
 			withCurrentUserHydration( hydrateUser )( HydratedEmbedLayout );
 	}
 	// Render the header.
-	render( <HydratedEmbedLayout />, embeddedRoot );
+	createRoot( embeddedRoot ).render( <HydratedEmbedLayout /> );
 
 	embeddedRoot.classList.remove( 'is-embed-loading' );
 
@@ -79,17 +79,16 @@ if ( appRoot ) {
 		wpBody.querySelector( '.wrap' );
 	const noticeContainer = document.createElement( 'div' );
 
-	render(
+	createRoot( wpBody.insertBefore( noticeContainer, wrap ) ).render(
 		<div className="woocommerce-layout">
 			<NoticeArea />
-		</div>,
-		wpBody.insertBefore( noticeContainer, wrap )
+		</div>
 	);
 	const embeddedBodyContainer = document.createElement( 'div' );
-	render(
-		<EmbeddedBodyLayout />,
+
+	createRoot(
 		wpBody.insertBefore( embeddedBodyContainer, wrap.nextSibling )
-	);
+	).render( <EmbeddedBodyLayout /> );
 
 	possiblyRenderSettingsSlots();
 
@@ -112,10 +111,8 @@ if (
 	// Set up customer effort score survey.
 	( function () {
 		const root = appRoot || embeddedRoot;
-
-		render(
-			<CustomerEffortScoreTracksContainer />,
+		createRoot(
 			root.insertBefore( document.createElement( 'div' ), null )
-		);
+		).render( <CustomerEffortScoreTracksContainer /> );
 	} )();
 }
