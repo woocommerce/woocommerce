302eff9aa76e8be842af27c6fbd7b69b6052355a
Veljko V <veljano@yahoo.com>
2024-06-27 16:33:26 +0200

    Fix Core with Gutenberg e2e tests (#48896)
    * Fix Core Gutenberg e2e tests
    
    * Fix merchant create checkout block test

--- plugins/woocommerce/tests/e2e-pw/utils/editor.js
+++ plugins/woocommerce/tests/e2e-pw/utils/editor.js
@@ -47,12 +47,10 @@ const fillPageTitle = async ( page, title ) => {
 };
 
 const insertBlock = async ( page, blockName ) => {
-	const canvas = await getCanvas( page );
-	// Click the title to activate the block inserter.
-	await canvas.getByRole( 'textbox', { name: 'Add title' } ).click();
-	await canvas.getByLabel( 'Add block' ).click();
+	await page.getByLabel( 'Toggle block inserter' ).click();
 	await page.getByPlaceholder( 'Search', { exact: true } ).fill( blockName );
 	await page.getByRole( 'option', { name: blockName, exact: true } ).click();
+	await page.getByLabel( 'Toggle block inserter' ).click();
 };
 
 const insertBlockByShortcut = async ( page, blockName ) => {
