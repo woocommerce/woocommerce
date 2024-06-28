d2946c91d08c9d218f237af0cc354e2a077568e6
Damián Suárez <rdsuarez@gmail.com>
2024-06-28 08:57:14 +0100

    detect attributes loaded at data-layer level

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/products/block-editor/helpers/wait-for-global-attributes-loaded.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/products/block-editor/helpers/wait-for-global-attributes-loaded.js
@@ -1,18 +1,24 @@
 /**
- * Waits for the spinner to appear and disappear,
- * indicating that attributes have been loaded.
+ * Waits for the global attributes to be loaded on the page.
  *
- * This function confirms that the attributes have been
- * loaded by waiting for a spinner to appear and then disappear.
- * The spinner indicates that a loading process is occurring,
- * and its disappearance indicates that the process is complete.
+ * This function waits until the `hasFinishedResolution` selector
+ * from the `wc/admin/products/attributes` store indicates that
+ * the product attributes are no longer loading.
  *
  * @param {Object} page - The Playwright Page object.
+ * @return {Promise<void>} A promise that resolves when the global attributes are loaded.
  */
 export async function waitForGlobalAttributesLoaded( page ) {
-	const spinnerLocator = page.locator(
-		'.woocommerce-new-attribute-modal__table-row .components-spinner'
-	);
-	await spinnerLocator.waitFor( { state: 'visible' } );
-	await spinnerLocator.waitFor( { state: 'hidden' } );
+	await page.waitForFunction( () => {
+		const storeId = 'wc/admin/products/attributes';
+		const attributeSortCriteria = { order_by: 'name' };
+
+		const isLoadingAttributes = ! wp.data
+			.select( storeId )
+			.hasFinishedResolution( 'getProductAttributes', [
+				attributeSortCriteria,
+			] );
+
+		return ! isLoadingAttributes;
+	} );
 }
