4973becb441daf5ad36746554d5dbf81c66fe323
Damián Suárez <rdsuarez@gmail.com>
2024-06-28 18:32:10 +0100

    remove console.log to dev purpose

--- packages/js/product-editor/src/components/attribute-control/attribute-table-row.tsx
+++ packages/js/product-editor/src/components/attribute-control/attribute-table-row.tsx
@@ -461,7 +461,6 @@ export const AttributeTableRow: React.FC< AttributeTableRowProps > = ( {
 							);
 						}
 					} }
-					onInputChange={ console.log }
 					__experimentalExpandOnFocus={ true }
 					__experimentalAutoSelectFirstMatch={ true }
 					__experimentalShowHowTo={ true }
