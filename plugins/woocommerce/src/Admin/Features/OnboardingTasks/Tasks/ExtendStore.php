f07b901c3e34a9cfb75aeffcf01277a30461db8a
prahesa.setia <prahesa.kusuma.setia@automattic.com>
2024-07-01 11:35:23 +0700

    update title to "Enhance your store with extensions"

--- plugins/woocommerce/src/Admin/Features/OnboardingTasks/Tasks/ExtendStore.php
+++ plugins/woocommerce/src/Admin/Features/OnboardingTasks/Tasks/ExtendStore.php
@@ -23,7 +23,7 @@ class ExtendStore extends Task {
 	 * @return string
 	 */
 	public function get_title() {
-		return __( 'Extend your store', 'woocommerce' );
+		return __( 'Enhance your store with extensions', 'woocommerce' );
 	}
 
 	/**
