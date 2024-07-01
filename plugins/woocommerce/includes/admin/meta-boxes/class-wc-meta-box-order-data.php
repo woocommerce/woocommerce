5f9611ec4a364d53ab302c543cd84fac2de3bffb
Thomas Roberts <thomas.roberts@automattic.com>
2024-07-01 14:38:03 +0100

    Change wp_kses_post to wp_kses for all order note usage

--- plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-order-data.php
+++ plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-order-data.php
@@ -615,7 +615,7 @@ class WC_Meta_Box_Order_Data {
 								?>
 								<p class="form-field form-field-wide">
 									<label for="customer_note"><?php esc_html_e( 'Customer provided note', 'woocommerce' ); ?>:</label>
-									<textarea rows="1" cols="40" name="customer_note" tabindex="6" id="excerpt" placeholder="<?php esc_attr_e( 'Customer notes about the order', 'woocommerce' ); ?>"><?php echo wp_kses_post( $order->get_customer_note() ); ?></textarea>
+									<textarea rows="1" cols="40" name="customer_note" tabindex="6" id="excerpt" placeholder="<?php esc_attr_e( 'Customer notes about the order', 'woocommerce' ); ?>"><?php echo wp_kses( $order->get_customer_note(), array() ); ?></textarea>
 								</p>
 							<?php endif; ?>
 						</div>
