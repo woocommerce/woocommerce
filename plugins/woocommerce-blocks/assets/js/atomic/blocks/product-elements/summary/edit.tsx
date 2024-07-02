fe524d25e533bce57b9c09e098ba205a9ffe86df
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-07-02 11:57:48 +0200

    Extract controls to spearate components, update descriptions and cleanup tyopes

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/edit.tsx
@@ -4,8 +4,6 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import { useEffect } from '@wordpress/element';
-import type { BlockEditProps } from '@wordpress/blocks';
-import type { ProductQueryContext as Context } from '@woocommerce/blocks/product-query/types';
 import {
 	RangeControl,
 	ToggleControl,
@@ -23,20 +21,110 @@ import {
 import Block from './block';
 import withProductSelector from '../shared/with-product-selector';
 import {
-	BLOCK_TITLE as label,
+	BLOCK_TITLE as title,
 	BLOCK_ICON as icon,
 	BLOCK_DESCRIPTION as description,
 } from './constants';
 import './editor.scss';
 import { useIsDescendentOfSingleProductBlock } from '../shared/use-is-descendent-of-single-product-block';
 import { useIsDescendentOfSingleProductTemplate } from '../shared/use-is-descendent-of-single-product-template';
-import type { Attributes } from './types';
+import type { EditProps, ControlProps } from './types';
+
+const ShowDescriptionIfEmptyControl = ( {
+	showDescriptionIfEmpty,
+	setAttributes,
+}: ControlProps< 'showDescriptionIfEmpty' > ) => {
+	const label = __( 'Show description if empty', 'woocommerce' );
+	const help = __(
+		"Display the product description if it doesn't have a summary",
+		'woocommerce'
+	);
+
+	return (
+		<ToolsPanelItem
+			label={ label }
+			hasValue={ () => showDescriptionIfEmpty === true }
+			isShownByDefault
+		>
+			<ToggleControl
+				label={ label }
+				help={ help }
+				checked={ showDescriptionIfEmpty }
+				onChange={ ( value ) => {
+					setAttributes( {
+						showDescriptionIfEmpty: value,
+					} );
+				} }
+			/>
+		</ToolsPanelItem>
+	);
+};
+
+const MaxWordCountControl = ( {
+	summaryLength,
+	setAttributes,
+}: ControlProps< 'summaryLength' > ) => {
+	const label = __( 'Max word count', 'woocommerce' );
+	const help = __( 'Set to 0 to remove the limit completely', 'woocommerce' );
+
+	return (
+		<ToolsPanelItem
+			label={ label }
+			hasValue={ () => summaryLength !== 0 }
+			isShownByDefault
+		>
+			<RangeControl
+				label={ label }
+				help={ help }
+				value={ summaryLength }
+				onChange={ ( value ) => {
+					setAttributes( {
+						summaryLength: value || 0,
+					} );
+				} }
+				min={ 0 }
+				max={ 1000 }
+				step={ 5 }
+			/>
+		</ToolsPanelItem>
+	);
+};
+
+const LinkToDescriptionControl = ( {
+	showLink,
+	setAttributes,
+}: ControlProps< 'showLink' > ) => {
+	const label = __( 'Link to description', 'woocommerce' );
+	const help = __(
+		"Display a button to let shoppers jump to the product's description",
+		'woocommerce'
+	);
+
+	return (
+		<ToolsPanelItem
+			label={ label }
+			hasValue={ () => showLink === false }
+			isShownByDefault
+		>
+			<ToggleControl
+				label={ label }
+				help={ help }
+				checked={ showLink }
+				onChange={ ( value ) => {
+					setAttributes( {
+						showLink: value,
+					} );
+				} }
+			/>
+		</ToolsPanelItem>
+	);
+};
 
 const Edit = ( {
 	attributes,
 	setAttributes,
 	context,
-}: BlockEditProps< Attributes > & { context: Context } ): JSX.Element => {
+}: EditProps ): JSX.Element => {
 	const blockProps = useBlockProps();
 	const { showDescriptionIfEmpty, showLink, summaryLength } = attributes;
 
@@ -77,67 +165,20 @@ const Edit = ( {
 						setAttributes( defaultSettings );
 					} }
 				>
-					<ToolsPanelItem
-						label={ __(
-							'Fallback to product description',
-							'woocommerce'
-						) }
-						hasValue={ () => showDescriptionIfEmpty === true }
-						isShownByDefault
-					>
-						<ToggleControl
-							label={ __(
-								'Fallback to product description',
-								'woocommerce'
-							) }
-							checked={ showDescriptionIfEmpty }
-							onChange={ ( value ) => {
-								setAttributes( {
-									showDescriptionIfEmpty: value,
-								} );
-							} }
+					<ShowDescriptionIfEmptyControl
+						showDescriptionIfEmpty={ showDescriptionIfEmpty }
+						setAttributes={ setAttributes }
+					/>
+					{ showDescriptionIfEmpty && (
+						<MaxWordCountControl
+							summaryLength={ summaryLength }
+							setAttributes={ setAttributes }
 						/>
-					</ToolsPanelItem>
-					<ToolsPanelItem
-						label={ __( 'Show link on new line', 'woocommerce' ) }
-						hasValue={ () => showLink === false }
-						isShownByDefault
-					>
-						<ToggleControl
-							label={ __(
-								'Show link on new line',
-								'woocommerce'
-							) }
-							checked={ showLink }
-							onChange={ ( value ) => {
-								setAttributes( {
-									showLink: value,
-								} );
-							} }
-						/>
-					</ToolsPanelItem>
-					<ToolsPanelItem
-						label={ __( 'Max word count', 'woocommerce' ) }
-						hasValue={ () => summaryLength !== 0 }
-						isShownByDefault
-					>
-						<RangeControl
-							label={ __( 'Max word count', 'woocommerce' ) }
-							value={ summaryLength }
-							onChange={ ( value ) => {
-								setAttributes( {
-									summaryLength: value || 0,
-								} );
-							} }
-							min={ 0 }
-							max={ 1000 }
-							step={ 5 }
-							help={ __(
-								'Set to 0 to remove the limit completely',
-								'woocommerce'
-							) }
-						/>
-					</ToolsPanelItem>
+					) }
+					<LinkToDescriptionControl
+						showLink={ showLink }
+						setAttributes={ setAttributes }
+					/>
 				</ToolsPanel>
 			</InspectorControls>
 		</div>
@@ -145,4 +186,4 @@ const Edit = ( {
 };
 
 // @todo: Refactor this to remove the HOC 'withProductSelector()' component as users will not see this block in the inserter. Therefore, we can export the Edit component by default. The HOC 'withProductSelector()' component should also be removed from other `product-elements` components. See also https://github.com/woocommerce/woocommerce-blocks/pull/7566#pullrequestreview-1168635469.
-export default withProductSelector( { icon, label, description } )( Edit );
+export default withProductSelector( { icon, title, description } )( Edit );
--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/types.ts
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/types.ts
@@ -1,3 +1,9 @@
+/**
+ * External dependencies
+ */
+import type { BlockEditProps } from '@wordpress/blocks';
+import type { ProductQueryContext as Context } from '@woocommerce/blocks/product-query/types';
+
 export interface Attributes {
 	productId: number;
 	isDescendentOfQueryLoop: boolean;
@@ -7,3 +13,13 @@ export interface Attributes {
 	showLink: boolean;
 	summaryLength: number;
 }
+
+export type SetAttributes = Pick<
+	BlockEditProps< Attributes >,
+	'setAttributes'
+>;
+
+export type EditProps = BlockEditProps< Attributes > & { context: Context };
+
+export type ControlProps< T extends keyof Attributes > = Pick< Attributes, T > &
+	SetAttributes;
