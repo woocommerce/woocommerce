929464554325f69e752dab1e6f381a4ee39c46fc
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-07-02 10:38:34 +0200

    Don't update 'wc_blocks_use_blockified_product_grid_block_as_template' when switching between block themes

--- plugins/woocommerce/src/Blocks/TemplateOptions.php
+++ plugins/woocommerce/src/Blocks/TemplateOptions.php
@@ -23,24 +23,20 @@ class TemplateOptions {
 	 * Checks the old and current themes and determines if the "wc_blocks_use_blockified_product_grid_block_as_template"
 	 * option need to be updated accordingly.
 	 *
+	 * @param string    $old_name Old theme name.
+	 * @param \WP_Theme $old_theme Instance of the old theme.
 	 * @return void
 	 */
-	public function check_should_use_blockified_product_grid_templates() {
-		$should_use_blockified = wc_current_theme_is_fse_theme();
-		// We don't need to do anything if the user switched to a classic theme.
-		if ( ! $should_use_blockified ) {
-			return;
+	public function check_should_use_blockified_product_grid_templates( $old_name, $old_theme ) {
+		if ( ! $old_theme->is_block_theme() && wc_current_theme_is_fse_theme() ) {
+			$option_name = Options::WC_BLOCK_USE_BLOCKIFIED_PRODUCT_GRID_BLOCK_AS_TEMPLATE;
+			// We previously stored "yes" or "no" values. This will convert them to true or false.
+			$option_value = wc_string_to_bool( get_option( $option_name ) );
+
+			// We don't need to do anything if the option is already set to true.
+			if ( ! $option_value ) {
+				update_option( $option_name, true );
+			}
 		}
-
-		$option_name = Options::WC_BLOCK_USE_BLOCKIFIED_PRODUCT_GRID_BLOCK_AS_TEMPLATE;
-		// We previously stored "yes" or "no" values. This will convert them to true or false.
-		$option_value = wc_string_to_bool( get_option( $option_name ) );
-
-		// We don't need to do anything if the option is already set to true.
-		if ( $option_value ) {
-			return;
-		}
-
-		update_option( $option_name, $should_use_blockified );
 	}
 }
