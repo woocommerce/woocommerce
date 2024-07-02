ee7da75766c0f8617bc8a5b69507d48c03e1690d
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-07-02 21:46:23 +0300

    Update env setup scripts

--- plugins/woocommerce/tests/e2e-pw/envs/default-permanent/env-setup.sh
+++ plugins/woocommerce/tests/e2e-pw/envs/default-permanent/env-setup.sh
@@ -3,13 +3,3 @@
 set -eo pipefail
 
 echo "Default permanent site setup."
-
-SCRIPT_PATH=$(
-  cd "$(dirname "${BASH_SOURCE[0]}")" || return
-  pwd -P
-)
-
-dotenvPath="$SCRIPT_PATH"/../../.env
-echo "Decrypting .env.enc file to $dotenvPath"
-
-openssl enc -aes-256-cbc -iter 1000 -d -pass env:E2E_ENV_KEY -in "$SCRIPT_PATH"/.env.enc -out "$dotenvPath"
--- plugins/woocommerce/tests/e2e-pw/run-tests-with-env.sh
+++ plugins/woocommerce/tests/e2e-pw/run-tests-with-env.sh
@@ -11,8 +11,22 @@ SCRIPT_PATH=$(
 )
 
 echo "Setting up environment: $envName"
+
+cleanDotEnv=0
+
+if [ -f "$SCRIPT_PATH/envs/$envName/.env.enc" ]; then
+	echo "Found encrypted .env file for environment: $envName"
+	"$SCRIPT_PATH/bin/dotenv.sh" -d "$SCRIPT_PATH/envs/$envName"
+	cleanDotEnv=1
+fi
+
 "$SCRIPT_PATH/envs/$envName/env-setup.sh"
 
 echo "Running tests with environment: '$envName'"
 echo "Arguments: $*"
 pnpm playwright test --config="$SCRIPT_PATH"/envs/"$envName"/playwright.config.js "$@"
+
+if [ "$cleanDotEnv" -eq 1 ]; then
+	echo "Cleaning up .env file"
+	rm "$SCRIPT_PATH/.env"
+fi
