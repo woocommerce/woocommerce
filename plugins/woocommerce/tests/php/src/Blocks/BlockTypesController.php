05d1877667f063e497daba601f3de29fce93e6a3
Thomas Roberts <thomas.roberts@automattic.com>
2024-07-02 11:24:55 +0100

    Add docblocks to test

--- plugins/woocommerce/tests/php/src/Blocks/BlockTypesController.php
+++ plugins/woocommerce/tests/php/src/Blocks/BlockTypesController.php
@@ -12,8 +12,19 @@ use Automattic\WooCommerce\Blocks\Package;
  */
 class BlockTypesController extends \WP_UnitTestCase {
 
+	/**
+	 * Holds the BlockTypesController under test.
+	 *
+	 * @var TestedBlockTypesController The BlockTypesController under test.
+	 */
 	private $block_types_controller;
 
+	/**
+	 * Sets up a new TestedBlockTypesController so it can be tested.
+	 *
+	 * @return void
+	 * @throws \Exception If there is no dependency for the given identifier in the container the setup will fail.
+	 */
 	protected function setUp(): void {
 		parent::setUp();
 		$this->block_types_controller = new TestedBlockTypesController(
@@ -22,9 +33,13 @@ class BlockTypesController extends \WP_UnitTestCase {
 		);
 	}
 
+	/**
+	 * Register 3 blocks, one will be allowed by full name, one by namespace,and one because it has a parent with a
+	 * woocommerce namespace.
+	 *
+	 * @return void
+	 */
 	public function test_block_should_have_data_attributes() {
-		// Register 3 blocks, one will be allowed by full name, one by namespace,and one because it has a parent with a
-		// woocommerce namespace.
 
 		// A block that will not be allowed data attributes.
 		register_block_type(
