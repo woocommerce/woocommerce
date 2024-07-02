3e93c6b0f5f6c92f23dcaff1ff2e9a0a935dd3c9
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-07-02 14:26:55 +0200

    Update types

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/types.ts
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/types.ts
@@ -3,12 +3,11 @@
  */
 import type { BlockEditProps } from '@wordpress/blocks';
 import type { ProductQueryContext as Context } from '@woocommerce/blocks/product-query/types';
+import type { CSSProperties } from '@wordpress/element';
 
 export interface Attributes {
 	productId: number;
-	isDescendentOfQueryLoop: boolean;
-	isDescendentOfSingleProductTemplate: boolean;
-	isDescendentOfSingleProductBlock: boolean;
+	isDescendantOfAllProducts: boolean;
 	showDescriptionIfEmpty: boolean;
 	showLink: boolean;
 	summaryLength: number;
@@ -23,3 +22,8 @@ export type EditProps = BlockEditProps< Attributes > & { context: Context };
 
 export type ControlProps< T extends keyof Attributes > = Pick< Attributes, T > &
 	SetAttributes;
+
+export type BlockProps = Attributes & {
+	style?: CSSProperties;
+	className?: string;
+};
