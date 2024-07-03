3df41f4ce140798867a12a3c5d964fd113c05830
Thomas Roberts <thomas.roberts@automattic.com>
2024-07-03 12:35:18 +0100

    Revert "Temporary change to code to make CI run in PR."
    This reverts commit 9dd6c945f4ab7252b47c5c64bf0a0aa81f9b899e.

--- plugins/woocommerce-blocks/assets/js/base/utils/render-frontend.tsx
+++ plugins/woocommerce-blocks/assets/js/base/utils/render-frontend.tsx
@@ -17,7 +17,6 @@ type BlockProps<
 	attributes?: TAttribute;
 };
 
-// Temporary change to code to make CI run.
 type BlockType<
 	TProps extends Record< string, unknown >,
 	TAttribute extends Record< string, unknown >
