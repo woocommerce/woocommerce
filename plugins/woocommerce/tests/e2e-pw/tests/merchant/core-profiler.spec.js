2c2622c624d74d9c91ae03c1cd93f524fbe6d9e6
paul sealock <psealock@gmail.com>
2024-07-04 10:20:18 +1200

    next screens

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/core-profiler.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/core-profiler.spec.js
@@ -18,43 +18,76 @@ test.describe( 'Core Profiler', { tag: [ '@gutenberg', '@services' ] }, () => {
 			baseURL + '/wp-admin/admin.php?page=wc-admin&path=%2Fsetup-wizard'
 		);
 
-		await expect(
-			page.getByRole( 'checkbox', {
-				name: 'I agree to share my data',
-				exact: false,
-			} )
-		).toBeChecked();
+		// await expect(
+		// 	page.getByRole( 'checkbox', {
+		// 		name: 'I agree to share my data',
+		// 		exact: false,
+		// 	} )
+		// ).toBeChecked();
 
-		await expect( page.getByText( 'Welcome to Woo!' ) ).toBeVisible();
+		// await expect( page.getByText( 'Welcome to Woo!' ) ).toBeVisible();
 
 		await page.getByRole( 'button', { name: 'Set up my store' } ).click();
 
-		await expect(
-			page.getByRole( 'radio', {
-				name: "I'm just starting my business",
-				exact: true,
-			} )
-		).toBeChecked();
+		// await expect(
+		// 	page.getByRole( 'radio', {
+		// 		name: "I'm just starting my business",
+		// 		exact: true,
+		// 	} )
+		// ).toBeChecked();
+
+		// await expect(
+		// 	page.getByRole( 'radio', {
+		// 		name: "I'm already selling",
+		// 		exact: true,
+		// 	} )
+		// ).toBeVisible();
+
+		// await expect(
+		// 	page.getByRole( 'radio', {
+		// 		name: "I'm setting up a store for a client",
+		// 		exact: true,
+		// 	} )
+		// ).toBeVisible();
+
+		await page.click( 'text="I\'m already selling"' );
 
 		await expect(
-			page.getByRole( 'radio', {
-				name: "I'm already selling",
-				exact: true,
-			} )
-		).toBeVisible();
-
-		await expect(
-			page.getByRole( 'radio', {
-				name: "I'm setting up a store for a client",
-				exact: true,
-			} )
+			page.getByRole( 'combobox', { name: 'Select an option' } )
 		).toBeVisible();
 
 		await page
-			.getByRole( 'radio', {
-				name: "I'm already selling",
-				exact: true,
-			} )
+			.getByRole( 'combobox', { name: 'Select an option' } )
 			.click();
+
+		await page
+			.getByRole( 'option', { name: "Yes, I'm selling online" } )
+			.click();
+
+		await expect(
+			page.getByPlaceholder( 'Select platforms' )
+		).toBeVisible();
+
+		page.getByPlaceholder( 'Select platforms' ).click();
+
+		await page.getByRole( 'checkbox', { name: 'Pinterest' } ).click();
+
+		await page.getByRole( 'button', { name: 'Continue' } ).click();
+
+		await expect(
+			page.getByText( 'Tell us a bit about your store' )
+		).toBeVisible();
+
+		page.getByPlaceholder( 'Ex. My awesome store' ).fill(
+			'New Zealand meat pies'
+		);
+
+		await page
+			.getByRole( 'combobox', { name: 'Select an industry' } )
+			.click();
+
+		await page.getByRole( 'button', { name: 'Food and drink' } ).click();
+
+		await page.getByRole( 'button', { name: 'Continue' } ).click();
 	} );
 } );
