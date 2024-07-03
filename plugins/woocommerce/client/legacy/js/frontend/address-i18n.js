6f45a2f6d4804dbb890c7b779448f4399b8e7bb7
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-07-03 12:52:06 -0300

    Prevent screen readers from reading the asterisk on checkout fields

--- plugins/woocommerce/client/legacy/js/frontend/address-i18n.js
+++ plugins/woocommerce/client/legacy/js/frontend/address-i18n.js
@@ -17,7 +17,10 @@ jQuery( function( $ ) {
 				field.find( 'label' ).append(
 					'&nbsp;<abbr class="required" title="' +
 					wc_address_i18n_params.i18n_required_text +
-					'">*</abbr>'
+					'" aria-hidden="true">*</abbr>' + 
+					'<span class="screen-reader-text">' + 
+					wc_address_i18n_params.i18n_required_text +
+					'</span>'
 				);
 			}
 		} else {
