da71cc721e3f60753549b3d2cef6be71467a6277
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-07-04 18:28:28 -0300

    Add address title to edit/add buttons on my account

--- plugins/woocommerce/templates/myaccount/my-address.php
+++ plugins/woocommerce/templates/myaccount/my-address.php
@@ -60,7 +60,7 @@ $col    = 1;
 	<div class="u-column<?php echo $col < 0 ? 1 : 2; ?> col-<?php echo $oldcol < 0 ? 1 : 2; ?> woocommerce-Address">
 		<header class="woocommerce-Address-title title">
 			<h3><?php echo esc_html( $address_title ); ?></h3>
-			<a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-address', $name ) ); ?>" class="edit"><?php echo $address ? esc_html__( 'Edit', 'woocommerce' ) : esc_html__( 'Add', 'woocommerce' ); ?></a>
+			<a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-address', $name ) ); ?>" class="edit"><?php echo $address ? esc_html__( 'Edit', 'woocommerce' ) : esc_html__( 'Add', 'woocommerce' ); ?> <?php echo esc_html( $address_title ); ?></a>
 		</header>
 		<address>
 			<?php
