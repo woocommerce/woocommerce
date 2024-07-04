3c8f964c2c103ce02bbfcb004e7b9e69d3ad36e3
Vlad Olaru <vlad@pixelgrade.com>
2024-07-04 18:38:38 +0300

    Better docs and minor refactoring for the EvaluationLogger

--- plugins/woocommerce/src/Admin/RemoteSpecs/RuleProcessors/EvaluationLogger.php
+++ plugins/woocommerce/src/Admin/RemoteSpecs/RuleProcessors/EvaluationLogger.php
@@ -25,23 +25,23 @@ class EvaluationLogger {
 	/**
 	 * Logger class to use.
 	 *
-	 * @var WC_Logger_Interface|null
+	 * @var \WC_Logger_Interface|null
 	 */
 	private $logger;
 
 	/**
 	 * Logger source.
 	 *
-	 * @var string logger source.
+	 * @var string Logger source.
 	 */
 	private $source = '';
 
 	/**
 	 * EvaluationLogger constructor.
 	 *
-	 * @param string               $slug Slug of a spec that is being evaluated.
-	 * @param null                 $source Logger source.
-	 * @param \WC_Logger_Interface $logger Logger class to use.
+	 * @param string                    $slug   Slug/ID of a spec that is being evaluated.
+	 * @param string|null               $source Logger source.
+	 * @param \WC_Logger_Interface|null $logger Logger class to use. Default to using the WC logger.
 	 */
 	public function __construct( $slug, $source = null, \WC_Logger_Interface $logger = null ) {
 		$this->slug = $slug;
@@ -59,16 +59,13 @@ class EvaluationLogger {
 	/**
 	 * Add evaluation result of a rule.
 	 *
-	 * @param string  $rule_type name of the rule being tested.
-	 * @param boolean $result result of a given rule.
+	 * @param string  $rule_type Name of the rule being tested.
+	 * @param boolean $result    Result of a given rule.
 	 */
 	public function add_result( $rule_type, $result ) {
-		array_push(
-			$this->results,
-			array(
-				'rule'   => $rule_type,
-				'result' => $result ? 'passed' : 'failed',
-			)
+		$this->results[] = array(
+			'rule'   => $rule_type,
+			'result' => $result ? 'passed' : 'failed',
 		);
 	}
 
