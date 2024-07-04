641a30756859dec91f55fc900f986cfba223971a
Vlad Olaru <vlad@pixelgrade.com>
2024-07-04 18:39:41 +0300

    Add support for logger args to EvaluateSuggestion methods

--- plugins/woocommerce/src/Admin/Features/PaymentGatewaySuggestions/EvaluateSuggestion.php
+++ plugins/woocommerce/src/Admin/Features/PaymentGatewaySuggestions/EvaluateSuggestion.php
@@ -17,14 +17,19 @@ class EvaluateSuggestion {
 	 * Evaluates the spec and returns the suggestion.
 	 *
 	 * @param object|array $spec The suggestion to evaluate.
+	 * @param array $logger_args Optional. Arguments for the rule evaluator logger.
+	 *
 	 * @return object The evaluated suggestion.
 	 */
-	public static function evaluate( $spec ) {
+	public static function evaluate( $spec, $logger_args = [] ) {
 		$rule_evaluator = new RuleEvaluator();
 		$suggestion     = is_array( $spec ) ? (object) $spec : clone $spec;
 
 		if ( isset( $suggestion->is_visible ) ) {
-			$is_visible             = $rule_evaluator->evaluate( $suggestion->is_visible );
+			$is_visible             = $rule_evaluator->evaluate( $suggestion->is_visible, null, [
+				'slug'   => $suggestion->id ?? $suggestion->title ?? 'unknown',
+				'source' => $logger_args['source'] ?? 'wc-payment-gateway-suggestions',
+			] );
 			$suggestion->is_visible = $is_visible;
 		}
 
@@ -35,15 +40,17 @@ class EvaluateSuggestion {
 	 * Evaluates the specs and returns the visible suggestions.
 	 *
 	 * @param array $specs payment suggestion spec array.
+	 * @param array $logger_args Optional. Arguments for the rule evaluator logger.
+	 *
 	 * @return array The visible suggestions and errors.
 	 */
-	public static function evaluate_specs( $specs ) {
+	public static function evaluate_specs( $specs, $logger_args = [] ) {
 		$suggestions = array();
 		$errors      = array();
 
 		foreach ( $specs as $spec ) {
 			try {
-				$suggestion = self::evaluate( $spec );
+				$suggestion = self::evaluate( $spec, $logger_args );
 				if ( ! property_exists( $suggestion, 'is_visible' ) || $suggestion->is_visible ) {
 					$suggestions[] = $suggestion;
 				}
