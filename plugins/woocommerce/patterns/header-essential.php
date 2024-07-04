ccfc9efeba7de33660bd184eb0e5d9d81644cbcb
Alba Rincón <alba.rincon@automattic.com>
2024-07-04 09:56:18 +0200

    Bring back some patterns

---
+++ plugins/woocommerce/patterns/header-essential.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Essential Header
+ * Slug: woocommerce-blocks/header-essential
+ * Categories: WooCommerce
+ * Block Types: core/template-part/header
+ */
+?>
+
+<!-- wp:group {"className":"wc-blocks-header-pattern","align":"full","style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"40px","right":"40px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wc-blocks-pattern-header-essential wc-blocks-header-pattern wp-block-group alignfull" style="padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px">
+	<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group">
+		<!-- wp:site-logo /-->
+	</div>
+	<!-- /wp:group -->
+
+	<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+		<div class="wp-block-group">
+			<!-- wp:woocommerce/mini-cart {"hasHiddenPrice":true} /-->
+			<!-- wp:woocommerce/customer-account {"displayStyle":"icon_only","iconClass":"wc-block-customer-account__account-icon"} /-->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:navigation {"style":{"spacing":{"blockGap":"24px"}}} /-->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
