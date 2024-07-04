37f102d0b5d21aae246a02ce4339946e4d19d304
Akeda Bagus <akeda.bagus@automattic.com>
2024-07-04 15:00:14 +0700

    Fix linter warnings

--- plugins/woocommerce-admin/client/wp-admin-scripts/woo-check-subscription/modal.js
+++ plugins/woocommerce-admin/client/wp-admin-scripts/woo-check-subscription/modal.js
@@ -106,7 +106,10 @@ export default function CheckSubscriptionModal( {
 
 	const renderBenefits = () => {
 		const subtitle = isExpired
-			? __( 'Reactivate your subscription and benefit from:', 'woocommerce' )
+			? __(
+					'Reactivate your subscription and benefit from:',
+					'woocommerce'
+			  )
 			: __( 'Purchase a subscription to benefit from:', 'woocommerce' );
 
 		const benefits = [
@@ -114,24 +117,35 @@ export default function CheckSubscriptionModal( {
 				key: 'get-updates',
 				icon: reusableBlock,
 				title: __( 'Improvements and security updates', 'woocommerce' ),
-				content: __( 'Access the latest features and product updates.', 'woocommerce' ),
+				content: __(
+					'Access the latest features and product updates.',
+					'woocommerce'
+				),
 			},
 			{
 				key: 'get-supports',
 				icon: commentContent,
 				title: __( 'Help when you need it', 'woocommerce' ),
-				content: __( 'Get streamlined support from our global support team.', 'woocommerce' ),
+				content: __(
+					'Get streamlined support from our global support team.',
+					'woocommerce'
+				),
 			},
 			{
 				key: 'supporting-ecosystem',
 				icon: people,
 				title: __( 'Supporting the ecosystem', 'woocommerce' ),
 				content: createInterpolateElement(
-					__( 'A subscription helps us to continuously improve your extensions, themes, and WooCommerce experience. <readMore />', 'woocommerce' ),
+					__(
+						'A subscription helps us to continuously improve your extensions, themes, and WooCommerce experience. <readMore />',
+						'woocommerce'
+					),
 					{
 						readMore: (
-						<ExternalLink href="#">{ __( 'Read more', 'woocommerce' ) }</ExternalLink>
-					),
+							<ExternalLink href="#">
+								{ __( 'Read more', 'woocommerce' ) }
+							</ExternalLink>
+						),
 					}
 				),
 			},
@@ -142,73 +156,68 @@ export default function CheckSubscriptionModal( {
 				<h3>{ subtitle }</h3>
 
 				{ benefits.map( ( { key, icon, title, content } ) => (
-					<div className="woocommerce-subscription-benefits__item" key={ key }>
+					<div
+						className="woocommerce-subscription-benefits__item"
+						key={ key }
+					>
 						<div className="woocommerce-subscription-benefits__icon">
 							<Icon icon={ icon } />
 						</div>
 
 						<div className="woocommerce-subscription-benefits__content">
-							<Text
-								as="h4"
-								lineHeight={ "20px" }
-							>
+							<Text as="h4" lineHeight={ '20px' }>
 								{ title }
 							</Text>
-							<Text as="p">
-								{ content }
-							</Text>
+							<Text as="p">{ content }</Text>
 						</div>
 					</div>
 				) ) }
 			</div>
-		)
+		);
 	};
 
 	const renderPrimaryCard = () => {
+		const status = isExpired
+			? __( 'Expired', 'woocommerce' )
+			: __( 'Unregistered', 'woocommerce' );
+
+		const title = isExpired
+			? sprintf(
+					/* translators: %s is product name */
+					__( 'Renew %s', 'woocommerce' ),
+					productName
+			  )
+			: sprintf(
+					/* translators: %s is product name */
+					__( 'Subscribe to %s', 'woocommerce' ),
+					productName
+			  );
+
+		const buttonLabel = isExpired
+			? sprintf(
+					/* translators: %s is product price */
+					__( 'Renew for $%s', 'woocommerce' ),
+					productRegularPrice
+			  )
+			: sprintf(
+					/* translators: %s is product price */
+					__( 'Subscribe for $%s', 'woocommerce' ),
+					productRegularPrice
+			  );
+
 		return (
 			<Card className="primary">
 				<CardHeader>
 					<div>
-						<Text
-							className="subscription-status subscription-status__expired"
-						>
-							{
-								isExpired
-									? __( 'Expired', 'woocommerce' )
-									: __( 'Unregistered', 'woocommerce' )
-							}
+						<Text className="subscription-status subscription-status__expired">
+							{ status }
 						</Text>
 					</div>
-					<h2>
-						{
-							isExpired
-								? sprintf(
-									/* translators: %s is product name */
-									__(
-										'Renew %s',
-										'woocommerce'
-									),
-									productName
-								)
-								: sprintf(
-									/* translators: %s is product name */
-									__(
-										'Subscribe to %s',
-										'woocommerce'
-									),
-									productName
-								)
-						}
-					</h2>
+					<h2>{ title }</h2>
 				</CardHeader>
-				<CardBody>
-					{ renderBenefits() }
-				</CardBody>
+				<CardBody>{ renderBenefits() }</CardBody>
 				<CardFooter>
-					<Button
-						onClick={ remindLater }
-						variant="secondary"
-					>
+					<Button onClick={ remindLater } variant="secondary">
 						{ __( 'Maybe later', 'woocommerce' ) }
 					</Button>
 
@@ -216,21 +225,9 @@ export default function CheckSubscriptionModal( {
 						isPrimary
 						target="_blank"
 						href={ isExpired ? renewUrl : subscribeUrl }
-						onClick={ () => isExpired ? renew() : subscribe() }
+						onClick={ () => ( isExpired ? renew() : subscribe() ) }
 					>
-						{
-							isExpired
-								? sprintf(
-									/* translators: %s is product price */
-									__( 'Renew for $%s', 'woocommerce' ),
-									productRegularPrice
-								)
-								: sprintf(
-									/* translators: %s is product price */
-									__( 'Subscribe for $%s', 'woocommerce' ),
-									productRegularPrice
-								)
-						}
+						{ buttonLabel }
 					</Button>
 				</CardFooter>
 			</Card>
@@ -241,11 +238,11 @@ export default function CheckSubscriptionModal( {
 		return (
 			<Card className="secondary">
 				<CardMedia>
-					<ResponsiveWrapper naturalWidth={ 240 } naturalHeight={ 240 }>
-						<img
-							src={ extensionsSvg }
-							alt=""
-						/>
+					<ResponsiveWrapper
+						naturalWidth={ 240 }
+						naturalHeight={ 240 }
+					>
+						<img src={ extensionsSvg } alt="" />
 					</ResponsiveWrapper>
 				</CardMedia>
 			</Card>
@@ -267,12 +264,8 @@ export default function CheckSubscriptionModal( {
 				align={ 'stretch' }
 				direction={ [ 'column-reverse', 'row' ] }
 			>
-				<FlexItem>
-					{ renderPrimaryCard() }
-				</FlexItem>
-				<FlexItem>
-					{ renderSecondaryCard() }
-				</FlexItem>
+				<FlexItem>{ renderPrimaryCard() }</FlexItem>
+				<FlexItem>{ renderSecondaryCard() }</FlexItem>
 			</Flex>
 		</Modal>
 	);
