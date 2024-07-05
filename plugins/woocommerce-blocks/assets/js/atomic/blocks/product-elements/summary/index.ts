6ec7f548c51127f277a60a685f5c0e7b8eeeb4cd
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-07-05 10:29:30 +0200

    Make the Product Summary avialabnle in Single Product template

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/index.ts
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/summary/index.ts
@@ -1,31 +1,28 @@
 /**
  * External dependencies
  */
-import { registerBlockType } from '@wordpress/blocks';
 import type { BlockConfiguration } from '@wordpress/blocks';
+import { registerBlockSingleProductTemplate } from '@woocommerce/atomic-utils';
 
 /**
  * Internal dependencies
  */
 import sharedConfig from '../shared/config';
-import attributes from './attributes';
 import edit from './edit';
 import { supports } from './supports';
-import {
-	BLOCK_TITLE as title,
-	BLOCK_ICON as icon,
-	BLOCK_DESCRIPTION as description,
-} from './constants';
+import { BLOCK_ICON as icon } from './constants';
+import metadata from './block.json';
 
 const blockConfig: BlockConfiguration = {
 	...sharedConfig,
-	apiVersion: 2,
-	title,
-	description,
 	icon: { src: icon },
-	attributes,
 	supports,
 	edit,
 };
 
-registerBlockType( 'woocommerce/product-summary', blockConfig );
+registerBlockSingleProductTemplate( {
+	blockName: 'woocommerce/product-summary',
+	blockMetadata: metadata,
+	blockSettings: blockConfig,
+	isAvailableOnPostEditor: true,
+} );
