c4d160bf2138356853d7dc093cb77d903ee83349
Alba Rincón <albarin@users.noreply.github.com>
2024-07-05 08:54:23 +0200

    CYS -  Add missing patterns to their categories for the assembler  (#49154)
    * Add the Featured Category Cover Image to Intro category
    
    * Add missing patterns to their categories for the assembler
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/patterns/product-collection-4-columns.php
+++ plugins/woocommerce/patterns/product-collection-4-columns.php
@@ -2,7 +2,7 @@
 /**
  * Title: Product Collection 4 Columns
  * Slug: woocommerce-blocks/product-collection-4-columns
- * Categories: WooCommerce
+ * Categories: WooCommerce, featured-selling
  */
 
 $products_title = $content['titles'][0]['default'] ?? '';
