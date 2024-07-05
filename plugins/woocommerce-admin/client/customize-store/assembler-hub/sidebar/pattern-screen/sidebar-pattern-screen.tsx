7a1d248cd7b87840054685c1f755007f139890ad
Luigi Teschio <gigitux@gmail.com>
2024-07-05 19:37:55 +0200

    CYS: Fix pattern preview border color when the user hovers the preview; fix pattern preview border color when the pattern is present in the site preview

--- plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/pattern-screen/sidebar-pattern-screen.tsx
+++ plugins/woocommerce-admin/client/customize-store/assembler-hub/sidebar/pattern-screen/sidebar-pattern-screen.tsx
@@ -51,6 +51,29 @@ import {
 import { useIsActiveNewNeutralVariation } from '../../hooks/use-is-active-new-neutral-variation';
 
 /**
+ * Adds a 'is-added' CSS class to each pattern preview element in the pattern list that matches a block's pattern name.
+ * This function iterates through an array of blocks added in the page, extracts the pattern name from each block's metadata,
+ * and finds the corresponding pattern preview element in the pattern list by its ID. If found, the 'is-added' class is added to the element.
+ */
+const addIsAddedClassToPatternPreview = (
+	patternListEl: HTMLElement,
+	blocks: BlockInstance[]
+) => {
+	blocks.forEach( ( block ) => {
+		const patterName = block.attributes.metadata?.patternName;
+		if ( ! patterName ) {
+			return;
+		}
+
+		const element = patternListEl.querySelector( `[id="${ patterName }"]` );
+
+		if ( element ) {
+			element.classList.add( 'is-added' );
+		}
+	} );
+};
+
+/**
  * Sorts patterns by category. For 'intro' and 'about' categories
  * priorizied DotCom Patterns. For intro category, it also prioritizes the "centered-content-with-image-below" pattern.
  * For other categories, it simply sorts patterns to prioritize Woo Patterns.
@@ -169,6 +192,36 @@ export const SidebarPatternScreen = ( { category }: { category: string } ) => {
 	const isEditorLoading = useIsSiteEditorLoading();
 
 	const isSpinnerVisible = isLoading || isEditorLoading;
+
+	useEffect( () => {
+		if ( isSpinnerVisible || refElement.current === null ) {
+			return;
+		}
+
+		// We want to add the is-added class to the pattern preview when the pattern is loaded in the editor and for each mutation.
+		addIsAddedClassToPatternPreview( refElement.current, blocks );
+
+		const observer = new MutationObserver( () => {
+			addIsAddedClassToPatternPreview(
+				refElement.current as HTMLElement,
+				blocks
+			);
+		} );
+
+		const previewPatternList = document.querySelector(
+			'.woocommerce-customize-store-edit-site-layout__sidebar-extra__pattern .block-editor-block-patterns-list'
+		);
+
+		if ( previewPatternList ) {
+			observer.observe( previewPatternList, {
+				childList: true,
+			} );
+		}
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [ isLoading, blocks, isSpinnerVisible ] );
 
 	useEffect( () => {
 		if ( isEditorLoading ) {
