c92c6e14951baba12be26a001ebe564da50886c7
Thomas Roberts <thomas.roberts@automattic.com>
2024-07-05 16:36:16 +0100

    Create add_correct_post_id_to_template function
    This will add the postId to the page-content-wrapper block from the template.

--- plugins/woocommerce/src/Blocks/BlockTypes/PageContentWrapper.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/PageContentWrapper.php
@@ -15,6 +15,87 @@ class PageContentWrapper extends AbstractBlock {
 	protected $block_name = 'page-content-wrapper';
 
 	/**
+	 * @param \WP_Block_Template[] $block_templates Array of block templates.
+	 *
+	 * @return \WP_Block_Template[]
+	 */
+	public function add_correct_post_id_to_template( $block_templates ) {
+
+		if ( ! is_array( $block_templates ) ) {
+			return $block_templates;
+		}
+
+		// Filter $block_templates to find ones where $block_template->content contains woocommerce/page-content-wrapper.
+		$page_wrapper_templates = array_filter(
+			$block_templates,
+			function ( $block_template ) {
+				if ( empty( $block_template->content ) ) {
+					return false;
+				}
+				return false !== strpos( $block_template->content, 'woocommerce/page-content-wrapper' );
+			}
+		);
+
+		foreach ( $page_wrapper_templates as $template_index => $page_wrapper_template ) {
+
+			if ( empty( $page_wrapper_template->content ) ) {
+				continue;
+			}
+
+			// Parse the template into actual blocks, this will make it easier for us to reason with than a string.
+			$parsed_block_content = parse_blocks( $page_wrapper_template->content );
+
+			// Find the woocommerce/page-content-wrapper blocks from the array of blocks in the template.
+			$blocks = array_values(
+				array_filter(
+					$parsed_block_content,
+					function ( $block ) {
+						if ( empty( $block['blockName'] ) ) {
+							return false;
+						}
+						return 'woocommerce/page-content-wrapper' === $block['blockName'];
+					}
+				)
+			);
+			// Find each match of the block in the $block_content->content.
+			preg_match( '/<!--\s?wp:woocommerce\/page-content-wrapper.*?\s?-->/', $page_wrapper_template->content, $matches );
+
+			if ( ! is_array( $matches ) ) {
+				continue;
+			}
+
+			foreach ( $matches as $index => $match ) {
+				// Skip modifying if postId is already set.
+				if ( ! empty( $blocks[ $index ]['attrs']['postId'] ) ) {
+					continue;
+				}
+
+				// If there's no page attribute, skip modifying because we won't be able to tell which post ID to assign.
+				if ( empty( $blocks[ $index ]['attrs']['page'] ) ) {
+					continue;
+				}
+
+				if ( 'cart' === $blocks[ $index ]['attrs']['page'] ) {
+					$blocks[ $index ]['attrs']['postId'] = wc_get_page_id( 'cart' );
+				}
+				if ( 'checkout' === $blocks[ $index ]['attrs']['page'] ) {
+					$blocks[ $index ]['attrs']['postId'] = wc_get_page_id( 'checkout' );
+				}
+
+				// If we still don't have a post ID after the checks, skip modifying. This will happen if page was not cart or checkout.
+				if ( empty( $blocks[ $index ]['attrs']['postId'] ) ) {
+					continue;
+				}
+
+				$new_block                      = '<!-- wp:woocommerce/page-content-wrapper { "page": "' . $blocks[ $index ]['attrs']['page'] . '", "postId": ' . $blocks[ $index ]['attrs']['postId'] . ', "postType": "page" } -->';
+				$page_wrapper_template->content = str_replace( $match, $new_block, $page_wrapper_template->content );
+			}
+			$block_templates[ $template_index ] = $page_wrapper_template;
+		}
+		return $block_templates;
+	}
+
+	/**
 	 * It isn't necessary to register block assets.
 	 *
 	 * @param string $key Data to get, or default to everything.
