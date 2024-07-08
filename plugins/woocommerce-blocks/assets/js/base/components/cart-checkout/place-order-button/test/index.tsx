00400b549edd70bd0f711b1439734bff18c2f021
Thomas Roberts <thomas.roberts@automattic.com>
2024-07-08 19:52:29 +0100

    Add tests for PlaceOrderButton

---
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/place-order-button/test/index.tsx
@@ -0,0 +1,57 @@
+/**
+ * External dependencies
+ */
+import { render, screen } from '@testing-library/react';
+import * as baseContextHooks from '@woocommerce/base-context/hooks';
+
+/**
+ * Internal dependencies
+ */
+import PlaceOrderButton from '../index';
+
+jest.mock( '@woocommerce/base-context/hooks', () => {
+	const originalModule = jest.requireActual(
+		'@woocommerce/base-context/hooks'
+	);
+	return {
+		...originalModule,
+		useCheckoutSubmit: jest.fn().mockReturnValue( {
+			onSubmit: jest.fn(),
+			isCalculating: false,
+			isDisabled: false,
+			waitingForProcessing: false,
+			waitingForRedirect: false,
+		} ),
+	};
+} );
+describe( 'PlaceOrderButton', () => {
+	it( `does not render the price and separator`, async () => {
+		render( <PlaceOrderButton label="Place order" /> );
+		expect( screen.getByText( /Place order/ ) ).toBeInTheDocument();
+		expect( screen.queryByText( /&#36;0.00/ ) ).not.toBeInTheDocument();
+		expect( screen.queryByText( /·/ ) ).not.toBeInTheDocument();
+	} );
+	it( `renders the price and separator`, async () => {
+		render( <PlaceOrderButton label="Place order" showPrice={ true } /> );
+		expect( screen.getByText( /Place order/ ) ).toBeInTheDocument();
+		expect( screen.getByText( /&#36;0.00/ ) ).toBeInTheDocument();
+		expect( screen.getByText( /·/ ) ).toBeInTheDocument();
+	} );
+	it( `renders the spinner when processing`, async () => {
+		jest.resetModules();
+		baseContextHooks.useCheckoutSubmit.mockReturnValue( {
+			onSubmit: jest.fn(),
+			isCalculating: false,
+			isDisabled: false,
+			waitingForProcessing: true,
+			waitingForRedirect: false,
+		} );
+		const { container } = render(
+			<PlaceOrderButton label="Place order" showPrice={ true } />
+		);
+		expect( screen.queryByText( /Place order/ ) ).not.toBeInTheDocument();
+		expect(
+			container.querySelector( '.wc-block-components-spinner' )
+		).toBeInTheDocument();
+	} );
+} );
