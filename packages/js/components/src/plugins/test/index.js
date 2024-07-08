4d26c77b048987254448cafe2bca547f7f0791c6
Vladimir Reznichenko <kalessil@gmail.com>
2024-07-08 16:48:56 +0200

    CI: try fixing failing tests.

--- packages/js/components/src/plugins/test/index.js
+++ packages/js/components/src/plugins/test/index.js
@@ -13,8 +13,6 @@ import { useDispatch } from '@wordpress/data';
 import { Plugins } from '../index';
 
 jest.mock( '@wordpress/data', () => ( {
-	__esModule: true,
-	...jest.requireActual( '@wordpress/data' ),
 	useDispatch: jest
 		.fn()
 		.mockReturnValue( { installAndActivatePlugins: jest.fn() } ),
