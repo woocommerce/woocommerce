daa945124d67ecad7a15d5741431a9b89f32d18f
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-07-11 01:27:28 +0100

    Fixed overflow when formatting price for Store API responses

--- plugins/woocommerce/src/StoreApi/Formatters/MoneyFormatter.php
+++ plugins/woocommerce/src/StoreApi/Formatters/MoneyFormatter.php
@@ -8,13 +8,14 @@ namespace Automattic\WooCommerce\StoreApi\Formatters;
  */
 class MoneyFormatter implements FormatterInterface {
 	/**
-	 * Format a given value and return the result.
+	 * Format a given price value and return the result as a string without decimals.
 	 *
-	 * @param mixed $value Value to format.
+	 * @param int|float|string $value Value to format. Int is allowed, as it may also represent a valid price.
 	 * @param array $options Options that influence the formatting.
-	 * @return mixed
+	 * @return string
 	 */
 	public function format( $value, array $options = [] ) {
+
 		$options = wp_parse_args(
 			$options,
 			[
@@ -23,12 +24,22 @@ class MoneyFormatter implements FormatterInterface {
 			]
 		);
 
-		return (string) intval(
-			round(
-				( (float) wc_format_decimal( $value ) ) * ( 10 ** absint( $options['decimals'] ) ),
-				0,
-				absint( $options['rounding_mode'] )
-			)
-		);
+		// Ensure rounding mode is valid.
+		$rounding_modes           = [ PHP_ROUND_HALF_UP, PHP_ROUND_HALF_DOWN, PHP_ROUND_HALF_EVEN, PHP_ROUND_HALF_ODD ];
+		$options['rounding_mode'] = absint( $options['rounding_mode'] );
+		if ( ! in_array( $options['rounding_mode'], $rounding_modes, true ) ) {
+			$options['rounding_mode'] = PHP_ROUND_HALF_UP;
+		}
+
+		if( is_int( $value ) || is_string( $value ) ) {
+			$value = floatval( $value );
+		}
+
+		// Remove the price decimal points for rounding purposes.
+		$value = $value * pow( 10, absint( $options['decimals'] ) );
+		$value = round( $value, 0, $options['rounding_mode'] );
+
+		// This ensures returning the value as a string ready for price parsing.
+		return wc_format_decimal( $value );
 	}
 }
