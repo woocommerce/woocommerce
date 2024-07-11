6288ff800d4ffa3c49feac2ce62d409b4e07181f
amesplant <ames.plant@10up.com>
2024-07-11 13:08:07 -0500

    Update breadcrumb version number

--- plugins/woocommerce/templates/global/breadcrumb.php
+++ plugins/woocommerce/templates/global/breadcrumb.php
@@ -12,7 +12,7 @@
  *
  * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     2.3.0
+ * @version     9.2.0
  * @see         woocommerce_breadcrumb()
  */
 
