e10b50b196a7e07d0d1e6bc6fd8d2364778224da
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2024-07-12 19:01:07 +0100

    Renamed $mock_formatter. This is not a mock.

--- plugins/woocommerce/tests/php/src/Blocks/StoreApi/Formatters/TestMoneyFormatter.php
+++ plugins/woocommerce/tests/php/src/Blocks/StoreApi/Formatters/TestMoneyFormatter.php
@@ -12,7 +12,7 @@ use Automattic\WooCommerce\StoreApi\Formatters\MoneyFormatter;
  */
 class TestMoneyFormatter extends \WP_UnitTestCase {
 
-	private $mock_formatter;
+	private $formatter;
 
 	/**
 	 * Setup test product data. Called before every test.
@@ -20,42 +20,43 @@ class TestMoneyFormatter extends \WP_UnitTestCase {
 	protected function setUp(): void {
 		parent::setUp();
 
-		$this->mock_formatter = new MoneyFormatter();
+		$this->formatter = new MoneyFormatter();
 	}
 
 	/**
 	 * Test formatting.
 	 */
 	public function test_format() {
-		$this->assertEquals( "1000", $this->mock_formatter->format( 10 ) );
-		$this->assertEquals( "1000", $this->mock_formatter->format( "10" ) );
+		$this->assertEquals( "1000", $this->formatter->format( 10 ) );
+		$this->assertEquals( "1000", $this->formatter->format( "10" ) );
 	}
 
 	/**
 	 * Test formatting with custom DP.
 	 */
 	public function test_format_dp() {
-		$this->assertEquals( "100000", $this->mock_formatter->format( 10, [ 'decimals' => 4 ] ) );
-		$this->assertEquals( "100000", $this->mock_formatter->format( "10", [ 'decimals' => 4 ] ) );
+		$this->assertEquals( "100000", $this->formatter->format( 10, [ 'decimals' => 4 ] ) );
+		$this->assertEquals( "100000", $this->formatter->format( "10", [ 'decimals' => 4 ] ) );
 	}
 
 	/**
 	 * Test formatting with custom DP.
 	 */
 	public function test_format_rounding_mode() {
-		$this->assertEquals( "156", $this->mock_formatter->format( 1.555, [ 'rounding_mode' => PHP_ROUND_HALF_UP ] ) );
-		$this->assertEquals( "155", $this->mock_formatter->format( 1.555, [ 'rounding_mode' => PHP_ROUND_HALF_DOWN ] ) );
-		$this->assertEquals( "156", $this->mock_formatter->format( 1.555, [ 'rounding_mode' => PHP_ROUND_HALF_EVEN ] ) );
-		$this->assertEquals( "155", $this->mock_formatter->format( 1.555, [ 'rounding_mode' => PHP_ROUND_HALF_ODD ] ) );
-		$this->assertEquals( "156", $this->mock_formatter->format( 1.555, [ 'rounding_mode' => 123456 ] ) );
+		$this->assertEquals( "156", $this->formatter->format( 1.555 ) );
+		$this->assertEquals( "156", $this->formatter->format( 1.555, [ 'rounding_mode' => PHP_ROUND_HALF_UP ] ) );
+		$this->assertEquals( "155", $this->formatter->format( 1.555, [ 'rounding_mode' => PHP_ROUND_HALF_DOWN ] ) );
+		$this->assertEquals( "156", $this->formatter->format( 1.555, [ 'rounding_mode' => PHP_ROUND_HALF_EVEN ] ) );
+		$this->assertEquals( "155", $this->formatter->format( 1.555, [ 'rounding_mode' => PHP_ROUND_HALF_ODD ] ) );
+		$this->assertEquals( "156", $this->formatter->format( 1.555, [ 'rounding_mode' => 123456 ] ) );
 	}
 
 	/**
 	 * Test formatting for int overflow values.
 	 */
 	public function test_format_int_overflow() {
-		$this->assertEquals( '922337203685477580800', $this->mock_formatter->format( '9223372036854775808' ) );
-		$this->assertEquals( '922337203685477580800', $this->mock_formatter->format( floatval( '9223372036854775808' ) ) );
+		$this->assertEquals( '922337203685477580800', $this->formatter->format( '9223372036854775808' ) );
+		$this->assertEquals( '922337203685477580800', $this->formatter->format( floatval( '9223372036854775808' ) ) );
 	}
 
 	/**
@@ -63,9 +64,9 @@ class TestMoneyFormatter extends \WP_UnitTestCase {
 	 * @expectedNotice
 	 */
 	public function test_format_unexpected_param_types() {
-		$this->assertEquals( '', $this->mock_formatter->format( true ) );
-		$this->assertEquals( '', $this->mock_formatter->format( null ) );
-		$this->assertEquals( '', $this->mock_formatter->format( array( 'Not right' ) ) );
-		$this->assertEquals( '', $this->mock_formatter->format( new \StdClass() ) );
+		$this->assertEquals( '', $this->formatter->format( true ) );
+		$this->assertEquals( '', $this->formatter->format( null ) );
+		$this->assertEquals( '', $this->formatter->format( array( 'Not right' ) ) );
+		$this->assertEquals( '', $this->formatter->format( new \StdClass() ) );
 	}
 }
