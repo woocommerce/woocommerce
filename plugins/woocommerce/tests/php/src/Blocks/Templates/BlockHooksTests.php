39c19294c644c9b341760976617cca374fe234c6
Tom Cafferkey <tjcafferkey@gmail.com>
2024-07-12 12:18:40 +0100

    Update tests

--- plugins/woocommerce/tests/php/src/Blocks/Templates/BlockHooksTests.php
+++ plugins/woocommerce/tests/php/src/Blocks/Templates/BlockHooksTests.php
@@ -34,35 +34,36 @@ class BlockHooksTests extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Test block does not get hooked because no version is set.
-	 *
-	 * @return void
-	 */
-	public function test_mocked_block_does_not_get_hooked() {
-		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment -- test code.
-		$hooked_block_types = apply_filters( 'hooked_block_types', array(), 'after', 'core/navigation', array( 'mock-context' ) );
-		$this->assertEquals(
-			array(),
-			$hooked_block_types,
-			'Hooked block shouldnt be added unless a version is set'
-		);
-	}
-
-	/**
 	 * Test block gets hooked with correct version
 	 *
 	 * @return void
 	 */
 	public function test_mocked_block_gets_hooked_with_correct_version() {
-		add_option( self::$option_name, '8.4.0' );
+		update_option( self::$option_name, '8.4.0', false );
 		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment -- test code.
 		$hooked_block_types = apply_filters( 'hooked_block_types', array(), 'after', 'core/navigation', array( 'mock-context' ) );
-		$this->assertEquals(
-			array( 'woocommerce/test-block' ),
+		$this->assertContains(
+			'woocommerce/test-block',
 			$hooked_block_types,
-			'Test block should be included in hooked blocks'
+			'Test block should be included in hooked blocks with correct version'
 		);
 		delete_option( self::$option_name );
+	}
+
+	/**
+	 * Test block does not get hooked because no version is set.
+	 *
+	 * @return void
+	 */
+	public function test_mocked_block_does_not_get_hooked() {
+		delete_option( self::$option_name );
+		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment -- test code.
+		$hooked_block_types = apply_filters( 'hooked_block_types', array(), 'after', 'core/navigation', array( 'mock-context' ) );
+		$this->assertNotContains(
+			'woocommerce/test-block',
+			$hooked_block_types,
+			'Hooked block shouldnt be added unless a version is set'
+		);
 	}
 
 	/**
@@ -71,13 +72,13 @@ class BlockHooksTests extends WP_UnitTestCase {
 	 * @return void
 	 */
 	public function test_mocked_block_does_not_get_hooked_with_incorrect_version() {
-		add_option( self::$option_name, '8.3.0' );
+		update_option( self::$option_name, '8.3.0', false );
 		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment -- test code.
 		$hooked_block_types = apply_filters( 'hooked_block_types', array(), 'after', 'core/navigation', array( 'mock-context' ) );
-		$this->assertEquals(
-			array(),
+		$this->assertNotContains(
+			'woocommerce/test-block',
 			$hooked_block_types,
-			'Test block should be included in hooked blocks'
+			'Test block should not be included in hooked blocks with incorrect version'
 		);
 		delete_option( self::$option_name );
 	}
