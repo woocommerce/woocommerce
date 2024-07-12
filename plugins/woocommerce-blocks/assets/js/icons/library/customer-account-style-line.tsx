6d98895ce19da5b678b3f7ce10040c74d38f58ae
Alba Rincón <alba.rincon@automattic.com>
2024-07-12 09:46:01 +0200

    Import Circle and Path and fix attribute case

--- plugins/woocommerce-blocks/assets/js/icons/library/customer-account-style-line.tsx
+++ plugins/woocommerce-blocks/assets/js/icons/library/customer-account-style-line.tsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { SVG } from '@wordpress/primitives';
+import { Circle, SVG, Path } from '@wordpress/primitives';
 
 const customerAccountStyleLine = (
 	<SVG
@@ -11,16 +11,16 @@ const customerAccountStyleLine = (
 		fill="none"
 		xmlns="http://www.w3.org/2000/svg"
 	>
-		<circle
+		<Circle
 			cx="6"
 			cy="3"
 			r="2.25"
 			stroke="currentColor"
-			stroke-width="1.5"
+			strokeWidth="1.5"
 		/>
-		<path
-			fill-rule="evenodd"
-			clip-rule="evenodd"
+		<Path
+			fillRule="evenodd"
+			clipRule="evenodd"
 			d="M3 8.5H9C9.82843 8.5 10.5 9.17157 10.5 10V13H12V10C12 8.34315 10.6569 7 9 7H3C1.34315 7 0 8.34314 0 10V13H1.5V10C1.5 9.17157 2.17157 8.5 3 8.5Z"
 			fill="currentColor"
 		/>
