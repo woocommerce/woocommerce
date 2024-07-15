07bd730b82762f39846e173c13c95009e0c9172f
simonporter007 <simonporter007@users.noreply.github.com>
2024-07-15 16:44:33 +0100

    refactor tests to single spec using rerender

--- plugins/woocommerce-blocks/assets/js/base/components/quantity-selector/test/index.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/quantity-selector/test/index.tsx
@@ -18,7 +18,7 @@ const defaults = {
 
 describe( 'QuantitySelector', () => {
 	it( 'The quantity step buttons are rendered when the quantity is editable', () => {
-		render( <QuantitySelector { ...defaults } /> );
+		const { rerender } = render( <QuantitySelector { ...defaults } /> );
 
 		expect(
 			screen.getByLabelText(
@@ -28,10 +28,8 @@ describe( 'QuantitySelector', () => {
 		expect(
 			screen.getByLabelText( `Reduce quantity of ${ defaults.itemName }` )
 		).toBeInTheDocument();
-	} );
 
-	it( 'The quantity step buttons are not rendered when the quantity is not editable', () => {
-		render( <QuantitySelector { ...defaults } editable={ false } /> );
+		rerender( <QuantitySelector { ...defaults } editable={ false } /> );
 
 		expect(
 			screen.queryByLabelText(
