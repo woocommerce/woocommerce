bc746d3fc9429d12be60f7128d096f5f915085c2
Vladimir Reznichenko <kalessil@gmail.com>
2024-07-15 07:59:36 +0200

    CI: experiments with more workers in E2E core tests

--- plugins/woocommerce/tests/e2e-pw/envs/default/playwright.config.js
+++ plugins/woocommerce/tests/e2e-pw/envs/default/playwright.config.js
@@ -5,10 +5,12 @@ process.env.USE_WP_ENV = 'true';
 
 config = {
 	...config,
+	workers: 2,
 	projects: [
 		{
 			name: 'default',
 			use: { ...devices[ 'Desktop Chrome' ] },
+			grepInvert: /@gutenberg/,
 		},
 	],
 };
