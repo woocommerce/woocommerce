d845f8dd4fb87d31b2b1bb5ae1c3eaa2ef79c620
Roy Ho <roykho77@gmail.com>
2024-07-16 14:24:08 -0700

    Update plugins/woocommerce-blocks/assets/js/icons/library/filter-three-lines.tsx
    Co-authored-by: Alexandre Lara <allexandrelara@gmail.com>

--- plugins/woocommerce-blocks/assets/js/icons/library/filter-three-lines.tsx
+++ plugins/woocommerce-blocks/assets/js/icons/library/filter-three-lines.tsx
@@ -1,11 +1,11 @@
 /**
  * External dependencies
  */
-import { SVG } from '@wordpress/primitives';
+import { SVG, Path } from '@wordpress/primitives';
 
 const filterThreeLines = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-		<path
+		<Path
 			d="M4 11.5H8V10H4V11.5ZM0 0V1.5H12V0H0ZM2 6.5H10V5H2V6.5Z"
 			transform="translate(6,6)"
 		/>
