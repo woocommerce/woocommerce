98083ce867faff000ddb87f3991431e1f34ace37
Rodel Calasagsag <od.rodel.calasagsag@gmail.com>
2024-07-18 13:50:04 +0800

    Prepare for WP 6.6 by updated tested / required versions

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -8,7 +8,7 @@
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
  * Domain Path: /i18n/languages/
- * Requires at least: 6.4
+ * Requires at least: 6.5
  * Requires PHP: 7.4
  *
  * @package WooCommerce
