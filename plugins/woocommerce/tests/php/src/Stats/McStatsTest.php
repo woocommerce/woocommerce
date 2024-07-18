8870f2e8b391e1649e555a619a4e59d5eba74254
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-07-18 16:33:49 +0800

    Add tests

---
+++ plugins/woocommerce/tests/php/src/Stats/McStatsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Automattic\WooCommerce\Tests\Stats;
+
+use Automattic\WooCommerce\Stats\McStats;
+
+/**
+ * Test Mc Stats class
+ */
+class McStatsTest extends \WC_Unit_Test_Case {
+
+	/**
+	 * Set up. Runs before each test.
+	 */
+	public function setUp(): void {
+		$this->sut = wc_get_container()->get( McStats::class );
+	}
+
+	/**
+	 * Test get group query args
+	 */
+	public function test_get_group_query_args() {
+
+		$this->sut->add( 'group', 'test' );
+		$this->sut->add( 'group', 'test2' );
+
+		$this->assertEmpty( $this->sut->get_group_query_args( 'group2' ) );
+
+		$check = $this->sut->get_group_query_args( 'group' );
+
+		$this->assertCount( 1, $check );
+		$this->assertArrayHasKey( 'x_woocommerce-group', $check );
+		$this->assertEquals( 'test,test2', $check['x_woocommerce-group'] );
+	}
+}
