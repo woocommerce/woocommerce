0c5f7b37d72f08bcc43724c42ff4a7da8f9cdd93
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2024-07-18 11:08:07 +0530

    Prep/release/9.1 for next dev cycle 9.1 (#49457)
    * Revert PR 46857 for backward compatibility (#48753)
    
    * Revert PR 48753 to restore back compat.
    
    * Typo fixes.
    
    * Prep release/9.1 for 9.1 cycle with version bump to 9.1.2

--- plugins/woocommerce/woocommerce.php
+++ plugins/woocommerce/woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce
  * Plugin URI: https://woocommerce.com/
  * Description: An ecommerce toolkit that helps you sell anything. Beautifully.
- * Version: 9.1.1
+ * Version: 9.1.2
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain: woocommerce
