6c94d2e5031e5a65bae9ef9aa0aec956c32215dc
Roy Ho <roykho77@gmail.com>
2024-07-18 11:33:25 -0700

    [Experimental] Add Product filters overlay settings (#48732)
    * Add overlay settings
    
    * Tweak icon size slider
    
    * Add overlay template part link
    
    * Add changefile(s) from automation for the following project(s): woocommerce-blocks, woocommerce
    
    * Fix scss linting
    
    * Fix php linting
    
    * Remove preview
    
    * Rename setting props to be more clear
    
    * Remove unused import
    
    * Update plugins/woocommerce-blocks/assets/js/blocks/product-filters/editor.scss
    
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>
    
    * Update plugins/woocommerce-blocks/assets/js/blocks/product-filters/edit.tsx
    
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>
    
    * Update plugins/woocommerce-blocks/assets/js/blocks/product-filters/types.ts
    
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>
    
    * Update plugins/woocommerce-blocks/assets/js/blocks/product-filters/edit.tsx
    
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>
    
    * Move experimental controls to last of list
    
    * Improve typescript code
    
    * Remove unneeded constructor code
    
    * Extract icons into own component
    
    * Moved overlay template part link to the bottom
    
    * Add apply button
    
    * Hide overlay settings when in always mode
    
    * Remove overlay template part from list and put into general area
    
    * Update api version to 3
    
    * Update plugins/woocommerce-blocks/assets/js/icons/library/filter.tsx
    
    Co-authored-by: Alexandre Lara <allexandrelara@gmail.com>
    
    * Update plugins/woocommerce-blocks/assets/js/icons/library/filter-three-lines.tsx
    
    Co-authored-by: Alexandre Lara <allexandrelara@gmail.com>
    
    * Fix overlay button link style and clickable
    
    * Remove edit overlay link from never mode
    
    * Add e2e tests for the filters overlay settings
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>
    Co-authored-by: Alexandre Lara <allexandrelara@gmail.com>

--- plugins/woocommerce/src/Blocks/Templates/ProductFiltersOverlayTemplate.php
+++ plugins/woocommerce/src/Blocks/Templates/ProductFiltersOverlayTemplate.php
@@ -20,14 +20,12 @@ class ProductFiltersOverlayTemplate extends AbstractTemplatePart {
 	 *
 	 * @var string
 	 */
-	public $template_area = 'product-filters-overlay';
+	public $template_area = 'uncategorized';
 
 	/**
 	 * Initialization method.
 	 */
-	public function init() {
-		add_filter( 'default_wp_template_part_areas', array( $this, 'register_product_filters_overlay_template_part_area' ), 10, 1 );
-	}
+	public function init() {}
 
 	/**
 	 * Returns the title of the template.
@@ -45,22 +43,5 @@ class ProductFiltersOverlayTemplate extends AbstractTemplatePart {
 	 */
 	public function get_template_description() {
 		return __( 'Template used to display the Product Filters Overlay.', 'woocommerce' );
-	}
-
-	/**
-	 * Add Filters Overlay to the default template part areas.
-	 *
-	 * @param array $default_area_definitions An array of supported area objects.
-	 * @return array The supported template part areas including the Filters Overlay one.
-	 */
-	public function register_product_filters_overlay_template_part_area( $default_area_definitions ) {
-		$product_filters_overlay_template_part_area = array(
-			'area'        => 'product-filters-overlay',
-			'label'       => $this->get_template_title(),
-			'description' => $this->get_template_description(),
-			'icon'        => 'filter',
-			'area_tag'    => 'product-filters-overlay',
-		);
-		return array_merge( $default_area_definitions, array( $product_filters_overlay_template_part_area ) );
 	}
 }
