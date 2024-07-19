f0e6eaa2812fd8decb8b078e146cc395aad2de14
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-07-19 11:55:19 +0800

    Rebase

--- plugins/woocommerce/includes/class-woocommerce.php
+++ plugins/woocommerce/includes/class-woocommerce.php
@@ -28,7 +28,7 @@ use Automattic\WooCommerce\Internal\Utilities\WebhookUtil;
 use Automattic\WooCommerce\Internal\Admin\Marketplace;
 use Automattic\WooCommerce\Proxies\LegacyProxy;
 use Automattic\WooCommerce\Utilities\{LoggingUtil, RestApiUtil, TimeUtil};
-use Automattic\WooCommerce\Logging\RemoteLogger;
+use Automattic\WooCommerce\Loggers\RemoteLogger;
 
 /**
  * Main WooCommerce Class.
