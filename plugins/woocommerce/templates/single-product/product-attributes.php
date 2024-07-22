5369590a7ee5abfc5ffc4572fead4a741d97cf36
amesplant <ames.plant@10up.com>
2024-07-22 17:07:25 -0500

    Add scope attribute to the product attrubutes table

--- plugins/woocommerce/templates/single-product/product-attributes.php
+++ plugins/woocommerce/templates/single-product/product-attributes.php
@@ -26,7 +26,7 @@ if ( ! $product_attributes ) {
 <table class="woocommerce-product-attributes shop_attributes">
 	<?php foreach ( $product_attributes as $product_attribute_key => $product_attribute ) : ?>
 		<tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr( $product_attribute_key ); ?>">
-			<th class="woocommerce-product-attributes-item__label"><?php echo wp_kses_post( $product_attribute['label'] ); ?></th>
+			<th class="woocommerce-product-attributes-item__label" scope="row"><?php echo wp_kses_post( $product_attribute['label'] ); ?></th>
 			<td class="woocommerce-product-attributes-item__value"><?php echo wp_kses_post( $product_attribute['value'] ); ?></td>
 		</tr>
 	<?php endforeach; ?>
