7399380458761ea485cac328a578b09af6d233ed
Hug0-Drelon <hugo.drelon@gmail.com>
2024-07-22 12:21:03 +0200

    Create new filter wc_lock_sku_query.

--- plugins/woocommerce/includes/data-stores/class-wc-product-data-store-cpt.php
+++ plugins/woocommerce/includes/data-stores/class-wc-product-data-store-cpt.php
@@ -131,6 +131,18 @@ class WC_Product_Data_Store_CPT extends WC_Data_Store_WP implements WC_Object_Da
 			$sku,
 			$sku
 		);
+
+		/**
+		 * Filters the query to lock SKU on product creation.
+		 * Expects a prepared SQL query.
+		 *
+		 * @since 9.2.0
+		 *
+		 * @param $query   The query to lock SKU.
+		 * @param $product The product being created.
+		 */
+		$query = apply_filters( 'wc_lock_sku_query', $query, $product );
+
 		// phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
 		$result = $wpdb->query( $query );
 
