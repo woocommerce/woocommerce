7ea1ba1efbbb9e07116a44e326165eb52ce39311
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-07-22 16:13:50 -0300

    Change address title from h3 to h2 on edit address page

--- plugins/woocommerce/templates/myaccount/form-edit-address.php
+++ plugins/woocommerce/templates/myaccount/form-edit-address.php
@@ -27,7 +27,7 @@ do_action( 'woocommerce_before_edit_account_address_form' ); ?>
 
 	<form method="post">
 
-		<h3><?php echo apply_filters( 'woocommerce_my_account_edit_address_title', $page_title, $load_address ); ?></h3><?php // @codingStandardsIgnoreLine ?>
+		<h2><?php echo apply_filters( 'woocommerce_my_account_edit_address_title', $page_title, $load_address ); ?></h2><?php // @codingStandardsIgnoreLine ?>
 
 		<div class="woocommerce-address-fields">
 			<?php do_action( "woocommerce_before_edit_address_form_{$load_address}" ); ?>
