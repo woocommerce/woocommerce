bad2e74f2bc3af0059804ea361895ab3bc92acea
Mike Jolley <mike.jolley@me.com>
2024-07-22 17:30:21 +0100

    Clean up notifyerrors helpers

--- plugins/woocommerce-blocks/assets/js/data/cart/notify-errors.ts
+++ plugins/woocommerce-blocks/assets/js/data/cart/notify-errors.ts
@@ -7,26 +7,49 @@ import { decodeEntities } from '@wordpress/html-entities';
 import { dispatch } from '@wordpress/data';
 
 /**
- * This function is used to notify the user of cart item errors/conflicts
+ * This function is used to notify the user of errors/conflicts from an API error response object.
  */
-export const notifyCartErrors = (
-	errors: ApiErrorResponse[] | null = null,
-	oldErrors: ApiErrorResponse[] | null = null
-) => {
-	if ( oldErrors ) {
-		oldErrors.forEach( ( error ) => {
-			dispatch( 'core/notices' ).removeNotice( error.code, 'wc/cart' );
+const createNotices = ( errors: ApiErrorResponse[] ) => {
+	errors.forEach( ( error ) => {
+		createNotice( 'error', decodeEntities( error.message ), {
+			id: error.code,
+			context: error?.data?.context || 'wc/cart',
 		} );
-	}
+	} );
+};
 
+/**
+ * This function is used to dismiss old errors from the store.
+ */
+const dismissNotices = ( oldErrors: ApiErrorResponse[] ) => {
+	oldErrors.forEach( ( error ) => {
+		dispatch( 'core/notices' ).removeNotice(
+			error.code,
+			error?.data?.context || 'wc/cart'
+		);
+	} );
+};
+
+/**
+ * This function is used to normalize errors into an array of ApiErrorResponse objects.
+ */
+const normalizeErrors = ( errors: ApiErrorResponse | ApiErrorResponse[] ) => {
+	return isApiErrorResponse( errors )
+		? [ errors ]
+		: errors.filter( isApiErrorResponse );
+};
+
+/**
+ * This function is used to notify the user of cart errors/conflicts.
+ */
+export const notifyErrors = (
+	errors: ApiErrorResponse | ApiErrorResponse[] | null = null,
+	oldErrors: ApiErrorResponse | ApiErrorResponse[] | null = null
+) => {
+	if ( oldErrors !== null ) {
+		dismissNotices( normalizeErrors( oldErrors ) );
+	}
 	if ( errors !== null ) {
-		errors.forEach( ( error ) => {
-			if ( isApiErrorResponse( error ) ) {
-				createNotice( 'error', decodeEntities( error.message ), {
-					id: error.code,
-					context: 'wc/cart',
-				} );
-			}
-		} );
+		createNotices( normalizeErrors( errors ) );
 	}
 };
