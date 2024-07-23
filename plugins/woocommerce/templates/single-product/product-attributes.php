a189b7dce824c9ea495ea29cb70c184bf3f55459
amesplant <95257231+amesplant-dmv@users.noreply.github.com>
2024-07-23 15:19:49 -0500

    Add a label to the table

--- plugins/woocommerce/templates/single-product/product-attributes.php
+++ plugins/woocommerce/templates/single-product/product-attributes.php
@@ -23,7 +23,7 @@ if ( ! $product_attributes ) {
 	return;
 }
 ?>
-<table class="woocommerce-product-attributes shop_attributes">
+<table class="woocommerce-product-attributes shop_attributes" aria-label="<?php echo esc_html_e('Product Details', 'woocommerece'); ?>">
 	<?php foreach ( $product_attributes as $product_attribute_key => $product_attribute ) : ?>
 		<tr class="woocommerce-product-attributes-item woocommerce-product-attributes-item--<?php echo esc_attr( $product_attribute_key ); ?>">
 			<th class="woocommerce-product-attributes-item__label" scope="row"><?php echo wp_kses_post( $product_attribute['label'] ); ?></th>
