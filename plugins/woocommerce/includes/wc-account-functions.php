2551d77fe4b6e0c58769b1fa46984e4c2484b981
amesplant <95257231+amesplant-dmv@users.noreply.github.com>
2024-07-23 08:44:19 -0500

    Update current page check for dashboard, orders, and payment methods

--- plugins/woocommerce/includes/wc-account-functions.php
+++ plugins/woocommerce/includes/wc-account-functions.php
@@ -175,8 +175,13 @@ function wc_get_current_page( $endpoint ) {
 	global $wp;
 	$current = isset( $wp->query_vars[ $endpoint ] );
 	$aria_current = '';
-	if ( isset( $wp->query_vars['page'] ) ) {
-		$current = $wp->query_vars['page'];
+
+	if ( 'dashboard' === $endpoint && ( isset( $wp->query_vars['page'] ) || empty( $wp->query_vars ) ) ) {
+		$current = true; // Dashboard is not an endpoint, so needs a custom check.
+	} elseif ( 'orders' === $endpoint && isset( $wp->query_vars['view-order'] ) ) {
+		$current = true; // When looking at individual order, highlight Orders list item (to signify where in the menu the user currently is).
+	} elseif ( 'payment-methods' === $endpoint && isset( $wp->query_vars['add-payment-method'] ) ) {
+		$current = true;
 	}
 
 	if ( $current ) {
