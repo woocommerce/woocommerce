81fcb6f9f0ebd96377b65e470bc82d2a3b3e023c
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-07-23 14:49:42 +0800

    Enqueue styles

--- plugins/woocommerce/src/Blocks/BlockTypes/ComingSoon.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ComingSoon.php
@@ -21,12 +21,34 @@ class ComingSoon extends AbstractBlock {
 	}
 
 	/**
+	 * Enqueue frontend assets for this block, just in time for rendering.
+	 *
+	 * @internal This prevents the block script being enqueued on all pages. It is only enqueued as needed. Note that
+	 * we intentionally do not pass 'script' to register_block_type.
+	 *
+	 * @param array    $attributes  Any attributes that currently are available from the block.
+	 * @param string   $content    The block content.
+	 * @param WP_Block $block    The block object.
+	 */
+	protected function enqueue_assets( array $attributes, $content, $block ) {
+		parent::enqueue_assets( $attributes, $content, $block );
+
+		if ( isset( $attributes['color'] ) ) {
+			wp_add_inline_style(
+				'wc-blocks-style',
+				':root{--woocommerce-coming-soon-color: ' . esc_html( $attributes['color'] ) . '}'
+			);
+		}
+	}
+
+	/**
 	 * Get the frontend style handle for this block type.
 	 *
-	 * @return null
+	 * @return string[]|null
 	 */
 	protected function get_block_type_style() {
-		return null;
+		$styles = parent::get_block_type_style();
+		return $styles;
 	}
 
 	/**
