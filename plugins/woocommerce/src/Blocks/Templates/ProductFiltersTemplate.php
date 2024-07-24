2d719c6936f26965da720f00f8d3376643940874
Tung Du <dinhtungdu@gmail.com>
2024-07-24 11:50:38 +0700

    add: register variation for unmodified template part
    We should move this to abstract class later.

--- plugins/woocommerce/src/Blocks/Templates/ProductFiltersTemplate.php
+++ plugins/woocommerce/src/Blocks/Templates/ProductFiltersTemplate.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 namespace Automattic\WooCommerce\Blocks\Templates;
 
+use Automattic\WooCommerce\Blocks\Utils\BlockTemplateUtils;
+
 /**
  * ProductFiltersTemplate class.
  *
@@ -26,7 +28,9 @@ class ProductFiltersTemplate extends AbstractTemplatePart {
 	/**
 	 * Initialization method.
 	 */
-	public function init() {}
+	public function init() {
+		add_filter( 'get_block_type_variations', array( $this, 'register_block_type_variation' ), 10, 2 );
+	}
 
 	/**
 	 * Returns the title of the template.
@@ -44,9 +48,48 @@ class ProductFiltersTemplate extends AbstractTemplatePart {
 	 */
 	public function get_template_description() {
 		return __(
-			'This is the template part for the product filters
-displayed on different pages across your store.',
+			'This is the template part for the product filters displayed on different pages across your store.',
 			'woocommerce'
 		);
+	}
+
+	/**
+	 * Add variation for this template part to make it available in the block inserter.
+	 *
+	 * @param array         $variations Array of registered variations for a block type.
+	 * @param WP_Block_Type $block_type The full block type object.
+	 */
+	public function register_block_type_variation( $variations, $block_type ) {
+		if ( 'core/template-part' !== $block_type->name ) {
+			return $variations;
+		}
+
+		// If template part is modified, Core will pick it up and register a variation
+		// for it. Check if the variation already exists before adding it.
+		foreach ( $variations as $variation ) {
+			if ( ! empty( $variation['attributes']['slug'] ) && $variation['attributes']['slug'] === 'product-filters' ) {
+					return $variations;
+			}
+		}
+
+		$theme = 'woocommerce/woocommerce';
+		// Check if current theme overrides this template part.
+		if ( BlockTemplateUtils::theme_has_template_part( 'product-filters' ) ) {
+			$theme = wp_get_theme()->get( 'TextDomain' );
+		}
+
+		$variations[] = array(
+			'name'        => 'file_' . self::SLUG,
+			'title'       => $this->get_template_title(),
+			'description' => true,
+			'attributes'  => array(
+				'slug'  => self::SLUG,
+				'theme' => $theme,
+				'area'  => $this->template_area,
+			),
+			'scope'       => array( 'inserter' ),
+			'icon'        => 'layout',
+		);
+		return $variations;
 	}
 }
