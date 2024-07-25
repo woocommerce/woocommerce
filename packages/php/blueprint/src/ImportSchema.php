ffeb07c7ad951ea3667131f1e79427291c3bf62c
moon <moon.kyong@automattic.com>
2024-07-25 12:48:22 -0700

    Add hook comment for wooblueprint_exporters and wooblueprint_importers

--- packages/php/blueprint/src/ImportSchema.php
+++ packages/php/blueprint/src/ImportSchema.php
@@ -35,10 +35,10 @@ class ImportSchema {
 	/**
 	 * ImportSchema constructor.
 	 *
-	 * @param JsonSchema $schema The schema instance.
+	 * @param JsonSchema     $schema The schema instance.
 	 * @param Validator|null $validator The validator instance, optional.
 	 */
-	public function __construct( JsonSchema $schema,  Validator $validator = null ) {
+	public function __construct( JsonSchema $schema, Validator $validator = null ) {
 		$this->schema = $schema;
 		if ( null === $validator ) {
 			$this->validator = new Validator();
@@ -97,19 +97,32 @@ class ImportSchema {
 		$result    = StepProcessorResult::success( 'ImportSchema' );
 		$results[] = $result;
 
-		$step_processors    = $this->builtin_step_processors->get_all();
-		$step_processors    = $this->wp_apply_filters( 'wooblueprint_importers', $step_processors );
-		$indexed_step_processors = Util::index_array( $step_processors, function($key, $step_processor) {
-			return $step_processor->get_step_class()::get_step_name();
-		} );
+		$step_processors = $this->builtin_step_processors->get_all();
+
+		/**
+		 * Filters the step processors.
+		 *
+		 * Allows adding/removing custom step processors.
+		 *
+		 * @param StepProcessor[] $step_processors The step processors.
+		 *
+		 * @since 0.0.1
+		 */
+		$step_processors = $this->wp_apply_filters( 'wooblueprint_importers', $step_processors );
+
+		$indexed_step_processors = Util::index_array(
+			$step_processors,
+			function ( $key, $step_processor ) {
+				return $step_processor->get_step_class()::get_step_name();
+			}
+		);
 
 		// validate steps before processing
 		$this->validate_step_schemas( $indexed_step_processors, $result );
 
-		if ( count( $result->get_messages('error') ) !== 0 ) {
+		if ( count( $result->get_messages( 'error' ) ) !== 0 ) {
 			return $results;
 		}
-
 
 		foreach ( $this->schema->get_steps() as $step_schema ) {
 			$stepProcessor = $indexed_step_processors[ $step_schema->step ] ?? null;
@@ -125,28 +138,31 @@ class ImportSchema {
 	}
 
 	protected function validate_step_schemas( array $indexed_step_processors, StepProcessorResult $result ) {
-		$step_schemas = array_map( function($step_processor) {
-			return $step_processor->get_step_class()::get_schema();
-		}, $indexed_step_processors );
+		$step_schemas = array_map(
+			function ( $step_processor ) {
+				return $step_processor->get_step_class()::get_schema();
+			},
+			$indexed_step_processors
+		);
 
-		foreach ($this->schema->get_steps() as $step_json) {
+		foreach ( $this->schema->get_steps() as $step_json ) {
 			$step_schema = $step_schemas[ $step_json->step ] ?? null;
 			if ( ! $step_schema ) {
 				$result->add_info( "No schema found for step $step_json->step" );
 				continue;
 			}
 
-			$validate = $this->validator->validate( $step_json, json_encode($step_schema) );
+			$validate = $this->validator->validate( $step_json, json_encode( $step_schema ) );
 
 			if ( ! $validate->isValid() ) {
 				$result->add_error( "Schema validation failed for step {$step_json->step}" );
-				$errors = ( new ErrorFormatter() )->format( $validate->error() );
+				$errors           = ( new ErrorFormatter() )->format( $validate->error() );
 				$formatted_errors = array();
-				foreach ($errors as $value) {
-					$formatted_errors[] = implode("\n", $value);
+				foreach ( $errors as $value ) {
+					$formatted_errors[] = implode( "\n", $value );
 				}
 
-				$result->add_error( implode("\n", $formatted_errors) );
+				$result->add_error( implode( "\n", $formatted_errors ) );
 			}
 		}
 	}
