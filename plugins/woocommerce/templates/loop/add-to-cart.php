a5253a2573935f7f94a606eccd65d554dba6987b
Sam Seay <samueljseay@gmail.com>
2024-07-25 15:14:29 +0800

    Bump template version

--- plugins/woocommerce/templates/loop/add-to-cart.php
+++ plugins/woocommerce/templates/loop/add-to-cart.php
@@ -12,7 +12,7 @@
  *
  * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     9.0.0
+ * @version     9.2.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
