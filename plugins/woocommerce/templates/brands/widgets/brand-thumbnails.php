edfcba96ecbd3164f0ff995ce642a76eb486aba8
Jason Kytros <jimjasson@outlook.com.gr>
2024-07-25 16:56:08 +0300

    Fix linting errors in brand-thumbnails.php

--- plugins/woocommerce/templates/brands/widgets/brand-thumbnails.php
+++ plugins/woocommerce/templates/brands/widgets/brand-thumbnails.php
@@ -16,27 +16,27 @@
  */
 
 $wrapper_class = 'fluid-columns';
-if ( ! $fluid_columns && in_array( $columns, array( 1, 2, 3, 4, 5, 6 ) ) ) {
-	$wrapper_class = 'columns-' .  $columns;
+if ( ! $fluid_columns && in_array( $columns, array( 1, 2, 3, 4, 5, 6 ), true ) ) {
+	$wrapper_class = 'columns-' . $columns;
 }
 ?>
 <ul class="brand-thumbnails <?php echo esc_attr( $wrapper_class ); ?>">
 
 <?php
-	foreach ( array_values( $brands ) as $index => $brand ) :
-		$class = '';
-		if ( $index == 0 || $index % $columns == 0 ) {
-			$class = 'first';
-		} else if ( ( $index + 1 ) % $columns == 0 ) {
-			$class = 'last';
-		}
-		?>
+foreach ( array_values( $brands ) as $index => $brand ) :
+	$class = '';
+	if ( $index === 0 || $index % $columns === 0 ) {
+		$class = 'first';
+	} else if ( ( $index + 1 ) % $columns === 0 ) {
+		$class = 'last';
+	}
+	?>
 
-		<li class="<?php echo esc_attr( $class ); ?>">
-			<a href="<?php echo esc_url( get_term_link( $brand->slug, 'product_brand' ) ); ?>" title="<?php echo esc_attr( $brand->name ); ?>">
-				<?php echo get_brand_thumbnail_image( $brand ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
-			</a>
-		</li>
+	<li class="<?php echo esc_attr( $class ); ?>">
+		<a href="<?php echo esc_url( get_term_link( $brand->slug, 'product_brand' ) ); ?>" title="<?php echo esc_attr( $brand->name ); ?>">
+			<?php echo get_brand_thumbnail_image( $brand ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
+		</a>
+	</li>
 
 <?php endforeach; ?>
 
