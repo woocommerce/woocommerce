df2d1b8ff1c6c6d3250fef6e298a59f169ab558b
rjchow <me@rjchow.com>
2024-07-26 15:17:08 +0800

    Added a new settings-payments-offline screen to the WooCommerce admin plugin.

--- plugins/woocommerce-admin/client/settings-payments/index.tsx
+++ plugins/woocommerce-admin/client/settings-payments/index.tsx
@@ -10,10 +10,25 @@ const SettingsPaymentsMainChunk = lazy(
 		)
 );
 
+const SettingsPaymentsOfflineChunk = lazy(
+	() =>
+		import(
+			/* webpackChunkName: "settings-payments-offline" */ './settings-payments-offline'
+		)
+);
+
 export const SettingsPaymentsMainWrapper: React.FC = () => {
 	return (
 		<Suspense fallback={ null }>
 			<SettingsPaymentsMainChunk />
+		</Suspense>
+	);
+};
+
+export const SettingsPaymentsOfflineWrapper: React.FC = () => {
+	return (
+		<Suspense fallback={ null }>
+			<SettingsPaymentsOfflineChunk />
 		</Suspense>
 	);
 };
