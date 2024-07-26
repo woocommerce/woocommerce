10ff8201f698e6d3bb343d1e37eed5c29aae06b4
Jason Kytros <jimjasson@outlook.com.gr>
2024-07-26 09:22:55 +0300

    Lint errors: Fix taxonomy-product_brand

--- plugins/woocommerce/templates/brands/taxonomy-product_brand.php
+++ plugins/woocommerce/templates/brands/taxonomy-product_brand.php
@@ -1,6 +1,10 @@
 <?php
 /**
  * Use the WooCommerce archive template for brand taxonomy pages
+ *
+ * @package WooCommerce\Templates
+ * @version x.x.x
  */
 
+// phpcs:ignore WordPress.Files.FileName.NotHyphenatedLowercase for backwards compatibility.
 wc_get_template( 'archive-product.php' );
