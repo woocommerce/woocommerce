3fa93372ee224d7551ac9e7929bf995d76f5849e
Moon <moon.kyong@automattic.com>
2024-07-26 11:18:56 -0700

    blueprint - add rest api (#49765)
    * Add blueprint rest api endpoint
    
    * Rename process to import
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Update plugins/woocommerce/src/Admin/Features/Blueprint/Init.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Update plugins/woocommerce/src/Admin/Features/Blueprint/RestApi.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Update plugins/woocommerce/src/Admin/Features/Blueprint/RestApi.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Update plugins/woocommerce/src/Admin/Features/Blueprint/RestApi.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Update plugins/woocommerce/src/Admin/Features/Blueprint/RestApi.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Remove unavailable 2nd argument from get_param -- default values are already set from the rest api def.
    
    * blueprint - add builder page  (#49774)
    
    * Add blueprint builder page
    
    * Change to the correct endpoint
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Note that the builder intro page is temporary
    
    * Remove unused code
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    
    * blueprint - add blueprint settings page (#49775)
    
    * Add blueprint settings page
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    
    * Fix broken test
    
    * Lint fixes
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>

---
+++ plugins/woocommerce/src/Admin/Features/Blueprint/Init.php
@@ -0,0 +1,55 @@
+<?php
+
+declare( strict_types = 1);
+
+namespace Automattic\WooCommerce\Admin\Features\Blueprint;
+
+use Automattic\WooCommerce\Admin\PageController;
+
+/**
+ * Class Init
+ *
+ * This class initializes the Blueprint feature for WooCommerce.
+ */
+class Init {
+	/**
+	 * Init constructor.
+	 */
+	public function __construct() {
+		add_action( 'rest_api_init', array( $this, 'init_rest_api' ) );
+		add_filter( 'woocommerce_admin_shared_settings', array( $this, 'add_upload_nonce_to_settings' ) );
+	}
+
+	/**
+	 * Register REST API routes.
+	 *
+	 * @return void
+	 */
+	public function init_rest_api() {
+		( new RestApi() )->register_routes();
+	}
+
+
+	/**
+	 * Add upload nonce to global JS settings.
+	 *
+	 * The value can be accessed at wcSettings.admin.blueprint_upload_nonce
+	 *
+	 * @param array $settings Global JS settings.
+	 *
+	 * @return array
+	 */
+	public function add_upload_nonce_to_settings( array $settings ) {
+		if ( ! is_admin() ) {
+			return $settings;
+		}
+
+		$page_id = PageController::get_instance()->get_current_screen_id();
+		if ( 'woocommerce_page_wc-admin' === $page_id ) {
+			$settings['blueprint_upload_nonce'] = wp_create_nonce( 'blueprint_upload_nonce' );
+			return $settings;
+		}
+
+		return $settings;
+	}
+}
