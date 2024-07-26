1be22adbdda104108dce5fb26292b40796f7f87c
nigeljamesstevenson <105309450+nigeljamesstevenson@users.noreply.github.com>
2024-07-26 09:46:15 +0100

    Cherry pick #351 into release/9.1 (#50010)
    * XSS vulnerability on various unescaped attributes
    
    * escape Product Filter Stock status
    
    * escape more blocks
    
    * escape more blocks
    
    * escape more blocks
    
    * add changelog
    
    * update changelog
    
    ---------
    
    Co-authored-by: Luigi Teschio <gigitux@gmail.com>

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection.php
@@ -112,7 +112,7 @@ class ProductCollection extends AbstractBlock {
 					'data-wc-navigation-id',
 					'wc-product-collection-' . $this->parsed_block['attrs']['queryId']
 				);
-				$p->set_attribute( 'data-wc-interactive', wp_json_encode( array( 'namespace' => 'woocommerce/product-collection' ) ) );
+				$p->set_attribute( 'data-wc-interactive', wp_json_encode( array( 'namespace' => 'woocommerce/product-collection' ), JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP ) );
 				$p->set_attribute(
 					'data-wc-context',
 					wp_json_encode(
@@ -124,7 +124,7 @@ class ProductCollection extends AbstractBlock {
 							// This way we avoid prefetching when the page loads.
 							'isPrefetchNextOrPreviousLink' => false,
 						),
-						JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP
+						JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
 					)
 				);
 				$block_content = $p->get_updated_html();
@@ -226,7 +226,7 @@ class ProductCollection extends AbstractBlock {
 				'class_name' => $class_name,
 			)
 		) ) {
-			$processor->set_attribute( 'data-wc-interactive', wp_json_encode( array( 'namespace' => 'woocommerce/product-collection' ) ) );
+			$processor->set_attribute( 'data-wc-interactive', wp_json_encode( array( 'namespace' => 'woocommerce/product-collection' ), JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP ) );
 			$processor->set_attribute( 'data-wc-on--click', 'actions.navigate' );
 			$processor->set_attribute( 'data-wc-key', $key_prefix . '--' . esc_attr( wp_rand() ) );
 
