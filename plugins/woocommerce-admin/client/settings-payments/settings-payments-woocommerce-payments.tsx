a2e5a66e39c70930f3922b9152c13a56c143ade7
rjchow <me@rjchow.com>
2024-07-26 16:37:46 +0800

    Removed unnecessary Card component and placeholder text from WooCommerce Payments settings.

--- plugins/woocommerce-admin/client/settings-payments/settings-payments-woocommerce-payments.tsx
+++ plugins/woocommerce-admin/client/settings-payments/settings-payments-woocommerce-payments.tsx
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { Card } from '@wordpress/components';
-
-/**
  * Internal dependencies
  */
 import './settings-payments-woocommerce-payments.scss';
@@ -12,10 +7,6 @@ export const SettingsPaymentsWooCommercePayments: React.FC = () => {
 	return (
 		<div className="settings-payments-woocommerce-payments__container">
 			<h1>WooCommerce Payments Settings</h1>
-			<Card>
-				<p>This is a placeholder for WooCommerce Payments settings.</p>
-				{ /* Add more WooCommerce Payments specific settings here */ }
-			</Card>
 		</div>
 	);
 };
