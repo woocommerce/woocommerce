ef8d98e881790ced406cd3a9c96bb95c61141d26
Jason Kytros <jimjasson@outlook.com.gr>
2024-07-26 12:23:59 +0300

    Another try to fix missing docblocks

--- plugins/woocommerce/includes/widgets/class-wc-widget-brand-nav.php
+++ plugins/woocommerce/includes/widgets/class-wc-widget-brand-nav.php
@@ -431,14 +431,19 @@ class WC_Widget_Brand_Nav extends WC_Widget {
 
 			echo '<li class="wc-layered-nav-term ' . ( $option_is_set ? 'chosen' : '' ) . '">';
 
-			echo ( $count > 0 || $option_is_set ) ? '<a href="' . esc_url( apply_filters( 'woocommerce_layered_nav_link', $link ) ) . '">' : '<span>'; // phpcs:ignoreWooCommerce.Commenting.CommentHooks.MissingHookComment
-
+			/**
+			 * phpcs:ignoreWooCommerce.Commenting.CommentHooks.MissingHookComment
+			 */
+			echo ( $count > 0 || $option_is_set ) ? '<a href="' . esc_url( apply_filters( 'woocommerce_layered_nav_link', $link ) ) . '">' : '<span>';
 
 			echo esc_html( $term->name );
 
 			echo ( $count > 0 || $option_is_set ) ? '</a> ' : '</span> ';
 
-			echo wp_kses_post( apply_filters( 'woocommerce_layered_nav_count', '<span class="count">(' . absint( $count ) . ')</span>', $count, $term ) ); // phpcs:ignoreWooCommerce.Commenting.CommentHooks.MissingHookComment
+			/**
+			 * phpcs:ignoreWooCommerce.Commenting.CommentHooks.MissingHookComment
+			 */
+			echo wp_kses_post( apply_filters( 'woocommerce_layered_nav_count', '<span class="count">(' . absint( $count ) . ')</span>', $count, $term ) );
 
 			$child_terms = get_terms(
 				array(
@@ -503,14 +508,20 @@ class WC_Widget_Brand_Nav extends WC_Widget {
 			AND terms.term_id IN (' . implode( ',', array_map( 'absint', $term_ids ) ) . ')
 		';
 		$query['group_by'] = 'GROUP BY terms.term_id';
-		$query             = apply_filters( 'woocommerce_get_filtered_term_product_counts_query', $query ); // phpcs:ignoreWooCommerce.Commenting.CommentHooks.MissingHookComment
-		$query             = implode( ' ', $query );
+		/**
+		 * phpcs:ignoreWooCommerce.Commenting.CommentHooks.MissingHookComment
+		 */
+		$query = apply_filters( 'woocommerce_get_filtered_term_product_counts_query', $query );
+		$query = implode( ' ', $query );
 
 		// We have a query - let's see if cached results of this query already exist.
 		$query_hash = md5( $query );
 
 		// Maybe store a transient of the count values.
-		$cache = apply_filters( 'woocommerce_layered_nav_count_maybe_cache', true ); // phpcs:ignoreWooCommerce.Commenting.CommentHooks.MissingHookComment
+		/**
+		 * phpcs:ignoreWooCommerce.Commenting.CommentHooks.MissingHookComment
+		 */
+		$cache = apply_filters( 'woocommerce_layered_nav_count_maybe_cache', true );
 		if ( true === $cache ) {
 			$cached_counts = (array) get_transient( 'wc_layered_nav_counts_' . sanitize_title( $taxonomy ) );
 		} else {
