064e5b4933150a55d265b81fe6f43ad080f53fe9
Walther Lalk <83255+dakota@users.noreply.github.com>
2024-07-26 09:03:11 +0100

    Better messages for fix-branch.sh

--- plugins/woocommerce/bin/fix-branch.sh
+++ plugins/woocommerce/bin/fix-branch.sh
@@ -2,12 +2,12 @@
 
 # Lint branch
 #
-# Runs phpcs-changed, comparing the current branch to its "base" or "parent" branch.
+# Runs phpcbf, comparing the current branch to its "base" or "parent" branch.
 # The base branch defaults to trunk, but another branch name can be specified as an
 # optional positional argument.
 #
 # Example:
-# ./lint-branch.sh base-branch
+# ./fix-branch.sh base-branch
 
 baseBranch=${1:-"trunk"}
 redColoured='\033[0;31m'
@@ -15,14 +15,14 @@ redColoured='\033[0;31m'
 # Lint changed php-files to match code style.
 changedFiles=$(git diff $(git merge-base HEAD $baseBranch) --relative --name-only --diff-filter=d -- '*.php')
 if [[ -n $changedFiles ]]; then
-	printf "Linting the following files with CodeSniffer:\n"
+	printf "Attemping to fix the following files with Code Beautifier and Fixer:\n"
 	printf "    %s\n" $changedFiles
 
     composer exec phpcbf -- -s $changedFiles
     if [[ $? != 0 ]]; then
-    	printf "${redColoured}Unfortunately, CodeSniffer reported some violations which need to be addressed (see above).\n"
+    	printf "${redColoured}Unfortunately, Code Beautifier and Fixer could not fix all violations which need to be addressed manually - Please run lint-branch.sh.\n"
     	exit 1
 	else
-		printf "CodeSniffer reported no violations, well done!\n"
+		printf "Code Beautifier and Fixer reported no violations or was able to fix everything, well done!\n"
     fi
 fi
