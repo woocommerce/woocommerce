0dead27effa18ee0f227557b05bc696abe4812c2
moon <moon.kyong@automattic.com>
2024-07-29 10:55:57 -0700

    Remove unused construct code

--- packages/php/blueprint/tests/TestCase.php
+++ packages/php/blueprint/tests/TestCase.php
@@ -9,11 +9,6 @@ require_once __DIR__.'/helpers.php';
 abstract class TestCase extends PHPUnitTestCase
 {
 	protected bool $load_wp = false;
-	public function __construct() {
-		if ( $this->load_wp ) {
-		}
-		parent::__construct();
-	}
 
 	public function get_fixture_path( $filename ) {
 		return __DIR__ . '/fixtures/' . $filename;
