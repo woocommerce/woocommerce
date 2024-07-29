7f97056e2a902f69721484c818204d2c53ef201a
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-07-29 16:20:54 +0800

    Update .eslintrc.js

--- packages/js/remote-logging/.eslintrc.js
+++ packages/js/remote-logging/.eslintrc.js
@@ -2,21 +2,6 @@ module.exports = {
 	extends: [ 'plugin:@woocommerce/eslint-plugin/recommended' ],
 	root: true,
 	ignorePatterns: [ '**/test/*.ts', '**/test/*.tsx' ],
-	overrides: [
-		{
-			files: [
-				'**/stories/*.js',
-				'**/stories/*.jsx',
-				'**/docs/example.js',
-			],
-			rules: {
-				'import/no-unresolved': [
-					'warn',
-					{ ignore: [ '@woocommerce/components' ] },
-				],
-			},
-		},
-	],
 	settings: {
 		'import/core-modules': [ '@woocommerce/settings' ],
 		'import/resolver': {
