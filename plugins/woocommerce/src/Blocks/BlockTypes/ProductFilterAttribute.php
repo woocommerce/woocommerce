289e6b96524a2084439b3e4b787424c7e3b5a12f
nigeljamesstevenson <105309450+nigeljamesstevenson@users.noreply.github.com>
2024-07-29 13:15:09 +0100

    Cherry pick #351 into release/9.2 (#50072)
    * XSS vulnerability on various unescaped attributes
    
    * escape Product Filter Stock status
    
    * escape more blocks
    
    * escape more blocks
    
    * escape more blocks
    
    * add changelog
    
    * update changelog
    
    ---------
    
    Co-authored-by: Luigi Teschio <gigitux@gmail.com>

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductFilterAttribute.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductFilterAttribute.php
@@ -107,7 +107,8 @@ final class ProductFilterAttribute extends AbstractBlock {
 									'value'         => $term,
 									'attributeSlug' => $product_attribute,
 									'queryType'     => get_query_var( "query_type_{$product_attribute}" ),
-								)
+								),
+								JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
 							),
 						),
 					);
@@ -146,7 +147,7 @@ final class ProductFilterAttribute extends AbstractBlock {
 				'<div %s></div>',
 				get_block_wrapper_attributes(
 					array(
-						'data-wc-interactive' => wp_json_encode( array( 'namespace' => $this->get_full_block_name() ) ),
+						'data-wc-interactive' => wp_json_encode( array( 'namespace' => $this->get_full_block_name() ), JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP ),
 						'data-has-filter'     => 'no',
 					)
 				),
@@ -198,8 +199,8 @@ final class ProductFilterAttribute extends AbstractBlock {
 			'<div %1$s>%2$s%3$s</div>',
 			get_block_wrapper_attributes(
 				array(
-					'data-wc-context'     => wp_json_encode( $context ),
-					'data-wc-interactive' => wp_json_encode( array( 'namespace' => $this->get_full_block_name() ) ),
+					'data-wc-context'     => wp_json_encode( $context, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP ),
+					'data-wc-interactive' => wp_json_encode( array( 'namespace' => $this->get_full_block_name() ), JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP ),
 					'data-has-filter'     => 'yes',
 				)
 			),
@@ -320,7 +321,7 @@ final class ProductFilterAttribute extends AbstractBlock {
 
 		$attribute_counts = array_reduce(
 			$attribute_counts,
-			function( $acc, $count ) {
+			function ( $acc, $count ) {
 				$acc[ $count['term'] ] = $count['count'];
 				return $acc;
 			},
