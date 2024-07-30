b006e145f977e6e40a3ef614ec62ad377f4d9d55
Nathan Schneider <nsschneider1@gmail.com>
2024-07-30 15:52:12 -0300

    Add additional tests for SelectTree

--- packages/js/components/src/experimental-select-tree-control/test/select-tree.test.tsx
+++ packages/js/components/src/experimental-select-tree-control/test/select-tree.test.tsx
@@ -1,4 +1,6 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useState } from 'react';
 import React, { createElement } from '@wordpress/element';
 import { SelectTree } from '../select-tree';
 import { Item } from '../../experimental-tree-control';
@@ -24,6 +26,44 @@ const DEFAULT_PROPS = {
 	items: mockItems,
 	label: 'Select Tree',
 	placeholder: 'Type here',
+};
+
+const TestComponent = ( { multiple }: { multiple?: boolean } ) => {
+	const [ typedValue, setTypedValue ] = useState( '' );
+	const [ selected, setSelected ] = useState< any >( [] );
+
+	return createElement( SelectTree, {
+		...DEFAULT_PROPS,
+		multiple,
+		shouldShowCreateButton: () => true,
+		onInputChange: ( value ) => {
+			setTypedValue( value || '' );
+		},
+		createValue: typedValue,
+		selected: Array.isArray( selected )
+			? selected.map( ( i ) => ( {
+					value: String( i.id ),
+					label: i.name,
+			  } ) )
+			: {
+					value: String( selected.id ),
+					label: selected.name,
+			  },
+		onSelect: ( item: Item | Item[] ) =>
+			item && Array.isArray( item )
+				? setSelected(
+						item.map( ( i ) => ( {
+							id: +i.value,
+							name: i.label,
+							parent: i.parent ? +i.parent : 0,
+						} ) )
+				  )
+				: setSelected( {
+						id: +item.value,
+						name: item.label,
+						parent: item.parent ? +item.parent : 0,
+				  } ),
+	} );
 };
 
 describe( 'SelectTree', () => {
@@ -58,8 +98,9 @@ describe( 'SelectTree', () => {
 		expect( queryByText( 'Create new' ) ).not.toBeInTheDocument();
 	} );
 	it( 'should show a root item when focused and child when expand button is clicked', () => {
-		const { queryByText, queryByLabelText, queryByRole } =
-			render( <SelectTree { ...DEFAULT_PROPS } /> );
+		const { queryByText, queryByLabelText, queryByRole } = render(
+			<SelectTree { ...DEFAULT_PROPS } />
+		);
 		queryByRole( 'combobox' )?.focus();
 		expect( queryByText( 'Item 1' ) ).toBeInTheDocument();
 
@@ -103,5 +144,31 @@ describe( 'SelectTree', () => {
 		queryByRole( 'combobox' )?.focus();
 		queryByText( 'Create "new item"' )?.click();
 		expect( mockFn ).toBeCalledTimes( 1 );
+	} );
+	it( 'correctly selects existing item in single mode with arrow keys', async () => {
+		const { findByRole } = render( <TestComponent /> );
+		const combobox = ( await findByRole( 'combobox' ) ) as HTMLInputElement;
+		combobox.focus();
+		userEvent.keyboard( '{arrowdown}{enter}' );
+		expect( combobox.value ).toBe( 'Item 1' );
+	} );
+	it( 'correctly selects existing item in single mode by typing and pressing Enter', async () => {
+		const { findByRole } = render( <TestComponent /> );
+		const combobox = ( await findByRole( 'combobox' ) ) as HTMLInputElement;
+		combobox.focus();
+		userEvent.keyboard( 'Item 1{enter}' );
+		userEvent.tab();
+		expect( combobox.value ).toBe( 'Item 1' );
+	} );
+	it( 'correctly selects existing item in multiple mode by typing and pressing Enter', async () => {
+		const { findByRole, getAllByText } = render(
+			<TestComponent multiple />
+		);
+		const combobox = ( await findByRole( 'combobox' ) ) as HTMLInputElement;
+		combobox.focus();
+		userEvent.keyboard( 'Item 1' );
+		userEvent.keyboard( '{enter}' );
+		expect( combobox.value ).toBe( '' ); // input is cleared
+		expect( getAllByText( 'Item 1' )[ 0 ] ).toBeInTheDocument(); // item is selected (turns into a token)
 	} );
 } );
