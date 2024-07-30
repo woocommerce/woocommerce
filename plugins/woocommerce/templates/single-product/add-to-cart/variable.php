6e6704651cb2f8ae0ce67d8388a1404e79c2f135
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-07-30 11:59:34 -0300

    Add docblock comment to woocommerce_reset_variations_link filter

--- plugins/woocommerce/templates/single-product/add-to-cart/variable.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/variable.php
@@ -45,6 +45,13 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 										'product'   => $product,
 									)
 								);
+								/**
+								 * Filters the reset variation link.
+								 *
+								 * @since 2.5.0
+								 *
+								 * @param string  $link The reset variation link HTML.
+								 */
 								echo end( $attribute_keys ) === $attribute_name ? wp_kses_post( apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#" aria-label="' . esc_attr__( 'Clear all options', 'woocommerce' ) . '">' . esc_html__( 'Clear', 'woocommerce' ) . '</a>' ) ) : '';
 							?>
 						</td>
