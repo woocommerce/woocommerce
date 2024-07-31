9bda9cfb4144c411fa80d57bee034017944636ee
Manish Menaria <the.manish.menaria@gmail.com>
2024-07-31 18:12:25 +0530

    Try using ProductSelect component from Product Editor in WooCommerce Blocks

--- plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/EditorProductPicker.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/EditorProductPicker.tsx
@@ -4,7 +4,10 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 import { Icon, info } from '@wordpress/icons';
-import ProductControl from '@woocommerce/editor-components/product-control';
+import { useEffect, useState } from '@wordpress/element';
+import { __experimentalProductSelect as ProductSelect } from '@woocommerce/product-editor';
+import { getProducts } from '@woocommerce/editor-components/utils';
+import { Product } from '@woocommerce/data';
 import {
 	Placeholder,
 	// @ts-expect-error Using experimental features
@@ -19,14 +22,47 @@ import {
 import type { ProductCollectionEditComponentProps } from '../types';
 import { getCollectionByName } from '../collections';
 
+const getSearchResult = async ( search = '' ) => {
+	const query = {
+		queryArgs: {
+			search,
+			// Limit search to 40 results. If results are not satisfying
+			// user needs to type more characters to get closer to actual
+			// product name.
+			per_page: 40,
+		},
+	};
+	return ( await getProducts( query ) ) as Product[];
+};
+
 const EditorProductPicker = ( props: ProductCollectionEditComponentProps ) => {
+	const [ selectedProduct, setSelectedProduct ] = useState( null );
+	const [ searchResults, setSearchResults ] = useState< Product[] >( [] );
 	const blockProps = useBlockProps();
-	const attributes = props.attributes;
+	// const attributes = props.attributes;
+
+	console.log( 'searchResults', searchResults );
+
+	useEffect( () => {
+		getSearchResult().then( ( results ) => {
+			setSearchResults( results as Product[] );
+		} );
+	}, [] );
 
 	const collection = getCollectionByName( props.attributes.collection );
 	if ( ! collection ) {
 		return;
 	}
+
+	const handleSelect = ( product ) => {
+		setSelectedProduct( product );
+	};
+
+	const handleFilter = async ( search?: string ) => {
+		return getSearchResult( search );
+	};
+
+	console.log( 'ProductSelect', ProductSelect );
 
 	return (
 		<div { ...blockProps }>
@@ -41,7 +77,7 @@ const EditorProductPicker = ( props: ProductCollectionEditComponentProps ) => {
 						to be selected in order to display associated items.
 					</Text>
 				</HStack>
-				<ProductControl
+				{ /* <ProductControl
 					selected={ attributes.selectedReference?.id || 0 }
 					showVariations
 					onChange={ ( value = [] ) => {
@@ -53,6 +89,13 @@ const EditorProductPicker = ( props: ProductCollectionEditComponentProps ) => {
 							},
 						} );
 					} }
+				/> */ }
+				{ /* <ProductSelect /> */ }
+				<ProductSelect
+					items={ searchResults }
+					filter={ handleFilter }
+					selected={ null }
+					onSelect={ handleSelect }
 				/>
 			</Placeholder>
 		</div>
