cb6bf29d2ef93407595180831f7b172f3719f476
Nathan Schneider <nsschneider1@gmail.com>
2024-07-31 10:03:03 -0300

    Remove blank line

--- plugins/woocommerce/tests/php/includes/wc-core-functions-test.php
+++ plugins/woocommerce/tests/php/includes/wc-core-functions-test.php
@@ -132,6 +132,5 @@ class WC_Core_Functions_Test extends \WC_Unit_Test_Case {
 		$expected = '<span class="woocommerce-help-tip" tabindex="0" aria-label="Strong text regular text" data-tip="&lt;strong&gt;Strong text&lt;/strong&gt; regular text"></span>';
 		$this->assertEquals( $expected, wc_help_tip( '<strong>Strong text</strong> regular text', false ) );
 		$this->assertEquals( $expected, wc_help_tip( '<strong>Strong text</strong> regular text', true ) );
-
 	}
 }
