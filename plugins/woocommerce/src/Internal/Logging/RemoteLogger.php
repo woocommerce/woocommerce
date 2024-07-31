3c7c4e81276e56eba9877d407d70a75a44b06bbb
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-07-31 14:06:26 +0800

    Update plugins/woocommerce/src/Internal/Logging/RemoteLogger.php
    Co-authored-by: Barry Hughes <3594411+barryhughes@users.noreply.github.com>

--- plugins/woocommerce/src/Internal/Logging/RemoteLogger.php
+++ plugins/woocommerce/src/Internal/Logging/RemoteLogger.php
@@ -39,10 +39,9 @@ class RemoteLogger extends \WC_Log_Handler {
 	 * @param \WC_Logger_Interface|null $logger Logger instance.
 	 */
 	public function __construct( \WC_Logger_Interface $logger = null ) {
-		if ( null === $logger ) {
-			$this->local_logger = wc_get_logger();
-		}
-		$this->local_logger = $logger;
+		$this->local_logger = null === $logger
+			? wc_get_logger()
+			: $logger;
 	}
 
 	/**
