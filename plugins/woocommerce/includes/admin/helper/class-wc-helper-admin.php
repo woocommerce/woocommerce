fe7a2c136881d6e975cdf61bafcbb33719a15fe8
Akeda Bagus <akeda.bagus@automattic.com>
2024-08-01 14:01:21 +0700

    Use UTM parameters to link Tracks events: CTA from connect notice and successful site connection (#50126)
    Pass UTM parameters to get events from clicking "connect" notice to site being connected

--- plugins/woocommerce/includes/admin/helper/class-wc-helper-admin.php
+++ plugins/woocommerce/includes/admin/helper/class-wc-helper-admin.php
@@ -101,6 +101,14 @@ class WC_Helper_Admin {
 			$connect_url_args['wc-helper-nonce']   = wp_create_nonce( 'connect' );
 		}
 
+		if ( ! empty( $_GET['utm_source'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			$connect_url_args['utm_source'] = wc_clean( wp_unslash( $_GET['utm_source'] ) ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		}
+
+		if ( ! empty( $_GET['utm_campaign'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			$connect_url_args['utm_campaign'] = wc_clean( wp_unslash( $_GET['utm_campaign'] ) ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		}
+
 		return add_query_arg(
 			$connect_url_args,
 			admin_url( 'admin.php' )
