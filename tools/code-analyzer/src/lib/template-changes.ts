320a7d04add01f173afecb5263498a343ffbf6ef
Jason Kytros <jimjasson@outlook.com.gr>
2024-08-01 17:56:58 +0300

    Add debug statements

--- tools/code-analyzer/src/lib/template-changes.ts
+++ tools/code-analyzer/src/lib/template-changes.ts
@@ -24,7 +24,8 @@ export const scanForTemplateChanges = async (
 	repositoryPath?: string
 ) => {
 	const changes: Map< string, TemplateChangeDescription > = new Map();
-
+console.log( "Scanning for changes" );
+console.log( content );
 	if ( ! content.match( /diff --git a\/(.+)\/templates\/(.+)\.php/g ) ) {
 		return changes;
 	}
