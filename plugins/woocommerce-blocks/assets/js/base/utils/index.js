a8526f193a742a479cac255cc051b410991b5a62
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-08-01 10:26:47 +0200

    Remove the old legacy-events util

--- plugins/woocommerce-blocks/assets/js/base/utils/index.js
+++ plugins/woocommerce-blocks/assets/js/base/utils/index.js
@@ -1,7 +1,6 @@
 export * from './errors';
 export * from './address';
 export * from './shipping-rates';
-export * from './legacy-events';
 export * from './render-frontend';
 export * from './get-valid-block-attributes';
 export * from './product-data';
