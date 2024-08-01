3d0bb5891019dc8618b10977a126dcafbd54fe9b
Jason Kytros <jimjasson@outlook.com.gr>
2024-08-01 18:22:41 +0300

    Remove unnecessary change

--- tools/code-analyzer/src/lib/template-changes.ts
+++ tools/code-analyzer/src/lib/template-changes.ts
@@ -24,7 +24,7 @@ export const scanForTemplateChanges = async (
 	repositoryPath?: string
 ) => {
 	const changes: Map< string, TemplateChangeDescription > = new Map();
-	
+
 	if ( ! content.match( /diff --git a\/(.+)\/templates\/(.+)\.php/g ) ) {
 		return changes;
 	}
