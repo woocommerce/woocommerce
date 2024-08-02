408ad4813170c397c6057d683d6a22cca593238b
amesplant <ames.plant@10up.com>
2024-08-02 08:36:33 -0500

    Fix linting errors

--- plugins/woocommerce/templates/emails/email-styles.php
+++ plugins/woocommerce/templates/emails/email-styles.php
@@ -20,12 +20,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Load colors.
-$bg                = get_option( 'woocommerce_email_background_color' );
-$body              = get_option( 'woocommerce_email_body_background_color' );
-$base              = get_option( 'woocommerce_email_base_color' );
-$base_text         = wc_light_or_dark( $base, '#202020', '#ffffff' );
-$text              = get_option( 'woocommerce_email_text_color' );
-$footer_text       = get_option( 'woocommerce_email_footer_text_color' );
+$bg          = get_option( 'woocommerce_email_background_color' );
+$body        = get_option( 'woocommerce_email_body_background_color' );
+$base        = get_option( 'woocommerce_email_base_color' );
+$base_text   = wc_light_or_dark( $base, '#202020', '#ffffff' );
+$text        = get_option( 'woocommerce_email_text_color' );
+$footer_text = get_option( 'woocommerce_email_footer_text_color' );
 
 // Pick a contrasting color for links.
 $link_color = wc_hex_is_light( $base ) ? $base : $base_text;
