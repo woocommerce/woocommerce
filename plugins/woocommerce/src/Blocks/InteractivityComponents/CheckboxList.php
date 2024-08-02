3c1f375ff6a6c09f72e456cc417a9d4fcf09d7dc
Tung Du <dinhtungdu@gmail.com>
2024-08-02 14:40:26 +0700

    update: new simplified markup and style

--- plugins/woocommerce/src/Blocks/InteractivityComponents/CheckboxList.php
+++ plugins/woocommerce/src/Blocks/InteractivityComponents/CheckboxList.php
@@ -34,44 +34,49 @@ class CheckboxList {
 		ob_start();
 		?>
 		<div data-wc-interactive='<?php echo esc_attr( $namespace ); ?>'>
-			<div data-wc-context='<?php echo wp_json_encode( $checkbox_list_context, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP ); ?>' >
-			<div class="wc-block-stock-filter style-list">
-					<ul class="wc-block-components-checkbox-list">
-						<?php foreach ( $items as $item ) { ?>
-							<?php
-							$item['id'] = $item['id'] ?? uniqid( 'checkbox-' );
-							// translators: %s: checkbox label.
-							$i18n_label = sprintf( __( 'Checkbox: %s', 'woocommerce' ), $item['aria_label'] ?? '' );
-							?>
-							<li data-wc-key="<?php echo esc_attr( $item['id'] ); ?>">
-								<div class="wc-block-components-checkbox">
-									<label for="<?php echo esc_attr( $item['id'] ); ?>">
-										<input
-											id="<?php echo esc_attr( $item['id'] ); ?>"
-											class="wc-block-components-checkbox__input"
-											type="checkbox"
-											aria-invalid="false"
-											aria-label="<?php echo esc_attr( $i18n_label ); ?>"
-											data-wc-on--change--select-item="actions.selectCheckboxItem"
-											data-wc-on--change--parent-action="<?php echo esc_attr( $on_change ); ?>"
-											value="<?php echo esc_attr( $item['value'] ); ?>"
-											<?php checked( $item['checked'], 1 ); ?>
-										>
-											<svg class="wc-block-components-checkbox__mark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20">
-												<path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path>
-											</svg>
-											<span class="wc-block-components-checkbox__label">
-												<?php // The label can be HTML, so we don't want to escape it. ?>
-												<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-												<?php echo $item['label']; ?>
-											</span>
-									</label>
-								</div>
-							</li>
-						<?php } ?>
-					</ul>
-				</div>
-			</div>
+			<ul
+				class="wc-block-interactivity-components-checkbox-list"
+				data-wc-context='<?php echo wp_json_encode( $checkbox_list_context, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP ); ?>'
+			>
+				<?php foreach ( $items as $item ) { ?>
+					<?php
+					$item['id'] = $item['id'] ?? uniqid( 'checkbox-' );
+					// translators: %s: checkbox label.
+					$i18n_label = sprintf( __( 'Checkbox: %s', 'woocommerce' ), $item['aria_label'] ?? '' );
+					?>
+					<li
+						class="wc-block-interactivity-components-checkbox-list__item"
+						data-wc-key="<?php echo esc_attr( $item['id'] ); ?>"
+					>
+						<label
+							class="wc-block-interactivity-components-checkbox-list__label"
+							for="<?php echo esc_attr( $item['id'] ); ?>"
+						>
+							<span class="wc-block-interactivity-components-checkbox-list__input-wrapper">
+								<input
+									id="<?php echo esc_attr( $item['id'] ); ?>"
+									class="wc-block-interactivity-components-checkbox-list__input"
+									type="checkbox"
+									aria-invalid="false"
+									aria-label="<?php echo esc_attr( $i18n_label ); ?>"
+									data-wc-on--change--select-item="actions.selectCheckboxItem"
+									data-wc-on--change--parent-action="<?php echo esc_attr( $on_change ); ?>"
+									value="<?php echo esc_attr( $item['value'] ); ?>"
+									<?php checked( $item['checked'], 1 ); ?>
+								>
+								<svg class="wc-block-interactivity-components-checkbox-list__mark" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M9.25 1.19922L3.75 6.69922L1 3.94922" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+								</svg>
+							</span>
+							<span class="wc-block-interactivity-components-checkbox-list__text">
+								<?php // The label can be HTML, so we don't want to escape it. ?>
+								<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+								<?php echo $item['label']; ?>
+							</span>
+						</label>
+					</li>
+				<?php } ?>
+			</ul>
 		</div>
 		<?php
 		return ob_get_clean();
