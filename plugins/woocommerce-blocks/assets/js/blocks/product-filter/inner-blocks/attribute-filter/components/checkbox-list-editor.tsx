ca1329a7763b80fde03667778268aa861b3be950
Tung Du <dinhtungdu@gmail.com>
2024-08-05 20:35:20 +0700

    update: set some preview items checked for styling purpose

--- plugins/woocommerce-blocks/assets/js/blocks/product-filter/inner-blocks/attribute-filter/components/checkbox-list-editor.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filter/inner-blocks/attribute-filter/components/checkbox-list-editor.tsx
@@ -24,25 +24,31 @@ export const Preview = ( { items }: { items: string[] } ) => {
 								className=" wc-block-interactivity-components-checkbox-list__label"
 							>
 								<span className="wc-block-interactive-components-checkbox-list__input-wrapper">
-									<input
-										name={ `interactive-checkbox-${ index }` }
-										type="checkbox"
-										className="wc-block-interactivity-components-checkbox-list__input"
-									/>
-									<svg
-										className="wc-block-interactivity-components-checkbox-list__mark"
-										viewBox="0 0 10 8"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											d="M9.25 1.19922L3.75 6.69922L1 3.94922"
-											stroke="currentColor"
-											strokeWidth="1.5"
-											strokeLinecap="round"
-											strokeLinejoin="round"
+									<span className="wc-block-interactivity-components-checkbox-list__input-wrapper">
+										<input
+											name={ `interactive-checkbox-${ index }` }
+											type="checkbox"
+											className="wc-block-interactivity-components-checkbox-list__input"
+											// Harded coded some checked items for styling purpose.
+											defaultChecked={ [
+												1, 3, 4,
+											].includes( index ) }
 										/>
-									</svg>
+										<svg
+											className="wc-block-interactivity-components-checkbox-list__mark"
+											viewBox="0 0 10 8"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												d="M9.25 1.19922L3.75 6.69922L1 3.94922"
+												stroke="currentColor"
+												strokeWidth="1.5"
+												strokeLinecap="round"
+												strokeLinejoin="round"
+											/>
+										</svg>
+									</span>
 								</span>
 								<span className="wc-block-interactivity-components-checkbox-list__text">
 									{ item }
