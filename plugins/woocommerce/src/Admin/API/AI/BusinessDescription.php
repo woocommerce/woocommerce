ae6902181ab1f3b51d791aca7ef21bbc44649a17
Alba Rincón <alba.rincon@automattic.com>
2024-08-05 13:49:52 +0200

    CYS - Move the ai/business-description endpoint to woocommerce admin API

---
+++ plugins/woocommerce/src/Admin/API/AI/BusinessDescription.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Automattic\WooCommerce\Admin\API\AI;
+
+use WP_Error;
+use WP_REST_Request;
+use WP_REST_Response;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Store Title controller
+ *
+ * @internal
+ */
+class BusinessDescription {
+	/**
+	 * Endpoint namespace.
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'wc-admin';
+
+	/**
+	 * Route base.
+	 *
+	 * @var string
+	 */
+	protected $rest_base = 'ai';
+
+	/**
+	 * Register routes.
+	 */
+	public function register_routes() {
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/business-description',
+			array(
+				array(
+					'methods'             => \WP_REST_Server::CREATABLE,
+					'callback'            => array( $this, 'update_business_description' ),
+					'permission_callback' => array( Middleware::class, 'is_authorized' ),
+					'args'                => array(
+						'business_description' => array(
+							'description' => __( 'The business description for a given store.', 'woocommerce' ),
+							'type'        => 'string',
+						),
+					),
+				),
+				'schema' => array( $this, 'get_schema' ),
+			)
+		);
+	}
+
+	/**
+	 * Update the business description.
+	 *
+	 * @param WP_REST_Request $request Request object.
+	 * @return WP_REST_Response|WP_Error Response object.
+	 */
+	public function update_business_description( $request ) {
+
+		$business_description = $request->get_param( 'business_description' );
+
+		if ( ! $business_description ) {
+			return new WP_Error(
+				'invalid_business_description',
+				__( 'Invalid business description.', 'woocommerce' )
+			);
+		}
+
+		update_option( 'last_business_description_with_ai_content_generated', $business_description );
+
+		return rest_ensure_response(
+			array(
+				'ai_content_generated' => true,
+			)
+		);
+	}
+
+	/**
+	 * Get the Business Description response.
+	 *
+	 * @return array
+	 */
+	public function get_schema() {
+		return array(
+			'ai_content_generated' => true,
+		);
+	}
+}
