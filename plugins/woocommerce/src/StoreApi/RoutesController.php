7ab23e71e344651c666f573fb6b0b5e2845ec83b
Alba Rincón <alba.rincon@automattic.com>
2024-08-06 11:08:55 +0200

    CYS - Move the `ai/product` endpoint to woocommerce admin API

--- plugins/woocommerce/src/StoreApi/RoutesController.php
+++ plugins/woocommerce/src/StoreApi/RoutesController.php
@@ -68,9 +68,8 @@ class RoutesController {
 			],
 			// @todo Migrate internal AI routes to WooCommerce Core codebase.
 			'private' => [
-				Routes\V1\AI\Product::IDENTIFIER   => Routes\V1\AI\Product::class,
-				Routes\V1\AI\Products::IDENTIFIER  => Routes\V1\AI\Products::class,
-				Routes\V1\Patterns::IDENTIFIER     => Routes\V1\Patterns::class,
+				Routes\V1\AI\Products::IDENTIFIER => Routes\V1\AI\Products::class,
+				Routes\V1\Patterns::IDENTIFIER    => Routes\V1\Patterns::class,
 			],
 		];
 	}
