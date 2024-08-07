c1b6b5591c47ffdc7855962ac6dc5247bd12a74b
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-08-07 13:57:50 +0200

    Remove auto migration code

--- plugins/woocommerce-blocks/assets/js/blocks/migration-products-to-product-collection/constants.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/migration-products-to-product-collection/constants.ts
@@ -3,7 +3,6 @@
  */
 import type { UpgradeNoticeStatus, UpgradeNoticeStatuses } from './types';
 
-export const AUTO_REPLACE_PRODUCTS_WITH_PRODUCT_COLLECTION = false;
 export const MANUAL_REPLACE_PRODUCTS_WITH_PRODUCT_COLLECTION = true;
 export const HOURS_TO_DISPLAY_UPGRADE_NOTICE = 72;
 export const UPGRADE_NOTICE_DISPLAY_COUNT_THRESHOLD = 4;
