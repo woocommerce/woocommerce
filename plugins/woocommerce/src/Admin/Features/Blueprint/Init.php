2586445251955f7f98a613f6e2c70b131e40035f
Moon <moon.kyong@automattic.com>
2024-08-07 09:33:52 -0700

    blueprint - Add woo blueprint exporters and importers (#50057)
    * Add woo blueprint exporters and importers
    
    * Comment fix
    
    * Update plugins/woocommerce/src/Admin/Features/Blueprint/Exporters/ExportWCSettings.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Update plugins/woocommerce/src/Admin/Features/Blueprint/Exporters/ExportWCSettings.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Update plugins/woocommerce/src/Admin/Features/Blueprint/Exporters/ExportWCSettings.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Update plugins/woocommerce/src/Admin/Features/Blueprint/Exporters/ExportWCSettings.php
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    * Add a test for ImportSetWCTaxRates
    
    * Lint fixes
    
    * Lint fixes
    
    * Fix setWCShipping schema and data format
    
    * Add woocommerce_default_country option
    
    * Lint fixes
    
    * Fix typo -- it should be decode, not encode
    
    * Fix incorrect argument order
    
    * Fix typo
    
    * Export and import shipping method settings
    
    * Lint fixes
    
    * Lint fixes
    
    * Type functions -- base PR has changed
    
    ---------
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>

--- plugins/woocommerce/src/Admin/Features/Blueprint/Init.php
+++ plugins/woocommerce/src/Admin/Features/Blueprint/Init.php
@@ -4,7 +4,18 @@ declare( strict_types = 1 );
 
 namespace Automattic\WooCommerce\Admin\Features\Blueprint;
 
+use Automattic\WooCommerce\Admin\Features\Blueprint\Exporters\ExportWCCoreProfilerOptions;
+use Automattic\WooCommerce\Admin\Features\Blueprint\Exporters\ExportWCPaymentGateways;
+use Automattic\WooCommerce\Admin\Features\Blueprint\Exporters\ExportWCSettings;
+use Automattic\WooCommerce\Admin\Features\Blueprint\Exporters\ExportWCShipping;
+use Automattic\WooCommerce\Admin\Features\Blueprint\Exporters\ExportWCTaskOptions;
+use Automattic\WooCommerce\Admin\Features\Blueprint\Exporters\ExportWCTaxRates;
+use Automattic\WooCommerce\Admin\Features\Blueprint\Importers\ImportSetWCPaymentGateways;
+use Automattic\WooCommerce\Admin\Features\Blueprint\Importers\ImportSetWCShipping;
+use Automattic\WooCommerce\Admin\Features\Blueprint\Importers\ImportSetWCTaxRates;
 use Automattic\WooCommerce\Admin\PageController;
+use Automattic\WooCommerce\Blueprint\Exporters\StepExporter;
+use Automattic\WooCommerce\Blueprint\StepProcessor;
 
 /**
  * Class Init
@@ -18,6 +29,16 @@ class Init {
 	public function __construct() {
 		add_action( 'rest_api_init', array( $this, 'init_rest_api' ) );
 		add_filter( 'woocommerce_admin_shared_settings', array( $this, 'add_upload_nonce_to_settings' ) );
+
+		add_filter(
+			'wooblueprint_export_landingpage',
+			function () {
+				return 'admin.php?page=wc-admin';
+			}
+		);
+
+		add_filter( 'wooblueprint_exporters', array( $this, 'add_woo_exporters' ) );
+		add_filter( 'wooblueprint_importers', array( $this, 'add_woo_importers' ) );
 	}
 
 	/**
@@ -51,5 +72,44 @@ class Init {
 		}
 
 		return $settings;
+	}
+
+	/**
+	 * Add Woo Specific Exporters.
+	 *
+	 * @param StepExporter[] $exporters Array of step exporters.
+	 *
+	 * @return StepExporter[]
+	 */
+	public function add_woo_exporters( array $exporters ) {
+		return array_merge(
+			$exporters,
+			array(
+				new ExportWCCoreProfilerOptions(),
+				new ExportWCSettings(),
+				new ExportWCPaymentGateways(),
+				new ExportWCShipping(),
+				new ExportWCTaskOptions(),
+				new ExportWCTaxRates(),
+			)
+		);
+	}
+
+	/**
+	 * Add Woo Specific Importers.
+	 *
+	 * @param StepProcessor[] $importers Array of step processors.
+	 *
+	 * @return array
+	 */
+	public function add_woo_importers( array $importers ) {
+		return array_merge(
+			$importers,
+			array(
+				new ImportSetWCPaymentGateways(),
+				new ImportSetWCShipping(),
+				new ImportSetWCTaxRates(),
+			)
+		);
 	}
 }
