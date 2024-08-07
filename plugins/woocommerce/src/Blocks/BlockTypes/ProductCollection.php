54c45119f70849103e0e51364b3f6527865f3571
amesplant <95257231+amesplant-dmv@users.noreply.github.com>
2024-08-07 09:07:19 -0500

    Add aria-label for page numbers within pagination

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection.php
@@ -302,7 +302,22 @@ class ProductCollection extends AbstractBlock {
 			if ( in_array( $class_name, array( 'wp-block-query-pagination-next', 'wp-block-query-pagination-previous' ), true ) ) {
 				$processor->set_attribute( 'data-wc-watch', 'callbacks.prefetch' );
 				$processor->set_attribute( 'data-wc-on--mouseenter', 'actions.prefetchOnHover' );
+			} else {
+				$innerText = __( 'Page ', 'woocommerce' );
+				$processor->set_bookmark( 'page_number' );
+				while ( $processor->next_token() ) {
+					if ( '#text' === $processor->get_token_type() ) {
+						$page = trim( $processor->get_modifiable_text() );
+						if ( is_numeric( $page ) ) {
+							$innerText .= $page;
+							break;
+						}
+					}
+				}
+				$processor->seek( 'page_number' );
+				$processor->set_attribute( 'aria-label', $innerText );
 			}
+
 		}
 	}
 
