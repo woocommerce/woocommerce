a57b88d1e71ee82b451a0ef3c180152689ef3f4e
moon <moon.kyong@automattic.com>
2024-08-06 19:58:13 -0700

    Add back removed functions

--- packages/php/blueprint/src/Util.php
+++ packages/php/blueprint/src/Util.php
@@ -27,6 +27,49 @@ class Util {
 	}
 
 	/**
+	 * Convert a string from snake_case to camelCase.
+	 *
+	 * @param string $string The string to be converted.
+	 *
+	 * @return string
+	 */
+	public static function snake_to_camel( $string ) {
+		// Split the string by underscores
+		$words = explode( '_', $string );
+
+		// Capitalize the first letter of each word
+		$words = array_map( 'ucfirst', $words );
+
+		// Join the words back together
+		return implode( '', $words );
+	}
+
+	public static function array_flatten($array) {
+		return new RecursiveIteratorIterator(new RecursiveArrayIterator($array));
+	}
+
+	/**
+	 * Convert a string from camelCase to snake_case.
+	 * 
+	 * @param string $input The string to be converted.
+	 *
+	 * @return string
+	 */
+	public static function camel_to_snake( $input ) {
+		// Replace all uppercase letters with an underscore followed by the lowercase version of the letter
+		$pattern     = '/([a-z])([A-Z])/';
+		$replacement = '$1_$2';
+		$snake       = preg_replace( $pattern, $replacement, $input );
+
+		// Replace spaces with underscores
+		$snake = str_replace( ' ', '_', $snake );
+
+		// Convert the entire string to lowercase
+		return strtolower( $snake );
+	}
+
+
+	/**
 	 * Index an array using a callback function.
 	 *
 	 * @param array    $array The array to be indexed.
