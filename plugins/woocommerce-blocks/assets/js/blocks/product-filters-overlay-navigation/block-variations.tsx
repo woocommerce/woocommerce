3abe5fb7351a7d4b12a734dee8a8cce029668517
Alexandre Lara <allexandrelara@gmail.com>
2024-08-07 10:53:09 -0400

    Remove the `isActive` property from the block variations

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters-overlay-navigation/block-variations.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters-overlay-navigation/block-variations.tsx
@@ -15,14 +15,4 @@ const variations: BlockVariation[] = [
 	},
 ];
 
-/**
- * Add `isActive` function to all Product Filters Overlay Navigation block variations.
- * `isActive` function is used to find a variation match from a created
- *  Block by providing its attributes.
- */
-variations.forEach( ( variation ) => {
-	// @ts-expect-error: `isActive` is currently typed wrong in `@wordpress/blocks`.
-	variation.isActive = [ 'triggerType' ];
-} );
-
 export const blockVariations = variations;
