232a187b23dd963e0dc4c738821c683562c3585b
Alba Rincón <alba.rincon@automattic.com>
2024-08-09 14:30:07 +0200

    Fix lint

--- plugins/woocommerce/src/StoreApi/RoutesController.php
+++ plugins/woocommerce/src/StoreApi/RoutesController.php
@@ -68,10 +68,10 @@ class RoutesController {
 			],
 			// @todo Migrate internal AI routes to WooCommerce Core codebase.
 			'private' => [
-				Routes\V1\AI\Patterns::IDENTIFIER  => Routes\V1\AI\Patterns::class,
-				Routes\V1\AI\Product::IDENTIFIER   => Routes\V1\AI\Product::class,
-				Routes\V1\AI\Products::IDENTIFIER  => Routes\V1\AI\Products::class,
-				Routes\V1\Patterns::IDENTIFIER     => Routes\V1\Patterns::class,
+				Routes\V1\AI\Patterns::IDENTIFIER => Routes\V1\AI\Patterns::class,
+				Routes\V1\AI\Product::IDENTIFIER  => Routes\V1\AI\Product::class,
+				Routes\V1\AI\Products::IDENTIFIER => Routes\V1\AI\Products::class,
+				Routes\V1\Patterns::IDENTIFIER    => Routes\V1\Patterns::class,
 			],
 		];
 	}
