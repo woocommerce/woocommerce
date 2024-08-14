9552dd0f3fe2d8d8b89291014c1b99e6a579627b
Alba Rincón <alba.rincon@automattic.com>
2024-08-14 16:05:25 +0200

    Not use Atomic class

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductMeta.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductMeta.php
@@ -7,11 +7,49 @@ namespace Automattic\WooCommerce\Blocks\BlockTypes;
 /**
  * ProductMeta class.
  */
-class ProductMeta extends AtomicBlock {
+class ProductMeta extends AbstractBlock {
 	/**
 	 * Block name.
 	 *
 	 * @var string
 	 */
 	protected $block_name = 'product-meta';
+
+	/**
+	 * Get the editor script data for this block type.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 * @return null
+	 */
+	protected function get_block_type_editor_script( $key = null ) {
+		return null;
+	}
+
+	/**
+	 * Get the editor style handle for this block type.
+	 *
+	 * @return null
+	 */
+	protected function get_block_type_editor_style() {
+		return null;
+	}
+
+	/**
+	 * Get the frontend script handle for this block type.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 * @return null
+	 */
+	protected function get_block_type_script( $key = null ) {
+		return null;
+	}
+
+	/**
+	 * Get the frontend style handle for this block type.
+	 *
+	 * @return null
+	 */
+	protected function get_block_type_style() {
+		return null;
+	}
 }
