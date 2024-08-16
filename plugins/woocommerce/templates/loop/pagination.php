b578808c79976ef1daad877d4542c130dce08f92
Ames Plant <ames.plant@10up.com>
2024-08-16 10:06:19 -0500

    Update plugins/woocommerce/templates/loop/pagination.php
    Co-authored-by: Karol Manijak <20098064+kmanijak@users.noreply.github.com>

--- plugins/woocommerce/templates/loop/pagination.php
+++ plugins/woocommerce/templates/loop/pagination.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 9.4.0
+ * @version 9.3.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
