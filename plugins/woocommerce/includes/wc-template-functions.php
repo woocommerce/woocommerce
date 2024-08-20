fb2a21bdcc3ab55efdbdabeb6efd82cd4e7d04bd
Naman Malhotra <naman03malhotra@gmail.com>
2024-08-20 23:28:39 +0700

    Update plugins/woocommerce/includes/wc-template-functions.php

--- plugins/woocommerce/includes/wc-template-functions.php
+++ plugins/woocommerce/includes/wc-template-functions.php
@@ -246,7 +246,7 @@ function wc_set_loop_prop( $prop, $value = '' ) {
  * Set the current visbility for a product in the woocommerce_loop global.
  *
  * @since 4.4.0
- * @param int  $product_id Product it to cache visbiility for.
+ * @param int  $product_id Product it to cache visibility for.
  * @param bool $value The product visibility value to cache.
  */
 function wc_set_loop_product_visibility( $product_id, $value ) {
