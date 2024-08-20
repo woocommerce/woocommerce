e8cc59c9a693ce1efa731654dfcaa705e8cf51f7
Tomek Wytrębowicz <tomalecpub@gmail.com>
2024-08-20 17:36:19 +0200

    Remove constructor override

--- plugins/woocommerce/src/Admin/API/Reports/Query.php
+++ plugins/woocommerce/src/Admin/API/Reports/Query.php
@@ -13,18 +13,6 @@ defined( 'ABSPATH' ) || exit;
  * @deprecated x.x.x Query class is deprecated, please use GenericQuery or \WC_Object_Query instead.
  */
 abstract class Query extends \WC_Object_Query {
-
-	/**
-	 * Create a new query.
-	 *
-	 * @deprecated x.x.x Query class is deprecated, please use GenericQuery or \WC_Object_Query instead.
-	 *
-	 * @param array $args Criteria to query on in a format similar to WP_Query.
-	 */
-	public function __construct( $args = array() ) {
-		parent::__construct( $args );
-	}
-
 	/**
 	 * Get report data matching the current query vars.
 	 *
