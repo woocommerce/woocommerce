47730bc02b84e349c1be455516b12933ff964829
Fernando Marichal <fernando.marichal@automattic.com>
2024-08-21 15:06:09 -0300

    Refactor rating blocks

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/rating/block.tsx
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/rating/block.tsx
@@ -1,38 +1,22 @@
 /**
  * External dependencies
  */
-import clsx from 'clsx';
 import {
 	useInnerBlockLayoutContext,
 	useProductDataContext,
 } from '@woocommerce/shared-context';
 import { useStyleProps } from '@woocommerce/base-hooks';
 import { withProductDataContext } from '@woocommerce/shared-hocs';
-import { isNumber, ProductResponseItem } from '@woocommerce/types';
-import ProductRating from '@woocommerce/editor-components/product-rating';
+import {
+	ProductRating,
+	getAverageRating,
+	getRatingCount,
+} from '@woocommerce/editor-components/product-rating';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
-
-const getAverageRating = (
-	product: Omit< ProductResponseItem, 'average_rating' > & {
-		average_rating: string;
-	}
-) => {
-	const rating = parseFloat( product.average_rating );
-
-	return Number.isFinite( rating ) && rating > 0 ? rating : 0;
-};
-
-const getRatingCount = ( product: ProductResponseItem ) => {
-	const count = isNumber( product.review_count )
-		? product.review_count
-		: parseInt( product.review_count, 10 );
-
-	return Number.isFinite( count ) && count > 0 ? count : 0;
-};
 
 type ProductRatingProps = {
 	className?: string;
@@ -46,39 +30,33 @@ type ProductRatingProps = {
 
 export const Block = ( props: ProductRatingProps ): JSX.Element | undefined => {
 	const {
-		textAlign,
+		textAlign = '',
 		isDescendentOfSingleProductBlock,
 		shouldDisplayMockedReviewsWhenProductHasNoReviews,
 	} = props;
-	const { className, style } = useStyleProps( props );
+	const styleProps = useStyleProps( props );
 	const { parentClassName } = useInnerBlockLayoutContext();
 	const { product } = useProductDataContext();
 	const rating = getAverageRating( product );
 	const reviews = getRatingCount( product );
 
-	const ratingClassName = clsx(
-		className,
-		'wc-block-components-product-rating',
-		{
-			[ `${ parentClassName }__product-rating` ]: parentClassName,
-			[ `has-text-align-${ textAlign }` ]: textAlign,
-		}
-	);
+	const className = 'wc-block-components-product-rating';
 
 	if ( reviews || shouldDisplayMockedReviewsWhenProductHasNoReviews ) {
 		return (
 			<ProductRating
-				className={ ratingClassName }
+				className={ className }
 				isDescendentOfSingleProductBlock={
 					isDescendentOfSingleProductBlock
 				}
 				shouldDisplayMockedReviewsWhenProductHasNoReviews={
 					shouldDisplayMockedReviewsWhenProductHasNoReviews
 				}
-				style={ style }
+				styleProps={ styleProps }
 				parentClassName={ parentClassName }
 				reviews={ reviews }
 				rating={ rating }
+				textAlign={ textAlign }
 			/>
 		);
 	}
