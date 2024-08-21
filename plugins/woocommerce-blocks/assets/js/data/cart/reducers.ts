112fca58a6b33882b3c6414d7eca6177c9704e33
Thomas Roberts <thomas.roberts@automattic.com>
2024-08-21 10:34:10 +0100

    Fix lint error

--- plugins/woocommerce-blocks/assets/js/data/cart/reducers.ts
+++ plugins/woocommerce-blocks/assets/js/data/cart/reducers.ts
@@ -59,7 +59,9 @@ const reducer: Reducer< CartState > = ( state = defaultCartState, action ) => {
 				...state,
 				metaData: {
 					...state.metaData,
-					isCustomerDataDirty: state.metaData.isCustomerDataDirty || billingAddressChanged,
+					isCustomerDataDirty:
+						state.metaData.isCustomerDataDirty ||
+						billingAddressChanged,
 				},
 				cartData: {
 					...state.cartData,
