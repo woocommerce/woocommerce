d31aae633afd91582afccdc48418e42968c6db14
Tomek Wytrębowicz <tomalecpub@gmail.com>
2024-08-21 20:12:13 +0200

    Move `ReportError` to `@woocommerce/components` as `AnalyticsError` (#50108)
    Co-authored-by: Eason <eason.su.tw@gmail.com>

--- plugins/woocommerce-admin/client/analytics/components/index.js
+++ plugins/woocommerce-admin/client/analytics/components/index.js
@@ -1,4 +1,3 @@
 export { default as ReportChart } from './report-chart';
-export { default as ReportError } from './report-error';
 export { default as ReportSummary } from './report-summary';
 export { default as ReportTable } from './report-table';
