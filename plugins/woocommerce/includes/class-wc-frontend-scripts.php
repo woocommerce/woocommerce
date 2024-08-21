f83096d7f5fc7169ff31db0ab44960d67097ea69
Naman Malhotra <naman03malhotra@gmail.com>
2024-08-21 22:56:52 +0300

    Fix Select 2 conflict with ACF

--- plugins/woocommerce/includes/class-wc-frontend-scripts.php
+++ plugins/woocommerce/includes/class-wc-frontend-scripts.php
@@ -241,7 +241,7 @@ class WC_Frontend_Scripts {
 			'selectWoo'                  => array(
 				'src'     => self::get_asset_url( 'assets/js/selectWoo/selectWoo.full' . $suffix . '.js' ),
 				'deps'    => array( 'jquery' ),
-				'version' => '1.0.9-wc.' . $version,
+				'version' => '4.0.3-wc.' . $version,
 			),
 			'wc-address-i18n'            => array(
 				'src'     => self::get_asset_url( 'assets/js/frontend/address-i18n' . $suffix . '.js' ),
