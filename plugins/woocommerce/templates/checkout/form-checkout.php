e424a437c14f7f18837e979e56ef9a2b515614de
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-08-23 12:21:31 -0300

    Add aria-label to checkout form

--- plugins/woocommerce/templates/checkout/form-checkout.php
+++ plugins/woocommerce/templates/checkout/form-checkout.php
@@ -29,7 +29,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 ?>
 
-<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data" aria-label="<?php echo esc_attr__( 'Checkout', 'woocommerce' ); ?>">
 
 	<?php if ( $checkout->get_checkout_fields() ) : ?>
 
