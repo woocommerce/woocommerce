e405536c8c02b1d199f8c455f4695af62522b6cb
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-08-23 09:41:18 +0200

    Migrate Product Image to block.json

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/image/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/image/edit.tsx
@@ -27,11 +27,8 @@ import {
  */
 import Block from './block';
 import withProductSelector from '../shared/with-product-selector';
-import {
-	BLOCK_TITLE as label,
-	BLOCK_ICON as icon,
-	BLOCK_DESCRIPTION as description,
-} from './constants';
+import { BLOCK_ICON as icon } from './constants';
+import { title, description } from './block.json';
 import { BlockAttributes, ImageSizing } from './types';
 import { ImageSizeSettings } from './image-size-settings';
 
@@ -172,4 +169,4 @@ const Edit = ( {
 	);
 };
 
-export default withProductSelector( { icon, label, description } )( Edit );
+export default withProductSelector( { icon, title, description } )( Edit );
---
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/image/index.tsx
@@ -0,0 +1,23 @@
+/**
+ * External dependencies
+ */
+import { registerBlockType } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies
+ */
+import edit from './edit';
+import { BLOCK_ICON as icon } from './constants';
+import { supports } from './supports';
+import sharedConfig from '../shared/config';
+import metadata from './block.json';
+
+registerBlockType( metadata, {
+	...sharedConfig,
+	icon,
+	attributes: {
+		...metadata.attributes,
+	},
+	supports,
+	edit,
+} );
