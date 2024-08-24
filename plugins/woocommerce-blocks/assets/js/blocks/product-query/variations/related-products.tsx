e085a3597403529d55d261a0f1e984ed0019e560
Alexandre Lara <allexandrelara@gmail.com>
2024-08-23 22:53:02 -0300

    Remove unnecessary imports

--- plugins/woocommerce-blocks/assets/js/blocks/product-query/variations/related-products.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-query/variations/related-products.tsx
@@ -2,18 +2,15 @@
  * External dependencies
  */
 import {
-	BlockAttributes,
-	InnerBlockTemplate,
 	registerBlockVariation,
 } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
-import { registerBlockSingleProductTemplate } from '@woocommerce/atomic-utils';
 
 /**
  * Internal dependencies
  */
 import { QUERY_LOOP_ID } from '../constants';
-import { RELATED_PRODUCTS_VARIATION_NAME, RelatedProductsControlsBlockVariationSettings } from './related-products-settings';
+import { RelatedProductsControlsBlockVariationSettings } from './related-products-settings';
 
 
 registerBlockVariation(
