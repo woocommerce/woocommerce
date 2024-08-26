22d040ebb2209cfa6092574763f715ea7aacd44c
Ivan Stojadinov <ivan.stojadinov@automattic.com>
2024-08-26 16:23:15 +0200

    Include products/block-editor into default-pressable set of tests

--- plugins/woocommerce/tests/e2e-pw/envs/default-pressable/playwright.config.js
+++ plugins/woocommerce/tests/e2e-pw/envs/default-pressable/playwright.config.js
@@ -7,7 +7,10 @@ config = {
 		{
 			name: 'default pressable',
 			use: { ...devices[ 'Desktop Chrome' ] },
-			testMatch: '**basic.spec.js',
+			testMatch: [
+				'**/basic.spec.js',
+				'**/merchant/products/block-editor/**/*.spec.js',
+			],
 		},
 	],
 };
