379c8f3af7d4e44384d9ef96382eb80eb436a2da
Alba Rincón <alba.rincon@automattic.com>
2024-08-27 10:15:34 +0200

    Not show reviews in the frontend when they are disabled at store or product level

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductRating.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductRating.php
@@ -114,7 +114,9 @@ class ProductRating extends AbstractBlock {
 		$post_id = isset( $block->context['postId'] ) ? $block->context['postId'] : '';
 		$product = wc_get_product( $post_id );
 
-		if ( $product && $product->get_review_count() > 0 ) {
+		if ( $product && $product->get_review_count() > 0
+			&& $product->get_reviews_allowed()
+			&& wc_reviews_enabled() ) {
 			$product_reviews_count                    = $product->get_review_count();
 			$product_rating                           = $product->get_average_rating();
 			$parsed_attributes                        = $this->parse_attributes( $attributes );
