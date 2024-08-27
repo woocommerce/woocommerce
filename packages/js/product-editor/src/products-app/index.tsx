c1a61ad0945f061ecea74861111e09b62710c2af
Lourens Schep <lourensschep@gmail.com>
2024-08-27 10:55:52 +0200

    Add basic products app file

---
+++ packages/js/product-editor/src/products-app/index.tsx
@@ -0,0 +1,33 @@
+/**
+ * External dependencies
+ */
+import { createElement } from '@wordpress/element';
+import { privateApis as routerPrivateApis } from '@wordpress/router';
+import {
+	UnsavedChangesWarning,
+	// @ts-expect-error No types for this exist yet.
+	privateApis as editorPrivateApis,
+} from '@wordpress/editor';
+
+/**
+ * Internal dependencies
+ */
+import { unlock } from '../lock-unlock';
+
+const { RouterProvider } = unlock( routerPrivateApis );
+const { GlobalStylesProvider } = unlock( editorPrivateApis );
+
+function ProductsLayout() {
+	return <div>Initial Products Layout</div>;
+}
+
+export default function ProductsApp() {
+	return (
+		<GlobalStylesProvider>
+			<UnsavedChangesWarning />
+			<RouterProvider>
+				<ProductsLayout />
+			</RouterProvider>
+		</GlobalStylesProvider>
+	);
+}
