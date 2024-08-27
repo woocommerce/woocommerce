33c20ba3f088af03f8e844eba4b07ec006204213
Lourens Schep <lourensschep@gmail.com>
2024-08-27 15:16:48 +0200

    Add products data views list

--- packages/js/product-editor/src/products-app/index.tsx
+++ packages/js/product-editor/src/products-app/index.tsx
@@ -3,6 +3,7 @@
  */
 import { createElement } from '@wordpress/element';
 import { privateApis as routerPrivateApis } from '@wordpress/router';
+import useInitEditedEntityFromURL from '@wordpress/edit-site/build-module/components/sync-state-with-url/use-init-edited-entity-from-url';
 import {
 	UnsavedChangesWarning,
 	// @ts-expect-error No types for this exist yet.
@@ -13,12 +14,17 @@ import {
  * Internal dependencies
  */
 import { unlock } from '../lock-unlock';
+import useLayoutAreas from './router';
+import { Layout } from './layout';
 
 const { RouterProvider } = unlock( routerPrivateApis );
 const { GlobalStylesProvider } = unlock( editorPrivateApis );
 
 function ProductsLayout() {
-	return <div>Initial Products Layout</div>;
+	// This ensures the edited entity id and type are initialized properly.
+	useInitEditedEntityFromURL();
+	const route = useLayoutAreas();
+	return <Layout route={ route } />;
 }
 
 export function ProductsApp() {
