737d1e1608074b0dc970c04422e935c2414d1684
Lourens Schep <lourensschep@gmail.com>
2024-08-27 13:55:39 +0200

    Address final lint error

--- plugins/woocommerce/src/Admin/Features/ProductDataViews/Init.php
+++ plugins/woocommerce/src/Admin/Features/ProductDataViews/Init.php
@@ -126,11 +126,13 @@ class Init {
 		$suffix  = Constants::is_true( 'SCRIPT_DEBUG' ) ? '' : '.min';
 		$version = Constants::get_constant( 'WC_VERSION' );
 		if ( function_exists( 'gutenberg_url' ) ) {
+			// phpcs:disable WordPress.WP.EnqueuedResourceParameters.MissingVersion
 			wp_register_style(
 				'wp-gutenberg-posts-dashboard',
 				gutenberg_url( 'build/edit-site/posts.css', __FILE__ ),
-				array()
+				array( 'wp-components' ),
 			);
+			// phpcs:enable WordPress.WP.EnqueuedResourceParameters.MissingVersion
 			wp_enqueue_style( 'wp-gutenberg-posts-dashboard' );
 		}
 		WCAdminAssets::get_instance();
