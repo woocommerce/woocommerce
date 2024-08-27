2ebbe685556920c86d21284711cae0f60713d5f6
Gabriel Manussakis <9420947+Manussakis@users.noreply.github.com>
2024-08-27 11:23:04 -0300

    [Accessibility] Fix typos in inline documentation in woocommerce client, includes, and lib folders (#50739)
    * Fix typo in inline doc in woocommerce client, includes, and lib folders
    
    * Fix typo in comment
    
    * Uppercase the Brazilian word
    
    ---------
    
    Co-authored-by: Darin Kotter <darin.kotter@gmail.com>
    Co-authored-by: Néstor Soriano <konamiman@konamiman.com>

--- plugins/woocommerce/includes/abstracts/abstract-wc-order.php
+++ plugins/woocommerce/includes/abstracts/abstract-wc-order.php
@@ -1413,7 +1413,7 @@ abstract class WC_Abstract_Order extends WC_Abstract_Legacy_Order {
 	private function get_temporary_coupon( WC_Order_Item_Coupon $coupon_item ): WC_Coupon {
 		$coupon_object = new WC_Coupon();
 
-		// Since WooCommerce 8.7 a succint 'coupon_info' line item meta entry is created
+		// Since WooCommerce 8.7 a succinct 'coupon_info' line item meta entry is created
 		// whenever a coupon is applied to an order. Previously a more verbose 'coupon_data' was created.
 
 		$coupon_info = $coupon_item->get_meta( 'coupon_info', true );
