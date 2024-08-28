3f6d91062ba5fea3bea34f125686a0a4feaa4c06
Lourens Schep <lourensschep@gmail.com>
2024-08-28 11:15:24 +0200

    Remove dependency on edit-site package

--- packages/js/product-editor/src/products-app/index.tsx
+++ packages/js/product-editor/src/products-app/index.tsx
@@ -3,7 +3,6 @@
  */
 import { createElement } from '@wordpress/element';
 import { privateApis as routerPrivateApis } from '@wordpress/router';
-import useInitEditedEntityFromURL from '@wordpress/edit-site/build-module/components/sync-state-with-url/use-init-edited-entity-from-url';
 import {
 	UnsavedChangesWarning,
 	// @ts-expect-error No types for this exist yet.
@@ -22,7 +21,6 @@ const { GlobalStylesProvider } = unlock( editorPrivateApis );
 
 function ProductsLayout() {
 	// This ensures the edited entity id and type are initialized properly.
-	useInitEditedEntityFromURL();
 	const route = useLayoutAreas();
 	return <Layout route={ route } />;
 }
