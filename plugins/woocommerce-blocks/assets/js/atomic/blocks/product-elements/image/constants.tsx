81ebd85f9e7f834d0f1c4d0254388c5402121d8c
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-08-28 19:56:27 +0200

    Migrate Product Image to `block.json` (#50903)
    * Migrate Product Image to block.json
    
    * Remove unnecessary property
    
    * Add changelog
    
    * Update block references
    
    * Remove duplicated attributes
    
    * Update block references and doc manifest

--- plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/image/constants.tsx
+++ plugins/woocommerce-blocks/assets/js/atomic/blocks/product-elements/image/constants.tsx
@@ -1,14 +1,8 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { image, Icon } from '@wordpress/icons';
 
-export const BLOCK_TITLE: string = __( 'Product Image', 'woocommerce' );
 export const BLOCK_ICON: JSX.Element = (
 	<Icon icon={ image } className="wc-block-editor-components-block-icon" />
-);
-export const BLOCK_DESCRIPTION: string = __(
-	'Display the main product image.',
-	'woocommerce'
 );
