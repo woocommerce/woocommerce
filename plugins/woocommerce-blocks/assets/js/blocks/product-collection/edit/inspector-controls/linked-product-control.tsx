7c77aa1ed76c2122241415a62c7d78a411894b5b
Manish Menaria <the.manish.menaria@gmail.com>
2024-08-28 17:44:16 +0530

    Convert jsxProductButton to ProductButton component

--- plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-controls/linked-product-control.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-collection/edit/inspector-controls/linked-product-control.tsx
@@ -6,6 +6,7 @@ import ProductControl from '@woocommerce/editor-components/product-control';
 import { SelectedOption } from '@woocommerce/block-hocs';
 import { useState, useMemo } from '@wordpress/element';
 import { WooCommerceBlockLocation } from '@woocommerce/blocks/product-template/utils';
+import { ProductResponseItem } from '@woocommerce/types';
 import {
 	PanelBody,
 	PanelRow,
@@ -27,6 +28,43 @@ import {
 	ProductCollectionQuery,
 	ProductCollectionSetAttributes,
 } from '../../types';
+
+const ProductButton: React.FC< {
+	isOpen: boolean;
+	onToggle: () => void;
+	product: ProductResponseItem | null;
+	isLoading: boolean;
+} > = ( { isOpen, onToggle, product, isLoading } ) => {
+	if ( isLoading && ! product ) {
+		return <Spinner />;
+	}
+
+	return (
+		<Button
+			className="wc-block-product-collection-linked-product-control__button"
+			onClick={ onToggle }
+			aria-expanded={ isOpen }
+			disabled={ isLoading }
+		>
+			<Flex direction="row" expanded justify="flex-start">
+				<FlexItem className="wc-block-product-collection-linked-product-control__image-container">
+					<img
+						src={ product?.images?.[ 0 ]?.src }
+						alt={ product?.name }
+					/>
+				</FlexItem>
+				<Flex direction="column" align="flex-start" gap={ 0 }>
+					<FlexItem>
+						<Text color="inherit">{ product?.name }</Text>
+					</FlexItem>
+					<FlexItem>
+						<Text color="inherit">{ product?.sku }</Text>
+					</FlexItem>
+				</Flex>
+			</Flex>
+		</Button>
+	);
+};
 
 const LinkedProductControl = ( {
 	query,
@@ -54,44 +92,6 @@ const LinkedProductControl = ( {
 			isProductContextSelected
 		);
 	}, [ location.type, query?.productReference, usesReference ] );
-
-	const jsxProductButton = ( {
-		isOpen,
-		onToggle,
-	}: {
-		isOpen: boolean;
-		onToggle: () => void;
-	} ) => {
-		if ( isLoading && ! product ) {
-			return <Spinner />;
-		}
-
-		return (
-			<Button
-				className="wc-block-product-collection-linked-product-control__button"
-				onClick={ onToggle }
-				aria-expanded={ isOpen }
-				disabled={ isLoading }
-			>
-				<Flex direction="row" expanded justify="flex-start">
-					<FlexItem className="wc-block-product-collection-linked-product-control__image-container">
-						<img
-							src={ product?.images?.[ 0 ]?.src }
-							alt={ product?.name }
-						/>
-					</FlexItem>
-					<Flex direction="column" align="flex-start" gap={ 0 }>
-						<FlexItem>
-							<Text color="inherit">{ product?.name }</Text>
-						</FlexItem>
-						<FlexItem>
-							<Text color="inherit">{ product?.sku }</Text>
-						</FlexItem>
-					</Flex>
-				</Flex>
-			</Button>
-		);
-	};
 
 	const jsxPopoverContent = (
 		<ProductControl
@@ -123,9 +123,14 @@ const LinkedProductControl = ( {
 					className="wc-block-product-collection-linked-product-control"
 					contentClassName="wc-block-product-collection-linked-product__popover-content"
 					popoverProps={ { placement: 'left-start' } }
-					renderToggle={ ( { isOpen, onToggle } ) =>
-						jsxProductButton( { isOpen, onToggle } )
-					}
+					renderToggle={ ( { isOpen, onToggle } ) => (
+						<ProductButton
+							isOpen={ isOpen }
+							onToggle={ onToggle }
+							product={ product }
+							isLoading={ isLoading }
+						/>
+					) }
 					renderContent={ () => jsxPopoverContent }
 					open={ isDropdownOpen }
 					onToggle={ () => setIsDropdownOpen( ! isDropdownOpen ) }
