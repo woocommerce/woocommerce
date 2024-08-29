cd638721f9e133b2cf2f15ebb22157441a14127b
Tung Du <dinhtungdu@gmail.com>
2024-08-29 22:23:40 +0700

    [Experimental] Update folder structure for new product filter blocks (#51029)

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/stock-filter/frontend.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/stock-filter/frontend.ts
@@ -9,7 +9,7 @@ import { CheckboxListContext } from '@woocommerce/interactivity-components/check
 /**
  * Internal dependencies
  */
-import { navigate } from '../../frontend';
+import { navigate } from '../product-filter/frontend';
 
 const getUrl = ( activeFilters: string ) => {
 	const url = new URL( window.location.href );
