3e98e0540304989d3b08bfc6b86746625186ea85
Alex Florisca <alex.florisca@automattic.com>
2024-09-02 15:09:46 +0100

    Render from content instead of edit prop and adjust css

--- plugins/woocommerce-blocks/assets/js/blocks/cart-checkout-shared/payment-methods/express-payment-methods.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/cart-checkout-shared/payment-methods/express-payment-methods.tsx
@@ -142,20 +142,6 @@ const ExpressPaymentMethods = () => {
 		[ __internalSetExpressPaymentError, onExpressPaymentError ]
 	);
 
-	// In the editor, we apply styles to the button containers to show the changes of the height and border-radius controls,
-	// which would be passed to the payment APIs on the front-end
-	const stylesForButtonContainers = isEditor
-		? {
-				height: `${ showButtonStyles ? buttonHeight : '48' }px`,
-				borderRadius: `${
-					showButtonStyles ? buttonBorderRadius : '4'
-				}px`,
-				pointerEvents: 'none',
-				userSelect: 'none',
-				ariaDisabled: true,
-		  }
-		: {};
-
 	/**
 	 * @todo Find a way to Memoize Express Payment Method Content
 	 *
@@ -167,15 +153,9 @@ const ExpressPaymentMethods = () => {
 	const content =
 		entries.length > 0 ? (
 			entries.map( ( [ id, paymentMethod ] ) => {
-				const expressPaymentMethod = isEditor
-					? paymentMethod.edit
-					: paymentMethod.content;
+				const expressPaymentMethod = paymentMethod.content;
 				return isValidElement( expressPaymentMethod ) ? (
-					<li
-						key={ id }
-						id={ `express-payment-method-${ id }` }
-						style={ stylesForButtonContainers }
-					>
+					<li key={ id } id={ `express-payment-method-${ id }` }>
 						{ cloneElement( expressPaymentMethod, {
 							...paymentMethodInterface,
 							onClick: onExpressPaymentClick( id ),
