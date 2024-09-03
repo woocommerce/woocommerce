dd1fd45d445490bba4f0003a41ae6615a6a7b58d
roykho <roykho77@gmail.com>
2024-09-03 13:40:37 -0700

    Fix no products component prematurely being rendered

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/attribute-filter/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/attribute-filter/edit.tsx
@@ -27,6 +27,7 @@ import { attributeOptionsPreview } from './constants';
 import './style.scss';
 import { EditProps, isAttributeCounts } from './types';
 import { getAttributeFromId } from './utils';
+import './editor.scss';
 
 const ATTRIBUTES = getSetting< AttributeSetting[] >( 'attributes', [] );
 
@@ -49,22 +50,26 @@ const Edit = ( props: EditProps ) => {
 		AttributeTerm[]
 	>( [] );
 
-	const { results: attributeTerms } = useCollection< AttributeTerm >( {
-		namespace: '/wc/store/v1',
-		resourceName: 'products/attributes/terms',
-		resourceValues: [ attributeObject?.id || 0 ],
-		shouldSelect: !! attributeObject?.id,
-		query: { orderby: 'menu_order', hide_empty: hideEmpty },
-	} );
+	const { results: attributeTerms, isLoading: isTermsLoading } =
+		useCollection< AttributeTerm >( {
+			namespace: '/wc/store/v1',
+			resourceName: 'products/attributes/terms',
+			resourceValues: [ attributeObject?.id || 0 ],
+			shouldSelect: !! attributeObject?.id,
+			query: { orderby: 'menu_order', hide_empty: hideEmpty },
+		} );
 
-	const { results: filteredCounts } = useCollectionData( {
-		queryAttribute: {
-			taxonomy: attributeObject?.taxonomy || '',
-			queryType,
-		},
-		queryState: {},
-		isEditor: true,
-	} );
+	const { results: filteredCounts, isLoading: isCountsLoading } =
+		useCollectionData( {
+			queryAttribute: {
+				taxonomy: attributeObject?.taxonomy || '',
+				queryType,
+			},
+			queryState: {},
+			isEditor: true,
+		} );
+
+	const isLoading = isTermsLoading || isCountsLoading;
 
 	useEffect( () => {
 		const termIdHasProducts =
@@ -144,7 +149,31 @@ const Edit = ( props: EditProps ) => {
 			</Wrapper>
 		);
 
-	if ( attributeOptions.length === 0 )
+	if ( isLoading )
+		return (
+			<Wrapper>
+				<ul className="is-loading wp-block-woocommerce-product-filter-attribute__loading">
+					{ [ ...Array( 5 ) ].map( ( x, i ) => {
+						return (
+							<li
+								key={ i }
+								style={ {
+									/* stylelint-disable */
+									width:
+										Math.floor(
+											Math.random() * ( 100 - 25 )
+										) + '%',
+								} }
+							>
+								&nbsp;
+							</li>
+						);
+					} ) }
+				</ul>
+			</Wrapper>
+		);
+
+	if ( attributeTerms.length === 0 )
 		return (
 			<Wrapper>
 				<Notice status="warning" isDismissible={ false }>
