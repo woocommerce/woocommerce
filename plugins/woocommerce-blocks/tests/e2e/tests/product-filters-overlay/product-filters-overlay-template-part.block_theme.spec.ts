8e182980caa54233a54917d3b96224c17bcf615b
Alexandre Lara <allexandrelara@gmail.com>
2024-09-04 17:47:18 -0300

    Add e2e test to Product Filters Overlay template part

--- plugins/woocommerce-blocks/tests/e2e/tests/product-filters-overlay/product-filters-overlay-template-part.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/product-filters-overlay/product-filters-overlay-template-part.block_theme.spec.ts
@@ -40,4 +40,90 @@ test.describe( 'Filters Overlay Template Part', () => {
 
 		await expect( productFiltersTemplatePart ).toBeVisible();
 	} );
+
+	test.describe( 'frontend', () => {
+		test.beforeEach( async ( { admin } ) => {
+			await admin.visitSiteEditor( {
+				postId: `woocommerce/woocommerce//archive-product`,
+				postType: 'wp_template',
+				canvas: 'edit',
+			} );
+		} );
+
+		test( 'should open and close the dialog when clicking on the Product Filters Overlay Navigation block', async ( {
+			editor,
+			page,
+			frontendUtils,
+		} ) => {
+			await editor.setContent( '' );
+			await editor.openGlobalBlockInserter();
+			await page.getByText( 'Product Filters (Experimental)' ).click();
+			const block = editor.canvas.getByLabel(
+				'Block: Product Filters (Experimental)'
+			);
+			await expect( block ).toBeVisible();
+
+			// This forces the list view to show the inner blocks of the Product Filters template part.
+			await editor.canvas
+				.getByLabel( 'Block: Active (Experimental)' )
+				.getByLabel( 'Block: Filter Options' )
+				.click();
+
+			await editor.openDocumentSettingsSidebar();
+			await page.getByLabel( 'Document Overview' ).click();
+			await page
+				.getByRole( 'link', { name: 'Product Filters (Experimental)' } )
+				.nth( 1 )
+				.click();
+
+			const layoutSettings = editor.page.getByText(
+				'OverlayNeverMobileAlways'
+			);
+			await layoutSettings.getByLabel( 'Always' ).click();
+			await editor.page
+				.getByRole( 'link', {
+					name: 'Overlay Navigation (Experimental)',
+				} )
+				.click();
+
+			await editor.saveSiteEditorEntities( {
+				isOnlyCurrentEntityDirty: false,
+			} );
+
+			await page.goto( '/shop/' );
+
+			const productFiltersOverlayNavigation = (
+				await frontendUtils.getBlockByName(
+					'woocommerce/product-filters-overlay-navigation'
+				)
+			 ).filter( {
+				has: page.locator( ':visible' ),
+			} );
+
+			await expect( productFiltersOverlayNavigation ).toBeVisible();
+
+			await page
+				.locator( '.wc-block-product-filters-overlay-navigation' )
+				.first()
+				.click();
+
+			const productFiltersDialog = page.locator(
+				'.wc-block-product-filters--dialog-open'
+			);
+
+			await expect( productFiltersDialog ).toBeVisible();
+
+			const productFiltersDialogCloseButton = (
+				await frontendUtils.getBlockByName(
+					'woocommerce/product-filters-overlay-navigation'
+				)
+			 ).filter( { hasText: 'Close' } );
+
+			await expect( productFiltersDialogCloseButton ).toBeVisible();
+
+			await productFiltersDialogCloseButton.click();
+
+			await expect( productFiltersDialog ).toBeHidden();
+		} );
+	} );
 } );
