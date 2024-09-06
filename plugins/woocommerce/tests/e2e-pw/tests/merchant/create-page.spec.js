131362814734f48b488b429a981e1d5b19831999
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-09-06 14:54:00 +0300

    Remove debug statements

--- plugins/woocommerce/tests/e2e-pw/tests/merchant/create-page.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/merchant/create-page.spec.js
@@ -18,25 +18,20 @@ test.describe(
 		// eslint-disable-next-line playwright/expect-expect
 		test( 'can create new page', async ( { page, testPage } ) => {
 			await goToPageEditor( { page } );
-
 			await closeChoosePatternModal( { page } );
-
 			await fillPageTitle( page, testPage.title );
 
-			await page.pause();
 			const canvas = await getCanvas( page );
 
 			await canvas
 				.getByRole( 'button', { name: 'Add default block' } )
 				.click();
 
-			await page.pause();
 			await canvas
 				.getByRole( 'document', {
 					name: 'Empty block; start writing or type forward slash to choose a block',
 				} )
 				.fill( 'Test Page' );
-			await page.pause();
 
 			await publishPage( page, testPage.title );
 		} );
