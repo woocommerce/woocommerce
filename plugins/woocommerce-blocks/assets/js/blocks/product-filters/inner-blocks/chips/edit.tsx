ba154cd789bd3c1e1906db8c30bf520a1ec9c6ec
Tung Du <dinhtungdu@gmail.com>
2024-09-06 17:36:50 +0700

    chore: js lint

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/chips/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/chips/edit.tsx
@@ -7,9 +7,8 @@ import { useBlockProps } from '@wordpress/block-editor';
  * Internal dependencies
  */
 import './style.scss';
-import { EditProps } from './types';
 
-const Edit = ( props: EditProps ) => {
+const Edit = () => {
 	return <div { ...useBlockProps() }>These are chips.</div>;
 };
 
