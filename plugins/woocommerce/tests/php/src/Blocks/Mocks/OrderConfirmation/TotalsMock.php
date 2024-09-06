31552aa993d6bebd2fb6c6e4781c1a604dbe95d9
Vladimir Reznichenko <kalessil@gmail.com>
2024-09-06 15:24:07 +0200

    PSR-4: cleanup new violations.

--- plugins/woocommerce/tests/php/src/Blocks/Mocks/OrderConfirmation/TotalsMock.php
+++ plugins/woocommerce/tests/php/src/Blocks/Mocks/OrderConfirmation/TotalsMock.php
@@ -1,4 +1,6 @@
 <?php
+declare( strict_types = 1 );
+
 namespace Automattic\WooCommerce\Tests\Blocks\Mocks\OrderConfirmation;
 
 use Automattic\WooCommerce\Blocks\Package;
--- plugins/woocommerce/tests/php/src/Blocks/Mocks/ProductQueryMock.php
+++ plugins/woocommerce/tests/php/src/Blocks/Mocks/ProductQueryMock.php
@@ -1,4 +1,6 @@
 <?php
+declare( strict_types = 1 );
+
 namespace Automattic\WooCommerce\Tests\Blocks\Mocks;
 
 use Automattic\WooCommerce\Blocks\BlockTypes\ProductQuery;
