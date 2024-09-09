0e8d19d43bb775ee2ff7d85f2912970b9c23dbca
louwie17 <lourensschep@gmail.com>
2024-09-09 17:06:01 +0200

    Upgrade storybook to version 7 and add pages workflow for GH publishing (#51168)
    * Updated storybook and renamed stories to use the *.story.js/tsx format
    
    * Renamed stories within product editor and wc admin package
    
    * Include *story files in product editor package
    
    * Add storybook pages workflow
    
    * Remove the 'on' param in storybook-pages
    
    * Add changelogs
    
    * Fix failing tests
    
    * Add dirname for any other webpack configs that rely on the woocommerce-admin one
    
    * Fix React 18 warnings
    
    * Remove unneeded storybook packages from components package
    
    * Remove storybook workflow, to be added as a follow up instead
    
    * Revert @storybook/react back to fix unit tests

--- packages/js/components/src/media-uploader/stories/media-uploader.story.tsx
+++ packages/js/components/src/media-uploader/stories/media-uploader.story.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React, { createElement } from 'react';
-import { Modal, Notice } from '@wordpress/components';
+import { Notice } from '@wordpress/components';
 import { MediaItem } from '@wordpress/media-utils';
 import { useState } from '@wordpress/element';
 import { cloudUpload } from '@wordpress/icons';
@@ -12,59 +12,7 @@ import { cloudUpload } from '@wordpress/icons';
  */
 import { MediaUploader } from '../';
 import { File } from '../types';
-
-const MockMediaUpload = ( { onSelect, render } ) => {
-	const [ isOpen, setOpen ] = useState( false );
-
-	return (
-		<>
-			{ render( {
-				open: () => setOpen( true ),
-			} ) }
-			{ isOpen && (
-				<Modal
-					title="Media Modal"
-					onRequestClose={ ( event ) => {
-						setOpen( false );
-						event.stopPropagation();
-					} }
-				>
-					<p>
-						Use the default built-in{ ' ' }
-						<code>MediaUploadComponent</code> prop to render the WP
-						Media Modal.
-					</p>
-					{ Array( ...Array( 3 ) ).map( ( n, i ) => {
-						return (
-							<button
-								key={ i }
-								onClick={ ( event ) => {
-									onSelect( {
-										alt: 'Random',
-										url: `https://picsum.photos/200?i=${ i }`,
-									} );
-									setOpen( false );
-									event.stopPropagation();
-								} }
-								style={ {
-									marginRight: '16px',
-								} }
-							>
-								<img
-									src={ `https://picsum.photos/200?i=${ i }` }
-									alt="Random"
-									style={ {
-										maxWidth: '100px',
-									} }
-								/>
-							</button>
-						);
-					} ) }
-				</Modal>
-			) }
-		</>
-	);
-};
+import { MockMediaUpload } from './mock-media-uploader';
 
 const ImageGallery = ( { images }: { images: File[] } ) => {
 	return (
