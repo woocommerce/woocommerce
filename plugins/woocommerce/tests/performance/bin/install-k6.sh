3790a73380f8f5f591d1c12ffabdc3f754e9c117
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2024-09-09 20:08:52 +0300

    Improve install script

--- plugins/woocommerce/tests/performance/bin/install-k6.sh
+++ plugins/woocommerce/tests/performance/bin/install-k6.sh
@@ -7,8 +7,9 @@ DOWNLOAD_URL="https://github.com/grafana/k6/releases/download/v$K6_VERSION"
 SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" || return; pwd -P)
 
 download_archive() {
-  local download_url=$1
-  local download_path=$2
+  local archive=$1
+  local download_url="$DOWNLOAD_URL/$archive"
+  local download_path="$SCRIPT_PATH/$archive"
 
   echo "Downloading from $download_url to $download_path"
   curl "$download_url" -L -o "$download_path"
@@ -27,11 +28,11 @@ fi
 
 if [ "$(uname)" == "Darwin" ]; then
 	archive="k6-v$K6_VERSION-macos-$arch.zip"
-    download_archive "$DOWNLOAD_URL/$archive" "$SCRIPT_PATH/$archive"
+    download_archive "$archive"
     unzip -j -o "$SCRIPT_PATH/$archive" -d "$SCRIPT_PATH"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	archive="k6-v$K6_VERSION-linux-$arch.tar.gz"
-    download_archive "$DOWNLOAD_URL/$archive" "$SCRIPT_PATH/$archive"
+    download_archive "$archive"
     tar --strip-components=1 -xzf "$SCRIPT_PATH/$archive" -C "$SCRIPT_PATH"
 else
     echo "Unsupported operating system. Please check K6 docs and install the right version for your system."
