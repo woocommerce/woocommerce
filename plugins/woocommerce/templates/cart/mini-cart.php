23eacfe662fc840fe44e9371ea378a322ddad34c
narenin <narendra.sisodiya10@gmail.com>
2024-09-11 16:21:14 +0530

    Fixed Fatal error call to a member function is_empty() in mini-cart.php:24

--- plugins/woocommerce/templates/cart/mini-cart.php
+++ plugins/woocommerce/templates/cart/mini-cart.php
@@ -21,7 +21,7 @@ defined( 'ABSPATH' ) || exit;
 
 do_action( 'woocommerce_before_mini_cart' ); ?>
 
-<?php if ( ! WC()->cart->is_empty() ) : ?>
+<?php if ( WC()->cart && ! WC()->cart->is_empty() ) : ?>
 
 	<ul class="woocommerce-mini-cart cart_list product_list_widget <?php echo esc_attr( $args['list_class'] ); ?>">
 		<?php
