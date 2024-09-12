42bbcf143dd0f192473687358e3c328b0fcb3719
Tung Du <dinhtungdu@gmail.com>
2024-09-12 17:45:00 +0700

    test: udpate overlay e2e tests

--- plugins/woocommerce-blocks/tests/e2e/tests/product-filters-overlay/product-filters-overlay-template-part.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/product-filters-overlay/product-filters-overlay-template-part.block_theme.spec.ts
@@ -105,10 +105,6 @@ test.describe( 'Filters Overlay Template Part', () => {
 					templatePartData.selectors.editor.blocks.activeFilters
 						.blockLabel
 				)
-				.getByLabel(
-					templatePartData.selectors.editor.blocks.filterOptions
-						.blockLabel
-				)
 				.click();
 
 			await editor.openDocumentSettingsSidebar();
@@ -199,10 +195,6 @@ test.describe( 'Filters Overlay Template Part', () => {
 					templatePartData.selectors.editor.blocks.activeFilters
 						.blockLabel
 				)
-				.getByLabel(
-					templatePartData.selectors.editor.blocks.filterOptions
-						.blockLabel
-				)
 				.click();
 
 			await editor.openDocumentSettingsSidebar();
@@ -219,12 +211,6 @@ test.describe( 'Filters Overlay Template Part', () => {
 				'OverlayNeverMobileAlways'
 			);
 			await layoutSettings.getByLabel( 'Never' ).click();
-			await editor.page
-				.getByRole( 'link', {
-					name: templatePartData.selectors.editor.blocks
-						.productFiltersOverlayNavigation.title,
-				} )
-				.click();
 
 			await editor.saveSiteEditorEntities( {
 				isOnlyCurrentEntityDirty: true,
@@ -267,10 +253,6 @@ test.describe( 'Filters Overlay Template Part', () => {
 			await editor.canvas
 				.getByLabel(
 					templatePartData.selectors.editor.blocks.activeFilters
-						.blockLabel
-				)
-				.getByLabel(
-					templatePartData.selectors.editor.blocks.filterOptions
 						.blockLabel
 				)
 				.click();
