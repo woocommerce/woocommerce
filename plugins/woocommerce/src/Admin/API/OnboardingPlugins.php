b93dc7e0810158f3355710a726a8b191ca1ca383
moon <moon.kyong@automattic.com>
2024-09-16 16:07:06 -0700

    Return a full locale with language and region code

--- plugins/woocommerce/src/Admin/API/OnboardingPlugins.php
+++ plugins/woocommerce/src/Admin/API/OnboardingPlugins.php
@@ -239,7 +239,7 @@ class OnboardingPlugins extends WC_REST_Data_Controller {
 		$calypso_env  = defined( 'WOOCOMMERCE_CALYPSO_ENVIRONMENT' ) && in_array( WOOCOMMERCE_CALYPSO_ENVIRONMENT, array( 'development', 'wpcalypso', 'horizon', 'stage' ), true ) ? WOOCOMMERCE_CALYPSO_ENVIRONMENT : 'production';
 
 		$authorization_url = $manager->get_authorization_url( null, $redirect_url );
-		$authorization_url = add_query_arg( 'locale', explode( '_', get_locale() )[0], $authorization_url );
+		$authorization_url = add_query_arg( 'locale', $this->get_wpcom_locale(), $authorization_url );
 
 		return array(
 			'success' => ! $errors->has_errors(),
@@ -252,6 +252,44 @@ class OnboardingPlugins extends WC_REST_Data_Controller {
 				$authorization_url
 			),
 		);
+	}
+
+	/**
+	 * Return a locale string for wpcom.
+	 *
+	 * @return string
+	 */
+	private function get_wpcom_locale() {
+		// List of locales that should be used with region code
+		$locale_to_lang = array(
+			'bre' => 'br',
+			'de_AT' => 'de-at',
+			'de_CH' => 'de-ch',
+			'de' => 'de_formal',
+			'el' => 'el-po',
+			'en_GB' => 'en-gb',
+			'es_CL' => 'es-cl',
+			'es_MX' => 'es-mx',
+			'fr_BE' => 'fr-be',
+			'fr_CA' => 'fr-ca',
+			'nl_BE' => 'nl-be',
+			'nl' => 'nl_formal',
+			'pt_BR' => 'pt-br',
+			'sr' => 'sr_latin',
+			'zh_CN' => 'zh-cn',
+			'zh_HK' => 'zh-hk',
+			'zh_SG' => 'zh-sg',
+			'zh_TW' => 'zh-tw'
+		);
+
+		$system_locale = get_locale();
+		if ( isset( $locale_to_lang[ $system_locale ] ) ) {
+			// Return the locale with region code if it's in the list.
+			return $locale_to_lang[ $system_locale ];
+		}
+
+		// If the locale is not in the list, return the language code only.
+		return explode( '_', $system_locale )[0];
 	}
 
 	/**
