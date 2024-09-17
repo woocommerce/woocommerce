5376f3cb7dc423f40c85eba2dac5d00e36d9acb0
Veljko <veljano@yahoo.com>
2024-09-17 12:48:52 +0200

    Relogic add customer order

--- plugins/woocommerce/tests/performance/setup/add-customer-order.js
+++ plugins/woocommerce/tests/performance/setup/add-customer-order.js
@@ -6,7 +6,6 @@
  */
 import encoding from 'k6/encoding';
 import http from 'k6/http';
-import { findBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 /**
  * Internal dependencies
@@ -17,36 +16,49 @@ import { base_url, admin_username, admin_password } from '../config.js';
  * Convert Cart & Checkout pages to shortcode.
  */
 export function addCustomerOrder() {
-	function addOrderToCustomer() {
-		let response;
+	let response;
+	const customerUsername = 'customer';
 
-		const credentials = `${ admin_username }:${ admin_password }`;
-		const encodedCredentials = encoding.b64encode( credentials );
-		const requestHeaders = {
-			Authorization: `Basic ${ encodedCredentials }`,
-			'Content-Type': 'application/json',
-		};
+	const credentials = `${ admin_username }:${ admin_password }`;
+	const encodedCredentials = encoding.b64encode( credentials );
+	const requestHeaders = {
+		Authorization: `Basic ${ encodedCredentials }`,
+		'Content-Type': 'application/json',
+	};
 
-		response = http.get( `${ base_url }/wp-json/wc/v3/customers`, {
-			headers: requestHeaders,
-			tags: { name: 'API - Retrieve Customer' },
-		} );
+	// Fetch the list of customers
+	response = http.get( `${ base_url }/wp-json/wc/v3/customers`, {
+		headers: requestHeaders,
+	} );
 
-		const customerId = findBetween( response.body, '"id":', ',' );
+	// Parse the response body as JSON to find the customer by username
+	const customers = JSON.parse( response.body );
+	let customerId = null;
 
-		const createCustomerOrderData = {
-			customer_id: customerId,
-			status: 'completed',
-		};
-		response = http.post(
-			`${ base_url }/wp-json/wc/v3/orders`,
-			JSON.stringify( createCustomerOrderData ),
-			{
-				headers: requestHeaders,
-				tags: { name: 'API - Create Customer Order' },
-			}
+	for ( let i = 0; i < customers.length; i++ ) {
+		if ( customers[ i ].username === customerUsername ) {
+			customerId = customers[ i ].id;
+			break;
+		}
+	}
+
+	// If the customer isn't found, throw an error
+	if ( ! customerId ) {
+		throw new Error(
+			`Customer with username ${ customerUsername } not found`
 		);
 	}
 
-	addOrderToCustomer();
+	// Create a new order for the identified customer
+	const createCustomerOrderData = {
+		customer_id: customerId,
+		status: 'completed',
+	};
+	response = http.post(
+		`${ base_url }/wp-json/wc/v3/orders`,
+		JSON.stringify( createCustomerOrderData ),
+		{
+			headers: requestHeaders,
+		}
+	);
 }
