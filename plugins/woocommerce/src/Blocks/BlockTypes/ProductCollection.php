376a1b5223899c808702f424e156e03d7b6a9a16
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2024-09-18 08:11:07 -0700

    Hide Empty Related Product Collections

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection.php
@@ -1839,13 +1839,20 @@ class ProductCollection extends AbstractBlock {
 					);
 				}
 
+				$related_products = wc_get_related_products(
+					$collection_args['relatedProductReference'],
+					// Use a higher limit so that the result set contains enough products for the collection to subsequently filter.
+					100
+				);
+				if ( empty( $related_products ) ) {
+					return array(
+						'post__in' => array( -1 ),
+					);
+				}
+
 				// Have it filter the results to products related to the one provided.
 				return array(
-					'post__in' => wc_get_related_products(
-						$collection_args['relatedProductReference'],
-						// Use a higher limit so that the result set contains enough products for the collection to subsequently filter.
-						100
-					),
+					'post__in' => $related_products,
 				);
 			},
 			function ( $collection_args, $query ) {
