869164f606ac1cb05a8326e3e1baeb0b0ce7f60c
Vladimir Reznichenko <kalessil@gmail.com>
2024-09-19 15:11:47 +0200

    Monorepo: speedup.

--- plugins/woocommerce/bin/package-update.sh
+++ plugins/woocommerce/bin/package-update.sh
@@ -24,21 +24,16 @@ fi
 # We running the optimization in background to speedup build process, with dev-environments in mind.
 # Building zips enforcing the optimization during the install process, so we are good around building releases as well.
 output 3 "Updating autoloader classmaps..."
-composer dump-autoload --optimize  --quiet &
-output 2 "Done"
+composer dump-autoload --optimize --quiet &
 
 if [ -z "$SKIP_UPDATE_TEXTDOMAINS" ]; then
 	# Convert textdomains
 	output 3 "Updating package PHP textdomains..."
 
 	# Replace text domains within packages with woocommerce
-	pnpm packages:fix:textdomain
-	output 2 "Done!"
-
-#	output 3 "Updating package JS textdomains..."
-#	find ./packages/woocommerce-blocks \( -iname '*.js' -o -iname '*.json' \) -exec sed -i.bak -e "s/'woo-gutenberg-products-block'/'woocommerce'/g" -e "s/\"woo-gutenberg-products-block\"/\"woocommerce\"/g" {} \;
+	node ./bin/package-update-textdomain.js
 fi
 
 # Cleanup backup files
-find ./packages -name "*.bak" -type f -delete
+find ./packages -name "*.bak" -type f -delete &
 output 2 "Done!"
