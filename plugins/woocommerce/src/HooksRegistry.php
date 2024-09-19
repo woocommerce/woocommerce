37ea405cffdcd93b4f815d83699376ed9f9c3713
Vedanshu Jain <vedanshu.jain.2012@gmail.com>
2024-09-19 13:45:52 +0530

    Use ::class notation for better IDE support.

--- plugins/woocommerce/src/HooksRegistry.php
+++ plugins/woocommerce/src/HooksRegistry.php
@@ -14,17 +14,17 @@ class HooksRegistry {
 	 * @var array[] $all_request_actions
 	 */
 	private static array $all_request_actions = array(
-		array( 'pre_set_site_transient_update_plugins', array( 'WC_Helper_Updater', 'transient_update_plugins' ), 21, 1 ),
-		array( 'pre_set_site_transient_update_themes', array( 'WC_Helper_Updater', 'transient_update_themes' ), 21, 1 ),
-		array( 'upgrader_process_complete', array( 'WC_Helper_Updater', 'upgrader_process_complete' ) ),
-		array( 'upgrader_pre_download', array( 'WC_Helper_Updater', 'block_expired_updates' ), 10, 2 ),
+		array( 'pre_set_site_transient_update_plugins', array( \WC_Helper_Updater::class, 'transient_update_plugins' ), 21, 1 ),
+		array( 'pre_set_site_transient_update_themes', array( \WC_Helper_Updater::class, 'transient_update_themes' ), 21, 1 ),
+		array( 'upgrader_process_complete', array( \WC_Helper_Updater::class, 'upgrader_process_complete' ) ),
+		array( 'upgrader_pre_download', array( \WC_Helper_Updater::class, 'block_expired_updates' ), 10, 2 ),
 
-		array( 'plugins_api', array( 'WC_Plugin_Api_Updater', 'plugins_api' ), 20, 3 ),
-		array( 'themes_api', array( 'WC_Plugin_Api_Updater', 'themes_api' ), 20, 3 ),
+		array( 'plugins_api', array( \WC_Plugin_Api_Updater::class, 'plugins_api' ), 20, 3 ),
+		array( 'themes_api', array( \WC_Plugin_Api_Updater::class, 'themes_api' ), 20, 3 ),
 
-		array( 'woocommerce_helper_loaded', array( 'WC_Helper_Compat', 'helper_loaded' ) ),
-		array( 'woocommerce_wccom_install_products', array( 'WC_WCCOM_Site_Installer', 'install' ) ),
-		array( 'woocommerce_rest_api_get_rest_namespaces', array( 'WC_WCCOM_Site', 'register_rest_namespace' ) ),
+		array( 'woocommerce_helper_loaded', array( \WC_Helper_Compat::class, 'helper_loaded' ) ),
+		array( 'woocommerce_wccom_install_products', array( \WC_WCCOM_Site_Installer::class, 'install' ) ),
+		array( 'woocommerce_rest_api_get_rest_namespaces', array( \WC_WCCOM_Site::class, 'register_rest_namespace' ) ),
 	);
 
 	/**
@@ -33,9 +33,9 @@ class HooksRegistry {
 	 * @var array[] $all_request_filters
 	 */
 	private static array $all_request_filters = array(
-		array( 'rest_api_init', array( 'WC_Helper_Subscriptions_API', 'register_rest_routes' ) ),
-		array( 'rest_api_init', array( 'WC_Helper_Orders_API', 'register_rest_routes' ) ),
-		array( 'determine_current_user', array( 'WC_WCCOM_Site', 'authenticate_wccom' ), 14 ),
+		array( 'rest_api_init', array( \WC_Helper_Subscriptions_API::class, 'register_rest_routes' ) ),
+		array( 'rest_api_init', array( \WC_Helper_Orders_API::class, 'register_rest_routes' ) ),
+		array( 'determine_current_user', array( \WC_WCCOM_Site::class, 'authenticate_wccom' ), 14 ),
 	);
 
 	/**
@@ -58,12 +58,12 @@ class HooksRegistry {
 	 * @var array[] $admin_actions
 	 */
 	private static array $admin_actions = array(
-		array( 'admin_notices', array( 'WC_Woo_Update_Manager_Plugin', 'show_woo_update_manager_install_notice' ) ),
-		array( 'admin_init', array( 'WC_Helper_Updater', 'add_hook_for_modifying_update_notices' ) ),
-		array( 'current_screen', array( 'WC_Product_Usage_Notice', 'maybe_show_product_usage_notice' ) ),
-		array( 'wp_ajax_woocommerce_dismiss_product_usage_notice', array( 'WC_Product_Usage_Notice', 'ajax_dismiss' ) ),
-		array( 'wp_ajax_woocommerce_remind_later_product_usage_notice', array( 'WC_Product_Usage_Notice', 'ajax_remind_later' ) ),
-		array( 'woocommerce_helper_loaded', array( 'WC_Helper_Compat', 'helper_loaded' ) ),
+		array( 'admin_notices', array( \WC_Woo_Update_Manager_Plugin::class, 'show_woo_update_manager_install_notice' ) ),
+		array( 'admin_init', array( \WC_Helper_Updater::class, 'add_hook_for_modifying_update_notices' ) ),
+		array( 'current_screen', array( \WC_Product_Usage_Notice::class, 'maybe_show_product_usage_notice' ) ),
+		array( 'wp_ajax_woocommerce_dismiss_product_usage_notice', array( \WC_Product_Usage_Notice::class, 'ajax_dismiss' ) ),
+		array( 'wp_ajax_woocommerce_remind_later_product_usage_notice', array( \WC_Product_Usage_Notice::class, 'ajax_remind_later' ) ),
+		array( 'woocommerce_helper_loaded', array( \WC_Helper_Compat::class, 'helper_loaded' ) ),
 	);
 
 	/**
