2eec13d865f3e13b690c19655198b873898b5591
Mike Jolley <mike.jolley@me.com>
2024-09-20 15:39:23 +0100

    Escape strings in default content

--- plugins/woocommerce/src/Blocks/BlockTypes/OrderConfirmation/CreateAccount.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/OrderConfirmation/CreateAccount.php
@@ -42,20 +42,22 @@ class CreateAccount extends AbstractOrderConfirmationBlock {
 			return $parsed_hooked_block;
 		}
 
+		/* translators: %s: Site title */
+		$site_title_heading                  = sprintf( __( 'Create an account with %s', 'woocommerce' ), wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES ) );
 		$parsed_hooked_block['innerContent'] = array(
 			'<div class="wp-block-woocommerce-order-confirmation-create-account alignwide">
 			<!-- wp:heading {"level":3} -->
-			<h3 class="wp-block-heading">' . __( 'Create an account with Test Store', 'woocommerce' ) . '</h3>
+			<h3 class="wp-block-heading">' . esc_html( $site_title_heading ) . '</h3>
 			<!-- /wp:heading -->
 			<!-- wp:list {"className":"is-style-checkmark-list"} -->
 			<ul class="wp-block-list is-style-checkmark-list"><!-- wp:list-item -->
-			<li>' . __( 'Faster future purchases', 'woocommerce' ) . '</li>
+			<li>' . esc_html__( 'Faster future purchases', 'woocommerce' ) . '</li>
 			<!-- /wp:list-item -->
 			<!-- wp:list-item -->
-			<li>' . __( 'Securely save payment info', 'woocommerce' ) . '</li>
+			<li>' . esc_html__( 'Securely save payment info', 'woocommerce' ) . '</li>
 			<!-- /wp:list-item -->
 			<!-- wp:list-item -->
-			<li>' . __( 'Track orders &amp; view shopping history', 'woocommerce' ) . '</li>
+			<li>' . esc_html__( 'Track orders &amp; view shopping history', 'woocommerce' ) . '</li>
 			<!-- /wp:list-item --></ul>
 			<!-- /wp:list -->
 			</div>',
