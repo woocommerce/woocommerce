a798aba8b09039a2d4a63f73c28fd713eba0b8e7
moon <moon.kyong@automattic.com>
2024-09-23 14:00:42 -0700

    Render coming-soon template content without header and footer if the current theme is not a fse

--- plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
+++ plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
@@ -78,7 +78,11 @@ class ComingSoonRequestHandler {
 		);
 
 		if ( ! empty( $coming_soon_template ) && file_exists( $coming_soon_template ) ) {
-			include $coming_soon_template;
+			if ( ! $is_fse_theme ) {
+				echo get_the_block_template_html();
+			} else {
+				include $coming_soon_template;
+			}
 		}
 
 		if ( ! $is_fse_theme && $is_store_coming_soon ) {
