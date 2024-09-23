b0e55441c64562d0824e7988a625acf904d5ef91
Veljko <veljano@yahoo.com>
2024-09-23 14:02:52 +0200

    Make the util compatible with local and external envs

--- plugins/woocommerce/tests/e2e-pw/utils/themes.js
+++ plugins/woocommerce/tests/e2e-pw/utils/themes.js
@@ -1,6 +1,6 @@
-const { exec } = require( 'node:child_process' );
 const { encodeCredentials } = require( '../utils/plugin-utils' );
 const https = require( 'https' );
+const http = require( 'http' );
 const { admin } = require( '../test-data/data' );
 const { BASE_URL } = process.env;
 
@@ -8,54 +8,41 @@ export const DEFAULT_THEME = 'twentytwentythree';
 
 export const activateTheme = ( themeName ) => {
 	return new Promise( ( resolve, reject ) => {
-		const isLocalhost = BASE_URL.includes( 'localhost' );
-		if ( isLocalhost ) {
-			// Command for local environment
-			const command = `wp-env run tests-cli wp theme install ${ themeName } --activate`;
+		const url = new URL( BASE_URL );
+		const isHttp = url.protocol === 'http:';
+		const requestModule = isHttp ? http : https;
 
-			exec( command, ( error, stdout ) => {
-				if ( error ) {
-					console.error( `Error executing command: ${ error }` );
-					return reject( error );
-				}
-				resolve( stdout );
-			} );
-		} else {
-			// HTTPS request for external environment
-			const url = new URL( BASE_URL );
-			const options = {
-				hostname: url.hostname,
-				port: url.port || 443,
-				path: '/wp-json/e2e-theme/activate',
-				method: 'POST',
-				headers: {
-					Authorization: `Basic ${ encodeCredentials(
-						admin.username,
-						admin.password
-					) }`,
-					'Content-Type': 'application/json',
-				},
-			};
+		const options = {
+			hostname: url.hostname,
+			port: url.port || ( isHttp ? 80 : 443 ),
+			path: '/wp-json/e2e-theme/activate',
+			method: 'POST',
+			headers: {
+				Authorization: `Basic ${ encodeCredentials(
+					admin.username,
+					admin.password
+				) }`,
+				'Content-Type': 'application/json',
+			},
+		};
 
-			const req = https.request( options, ( res ) => {
-				let data = '';
+		const req = requestModule.request( options, ( res ) => {
+			let data = '';
 
-				res.on( 'data', ( chunk ) => {
-					data += chunk;
-				} );
-
-				res.on( 'end', () => {
-					resolve( JSON.parse( data ).message );
-				} );
+			res.on( 'data', ( chunk ) => {
+				data += chunk;
 			} );
 
-			req.on( 'error', ( error ) => {
-				reject( error );
+			res.on( 'end', () => {
+				resolve( JSON.parse( data ).message );
 			} );
+		} );
 
-			// Send the request body
-			req.write( JSON.stringify( { theme_name: themeName } ) );
-			req.end();
-		}
+		req.on( 'error', ( error ) => {
+			reject( error );
+		} );
+
+		req.write( JSON.stringify( { theme_name: themeName } ) );
+		req.end();
 	} );
 };
