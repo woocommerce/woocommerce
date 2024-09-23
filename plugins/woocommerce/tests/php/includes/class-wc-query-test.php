cf11db6d3c04c24244cc40543bd571459a9c7a03
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-09-23 17:14:54 +0200

    Add unit tests

--- plugins/woocommerce/tests/php/includes/class-wc-query-test.php
+++ plugins/woocommerce/tests/php/includes/class-wc-query-test.php
@@ -32,4 +32,37 @@ class WC_Query_Test extends \WC_Unit_Test_Case {
 
 		$this->assertEquals( $expected, $args['where'] );
 	}
+
+	/**
+	 * @testdox Shop page can be set as the homepage on block themes.
+	 */
+	public function test_shop_page_in_home_displays_correctly() {
+		switch_theme( 'twentytwentyfour' );
+
+		// Set the shop page as the homepage.
+		$shop_page_id          = wc_get_page_id( 'shop' );
+		$default_show_on_front = get_option( 'show_on_front' );
+		$default_page_on_front = get_option( 'page_on_front' );
+		update_option( 'show_on_front', 'page' );
+		update_option( 'page_on_front', $shop_page_id );
+
+		$query = new WP_Query(
+			array(
+				'post_type' => 'page',
+				'page_id'   => $shop_page_id,
+			)
+		);
+		// Make it so it's the main query.
+		global $wp_the_query;
+		$previous_wp_the_query = $wp_the_query;
+		$wp_the_query          = $query; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		$query->get_posts();
+
+		$this->assertTrue( defined( 'SHOP_IS_ON_FRONT' ) && SHOP_IS_ON_FRONT );
+
+		// Reset main query and options.
+		$wp_the_query = $previous_wp_the_query; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		update_option( 'show_on_front', $default_show_on_front );
+		update_option( 'page_on_front', $default_page_on_front );
+	}
 }
