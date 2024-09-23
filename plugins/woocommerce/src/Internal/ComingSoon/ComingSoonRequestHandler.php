d27b248ad138cbb31d14b7bb1c0782e7b28c21bd
moon <moon.kyong@automattic.com>
2024-09-23 14:17:46 -0700

    Check for store pages only

--- plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
+++ plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
@@ -78,7 +78,7 @@ class ComingSoonRequestHandler {
 		);
 
 		if ( ! empty( $coming_soon_template ) && file_exists( $coming_soon_template ) ) {
-			if ( ! $is_fse_theme ) {
+			if ( ! $is_fse_theme && $is_store_coming_soon ) {
 				echo get_the_block_template_html();
 			} else {
 				include $coming_soon_template;
