d3b404205bea8cb91f28b973ad72b698513b408a
moon <moon.kyong@automattic.com>
2024-09-23 21:04:18 -0700

    Make sure get_the_block_template_html exist

--- plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
+++ plugins/woocommerce/src/Internal/ComingSoon/ComingSoonRequestHandler.php
@@ -78,7 +78,7 @@ class ComingSoonRequestHandler {
 		);
 
 		if ( ! empty( $coming_soon_template ) && file_exists( $coming_soon_template ) ) {
-			if ( ! $is_fse_theme && $is_store_coming_soon ) {
+			if ( ! $is_fse_theme && $is_store_coming_soon && function_exists( 'get_the_block_template_html' )) {
 				echo get_the_block_template_html();
 			} else {
 				include $coming_soon_template;
