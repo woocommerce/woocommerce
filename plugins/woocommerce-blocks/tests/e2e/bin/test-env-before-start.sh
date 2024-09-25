f61787a43df785e87ee1caa597b3ca5ef93fad7b
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2024-09-25 16:16:41 +0200

    Revert "Try setting stable mariadb version"
    This reverts commit a76dbd375e70e14bb6c4d31fc284707587fa7049.

--- plugins/woocommerce-blocks/tests/e2e/bin/test-env-before-start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Set the database version to 11.2.4-jammy.
-# See https://github.com/WordPress/gutenberg/issues/62242
-sed -i -e "s/image: 'mariadb:lts'/image: 'mariadb:11.2.4-jammy'/g" node_modules/@wordpress/env/lib/build-docker-compose-config.js
