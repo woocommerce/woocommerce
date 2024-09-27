7bbffae409d84926df18f1a12b2944c545024e3a
And Finally <andfinally@users.noreply.github.com>
2024-09-27 17:12:15 +0100

    Styling the WooCommerce > Extensions version of the promo component.
    - Tweaked data on `class-wc-admin-assets.php` to pass `style` and `icon` attributes for that promo.
    - Moved promo on this page from the marketplace `Header` component to the `Content`, above the Sales Banner notices.
    - Deleted `percent.svg` and moved its content into a React component.
    - CSS tweaks for this style of promo.
    - Changes to elements of `PromoCard` component to allow the layout for this style.

--- plugins/woocommerce-admin/client/marketplace/components/header/header.tsx
+++ plugins/woocommerce-admin/client/marketplace/components/header/header.tsx
@@ -6,12 +6,10 @@ import HeaderTitle from '../header-title/header-title';
 import HeaderAccount from '../header-account/header-account';
 import Tabs from '../tabs/tabs';
 import Search from '../search/search';
-import Promotions from '../promotions/promotions';
 
 export default function Header() {
 	return (
 		<header className="woocommerce-marketplace__header">
-			<Promotions promoCardOnly={ true } />
 			<HeaderTitle />
 			<Tabs
 				additionalClassNames={ [
