92c3b9ec1ed6b5adffbe0b8875f9efdd17d01fd8
Dylan Pierce <me@dylanjpierce.com>
2024-09-27 13:32:23 -0400

    Update plugins/woocommerce/tests/php/src/Internal/DataStores/Orders/OrdersTableDataStoreTests.php
    Co-authored-by: Thomas Roberts <5656702+opr@users.noreply.github.com>

--- plugins/woocommerce/tests/php/src/Internal/DataStores/Orders/OrdersTableDataStoreTests.php
+++ plugins/woocommerce/tests/php/src/Internal/DataStores/Orders/OrdersTableDataStoreTests.php
@@ -2800,7 +2800,7 @@ class OrdersTableDataStoreTests extends \HposTestCase {
 					$errors->add('custom_error', 'This is a custom validation error');
 			}, 10, 2);
 
-			// Use reflection to make the protected method accessible
+			// Use reflection to make the protected method accessible.
 			$reflection = new ReflectionClass( $order_controller );
 			$method = $reflection->getMethod( 'perform_custom_order_validation' );
 			$method->setAccessible( true );
