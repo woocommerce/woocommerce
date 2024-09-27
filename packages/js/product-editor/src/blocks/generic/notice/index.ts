bf6fc726990debcfa87791a600bf41a100899463
Nathan Schneider <nsschneider1@gmail.com>
2024-09-27 14:31:38 -0300

    Create reusable block for Notices

---
+++ packages/js/product-editor/src/blocks/generic/notice/index.ts
@@ -0,0 +1,32 @@
+/**
+ * External dependencies
+ */
+import { BlockConfiguration } from '@wordpress/blocks';
+
+/**
+ * Internal dependencies
+ */
+import { registerProductEditorBlockType } from '../../../utils';
+
+/**
+ * Internal dependencies
+ */
+import blockConfiguration from './block.json';
+import { Edit } from './edit';
+
+const { name, ...metadata } =
+	blockConfiguration as BlockConfiguration< TextBlockAttributes >;
+
+export { metadata, name };
+
+export const settings = {
+	example: {},
+	edit: Edit,
+};
+
+export const init = () =>
+	registerProductEditorBlockType( {
+		name,
+		metadata: metadata as never,
+		settings: settings as never,
+	} );
