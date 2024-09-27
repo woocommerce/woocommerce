3673c6b0842d86f3fbbc07a473a88ffe60418654
Vladimir Reznichenko <kalessil@gmail.com>
2024-09-27 09:03:02 +0200

    CI: WIP, reworking run-metrics.sh script.

--- .github/workflows/scripts/run-metrics.sh
+++ .github/workflows/scripts/run-metrics.sh
@@ -2,9 +2,9 @@
 
 set -eo pipefail
 
-GITHUB_EVENT_NAME='pull_request'
-GITHUB_SHA='1cb6eedc0b4e7cf14457699611341d4b778e90db'
-ARTIFACTS_PATH='~/PhpstormProjects/woocommerce/tools/compare-perf/artifacts'
+# GITHUB_EVENT_NAME='pull_request'
+# GITHUB_SHA='1cb6eedc0b4e7cf14457699611341d4b778e90db'
+# ARTIFACTS_PATH='~/PhpstormProjects/woocommerce/tools/compare-perf/artifacts'
 
 if [[ -z "$GITHUB_EVENT_NAME" ]]; then
  	echo "::error::GITHUB_EVENT_NAME must be set"
@@ -20,13 +20,32 @@ if [ "$GITHUB_EVENT_NAME" == "push" ] || [ "$GITHUB_EVENT_NAME" == "pull_request
 	# where compare-perf reporting mode was introduced for processing the provided reports.
 	BASE_SHA=55f855a2e6d769b5ae44305b2772eb30d3e721df
 	WP_VERSION=$(awk -F ': ' '/^Tested up to/{print $2}' readme.txt)
-	echo "Comparing performance between: $BASE_SHA (base) and $GITHUB_SHA (head) on WordPress v$WP_VERSION"
+	title "Comparing performance between: $BASE_SHA (base@trunk) and $GITHUB_SHA (head@$GITHUB_REF) on WordPress v$WP_VERSION"
 
 	title "Setting up compare-perf"
-    pnpm install --filter='compare-perf...' --frozen-lockfile --config.dedupe-peer-dependents=false
+    # pnpm install --filter='compare-perf...' --frozen-lockfile --config.dedupe-peer-dependents=false
 
-  	title "Comparing performance: benchmarking"
+	title "Comparing performance: building head"
+	# git reset --hard && git checkout $GITHUB_REF
+	# pnpm clean:build
+	# pnpm install --filter='@woocommerce/plugin-woocommerce...' --frozen-lockfile --config.dedupe-peer-dependents=false
+	# pnpm --filter='@woocommerce/plugin-woocommerce' build
+
+  	title "Comparing performance: benchmarking head"
 	# TODO: benchmark for missing reports only.
+	# RESULTS_ID="editor_${GITHUB_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics editor
+	# RESULTS_ID="product-editor_${GITHUB_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics product-editor
+
+	title "Comparing performance: building baseline"
+	# git reset --hard && git checkout 55f855a2e6d769b5ae44305b2772eb30d3e721df
+	# pnpm clean:build
+	# pnpm install --filter='@woocommerce/plugin-woocommerce...' --frozen-lockfile --config.dedupe-peer-dependents=false
+	# pnpm --filter='@woocommerce/plugin-woocommerce' build
+
+  	title "Comparing performance: benchmarking baseline"
+	# TODO: benchmark for missing reports only.
+	# RESULTS_ID="editor_${BASE_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics editor
+	# RESULTS_ID="product-editor_${BASE_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics product-editor
 
   	title "Comparing performance: processing reports"
 	# Updating the WP version used for performance jobs means there’s a high
@@ -39,7 +58,7 @@ if [ "$GITHUB_EVENT_NAME" == "push" ] || [ "$GITHUB_EVENT_NAME" == "pull_request
 	# - Be tracked on https://www.codevitals.run/project/woo for all existing
 	#   metrics.
 	# IFS=. read -ra WP_VERSION_ARRAY <<< "$WP_VERSION"
-	# pnpm --filter="compare-perf" run compare perf $GITHUB_SHA $BASE_SHA --tests-branch $GITHUB_SHA --wp-version "${WP_VERSION_ARRAY[0]}.${WP_VERSION_ARRAY[1]}"
+	# pnpm --filter="compare-perf" run compare perf $GITHUB_SHA $BASE_SHA --tests-branch $GITHUB_SHA --wp-version "${WP_VERSION_ARRAY[0]}.${WP_VERSION_ARRAY[1]}" --skip-benchmarking
 
 	# title "Publish results to CodeVitals"
 	# COMMITTED_AT=$(git show -s $GITHUB_SHA --format="%cI")
