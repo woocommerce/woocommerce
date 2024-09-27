e095bacb9c40159856d8485866b5efc0aeb128df
Vladimir Reznichenko <kalessil@gmail.com>
2024-09-27 17:31:49 +0200

    Revert "CI: pick original base reference."
    This reverts commit d8ad1e3563af8b47cbf3ee4f278e8962d163e825.

--- .github/workflows/scripts/run-metrics.sh
+++ .github/workflows/scripts/run-metrics.sh
@@ -19,7 +19,9 @@ function title() {
 if [ "$GITHUB_EVENT_NAME" == "push" ] || [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
 	mkdir -p $ARTIFACTS_PATH && export WP_ARTIFACTS_PATH=$ARTIFACTS_PATH
 
-	BASE_SHA=3d7d7f02017383937f1a4158d433d0e5d44b3dc9
+	# It should be 3d7d7f02017383937f1a4158d433d0e5d44b3dc9, but we pick 55f855a2e6d769b5ae44305b2772eb30d3e721df
+	# where compare-perf reporting mode was introduced for processing the provided reports.
+	BASE_SHA=55f855a2e6d769b5ae44305b2772eb30d3e721df
 	HEAD_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 	WP_VERSION=$(awk -F ': ' '/^Tested up to/{print $2}' readme.txt)
 	title "Comparing performance between: $BASE_SHA@trunk (base) and $GITHUB_SHA@$HEAD_BRANCH (head) on WordPress v$WP_VERSION"
