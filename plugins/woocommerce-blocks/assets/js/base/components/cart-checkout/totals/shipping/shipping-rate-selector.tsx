a97c4571446e8ce2a2e24fc0866ed2ac0c466383
Sam Seay <samueljseay@gmail.com>
2024-09-30 14:07:38 +0800

    Adjust copy and show the address in the updated warning message.

--- plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/shipping/shipping-rate-selector.tsx
+++ plugins/woocommerce-blocks/assets/js/base/components/cart-checkout/totals/shipping/shipping-rate-selector.tsx
@@ -1,18 +1,23 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
-import type { CartResponseShippingRate } from '@woocommerce/types';
+import { __, sprintf } from '@wordpress/i18n';
+import type {
+	CartResponseShippingAddress,
+	CartResponseShippingRate,
+} from '@woocommerce/types';
 import NoticeBanner from '@woocommerce/base-components/notice-banner';
-
+import { createInterpolateElement } from '@wordpress/element';
 /**
  * Internal dependencies
  */
 import ShippingRatesControl from '../../shipping-rates-control';
+import { formatShippingAddress } from '../../../../utils';
 
 export interface ShippingRateSelectorProps {
 	hasRates: boolean;
 	shippingRates: CartResponseShippingRate[];
+	shippingAddress: CartResponseShippingAddress;
 	isLoadingRates: boolean;
 	isAddressComplete: boolean;
 }
@@ -20,12 +25,14 @@ export interface ShippingRateSelectorProps {
 export const ShippingRateSelector = ( {
 	hasRates,
 	shippingRates,
+	shippingAddress,
 	isLoadingRates,
 	isAddressComplete,
 }: ShippingRateSelectorProps ): JSX.Element => {
 	const legend = hasRates
 		? __( 'Shipping options', 'woocommerce' )
 		: __( 'Choose a shipping option', 'woocommerce' );
+
 	return (
 		<fieldset className="wc-block-components-totals-shipping__fieldset">
 			<legend className="screen-reader-text">{ legend }</legend>
@@ -39,9 +46,18 @@ export const ShippingRateSelector = ( {
 								className="wc-block-components-shipping-rates-control__no-results-notice"
 								status="warning"
 							>
-								{ __(
-									'There are no shipping options available. Please check your shipping address.',
-									'woocommerce'
+								{ createInterpolateElement(
+									sprintf(
+										// translators: %s is the address that was used to calculate shipping.
+										__(
+											'No delivery options available for <strong>%s</strong>. Please verify the address is correct or try a different address.',
+											'woocommerce'
+										),
+										formatShippingAddress( shippingAddress )
+									),
+									{
+										strong: <strong />,
+									}
 								) }
 							</NoticeBanner>
 						) }
