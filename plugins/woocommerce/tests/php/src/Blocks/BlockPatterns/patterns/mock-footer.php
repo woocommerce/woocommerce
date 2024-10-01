68eaabf36bc40df577c8f6b0876365e874a1f4c1
“Joshua <joshuatf@gmail.com>
2024-10-01 08:44:48 +0800

    Add tests around block pattern registration

---
+++ plugins/woocommerce/tests/php/src/Blocks/BlockPatterns/patterns/mock-footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Mock Footer
+ * Slug: woocommerce-blocks/mock-footer
+ * Categories: WooCommerce
+ * Block Types: core/template-part/footer
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"20px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+    Mock footer
+</div>
+<!-- /wp:group -->
---
+++ plugins/woocommerce/tests/php/src/Blocks/BlockPatterns/patterns/mock-header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Mock Header
+ * Slug: woocommerce-blocks/mock-header
+ * Categories: WooCommerce
+ * Block Types: core/template-part/header
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"20px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+    Mock header
+</div>
+<!-- /wp:group -->
