e6de59967fa8651fd2101ffbb8b5aee7eeb7ad96
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-10-02 10:53:51 +0200

    Add Products by Brand user-friendly title to the New template popup

--- plugins/woocommerce/includes/class-wc-brands.php
+++ plugins/woocommerce/includes/class-wc-brands.php
@@ -269,6 +269,7 @@ class WC_Brands {
 					'labels'                => array(
 						'name'              => __( 'Brands', 'woocommerce' ),
 						'singular_name'     => __( 'Brand', 'woocommerce' ),
+						'template_name'     => _x( 'Products by Brand', 'Template name', 'woocommerce' ),
 						'search_items'      => __( 'Search Brands', 'woocommerce' ),
 						'all_items'         => __( 'All Brands', 'woocommerce' ),
 						'parent_item'       => __( 'Parent Brand', 'woocommerce' ),
