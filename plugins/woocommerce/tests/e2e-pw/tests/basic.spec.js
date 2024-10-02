ed6021e64fbfc22d2f9056c5e100f2a1414d0f13
Jon Lane <jon.lane@automattic.com>
2024-10-02 10:42:56 -0700

    Minor change to an e2e test to trigger CI

--- plugins/woocommerce/tests/e2e-pw/tests/basic.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/basic.spec.js
@@ -23,6 +23,7 @@ test( 'Load the home page', async ( { page } ) => {
 	await expect(
 		page.getByRole( 'link', { name: 'WordPress' } )
 	).toBeVisible();
+	// Minor change to test the retry action.
 } );
 
 test( 'Load wp-admin as admin', async ( { page } ) => {
