856c5df740c9c273f150260a124ead5407b66752
Vladimir Reznichenko <kalessil@gmail.com>
2024-10-03 13:18:22 +0200

    Monorepo: benchmark baseline with tests from head.

--- .github/workflows/scripts/run-metrics.sh
+++ .github/workflows/scripts/run-metrics.sh
@@ -62,6 +62,7 @@ if [ "$GITHUB_EVENT_NAME" == "push" ] || [ "$GITHUB_EVENT_NAME" == "pull_request
 		echo '##[endgroup]'
 
 		title "##[group]Benchmarking baseline"
+		git restore --source $GITHUB_SHA plugins/woocommerce/tests
 		RESULTS_ID="editor_${BASE_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics editor
 		RESULTS_ID="product-editor_${BASE_SHA}_round-1" pnpm --filter="@woocommerce/plugin-woocommerce" test:metrics product-editor
 		echo '##[endgroup]'
