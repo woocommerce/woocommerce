10aafd397343b540db44a1285ea9711e75c9fbdd
Maikel Perez <mdperez86@gmail.com>
2024-10-03 16:14:04 -0300

    Revert "Update endpoint"
    This reverts commit eff1e891473a10e68a2b5a6eb9c98422d9c26491.

--- plugins/woocommerce-admin/client/customize-store/design-with-ai/services.ts
+++ plugins/woocommerce-admin/client/customize-store/design-with-ai/services.ts
@@ -250,7 +250,7 @@ export const updateStorePatterns = async (
 		const { is_ai_generated } = await apiFetch< {
 			is_ai_generated: boolean;
 		} >( {
-			path: '/wc-admin/ai/store-info',
+			path: '/wc/private/ai/store-info',
 			method: 'GET',
 		} );
 
