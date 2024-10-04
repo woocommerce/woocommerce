dc640d7179bddffd013a7c2db9b2e2764e8ea991
Luigi Teschio <gigitux@gmail.com>
2024-10-04 17:55:03 +0200

    add declare strict type declaration

--- plugins/woocommerce/src/StoreApi/RoutesController.php
+++ plugins/woocommerce/src/StoreApi/RoutesController.php
@@ -1,4 +1,6 @@
 <?php
+declare( strict_types = 1 );
+
 namespace Automattic\WooCommerce\StoreApi;
 
 use Automattic\WooCommerce\StoreApi\Routes\V1\AbstractRoute;
--- plugins/woocommerce/src/StoreApi/Schemas/V1/AI/ProductsSchema.php
+++ plugins/woocommerce/src/StoreApi/Schemas/V1/AI/ProductsSchema.php
@@ -1,4 +1,6 @@
 <?php
+declare( strict_types = 1 );
+
 namespace Automattic\WooCommerce\StoreApi\Schemas\V1\AI;
 
 use Automattic\WooCommerce\StoreApi\Schemas\V1\AbstractSchema;
