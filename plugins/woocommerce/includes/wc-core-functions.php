506ee2e76346a13d0112f43e9a45815d91a06cc3
Mike Jolley <mike.jolley@me.com>
2024-10-04 10:39:16 +0100

    MIssing doc block

--- plugins/woocommerce/includes/wc-core-functions.php
+++ plugins/woocommerce/includes/wc-core-functions.php
@@ -1310,6 +1310,7 @@ function wc_get_base_location() {
  * Uses geolocation to get the customer country and state only if they are valid values.
  *
  * @since 9.5.0
+ * @param array $fallback Fallback location.
  * @return array
  */
 function wc_get_customer_geolocation( $fallback = array(
