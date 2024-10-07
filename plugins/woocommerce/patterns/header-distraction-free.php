bf918ac4b60d0390050322c1323915b6678db608
Mike Jolley <mike.jolley@me.com>
2024-10-07 16:20:21 +0100

    Add missing class

--- plugins/woocommerce/patterns/header-distraction-free.php
+++ plugins/woocommerce/patterns/header-distraction-free.php
@@ -8,7 +8,7 @@
 ?>
 
 <!-- wp:group {"className":"wc-blocks-header-pattern","metadata":{"name":"Checkout Header"},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group wc-blocks-header-pattern">
+<div class="wc-blocks-pattern-header-distraction-free wc-blocks-header-pattern wp-block-group">
 	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|20","top":"var:preset|spacing|20"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
 		<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)">
 			<!-- wp:site-title {"level":0} /-->
