6dfeaf526ec6fe6370a9064caef27a7219046884
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-10-07 07:28:58 -0300

    Add docblock comment to woocommerce_single_product_image_thumbnail_html filter

--- plugins/woocommerce/templates/single-product/product-thumbnails.php
+++ plugins/woocommerce/templates/single-product/product-thumbnails.php
@@ -28,6 +28,14 @@ $attachment_ids = $product->get_gallery_image_ids();
 
 if ( $attachment_ids && $product->get_image_id() ) {
 	foreach ( $attachment_ids as $key => $attachment_id ) {
-		echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_id, false, $key ), $attachment_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
+		/**
+		 * Filter product image thumbnail HTML string.
+		 *
+		 * @since 1.6.4
+		 *
+		 * @param string $html          Product image thumbnail HTML string.
+		 * @param int    $attachment_id Attachment ID.
+		 */
+		echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_id, false, $key ), $attachment_id ); // PHPCS:Ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
