0a82f0728dc538335c90358072ea44e9fe7efff0
Alexandre Faustino <alexmigf@gmail.com>
2024-10-07 14:55:09 +0100

    Update CheckoutFieldsFrontend.php

--- plugins/woocommerce/src/Blocks/Domain/Services/CheckoutFieldsFrontend.php
+++ plugins/woocommerce/src/Blocks/Domain/Services/CheckoutFieldsFrontend.php
@@ -64,11 +64,11 @@ class CheckoutFieldsFrontend {
 	 * @return string
 	 */
 	protected function render_additional_field( $field ) {
-		$label  = esc_html( $field['label'] );
-		$value  = esc_html( $field['value'] );
-		$render = sprintf( '<dt>%s</dt><dd>%s</dd>', $label, $value );
-
-		return apply_filters( 'woocommerce_checkout_render_additional_field', $render, $label, $value, $this );
+		return apply_filters( 'woocommerce_checkout_render_additional_field', sprintf(
+			'<dt>%1$s</dt><dd>%2$s</dd>',
+			esc_html( $field['label'] ),
+			esc_html( $field['value'] )
+		), $field );
 	}
 
 	/**
@@ -130,11 +130,7 @@ class CheckoutFieldsFrontend {
 				continue;
 			}
 
-			$label  = wp_kses_post( $field['label'] );
-			$value  = wp_kses_post( $value );
-			$render = sprintf( '<br><strong>%s</strong>: %s', $label, $value );
-
-			echo apply_filters( 'woocommerce_checkout_render_additional_field', $render, $label, $value, $this );
+			echo apply_filters( 'woocommerce_checkout_render_address_field', sprintf( '<br><strong>%s</strong>: %s', wp_kses_post( $field['label'] ), wp_kses_post( $value ) ), $field, $value, $key, $address_type, $this );
 		}
 	}
 
