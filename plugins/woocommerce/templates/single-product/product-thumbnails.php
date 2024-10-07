7921f2918b90a53373fc0a5a85fc49f4447bdb6b
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-10-07 10:01:58 -0300

    Bump product-thumbnail template version

--- plugins/woocommerce/templates/single-product/product-thumbnails.php
+++ plugins/woocommerce/templates/single-product/product-thumbnails.php
@@ -12,7 +12,7 @@
  *
  * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     3.5.1
+ * @version     9.5.0
  */
 
 defined( 'ABSPATH' ) || exit;
