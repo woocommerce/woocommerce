80b0d59dd7377b6bcfb1b08fe3f46ec0ff3c5b5e
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2024-10-08 06:57:37 -0300

    Update product gallery trigger markup

--- plugins/woocommerce/client/legacy/js/frontend/single-product.js
+++ plugins/woocommerce/client/legacy/js/frontend/single-product.js
@@ -250,10 +250,10 @@ jQuery( function( $ ) {
 	ProductGallery.prototype.initPhotoswipe = function() {
 		if ( this.zoom_enabled && this.$images.length > 0 ) {
 			this.$target.prepend(
-				'<button class="woocommerce-product-gallery__trigger" aria-haspopup="dialog" aria-label="'+
+				'<a href="#" role="button" class="woocommerce-product-gallery__trigger" aria-haspopup="dialog" aria-label="'+
 				wc_single_product_params.i18n_product_gallery_trigger_text + '">' +
 					'<span aria-hidden="true">🔍</span>' +
-				'</button>'
+				'</a>'
 			);
 			this.$target.on( 'click', '.woocommerce-product-gallery__trigger', this.openPhotoswipe );
 			this.$target.on( 'click', '.woocommerce-product-gallery__image a', function( e ) {
