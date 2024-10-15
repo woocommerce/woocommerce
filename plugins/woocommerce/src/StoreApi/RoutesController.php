6a369623831856ba9bfad07ad3abe431c5506af0
Albert Juhé Lluveras <contact@albertjuhe.com>
2024-10-15 12:39:15 +0200

    Fix `Error: Failed opening required '.../wp-content/plugins/woocommerce/src/StoreApi/Schemas/V1/AI/ProductsSchema.php` fatal error (WC 9.4) (#52023)
    * Revert "CYS - Move the `private/patterns` endpoint to woocommerce admin API (#50400)"
    
    This reverts commit 091141218ed0f27e717f479f4bac52e8b594e5a9.
    
    * Revert "CYS - Move the `ai/products` endpoint to woocommerce admin API (#50396)"
    
    This reverts commit ae6f7837b224bd39005e65c80420b0c1a83a4201.
    
    * fix endpoint
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * fix e2e test
    
    * add declare strict type declaration
    
    * Remove old TODO comment
    
    * Add strict_types to Products route class file
    
    ---------
    
    Co-authored-by: Luigi Teschio <gigitux@gmail.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/src/StoreApi/RoutesController.php
+++ plugins/woocommerce/src/StoreApi/RoutesController.php
@@ -1,4 +1,6 @@
 <?php
+declare( strict_types = 1 );
+
 namespace Automattic\WooCommerce\StoreApi;
 
 use Automattic\WooCommerce\StoreApi\Routes\V1\AbstractRoute;
@@ -36,7 +38,7 @@ class RoutesController {
 	public function __construct( SchemaController $schema_controller ) {
 		$this->schema_controller = $schema_controller;
 		$this->routes            = [
-			'v1' => [
+			'v1'      => [
 				Routes\V1\Batch::IDENTIFIER              => Routes\V1\Batch::class,
 				Routes\V1\Cart::IDENTIFIER               => Routes\V1\Cart::class,
 				Routes\V1\CartAddItem::IDENTIFIER        => Routes\V1\CartAddItem::class,
@@ -65,6 +67,10 @@ class RoutesController {
 				Routes\V1\Products::IDENTIFIER           => Routes\V1\Products::class,
 				Routes\V1\ProductsById::IDENTIFIER       => Routes\V1\ProductsById::class,
 				Routes\V1\ProductsBySlug::IDENTIFIER     => Routes\V1\ProductsBySlug::class,
+			],
+			'private' => [
+				Routes\V1\AI\Products::IDENTIFIER => Routes\V1\AI\Products::class,
+				Routes\V1\Patterns::IDENTIFIER    => Routes\V1\Patterns::class,
 			],
 		];
 	}
