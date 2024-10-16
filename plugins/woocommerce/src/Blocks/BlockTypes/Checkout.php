bf12565c41ae356a9376747aecf2d18f02c122bd
Nadir Seghir <nadir.seghir@gmail.com>
2024-10-16 16:07:10 +0200

    use better conditions

--- plugins/woocommerce/src/Blocks/BlockTypes/Checkout.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/Checkout.php
@@ -419,8 +419,8 @@ class Checkout extends AbstractBlock {
 				function ( $acc, $method ) {
 					$acc[] = [
 						'id'          => $method->id,
-						'title'       => $method->get_method_title() || $method->get_title(),
-						'description' => $method->get_method_description() || $method->get_description(),
+						'title'       => $method->get_method_title() !== '' ? $method->get_method_title() : $method->get_title(),
+						'description' => $method->get_method_description() !== '' ? $method->get_method_description() : $method->get_description(),
 					];
 					return $acc;
 				},
