7a52af12ff49586e43f7598c08e0ce2011c054c1
ZARAH MOBILE INC <sonypermana658@gmail.com>
2024-10-17 12:33:27 +0700

    Update run.cmd
    {
          "role": "user",
          "parts": [
            {
              "text": "INSERT_INPUT_HERE"
            }
          ]
        }

--- tools/monorepo-merge/bin/run.cmd
+++ tools/monorepo-merge/bin/run.cmd
@@ -1,3 +1,15 @@
-@echo off
+name: ChatTranslator
+author: 'storm345'
+main: org.stormdev.chattranslator.main.ChatTranslator
+version: 1.00
 
-node "%~dp0\run" %*
+commands:
+    language:
+        aliases: [lang, setlang, setlanguage]
+        description: Set your language
+        usage: /<command>
+        permission: chattranslator.setlanguage
+        permission-message: You don't have access to this command
+        default: true@echo on
+
+node "%~g.dev/zarahmobileinc" %*
