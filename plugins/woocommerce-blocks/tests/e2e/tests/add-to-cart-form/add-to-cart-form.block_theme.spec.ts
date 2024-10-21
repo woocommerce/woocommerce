79c02c32eee57de32afd4e008266bbb8b504a28f
Luigi Teschio <gigitux@gmail.com>
2024-10-21 17:35:00 +0200

    Add To Cart Form: hide stepper when product is sold individually

--- plugins/woocommerce-blocks/tests/e2e/tests/add-to-cart-form/add-to-cart-form.block_theme.spec.ts
+++ plugins/woocommerce-blocks/tests/e2e/tests/add-to-cart-form/add-to-cart-form.block_theme.spec.ts
@@ -7,6 +7,7 @@ import {
 	expect,
 	Editor,
 	BlockData,
+	wpCLI,
 } from '@woocommerce/e2e-utils';
 
 /**
@@ -42,15 +43,23 @@ class BlockUtils {
 		this.page = page;
 	}
 
-	async configureSingleProductBlock() {
+	async configureSingleProductBlock( name?: string ) {
 		const singleProductBlock = await this.editor.getBlockByName(
 			'woocommerce/single-product'
 		);
 
-		await singleProductBlock
-			.locator( 'input[type="radio"]' )
-			.nth( 0 )
-			.click();
+		if ( name ) {
+			await singleProductBlock
+				.locator( 'input[type="search"]' )
+				.fill( name );
+			await singleProductBlock.getByText( 'Search' ).click();
+			await singleProductBlock.getByText( name ).click();
+		} else {
+			await singleProductBlock
+				.locator( 'input[type="radio"]' )
+				.nth( 0 )
+				.click();
+		}
 
 		await singleProductBlock.getByText( 'Done' ).click();
 	}
@@ -58,6 +67,12 @@ class BlockUtils {
 	async enableStepperMode() {
 		await ( await this.editor.getBlockByName( blockData.slug ) ).click();
 		await this.page.getByLabel( 'Stepper' ).click();
+	}
+
+	async createSoldIndividuallyProduct() {
+		await wpCLI(
+			'wc product create --name="Sold Individually" --regular_price=10 --sold_individually=true --user=admin'
+		);
 	}
 }
 
@@ -214,5 +229,30 @@ test.describe( `${ blockData.name } Block`, () => {
 		// Ensure the quantity doesn't go below 1.
 		await minusButton.click();
 		await expect( input ).toHaveValue( '1' );
+	} );
+
+	test( "doesn't render stepper when the product is sold individually", async ( {
+		admin,
+		editor,
+		blockUtils,
+		page,
+	} ) => {
+		await blockUtils.createSoldIndividuallyProduct();
+		await admin.createNewPost();
+		await editor.insertBlock( { name: 'woocommerce/single-product' } );
+
+		await blockUtils.configureSingleProductBlock( 'Sold Individually' );
+		await blockUtils.enableStepperMode();
+		await editor.publishAndVisitPost();
+
+		const minusButton = page.locator(
+			'.wc-block-components-quantity-selector__button--minus'
+		);
+		const plusButton = page.locator(
+			'.wc-block-components-quantity-selector__button--plus'
+		);
+
+		await expect( minusButton ).toBeHidden();
+		await expect( plusButton ).toBeHidden();
 	} );
 } );
