e3634f0306318cd5fde539b05b0c78de2f0ba455
Alexandre Lara <allexandrelara@gmail.com>
2024-10-21 17:04:17 -0300

    Remove clear button when setting is disabled

--- plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/attribute-filter/edit.tsx
+++ plugins/woocommerce-blocks/assets/js/blocks/product-filters/inner-blocks/attribute-filter/edit.tsx
@@ -14,11 +14,14 @@ import {
 	useBlockProps,
 	useInnerBlocksProps,
 	BlockContextProvider,
+	store as blockEditorStore,
 } from '@wordpress/block-editor';
 import { withSpokenMessages } from '@wordpress/components';
 import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { getSetting } from '@woocommerce/settings';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { BlockAttributes, createBlock } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
@@ -36,8 +39,31 @@ import { Notice } from '../../components/notice';
 
 const ATTRIBUTES = getSetting< AttributeSetting[] >( 'attributes', [] );
 
+function findClientIdByName(
+	block: BlockAttributes,
+	targetName: string
+): string | undefined {
+	if ( block.name === targetName ) {
+		return block.clientId;
+	}
+
+	if ( block.innerBlocks && block.innerBlocks.length > 0 ) {
+		for ( const innerBlock of block.innerBlocks ) {
+			const blockId: string | undefined = findClientIdByName(
+				innerBlock,
+				targetName
+			);
+			if ( blockId ) {
+				return blockId;
+			}
+		}
+	}
+
+	return undefined;
+}
+
 const Edit = ( props: EditProps ) => {
-	const { attributes: blockAttributes } = props;
+	const { attributes: blockAttributes, clientId } = props;
 
 	const {
 		attributeId,
@@ -47,6 +73,7 @@ const Edit = ( props: EditProps ) => {
 		showCounts,
 		sortOrder,
 		hideEmpty,
+		clearButton,
 	} = blockAttributes;
 
 	const attributeObject = getAttributeFromId( attributeId );
@@ -56,6 +83,21 @@ const Edit = ( props: EditProps ) => {
 	>( [] );
 	const [ isOptionsLoading, setIsOptionsLoading ] =
 		useState< boolean >( true );
+	const [ previousClearButtonState, setPreviousClearButtonState ] =
+		useState< boolean >( clearButton );
+
+	const { clearButtonBlockId } = useSelect( ( select ) => {
+		const { getBlock } = select( blockEditorStore );
+		const attributeFilterBlock = getBlock( clientId );
+		const clearButtonId = findClientIdByName(
+			attributeFilterBlock,
+			'woocommerce/product-filter-clear-button'
+		);
+
+		return {
+			clearButtonBlockId: clearButtonId,
+		};
+	} );
 
 	const { results: attributeTerms, isLoading: isTermsLoading } =
 		useCollection< AttributeTerm >( {
@@ -75,6 +117,9 @@ const Edit = ( props: EditProps ) => {
 			queryState: {},
 			isEditor: true,
 		} );
+	// @ts-expect-error @wordpress/data types are outdated.
+	const { insertBlock, removeBlock, updateBlockAttributes } =
+		useDispatch( blockEditorStore );
 
 	useEffect( () => {
 		if ( isTermsLoading || isFilterCountsLoading ) return;
@@ -128,6 +173,31 @@ const Edit = ( props: EditProps ) => {
 		hideEmpty,
 		isTermsLoading,
 		isFilterCountsLoading,
+	] );
+
+	useEffect( () => {
+		if ( clearButton !== previousClearButtonState ) {
+			if ( clearButton ) {
+				insertBlock(
+					createBlock( 'woocommerce/product-filter-clear-button' ),
+					1
+				);
+			} else if (
+				clearButton === false &&
+				Boolean( clearButtonBlockId )
+			) {
+				updateBlockAttributes( clearButtonBlockId, {
+					lock: { remove: false, move: false },
+				} );
+				removeBlock( clearButtonBlockId );
+			}
+		}
+	}, [
+		clearButton,
+		previousClearButtonState,
+		clearButtonBlockId,
+		insertBlock,
+		removeBlock,
 	] );
 
 	const { children, ...innerBlocksProps } = useInnerBlocksProps(
