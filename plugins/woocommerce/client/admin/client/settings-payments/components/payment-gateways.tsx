f8644b35858518ecd63e9fa0fe3c105cb232f263
Daniel Mallory <daniel.mallory@automattic.com>
2024-10-22 14:14:32 +0100

    Updates to support gateway management.

--- plugins/woocommerce/client/admin/client/settings-payments/components/payment-gateways.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/components/payment-gateways.tsx
@@ -1,119 +1,173 @@
 /**
  * External dependencies
  */
-import { Gridicon } from '@automattic/components';
+import React, { useEffect, useState } from 'react';
 import { Button, Card, CardHeader, SelectControl } from '@wordpress/components';
-import React from '@wordpress/element';
 import { decodeEntities } from '@wordpress/html-entities';
 import { __ } from '@wordpress/i18n';
 import { EllipsisMenu, List, Pill } from '@woocommerce/components';
 import { WooPaymentMethodsLogos } from '@woocommerce/onboarding';
+import { Gridicon } from '@automattic/components';
 
 /**
  * Internal dependencies
  */
 
+interface PaymentGateway {
+	id: string;
+	title: string;
+	description: string;
+	order: string | number;
+	enabled: boolean;
+	method_title: string;
+	method_description: string;
+	needs_setup: boolean;
+	image?: string;
+	image_72x72?: string;
+	square_image?: string;
+	actionText?: string;
+	recommended?: boolean;
+}
+
+interface WooPaymentsData {
+	isSupported: boolean;
+	isOnboarded: boolean;
+	isInTestMode: boolean;
+}
+
+const parseScriptTag = ( elementId: string ) => {
+	const scriptTag = document.getElementById( elementId );
+	return scriptTag ? JSON.parse( scriptTag.textContent || '' ) : [];
+};
+
 export const PaymentGateways = () => {
-	// Mock payment gateways for now.
-	// TODO Get the list of gateways via the API in future PR.
-	const mockPaymentGateways = [
-		{
-			id: 'woocommerce_payments',
-			title: __( 'Accept payments with Woo', 'woocommerce' ),
-			content: __(
-				'Credit/debit cards, Apple Pay, Google Pay & more.',
-				'woocommerce'
-			),
-			image: 'https://woocommerce.com/wp-content/plugins/wccom-plugins/payment-gateway-suggestions/images/wcpay.svg',
-			square_image:
-				'https://woocommerce.com/wp-content/plugins/wccom-plugins/payment-gateway-suggestions/images/woocommerce.svg',
-			image_72x72:
-				'https://woocommerce.com/wp-content/plugins/wccom-plugins/payment-gateway-suggestions/images/wcpay.svg',
-			actionText: '',
-			recommended: true,
-		},
-		{
-			id: 'ppcp-gateway',
-			title: 'PayPal Payments',
-			content: __(
-				"Safe and secure payments using credit cards or your customer's PayPal account.",
-				'woocommerce'
-			),
-			image: 'https://woocommerce.com/wp-content/plugins/woocommerce/assets/images/paypal.png',
-			image_72x72:
-				'https://woocommerce.com/wp-content/plugins/wccom-plugins/payment-gateway-suggestions/images/72x72/paypal.png',
-			square_image:
-				'https://woocommerce.com/wp-content/plugins/wccom-plugins/payment-gateway-suggestions/images/paypal.svg',
-			actionText: '',
-			recommended: false,
-		},
-	];
+	const [ paymentGateways, setPaymentGateways ] = useState( [] );
+	const [ wooPaymentsData, setWooPaymentsData ] = useState( {} );
+
+	const recommendedGateways = [ 'woocommerce_payments' ];
+
+	useEffect( () => {
+		setWooPaymentsData(
+			parseScriptTag( 'experimental_wc_settings_payments_woopayments' )
+		);
+		setPaymentGateways(
+			parseScriptTag( 'experimental_wc_settings_payments_gateways' )
+		);
+	}, [] );
+
+	const enableGateway = ( gateway: PaymentGateway ) => () => {
+		console.log( 'Enable gateway', gateway );
+	};
+
+	const manageGateway = ( gateway: PaymentGateway ) => () => {
+		console.log( 'Manage gateway', gateway );
+	};
+
+	const setupLivePayments = () => {
+		console.log( 'Setup live payments' );
+	};
+
+	console.log( 'paymentGateways', paymentGateways );
+	console.log( 'wooPaymentsData', wooPaymentsData );
 
 	// Transform plugins comply with List component format.
-	const paymentGatewaysList = mockPaymentGateways.map( ( gateway ) => {
-		return {
-			key: gateway.id,
-			title: (
-				<>
-					{ gateway.title }
-					{ gateway.recommended && (
-						<Pill>{ __( 'Recommended', 'woocommerce' ) }</Pill>
-					) }
-				</>
-			),
-			content: (
-				<>
-					{ decodeEntities( gateway.content ) }
-					{ gateway.id === 'woocommerce_payments' && (
-						<WooPaymentMethodsLogos
-							maxElements={ 10 }
-							isWooPayEligible={ true }
-						/>
-					) }
-				</>
-			),
-			after: (
-				<div className="woocommerce-list__item-after__actions">
-					<Button
-						variant={ 'primary' }
-						// onClick={ () => ( console.log('test') ) }
-						isBusy={ false }
-						disabled={ false }
-					>
-						{ gateway.actionText ||
-							__( 'Get started', 'woocommerce' ) }
-					</Button>
-					<EllipsisMenu
-						label={ __( 'Task List Options', 'woocommerce' ) }
-						renderContent={ () => (
-							<div>
-								<Button>
-									{ __( 'Learn more', 'woocommerce' ) }
-								</Button>
-								<Button>
-									{ __(
-										'See Terms of Service',
-										'woocommerce'
-									) }
-								</Button>
-							</div>
+	const paymentGatewaysList = paymentGateways.map(
+		( gateway: PaymentGateway ) => {
+			return {
+				key: gateway.id,
+				title: (
+					<>
+						{ gateway.method_title }
+						{ recommendedGateways.includes( gateway.id ) && (
+							// TODO: Support recommended flag
+							<Pill>{ __( 'Recommended', 'woocommerce' ) }</Pill>
 						) }
+					</>
+				),
+				content: (
+					<>
+						{ decodeEntities( gateway.method_description ) }
+						{ gateway.id === 'woocommerce_payments' && (
+							<WooPaymentMethodsLogos
+								maxElements={ 10 }
+								isWooPayEligible={ true }
+							/>
+						) }
+					</>
+				),
+				after: (
+					<div className="woocommerce-list__item-after__actions">
+						<>
+							<Button
+								variant={
+									gateway.enabled ? 'secondary' : 'primary'
+								}
+								onClick={
+									gateway.enabled
+										? manageGateway( gateway )
+										: enableGateway( gateway )
+								}
+								isBusy={ false }
+								disabled={ false }
+							>
+								{ gateway.enabled
+									? __( 'Manage', 'woocommerce' )
+									: __( 'Enable', 'woocommerce' ) }
+							</Button>
+							{ gateway.id === 'woocommerce_payments' &&
+								wooPaymentsData.isInTestMode && (
+									<Button
+										variant="primary"
+										onClick={ setupLivePayments }
+										isBusy={ false }
+										disabled={ false }
+									>
+										{ __(
+											'Set up live payments',
+											'woocommerce'
+										) }
+									</Button>
+								) }
+							<EllipsisMenu
+								label={ __(
+									'Task List Options',
+									'woocommerce'
+								) }
+								renderContent={ () => (
+									<div>
+										<Button>
+											{ __(
+												'Learn more',
+												'woocommerce'
+											) }
+										</Button>
+										<Button>
+											{ __(
+												'See Terms of Service',
+												'woocommerce'
+											) }
+										</Button>
+									</div>
+								) }
+							/>
+						</>
+					</div>
+				),
+				// TODO add drag-and-drop icon before image (future PR)
+				before: (
+					<img
+						src={
+							gateway.square_image ||
+							gateway.image_72x72 ||
+							gateway.image ||
+							'https://woocommerce.com/wp-content/plugins/wccom-plugins/payment-gateway-suggestions/images/wcpay.svg'
+						}
+						alt={ gateway.title + ' logo' }
 					/>
-				</div>
-			),
-			// TODO add drag-and-drop icon before image (future PR)
-			before: (
-				<img
-					src={
-						gateway.square_image ||
-						gateway.image_72x72 ||
-						gateway.image
-					}
-					alt={ gateway.title + ' logo' }
-				/>
-			),
-		};
-	} );
+				),
+			};
+		}
+	);
 
 	// Add offline payment provider.
 	paymentGatewaysList.push( {
@@ -147,7 +201,7 @@ export const PaymentGateways = () => {
 				</div>
 				<div className="settings-payment-gateways__header-select-container">
 					<SelectControl
-						className="woocommerce-profiler-select-control__country"
+						className="woocommerce-select-control__country"
 						prefix={ __( 'Business location :', 'woocommerce' ) }
 						placeholder={ '' }
 						label={ '' }
