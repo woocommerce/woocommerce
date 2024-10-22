b8d79da6ca9bc1fc39a2cbd3bcc2919d4048a94e
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2024-10-22 12:42:00 +0800

    Fix "Finish setup" icon remains if tasks are disabled without being completed

--- plugins/woocommerce/client/admin/client/activity-panel/activity-panel.js
+++ plugins/woocommerce/client/admin/client/activity-panel/activity-panel.js
@@ -180,7 +180,7 @@ export const ActivityPanel = ( { isEmbedded, query } ) => {
 
 		const setupList = activeSetupList && getTaskList( activeSetupList );
 
-		const isSetupTaskListHidden = setupList?.isHidden ?? false;
+		const isSetupTaskListHidden = setupList ? setupList.isHidden : true; // If setupList is null, it means the setup task list is disabled.
 		const setupVisibleTasks = getVisibleTasks( setupList?.tasks || [] );
 		const extendedTaskList = getTaskList( 'extended' );
 
@@ -314,7 +314,7 @@ export const ActivityPanel = ( { isEmbedded, query } ) => {
 			},
 			visible: isAddProductPage(),
 		};
-
+		console.log( 'setupTaskListHidden', setupTaskListHidden );
 		const setup = {
 			name: 'setup',
 			title: __( 'Finish setup', 'woocommerce' ),
