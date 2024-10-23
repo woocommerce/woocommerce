dcb9aea767fc66951d1ac8e8c507e4d2cd6b5df0
paul sealock <psealock@gmail.com>
2024-10-24 11:33:01 +1300

    better name

--- plugins/woocommerce/src/Deprecated.php
+++ plugins/woocommerce/src/Deprecated.php
@@ -8,6 +8,6 @@
 
 use Automattic\WooCommerce\Admin\Features\Navigation\RemovedDeprecated;
 
-class_alias( \Automattic\WooCommerce\Admin\Features\Navigation\RemovedDeprecated::class, \Automattic\WooCommerce\Admin\Features\Navigation\Screen::class );
-class_alias( \Automattic\WooCommerce\Admin\Features\Navigation\RemovedDeprecated::class, \Automattic\WooCommerce\Admin\Features\Navigation\Menu::class );
-class_alias( \Automattic\WooCommerce\Admin\Features\Navigation\RemovedDeprecated::class, \Automattic\WooCommerce\Admin\Features\Navigation\CoreMenu::class );
+class_alias( RemovedDeprecated::class, \Automattic\WooCommerce\Admin\Features\Navigation\Screen::class );
+class_alias( RemovedDeprecated::class, \Automattic\WooCommerce\Admin\Features\Navigation\Menu::class );
+class_alias( RemovedDeprecated::class, \Automattic\WooCommerce\Admin\Features\Navigation\CoreMenu::class );
