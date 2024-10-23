b797685a1bc74537cc51f1395fb0586c9e3e1718
 Ján Mikláš <neosinner@gmail.com>
2024-10-23 15:35:21 +0200

    Remove no longer needed descriptions in "From" name and address
    #52216

--- plugins/woocommerce/includes/admin/settings/class-wc-settings-emails.php
+++ plugins/woocommerce/includes/admin/settings/class-wc-settings-emails.php
@@ -82,7 +82,7 @@ class WC_Settings_Emails extends WC_Settings_Page {
 
 				array(
 					'title'    => __( '"From" name', 'woocommerce' ),
-					'desc'     => __( 'How the sender name appears in outgoing WooCommerce emails.', 'woocommerce' ),
+					'desc'     => '',
 					'id'       => 'woocommerce_email_from_name',
 					'type'     => 'text',
 					'css'      => 'min-width:400px;',
@@ -93,7 +93,7 @@ class WC_Settings_Emails extends WC_Settings_Page {
 
 				array(
 					'title'             => __( '"From" address', 'woocommerce' ),
-					'desc'              => __( 'How the sender email appears in outgoing WooCommerce emails.', 'woocommerce' ),
+					'desc'              => '',
 					'id'                => 'woocommerce_email_from_address',
 					'type'              => 'email',
 					'custom_attributes' => array(
