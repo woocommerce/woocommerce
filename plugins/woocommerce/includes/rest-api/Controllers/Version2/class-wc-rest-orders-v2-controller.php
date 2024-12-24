4e9e4787c8a96f44aea04c220eefc6110ef0bc18
Job <jobjen02@gmail.com>
2024-12-24 15:14:09 +0100

    Added the global unique id to the orders endpoint

--- plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-orders-v2-controller.php
+++ plugins/woocommerce/includes/rest-api/Controllers/Version2/class-wc-rest-orders-v2-controller.php
@@ -224,15 +224,27 @@ class WC_REST_Orders_V2_Controller extends WC_REST_CRUD_Controller {
 		}
 
 		// Add SKU, PRICE, and IMAGE to products.
+		// Add parent_name if the product is a variation.
 		if ( is_callable( array( $item, 'get_product' ) ) ) {
-			$data['sku']   = $item->get_product() ? $item->get_product()->get_sku() : null;
-			$data['price'] = $item->get_quantity() ? $item->get_total() / $item->get_quantity() : 0;
+			$product = $item->get_product();
 
-			$image_id      = $item->get_product() ? $item->get_product()->get_image_id() : 0;
-			$data['image'] = array(
-				'id'  => $image_id,
-				'src' => $image_id ? wp_get_attachment_image_url( $image_id, 'full' ) : '',
-			);
+			if ( $product ) {
+				$data['sku']   = $product->get_sku();
+				$data['global_unique_id']   = $product->get_global_unique_id();
+				$data['price'] = $item->get_quantity() ? $item->get_total() / $item->get_quantity() : 0;
+
+				$image_id      = $product->get_image_id();
+				$data['image'] = array(
+					'id'  => $image_id,
+					'src' => $image_id ? wp_get_attachment_image_url( $image_id, 'full' ) : '',
+				);
+
+				if ( is_callable( array( $product, 'get_parent_data' ) ) ) {
+					$data['parent_name'] = $product->get_title();
+				} else {
+					$data['parent_name'] = null;
+				}
+			}
 		}
 
 		// Add parent_name if the product is a variation.
