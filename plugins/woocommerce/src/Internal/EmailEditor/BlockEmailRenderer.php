7bf11153b0815f0738ad3c35f950bac0dc29adef
Rostislav Wolny <rosta.wolny@automattic.com>
2025-03-06 17:09:58 +0100

    Move the logic for capturing the Woo mail content to BlockEmailRenderer
    This is the first step of refactoring the functionality for capturing
    Woo email's content to an extra service.
    We will need such a service to be able grab the content for block
    or for preview.

--- plugins/woocommerce/src/Internal/EmailEditor/BlockEmailRenderer.php
+++ plugins/woocommerce/src/Internal/EmailEditor/BlockEmailRenderer.php
@@ -71,19 +71,35 @@ class BlockEmailRenderer {
 	/**
 	 * Maybe render block-based email content.
 	 *
+	 * @param \WC_Email $wc_email WooCommerce email.
+	 * @return string|null Modified email content
+	 */
+	public function maybe_render_block_email( \WC_Email $wc_email ): ?string {
+		$email_post = $this->get_email_post_by_wc_email( $wc_email );
+		if ( ! $email_post ) {
+			return null;
+		}
+
+		$woo_content = $this->capture_woo_content( $wc_email );
+		return $this->render_block_email( $email_post, $woo_content, $wc_email );
+	}
+
+	/**
+	 * Maybe render block-based email content.
+	 *
 	 * @param \WP_Post  $email_post Email post.
 	 * @param string    $woo_content WooCommerce email content.
 	 * @param \WC_Email $wc_email WooCommerce email.
 	 * @return string Modified email content
 	 */
-	public function render_block_email( \WP_Post $email_post, string $woo_content, \WC_Email $wc_email ): ?string {
+	private function render_block_email( \WP_Post $email_post, string $woo_content, \WC_Email $wc_email ): ?string {
 		$subject   = $wc_email->get_subject(); // We will get subject from $email_post after we add it to the editor.
 		$preheader = ''; // We will get the preheader from $email_post after we add it to the editor.
 		try {
 			$this->personalizer->set_context( $this->prepare_context_data( $wc_email ) );
 			$rendered_email_data = $this->renderer->render( $email_post, $subject, $preheader, 'en' );
 			$personalized_email  = $this->personalizer->personalize_content( $rendered_email_data['html'] );
-			$rendered_email      = str_replace( self::WOO_EMAIL_CONTENT_PLACEHOLDER, $this->prepare_woo_content( $woo_content ), $personalized_email );
+			$rendered_email      = str_replace( self::WOO_EMAIL_CONTENT_PLACEHOLDER, $woo_content, $personalized_email );
 			add_filter( 'woocommerce_email_styles', array( $this, 'prepare_css' ), 10, 2 );
 			return $rendered_email;
 		} catch ( \Exception $e ) {
@@ -132,11 +148,48 @@ class BlockEmailRenderer {
 	public function prepare_css( string $css, \WC_Email $wc_email ): string {
 		remove_filter( 'woocommerce_email_styles', array( $this, 'prepare_css' ) );
 		$editor_css = $this->theme_controller->get_stylesheet_for_rendering();
-		
+
 		// Remove color and font-family declarations from WooCommerce CSS.
 		$css = preg_replace('/color\s*:\s*[^;]+;/', '', $css);
 		$css = preg_replace('/font-family\s*:\s*[^;]+;/', '', $css);
 
 		return $css . "\n" . $editor_css;
+	}
+
+	/**
+	 * Capture the WooCommerce content excluding headers and footers.
+	 *
+	 * @param \WC_Email $wc_email WooCommerce email.
+	 * @return string
+	 */
+	private function capture_woo_content( \WC_Email $wc_email ): string {
+		// Store the existing header and footer callbacks.
+		global $wp_filter;
+		$original_header_filters = isset( $wp_filter['woocommerce_email_header'] ) ? clone $wp_filter['woocommerce_email_header'] : null;
+		$original_footer_filters = isset( $wp_filter['woocommerce_email_footer'] ) ? clone $wp_filter['woocommerce_email_footer'] : null;
+
+		// Remove header and footer filters because we want to get only the main content.
+		remove_all_filters( 'woocommerce_email_header' );
+		remove_all_filters( 'woocommerce_email_footer' );
+
+		$woo_content = $wc_email->get_content_html();
+
+		// Restore the original header and footer filters.
+		if ( $original_header_filters ) {
+			foreach ( $original_header_filters->callbacks as $priority => $callbacks ) {
+				foreach ( $callbacks as $filter ) {
+					add_filter( 'woocommerce_email_header', $filter['function'], $priority, $filter['accepted_args'] );
+				}
+			}
+		}
+		if ( $original_footer_filters ) {
+			foreach ( $original_footer_filters->callbacks as $priority => $callbacks ) {
+				foreach ( $callbacks as $filter ) {
+					add_filter( 'woocommerce_email_footer', $filter['function'], $priority, $filter['accepted_args'] );
+				}
+			}
+		}
+
+		return $woo_content;
 	}
 }
