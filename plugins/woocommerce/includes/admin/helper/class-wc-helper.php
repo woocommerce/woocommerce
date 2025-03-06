65e398efe6ab4d78c10fa007e59cf1a6d9dc4252
raicem <cem.unalan@automattic.com>
2025-03-06 11:54:35 +0300

    Helper: flush cache earlier when activating a plugin

--- plugins/woocommerce/includes/admin/helper/class-wc-helper.php
+++ plugins/woocommerce/includes/admin/helper/class-wc-helper.php
@@ -2199,6 +2199,9 @@ class WC_Helper {
 		$product_id   = $plugin['_product_id'];
 		$subscription = self::get_available_subscription( $product_id );
 
+		self::_flush_subscriptions_cache();
+		self::_flush_updates_cache();
+
 		// No valid subscription found.
 		if ( ! $subscription ) {
 			return;
@@ -2231,9 +2234,6 @@ class WC_Helper {
 			 */
 			do_action( 'woocommerce_helper_subscription_activate_error', $product_id, $product_key, $activation_response );
 		}
-
-		self::_flush_subscriptions_cache();
-		self::_flush_updates_cache();
 	}
 
 	/**
