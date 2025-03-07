c494cb187e5a3fe4172577ca5aed2d561f5bd1a6
Maikel Perez <mdperez86@gmail.com>
2025-03-07 10:08:00 -0500

    Setup the attribute selector as a dropdown with its own store

--- plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/variation-selector/attribute-options/frontend.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/variation-selector/attribute-options/frontend.ts
@@ -12,109 +12,144 @@ type Context = {
 	selected: string | null;
 	option: Option;
 	options: Option[];
+};
+
+type PillsContext = Context & {
 	isSelected?: boolean;
 	tabIndex?: number;
 };
 
-store( 'woocommerce/add-to-cart-with-options', {
-	state: {},
-	actions: {
-		handleClick() {
-			const context = getContext< Context >();
-			if ( context.selected === context.option.value ) {
-				context.selected = null;
-			} else {
+store(
+	'woocommerce/add-to-cart-with-options-variation-selector-attribute-options__pills',
+	{
+		state: {},
+		actions: {
+			handleClick() {
+				const context = getContext< PillsContext >();
+				if ( context.selected === context.option.value ) {
+					context.selected = null;
+				} else {
+					context.selected = context.option.value;
+				}
+			},
+			handleKeyDown( event: KeyboardEvent ) {
+				const context = getContext< PillsContext >();
+
+				const target = event.currentTarget as HTMLDivElement;
+				let flag = false;
+
+				switch ( event.key ) {
+					case ' ':
+						if ( context.selected ) {
+							context.selected = null;
+						} else {
+							context.selected = context.option.value;
+						}
+						flag = true;
+						break;
+
+					case 'Up':
+					case 'ArrowUp':
+					case 'Left':
+					case 'ArrowLeft': {
+						const index = context.options.findIndex(
+							( option ) => option.value === context.option.value
+						);
+						if ( index === -1 ) return;
+						if ( index > 0 ) {
+							const prevSibling =
+								target.previousElementSibling as HTMLDivElement;
+							prevSibling?.focus();
+
+							context.selected =
+								context.options[ index - 1 ].value;
+						} else {
+							const lastSibling = target.parentElement
+								?.lastElementChild as HTMLDivElement;
+							lastSibling?.focus();
+
+							context.selected =
+								context.options[
+									context.options.length - 1
+								].value;
+						}
+						flag = true;
+						break;
+					}
+
+					case 'Down':
+					case 'ArrowDown':
+					case 'Right':
+					case 'ArrowRight': {
+						const index = context.options.findIndex(
+							( option ) => option.value === context.option.value
+						);
+						if ( index === -1 ) return;
+						if ( index < context.options.length - 1 ) {
+							const nextSibling =
+								target.nextElementSibling as HTMLDivElement;
+							nextSibling?.focus();
+
+							context.selected =
+								context.options[ index + 1 ].value;
+						} else {
+							const firstSibling = target.parentElement
+								?.firstElementChild as HTMLDivElement;
+							firstSibling?.focus();
+
+							context.selected = context.options[ 0 ].value;
+						}
+						flag = true;
+						break;
+					}
+					default:
+						break;
+				}
+
+				if ( flag ) {
+					event.stopPropagation();
+					event.preventDefault();
+				}
+			},
+		},
+		callbacks: {
+			watchSelected() {
+				const context = getContext< PillsContext >();
+				context.isSelected = context.selected === context.option.value;
+				if ( context.selected ) {
+					context.tabIndex = context.isSelected ? 0 : -1;
+				} else {
+					const index = context.options.findIndex(
+						( option ) => option.value === context.option.value
+					);
+					context.tabIndex = index === 0 ? 0 : -1;
+				}
+			},
+		},
+	}
+);
+
+type DropdownContext = Context & {
+	isSelected: 'selected' | undefined;
+};
+
+store(
+	'woocommerce/add-to-cart-with-options-variation-selector-attribute-options__dropdown',
+	{
+		state: {},
+		actions: {
+			handleChange() {
+				const context = getContext< DropdownContext >();
 				context.selected = context.option.value;
-			}
+			},
 		},
-		handleKeyDown( event: KeyboardEvent ) {
-			const context = getContext< Context >();
-
-			const target = event.currentTarget as HTMLDivElement;
-			let flag = false;
-
-			switch ( event.key ) {
-				case ' ':
-					if ( context.selected ) {
-						context.selected = null;
-					} else {
-						context.selected = context.option.value;
-					}
-					flag = true;
-					break;
-
-				case 'Up':
-				case 'ArrowUp':
-				case 'Left':
-				case 'ArrowLeft': {
-					const index = context.options.findIndex(
-						( option ) => option.value === context.option.value
-					);
-					if ( index === -1 ) return;
-					if ( index > 0 ) {
-						const prevSibling =
-							target.previousElementSibling as HTMLDivElement;
-						prevSibling?.focus();
-
-						context.selected = context.options[ index - 1 ].value;
-					} else {
-						const lastSibling = target.parentElement
-							?.lastElementChild as HTMLDivElement;
-						lastSibling?.focus();
-
-						context.selected =
-							context.options[ context.options.length - 1 ].value;
-					}
-					flag = true;
-					break;
+		callbacks: {
+			init() {
+				const context = getContext< DropdownContext >();
+				if ( context.selected === context.option.value ) {
+					context.isSelected = 'selected';
 				}
-
-				case 'Down':
-				case 'ArrowDown':
-				case 'Right':
-				case 'ArrowRight': {
-					const index = context.options.findIndex(
-						( option ) => option.value === context.option.value
-					);
-					if ( index === -1 ) return;
-					if ( index < context.options.length - 1 ) {
-						const nextSibling =
-							target.nextElementSibling as HTMLDivElement;
-						nextSibling?.focus();
-
-						context.selected = context.options[ index + 1 ].value;
-					} else {
-						const firstSibling = target.parentElement
-							?.firstElementChild as HTMLDivElement;
-						firstSibling?.focus();
-
-						context.selected = context.options[ 0 ].value;
-					}
-					flag = true;
-					break;
-				}
-				default:
-					break;
-			}
-
-			if ( flag ) {
-				event.stopPropagation();
-				event.preventDefault();
-			}
+			},
 		},
-	},
-	callbacks: {
-		watchSelected() {
-			const context = getContext< Context >();
-			context.isSelected = context.selected === context.option.value;
-			if ( context.selected ) {
-				context.tabIndex = context.isSelected ? 0 : -1;
-			} else {
-				const index = context.options.findIndex(
-					( option ) => option.value === context.option.value
-				);
-				context.tabIndex = index === 0 ? 0 : -1;
-			}
-		},
-	},
-} );
+	}
+);
--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptionsVariationSelectorAttributeOptions.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptionsVariationSelectorAttributeOptions.php
@@ -54,22 +54,10 @@ class AddToCartWithOptionsVariationSelectorAttributeOptions extends AbstractBloc
 	
 			$field_style = $attributes['style'];
 
-			$classes = implode(
-				' ',
-				array_filter(
-					array(
-						'dropdown' === $field_style ?
-							'wc-block-add-to-cart-with-options-variation-selector-attribute-options__dropdown' :
-							'wc-block-add-to-cart-with-options-variation-selector-attribute-options__pills',
-						esc_attr( $classes_and_styles['classes'] ),
-					)
-				)
-			);
-
 			$wrapper_attributes = get_block_wrapper_attributes(
 				array(
 					'data-wp-interactive' => 'woocommerce/add-to-cart-with-options',
-					'class' => $classes,
+					'class' => esc_attr( $classes_and_styles['classes'] ),
 					'style' => esc_attr( $classes_and_styles['styles'] ),
 				)
 			);
@@ -103,7 +91,13 @@ class AddToCartWithOptionsVariationSelectorAttributeOptions extends AbstractBloc
 		$merged_attributes = array_merge( $default_attributes, $attributes );
 
 		foreach ( $merged_attributes as $key => $value ) {
-			$normalized_attributes[] = $key . '="' . esc_attr( $value ) . '"';
+			if ( is_array( $value ) || is_object( $value ) ) {
+				$value = wp_json_encode(
+					$value,
+					JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
+				);
+			}
+			$normalized_attributes[] = sprintf( '%s="%s"', esc_attr( $key ), esc_attr( $value ) );
 		}
 
 		return implode( ' ', $normalized_attributes );
@@ -147,16 +141,14 @@ class AddToCartWithOptionsVariationSelectorAttributeOptions extends AbstractBloc
 			</div>',
 			$this->get_normalized_attributes(
 				array(
-					'role'            => "radiogroup",
-					'class'           => 'wc-block-add-to-cart-with-options-variation-selector-attribute-options__pills',
-					'id'              => esc_attr( 'attribute_' . sanitize_title( $attribute_name ) ),
-					'aria-labeledby'  => esc_attr( 'attribute_' . sanitize_title( $attribute_name ) . '_label' ),
-					'data-wp-context' => wp_json_encode(
-						array(
-							'options'  => $attribute_terms,
-							'selected' => $this->get_default_selected_attribute(),
-						),
-						JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
+					'role'                => "radiogroup",
+					'class'               => 'wc-block-add-to-cart-with-options-variation-selector-attribute-options__pills',
+					'id'                  => 'attribute_' . sanitize_title( $attribute_name ),
+					'aria-labeledby'      => 'attribute_' . sanitize_title( $attribute_name ) . '_label',
+					'data-wp-interactive' => $this->get_full_block_name() . '__pills',
+					'data-wp-context'     => array(
+						'options'  => $attribute_terms,
+						'selected' => $this->get_default_selected_attribute(),
 					),
 				),
 			),
@@ -170,12 +162,9 @@ class AddToCartWithOptionsVariationSelectorAttributeOptions extends AbstractBloc
 					'data-wp-watch'              => "callbacks.watchSelected",
 					'data-wp-on--click'          => "actions.handleClick",
 					'data-wp-on--keydown'        => "actions.handleKeyDown",
-					'data-wp-context'            => wp_json_encode(
-						array(
-							'isSelected' => false,
-							'tabIndex'   => -1,
-						),
-						JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
+					'data-wp-context'            => array(
+						'isSelected' => false,
+						'tabIndex'   => -1,
 					),
 				),
 			),
@@ -185,87 +174,47 @@ class AddToCartWithOptionsVariationSelectorAttributeOptions extends AbstractBloc
 	/**
 	 * Render the attribute options as a dropdown.
 	 *
-	 * @param array $wrapper_attributes The wrapper attributes.
+	 * @param array $attributes The block's attributes.
 	 * @return string The dropdown.
 	 */
-	protected function render_dropdown( $wrapper_attributes ) {
-		global $product;
+	protected function render_dropdown( $attributes ) {
 		global $attribute_name;
 		global $attribute_terms;
 
-		$options = sprintf(
-			'<option value="">%s</option>',
-			esc_html__( 'Choose an option', 'woocommerce' )
+		$options = array_merge(
+			array( array( 'label' => esc_html__( 'Choose an option', 'woocommerce' ), 'value' => '' ) ),
+			$attribute_terms
 		);
 
-		$selected = $product->get_variation_default_attribute( $attribute_name );
-
-		$options .= $this->render_attribute_options( $product, $attribute_name, $attribute_terms, $selected, taxonomy_exists( $attribute_name ) );
-		
 		return sprintf(
-			'<select %s data-attribute_name="attribute_%s">%s</select>',
-			$wrapper_attributes,
-			esc_attr( sanitize_title( $attribute_name ) ),
-			$options
+			'<select %s>
+				<template data-wp-each--option="context.options">
+        	<option %s></option>
+    		</template>
+			</select>',
+			$this->get_normalized_attributes(
+				array(
+					'class'               => 'wc-block-add-to-cart-with-options-variation-selector-attribute-options__dropdown',
+					'id'                  => 'attribute_' . sanitize_title( $attribute_name ),
+					'data-wp-interactive' => $this->get_full_block_name() . '__dropdown',
+					'data-wp-context'     => array(
+						'options'  => $options,
+						'selected' => $this->get_default_selected_attribute(),
+					),
+				),
+			),
+			$this->get_normalized_attributes(
+				array(
+					'data-wp-text'           => "context.option.label",
+					'data-wp-bind--value'    => "context.option.value",
+					'data-wp-bind--selected' => "context.isSelected",
+					'data-wp-init'           => "callbacks.init",
+					'data-wp-on--change'     => "actions.handleChange",
+					'data-wp-context'        => array(
+						'isSelected' => null,
+					),
+				),
+			),
 		);
-	}
-
-	/**
-	 * Get HTML for attribute options.
-	 *
-	 * @param WC_Product $product The product object.
-	 * @param string     $attribute_name Name of the attribute.
-	 * @param array      $options Available options.
-	 * @param string     $selected Selected value.
-	 * @param bool       $is_taxonomy Whether this is a taxonomy-based attribute.
-	 * @return string Options HTML
-	 */
-	private function render_attribute_options( $product, $attribute_name, $options, $selected, $is_taxonomy ): string {
-		if ( empty( $options ) ) {
-			return '';
-		}
-
-		$html  = '';
-		$items = $is_taxonomy
-			? wc_get_product_terms( $product->get_id(), $attribute_name, array( 'fields' => 'all' ) )
-			: $options;
-
-		foreach ( $items as $item ) {
-			$option_value = $is_taxonomy ? $item->slug : $item;
-			$option_label = $is_taxonomy ? $item->name : $item;
-
-			if ( ! $is_taxonomy || in_array( $option_value, $options, true ) ) {
-				$selected_attr = $is_taxonomy
-					? selected( sanitize_title( $selected ), $option_value, false )
-					: selected( $selected, $option_value, false );
-
-				/**
-				 * Filter the variation option name.
-				 *
-				 * @since 9.7.0
-				 *
-				 * @param string     $option_label    The option label.
-				 * @param WP_Term|string|null $item   Term object for taxonomies, option string for custom attributes.
-				 * @param string     $attribute_name  Name of the attribute.
-				 * @param WC_Product $product         Product object.
-				 */
-				$filtered_label = apply_filters(
-					'woocommerce_variation_option_name',
-					$option_label,
-					$is_taxonomy ? $item : null,
-					$attribute_name,
-					$product
-				);
-
-				$html .= sprintf(
-					'<option value="%s" %s>%s</option>',
-					esc_attr( $option_value ),
-					$selected_attr,
-					esc_html( $filtered_label )
-				);
-			}
-		}
-
-		return $html;
 	}
 }
