5bfdae1450d8bdb166148dcb45d2592247ba4825
Lourens Schep <lourensschep@gmail.com>
2025-03-11 09:57:05 +0100

    Add HTML Element inspector control

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-reviews/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-reviews/edit.tsx
@@ -1,15 +1,58 @@
 /**
  * External dependencies
  */
-import { InnerBlocks } from '@wordpress/block-editor';
+import { SelectControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import {
+	InspectorControls,
+	useBlockProps,
+	// @ts-expect-error missing types.
+	useInnerBlocksProps,
+} from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
 import TEMPLATE from './template';
+import { ProductReviewsEditProps } from './types';
+import { htmlElementMessages } from '../../utils/messages';
 
-const Edit = () => {
-	return <InnerBlocks template={ TEMPLATE } />;
+const Edit = ( { attributes, setAttributes }: ProductReviewsEditProps ) => {
+	const { tagName: TagName } = attributes;
+	const blockProps = useBlockProps();
+	const innerBlocksProps = useInnerBlocksProps( blockProps, {
+		template: TEMPLATE,
+	} );
+
+	return (
+		<>
+			<InspectorControls>
+				{ /* @ts-expect-error missing types */ }
+				<InspectorControls group="advanced">
+					<SelectControl
+						// @ts-expect-error missing types.
+						__nextHasNoMarginBottom
+						__next40pxDefaultSize
+						label={ __( 'HTML element', 'woocommerce' ) }
+						options={ [
+							{
+								label: __( 'Default (<div>)', 'woocommerce' ),
+								value: 'div',
+							},
+							{ label: '<section>', value: 'section' },
+							{ label: '<aside>', value: 'aside' },
+						] }
+						value={ TagName }
+						onChange={ ( value: 'div' | 'section' | 'aside' ) =>
+							setAttributes( { tagName: value } )
+						}
+						help={ htmlElementMessages[ TagName ] }
+					/>
+				</InspectorControls>
+			</InspectorControls>
+			<TagName { ...innerBlocksProps } />;
+		</>
+	);
 };
 
 export default Edit;
