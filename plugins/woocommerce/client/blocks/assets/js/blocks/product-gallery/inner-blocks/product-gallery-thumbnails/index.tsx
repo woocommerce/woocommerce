a8417758b6e0429327d24214ad16b7435444c0a9
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-03-12 16:40:39 +0100

    Add new thumbnails size attribute

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/index.tsx
@@ -17,4 +17,23 @@ registerBlockType( metadata, {
 	save() {
 		return null;
 	},
+	deprecated: [
+		{
+			attributes: {
+				numberOfThumbnails: {
+					type: 'number',
+					default: 3,
+				},
+			},
+
+			migrate( { numberOfThumbnails } ) {
+				// Some arbitrary values to preserve the aspect ratio more or less.
+				// - 33% for 3 thumbnails
+				// - 12.5% for 8 thumbnails
+				return {
+					thumbnailSize: `${ 100 / numberOfThumbnails }%`,
+				};
+			},
+		},
+	],
 } );
