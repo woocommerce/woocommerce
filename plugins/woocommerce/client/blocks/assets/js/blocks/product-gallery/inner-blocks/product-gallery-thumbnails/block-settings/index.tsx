6f5e11daa6baa38b1338ca7b5cca68c95b51f9a4
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-03-14 16:36:12 +0100

    Update plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/block-settings/index.tsx
    Co-authored-by: Harris Papazoglou <hrrsppzgl@gmail.com>

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/block-settings/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/block-settings/index.tsx
@@ -36,7 +36,7 @@ export const ProductGalleryThumbnailsBlockSettings = ( {
 			size="default"
 			__next36pxDefaultSize
 			help={ __(
-				'Choose the size of each thumbnail. If thumbnails size is bigger than the product image, thumbnails will turn to slider.',
+				'Choose the size of each thumbnail in respect to the product image. If thumbnails container size gets bigger than the product image, thumbnails will turn to slider.',
 				'woocommerce'
 			) }
 		/>
