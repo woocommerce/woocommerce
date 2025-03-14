3c7844ccc45c1cd0c38bc43ccf259dc0ced4255c
Timur Karimov <timur.karimow.95@gmail.com>
2025-03-14 08:23:11 +0100

    fix linting

--- plugins/woocommerce/includes/wc-core-functions.php
+++ plugins/woocommerce/includes/wc-core-functions.php
@@ -37,9 +37,9 @@ require WC_ABSPATH . 'includes/wc-webhook-functions.php';
 /**
  * Filters on data used in admin and frontend.
  */
-foreach ( array('woocommerce_coupon_code', 'woocommerce_coupon_code_without_lower') as $filter ) {
-    add_filter( $filter, 'html_entity_decode' );
-    add_filter( $filter, 'wc_sanitize_coupon_code' );
+foreach ( array( 'woocommerce_coupon_code', 'woocommerce_coupon_code_without_lower' ) as $filter ) {
+	add_filter( $filter, 'html_entity_decode' );
+	add_filter( $filter, 'wc_sanitize_coupon_code' );
 }
 add_filter( 'woocommerce_coupon_code', 'wc_strtolower' );
 add_filter( 'woocommerce_stock_amount', 'intval' ); // Stock amounts are integers by default.
--- plugins/woocommerce/includes/wc-formatting-functions.php
+++ plugins/woocommerce/includes/wc-formatting-functions.php
@@ -359,7 +359,7 @@ function wc_format_localized_decimal( $value ) {
 
 /**
  * Format a coupon code.
- * 
+ *
  * Either format the coupon code with transforming to lowercase or skip the lowercase conversion.
  * Skipping the lowercase conversion is useful when coupon codes contain non-ASCII characters (like 'Å', 'Ö', 'Ü')
  * which can be negatively affected by the lowercase conversion, especially on sites using latin1 charset.
@@ -371,7 +371,9 @@ function wc_format_localized_decimal( $value ) {
  */
 function wc_format_coupon_code( $value, $skip_lowercase = false ) {
 	if ( $skip_lowercase ) {
+		/* phpcs:disable WooCommerce.Commenting.CommentHooks.MissingHookComment */
 		return apply_filters( 'woocommerce_coupon_code_without_lower', $value );
+		/* phpcs:enable */
 	}
 	return apply_filters( 'woocommerce_coupon_code', $value );
 }
