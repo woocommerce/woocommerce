44291df32daef31c5580d80789513b27b50e4348
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-03-14 14:47:29 +0100

    Remove migration logic

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/index.tsx
@@ -18,24 +18,4 @@ registerBlockType( metadata, {
 	save() {
 		return null;
 	},
-	// TODO: Decide about the migration strategy.
-	// deprecated: [
-	// 	{
-	// 		attributes: {
-	// 			numberOfThumbnails: {
-	// 				type: 'number',
-	// 				default: 3,
-	// 			},
-	// 		},
-
-	// 		migrate( { numberOfThumbnails } ) {
-	// 			// Some arbitrary values to preserve the aspect ratio more or less.
-	// 			// - 33% for 3 thumbnails
-	// 			// - 12.5% for 8 thumbnails
-	// 			return {
-	// 				thumbnailSize: `${ 100 / numberOfThumbnails }%`,
-	// 			};
-	// 		},
-	// 	},
-	// ],
 } );
