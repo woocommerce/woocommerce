206703b58895f6333f87389b8dc2a2089ac33c19
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2025-03-15 12:39:53 +0000

    Optimized for performance by using str_replace instead

--- plugins/woocommerce/includes/wc-formatting-functions.php
+++ plugins/woocommerce/includes/wc-formatting-functions.php
@@ -1591,27 +1591,26 @@ function wc_sanitize_endpoint_slug( $raw_value ) {
  * @return string The sanitized string without problematic characters.
  * @since 9.9.0
  */
-function wc_remove_non_displayable_chars( string $raw_value ): string {
+function wc_remove_non_displayable_chars( string $string ): string {
+	$remove_chars = [
+		"\u{00AD}", // Soft Hyphen
+		"\u{200B}", // Zero Width Space
+		"\u{200C}", // Zero Width Non-Joiner
+		"\u{200D}", // Zero Width Joiner
+		"\u{200E}", // Left-to-Right Mark
+		"\u{200F}", // Right-to-Left Mark
+		"\u{202A}", // Left-to-Right Embedding
+		"\u{202B}", // Right-to-Left Embedding
+		"\u{202C}", // Pop Directional Formatting
+		"\u{202D}", // Left-to-Right Override
+		"\u{202E}", // Right-to-Left Override
+		"\u{FEFF}", // Byte Order Mark (BOM)
+		"\u{FFF9}", // Interlinear Annotation Anchor
+		"\u{FFFA}", // Interlinear Annotation Separator
+		"\u{FFFB}", // Interlinear Annotation Terminator
+	];
 
-	$remove_chars = array(
-		'\x{00AD}', // Soft Hyphen.
-		'\x{200B}', // Zero Width Space.
-		'\x{200C}', // Zero Width Non-Joiner.
-		'\x{200D}', // Zero Width Joiner.
-		'\x{200E}', // Left-to-Right Mark.
-		'\x{200F}', // Right-to-Left Mark.
-		'\x{202A}', // Left-to-Right Embedding.
-		'\x{202B}', // Right-to-Left Embedding.
-		'\x{202C}', // Pop Directional Formatting.
-		'\x{202D}', // Left-to-Right Override.
-		'\x{202E}', // Right-to-Left Override.
-		'\x{FEFF}', // Byte Order Mark (BOM).
-		'\x{FFF9}', // Interlinear Annotation Anchor.
-		'\x{FFFA}', // Interlinear Annotation Separator.
-		'\x{FFFB}', // Interlinear Annotation Terminator.
-	);
-
-	return preg_replace( '/[' . implode( '', $remove_chars ) . ']/u', '', $raw_value );
+	return str_replace($remove_chars, '', $string);
 }
 
 add_filter( 'woocommerce_admin_settings_sanitize_option_woocommerce_checkout_pay_endpoint', 'wc_sanitize_endpoint_slug', 10, 1 );
