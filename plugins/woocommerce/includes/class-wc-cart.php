3f2624c2c44b82920a6fa7e9611ac83afef2fcc2
Timur Karimov <timur.karimow.95@gmail.com>
2025-03-16 19:59:51 +0100

    stay on a safer side by additionally sanitizing applied coupons before comparison

--- plugins/woocommerce/includes/class-wc-cart.php
+++ plugins/woocommerce/includes/class-wc-cart.php
@@ -1752,7 +1752,9 @@ class WC_Cart extends WC_Legacy_Cart {
 	 * @return bool
 	 */
 	public function has_discount( $coupon_code = '' ) {
-		return $coupon_code ? in_array( wc_strtolower( $coupon_code ), array_map( 'wc_strtolower', $this->applied_coupons ), true ) : count( $this->applied_coupons ) > 0;
+		return $coupon_code ? in_array( wc_strtolower( wc_format_coupon_code( $coupon_code ) ), array_map( function( $code ) {
+			return wc_strtolower( wc_format_coupon_code( $code ) );
+		}, $this->applied_coupons ), true ) : count( $this->applied_coupons ) > 0;
 	}
 
 	/**
@@ -1916,7 +1918,9 @@ class WC_Cart extends WC_Legacy_Cart {
 	 */
 	public function remove_coupon( $coupon_code ) {
 		$coupon_code = wc_format_coupon_code( $coupon_code );
-		$position    = array_search( wc_strtolower( $coupon_code ), array_map( 'wc_strtolower', $this->get_applied_coupons() ), true );
+		$position    = array_search( wc_strtolower( $coupon_code ), array_map( function( $code ) {
+			return wc_strtolower( wc_format_coupon_code( $code ) );
+		}, $this->get_applied_coupons() ), true );
 
 		if ( false !== $position ) {
 			unset( $this->applied_coupons[ $position ] );
