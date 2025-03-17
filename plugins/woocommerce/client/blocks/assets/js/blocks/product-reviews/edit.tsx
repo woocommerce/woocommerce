17804dd23ce13fdfe4b5a4bacdcceac11dbd513a
Lourens Schep <lourensschep@gmail.com>
2025-03-17 13:45:13 +0100

    Update ancestors and show warning if invalid product context

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-reviews/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-reviews/edit.tsx
@@ -1,11 +1,16 @@
 /**
  * External dependencies
  */
+
 import { SelectControl } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import {
 	InspectorControls,
 	useBlockProps,
+	Warning,
+	store as blockEditorStore,
 	// @ts-expect-error missing types.
 	useInnerBlocksProps,
 } from '@wordpress/block-editor';
@@ -17,12 +22,51 @@ import TEMPLATE from './template';
 import { ProductReviewsEditProps } from './types';
 import { htmlElementMessages } from '../../utils/messages';
 
-const Edit = ( { attributes, setAttributes }: ProductReviewsEditProps ) => {
+/**
+ * Check if block is inside a Query Loop with non-product post type
+ *
+ * @param {string} clientId The block's client ID
+ * @param {string} postType The current post type
+ * @return {boolean} Whether the block is in an invalid Query Loop context
+ */
+const useIsInvalidQueryLoopContext = ( clientId: string, postType: string ) => {
+	const { getBlockParentsByBlockName } = useSelect( blockEditorStore, [] );
+	const blockParents = useMemo( () => {
+		return getBlockParentsByBlockName( clientId, 'core/post-template' );
+	}, [ getBlockParentsByBlockName, clientId ] );
+
+	return blockParents.length > 0 && postType !== 'product';
+};
+
+const Edit = ( {
+	attributes,
+	setAttributes,
+	clientId,
+	context,
+}: ProductReviewsEditProps ) => {
 	const { tagName: TagName } = attributes;
 	const blockProps = useBlockProps();
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
 		template: TEMPLATE,
 	} );
+
+	const { postType: contextPostType } = context;
+	const isInvalidQueryLoopContext = useIsInvalidQueryLoopContext(
+		clientId,
+		contextPostType
+	);
+	if ( isInvalidQueryLoopContext ) {
+		return (
+			<div { ...blockProps }>
+				<Warning>
+					{ __(
+						'The Product Reviews block requires a product context. When used in a Query Loop, the Query Loop must be configured to display products.',
+						'woocommerce'
+					) }
+				</Warning>
+			</div>
+		);
+	}
 
 	return (
 		<>
