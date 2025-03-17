4b2bdc3869fe54e36cdd1fee88438ed2ddc7e3e7
paul sealock <psealock@gmail.com>
2025-03-18 12:09:50 +1300

    lint

--- plugins/woocommerce/src/Admin/RemoteInboxNotifications/RemoteInboxNotificationsEngine.php
+++ plugins/woocommerce/src/Admin/RemoteInboxNotifications/RemoteInboxNotificationsEngine.php
@@ -167,7 +167,7 @@ class RemoteInboxNotificationsEngine extends RemoteSpecsEngine {
 	public static function get_stored_state() {
 		$stored_state = get_option( self::STORED_STATE_OPTION_NAME );
 
-		if ( false === $stored_state || ! is_object( $stored_state) ) {
+		if ( false === $stored_state || ! is_object( $stored_state ) ) {
 			$stored_state = new \stdClass();
 
 			$stored_state = StoredStateSetupForProducts::init_stored_state(
