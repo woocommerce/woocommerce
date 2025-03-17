5b75de3160c8cb61a237309ce40257b0ceaaef3c
Vlad Olaru <vlad@pixelgrade.com>
2025-03-17 11:59:42 +0200

    Adjust experiment failure caching logic

--- plugins/woocommerce/src/Internal/Admin/Settings/PaymentProviders/WooPayments.php
+++ plugins/woocommerce/src/Internal/Admin/Settings/PaymentProviders/WooPayments.php
@@ -196,25 +196,28 @@ class WooPayments extends PaymentGateway {
 			$live_onboarding = true;
 		}
 
-		$transient_key = 'wc_experiment_failure_woocommerce_payment_settings_onboarding_2025_v1';
-
-		// Try to get cached result first.
-		$cached_result = get_transient( $transient_key );
-
 		// We run an experiment to determine the efficiency of test-account-first onboarding vs straight-to-live onboarding.
-		// If the experiment is active and the store is in the treatment group, we will do live onboarding.
+		// If the experiment is active and the store is in the treatment group, we will force live onboarding.
 		// Otherwise, we will do test-account-first onboarding (control group).
-		try {
-			// If we have a cache entry that indicates an error, disable the live onboarding.
-			if ( 'error' === $cached_result ) {
-				$live_onboarding = false;
-			} else {
-				$live_onboarding = ! $live_onboarding && Experimental_Abtest::in_treatment( 'woocommerce_payment_settings_onboarding_2025_v1' );
+		// Stores that are determined by our routing logic that they should do straight-to-live onboarding
+		// will not be affected by the experiment.
+		if ( ! $live_onboarding ) {
+			$transient_key = 'wc_experiment_failure_woocommerce_payment_settings_onboarding_2025_v1';
+
+			// Try to get cached result first.
+			$cached_result = get_transient( $transient_key );
+
+			// If we have a cache entry that indicates an error, don't enforce anything. Just let the routing logic decide.
+			if ( 'error' !== $cached_result ) {
+				try {
+					if ( Experimental_Abtest::in_treatment( 'woocommerce_payment_settings_onboarding_2025_v1' ) ) {
+						$live_onboarding = true;
+					}
+				} catch ( \Exception $e ) {
+					// If the experiment fails, set a transient to avoid repeated failures.
+					set_transient( $transient_key, 'error', HOUR_IN_SECONDS );
+				}
 			}
-		} catch ( \Exception $e ) {
-			// If the experiment fails, set a transient to avoid repeated failures and set the flag to false.
-			$live_onboarding = false;
-			set_transient( $transient_key, 'error', HOUR_IN_SECONDS );
 		}
 
 		// If we are doing live onboarding, we don't need to add more to the URL.
