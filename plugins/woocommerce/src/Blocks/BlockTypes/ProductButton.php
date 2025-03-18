fb6f3a1e82c55cda859befa563a1c181fa42c5cf
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-03-18 09:30:48 +0100

    plugins/woocommerce/src/Blocks/BlockTypes/ProductButton.php

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductButton.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductButton.php
@@ -274,14 +274,14 @@ class ProductButton extends AbstractBlock {
 	 */
 	private function initialize_cart_state() {
 		if ( null === self::$cart ) {
-			$cart_items = isset( WC()->cart )
-				? rest_do_request( new \WP_REST_Request( 'GET', '/wc/store/v1/cart/items' ) )->data
+			$cart = isset( WC()->cart )
+				? rest_do_request( new \WP_REST_Request( 'GET', '/wc/store/v1/cart' ) )->data
 				: array();
 
 			wp_interactivity_state(
 				'woocommerce',
 				array(
-					'cart'     => array( 'items' => $cart_items ),
+					'cart'     => $cart,
 					'nonce'    => wp_create_nonce( 'wc_store_api' ),
 					'noticeId' => '',
 					'restUrl'  => get_rest_url(),
