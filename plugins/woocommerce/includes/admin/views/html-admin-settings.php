9ed4c0bc7115c7f344bbe8adec946fbbc4611ad8
moon <moon.kyong@automattic.com>
2025-03-18 12:30:00 -0700

    Remove settings-content div

--- plugins/woocommerce/includes/admin/views/html-admin-settings.php
+++ plugins/woocommerce/includes/admin/views/html-admin-settings.php
@@ -60,7 +60,6 @@ $hide_nav = Features::is_enabled( 'reactify-classic-payments-settings' ) &&
 				?>
 			</nav>
 		<?php endif; ?>
-		<div class="settings-content">
 			<h1 class="screen-reader-text"><?php echo esc_html( $current_tab_label ); ?></h1>
 			<?php
 				do_action( 'woocommerce_sections_' . $current_tab );
@@ -76,7 +75,6 @@ $hide_nav = Features::is_enabled( 'reactify-classic-payments-settings' ) &&
 				<?php endif; ?>
 				<?php wp_nonce_field( 'woocommerce-settings' ); ?>
 			</p>
-		</div>
 	</form>
 	<?php do_action( 'woocommerce_after_settings_' . $current_tab ); ?>
 </div>
