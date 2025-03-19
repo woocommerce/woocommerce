03f0752c6acea97960ff618d9be255b3407d9b2d
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2025-03-19 16:29:28 -0700

    Appease the linter

--- plugins/woocommerce/tests/php/includes/wc-term-functions-tests.php
+++ plugins/woocommerce/tests/php/includes/wc-term-functions-tests.php
@@ -1,4 +1,5 @@
 <?php
+declare( strict_types = 1 );
 
 use Automattic\WooCommerce\Enums\ProductStockStatus;
 
@@ -24,12 +25,12 @@ class WC_Term_Functions_Tests extends \WC_Unit_Test_Case {
 	public function setUp(): void {
 		parent::setUp();
 
-		$this->terms['parent']        = wp_insert_term( 'Parent term', 'product_cat' );
-		$this->terms['child1']        = wp_insert_term( 'Child term 1', 'product_cat', array( 'parent' => $this->terms['parent']['term_id'] ) );
-		$this->terms['child2']        = wp_insert_term( 'Child term 2', 'product_cat', array( 'parent' => $this->terms['parent']['term_id'] ) );
+		$this->terms['parent'] = wp_insert_term( 'Parent term', 'product_cat' );
+		$this->terms['child1'] = wp_insert_term( 'Child term 1', 'product_cat', array( 'parent' => $this->terms['parent']['term_id'] ) );
+		$this->terms['child2'] = wp_insert_term( 'Child term 2', 'product_cat', array( 'parent' => $this->terms['parent']['term_id'] ) );
 
-		$this->terms['tag1']          = wp_insert_term( 'Tag 1', 'product_tag' );
-		$this->terms['tag2']          = wp_insert_term( 'Tag 2', 'product_tag' );
+		$this->terms['tag1'] = wp_insert_term( 'Tag 1', 'product_tag' );
+		$this->terms['tag2'] = wp_insert_term( 'Tag 2', 'product_tag' );
 
 		$this->products['product1'] = WC_Helper_Product::create_simple_product(
 			true,
@@ -78,10 +79,12 @@ class WC_Term_Functions_Tests extends \WC_Unit_Test_Case {
 	 * @testdox Term product counts with default settings.
 	 */
 	public function test_term_count_baseline(): void {
-		$terms = get_terms( array(
-			'taxonomy'   => array( 'product_cat', 'product_tag' ),
-			'hide_empty' => false,
-		) );
+		$terms       = get_terms(
+			array(
+				'taxonomy'   => array( 'product_cat', 'product_tag' ),
+				'hide_empty' => false,
+			)
+		);
 		$term_counts = wp_list_pluck( $terms, 'count', 'term_id' );
 
 		$this->assertEquals( 3, $term_counts[ $this->terms['parent']['term_id'] ] );
@@ -101,10 +104,12 @@ class WC_Term_Functions_Tests extends \WC_Unit_Test_Case {
 		wc_recount_all_terms();
 		delete_transient( 'wc_term_counts' );
 
-		$terms = get_terms( array(
-			'taxonomy'   => array( 'product_cat', 'product_tag' ),
-			'hide_empty' => false,
-		) );
+		$terms       = get_terms(
+			array(
+				'taxonomy'   => array( 'product_cat', 'product_tag' ),
+				'hide_empty' => false,
+			)
+		);
 		$term_counts = wp_list_pluck( $terms, 'count', 'term_id' );
 
 		$this->assertEquals( 2, $term_counts[ $this->terms['parent']['term_id'] ] );
@@ -123,10 +128,12 @@ class WC_Term_Functions_Tests extends \WC_Unit_Test_Case {
 		wc_recount_all_terms();
 		delete_transient( 'wc_term_counts' );
 
-		$terms = get_terms( array(
-			'taxonomy'   => array( 'product_cat', 'product_tag' ),
-			'hide_empty' => false,
-		) );
+		$terms       = get_terms(
+			array(
+				'taxonomy'   => array( 'product_cat', 'product_tag' ),
+				'hide_empty' => false,
+			)
+		);
 		$term_counts = wp_list_pluck( $terms, 'count', 'term_id' );
 
 		$this->assertEquals( 2, $term_counts[ $this->terms['parent']['term_id'] ] );
