4eaa5d3da5e54f686f2962771a3054e1a3a312c2
Oluwaseun Olorunsola <seun.olorunsola@automattic.com>
2025-03-19 01:46:03 +0100

    Refactor email template generation to utilize core transactional emails
    - Updated the WCEmailTemplateGenerator to dynamically initialize default email templates based on core transactional emails.
    - Implemented a method to retrieve email templates, enhancing maintainability and reducing hardcoded values.
    - Improved the generation of initial email templates by checking for existing templates and only creating those that are missing.
    - Added detailed documentation for new methods to clarify their purpose and usage.

--- plugins/woocommerce/src/Internal/EmailEditor/WCTransactionalEmails/WCEmailTemplateGenerator.php
+++ plugins/woocommerce/src/Internal/EmailEditor/WCTransactionalEmails/WCEmailTemplateGenerator.php
@@ -14,60 +14,145 @@ class WCEmailTemplateGenerator {
 	 * @var WCEmailTemplateManager
 	 */
 	private $template_manager;
+
+	/**
+	 * Default templates.
+	 * @var array
+	 */
 	private $default_templates = [];
 
 	public function __construct() {
 		$this->template_manager = WCEmailTemplateManager::get_instance();
 	}
 
+	/**
+	 * Initialize the email template generator.
+	 *
+	 * This function initializes the email template generator by loading the default templates and generating initial email templates if needed.
+	 */
 	public function init() {
 		$this->init_default_templates();
 		$this->generate_initial_email_templates();
 	}
 
+	/**
+	 * Initialize the default templates.
+	 *
+	 * This function initializes the default templates for the core transactional emails.
+	 * It fetches all the emails from WooCommerce and filters them to include only the core transactional emails.
+	 *
+	 */
 	private function init_default_templates() {
-		$this->default_templates = [
-			'new_order' => [
-				'title' => __('New Order', 'woocommerce'),
-				'content' => $this->get_new_order_template(),
-				'enabled' => true
-			],
-			'customer_processing_order' => [
-				'title' => __('Processing Order', 'woocommerce'),
-				'content' => $this->get_processing_order_template(),
-				'enabled' => true
-			],
-			'customer_note' => [
-				'title' => __('Customer Note', 'woocommerce'),
-				'content' => $this->get_customer_note_template(),
-				'enabled' => true
-			],
-			// other email types...
-		];
+		$core_transactional_emails = WCTransactionalEmails::$core_transactional_emails;
+
+		$wc_emails = \WC_Emails::instance();
+		$email_types = $wc_emails->get_emails();
+		$email_types = array_filter( $email_types, function( $email ) use ( $core_transactional_emails ) {
+			return in_array( $email->id, $core_transactional_emails );
+		} );
+
+		$this->default_templates = array_reduce( $email_types, function( $acc, $email ) {
+			$acc[ $email->id ] = [
+				'title' => $email->title,
+				'content' => $this->get_email_template( $email ),
+				'enabled' => $email->is_enabled(),
+			];
+			return $acc;
+		}, [] );
 	}
 
+	/**
+	 * Get the email template for the given email.
+	 *
+	 * This is a temporary solution to get the initial email block content.
+	 *
+	 * @param \WC_Email $email The email object.
+	 * @return string The email template.
+	 */
+	public function get_email_template( $email ) {
+		return wc_get_template_html(
+			 str_replace('plain', 'block', $email->template_plain),
+			array(
+				'order'              => $email->object,
+				'sent_to_admin'      => true,
+				'plain_text'         => false,
+				'email'              => $email,
+			)
+		);
+	}
+
+	/**
+	 * Generate initial email templates.
+	 *
+	 * This function generates the initial email templates for the core transactional emails.
+	 * It checks if the templates are already generated and if not, it generates them.
+	 *
+	 * @return bool True if the templates are generated, false otherwise.
+	 */
 	public function generate_initial_email_templates() {
-		if ( empty( $this->template_manager->get_email_template_post_id( 'new_order' ) ) ) {
-			return $this->generate_email_templates();
+
+		if ( get_option( 'wc_email_editor_initial_templates_generated' ) ) {
+			// if templates are already generated, we don't need to run this function again
+			return true;
 		}
 
+		$core_transactional_emails = WCTransactionalEmails::$core_transactional_emails;
+
+		$templates_to_generate = [];
+		foreach ( $core_transactional_emails as $email_type ) {
+			if ( empty( $this->template_manager->get_email_template_post_id( $email_type ) ) ) {
+				$templates_to_generate[] = $email_type;
+			}
+		}
+
+		if ( ! empty( $templates_to_generate ) ) {
+			return $this->generate_email_templates( $templates_to_generate );
+		}
+
+		update_option( 'wc_email_editor_initial_templates_generated', true );
 		return true;
 	}
 
+	/**
+	 * Generate email template if it doesn't exist.
+	 *
+	 * This function generates an email template if it doesn't exist.
+	 *
+	 * @param string $email_type The email type.
+	 */
 	public function generate_email_template_if_not_exists( $email_type ) {
-		if ( ! $this->template_manager->template_exists( $email_type ) ) {
-			$this->generate_single_template( $email_type, $this->default_templates[ $email_type ] );
+		$template_data = $this->default_templates[ $email_type ];
+
+		if ( $this->template_manager->get_email_template_post_id( $email_type ) || empty( $template_data ) ) {
+			return true;
 		}
+
+		return $this->generate_single_template( $email_type, $template_data );
 	}
 
-	public function generate_email_templates() {
+	/**
+	 * Generate email templates.
+	 *
+	 * This function generates the email templates for the given email types.
+	 *
+	 * @param array $templates_to_generate The email types to generate.
+	 */
+	public function generate_email_templates( $templates_to_generate ) {
 		global $wpdb;
+
+		$templates = array_filter( $this->default_templates, function( $email_template_id ) use ( $templates_to_generate ) {
+			return in_array( $email_template_id, $templates_to_generate );
+		}, ARRAY_FILTER_USE_KEY);
+
+		if ( empty( $templates ) ) {
+			return true;
+		}
 
 		// Start transaction
 		$wpdb->query('START TRANSACTION');
 
 		try {
-			foreach ($this->default_templates as $email_type => $template) {
+			foreach ( $templates as $email_type => $template) {
 				$this->generate_single_template($email_type, $template);
 			}
 
@@ -80,15 +165,19 @@ class WCEmailTemplateGenerator {
 		}
 	}
 
+	/**
+	 * Generate a single email template.
+	 *
+	 * This function generates a single email template post and sets its postmeta association.
+	 *
+	 * @param string $email_type The email type.
+	 * @param array $template_data The template data.
+	 * @return int The post ID of the generated template.
+	 */
 	private function generate_single_template($email_type, $template_data) {
-		// Skip if template already exists
-		if ($this->template_manager->template_exists($email_type)) {
-			return;
-		}
-
 		$post_data = [
 			'post_type' => Integration::EMAIL_POST_TYPE,
-			'post_status' => 'publish',
+			'post_status' => $template_data['enabled'] ? 'publish' : 'draft',
 			'post_name' => $email_type,
 			'post_title' => $template_data['title'],
 			'post_content' => $template_data['content'],
@@ -108,43 +197,5 @@ class WCEmailTemplateGenerator {
 		$this->template_manager->save_email_template_post_id( $email_type, $post_id );
 
 		return $post_id;
-	}
-
-	private function get_new_order_template() {
-		return '<!-- wp:heading -->
-		<h2>' . __('New Order Received', 'woocommerce') . '</h2>
-		<!-- /wp:heading -->
-
-		<!-- wp:paragraph -->
-		<p>' . __('You have received a new order.', 'woocommerce') . '</p>
-		<!-- /wp:paragraph -->
-
-		<!-- wp:woo/email-content {"lock":{"move":false,"remove":true}} -->
-		<div class="wp-block-woo-email-content">' . BlockEmailRenderer::WOO_EMAIL_CONTENT_PLACEHOLDER . '</div>
-		<!-- /wp:woo/email-content -->';
-	}
-
-	private function get_processing_order_template() {
-		return '<!-- wp:heading -->
-		<h2>' . __('Processing Order', 'woocommerce') . '</h2>
-		<!-- /wp:heading -->
-
-		<!-- wp:paragraph -->
-		<p>' . __('Your order is currently being processed.', 'woocommerce') . '</p>
-		<!-- /wp:paragraph -->
-
-		<!-- wp:woo/email-content {"lock":{"move":false,"remove":true}} -->
-		<div class="wp-block-woo-email-content">' . BlockEmailRenderer::WOO_EMAIL_CONTENT_PLACEHOLDER . '</div>
-		<!-- /wp:woo/email-content -->';
-	}
-
-	private function get_customer_note_template() {
-		return '<!-- wp:heading -->
-		<h2>' . __('Customer Note', 'woocommerce') . '</h2>
-		<!-- /wp:heading -->
-
-		<!-- wp:woo/email-content {"lock":{"move":false,"remove":true}} -->
-		<div class="wp-block-woo-email-content">' . BlockEmailRenderer::WOO_EMAIL_CONTENT_PLACEHOLDER . '</div>
-		<!-- /wp:woo/email-content -->';
 	}
 }
--- plugins/woocommerce/src/Internal/EmailEditor/WCTransactionalEmails/WCTransactionalEmails.php
+++ plugins/woocommerce/src/Internal/EmailEditor/WCTransactionalEmails/WCTransactionalEmails.php
@@ -4,7 +4,20 @@ namespace Automattic\WooCommerce\Internal\EmailEditor\WCTransactionalEmails;
 
 class WCTransactionalEmails {
 
-	public function __construct() {}
+	public static $core_transactional_emails = array(
+		'cancelled_order',
+		'customer_completed_order',
+		'customer_failed_order',
+		'customer_invoice',
+		'customer_new_account',
+		'customer_note',
+		'customer_on_hold_order',
+		'customer_processing_order',
+		'customer_refunded_order',
+		'customer_reset_password',
+		'failed_order',
+		'new_order',
+	);
 
 	public function init() {
 		$this->init_email_templates();
@@ -12,9 +25,8 @@ class WCTransactionalEmails {
 
 	public function init_email_templates() {
 		$is_wc_email_settings_page = is_admin()
-        && isset($_GET['page'])
+        && isset($_GET['page'], $_GET['tab'])
         && $_GET['page'] === 'wc-settings'
-        && isset($_GET['tab'])
         && $_GET['tab'] === 'email';
 
 		if ( $is_wc_email_settings_page ) {
