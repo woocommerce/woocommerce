5c26983e8b567dc30034d5a5e38a59d2e4f3202e
mordeth <cvetan.cvetanov@automattic.com>
2025-03-21 15:23:02 +0200

    Add onboarding steps to MOX step

--- plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/mox/index.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/mox/index.tsx
@@ -3,8 +3,6 @@
  */
 import React from 'react';
 import { __ } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
-
 
 /**
  * Internal dependencies
@@ -13,27 +11,43 @@ import {
 	useOnboardingContext,
 } from '../../data/onboarding-context';
 import WooPaymentsStepHeader from '../../components/header';
+import { OnboardingContextProvider } from './data/onboarding-context';
+import { OnboardingForm } from './components/form';
+import BusinessDetails from './steps/business-details';
+import { Stepper } from './components/stepper';
+import Step from './components/step';
 import './style.scss';
 
 export const MOXStep: React.FC = () => {
 	const { currentStep } = useOnboardingContext();
 
+    const initialData = {
+        business_name: 'Test', // To-Do: Replace with wcSettings?.siteTitle,
+        mcc: '1231', // To-Do: Replace with getMccFromIndustry(),
+        site: 'https://wcpay.test', // To-Do: Replace with URL
+        country: 'US', // To-Do: Replace with country from WooCommerce settings
+    };
+
+    const handleStepChange = () => window.scroll( 0, 0 );
+
 	return (
 		<>
 			<WooPaymentsStepHeader onClose={ () => {} } />
-			<div className="settings-payments-onboarding-modal__step--content">
-				<div className="settings-payments-onboarding-modal__step--content-mox">
-					<h1 className="settings-payments-onboarding-modal__step--content-mox-title">
-						{ __( 'Let’s get your store ready to accept payments', 'woocommerce' ) }
-					</h1>
-					<p className="settings-payments-onboarding-modal__step--content-mox-description">
-						{ __(
-							'We’ll use these details to enable payments for your store. This information can’t be changed after your account is created.',
-							'woocommerce'
-						) }
-					</p>
-				</div>
-			</div>
+
+            <OnboardingContextProvider initialData={ initialData }>
+                <Stepper onStepChange={ handleStepChange } onExit={ () => {} }>
+                    <Step name="business">
+                        <OnboardingForm>
+                            <BusinessDetails />
+                        </OnboardingForm>
+                    </Step>
+                    <Step name="embedded" showHeading={ false }>
+                        <>
+                            To-Do: Embedded Onboarding
+                        </>
+                    </Step>
+                </Stepper>
+			</OnboardingContextProvider>
 		</>
 	);
 };
