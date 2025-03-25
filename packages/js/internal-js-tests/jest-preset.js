3b8085e320b76dcb32a3a52599cce9f4997188df
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-03-25 15:05:07 +0800

    Fix tests
    - Updated `@wordpress/private-apis` version from `wp-6.6` to `wp-6.7` in `package.json`.
    - Adjusted Jest configuration to ensure consistent usage of WP packages across tests.
    - Refactored test snapshots for `SearchListControl` to align with updated component structure.
    - Removed deprecated `@types/wordpress__components` references from `package.json`.
    - Added mappings for WP packages in the Jest preset to prevent version conflicts during testing.

--- packages/js/internal-js-tests/jest-preset.js
+++ packages/js/internal-js-tests/jest-preset.js
@@ -44,6 +44,16 @@ module.exports = {
 		'lib0/webcrypto': require.resolve( 'lib0/webcrypto' ), // use the CJS entry point so that it uses the node:crypto API as jsdom doesn't have a crypto API
 		uuid: require.resolve( 'uuid' ),
 		memize: require.resolve( 'memize' ),
+		/**
+		 * To ensure consistency in the test environment, all test files should use the same instance of the WP packages from the project's node_modules. This prevents potential conflicts with different versions of the packages. For example, a specific version of @wordpress/private-apis is defined in the package.json, but different instances can be used due to sub-dependencies having specific versions, which can cause issues.
+		 *
+		 * This approach aligns the test environment more closely with production, where the same version of the WP packages is used.
+		 *
+		 * Add additional mappings for other WP packages that are used in the project if needed.
+		 */
+		'@wordpress/private-apis': require.resolve( '@wordpress/private-apis' ),
+		'@wordpress/core-data': require.resolve( '@wordpress/core-data' ),
+		'@wordpress/components': require.resolve( '@wordpress/components' ),
 	},
 	restoreMocks: true,
 	setupFiles: [
