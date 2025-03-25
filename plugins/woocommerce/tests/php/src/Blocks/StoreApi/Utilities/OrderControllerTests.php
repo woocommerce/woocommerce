a410141073ec428c462fa2ee3f728737d2f27cda
Daniel Guerra <daniel.guerra@automattic.com>
2025-03-25 07:23:09 -0600

    Add OrderController tests

--- plugins/woocommerce/tests/php/src/Blocks/StoreApi/Utilities/OrderControllerTests.php
+++ plugins/woocommerce/tests/php/src/Blocks/StoreApi/Utilities/OrderControllerTests.php
@@ -3,8 +3,11 @@ declare( strict_types = 1 );
 
 namespace Automattic\WooCommerce\Tests\Blocks\StoreApi\Utilities;
 
+use WC_Helper_Order;
+use Automattic\WooCommerce\Enums\OrderStatus;
 use Automattic\WooCommerce\StoreApi\Exceptions\RouteException;
 use Automattic\WooCommerce\StoreApi\Utilities\OrderController;
+use Automattic\WooCommerce\RestApi\UnitTests\Helpers\CouponHelper;
 use Yoast\PHPUnitPolyfills\TestCases\TestCase;
 
 /**
@@ -43,5 +46,82 @@ class OrderControllerTests extends TestCase {
 		// The above methods throw Exception on error, but this is classed as a risky test because there are no
 		// assertions. Assert true to work around this warning.
 		$this->assertTrue( true );
+	}
+
+	/**
+	 * test_validate_order_before_payment_invalid_coupons.
+	 */
+	public function test_validate_order_before_payment_invalid_coupons() {
+		$this->expectException( RouteException::class );
+		$this->expectExceptionCode( 409 );
+		$this->expectExceptionMessage( '"fake-coupon" was removed from the cart. Please enter a valid email at checkout to use coupon code "fake-coupon".' );
+
+		$order = WC_Helper_Order::create_order();
+		$coupon = CouponHelper::create_coupon( 'fake-coupon', 'publish', array( 'customer_email' => 'random-email@example.com' ) );
+		$order->add_coupon( $coupon->get_code() );
+		$order->save();
+		$this->assertEquals( array( 'fake-coupon' ), $order->get_coupon_codes() );
+
+		$class = new OrderController();
+		try {
+			$class->validate_order_before_payment( $order );
+		} finally {
+			$this->assertEmpty( $order->get_coupon_codes() );
+		}
+	}
+
+	/**
+	 * test_validate_order_before_payment_invalid_email.
+	 */
+	public function test_validate_order_before_payment_invalid_email() {
+		$this->expectException( RouteException::class );
+		$this->expectExceptionCode( 400 );
+		$this->expectExceptionMessage( 'A valid email address is required' );
+
+		$order = new \WC_Order();
+		$order->set_status( OrderStatus::PENDING );
+		$order->save();
+
+		$class = new OrderController();
+		$class->validate_order_before_payment( $order );
+	}
+
+	/**
+	 * test_validate_order_before_payment_invalid_addresses.
+	 */
+	public function test_validate_order_before_payment_invalid_addresses() {
+		$this->expectException( RouteException::class );
+		$this->expectExceptionCode( 400 );
+		$this->expectExceptionMessage( 'Sorry, we do not ship orders to the provided country (Invalid)' );
+
+		$order = WC_Helper_Order::create_order();
+		$order->set_shipping_country( 'Invalid' );
+		$order->save();
+
+		/** @var \WC_Order_Item_Product $item */
+		$item = reset($order->get_items() );
+		$this->assertInstanceOf( \WC_Order_Item_Product::class, $item );
+
+		WC()->cart->add_to_cart( $item->get_product()->get_id() );
+
+		$class = new OrderController();
+		$class->validate_order_before_payment( $order );
+	}
+
+	/**
+	 * test_validate_existing_order_before_payment_invalid_addresses.
+	 */
+	public function test_validate_existing_order_before_payment_invalid_addresses() {
+		$this->expectException( RouteException::class );
+		$this->expectExceptionCode( 400 );
+		$this->expectExceptionMessage( 'Sorry, we do not ship orders to the provided country (Invalid)' );
+
+		$order = WC_Helper_Order::create_order();
+		$order->set_shipping_country( 'Invalid' );
+		$order->save();
+
+		// There is no need to update the cart here, we just check the order.
+		$class = new OrderController();
+		$class->validate_existing_order_before_payment( $order );
 	}
 }
