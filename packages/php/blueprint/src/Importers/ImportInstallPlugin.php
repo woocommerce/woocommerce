9c641a053dd76603db13a9ae5c8ed092b839c6d0
moon <moon.kyong@automattic.com>
2025-03-27 09:19:19 -0700

    Fix options->activate logic

--- packages/php/blueprint/src/Importers/ImportInstallPlugin.php
+++ packages/php/blueprint/src/Importers/ImportInstallPlugin.php
@@ -55,8 +55,8 @@ class ImportInstallPlugin implements StepProcessor {
 		$plugin = $schema->pluginData;
 
 		// We only support CorePluginReference at the moment.
-		if (  'wordpress.org/plugins' !== $plugin->resource ) {
-			$result->add_info("Skipped installing a plugin. Unsupported resource type. Only 'wordpress.org/plugins' is supported at the moment.");
+		if ( 'wordpress.org/plugins' !== $plugin->resource ) {
+			$result->add_info( "Skipped installing a plugin. Unsupported resource type. Only 'wordpress.org/plugins' is supported at the moment." );
 			return $result;
 		}
 
@@ -78,7 +78,7 @@ class ImportInstallPlugin implements StepProcessor {
 		$install = $this->install( $downloaded_path );
 		$install && $result->add_info( "Installed {$plugin->slug}." );
 
-		if ( isset( $plugin->options, $plugin->options->activate ) && true === $plugin->options->activate ) {
+		if ( isset( $schema->options, $schema->options->activate ) && true === $schema->options->activate ) {
 			$activate = $this->activate( $plugin->slug );
 
 			if ( $activate instanceof \WP_Error ) {
