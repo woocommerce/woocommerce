eb5f4cebed5fc4fa25cb8d14c08ca4617442e1a4
paul sealock <psealock@gmail.com>
2025-03-27 19:11:51 +1300

    Move blueprint hide save button logic

--- plugins/woocommerce/src/Admin/Features/Blueprint/Init.php
+++ plugins/woocommerce/src/Admin/Features/Blueprint/Init.php
@@ -55,18 +55,6 @@ class Init {
 		);
 
 		add_filter( 'wooblueprint_exporters', array( $this, 'add_woo_exporters' ) );
-
-		// Add a filter to hide the save button on the blueprint settings page.
-		add_action(
-			'woocommerce_settings_advanced',
-			function () {
-				global $hide_save_button;
-				$page_id = PageController::get_instance()->get_current_screen_id();
-				if ( 'woocommerce_page_wc-settings-advanced-blueprint' === $page_id ) {
-					$hide_save_button = true;
-				}
-			}
-		);
 	}
 
 	/**
