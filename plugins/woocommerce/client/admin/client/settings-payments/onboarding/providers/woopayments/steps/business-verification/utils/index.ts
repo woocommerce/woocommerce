3cfcafbb292e42f83fae1047866df3fb52f98a13
mordeth <cvetan.cvetanov@automattic.com>
2025-03-27 21:28:12 +0200

    Fix linting errors

--- plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/business-verification/utils/index.ts
+++ plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/business-verification/utils/index.ts
@@ -56,6 +56,7 @@ export const getBusinessTypes = ( data: Country[] ): Country[] => {
 
 /**
  * Make an API request to finalize the onboarding process.
+ * 
  * @param apiUrl    The API URL.
  * @param urlSource The source URL.
  */
