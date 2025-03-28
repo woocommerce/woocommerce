232f34d20a530c9aadecb8f519ddea9bca025fc3
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2025-03-28 11:40:11 -0400

    Slight re-work to compatibility layer so that before and after add to cart button hooks are available for custom product types

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions.php
@@ -158,20 +158,6 @@ class AddToCartWithOptions extends AbstractBlock {
 					 * @since 1.5.0
 					 */
 					do_action( 'woocommerce_before_add_to_cart_button' );
-				} elseif ( ProductType::EXTERNAL === $product_type ) {
-					/**
-					 * Hook: woocommerce_before_add_to_cart_button.
-					 *
-					 * @since 1.5.0
-					 */
-					do_action( 'woocommerce_before_add_to_cart_button' );
-				} elseif ( ProductType::GROUPED === $product_type ) {
-					/**
-					 * Hook: woocommerce_before_add_to_cart_button.
-					 *
-					 * @since 1.5.0
-					 */
-					do_action( 'woocommerce_before_add_to_cart_button' );
 				} elseif ( ProductType::VARIABLE === $product_type ) {
 					/**
 					 * Hook: woocommerce_before_variations_form.
@@ -179,6 +165,13 @@ class AddToCartWithOptions extends AbstractBlock {
 					 * @since 2.4.0
 					 */
 					do_action( 'woocommerce_before_variations_form' );
+				} else {
+					/**
+					 * Hook: woocommerce_before_add_to_cart_button.
+					 *
+					 * @since 1.5.0
+					 */
+					do_action( 'woocommerce_before_add_to_cart_button' );
 				}
 				$hooks_before = ob_get_clean();
 
@@ -196,20 +189,6 @@ class AddToCartWithOptions extends AbstractBlock {
 					 * @since 1.5.0
 					 */
 					do_action( 'woocommerce_after_add_to_cart_button' );
-				} elseif ( ProductType::EXTERNAL === $product_type ) {
-					/**
-					 * Hook: woocommerce_after_add_to_cart_button.
-					 *
-					 * @since 1.5.0
-					 */
-					do_action( 'woocommerce_after_add_to_cart_button' );
-				} elseif ( ProductType::GROUPED === $product_type ) {
-					/**
-					 * Hook: woocommerce_after_add_to_cart_button.
-					 *
-					 * @since 1.5.0
-					 */
-					do_action( 'woocommerce_after_add_to_cart_button' );
 				} elseif ( ProductType::VARIABLE === $product_type ) {
 					/**
 					 * Hook: woocommerce_after_variations_form.
@@ -217,7 +196,15 @@ class AddToCartWithOptions extends AbstractBlock {
 					 * @since 2.4.0
 					 */
 					do_action( 'woocommerce_after_variations_form' );
+				} else {
+					/**
+					 * Hook: woocommerce_after_add_to_cart_button.
+					 *
+					 * @since 1.5.0
+					 */
+					do_action( 'woocommerce_after_add_to_cart_button' );
 				}
+				
 				$hooks_after = ob_get_clean();
 			}
 
