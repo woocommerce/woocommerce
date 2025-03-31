e975e352b7a170f8bc3ce8b3acc288e7bef91921
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-03-31 11:49:03 +0100

    Mark mini cart widget test as fixme

--- plugins/woocommerce/client/blocks/tests/e2e/tests/mini-cart/mini-cart-block.merchant.classic_theme.spec.ts
+++ plugins/woocommerce/client/blocks/tests/e2e/tests/mini-cart/mini-cart-block.merchant.classic_theme.spec.ts
@@ -48,24 +48,23 @@ test.describe( 'Merchant → Mini Cart', () => {
 				await editor.getBlockByName( blockData.slug )
 			).toBeVisible();
 		} );
-		test( 'can only be inserted once', async ( {
-			page,
-			admin,
-			editor,
-		} ) => {
-			await admin.visitWidgetEditor();
-			await editor.openGlobalBlockInserter();
+		test.fixme(
+			'can only be inserted once',
+			async ( { page, admin, editor } ) => {
+				await admin.visitWidgetEditor();
+				await editor.openGlobalBlockInserter();
 
-			await editor.page
-				.getByRole( 'searchbox', { name: 'Search' } )
-				.fill( blockData.slug );
+				await editor.page
+					.getByRole( 'searchbox', { name: 'Search' } )
+					.fill( blockData.slug );
 
-			const miniCartButton = page.getByRole( 'option', {
-				name: blockData.name,
-				exact: true,
-			} );
+				const miniCartButton = page.getByRole( 'option', {
+					name: blockData.name,
+					exact: true,
+				} );
 
-			await expect( miniCartButton ).toBeHidden();
-		} );
+				await expect( miniCartButton ).toBeHidden();
+			}
+		);
 	} );
 } );
