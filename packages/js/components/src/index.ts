2a65072102acf68156e2922ad34b02308c4d53b6
Vladimir Reznichenko <kalessil@gmail.com>
2025-03-31 12:19:56 +0200

    CI: test changes.

--- packages/js/components/src/index.ts
+++ packages/js/components/src/index.ts
@@ -104,3 +104,5 @@ export { DisplayState } from './display-state';
 export { ProgressBar } from './progress-bar';
 export { ConfettiAnimation } from './confetti-animation';
 export { ErrorBoundary as __experimentalErrorBoundary } from './error-boundary';
+
+// Test changes.
