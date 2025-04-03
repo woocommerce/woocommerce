010160ae5805f2cc69c0e6e223bca8fe197fb176
Mike Jolley <mike.jolley@me.com>
2025-04-03 12:33:37 +0100

    Unused variable

--- plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/form/validate-country.ts
+++ plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/form/validate-country.ts
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { __, sprintf } from '@wordpress/i18n';
+import { __ } from '@wordpress/i18n';
 import { type ShippingAddress } from '@woocommerce/settings';
 import { select, dispatch } from '@wordpress/data';
 import { validationStore } from '@woocommerce/block-data';
