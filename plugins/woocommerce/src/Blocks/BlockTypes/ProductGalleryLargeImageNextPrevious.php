684294f648e53c734229d8d1866f01e396706cbd
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-04-09 18:16:26 +0200

    Align the frontend with editor

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductGalleryLargeImageNextPrevious.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductGalleryLargeImageNextPrevious.php
@@ -59,96 +59,30 @@ class ProductGalleryLargeImageNextPrevious extends AbstractBlock {
 			return '';
 		}
 
-		$prev_button = $this->get_button( 'previous' );
-		$p           = new \WP_HTML_Tag_Processor( $prev_button );
-
-		if ( $p->next_tag() ) {
-			$p->set_attribute(
-				'data-wp-on--click',
-				'actions.selectPreviousImage'
-			);
-			$p->set_attribute(
-				'aria-label',
-				__( 'Previous image', 'woocommerce' )
-			);
-			$prev_button = $p->get_updated_html();
-		}
-
-		$next_button = $this->get_button( 'next' );
-		$p           = new \WP_HTML_Tag_Processor( $next_button );
-
-		if ( $p->next_tag() ) {
-			$p->set_attribute(
-				'data-wp-on--click',
-				'actions.selectNextImage'
-			);
-			$p->set_attribute(
-				'aria-label',
-				__( 'Next image', 'woocommerce' )
-			);
-			$next_button = $p->get_updated_html();
-		}
-
-		return strtr(
+		return sprintf(
 			'<div
 				class="wc-block-product-gallery-large-image-next-previous wp-block-woocommerce-product-gallery-large-image-next-previous"
-				data-wp-interactive=\'{data_wp_interactive}\'
+				data-wp-interactive="woocommerce/product-gallery"
 			>
 				<div class="wc-block-product-gallery-large-image-next-previous-container">
-					{prev_button}
-					{next_button}
+					<button
+						className="wc-block-product-gallery-large-image-next-previous-left"
+						data-wp-on--click="actions.selectPreviousImage"
+						data-wp-bind--disabled="context.disableLeft"
+						aria-label="Previous image"
+					>
+						&lt;
+					</button>
+					<button
+						className="wc-block-product-gallery-large-image-next-previous-right"
+						data-wp-on--click="actions.selectNextImage"
+						data-wp-bind--disabled="context.disableRight"
+						aria-label="Next image"
+					>
+						&gt;
+					</button>
 				</div>
-		</div>',
-			array(
-				'{prev_button}'         => $prev_button,
-				'{next_button}'         => $next_button,
-				'{data_wp_interactive}' => 'woocommerce/product-gallery',
-			)
-		);
-	}
-
-	/**
-	 * Generates the HTML for a next or previous button for the product gallery large image.
-	 *
-	 * @param string $button_type The type of button to generate. Either 'previous' or 'next'.
-	 * @return string The HTML for the generated button.
-	 */
-	protected function get_button( $button_type ) {
-		$previous_button_icon_path = 'M28.1 12L30.5 14L21.3 24L30.5 34L28.1 36L17.3 24L28.1 12Z';
-		$next_button_icon_path     = 'M21.7001 12L19.3 14L28.5 24L19.3 34L21.7001 36L32.5 24L21.7001 12Z';
-		$icon_path                 = $previous_button_icon_path;
-		$button_side_class         = 'left';
-		$button_disabled_directive = 'context.disableLeft';
-
-		if ( 'next' === $button_type ) {
-			$icon_path                 = $next_button_icon_path;
-			$button_side_class         = 'right';
-			$button_disabled_directive = 'context.disableRight';
-		}
-
-		return sprintf(
-			'<button
-				data-wp-bind--disabled="%1$s"
-				class="wc-block-product-gallery-large-image-next-previous--button wc-block-product-gallery-large-image-next-previous-%2$s"
-			>
-				<svg xmlns="http://www.w3.org/2000/svg" width="49" height="48" viewBox="0 0 49 48" fill="none">
-					<g filter="url(#filter0_b_397_11354)">
-						<rect x="0.5" width="48" height="48" rx="5" fill="black" fill-opacity="0.5"/>
-						<path d="%3$s" fill="white"/>
-					</g>
-					<defs>
-						<filter id="filter0_b_397_11354" x="-9.5" y="-10" width="68" height="68" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-							<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-							<feGaussianBlur in="BackgroundImageFix" stdDeviation="5"/>
-							<feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_397_11354"/>
-							<feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_397_11354" result="shape"/>
-						</filter>
-					</defs>
-				</svg>
-			</button>',
-			$button_disabled_directive,
-			$button_side_class,
-			$icon_path
+			</div>'
 		);
 	}
 }
