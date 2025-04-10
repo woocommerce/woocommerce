6e4f62626e3aeeb49336a87cdb3b1bf155e478a2
Vladimir Reznichenko <kalessil@gmail.com>
2025-04-10 09:55:31 +0200

    CI: pick randomized ports in core E2E tests.

--- plugins/woocommerce/tests/e2e-pw/playwright.config.js
+++ plugins/woocommerce/tests/e2e-pw/playwright.config.js
@@ -8,7 +8,9 @@ require( 'dotenv' ).config( { path: __dirname + '/.env' } );
 if ( ! process.env.BASE_URL ) {
 	process.env.BASE_URL =
 		'http://localhost:' + ( process.env.WP_ENV_TESTS_PORT || '8086' );
-	console.log( 'BASE_URL is not set. Using default: ' + process.env.BASE_URL );
+	console.log(
+		'BASE_URL is not set. Using default: ' + process.env.BASE_URL
+	);
 }
 
 const { BASE_URL, CI, E2E_MAX_FAILURES, REPEAT_EACH } = process.env;
