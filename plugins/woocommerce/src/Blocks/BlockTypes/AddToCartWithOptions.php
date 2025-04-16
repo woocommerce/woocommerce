39ecac2dbb9bd5619479a19ccbd407ca1ff03f7d
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2025-04-16 09:40:34 -0400

    Fix incorrect @param

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions.php
@@ -87,7 +87,7 @@ class AddToCartWithOptions extends AbstractBlock {
 			 * Filter to declare product type's cart block template is supported.
 			 * 
 			 * @since x.x.x
-			 * @param boolean $supports Is the type supported
+			 * @param mixed string|boolean The template part path if it exists
 			 * @param string $product_type The product type 
 			 */
 			$template_part_path = apply_filters( '__experimental_woocommerce_' . $product_type . '_add_to_cart_with_options_block_template_part', false, $product_type );
