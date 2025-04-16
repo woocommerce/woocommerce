91f67ef3b5d65dc19cf2673c5535b8a42bcd9081
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2025-04-16 09:44:37 -0400

    Bring back remove_action|add_action for core add to cart. Erroneously removed in 4c5debd6e2e304889b1591ef523cc955e9737556. Prevents double-display of add to cart form.

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions.php
@@ -242,13 +242,17 @@ class AddToCartWithOptions extends AbstractBlock {
 			ob_start();
 
 			if ( in_array( $product_type, array( ProductType::SIMPLE, ProductType::EXTERNAL, ProductType::VARIABLE, ProductType::GROUPED ), true ) ) {
+
 				$add_to_cart_fn = 'woocommerce_' . $product_type . '_add_to_cart';
+				remove_action( 'woocommerce_' . $product_type . '_add_to_cart', $add_to_cart_fn, 30 );
+
 				/**
 				 * Trigger the single product add to cart action that prints the markup.
 				 *
 				 * @since 9.9.0
 				 */
 				do_action( 'woocommerce_' . $product->get_type() . '_add_to_cart' );
+				add_action( 'woocommerce_' . $product_type . '_add_to_cart', $add_to_cart_fn, 30 );
 			}
 
 			$form_html = $form_html . ob_get_clean();
