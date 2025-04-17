0c5172855e7e68588fa26e48a0a3e19c955cb3c0
mordeth <cvetan.cvetanov@automattic.com>
2025-04-17 16:41:38 +0300

    Invalidate getPaymentGateways store on modal close

--- plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/data/onboarding-context.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/data/onboarding-context.tsx
@@ -12,6 +12,7 @@ import { useDispatch, useSelect } from '@wordpress/data';
 import {
 	woopaymentsOnboardingStore,
 	WooPaymentsOnboardingStepContent,
+	paymentSettingsStore,
 } from '@woocommerce/data';
 import { getHistory, getNewPath } from '@woocommerce/navigation';
 
@@ -58,6 +59,11 @@ export const OnboardingProvider: React.FC< {
 
 	const { invalidateResolutionForStoreSelector } = useDispatch(
 		woopaymentsOnboardingStore
+	);
+
+	// Make UI refresh when plugin is installed.
+	const { invalidateResolutionForStoreSelector: invalidatePaymentGateways } = useDispatch(
+		paymentSettingsStore
 	);
 
 	// Initial data fetch from store
@@ -246,6 +252,10 @@ export const OnboardingProvider: React.FC< {
 					// Refresh the onboarding steps to get the latest data after closing the modal.
 					// This is to avoid showing the loader next time, but still have fresh data.
 					refreshOnboardingSteps();
+
+					// Invalidate the getPaymentProviders store selector to ensure the latest data is fetched.
+					// This is important to ensure that the payment providers buttons are up to date.
+					invalidatePaymentGateways( 'getPaymentProviders' );
 				},
 			} }
 		>
