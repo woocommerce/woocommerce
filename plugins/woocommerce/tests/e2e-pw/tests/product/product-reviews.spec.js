bde9f39bd82b9b2aa5b2f6df6c917d096fafd843
Jason Kytros <jimjasson@outlook.com.gr>
2025-04-17 10:23:04 +0300

    Lint fixes

--- plugins/woocommerce/tests/e2e-pw/tests/product/product-reviews.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/product/product-reviews.spec.js
@@ -313,7 +313,9 @@ test.describe( 'Product Reviews', () => {
 			await reviewRow.hover();
 
 			await reviewRow.getByRole( 'button', { name: 'Trash' } ).click();
-			await expect( page.locator( '.trash-undo-inside' ).first() ).toContainText(
+			await expect(
+				page.locator( '.trash-undo-inside' ).first()
+			).toContainText(
 				`Comment by ${ review.reviewer } moved to the Trash`
 			);
 			await page.getByRole( 'button', { name: 'Undo' } ).click();
@@ -324,7 +326,9 @@ test.describe( 'Product Reviews', () => {
 
 			await reviewRow.getByRole( 'button', { name: 'Trash' } ).click();
 
-			await expect( page.locator( '.trash-undo-inside' ).first() ).toContainText(
+			await expect(
+				page.locator( '.trash-undo-inside' ).first()
+			).toContainText(
 				`Comment by ${ review.reviewer } moved to the Trash`
 			);
 
