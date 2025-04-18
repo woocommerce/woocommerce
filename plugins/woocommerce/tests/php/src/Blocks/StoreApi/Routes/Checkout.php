60069606b62c61100bbb1ee218fd20e404964a4c
Łukasz Strączyński <l.straczynski@hotmail.com>
2025-04-18 11:58:05 +0200

    Add test

--- plugins/woocommerce/tests/php/src/Blocks/StoreApi/Routes/Checkout.php
+++ plugins/woocommerce/tests/php/src/Blocks/StoreApi/Routes/Checkout.php
@@ -1499,21 +1499,24 @@ class Checkout extends MockeryTestCase {
 	 * Test that custom status 'ready_for_pickup' is not changed when order is finished.
 	 */
 	public function test_custom_status_not_changed_on_payment_complete() {
-		add_filter( 'woocommerce_register_shop_order_post_statuses', function ( $order_statuses ) {
-			$order_statuses['wc-ready_for_pickup'] = array(
-				'label'                     => 'Ready for Pickup',
-				'public'                    => false,
-				'exclude_from_search'       => false,
-				'show_in_admin_all_list'    => true,
-				'show_in_admin_status_list' => true,
-				'label_count'               => _n_noop( 'Ready for Pickup (%s)', 'Ready for Pickup (%s)' ),
-			);
-			return $order_statuses;
-		} );
-		
+		add_filter(
+			'woocommerce_register_shop_order_post_statuses',
+			function ( $order_statuses ) {
+				$order_statuses['wc-ready_for_pickup'] = array(
+					'label'                     => 'Ready for Pickup',
+					'public'                    => false,
+					'exclude_from_search'       => false,
+					'show_in_admin_all_list'    => true,
+					'show_in_admin_status_list' => true,
+					'label_count'               => _n_noop( 'Ready for Pickup (%s)', 'Ready for Pickup (%s)' ),
+				);
+				return $order_statuses;
+			}
+		);
+
 		// Create a simple product and add to cart.
 		$product = \WC_Helper_Product::create_simple_product();
-		$product->save();  
+		$product->save();
 		WC()->cart->empty_cart();
 		WC()->cart->add_to_cart( $product->get_id(), 1 );
 
