06b002cf45ed9747a073cefcf606540364668735
Sam Seay <samueljseay@gmail.com>
2025-04-20 07:44:40 +0800

    Add specific lint rules to phpcs ignore

--- plugins/woocommerce/src/Blocks/BlockTypes/EnableBlockJsonAssetsTrait.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/EnableBlockJsonAssetsTrait.php
@@ -11,7 +11,7 @@ trait EnableBlockJsonAssetsTrait {
 	 * @param string|null $key The key of the script to get.
 	 * @return null
 	 */
-	// phpcs:ignore
+	// phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found, Squiz.Commenting.FunctionComment.Missing
 	protected function get_block_type_script( $key = null ) {
 		return null;
 	}
