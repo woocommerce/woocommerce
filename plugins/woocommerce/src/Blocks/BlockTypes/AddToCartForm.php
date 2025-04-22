d131d23781d7834244f28e93d4f15a6d955cf6ef
Luigi Teschio <gigitux@gmail.com>
2025-04-22 14:07:34 +0200

    Add to Cart - stepper style: fix input field when the product stock quantity is 1

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartForm.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartForm.php
@@ -136,7 +136,12 @@ class AddToCartForm extends AbstractBlock {
 		}
 
 		$is_external_product_with_url = $product instanceof \WC_Product_External && $product->get_product_url();
-		$is_stepper_style             = 'stepper' === $attributes['quantitySelectorStyle'] && ! $product->is_sold_individually() && Features::is_enabled( 'add-to-cart-with-options-stepper-layout' );
+		$stock_quantity               = $product->get_stock_quantity();
+
+		/**
+		 * The stepper buttons don't show when the product is sold individually or stock quantity is less or equal to 1 because the quantity input field is hidden.
+		 */
+		$is_stepper_style = 'stepper' === $attributes['quantitySelectorStyle'] && ! $product->is_sold_individually() && $stock_quantity > 1 && Features::is_enabled( 'add-to-cart-with-options-stepper-layout' );
 
 		if ( $is_descendent_of_single_product_block ) {
 			add_filter( 'woocommerce_add_to_cart_form_action', array( $this, 'add_to_cart_form_action' ), 10 );
