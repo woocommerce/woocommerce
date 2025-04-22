52a30aba16c67e733da936e78bb2ccb38a50569c
Amit Raj <harsh.harshraj.ra@gmail.com>
2025-04-22 13:12:28 +0530

    fix: Mini Cart Shopping Button block to set background color properly

--- plugins/woocommerce/client/blocks/assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-shopping-button-block/block.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-shopping-button-block/block.tsx
@@ -14,14 +14,37 @@ import { getVariant } from '../utils';
 type MiniCartShoppingButtonBlockProps = {
 	className: string;
 	startShoppingButtonLabel: string;
+	style?: Record< string, any >;
+	textColor?: string;
+	backgroundColor?: string;
 };
 
 const Block = ( {
 	className,
 	startShoppingButtonLabel,
+	style,
+	textColor,
+	backgroundColor,
 }: MiniCartShoppingButtonBlockProps ): JSX.Element | null => {
 	if ( ! SHOP_URL ) {
 		return null;
+	}
+
+	// Generate color classes based on attributes.
+	const colorClasses = clsx( {
+		[ `has-${ textColor }-color` ]: textColor,
+		[ `has-${ backgroundColor }-background-color` ]: backgroundColor,
+	} );
+
+	const buttonStyles: Record< string, string > = {};
+
+	const parsedStyle =
+		typeof style === 'string' ? JSON.parse( style ) : style || {};
+	if ( parsedStyle?.color?.text ) {
+		buttonStyles.color = parsedStyle.color.text;
+	}
+	if ( parsedStyle?.color?.background ) {
+		buttonStyles.backgroundColor = parsedStyle.color.background;
 	}
 
 	return (
@@ -30,8 +53,14 @@ const Block = ( {
 				className={ clsx(
 					className,
 					'wp-block-button__link',
-					'wc-block-mini-cart__shopping-button'
+					'wc-block-mini-cart__shopping-button',
+					colorClasses
 				) }
+				style={
+					Object.keys( buttonStyles ).length > 0
+						? buttonStyles
+						: undefined
+				}
 				variant={ getVariant( className, 'contained' ) }
 				href={ SHOP_URL }
 			>
