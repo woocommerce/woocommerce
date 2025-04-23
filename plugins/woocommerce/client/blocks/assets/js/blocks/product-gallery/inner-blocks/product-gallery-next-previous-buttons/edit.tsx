daeeb593de869154b5d8916707ca0fe275ffb37a
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-04-23 15:00:20 +0200

    Handle alignment in editor

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-next-previous-buttons/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-next-previous-buttons/edit.tsx
@@ -2,23 +2,53 @@
  * External dependencies
  */
 import { useBlockProps } from '@wordpress/block-editor';
+import clsx from 'clsx';
 
 /**
  * Internal dependencies
  */
 import { PrevIcon, NextIcon } from './icons';
 
-export const Edit = (): JSX.Element => {
-	const blockProps = useBlockProps( {
-		className: 'wc-block-product-gallery-large-image-next-previous__button',
+const getVerticalAlignmentClass = ( verticalAlignment?: string ) => {
+	if ( verticalAlignment === 'top' ) {
+		return 'aligntop';
+	}
+	if ( verticalAlignment === 'bottom' ) {
+		return 'alignbottom';
+	}
+	// Default to center.
+	return '';
+};
+
+export const Edit = ( {
+	attributes,
+}: {
+	attributes: { layout: { verticalAlignment: string } };
+} ) => {
+	const verticalAlignment = attributes?.layout?.verticalAlignment;
+	const verticalAlignmentClass =
+		getVerticalAlignmentClass( verticalAlignment );
+
+	const { style, ...blockProps } = useBlockProps( {
+		className: clsx(
+			'wc-block-product-gallery-large-image-next-previous',
+			verticalAlignmentClass
+		),
 	} );
 
 	return (
-		<div className="wc-block-product-gallery-large-image-next-previous">
-			<button { ...blockProps } disabled>
+		<div { ...blockProps }>
+			<button
+				className="wc-block-product-gallery-large-image-next-previous__button"
+				style={ style }
+				disabled
+			>
 				<PrevIcon className="wc-block-product-gallery-large-image-next-previous__icon wc-block-product-gallery-large-image-next-previous__icon--left" />
 			</button>
-			<button { ...blockProps }>
+			<button
+				className="wc-block-product-gallery-large-image-next-previous__button"
+				style={ style }
+			>
 				<NextIcon className="wc-block-product-gallery-large-image-next-previous__icon wc-block-product-gallery-large-image-next-previous__icon--right" />
 			</button>
 		</div>
