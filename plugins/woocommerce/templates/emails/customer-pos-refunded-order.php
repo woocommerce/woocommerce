41344990b433cde4231f27796c5d90af320473a4
Jaclyn Chen <watertranquil@gmail.com>
2025-04-23 15:58:34 +0800

    Fix outdated WC version of new templates.

--- plugins/woocommerce/templates/emails/customer-pos-refunded-order.php
+++ plugins/woocommerce/templates/emails/customer-pos-refunded-order.php
@@ -12,7 +12,7 @@
  *
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
- * @version 9.8.0
+ * @version 9.9.0
  */
 
 use Automattic\WooCommerce\Utilities\FeaturesUtil;
