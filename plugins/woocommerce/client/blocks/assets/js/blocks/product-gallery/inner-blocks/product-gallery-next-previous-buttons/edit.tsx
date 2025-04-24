7497fc87668ac698fb82262103512f5e5e874ca7
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-04-24 11:49:35 +0200

    Pass blockProps to container but provide proper classNames and styles to buttons

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-next-previous-buttons/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-next-previous-buttons/edit.tsx
@@ -1,16 +1,18 @@
 /**
  * External dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
-import clsx from 'clsx';
 import type { BlockAttributes } from '@wordpress/blocks';
+import clsx from 'clsx';
+import { useBlockProps } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
 import { PrevIcon, NextIcon } from './icons';
 
-const getVerticalAlignmentClass = ( verticalAlignment?: string ) => {
+const getVerticalAlignmentClass = ( attributes: BlockAttributes ) => {
+	const verticalAlignment = attributes?.layout?.verticalAlignment;
+
 	if ( verticalAlignment === 'top' ) {
 		return 'aligntop';
 	}
@@ -21,45 +23,47 @@ const getVerticalAlignmentClass = ( verticalAlignment?: string ) => {
 	return '';
 };
 
-const getAlignClass = ( align?: string ) => {
-	if ( align === 'left' ) {
-		return 'alignleft';
-	}
-	if ( align === 'right' ) {
-		return 'alignright';
-	}
-	if ( align === 'center' ) {
-		return 'aligncenter';
-	}
-	// Default to spread.
-	return '';
+const splitClassName = ( className: string ) => {
+	const classNamesArray = className.split( ' ' );
+	const containerClassName = classNamesArray
+		.filter( ( cn ) => ! cn.startsWith( 'has' ) )
+		.join( ' ' );
+	const buttonClassName = classNamesArray
+		.filter( ( cn ) => cn.startsWith( 'has' ) )
+		.join( ' ' );
+	return { containerClassName, buttonClassName };
 };
 
 export const Edit = ( { attributes }: { attributes: BlockAttributes } ) => {
-	const blockProps = useBlockProps( {
-		className: 'wc-block-product-gallery-large-image-next-previous__button',
+	const verticalAlignmentClass = getVerticalAlignmentClass( attributes );
+	const { style, className, ...blockProps } = useBlockProps( {
+		className: clsx(
+			'wc-block-product-gallery-large-image-next-previous',
+			verticalAlignmentClass
+		),
 	} );
 
-	// We're passing blockProps to buttons because they're the main block content.
-	// However, alignment has to be applied to the container hence we're handling
-	// these attributes "manually" here.
-	const verticalAlign = attributes?.layout?.verticalAlignment;
-	const verticalAlignClass = getVerticalAlignmentClass( verticalAlign );
-	const horizontalAlign = attributes?.align;
-	const horizontalAlignClass = getAlignClass( horizontalAlign );
-
-	const containerClassName = clsx(
-		'wc-block-product-gallery-large-image-next-previous',
-		verticalAlignClass,
-		horizontalAlignClass
-	);
+	const { containerClassName, buttonClassName } = splitClassName( className );
 
 	return (
-		<div className={ containerClassName }>
-			<button { ...blockProps } disabled>
+		<div { ...blockProps } className={ containerClassName }>
+			<button
+				className={ clsx(
+					buttonClassName,
+					'wc-block-product-gallery-large-image-next-previous__button'
+				) }
+				style={ style }
+				disabled
+			>
 				<PrevIcon className="wc-block-product-gallery-large-image-next-previous__icon wc-block-product-gallery-large-image-next-previous__icon--left" />
 			</button>
-			<button { ...blockProps }>
+			<button
+				style={ style }
+				className={
+					buttonClassName +
+					' wc-block-product-gallery-large-image-next-previous__button'
+				}
+			>
 				<NextIcon className="wc-block-product-gallery-large-image-next-previous__icon wc-block-product-gallery-large-image-next-previous__icon--right" />
 			</button>
 		</div>
