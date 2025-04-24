70d1a890cc4a1202454596ca62b839263f69759b
Tom Cafferkey <tjcafferkey@gmail.com>
2025-04-24 11:30:57 +0100

    is_object check

--- plugins/woocommerce/src/Blocks/Shipping/ShippingController.php
+++ plugins/woocommerce/src/Blocks/Shipping/ShippingController.php
@@ -334,8 +334,9 @@ class ShippingController {
 	 */
 	public function register_local_pickup() {
 		if ( CartCheckoutUtils::is_checkout_block_default() ) {
-			if ( WC() && method_exists( WC(), 'shipping' ) && method_exists( WC()->shipping, 'register_shipping_method' ) ) {
-				WC()->shipping->register_shipping_method( new PickupLocation() );
+			$wc_instance = WC();
+			if ( is_object( $wc_instance ) && method_exists( $wc_instance, 'shipping' ) && method_exists( $wc_instance->shipping, 'register_shipping_method' ) ) {
+				$wc_instance->shipping->register_shipping_method( new PickupLocation() );
 			} else {
 				wc_get_logger()->error( 'Error registering pickup location: WC()->shipping->register_shipping_method is not available', array( 'source' => 'shipping-controller' ) );
 			}
