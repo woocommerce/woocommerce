2ccc20cc74e3137a78f4fabad85a1ee1d303082a
oaratovskyi <oleksandr.aratovskyi@automattic.com>
2025-04-25 11:51:11 +0300

    One more lint fix

--- plugins/woocommerce/client/admin/client/settings-payments/components/bank-accounts-list/utils.ts
+++ plugins/woocommerce/client/admin/client/settings-payments/components/bank-accounts-list/utils.ts
@@ -1,6 +1,4 @@
-export const getDefaultRoutingField = (
-	country: string
-) => {
+export const getDefaultRoutingField = ( country: string ) => {
 	if ( country === 'US' ) return 'routing_number';
 	if ( country === 'AU' ) return 'sort_code';
 	return 'iban';
