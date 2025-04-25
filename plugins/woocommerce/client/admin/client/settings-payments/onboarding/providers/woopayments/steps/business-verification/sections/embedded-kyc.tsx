a81753f394b37a0a805dff19b92fcee5a7d0fc5b
Daniel Mallory <daniel.mallory@automattic.com>
2025-04-25 17:03:26 +0100

    Linter fixes.

--- plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/business-verification/sections/embedded-kyc.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/business-verification/sections/embedded-kyc.tsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { __ } from '@wordpress/i18n';
 import { LoadError } from '@stripe/connect-js';
 
