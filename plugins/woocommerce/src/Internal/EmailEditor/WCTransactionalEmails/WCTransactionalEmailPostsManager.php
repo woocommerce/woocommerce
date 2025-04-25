c48be6ae5a53062308adc72d45ec3b7e3416d048
Oluwaseun Olorunsola <seun.olorunsola@automattic.com>
2025-04-25 15:44:13 +0100

    Refactor WooCommerce email management to use options table for email type association
    - Introduced a new method in WCTransactionalEmailPostsManager to retrieve email type from the options table based on post ID.
    - Updated Integration class to utilize the new method for email type retrieval, removing reliance on post meta.
    - Adjusted email template management to streamline the association between email types and their corresponding post IDs.
    - Added unit tests to verify the correct functionality of the new email type retrieval method.
    
    This change enhances the efficiency and clarity of email type management within WooCommerce.

--- plugins/woocommerce/src/Internal/EmailEditor/WCTransactionalEmails/WCTransactionalEmailPostsManager.php
+++ plugins/woocommerce/src/Internal/EmailEditor/WCTransactionalEmails/WCTransactionalEmailPostsManager.php
@@ -8,6 +8,8 @@ namespace Automattic\WooCommerce\Internal\EmailEditor\WCTransactionalEmails;
  * Class responsible for managing WooCommerce email editor post templates.
  */
 class WCTransactionalEmailPostsManager {
+	const WC_OPTION_NAME = 'woocommerce_email_templates_%_post_id';
+
 	/**
 	 * Singleton instance of the class.
 	 *
@@ -52,6 +54,35 @@ class WCTransactionalEmailPostsManager {
 	}
 
 	/**
+	 * Retrieves the WooCommerce email type from the options table when post ID is provided.
+	 *
+	 * @param int|string $post_id The post ID.
+	 * @return string|null The WooCommerce email type if found, null otherwise.
+	 */
+	public function get_email_type_from_post_id( $post_id ) {
+		// Early return if post_id is invalid.
+		if ( empty( $post_id ) ) {
+			return null;
+		}
+
+		global $wpdb;
+
+		$option_name = $wpdb->get_var(
+			$wpdb->prepare(
+				"SELECT option_name FROM {$wpdb->options} WHERE option_name LIKE %s AND option_value = %s LIMIT 1",
+				self::WC_OPTION_NAME,
+				$post_id
+			)
+		);
+
+		if ( empty( $option_name ) ) {
+			return null;
+		}
+
+		return $this->get_email_type_from_option_name( $option_name );
+	}
+
+	/**
 	 * Checks if an email template exists for the given type.
 	 *
 	 * Type here refers to the email type, e.g. 'customer_new_account' from the WC_Email->id property.
@@ -70,7 +101,7 @@ class WCTransactionalEmailPostsManager {
 	 * @param int    $post_id    The post ID to save.
 	 */
 	public function save_email_template_post_id( $email_type, $post_id ) {
-		$option_name = 'woocommerce_email_templates_' . $email_type . '_post_id';
+		$option_name = $this->get_option_name( $email_type );
 		update_option( $option_name, $post_id );
 	}
 
@@ -81,7 +112,7 @@ class WCTransactionalEmailPostsManager {
 	 * @return int|false The post ID if found, false otherwise.
 	 */
 	public function get_email_template_post_id( $email_type ) {
-		$option_name = 'woocommerce_email_templates_' . $email_type . '_post_id';
+		$option_name = $this->get_option_name( $email_type );
 		return get_option( $option_name );
 	}
 
@@ -91,10 +122,37 @@ class WCTransactionalEmailPostsManager {
 	 * @param string $email_type The type of email template e.g. 'customer_new_account' from the WC_Email->id property.
 	 */
 	public function delete_email_template( $email_type ) {
-		$option_name = 'woocommerce_email_templates_' . $email_type . '_post_id';
+		$option_name = $this->get_option_name( $email_type );
 		if ( ! get_option( $option_name ) ) {
 			return;
 		}
 		delete_option( $option_name );
+	}
+
+	/**
+	 * Gets the option name for a specific email type.
+	 *
+	 * @param string $email_type The type of email template e.g. 'customer_new_account' from the WC_Email->id property.
+	 * @return string The option name e.g. 'woocommerce_email_templates_customer_new_account_post_id'
+	 */
+	private function get_option_name( $email_type ) {
+		return str_replace( '%', $email_type, self::WC_OPTION_NAME );
+	}
+
+	/**
+	 * Gets the email type from the option name.
+	 *
+	 * @param string $option_name The option name e.g. 'woocommerce_email_templates_customer_new_account_post_id'.
+	 * @return string The email type e.g. 'customer_new_account'
+	 */
+	private function get_email_type_from_option_name( $option_name ) {
+		return str_replace(
+			array(
+				'woocommerce_email_templates_',
+				'_post_id',
+			),
+			'',
+			$option_name
+		);
 	}
 }
