6d8aaf44203daf4fdb1bf613bb0b9f8bdc3796b9
Sarah Norris <sarah@sekai.co.uk>
2025-04-25 16:19:14 +0100

    Add domain to __()

--- plugins/woocommerce/src/Blocks/BlockTypes/Reviews/ProductReviewDate.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/Reviews/ProductReviewDate.php
@@ -37,7 +37,7 @@ class ProductReviewDate extends AbstractBlock {
 		$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => $classes ) );
 		if ( isset( $attributes['format'] ) && 'human-diff' === $attributes['format'] ) {
 			// translators: %s: human-readable time difference.
-			$formatted_date = sprintf( __( '%s ago' ), human_time_diff( get_comment_date( 'U', $comment ) ) );
+			$formatted_date = sprintf( __( '%s ago', 'woocommerce' ), human_time_diff( get_comment_date( 'U', $comment ) ) );
 		} else {
 			$formatted_date = get_comment_date( empty( $attributes['format'] ) ? '' : $attributes['format'], $comment );
 		}
