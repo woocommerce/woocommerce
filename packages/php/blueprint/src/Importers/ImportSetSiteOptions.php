630deca037e4de8c75485902e5bc07802b6eb79c
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-04-25 17:45:17 +0800

    [Blueprint] Add restrictions for options in Blueprint importer (#57381)
    * Implement restricted options handling in ImportSetSiteOptions
    
    - Added a list of restricted WordPress options that cannot be modified for security reasons.
    - Updated the process method to skip updates for these restricted options and log warnings accordingly.
    - Enhanced unit tests to verify that restricted options are not modified and appropriate warnings are generated.
    
    * Add changelog
    
    * Change message

--- packages/php/blueprint/src/Importers/ImportSetSiteOptions.php
+++ packages/php/blueprint/src/Importers/ImportSetSiteOptions.php
@@ -18,6 +18,28 @@ class ImportSetSiteOptions implements StepProcessor {
 	use UseWPFunctions;
 
 	/**
+	 * List of WordPress options that should not be modified.
+	 *
+	 * @var array<string>
+	 */
+	private const RESTRICTED_OPTIONS = array(
+		'siteurl',
+		'home',
+		'active_plugins',
+		'template',
+		'stylesheet',
+		'admin_email',
+		'unfiltered_html',
+		'users_can_register',
+		'default_role',
+		'db_version',
+		'cron',
+		'rewrite_rules',
+		'wp_user_roles',
+	);
+
+
+	/**
 	 * Process the step.
 	 *
 	 * @param object $schema The schema to process.
@@ -27,16 +49,23 @@ class ImportSetSiteOptions implements StepProcessor {
 	public function process( $schema ): StepProcessorResult {
 		$result = StepProcessorResult::success( SetSiteOptions::get_step_name() );
 		foreach ( $schema->options as $key => $value ) {
+			// Skip if the option should not be modified.
+			if ( in_array( $key, self::RESTRICTED_OPTIONS, true ) ) {
+				$result->add_warn( "Cannot modify '{$key}' option: Modifying is restricted for this key." );
+				continue;
+			}
+
 			$value   = json_decode( wp_json_encode( $value ), true );
 			$updated = $this->wp_update_option( $key, $value );
 
 			if ( $updated ) {
-				$result->add_info( "{$key} has been updated" );
-			} else {
-				$current_value = $this->wp_get_option( $key );
-				if ( $current_value === $value ) {
-					$result->add_info( "{$key} has not been updated because the current value is already up to date." );
-				}
+				$result->add_info( "{$key} has been updated." );
+				continue;
+			}
+
+			$current_value = $this->wp_get_option( $key );
+			if ( $current_value === $value ) {
+				$result->add_info( "{$key} has not been updated because the current value is already up to date." );
 			}
 		}
 
