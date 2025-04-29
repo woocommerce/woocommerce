fcbe2aaf68de86e879a42ba87489531153db3962
Jaclyn Chen <watertranquil@gmail.com>
2025-04-29 14:26:27 +0800

    Show order item unit price (subtotal) with the same tax display behavior as line subtotal in `order.get_formatted_line_subtotal`.

--- plugins/woocommerce/includes/abstracts/abstract-wc-order.php
+++ plugins/woocommerce/includes/abstracts/abstract-wc-order.php
@@ -2176,6 +2176,48 @@ abstract class WC_Abstract_Order extends WC_Abstract_Legacy_Order {
 	}
 
 	/**
+	 * Gets item subtotal - formatted for display.
+	 * 
+	 * This method returns the formatted unit price for an order item.
+	 *
+	 * @param object $item Item to get unit price from.
+	 * @param string $tax_display Incl or excl tax display mode.
+	 * @return string Formatted item subtotal.
+	 */
+	public function get_formatted_item_subtotal( $item, $tax_display = '' ) {
+		$tax_display = $tax_display ? $tax_display : get_option( 'woocommerce_tax_display_cart' );
+		$include_tax = 'excl' !== $tax_display;
+		$item_subtotal = $this->get_item_subtotal( $item, $include_tax );
+		$formatted_item_subtotal = $this->get_formatted_price( $item_subtotal, $include_tax );
+
+		return apply_filters( 'woocommerce_order_formatted_line_subtotal', $formatted_item_subtotal, $item, $this );
+	}
+
+	/**
+	 * Helper method to format price with or without tax.
+	 *
+	 * @param float $amount The amount to format.
+	 * @param bool $include_tax Whether to include tax in the formatted price.
+	 * @return string Formatted price string.
+	 */
+	private function get_formatted_price( $amount, $include_tax ) {
+		if ( $include_tax ) {
+			$subtotal = wc_price( $amount, array( 'currency' => $this->get_currency() ) );			
+		} else {
+			$ex_tax_label = $this->get_prices_include_tax() ? 1 : 0;
+
+			$subtotal = wc_price(
+				$amount,
+				array(
+					'ex_tax_label' => $ex_tax_label,
+					'currency'     => $this->get_currency(),
+				)
+			);
+		}
+		return $subtotal;
+	}
+
+	/**
 	 * Gets order total - formatted for display.
 	 *
 	 * @return string
--- plugins/woocommerce/includes/emails/trait-wc-pos-email-customizations.php
+++ plugins/woocommerce/includes/emails/trait-wc-pos-email-customizations.php
@@ -32,29 +32,14 @@ trait WC_POS_Email_Customizations {
 	/**
 	 * Add unit price to order item meta start position.
 	 *
-	 * @param int        $item_id       Order item ID.
-	 * @param array     $item          Order item data.
-	 * @param WC_Order  $order         Order object.
-	 * @param bool      $plain_text    Whether is plain text email.
+	 * @param int      $item_id       Order item ID.
+	 * @param array    $item          Order item data.
+	 * @param WC_Order $order         Order object.
+	 * @param bool     $plain_text    Whether is plain text email.
 	 */
 	public function add_unit_price( $item_id, $item, $order, $plain_text = false ) {
-		$unit_price = $this->get_formatted_item_subtotal($order, $item);
+		$unit_price = $order->get_formatted_item_subtotal( $item );
 		echo '<br /><small>' . $unit_price . '</small>';
-	}
-
-	/**
-	* Get item subtotal for unit price - formatted for display.
-	*
-	* @param object $order Order object.
-	* @param object $item Item for filter `woocommerce_order_formatted_item_subtotal`.
-	* @return string
-	*/
-	private function get_formatted_item_subtotal( $order, $item ) {
-		$subtotal = wc_price( 
-			$order->get_item_subtotal( $item, true ), 
-			array( 'currency' => $order->get_currency() ) 
-		);
-		return apply_filters( 'woocommerce_order_formatted_item_subtotal', $subtotal, $item, $this );
 	}
 
 	/**
