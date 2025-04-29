10730ddafe761ca44caed60351eef90f4c9518aa
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2025-04-29 15:22:45 +0300

    Fix Quote block styles for CSS inliner

--- packages/php/email-editor/src/Integrations/Core/Renderer/Blocks/class-quote.php
+++ packages/php/email-editor/src/Integrations/Core/Renderer/Blocks/class-quote.php
@@ -62,7 +62,7 @@ class Quote extends Abstract_Block_Renderer {
 
 		return $this->add_spacer(
 			sprintf(
-				'<cite class="email-block-quote-citation" style="margin: 0; font-style: italic;">%s</cite>',
+				'<cite class="email-block-quote-citation" style="margin: 0;">%s</cite>',
 				$citation_content
 			),
 			$email_attrs
