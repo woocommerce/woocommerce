43ec372fb2393c363baffdba15c45e82f28ccadf
Luigi Teschio <gigitux@gmail.com>
2025-04-30 17:55:09 +0200

    remove comment and useEffect

--- plugins/woocommerce/client/admin/client/customize-store/assembler-hub/sidebar/global-styles/color-palette-variations/index.tsx
+++ plugins/woocommerce/client/admin/client/customize-store/assembler-hub/sidebar/global-styles/color-palette-variations/index.tsx
@@ -4,31 +4,21 @@
  * External dependencies
  */
 import { __experimentalGrid as Grid } from '@wordpress/components';
-import { useEffect, useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { VariationContainer } from '../variation-container';
-import { COLOR_PALETTES, DEFAULT_COLOR_PALETTES } from './constants';
+import { DEFAULT_COLOR_PALETTES } from './constants';
 import { ColorPaletteVariationPreview } from './preview';
 
 export const ColorPalette = () => {
-	const [ colorPalettes, setColorPalettes ] = useState(
-		[] as typeof COLOR_PALETTES
-	);
-
-	useEffect( () => {
-		// seems that aiSuggestions weren't correctly populated, we'll just use the first 9
-		setColorPalettes( DEFAULT_COLOR_PALETTES as typeof COLOR_PALETTES );
-	}, [] );
-
 	return (
 		<Grid
 			columns={ 3 }
 			className="woocommerce-customize-store_color-palette-container"
 		>
-			{ colorPalettes?.map( ( variation, index ) => (
+			{ DEFAULT_COLOR_PALETTES?.map( ( variation, index ) => (
 				<VariationContainer key={ index } variation={ variation }>
 					<ColorPaletteVariationPreview title={ variation?.title } />
 				</VariationContainer>
