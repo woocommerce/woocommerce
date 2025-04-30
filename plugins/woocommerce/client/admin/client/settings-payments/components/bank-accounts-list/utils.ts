c91f8893245728232f2d92e6b10a9aaa4e20137b
Daniel Mallory <daniel.mallory@automattic.com>
2025-04-30 22:28:30 +0100

    Update getSortcodeLabel to use switch statement.

--- plugins/woocommerce/client/admin/client/settings-payments/components/bank-accounts-list/utils.ts
+++ plugins/woocommerce/client/admin/client/settings-payments/components/bank-accounts-list/utils.ts
@@ -6,21 +6,30 @@ import { __ } from '@wordpress/i18n';
 /**
  * Determines the label for the sort code field based on the country.
  *
- * @param country The country code (e.g., 'AU', 'CA', etc.).
+ * @param  country The country code (e.g., 'AU', 'CA', etc.).
  *
  * @return {string} The label for the sort code field.
  */
 export const getSortcodeLabel = ( country: string ) => {
-	if ( country === 'AU' ) return __( 'BSB', 'woocommerce' );
-	if ( country === 'CA' ) return __( 'Bank transit number', 'woocommerce' );
-	if ( country === 'IN' ) return __( 'IFSC', 'woocommerce' );
-	if ( country === 'IT' ) return __( 'Branch sort', 'woocommerce' );
-	if ( country === 'NZ' ) return __( 'Bank code', 'woocommerce' );
-	if ( country === 'SE' ) return __( 'Bank code', 'woocommerce' );
-	if ( country === 'US' ) return __( 'Routing number', 'woocommerce' );
-	if ( country === 'ZA' ) return __( 'Branch code', 'woocommerce' );
-
-	return __( 'Sort code', 'woocommerce' );
+	switch ( country ) {
+		case 'AU':
+			return __( 'BSB', 'woocommerce' );
+		case 'CA':
+			return __( 'Bank transit number', 'woocommerce' );
+		case 'IN':
+			return __( 'IFSC', 'woocommerce' );
+		case 'IT':
+			return __( 'Branch sort', 'woocommerce' );
+		case 'NZ':
+		case 'SE':
+			return __( 'Bank code', 'woocommerce' );
+		case 'US':
+			return __( 'Routing number', 'woocommerce' );
+		case 'ZA':
+			return __( 'Branch code', 'woocommerce' );
+		default:
+			return __( 'Sort code', 'woocommerce' );
+	}
 };
 
 /**
