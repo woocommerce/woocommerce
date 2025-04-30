b4eadb661fe873998fcfaab49b6069d34c2ae51e
AhmarZaidi <ahmarzaidi07@gmail.com>
2025-04-30 14:32:38 +0530

    fix: linting errors, update stepper button styles

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-elements/add-to-cart-form/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-elements/add-to-cart-form/edit.tsx
@@ -8,9 +8,8 @@ import { BlockEditProps } from '@wordpress/blocks';
 import { Disabled, Tooltip } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { isSiteEditorPage } from '@woocommerce/utils';
-import { getSettingWithCoercion } from '@woocommerce/settings';
+import { getSettingWithCoercion, getSetting } from '@woocommerce/settings';
 import { isBoolean } from '@woocommerce/types';
-import { getSetting } from '@woocommerce/settings';
 
 /**
  * Internal dependencies
@@ -47,9 +46,11 @@ const AddToCartFormEdit = ( props: BlockEditProps< Attributes > ) => {
 		[]
 	);
 
-	const isBlockTheme = getSetting( 'isBlockTheme', false  );
-	const buttonBlockClass = ! isBlockTheme ? 'wp-block-button' : '' 
-	const buttonLinkClass = ! isBlockTheme ? 'wp-block-button__link wc-block-components-button' : ''
+	const isBlockTheme = getSetting( 'isBlockTheme', false );
+	const buttonBlockClass = ! isBlockTheme ? 'wp-block-button' : '';
+	const buttonLinkClass = ! isBlockTheme
+		? 'wp-block-button__link wc-block-components-button'
+		: '';
 
 	return (
 		<>
@@ -103,11 +104,14 @@ const AddToCartFormEdit = ( props: BlockEditProps< Attributes > ) => {
 											readOnly
 										/>
 									</div>
-									<div className={buttonBlockClass}>
+									<div className={ buttonBlockClass }>
 										<button
-											className={ `single_add_to_cart_button alt wp-element-button ${buttonLinkClass}` }
+											className={ `single_add_to_cart_button alt wp-element-button ${ buttonLinkClass }` }
 										>
-											{ __( 'Add to cart', 'woocommerce' ) }
+											{ __(
+												'Add to cart',
+												'woocommerce'
+											) }
 										</button>
 									</div>
 								</>
@@ -147,14 +151,16 @@ const AddToCartFormEdit = ( props: BlockEditProps< Attributes > ) => {
 												+
 											</button>
 										</div>
-										<button
-											className={ `single_add_to_cart_button alt wp-element-button` }
-										>
-											{ __(
-												'Add to cart',
-												'woocommerce'
-											) }
-										</button>
+										<div className={ buttonBlockClass }>
+											<button
+												className={ `single_add_to_cart_button alt wp-element-button ${ buttonLinkClass }` }
+											>
+												{ __(
+													'Add to cart',
+													'woocommerce'
+												) }
+											</button>
+										</div>
 									</>
 								) }
 						</Disabled>
