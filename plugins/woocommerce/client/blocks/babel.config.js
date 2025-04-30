e5b5c7e817432e3aa302fceed767e83fc9a2cdfb
Sam Seay <samueljseay@gmail.com>
2025-04-30 12:48:23 +0800

    Remove more redundant changes

--- plugins/woocommerce/client/blocks/babel.config.js
+++ plugins/woocommerce/client/blocks/babel.config.js
@@ -11,7 +11,14 @@ module.exports = {
 		},
 	},
 	presets: [
-		[ '@wordpress/babel-preset-default' ],
+		[
+			'@wordpress/babel-preset-default',
+			{
+				targets: {
+					node: 'current',
+				},
+			},
+		],
 		[ '@babel/preset-typescript' ],
 	],
 };
