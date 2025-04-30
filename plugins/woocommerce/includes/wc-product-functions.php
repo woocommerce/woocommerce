afedf4db61937e316b66512e3017dd2e6ee089f9
Tom Cafferkey <tjcafferkey@gmail.com>
2025-04-30 10:51:27 +0100

    Refactor wc_delete_product_transients to use AS get_next() instead of search()

--- plugins/woocommerce/includes/wc-product-functions.php
+++ plugins/woocommerce/includes/wc-product-functions.php
@@ -165,14 +165,10 @@ function wc_delete_product_transients( $post_id = 0 ) {
 
 			if ( ! isset( $scheduled[ $cache_key ] ) ) {
 				$queue                   = WC()->queue();
-				$existing                = $queue->search(
-					array(
-						'hook'     => 'wc_delete_related_product_transients_async',
-						'args'     => array( 'post_id' => $post_id ),
-						'status'   => 'pending',
-						'group'    => 'wc_delete_related_product_transients_group',
-						'per_page' => 1,
-					)
+				$existing                = $queue->get_next(
+					'wc_delete_related_product_transients_async',
+					array( 'post_id' => $post_id ),
+					'wc_delete_related_product_transients_group'
 				);
 				$scheduled[ $cache_key ] = ! empty( $existing );
 			}
