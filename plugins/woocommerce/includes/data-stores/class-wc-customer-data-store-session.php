729c78faf3302da815f59f4f8276ba0b35ae105d
Michael Pretty <prettyboymp@users.noreply.github.com>
2025-04-30 16:57:48 -0400

    revert changes in class WC_Customer_Data_Store_Session

--- plugins/woocommerce/includes/data-stores/class-wc-customer-data-store-session.php
+++ plugins/woocommerce/includes/data-stores/class-wc-customer-data-store-session.php
@@ -114,14 +114,10 @@ class WC_Customer_Data_Store_Session extends WC_Data_Store_WP implements WC_Cust
 						);
 					}
 				}
-				if ( ! empty( $session_value ) ) {
-					$data['meta_data'] = $session_value;
-				}
+				$data['meta_data'] = $session_value;
 			} else {
-				$session_value = (string) $customer->{ "get_$function_key" }( 'edit' );
-				if ( '' !== $session_value || isset( $customer->get_changes()[ $session_key ] ) ) {
-					$data[ $session_key ] = $session_value;
-				}
+				$session_value        = $customer->{"get_$function_key"}( 'edit' );
+				$data[ $session_key ] = (string) $session_value;
 			}
 		}
 
