510d5d681ee1302b9ca17b2033321cfa2f7c68f6
Sarah Norris <sarah@sekai.co.uk>
2025-05-01 17:54:22 +0100

    Add grouped products context to selector

--- plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/grouped-product-selector/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/grouped-product-selector/edit.tsx
@@ -4,11 +4,14 @@
 import { useProductDataContext } from '@woocommerce/shared-context';
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 import { type BlockEditProps } from '@wordpress/blocks';
+import { resolveSelect } from '@wordpress/data';
+import { productsStore } from '@woocommerce/data';
+import { useEffect, useState } from '@wordpress/element';
+import type { ProductResponseItem } from '@woocommerce/types';
 
 /**
  * Internal dependencies
  */
-import { useProductTypeSelector } from '../../../shared/stores/product-type-template-state';
 import { GROUPED_PRODUCT_ITEM_TEMPLATE } from './product-item-template/constants';
 
 interface Attributes {
@@ -19,11 +22,7 @@ export default function AddToCartWithOptionsGroupedProductSelectorEdit(
 	props: BlockEditProps< Attributes >
 ) {
 	const { className } = props.attributes;
-	const { current: currentProductType } = useProductTypeSelector();
 	const { product } = useProductDataContext();
-	const productType =
-		product.id === 0 ? currentProductType?.slug : product.type;
-
 	const blockProps = useBlockProps( {
 		className,
 	} );
@@ -31,8 +30,32 @@ export default function AddToCartWithOptionsGroupedProductSelectorEdit(
 		template: GROUPED_PRODUCT_ITEM_TEMPLATE,
 		templateLock: 'all',
 	} );
+	const [ groupedProduct, setGroupedProduct ] = useState<
+		ProductResponseItem[]
+	>( [] );
 
-	if ( productType !== 'grouped' ) {
+	// If there is no product context, query for an existing grouped product.
+	useEffect( () => {
+		if ( ! groupedProduct ) {
+			if ( product.id !== 0 && product.type === 'grouped' ) {
+				setGroupedProduct( [ product as ProductResponseItem ] );
+			}
+			if ( product.id === 0 ) {
+				resolveSelect( productsStore )
+					.getProducts( {
+						type: 'grouped',
+						per_page: 3,
+					} )
+					.then( ( fetchedProduct ) => {
+						if ( fetchedProduct.length > 0 ) {
+							setGroupedProduct( fetchedProduct );
+						}
+					} );
+			}
+		}
+	}, [ groupedProduct, product ] );
+
+	if ( groupedProduct.length === 0 ) {
 		return null;
 	}
 
