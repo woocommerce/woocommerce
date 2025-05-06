6bce62923e46a2064b87edab57cefaf2cedfd7c9
Pavel Dohnal <pavel.dohnal@automattic.com>
2025-05-06 12:42:06 +0200

    Add the status switch component

--- plugins/woocommerce/client/admin/client/wp-admin-scripts/email-editor-integration/email_status.tsx
+++ plugins/woocommerce/client/admin/client/wp-admin-scripts/email-editor-integration/email_status.tsx
@@ -1,42 +1,57 @@
 import { __, sprintf } from '@wordpress/i18n';
-import { PanelRow, Button, Flex, FlexItem } from '@wordpress/components';
+import { PanelRow, Button, Flex, FlexItem, Dropdown, Icon } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 import { EMAIL_STATUSES } from '../../settings-email/settings-email-listing-status';
 
 // TODO classes and styles
 
 export function EmailStatus() {
-	const isOpen = false; // TODO
 	const [statusValue] = useState('enabled');
 	const status = EMAIL_STATUSES.find((s) => s.value === statusValue) ?? EMAIL_STATUSES[1];
-	function onToggle() {
-		// TODO
-	}
 
 	return (
-		<PanelRow
-			className='editor-post-panel__row'
-		>
+		<PanelRow className='editor-post-panel__row'>
 			<Flex justify={ 'start' }>
 				<FlexItem className="editor-post-panel__row-label">
 					{ __( 'Email Status', 'woocommerce' ) }
 				</FlexItem>
 				<FlexItem>
-					<Button
-						variant="tertiary"
-						className="editor-post-status__toggle"
-						icon={status.icon}
-						size="compact"
-						onClick={ onToggle }
-						aria-label={ sprintf(
-							// translators: %s: Current post status.
-							__( 'Change status: %s' ),
-							status.label
-						) }
-						aria-expanded={ isOpen }
-					>
-						{ status.label }
-					</Button>
+					<Dropdown
+						position="bottom left"
+						renderToggle={ ( { isOpen, onToggle } ) => (
+							<Button
+								variant="tertiary"
+								className="editor-post-status__toggle"
+								icon={status.icon}
+								size="compact"
+								onClick={ onToggle }
+								aria-label={ sprintf(
+									// translators: %s: Current post status.
+									__( 'Change status: %s' ),
+									status.label
+								) }
+								aria-expanded={ isOpen }
+							>
+								{ status.label }
+							</Button>
+						)}
+						renderContent={ () => (
+							<div style={{ minWidth: 160 }}>
+								{EMAIL_STATUSES.map((option) => (
+									<Button
+										key={option.value}
+										variant="tertiary"
+										className="editor-post-status__dropdown-item"
+										icon={option.icon}
+										disabled={option.value === statusValue}
+										style={{ width: '100%', justifyContent: 'flex-start' }}
+									>
+										{option.label}
+									</Button>
+								))}
+							</div>
+						)}
+					/>
 				</FlexItem>
 			</Flex>
 		</PanelRow>
