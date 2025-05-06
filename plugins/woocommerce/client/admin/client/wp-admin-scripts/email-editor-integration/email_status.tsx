e4b4d7a0ca4eaf829a23c070c8897f95d7c380f3
Pavel Dohnal <pavel.dohnal@automattic.com>
2025-05-06 12:27:52 +0200

    Update the email status markup to match the post status

--- plugins/woocommerce/client/admin/client/wp-admin-scripts/email-editor-integration/email_status.tsx
+++ plugins/woocommerce/client/admin/client/wp-admin-scripts/email-editor-integration/email_status.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
-import { __ } from '@wordpress/i18n';
-import { __experimentalHStack as HStack, Icon } from '@wordpress/components';
+import { __, sprintf } from '@wordpress/i18n';
+import { PanelRow, Button, Flex, FlexItem } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 import { EMAIL_STATUSES } from '../../settings-email/settings-email-listing-status';
 
+// TODO classes and styles
+
 export function EmailStatus() {
+	const isOpen = false; // TODO
+	const [statusValue] = useState('enabled');
+	const status = EMAIL_STATUSES.find((s) => s.value === statusValue) ?? EMAIL_STATUSES[1];
+	function onToggle() {
+		// TODO
+	}
+
 	return (
-		<div>
-			<strong>Email Status:</strong>
-			{EMAIL_STATUSES.map((status) => (
-				<HStack key={status.value} alignment="left" spacing={0} style={{ margin: '4px 0' }}>
-					<Icon icon={status.icon} size={24} />
-					<span style={{ marginLeft: 8 }}>{status.label}</span>
-				</HStack>
-			))}
-		</div>
+		<PanelRow
+			className='editor-post-panel__row'
+		>
+			<Flex justify={ 'start' }>
+				<FlexItem className="editor-post-panel__row-label">
+					{ __( 'Email Status', 'woocommerce' ) }
+				</FlexItem>
+				<FlexItem>
+					<Button
+						variant="tertiary"
+						className="editor-post-status__toggle"
+						icon={status.icon}
+						size="compact"
+						onClick={ onToggle }
+						aria-label={ sprintf(
+							// translators: %s: Current post status.
+							__( 'Change status: %s' ),
+							status.label
+						) }
+						aria-expanded={ isOpen }
+					>
+						{ status.label }
+					</Button>
+				</FlexItem>
+			</Flex>
+		</PanelRow>
 	);
 }
