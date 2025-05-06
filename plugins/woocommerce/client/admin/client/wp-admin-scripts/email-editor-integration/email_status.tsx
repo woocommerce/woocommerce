56f08ed46427a2fb7ce2c5f815115fc120844dbd
Pavel Dohnal <pavel.dohnal@automattic.com>
2025-05-06 10:06:04 +0200

    Refactor share EMAIL_STATUSES constant

--- plugins/woocommerce/client/admin/client/wp-admin-scripts/email-editor-integration/email_status.tsx
+++ plugins/woocommerce/client/admin/client/wp-admin-scripts/email-editor-integration/email_status.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { __ } from '@wordpress/i18n';
+import { __experimentalHStack as HStack, Icon } from '@wordpress/components';
+import { EMAIL_STATUSES } from '../../settings-email/settings-email-listing-status';
 
 export function EmailStatus() {
 	return (
 		<div>
-			<strong>Email Status:</strong> enabled | disabled | manual
+			<strong>Email Status:</strong>
+			{EMAIL_STATUSES.map((status) => (
+				<HStack key={status.value} alignment="left" spacing={0} style={{ margin: '4px 0' }}>
+					<Icon icon={status.icon} size={24} />
+					<span style={{ marginLeft: 8 }}>{status.label}</span>
+				</HStack>
+			))}
 		</div>
 	);
 }
