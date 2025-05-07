f3cd62d0aa43d2c3cafcca04f00509142c493276
Daniel Mallory <daniel.mallory@automattic.com>
2025-05-07 14:57:37 +0100

    Update to add tracking event to the In context flow for continuing on the payment methods step, adding new params, deprecate old logic.

--- plugins/woocommerce/client/admin/client/settings-payments/index.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/index.tsx
@@ -3,36 +3,21 @@
  */
 import { Gridicon } from '@automattic/components';
 import { Button, SelectControl } from '@wordpress/components';
-import { paymentSettingsStore } from '@woocommerce/data';
-import { useSelect } from '@wordpress/data';
-import React, {
-	useState,
-	lazy,
-	Suspense,
-	useCallback,
-	useEffect,
-} from '@wordpress/element';
+import React, { lazy, Suspense } from '@wordpress/element';
 import {
 	unstable_HistoryRouter as HistoryRouter,
 	Route,
 	Routes,
-	useLocation,
 } from 'react-router-dom';
-import { getHistory, getNewPath } from '@woocommerce/navigation';
+import { getHistory } from '@woocommerce/navigation';
 import { __ } from '@wordpress/i18n';
 import { getAdminLink } from '@woocommerce/settings';
-import { recordEvent } from '@woocommerce/tracks';
 
 /**
  * Internal dependencies
  */
 import { Header } from './components/header/header';
-import { BackButton } from './components/buttons/back-button';
 import { ListPlaceholder } from '~/settings-payments/components/list-placeholder';
-import {
-	getWooPaymentsTestDriveAccountLink,
-	getWooPaymentsFromProviders,
-} from '~/settings-payments/utils';
 import './settings-payments-main.scss';
 
 /**
@@ -42,16 +27,6 @@ const SettingsPaymentsMainChunk = lazy(
 	() =>
 		import(
 			/* webpackChunkName: "settings-payments-main" */ './settings-payments-main'
-		)
-);
-
-/**
- * Lazy-loaded chunk for the recommended payment methods settings page.
- */
-const SettingsPaymentsMethodsChunk = lazy(
-	() =>
-		import(
-			/* webpackChunkName: "settings-payments-methods" */ './settings-payments-methods'
 		)
 );
 
