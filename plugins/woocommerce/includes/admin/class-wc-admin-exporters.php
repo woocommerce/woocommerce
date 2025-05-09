4c31a05bc13780297646e4d6523564dcff14fbaf
Jason Kytros <jimjasson@outlook.com.gr>
2025-05-09 13:47:33 +0300

    Fix lint errors

--- plugins/woocommerce/includes/admin/class-wc-admin-exporters.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-exporters.php
@@ -93,7 +93,7 @@ class WC_Admin_Exporters {
 		$screen = get_current_screen();
 
 		if ( $screen && 'product_page_product_exporter' === $screen->id ) {
-			$submenu_file = 'edit.php?post_type=product'; // WPCS: override ok.
+			$submenu_file = 'edit.php?post_type=product'; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
 		}
 	}
 
