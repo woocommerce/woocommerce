4960244f7fc2ceb928296eb23ff7680b3748a1a2
 Ján Mikláš <neosinner@gmail.com>
2025-05-10 16:58:04 +0200

    More descriptive method name

--- plugins/woocommerce/includes/class-wc-install.php
+++ plugins/woocommerce/includes/class-wc-install.php
@@ -324,7 +324,7 @@ class WC_Install {
 		add_action( 'init', array( __CLASS__, 'manual_database_update' ), 20 );
 		add_action( 'woocommerce_newly_installed', array( __CLASS__, 'maybe_enable_hpos' ), 20 );
 		add_action( 'woocommerce_newly_installed', array( __CLASS__, 'add_coming_soon_option' ), 20 );
-		add_action( 'woocommerce_newly_installed', array( __CLASS__, 'enable_email_improvements' ), 20 );
+		add_action( 'woocommerce_newly_installed', array( __CLASS__, 'enable_email_improvements_for_newly_installed' ), 20 );
 		add_action( 'admin_init', array( __CLASS__, 'wc_admin_db_update_notice' ) );
 		add_action( 'admin_init', array( __CLASS__, 'add_admin_note_after_page_created' ) );
 		add_action( 'woocommerce_run_update_callback', array( __CLASS__, 'run_update_callback' ) );
@@ -1091,7 +1091,7 @@ class WC_Install {
 	 *
 	 * @since 9.8.0
 	 */
-	public static function enable_email_improvements() {
+	public static function enable_email_improvements_for_newly_installed() {
 		update_option( 'woocommerce_feature_email_improvements_enabled', 'yes' );
 		update_option( 'woocommerce_email_improvements_default_enabled', 'yes' );
 		update_option( 'woocommerce_email_auto_sync_with_theme', 'yes' );
