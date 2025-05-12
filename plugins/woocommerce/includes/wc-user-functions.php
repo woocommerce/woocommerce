02c326de7d20ca6293868d7ba2e54b2e4815e7e4
Vladimir Reznichenko <kalessil@gmail.com>
2025-05-12 16:14:51 +0200

    Performance: wc_customer_bought_product SQL performance tweaks without HPOS and lookup tables.

--- plugins/woocommerce/includes/wc-user-functions.php
+++ plugins/woocommerce/includes/wc-user-functions.php
@@ -521,7 +521,7 @@ SELECT DISTINCT im.meta_value FROM {$wpdb->posts} AS p
 INNER JOIN {$wpdb->postmeta} AS pm ON p.ID = pm.post_id
 INNER JOIN {$wpdb->prefix}woocommerce_order_items AS i ON p.ID = i.order_id
 INNER JOIN {$wpdb->prefix}woocommerce_order_itemmeta AS im ON i.order_item_id = im.order_item_id
-WHERE p.post_status IN ( 'wc-" . implode( "','wc-", $statuses ) . "' )
+WHERE p.post_status IN ( 'wc-" . implode( "','wc-", $statuses ) . "' ) AND p.post_type = 'shop_order'
 AND pm.meta_key IN ( '_billing_email', '_customer_user' )
 AND im.meta_key IN ( '_product_id', '_variation_id' )
 AND im.meta_value != 0
