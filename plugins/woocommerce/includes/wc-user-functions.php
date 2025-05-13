fa0873d66a8d8705092b47602e01487acba8e051
Vladimir Reznichenko <kalessil@gmail.com>
2025-05-13 15:16:54 +0200

    Performance: performance tweaks.

--- plugins/woocommerce/includes/wc-user-functions.php
+++ plugins/woocommerce/includes/wc-user-functions.php
@@ -398,8 +398,8 @@ add_action( 'woocommerce_order_status_completed', 'wc_paying_customer' );
  * Checks if a user (by email or ID or both) has bought an item.
  *
  * @param string $customer_email Customer email to check.
- * @param int    $user_id User ID to check.
- * @param int    $product_id Product ID to check.
+ * @param int    $user_id        User ID to check.
+ * @param int    $product_id     Product ID to check.
  * @return bool
  */
 function wc_customer_bought_product( $customer_email, $user_id, $product_id ) {
