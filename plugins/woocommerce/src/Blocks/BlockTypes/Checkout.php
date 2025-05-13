915c3fcbaab74497d4ff391613fff6e62464bc8e
Raluca Stan <ralucastn@gmail.com>
2025-05-13 18:38:32 +0300

    fix linting

--- plugins/woocommerce/src/Blocks/BlockTypes/Checkout.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/Checkout.php
@@ -473,7 +473,9 @@ class Checkout extends AbstractBlock {
 					wc_get_logger()->warning(
 						sprintf(
 							/* translators: %s: address field for shipping rates */
-							__( 'Address fields for shipping rates values must be strings. Non-string value removed: %s', 'woocommerce' ), $value ),
+							__('Address fields for shipping rates values must be strings. Non-string value removed: %s', 'woocommerce' ),
+							$value
+						),
 						array( 'source' => 'woocommerce_address_fields_for_shipping_rates' )
 					);
 					unset( $filtered_fields[ $key ] );
