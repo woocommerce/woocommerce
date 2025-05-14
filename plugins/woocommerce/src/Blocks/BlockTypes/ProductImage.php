77fbc4e4035e8e864076e4c9f50483f6ecfae831
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-05-14 16:01:46 +0200

    Render On Sale Badge in Product Image instead of recreating the content

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductImage.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductImage.php
@@ -69,34 +69,19 @@ class ProductImage extends AbstractBlock {
 			return '';
 		}
 
-		$font_size = StyleAttributesUtils::get_font_size_class_and_style( $attributes );
-
-		/**
-		 * Filters the product sale badge text.
-		 *
-		 * @hook woocommerce_sale_badge_text
-		 * @since 9.9.0
-		 *
-		 * @param string $sale_text The sale badge text.
-		 * @param WC_Product $product The product object.
-		 * @return string The filtered sale badge text.
-		 */
-		$sale_text = apply_filters( 'woocommerce_sale_badge_text', __( 'Sale', 'woocommerce' ), $product );
-
-		$on_sale_badge = sprintf(
-			'
-		<div class="wc-block-components-product-sale-badge wc-block-components-product-sale-badge--align-%s wc-block-grid__product-onsale %s" style="%s">
-			<span aria-hidden="true">%s</span>
-			<span class="screen-reader-text">%s</span>
-		</div>
-	',
-			esc_attr( $attributes['saleBadgeAlign'] ),
-			isset( $font_size['class'] ) ? esc_attr( $font_size['class'] ) : '',
-			isset( $font_size['style'] ) ? esc_attr( $font_size['style'] ) : '',
-			esc_html( $sale_text ),
-			esc_html__( 'Product on sale', 'woocommerce' )
+		$block = new \WP_Block(
+			array(
+				'blockName' => 'woocommerce/product-sale-badge',
+				'attrs'     => array(
+					'align' => $attributes['saleBadgeAlign'],
+				),
+			),
+			array(
+				'postId' => $product->get_id(),
+			)
 		);
-		return $on_sale_badge;
+
+		return $block->render();
 	}
 
 	/**
