9ce1780c78b48200bc051d66d6d439385599bb4a
Jason Kytros <jason.kytros@automattic.com>
2025-05-14 13:21:23 +0300

    Added admin validation for Minimum/Maximum spend coupon fields (#57942)
    * Added admin validation for Minimum/Maximum spend coupon fields
    
    * Add changefile(s) from automation for the following project(s): woocommerce
    
    * Ensure that Maximum amount is set before validating it
    
    * Remove check for maximum amount greater than zero
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Panos (Panagiotis) Synetos <2484390+PanosSynetos@users.noreply.github.com>

--- plugins/woocommerce/includes/class-wc-coupon.php
+++ plugins/woocommerce/includes/class-wc-coupon.php
@@ -733,6 +733,10 @@ class WC_Coupon extends WC_Legacy_Coupon {
 	 * @param float $amount Maximum amount.
 	 */
 	public function set_maximum_amount( $amount ) {
+		if ( $amount && $this->get_minimum_amount() > $amount ) {
+			$this->error( 'coupon_invalid_maximum_amount', __( 'Invalid maximum spend value.', 'woocommerce' ) );
+		}
+
 		$this->set_prop( 'maximum_amount', wc_format_decimal( $amount ) );
 	}
 
