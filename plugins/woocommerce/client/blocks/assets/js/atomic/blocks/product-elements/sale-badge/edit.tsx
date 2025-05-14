002c2ec4a4966f0560d9e3b38bad4d7ff11cac29
Luigi Teschio <gigitux@gmail.com>
2025-05-14 10:24:35 +0200

    lint code

--- plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/sale-badge/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/sale-badge/edit.tsx
@@ -4,7 +4,6 @@
 import { useBlockProps } from '@wordpress/block-editor';
 import type { BlockEditProps } from '@wordpress/blocks';
 import { ProductQueryContext as Context } from '@woocommerce/blocks/product-query/types';
-import { useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
