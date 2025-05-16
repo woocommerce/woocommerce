a2d3b0898eb7fba424a8f5f6df3f5fe9c5b1dcc6
Vlad Olaru <vlad@pixelgrade.com>
2025-05-17 01:01:58 +0300

    Lint fixes

--- plugins/woocommerce/client/admin/client/payments/payment-recommendations-wrapper.tsx
+++ plugins/woocommerce/client/admin/client/payments/payment-recommendations-wrapper.tsx
@@ -7,8 +7,7 @@
  */
 import { EmbeddedBodyProps } from '../embedded-body-layout/embedded-body-props';
 
-export const PaymentRecommendations = ( {
-}: EmbeddedBodyProps ) => {
+export const PaymentRecommendations = ( {}: EmbeddedBodyProps ) => {
 	// With the new Payments settings page, this is no longer needed.
 	return null;
 };
