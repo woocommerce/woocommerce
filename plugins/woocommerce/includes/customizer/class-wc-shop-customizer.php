46717ded9a482bea4236aa3effd45426d5fb3151
Amit Raj <harsh.harshraj.ra@gmail.com>
2025-05-16 18:14:43 +0530

    revert: 598bef4

--- plugins/woocommerce/includes/customizer/class-wc-shop-customizer.php
+++ plugins/woocommerce/includes/customizer/class-wc-shop-customizer.php
@@ -325,7 +325,7 @@ class WC_Shop_Customizer {
 		$options = apply_filters(
 			'woocommerce_default_catalog_orderby_options',
 			array(
-				'menu_order' => __( 'Manual (menu order + name)', 'woocommerce' ),
+				'menu_order' => __( 'Default sorting (custom ordering + name)', 'woocommerce' ),
 				'popularity' => __( 'Popularity (sales)', 'woocommerce' ),
 				'rating'     => __( 'Average rating', 'woocommerce' ),
 				'date'       => __( 'Sort by most recent', 'woocommerce' ),
@@ -496,7 +496,7 @@ class WC_Shop_Customizer {
 				'choices'     => apply_filters(
 					'woocommerce_default_catalog_orderby_options',
 					array(
-						'menu_order' => __( 'Manual (menu order + name)', 'woocommerce' ),
+						'menu_order' => __( 'Default sorting (custom ordering + name)', 'woocommerce' ),
 						'popularity' => __( 'Popularity (sales)', 'woocommerce' ),
 						'rating'     => __( 'Average rating', 'woocommerce' ),
 						'date'       => __( 'Sort by most recent', 'woocommerce' ),
