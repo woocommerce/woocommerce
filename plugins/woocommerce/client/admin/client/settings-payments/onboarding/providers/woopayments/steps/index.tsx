583b29764c2e763d18fff566c01319b15e9e191e
Ahmed <ahmed.el.azzabi@automattic.com>
2025-05-22 13:41:09 +0100

    add LYS-specific payments steps

--- plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/index.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/index.tsx
@@ -54,3 +54,37 @@ export const steps: WooPaymentsProviderOnboardingStep[] = [
 		content: <FinishStep />,
 	},
 ];
+
+export const LYSPaymentsSteps: WooPaymentsProviderOnboardingStep[] = [
+	{
+		id: 'payment_methods',
+		order: 1,
+		type: 'backend',
+		label: 'Choose your payment methods',
+		content: <PaymentMethodsSelection />,
+	},
+	{
+		id: 'wpcom_connection',
+		order: 2,
+		type: 'backend',
+		label: 'Connect with WordPress.com',
+		content: <WordPressComStep />,
+		dependencies: [ 'payment_methods' ],
+	},
+	{
+		id: 'business_verification',
+		order: 4,
+		type: 'backend',
+		label: 'Activate Payments',
+		dependencies: [ 'test_account' ],
+		content: <BusinessVerificationStep />,
+	},
+	{
+		id: 'finish',
+		order: 5,
+		type: 'frontend',
+		label: 'Submit for verification',
+		dependencies: [ 'business_verification' ],
+		content: <FinishStep />,
+	},
+];
