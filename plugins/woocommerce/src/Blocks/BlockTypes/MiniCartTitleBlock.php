19b7acc69c71ea181ec1a9fb45c5c6ee1b4f2d75
Sam Seay <samueljseay@gmail.com>
2025-05-22 19:17:58 +0800

    Implement the mini cart title block powered by iAPI

--- plugins/woocommerce/src/Blocks/BlockTypes/MiniCartTitleBlock.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/MiniCartTitleBlock.php
@@ -1,6 +1,8 @@
 <?php
 namespace Automattic\WooCommerce\Blocks\BlockTypes;
 
+use Automattic\WooCommerce\Admin\Features\Features;
+
 /**
  * MiniCartTitleBlock class.
  */
@@ -11,4 +13,63 @@ class MiniCartTitleBlock extends AbstractInnerBlock {
 	 * @var string
 	 */
 	protected $block_name = 'mini-cart-title-block';
+
+	/**
+	 * Render the block.
+	 *
+	 * @param array    $attributes Block attributes.
+	 * @param string   $content    Block content.
+	 * @param WP_Block $block      Block instance.
+	 * @return string Rendered block type output.
+	 */
+	protected function render( $attributes, $content, $block ) {
+		if ( Features::is_enabled( 'experimental-iapi-mini-cart' ) ) {
+			return $this->render_experimental_iapi_title_block( $attributes, $content, $block );
+		}
+		return $content;
+	}
+
+	/**
+	 * Render the interactivity API powered experimental title block.
+	 *
+	 * @param array    $attributes Block attributes.
+	 * @param string   $content    Block content.
+	 * @param WP_Block $block      Block instance.
+	 * @return string Rendered block type output.
+	 */
+	protected function render_experimental_iapi_title_block( $attributes, $content, $block ) {
+		$cart            = $this->get_cart_instance();
+		$cart_item_count = $cart ? $cart->get_cart_contents_count() : 0;
+		// translators: %d number of items in the cart.
+		$cart_item_text = _n( '(%d item)', '(%d items)', $cart_item_count, 'woocommerce' );
+
+		ob_start();
+		?>
+		<h2 class="wp-block-woocommerce-mini-cart-title-block wc-block-mini-cart__title">
+			<span class="wp-block-woocommerce-mini-cart-title-label-block">Your cart</span>
+			<span class="wp-block-woocommerce-mini-cart-title-items-counter-block">
+				<?php
+					// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					printf( $cart_item_text, $cart_item_count );
+				?>
+			</span>
+		</h2>
+		<?php
+		return ob_get_clean();
+	}
+
+	/**
+	 * Return the main instance of WC_Cart class.
+	 *
+	 * @return \WC_Cart CartController class instance.
+	 */
+	protected function get_cart_instance() {
+		$cart = WC()->cart;
+
+		if ( $cart && $cart instanceof \WC_Cart ) {
+			return $cart;
+		}
+
+		return null;
+	}
 }
