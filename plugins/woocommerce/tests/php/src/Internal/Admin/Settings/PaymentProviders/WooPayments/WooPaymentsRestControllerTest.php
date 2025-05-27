50225cabb562eb604a9f659a382a3175659f547e
Vlad Olaru <vlad@pixelgrade.com>
2025-05-27 15:13:05 +0300

    test: Add unit tests for onboarding preload

--- plugins/woocommerce/tests/php/src/Internal/Admin/Settings/PaymentProviders/WooPayments/WooPaymentsRestControllerTest.php
+++ plugins/woocommerce/tests/php/src/Internal/Admin/Settings/PaymentProviders/WooPayments/WooPaymentsRestControllerTest.php
@@ -135,6 +135,7 @@ class WooPaymentsRestControllerTest extends WC_REST_Unit_Test_Case {
 		$this->assertArrayHasKey( 'kyc_session', $step['actions'] );
 		$this->assertArrayHasKey( 'kyc_session_finish', $step['actions'] );
 		$this->assertArrayHasKey( 'kyc_fallback', $step['actions'] );
+		$this->assertArrayHasKey( 'clean', $step['actions'] );
 
 		// Clean up.
 		remove_filter( 'user_has_cap', $filter_callback );
@@ -940,6 +941,58 @@ class WooPaymentsRestControllerTest extends WC_REST_Unit_Test_Case {
 
 		// Act.
 		$request = new WP_REST_Request( 'POST', self::ENDPOINT . '/onboarding/step/' . $step_id . '/kyc_session/finish' );
+		$request->set_param( 'location', $country_code );
+		$response = $this->server->dispatch( $request );
+
+		// Assert.
+		$this->assertSame( $expected_code, $response->get_data()['code'] );
+		$this->assertSame( $expected_message, $response->get_data()['message'] );
+		$this->assertSame( $expected_http_code, $response->get_status() );
+	}
+
+	/**
+	 * Test onboarding preload.
+	 */
+	public function test_handle_onboarding_preload() {
+		// Arrange.
+		$country_code = 'US';
+
+		$this->mock_woopayments_service
+			->expects( $this->once() )
+			->method( 'onboarding_preload' )
+			->with( $country_code )
+			->willReturn( array( 'success' => true ) );
+
+		// Act.
+		$request = new WP_REST_Request( 'POST', self::ENDPOINT . '/onboarding/preload' );
+		$request->set_param( 'location', $country_code );
+		$response = $this->server->dispatch( $request );
+
+		// Assert.
+		$this->assertSame( 200, $response->get_status() );
+
+		$data = $response->get_data();
+		$this->assertArrayHasKey( 'success', $data );
+		$this->assertTrue( $data['success'] );
+	}
+
+	/**
+	 * Test onboarding preload with exception.
+	 */
+	public function test_handle_onboarding_preload_with_exception() {
+		// Arrange.
+		$country_code = 'US';
+
+		$expected_code      = 'test_exception';
+		$expected_message   = 'Test exception message.';
+		$expected_http_code = 123;
+		$this->mock_woopayments_service
+			->expects( $this->once() )
+			->method( 'onboarding_preload' )
+			->willThrowException( new ApiException( $expected_code, $expected_message, $expected_http_code ) );
+
+		// Act.
+		$request = new WP_REST_Request( 'POST', self::ENDPOINT . '/onboarding/preload' );
 		$request->set_param( 'location', $country_code );
 		$response = $this->server->dispatch( $request );
 
