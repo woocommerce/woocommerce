e6067f4ea0970681e45dc80b652ade7c6324519d
Jaclyn Chen <watertranquil@gmail.com>
2025-05-27 11:41:07 +0800

    Move POS feature switch check to POS settings class so that the tab visibility is updated when the feature is toggled.

--- plugins/woocommerce/includes/admin/class-wc-admin-settings.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-settings.php
@@ -8,7 +8,6 @@
 
 use Automattic\Jetpack\Constants;
 use Automattic\WooCommerce\Admin\Features\Features;
-use Automattic\WooCommerce\Utilities\FeaturesUtil;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -62,9 +61,7 @@ if ( ! class_exists( 'WC_Admin_Settings', false ) ) :
 				if ( \Automattic\WooCommerce\Admin\Features\Features::is_enabled( 'launch-your-store' ) ) {
 					$settings[] = include __DIR__ . '/settings/class-wc-settings-site-visibility.php';
 				}
-				if ( FeaturesUtil::feature_is_enabled( 'point_of_sale' ) ) {
-					$settings[] = include __DIR__ . '/settings/class-wc-settings-point-of-sale.php';
-				}
+				$settings[] = include __DIR__ . '/settings/class-wc-settings-point-of-sale.php';
 				$settings[] = include __DIR__ . '/settings/class-wc-settings-advanced.php';
 
 				self::$settings = apply_filters( 'woocommerce_get_settings_pages', $settings );
