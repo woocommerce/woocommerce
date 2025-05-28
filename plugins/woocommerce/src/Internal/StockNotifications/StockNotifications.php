58c65e85ea2a46cb3e4ae99db4ac0d55ead6d330
Chris Lilitsas <1105590+xristos3490@users.noreply.github.com>
2025-05-28 14:30:26 +0300

    Cleanup renamed files

--- plugins/woocommerce/src/Internal/StockNotifications/StockNotifications.php
+++ plugins/woocommerce/src/Internal/StockNotifications/StockNotifications.php
@@ -5,6 +5,7 @@ declare( strict_types = 1 );
 namespace Automattic\WooCommerce\Internal\StockNotifications;
 
 use Automattic\WooCommerce\Internal\DataStores\StockNotifications\StockNotificationsDataStore;
+use Automattic\WooCommerce\Internal\StockNotifications\Emails\EmailManager;
 
 /**
  * The controller for the stock notifications.
@@ -17,7 +18,20 @@ class StockNotifications {
 	 * @internal
 	 */
 	final public function init() {
+		add_action( 'plugins_loaded', array( $this, 'init_hooks' ) );
+	}
+
+	/**
+	 * Regiter hooks and services.
+	 *
+	 * @internal
+	 */
+	public function init_hooks() {
+
 		add_filter( 'woocommerce_data_stores', array( $this, 'register_data_stores' ) );
+
+		$container = wc_get_container();
+		$container->get( EmailManager::class );
 	}
 
 	/**
@@ -27,6 +41,10 @@ class StockNotifications {
 	 * @return array
 	 */
 	public function register_data_stores( $data_stores ) {
+		if ( ! is_array( $data_stores ) ) {
+			return $data_stores;
+		}
+
 		$data_stores['stock_notification'] = wc_get_container()->get( StockNotificationsDataStore::class );
 		return $data_stores;
 	}
