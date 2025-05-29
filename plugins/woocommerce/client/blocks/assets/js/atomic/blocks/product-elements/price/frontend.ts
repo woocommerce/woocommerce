ac3f3d8d4b3abbe07b3c4df877bf908cbd47beb8
Luigi Teschio <gigitux@gmail.com>
2025-05-29 16:45:59 +0200

    fix unlock

--- plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/price/frontend.ts
+++ plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/price/frontend.ts
@@ -2,7 +2,12 @@
  * External dependencies
  */
 import { getContext, getElement, store } from '@wordpress/interactivity';
+
 import { DisplayedProduct } from '../types';
+
+// Stores are locked to prevent 3PD usage until the API is stable.
+const universalLock =
+	'I acknowledge that using a private store means my plugin will inevitably break on the next store release.';
 
 const productPriceStore = store(
 	'woocommerce/product-price',
@@ -18,7 +23,11 @@ const productPriceStore = store(
 				if ( context ) {
 					element.ref.innerHTML = context.displayedProduct.price_html;
 				} else {
-					const { state } = store< WooCommerce >( 'woocommerce' );
+					const { state } = store< WooCommerce >(
+						'woocommerce',
+						{},
+						{ lock: universalLock }
+					);
 
 					if ( state.displayedProduct ) {
 						element.ref.innerHTML =
