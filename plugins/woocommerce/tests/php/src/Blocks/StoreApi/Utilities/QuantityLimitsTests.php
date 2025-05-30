dc42e3a1e2a09b145a8c5cce1bd214dabfd4a9c8
Tom Cafferkey <tjcafferkey@gmail.com>
2025-05-30 08:40:29 +0100

    Tests for backorders and sold individually

--- plugins/woocommerce/tests/php/src/Blocks/StoreApi/Utilities/QuantityLimitsTests.php
+++ plugins/woocommerce/tests/php/src/Blocks/StoreApi/Utilities/QuantityLimitsTests.php
@@ -91,4 +91,112 @@ class QuantityLimitsTests extends TestCase {
 
 		$this->assertEquals( 1, $limits['maximum'], 'When stock quantity is sold individually, maximum quantity should be 1' );
 	}
+
+	/**
+	 * Test quantity limit when backorders are allowed.
+	 */
+	public function test_quantity_limit_when_backorders_allowed() {
+		$fixtures = new FixtureData();
+		$product  = $fixtures->get_simple_product(
+			array(
+				'name'          => 'Test Product',
+				'regular_price' => 10,
+			)
+		);
+
+		// Enable stock management globally.
+		update_option( 'woocommerce_manage_stock', 'yes' );
+
+		// Set up product with stock management and backorders allowed.
+		$product->set_manage_stock( 'no' );
+		$product->set_stock_quantity( 10 );
+		$product->set_backorders( 'yes' );
+		$product->save();
+
+		$quantity_limits = new QuantityLimits();
+		$limits          = $quantity_limits->get_add_to_cart_limits( $product );
+
+		$this->assertEquals( 9999, $limits['maximum'], 'When backorders are allowed, maximum quantity should be 9999' );
+	}
+
+	/**
+	 * Test quantity limit when backorders are not allowed.
+	 */
+	public function test_quantity_limit_when_backorders_not_allowed() {
+		$fixtures = new FixtureData();
+		$product  = $fixtures->get_simple_product(
+			array(
+				'name'          => 'Test Product',
+				'regular_price' => 10,
+			)
+		);
+
+		// Enable stock management globally.
+		update_option( 'woocommerce_manage_stock', 'yes' );
+
+		// Set up product with stock management and backorders not allowed.
+		$product->set_manage_stock( true );
+		$product->set_stock_quantity( 10 );
+		$product->set_backorders( 'no' );
+		$product->save();
+
+		$quantity_limits = new QuantityLimits();
+		$limits          = $quantity_limits->get_add_to_cart_limits( $product );
+
+		$this->assertEquals( 10, $limits['maximum'], 'When backorders are not allowed, maximum quantity should be limited to stock quantity' );
+	}
+
+	/**
+	 * Test quantity limit when product is sold individually with stock management.
+	 */
+	public function test_quantity_limit_when_sold_individually_with_stock() {
+		$fixtures = new FixtureData();
+		$product  = $fixtures->get_simple_product(
+			array(
+				'name'          => 'Test Product',
+				'regular_price' => 10,
+			)
+		);
+
+		// Enable stock management globally.
+		update_option( 'woocommerce_manage_stock', 'yes' );
+
+		// Set up product as sold individually with stock management.
+		$product->set_manage_stock( true );
+		$product->set_stock_quantity( 10 );
+		$product->set_backorders( 'no' );
+		$product->set_sold_individually( true );
+		$product->save();
+
+		$quantity_limits = new QuantityLimits();
+		$limits          = $quantity_limits->get_add_to_cart_limits( $product );
+
+		$this->assertEquals( 1, $limits['maximum'], 'When product is sold individually, maximum quantity should be 1 regardless of stock' );
+	}
+
+	/**
+	 * Test quantity limit when product is sold individually without stock management.
+	 */
+	public function test_quantity_limit_when_sold_individually_without_stock() {
+		$fixtures = new FixtureData();
+		$product  = $fixtures->get_simple_product(
+			array(
+				'name'          => 'Test Product',
+				'regular_price' => 10,
+			)
+		);
+
+		// Disable stock management globally.
+		update_option( 'woocommerce_manage_stock', 'no' );
+
+		// Set up product as sold individually without stock management.
+		$product->set_manage_stock( false );
+		$product->set_sold_individually( true );
+		$product->save();
+
+		$quantity_limits = new QuantityLimits();
+		$limits          = $quantity_limits->get_add_to_cart_limits( $product );
+
+		$this->assertEquals( 1, $limits['maximum'], 'When product is sold individually without stock management, maximum quantity should be 1' );
+	}
 }
