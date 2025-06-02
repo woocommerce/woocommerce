5c8ff0f58521d23456dd8c2409f87f42ca597da4
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-06-02 17:33:25 +0200

    Provide generic fallback alt

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductGallery.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductGallery.php
@@ -118,8 +118,8 @@ class ProductGallery extends AbstractBlock {
 		$initial_image_id       = count( $image_ids ) > 0 ? $image_ids[0] : -1;
 		$classname_single_image = count( $image_ids ) < 2 ? 'is-single-product-gallery-image' : '';
 		$product_id             = strval( $product->get_id() );
-		$full_image_data        = ProductGalleryUtils::get_image_src_data( $image_ids, 'full' );
-		$gallery_with_dialog    = $this->inject_dialog( $content, $this->render_dialog( $full_image_data ) );
+		$fullsize_image_data    = ProductGalleryUtils::get_image_src_data( $image_ids, 'full', $product->get_title() );
+		$gallery_with_dialog    = $this->inject_dialog( $content, $this->render_dialog( $fullsize_image_data ) );
 		$p                      = new \WP_HTML_Tag_Processor( $gallery_with_dialog );
 
 		if ( $p->next_tag() ) {
--- plugins/woocommerce/src/Blocks/Utils/ProductGalleryUtils.php
+++ plugins/woocommerce/src/Blocks/Utils/ProductGalleryUtils.php
@@ -38,7 +38,7 @@ class ProductGalleryUtils {
 	 */
 	public static function get_product_gallery_image_data( $product, $size ) {
 		$all_image_ids = self::get_all_image_ids( $product );
-		return self::get_image_src_data( $all_image_ids, $size );
+		return self::get_image_src_data( $all_image_ids, $size, $product->get_title() );
 	}
 
 	/**
@@ -57,12 +57,13 @@ class ProductGalleryUtils {
 	 *
 	 * @param array  $image_ids The image IDs to retrieve the source data for.
 	 * @param string $size The size of the image to retrieve.
+	 * @param string $product_title The title of the product used for alt fallback.
 	 * @return array An array of image source data.
 	 */
-	public static function get_image_src_data( $image_ids, $size ) {
+	public static function get_image_src_data( $image_ids, $size, $product_title = '' ) {
 		$image_src_data = array();
 
-		foreach ( $image_ids as $image_id ) {
+		foreach ( $image_ids as $index => $image_id ) {
 			if ( 0 === $image_id ) {
 				// Handle placeholder image.
 				$image_src_data[] = array(
@@ -88,7 +89,7 @@ class ProductGalleryUtils {
 				'src'    => $full_src ? $full_src[0] : '',
 				'srcset' => $srcset ? $srcset : '',
 				'sizes'  => $sizes ? $sizes : '',
-				'alt'    => $alt ? $alt : '',
+				'alt'    => $alt ? $alt : sprintf( '%s - Image %d', $product_title, $index + 1 ),
 			);
 		}
 
