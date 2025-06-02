68532b23a308545abbe1ff6c144f6161d05e6777
Amit Raj <harsh.harshraj.ra@gmail.com>
2025-06-02 11:39:48 +0530

    feat: Add useCanvasMode hook to manage canvas edit mode state

---
+++ plugins/woocommerce/client/blocks/assets/js/shared/stores/canvas-mode/use-canvas-mode.ts
@@ -0,0 +1,33 @@
+/**
+ * External dependencies
+ */
+import { useSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { store as canvasModeStore } from './index';
+
+type CanvasModeSelector = {
+	isEditMode: boolean;
+};
+
+/**
+ * Hook to get canvas mode data from the store.
+ * Returns true if canvas=edit is in the URL, false otherwise.
+ * Store state changes trigger re-renders when URL changes.
+ *
+ * @return {CanvasModeSelector} The canvas mode data.
+ */
+export default function useCanvasMode(): CanvasModeSelector {
+	const { isEditMode } = useSelect( ( select ) => {
+		const { isEditMode: getIsEditMode } = select( canvasModeStore );
+		return {
+			isEditMode: getIsEditMode(),
+		};
+	}, [] );
+
+	return {
+		isEditMode,
+	};
+}
