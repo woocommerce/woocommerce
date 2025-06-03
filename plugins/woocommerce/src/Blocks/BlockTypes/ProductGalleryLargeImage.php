bc5e6fcb28d47c748ee7f22bb6fa725137fa1a0f
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-06-03 15:48:26 +0200

    Make large image gallery focusable and make it react to keyboard arrows

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductGalleryLargeImage.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductGalleryLargeImage.php
@@ -119,8 +119,11 @@ class ProductGalleryLargeImage extends AbstractBlock {
 		?>
 			<ul
 				class="wc-block-product-gallery-large-image__container"
-				tabindex="-1"
 				data-wp-interactive="woocommerce/product-gallery"
+				data-wp-on--keydown="actions.onSelectedLargeImageKeyDown"
+				aria-label="<?php esc_attr_e( 'Product gallery', 'woocommerce' ); ?>"
+				tabindex="0"
+				aria-roledescription="carousel"
 			>
 				<?php foreach ( $image_data as $index => $image ) : ?>
 					<li class="wc-block-product-gallery-large-image__wrapper">
@@ -131,11 +134,9 @@ class ProductGalleryLargeImage extends AbstractBlock {
 							sizes="<?php echo esc_attr( $image['sizes'] ); ?>"
 							data-image-id="<?php echo esc_attr( $image['id'] ); ?>"
 							alt="<?php echo esc_attr( $image['alt'] ); ?>"
-							data-wp-on--keydown="actions.onSelectedLargeImageKeyDown"
 							data-wp-on--touchstart="actions.onTouchStart"
 							data-wp-on--touchmove="actions.onTouchMove"
 							data-wp-on--touchend="actions.onTouchEnd"
-							data-wp-watch="callbacks.toggleActiveImageAttributes"
 							<?php if ( $context['hoverZoom'] ) : ?>
 								data-wp-on--mousemove="actions.startZoom"
 								data-wp-on--mouseleave="actions.resetZoom"
@@ -149,7 +150,7 @@ class ProductGalleryLargeImage extends AbstractBlock {
 								fetchpriority="low"
 								loading="lazy"
 							<?php endif; ?>
-							tabindex="0"
+							tabindex="-1"
 						/>
 					</li>
 				<?php endforeach; ?>
