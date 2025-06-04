0ea453f16a1341a7b841f15f6151fa4efa065eae
Raluca Stan <ralucastn@gmail.com>
2025-06-04 14:54:40 +0200

    Improve payment options skeleton

--- plugins/woocommerce/client/blocks/assets/js/base/components/skeleton/patterns/checkout-payment/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/base/components/skeleton/patterns/checkout-payment/index.tsx
@@ -7,8 +7,11 @@ import './style.scss';
 export const CheckoutPaymentOptionSkeleton = () => {
 	return (
 		<div className="wc-block-components-skeleton wc-block-components-skeleton--checkout-payment-option">
-			<Skeleton height="22px" width="22px" borderRadius="100%" />
-			<Skeleton height="22px" maxWidth="148px" />
+			<div className="wc-block-components-skeleton--checkout-payment-option-container">
+				<Skeleton height="22px" width="22px" borderRadius="100%" />
+				<Skeleton height="22px" maxWidth="148px" />
+			</div>
+			<Skeleton height="22px" />
 		</div>
 	);
 };
