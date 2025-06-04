be374af192bf93874d654e217ab160286c6e2f2f
Raluca Stan <ralucastn@gmail.com>
2025-06-04 14:39:31 +0200

    Add Payment options skeleton

--- plugins/woocommerce/client/blocks/assets/js/base/components/skeleton/patterns/checkout-payment/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/base/components/skeleton/patterns/checkout-payment/index.tsx
@@ -2,13 +2,13 @@
  * Internal dependencies
  */
 import { Skeleton } from '../..';
+import './style.scss';
 
-export const CheckoutPaymentSkeleton = () => {
+export const CheckoutPaymentOptionSkeleton = () => {
 	return (
-		<div className="wc-block-components-skeleton wc-block-components-skeleton--checkout-payment">
-			<Skeleton height="28px" width="177px" />
-			<Skeleton width="172px" />
-			<Skeleton height="47px" />
+		<div className="wc-block-components-skeleton wc-block-components-skeleton--checkout-payment-option">
+			<Skeleton height="22px" width="22px" borderRadius="100%" />
+			<Skeleton height="22px" maxWidth="148px" />
 		</div>
 	);
 };
