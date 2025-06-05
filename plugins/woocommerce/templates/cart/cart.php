591ab37b77f5e953879ebb2aa18e5d7912188e4f
Mahfuzur Rahman <mahfuz468865@gmail.com>
2025-06-05 21:37:18 +0600

    Updated the @version to 10.0.0

--- plugins/woocommerce/templates/cart/cart.php
+++ plugins/woocommerce/templates/cart/cart.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 7.9.0
+ * @version 10.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
