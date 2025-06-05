6b64060dc4521c028665e692d55bcdc42e2dc829
Raluca Stan <ralucastn@gmail.com>
2025-06-05 16:53:22 +0200

    Revert to JS handling of the CheckoutOrderSummaryFill Using the reverted condition, to make sure this doesn't get rendered before we have a value for !isLarge

--- plugins/woocommerce/client/blocks/assets/js/blocks/checkout/inner-blocks/checkout-order-summary-block/frontend.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/checkout/inner-blocks/checkout-order-summary-block/frontend.tsx
@@ -24,27 +24,28 @@ const FrontendBlock = ( {
 	className?: string;
 } ): JSX.Element | null => {
 	const { cartTotals } = useStoreCart();
-	const { isLarge } = useContainerWidthContext();
+	const { isMedium, isSmall, isMobile } = useContainerWidthContext();
 	const [ isOpen, setIsOpen ] = useState( false );
 
 	const totalsCurrency = getCurrencyFromPriceResponse( cartTotals );
 	const totalPrice = parseInt( cartTotals.total_price, 10 );
 	const ariaControlsId = useId();
 
-	const orderSummaryProps = ! isLarge
-		? {
-				role: 'button',
-				onClick: () => setIsOpen( ! isOpen ),
-				'aria-expanded': isOpen,
-				'aria-controls': ariaControlsId,
-				tabIndex: 0,
-				onKeyDown: ( event: React.KeyboardEvent ) => {
-					if ( event.key === 'Enter' || event.key === ' ' ) {
-						setIsOpen( ! isOpen );
-					}
-				},
-		  }
-		: {};
+	const orderSummaryProps =
+		isMedium || isSmall || isMobile
+			? {
+					role: 'button',
+					onClick: () => setIsOpen( ! isOpen ),
+					'aria-expanded': isOpen,
+					'aria-controls': ariaControlsId,
+					tabIndex: 0,
+					onKeyDown: ( event: React.KeyboardEvent ) => {
+						if ( event.key === 'Enter' || event.key === ' ' ) {
+							setIsOpen( ! isOpen );
+						}
+					},
+			  }
+			: {};
 
 	// Render the summary once here in the block and once in the fill. The fill can be slotted once elsewhere. The fill is only
 	// rendered on small and mobile screens.
@@ -95,25 +96,27 @@ const FrontendBlock = ( {
 				</div>
 			</div>
 
-			<CheckoutOrderSummaryFill>
-				<div
-					className={ `${ className } checkout-order-summary-block-fill-wrapper` }
-				>
-					<FormStepHeading>
-						<>{ __( 'Order summary', 'woocommerce' ) }</>
-					</FormStepHeading>
-					<div className="checkout-order-summary-block-fill">
-						{ children }
-						<div className="wc-block-components-totals-wrapper">
-							<TotalsFooterItem
-								currency={ totalsCurrency }
-								values={ cartTotals }
-							/>
+			{ ( isMedium || isSmall || isMobile ) && (
+				<CheckoutOrderSummaryFill>
+					<div
+						className={ `${ className } checkout-order-summary-block-fill-wrapper` }
+					>
+						<FormStepHeading>
+							<>{ __( 'Order summary', 'woocommerce' ) }</>
+						</FormStepHeading>
+						<div className="checkout-order-summary-block-fill">
+							{ children }
+							<div className="wc-block-components-totals-wrapper">
+								<TotalsFooterItem
+									currency={ totalsCurrency }
+									values={ cartTotals }
+								/>
+							</div>
+							<OrderMetaSlotFill />
 						</div>
-						<OrderMetaSlotFill />
 					</div>
-				</div>
-			</CheckoutOrderSummaryFill>
+				</CheckoutOrderSummaryFill>
+			) }
 		</>
 	);
 };
