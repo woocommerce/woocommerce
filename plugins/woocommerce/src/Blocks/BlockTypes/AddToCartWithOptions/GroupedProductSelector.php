f92d03d7ff4bb294d9ed74fb82a3dde26bdf3b21
Sarah Norris <sarah@sekai.co.uk>
2025-06-06 09:43:28 +0100

    Remove ajax_add_to_cart check

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/GroupedProductSelector.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/GroupedProductSelector.php
@@ -32,31 +32,9 @@ class GroupedProductSelector extends AbstractBlock {
 		global $product;
 
 		if ( $product instanceof \WC_Product && $product->is_type( 'grouped' ) ) {
-			add_filter( 'woocommerce_product_supports', array( $this, 'check_product_supports_ajax_add_to_cart' ), 10, 3 );
-
 			return $content;
 		}
 
 		return '';
-	}
-
-	/**
-	 * Add 'ajax_add_to_cart' support to a Grouped Product.
-	 *
-	 * This is needed so the ProductButton block could add a Grouped Product to
-	 * the Cart without a page refresh.
-	 *
-	 * @param  bool        $supports If features are already supported or not.
-	 * @param  string      $feature  The feature to check if is supported.
-	 * @param  \WC_Product $product  The product to check.
-	 * @return bool True if the product supports the feature, false otherwise.
-	 * @since  9.9.0
-	 */
-	public function check_product_supports_ajax_add_to_cart( $supports, $feature, $product ) {
-		if ( 'ajax_add_to_cart' === $feature ) {
-			return true;
-		}
-
-		return $supports;
 	}
 }
