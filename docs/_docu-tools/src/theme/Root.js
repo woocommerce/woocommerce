f41b8f6a0d957f2adc47ec347194f8e3bd59c823
Brian Coords <bacoords@gmail.com>
2025-06-10 11:19:14 -0700

    updates markdowncopy component to use swizzling into the page and removes all observers

--- docs/_docu-tools/src/theme/Root.js
+++ docs/_docu-tools/src/theme/Root.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { CookieBanner } from '../components/CookieBanner';
-import MarkdownCopy from '../components/MarkdownCopy';
 
 export default function Root({ children }) {
   return (
     <>
       {children}
       <CookieBanner />
-      <MarkdownCopy />
     </>
   );
 }
