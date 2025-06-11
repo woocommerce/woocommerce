5136a777cd11935f599f7337d9d65103989a160b
Tung Du <dinhtungdu@gmail.com>
2025-06-12 06:57:50 +0700

    Revert "fix: always save the product image inner blocks"
    This reverts commit 008178af145d985e292ecac70a98ae20540bfb5c.

--- plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/image/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/image/index.tsx
@@ -1,12 +1,14 @@
 /**
  * External dependencies
  */
+import clsx from 'clsx';
 import { InnerBlocks } from '@wordpress/block-editor';
 import { registerBlockType } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
  */
+import { BlockAttributes } from './types';
 import deprecated from './deprecated';
 import edit from './edit';
 import { BLOCK_ICON as icon } from './constants';
@@ -16,7 +18,13 @@ registerBlockType( metadata, {
 	deprecated,
 	icon,
 	edit,
-	save: () => {
-		return <InnerBlocks.Content />;
+	save: ( { attributes }: { attributes: BlockAttributes } ) => {
+		if (
+			attributes.isDescendentOfQueryLoop ||
+			attributes.isDescendentOfSingleProductBlock
+		) {
+			return <InnerBlocks.Content />;
+		}
+		return <div className={ clsx( 'is-loading', attributes.className ) } />;
 	},
 } );
