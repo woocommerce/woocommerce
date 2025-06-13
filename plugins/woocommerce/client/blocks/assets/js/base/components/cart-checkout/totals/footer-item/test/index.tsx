f3abbb56ea2ac453bf2a2b927ac3cc501b2000b3
Raluca Stan <ralucastn@gmail.com>
2025-06-13 16:46:46 +0200

    Update test with assertions instead of snapshots

--- plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/totals/footer-item/test/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/totals/footer-item/test/index.tsx
@@ -1,22 +1,30 @@
 /**
  * External dependencies
  */
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { allSettings } from '@woocommerce/settings';
-import { Currency, CartResponseTotals } from '@woocommerce/types';
+import { CurrencyCode } from '@woocommerce/types';
 
 /**
  * Internal dependencies
  */
 import TotalsFooterItem from '../index';
 
+jest.mock( '@woocommerce/base-context', () => ( {
+	...jest.requireActual( '@woocommerce/base-context' ),
+	useStoreCart: () => ( {
+		cartIsLoading: false,
+	} ),
+} ) );
+
 describe( 'TotalsFooterItem', () => {
 	beforeEach( () => {
 		allSettings.taxesEnabled = true;
 		allSettings.displayCartPricesIncludingTax = true;
 	} );
-	const currency: Currency = {
-		code: 'GBP',
+
+	const currency = {
+		code: 'GBP' as CurrencyCode,
 		decimalSeparator: '.',
 		minorUnit: 2,
 		prefix: '£',
@@ -25,8 +33,8 @@ describe( 'TotalsFooterItem', () => {
 		thousandSeparator: ',',
 	};
 
-	const values: CartResponseTotals = {
-		currency_code: 'GBP',
+	const values = {
+		currency_code: 'GBP' as CurrencyCode,
 		currency_decimal_separator: '.',
 		currency_minor_unit: 2,
 		currency_prefix: '£',
@@ -46,14 +54,19 @@ describe( 'TotalsFooterItem', () => {
 		total_tax: '0',
 	};
 
-	it( 'Does not show the "including %s of tax" line if tax is 0', () => {
-		const { container } = render(
-			<TotalsFooterItem currency={ currency } values={ values } />
-		);
-		expect( container ).toMatchSnapshot();
+	it( 'Does not show the "including %s of tax" line if tax is 0', async () => {
+		render( <TotalsFooterItem currency={ currency } values={ values } /> );
+
+		// Check that the total price is displayed
+		expect( screen.getByText( /£85\.00/ ) ).toBeInTheDocument();
+
+		// Check that no tax information is displayed
+		expect(
+			screen.queryByText( /including.*tax/i )
+		).not.toBeInTheDocument();
 	} );
 
-	it( 'Does not show the "including %s of tax" line if tax is disabled', () => {
+	it( 'Does not show the "including %s of tax" line if tax is disabled', async () => {
 		allSettings.taxesEnabled = false;
 		/* This shouldn't ever happen if taxes are disabled, but this is to test whether the taxesEnabled setting works */
 		const valuesWithTax = {
@@ -61,38 +74,63 @@ describe( 'TotalsFooterItem', () => {
 			total_tax: '100',
 			total_items_tax: '100',
 		};
-		const { container } = render(
+		render(
 			<TotalsFooterItem currency={ currency } values={ valuesWithTax } />
 		);
-		expect( container ).toMatchSnapshot();
+
+		// Check that the total price is displayed
+		expect( screen.getByText( /£85\.00/ ) ).toBeInTheDocument();
+
+		// Check that no tax information is displayed when taxes are disabled
+		expect(
+			screen.queryByText( /including.*tax/i )
+		).not.toBeInTheDocument();
 	} );
 
-	it( 'Shows the "including %s of tax" line if tax is greater than 0', () => {
+	it( 'Shows the "including %s of tax" line if tax is greater than 0', async () => {
 		const valuesWithTax = {
 			...values,
 			total_tax: '100',
 			total_items_tax: '100',
 		};
-		const { container } = render(
+		render(
 			<TotalsFooterItem currency={ currency } values={ valuesWithTax } />
 		);
-		expect( container ).toMatchSnapshot();
+
+		// Check that the total price is displayed
+		expect( screen.getByText( /£85\.00/ ) ).toBeInTheDocument();
+
+		// Check that tax information is displayed
+		const taxInfo = screen.getByText( /including.*tax/i );
+		expect( taxInfo ).toBeInTheDocument();
+		expect( taxInfo ).toHaveClass(
+			'wc-block-components-totals-footer-item-tax'
+		);
 	} );
 
-	it( 'Shows the "including %s TAX LABEL" line with single tax label', () => {
+	it( 'Shows the "including %s TAX LABEL" line with single tax label', async () => {
 		const valuesWithTax = {
 			...values,
 			total_tax: '100',
 			total_items_tax: '100',
 			tax_lines: [ { name: '10% VAT', price: '100', rate: '10.000' } ],
 		};
-		const { container } = render(
+		render(
 			<TotalsFooterItem currency={ currency } values={ valuesWithTax } />
 		);
-		expect( container ).toMatchSnapshot();
+
+		// Check that the total price is displayed
+		expect( screen.getByText( /£85\.00/ ) ).toBeInTheDocument();
+
+		// Check that tax information with label is displayed
+		const taxInfo = screen.getByText( /including.*10% VAT/i );
+		expect( taxInfo ).toBeInTheDocument();
+		expect( taxInfo ).toHaveClass(
+			'wc-block-components-totals-footer-item-tax'
+		);
 	} );
 
-	it( 'Shows the "including %s TAX LABELS" line with multiple tax labels', () => {
+	it( 'Shows the "including %s TAX LABELS" line with multiple tax labels', async () => {
 		const valuesWithTax = {
 			...values,
 			total_tax: '100',
@@ -102,9 +140,18 @@ describe( 'TotalsFooterItem', () => {
 				{ name: '5% VAT', price: '50', rate: '5.000' },
 			],
 		};
-		const { container } = render(
+		render(
 			<TotalsFooterItem currency={ currency } values={ valuesWithTax } />
 		);
-		expect( container ).toMatchSnapshot();
+
+		// Check that the total price is displayed
+		expect( screen.getByText( /£85\.00/ ) ).toBeInTheDocument();
+
+		// Check that tax information with multiple labels is displayed
+		const taxInfo = screen.getByText( /including.*10% VAT.*5% VAT/i );
+		expect( taxInfo ).toBeInTheDocument();
+		expect( taxInfo ).toHaveClass(
+			'wc-block-components-totals-footer-item-tax'
+		);
 	} );
 } );
