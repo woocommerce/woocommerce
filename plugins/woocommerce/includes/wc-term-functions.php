2440138b90f92eee2a7c776b8352d0bb6c9da061
Michael Pretty <prettyboymp@users.noreply.github.com>
2025-06-13 19:55:37 -0400

    aligning equal signs

--- plugins/woocommerce/includes/wc-term-functions.php
+++ plugins/woocommerce/includes/wc-term-functions.php
@@ -583,8 +583,8 @@ function wc_change_term_counts( $terms, $taxonomies ) {
 		if ( $term instanceof WP_Term && in_array( $term->taxonomy, $current_taxonomies, true ) ) {
 			$key = $term->term_id . '_' . $term->taxonomy;
 			if ( ! isset( $term_counts[ $key ] ) ) {
-				$count = get_term_meta( $term->term_id, 'product_count_' . $term->taxonomy, true );
-				$count = '' !== $count ? absint( $count ) : 0;
+				$count               = get_term_meta( $term->term_id, 'product_count_' . $term->taxonomy, true );
+				$count               = '' !== $count ? absint( $count ) : 0;
 				$term_counts[ $key ] = $count;
 			}
 
