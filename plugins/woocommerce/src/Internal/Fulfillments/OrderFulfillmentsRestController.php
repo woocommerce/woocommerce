af3910fef481c8469a029a2ae33a135c51c8133f
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2025-06-17 11:44:02 +0300

    Fix alignment

--- plugins/woocommerce/src/Internal/Fulfillments/OrderFulfillmentsRestController.php
+++ plugins/woocommerce/src/Internal/Fulfillments/OrderFulfillmentsRestController.php
@@ -422,10 +422,10 @@ class OrderFulfillmentsRestController extends RestApiControllerBase {
 
 		if ( $fulfillment->get_is_fulfilled() && $notify_customer ) {
 			/**
-		 * Trigger the fulfillment deleted notification.
-		 *
-		 * @since 9.9.0
-		 */
+			 * Trigger the fulfillment deleted notification.
+			 *
+			 * @since 9.9.0
+			 */
 			do_action( 'woocommerce_fulfillment_deleted_notification', $order_id, $fulfillment, wc_get_order( $order_id ) );
 		}
 		return new WP_REST_Response(
