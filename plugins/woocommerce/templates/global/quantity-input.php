93fb549832727005c3917019e2c19f7f1ab83619
Sarah Norris <sarah@sekai.co.uk>
2025-06-17 10:55:01 +0100

    Only render max if over 0

--- plugins/woocommerce/templates/global/quantity-input.php
+++ plugins/woocommerce/templates/global/quantity-input.php
@@ -46,7 +46,9 @@ $label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 
 			size="4"
 		<?php endif; ?>
 		min="<?php echo esc_attr( $min_value ); ?>"
-		max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
+		<?php if ( 0 < $max_value ) : ?>
+			max="<?php echo esc_attr( $max_value ); ?>"
+		<?php endif; ?>
 		<?php if ( ! $readonly ) : ?>
 			step="<?php echo esc_attr( $step ); ?>"
 			placeholder="<?php echo esc_attr( $placeholder ); ?>"
