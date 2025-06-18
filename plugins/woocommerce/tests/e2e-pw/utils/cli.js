d6a8dfe9eed2e7c424c4c81209b31a5accbbb887
Luigi Teschio <gigitux@gmail.com>
2025-06-18 16:50:13 +0200

    fix import

--- plugins/woocommerce/tests/e2e-pw/utils/cli.js
+++ plugins/woocommerce/tests/e2e-pw/utils/cli.js
@@ -2,11 +2,15 @@ const { promisify } = require( 'util' );
 
 const execAsync = promisify( require( 'child_process' ).exec );
 
-export const wpCLI = async ( command ) => {
+const wpCLI = async ( command ) => {
 	const { stdout, stderr } = await execAsync(
 		`pnpm exec wp-env run tests-cli -- ${ command }`
 	);
 
 	console.log( stdout );
 	console.error( stderr );
+};
+
+module.exports = {
+	wpCLI,
 };
