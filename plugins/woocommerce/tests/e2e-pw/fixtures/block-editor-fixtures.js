4ab17805bb7da57a2a6fbe949e6bb165ddc83124
Luigi Teschio <gigitux@gmail.com>
2025-06-19 09:55:09 +0200

    fix E2E test

--- plugins/woocommerce/tests/e2e-pw/fixtures/block-editor-fixtures.js
+++ plugins/woocommerce/tests/e2e-pw/fixtures/block-editor-fixtures.js
@@ -2,22 +2,13 @@
  * Internal dependencies
  */
 import { test as baseTest } from './fixtures';
-import { WC_API_PATH } from '../utils/api-client';
 import { ADMIN_STATE_PATH } from '../playwright.config';
 import { wpCLI } from '../utils/cli';
 
 export const test = baseTest.extend( {
 	page: async ( { page, restApi }, use ) => {
 		await wpCLI(
-			'option set woocommerce_feature_product_block_editor_enabled yes'
-		);
-
-		// Enable product block editor
-		await restApi.put(
-			`${ WC_API_PATH }/settings/advanced/woocommerce_feature_product_block_editor_enabled`,
-			{
-				value: 'yes',
-			}
+			'wp option set woocommerce_feature_product_block_editor_enabled yes'
 		);
 
 		// Disable the product editor tour
@@ -28,7 +19,7 @@ export const test = baseTest.extend( {
 		await use( page );
 
 		await wpCLI(
-			'option set woocommerce_feature_product_block_editor_enabled no'
+			'wp option set woocommerce_feature_product_block_editor_enabled no'
 		);
 	},
 	storageState: ADMIN_STATE_PATH,
