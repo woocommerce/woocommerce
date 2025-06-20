cd96077cf468770af9b7dd13eed52ae1904476eb
Kallyan Singha <singhakallyan@gmail.com>
2025-06-20 19:02:28 +0530

    Feat: Added invalid form state incase of outofstock variation product

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductButton.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductButton.php
@@ -218,7 +218,7 @@ class ProductButton extends AbstractBlock {
 		';
 
 		$button_directives = $is_descendant_of_add_to_cart_form ?
-			'data-wp-class--disabled="woocommerce/add-to-cart-with-options::!state.isFormValid"' :
+			'data-wp-bind--disabled="woocommerce/add-to-cart-with-options::!state.isFormValid"' :
 			'data-wp-on--click="actions.addCartItem"';
 		$anchor_directive  = $is_descendant_of_add_to_cart_form ? '' : 'data-wp-on--click="woocommerce/product-collection::actions.viewProduct"';
 
