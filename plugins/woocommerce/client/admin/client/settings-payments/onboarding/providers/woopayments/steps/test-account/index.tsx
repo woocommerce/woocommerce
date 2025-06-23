76421dd604690a65d62d88320c765220f7f7e7ea
Ahmed <ahmed.el.azzabi@automattic.com>
2025-06-23 17:08:59 +0200

    [NOX In-Context] Update the “Ready to test payments” screen for clarity (#59076)
    * update copy for the continue your store setup
    
    * update copy for the activate payments step
    
    * Move buttons around to match the design
    
    * CSS changes to match the new design
    
    * Add changefile(s) from automation for the following project(s): woocommerce, woocommerce/client/admin
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/test-account/index.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/test-account/index.tsx
@@ -451,18 +451,51 @@ const TestAccountStep = () => {
 									<div className="woocommerce-woopayments-modal__content__item-flex__description">
 										<h3>
 											{ __(
-												'Continue your store setup',
+												'Continue setting up your store',
 												'woocommerce'
 											) }
 										</h3>
 										<div>
 											{ __(
-												'Finish completing the tasks required to launch your store.',
+												'Test payments and finish off any other tasks required to launch your store.',
 												'woocommerce'
 											) }
 										</div>
 									</div>
 								</div>
+								<Button
+									variant="primary"
+									onClick={ () => {
+										recordPaymentsOnboardingEvent(
+											'woopayments_onboarding_modal_click',
+											{
+												step:
+													currentStep?.id ||
+													'unknown',
+												action: 'continue_store_setup',
+											}
+										);
+
+										// Navigate to wc-admin page
+										navigateTo( {
+											url: getNewPath( {}, '', {
+												page: 'wc-admin',
+											} ),
+										} );
+									} }
+								>
+									{ __(
+										'Continue store setup',
+										'woocommerce'
+									) }
+								</Button>
+
+								<div className="woocommerce-payments-test-account-step__success_content_or-divider">
+									<hr />
+									{ __( 'OR', 'woocommerce' ) }
+									<hr />
+								</div>
+
 								<div className="woocommerce-woopayments-modal__content__item-flex">
 									<img
 										src={
@@ -474,7 +507,7 @@ const TestAccountStep = () => {
 									<div className="woocommerce-woopayments-modal__content__item-flex__description">
 										<h3>
 											{ __(
-												'Activate payments',
+												'Activate real payments',
 												'woocommerce'
 											) }
 										</h3>
@@ -482,7 +515,7 @@ const TestAccountStep = () => {
 											<p>
 												{ interpolateComponents( {
 													mixedString: __(
-														'Provide some additional details about your business so you can begin accepting real payments. {{link}}Learn more{{/link}}',
+														'Provide additional details about your business so you can begin accepting real payments. {{link}}Learn more{{/link}}',
 														'woocommerce'
 													),
 													components: {
@@ -500,42 +533,15 @@ const TestAccountStep = () => {
 										</div>
 									</div>
 								</div>
+								<Button
+									variant="secondary"
+									isBusy={ isContinueButtonLoading }
+									disabled={ isContinueButtonLoading }
+									onClick={ handleContinue }
+								>
+									{ __( 'Activate payments', 'woocommerce' ) }
+								</Button>
 							</div>
-							<Button
-								variant="primary"
-								onClick={ () => {
-									recordPaymentsOnboardingEvent(
-										'woopayments_onboarding_modal_click',
-										{
-											step: currentStep?.id || 'unknown',
-											action: 'continue_store_setup',
-										}
-									);
-
-									// Navigate to wc-admin page
-									navigateTo( {
-										url: getNewPath( {}, '', {
-											page: 'wc-admin',
-										} ),
-									} );
-								} }
-							>
-								{ __( 'Continue store setup', 'woocommerce' ) }
-							</Button>
-							<div className="woocommerce-payments-test-account-step__success_content_or-divider">
-								<hr />
-								{ __( 'OR', 'woocommerce' ) }
-								<hr />
-							</div>
-
-							<Button
-								variant="secondary"
-								isBusy={ isContinueButtonLoading }
-								disabled={ isContinueButtonLoading }
-								onClick={ handleContinue }
-							>
-								{ __( 'Activate payments', 'woocommerce' ) }
-							</Button>
 						</div>
 					</div>
 				</div>
