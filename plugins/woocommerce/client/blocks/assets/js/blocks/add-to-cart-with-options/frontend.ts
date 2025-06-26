ecafb976ed8e9772767d1a3a38599505082a8edf
Kallyan Singha <singhakallyan@gmail.com>
2025-06-26 17:09:14 +0530

    Chore: Minor conflict fix

--- plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/frontend.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/frontend.ts
@@ -156,16 +156,12 @@ const addToCartWithOptionsStore = store(
 					availableVariations,
 					selectedAttributes
 				);
-<<<<<<< feat/blockified-add-to-cart-button-disable
 
 				// Variable products must be in stock and have a selected variation
 				return Boolean(
 					matchedVariation?.is_in_stock &&
 						matchedVariation?.variation_id
 				);
-=======
-				return !! matchedVariation?.variation_id;
->>>>>>> trunk
 			},
 			get variationId(): number | null {
 				const context = getContext< Context >();
