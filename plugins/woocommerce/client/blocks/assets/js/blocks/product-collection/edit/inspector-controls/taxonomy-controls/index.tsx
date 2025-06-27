10615105ede365f1aa676b0102f532f8b35b8a60
Amit Raj <harsh.harshraj.ra@gmail.com>
2025-06-27 19:45:43 +0530

    feat: Split out useTaxonomyControls in separate file

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-collection/edit/inspector-controls/taxonomy-controls/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-collection/edit/inspector-controls/taxonomy-controls/index.tsx
@@ -6,7 +6,6 @@ import { useMemo } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
 import {
-	// @ts-expect-error Using experimental features
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 	__experimentalToolsPanelItem as ToolsPanelItem,
 } from '@wordpress/components';
@@ -17,9 +16,8 @@ import {
 import TaxonomyItem from './taxonomy-item';
 import {
 	QueryControlProps,
-	CoreFilterNames,
-	CoreCollectionNames,
 } from '../../../types';
+import useTaxonomyControls from './use-taxonomy-controls';
 
 /**
  * Hook that returns the taxonomies associated with product post type.
@@ -53,68 +51,6 @@ const normalizeName = ( name: string | undefined | null ) => {
 		.map( ( word ) => word.charAt( 0 ).toUpperCase() + word.slice( 1 ) )
 		.join( ' ' );
 };
-
-/**
- * Shared hook for taxonomy control logic - filters taxonomies based on context and provides common handlers.
- */
-function useTaxonomyControls( {
-	setQueryAttribute,
-	trackInteraction,
-	query,
-	collection,
-	isFiltersPanel,
-}: QueryControlProps & { collection: string | undefined } & {
-	isFiltersPanel?: boolean;
-} ) {
-	const { taxQuery } = query;
-	const taxonomies = useTaxonomies();
-
-	const filteredTaxonomies = useMemo( () => {
-		if ( ! taxonomies || taxonomies.length === 0 ) {
-			return [];
-		}
-
-		if ( collection === CoreCollectionNames.BY_CATEGORY ) {
-			return taxonomies.filter( ( taxonomy ) =>
-				// If it's in filter panel, we want to show everything BUT the category control.
-				// Otherwise, it's a collection specific filter and we want to show ONLY the category control.
-				isFiltersPanel
-					? taxonomy.slug !== 'product_cat'
-					: taxonomy.slug === 'product_cat'
-			);
-		}
-		if ( collection === CoreCollectionNames.BY_TAG ) {
-			return taxonomies.filter( ( taxonomy ) =>
-				// If it's in filter panel, we want to show everything BUT the tag control.
-				// Otherwise, it's a collection specific filter and we want to show ONLY the tag control.
-				isFiltersPanel
-					? taxonomy.slug !== 'product_tag'
-					: taxonomy.slug === 'product_tag'
-			);
-		}
-
-		return isFiltersPanel ? taxonomies : [];
-	}, [ taxonomies, collection, isFiltersPanel ] );
-
-	const createHandleChange = ( slug: string ) => ( newTermIds: number[] ) => {
-		setQueryAttribute( {
-			taxQuery: {
-				...taxQuery,
-				[ slug ]: newTermIds,
-			},
-		} );
-		trackInteraction( `${ CoreFilterNames.TAXONOMY }__${ slug }` );
-	};
-
-	const shouldShowTaxonomyControl = filteredTaxonomies.length > 0;
-
-	return {
-		filteredTaxonomies,
-		taxQuery,
-		createHandleChange,
-		shouldShowTaxonomyControl,
-	};
-}
 
 function TaxonomyControls( {
 	setQueryAttribute,
