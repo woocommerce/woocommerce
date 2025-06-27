51fdcd4ed518694a539b4b4e88030b8a4e8e714a
Luigi Teschio <gigitux@gmail.com>
2025-06-27 11:35:07 +0200

    improve code style

--- plugins/woocommerce/client/legacy/js/admin/product-editor.js
+++ plugins/woocommerce/client/legacy/js/admin/product-editor.js
@@ -7,8 +7,7 @@ jQuery( function ( $ ) {
 		 * In the Product Editor context, the footer needs to be hidden otherwise the computation of the postbox position is wrong.
 		 * For more details, see https://github.com/woocommerce/woocommerce/pull/59212.
 		 */
-		$( '#wpfooter' ).css( 'visibility', 'hidden' );
-		$( '#wpfooter' ).css( 'display', 'unset' );
+		$( '#wpfooter' ).css( { visibility: 'hidden', display: 'unset' } );
 
 		if ( editorWrapper.length ) {
 			editorWrapper.addClass( 'postbox woocommerce-product-description' );
