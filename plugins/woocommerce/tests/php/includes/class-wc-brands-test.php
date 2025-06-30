703c4ec1541b30625e5622ba60419d601f45c5c2
M. L. Giannotta <sunyatasattva@gmail.com>
2025-06-30 23:23:58 +0200

    Lint

--- plugins/woocommerce/tests/php/includes/class-wc-brands-test.php
+++ plugins/woocommerce/tests/php/includes/class-wc-brands-test.php
@@ -5,7 +5,7 @@
  * @package woocommerce-brands
  */
 
- declare( strict_types = 1);
+declare( strict_types = 1);
 
 require_once WC_ABSPATH . '/includes/class-wc-brands.php';
 
@@ -13,7 +13,7 @@ require_once WC_ABSPATH . '/includes/class-wc-brands.php';
  * WC Brands test
  */
 class WC_Brands_Test extends WC_Unit_Test_Case {
-  /**
+	/**
 	 * Test that `product_brand_thumbnails` shortcode's `show_empty` argument works as expected.
 	 * This test prevents regression of the issue where double filtering caused no brands to be displayed.
 	 */
@@ -21,7 +21,7 @@ class WC_Brands_Test extends WC_Unit_Test_Case {
 		WC_Brands::init_taxonomy();
 
 		$brand_with_products = wp_insert_term( 'Full Brand', 'product_brand' );
-		$empty_brand = wp_insert_term( 'Empty Brand', 'product_brand' );
+		$empty_brand         = wp_insert_term( 'Empty Brand', 'product_brand' );
 
 		$product = WC_Helper_Product::create_simple_product();
 		$product->set_name( 'Test Product' );
@@ -29,17 +29,17 @@ class WC_Brands_Test extends WC_Unit_Test_Case {
 		wp_set_object_terms( $product->get_id(), array( $brand_with_products['term_id'] ), 'product_brand' );
 
 		$brands_instance = new WC_Brands();
-		$output = $brands_instance->output_product_brand_thumbnails( array( 'show_empty' => 'false' ) );
+		$output          = $brands_instance->output_product_brand_thumbnails( array( 'show_empty' => 'false' ) );
 
-    // Full brand is shown, empty brand is not.
+		// Full brand is shown, empty brand is not.
 		$this->assertStringContainsString( 'Full Brand', $output );
 		$this->assertStringNotContainsString( 'Empty Brand', $output );
 
-    $output = $brands_instance->output_product_brand_thumbnails( array( 'show_empty' => 'true' ) );
+		$output = $brands_instance->output_product_brand_thumbnails( array( 'show_empty' => 'true' ) );
 
-    // Both brands are shown.
-    $this->assertStringContainsString( 'Full Brand', $output );
-    $this->assertStringContainsString( 'Empty Brand', $output );
+		// Both brands are shown.
+		$this->assertStringContainsString( 'Full Brand', $output );
+		$this->assertStringContainsString( 'Empty Brand', $output );
 	}
 
 	/**
@@ -49,23 +49,23 @@ class WC_Brands_Test extends WC_Unit_Test_Case {
 		WC_Brands::init_taxonomy();
 
 		$brand_with_products = wp_insert_term( 'Full Brand', 'product_brand' );
-		$empty_brand = wp_insert_term( 'Empty Brand', 'product_brand' );
+		$empty_brand         = wp_insert_term( 'Empty Brand', 'product_brand' );
 
 		$product = WC_Helper_Product::create_simple_product();
 		wp_set_object_terms( $product->get_id(), array( $brand_with_products['term_id'] ), 'product_brand' );
 
 		$brands_instance = new WC_Brands();
-		$output = $brands_instance->output_product_brand_thumbnails_description( array( 'show_empty' => 'false' ) );
+		$output          = $brands_instance->output_product_brand_thumbnails_description( array( 'show_empty' => 'false' ) );
 
 		// Full brand is shown, empty brand is not.
 		$this->assertStringContainsString( 'Full Brand', $output );
 		$this->assertStringNotContainsString( 'Empty Brand', $output );
 
-    $output = $brands_instance->output_product_brand_thumbnails_description( array( 'show_empty' => 'true' ) );
+		$output = $brands_instance->output_product_brand_thumbnails_description( array( 'show_empty' => 'true' ) );
 
-    // Both brands are shown.
-    $this->assertStringContainsString( 'Full Brand', $output );
-    $this->assertStringContainsString( 'Empty Brand', $output );
+		// Both brands are shown.
+		$this->assertStringContainsString( 'Full Brand', $output );
+		$this->assertStringContainsString( 'Empty Brand', $output );
 	}
 
 	/**
@@ -75,13 +75,13 @@ class WC_Brands_Test extends WC_Unit_Test_Case {
 		WC_Brands::init_taxonomy();
 
 		$brand_with_products = wp_insert_term( 'Full Brand', 'product_brand' );
-		$empty_brand = wp_insert_term( 'Empty Brand', 'product_brand' );
+		$empty_brand         = wp_insert_term( 'Empty Brand', 'product_brand' );
 
 		$product = WC_Helper_Product::create_simple_product();
 		wp_set_object_terms( $product->get_id(), array( $brand_with_products['term_id'] ), 'product_brand' );
 
 		$brands_instance = new WC_Brands();
-		$output = $brands_instance->output_product_brand_list( array( 'show_empty_brands' => 'false' ) );
+		$output          = $brands_instance->output_product_brand_list( array( 'show_empty_brands' => 'false' ) );
 
 		// Full brand is shown, empty brand is not.
 		$this->assertStringContainsString( 'Full Brand', $output );
