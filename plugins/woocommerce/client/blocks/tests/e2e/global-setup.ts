eb0f46cacbe32e631f12d4144f065e983cd0b8bd
Tung Du <dinhtungdu@gmail.com>
2025-06-30 21:33:38 +0700

    cc

--- plugins/woocommerce/client/blocks/tests/e2e/global-setup.ts
+++ plugins/woocommerce/client/blocks/tests/e2e/global-setup.ts
@@ -3,113 +3,46 @@
 /**
  * External dependencies
  */
-import { chromium, request } from '@playwright/test';
-import { RequestUtils } from '@wordpress/e2e-test-utils-playwright';
-import {
-	adminFile,
-	wpCLI,
-	customerFile,
-	BLOCK_THEME_SLUG,
-	DB_EXPORT_FILE,
-} from '@woocommerce/e2e-utils';
+import { runCLI } from '@wp-playground/cli';
+import { createReadStream, unlinkSync } from 'fs';
+import { resolve } from 'path';
+import * as unzipper from 'unzipper';
 
 /**
  * Internal dependencies
  */
-import { customer, admin } from './test-data/data/data';
-import { BASE_URL } from './utils/constants';
 
-const prepareAttributes = async () => {
-	const browser = await chromium.launch();
-	const context = await browser.newContext( {
-		baseURL: BASE_URL,
-		storageState: adminFile,
-	} );
-
-	const page = await context.newPage();
-
-	// Intercept the dialog event. This is needed because when the regenerate
-	// button is clicked, a dialog is shown.
-	page.on( 'dialog', ( dialog ) => {
-		void dialog.accept();
-	} );
-
-	await page.goto( '/wp-admin/admin.php?page=wc-status&tab=tools' );
-
-	// Attributes regeneration should be doable via a CLI command, e.g.:
-	// "wp wc tool run regenerate_product_attributes_lookup_table --user=1"
-	// It doesn't seem to be working correctly ATM so we need to do it via
-	// browser actions.
-	// See: https://github.com/woocommerce/woocommerce/issues/32831
-	await page
-		.getByRole( 'row', {
-			name: /Regenerate the product attributes lookup table/,
-		} )
-		.getByRole( 'button' )
-		.click();
-
-	await context.close();
-	await browser.close();
-
-	// Note that the two commands below are intentionally duplicated as we need
-	// to run the cron task twice as we need to process more than 1 batch of
-	// items.
-	const cronTask =
-		'action-scheduler run --hooks="woocommerce_run_product_attribute_lookup_regeneration_callback"';
-	await wpCLI( cronTask );
-	await wpCLI( cronTask );
-};
-
-async function globalSetup() {
-	console.log( 'Running global setup:' );
-	console.time( '└ Total time' );
-
-	let databaseImported = false;
-
+export default async function () {
+	const snapshotPath = resolve( __dirname, './playground/tmp/wordpress.zip' );
+	const snapshotDir = resolve( __dirname, './playground/tmp/' );
 	try {
-		await wpCLI( `db import ${ DB_EXPORT_FILE }` );
-		console.log( '├ Database snapshot imported, running basic setup…' );
-		databaseImported = true;
+		await runCLI( {
+			command: 'build-snapshot',
+			outfile: snapshotPath,
+			blueprint: resolve( __dirname, './playground/blueprint.json' ),
+			port: 9401,
+			quiet: true,
+		} );
 	} catch ( error ) {
-		if (
-			error instanceof Error &&
-			! error.message.includes( 'Import file missing' )
-		) {
-			// Throw if the error is not related to the import file missing.
-			throw error;
-		}
-
-		console.log( '├ Database snapshot not found, running full setup…' );
+		// runCLI exits with a error that needs to be fixed in Playground
+		// Error: process.exit unexpectedly called with "0"
 	}
 
-	const requestContext = await request.newContext( {
-		baseURL: BASE_URL,
-	} );
+	// extract the snapshot zip
+	await createReadStream( snapshotPath )
+		.pipe( unzipper.Extract( { path: snapshotDir } ) )
+		.promise();
 
-	console.log( '├ Pre-authenticating users…' );
-	await new RequestUtils( requestContext, {
-		user: customer,
-		storageStatePath: customerFile,
-	} ).setupRest();
-	const requestUtils = new RequestUtils( requestContext, {
-		user: admin,
-		storageStatePath: adminFile,
-	} );
-	await requestUtils.setupRest();
+	unlinkSync(
+		resolve( snapshotDir, 'wordpres/wp-content/plugins/woocommerce' )
+	);
 
-	if ( ! databaseImported ) {
-		console.log( '├ Activating default theme…' );
-		await requestUtils.activateTheme( BLOCK_THEME_SLUG );
+	// // Create the wp-config.php file
+	// copyFileSync(
+	// 	join( snapshotDir, 'wordpress', 'wp-config-sample.php' ),
+	// 	wpConfigPath
+	// );
 
-		console.log( '├ Preparing product attributes…' );
-		await prepareAttributes();
-	}
-
-	console.log( '├ Exporting database snapshot…' );
-	await wpCLI( `db export ${ DB_EXPORT_FILE }` );
-
-	await requestContext.dispose();
-	console.timeEnd( '└ Total time' );
+	// remove the zip file
+	unlinkSync( snapshotPath );
 }
-
-export default globalSetup;
