e8328e3465c171433925416c35950988c455ac8b
Raluca Stan <ralucastn@gmail.com>
2025-07-01 18:14:44 +0200

    Expose registeredExpressPaymentMethods in the data store

--- plugins/woocommerce/client/blocks/assets/js/base/context/hooks/payment-methods/index.js
+++ plugins/woocommerce/client/blocks/assets/js/base/context/hooks/payment-methods/index.js
@@ -1,2 +1,3 @@
 export { usePaymentMethodInterface } from './use-payment-method-interface';
 export * from './use-payment-methods';
+export { useExpressPaymentLoadingState } from './use-express-payment-loading-state';
