20a8f7d925763c08e9eeb5bd71e3c70bf746d732
Naman Malhotra <naman03malhotra@gmail.com>
2025-07-02 18:31:06 +0530

    lint issues

--- plugins/woocommerce/src/Admin/Features/Blueprint/Exporters/ExportWCSettingsTax.php
+++ plugins/woocommerce/src/Admin/Features/Blueprint/Exporters/ExportWCSettingsTax.php
@@ -19,12 +19,12 @@ use Automattic\WooCommerce\Admin\Features\Blueprint\SettingOptions;
 class ExportWCSettingsTax extends ExportWCSettings {
 	use UseWPFunctions;
 
+	// phpcs:ignore Generic.CodeAnalysis.UselessOverridingMethod.Found
 	/**
 	 * Constructor.
 	 *
 	 * @param SettingOptions|null $setting_options The setting options class.
 	 */
-	// phpcs:ignore Generic.CodeAnalysis.UselessOverridingMethod.Found
 	public function __construct( ?SettingOptions $setting_options = null ) {
 		parent::__construct( $setting_options );
 	}
