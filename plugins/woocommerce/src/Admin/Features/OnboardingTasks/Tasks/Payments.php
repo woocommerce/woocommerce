f9c217867cae4d77437fe9ebd1f9d0d3c2f392bf
Vlad Olaru <vlad@pixelgrade.com>
2025-07-02 12:09:00 +0300

    Update the task completion logic

--- plugins/woocommerce/src/Admin/Features/OnboardingTasks/Tasks/Payments.php
+++ plugins/woocommerce/src/Admin/Features/OnboardingTasks/Tasks/Payments.php
@@ -70,7 +70,13 @@ class Payments extends Task {
 	 */
 	public function is_complete() {
 		if ( null === $this->is_complete_result ) {
-			$this->is_complete_result = self::has_gateways();
+			if ( $this->is_woopayments_active() ) {
+				// If WooPayments is active, check if it is fully onboarded with a live account.
+				$this->is_complete_result = $this->is_woopayments_onboarded() && ! $this->has_woopayments_test_account();
+			} else {
+				// If WooPayments is not active, check if there are any enabled gateways.
+				$this->is_complete_result = self::has_gateways();
+			}
 		}
 
 		return $this->is_complete_result;
