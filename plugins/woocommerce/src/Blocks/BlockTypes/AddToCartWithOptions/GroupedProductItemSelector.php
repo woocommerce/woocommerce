6102bc723ac11e9cee78a5690d38a068b824e0f2
Amit Raj <harsh.harshraj.ra@gmail.com>
2025-07-02 15:49:59 +0530

    feat: Update minimum quantity value logic in grouped product item selector

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/GroupedProductItemSelector.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/GroupedProductItemSelector.php
@@ -38,6 +38,7 @@ class GroupedProductItemSelector extends AbstractBlock {
 				'input_name'  => 'quantity[' . $product->get_id() . ']',
 				'input_id'    => 'quantity_' . $product->get_id(),
 				'input_value' => isset( $_POST['quantity'][ $product->get_id() ] ) ? wc_stock_amount( wc_clean( wp_unslash( $_POST['quantity'][ $product->get_id() ] ) ) ) : '', // phpcs:ignore WordPress.Security.NonceVerification.Missing
+				'min_value'   => AddToCartWithOptionsUtils::is_min_max_quantity_same( $product ) ? 0 :
 				/**
 				 * Filter the minimum quantity value allowed for the product.
 				 *
@@ -45,7 +46,7 @@ class GroupedProductItemSelector extends AbstractBlock {
 				 * @param int        $min_value Minimum quantity value.
 				 * @param WC_Product $product   Product object.
 				 */
-				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', 0, $product ),
+				apply_filters( 'woocommerce_quantity_input_min', 0, $product ),
 				/**
 				 * Filter the maximum quantity value allowed for the product.
 				 *
