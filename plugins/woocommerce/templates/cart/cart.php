a7774b0ecdfda93b4e9c5c739656d38a879b3bb3
Mike Jolley <mike.jolley@me.com>
2025-07-03 17:30:08 +0100

    Template bump

--- plugins/woocommerce/templates/cart/cart.php
+++ plugins/woocommerce/templates/cart/cart.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 10.0.0
+ * @version 10.1.0
  */
 
 defined( 'ABSPATH' ) || exit;
