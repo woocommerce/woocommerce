b42cf9d32d32041ba1b805c3d410f99ac2f25270
Mike Jolley <mike.jolley@me.com>
2025-07-03 17:40:25 +0100

    Update tests

--- plugins/woocommerce/tests/php/src/Utilities/NumberUtilTest.php
+++ plugins/woocommerce/tests/php/src/Utilities/NumberUtilTest.php
@@ -65,15 +65,6 @@ class NumberUtilTest extends \WC_Unit_Test_Case {
 	}
 
 	/**
-	 * @testdox `round` should work as the built-in function of the same name when passing a number-like string with spaces.
-	 */
-	public function test_round_when_passing_a_number_like_string_with_spaces() {
-		$actual   = NumberUtil::round( '  1234.5678  ' );
-		$expected = 1235;
-		$this->assertEquals( $expected, $actual );
-	}
-
-	/**
 	 * Data provider for the `round` tests for non-numeric values.
 	 *
 	 * @return array Values to test.
@@ -102,11 +93,11 @@ class NumberUtilTest extends \WC_Unit_Test_Case {
 	}
 
 	/**
-	 * @testdox `round` should return 1 when passing the boolean 'true'.
+	 * @testdox `round` should return 0 due to it not being numeric.
 	 */
 	public function test_round_when_passing_the_boolean_true() {
 		$actual   = NumberUtil::round( true );
-		$expected = 1;
+		$expected = 0;
 		$this->assertEquals( $expected, $actual );
 	}
 
