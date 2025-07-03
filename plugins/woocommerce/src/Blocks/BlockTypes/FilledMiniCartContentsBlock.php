d4d436c061dab8e1e65f6e77c626d8a6a92a5f63
David <david.arenas@automattic.com>
2025-07-03 13:34:15 +0200

    Render store notices inside experimental FilledMiniCartContents

--- plugins/woocommerce/src/Blocks/BlockTypes/FilledMiniCartContentsBlock.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/FilledMiniCartContentsBlock.php
@@ -39,9 +39,60 @@ class FilledMiniCartContentsBlock extends AbstractInnerBlock {
 	 * @return string Rendered block type output.
 	 */
 	protected function render_experimental_filled_mini_cart_contents( $attributes, $content, $block ) {
+		$context = wp_json_encode(
+			array(
+				'notices' => array(),
+			),
+			JSON_NUMERIC_CHECK
+				| JSON_HEX_TAG
+				| JSON_HEX_APOS
+				| JSON_HEX_QUOT
+				| JSON_HEX_AMP
+		);
+
 		ob_start();
 		?>
-		<div data-wp-bind--hidden="state.cartIsEmpty" data-wp-interactive="woocommerce/mini-cart" class="wp-block-woocommerce-filled-mini-cart-contents-block">
+		<div
+			class="wp-block-woocommerce-filled-mini-cart-contents-block"
+			data-wp-interactive="woocommerce/mini-cart"
+			data-wp-context='woocommerce/store-notices::<?php echo $context ?>'
+			data-wp-bind--hidden="state.cartIsEmpty"
+		>
+			<div
+				class="wc-block-components-notices"
+				data-wp-interactive="woocommerce/store-notices"
+			>
+				<template 
+					data-wp-each--notice="context.notices"
+					data-wp-each-key="context.notice.id"
+				>
+					<div
+						class="wc-block-components-notice-banner"
+						data-wp-class--is-error="state.isError"
+						data-wp-class--is-success ="state.isSuccess"
+						data-wp-class--is-info="state.isInfo"
+						data-wp-class--is-dismissible="context.notice.dismissible"
+						data-wp-bind--role="state.role"
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false">
+							<path data-wp-bind--d="state.iconPath"></path>
+						</svg>
+						<div class="wc-block-components-notice-banner__content">
+							<span data-wp-init="callbacks.renderNoticeContent"></span>
+						</div>
+						<button
+							data-wp-bind--hidden="!context.notice.dismissible"
+							class="wc-block-components-button wp-element-button wc-block-components-notice-banner__dismiss contained"
+							aria-label="<?php esc_attr_e( 'Dismiss this notice', 'woocommerce' ); ?>"
+							data-wp-on--click="actions.removeNotice"
+						>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+								<path d="M13 11.8l6.1-6.3-1-1-6.1 6.2-6.1-6.2-1 1 6.1 6.3-6.5 6.7 1 1 6.5-6.6 6.5 6.6 1-1z" />
+							</svg>
+						</button>
+					</div>
+				</template>
+			</div>
 			<?php
 				// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				echo $content;
