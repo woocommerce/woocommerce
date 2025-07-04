18e3f21158a82848bdcef9dceaf1a0e0ec3d9ca7
Łukasz Strączyński <lukasz.straczynski@automattic.com>
2025-07-04 16:58:34 +0200

    Revert "Fix edge case scenario with subsequent aborted requests"
    This reverts commit 895454e4f7ada01c2f2c42db43e0e52e487fb0de.

--- plugins/woocommerce/client/blocks/assets/js/data/cart/thunks.ts
+++ plugins/woocommerce/client/blocks/assets/js/data/cart/thunks.ts
@@ -484,11 +484,13 @@ export const selectShippingRate =
 
 			dispatch.receiveCart( rest );
 			dispatch.shippingRatesBeingSelected( false );
-			return response;
+			return response as CartResponse;
 		} catch ( error ) {
 			dispatch.receiveError( isApiErrorResponse( error ) ? error : null );
 			dispatch.shippingRatesBeingSelected( false );
 			return Promise.reject( error );
+		} finally {
+			abortController = null;
 		}
 	};
 
