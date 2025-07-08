391fb61e4c169a38044e3c197978ac2f2c1fd5ab
Łukasz Strączyński <lukasz.straczynski@automattic.com>
2025-07-08 13:47:10 +0200

    Treat form step title as heading

--- plugins/woocommerce/client/blocks/packages/components/title/index.tsx
+++ plugins/woocommerce/client/blocks/packages/components/title/index.tsx
@@ -18,11 +18,16 @@ const Title = ( {
 	headingLevel,
 	...props
 }: TitleProps ): JSX.Element => {
-	const buttonClassName = clsx( 'wc-block-components-title', className );
-	const TagName = `h${ headingLevel }` as keyof JSX.IntrinsicElements;
+	const TagName = `h${ headingLevel }` as const;
 
 	return (
-		<TagName className={ buttonClassName } { ...props }>
+		<TagName
+			className={ clsx(
+				'wc-block-components-title wp-block-heading',
+				className
+			) }
+			{ ...props }
+		>
 			{ children }
 		</TagName>
 	);
