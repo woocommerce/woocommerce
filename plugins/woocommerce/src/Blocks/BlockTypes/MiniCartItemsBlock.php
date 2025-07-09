65aea6ef6e5b019116dc8aa71dc1feee343c22bc
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2025-07-09 11:51:07 +0200

    Add `wrapper_attributes` to `MiniCartItems`

--- plugins/woocommerce/src/Blocks/BlockTypes/MiniCartItemsBlock.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/MiniCartItemsBlock.php
@@ -73,9 +73,11 @@ class MiniCartItemsBlock extends AbstractInnerBlock {
 			)
 		);
 
+		$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => 'wc-block-mini-cart__items' ) );
+
 		ob_start();
 		?>
-		<div data-wp-interactive="<?php echo esc_attr( $this->get_full_block_name() ); ?>" class="wp-block-woocommerce-mini-cart-items-block wc-block-mini-cart__items" tabindex="-1">
+		<div data-wp-interactive="<?php echo esc_attr( $this->get_full_block_name() ); ?>" <?php echo $wrapper_attributes; ?> tabindex="-1">
 			<div class="wp-block-woocommerce-mini-cart-products-table-block wc-block-mini-cart__products-table">
 				<table class="wc-block-cart-items wc-block-mini-cart-items" tabindex="-1">
 					<caption class="screen-reader-text">
