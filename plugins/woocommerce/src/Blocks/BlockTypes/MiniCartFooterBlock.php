84f0cbf08586c20201b83229c02be547c9d0e482
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2025-07-09 11:51:07 +0200

    Add `wrapper_attributes` to `MiniCartFooter`

--- plugins/woocommerce/src/Blocks/BlockTypes/MiniCartFooterBlock.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/MiniCartFooterBlock.php
@@ -32,6 +32,7 @@ class MiniCartFooterBlock extends AbstractInnerBlock {
 		$subtotal                         = $display_cart_price_including_tax ? $cart->get_subtotal_tax() : $cart->get_subtotal();
 		$formatted_subtotal               = '';
 		$html                             = new \WP_HTML_Tag_Processor( wc_price( $subtotal ) );
+		$wrapper_attributes     		  = get_block_wrapper_attributes( array( 'class' => 'wc-block-mini-cart__footer' ) );
 
 		if ( $html->next_tag( 'bdi' ) ) {
 			while ( $html->next_token() ) {
@@ -50,7 +51,7 @@ class MiniCartFooterBlock extends AbstractInnerBlock {
 
 		?>
 		<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-		<div data-wp-interactive="woocommerce/mini-cart-footer-block" class="wp-block-woocommerce-mini-cart-footer-block wc-block-mini-cart__footer">
+		<div data-wp-interactive="woocommerce/mini-cart-footer-block" <?php echo $wrapper_attributes; ?>>
 			<div class="wc-block-components-totals-item wc-block-mini-cart__footer-subtotal">
 				<span class="wc-block-components-totals-item__label">
 					<?php echo esc_html( $subtotal_label ); ?>
