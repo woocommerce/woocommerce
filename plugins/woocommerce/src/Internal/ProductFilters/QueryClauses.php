e816500c7c1bdded2ae649fa162d99a7a74f3aa7
Tung Du <dinhtungdu@gmail.com>
2025-07-10 11:23:42 +0700

    fix: filtering param value early to avoid unnecessary calculation

--- plugins/woocommerce/src/Internal/ProductFilters/QueryClauses.php
+++ plugins/woocommerce/src/Internal/ProductFilters/QueryClauses.php
@@ -576,12 +576,12 @@ class QueryClauses implements QueryClausesGenerator, MainQueryClausesGenerator {
 		}
 
 		foreach ( $this->params->get_param( 'taxonomy' ) as $taxonomy => $param ) {
-			if ( isset( $query_vars[ $param ] ) ) {
+			if ( isset( $query_vars[ $param ] ) && ! empty( trim( $query_vars[ $param ] ) ) ) {
 				$chosen_taxonomies[ $taxonomy ] = array_map( 'sanitize_title', explode( ',', $query_vars[ $param ] ) );
 			}
 		}
 
-		return array_filter( $chosen_taxonomies );
+		return $chosen_taxonomies;
 	}
 
 	/**
