84bc38c35406e447f3dcbe24257b30372b726951
Amit Raj <harsh.harshraj.ra@gmail.com>
2025-07-11 12:13:51 +0530

    refactor: remove unused method get_base_parsed_block from RestApi test class

--- plugins/woocommerce/tests/php/src/Blocks/BlockTypes/ProductCollection/RestApi.php
+++ plugins/woocommerce/tests/php/src/Blocks/BlockTypes/ProductCollection/RestApi.php
@@ -19,37 +19,6 @@ class RestApi extends \WP_UnitTestCase {
 	private $block_instance;
 
 	/**
-	 * Return starting point for parsed block test data.
-	 * Using a method instead of property to avoid sharing data between tests.
-	 */
-	private function get_base_parsed_block() {
-		return array(
-			'blockName' => 'woocommerce/product-collection',
-			'attrs'     => array(
-				'query' => array(
-					'perPage'                  => 9,
-					'pages'                    => 0,
-					'offset'                   => 0,
-					'postType'                 => 'product',
-					'order'                    => 'desc',
-					'orderBy'                  => 'date',
-					'search'                   => '',
-					'exclude'                  => array(),
-					'sticky'                   => '',
-					'inherit'                  => true,
-					'isProductCollectionBlock' => true,
-					'woocommerceAttributes'    => array(),
-					'woocommerceStockStatus'   => array(
-						ProductStockStatus::IN_STOCK,
-						ProductStockStatus::OUT_OF_STOCK,
-						ProductStockStatus::ON_BACKORDER,
-					),
-				),
-			),
-		);
-	}
-
-	/**
 	 * Build a simplified request for testing.
 	 *
 	 * @param array $params The parameters to set on the request.
