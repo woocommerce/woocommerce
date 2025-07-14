5becd93fc4af10800ef8dc49cbad753df6f3bbde
Ismael Martín Alabarce <info@ismaeld.com>
2025-07-14 14:10:55 +0200

    Prevent select to be empty
    and ensure the everywhere selection works as expected

--- plugins/woocommerce/client/admin/client/wp-admin-scripts/shipping-settings-region-picker/region-picker.js
+++ plugins/woocommerce/client/admin/client/wp-admin-scripts/shipping-settings-region-picker/region-picker.js
@@ -5,13 +5,20 @@ import { useState } from '@wordpress/element';
 import { TreeSelectControl } from '@woocommerce/components';
 import { __ } from '@wordpress/i18n';
 
+const everywhere = '__WC_TREE_SELECT_COMPONENT_ROOT__';
+
 export const RegionPicker = ( { options, initialValues } ) => {
 	const [ selected, setSelected ] = useState(
-		initialValues.length
-			? initialValues
-			: [ '__WC_TREE_SELECT_COMPONENT_ROOT__' ]
+		initialValues.length ? initialValues : [ everywhere ]
 	);
 	const onChange = ( value ) => {
+		// Set selection to 'everywhere' when empty or when 'everywhere' is the last selected.
+		if ( value.length === 0 || value[ value.length - 1 ] === everywhere ) {
+			value = [ everywhere ];
+		} else {
+			// Remove 'everywhere' from selection when other regions are chosen.
+			value = value.filter( ( item ) => item !== everywhere );
+		}
 		document.body.dispatchEvent(
 			new CustomEvent( 'wc_region_picker_update', { detail: value } )
 		);
