3d2e429c3dfb4b329f5f0d979528663aa9cf21eb
Luis Herranz <luisherranz@gmail.com>
2025-07-16 14:14:00 +0200

    Add `saleBadgePriceFormat` to cart item total price

--- plugins/woocommerce/client/blocks/assets/js/blocks/mini-cart/iapi-frontend.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/mini-cart/iapi-frontend.ts
@@ -288,10 +288,33 @@ const { state: cartItemState } = store(
 					.convertPrecision( cartItemState.currency.minorUnit )
 					.getAmount();
 
-				return formatPriceWithCurrency(
+				const price = formatPriceWithCurrency(
 					totalLineItemDiscount,
 					cartItemState.currency
 				);
+
+				// TODO: Add deprecation notice urging to replace with a
+				// `data-wp-text` directive or an alternative solution.
+				if (
+					( window.wc as any )?.blocksCheckout.applyCheckoutFilter
+				) {
+					const priceText = (
+						window.wc as any
+					 ).blocksCheckout.applyCheckoutFilter( {
+						filterName: 'saleBadgePriceFormat',
+						defaultValue: '<price/>',
+						extensions: cartItemState.cartItem.extensions,
+						arg: {
+							context: 'cart',
+							cartItem: cartItemState.cartItem,
+							cart: woocommerceState.cart,
+						},
+					} );
+
+					return priceText.replace( '<price/>', price );
+				}
+
+				return price;
 			},
 
 			get cartItemHasDiscount(): boolean {
