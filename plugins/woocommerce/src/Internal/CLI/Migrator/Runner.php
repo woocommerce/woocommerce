1277490266fb4424ed00f37c80e7f7ac04fa00f2
Naman Malhotra <naman03malhotra@gmail.com>
2025-07-17 18:21:43 +0530

    WooCommerce Migrator Init

---
+++ plugins/woocommerce/src/Internal/CLI/Migrator/Runner.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Automattic\WooCommerce\Internal\CLI\Migrator;
+
+use Automattic\WooCommerce\Internal\CLI\Migrator\Commands\ProductsCommand;
+use Automattic\WooCommerce\Internal\CLI\Migrator\Commands\ResetCommand;
+use Automattic\WooCommerce\Internal\CLI\Migrator\Commands\SetupCommand;
+use WP_CLI;
+use WC_Product_Factory;
+
+/**
+ * The main runner for the migrator.
+ */
+final class Runner {
+
+	/**
+	 * Register the commands for the migrator.
+	 *
+	 * @return void
+	 */
+	public static function register_commands(): void {
+		$container = wc_get_container();
+
+		// Products Command
+		WP_CLI::add_command(
+			'wc migrator products',
+			$container->get( ProductsCommand::class ),
+			array(
+				'shortdesc' => 'Migrate products from a source platform to WooCommerce.',
+				'longdesc'  => 'Migrate products from a source platform to WooCommerce. The migrator will fetch products from the source platform, map them to the WooCommerce product schema, and then import them into WooCommerce.',
+			)
+		);
+
+		// Reset Command
+		WP_CLI::add_command(
+			'wc migrator reset',
+			$container->get( ResetCommand::class ),
+			array(
+				'shortdesc' => 'Resets (deletes) the credentials for a given platform.',
+			)
+		);
+
+		// Setup Command
+		WP_CLI::add_command(
+			'wc migrator setup',
+			$container->get( SetupCommand::class ),
+			array(
+				'shortdesc' => 'Interactively sets up the credentials for a given platform.',
+			)
+		);
+	}
+} 
