32cf90c574410f8440e6f7d71c7faa78d6857fde
Luigi Teschio <gigitux@gmail.com>
2025-07-17 22:29:11 +0200

    update implementation

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-details/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-details/edit.tsx
@@ -1,13 +1,15 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { InnerBlockTemplate } from '@wordpress/blocks';
+import { productsStore } from '@woocommerce/data';
+import { useMemo } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
 import {
-	useBlockProps,
-	useInnerBlocksProps,
 	store as blockEditorStore,
+	useBlockProps,
+	// @ts-expect-error - useInnerBlocksProps is not exported from @wordpress/block-editor
+	useInnerBlocksProps,
 	Warning,
 } from '@wordpress/block-editor';
 
@@ -15,74 +17,7 @@ import {
  * Internal dependencies
  */
 import { ProductDetailsEditProps } from './types';
-
-const TEMPLATE: InnerBlockTemplate[] = [
-	[
-		'woocommerce/accordion-group',
-		{
-			metadata: {
-				isDescendantOfProductDetails: true,
-			},
-		},
-		[
-			[
-				'woocommerce/accordion-item',
-				{
-					openByDefault: true,
-				},
-				[
-					[
-						'woocommerce/accordion-header',
-						{ title: __( 'Description', 'woocommerce' ) },
-						[],
-					],
-					[
-						'woocommerce/accordion-panel',
-						{},
-						[ [ 'woocommerce/product-description', {}, [] ] ],
-					],
-				],
-			],
-			[
-				'woocommerce/accordion-item',
-				{},
-				[
-					[
-						'woocommerce/accordion-header',
-						{
-							title: __(
-								'Additional Information',
-								'woocommerce'
-							),
-						},
-						[],
-					],
-					[
-						'woocommerce/accordion-panel',
-						{},
-						[ [ 'woocommerce/product-specifications', {} ] ],
-					],
-				],
-			],
-			[
-				'woocommerce/accordion-item',
-				{},
-				[
-					[
-						'woocommerce/accordion-header',
-						{ title: __( 'Reviews', 'woocommerce' ) },
-						[],
-					],
-					[
-						'woocommerce/accordion-panel',
-						{},
-						[ [ 'woocommerce/product-reviews', {} ] ],
-					],
-				],
-			],
-		],
-	],
-];
+import { getTemplate } from './utils';
 
 /**
  * Check if block is inside a Query Loop with non-product post type
@@ -106,8 +41,27 @@ const useIsInvalidQueryLoopContext = ( clientId: string, postType: string ) => {
 const Edit = ( { clientId, context }: ProductDetailsEditProps ) => {
 	const blockProps = useBlockProps();
 
+	const product = useSelect(
+		( select ) => {
+			if ( ! context.postId ) {
+				return null;
+			}
+			const { getProduct } = select( productsStore );
+			return getProduct( Number( context.postId ) );
+		},
+		[ context.postId ]
+	);
+
+	const template = useMemo( () => {
+		if ( ! product ) {
+			return [];
+		}
+
+		return getTemplate( product );
+	}, [ product ] );
+
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
-		template: TEMPLATE,
+		template,
 	} );
 
 	const isInvalidQueryLoopContext = useIsInvalidQueryLoopContext(
--- plugins/woocommerce/client/blocks/tests/integration/helpers/with-fake-timers.ts
+++ plugins/woocommerce/client/blocks/tests/integration/helpers/with-fake-timers.ts
@@ -8,6 +8,8 @@
 export async function withFakeTimers< T >( fn: () => T ) {
 	const usingFakeTimers = jest.isMockFunction( setTimeout );
 
+	jest.setTimeout( 20000 );
+
 	// Portions of the React Native Animation API rely upon these APIs. However,
 	// Jest's 'legacy' fake timers mutate these globals, which breaks the Animated
 	// API. We preserve the original implementations to restore them later.
@@ -15,7 +17,10 @@ export async function withFakeTimers< T >( fn: () => T ) {
 	const cancelAnimationFrameCopy = global.cancelAnimationFrame;
 
 	if ( ! usingFakeTimers ) {
-		jest.useFakeTimers( { legacyFakeTimers: true } );
+		jest.useFakeTimers( {
+			now: new Date( 2023, 9, 15 ),
+			doNotFake: [ 'setTimeout' ],
+		} );
 	}
 
 	const result = await fn();
