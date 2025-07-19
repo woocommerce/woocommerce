593b8e0addb5c1cd5e75eedfd8912f44bf5d63b0
roccotrip <rocco.tripaldi@automattic.com>
2025-07-18 21:52:30 -0400

    restructure class and tests

--- plugins/woocommerce/src/Internal/StockNotifications/Emails/EmailActionController.php
+++ plugins/woocommerce/src/Internal/StockNotifications/Emails/EmailActionController.php
@@ -4,6 +4,7 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\Internal\StockNotifications\Emails;
 
+use Automattic\WooCommerce\Internal\StockNotifications\Enums\NotificationCancellationSource;
 use Automattic\WooCommerce\Internal\StockNotifications\Enums\NotificationStatus;
 use Automattic\WooCommerce\Internal\StockNotifications\Factory;
 use Automattic\WooCommerce\Internal\StockNotifications\Notification;
@@ -16,11 +17,19 @@ use Automattic\WooCommerce\Internal\StockNotifications\Notification;
  * @package Automattic\WooCommerce\Internal\StockNotifications\Emails
  */
 class EmailActionController {
+    /**
+     * EmailActionController constructor.
+     *
+     * Initializes the controller by adding actions to process verification and unsubscribe actions from requests.
+     */
     public function __construct() {
         add_action( 'template_redirect', array( $this, 'maybe_process_verification_action_from_request' ) );
         add_action( 'template_redirect', array( $this, 'maybe_process_unsubscribe_action_from_request' ) );
     }
 
+    /**
+     * This method checks if the request contains a verification action and processes it.
+     */
     public function maybe_process_verification_action_from_request(): void {
         $this->maybe_process_verification_action(
             $_GET['notification_id'] ?? null,
@@ -28,7 +37,9 @@ class EmailActionController {
         );
     }
 
-    // Wrapper for production use
+    /**
+     * This method checks if the request contains an unsubscribe action and processes it.
+     */
     public function maybe_process_unsubscribe_action_from_request(): void {
         $this->maybe_process_unsubscribe_action(
             $_GET['notification_id'] ?? null,
@@ -42,15 +53,20 @@ class EmailActionController {
      *
      * @param int|null $notification_id The ID of the notification to process.
      * @param string|null $action_key The action key to verify.
+     * @return void
 	 */
-    public function maybe_process_verification_action( $notification_id, $action_key ): void  {
-        $notification = $this->get_notification_to_be_processed();
+    public function maybe_process_verification_action( int|null $notification_id, string|null $action_key ): void  {
+        if ( ! $action_key ) {
+            return;
+        }
+
+        $notification = $this->get_notification_to_be_processed( $notification_id );
 
         if ( ! $notification ) {
             return;
         }
 
-		if ( $notification->check_verification_key( $_GET['email_link_action_key'] ) ) {
+		if ( $notification->check_verification_key( $action_key ) ) {
 			$notification->set_status( NotificationStatus::ACTIVE );
 			$notification->set_date_confirmed( time() );
 			$notification->save();
@@ -63,17 +79,24 @@ class EmailActionController {
      *
      * @param int|null $notification_id The ID of the notification to process.
      * @param string|null $action_key The action key to verify.
+     * @return void
 	 */
-    public function maybe_process_unsubscribe_action( $notification_id, $action_key ): void {
-        $notification = $this->get_notification_to_be_processed();
+    public function maybe_process_unsubscribe_action( int|null $notification_id, string|null $action_key ): void {
+        if ( ! $action_key ) {
+            return;
+        }
+
+        $notification = $this->get_notification_to_be_processed( $notification_id );
 
         if ( ! $notification ) {
             return;
         }
 
-        if ( $notification->check_unsubscribe_key( $_GET['email_link_action_key'] ) ) {
-			$this->notification->set_status( NotificationStatus::CANCELLED );
-			$this->notification->save();
+        if ( $notification->check_unsubscribe_key( $action_key) ) {
+			$notification->set_status( NotificationStatus::CANCELLED );
+            $notification->set_cancellation_source( NotificationCancellationSource::USER );
+            $notification->set_date_cancelled( time() );
+			$notification->save();
 		}
 	}
 
@@ -81,15 +104,10 @@ class EmailActionController {
      * Retrieves the notification to be processed based on the provided notification ID and action key.
      *
      * @param int|null $notification_id The ID of the notification to process.
-     * @param string|null $action_key The action key to verify.
-     * @return Notification|null The notification object if found, otherwise null.
+     * @return Notification|null The notification object if found and has an action key, null otherwise.
      */
-    public function get_notification_to_be_processed( $notification_id, $action_key ): Notification | null {
-        if ( ! isset( $notification_id ) || ! isset( $action_key ) ) {
-            return null;
-        }
-
-        $notification = Factory::get_notification( $_GET['notification_id'] );
+    public function get_notification_to_be_processed( int|null $notification_id ): Notification | null {
+        $notification = Factory::get_notification( $notification_id );
 
         if ( ! $notification ) {
             return null;
--- plugins/woocommerce/tests/php/src/Internal/StockNotifications/Emails/EmailActionControllerTests.php
+++ plugins/woocommerce/tests/php/src/Internal/StockNotifications/Emails/EmailActionControllerTests.php
@@ -4,12 +4,20 @@ declare( strict_types = 1 );
 namespace Automattic\WooCommerce\Tests\Internal\StockNotifications\Emails;
 
 use Automattic\WooCommerce\Internal\StockNotifications\Emails\EmailActionController;
+use Automattic\WooCommerce\Internal\StockNotifications\Enums\NotificationCancellationSource;
 use Automattic\WooCommerce\Internal\StockNotifications\Notification;
 use Automattic\WooCommerce\Internal\StockNotifications\Factory;
 use Automattic\WooCommerce\Internal\StockNotifications\Enums\NotificationStatus;
 use Automattic\WooCommerce\Internal\StockNotifications\Utilities\HasherHelper;
 
+/**
+ * EmailActionControllerTests tests.
+ */
 class EmailActionControllerTests extends \WC_Unit_Test_Case {
+
+    /**
+     * Test that verification action is sets notification status to active.
+     */
     public function test_process_verification_action_sets_status_active() {
         $notification = new Notification();
         $notification->set_product_id( 1 );
@@ -25,6 +33,9 @@ class EmailActionControllerTests extends \WC_Unit_Test_Case {
         $this->assertEquals(NotificationStatus::ACTIVE, $notification->get_status());
     }
 
+    /**
+     * Test that unsubscribe action sets notification status to cancelled, and sets cancellation source to user.
+     */
     public function test_process_unsubscribe_action_sets_status_cancelled() {
         $notification = new Notification();
         $notification->set_product_id( 1 );
@@ -38,5 +49,6 @@ class EmailActionControllerTests extends \WC_Unit_Test_Case {
         $controller->maybe_process_unsubscribe_action($id, 'test');
         $notification = Factory::get_notification( $id );
         $this->assertEquals( NotificationStatus::CANCELLED, $notification->get_status() );
+        $this->assertEquals( NotificationCancellationSource::USER, $notification->get_cancellation_source() );
     }
 }
