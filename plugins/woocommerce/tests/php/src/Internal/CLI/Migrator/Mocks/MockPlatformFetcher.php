9fd47be9f8ca65bf8f8b8f10246d136c0a89c664
Naman Malhotra <naman03malhotra@gmail.com>
2025-07-21 18:52:08 +0530

    Shopify fetcher tests

---
+++ plugins/woocommerce/tests/php/src/Internal/CLI/Migrator/Mocks/MockPlatformFetcher.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Mock Platform Fetcher class for testing.
+ *
+ * @package Automattic\WooCommerce\Tests\Internal\CLI\Migrator\Mocks
+ */
+
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Tests\Internal\CLI\Migrator\Mocks;
+
+use Automattic\WooCommerce\Internal\CLI\Migrator\Interfaces\PlatformFetcherInterface;
+
+/**
+ * A mock fetcher class for testing purposes.
+ */
+class MockPlatformFetcher implements PlatformFetcherInterface {
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param array $args Arguments for fetching.
+	 */
+	public function fetch_batch( array $args ): array {
+		return array(
+			'items'       => array(
+				(object) array(
+					'id'   => 1,
+					'name' => 'Test Product 1',
+				),
+				(object) array(
+					'id'   => 2,
+					'name' => 'Test Product 2',
+				),
+			),
+			'cursor'      => 'next-cursor',
+			'hasNextPage' => true,
+		);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param array $args Arguments for fetching.
+	 */
+	public function fetch_total_count( array $args ): int {
+		return 42;
+	}
+}
