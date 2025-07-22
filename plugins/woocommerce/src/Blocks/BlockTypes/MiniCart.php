a2ef6773537b6522d210825b99f2f7a4f2c9250c
MaggieCabrera <maggie.cabrera@automattic.com>
2025-07-22 11:05:59 +0200

    undo unrelated change

--- plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
@@ -502,6 +502,7 @@ class MiniCart extends AbstractBlock {
 
 		if ( $cart ) {
 			$classes_styles                   = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes, array(), array( 'extra_classes' ) );
+			var_dump($classes_styles);
 			$icon_color                       = isset( $attributes['iconColor']['color'] ) ? esc_attr( $attributes['iconColor']['color'] ) : 'currentColor';
 			$product_count_color              = isset( $attributes['productCountColor']['color'] ) ? esc_attr( $attributes['productCountColor']['color'] ) : '';
 			$styles                           = $product_count_color ? 'background:' . $product_count_color : '';
@@ -544,7 +545,7 @@ class MiniCart extends AbstractBlock {
 					'formattedSubtotal'  => $formatted_subtotal,
 					'drawerOverlayClass' => 'wc-block-components-drawer__screen-overlay wc-block-components-drawer__screen-overlay--with-slide-out wc-block-components-drawer__screen-overlay--is-hidden',
 					'buttonAriaLabel'    => function () use ( $button_aria_label_template ) {
-						$state = wp_interactivity_state( 'woocommerce/mini-cart' );
+						$state = wp_interactivity_state();
 						return isset( $attributes['hasHiddenPrice'] ) && false !== $attributes['hasHiddenPrice']
 							? sprintf( $button_aria_label_template, $state['totalItemsInCart'] )
 							: sprintf( $button_aria_label_template, $state['totalItemsInCart'], $state['formattedSubtotal'] );
