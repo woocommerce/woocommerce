a6d99353b96baf451abc14075978d1bef9ef0f75
Luigi Teschio <gigitux@gmail.com>
2025-07-23 13:58:10 +0200

    fix unit test

--- plugins/woocommerce/client/blocks/assets/js/blocks/cart/test/block.js
+++ plugins/woocommerce/client/blocks/assets/js/blocks/cart/test/block.js
@@ -214,7 +214,6 @@ describe( 'Testing cart', () => {
 				'16€'
 			)
 		);
-		expect( screen.getAllByRole( 'cell' )[ 1 ] ).toHaveTextContent( '16€' );
 	} );
 
 	it( 'updates quantity when changed in server', async () => {
@@ -290,6 +289,18 @@ describe( 'Testing cart', () => {
 		act( () => {
 			dispatch( storeKey ).receiveCart( cart );
 		} );
+
+		expect( console ).toHaveErrored();
+
+		// TODO: This can be simplified to expect(console).toHaveErroredWith('error message', expect.any( String ))
+		// after this ticket is done https://github.com/WordPress/gutenberg/issues/22850
+		// eslint-disable-next-line no-console
+		const [ firstArg, secondArg ] = console.error.mock.calls.at( -1 );
+		expect( firstArg ).toEqual(
+			'Warning: findDOMNode is deprecated and will be removed in the next major release. Instead, add a ref directly to the element you want to reference. Learn more about using refs safely here: https://reactjs.org/link/strict-mode-find-node%s'
+		);
+		// The stack trace
+		expect( secondArg ).toBeTruthy();
 
 		expect( screen.queryAllByText( /Remove item/i ).length ).toBe( 0 );
 	} );
