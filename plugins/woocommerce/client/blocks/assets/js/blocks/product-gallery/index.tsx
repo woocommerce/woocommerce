9eb4059a542309a9489dd2aa9f3f342761dc54ac
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-07-23 17:05:54 +0200

    Implement migration in Product Gallery

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/index.tsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { registerProductBlockType } from '@woocommerce/atomic-utils';
+import { createBlock, BlockAttributes, BlockInstance } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
@@ -13,6 +14,30 @@ import icon from './icon';
 import './inner-blocks/product-gallery-next-previous-buttons';
 import './inner-blocks/product-gallery-thumbnails';
 
+const updateInnerBlocks = ( blocks: BlockInstance[] ): BlockInstance[] => {
+	return blocks.map( ( block: BlockInstance ) => {
+		if (
+			block.name ===
+			'woocommerce/product-gallery-large-image-next-previous'
+		) {
+			const newBlock = createBlock(
+				'woocommerce/navigation-arrows',
+				block.attributes
+			);
+			return newBlock;
+		}
+
+		if ( block.innerBlocks?.length ) {
+			return {
+				...block,
+				innerBlocks: updateInnerBlocks( block.innerBlocks ),
+			};
+		}
+
+		return block;
+	} );
+};
+
 const blockConfig = {
 	...metadata,
 	icon,
@@ -22,4 +47,16 @@ const blockConfig = {
 
 registerProductBlockType( blockConfig, {
 	isAvailableOnPostEditor: true,
+	deprecated: [
+		{
+			isEligible: () => true,
+			attributes: metadata.attributes,
+			supports: metadata.supports,
+			save: Save,
+			migrate: (
+				attributes: BlockAttributes,
+				innerBlocks: BlockInstance[]
+			) => [ attributes, updateInnerBlocks( innerBlocks ) ],
+		},
+	],
 } );
