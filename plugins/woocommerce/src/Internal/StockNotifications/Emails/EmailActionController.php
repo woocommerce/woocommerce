d07dd0371a1fc5ad2028a62c481f81f71777633a
roccotrip <rocco.tripaldi@automattic.com>
2025-07-23 10:19:09 -0400

    sanitize request vars before processing

--- plugins/woocommerce/src/Internal/StockNotifications/Emails/EmailActionController.php
+++ plugins/woocommerce/src/Internal/StockNotifications/Emails/EmailActionController.php
@@ -34,18 +34,18 @@ class EmailActionController {
 		if ( ! isset( $_GET['notification_id'] ) || ! isset( $_GET['email_link_action_key'] ) ) {
 			return;
 		}
-        // phpcs:ignore WordPress.Security.NonceVerification.Recommended, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-		$this->validate_request( wp_unslash( $_GET['notification_id'] ), wp_unslash( $_GET['email_link_action_key'] ) );
+        // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		$this->validate_and_maybe_process_request( absint( $_GET['notification_id'] ), sanitize_text_field( $_GET['email_link_action_key'] ) );
 	}
 
 	/**
 	 * Checks request parameters and processes the notification based on the action key.
 	 *
-	 * @param string $notification_id The ID of the notification to process.
+	 * @param int $notification_id The ID of the notification to process.
 	 * @param string $email_link_action_key The action key from the email link.
 	 * @return void
 	 */
-	public function validate_request( string $notification_id, string $email_link_action_key ): void {
+	private function validate_and_maybe_process_request( int $notification_id, string $email_link_action_key ): void {
 		if ( empty( $email_link_action_key ) || empty( $notification_id ) ) {
 			return;
 		}
@@ -71,7 +71,7 @@ class EmailActionController {
 	 * @param string       $action_key The action key to verify.
 	 * @return void
 	 */
-	public function process_verification_action( Notification $notification, string $action_key ): void {
+	private function process_verification_action( Notification $notification, string $action_key ): void {
 		if ( $notification->check_verification_key( $action_key ) ) {
 			$notification->set_status( NotificationStatus::ACTIVE );
 			$notification->set_date_confirmed( time() );
@@ -89,14 +89,14 @@ class EmailActionController {
 			$notice_text = sprintf( esc_html__( 'Successfully verified stock notifications for "%s".', 'woocommerce' ), $product->get_name() );
 			wc_add_notice( $notice_text );
 			/**
-			 * `woocommerce_customer_stock_notification_verified_url` filter.
+			 * `woocommerce_customer_stock_notification_verified_redirect_url` filter.
 			 *
 			 * @since 0.0.0
 			 *
 			 * @param  string  $url
 			 * @return string
 			 */
-			$url = apply_filters( 'woocommerce_customer_stock_notification_verified_url', get_permalink( wc_get_page_id( 'shop' ) ) );
+			$url = apply_filters( 'woocommerce_customer_stock_notification_verified_redirect_url', get_permalink( wc_get_page_id( 'shop' ) ) );
 			wp_safe_redirect( $url );
 		}
 	}
@@ -108,7 +108,7 @@ class EmailActionController {
 	 * @param string       $action_key The action key to verify.
 	 * @return void
 	 */
-	public function process_unsubscribe_action( Notification $notification, string $action_key ): void {
+	private function process_unsubscribe_action( Notification $notification, string $action_key ): void {
 		if ( $notification->check_unsubscribe_key( $action_key ) ) {
 			$notification->set_status( NotificationStatus::CANCELLED );
 			$notification->set_cancellation_source( NotificationCancellationSource::USER );
@@ -127,14 +127,14 @@ class EmailActionController {
 			$notice_text = sprintf( esc_html__( 'Successfully unsubscribed %1$s. You will not receive a notification when "%2$s" becomes available.', 'woocommerce' ), $notification->get_user_email(), $product->get_name() );
 			wc_add_notice( $notice_text );
 			/**
-			 * `woocommerce_customer_stock_notification_unsubscribe_url` filter.
+			 * `woocommerce_customer_stock_notification_unsubscribe_redirect_url` filter.
 			 *
 			 * @since 0.0.0
 			 *
 			 * @param  string  $url
 			 * @return string
 			 */
-			$url = apply_filters( 'woocommerce_customer_stock_notification_unsubscribe_url', get_permalink( wc_get_page_id( 'shop' ) ) );
+			$url = apply_filters( 'woocommerce_customer_stock_notification_unsubscribe_redirect_url', get_permalink( wc_get_page_id( 'shop' ) ) );
 			wp_safe_redirect( $url );
 		}
 	}
@@ -142,10 +142,10 @@ class EmailActionController {
 	/**
 	 * Retrieves the notification to be processed based on the provided notification ID and action key.
 	 *
-	 * @param string $notification_id The ID of the notification to process.
+	 * @param int $notification_id The ID of the notification to process.
 	 * @return Notification|false The notification object if found and has an action key, null otherwise.
 	 */
-	public function get_notification_to_be_processed( string $notification_id ): ?Notification {
+	private function get_notification_to_be_processed( int $notification_id ): ?Notification {
 		$notification = Factory::get_notification( (int) $notification_id );
 
 		if ( ! $notification ) {
