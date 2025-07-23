c2c11b039135e8c058ab081386c52554f9589b80
Naman Malhotra <naman03malhotra@gmail.com>
2025-07-23 21:34:50 +0530

    lint and test fixes

--- plugins/woocommerce/tests/php/src/Internal/CLI/Migrator/Mocks/MockWPCLI.php
+++ plugins/woocommerce/tests/php/src/Internal/CLI/Migrator/Mocks/MockWPCLI.php
@@ -1,45 +1,53 @@
 <?php
+
+declare( strict_types=1 );
+
 /**
  * Mock WP_CLI class for testing.
  *
  * @package WooCommerce\Tests\Internal\CLI\Migrator\Mocks
  */
 
-if ( ! class_exists( 'WP_CLI' ) ) {
+namespace Automattic\WooCommerce\Tests\Internal\CLI\Migrator\Mocks;
+
+/**
+ * Mock WP_CLI class for testing purposes.
+ */
+class MockWPCLI {
 	/**
-	 * Mock WP_CLI class for testing purposes.
+	 * Last debug message logged.
+	 *
+	 * @var string
 	 */
-	class WP_CLI {
-		/**
-		 * Last debug message logged.
-		 *
-		 * @var string
-		 */
-		public static $last_debug_message = '';
+	public static $last_debug_message = '';
 
-		/**
-		 * Last warning message logged.
-		 *
-		 * @var string
-		 */
-		public static $last_warning_message = '';
+	/**
+	 * Last warning message logged.
+	 *
+	 * @var string
+	 */
+	public static $last_warning_message = '';
 
-		/**
-		 * Mock debug method.
-		 *
-		 * @param string $message Debug message.
-		 */
-		public static function debug( $message ) {
-			self::$last_debug_message = $message;
-		}
-
-		/**
-		 * Mock warning method.
-		 *
-		 * @param string $message Warning message.
-		 */
-		public static function warning( $message ) {
-			self::$last_warning_message = $message;
-		}
+	/**
+	 * Mock debug method.
+	 *
+	 * @param string $message Debug message.
+	 */
+	public static function debug( $message ): void {
+		self::$last_debug_message = $message;
 	}
+
+	/**
+	 * Mock warning method.
+	 *
+	 * @param string $message Warning message.
+	 */
+	public static function warning( $message ): void {
+		self::$last_warning_message = $message;
+	}
+}
+
+// Create global WP_CLI class alias if it doesn't exist.
+if ( ! class_exists( 'WP_CLI' ) ) {
+	class_alias( MockWPCLI::class, 'WP_CLI' );
 }
