be1f814571a65f0182422cdf044069f83a95247b
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-07-23 17:09:46 +0200

    Remove unnecessary stuff from old block

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-next-previous-buttons/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-next-previous-buttons/index.tsx
@@ -6,14 +6,10 @@ import { registerBlockType } from '@wordpress/blocks';
 /**
  * Internal dependencies
  */
-import { Edit } from './edit';
 import metadata from './block.json';
 import { Save } from './save';
-import { Icon } from './icons';
 
 // @ts-expect-error: `metadata` currently does not have a type definition in WordPress core
 registerBlockType( metadata, {
-	icon: Icon,
-	edit: Edit,
 	save: Save,
 } );
