e46ca7bf4cd88aa500510b57fb4b0faea1a4c54b
Panos (Panagiotis) Synetos <2484390+PanosSynetos@users.noreply.github.com>
2025-07-24 09:59:33 +0300

    Bump template version

--- plugins/woocommerce/templates/cart/cart.php
+++ plugins/woocommerce/templates/cart/cart.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 10.1.0
+ * @version 10.2.0
  */
 
 defined( 'ABSPATH' ) || exit;
