7b8ec5bee2e8f7b865832b009772698508a2304d
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-07-25 17:26:22 +0200

    Fix lint

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection/Renderer.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductCollection/Renderer.php
@@ -128,8 +128,8 @@ class Renderer {
 			$collection                     = $block['attrs']['collection'] ?? '';
 			$is_enhanced_pagination_enabled = ! ( $block['attrs']['forcePageReload'] ?? false );
 			$context                        = array(
-				'notices' => array(),
-				// Next/Previous Buttons block context
+				'notices'            => array(),
+				// Next/Previous Buttons block context.
 				'isDisabledPrevious' => false,
 				'isDisabledNext'     => false,
 				'ariaLabelPrevious'  => __( 'Scroll products left', 'woocommerce' ),
@@ -356,8 +356,6 @@ class Renderer {
 				'productId' => absint( $context['postId'] ),
 			),
 		) : $this->get_location_context();
-
-
 
 		return $context;
 	}
