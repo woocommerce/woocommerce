dc602e91d5250dbaf8126c398b529c80260d28f0
Jason Kytros <jimjasson@outlook.com.gr>
2025-07-25 12:19:43 +0300

    Let's add more type casting to be sure

--- plugins/woocommerce/includes/class-wc-coupon.php
+++ plugins/woocommerce/includes/class-wc-coupon.php
@@ -733,7 +733,7 @@ class WC_Coupon extends WC_Legacy_Coupon {
 	 * @param float|string $amount Maximum amount.
 	 */
 	public function set_maximum_amount( $amount ) {
-		if ( (float) $amount && $this->get_minimum_amount() > $amount ) {
+		if ( (float) $amount && (float) $this->get_minimum_amount() > (float) $amount ) {
 			$this->error( 'coupon_invalid_maximum_amount', __( 'Invalid maximum spend value.', 'woocommerce' ) );
 		}
 
