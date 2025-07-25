25050b7d98d2afb96e7f8a6dcd75c6db5f3b05a5
Tung Du <dinhtungdu@gmail.com>
2025-07-25 17:17:33 +0700

    refactor: use ToolsPanelItem for Status Filter block

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-filters/inner-blocks/status-filter/inspector.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-filters/inner-blocks/status-filter/inspector.tsx
@@ -3,13 +3,23 @@
  */
 import { InspectorControls } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
-import { PanelBody, ToggleControl } from '@wordpress/components';
+import {
+	ToggleControl,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToolsPanel as ToolsPanel,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToolsPanelItem as ToolsPanelItem,
+} from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import { EditProps } from './types';
-import { DisplayStyleSwitcher } from '../../components/display-style-switcher';
+import {
+	DisplayStyleSwitcher,
+	resetDisplayStyleBlock,
+} from '../../components/display-style-switcher';
+import metadata from './block.json';
 
 export const Inspector = ( {
 	attributes,
@@ -19,9 +29,39 @@ export const Inspector = ( {
 	const { displayStyle, showCounts, hideEmpty } = attributes;
 
 	return (
-		<>
-			<InspectorControls group="settings">
-				<PanelBody title={ __( 'Display', 'woocommerce' ) }>
+		<InspectorControls>
+			<ToolsPanel
+				label={ __( 'Display Settings', 'woocommerce' ) }
+				resetAll={ () => {
+					setAttributes( {
+						displayStyle: metadata.attributes.displayStyle.default,
+						showCounts: metadata.attributes.showCounts.default,
+						hideEmpty: metadata.attributes.hideEmpty.default,
+					} );
+					resetDisplayStyleBlock(
+						clientId,
+						metadata.attributes.displayStyle.default
+					);
+				} }
+			>
+				<ToolsPanelItem
+					label={ __( 'Display Style', 'woocommerce' ) }
+					hasValue={ () =>
+						displayStyle !==
+						'woocommerce/product-filter-checkbox-list'
+					}
+					isShownByDefault={ true }
+					onDeselect={ () => {
+						setAttributes( {
+							displayStyle:
+								metadata.attributes.displayStyle.default,
+						} );
+						resetDisplayStyleBlock(
+							clientId,
+							metadata.attributes.displayStyle.default
+						);
+					} }
+				>
 					<DisplayStyleSwitcher
 						clientId={ clientId }
 						currentStyle={ displayStyle }
@@ -29,24 +69,43 @@ export const Inspector = ( {
 							setAttributes( { displayStyle: value } )
 						}
 					/>
+				</ToolsPanelItem>
+				<ToolsPanelItem
+					label={ __( 'Product counts', 'woocommerce' ) }
+					hasValue={ () => showCounts }
+					onDeselect={ () =>
+						setAttributes( {
+							showCounts: metadata.attributes.showCounts.default,
+						} )
+					}
+					isShownByDefault={ true }
+				>
 					<ToggleControl
 						label={ __( 'Product counts', 'woocommerce' ) }
 						checked={ showCounts }
-						onChange={ ( value ) =>
+						onChange={ ( value: boolean ) =>
 							setAttributes( { showCounts: value } )
 						}
-						__nextHasNoMarginBottom
 					/>
+				</ToolsPanelItem>
+				<ToolsPanelItem
+					label={ __( 'Empty filter options', 'woocommerce' ) }
+					hasValue={ () => ! hideEmpty }
+					onDeselect={ () =>
+						setAttributes( {
+							hideEmpty: metadata.attributes.hideEmpty.default,
+						} )
+					}
+				>
 					<ToggleControl
 						label={ __( 'Empty filter options', 'woocommerce' ) }
 						checked={ ! hideEmpty }
-						onChange={ ( value ) =>
+						onChange={ ( value: boolean ) =>
 							setAttributes( { hideEmpty: ! value } )
 						}
-						__nextHasNoMarginBottom
 					/>
-				</PanelBody>
-			</InspectorControls>
-		</>
+				</ToolsPanelItem>
+			</ToolsPanel>
+		</InspectorControls>
 	);
 };
