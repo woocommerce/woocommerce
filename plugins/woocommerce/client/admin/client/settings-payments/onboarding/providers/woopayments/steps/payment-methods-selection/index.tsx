d8f81d6e4220e0449a368e41c13914e9a2dba94b
Vlad Olaru <vlad@pixelgrade.com>
2025-07-25 14:32:43 +0300

    Memoize PMs lists to better performance

--- plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/payment-methods-selection/index.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/onboarding/providers/woopayments/steps/payment-methods-selection/index.tsx
@@ -292,24 +292,31 @@ export default function PaymentMethodsSelection() {
 									} );
 								} )
 								.then( () => {
+									const displayedPaymentMethodsIds =
+										Object.keys(
+											initialVisibilityMap || {}
+										);
+									const paymentMethodsIds =
+										Object.keys( paymentMethodsState );
+
 									const eventProps = {
 										// This is the entire list of payment methods that are available to the user,
 										// regardless of whether they are enabled or not, shown by default or hidden behind a Show more section.
-										displayed_payment_methods: Object.keys(
-											initialVisibilityMap || {}
-										).join( ', ' ),
+										displayed_payment_methods:
+											displayedPaymentMethodsIds.join(
+												', '
+											),
 										// This is the list of payment methods that were initially displayed to the user
 										// when the step became visible, regardless of whether they were enabled or not.
-										default_displayed_pms: Object.keys(
-											initialVisibilityMap || {}
-										)
-											.filter(
-												( paymentMethod ) =>
-													initialVisibilityMap?.[
-														paymentMethod
-													] !== false
-											)
-											.join( ', ' ),
+										default_displayed_pms:
+											displayedPaymentMethodsIds
+												.filter(
+													( paymentMethod ) =>
+														initialVisibilityMap?.[
+															paymentMethod
+														] !== false
+												)
+												.join( ', ' ),
 										// This is the list of payment methods that were enabled by default
 										// when the step became visible, regardless of whether they ended up selected or not.
 										default_selected_pms:
@@ -321,27 +328,25 @@ export default function PaymentMethodsSelection() {
 												.map( ( method ) => method.id )
 												.join( ', ' ),
 										// This is the list of payment methods that ended up enabled (either by user selection or default).
-										selected_payment_methods: Object.keys(
-											paymentMethodsState
-										)
-											.filter(
-												( paymentMethod ) =>
-													paymentMethodsState[
-														paymentMethod
-													]
-											)
-											.join( ', ' ),
+										selected_payment_methods:
+											paymentMethodsIds
+												.filter(
+													( paymentMethod ) =>
+														paymentMethodsState[
+															paymentMethod
+														]
+												)
+												.join( ', ' ),
 										// This is the list of payment methods that ended up disabled (either by user selection or default).
-										deselected_payment_methods: Object.keys(
-											paymentMethodsState
-										)
-											.filter(
-												( paymentMethod ) =>
-													! paymentMethodsState[
-														paymentMethod
-													]
-											)
-											.join( ', ' ),
+										deselected_payment_methods:
+											paymentMethodsIds
+												.filter(
+													( paymentMethod ) =>
+														! paymentMethodsState[
+															paymentMethod
+														]
+												)
+												.join( ', ' ),
 										business_country:
 											window.wcSettings?.admin
 												?.woocommerce_payments_nox_profile
