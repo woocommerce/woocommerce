09775779e41b9b2cbf8682b5b6825654e4062736
Łukasz Strączyński <8038404+straku@users.noreply.github.com>
2025-07-25 15:50:35 +0200

    Apply consistent font sizes and spacing in checkout (shipping and billing blocks) (#59754)
    * Remove unnecessary xmlnx attribute
    
    * Change checkbox styles
    
    * TextInput 😌
    
    * Update snapshots
    
    * Country select
    
    * Don't use global styles in attribute filter block
    
    * Smaller horizontal gap between form elements
    
    * Reset line-height for checkbox
    
    * Comment that actually makes sense
    
    * Modify address card
    
    * Move phone to the same line as address in address block
    
    * Apply 16px font-size in text input for mobile
    
    * Reset line-height for select container element
    
    * Be consistent with comments
    
    * Fix misaligned select component
    
    * Bring back explicit height for text-input and select components
    
    * Remove duplicated height declaration

--- plugins/woocommerce/client/blocks/packages/components/checkbox-control/index.tsx
+++ plugins/woocommerce/client/blocks/packages/components/checkbox-control/index.tsx
@@ -85,7 +85,6 @@ export const CheckboxControl = forwardRef<
 					<svg
 						className="wc-block-components-checkbox__mark"
 						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 20"
 					>
 						<path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
