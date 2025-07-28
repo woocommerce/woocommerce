c6241fd914624b97b1eafb00bd0ce0b0976ebe9e
Anne Mirasol <anne.mirasol@automattic.com>
2025-07-28 11:58:36 -0500

    WPS migration: include testmode flag in requests (#60041)

--- plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
+++ plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
@@ -110,7 +110,7 @@ class WC_Gateway_Paypal_Request {
 	 */
 	public function create_paypal_order( $order ) {
 		try {
-			$request_body = $this->get_paypal_create_order_request_body( $order );
+			$order_request_params = $this->get_paypal_create_order_request_params( $order );
 
 			// phpcs:disable Generic.Commenting.Todo.TaskFound
 			// TODO: Replace with the wpcom endpoint when it's ready.
@@ -121,7 +121,12 @@ class WC_Gateway_Paypal_Request {
 					'headers' => array(
 						'Content-Type' => 'application/json',
 					),
-					'body'    => wp_json_encode( $request_body ),
+					'body'    => wp_json_encode(
+						array(
+							'testmode' => $this->gateway->testmode,
+							'order'    => $order_request_params,
+						)
+					),
 					'timeout' => 60,
 				)
 			);
@@ -176,6 +181,7 @@ class WC_Gateway_Paypal_Request {
 			$request_body = array(
 				'capture_url'     => $capture_url,
 				'paypal_order_id' => $paypal_order_id,
+				'testmode'        => $this->gateway->testmode,
 			);
 
 			// phpcs:disable Generic.Commenting.Todo.TaskFound
@@ -264,12 +270,12 @@ class WC_Gateway_Paypal_Request {
 	}
 
 	/**
-	 * Build the request body for the PayPal create-order request.
+	 * Build the request parameters for the PayPal create-order request.
 	 *
 	 * @param WC_Order $order Order object.
 	 * @return array
 	 */
-	private function get_paypal_create_order_request_body( $order ) {
+	private function get_paypal_create_order_request_params( $order ) {
 		$currency = $order->get_currency();
 
 		return array(
