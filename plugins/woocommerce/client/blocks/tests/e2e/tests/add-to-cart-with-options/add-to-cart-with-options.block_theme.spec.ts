136a28d1d8e812b2e38ee7a2ded64a77b7e05e33
Albert Juhé Lluveras <contact@albertjuhe.com>
2025-07-29 12:44:04 +0200

    Remove check for aria-haspopup in 'Save' button in Add to Cart + Options e2e test

--- plugins/woocommerce/client/blocks/tests/e2e/tests/add-to-cart-with-options/add-to-cart-with-options.block_theme.spec.ts
+++ plugins/woocommerce/client/blocks/tests/e2e/tests/add-to-cart-with-options/add-to-cart-with-options.block_theme.spec.ts
@@ -263,15 +263,6 @@ test.describe( 'Add to Cart + Options Block', () => {
 
 		await page.getByRole( 'radio', { name: 'Dropdown' } ).click();
 
-		// We need to make sure the block updated before saving.
-		// @see https://github.com/woocommerce/woocommerce/issues/57718
-		// Verify that `.editor-post-publish-button__button` has an attribute
-		// `aria-haspopup="dialog"`. When https://github.com/woocommerce/woocommerce/issues/48936
-		// is fixed, we can simply check that the Save button becomes enabled.
-		await expect(
-			page.getByRole( 'button', { name: 'Save', exact: true } )
-		).toHaveAttribute( 'aria-haspopup', 'dialog' );
-
 		await editor.saveSiteEditorEntities();
 
 		await page.goto( '/hoodie' );
