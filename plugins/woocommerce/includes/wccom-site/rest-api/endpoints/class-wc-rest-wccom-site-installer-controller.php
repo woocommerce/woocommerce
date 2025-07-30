39a7e86279f764c09c1cc17d76aac14089ec53a8
Thilina Pituwala <thilina.hasantha@gmail.com>
2025-07-30 10:54:11 +1000

    Fix lint errors in WC_REST_WCCOM_Site_Installer_Controller.

--- plugins/woocommerce/includes/wccom-site/rest-api/endpoints/class-wc-rest-wccom-site-installer-controller.php
+++ plugins/woocommerce/includes/wccom-site/rest-api/endpoints/class-wc-rest-wccom-site-installer-controller.php
@@ -134,7 +134,7 @@ class WC_REST_WCCOM_Site_Installer_Controller extends WC_REST_WCCOM_Site_Control
 		$installation_manager = new WC_WCCOM_Site_Installation_Manager( $product_id, '' );
 
 		try {
-			$state = $installation_manager->get_installation_status( );
+			$state = $installation_manager->get_installation_status();
 			return $this->success_response( $product_id, $state );
 		} catch ( Installer_Error $exception ) {
 			return $this->failure_response( $product_id, $exception );
@@ -194,7 +194,7 @@ class WC_REST_WCCOM_Site_Installer_Controller extends WC_REST_WCCOM_Site_Control
 	 * Generate a standardized response for a successful request.
 	 *
 	 * @param int                                   $product_id Product ID.
-	 * @param WC_WCCOM_Site_Installation_State|null $state
+	 * @param WC_WCCOM_Site_Installation_State|null $state Installation state object.
 	 * @return WP_REST_Response|WP_Error
 	 */
 	protected function success_response( $product_id, ?WC_WCCOM_Site_Installation_State $state = null ) {
