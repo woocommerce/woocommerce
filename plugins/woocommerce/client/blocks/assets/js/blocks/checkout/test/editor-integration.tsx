1a5ed44c40eefdff3ac4ebbbcbcb7116903f6f53
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-08-04 15:36:51 +0200

    Await for more elements

--- plugins/woocommerce/client/blocks/assets/js/blocks/checkout/test/editor-integration.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/checkout/test/editor-integration.tsx
@@ -81,11 +81,13 @@ describe( 'Checkout block editor integration', () => {
 			( element ) => element.textContent === 'Audio'
 		);
 
-		// Verify Table option is available (should be available on all blocks).
-		expect( tableOption ).toBeInTheDocument();
+		await waitFor( () => {
+			// Verify Table option is available (should be available on all blocks).
+			expect( tableOption ).toBeInTheDocument();
 
-		// Verify Audio option is available (added only for checkout totals block).
-		expect( audioOption ).toBeInTheDocument();
+			// Verify Audio option is available (added only for checkout totals block).
+			expect( audioOption ).toBeInTheDocument();
+		} );
 
 		await act( async () => {
 			await userEvent.click(
