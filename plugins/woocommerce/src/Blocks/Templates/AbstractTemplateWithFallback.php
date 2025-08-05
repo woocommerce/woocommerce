9f79d8cc7c5d1003cf2ad1abc5e6494891368667
Albert Juhé Lluveras <contact@albertjuhe.com>
2025-08-05 10:49:45 +0200

    Fix fall back template some times not being added to the hierarchy when it was the first match

--- plugins/woocommerce/src/Blocks/Templates/AbstractTemplateWithFallback.php
+++ plugins/woocommerce/src/Blocks/Templates/AbstractTemplateWithFallback.php
@@ -33,7 +33,8 @@ abstract class AbstractTemplateWithFallback extends AbstractTemplate {
 	 * @param array $templates Templates that match the taxonomy_template_hierarchy.
 	 */
 	public function template_hierarchy( $templates ) {
-		$index = array_search( static::SLUG, $templates, true ) || array_search( static::SLUG . '.php', $templates, true );
+		$index = array_search( static::SLUG, $templates, true ) !== false ? array_search( static::SLUG, $templates, true ) : array_search( static::SLUG . '.php', $templates, true );
+
 		if (
 			false !== $index && (
 				! array_key_exists( $index + 1, $templates ) || $templates[ $index + 1 ] !== $this->fallback_template
