70a1793c388cb63f6695dcbd68a30d9812f13bdc
M. L. Giannotta <sunyatasattva@gmail.com>
2025-08-07 16:06:38 +0200

    Change `alert` to `status`

--- plugins/woocommerce/templates/notices/notice.php
+++ plugins/woocommerce/templates/notices/notice.php
@@ -26,7 +26,7 @@ if ( ! $notices ) {
 ?>
 
 <?php foreach ( $notices as $notice ) : ?>
-	<div class="woocommerce-info"<?php echo wc_get_notice_data_attr( $notice ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?> role="alert">
+	<div class="woocommerce-info"<?php echo wc_get_notice_data_attr( $notice ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?> role="status">
 		<?php echo wc_kses_notice( $notice['notice'] ); ?>
 	</div>
 <?php endforeach; ?>
