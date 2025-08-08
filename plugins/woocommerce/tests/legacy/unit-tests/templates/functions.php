0c281bc160f888926a1c6168d0a114f87e5b81a0
Peter Wilson <git-commit-by@peterwilson.cc>
2025-08-08 12:19:31 +1000

    Test aria-label is escaped for output.

--- plugins/woocommerce/tests/legacy/unit-tests/templates/functions.php
+++ plugins/woocommerce/tests/legacy/unit-tests/templates/functions.php
@@ -136,6 +136,23 @@ class WC_Tests_Template_Functions extends WC_Unit_Test_Case {
 	}
 
 	/**
+	 * Test: test_wc_dropdown_variation_attribute_options_escapes_aria_label_attribute.
+	 */
+	public function test_wc_dropdown_variation_attribute_options_escapes_aria_label_attribute() {
+		$product = WC_Helper_Product::create_variation_product();
+
+		$this->expectOutputString( '<select id="pa_size" class="" name="attribute_pa_size" aria-label="&quot; onload=&quot;alert(&#039;XSS&#039;)&quot;" data-attribute_name="attribute_pa_size" data-show_option_none="yes"><option value="">Choose an option</option><option value="huge" >huge</option><option value="large" >large</option><option value="small" >small</option></select>' );
+
+		wc_dropdown_variation_attribute_options(
+			array(
+				'product'   => $product,
+				'attribute' => 'pa_size',
+				'aria-label' => '" onload="alert(\'XSS\')"',
+			)
+		);
+	}
+
+	/**
 	 * Test wc_query_string_form_fields.
 	 *
 	 * @return void
