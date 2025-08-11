a7b60a3f4344eb6f9560529ba5e9c47830bd9e2c
Akeda Bagus <akeda.bagus@automattic.com>
2025-08-11 17:29:36 +0700

    Slice during mapping and remove the subsequent reassignment

--- plugins/woocommerce/client/admin/client/marketplace/components/product-list-content/no-results.tsx
+++ plugins/woocommerce/client/admin/client/marketplace/components/product-list-content/no-results.tsx
@@ -44,18 +44,13 @@ export default function NoResults( props: {
 					return;
 				}
 
-				const productGroupsToDisplay = products.filter( ( group ) => {
-					return productGroupIds.includes( group.id );
-				} ).map( group => ( { ...group, items: { ...group.items } } ) );
+				const productGroupsToDisplay = products
+					.filter( group => productGroupIds.includes( group.id ) )
+					.map( group => ( { ...group, items: group.items.slice( 0, 4 ) } ) );
 
 				if ( ! productGroupsToDisplay ) {
 					return;
 				}
-
-				// Limit productGroup.items to 4 items.
-				productGroupsToDisplay.forEach( ( group ) => {
-					group.items = group.items.slice( 0, 4 );
-				} );
 
 				setProductGroups( productGroupsToDisplay );
 			} )
