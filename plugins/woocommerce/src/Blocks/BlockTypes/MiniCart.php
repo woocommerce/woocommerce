eb32f224c82f75f94b611af6d51e7afdf6e0a6db
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2025-08-19 14:12:51 +0200

    Fix linting

--- plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
@@ -576,41 +576,44 @@ class MiniCart extends AbstractBlock {
 				: '';
 
 			// Render the minicart overlay in the body, outside of the block itself.
-			add_action( 'wp_footer', function() {
-				ob_start();
-				$template_part_contents           = $this->get_template_part_contents( false );
-				$template_part_contents           = do_blocks( $this->process_template_contents( $template_part_contents ) );
-				
-				?>
+			add_action(
+				'wp_footer',
+				function () {
+					ob_start();
+					$template_part_contents = $this->get_template_part_contents( false );
+					$template_part_contents = do_blocks( $this->process_template_contents( $template_part_contents ) );					
+					?>
 
-				<div
-					data-wp-interactive="woocommerce/mini-cart"
-					data-wp-router-region='{ "id": "woocommerce/mini-cart-overlay", "attachTo": "body" }'
-					data-wp-key="wc-mini-cart-overlay"
-					data-wp-on--click="callbacks.overlayCloseDrawer"
-					data-wp-bind--class="state.drawerOverlayClass"
-				>
-					<div 
-						data-wp-bind--role="state.drawerRole"
-						data-wp-bind--aria-modal="state.isOpen"
-						data-wp-bind--aria-hidden="!state.isOpen"
-						data-wp-bind--tabindex="state.drawerTabIndex"
-						class="wc-block-mini-cart__drawer wc-block-components-drawer is-mobile"
+					<div
+						data-wp-interactive="woocommerce/mini-cart"
+						data-wp-router-region='{ "id": "woocommerce/mini-cart-overlay", "attachTo": "body" }'
+						data-wp-key="wc-mini-cart-overlay"
+						data-wp-on--click="callbacks.overlayCloseDrawer"
+						data-wp-bind--class="state.drawerOverlayClass"
 					>
-						<div class="wc-block-components-drawer__content">
-							<div class="wc-block-mini-cart__template-part">
-								<?php
-									// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-									echo $template_part_contents;
-								?>
+						<div 
+							data-wp-bind--role="state.drawerRole"
+							data-wp-bind--aria-modal="state.isOpen"
+							data-wp-bind--aria-hidden="!state.isOpen"
+							data-wp-bind--tabindex="state.drawerTabIndex"
+							class="wc-block-mini-cart__drawer wc-block-components-drawer is-mobile"
+						>
+							<div class="wc-block-components-drawer__content">
+								<div class="wc-block-mini-cart__template-part">
+									<?php
+										// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+										echo $template_part_contents;
+									?>
+								</div>
 							</div>
 						</div>
-					</div>
-				</div>				
+					</div>				
 
-				<?php
-				echo wp_interactivity_process_directives( ob_get_clean() );
-			});
+					<?php
+					// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					echo wp_interactivity_process_directives( ob_get_clean() );
+				}
+			);
 
 			ob_start();
 			?>
