f3cd0ad9444c58b4a7148081ac777f6d5a234606
Cvetan Cvetanov <cvetan.cvetanov@automattic.com>
2025-08-19 09:53:48 +0300

    Fix linting errors

--- plugins/woocommerce/client/admin/client/settings-payments/components/modals/index.ts
+++ plugins/woocommerce/client/admin/client/settings-payments/components/modals/index.ts
@@ -1,5 +1,7 @@
-// Import styles once to avoid build errors.
-import './modals.scss';
+/**
+ * Internal dependencies
+ */
+import './modals.scss'; // Import styles once to avoid build errors.
 
 export * from './woo-payments-reset-account-modal';
 export * from './woo-payments-post-sandbox-account-setup-modal';
