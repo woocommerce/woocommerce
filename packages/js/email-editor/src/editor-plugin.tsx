9a406081226a8b66cf6888e111fd06d344ebb503
Rostislav Wolny <rosta.wolny@automattic.com>
2025-08-19 16:57:13 +0200

    Initialize block hooks and call unmount cleanup functions

--- packages/js/email-editor/src/editor-plugin.tsx
+++ packages/js/email-editor/src/editor-plugin.tsx
@@ -8,13 +8,20 @@ import { useEffect, useState } from 'react';
  */
 import { SendPreview } from './components/preview';
 import { StylesSidebar } from './components/styles-sidebar';
+import { initBlocks } from './blocks';
 import { createStore } from './store';
 
 export const EditorPlugin = () => {
 	const [ isInitialized, setIsInitialized ] = useState( false );
 	useEffect( () => {
+		const cleanups = [];
 		createStore();
+		cleanups.push( initBlocks() );
 		setIsInitialized( true );
+		return () => {
+			console.log( 'Unmounting editor plugin' );
+			cleanups.forEach( ( cleanup ) => cleanup() );
+		};
 	}, [] );
 
 	if ( ! isInitialized ) {
