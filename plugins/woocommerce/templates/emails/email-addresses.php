3ebb088294e2dd44770ea2520e5cf5f93f043699
Peter Wilson <git-commit-by@peterwilson.cc>
2025-08-19 12:33:21 +1000

    Add presentation role to address layout table.

--- plugins/woocommerce/templates/emails/email-addresses.php
+++ plugins/woocommerce/templates/emails/email-addresses.php
@@ -26,7 +26,7 @@ $shipping = $order->get_formatted_shipping_address();
 
 $email_improvements_enabled = FeaturesUtil::feature_is_enabled( 'email_improvements' );
 
-?><table id="addresses" cellspacing="0" cellpadding="0" style="width: 100%; vertical-align: top; margin-bottom: <?php echo $email_improvements_enabled ? '0' : '40px'; ?>; padding:0;" border="0">
+?><table id="addresses" cellspacing="0" cellpadding="0" style="width: 100%; vertical-align: top; margin-bottom: <?php echo $email_improvements_enabled ? '0' : '40px'; ?>; padding:0;" border="0" role="presentation">
 	<tr>
 		<td class="font-family text-align-left" style="border:0; padding:0;" valign="top" width="50%">
 			<?php if ( $email_improvements_enabled ) { ?>
