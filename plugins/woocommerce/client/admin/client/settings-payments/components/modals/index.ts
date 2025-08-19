3083970733c9c0ec3e336d1cff03840e211afb2c
Cvetan Cvetanov <cvetan.cvetanov@automattic.com>
2025-08-19 17:53:12 +0300

    Import the styles into the settings-payments-main.scss instead of modules index

--- plugins/woocommerce/client/admin/client/settings-payments/components/modals/index.ts
+++ plugins/woocommerce/client/admin/client/settings-payments/components/modals/index.ts
@@ -1,7 +1,2 @@
-/**
- * Internal dependencies
- */
-import './modals.scss'; // Import styles once to avoid build errors.
-
 export * from './woo-payments-reset-account-modal';
 export * from './woo-payments-post-sandbox-account-setup-modal';
