65446b78828d51ef5b8088a92fc4c8432d19256b
Cvetan Cvetanov <cvetan.cvetanov@automattic.com>
2025-08-19 12:41:35 +0300

    Fix unit tests

--- plugins/woocommerce/client/admin/client/settings-payments/components/modals/test/woo-payments-update-required-modal.test.tsx
+++ plugins/woocommerce/client/admin/client/settings-payments/components/modals/test/woo-payments-update-required-modal.test.tsx
@@ -2,27 +2,11 @@
  * External dependencies
  */
 import { render, fireEvent, screen } from '@testing-library/react';
-import { getAdminLink, getSetting } from '@woocommerce/settings';
 
 /**
  * Internal dependencies
  */
 import { WooPaymentsUpdateRequiredModal } from '..';
-
-// Mock @woocommerce/settings
-jest.mock( '@woocommerce/settings', () => ( {
-	getAdminLink: jest.fn(),
-	getSetting: jest.fn( () => ( {} ) ),
-} ) );
-
-// Mock window.location
-const mockLocation = {
-	href: '',
-};
-Object.defineProperty( window, 'location', {
-	value: mockLocation,
-	writable: true,
-} );
 
 describe( 'WooPaymentsUpdateRequiredModal', () => {
 	const defaultProps = {
@@ -32,11 +16,6 @@ describe( 'WooPaymentsUpdateRequiredModal', () => {
 
 	beforeEach( () => {
 		jest.clearAllMocks();
-		( getAdminLink as jest.Mock ).mockReturnValue(
-			'/wp-admin/plugins.php'
-		);
-		( getSetting as jest.Mock ).mockReturnValue( {} );
-		mockLocation.href = '';
 	} );
 
 	it( 'should render modal when isOpen is true', () => {
@@ -106,46 +85,5 @@ describe( 'WooPaymentsUpdateRequiredModal', () => {
 		fireEvent.click( notNowButton );
 
 		expect( onClose ).toHaveBeenCalledTimes( 1 );
-	} );
-
-	it( 'should navigate to plugins page when "Update WooPayments" button is clicked', () => {
-		render( <WooPaymentsUpdateRequiredModal { ...defaultProps } /> );
-
-		const updateButton = screen.getByRole( 'button', {
-			name: 'Update WooPayments',
-		} );
-		fireEvent.click( updateButton );
-
-		expect( getAdminLink ).toHaveBeenCalledWith( 'plugins.php' );
-		expect( mockLocation.href ).toBe( '/wp-admin/plugins.php' );
-	} );
-
-	it( 'should show loading state on "Update WooPayments" button when clicked', () => {
-		render( <WooPaymentsUpdateRequiredModal { ...defaultProps } /> );
-
-		const updateButton = screen.getByRole( 'button', {
-			name: 'Update WooPayments',
-		} );
-
-		expect( updateButton ).not.toHaveClass( 'is-busy' );
-
-		fireEvent.click( updateButton );
-
-		expect( updateButton ).toHaveClass( 'is-busy' );
-	} );
-
-	it( 'should disable "Not now" button when updating', () => {
-		render( <WooPaymentsUpdateRequiredModal { ...defaultProps } /> );
-
-		const updateButton = screen.getByRole( 'button', {
-			name: 'Update WooPayments',
-		} );
-		const notNowButton = screen.getByRole( 'button', { name: 'Not now' } );
-
-		expect( notNowButton ).not.toBeDisabled();
-
-		fireEvent.click( updateButton );
-
-		expect( notNowButton ).toBeDisabled();
 	} );
 } );
