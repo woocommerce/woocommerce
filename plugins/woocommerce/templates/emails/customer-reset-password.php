437992b9c5fcccf39e0b69b4e843fdaa35330659
Peter Wilson <git-commit-by@peterwilson.cc>
2025-08-19 12:38:47 +1000

    Add presentation roles to additional content tables.

--- plugins/woocommerce/templates/emails/customer-reset-password.php
+++ plugins/woocommerce/templates/emails/customer-reset-password.php
@@ -61,7 +61,7 @@ $email_improvements_enabled = FeaturesUtil::feature_is_enabled( 'email_improveme
  * Show user-defined additional content - this is set in each email's settings.
  */
 if ( $additional_content ) {
-	echo $email_improvements_enabled ? '<table border="0" cellpadding="0" cellspacing="0" width="100%"><tr><td class="email-additional-content email-additional-content-aligned">' : '';
+	echo $email_improvements_enabled ? '<table border="0" cellpadding="0" cellspacing="0" width="100%" role="presentation"><tr><td class="email-additional-content email-additional-content-aligned">' : '';
 	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
 	echo $email_improvements_enabled ? '</td></tr></table>' : '';
 }
