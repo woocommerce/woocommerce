d4e82e3ae0b55649b1ebd04d46d4509699f3d88b
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-08-21 14:40:00 +0200

    Simplify useEditedPostType since we can rely on a single store since WP 6.7

--- plugins/woocommerce/client/admin/client/wp-admin-scripts/command-palette/use-edited-post-type.js
+++ plugins/woocommerce/client/admin/client/wp-admin-scripts/command-palette/use-edited-post-type.js
@@ -4,32 +4,11 @@
 import { useSelect } from '@wordpress/data';
 
 export const useEditedPostType = () => {
-	// This will return 'post' or 'page' when in the post/page editor.
-	const { currentPostType } = useSelect( ( select ) => {
-		const store = select( 'core/editor' );
-		if ( ! store ) {
-			return {
-				currentPostType: null,
-			};
-		}
-		const { getCurrentPostType } = store;
-		return {
-			currentPostType: getCurrentPostType(),
-		};
-	} );
-	// This will return 'wp_template' or 'wp_template_part' when in the Site Editor.
 	const { editedPostType } = useSelect( ( select ) => {
-		const store = select( 'core/edit-site' );
-		if ( ! store ) {
-			return {
-				editedPostType: null,
-			};
-		}
-		const { getEditedPostType } = store;
 		return {
-			editedPostType: getEditedPostType(),
+			editedPostType: select( 'core/editor' ).getCurrentPostType(),
 		};
 	} );
 
-	return { editedPostType: editedPostType || currentPostType };
+	return { editedPostType };
 };
