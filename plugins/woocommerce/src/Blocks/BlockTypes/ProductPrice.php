378be9501aec68a1c74e7768a2e05a7b42433aa4
Albert Juhé Lluveras <contact@albertjuhe.com>
2025-08-21 15:24:24 +0200

    Don't make Product Price block interactive if all variations have the same price

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductPrice.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductPrice.php
@@ -85,34 +85,45 @@ class ProductPrice extends AbstractBlock {
 			if ( $is_interactive ) {
 				$variations_data           = $product->get_available_variations();
 				$formatted_variations_data = array();
+				$is_price_html_different   = false;
+				$last_price_html           = '';
 				foreach ( $variations_data as $variation ) {
 					if ( ! isset( $variation['variation_id'] ) || ! isset( $variation['price_html'] ) ) {
 							continue;
+					}
+					if ( $variation['price_html'] !== $last_price_html ) {
+						$is_price_html_different = true;
+					} else {
+						$last_price_html = $variation['price_html'];
 					}
 					$formatted_variations_data[ $variation['variation_id'] ] = array(
 						'price_html' => $variation['price_html'],
 					);
 				}
 
-				wp_interactivity_state(
-					'woocommerce',
-					array(
-						'products' => array(
-							$product->get_id() => array(
-								'price_html' => $product->get_price_html(),
-								'variations' => $formatted_variations_data,
+				if ( ! $is_price_html_different ) {
+					$is_interactive = false;
+				} else {
+					wp_interactivity_state(
+						'woocommerce',
+						array(
+							'products' => array(
+								$product->get_id() => array(
+									'price_html' => $product->get_price_html(),
+									'variations' => $formatted_variations_data,
+								),
 							),
-						),
-					)
-				);
+						)
+					);
 
-				wp_enqueue_script_module( 'woocommerce/product-elements' );
-				$wrapper_attributes['data-wp-interactive'] = 'woocommerce/product-elements';
-				$context                                   = array(
-					'productElementKey' => 'price_html',
-				);
-				$wrapper_attributes['data-wp-context']     = wp_json_encode( $context, JSON_NUMERIC_CHECK | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP );
-				$watch_attribute                           = 'data-wp-watch="callbacks.updateValue"';
+					wp_enqueue_script_module( 'woocommerce/product-elements' );
+					$wrapper_attributes['data-wp-interactive'] = 'woocommerce/product-elements';
+					$context                                   = array(
+						'productElementKey' => 'price_html',
+					);
+					$wrapper_attributes['data-wp-context']     = wp_json_encode( $context, JSON_NUMERIC_CHECK | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP );
+					$watch_attribute                           = 'data-wp-watch="callbacks.updateValue"';
+				}
 			}
 
 			return sprintf(
