3fa752f88f82d0569e2ca63a725f2e8a8d3eda27
Mayisha <mayisha.alam@automattic.com>
2025-08-22 13:20:26 +0600

    show notice on the payments settings page

--- plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-notices.php
+++ plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-notices.php
@@ -23,6 +23,11 @@ class WC_Gateway_Paypal_Notices {
 	 */
 	public function __construct() {
 		add_action( 'admin_notices', array( $this, 'add_paypal_migration_notice' ) );
+
+        // Use admin_head to inject notice on payments settings page.
+		// This bypasses the suppress_admin_notices() function which removes all admin_notices hooks on the payments page
+		// This is a workaround to avoid the notice being suppressed by the suppress_admin_notices() function.
+		add_action( 'admin_head', array( $this, 'add_paypal_migration_notice_on_payments_settings_page' ) );
 	}
 
 	/**
@@ -47,7 +52,23 @@ class WC_Gateway_Paypal_Notices {
 		}
 
 		echo '<div class="notice notice-warning is-dismissible">';
-		echo '<p>WooCommerce will automatically upgrade your PayPal integration from PayPal Standard to PayPal Payments (PPCP) in version 10.3.0, for a more reliable and modern checkout experience. If you prefer not to migrate, we recommed upgrading to <a href="https://woocommerce.com/document/woocommerce-paypal-payments/paypal-payments-upgrade-guide/" target="_blank">PayPal Payments</a> extension.</p>';
+		echo '<p>WooCommerce will automatically upgrade your PayPal integration from PayPal Standard to PayPal Payments (PPCP) in version 10.3.0, for a more reliable and modern checkout experience. If you prefer not to migrate, we recommend upgrading to <a href="https://woocommerce.com/document/woocommerce-paypal-payments/paypal-payments-upgrade-guide/" target="_blank">PayPal Payments</a> extension.</p>';
 		echo '</div>';
+	}
+
+    /**
+	 * Add notice warning about the migration to PayPal Payments on the Payments settings page.
+	 *
+	 * @return void
+	 */
+	public function add_paypal_migration_notice_on_payments_settings_page() {
+		global $current_tab, $current_section;
+		$is_payments_settings_page = 'woocommerce_page_wc-settings' === get_current_screen()->id && 'checkout' === $current_tab && empty( $current_section );
+
+		// Only add the notice from this callback on the payments settings page.
+		if ( ! $is_payments_settings_page ) {
+			return;
+		}
+		$this->add_paypal_migration_notice();
 	}
 }
