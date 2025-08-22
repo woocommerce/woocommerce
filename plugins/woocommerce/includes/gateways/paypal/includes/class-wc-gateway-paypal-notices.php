0769ab85e59a13ab454bef43f1f207d7d8190102
Mayisha <mayisha.alam@automattic.com>
2025-08-22 20:52:50 +0600

    add release calendar link

--- plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-notices.php
+++ plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-notices.php
@@ -52,14 +52,17 @@ class WC_Gateway_Paypal_Notices {
 		}
 
 		$doc_url     = 'https://woocommerce.com/document/woocommerce-paypal-payments/paypal-payments-upgrade-guide/';
+		$release_url = 'https://developer.woocommerce.com/release-calendar/';
 		$dismiss_url = wp_nonce_url(
 			add_query_arg( 'wc-hide-notice', 'paypal_migration' ),
 			'woocommerce_hide_notices_nonce',
 			'_wc_notice_nonce'
 		);
 		$message     = sprintf(
-			/* translators: 1: opening <a> tag, 2: closing </a> tag */
-			esc_html__( 'WooCommerce will automatically upgrade your PayPal integration from PayPal Standard to PayPal Payments (PPCP) in version 10.3.0, for a more reliable and modern checkout experience. If you prefer not to migrate, we recommend upgrading to %1$sPayPal Payments%2$s extension.', 'woocommerce' ),
+			/* translators: 1: opening <a> tag, 2: closing </a> tag, 3: opening <a> tag, 4: closing </a> tag */
+			esc_html__( 'WooCommerce will automatically upgrade your PayPal integration from PayPal Standard to PayPal Payments (PPCP) in version %1$s10.3.0%2$s, for a more reliable and modern checkout experience. If you prefer not to migrate, we recommend upgrading to %3$sPayPal Payments%4$s extension.', 'woocommerce' ),
+			'<a href="' . esc_url( $release_url ) . '" target="_blank" rel="noopener noreferrer">',
+			'</a>',
 			'<a href="' . esc_url( $doc_url ) . '" target="_blank" rel="noopener noreferrer">',
 			'</a>',
 		);
