d0f72c787d75834c29335ac08f853837b5972669
Anne Mirasol <anne.mirasol@automattic.com>
2025-08-22 14:56:33 -0500

    Do not load and instantiate if not is_admin() or is ajax

--- plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
+++ plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
@@ -569,6 +569,10 @@ class WC_Gateway_Paypal extends WC_Payment_Gateway {
 add_action(
 	'init',
 	function () {
+		if ( ! is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+			return;
+		}
+
 		include_once __DIR__ . '/includes/class-wc-gateway-paypal-notices.php';
 		new WC_Gateway_Paypal_Notices();
 	}
