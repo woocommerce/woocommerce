5c0d703f9f158bd15734549a96b02e3ddfb64034
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-08-26 15:42:50 +0200

    Use store object rather than hardcoded store name

--- plugins/woocommerce/client/admin/client/wp-admin-scripts/command-palette/use-edited-post-type.js
+++ plugins/woocommerce/client/admin/client/wp-admin-scripts/command-palette/use-edited-post-type.js
@@ -2,11 +2,12 @@
  * External dependencies
  */
 import { useSelect } from '@wordpress/data';
+import { store } from '@wordpress/editor';
 
 export const useEditedPostType = () => {
 	const { editedPostType } = useSelect( ( select ) => {
 		return {
-			editedPostType: select( 'core/editor' ).getCurrentPostType(),
+			editedPostType: select( store ).getCurrentPostType(),
 		};
 	} );
 
