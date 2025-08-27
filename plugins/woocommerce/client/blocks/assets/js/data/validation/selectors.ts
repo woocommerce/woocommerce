133d74940fc6a388a34f0c63bd3ccfe0b8c0b418
Raluca Stan <ralucastn@gmail.com>
2025-08-27 16:44:56 +0200

    Update selector

--- plugins/woocommerce/client/blocks/assets/js/data/validation/selectors.ts
+++ plugins/woocommerce/client/blocks/assets/js/data/validation/selectors.ts
@@ -41,6 +41,6 @@ export const hasValidationErrors = ( state: State ) => {
  * Gets all validation errors.
  *
  * @param { State } state The current state.
- * @return { Record<string, any> } All validation errors.
+ * @return { Record<string, FieldValidationStatus> } All validation errors.
  */
 export const getValidationErrors = ( state: State ) => state;
