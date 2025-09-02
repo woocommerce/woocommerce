5214df16249a541372c72856cb9bb074f7695d99
Tung Du <dinhtungdu@gmail.com>
2025-09-02 16:09:35 +0700

    fix: update template version

--- plugins/woocommerce/templates/single-product/related.php
+++ plugins/woocommerce/templates/single-product/related.php
@@ -12,7 +12,7 @@
  *
  * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates
- * @version     9.6.0
+ * @version     10.3.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
