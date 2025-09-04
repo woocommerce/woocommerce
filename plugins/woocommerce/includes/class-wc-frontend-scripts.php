7a1b15ed5eecec03dea5c442a96954d9176684e4
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-09-04 15:23:59 +0100

    Remove deprecated function from branding HTML

--- plugins/woocommerce/includes/class-wc-frontend-scripts.php
+++ plugins/woocommerce/includes/class-wc-frontend-scripts.php
@@ -669,9 +669,7 @@ class WC_Frontend_Scripts {
 							return array(
 								'id'            => sanitize_key( $provider->id ),
 								'name'          => sanitize_text_field( $provider->name ),
-								'branding_html' => wp_targeted_link_rel(
-									trim( wp_kses_post( (string) ( $provider->branding_html ?? '' ) ) )
-								),
+								'branding_html' => trim( wp_kses_post( (string) ( $provider->branding_html ?? '' ) ) ),
 							);
 						},
 						$providers
