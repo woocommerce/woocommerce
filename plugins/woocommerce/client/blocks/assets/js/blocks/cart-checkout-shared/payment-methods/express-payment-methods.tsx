479349e0e0af696f5323ab3260fc757e5017443b
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2025-09-05 10:32:44 +1000

    Fix typos in comments.
    Co-authored-by: Thomas Roberts <5656702+opr@users.noreply.github.com>

--- plugins/woocommerce/client/blocks/assets/js/blocks/cart-checkout-shared/payment-methods/express-payment-methods.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/cart-checkout-shared/payment-methods/express-payment-methods.tsx
@@ -164,8 +164,8 @@ const ExpressPaymentMethods = () => {
 	 * unordered list to display each option.
 	 *
 	 * When only one express payment option is available, this will use a
-	 * non-sematic DIV for both the wrapper and the individual items. This
-	 * it to prevent accessibility issues caused by a list of one (which isn't
+	 * non-semantic DIV for both the wrapper and the individual items. This
+	 * is to prevent accessibility issues caused by a list of one (which isn't
 	 * a list).
 	 */
 	const ExpressPayWrapper = entries.length > 1 ? 'ul' : 'div';
