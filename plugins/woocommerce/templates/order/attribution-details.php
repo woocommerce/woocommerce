395dcd1350609b22e28eddad2d69f126fbe5a670
“Joshua <joshuatf@gmail.com>
2025-09-08 10:42:41 -0400

    Add extra safety check around marketplace class existence and tests

--- plugins/woocommerce/templates/order/attribution-details.php
+++ plugins/woocommerce/templates/order/attribution-details.php
@@ -136,7 +136,7 @@ defined( 'ABSPATH' ) || exit;
 		</span>
 	<?php endif; ?>
 	<!-- A placeholder for the OA install banner React component. -->
-	<?php if ( \WC_Marketplace_Suggestions::allow_suggestions() ) : ?>
+	<?php if ( class_exists( 'WC_Marketplace_Suggestions' ) && \WC_Marketplace_Suggestions::allow_suggestions() ) : ?>
 		<div id="order-attribution-install-banner-slotfill"></div>
 	<?php endif; ?>
 </div>
