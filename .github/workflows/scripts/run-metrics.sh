8c83a6338c2154ba54110cd795fdced1d81087aa
Vladimir Reznichenko <kalessil@gmail.com>
2025-09-08 12:31:00 +0200

    CI: cleanup.

--- .github/workflows/scripts/run-metrics.sh
+++ .github/workflows/scripts/run-metrics.sh
@@ -33,11 +33,7 @@ if [ "$GITHUB_EVENT_NAME" == "push" ] || [ "$GITHUB_EVENT_NAME" == "pull_request
 		git fetch --depth=1 --no-tags origin trunk
 		BASE_SHA=$(git rev-parse origin/trunk)
 
-		BASE_SHA=9d76114472b7bc17d39b21ffe22c36637169669a
-		# 9d76114472b7bc17d39b21ffe22c36637169669a next after 55f855a2e6d769b5ae44305b2772eb30d3e721df
-		# Worked, sequential pushes testing:
-		# - https://github.com/woocommerce/woocommerce/actions/runs/17462514966/job/49590309799?pr=60763
-		# -
+		BASE_SHA=55f855a2e6d769b5ae44305b2772eb30d3e721df
 	fi
 	HEAD_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 	WP_VERSION=$(awk -F ': ' '/^Tested up to/{print $2}' readme.txt)
