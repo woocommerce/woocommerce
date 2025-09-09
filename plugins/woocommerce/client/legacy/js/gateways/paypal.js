000baeb596c7e7f4ad8d1be8678da25645dd2295
Mayisha <mayisha.alam@automattic.com>
2025-09-09 18:31:01 +0600

    show error on shortcode cart, checkout and product pages

--- plugins/woocommerce/client/legacy/js/gateways/paypal.js
+++ plugins/woocommerce/client/legacy/js/gateways/paypal.js
@@ -10,15 +10,32 @@ jQuery(function ($) {
 		const buttons = paypal.Buttons( {
 			async createOrder() {
 				// TODO: Add createOrder logic here
+				return null;
 			},
 
 			async onApprove( data ) {
 				// TODO: Add onApprove logic here
 			},
 
-			onError: function ( err ) {
-				// TODO: Add onError logic here
-				console.error( 'PayPal error:', err );
+			onError: function ( error ) {
+				console.log( 'PayPal onError:', error );
+				
+				const messageWrapper =
+					'<ul class="woocommerce-error" role="alert"><li>' +
+						'PayPal error: ' +
+						error.message +
+					'</li></ul>';	
+				
+				const $noticeContainer = $( '.woocommerce-notices-wrapper' ).first();
+
+				if ( ! $noticeContainer.length ) {
+					return;
+				}
+		
+				$(
+					'.woocommerce-NoticeGroup-checkout, .woocommerce-error, .woocommerce-message'
+				).remove();
+				$noticeContainer.prepend( messageWrapper );
 			},
 
 			onCancel( data ) {
