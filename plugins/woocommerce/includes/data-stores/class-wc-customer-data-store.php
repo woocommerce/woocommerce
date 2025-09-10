f49596a62717598dce2493e630b15a9d8c04ee9e
Vladimir Reznichenko <kalessil@gmail.com>
2025-09-10 08:55:30 +0200

    Performance: cleanup.

--- plugins/woocommerce/includes/data-stores/class-wc-customer-data-store.php
+++ plugins/woocommerce/includes/data-stores/class-wc-customer-data-store.php
@@ -599,11 +599,6 @@ class WC_Customer_Data_Store extends WC_Data_Store_WP implements WC_Customer_Dat
 	 * @return array
 	 */
 	public function get_user_ids_for_billing_email( $emails ) {
-		if ( Features::is_enabled( 'analytics' ) ) {
-			// if no pending wc-admin_import_orders jobs, we can query directly from the lookup tables
-			// SELECT user_id FROM wp_wc_customer_lookup WHERE email = 'fprasetyo@example.org'
-		}
-
 		$emails      = array_unique( array_map( 'strtolower', array_map( 'sanitize_email', $emails ) ) );
 		$users_query = new WP_User_Query(
 			array(
--- plugins/woocommerce/tests/legacy/unit-tests/woocommerce-admin/api/products-lowinstock.php
+++ plugins/woocommerce/tests/legacy/unit-tests/woocommerce-admin/api/products-lowinstock.php
@@ -31,13 +31,14 @@ class WC_Admin_Tests_API_ProductsLowInStock extends WC_REST_Unit_Test_Case {
 				'role' => 'administrator',
 			)
 		);
-		wp_set_current_user( $this->user );
 	}
 
 	/**
 	 * Test low stock query.
 	 */
 	public function test_low_stock() {
+		wp_set_current_user( $this->user );
+
 		// Create a product with stock management.
 		$product = WC_Helper_Product::create_simple_product();
 		$product->set_manage_stock( true );
@@ -71,6 +72,8 @@ class WC_Admin_Tests_API_ProductsLowInStock extends WC_REST_Unit_Test_Case {
 	 * Test multiple products with custom low stock amounts.
 	 */
 	public function test_multiple_products_custom_low_stock() {
+		wp_set_current_user( $this->user );
+
 		// Create three products with different stock quantities.
 		$product1 = WC_Helper_Product::create_simple_product();
 		$product1->set_manage_stock( true );
@@ -104,6 +107,8 @@ class WC_Admin_Tests_API_ProductsLowInStock extends WC_REST_Unit_Test_Case {
 	 * Test multiple products with global low stock setting.
 	 */
 	public function test_multiple_products_global_low_stock() {
+		wp_set_current_user( $this->user );
+
 		// Set global low stock amount.
 		update_option( 'woocommerce_notify_low_stock_amount', 5 );
 
