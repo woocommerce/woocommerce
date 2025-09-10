0cf2412cd3276f319ed3fe20604aa04078bc1261
Peter Wilson <git-commit-by@peterwilson.cc>
2025-09-11 09:04:25 +1000

    Bump modified templates versions.
    Signed-off-by: Peter Wilson <git-commit-by@peterwilson.cc>

--- plugins/woocommerce/templates/emails/customer-fulfillment-updated.php
+++ plugins/woocommerce/templates/emails/customer-fulfillment-updated.php
@@ -12,7 +12,7 @@
  *
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails\HTML
- * @version 10.1.0
+ * @version 10.3.0
  */
 
 defined( 'ABSPATH' ) || exit;
--- plugins/woocommerce/templates/emails/email-order-items.php
+++ plugins/woocommerce/templates/emails/email-order-items.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
- * @version 9.9.0
+ * @version 10.3.0
  */
 
 use Automattic\WooCommerce\Utilities\FeaturesUtil;
