edc70a0cb1ed34bfa4a0e7e88a4790220b2eb38c
louwie17 <lourensschep@gmail.com>
2025-09-11 15:12:32 +0200

    Disable the Analytics refund fix, and only enable for stores without refunds (#60814)
    * Limit the refund fix until explicitly triggered
    
    * Add tool to fix refund data already
    
    * Fix tooltip
    
    * Revert refunds logic if not using full refund data yet
    
    * Add changelog
    
    * Add missing import
    
    * Fix lint issues
    
    * Add doc block
    
    * Update refund tool message
    
    * Add two tests
    
    * Fix lint errors
    
    * Fix lint issue
    
    * Update new full refund check

--- plugins/woocommerce/src/Internal/Admin/Settings.php
+++ plugins/woocommerce/src/Internal/Admin/Settings.php
@@ -11,6 +11,7 @@ use Automattic\WooCommerce\Admin\Features\Features;
 use Automattic\WooCommerce\Admin\PageController;
 use Automattic\WooCommerce\Admin\PluginsHelper;
 use Automattic\WooCommerce\Utilities\FeaturesUtil;
+use Automattic\WooCommerce\Utilities\OrderUtil;
 use WC_Marketplace_Suggestions;
 
 /**
@@ -217,6 +218,7 @@ class Settings {
 			// We may have synced orders with a now-unregistered status.
 			// E.g. an extension that added statuses is now inactive or removed.
 			$settings['unregisteredOrderStatuses'] = $this->get_unregistered_order_statuses();
+			$settings['usesNewFullRefundData']     = OrderUtil::uses_new_full_refund_data();
 		}
 
 		// The separator used for attributes found in Variation titles.
