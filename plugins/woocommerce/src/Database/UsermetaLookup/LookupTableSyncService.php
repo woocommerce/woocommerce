aae157f66912f765648c1445d0be668d5c99426a
Vladimir Reznichenko <kalessil@gmail.com>
2025-09-11 08:36:01 +0200

    Performance: WIP, sync service implementation.

--- plugins/woocommerce/src/Database/UsermetaLookup/LookupTableSyncService.php
+++ plugins/woocommerce/src/Database/UsermetaLookup/LookupTableSyncService.php
@@ -1,8 +1,20 @@
 <?php
 
+declare( strict_types=1 );
+
 namespace Automattic\WooCommerce\Database\UsermetaLookup;
 
+/**
+ * This service handles keeping `wc_user_meta_lookup` table up-to-date with `users` and `usermeta` tables changes.
+ *
+ * @package Automattic\WooCommerce\Database\UsermetaLookup
+ */
 class LookupTableSyncService {
+	/**
+	 * The list of metas we track under lookup table.
+	 *
+	 * @var string[]
+	 */
 	private const META_KEYS = [
 		'billing_email',
 		'first_name',
@@ -11,43 +23,93 @@ class LookupTableSyncService {
 		'wc_last_active',
 	];
 
+	/**
+	 * The lookup table name.
+	 *
+	 * @var string
+	 */
 	private string $table_name;
 
+	/**
+	 * Constructor.
+	 *
+	 * @since 10.3.0
+	 */
 	public function __construct() {
 		global $wpdb;
-
 		$this->table_name = $wpdb->prefix . 'wc_user_meta_lookup';
 	}
 
+	/**
+	 * Adds handlers for WordPress user and meta hooks, which require the lookup table update.
+	 *
+	 * @since 10.3.0
+	 *
+	 * @return void
+	 */
 	public function init_hooks(): void {
-		// user: create/delete; meta: added/updated/removed
-		// do_action( 'edit_user_created_user', $user_id, $notify );
-		// do_action( 'deleted_user', $id, $reassign, $user );
+		add_action( 'edit_user_created_user', array( $this, 'create_entry_for_user' ), 10, 1 );
+		add_action( 'deleted_user', array( $this, 'delete_entry_for_user' ), 10, 1 );
 
-		// $meta_type = user
-		// do_action( "updated_{$meta_type}_meta", $meta_id, $object_id, $meta_key, $_meta_value );
-		// do_action( "added_{$meta_type}_meta", $mid, $object_id, $meta_key, $_meta_value );
-		// do_action( "deleted_{$meta_type}_meta", $meta_ids, $object_id, $meta_key, $_meta_value );
+		add_action( 'updated_user_meta', array( $this, 'update_entry_for_user' ), 10, 4 );
+		add_action( 'added_user_meta', array( $this, 'update_entry_for_user' ), 10, 4 );
+		add_action( 'deleted_user_meta', array( $this, 'update_entry_for_user' ), 10, 3 );
 	}
 
+	/**
+	 * Returns the lookup table name.
+	 *
+	 * @since 10.3.0
+	 *
+	 * @return string
+	 */
 	public function get_table_name(): string {
 		return $this->table_name;
 	}
 
+	/**
+	 * Handles `edit_user_created_user` hook.
+	 *
+	 * @since 10.3.0
+	 *
+	 * @param int|\WP_Error $user_id User ID or user creation error object.
+	 * @return void
+	 */
 	public function create_entry_for_user( $user_id ): void {
-		// create a placeholder record
-		// "INSERT IGNORE INTO {$wpdb->prefix}wc_user_meta_lookup (user_id) VALUES (%d)", $user_id
+		if ( ! is_wp_error( $user_id ) ) {
+			global $wpdb;
+			$wpdb->query( $wpdb->prepare( "INSERT INTO %i (user_id) VALUES (%d)", $this->table_name, $user_id ) );
+		}
 	}
 
-	public function drop_entry_for_user( $user_id ): void {
-		// drop the user-specific record
-		// "DELETE FROM {$wpdb->prefix}wc_user_meta_lookup WHERE user_id = %d", $user_id
+	/**
+	 * Handles `deleted_user` hook.
+	 *
+	 * @since 10.3.0
+	 *
+	 * @param int $user_id User ID.
+	 * @return void
+	 */
+	public function delete_entry_for_user( $user_id ): void {
+		global $wpdb;
+		$wpdb->query( $wpdb->prepare( "DELETE FROM %i WHERE user_id = %d", $this->table_name, $user_id ) );
 	}
 
+	/**
+	 * Handles `added_user_meta`, `updated_user_meta`, `deleted_user_meta` hooks.
+	 *
+	 * @since 10.3.0
+	 *
+	 * @param int|string[] $meta_ids   Meta ID(s).
+	 * @param int          $user_id    User ID.
+	 * @param string       $meta_key   Meta key.
+	 * @param mixed        $meta_value Meta value.
+	 * @return void
+	 */
 	public function update_entry_for_user( $meta_ids, $user_id, $meta_key, $meta_value = null ): void {
 		if ( in_array( $meta_key, self::META_KEYS, true ) ) {
-			// set the column value: provided value or null in case of deleted meta
-			// "UPDATE {$wpdb->prefix}wc_user_meta_lookup SET {$meta_key} = %s WHERE user_id = %d", $meta_value, $user_id
+			global $wpdb;
+			$wpdb->query( $wpdb->prepare( "UPDATE %i SET %i = %s WHERE user_id = %d", $this->table_name, $meta_key, $meta_value, $user_id ) );
 		}
 	}
 }
