88d6237cb0e594259cfa3425304775f7497e9add
Lucas Bustamante <lucasfbustamante@gmail.com>
2025-09-11 10:56:32 -0300

    Revert "WIP"
    This reverts commit be77d1943b6c11f04d84f1c14f2e703ec9935860.

--- plugins/woocommerce/tests/e2e/tests/example.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('site is reachable and has a body', async ({ page }) => {
-  const response = await page.goto('/');
-  expect(response?.status()).toBe(200);
-
-  await expect(page.locator('body')).toBeVisible();
-});
