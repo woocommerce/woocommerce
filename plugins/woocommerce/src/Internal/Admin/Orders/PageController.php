767fee1ab9e19be9fecdae89a4ae38dd63668c53
James Kemp <me@jckemp.com>
2025-09-12 09:19:21 +0100

    Use params instead of string for submenu url

--- plugins/woocommerce/src/Internal/Admin/Orders/PageController.php
+++ plugins/woocommerce/src/Internal/Admin/Orders/PageController.php
@@ -304,7 +304,7 @@ class PageController {
 				$post_type->labels->add_new_item,
 				__( 'Add new order', 'woocommerce' ),
 				$post_type->cap->create_posts,
-				add_query_arg( 'action', 'new', admin_url( 'admin.php?page=wc-orders' ) ),
+				add_query_arg( array( 'page' => 'wc-orders', 'action' => 'new' ), admin_url( 'admin.php' ) ),
 				''
 			);
 		}
