d5062ec58ec9ea234afc202fa35f766642b85404
Mayisha <mayisha.alam@automattic.com>
2025-09-12 16:38:42 +0600

    create class for paypal constants

---
+++ plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-constants.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * PayPal Gateway Constants.
+ *
+ * Provides constants for PayPal payment statuses, intents, and other PayPal-related values.
+ *
+ * @version     10.3.0
+ * @package  WooCommerce\Gateways
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * WC_Gateway_Paypal_Constants Class.
+ */
+class WC_Gateway_Paypal_Constants {
+
+	/**
+	 * PayPal payment statuses.
+	 */
+	const STATUS_COMPLETED = 'completed';
+	const STATUS_APPROVED  = 'approved';
+	const STATUS_CAPTURED  = 'captured';
+
+	/**
+	 * PayPal payment intents.
+	 */
+	const INTENT_CAPTURE   = 'CAPTURE';
+	const INTENT_AUTHORIZE = 'AUTHORIZE';
+}
