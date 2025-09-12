8ee53f087e19c39c2d029c1aabfd3a1d98e695d0
Vladimir Reznichenko <kalessil@gmail.com>
2025-09-12 11:19:18 +0200

    Performance: fixed failing tests.

--- plugins/woocommerce/tests/php/src/Internal/Customers/SearchServiceTest.php
+++ plugins/woocommerce/tests/php/src/Internal/Customers/SearchServiceTest.php
@@ -7,11 +7,14 @@ namespace Automattic\WooCommerce\Tests\Internal\Customers;
 use Automattic\WooCommerce\Internal\Customers\SearchService as CustomerSearchService;
 use Automattic\WooCommerce\RestApi\UnitTests\Helpers\CustomerHelper;
 use Automattic\WooCommerce\RestApi\UnitTests\Helpers\OrderHelper;
+use Automattic\WooCommerce\RestApi\UnitTests\HPOSToggleTrait;
+use Automattic\WooCommerce\Utilities\OrderUtil;
 
 /**
  * Tests for `\Automattic\WooCommerce\Internal\Customers\SearchService` class.
  */
 class SearchServiceTest extends \WC_Unit_Test_Case {
+	use HPOSToggleTrait;
 
 	/**
 	 * Service instance.
@@ -21,11 +24,31 @@ class SearchServiceTest extends \WC_Unit_Test_Case {
 	private CustomerSearchService $service;
 
 	/**
-	 * Setup.
+	 * Original HPOS status.
+	 *
+	 * @var bool
+	 */
+	private bool $original_hpos_status;
+
+	/**
+	 * Modify the testing environment.
 	 */
 	public function setUp(): void {
 		parent::setUp();
 		$this->service = wc_get_container()->get( CustomerSearchService::class );
+
+		$this->original_hpos_status = OrderUtil::custom_orders_table_usage_is_enabled();
+		$this->setup_cot();
+		$this->toggle_cot_feature_and_usage( true );
+	}
+
+	/**
+	 * Restore the testing environment.
+	 */
+	public function tearDown(): void {
+		$this->toggle_cot_feature_and_usage( $this->original_hpos_status );
+
+		parent::tearDown();
 	}
 
 	/**
@@ -34,8 +57,8 @@ class SearchServiceTest extends \WC_Unit_Test_Case {
 	 * @return void
 	 */
 	public function test_find_user_ids_by_billing_email(): void {
-		$customer1 = CustomerHelper::create_customer( 'customer1', '', 'customer1@example.com' );
-		$customer2 = CustomerHelper::create_customer( 'customer2', '', 'customer2@example.com' );
+		$customer1 = $this->create_customer( 'customer1', '', 'customer1@example.com' );
+		$customer2 = $this->create_customer( 'customer2', '', 'customer2@example.com' );
 
 		$this->assertSame(
 			array( $customer1->get_id() ),
@@ -56,9 +79,9 @@ class SearchServiceTest extends \WC_Unit_Test_Case {
 	 * @return void
 	 */
 	public function test_find_user_ids_by_billing_email_with_have_orders_flag(): void {
-		$customer1 = CustomerHelper::create_customer( 'customer1', '', 'customer1@example.com' );
-		$customer2 = CustomerHelper::create_customer( 'customer2', '', 'customer2@example.com' );
-		$order     = OrderHelper::create_order( $customer1->get_id() );
+		$customer1 = $this->create_customer( 'customer1', '', 'customer1@example.com' );
+		$customer2 = $this->create_customer( 'customer2', '', 'customer2@example.com' );
+		$order     = $this->create_order_for( $customer1 );
 
 		$this->assertSame(
 			array( $customer1->get_id() ),
@@ -72,5 +95,33 @@ class SearchServiceTest extends \WC_Unit_Test_Case {
 		$order->delete( true );
 		$customer1->delete( true );
 		$customer2->delete( true );
+	}
+
+	/**
+	 * Wrapper around the helper, as injecting the billing in there cause some tests to fail.
+	 *
+	 * @param string $username
+	 * @param string $password
+	 * @param string $email
+	 * @return \WC_Customer
+	 */
+	private function create_customer( string $username, string $password, string $email ): \WC_Customer {
+		$customer = CustomerHelper::create_customer( $username, $password, $email );
+		$customer->set_billing_email( $email );
+		$customer->save();
+		return $customer;
+	}
+
+	/**
+	 * Wrapper around the helper, as injecting the billing in there cause some tests to fail.
+	 *
+	 * @param \WC_Customer $customer Customer object.
+	 * @return \WC_Order
+	 */
+	private function create_order_for( \WC_Customer $customer ): \WC_Order {
+		$order = OrderHelper::create_order( $customer->get_id() );
+		$order->set_billing_email( $customer->get_billing_email( 'edit' ) );
+		$order->save();
+		return $order;
 	}
 }
