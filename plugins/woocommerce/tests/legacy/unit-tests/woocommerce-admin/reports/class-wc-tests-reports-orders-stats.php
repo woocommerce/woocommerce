e6edf30684fee9aadda248dcda3b747577ed4e65
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-09-16 22:04:24 +0800

    Cherry-pick 92f39296583b008351a5ea8f5515a835ff181c63 with unresolved conflicts from #60944

--- plugins/woocommerce/tests/legacy/unit-tests/woocommerce-admin/reports/class-wc-tests-reports-orders-stats.php
+++ plugins/woocommerce/tests/legacy/unit-tests/woocommerce-admin/reports/class-wc-tests-reports-orders-stats.php
@@ -22,6 +22,12 @@ class WC_Admin_Tests_Reports_Orders_Stats extends WC_Unit_Test_Case {
 	 */
 	public static function setUpBeforeClass(): void {
 		add_filter( 'woocommerce_analytics_report_should_use_cache', '__return_false' );
+<<<<<<< HEAD
+=======
+		$db_version = strstr( WC()->version, '-', true );
+		$db_version = $db_version ? $db_version : WC()->version;
+		update_option( 'woocommerce_db_version', $db_version );
+>>>>>>> 92f392965 (Fix DB version update logic in order stats tests for release 10.2 (#60944))
 	}
 
 	/**
