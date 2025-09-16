e28c0741f9ca94672c43e00c7c024ae7fbbf4e76
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-09-16 17:06:45 +0100

    Remove unused imports and duplicate type def

--- plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/address-autocomplete/address-autocomplete.tsx
+++ plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/address-autocomplete/address-autocomplete.tsx
@@ -1,38 +1,21 @@
 /**
  * External dependencies
  */
-import {
-	ValidatedTextInput,
-	type ValidatedTextInputHandle,
-} from '@woocommerce/blocks-components';
-import type {
-	ClientAddressAutocompleteProvider,
-	ServerAddressAutocompleteProvider,
-} from '@woocommerce/types';
+import { ValidatedTextInput } from '@woocommerce/blocks-components';
+import type { ServerAddressAutocompleteProvider } from '@woocommerce/types';
 import { cartStore, checkoutStore } from '@woocommerce/block-data';
 import { useDispatch, useSelect } from '@wordpress/data';
-import { useEffect, useRef } from '@wordpress/element';
-import { FormType, getSettingWithCoercion } from '@woocommerce/settings';
+import { useEffect } from '@wordpress/element';
+import {
+	type AddressFormType,
+	getSettingWithCoercion,
+} from '@woocommerce/settings';
 
 /**
  * Internal dependencies
  */
 import { ValidatedTextInputProps } from '../../../../../../packages/components/text-input/types';
 import './style.scss';
-
-declare global {
-	interface Window {
-		wc: {
-			addressAutocomplete: {
-				providers: Record< string, ClientAddressAutocompleteProvider >;
-				activeProvider: {
-					shipping: ClientAddressAutocompleteProvider | null;
-					billing: ClientAddressAutocompleteProvider | null;
-				};
-			};
-		};
-	}
-}
 
 /**
  * Address Autocomplete component.
@@ -46,7 +29,7 @@ export const AddressAutocomplete = ( {
 	addressType,
 	id,
 	...props
-}: { addressType: FormType; id: string } & ValidatedTextInputProps ) => {
+}: { addressType: AddressFormType; id: string } & ValidatedTextInputProps ) => {
 	const serverProviders = getSettingWithCoercion<
 		ServerAddressAutocompleteProvider[]
 	>(
@@ -108,9 +91,8 @@ export const AddressAutocomplete = ( {
 				);
 
 				// Set globally as this is going to be the source of truth where the actual provider objects are stored.
-				window.wc.addressAutocomplete.activeProvider[
-					addressType as 'shipping' | 'billing'
-				] = provider;
+				window.wc.addressAutocomplete.activeProvider[ addressType ] =
+					provider;
 				return;
 			}
 		}
@@ -118,9 +100,7 @@ export const AddressAutocomplete = ( {
 		setActiveAddressAutocompleteProvider( '', addressType );
 		// Set globally as this is going to be the source of truth where the actual provider objects are stored.
 		if ( window?.wc?.addressAutocomplete?.activeProvider ) {
-			window.wc.addressAutocomplete.activeProvider[
-				addressType as 'shipping' | 'billing'
-			] = null;
+			window.wc.addressAutocomplete.activeProvider[ addressType ] = null;
 		}
 	}, [
 		country,
