49aae6d659c1baec7c10b7feedc4523888840b96
Ayush Pahwa <ayush.pahwa@automattic.com>
2025-09-17 15:07:46 +0700

    update: add schema w/ view context for shipping zones

--- plugins/woocommerce/src/RestApi/Routes/V4/ShippingZones/ShippingZoneSchema.php
+++ plugins/woocommerce/src/RestApi/Routes/V4/ShippingZones/ShippingZoneSchema.php
@@ -35,13 +35,99 @@ class ShippingZoneSchema extends AbstractSchema {
 	 */
 	public static function get_item_schema_properties(): array {
 		$schema = array(
-			'id'               => array(
-				'description' => __( 'Unique identifier for the resource.', 'woocommerce' ),
+			'id'        => array(
+				'description' => __( 'Unique identifier for the shipping zone.', 'woocommerce' ),
 				'type'        => 'integer',
 				'context'     => self::VIEW_EDIT_EMBED_CONTEXT,
 				'readonly'    => true,
 			),
-			/* @todo: add the rest of the properties */
+			'name'      => array(
+				'description' => __( 'Shipping zone name.', 'woocommerce' ),
+				'type'        => 'string',
+				'context'     => self::VIEW_EDIT_EMBED_CONTEXT,
+				'readonly'    => true,
+			),
+			'order'     => array(
+				'description' => __( 'Shipping zone order.', 'woocommerce' ),
+				'type'        => 'integer',
+				'context'     => self::VIEW_EDIT_EMBED_CONTEXT,
+				'readonly'    => true,
+			),
+			'locations' => array(
+				'description' => __( 'Zone locations. Returns formatted string for view context, detailed array for edit context.', 'woocommerce' ),
+				'context'     => self::VIEW_EDIT_EMBED_CONTEXT,
+				'readonly'    => true,
+				'oneOf'       => array(
+					array(
+						'type'        => 'string',
+						'description' => __( 'Formatted location string for display (view context).', 'woocommerce' ),
+					),
+					array(
+						'type'        => 'array',
+						'description' => __( 'Array of location objects with detailed information (edit context).', 'woocommerce' ),
+						'items'       => array(
+							'type'       => 'object',
+							'properties' => array(
+								'code'         => array(
+									'description' => __( 'Location code (e.g., "US", "US:CA").', 'woocommerce' ),
+									'type'        => 'string',
+								),
+								'type'         => array(
+									'description' => __( 'Location type.', 'woocommerce' ),
+									'type'        => 'string',
+									'enum'        => array( 'continent', 'country', 'state', 'postcode' ),
+								),
+								'name'         => array(
+									'description' => __( 'Human-readable location name.', 'woocommerce' ),
+									'type'        => 'string',
+								),
+								'country_name' => array(
+									'description' => __( 'Country name (for state locations only).', 'woocommerce' ),
+									'type'        => 'string',
+								),
+							),
+						),
+					),
+				),
+			),
+			'methods'   => array(
+				'description' => __( 'Shipping methods for this zone.', 'woocommerce' ),
+				'type'        => 'array',
+				'context'     => self::VIEW_EDIT_EMBED_CONTEXT,
+				'readonly'    => true,
+				'items'       => array(
+					'type'       => 'object',
+					'properties' => array(
+						'instance_id'      => array(
+							'description' => __( 'Shipping method instance ID.', 'woocommerce' ),
+							'type'        => 'integer',
+						),
+						'method_id'        => array(
+							'description' => __( 'Shipping method ID (edit context only).', 'woocommerce' ),
+							'type'        => 'string',
+							'context'     => self::EDIT_CONTEXT,
+						),
+						'title'            => array(
+							'description' => __( 'Shipping method title.', 'woocommerce' ),
+							'type'        => 'string',
+						),
+						'enabled'          => array(
+							'description' => __( 'Whether the shipping method is enabled.', 'woocommerce' ),
+							'type'        => 'boolean',
+						),
+						'rate_description' => array(
+							'description' => __( 'Formatted rate description for display (view context only).', 'woocommerce' ),
+							'type'        => 'string',
+							'context'     => self::VIEW_CONTEXT,
+						),
+						'settings'         => array(
+							'description' => __( 'Shipping method settings (edit context only).', 'woocommerce' ),
+							'type'        => 'object',
+							'context'     => self::EDIT_CONTEXT,
+						),
+					),
+				),
+			),
 		);
 
 		return $schema;
