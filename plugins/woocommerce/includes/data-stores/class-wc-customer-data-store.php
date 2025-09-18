bd6a7c686ed8c2a361eefcb4445f3f84d351dd86
Vladimir Reznichenko <kalessil@gmail.com>
2025-09-18 12:57:56 +0200

    Performance: cleanup.

--- plugins/woocommerce/includes/data-stores/class-wc-customer-data-store.php
+++ plugins/woocommerce/includes/data-stores/class-wc-customer-data-store.php
@@ -600,7 +600,7 @@ class WC_Customer_Data_Store extends WC_Data_Store_WP implements WC_Customer_Dat
 	 * @return int[]
 	 */
 	public function get_user_ids_for_billing_email( $emails ) {
-		wc_deprecated_function( __CLASS__ . '::' . __FUNCTION__, '10.3.0' );
+		wc_deprecated_function( __METHOD__, '10.3.0' );
 		return wc_get_container()->get( CustomerSearchService::class )->find_user_ids_by_billing_email( $emails, false );
 	}
 }
