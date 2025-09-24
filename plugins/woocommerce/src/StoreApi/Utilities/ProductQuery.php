7d284ec92ae03ea92b98dbbe897565dce31ec4b1
Alba Rincón <alba.rincon@automattic.com>
2025-09-24 09:15:15 +0200

    Check if _prime_post_caches is callable

--- plugins/woocommerce/src/StoreApi/Utilities/ProductQuery.php
+++ plugins/woocommerce/src/StoreApi/Utilities/ProductQuery.php
@@ -323,7 +323,9 @@ class ProductQuery implements QueryClausesGenerator {
 	public function get_objects( $request ) {
 		$results = $this->get_results( $request );
 
-		_prime_post_caches( $results['results'] );
+		if ( is_callable( '_prime_post_caches' ) ) {
+			_prime_post_caches( $results['results'] );
+		}
 
 		return array(
 			'objects' => array_map( 'wc_get_product', $results['results'] ),
