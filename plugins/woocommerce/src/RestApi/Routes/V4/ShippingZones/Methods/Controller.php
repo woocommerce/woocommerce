7ac4c091ddf1328829513bb115b324b787bcf226
Ayush Pahwa <ayush.pahwa@automattic.com>
2025-09-24 15:24:59 +0700

    create: make files for hosting shipping methods apis

---
+++ plugins/woocommerce/src/RestApi/Routes/V4/ShippingZones/Methods/Controller.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Shipping Zone Methods Controller.
+ *
+ * @package WooCommerce\RestApi
+ */
+
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\RestApi\Routes\V4\ShippingZones\Methods;
+
+use Automattic\WooCommerce\RestApi\Routes\V4\AbstractController;
+
+/**
+ * Shipping Zone Methods Controller class.
+ */
+class Controller extends AbstractController {
+
+	/**
+	 * Route base.
+	 *
+	 * @var string
+	 */
+	protected $rest_base = 'shipping-zones/methods';
+
+	/**
+	 * Register the routes for shipping zone methods.
+	 */
+	public function register_routes() {
+		// TODO: Implement route registration
+	}
+}
