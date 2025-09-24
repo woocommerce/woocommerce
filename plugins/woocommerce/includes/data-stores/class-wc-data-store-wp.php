12b1d07814efab53938ca5029771e25dd32c55a9
Mike Jolley <mike.jolley@me.com>
2025-09-24 13:50:47 +0100

    move default order by

--- plugins/woocommerce/includes/data-stores/class-wc-data-store-wp.php
+++ plugins/woocommerce/includes/data-stores/class-wc-data-store-wp.php
@@ -276,8 +276,6 @@ class WC_Data_Store_WP {
 
 		$skipped_values = array( '', array(), null );
 		$wp_query_args  = array(
-			'orderby'    => 'id',
-			'order'      => 'desc',
 			'errors'     => array(),
 			'meta_query' => array(), // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
 		);
