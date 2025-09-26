5f692125b3ed407e4b4ca7ae6de600906fa3feaf
Anne Mirasol <anne.mirasol@automattic.com>
2025-09-26 15:16:16 -0500

    Use rest_url() instead of hardcoding shipping event callback URLs

--- plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
+++ plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
@@ -421,7 +421,7 @@ class WC_Gateway_Paypal_Request {
 						'locale'                       => str_replace( '_', '-', get_locale() ),
 						'order_update_callback_config' => array(
 							'callback_events' => array( 'SHIPPING_ADDRESS', 'SHIPPING_OPTIONS' ),
-							'callback_url'    => get_site_url( null, '/wp-json/wc/v3/paypal-standard/update-shipping' ),
+							'callback_url'    => esc_url_raw( rest_url( 'wc/v3/paypal-standard/update-shipping' ) ),
 						),
 						'app_switch_preference'        => array(
 							'launch_paypal_app' => true,
