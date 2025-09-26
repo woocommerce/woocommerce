ed4e248f60d74342ab3d9ceedbda6ee6d395a196
Mayisha <mayisha.alam@automattic.com>
2025-09-26 14:25:41 +0600

    i18n generic error

--- plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
+++ plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
@@ -791,11 +791,12 @@ class WC_Gateway_Paypal extends WC_Payment_Gateway {
 			'wc-paypal-frontend',
 			'paypal_standard',
 			array(
-				'gateway_id'           => $this->id,
-				'isProductPage'        => is_product(),
-				'wc_store_api_nonce'   => wp_create_nonce( 'wc_store_api' ),
-				'create_order_nonce'   => wp_create_nonce( 'wc_gateway_paypal_standard_create_order' ),
-				'cancel_payment_nonce' => wp_create_nonce( 'wc_gateway_paypal_standard_cancel_payment' ),
+				'gateway_id'            => $this->id,
+				'isProductPage'         => is_product(),
+				'wc_store_api_nonce'    => wp_create_nonce( 'wc_store_api' ),
+				'create_order_nonce'    => wp_create_nonce( 'wc_gateway_paypal_standard_create_order' ),
+				'cancel_payment_nonce'  => wp_create_nonce( 'wc_gateway_paypal_standard_cancel_payment' ),
+				'generic_error_message' =>  __( 'An unknown error occurred', 'woocommerce' ),
 			)
 		);
 
