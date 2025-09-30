61d537ce64e62500426a163f0080a0b0fffa68ec
Nestor Soriano <konamiman@konamiman.com>
2025-09-30 16:22:16 +0200

    Repurpose the logs deletion scheduler as a standard batch processor.

---
+++ plugins/woocommerce/src/Internal/Logging/OrderLogsDeletionProcessor.php
@@ -0,0 +1,251 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Internal\Logging;
+
+use Automattic\WooCommerce\Internal\BatchProcessing\BatchProcessorInterface;
+use Automattic\WooCommerce\Internal\DataStores\Orders\CustomOrdersTableController;
+use Automattic\WooCommerce\Utilities\StringUtil;
+
+/**
+ * Batch processor for deleting log entries of completed orders.
+ * It only works when either HPOS is enabled or the orders data store is the old CPT-based one,
+ * because otherwise the ability to query orders by meta key is not guaranteed.
+ */
+class OrderLogsDeletionProcessor implements BatchProcessorInterface {
+
+	/**
+	 * Constant representing the default size of the batches to process.
+	 *
+	 * Note that meta entry ids taken from batch items will be concatenated to form a
+	 * "delete ... where id in (id, id...)" SQL query, so this value can't be very big.
+	 */
+	public const DEFAULT_BATCH_SIZE = 1000;
+
+	/**
+	 * True if HPOS is enabled.
+	 *
+	 * @var bool
+	 */
+	private bool $hpos_in_use = false;
+
+	/**
+	 * True if HPOS is disabled and the orders data store in use is the old CPT one.
+	 *
+	 * @var bool
+	 */
+	private bool $cpt_in_use = false;
+
+	/**
+	 * The logger to use to delete log entries.
+	 *
+	 * @var \WC_Logger_Interface
+	 */
+	private \WC_Logger_Interface $logger;
+
+	/**
+	 * Initialize the instance.
+	 * This is invoked by the dependency injection container.
+	 *
+	 * @internal
+	 *
+	 * @param CustomOrdersTableController $hpos_controller The instance of CustomOrdersTableController to use.
+	 */
+	final public function init( CustomOrdersTableController $hpos_controller ) {
+		$this->hpos_in_use = $hpos_controller->custom_orders_table_usage_is_enabled();
+		if ( ! $this->hpos_in_use ) {
+			$this->cpt_in_use = \WC_Order_Data_Store_CPT::class === \WC_Data_Store::load( 'order' )->get_current_class_name();
+		}
+
+		$this->logger = wc_get_logger();
+	}
+
+	/**
+	 * Get the name of the processor.
+	 *
+	 * @return string
+	 */
+	public function get_name(): string {
+		return 'Order logs deletion process';
+	}
+
+	/**
+	 * Get a description of the processor.
+	 *
+	 * @return string
+	 */
+	public function get_description(): string {
+		return 'Deletes debug logs of completed orders.';
+	}
+
+	/**
+	 * Get the default batch size for this processor.
+	 *
+	 * @return int
+	 */
+	public function get_default_batch_size(): int {
+		return self::DEFAULT_BATCH_SIZE;
+	}
+
+	/**
+	 * Get the total count of entries pending processing.
+	 *
+	 * @return int
+	 */
+	public function get_total_pending_count(): int {
+		if ( $this->hpos_in_use ) {
+			return $this->get_total_pending_count_hpos();
+		} elseif ( $this->cpt_in_use ) {
+			return $this->get_total_pending_count_cpt();
+		} else {
+			$this->throw_doing_it_wrong( StringUtil::class_name_without_namespace( __CLASS__ ) . '::' . __FUNCTION__ );
+			return 0;
+		}
+	}
+
+	/**
+	 * Get the total count of entries pending processing, HPOS version.
+	 *
+	 * @return int
+	 */
+	private function get_total_pending_count_hpos(): int {
+		global $wpdb;
+
+		return $wpdb->get_var(
+			$wpdb->prepare(
+				"SELECT COUNT(*)
+                 FROM {$wpdb->prefix}wc_orders_meta
+                 WHERE meta_key = %s",
+				'_debug_log_source_pending_deletion'
+			)
+		);
+	}
+
+	/**
+	 * Get the total count of entries pending processing, CPT datastore version.
+	 *
+	 * @return int
+	 */
+	private function get_total_pending_count_cpt(): int {
+		global $wpdb;
+
+		return (int)$wpdb->get_var(
+			$wpdb->prepare(
+				"SELECT COUNT(*)
+                 FROM {$wpdb->postmeta} pm
+                 INNER JOIN {$wpdb->posts} p ON pm.post_id = p.ID
+                 WHERE pm.meta_key = %s
+                 AND p.post_type = %s",
+				'_debug_log_source_pending_deletion',
+				'shop_order'
+			)
+		);
+	}
+
+	/**
+	 * Get the next batch of items to process.
+	 * An item will be an associative array of 'meta_key' and 'meta_value'.
+	 *
+	 * @param int $size Maximum size of the batch to return.
+	 * @return array
+	 */
+	public function get_next_batch_to_process( int $size ): array {
+		if ( $this->hpos_in_use ) {
+			return $this->get_next_batch_to_process_hpos( $size );
+		} elseif ( $this->cpt_in_use ) {
+			return $this->get_next_batch_to_process_cpt( $size );
+		} else {
+			$this->throw_doing_it_wrong( StringUtil::class_name_without_namespace( __CLASS__ ) . '::' . __FUNCTION__ );
+			return array();
+		}
+	}
+
+	/**
+	 * Get the next batch of items to process, HPOS version.
+	 *
+	 * @param int $size Maximum size of the batch to return.
+	 * @return array
+	 */
+	private function get_next_batch_to_process_hpos( int $size ): array {
+		global $wpdb;
+
+		return $wpdb->get_results(
+			$wpdb->prepare(
+				"SELECT id as meta_id, meta_value
+                 FROM {$wpdb->prefix}wc_orders_meta
+                 WHERE meta_key = %s
+                 ORDER BY id
+                 LIMIT %d",
+				'_debug_log_source_pending_deletion',
+				$size
+			),
+			ARRAY_A
+		);
+	}
+
+	/**
+	 * Get the next batch of items to process, CPT datastore version.
+	 *
+	 * @param int $size Maximum size of the batch to return.
+	 * @return array
+	 */
+	private function get_next_batch_to_process_cpt( int $size ): array {
+		global $wpdb;
+
+		return $wpdb->get_results(
+			$wpdb->prepare(
+				"SELECT pm.meta_id, pm.meta_value
+                 FROM {$wpdb->postmeta} pm
+                 INNER JOIN {$wpdb->posts} p ON pm.post_id = p.ID
+                 WHERE pm.meta_key = %s
+                 AND p.post_type = 'shop_order'
+                 ORDER BY pm.meta_id
+                 LIMIT %d",
+				'_debug_log_source_pending_deletion',
+				$size
+			),
+			ARRAY_A
+		);
+	}
+
+	/**
+	 * Process a batch of items.
+	 * Items are expected to be in the format returned by get_next_batch_to_process.
+	 *
+	 * @param array $batch Batch of items to process.
+	 */
+	public function process_batch( array $batch ): void {
+		if ( ! $this->hpos_in_use && ! $this->cpt_in_use ) {
+			$this->throw_doing_it_wrong( StringUtil::class_name_without_namespace( __CLASS__ ) . '::' . __FUNCTION__ );
+			return;
+		}
+
+		foreach ( $batch as $item ) {
+			$this->logger->clear( $item['meta_value'] );
+		}
+
+		global $wpdb;
+		$table_name = $this->hpos_in_use ? "{$wpdb->prefix}wc_orders_meta" : $wpdb->postmeta;
+		$id_column_name = $this->hpos_in_use ? 'id' : 'meta_id';
+
+		$meta_ids     = array_map( fn( $item ) => $item['meta_id'], $batch );
+		$placeholders = implode( ',', $meta_ids );
+
+		// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+		$wpdb->query( "DELETE FROM {$table_name} WHERE {$id_column_name} IN ({$placeholders})" );
+	}
+
+	/**
+	 * Throw a "doing it wrong" error.
+	 *
+	 * @param string $function_name Class and function name to include in the error.
+	 */
+	private function throw_doing_it_wrong( string $function_name ) {
+		wc_doing_it_wrong(
+			$function_name,
+			"This processor shouldn't be enqueued when the orders data store in use is neither the HPOS one nor the CPT one. Just delete the order debug logs directly.",
+			'10.3.0'
+		);
+	}
+}
