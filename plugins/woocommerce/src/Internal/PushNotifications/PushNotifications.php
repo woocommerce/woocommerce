aa9c53e2cec7098782731b1b3f80239b77b757eb
Hannah Tinkler <hannah.tinkler@gmail.com>
2025-09-30 14:24:50 +0100

    Adds core push notifications class and test, adds ability to check Jetpack connection.

---
+++ plugins/woocommerce/src/Internal/PushNotifications/PushNotifications.php
@@ -0,0 +1,54 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Automattic\WooCommerce\Internal\PushNotifications;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+use Automattic\Jetpack\Connection\Manager as Jetpack_Connection_Manager;
+use Automattic\WooCommerce\Proxies\LegacyProxy;
+
+/**
+ * WC Push Notifications
+ *
+ * Class for setting up the WooCommerce-driven push notifications.
+ */
+class PushNotifications {
+	/**
+	 * Loads the push notifications class.
+	 *
+	 * @since X.X.X
+	 * @return void
+	 */
+	public function register() {
+		if ( ! $this->should_be_enabled() ) {
+			return;
+		}
+
+		/**
+		 * Load functionality.
+		 */
+	}
+
+	/**
+	 * Determines if local push notification functionality should be enabled. It
+	 * should be enabled if:
+	 * - Jetpack is connected
+	 * - WooCommerce.com account is connected
+	 *
+	 * @since X.X.X
+	 * @return bool
+	 */
+	public function should_be_enabled(): bool {
+		$proxy = wc_get_container()->get( LegacyProxy::class );
+
+		if ( ! $proxy->get_instance_of( Jetpack_Connection_Manager::class )->is_connected() ) {
+			return false;
+		}
+
+		return true;
+	}
+}
