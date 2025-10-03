4212f88e0ada51a0a18ba84118c43482e966e543
Cvetan Cvetanov <cvetan.cvetanov@automattic.com>
2025-10-03 13:03:05 +0300

    Update docblock

--- plugins/woocommerce/src/RestApi/Routes/V4/OfflinePaymentMethods/Controller.php
+++ plugins/woocommerce/src/RestApi/Routes/V4/OfflinePaymentMethods/Controller.php
@@ -2,7 +2,7 @@
 /**
  * REST API Offline Payment Methods Controller
  *
- * Handles requests to the /payments/offline-methods endpoint.
+ * Handles requests to the /settings/payments/offline-methods endpoint.
  *
  * @package WooCommerce\RestApi
  */
