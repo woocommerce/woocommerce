7e93738142c666f14788b778b2cec2f3f2d923dd
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-10-06 09:05:36 +0200

    Disable CYS E2E tests with GB nightly

--- plugins/woocommerce/tests/e2e-pw/envs/gutenberg-nightly/playwright.config.js
+++ plugins/woocommerce/tests/e2e-pw/envs/gutenberg-nightly/playwright.config.js
@@ -1,3 +1,19 @@
-const config = require( '../gutenberg-stable/playwright.config.js' );
+let config = require( '../gutenberg-stable/playwright.config.js' );
+const { tags } = require( '../../fixtures/fixtures' );
+
+config = {
+	...config,
+	projects: [
+		...config.projects,
+		{
+			name: 'Gutenberg',
+			grep: new RegExp( tags.GUTENBERG ),
+			// Customise Your Store tests are failing with Gutenberg nightly, since 21.7RC.
+			// We're disabling them as we're considering sunsetting CYS in 10.3.
+			testIgnore: [ '**/customize-store/**' ],
+			dependencies: [ 'site setup' ],
+		},
+	],
+};
 
 module.exports = config;
