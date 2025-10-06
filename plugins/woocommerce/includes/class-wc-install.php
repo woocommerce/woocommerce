4a2941ed3f496f854cd82304d3693ff517f0c243
Michael Pretty <prettyboymp@users.noreply.github.com>
2025-10-06 10:34:38 -0400

    moving to version 10.4.0 as we missed the 10.3.0 release window.

--- plugins/woocommerce/includes/class-wc-install.php
+++ plugins/woocommerce/includes/class-wc-install.php
@@ -303,10 +303,8 @@ class WC_Install {
 		'10.2.0' => array(
 			'wc_update_1020_add_old_refunded_order_items_to_product_lookup_table',
 		),
-		'10.3.0' => array(
-			'wc_update_1030_add_comments_date_type_index',
-		),
 		'10.4.0' => array(
+			'wc_update_1030_add_comments_date_type_index',
 			'wc_update_1040_add_idx_date_paid_status_parent',
 		),
 	);
