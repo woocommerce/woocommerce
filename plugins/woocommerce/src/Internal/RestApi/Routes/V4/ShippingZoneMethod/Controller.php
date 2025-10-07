dbd14dc2483e5d4adde361f5e8cbdd516355f3e9
Ayush Pahwa <ayush.pahwa@automattic.com>
2025-10-08 00:31:41 +0700

    update: ensure the method is deleted if the create fails

--- plugins/woocommerce/src/Internal/RestApi/Routes/V4/ShippingZoneMethod/Controller.php
+++ plugins/woocommerce/src/Internal/RestApi/Routes/V4/ShippingZoneMethod/Controller.php
@@ -151,6 +151,9 @@ class Controller extends AbstractController {
 		// Update method settings, enabled status, and order.
 		$result = $method->update_from_api_request( $zone, $instance_id, $request->get_params() );
 		if ( is_wp_error( $result ) ) {
+			// Delete the method instance to rollback the creation.
+			// This ensures a failed POST would not leave an orphaned method
+			$zone->delete_shipping_method( $instance_id );
 			return $result;
 		}
 
