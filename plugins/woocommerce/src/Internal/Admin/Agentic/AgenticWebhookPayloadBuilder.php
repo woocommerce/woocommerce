d25566d49f1917e0fbd7156c293987b953d734a7
Eric Binnion <ericbinnion@gmail.com>
2025-10-08 17:28:36 -0500

    Add @see to status map filter

--- plugins/woocommerce/src/Internal/Admin/Agentic/AgenticWebhookPayloadBuilder.php
+++ plugins/woocommerce/src/Internal/Admin/Agentic/AgenticWebhookPayloadBuilder.php
@@ -116,6 +116,8 @@ class AgenticWebhookPayloadBuilder {
 		 * Allows extensions to map custom WooCommerce order statuses to ACP order statuses.
 		 * The mapped status must be one of: created, manual_review, confirmed, canceled, shipped, fulfilled.
 		 *
+		 * @see Automattic\WooCommerce\StoreApi\Routes\V1\Agentic\Enums\Specs\OrderStatus
+		 *
 		 * @since 10.3.0
 		 *
 		 * @param array  $status_map Associative array of WooCommerce status => ACP status.
