47283799a63f98264c45be7f6dfded53c9c53faa
Michal Iwanow <4765119+mcliwanow@users.noreply.github.com>
2025-10-08 14:07:51 +0200

    In-App marketplace: clearer user information in the header
    In Marketplace header, we will show connected account more clearly - with full email on tablet+ views. This change doesn't affect non-marketplace pages

--- plugins/woocommerce/client/admin/client/marketplace/components/header-account/header-account.tsx
+++ plugins/woocommerce/client/admin/client/marketplace/components/header-account/header-account.tsx
@@ -8,7 +8,7 @@ import {
 	MenuGroup,
 	MenuItem as OriginalMenuItem,
 } from '@wordpress/components';
-import { Icon, commentAuthorAvatar, external, linkOff } from '@wordpress/icons';
+import { Icon, commentAuthorAvatar, external, linkOff, chevronDown } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 import { recordEvent } from '@woocommerce/tracks';
 
@@ -64,6 +64,28 @@ export default function HeaderAccount( {
 		);
 	};
 
+	const dropdownTrigger = () => {
+		const isInApp = 'wc-addons' === page;
+		if ( ! isInApp ) {
+			return avatar();
+		}
+
+		return (
+			<span className="woocommerce-marketplace__header-account-trigger">
+				{ avatar() }
+				<span
+					className="woocommerce-marketplace__header-account-trigger__email"
+					title={ isConnected ? userEmail : __( 'Connect to WooCommerce.com', 'woocommerce' ) }
+				>
+					{ isConnected
+						? userEmail
+						: __( 'Connect to WooCommerce.com', 'woocommerce' ) }
+				</span>
+				<Icon icon={ chevronDown } size={ 24 } className="woocommerce-marketplace__header-account-trigger__expand-icon" />
+			</span>
+		);
+	};
+
 	const connectionStatusText = isConnected
 		? __( 'Connected to WooCommerce.com', 'woocommerce' )
 		: __( 'Connect to WooCommerce.com', 'woocommerce' );
@@ -107,7 +129,7 @@ export default function HeaderAccount( {
 		<>
 			<DropdownMenu
 				className="woocommerce-layout__activity-panel-tab woocommerce-marketplace__user-menu"
-				icon={ avatar() }
+				icon={ dropdownTrigger() }
 				label={ __( 'User options', 'woocommerce' ) }
 				toggleProps={ {
 					className: 'woocommerce-layout__activity-panel-tab',
