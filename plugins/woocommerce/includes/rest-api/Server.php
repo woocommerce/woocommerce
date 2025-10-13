d70880ff6cfa62779cc2aec055320cf70c3940e2
Daniel Mallory <daniel.mallory@automattic.com>
2025-10-13 14:26:05 +0100

    Lint fixes.

--- plugins/woocommerce/includes/rest-api/Server.php
+++ plugins/woocommerce/includes/rest-api/Server.php
@@ -219,18 +219,18 @@ class Server {
 	 */
 	protected function get_v4_controllers() {
 		return array(
-			'ping'                    => 'WC_REST_Ping_V4_Controller',
-			'fulfillments'            => 'WC_REST_Fulfillments_V4_Controller',
-			'products'                => 'WC_REST_Products_V4_Controller',
-			'order-notes'             => OrderNotesController::class,
-			'shipping-zones'          => ShippingZonesController::class,
-			'shipping-zone-method'    => ShippingZoneMethodController::class,
-			'orders'                  => OrdersController::class,
-			'offline-payment-methods' => OfflinePaymentMethodsController::class,
-			'settings-general'        => GeneralSettingsController::class,
-			'settings-email'          => 'WC_REST_Email_Settings_V4_Controller',
-			'settings-products'       => ProductsController::class,
-      'settings-payment-gateways' => PaymentGatewaysController::class,
+			'ping'                      => 'WC_REST_Ping_V4_Controller',
+			'fulfillments'              => 'WC_REST_Fulfillments_V4_Controller',
+			'products'                  => 'WC_REST_Products_V4_Controller',
+			'order-notes'               => OrderNotesController::class,
+			'shipping-zones'            => ShippingZonesController::class,
+			'shipping-zone-method'      => ShippingZoneMethodController::class,
+			'orders'                    => OrdersController::class,
+			'offline-payment-methods'   => OfflinePaymentMethodsController::class,
+			'settings-general'          => GeneralSettingsController::class,
+			'settings-email'            => 'WC_REST_Email_Settings_V4_Controller',
+			'settings-products'         => ProductsController::class,
+			'settings-payment-gateways' => PaymentGatewaysController::class,
 			// This is a wrapper that redirects V4 settings requests to the V3 settings controller.
 			'settings'                  => 'WC_REST_Settings_V4_Controller',
 		);
