1624af96420289d6e9e75d7517c25a4907f31b4c
Wesley Rosa <wesleyjrosa@gmail.com>
2025-10-17 12:50:33 -0300

    Refactoring shipping controller test class

--- plugins/woocommerce/tests/php/includes/rest-api/Controllers/Version3/class-wc-rest-paypal-standard-controller-test.php
+++ plugins/woocommerce/tests/php/includes/rest-api/Controllers/Version3/class-wc-rest-paypal-standard-controller-test.php
@@ -23,11 +23,22 @@ class WC_REST_Paypal_Standard_Controller_Test extends WC_REST_Unit_Test_Case {
 	}
 
 	/**
+	 * Tear down the test environment.
+	 */
+	public function tearDown(): void {
+		parent::tearDown();
+
+		wp_set_current_user( 0 );
+	}
+
+	/**
 	 * Tests for `process_shipping_callback` method.
 	 *
 	 * @param string $paypal_order_id PayPal order ID.
 	 * @param array  $purchase_units Purchase units from PayPal order.
 	 * @param array  $shipping_option Shipping option selected by customer.
+	 * @param array  $shipping_methods Available shipping methods.
+	 * @param array  $shipping_rates Available shipping rates.
 	 * @param array  $expected_response Expected response from the endpoint.
 	 * @param int    $expected_status Expected HTTP status code.
 	 * @return void
@@ -38,9 +49,21 @@ class WC_REST_Paypal_Standard_Controller_Test extends WC_REST_Unit_Test_Case {
 		string $paypal_order_id,
 		array $purchase_units,
 		array $shipping_option,
+		array $shipping_methods,
+		array $shipping_rates,
 		array $expected_response,
 		int $expected_status
 	): void {
+		$shipping_methods_hook = function () use ( $shipping_methods ) {
+			return $shipping_methods;
+		};
+		add_action( 'woocommerce_shipping_methods', $shipping_methods_hook );
+
+		$package_rates_hook = function ( $rates, $package ) use ( $shipping_rates ) {
+			return $shipping_rates;
+		};
+		add_filter( 'woocommerce_package_rates', $package_rates_hook, 10, 2 );
+
 		$request = new WP_REST_Request( 'POST', '/wc/v3/paypal-standard/update-shipping' );
 		$request->set_body_params(
 			array(
@@ -57,6 +80,19 @@ class WC_REST_Paypal_Standard_Controller_Test extends WC_REST_Unit_Test_Case {
 		);
 		$response = $this->server->dispatch( $request );
 
+		// Clean up.
+		if ( ! empty( $purchase_units['custom_id'] ) ) {
+			$custom_id = json_decode( $purchase_units['custom_id'], true );
+			if ( isset( $custom_id['order_id'] ) ) {
+				$order = wc_get_order( $custom_id['order_id'] );
+				if ( $order ) {
+					$order->delete( true );
+				}
+			}
+		}
+		remove_filter( 'woocommerce_package_rates', $package_rates_hook, 10 );
+		remove_action( 'woocommerce_shipping_methods', $shipping_methods_hook );
+
 		$this->assertEquals( $expected_status, $response->get_status() );
 		$this->assertEquals( $expected_response, $response->get_data() );
 	}
@@ -67,6 +103,32 @@ class WC_REST_Paypal_Standard_Controller_Test extends WC_REST_Unit_Test_Case {
 	 * @return array
 	 */
 	public function provide_test_process_shipping_callback(): array {
+		$shipping_method = new class() extends WC_Shipping_Method {
+			/**
+			 * Custom pickup shipping method.
+			 * @var string
+			 */
+			public $id = 'flat_rate';
+
+			/**
+			 * Array of features this rate supports.
+			 * @var array
+			 */
+			public $supports = array( 'local-pickup' );
+
+			/**
+			 * Get rates for package.
+			 * @param array $package package.
+			 *
+			 * @return WC_Shipping_Rate[]
+			 */
+			public function get_rates_for_package( $package ) {
+				return array( 'flat_rate:1' => new WC_Shipping_Rate( 'flat_rate:1', 'Flat Rate 1', '10.00', array(), 'flat_rate' ) );
+			}
+		};
+
+		$flat_rate = new WC_Shipping_Rate( 'flat_rate:1', 'Flat Rate 1', '10.00', array(), 'flat_rate' );
+
 		$order = WC_Helper_Order::create_order();
 		$order->save();
 		$order->update_meta_data( '_paypal_order_id', '94N960803Z669244Y' );
@@ -82,6 +144,8 @@ class WC_REST_Paypal_Standard_Controller_Test extends WC_REST_Unit_Test_Case {
 				'PayPal order ID'   => '',
 				'purchase units'    => array(),
 				'shipping option'   => array(),
+				'shipping methods'  => array( $shipping_method ),
+				'shipping rates'    => array( 'flat_rate:1' => $flat_rate ),
 				'expected response' => array(
 					'name'    => 'UNPROCESSABLE_ENTITY',
 					'details' => array(
@@ -96,6 +160,8 @@ class WC_REST_Paypal_Standard_Controller_Test extends WC_REST_Unit_Test_Case {
 					'custom_id' => 'non_existent_order',
 				),
 				'shipping option'   => array(),
+				'shipping methods'  => array( $shipping_method ),
+				'shipping rates'    => array( 'flat_rate:1' => $flat_rate ),
 				'expected response' => array(
 					'name'    => 'UNPROCESSABLE_ENTITY',
 					'details' => array(
@@ -115,6 +181,8 @@ class WC_REST_Paypal_Standard_Controller_Test extends WC_REST_Unit_Test_Case {
 					),
 				),
 				'shipping option'   => array(),
+				'shipping methods'  => array( $shipping_method ),
+				'shipping rates'    => array( 'flat_rate:1' => $flat_rate ),
 				'expected response' => array(
 					'name'    => 'UNPROCESSABLE_ENTITY',
 					'details' => array(
@@ -136,6 +204,8 @@ class WC_REST_Paypal_Standard_Controller_Test extends WC_REST_Unit_Test_Case {
 					),
 				),
 				'shipping option'   => array(),
+				'shipping methods'  => array(),
+				'shipping rates'    => array(),
 				'expected response' => array(
 					'name'    => 'UNPROCESSABLE_ENTITY',
 					'details' => array(
@@ -159,29 +229,47 @@ class WC_REST_Paypal_Standard_Controller_Test extends WC_REST_Unit_Test_Case {
 				'shipping option'   => array(
 					'id' => 'flat_rate:1',
 				),
+				'shipping methods'  => array( $shipping_method ),
+				'shipping rates'    => array( 'flat_rate:1' => $flat_rate ),
 				'expected response' => array(
-					'shipping_options' => array(
+					'id'                 => '94N960803Z669244Y',
+					'purchase_units'     => array(
 						array(
-							'id'          => 'flat_rate:1',
-							'label'       => 'Flat rate',
-							'type'        => 'SHIPPING',
-							'amount'      => array(
-								'currency_code' => 'USD',
-								'value'         => '10.00',
+							'shipping_options' => array(
+								array(
+									'id'          => 'flat_rate:1',
+									'label'       => 'Flat Rate 1',
+									'type'        => 'SHIPPING',
+									'amount'      => array(
+										'currency_code' => 'USD',
+										'value'         => '10.00',
+									),
+									'selected'    => true,
+								),
 							),
-							'default'     => true,
-							'description' => '',
-						),
-						array(
-							'id'          => 'free_shipping:2',
-							'label'       => 'Free shipping',
-							'type'        => 'SHIPPING',
-							'amount'      => array(
+							'reference_id'    => '',
+							'amount'          => array(
 								'currency_code' => 'USD',
-								'value'         => '0.00',
+								'value'         => '50.00',
+								'breakdown'    => array(
+									'item_total'     => array(
+										'currency_code' => 'USD',
+										'value'         => '40.00',
+									),
+									'shipping'       => array(
+										'currency_code' => 'USD',
+										'value'         => '10.00',
+									),
+									'tax_total'     => array(
+										'currency_code' => 'USD',
+										'value'         => '0.00',
+									),
+									'discount'      => array(
+										'currency_code' => 'USD',
+										'value'         => '0.00',
+									),
+								),
 							),
-							'default'     => false,
-							'description' => '',
 						),
 					),
 				),
