d5b463480e00a09fa3b7e602cddc9219132c1c1d
Michael Pretty <prettyboymp@users.noreply.github.com>
2025-10-20 12:02:17 -0400

    Update test

--- plugins/woocommerce/src/Internal/Features/FeaturesController.php
+++ plugins/woocommerce/src/Internal/Features/FeaturesController.php
@@ -1759,21 +1759,25 @@ class FeaturesController {
 			return;
 		}
 
-		wc_enqueue_js(
-			"
-		const warningRows = document.querySelectorAll('tr[data-plugin-row-type=\"feature-incomp-warn\"]');
-		for(const warningRow of warningRows) {
-			const pluginName = warningRow.getAttribute('data-plugin');
-			const pluginInfoRow = document.querySelector('tr.active[data-plugin=\"' + pluginName + '\"]:not(.plugin-update-tr), tr.inactive[data-plugin=\"' + pluginName + '\"]:not(.plugin-update-tr)');
-			if(pluginInfoRow.classList.contains('update')) {
-				warningRow.classList.remove('plugin-update-tr');
-				warningRow.querySelector('.notice').style.margin = '5px 10px 15px 30px';
-			}
-			else {
-				pluginInfoRow.classList.add('update');
-			}
-		}
-		"
+		$handle = 'wc-features-fix-plugin-list-html';
+		wp_register_script( $handle, '', array(), WC_VERSION, true );
+		wp_enqueue_script( $handle );
+		wp_add_inline_script(
+            $handle,
+        "
+            const warningRows = document.querySelectorAll('tr[data-plugin-row-type=\"feature-incomp-warn\"]');
+            for(const warningRow of warningRows) {
+                const pluginName = warningRow.getAttribute('data-plugin');
+                const pluginInfoRow = document.querySelector('tr.active[data-plugin=\"' + pluginName + '\"]:not(.plugin-update-tr), tr.inactive[data-plugin=\"' + pluginName + '\"]:not(.plugin-update-tr)');
+                if(pluginInfoRow.classList.contains('update')) {
+                    warningRow.classList.remove('plugin-update-tr');
+                    warningRow.querySelector('.notice').style.margin = '5px 10px 15px 30px';
+                }
+                else {
+                    pluginInfoRow.classList.add('update');
+                }
+            }
+            "
 		);
 	}
 
