29e266e7897a54c68fca71f53ed5927542820f97
Bartosz Budzanowski <bartosz.budzanowski@automattic.com>
2025-10-20 20:58:17 +0200

    Cherry-pick 654c7ed8339425f1daca435354a53ea1136d747c with unresolved conflicts from #60799

--- plugins/woocommerce/tests/e2e-pw/tests/js-file-monitor/monitor-js-file-number.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/js-file-monitor/monitor-js-file-number.spec.js
@@ -13,7 +13,11 @@ const merchantPages = [
 	{
 		name: 'WC Dashboard',
 		url: 'wp-admin/admin.php?page=wc-admin',
+<<<<<<< HEAD
 		expectedCount: 83,
+=======
+		expectedCount: 81,
+>>>>>>> 654c7ed833 (Update WordPress Abilities API integration to v0.3.0 (#60799))
 	},
 	{
 		name: 'Reports',
