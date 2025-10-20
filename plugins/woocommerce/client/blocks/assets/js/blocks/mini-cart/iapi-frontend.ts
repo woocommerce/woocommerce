2d8e135a027098da54b66425c7dd1ef30a926e46
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2025-10-20 14:11:28 -0400

    Use cart.items_count from the cart response to display number of items in mini cart. Closes #61466.

--- plugins/woocommerce/client/blocks/assets/js/blocks/mini-cart/iapi-frontend.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/mini-cart/iapi-frontend.ts
@@ -126,10 +126,7 @@ store< MiniCart >(
 	{
 		state: {
 			get totalItemsInCart() {
-				return woocommerceState.cart.items.reduce< number >(
-					( total, { quantity } ) => total + quantity,
-					0
-				);
+				return woocommerceState.cart.items_count;
 			},
 
 			get formattedSubtotal(): string {
