37239d75bcc2ef31323bb5af4486b9cbb24c0df0
Tom Cafferkey <tjcafferkey@gmail.com>
2025-10-21 15:29:27 +0100

    Only allow customer role

--- plugins/woocommerce/includes/data-stores/class-wc-customer-data-store.php
+++ plugins/woocommerce/includes/data-stores/class-wc-customer-data-store.php
@@ -689,7 +689,7 @@ class WC_Customer_Data_Store extends WC_Data_Store_WP implements WC_Customer_Dat
 				break;
 		}
 
-		if ( 'customer' !== $args['role'] ) {
+		if ( 'customer' === $args['role'] ) {
 			$query_args['role'] = 'customer';
 		}
 
