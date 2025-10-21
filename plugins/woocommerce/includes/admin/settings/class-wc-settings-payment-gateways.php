85f313e34c387e4480ac8f673304f08cced19aa7
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-10-21 16:06:59 +0100

    Lint fixes

--- plugins/woocommerce/includes/admin/settings/class-wc-settings-payment-gateways.php
+++ plugins/woocommerce/includes/admin/settings/class-wc-settings-payment-gateways.php
@@ -22,12 +22,12 @@ class WC_Settings_Payment_Gateways extends WC_Settings_Page {
 
 	const TAB_NAME = 'checkout';
 
-	const MAIN_SECTION_NAME            = 'main';
-	const OFFLINE_SECTION_NAME         = 'offline';
-	const COD_SECTION_NAME             = 'cod';  // Cash on delivery.
-	const PAL_SECTION_NAME = 'pal';  // Pay at Location.
-	const BACS_SECTION_NAME            = 'bacs';  // Direct bank transfer.
-	const CHEQUE_SECTION_NAME          = 'cheque';  // Cheque payments.
+	const MAIN_SECTION_NAME    = 'main';
+	const OFFLINE_SECTION_NAME = 'offline';
+	const COD_SECTION_NAME     = 'cod';  // Cash on delivery.
+	const PAL_SECTION_NAME     = 'pal';  // Pay at Location.
+	const BACS_SECTION_NAME    = 'bacs';  // Direct bank transfer.
+	const CHEQUE_SECTION_NAME  = 'cheque';  // Cheque payments.
 
 	/**
 	 * Setting page icon.
