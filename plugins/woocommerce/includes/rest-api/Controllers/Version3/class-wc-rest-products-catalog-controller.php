c3d1cf2b483d671cb44aa308d8464870ee17912e
Jaclyn Chen <watertranquil@gmail.com>
2025-10-22 14:03:32 +0800

    Update `fields` parameter to be required with non-empty validation.

--- plugins/woocommerce/includes/rest-api/Controllers/Version3/class-wc-rest-products-catalog-controller.php
+++ plugins/woocommerce/includes/rest-api/Controllers/Version3/class-wc-rest-products-catalog-controller.php
@@ -53,7 +53,7 @@ class WC_REST_Products_Catalog_Controller extends WC_REST_Controller {
 							'description'       => __( 'Product/variation fields to include in the catalog. Can be an array or comma-separated string.', 'woocommerce' ),
 							'type'              => array( 'array', 'string' ),
 							'items'             => array( 'type' => 'string' ),
-							'default'           => array(),
+							'required'          => true,
 							'validate_callback' => array( $this, 'validate_fields_arg' ),
 							'sanitize_callback' => array( $this, 'sanitize_fields_arg' ),
 						),
@@ -127,6 +127,11 @@ class WC_REST_Products_Catalog_Controller extends WC_REST_Controller {
 		if ( ! is_array( $value ) && ! is_string( $value ) ) {
 			return new WP_Error( 'invalid_fields', __( 'fields must be an array of strings or a comma-separated string.', 'woocommerce' ) );
 		}
+
+		if ( ( is_array( $value ) && empty( $value ) ) || ( is_string( $value ) && '' === trim( $value ) ) ) {
+			return new WP_Error( 'invalid_fields', __( 'fields cannot be empty.', 'woocommerce' ) );
+		}
+
 		return true;
 	}
 
