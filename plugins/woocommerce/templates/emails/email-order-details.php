2f3a249969d27e8debacaad4a47dd21485d5d197
Peter Wilson <git-commit-by@peterwilson.cc>
2025-10-23 12:39:47 +1100

    Align price heading according to `$order_total_text_align`.

--- plugins/woocommerce/templates/emails/email-order-details.php
+++ plugins/woocommerce/templates/emails/email-order-details.php
@@ -75,7 +75,7 @@ do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plai
 			<tr>
 				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
 				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $text_align ); ?>;"><?php esc_html_e( 'Quantity', 'woocommerce' ); ?></th>
-				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $email_improvements_enabled ? 'right' : $text_align ); ?>;"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
+				<th class="td" scope="col" style="text-align:<?php echo esc_attr( $order_total_text_align ); ?>;"><?php esc_html_e( 'Price', 'woocommerce' ); ?></th>
 			</tr>
 		</thead>
 		<tbody>
