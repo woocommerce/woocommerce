65b006e9b2a946d248f9ecec9a47ade475413c64
Peter Wilson <git-commit-by@peterwilson.cc>
2025-10-23 12:24:02 +1100

    Bump template versions for expected WooCommerce release.

--- plugins/woocommerce/templates/emails/customer-fulfillment-created.php
+++ plugins/woocommerce/templates/emails/customer-fulfillment-created.php
@@ -12,7 +12,7 @@
  *
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails\HTML
- * @version 10.3.0
+ * @version 10.4.0
  */
 
 defined( 'ABSPATH' ) || exit;
--- plugins/woocommerce/templates/emails/email-downloads.php
+++ plugins/woocommerce/templates/emails/email-downloads.php
@@ -12,7 +12,7 @@
  *
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 10.3.0
+ * @version 10.4.0
  */
 
 use Automattic\WooCommerce\Utilities\FeaturesUtil;
