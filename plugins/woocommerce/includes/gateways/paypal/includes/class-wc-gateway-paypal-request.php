5e1b399425afb2db6f1c51e8dcab39084cabe8b2
Wesley Rosa <wesleyjrosa@gmail.com>
2025-10-26 08:45:38 -0500

    Unifying sum of items amount

--- plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
+++ plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
@@ -574,7 +574,7 @@ class WC_Gateway_Paypal_Request {
 	private function get_paypal_order_items_subtotal( $order ) {
 		$total = 0;
 		foreach ( $order->get_items( array( 'line_item', 'fee' ) ) as $item ) {
-			$total += $this->get_paypal_order_item_amount( $order, $item );
+			$total += $this->get_paypal_order_item_amount( $order, $item ) * $item->get_quantity();
 		}
 
 		return $total;
