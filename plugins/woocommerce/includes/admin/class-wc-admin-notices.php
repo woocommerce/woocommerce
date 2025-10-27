f590ed1a38dc21832d0d04854388b3c889b3f64b
Jorge Torres <jorge.torres@automattic.com>
2025-10-27 16:46:04 +0000

    Remove unnecessary import

--- plugins/woocommerce/includes/admin/class-wc-admin-notices.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-notices.php
@@ -9,7 +9,6 @@
 use Automattic\Jetpack\Constants;
 use Automattic\WooCommerce\Internal\Utilities\Users;
 use Automattic\WooCommerce\Internal\Utilities\WebhookUtil;
-use Automattic\WooCommerce\Utilities\FeaturesUtil;
 
 defined( 'ABSPATH' ) || exit;
 
