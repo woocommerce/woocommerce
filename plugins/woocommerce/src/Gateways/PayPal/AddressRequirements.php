9cccf381736335ab5aa28194069ddb02926c9aae
Wesley Rosa <wesleyjrosa@gmail.com>
2025-10-30 11:35:20 -0300

    Update plugins/woocommerce/src/Gateways/PayPal/AddressRequirements.php
    Co-authored-by: daledupreez <daledupreez@gmail.com>

--- plugins/woocommerce/src/Gateways/PayPal/AddressRequirements.php
+++ plugins/woocommerce/src/Gateways/PayPal/AddressRequirements.php
@@ -332,7 +332,7 @@ class AddressRequirements {
 	 * @param string $country_code The ISO 3166-1 alpha-2 country code.
 	 * @return bool
 	 */
-	public static function country_requires_city( $country_code ) {
+	public static function country_requires_city( string $country_code ) {
 		return in_array( strtoupper( $country_code ), self::COUNTRIES_REQUIRING_CITY, true );
 	}
 
