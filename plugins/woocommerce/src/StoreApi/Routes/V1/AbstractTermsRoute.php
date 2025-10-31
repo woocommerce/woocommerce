dc5c7c1caecce874b47f31809211553529495650
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2025-10-31 15:14:01 -0700

    Limit StoreAPI Term Queries

--- plugins/woocommerce/src/StoreApi/Routes/V1/AbstractTermsRoute.php
+++ plugins/woocommerce/src/StoreApi/Routes/V1/AbstractTermsRoute.php
@@ -37,7 +37,8 @@ abstract class AbstractTermsRoute extends AbstractRoute {
 		$params['per_page'] = array(
 			'description'       => __( 'Maximum number of items to be returned in result set. Defaults to no limit if left blank.', 'woocommerce' ),
 			'type'              => 'integer',
-			'minimum'           => 0,
+			'default'           => 100,
+			'minimum'           => 1,
 			'maximum'           => 100,
 			'sanitize_callback' => 'absint',
 			'validate_callback' => 'rest_validate_request_arg',
