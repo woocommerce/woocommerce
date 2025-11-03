75e7661b7aaef797e01cc4f37ecf9b4c8b1dcd4e
Sam Seay <samueljseay@gmail.com>
2025-11-03 12:15:58 +0800

    Fix a bug where the key for displayed attributes was not correct between variations/non-variations causing repeating attributes.

--- plugins/woocommerce/src/Blocks/BlockTypes/MiniCartProductsTableBlock.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/MiniCartProductsTableBlock.php
@@ -261,6 +261,9 @@ class MiniCartProductsTableBlock extends AbstractInnerBlock {
 	protected function render_experimental_iapi_product_details_markup( $property ) {
 		$context = array( 'dataProperty' => $property );
 
+		// Variations have a `raw_attribute` property on attributes, but non-variations have `key` property.
+		$each_key = 'variation' === $property ? 'raw_attribute' : 'key';
+
 		// If the property is item_data, so not a variation, we need to skip the text directive.
 		$is_item_data = 'item_data' === $context['dataProperty'];
 
@@ -280,7 +283,7 @@ class MiniCartProductsTableBlock extends AbstractInnerBlock {
 		>
 			<template
 				data-wp-each--item-data="state.cartItem.<?php echo esc_attr( $property ); ?>"
-				data-wp-each-key="context.itemData.raw_attribute"
+				data-wp-each-key="context.itemData.<?php echo esc_attr( $each_key ); ?>"
 			>
 				<?php echo $this->render_experimental_iapi_product_details_item_markup( 'li', $is_item_data ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 			</template>
@@ -302,7 +305,6 @@ class MiniCartProductsTableBlock extends AbstractInnerBlock {
 		<<?php echo tag_escape( $tag_name ); ?>
 			data-wp-bind--hidden="state.cartItemDataAttr.hidden"
 			data-wp-bind--class="state.cartItemDataAttr.className"
-
 		>
 		<?php if ( $is_item_data ) : ?>
 			<span class="wc-block-components-product-details__name" data-wp-watch="callbacks.itemDataNameInnerHTML"></span><?php echo ': '; ?>
