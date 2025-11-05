7e7c5a2e01faa5953c5de8549b12b86f345f1644
Albert Juhé Lluveras <contact@albertjuhe.com>
2025-11-05 13:19:51 +0100

    Remove allowZero from product data

--- plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/frontend.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/frontend.ts
@@ -86,7 +86,6 @@ export const getProductData = (
 		return null;
 	}
 
-	const allowZero = productData.allowZero ?? false;
 	const min = typeof productData.min === 'number' ? productData.min : 1;
 	const max =
 		typeof productData.max === 'number' && productData.max >= 1
@@ -101,7 +100,6 @@ export const getProductData = (
 		max,
 		step,
 		type,
-		allowZero,
 	};
 };
 
