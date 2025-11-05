4f8aa865d6dbfe067f242c4aa38ceeab41294a13
Ahmed <ahmed.el.azzabi@automattic.com>
2025-11-05 11:27:23 +0100

    Use newly created controller in the v4 endpoints

--- plugins/woocommerce/includes/rest-api/Server.php
+++ plugins/woocommerce/includes/rest-api/Server.php
@@ -23,6 +23,7 @@ use Automattic\WooCommerce\Internal\RestApi\Routes\V4\Settings\OfflinePaymentMet
 use Automattic\WooCommerce\Internal\RestApi\Routes\V4\Customers\Controller as CustomersController;
 use Automattic\WooCommerce\Internal\RestApi\Routes\V4\Settings\General\Controller as GeneralSettingsController;
 use Automattic\WooCommerce\Internal\RestApi\Routes\V4\Settings\Email\Controller as EmailSettingsController;
+use Automattic\WooCommerce\Internal\RestApi\Routes\V4\Settings\Account\Controller as AccountSettingsController;
 
 /**
  * Class responsible for loading the REST API and all REST API namespaces.
@@ -240,6 +241,7 @@ class Server {
 			'settings-email'            => EmailSettingsController::class,
 			'settings-products'         => SettingsProductsController::class,
 			'settings-payment-gateways' => PaymentGatewaysController::class,
+			'settings-account'          => AccountSettingsController::class,
 			// This is a wrapper that redirects V4 settings requests to the V3 settings controller.
 			'settings'                  => 'WC_REST_Settings_V4_Controller',
 		);
