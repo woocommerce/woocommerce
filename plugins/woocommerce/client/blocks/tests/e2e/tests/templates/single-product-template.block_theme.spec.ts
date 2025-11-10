024a6d2fdfd0d869c3293c0313343003c5d57a5e
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-11-10 11:03:30 +0100

    Bring back some of the editor navigation logic

--- plugins/woocommerce/client/blocks/tests/e2e/tests/templates/single-product-template.block_theme.spec.ts
+++ plugins/woocommerce/client/blocks/tests/e2e/tests/templates/single-product-template.block_theme.spec.ts
@@ -66,12 +66,7 @@ test.describe( 'Single Product template', () => {
 		await admin.visitSiteEditor( {
 			postType: testData.templateType,
 		} );
-		// Templates added via "New template" display the path as the name since WP 6.9.
-		const templateName =
-			wpCoreVersion >= 6.9
-				? testData.templatePath
-				: testData.templateName;
-		await editor.revertTemplate( { templateName } );
+		await editor.revertTemplate( { templateName: testData.templateName } );
 		await page.goto( testData.permalink );
 
 		// Verify the edits are no longer visible.
