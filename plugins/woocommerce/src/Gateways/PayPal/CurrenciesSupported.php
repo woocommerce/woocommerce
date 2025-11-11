58dee5711009698455e30ea3e085fe877ecc3156
Wesley Rosa <wesleyjrosa@gmail.com>
2025-11-11 12:09:03 -0300

    Fix lint issues

--- plugins/woocommerce/src/Gateways/PayPal/CurrenciesSupported.php
+++ plugins/woocommerce/src/Gateways/PayPal/CurrenciesSupported.php
@@ -23,32 +23,32 @@ class CurrenciesSupported {
 	 *
 	 * @var array<string>
 	 */
-	const SUPPORTED_CURRENCIES = [
-		'AUD', // Australian Dollar
-		'BRL', // Brazilian Real
-		'CAD', // Canadian Dollar
-		'CNY', // Chinese Renminbi
-		'CZK', // Czech Koruna
-		'DKK', // Danish Krone
-		'EUR', // Euro
-		'HKD', // Hong Kong Dollar
-		'HUF', // Hungarian Forint
-		'ILS', // Israeli New Shekel
-		'JPY', // Japanese Yen
-		'MYR', // Malaysian Ringgit
-		'MXN', // Mexican Peso
-		'TWD', // New Taiwan Dollar
-		'NZD', // New Zealand Dollar
-		'NOK', // Norwegian Krone
-		'PHP', // Philippine Peso
-		'PLN', // Polish Złoty
-		'GBP', // Pound Sterling
-		'SGD', // Singapore Dollar
-		'SEK', // Swedish Krona
-		'CHF', // Swiss Franc
-		'THB', // Thai Baht
-		'USD', // United States Dollar
-	];
+	const SUPPORTED_CURRENCIES = array(
+		'AUD', // Australian Dollar.
+		'BRL', // Brazilian Real.
+		'CAD', // Canadian Dollar.
+		'CNY', // Chinese Renminbi.
+		'CZK', // Czech Koruna.
+		'DKK', // Danish Krone.
+		'EUR', // Euro.
+		'HKD', // Hong Kong Dollar.
+		'HUF', // Hungarian Forint.
+		'ILS', // Israeli New Shekel.
+		'JPY', // Japanese Yen.
+		'MYR', // Malaysian Ringgit.
+		'MXN', // Mexican Peso.
+		'TWD', // New Taiwan Dollar.
+		'NZD', // New Zealand Dollar.
+		'NOK', // Norwegian Krone.
+		'PHP', // Philippine Peso.
+		'PLN', // Polish Złoty.
+		'GBP', // Pound Sterling.
+		'SGD', // Singapore Dollar.
+		'SEK', // Swedish Krona.
+		'CHF', // Swiss Franc.
+		'THB', // Thai Baht.
+		'USD', // United States Dollar.
+	);
 
 	/**
 	 * Get class instance.
