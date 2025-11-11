6ff7e38dbb12bd6819b168bb8ca84972c898f59c
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-11-11 15:08:59 +0200

    Add playwright-ctrf-json-reporter

--- plugins/woocommerce/tests/e2e-pw/playwright.config.js
+++ plugins/woocommerce/tests/e2e-pw/playwright.config.js
@@ -39,6 +39,13 @@ const reporter = [
 		},
 	],
 	[
+		'playwright-ctrf-json-reporter',
+		{
+			outputDir: `${ TESTS_ROOT_PATH }/test-results`,
+			outputFile: 'ctrf-report.json',
+		},
+	],
+	[
 		`${ TESTS_ROOT_PATH }/reporters/environment-reporter.js`,
 		{ outputFolder: `${ TESTS_ROOT_PATH }/test-results/allure-results` },
 	],
