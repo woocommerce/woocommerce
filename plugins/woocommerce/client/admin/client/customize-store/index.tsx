b90f0384f29bb070d9f41ec596653be2406c76fe
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-11-18 10:07:05 +0100

    Align the style with Site Hub

--- plugins/woocommerce/client/admin/client/customize-store/index.tsx
+++ plugins/woocommerce/client/admin/client/customize-store/index.tsx
@@ -1,12 +1,22 @@
+/* eslint-disable @woocommerce/dependency-group */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /**
  * External dependencies
  */
 import { useEffect } from '@wordpress/element';
 import { __, isRTL } from '@wordpress/i18n';
-import { Button } from '@wordpress/components';
-import { chevronLeft, chevronRight } from '@wordpress/icons';
-import { getNewPath } from '@woocommerce/navigation';
+import {
+	Button,
+	__experimentalHStack as HStack,
+	__experimentalHeading as Heading,
+	__experimentalVStack as VStack,
+	__unstableMotion as motion,
+} from '@wordpress/components';
 import { getAdminLink } from '@woocommerce/settings';
+import { getNewPath } from '@woocommerce/navigation';
+// @ts-ignore No types for this exist yet.
+import SidebarButton from '@wordpress/edit-site/build-module/components/sidebar-button';
+import { chevronRight, chevronLeft } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -14,6 +24,7 @@ import { getAdminLink } from '@woocommerce/settings';
 import { useFullScreen } from '~/utils';
 import { isWooExpress } from '~/utils/is-woo-express';
 import { isFeatureEnabled } from '~/utils/features';
+import { SiteHub } from './assembler-hub/site-hub';
 import './style.scss';
 
 const CustomizeStoreController = () => {
@@ -45,98 +56,146 @@ const CustomizeStoreController = () => {
 		}
 	};
 
-	const handleBackClick = () => {
-		window.location.href = getNewPath( {}, '/', {} );
-	};
+	const chevronIcon = isRTL() ? chevronRight : chevronLeft;
 
-	const icon = isRTL() ? chevronRight : chevronLeft;
+	const sidebarTitle = (
+		<Button
+			onClick={ () => {
+				window.location.href = getNewPath( {}, '/', {} );
+			} }
+		>
+			{ __( 'Customize your store', 'woocommerce' ) }
+		</Button>
+	);
+
+	const sidebarDescription = __(
+		'Design a store that reflects your brand and business. Customize your active theme, select a professionally designed theme, or create a new look using our store designer.',
+		'woocommerce'
+	);
 
 	return (
 		<div className="woocommerce-customize-store__container">
-			<div className="woocommerce-customize-store-container">
-				<div className="woocommerce-customize-store-sidebar">
-					<div className="woocommerce-customize-store-sidebar__title">
-						<button onClick={ handleBackClick }>{ icon }</button>
-						{ __( 'Customize your store', 'woocommerce' ) }
+			<div className="woocommerce-customize-store-sidebar">
+				<motion.div
+					className="woocommerce-edit-site-layout__header-container"
+					animate={ 'view' }
+				>
+					<SiteHub
+						variants={ {
+							view: { x: 0 },
+						} }
+						isTransparent={ false }
+						className="woocommerce-edit-site-layout__hub"
+					/>
+				</motion.div>
+
+				<VStack
+					className="woocommerce-edit-site-sidebar-navigation-screen__main"
+					spacing={ 0 }
+					justify="flex-start"
+				>
+					<HStack
+						spacing={ 4 }
+						alignment="flex-start"
+						className="woocommerce-edit-site-sidebar-navigation-screen__title-icon"
+					>
+						<SidebarButton
+							onClick={ () => {
+								window.location.href = getNewPath(
+									{},
+									'/',
+									{}
+								);
+							} }
+							icon={ chevronIcon }
+							label={ __( 'Back', 'woocommerce' ) }
+							showTooltip={ false }
+						/>
+
+						<Heading
+							className="woocommerce-edit-site-sidebar-navigation-screen__title"
+							level={ 1 }
+							as="h1"
+						>
+							{ sidebarTitle }
+						</Heading>
+					</HStack>
+
+					<div className="woocommerce-edit-site-sidebar-navigation-screen__content">
+						<p className="woocommerce-edit-site-sidebar-navigation-screen__description">
+							{ sidebarDescription }
+						</p>
 					</div>
-					<p>
-						{ __(
-							'Design a store that reflects your brand and business. Customize your active theme, select a professionally designed theme, or create a new look using our store designer.',
-							'woocommerce'
-						) }
-					</p>
+				</VStack>
+			</div>
+
+			<div className="woocommerce-customize-store-main">
+				<div className="woocommerce-customize-store-banner no-ai-banner">
+					<div className="woocommerce-customize-store-banner-content">
+						<div className="banner-actions">
+							<h1>{ __( 'Design your own', 'woocommerce' ) }</h1>
+							<p>
+								{ __(
+									'Quickly create a beautiful store using our built-in store designer. Choose your layout, select a style, and much more.',
+									'woocommerce'
+								) }
+							</p>
+							<Button
+								variant="primary"
+								onClick={ handleDesignClick }
+							>
+								{ __( 'Start designing', 'woocommerce' ) }
+							</Button>
+						</div>
+					</div>
 				</div>
 
-				<div className="woocommerce-customize-store-main">
-					<div className="woocommerce-customize-store-banner no-ai-banner">
-						<div className="woocommerce-customize-store-banner-content">
-							<div className="banner-actions">
-								<h1>
-									{ __( 'Design your own', 'woocommerce' ) }
-								</h1>
+				<div className="woocommerce-customize-store-banner pick-your-theme-banner">
+					<div className="woocommerce-customize-store-banner-content">
+						<div className="banner-actions">
+							<h2>
+								{ __(
+									'Pick your perfect theme',
+									'woocommerce'
+								) }
+							</h2>
+							<div className="pick-your-theme-banner__content">
 								<p>
 									{ __(
-										'Quickly create a beautiful store using our built-in store designer. Choose your layout, select a style, and much more.',
+										'Bring your vision to life — no coding required. Explore hundreds of free and paid ecommerce-optimized themes.',
 										'woocommerce'
 									) }
 								</p>
-								<Button
-									variant="primary"
-									onClick={ handleDesignClick }
-								>
-									{ __( 'Start designing', 'woocommerce' ) }
-								</Button>
-							</div>
-						</div>
-					</div>
-
-					<div className="woocommerce-customize-store-banner pick-your-theme-banner">
-						<div className="woocommerce-customize-store-banner-content">
-							<div className="banner-actions">
-								<h2>
-									{ __(
-										'Pick your perfect theme',
-										'woocommerce'
-									) }
-								</h2>
-								<div className="pick-your-theme-banner__content">
-									<p>
+								<ul>
+									<li>
 										{ __(
-											'Bring your vision to life — no coding required. Explore hundreds of free and paid ecommerce-optimized themes.',
+											'Themes for every industry',
 											'woocommerce'
 										) }
-									</p>
-									<ul>
-										<li>
-											{ __(
-												'Themes for every industry',
-												'woocommerce'
-											) }
-										</li>
-										<li>
-											{ __(
-												'Ready to use out of the box',
-												'woocommerce'
-											) }
-										</li>
-										<li>
-											{ __(
-												'30-day money-back guarantee',
-												'woocommerce'
-											) }
-										</li>
-									</ul>
-								</div>
-								<Button
-									variant="primary"
-									onClick={ handleMarketplaceClick }
-								>
-									{ __(
-										'Browse the Marketplace',
-										'woocommerce'
-									) }
-								</Button>
+									</li>
+									<li>
+										{ __(
+											'Ready to use out of the box',
+											'woocommerce'
+										) }
+									</li>
+									<li>
+										{ __(
+											'30-day money-back guarantee',
+											'woocommerce'
+										) }
+									</li>
+								</ul>
 							</div>
+							<Button
+								variant="primary"
+								onClick={ handleMarketplaceClick }
+							>
+								{ __(
+									'Browse the Marketplace',
+									'woocommerce'
+								) }
+							</Button>
 						</div>
 					</div>
 				</div>
