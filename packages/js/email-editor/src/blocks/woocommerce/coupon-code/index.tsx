e3023045d215ae517ede2ce2de84896c0c163090
Pavel Dohnal <pavel.dohnal@automattic.com>
2025-11-25 16:08:55 +0100

    Remove duplicities

--- packages/js/email-editor/src/blocks/woocommerce/coupon-code/index.tsx
+++ packages/js/email-editor/src/blocks/woocommerce/coupon-code/index.tsx
@@ -7,6 +7,7 @@ import { tag as icon } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
+import metadata from './block.json';
 import { Edit } from './edit';
 import { Save } from './save';
 
@@ -14,42 +15,9 @@ import { Save } from './save';
  * Register the Coupon Code block.
  */
 export function registerCouponCodeBlock() {
-	// @ts-expect-error: 'email' is a custom property not in BlockSupports type
-	registerBlockType( 'woocommerce/coupon-code', {
-		apiVersion: 3,
-		title: 'Coupon Code',
-		category: 'woocommerce',
-		description:
-			'Include a coupon code to entice recipients to make a purchase.',
+	// @ts-expect-error: block.json metadata types don't perfectly match BlockConfiguration, but WordPress handles this at runtime
+	registerBlockType( metadata, {
 		icon,
-		supports: {
-			email: true,
-			html: false,
-			align: true,
-			color: {
-				text: true,
-				background: true,
-			},
-			typography: {
-				fontSize: true,
-			},
-			spacing: {
-				margin: true,
-				padding: true,
-			},
-			__experimentalBorder: {
-				color: true,
-				radius: true,
-				style: true,
-				width: true,
-			},
-		},
-		attributes: {
-			couponCode: {
-				type: 'string',
-				default: '',
-			},
-		},
 		edit: Edit,
 		save: Save,
 	} );
