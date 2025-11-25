adeee5fd68a84735886370cf1bfd8b086b296dea
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2025-11-25 10:41:31 +0100

    Fix linting

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/Utils.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/Utils.php
@@ -119,7 +119,7 @@ class Utils {
 		$wrapper_attributes = array_merge(
 			array(
 				'data-wp-interactive' => 'woocommerce/add-to-cart-with-options-quantity-selector',
-				'data-wp-init' 	      => 'callbacks.storeInputElementRef',
+				'data-wp-init'        => 'callbacks.storeInputElementRef',
 			),
 			$wrapper_attributes
 		);
