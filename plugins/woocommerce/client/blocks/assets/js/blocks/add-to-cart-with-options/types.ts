91c68fec66f94a748b6024b876e2224584b045e8
Mario Santos <34552881+SantosGuillamot@users.noreply.github.com>
2025-11-26 17:14:11 +0100

    Remove types from add-to-cart-with-options

--- plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/types.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/add-to-cart-with-options/types.ts
@@ -1,29 +1,3 @@
-/**
- * External dependencies
- */
-import {
-	ProductData,
-	VariationData,
-} from '@woocommerce/stores/woocommerce/cart';
-
-export type QuantitySelectorStyleProps = 'input' | 'stepper';
-
 export interface Attributes {
 	className?: string;
 }
-
-// What are these normalized types for? Is not the same as ProductData and VariationData?
-export type NormalizedProductData = ProductData & {
-	id: number;
-	min: number;
-	max: number;
-	step: number;
-};
-
-export type NormalizedVariationData = VariationData & {
-	id: number;
-	min: number;
-	max: number;
-	step: number;
-	type: 'variation';
-};
