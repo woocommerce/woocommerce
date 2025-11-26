111b65fd912c0170c7cf56b316552db12f0b99bd
Łukasz Strączyński <lukasz.straczynski@automattic.com>
2025-11-26 14:59:19 +0100

    Add some phpcs:ignore comments for already present filters

--- plugins/woocommerce/includes/class-wc-cart-session.php
+++ plugins/woocommerce/includes/class-wc-cart-session.php
@@ -618,13 +618,16 @@ final class WC_Cart_Session {
 
 			$product_data = wc_get_product( $variation_id ? $variation_id : $product_id );
 			if ( $product_data instanceof WC_Product && $product_data->is_sold_individually() ) {
+				// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment -- Filter documented in CartController::validate_add_to_cart().
 				$quantity = apply_filters( 'woocommerce_add_to_cart_sold_individually_quantity', 1, $quantity, $product_id, $variation_id, $cart_item_data );
 
-				$cart_id       = WC()->cart->generate_cart_id( $product_id, $variation_id, $variations, $cart_item_data );
+				$cart_id = WC()->cart->generate_cart_id( $product_id, $variation_id, $variations, $cart_item_data );
+				// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment -- Filter first added in WC_Cart::add_to_cart(), add documentation there.
 				$found_in_cart = apply_filters( 'woocommerce_add_to_cart_sold_individually_found_in_cart', isset( $cart[ $cart_id ] ) && isset( $cart[ $cart_id ]['quantity'] ) && $cart[ $cart_id ]['quantity'] > 0, $product_id, $variation_id, $cart_item_data, $cart_id );
 				if ( $found_in_cart ) {
 					/* translators: %s: product name */
-					$message         = sprintf( __( 'You cannot add another "%s" to your cart.', 'woocommerce' ), $product_data->get_name() );
+					$message = sprintf( __( 'You cannot add another "%s" to your cart.', 'woocommerce' ), $product_data->get_name() );
+					// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment -- Filter documented in WC_Cart::add_to_cart().
 					$message         = apply_filters( 'woocommerce_cart_product_cannot_add_another_message', $message, $product_data );
 					$wp_button_class = wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '';
 					$message         = sprintf( '%s <a href="%s" class="button wc-forward%s">%s</a>', $message, esc_url( wc_get_cart_url() ), esc_attr( $wp_button_class ), __( 'View cart', 'woocommerce' ) );
