166ae90dd175653482beaf77e8196cc9255d1205
Mayisha <mayisha.alam@automattic.com>
2025-11-28 09:14:36 +0600

    add type safety

--- plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
+++ plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
@@ -159,7 +159,7 @@ class WC_Gateway_Paypal_Request {
 			$response_data = json_decode( $body, true );
 
 			// Handle PayPal notices based on response status.
-			$this->maybe_add_or_remove_notice( $http_code, $response_data );
+			$this->maybe_add_or_remove_notice( $http_code, $response_data ?? array() );
 
 			if ( ! in_array( $http_code, array( 200, 201 ), true ) ) {
 				$paypal_debug_id = isset( $response_data['debug_id'] ) ? $response_data['debug_id'] : null;
@@ -1379,8 +1379,8 @@ class WC_Gateway_Paypal_Request {
 	 * Sets the account restriction flag if the status is 422 (account restricted),
 	 * or clears it if the status indicates success (200, 201).
 	 *
-	 * @param int         $http_code The HTTP status code from the PayPal API response.
-	 * @param array|null  $response_data The response data from the PayPal API response.
+	 * @param int   $http_code The HTTP status code from the PayPal API response.
+	 * @param array $response_data The response data from the PayPal API response.
 	 * @return void
 	 */
 	protected function maybe_add_or_remove_notice( int $http_code, array $response_data ): void {
