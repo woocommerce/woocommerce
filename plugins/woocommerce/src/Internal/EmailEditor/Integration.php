b6cef8181a33b50563c3014ddd6bb71b7b7f25a5
Pavel Dohnal <pavel.dohnal@automattic.com>
2025-11-28 11:13:12 +0100

    Linter fixes

--- plugins/woocommerce/src/Internal/EmailEditor/Integration.php
+++ plugins/woocommerce/src/Internal/EmailEditor/Integration.php
@@ -129,8 +129,8 @@ class Integration {
 	 * Register the coupon code block.
 	 */
 	public function register_coupon_code_block() {
-		$editor_container              = \Automattic\WooCommerce\EmailEditor\Email_Editor_Container::container();
-		$woocommerce_integration       = $editor_container->get( \Automattic\WooCommerce\EmailEditor\Integrations\WooCommerce\Initializer::class );
+		$editor_container        = \Automattic\WooCommerce\EmailEditor\Email_Editor_Container::container();
+		$woocommerce_integration = $editor_container->get( \Automattic\WooCommerce\EmailEditor\Integrations\WooCommerce\Initializer::class );
 
 		register_block_type(
 			'woocommerce/coupon-code',
