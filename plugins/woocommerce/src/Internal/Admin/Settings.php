3b51c316a13378a2fd43be305782985179abd940
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-12-01 17:57:54 +0800

    Fix lint

--- plugins/woocommerce/src/Internal/Admin/Settings.php
+++ plugins/woocommerce/src/Internal/Admin/Settings.php
@@ -364,7 +364,7 @@ class Settings {
 			);
 
 			// Add hidden setting for the import interval to display in the client side.
-			$import_interval        = \Automattic\WooCommerce\Internal\Admin\Schedulers\OrdersScheduler::get_import_interval();
+			$import_interval = \Automattic\WooCommerce\Internal\Admin\Schedulers\OrdersScheduler::get_import_interval();
 			// format the import interval to a human-readable string.
 			$import_interval_string = human_time_diff( 0, $import_interval );
 			$settings[]             = array(
