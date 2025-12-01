e123a1d69b72fb7d81b9262050d38ee2dfce31e1
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-12-01 16:54:47 +0200

    Fix lint error

--- plugins/woocommerce/client/blocks/tests/e2e/tests/cart/cart-checkout-block-translations.shopper.block_theme.spec.ts
+++ plugins/woocommerce/client/blocks/tests/e2e/tests/cart/cart-checkout-block-translations.shopper.block_theme.spec.ts
@@ -35,7 +35,9 @@ test.describe( 'Shopper → Translations', () => {
 		await frontendUtils.goToCart();
 
 		const totalsHeader = page
-			.getByRole( 'columnheader', { name: getTestTranslation( 'Total' ) } )
+			.getByRole( 'columnheader', {
+				name: getTestTranslation( 'Total' ),
+			} )
 			.locator( 'span' );
 		await expect( totalsHeader ).toBeVisible();
 
