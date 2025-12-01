bf2c1f505433ff44d04711dd5d71d79b343a4aef
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-12-01 17:53:29 +0800

    Fix lint

--- plugins/woocommerce/client/admin/client/analytics/settings/index.js
+++ plugins/woocommerce/client/admin/client/analytics/settings/index.js
@@ -117,13 +117,6 @@ const Settings = ( { createNotice, query } ) => {
 	const handleInputChange = ( e ) => {
 		const { checked, name, type, value } = e.target;
 
-		console.warn( 'name==>', name === IMMEDIATE_IMPORT_SETTING_NAME );
-		console.warn(
-			'wcAdminSettings[ name ]==>',
-			wcAdminSettings[ name ] === 'no'
-		);
-		console.warn( 'value==>', value === 'yes' );
-
 		// Intercept import mode change from scheduled to immediate
 		if (
 			name === IMMEDIATE_IMPORT_SETTING_NAME &&
