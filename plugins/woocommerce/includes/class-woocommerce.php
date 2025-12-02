dcb8e3b61a6b9f15ea94ee85039cbf5eb8e6385c
Vasily Belolapotkov <vasily.belolapotkov@automattic.com>
2025-12-02 17:26:02 +0100

    Remove old PoC endpoints

--- plugins/woocommerce/includes/class-woocommerce.php
+++ plugins/woocommerce/includes/class-woocommerce.php
@@ -18,7 +18,6 @@ use Automattic\WooCommerce\Internal\DataStores\Orders\CustomOrdersTableControlle
 use Automattic\WooCommerce\Internal\DownloadPermissionsAdjuster;
 use Automattic\WooCommerce\Internal\Features\FeaturesController;
 use Automattic\WooCommerce\Internal\FraudProtection\AdminSettingsHandler;
-use Automattic\WooCommerce\Internal\FraudProtection\ClearanceRestController;
 use Automattic\WooCommerce\Internal\FraudProtection\FraudProtectionController;
 use Automattic\WooCommerce\Internal\FraudProtection\SessionClearanceManager;
 use Automattic\WooCommerce\Internal\MCP\MCPAdapterProvider;
@@ -325,7 +324,6 @@ final class WooCommerce {
 		add_action( 'woocommerce_installed', array( $this, 'add_woocommerce_inbox_variant' ) );
 		add_action( 'woocommerce_updated', array( $this, 'add_woocommerce_inbox_variant' ) );
 		add_action( 'rest_api_init', array( $this, 'register_wp_admin_settings' ) );
-		add_action( 'rest_api_init', array( $this, 'register_fraud_protection_endpoints' ) );
 		add_action( 'woocommerce_installed', array( $this, 'add_woocommerce_remote_variant' ) );
 		add_action( 'woocommerce_updated', array( $this, 'add_woocommerce_remote_variant' ) );
 		add_action( 'woocommerce_newly_installed', 'wc_set_hooked_blocks_version', 10 );
@@ -1648,18 +1646,5 @@ final class WooCommerce {
 		) {
 			new WC_Shop_Customizer();
 		}
-	}
-
-	/**
-	 * Register fraud protection REST API endpoints.
-	 *
-	 * @internal
-	 *
-	 * @since 10.4.0
-	 */
-	public function register_fraud_protection_endpoints() {
-		$container = wc_get_container();
-		$controller = $container->get( ClearanceRestController::class );
-		$controller->register_routes();
 	}
 }
