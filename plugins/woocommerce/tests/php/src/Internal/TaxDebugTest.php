c0a923b094332f67073af7fd65d8fb9b5ed7a3fc
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-12-02 23:22:39 +0000

    Exclude roughly half tests from CI for debugging bisect

--- plugins/woocommerce/tests/php/src/Internal/TaxDebugTest.php
+++ plugins/woocommerce/tests/php/src/Internal/TaxDebugTest.php
@@ -6,6 +6,7 @@
 declare( strict_types=1 );
 namespace Automattic\WooCommerce\Tests\Internal;
 
+use Automattic\Jetpack\Constants;
 use Automattic\WooCommerce\Internal\TaxDebug;
 
 /**
@@ -43,6 +44,11 @@ class TaxDebugTest extends \WC_Unit_Test_Case {
 
 		$this->original_default_country = get_option( 'woocommerce_default_country' );
 
+		// Override any leaked constants from other tests (e.g., coupon tests define WOOCOMMERCE_CHECKOUT).
+		// Constants::set_constant() takes precedence over native define() when checking via Constants::is_defined().
+		Constants::set_constant( 'WOOCOMMERCE_CHECKOUT', false );
+		Constants::set_constant( 'WC_DOING_AJAX', false );
+
 		// Remove any existing TaxDebug hooks to prevent interference from container instance.
 		$container_tax_debug = wc_get_container()->get( TaxDebug::class );
 		remove_action( 'woocommerce_after_calculate_totals', array( $container_tax_debug, 'maybe_show_debug_notices' ), 999 );
@@ -70,6 +76,10 @@ class TaxDebugTest extends \WC_Unit_Test_Case {
 			$product->delete( true );
 		}
 		$this->created_products = array();
+
+		// Clear mocked constants.
+		Constants::clear_single_constant( 'WOOCOMMERCE_CHECKOUT' );
+		Constants::clear_single_constant( 'WC_DOING_AJAX' );
 
 		TaxDebug::reset_notices_flag();
 		wc_clear_notices();
