df0507a51188d10a793462b85f6fe6288455a5c3
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-12-04 10:11:36 +0100

    Add inspector controls

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/block-settings/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/block-settings/index.tsx
@@ -14,7 +14,10 @@ import {
 /**
  * Internal dependencies
  */
-import type { ProductGalleryThumbnailsSettingsProps } from '../types';
+import type {
+	ProductGalleryThumbnailsSettingsProps,
+	ProductGalleryThumbnailsBlockAttributes,
+} from '../types';
 
 const minValue = 10;
 const maxValue = 50;
@@ -24,7 +27,7 @@ export const ProductGalleryThumbnailsBlockSettings = ( {
 	attributes,
 	setAttributes,
 }: ProductGalleryThumbnailsSettingsProps ) => {
-	const { thumbnailSize, aspectRatio } = attributes;
+	const { thumbnailSize, aspectRatio, activeThumbnailStyle } = attributes;
 
 	const aspectRatioOptions = [
 		{
@@ -63,6 +66,17 @@ export const ProductGalleryThumbnailsBlockSettings = ( {
 		{
 			value: '9/16',
 			label: __( 'Tall - 9:16', 'woocommerce' ),
+		},
+	];
+
+	const activeThumbnailStyleOptions = [
+		{
+			value: 'overlay',
+			label: __( 'Overlay', 'woocommerce' ),
+		},
+		{
+			value: 'outline',
+			label: __( 'Outline', 'woocommerce' ),
 		},
 	];
 
@@ -107,6 +121,23 @@ export const ProductGalleryThumbnailsBlockSettings = ( {
 				} }
 				help={ __(
 					'Applies the selected aspect ratio to product thumbnails.',
+					'woocommerce'
+				) }
+			/>
+			<SelectControl
+				__next40pxDefaultSize
+				multiple={ false }
+				value={ activeThumbnailStyle }
+				options={ activeThumbnailStyleOptions }
+				label={ __( 'Active Thumbnail Style', 'woocommerce' ) }
+				onChange={ ( value ) => {
+					setAttributes( {
+						activeThumbnailStyle:
+							value as ProductGalleryThumbnailsBlockAttributes[ 'activeThumbnailStyle' ],
+					} );
+				} }
+				help={ __(
+					'Choose how the active thumbnail is highlighted.',
 					'woocommerce'
 				) }
 			/>
