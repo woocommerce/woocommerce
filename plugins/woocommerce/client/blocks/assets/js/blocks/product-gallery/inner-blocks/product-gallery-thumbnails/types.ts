692d126483ea14b3fe40c3d93ca0e7f818944d34
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2025-12-04 10:05:52 +0100

    Create attribute for active thumbanils state

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/types.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-gallery/inner-blocks/product-gallery-thumbnails/types.ts
@@ -1,6 +1,7 @@
 export type ProductGalleryThumbnailsBlockAttributes = {
 	thumbnailSize: string;
 	aspectRatio: string;
+	activeThumbnailStyle: 'overlay' | 'outline';
 };
 
 export type ProductGalleryThumbnailsSettingsProps = {
