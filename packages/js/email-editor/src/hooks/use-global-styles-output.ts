95b7581db2186a9396015dfe1ff20e675b12d4dc
Rostislav Wolny <rosta.wolny@automattic.com>
2025-12-08 17:46:07 +0100

    Remove optional getBlockStyles from global styles hook

--- packages/js/email-editor/src/hooks/use-global-styles-output.ts
+++ packages/js/email-editor/src/hooks/use-global-styles-output.ts
@@ -6,8 +6,6 @@ import {
 	type GlobalStylesConfig,
 } from '@wordpress/global-styles-engine';
 import { useMemo } from '@wordpress/element';
-import { store as blocksStore } from '@wordpress/blocks';
-import { useSelect } from '@wordpress/data';
 
 const EMPTY_OBJECT = {};
 const EMPTY_ARRAY = [];
@@ -15,13 +13,6 @@ const EMPTY_ARRAY = [];
 export const useGlobalStylesOutputWithConfig = (
 	mergedConfig: GlobalStylesConfig = {}
 ) => {
-	const { getBlockStyles } = useSelect( ( select ) => {
-		return {
-			// @ts-expect-error No types for blocksStore.getBlockStyles.
-			getBlockStyles: select( blocksStore ).getBlockStyles,
-		};
-	} );
-
 	return useMemo( () => {
 		if ( ! mergedConfig?.styles || ! mergedConfig?.settings ) {
 			return [ EMPTY_ARRAY, EMPTY_OBJECT ];
@@ -32,8 +23,7 @@ export const useGlobalStylesOutputWithConfig = (
 			hasFallbackGapSupport: false,
 			disableLayoutStyles: false,
 			disableRootPadding: false,
-			getBlockStyles,
 		} );
 		return styles;
-	}, [ mergedConfig, getBlockStyles ] );
+	}, [ mergedConfig ] );
 };
