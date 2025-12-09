a043aedfe723c26d24cdb46593d60959757fb069
Rostislav Wolny <rosta.wolny@automattic.com>
2025-12-09 11:28:06 +0100

    Remove Editor store overrides

--- packages/js/email-editor/src/editor.tsx
+++ packages/js/email-editor/src/editor.tsx
@@ -21,12 +21,7 @@ import '@wordpress/format-library'; // Enables text formatting capabilities
 import { getAllowedBlockNames, initBlocks } from './blocks';
 import { initializeLayout } from './layouts/flex-email';
 import { InnerEditor } from './components/block-editor';
-import {
-	createStore,
-	storeName,
-	initStoreOverrides,
-	deactivateStoreOverrides,
-} from './store';
+import { createStore, storeName } from './store';
 import { initTextHooks } from './text-hooks';
 import {
 	initEventCollector,
@@ -100,7 +95,7 @@ function Editor( {
 	);
 }
 
-function onInit( config: EmailEditorConfig ) {
+function onInit() {
 	initEventCollector();
 	initStoreTracking();
 	initDomTracking();
@@ -109,7 +104,6 @@ function onInit( config: EmailEditorConfig ) {
 	initializeLayout();
 	initBlocks();
 	initTextHooks();
-	initStoreOverrides( config );
 }
 
 export function initialize( elementId: string ) {
@@ -133,7 +127,7 @@ export function initialize( elementId: string ) {
 		Editor
 	) as typeof Editor;
 
-	onInit( getEditorConfigFromWindow() );
+	onInit();
 
 	// Set configuration to store from window object for backward compatibility
 	const editorConfig = getEditorConfigFromWindow();
@@ -167,7 +161,7 @@ export function ExperimentalEmailEditor( {
 		const backupEditorSettings = select( editorStore ).getEditorSettings();
 		// Set configuration to store from window object for backward compatibility
 		const editorConfig = config || getEditorConfigFromWindow();
-		onInit( editorConfig );
+		onInit();
 
 		dispatch( storeName ).setEditorConfig( editorConfig );
 		setIsInitialized( true );
@@ -175,7 +169,6 @@ export function ExperimentalEmailEditor( {
 		return () => {
 			try {
 				cleanupConfigurationChanges();
-				deactivateStoreOverrides();
 			} finally {
 				dispatch( editorStore ).updateEditorSettings(
 					backupEditorSettings
