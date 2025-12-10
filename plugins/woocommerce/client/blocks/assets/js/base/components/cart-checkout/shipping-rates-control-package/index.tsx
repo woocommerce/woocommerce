aba6d8bd1056fb9863e1eb008715f2cf0c7292f1
Alefe Souza <contact@alefesouza.com>
2025-12-09 21:50:45 -0300

    Add better sanitization on package names

--- plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/shipping-rates-control-package/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/shipping-rates-control-package/index.tsx
@@ -79,7 +79,7 @@ export const ShippingRatesControlPackage = ( {
 				<div
 					className="wc-block-components-shipping-rates-control__package-title"
 					dangerouslySetInnerHTML={ {
-						__html: sanitizeHTML( packageData.name ),
+						__html: sanitizeHTML( String( packageData.name ?? '' ) ),
 					} }
 				/>
 				{ shouldBeCollapsible && (
