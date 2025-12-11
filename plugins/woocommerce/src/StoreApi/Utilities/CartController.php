af4c2eb266d86cd22c17e98657ad7844dbddfa95
Mike Jolley <mike.jolley@me.com>
2025-12-11 13:02:20 +0000

    Move package ID and Name creation to cart class

--- plugins/woocommerce/src/StoreApi/Utilities/CartController.php
+++ plugins/woocommerce/src/StoreApi/Utilities/CartController.php
@@ -905,22 +905,10 @@ class CartController {
 
 		$packages = $cart->get_shipping_packages();
 
-		// Return early if invalid object supplied by the filter or no packages.
-		if ( ! is_array( $packages ) || empty( $packages ) ) {
+		// Return early if no packages.
+		if ( empty( $packages ) ) {
 			return [];
 		}
-
-		// Add extra package data to array.
-		$packages = array_map(
-			function ( $key, $package, $index ) {
-				$package['package_id']   = isset( $package['package_id'] ) ? $package['package_id'] : $key;
-				$package['package_name'] = isset( $package['package_name'] ) ? $package['package_name'] : $this->get_package_name( $package, $index );
-				return $package;
-			},
-			array_keys( $packages ),
-			$packages,
-			range( 1, count( $packages ) )
-		);
 
 		return $calculate_rates ? wc()->shipping()->calculate_shipping( $packages ) : $packages;
 	}
