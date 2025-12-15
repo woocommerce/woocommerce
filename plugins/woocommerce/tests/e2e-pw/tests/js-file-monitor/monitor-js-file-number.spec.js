a23c43b1ce4f12b1dafacdee25351fa37f369998
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-12-15 15:42:06 +0200

    Update expected JS file counts for Cart and Checkout pages in monitoring tests

--- plugins/woocommerce/tests/e2e-pw/tests/js-file-monitor/monitor-js-file-number.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/js-file-monitor/monitor-js-file-number.spec.js
@@ -7,8 +7,8 @@ const pageGroups = [
 		storageState: undefined,
 		pages: [
 			{ name: 'Shop page', url: 'shop/', expectedCount: 50 },
-			{ name: 'Cart', url: 'cart/', expectedCount: 54 },
-			{ name: 'Checkout', url: 'checkout/', expectedCount: 54 },
+			{ name: 'Cart', url: 'cart/', expectedCount: 55 },
+			{ name: 'Checkout', url: 'checkout/', expectedCount: 55 },
 		],
 	},
 	{
