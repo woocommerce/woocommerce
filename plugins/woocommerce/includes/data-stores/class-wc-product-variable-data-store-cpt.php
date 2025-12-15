woocommerce/woocommerce

commit 5c6f102de5883f2140d6505c24194a0da02a0e79

Fix undefined array key warning when firing woocommerce_variation_prices (#61472)

plugins/woocommerce/includes/data-stores/class-wc-product-variable-data-store-cpt.php
@@ -289,7 +289,9 @@ class WC_Product_Variable_Data_Store_CPT extends WC_Product_Data_Store_CPT imple
 			}
 
 			// If the prices are not stored for this hash, generate them and add to the transient.
-			if ( empty( $transient_cached_prices_array[ $price_hash ] ) ) {
+			// Check also the opposite price hash as it may have changed (see get_price_hash).
+			if ( empty( $transient_cached_prices_array[ $price_hash ] ) ||
+				( ( $opposite_price_hash !== $price_hash ) && empty( $transient_cached_prices_array[ $opposite_price_hash ] ) ) ) {
 				$prices_array = array(
 					'price'         => array(),
 					'regular_price' => array(),

commit 5f134ef06363fbe529653c0c98035c69e8701b4d

Fix a bug introduced in #61472
$opposite_price_hash was compared against
$price_hash but was not checked for null first,
causing '$transient_cached_prices_array[ $opposite_price_hash ]'
to throw a notice.

plugins/woocommerce/includes/data-stores/class-wc-product-variable-data-store-cpt.php
@@ -305,7 +305,7 @@ class WC_Product_Variable_Data_Store_CPT extends WC_Product_Data_Store_CPT imple
 			// If the prices are not stored for this hash, generate them and add to the transient.
 			// Check also the opposite price hash as it may have changed (see get_price_hash).
 			if ( empty( $transient_cached_prices_array[ $price_hash ] ) ||
-				( ( $opposite_price_hash !== $price_hash ) && empty( $transient_cached_prices_array[ $opposite_price_hash ] ) ) ) {
+				(  ! is_null( $opposite_price_hash ) && ( $opposite_price_hash !== $price_hash ) && empty( $transient_cached_prices_array[ $opposite_price_hash ] ) ) ) {
 				$prices_array = array(
 					'price'         => array(),
 					'regular_price' => array(),