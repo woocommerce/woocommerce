72ebc6b83ba93ef631631ff21fff013cb38de9b0
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2025-12-16 13:47:36 -0300

    Added connect to jetpack admin button

--- plugins/woocommerce/src/Internal/FraudProtection/AdminSettingsHandler.php
+++ plugins/woocommerce/src/Internal/FraudProtection/AdminSettingsHandler.php
@@ -7,6 +7,9 @@ declare( strict_types=1 );
 
 namespace Automattic\WooCommerce\Internal\FraudProtection;
 
+use Automattic\Jetpack\Connection\Manager;
+use Automattic\WooCommerce\Admin\API\OnboardingPlugins;
+
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -21,6 +24,7 @@ class AdminSettingsHandler {
 	 */
 	public function __construct() {
 		add_action( 'woocommerce_admin_field_fraud_protection_reset_sessions', array( $this, 'handle_output_reset_button' ), 10, 1 );
+		add_action( 'woocommerce_admin_field_jetpack_connection', array( $this, 'handle_output_jetpack_connection_button' ), 10, 1 );
 		add_action( 'admin_init', array( $this, 'handle_reset_sessions_action' ), 10, 0 );
 		add_action( 'admin_enqueue_scripts', array( $this, 'handle_enqueue_admin_scripts' ), 10, 1 );
 	}
@@ -60,6 +64,76 @@ class AdminSettingsHandler {
 			</td>
 		</tr>
 		<?php
+	}
+
+	/**
+	 * Output the reset sessions button field.
+	 *
+	 * @internal
+	 *
+	 * @param array $value Field configuration.
+	 * @return void
+	 */
+	public function handle_output_jetpack_connection_button( $value ): void {
+		$field_id          = esc_attr( $value['id'] );
+		$field_title       = esc_html( $value['title'] );
+		$field_description = esc_html( $value['desc'] );
+
+
+		$manager = new Manager( 'woocommerce' );
+		if ( ! $manager->is_connected() ) {
+			$request = new \WP_REST_Request();
+			$request->set_param( 'redirect_url', admin_url() );
+			$plugin_onboarding = new OnboardingPlugins();
+			$result = $plugin_onboarding->get_jetpack_authorization_url( $request );
+
+			// Todo: change parameters
+			if ( ! empty( $result['url'] ) ) {
+				$result['url'] = add_query_arg(
+					array(
+						'redirect_uri' => urlencode( admin_url() . 'admin.php/?page=wc-settings&tab=advanced&section=features' ),
+						// We use the new WooDNA value.
+						'from'         => 'woocommerce-setup-wizard',
+						// We inform Calypso that this is a WooPayments onboarding flow.
+						'plugin_name'  => 'woocommerce',
+						// Use the current user's WP admin color scheme.
+						'color_scheme' => $result['color_scheme'],
+					),
+					$result['url']
+				);
+				$connection_url = $result['url'];
+			}
+
+			?>
+			<tr valign="top">
+				<th scope="row" class="titledesc">
+					<label for="<?php echo $field_id; ?>"><?php echo $field_title; ?></label>
+				</th>
+				<td class="forminp forminp-button">
+					<button
+						type="button"
+						id="<?php echo $field_id; ?>"
+						class="button button-secondary jetpack_connection_button"
+						data-reset-url="<?php echo esc_url( $connection_url ); ?>"
+					>
+						<?php esc_html_e( 'Connect to Jetpack', 'woocommerce' ); ?>
+					</button>
+					<p class="description"><?php echo $field_description; ?></p>
+				</td>
+			</tr>
+			<?php
+		} else {
+			?>
+			<tr valign="top">
+				<th scope="row" class="titledesc">
+					<label for="<?php echo $field_id; ?>"><?php echo $field_title; ?></label>
+				</th>
+				<td class="forminp forminp-button">
+					<p class="description"><?php _e( 'Already connected to Jetpack', 'woocommerce' )  ?></p>
+				</td>
+			</tr>
+			<?php
+		}
 	}
 
 	/**
@@ -136,6 +210,12 @@ class AdminSettingsHandler {
 				if (!confirm('" . esc_js( __( 'Are you sure you want to reset all session clearances? This will require all users to clear their sessions again.', 'woocommerce' ) ) . "')) {
 					return;
 				}
+
+				var resetUrl = $(this).data('reset-url');
+				window.location.href = resetUrl;
+			});
+			$('.jetpack_connection_button').on('click', function(e) {
+				e.preventDefault();
 
 				var resetUrl = $(this).data('reset-url');
 				window.location.href = resetUrl;
