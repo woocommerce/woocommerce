1777f54d93792c098ac7b04721b815cfea6250cb
Radoslav Georgiev <rageorgiev@gmail.com>
2025-12-22 14:58:51 +0200

    Add a positive test

--- plugins/woocommerce/tests/php/src/Internal/ProductFeed/Integrations/POSCatalog/AsyncGeneratorTest.php
+++ plugins/woocommerce/tests/php/src/Internal/ProductFeed/Integrations/POSCatalog/AsyncGeneratorTest.php
@@ -120,4 +120,20 @@ class AsyncGeneratorTest extends \WC_Unit_Test_Case {
 
 		$this->assertFalse( $method->invoke( $this->sut, $status ) );
 	}
+
+	/**
+	 * Test that validate_status returns true for non-expired feeds.
+	 */
+	public function test_validate_status_returns_true_for_non_expired_feed() {
+		$status = array(
+			'state'        => AsyncGenerator::STATE_COMPLETED,
+			'path'         => __FILE__, // We just need a path that exists.
+			'completed_at' => time() + AsyncGenerator::FEED_EXPIRY,
+		);
+
+		$method = ( new ReflectionClass( $this->sut ) )->getMethod( 'validate_status' );
+		$method->setAccessible( true );
+
+		$this->assertTrue( $method->invoke( $this->sut, $status ) );
+	}
 }
