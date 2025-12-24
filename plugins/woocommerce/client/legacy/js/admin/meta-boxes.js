fcfb38748aa337b322c81c82f447f010b7c4341a
Ninos Ego <me@ninosego.de>
2025-12-24 03:41:18 +0100

    Don't disable submit button in meta boxes with empty textarea fields

--- plugins/woocommerce/client/legacy/js/admin/meta-boxes.js
+++ plugins/woocommerce/client/legacy/js/admin/meta-boxes.js
@@ -8,8 +8,9 @@ jQuery( function ( $ ) {
 		var has_empty_fields = false;
 		attributes_and_variations_data.each( function () {
 			var $this = $( this );
-			// Check if the field is checkbox or a search field.
+			// Check if the field is textarea, checkbox or a search field.
 			if (
+				$this.is( 'textarea' ) ||
 				$this.hasClass( 'checkbox' ) ||
 				$this.filter( '[class*=search__field]' ).length
 			) {
