6c611d94006cab1148f7dd94d1fb536331b1a2e3
Wesley Rosa <wesleyjrosa@gmail.com>
2025-12-26 12:54:16 -0300

    PayPal Standard e2e tests

---
+++ plugins/woocommerce/tests/e2e-pw/tests/paypal-standard/installation.spec.js
@@ -0,0 +1,30 @@
+/**
+ * Internal dependencies
+ */
+import { expect, tags, test } from '../../fixtures/fixtures';
+import { ADMIN_STATE_PATH } from '../../playwright.config';
+
+test.describe(
+	'PayPal Installation',
+	{ tag: [ tags.PAYMENTS, tags.PAYPAL ] },
+	() => {
+		test.use( { storageState: ADMIN_STATE_PATH } );
+
+		test( 'PayPal should be listed for install', async ( { page } ) => {
+			await page.goto( '/wp-admin/admin.php?page=wc-settings' );
+
+			await page.getByRole( 'link', { name: 'Payments' } ).click();
+
+			await page.waitForSelector(
+				'.settings-payment-gateways__header-title'
+			);
+
+			// Confirm PayPal is listed.
+			const paypalDiv = page.locator( '#_wc_pes_paypal_full_stack' );
+			await expect( paypalDiv ).toBeVisible();
+			await expect(
+				paypalDiv.getByRole( 'button', { name: 'Install' } )
+			).toBeVisible();
+		} );
+	}
+);
