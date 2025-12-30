ed4bc2b956317949b7002c649470bb54efc4c3fa
Devanshi Joshi <86941664+devanshijoshi9@users.noreply.github.com>
2025-12-30 13:54:33 +0530

    Fix: Add conditional checks to prevent the WooCommerce Accordion block from appearing in the block inserter on WordPress 6.9 and later

--- plugins/woocommerce/src/Blocks/BlockTypes/Accordion/AccordionGroup.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/Accordion/AccordionGroup.php
@@ -5,6 +5,7 @@ namespace Automattic\WooCommerce\Blocks\BlockTypes\Accordion;
 
 use Automattic\WooCommerce\Blocks\BlockTypes\AbstractBlock;
 use Automattic\WooCommerce\Blocks\BlockTypes\EnableBlockJsonAssetsTrait;
+use Automattic\WooCommerce\Blocks\Utils\Utils;
 
 /**
  * AccordionGroup class.
@@ -19,6 +20,46 @@ class AccordionGroup extends AbstractBlock {
 	 * @var string
 	 */
 	protected $block_name = 'accordion-group';
+
+	/**
+	 * Initialize this block type.
+	 *
+	 * - Hook into WP lifecycle.
+	 * - Register the block with WordPress.
+	 */
+	protected function initialize() {
+		add_filter( 'block_type_metadata_settings', array( $this, 'add_accordion_block_type_metadata_settings' ), 10, 2 );
+		parent::initialize();
+	}
+
+	/**
+	 * Modify block type metadata settings to hide accordion blocks from inserter on WordPress 6.9 or later.
+	 *
+	 * @param array $settings Array of determined settings for registering a block type.
+	 * @param array $metadata Metadata provided for registering a block type.
+	 *
+	 * @return array Modified settings.
+	 */
+	public function add_accordion_block_type_metadata_settings( $settings, $metadata ) {
+		// Check if this is the accordion group block.
+		if (
+			! empty( $metadata['name'] ) &&
+			'woocommerce/accordion-group' === $metadata['name']
+		) :
+			// Hide the accordion block from the inserter on WordPress 6.9 or later.
+			if ( Utils::wp_version_compare( '6.9', '>=' ) ) :
+				// Ensure supports array exists.
+				if ( ! isset( $settings['supports'] ) ) :
+					$settings['supports'] = array();
+				endif;
+
+				// Hide from inserter.
+				$settings['supports']['inserter'] = false;
+			endif;
+		endif;
+
+		return $settings;
+	}
 
 	/**
 	 * Include and render the block.
