a76be0bb3a87f02e91b9073a422edfa0102edd75
Mayisha <mayisha.alam@automattic.com>
2025-12-30 17:35:40 +0600

    fix lint

--- plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
+++ plugins/woocommerce/includes/gateways/paypal/includes/class-wc-gateway-paypal-request.php
@@ -798,7 +798,7 @@ class WC_Gateway_Paypal_Request {
 
 			$quantity = $item->get_quantity();
 			// PayPal does not accept zero or fractional quantities.
-			if ( 0 === $quantity || floor( $quantity ) != $quantity ) {
+			if ( 0 === $quantity || floor( $quantity ) != $quantity ) { // phpcs:ignore Universal.Operators.StrictComparisons.LooseNotEqual
 				return array();
 			}
 
