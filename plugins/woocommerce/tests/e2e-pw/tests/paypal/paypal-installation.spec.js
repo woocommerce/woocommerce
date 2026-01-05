2c49611cec39c8d4092c932642a4d23186f2575c
Wesley Rosa <wesleyjrosa@gmail.com>
2026-01-05 10:56:49 -0300

    Reducing PR scope

--- plugins/woocommerce/tests/e2e-pw/tests/paypal/paypal-installation.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/paypal/paypal-installation.spec.js
@@ -1,14 +1,8 @@
 /**
- * External dependencies
- */
-import { request } from '@playwright/test';
-
-/**
  * Internal dependencies
  */
 import { expect, tags, test } from '../../fixtures/fixtures';
 import { ADMIN_STATE_PATH } from '../../playwright.config';
-const { setOption } = require( '../../utils/options' );
 
 test.describe(
 	'PayPal Standard Installation',
@@ -52,35 +46,6 @@ test.describe(
 
 			// Confirm the Active label is present.
 			await expect( paypalDiv.getByText( 'Active' ) ).toBeVisible();
-		} );
-
-		test( 'PayPal Standard Orders V2 integration is available after onboarding to Jetpack', async ( {
-			page,
-			baseURL,
-		} ) => {
-			await setOption(
-				request,
-				baseURL,
-				'transact_onboarding_complete',
-				'yes'
-			);
-
-			await openPayments( page );
-
-			const paypalDiv = await waitForPayPalToLoad( page );
-
-			const manageButton = paypalDiv.getByRole( 'button', {
-				name: 'Manage',
-			} );
-			await expect( manageButton ).toBeVisible();
-
-			// Click the Manage button
-			await manageButton.click();
-
-			// Confirm the PayPal Buttons setting is present. It is only available for the Orders V2 integration.
-			await expect(
-				page.getByText( 'Enable PayPal Buttons' )
-			).toBeVisible();
 		} );
 	}
 );
