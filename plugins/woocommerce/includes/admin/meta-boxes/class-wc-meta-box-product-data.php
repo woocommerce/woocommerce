7578486ddabf358e9accb5c1e6460f2b50862299
malinajirka <malinajirka@gmail.com>
2026-01-07 16:52:18 +0100

    Update plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
    Co-authored-by: Radoslav Georgiev <rageorgiev@gmail.com>

--- plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
+++ plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
@@ -428,14 +428,12 @@ class WC_Meta_Box_Product_Data {
 		$product->delete_meta_data( '_product_template_id' );
 
 		// Handle product visibility in POS only if the field was submitted.
-		if ( isset( $_POST['_visible_in_pos'] ) ) {
-			$visible_in_pos = 'yes' === wc_clean( wp_unslash( $_POST['_visible_in_pos'] ) );
-			if ( $visible_in_pos ) {
-				wp_remove_object_terms( $post_id, 'pos-hidden', 'pos_product_visibility' );
-			} else {
-				wp_set_object_terms( $post_id, 'pos-hidden', 'pos_product_visibility' );
-			}
-		}
+        $visible_in_pos = isset( $_POST['_visible_in_pos'] ) && 'yes' === wc_clean( wp_unslash( $_POST['_visible_in_pos'] ) );
+        if ( $visible_in_pos ) {
+            wp_remove_object_terms( $post_id, 'pos-hidden', 'pos_product_visibility' );
+        } else {
+            wp_set_object_terms( $post_id, 'pos-hidden', 'pos_product_visibility' );
+        }
 
 		/**
 		 * Set props before save.
