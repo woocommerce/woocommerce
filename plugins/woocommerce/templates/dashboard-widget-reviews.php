ebfb3a059ddf9a662616820eb8422e0498df0ccb
Vladimir Reznichenko <kalessil@gmail.com>
2026-01-07 15:08:22 +0100

    Reviews: cleanup

--- plugins/woocommerce/templates/dashboard-widget-reviews.php
+++ plugins/woocommerce/templates/dashboard-widget-reviews.php
@@ -32,7 +32,7 @@ defined( 'ABSPATH' ) || exit;
  * @param string      $product_name The product name.
  * @param \WP_Comment $comment      The comment.
  */
-$product_title = apply_filters( 'woocommerce_admin_dashboard_recent_reviews', $product->get_name(), $comment )
+$product_name = apply_filters( 'woocommerce_admin_dashboard_recent_reviews', $product->get_name(), $comment )
 
 ?>
 
@@ -42,7 +42,7 @@ $product_title = apply_filters( 'woocommerce_admin_dashboard_recent_reviews', $p
 	<?php echo wc_get_rating_html( (int) get_comment_meta( $comment->comment_ID, 'rating', true ) ); ?>
 
 	<h4 class="meta">
-		<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><?php echo wp_kses_post( $product_title ); ?></a>
+		<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><?php echo wp_kses_post( $product_name ); ?></a>
 		<?php
 		/* translators: %s: review author */
 		echo sprintf( esc_html__( 'reviewed by %s', 'woocommerce' ), get_comment_author( $comment->comment_ID ) );
