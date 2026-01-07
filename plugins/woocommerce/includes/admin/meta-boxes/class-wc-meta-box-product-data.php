04213e911814f774f84113c968a7ac804f54b77b
malinajirka <malinajirka@gmail.com>
2026-01-07 17:02:48 +0100

    Fix lint/formatting in class-wc-meta-box-product-data.php

--- plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
+++ plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
@@ -427,12 +427,12 @@ class WC_Meta_Box_Product_Data {
 		// Remove _product_template_id for products that were created with the new product editor.
 		$product->delete_meta_data( '_product_template_id' );
 
-        $visible_in_pos = isset( $_POST['_visible_in_pos'] ) && 'yes' === wc_clean( wp_unslash( $_POST['_visible_in_pos'] ) );
-        if ( $visible_in_pos ) {
-            wp_remove_object_terms( $post_id, 'pos-hidden', 'pos_product_visibility' );
-        } else {
-            wp_set_object_terms( $post_id, 'pos-hidden', 'pos_product_visibility' );
-        }
+		$visible_in_pos = isset( $_POST['_visible_in_pos'] ) && 'yes' === wc_clean( wp_unslash( $_POST['_visible_in_pos'] ) );
+		if ( $visible_in_pos ) {
+			wp_remove_object_terms( $post_id, 'pos-hidden', 'pos_product_visibility' );
+		} else {
+			wp_set_object_terms( $post_id, 'pos-hidden', 'pos_product_visibility' );
+		}
 
 		/**
 		 * Set props before save.
