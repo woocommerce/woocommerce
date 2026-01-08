79f552f3258cb59f955b97229e700c4618180b8f
Seghir Nadir <nadir.seghir@gmail.com>
2026-01-08 12:45:43 +0100

    Update plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/form/use-form-validation.ts

--- plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/form/use-form-validation.ts
+++ plugins/woocommerce/client/blocks/assets/js/base/components/cart-checkout/form/use-form-validation.ts
@@ -118,7 +118,6 @@ export const useFormValidation = (
 	let values:
 		| BillingAddress
 		| ShippingAddress
-		| BillingAddress
 		| ContactFormValues
 		| OrderFormValues
 		| Record< string, never >;
