3736fc9258b4ed59b8981d3bfda591424e8f6270
malinajirka <malinajirka@gmail.com>
2026-01-08 14:28:51 +0100

    Guard POS visibility save logic with feature flag check
    When the POS feature is disabled, the _visible_in_pos checkbox is not rendered in the admin product edit UI. However, the save logic was executing unconditionally. This caused $_POST['_visible_in_pos'] to be unset, resulting in $visible_in_pos becoming false and unintentionally hiding products from POS.
    
    This fix wraps the POS visibility save logic with a feature flag check, matching the UI logic that also checks feature_is_enabled('point_of_sale') before rendering the checkbox. This prevents data loss when editing products while the POS feature is disabled.

--- plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
+++ plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
@@ -12,6 +12,7 @@ use Automattic\WooCommerce\Enums\ProductStatus;
 use Automattic\WooCommerce\Enums\ProductType;
 use Automattic\WooCommerce\Internal\CostOfGoodsSold\CostOfGoodsSoldController;
 use Automattic\WooCommerce\Internal\ProductFeed\Integrations\POSCatalog\POSProductVisibilitySync;
+use Automattic\WooCommerce\Utilities\FeaturesUtil;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -428,8 +429,10 @@ class WC_Meta_Box_Product_Data {
 		// Remove _product_template_id for products that were created with the new product editor.
 		$product->delete_meta_data( '_product_template_id' );
 
-		$visible_in_pos = isset( $_POST['_visible_in_pos'] ) && 'yes' === wc_clean( wp_unslash( $_POST['_visible_in_pos'] ) );
-		wc_get_container()->get( POSProductVisibilitySync::class )->set_product_pos_visibility( $post_id, $visible_in_pos );
+		if ( FeaturesUtil::feature_is_enabled( 'point_of_sale' ) ) {
+			$visible_in_pos = isset( $_POST['_visible_in_pos'] ) && 'yes' === wc_clean( wp_unslash( $_POST['_visible_in_pos'] ) );
+			wc_get_container()->get( POSProductVisibilitySync::class )->set_product_pos_visibility( $post_id, $visible_in_pos );
+		}
 
 		/**
 		 * Set props before save.
