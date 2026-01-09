eb25a6b90110d2190b1adf76e85b381cb77b8d0d
Vladimir Reznichenko <kalessil@gmail.com>
2026-01-09 10:50:13 +0100

    Reviews: address bots feedback.

--- plugins/woocommerce/templates/dashboard-widget-reviews.php
+++ plugins/woocommerce/templates/dashboard-widget-reviews.php
@@ -39,7 +39,7 @@ defined( 'ABSPATH' ) || exit;
 		<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><?php echo wp_kses_post( $product->get_name() ); ?></a>
 		<?php
 		/* translators: %s: review author */
-		printf( esc_html__( 'reviewed by %s', 'woocommerce' ), get_comment_author( $comment->comment_ID ) );
+		printf( esc_html__( 'reviewed by %s', 'woocommerce' ), esc_html( get_comment_author( $comment->comment_ID  ) ) );
 		?>
 	</h4>
 
