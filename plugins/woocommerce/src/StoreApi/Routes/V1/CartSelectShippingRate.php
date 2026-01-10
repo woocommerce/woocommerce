fb2ec01e0f8622673dd570e2055b1b7f0c74d7fe
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2026-01-10 13:49:09 -0300

    Removed blocks checkout shipping method tracker

--- plugins/woocommerce/src/StoreApi/Routes/V1/CartSelectShippingRate.php
+++ plugins/woocommerce/src/StoreApi/Routes/V1/CartSelectShippingRate.php
@@ -1,8 +1,6 @@
 <?php
 namespace Automattic\WooCommerce\StoreApi\Routes\V1;
 
-use Automattic\WooCommerce\Internal\FraudProtection\CheckoutEventTracker;
-use Automattic\WooCommerce\Internal\FraudProtection\FraudProtectionController;
 use Automattic\WooCommerce\StoreApi\Exceptions\RouteException;
 
 /**
@@ -108,13 +106,6 @@ class CartSelectShippingRate extends AbstractCartRoute {
 		 * @param \WP_REST_Request $request Full details about the request.
 		 */
 		do_action( 'woocommerce_store_api_cart_select_shipping_rate', $package_id, $rate_id, $request );
-
-		$cart->calculate_totals();
-
-		$container = wc_get_container();
-		if ( $container->get( FraudProtectionController::class )->feature_is_enabled() ) {
-			$container->get( CheckoutEventTracker::class )->track_blocks_checkout_shipping_method_update( $package_id, $rate_id );
-		}
 
 		return rest_ensure_response( $this->cart_schema->get_item_response( $cart ) );
 	}
