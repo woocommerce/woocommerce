971160aed89edbb0b66071a0669d8048cd61eac0
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2026-01-10 16:54:00 -0300

    Removed shipping data

--- plugins/woocommerce/src/Internal/FraudProtection/CheckoutEventTracker.php
+++ plugins/woocommerce/src/Internal/FraudProtection/CheckoutEventTracker.php
@@ -141,8 +141,6 @@ class CheckoutEventTracker implements RegisterHooksInterface {
 			$this->extract_payment_method( $collected_event_data ),
 		);
 
-		$event_data['shipping_methods'] = $collected_event_data['shipping_method'] ?? array();
-
 		return $event_data;
 	}
 
