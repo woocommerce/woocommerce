93b6197cde561790001eeba9e60d4c0041b1f818
Boro Sitnikovski <buritomath@gmail.com>
2026-01-12 16:34:50 +0100

    Change instanceof check to WC_Logger, per discussions

--- plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
+++ plugins/woocommerce/includes/gateways/paypal/class-wc-gateway-paypal.php
@@ -381,7 +381,7 @@ class WC_Gateway_Paypal extends WC_Payment_Gateway {
 			if ( empty( self::$log ) ) {
 				self::$log = wc_get_logger();
 			}
-			if ( self::$log instanceof WC_Logger_Interface && is_callable( array( self::$log, 'clear' ) ) ) {
+			if ( self::$log instanceof WC_Logger && is_callable( array( self::$log, 'clear' ) ) ) {
 				self::$log->clear( self::ID );
 			}
 		}
