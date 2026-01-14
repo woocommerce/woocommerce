65556bb537d626c35c94b426777affe8128b2f8e
Abdalsalaam Halawa <abdalsalaamnafez@gmail.com>
2026-01-15 00:20:40 +0100

    Add trait ProductLinksTrait Shared functionality for preparing product links including embeddable links for upsells, cross-sells, and related products

---
+++ plugins/woocommerce/src/StoreApi/Utilities/ProductLinksTrait.php
@@ -0,0 +1,72 @@
+<?php
+declare( strict_types = 1 );
+
+namespace Automattic\WooCommerce\StoreApi\Utilities;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * ProductLinksTrait
+ *
+ * Shared functionality for preparing product links including embeddable links for upsells, cross-sells, and related products.
+ */
+trait ProductLinksTrait {
+	/**
+	 * Prepare links for the request.
+	 *
+	 * @param \WC_Product      $item Product object.
+	 * @param \WP_REST_Request $request Request object.
+	 * @return array
+	 */
+	protected function prepare_links( $item, $request ): array {
+		$links = array(
+			'self'       => array(
+				'href' => rest_url( $this->get_namespace() . '/products/' . $item->get_id() ),
+			),
+			'collection' => array(
+				'href' => rest_url( $this->get_namespace() . '/products' ),
+			),
+		);
+
+		if ( $item->get_parent_id() ) {
+			$links['up'] = array(
+				'href' => rest_url( $this->get_namespace() . '/products/' . $item->get_parent_id() ),
+			);
+		}
+
+		$upsell_ids = $item->get_upsell_ids();
+		if ( ! empty( $upsell_ids ) ) {
+			$links['upsells'] = array(
+				'href'       => add_query_arg(
+					array( 'include' => implode( ',', $upsell_ids ) ),
+					rest_url( $this->get_namespace() . '/products' )
+				),
+				'embeddable' => true,
+			);
+		}
+
+		$cross_sell_ids = $item->get_cross_sell_ids();
+		if ( ! empty( $cross_sell_ids ) ) {
+			$links['cross_sells'] = array(
+				'href'       => add_query_arg(
+					array( 'include' => implode( ',', $cross_sell_ids ) ),
+					rest_url( $this->get_namespace() . '/products' )
+				),
+				'embeddable' => true,
+			);
+		}
+
+		$related_ids = wc_get_related_products( $item->get_id() );
+		if ( ! empty( $related_ids ) ) {
+			$links['related'] = array(
+				'href'       => add_query_arg(
+					array( 'include' => implode( ',', $related_ids ) ),
+					rest_url( $this->get_namespace() . '/products' )
+				),
+				'embeddable' => true,
+			);
+		}
+
+		return $links;
+	}
+}
