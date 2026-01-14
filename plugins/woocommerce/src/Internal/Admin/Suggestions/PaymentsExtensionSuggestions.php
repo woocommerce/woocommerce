f1f65836b1084f84f0a4a73ac260a5d9f0f688fe
oaratovskyi <oleksandr.aratovskyi@automattic.com>
2026-01-14 11:37:54 +0200

    Update ngenius icon and description

--- plugins/woocommerce/src/Internal/Admin/Suggestions/PaymentsExtensionSuggestions.php
+++ plugins/woocommerce/src/Internal/Admin/Suggestions/PaymentsExtensionSuggestions.php
@@ -4152,7 +4152,7 @@ class PaymentsExtensionSuggestions {
 			self::NGENIUS           => array(
 				'_type'       => self::TYPE_PSP,
 				'title'       => esc_html__( 'N-Genius Online', 'woocommerce' ),
-				'description' => esc_html__( 'Smart, secure, and seamless online payments for your store.', 'woocommerce' ),
+				'description' => esc_html__( 'Power your business with N-Genius Online—smart, secure, and built for the future', 'woocommerce' ),
 				'icon'        => plugins_url( 'assets/images/onboarding/icons/ngenius.svg', WC_PLUGIN_FILE ),
 				'plugin'      => array(
 					'_type' => self::PLUGIN_TYPE_WPORG,
