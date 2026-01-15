0d4da7c809241eaac01b1c1dbe66e6884162e404
Shahed <125728402+dev-shahed@users.noreply.github.com>
2026-01-15 16:30:35 +0600

    Update comment in PageController to clarify behavior regarding ID collisions in page options

--- plugins/woocommerce/src/Admin/PageController.php
+++ plugins/woocommerce/src/Admin/PageController.php
@@ -114,7 +114,7 @@ class PageController {
 		 */
 		$options = apply_filters( 'woocommerce_navigation_connect_page_options', $options );
 
-		// @todo check for null ID, or collision.
+		// @todo check for collision, but keep in mind that the current behavior is: the later call silently overwrites the earlier one.
 		if ( isset( $options['id'] ) ) {
 			$this->pages[ $options['id'] ] = $options;
 		}		
