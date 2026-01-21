d87c4bc9e97099301f590697fba5f0369e84efb9
Peter Wilson <git-commit-by@peterwilson.cc>
2026-01-22 10:55:50 +1100

    CS Fixes.

--- plugins/woocommerce/client/blocks/packages/checkout/utils/validation/validate-filter.ts
+++ plugins/woocommerce/client/blocks/packages/checkout/utils/validation/validate-filter.ts
@@ -14,10 +14,14 @@ export const productPriceValidation = ( value: string ) =>
  * to be open so the extensions can replace the text with their own in its
  * entirety.
  */
-export const productPriceScreenReaderValidation = ( value: string ): true | never => {
+export const productPriceScreenReaderValidation = (
+	value: string
+): true | never => {
 	if ( typeof value === 'string' && value.length > 0 ) {
 		return true;
 	}
 
-	throw Error( 'Returned value must be a non-empty string for product price screen reader format.' );
-}
+	throw Error(
+		'Returned value must be a non-empty string for product price screen reader format.'
+	);
+};
