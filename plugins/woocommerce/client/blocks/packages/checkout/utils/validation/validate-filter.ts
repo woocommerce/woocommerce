edc9a8e2054ab433b66b98f71e2fe91f45582639
Peter Wilson <git-commit-by@peterwilson.cc>
2026-01-23 10:03:01 +1100

    Filter value must contain placeholders.

--- plugins/woocommerce/client/blocks/packages/checkout/utils/validation/validate-filter.ts
+++ plugins/woocommerce/client/blocks/packages/checkout/utils/validation/validate-filter.ts
@@ -17,11 +17,8 @@ export const productPriceValidation = ( value: string ) =>
 export const productPriceScreenReaderValidation = (
 	value: string
 ): true | never => {
-	if ( typeof value === 'string' && value.length > 0 ) {
-		return true;
-	}
-
-	throw Error(
-		'Returned value must be a non-empty string for product price screen reader format.'
+	return (
+		mustContain( value, '<quantity/>' ) &&
+		mustContain( value, '<productName/>' )
 	);
 };
