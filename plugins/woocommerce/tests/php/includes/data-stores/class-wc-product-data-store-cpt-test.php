3d3d35bad41b453632f34a354bf946c6c480aeba
Vladimir Reznichenko <kalessil@gmail.com>
2026-01-23 13:20:46 +0100

    Products: cleanup.

--- plugins/woocommerce/tests/php/includes/data-stores/class-wc-product-data-store-cpt-test.php
+++ plugins/woocommerce/tests/php/includes/data-stores/class-wc-product-data-store-cpt-test.php
@@ -349,20 +349,20 @@ class WC_Product_Data_Store_CPT_Test extends WC_Unit_Test_Case {
 		$product->save();
 
 		$product_id = $product->get_id();
-		$this->assertSame( '-1', get_post_meta( $product_id, '_stock', true ) );
+		$this->assertSame( -1, get_post_meta( $product_id, '_stock', true ) );
 
 		$store->update_product_stock( $product_id, null, 'set' );
-		$this->assertSame( '0', get_post_meta( $product_id, '_stock', true ) );
+		$this->assertSame( 0, get_post_meta( $product_id, '_stock', true ) );
 		$store->update_product_stock( $product_id, 10, 'set' );
-		$this->assertSame( '10', get_post_meta( $product_id, '_stock', true ) );
+		$this->assertSame( 10, get_post_meta( $product_id, '_stock', true ) );
 		$store->update_product_stock( $product_id, 20.0, 'set' );
-		$this->assertSame( '20.000000', get_post_meta( $product_id, '_stock', true ) );
+		$this->assertSame( 20.0, get_post_meta( $product_id, '_stock', true ) );
 
 		$store->update_product_sales( $product_id, null, 'set' );
-		$this->assertSame( '0', get_post_meta( $product_id, 'total_sales', true ) );
+		$this->assertSame( 0, get_post_meta( $product_id, 'total_sales', true ) );
 		$store->update_product_sales( $product_id, 10, 'set' );
-		$this->assertSame( '10', get_post_meta( $product_id, 'total_sales', true ) );
+		$this->assertSame( 10, get_post_meta( $product_id, 'total_sales', true ) );
 		$store->update_product_sales( $product_id, 20.0, 'set' );
-		$this->assertSame( '20.000000', get_post_meta( $product_id, 'total_sales', true ) );
+		$this->assertSame( 20.0, get_post_meta( $product_id, 'total_sales', true ) );
 	}
 }
