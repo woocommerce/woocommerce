395d6b2d5472c90947a92017f85e1cddd79c6553
Ahmed <ahmed.el.azzabi@automattic.com>
2026-01-23 15:08:52 +0100

    Remove default given we have logging for this already

--- plugins/woocommerce/client/admin/client/settings/field-transformers.js
+++ plugins/woocommerce/client/admin/client/settings/field-transformers.js
@@ -200,14 +200,6 @@ export const baseFieldTransformer = ( setting ) => {
 					);
 				},
 			};
-		default:
-			// Return a read-only text field for unsupported field types
-			// This prevents silent failures when rendering unsupported fields
-			return {
-				id: setting.id,
-				type: 'text',
-				Edit: () => <div>To be implemented: { setting.type }</div>,
-			};
 	}
 };
 
