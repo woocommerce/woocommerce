666f65c95eb76c5466fc994238bf7e72f2798d68
Michael Pretty <prettyboymp@users.noreply.github.com>
2026-01-26 12:24:37 -0500

    Lint fixes

--- plugins/woocommerce/includes/wc-order-functions.php
+++ plugins/woocommerce/includes/wc-order-functions.php
@@ -1118,7 +1118,7 @@ function wc_cancel_unpaid_orders() {
 		foreach ( $unpaid_orders as $unpaid_order ) {
 			$order = wc_get_order( $unpaid_order );
 
-			if ( apply_filters( 'woocommerce_cancel_unpaid_order', in_array($order->get_created_via(), ['checkout', 'store-api'], true), $order ) ) {
+			if ( apply_filters( 'woocommerce_cancel_unpaid_order', in_array( $order->get_created_via(), array( 'checkout', 'store-api' ), true ), $order ) ) {
 				$order->update_status( OrderStatus::CANCELLED, __( 'Unpaid order cancelled - time limit reached.', 'woocommerce' ) );
 			}
 		}
