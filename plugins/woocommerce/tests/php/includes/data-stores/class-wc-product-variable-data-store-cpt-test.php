e6b02a09b1f10599b51b8de1b1a130e29ec92384
Vladimir Reznichenko <kalessil@gmail.com>
2026-01-27 11:23:22 +0100

    Products: code style tweaks.

--- plugins/woocommerce/tests/php/includes/data-stores/class-wc-product-variable-data-store-cpt-test.php
+++ plugins/woocommerce/tests/php/includes/data-stores/class-wc-product-variable-data-store-cpt-test.php
@@ -740,13 +740,13 @@ class WC_Product_Variable_Data_Store_CPT_Test extends WC_Unit_Test_Case {
 					'name'         => $attribute_name,
 					'value'        => '...',
 					'is_variation' => 1,
-				)
+				),
 			)
 		);
 
 		$attribute_meta_update_counter = 0;
 		$root_cause_update_counter     = 0;
-		$callback = function( ?bool $check, int $object_id, string $meta_key, $meta_value ) use ( $new_slug, &$attribute_meta_update_counter, &$root_cause_update_counter ) {
+		$callback                      = function ( ?bool $check, int $object_id, string $meta_key, $meta_value ) use ( $new_slug, &$attribute_meta_update_counter, &$root_cause_update_counter ) {
 			$root_cause_update_counter     += (int) ( isset( $meta_value['size-style'] ) && ! isset( $meta_value['Size/Style'] ) );
 			$attribute_meta_update_counter += (int) ( $new_slug === $meta_key && '...' === $meta_value );
 			return $check;
