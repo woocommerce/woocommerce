a7e00a04b1f9cd6f654b47d092705c2a9c1afb97
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2026-01-27 16:45:10 +0100

    Introduce taxonomy picker

---
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-collection/edit/TaxonomyPicker.tsx
@@ -0,0 +1,184 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Icon, category, tag } from '@wordpress/icons';
+import { Placeholder, Button } from '@wordpress/components';
+import { useBlockProps } from '@wordpress/block-editor';
+import ProductTagControl from '@woocommerce/editor-components/product-tag-control';
+import ProductCategoryControl from '@woocommerce/editor-components/product-category-control';
+
+/**
+ * Internal dependencies
+ */
+import type { ProductCollectionEditComponentProps } from '../types';
+import { CoreCollectionNames } from '../types';
+import { getCollectionByName } from '../collections';
+import { setQueryAttribute } from '../utils';
+import TaxonomyControls from './inspector-controls/taxonomy-controls';
+
+interface TaxonomyPickerProps extends ProductCollectionEditComponentProps {
+	onDone: () => void;
+}
+
+/**
+ * Get the taxonomy slug for a given collection.
+ */
+export const getTaxonomySlugForCollection = (
+	collection: string | undefined
+): string | null => {
+	switch ( collection ) {
+		case CoreCollectionNames.BY_CATEGORY:
+			return 'product_cat';
+		case CoreCollectionNames.BY_TAG:
+			return 'product_tag';
+		case CoreCollectionNames.BY_BRAND:
+			return 'product_brand';
+		default:
+			return null;
+	}
+};
+
+/**
+ * Get the description text for a given collection.
+ */
+const getDescriptionForCollection = (
+	collection: string | undefined
+): string => {
+	switch ( collection ) {
+		case CoreCollectionNames.BY_CATEGORY:
+			return __(
+				'Display a grid of products from your selected categories.',
+				'woocommerce'
+			);
+		case CoreCollectionNames.BY_TAG:
+			return __(
+				'Display a grid of products from your selected tags.',
+				'woocommerce'
+			);
+		case CoreCollectionNames.BY_BRAND:
+			return __(
+				'Display a grid of products from your selected brands.',
+				'woocommerce'
+			);
+		default:
+			return __(
+				'Select taxonomy terms to display products from.',
+				'woocommerce'
+			);
+	}
+};
+
+/**
+ * Get the icon for a given collection.
+ */
+const getIconForCollection = ( collection: string | undefined ) => {
+	switch ( collection ) {
+		case CoreCollectionNames.BY_CATEGORY:
+			return category;
+		case CoreCollectionNames.BY_TAG:
+			return tag;
+		default:
+			// For brands and others, use category icon as fallback
+			return category;
+	}
+};
+
+const TaxonomyPicker = ( props: TaxonomyPickerProps ) => {
+	const { attributes, onDone } = props;
+	const blockProps = useBlockProps();
+
+	const collectionData = getCollectionByName( attributes.collection );
+	const taxonomySlug = getTaxonomySlugForCollection( attributes.collection );
+
+	// Get selected term IDs for the relevant taxonomy
+	const selectedTermIds: number[] = taxonomySlug
+		? attributes.query?.taxQuery?.[ taxonomySlug ] || []
+		: [];
+
+	const hasSelectedTerms = selectedTermIds.length > 0;
+
+	if ( ! collectionData || ! taxonomySlug ) {
+		return null;
+	}
+
+	const handleTermChange = ( termIds: number[] ) => {
+		setQueryAttribute( props, {
+			taxQuery: {
+				...attributes.query?.taxQuery,
+				[ taxonomySlug ]: termIds,
+			},
+		} );
+	};
+
+	const renderTaxonomyControl = () => {
+		switch ( attributes.collection ) {
+			case CoreCollectionNames.BY_CATEGORY:
+				return (
+					<ProductCategoryControl
+						selected={ selectedTermIds }
+						onChange={ ( value = [] ) => {
+							const ids = value.map(
+								( { id }: { id: number } ) => id
+							);
+							handleTermChange( ids );
+						} }
+					/>
+				);
+			case CoreCollectionNames.BY_TAG:
+				return (
+					<ProductTagControl
+						selected={ selectedTermIds }
+						onChange={ ( value = [] ) => {
+							const ids = value.map( ( item ) =>
+								Number( item.id )
+							);
+							handleTermChange( ids );
+						} }
+					/>
+				);
+			case CoreCollectionNames.BY_BRAND:
+				// For brands, use the generic TaxonomyControls
+				return (
+					<TaxonomyControls
+						query={ attributes.query }
+						setQueryAttribute={ ( queryUpdate ) => {
+							setQueryAttribute( props, queryUpdate );
+						} }
+						trackInteraction={ () => {} }
+						collection={ attributes.collection }
+						renderMode="standalone"
+					/>
+				);
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div { ...blockProps }>
+			<Placeholder
+				icon={
+					// @ts-expect-error Icon types are incomplete
+					<Icon
+						icon={ getIconForCollection( attributes.collection ) }
+						className="block-editor-block-icon"
+					/>
+				}
+				label={ collectionData.title }
+			>
+				{ getDescriptionForCollection( attributes.collection ) }
+				<div className="wc-blocks-product-collection__editor-taxonomy-picker-selection">
+					{ renderTaxonomyControl() }
+					{ hasSelectedTerms && (
+						<Button variant="primary" onClick={ onDone }>
+							{ __( 'Done', 'woocommerce' ) }
+						</Button>
+					) }
+				</div>
+			</Placeholder>
+		</div>
+	);
+};
+
+export default TaxonomyPicker;
