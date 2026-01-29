c8788e79409ba0efafd58eac85e6b8b6849644ee
Vladimir Reznichenko <kalessil@gmail.com>
2026-01-29 09:55:53 +0100

    Blocks: cleanup.

--- plugins/woocommerce/src/Blocks/BlockTypes/RelatedProducts.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/RelatedProducts.php
@@ -145,7 +145,7 @@ class RelatedProducts extends AbstractBlock {
 	 */
 	private function is_related_products_block( $parsed_block, $rendered_block = null ) {
 		$is_product_collection_block = $rendered_block->context['query']['isProductCollectionBlock'] ?? false;
-		if ( ProductQuery::is_woocommerce_variation( $parsed_block ) && isset( $parsed_block['attrs']['namespace'] ) && 'woocommerce/related-products' === $parsed_block['attrs']['namespace'] && ! $is_product_collection_block ) {
+		if ( ! $is_product_collection_block && 'woocommerce/related-products' === ( $parsed_block['attrs']['namespace'] ?? null ) && ProductQuery::is_woocommerce_variation( $parsed_block ) ) {
 			return true;
 		}
 
@@ -170,15 +170,9 @@ class RelatedProducts extends AbstractBlock {
 
 		$related_products_ids = wc_get_related_products( $product->get_id(), $product_per_page, $product->get_upsell_ids() );
 		if ( ! empty( $related_products_ids ) ) {
-			// Optimization: reduce the number of SQLs needed to populate product objects.
-			_prime_post_caches( $related_products_ids );
-
 			$related_products = array_filter( array_map( 'wc_get_product', $related_products_ids ), 'wc_products_array_filter_visible' );
 			$related_products = wc_products_array_orderby( $related_products, 'rand', 'desc' );
 			/** @var \WC_Product[] $related_products */ // phpcs:ignore Generic.Commenting.DocComment.MissingShort
-
-			// Optimization: reduce the number of SQLs needed to fetch images when rendering.
-			_prime_post_caches( array_filter( array_map( fn( $product ) => $product->get_image_id(), $related_products ) ) );
 
 			$related_products_ids = array_map( fn( $product ) => $product->get_id(), $related_products );
 		}
