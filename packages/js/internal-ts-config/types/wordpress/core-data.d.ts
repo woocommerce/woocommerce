9f15df487202949548a235c0248747bbd27bceb2
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2026-01-29 21:16:28 -0800

    Removed Top-Level Imports
    This way these are _only_ side-effect imports with ambient
    declarations. It protects the types!

--- packages/js/internal-ts-config/types/wordpress/core-data.d.ts
+++ packages/js/internal-ts-config/types/wordpress/core-data.d.ts
@@ -1,8 +1,23 @@
-import type { store } from '@wordpress/core-data/build-types';
-
 declare module '@wordpress/data' {
+	import type * as coreDataSelectors from '@wordpress/core-data/build-types/selectors';
+	import type * as coreDataActions from '@wordpress/core-data/build-types/actions';
+	import type {
+		StoreDescriptor,
+		ReduxStoreConfig,
+	} from '@wordpress/data/build-types/types';
+
+	type CoreDataConfig = ReduxStoreConfig<
+		unknown,
+		{ [ K in keyof typeof coreDataActions ]: ( typeof coreDataActions )[ K ] },
+		{ [ K in keyof typeof coreDataSelectors ]: ( typeof coreDataSelectors )[ K ] }
+	>;
+
+	type CoreDataStoreDescriptor = StoreDescriptor< CoreDataConfig > & {
+		name: 'core';
+	};
+
 	interface StoreRegistry {
-		core: typeof store;
+		core: CoreDataStoreDescriptor;
 	}
 }
 
--- packages/js/internal-ts-config/types/wordpress/notices.d.ts
+++ packages/js/internal-ts-config/types/wordpress/notices.d.ts
@@ -1,7 +1,22 @@
-import type { store } from '@wordpress/notices/build-types/store';
-
 declare module '@wordpress/data' {
+	import type * as noticesSelectors from '@wordpress/notices/build-types/store/selectors';
+	import type * as noticesActions from '@wordpress/notices/build-types/store/actions';
+	import type {
+		StoreDescriptor,
+		ReduxStoreConfig,
+	} from '@wordpress/data/build-types/types';
+
+	type NoticesConfig = ReduxStoreConfig<
+		unknown,
+		{ [ K in keyof typeof noticesActions ]: ( typeof noticesActions )[ K ] },
+		{ [ K in keyof typeof noticesSelectors ]: ( typeof noticesSelectors )[ K ] }
+	>;
+
+	type NoticesStoreDescriptor = StoreDescriptor< NoticesConfig > & {
+		name: 'core/notices';
+	};
+
 	interface StoreRegistry {
-		'core/notices': typeof store;
+		'core/notices': NoticesStoreDescriptor;
 	}
 }
