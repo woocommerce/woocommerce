a588a4ea6cfb655fa1a7f0845c92d762a2ed978c
Brandon Kraft <public@brandonkraft.com>
2026-02-02 16:53:50 -0600

    Remove orphaned email tracker REST endpoint
    Follow-up to #55660 which removed the merchant email notes feature.
    The `/wc-analytics/admin/notes/tracker/{note_id}/user/{user_id}` endpoint
    and its handler were missed during that cleanup.
    
    This endpoint was used to track email opens via an embedded tracking pixel,
    but since the email notification system was removed, this endpoint no longer
    serves any purpose.

--- plugins/woocommerce/src/Admin/API/Notes.php
+++ plugins/woocommerce/src/Admin/API/Notes.php
@@ -116,19 +116,6 @@ class Notes extends \WC_REST_CRUD_Controller {
 
 		register_rest_route(
 			$this->namespace,
-			'/' . $this->rest_base . '/tracker/(?P<note_id>[\d-]+)/user/(?P<user_id>[\d-]+)',
-			array(
-				array(
-					'methods'             => \WP_REST_Server::READABLE,
-					'callback'            => array( $this, 'track_opened_email' ),
-					'permission_callback' => '__return_true',
-				),
-				'schema' => array( $this, 'get_public_item_schema' ),
-			)
-		);
-
-		register_rest_route(
-			$this->namespace,
 			'/' . $this->rest_base . '/update',
 			array(
 				array(
@@ -607,21 +594,6 @@ class Notes extends \WC_REST_CRUD_Controller {
 		 * @since 3.9.0
 		 */
 		return apply_filters( 'woocommerce_rest_prepare_note', $response, $data, $request );
-	}
-
-
-	/**
-	 * Track opened emails.
-	 *
-	 * @param WP_REST_Request $request Request object.
-	 */
-	public function track_opened_email( $request ) {
-		$note = NotesRepository::get_note( $request->get_param( 'note_id' ) );
-		if ( ! $note ) {
-			return;
-		}
-
-		NotesRepository::record_tracks_event_with_user( $request->get_param( 'user_id' ), 'email_note_opened', array( 'note_name' => $note->get_name() ) );
 	}
 
 	/**
