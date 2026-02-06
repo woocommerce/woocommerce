9082dbea3a4df82a1b37b5bc8d3cf917d2719e76
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2026-02-06 19:31:36 +0300

    Fix wrong bump of email template version

--- plugins/woocommerce/templates/emails/plain/email-fulfillment-details.php
+++ plugins/woocommerce/templates/emails/plain/email-fulfillment-details.php
@@ -12,7 +12,7 @@
  *
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails\Plain
- * @version 10.6.0
+ * @version 10.1.0
  */
 
 defined( 'ABSPATH' ) || exit;
