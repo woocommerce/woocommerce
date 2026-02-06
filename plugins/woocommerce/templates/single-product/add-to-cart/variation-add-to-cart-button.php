fee45592cae83c403523b1e684e12821fd97c2da
Albert Juhé Lluveras <contact@albertjuhe.com>
2026-02-06 18:20:32 +0100

    Only disable add-to-cart button in variable products when wc-add-to-cart-variation is enqueued

--- plugins/woocommerce/templates/single-product/add-to-cart/variation-add-to-cart-button.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -2,6 +2,13 @@
 /**
  * Single variation cart button
  *
+ * Variables available to callers of wc_get_template() for this template:
+ *
+ * @var bool $add_to_cart_button_disabled Optional. When provided, overrides whether the add to cart button
+ *                                        is initially disabled. If not set, defaults to true when the
+ *                                        wc-add-to-cart-variation script is enqueued (so the button can be
+ *                                        enabled once a variation is selected).
+ *
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
  * @version 10.5.0
@@ -10,6 +17,10 @@
 defined( 'ABSPATH' ) || exit;
 
 global $product;
+
+$is_add_to_cart_button_disabled = isset( $add_to_cart_button_disabled )
+	? (bool) $add_to_cart_button_disabled
+	: wp_script_is( 'wc-add-to-cart-variation', 'enqueued' );
 ?>
 <div class="woocommerce-variation-add-to-cart variations_button">
 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
@@ -28,7 +39,7 @@ global $product;
 	do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
 
-	<button type="submit" class="single_add_to_cart_button button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" disabled><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+	<button type="submit" class="single_add_to_cart_button button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"<?php echo $is_add_to_cart_button_disabled ? ' disabled' : ''; ?>><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
 
 	<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 
