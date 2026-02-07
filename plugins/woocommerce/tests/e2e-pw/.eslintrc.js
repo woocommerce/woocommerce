0d74d7f9ed161fb3076308ae9a5195d2eb3da5a1
Luca Tumedei <luca@theaveragedev.com>
2026-02-07 16:07:37 +0100

    Update .eslintrc.js to support TypeScript files and cross-package imports

--- plugins/woocommerce/tests/e2e-pw/.eslintrc.js
+++ plugins/woocommerce/tests/e2e-pw/.eslintrc.js
@@ -11,5 +11,26 @@ module.exports = {
 		'jest/no-standalone-expect': 'off',
 		'jest/valid-title': 'off',
 		'testing-library/await-async-utils': 'off',
+		/*
+		 * The e2e-pw tests use dependencies from the parent woocommerce package.
+		 * This configuration tells ESLint to check both the local package.json
+		 * and the parent package.json when validating imports.
+		 */
+		'import/no-extraneous-dependencies': [
+			'warn',
+			{
+				packageDir: [ '.', '../..' ],
+			},
+		],
 	},
+	overrides: [
+		{
+			files: [ '**/*.ts', '**/*.tsx' ],
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
+		},
+	],
 };
