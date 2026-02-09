28d0690a52bbede01d180f873f34697e790f5d84
Vladimir Reznichenko <kalessil@gmail.com>
2026-02-09 13:01:52 +0100

    Admin: new test cases.

---
+++ plugins/woocommerce/tests/php/includes/admin/class-wc-admin-reports-test.php
@@ -0,0 +1,32 @@
+<?php
+
+declare( strict_types = 1);
+
+/**
+ * Tests for `WC_Admin_Reports` class.
+ */
+final class WC_Admin_Reports_Test extends WC_Unit_Test_Case {
+	/**
+	 * Verify the workflows execution in `delete_legacy_reports_transients`.
+	 */
+	public function test_delete_legacy_reports_transients(): void {
+		// Verify the integration point invocation.
+		$this->assertTrue( has_action( 'woocommerce_delete_shop_order_transients', array( \WC_Admin_Reports::class, 'delete_legacy_reports_transients' ), 10 ) );
+		$this->assertTrue( has_action( 'woocommerce_delete_legacy_report_transients' ) );
+
+		// Cleanup.
+		as_unschedule_action( 'woocommerce_delete_legacy_report_transients', null, 'woocommerce' );
+
+		// Verify the defer-workflow.
+		$this->assertFalse( as_has_scheduled_action( 'woocommerce_delete_legacy_report_transients', null, 'woocommerce' ) );
+		\WC_Admin_Reports::delete_legacy_reports_transients( 0 );
+		$this->assertTrue( as_has_scheduled_action( 'woocommerce_delete_legacy_report_transients', null, 'woocommerce' ) );
+
+		// Cleanup.
+		as_unschedule_action( 'woocommerce_delete_legacy_report_transients', null, 'woocommerce' );
+
+		// Verify the purge-workflow.
+		do_action( 'woocommerce_delete_legacy_report_transients', 0, true );
+		$this->assertFalse( as_has_scheduled_action( 'woocommerce_delete_legacy_report_transients', null, 'woocommerce' ) );
+	}
+}
