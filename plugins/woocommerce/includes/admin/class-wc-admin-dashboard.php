1a48bfc5f3b555e30e092d973034585249508ee6
Vladimir Reznichenko <kalessil@gmail.com>
2026-02-10 14:35:27 +0100

    Admin: please CI.

--- plugins/woocommerce/includes/admin/class-wc-admin-dashboard.php
+++ plugins/woocommerce/includes/admin/class-wc-admin-dashboard.php
@@ -529,7 +529,7 @@ if ( ! class_exists( 'WC_Admin_Dashboard', false ) ) :
 			);
 			$comments = array_filter(
 				$comments,
-				static fn( \WP_Comment $comment ) => current_user_can( 'read_product', $comment->comment_post_ID ) && ! post_password_required( $comment->comment_post_ID )
+				static fn( \WP_Comment $comment ) => current_user_can( 'read_product', $comment->comment_post_ID ) && ! post_password_required( (int) $comment->comment_post_ID )
 			);
 			if ( $comments ) {
 				echo '<ul>';
