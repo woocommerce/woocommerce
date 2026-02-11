8f5039759a05976c7cd49896814b88e21ca6ac5a
Wesley Rosa <wesleyjrosa@gmail.com>
2026-02-11 15:55:20 -0300

    Attempting coderabbit suggestion to lock tests

--- plugins/woocommerce/tests/e2e-pw/fixtures/paypal-fixtures.js
+++ plugins/woocommerce/tests/e2e-pw/fixtures/paypal-fixtures.js
@@ -1,12 +1,28 @@
 /**
+ * External dependencies
+ */
+import fs from 'fs';
+import path from 'path';
+
+/**
  * Internal dependencies
  */
 import { test as baseTest } from './fixtures';
 import { ADMIN_STATE_PATH } from '../playwright.config';
 import { wpCLI } from '../utils/cli';
 
+const LOCK_FILE = path.join( __dirname, '../../.state/paypal.lock' );
+
 export const test = baseTest.extend( {
 	page: async ( { page }, use ) => {
+		// Wait for lock to be available
+		while ( fs.existsSync( LOCK_FILE ) ) {
+			await new Promise( ( resolve ) => setTimeout( resolve, 1000 ) );
+		}
+
+		// Acquire lock
+		fs.writeFileSync( LOCK_FILE, process.pid.toString() );
+
 		await wpCLI(
 			"wp option patch update woocommerce_paypal_settings _should_load 'yes'"
 		);
@@ -16,6 +32,13 @@ export const test = baseTest.extend( {
 		await wpCLI(
 			"wp option patch update woocommerce_paypal_settings _should_load 'no'"
 		);
+
+		// Release lock
+		try {
+			fs.unlinkSync( LOCK_FILE );
+		} catch ( e ) {
+			// Lock already released
+		}
 	},
 	storageState: ADMIN_STATE_PATH,
 } );
