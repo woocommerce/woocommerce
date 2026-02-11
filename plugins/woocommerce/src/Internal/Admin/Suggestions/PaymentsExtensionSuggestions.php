4e8040dd7dbfde041452a206bb3165c4575d268c
oaratovskyi <oleksandr.aratovskyi@automattic.com>
2026-02-11 14:17:11 +0200

    Update N-Genius name description as requested from partners.

--- plugins/woocommerce/src/Internal/Admin/Suggestions/PaymentsExtensionSuggestions.php
+++ plugins/woocommerce/src/Internal/Admin/Suggestions/PaymentsExtensionSuggestions.php
@@ -4151,8 +4151,8 @@ class PaymentsExtensionSuggestions {
 			),
 			self::NGENIUS           => array(
 				'_type'       => self::TYPE_PSP,
-				'title'       => esc_html__( 'N-Genius Online', 'woocommerce' ),
-				'description' => esc_html__( 'Power your business with N-Genius Online—smart, secure, and built for the future', 'woocommerce' ),
+				'title'       => esc_html__( 'N-Genius Online by Network', 'woocommerce' ),
+				'description' => esc_html__( 'Power your business with N-Genius Online by Network—smart, secure, and built for the future', 'woocommerce' ),
 				'icon'        => plugins_url( 'assets/images/onboarding/icons/ngenius.svg', WC_PLUGIN_FILE ),
 				'plugin'      => array(
 					'_type' => self::PLUGIN_TYPE_WPORG,
