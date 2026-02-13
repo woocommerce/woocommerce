c1f7b8ec46594a816bf006f97242bd63ab4d83c0
Vladimir Reznichenko <kalessil@gmail.com>
2026-02-13 10:23:02 +0100

    Order: rework checkout draft handling.

--- plugins/woocommerce/includes/wc-order-functions.php
+++ plugins/woocommerce/includes/wc-order-functions.php
@@ -509,15 +509,20 @@ function wc_delete_shop_order_transients( $order = 0 ) {
 	if ( $order && is_a( $order, 'WC_Order' ) ) {
 		$order_id = $order->get_id();
 
-		// Optimization note: in the vanilla core the checkout draft is not considered by the SQLs populating those metas so
-		// we skip the purge and underlying SQLs. For backward compatibility we additionally validate if extensions are
-		// altering the states via `wc_order_statuses` hook and adding the status in there - if so, the purge is necessary.
-		$purge_usermeta = OrderStatus::CHECKOUT_DRAFT !== $order->get_status() || array_key_exists( OrderInternalStatus::CHECKOUT_DRAFT, wc_get_order_statuses() );
-		if ( $purge_usermeta ) {
-			$customer_id = $order->get_customer_id();
-			Users::delete_site_user_meta( $customer_id, 'wc_money_spent' );
-			Users::delete_site_user_meta( $customer_id, 'wc_order_count' );
-			Users::delete_site_user_meta( $customer_id, 'wc_last_order' );
+		$customer_id = $order->get_customer_id();
+		if ( $customer_id ) {
+			// Optimization note: the function is fired multiple times during order persistence lifecycle, and by
+			// verifying that metas carry non-empty values we ensure no repetitive attempts dropping the metas.
+			$memorized_count = Users::get_site_user_meta( $customer_id, 'wc_order_count' );
+			$memorized_last  = Users::get_site_user_meta( $customer_id, 'wc_last_order' );
+			if ( $memorized_count || $memorized_last ) {
+				Users::delete_site_user_meta( $customer_id, 'wc_order_count' );
+				Users::delete_site_user_meta( $customer_id, 'wc_last_order' );
+			}
+			// Optimisation note: total spent is calculated for orders with a paid status, which checkout drafts are not.
+			if ( OrderStatus::CHECKOUT_DRAFT !== $order->get_status() ) {
+				Users::delete_site_user_meta( $customer_id, 'wc_money_spent' );
+			}
 		}
 	}
 
