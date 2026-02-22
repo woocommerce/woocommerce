06e9c0216bf6916512c270d370426cfb9308b4a2
Vladimir Reznichenko <kalessil@gmail.com>
2026-02-22 19:04:25 +0100

    Blocks: cleanup.

--- plugins/woocommerce/tests/php/src/Blocks/Utils/BlockTemplateUtilsTest.php
+++ plugins/woocommerce/tests/php/src/Blocks/Utils/BlockTemplateUtilsTest.php
@@ -356,8 +356,7 @@ class BlockTemplateUtilsTest extends WP_UnitTestCase {
 		$this->assertSame( array(), wp_cache_get( 'wp_template_part-ids', 'woocommerce_blocks' )['twentytwentytwo'] ?? null );
 		$this->assertCount( 0, $templates );
 
-		wp_cache_delete( 'wp_template-ids', 'woocommerce_blocks' );
-		wp_cache_delete( 'wp_template_part-ids', 'woocommerce_blocks' );
+		wp_cache_delete_multiple( array( 'wp_template-ids', 'wp_template_part-ids' ), 'woocommerce_blocks' );
 	}
 
 	/**
