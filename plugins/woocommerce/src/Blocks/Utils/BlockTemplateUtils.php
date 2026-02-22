75f99131b8beed0f9d9ee8e894cbc931aa79ff53
Vladimir Reznichenko <kalessil@gmail.com>
2026-02-22 18:40:53 +0100

    Blocks: incorporate theme in the caching.

--- plugins/woocommerce/src/Blocks/Utils/BlockTemplateUtils.php
+++ plugins/woocommerce/src/Blocks/Utils/BlockTemplateUtils.php
@@ -718,11 +718,13 @@ class BlockTemplateUtils {
 		$bypass_cache               = false;
 
 		// Optimization note: first query, which optimized for fetching IDs, to minimize temporary/filesort overhead.
-		$ids = wp_cache_get( $template_type . '-ids', 'woocommerce_blocks' );
-		if ( false === $ids || $bypass_cache ) {
+		$theme = get_stylesheet();
+		$ids   = wp_cache_get( $template_type . '-ids', 'woocommerce_blocks' );
+		if ( ! isset( $ids[ $theme ] ) || $bypass_cache ) {
 			// 'post__not_in' guarantees using `type_status_date` index on the posts table; as the table grows the
 			// template type entries still remain a handful (small selectivity) enabling this query performance.
-			$ids = ( new \WP_Query(
+			$ids           = false === $ids ? array() : $ids;
+			$ids[ $theme ] = ( new \WP_Query(
 				array(
 					'post_type'      => $template_type,
 					'post__not_in'   => array( 0 ),
@@ -733,24 +735,23 @@ class BlockTemplateUtils {
 						array(
 							'taxonomy' => 'wp_theme',
 							'field'    => 'name',
-							'terms'    => array( self::DEPRECATED_PLUGIN_SLUG, self::PLUGIN_SLUG, get_stylesheet() ),
+							'terms'    => array( self::DEPRECATED_PLUGIN_SLUG, self::PLUGIN_SLUG, $theme ),
 						),
 					),
 				)
 			) )->posts;
 			// 12 hours is half of default nonce lifetime, if out of sync, templates operating with nonce will keep running.
 			wp_cache_set( $template_type . '-ids', $ids, 'woocommerce_blocks', 12 * HOUR_IN_SECONDS );
-			$request_level_cache[ $template_type ] = null;
+			$request_level_cache[ $template_type ][ $theme ] = null;
 		}
 
 		// Optimization note: second query, which optimized for fetching templates data, to minimize grouping overhead.
-		if ( null === ( $request_level_cache[ $template_type ] ?? null ) ) {
-			$request_level_cache[ $template_type ] = empty( $ids ) ? array() : ( new \WP_Query(
+		if ( null === ( $request_level_cache[ $template_type ][ $theme ] ?? null ) ) {
+			$request_level_cache[ $template_type ][ $theme ] = empty( $ids ) ? array() : ( new \WP_Query(
 				array(
 					'post_type'              => $template_type,
 					'post__in'               => $ids,
 					'posts_per_page'         => -1,
-					//'orderby'                => 'none',
 					'no_found_rows'          => true,
 					'update_post_meta_cache' => false,
 				)
@@ -758,7 +759,7 @@ class BlockTemplateUtils {
 		}
 
 		// Optimization note: populate template objects; optimized for subsequent calls, without spawning consequent SQLs.
-		$saved_templates = $request_level_cache[ $template_type ];
+		$saved_templates = $request_level_cache[ $template_type ][ $theme ];
 		if ( ! empty( $saved_templates ) && is_array( $slugs ) && array() !== $slugs ) {
 			$saved_templates = array_filter( $saved_templates, fn( $template ) => in_array( $template->post_name, $slugs, true ) );
 		}
