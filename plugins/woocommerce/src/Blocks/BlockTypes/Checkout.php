df6290636e30457ce24b9b809258487cfc1e767c
Vladimir Reznichenko <kalessil@gmail.com>
2026-02-23 11:34:49 +0100

    Blocks: trim more SQLs.

--- plugins/woocommerce/src/Blocks/BlockTypes/Checkout.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/Checkout.php
@@ -443,7 +443,7 @@ class Checkout extends AbstractBlock {
 				FILTER_VALIDATE_BOOLEAN
 			)
 		);
-		// Optimization note: reduce the number of SQLs required to fetch the options in the lines below.
+		// Optimization note: reduce the number of SQLs required to fetch the options in the lines below (1 instead of 4).
 		wp_prime_option_caches(
 			array(
 				'woocommerce_enable_checkout_login_reminder',
