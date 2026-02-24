5fc4b75b7604dea338117069403d0d4027741d44
Seun Olorunsola <30554163+triple0t@users.noreply.github.com>
2026-02-24 08:54:19 +0100

    Cherry-pick e6dae7256bab67905d0987c256c68e44a3334053 with unresolved conflicts from #63415

--- packages/js/email-editor/assets/rich-text.asset.php
+++ packages/js/email-editor/assets/rich-text.asset.php
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 <?php return array('dependencies' => array('wp-a11y', 'wp-compose', 'wp-data', 'wp-deprecated', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-keycodes'), 'version' => '6249fa45d0f64d6a0eda');
+=======
+<?php
+// Deprecated: Do not use. This file will be deleted after 2026-08-24. Use the version available in WordPress core.
+return array('dependencies' => array('wp-a11y', 'wp-compose', 'wp-data', 'wp-deprecated', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-keycodes'), 'version' => '6249fa45d0f64d6a0eda');
+>>>>>>> e6dae7256b (Follow up to wooprd 2309 (#63415))
