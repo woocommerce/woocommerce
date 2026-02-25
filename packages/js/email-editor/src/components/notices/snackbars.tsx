625da62fcd3a3cb77e038fa2f6774e4cedc6ade3
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2026-02-25 15:19:36 +0200

    Rework email editor notices overrides

--- packages/js/email-editor/src/components/notices/snackbars.tsx
+++ packages/js/email-editor/src/components/notices/snackbars.tsx
@@ -1,9 +1,7 @@
 /**
  * External dependencies
  */
-import { useMemo } from '@wordpress/element';
 import { SnackbarList } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { store as noticesStore } from '@wordpress/notices';
 
@@ -17,48 +15,11 @@ export function EditorSnackbars( { context = 'email-editor' } ) {
 		[ context ]
 	);
 
-	// Some global notices are not suitable for the email editor context
-	// This map allows us to change the content of the notice
-	const globalNoticeChangeMap = useMemo( () => {
-		return {
-			'site-editor-save-success': {
-				content: __( 'Email design updated.', 'woocommerce' ),
-				removeActions: true,
-			},
-			'editor-save': {
-				content: __( 'Email saved.', 'woocommerce' ),
-				removeActions: false,
-				contentCheck: ( notice ) => {
-					// eslint-disable-next-line @wordpress/i18n-text-domain
-					return notice.content.includes( __( 'Post updated.' ) ); // It is intentionally without domain to match core translation
-				},
-			},
-		};
-	}, [] );
-
 	const { removeNotice } = useDispatch( noticesStore );
 
-	const snackbarNotices = notices
-		.filter( ( { type } ) => type === 'snackbar' )
-		.map( ( notice ) => {
-			if ( ! globalNoticeChangeMap[ notice.id ] ) {
-				return notice;
-			}
-			if (
-				globalNoticeChangeMap[ notice.id ].contentCheck &&
-				! globalNoticeChangeMap[ notice.id ].contentCheck( notice )
-			) {
-				return notice;
-			}
-			return {
-				...notice,
-				content: globalNoticeChangeMap[ notice.id ].content,
-				spokenMessage: globalNoticeChangeMap[ notice.id ].content,
-				actions: globalNoticeChangeMap[ notice.id ].removeActions
-					? []
-					: notice.actions,
-			};
-		} );
+	const snackbarNotices = notices.filter(
+		( { type } ) => type === 'snackbar'
+	);
 
 	return (
 		<SnackbarList
