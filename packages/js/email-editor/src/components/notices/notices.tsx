bc3de5732cf2f6e09d395699889d9f1b975326e6
Yuliyan Slavchev <yuliyan.slavchev@gmail.com>
2026-02-25 15:30:06 +0200

    Remove EditorSnackbars in favor of SnackbarNotices

--- packages/js/email-editor/src/components/notices/notices.tsx
+++ packages/js/email-editor/src/components/notices/notices.tsx
@@ -3,13 +3,12 @@
  */
 import { NoticeList } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { store as noticesStore } from '@wordpress/notices';
+import { store as noticesStore, SnackbarNotices } from '@wordpress/notices';
 
 /**
  * Internal dependencies
  */
 import { ValidationNotices } from './validation-notices';
-import { EditorSnackbars } from './snackbars';
 import { NoticesSlot } from '../../hacks/notices-slot';
 
 // See: https://github.com/WordPress/gutenberg/blob/5be0ec4153c3adf9f0f2513239f4f7a358ba7948/packages/editor/src/components/editor-notices/index.js
@@ -46,8 +45,14 @@ export function EditorNotices() {
 				/>
 				<ValidationNotices />
 			</NoticesSlot>
-			<EditorSnackbars context="global" />
-			<EditorSnackbars context="email-editor" />
+			<SnackbarNotices
+				className="components-editor-notices__snackbar"
+				context="global"
+			/>
+			<SnackbarNotices
+				className="components-editor-notices__snackbar"
+				context="email-editor"
+			/>
 		</>
 	);
 }
