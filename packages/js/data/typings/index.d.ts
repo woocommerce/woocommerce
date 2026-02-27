7c94f230d51e99276fdc0fd0108c2f44ace6fcb5
Christopher Allford <6451942+ObliviousHarmony@users.noreply.github.com>
2026-02-26 17:34:42 -0800

    Add data store type augmentations to @woocommerce/data

--- packages/js/data/typings/index.d.ts
+++ packages/js/data/typings/index.d.ts
@@ -1,8 +1,3 @@
-/**
- * External dependencies
- */
-import '@woocommerce/internal-ts-config/types/@wordpress/data';
-import '@woocommerce/internal-ts-config/types/@wordpress/core-data';
 import { Entity } from '@wordpress/core-data';
 
 declare module 'rememo';
