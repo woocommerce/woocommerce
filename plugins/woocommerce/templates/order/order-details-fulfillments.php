10bd74967b5e5552ad7f14b0f67c10aeb8e3e39d
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2026-02-28 15:38:45 +0300

    Update version number to 10.7.0

--- plugins/woocommerce/templates/order/order-details-fulfillments.php
+++ plugins/woocommerce/templates/order/order-details-fulfillments.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 10.1.0
+ * @version 10.7.0
  *
  * @var bool $show_downloads Controls whether the downloads table should be rendered.
  */
