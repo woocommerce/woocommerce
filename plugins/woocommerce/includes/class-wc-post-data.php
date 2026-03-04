9fd08af3bf5d114d8bcb5758ae775e71e77df982
Vladimir Reznichenko <kalessil@gmail.com>
2026-03-04 10:14:41 +0100

    Blocks: address code review feedback.

--- plugins/woocommerce/includes/class-wc-post-data.php
+++ plugins/woocommerce/includes/class-wc-post-data.php
@@ -38,7 +38,7 @@ class WC_Post_Data {
 	 */
 	public static function init() {
 		add_action( 'clean_post_cache', array( __CLASS__, 'invalidate_products_last_modified' ), 10, 2 );
-		add_action( 'clean_post_cache', array( __CLASS__, 'invalidate_db_block_patterns_cache' ), 10, 2 );
+		add_action( 'clean_post_cache', array( __CLASS__, 'invalidate_db_block_templates_cache'), 10, 2 );
 		add_filter( 'post_type_link', array( __CLASS__, 'variation_post_link' ), 10, 2 );
 		add_action( 'shutdown', array( __CLASS__, 'do_deferred_product_sync' ), 10 );
 		add_action( 'set_object_terms', array( __CLASS__, 'force_default_term' ), 10, 5 );
@@ -192,7 +192,7 @@ class WC_Post_Data {
 	 *
 	 * @return void
 	 */
-	public static function invalidate_db_block_patterns_cache( $post_id, $post ): void {
+	public static function invalidate_db_block_templates_cache( $post_id, $post ): void {
 		if ( $post instanceof \WP_Post && in_array( $post->post_type, array( 'wp_template_part', 'wp_template' ), true ) ) {
 			wp_cache_delete( $post->post_type . '-ids', 'woocommerce_blocks' );
 		}
