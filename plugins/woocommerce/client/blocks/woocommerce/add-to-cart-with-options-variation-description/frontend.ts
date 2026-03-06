6d113555f70d85ad47b440ab886b3fb2bacdfe11
Dat Hoang <htdat@users.noreply.github.com>
2026-03-06 16:37:22 +0700

    Revert "Fix tsconfig indentation and add variation description frontend entrypoint"
    This reverts commit 5212d71c7a4bea67d966f6f57d1d579ec2a3382c.

--- plugins/woocommerce/client/blocks/woocommerce/add-to-cart-with-options-variation-description/frontend.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Frontend entrypoint for the `woocommerce/add-to-cart-with-options-variation-description` block.
- *
- * This file must exist because the block's `block.json` declares a `viewScriptModule`.
- * If/when frontend behavior is needed, implement it here.
- */
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export {};
