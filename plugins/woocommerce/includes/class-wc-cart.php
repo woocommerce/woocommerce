d7369da07b96ed9c16ca66cee6cde5a383c56c35
Sam Najian <dev.samnajian@gmail.com>
2026-03-11 20:14:29 +0100

    Cleanup the logic around showing the package number

--- plugins/woocommerce/includes/class-wc-cart.php
+++ plugins/woocommerce/includes/class-wc-cart.php
@@ -1723,6 +1723,20 @@ class WC_Cart extends WC_Legacy_Cart {
 	 * @return string
 	 */
 	private function get_shipping_package_name( $package, $index, $total_packages ) {
+
+		$shipping_package_name = _x( 'Shipment', 'shipping packages', 'woocommerce' );
+
+		/**
+		 * If there are multiple packages, use the index to show the package number.
+		 */
+		if ( 1 !== $total_packages ) {
+			$shipping_package_name = sprintf(
+				/* translators: %d: shipping package number. */
+				_x( 'Shipment %d', 'shipping packages', 'woocommerce' ),
+				$index
+			);
+		}
+
 		/**
 		 * Filters the shipping package name.
 		 *
@@ -1730,19 +1744,15 @@ class WC_Cart extends WC_Legacy_Cart {
 		 * @param string $shipping_package_name Shipping package name.
 		 * @param string $package_id Shipping package ID.
 		 * @param array $package Shipping package from WooCommerce.
+		 * @param int $total_packages Total number of shipping packages.
 		 * @return string Shipping package name.
 		 */
 		return apply_filters(
 			'woocommerce_shipping_package_name',
-
-				sprintf(
-					/* translators: %d: shipping package number */
-					_x( 'Shipment %d', 'shipping packages', 'woocommerce' ),
-					$index
-				),
-
+			$shipping_package_name,
 			$package['package_id'],
-			$package
+			$package,
+			$total_packages
 		);
 	}
 
