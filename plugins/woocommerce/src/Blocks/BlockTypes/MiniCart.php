076eb8af8583a4a398df5d49afbe1d1b1982e16f
Serhio Foo (S.K) <desoft@i.ua>
2026-03-11 16:57:05 +0200

    Back to data-wp-init--mark-as-hydrated

--- plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
@@ -608,7 +608,7 @@ class MiniCart extends AbstractBlock {
 				data-wp-on-document--wc-blocks_added_to_cart---open-drawer="actions.openDrawer"
 				<?php endif; ?>
 				data-wp-watch="callbacks.disableScrollingOnBody"
-				data-wp-init---mark-as-hydrated="callbacks.markAsHydrated"
+				data-wp-init--mark-as-hydrated="callbacks.markAsHydrated"
 				<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 				<?php echo wp_interactivity_data_wp_context( $context ); ?>
 				class="<?php echo esc_attr( $wrapper_classes ); ?>"
