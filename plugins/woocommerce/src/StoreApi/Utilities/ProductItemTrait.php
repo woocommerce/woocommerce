e2da516a97831d76349167ac7f793ec833c41d01
Tiago <77335756+tiago-123@users.noreply.github.com>
2026-03-13 12:18:54 -0300

    Apply filter to variation option name in ProductItemTrait (with the appropriate PHPDoc comment).

--- plugins/woocommerce/src/StoreApi/Utilities/ProductItemTrait.php
+++ plugins/woocommerce/src/StoreApi/Utilities/ProductItemTrait.php
@@ -50,6 +50,20 @@ trait ProductItemTrait {
 				// If this is a term slug, get the term's nice name.
 				$term = get_term_by( 'slug', $value, $taxonomy );
 				if ( ! is_wp_error( $term ) && $term && $term->name ) {
+
+					/**
+                     * Filters the variation option name for taxonomy attributes.
+                     *
+                     * @since 9.7.0
+                     *
+                     * @internal Matches filter name in WooCommerce core.
+                     *
+                     * @param string      $name     The term name to display.
+                     * @param \WP_Term    $term     Term object.
+                     * @param string      $taxonomy Taxonomy name.
+                     * @param \WC_Product $product  Product data.
+                     * @return string
+                     */
 					$value = apply_filters( 'woocommerce_variation_option_name', $term->name, $term, $taxonomy, $product );
 				}
 				$label = wc_attribute_label( $taxonomy );
