06b2f9b350130e7d13a9bbeafb50d1ffabe4187b
kalessil <kalessil@gmail.com>
2026-03-16 20:04:04 +0100

    Performance: consolidate the pattern usage across codebase.

--- plugins/woocommerce/includes/class-wc-product-factory.php
+++ plugins/woocommerce/includes/class-wc-product-factory.php
@@ -41,7 +41,8 @@ class WC_Product_Factory {
 				return $product;
 			}
 		}
-		_prime_post_caches( array( $product_id ) );
+		// Prime caches to reduce future queries.
+	_prime_post_caches( array( $product_id ) );
 
 		$product_type = self::get_product_type( $product_id );
 
