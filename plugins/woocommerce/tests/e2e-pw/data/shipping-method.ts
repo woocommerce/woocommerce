e674d490318296b7bf93d74cc2a781bfa45d6953
Luca Tumedei <luca.tumedei@automattic.com>
2026-03-16 16:25:37 +0100

    Add ShippingMethodId union type for better IDE suggestions

--- plugins/woocommerce/tests/e2e-pw/data/shipping-method.ts
+++ plugins/woocommerce/tests/e2e-pw/data/shipping-method.ts
@@ -1,21 +1,21 @@
 /**
  * Constructs a shipping method based on the given `methodId` and `cost`.
  *
- * `methodId` should be one of the following:
- * - `free_shipping`
- * - `flat_rate`
- * - `local_pickup`
- *
- * @param {string}  methodId
- * @param {number=} cost
+ * @param {ShippingMethodId} methodId
+ * @param {number=}          cost
  * @return {ShippingMethodExample} Shipping method object that can serve as a request payload for adding a shipping method to a shipping zone.
  */
+type ShippingMethodId = 'free_shipping' | 'flat_rate' | 'local_pickup';
+
 interface ShippingMethodExample {
-	method_id: string;
+	method_id: ShippingMethodId;
 	settings?: { cost: number };
 }
 
-export const getShippingMethodExample = ( methodId: string, cost?: number ) => {
+export const getShippingMethodExample = (
+	methodId: ShippingMethodId,
+	cost?: number
+) => {
 	const shippingMethodExample: ShippingMethodExample = {
 		method_id: methodId,
 	};
