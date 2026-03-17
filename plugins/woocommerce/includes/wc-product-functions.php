c657e620297b24af3e128a9b224eced8301bb499
Bhavik Tank <bhaviktank1999@gmail.com>
2026-03-17 17:15:01 +0530

    fix: ensure product_cat query var is safely validated before use

--- plugins/woocommerce/includes/wc-product-functions.php
+++ plugins/woocommerce/includes/wc-product-functions.php
@@ -398,9 +398,14 @@ function wc_product_canonical_redirect(): void {
 
 	// In the event we are dealing with ugly permalinks, this will be empty.
 	$specified_category_slug = get_query_var( 'product_cat' );
-	$specified_category_slug = urldecode( $specified_category_slug );
 
-	if ( ! is_string( $specified_category_slug ) || strlen( $specified_category_slug ) < 1 ) {
+	if ( ! is_string( $specified_category_slug ) ) {
+		return;
+	}
+
+	$specified_category_slug = urldecode( $specified_category_slug );
+	
+	if ( $specified_category_slug === '' ) {
 		return;
 	}
 
