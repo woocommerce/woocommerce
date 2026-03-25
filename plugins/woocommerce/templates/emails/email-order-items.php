ab82e158e6791699b2ca8f47e09a910ed0d0c0fa
Josh Heald <josh.heald@automattic.com>
2026-03-25 14:21:42 -0400

    Make product name match alignment of price/qty.

--- plugins/woocommerce/templates/emails/email-order-items.php
+++ plugins/woocommerce/templates/emails/email-order-items.php
@@ -43,7 +43,7 @@ foreach ( $items as $item_id => $item ) :
 
 	?>
 	<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_order_item_class', 'order_item', $item, $order ) ); ?>">
-		<td class="td font-family text-align-left" style="vertical-align: <?php echo $block_email_editor_enabled ? 'top' : 'middle'; ?>; word-wrap:break-word;">
+		<td class="td font-family text-align-left" style="vertical-align: middle; word-wrap:break-word;">
 			<?php if ( $email_improvements_enabled ) { ?>
 				<table class="order-item-data" role="presentation">
 					<tr>
