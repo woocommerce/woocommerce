cbeff2cf29e5c3f352becd970e199e54bc9c7880
Peter Petrov <peter.petrov89@gmail.com>
2026-03-26 15:57:27 +0200

    Bump versions for updated templates

--- plugins/woocommerce/templates/emails/email-order-items.php
+++ plugins/woocommerce/templates/emails/email-order-items.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
- * @version 10.7.0
+ * @version 10.7.1
  */
 
 use Automattic\WooCommerce\Utilities\FeaturesUtil;
--- plugins/woocommerce/templates/emails/plain/email-order-items.php
+++ plugins/woocommerce/templates/emails/plain/email-order-items.php
@@ -12,7 +12,7 @@
  *
  * @see         https://woocommerce.com/document/template-structure/
  * @package     WooCommerce\Templates\Emails\Plain
- * @version     9.8.0
+ * @version     9.8.1
  */
 
 use Automattic\WooCommerce\Utilities\FeaturesUtil;
