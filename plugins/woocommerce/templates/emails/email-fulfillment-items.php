db5f447c8b33c6066017cbca24a52a256bf03efd
Peter Petrov <peter.petrov89@gmail.com>
2026-03-26 15:13:03 +0200

    Fix empty quantity output handling in email order item templates

--- plugins/woocommerce/templates/emails/email-fulfillment-items.php
+++ plugins/woocommerce/templates/emails/email-fulfillment-items.php
@@ -145,7 +145,6 @@ foreach ( $items as $item_id => $item ) :
 		</td>
 		<td class="td font-family text-align-<?php echo esc_attr( $price_text_align ); ?>" style="vertical-align:middle;">
 			<?php
-			echo '&times;';
 			$qty         = $item->qty;
 			$qty_display = esc_html( $qty );
 			/**
@@ -153,7 +152,10 @@ foreach ( $items as $item_id => $item ) :
 			 *
 			 * @since 2.4.0
 			 */
-			echo wp_kses_post( apply_filters( 'woocommerce_email_order_item_quantity', $qty_display, $item->item ) );
+			$quantity = wp_kses_post( apply_filters( 'woocommerce_email_order_item_quantity', $qty_display, $item->item ) );
+			if ( $quantity !== '' ) {
+				echo '&times; ' . $quantity;
+			}
 			?>
 		</td>
 		<td class="td font-family text-align-<?php echo esc_attr( $price_text_align ); ?>" style="vertical-align:middle;">
--- plugins/woocommerce/templates/emails/email-order-items.php
+++ plugins/woocommerce/templates/emails/email-order-items.php
@@ -193,7 +193,6 @@ foreach ( $items as $item_id => $item ) :
 		</td>
 		<td class="td font-family text-align-<?php echo esc_attr( $price_text_align ); ?>" style="vertical-align:middle;">
 			<?php
-			echo $email_improvements_enabled ? '&times;' : '';
 			$qty          = $item->get_quantity();
 			$refunded_qty = $order->get_qty_refunded_for_item( $item_id );
 
@@ -202,7 +201,11 @@ foreach ( $items as $item_id => $item ) :
 			} else {
 				$qty_display = esc_html( $qty );
 			}
-			echo wp_kses_post( apply_filters( 'woocommerce_email_order_item_quantity', $qty_display, $item ) );
+			$quantity = wp_kses_post( apply_filters( 'woocommerce_email_order_item_quantity', $qty_display, $item ) );
+			if ( $quantity !== '' ) {
+				$quantity_prefix = $email_improvements_enabled ? '&times;' : '';
+				echo $quantity_prefix . $quantity;
+			}
 			?>
 		</td>
 		<td class="td font-family text-align-<?php echo esc_attr( $price_text_align ); ?>" style="vertical-align:middle;">
