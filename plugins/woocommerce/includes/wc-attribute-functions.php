b4cacb1cb8d016984e8eb0d60b07597cbd305d0c
Robert Felty <robfelty@gmail.com>
2026-03-31 16:49:30 +0200

    Fix fatal error with product attributes

--- plugins/woocommerce/includes/wc-attribute-functions.php
+++ plugins/woocommerce/includes/wc-attribute-functions.php
@@ -18,6 +18,12 @@ defined( 'ABSPATH' ) || exit;
  * @return array
  */
 function wc_get_text_attributes( $raw_attributes ) {
+	if ( is_array( $raw_attributes ) ) {
+        $raw_attributes = implode( WC_DELIMITER, array_filter( array_map( 'wp_json_encode', $raw_attributes ) ) );
+    }
+    if ( ! is_string( $raw_attributes ) ) {
+        return array();
+    }
 	return array_filter( array_map( 'trim', explode( WC_DELIMITER, html_entity_decode( $raw_attributes, ENT_QUOTES, get_bloginfo( 'charset' ) ) ) ), 'wc_get_text_attributes_filter_callback' );
 }
 
