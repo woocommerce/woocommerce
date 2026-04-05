3db7abefb6aa82ddb6d6ed30f0178f4c006a4fa9
Bhavik Tank <bhaviktank1999@gmail.com>
2026-04-05 18:57:05 +0530

    feat: add the test again

--- plugins/woocommerce/tests/php/src/Blocks/BlockTypes/ProductCollection/QueryBuilder.php
+++ plugins/woocommerce/tests/php/src/Blocks/BlockTypes/ProductCollection/QueryBuilder.php
@@ -1231,4 +1231,22 @@ class QueryBuilder extends \WP_UnitTestCase {
 
 		return $clauses;
 	}
+
+	/**
+	 * Tests that empty string values for perPage and offset are handled correctly.
+	 * This ensures the null coalescing and integer casting work as expected 
+	 * when a key exists but contains an empty value.
+	 */
+	public function test_per_page_and_offset_empty_string_handling() {
+		$parsed_block = Utils::get_base_parsed_block();
+		
+		// Set values as empty strings.
+		$parsed_block['attrs']['query']['perPage'] = '';
+		$parsed_block['attrs']['query']['offset']  = '';
+
+		$merged_query = Utils::initialize_merged_query( $this->block_instance, $parsed_block );
+
+		$this->assertEquals( 0, $merged_query['offset'] );
+		$this->assertEquals( 9, $merged_query['posts_per_page'] );
+	}
 }
