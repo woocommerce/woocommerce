ca8ab692311dcd310d432b687eb91a429e329ad0
raicem <cem.unalan@automattic.com>
2026-04-08 23:14:57 +0300

    Keep wc_translate_user_roles legacy version guard

--- plugins/woocommerce/includes/wc-user-functions.php
+++ plugins/woocommerce/includes/wc-user-functions.php
@@ -1157,6 +1157,11 @@ function wc_update_user_last_active( $user_id ) {
  * @return string
  */
 function wc_translate_user_roles( $translation, $text, $context, $domain ) {
+	// translate_user_role() only accepts a second parameter starting in WP 5.2.
+	if ( version_compare( get_bloginfo( 'version' ), '5.2', '<' ) ) {
+		return $translation;
+	}
+
 	if ( 'User role' === $context && 'default' === $domain && in_array( $text, array( 'Shop manager', 'Customer' ), true ) ) {
 		return translate_user_role( $text, 'woocommerce' );
 	}
