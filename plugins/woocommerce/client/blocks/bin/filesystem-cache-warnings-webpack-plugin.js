87b0da5eda7cb9ed7f9cc351b11fe7ae721367c4
Vladimir Reznichenko <kalessil@gmail.com>
2026-04-10 12:46:04 +0200

    Monorepo: fix webpack caching issues in blocks.

---
+++ plugins/woocommerce/client/blocks/bin/filesystem-cache-warnings-webpack-plugin.js
@@ -0,0 +1,24 @@
+// ExternalModule instances (wp.*, wc.* externals) are not serializable by webpack's PackFileCacheStrategy.
+// Suppress here since ignoreWarnings only affects compilation warnings, not infrastructure logs.
+
+function FilesystemCacheWarningsPlugin() {}
+
+FilesystemCacheWarningsPlugin.prototype.apply = function ( compiler ) {
+	compiler.hooks.infrastructureLog.tap(
+		'SuppressExternalModuleCacheWarning',
+		( name, type, args ) => {
+			if (
+				type === 'warn' &&
+				name === 'webpack.cache.PackFileCacheStrategy'
+			) {
+				return (
+					args[ 0 ]?.includes?.( 'ModuleExternalInitFragment' ) ||
+					args[ 0 ]?.includes?.( 'ExternalModule' ) ||
+					args[ 0 ]?.includes?.( 'Warning' )
+				);
+			}
+		}
+	);
+};
+
+module.exports = FilesystemCacheWarningsPlugin;
