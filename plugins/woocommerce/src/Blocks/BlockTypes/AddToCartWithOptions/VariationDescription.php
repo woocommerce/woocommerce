60fc1dff3e7d183f532e0aa958ac9d85d50d291b
David <david.arenas@automattic.com>
2026-04-13 12:11:43 +0200

    Hide variation description when it doesn't exist

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/VariationDescription.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/VariationDescription.php
@@ -44,7 +44,7 @@ class VariationDescription extends AbstractBlock {
 
 		$wrapper_attributes = array(
 			'data-wp-interactive'  => 'woocommerce/product-elements',
-			'data-wp-bind--hidden' => 'woocommerce/products::!state.productInContext.description',
+			'data-wp-bind--hidden' => 'woocommerce/products::!state.selectedVariation.description',
 			'aria-live'            => 'polite',
 			'aria-atomic'          => 'true',
 		);
