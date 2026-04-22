23120150c5789ada8c3b0d606cc62225a5d785ca
Vladimir Reznichenko <kalessil@gmail.com>
2026-04-22 12:31:54 +0200

    Troubleshooting: tests passing locally but failing in CI.

--- plugins/woocommerce/includes/class-wc-checkout.php
+++ plugins/woocommerce/includes/class-wc-checkout.php
@@ -563,9 +563,9 @@ class WC_Checkout {
 			// Order model compositions: inject the order and product instances.
 			if ( $item instanceof WC_Order_Item_Product ) {
 				$item->set_order( $order ); // Earlier order instance injection aiming set_backorder_meta logic.
-				if ( $product ) {
-					$item->set_product( $product );
-				}
+				// if ( $product ) {
+				// 	$item->set_product( $product );
+				// }
 			}
 
 			$item->set_backorder_meta();
