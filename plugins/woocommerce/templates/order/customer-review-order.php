bf07269f28df15678e44845ccca2c5a7ac26a35d
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2026-05-01 03:48:54 +0300

    Avoid empty parens when order date is unavailable on Review Order page
    Address Copilot review: when an order has no creation date, the meta
    string previously rendered as 'Order #123 ()'. Use a separate
    translatable string in that case.

--- plugins/woocommerce/templates/order/customer-review-order.php
+++ plugins/woocommerce/templates/order/customer-review-order.php
@@ -33,16 +33,26 @@ $customer_email  = $order->get_billing_email();
 $order_number    = $order->get_order_number();
 $order_date_text = $date_created ? wc_format_datetime( $date_created ) : '';
 
+if ( '' !== $order_date_text ) {
+	$order_summary = sprintf(
+		/* translators: 1: order number, 2: order date */
+		__( 'Order #%1$s (%2$s)', 'woocommerce' ),
+		$order_number,
+		$order_date_text
+	);
+} else {
+	$order_summary = sprintf(
+		/* translators: %s: order number */
+		__( 'Order #%s', 'woocommerce' ),
+		$order_number
+	);
+}
+
 $meta_parts = array_filter(
 	array(
 		$customer_name,
 		$customer_email,
-		sprintf(
-			/* translators: 1: order number, 2: order date */
-			__( 'Order #%1$s (%2$s)', 'woocommerce' ),
-			$order_number,
-			$order_date_text
-		),
+		$order_summary,
 	)
 );
 
