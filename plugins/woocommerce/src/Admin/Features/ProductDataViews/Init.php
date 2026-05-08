16fd25d8c61aa45a0dca6e9ff1a2c2565b1e0823
Luigi Teschio <gigitux@gmail.com>
2026-05-08 14:02:15 +0200

    remove priority

--- plugins/woocommerce/src/Admin/Features/ProductDataViews/Init.php
+++ plugins/woocommerce/src/Admin/Features/ProductDataViews/Init.php
@@ -27,8 +27,8 @@ class Init {
 				}
 			);
 
-			add_filter( 'admin_footer_text', '__return_empty_string', PHP_INT_MAX );
-			add_filter( 'update_footer', '__return_empty_string', PHP_INT_MAX );
+			add_filter( 'admin_footer_text', '__return_empty_string' );
+			add_filter( 'update_footer', '__return_empty_string' );
 		}
 	}
 
