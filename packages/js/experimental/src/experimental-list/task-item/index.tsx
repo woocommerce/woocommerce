eb483f0cbeb26b9e19de58a232af55b966f30388
kalessil <kalessil@gmail.com>
2026-05-08 12:25:58 +0200

    Monorepo: fix circular dependencies surfaced by swc.

--- packages/js/experimental/src/experimental-list/task-item/index.tsx
+++ packages/js/experimental/src/experimental-list/task-item/index.tsx
@@ -18,7 +18,8 @@ import { sanitizeHTML } from '@woocommerce/sanitize';
 /**
  * Internal dependencies
  */
-import { Text, ListItem } from '../../';
+import { Text } from '@wordpress/components';
+import { ExperimentalListItem as ListItem } from '../experimental-list-item';
 import { VerticalCSSTransition } from '../../vertical-css-transition';
 
 const ALLOWED_TAGS = [ 'a', 'b', 'em', 'i', 'strong', 'p', 'br' ];
