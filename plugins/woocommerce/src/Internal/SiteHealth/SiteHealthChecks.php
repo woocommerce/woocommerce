6cf6f31af723e8e78cbb8adc250dc05a06cebc20
Beau Lebens <hi@beaulebens.com>
2026-05-08 11:03:55 -0600

    Point HTTPS Site Health check action link to WooCommerce docs

--- plugins/woocommerce/src/Internal/SiteHealth/SiteHealthChecks.php
+++ plugins/woocommerce/src/Internal/SiteHealth/SiteHealthChecks.php
@@ -344,7 +344,7 @@ class SiteHealthChecks {
 				'description' => '<p>' . esc_html__( 'Your store should use HTTPS so checkout and account data are protected in transit.', 'woocommerce' ) . '</p>',
 				'actions'     => sprintf(
 					'<p><a href="%s">%s</a></p>',
-					esc_url( 'https://wordpress.org/documentation/article/why-should-i-use-https/' ),
+					esc_url( 'https://woocommerce.com/document/ssl-and-https/' ),
 					esc_html__( 'Learn more about HTTPS', 'woocommerce' )
 				),
 				'test'        => 'woocommerce_https',
