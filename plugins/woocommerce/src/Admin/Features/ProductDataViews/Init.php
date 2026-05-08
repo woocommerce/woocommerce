b3e99daab72888b4bc7139b318078965fbf3fab2
Luigi Teschio <gigitux@gmail.com>
2026-05-08 14:07:15 +0200

    fix priority

--- plugins/woocommerce/src/Admin/Features/ProductDataViews/Init.php
+++ plugins/woocommerce/src/Admin/Features/ProductDataViews/Init.php
@@ -27,8 +27,8 @@ class Init {
 				}
 			);
 
-			add_filter( 'admin_footer_text', '__return_empty_string' );
-			add_filter( 'update_footer', '__return_empty_string' );
+			add_filter( 'admin_footer_text', '__return_empty_string', 20);
+			add_filter( 'update_footer', '__return_empty_string', 20 );
 		}
 	}
 
