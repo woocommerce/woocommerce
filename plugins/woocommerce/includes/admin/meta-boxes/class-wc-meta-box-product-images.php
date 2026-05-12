6b03858e69b7fcdf267d20bae21ed18dc4283e60
James Kemp <me@jckemp.com>
2026-05-12 22:42:31 +0100

    Fix wrapper modifier to use rendered IDs instead of raw IDs

--- plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-images.php
+++ plugins/woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-images.php
@@ -35,45 +35,48 @@ class WC_Meta_Box_Product_Images {
 		$rendered_ids   = array();
 
 		wp_nonce_field( 'woocommerce_save_data', 'woocommerce_meta_nonce' );
-		?>
-		<div id="wc-product-images__list" class="wc-product-images__list<?php echo ! empty( $all_ids ) ? ' wc-product-images__list--has-images' : ''; ?>">
-			<?php
-			if ( ! empty( $all_ids ) ) {
-				foreach ( $all_ids as $attachment_id ) {
-					$img = wp_get_attachment_image( $attachment_id, empty( $rendered_ids ) ? 'medium' : 'thumbnail' );
 
-					if ( empty( $img ) ) {
-						continue;
-					}
+		// Buffer image tiles so $rendered_ids is fully populated before the wrapper div is emitted.
+		ob_start();
+		if ( ! empty( $all_ids ) ) {
+			foreach ( $all_ids as $attachment_id ) {
+				$img = wp_get_attachment_image( $attachment_id, empty( $rendered_ids ) ? 'medium' : 'thumbnail' );
 
-					$rendered_ids[] = (int) $attachment_id;
-					$is_featured    = ( 1 === count( $rendered_ids ) );
-
-					$modifier = $is_featured ? 'featured' : 'gallery';
-					?>
-					<div class="wc-product-images__image wc-product-images__image--<?php echo esc_attr( $modifier ); ?>" data-attachment-id="<?php echo esc_attr( (string) $attachment_id ); ?>" tabindex="0">
-						<?php echo $img; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-						<button type="button" class="wc-product-images__remove" tabindex="-1" aria-label="<?php esc_attr_e( 'Remove image', 'woocommerce' ); ?>"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 4c-4.4 0-8 3.6-8 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8Zm3.8 10.7-1.1 1.1-2.7-2.7-2.7 2.7-1.1-1.1 2.7-2.7-2.7-2.7 1.1-1.1 2.7 2.7 2.7-2.7 1.1 1.1-2.7 2.7 2.7 2.7Z" /></svg></button>
-						<?php
-						if ( ! $is_featured ) {
-							/**
-							 * Fires after a product gallery item is rendered in the meta box.
-							 *
-							 * @param int $thepostid     The product post ID.
-							 * @param int $attachment_id The attachment ID.
-							 *
-							 * @since 2.4.0
-							 */
-							do_action( 'woocommerce_admin_after_product_gallery_item', $thepostid, $attachment_id );
-						}
-						?>
-					</div>
-					<?php
+				if ( empty( $img ) ) {
+					continue;
 				}
-			}
 
-			$slot_modifier = empty( $rendered_ids ) ? 'featured' : 'gallery';
-			?>
+				$rendered_ids[] = (int) $attachment_id;
+				$is_featured    = ( 1 === count( $rendered_ids ) );
+
+				$modifier = $is_featured ? 'featured' : 'gallery';
+				?>
+				<div class="wc-product-images__image wc-product-images__image--<?php echo esc_attr( $modifier ); ?>" data-attachment-id="<?php echo esc_attr( (string) $attachment_id ); ?>" tabindex="0">
+					<?php echo $img; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					<button type="button" class="wc-product-images__remove" tabindex="-1" aria-label="<?php esc_attr_e( 'Remove image', 'woocommerce' ); ?>"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 4c-4.4 0-8 3.6-8 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8Zm3.8 10.7-1.1 1.1-2.7-2.7-2.7 2.7-1.1-1.1 2.7-2.7-2.7-2.7 1.1-1.1 2.7 2.7 2.7-2.7 1.1 1.1-2.7 2.7 2.7 2.7Z" /></svg></button>
+					<?php
+					if ( ! $is_featured ) {
+						/**
+						 * Fires after a product gallery item is rendered in the meta box.
+						 *
+						 * @param int $thepostid     The product post ID.
+						 * @param int $attachment_id The attachment ID.
+						 *
+						 * @since 2.4.0
+						 */
+						do_action( 'woocommerce_admin_after_product_gallery_item', $thepostid, $attachment_id );
+					}
+					?>
+				</div>
+				<?php
+			}
+		}
+		$image_tiles = ob_get_clean();
+
+		$slot_modifier = empty( $rendered_ids ) ? 'featured' : 'gallery';
+		?>
+		<div id="wc-product-images__list" class="wc-product-images__list<?php echo ! empty( $rendered_ids ) ? ' wc-product-images__list--has-images' : ''; ?>">
+			<?php echo $image_tiles; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 			<div id="wc-product-images__add-slot" class="wc-product-images__add-slot wc-product-images__add-slot--<?php echo esc_attr( $slot_modifier ); ?> hide-if-no-js" role="button" tabindex="0" aria-label="<?php esc_attr_e( 'Add product images', 'woocommerce' ); ?>">
 				<span class="wc-product-images__add-label"><?php esc_html_e( 'Add an image', 'woocommerce' ); ?></span>
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11 12.5V17.5H12.5V12.5H17.5V11H12.5V6H11V11H6V12.5H11V12.5Z"/></svg>
