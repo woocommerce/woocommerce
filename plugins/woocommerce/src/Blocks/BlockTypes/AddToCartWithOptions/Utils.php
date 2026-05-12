3dc355704571099d1bc6b8d2dfa1a420a7d5ac7d
Albert Juhé Lluveras <contact@albertjuhe.com>
2026-05-12 16:17:56 +0200

    Update has_visible_quantity_input() as well

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/Utils.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/Utils.php
@@ -23,7 +23,7 @@ class Utils {
 		while ( $processor->next_tag() ) {
 			if (
 				$processor->get_tag() === 'INPUT' &&
-				$processor->get_attribute( 'name' ) === 'quantity' &&
+				$processor->has_class( 'qty' ) &&
 				$processor->get_attribute( 'type' ) !== 'hidden'
 			) {
 				return true;
