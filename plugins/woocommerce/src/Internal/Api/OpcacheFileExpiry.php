2b523aa012c4094edfe6e7eb2293ac8f0f41eeb2
Alba Rincón <alba.rincon@automattic.com>
2026-05-14 10:05:41 +0200

    Fix early textdomain loading regression in GraphQL API

--- plugins/woocommerce/src/Internal/Api/OpcacheFileExpiry.php
+++ plugins/woocommerce/src/Internal/Api/OpcacheFileExpiry.php
@@ -62,12 +62,16 @@ class OpcacheFileExpiry {
 	 * Action Scheduler callback: delete expired files and reschedule.
 	 *
 	 * Immediate reschedule when files were deleted (drain the backlog), 24h
-	 * otherwise.
+	 * otherwise. Skipped when the feature is disabled.
 	 *
 	 * @internal
 	 */
 	public static function handle_cleanup_action(): void {
 		$interval = self::delete_expired_files() > 0 ? 1 : DAY_IN_SECONDS;
+
+		if ( ! Main::is_enabled() ) {
+			return;
+		}
 
 		if ( function_exists( 'as_schedule_single_action' ) ) {
 			as_schedule_single_action( time() + $interval, self::ACTION_HOOK, array(), self::ACTION_GROUP );
