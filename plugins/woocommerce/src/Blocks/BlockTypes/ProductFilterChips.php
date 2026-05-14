53018d7c2696aeae739e2df40011f9fba10f1a69
Albert Juhé Lluveras <contact@albertjuhe.com>
2026-05-14 11:49:05 +0200

    Undo unnecessary change

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductFilterChips.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductFilterChips.php
@@ -76,11 +76,6 @@ final class ProductFilterChips extends AbstractBlock {
 			$wrapper_attributes['class'] = esc_attr( $classes );
 		}
 
-		$items_wrapper_attributes = array(
-			'class'               => 'wc-block-product-filter-chips__items',
-			'data-wp-interactive' => $store_namespace,
-		);
-
 		ob_start();
 		?>
 		<div <?php echo get_block_wrapper_attributes( $wrapper_attributes ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
@@ -88,7 +83,10 @@ final class ProductFilterChips extends AbstractBlock {
 				<?php if ( ! empty( $block_context['groupLabel'] ) ) : ?>
 					<legend class="screen-reader-text"><?php echo esc_html( $block_context['groupLabel'] ); ?></legend>
 				<?php endif; ?>
-				<div <?php echo get_block_wrapper_attributes( $items_wrapper_attributes ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
+				<div
+					class="wc-block-product-filter-chips__items"
+					data-wp-interactive="<?php echo esc_attr( $store_namespace ); ?>"
+				>
 					<?php
 					foreach ( $visible_items as $index => $item ) :
 						$context_item = array_merge( $item, array( 'index' => $index ) );
