4d1dd6e69d10791c1979daf22d7be46e81e38ee0
Jorge Torres <jorge.torres@automattic.com>
2026-05-14 08:40:28 +0100

    Revert test helper class extractions and use phpcs:disable
    Restore the three test files that previously contained inline helper
    classes, and update their existing phpcs:disable annotations to
    reference the new Slevomat rule
    (SlevomatCodingStandard.Files.TypeNameMatchesFileName.NoMatchBetweenTypeNameAndFileName)
    instead of the removed Suin one.

--- plugins/woocommerce/tests/php/src/Internal/Logging/RemoteLoggerTest.php
+++ plugins/woocommerce/tests/php/src/Internal/Logging/RemoteLoggerTest.php
@@ -718,6 +718,37 @@ namespace Automattic\WooCommerce\Tests\Internal\Logging {
 			return $method->invokeArgs( $obj, $parameters );
 		}
 	}
+
+
+//phpcs:disable Generic.Files.OneObjectStructurePerFile.MultipleFound, Squiz.Classes.ClassFileName.NoMatch, SlevomatCodingStandard.Files.TypeNameMatchesFileName.NoMatchBetweenTypeNameAndFileName
+	/**
+	 * Mock class that extends RemoteLogger to allow overriding is_dev_or_local_environment.
+	 */
+	class RemoteLoggerWithEnvironmentOverride extends RemoteLogger {
+		/**
+		 * The is_dev_or_local value.
+		 *
+		 * @var bool
+		 */
+		private $is_dev_or_local = false;
+
+		/**
+		 * Set the is_dev_or_local value.
+		 *
+		 * @param bool $value The value to set.
+		 */
+		public function set_is_dev_or_local( $value ) {
+			$this->is_dev_or_local = $value;
+		}
+
+		/**
+		 * @inheritDoc
+		 */
+		protected function is_dev_or_local_environment() {
+			return $this->is_dev_or_local;
+		}
+	}
+//phpcs:enable Generic.Files.OneObjectStructurePerFile.MultipleFound, Squiz.Classes.ClassFileName.NoMatch, SlevomatCodingStandard.Files.TypeNameMatchesFileName.NoMatchBetweenTypeNameAndFileName
 }
 
 /**
