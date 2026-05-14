9fe80c3d675f09d6e726dc563eebad9c0a591ad7
Luigi Teschio <gigitux@gmail.com>
2026-05-14 18:11:44 +0200

    Replace tree select control with searchable chip select

--- packages/js/experimental-products-app/src/fields/components/tree-select-control/constants.ts
+++
@@ -1,8 +0,0 @@
-export const ROOT_VALUE = '__TREE_SELECT_COMPONENT_ROOT__';
-export const BACKSPACE = 'Backspace';
-export const ESCAPE = 'Escape';
-export const ENTER = 'Enter';
-export const ARROW_UP = 'ArrowUp';
-export const ARROW_DOWN = 'ArrowDown';
-export const ARROW_LEFT = 'ArrowLeft';
-export const ARROW_RIGHT = 'ArrowRight';
