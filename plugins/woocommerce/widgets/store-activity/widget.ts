32a4d3abc7320e36bdaee19006fc9d569e11b723
Damián Suárez <rdsuarez@gmail.com>
2026-05-18 10:37:54 +0100

    translate store-activity widget title

--- plugins/woocommerce/widgets/store-activity/widget.ts
+++ plugins/woocommerce/widgets/store-activity/widget.ts
@@ -1,8 +1,10 @@
+import { __ } from '@wordpress/i18n';
+
 /**
  * Widget type definition.
  */
 export default {
 	name: 'woocommerce/store-activity',
-	title: 'Store activity',
+	title: __( 'Store activity', 'woocommerce' ),
 	icon: 'list-view',
 };
