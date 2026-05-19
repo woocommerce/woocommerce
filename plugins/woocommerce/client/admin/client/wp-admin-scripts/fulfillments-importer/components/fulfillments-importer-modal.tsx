f5ed199cf125ddb55585e560004a27da3744a5bb
Abdalsalaam Halawa <abdalsalaamnafez@gmail.com>
2026-05-20 00:27:29 +0300

    Revert "Reject oversized CSV uploads in the importer modal"
    This reverts commit 16add0e783a392eb6afc66c74e9955d2cbebe3ec.

--- plugins/woocommerce/client/admin/client/wp-admin-scripts/fulfillments-importer/components/fulfillments-importer-modal.tsx
+++ plugins/woocommerce/client/admin/client/wp-admin-scripts/fulfillments-importer/components/fulfillments-importer-modal.tsx
@@ -21,8 +21,6 @@ import { recordEvent } from '@woocommerce/tracks';
 import type { ImporterSummary } from '../data/types';
 import ImporterSummaryPanel from './importer-summary';
 
-const MAX_UPLOAD_BYTES = 10 * 1024 * 1024; // 10 MB.
-
 interface Props {
 	isOpen: boolean;
 	onClose: () => void;
@@ -59,16 +57,6 @@ const FulfillmentsImporterModal: React.FC< Props > = ( {
 	const handleFileChange = useCallback(
 		( e: React.ChangeEvent< HTMLInputElement > ) => {
 			const picked = e.target.files?.[ 0 ] ?? null;
-			if ( picked && picked.size > MAX_UPLOAD_BYTES ) {
-				setFile( null );
-				setError(
-					__(
-						'CSV file is too large. Choose a file smaller than 10 MB.',
-						'woocommerce'
-					)
-				);
-				return;
-			}
 			setFile( picked );
 			setError( null );
 		},
