6c5b4be8b38e94eab214a48a16ea9a985b7f16c9
Poli Gilad <83961704+poligilad-auto@users.noreply.github.com>
2026-05-22 11:12:33 +0200

    Revert accidental JSDoc change in confirmation-modal.jsx

--- plugins/woocommerce/client/admin/client/launch-your-store/settings/components/confirmation-modal.jsx
+++ plugins/woocommerce/client/admin/client/launch-your-store/settings/components/confirmation-modal.jsx
@@ -13,7 +13,7 @@ import './confirmation-modal.scss';
 /**
  * Confirmation modal for the site visibility settings.
  *
- * @return {JSX.Element} The confirmation modal component.
+ * @return {React.ReactNode} The confirmation modal component.
  */
 export const ConfirmationModal = ( {
 	formRef,
