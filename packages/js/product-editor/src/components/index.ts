0e3509449827255cd4b56d87d15bb3979a7f7fd0
Luigi Teschio <gigitux@gmail.com>
2026-05-27 15:32:21 +0200

    remove feedback bar

--- packages/js/product-editor/src/components/index.ts
+++ packages/js/product-editor/src/components/index.ts
@@ -19,7 +19,6 @@ export {
 	BlockIcon as __experimentalBlockIcon,
 	BlockIconProps,
 } from './block-icon';
-export { FeedbackBar as __experimentalProductMVPCESFooter } from './feedback-bar';
 export { ProductMVPFeedbackModal as __experimentalProductMVPFeedbackModal } from './product-mvp-feedback-modal';
 export { ProductMVPFeedbackModalContainer as __experimentalProductMVPFeedbackModalContainer } from './product-mvp-feedback-modal-container';
 export {
