f50afe0fa68322863ae1d1d91586f61f3aa7edf1
Tung Du <dinhtungdu@gmail.com>
2026-05-29 15:44:48 +0700

    refactor: isolate visual attribute term schema

--- plugins/woocommerce/src/StoreApi/Schemas/V1/TermSchema.php
+++ plugins/woocommerce/src/StoreApi/Schemas/V1/TermSchema.php
@@ -1,8 +1,6 @@
 <?php
 namespace Automattic\WooCommerce\StoreApi\Schemas\V1;
 
-use Automattic\WooCommerce\Internal\ProductAttributes\VisualAttributeTermMeta;
-
 /**
  * TermSchema class.
  */
@@ -28,56 +26,41 @@ class TermSchema extends AbstractSchema {
 	 */
 	public function get_properties() {
 		return [
-			'id'                   => array(
+			'id'          => array(
 				'description' => __( 'Unique identifier for the resource.', 'woocommerce' ),
 				'type'        => 'integer',
 				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
 			),
-			'name'                 => array(
+			'name'        => array(
 				'description' => __( 'Term name.', 'woocommerce' ),
 				'type'        => 'string',
 				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
 			),
-			'slug'                 => array(
+			'slug'        => array(
 				'description' => __( 'String based identifier for the term.', 'woocommerce' ),
 				'type'        => 'string',
 				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
 			),
-			'description'          => array(
+			'description' => array(
 				'description' => __( 'Term description.', 'woocommerce' ),
 				'type'        => 'string',
 				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
 			),
-			'parent'               => array(
+			'parent'      => array(
 				'description' => __( 'Parent term ID, if applicable.', 'woocommerce' ),
 				'type'        => 'integer',
 				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
 			),
-			'count'                => array(
+			'count'       => array(
 				'description' => __( 'Number of objects (posts of any type) assigned to the term.', 'woocommerce' ),
 				'type'        => 'integer',
 				'context'     => array( 'view', 'edit' ),
 				'readonly'    => true,
-			),
-			'__experimentalVisual' => array(
-				'description' => __( 'Experimental visual swatch data for wc-visual attribute terms.', 'woocommerce' ),
-				'type'        => 'object',
-				'context'     => array( 'view', 'edit' ),
-				'readonly'    => true,
-				'properties'  => array(
-					'type'  => array(
-						'type' => 'string',
-						'enum' => array( VisualAttributeTermMeta::TYPE_COLOR, VisualAttributeTermMeta::TYPE_IMAGE, VisualAttributeTermMeta::TYPE_NONE ),
-					),
-					'value' => array(
-						'type' => 'string',
-					),
-				),
 			),
 		];
 	}
@@ -89,7 +72,7 @@ class TermSchema extends AbstractSchema {
 	 * @return array
 	 */
 	public function get_item_response( $term ) {
-		$response = [
+		return [
 			'id'          => (int) $term->term_id,
 			'name'        => $this->prepare_html_response( $term->name ),
 			'slug'        => $term->slug,
@@ -97,11 +80,5 @@ class TermSchema extends AbstractSchema {
 			'parent'      => (int) $term->parent,
 			'count'       => (int) $term->count,
 		];
-
-		if ( VisualAttributeTermMeta::is_visual_attribute_taxonomy( $term->taxonomy ) ) {
-			$response['__experimentalVisual'] = VisualAttributeTermMeta::get_term_visual( (int) $term->term_id );
-		}
-
-		return $response;
 	}
 }
