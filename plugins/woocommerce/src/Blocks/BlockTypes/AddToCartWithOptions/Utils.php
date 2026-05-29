f72061733cf3b01bf5813ba85fabb1333e61dcf9
Albert Juhé Lluveras <contact@albertjuhe.com>
2026-05-29 15:53:22 +0200

    Remove no longer needed render_block_with_context() util

--- plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/Utils.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/AddToCartWithOptions/Utils.php
@@ -227,27 +227,6 @@ class Utils {
 	}
 
 	/**
-	 * Renders a new block with custom context
-	 *
-	 * @param WP_Block $block The block instance.
-	 * @param array    $context The context for the new block.
-	 * @return string Rendered block content
-	 */
-	public static function render_block_with_context( $block, $context ) {
-		// Get an instance of the current block.
-		$block_instance = $block->parsed_block;
-
-		// Create new block with custom context.
-		$new_block = new WP_Block(
-			$block_instance,
-			$context
-		);
-
-		// Render with dynamic set to false to prevent calling render_callback.
-		return $new_block->render( array( 'dynamic' => false ) );
-	}
-
-	/**
 	 * Check if min and max purchase quantity are the same for a product.
 	 *
 	 * @param \WC_Product $product The product to check.
