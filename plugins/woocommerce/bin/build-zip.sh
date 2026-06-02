b11c39ad9d77e5876330ba6d537ae20de227909b
Christopher Allford <christopher.allford@automattic.com>
2026-06-02 10:44:54 -0700

    Use WooCommerce Package Symlinks
    Rather than having a `copy-assets` for
    the packages, just rely on Composer.
    It was already being used for the
    autoloader, however, the symlink
    meant that the build:zip would
    break. This change now uses
    the rsync -L option to deference
    the symlink when building the zip.

--- plugins/woocommerce/bin/build-zip.sh
+++ plugins/woocommerce/bin/build-zip.sh
@@ -30,7 +30,8 @@ composer install --no-dev --quiet || exit "$?"
 echo "Run makepot..."
 pnpm --filter=@woocommerce/plugin-woocommerce makepot || exit "$?"
 echo "Syncing files..."
-rsync -rc --exclude-from="$PROJECT_PATH/.distignore" "$PROJECT_PATH/" "$DEST_PATH/" --delete --delete-excluded
+# Make sure to dereference the symlinks from Composer or the archive won't have all of the files!
+rsync -rcL --exclude-from="$PROJECT_PATH/.distignore" "$PROJECT_PATH/" "$DEST_PATH/" --delete --delete-excluded
 
 echo "Regenerating autoloader for production..."
 cd "$DEST_PATH" || exit
