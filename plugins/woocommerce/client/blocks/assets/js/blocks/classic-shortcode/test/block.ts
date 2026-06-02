67119e6c3a8f6339c79f1b802f24b154447a2195
Luigi Teschio <gigitux@gmail.com>
2026-06-02 21:09:44 +0200

    Fix block library for WordPress 6.9 package update (#65356)
    * Fix components package for WordPress 6.9 packages
    
    * Fix currency package for WordPress 6.9 packages
    
    * Fix data package for WordPress 6.9 packages
    
    * Fix email editor package for WordPress 6.9 packages
    
    * Fix product editor package for WordPress 6.9 packages
    
    * Fix admin library for WordPress 6.9 packages
    
    * Fix block library for WordPress 6.9 packages
    
    * Fix setting options core data locks
    
    * Fix setting options action types
    
    * Derive setting option lock types from core data
    
    * Avoid internal lock action types
    
    * improve types
    
    * lint code
    
    * update snapshots
    
    * clean up changes
    
    * fix unit test
    
    * lint code
    
    * Fix product editor selector mocks
    
    * lint code
    
    * delete file
    
    * delete file
    
    * fix unit test
    
    * clean up changes
    
    * fix icon
    
    * restore pnpm-lock

--- plugins/woocommerce/client/blocks/assets/js/blocks/classic-shortcode/test/block.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/classic-shortcode/test/block.ts
@@ -39,10 +39,6 @@ describe( 'Classic Shortcode block', () => {
 		} );
 
 		expect( screen.getByLabelText( /^Block: Cart$/i ) ).toBeInTheDocument();
-
-		// wp-6.8: @wordpress/block-editor's RichText fires an internal
-		// __unstableUseRichText deprecation via @wordpress/deprecated.
-		expect( console ).toHaveWarned();
 	} );
 	test( 'can convert to Checkout block', async () => {
 		await setup( { shortcode: 'checkout' } );
