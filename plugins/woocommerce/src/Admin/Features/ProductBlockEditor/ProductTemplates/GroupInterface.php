cff8ba8c620b9124ab971f5d9c52fad2ca77bff7
Luigi Teschio <gigitux@gmail.com>
2026-06-05 11:28:18 +0200

    add compatibility shims

---
+++ plugins/woocommerce/src/Admin/Features/ProductBlockEditor/ProductTemplates/GroupInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * WooCommerce Product Editor group interface compatibility shim.
+ */
+
+declare( strict_types = 1 );
+
+namespace Automattic\WooCommerce\Admin\Features\ProductBlockEditor\ProductTemplates;
+
+/**
+ * Removed product editor group container interface.
+ *
+ * @deprecated 10.9.0 Product editor extension APIs were deprecated. The product block editor was removed in 11.0.0 with no replacement.
+ */
+interface GroupInterface {}
