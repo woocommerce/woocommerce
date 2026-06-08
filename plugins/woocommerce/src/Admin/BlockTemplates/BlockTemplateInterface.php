1b35a431ac81f83d5aae90a89c4da0294b3b5a91
Luigi Teschio <gigitux@gmail.com>
2026-06-08 10:14:16 +0200

    Add compatibility shims for the BlockTemplate feature (#65539)
    * Add block templates compatibility shims
    
    * Add changelog entry for block templates shims

---
+++ plugins/woocommerce/src/Admin/BlockTemplates/BlockTemplateInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * WooCommerce Block Templates template interface compatibility shim.
+ */
+
+namespace Automattic\WooCommerce\Admin\BlockTemplates;
+
+/**
+ * Removed block templates template interface.
+ *
+ * @deprecated 10.9.0 Block template extension APIs were deprecated. The block templates API was removed in 11.0.0 with no replacement.
+ */
+interface BlockTemplateInterface extends ContainerInterface {}
