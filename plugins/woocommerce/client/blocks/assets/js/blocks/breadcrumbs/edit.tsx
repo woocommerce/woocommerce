6a2ea4a4b2f2dca068739f8fa5bc7a5ed0fc9238
Albert Juhé Lluveras <contact@albertjuhe.com>
2026-06-09 14:56:51 +0200

    Make sure theme.json Breadcrumbs font size is also applied in the editor, matching the frontend

--- plugins/woocommerce/client/blocks/assets/js/blocks/breadcrumbs/edit.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/breadcrumbs/edit.tsx
@@ -4,15 +4,36 @@
 import { useBlockProps } from '@wordpress/block-editor';
 import { Disabled } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import type { BlockEditProps } from '@wordpress/blocks';
 
-export interface Attributes {
+/**
+ * Internal dependencies
+ */
+import { useBreadcrumbsThemeFontSize } from './hooks';
+
+export type Attributes = {
 	className?: string;
-}
+	fontSize?: string;
+};
 
-const Edit = () => {
+const Edit = ( { attributes }: BlockEditProps< Attributes > ) => {
 	const blockProps = useBlockProps( {
 		className: 'woocommerce wc-block-breadcrumbs',
 	} );
+
+	const themeFontSize = useBreadcrumbsThemeFontSize();
+
+	if (
+		attributes.fontSize === 'small' &&
+		themeFontSize &&
+		themeFontSize !== 'var(--wp--preset--font-size--small)'
+	) {
+		blockProps.className = blockProps.className
+			.split( ' ' )
+			.filter( ( cls ) => cls && cls !== 'has-small-font-size' )
+			.join( ' ' );
+		blockProps.style.fontSize = themeFontSize;
+	}
 
 	return (
 		<div { ...blockProps }>
