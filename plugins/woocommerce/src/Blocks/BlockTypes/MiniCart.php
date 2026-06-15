765ed8091d391ea74b72ef1a08f6d5075104e712
Luigi Teschio <gigitux@gmail.com>
2026-06-15 10:40:10 +0200

    address feedback

--- plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/MiniCart.php
@@ -156,15 +156,6 @@ class MiniCart extends AbstractBlock {
 	}
 
 	/**
-	 * Get the frontend style handle for this block type.
-	 *
-	 * @return string[]
-	 */
-	protected function get_block_type_style() {
-		return array_merge( parent::get_block_type_style(), array( 'wc-blocks-packages-style' ) );
-	}
-
-	/**
 	 * Extra data passed through from server to client for block.
 	 *
 	 * @param array $attributes  Any attributes that currently are available from the block.
