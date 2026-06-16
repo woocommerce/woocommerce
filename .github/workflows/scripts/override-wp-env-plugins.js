177dc0bdb407f670f42bd51c67e7eac71e88b183
Daniel Mallory <daniel.mallory@automattic.com>
2026-06-16 23:16:26 +0100

    ci: run e2e against shared plugin artifact

--- .github/workflows/scripts/override-wp-env-plugins.js
+++ .github/workflows/scripts/override-wp-env-plugins.js
@@ -1,15 +1,20 @@
 /* eslint-disable no-console */
 const fs = require( 'fs' );
 
-const { RELEASE_TAG, ARTIFACT_NAME, WP_ENV_CONFIG_PATH } = process.env;
+const { RELEASE_TAG, ARTIFACT_NAME, PLUGIN_PATH, WP_ENV_CONFIG_PATH } =
+	process.env;
 
-if ( ! RELEASE_TAG ) {
-	console.error( 'Please set the RELEASE_TAG environment variable!' );
+if ( ! PLUGIN_PATH && ! RELEASE_TAG ) {
+	console.error(
+		'Please set the PLUGIN_PATH or RELEASE_TAG environment variable!'
+	);
 	process.exit( 1 );
 }
 
-if ( ! ARTIFACT_NAME ) {
-	console.error( 'Please set the ARTIFACT_NAME environment variable!' );
+if ( ! PLUGIN_PATH && ! ARTIFACT_NAME ) {
+	console.error(
+		'Please set the PLUGIN_PATH or ARTIFACT_NAME environment variable!'
+	);
 	process.exit( 1 );
 }
 
@@ -18,7 +23,9 @@ if ( ! WP_ENV_CONFIG_PATH ) {
 	process.exit( 1 );
 }
 
-const artifactUrl = `https://github.com/woocommerce/woocommerce/releases/download/${ RELEASE_TAG }/${ ARTIFACT_NAME }`;
+const pluginSource =
+	PLUGIN_PATH ||
+	`https://github.com/woocommerce/woocommerce/releases/download/${ RELEASE_TAG }/${ ARTIFACT_NAME }`;
 
 const configPath = `${ WP_ENV_CONFIG_PATH }/.wp-env.json`;
 console.log( `Reading ${ configPath }` );
@@ -39,25 +46,25 @@ if ( wpEnvConfig.env?.tests?.plugins ) {
 	};
 }
 
-const entriesToReplace = [ '.', '../woocommerce' ];
+const entriesToReplace = [ '.', '../woocommerce', '../..' ];
 
 for ( const entry of entriesToReplace ) {
 	// Search and replace in root plugins
-	let found = overrideConfig.plugins.indexOf( entry );
+	let found = overrideConfig.plugins?.indexOf( entry ) ?? -1;
 	if ( found >= 0 ) {
 		console.log(
-			`Replacing ${ entry } with ${ artifactUrl } in root plugins`
+			`Replacing ${ entry } with ${ pluginSource } in root plugins`
 		);
-		overrideConfig.plugins[ found ] = artifactUrl;
+		overrideConfig.plugins[ found ] = pluginSource;
 	}
 
 	// Search and replace in test env plugins
-	found = overrideConfig.env?.tests?.plugins?.indexOf( entry );
+	found = overrideConfig.env?.tests?.plugins?.indexOf( entry ) ?? -1;
 	if ( found >= 0 ) {
 		console.log(
-			`Replacing ${ entry } with ${ artifactUrl } in env.tests.plugins`
+			`Replacing ${ entry } with ${ pluginSource } in env.tests.plugins`
 		);
-		overrideConfig.env.tests.plugins[ found ] = artifactUrl;
+		overrideConfig.env.tests.plugins[ found ] = pluginSource;
 	}
 }
 
