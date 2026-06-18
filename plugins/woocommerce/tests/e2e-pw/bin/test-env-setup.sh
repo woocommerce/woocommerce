fc4008d3dd0afa627b437d9a4c4510ac0ff1a90a
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2026-06-18 13:08:12 +0300

    Harden remaining E2E single-file mounts against gRPC FUSE

--- plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
+++ plugins/woocommerce/tests/e2e-pw/bin/test-env-setup.sh
@@ -4,7 +4,9 @@ if [ ! -z ${CI+y} ]; then
     # In CI we want to execute the setup behind single container call, while in dev-environments we use the script as it is.
     # Inside the container the command executed from /var/www/html path as pwd
     echo -e '--> Dispatching script execution into tests-cli\n'
-    wp-env run --debug tests-cli cp test-env-setup.sh test-env-setup-ci.sh
+    # Source from the e2e-test-helpers directory mount; a single-file mount of this
+    # script can surface as an empty file under Docker gRPC FUSE.
+    wp-env run --debug tests-cli cp wp-content/plugins/e2e-test-helpers/test-env-setup.sh test-env-setup-ci.sh
     wp-env run --debug tests-cli sed -i -e 's/wp-env run tests-cli //' test-env-setup-ci.sh
     wp-env run --debug tests-cli bash test-env-setup-ci.sh
     exit $?
@@ -15,6 +17,13 @@ wp-env run tests-cli wp theme install storefront twentytwenty twentytwentytwo &
 
 echo -e 'Activate default theme \n'
 wp-env run tests-cli wp theme activate twentytwentythree
+
+# Provision wp-cli.yml in-container instead of mapping it. Single-file Docker
+# mounts can surface as empty files under gRPC FUSE, which would silently drop
+# the apache_modules declaration that `wp rewrite ... --hard` needs to write the
+# mod_rewrite block to .htaccess.
+echo -e 'Provision wp-cli.yml \n'
+wp-env run tests-cli bash -c 'printf "apache_modules:\n  - mod_rewrite\n" > /var/www/html/wp-cli.yml'
 
 echo -e 'Update URL structure \n'
 wp-env run tests-cli wp rewrite structure '/%postname%/' --hard
