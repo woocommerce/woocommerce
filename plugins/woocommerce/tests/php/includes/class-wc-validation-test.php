c3ef45991f4e6beabb1f83d6ae909e1dc8bc22bb
Parsa Kafi <parselearn@gmail.com>
2026-06-20 02:16:40 +0330

    Add test for woocommerce_validate_phone filter

--- plugins/woocommerce/tests/php/includes/class-wc-validation-test.php
+++ plugins/woocommerce/tests/php/includes/class-wc-validation-test.php
@@ -10,6 +10,51 @@
  */
 class WC_Validation_Test extends \WC_Unit_Test_Case {
 	/**
+	 * Data provider for test_is_phone()
+	 */
+	public function data_provider_test_is_phone() {
+		$enPhones = array(
+			array( true, WC_Validation::is_phone( '+00 000 00 00 000' ) ),
+			array( true, WC_Validation::is_phone( '+00-000-00-00-000' ) ),
+			array( true, WC_Validation::is_phone( '(000) 00 00 000' ) ),
+			array( true, WC_Validation::is_phone( '+00.000.00.00.000' ) ),
+			array( false, WC_Validation::is_phone( '+00 aaa dd ee fff' ) ),
+		);
+
+		// Check non-English phone numbers, This test check Persian (Farsi) phone numbers.
+		$faPhoneValidation = function ( $valid, $phone, $country ) {
+			$phone = str_replace(
+				array( '۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹' ),
+				range( 0, 9 ),
+				$phone
+			);
+
+			return preg_match( '/^(0|0098|\+98)?(9\d{9}|[1-8]\d{9,10})$/', $phone );
+		};
+		add_filter( 'woocommerce_validate_phone', $faPhoneValidation, 10, 3 );
+		$faPhones = array(
+			array( true, WC_Validation::is_phone( '+۹۸۹۱۵۱۱۱۲۲۳۳' ) ),
+			array( true, WC_Validation::is_phone( '۰۰۹۸۹۱۵۱۱۱۲۲۳۳' ) ),
+			array( true, WC_Validation::is_phone( '۰۹۱۵۱۱۱۲۲۳۳' ) ),
+		);
+		remove_filter( 'woocommerce_validate_phone', $faPhoneValidation );
+
+		return array_merge( $enPhones, $faPhones );
+	}
+
+	/**
+	 * Test phone validation.
+	 *
+	 * @param mixed $assert Expected value.
+	 * @param mixed $values Actual value.
+	 *
+	 * @dataProvider data_provider_test_is_phone
+	 */
+	public function test_is_phone( $assert, $values ) {
+		$this->assertEquals( $assert, $values );
+	}
+
+	/**
 	 * Data provider for test_is_postcode().
 	 */
 	public function data_provider_test_is_postcode(): array {
