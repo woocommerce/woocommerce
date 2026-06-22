9c94b430c9a2465bdd198939cf7ffa98b520737b
Jorge A. Torres <jorge.torres@automattic.com>
2026-06-22 11:29:41 +0100

    Update return docblock for from_product method

--- plugins/woocommerce/src/Internal/ShopperLists/ShopperListItem.php
+++ plugins/woocommerce/src/Internal/ShopperLists/ShopperListItem.php
@@ -130,7 +130,7 @@ class ShopperListItem {
 	 * @param int   $product_or_variation_id Product or variation ID.
 	 * @param array $variation               Variation attributes keyed by attribute name.
 	 * @param int   $quantity                Saved quantity. Coerced to a minimum of 1.
-	 * @return self|null Null if the underlying product can't be resolved or isn't publicly live.
+	 * @return self|null Null if the underlying product can't be resolved or isn't published.
 	 */
 	public static function from_product( int $product_or_variation_id, array $variation = array(), int $quantity = 1 ): ?self {
 		$product = wc_get_product( absint( $product_or_variation_id ) );
