d56a7d4df87a3f0ac85239732dd84a058426be5a
Parsa Kafi <parselearn@gmail.com>
2026-06-24 16:39:49 +0330

    chore: run php lint, fix space

--- plugins/woocommerce/includes/class-wc-checkout.php
+++ plugins/woocommerce/includes/class-wc-checkout.php
@@ -921,7 +921,7 @@ class WC_Checkout {
 					// This is a safe sanitize to prevent copy-paste issues with invisible chars. Won't ensure validation.
 					$data[ $key ] = wc_remove_non_displayable_chars( $data[ $key ] );
 
-					if ( $validate_fieldset && '' !== $data[ $key ] && ! WC_Validation::is_phone( $data[ $key ], $country) ) {
+					if ( $validate_fieldset && '' !== $data[ $key ] && ! WC_Validation::is_phone( $data[ $key ], $country ) ) {
 						/* translators: %s: phone number */
 						$errors->add( $key . '_validation', sprintf( __( '%s is not a valid phone number.', 'woocommerce' ), '<strong>' . esc_html( $field_label ) . '</strong>' ), array( 'id' => $key ) );
 					}
