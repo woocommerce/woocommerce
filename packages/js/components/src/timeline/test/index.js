ea0024a856fe53a1a392636b3f81bc9affecd666
Luigi Teschio <gigitux@gmail.com>
2026-07-01 10:40:54 +0200

    fix lint error

--- packages/js/components/src/timeline/test/index.js
+++ packages/js/components/src/timeline/test/index.js
@@ -21,6 +21,33 @@ jest.mock( '@wordpress/date', () => {
 	};
 } );
 
+const mockData = [
+	{
+		date: new Date( 2020, 0, 20, 1, 30 ),
+		body: [ <p key={ '1' }>{ 'p element in body' }</p>, 'string in body' ],
+		headline: <p>{ 'p tag in headline' }</p>,
+		icon: null,
+		hideTimestamp: true,
+	},
+	{
+		date: new Date( 2020, 0, 20, 23, 45 ),
+		body: [],
+		headline: <span>{ 'span in headline' }</span>,
+		icon: null,
+	},
+	{
+		date: new Date( 2020, 0, 22, 15, 13 ),
+		body: [ <span key={ '1' }>{ 'span in body' }</span> ],
+		headline: 'string in headline',
+		icon: null,
+	},
+	{
+		date: new Date( 2020, 0, 17, 1, 45 ),
+		headline: 'undefined body and string headline',
+		icon: null,
+	},
+];
+
 describe( 'Timeline', () => {
 	const actualDateModule = jest.requireActual( '@wordpress/date' );
 	const originalDateSettings = actualDateModule.getSettings();
