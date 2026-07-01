0dc293be14850fc2bc3b14e12ebd37d731c52a75
Luigi Teschio <gigitux@gmail.com>
2026-07-01 15:38:27 +0200

    Reduce block editor CES startup cost

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-collection/edit/inspector-controls/index.tsx
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-collection/edit/inspector-controls/index.tsx
@@ -6,11 +6,10 @@ import { __ } from '@wordpress/i18n';
 import { useMemo } from '@wordpress/element';
 import { useIsEmailEditor } from '@woocommerce/email-editor';
 import { recordEvent } from '@woocommerce/tracks';
-import { CesFeedbackButton } from '@woocommerce/editor-components/ces-feedback-button';
 import {
-	PanelBody,
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 	__experimentalToolsPanel as ToolsPanel,
+	PanelBody,
 } from '@wordpress/components';
 
 /**
@@ -289,10 +288,6 @@ const ProductCollectionInspectorControls = (
 					) }
 				</ToolsPanel>
 			) : null }
-			<CesFeedbackButton
-				blockName={ `${ metadata.title } block` }
-				wrapper={ PanelBody }
-			/>
 		</InspectorControls>
 	);
 };
