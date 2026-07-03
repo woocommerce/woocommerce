970ee440185799f27200abee3ea1e65f2cc1eee3
Albert Juhé Lluveras <contact@albertjuhe.com>
2026-07-03 17:25:55 +0200

    Fix imageSizing value in tests

--- plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/image/test/block.test.tsx
+++ plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/image/test/block.test.tsx
@@ -371,7 +371,7 @@ describe( 'Product Image Block', () => {
 						productId={ productWithImages.id }
 						showSaleBadge={ false }
 						saleBadgeAlign={ 'left' }
-						imageSizing={ ImageSizing.SINGLE }
+						imageSizing={ ImageSizing.THUMBNAIL }
 						isDescendentOfQueryLoop={ false }
 						aspectRatio="3/5"
 					/>
@@ -405,7 +405,7 @@ describe( 'Product Image Block', () => {
 						productId={ productWithImages.id }
 						showSaleBadge={ false }
 						saleBadgeAlign={ 'left' }
-						imageSizing={ ImageSizing.SINGLE }
+						imageSizing={ ImageSizing.THUMBNAIL }
 						isDescendentOfQueryLoop={ false }
 					/>
 				</ProductDataContextProvider>
