62039294d534dc604f09725093b22a313afb39d8
DevanshirtCamp <devanshi.joshi@rtcamp.com>
2026-07-08 18:13:04 +0530

    Fix: change the PR reference in product price deprecated file

--- plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/price/deprecated.ts
+++ plugins/woocommerce/client/blocks/assets/js/atomic/blocks/product-elements/price/deprecated.ts
@@ -6,7 +6,7 @@ import save from '../save';
 
 const { attributes: blockAttributes } = metadata;
 
-// In https://github.com/woocommerce/woocommerce/pull/57980, the `isDescendentOf*` attributes were removed from editor-load updates and block attributes.
+// In https://github.com/woocommerce/woocommerce/pull/66287, the `isDescendentOf*` attributes were removed from editor-load updates and block attributes.
 const v1 = {
 	attributes: {
 		...blockAttributes,
