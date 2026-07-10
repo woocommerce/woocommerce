68d2077583a101323c3893f5c6e0b6901dc05e7e
Vlad Olaru <vlad@thinkwritecode.com>
2026-07-10 15:23:29 +0300

    test: scope V3 product metadata fixtures
    [Context]
    V3 product metadata collection tests create four class-owned products with known test1 and test2 metadata.
    
    [Problem]
    They queried the unrestricted catalog and hardcoded a four-item count, so unrelated bootstrap products made the same tests fail in isolation.
    
    [Solution]
    Pass dynamically allocated class fixture IDs through the production include parameter and assert the dynamic fixture count while preserving every include_meta and exclude_meta response assertion.

--- plugins/woocommerce/tests/php/includes/rest-api/Controllers/Version3/class-wc-rest-products-controller-tests.php
+++ plugins/woocommerce/tests/php/includes/rest-api/Controllers/Version3/class-wc-rest-products-controller-tests.php
@@ -87,6 +87,20 @@ class WC_REST_Products_Controller_Tests extends WC_REST_Unit_Test_Case {
 	}
 
 	/**
+	 * Get the IDs of the products owned by this test class.
+	 *
+	 * @return int[]
+	 */
+	private function get_fixture_product_ids(): array {
+		return array_map(
+			static function ( $product ) {
+				return $product->get_id();
+			},
+			self::$products
+		);
+	}
+
+	/**
 	 * Setup our test server, endpoints, and user info.
 	 */
 	public function setUp(): void {
@@ -424,11 +438,12 @@ class WC_REST_Products_Controller_Tests extends WC_REST_Unit_Test_Case {
 	public function test_collection_param_include_meta() {
 		$request = new WP_REST_Request( 'GET', '/wc/v3/products' );
 		$request->set_param( 'include_meta', 'test1' );
+		$request->set_param( 'include', $this->get_fixture_product_ids() );
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
 
 		$response_data = $response->get_data();
-		$this->assertCount( 4, $response_data );
+		$this->assertCount( count( self::$products ), $response_data );
 
 		foreach ( $response_data as $order ) {
 			$this->assertArrayHasKey( 'meta_data', $order );
@@ -449,11 +464,12 @@ class WC_REST_Products_Controller_Tests extends WC_REST_Unit_Test_Case {
 	public function test_collection_param_include_meta_empty() {
 		$request = new WP_REST_Request( 'GET', '/wc/v3/products' );
 		$request->set_param( 'include_meta', '' );
+		$request->set_param( 'include', $this->get_fixture_product_ids() );
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
 
 		$response_data = $response->get_data();
-		$this->assertCount( 4, $response_data );
+		$this->assertCount( count( self::$products ), $response_data );
 
 		foreach ( $response_data as $order ) {
 			$this->assertArrayHasKey( 'meta_data', $order );
@@ -474,11 +490,12 @@ class WC_REST_Products_Controller_Tests extends WC_REST_Unit_Test_Case {
 	public function test_collection_param_exclude_meta() {
 		$request = new WP_REST_Request( 'GET', '/wc/v3/products' );
 		$request->set_param( 'exclude_meta', 'test1' );
+		$request->set_param( 'include', $this->get_fixture_product_ids() );
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
 
 		$response_data = $response->get_data();
-		$this->assertCount( 4, $response_data );
+		$this->assertCount( count( self::$products ), $response_data );
 
 		foreach ( $response_data as $order ) {
 			$this->assertArrayHasKey( 'meta_data', $order );
@@ -500,11 +517,12 @@ class WC_REST_Products_Controller_Tests extends WC_REST_Unit_Test_Case {
 		$request = new WP_REST_Request( 'GET', '/wc/v3/products' );
 		$request->set_param( 'include_meta', 'test1' );
 		$request->set_param( 'exclude_meta', 'test1' );
+		$request->set_param( 'include', $this->get_fixture_product_ids() );
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
 
 		$response_data = $response->get_data();
-		$this->assertCount( 4, $response_data );
+		$this->assertCount( count( self::$products ), $response_data );
 
 		foreach ( $response_data as $order ) {
 			$this->assertArrayHasKey( 'meta_data', $order );
