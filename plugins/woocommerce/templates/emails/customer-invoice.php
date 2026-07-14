dad55ac87fdb9441502d74efe38f66b554204f08
Marco Almeida <marcoalmeida@webdados.pt>
2026-07-14 10:37:58 +0100

    Set template version to 11.1.0

--- plugins/woocommerce/templates/emails/customer-invoice.php
+++ plugins/woocommerce/templates/emails/customer-invoice.php
@@ -12,7 +12,7 @@
  *
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
- * @version 11.0.0
+ * @version 11.1.0
  */
 
 use Automattic\WooCommerce\Enums\OrderStatus;
