9051bdaee1e59ece6400688677b46f561b07d6ef
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2026-07-14 15:54:14 +0200

    Fix Product Gallery touch gesture reliability

--- plugins/woocommerce/tests/php/src/Blocks/BlockTypes/ProductGallery.php
+++ plugins/woocommerce/tests/php/src/Blocks/BlockTypes/ProductGallery.php
@@ -191,6 +191,10 @@ class ProductGallery extends \WP_UnitTestCase {
 
 		// Check that the gallery wrapper is rendered.
 		$this->assertStringContainsString( 'wc-block-product-gallery', $markup );
+		$this->assertStringContainsString(
+			'data-wp-init--touch-move-listener="callbacks.initTouchMoveListener"',
+			$markup
+		);
 
 		// Check that the viewer block is rendered.
 		$this->assertStringContainsString( 'wc-block-product-gallery-large-image', $markup );
