4e58ac1bde6aaf5de7056c72c442803cb87246fb
Daniel Mallory <daniel.mallory@automattic.com>
2026-07-18 07:05:07 +0100

    Simplify Settings UI saves and extension registration

--- packages/js/settings-ui/src/index.ts
+++ packages/js/settings-ui/src/index.ts
@@ -1,13 +1,5 @@
 export { SettingsUIErrorBoundary, SettingsUIPage } from './settings-ui-page';
-export { HiddenInputs, getHiddenInputs } from './hidden-inputs';
-export {
-	registerSettingsExtension,
-	resolveFieldComponent,
-	resolveFieldVisibilityPredicate,
-	resolveGroupVisibilityPredicate,
-	resolveRegionComponent,
-	resolveSaveHandler,
-} from './registry';
+export { registerSettingsExtension } from './registry';
 export { useSettingsUIContext } from './settings-ui-context';
 export type {
 	SettingsEditControlProps,
@@ -18,7 +10,6 @@ export type {
 	SettingsUIGroupAction,
 	SettingsUIOption,
 	SettingsUIPageContextValue,
-	SettingsUIRegistry,
 	SettingsUISaveSchema,
 	SettingsUISaveStrategy,
 	SettingsUISchema,
@@ -34,8 +25,6 @@ export type {
 	SettingsFieldContext,
 	SettingsFieldValidator,
 	SettingsFieldValidatorArgs,
-	SettingsRegionComponent,
-	SettingsRegionComponentProps,
 	SettingsSaveHandler,
 	SettingsSaveHandlerArgs,
 	SettingsSaveResult,
