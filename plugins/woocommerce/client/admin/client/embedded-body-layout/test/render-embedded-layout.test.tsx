a7fa9d16be9d64d48958957bbc552ae91c72d0f5
Luigi Teschio <gigitux@gmail.com>
2026-08-03 18:56:31 +0200

    Fix product editor content shift on initial load

--- plugins/woocommerce/client/admin/client/embedded-body-layout/test/render-embedded-layout.test.tsx
+++ plugins/woocommerce/client/admin/client/embedded-body-layout/test/render-embedded-layout.test.tsx
@@ -65,6 +65,11 @@ describe( 'embedded-layout', () => {
 		expect(
 			mockEmbeddedRoot.classList.contains( 'is-embed-loading' )
 		).toBeFalsy();
+		const noticeLayout = document.querySelector(
+			'#wpbody-content > .woocommerce-layout'
+		);
+		expect( noticeLayout ).toBeInTheDocument();
+		expect( noticeLayout?.nextElementSibling ).toHaveClass( 'wrap' );
 		expect( result ).toBeTruthy();
 	} );
 
