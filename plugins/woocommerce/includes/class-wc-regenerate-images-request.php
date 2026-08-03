16cf07b86cc9802fa1afaf57361e2efdd218dad1
M. L. Giannotta <sunyatasattva@gmail.com>
2026-08-03 18:36:41 +0200

    Skip image regeneration for files without a supporting image editor

--- plugins/woocommerce/includes/class-wc-regenerate-images-request.php
+++ plugins/woocommerce/includes/class-wc-regenerate-images-request.php
@@ -101,6 +101,11 @@ class WC_Regenerate_Images_Request extends WC_Background_Process {
 			return false;
 		}
 
+		// Files without a supporting image editor (e.g. SVGs) can never be regenerated, so remove them from the queue.
+		if ( ! wp_image_editor_supports( array( 'mime_type' => $attachment->post_mime_type ) ) ) {
+			return false;
+		}
+
 		if ( ! function_exists( 'wp_crop_image' ) ) {
 			include ABSPATH . 'wp-admin/includes/image.php';
 		}
