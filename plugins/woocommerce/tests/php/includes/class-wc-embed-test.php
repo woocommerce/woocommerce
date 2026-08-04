68db7247f65e5ffad3a83e9e0507c2fb26e6e118
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2026-08-04 16:16:18 +0200

    Limit embed tests to product summaries

--- plugins/woocommerce/tests/php/includes/class-wc-embed-test.php
+++ plugins/woocommerce/tests/php/includes/class-wc-embed-test.php
@@ -7,18 +7,13 @@ declare( strict_types = 1 );
 class WC_Embed_Test extends WC_Unit_Test_Case {
 
 	/**
-	 * @testdox Password-protected product embeds should not expose the short description.
+	 * @testdox Password-protected product embeds should not expose the product summary.
 	 */
-	public function test_embed_does_not_expose_password_protected_product_data(): void {
+	public function test_embed_does_not_expose_password_protected_product_summary(): void {
 		$product = WC_Helper_Product::create_simple_product();
-		$product->set_regular_price( '19.99' );
 		$product->set_short_description( 'Protected short description' );
 		$product->save();
 
-		$review_id = WC_Helper_Product::create_product_review( $product->get_id() );
-		update_comment_meta( $review_id, 'rating', 5 );
-		WC_Comments::clear_transients( $product->get_id() );
-		$this->assertGreaterThan( 0, (float) wc_get_product( $product->get_id() )->get_average_rating(), 'The product should have a rating to render.' );
 		wp_update_post(
 			array(
 				'ID'            => $product->get_id(),
@@ -31,21 +26,17 @@ class WC_Embed_Test extends WC_Unit_Test_Case {
 
 		ob_start();
 		$excerpt = WC_Embed::the_excerpt( 'Password required' );
-		WC_Embed::get_ratings();
-		$output = ob_get_clean();
+		ob_end_clean();
 
-		$this->assertStringContainsString( '19.99', $output, 'The product price should continue to be rendered.' );
-		$this->assertStringContainsString( 'Rated 5.00 out of 5', $output, 'The product rating should continue to be rendered.' );
 		$this->assertStringContainsString( 'Password required', $excerpt, 'The password-protected excerpt should continue to be rendered.' );
 		$this->assertStringNotContainsString( 'Protected short description', $excerpt, 'The protected short description should not replace the password-protected excerpt.' );
 	}
 
 	/**
-	 * @testdox Public product embeds should continue to render product data.
+	 * @testdox Public product embeds should continue to render the product summary.
 	 */
-	public function test_the_excerpt_renders_public_product_data(): void {
+	public function test_embed_renders_public_product_summary(): void {
 		$product = WC_Helper_Product::create_simple_product();
-		$product->set_regular_price( '19.99' );
 		$product->set_short_description( 'Public short description' );
 		$product->save();
 
@@ -53,9 +44,8 @@ class WC_Embed_Test extends WC_Unit_Test_Case {
 
 		ob_start();
 		$excerpt = WC_Embed::the_excerpt( 'Original excerpt' );
-		$output  = ob_get_clean();
+		ob_end_clean();
 
-		$this->assertStringContainsString( '19.99', $output, 'The product price should be rendered.' );
 		$this->assertStringContainsString( 'Public short description', $excerpt, 'The product short description should be rendered.' );
 	}
 
