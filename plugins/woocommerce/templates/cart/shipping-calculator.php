3a6049e811ba70ae006c6819cb3afd11482fd794
frosso <francesco.rosso@automattic.com>
2026-08-06 10:16:28 +0200

    revert template

--- plugins/woocommerce/templates/cart/shipping-calculator.php
+++ plugins/woocommerce/templates/cart/shipping-calculator.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 11.1.0
+ * @version 9.7.0
  */
 
 defined( 'ABSPATH' ) || exit;
