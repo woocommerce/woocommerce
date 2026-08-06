5b9d377018e0c5f9b0cad4b85fbf52bb59d91555
Tom Cafferkey <tjcafferkey@gmail.com>
2026-08-06 09:23:16 +0100

    Update order withdrawal notice copy

--- plugins/woocommerce/src/Internal/OrderWithdrawal/OrderWithdrawalFeatureHighlightNotification.php
+++ plugins/woocommerce/src/Internal/OrderWithdrawal/OrderWithdrawalFeatureHighlightNotification.php
@@ -106,7 +106,7 @@ final class OrderWithdrawalFeatureHighlightNotification implements RegisterHooks
 		);
 		$note->set_content(
 			__(
-				'Stores selling to EU countries may need to offer customers a way to withdraw from qualifying orders. WooCommerce includes an order withdrawal feature you can enable.',
+				'Stores selling to EU countries may need to offer customers a way to withdraw from qualifying orders. Review how to enable the order withdrawal feature in the Advanced settings.',
 				'woocommerce'
 			)
 		);
@@ -116,7 +116,7 @@ final class OrderWithdrawalFeatureHighlightNotification implements RegisterHooks
 		$note->set_source( 'woocommerce-admin' );
 		$note->add_action(
 			'review-feature-settings',
-			__( 'Review feature settings', 'woocommerce' ),
+			__( 'Get started', 'woocommerce' ),
 			admin_url( self::FEATURES_SETTINGS_URL ),
 			Note::E_WC_ADMIN_NOTE_ACTIONED,
 			true
