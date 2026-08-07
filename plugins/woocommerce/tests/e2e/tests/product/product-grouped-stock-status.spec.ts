6746655fa4ea2fe4a4edd187da00fdfdc171ff41
Luigi Teschio <gigitux@gmail.com>
2026-08-07 17:21:40 +0200

    Simplify grouped product stock status coverage

--- plugins/woocommerce/tests/e2e/tests/product/product-grouped-stock-status.spec.ts
+++ plugins/woocommerce/tests/e2e/tests/product/product-grouped-stock-status.spec.ts
@@ -25,19 +25,6 @@ const test = baseTest.extend( {
 			{ force: true }
 		);
 	},
-	outOfStockGroupedProduct: async ( { restApi }, fixtureUse ) => {
-		const response = await restApi.post( `${ WC_API_PATH }/products`, {
-			...getFakeProduct( { type: 'grouped' } ),
-			stock_status: 'outofstock',
-		} );
-
-		await fixtureUse( response.data );
-
-		await restApi.delete(
-			`${ WC_API_PATH }/products/${ response.data.id }`,
-			{ force: true }
-		);
-	},
 } );
 
 test( 'resets stock status when converting a product to grouped', async ( {
@@ -71,40 +58,6 @@ test( 'resets stock status when converting a product to grouped', async ( {
 
 	const response = await restApi.get(
 		`${ WC_API_PATH }/products/${ outOfStockProduct.id }`
-	);
-
-	expect( response.data.type ).toBe( 'grouped' );
-	expect( response.data.stock_status ).toBe( 'instock' );
-} );
-
-test( 'resets stock status when loading an out-of-stock grouped product', async ( {
-	page,
-	restApi,
-	outOfStockGroupedProduct,
-} ) => {
-	expect( outOfStockGroupedProduct.type ).toBe( 'grouped' );
-	expect( outOfStockGroupedProduct.stock_status ).toBe( 'outofstock' );
-
-	await page.goto(
-		`wp-admin/post.php?post=${ outOfStockGroupedProduct.id }&action=edit`
-	);
-
-	await expect(
-		page.locator( 'input[name="_stock_status"][value="instock"]' )
-	).toBeChecked();
-
-	await page
-		.locator( '#publishing-action' )
-		.getByRole( 'button', { name: 'Update' } )
-		.click();
-	await expect(
-		page
-			.locator( 'div.notice-success > p' )
-			.filter( { hasText: 'Product updated' } )
-	).toBeVisible();
-
-	const response = await restApi.get(
-		`${ WC_API_PATH }/products/${ outOfStockGroupedProduct.id }`
 	);
 
 	expect( response.data.type ).toBe( 'grouped' );
