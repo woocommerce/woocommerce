028f6004d9b32247ddb9bada7956575b57d93828
luisherranz <luis.herranz@automattic.com>
2026-08-08 09:38:46 +0200

    Align the moved Store API test namespaces with their new directory

--- plugins/woocommerce/tests/php/src/Blocks/StoreApi/Utilities/CartItemUtilsTest.php
+++ plugins/woocommerce/tests/php/src/Blocks/StoreApi/Utilities/CartItemUtilsTest.php
@@ -1,7 +1,7 @@
 <?php
 declare( strict_types = 1 );
 
-namespace Automattic\WooCommerce\Tests\StoreApi\Utilities;
+namespace Automattic\WooCommerce\Tests\Blocks\StoreApi\Utilities;
 
 use Automattic\WooCommerce\StoreApi\Utilities\CartItemUtils;
 
