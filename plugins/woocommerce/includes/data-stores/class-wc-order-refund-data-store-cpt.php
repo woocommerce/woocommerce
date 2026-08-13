4111fe18252c5bdd8211bc3a2393676d4c5bfa50
Md Abdullah Al Arif <faithcoder1@gmail.com>
2026-08-13 17:34:31 +0600

    Fix #67456: Order/refund hydration fatals with "Cannot use object of type stdClass as array" when a postmeta entry is malformed (read_order_data)

--- plugins/woocommerce/includes/data-stores/class-wc-order-refund-data-store-cpt.php
+++ plugins/woocommerce/includes/data-stores/class-wc-order-refund-data-store-cpt.php
@@ -80,14 +80,14 @@ class WC_Order_Refund_Data_Store_CPT extends Abstract_WC_Order_Data_Store_CPT im
 
 		$post_meta = get_post_meta( $id );
 
-		$refunded_by = $post_meta['_refunded_by'][0] ?? null;
-		$reason      = $post_meta['_refund_reason'][0] ?? '';
+		$refunded_by = $this->get_order_meta_value( $post_meta, '_refunded_by', null, $id );
+		$reason      = $this->get_order_meta_value( $post_meta, '_refund_reason', '', $id );
 
 		$refund->set_props(
 			array(
-				'amount'           => $post_meta['_refund_amount'][0] ?? 0,
+				'amount'           => $this->get_order_meta_value( $post_meta, '_refund_amount', 0, $id ),
 				'refunded_by'      => metadata_exists( 'post', $id, '_refunded_by' ) ? $refunded_by : absint( $post_object->post_author ),
-				'refunded_payment' => wc_string_to_bool( $post_meta['_refunded_payment'][0] ?? false ),
+				'refunded_payment' => wc_string_to_bool( $this->get_order_meta_value( $post_meta, '_refunded_payment', false, $id ) ),
 				'reason'           => metadata_exists( 'post', $id, '_refund_reason' ) ? $reason : $post_object->post_excerpt,
 			)
 		);
