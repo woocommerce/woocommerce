14bf9923a9fd8e8a1351bd484f99ed6a09f5b4f2
Luigi Teschio <gigitux@gmail.com>
2026-08-17 11:47:38 +0200

    don't use legacy proxy

--- plugins/woocommerce/src/Internal/TermCount.php
+++ plugins/woocommerce/src/Internal/TermCount.php
@@ -8,7 +8,6 @@ declare( strict_types=1 );
 namespace Automattic\WooCommerce\Internal;
 
 use Automattic\WooCommerce\Enums\ProductStockStatus;
-use Automattic\WooCommerce\Proxies\LegacyProxy;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -28,24 +27,13 @@ defined( 'ABSPATH' ) || exit;
  */
 class TermCount {
 	/**
-	 * Proxy for WordPress and legacy WooCommerce functions.
-	 *
-	 * @var LegacyProxy
-	 */
-	private LegacyProxy $legacy_proxy;
-
-	/**
-	 * Class initialization, to be executed when the class is resolved by the container.
+	 * Class initialization, executed when the class is resolved by the container.
 	 *
 	 * @since 11.1.0
 	 *
 	 * @internal
-	 *
-	 * @param LegacyProxy $legacy_proxy Proxy for WordPress and legacy WooCommerce functions.
 	 */
-	final public function init( LegacyProxy $legacy_proxy ): void {
-		$this->legacy_proxy = $legacy_proxy;
-
+	final public function init(): void {
 		add_action( 'set_object_terms', array( $this, 'handle_set_object_terms' ), 10, 6 );
 		add_action( 'deleted_term_relationships', array( $this, 'handle_deleted_term_relationships' ), 10, 3 );
 	}
@@ -88,7 +76,7 @@ class TermCount {
 		$added_tt_ids = $append ? $new_tt_ids : array_diff( $new_tt_ids, $old_tt_ids );
 
 		if ( ! empty( array_intersect( $added_tt_ids, $counting_tt_ids ) ) ) {
-			$this->recount_product_terms( $object_id );
+			_wc_recount_terms_by_product( $object_id );
 		}
 	}
 
@@ -118,7 +106,7 @@ class TermCount {
 				)
 			)
 		) {
-			$this->recount_product_terms( $object_id );
+			_wc_recount_terms_by_product( $object_id );
 		}
 	}
 
@@ -131,25 +119,16 @@ class TermCount {
 		/**
 		 * Product visibility term taxonomy IDs.
 		 *
-		 * @var array<string, int> $visibility_term_ids
+		 * @var array $visibility_term_ids
 		 */
-		$visibility_term_ids = $this->legacy_proxy->call_function( 'wc_get_product_visibility_term_ids' );
+		$visibility_term_ids = wc_get_product_visibility_term_ids();
 		$counting_tt_ids     = array( $visibility_term_ids['exclude-from-catalog'] ?? 0 );
 
-		if ( 'yes' === $this->legacy_proxy->call_function( 'get_option', 'woocommerce_hide_out_of_stock_items' ) ) {
+		if ( 'yes' === get_option( 'woocommerce_hide_out_of_stock_items' ) ) {
 			$counting_tt_ids[] = $visibility_term_ids[ ProductStockStatus::OUT_OF_STOCK ] ?? 0;
 		}
 
 		return $this->normalize_ids( $counting_tt_ids );
-	}
-
-	/**
-	 * Recounts product category, tag, and brand terms for a product.
-	 *
-	 * @param int $product_id Product ID.
-	 */
-	private function recount_product_terms( int $product_id ): void {
-		$this->legacy_proxy->call_function( '_wc_recount_terms_by_product', $product_id );
 	}
 
 	/**
