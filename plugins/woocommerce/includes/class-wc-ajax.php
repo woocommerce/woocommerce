1c36600d1a8c3dd7b4f4ff121aaec80bdd329d09
Luigi Teschio <gigitux@gmail.com>
2026-08-18 18:00:53 +0200

    Fix stale product term caches after reordering

--- plugins/woocommerce/includes/class-wc-ajax.php
+++ plugins/woocommerce/includes/class-wc-ajax.php
@@ -2325,6 +2325,7 @@ class WC_AJAX {
 		}
 
 		wc_reorder_terms( $term, $next_id, $taxonomy );
+		WC_Cache_Helper::invalidate_cache_group( 'product_terms_' . $taxonomy );
 
 		$children = get_terms( $taxonomy, "child_of=$id&menu_order=ASC&hide_empty=0" );
 
