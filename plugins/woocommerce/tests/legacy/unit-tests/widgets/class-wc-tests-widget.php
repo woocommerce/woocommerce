a5a61eec04295d8adfe5d906988d8ba25180b960
Darren Ethier <darren@roughsmootheng.in>
2026-08-18 10:28:13 -0400

    Fix Recently Viewed Products widget query cap

--- plugins/woocommerce/tests/legacy/unit-tests/widgets/class-wc-tests-widget.php
+++ plugins/woocommerce/tests/legacy/unit-tests/widgets/class-wc-tests-widget.php
@@ -64,4 +64,42 @@ class WC_Tests_Widget extends WC_Unit_Test_Case {
 		$dummy_widget = $wp_widget_factory->widgets['Dummy_Widget'];
 		$this->assertEmpty( $dummy_widget->form( array( 'widget_id' => $dummy_widget->widget_id ) ) );
 	}
+
+	/**
+	 * @testdox Should limit recently viewed widget queries from oversized cookies.
+	 */
+	public function test_recently_viewed_widget_query_limits_oversized_cookie(): void {
+		$viewed_product_ids = range( 1, 20 );
+		$query_args         = array();
+		$query_args_filter  = static function ( $args ) use ( &$query_args ) {
+			$query_args = $args;
+
+			return $args;
+		};
+		$cookies            = $_COOKIE; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized -- Preserve global test cookie state.
+
+		$_COOKIE['woocommerce_recently_viewed'] = implode( '|', $viewed_product_ids );
+		add_filter( 'woocommerce_recently_viewed_products_widget_query_args', $query_args_filter );
+
+		try {
+			$widget = new WC_Widget_Recently_Viewed();
+
+			ob_start();
+			try {
+				$widget->widget( array(), array() );
+			} finally {
+				ob_end_clean();
+			}
+
+			$this->assertSame(
+				array_slice( array_reverse( $viewed_product_ids ), 0, 15 ),
+				$query_args['post__in'],
+				'An oversized recently viewed cookie should only send the 15 newest product IDs to the query.'
+			);
+		} finally {
+			remove_filter( 'woocommerce_recently_viewed_products_widget_query_args', $query_args_filter );
+
+			$_COOKIE = $cookies;
+		}
+	}
 }
