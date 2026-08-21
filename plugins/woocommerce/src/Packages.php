c82fad83e5d2216d8ff90b0bb22fb813f1d4dc1a
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2026-08-21 11:12:45 +0200

    Normalize active plugin option before merging

--- plugins/woocommerce/src/Packages.php
+++ plugins/woocommerce/src/Packages.php
@@ -193,7 +193,7 @@ class Packages {
 		}
 
 		// Scroll through all of the active plugins and disable them if they're merged packages.
-		$active_plugins = get_option( 'active_plugins', array() );
+		$active_plugins = (array) get_option( 'active_plugins', array() );
 		if ( is_multisite() ) {
 			$active_plugins = array_unique(
 				array_merge(
