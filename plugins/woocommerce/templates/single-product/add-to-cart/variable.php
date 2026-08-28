9f7f8b6ff495b8a1e746abefb553db16537ee29a
Darren Ethier <darren@roughsmootheng.in>
2026-08-28 15:08:06 -0400

    Fix template markup contracts and shipping label targets (#68020)

--- plugins/woocommerce/templates/single-product/add-to-cart/variable.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/variable.php
@@ -35,7 +35,7 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 			<tbody>
 				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
 					<tr>
-						<th class="label"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok. ?></label></th>
+						<th class="label"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Attribute names are sanitized on save; the filter output may carry markup by design. ?></label></th>
 						<td class="value">
 							<?php
 								wc_dropdown_variation_attribute_options(
