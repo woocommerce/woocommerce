2134be3caea329528bbd121ef60a2db1aceecc0d
MrJnrman <jnoelreid@gmail.com>
2026-09-02 15:43:03 -0500

    POC: revert the number touch

--- packages/js/number/src/index.ts
+++ packages/js/number/src/index.ts
@@ -151,5 +151,3 @@ export function parseNumber(
 
 	return Number.parseFloat( parsedValue ).toFixed( parsedPrecision );
 }
-
-// POC: a real source change, to trigger the JavaScript unit test matrix.
