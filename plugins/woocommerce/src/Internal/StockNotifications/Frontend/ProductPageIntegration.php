ec7e3fe43235d46e7ac3bb8044f42c15eabec073
Chris Lilitsas <1105590+xristos3490@users.noreply.github.com>
2026-09-08 15:53:25 +0300

    fix: use wc_get_account_endpoint_url to keep endpoint-url filter on stock notification login link

--- plugins/woocommerce/src/Internal/StockNotifications/Frontend/ProductPageIntegration.php
+++ plugins/woocommerce/src/Internal/StockNotifications/Frontend/ProductPageIntegration.php
@@ -152,7 +152,7 @@ class ProductPageIntegration {
 		}
 
 		$text = __( 'Please {login_link} to sign up for stock notifications.', 'woocommerce' );
-		$text = str_replace( '{login_link}', '<a href="' . wc_get_page_permalink( 'myaccount' ) . '">' . _x( 'log in', 'back in stock form', 'woocommerce' ) . '</a>', $text );
+		$text = str_replace( '{login_link}', '<a href="' . wc_get_account_endpoint_url( 'dashboard' ) . '">' . _x( 'log in', 'back in stock form', 'woocommerce' ) . '</a>', $text );
 		wc_print_notice( $text, 'notice' );
 	}
 
