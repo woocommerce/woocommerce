58002cad8f6c44ba2e99370f8348c2861bcb7d57
Valery Sukhomlinov <683297+dmvrtx@users.noreply.github.com>
2026-09-10 19:13:32 +0200

    Remove `skip_on_php_8_1()` on WooCommerce Core

--- plugins/woocommerce/tests/legacy/framework/class-wc-unit-test-case.php
+++ plugins/woocommerce/tests/legacy/framework/class-wc-unit-test-case.php
@@ -587,16 +587,6 @@ class WC_Unit_Test_Case extends WP_HTTP_TestCase {
 	}
 
 	/**
-	 * Skip the current test on PHP 8.1 and higher.
-	 * TODO: Remove this method and its usages once WordPress is compatible with PHP 8.1. Please note that there are multiple copies of this method.
-	 */
-	protected function skip_on_php_8_1() {
-		if ( version_compare( PHP_VERSION, '8.1', '>=' ) ) {
-			$this->markTestSkipped( 'Waiting for WordPress compatibility with PHP 8.1' );
-		}
-	}
-
-	/**
 	 * Get recorded tracks event by name.
 	 *
 	 * @param string $event_name Event name.
