0c794d30303d167296591411565d861afbc3aa15
Luigi Teschio <gigitux@gmail.com>
2026-09-14 13:47:48 +0200

    fix page

--- packages/js/settings-dataform/packages/settings-ui/fields/product/shop-page-id.ts
+++ packages/js/settings-dataform/packages/settings-ui/fields/product/shop-page-id.ts
@@ -1,6 +1,10 @@
 /**
  * External dependencies
  */
+import { store as coreStore } from '@wordpress/core-data';
+import type { Page } from '@wordpress/core-data';
+import { resolveSelect } from '@wordpress/data';
+import { decodeEntities } from '@wordpress/html-entities';
 import { __ } from '@wordpress/i18n';
 
 export default {
@@ -12,5 +16,24 @@ export default {
 		'woocommerce'
 	),
 	Edit: 'select',
-	elements: [],
+	getElements: async () => {
+		const pages: Pick< Page, 'id' | 'title' >[] | null =
+			await resolveSelect( coreStore ).getEntityRecords(
+				'postType',
+				'page',
+				{
+					per_page: -1,
+					orderby: 'title',
+					order: 'asc',
+					_fields: [ 'id', 'title' ],
+				}
+			);
+
+		return ( pages ?? [] ).map( ( page ) => ( {
+			value: String( page.id ),
+			label:
+				decodeEntities( page.title.rendered ) ||
+				__( '(no title)', 'woocommerce' ),
+		} ) );
+	},
 };
