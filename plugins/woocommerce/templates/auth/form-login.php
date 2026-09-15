10f411f18751984841546fd04ade372f91d53f10
Alefe Souza <contact@alefesouza.com>
2026-09-15 15:20:38 -0300

    Set template @version to 11.3.0 after the trunk version bump
    Trunk moved to 11.3.0-dev (#68712) after this branch was created, so
    the three templates changed here now carry the version they will ship
    in.

--- plugins/woocommerce/templates/auth/form-login.php
+++ plugins/woocommerce/templates/auth/form-login.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Auth
- * @version 11.2.0
+ * @version 11.3.0
  */
 
 defined( 'ABSPATH' ) || exit;
