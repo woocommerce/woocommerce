e2e97c5ed8a0fa581e8f5cffdb93b2d976a62d49
Vlad Olaru <vlad@thinkwritecode.com>
2026-09-15 13:03:54 +0300

    Fix brand notice in the My Account payment methods template
    Custom token types whose extension sets no card brand made the template
    emit an "Undefined array key" warning and a strtolower() deprecation. The
    brand is now read with a null-safe fallback, the touched echo sprintf()
    becomes printf(), and the template version is bumped.

--- plugins/woocommerce/templates/myaccount/payment-methods.php
+++ plugins/woocommerce/templates/myaccount/payment-methods.php
@@ -14,7 +14,7 @@
  *
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 8.9.0
+ * @version 11.3.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -44,11 +44,13 @@ do_action( 'woocommerce_before_account_payment_methods', $has_methods ); ?>
 							if ( has_action( 'woocommerce_account_payment_methods_column_' . $column_id ) ) {
 								do_action( 'woocommerce_account_payment_methods_column_' . $column_id, $method );
 							} elseif ( 'method' === $column_id ) {
+								// Custom token types may not set a brand.
+								$brand_label = wc_get_credit_card_type_label( $method['method']['brand'] ?? '' );
 								if ( ! empty( $method['method']['last4'] ) ) {
 									/* translators: 1: credit card type 2: last 4 digits */
-									echo sprintf( esc_html__( '%1$s ending in %2$s', 'woocommerce' ), esc_html( wc_get_credit_card_type_label( $method['method']['brand'] ) ), esc_html( $method['method']['last4'] ) );
+									printf( esc_html__( '%1$s ending in %2$s', 'woocommerce' ), esc_html( $brand_label ), esc_html( $method['method']['last4'] ) );
 								} else {
-									echo esc_html( wc_get_credit_card_type_label( $method['method']['brand'] ) );
+									echo esc_html( $brand_label );
 								}
 							} elseif ( 'expires' === $column_id ) {
 								echo esc_html( $method['expires'] );
---
+++ plugins/woocommerce/tests/php/includes/templates/class-wc-my-account-payment-methods-template-test.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Tests for the My Account payment methods template.
+ */
+
+declare( strict_types = 1 );
+
+use Automattic\WooCommerce\Tests\Blocks\Mocks\PaymentTokenMock;
+
+/**
+ * My Account payment methods template test.
+ */
+class WC_My_Account_Payment_Methods_Template_Test extends WC_Unit_Test_Case {
+
+	/**
+	 * @testdox Should render a saved method of a custom token type that provides no card brand, with its actions.
+	 */
+	public function test_renders_custom_token_without_brand(): void {
+		add_filter(
+			'woocommerce_payment_token_class',
+			static function ( $class_name, $type ) {
+				return 'checkout_test' === $type ? PaymentTokenMock::class : $class_name;
+			},
+			10,
+			2
+		);
+
+		$user_id = self::factory()->user->create();
+		wp_set_current_user( $user_id );
+
+		$token = new PaymentTokenMock();
+		$token->set_token( 'brandless-token' );
+		$token->set_gateway_id( 'bacs' );
+		$token->set_user_id( $user_id );
+		$token->save();
+
+		$html = wc_get_template_html( 'myaccount/payment-methods.php' );
+
+		$this->assertStringContainsString( 'payment-method-method', $html, 'The method column should render for a brand-less token.' );
+		$this->assertStringContainsString( 'class="button delete"', $html, 'The Delete action should render for a brand-less token.' );
+		$this->assertStringNotContainsString( 'Undefined array key', $html, 'The template should not emit PHP notices for a brand-less token.' );
+	}
+}
