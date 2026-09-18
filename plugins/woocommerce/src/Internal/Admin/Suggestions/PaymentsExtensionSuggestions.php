59280ee8549c4de581919d693e5acc7efd9079ee
oaratovskyi <oleksandr.aratovskyi@automattic.com>
2026-09-19 00:32:27 +0300

    fix(payments): Recommend GoCardless in the countries it supports
    The partner tracker lists GoCardless in 26 countries. The suggestions only
    recommended it in 12, so merchants in the other 14 never saw it. In GB it
    was also listed after Kustom Checkout, while the tracker places it before.
    
    Add GoCardless as the last payment provider in ES, GR, IE, IT, LT, LU, LV,
    MT, NL, NZ, PT, SE, SI, and SK, matching the tracker's order in each, and
    move it ahead of Kustom Checkout in GB.

--- plugins/woocommerce/src/Internal/Admin/Suggestions/PaymentsExtensionSuggestions.php
+++ plugins/woocommerce/src/Internal/Admin/Suggestions/PaymentsExtensionSuggestions.php
@@ -263,8 +263,8 @@ class PaymentsExtensionSuggestions {
 			self::VISA,
 			self::AIRWALLEX,
 			self::VIVA_WALLET,
-			self::KUSTOM_CHECKOUT,
 			self::GOCARDLESS,
+			self::KUSTOM_CHECKOUT,
 			self::ELAVON,
 			self::PAYPAL_WALLET,
 			self::AMAZON_PAY,
@@ -722,6 +722,7 @@ class PaymentsExtensionSuggestions {
 			self::MOLLIE,
 			self::VISA,
 			self::VIVA_WALLET,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 			self::KLARNA => array(
 				'_merge_on_type' => array(
@@ -820,6 +821,7 @@ class PaymentsExtensionSuggestions {
 			self::VISA,
 			self::AIRWALLEX,
 			self::VIVA_WALLET,
+			self::GOCARDLESS,
 			self::ELAVON,
 			self::PAYPAL_WALLET,
 			self::AMAZON_PAY,
@@ -853,6 +855,7 @@ class PaymentsExtensionSuggestions {
 			self::VISA,
 			self::AIRWALLEX,
 			self::VIVA_WALLET,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 			self::AMAZON_PAY,
 			self::KLARNA => array(
@@ -883,6 +886,7 @@ class PaymentsExtensionSuggestions {
 			self::STRIPE,
 			self::MOLLIE,
 			self::VISA,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 		),
 		'LI' => array(
@@ -903,6 +907,7 @@ class PaymentsExtensionSuggestions {
 			self::MOLLIE,
 			self::VISA,
 			self::AIRWALLEX,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 		),
 		'LU' => array(
@@ -912,6 +917,7 @@ class PaymentsExtensionSuggestions {
 			self::MOLLIE,
 			self::VISA,
 			self::VIVA_WALLET,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 			self::AMAZON_PAY,
 		),
@@ -922,6 +928,7 @@ class PaymentsExtensionSuggestions {
 			self::MOLLIE,
 			self::VISA,
 			self::VIVA_WALLET,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 		),
 		'MD' => array(
@@ -960,6 +967,7 @@ class PaymentsExtensionSuggestions {
 			self::VISA,
 			self::VIVA_WALLET,
 			self::KUSTOM_CHECKOUT,
+			self::GOCARDLESS,
 			self::ELAVON,
 			self::PAYPAL_WALLET,
 			self::AMAZON_PAY,
@@ -1043,6 +1051,7 @@ class PaymentsExtensionSuggestions {
 			self::VISA,
 			self::AIRWALLEX,
 			self::VIVA_WALLET,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 			self::AMAZON_PAY,
 			self::KLARNA => array(
@@ -1117,6 +1126,7 @@ class PaymentsExtensionSuggestions {
 			self::STRIPE,
 			self::MOLLIE,
 			self::VISA,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 			self::KLARNA => array(
 				'_merge_on_type' => array(
@@ -1139,6 +1149,7 @@ class PaymentsExtensionSuggestions {
 			self::STRIPE,
 			self::MOLLIE,
 			self::VISA,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 		),
 		'ES' => array(
@@ -1164,6 +1175,7 @@ class PaymentsExtensionSuggestions {
 			self::MONEI,
 			self::AIRWALLEX,
 			self::VIVA_WALLET,
+			self::GOCARDLESS,
 			self::ELAVON,
 			self::PAYPAL_WALLET,
 			self::AMAZON_PAY,
@@ -1198,6 +1210,7 @@ class PaymentsExtensionSuggestions {
 			self::VIVA_WALLET,
 			self::KUSTOM_CHECKOUT,
 			self::NEXI_CHECKOUT,
+			self::GOCARDLESS,
 			self::ELAVON,
 			self::PAYPAL_WALLET,
 			self::AMAZON_PAY,
@@ -2059,6 +2072,7 @@ class PaymentsExtensionSuggestions {
 			),
 			self::VISA,
 			self::AIRWALLEX,
+			self::GOCARDLESS,
 			self::PAYPAL_WALLET,
 			self::AFTERPAY,
 			self::KLARNA => array(
--- plugins/woocommerce/tests/php/src/Internal/Admin/Suggestions/fixtures/expected-country-placements.php
+++ plugins/woocommerce/tests/php/src/Internal/Admin/Suggestions/fixtures/expected-country-placements.php
@@ -77,8 +77,8 @@ return array(
 			'visa_as',
 			'airwallex',
 			'viva_wallet',
-			'kustom_checkout',
 			'gocardless',
+			'kustom_checkout',
 			'elavon',
 		),
 		'other_express_checkout' => array(
@@ -314,6 +314,7 @@ return array(
 			'mollie',
 			'visa_as',
 			'viva_wallet',
+			'gocardless',
 		),
 		'other_bnpl'  => array(
 			'klarna',
@@ -363,6 +364,7 @@ return array(
 			'visa_as',
 			'airwallex',
 			'viva_wallet',
+			'gocardless',
 			'elavon',
 		),
 		'other_express_checkout' => array(
@@ -384,6 +386,7 @@ return array(
 			'visa_as',
 			'airwallex',
 			'viva_wallet',
+			'gocardless',
 		),
 		'other_express_checkout' => array(
 			'amazon_pay',
@@ -402,6 +405,7 @@ return array(
 			'stripe',
 			'mollie',
 			'visa_as',
+			'gocardless',
 		),
 	),
 	'LI' => array(
@@ -420,6 +424,7 @@ return array(
 			'mollie',
 			'visa_as',
 			'airwallex',
+			'gocardless',
 		),
 	),
 	'LU' => array(
@@ -430,6 +435,7 @@ return array(
 			'mollie',
 			'visa_as',
 			'viva_wallet',
+			'gocardless',
 		),
 		'other_express_checkout' => array(
 			'amazon_pay',
@@ -443,6 +449,7 @@ return array(
 			'mollie',
 			'visa_as',
 			'viva_wallet',
+			'gocardless',
 		),
 	),
 	'MD' => array(
@@ -465,6 +472,7 @@ return array(
 			'visa_as',
 			'viva_wallet',
 			'kustom_checkout',
+			'gocardless',
 			'elavon',
 		),
 		'other_express_checkout' => array(
@@ -516,6 +524,7 @@ return array(
 			'visa_as',
 			'airwallex',
 			'viva_wallet',
+			'gocardless',
 		),
 		'other_express_checkout' => array(
 			'amazon_pay',
@@ -555,6 +564,7 @@ return array(
 			'stripe',
 			'mollie',
 			'visa_as',
+			'gocardless',
 		),
 		'other_bnpl'  => array(
 			'klarna',
@@ -567,6 +577,7 @@ return array(
 			'stripe',
 			'mollie',
 			'visa_as',
+			'gocardless',
 		),
 	),
 	'ES' => array(
@@ -581,6 +592,7 @@ return array(
 			'monei',
 			'airwallex',
 			'viva_wallet',
+			'gocardless',
 			'elavon',
 		),
 		'other_express_checkout' => array(
@@ -603,6 +615,7 @@ return array(
 			'viva_wallet',
 			'kustom_checkout',
 			'nexi_checkout',
+			'gocardless',
 			'elavon',
 		),
 		'other_express_checkout' => array(
@@ -1236,6 +1249,7 @@ return array(
 			'eway',
 			'visa_as',
 			'airwallex',
+			'gocardless',
 		),
 		'other_bnpl'  => array(
 			'afterpay',
