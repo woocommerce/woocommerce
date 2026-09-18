c9821644380d4b5bb233602cce6d7510f096eb8f
Albert Juhé Lluveras <contact@albertjuhe.com>
2026-09-18 14:57:06 +0200

    Fix code structure

--- plugins/woocommerce/src/Blocks/BlockTypes/CustomerAccount.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/CustomerAccount.php
@@ -358,13 +358,13 @@ class CustomerAccount extends AbstractBlock {
 			return '';
 		}
 
-		$avatar_default = get_option( 'avatar_default', 'mystery' );
-		if ( 'blank' === $avatar_default ) {
-			$avatar_default = 'mystery';
-		}
-
 		$user_id = get_current_user_id();
 		if ( $user_id ) {
+			$avatar_default = get_option( 'avatar_default', 'mystery' );
+			if ( 'blank' === $avatar_default ) {
+				$avatar_default = 'mystery';
+			}
+
 			$avatar = get_avatar(
 				$user_id,
 				48,
