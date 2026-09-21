17322f7c30e57394c3ac28424dd62c554a1b38a3
Tom Cafferkey <tjcafferkey@gmail.com>
2026-09-21 15:34:11 +0100

    Update expected email

--- plugins/woocommerce/src/Internal/CustomerEmailVerification/VerificationEventListener.php
+++ plugins/woocommerce/src/Internal/CustomerEmailVerification/VerificationEventListener.php
@@ -52,11 +52,7 @@ class VerificationEventListener {
 	 */
 	public function on_password_reset( $user ): void {
 		if ( $user instanceof WP_User ) {
-			$current_user = get_user_by( 'id', $user->ID );
-			if ( ! $current_user instanceof \WP_User ) {
-				return;
-			}
-			$this->service->mark_verified( $user->ID, $current_user->user_email );
+			$this->service->mark_verified( $user->ID, $user->user_email );
 		}
 	}
 }
