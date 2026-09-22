242e441b1ae3be44eda0ed0f273709856510574f
Luigi Teschio <gigitux@gmail.com>
2026-09-22 22:16:53 +0200

    restore class

---
+++ plugins/woocommerce/src/Blocks/BlockTypes/AtomicBlock.php
@@ -0,0 +1,12 @@
+<?php
+namespace Automattic\WooCommerce\Blocks\BlockTypes;
+
+/**
+ * AtomicBlock class.
+ *
+ * Do not delete this file until https://github.com/woocommerce/woocommerce/issues/52311 is resolved.
+ * The upgrade flow can still load stale autoloader manifests that point to this file.
+ *
+ * @internal
+ */
+class AtomicBlock extends AbstractBlock {}
