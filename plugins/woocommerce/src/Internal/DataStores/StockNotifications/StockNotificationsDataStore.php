45377c54e59d4060574592ca9637345f9ca67551
Chris Lilitsas <1105590+xristos3490@users.noreply.github.com>
2026-09-23 17:58:15 +0300

    fix: translate stock notification data store error messages

--- plugins/woocommerce/src/Internal/DataStores/StockNotifications/StockNotificationsDataStore.php
+++ plugins/woocommerce/src/Internal/DataStores/StockNotifications/StockNotificationsDataStore.php
@@ -190,7 +190,7 @@ CREATE TABLE $meta_table_name (
 		);
 
 		if ( false === $insert ) {
-			return new \WP_Error( 'db_insert_error', 'Could not insert stock notification into the database.' );
+			return new \WP_Error( 'db_insert_error', __( 'Could not insert stock notification into the database.', 'woocommerce' ) );
 		}
 
 		$notification_id = (int) $wpdb->insert_id;
@@ -260,7 +260,7 @@ CREATE TABLE $meta_table_name (
 		global $wpdb;
 
 		if ( 0 === $notification->get_id() ) {
-			return new \WP_Error( 'invalid_stock_notification', 'Invalid notification ID.' );
+			return new \WP_Error( 'invalid_stock_notification', __( 'Invalid notification ID.', 'woocommerce' ) );
 		}
 
 		$changes = $notification->get_changes();
