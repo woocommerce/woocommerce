2762e0093509f92e501f0aec31460a06fbb33bfb
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2026-09-23 10:45:19 +0800

    Stop URL-encoding array event properties twice in the analytics pixel (#68907)
    * Fix array event properties being URL-encoded twice on the pixel URL
    
    * Add changelog entry for array property double encoding fix

--- packages/php/woocommerce-analytics/tests/php/WC_Analytics_Tracking_Reserved_Props_Test.php
+++ packages/php/woocommerce-analytics/tests/php/WC_Analytics_Tracking_Reserved_Props_Test.php
@@ -517,7 +517,7 @@ class WC_Analytics_Tracking_Reserved_Props_Test extends BaseTestCase {
 		$props     = WC_Analytics_Tracking::get_properties( 'woocommerceanalytics_product_view', $sanitized, true );
 
 		$this->assertSame(
-			rawurlencode( implode( ',', array_fill( 0, WC_Analytics_Tracking::MAX_CLIENT_ARRAY_MEMBERS, 'a' ) ) ),
+			implode( ',', array_fill( 0, WC_Analytics_Tracking::MAX_CLIENT_ARRAY_MEMBERS, 'a' ) ),
 			$props['pc']
 		);
 	}
