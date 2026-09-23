1003e89d81b236a4ca0a6331b471aa21d9a50da3
Tung Du <dinhtungdu@gmail.com>
2026-09-23 17:30:05 +0700

    test: restore settings section after reassignment check

--- plugins/woocommerce/tests/php/includes/settings/class-wc-settings-advanced-test.php
+++ plugins/woocommerce/tests/php/includes/settings/class-wc-settings-advanced-test.php
@@ -141,6 +141,7 @@ class WC_Settings_Advanced_Test extends WC_Settings_Unit_Test_Case {
 		update_option( 'woocommerce_refund_returns_page_id', $old_page_id );
 
 		$original_post    = $_POST; // phpcs:ignore WordPress.Security.NonceVerification.Missing -- Restore the test request after saving.
+		$had_section      = array_key_exists( 'current_section', $GLOBALS );
 		$original_section = $GLOBALS['current_section'] ?? null;
 		try {
 			$GLOBALS['current_section'] = '';
@@ -150,8 +151,12 @@ class WC_Settings_Advanced_Test extends WC_Settings_Unit_Test_Case {
 			$this->assertSame( (string) $new_page_id, get_option( 'woocommerce_refund_returns_page_id' ), 'The new selection should be saved.' );
 			$this->assertSame( $new_page_id, wc_get_page_id( 'refund_returns' ), 'Consumers should resolve the reassigned page.' );
 		} finally {
-			$_POST                      = $original_post;
-			$GLOBALS['current_section'] = $original_section;
+			$_POST = $original_post;
+			if ( $had_section ) {
+				$GLOBALS['current_section'] = $original_section;
+			} else {
+				unset( $GLOBALS['current_section'] );
+			}
 		}
 	}
 
