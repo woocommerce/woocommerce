c73834dacdedbfcf18b3e1302bb2086b1bfb05fe
Daniel Mallory <daniel.mallory@automattic.com>
2026-09-24 21:00:10 +0100

    style: align Shop page search attributes

--- plugins/woocommerce/includes/admin/settings/class-wc-settings-products.php
+++ plugins/woocommerce/includes/admin/settings/class-wc-settings-products.php
@@ -90,7 +90,7 @@ class WC_Settings_Products extends WC_Settings_Page {
 					'default'           => '',
 					'class'             => 'wc-page-search',
 					'custom_attributes' => array(
-						'data-limit'               => 20,
+						'data-limit'                => 20,
 						'data-minimum_input_length' => 0,
 					),
 					'css'               => 'min-width:300px;',
