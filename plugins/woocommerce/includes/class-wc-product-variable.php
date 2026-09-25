1efc555908f527a55acf14e81887598b52b47245
kalessil <kalessil@gmail.com>
2026-09-25 10:27:05 +0200

    Performance: WIP.

--- plugins/woocommerce/includes/class-wc-product-variable.php
+++ plugins/woocommerce/includes/class-wc-product-variable.php
@@ -352,13 +352,12 @@ class WC_Product_Variable extends WC_Product {
 			// Prime caches to reduce future queries.
 			_prime_post_caches( $variation_ids );
 
-
 			$precheck_with_meta = $hide_out_of_stock_items && ! has_filter( 'woocommerce_product_is_in_stock' );
 			foreach ( $variation_ids as $variation_id ) {
 				// Performance note: inactive optimization for most of stores; see details in has_purchasable_variations method.
-				if ( $precheck_with_meta) {
+				if ( $precheck_with_meta ) {
 					$skip = ProductStockStatus::OUT_OF_STOCK === get_post_meta( $variation_id, '_stock_status', true ) ||
-						    ProductStatus::PUBLISH !== get_post_status( $variation_id );
+							ProductStatus::PUBLISH !== get_post_status( $variation_id );
 					if ( $skip ) {
 						continue;
 					}
@@ -427,7 +426,7 @@ class WC_Product_Variable extends WC_Product {
 				// Performance note: applicable to estimated 90% of stores; leverage lightweight primed dat read before constructing product object.
 				if ( $precheck_with_meta ) {
 					$skip = ProductStockStatus::OUT_OF_STOCK === get_post_meta( $variation_id, '_stock_status', true ) ||
-						    ProductStatus::PUBLISH !== get_post_status( $variation_id );
+							ProductStatus::PUBLISH !== get_post_status( $variation_id );
 					if ( $skip ) {
 						continue;
 					}
