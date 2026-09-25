88032976688c939f72ff8b2ffa458e2d51d09cda
kalessil <kalessil@gmail.com>
2026-09-25 11:40:16 +0200

    Performance: WIP.

--- plugins/woocommerce/includes/class-wc-product-variable.php
+++ plugins/woocommerce/includes/class-wc-product-variable.php
@@ -352,11 +352,15 @@ class WC_Product_Variable extends WC_Product {
 			// Prime caches to reduce future queries.
 			_prime_post_caches( $variation_ids );
 
-			$run_meta_prechecks = $hide_out_of_stock_items && ! has_filter( 'woocommerce_product_is_in_stock' );
+			$precheck_with_meta = $hide_out_of_stock_items && ! has_filter( 'woocommerce_product_is_in_stock' );
 			foreach ( $variation_ids as $variation_id ) {
-				// Performance note: inactive optimization for most of stores; leverage lightweight primed data read before constructing product object.
-				if ( $run_meta_prechecks && ProductStockStatus::OUT_OF_STOCK === get_post_meta( $variation_id, '_stock_status', true ) ) {
-					continue;
+				// Performance note: inactive optimization for most of stores; see details in has_purchasable_variations method.
+				if ( $precheck_with_meta ) {
+					$skip = ProductStockStatus::OUT_OF_STOCK === get_post_meta( $variation_id, '_stock_status', true ) ||
+							ProductStatus::PUBLISH !== get_post_status( $variation_id );
+					if ( $skip ) {
+						continue;
+					}
 				}
 
 				$variation = wc_get_product( $variation_id );
@@ -417,11 +421,15 @@ class WC_Product_Variable extends WC_Product {
 			// Prime caches to reduce future queries.
 			_prime_post_caches( $variation_ids );
 
-			$run_meta_prechecks = ! has_filter( 'woocommerce_product_is_in_stock' );
+			$precheck_with_meta = ! has_filter( 'woocommerce_product_is_in_stock' );
 			foreach ( $variation_ids as $variation_id ) {
-				// Performance note: applicable to estimated 90% of stores; leverage lightweight primed data read before constructing product object.
-				if ( $run_meta_prechecks && ProductStockStatus::OUT_OF_STOCK === get_post_meta( $variation_id, '_stock_status', true ) ) {
-					continue;
+				// Performance note: applicable to estimated 90% of stores; leverage lightweight primed dat read before constructing product object.
+				if ( $precheck_with_meta ) {
+					$skip = ProductStockStatus::OUT_OF_STOCK === get_post_meta( $variation_id, '_stock_status', true ) ||
+							ProductStatus::PUBLISH !== get_post_status( $variation_id );
+					if ( $skip ) {
+						continue;
+					}
 				}
 
 				$variation = wc_get_product( $variation_id );
